65e0ca831ad995b176cb657e036e405927a2f153
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-10-22 18:09:46 +0900

    refactor: CarGameManager private 메소드 분리 & 주석 작성

--- src/main/java/racingcar/CarGameManager.java
+++ src/main/java/racingcar/CarGameManager.java
@@ -12,10 +12,24 @@ public class CarGameManager {
     }
 
     public void startGame() {
-        RacingCarConsoleWriter.writeInputMessage(RacingCarConsoleWriter.InputType.READ_CAR_NAMES);
-        String input = ConsoleReader.readInput();
+        String input = readCarInput();
         List<Car> cars = carFactory.createCarsFromInput(input);
 
+        int roundCount = readRoundCount();
+
+        playGame(cars, roundCount, moveCondition);
+    }
+
+    private String readCarInput() {
+        RacingCarConsoleWriter.writeInputMessage(RacingCarConsoleWriter.InputType.READ_CAR_NAMES);
+        return ConsoleReader.readInput();
+    }
+
+    /**
+     * 시도 횟수를 입력받는다.
+     * 양의 정수가 아닌 경우 예외를 발생시킨다.
+     */
+    private int readRoundCount() {
         RacingCarConsoleWriter.writeInputMessage(RacingCarConsoleWriter.InputType.READ_TRY_COUNT);
         String roundCountInput = ConsoleReader.readInput();
         int roundCount;
@@ -24,18 +38,18 @@ public class CarGameManager {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
         }
-        if(roundCount < 1) {
+        if (roundCount < 1) {
             throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
         }
-
-
-        List<String> topCarNames = playGame(cars, roundCount, moveCondition);
-
-        RacingCarConsoleWriter.writeWinnerMessage(topCarNames);
+        return roundCount;
     }
 
 
-    private List<String> playGame(List<Car> cars, int roundCount, MoveCondition moveCondition) {
+    /**
+     * `CarGame`을 생성하고, 게임을 진행한다.
+     * 게임 진행 내역을 매 라운드마다 출력하고, 게임이 끝나면 우승자를 출력한다.
+     */
+    private void playGame(List<Car> cars, int roundCount, MoveCondition moveCondition) {
         CarGame carGame = new CarGame(moveCondition, roundCount, cars);
 
         RacingCarConsoleWriter.writeRoundStartMessage();
@@ -45,7 +59,8 @@ public class CarGameManager {
             RacingCarConsoleWriter.writeCarState(currentRacingCarStates);
         }
 
-        return carGame.getTopCarNames();
+        List<String> topCarNames = carGame.getTopCarNames();
+        RacingCarConsoleWriter.writeWinnerMessage(topCarNames);
     }
 
 
