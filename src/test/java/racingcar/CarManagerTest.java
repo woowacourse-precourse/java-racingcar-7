686ed0390928316fa295b2d3bc9e34fe4afd8981
박철현 <park82602@gmail.com>
2024-10-22 20:33:28 +0900

    Test : 음수 입력 시 에러가 정상적으로 던져지는지에 대한 테스트 코드를 작성했습니다

---
+++ src/test/java/racingcar/CarManagerTest.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarManagerTest {
+
+    @Test
+    void count_exception() {
+        //given
+        String count = "-5";
+        CarManager manager = new CarManager();
+        //when //then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> manager.run(count));
+
+    }
+}
