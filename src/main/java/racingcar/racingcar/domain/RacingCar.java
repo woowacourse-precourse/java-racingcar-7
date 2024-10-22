ed202e6558889498b947967b13af3b13b03e6e12
EHCTH <lees90896@gmail.com>
2024-10-22 16:36:39 +0900

    feat(RacingCar): Racing 클래스 작성 * feat: 정적 RacingCar 생성 메서드 추가 * feat: vo 사용

--- src/main/java/racingcar/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/racingcar/domain/RacingCar.java
@@ -1,4 +1,31 @@
 package racingcar.racingcar.domain;
 
+import java.util.Objects;
+
 public class RacingCar {
+    private final Name name;
+    private RacingCar(Name name) {
+        this.name = name;
+    }
+    public static RacingCar of(String name) {
+        return new RacingCar(
+                new Name(name)
+        );
+    }
+    @Override
+    public String toString() {
+        return name.toString();
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj instanceof RacingCar that) {
+            return this.name.equals(that.name);
+        }
+        return false;
+    }
 }
