709fba533afbc10ce631d6a605c479a80f91ca8d
박성춘 <songchoon10@naver.com>
2024-10-22 20:27:19 +0900

    feat: 시도횟수 입력받는 기능 구현 완료
    사용자로부터 시도 횟수를 입력받는 기능을 완료하였습니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,10 +7,19 @@ public class InputView {
     private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
     public String readCarNames() {
+        System.out.println(INPUT_NAME_MESSAGE);
         String carNames = Console.readLine();
         checkCarNameIsNull(carNames);
 
         return carNames;
+    }
+
+    public int readTryCount() {
+        System.out.println(INPUT_TRY_COUNT_MESSAGE);
+        String tryCount = Console.readLine();
+
+        checkCarNameIsNull(tryCount);
+        return Integer.parseInt(tryCount);
     }
 
     private void checkCarNameIsNull(String carNames) {
