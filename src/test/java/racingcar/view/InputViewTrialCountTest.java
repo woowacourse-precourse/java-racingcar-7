91d108dc83e34eb7723ade2da6893b1f22ddfa65
corpi <lj845854@gmail.com>
2024-10-23 06:25:40 +0900

    test(InputViewTrialCountTest): 시도 횟수 입력을 받는다.

---
+++ src/test/java/racingcar/view/InputViewTrialCountTest.java
@@ -0,0 +1,42 @@
+package racingcar.view;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class InputViewTrialCountTest extends NsTest {
+    private InputView inputView;
+
+    @BeforeEach
+    public void setup() {
+        inputView = new InputView();
+    }
+
+    @DisplayName("시도 횟수 입력을 받는다.")
+    @MethodSource("inputTrialCount")
+    @ParameterizedTest
+    public void inputTrialCount(String input, String output) {
+        run(input);
+        assertThat(output().contains(output)).isTrue();
+    }
+
+    private static Stream<Arguments> inputTrialCount() {
+        return Stream.of(
+                Arguments.of("1", "1"),
+                Arguments.of("23", "23"),
+                Arguments.of("2147483647", "2147483647")
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        int trialCount = inputView.inputTrialCount();
+        System.out.println(trialCount);
+    }
+}
