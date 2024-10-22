a01c983305abe3a511393deee6d1e770deb9513c
Yehun <yhyh4420@naver.com>
2024-10-22 18:49:52 +0900

    feat: 전진 조건에 따른 해쉬맵 갱신 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,23 +1,32 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashMap;
 
 public class Application {
-    public static HashMap<String, Object> getUserInfo() {
+    public static HashMap<String, Integer> getUserInfo() {
         String input = Console.readLine();
-        int times = Integer.parseInt(Console.readLine());
         String[] StrArr_Name = input.split(",");
-
-        HashMap<String, Object> scoreMap = new HashMap<String, Object>();
+        int times = Integer.parseInt(Console.readLine());
+        HashMap<String, Integer> scoreMap = new HashMap<>();
         for (String name:StrArr_Name) {
             scoreMap.put(name, 0);
-        };
-        return scoreMap;
+        }
+        for (int i = 0; i < times; i++) {
+            for (String name:scoreMap.keySet()) {
+                if (Randoms.pickNumberInRange(0,9) >= 4){
+                    int step = scoreMap.get(name) + 1;
+                    scoreMap.put(name, step);
+                }
+            }
+        }
 
+        return scoreMap;
     }
 
     public static void main(String[] args) {
-        getUserInfo();
+        System.out.println(getUserInfo());
+
     }
 }
