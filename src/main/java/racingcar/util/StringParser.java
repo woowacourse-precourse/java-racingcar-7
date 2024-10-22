95f48c347944b4f38057844c04309dc829c26194
Hacanna42 <dtmdgus901@gmail.com>
2024-10-23 02:54:09 +0900

    feat(util): change regex and add trim to makeList()
    경주 자동차를 입력받을 때, 이제 반점(구분자) 사이의 스페이스를 허용합니다.
    따라서 StringParser의 makeList() 메서드가 각 요소를 .trim() 하도록 변경했습니다.
    단, 자동차 이름에는 공백이 포함될 수 없습니다.

--- src/main/java/racingcar/util/StringParser.java
+++ src/main/java/racingcar/util/StringParser.java
@@ -1,11 +1,14 @@
 package racingcar.util;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class StringParser {
     private static final String DELIMITER = ",";
 
     public List<String> makeList(String input) {
-        return List.of(input.split(DELIMITER));
+        return Arrays.stream(input.split(DELIMITER))
+                .map(String::trim)
+                .toList();
     }
 }
