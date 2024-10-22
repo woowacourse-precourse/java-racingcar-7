0e0eeb5c09899ff249b2932780ccf5b4fef4e417
yngbao97 <yukyj0418@gmail.com>
2024-10-22 20:40:25 +0900

    feat: 자동차 이름 입력 안내 문구 출력 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+
+
+    }
+
+    private void run() {
+
+        print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+    }
+
+    private void print(String message) {
+        System.out.println(message);
     }
 }
