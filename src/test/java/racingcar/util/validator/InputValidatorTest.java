7c34c0b716341e097a9344257c090c8def3b6fa3
Firedrago95 <yonghwa95@naver.com>
2024-10-22 15:52:21 +0900

    test(InputValidatorTest): 자동차 이름 검증 테스트 코드 작성

---
+++ src/test/java/racingcar/util/validator/InputValidatorTest.java
@@ -0,0 +1,36 @@
+package racingcar.util.validator;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class InputValidatorTest {
+
+    @Test
+    void 자동차_이름_검증_테스트() {
+        //given
+        String input1 = "포비,Alex,james";
+        String input2 = "포비,알lexs,제임s";
+
+        //then
+        assertDoesNotThrow(() -> InputValidator.validateCarNames(input1));
+        assertDoesNotThrow(() -> InputValidator.validateCarNames(input2));
+    }
+
+    @Test
+    void 자동차_이름_검증_예외_테스트() {
+        //given
+        String input1 = "LongNameMan,short";
+        String input2 = "john:rock";
+        String input3 = "";
+
+        //then
+        assertThrows(IllegalArgumentException.class,
+            () -> InputValidator.validateCarNames(input1));
+        assertThrows(IllegalArgumentException.class,
+            () -> InputValidator.validateCarNames(input2));
+        assertThrows(IllegalArgumentException.class,
+            () -> InputValidator.validateCarNames(input3));
+    }
+}
