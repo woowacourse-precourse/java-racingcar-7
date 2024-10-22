35377222966df3d8f522136310c45a8990b048eb
rdyjun <rdyjun00@gmail.com>
2024-10-22 22:37:30 +0900

    refactor: 자동차 경주 로직 순서 변경
    필요한 입력을 모두 받은 후 로직을 수행하도록 수정

--- src/main/java/racingcar/CarRacing.java
+++ src/main/java/racingcar/CarRacing.java
@@ -9,13 +9,13 @@ public class CarRacing {
 
     public static void start() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-
         String carNames = Console.readLine();
-        initCarArray(carNames);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
-
         String tryCount = Console.readLine();
+
+        initCarArray(carNames);
+
         validateTryCountInput(tryCount);
         CarRacing.tryCount = Integer.parseInt(tryCount);
 
