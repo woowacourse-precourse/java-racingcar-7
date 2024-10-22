7626df0549674cd7f5991104e3f2e19ebc2d703e
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-10-23 03:03:19 +0900

    test(InputMessageTest): 테스트 코드 추가
    - 몇가지 입력이 정상적으로 실행되는 지 테스트
    - 자동차 이름에 문자와 숫자 이외의 것들이 입력되었는 지 테스트
    
    - 정상적으로 실행이 되는 지 검사했고, 특수 문자나 공백 같은 것을
      제거하고 싶어서 위 테스트를 추가하였습니다.

--- src/test/java/racingcar/InputMessageTest.java
+++ src/test/java/racingcar/InputMessageTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -9,11 +10,37 @@ import org.junit.jupiter.api.Test;
 
 public class InputMessageTest {
     @Test
+    public void 정상적으로_실행() {
+        InputMessage inputMessage = new InputMessage();
+
+        assertDoesNotThrow(() -> {
+            inputMessage.splitCarName("a, ab, abc, abcd, abcde");
+        });
+
+        assertDoesNotThrow(() -> {
+            inputMessage.splitCarName("a3, B2");
+        });
+    }
+
+    @Test
     void 자동차_이름이_5자를_초과하지_않는_지_테스트(){
         InputMessage inputMessage = new InputMessage();
 
         assertThrows(IllegalArgumentException.class, () -> {
             inputMessage.splitCarName("abc, abcd, abcdef");
+        });
+    }
+
+    @Test
+    void 자동차_이름에_문자와_숫자만_들어있는_지_테스트(){
+        InputMessage inputMessage = new InputMessage();
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            inputMessage.splitCarName("abc, ab/");
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            inputMessage.splitCarName("abc, ab d");
         });
     }
 }
