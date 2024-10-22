fa840e1011e9eab5412df9a363fa0b6a3671af16
jbilee <julishahn@gmail.com>
2024-10-23 01:59:23 +0900

    test(validator): RoundInputValidator 기능에 대한 테스트 코드 작성

---
+++ src/test/java/racingcar/RoundInputValidatorTest.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.validators.RoundInputValidator;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class RoundInputValidatorTest {
+    private final RoundInputValidator validator = new RoundInputValidator();
+
+    @DisplayName("시도 횟수로 입력한 값이 유효하지 않으면 예외를 발생시킨다.")
+    @Test
+    void 횟수_입력값_검증_테스트() {
+        assertThrows(RuntimeException.class, () -> validator.validate("asdf"));
+        assertThrows(RuntimeException.class, () -> validator.validate("0"));
+        assertThrows(RuntimeException.class, () -> validator.validate("-10"));
+        assertThrows(RuntimeException.class, () -> validator.validate("1.23"));
+    }
+}
