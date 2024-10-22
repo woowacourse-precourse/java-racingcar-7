6ba88948b7c2f6ebc20eec4b673cde59ef8defba
박철현 <park82602@gmail.com>
2024-10-22 20:13:10 +0900

    Style : 기존의 코드 수정에 따른 Test 코드도 통과할 수 있도록 수정했습니다

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class CarTest {
 
     @Test
@@ -14,27 +15,42 @@ class CarTest {
         String case1 = "pobi,woni,jun";
         String case2 = "pobi";
 
+        CarManager manager1 = new CarManager();
+        manager1.add(case1);
+
+        CarManager manager2 = new CarManager();
+        manager2.add(case2);
+
         //when
-        List<Car> cars1 = Car.generateCars(case1);
-        List<Car> cars2 = Car.generateCars(case2);
+
+        List<Car> list1 = manager1.getList();
+        List<Car> list2 = manager2.getList();
 
         //then
         Assertions.assertAll(
-                () -> assertEquals(3, cars1.size()),
-                () -> assertEquals(1, cars2.size()),
-                () -> assertEquals(cars1.getFirst().getName(), "pobi")
+                () -> assertEquals(3, list1.size()),
+                () -> assertEquals(1, list2.size()),
+                () -> assertEquals(list1.getFirst().getName(), "pobi")
         );
-;    }
+
+    }
+
     @Test
     void createCar_exception() {
         //given
         String case1 = "hello";
         String case2 = "helloWorld";
 
+        CarManager manager1 = new CarManager();
+        manager1.add(case1);
+
+        CarManager manager2 = new CarManager();
+
         //when // then
         Assertions.assertAll(
-                () -> assertEquals(1, Car.generateCars(case1).size()),
-                () -> assertThrows(IllegalArgumentException.class, () -> Car.generateCars(case2))
+                () -> assertEquals(1, manager1.getList().size()),
+                () -> assertThrows(IllegalArgumentException.class, () -> manager2.add(case2))
         );
+
     }
 }
