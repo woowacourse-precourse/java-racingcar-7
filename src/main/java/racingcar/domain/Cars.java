58efaea73d975fff040cd04c8a90289ffc6302e0
dnwldla <leeemjw@naver.com>
2024-10-22 21:06:40 +0900

    feat: 올바른 입력값을 검증하는 Cars 클래스 추가

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,42 @@
+package racingcar.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Cars {
+
+    private static final String DUPLICATED_NAME="각 차는 한번만 입력해야 합니다";
+    private static final String OUT_OF_RANGE_NAME="차는 다섯글자 이하이어야 합니다";
+
+    private static final String SPLITOR=",";
+    private static final Integer HIGHEST_NAME_LENGTH=5;
+
+    private List<Car> cars;
+
+    public Cars(String carsInput){
+        validateDuplicatedName(carsInput);
+        validateOutOfRangeName(carsInput);
+    }
+
+    private static void validateDuplicatedName(String carsInput){
+        long before=carsInput.split(SPLITOR).length;
+
+        long after=Arrays.stream(carsInput.split(SPLITOR)).
+                distinct().
+                count();
+
+        if (before!=after){
+            throw new IllegalArgumentException(DUPLICATED_NAME);
+        }
+    }
+
+    private void validateOutOfRangeName(String carsInput) {
+        boolean result=Arrays.stream(carsInput.split(SPLITOR))
+                .anyMatch((car)->car.length()>HIGHEST_NAME_LENGTH);
+
+        if (result){
+            throw new IllegalArgumentException(OUT_OF_RANGE_NAME);
+        }
+    }
+
+}
