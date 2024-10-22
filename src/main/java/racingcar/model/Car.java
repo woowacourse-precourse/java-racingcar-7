292ac7618357258d417dd291748b0d4c10df050c
corpi <lj845854@gmail.com>
2024-10-23 04:39:20 +0900

    feat(Car): 자동차는 전진할 수 있다.
    0에서 9의 무작위 값 중 4이상이면 position을 1 증가시킨다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,5 +1,7 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
     private int position;
@@ -11,5 +13,13 @@ public class Car {
 
     public static Car of(String name) {
         return new Car(name, 0);
+    }
+
+    public void moveIfPossible() {
+        int number = Randoms.pickNumberInRange(0, 9);
+
+        if (number >= 4) {
+            this.position += 1;
+        }
     }
 }
