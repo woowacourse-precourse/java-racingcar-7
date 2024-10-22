47b5e4be52df636d5e91b0c58b5eb22058ad81b7
박성춘 <songchoon10@naver.com>
2024-10-22 23:49:34 +0900

    feat: 레이싱 결과를 출력하는 로직 구현 완료
    - docs: '경기 기록 및 우승 자동차이름 출력' 항목 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,11 +3,12 @@ package racingcar;
 import racingcar.controller.RacingController;
 import racingcar.service.RacingService;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RacingController racingController = new RacingController(new InputView(), new RacingService());
+        RacingController racingController = new RacingController(new InputView(), new OutputView(), new RacingService());
 
         racingController.run();
     }
--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -5,13 +5,16 @@ import racingcar.dto.RacingResult;
 import racingcar.model.Racing;
 import racingcar.service.RacingService;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingController {
     private final InputView inputView;
+    private final OutputView outputView;
     private final RacingService racingService;
 
-    public RacingController(InputView inputView, RacingService racingService) {
+    public RacingController(InputView inputView, OutputView outputView, RacingService racingService) {
         this.inputView = inputView;
+        this.outputView = outputView;
         this.racingService = racingService;
     }
 
@@ -19,9 +22,15 @@ public class RacingController {
         Racing racing = new Racing(readRacingInfo());
 
         RacingResult racingResult = racingService.racing(racing);
+
+        printRacingResult(racingResult);
     }
 
     private RacingInfo readRacingInfo() {
         return inputView.readRacingInfo();
+    }
+
+    private void printRacingResult(RacingResult racingResult) {
+        outputView.printRacingResult(racingResult);
     }
 }
