c2f908d74fba85b3b6d9275fbbb1d9729a05d5f1
urim96 <qusdbfla1@gmail.com>
2024-10-22 20:35:59 +0900

    사용자에게 시도할 횟수 입력받기
    값 유효성 검사하는 클래스 생성
    입력받은 값이 0이상의 숫자인지 확인하는 메소드 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,8 +8,13 @@ public class Application {
     	try{
     		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분 / 자동차 이름은 5자 이하)");
     		String carName = Console.readLine();
-    		CarArrayCheck check = new CarArrayCheck();
-    		check.carNameSplit(carName);
+    		CarArrayCheck carCheck = new CarArrayCheck();
+    		carCheck.carNameSplit(carName);
+    		
+    		System.out.println("시도할 횟수는 몇 회인가요?");
+    		String playCnt = Console.readLine();
+    		PlayCntCheck playCntCheck = new PlayCntCheck();
+    		playCntCheck.playCntInt(playCnt);
     	}catch(Exception e){
     		throw new IllegalArgumentException(); 
     	}
