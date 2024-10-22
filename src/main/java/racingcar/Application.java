6e2b758484349d9ae02dbf4e433171a4a315ed36
YongLeeCode <yonghwan311@gmail.com>
2024-10-23 00:02:01 +0900

    test the split and random function

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("test!");
+        String racingGame = "pobi,woni,jun,gjd,adfi,adsf";
+        
+        String[] a = racingGame.split(",");
+        int try_it = 5;
+        for (int i = 0; i < try_it; i++) {
+            int num = Randoms.pickNumberInRange(0, 9);
+            System.out.println(num);
+        }
+        
+        
+
     }
 }
