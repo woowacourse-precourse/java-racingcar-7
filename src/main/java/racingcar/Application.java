e0a618a03b017be78f5a58b7a097f93aa58dfdbc
KIMB0B <axzsd96301@gmail.com>
2024-10-22 23:37:29 +0900

    feat(Application): 전체 차량 랜덤으로 이동 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,5 +15,18 @@ public class Application {
         for (String carName : carNames) {
             cars.put(carName, 0);
         }
+    }
+
+    public static void moveCars(Map<String, Integer> cars) {
+        for (String carName : cars.keySet()) {
+            if (isMoveSuccess()) {
+                cars.put(carName, cars.get(carName) + 1);
+            }
+        }
+    }
+
+    public static boolean isMoveSuccess() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        return randomNumber >= 4;
     }
 }
