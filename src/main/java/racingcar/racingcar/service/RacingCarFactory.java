d32d43c1fef755fb2d3d9ddb4001f2efa82bc22b
EHCTH <lees90896@gmail.com>
2024-10-22 16:38:39 +0900

    feat(RacingCarFactory): RacingCarInfo를 생성하는 팩토리 클래스 생성 * feat: create() 기능 추가

--- src/main/java/racingcar/racingcar/service/RacingCarFactory.java
+++ src/main/java/racingcar/racingcar/service/RacingCarFactory.java
@@ -1,4 +1,17 @@
 package racingcar.racingcar.service;
 
-public class RacingCarFactroy {
+import racingcar.racingcar.domain.RacingCar;
+import racingcar.racingcar.dto.RacingCarInfo;
+import racingcar.racingcar.util.RacingCarConvertor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingCarFactory {
+    public static RacingCarInfo create(String carName) {
+        List<String> carNameInfo = RacingCarConvertor.splitByCommaToList(carName);
+        return carNameInfo.stream()
+                .map(RacingCar::of)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), RacingCarInfo::new));
+    }
 }
