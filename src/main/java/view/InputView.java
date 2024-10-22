2dbd34c03b5c18f0b83610840a0e2eda8e32537e
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-10-22 19:05:16 +0900

    feat: 쉼표가 아닌 다른 구분자 입력된 경우 예외 처리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,12 +2,28 @@ package view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputView {
+
+    private static final String DELIMITER_ERROR_MESSAGE = "올바른 구분자를 입력해야 합니다.";
+
     private static final String DELIMITER = ",";
+    private static final String OTHER_DELIMITER_REGEX = "[^,\\w\\s]";
 
     public static String[] getCarNames() {
         String input = Console.readLine();
+        validateDelimiter(input);
         String[] carNames = input.split(DELIMITER);
         return carNames;
+    }
+
+    private static void validateDelimiter(final String input) {
+        Pattern pattern = Pattern.compile(OTHER_DELIMITER_REGEX);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            throw new IllegalArgumentException(DELIMITER_ERROR_MESSAGE);
+        }
     }
 }
