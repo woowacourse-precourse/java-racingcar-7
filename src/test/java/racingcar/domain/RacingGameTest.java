0a8d7e41e44d06d1b726734952294258770f2bee
openmpy <suhwan@kakao.com>
2024-10-23 01:17:48 +0900

    test: RacingGame 클래스에 대한 단위 테스트 추가
    - 정상적인 레이싱 게임 시작
    - 레이싱 게임 단독 우승자
    - 레이싱 게임 공동 우승자

---
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -0,0 +1,72 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RacingGameTest {
+
+    private List<Car> cars;
+
+    @BeforeEach
+    void setUp() {
+        cars = List.of(new Car("1"), new Car("2"), new Car("3"));
+    }
+
+    @Test
+    void 정상적인_레이싱_게임_시작() {
+        // given
+        int gameRound = 5;
+        RacingGame racingGame = new RacingGame(cars, gameRound);
+
+        // when
+        racingGame.play();
+    }
+
+    @Test
+    void 레이싱_게임_단독_우승자() {
+        // given
+        int randomNumber = 4;
+        int gameRound = 5;
+        RacingGame racingGame = new RacingGame(cars, gameRound);
+
+        // when
+        IntStream.range(0, 5).forEach(i -> {
+            cars.get(0).forward(randomNumber);
+        });
+
+        List<Car> winners = racingGame.getWinners();
+
+        // then
+        assertThat(winners).hasSize(1);
+        assertThat(winners.getFirst().getName()).isEqualTo("1");
+        assertThat(winners.getFirst().getDistance()).isEqualTo(5);
+    }
+
+    @Test
+    void 레이싱_게임_공동_우승자() {
+        // given
+        int randomNumber = 4;
+        int gameRound = 5;
+
+        RacingGame racingGame = new RacingGame(cars, gameRound);
+
+        // when
+        IntStream.range(0, 5).forEach(i -> {
+            cars.get(0).forward(randomNumber);
+            cars.get(1).forward(randomNumber);
+        });
+
+        List<Car> winners = racingGame.getWinners();
+
+        // then
+        assertThat(winners).hasSize(2);
+        assertThat(winners.getFirst().getName()).isEqualTo("1");
+        assertThat(winners.getFirst().getDistance()).isEqualTo(5);
+        assertThat(winners.getLast().getName()).isEqualTo("2");
+        assertThat(winners.getLast().getDistance()).isEqualTo(5);
+    }
+}
