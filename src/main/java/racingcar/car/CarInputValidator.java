3fb20b9f0010511e448bdbcec3b8594ac450b049
sik9252 <sik9252@naver.com>
2024-10-22 22:13:03 +0900

    refactor: 문맥을 중복하는 이름을 자제하기 위해 메서드 내부에서 CarName 텍스트 제거

--- src/main/java/racingcar/car/CarInputValidator.java
+++ src/main/java/racingcar/car/CarInputValidator.java
@@ -6,30 +6,30 @@ import java.util.HashSet;
 
 public class CarInputValidator {
     public void checkIsCarNameValid(String carName, HashSet<String> carsToCheckDuplicate) {
-        checkIsCarNameEmpty(carName);
-        checkCarNameLength(carName);
-        checkIsCarNameAlreadyExist(carName, carsToCheckDuplicate);
+        checkIsEmpty(carName);
+        checkLength(carName);
+        checkIsAlreadyExist(carName, carsToCheckDuplicate);
     }
 
-    public void checkIsCarInputEmpty(String carInput) {
+    public void checkIsAllInputEmpty(String carInput) {
         if (carInput.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_IS_CAR_INPUT_EMPTY.getMessage());
         }
     }
 
-    private void checkCarNameLength(String carName) {
+    private void checkLength(String carName) {
         if (carName.length() > 5) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_IS_CAR_NAME_TO_LONG.getMessage() + ": " + carName);
         }
     }
 
-    private void checkIsCarNameEmpty(String carName) {
+    private void checkIsEmpty(String carName) {
         if (carName.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_IS_CAR_NAME_EMPTY.getMessage() + ": " + carName);
         }
     }
 
-    private void checkIsCarNameAlreadyExist(String carName, HashSet<String> carsToCheckDuplicate) {
+    private void checkIsAlreadyExist(String carName, HashSet<String> carsToCheckDuplicate) {
         if (carsToCheckDuplicate.contains(carName)) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_IS_CAR_NAME_ALREADY_EXIST.getMessage() + ": " + carName);
         }
