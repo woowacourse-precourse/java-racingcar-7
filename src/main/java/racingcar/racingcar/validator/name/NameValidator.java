6c71a44da691e05ed5cebbbb378d7241df937037
EHCTH <lees90896@gmail.com>
2024-10-22 16:42:30 +0900

    feat(NameValidator): Name 예외 처리 클래스 추가

--- src/main/java/racingcar/racingcar/validator/name/NameValidator.java
+++ src/main/java/racingcar/racingcar/validator/name/NameValidator.java
@@ -1,4 +1,7 @@
 package racingcar.racingcar.validator.name;
 
 public class NameValidator {
+    public static void withingRangeNameLength(String name) {
+        if (name.length() > 5) throw new IllegalArgumentException("[ERROR] 이름 길이가 5초과입니다");
+    }
 }
