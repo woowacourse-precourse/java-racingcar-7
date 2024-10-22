aed82ab85ddc83748c845d0021430672f50fadef
배석현 <bjh4966@naver.com>
2024-10-22 21:21:46 +0900

    5.우승 결정 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -56,4 +56,16 @@ public class Application {
             System.out.println(car);
         }
     }
+    private static void Winners(List<Car> cars) {
+        int maxPosition = cars.stream().mapToInt(Car::getPosition).max().orElse(0);
+        List<String> winners = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
 }
