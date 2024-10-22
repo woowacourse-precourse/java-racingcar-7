58639a7ffcbc17fe7e47a3a2b180a12a3ab13208
박철현 <park82602@gmail.com>
2024-10-22 19:41:04 +0900

    Style : given, when, then 주석을 테스트 코드에 추가하였습니다

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -10,12 +10,15 @@ class CarTest {
 
     @Test
     void createCar_success() {
+        //given
         String case1 = "pobi,woni,jun";
         String case2 = "pobi";
 
+        //when
         List<Car> cars1 = Car.generateCars(case1);
         List<Car> cars2 = Car.generateCars(case2);
 
+        //then
         Assertions.assertAll(
                 () -> assertEquals(3, cars1.size()),
                 () -> assertEquals(1, cars2.size())
@@ -23,9 +26,11 @@ class CarTest {
     }
     @Test
     void createCar_exception() {
+        //given
         String case1 = "hello";
         String case2 = "helloWorld";
 
+        //when // then
         Assertions.assertAll(
                 () -> assertEquals(1, Car.generateCars(case1).size()),
                 () -> assertThrows(IllegalArgumentException.class, () -> Car.generateCars(case2))
