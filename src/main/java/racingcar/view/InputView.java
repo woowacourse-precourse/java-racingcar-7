01283c6e125265efa5d7e8c32e1e3233e9efecbe
openmpy <suhwan@kakao.com>
2024-10-23 01:45:12 +0900

    feat: getRound 메서드 및 INPUT_ROUND 메세지 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,7 @@
 package racingcar.view;
 
 import static racingcar.view.constants.ViewMessage.INPUT_CAR_NAMES;
+import static racingcar.view.constants.ViewMessage.INPUT_ROUND;
 
 import camp.nextstep.edu.missionutils.Console;
 
@@ -9,5 +10,11 @@ public class InputView {
     public static String getCarNames() {
         System.out.println(INPUT_CAR_NAMES.getMessage());
         return Console.readLine();
+    }
+
+    public static Integer getRound() {
+        System.out.println(INPUT_ROUND.getMessage());
+        String roundInput = Console.readLine();
+        return Integer.parseInt(roundInput);
     }
 }
--- src/main/java/racingcar/view/constants/ViewMessage.java
+++ src/main/java/racingcar/view/constants/ViewMessage.java
@@ -3,6 +3,7 @@ package racingcar.view.constants;
 public enum ViewMessage {
 
     INPUT_CAR_NAMES("자동차 이름을 쉼표(,)로 구분하여 입력하세요."),
+    INPUT_ROUND("시도할 횟수는 몇 회인가요?"),
     RACE_RESULT_TITLE("실행 결과"),
     RACE_STATUS("%s : %s"),
     ;
