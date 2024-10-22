6e0516fcc88089b1d8dae0a7e36d51d03fa7aa2e
minzix <minzikx@sookmyung.ac.kr>
2024-10-22 16:39:42 +0900

    feat: change Application
    - readLine 으로 사용자 입력 받음
    - 입력값 예외 검사

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,44 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carNames = getValidCarNames(readLine());
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int attempts = getValidAttempts(readLine());
+
+        try {
+            System.out.println("최종 우승자 : ");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+    }
+
+    public static int getValidAttempts(String input){
+        try {
+            int attempts = Integer.parseInt(input);
+            if (attempts < 0) {
+                throw new IllegalArgumentException("음수는 시도 횟수로 입력될 수 없습니다.");
+            }
+            return attempts;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수로는 숫자가 입력되어야 합니다.");
+        }
+    }
+
+    public static String[] getValidCarNames(String input){
+        String[] carNames = input.split(",");
+        for (String name: carNames){
+            name = name.trim();
+            if (name.length() > 5 || name.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 1자 이상, 5자 이하만 입력될 수 있습니다.");
+            }
+        }
+        return carNames;
     }
 }
+
