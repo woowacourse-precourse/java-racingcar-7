42ac1d7b64d6dc5cf6fe95016cbd49815af81e0e
JunHyuk Yu <ericyoo0107@gmail.com>
2024-10-22 23:24:45 +0900

    feat: 자동차 이름 입력 후 검증 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String carNames = readLine();
+        List<String> carNameList = validCarNames(carNames);
+    }
+
+    public static List<String> validCarNames(String carNames) {
+        if (carNames.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름이 입력되지 않았습니다.");
+        }
+        List<String> carName = List.of(carNames.split(",", -1));
+        for (String name : carName) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름을 5자 이하로 적어주세요.");
+            }
+            if (name.isBlank()) {
+                throw new IllegalArgumentException("자동차 이름은 공백이 될 수 없습니다.");
+            }
+        }
+        return carName;
     }
 }
