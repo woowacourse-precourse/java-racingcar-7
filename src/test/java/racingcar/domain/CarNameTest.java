0318699751afb45f3f2b16575007c509816a6e75
hwan2-99 <thh1233123@gmail.com>
2024-10-23 01:53:05 +0900

    test: 자동차 이름생성 테스트 구현

--- src/test/java/racingcar/domain/CarNameTest.java
+++ src/test/java/racingcar/domain/CarNameTest.java
@@ -1,7 +1,9 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,8 +11,13 @@ class CarNameTest {
     @ParameterizedTest
     @ValueSource(strings = {" ", "hwan22"})
     void 자동차_이름_검증(String value) {
-        assertThatThrownBy(() -> new Car(value))
+        assertThatThrownBy(() -> new CarName(value))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    void 자동차_이름생성(){
+        CarName carName = new CarName("hwan2");
+        assertThat(carName).isNotNull();
+    }
 }
