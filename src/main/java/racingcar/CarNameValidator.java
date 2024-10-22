a92586be39fb627a2eded57df12d701a92fb12e9
sik9252 <sik9252@naver.com>
2024-10-22 21:19:48 +0900

    refactor: 검증 우선순위 변경, 어느 자동차 이름에서 발생한 에러인지 나타내는 코드를 추가하여 에러 메시지 구체화

--- src/main/java/racingcar/CarNameValidator.java
+++ src/main/java/racingcar/CarNameValidator.java
@@ -6,26 +6,26 @@ import java.util.HashSet;
 
 public class CarNameValidator {
     public void checkIsCarNameValid(String carName, HashSet<String> carsToCheckDuplicate) {
-        checkCarNameLength(carName);
         checkIsCarNameEmpty(carName);
+        checkCarNameLength(carName);
         checkIsCarNameAlreadyExist(carName, carsToCheckDuplicate);
     }
 
     private void checkCarNameLength(String carName) {
         if (carName.length() > 5) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_IS_CAR_NAME_TO_LONG.getMessage());
+            throw new IllegalArgumentException(ErrorMessage.ERROR_IS_CAR_NAME_TO_LONG.getMessage() + ": " + carName);
         }
     }
 
     private void checkIsCarNameEmpty(String carName) {
         if (carName.isEmpty()) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_IS_CAR_NAME_EMPTY.getMessage());
+            throw new IllegalArgumentException(ErrorMessage.ERROR_IS_CAR_NAME_EMPTY.getMessage() + ": " + carName);
         }
     }
 
-    private void checkIsCarNameAlreadyExist(String carName, HashSet<String> carSet) {
-        if (carSet.contains(carName)) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_IS_CAR_NAME_ALREADY_EXIST.getMessage());
+    private void checkIsCarNameAlreadyExist(String carName, HashSet<String> carsToCheckDuplicate) {
+        if (carsToCheckDuplicate.contains(carName)) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_IS_CAR_NAME_ALREADY_EXIST.getMessage() + ": " + carName);
         }
     }
 }
