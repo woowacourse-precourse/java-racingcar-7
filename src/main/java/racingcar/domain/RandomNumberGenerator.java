bb87dc5d713f0fe143241933484d1341c7178ee8
dnwldla <leeemjw@naver.com>
2024-10-22 19:46:14 +0900

    feat: 랜덤수를 만드는 클래스 추가

---
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+
+    public static int createRandomNumber(){
+        int randomNumber=Randoms.pickNumberInRange(0, 9);
+
+        return randomNumber;
+    }
+}
