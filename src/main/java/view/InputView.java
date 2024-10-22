cbf69481ebd1e3e8ce0f7350fd0b5f7922940383
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-10-22 19:17:20 +0900

    feat: 이름이 중복되는 경우 예외 처리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,20 +2,24 @@ package view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class InputView {
 
     private static final String DELIMITER_ERROR_MESSAGE = "올바른 구분자를 입력해야 합니다.";
+    private static final String DUPLICATE_ERROR_MESSAGE = "중복되지 않은 이름을 입력해야 합니다.";
 
     private static final String DELIMITER = ",";
     private static final String OTHER_DELIMITER_REGEX = "[^,\\w\\s]";
+    private static final Integer DUPLICATE_VALUE = 2;
 
     public static String[] getCarNames() {
         String input = Console.readLine();
         validateDelimiter(input);
         String[] carNames = input.split(DELIMITER);
+        validateDuplicate(carNames);
         return carNames;
     }
 
@@ -24,6 +28,16 @@ public class InputView {
         Matcher matcher = pattern.matcher(input);
         if (matcher.find()) {
             throw new IllegalArgumentException(DELIMITER_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateDuplicate(final String[] carNames) {
+        Boolean result = Arrays.stream(carNames)
+                .distinct()
+                .count() >= DUPLICATE_VALUE;
+
+        if(result) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
         }
     }
 }
