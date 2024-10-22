38374b18bdfe8f7a9fab6658b85e7a3df610f58d
Yehun <yhyh4420@naver.com>
2024-10-22 17:25:42 +0900

    feat:HashMap으로 name:score 초기화 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.HashMap;
+
 public class Application {
+    public static HashMap<String, Object> getUserInfo() {
+        String input = Console.readLine();
+        int times = Integer.parseInt(Console.readLine());
+        String[] StrArr_Name = input.split(",");
+
+        HashMap<String, Object> scoreMap = new HashMap<String, Object>();
+        for (String name:StrArr_Name) {
+            scoreMap.put(name, 0);
+        };
+        return scoreMap;
+
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        getUserInfo();
     }
 }
