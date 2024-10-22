6f7fde61331ea3aff0427e908673cd07430f90c9
kimdev <kim__dev@hufs.ac.kr>
2024-10-22 23:01:00 +0900

    test(Race): 경주를 시작하는 테스트 케이스를 정의하라

---
+++ src/test/java/racingcar/RaceTest.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RaceTest {
+    @Test
+    @DisplayName("일정한_턴을_가진_게임이_생성된다")
+    public void 일정한_턴을_가진_게임이_생성된다() {
+        int turnCount = 5;
+
+        Race race = new Race(5);
+        assertTrue(true);
+    }
+}
