103a688d561c19d60a892581d2978de8d55bbe42
KIMB0B <axzsd96301@gmail.com>
2024-10-23 01:13:15 +0900

    refactor(Application): 함수 세분화 리팩토링 진행
    initializeCars()함수를 통해 차량 목록들을 초기화하였고, 차량들의 유효성
    검사도 validate 함수들을 만들어서 진행
    추가적으로 getMoveCount()함수를 사용하여 사용자에게 이동 횟수를 물어보고
    예외처리도 진행하도록 리팩토링함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,34 +9,39 @@ import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
+        Map<String, Integer> cars = initializeCars();
+        int moveCount = getMoveCount();
+
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < moveCount; i++) {
+            moveCars(cars);
+        }
+
+        ArrayList<String> winners = getWinners(cars);
+        System.out.print("최종 우승자 : " + String.join(", ", winners));
+    }
+
+    public static Map<String, Integer> initializeCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] carNames = Console.readLine().split(",");
 
         Map<String, Integer> cars = new LinkedHashMap<>();
         for (String carName : carNames) {
-            if (carName.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
-            }
+            validateCarName(carName);
             cars.put(carName.trim(), 0);
         }
-        if (cars.size() < 1) {
-            throw new IllegalArgumentException("자동차는 1대 이상 있어야합니다.");
-        }
+        validateCarsCount(cars);
 
+        return cars;
+    }
+
+    public static int getMoveCount() {
         try {
             System.out.println("시도할 횟수는 몇 회인가요?");
-            int moveCount = Integer.parseInt(Console.readLine());
-
-            System.out.println("\n실행 결과");
-            for (int i = 0; i < moveCount; i++) {
-                moveCars(cars);
-            }
+            return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("시도 횟수는 숫자만 입력할 수 있습니다.");
         }
-
-        ArrayList<String> winners = getWinners(cars);
-        System.out.print("최종 우승자 : " + String.join(", ", winners));
     }
 
     public static void moveCars(Map<String, Integer> cars) {
@@ -78,5 +83,17 @@ public class Application {
             }
         }
         return winners;
+    }
+
+    public static void validateCarName(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    public static void validateCarsCount(Map<String, Integer> cars) {
+        if (cars.isEmpty()) {
+            throw new IllegalArgumentException("자동차는 1대 이상 있어야합니다.");
+        }
     }
 }
