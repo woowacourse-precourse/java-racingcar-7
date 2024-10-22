c5f83eae93de108d19c29e20ba32a069eb904b82
Firedrago95 <yonghwa95@naver.com>
2024-10-22 20:23:34 +0900

    feat(InputValidator): 이동횟수 정규식 검증, 양수 검증 기능 구현

--- src/main/java/racingcar/util/validator/InputValidator.java
+++ src/main/java/racingcar/util/validator/InputValidator.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 public class InputValidator {
 
     private static final String CAR_NAMES_REGEX = "[가-힣|a-zA-Z]{1,5}(,[가-힣|a-zA-Z]{1,5})*";
+    private static final String NUMBER_OF_TRIAL_REGEX = "\\d";
 
     public static void validateCarNames(String input) {
         checkInputForm(input);
@@ -24,6 +25,24 @@ public class InputValidator {
             if (!carNames.add(name)) {
                 throw new IllegalArgumentException("[ERROR] 중복된 자동차 이름을 입력하셨습니다.");
             }
+        }
+    }
+
+    public static void validateNumberOfTrial(String numberOfTrial) {
+        checkNumberForm(numberOfTrial);
+        checkPositiveNumber(numberOfTrial);
+    }
+
+    private static void checkNumberForm(String numberOfTrial) {
+        if (!numberOfTrial.matches(NUMBER_OF_TRIAL_REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 올바른 숫자를 입력해주세요");
+        }
+    }
+
+    private static void checkPositiveNumber(String numberOfTrial) {
+        int num = Integer.parseInt(numberOfTrial);
+        if (num <= 0) {
+            throw new IllegalArgumentException("[ERROR] 올바른 숫자를 입력해주세요");
         }
     }
 }
