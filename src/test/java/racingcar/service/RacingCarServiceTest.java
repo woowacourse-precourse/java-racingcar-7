b1ae7ebbd372f106f08a6c68f364b9a38d129083
openmpy <suhwan@kakao.com>
2024-10-23 03:21:10 +0900

    test: 빈 자동차 이름 검증을 위한 ParameterizedTest 추가

--- src/test/java/racingcar/service/RacingCarServiceTest.java
+++ src/test/java/racingcar/service/RacingCarServiceTest.java
@@ -1,10 +1,15 @@
 package racingcar.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.exception.constants.ErrorMessage.EMPTY_CAR_NAME_NOT_ALLOWED;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.dto.RacingCarRequestDto;
 import racingcar.dto.RacingCarResponseDto;
+import racingcar.exception.RacingCarException;
 
 class RacingCarServiceTest {
 
@@ -23,5 +28,17 @@ class RacingCarServiceTest {
 
         assertThat(winnerName).isNotNull();
         System.out.println(winnerName);
+    }
+
+    @ParameterizedTest(name = "입력값: {0}")
+    @ValueSource(strings = {"", "1,", "1,,3", "1, ,3"})
+    void 예외_빈_값_입력(String input) {
+        // given
+        RacingCarRequestDto requestDto = new RacingCarRequestDto(input, 5);
+
+        // when & then
+        assertThatThrownBy(() -> racingCarService.start(requestDto))
+                .isInstanceOf(RacingCarException.class)
+                .hasMessage(EMPTY_CAR_NAME_NOT_ALLOWED.getMessage());
     }
 }
