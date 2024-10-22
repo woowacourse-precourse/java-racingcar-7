44ea01461505b54090b3506f14496af183cecfb1
EHCTH <lees90896@gmail.com>
2024-10-22 16:39:34 +0900

    feat(RacingCarConvertor): RacingCar Convertor 클래스 추가 * feat: 문자열 -> 문자열 배열 기능인 splitComma(String input) 기능 * feat: 문자열배열 -> 리스트 배열 기능인 splitByCommatoList(String input) 기능 추가 추후에 정적으로 갈지 아니면 의존성을 주입할지 고려

--- src/main/java/racingcar/racingcar/util/RacingCarConvertor.java
+++ src/main/java/racingcar/racingcar/util/RacingCarConvertor.java
@@ -1,4 +1,15 @@
 package racingcar.racingcar.util;
 
-public class RacingCarConvertro {
+import java.util.Arrays;
+import java.util.List;
+
+public class RacingCarConvertor {
+    public static List<String> splitByCommaToList(String input) {
+
+        return Arrays.stream(splitByComma(input))
+                .toList();
+    }
+    private static String[] splitByComma(String input) {
+        return input.split(",");
+    }
 }
