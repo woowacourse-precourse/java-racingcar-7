d496ac40397573c76dec34acbba7c146bd3df916
Min <binine2@gmail.com>
2024-10-22 20:17:35 +0900

    feat: 입력 조건 쉼표, 글자수 제한 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,5 +11,14 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputName = Console.readLine();
 
+        String[] names = inputName.split(",");
+
+        for (String name : names){
+            if (name.length() > 5){
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
+
+        System.out.println(Arrays.toString(names));
     }
 }
