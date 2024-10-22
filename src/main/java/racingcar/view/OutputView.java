3c532584c4fa5520486a5e7dda3c585cc3feb08a
HOKAGO-MEMORIES <minchotejava@gmail.com>
2024-10-23 02:45:27 +0900

    feat(OutputView): 출력 기능 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,34 @@
+package racingcar.view;
+
+import java.util.HashMap;
+
+public class OutputView {
+
+    private static final String FIRST_INITIAL_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String SECOND_INITIAL_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String PLAY_MESSAGE = "\n실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
+
+    public void printFirstInitialMessage() {
+        System.out.println(FIRST_INITIAL_MESSAGE);
+    }
+
+    public void printSecondInitialMessage() {
+        System.out.println(SECOND_INITIAL_MESSAGE);
+    }
+
+    public void printPlayMessage() {
+        System.out.println(PLAY_MESSAGE);
+    }
+
+    public void printGameResult(HashMap<String, String> gameResult) {
+        for (String key : gameResult.keySet()) {
+            System.out.println(key + " : " + gameResult.get(key));
+        }
+        System.out.println('\n');
+    }
+
+    public void printResultMessage(String winner) {
+        System.out.println(WINNER_MESSAGE + winner);
+    }
+}
