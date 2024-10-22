eff4dbed28ab358392a2a43c6c9f786b3d7133a0
urim96 <qusdbfla1@gmail.com>
2024-10-22 20:09:57 +0900

    배열 값 확인 후 map에 저장
    자동차 이름이 5글자 이하인지 확인
    자동차 이름이 빈문자열이나 공백인지 확인
    배열을 map에 저장하면서 자동차 이름 중복 여부 확인
    자동차 이름에 대한 체크사항 메소드들을 실행하는 메소드 생성

--- src/main/java/racingcar/CarArrayCheck.java
+++ src/main/java/racingcar/CarArrayCheck.java
@@ -1,20 +1,53 @@
 package racingcar;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CarArrayCheck {
 	
 	String[] carArray = {};
 	int carLength = 0;
+	Map<String, Integer> carMap = new HashMap<String, Integer>();
 	
 	public void carNameSplit(String carName) {
 		this.carArray = carName.split(",");
 		this.carLength = carArray.length;
-		carArrayLength();
+		check();
 	}
 	
-	public void carArrayLength() {
+	private void check() {
+		carArrayLength();
+		carNameCheck();
+		carMap();
+	}
+	
+	private void carArrayLength() {
 		if(carArray == null || carLength <= 1) {
 			System.err.println("경주할 자동차를 2개 이상 입력해주세요.");
 			throw new IllegalArgumentException(); 
+		}
+	}
+	
+	private void carNameCheck() {
+		for(String name : carArray) {
+			if(name.length() > 5) {
+				System.err.println("자동차 이름은 5자 이하로 입력해주세요.");
+				throw new IllegalArgumentException(); 
+			}
+			if(name.isEmpty()) {
+				System.err.println("자동차 이름은 공백으로 입력할 수 없습니다.");
+				throw new IllegalArgumentException(); 
+			}
+		}
+	}
+	
+	private void carMap() {
+		for(String name : carArray) {
+			if(carMap.containsKey(name)) {
+				System.err.println("자동차 이름은 중복될 수 없습니다.");
+				throw new IllegalArgumentException(); 
+			}
+			carMap.put(name, 0);
 		}
 	}
 }
