bb5d47f5fc109dfb24f008d2d5b84df5edb66323
gracepec <gracepec0621@gmail.com>
2024-10-22 23:41:21 +0900

    feat(Car): 자동차 객체 구현
    - 이름 부여
    - 전진 횟수 관리

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int advanceCount;
+
+    public Car(String name) {
+        this.name = name;
+        advanceCount = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void advance() {
+        advanceCount++;
+    }
+
+    public int getAdvanceCount() {
+        return advanceCount;
+    }
+}
