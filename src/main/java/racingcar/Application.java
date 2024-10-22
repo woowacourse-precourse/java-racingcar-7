87089f9add2ad407c8283606e238cebf85aa9467
배석현 <bjh4966@naver.com>
2024-10-22 21:12:20 +0900

    1.입력과 출력을 나타낸다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,30 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringBuilder carNames = new StringBuilder();
+        String input;
+        System.out.println("경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분): ");
+        while (true) {
+            input = Console.readLine(); // 입력 받기
+
+            carNames.append(input);
+
+            // 마지막에 쉼표가 없으면 입력 종료
+            if (!input.endsWith(",")) {
+                break;
+            }
+        }
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int attempts = Integer.parseInt(Console.readLine());
+        System.out.println();
+        System.out.println("실행 결과");
+        // 입력된 자동차 이름 출력
+//        String[] carNameArray = carNames.toString().split(",");
+//        for (String name : carNameArray) {
+//            System.out.print(name.trim() +","); // 트림하여 공백 제거
+//        }
     }
 }
