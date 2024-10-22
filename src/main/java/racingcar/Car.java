f6d0c99940a7cdb9951f8b5affa729dbac03a1f1
박철현 <park82602@gmail.com>
2024-10-22 19:07:37 +0900

    feat : 입력받은 자동차 이름을 가지고 자동차 List를 반환하는 기능을 구현하였습니다

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,9 +1,24 @@
 package racingcar;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
 
     private String name;
     private String position;
 
+    private Car(String name) {
+        this.name = name;
+        this.position = "";
+    }
+    public static List<Car> generateCars(String carNames) {
+        String[] names = carNames.split(",");
+        List<Car> cars = new ArrayList<>();
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
 }
