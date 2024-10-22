c4b5c9d6439dc49a4e7bd7ee8cdc85f30ea7a063
corpi <lj845854@gmail.com>
2024-10-23 05:58:55 +0900

    refactor(InputViewCarNameTest): InputViewTest -> InputViewCarNameTest 네이밍 수정

--- src/test/java/racingcar/view/InputViewCarNameTest.java
+++ src/test/java/racingcar/view/InputViewCarNameTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-class InputViewTest extends NsTest {
+class InputViewCarNameTest extends NsTest {
     private InputView inputView;
 
     @BeforeEach
