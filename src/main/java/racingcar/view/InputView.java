24ffd213e79559e7f8f9ce3979da6e07ca897df2
corpi <lj845854@gmail.com>
2024-10-23 04:57:08 +0900

    feat(InputView): 자동차 이름 입력안내 문구 출력 후 사용자의 입력을 받는다.

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class InputView {
+    private final String InputInstruction = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public List<String> inputCarNames() {
+        System.out.println(InputInstruction);
+        String[] splitInput = Console.readLine().split(",");
+
+        return List.of(splitInput);
+    }
+}
