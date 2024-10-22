8c659a8bdf368fd3e9a7a332c669cf087ee03cb1
JuSeong1130 <jsmoon1130@naver.com>
2024-10-22 20:21:05 +0900

    feat(Application) : 자동차 게임 경주 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,10 @@
 package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
@@ -9,7 +13,37 @@ public class Application {
         String names = readLine();
         System.out.println("시도할 횟수는 몇 회인가요?");
         int tryCount = Integer.parseInt(readLine());
-        System.out.println("names = " + names);
-        System.out.println("tryCount = " + tryCount);
+
+        Map<String, boolean[]>  playerGameHistory = new HashMap<>();
+        for(String name : names.split(",")) {
+            playerGameHistory.put(name, new boolean[tryCount]);
+        }
+
+        for(String player : playerGameHistory.keySet()) {
+            boolean[] playerHistory = playerGameHistory.get(player);
+            for (int i = 0; i < tryCount; i++) {
+                int randomValue = pickNumberInRange(0, 9);
+                if(randomValue >= 4) {
+                    playerHistory[i] = true;
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+        
+        
+
+
+
+
+
+
+
     }
 }
