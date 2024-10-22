4a198b8b89b92067bafd5daa990c5efc51e96c44
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-10-22 19:58:36 +0900

    feat: 자동차 이름이 공백이거나 아무런 문자도 입력하지 않은 경우 예외처리

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -3,11 +3,13 @@ package domain;
 public class Car {
 
     private static final String MAXIMUM_NAME_SIZE_ERROR_MESSAGE = "자동차 이름은 5글자 이하로 입력해야 합니다";
+    private static final String NAME_EMPTY_ERROR_MESSAGE = "올바른 자동차 이름을 입력해야 합니다.";
     private static final Integer MAXIMUM_NAME_SIZE = 6;
 
     private final String name;
 
     public Car(final String name) {
+        validateEmpty(name);
         validateNameSize(name);
         this.name = name;
     }
@@ -15,6 +17,12 @@ public class Car {
     public void validateNameSize(final String name) {
         if(name.length() > MAXIMUM_NAME_SIZE) {
             throw new IllegalArgumentException(MAXIMUM_NAME_SIZE_ERROR_MESSAGE);
+        }
+    }
+
+    public void validateEmpty(final String name) {
+        if(name.isBlank()) {
+            throw new IllegalArgumentException(NAME_EMPTY_ERROR_MESSAGE);
         }
     }
 }
