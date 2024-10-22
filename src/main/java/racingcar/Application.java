48beb3eeacb05a5b1321eb7dd353ff5e57a9ab58
inha0319@naver.com <inha031913@gmail.com>
2024-10-22 19:39:55 +0900

    [feat] InputView 클래스 구현
    - 자동차 이름 입력 기능 구현
    - 시도 횟수 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        String[] carNames = InputView.inputCarNames();
+        int rounds = InputView.inputRounds();
+
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,22 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public static String[] inputCarNames(){
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+        return input.split(",");
+    }
+
+    public static int inputRounds(){
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+
+        return Integer.parseInt(Console.readLine());
+    }
+
 }
