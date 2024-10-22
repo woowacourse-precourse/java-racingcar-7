1204a7363185787ebfc232ea538b7b8f4cce9129
Firedrago95 <yonghwa95@naver.com>
2024-10-22 20:10:12 +0900

    feat(OutputView): 라운드 결과 출력 기능 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,28 @@
+package racingcar.view;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class OutputView {
+
+    public static void printResultMessage() {
+        System.out.println("실행결과");
+    }
+
+    public static void printRoundResult(Map<String, Integer> result) {
+        List<String> names = new ArrayList<>(result.keySet());
+        for (String name : names) {
+            System.out.printf("%s : %s",name, printCountBar(result.get(name)));
+            System.out.println();
+        }
+    }
+
+    private static String printCountBar(Integer integer) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < integer; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
+    }
+}
