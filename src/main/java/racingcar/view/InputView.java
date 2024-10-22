f9b94851766db88708fa3f7365a4863bb8fe57a3
corpi <lj845854@gmail.com>
2024-10-23 06:03:02 +0900

    feat(InputView): 시도 횟수 입력안내 문구 출력 후 사용자의 입력을 받는다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,12 +5,20 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
-    private final String InputInstruction = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String inputCarNameInstruction = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String inputTrialCountInstruction = "시도할 횟수는 몇 회인가요?";
 
     public List<String> inputCarNames() {
-        System.out.println(InputInstruction);
+        System.out.println(inputCarNameInstruction);
         String[] splitInput = Console.readLine().split(",");
 
         return Arrays.stream(splitInput).map(String::trim).toList();
+    }
+
+    public int inputTrialCount() {
+        System.out.println(inputTrialCountInstruction);
+        String input = Console.readLine();
+
+        return Integer.parseInt(input);
     }
 }
