df4673fec9fa6011183e29c02be05a71219ff877
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-10-23 01:33:21 +0900

    feat(InputMessageTest): 테스트 추가
    - 자동차 이름이 5자를 초과하지 않는지 테스트합니다.

--- src/test/java/racingcar/InputMessageTest.java
+++ src/test/java/racingcar/InputMessageTest.java
@@ -1,4 +1,19 @@
 package racingcar;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class InputMessageTest {
+    @Test
+    void 자동차_이름이_5자를_초과하지_않는_지_테스트(){
+        InputMessage inputMessage = new InputMessage();
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            inputMessage.splitCarName("abc, abcd, abcdef");
+        });
+    }
 }
