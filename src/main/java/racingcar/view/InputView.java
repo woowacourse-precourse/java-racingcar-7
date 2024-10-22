6273cd460318f431fc82c9f6456970c453326867
MinJun Choi <mj043000@naver.com>
2024-10-22 16:33:54 +0900

    feat: 입력 기능 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static String readCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return scanner.nextLine();
+    }
+
+    public static String readTryCounts() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return scanner.nextLine();
+    }
+}
