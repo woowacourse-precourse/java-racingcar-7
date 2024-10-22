cb61274307cf607f543d753d3f3f877488e637c3
openmpy <suhwan@kakao.com>
2024-10-23 03:12:26 +0900

    test: CarParser 클래스 ParameterizedTest 로 변경

--- src/test/java/racingcar/common/CarParserTest.java
+++ src/test/java/racingcar/common/CarParserTest.java
@@ -3,20 +3,29 @@ package racingcar.common;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import racingcar.domain.Car;
 
 class CarParserTest {
 
-    @Test
-    void 자동차_이름_배열을_콤마로_연결() {
-        // given
-        List<Car> winners = List.of(new Car("1"), new Car("2"), new Car("3"));
-
+    @ParameterizedTest(name = "기대값: {1}")
+    @MethodSource(value = "provideCarNames")
+    void 자동차_이름_배열을_콤마로_연결(final List<Car> input, final String expected) {
         // when
-        String winnerNames = CarParser.convertCarsToNames(winners);
+        String winnerNames = CarParser.convertCarsToNames(input);
 
         // then
-        assertThat(winnerNames).isEqualTo("1, 2, 3");
+        assertThat(winnerNames).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> provideCarNames() {
+        return Stream.of(
+                Arguments.of(List.of(new Car("1")), "1"),
+                Arguments.of(List.of(new Car("1"), new Car("2")), "1, 2"),
+                Arguments.of(List.of(new Car("1"), new Car("2"), new Car("3")), "1, 2, 3")
+        );
     }
 }
