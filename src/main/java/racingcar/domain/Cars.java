89708faaa93a49167ac28c66327265246e37c6a8
JS <zks145@naver.com>
2024-10-22 23:05:03 +0900

    feat (domain): Cars 클래스 생성

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    public static List<Car> cars;
+
+    public Cars(String carNames) {
+
+    }
+
+}
