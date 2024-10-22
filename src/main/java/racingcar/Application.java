cd6e7d5dd0e3e8db91fc6bc1000066b2be9fbe95
hwan2-99 <thh1233123@gmail.com>
2024-10-23 00:08:03 +0900

    feat: 시도횟수(라운드) 입력기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,16 @@
 package racingcar;
 
 import racingcar.domain.Cars;
+import racingcar.strategy.RandomMoveStrategy;
 import racingcar.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         final Cars cars = Cars.from(InputView.inputCarNames());
+        final int round = InputView.inputRound();
+
+        for (int i = 0; i < round; i++) {
+            cars.racing(new RandomMoveStrategy());
+        }
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,7 +3,11 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final int MIN_ROUND_VALUE = 1;
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ROUND_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String NUMBER_ERROR_MESSAGE = "숫자만 입력 가능합니다.";
+    private static final String MIN_VALUE_ERROR_MESSAGE = "시도 횟수는 " + MIN_ROUND_VALUE + "이상이어야 합니다.";
 
     private static String inputValue() {
         return Console.readLine();
@@ -12,5 +16,32 @@ public class InputView {
     public static String inputCarNames() {
         System.out.println(INPUT_CAR_NAMES_MESSAGE);
         return inputValue();
+    }
+
+    public static int inputRound() {
+        System.out.println(INPUT_ROUND_MESSAGE);
+        int numberValue = inputNumberValue();
+        validateMinNumber(numberValue);
+        return numberValue;
+    }
+
+    private static void validateMinNumber(int numberValue) {
+        if (numberValue < MIN_ROUND_VALUE) {
+            throw new IllegalArgumentException(MIN_VALUE_ERROR_MESSAGE);
+        }
+    }
+
+    private static int inputNumberValue() {
+        String inputValue = inputValue();
+        validateNumber(inputValue);
+        return Integer.parseInt(inputValue);
+    }
+
+    private static void validateNumber(String inputValue) {
+        try {
+            Integer.parseInt(inputValue);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMBER_ERROR_MESSAGE);
+        }
     }
 }
