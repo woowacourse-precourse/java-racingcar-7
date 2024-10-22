426534f1b98623354560bcc579c48fe78c37e97c
Hacanna42 <dtmdgus901@gmail.com>
2024-10-23 00:01:56 +0900

    feat: implement Validator

---
+++ src/main/java/racingcar/util/Validator.java
@@ -0,0 +1,18 @@
+package racingcar.util;
+
+public class Validator {
+    private static final String CAR_REGEX = "^([^,]+,)+[^,]+$"; // 차 이름에는 구분자(반점)가 포함될 수 없다.
+    private static final String ROUND_REGEX = "^[1-9][0-9]*$"; // 라운드 수는 0이거나, 비어있을 수 없다.
+
+    public void checkAboutCars(String input) {
+        if (!input.matches(CAR_REGEX)) {
+            throw new IllegalArgumentException("경주할 자동차 이름을 잘 못 입력하셨습니다.");
+        }
+    }
+
+    public void checkAboutRound(String input) {
+        if (!input.matches(ROUND_REGEX)) {
+            throw new IllegalArgumentException("시도할 횟수는 0이거나 비어있을 수 없습니다.");
+        }
+    }
+}
