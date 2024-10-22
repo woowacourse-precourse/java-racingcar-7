749de92f51b30674cc0a21679dc49f2eecb1e15a
joungeun0825 <joungeun0825@gmail.com>
2024-10-22 20:24:20 +0900

    feat(car): 이동 수가 4 이상일 경우 자동차 전진 기능 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int movedNumber;
+
+    public Car(String name){
+        this.name = name;
+        this.movedNumber = 0;
+    }
+
+    public void move(int movedNumber){
+        if(canCarMove(movedNumber)){
+            this.movedNumber += movedNumber;
+        }
+    }
+
+    public int getMovedNumber(){
+        return this.movedNumber;
+    }
+
+    private boolean canCarMove(int moveNumber){
+        return moveNumber >= 4;
+    }
+}
