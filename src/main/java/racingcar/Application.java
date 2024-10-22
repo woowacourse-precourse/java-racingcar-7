5e4b0d6e4029e3abd738e71df4f10d1e2731f8ea
SeongbinPark <hyucse22@gmail.com>
2024-10-22 16:19:29 +0900

    feat(Application): Get input from user
    입력받은 자동차 이름들을 리스트화 시킨다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,13 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = readLine();
+        String[] carNames = input.split(",");
+
 
     }
 }
