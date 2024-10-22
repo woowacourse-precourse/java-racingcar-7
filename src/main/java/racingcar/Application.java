b7b999292d037efc830c24de574c07fb62846bf5
jinukee <jinukee333@gmail.com>
2024-10-22 23:25:41 +0900

    feat(Application) : Car class 추가
    Car class 를 만들어 각각의 자동차의 이름과 현재 위치를 저장할 수 있도록 구현했습니다.
    class 내부의 move(), displayPosition() method를 이용해 자동차를 전진 시킬 수 있고,
    현재 위치를 "-"로 표현해서 받아올 수 있습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,4 +4,29 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    class Car{
+        private String name;
+        private int position = 0;
+
+        public Car(String name){
+            this.name = name;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public int getPosition(){
+            return position;
+        }
+
+        public void move(){
+            position++;
+        }
+
+        public String displayPosition(){
+            return "-".repeat(position);
+        }
+    }
 }
