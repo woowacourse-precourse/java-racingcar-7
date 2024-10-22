6f03d30ca4aa8c0b8be94d726354c7fe85938926
yngbao97 <yukyj0418@gmail.com>
2024-10-22 20:53:07 +0900

    feat: 입력값을 구분자 ","로 분리하는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,18 +5,24 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
 
-
+        run();
 
     }
 
-    private void run() {
+    private static void run() {
 
+        inputCars();
+
+    }
+
+    private static void inputCars() {
         print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carsInput = Console.readLine();
+        String[] inputs = carsInput.split(",");
 
     }
 
-    private void print(String message) {
+    private static void print(String message) {
         System.out.println(message);
     }
 }
