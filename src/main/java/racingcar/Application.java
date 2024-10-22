3fdd88e7a3550e2f4e03c2851beb66e8af1ca117
sik9252 <sik9252@naver.com>
2024-10-23 00:24:07 +0900

    feat: main 함수에 게임 실행을 위한 객체 생성 및 의존성 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import racingcar.car.CarInputService;
+import racingcar.car.CarInputValidator;
+import racingcar.count.CountService;
+import racingcar.count.CountValidator;
+import racingcar.game.Game;
+import racingcar.game.GameService;
+import racingcar.game.GameView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarInputValidator carInputValidator = new CarInputValidator();
+        CarInputService carInputService = new CarInputService(carInputValidator);
+
+        CountValidator countValidator = new CountValidator();
+        CountService countService = new CountService(countValidator);
+
+        GameService gameService = new GameService();
+        GameView gameView = new GameView(gameService);
+
+        Game game = new Game(carInputService, countService, gameView);
+
+        game.run();
     }
 }
