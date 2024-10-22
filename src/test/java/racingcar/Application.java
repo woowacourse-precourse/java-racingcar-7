45acd4bf9656bcd2cf93d01998554d1628d12be5
joshinhyeok <shtree07@naver.com>
2024-10-23 00:50:00 +0900

    feat: 경주할 자동차 이름을 입력받는 기능 구현

---
+++ src/test/java/racingcar/Application.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String inputString = Console.readLine();
+    }
+}
