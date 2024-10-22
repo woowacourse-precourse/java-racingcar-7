78cc3c78153ca935de50759bb0c2f2a56b9d2e61
moonbinchoi <whsahd12@gmail.com>
2024-10-22 19:44:58 +0900

    feat(Application): main() 메서드 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import racingcar.domain.RacingGame;
+import racingcar.domain.RacingCars;
+import racingcar.util.ConsoleIOHandler;
+import racingcar.util.StringParser;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        try (ConsoleIOHandler handler = new ConsoleIOHandler()) {
+            String names = handler.read("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String times = handler.read("시도할 횟수는 몇 회인가요?");
+
+            RacingGame racingGame = RacingGame.of(
+                    RacingCars.from(StringParser.parseCarName(names)),
+                    StringParser.parseInt(times));
+
+            racingGame.start();
+            RacingCars winners = racingGame.judge();
+            handler.print(winners);
+        }
     }
 }
