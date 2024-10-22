74ec804ad93c5b4beaa09d4ab19ae8a80c09c2c8
DawnGlow <ferrater1013@gmail.com>
2024-10-22 21:19:32 +0900

    fix: 자동차 이름 파싱 수정
    사용자가 입력한 문자열에서 자동차 이름을 얻을 때 trim 을 적용합니다.

--- src/main/java/racingcar/utility/CarNameParser.java
+++ src/main/java/racingcar/utility/CarNameParser.java
@@ -2,12 +2,15 @@ package racingcar.utility;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarNameParser {
 
     public static final String NAME_DELIMITER = ",";
 
     public static List<String> parseCarNames(String carNamesAsString) {
-        return Arrays.stream(carNamesAsString.split(NAME_DELIMITER)).toList();
+        return Arrays.stream(carNamesAsString.split(NAME_DELIMITER))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 }
