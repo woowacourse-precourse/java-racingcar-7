e753d12ad536aab524880820f495d87c995ebcdc
dnwldla <leeemjw@naver.com>
2024-10-23 05:18:31 +0900

    feat: 입력을 받는 InputView 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String enterCounts(){
+        String counts=Console.readLine();
+
+        return counts;
+    }
+
+    public static String enterCarNames() {
+        String carNames=Console.readLine();
+
+        return carNames;
+    }
+}
