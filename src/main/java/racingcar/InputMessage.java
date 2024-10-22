ee264741583d91f2d7be2733c5beb23fba890d85
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-10-23 02:53:08 +0900

    test(InputMessage): 예외 처리 코드 추가
    - validateInput 메서드로 이름 변경
    - 변경한 이유는 예외 처리 할 때마다 새로운 메서드를 만드는 것이 맞을까?
      라는 고민을 했습니다. 코드도 중복되는 문제도 있고 해서 한 메서드 안에
      넣고 주석으로 어떤 예외를 처리하는 지 알리고자 했습니다.
    
    - 자동차 이름이 문자, 숫자 이외의 것들이 들어가면 예외를 발생시키는
      기능을 추가했습니다.

--- src/main/java/racingcar/InputMessage.java
+++ src/main/java/racingcar/InputMessage.java
@@ -1,9 +1,12 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 import org.junit.jupiter.api.DisplayName;
 
 public class InputMessage {
+    private static final Pattern INVALID_PATTERN = Pattern.compile("[^a-zA-Z0-9]");
+
     public String inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
@@ -11,18 +14,23 @@ public class InputMessage {
 
     public String[] splitCarName(String inputCarName){
         String[] carNames = inputCarName.split(",");
-        carNameMoreThan5CharactersException(carNames);
+        validateInput(carNames);
 
         return carNames;
     }
-
-    @DisplayName("자동차 이름이 5자 초과 시 예외 발생")
-    public void carNameMoreThan5CharactersException(String[] carNames){
+    
+    public void validateInput(String[] carNames){
         int carNameLimits = Constants.CAR_NAME_LIMITS;
-
+        
         for (String carName : carNames) {
-            if (carName.trim().length() > carNameLimits) {  // trim()은 앞뒤 공백 제거
+            // 자동차 이름이 5자 초과 시 예외 발생
+            if (carName.trim().length() > carNameLimits) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다: " + carName);
+            }
+            
+            // 자동차 이름이 문자, 숫자 이외의 것들이 들어갈 시 예외 발생
+            if (INVALID_PATTERN.matcher(carName.trim()).find()) {
+                throw new IllegalArgumentException("자동차 이름은 영대소문자와 숫자 밖에 안 됩니다.");
             }
         }
     }
