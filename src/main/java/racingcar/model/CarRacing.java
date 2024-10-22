86e030a0c5fae99f3a6a7fd7be33547b5fa4fd73
corpi <lj845854@gmail.com>
2024-10-23 07:57:07 +0900

    feat(CarRacing): 사용자의 입력을 받고 자동차를 등록한다.

--- src/main/java/racingcar/model/CarRacing.java
+++ src/main/java/racingcar/model/CarRacing.java
@@ -2,11 +2,23 @@ package racingcar.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import racingcar.view.InputView;
 
 public class CarRacing {
     private final List<Car> carList = new ArrayList<>();
+    private final InputView inputView;
 
-    public void enroll(String name) {
+    public CarRacing(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void start() {
+        List<String> carNameList = inputView.inputCarNames();
+
+        carNameList.forEach(this::enroll);
+    }
+
+    private void enroll(String name) {
         carList.add(Car.of(name));
     }
 }
