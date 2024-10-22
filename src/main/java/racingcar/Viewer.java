42ec55ca987814ec1542856ea767c393618c5457
byeongdoohan <quden04@gmail.com>
2024-10-23 04:46:06 +0900

    feat: Viewer 클래스 추가 - 차 이름과 시도 횟수를 입력받는 기능 추가 - 차 이름을 리스트로 변환하는 기능 추가

---
+++ src/main/java/racingcar/Viewer.java
@@ -0,0 +1,40 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Viewer {
+    private final String carInputString = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String tryInputString = "시도할 횟수는 몇 회인가요?";
+    private final String outputString = "최종 우승자 : ";
+    private String carInput;
+    private String tryInput;
+    private List<String> carList;
+
+    public void carInputToCarList(){
+        this.carList = Arrays.asList(carInput.split(","));
+    }
+    public void readInput(){
+        readCarInput();
+        readTryInput();
+    }
+    public void readCarInput() {
+        System.out.println(carInputString);
+        this.carInput = Console.readLine();
+    }
+
+    public void readTryInput() {
+        System.out.println(tryInputString);
+        this.tryInput = Console.readLine();
+    }
+
+    public String getCarInput(){
+        return this.carInput;
+    }
+
+    public String getTryInput(){
+        return this.tryInput;
+    }
+}
