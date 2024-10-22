4236bb7f7b07ada432c475da45a8dccda4b0d029
rdyjun <rdyjun00@gmail.com>
2024-10-22 20:28:55 +0900

    feat: 자동차 이름을 입력받아, 각 이름 유효성 검사 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,9 @@
 package racingcar;
 
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         CarRacing.start();
     }
 }
--- src/main/java/racingcar/CarRacing.java
+++ src/main/java/racingcar/CarRacing.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class CarRacing {
-    public static void start() {
+    public static void start() throws IOException {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String carNames = bufferedReader.readLine();
+
+        String[] carNameArray = Arrays.stream(carNames.split(","))
+                .peek(CarRacing::validateCarName)
+                .toArray(String[]::new);
+    }
+
+    private static void validateCarName(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
     }
 }
