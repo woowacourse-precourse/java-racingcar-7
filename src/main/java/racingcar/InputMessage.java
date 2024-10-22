c271f066ab3764e6001466068a09ca6f5a25fbdf
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-10-23 01:10:49 +0900

    feat(InputMessage): 메서드 추가적으로 생성
    - 메서드 명 : carNameMoreThan5CharactersException
    - 자동차 이름이 5자 초과 시 예외를 발생시키는 메서드
    
    - 메서드 명 : splitCarName
    - inputCarName메서드에서 분리하였습니다. 이유는 inputCarName의 목적은
      자동차의 이름을 입력 받는 것이 목적이여서 splitCarName메서드를
      생성하고 목적은 입력 받은 문자열을 쉼표로 구분해주는 것입니다.

--- src/main/java/racingcar/InputMessage.java
+++ src/main/java/racingcar/InputMessage.java
@@ -1,14 +1,29 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.DisplayName;
 
 public class InputMessage {
-    public String[] inputCarName() {
+    public String inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String inputCarName = Console.readLine();
+        return Console.readLine();
+    }
 
+    public String[] splitCarName(String inputCarName){
         String[] carNames = inputCarName.split(",");
+        carNameMoreThan5CharactersException(carNames);
 
         return carNames;
+    }
+
+    @DisplayName("자동차 이름이 5자 초과 시 예외 발생")
+    public void carNameMoreThan5CharactersException(String[] carNames){
+        int carNameLimits = Constants.CAR_NAME_LIMITS;
+
+        for (String carName : carNames) {
+            if (carName.trim().length() > carNameLimits) {  // trim()은 앞뒤 공백 제거
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다: " + carName);
+            }
+        }
     }
 }
