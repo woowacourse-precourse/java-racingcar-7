1c725c867d4d80fcc2a61b1cc14fe57d24f38efa
Firedrago95 <yonghwa95@naver.com>
2024-10-22 20:45:34 +0900

    feat(Controller): 이동 횟수 만큼 라운드 실행 구현

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,37 @@
+package racingcar.controller;
+
+import java.util.List;
+import java.util.Map;
+import racingcar.model.Cars;
+import racingcar.util.RandomNumGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingCarController {
+
+    private Cars cars;
+
+    public void run() {
+        createCars();
+        playRacingRounds();
+        printWinner();
+    }
+
+    private void createCars() {
+        List<String> carNames = InputView.readCarNames();
+        cars = new Cars(carNames);
+    }
+
+    private void playRacingRounds() {
+        int numberOfTrial = InputView.readNumberOfTrial();
+        OutputView.printResultMessage();
+        for (int i = 0; i < numberOfTrial; i++) {
+            cars.playRound(new RandomNumGenerator());
+            OutputView.printRoundResult(cars.getRoundResult());
+        }
+    }
+
+    private void printWinner() {
+        OutputView.printFinalResult(cars.getFinalResult());
+    }
+}
