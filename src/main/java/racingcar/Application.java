7d8824fc3eb88c7a4c6bf9b14344db1c1ed11b3c
SeongbinPark <hyucse22@gmail.com>
2024-10-22 17:31:14 +0900

    feat(Application): Get attempt from user

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.security.PrivateKey;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +10,7 @@ public class Application {
     public static void main(String[] args) {
 
         List<Car> cars = new ArrayList<>();
+        int attempts;
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = readLine();
@@ -17,6 +19,13 @@ public class Application {
         for (String carName : carNames) {
             cars.add(new Car(carName));
         }
+
+
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String inputAttempt = readLine();
+        attempts = Integer.parseInt(inputAttempt);
+
 
     }
 }
