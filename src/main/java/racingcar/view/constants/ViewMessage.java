57d9ac9c2b8bf59f12483c365ecc97f5bab647b6
openmpy <suhwan@kakao.com>
2024-10-23 03:37:58 +0900

    refactor: INPUT_CAR_NAMES 상수 메세지 내용 변경

--- src/main/java/racingcar/view/constants/ViewMessage.java
+++ src/main/java/racingcar/view/constants/ViewMessage.java
@@ -2,7 +2,7 @@ package racingcar.view.constants;
 
 public enum ViewMessage {
 
-    INPUT_CAR_NAMES("자동차 이름을 쉼표(,)로 구분하여 입력하세요."),
+    INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     INPUT_ROUND("시도할 횟수는 몇 회인가요?"),
     RACE_RESULT_TITLE("실행 결과"),
     RACE_STATUS("%s : %s"),
