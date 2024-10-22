5dee5b911e927d06f900ff9dd2c9581f5857f1e5
urim96 <qusdbfla1@gmail.com>
2024-10-22 19:09:15 +0900

    배열 길이 확인
    경주하는 자동차가 1개 이하인 경우 예외처리

--- src/main/java/racingcar/CarArrayCheck.java
+++ src/main/java/racingcar/CarArrayCheck.java
@@ -3,8 +3,18 @@ package racingcar;
 public class CarArrayCheck {
 	
 	String[] carArray = {};
+	int carLength = 0;
 	
 	public void carNameSplit(String carName) {
 		this.carArray = carName.split(",");
+		this.carLength = carArray.length;
+		carArrayLength();
+	}
+	
+	public void carArrayLength() {
+		if(carArray == null || carLength <= 1) {
+			System.err.println("경주할 자동차를 2개 이상 입력해주세요.");
+			throw new IllegalArgumentException(); 
+		}
 	}
 }
