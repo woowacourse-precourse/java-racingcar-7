b7328285492ba564ac40cc40925397e37d289c0b
Firedrago95 <yonghwa95@naver.com>
2024-10-22 17:10:40 +0900

    feat(Cars): 자동차 객체 생성 기능 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,15 @@
+package racingcar.model;
+
+import java.util.Objects;
+
+public class Car {
+
+    public static final int START_POSITION = 0;
+    private String name;
+    private int position;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+}
---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,15 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class Cars {
+
+    public static final int START_POSITION = 0;
+    private List<Car> cars;
+
+    public Cars(List<String> carNames) {
+        cars = carNames.stream()
+            .map(carName -> new Car(carName, START_POSITION))
+            .toList();
+    }
+}
