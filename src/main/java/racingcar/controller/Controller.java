109892c72104d112c93edb357526f51e5242307d
choi14123 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-10-22 16:05:18 +0900

    feat: Controller PrintHandler 생성

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,7 +1,17 @@
 package racingcar.controller;
 
+import racingcar.handler.PrintHandler;
+
 public class Controller {
+    private final PrintHandler printHandler;
+
+
+    public Controller() {
+        this.printHandler = new PrintHandler();
+    }
+
     public void run () {
+        printHandler.racingStartMessage();
 
     }
 }
