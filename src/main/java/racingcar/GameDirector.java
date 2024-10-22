10d2008242f91fe644d6d30530b3eb36e3c913d5
박철현 <park82602@gmail.com>
2024-10-22 21:32:20 +0900

    Feat : 랜덤넘버에 따른 자동차들이 전진 및 정지하는 기능 구현 완료

--- src/main/java/racingcar/GameDirector.java
+++ src/main/java/racingcar/GameDirector.java
@@ -1,18 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class GameDirector {
-
-    private final String count;
+    private final int count;
     private final CarManager manager = new CarManager();
 
     public GameDirector(String carNames,String count) {
         manager.add(carNames);
-        this.count = count;
+        this.count = Integer.parseInt(count);
     }
 
-    public void run(String carNames, String count) {
-
+    public void run() {
+        List<Car> carList = manager.getList();
+        for (int i = 0; i < count; i++) {
+            for (Car car : carList) {
+                int moveNum = Randoms.pickNumberInRange(0, 9);
+                car.move(moveNum);
+            }
+        }
     }
 }
