861ad8871392da2466aa0410a4413820868b933b
Firedrago95 <yonghwa95@naver.com>
2024-10-22 16:55:51 +0900

    feat(InputValidator): 자동차 이름 중복 검사 기능 구현

--- src/main/java/racingcar/util/validator/InputValidator.java
+++ src/main/java/racingcar/util/validator/InputValidator.java
@@ -1,12 +1,29 @@
 package racingcar.util.validator;
 
+import java.util.HashSet;
+
 public class InputValidator {
 
     private static final String CAR_NAMES_REGEX = "[가-힣|a-zA-Z]{1,5}(,[가-힣|a-zA-Z]{1,5})*";
 
     public static void validateCarNames(String input) {
+        checkInputForm(input);
+        checkDuplicate(input);
+    }
+
+    private static void checkInputForm(String input) {
         if (!input.matches(CAR_NAMES_REGEX)) {
             throw new IllegalArgumentException("[ERROR] 올바른 자동차 이름을 입력해주세요");
+        }
+    }
+
+    private static void checkDuplicate(String input) {
+        String[] names = input.split(",");
+        HashSet<String> carNames  = new HashSet<>();
+        for (String name : names) {
+            if (!carNames.add(name)) {
+                throw new IllegalArgumentException("[ERROR] 중복된 자동차 이름을 입력하셨습니다.");
+            }
         }
     }
 }
