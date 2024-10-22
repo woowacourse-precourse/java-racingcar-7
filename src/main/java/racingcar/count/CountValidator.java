81d51214b96695547f480aa7f12e4270b11d7fbc
sik9252 <sik9252@naver.com>
2024-10-22 23:11:13 +0900

    feat: 시도 횟수에 대한 모든 검증을 수행하는 메서드를 추가하여 로직을 하나로 묶기

--- src/main/java/racingcar/count/CountValidator.java
+++ src/main/java/racingcar/count/CountValidator.java
@@ -4,6 +4,12 @@ import racingcar.constant.ErrorMessage;
 import racingcar.utils.StringUtils;
 
 public class CountValidator {
+    public void checkIsAttemptCountValid(String attemptCount) {
+        checkIsEmpty(attemptCount);
+        checkIsNumber(attemptCount);
+        checkIsSmallThanZero(attemptCount);
+    }
+
     private void checkIsEmpty(String attemptCount) {
         if (attemptCount.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_IS_ATTEMPT_COUNT_EMPTY.getMessage());
