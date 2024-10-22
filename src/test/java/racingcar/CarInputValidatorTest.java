260a756cd14ca2887ee14ce3c4b50cd519e82c39
jbilee <julishahn@gmail.com>
2024-10-23 00:43:52 +0900

    test(validator): CarInputValidator 기능에 대한 테스트 코드 작성

---
+++ src/test/java/racingcar/CarInputValidatorTest.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.validators.CarInputValidator;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CarInputValidatorTest {
+    private final CarInputValidator validator = new CarInputValidator();
+
+    @DisplayName("자동차 이름으로 입력한 값이 유효하지 않으면 예외를 발생시킨다.")
+    @Test
+    void 자동차_입력값_검증_테스트() {
+        assertThrows(RuntimeException.class, () -> validator.validate("car1"));
+        assertThrows(RuntimeException.class, () -> validator.validate("car111,car2"));
+    }
+}
