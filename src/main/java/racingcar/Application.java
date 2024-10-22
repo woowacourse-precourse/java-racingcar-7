eb16ff3ae80b4cf13d3b57362a5aad308bb670a7
kim_sangwoo <gnbupi@naver.com>
2024-10-22 18:07:08 +0900

    Feat(input): 자동차 이름 및 횟수 입력 기능 구현
    자동차 이름과 레이스 횟수를 입력받고 검증하는 기능을 개발했습니다.
    - 자동차 이름을 입력받아 5글자 이하로 검증합니다.
    - 이름 검증 후 레이스 횟수를 입력받습니다.
    - 5글자를 초과하는 잘못된 자동차 이름에 대해 오류를 발생시킵니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,49 @@
 package racingcar;
 
-public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+import camp.nextstep.edu.missionutils.Console;
+
+public class Application
+{
+
+    public static void checkWrongInput(String name)
+    {
+        if(name.length()>5)
+            throwInputError();
+    }
+
+    public static void throwInputError()
+    {
+        throw new IllegalArgumentException();
+    }
+
+    public static String[] splitNames(String carNames)
+    {
+        String[] carName = carNames.split(",");
+        for(int i=0; i<carName.length;i++)
+        {
+            carName[i]=carName[i].trim();
+            checkWrongInput(carName[i]);
+        }
+        return carName;
+    }
+
+    public static String[] nameInput()
+    {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        return splitNames(carNames);
+    }
+
+    public static int countInput()
+    {
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public static void main(String[] args)
+    {
+        //TODO 5글자 이하인지 검증-잘못 입력시 오류
+        //TODO 반복횟수 입력
+        String[] tokenName=nameInput();
+        int count=countInput();
     }
 }
