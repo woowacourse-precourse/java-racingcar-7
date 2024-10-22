771e3b7e8fea1c73b6f953650b57a3c41fc0f2f1
박성춘 <songchoon10@naver.com>
2024-10-22 20:33:42 +0900

    feat: Controller로 부터 프로그램이 시작되며 View에서 읽어온 값들을 넘겨줌

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,4 +1,26 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+
 public class RacingController {
+    private final InputView inputView;
+
+    public RacingController(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void run() {
+        // Todo : record를 이용하여 DTO 로 넘겨주는 방식으로 리팩토링 가능할듯함.
+        String carNames = readCarNames();
+        int tryCount = readTryCount();
+
+    }
+
+    private String readCarNames() {
+        return inputView.readCarNames();
+    }
+
+    private int readTryCount() {
+        return inputView.readTryCount();
+    }
 }
