ebfc2720d8950ec3383565d1da8171ec3bb60d0d
SeongbinPark <hyucse22@gmail.com>
2024-10-22 17:12:36 +0900

    feat(Application): 각 자동차 이름으로 Car배열을 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,12 +2,21 @@ package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
+
+        List<Car> cars = new ArrayList<>();
+
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = readLine();
         String[] carNames = input.split(",");
 
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
 
     }
 }
