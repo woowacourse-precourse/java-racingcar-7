998f5370320ca96f56f12311dcbaaddc08dcb92b
Yehun <yhyh4420@naver.com>
2024-10-22 23:14:06 +0900

    feat:해쉬맵 갱신에 따른 winner의 score 및 winner 구하는 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,9 +2,13 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class Application {
+
     public static HashMap<String, Integer> getUserInfo() {
         String input = Console.readLine();
         String[] StrArr_Name = input.split(",");
@@ -19,14 +23,26 @@ public class Application {
                     int step = scoreMap.get(name) + 1;
                     scoreMap.put(name, step);
                 }
+                System.out.println(name + " : " + scoreMap.get(name));
             }
-        }
 
+        }
         return scoreMap;
     }
+    public static ArrayList findWinner(HashMap<String, Integer> scoreMap) {
+        Integer winnerScore = Collections.max(scoreMap.values());
+        ArrayList<String> ArrWinner = new ArrayList<>(scoreMap.size());
+        for (String name:scoreMap.keySet()) {
+            if (scoreMap.get(name).equals(winnerScore)) {
+                ArrWinner.add(name);
+            }
+        }
+        return ArrWinner;
+        }
 
-    public static void main(String[] args) {
-        System.out.println(getUserInfo());
 
+    public static void main(String[] args){
+        HashMap<String, Integer> scoreMap = getUserInfo();
+        System.out.println(findWinner(scoreMap));
     }
 }
