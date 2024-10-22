057e50f97f48444e3484566ac2ab22925b835db1
Min <binine2@gmail.com>
2024-10-23 01:55:22 +0900

    feat: 시도횟수 입력 예외 처리 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -19,8 +19,8 @@ public class Application {
         String[] names = inputName.split(",");
 
         // 자동차 이름 글자수 검증
-        for (String name : names){
-            if (name.length() > 5){
+        for (String name : names) {
+            if (name.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
             }
         }
@@ -34,20 +34,26 @@ public class Application {
 
         // 시도 횟수 입력
         System.out.println("시도할 횟수는 몇 회인가요?");
-        int inputCount = Integer.parseInt(Console.readLine());
+        int inputCount;
+
+        try {
+            inputCount = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수만 입력해 주세요.");
+        }
 
         // 실행 결과 출력
         System.out.println("실행 결과");
-        for (int i = 0; i < inputCount; i++){
-            for (int j = 0; j < names.length; j++ ){
+        for (int i = 0; i < inputCount; i++) {
+            for (int j = 0; j < names.length; j++) {
                 int randomNum = Randoms.pickNumberInRange(0, 9);
 
-                if (randomNum > 3){
+                if (randomNum > 3) {
                     cars.get(j).advance += 1;
                 }
             }
 
-            for (int j =0; j < names.length; j++){
+            for (int j = 0; j < names.length; j++) {
                 System.out.println(cars.get(j).name + " : " + "-".repeat(cars.get(j).advance));
             }
             System.out.println();
@@ -57,14 +63,14 @@ public class Application {
         List<String> winner = new ArrayList<>();
         List<Integer> advances = new ArrayList<>();
 
-        for (int i = 0; i < names.length; i++){
+        for (int i = 0; i < names.length; i++) {
             advances.add(cars.get(i).advance);
         }
 
         int maxAdvance = Collections.max(advances);
 
-        for (int i = 0; i < names.length; i++){
-            if (cars.get(i).advance == maxAdvance){
+        for (int i = 0; i < names.length; i++) {
+            if (cars.get(i).advance == maxAdvance) {
                 winner.add(cars.get(i).name);
             }
         }
