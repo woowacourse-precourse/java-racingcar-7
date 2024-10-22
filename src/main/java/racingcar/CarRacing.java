d90b5b65186faf293bab8dcd31124101db2dbd6d
rdyjun <rdyjun00@gmail.com>
2024-10-22 20:47:17 +0900

    feat: 이름을 통해 Car인스턴스를 생성하여, 멤버 변수에 저장하는 기능 구현

--- src/main/java/racingcar/CarRacing.java
+++ src/main/java/racingcar/CarRacing.java
@@ -6,15 +6,18 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class CarRacing {
+    private static Car[] cars;
+
     public static void start() throws IOException {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String carNames = bufferedReader.readLine();
 
-        String[] carNameArray = Arrays.stream(carNames.split(","))
+        CarRacing.cars = Arrays.stream(carNames.split(","))
                 .peek(CarRacing::validateCarName)
-                .toArray(String[]::new);
+                .map(Car::new)
+                .toArray(Car[]::new);
     }
 
     private static void validateCarName(String carName) {
