7b572ce821af2a84b2cb35ec7f536b13581071e0
openmpy <suhwan@kakao.com>
2024-10-23 01:06:30 +0900

    feat: RacingGame 클래스 추가 및 ViewMessage 상수 업데이트

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,47 @@
+package racingcar.domain;
+
+import static racingcar.view.constants.ViewMessage.RACE_RESULT_TITLE;
+import static racingcar.view.constants.ViewMessage.RACE_STATUS;
+
+import java.util.List;
+import racingcar.common.RandomNumber;
+
+public class RacingGame {
+
+    private final List<Car> cars;
+    private final RacingGameRound gameRound;
+
+    public RacingGame(final List<Car> cars, final Integer gameRound) {
+        this.cars = cars;
+        this.gameRound = new RacingGameRound(gameRound);
+    }
+
+    public void play() {
+        Integer round = gameRound.getRound();
+
+        for (int i = 0; i < round; i++) {
+            raceRound();
+        }
+    }
+
+    private void printRaceStatus() {
+        for (Car car : cars) {
+            String carName = car.getName();
+            Integer distance = car.getDistance();
+            String raceStatus = String.format(RACE_STATUS.getMessage(), carName, "-".repeat(distance));
+
+            System.out.println(raceStatus);
+        }
+        System.out.println();
+    }
+
+    private void raceRound() {
+        System.out.println(RACE_RESULT_TITLE.getMessage());
+
+        for (Car car : cars) {
+            int randomNumber = RandomNumber.pick();
+            car.forward(randomNumber);
+        }
+        printRaceStatus();
+    }
+}
--- src/main/java/racingcar/view/constants/ViewMessage.java
+++ src/main/java/racingcar/view/constants/ViewMessage.java
@@ -3,6 +3,8 @@ package racingcar.view.constants;
 public enum ViewMessage {
 
     INPUT_CAR_NAMES("자동차 이름을 쉼표(,)로 구분하여 입력하세요."),
+    RACE_RESULT_TITLE("실행 결과"),
+    RACE_STATUS("%s : %s"),
     ;
 
     private final String message;
