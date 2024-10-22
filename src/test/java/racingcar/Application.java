697e59bae1ff81878b4097de679e2709517e3f4d
joshinhyeok <shtree07@naver.com>
2024-10-23 01:04:49 +0900

    feat: 시도할 횟수를 입력받는 기능 추가

--- src/test/java/racingcar/Application.java
+++ src/test/java/racingcar/Application.java
@@ -4,7 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        RacingCar racingCar = new RacingCar();
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String inputString = Console.readLine();
+        String[] racingCars = racingCar.separateCarNames(inputString);
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String inputInt = Console.readLine();
     }
 }
