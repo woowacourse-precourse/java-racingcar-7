08c7ef8a86476bf06680f4ae906f93a1ee743226
hwan2-99 <thh1233123@gmail.com>
2024-10-23 00:18:39 +0900

    refactor: 자동차 전진 조건 수정

--- src/main/java/racingcar/strategy/RandomMoveStrategy.java
+++ src/main/java/racingcar/strategy/RandomMoveStrategy.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RandomMoveStrategy implements MoveStrategy {
     private static final int MOVE_STANDARD_VALUE = 4;
     private static final int START_INCLUSIVE = 0;
-    private static final int END_INCLUSIVE = 4;
+    private static final int END_INCLUSIVE = 9;
 
     @Override
     public boolean isMovable() {
