e3d713a58ed9c3113e0dfe90b503389baafff780
mohyerolo <garam2101@naver.com>
2024-10-22 23:10:38 +0900

    fix: enum 클래스의 value를 가지고 오도록 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,13 +5,13 @@ import racingcar.domain.Input;
 
 public class InputView {
     public Input readRacingCarNames() {
-        System.out.println(DisplayedMessage.INPUT_CARS_MESSAGE);
+        System.out.println(DisplayedMessage.INPUT_CARS_MESSAGE.getValue());
         String cars = Console.readLine();
         return new Input(cars);
     }
 
     public Input readRacingTryCount() {
-        System.out.println(DisplayedMessage.INPUT_TRY_COUNT_MESSAGE);
+        System.out.println(DisplayedMessage.INPUT_TRY_COUNT_MESSAGE.getValue());
         String count = Console.readLine();
         return new Input(count);
     }
