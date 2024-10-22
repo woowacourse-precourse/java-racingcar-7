5692279f7a230c69853a8b68c1d01e055b202064
leejunnyeop <daffodil8620@naver.com>
2024-10-23 03:57:50 +0900

    [feat] main 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+
+import racingcar.controller.RacingController;
+import racingcar.model.RacingcarModel;
+import racingcar.view.RacingView;
+
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingcarModel model = new RacingcarModel();
+        RacingView view = new RacingView();
+        RacingController controller = new RacingController(model, view);
+
+        controller.run();  // 게임 실행
+
     }
 }
