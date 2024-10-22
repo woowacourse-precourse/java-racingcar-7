60d6e350d4356217658ac0499fb5fd7557711ada
choi14123 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-10-22 16:21:17 +0900

    feat: 몇번 반복할지 받는 기능 추가

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,17 +1,20 @@
 package racingcar.controller;
 
+import java.util.Arrays;
+import racingcar.handler.InputHandler;
 import racingcar.handler.PrintHandler;
 
 public class Controller {
     private final PrintHandler printHandler;
-
+    private final InputHandler inputHandler;
 
     public Controller() {
         this.printHandler = new PrintHandler();
+        this.inputHandler = new InputHandler();
     }
 
     public void run () {
         printHandler.racingStartMessage();
-
+        String [] tempCarNames = inputHandler.inputCarNames();
     }
 }
--- src/main/java/racingcar/handler/PrintHandler.java
+++ src/main/java/racingcar/handler/PrintHandler.java
@@ -2,8 +2,13 @@ package racingcar.handler;
 
 public class PrintHandler {
     private static final String RACING_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String RACING_NUMBER_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
     public void racingStartMessage() {
         System.out.println(RACING_START_MESSAGE);
+    }
+
+    public void racingNumberMessage() {
+        System.out.println(RACING_NUMBER_MESSAGE);
     }
 }
