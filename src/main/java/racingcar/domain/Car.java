b6638798b110d5b69092a860cf52118a17043f2e
openmpy <suhwan@kakao.com>
2024-10-23 00:14:40 +0900

    feat: 난수 기반의 자동차 이동 기능 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,12 +2,20 @@ package racingcar.domain;
 
 public class Car {
 
+    private static final int FORWARD_NUMBER_LIMIT = 4;
+
     private final CarName name;
-    private final Integer distance;
+    private Integer distance;
 
     public Car(final String name) {
         this.name = new CarName(name);
         this.distance = 0;
+    }
+
+    public void forward(final int randomNumber) {
+        if (randomNumber >= FORWARD_NUMBER_LIMIT) {
+            distance++;
+        }
     }
 
     public String getName() {
