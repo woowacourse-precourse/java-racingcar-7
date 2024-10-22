8a432c937557f33b9582dd0b07ec08b58cd07a14
박철현 <park82602@gmail.com>
2024-10-22 19:03:31 +0900

    Style : sudo 코드로 시나리오를 작성했습니다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,13 +2,18 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Printer.print(SystemMessage.START_MESSAGE);
         String carNames = Console.readLine();
-
         Printer.print(SystemMessage.ATTEMPT_COUNT_MESSAGE);
         String count = Console.readLine();
+        /*
+        List<Car> list = Car.createCars(carNames);
+        GameDirector.run(list, count);
+        */
     }
 }
