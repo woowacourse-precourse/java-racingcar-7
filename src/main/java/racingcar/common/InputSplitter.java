fef359f3e8ed875c3de4f6d50e372017d06f5075
openmpy <suhwan@kakao.com>
2024-10-23 02:32:27 +0900

    refactor: DEFAULT_JOINER 상수 사용

--- src/main/java/racingcar/common/InputSplitter.java
+++ src/main/java/racingcar/common/InputSplitter.java
@@ -5,12 +5,13 @@ import java.util.List;
 public class InputSplitter {
 
     private static final String DEFAULT_DELIMITER = ",";
+    private static final String DEFAULT_JOINER = ", ";
 
     public static String[] split(final String input) {
         return input.split(DEFAULT_DELIMITER);
     }
 
-    public static String join(final List<String> elements) {
-        return String.join(", ", elements);
+    public static String join(final List<String> carNames) {
+        return String.join(DEFAULT_JOINER, carNames);
     }
 }
