1de17ed66195ff1ed00be20e2ee733a59f4de390
byeongdoohan <quden04@gmail.com>
2024-10-23 04:43:32 +0900

    feat: Car 클래스 추가 - 랜덤값 4이상일 때 drive 기능 추가

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,39 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+import java.util.List;
+
+public class Car {
+    private final String name;
+    private int score;
+    private boolean winner;
+    private final List<Integer> randomNumbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("차 이름은 5자를 초과할 수 없습니다.");
+        }
+        this.name = name;
+        this.score = 0;
+        this.winner = false;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public void addScore() {
+        this.score = getScore() + 1;
+    }
+
+    public void drive() {
+        if (Randoms.pickNumberInList(randomNumbers) >= 4) {
+            addScore();
+        }
+    }
+}
