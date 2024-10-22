c384ad73260a6d52a390ef8c026188a39e99d30b
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-10-22 16:58:39 +0900

    feat: car name 공백 에러 검사 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,10 +4,17 @@ public class Car {
     private final String name;
 
     public Car(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
         if(name.length() > 5){
             throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
         }
-        this.name = name;
+        if(name.isBlank()){
+            throw new IllegalArgumentException("자동차 이름은 공백이 될 수 없습니다.");
+        }
     }
 
     public String getName() {
--- src/main/java/racingcar/CommaCarNameTokenizer.java
+++ src/main/java/racingcar/CommaCarNameTokenizer.java
@@ -10,6 +10,9 @@ public class CommaCarNameTokenizer {
             if(carName.length() > 5){
                 throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
             }
+            if(carName.isBlank()){
+                throw new IllegalArgumentException("자동차 이름은 공백이 될 수 없습니다.");
+            }
         }
 
         return carNames;
