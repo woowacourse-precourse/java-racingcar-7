e11c26126f77f7a8d10a90a40967f2a5c009cc84
JS <zks145@naver.com>
2024-10-22 23:04:59 +0900

    feat (domain): Car 클래스 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+public class Car {
+
+    public String carName;
+    public String carMove;
+
+    public Car(String carName, String carMove) {
+        this.carName = carName;
+        this.carMove = carMove;
+    }
+}
