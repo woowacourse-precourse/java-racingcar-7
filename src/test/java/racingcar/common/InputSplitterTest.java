7ba78b644ffc33fe91248a9a060b92e15efabeee
openmpy <suhwan@kakao.com>
2024-10-22 23:35:39 +0900

    test: InputSplitter 에 대한 단위 테스트 추가
    - 입력값 분할 기능 테스트

---
+++ src/test/java/racingcar/common/InputSplitterTest.java
@@ -0,0 +1,35 @@
+package racingcar.common;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class InputSplitterTest {
+
+    @ParameterizedTest(name = "입력값: {0}, 기대값: {1}")
+    @MethodSource(value = "provideInput")
+    void 입력값_분할_기능_테스트(final String input, final String[] expected, final int expectedSize) {
+        // when
+        String[] numbers = InputSplitter.split(input);
+
+        // then
+        assertThat(numbers).hasSize(expectedSize);
+        assertThat(numbers).containsExactly(expected);
+
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    static Stream<Arguments> provideInput() {
+        return Stream.of(
+                Arguments.of("1", new String[]{"1"}, 1),
+                Arguments.of("1,2", new String[]{"1", "2"}, 2),
+                Arguments.of("1,2,3", new String[]{"1", "2", "3"}, 3),
+                Arguments.of("1,2 3,4", new String[]{"1", "2 3", "4"}, 3),
+                Arguments.of("1,,3", new String[]{"1", "", "3"}, 3)
+        );
+    }
+}
