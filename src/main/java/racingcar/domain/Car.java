3987392effec0ee92f7dfe01a462b614964ea0d7
hwan2-99 <thh1233123@gmail.com>
2024-10-22 22:14:03 +0900

    feat: 자동차를 다루는 클래스 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+public class Car {
+    private static final int MAX_LENGTH = 5;
+    private static final String BLANK = " ";
+    private static final String NAME_LENGTH_ERROR = "자동차의 이름의 길이는 1 이상 " + MAX_LENGTH + "이하이어야 합니다.";
+    private static final String BLANK_CONTAIN_ERROR_MESSAGE = "자동차 이름에 공백은 포함될 수 없습니다.";
+
+    private final String name;
+
+    public Car(String name) {
+        validateNameLength(name);
+        validateContainBlank(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.isEmpty() || name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR);
+        }
+    }
+
+    private void validateContainBlank(String name) {
+        if (name.contains(BLANK)) {
+            throw new IllegalArgumentException(BLANK_CONTAIN_ERROR_MESSAGE);
+        }
+    }
+}
