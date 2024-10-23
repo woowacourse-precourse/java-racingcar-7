83b0943b4616bcd1a4031bbe1de4ca8e295f896c
ohhyungsuh <ohhyungsuh@gmail.com>
2024-10-24 02:08:39 +0900

    test: mock 테스트 코드 작성
    - 공동 우승자 없는 경우
    - 공동 우승자 있는 경우
    - 입력값 공백인 경우
    - 자동차 이름 공백인 경우
    - 자동차 글자수 초과한 경우
    - 이동 횟수 공백인 경우
    - 이동 횟수 0 또는 음수인 경우
    - 이동 횟수 숫자 아닌 경우

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,108 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 공동_우승자_없는_경우 () {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "3 ");
+                    assertThat(output()).contains("pobi : --", "woni : -", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD, STOP, STOP, MOVING_FORWARD
+                );
+    }
+
+    @Test
+    void 공동_우승자_있는_경우 () {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "4");
+                    assertThat(output()).contains("pobi : --", "woni : --", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, STOP, STOP, MOVING_FORWARD, MOVING_FORWARD, STOP, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 입력값_공백인_경우 () {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("         ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름_공백인_경우1 () {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni, ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+//    @Test
+//    void 자동차_이름_공백인_경우2 () {
+//        assertSimpleTest(() ->
+//                assertThatThrownBy(() -> runException("pobi,woni,", "1"))
+//                        .isInstanceOf(IllegalArgumentException.class)
+//        );
+//    }
+
+    @Test
+    void 자동차_이름_5글자_초과한_경우 () {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni,woowacourse", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이동_횟수_공백인_경우1 () {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "\n"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이동_횟수_공백인_경우2 () {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "  "))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+//    @Test
+//    void 이동_횟수_공백인_경우3 () {
+//        assertSimpleTest(() ->
+//                assertThatThrownBy(() -> runException("pobi,woni", ""))
+//                        .isInstanceOf(IllegalArgumentException.class)
+//        );
+//    }
+
+    @Test
+    void 이동_횟수_음수인_경우 () {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "-2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이동_횟수_0인_경우 () {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이동_횟수_숫자_아닌_경우 () {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "hello"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
