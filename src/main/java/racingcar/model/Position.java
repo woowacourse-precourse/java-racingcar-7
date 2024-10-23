7142e37533fc4b756eb291a0f1c9f00ce7a6983d
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-10-24 00:50:45 +0900

    feat(position): add equals and hashCode
    - equals and hashCode methods for checking same Position

--- src/main/java/racingcar/model/Position.java
+++ src/main/java/racingcar/model/Position.java
@@ -1,5 +1,7 @@
 package racingcar.model;
 
+import java.util.Objects;
+
 public class Position {
 
     private static final int DEFAULT_POSITION = 0;
@@ -25,5 +27,22 @@ public class Position {
 
     public int value() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position position = (Position) o;
+        return value == position.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
