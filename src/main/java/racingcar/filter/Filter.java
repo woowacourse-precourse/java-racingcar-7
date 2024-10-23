e881a04c4de684affddbb5b2adff2641ee477d19
KNU-K <knukang334@gmail.com>
2024-10-24 03:48:53 +0900

    feat(filter): 필터 기본 인터페이스 추가
    - doFilter를 통해서 해당 필터에 대한 작업을 수행한다.
    - 제네릭을 통해서 유연한 요청, 응답에 대응한다.

---
+++ src/main/java/racingcar/filter/Filter.java
@@ -0,0 +1,22 @@
+package racingcar.filter;
+
+/**
+ * 필터 인터페이스입니다.
+ * <p>
+ * 이 인터페이스는 요청을 처리하는 필터의 기본 구조를 정의합니다. 각 필터는 요청을 받아들이고, 다음 필터 체인으로 전달할 수 있습니다.
+ * </p>
+ *
+ * @param <R> 요청 객체의 타입
+ * @param <T> 응답 객체의 타입
+ */
+public interface Filter<R, T> {
+    /**
+     * 주어진 요청을 처리하고, 다음 필터 체인을 통해 응답을 반환합니다.
+     *
+     * @param request     처리할 요청 객체
+     * @param response    처리할 응답 객체
+     * @param filterChain 다음 필터 체인
+     * @return 처리된 응답 객체
+     */
+    void doFilter(R request, T response, RaceFilterChain filterChain);
+}
