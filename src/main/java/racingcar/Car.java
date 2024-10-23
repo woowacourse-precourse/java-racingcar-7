3cf47b68b8f4b3d5fd708fb7284f0e53beb3651f
alpin87 <qortmdals94@naver.com>
2024-10-23 20:26:49 +0900

    feat: 자동차의 현재 위치 표시 기능

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,6 +3,8 @@ package racingcar;
 public class Car {
     private static final int MAX_NAME_LENGTH = 5;
     private static final String ERROR_NAME_LENGTH = "자동차 이름은 5자 이하여야 합니다.";
+    private static final String POSITION_SEPARATOR = " : ";
+    private static final String POSITION_INDICATOR = "-";
 
     private final String name;
     private int position;
@@ -29,5 +31,9 @@ public class Car {
         if (name == null || name.trim().isEmpty() || name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(ERROR_NAME_LENGTH);
         }
+    }
+
+    public String getCurrentPosition() {
+        return name + POSITION_SEPARATOR + POSITION_INDICATOR.repeat(position);
     }
 }
