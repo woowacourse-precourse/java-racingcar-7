1ece01d69b0e9ea90411899549d39706a33a8c1c
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-10-24 00:40:04 +0900

    feat(RacingCarApp): 자동차 생성 기능 작성
    - 주어진 문자열에서 ,을 기준으로 분리하여 자동차 객체 성성 후 저장

--- src/main/java/racingcar/RacingCarApp.java
+++ src/main/java/racingcar/RacingCarApp.java
@@ -1,8 +1,10 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 
 public class RacingCarApp {
+    private ArrayList<Car> cars = new ArrayList<>();
     public String getCarInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
@@ -13,6 +15,12 @@ public class RacingCarApp {
             return Integer.parseInt(Console.readLine());
         }catch (Exception e){ // 정수 변환 실패시
             throw new IllegalArgumentException();
+        }
+    }
+
+    public void makeCar(String inputData) {
+        for (String carName : inputData.split(",")) {
+            cars.add(new Car(carName));
         }
     }
 }
