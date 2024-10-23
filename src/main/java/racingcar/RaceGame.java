4ec3a4bb46934653de39fba83a49114dbc09e0ee
alpin87 <qortmdals94@naver.com>
2024-10-23 20:16:35 +0900

    feat: 입력받은 이름으로 자동차 객체 생성 기능

--- src/main/java/racingcar/RaceGame.java
+++ src/main/java/racingcar/RaceGame.java
@@ -1,4 +1,22 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RaceGame {
+
+    private final List<Car> cars;
+
+    public RaceGame(String carNames) {
+        this.cars = createCars(carNames);
+    }
+
+    private List<Car> createCars(String carNames) {
+        String[] names = RaceUtil.parseCarNames(carNames);
+        List<Car> carList = new ArrayList<>();
+        for (String name : names) {
+            carList.add(new Car(name));
+        }
+        return carList;
+    }
 }
