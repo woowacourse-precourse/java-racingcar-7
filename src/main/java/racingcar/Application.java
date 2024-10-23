18a6afedd4711f7c2f9d61ba143ee72f4ef19264
dev-Rhea <dev.rhea4444@gmail.com>
2024-10-23 21:11:04 +0900

    :sparkles: feat : 기능 작동 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import java.util.LinkedHashMap;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        LinkedHashMap<String, Integer> carNameAndScore = CarInformation.getCarInformation();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int tryCount = Input.inputTryCount();
+
+        RacingGame racingGame = new RacingGame(carNameAndScore, tryCount);
+        racingGame.randomMove();
+
+        racingGame.printWinner();
     }
 }
