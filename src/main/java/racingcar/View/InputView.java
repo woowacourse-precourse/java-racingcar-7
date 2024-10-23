27efb4bcb4c3f67d8fc94b161a93c34519838c6e
qudcks8084 <qudcks8084@naver.com>
2024-10-23 16:58:07 +0900

    Feat : Add view package and inputview.java
    - Add view package under racingcar package
    - Implement InputView.java to get user input
    - Implement 2 method to get 2 paramenter
    	- getRacingCarNameFromUser() : String
    	- getNumberOfRaceFromUser() : String

---
+++ src/main/java/racingcar/View/InputView.java
@@ -0,0 +1,32 @@
+package racingcar.View;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+import static java.lang.Integer.*;
+
+public class InputView {
+
+    /**
+     * Get Racing Car Names From User
+     * Input : X
+     * Output : String
+    **/
+    static public String getRacingCarNameFromUser(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return readLine();
+    }
+
+    /**
+     * Get How Many Times to Race From User
+      * Input : X
+      * Output : String
+     **/
+    static public String getNumberOfRaceFromUser(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return readLine();
+    }
+}
