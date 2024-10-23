79d04e3eec7fb3cfc8a5173aa81d86c0ea2a67ee
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-10-24 01:23:54 +0900

    feat(RacingCarApp): 프로그램 구동 함수 작성
    - 각 기능을 수행하기 위한 프로그램 구동함수 run() 작성

--- src/main/java/racingcar/RacingCarApp.java
+++ src/main/java/racingcar/RacingCarApp.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 
 public class RacingCarApp {
     private ArrayList<Car> cars = new ArrayList<>();
+
+    public void run() {
+        String inputData = getCarInput();
+        int attemptCount = getAttemptCount();
+        makeCar(inputData);
+    }
     public String getCarInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
