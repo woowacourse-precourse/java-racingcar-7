5d975d010f03affecc5ae8c28e8f84107597c416
bowook <mbosx316@naver.com>
2024-10-24 00:16:52 +0900

    feat(Race) : Race 기능 추가
    - 우승자 리스트 추출 기능
    - 전진인지 정지인지 판단하는 기능
    - 레이스 동작 기능

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -1,2 +1,55 @@
-package racingcar.domain;public class Race {
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+
+    private final Cars cars;
+    private final TrialCounts trialCounts;
+    private int currentCounts;
+    private int maxPosition;
+
+    public Race(Cars cars, TrialCounts trialCounts) {
+        this.cars = cars;
+        this.trialCounts = trialCounts;
+        this.currentCounts = 0;
+        this.maxPosition = 0;
+    }
+
+    private boolean goAndStop() {
+        int randomNumber = Randoms.pickNumberInRange(0,9);
+        return randomNumber >= 4;
+    }
+
+    private void moveCars() {
+        for(Car car : cars.getCars()) {
+            if(goAndStop()) {
+                car.move();
+            }
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+    }
+
+    public void raceOneStep() {
+        moveCars();
+        currentCounts++;
+    }
+
+    public boolean isRaceOngoing() {
+        return currentCounts < trialCounts.getTrialCounts();
+    }
+
+    public List<String> getWinners() {
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars.getCars()) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car.getCarName());
+            }
+        }
+        return winners;
+    }
+
 }
