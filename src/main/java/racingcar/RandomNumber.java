7c10bba2807fde11e3bc123282dda4ae5326b0ca
GaBaljaintheroom <junsu1222@naver.com>
2024-10-24 00:05:44 +0900

    refactor(RandomNumber): 클래스명/변수명 수정

---
+++ src/main/java/racingcar/RandomNumber.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public final class RandomNumber {
+
+    private RandomNumber() {
+    }
+
+    public static int generateInRange(int start, int end) {
+        return Randoms.pickNumberInRange(start, end);
+    }
+
+}
