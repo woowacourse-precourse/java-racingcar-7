74e1848ac6ea5b4d6f6f208677a155ae3fc7b94a
je488 <pje488@naver.com>
2024-10-23 13:41:13 +0900

    feat: 0부터 9 사이의 무작위 값을 생성하여 리턴하는 함수 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+    public static int makeRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
