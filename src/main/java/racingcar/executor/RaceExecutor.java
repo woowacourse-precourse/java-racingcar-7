8d6d742721d1a020924e9b930117939eeffeca80
KNU-K <knukang334@gmail.com>
2024-10-24 04:10:10 +0900

    feat(executor): 경주 실행 기능 구현
    - `RaceExecutor` 클래스 추가.
    - `CarList`를 기반으로 자동차들의 이동을 반복 횟수만큼 실행하는 `executeRace` 메서드 구현.
    - 주어진 반복 횟수(iterations)만큼 모든 자동차가 이동할 수 있도록 설정.

---
+++ src/main/java/racingcar/executor/RaceExecutor.java
@@ -0,0 +1,11 @@
+package racingcar.executor;
+
+import racingcar.model.CarList;
+
+public class RaceExecutor {
+    public void executeRace(CarList carList, int iterations) {
+        for (int i = 1; i <= iterations; i++) {
+            carList.moveAll();
+        }
+    }
+}
