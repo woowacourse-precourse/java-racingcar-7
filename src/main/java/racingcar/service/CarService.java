6e65f4965678db88d9103d9f9cca04e8c50cf0c2
kim <hwiyul_g@daum.net>
2024-10-23 14:41:19 +0900

    refactor(CarService): 순서를 유지시키기 위해 LinkedHashSet 이용 및 자동차 추가시 distance도 선택 가능하도록 추가

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -1,16 +1,18 @@
 package racingcar.service;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import racingcar.domain.Car;
 
 public class CarService {
     private static final int BASE_DISTANCE = 0;
 
-    private final Set<Car> cars = new HashSet<>();
+    private final Set<Car> cars = new LinkedHashSet<>();
 
-    public void addCar(String carName) {
-        Car car = new Car(carName, BASE_DISTANCE);
+    public void addCar(String carName, int distance) {
+        Car car = new Car(carName, distance);
         if (!cars.add(car)) {
             throw new IllegalArgumentException("[ERROR] 중복된 자동차 이름을 추가할 수 없습니다.");
         }
@@ -21,4 +23,5 @@ public class CarService {
             cars.forEach(Car::move);
         }
     }
+
 }
