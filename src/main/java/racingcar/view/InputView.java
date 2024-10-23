e2d8b274e2270b96bdaa60607a5ddd4c0b82e0a4
bowook <mbosx316@naver.com>
2024-10-23 21:04:34 +0900

    fix(InputView) : static으로 수정
    - 상태를 저장하고 있지 않기 때문에 static으로 수정하였습니다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,14 +3,14 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public String inputCarNames() {
+    public static String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
         Console.close();
         return carNames;
     }
 
-    public String inputTrialCounts() {
+    public static String inputTrialCounts() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String trialCounts = Console.readLine();
         Console.close();
