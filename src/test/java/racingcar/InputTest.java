467e5ece4eaff40ad65964d80db7886b4391396d
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2024-10-23 12:16:01 +0900

    test : 자동차 이름 유효성 테스트 추가

---
+++ src/test/java/racingcar/InputTest.java
@@ -0,0 +1,47 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+class InputTest extends NsTest {
+    @Test
+    void 자동차이름_성공_테스트() {
+        assertSimpleTest(() ->
+                runException("핍비,피비", "1")
+        );
+    }
+
+    @Test
+    void 자동차이름_공백_실패_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",다섯글자초과", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차이름_길이_초과_실패_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("다섯글자초과,둘다다섯글자초과", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차이름_특수문자_실패_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("다섯글자초과,둘다다섯글자초과", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
