9e327217e70970b2bb51d761e22634b93844e27c
dnwldla <leeemjw@naver.com>
2024-10-24 01:26:10 +0900

    feat: 우승자 목록을 문장으로 반환하는 함수 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,16 +1,18 @@
 package racingcar.domain;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
 
     private static final String DUPLICATED_NAME = "각 차는 한번만 입력해야 합니다";
     private static final String OUT_OF_RANGE_NAME = "차는 다섯글자 이하이어야 합니다";
 
-    private static final String BLANK="";
-    private static final String SPACE=" ";
-    private static final String DELIMITER=":";
+    private static final String BLANK = "";
+    private static final String SPACE = " ";
+    private static final String DELIMITER = ":";
     private static final String SPLITOR = ",";
     private static final Integer HIGHEST_NAME_LENGTH = 5;
 
@@ -62,8 +64,34 @@ public class Cars {
     public void printState() {
         cars.stream()
                 .forEach(car -> {
-                    System.out.println(car.name + SPACE + DELIMITER +SPACE+ car.state);
+                    System.out.println(car.name + SPACE + DELIMITER + SPACE + car.state);
                 });
         System.out.println(BLANK);
+    }
+
+    public String findWinners() {
+        int maxDistance = findMaxDistance();
+
+        List<Car> winnerList = filterWinners(maxDistance);
+
+        String winnerNames = winnerList.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(","));
+
+        return winnerNames;
+    }
+
+    private List<Car> filterWinners(int maxDistance) {
+        return cars.stream()
+                .filter((car) -> car.getDistance() == maxDistance)
+                .toList();
+    }
+
+    private int findMaxDistance() {
+        Car winner = cars.stream()
+                .max(Comparator.comparing(car -> car.getDistance()))
+                .orElseThrow(NullPointerException::new);
+
+        return winner.getDistance();
     }
 }
