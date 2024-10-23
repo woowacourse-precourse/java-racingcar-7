c740949d199fc0484448ce6a7f77b633e42d56cc
jang_jiwon <kooriangman@gmail.com>
2024-10-23 23:56:15 +0900

    feat : 자동차 이름 받기 기능 구현 & Readme 파일에 예외 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
+        if (carNames.length < 2) {
+            throw new IllegalArgumentException("자동차를 2개 이상 등록하세요.");
+        }
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            } else if (carName.trim().isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 빈 문자열이 될 수 없습니다.");
+            }
+        }
+
     }
 }
