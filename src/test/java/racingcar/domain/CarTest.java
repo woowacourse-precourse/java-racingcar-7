df632593f36840a91680ec019895cbca0a7d863f
imjunghun <dlawndgns9@naver.com>
2024-10-23 17:14:36 +0900

    test(Car): Car 도메인 로직 테스트

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,33 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CarTest {
+
+    Car car;
+
+    @BeforeEach
+    void init() {
+        car = new Car("pobi");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 9})
+    void 전진(int value) {
+        car.process(value);
+
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 3})
+    void 정지(int value) {
+        car.process(value);
+
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+}
