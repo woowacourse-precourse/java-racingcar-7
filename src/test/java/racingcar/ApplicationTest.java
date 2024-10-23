66eb4cd386dae35d3888510d08753e5850be944f
jenna <shinysuh1992@gmail.com>
2024-10-23 17:41:28 +0900

    test: 기능별 테스트 코드 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -24,11 +24,82 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 기능_테스트_2() {
+        assertRandomNumberInRangeTest(
+            () -> {
+                run("jenna ,ryan ,jason", "5");
+                assertThat(output()).contains(
+                    "jenna : -", "ryan : ", "jason : ",
+                    "jenna : --", "ryan : ", "jason : ",
+                    "jenna : --", "ryan : ", "jason : -",
+                    "jenna : --", "ryan : -", "jason : -",
+                    "jenna : --", "ryan : -", "jason : --",
+                    "최종 우승자 : jenna, jason"
+                );
+            },
+            MOVING_FORWARD, STOP, STOP,
+            MOVING_FORWARD, STOP, STOP,
+            STOP, STOP, MOVING_FORWARD,
+            STOP, MOVING_FORWARD, STOP,
+            STOP, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 자동차_이름_유효성_테스트_1_빈값_1() {
+        assertThatThrownBy(() -> runException("", "4"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("빈 이름은 허용되지 않습니다");
+    }
+
+    @Test
+    void 자동차_이름_유효성_테스트_1_빈값_2() {
+        assertThatThrownBy(() -> runException("test, test1, , test7", "4"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("빈 이름은 허용되지 않습니다");
+    }
+
+    @Test
+    void 자동차_이름_유효성_테스트_2_정규식() {
+        assertThatThrownBy(() -> runException("jenna, jason, summer, ryan", "3"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("자동차 이름은 영문, 한글, 숫자로 이루어진 5자 이하의 문자열만 허용됩니다");
+    }
+
+    @Test
+    void 자동차_이름_유효성_테스트_3_중복() {
+        assertThatThrownBy(() -> runException("luna, david, jay, luna", "3"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("중복된 이름은 허용되지 않습니다");
+    }
+
+    @Test
+    void 라운드_수_유효성_테스트_1_0() {
+        assertThatThrownBy(() -> runException("jenna, jason", "0"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("시도 횟수가 적절하지 않습니다");
+    }
+
+    @Test
+    void 라운드_수_유효성_테스트_2_음수() {
+        assertThatThrownBy(() -> runException("jenna, jason", "-3"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("시도 횟수가 적절하지 않습니다");
+    }
+
+    @Test
+    void 라운드_수_유효성_테스트_1_문자열() {
+        assertThatThrownBy(() -> runException("jenna, jason", "gogo"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("숫자를 입력해주세요");
     }
 
     @Override
