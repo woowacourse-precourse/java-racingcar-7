d4ae02e7d2bb1b8c275381aa410a7c64ccf11070
ParkHyunS00 <parkhyuns00@naver.com>
2024-10-23 19:23:43 +0900

    fix(Constant): 실행 결과 출력 메시지에 개행 문자 포함

--- src/main/java/racingcar/utils/Constant.java
+++ src/main/java/racingcar/utils/Constant.java
@@ -35,7 +35,7 @@ public class Constant {
 
     // 출력 메시지
     public static final String GAME_RESULT_MESSAGE = "\n실행 결과";
-    public static final String CAR_STATUS_MESSAGE = "%s : %s";
+    public static final String CAR_STATUS_MESSAGE = "%s : %s\n";
     public static final String GAME_WINNER_MESSAGE = "최종 우승자 : %s";
     public static final String MOVE_STRING = "-";
 
