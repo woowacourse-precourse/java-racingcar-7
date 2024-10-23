ac13e9bfd236a4fbda0e2dbb934c7a84b9b8625c
7i.w__k <tizmfns1218@Naver.com>
2024-10-23 12:36:31 +0900

    test(Input, TryCount): inputTest
    입력 검증을 위한 테스트

---
+++ src/test/java/racingcar/entity/InputTest.java
@@ -0,0 +1,65 @@
+package racingcar.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static racingcar.utils.ErrorMessage.INVALID_CAR_NAME;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class InputTest {
+
+    @Test
+    @DisplayName("정상 입력 테스트")
+    void test1() {
+        String s = "pobi,woni,jun";
+
+        Input input = new Input(s);
+        CarList expect = new CarList();
+        expect.add("pobi");
+        expect.add("woni");
+        expect.add("jun");
+
+        CarList result = input.toCarList();
+
+        assertThat(result).isEqualTo(expect);
+    }
+
+    @Test
+    @DisplayName("에러 : ")
+    void test2() {
+        String s = " ,woni,jun";
+        Input input = new Input(s);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                input::toCarList);
+
+        assertThat(exception.getMessage()).contains(INVALID_CAR_NAME.getMessage());
+    }
+
+    @Test
+    @DisplayName("에러 : 5자 초과")
+    void test3() {
+        String s = "pooobi,woni,jun";
+        Input input = new Input(s);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                input::toCarList);
+
+        assertThat(exception.getMessage()).contains(INVALID_CAR_NAME.getMessage());
+    }
+
+    @Test
+    @DisplayName("빈값 처리")
+    void test4() {
+        String s = ",,";
+        Input input = new Input(s);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                input::toCarList);
+
+        assertThat(exception.getMessage()).contains(INVALID_CAR_NAME.getMessage());
+    }
+
+}
---
+++ src/test/java/racingcar/entity/TryCountTest.java
@@ -0,0 +1,69 @@
+package racingcar.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static racingcar.utils.ErrorMessage.INVALID_COUNT;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class TryCountTest {
+
+    @Test
+    @DisplayName("정상 값 테스트")
+    void test1() {
+        String s = "5";
+        TryCount result = new TryCount(s);
+        TryCount except = new TryCount(5L);
+
+        assertThat(result).isEqualTo(except);
+    }
+
+    @Test
+    @DisplayName("try 동작 테스트")
+    void test2() {
+        int result = 0;
+        int except = 5;
+
+        String s = "5";
+        TryCount trycount = new TryCount(s);
+
+        while (trycount.canTry()) {
+            result++;
+        }
+
+        assertThat(result).isEqualTo(except);
+    }
+
+    @Test
+    @DisplayName("예외 : 소수 입력")
+    void test3() {
+        String s = "3.5";
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new TryCount(s));
+
+        assertThat(exception.getMessage()).isEqualTo(INVALID_COUNT.getMessage());
+    }
+
+    @Test
+    @DisplayName("0 입력")
+    void test4() {
+        String s = "0";
+
+        TryCount result = new TryCount(s);
+        TryCount except = new TryCount(0L);
+
+        assertThat(result).isEqualTo(except);
+    }
+
+    @Test
+    @DisplayName("음수 입력")
+    void test5() {
+        String s = "-5";
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new TryCount(s));
+
+        assertThat(exception.getMessage()).isEqualTo(INVALID_COUNT.getMessage());
+    }
+}
