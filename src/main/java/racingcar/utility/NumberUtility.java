dc3192e1136725e98de00badc5aa559cfffb9168
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2024-10-23 15:01:42 +0900

    fix(NumberUtility.java) : getNumber 함수 안정성개선 및 실수 지원하도록 수정
    getNumber 함수가 api로써 사용가능하기 때문에 숫자입력 예외 처리를 추가하였습니다.

--- src/main/java/racingcar/utility/NumberUtility.java
+++ src/main/java/racingcar/utility/NumberUtility.java
@@ -1,19 +1,25 @@
 package racingcar.utility;
 
+import racingcar.exception.RaceException;
+
 public class NumberUtility {
 
     private final static String NUMBER_REGEX = "^[0-9]+(\\\\.[0-9]+)?$";
+    private final static String INVALID_PARSE_NUMBER_MESSAGE = "유효하지 않은 숫자 양식입니다";
 
     public static boolean isNumberStr(String input){
         return input.matches(NUMBER_REGEX);
     }
 
-    public static boolean isPositiveNumber(int moveCount) {
-        return moveCount >= 0;
+    public static boolean isPositiveNumber(int numberStr) {
+        return numberStr >= 0;
     }
 
-    public static int getNumber(String moveCount) {
-        return Integer.parseInt(moveCount);
+    public static int getNumber(String numberStr) {
+        if(!isNumberStr(numberStr)){
+            throw new RaceException(INVALID_PARSE_NUMBER_MESSAGE);
+        }
+        return (int) Double.parseDouble(numberStr);
     }
 
 }
