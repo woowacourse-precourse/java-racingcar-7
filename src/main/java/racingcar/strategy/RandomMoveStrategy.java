405f509587a001817da039457879ffda3bf41631
Seo Jinmann <sjmmics@gmail.com>
2024-10-23 14:48:25 +0900

    feat : ($Strategy, $RandomMoveStrategy) 자동차 전진(또는 멈춤) 전략 패턴 추가
    주어진 횟수만큼 자동차가 이동할 기회를 얻는데 그 기회에 이동하는 방식을 결정하는 클래스로 strategy 클래스를 구현함. 0 ~ 9 사이의 정수 중 무작위로 정수를 추출해서 4 이상이 나오면 한 칸 전진하는 RandomMoveStrategy 클래스를 구현함.

---
+++ src/main/java/racingcar/strategy/RandomMoveStrategy.java
@@ -0,0 +1,18 @@
+package racingcar.strategy;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+/**
+ * 무작위 숫자로 진행 또는 멈춤을 정하는 전략
+ */
+public class RandomMoveStrategy implements Strategy {
+
+    @Override
+    public int getDistanceMove() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= 4) {
+            return 1;
+        }
+        return 0;
+    }
+}
---
+++ src/main/java/racingcar/strategy/Strategy.java
@@ -0,0 +1,9 @@
+package racingcar.strategy;
+
+/**
+ * 자동차가 전진할지 멈출지 전략을 제공하는 클래스
+ */
+public interface Strategy {
+
+    int getDistanceMove();
+}
