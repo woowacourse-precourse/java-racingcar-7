031f2b186507e7f7b071a1e1b5a7f68ea9d5dde0
seunghee <seunghee_chae3@tmax.co.kr>
2024-10-23 18:46:41 +0900

    feat(OutputView): 실행 결과 메세지 출력

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,10 @@
+package racingcar.view;
+
+public class OutputView {
+
+    private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
+
+    public static void printExecutionResultMessage() {
+        System.out.println(EXECUTION_RESULT_MESSAGE);
+    }
+}
