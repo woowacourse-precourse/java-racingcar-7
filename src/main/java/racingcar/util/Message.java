d0b2a0459a9806dba345e809e8eb96c7c1362c34
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-23 16:36:04 +0900

    refactor(Message) : 메시지 관련 상수 클래스 추가

---
+++ src/main/java/racingcar/util/Message.java
@@ -0,0 +1,16 @@
+package racingcar.util;
+
+public final class Message {
+
+    public static final String INPUT_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
+    public static final String INPUT_TRIAL_COUNT = "시도할 횟수는 몇 회인가요?";
+
+    public static final String OUTPUT_RACE_RESULT = "\n실행 결과";
+    public static final String OUTPUT_WINNER = "최종 우승자";
+
+    public static final String DELIMITER_POSITION_INCORRECT = "쉼표(,)의 위치가 잘못되었습니다.";
+    public static final String NAME_LENGTH_NOT_VALID = "이름의 길이가 맞지 않습니다.";
+
+    private Message() {
+    }
+}
