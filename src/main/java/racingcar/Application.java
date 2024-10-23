e6cdc77ac15e4249a1250c7d3709164fd413f6f8
ljhee92 <weilainib7@gmail.com>
2024-10-23 11:29:06 +0900

    fix: 시도할 횟수 입력 요청 문구 출력
    시도할 횟수를 입력 요청하는 문구 출력 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,8 +2,6 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-// 2. 시도할 횟수를 입력 받는다.
-//    * 횟수가 숫자인지 검증한다.
 // 3. 시도 횟수만큼 경주를 반복하고, 횟수별 경주 결과를 출력한다.
 // 4. 경주마다 자동차별 0~9 사이 무작위 값을 구한 후, 값이 4 이상이면 전진한다.
 // 5. 마지막 경주 이후 최종 우승자를 출력한다.
@@ -24,6 +22,7 @@ public class Application {
             } // end if
         } // end for
 
+        System.out.println("시도할 횟수는 몇 회인가요?");
         String inputTimes = Console.readLine();
         if (!inputTimes.matches("^[0-9]+$")) {
 //            throw new NumberFormatException("숫자를 입력해주세요.");
