8e5a2313fed796e595dbc77c15490495fb95056c
KEEKE132 <dksvlfdhs1@naver.com>
2024-10-23 21:51:34 +0900

    feat(RacingCarService): 자동차 객체 생성 기능 추가

--- src/main/java/racingcar/domain/RacingCarService.java
+++ src/main/java/racingcar/domain/RacingCarService.java
@@ -1,11 +1,17 @@
 package racingcar.domain;
 
+import racingcar.controller.NameExtractor;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCarService {
     private List<RacingCar> cars = new ArrayList<>();
-    Integer number;
+    private Integer number;
 
-    public RacingCarService(String nameStr,Integer number) {}
+    public RacingCarService(String nameStr,Integer number) {
+        this.number = number;
+        NameExtractor.extractNames(nameStr).stream()
+                .forEach(name -> cars.add(new RacingCar(name)));
+    }
 }
