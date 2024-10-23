2bab4d70edf9c9f2e181be78cf64530eca7396b8
NaMinhyeok <nmh9097@gmail.com>
2024-10-23 14:21:30 +0900

    feat: 자동차를 관리할 일급컬렉션 구현

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import java.util.List;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    private Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars from(List<Car> cars) {
+        return new Cars(cars);
+    }
+
+}
