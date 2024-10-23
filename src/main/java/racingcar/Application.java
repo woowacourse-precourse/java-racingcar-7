392bf37b95a8736f6ccdaa860e51faab0bc385e4
yongminKwon1 <ym.kwon@lipis.kr>
2024-10-24 05:08:09 +0900

    feat(RacingCar): 자동차 이동로직 추가
    자동차 이동 로직을 추가했습니다. 60% 확률로 이동합니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -9,5 +10,18 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = readLine();
         List<String> carNames = StringParser.parse(input, ",");
+
+        List<RacingCar> racingCars = new ArrayList<>();
+        for (String carName : carNames) {
+            racingCars.add(new RacingCar(carName));
+        }
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int moveTryCount = Integer.parseInt(readLine());
+        for (int i = 0; i < moveTryCount; i++) {
+            for (RacingCar racingCar : racingCars) {
+                racingCar.move();
+            }
+        }
     }
 }
--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,11 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCar {
-    private String name;
     private int position;
 
     public RacingCar(String name) {
-        this.name = name;
         this.position = 0;
+    }
+
+    public int move() {
+        if (4 <= Randoms.pickNumberInRange(0, 9)) {
+            position++;
+        }
+        return position;
     }
 }
