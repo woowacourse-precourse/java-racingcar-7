6a3ebc1e563cc766266662f657d96c8def3af21a
MinseokGo <rhalstjr1999@naver.com>
2024-10-23 16:43:57 +0900

    feat: 자동차 이름 길이 유효성을 검사하는 기능

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,6 +5,9 @@ public class Car {
     private final String name;
 
     public Car(final String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5글자를 초과할 수 없습니다.");
+        }
         this.name = name;
     }
 }
