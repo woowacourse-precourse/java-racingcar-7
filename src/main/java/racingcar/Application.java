d847d0c1b4eb1c14fadba98fa9461034d77e8ac6
dhrgodms <dhrgodms123@gmail.com>
2024-10-23 21:42:20 +0900

    feat: Add 5-경주 게임 진행(stub)
    - racingStart(): attempts 수만큼 반복하며 자동차 게임을 진행하고, 승자를 결정하는 함수를 반환한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -14,8 +16,12 @@ public class Application {
 
     public static class Car {
         final String name;
+
         public Car(String name) {
             this.name = name;
+        }
+
+        public void run() {
         }
     }
 
@@ -39,7 +45,7 @@ public class Application {
         ArrayList<String> convertCarNames(String nameInput) {
             ArrayList<String> carNames = new ArrayList<>();
 
-            if(validateInput(CAR_NAME_REGEXP, nameInput)) {
+            if (validateInput(CAR_NAME_REGEXP, nameInput)) {
                 carNames = new ArrayList<>(Arrays.asList(nameInput.split(",")));
             }
             // 자동차 이름이 하나만 입력된 경우 -> 경주를 진행할 수 없다.
@@ -50,7 +56,7 @@ public class Application {
         }
 
         int convertAttempts(String countInput) {
-            if(validateInput(ATTEMPTS_REGEXP, countInput)) {
+            if (validateInput(ATTEMPTS_REGEXP, countInput)) {
                 return Integer.parseInt(countInput);
             }
             return 0;
@@ -65,15 +71,33 @@ public class Application {
 
         ArrayList<Car> createCarObjects(ArrayList<String> carNames) {
             ArrayList<Car> cars = new ArrayList<>();
-            for(String name: carNames) {
+            for (String name : carNames) {
                 cars.add(new Car(name));
             }
             return cars;
         }
 
         ArrayList<String> racingStart(ArrayList<Car> cars, int attempts) {
-            return new ArrayList<>();
+            for (int i = 0; i < attempts; i++) {
+                // 랜덤 값 생성하고 자동차 전진
+                for (Car car : cars) {
+                    car.run();
+                }
+                // 현황 문자열 생성하고 값 출력
+                printGameStatus();
+            }
+            return determineWinner(cars);
         }
+
+        private ArrayList<String> determineWinner(ArrayList<Car> cars) {
+            return new ArrayList<String>();
+        }
+
+        private void printGameStatus() {
+        }
+
+
+
 
     }
 }
