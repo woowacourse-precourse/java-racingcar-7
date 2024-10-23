ffa39cf6b9a715ec47c1953ca3869c1faf9fbf95
chaerish <ccyy8432@naver.com>
2024-10-23 23:26:15 +0900

    feat(InputView): 사용자 입력 공백값 검사 메소드 생성
    InputView의 사용자 입력이 공백일 시 false을 리턴하는 메소드를 생성한다.

--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -24,5 +24,15 @@ class InputViewTest {
         boolean isValidateTryNum = inputView.isValidateTryNum("1");
         assertThat(isValidateTryNum).isTrue();
     }
+    @Test
+    void 차이름_공백인지_검사(){
+        boolean isBlankCarName = inputView.isValidateCarName("      ");
+        assertThat(isBlankCarName).isFalse();
+    }
+    @Test
+    public void 시도숫자_공백값인지_검사(){
+        boolean isValidateTryNum = inputView.isValidateTryNum(" ");
+        assertThat(isValidateTryNum).isFalse();
+    }
 
 }
