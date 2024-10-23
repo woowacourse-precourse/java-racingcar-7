d1cc1f0d31ec5632357d22bfa78d7f2e55007bbb
Leegeonwoo7 <workgeon07@naver.com>
2024-10-24 01:39:59 +0900

    feat: 우승자 출력 구현

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -14,6 +14,67 @@ public class RacingCar {
         String inputCarNames = Console.readLine();
         System.out.println("시도할 횟수는 몇 회인가요?");
         String inputRoofCount = Console.readLine();
+
+        System.out.println("실행 결과");
+        for (int i = 0; i < roofCount; i++) {
+            for (Car car : cars) {
+                if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                    car.moveForward();
+                }
+            }
+
+            printCurrentResult(cars);
+            System.out.println();
+        }
+
+        List<Car> winners = getWinners(cars);
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < winners.size(); i++) {
+            System.out.print(winners.get(i).getName());
+            if (i < winners.size() - 1) {
+                System.out.print(",");
+            }
+        }
+
+    }
+
+    private static List<Car> getWinners(List<Car> cars) {
+        int highestPoint = 0;
+
+        //stream max 사용 가능?
+        for (Car car : cars) {
+            int winningPoint = car.getWinningPoint();
+            if (winningPoint > highestPoint) {
+                highestPoint = winningPoint;
+            }
+        }
+
+        //stream filter가 나을지도? 성능고려?
+        List<Car> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.winningPoint == highestPoint) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
+
+    //StringBuilder 사용하면 더 나을지도
+    private static void printCurrentResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + parse(car.winningPoint));
+        }
+    }
+
+    //StringBuilder 사용 하면 더 나을지도
+    private static String parse(int point) {
+        String result = "";
+        for (int i = 0; i < point; i++) {
+            result += "-";
+        }
+        return result;
+    }
+
     public static class Car {
         private String name;
         private int winningPoint;
