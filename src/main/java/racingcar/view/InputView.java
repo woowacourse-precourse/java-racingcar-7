dee5325d9438ca1c808f4bd46abb986afec88067
DoomchitYJ <poj1004@inha.edu>
2024-10-23 15:32:03 +0900

    feat: 자동차 이름을 입력받는 기능 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String INPUT_VIEW = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public static String readInputString() {
+        System.out.println(INPUT_VIEW);
+        String input = Console.readLine().trim();
+        Console.close();
+
+        return input;
+    }
+}
