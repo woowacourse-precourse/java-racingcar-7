0fc689f370233201eec630e2ef7be1d58844e2cf
bowook <mbosx316@naver.com>
2024-10-23 21:40:55 +0900

    refactor(Cars) : 자동차 이름 검증 기능 분리
    Cars에서 수행하던 검증을 CarNameValidator로 분리하였습니다

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,45 +1,23 @@
 package racingcar.domain;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Cars {
-    private final static String COMMA = ",";
+    private static final String COMMA = ",";
     private final List<Car> cars;
-    public Cars(String carNames) {
+
+    public Cars(String carNames, CarNameValidator validator) {
         List<String> convertedStringToList = convertStringToList(carNames);
-        validateProcess(convertedStringToList);
+        validator.validate(convertedStringToList);
         this.cars = convertedStringToList.stream().map(Car::new).collect(Collectors.toList());
     }
+
     private List<String> convertStringToList(String carNames) {
         return List.of(carNames.split(COMMA));
     }
-    private void validateProcess(List<String> carNames) {
-        validateLengthCarName(carNames);
-        validateDuplicationCarName(carNames);
-    }
 
-    private void validateLengthCarName(List<String> carNames) {
-        for(String carName : carNames) {
-            if(carName.isBlank()) {
-                throw new IllegalArgumentException();
-            }
-            if(carName.length() > 5) {
-                throw new IllegalArgumentException();
-            }
-        }
-    }
-
-    private void validateDuplicationCarName(List<String> carNames) {
-        HashMap<String, Integer> carsMap = new HashMap<>();
-        for (String carName : carNames) {
-            carsMap.put(carName, carsMap.getOrDefault(carName,0) + 1);
-        }
-        for (int count : carsMap.values()) {
-            if (count > 1) {
-                throw new IllegalArgumentException();
-            }
-        }
+    public List<Car> getCars() {
+        return cars;
     }
 }
