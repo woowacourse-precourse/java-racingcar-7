2d3171561f876d3bd9bf7ce762a64dfcbeff834b
Wooseok Jang <wowseok2774@gmail.com>
2024-10-23 19:12:36 +0900

    feat: 애플리케이션 초기 설정 및 MainController 실행 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import controller.MainController;
+import controller.RacingCarService;
+import factory.CarFactory;
+import view.InputHandler;
+import view.OutputHandler;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        InputHandler inputHandler = new InputHandler();
+        OutputHandler outputHandler = new OutputHandler();
+        CarFactory carFactory = new CarFactory();
+        RacingCarService racingCarService = new RacingCarService();
+        MainController mainController = new MainController(inputHandler, outputHandler, carFactory, racingCarService);
+        mainController.run();
+
     }
 }
