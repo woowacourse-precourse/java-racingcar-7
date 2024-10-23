b327c361c70cde1188351a9fa8b68b67040b5a45
변우영 <likevangogh24@gmail.com>
2024-10-23 22:52:02 +0900

    feat(RacingGame): 해당 라운드의 경주 결과를 반환하는 메서드 추가

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,6 +1,9 @@
 package racingcar.domain;
 
+import racingcar.dto.CarStatusDto;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingGame {
 
@@ -10,5 +13,11 @@ public class RacingGame {
     public RacingGame(List<Car> cars, int round) {
         this.racingCars = cars;
         this.round = round;
+    }
+    
+    public List<CarStatusDto> getCurrentRoundStatus() {
+        return racingCars.stream()
+                .map(car -> new CarStatusDto(car.getName(), car.getPosition()))
+                .collect(Collectors.toList());
     }
 }
