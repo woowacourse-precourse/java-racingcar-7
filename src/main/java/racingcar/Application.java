62a495595666fe0e9698e9f4ea59d77b8cb52a63
kanggyeonggu <rudrn0110@naver.com>
2024-10-23 18:26:51 +0900

    feat: 사용자 입력 검증 및 예외 처리 구현
    - 사용자로부터 자동차 이름을 쉼표(,)로 구분하여 입력받는 기능 구현
    - 입력이 빈 문자열 또는 null일 경우 `IllegalArgumentException` 예외처리 구현
    - 입력 문자열에 쉼표가 포함되지 않은 경우 `IllegalArgumentException` 예외처리 구현
    - 각 이름이 비어있거나 5자를 초과하는 경우 `IllegalArgumentException` 예외처리 구현
    
    테스트 케이스 추가 및 성공 내역:
    - [O] 빈 문자열 입력: `IllegalArgumentException` 발생
    - [O] 공백 문자열 입력: `IllegalArgumentException` 발생
    - [O] 쉼표 외의 구분자가 포함된 경우: `IllegalArgumentException` 발생
    - [O] 5자를 초과하는 이름 포함 시: `IllegalArgumentException` 발생

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,60 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+/*
+01. 사용자 입력 구현
+    - ex) "pobi,woni,jun"
+    - n명 이름 입력 받기 ➡> 구분자 검사 (쉼표 이외인 경우 예외처리 발생)
+    - 유효한 구분자의 경우, n명 이름 분할 ➡> 이름 5자 이하 검사 (5자 초과인 경우 예외처리 발생)
+*/
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+        try {
+            isValidInput(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println("입력 오류 : " + e.getMessage());
+            return;
+        }
+
+        Console.close();
+    }
+
+    /**
+     * 사용자 입력의 유효성을 검사합니다.
+     *
+     * @param input 사용자로부터 입력된 문자열
+     * @throws IllegalArgumentException 입력이 유효하지 않은 경우 발생
+     */
+    public static void isValidInput(final String input) {
+        // 1. 입력이 빈 문자열 또는 null 인 경우
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("유효한 입력이 아닙니다."); // 예외 메시지
+        }
+
+        // 2. 쉼표로 구분되지 않은 경우
+        if (!input.contains(",")) {
+            throw new IllegalArgumentException("이름은 쉼표로 구분되어야 합니다."); // 예외 메시지
+        }
+
+        // 3. 사용자 이름이 비어있거나 5자 이상인 경우
+        List<String> usernames = new ArrayList<>(Arrays.asList(input.split(",")));
+        for (String username : usernames) {
+            if (username.isEmpty()) {
+                throw new IllegalArgumentException("이름은 공백일 수 없습니다."); // 예외 메시지
+            }
+            if (username.length() > 5) {
+                throw new IllegalArgumentException("이름은 5자 이내이어야 합니다."); // 예외 메시지
+            }
+        }
     }
 }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -9,26 +9,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
+
 
     @Test
-    void 기능_테스트() {
-        assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
-        );
-    }
+    void is_vaild_input() {
+        // 빈 문자열의 경우 예외 처리
+        assertThatThrownBy(() -> Application.isValidInput("")).isInstanceOf(IllegalArgumentException.class);
+        // 공백 문자열의 경우 예외 처리
+        assertThatThrownBy(() -> Application.isValidInput(" ")).isInstanceOf(IllegalArgumentException.class);
+        // (,) 쉼표 이외 구분자의 경우 예외 처리
+        assertThatThrownBy(() -> Application.isValidInput("pobi, nabi; helo")).isInstanceOf(IllegalArgumentException.class);
+        // 이름이 5자 초과인 경우 예외 처리
+        assertThatThrownBy(() -> Application.isValidInput("pobi, nabi, javaji")).isInstanceOf(IllegalArgumentException.class);
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
     }
 
     @Override
