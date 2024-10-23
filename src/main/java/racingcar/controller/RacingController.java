1d0a37ee96ac1a5ce44ef9c388577ecef3899737
Chanwoo yang <moomsss@naver.com>
2024-10-23 20:35:29 +0900

    feat: 입력받은 횟수에 맞게 게임 루프 실행기능 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,10 +1,14 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.inputview.InputHandler;
+import racingcar.model.Car;
 import racingcar.outputview.OutputHandler;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class RacingController {
     InputHandler inputHandler;
@@ -20,6 +24,32 @@ public class RacingController {
         outputHandler.promptAttemptCountInput();
         int attemptGameCount = inputHandler.getAttemptGameCount();
 
+        List<Car> carList = new ArrayList<>();
+        for(String car: carNames){
+            carList.add(new Car(car));
+        }
+
+        List<Car> result = gameStart(carList, attemptGameCount);
+
+        for(Car car : result){
+            System.out.println(car.getPosition());
+        }
+
+
+
+    }
+
+    private List<Car> gameStart(List<Car> carList , int gameCount){
+        int currentCount = 0;
+        while(currentCount < gameCount){
+            for(Car car : carList){
+                if(Randoms.pickNumberInRange(0,9) >= 4){
+                    car.moveFoward();
+                }
+            }
+            currentCount++;
+        }
+        return carList;
     }
 
 
