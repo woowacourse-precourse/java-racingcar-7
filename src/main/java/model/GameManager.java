dc13620cbc66c5c2d6c64a899c8f317af7436ca0
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-10-24 01:06:52 +0900

    feat($model) : Provide progress of RacingGame
    내용
    - raceOnce : 레이싱 1번을 진행하는 함수 제작
    - raceGameManage : 주어진 횟수만큼 레이싱 진행

--- src/main/java/model/GameManager.java
+++ src/main/java/model/GameManager.java
@@ -1,17 +1,37 @@
 package model;
 
+import dto.CarDTO;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameManager {
     private List<Car> carList;
     private int runTimes;
 
-    public GameManager(List<String> cars, int runTimes) {
+    public GameManager(List<String> carNameList, int runTimes) {
         this.runTimes = runTimes;
-        for (String carName : cars) {
+        for (String carName : carNameList) {
             this.carList.add(new Car(carName));
         }
     }
 
+    
+    //한번의 레이싱
+    private List<CarDTO> raceOnce() {
+        List<CarDTO> carDTOList = new ArrayList<>();
+        for (Car car : this.carList) {
+            car.goOrNot();
+            carDTOList.add(new CarDTO(car.getName(), car.getDistance()));
+        }
+        return carDTOList;
+    }
 
+    // 레이싱 진행
+    public List<List<CarDTO>> raceGameManage() {
+        List<List<CarDTO>> racingResult = new ArrayList<>();
+        for (int i = 0; i < this.runTimes; i++) {
+            racingResult.add(this.raceOnce());
+        }
+        return racingResult;
+    }
 }
