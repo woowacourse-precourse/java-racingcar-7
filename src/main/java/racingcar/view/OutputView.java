ec07dfa3a3fc2e94c4e4203c572f876b582fa1b7
DoomchitYJ <poj1004@inha.edu>
2024-10-23 17:58:42 +0900

    feat: 최종 우승자를 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,14 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
 
     private static final String RACE_START_VIEW = "실행 결과";
     private static final String NAME_DELIMITER = " : ";
     private static final String WIN_INDICATOR = "-";
+    private static final String WINNER_VIEW = "최종 우승자 : ";
+    private static final String WINNERS_DELIMITER = ", ";
 
     public static void showRaceStart() {
         System.out.println("Race Start");
@@ -18,5 +22,11 @@ public class OutputView {
 
     public static void showNewLine() {
         System.out.println();
+    }
+
+    public static void showWinner(List<String> winners) {
+        String result = String.join(WINNERS_DELIMITER, winners);
+
+        System.out.println(WINNER_VIEW + result);
     }
 }
