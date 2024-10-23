096309367bedd4ac5384116ee55d0e01178dc1f4
MinseokGo <rhalstjr1999@naver.com>
2024-10-23 16:19:17 +0900

    docs: 수정 요구 사항 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,6 +10,10 @@ public class Application {
         // 사용자에게 경주할 자동차 이름을 입력 받는다.
         final String carNamesInput = Console.readLine();
 
+        /*
+          수정 대상
+          컬렉션을 사용하고, Immutable 하게 만든다.
+         */
         // 입력 받은 자동차 이름 문자열을 `쉼표(,)` 기준으로 분리한다.
         final String[] carNames = carNamesInput.split(",");
     }
