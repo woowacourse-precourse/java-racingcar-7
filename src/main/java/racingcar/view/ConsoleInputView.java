a9ee04fd1a063b33420337248cdd5a3d79ff5896
choiminu <alsdn4590@gmail.com>
2024-10-23 17:10:39 +0900

    feat(view): 사용자에게 문자열을 입력받는 기능 추가
    사용자에게 자동차의 이름과 게임 횟수를 입력받기 위해 `Console.readLine()` 메서드를 사용하여 문자열을 입력받고, 입력받은 문자열을 반환하는 기능을 추가하였습니다.

---
+++ src/main/java/racingcar/view/ConsoleInputView.java
@@ -0,0 +1,9 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ConsoleInputView {
+    private String getUserInput() {
+        return Console.readLine();
+    }
+}
