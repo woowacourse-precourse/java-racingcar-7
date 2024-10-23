6d1c5b1d3d443e56ebf23d5f9a295260a8487acc
JunBe <wnsqowid0901@gmail.com>
2024-10-23 21:52:37 +0900

    feat: 한 게임 후 전진 상태 출력

--- src/main/java/racingcar/io/Output.java
+++ src/main/java/racingcar/io/Output.java
@@ -6,7 +6,13 @@ public class Output {
     //실행 결과 출력을 여기다 추가해야 하나?
 
     public void gameResult(Map<String, Integer> carMap) {
-
+        for (String key : carMap.keySet()) {
+            System.out.print(key + " : ");
+            for (int i = 0; i < carMap.get(key); i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
     }
 
     public void winner() {
