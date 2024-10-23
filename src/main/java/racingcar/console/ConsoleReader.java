a7d29f1dc9ed53fbbec73b7f3be7cadd312ce3d8
ufo9430 <ufo9430@naver.com>
2024-10-22 23:38:12 -0400

    refactor: ConsoleReader의 readName 이름 수정

--- src/main/java/racingcar/console/ConsoleReader.java
+++ src/main/java/racingcar/console/ConsoleReader.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class ConsoleReader {
 
-    public static String read(String input){
+    public static String readName(String input){
         return Validator.validate(input);
     }
 
--- src/test/java/racingcar/console/ConsoleReaderTest.java
+++ src/test/java/racingcar/console/ConsoleReaderTest.java
@@ -7,17 +7,17 @@ class ConsoleReaderTest {
     @Test
     void 구분자_사이_문자가_없는지_검증한다(){
         Assertions.assertThrows(IllegalArgumentException.class, () ->
-                ConsoleReader.read("abc,,umu"));
+                ConsoleReader.readName("abc,,umu"));
     }
     @Test
     void 중복되는_이름이_있는지_검증한다(){
         Assertions.assertThrows(IllegalArgumentException.class, ()->
-                ConsoleReader.read("abc,abc"));
+                ConsoleReader.readName("abc,abc"));
     }
 
     @Test
     void 이름이_5자_이상인지_검증한다(){
         Assertions.assertThrows(IllegalArgumentException.class, ()->
-                ConsoleReader.read("abcdefg"));
+                ConsoleReader.readName("abcdefg"));
     }
 }
