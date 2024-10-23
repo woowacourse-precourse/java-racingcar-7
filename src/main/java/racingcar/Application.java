694f52d069e2a87072e154b7764ee010c5d37b6b
Seo Jinmann <sjmmics@gmail.com>
2024-10-23 16:38:28 +0900

    feat : ($Application, $CarController) 자동차 전진을 관리하는 클래스 추가
    inputView로부터 입력을 받아서 Car, Cars 객체를 주어진 시도 횟수만큼 전진(또는 멈출) 메소드를 호출하며 OutView에 중간 결과를 전달한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,13 +2,14 @@ package racingcar;
 
 import racingcar.controller.CarController;
 import racingcar.view.ConsoleInputView;
+import racingcar.view.ConsoleOutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         CarController controller =
-                new CarController(new ConsoleInputView());
+                new CarController(new ConsoleInputView(), new ConsoleOutputView());
         controller.run();
     }
 }
--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -1,6 +1,12 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
+import racingcar.model.CarRacingProgress;
+import racingcar.model.Cars;
+import racingcar.model.Winner;
+import racingcar.strategy.RandomMoveStrategy;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 /**
  * 전체적인 진행을 맡는 클래스
@@ -10,12 +16,31 @@ public class CarController {
 
     private final InputView inputView;
 
-    public CarController(InputView inputView) {
+    private final OutputView outputView;
+
+    public CarController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
+        this.outputView = outputView;
     }
 
     public void run() {
         String carNames = inputView.getCarNames();
         int chanceToMove = inputView.getChanceToMove();
+        Cars cars = Cars.getOfNamesAndStrategy(carNames, new RandomMoveStrategy());
+        raceCarsAndTimes(cars, chanceToMove);
+        Winner winner = Winner.getFromCars(cars);
+        outputView.printRaceResult(winner);
+    }
+
+    private void raceCarsAndTimes(Cars cars, int chanceToMove) {
+        outputView.printStartRace();
+        for (int i = 0; i < chanceToMove; i++) {
+            for (Car car : cars) {
+                car.moveOrNot();
+                CarRacingProgress progress = new CarRacingProgress(car);
+                outputView.printProgress(progress);
+            }
+            outputView.printLineBreaking();
+        }
     }
 }
