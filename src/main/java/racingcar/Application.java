d249c3958211cb768a965086d64f0502e11ab784
7i.w__k <tizmfns1218@Naver.com>
2024-10-23 15:15:30 +0900

    feat: add errormessage class

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import racingcar.controller.CarController;
+import racingcar.entity.Input;
+import racingcar.entity.TryCount;
+import racingcar.service.CarService;
+import racingcar.service.impl.CarServiceImpl;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarService carService = new CarServiceImpl();
+        CarController carController = new CarController(carService);
+
+        Input input = carController.toInput();
+        TryCount tryCount = carController.toTryCount();
+
+        carController.process(input, tryCount);
     }
 }
---
+++ src/main/java/racingcar/utils/ErrorMessage.java
@@ -0,0 +1,17 @@
+package racingcar.utils;
+
+public enum ErrorMessage {
+    USE_NUMBER("숫자만 가능합니다."),
+    INVALID_CAR_NAME("사용 불가능한 차 이름입니다. 5자 이하만 가능합니다."),
+    INVALID_COUNT("0이거나 자연수만 가능합니다.");
+
+    final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
