1bbf5ed8457c730930c8a05f0fd54634a0bbc5fc
chaerish <ccyy8432@naver.com>
2024-10-23 23:00:55 +0900

    feat(InputView): 사용자 입력위해 InputView 생성
    사용자의 입력을 받는 InputView를 생성한다.

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private final int maxLen = 5;
+    public void getInput(){
+        String carNames = Console.readLine();
+        String tryNums = Console.readLine();
+    }
+    public boolean isValidateCarName(String input){
+        return true;
+    }
+    public boolean isValidateTryNum(String input){
+        return true;
+    }
+
+}
