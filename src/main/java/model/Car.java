2c3090f5d4d6dc7e9eacad7877a516f40254fe9b
Wooseok Jang <wowseok2774@gmail.com>
2024-10-23 19:11:13 +0900

    feat: 현재 자동차의 위치 상태를 반환하는 getPositionStatus 함수 추가

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -15,16 +15,27 @@ public class Car {
         return name;
     }
 
+    public String getPositionStatus() {
+        return name + " : " + "-".repeat(position);  // 위치에 따라 '-' 반복
+    }
+
     public int getPosition() {
         return position;
     }
 
-    public void move(String name) {
-        position++;
+    public void move(int randomValue) {
+        if (randomValue >= 4) {
+            position++;
+        }
     }
 
     public void stop() {
         // 현재 아무 동작도 하지 않음
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 
 }
