471ee910fd3a9fe7dff2c36c06cf71e6f64dd852
ohhyungsuh <ohhyungsuh@gmail.com>
2024-10-23 23:34:53 +0900

    test(레이싱 카 이동): 랜덤 숫자에 따른 레이싱 카 이동여부
    - 숫자 0이상 4미만일 때 이동 및 출력 테스트
    - 숫자 4이상 10미만일 때 이동 및 출력 테스트

--- src/test/java/racingcar/service/RacingCarServiceImplTest.java
+++ src/test/java/racingcar/service/RacingCarServiceImplTest.java
@@ -67,5 +67,45 @@ class RacingCarServiceImplTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    @DisplayName("숫자_4미만_자동차_이동_테스트")
+    void 숫자_4미만_자동차_이동_테스트() {
+        Car car = new Car("woowa");
 
+        car.move(3);
+        assertThat(car)
+                .extracting(Car::getName, Car::getMoveCount)
+                .containsExactly("woowa", 0);
+    }
+
+    @Test
+    @DisplayName("숫자_4미만_자동차_이동_출력_테스트")
+    void 숫자_4미만_자동차_이동_출력_테스트() {
+        Car car = new Car("woowa");
+
+        car.move(2);
+        assertThat(car.toString()).isEqualTo("woowa : ");
+    }
+
+    @Test
+    @DisplayName("숫자_4이상_자동차_이동_테스트")
+    void 숫자_4이상_자동차_이동_테스트() {
+        Car car = new Car("woowa");
+
+        car.move(4);
+        car.move(0);
+        assertThat(car)
+                .extracting(Car::getName, Car::getMoveCount)
+                .containsExactly("woowa", 1);
+    }
+
+    @Test
+    @DisplayName("숫자_4이상_자동차_이동_출력_테스트")
+    void 숫자_4이상_자동차_이동_출력_테스트() {
+        Car car = new Car("woowa");
+
+        car.move(5);
+        car.move(9);
+        assertThat(car.toString()).isEqualTo("woowa : --");
+    }
 }
