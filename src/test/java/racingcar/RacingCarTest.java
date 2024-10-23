92021537e098a1ecabec12c057899b90eb56aea8
jseom <2ujs4311@gmail.com>
2024-10-23 15:43:42 +0900

    TEST: 이름이 5자를 초과하면 에러 발생시키는지 확인하는 테스트 코드 추가
    1~5자까지는 이름을 허용하고 6자부터 에러를 발생시키는지 테스트하는 코드를 추가했습니다.

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -10,6 +10,9 @@ class RacingCarTest {
   @Test
   void 자동차이름_쉼표기준으로_나누기() {
       assertEquals(List.of("pobi","woni","jun"), RacingCar.makeNameList("pobi,woni,jun"));
+      assertEquals(List.of("abcde","woni","jun"), RacingCar.makeNameList("abcde,woni,jun"));
+      assertEquals(List.of("가나다라마","woni","jun"), RacingCar.makeNameList("가나다라마,woni,jun"));
+      assertEquals(List.of("12345","woni","jun"), RacingCar.makeNameList("12345,woni,jun"));
       assertEquals(List.of("월","woni","jun"), RacingCar.makeNameList("월,woni,jun"));
       assertEquals(List.of(";=;","월","jun"), RacingCar.makeNameList(";=;,월,jun"));
   }
@@ -17,8 +20,18 @@ class RacingCarTest {
   @DisplayName(",를 기준으로 구분했을 때 빈문자열이 있으면 오류")
   @Test
   void 자동차이름_잘못된_입력() {
+      assertThrows(IllegalArgumentException.class, ()  -> RacingCar.makeNameList(""));
       assertThrows(IllegalArgumentException.class, ()  -> RacingCar.makeNameList("pobi,,woni,jun"));
       assertThrows(IllegalArgumentException.class, ()  -> RacingCar.makeNameList(",pobi,woni,jun"));
       assertThrows(IllegalArgumentException.class, ()  -> RacingCar.makeNameList("pobi,woni,jun,"));
   }
+
+  @Test
+  void 자동차이름_5자_제한_확인() {
+      assertThrows(IllegalArgumentException.class, () -> RacingCar.makeNameList("abcdef,pobi,jun"));
+      assertThrows(IllegalArgumentException.class, () -> RacingCar.makeNameList("가나다라마바,pobi,jun"));
+      assertThrows(IllegalArgumentException.class, () -> RacingCar.makeNameList("123456,pobi,jun"));
+  }
+
+
 }
