1f1f10ae6a195e685f19e5e148f6dd0a0e2089de
Geonwoo Pack <packdev937@gmail.com>
2024-10-23 15:11:24 +0900

    test (car) : 자동차 전지 로직에 대한 failing test를 작성한다

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Car 클래스 테스트")
+public class CarTest {
+
+    @Test
+    void 무작위_값이_4_이상이라면_전진한다() {
+        // given
+        Car car = new Car("pobi");
+
+        // when
+        car.move(4);
+
+        // then
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+}
