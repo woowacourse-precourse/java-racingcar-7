af857fb2aa7a2878854aa3b53784858d36ca8567
김성민 <klg1425@gmail.com>
2024-10-23 22:23:15 +0900

    feat: 자동차 경주를 위한 자동차 이름 입력 받기
    CarInputHandler 클래스 생성후 안에userInputCarNames(질문 후 입력 받는 메서드), splitCarNames(쉼표(",")를 기준으로 자동차 이름 분리) 메서드 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CarInputHandler carInputHandler = new CarInputHandler();
+        String inputCarNames = carInputHandler.userInputCarNames();
+        String[] separatedCarNames = carInputHandler.splitCarNames(inputCarNames);
+        List<String> carNameList = new ArrayList<>(Arrays.asList(separatedCarNames));
+        // 출력 TEST 출력 메서드 따로 생성후 삭제 예정
+        for (String name : carNameList) {
+            System.out.println(name);
+        }
     }
 }
