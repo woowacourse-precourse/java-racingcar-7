2e2da1d46615d9ef2a336e6ef8a46fc4372c16f0
chaerish <ccyy8432@naver.com>
2024-10-24 01:00:50 +0900

    test(InputViewTest): 시도 횟수 입력값 숫자인지 검사 테스트 생성
    사용자의 시도 횟수 입력값이 숫자값인지 성공, 실패 유효성 테스트를 수행한다.

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,25 +1,71 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class InputView {
-    private final int maxLen = 5;
-    public void getInput(){
+    private final static int MAX_LENGTH = 5;
+    private final static String DELIM = ",";
+    final static List<String> userNames = new ArrayList<>();
+
+    public CarDTO getInput() {
         String carNames = Console.readLine();
         String tryNums = Console.readLine();
+        StringTokenizer stringTokenizer = new StringTokenizer(carNames);
+
+        if (!tryNums.isBlank()) {
+            throw new IllegalArgumentException("숫자 공백 입력 값은 허용하지 않습니다.");
+        }
+        while (stringTokenizer.hasMoreTokens()) {
+            String name = stringTokenizer.nextToken(DELIM);
+            if (!isMoreThanFive(name)) {
+                throw new IllegalArgumentException("이름이 5자 이상일 수 없습니다.");
+            }
+            if (isDuplicate(carNames)) {
+                throw new IllegalArgumentException("이름이 중복되선 안 됩니다.");
+            }
+            userNames.add(name);
+        }
+
+        return new CarDTO(userNames, Long.parseLong(tryNums));
 
     }
-    public boolean isValidateCarName(String input){
-        if(input.isBlank()){
+
+    public boolean isContainComma(String input) {
+        if (!input.contains(DELIM)) {
             return false;
         }
         return true;
     }
-    public boolean isValidateTryNum(String input){
-        if(input.isBlank()){
-           return false;
+
+    public boolean isBlank(String input) {
+        if (!input.isBlank()) {
+            return false;
         }
         return true;
     }
+
+    public boolean isDuplicate(String carName) {
+        return userNames.contains(carName);
+    }
+
+    public boolean isMoreThanFive(String carName) {
+        if (carName.length() > MAX_LENGTH) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isNumeric(String tryNums) {
+        for (char num : tryNums.toCharArray()) {
+            if (!Character.isDigit(num)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
