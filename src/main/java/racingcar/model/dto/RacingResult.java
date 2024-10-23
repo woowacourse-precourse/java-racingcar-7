d5519f09e82890857c7499a84cd0517a97d0c2cc
immyeong <wpfls6670@naver.com>
2024-10-23 11:34:01 +0900

    feat: 횟수 당 결과를 담는 객체 생성

---
+++ src/main/java/racingcar/model/dto/RacingResult.java
@@ -0,0 +1,11 @@
+package racingcar.model.dto;
+
+import java.util.List;
+
+public class RacingResult {
+    private List<Car> cars;
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
