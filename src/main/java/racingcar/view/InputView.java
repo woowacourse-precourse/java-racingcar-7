df1239cd37bcb41982103892ee2c667890cbee13
dongyun <ehddbs452100@naver.com>
2024-10-23 20:48:18 +0900

    fix(InputView): print message contents
    - Message Enum 클래스의 변수들 자체가 아닌 변수들의 값을 출력

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,18 +1,18 @@
 package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
-
-import racingcar.global.Message;
+import static racingcar.global.Message.ENTER_CAR_NAMES;
+import static racingcar.global.Message.ENTER_TRY_COUNT;
 
 public class InputView {
 
     public String getCarName() {
-        System.out.println(Message.ENTER_CAR_NAMES);
+        System.out.println(ENTER_CAR_NAMES.getMessage());
         return readLine();
     }
 
     public int getTryCounts() {
-        System.out.println(Message.ENTER_TRY_COUNT);
+        System.out.println(ENTER_TRY_COUNT.getMessage());
         return Integer.parseInt(readLine());
     }
 }
