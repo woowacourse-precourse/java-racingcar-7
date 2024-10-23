a5001f65dbb734ac198571eb8af561834734d75e
NaMinhyeok <nmh9097@gmail.com>
2024-10-23 20:59:51 +0900

    refactor: Cars 생성 로직 InputHandler로 이관

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -5,7 +5,6 @@ import racingcar.io.OutputHandler;
 import racingcar.model.Car;
 import racingcar.model.Cars;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class RacingGame {
@@ -19,9 +18,7 @@ public class RacingGame {
     }
 
     public void run() {
-        String namesOfCars = getNamesOfCarsFromUser();
-        Cars cars = generateCarsFrom(namesOfCars);
-        
+        Cars cars = getCarsFromUser();
         int tryCount = getTryCountFromUser();
 
         executeRaceRoundBy(tryCount, cars);
@@ -30,23 +27,7 @@ public class RacingGame {
         outputHandler.showWinners(winnerCars);
     }
 
-    private void executeRaceRoundBy(int tryCount, Cars cars) {
-        outputHandler.showResultComment();
-        
-        for (int i = 0; i < tryCount; i++) {
-            List<Car> movedCars = cars.move();
-            outputHandler.showCarsPosition(movedCars);
-        }
-    }
-
-    private Cars generateCarsFrom(String namesOfCars) {
-        String[] split = namesOfCars.split(",");
-        return Cars.from(Arrays.stream(split)
-            .map(Car::from)
-            .toList());
-    }
-
-    private String getNamesOfCarsFromUser() {
+    private Cars getCarsFromUser() {
         outputHandler.showGameStartComment();
         return inputHandler.getCarNamesFromUser();
     }
@@ -54,6 +35,15 @@ public class RacingGame {
     private int getTryCountFromUser() {
         outputHandler.showTryCountComment();
         return inputHandler.getTryCountFromUser().tryCount();
+    }
+
+    private void executeRaceRoundBy(int tryCount, Cars cars) {
+        outputHandler.showResultComment();
+
+        for (int i = 0; i < tryCount; i++) {
+            List<Car> movedCars = cars.move();
+            outputHandler.showCarsPosition(movedCars);
+        }
     }
 
 }
--- src/main/java/racingcar/io/InputHandler.java
+++ src/main/java/racingcar/io/InputHandler.java
@@ -2,7 +2,10 @@ package racingcar.io;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.io.request.TryCountRequest;
+import racingcar.model.Car;
+import racingcar.model.Cars;
 
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class InputHandler {
@@ -10,25 +13,31 @@ public class InputHandler {
     private static final Pattern CAR_NAME_PATTERN = Pattern.compile("^[^\\s]+(,[^\\s])*$");
 
 
-    public String getCarNamesFromUser() {
+    public Cars getCarNamesFromUser() {
         String userInput = Console.readLine();
-        validateCarNames(userInput);
-        return userInput;
+        return generateCarsFrom(userInput);
     }
 
-    public boolean validateCarNames(String userInput) {
+    public TryCountRequest getTryCountFromUser() {
+        String userInput = Console.readLine();
+        return new TryCountRequest(userInput);
+    }
+
+    private Cars generateCarsFrom(String namesOfCars) {
+        validateCarNames(namesOfCars);
+        String[] split = namesOfCars.split(",");
+        return Cars.from(Arrays.stream(split)
+            .map(Car::from)
+            .toList());
+    }
+
+    private void validateCarNames(String userInput) {
         if (userInput == null || userInput.isBlank()) {
             throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
         }
         if (!CAR_NAME_PATTERN.matcher(userInput).matches()) {
             throw new IllegalArgumentException("자동차 이름과 쉼표만 입력해주세요. 공백은 사용 할 수 없습니다.");
         }
-        return true;
-    }
-
-    public TryCountRequest getTryCountFromUser() {
-        String userInput = Console.readLine();
-        return new TryCountRequest(userInput);
     }
 
 }
