8ebc88a3a563685539b1040990cd3e2779ae6c7b
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-10-24 00:24:05 +0900

    feat(RacingCarApp): 시도횟수 입력받기 기능 작성
    - getAttemptCount() 작성
    - 잘못된 값 입력시 IllegalArgumentException 반환

--- src/main/java/racingcar/RacingCarApp.java
+++ src/main/java/racingcar/RacingCarApp.java
@@ -7,4 +7,12 @@ public class RacingCarApp {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
+    public int getAttemptCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        try{
+            return Integer.parseInt(Console.readLine());
+        }catch (Exception e){ // 정수 변환 실패시
+            throw new IllegalArgumentException();
+        }
+    }
 }
