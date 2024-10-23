77c1356be32a164d08654086696937ccbb5a5f31
박철현 <park82602@gmail.com>
2024-10-23 09:57:51 +0900

    Feat : 우승자 출력을 위한 기능 추가

--- src/main/java/racingcar/CarManager.java
+++ src/main/java/racingcar/CarManager.java
@@ -4,38 +4,48 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 public class CarManager {
-
+    private static final String DELIMITER = ",";
     private final List<Car> list;
 
     public CarManager() {
         this.list = new ArrayList<>();
     }
+
     public void add(String carNames) {
-        Arrays.stream(carNames.split(","))
+        Arrays.stream(carNames.split(DELIMITER))
                 .map(Validator::validateNameLength)
                 .map(Car::generateCars)
                 .forEach(list::add);
     }
 
-    public List<Car> moveCars(int count) {
-        List<Car> resultList = getList();
-        IntStream.range(0, count).forEach(i -> {
-            resultList.forEach(
-                    car -> {
-                        int moveNum = Randoms.pickNumberInRange(0, 9);
-                        car.move(moveNum);
-                        Printer.print(car);
-                    });
+    public void moveCars(int count) {
+        for (int i = 0; i < count; i++) {
+            for (Car car : this.getList()) {
+                int randomNum = Randoms.pickNumberInRange(0, 9);
+                car.move(randomNum);
+                Printer.print(car);
+            }
             Printer.newLine();
-        });
-        return resultList;
+        }
     }
 
     public List<Car> getList() {
         return new ArrayList<>(list);
+    }
+
+    public List<String> getWinner() {
+        return getList().stream()
+                .filter(car -> car.getPosition() == getMaxPosition())
+                .map(Car::getName)
+                .toList();
+    }
+    private int getMaxPosition() {
+        getList().sort(Comparator.comparing(Car::getPosition).reversed());
+        return getList().getFirst().getPosition();
     }
 }
