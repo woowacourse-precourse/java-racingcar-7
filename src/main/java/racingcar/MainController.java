aecb284980e303c6e3b734a1560f5ead31066f24
hyojun <hjlim45678@gmail.com>
2024-10-23 17:25:43 +0900

    refactor: 기능 단위로 코드 매서드화하여 묶음

--- src/main/java/racingcar/MainController.java
+++ src/main/java/racingcar/MainController.java
@@ -1,9 +1,7 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import racingcar.view.InputView;
 
@@ -12,18 +10,22 @@ public class MainController {
         List<Car> cars = InputView.readCarNames();
         int inputTryCount = InputView.readTryCount();
 
-        for (int i = 0; i < inputTryCount; i++) {
-            for (Car car : cars) {
-                car.randomlyMove(if60PercentChance());
-            }
-        }
+        racingStart(inputTryCount, cars);
 
-        System.out.println("실행 결과");
-        for (Car car : cars) {
-            String carResult = car.getName() + " : " +"-".repeat(car.getPosition());
-            System.out.println(carResult);
-        }
+        printTryResult(cars);
 
+        List<String> winners = getWinners(cars);
+
+        printWinnerResult(winners);
+
+    }
+
+    private static void printWinnerResult(List<String> winners) {
+        String winnerResult = "최종 우승자" + " : " + String.join(", ", winners);
+        System.out.println(winnerResult);
+    }
+
+    private static List<String> getWinners(List<Car> cars) {
         int maxPosition = cars.get(0).getPosition();
         for (int i = 0; i < cars.size(); i++) {
             int comparedPosition = cars.get(i).getPosition();
@@ -38,12 +40,24 @@ public class MainController {
                 winners.add(car.getName());
             }
         }
-
-        String winnerResult = "최종 우승자" + " : " + String.join(", ", winners);
-        System.out.println(winnerResult);
-
+        return winners;
     }
 
+    private static void printTryResult(List<Car> cars) {
+        System.out.println("실행 결과");
+        for (Car car : cars) {
+            String carResult = car.getName() + " : " +"-".repeat(car.getPosition());
+            System.out.println(carResult);
+        }
+    }
+
+    private static void racingStart(int inputTryCount, List<Car> cars) {
+        for (int i = 0; i < inputTryCount; i++) {
+            for (Car car : cars) {
+                car.randomlyMove(if60PercentChance());
+            }
+        }
+    }
 
 
     private static boolean if60PercentChance() {
