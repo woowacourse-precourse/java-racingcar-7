12585aab2226fbe215ddc5d4f72905c038b3d320
seaniiio <cnc4750@naver.com>
2024-10-23 18:56:01 +0900

    feat: 입력 받은 자동차 이름을 기반으로 자동차 List 생성

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,7 +1,12 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class RacingController {
 
@@ -16,6 +21,8 @@ public class RacingController {
     public void run() {
         String carInput = getCarInput();
         String countInput = getCountInput();
+
+        List<Car> cars = getCars(carInput);
     }
 
     private String getCarInput() {
@@ -26,5 +33,22 @@ public class RacingController {
     private String getCountInput() {
         outputView.printCountInputMessage();
         return inputView.input();
+    }
+
+    private List<Car> getCars(String carInput) {
+        List<String> stringCars = splitCar(carInput);
+        return convertStringCarsToCars(stringCars);
+    }
+
+    private List<String> splitCar(String carInput) {
+        return new ArrayList<String>(Arrays.asList(carInput.split(",")));
+    }
+
+    private List<Car> convertStringCarsToCars(List<String> stringCars) {
+        List<Car> cars = new ArrayList<>();
+        for (String stringCar : stringCars) {
+            cars.add(new Car(stringCar));
+        }
+        return cars;
     }
 }
---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,12 @@
+package racingcar.model;
+
+public class Car {
+
+    private String name;
+    private int status;
+
+    public Car(String name) {
+        this.name = name;
+        this.status = 0;
+    }
+}
