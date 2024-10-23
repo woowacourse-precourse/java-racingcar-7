f5d4bfc3f359caa75a13ccd83e96f0ef8d025405
Hyso <hsk7953@gmail.com>
2024-10-23 23:45:30 +0900

    :sparkles: feat(Car) : 자동차 클래스 정의 - 이름과 위치를 필드로 가짐 - 4 이상의 숫자에서는 전지하는 move 메서드 구현 - 이름과 위치를 반환하는 getter 메서드 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,24 @@
+package racingcar.model;
+
+public class Car {
+    private final String name;
+    private int position;
+
+   public Car(String name){
+       this.name = name;
+   }
+
+   public void move(int number){
+       if(number >= 4){
+           position++;
+       }
+   }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
