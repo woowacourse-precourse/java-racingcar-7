088c99b3e948a0c1afd64f59442a8dfef908fe10
dev-Rhea <dev.rhea4444@gmail.com>
2024-10-23 20:28:23 +0900

    :sparkles: feat : 자동차 이름 저장, 전진 결과 저장 메서드 추가

---
+++ src/main/java/racingcar/CarInformation.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import java.util.LinkedHashMap;
+
+public class CarInformation {
+
+    public static LinkedHashMap<String, Integer> getCarInformation() {
+        String carName = Input.inputCarName();
+
+        LinkedHashMap<String, Integer> carInformation = new LinkedHashMap<>();
+        String[] names = carName.split(",");
+
+        for (String name : names) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+            }
+            if (!name.matches("^[a-zA-Z]+$")) {
+                throw new IllegalArgumentException("자동차 이름은 영문으로 입력해주세요.");
+            }
+            carInformation.put(name, 0);
+
+        }
+
+        return carInformation;
+    }
+}
