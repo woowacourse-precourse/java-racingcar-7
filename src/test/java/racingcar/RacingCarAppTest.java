e4fb3c35201a58360204f54672a09d9c5fcca465
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-10-24 01:01:14 +0900

    test(RacingCarAppTest): 자동차 이름 다섯글자 미만 입력 테스트 작성
    - 자동차 이름은 5글자 이상이 될 수 없다

--- src/test/java/racingcar/RacingCarAppTest.java
+++ src/test/java/racingcar/RacingCarAppTest.java
@@ -1,8 +1,10 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 
 class RacingCarAppTest {
@@ -14,5 +16,14 @@ class RacingCarAppTest {
         assertThat(app.getCars().size()).isEqualTo(2);
         assertThat(app.getCars().get(0).getCarName()).isEqualTo("car1");
         assertThat(app.getCars().get(1).getCarName()).isEqualTo("car2");
+    }
+
+
+    @Test
+    void 자동차_이름은_다섯글자_미만() {
+        RacingCarApp app = new RacingCarApp();
+        assertThatThrownBy(() -> {
+            app.makeCar("carcar,car2");
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
