23576593eec7786f6eb3f114a50dee9e107b3f62
JunBe <wnsqowid0901@gmail.com>
2024-10-23 21:43:15 +0900

    feat: 자동차 이름 및 게임 횟수 입력 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,46 @@
 package racingcar;
 
+import racingcar.io.Input;
+
+import java.util.*;
+
 public class Application {
+    public static final Input input = new Input();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        runFullGame();
     }
+
+    private static void runFullGame() {
+        //입력 받기
+        Map<String, Integer> carStepsMap = getCarMap();
+        int round = getRound();
+
+        //로직 실행
+
+        //우승자 출력 받기
+    }
+
+    private static Map<String, Integer> getCarMap() {
+        String[] splitCarName = input.carName().split(",");
+        Map<String, Integer> carMap = new HashMap<>();
+        for (String carName : splitCarName) {
+            carMap.put(carName, 0);
+        }
+        return carMap;
+    }
+
+    private static int getRound() {
+        int numRound;
+        try {
+            numRound = Integer.parseInt(input.gameRounds());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수 이외의 값은 입력이 불가능합니다.");
+        }
+        if (numRound < 0) {
+            throw new IllegalArgumentException("음수는 입력이 불가능합니다.");
+        }
+        return numRound;
+    }
+
 }
