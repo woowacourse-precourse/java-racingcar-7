35f4e4c9b49156f885d8c11f04416160eb777b6f
OhYeHyun <ohyhohyl@naver.com>
2024-10-23 18:10:53 +0900

    feat(createRandomValue): 0 ~ 9 사이의 랜덤 값을 추출하는 기능 추가

--- src/main/java/racingcar/RacingService.java
+++ src/main/java/racingcar/RacingService.java
@@ -1,9 +1,15 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingService {
     private final String CAR_INPUT_DELIMITER = ",";
 
     public String[] convertCarInputToArray(String input) {
         return input.split(CAR_INPUT_DELIMITER);
+    }
+
+    public int createRandomValue() {
+        return Randoms.pickNumberInRange(0, 9);
     }
 }
