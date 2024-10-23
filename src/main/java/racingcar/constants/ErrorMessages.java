f290ab35dbc94603ca82837c6e547a6fdbe43b31
binn <anyubin0724@naver.com>
2024-10-23 18:32:54 +0900

    fix: 예외처리 메시지 수정
    예외처리 메시지 추가 및 함수 이름 수정

--- src/main/java/racingcar/constants/ErrorMessages.java
+++ src/main/java/racingcar/constants/ErrorMessages.java
@@ -1,9 +1,11 @@
 package racingcar.constants;
 
 public enum ErrorMessages {
+    INVALID_DELIMITER("구분자는 쉼표만 가능합니다."),
+    NO_EXIST_TRY_NUMBER("시도 횟수를 입력해주세요."),
+    NO_EXIST_CAR_NAME("자동차의 이름을 입력해주세요."),
     INVALID_TRY_NUMBER("시도 횟수는 숫자만 입력이 가능합니다."),
-    EXCEED_CAR_NAME_LIMIT("자동차의 이름은 5자 이하만 가능합니다."),
-    INVALID_DELIMITER("구분자는 쉼표만 가능합니다.")
+    EXCEED_CAR_NAME_LIMIT("자동차의 이름은 5자 이하만 가능합니다.")
     ;
 
     private final String message;
@@ -12,7 +14,7 @@ public enum ErrorMessages {
         this.message = message;
     }
 
-    public String getErrorMessage() {
+    public String getMessage() {
         return message;
     }
 }
