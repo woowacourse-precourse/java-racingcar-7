1b21dd9cf3f4b50427a2ea7b970eda7bba0d2aed
KNU-K <knukang334@gmail.com>
2024-10-24 04:07:01 +0900

    feat(collector): 자동차 리스트 수집을 위한 Collector 추가
    - `Drivable` 인터페이스 구현체를 `CarList`로 수집하는 `CarListCollector` 구현.
    - `Collector.of`를 사용해 새로운 `CarList` 생성 및 자동차 추가 로직을 처리.
    - 두 개의 `CarList`를 병합하기 위한 `mergeCarLists` 메서드 추가.
    - 병합 과정에서 `getAllCars()`를 이용해 자동차 리스트를 결합.

---
+++ src/main/java/racingcar/collector/CarListCollector.java
@@ -0,0 +1,33 @@
+package racingcar.collector;
+
+import java.util.stream.Collector;
+import racingcar.model.CarList;
+import racingcar.model.Drivable;
+
+public class CarListCollector {
+
+    /**
+     * Drivable 인터페이스의 구현 객체를 CarList로 수집하는 Collector를 반환합니다.
+     *
+     * @return Drivable 인터페이스의 구현체를 CarList로 수집하기 위한 Collector
+     */
+    public static Collector<Drivable, ?, CarList> toCarList() {
+        return Collector.of(
+                CarList::new, // 수집기의 결과를 위한 새로운 CarList 생성
+                CarList::addCar, // 요소를 CarList에 추가하는 accumulator
+                CarListCollector::mergeCarLists // 두 개의 CarList를 병합하는 combiner
+        );
+    }
+
+    /**
+     * 두 개의 CarList를 병합합니다.
+     *
+     * @param cl1 첫 번째 CarList
+     * @param cl2 두 번째 CarList
+     * @return 병합된 CarList
+     */
+    private static CarList mergeCarLists(CarList cl1, CarList cl2) {
+        cl1.getAllCars().addAll(cl2.getAllCars());
+        return cl1;
+    }
+}
