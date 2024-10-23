308cd71783b50c8d62831d097ab8f79e57ea9695
Sy P <yongsa0221@gmail.com>
2024-10-24 00:43:20 +0900

    test: 가장 멀리 전진한 자동차를 찾는 기능 관련 테스트 추가

--- src/test/java/racingcar/domain/race/RacePositionTest.java
+++ src/test/java/racingcar/domain/race/RacePositionTest.java
@@ -2,10 +2,15 @@ package racingcar.domain.race;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import racingcar.domain.car.Car;
 import racingcar.domain.car.CarName;
+import racingcar.domain.car.CarPosition;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -24,7 +29,7 @@ class RacePositionTest {
 
     @Test
     @DisplayName("CarName에 따른 Car를 반환하는지 확인")
-    void testfindCarByCarName() {
+    void testFindCarByCarName() {
         CarName targetName = CarName.of("dummy");
         Car targetCar = new Car(targetName);
         List<Car> cars = List.of(targetCar);
@@ -33,5 +38,32 @@ class RacePositionTest {
         Car actual = racePosition.findByCarName(targetName);
 
         assertThat(actual).isEqualTo(targetCar);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Position의 값이 가장 큰 Car의 List를 반환하는지 확인")
+    @MethodSource("generateRacePosition")
+    void testFindFarthestCar(List<Car> cars, List<Car> farthestCars) {
+        RacePosition racePosition = new RacePosition(cars);
+        List<Car> actual = racePosition.findFarthestCar();
+
+        assertThat(actual).isEqualTo(farthestCars);
+    }
+
+    private static Stream<Arguments> generateRacePosition() {
+        return Stream.of(
+                Arguments.of(
+                        List.of(generateCar("a", 1), generateCar("b", 2)),
+                        List.of(generateCar("b", 2))
+                        ),
+                Arguments.of(
+                        List.of(generateCar("a", 1), generateCar("b", 1)),
+                        List.of(generateCar("a", 1), generateCar("b", 1))
+                )
+        );
+    }
+
+    private static Car generateCar(String carName, int position) {
+        return new Car(CarName.of(carName), CarPosition.of(position));
     }
 }
