c68c8f1bad33cae9bfb5b09f67440cf808b94feb
alpin87 <qortmdals94@naver.com>
2024-10-23 20:10:19 +0900

    feat: 자동차 이동 조건 검증

--- src/main/java/racingcar/RaceUtil.java
+++ src/main/java/racingcar/RaceUtil.java
@@ -4,9 +4,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RaceUtil {
     private static final String NAME_DELIMITER = ",";
+    private static final int RANDOM_START = 0;
+    private static final int RANDOM_END = 9;
+    private static final int MOVE_THRESHOLD = 4;
 
     public static String[] parseCarNames(String input) {
         return input.trim().split(NAME_DELIMITER);
+    }
+
+    public static boolean canMove(){
+        return Randoms.pickNumberInRange(RANDOM_START, RANDOM_END) >= MOVE_THRESHOLD;
     }
 }
 
