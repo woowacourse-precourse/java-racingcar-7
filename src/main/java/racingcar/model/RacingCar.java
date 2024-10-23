8f7078803ef0a4dfe2e33618bacf91d5a086741c
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-23 16:00:11 +0900

    feat($RacingCar): 자동차 이름에 대한 예외 처리 구현
    - 자동차 이름이 5자를 초과하는 경우.
    - 자동차 이름이 공백인 경우.
    - IllegalArgumentException 발생.

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -5,8 +5,10 @@ public class RacingCar {
     private final String name;
     private final int position;
     private final int DEFAULT_POSITION = 0;
+    private final int MAX_NAME_LENGTH = 5;
 
     public RacingCar(String name) {
+        validateName(name);
         this.name = name;
         this.position = DEFAULT_POSITION;
     }
@@ -17,5 +19,14 @@ public class RacingCar {
 
     public int getPosition() {
         return position;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
+        }
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름은 공백일 수 없습니다.");
+        }
     }
 }
