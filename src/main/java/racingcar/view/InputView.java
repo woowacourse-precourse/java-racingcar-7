2084746dd452abb2806c5fd0f108a393317c32e7
DoomchitYJ <poj1004@inha.edu>
2024-10-23 16:03:24 +0900

    fix: 자동차 이름을 입력받을 때 출력내용의 변수명 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,10 +3,10 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String INPUT_VIEW = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String CAR_NAMES_INPUT_VIEW = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
     public static String readCarNames() {
-        System.out.println(INPUT_VIEW);
+        System.out.println(CAR_NAMES_INPUT_VIEW);
         String input = Console.readLine().trim();
         Console.close();
 
