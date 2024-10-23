effae4a5634e97eb56125bad7c882e90f0a7d3be
bowook <mbosx316@naver.com>
2024-10-23 19:34:36 +0900

    fix(InputView) : Console 자원 관리 개선
    입력 후 Console.close()를 호출하여 자원을 관리하도록 수정했습니다.
    - 자동차 이름 입력 및 시도 횟수 입력 후 Console을 닫습니다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,11 +8,15 @@ public class InputView {
     private final static String COMMA = ",";
     public List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return List.of(Console.readLine().split(COMMA));
+        String carNames = Console.readLine();
+        Console.close();
+        return List.of(carNames.split(COMMA));
     }
 
     public String inputTrialCounts() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        return Console.readLine();
+        String trialCounts = Console.readLine();
+        Console.close();
+        return trialCounts;
     }
 }
