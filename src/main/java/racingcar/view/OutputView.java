90c9e9c6c5a263126ca255a63b1171882b42f651
koomchang <leahpar0401@gmail.com>
2024-10-23 17:21:15 +0900

    feat: 출력을 담당하는 OutputView 클래스 생성

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,14 @@
+package racingcar.view;
+
+public class OutputView {
+    private static final String ASK_FOR_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ASK_FOR_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+
+    public static void askForCarNames() {
+        System.out.println(ASK_FOR_CAR_NAMES);
+    }
+
+    public static void askForTryCount() {
+        System.out.println(ASK_FOR_TRY_COUNT);
+    }
+}
