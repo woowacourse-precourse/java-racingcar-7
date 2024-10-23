15a322c98b98f4b1da5195efa789f20ff3122640
Yoon-Jemin <yjm1800@naver.com>
2024-10-24 01:04:22 +0900

    feat(controller): 사용자의 입력을 서비스 계층에 전달
    - 사용자가 입력한 이름을 서비스로 넘겨주는 로직 추가
    - 게임을 시작하기 위한 startGame 메서드 추가
    - 서비스로부터 우승자들을 조회하는 로직 추가

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -5,21 +5,39 @@ import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CarController {
 
     private final OutputView outputView;
     private final InputView inputView;
+    private final CarService carService;
 
     public CarController() {
         this.outputView = new OutputView();
         this.inputView = new InputView();
+        this.carService = new CarService();
     }
 
     public void run() {
         outputView.printStartMessage();
         ArrayList<String> carNames = inputView.readCarNames();
+
+        for (String carName : carNames) {
+            carService.addParticipant(carName);
+        }
+
         outputView.printAttemptMessage();
         int attempts = inputView.readAttempts();
+
+        startGame(attempts);
+        List<String> winners = carService.getWinners();
+    }
+
+    private void startGame(int attempts) {
+        for (int i = 0; i < attempts; i++) {
+            carService.advanceCars();
+        }
+        carService.updateWinners();
     }
 }
