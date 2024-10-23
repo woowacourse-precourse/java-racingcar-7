83ddfd421273172b574e259e25a7f805ae9bbbc3
bowook <mbosx316@naver.com>
2024-10-23 21:44:58 +0900

    refactor(InputView) : close 기능 분리
    > InputView에 각 메소드에서 실행하던 Console.close()를 따로 분리하였습니다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,15 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public static String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = Console.readLine();
-        Console.close();
-        return carNames;
+        return Console.readLine();
     }
 
     public static String inputTrialCounts() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        String trialCounts = Console.readLine();
+        return Console.readLine();
+    }
+
+    public static void closeConsole() {
         Console.close();
-        return trialCounts;
     }
 }
