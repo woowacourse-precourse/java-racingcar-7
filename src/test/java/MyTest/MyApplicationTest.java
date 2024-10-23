b1a80ed8db2946e7b6578a58cf9da851e32698fc
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-10-24 04:31:37 +0900

    test(MyApplicationTest): 테스트 추가
    - 하나의 메서드에 2개 이상의 테스트가 들어간 경우 테스트를 분리했습니다.
      하나의 메서드에는 1개의 테스트만 진행하는 것이 나중에 테스트 결과
      확인시에도 분별할 수 있을 것이라고 판단했습니다.
    
    - 자동차 이름에 null값과 빈칸 사용시 예외가 발생하는지 테스트를
      작성하였습니다.
    
    - 자동차를 1대만 입력할 경우 예외가 발생하는 지 테스트 하였습니다.
      레이싱 경주 특성상 1대만 참가를 할 수 없기 때문에 2 대 이상을 입력해야
      된다고 생각했습니다.

--- src/test/java/MyTest/MyApplicationTest.java
+++ src/test/java/MyTest/MyApplicationTest.java
@@ -1,69 +1,100 @@
-package racingcar;
+package MyTest;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import racingcar.InputMessage;
 
 public class MyApplicationTest {
-//    public final InputMessage inputMessage;
-//
-//    public InputMessageTest(InputMessage inputMessage) {
-//        this.inputMessage = inputMessage;
-//    }
-//
-//    @Test
-//    public void 정상적으로_실행() {
-//        InputMessage inputMessage = new InputMessage();
-//
-//        assertDoesNotThrow(() -> {
-//            inputMessage.splitCarName("a, ab, abc, abcd, abcde", ",");
-//        });
-//
-//        assertDoesNotThrow(() -> {
-//            inputMessage.splitCarName("a3, B2", ",");
-//        });
-//    }
-//
-//    @Test
-//    void 자동차_이름이_5자를_초과하지_않는_지_테스트(){
-//        InputMessage inputMessage = new InputMessage();
-//
-//        assertThrows(IllegalArgumentException.class, () -> {
-//            inputMessage.splitCarName("abc, abcd, abcdef", ",");
-//        });
-//    }
-//
-//    @Test
-//    void 자동차_이름에_문자와_숫자만_들어있는_지_테스트(){
-//        InputMessage inputMessage = new InputMessage();
-//
-//        assertThrows(IllegalArgumentException.class, () -> {
-//            inputMessage.splitCarName("abc, ab/", ",");
-//        });
-//
-//        assertThrows(IllegalArgumentException.class, () -> {
-//            inputMessage.splitCarName("abc, ab d", ",");
-//        });
-//    }
+    InputMessage inputMessage = new InputMessage();
 
-//    @Test
-//    void 자동차가_두_대_이상_입력됐는_지_테스트(){
-//        InputMessage inputMessage = new InputMessage();
-//
-//        assertThrows(IllegalArgumentException.class, () -> {
-//            inputMessage.splitCarName("", ",");
-//        });
-//
-//        assertThrows(IllegalArgumentException.class, () -> {
-//            inputMessage.splitCarName(" ", ",");
-//        });
-//
-//        assertThrows(IllegalArgumentException.class, () -> {
-//            inputMessage.splitCarName("abc", ",");
-//        });
-//
-//        assertThrows(IllegalArgumentException.class, () -> {
-//            inputMessage.splitCarName("abc,", ",");
-//        });
-//    }
+    @Test
+    public void inputCarName_메서드_사용시_쉼표로_구분() {
+        String input = "a,ab,abcde";
+        String[] result = input.split(",");
+        String[] expected = {"a", "ab", "abcde"};
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void inputCarName_메서드_사용시_쉼표로_구분2() {
+        String input = "a, ab, abcde";
+        String[] result = input.split(",\\s*");
+        String[] expected = {"a", "ab", "abcde"};
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void 자동차_이름_생성시_문자와_숫자_사용() {
+        String input = "a3,B2";
+        String[] result = input.split(",");
+        String[] expected = {"a3", "B2"};
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void 자동차_이름에_특수문자_금지(){
+        String input = "abc,ab/";
+        String[] inputSplit = input.split(",");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            inputMessage.validateInput(inputSplit);
+        });
+    }
+
+    @Test
+    void 자동차_이름에_스페이스_금지(){
+        String input = "abc, ab d";
+        String[] inputSplit = input.split(",");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            inputMessage.validateInput(inputSplit);
+        });
+    }
+
+    @Test
+    void 자동차_이름이_5자를_초과하지_않는_지_테스트(){
+        String input = "abc, abcd, abcdef";
+        String[] inputSplit = input.split(",");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            inputMessage.validateInput(inputSplit);
+        });
+    }
+
+    @Test
+    void null_값_금지(){
+        String input = "";
+        String[] inputSplit = input.split(",");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            inputMessage.validateInput(inputSplit);
+        });
+    }
+
+    @Test
+    void 빈_칸_금지(){
+        String input = " ";
+        String[] inputSplit = input.split(",");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            inputMessage.validateInput(inputSplit);
+        });
+    }
+
+    @Test
+    void 자동차를_두_대_이상_입력했는_지_테스트(){
+        String input = "abc";
+        String[] inputSplit = input.split(",");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            inputMessage.validateInput(inputSplit);
+        });
+    }
 }
