1051087b5fc92c024fd59ea330944c7f1a63dc17
JJhwan7099 <ghks7099@naver.com>
2024-10-23 19:25:12 +0900

    feat: 레이스 진행 및 자동차 이동 기능 구현
    입력으로 주어진 횟수만큼 레이스를 진행하는 함수와 자동차를 정해진 규칙에
    맞게 이동하는 함수를 구현했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 
@@ -21,5 +22,21 @@ public class Application {
         if(car_name.length() > 5) {
             throw new IllegalArgumentException();
         }
+    }
+    public static ArrayList<Car> race_start(String cars, String move_cnt) {
+        ArrayList<Car> carList = add_cars(cars);
+        int move_time = Integer.parseInt(move_cnt);
+        while(move_time-- > 0) {
+            carList = move_car(carList);
+        }
+        return carList;
+    }
+    public static ArrayList<Car> move_car(ArrayList<Car> carList) {
+        for(Car car : carList) {
+            if(Randoms.pickNumberInRange(0,9) >= 4) {
+                car.setDistance(car.getDistance() + 1);
+            }
+        }
+        return carList;
     }
 }
