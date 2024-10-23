0ed3fe43a7371a791a8da3b22f3d8fcf8bbb69e5
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-10-23 23:36:56 +0900

    feat($model) : Provide Car include moving forward function
    내용
    - 자동차 클래스 개발
    - 해당 클래스는 전진 여부를 결정하고 실행함

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,28 @@
+package model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private String name;
+    private int distance = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getDistance() {
+        return this.distance;
+    }
+
+    //전진 할지 말지 결정
+    public void goOrNot() {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
+        if (randomNum >= 4) {
+            this.distance += 1;
+        }
+    }
+}
