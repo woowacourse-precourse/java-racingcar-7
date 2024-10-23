680c5985db25722721d5a4f175f8040ab8440050
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2024-10-23 15:19:35 +0900

    fix : 누락된 import 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Cars;
@@ -11,6 +13,7 @@ public class CarTest extends NsTest{
         Cars cars = new Cars("핍비,피비");
         assertThat(cars.getCarCount()).isEqualTo(2);
     }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
