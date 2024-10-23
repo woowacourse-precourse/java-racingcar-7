a902e904b9c6faf21b63bc877aaf693fbb2e97ac
MinseokGo <rhalstjr1999@naver.com>
2024-10-23 16:58:06 +0900

    refactor: 유효성 검증 메서드 네이밍 수정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,18 +5,18 @@ public class Car {
     private final String name;
 
     public Car(final String name) {
-        validNameIsBlank(name);
-        validNameLength(name);
+        validateNameIsBlank(name);
+        validateNameLength(name);
         this.name = name;
     }
 
-    private void validNameIsBlank(final String name) {
+    private void validateNameIsBlank(final String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("자동차 이름은 빈 문자열일 수 없습니다.");
         }
     }
 
-    private void validNameLength(final String name) {
+    private void validateNameLength(final String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 5글자를 초과할 수 없습니다.");
         }
