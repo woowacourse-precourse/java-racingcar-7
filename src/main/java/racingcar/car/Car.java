e2595778a9d902b2ed573ae4ceb42632995df127
koo-sangwoo <luckytkddn@gmail.com>
2024-10-24 00:18:16 +0900

    feat : 피드백 영상 시청 후 테스트를 작성하기 전 정확한 개발 기능 파악을 위해 임의 개발
    - 실제로 프로덕션 코드가 될지는 모르겠지만,
    구현의 큰 그림을 이해하기 위해 Car객체가 가져야하는 기능을 빠르게 구현해보았다.
    
    - 추가로 패키지의 구조를 수정했다.

---
+++ src/main/java/racingcar/car/Car.java
@@ -0,0 +1,58 @@
+package racingcar.car;
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author ddaerae
+ * 자동차와 관련된 기능이 모이는 유틸리티 클래스
+ */
+public class Car {
+
+    // 요구 사항 먼저 구현해보기
+
+    // 자동차의 이름과 전진횟수를 넣을 수 있는 객체를 리턴함
+    static List<Map<String,String>> getCarNameAndGoCountList(String inputValue) {
+        return Arrays.stream(inputValue.split(",")).map(Car::validateForCarName).toList();
+    }
+
+    static Map<String,String> validateForCarName(String carName) {
+        if (carName != null && !carName.isEmpty() && carName.length() <= 5) {
+            var carNameAndGoCount = new HashMap<String, String>();
+            carNameAndGoCount.put(carName,"");
+            return carNameAndGoCount;
+        } else if(carName != null && carName.length() > 5) {
+            throw new IllegalArgumentException("자동차명은 5글자를 넘을 수 없습니다.");
+        } else {
+            throw new IllegalArgumentException("자동차 명이 정상적이지 못합니다. 입력 값을 확인하세요");
+        }
+    }
+
+
+
+
+    // 차량의 정지, 혹은 전진 여부를 판단하여 문자열을 리턴한다.
+    public static String goOrStop(int ranNum) {
+        if (ranNum >= 4) {
+            return "-";
+        } else {
+            return "";
+        }
+    }
+
+    public static void specialCharValidation(String inputVal) {
+        boolean isContainAnotherSpecialChar = inputVal.matches(".*[^,a-zA-Z0-9].*");
+        if(isContainAnotherSpecialChar){
+            throw new IllegalArgumentException("특수기호는 , 외에 사용하면 안됩니다.");
+        }
+    }
+
+    private Car() {
+
+    }
+
+
+}
