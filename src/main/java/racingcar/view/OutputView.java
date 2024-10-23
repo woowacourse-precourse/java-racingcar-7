e9f528d81422e05385e17c814d990c66b2dbef48
slay <palgalow@gmail.com>
2024-10-23 23:32:31 +0900

    feat(OutputView): 시도할 횟수 입력 메시지 출력 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,8 +3,13 @@ package racingcar.view;
 public class OutputView {
 
     private final String INPUT_CAR_NAMES_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표() 기준으로 구분)";
+    private final String INPUT_TRY_COUNT_MSG = "시도할 횟수는 몇 회인가요?";
 
     public void printCarNameMessage() {
         System.out.println(INPUT_CAR_NAMES_MSG);
+    }
+
+    public void printTryCountMessage() {
+        System.out.println(INPUT_TRY_COUNT_MSG);
     }
 }
