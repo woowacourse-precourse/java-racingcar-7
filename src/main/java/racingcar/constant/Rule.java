e051084979ee0df86d271ee25cb527fbd0492bd1
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-24 01:17:37 +0900

    feat(Rule): 게임 규칙 상수로 정의

---
+++ src/main/java/racingcar/constant/Rule.java
@@ -0,0 +1,15 @@
+package racingcar.constant;
+
+public class Rule {
+
+    public static final String DELIMITER_COMMA = ",";
+    public static final int RANDOM_NUMBER_MIN = 0;
+    public static final int RANDOM_NUMBER_MAX = 9;
+    public static final int CAR_FORWARD_CONDITION = 4;
+    public static final int CAR_NAME_LENGTH_MAX = 5;
+    public static final int COUNT_INTEGER_MAX = Integer.MAX_VALUE;
+
+    private Rule() {
+
+    }
+}
