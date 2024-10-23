7ac9808fc0779c6b687aea0159801050dfd3b666
Jin409 <sualng123@gmail.com>
2024-10-24 00:11:21 +0900

    feat(Cars, CarsTest): 중복된 이름이 존재하는 경우 예외를 던져라
    중복된 이름이 입력되는 경우 예외를 던지는 기능 구현:
    - 중복된 이름이 존재 시 게임에 참여하는 자동차를 구분할 수 없기에 예외를 던지도록 한다.

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,17 +1,29 @@
 package racingcar.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.SequencedMap;
+import java.util.Set;
 
 public class Cars {
     private final List<Car> cars;
 
     public Cars(String[] names) {
+        validateUniqueName(names);
+
         this.cars = new ArrayList<>();
         for (String name : names) {
             cars.add(new Car(name));
+        }
+    }
+
+    private void validateUniqueName(String[] names) {
+        Set<String> uniqueNames = new HashSet<>(List.of(names));
+
+        if (names.length != uniqueNames.size()) {
+            throw new IllegalArgumentException("중복된 이름이 존재합니다.");
         }
     }
 
---
+++ src/test/java/racingcar/model/CarsTest.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarsTest {
+
+    @Test
+    @DisplayName("중복된 이름을 입력하는 경우 예외가 발생한다.")
+    void throwExceptionWhenNamesAreDuplicated() {
+        // given
+        String name = "pobi";
+        String[] duplicatedNames = {name, name, name};
+
+        // when & then
+        assertThatThrownBy(() -> new Cars(duplicatedNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 이름이 존재합니다.");
+    }
+
+}
