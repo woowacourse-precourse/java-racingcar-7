007ec1e8f91bfdab2ad18e5a298a8f64be9b6da4
Minuring <mw9505@gmail.com>
2024-10-24 00:22:33 +0900

    feat(validation): 횟수 입력에대한 검증
    횟수가 아래와 같을 때 통과합니다.
    - 1이상 50이하 정수일 때

---
+++ src/main/java/racingcar/validation/CountValidator.java
@@ -0,0 +1,22 @@
+package racingcar.validation;
+
+public class CountValidator implements InputStringValidator {
+
+    @Override
+    public void validate(String string) {
+        if (string == null || string.isEmpty() || string.isBlank()) {
+            throw new IllegalArgumentException("횟수를 입력해주세요.");
+        }
+
+        string = string.trim();
+        
+        if (!string.matches("\\d*")) {
+            throw new IllegalArgumentException("정수만 입력 가능합니다.");
+        }
+
+        int value = Integer.parseInt(string);
+        if (value <= 0 || value >= 51) {
+            throw new IllegalArgumentException("1이상 50이하의 정수만 입력 가능합니다.");
+        }
+    }
+}
---
+++ src/test/java/racingcar/validation/CountValidatorTest.java
@@ -0,0 +1,67 @@
+package racingcar.validation;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CountValidatorTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "10", "50"})
+    public void 횟수가_1이상_50이하이면_통과한다(String count) throws Exception {
+        //given
+        CountValidator countValidator = new CountValidator();
+
+        //when
+        //then
+        assertDoesNotThrow(() -> countValidator.validate(count));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {" 1", "1 ", " 50", "50 ", " 50 "})
+    public void 횟수_입력의_앞뒤로_공백이_있더라도_숫자면_통과한다(String count) throws Exception {
+        //given
+        CountValidator countValidator = new CountValidator();
+
+        //when
+        //then
+        assertDoesNotThrow(() -> countValidator.validate(count));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1", "51"})
+    public void 횟수가_1이상_50이하가_아니면_예외를_던진다(String count) throws Exception {
+        //given
+        CountValidator countValidator = new CountValidator();
+        //when
+        //then
+        assertThrows(IllegalArgumentException.class,
+            () -> countValidator.validate(count));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "\n", "\t"})
+    public void 횟수가_공백이면_예외를_던진다(String count) throws Exception {
+        //given
+        CountValidator countValidator = new CountValidator();
+
+        //when
+        //then
+        assertThrows(IllegalArgumentException.class,
+            () -> countValidator.validate(count));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "1a", "a1"})
+    public void 횟수에_문자가_있으면_예외를_던진다(String count) throws Exception {
+        //given
+        CountValidator countValidator = new CountValidator();
+
+        //when
+        //then
+        assertThrows(IllegalArgumentException.class,
+            () -> countValidator.validate(count));
+    }
+}
