533f21866b8fad25eb91c3723efbd753db476dc4
SungJun <kmymy00@naver.com>
2024-10-23 19:44:39 +0900

    refactor(Racing): 메서드 분리

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -16,6 +16,20 @@ public class Racing {
     Validator numberValidator = new NumberValidator();
 
     public void race() {
+        inputRacingCar();
+
+        int number = inputFrequency();
+        printExecution(number);
+
+        int maxDistance = countMaxDistance();
+        List<Car> winners = decisionWinners(maxDistance);
+
+        printWinners(winners);
+
+        Console.close();
+    }
+
+    private void inputRacingCar() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNameInput = Console.readLine();
         List<String> carNames = List.of(carNameInput.split(","));
@@ -25,22 +39,32 @@ public class Racing {
             Car car = new Car(carName);
             carRepository.save(car);
         }
+    }
 
+    private int inputFrequency() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String numberInput = Console.readLine();
         numberValidator.validate(numberInput);
         int number = Integer.parseInt(numberInput);
 
+        return number;
+    }
+
+    private void printExecution(int number) {
         List<Car> cars = carRepository.findAll();
 
+        System.out.println();
         System.out.println("실행 결과");
         for (int i = 0; i < number; i++) {
             for (Car car : cars) {
                 car.moveOrStop();
                 System.out.println(car.getName() + " : " + "-".repeat(car.getDistance()));
             }
+            System.out.println();
         }
+    }
 
+    private int countMaxDistance() {
         List<Car> carsResult = carRepository.findAll();
         int maxDistance = 0;
         for (Car car : carsResult) {
@@ -50,6 +74,11 @@ public class Racing {
             }
         }
 
+        return maxDistance;
+    }
+
+    private List<Car> decisionWinners(int maxDistance) {
+        List<Car> carsResult = carRepository.findAll();
         List<Car> winners = new ArrayList<>();
         for (Car car : carsResult) {
             if (car.getDistance() == maxDistance) {
@@ -57,13 +86,16 @@ public class Racing {
             }
         }
 
+        return winners;
+    }
+
+    private void printWinners(List<Car> winners) {
         System.out.print("최종 우승자 : " + winners.getFirst().getName());
         if (winners.size() > 1) {
             for (int i = 1; i < winners.size(); i++) {
                 System.out.print(", " + winners.get(i).getName());
             }
         }
-
-        Console.close();
     }
+
 }
