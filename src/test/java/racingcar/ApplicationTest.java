7aae0fd89c0f96cfad7e2b585a63e114388d13d6
immyeong <wpfls6670@naver.com>
2024-10-23 14:56:33 +0900

    test: racing 구현 메서드 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,11 +2,17 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.model.dto.Car;
+import racingcar.model.dto.RacingResult;
+import racingcar.model.service.RacingService;
+
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -28,6 +34,58 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void play_기능_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    List<Car> cars = List.of(new Car("pobi", 0), new Car("woni", 0));
+                    RacingService racingService = new RacingService();
+                    List<RacingResult> results = racingService.play(cars, 2);
+
+                    assertThat(results.get(0).getCars()).extracting("name", "moveCount")
+                            .containsExactlyInAnyOrder(
+                                    tuple("pobi", 2),
+                                    tuple("woni", 1)
+                            );
+                },
+                MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void getPlayWinner_기능_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    List<Car> cars = List.of(new Car("pobi", 0), new Car("woni", 0));
+                    RacingService racingService = new RacingService();
+                    racingService.play(cars, 1);
+
+                    String winner = racingService.getPlayWinner();
+                    assertThat(winner).isEqualTo("pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void playAttemptCount_기능_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    List<Car> cars = List.of(new Car("pobi", 0), new Car("woni", 0));
+                    RacingService racingService = new RacingService();
+                    racingService.playAttemptCount(cars);
+
+                    assertThat(cars).extracting("name", "moveCount")
+                            .containsExactlyInAnyOrder(
+                                    tuple("pobi", 1),
+                                    tuple("woni", 0)
+                            );
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
