04bb895cfd41cd0bf17e29aad6c6c8731b17b6e7
Minuring <mw9505@gmail.com>
2024-10-23 23:58:05 +0900

    feat(validation): 자동차 이름 입력에대한 검증
    자동차 이름이 아래와 같을 때 통과합니다.
    - 1자리 이상 5자리 이하일 때
    - 공백이 아닐 때

---
+++ src/main/java/racingcar/validation/CarNameValidator.java
@@ -0,0 +1,15 @@
+package racingcar.validation;
+
+public class CarNameValidator implements InputStringValidator {
+
+    @Override
+    public void validate(String string) {
+        if (string == null || string.isBlank() || string.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+        }
+
+        if (string.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
+    }
+}
---
+++ src/test/java/racingcar/validation/CarNameValidatorTest.java
@@ -0,0 +1,45 @@
+package racingcar.validation;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CarNameValidatorTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "bb", "ccc", "dddd", "eeeee"})
+    public void 자동차이름이_1자리이상_5자리이하면_통과된다(String carName) throws Exception {
+        //given
+        CarNameValidator carNameValidator = new CarNameValidator();
+
+        //when
+        //then
+        assertDoesNotThrow(() -> carNameValidator.validate(carName));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "\n", "\t"})
+    public void 자동차이름이_공백이면_예외를_던진다(String carName) throws Exception {
+        //given
+        CarNameValidator carNameValidator = new CarNameValidator();
+
+        //when
+        //then
+        assertThrows(IllegalArgumentException.class,
+            () -> carNameValidator.validate(carName));
+    }
+
+    @Test
+    public void 자동차이름이_6자리_이상이면_예외를_던진다() throws Exception {
+        //given
+        CarNameValidator carNameValidator = new CarNameValidator();
+
+        //when
+        //then
+        assertThrows(IllegalArgumentException.class,
+            () -> carNameValidator.validate("abcdef"));
+    }
+}
