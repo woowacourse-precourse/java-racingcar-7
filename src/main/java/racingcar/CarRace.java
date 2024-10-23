8410db075d1fa378cacc368c7f701eb4c22a23e9
whdmscks11 <whdmscks11@gmail.com>
2024-10-23 15:35:14 +0900

    feat(carRace): create class where main logic will be implemented
    Two main methods will be created:
    - start(): the actual race event will be performed in this method
    - printFinalWinner(): prints the final winner of the race

---
+++ src/main/java/racingcar/CarRace.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class CarRace {
+    public static String start(String cars, int moveAttempts){
+        return "";
+    }
+
+    public static void printFinalWinner(){
+
+    }
+}
