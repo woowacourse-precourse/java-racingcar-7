f1f4267f663f3fd3107abd01419d74daf5e4ef13
jseom <2ujs4311@gmail.com>
2024-10-23 14:44:36 +0900

    TEST: 잘못된 값이 입력됐는지 확인하는 예외 테스트 추가
    쉼표(,)를 기준으로 구분했을 때 빈 문자열("")이 있다면 잘못된 입력이라고 정의했습니다.
    잘못된 값이 입력되면 IllegalArgumentException이 발생하는지 확인하는 예외 테스트를 추가했습니다.

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -3,6 +3,7 @@ package racingcar;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RacingCarTest {
@@ -11,6 +12,13 @@ class RacingCarTest {
       assertEquals(List.of("pobi","woni","jun"), RacingCar.makeNameList("pobi,woni,jun"));
       assertEquals(List.of("월","woni","jun"), RacingCar.makeNameList("월,woni,jun"));
       assertEquals(List.of(";=;","월","jun"), RacingCar.makeNameList(";=;,월,jun"));
-      assertEquals(List.of("hi","+=+","jun"), RacingCar.makeNameList("hi,,+=+,jun"));
+  }
+
+  @DisplayName(",를 기준으로 구분했을 때 빈문자열이 있으면 오류")
+  @Test
+  void 자동차이름_잘못된_입력() {
+      assertThrows(IllegalArgumentException.class, ()  -> RacingCar.makeNameList("pobi,,woni,jun"));
+      assertThrows(IllegalArgumentException.class, ()  -> RacingCar.makeNameList(",pobi,woni,jun"));
+      assertThrows(IllegalArgumentException.class, ()  -> RacingCar.makeNameList("pobi,woni,jun,"));
   }
 }
