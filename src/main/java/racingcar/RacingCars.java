263661882b34b3e184c980abf20a642bc93d4251
BaSak0630 <babamba125@gmail.com>
2024-10-24 04:09:50 +0900

    fix : Add RacingCars validationListSize
    RacingCars객체 list 최대 크기 검증추가

--- src/main/java/racingcar/RacingCars.java
+++ src/main/java/racingcar/RacingCars.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCars {
+    private static final int CAR_LIST_MAX_SIZE = 10;
     private final List<Car> carList;
     public RacingCars(String readLine) {
         List<Car> tmpList = new ArrayList<>();
@@ -12,6 +13,13 @@ public class RacingCars {
             Car car = new Car(name);
             tmpList.add(car);
         }
+        validationListSize(tmpList);
         this.carList = tmpList;
+    }
+
+    private void validationListSize(List<Car> tmpList) {
+        if (tmpList.size() > CAR_LIST_MAX_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 경주할 자동차의 수는 " + CAR_LIST_MAX_SIZE +"이하만 가능합니다.");
+        }
     }
 }
