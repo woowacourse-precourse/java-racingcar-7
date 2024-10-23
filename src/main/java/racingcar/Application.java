0c7c273e19bfb8af69159eafc5ddebf76ed4d671
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2024-10-23 21:00:59 +0900

    feat: 자동차를 만드는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,8 +3,37 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class Application {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
 
+public class Application {
+
+    static class Car {
+        private String name;
+        private int distance;
+        public Car( String name ){
+            this.name = name;
+            this.distance = 0;
+        }
+        String getName() { return this.name; }
+        void drive( int distance ){ this.distance += distance; }
+    }
+
+    static List<Car> createCarList( String input ){
+        StringTokenizer tokenizer = new StringTokenizer( input, "," );
+        List<Car> carList = new ArrayList<Car>();
+        while( tokenizer.hasMoreTokens() ){
+            Car newCar = new Car( tokenizer.nextToken() );
+            carList.add( newCar );
+        }
+        return carList;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽고(,) 기준으로 구분)");
+        String input = Console.readLine();
+        List<Car> carList = createCarList( input );
     }
 }
