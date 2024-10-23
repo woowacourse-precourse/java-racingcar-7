c6566ec6df199212fba274c4ca67f6284d6ca836
juhyeongKim <zxcvbm804@gmail.com>
2024-10-24 01:12:52 +0900

    feat: 문자열을 나눠 자동차를 만드는 기능 추가
    InputString 클래스 내부에 Car 리스트의 래핑 클래스 CarCollection을
    반환하는 getCarCollection 메소드 추가
    메소드 내부에서 이름의 길이를 확인하고 5글자 초과 시 잘못된 값으로
    간주하여 에러 발생

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
 
@@ -12,11 +14,13 @@ public class Application {
 class Racing {
 
     InputString inputString;
+    CarCollection carCollection;
 
     Racing() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String str = Console.readLine();
         inputString = new InputString(str);
+        carCollection = inputString.getCarCollection();
     }
 }
 
@@ -25,8 +29,9 @@ class InputString {
     private String inputString;
 
     InputString(String string) {
-        if(string == null || string.isEmpty())
+        if (string == null || string.isEmpty()) {
             throw new IllegalArgumentException();
+        }
         inputString = string;
     }
 
@@ -34,5 +39,55 @@ class InputString {
         return inputString;
     }
 
+    public CarCollection getCarCollection() {
+        String names[] = inputString.split(",");
+        checkingValid(names);
+        List<Car> carList = new ArrayList<>();
+        for (String name : names) {
+            carList.add(new Car(name));
+        }
+        return new CarCollection(carList);
+    }
 
+    private static void checkingValid(String[] names) {
+        for (String name : names) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+}
+
+class CarCollection {
+
+    private List<Car> carList;
+
+    public CarCollection(List<Car> carList) {
+        this.carList = carList;
+    }
+
+    public List<String> getCarNames() {
+        List<String> carNames = new ArrayList<>();
+        for(Car car: carList)
+            carNames.add(car.getName());
+        return carNames;
+    }
+}
+
+class Car {
+
+    private String name;
+    private int moveCount = 0;
+
+    Car(String name) {
+        this.name = name;
+    }
+
+    public void move() {
+        this.moveCount++;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -9,6 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
@@ -32,28 +34,48 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void inputStringTest(){
+    void inputStringTest() {
         InputString inputString = new InputString("pobi,woni");
         assertSimpleTest(
-            ()->assertThat(inputString.getInputString()).isEqualTo("pobi,woni")
+            () -> assertThat(inputString.getInputString()).isEqualTo("pobi,woni")
         );
     }
+
     @Test
-    void emptyStringTest(){
+    void emptyStringTest() {
         assertSimpleTest(
-            ()->assertThatThrownBy(
-                ()->new InputString("")
+            () -> assertThatThrownBy(
+                () -> new InputString("")
             ).isInstanceOf(IllegalArgumentException.class)
         );
     }
 
     @Test
-    void nullStringTest(){
+    void nullStringTest() {
         assertSimpleTest(
-            ()->assertThatThrownBy(
-                ()->new InputString(null)
+            () -> assertThatThrownBy(
+                () -> new InputString(null)
             ).isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 자동차_생성_확인() {
+        InputString inputString = new InputString("pobi,woni");
+        CarCollection carCollection1 = inputString.getCarCollection();
+
+        Car pobiCar = new Car("pobi");
+        Car woniCar = new Car("woni");
+        CarCollection carCollection2 = new CarCollection(List.of(pobiCar, woniCar));
+        assertThat(carCollection1.getCarNames()).isEqualTo(carCollection2.getCarNames());
+    }
+
+    @Test
+    void 자동차_생성_예외_테스트() {
+        InputString inputString = new InputString("pobi,woniii");
+        assertThatThrownBy(
+            () -> inputString.getCarCollection()
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
