924ec5e3fea853b97af17173f76bb4a3144620cb
qyinm <qusseun@gmail.com>
2024-10-23 21:18:29 +0900

    feat: 이동 횟수 입력 유효성 검사 함수 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class InputView {
 
     private static final Pattern VALID_CAR_INPUT_PATTERN = Pattern.compile("[a-zA-Z]{1,4}(,[a-zA-z]{1,4})*");
-    private static final Pattern VALID_TRY_COUNT_INPUT_PATTERN = Pattern.compile("^(0|[1-9][0-9]*)$");
+    private static final Pattern VALID_MOVE_COUNT_INPUT_PATTERN = Pattern.compile("^(0|[1-9][0-9]*)$");
 
     public static String readLine() {
         return Console.readLine();
@@ -18,6 +18,10 @@ public class InputView {
 
     public static boolean isValidCarInput(String input) {
         return VALID_CAR_INPUT_PATTERN.matcher(input).matches();
+    }
+
+    public static boolean isValidMoveCountInput(String input) {
+        return VALID_MOVE_COUNT_INPUT_PATTERN.matcher(input).matches();
     }
 
 }
