03904fc879fb876071584fbfd79ee53729441f6e
hyojun <hjlim45678@gmail.com>
2024-10-23 18:31:26 +0900

    feat: 알파벳,숫자와 언더바(_)이외의 문자를 이름으로 한 경우 예외처리 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import racingcar.Car;
 
 public class InputView {
@@ -17,17 +19,12 @@ public class InputView {
         List<Car> cars = new ArrayList<>();
         for (String inputname : splitNames) {
             inputname = inputname.trim();
+            validateNameFormat(inputname);
             validateNameLength(inputname);
             Car car = new Car(inputname);
             cars.add(car);
         }
         return cars;
-    }
-
-    private static void validateNameLength(String inputname) {
-        if (inputname.length() > 5) {
-            throw new IllegalArgumentException("5글자 이하의 이름만 입력할 수 있습니다.");
-        }
     }
 
     public static int readTryCount() {
@@ -42,6 +39,20 @@ public class InputView {
     private static void validateEmptyString(String inputString) {
         if (inputString.isBlank()) {
             throw new IllegalArgumentException("빈 값을 입력하셨습니다");
+        }
+    }
+    private static void validateNameLength(String inputname) {
+        if (inputname.length() > 5) {
+            throw new IllegalArgumentException("5글자 이하의 이름만 입력할 수 있습니다.");
+        }
+    }
+    private static void validateNameFormat(String inputname) {
+        String rex = "[\\W]+";
+        Pattern pattern = Pattern.compile(rex);
+        Matcher matcher = pattern.matcher(inputname);
+
+        if (matcher.find()) {
+            throw new IllegalArgumentException("이름은 알파벳,숫자와 언더바(_)만 사용할 수 있습니다.");
         }
     }
 }
