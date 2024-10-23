1651bd01bb91a96fcbc22057b5d3a51b291ea2a0
GiSeok Kang <ime.rokz@gmail.com>
2024-10-24 01:07:30 +0900

    refactor: Car, Name 클래스 패키지 변경

--- src/test/java/racingcar/NameTest.java
+++ src/test/java/racingcar/NameTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.model.Name;
 
 class NameTest {
 
