4ef66ec6c1f83cd57d36db98b2c9b053cf2d4b22
JJhwan7099 <ghks7099@naver.com>
2024-10-23 19:17:35 +0900

    feat: Car 리스트를 생성 및 예외처리
    Car 리스트를 생성하고 자동차 이름에 대해서 예외를 처리했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,9 +2,24 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         String cars = Console.readLine();
         String move_cnt = Console.readLine();
+    }
+    public static ArrayList<Car> add_cars(String cars) {
+        ArrayList<Car> carList = new ArrayList<>();
+        for (String car_name : cars.split(",")) {
+            validate(car_name);
+            carList.add(new Car(car_name, 0));
+        }
+        return carList;
+    }
+    public static void validate(String car_name) {
+        if(car_name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
     }
 }
