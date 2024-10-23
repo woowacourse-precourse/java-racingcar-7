cad0f84e4310d01d7202e23f3e5a790abf2f2cad
eeekeee <ekrtionl@gmail.com>
2024-10-23 17:10:31 +0900

    style: AttemptTest Class 메서드 명 수정

--- src/test/java/domain/AttemptTest.java
+++ src/test/java/domain/AttemptTest.java
@@ -2,14 +2,13 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AttemptTest {
     @Test
     @DisplayName("시도 횟수가 숫자가 아니면 예외 발생")
-    void validAttempt() {
+    void notDigitAttemptException() {
         String attemptCount = "a";
 
         assertThatThrownBy(() -> new Attempt(attemptCount))
@@ -18,7 +17,7 @@ public class AttemptTest {
 
     @Test
     @DisplayName("시도 횟수가 음수일 경우 예외 발생")
-    void negativeAttempt() {
+    void negativeAttemptException() {
         String attemptCount = "-1";
 
         assertThatThrownBy(() -> new Attempt(attemptCount))
@@ -27,7 +26,7 @@ public class AttemptTest {
 
     @Test
     @DisplayName("시도 횟수가 0일 경우 예외 발생")
-    void zeroAttempt() {
+    void zeroAttemptException() {
         String attemptCount = "0";
 
         assertThatThrownBy(() -> new Attempt(attemptCount))
