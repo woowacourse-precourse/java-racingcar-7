b51e4ff8d93520fc6e89c7c1db87a04ae9a922db
jenna <shinysuh1992@gmail.com>
2024-10-23 17:26:12 +0900

    feat: 자동차별 랜덤값으로 인한 전진/멈춤 값 업데이트

--- src/main/java/racingcar/runner/RacingCar.java
+++ src/main/java/racingcar/runner/RacingCar.java
@@ -1,5 +1,8 @@
 package racingcar.runner;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.runner.dto.CarDto;
+
 public class RacingCar {
 
     private final String carString;
@@ -12,5 +15,21 @@ public class RacingCar {
 
     public String run() {
         return "";
+    }
+
+    private void updateMovementStatusByCar(CarDto car) {
+        int movementStatus = car.getMovementStatus() + this.getMovingStatus();
+        car.setMovementStatus(movementStatus);
+    }
+
+    private int getMovingStatus() {
+        int minForMovingForward = 4;
+        int random = Randoms.pickNumberInRange(0, 9);
+
+        if (random < minForMovingForward) {
+            return 0;
+        }
+
+        return 1;
     }
 }
