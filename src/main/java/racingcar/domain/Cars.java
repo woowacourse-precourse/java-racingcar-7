37deca562c01ab29d9e53146af2933eb111dd799
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2024-10-23 15:22:16 +0900

    fix : 자동차 안정성 추가 및 누락된 초기화 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,19 +1,21 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import racingcar.utility.StringUtility;
 
 public class Cars {
-    public List<Car> cars;
+    public final List<Car> cars;
 
     public Cars(String raceCarNames){
+        cars = new ArrayList<>();
         List<String> carNameList = StringUtility.splitStringToList(raceCarNames);
         for(String carName : carNameList){
             cars.add(new Car(carName));
         }
     }
 
-    public int getCarSize(){
+    public int getCarCount(){
         return cars.size();
     }
 
