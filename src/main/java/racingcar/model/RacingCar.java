9f18da642090eedeed54512902c1710e47f2dc3c
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-23 15:49:16 +0900

    refactor($RacingCar): 이름을 검증하지 않도록 변경
    - 이름 검증은 추후 구현 예정.

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -5,18 +5,10 @@ public class RacingCar {
     private final String name;
     private final int position;
     private final int DEFAULT_POSITION = 0;
-    private final int MAX_NAME_LENGTH = 5;
 
     public RacingCar(String name) {
-        validateName(name);
         this.name = name;
         this.position = DEFAULT_POSITION;
-    }
-
-    private void validateName(String name) {
-        if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
-        }
     }
 
     public String getName() {
