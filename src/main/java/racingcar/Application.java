2c1ee9baf80062fb9b01bb7e99cf50537995c7f5
jjuchan <163832764+jjuchan@users.noreply.github.com>
2024-10-23 10:58:40 +0900

    readme.md 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        System.out.println("경주 할 자동차 이름을 입력해주세요.");
+        String name = sc.nextLine();
+        System.out.println("시도할 횟수를 입력해주세요.");
+        int move = sc.nextInt();
     }
 }
