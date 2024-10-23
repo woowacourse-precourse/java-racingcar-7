427a6073fbe15eb85b76fc9fbb8ec2edb7d13432
jaeyeong <zzz26lk@gmail.com>
2024-10-23 13:11:19 +0900

    refactor: 검증 위치 변경
    players 입력을 playerMaker 객체에서 검사하면 늦기 때문에
    검사를 입력을 받자마자 바로 한다.

--- src/main/java/racingcar/domain/PlayerMaker.java
+++ src/main/java/racingcar/domain/PlayerMaker.java
@@ -10,8 +10,6 @@ public class PlayerMaker {
     private final static int SET_SCORE = 0;
 
     public List<Player> createPlayer(String players) {
-        PlayerValidation.validation(players);
-
         List<Player> playerList = new ArrayList<>();
         for (String player : players.split(",")) {
             playerList.add(new Player(player, SET_SCORE));
