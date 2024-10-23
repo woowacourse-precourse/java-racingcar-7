a9da6c327f983315116a5b5583a723706ac7c36c
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-23 20:52:45 +0900

    test(Validator) : 유효성 검사 로직 관련 테스트 코드 추가 - 숫자 뽑기 횟수를 숫자형으로 변환할 수 없는 경우에 관한 테스트 코드 작성 - 유효성 검사에 관한 기존 테스트 코드 가독성 개선

--- src/test/java/racingcar/util/ValidatorTests.java
+++ src/test/java/racingcar/util/ValidatorTests.java
@@ -2,14 +2,16 @@ package racingcar.util;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static racingcar.util.Validator.validateInputString;
+import static racingcar.util.Validator.validateInteger;
 import static racingcar.util.Validator.validateNameCount;
 import static racingcar.util.Validator.validateNameLength;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ValidatorTests {
 
@@ -23,25 +25,20 @@ class ValidatorTests {
         assertEquals(expected, result);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("입력한 문자열이 빈 문자열이거나 공백만 포함하는 문자열일 경우 예외 발생")
-    void exceptionIfInputIsBlank() {
-        assertThatThrownBy(() -> validateInputString("      "))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> validateInputString(""))
+    @ValueSource(strings = {"", "      "})
+    void exceptionIfInputIsBlank(String input) {
+        assertThatThrownBy(() -> validateInputString(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("입력한 문자열이 쉼표로 시작하거나 끝나면 예외 발생")
-    void exceptionIfStartOrEndWithDelimiter() {
-        String input1 = ",alice,bob,john";
-        String input2 = "alice,bob,john,";
-        String input3 = ",alice,bob,john,";
-
-        assertThrows(IllegalArgumentException.class, () -> validateInputString(input1));
-        assertThrows(IllegalArgumentException.class, () -> validateInputString(input2));
-        assertThrows(IllegalArgumentException.class, () -> validateInputString(input3));
+    @ValueSource(strings = {",alice,bob,john", ",alice,bob,john", ",alice,bob,john"})
+    void exceptionIfStartOrEndWithDelimiter(String input) {
+        assertThatThrownBy(() -> validateInputString(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -52,15 +49,19 @@ class ValidatorTests {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("자동차 이름이 5글자를 초과하거나 빈 문자열일 경우 예외 발생")
-    void testIfNameLengthNotInRange() {
-        String name1 = "racingCar";
-        String name2 = "";
-
-        assertThatThrownBy(() -> validateNameLength(name1))
+    @ValueSource(strings = {"", "racingCar"})
+    void testIfNameLengthNotInRange(String name) {
+        assertThatThrownBy(() -> validateNameLength(name))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> validateNameLength(name2))
+    }
+
+    @Test
+    @DisplayName("숫자 뽑기 횟수를 숫자형으로 변환할 수 없을 경우 IllegalArgumentException 발생")
+    void exceptionIfTrialCountNotInteger() {
+        String trialCountString = "a";
+        assertThatThrownBy(() -> validateInteger(trialCountString))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
