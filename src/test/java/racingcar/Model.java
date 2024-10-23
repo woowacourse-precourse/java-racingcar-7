741e272c7e18f39b2f6e728b1272fb77e221009c
joshinhyeok <shtree07@naver.com>
2024-10-23 21:39:37 +0900

    feat: 자동차 이름이 5자가 초과할 경우 예외발생 기능 추가

--- src/test/java/racingcar/Model.java
+++ src/test/java/racingcar/Model.java
@@ -3,6 +3,13 @@ package racingcar;
 public class Model {
 
     String[] getCarNames(String inputString) {
-        return inputString.split(",");
+        String[] carNames = inputString.split(",");
+
+        for (int i = 0; i < carNames.length; i++) {
+            if (carNames[i].length() > 5) {
+                throw new IllegalArgumentException("자동차 이름이 5자를 초과합니다.");
+            }
+        }
+        return carNames;
     }
 }
