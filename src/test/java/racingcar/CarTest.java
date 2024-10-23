9e83c91c570c95c7d050268c375d81ae0f6126db
NamJihyun99 <namjihyun9936@gmail.com>
2024-10-24 00:15:48 +0900

    test:> car compareTo 검증

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -16,24 +16,48 @@ class CarTest {
     }
 
     @Test
-    @DisplayName("[성공] 기준 숫자 이상이면 전진")
+    @DisplayName("기준 숫자 이상이면 전진")
     void executeMove() {
         car.execute(4);
         assertThat(car.getDistance()).isEqualTo(1);
     }
 
     @Test
-    @DisplayName("[성공] 기준 숫자 미만이면 정지")
+    @DisplayName("기준 숫자 미만이면 정지")
     void executeStay() {
         car.execute(0);
         assertThat(car.getDistance()).isEqualTo(0);
     }
 
     @Test
+    @DisplayName("Car 한 대의 현재 상태 문자열 반환 검증")
     void testToString() {
+        car.execute(4);
+        car.execute(4);
+        assertThat(car.toString()).isEqualTo("Jihyun : --");
     }
 
     @Test
-    void compareTo() {
+    @DisplayName("이동 거리가 더 긴 Car가 우선순위 높음")
+    void compareDistance() {
+        Car anotherCar = new Car("Jihye");
+        car.execute(4);
+        car.execute(4);
+        anotherCar.execute(4);
+        anotherCar.execute(0);
+        assertThat(car.getDistance()).isEqualTo(2); // car은 이동 거리가 2
+        assertThat(anotherCar.getDistance()).isEqualTo(1); // anotherCar은 이동 거리가 1
+        assertThat(car.compareTo(anotherCar)).isNegative();
+    }
+
+    @Test
+    @DisplayName("이동 거리가 같으면 이름을 사전식 나열시 더 앞에 있는 Car의 우선순위 높음")
+    void compareName() {
+        Car anotherCar = new Car("Jihye");
+        car.execute(4);
+        anotherCar.execute(4);
+        assertThat(car.getDistance()).isEqualTo(1); // car은 이동 거리가 1
+        assertThat(anotherCar.getDistance()).isEqualTo(1); // anotherCar은 이동 거리가 1
+        assertThat(car.compareTo(anotherCar)).isPositive(); // Jihye가 Jihyun보다 우선순위 높음
     }
 }
