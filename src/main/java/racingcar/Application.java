e41c30e6f2c218735e58ed8ae482bd779624f2a6
HanJun Lee <137124830+LeeHanJun00@users.noreply.github.com>
2024-10-23 23:19:56 +0900

    feat: 경주할 자동차 이름을 입력받는 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 이름입력 기능
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCarName = Console.readLine();
+
+        // 이름 판단
+        List<String> carNameList = Validate.carName(inputCarName);
+        System.out.println(carNameList);
     }
 }
---
+++ src/main/java/racingcar/Validate.java
@@ -0,0 +1,57 @@
+package racingcar;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Validate {
+    private static final int MAX_NAME_LENGTH = 5;
+
+    public static List<String> carName(String inputCarName) {
+        try {
+            List<String> splitComma = checkComma(inputCarName);
+            List<String> carNameList = checkLength(splitComma);
+            List<String> finalCarNameList = checkCarNameList(carNameList);
+
+            return finalCarNameList;
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private static List<String> checkCarNameList(List<String> carNameList) {
+        for (String carName : carNameList) {
+            if (carName == null || carName.trim().isEmpty()) {
+                System.out.println(carNameList);
+                throw new IllegalArgumentException("자동차 이름이 비어있습니다.");
+            }
+        }
+        return carNameList;
+    }
+
+    // 각 자동차 이름의 길이가 5자 이하인지 확인
+    private static List<String> checkLength(List<String> splitComma) {
+        for (String name : splitComma) {
+            if (name.trim().length() > MAX_NAME_LENGTH) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다: " + name);
+            }
+        }
+        return splitComma;
+    }
+
+
+    private static List<String> checkComma(String inputCarName) {
+        List<String> splitCarName;
+
+        // 입력 문자열에 콤마가 있는 경우 콤마로 분리하여 리스트로 변환
+        if (inputCarName.contains(",")) {
+            // TODO ,, 로 끝날경우?
+            splitCarName = Arrays.asList(inputCarName.split(","));
+            return splitCarName;
+        }
+
+        // 콤마가 없는 경우, 입력된 문자열을 그대로 리스트로 반환
+        splitCarName = List.of(inputCarName);
+        return splitCarName;
+    }
+
+}
