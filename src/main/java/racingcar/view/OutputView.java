9c4a9a5612b0639df3516304cf12537222f6c211
bowook <mbosx316@naver.com>
2024-10-24 00:18:22 +0900

    feat(OutputView) : OutputView 기능 추가
    - 실행 결과 메시지 출력 기능
    - 각 단계 현재 위치 출력 기능
    - 최종 우승자 출력 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,30 @@
-package racingcar.view;public class OutputView {
+package racingcar.view;
+
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
+import java.util.List;
+
+public class OutputView {
+
+    public void executionResultMessage() {
+        System.out.println("\n실행 결과");
+    }
+
+    public void currentStatusMessage(Cars cars) {
+        for (Car car : cars.getCars()) {
+            System.out.println(car.getCarName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    public void finalWinnerMessage(List<String> winners) {
+        String message = "최종 우승자 : " + winners.get(0);
+        if (winners.size() > 1) {
+            message += ", " + String.join(", ", winners.subList(1, winners.size()));
+        }
+        System.out.println(message);
+    }
+
+
 }
