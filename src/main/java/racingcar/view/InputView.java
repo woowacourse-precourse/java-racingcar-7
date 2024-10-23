def6076a4b0f2d605b50a9f1691273d3438d3b79
DoomchitYJ <poj1004@inha.edu>
2024-10-23 16:00:29 +0900

    fix: 자동차 이름을 입력받는 함수 이름 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String INPUT_VIEW = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
-    public static String readInputString() {
+    public static String readCarNames() {
         System.out.println(INPUT_VIEW);
         String input = Console.readLine().trim();
         Console.close();
