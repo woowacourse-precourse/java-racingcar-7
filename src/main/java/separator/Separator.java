7e6aa002437a410bae3dade5d17b12d12f4c8689
ouvir <kilhyunii@naver.com>
2024-10-23 20:17:44 +0900

    feat: 자동차 이름 구분 기능 구현
    - 입력 문자열을 콤마(,)로 구분하여 자동차 이름 리스트로 변환하는 기능 구현
    - 유효성 검사(빈 값 및 중복 값) 로직 추가

---
+++ src/main/java/separator/Separator.java
@@ -0,0 +1,39 @@
+package separator;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Separator {
+
+    public static List<String> split(String text) throws IllegalArgumentException {
+        if (text == null || text.isEmpty()) {
+            throw new IllegalArgumentException("input is empty");
+        }
+
+        List<String> splitItems  = List.of(text.split(","));
+
+        if (isContainEmpty(splitItems)) {
+            throw new IllegalArgumentException("split value is empty");
+        }
+        if (isNameDuplicate(splitItems)) {
+            throw new IllegalArgumentException("duplicate input");
+        }
+
+        return splitItems;
+    }
+
+    private static boolean isContainEmpty(List<String> splitTexts) {
+        for (String text : splitTexts) {
+            if (text.trim().isEmpty()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNameDuplicate(List<String> nameList) {
+        Set<String> uniqueNames = new HashSet<>(nameList);
+        return uniqueNames.size() != nameList.size();
+    }
+}
