f1e4bdd842b0cc52a6f5f513d0d10ee6ebf05583
eb9862 <luclead622@gmail.com>
2024-10-23 18:36:31 +0900

    feat : 이동을 시도할 횟수 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,10 +6,16 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String carNames = readCarNames();
+        String tryCount = readTryCount();
     }
 
     static String readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    static String readTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
         return Console.readLine();
     }
 }
