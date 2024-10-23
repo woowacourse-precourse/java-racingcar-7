06db51606eca09349e29d0a371711afb7ba9a0fe
mohyerolo <garam2101@naver.com>
2024-10-23 13:16:17 +0900

    fix: 입력 검증 값 수정

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -5,13 +5,13 @@ public class InputValidator {
     private static final int CAR_NAME_LIMIT_LENGTH = 5;
 
     public void validateCarNameLength(final String name) {
-        if (name.isBlank() || name.length() > CAR_NAME_LIMIT_LENGTH) {
+        if (name == null || name.isBlank() || name.length() > CAR_NAME_LIMIT_LENGTH) {
             throw new IllegalArgumentException("자동차의 이름은 존재해야하며 5자 이하만 가능합니다.");
         }
     }
 
     public void validateTryCountGreaterZero(final int count) {
-        if (count < 0) {
+        if (count < 1) {
             throw new IllegalArgumentException("시도 횟수는 0보다 커야됩니다.");
         }
     }
