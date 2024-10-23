2fb14f551585d430e8e34f68d097ce4464145cfd
joshinhyeok <shtree07@naver.com>
2024-10-23 18:17:41 +0900

    feat: 전체 제거 후 처음부터 구현
    두 클래스에 쭉 구현하는 식으로 테스트를 통과하는 코드는 만들어냈으나, T"D"D 방식으로 구현을 하고, 중간중간 필요할때 바로 리팩토링과 객채분리를 하는식으로 다시 처음부터 구현

--- src/test/java/racingcar/Application.java
+++ src/test/java/racingcar/Application.java
@@ -1,19 +1,6 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
     public static void main(String[] args) {
-        RacingCar racingCar = new RacingCar();
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
-        String inputString = Console.readLine();
-        String[] racingCars = racingCar.separateCarNames(inputString);
-
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String inputString2 = Console.readLine();
-        int NumberOfAttempts = racingCar.validateNumberOfAttempts(inputString2);
-        racingCar.race(NumberOfAttempts);
-        String winners = racingCar.extractWinners();
-        System.out.println("최종 우승자 : " + winners);
     }
 }
