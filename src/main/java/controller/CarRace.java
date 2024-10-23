73eb2370e34c15fd3ba404e26485a34f53813ffd
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-10-23 23:11:32 +0900

    feat: 0~9 사이의 무작위 값을 구하고, 값이 4이상인 경우만 전진

--- src/main/java/controller/CarRace.java
+++ src/main/java/controller/CarRace.java
@@ -1,14 +1,26 @@
 package controller;
 
+import domain.Car;
 import view.InputView;
 import view.OutputView;
+
+import java.util.List;
 
 public class CarRace {
 
     public static void start() {
         OutputView.printInputCarNames();
-        InputView.getCarNames();
+        List<Car> cars = InputView.getCarNames();
         OutputView.printInputTryCount();
-        InputView.getTryCount();
+        int tryCount = InputView.getTryCount();
+
+        while(tryCount > 0) {
+
+            for (Car car: cars) {
+                car.move();
+            }
+
+            tryCount--;
+        }
     }
 }
--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,17 +1,22 @@
 package domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
     private static final String MAXIMUM_NAME_SIZE_ERROR_MESSAGE = "자동차 이름은 5글자 이하로 입력해야 합니다";
     private static final String NAME_EMPTY_ERROR_MESSAGE = "올바른 자동차 이름을 입력해야 합니다.";
+    private static final Integer MIN_MOVE_COUNT = 4;
     private static final Integer MAXIMUM_NAME_SIZE = 6;
 
     private final String name;
+    private int status;
 
     public Car(final String name) {
         validateEmpty(name);
         validateNameSize(name);
         this.name = name;
+        this.status = 0;
     }
 
     public void validateNameSize(final String name) {
@@ -24,5 +29,16 @@ public class Car {
         if(name.isBlank()) {
             throw new IllegalArgumentException(NAME_EMPTY_ERROR_MESSAGE);
         }
+    }
+
+    public void move() {
+        int number = Randoms.pickNumberInRange(0, 9);
+        if(number >= MIN_MOVE_COUNT) {
+            status++;
+        }
+    }
+
+    public int getStatus() {
+        return status;
     }
 }
