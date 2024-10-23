771aea7ca64496909a0dd17b61b00848b5aff4d8
Hyungjung Kim <gudwnd1113@gmail.com>
2024-10-23 20:51:50 +0900

    feat(RacingCar): define rollDice() method
    자동차의 전진 여부를 결정하는 함수.
    0-9 사이에서 구한 무작위 값이 4 이상일 경우, 인스턴스 변수 count를
    증가시킨다.

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCar {
     private String name;
     private int count;
@@ -7,5 +9,11 @@ public class RacingCar {
     public RacingCar(final String name) {
         this.name = name;
         this.count = 0;
+    }
+
+    public void rollDice() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            this.count += 1;
+        }
     }
 }
