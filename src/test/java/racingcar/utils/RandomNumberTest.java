786adc48b3b4f4596666a652866a0f285f8a3425
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-10-23 21:30:54 +0900

    test: RandomNumber 테스트코드 작성
    @RepeatedTest(50)을 통해 50번의 범위 테스트를 실시함

---
+++ src/test/java/racingcar/utils/RandomNumberTest.java
@@ -0,0 +1,18 @@
+package racingcar.utils;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+public class RandomNumberTest {
+
+    @DisplayName("0~9 범위로 숫자가 생성되는지, 100번의 테스트로 검증한다")
+    @RepeatedTest(50)
+    void 랜덤번호_생성_테스트() {
+        int generateNumber = RandomNumber.generate();
+        assertThat(generateNumber).isBetween(0, 9);
+    }
+
+}
