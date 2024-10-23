0a318251fba7577ecd7b5a1a62fee6abb6db699c
je488 <pje488@naver.com>
2024-10-23 23:07:05 +0900

    feat: 자동차 이름을 입력받아 리턴하는 메서드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +10,10 @@ public class Application {
 
     public static void printInputMessageCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static String inputCarName() {
+        return Console.readLine();
     }
 
     public static int makeRandomNumber() {
@@ -22,15 +27,6 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        printInputMessageCarName();
-        car.put("박", 0);
-        car.put("재", 0);
-        car.put("은", 0);
-        int randomNumber = makeRandomNumber();
-        System.out.println(randomNumber);
-        moveOrStop("박", randomNumber);
-        for (String key : car.keySet()) {
-            System.out.println(key + ":" + car.get(key));
-        }
+
     }
 }
