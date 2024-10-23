a80798924bc4c8b73c734007cf90174a20e926f6
변우영 <likevangogh24@gmail.com>
2024-10-23 22:59:52 +0900

    feat(RacingGame): 경주 진행 메서드 추가

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -2,7 +2,9 @@ package racingcar.domain;
 
 import racingcar.dto.CarStatusDto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class RacingGame {
@@ -14,7 +16,20 @@ public class RacingGame {
         this.racingCars = cars;
         this.round = round;
     }
-    
+
+    public Map<Integer, List<CarStatusDto>> raceAllRound() {
+        Map<Integer, List<CarStatusDto>> roundResults = new HashMap<>();
+        for (int i = 1; i <= round; i++) {
+            raceRound();
+            roundResults.put(i, getCurrentRoundStatus());
+        }
+        return roundResults;
+    }
+
+    public void raceRound() {
+        racingCars.forEach(Car::move);
+    }
+
     public List<CarStatusDto> getCurrentRoundStatus() {
         return racingCars.stream()
                 .map(car -> new CarStatusDto(car.getName(), car.getPosition()))
