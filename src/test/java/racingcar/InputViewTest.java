4089c2840ffac57f475ff8af30f5381ae0bcb9c3
BaSak0630 <babamba125@gmail.com>
2024-10-24 03:29:01 +0900

    test : Add InputViewTest
    공백 입력 테스트 추가

---
+++ src/test/java/racingcar/InputViewTest.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputViewTest extends NsTest {
+    @Test
+    @DisplayName("공백 입력 테스트")
+    public void readLineTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" "))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
