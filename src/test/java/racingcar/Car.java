93ce5ee00d6a6bebef13bca8c21ce8190e9e5715
joshinhyeok <shtree07@naver.com>
2024-10-24 05:29:17 +0900

    feat: Car 생성자 추가
    test에서 우승자 테스트할때 position 조작을 위해 추가

--- src/test/java/racingcar/Car.java
+++ src/test/java/racingcar/Car.java
@@ -8,6 +8,11 @@ public class Car {
         this.carName = carName;
     }
 
+    public Car(String carName, int position) {
+        this.carName = carName;
+        this.position = position;
+    }
+
     public String getCarName() {
         return carName;
     }
