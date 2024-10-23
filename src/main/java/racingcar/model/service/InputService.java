b0eb3f20820aa895cc25595de1c102579c5800fb
immyeong <wpfls6670@naver.com>
2024-10-23 13:34:48 +0900

    refactor: CustomException으로 변경

--- src/main/java/racingcar/model/service/InputService.java
+++ src/main/java/racingcar/model/service/InputService.java
@@ -1,6 +1,9 @@
 package racingcar.model.service;
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.model.dto.Car;
+import racingcar.model.exception.CarNameLengthExceededException;
+import racingcar.model.exception.InvalidAttemptCountException;
+import racingcar.model.exception.MissingInputValueException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,21 +17,21 @@ public class InputService {
 
     public List<Car> inputValidator(String carValue, String countValue){
         if(carValue.isEmpty() || countValue.isEmpty() || carValue == null || countValue == null){
-            throw new IllegalArgumentException("입력값이 누락되었습니다.");
+            throw new MissingInputValueException("입력값이 누락되었습니다.");
         }
 
         List<String> cars = splitCarValue(carValue);
         List<Car> carList = new ArrayList<>();
         for (String car : cars) {
             if(car.length() > 5){
-                throw new IllegalArgumentException("자동차 이름의 입력값은 최대 5자입니다.");
+                throw new CarNameLengthExceededException("자동차 이름의 입력값은 최대 5자입니다.");
             }
             Car carInfo = new Car(car, 0);
             carList.add(carInfo);
         }
 
         if(!isNumeric(countValue)){
-            throw new IllegalArgumentException("시도할 횟수는 숫자여야합니다.");
+            throw new InvalidAttemptCountException("시도할 횟수는 숫자여야합니다.");
         }
 
         return carList;
