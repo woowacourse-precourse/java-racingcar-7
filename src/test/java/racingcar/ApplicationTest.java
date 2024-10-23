5f1bcf19c18903d35331dc0dd3ac88c105d9044c
Seonjeongg <sj030205@naver.com>
2024-10-23 23:17:54 +0900

    test: 실행횟수 음수 처리 에러 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -6,12 +6,14 @@ import racingcar.Model.Car;
 import racingcar.Service.GameService;
 import racingcar.View.InputView;
 
+import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -48,7 +50,15 @@ class ApplicationTest extends NsTest {
         assertThat(carList.get(1).getCarName()).isEqualTo("woni");
         assertThat(carList.get(2).getCarName()).isEqualTo("jun");
     }
-
+    @Test
+    void 실행횟수_음수_처리_에러_테스트(){
+        String Count="-1";
+        ByteArrayInputStream Countin=new ByteArrayInputStream(Count.getBytes());
+        System.setIn(Countin);
+        assertThrows(IllegalArgumentException.class,()->{
+            InputView.Game_count();
+        });
+    }
     @Override
     public void runMain() {
         Application.main(new String[]{});
