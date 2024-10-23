b751bdff69a6f94a5223d03205fe5a7d55ae66e6
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-23 22:00:51 +0900

    refactor: 입출력 메시지 수정
    최종 우승자 출력 헤더의 뒷 부분 공백 문자를 제거

--- src/main/java/racingcar/enums/IOMessages.java
+++ src/main/java/racingcar/enums/IOMessages.java
@@ -3,7 +3,7 @@ package racingcar.enums;
 public enum IOMessages {
     INPUT_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     INPUT_REPEAT_TIMES("시도할 횟수는 몇 회인가요?"),
-    RESULT_HEADER("최종 우승자 : ");
+    RESULT_HEADER("최종 우승자 :");
 
     private final String msg;
 
