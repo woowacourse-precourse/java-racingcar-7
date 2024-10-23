690bda0d548307a0bfc3673f8c3e380c699984ed
daxxn <dahyeonhan4529@gmail.com>
2024-10-23 22:18:50 +0900

    feat(input): add input class and exception class
    - Implement InputView class to manager user input for car and count data
    - Implement ExceptionMessage class
    - Included error handling for empty input cases

---
+++ src/main/java/error/ExceptionMessage.java
@@ -0,0 +1,6 @@
+package error;
+
+public class ExceptionMessage {
+    public static final String CARS_INPUT_EMPTY = "경주할 자동차 이름을 입력해주세요";
+    public static final String COUNTS_INPUT_EMPTY = "시도할 횟수를 입력해주세요";
+}
---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,28 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+import error.ExceptionMessage;
+
+public class InputView {
+
+    public String inputCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String cars = Console.readLine();
+        if (cars.isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessage.CARS_INPUT_EMPTY);
+        }
+        Console.close();
+        return cars;
+    }
+
+    public int inputCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String countString = Console.readLine();
+        if (countString.isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessage.COUNTS_INPUT_EMPTY);
+        }
+        Console.close();
+        return Integer.parseInt(countString);
+    }
+
+}
