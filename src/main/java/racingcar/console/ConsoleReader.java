3523f99fa2e88e99a1e4aa3c594dc0a58f61f738
ufo9430 <ufo9430@naver.com>
2024-10-22 23:23:08 -0400

    feat: 자동차의 이름을 입력하고 유효성을 검사하는 기능 구현

--- src/main/java/racingcar/console/ConsoleReader.java
+++ src/main/java/racingcar/console/ConsoleReader.java
@@ -1,4 +1,43 @@
 package racingcar.console;
 
-public class ConsoleManager {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class ConsoleReader {
+
+    public static String read(String input){
+        return Validator.validate(input);
+    }
+
+    private static class Validator {
+        public static String validate(String input){
+            List<String> nameList = Arrays.stream(input.split(",")).toList();
+            for (String name : nameList) {
+                isBlank(name);
+                isOversized(name.length() > 5);
+            }
+            isDuplicated(nameList);
+            return input;
+        }
+
+        private static void isDuplicated(List<String> nameList) {
+            HashSet<String> nameSet = new HashSet<>(nameList);
+            if(nameList.size() != nameSet.size()){
+                throw new IllegalArgumentException();
+            }
+        }
+
+        private static void isOversized(boolean name) {
+            if (name) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        private static void isBlank(String name) {
+            if(name.isEmpty()){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
