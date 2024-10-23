eb66b0525f5d0e3e4e14c0e8f7da5dfed5f6945b
alpin87 <qortmdals94@naver.com>
2024-10-23 20:44:21 +0900

    feat: 게임 실행을 위한 메인 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String carNames = Console.readLine();
 
+            System.out.println("시도할 횟수는 몇 회인가요?");
+            int attempts = Integer.parseInt(Console.readLine());
+
+            RaceGame game = new RaceGame(carNames);
+            game.playGame(attempts);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
