1f9e178d085b6bd75ebda86162b3405fe9f57c78
pakaheo <k70923@konkuk.ac.kr>
2024-10-24 07:04:46 +0900

    chore : Initialize project

--- src/main/java/racingcar/CarFactory.java
+++
@@ -1,9 +0,0 @@
-package racingcar;
-
-import java.util.Arrays;
-
-public class CarFactory {
-    public static Cars createCars(String input) {
-        return new Cars(Arrays.stream(input.split(",")).map(Car::new).toList());
-    }
-}
