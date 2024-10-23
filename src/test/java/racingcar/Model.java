a6c3d2f5b903188c3ad939722c75560088995b7e
joshinhyeok <shtree07@naver.com>
2024-10-23 21:43:24 +0900

    refactor: 자동차 이름 유효성 검사를 별도의 메서드로 분리

--- src/test/java/racingcar/Model.java
+++ src/test/java/racingcar/Model.java
@@ -4,12 +4,15 @@ public class Model {
 
     String[] getCarNames(String inputString) {
         String[] carNames = inputString.split(",");
+        validateCarNames(carNames);
+        return carNames;
+    }
 
-        for (int i = 0; i < carNames.length; i++) {
-            if (carNames[i].length() > 5) {
+    private void validateCarNames(String[] carNames) {
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름이 5자를 초과합니다.");
             }
         }
-        return carNames;
     }
 }
