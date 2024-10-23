bd6caf9978b8348e6ba766c98f1ac2271994a45c
Hacanna42 <dtmdgus901@gmail.com>
2024-10-23 21:05:36 +0900

    feat(controller): implement RacingGame

---
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -0,0 +1,31 @@
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.view.OutputHandler;
+
+public class RacingGame {
+    private final OutputHandler outputHandler;
+    private final List<Car> cars;
+    private int rounds;
+
+    public RacingGame(OutputHandler outputHandler, List<Car> cars, int rounds) {
+        this.outputHandler = outputHandler;
+        this.cars = cars;
+        this.rounds = rounds;
+    }
+
+    public void start() {
+        while (rounds-- > 0) {
+            cars.forEach(Car::move);
+            outputHandler.printStatus(cars);
+        }
+        outputHandler.printWinner(getWinners());
+    }
+
+    private List<Car> getWinners() {
+        int maxPosition = cars.stream().mapToInt(Car::getPosition).max().orElse(0);
+
+        return cars.stream().filter(car -> car.getPosition() == maxPosition).toList();
+    }
+}
