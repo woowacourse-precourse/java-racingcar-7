f4814bf6bf44e4338a6e736c8198f92b62b90a1e
ParkHyunS00 <parkhyuns00@naver.com>
2024-10-23 16:44:55 +0900

    feat(InputView): InputView 클래스 내의 메서드에서 경주할 자동차 이름을 입력할 것을 출력

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,12 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.utils.Constant;
+
 public class InputView {
+
+    public List<String> getCarNames() {
+        System.out.println(Constant.CAR_NAME_INPUT_MESSAGE);
+    }
 }
