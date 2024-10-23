a495bc29055caab2f76051c07b4fed88e6e96b7a
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-23 19:11:11 +0900

    feat : 자동차 이름 유효검사

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,10 +1,19 @@
 package racingcar.domain;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 이름 유효검사
+ */
 public class Car {
     private final String name;
     private int distance = 0;
 
     public Car(String name) {
+        validateNameEmpty(name);
+        validateNameLength(name);
+        validateNameSpecialCharacter(name);
         this.name = name;
     }
 
@@ -18,6 +27,27 @@ public class Car {
 
     public int getDistance() {
         return distance;
+    }
+
+
+    private void validateNameLength(String name) {
+        if (name.length() > 6) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하까지 입력 가능합니다.");
+        }
+    }
+
+    private void validateNameEmpty(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("공백으로 입력된 이름이 있습니다.");
+        }
+    }
+
+    private void validateNameSpecialCharacter(String name) {
+        Matcher matcher = Pattern.compile("[ !@#$%^&*(),.?\":{}|<>]").matcher(name);
+        if (matcher.find()) {
+            throw new IllegalArgumentException("자동차 이름에 특수문자는 사용 불가합니다.");
+        }
+
     }
 
 }
