ab85a0d50e7879753e949e2a6456a72e178b5624
JunBe <wnsqowid0901@gmail.com>
2024-10-23 21:47:43 +0900

    feat: 출력 클래스 생성

--- src/main/java/racingcar/io/Output.java
+++ src/main/java/racingcar/io/Output.java
@@ -1,4 +1,15 @@
 package racingcar.io;
 
+import java.util.Map;
+
 public class Output {
+    //실행 결과 출력을 여기다 추가해야 하나?
+
+    public void gameResult(Map<String, Integer> carMap) {
+
+    }
+
+    public void winner() {
+
+    }
 }
