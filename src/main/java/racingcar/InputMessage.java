15914560e30a3c44ffb616523775b399d46e3feb
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-10-24 04:19:17 +0900

    feat(InputMessage): validateInput메서드 예외 처리 추가
    - 자동차 이름 입력 시 null값이 입력될 경우 예외 발생
    - 자동차 이름 입력 시 빈 칸이 입력될 경우 예외 발생
    - 자동차 이름 입력 시 1대만 입력할 경우 예외 발생

--- src/main/java/racingcar/InputMessage.java
+++ src/main/java/racingcar/InputMessage.java
@@ -2,34 +2,31 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.regex.Pattern;
-import org.junit.jupiter.api.DisplayName;
 
 public class InputMessage {
     private static final Pattern INVALID_PATTERN = Pattern.compile("[^a-zA-Z0-9]");
+    public static final int CAR_NAME_LIMITS = 5;
 
-    public String inputCarName() {
+    public String[] inputCarNameReturnSplit(String separator) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+        String inputCarName =  Console.readLine();
+
+        String[] splitCarNames = inputCarName.split(separator);
+        validateInput(splitCarNames);
+
+        return splitCarNames;
     }
 
     public String inputNumberOfTries(){
         System.out.println("시도할 횟수는 몇 회인가요?");
         return Console.readLine();
     }
-
-    public String[] splitCarName(String inputCarName){
-        String[] carNames = inputCarName.split(",");
-        validateInput(carNames);
-
-        return carNames;
-    }
     
     public void validateInput(String[] carNames){
-        int carNameLimits = Constants.CAR_NAME_LIMITS;
         
         for (String carName : carNames) {
             // 자동차 이름이 5자 초과 시 예외 발생
-            if (carName.trim().length() > carNameLimits) {
+            if (carName.trim().length() > CAR_NAME_LIMITS) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다: " + carName);
             }
             
@@ -37,6 +34,21 @@ public class InputMessage {
             if (INVALID_PATTERN.matcher(carName.trim()).find()) {
                 throw new IllegalArgumentException("자동차 이름은 영대소문자와 숫자 밖에 안 됩니다.");
             }
+            
+            // null값 입력 금지
+            if (carName.isEmpty()){
+                throw new IllegalArgumentException("아무것도 입력하지 않으시면 게임이 시작되지 않습니다.");
+            }
+
+            // 빈 칸 입력 금지
+            if (carName.contains(" ")){
+                throw new IllegalArgumentException("빈 칸을 입력하지 마세요.");
+            }
+        }
+
+        // 자동차를 2대 이상 입력했는 지 테스트
+        if (carNames.length < 2){
+            throw new IllegalArgumentException("자동차를 두 대 이상 입력하셔야 합니다.");
         }
     }
 }
---
+++ src/test/java/MyTest/MyApplicationTest.java
@@ -0,0 +1,69 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+public class MyApplicationTest {
+//    public final InputMessage inputMessage;
+//
+//    public InputMessageTest(InputMessage inputMessage) {
+//        this.inputMessage = inputMessage;
+//    }
+//
+//    @Test
+//    public void 정상적으로_실행() {
+//        InputMessage inputMessage = new InputMessage();
+//
+//        assertDoesNotThrow(() -> {
+//            inputMessage.splitCarName("a, ab, abc, abcd, abcde", ",");
+//        });
+//
+//        assertDoesNotThrow(() -> {
+//            inputMessage.splitCarName("a3, B2", ",");
+//        });
+//    }
+//
+//    @Test
+//    void 자동차_이름이_5자를_초과하지_않는_지_테스트(){
+//        InputMessage inputMessage = new InputMessage();
+//
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            inputMessage.splitCarName("abc, abcd, abcdef", ",");
+//        });
+//    }
+//
+//    @Test
+//    void 자동차_이름에_문자와_숫자만_들어있는_지_테스트(){
+//        InputMessage inputMessage = new InputMessage();
+//
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            inputMessage.splitCarName("abc, ab/", ",");
+//        });
+//
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            inputMessage.splitCarName("abc, ab d", ",");
+//        });
+//    }
+
+//    @Test
+//    void 자동차가_두_대_이상_입력됐는_지_테스트(){
+//        InputMessage inputMessage = new InputMessage();
+//
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            inputMessage.splitCarName("", ",");
+//        });
+//
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            inputMessage.splitCarName(" ", ",");
+//        });
+//
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            inputMessage.splitCarName("abc", ",");
+//        });
+//
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            inputMessage.splitCarName("abc,", ",");
+//        });
+//    }
+}
