50d980138f3e1c6f372df76f20bc5505cf3c7f76
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-10-24 01:19:06 +0900

    feat(Car): 자동차 이동 함수 작성
    - 멤버 변수 currentPositon 추가
    - move() 함수 작성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,10 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String carName;
+    private int currentPosition;
 
     public Car(String carName) {
         this.carName = carName;
+        currentPosition = 0;
+    }
+
+    public void move() {
+        System.out.print(carName + " : ");
+        for (int i = 0; i < currentPosition; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 
     public String getCarName() {
