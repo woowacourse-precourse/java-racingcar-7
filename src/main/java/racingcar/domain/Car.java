bf28175b62cc658f68d82aff2edab3f97cb1b253
변우영 <likevangogh24@gmail.com>
2024-10-23 15:50:23 +0900

    feat(Car): 자동차 이름 검증 메서드 추가
    - 자동차 이름이 5자 이하가 아닐 경우, 예외를 발생시킨다.
    - 자동차 이름이 빈 문자열일 경우, 예외를 발생시킨다. (ex. "")
    - 자동차 이름에 공백이 들어갈 경우, 예외를 발생시킨다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -8,6 +8,7 @@ public class Car {
     private int distance;
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
         this.distance = 0;
     }
@@ -15,6 +16,30 @@ public class Car {
     public void move() {
         if (Randoms.pickNumberInRange(0, 9) >= 4) {
             distance += 1;
+        }
+    }
+
+    private void validateName(String name) {
+        validateNameLength(name);
+        validateNonBlankName(name);
+        validateNameWithoutWhiteSpace(name);
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
+    }
+
+    private void validateNonBlankName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름에 빈 문자열은 허용되지 않습니다.");
+        }
+    }
+
+    private void validateNameWithoutWhiteSpace(String name) {
+        if (name.contains(" ")) {
+            throw new IllegalArgumentException("자동차 이름에 공백이 포함될 수 없습니다.");
         }
     }
 }
