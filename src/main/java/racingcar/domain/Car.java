c6c8e863a2a8eec4e5bdcbe10057c270c107fb20
DoomchitYJ <poj1004@inha.edu>
2024-10-23 17:40:02 +0900

    feat: 무작위 값을 추출하고 전진 조건에 부합할 경우 승리 횟수를 1 높이는 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,9 +1,13 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
     private static final int START_COUNT = 0;
     private static final int DEFAULT_RANDOM_NUMBER = 0;
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
 
     private final String name;
     private int randomNumber;
@@ -15,4 +19,18 @@ public class Car {
         this.winCount = START_COUNT;
     }
 
+    public void move() {
+        randomNumber = makeRandomNumber();
+        if (randomNumber > 3) {
+            winCount++;
+        }
+    }
+
+    private int makeRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+    }
+
+    public int getWinCount() {
+        return winCount;
+    }
 }
