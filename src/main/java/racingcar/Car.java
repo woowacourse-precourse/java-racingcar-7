3079d62f9e2da4ab00ab80b1750f5105ef9f1885
alpin87 <qortmdals94@naver.com>
2024-10-23 19:58:16 +0900

    feat: Car 클래스의 자동차 이름 유효성 검증 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,20 @@
 package racingcar;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String ERROR_NAME_LENGTH = "자동차 이름은 5자 이하여야 합니다.";
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+        this.position = 0;
+    }
+    private void validateName(String name) {
+        if (name == null || name.trim().isEmpty() || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(ERROR_NAME_LENGTH);
+        }
+    }
 }
