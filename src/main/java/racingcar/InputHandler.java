d7956c6de773624708d5ae42932082516b2fc906
howareyoo <newworld121@naver.com>
2024-10-24 00:02:28 +0900

    feat : 경주에 참가할 자동차 이름 입력

---
+++ src/main/java/racingcar/InputHandler.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+
+    public static String enterCarName(){
+        return Console.readLine();
+    }
+
+}
