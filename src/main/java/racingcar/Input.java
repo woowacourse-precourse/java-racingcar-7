405bb56032db40a67b3fd01b417f4c6d96ca169d
eunho <eunho1032@gmail.com>
2024-10-24 03:33:22 +0900

    feat(Input): 상수로 입력 메시지 분리

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -4,21 +4,24 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
-	public String[] inputCarNames() {
+	private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String INPUT_TRY_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
-		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+	public static String[] inputCarNames() {
+
+		System.out.println(INPUT_NAME_MESSAGE);
 		String[] carNames = splitCarNames(Console.readLine());
 		Validation.validateCarNames(carNames);
 		return carNames;
 	}
 
-	private String[] splitCarNames(String input) {
+	private static String[] splitCarNames(String input) {
 		return input.split(",");
 	}
 
-	public int inputTryCounts() {
-		System.out.println("시도할 횟수는 몇 회인가요?");
+	public static int inputTryCounts() {
+		System.out.println(INPUT_TRY_MESSAGE);
 		String input = Console.readLine();
-		return 0;
+		return Validation.validateTryCounts(input);
 	}
 }
