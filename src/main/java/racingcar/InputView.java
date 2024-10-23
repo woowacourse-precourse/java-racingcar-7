9988c41b36b837ab0597bb9d704b51bfafc96572
BaSak0630 <babamba125@gmail.com>
2024-10-24 03:27:19 +0900

    fix : Fix InputView readLine
    공백입력 시 예외발생 처리 오류 수정

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -6,7 +6,8 @@ public class InputView {
 
     public String readLine() {
         String readLine = Console.readLine();
-        if(readLine.isEmpty()){
+        String trim = readLine.trim();
+        if(trim.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         }
         return readLine;
