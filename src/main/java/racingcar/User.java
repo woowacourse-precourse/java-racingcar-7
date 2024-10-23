9d8e5440691624d487e9f2af196347303c23e293
mipangg <morganjeong119@gmail.com>
2024-10-23 18:13:20 +0900

    <feat>(User): Store user car name and results of each round

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -1,4 +1,13 @@
 package racingcar;
 
+import racingcar.RacingCar;
+
 public class User {
+    String name;
+    Boolean result[];
+
+    User(String name){
+        this.name = name;
+        result = null;
+    }
 }
