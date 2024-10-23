dff606cb68680c882b2fee68b3789650fb8ba38f
yongminKwon1 <ym.kwon@lipis.kr>
2024-10-24 03:31:20 +0900

    feat(StringParser): 빈 문자열 관련 예외 처리 로직 추가
    빈 문자열 관련 예외처리 로직을 추가하고, 관련 테스트 코드를 추가했습니다.
    예외처리 내용:
    - 입력값 및 구분자가 비어있을 때 IllegalArgumentException 발생
    - 차동차 이름으로 공백문자만 들어올 때 IllegalArgumentException 발생

--- src/main/java/racingcar/StringParser.java
+++ src/main/java/racingcar/StringParser.java
@@ -5,8 +5,27 @@ import java.util.List;
 
 public class StringParser {
     static List<String> parse(String input, String delimiter) {
-        return Arrays.stream(input.split(delimiter))
-                .map(String::strip) // 각 요소의 앞뒤 공백 제거
-                .toList();
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("입력값이 비어있습니다.");
+        }
+        if (delimiter.isEmpty()) {
+            throw new IllegalArgumentException("구분자가 비어있습니다.");
+        }
+
+        List<String> result = Arrays.stream(input.split(delimiter))
+                    .map(String::strip)
+                    .toList();
+
+        // 빈 문자열만 들어온 경우
+        if (result.isEmpty()) {
+            throw new IllegalArgumentException("빈 문자열은 자동차 이름으로 사용할 수 없습니다.");
+        }
+        // 빈 문자열과 쉼표만 들어온 경우
+        boolean hasEmptyName = result.stream().anyMatch(String::isEmpty);
+        if (hasEmptyName) {
+            throw new IllegalArgumentException("빈 문자열은 자동차 이름으로 사용할 수 없습니다.");
+        }
+
+        return result;
     }
 }
--- src/test/java/racingcar/StringParserTest.java
+++ src/test/java/racingcar/StringParserTest.java
@@ -1,6 +1,8 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -9,15 +11,15 @@ public class StringParserTest {
     @Test
     void 쉼표_기준으로_문자열을_분리() {
         // Given
-        String input = "pobi,woni";
+        String input = "pobi,woni,jun";
         String delimiter = ",";
 
         // When
         List<String> result = StringParser.parse(input, delimiter);
 
         // Then
-        assertThat(result).hasSize(2);
-        assertThat(result).containsExactly("pobi", "woni");
+        assertThat(result).hasSize(3);
+        assertThat(result).containsExactly("pobi", "woni","jun");
     }
 
     @Test
@@ -46,5 +48,59 @@ public class StringParserTest {
         // Then
         assertThat(result).hasSize(2);
         assertThat(result).containsExactly("pobi", "woni");
+    }
+
+    @Test
+    void 빈_입력값() {
+        // Given
+        String input = "";
+        String delimiter = ",";
+
+        // When & Then
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> StringParser.parse(input, delimiter))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("입력값이 비어있습니다.")
+        );
+    }
+
+    @Test
+    void 빈_구분자() {
+        // Given
+        String input = "woni";
+        String delimiter = "";
+
+        // When & Then
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> StringParser.parse(input, delimiter))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("구분자가 비어있습니다.")
+        );
+    }
+
+    @Test
+    void 입력으로_공백문자만_들어옴() {
+        // Given
+        String input = "   ";
+        String delimiter = ",";
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> StringParser.parse(input, delimiter))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("빈 문자열은 자동차 이름으로 사용할 수 없습니다.")
+        );
+    }
+
+    @Test
+    void 입력으로_공백문자와_쉼표만_들어옴() {
+        // Given
+        String input = " ,  ,";
+        String delimiter = ",";
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> StringParser.parse(input, delimiter))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("빈 문자열은 자동차 이름으로 사용할 수 없습니다.")
+        );
     }
 }
