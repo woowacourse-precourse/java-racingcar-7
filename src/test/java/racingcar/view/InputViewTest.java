c73df56a59c843c51bce390f6b1941d8d84f8b24
chaeshee0908 <chaeshee0908@naver.com>
2024-10-24 00:42:30 +0900

    test(InputView): 통합 테스트 시 2번째 테스트부터 에러 발생 해결
    issue) 테스트 환경에서 System.setIn을 통해 가상 입력으로 변경하는 와중
    두번째 테스트코드부터는 이전의 입력 스트림이 제대로 초기화되지 않고
    유지되었기 때문에 제대로 값을 읽어오지 못하는 이슈 발생
    
    solve) Console 라이브러리를 확인해보니 close() 함수 발견
    AfterEach를 통해 하나의 테스트 이후 매번 원래의 System.in을 복원 해준 후
    Console.close()를 통해 Scanner를 종료하여 초기화 해줌

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -10,9 +12,18 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 public class InputViewTest {
 
+    private final InputStream originalIn = System.in;   // 원래의 System.in 저장
+
+    @AfterEach
+    void restoreSystemIn() {
+        // 각 테스트 이후 원래 System.in 복원 및 Scanner 종료
+        System.setIn(originalIn);
+        Console.close();    // Scanner를 종료하여 초기화
+    }
+
     @Test
     void 시도_횟수_양수_입력시_정상_작동() {
-        String validInput = "5";
+        String validInput = "5\n";
         InputStream inputStream = new ByteArrayInputStream(validInput.getBytes());
         System.setIn(inputStream);
         int moveNumber = InputView.readMoveNumber();
@@ -22,7 +33,7 @@ public class InputViewTest {
     @Test
     void 시도_횟수_입력값이_문자일때_예외_발생() {
         try {
-            String invalidInput = "five";
+            String invalidInput = "five\n";
             InputStream inputStream = new ByteArrayInputStream(invalidInput.getBytes());
             System.setIn(inputStream);
             int moveNumber = InputView.readMoveNumber();
@@ -35,7 +46,7 @@ public class InputViewTest {
     @Test
     void 시도_횟수_입력값이_0일때_예외_발생() {
         try {
-            String invalidInput = "0";
+            String invalidInput = "0\n";
             InputStream inputStream = new ByteArrayInputStream(invalidInput.getBytes());
             System.setIn(inputStream);
             int moveNumber = InputView.readMoveNumber();
