7436d496c2cbad45a67ef1d14904572c10bdc336
posite <su84053@gmail.com>
2024-10-23 11:26:49 +0900

    chore(TextString): 출력에 필요한 String 정리
    사용자의 입력 유도, 분리, 실행 결과, 우승자 출력에 필요한 문자열들을 관리하기 위해 enum class를 구현하였습니다.

---
+++ src/main/java/racingcar/TextString.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+public enum TextString {
+    CAR_NAME_TEXT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    NAME_SEPARATOR(","),
+    TRY_COUNT_TEXT("시도할 횟수는 몇 회인가요?"),
+    RESULT_TEXT("실행 결과"),
+    FINAL_WINNERS_TEXT("최종 우승자 : ");
+
+
+    private final String value;
+
+    TextString(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
