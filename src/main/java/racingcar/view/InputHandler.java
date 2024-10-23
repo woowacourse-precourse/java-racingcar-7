c872d0281732b5d7b0eb8c8114f66dd0dc2eeadb
Hacanna42 <dtmdgus901@gmail.com>
2024-10-23 16:23:37 +0900

    refactor(InputHandler): add final to DI fields

--- src/main/java/racingcar/view/InputHandler.java
+++ src/main/java/racingcar/view/InputHandler.java
@@ -11,8 +11,8 @@ public class InputHandler {
     private static final String REQUEST_MESSAGE_CAR = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String REQUEST_MESSAGE_ROUND = "시도할 횟수는 몇 회인가요?";
 
-    private Validator validator;
-    private StringParser stringParser;
+    private final Validator validator;
+    private final StringParser stringParser;
 
     public InputHandler(Validator validator, StringParser stringParser) {
         this.validator = validator;
