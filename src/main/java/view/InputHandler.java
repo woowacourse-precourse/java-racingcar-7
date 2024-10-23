66913f87660f7c8f312835ca76cc6f5f7737a351
Wooseok Jang <wowseok2774@gmail.com>
2024-10-23 15:02:04 +0900

    feat: 자동차 이름과 시도 횟수 입력을 처리하는 InputHandler 추가

---
+++ src/main/java/view/InputHandler.java
@@ -0,0 +1,17 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+
+    public String inputNames() {
+        return Console.readLine();
+    }
+
+    public String inputAttemp() {
+        return Console.readLine();
+    }
+}
+
+
+
