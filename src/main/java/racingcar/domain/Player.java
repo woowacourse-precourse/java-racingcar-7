7fd5214fce7779b48021b0f360ff936210b97b54
choiminu <alsdn4590@gmail.com>
2024-10-23 18:16:29 +0900

    feat(domain): 자동차 위치 관리 기능 추가
    자동차의 현재 위치를 관리하는 `Position` 클래스를 추가하였고, 현재 사용하고 있지 않은 `Player` 클래스를 삭제하였음.

--- src/main/java/racingcar/domain/Player.java
+++
@@ -1,7 +0,0 @@
-package racingcar.domain;
-
-import racingcar.domain.player.Name;
-
-public class Player {
-    private Name name;
-}
---
+++ src/main/java/racingcar/domain/player/Position.java
@@ -0,0 +1,13 @@
+package racingcar.domain.player;
+
+public class Position {
+    private int currentPosition;
+
+    public Position() {
+        this.currentPosition = 0;
+    }
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+}
