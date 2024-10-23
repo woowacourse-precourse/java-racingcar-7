69fe68560eef615a57ca757bd51e9a74b6627366
Yoon-Jemin <yjm1800@naver.com>
2024-10-23 22:42:11 +0900

    docs(README): 기능 구현 목록 수정
    - 2번에 자동차와 자동차 게임 객체를 선언하고 자동차 게임 객체에 자동차 객체를 추가하는 로직 추가

---
+++ src/main/java/racingcar/service/CarService.java
@@ -0,0 +1,15 @@
+package racingcar.service;
+
+import racingcar.model.Car;
+import racingcar.model.CarGame;
+
+public class CarService {
+
+    private final CarGame carGame;
+
+    public CarService(CarGame carGame) {
+        this.carGame = carGame;
+    }
+
+
+}
