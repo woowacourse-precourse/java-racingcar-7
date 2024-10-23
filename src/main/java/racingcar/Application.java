359848e658acaee5650a6f11bbba326490452a56
Lys <redspinyant@gmail.com>
2024-10-23 17:22:18 +0900

    feat: 게임 진행 기능 구현
    - 게임 라운드 진행(각 차량별 전진 처리)
    - 게임 결과 처리(우승자)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.domain.controller.RaceController;
+import racingcar.domain.model.CarList;
+import racingcar.domain.view.RaceView;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		CarList carList = new CarList();
+		RaceView raceView = new RaceView();
+		RaceController raceController = new RaceController(carList, raceView);
+
+		raceController.run();
+	}
 }
--- src/main/java/racingcar/domain/controller/RaceController.java
+++ src/main/java/racingcar/domain/controller/RaceController.java
@@ -2,12 +2,15 @@ package racingcar.domain.controller;
 
 import java.util.Arrays;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.domain.model.Car;
 import racingcar.domain.model.CarList;
 import racingcar.domain.model.ErrorMessage;
 import racingcar.domain.view.RaceView;
 
 public class RaceController {
-
+	private static final int MIN_PICK_NUMBER = 0;
+	private static final int MAX_PICK_NUMBER = 9;
 	private static final String DEFAULT_SEPARATOR = ",";
 	private static final String EMPTY_INPUT_REGEX = ".*\\s+.*";
 	private static final int MAX_CAR_NAME_LENGTH = 5;
@@ -18,6 +21,15 @@ public class RaceController {
 	public RaceController(CarList carList, RaceView raceView) {
 		this.raceView = raceView;
 		this.carList = carList;
+	}
+
+	public void run() {
+		receiveCarNames();
+		int lap = receiveRaceLap();
+		for (int i = 0; i < lap; ++i) {
+			getLapResult();
+		}
+		getWinner();
 	}
 
 	public void receiveCarNames() {
@@ -35,6 +47,25 @@ public class RaceController {
 		validLapCount(lap);
 
 		return lap;
+	}
+
+	private void getLapResult() {
+		for (int i = 0; i < carList.getCount(); ++i) {
+			Car car = carList.getCar(i);
+			moveForwardTargetCar(car);
+		}
+	}
+
+	private int pickNumberInRange() {
+		return Randoms.pickNumberInRange(MIN_PICK_NUMBER, MAX_PICK_NUMBER);
+	}
+
+	private void moveForwardTargetCar(Car car) {
+		car.moveForward(pickNumberInRange());
+	}
+
+	private String getWinner() {
+		return carList.getWinner().orElse("");
 	}
 
 	private void validateInput(String input) {
