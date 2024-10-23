98679745f216026db5da47f6560ef039b192ae42
seongmin <0411tjdals34@gmail.com>
2024-10-24 07:12:00 +0900

    feat: 자동차 경주 후 결과 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -15,7 +15,23 @@ public class Application {
         }
 
         int T = Integer.parseInt(Console.readLine());
+        for (int i = 0; i < T; i++) {
+            for (Car car : cars) {
+                car.move();
+            }
+            printRaceResult(cars);
+        }
+    }
 
+    public static void printRaceResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.print(car.getName() + " : ");
+            for (int i = 0; i < car.getPosition(); i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 class Car {
     private String name;
+    private int position = 0;
 
     public Car(String name) {
         this.name = name;
@@ -9,5 +12,16 @@ class Car {
 
     public String getName() {
         return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        int randomValue = Randoms.pickNumberInRange(0, 9);
+        if (randomValue >= 4) {
+            position++;
+        }
     }
 }
