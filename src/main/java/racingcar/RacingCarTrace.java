2717e9c4c8c34fee3d9dbbf9d61dc53752940fc6
sunjong0214 <rlatjswhd8200@gmail.com>
2024-10-23 21:16:31 +0900

    feat(RacingCarTrace): RacingCarTrace put() 메서드 구현

--- src/main/java/racingcar/RacingCarTrace.java
+++ src/main/java/racingcar/RacingCarTrace.java
@@ -6,5 +6,7 @@ import java.util.Map;
 public class RacingCarTrace {
     private final Map<RacingCar, Integer> trace = new LinkedHashMap<>();
 
-
+    public void put(RacingCar racingCar) {
+        trace.put(racingCar, 0);
+    }
 }
