7a22bc1904c3b24932a2fc239ce98f83e50f360c
seongmin <0411tjdals34@gmail.com>
2024-10-23 14:51:59 +0900

    feat: 자동차 이름 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+        List<Car> cars = new ArrayList<>();
+        String[] carNames = input.split(",");
+        for (String name : carNames) {
+            cars.add(new Car(name.trim())); // name 앞뒤 공백 제거
+        }
+        System.out.println("입력된 자동차 이름:");
+        for (Car car : cars) {
+            System.out.println(car.getName());
+        }
     }
+
 }
