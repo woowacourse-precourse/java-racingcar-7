ada3b91a99a9eb445a2af29e370659953e1521c4
sonsuchang <son336208@naver.com>
2024-10-23 12:32:57 +0900

    refactor: <자동차 이름, 초기 위치(0)>을 HashMap에 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,22 +1,20 @@
 package racingcar;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 
-//- **몇 번의 이동을 할 지 사용자에게 입력받기**
-//        - 시도할 횟수는 몇 회인가요?
-//        - 10번 이하로 가정하고 입력받기
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        ArrayList<String> cars = new ArrayList<>(Arrays.asList(readLine().split(",")));
-        for (String car : cars){
-            if (car.length() > 5){
+        HashMap<String, Integer> carHashMap = new HashMap<>();
+        String[] carNameArray = readLine().split(",");
+        for (String carName : carNameArray) {
+            if (carName.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력 해야합니다.");
             }
+            carHashMap.put(carName, 0);
         }
 
         System.out.println("시도할 횟수는 몇 회인가요?");
