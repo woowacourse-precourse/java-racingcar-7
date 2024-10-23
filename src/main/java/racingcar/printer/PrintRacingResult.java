322f9745dcd88abb42befc1f6fdf6696bcd619b3
Sumin <new.sumyang@gmail.com>
2024-10-23 21:29:00 +0900

    refactor(printer): 출력 기능 일원화

--- src/main/java/racingcar/printer/PrintRacingResult.java
+++ src/main/java/racingcar/printer/PrintRacingResult.java
@@ -14,30 +14,29 @@ public class PrintRacingResult implements Printer {
     }
 
     @Override
-    public void printResultAfterGame(Result afterGameCars) {
-        System.out.println("실행 결과");
-        for (Cars cars : afterGameCars.finalResultCars()) {
-            System.out.println(makeResultOutput(cars));
+    public void printResultAfterGame(Result result) {
+        print("실행 결과");
+        for (Cars cars : result.finalResultCars()) {
+            print(makeResultOutput(cars));
         }
-        printFinalWinner(afterGameCars.findWinners());
-    }
-
-    private void printFinalWinner(List<String> winner) {
-        String finalWinner = "최종 우승자 : "
-                + String.join(", ", winner);
-        System.out.println(finalWinner);
+        printFinalWinner(result.findWinners());
     }
 
     private String makeResultOutput(Cars cars) {
         StringBuilder resultString = new StringBuilder();
         for (Car car : cars.allCar()) {
-            resultString.append(makeCarDistanceResult(car));
-            resultString.append('\n');
+            resultString.append(makeCarDistanceResult(car)).append('\n');
         }
         return resultString.toString();
     }
 
     private String makeCarDistanceResult(Car car) {
         return car.name() + " : " + "-".repeat(Math.max(0, car.distance()));
+    }
+
+    private void printFinalWinner(List<String> winners) {
+        String finalWinner = "최종 우승자 : "
+                + String.join(", ", winners);
+        print(finalWinner);
     }
 }
