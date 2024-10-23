290a4271369912460736138579d23d4f25b86913
dongyun <ehddbs452100@naver.com>
2024-10-23 22:24:04 +0900

    feat(RacingService): validate trying counts
    - 시도 횟수가 정수형 데이터인지 정규식을 이용해 코드의 양을 줄여 편리
    - 정수이면 0보다 작거나 같으면 false를 반환하여 시도횟수 입력 관련 오류가 발생했다는걸 알림

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -2,15 +2,18 @@ package racingcar.service;
 
 import static racingcar.global.error.Error.NAME_BLANK_EXCEPION;
 import static racingcar.global.error.Error.NAME_LENGTH_EXCEPION;
+import static racingcar.global.error.Error.TRY_COUNT_EXCEPION;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.global.error.NameError;
+import racingcar.global.error.TryCountError;
 
 public class RacingService {
 
     private final String COMMA = ",";
+    private String POSITIVE_NUMBER_REG = "\\d+";
 
     public List<String> splitCarName(String input) {
         return Arrays.stream(input.split(COMMA)).collect(Collectors.toList());
@@ -26,6 +29,12 @@ public class RacingService {
         }
     }
 
+    public void validateTryCount(String count) {
+
+        if (!validatePositiveIntegerCount(count)) {
+            throw new TryCountError(TRY_COUNT_EXCEPION);
+        }
+    }
     private boolean validateNameNotNull(List<String> carNames) {
 
         return carNames.stream().allMatch(name -> name != null && !name.isEmpty());
@@ -35,4 +44,10 @@ public class RacingService {
         return carNames.stream().allMatch(name -> name.length() <= 5);
     }
 
+    private boolean validatePositiveIntegerCount(String count) {
+        if (count.matches(POSITIVE_NUMBER_REG)) {
+            return Integer.parseInt(count) > 0;
+        }
+        return false;
+    }
 }
