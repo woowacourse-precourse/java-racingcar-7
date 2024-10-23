7bedaadd50a69e1b56114d9b41d5cb58b869b3eb
chaerish <ccyy8432@naver.com>
2024-10-23 23:03:42 +0900

    test(InputViewTest): 사용자 입력 유효성 검사 메소드 생성
    InputView의 사용자 인증을 검증하는 유효성 검사 메소드 테스트를 생성한다.

---
+++ src/test/java/racingcar/InputViewTest.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+    private InputView inputView;
+
+    @BeforeEach
+    public void setUp(){
+        this.inputView = new InputView();
+    }
+    @Test
+    public void 차이름_유효성_검사(){
+        boolean isValidateCarName = inputView.isValidateCarName("차이름");
+        assertThat(isValidateCarName).isTrue();
+    }
+    @Test
+    public void 시도숫자_유효성_검사(){
+        boolean isValidateTryNum = inputView.isValidateTryNum("1");
+        assertThat(isValidateTryNum).isTrue();
+    }
+
+}
