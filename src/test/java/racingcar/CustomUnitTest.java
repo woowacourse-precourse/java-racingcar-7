bd5acd2e61b1f82d0757eeae7e333c3de75614fa
Kiki1875b <kevinheo0413@gmail.com>
2024-10-23 11:47:31 +0900

    test(CustomUnitTest): add test for Randoms.pickNumberInRange()

---
+++ src/test/java/racingcar/CustomUnitTest.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CustomUnitTest {
+
+    @Test
+    void CHECK_RANDOM_GENERATOR(){
+        int randomNumber = Randoms.pickNumberInRange(8,9);
+        assertThat(randomNumber).isBetween(8,9);
+    }
+    
+}
