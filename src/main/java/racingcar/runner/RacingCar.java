0e31962fdcae14ec4e4a4544123732f57938287e
jenna <shinysuh1992@gmail.com>
2024-10-23 17:25:20 +0900

    feat: 자동차 이름, 시도 횟수 입력 및 실행 클래스 호출부 구현

---
+++ src/main/java/racingcar/runner/RacingCar.java
@@ -0,0 +1,16 @@
+package racingcar.runner;
+
+public class RacingCar {
+
+    private final String carString;
+    private final String roundString;
+
+    public RacingCar(String carString, String roundString) {
+        this.carString = carString;
+        this.roundString = roundString;
+    }
+
+    public String run() {
+        return "";
+    }
+}
