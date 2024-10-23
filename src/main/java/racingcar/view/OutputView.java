c57b603d7bbc800b3ae8da63d7d2bf23fee314a4
dnwldla <leeemjw@naver.com>
2024-10-24 01:54:14 +0900

    feat: 최종 결과를 출력하는 view 함수 작성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,7 +4,7 @@ public class OutputView {
     private static final String START_COMMAND="경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String RACING_COUNTS="시도할 횟수는 몇 회인가요?";
     private static final String RACING_PROCESS="실행 결과";
-    private static final String RACING_WINNER="결과 : %s";
+    private static final String RACING_WINNER="최종 우승자 : %s";
 
     public static void printStartCommand(){
         System.out.println(START_COMMAND);
@@ -15,10 +15,11 @@ public class OutputView {
     }
 
     public static void printRacingProcess(){
+        System.out.println();
         System.out.println(RACING_PROCESS);
     }
 
-    public static void printRacingWinner(){
-        System.out.println(RACING_WINNER);
+    public static void printRacingWinner(String result){
+        System.out.printf(RACING_WINNER,result);
     }
 }
