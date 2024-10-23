a21c0f8f54b9f4afdcc7016cd35c5dcdb803bdd9
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-10-23 21:41:01 +0900

    feat($view) : Get initial inputs
    초기 사용자 입력 받아주는 기능 제작

---
+++ src/main/java/view/ViewScreen.java
@@ -0,0 +1,16 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
+public class ViewScreen {
+    public List<String> getInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNamesInput = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String runTimes = Console.readLine();
+        return Arrays.asList(carNamesInput, runTimes);
+    }
+
+}
