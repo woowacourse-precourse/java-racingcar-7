317163df6116b457e5e1e03c9aa649eef0dc0a06
Seo Jinmann <sjmmics@gmail.com>
2024-10-23 14:41:22 +0900

    feat : ($ConsoleInputView) 입력값 검증 기능 수정 및 추가
    시도할 횟수 검증하는 기능에 0 또는 아무런 입력이 없는 경우 잘못된 입력으로 IllegalArgumentException를 호출함

--- src/main/java/racingcar/view/ConsoleInputView.java
+++ src/main/java/racingcar/view/ConsoleInputView.java
@@ -17,30 +17,52 @@ public class ConsoleInputView implements InputView {
     @Override
     public String getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return validateNameFormat(Console.readLine());
+        String carNamesToValidate = Console.readLine();
+        validateNameFormat(carNamesToValidate);
+        return carNamesToValidate;
     }
 
     @Override
     public int getChanceToMove() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        return validateNumberFormat(Console.readLine());
+        String chanceInputToValidate = Console.readLine();
+        validateNumberFormat(chanceInputToValidate);
+        return Integer.parseInt(chanceInputToValidate);
     }
 
-    private String validateNameFormat(String nameToValidate) {
+    private void validateNameFormat(String nameToValidate) {
         Matcher matcher = carNamePattern.matcher(nameToValidate);
-        if (matcher.matches()) {
-            return nameToValidate;
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("[ERROR] 올바른 자동차 이름 형식이 아닙니다.");
         }
-        throw new IllegalArgumentException("[ERROR] 올바른 자동차 이름 형식이 아닙니다.");
     }
 
-    private int validateNumberFormat(String numberToValidate) {
-        int chance = 0;
-        try {
-            chance = Integer.parseInt(numberToValidate);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 숫자가 아니거나 int 범위를 벗어납니다.");
-        }
-        return chance;
+    private void validateNumberFormat(String numberToValidate) {
+        validateNotBlank(numberToValidate);
+        validateOnlyDigit(numberToValidate);
+        validateNotZero(numberToValidate);
     }
+
+    private void validateNotBlank(String numberToValidate) {
+        if (numberToValidate.isEmpty()) {
+            throw new IllegalArgumentException("문자열이 공백입니다. 숫자를 입력하세요.");
+        }
+    }
+
+    private void validateOnlyDigit(String numberToValidate) {
+        try {
+            Integer.parseInt(numberToValidate);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아니거나 int 범위를 벗어납니다.");
+        }
+    }
+
+    private void validateNotZero(String numberToValidate) {
+        int number = Integer.parseInt(numberToValidate);
+        if (number == 0) {
+            throw new IllegalArgumentException("[ERROR] 시도할 횟수는 양수이어야 합니다.");
+        }
+    }
+
+
 }
