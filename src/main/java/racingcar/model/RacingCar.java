95d1d1f317fac896e2f0e21991d28f5e83384ed9
KNU-K <knukang334@gmail.com>
2024-10-24 03:54:45 +0900

    feat(model) : RacingCar 모델 생성
    - Drivable 인터페이스의 구현체임.
    - 이동에 대한 유무를 Randoms.pickNumberInRange를 통해서 구분함.

---
+++ src/main/java/racingcar/model/RacingCar.java
@@ -0,0 +1,39 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RacingCar implements Drivable {
+
+    private static final int INITIAL_DISTANCE = 0;
+    private static final int MOVE_THRESHOLD = 4;
+
+    private final String name;
+    private int distance;
+
+    public RacingCar(String name) {
+        this.name = name;
+        this.distance = INITIAL_DISTANCE;
+    }
+
+    @Override
+    public int getDistance() {
+        return distance;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void move() {
+        if (canMoveForward()) {
+            distance++;
+        }
+    }
+
+    private boolean canMoveForward() {
+        int randomValue = Randoms.pickNumberInRange(0, 9);
+        return randomValue >= MOVE_THRESHOLD;
+    }
+}
