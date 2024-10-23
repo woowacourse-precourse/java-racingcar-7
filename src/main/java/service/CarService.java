a9b25fedb63faa861f3ad8c690a0632533b67b99
daxxn <dahyeonhan4529@gmail.com>
2024-10-23 23:50:11 +0900

    feat(service): add service class to manage car operations
    - Added CarService class for handling car-related logic
    - Included methods for checking car existence, validating car names, and retrieving winner
    - Implemented a constructor for dependency injection of CarList

---
+++ src/main/java/service/CarService.java
@@ -0,0 +1,63 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Car;
+import model.CarList;
+
+public class CarService {
+    private CarList carList;
+
+    public CarService() {
+        carList = new CarList();
+    }
+
+    public CarService(CarList carList) {
+        this.carList = carList;
+    }
+
+    public boolean isCarExist(String newCarName) {
+        for (Car car : carList.getCars()) {
+            String existingName = car.getName();
+            if (existingName.equals(newCarName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean validateCarName(String newCarName) {
+        if (!isCarExist(newCarName)) {
+            int carNameLength = newCarName.length();
+            return carNameLength > 0 && carNameLength <= 5;
+        }
+        return false;
+    }
+
+    public List<String> getWinners() {
+
+        int winnerDistance = getMaxDistance();
+
+        List<String> winners = new ArrayList<>();
+        for (Car car : carList.getCars()) {
+            if (car.getDistance() == winnerDistance) {
+                winners.add(car.getName());
+            }
+        }
+
+        return winners;
+    }
+
+    public int getMaxDistance() {
+        int maxDistance = 0;
+
+        for (Car car : carList.getCars()) {
+            int distance = car.getDistance();
+            if (distance > maxDistance) {
+                maxDistance = distance;
+            }
+        }
+        return maxDistance;
+    }
+
+}
