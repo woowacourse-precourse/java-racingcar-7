3aa5c1160796913bdfedaa809bcbb9a28660a387
bowook <mbosx316@naver.com>
2024-10-23 21:42:15 +0900

    refactor(TrialCounts) : 시도 횟수 검증 기능 분리
    TrialCounts에서 수행하던 검증을 TrialCountsValidator로 분리하였습니다

--- src/main/java/racingcar/domain/TrialCounts.java
+++ src/main/java/racingcar/domain/TrialCounts.java
@@ -3,30 +3,12 @@ package racingcar.domain;
 public class TrialCounts {
     private final int trialCounts;
 
-    public TrialCounts(String trialCounts) {
-        validateProcess(trialCounts);
+    public TrialCounts(String trialCounts, TrialCountsValidator trialCountsValidator) {
+        trialCountsValidator.validate(trialCounts);
         this.trialCounts = Integer.parseInt(trialCounts);
     }
 
-    private void validateProcess(String trialCounts) {
-        int counts = validateIsNumber(trialCounts);
-        validateIsZero(counts);
-    }
-
-    private int validateIsNumber(String trialCounts) {
-        int tempCounts;
-        try {
-            tempCounts = Integer.parseInt(trialCounts);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
-        }
-
-        return tempCounts;
-    }
-
-    private void validateIsZero(int counts) {
-        if(counts <= 0) {
-            throw new IllegalArgumentException();
-        }
+    public int getTrialCounts() {
+        return trialCounts;
     }
 }
