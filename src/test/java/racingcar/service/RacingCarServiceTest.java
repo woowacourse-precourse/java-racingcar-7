9ed03b6dc6baa64369ba4bacff13adbe98bddf08
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-10-24 01:12:17 +0900

    test(RacingCarService): 시도 횟수만큼 경기를 ë반복했는지 확인하는 테스트 코드 작성

---
+++ src/test/java/racingcar/service/RacingCarServiceTest.java
@@ -0,0 +1,34 @@
+package racingcar.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingCarServiceTest {
+
+    private RacingCarService racingCarService;
+
+    @BeforeEach
+    void setUp() {
+        racingCarService = new RacingCarService();
+    }
+
+    @Test
+    void 시도_횟수만큼_경기_반복() {
+        // given
+        List<Car> cars = Arrays.asList(new Car("car1"));
+        Race race = new Race(cars, 0);
+        int attemptCount = 5;
+
+        // when
+        racingCarService.startRace(race, attemptCount);
+
+        // then
+        assertThat(race.getNumOfMatches()).isEqualTo(attemptCount);
+        assertThat(cars.get(0).getPosition()).isBetween(0, attemptCount);
+    }
+}
