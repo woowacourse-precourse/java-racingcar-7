901c81296e8bc273566b566c479ceccb915f4227
seunghee <seunghee_chae3@tmax.co.kr>
2024-10-23 18:34:54 +0900

    feat(InputView): 시도 횟수 양수인지 유효성 검사

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,6 +7,8 @@ public class InputView {
     private static final String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String MOVE_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
+    private static final String POSITIVE_NUMBER_REGEX = "^[1-9]\\d*$";
+
     public static void printCarInputMessage() {
         System.out.println(CAR_INPUT_MESSAGE);
     }
@@ -21,6 +23,13 @@ public class InputView {
 
     public static int readMoveNumber() {
         String input = Console.readLine();
+        validatePositiveNumber(input);
         return Integer.parseInt(input);
+    }
+
+    private static void validatePositiveNumber(String input) {
+        if (!input.matches(POSITIVE_NUMBER_REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 양수만 입력 가능합니다.");
+        }
     }
 }
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,4 +1,48 @@
 package racingcar.view;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class InputViewTest {
+
+    @Test
+    void 시도_횟수_양수_입력시_정상_작동() {
+        String validInput = "5";
+        InputStream inputStream = new ByteArrayInputStream(validInput.getBytes());
+        System.setIn(inputStream);
+        int moveNumber = InputView.readMoveNumber();
+        assertEquals(5, moveNumber);
+    }
+
+    @Test
+    void 시도_횟수_입력값이_문자일때_예외_발생() {
+        try {
+            String invalidInput = "five";
+            InputStream inputStream = new ByteArrayInputStream(invalidInput.getBytes());
+            System.setIn(inputStream);
+            int moveNumber = InputView.readMoveNumber();
+            fail("테스트에 실패하였습니다.");
+        } catch (IllegalArgumentException e) {
+            assertEquals("[ERROR] 시도 횟수는 양수만 입력 가능합니다.", e.getMessage());
+        }
+    }
+
+    @Test
+    void 시도_횟수_입력값이_0일때_예외_발생() {
+        try {
+            String invalidInput = "0";
+            InputStream inputStream = new ByteArrayInputStream(invalidInput.getBytes());
+            System.setIn(inputStream);
+            int moveNumber = InputView.readMoveNumber();
+            fail("테스트에 실패하였습니다.");
+        } catch (IllegalArgumentException e) {
+            assertEquals("[ERROR] 시도 횟수는 양수만 입력 가능합니다.", e.getMessage());
+        }
+    }
+
 }
