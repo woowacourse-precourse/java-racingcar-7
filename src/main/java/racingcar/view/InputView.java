7286348c2f8129613c01fea4e800c768fae38576
seunghee <seunghee_chae3@tmax.co.kr>
2024-10-23 16:43:58 +0900

    feat(InputView): 시도 횟수 입력 받기

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String MOVE_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
     public static void printCarInputMessage() {
         System.out.println(CAR_INPUT_MESSAGE);
@@ -12,5 +13,9 @@ public class InputView {
 
     public static String readCars() {
         return Console.readLine();
+    }
+
+    public static void printMoveInputMessage() {
+        System.out.println(MOVE_INPUT_MESSAGE);
     }
 }
