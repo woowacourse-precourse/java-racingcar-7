df93dbb41a6510182867ca6274c68c9938e817b7
NamJihyun99 <namjihyun9936@gmail.com>
2024-10-23 23:54:53 +0900

    feat:> 경주 플레이 틀 구현

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,13 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Race {
+    private final NumberGenerator numberGenerator;
     private final Integer rounds;
     private final List<Car> cars;
 
-    public Race(final Integer rounds, final List<Car> cars) {
+    public Race(final NumberGenerator numberGenerator, final Integer rounds, final List<Car> cars) {
+        this.numberGenerator = numberGenerator;
         this.rounds = rounds;
         this.cars = cars;
+    }
+
+    public void run() {
+        for (int round = 0; round < rounds; round++) {
+            List<Integer> numbers = new ArrayList<>();
+            for (int i = 0; i < cars.size(); i++) {
+                numbers.add(numberGenerator.generateNumber());
+            }
+            Round.play(cars, numbers);
+        }
     }
 }
