0d7ca3541666901c78938e435b49ead73b4bf320
slay <palgalow@gmail.com>
2024-10-24 01:39:18 +0900

    feat(ValidatorTest): 랜덤 숫자가 4 이상인지 검증하는 로직 테스트하는 기능 추가

---
+++ src/test/java/racingcar/util/ValidatorTest.java
@@ -0,0 +1,28 @@
+package racingcar.util;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ValidatorTest {
+
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        this.validator = new Validator();
+    }
+
+    @DisplayName("랜덤 숫자가 4 이상인지 검증하는 로직 테스트")
+    @Test
+    public void isMoreFourTest() {
+        assertFalse(validator.isMoreFour(1));
+        assertFalse(validator.isMoreFour(2));
+        assertFalse(validator.isMoreFour(3));
+        assertTrue(validator.isMoreFour(4));
+        assertTrue(validator.isMoreFour(5));
+    }
+}
