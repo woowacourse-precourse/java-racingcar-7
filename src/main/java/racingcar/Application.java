f53a68c688d0e7785d3a777409aaabcf83803700
wlstjd <xkakrlfh@naver.com>
2024-10-23 11:25:00 +0900

    feat: 자동차 이름 입력 및 예외 처리 기능을 구현해라
    - 자동차 이름을 쉼표(,)로 구분하여 입력받는 기능 추가
    - 자동차 이름이 5글자를 초과하지 않도록 검증 로직 추가
    - 5글자를 초과하는 경우 IllegalArgumentException 발생하도록 예외 처리 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        List<String> carNames = Arrays.asList(input.split(","));
+
+        validateCarNames(carNames);
+    }
+
+    private static void validateCarNames(List<String> carNames) {
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5글자를 초과할 수 없습니다: " + name);
+            }
+        }
     }
 }
