b1e665d69d5b33eaa79c14222c882cbaab59fef2
jjuchan <163832764+jjuchan@users.noreply.github.com>
2024-10-23 11:23:24 +0900

    application 입력 에러 메세지 출력 기능 구현

--- src/main/java/controller/RaceController.java
+++ src/main/java/controller/RaceController.java
@@ -3,7 +3,10 @@ package controller;
 import racingcar.Car;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class RaceController {
     private static final int MIN_Car_Name = 1;
@@ -13,7 +16,18 @@ public class RaceController {
     private final List<Car> cars = new ArrayList<>();
     private int round;
 
+    public void run(Scanner sc) {
+        SetCars(sc);
+    }
 
+    private void SetCars(Scanner sc) {
+        List<String> names;
+        String[] input;
+        System.out.println("경주할 차 이름을 입력해주세요.");
+        input = sc.nextLine().split(","); //쉼표 기준으로 이름을 구분한다.
+        names = Arrays.stream(input).map(String::trim).collect(Collectors.toList());
+        names.forEach(name -> cars.add(new Car(name)));
+    }
 
 
 }
