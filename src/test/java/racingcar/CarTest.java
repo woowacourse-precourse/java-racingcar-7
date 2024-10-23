d52b261f3ac6ee3b8c51ee95aa9a9f682465cdd5
jbilee <julishahn@gmail.com>
2024-10-24 02:07:56 +0900

    test(car): Car 클래스 테스트 코드 수정
    - Car 클래스의 메소드가 수정됨에 따라 테스트 코드도 변경

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,18 +3,18 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CarTest {
-    @DisplayName("인자로 전달된 값이 4 이상이면 true, 미만이면 false를 반환한다.")
+    @DisplayName("무작위 값이 4 이상이면 자동차가 전진한다.")
     @Test
-    void 전진_판단_기능_테스트() {
+    void 전진_기능_테스트() {
         Car car = new Car("car");
 
-        assertTrue(car.isMovable(9));
-        assertTrue(car.isMovable(4));
-        assertFalse(car.isMovable(0));
-        assertFalse(car.isMovable(3));
+        car.makeMove(2);
+        car.makeMove(4);
+        car.makeMove(9);
+
+        assertEquals(2, car.getMoves());
     }
 }
