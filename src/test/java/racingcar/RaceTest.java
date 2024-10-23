366eca2bcb432d9cb3f56d0f80ba0887d69828ee
jseom <2ujs4311@gmail.com>
2024-10-23 20:21:55 +0900

    TEST: 전진 및 정지 여부를 테스트하는 코드 추가
    - 무작위 수가 4 이상이 나왔을 경우 true, 4 미만일 경우 false를 반환합니다.
    - true는 전진, false는 정지를 의미합니다.

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class RaceTest {
@@ -9,5 +10,10 @@ class RaceTest {
     void 자동차경주_총라운드_최소_1회() {
         assertThrows(IllegalArgumentException.class, () -> new Race(0));
         assertThrows(IllegalArgumentException.class, () -> new Race(-1));
+    }
+
+    @Test
+    void 전진_정지_정하기() {
+        Assertions.assertThat(Race.moveForwardOrNot()).isIn(true, false);
     }
 }
