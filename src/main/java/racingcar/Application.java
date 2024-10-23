d14233e9919f40d99cfdd952ee580f7fe5ca44ec
MaiBoii <20183010@gs.cwnu.ac.kr>
2024-10-23 18:08:44 +0900

    feat: 입력 함수 구현
    자동차 이름 입력
    시도 횟수 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,37 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        
+    System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    String input = Console.readLine();
+
+    String[] racingCarArray = input.split(",");
+
+    List<String> racingCarList = new ArrayList<>();
+
+    try {
+        for (String racingCar : racingCarArray) {
+            racingCar = racingCar.trim();
+            if (racingCar.length()>5){
+                throw new IllegalArgumentException("이름은 5글자를 넘을 수 없습니다.");
+            }
+            racingCarList.add(racingCar);
+        }
+
+        System.out.println(racingCarList);
+
+    } catch (IllegalArgumentException e){
+        System.out.println(e.getMessage());
+        }
+    }
+
+    private static int inputNum() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String num = Console.readLine();
+        return Integer.parseInt(num);
     }
 }
