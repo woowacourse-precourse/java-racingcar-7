4c6e81f9e90cc34abd0644e8d3c01adf15934838
bowook <mbosx316@naver.com>
2024-10-23 21:45:53 +0900

    feat(RaceController) : RaceController 기능 추가
    RaceController 기능을 추가하였습니다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.controller.RaceController;
+
 public class Application {
     public static void main(String[] args) {
-
+        RaceController raceController = new RaceController();
+        raceController.start();
     }
 }
--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,2 +1,24 @@
-package racingcar.controller;public class RaceController {
+package racingcar.controller;
+
+import racingcar.domain.CarNameValidator;
+import racingcar.domain.Cars;
+import racingcar.domain.TrialCounts;
+import racingcar.domain.TrialCountsValidator;
+import racingcar.view.InputView;
+
+public class RaceController {
+    private final CarNameValidator carNameValidator = new CarNameValidator();
+    private final TrialCountsValidator trialCountsValidator = new TrialCountsValidator();
+
+    public void start() {
+        getInput();
+    }
+
+    private void getInput() {
+        String carNames = InputView.inputCarNames();
+        Cars cars = new Cars(carNames, carNameValidator);
+
+        String counts = InputView.inputTrialCounts();
+        TrialCounts trialCounts = new TrialCounts(counts, trialCountsValidator);
+    }
 }
