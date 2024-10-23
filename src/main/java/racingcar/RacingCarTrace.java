fb61b51c57e2c3a47879be14002b9ba5cda144d7
sunjong0214 <rlatjswhd8200@gmail.com>
2024-10-23 21:12:11 +0900

    feat(RacingCarTrace): RacingCarTrace 구현
    RacingCar를 key로 가지고, 이동거리를 value로 가지는 LinkedHashMap 구현

---
+++ src/main/java/racingcar/RacingCarTrace.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class RacingCarTrace {
+    private final Map<RacingCar, Integer> trace = new LinkedHashMap<>();
+
+
+}
