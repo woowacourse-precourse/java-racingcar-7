b8db74bd1dbdd19eb957e09b15a9027d100852e6
seunghee <seunghee_chae3@tmax.co.kr>
2024-10-23 16:21:31 +0900

    feat(InputView): 자동차 이름 문자열 입력 받기

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
     private static final String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -8,4 +10,7 @@ public class InputView {
         System.out.println(CAR_INPUT_MESSAGE);
     }
 
+    public static String readCars() {
+        return Console.readLine();
+    }
 }
