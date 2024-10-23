6e9e50f893ce87e58d9d10f9dc49fa79d7da2532
jjuchan <163832764+jjuchan@users.noreply.github.com>
2024-10-23 11:04:45 +0900

    랜덤 기능 구현 클래스 생성

--- src/main/java/controller/RaceController.java
+++ src/main/java/controller/RaceController.java
@@ -1,5 +1,18 @@
 package controller;
 
+import racingcar.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RaceController {
+    private static final int MIN_Car_Name = 1;
+    private static final int MAX_Car_Name = 5;
+    private static final int MIN_Race_Round = 1;
+
+    private final List<Car> cars = new ArrayList<>();
+    private int round;
+
+
 
 }
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,19 @@
 package racingcar;
 
+import controller.RaceController;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Scanner sc = new Scanner(System.in);
-        System.out.println("경주 할 자동차 이름을 입력해주세요.");
-        String name = sc.nextLine();
-        System.out.println("시도할 횟수를 입력해주세요.");
-        int move = sc.nextInt();
+        RaceController raceController = new RaceController();
+        try {
+            raceController.run(sc);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
+
