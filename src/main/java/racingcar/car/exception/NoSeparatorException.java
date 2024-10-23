b2bcc6e32714db07bbd18771397ee93248016483
hgh1472 <hgh1472@naver.com>
2024-10-23 17:27:36 +0900

    feat: 구분자 없는 경우의 예외 클래스

---
+++ src/main/java/racingcar/car/exception/NoSeparatorException.java
@@ -0,0 +1,19 @@
+package racingcar.car.exception;
+
+public class NoSeparatorException extends RuntimeException {
+	public NoSeparatorException() {
+		super();
+	}
+
+	public NoSeparatorException(String message) {
+		super(message);
+	}
+
+	public NoSeparatorException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public NoSeparatorException(Throwable cause) {
+		super(cause);
+	}
+}
