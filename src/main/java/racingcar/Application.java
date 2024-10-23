d973d5e640cddd4cba5578233d9efc13ad357477
고희준 <kohuijun@gohuijun-ui-MacBookAir.local>
2024-10-23 14:46:30 +0900

    feat : 자동차 이름 구분 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        ArrayList<String> carNames = new ArrayList<>();
+        SeparateCarNames(carNames);
+    }
+
+    public static void SeparateCarNames(ArrayList<String> carNames) {
+        String[] names = Console.readLine().split(",");
+        for (String name : names) {
+            name = name.trim();
+            if (name.length() <= 5) {
+                carNames.add(name);
+            } else {
+                throw new IllegalArgumentException("5자 이하로 해주세요.");
+            }
+        }
     }
 }
