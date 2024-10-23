088e63f44bcb369b9fe4158d1243a56b6c14182b
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-23 15:26:36 +0900

    feat($OutputConsole): 출력을 위한 콘솔 클래스 구현
    - 상속이 필요 없는 유틸리티 클래스이므로 final 클래스로 선언.

---
+++ src/main/java/racingcar/util/io/OutputConsole.java
@@ -0,0 +1,12 @@
+package racingcar.util.io;
+
+public final class OutputConsole {
+
+    public static void println(String message) {
+        System.out.println(message);
+    }
+
+    public static void print(String message) {
+        System.out.print(message);
+    }
+}
