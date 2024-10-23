0cfffc072be011121673b624fba0d2f571c7ea41
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-23 17:15:01 +0900

    feat($RacingService): Racing Service 추가
    - Racing 과 관련된 역할을 수행.

---
+++ src/main/java/racingcar/service/RacingService.java
@@ -0,0 +1,25 @@
+package racingcar.service;
+
+import racingcar.model.RacingCars;
+import racingcar.util.ramdom.RandomRange;
+
+public class RacingService {
+
+    private final RandomRange RANDOM_RANGE = new RandomRange(0, 9);
+    private RacingCars racingCars;
+
+    public void setRacingCars(RacingCars racingCars) {
+        this.racingCars = racingCars;
+    }
+
+    public void playRound() {
+        validateRacingCarsExistence();
+        racingCars.moveRandomly(RANDOM_RANGE);
+    }
+
+    private void validateRacingCarsExistence() {
+        if (racingCars == null) {
+            throw new IllegalStateException("게임을 시작하기 위한 자동차가 존재하지 않습니다.");
+        }
+    }
+}
