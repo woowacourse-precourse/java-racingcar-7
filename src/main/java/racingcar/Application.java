5ade67e5f71a2fd4690d3452023c0798dbfd635a
MinseokGo <rhalstjr1999@naver.com>
2024-10-23 15:55:22 +0900

    feat: 자동차 이름 입력을 안내하는 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,9 @@
 package racingcar;
 
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)" 메세지를 띄운다.
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 }
