4cb5e098f4c838601b2f57f66fef2693270e4bad
Seo Jinmann <sjmmics@gmail.com>
2024-10-24 08:30:53 +0900

    comment : ($Application) 불필요한 주석 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,7 +6,6 @@ import racingcar.view.ConsoleOutputView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
         CarRacingController controller =
                 new CarRacingController(new ConsoleInputView(), new ConsoleOutputView());
