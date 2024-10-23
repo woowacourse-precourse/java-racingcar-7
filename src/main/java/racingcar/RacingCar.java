bb08ed80e01830daceb44f2cb2ea35b5b3cdb2a9
Hyungjung Kim <gudwnd1113@gmail.com>
2024-10-23 21:13:26 +0900

    feat(RacingCar): throw exception on RacingCar constructor
    자동차의 이름이 1자에서 5자 사이가 아닐 시 IllegalArgumentException 발생.

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -3,10 +3,13 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingCar {
-    private String name;
+    private final String name;
     private int count;
 
     public RacingCar(final String name) {
+        if (name == null || name.isEmpty() || name.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 1자에서 5자 사이여야 합니다.");
+        }
         this.name = name;
         this.count = 0;
     }
