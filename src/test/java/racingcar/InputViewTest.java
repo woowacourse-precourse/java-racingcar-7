6d168c73378b28c5eb2b50ab4ccfcc9dc7973b29
chaerish <ccyy8432@naver.com>
2024-10-24 00:39:23 +0900

    test(InputViewTest): 사용자 입력 값 유효성 검사 테스트 생성
    입력 값 유효성 검사를 위해 사용자 입력 값이 공백인지, 이름의 길이가 5자 이상인지, 이름이 중복인지 검사하는 테스트 코드를 생성한다.

--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -1,38 +1,54 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
     private InputView inputView;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         this.inputView = new InputView();
     }
-    @Test
-    public void 차이름_유효성_검사(){
-        boolean isValidateCarName = inputView.isValidateCarName("차이름");
-        assertThat(isValidateCarName).isTrue();
+
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "  ", "   ", "     "})
+    void 입력값이_공백인지_검사(String userInput) {
+        boolean isBlankCarName = inputView.isBlank(userInput);
+        assertThat(isBlankCarName).isTrue();
     }
-    @Test
-    public void 시도숫자_유효성_검사(){
-        boolean isValidateTryNum = inputView.isValidateTryNum("1");
-        assertThat(isValidateTryNum).isTrue();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"안녕하세요힛", "안녕하세요요요", "내이름은안녕하세요", "이름이특이하네요"})
+    void 차이름_길이가_5자_이상인지_검사(String userInput) {
+        boolean isNameMoreThanFive = inputView.isMoreThanFive(userInput);
+        assertThat(isNameMoreThanFive).isTrue();
     }
-    @Test
-    void 차이름_공백인지_검사(){
-        boolean isBlankCarName = inputView.isValidateCarName("      ");
-        assertThat(isBlankCarName).isFalse();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"반가워요", "세글자", "이름", "이", "안녕하세요"})
+    void 차이름_길이가_5자_이하인지_검사(String userInput) {
+        boolean isNameLessThanFive = inputView.isMoreThanFive(userInput);
+        assertThat(isNameLessThanFive).isFalse();
     }
+
     @Test
-    public void 시도숫자_공백값인지_검사(){
-        boolean isValidateTryNum = inputView.isValidateTryNum(" ");
-        assertThat(isValidateTryNum).isFalse();
+    void 차이름이_중복인지_검사() {
+        InputView.userName.add("중복이");
+        boolean isDuplicate = inputView.isDuplicate("중복이");
+        assertThat(isDuplicate).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"안녕하세요!구분자가없으면어떻게되나요!느낌표가구분자입니다.", "안녕하세요?구분자가없으면어떻게되죠?"})
+    void 차이름에_쉼표가_없을때_검사(String userInput) {
+        boolean isContainComma = inputView.isContainComma(userInput);
+        assertThat(isContainComma).isFalse();
     }
 
 }
+
