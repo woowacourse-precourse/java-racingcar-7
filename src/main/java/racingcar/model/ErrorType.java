eea521f67947f2962416fad9b9008219e8930252
Teddysir <sdzx0719@naver.com>
2024-10-23 20:42:59 +0900

    feat: ErrorType enum 클래스 생성
    유효성 검사 로직에서 사용될 오류 내용 enum타입으로 생성
    README 두개의 오류 같은 오류 타입 사용하도록 수정

---
+++ src/main/java/racingcar/model/ErrorType.java
@@ -0,0 +1,20 @@
+package racingcar.model;
+
+public enum ErrorType {
+
+    INPUT_NULL_ERROR("[ERROR] 빈 문자열은 입력할 수 없습니다."),
+    CAR_NAME_LENGTH_ERROR("[ERROR] 이름은 5자 이하여야 합니다."),
+    GAME_TRY_COUNT_ERROR("[ERROR] 입력 횟수는 1회 이상이여야 합니다."),
+    CAR_NAME_DUPLICATE_ERROR("[ERROR] 중복된 이름이 있습니다.");
+
+
+    private final String message;
+
+    ErrorType(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
