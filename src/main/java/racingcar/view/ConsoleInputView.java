b53d33e515dd338fda237e30ae2397df7feba15c
Seo Jinmann <sjmmics@gmail.com>
2024-10-24 08:58:12 +0900

    Refactor : ($ConsoleInputView) 입력값 검증 패턴을 알기 쉽게 기능 별로 변수로 변경
    ONLY_DIGIT_ALPHABET_HANGEUL : 숫자, 알파벳, 한글만 있는 문자열
    CAR_NAME_DELIMITER : 자동차 이름을 나누는 기준 문자
    CAR_NAME_LENGTH_MIN_BETWEEN_MAX : 자동차 이름 최소 최대 사이
    MAX_CAR_NAME_LENGTH : 자동차 이름 최대 길이

--- src/main/java/racingcar/view/ConsoleInputView.java
+++ src/main/java/racingcar/view/ConsoleInputView.java
@@ -11,8 +11,26 @@ import java.util.regex.Pattern;
  */
 public class ConsoleInputView implements InputView {
 
-    private static final Pattern carNamePattern =
-            Pattern.compile("^[0-9a-zA-Z가-힣]{1,5}(,[0-9a-zA-Z가-힣]{1,5})*$");
+    public static final String EXCEPTION_MESSAGE_PREFIX = "[ERROR] ";
+
+    public static final String ONLY_DIGIT_ALPHABET_HANGEUL = "[0-9a-zA-Z가-힣]";
+
+    public static final String MAX_CAR_NAME_LENGTH = "5";
+
+    public static final String CAR_NAME_LENGTH_MIN_BETWEEN_MAX =
+            "{1," + MAX_CAR_NAME_LENGTH + "}";
+
+    public static final String CAR_NAME_DELIMITER = ",";
+
+    // 자동차 이름 문자열 형식 검증 패턴
+    public static final Pattern carNamePattern =
+            Pattern.compile("^" +
+                    ONLY_DIGIT_ALPHABET_HANGEUL +
+                    CAR_NAME_LENGTH_MIN_BETWEEN_MAX +
+                    "(" + CAR_NAME_DELIMITER +
+                    ONLY_DIGIT_ALPHABET_HANGEUL +
+                    CAR_NAME_LENGTH_MIN_BETWEEN_MAX +
+                    ")*$");
 
     /**
      * 콘솔로부터 자동차 이름 문자열을 받아서 형식을 검증한 후 문자열로 반환하는 메서드
@@ -48,7 +66,8 @@ public class ConsoleInputView implements InputView {
     private void validateNameFormat(String nameToValidate) {
         Matcher matcher = carNamePattern.matcher(nameToValidate);
         if (!matcher.matches()) {
-            throw new IllegalArgumentException("[ERROR] 올바른 자동차 이름 형식이 아닙니다.");
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_PREFIX
+                    + "올바른 자동차 이름 형식이 아닙니다.");
         }
     }
 
@@ -60,7 +79,8 @@ public class ConsoleInputView implements InputView {
 
     private void validateNotBlank(String numberToValidate) {
         if (numberToValidate.isEmpty()) {
-            throw new IllegalArgumentException("문자열이 공백입니다. 숫자를 입력하세요.");
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_PREFIX
+                    + "문자열이 공백입니다. 숫자를 입력하세요.");
         }
     }
 
@@ -68,14 +88,16 @@ public class ConsoleInputView implements InputView {
         try {
             Integer.parseInt(numberToValidate);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아니거나 int 범위를 벗어납니다.");
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_PREFIX
+                    + "숫자가 아니거나 int 범위를 벗어납니다.");
         }
     }
 
     private void validateNotZero(String numberToValidate) {
         int number = Integer.parseInt(numberToValidate);
         if (number == 0) {
-            throw new IllegalArgumentException("[ERROR] 시도할 횟수는 양수이어야 합니다.");
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_PREFIX
+                    + "시도할 횟수는 양수이어야 합니다.");
         }
     }
 
