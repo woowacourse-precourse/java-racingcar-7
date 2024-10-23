c001893e2924b90659f7615f52bcc124cdd98642
qyinm <qusseun@gmail.com>
2024-10-23 21:13:18 +0900

    feat: 자동차 이름 분리 기능 작성

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,20 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
+
+public class InputView {
+
+    private static final Pattern VALID_CAR_INPUT_PATTERN = Pattern.compile("[a-zA-Z]{1,4}(,[a-zA-z]{1,4})*");
+    private static final Pattern VALID_TRY_COUNT_INPUT_PATTERN = Pattern.compile("^(0|[1-9][0-9]*)$");
+
+    public static String readLine() {
+        return Console.readLine();
+    }
+
+    public static String[] splitCarNamesByComma(String input) {
+        return input.split(",");
+    }
+
+
+}
