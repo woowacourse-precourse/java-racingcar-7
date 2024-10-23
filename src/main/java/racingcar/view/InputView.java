dab653e2b45fa3318a8d792e82ddd8d19fb8f7d4
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-23 16:33:17 +0900

    feat($InputView): 시도 횟수에 대한 예외 처리 추가
    - 시도 횟수가 0 이하인 경우.
    - 시도 횟수가 숫자가 아닌 경우.
    - 시도 횟수가 정수(Long) 범위를 초과하는 경우.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,11 +4,25 @@ import racingcar.util.io.InputConsole;
 
 public class InputView {
 
+    private final long MINIMUM_TRY_COUNT = 1;
+
     public String getCarNames() {
         return InputConsole.readLine();
     }
 
     public long getTryCount() {
-        return Long.parseLong(InputConsole.readLine());
+        try {
+            long tryCount = Long.parseLong(InputConsole.readLine());
+            validateTryCount(tryCount);
+            return tryCount;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("유효하지 않은 시도 횟수입니다.");
+        }
+    }
+
+    private void validateTryCount(long tryCount) {
+        if (tryCount < MINIMUM_TRY_COUNT) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+        }
     }
 }
