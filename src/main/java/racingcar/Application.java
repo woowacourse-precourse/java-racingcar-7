88c20b1694799430315795cbff12cb48545b747a
KNU-K <knukang334@gmail.com>
2024-10-24 04:22:01 +0900

    feat(application): 메인 애플리케이션 로직 구현
    - `ApplicationConfig`를 통해 `RaceController` 인스턴스를 생성.
    - 사용자로부터 자동차 이름과 시도 횟수를 입력받는 기능 추가.
    - 경주를 시작하고 결과를 출력하는 로직 구현.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import racingcar.config.ApplicationConfig;
+import racingcar.constant.MessageConstants;
+import racingcar.controller.RaceController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 1. ApplicationConfig로부터 RaceController를 생성
+        ApplicationConfig config = new ApplicationConfig();
+        RaceController raceController = config.getRaceController();
+
+        // 2. 사용자로 부터 입력(Client)
+        String command = InputView.inputString(MessageConstants.INPUT_TRY_COUNT);
+        Integer iterations = InputView.inputInteger(MessageConstants.INPUT_CAR_NAMES);
+
+        // 3. 경주 시작
+        String winners = raceController.startRace(command, iterations);
+
+        // 4. 사용자에게 출력 (Client)
+        OutputView.printResult(MessageConstants.OUTPUT_RESULT_TEMPLATE, winners);
+
     }
 }
