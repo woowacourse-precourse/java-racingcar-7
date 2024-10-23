4d715e0b7f3639867d232992ba104db5244e69cd
AwesomeMinsik <beaut3579@gmail.com>
2024-10-23 20:58:49 +0900

    refactor : 검증 로직 집중화로 IOHandler 의 검증 메서드 호출로직을 추가하였습니다.

--- src/main/java/racingcar/IOHandler.java
+++ src/main/java/racingcar/IOHandler.java
@@ -9,20 +9,26 @@ public class IOHandler {
     private final Validator validator = new Validator();
 
     public List<String> setRacerList() {
-        String string = Console.readLine();
-        return Arrays.asList(string.split(","));
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        List<String> racerList = Arrays.asList(input.split(","));
+        validator.validateName(racerList);
+        return racerList;
     }
 
     public int setRaceCycle() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         int cycle;
         String cycleInput = Console.readLine();
-        cycle = validator.validateInput(cycleInput);
+        cycle = validator.validateCycle(cycleInput);
         return cycle;
     }
 
     public void printRaceState(RacingCar racingCar) {
         System.out.println(racingCar.getName() + " : " + racingCar.getStatusToDash());
+    }
+    public String getWinnerList(List<String> winners) {
+        return String.join(",", winners);
     }
 
 }
