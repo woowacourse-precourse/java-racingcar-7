ddfe83192fdb8c7561300b4ee49040dcbbe176ca
jseom <2ujs4311@gmail.com>
2024-10-23 15:50:11 +0900

    FEAT(RacingCar): 이름을 5자 이하로 제한하는 기능 추가
    이름이 5자가 초과되면 IllegalArgumentException을 발생시키는 코드를 구현했습니다.

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -7,12 +7,21 @@ public class RacingCar {
 
     public static ArrayList makeNameList(String str) {
         ArrayList<String> nameList = new ArrayList<>(List.of(str.split(",", -1)));
-        throwError(nameList);
+        wrongValue(nameList);
+        biggerThanFive(nameList);
 
         return nameList;
     }
 
-    private static void throwError(ArrayList<String> nameList) {
+    private static void biggerThanFive(ArrayList<String> nameList) {
+        for (String s : nameList) {
+            if (s.length() > 5) {
+                throw new IllegalArgumentException("5자를 넘었습니다.");
+            }
+        }
+    }
+
+    private static void wrongValue(ArrayList<String> nameList) {
         if (nameList.contains("")) {
             throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
         }
