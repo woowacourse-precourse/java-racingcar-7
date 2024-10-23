63951018090f960fa1372947f9f557dfeba8c3eb
wkqkel <wkqkel@naver.com>
2024-10-23 22:16:53 +0900

    feat: 사용자 입력받기 기능 구현

---
+++ src/main/java/racingcar/request/CarCreateRequest.java
@@ -0,0 +1,38 @@
+package racingcar.request;
+
+public class CarCreateRequest {
+    private final String[] names;
+    private final int tryCount;
+
+    public CarCreateRequest(String[] names, int tryCount) {
+        validateName(names);
+        validateTryCount(tryCount);
+        this.names = names;
+        this.tryCount = tryCount;
+    }
+
+    private void validateTryCount(int tryCount) {
+        if (tryCount <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateName(String[] names) {
+        if (names == null || names.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        for (String name : names) {
+            validateName(name);
+        }
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,29 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.request.CarCreateRequest;
+
+public class InputView {
+
+    public CarCreateRequest readCars() {
+        String[] names = getNames();
+        int tryCount = getTryCount();
+        return new CarCreateRequest(names, tryCount);
+    }
+
+    public String[] getNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        return readLine().split(",");
+    }
+
+    public static int getTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+
+        return Integer.parseInt(readLine());
+    }
+
+    private static String readLine() {
+        return Console.readLine();
+    }
+}
