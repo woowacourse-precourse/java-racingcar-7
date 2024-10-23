ef7264d9f39d2816e05aa38c3e4caf221a29f9ad
Hacanna42 <dtmdgus901@gmail.com>
2024-10-23 21:07:45 +0900

    feat(RacingGame): change to inject OutputHandler via DI

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,7 @@ import racingcar.controller.RacingGame;
 import racingcar.model.*;
 import racingcar.util.*;
 import racingcar.view.InputHandler;
+import racingcar.view.OutputHandler;
 
 public class Application {
     public static void main(String[] args) {
@@ -16,14 +17,14 @@ public class Application {
         List<String> carNames = inputHandler.getInputAboutCars();
         int rounds = inputHandler.getInputAboutRound();
 
+        // 자동차 객체를 생성하고 전략 주입
         MoveStrategy moveStrategy = new RandomMoveStrategy();
         CarFactory carFactory = new CarFactory();
-
-        // 자동차 객체를 생성하고 전략 주입
         List<Car> cars = carFactory.createCars(carNames, moveStrategy);
 
         // 경주 게임 시작
-        RacingGame racingGame = new RacingGame(cars, rounds);
+        OutputHandler outputHandler = new OutputHandler();
+        RacingGame racingGame = new RacingGame(outputHandler, cars, rounds);
         racingGame.start();
     }
 }
