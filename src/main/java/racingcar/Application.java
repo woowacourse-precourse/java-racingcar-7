84c48a74c0894bf90f3fb3382f48e5a2344082b0
kangrae <kangrae0501@gmail.com>
2024-10-23 11:36:25 +0900

    feat: Implement user input feature
    콘솔을 통해 사용자 입력을 받는 클래스생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import view.ConsoleView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 1. 입력
+        ConsoleView consoleView = new ConsoleView();
+        consoleView.getCarNames();
+        consoleView.getMoveCount();
+
+        // TODO: 입력 구조 정검 및 다음 기능들 구현
     }
 }
---
+++ src/main/java/view/ConsoleView.java
@@ -0,0 +1,17 @@
+package view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class ConsoleView {
+
+    public String getCarNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return readLine();
+    }
+
+    public String getMoveCount(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return readLine();
+    }
+
+}
