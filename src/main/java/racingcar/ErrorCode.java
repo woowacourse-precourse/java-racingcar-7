94c5d2a9def9806325a2848105d0722b8d0b0b02
Geonwoo Pack <packdev937@gmail.com>
2024-10-23 13:16:12 +0900

    feat (validation) : 자동차 이름 길이에 관한 검증 로직을 추가한다

--- src/main/java/racingcar/ErrorCode.java
+++ src/main/java/racingcar/ErrorCode.java
@@ -1,7 +1,9 @@
 package racingcar;
 
 public enum ErrorCode {
-    CONSECUTIVE_COMMA("입력에 쉼표가 연속적으로 입력되었습니다.");
+    CONSECUTIVE_COMMA("입력에 쉼표가 연속적으로 입력되었습니다."),
+    CAR_NAME_LENGTH_EXCEEDED("자동차 이름은 5자를 초과할 수 없습니다."),
+    CAR_NAME_EMPTY("자동차 이름은 빈 문자열일 수 없습니다.");
 
     private final String message;
 
--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -5,7 +5,27 @@ import static racingcar.ErrorCode.*;
 class InputValidator {
 
     private final static String COMMAS = ",,";
-    public static void validateCarNames(String carNames) {
+    private final static int MAX_CAR_NAME_LENGTH_CRITERION = 5;
+
+    public static void validate(final String carNames) {
+        validateNoConsecutiveCommas(carNames);
+        validateCarNameLength(carNames);
+        validateCarNameEmpty(carNames);
+    }
+
+    private static void validateCarNameEmpty(final String carNames) {
+        if (carNames.isEmpty()) {
+            throw new IllegalArgumentException(CAR_NAME_EMPTY.getMessage());
+        }
+    }
+
+    private static void validateCarNameLength(final String carNames) {
+        if (carNames.length() > MAX_CAR_NAME_LENGTH_CRITERION) {
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_EXCEEDED.getMessage());
+        }
+    }
+
+    private static void validateNoConsecutiveCommas(final String carNames) {
         if (carNames.contains(COMMAS)) {
             throw new IllegalArgumentException(CONSECUTIVE_COMMA.getMessage());
         }
