f6d58db11def10a86762609c8691d8096a48f42b
imjunghun <dlawndgns9@naver.com>
2024-10-23 18:00:48 +0900

    feat(UIController): 레이스 결과와 우승자 출력하는 기능

--- src/main/java/racingcar/controller/UIController.java
+++ src/main/java/racingcar/controller/UIController.java
@@ -1,5 +1,6 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -7,6 +8,11 @@ public class UIController {
 
     private static final String QUESTION_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String QUESTION_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+    private static final String RACE_RESULT = "실행 결과";
+    private static final String RACE_SYMBOL = "-";
+    private static final String COLON = " : ";
+    private static final String WINNER_FORMAT = "최종 우승자 : ";
+    private static final String NEXT_LINE = "\n";
 
     public String receiveName() {
         OutputView.print(QUESTION_CAR_NAME);
@@ -16,6 +22,26 @@ public class UIController {
     public int receiveCount() {
         OutputView.print(QUESTION_TRY_COUNT);
         return validateCount(InputView.read());
+    }
+
+    public void printRaceResultPhrase() {
+        OutputView.print(RACE_RESULT);
+    }
+
+    public void printRaceResult(Car car) {
+        OutputView.print(fitRaceFormat(car));
+    }
+
+    public void printWinner(Car car) {
+        OutputView.print(WINNER_FORMAT + car.getName());
+    }
+
+    private String fitRaceFormat(Car car) {
+        return car.getName() + COLON + parseResultSymbol(car.getPosition()) + NEXT_LINE;
+    }
+
+    private String parseResultSymbol(int position) {
+        return RACE_SYMBOL.repeat(position);
     }
 
     private String validateName(String name) {
