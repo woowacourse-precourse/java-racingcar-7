9aa499f1ad74ca0e66b89603f0c7ec84fa3f8941
alpin87 <qortmdals94@naver.com>
2024-10-23 20:02:47 +0900

    feat: 입력받은 이름을 쉼표 기준으로 분리하는 기능 구현

--- src/main/java/racingcar/RaceUtil.java
+++ src/main/java/racingcar/RaceUtil.java
@@ -1,4 +1,12 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RaceUtil {
+    private static final String NAME_DELIMITER = ",";
+
+    public static String[] parseCarNames(String input) {
+        return input.trim().split(NAME_DELIMITER);
+    }
 }
+
