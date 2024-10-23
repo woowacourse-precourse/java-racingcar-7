14e5001dc85558aca648aec9d0799f426b8d9d84
byeongdoohan <quden04@gmail.com>
2024-10-24 08:58:02 +0900

    rename: Viewer -> RacingCarView 파일명 변경

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 
-public class Viewer {
+public class RacingCarView {
     private final String carInputString = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final String tryInputString = "시도할 횟수는 몇 회인가요?";
     private final String outputString = "최종 우승자 : ";
