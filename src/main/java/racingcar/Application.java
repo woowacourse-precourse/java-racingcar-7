e7f7f238cc8355e5d20948a505032a66856d1c3f
MinseokGo <rhalstjr1999@naver.com>
2024-10-23 16:52:47 +0900

    feat: 시도 횟수 입력 안내 메시지 출력 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -19,5 +19,7 @@ public class Application {
         for (String carName : carNames) {
             cars.add(new Car(carName));
         }
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
     }
 }
