de8c573a5b8b9496200bc9267c62f6543b016bec
ljhee92 <weilainib7@gmail.com>
2024-10-23 11:24:44 +0900

    feat: 시도할 횟수 입력, 검증
    시도할 횟수를 입력 받는다.
    횟수가 숫자인지 검증한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,9 +2,6 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-// 1. 경주할 자동차 이름을 입력 받는다.
-//    * 쉼표(,) 기준으로 구분하여 입력했는지 검증한다.
-//    * 자동차 이름이 5자 이하인지 검증한다.
 // 2. 시도할 횟수를 입력 받는다.
 //    * 횟수가 숫자인지 검증한다.
 // 3. 시도 횟수만큼 경주를 반복하고, 횟수별 경주 결과를 출력한다.
@@ -26,5 +23,12 @@ public class Application {
                 throw new IllegalArgumentException("이름은 5자 이하로 입력해주세요.");
             } // end if
         } // end for
+
+        String inputTimes = Console.readLine();
+        if (!inputTimes.matches("^[0-9]+$")) {
+//            throw new NumberFormatException("숫자를 입력해주세요.");
+            throw new IllegalArgumentException("시도 횟수는 숫자만 입력 가능합니다.");
+        } // end if
+        int times = Integer.parseInt(inputTimes);
     } // main
 } // class
