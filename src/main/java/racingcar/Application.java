5bc3bc45e41f84a77d83943b8d50b58392a48ca8
JJhwan7099 <ghks7099@naver.com>
2024-10-23 20:06:47 +0900

    style: 코드 포맷팅
    가독성을 위한 코드 포맷팅을 했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,10 +9,12 @@ public class Application {
     public static void main(String[] args) {
         String cars = Console.readLine();
         String move_cnt = Console.readLine();
-        ArrayList<Car> race_result = race_start(cars,move_cnt);
+
+        ArrayList<Car> race_result = race_start(cars, move_cnt);
         ArrayList<String> winners = getRaceWinners(race_result);
         printWinners(winners);
     }
+
     public static ArrayList<Car> add_cars(String cars) {
         ArrayList<Car> carList = new ArrayList<>();
         for (String car_name : cars.split(",")) {
@@ -21,59 +23,64 @@ public class Application {
         }
         return carList;
     }
+
     public static void validate(String car_name) {
-        if(car_name.length() > 5) {
+        if (car_name.length() > 5) {
             throw new IllegalArgumentException();
         }
     }
+
     public static ArrayList<Car> race_start(String cars, String move_cnt) {
         ArrayList<Car> carList = add_cars(cars);
         int move_time = Integer.parseInt(move_cnt);
-        while(move_time-- > 0) {
+        while (move_time-- > 0) {
             carList = move_car(carList);
             showRaceprogress(carList);
         }
         return carList;
     }
+
     public static ArrayList<Car> move_car(ArrayList<Car> carList) {
-        for(Car car : carList) {
-            if(Randoms.pickNumberInRange(0,9) >= 4) {
+        for (Car car : carList) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
                 car.setDistance(car.getDistance() + 1);
             }
         }
         return carList;
     }
+
     public static void showRaceprogress(ArrayList<Car> carList) {
-        for(Car car : carList) {
-            System.out.print(car.getName()+" : ");
-            for(int i=0; i<car.getDistance(); i++) {
+        for (Car car : carList) {
+            System.out.print(car.getName() + " : ");
+            for (int i = 0; i < car.getDistance(); i++) {
                 System.out.print('-');
             }
             System.out.println();
         }
         System.out.println();
     }
+
     public static ArrayList<String> getRaceWinners(ArrayList<Car> carList) {
         int maxwins = 0;
         ArrayList<String> winners = new ArrayList<>();
-        for(Car car : carList) {
-            if(car.getDistance() > maxwins) {
+        for (Car car : carList) {
+            if (car.getDistance() > maxwins) {
                 maxwins = car.getDistance();
                 winners.clear();
                 winners.add(car.getName());
-            }
-            else if(car.getDistance() == maxwins) {
+            } else if (car.getDistance() == maxwins) {
                 winners.add(car.getName());
             }
         }
         return winners;
     }
+
     public static void printWinners(ArrayList<String> winners) {
         int i;
         System.out.print("최종 우승자 : ");
-        for(i=0; i<winners.size()-1; i++) {
-            System.out.print(winners.get(i)+", ");
+        for (i = 0; i < winners.size() - 1; i++) {
+            System.out.print(winners.get(i) + ", ");
         }
-        System.out.print(winners.get(winners.size()-1));
+        System.out.print(winners.get(winners.size() - 1));
     }
 }
