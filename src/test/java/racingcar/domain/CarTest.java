bc04421e89e9ac4ee2110986f3fc71d459bf8615
SungJun <kmymy00@naver.com>
2024-10-24 00:30:01 +0900

    test(ApplicationTest): 예외 사항 테스트 케이스 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,7 +3,6 @@ package racingcar.domain;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
