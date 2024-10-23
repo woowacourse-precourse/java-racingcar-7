04a0eb955afce65097736aeb5be8261752097a09
kim <hwiyul_g@daum.net>
2024-10-23 18:56:20 +0900

    feat(ChatService): 기본 거리로 자동차 추가하는 함수 추가

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -8,30 +8,37 @@ import racingcar.domain.Car;
 public class CarService {
     private static final int BASE_DISTANCE = 0;
 
-    private final Set<Car> cars = new LinkedHashSet<>();
+    private final Set<Car> uniqueCars = new LinkedHashSet<>();
+
+    public void addCar(String carName){
+        Car car = new Car(carName, BASE_DISTANCE);
+        if(!uniqueCars.add(car)){
+            throw new IllegalArgumentException("[ERROR] 중복된 자동차 이름을 추가할 수 없습니다.");
+        }
+    }
 
     public void addCar(String carName, int distance) {
         Car car = new Car(carName, distance);
-        if (!cars.add(car)) {
+        if (!uniqueCars.add(car)) {
             throw new IllegalArgumentException("[ERROR] 중복된 자동차 이름을 추가할 수 없습니다.");
         }
     }
 
     public List<Car> moveAllCars() {
-        cars.forEach(Car::move);
-        return cars.stream().toList();
+        uniqueCars.forEach(Car::move);
+        return uniqueCars.stream().toList();
     }
 
     public List<String> getCarsWithMaxDistance() {
         int maxDistance = calculateMaxDistance();
-        return cars.stream()
+        return uniqueCars.stream()
                 .filter(car -> car.getDistance() == maxDistance)
                 .map(Car::getName)
                 .toList();
     }
 
     private int calculateMaxDistance() {
-        return cars.stream().mapToInt(Car::getDistance).max().orElse(BASE_DISTANCE);
+        return uniqueCars.stream().mapToInt(Car::getDistance).max().orElse(BASE_DISTANCE);
     }
 
 }
