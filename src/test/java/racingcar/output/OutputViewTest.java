f2ede035f55421e3fbc0e55a0832f31eddd5c1af
githublees <leeho_@naver.com>
2024-10-23 23:38:53 +0900

    test: Add tests for OutputViewer class

---
+++ src/test/java/racingcar/output/OutputViewTest.java
@@ -0,0 +1,46 @@
+package racingcar.output;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OutputViewTest {
+
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void 경주할_자동차_이름_안내문_출력() {
+        OutputViewer.getCarNameInputMessage();
+        assertThat(outputStreamCaptor.toString())
+                .contains("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+    }
+
+    @Test
+    void 시도할_횟수_안내문_출력() {
+        OutputViewer.getTryCountInputMessage();
+        assertThat(outputStreamCaptor.toString())
+                .contains("시도할 횟수는 몇 회인가요?");
+    }
+
+    @Test
+    void 실행_결과_안내문_출력() {
+        OutputViewer.getRacingProgressMessage();
+        assertThat(outputStreamCaptor.toString())
+                .contains("실행 결과");
+    }
+
+    @Test
+    void 최종_우승자_안내문_출력() {
+        OutputViewer.getRacingResultMessage("pobi, jun");
+        assertThat(outputStreamCaptor.toString())
+                .contains("최종 우승자 : pobi, jun");
+    }
+}
