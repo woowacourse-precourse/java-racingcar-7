ceee9bb01bc17b7821c781ae829e54c40b343fe5
eunho <eunho1032@gmail.com>
2024-10-24 03:34:07 +0900

    feat(Application): 간단 구현을 위한 로직 추가, 주석 삭제 및 추가 구현 필요

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,22 @@
 package racingcar;
 
 /*
-전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
-자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
-우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.
-사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.
-*/
+간단한 로직 완료, 리팩토링 필요, 예외 처리 필요, class 파일 패키지별 분리 필요
+ */
 
-// 입력 : 경주할 자동차 이름 ,로 구분
-// 시도할 횟수
+import java.util.ArrayList;
+import java.util.List;
 
-// [x] 출력 : 차수별 실행 결과
-// [x] 단독 우승자 안내 문구 [최종 우승자 : pobi]
-// [x] 공동 우승자 안내 문구 [최종 우승자 : pobi, jun]
 public class Application {
 
 	public static void main(String[] args) {
+		String[] carNames = Input.inputCarNames();
+		int tryCounts = Input.inputTryCounts();
+
+		List<Car> cars = new ArrayList<>();
+		for (int i = 0; i < carNames.length; i++) {
+			cars.add(new Car(carNames[i]));
+		}
+		Output.printResult(cars, tryCounts);
 	}
 }
