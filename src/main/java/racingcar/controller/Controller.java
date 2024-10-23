10b5f39db4990be8ee9149f398244eb79fd19cca
choi14123 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-10-23 09:26:04 +0900

    feat: Controller 기능 구현

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,8 +1,9 @@
 package racingcar.controller;
 
-import java.util.Arrays;
 import racingcar.handler.InputHandler;
 import racingcar.handler.PrintHandler;
+import racingcar.model.Car.Cars;
+import racingcar.model.Referee;
 
 public class Controller {
     private final PrintHandler printHandler;
@@ -11,12 +12,20 @@ public class Controller {
     public Controller() {
         this.printHandler = new PrintHandler();
         this.inputHandler = new InputHandler();
+
     }
 
-    public void run () {
+    public void run() {
         printHandler.racingStartMessage();
-        String [] tempCarNames = inputHandler.inputCarNames();
+        String[] tempCarNames = inputHandler.inputCarNames();
         printHandler.racingNumberMessage();
+        Cars cars = new Cars(tempCarNames);
         int gameNumber = inputHandler.inputGameNumber();
+        int maxCarListSize = cars.getCarListSize();
+        Referee referee = new Referee(cars, maxCarListSize, gameNumber);
+        // 앞으로 구현해야 하는 거
+        // 자동차가 랜덤 숫자가 4 이상 일 때, 앞으로 간다.
+        // 그걸 어떤 방식으로 해야 자동차 이름 별 position 값을 올릴 수 있을까?
+        // position 어디에 위치해야 하는 것일까?
     }
 }
