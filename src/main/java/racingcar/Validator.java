1fe123ea5d29e3cf0e278c55f952d62d795fd50a
AwesomeMinsik <beaut3579@gmail.com>
2024-10-23 20:50:19 +0900

    refactor : 중복되는 검증 로직을 메서드로 추출하여 제거하였습니다
    - 검증 클래스 Validator 의 메서드의 Null, 문자값제한, 음수, 글자수제한 검증로직을 각각의 메서드로 분리하였습니다.

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,20 +1,47 @@
 package racingcar;
 
+import java.util.List;
+
 public class Validator {
 
-    public int validateInput(String cycleInput) {
-        int cycle;
-        if (cycleInput.isEmpty())
-            throw new IllegalArgumentException();
+    public void validateName(List<String> names) {
+        for (String name : names) {
+            validateNull(name);
+            validateLength(name);
+        }
+    }
 
+    public int validateCycle(String cycleInput) {
+        int cycle;
+        validateNull(cycleInput);
+        cycle = validateParseInt(cycleInput);
+        validateNegativeNumber(cycle);
+        return cycle;
+    }
+
+    private static void validateNull(String name) {
+        if (name == null || name.isEmpty())
+            throw new IllegalArgumentException();
+    }
+
+    private static void validateLength(String name) {
+        if (name.length()>5) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateNegativeNumber(int cycle) {
+        if (cycle <= 0)
+            throw new IllegalArgumentException();
+    }
+
+    private static int validateParseInt(String cycleInput) {
+        int cycle;
         try {
             cycle = Integer.parseInt(cycleInput);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
         }
-
-        if (cycle <= 0)
-            throw new IllegalArgumentException();
         return cycle;
     }
 }
