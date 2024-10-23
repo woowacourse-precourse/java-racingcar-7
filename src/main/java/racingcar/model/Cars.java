22fa8dca1b6cfea12c3bfc4b2a4894feb8f27224
Jin409 <sualng123@gmail.com>
2024-10-23 23:54:19 +0900

    refactor(Cars, Game): Game 이 일급 컬렉션인 Cars 를 참조하도록 변경해라
    Game 에서 List 가 아닌 Cars 일급 컬렉션의 형태로 자동차들의 상태를 관리하도록 변경:
    - Cars 가 불변성을 지키도록 한다.
    - 여러개의 자동차 객체로서 비지니스 로직을 가질 수 있도록 한다.

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,16 +1,31 @@
 package racingcar.model;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.SequencedMap;
 
 public class Cars {
-    private List<Car> cars;
+    private final List<Car> cars;
 
-    public Cars() {
+    public Cars(String[] names) {
         this.cars = new ArrayList<>();
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
     }
 
-    public void addCar(Car car) {
-        this.cars.add(car);
+    public SequencedMap<String, Integer> getStatus() {
+        SequencedMap<String, Integer> carStatusMap = new LinkedHashMap<>();
+        cars.forEach(car -> carStatusMap.put(car.getName(), car.getPosition()));
+        return carStatusMap;
+    }
+
+    public int getSize() {
+        return cars.size();
+    }
+
+    public void move(int index) {
+        cars.get(index).move();
     }
 }
--- src/main/java/racingcar/model/Game.java
+++ src/main/java/racingcar/model/Game.java
@@ -1,24 +1,17 @@
 package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.SequencedMap;
 
 public class Game {
     private static final int RANDOM_MIN_NUMBER = 0;
     private static final int RANDOM_MAX_NUMBER = 9;
     private static final int MOVEMENT_CRITERIA = 4;
-    private final List<Car> cars;
+    private final Cars cars;
     private int attemptCount;
 
     public Game(String[] carNames, int attemptCount) {
-        this.cars = new ArrayList<>();
-        for (String carName : carNames) {
-            cars.add(new Car(carName));
-        }
-
+        this.cars = new Cars(carNames);
         this.attemptCount = attemptCount;
     }
 
@@ -28,11 +21,11 @@ public class Game {
     }
 
     private void attemptToMoveCars() {
-        for (Car car : cars) {
+        for (int index = 0; index < cars.getSize(); index++) {
             int randomNumber = Randoms.pickNumberInRange(RANDOM_MIN_NUMBER, RANDOM_MAX_NUMBER);
 
             if (randomNumber >= MOVEMENT_CRITERIA) {
-                car.move();
+                cars.move(index);
             }
         }
     }
@@ -42,8 +35,6 @@ public class Game {
     }
 
     public SequencedMap<String, Integer> getStatusOfCars() {
-        SequencedMap<String, Integer> carStatusMap = new LinkedHashMap<>();
-        cars.forEach(car -> carStatusMap.put(car.getName(), car.getPosition()));
-        return carStatusMap;
+        return cars.getStatus();
     }
 }
