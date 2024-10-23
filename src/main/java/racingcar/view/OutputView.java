bea44e552f3e4af19061c85bbad2dc3666b54411
seunghee <seunghee_chae3@tmax.co.kr>
2024-10-23 19:41:17 +0900

    feat(OutputView): 회차별 차 이동 내역 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,21 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
+import java.util.List;
+
 public class OutputView {
 
     private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
+    private static final String CAR_MOVE = "%s : %s";
 
     public static void printExecutionResultMessage() {
         System.out.println(EXECUTION_RESULT_MESSAGE);
+    }
+
+    public static void printCarMove(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(String.format(CAR_MOVE, car.getName(), "-".repeat(car.getPosition())));
+        }
     }
 }
--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -1,5 +1,36 @@
 package racingcar.view;
 
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class OutputViewTest {
 
+    @Test
+    void 차_이동_내역_출력_테스트() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        Car car1 = new Car("pobi");
+        for (int i = 0; i < 4; i++) {
+            car1.move();
+        }
+        Car car2 = new Car("jun");
+        for (int i = 0; i < 2; i++) {
+            car2.move();
+        }
+        List<Car> cars = Arrays.asList(car1, car2);
+        String expectedOutput = "pobi : ----\njun : --\n";
+
+        OutputView.printCarMove(cars);
+        assertEquals(expectedOutput, outputStream.toString());
+        System.setOut(System.out);
+    }
 }
