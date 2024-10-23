507b4e4750968cd2190c59fa1d4ee63541b5d4f3
ParkHyunS00 <parkhyuns00@naver.com>
2024-10-23 17:02:03 +0900

    feat(Constant): 자동차 이름 입력값에 대한 에러 메시지 추가

--- src/main/java/racingcar/utils/Constant.java
+++ src/main/java/racingcar/utils/Constant.java
@@ -6,5 +6,9 @@ public class Constant {
     public static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String ATTEMPT_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
+    // 에러 메시지
+    public static final String CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차의 이름은 5자 이하만 가능합니다.";
+    public static final String CAR_NAME_EMPTY_ERROR_MESSAGE = "한 개 이상의 자동차 이름을 입력해야 합니다.";
+
     private Constant() {}
 }
