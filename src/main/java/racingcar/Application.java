f0d0b79bad0014f1648a029140b75614559f47a1
mingyeongbae <bmg1203@naver.com>
2024-10-23 15:49:41 +0900

    [feat]: 입력 기능 추가
    경주할 자동차 문자열과 시도 회수를 입력하는 기능입니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static String INVAILD_INPUT_MESSAGE = "잘못된 입력입니다. ";
+    static List<String> carNames = new ArrayList<>();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 입력
+        input();
+    }
+
+    private static void input() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        carNames = List.of(Console.readLine().split(","));
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String attemptCountStr = Console.readLine();
     }
 }
