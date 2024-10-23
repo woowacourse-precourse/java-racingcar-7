3269ac773813568f0d08c68d79f6d0e0224c0901
bowook <mbosx316@naver.com>
2024-10-24 00:20:26 +0900

    feat(RaceController) : RaceController 기능 추가
    - domain과 view의 중간 역할 기능 수행

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,24 +1,32 @@
 package racingcar.controller;
 
-import racingcar.domain.CarNameValidator;
-import racingcar.domain.Cars;
-import racingcar.domain.TrialCounts;
-import racingcar.domain.TrialCountsValidator;
+import racingcar.domain.*;
+import racingcar.domain.validator.CarNameValidator;
+import racingcar.domain.validator.TrialCountsValidator;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RaceController {
     private final CarNameValidator carNameValidator = new CarNameValidator();
     private final TrialCountsValidator trialCountsValidator = new TrialCountsValidator();
+    private final OutputView outputView = new OutputView();
 
     public void start() {
-        getInput();
-    }
-
-    private void getInput() {
         String carNames = InputView.inputCarNames();
         Cars cars = new Cars(carNames, carNameValidator);
 
         String counts = InputView.inputTrialCounts();
         TrialCounts trialCounts = new TrialCounts(counts, trialCountsValidator);
+
+        InputView.closeConsole();
+
+        Race race = new Race(cars, trialCounts);
+
+        outputView.executionResultMessage();
+        while (race.isRaceOngoing()) {
+            race.raceOneStep();
+            outputView.currentStatusMessage(cars);
+        }
+        outputView.finalWinnerMessage(race.getWinners());
     }
 }
