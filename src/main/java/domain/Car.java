bf75adfa75e3f341eee845fb0ba40a9f6fc6faae
eeekeee <ekrtionl@gmail.com>
2024-10-23 14:34:37 +0900

    test: 자동차 목록 반환 테스트

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Car {
     private final String name;
     private int score = 0;
@@ -18,5 +20,22 @@ public class Car {
 
     public void addScore() {
         score += 1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return score == car.score && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
     }
 }
---
+++ src/test/java/service/RacingServiceTest.java
@@ -0,0 +1,41 @@
+package service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import domain.Car;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RacingServiceTest {
+    private RacingService racingService;
+
+    @BeforeEach
+    void setUp() {
+        racingService = new RacingService();
+    }
+
+    @Test
+    @DisplayName("정확한 자동차 목록을 반환")
+    void checkGetExactCarsList() {
+        String input = "c1,c2,c3";
+        Car car1 = new Car("c1");
+        Car car2 = new Car("c2");
+        Car car3 = new Car("c3");
+
+        List<Car> carsList = racingService.getCarsList(input);
+
+        assertThat(carsList)
+                .hasSize(3)
+                .contains(car1, car2, car3);
+    }
+
+    @Test
+    @DisplayName("자동차가 전진을 성공했는지 확인")
+    void checkCarsMovement() {
+    }
+
+}
