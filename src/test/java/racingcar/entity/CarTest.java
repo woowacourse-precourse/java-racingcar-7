2ad513f94b225f65c581d712b23e00cb2d58effe
7i.w__k <tizmfns1218@Naver.com>
2024-10-23 15:15:01 +0900

    test(car, winners): domain test

---
+++ src/test/java/racingcar/entity/CarTest.java
@@ -0,0 +1,24 @@
+package racingcar.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    @Test
+    @DisplayName("출력 결과 확인 테스트")
+    void carTest1() {
+        // given
+        String name = "car";
+        Car car = new Car("car", 5L);
+
+        // when
+        String result = car.print();
+
+        // then
+        assertThat(result).isEqualTo(name + " : " + "-----");
+    }
+
+}
---
+++ src/test/java/racingcar/entity/WinnersTest.java
@@ -0,0 +1,36 @@
+package racingcar.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static racingcar.utils.Constant.WINNER_RESULT;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class WinnersTest {
+
+    @Test
+    @DisplayName("우승자 출력 테스트")
+    void printWinners() {
+
+        // given
+        List<Car> test = new ArrayList<>();
+
+        Car pobi = new Car("pobi");
+        Car woni = new Car("woni");
+
+        test.add(pobi);
+        test.add(woni);
+
+        CarList carList = new CarList(test);
+
+        // when
+        Winners winners = carList.toWinners();
+        String result = winners.print();
+
+        // then
+        assertThat(result).isEqualTo(WINNER_RESULT + carList);
+    }
+}
