50d526b74092a295230c0b44927c8df0078ceb3d
dongyun <ehddbs452100@naver.com>
2024-10-23 20:36:13 +0900

    feat(service package): split car names by ","
    - 자동차 이름을 ","로 split하여 추후 필터 및 변경을 위해 List형태로 변경해서 반환

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,5 +1,15 @@
 package racingcar.service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RacingService {
+
+    private final String COMMA = ",";
+
+    public List<String> splitCarName(String input) {
+        return Arrays.stream(input.split(COMMA)).collect(Collectors.toList());
+    }
 
 }
