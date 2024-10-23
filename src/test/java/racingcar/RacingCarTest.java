25ca79442651379c17d8ef8f365b9afaed083b16
jseom <2ujs4311@gmail.com>
2024-10-23 13:44:54 +0900

    TEST: 자동차이름_쉼표기준으로_나누기 테스트 코드 추가
    - 영어와 쉼표(,)로 이루어진 문자열을 테스트 코드에 추가했습니다.
    - 기존 구성(영어와 쉼표)에 한글이 추가된 문자열을 테스트 코드에 추가했습니다.
    - 기존 구성(영어와 쉼표와 한글)에 쉼표를 제외한 기호가 추가된 문자열을 추가했습니다.
    - 쉼표가 연속으로 주어진 문자열을 테스트 코드에 추가했습니다.

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -2,11 +2,15 @@ package racingcar;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class RacingCarTest {
   @Test
-    void makeCarNameList(String str) {
-      assertEquals(["pobi","woni","jun"], str.split(","));
+  void 자동차이름_쉼표기준으로_나누기() {
+      assertEquals(List.of("pobi","woni","jun"), RacingCar.makeNameList("pobi,woni,jun"));
+      assertEquals(List.of("월","woni","jun"), RacingCar.makeNameList("월,woni,jun"));
+      assertEquals(List.of(";=;","월","jun"), RacingCar.makeNameList(";=;,월,jun"));
+      assertEquals(List.of("hi","+=+","jun"), RacingCar.makeNameList("hi,,+=+,jun"));
   }
 }
