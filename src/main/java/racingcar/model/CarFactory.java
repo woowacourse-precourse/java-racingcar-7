bcdb4998b11602cb327896eb2711ffdabb1bfcc7
Hacanna42 <dtmdgus901@gmail.com>
2024-10-23 18:41:23 +0900

    refactor(CarFactory): remove static keyword from createCars()
    createCars() 메서드의 static을 제거했습니다. createCars()는 비록 필드가 없지만
    전역적으로 사용되는 것이 아닌 Application.main()에서만 사용되기 때문에
    인스턴스를 생성해서 호출하는 것이 더 적합하다고 판단했습니다.
    
    다른 예로 util/의 StringParser와 Validator는 필드가 필요 없는 기능성 클래스지만
    역시 static으로 선언하지 않았습니다. 왜냐하면 나중에 자동차 이름 입력 규칙
    등의 변경 사항이 생기더라도, InputHandler에 의존성을 주입함으로써
    단순히 새 Validator나 StringParser을 만들고 DI된 모듈을 교체하는 것으로
    변경 작업을 마무리할 수 있기 때문입니다.

--- src/main/java/racingcar/model/CarFactory.java
+++ src/main/java/racingcar/model/CarFactory.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CarFactory {
-    public static List<Car> createCars(List<String> carNames, MoveStrategy moveStrategy) {
+    public List<Car> createCars(List<String> carNames, MoveStrategy moveStrategy) {
         return carNames.stream()
                 .map(carName -> new Car(moveStrategy, carName))
                 .collect(Collectors.toList());
