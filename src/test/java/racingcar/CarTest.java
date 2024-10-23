a78b0d27fb60c47da41bd808bd6e02fd591392ad
koo-sangwoo <luckytkddn@gmail.com>
2024-10-24 00:14:47 +0900

    test : 참여 자동차 대수 구하기 기능 테스트
    - 참여한 자동차 대수를 의도대로 구하는지에 대한 테스트 코드를 작성했다.

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.car.Car;
 
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.assertj.core.api.Assertions.*;
 
 /**
  * @author ddaerae
@@ -20,7 +23,7 @@ class CarTest {
     @DisplayName("주어진 라이브러리의 함수가 원하는 값을 리턴하는지")
     void 라이브러리_테스트() {
         assertDoesNotThrow(() -> {
-            int[] ranNumArray = new int[30];
+            var ranNumArray = new int[30];
             for (int i = 0; i < 30; i++) {
                 int ranNum = Randoms.pickNumberInRange(0, 9);
                 ranNumArray[i] = ranNum;
@@ -35,19 +38,19 @@ class CarTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {0,3,4,5})
+    @ValueSource(ints = {0, 3, 4, 5})
     @DisplayName("4 이상이면 - 기호 리턴, 아니면 공백리턴")
     void 전진_기호_리턴(int ranNum) {
         String result;
-        if(ranNum >= 4){
+        if (ranNum >= 4) {
             result = "-";
-        }else{
+        } else {
             result = "";
         }
 
         if (ranNum >= 4) {
             assertThat(result).isEqualTo("-");
-        }else{
+        } else {
             assertThat(result).isEmpty();
         }
     }
@@ -56,15 +59,21 @@ class CarTest {
     @ValueSource(ints = {9, 8, 7, 6, 5, 4, 3, 2, 1})
     @DisplayName("차량의 전진 OR 정지 판단")
     void 전진_아니면_정지(int number) {
+        var result = Car.goOrStop(number);
 
-        String result = new String();
-
-        result += Car.goOrStop(number);
-
-        if(number >= 4)
+        if (number >= 4)
             assertThat(result).isEqualTo("-");
         else
             assertThat(result).isEmpty();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"koo,sang,woo"})
+    @DisplayName("참여하는 자동차의 대수를 정확하게 구한다.")
+    void 참여_대수_구하기(String participatingCars) {
+        var carsList = Arrays.stream(participatingCars.split(",")).toList();
+
+        assertThat(carsList.size()).isSameAs(3);
     }
 
 }
