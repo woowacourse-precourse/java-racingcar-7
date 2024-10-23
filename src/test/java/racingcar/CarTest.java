f0243aa9d14dba04add3a23b3f8b2f05e0cee5b9
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2024-10-23 13:39:14 +0900

    test : 자동차 유효성 체크 테스트 추가

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Cars;
+
+public class CarTest extends NsTest{
+
+    @Test
+    void 자동차_개수_테스트() {
+        Cars cars = new Cars("핍비,피비");
+        assertThat(cars.getCarCount()).isEqualTo(2);
+    }
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
