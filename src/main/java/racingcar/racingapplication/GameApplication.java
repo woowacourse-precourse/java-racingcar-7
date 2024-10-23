a4cd68582281a61de8e14a173618b95da3479085
Sumin <new.sumyang@gmail.com>
2024-10-23 16:47:55 +0900

    refactor(racingapplication): 결과 출력 수정

--- src/main/java/racingcar/racingapplication/GameApplication.java
+++ src/main/java/racingcar/racingapplication/GameApplication.java
@@ -30,7 +30,7 @@ public class GameApplication {
 
         List<Cars> afterRaceCars = RacingApplication.race(gameNumber,
             Cars.makeOriginCars(carNames));
-        Result result = Result.of(afterRaceCars);
+        Result result = Result.of(afterRaceCars.subList(1, afterRaceCars.size()));
         List<String> winners = result.findWinners();
         printer.printAfterGameResult(result.finalResultCars());
         printer.printFinalWinner(winners);
