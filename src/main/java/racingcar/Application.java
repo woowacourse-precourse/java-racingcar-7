ff1f27f4f21ff5b6e55da0b7052bb7887c1f00d6
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2024-10-23 21:05:54 +0900

    feat: 주사위를 굴려 움직여야 하는 거리를 구하는 기능 제작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -31,9 +31,18 @@ public class Application {
         return carList;
     }
 
+    static int tryRoll(){
+        int result = Randoms.pickNumberInRange( 0, 9 );
+        if( result >= 4 ) return result;
+        else return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽고(,) 기준으로 구분)");
+        System.out.println( "경주할 자동차 이름을 입력하세요.(이름은 쉽고(,) 기준으로 구분)" );
         String input = Console.readLine();
         List<Car> carList = createCarList( input );
+        int count = Integer.parseInt( Console.readLine() );
+
+        System.out.println( "실행 결과" );
     }
 }
