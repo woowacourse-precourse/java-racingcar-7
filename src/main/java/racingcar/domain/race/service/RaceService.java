fbec7a81ebbe959515d00a7d0050b122e6b78d55
Geonwoo Pack <packdev937@gmail.com>
2024-10-23 18:29:27 +0900

    feat (race) : 최종 우승자를 계산한다

---
+++ src/main/java/racingcar/domain/race/service/RaceService.java
@@ -0,0 +1,22 @@
+package racingcar.domain.race.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.car.Car;
+import racingcar.domain.race.Race;
+
+public class RaceService {
+
+    public List<Car> findWinners(Race race) {
+        List<Car> cars = race.getCars();
+
+        int maxPosition = cars.stream()
+            .mapToInt(car -> car.getPosition())
+            .max()
+            .orElseThrow(IllegalArgumentException::new);
+
+        return cars.stream()
+            .filter(car -> car.getPosition() == maxPosition)
+            .collect(Collectors.toList());
+    }
+}
--- src/test/java/racingcar/RaceServiceTest.java
+++ src/test/java/racingcar/RaceServiceTest.java
@@ -1,27 +1,29 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.*;
-import static racingcar.race.constants.RaceCriterion.*;
+import static racingcar.domain.race.constants.RaceCriterion.MOVING_FORWARD;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import racingcar.car.Car;
-import racingcar.race.Race;
+import racingcar.domain.car.Car;
+import racingcar.domain.race.Race;
+import racingcar.domain.race.service.RaceService;
 
 public class RaceServiceTest {
 
-    private final RaceService raceService;
+    private RaceService raceService;
 
     @BeforeEach
     void setup() {
         raceService = new RaceService();
     }
+
     @Test
     void 우승자를_계산한다() {
         // given
-        String input = "eric, jack";
-        Race race = Race.of(input, new FixedAcceleration(MOVING_FORWARD.getCriterion()))
+        String input = "eric,jack";
+        Race race = Race.of(input, new FixedAcceleration(MOVING_FORWARD.getCriterion()));
         race.lap();
 
         // when
