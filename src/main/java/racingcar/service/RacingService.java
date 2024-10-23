6e80066e903f38357ba7531835d35555dc0441cd
dongyun <ehddbs452100@naver.com>
2024-10-23 22:15:38 +0900

    feat(RacingService): validate car names if null or not
    - 자동차 이름 데이터들이 null이거나 공백이 있으면 false를 반환하고 자동차 이름 입력에 오류가 발생했다는 걸 알림

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,8 +1,11 @@
 package racingcar.service;
 
+import static racingcar.global.error.Error.NAME_BLANK_EXCEPION;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import racingcar.global.error.NameError;
 
 public class RacingService {
 
@@ -10,6 +13,18 @@ public class RacingService {
 
     public List<String> splitCarName(String input) {
         return Arrays.stream(input.split(COMMA)).collect(Collectors.toList());
+    }
+
+    public void validateCarName(List<String> carNames) {
+
+        if (!validateNameNotNull(carNames)) {
+            throw new NameError(NAME_BLANK_EXCEPION);
+        }
+    }
+
+    private boolean validateNameNotNull(List<String> carNames) {
+
+        return carNames.stream().allMatch(name -> name != null && !name.isEmpty());
     }
 
 }
