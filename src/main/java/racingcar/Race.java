2652aac1f9e89c222e318ca3ebc70ac7fc85eef9
jseom <2ujs4311@gmail.com>
2024-10-23 20:24:14 +0900

    FEAT(Race): 자동차의 전진 및 정지 여부를 반환하는 메서드 구현
    - 0이상 9이하의 자연수에서 무작위 수를 뽑아 4 이상일 경우 true를 반환합니다.
    - true는 전진, false는 정지를 의미합니다.

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Race {
     private final int totalRaceTurn;
 
@@ -8,5 +10,19 @@ public class Race {
             throw new IllegalArgumentException("자동차 경주는 1회 이상 진행되어야 합니다.");
         }
         this.totalRaceTurn = totalRaceTurn;
+    }
+
+    private static int pickRandomNumber() {
+        int pickedNumber = Randoms.pickNumberInRange(0, 9);
+        return pickedNumber;
+    }
+
+    public static boolean moveForwardOrNot() {
+        int pickedNumber = pickRandomNumber();
+
+        if (pickedNumber >= 4) {
+            return true;
+        }
+        return false;
     }
 }
