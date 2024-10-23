124894ad0f874bfa78995a10940a913c2fe2e2bf
dhrgodms <dhrgodms123@gmail.com>
2024-10-23 13:17:35 +0900

    feat: Add 1-자동차 이름 처리 method
    - Car 객체의 name에 들어갈 입력 문자열의 유효성을 검증
    - 유효성 검증 후 배열로 변환
    - 예정) 유효성 검증 로직을 따로 추출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,48 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CarRacing carRacing = new CarRacing();
+        carRacing.init();
+    }
+
+    public class Car {
+        public Car(String name) {
+        }
+    }
+
+    public static class CarRacing {
+
+        public void init() {
+            String nameInput = Console.readLine();
+            convertCarNames(nameInput);
+        }
+
+        ArrayList<String> convertCarNames(String nameInput) {
+            ArrayList<String> carNames;
+
+            // nameInput이 비어있거나, 공백이거나, 문자+숫자+언더스코어(_)+쉼표(,) 제외하고 존재하는 경우
+            if (nameInput.isEmpty() || nameInput.isBlank() || !nameInput.matches("[\\w\\s,]+")) {
+                throw new IllegalArgumentException();
+            }
+
+            try {
+                carNames = new ArrayList<>(Arrays.asList(nameInput.split(",")));
+
+                // 자동차 이름이 하나만 입력된 경우 -> 경주를 진행할 수 없다.
+                if (carNames.size() < 2) {
+                    throw new IllegalArgumentException();
+                }
+            } catch (Exception e) {
+                throw new IllegalArgumentException();
+            }
+            return carNames;
+        }
     }
 }
