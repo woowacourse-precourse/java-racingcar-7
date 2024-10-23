eeb7c2670f5f84b3e04bd13a54757409d7f2a2c0
StoneCAU <seokwoo1999@naver.com>
2024-10-23 23:23:06 +0900

    test: CarNameParser 테스트 추가
    - 쉼표를 기준으로 구분되어 반환되는지 테스트

---
+++ src/test/java/racingcar/InputTest.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import racingcar.util.CarNameParser;
+
+import java.util.List;
+
+public class InputTest {
+
+    @Test
+    void 자동차_이름은_쉼표를_기준으로_구분한다() {
+        // given
+        final String input = "pobi,woni,jun";
+
+        // when
+        final List<String> carNames = CarNameParser.parse(input);
+
+        // then
+        assertThat(carNames).containsExactly("pobi", "woni", "jun");
+    }
+}
