72000dda85e8ff16bb421f59770a4755748dab07
immyeong <wpfls6670@naver.com>
2024-10-23 12:03:06 +0900

    feat: 자동차 이름 입력값 검증 함수

--- src/main/java/racingcar/model/service/InputService.java
+++ src/main/java/racingcar/model/service/InputService.java
@@ -1,9 +1,36 @@
 package racingcar.model.service;
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.dto.Car;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputService {
 
     public String inputValue(){
         return Console.readLine();
     }
+
+    public List<Car> inputValidator(String carValue, String countValue){
+        if(carValue.isEmpty() || countValue.isEmpty() || carValue == null || countValue == null){
+            throw new IllegalArgumentException("입력값이 누락되었습니다.");
+        }
+
+        String[] cars = splitCarValue(carValue);
+        List<Car> carList = new ArrayList<>();
+        for (String car : cars) {
+            if(car.length() > 5){
+                throw new IllegalArgumentException("자동차 이름의 입력값은 최대 5자입니다.");
+            }
+            Car carInfo = new Car(car, 0);
+            carList.add(carInfo);
+        }
+
+        return carList;
+    }
+
+    private String[] splitCarValue(String carValue){
+        return carValue.split(",");
+    }
+
 }
