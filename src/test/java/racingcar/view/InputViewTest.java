016ef22b220270fdc1d3d038a43c7b233e85972a
BaSak0630 <babamba125@gmail.com>
2024-10-24 06:01:41 +0900

    refactor : Change test file directory
    test 파일 디렉토릴 변경

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,8 +1,9 @@
-package racingcar;
+package racingcar.view;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.Application;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
