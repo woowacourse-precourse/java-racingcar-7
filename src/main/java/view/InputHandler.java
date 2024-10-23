94fec2bd00837c1b1aa9c9bd4b31187d7ca7c429
Wooseok Jang <wowseok2774@gmail.com>
2024-10-23 19:13:46 +0900

    feat: 사용자 입력을 처리하는 InputHandler 클래스 추가

--- src/main/java/view/InputHandler.java
+++ src/main/java/view/InputHandler.java
@@ -1,15 +1,29 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputHandler {
 
-    public String inputNames() {
-        return Console.readLine();
+    public List<String> getCarNames() {
+        String carNames = Console.readLine();
+        return parseCarNames(carNames);
     }
 
-    public String inputAttemp() {
-        return Console.readLine();
+    public int getAttemp() {
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public List<String> parseCarNames(String carNames) {
+        String[] nameArray = carNames.split(",");  // 쉼표로 나누어 배열 생성
+        List<String> carNameList = new ArrayList<>();
+
+        for (String name : nameArray) {
+            carNameList.add(name);
+        }
+
+        return carNameList;
     }
 }
 
