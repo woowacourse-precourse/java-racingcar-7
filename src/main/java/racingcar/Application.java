e7949f148a8d5548a700dfdac016c80a6999096c
je488 <pje488@naver.com>
2024-10-23 22:40:10 +0900

    feat: 무작위 값이 4 이상인 경우 자동차를 1만큼 전진시키는 메서드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,22 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Application {
+    static Map<String, Integer> car = new HashMap<>();
+
     public static int makeRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
+
+    public static void moveOrStop(String carName, int randomNumber) {
+        if (randomNumber >= 4) {
+            car.put(carName, car.get(carName)+1);
+        }
+    }
+
     public static void main(String[] args) {
 
     }
