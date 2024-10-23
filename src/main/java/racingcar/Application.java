b6708eacd5b643450adc8f40bb5cad3affccf07e
je488 <pje488@naver.com>
2024-10-23 22:56:10 +0900

    feat: 자동차 이름을 입력하라는 안내 메시지를 출력하는 메서드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,17 +7,30 @@ import java.util.Map;
 public class Application {
     static Map<String, Integer> car = new HashMap<>();
 
+    public static void printInputMessageCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
     public static int makeRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
 
     public static void moveOrStop(String carName, int randomNumber) {
         if (randomNumber >= 4) {
-            car.put(carName, car.get(carName)+1);
+            car.put(carName, car.get(carName) + 1);
         }
     }
 
     public static void main(String[] args) {
-
+        printInputMessageCarName();
+        car.put("박", 0);
+        car.put("재", 0);
+        car.put("은", 0);
+        int randomNumber = makeRandomNumber();
+        System.out.println(randomNumber);
+        moveOrStop("박", randomNumber);
+        for (String key : car.keySet()) {
+            System.out.println(key + ":" + car.get(key));
+        }
     }
 }
