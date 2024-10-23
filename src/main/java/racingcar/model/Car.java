9fcb1f224682e191dadb2e408f96e6055e38b900
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-10-24 00:55:19 +0900

    feat(car): add basic methods and domain logics
    - race method for move car from movementValue
    - isSamePosition method for comparing position of a car
    - equals, hashCode

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,51 @@
+package racingcar.model;
+
+import java.util.Objects;
+
+public class Car {
+
+    private final Name name;
+    private final Position position;
+    private final CarMovementPolicy movementPolicy;
+
+    public Car(final Name name, final CarMovementPolicy movementPolicy) {
+        this.name = name;
+        this.position = Position.defaultValue();
+        this.movementPolicy = movementPolicy;
+    }
+
+    public void race(final int movementValue) {
+        if (movementPolicy.isPossibleToMove(movementValue)) {
+            position.moveForward();
+        }
+    }
+
+    public boolean isSamePosition(final Car car) {
+        return this.position.equals(car.position);
+    }
+
+    public String name() {
+        return name.value();
+    }
+
+    public int position() {
+        return position.value();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
