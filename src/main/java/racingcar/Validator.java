40dbd0ac7ad1e29e15cc0b07bff75530123d174f
SeongbinPark <hyucse22@gmail.com>
2024-10-24 07:16:51 +0900

    fix(Validator): 자동차 이름 중복시 예외 발생
    대소문자 구분없이 중복을 체크, -> 추후 변경 가능성

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public final class Validator {
     private Validator() {
     } // Util Class
@@ -8,18 +11,25 @@ public final class Validator {
         if (names.length == 0) {
             throw new IllegalArgumentException("적어도 한 대의 자동차 이름을 입력해야 합니다.");
         }
+
+        Set<String> uniqueNames = new HashSet<>(); // 중복 체크용 Set
         for (String name : names) {
-            validateCarName(name);
+            if (name.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 빈 문자열일 수 없습니다.");
+            }
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+            }
+            if (hasSameCarNames(uniqueNames, name)) {
+                throw new IllegalArgumentException("자동차 이름에 중복이 존재합니다");
+            }
         }
     }
 
-    private static void validateCarName(String name) {
-        if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름은 빈 문자열일 수 없습니다.");
-        }
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
-        }
+    private static boolean hasSameCarNames(Set<String> uniqueNames, String name) {
+        String lowerCaseName = name.toLowerCase();
+
+        return !uniqueNames.add(lowerCaseName); // 같은 이름이 존재하면
     }
 
     public static void validateAttempts(int attempts) {
