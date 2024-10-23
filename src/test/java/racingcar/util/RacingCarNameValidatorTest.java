044eb3f9bfc303874aea2af103b009ec8cbdd476
qudcks8084 <qudcks8084@naver.com>
2024-10-23 17:13:29 +0900

    Test : add 5 Test case to check Validation
    - case 1 : there is name which is longer than 5
    - case 2 : there is empty name in middle
    - case 3 : there is empty name in front
    - case 4 : there is empty name in end
    - case 5 : there is Duplicate name

---
+++ src/test/java/racingcar/util/RacingCarNameValidatorTest.java
@@ -0,0 +1,70 @@
+package racingcar.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static racingcar.util.RacingCarNameValidator.checkRacingCarNameListIsAvailable;
+
+class RacingCarNameValidatorTest {
+    @Test
+    @DisplayName("Case 1: 5 이상의 길이를 가지는 값이 있는 경우")
+    void CarRacingNameTestCase_Length() {
+        // given
+        String input = "nameIs,hello,test";  // 5자를 초과하는 값이 있는 경우
+
+        // then
+        assertThrows(IllegalArgumentException.class,
+                // when
+                () -> checkRacingCarNameListIsAvailable(input));
+    }
+
+    @Test
+    @DisplayName("Case 2: 중간에 길이가 0인 값이 있는 경우")
+    void CarRacingNameTestCase_EmptyName() {
+        // given
+        String input = "name,,test";  // 중간에 빈 문자열이 있는 경우
+
+        // then
+        assertThrows(IllegalArgumentException.class,
+                // when
+                () -> checkRacingCarNameListIsAvailable(input));
+    }
+
+    @Test
+    @DisplayName("Case 2: 앞에 빈 문자열이 있는 경우")
+    void CarRacingNameTestCase_EmptyNameAtStart() {
+        // given
+        String input = ",name,test";  // 처음에 빈 문자열이 있는 경우
+
+        // then
+        assertThrows(IllegalArgumentException.class,
+                // when
+                () -> checkRacingCarNameListIsAvailable(input));
+    }
+
+    @Test
+    @DisplayName("Case 3: 뒤에 빈 문자열이 있는 경우")
+    void CarRacingNameTestCase_EmptyNameAtEnd() {
+        // given
+        String input = "name,test,";  // 마지막에 빈 문자열이 있는 경우
+
+        // then
+        assertThrows(IllegalArgumentException.class,
+                // when
+                () -> checkRacingCarNameListIsAvailable(input));
+    }
+
+    @Test
+    @DisplayName("Case 4: 중복 문자열이 존재하는 경우")
+    void CarRacingNameTestCase_Duplicate() {
+        // given
+        String input = "name,test,name";  // 마지막에 빈 문자열이 있는 경우
+
+        // then
+        assertThrows(IllegalArgumentException.class,
+                // when
+                () -> checkRacingCarNameListIsAvailable(input));
+    }
+}
