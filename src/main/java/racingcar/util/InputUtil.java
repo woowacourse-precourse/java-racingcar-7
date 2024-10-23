8441557f9c2d63cc367c65f57d46768055bd2c6b
dltmddus <tmddus4671@gmail.com>
2024-10-23 21:28:08 +0900

    feat: 유저 입력 Input 유틸 구현

---
+++ src/main/java/racingcar/util/InputUtil.java
@@ -0,0 +1,10 @@
+package racingcar.util;
+
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputUtil {
+    public String userInput(){
+        return Console.readLine();
+    }
+}
