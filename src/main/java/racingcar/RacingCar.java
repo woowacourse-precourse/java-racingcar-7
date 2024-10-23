9ba27352b298679fb04f84d8b120aef83d4f8085
jseom <2ujs4311@gmail.com>
2024-10-23 15:20:54 +0900

    FEAT(RacingCar): 잘못된 값 입력되면 에러 발생시키는 메서드 추가
    잘못됐다고 정의한 값이 입력되면 IllegalArgumentException을 발생시키는 메서드를 추가했습니다.
    메서드 이름: throwError

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -6,7 +6,15 @@ import java.util.List;
 public class RacingCar {
 
     public static ArrayList makeNameList(String str) {
-        ArrayList<String> nameList = new ArrayList<>(List.of(str.split(",")));
+        ArrayList<String> nameList = new ArrayList<>(List.of(str.split(",", -1)));
+        throwError(nameList);
+
         return nameList;
+    }
+
+    private static void throwError(ArrayList<String> nameList) {
+        if (nameList.contains("")) {
+            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
+        }
     }
 }
