6d205b78a901e230bc5b0dce269da0511e49883c
MinJun Choi <mj043000@naver.com>
2024-10-24 00:48:12 +0900

    feat: 출력 기능 구현 - 자동차의 이름을 받는 문구 출력하기 - 시도 횟수를 입력받는 문구 출력하기 - 각 차수별 진행사항 출력하기 - 종료 후 우승자 목록 출력하기

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,25 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.service.RacingCarService;
+
+public class OutputView {
+
+
+
+    public void printStartMessage() {
+        System.out.println("실행 결과");
+    }
+
+    public void printRacingProcess(RacingCarService racingCarService) {
+        for (Car car : racingCarService.getCars().getCars()) {
+            System.out.println(car.getName().toString() + " : " + "-".repeat(car.getPosition().getValue()));
+        }
+        System.out.println();
+    }
+
+    public void printWinners(RacingCarService racingCarService) {
+        System.out.println("최종 우승자 : " + String.join(", ", racingCarService.findWinnersName()));
+    }
+}
