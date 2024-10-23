45270187685406abe272b88ccb91390d78ae120d
0jun <bill5500@naver.com>
2024-10-23 17:03:12 +0900

    feat(input): add functionality to accept car names and attempt counts
    Implemented the input functionality to accept the names of racing cars,
    separated by commas, and the number of attempts for the race. The car
    names are stored in an array, and the number of cars is determined
    based on the input.
    
    Additionally, a Car class will be created in a future update.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
+
+        int carNum = carNames.length;
+        Car[] cars = new Car[carNum];
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int num = Integer.parseInt(Console.readLine());
+        for (int i = 0; i < carNum; i++) {
+            cars[i] = new Car(carNames[i]);
+        }
 }
