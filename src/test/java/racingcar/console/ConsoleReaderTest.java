0d4fd5093e4dfc9efb778ee8b361354183ddceb4
ufo9430 <ufo9430@naver.com>
2024-10-22 23:31:37 -0400

    test: 이름 입력에 따른 유효성 검증 테스트 작성

---
+++ src/test/java/racingcar/console/ConsoleReaderTest.java
@@ -0,0 +1,23 @@
+package racingcar.console;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ConsoleReaderTest {
+    @Test
+    void 구분자_사이_문자가_없는지_검증한다(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                ConsoleReader.read("abc,,umu"));
+    }
+    @Test
+    void 중복되는_이름이_있는지_검증한다(){
+        Assertions.assertThrows(IllegalArgumentException.class, ()->
+                ConsoleReader.read("abc,abc"));
+    }
+
+    @Test
+    void 이름이_5자_이상인지_검증한다(){
+        Assertions.assertThrows(IllegalArgumentException.class, ()->
+                ConsoleReader.read("abcdefg"));
+    }
+}
