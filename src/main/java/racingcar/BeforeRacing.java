505f06ac1ac3dac987643dbe23a19d6e547f2564
howareyoo <newworld121@naver.com>
2024-10-24 00:54:08 +0900

    feat : 이름 5자 이하 검증

--- src/main/java/racingcar/BeforeRacing.java
+++ src/main/java/racingcar/BeforeRacing.java
@@ -5,13 +5,23 @@ import java.util.List;
 
 public class BeforeRacing {
 
-    public static List<String> divideCarName(String carName) {
+    public static List<String> divideCarName(String carNameString) {
         List<String> carNameList = new ArrayList<>();
-        String[] split = carName.split(",");
-        for (String name : split) {
-            carNameList.add(name.trim());
+        String[] splitName = carNameString.split(",");
+        for (String name : splitName) {
+            String trimName;
+            trimName = name.trim();
+            validateName(trimName);
+            carNameList.add(trimName);
         }
         return carNameList;
+    }
+
+    public static void validateName(String carName) {
+        int length = carName.length();
+        if (length >= 6) {
+            throw new IllegalArgumentException("이름이 5자 이하가 아닙니다.");
+        }
     }
 
 }
