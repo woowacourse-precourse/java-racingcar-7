bbe5ca5187e2beadf18a6e9fb2c3d9ae9e4d3140
minjun <ijmtylhtf@naver.com>
2024-10-23 16:17:50 +0900

    feat : application에서 Cars 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,14 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.car.Cars;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
+        Cars cars = new Cars(carNames);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String numberOfRaces = Console.readLine();
