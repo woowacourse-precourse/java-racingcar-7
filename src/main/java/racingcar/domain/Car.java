47ebc10522488eabff92e4fc7516b07ecc3ac008
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-10-23 18:56:42 +0900

    feat: 랜덤값에 따라 distance를 증가시키는 함수 추가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,22 @@
+package racingcar.domain;
+
+public class Car {
+    private static final int MINIMUM_ADVANCE_VALUE = 4;
+
+    private final String name;
+    private int distance;
+
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    private void validateName(){}
+    
+    public void setDistance(int randomNumber) {
+        if (randomNumber >= MINIMUM_ADVANCE_VALUE) {
+            this.distance += 1;
+        }
+    }
+}
