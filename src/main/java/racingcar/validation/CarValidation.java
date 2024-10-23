391c737a3fcad241130657ea58c6e6e21d96ca28
JS <zks145@naver.com>
2024-10-23 23:28:24 +0900

    feat (validation): CarValidation 자동차 이름에 특수문자 체크 기능

--- src/main/java/racingcar/validation/CarValidation.java
+++ src/main/java/racingcar/validation/CarValidation.java
@@ -11,6 +11,8 @@ import static racingcar.exception.ErrorMessage.*;
 public class CarValidation {
 
     private static final Integer MAX_LENGTH = 5;
+    private static final Pattern REGEX = Pattern.compile("[!@#$%^&*]");
+
 
     private static void checkNullOrEmpty(String carName) {
         if (carName == null || carName.isEmpty()) {
@@ -21,6 +23,13 @@ public class CarValidation {
     private static void checkOverLength(String carName) {
         if (carName.length() > MAX_LENGTH) {
             throw new IllegalArgumentException(OVER_LENGTH_FOUND.getMessage());
+        }
+    }
+
+    private static void checkRegex(String carName) {
+        Matcher matcher = REGEX.matcher(carName);
+        if (matcher.find()) {
+            throw new IllegalArgumentException(REGEX_FOUND.getMessage());
         }
     }
 
