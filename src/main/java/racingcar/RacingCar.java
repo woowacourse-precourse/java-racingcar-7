5b3a9017f0efaa8121cf0a6867da18133b5eb60e
jseom <2ujs4311@gmail.com>
2024-10-23 14:35:27 +0900

    FEAT: 자동차 이름 리스트를 반환하는 기능 추가
    문자열을 입력받으면 쉼표(,)를 기준으로 구분해서 자동차 이름 ArrayList를 반환하는 기능을 추가했습니다.

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,4 +1,12 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingCar {
+
+    public static ArrayList makeNameList(String str) {
+        ArrayList<String> nameList = new ArrayList<>(List.of(str.split(",")));
+        return nameList;
+    }
 }
