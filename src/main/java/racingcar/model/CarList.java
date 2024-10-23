3af3de2f88c355b7bdd08ec9a2b69b462afbebab
KNU-K <knukang334@gmail.com>
2024-10-24 04:08:06 +0900

    feat(model): 자동차 리스트를 관리하는 일급 객체 생성
    - `CarList` 클래스 구현, 자동차 객체를 리스트로 관리.
    - `addCar` 메서드를 통해 자동차를 리스트에 추가.
    - 리스트에 있는 모든 자동차를 이동시키는 `moveAll` 메서드 추가.
    - 방어적 복사를 통해 자동차 리스트를 반환하는 `getAllCars` 메서드 구현.
    - 자동차들 중 가장 멀리 이동한 거리를 계산하는 `getMaxDistance` 메서드 추가.
    - 리스트의 크기를 반환하는 `size` 메서드 구현.

---
+++ src/main/java/racingcar/model/CarList.java
@@ -0,0 +1,37 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarList {
+    private final List<Drivable> cars;
+
+    public CarList() {
+        this.cars = new ArrayList<>();
+    }
+
+    public void addCar(Drivable car) {
+        cars.add(car);
+    }
+
+    public void moveAll() {
+        cars.forEach(Drivable::move);
+    }
+
+    public List<Drivable> getCars() {
+        return cars;
+    }
+
+    public List<Drivable> getAllCars() {
+        return new ArrayList<>(cars);// 방어적 복사
+    }
+
+    public int getMaxDistance() {
+        return cars.stream().mapToInt(Drivable::getDistance).max().orElse(0);
+    }
+
+    public int size() {
+        return cars.size();
+    }
+
+}
