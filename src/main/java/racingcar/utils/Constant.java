90d3cb906bc36a2bb926bd40a893227f73cb5112
ParkHyunS00 <parkhyuns00@naver.com>
2024-10-23 16:39:05 +0900

    feat(Constant): 상수를 관리하는 Constant 클래스 정의
    프로그램 내에서 사용하는 상수들을 관리하는 Constant 유틸 클래스를 정의

---
+++ src/main/java/racingcar/utils/Constant.java
@@ -0,0 +1,9 @@
+package racingcar.utils;
+
+public class Constant {
+
+    // 입력 요구 메시지
+    public static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    private Constant() {}
+}
