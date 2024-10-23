1fa83714edce56581e343ca8a7424ea40bed7581
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-10-24 00:19:38 +0900

    fix(RacingCarApp): getCarInput() 함수 타입 변경 void -> String

--- src/main/java/racingcar/RacingCarApp.java
+++ src/main/java/racingcar/RacingCarApp.java
@@ -3,8 +3,8 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class RacingCarApp {
-    public void getCarInput() {
+    public String getCarInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String racingCars = Console.readLine();
+        return Console.readLine();
     }
 }
