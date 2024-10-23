05b17d61d47bc77ee1cd4677aa255f20f1705e13
kwonhee <lkh8033@gmail.com>
2024-10-23 14:10:05 +0900

    docs: Application input : 입력받기 , racing : 진행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,57 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
 public class Application {
+    private ArrayList<Car> array;
+    private int count;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application app = new Application();
+        app.input();
+        app.racing();
+        app.printResult();
     }
+
+    public void input(){
+        Str str = new Str();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputString = Console.readLine();
+
+        array = str.arrayToList(inputString);
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        count = Integer.parseInt(Console.readLine());
+    }
+
+    private void racing(){
+        for(int i = 0; i< count; i++){
+            for(Car car : array){
+                car.update(1);
+                printStatus(car);
+            }
+            System.out.println("");
+        }
+    }
+
+    private void printStatus(Car car){
+        StringBuilder stringBuilder = new StringBuilder(car.name + " : ");
+        for(int i = 0; i< car.value; i++){
+            stringBuilder.append("-");
+        }
+        System.out.println(stringBuilder.toString());
+    }
+
+    private void printResult(){
+        StringBuilder result = new StringBuilder("최종 우승자 : ");
+        for(Car car : array){
+            if(car.isMax())
+                result.append(car.name).append(", ");
+        }
+        result.setLength(result.length()-2);
+        System.out.println(result.toString());
+    }
+
 }
