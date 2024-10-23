e1b5df52fae11c8b43d7ed8746c7dad7c09cf62e
dfb5361@gmail.com <dfb5361@gmail.com>
2024-10-23 16:13:51 +0900

    refactor(Application): extract functions from main
    - Extract car name input and movement count input from main for better readability and to allow variable access
    - Extract validation check from car name input for less indentation

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,25 +10,39 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<String> cars = getCarNames(br);
+        int moves = getMovementCounts(br);
 
+        System.out.println(cars);
+        System.out.println(moves);
+    }
+
+    public static List<String> getCarNames(BufferedReader br) {
         try{
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             String input = br.readLine().trim();
 
-            if (input.isEmpty() || input.startsWith(",") || input.endsWith(",") || input.contains(",,")){
-                throw new IllegalArgumentException("잘못된 입력입니다.");
-            } else {
-                List<String> cars = new ArrayList<>(List.of(input.split(",")));
-            }
-        } catch (IOException e) {
-            throw new IllegalArgumentException("잘못된 입력입니다.");
+            validateInput(input);
+            return new ArrayList<>(List.of(input.split(",")));
+        } catch (IOException e){
+            throw new IllegalArgumentException("입력 오류: 잘못된 입력입니다.");
         }
+    }
 
+    public static void validateInput(String input) {
+        if (input.isEmpty() || input.startsWith(",") || input.endsWith(",") || input.contains(",,")){
+            throw new IllegalArgumentException("입력 오류: 쉼표를 확인하세요.");
+        }
+    }
+
+    public static int getMovementCounts(BufferedReader br) {
         try{
             System.out.println("시도할 횟수는 몇 회인가요?");
-            int moves = Integer.parseInt(br.readLine());
+            return Integer.parseInt(br.readLine());
         } catch (IOException e) {
-            throw new IllegalArgumentException("잘못된 입력입니다.");
+            throw new IllegalArgumentException("입력 오류: 잘못된 입력입니다.");
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력 오류: 정수를 입력하세요.");
         }
     }
 }
