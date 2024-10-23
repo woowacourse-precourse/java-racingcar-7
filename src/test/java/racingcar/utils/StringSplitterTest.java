6493043e117bd975059371a0288c58a13568e3e6
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-10-23 23:01:55 +0900

    test: StringSplitter 테스트 코드 작성
    ',' 가 delimiter로 주어졌을 때, 반환값을 검증했음

---
+++ src/test/java/racingcar/utils/StringSplitterTest.java
@@ -0,0 +1,23 @@
+package racingcar.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringSplitterTest {
+
+    private StringSplitter stringSplitter = new StringSplitter();
+
+    @DisplayName("userInput을 구분자','로 나누어 리스트로 반환한다")
+    @Test
+    void userInput_split_then_return_list(){
+        String userInput = "aa,bb,cc,ddd";
+        List<String> expectedResult = List.of("aa","bb","cc","ddd");
+        List<String> splitLists = stringSplitter.splitByDelimiter(userInput);
+        assertThat(splitLists.size()).isEqualTo(expectedResult.size());
+        assertThat(splitLists).isEqualTo(expectedResult);
+    }
+
+}
