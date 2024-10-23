9919de64534e476ce4926755fe52711ab7bf72b5
Leegeonwoo7 <workgeon07@naver.com>
2024-10-24 01:27:17 +0900

    feat: 자동차 이름목록과 반복횟수 입력 구현

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,2 +1,19 @@
-package racingcar;public class RacingCar {
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RacingCar {
+
+    public static void main(String[] args) {
+        System.out.println("경주할 자동차 이름을 입력하세요");
+        String inputCarNames = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String inputRoofCount = Console.readLine();
+    }
 }
+
