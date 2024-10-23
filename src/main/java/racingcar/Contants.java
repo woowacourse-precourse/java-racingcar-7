7536eec5135d10776e4356c3c4791deb9881666c
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-10-23 23:48:36 +0900

    fix: max_car_name_length 값 수정

--- src/main/java/racingcar/Contants.java
+++ src/main/java/racingcar/Contants.java
@@ -9,7 +9,7 @@ public class Contants {
     static final String OUTPUT_DISTANCE_CHAR = "-";
 
 
-    static final int MAX_CAR_NAME_LENGTH = 1;
+    static final int MAX_CAR_NAME_LENGTH = 4;
     static final int MIN_RANDOM_NUMBER = 0;
     static final int MAX_RANDOM_NUMBER = 9;
     static final int COMPARISON_VALUE = 4;
