7e95ef04be639017cbcf080519fa836dd82990ad
ChiSaeHwang <cwh730909@gmail.com>
2024-10-23 17:33:32 +0900

    test(CarTest): 객체 생성 및 move 메서드 테스트

--- src/test/java/racingcar/domain/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/racingcar/domain/CarTest.java
@@ -13,5 +13,54 @@ import org.junit.jupiter.params.provider.ValueSource;
 class CarTest {
 
 
+    @Nested
+    @DisplayName("객체 생성 및 유효성 검증 테스트")
+    class CreationAndValidationTests {
+
+        @ParameterizedTest
+        @ValueSource(strings = {"chi1", "chi2", "chi3"})
+        @DisplayName("자동차 객체를 정상적으로 생성한다.")
+        void createAndValidateDate(String name) {
+            // when & then
+            Assertions.assertThatNoException()
+                    .isThrownBy(() -> Car.of(name));
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"chichi", "++++++++", ""})
+        @DisplayName("객체의 이름이 5를 초과하면 예외처리한다.")
+        void checkCarName(String name) {
+            // when & then
+            assertThatThrownBy(() -> Car.of(name))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("자동차의 이름이 적절하지 않습니다.");
+        }
+    }
+
+
+    @Nested
+    @DisplayName("객체 생성 및 유효성 검증 테스트")
+    class CarMoveTests {
+
+        @Test
+        @DisplayName("랜덤 값이 4 이상이면 자동차가 전진한다")
+        void carMove() {
+            Car car = Car.of("car1");
+            int initPosition = car.getPosition();
+            car.move(4);
+            assertEquals(initPosition + 1, car.getPosition());
+        }
+
+        @Test
+        @DisplayName("랜덤 값이 4 미만이면 자동차가 정지한다")
+        void carStopsWhenRandomValueIsLessThanFour() {
+            Car car = Car.of("car1");
+            int initPosition = car.getPosition();
+            car.move(3);
+            assertEquals(initPosition, car.getPosition());
+        }
+
+    }
+
 
 }
