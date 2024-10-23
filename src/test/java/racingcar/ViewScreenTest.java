b718a9448d83ecf565f97e6d7d9688fee276cfd2
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-10-23 22:41:09 +0900

    test($view) : Test printing car's running process
    경주 진행하는 자동차 진도 출력 기능 동작 테스트

--- src/test/java/racingcar/ViewScreenTest.java
+++ src/test/java/racingcar/ViewScreenTest.java
@@ -1,10 +1,15 @@
 package racingcar;
 
+import static java.lang.Integer.parseInt;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import view.ViewScreen;
 
 public class ViewScreenTest {
@@ -15,5 +20,18 @@ public class ViewScreenTest {
         String input = "pobi,woni,jun\n5\n";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         assertEquals(Arrays.asList("pobi,woni,jun", "5"), viewScreen.getInput());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"pobi,0,pobi :", "woni,2,woni : --", "a,5,a : -----"})
+    void 자동차_하나의_경주_상황_출력(String name, String strDistance, String expectedOutput) {
+        int intDistance = parseInt(strDistance);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        viewScreen.printRace(name, intDistance);
+        assertEquals(expectedOutput, outputStream.toString().trim());
+        System.setOut(originalOut);
     }
 }
