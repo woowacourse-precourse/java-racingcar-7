f40cb18cf83250ce34f935afa5b48271e0dcb2c4
Nahye Choi <skgp0628@naver.com>
2024-10-23 23:27:14 +0900

    feat: 자동차 전진 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.Arrays;
 import java.util.LinkedHashMap;
@@ -23,16 +24,26 @@ public class Application {
 //  - [x] 시도 횟수 입력 받기
 //    [ ] (시도 횟수는 양수만 허용)
         String carsName = Console.readLine();
-        String tryNumber = Console.readLine();
+       int tryNumber = Integer.parseInt(Console.readLine());
 
         // 경기 진행
 //  - [x] 입력 받은 경주 자동차 이름 구분
         Map<String, Integer> cars = new LinkedHashMap<>();
         cars = Arrays.stream(carsName.split(",")).collect(Collectors.toMap(s -> s, s -> 0));
-        System.out.println(cars);
+
+//      입력 횟수만큼 경기 진행
+        for (int i = 0; i < tryNumber; i++) {
 //  - [ ] 자동차 이동 조건
-//    - [ ] 전진 : 무작위 값이 4이상
-//    - [ ] 정지 : 무작위 값이 4미만
+//  - [x] 전진 : 무작위 값이 4이상
+            for (String name : cars.keySet()) {
+                if (Randoms.pickNumberInRange(0,9) >= 4) { // 랜덤값을 변수에 담는 게 좋을지
+                    cars.put(name, cars.get(name) + 1);
+                    System.out.println(cars.get(name));
+                }
+//  - [ ] 정지 : 무작위 값이 4미만
+            }
+        }
+
 //  - [ ] 우승자 조건 : 가장 많이 전진한 자동차(우승자는 2명 이상이 될 수 있다.)
     }
 }
