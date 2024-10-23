61a136e04de028c8b7eb66d28348940f37ae8256
JooYoung <beachu23@naver.com>
2024-10-23 17:52:47 +0900

    test: Validate car names after comma separation
    자동차 이름 목록 쉼표 기준 분리 기능과 유효성 검증 기능을 하나의 기능 단위이므로 테스트를 작성한다.
    
    Closes

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -25,9 +25,7 @@ class RacingCarTest {
 
         //Then
         Assertions.assertThat(carList.size()).isEqualTo(expectedSize);
-        for (int i = 0; i < carList.size(); i++) {
-            Assertions.assertThat(expectedCarList.get(i)).isEqualTo(carList.get(i));
-        }
+        Assertions.assertThat(carList).containsExactlyElementsOf(expectedCarList);
     }
 
     @Test
@@ -71,5 +69,66 @@ class RacingCarTest {
         //When, Then
         Assertions.assertThatThrownBy(() -> racingCar.validateCarName(carName))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 자동차이름목록_쉼표기준분리_5글자이하_테스트() throws Exception {
+        //Given
+        String carNames = "pobi,woni,jun";
+
+        List<String> expectedCarList = new ArrayList<>();
+        expectedCarList.add("pobi");
+        expectedCarList.add("woni");
+        expectedCarList.add("jun");
+        int expectedSize = expectedCarList.size();
+
+        //When
+        List<String> carList = racingCar.splitCarNamesByComma(carNames);
+
+        //Then
+        Assertions.assertThat(carList.size()).isEqualTo(expectedSize);
+        for (String carName : carList) {
+            Assertions.assertThatCode(() -> racingCar.validateCarName(carName))
+                    .doesNotThrowAnyException();
+        }
+    }
+
+    @Test
+    public void 자동차이름목록_쉼표_외_문자기준분리_5글자이하_예외테스트() throws Exception {
+        //Given
+        String carNames = "pobi,woni:jun";
+        int expectedSize = 2;
+
+        //When
+        List<String> carList = racingCar.splitCarNamesByComma(carNames);
+
+        //Then
+        Assertions.assertThat(carList.size()).isEqualTo(expectedSize);
+        Assertions.assertThatCode(() -> racingCar.validateCarName(carList.get(0)))
+                .doesNotThrowAnyException();
+        Assertions.assertThatThrownBy(() -> racingCar.validateCarName(carList.get(1)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 자동차이름_중복허용_테스트() throws Exception {
+        //Given
+        String carNames = "pobi,pobi,pobi";
+
+        List<String> expectedCarList = new ArrayList<>();
+        expectedCarList.add("pobi");
+        expectedCarList.add("pobi");
+        expectedCarList.add("pobi");
+        int expectedSize = expectedCarList.size();
+
+        //When
+        List<String> carList = racingCar.splitCarNamesByComma(carNames);
+
+        //Then
+        Assertions.assertThat(carList.size()).isEqualTo(expectedSize);
+        for (String carName : carList) {
+            Assertions.assertThatCode(() -> racingCar.validateCarName(carName))
+                    .doesNotThrowAnyException();
+        }
     }
 }
