469555432d6e8ad80b922fd5240f9a584ec637da
SunHee <76764643+sunni546@users.noreply.github.com>
2024-10-24 00:18:54 +0900

    feat: 자동차 이름 문자열 입력

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,9 +1,12 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class GameController {
     public void start() {
         OutputView.printInputNamesMessage();
+
+        String carNames = InputView.readInput();
     }
 }
