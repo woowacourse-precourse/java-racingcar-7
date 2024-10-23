654894d9404f74be64d324d579abafd49802a4a3
ouvir <kilhyunii@naver.com>
2024-10-23 19:57:22 +0900

    test: 자동차 이름 구분 테스트 작성
    사용자 입력에서 "," 를 통해, 이름을 구분하는 테스트 작성
    성공 테스트
    - 자동차 이름 구분 테스트
    실패 테스트
    - 자동차 이름 구분 예외 테스트(blank, space, null) > IllegalArgumentException
    - 연속된 구분자 테스트 > IllegalArgumentException
    - 자동차 이름 중복 테스트 > IllegalArgumentException

---
+++ src/test/java/separator/SeparatorTest.java
@@ -0,0 +1,51 @@
+package separator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeparatorTest {
+
+    @Test
+    @DisplayName("자동차 이름 구분 테스트")
+    void split() {
+        //when
+        List<String> output = Separator.split("pobi,woni,jun");
+        List<String> expected = List.of("pobi", "woni", "jun");
+        //then
+        Assertions.assertThat(output).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    @DisplayName("자동차 이름 구분 예외 테스트(null, blank)")
+    void split_X() {
+        //when
+        String valueOfBlank = "";
+        String valueOfNull = null;
+        String valueOfSpace = " ,  ";
+        //then
+        assertThrows(IllegalArgumentException.class, () -> Separator.split(valueOfBlank));
+        assertThrows(IllegalArgumentException.class, () -> Separator.split(valueOfNull));
+        assertThrows(IllegalArgumentException.class, () -> Separator.split(valueOfSpace));
+    }
+
+    @Test
+    void 연속된_구분자() {
+        //when
+        String repeatedSeparator = "pobi,woni,,jun";
+        //then
+        assertThrows(IllegalArgumentException.class, () -> Separator.split(repeatedSeparator));
+    }
+
+    @Test
+    void 자동차_이름_중복() {
+        //when
+        String duplicatedName = "pobi,jun,pobi";
+        //then
+        assertThrows(IllegalArgumentException.class, () -> Separator.split(duplicatedName));
+    }
+}
