42ef48c925b4d6831645dc5a297149ca35e15b74
bowook <mbosx316@naver.com>
2024-10-23 20:59:22 +0900

    fix(InputView) : inputCarNames 반환 타입 수정
    - List<String> -> String 으로 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,15 +2,12 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.List;
-
 public class InputView {
-    private final static String COMMA = ",";
-    public List<String> inputCarNames() {
+    public String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
         Console.close();
-        return List.of(carNames.split(COMMA));
+        return carNames;
     }
 
     public String inputTrialCounts() {
