c73626beca882e4e7d6311d1d0108b406e4e5985
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-23 16:13:53 +0900

    feat($RacingCars): 중복된 RacingCar 에 대한 예외 처리 구현
    - RacingCar 의 equals 를 사용해 판단.
    - IllegalArgumentException 발생.

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -7,6 +7,13 @@ public class RacingCars {
     private final List<RacingCar> values;
 
     public RacingCars(List<RacingCar> values) {
+        validateDuplication(values);
         this.values = List.copyOf(values);
+    }
+
+    private void validateDuplication(List<RacingCar> values) {
+        if (values.stream().distinct().count() != values.size()) {
+            throw new IllegalArgumentException("중복된 자동차가 존재합니다.");
+        }
     }
 }
