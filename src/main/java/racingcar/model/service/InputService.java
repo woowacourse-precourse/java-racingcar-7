cf21a8bb73003666e9f1f68da26b2923d0f3e2fa
immyeong <wpfls6670@naver.com>
2024-10-23 11:33:10 +0900

    feat: 입력 서비스 구현

---
+++ src/main/java/racingcar/model/service/InputService.java
@@ -0,0 +1,9 @@
+package racingcar.model.service;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputService {
+
+    public String inputValue(){
+        return Console.readLine();
+    }
+}
