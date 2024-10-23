1b9233558f3b3c167419071b7158157adaaec6ad
Hyungjung Kim <gudwnd1113@gmail.com>
2024-10-23 17:21:42 +0900

    feat(RacingCar): define RacingCar class
    RacingCar 클래스 정의.
    RacingCar 클래스의 생성자 및 인스턴스 변수 초기화 구현.

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class RacingCar {
+    private String name;
+    private int count;
+
+    public RacingCar(final String name) {
+        this.name = name;
+        this.count = 0;
+    }
+}
