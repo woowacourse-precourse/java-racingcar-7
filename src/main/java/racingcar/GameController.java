8e7b64ecee8a116ed444fb954fc9674c814c559e
ssuukko <tlstpstpdl11@gmail.com>
2024-10-23 20:35:15 +0900

    Feat : GameController 클래스 추가 및 전체 게임 로직 관리 기능 구현
    - 게임의 전반적인 흐름을 관리하는 GameController 클래스를 추가하였습니다.
    - RacingCarList를 통해 자동차 목록을 등록하는 기능을 구현하였습니다.
    - 향후 사용자 입력 및 게임 진행 로직을 추가할 준비가 완료되었습니다.

---
+++ src/main/java/racingcar/GameController.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+// GameManager 클래스명 바꿀까 고민
+public class GameController {
+
+    RacingCarList racingCarList = new RacingCarList();
+
+    public GameController(String carName, String attemptCount) {
+        racingCarList.add(carName);
+    }
+}
