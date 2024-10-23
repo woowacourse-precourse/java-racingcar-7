948c27d6af46dd0fcf5365df9361b78322fa7b27
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-10-23 19:31:02 +0900

    test(car): 랜덤 숫자가 4 이상인 경우 자동차를 전진시키는 테스트 코드 작성

---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,20 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+
+    @Test
+    void 랜덤_숫자가_4_이상인_경우_자동차_전진() {
+        // given
+        Car car = new Car("TestCar");
+
+        // when
+        car.moveIfPossible(4);
+
+        //then
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+}
