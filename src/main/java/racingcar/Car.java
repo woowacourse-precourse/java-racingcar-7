a549e5747e93312c88f67107507245f6e431ccb4
JUNYEON WON <wnsdus1008@gmail.com>
2024-10-23 21:09:10 +0900

    feat(Car, CarTest): Make car class to validate name and make movement

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int location = 0;
+
+    public Car(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 없거나 공백일 수 없습니다.");
+        }
+
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
+        this.name = name;
+    }
+
+    public void move() {
+        location++;
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + "-".repeat(location);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+}
---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,59 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    @Test
+    void 차_객체_생성() {
+        //given
+        String name = "test";
+
+        //when
+        Car car = new Car("test");
+
+        //then
+        assertThat(car.getName()).isEqualTo(name);
+        assertThat(car.getLocation()).isEqualTo(0);
+        assertThat(car.toString()).isEqualTo("test : ");
+    }
+
+    @Test
+    void 차_객체_생성_실패_5자이상() {
+        //given
+        String name = "name_fail";
+
+        //then
+        assertThrows(IllegalArgumentException.class, () -> new Car(name));
+    }
+
+    @Test
+    void 차_객체_생성_실패_공백_또는_null() {
+        //given
+        String name1 = "";
+        String name2 = "  ";
+        String name3 = null;
+
+        //then
+        assertThrows(IllegalArgumentException.class, () -> new Car(name1));
+        assertThrows(IllegalArgumentException.class, () -> new Car(name2));
+        assertThrows(IllegalArgumentException.class, () -> new Car(name3));
+    }
+
+    @Test
+    void 차_객체_전진() {
+        //given
+        String name = "test";
+        Car car = new Car("test");
+
+        //when
+        car.move();
+
+        //then
+        assertThat(car.getLocation()).isEqualTo(1);
+        assertThat(car.toString()).isEqualTo("test : -");
+    }
+
+}
