98509242d3ed8628bda6566142c1f3af14afa61b
minjun <ijmtylhtf@naver.com>
2024-10-23 16:20:45 +0900

    feat : 자동차 이름 길이 검증 메서드 구현

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -6,4 +6,10 @@ public class Car {
 	public Car(String name) {
 		this.name = name;
 	}
+
+	private static void validateCarLength(String carName) {
+		if (carName.length() > 5) {
+			throw new RuntimeException();
+		}
+	}
 }
