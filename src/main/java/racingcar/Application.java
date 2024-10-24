08a1ac1af2097dce37894ab2b6e3c86febfaaff7
JUNYEON WON <wnsdus1008@gmail.com>
2024-10-24 22:46:02 +0900

    feat: run racing car

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,36 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.RaceReady;
+import racingcar.domain.factory.CarFactory;
+import racingcar.service.RaceManager;
+import racingcar.view.InputView;
+import racingcar.view.ResultView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        racingcar.domain.factory.CarFactory carFactory = new CarFactory(",");
+        RaceManager raceManager = new RaceManager();
+        ResultView resultView = new ResultView();
+
+        RaceReady raceReady = inputView.readyForRace();
+
+        List<Car> cars = carFactory.getCars(raceReady.getNameInput());
+
+        for (int i = 0; i < raceReady.getNumberOfTime(); i++) {
+            String raceResult = raceManager.race(cars);
+            resultView.saveResult(raceResult);
+        }
+
+        resultView.printRaceResult();
+
+        List<Car> winners = raceManager.getWinners(cars);
+
+        resultView.printWinners(winners);
+
     }
+
 }
