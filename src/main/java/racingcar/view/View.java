a178e4f8fdd5b4f62baeca6d9e22b884073b3634
wogus <ind07152@naver.com>
2024-10-24 22:56:28 +0900

    feat(View): 자동차 경주 횟수 입력 기능 추가
    자동차 경주 횟수를 입력 받는 메서드를 추가한다.

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -6,9 +6,15 @@ import racingcar.utils.Parser;
 
 public final class View {
     private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
     public static List<String> getCarNames() {
         System.out.println(NAME_INPUT_MESSAGE);
         return Parser.parse(Console.readLine());
+    }
+
+    public static Integer getCount() {
+        System.out.println(COUNT_INPUT_MESSAGE);
+        return Integer.parseInt(Console.readLine());
     }
 }
