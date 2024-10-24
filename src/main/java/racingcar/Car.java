7f6c0d8d78dcf562e631198f741e419dea4893d4
speculatingwook <bwook9908@gmail.com>
2024-10-24 15:46:45 +0900

    feat: implement Car class, CarRegistry Class
    - set position 0 for not setting 0 again when Car object created
    - CarRegistry class if for making multiple Cars, and managing them

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,40 @@
+package racingcar;
+
+public class Car {
+    private Integer position;
+    private String name;
+
+    public Car(String name) {
+        validateCarNameLength(name);
+        this.name = name;
+        this.position = 0;
+    }
+
+    private void validateCarNameLength(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("Car name has to be 5 or under 5.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void goForward() {
+        position++;
+    }
+
+    /**
+     * 객체의 문자열 표현을 반환합니다.
+     * 이 표현은 객체의 이름과 함께 현재 위치에 해당하는
+     * 대시('-')로 구성된 문자열을 포함합니다.
+     *
+     * @return 이름과 현재 위치에 따라 결정된 대시가 포함된
+     *         형식 "이름 : ----" 형태의 문자열을 반환합니다.
+     */
+    @Override
+    public String toString() {
+        return name + " : " + "-".repeat(Math.max(0, position));
+    }
+
+}
---
+++ src/main/java/racingcar/CarRegistry.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CarRegistry {
+    private final Set<Car> cars;
+
+    public CarRegistry(String input) {
+        this.cars = convertInputToCarSet(input);
+    }
+
+    private Set<Car> convertInputToCarSet(String input) {
+        Set<Car> cars = new HashSet<>();
+        String[] carNames = input.split(",");
+        for (String carName : carNames) {
+            cars.add(createCar(carName));
+        }
+        return cars;
+    }
+
+    private Car createCar(String name) {
+        return new Car(name);
+    }
+}
