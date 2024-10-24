c48c4508f6dba1c734b50df623d3da09f1ca5768
posite <su84053@gmail.com>
2024-10-24 22:19:11 +0900

    feat(RandomNumberGenerator): 무작위 정수 생성 구현
    0부터 9까지 무작위 정수 값을 생성 기능을 구현하였습니다.

---
+++ src/main/java/racingcar/RandomNumberGenerator.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+    public static int generateRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    }
+
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 0;
+}
