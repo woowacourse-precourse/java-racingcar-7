34efe084a5ac8c73abf1b38a795d11e64517646b
Hanjjong <tim4974@naver.com>
2024-10-24 16:06:59 +0900

    feat(Validator): 자동차 이름이 알파벳과 숫자만 허용하도록 하는 기능 구현

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Set;
 
 public class Validator {
+    private static final String VALID_NAME_PATTERN = "^[a-zA-Z0-9]+$";
+
     public void carNameValidate(List<String> carNames) {
         Set<String> uniqueCarNames = new HashSet<>();
 
@@ -18,10 +20,12 @@ public class Validator {
             if (carName.length() > 5) {
                 throw new IllegalArgumentException();
             }
-            if(!uniqueCarNames.add(carName)) {
+            if (!uniqueCarNames.add(carName)) {
                 throw new IllegalArgumentException();
             }
-
+            if (!carName.matches(VALID_NAME_PATTERN)) {
+                throw new IllegalArgumentException();
+            }
         }
     }
 }
