b4e3db1c10c15f15dacf4d0a8840b236ad89431d
jaewon07 <jowonjae07@gmail.com>
2024-10-25 01:24:31 +0900

    feat: Add functionality for entering car names
    Added functionality to receive car names and set them in the Car class.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String CarName = Console.readLine();
+
+        List<Car> cars = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(CarName, ",");
+
+        while (tokenizer.hasMoreTokens()) {
+            String carName = tokenizer.nextToken();
+            cars.add(new Car(carName));
+        }
+
+
     }
 }
---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Car {
+
+    public String name;
+    public int score=0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+}
