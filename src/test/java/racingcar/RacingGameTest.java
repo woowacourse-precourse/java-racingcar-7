c551cebab25daec4f51d4ba25b028e5aff7c9224
이창헌 <changbill@naver.com>
2024-10-24 19:43:10 +0900

    refactor(test): add integration test

---
+++ src/test/java/racingcar/RacingGameTest.java
@@ -0,0 +1,109 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.util.ConstOutputMessage.EXECUTION_RESULT;
+import static racingcar.util.ConstOutputMessage.FINAL_WINNER;
+import static racingcar.util.ConstOutputMessage.INPUT_CAR_NAME;
+import static racingcar.util.ConstOutputMessage.INPUT_TURN_COUNT;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import racingcar.exception.EmptyInputException;
+import racingcar.exception.InvalidCharacterException;
+import racingcar.exception.InvalidLengthException;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import racingcar.exception.NumberRangeException;
+
+public class RacingGameTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    @Test
+    void 기능_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains(
+                            INPUT_CAR_NAME.getMessage(),
+                            INPUT_TURN_COUNT.getMessage(),
+                            EXECUTION_RESULT.getMessage(),
+                            "pobi : -",
+                            "woni : ",
+                            FINAL_WINNER.getMessage()+"pobi"
+                    );
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Nested
+    @DisplayName("자동차 등록중 예외 테스트")
+    class CarRegistExceptionTests {
+
+        @Test
+        void 길이_예외_테스트() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                            .isInstanceOf(InvalidLengthException.class)
+            );
+        }
+
+        @Test
+        void 특수문자_예외_테스트() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException("-pobi,java", "1"))
+                            .isInstanceOf(InvalidCharacterException.class)
+            );
+        }
+
+        @Test
+        void 빈_입력값_예외_테스트() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException("", "1"))
+                            .isInstanceOf(EmptyInputException.class)
+            );
+        }
+    }
+
+    @Nested
+    @DisplayName("시도횟수 등록중 예외테스트")
+    class ExecutionNumberRegistExceptionTests {
+
+        @Test
+        void 빈_입력값_예외_테스트() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException("abc", "\n"))
+                            .isInstanceOf(EmptyInputException.class)
+            );
+        }
+
+        @Test
+        @DisplayName("0 범위 예외 테스트")
+        void 범위_예외_테스트1() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException("abc", "0"))
+                            .isInstanceOf(NumberRangeException.class)
+            );
+        }
+
+        @Test
+        @DisplayName("최대 범위 예외 테스트")
+        void 범위_예외_테스트2() {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException("abc", "1001"))
+                            .isInstanceOf(NumberRangeException.class)
+            );
+        }
+    }
+
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
