429e5cf3c51ef1288ae66f7963e0f8c877355532
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-10-24 09:25:06 +0900

    Feat simultaneous movement for multiple cars

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    // TODO: 함수형으로 리팩토링
+    public List<Integer> go() {
+        List<Integer> positions = new ArrayList<>();
+        for (Car car : cars) {
+            int position = car.go();
+            positions.add(position);
+        }
+        return positions;
+    }
+}
---
+++ src/test/java/racingcar/CarsTest.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarsTest {
+
+    @DisplayName("n대의 차들이 모두 전진함에 따라 포지션이 1씩 증가한다는 로직을 테스트하라")
+    @Test
+    void test1() {
+        Cars cars = new Cars(List.of(new Car("A"), new Car("B"), new Car("C")));
+
+        List<Integer> positions1 = cars.go();
+        List<Integer> positions2 = cars.go();
+        List<Integer> positions3 = cars.go();
+
+        assertThat(positions1).isEqualTo(List.of(1, 1, 1));
+        assertThat(positions2).isEqualTo(List.of(2, 2, 2));
+        assertThat(positions3).isEqualTo(List.of(3, 3, 3));
+    }
+}
