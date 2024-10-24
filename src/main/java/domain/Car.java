ebb0160bd23a5dd50124e19e3a7d923d7aa32b65
sinji <tobeabestns@naver.com>
2024-10-24 23:46:57 +0900

    feat(Car): 자동차 객체 생성(이름과, 현재 위치를 갖음)

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,4 +1,17 @@
 package domain;
 
 public class Car {
+    private String carName;
+    private String carLocation;
+
+    // 생성자(객체)
+    public Car(final String carName, final String carLocation) {
+        this.carName = carName;
+        this.carLocation = carLocation;
+    }
+
+    // 자동차의 현재 위치
+    public String gerCarLocation() {
+        return carLocation;
+    }
 }
