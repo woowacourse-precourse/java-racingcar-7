53367c94b34e334d121d6ff8d9a0a9f8876cb323
임채연 <dlacodus0407@gmail.com>
2024-10-25 00:24:22 +0900

    feat(Input) : create input method
    - input() : 사용자에게 입력을 받습니다.

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     public void printNameMessage() {
         System.out.println(GameMessages.INPUT_GUIDE_NAME_MESSAGE);
@@ -7,5 +9,9 @@ public class InputView {
 
     public void printNumberMessage() {
         System.out.println(GameMessages.INPUT_GUIDE_NUMBER_MESSAGE);
+    }
+
+    public String input() {
+        return Console.readLine();
     }
 }
