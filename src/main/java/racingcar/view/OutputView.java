e3cd945aa3286f2af44e91ffb76bfb37c456f08f
bowook <mbosx316@naver.com>
2024-10-24 19:31:07 +0900

    refactor(OutputView) : 최종 우승자 기능 개선
    winners 리스트 사이즈 비교 없애고 출력한다

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -19,10 +19,7 @@ public class OutputView {
     }
 
     public void finalWinnerMessage(List<String> winners) {
-        String message = "최종 우승자 : " + winners.get(0);
-        if (winners.size() > 1) {
-            message += ", " + String.join(", ", winners.subList(1, winners.size()));
-        }
+        String message = "최종 우승자 : " + String.join(", ", winners);
         System.out.println(message);
     }
 
