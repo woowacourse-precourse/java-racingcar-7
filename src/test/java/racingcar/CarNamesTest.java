c2d222fa90eb82fe009797b1c8b403d6d1024af0
Seo Jinmann <sjmmics@gmail.com>
2024-10-24 11:18:25 +0900

    feat : ($CarNamesTest, $RacingChanceTest) CarNames, RacingChance 클래스의 입력값 검증 기능 테스트 추가, InputViewTest 삭제
    입력값 검증 기능이 InputView에서 CarNames, RacingChance 클래스으로 이동했기 때문에 테스트 코드 추가 및 삭제

---
+++ src/test/java/racingcar/CarNamesTest.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.model.io.CarNames;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class CarNamesTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ash,green,hotpink", "ash, green",
+            "ash ,green", "ash , green", "\n", "홍길동,두루미와다람쥐", "케서린 한"})
+    @DisplayName("이름 입력 검증 테스트 예외 처리")
+    void carNamesValidateThrowExceptionTest(String carNameInput) {
+        assertThatThrownBy(() -> CarNames.getAfterValidateForm(carNameInput)).
+                isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ash", "ash,green"})
+    @DisplayName("이름 입력 검증 테스트")
+    void carNamesValidateDoesNotThrowTest(String carNameInput) {
+        assertDoesNotThrow(() -> CarNames.getAfterValidateForm(carNameInput));
+    }
+}
---
+++ src/test/java/racingcar/RacingChanceTest.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.model.io.RacingChance;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class RacingChanceTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"마음껏달려봐", "111111111111111111111", "0", "\n"})
+    @DisplayName("시도할 횟수 검증 예외 테스트")
+    void chanceInputStringExceptionTest(String chanceNumberToValidate) {
+        assertThatThrownBy(() -> RacingChance.getAfterValidateForm(chanceNumberToValidate)).
+                isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "5", "2025"})
+    @DisplayName("시도할 횟수 검증 테스트")
+    void chanceInputStringValidateTest(String chanceNumberToValidate) {
+        assertDoesNotThrow(()
+                -> RacingChance.getAfterValidateForm(chanceNumberToValidate));
+    }
+}
