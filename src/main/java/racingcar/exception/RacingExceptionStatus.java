7e9ab0cb18ab1afcc462a943946db2f7e0fd2bd0
LeeDongHoon <likelion.leedh@gmail.com>
2024-10-24 18:34:02 +0900

    [FEAT]<RacingExceptionStatus> : INVALID_NAME_BLANK 예외처리 상태 추가

--- src/main/java/racingcar/exception/RacingExceptionStatus.java
+++ src/main/java/racingcar/exception/RacingExceptionStatus.java
@@ -3,7 +3,8 @@ package racingcar.exception;
 public enum RacingExceptionStatus {
 
     INVALID_NAME_LENGTH("자동차 이름은 5자 이하만 가능합니다."),
-    INVALID_ATTEMPTS_INTEGER("입력된 시도 횟수가 올바르지 않습니다.");
+    INVALID_ATTEMPTS_INTEGER("입력된 시도 횟수가 올바르지 않습니다."),
+    INVALID_NAME_BLANK("자동차 이름이 공백일 수 없습니다.");
 
     private final String message;
 
