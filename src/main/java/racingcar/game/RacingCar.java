7490de58ce30cc21d2671334d660685d5ae2e5a0
Revi1337 <david122123@gmail.com>
2024-10-24 14:47:10 +0900

    feat: 몇번을 이동할지 횟수를 입력하는 메시지를 출력하는 기능 구현

--- src/main/java/racingcar/game/RacingCar.java
+++ src/main/java/racingcar/game/RacingCar.java
@@ -9,6 +9,8 @@ public class RacingCar {
     public void race() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> carNames = getCarNamesFromUser();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
     }
 
     private List<String> getCarNamesFromUser() {
