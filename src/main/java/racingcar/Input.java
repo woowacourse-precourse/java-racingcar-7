4dce7e779f2a9dd2aa4cd758772f439e39befd8f
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2024-10-24 23:02:51 +0900

    :necktie: feat : checkRoundInt 메서드 추가
    - 정수형이 아닐시 예외 처리

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -15,4 +15,12 @@ public class Input {
         if (name == null || name.isEmpty()) throw new IllegalArgumentException("이름은 한 글자 이상 입력해야 합니다.");
     }
 
+    public void checkRoundInt(String round){
+        try{
+            Integer.parseInt(round);
+        }catch (NumberFormatException  e){
+            throw  new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
+    }
+
 }
