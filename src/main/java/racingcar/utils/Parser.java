e847e38c7b3d55bf5e93eb24b181376291e23809
wogus <ind07152@naver.com>
2024-10-24 22:47:29 +0900

    feat(Parser): 자동차 이름 검증 로직 추가
    파싱 진행 후 validator를 통해 자동차 이름 검증을 진행 후 반환한다.

--- src/main/java/racingcar/utils/Parser.java
+++ src/main/java/racingcar/utils/Parser.java
@@ -2,9 +2,12 @@ package racingcar.utils;
 
 import java.util.Arrays;
 import java.util.List;
+import racingcar.validator.Validator;
 
 public final class Parser {
     public static List<String> parse(String input) {
-        return Arrays.stream(input.split(",")).map(String::trim).toList();
+        List<String> names = Arrays.stream(input.split(",")).map(String::trim).toList();
+        Validator.namesValidate(names);
+        return names;
     }
 }
