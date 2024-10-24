1f238936ece2844757e955b77e9de33ddc2ae943
joshinhyeok <shtree07@naver.com>
2024-10-24 22:29:12 +0900

    feat: controller에 생성한 기능들 연결

--- src/test/java/racingcar/Application.java
+++ src/test/java/racingcar/Application.java
@@ -1,6 +1,28 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
+        InputView inputView = new InputView();
+        Model model = new Model();
+
+        String carNameInput = inputView.inputCarNamesString();
+        List<String> carNames = model.getCarNames(carNameInput);
+
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        String attemptCountInput = inputView.inputAttemptCount();
+        model.validateAttemptCount(attemptCountInput);
+        int attemptCount = Integer.parseInt(attemptCountInput);
+
+        RacingGame racingGame = new RacingGame();
+        racingGame.playRace(cars, attemptCount);
+
+        List<Car> winners = Winners.findwinner(cars);
     }
 }
