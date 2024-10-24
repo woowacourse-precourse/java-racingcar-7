89afd043f5f3f096c0a25a1bb859c70f76578486
CinaDor <cinador@github.com>
2024-10-24 22:01:54 +0900

    feat: 자동차 경주의 우승 자동차를 선정하는 기능
    자동차 경주 결과를 집계하여 우승 자동차를 결정하는 클래스(service.ResultService)를 구현

---
+++ src/main/java/racingcar/service/ResultService.java
@@ -0,0 +1,49 @@
+package racingcar.service;
+
+import racingcar.model.RaceStatus;
+import racingcar.model.dto.Car;
+
+public class ResultService {
+    private RaceStatus raceStatus;
+
+    private ResultService() {
+        raceStatus = RaceStatus.getInstance();
+    }
+
+    private static class SingletonHelper {
+        private static final ResultService INSTANCE = new ResultService();
+    }
+
+    public static ResultService getInstance() {
+        return SingletonHelper.INSTANCE;
+    }
+
+
+    public String getResult() {
+        StringBuilder result = new StringBuilder();
+        int carNumber = raceStatus.getCarNumber();
+        int maxDistance = 0;
+
+        for (int index = 0; index < carNumber; index++) {
+            Car car = raceStatus.getCarByIndex(index);
+
+            updateResult(car, result, maxDistance);
+            maxDistance = Math.max(car.getDistance(), maxDistance);
+        }
+
+        return result.toString();
+    }
+
+    private void updateResult(Car car, StringBuilder result, int maxDistance) {
+        if (car.getDistance() > maxDistance) {
+            result.setLength(0);
+            result.append(car.getName());
+        }
+
+        if (car.getDistance() == maxDistance) {
+            result.append(", ")
+                    .append(car.getName());
+        }
+    }
+
+}
