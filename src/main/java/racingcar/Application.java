5c3293c054557653f692b5bafc18fb57c3ce1435
jbilee <julishahn@gmail.com>
2024-10-25 01:32:59 +0900

    refactor(app): 메인 메소드 리팩토링
    - 메인 메소드에서 수행하던 로직을 기능별로 나눠서 presenter의 메소드로 이동
    - 결과 출력 코드를 View 컴포넌트로 이동

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,46 +1,12 @@
 package racingcar;
 
-import racingcar.models.Car;
-import racingcar.utils.Randomizer;
-import racingcar.validators.CarInputValidator;
-import racingcar.validators.RoundInputValidator;
-import racingcar.views.IOView;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import racingcar.presenters.ApplicationPresenter;
 
 public class Application {
     public static void main(String[] args) {
-        IOView view = new IOView();
-        String carInput = view.getCarInput();
-
-        CarInputValidator carInputValidator = new CarInputValidator();
-        carInputValidator.validate(carInput);
-
-        String roundInput = view.getRoundInput();
-
-        RoundInputValidator roundInputValidator = new RoundInputValidator();
-        roundInputValidator.validate(roundInput);
-
-        List<String> carNames = Arrays.asList(carInput.split(","));
-        List<Car> cars = carNames.stream().map(Car::new).toList();
-
-        System.out.println("\n실행 결과");
-        for (int i = 0; i < Integer.parseInt(roundInput); i++) {
-            for (Car car : cars) {
-                car.makeMove(Randomizer.getRandomValue());
-            }
-            cars.forEach(car -> System.out.println(car.getName() + " : " + "-".repeat(car.getMoves())));
-            System.out.println();
-        }
-
-        int highestMove = cars.stream().mapToInt(Car::getMoves).max().getAsInt();
-
-        List<Car> winners = cars.stream().filter(car -> car.getMoves() == highestMove).toList();
-
-        String result = winners.stream().map(Car::getName).collect(Collectors.joining(", "));
-
-        System.out.println("최종 우승자 : " + result);
+        ApplicationPresenter presenter = new ApplicationPresenter();
+        presenter.init();
+        presenter.runGame();
+        presenter.displayResults();
     }
 }
--- src/main/java/racingcar/views/IOView.java
+++ src/main/java/racingcar/views/IOView.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class IOView {
     private static final String CAR_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ROUND_PROMPT = "시도할 횟수는 몇 회인가요?";
+    private static final String RESULTS_HEADER = "\n실행 결과";
+    private static final String WINNERS_PREFIX = "최종 우승자 : ";
 
     public String getCarInput() {
         System.out.println(CAR_PROMPT);
@@ -14,5 +16,13 @@ public class IOView {
     public String getRoundInput() {
         System.out.println(ROUND_PROMPT);
         return Console.readLine().strip();
+    }
+
+    public void printResultsHeader() {
+        System.out.println(RESULTS_HEADER);
+    }
+
+    public void printWinners(String winners) {
+        System.out.println(WINNERS_PREFIX + winners);
     }
 }
