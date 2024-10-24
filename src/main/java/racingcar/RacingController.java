315856fd9057f1da049088a2f59cfebb77a6ae30
soojinlee <sojinlee1004@naver.com>
2024-10-24 22:35:56 +0900

    feat(controller) : 자동차 입력 문자열 쉼표를 기준으로 분리한다.

--- src/main/java/racingcar/RacingController.java
+++ src/main/java/racingcar/RacingController.java
@@ -1,4 +1,14 @@
 package racingcar;
 
 public class RacingController {
+    private final RacingInputView inputView;
+
+    public RacingController(RacingInputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public String[] SplitCarNames(String RacingCarNames) {
+        return RacingCarNames.split(",");
+    }
+
 }
