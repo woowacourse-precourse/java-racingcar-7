01e359953c8e33e272e9ebbbfec70d090135f884
choign3123 <choign3123@naver.com>
2024-10-24 18:05:18 +0900

    Feat(Application): 쉼표로 구분된 n대의 자동차 이름 입력받아 분리하는 로직 구현
    ex)
    gana,dara,roma -> gana / dara / roma

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        IInputDisplay inputDisplay = new WtcInputDisplay();
+
+        String carNameMass = inputDisplay.getPromptInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        String[] carNameList = carNameMass.split(",");
     }
 }
