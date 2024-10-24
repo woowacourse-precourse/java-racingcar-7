131425ab2ed4ed11e723f04f9bfb706e059269e1
YeaChan05 <qkenrdl05@gmail.com>
2024-10-24 23:53:04 +0900

    feat(Car): Car 도메인 객체 생성
    경주읮 주체가 되는 Car 객체 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private final int position;
+
+    Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+}
