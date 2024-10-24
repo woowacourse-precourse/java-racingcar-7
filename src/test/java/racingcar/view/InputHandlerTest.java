a2a8b2dab59e26b08ee3c1897ea28e8f239a9dc8
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-10-25 01:23:33 +0900

    test(validateCarNames): 입력값이 올바르면 예외 없음 테스트 작성

--- src/test/java/racingcar/view/InputHandlerTest.java
+++ src/test/java/racingcar/view/InputHandlerTest.java
@@ -4,15 +4,22 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class InputHandlerTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"", " ", ",pobi,", "pobi,,woni", "pobi!", "123", "abcdef"})
+    @ValueSource(strings = {"", " ", ",pobi,", "pobi,,woni", "pobi!", "abcdef"})
     void 올바르지_않은_입력값_예외_발생(String carNames) {
         // when & then
         assertThatThrownBy(() -> validateCarNames(carNames))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,woni,jun", "car1, car2"})
+    void 올바른_입력값_예외_없음(String carNames) {
+        //when & then
+        assertDoesNotThrow(() -> validateCarNames(carNames));
+    }
 }
