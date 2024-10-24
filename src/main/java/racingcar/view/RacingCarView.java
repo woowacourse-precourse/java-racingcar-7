087f37be7f6dfc896b131310a0abe9914c01b874
jinno Yun <jinno321@naver.com>
2024-10-24 22:01:07 +0900

    feat: 최종 우승자 생성 출력 메서드 작성

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -7,20 +7,47 @@ import java.util.List;
 
 public class RacingCarView {
 
-    public String[] readInput(){
+    public String[] readCarName(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine().split(",");
+    }
+
+    public int readNum(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(Console.readLine());
     }
 
     public void printStarting(){
         System.out.println("실행 결과");
     }
+
     public void printEachGame(List<Car> cars){
         for (Car car : cars) {
-            System.out.println(car.getName() + " : " + printPosition(car.getPosition()));
+            System.out.println(car.getName() + " : " + getStringPosition(car.getPosition()));
         }
     }
 
-    private String printPosition(int position){
+    private String getStringPosition(int position){
         return "-".repeat(position);
     }
+
+    private void printWinners(List<Car> cars) {
+        System.out.print("최종 우승자 : ");
+
+        StringBuilder winners = new StringBuilder();
+
+        for (int i = 0; i < cars.size(); i++) {
+            Car car = cars.get(i);
+            winners.append(car.getName());
+
+            // 마지막 요소가 아닐 경우 쉼표 추가
+            if (i < cars.size() - 1) {
+                winners.append(", ");
+            }
+        }
+
+        // 최종 우승자 출력
+        System.out.println(winners.toString());
+    }
+
 }
