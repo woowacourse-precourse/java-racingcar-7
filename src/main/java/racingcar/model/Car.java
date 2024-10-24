460ccb29f47bfc4b49bb85d7cc09fe9783191d87
Yehyeok Bang <qkddpgur318@gmail.com>
2024-10-24 23:29:45 +0900

    feat: 자동차 이름 검증 규칙 추가
    자동차 이름은 알파벳과 숫자만 사용할 수 있으며, 공백을 포함할 수 없습니다.
    필요한 값을 상수로 선언하여 가독성을 높입니다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,13 +2,31 @@ package racingcar.model;
 
 public record Car(String name) {
 
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+    private static final String WHITE_SPACE = " ";
+    private static final String POSSIBLE_NAME_REGEX = "[a-zA-Z0-9]+";
+
     public Car {
         validate(name);
     }
 
     private void validate(String name) {
-        if (name == null || name.isEmpty() || name.length() > 5) {
-            throw new IllegalArgumentException("자동차의 이름은 1자 이상 5자 이하만 가능합니다.");
+        if (name == null || name.isEmpty() || name.length() > MAX_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차의 이름은 1자 이상 5자 이하만 가능해요.");
         }
+        if (hasWhitespace(name)) {
+            throw new IllegalArgumentException("자동차의 이름에는 공백이 포함될 수 없어요.");
+        }
+        if (hasInvalidCharacters(name)) {
+            throw new IllegalArgumentException("자동차의 이름에는 알파벳과 숫자만 사용할 수 있어요.");
+        }
+    }
+
+    private boolean hasWhitespace(String name) {
+        return name.contains(WHITE_SPACE);
+    }
+
+    private boolean hasInvalidCharacters(String name) {
+        return !name.matches(POSSIBLE_NAME_REGEX);
     }
 }
