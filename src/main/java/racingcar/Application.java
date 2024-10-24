78dcd600a5f5f7d90c47d5a0640fc83da49756be
soeun2537 <soeun2537@gmail.com>
2024-10-24 11:36:47 +0900

    feat: Car 객체 생성 및 초기화 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -86,4 +86,22 @@ public class Application {
             throw new IllegalArgumentException();
         }
     }
+
+    private static class Car {
+        private final String name;
+        private int distance;
+
+        public Car(String name) {
+            this.name = name;
+            this.distance = 0;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getDistance() {
+            return distance;
+        }
+    }
 }
