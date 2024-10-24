779f4b856c52e8ed9388f9f05fb8f52e9840f568
sc@t0a14 <gusdn6671@gmail.com>
2024-10-24 21:29:53 +0900

    feat(Car): 자동차의 전진 기능을 만든다

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,12 +1,20 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     String name;
+    int position;
 
-    public Car(String name) {
+    public Car(String name, int position) {
         this.name = name;
+        this.position = position;
     }
 
-
-
+    public void move(){
+        int random = Randoms.pickNumberInRange(0,9);
+        if(random > 4){
+            position++;
+        }
+    }
 }
