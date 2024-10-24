bcb172474e3e907dca521a07681cef19a5ee17d6
jseom <2ujs4311@gmail.com>
2024-10-24 11:31:52 +0900

    REFACTOR: Cars 생성자로 테스트할 수 있게 테스트 코드 변경
    - Cars 파일에서 LinkedHashMap을 만들던 메서드를 생성자로 변경해주었습니다.
    - 따라서 메서드로 테스트하던 코드를 생성자를 써서 테스트할 수 있게 코드만 변경해주었습니다.

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -8,57 +8,57 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarsTest {
-    private ArrayList<String> convertMapToList(Map<String, Integer> map) {
-        return new ArrayList<>(map.keySet());
+    private ArrayList<String> convertMapToList(Cars cars) {
+        return new ArrayList<>(cars.getCarNames());
     }
 
     @Test
     void 자동차이름_쉼표기준으로_나누기() {
-        Assertions.assertThat(convertMapToList(Cars.createNameList("abcde,woni,jun")))
-                .isEqualTo(List.of("abcde", "woni", "jun"));
-        Assertions.assertThat(convertMapToList(Cars.createNameList("가나다라마,woni,jun")))
+        Assertions.assertThat(convertMapToList(new Cars("abcde,woni,jun")))
+                        .isEqualTo(List.of("abcde", "woni", "jun"));
+        Assertions.assertThat(convertMapToList(new Cars("가나다라마,woni,jun")))
                 .isEqualTo(List.of("가나다라마", "woni", "jun"));
-        Assertions.assertThat(convertMapToList(Cars.createNameList("12345,woni,jun")))
+        Assertions.assertThat(convertMapToList(new Cars("12345,woni,jun")))
                 .isEqualTo(List.of("12345", "woni", "jun"));
-        Assertions.assertThat(convertMapToList(Cars.createNameList("++==+,woni,jun")))
+        Assertions.assertThat(convertMapToList(new Cars("++==+,woni,jun")))
                 .isEqualTo(List.of("++==+", "woni", "jun"));
     }
 
     @DisplayName(",를 기준으로 구분했을 때 빈문자열이 있으면 오류")
     @Test
     void 자동차이름_잘못된_입력() {
-        Assertions.assertThatThrownBy(() -> Cars.createNameList(""))
+        Assertions.assertThatThrownBy(() -> new Cars(""))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("잘못된 값을 입력하셨습니다.");
 
-        Assertions.assertThatThrownBy(() -> Cars.createNameList(",pobi,woni,jun"))
+        Assertions.assertThatThrownBy(() -> new Cars(",pobi,woni,jun"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("잘못된 값을 입력하셨습니다.");
 
-        Assertions.assertThatThrownBy(() -> Cars.createNameList("pobi,,woni,jun"))
+        Assertions.assertThatThrownBy(() -> new Cars("pobi,,woni,jun"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("잘못된 값을 입력하셨습니다.");
 
-        Assertions.assertThatThrownBy(() -> Cars.createNameList("pobi,woni,jun,"))
+        Assertions.assertThatThrownBy(() -> new Cars("pobi,woni,jun,"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("잘못된 값을 입력하셨습니다.");
     }
 
     @Test
     void 자동차이름_5자_제한_확인() {
-        Assertions.assertThatThrownBy(() -> Cars.createNameList("abcdef,woni,jun"))
+        Assertions.assertThatThrownBy(() -> new Cars("abcdef,woni,jun"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름이 5자를 초과했습니다.");
 
-        Assertions.assertThatThrownBy(() -> Cars.createNameList("가나다라마바,woni,jun"))
+        Assertions.assertThatThrownBy(() -> new Cars("가나다라마바,woni,jun"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름이 5자를 초과했습니다.");
 
-        Assertions.assertThatThrownBy(() -> Cars.createNameList("123456,woni,jun"))
+        Assertions.assertThatThrownBy(() -> new Cars("123456,woni,jun"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름이 5자를 초과했습니다.");
 
-        Assertions.assertThatThrownBy(() -> Cars.createNameList("/*-+=_,woni,jun"))
+        Assertions.assertThatThrownBy(() -> new Cars("/*-+=_,woni,jun"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름이 5자를 초과했습니다.");
     }
