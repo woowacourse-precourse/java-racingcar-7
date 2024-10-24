e22a58ffac5e547b1292c9e1ec1ef662ef4b6ad6
reindeer002 <lsj000424@gmail.com>
2024-10-24 18:30:40 +0900

    feat(wrapper): Add contains method to Range class
    - Add contains method to check if a given value is within the defined range.

--- src/main/java/racingcar/common/wrapper/Range.java
+++ src/main/java/racingcar/common/wrapper/Range.java
@@ -4,4 +4,8 @@ public record Range(Integer min, Integer max) {
     public static Range of(Integer min, Integer max) {
         return new Range(min, max);
     }
+
+    public boolean contains(Integer value) {
+        return value != null && value >= min && value <= max;
+    }
 }
