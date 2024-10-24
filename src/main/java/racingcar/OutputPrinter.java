1bd99fcae73bbf478856170d41402859b8280718
문고은 <>
2024-10-25 00:31:42 +0900

    feat(output) : 자동차 이름과 시도 횟수 입력 요청 메시지 출력 기능 추가

---
+++ src/main/java/racingcar/OutputPrinter.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class OutputPrinter {
+    public void promptCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void promptAttemptCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+}
