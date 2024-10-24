26a0842e00468f90410d85d460848346fdc6c476
maedachaeva <parkchaeyeon.b@gmail.com>
2024-10-24 13:20:18 +0900

    feat: 경주할 자동차 이름을 입력받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,9 @@
 package racingcar;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        UserInput userInput = new UserInput();  // 인스턴스 네이밍
+        List<String> carNames = userInput.readCarNames();
     }
 }
