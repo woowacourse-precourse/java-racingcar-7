8b251b0f6ba97c4b1d77f1eff46b321e8151b39b
sc@t0a14 <gusdn6671@gmail.com>
2024-10-24 22:23:59 +0900

    feat(Cars): 자동차 목록을 생성한다

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,12 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = new ArrayList<>();
+    }
+}
