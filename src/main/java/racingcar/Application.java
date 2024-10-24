a3c60a710991778e8b6f57a5e1dfa69e302000b9
juyeon320 <ju010320@gmail.com>
2024-10-24 19:47:28 +0900

    feat: 시도횟수 입력 기능 추가
    - [시도할 횟수는 몇 회인가요?]를 출력하는 기능 추가
    - 시도할 횟수를 입력하는 기능을 추가했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 
 class CarNameInput {
     public String getInput() {
@@ -9,12 +10,15 @@ class CarNameInput {
         return Console.readLine();
     }
 }
+
 //입력된 자동차 이름 쉼표로 구분하기
  class CarNameParser{
     public List<String> parse(String input){
         return Arrays.asList(input.split(","));
     }
 }
+
+//입력된 자동차 이름이 5자가 넘는지 확인하는 클래스
 class CarNameValidator{
     public void validate(List<String> carNames){
         for(String name : carNames){
@@ -24,6 +28,15 @@ class CarNameValidator{
         }
     }
 }
+
+class CounterInput{
+    public int getInput(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
+}
+
+
 public class Application {
     public static void main(String[] args) {
         CarNameInput carNameInput = new CarNameInput();
@@ -34,5 +47,8 @@ public class Application {
 
         CarNameValidator validator = new CarNameValidator();
         validator.validate(carNames);
+
+        CounterInput attemptInput = new CounterInput();
+        int attempts = attemptInput.getInput();
     }
 }
