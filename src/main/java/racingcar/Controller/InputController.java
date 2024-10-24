f8da3e0436dec271d4b494e42a4fae132ca78cba
m0304s <cms8955@naver.com>
2024-10-24 16:11:25 +0900

    Fix: 사용자가 잘못입력하였을 경우 애플리케이션 종료

--- src/main/java/racingcar/Controller/InputController.java
+++ src/main/java/racingcar/Controller/InputController.java
@@ -7,21 +7,11 @@ import racingcar.View.InputView;
 
 public class InputController {
     public static RacingCars setRacingCars(){
-        try{
-            CarNameValidator carNameValidator = new CarNameValidator(InputView.inputCars());
-            return new RacingCars(carNameValidator.names);
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-            return setRacingCars();
-        }
+        CarNameValidator carNameValidator = new CarNameValidator(InputView.inputCars());
+        return new RacingCars(carNameValidator.names);
     }
     public static int setRoundNum(){
-        try{
-            RoundNumValidator roundNumValidator = new RoundNumValidator(InputView.inputRounds());
-            return Integer.parseInt(roundNumValidator.ROUNDNUM_STRING);
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-            return setRoundNum();
-        }
+        RoundNumValidator roundNumValidator = new RoundNumValidator(InputView.inputRounds());
+        return Integer.parseInt(roundNumValidator.ROUNDNUM_STRING);
     }
 }
