1ca8cc97f66ebf890b40867daff005f4f42b059d
leejunnyeop <daffodil8620@naver.com>
2024-10-24 19:03:32 +0900

    [refactor] RacingView(view) - 입력 받을 때, 예외처리(5자 이하, 한글 또는 영어만 허용)

--- src/main/java/racingcar/view/RacingView.java
+++ src/main/java/racingcar/view/RacingView.java
@@ -6,13 +6,28 @@ import racingcar.dto.RaceResponse;
 
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RacingView {
+
+    private final String regex = "[^a-zA-Z0-9가-힣ㄱ-ㅎㅏ-ㅣ]";
 
     // 자동차 이름 입력 받기
     public String[] getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표로 구분)");
-        return Console.readLine().trim().split(",");
+        String[] carName = Console.readLine().trim().split(",");
+        Pattern pattern = Pattern.compile(regex);
+        for(String car : carName){
+            Matcher matcher = pattern.matcher(car);
+            if(!matcher.find()){
+                if(car.length() > 5){
+                    throw new IllegalArgumentException("자동차 이름이 5자가 넘습니다 : " + car);
+                }
+            }
+            throw new IllegalArgumentException("한글 또는 영어만 헝용 합니다. : " + car);
+        }
+        return carName;
     }
 
     // 시도 횟수 입력 받기
