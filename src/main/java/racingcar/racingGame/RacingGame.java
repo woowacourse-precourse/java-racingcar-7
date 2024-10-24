0831edcf5d3048414c21f12e65287a274457887b
LeeDongHoon <likelion.leedh@gmail.com>
2024-10-24 19:11:04 +0900

    [FEAT] : RacingGameTemplate 구현 클래스 및 경주 게임 데이터 클래스 생성
    RacingGame : RacingGameTemplate의 추상 메서드 구현
    RacingGameData : RacingGame에서 사용되는 데이터 클래스 구현

---
+++ src/main/java/racingcar/racingGame/RacingGame.java
@@ -0,0 +1,74 @@
+package racingcar.racingGame;
+
+import racingcar.car.Car;
+import racingcar.validation.AttemptValidation;
+import racingcar.validation.CarNameValidation;
+import racingcar.views.InputView;
+import racingcar.views.OutputView;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingGame extends RacingGameTemplate {
+
+    private final RacingGameData data = new RacingGameData();
+
+    public RacingGame() {
+    }
+
+    @Override
+    protected void initializeGame() {
+        List<String> cars = new ArrayList<>();
+        for (String carName : InputView.inputCarNames().split(",")) {
+            CarNameValidation.validate(carName);
+            cars.add(carName);
+        }
+        String inputAttempts = InputView.inputRaceAttempts();
+        AttemptValidation.validate(inputAttempts);
+        data.initialize(cars, new BigInteger(inputAttempts));
+    }
+
+    @Override
+    protected void race() {
+        BigInteger attemptCount = BigInteger.ZERO;
+        racingPerAttempt(attemptCount);
+    }
+
+    @Override
+    protected void announceWinners() {
+        BigInteger winningPosition = getWinningPosition();
+        List<String> winnersNames = getWinnersName(winningPosition);
+        OutputView.printWinner(winnersNames);
+    }
+
+    private void racingPerAttempt(BigInteger attemptCount) {
+        while (attemptCount.compareTo(data.getAttempts()) < 0) {
+            moveOrStop();
+            attemptCount = attemptCount.add(BigInteger.ONE);
+            OutputView.printResultPerAttempts(data.getCars());
+        }
+    }
+
+    private List<String> getWinnersName(BigInteger winningPosition) {
+        return data.getCars()
+                .stream()
+                .filter(car -> car.getPosition().compareTo(winningPosition) == 0)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private BigInteger getWinningPosition() {
+        return data.getCars()
+                .stream()
+                .map(Car::getPosition)
+                .max(BigInteger::compareTo)
+                .orElse(BigInteger.ZERO);
+    }
+
+    private void moveOrStop() {
+        data.getCars()
+                .forEach(Car::moveOrStop);
+    }
+}
