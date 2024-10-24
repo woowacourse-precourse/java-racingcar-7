b6cebe9b98e2adefe2307ade25eb0b84e8f522d8
dbdb456 <adad568@naver.com>
2024-10-24 20:45:17 +0900

    feat(validator): 문자열이 비었는지 검증 메서드 추가

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -1,4 +1,10 @@
 package racingcar.validator;
 
 public class Validator {
+
+    public void validateInputNotBlank(String string) {
+        if (string.isBlank()) {
+            throw new IllegalArgumentException("입력받은 자동차 이름이 없습니다.");
+        }
+    }
 }
