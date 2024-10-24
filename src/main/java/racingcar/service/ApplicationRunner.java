6eb9f4b6de1e06cb34406e704277b04768ffd9f3
YeaChan05 <qkenrdl05@gmail.com>
2024-10-25 01:34:46 +0900

    feat(ApplicationRunner): 서비스 조합
    경주 서비스를 통해 서비스 마무리

--- src/main/java/racingcar/service/ApplicationRunner.java
+++ src/main/java/racingcar/service/ApplicationRunner.java
@@ -16,9 +16,8 @@ public abstract class ApplicationRunner {
         }
         CarFactory carFactory = new CarFactory();
         Cars cars=carFactory.createCars(firstLine);
-        //TODO 2024 10 24 22:38:46 : 선수들을 주입받는 경주 서비스 만들기
-        //TODO 2024 10 24 22:39:12 : 시행 횟수만큼 경주를 실행
-        //TODO 2024 10 24 22:39:25 : 각 시행마다 거리 출력
-        //TODO 2024 10 24 22:42:24 : 경기가 끝나면 우승자 출력
+
+        RacingService racingService = new RacingService(cars);
+        racingService.playGame(secondLine);
     }
 }
