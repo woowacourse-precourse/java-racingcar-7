cdeff64c741bae5b1712a30fc97a644a68ff7e97
Hanjjong <tim4974@naver.com>
2024-10-24 15:50:22 +0900

    style(Parser): 조건문 중괄호 추가

--- src/main/java/racingcar/util/Parser.java
+++ src/main/java/racingcar/util/Parser.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 public class Parser {
     public static List<String> parseCars(String input) {
-        if (input == null || input.isBlank())
+        if (input == null || input.isBlank()) {
             throw new IllegalArgumentException();
+        }
         String[] cars = input.split(",");
 
         return List.of(cars);
