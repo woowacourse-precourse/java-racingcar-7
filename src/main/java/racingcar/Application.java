e3780bab4f81ae9d1ccd70e0c2aa72fc2f3d813c
skilt <robot4440@naver.com>
2024-10-24 19:46:27 +0900

    feat: 시도 횟수 입력 기능 추가  - 시도 횟수는 양수값을 받는다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,6 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         String[] carName = Input.carName();
-        //int count = Input.count();
+        int count = Input.count();
     }
 }
