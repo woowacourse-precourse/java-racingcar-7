fb49b76ea13399e6e7c6da1d6f21a49be204f257
maedachaeva <parkchaeyeon.b@gmail.com>
2024-10-24 16:04:10 +0900

    feat: 입력받은 자동차 이름을 리스트에 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,10 @@
 package racingcar;
+
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        UserInput userInput = new UserInput();  // 인스턴스 네이밍
-        List<String> carNames = userInput.readCarNames();
+        UserInput userInput = new UserInput();
+        List<String> racingCarNames = userInput.readCarNames();
     }
 }
--- src/main/java/racingcar/UserInput.java
+++ src/main/java/racingcar/UserInput.java
@@ -1,11 +1,17 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class UserInput {
-    public String readCarNames() {
+    public List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String rawCarNames = Console.readLine();
         return parseCarNames(rawCarNames);
+    }
+
+    private List<String> parseCarNames(String rawCarNames) {
+        return Arrays.asList(rawCarNames.split(","));
     }
 }
