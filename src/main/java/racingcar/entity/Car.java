04dd00bce2a15c4a3ff20b0f165929566dddea40
yeong-hwan <jghff700@naver.com>
2024-10-24 22:36:32 +0900

    feat(entity): implement car entity

---
+++ src/main/java/racingcar/entity/Car.java
@@ -0,0 +1,33 @@
+package racingcar.entity;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5 || name.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        position++;
+    }
+
+    public String getPositionDisplay() {
+        return "-".repeat(position);
+    }
+}
