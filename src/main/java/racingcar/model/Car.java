ec3cf0eb8d981498b99c9dc4bd3bfb9085cd8cac
sc@t0a14 <gusdn6671@gmail.com>
2024-10-24 22:13:40 +0900

    feat(Car): 자동차를 생성할시 검증한다

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,20 +1,27 @@
 package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.utils.Validator;
 
 public class Car {
     String name;
     int position;
 
-    public Car(String name, int position) {
+    public Car(String name) {
+        validate(name);
         this.name = name;
-        this.position = position;
     }
 
-    public void move(){
-        int random = Randoms.pickNumberInRange(0,9);
-        if(random > 4){
+
+    public void move() {
+        int random = Randoms.pickNumberInRange(0, 9);
+        if (random > 4) {
             position++;
         }
+    }
+
+    private void validate(String name) {
+        Validator.isEmpty(name);
+        Validator.isCarNameRange(name);
     }
 }
