d83c546c21c3d5ae96c710422eb2701a747d5086
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-25 00:40:49 +0900

    feat($DefaultRandomMovingStrategy): 기본 랜덤 이동 전략 클래스 구현
    - RandomMovingStrategy 의 구현체로, 기본 랜덤 이동 전략을 구현함.

---
+++ src/main/java/racingcar/model/moving_strategy/random/DefaultRandomMovingStrategy.java
@@ -0,0 +1,21 @@
+package racingcar.model.moving_strategy.random;
+
+import racingcar.util.ramdom.RandomRange;
+import racingcar.util.ramdom.RandomUtils;
+
+public class DefaultRandomMovingStrategy implements RandomMovingStrategy {
+
+    private static final int DEFAULT_RANDOM_RANGE_MIN = 0;
+    private static final int DEFAULT_RANDOM_RANGE_MAX = 9;
+    private static final int MIN_MOVEABLE_NUMBER = 4;
+
+    @Override
+    public boolean isMovable() {
+        return generateRandomValue() >= MIN_MOVEABLE_NUMBER;
+    }
+
+    @Override
+    public Integer generateRandomValue() {
+        return RandomUtils.generateRandomNumber(new RandomRange(DEFAULT_RANDOM_RANGE_MIN, DEFAULT_RANDOM_RANGE_MAX));
+    }
+}
