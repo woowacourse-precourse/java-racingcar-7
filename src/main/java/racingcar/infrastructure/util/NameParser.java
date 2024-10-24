11d8cf34c1659d2ad8f7a69c30f4fb1297dbc768
jijongkwon <help3451@naver.com>
2024-10-25 01:39:21 +0900

    feat(util) : NameParser 생성
    TODO
    - parsingName : ',' 로 이름 구별하기

---
+++ src/main/java/racingcar/infrastructure/util/NameParser.java
@@ -0,0 +1,11 @@
+package racingcar.infrastructure.util;
+
+import java.util.List;
+
+public class NameParser {
+
+    // TODO : ',' 로 이름 구별하기
+    public List<String> parsingName(String name) {
+        return null;
+    }
+}
