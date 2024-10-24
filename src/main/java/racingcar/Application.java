63d70a6872822951acebd7f6cd4cdb49d1de6603
윤진완 <yunjin-wan@yunjin-wan-ui-MacBookAir.local>
2024-10-24 16:53:31 +0900

    feat(Car) Car객체 생성해서 과정 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,54 +3,45 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input = Console.readLine();
-        String[] cars = input.split(",");
+        String[] carNames = input.split(",");
+        /*
+        입력 예외 처리해야함
+        이름은 5자 이내
+        구분자는 ,(쉼표) 만
+         */
         String inputNum = Console.readLine();
         int count = Integer.parseInt(inputNum);
-        int mem1 = 0;
-        int mem2 = 0;
-        int mem3 = 0;
-
-        while(count >0) {
-            int a = Randoms.pickNumberInRange(0, 9);
-            int b = Randoms.pickNumberInRange(0, 9);
-            int c = Randoms.pickNumberInRange(0, 9);
-
-            if (a >= 4) {
-                mem1++;
-            }
-            if (b >= 4) {
-                mem2++;
-            }
-            if (c >= 4) {
-                mem3 ++;
-            }
-            System.out.print(cars[0] +" : " );
-            for (int i = 0; i <mem1; i++) {
-                System.out.print("-");
-            }
-            System.out.println();
-
-            System.out.print(cars[1] +" : " );
-            for (int i = 0; i <mem2; i++) {
-                System.out.print("-");
-            }
-            System.out.println();
-
-            System.out.print(cars[2] +" : " );
-            for (int i = 0; i <mem3; i++) {
-                System.out.print("-");
-            }
-            System.out.println();
 
 
+        List<Car> cars = new ArrayList<>();
+
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        while (count > 0) {
+            for (Car car : cars) {
+                car.move();
+            }
+
+            for (Car car : cars) {
+                System.out.print(car.getName() + " : ");
+                for (int i = 0; i < car.getLocation(); i++) {
+                    System.out.print("-");
+                }
+                System.out.println();
+            }
             System.out.println();
             count --;
         }
+        }
 
-    }
 }
 
