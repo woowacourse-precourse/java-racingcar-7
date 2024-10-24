453f16d9f4afe541c284fda7adac1f1033ab6f6d
Dev-LHY <dev.leehy@gmail.com>
2024-10-25 05:09:43 +0900

    feat(parsing): String[] to Map 변환 추가

--- src/main/java/racingcar/util/Parsing.java
+++ src/main/java/racingcar/util/Parsing.java
@@ -1,9 +1,20 @@
 package racingcar.util;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Parsing {
     private static final String DELIMITER = ",";
 
     public String[] splitCarName(String input) {
         return input.split(DELIMITER);
+    }
+
+    public static Map<String, String> convertStringArrToMap(String[] carNames) {
+        Map<String, String> player = new LinkedHashMap<>();
+        for (String carName : carNames) {
+            player.put(carName, "");
+        }
+        return player;
     }
 }
