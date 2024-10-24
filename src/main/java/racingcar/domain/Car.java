69f2d06769a098b24a7d289119d4d9b5b0ab09dd
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-24 19:56:12 +0900

    feat(Car): 자동차 이름을 검증하는 로직 추가
    자동차 이름이 5자 이하가 아닌 경우 예외가 발생

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,18 +1,19 @@
 package racingcar.domain;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Car {
 
     private final List<String> cars;
 
-    private Car(List<String> cars) {
-        this.cars = cars;
+    private Car(String input) {
+        List<String> carNames = splitByComma(input);
+        Validator.validateCars(carNames);
+        this.cars = carNames;
     }
 
     public static Car from(String input) {
-        return new Car(splitByComma(input));
+        return new Car(input);
     }
 
     public List<String> getCars() {
@@ -22,6 +23,23 @@ public class Car {
     private static List<String> splitByComma(String input) {
         return Arrays.stream(input.split(","))
                 .toList();
+    }
+
+    private static class Validator {
+
+        private final static int MAX_CAR_NAME_LENGTH = 5;
+
+        private static void validateCars(List<String> cars) {
+            validateCarNames(cars);
+        }
+
+        private static void validateCarNames(List<String> carNames) {
+            carNames.forEach(name -> {
+                if (name.length() > MAX_CAR_NAME_LENGTH) {
+                    throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+                }
+            });
+        }
     }
 
 }
