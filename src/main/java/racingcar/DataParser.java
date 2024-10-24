551d8f7683c0b29912c1193b8e5d9598b6ffac71
junghun00 <ji8657447@gmail.com>
2024-10-24 16:53:23 +0900

    feat: 자동차 이름 파싱 구현

--- src/main/java/racingcar/DataParser.java
+++ src/main/java/racingcar/DataParser.java
@@ -1,2 +1,55 @@
-package racingcar;public class DataParser {
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DataParser {
+    public static List<String> parseName(String input) {
+        List<String> list = Arrays.asList(input.split(","));
+
+        isNonEmpty(list);
+        checkLenght(list);
+        isAlphaNumeric(list);
+
+        return list;
+    }
+
+    private static void isAlphaNumeric(List<String> list) {
+        for (String input : list) {
+            isLetterOrDigit(input);
+        }
+    }
+
+    private static void isLetterOrDigit(String input) {
+        for (char c : input.toCharArray()) {
+            if ((!Character.isLetter(c) || Character.isDigit(c))) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private static void checkLenght(List<String> list) {
+        final int MAX_LENGHT = 5;
+
+        for (String s : list) {
+            if (s.length() > MAX_LENGHT) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public static void isNonEmpty(List<String> list) {
+        List<String> trimmdlist = new ArrayList<>();
+        for (String s : list) {
+            if (s.isBlank()) {
+                throw new IllegalArgumentException();
+            }
+            trimmdlist.add(s.trim());
+        }
+
+        list.clear();
+        list.addAll(trimmdlist);
+    }
+
 }
