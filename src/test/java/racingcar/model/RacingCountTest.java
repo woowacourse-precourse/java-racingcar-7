398ddaf7d462de08e1846563464a8c3769716ee2
DongYeop <s01054830455@gmail.com>
2024-10-24 16:18:28 +0900

    test: RacingCount 테스트
    - RacingCount 객체가 정상적으로 생성되는지 테스트
    - 시도 횟수 1미만에 대한 예외 테스트

---
+++ src/test/java/racingcar/model/RacingCountTest.java
@@ -0,0 +1,34 @@
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RacingCountTest {
+
+    @DisplayName("시도 횟수를 받아서 RacingCount 객체를 생성한다.")
+    @Test
+    void createRacingCount() {
+        //given
+        int count = 1;
+
+        //when
+        RacingCount racingCount = RacingCount.from(count);
+
+        //then
+        assertThat(racingCount).isEqualTo(RacingCount.from(1));
+    }
+
+    @DisplayName("경주 시도 횟수가 1보다 작은 경우 예외를 발생한다.")
+    @Test
+    void racingCountLessThenOne() {
+        //given
+        int count = 0;
+
+        //when //then
+        assertThatThrownBy(() -> RacingCount.from(count))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
