e8a55f7b1e608e9c0d0ec3cb896902ac42fc2c2a
choign3123 <choign3123@naver.com>
2024-10-24 23:00:05 +0900

    Feat(Application): 몇번의 라운드롤 실행할지 입력받는 로직 구현
    참고: 라운드=이동

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,5 @@
 package racingcar;
 
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -8,5 +7,8 @@ public class Application {
 
         String carNameMass = inputDisplay.getPromptInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
         String[] carNameList = carNameMass.split(",");
+
+        String roundStr = inputDisplay.getPromptInput("시도할 횟수는 몇 회인가요?\n");
+        int round = Integer.parseInt(roundStr);
     }
 }
