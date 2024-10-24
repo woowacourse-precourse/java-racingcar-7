13ab7a7b38f8d047ac4ba3b58326fb9aa21c80bb
jungsu <vwx1@naver.com>
2024-10-24 16:10:14 +0900

    feat: make a constructor for the Car class

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,5 +1,14 @@
 package racingcar.model;
 
 public class Car {
+  private final String name;
+  private int position;
 
+  public Car(String name) {
+    if (name.length() > 5) {
+      throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
+    }
+    this.name = name;
+    this.position = 0;
+  }
 }
