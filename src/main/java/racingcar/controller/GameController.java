62abc4d5ffe4dd92f3724a83f7d6e0f924fdfb28
choi <nippert@naver.com>
2024-10-24 15:00:56 +0900

    feat: 자동차 게임을 진행시키는 컨트롤러 작성

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,28 @@
+package racingcar.controller;
+
+import racingcar.domain.RacingCar;
+import racingcar.service.GameService;
+import view.InputView;
+import view.OutputView;
+import view.domain.InputResult;
+
+import java.util.List;
+
+public class GameController {
+
+    public final void run() throws IllegalArgumentException {
+        InputView inputView = new InputView();
+        InputResult input = inputView.input();
+
+        OutputView outputView = new OutputView();
+        GameService gameService = new GameService();
+        gameService.initializeRacingCars(input.getCars());
+        for (int i = 0; i < input.getGameCount(); i++) {
+            List<RacingCar> gamePerResult = gameService.game();
+            outputView.game(gamePerResult);
+        }
+
+        List<String> winner = gameService.findWinner();
+        outputView.printWinner(winner);
+    }
+}
