b5b53ceb22683d26d7359453c6088331d87b1814
johyewon <johyewon4601@gmail.com>
2024-10-24 22:32:43 +0900

    feat(Race): startRace와 getWinners 메소드 기능 추가

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,6 +1,6 @@
 package racingcar;
 
-import java.util.List;
+import java.util.*;
 
 public class Race {
     public List<Car> cars;
@@ -11,10 +11,27 @@ public class Race {
         this.moveCnt = moveCnt;
     }
     public void startRace() {
+        System.out.println("실행 결과");
 
+        for (int i = 0; i < moveCnt; i++) {
+            for (Car car : cars) {
+                car.move();
+                System.out.println(car.getName() + " : " + car.getDistance());
+            }
+            System.out.println();
+        }
     }
     public List<String> getWinners() {
+        int max = 0;
+        List<String> winners = new ArrayList<>();
 
-        return null;
+        for (Car car : cars) {
+            if (car.getDistance() > max) max = car.getDistance();
+        }
+
+        for (Car car : cars) {
+            if (car.getDistance() == max) winners.add(car.getName());
+        }
+        return winners;
     }
 }
