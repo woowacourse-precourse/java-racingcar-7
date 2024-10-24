d11232950f9737c4eb0b2edcfb323414c5efe047
jijongkwon <help3451@naver.com>
2024-10-25 01:40:42 +0900

    feat(service) : WinnerService 생성
    TODO
    - 이동거리가 최대값과 일치하는 지 비교하기
    - 최대값과 일치하는 자동차 저장하기
    - 우승자 반환하기

---
+++ src/main/java/racingcar/application/service/winner/WinnerService.java
@@ -0,0 +1,10 @@
+package racingcar.application.service.winner;
+
+public class WinnerService {
+
+    // TODO : 이동거리가 최대값과 일치하는 지 비교하기
+
+    // TODO : 최대값과 일치하는 자동차 저장하기
+
+    // TODO : 우승자 반환하기
+}
