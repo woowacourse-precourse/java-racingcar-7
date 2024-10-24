9e4e1f90ba73b07f8c855c109ea61265ecbcc075
daeuun <leeda51925@gmail.com>
2024-10-25 01:14:31 +0900

    feat: 최종 우승자를 출력한다
    경기 결과에서 구한 우승자 리스트를 출력한다. 우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,15 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
     static final String MESSAGE_RACE_RESULT = "실행 결과";
     static final String MOVE_PATTERN = "-";
+    static final String MESSAGE_WINNER = "최종 우승자 : ";
+
+    public void printLine() {
+        System.out.println();
+    }
 
     public void printResultMessage() {
         System.out.println(MESSAGE_RACE_RESULT);
@@ -18,4 +25,11 @@ public class OutputView {
     private String makeMovePatternFromDistance(int number) {
         return MOVE_PATTERN.repeat(Math.max(0, number));
     }
+
+    public void printWinners(List<String> winners) {
+        System.out.print(MESSAGE_WINNER);
+        String result = String.join(", ", winners);
+        System.out.println(result);
+    }
+
 }
