d617a85da9653d51fc875a18188ee0ffe646edcc
jangsubin <eltm521@naver.com>
2024-10-24 23:05:54 +0900

    test : 이름이 6자 이상인 경우 테스트 작성

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -6,4 +6,9 @@ public class InputValidator {
         //문자열이 비어있으면 true, 아니면 false
         return input.isEmpty();
     }
+
+    //문자열 길이가 5이하면 true, 5보다 크면 false
+    public boolean checkInputLength(String input) {
+        return true;
+    }
 }
--- src/test/java/racingcar/validator/InputValidatorTest.java
+++ src/test/java/racingcar/validator/InputValidatorTest.java
@@ -14,4 +14,17 @@ class InputValidatorTest {
         Assertions.assertThat(hasNoInput).isTrue();
     }
 
+    @Test
+    public void 이름이_6자_이상인_경우() {
+        InputValidator inputValidator = new InputValidator();
+
+        boolean length4 = inputValidator.checkInputLength("pobi,woni,jun"); //4자인 경우
+        boolean length5 = inputValidator.checkInputLength("pobii,woni,jun");//5자인 경우
+        boolean length6 = inputValidator.checkInputLength("pobiii,woni,jun");//6자인 경우
+
+        Assertions.assertThat(length4).isTrue();
+        Assertions.assertThat(length5).isTrue();
+        Assertions.assertThat(length6).isFalse();
+    }
+
 }
