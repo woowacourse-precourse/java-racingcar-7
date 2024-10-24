3810da8fe411a19c84a3018129415c46a26ca9e6
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-25 01:05:02 +0900

    test(InputHandlerTest): refactor 반복되는 문자열을 상수로 추출하여 가독성 개선
    - 자동차 이름과 시도 횟수를 상수로 추출하여 테스트 코드 가독성 향상
    - 빈 문자열을 상수로 선언하여 코드 중복 제거

--- src/test/java/racingcar/InputHandlerTest.java
+++ src/test/java/racingcar/InputHandlerTest.java
@@ -8,24 +8,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class InputHandlerTest extends NsTest {
 
+    private static final String CAR_NAMES = "pobi,woni,jun";
+    private static final String ATTEMPT_COUNT = "3";
+    private static final String EMPTY_STRING = "";
+
     @Test
     @DisplayName("올바른 입력 테스트")
     void 올바른입력테스트() {
         // given
         InputHandler inputHandler = new InputHandler();
-        String carNames = "pobi,woni,jun";
-        String attemptCount = "3";
 
         // when
-        run("pobi,woni,jun", "3");  // 여기서 미리 정의된 입력값을 run 메서드로 실행
-        UserInput userInput = ioHandler.inputCarNamesAndAttemptCount();
+        run(CAR_NAMES, ATTEMPT_COUNT);
+        UserInput userInput = inputHandler.inputCarNamesAndAttemptCount();
 
         // then
-        assertEquals("pobi,woni,jun", userInput.carNames());  // 자동차 이름이 올바르게 입력되었는지 검증
-        assertEquals("3", userInput.attemptCount());  // 시도 횟수가 올바르게 입력되었는지 검증
+        assertEquals(CAR_NAMES, userInput.carNames());
+        assertEquals(ATTEMPT_COUNT, userInput.attemptCount());
     }
 
-    // Validator 테스트에서 예외 발생 여부를 확인하므로 IOHandler에서는 예외가 발생하지 않음
     @Test
     @DisplayName("빈 문자열 입력 테스트 - 예외는 발생하지 않음")
     void 빈문자열입력테스트() {
@@ -33,17 +34,16 @@ class InputHandlerTest extends NsTest {
         InputHandler inputHandler = new InputHandler();
 
         // when
-        run("", "");
+        run(EMPTY_STRING, EMPTY_STRING);
         UserInput userInput = inputHandler.inputCarNamesAndAttemptCount();
 
         // then
-        assertEquals("", userInput.carNames());
-        assertEquals("", userInput.attemptCount());
+        assertEquals(EMPTY_STRING, userInput.carNames());
+        assertEquals(EMPTY_STRING, userInput.attemptCount());
     }
 
     @Override
     public void runMain() {
         Application.main(new String[]{});
     }
-
 }
