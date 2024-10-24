d4cc7a2455ab47401705d9d12d78319d7dbc8e80
joshinhyeok <shtree07@naver.com>
2024-10-25 00:47:34 +0900

    feat: Validator 추가 예외사항들과 테스트 코드 추가

--- src/test/java/racingcar/Validator.java
+++ src/test/java/racingcar/Validator.java
@@ -1,19 +1,41 @@
 package racingcar;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validator {
-    public static void validateCarNames(List<String> carNames) {
-        for (String carName : carNames) {
-            if (carName.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름이 5자를 초과합니다.");
-            }
+    public static void validateCarNames(List<Car> carList) {
+        Set<String> carNameSet = new HashSet<>();
+        for (Car car : carList) {
+            String carName = car.getCarName();
+            validateCarNameLength(carName);
+            validateCarNameIsEmpty(carName);
+            validateCarNameDuplication(carNameSet, carName);
         }
     }
 
-    static void validateAttemptCount(String input) {
+    public static void validateAttemptCount(String input) {
         if (!input.matches("^[1-9]\\d*$")) {
             throw new IllegalArgumentException("시행횟수가 양수가 아닙니다.");
+        }
+    }
+
+    private static void validateCarNameLength(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름이 5자를 초과합니다.");
+        }
+    }
+
+    private static void validateCarNameIsEmpty(String carName) {
+        if (carName.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름이 공백으로 되어있습니다.");
+        }
+    }
+
+    private static void validateCarNameDuplication(Set<String> carNameSet, String carName) {
+        if (!carNameSet.add(carName)) {
+            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
         }
     }
 }
--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -2,7 +2,7 @@ package racingcar;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -10,15 +10,35 @@ public class ValidatorTest {
 
     @Test
     void 자동차_이름이_5자_초과시_예외발생() {
-        List<String> carNames = Arrays.asList("pobiwoni,jun");
-
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("pobiwoni"));
         assertThrows(IllegalArgumentException.class, () -> {
-            Validator.validateCarNames(carNames);
+            Validator.validateCarNames(cars);
         });
     }
 
     @Test
-    void 시행_횟수_입력이_잘못된_값인_경우_예외발생() {
+    void 자동차_이름이_공백이면_예외발생() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car(""));
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validator.validateCarNames(cars);
+        });
+    }
+
+    @Test
+    void 자동차_이름이_중복되면_예외발생() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("pobi"));
+        cars.add(new Car("pobi"));
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validator.validateCarNames(cars);
+        });
+    }
+
+
+    @Test
+    void 시행_횟수_입력이_양수가_아니면_예외발생() {
         String input = "-1";
 
         assertThrows(IllegalArgumentException.class, () -> {
