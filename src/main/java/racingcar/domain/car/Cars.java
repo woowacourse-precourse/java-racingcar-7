bfc2ac36ee78125005c5f0a4dad5d21df0915a53
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-24 19:07:57 +0900

    feat: Cars 클래스 구현

---
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -0,0 +1,26 @@
+package racingcar.domain.car;
+
+import racingcar.domain.movement.RandomMovement;
+
+import java.security.PublicKey;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+
+    private List<Car> cars;
+
+    private Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public Cars of(List<String> carNames){
+        return new Cars(carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList()));
+    }
+
+    public void playOneRound(RandomMovement randomMovement){
+        cars.forEach(car -> car.go(randomMovement));
+    }
+}
