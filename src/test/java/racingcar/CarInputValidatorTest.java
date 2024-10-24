05611db160184acf83fd17e68b563bc2e41e3799
jbilee <julishahn@gmail.com>
2024-10-25 02:11:25 +0900

    test(validator): 자동차 및 시도 횟수 validator 클래스 테스트 코드 수정
    - 각 validator에 추가된 조건을 체크하는 테스트 코드 추가

--- src/test/java/racingcar/CarInputValidatorTest.java
+++ src/test/java/racingcar/CarInputValidatorTest.java
@@ -12,8 +12,9 @@ class CarInputValidatorTest {
     @DisplayName("자동차 이름으로 입력한 값이 유효하지 않으면 예외를 발생시킨다.")
     @Test
     void 자동차_입력값_검증_테스트() {
-        assertThrows(RuntimeException.class, () -> validator.validate("car1"));
-        assertThrows(RuntimeException.class, () -> validator.validate("car111,car2"));
-        assertThrows(RuntimeException.class, () -> validator.validate("car,car"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("car1"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("car111,car2"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("car,car"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("1,2,3,4,5,6,7,8,9,10,11"));
     }
 }
--- src/test/java/racingcar/RoundInputValidatorTest.java
+++ src/test/java/racingcar/RoundInputValidatorTest.java
@@ -12,9 +12,10 @@ class RoundInputValidatorTest {
     @DisplayName("시도 횟수로 입력한 값이 유효하지 않으면 예외를 발생시킨다.")
     @Test
     void 횟수_입력값_검증_테스트() {
-        assertThrows(RuntimeException.class, () -> validator.validate("asdf"));
-        assertThrows(RuntimeException.class, () -> validator.validate("0"));
-        assertThrows(RuntimeException.class, () -> validator.validate("-10"));
-        assertThrows(RuntimeException.class, () -> validator.validate("1.23"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("asdf"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("0"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("-10"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("1.23"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("301"));
     }
 }
