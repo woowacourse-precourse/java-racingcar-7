b35a9530ad3f7d519c9ae580ae648589e96b178d
zeus <jj99526@naver.com>
2024-10-24 20:29:00 +0900

    feat : 자동차의 이름을 입력 받고 자동차이름을 저장

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class Car {
+  private String name;
+  private int moveCount;
+
+  public Car(String name, int moveCount) {
+    this.name = name;
+    this.moveCount = moveCount;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getMoveCount() {
+    return moveCount;
+  }
+}
