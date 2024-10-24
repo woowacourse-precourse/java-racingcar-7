ff6d78e2e806568aaeb2c8f37dd472b83865ba3b
kgy1008 <gayeon1008@g.skku.edu>
2024-10-25 00:49:37 +0900

    refactor: cache patterns to improve performance

--- src/main/java/racingcar/model/car/Name.java
+++ src/main/java/racingcar/model/car/Name.java
@@ -1,10 +1,11 @@
 package racingcar.model.car;
 
+import java.util.regex.Pattern;
 import racingcar.common.ErrorMessage;
 
 public class Name {
     private static final int MAX_NAME_LENGTH = 5;
-    private static final String VALID_NAME_REGEX = "^[a-zA-Z0-9]+$";
+    private static final Pattern VALID_NAME_PATTERN = Pattern.compile("^[a-zA-Z0-9]+$");
     private final String name;
 
     public Name(final String name) {
@@ -24,7 +25,7 @@ public class Name {
     }
 
     private void validateNameCharacters(final String name) {
-        if (!name.matches(VALID_NAME_REGEX)) {
+        if (!VALID_NAME_PATTERN.matcher(name).matches()) {
             throw new IllegalArgumentException(ErrorMessage.NAME_INVALID_CHARACTER.getMessage());
         }
     }
