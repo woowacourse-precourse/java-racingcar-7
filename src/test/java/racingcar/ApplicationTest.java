9bcbb2494dfe67a713099b687e33815e4cd16616
donggundg <rlaehdrjs98@g.skku.edu>
2024-10-25 02:56:27 +0900

    feat(ApplicationTest) : 함수 테스트
    getCarInput 함수가 정상 작동 되고 있는지 확인하는 함수.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,12 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -13,6 +18,7 @@ class ApplicationTest extends NsTest {
     private static final int STOP = 3;
 
     @Test
+    @DisplayName("기능_테스트 number 1")
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
             () -> {
@@ -24,6 +30,17 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    @DisplayName("getCarInput 테스트")
+    void getCarInputTest() {
+        String input = "car1,car2,car3\n";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        String result = Application.getCarInput();
+        assertThat(result).isEqualTo("car1,car2,car3");
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 5 이상일 때")
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
