e3c0cd37e730afc60acc36ec5a84997bde428bd1
함지현 <gkawlgus01@naver.com>
2024-10-24 17:07:07 +0900

    feat: 이름 앞 뒤 공백 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String carName = Console.readLine();
+            List<String> carNames = List.of(carName.split(",")).stream().map(String::trim).collect(Collectors.toList());
+            System.out.println(carNames);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
