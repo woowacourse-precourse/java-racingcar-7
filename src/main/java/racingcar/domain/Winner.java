a25cf3666626ff73cdb14cbe5ba51dfa7e6d2910
bowook <mbosx316@naver.com>
2024-10-24 22:14:34 +0900

    feat(Winner) : 우승자 판단하는 기능 추가
    - 우승자만을 반환하기 위해 우승자 판단하는 기능 추가

--- src/main/java/racingcar/domain/Winner.java
+++ src/main/java/racingcar/domain/Winner.java
@@ -1,2 +1,25 @@
-package racingcar.domain;public class Winner {
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Winner {
+
+    private final Cars cars;
+    private final Race Race;
+    private final List<Car> winners;
+    public Winner(Cars cars, Race Race) {
+        this.cars = cars;
+        this.Race = Race;
+        this.winners = new ArrayList<>();
+    }
+
+    public List<Car> getWinners() {
+        for (Car car : cars.getCars()) {
+            if (car.getPosition() == Race.getMaxPosition()) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
 }
