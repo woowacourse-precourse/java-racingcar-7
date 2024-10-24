6350810de4ccd4c8f72ab1747d7f152b6b7b8fff
0tae <choiyt3465@naver.com>
2024-10-25 02:57:52 +0900

    feat: 자동차 엔티티 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,42 @@
+package racingcar;
+
+public class Car implements Comparable<Car>{
+    private String name;
+    private int status;
+
+    public Car(String givenName) {
+        this.name = givenName;
+        this.status = 0;
+    }
+
+    public void forward() {
+        increase();
+    }
+
+    public int getStatus(){
+        return this.status;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    private void setStatus(int givenStatus){
+        this.status = givenStatus;
+    }
+
+    private void increase(){
+        setStatus(getStatus()+1);
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return this.getStatus()-o.getStatus();
+    }
+
+    @Override
+    public String toString(){
+        return this.getName();
+    }
+}
+
