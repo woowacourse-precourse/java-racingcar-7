a5f7ee28b9421330626e65d98f4aef068f4c0f1d
Hacanna42 <dtmdgus901@gmail.com>
2024-10-24 17:24:41 +0900

    fix(OutputHandler): add a constant to fix printWinner()
    printWinner() 메서드가 공동 우승자를 출력할 때 구분자로 COLON(:)
    을 사용하는 오류를 수정했습니다. 이를 위해 DELIMITER_COMMA 라는 상수를
    선언했습니다.

--- src/main/java/racingcar/view/OutputHandler.java
+++ src/main/java/racingcar/view/OutputHandler.java
@@ -7,7 +7,8 @@ public class OutputHandler {
     private static final String RESULT_NOTICE_PHRASE = "\n실행 결과";
     private static final String WINNER_NOTICE_PHRASE = "최종 우승자";
     private static final String STATUS_BAR = "-";
-    private static final String DELIMITER = " : ";
+    private static final String DELIMITER_COLON = " : ";
+    private static final String DELIMITER_COMMA = ", ";
     private boolean isFirstPrint = true;
 
     public void printStatus(List<Car> cars) {
@@ -18,15 +19,15 @@ public class OutputHandler {
 
         for (Car car : cars) {
             String visualBar = STATUS_BAR.repeat(car.getPosition());
-            System.out.println(car.getName() + DELIMITER + visualBar);
+            System.out.println(car.getName() + DELIMITER_COLON + visualBar);
         }
         printNewLine();
     }
 
     public void printWinner(List<Car> winnerCars) {
         List<String> winnerNames = winnerCars.stream().map(Car::getName).toList();
-        String winnerName = String.join(DELIMITER, winnerNames);
-        System.out.println(WINNER_NOTICE_PHRASE + DELIMITER + winnerName);
+        String winnerName = String.join(DELIMITER_COMMA, winnerNames);
+        System.out.println(WINNER_NOTICE_PHRASE + DELIMITER_COLON + winnerName);
     }
 
     private void printNewLine() {
