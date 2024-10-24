3826950f35c4ea35a453741bf6bb4428300f8993
chaerish <ccyy8432@naver.com>
2024-10-24 17:32:50 +0900

    feat(InputDTO): InputDTO의 to 메소드 구현
    InputDTO에서 RacingCar로 변환하기 위해 to 메소드를 구현한다.

--- src/main/java/racingcar/InputDTO.java
+++ src/main/java/racingcar/InputDTO.java
@@ -3,4 +3,9 @@ package racingcar;
 import java.util.List;
 
 public record InputDTO(List<String> carNames, Long tryNums) {
+    public List<RacingCar> to() {
+        return this.carNames.stream()
+                .map(RacingCar::new)
+                .toList();
+    }
 }
