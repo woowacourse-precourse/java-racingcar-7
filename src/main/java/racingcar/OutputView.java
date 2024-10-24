ed9ee0466f6554ab35ba7ba7d09632bb310b8b37
tmdcheol <leesc08392@gmail.com>
2024-10-24 21:53:58 +0900

    feat: 모든 차 상태 출력 기능 추가
    - OutputView 클래스에 자동차 상태를 출력하는 printCar, printCars 메서드 추가
      - carName과 moveCount를 형식에 맞게 출력
      - 차 이름과 이동횟수를 출력하는 함수 분리
    
    - OutputViewTest 클래스에서 자동차 상태 출력 테스트 코드 작성
      - 단일 자동차의 상태를 출력하는 printCar 테스트
      - 여러 자동차의 상태를 출력하는 printCars 테스트

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,9 +1,33 @@
 package racingcar;
 
+import java.util.List;
+
 public class OutputView {
 
     public void println(final String message) {
         System.out.println(message);
+    }
+
+    public void printCars(final List<Car> cars) {
+        cars.forEach(this::printCar);
+    }
+
+    public void printCar(final Car car) {
+        String message = carNameFormat(car.getName()) + carMoveCountFormat(car.getMoveCount());
+        println(message);
+    }
+
+    private String carNameFormat(final String carName) {
+        String format = "%s : ";
+        return String.format(format, carName);
+    }
+
+    private String carMoveCountFormat(final int moveCount) {
+        StringBuilder builder = new StringBuilder();
+        for (int count = 0; count < moveCount; count++) {
+            builder.append("-");
+        }
+        return builder.toString();
     }
 
 }
--- src/test/java/racingcar/OutputViewTest.java
+++ src/test/java/racingcar/OutputViewTest.java
@@ -5,22 +5,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class OutputViewTest {
 
-    OutputView outputView = new OutputView();
+    OutputView outputView;
+    OutputStream outputStream;
+
+
+    Car pobiCar = new Car("pobi");
+    Car woniCar = new Car("woni");
+
+    String pobiMessage = "pobi : ";
+    String woniMessage = "woni : ";
+    String newLine = "\n";
+
+    @BeforeEach
+    void setUp() {
+        outputView = new OutputView();
+
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
 
     @Test
     @DisplayName("메세지 출력 테스트")
     public void 메세지_출력_테스트() {
         // given
         String message = "Hello, world!";
-        String newLine = "\n";
-
-        OutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
 
         // when
         outputView.println(message);
@@ -28,5 +43,30 @@ class OutputViewTest {
         // then
         assertThat(outputStream.toString()).isEqualTo(message + newLine);
     }
+
+    @Test
+    @DisplayName("printCar 테스트")
+    public void printCar_테스트() {
+        // when
+        outputView.printCar(pobiCar);
+
+        // then
+        assertThat(outputStream.toString()).isEqualTo(pobiMessage + newLine);
+    }
+
+    @Test
+    @DisplayName("printCars 테스트")
+    public void printCars_테스트() {
+        // given
+        List<Car> cars = List.of(pobiCar, woniCar);
+
+        // when
+        outputView.printCars(cars);
+
+        // then
+        assertThat(outputStream.toString())
+                .isEqualTo(pobiMessage + newLine + woniMessage + newLine);
+    }
+
 
 }
