c57e691a806b346236a0d989b79ce450bc55fc82
hongkikii <hyangmeeh@icloud.com>
2024-10-24 20:47:09 +0900

    feat(Application): 자동차 이름 입력 기능 수정
    자동차 이름 입력 시, 기본 조건 이외에 기능 목록에 정의된 모든 조건을 추가적으로 검증

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,8 +3,10 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,12 +14,7 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         String inputCarNames = Console.readLine();
-        String[] carNames = inputCarNames.split(",");
-        for(String carName : carNames) {
-            if (carName.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
-            }
-        }
+        String[] carNames = validate(inputCarNames);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
 
@@ -60,5 +57,58 @@ public class Application {
                 .toList();
 
         System.out.print("최종 우승자 : " + String.join(", ", winners));
+    }
+
+    public static String[] validate(String inputCarNames) {
+        if (inputCarNames == null) {
+            throw new IllegalArgumentException("자동차 이름은 null이어서는 안 됩니다.");
+        }
+        // 자동차 이름이 한 자 이상인 경우의 조건을 판단하는 코드가 분리되어 있음
+        if (inputCarNames.endsWith(",")) {
+            throw new IllegalArgumentException("자동차 이름은 1자 이상이어야 합니다.");
+        }
+        String[] carNames = inputCarNames.split(",");
+        if (carNames.length > 100) {
+            throw new IllegalArgumentException("자동차 이름은 100개 이하로 입력되어야 합니다.");
+        }
+        for(String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+            }
+            if (carName.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 1자 이상이어야 합니다.");
+            }
+            if (isAsciiCode(carName)) {
+                throw new IllegalArgumentException("자동차 이름은 아스키 코드여야 합니다.");
+            }
+            if (carName.trim().isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 공백 문자로만 이루어져 있어서는 안 됩니다.");
+            }
+        }
+        // 순서가 여기 있어야 논리적으로 맞음 -> 분리해야 할까?
+        if(isSameNameContained(carNames)) {
+            throw new IllegalArgumentException("자동차 이름이 중복되어서는 안 됩니다.");
+        }
+        return carNames;
+    }
+
+    private static boolean isAsciiCode(String carName) {
+        char[] chars = carName.toCharArray();
+        for(char c : chars) {
+            if (c > 127) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isSameNameContained(String[] carNames) {
+        Set<String> carNameCheckSet = new HashSet<>();
+        for(String carName : carNames) {
+            if(!carNameCheckSet.add(carName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
