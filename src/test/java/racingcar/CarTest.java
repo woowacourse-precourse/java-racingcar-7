7db31156dcaa4c839f7360d23337f147ba6ace26
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-10-24 21:25:26 +0900

    test(car): 자동차 이름 예외 기능 테스트 생성자 함수 사용으로 수정
    연쇄적으로 이루어져야 하는 검증이므로 두개의 정적 메서드를 모두 사용하는
    생성자 함수를 테스트하는 것이 올바르다고 생각함
    
    기존 : 정적 메서드 사용
    수정 : 생성자 함수 사용

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -27,28 +27,33 @@ class CarTest {
     @Test
     void 이름_길이_예외() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> Car.validateNameLength("pobiiii"))
-                    .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> new Car("pobiiii"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("자동차 이름은 5자 이하의 문자열만 가능합니다.");
         });
     }
 
     @Test
     void 빈_이름_예외() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> Car.validateEmptyName(""))
-                    .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> new Car(""))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("빈 이름은 사용할 수 없습니다.");
         });
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> Car.validateEmptyName("  "))
-                    .isInstanceOf(IllegalArgumentException.class);
+            (assertThatThrownBy(() -> new Car("  "))
+                    .isInstanceOf(IllegalArgumentException.class))
+                    .hasMessageContaining("빈 이름은 사용할 수 없습니다.");
         });
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> Car.validateEmptyName("\n\n"))
-                    .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> new Car(" \n\n "))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("빈 이름은 사용할 수 없습니다.");
         });
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> Car.validateEmptyName(null))
-                    .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> new Car(null))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("빈 이름은 사용할 수 없습니다.");
         });
     }
 }
