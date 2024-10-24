16a426be55f42c5cc718f65dec35a683518e7d39
JeongSH1 <jyt001532@gmail.com>
2024-10-24 21:50:02 +0900

    feat(UserInput): 사용자 입력에 대한 예외처리를 진행한다.
    구현된 사항
    - isValidCarName(String carName): boolean 메서드를 통해 carName에 글자수에 대한 검증을 진행한다.
    - isValidNumberOfAttempts(String numberOfAttemptsInput): boolean 메서드를 통해 시도할 횟수에 대한 검증을 진행한다.
    
    수정된 사항
    - readNamesOfCars(): String[] 자동차들의 이름 입력에 대한 검증을 진행하고, 형식에 어긋나면 IllegalArgumentException를 발생시킨다.
    - readNumberOfAttempts(): int 시도할 횟수에 대힌 입력을 검증하고, 형식에 어긋나면 IllegalArgumentException를 발생시킨다.

--- src/main/java/racingcar/UserInput.java
+++ src/main/java/racingcar/UserInput.java
@@ -5,10 +5,36 @@ import camp.nextstep.edu.missionutils.Console;
 public class UserInput {
 
     public String[] readNamesOfCars() {
-        return Console.readLine().split(",");
+        String[] namesOfCars = Console.readLine().split(",");
+
+        for (String carName : namesOfCars) {
+            if (!isValidCarName(carName)) {
+                throw new IllegalArgumentException("자동차 이름(\"" + carName + "\")에 대한 형식이 잘못되었습니다.");
+            }
+        }
+
+        return namesOfCars;
     }
 
     public int readNumberOfAttempts() {
+        String numberOfAttemptsInput = Console.readLine();
+
+        if (!isValidNumberOfAttempts(numberOfAttemptsInput)) {
+            throw new IllegalArgumentException("시도할 횟수(\"" + numberOfAttemptsInput + "\")에 대한 형식이 잘못되었습니다.");
+        }
         return Integer.parseInt(Console.readLine());
+    }
+
+    private boolean isValidCarName(String carName) {
+        return carName.length() > 5;
+    }
+
+    private boolean isValidNumberOfAttempts(String numberOfAttemptsInput) {
+        try {
+            int numberOfAttempts = Integer.parseInt(numberOfAttemptsInput);
+            return numberOfAttempts > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
