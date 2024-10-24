8f27fcf8500e664cf01402addc425d6e5ca584d0
LSBsity <dltmdqls51@naver.com>
2024-10-24 12:13:25 +0900

    feat(carList): carList 일급컬렉션 추가
    반환 시 불변 객체로 생성

--- src/main/java/racingcar/common/exception/ErrorCode.java
+++ src/main/java/racingcar/common/exception/ErrorCode.java
@@ -3,7 +3,8 @@ package racingcar.common.exception;
 public enum ErrorCode {
 
     LENGTH_ERROR("자동차의 이름은 1자 이상, 5자 이하여야 합니다."),
-    PARSING_ERROR("시도할 횟수는 숫자여야 합니다.");
+    PARSING_ERROR("시도할 횟수는 숫자여야 합니다."),
+    NAMES_ERROR("잘못된 입력입니다.");
 
     private final String message;
 
