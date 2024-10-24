3215f5c8fa597ed59cfbc02ce12904bd511c643e
btongtong <leeym2615@naver.com>
2024-10-24 14:45:59 +0900

    test(ApplicationTest): Test validation for car names
    Test validateStartEnd() for names starting or ending with a comma
    Test validateDuplication() for duplicate car names
    Test validateLength() to ensure names do not exceed 5 character
    Test validateEmpty() for emtpy names
    Test isValidName() to verity both success and fail cases

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -35,4 +35,61 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+
+    @Test
+    void validateStartEnd() {
+        String carNames = "Karina,Winter,Giselle,Ningning,";
+        assertThatThrownBy(() -> Application.validateStartEnd(carNames))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validateDuplication() {
+        String[] namesByComma = new String[] {"Karina", "Winter", "Winter", "Giselle", "Ningning"};
+        assertThatThrownBy(() -> Application.validateDuplication(namesByComma))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validateLength() {
+        String name = "NingNing";
+        assertThatThrownBy(() -> Application.validateLength(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validateEmpty() {
+        String emptyName = "";
+        assertThatThrownBy(() -> Application.validateEmpty(emptyName))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        String nullName = null;
+        assertThatThrownBy(() -> Application.validateEmpty(nullName))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        String blankSpaceName = "      ";
+        assertThatThrownBy(() -> Application.validateEmpty(blankSpaceName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void isValidName() {
+        String carNames = "Karin,Winte,Gisel,NingN";
+        assertThat(Application.isValidName(carNames)).isTrue();
+    }
+
+    @Test
+    void isValidName_fail() {
+        String longCarName = "Karina,Winter,Giselle,NingNing";
+        assertThatThrownBy(() -> Application.isValidName(longCarName))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        String duplicateName = "Karin,Wint,Wint,Gis,Ning";
+        assertThatThrownBy(() -> Application.isValidName(duplicateName))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        String emptyName = "Karin,Win,,Gis";
+        assertThatThrownBy(() -> Application.isValidName(emptyName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
