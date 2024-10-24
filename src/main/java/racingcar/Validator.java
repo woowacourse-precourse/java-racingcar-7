a6979936b20cadbac08ff0b7ebd0443cd78b7c70
5ms <oms010414@gmail.com>
2024-10-25 01:43:49 +0900

    Validator 사용자 입력 검증 기능  구현

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,4 +1,37 @@
 package racingcar;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class Validator {
+
+    public HashSet<String> usernameSet = new HashSet<>();
+
+    public void validateUsernames(List<String> usernames) {
+        for (String username : usernames) {
+            validateUsernameLength(username);
+            validateDuplicateUsername(username);
+            usernameSet.add(username);
+        }
+    }
+
+    private void validateUsernameLength(String name){
+        if(name.length() > 5 || name.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicateUsername(String name){
+        if(usernameSet.contains(name)){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void isDigit(String string){
+        for(int i = 0; i < string.length(); i++){
+            if(!Character.isDigit(string.charAt(i))){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
