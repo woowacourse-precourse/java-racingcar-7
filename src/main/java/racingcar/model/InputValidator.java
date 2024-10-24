767c160514a3991ba972a9888bf1f0873b5e24fb
corpi <lj845854@gmail.com>
2024-10-24 17:19:57 +0900

    refactor(InputValidator): InputException 에러를 발생하도록 변경

--- src/main/java/racingcar/model/InputValidator.java
+++ src/main/java/racingcar/model/InputValidator.java
@@ -1,6 +1,8 @@
 package racingcar.model;
 
 import java.util.regex.Pattern;
+import racingcar.error.InputError;
+import racingcar.error.InputException;
 
 public class InputValidator {
     public void validateCarName(String carName) {
@@ -10,13 +12,13 @@ public class InputValidator {
 
     private void validateLowerCaseOrKorean(String carName) {
         if (!Pattern.matches("^[가-힣a-z]{1,5}$", carName)) {
-            throw new IllegalArgumentException("한글과 영어 소문자로 작성해주세요.");
+            throw new InputException(InputError.INVALID_CAR_NAME_FORMAT);
         }
     }
 
     private void validateUnderFiveLength(String carName) {
         if (carName.length() > 5) {
-            throw new IllegalArgumentException("5자 이하의 이름을 작성해주세요.");
+            throw new InputException(InputError.INVALID_CAR_NAME_LENGTH);
         }
     }
 
@@ -29,7 +31,7 @@ public class InputValidator {
         try {
             Integer.parseInt(trialCount);
         } catch (NumberFormatException exception) {
-            throw new IllegalArgumentException("정수를 입력해주세요. (1 ~ 2147483647)");
+            throw new InputException(InputError.INVALID_TRIAL_COUNT_TYPE);
         }
     }
 
@@ -37,7 +39,7 @@ public class InputValidator {
         int parsedInt = Integer.parseInt(trialCount);
 
         if (parsedInt <= 0) {
-            throw new IllegalArgumentException("1 이상의 정수를 입력해주세요.");
+            throw new InputException(InputError.INVALID_TRIAL_COUNT_RANGE);
         }
     }
 }
