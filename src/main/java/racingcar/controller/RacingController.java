95975f76adc148bd89fc61a37f65534dc161a52e
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-10-25 00:16:24 +0900

    feat: RacingController 구현
    자동차 경주를 관리하는 controller 생성함

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,47 @@
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.Racing;
+import racingcar.utils.StringSplitter;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+
+    private Racing racing;
+    private Cars cars;
+    private StringSplitter stringSplitter;
+    private OutputView outputView;
+
+    public RacingController(Racing racing, Cars cars, StringSplitter stringSplitter,
+            OutputView outputView) {
+        this.racing = racing;
+        this.cars = cars;
+        this.stringSplitter = stringSplitter;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        String inputCarString = InputView.getInputCarString();
+        List<String> strings = stringSplitter.splitByDelimiter(inputCarString);
+        for (String s : strings) {
+            Car car = new Car(s);
+            cars.addCar(car);
+        }
+        int cnt = Integer.parseInt(InputView.getInputRepeatCount());
+        outputView.printGameResultMessage();
+        for (int i = 0; i < cnt; i++) {
+            List<Car> cars = racing.startRacing();
+            for (Car car : cars){
+                outputView.printGameResult(car.getName(), car.getMovedDistance());
+            }
+            outputView.printEmptyLine();
+        }
+
+        String winner = racing.findWinner(cars.getCars());
+        outputView.printWinner(winner);
+    }
+
+}
