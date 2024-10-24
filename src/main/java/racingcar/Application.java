b2ec362f54ddafe0a54f7c3700b2079c2eb060ee
kryptonite43 <sonyj0430@gmail.com>
2024-10-24 17:32:09 +0900

    feat: 랜덤 숫자로 자동차 전진 여부 판단 후 전진 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,9 +2,11 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Application {
+    static HashMap<String, Integer> carScores = new HashMap<String, Integer>();
     public static void main(String[] args) {
         // 차 이름 입력받고 이름을 하나씩 나누어 이름 배열에 저장 -> getCarNames()
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -16,17 +18,31 @@ public class Application {
         String repeatStr = Console.readLine();
         int repeat = Integer.parseInt(repeatStr);
 
+        // 자동차 스코어 초기화해서 해시맵에 저장
+        for (String car: cars) {
+            carScores.put(car, 0);
+        }
+        for (String car: cars) {
+            System.out.print(car+" - "+carScores.get(car)+"\n");
+        }
         // 입력받은 횟수만큼 차량 전진
         while (repeat-- > 0) {
-            moveCars(); // 차량 이동
+            moveCars(cars); // 차량 이동
             showCurrentStatus(); // 현재 차수 실행 결과 출력
         }
 
         // 프로그램 완료: 우승자 출력
         showWinner();
     }
-    public static void moveCars() {
-
+    public static void moveCars(String[] cars) {
+        for (int i=0; i<cars.length; i++) {
+            int randomNum = Randoms.pickNumberInRange(0, 9);
+            System.out.println(cars[i]+" 랜덤숫자: "+randomNum);
+            if (randomNum >= 4) {
+                int curScore = carScores.get(cars[i]);
+                carScores.put(cars[i], curScore+1);
+            }
+        }
     }
 
     public static void showCurrentStatus() {
