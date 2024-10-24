0191e25c018377607e8cfcb728465d480a638dab
minzzun99 <skg06123@naver.com>
2024-10-24 19:41:55 +0900

    feat(RaceMessage): 경주에 관련된 메시지를 관리하는 RaceMessage 추가
    - 자동차 이름 입력, 시도 횟수 입력, 결과 출력 등의 메시지를 상수로 관리
    - getMessage()를 통해 메시지 반환

---
+++ src/main/java/racingcar/view/RaceMessage.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+public enum RaceMessage {
+    CAR_NAME_INPUT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    NUMBER_OF_RACE_INPUT("시도할 회수는 몇회인가요?"),
+    PRINT_RACE_RESULT("\n실행 결과"),
+    PRINT_WINNER_FORMAT("최종 우승자 : ");
+
+    private final String msg;
+
+    RaceMessage(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMessage() {
+        return msg;
+    }
+}
