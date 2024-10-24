a1b9dc2b31e6c3fbe68219d24ee191f46364fc59
kaswhy <sxshx818@naver.com>
2024-10-24 20:25:17 +0900

    feat(ValicateCarNames): 경주할 자동차들이 빈 문자열인지 검증하는 로직 작성
    변경 후
    - 주어진 문자열이 빈 문자열인 경우 예외 발생

---
+++ src/main/java/racingcar/validator/ValidateCarNames.java
@@ -0,0 +1,11 @@
+package racingcar.validator;
+
+public class ValidateCarNames {
+
+    public void isBlank(String carNames) {
+        if(carNames.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름은 빈 값이 될 수 없습니다.");
+        }
+    }
+
+}
