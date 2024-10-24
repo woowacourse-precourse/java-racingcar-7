706ecb87878f0dfd385d7655f1509921ee6b8724
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-24 17:15:11 +0900

    refactor: 상수 primitive 형태로 변경

--- src/main/java/racingcar/domain/movement/RandomMovementGenerator.java
+++ src/main/java/racingcar/domain/movement/RandomMovementGenerator.java
@@ -4,9 +4,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomMovementGenerator implements RandomMovement {
 
-    public static final Integer RANDOM_MOVE_THRESHOLD = 4;
-    public static final Integer MAX_MOVEMENT_VALUE = 9;
-    public static final Integer MIN_MOVEMENT_VALUE = 0;
+    public static final int RANDOM_MOVE_THRESHOLD = 4;
+    public static final int MAX_MOVEMENT_VALUE = 9;
+    public static final int MIN_MOVEMENT_VALUE = 0;
 
     @Override
     public boolean canGo() {
