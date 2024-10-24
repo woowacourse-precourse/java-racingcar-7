3d5ce89e4aa38ae84d0c74b2cb104ec4a88b43ea
shin-chaeeun <chaeeun0217@naver.com>
2024-10-24 20:42:31 +0900

    Feat(Application): Make map and createMap method

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,12 +3,15 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Application {
 
   public static void main(String[] args) {
     List<String> cars = getCarInput();
+    Map<String, String> carGameRecords = createCarGameRecords(cars);
   }
 
   public static List<String> getCarInput() {
@@ -26,5 +29,20 @@ public class Application {
       throw new IllegalArgumentException("입력에 허용되지 않은 문자가 포함되어 있습니다.");
     }
   }
+
+  public static Map<String, String> createCarGameRecords(List<String> cars) {
+    Map<String, String> carGameRecords = new HashMap<>();
+
+    // 차 이름이 5자를 초과하는지 검증
+    for (String carName : cars) {
+      if (carName.length() > 5) {
+        throw new IllegalArgumentException("차 이름이 5자를 초과했습니다: " + carName);
+      }
+      carGameRecords.put(carName, "");  // 초기 값은 빈 문자열_전진을 안했으니까
+    }
+
+    return carGameRecords;
+  }
+
 
 }
