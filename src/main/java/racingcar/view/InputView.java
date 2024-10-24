8e12a489c252781b6fdb54f76b284fa445068666
jungsu <vwx1@naver.com>
2024-10-24 16:20:25 +0900

    feat: make a function  to take inputs
    - Added inputCarNames to receive car names as input.
    - Added inputTrialCount to receive the number of trials.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,17 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
+  public static String[] inputCarNames() {
+    System.out.println(START_MESSAGE);
+    String input =Console.readLine();;
+    return input.split(",");
+  }
+
+  public static int inputTrialCount() {
+    System.out.println(GET_TRIAL_MESSAGE);
+    return Integer.parseInt(Console.readLine());
+  }
 }
