f42457805b91c1c323df9831f49dfb4d2cfb1dbe
Koo-sangwoo <luckytkddn@gmail.com>
2024-10-24 22:43:27 +0900

    feat : 턴 진행 함수 구현 완료
    - 랜덤 함수를 사용해 4 이상일 시 해당 VO(자동차)의 전진값을 1 상승시킨다.
    - 이후 업데이트된 VO의 리스트를 다시 리턴한다.
    - 전체 게임 역시 반복문으로 진행될 가능성이 높기 때문에 업데이트만 시키고 리턴해야한다.

--- src/main/java/racingcar/util/game/GameUtil.java
+++ src/main/java/racingcar/util/game/GameUtil.java
@@ -1,5 +1,10 @@
 package racingcar.util.game;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.vo.CarVO;
+
+import java.util.List;
+
 /**
  * 게임 진행과 관련된 유틸리티 클래스
  */
@@ -13,12 +18,22 @@ public class GameUtil {
         }
     }
 
+    public static List<CarVO> oneTurnPrintAndUpdate(List<CarVO> carNameAndGoCountList) {
+        for (var carAndCount : carNameAndGoCountList) {
+            int randomNumber = Randoms.pickNumberInRange(0,9);
+            if (randomNumber >= 4) {
+                carAndCount.goOneStep();
+            }
+            System.out.println(carAndCount.getCarName() + " : " + "-".repeat(carAndCount.getGoCount()));
+        }
+
+        return carNameAndGoCountList;
+    }
+
+    // TODO : 우승한 자동차를 추출하고 출력하는 함수
+
     // TODO : 게임 진행 함수
 
-    // TODO : 게임의 한턴을 진행하여 각 자동차의 값을 업데이트 해주는 함수
-
-    // TODO : 우승한 자동차를 추출하는 함수
-
-
-    private GameUtil() {}
+    private GameUtil() {
+    }
 }
