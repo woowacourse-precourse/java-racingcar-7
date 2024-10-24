d355e8785abb2574c93dfed2381f4b1dec20a6cc
parkdamin <ferrorist@naver.com>
2024-10-24 19:09:18 +0900

    feat(split Name String): 이름 문자열 구분 기능 추가
    사용자가 입력한 문자열을 구분자인 쉼표를 기준으로 나누는 기능을 추가했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,15 +1,20 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Application {
-    private static final char DELIMITER = ',';
+    private static final String DELIMITER = ",";
     private static final int NAME_LENGTH_LIMIT = 5;
     private static final int MOVING_FORWARD = 4;
 
-    private static Map<String, Integer> currentSituation = null;
-    private static int playCount = 0;
+    private static Map<String, Integer> stateMap = null;
+    private static int progressCount = 0;
     public static void main(String[] args) {
         setArguments();
     }
@@ -21,9 +26,21 @@ public class Application {
      */
     private static void setArguments() throws IllegalArgumentException {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
         String InputCarNames = Console.readLine();
+        List<String> nameList = splitCarNameString(InputCarNames);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
-        playCount = Integer.parseInt(Console.readLine());
+        progressCount = Integer.parseInt(Console.readLine());
+    }
+
+    /**
+     * 사용자가 입력한 이름 문자열을 구분자인 쉼표(,)를 기준으로 이름을 구분합니다.
+     *
+     * @param inputString 사용자가 입력한 이름 문자열입니다.
+     * @return 구분한 이름들에 대한 List입니다.
+     */
+    private static List<String> splitCarNameString(String inputString) {
+        return new ArrayList<>(Arrays.asList(inputString.strip().split(DELIMITER)));
     }
 }
