2e4449d3bdd9168b1df211ffcdb14953b9aee526
sunjong0214 <rlatjswhd8200@gmail.com>
2024-10-24 20:02:28 +0900

    feat(RacingCar): RacingCar 생성 시 문자 확인 validation 추가
    이름 중 한글과 영어 외 다른 문자나 숫자 등이 포함되면 IllegalArgumentException 발생

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,11 +1,19 @@
 package racingcar;
 
+import java.util.Arrays;
+
 public class RacingCar {
     private final String name;
 
     public RacingCar(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
+        if (Arrays.stream(name.split("")).map(s -> s.charAt(0)).anyMatch(c -> {
+            boolean isKorean = (c >= '가' && c <= '힣');
+            return Character.isLetter(c) || isKorean;
+        })) {
+            throw new IllegalArgumentException("이름은 한글과 영어만 가능합니다.");
         }
         this.name = name;
     }
