b8935187c3f73adb4ba72080fa513b3e3c4c1e08
Dev-LHY <dev.leehy@gmail.com>
2024-10-25 04:29:44 +0900

    feat(invalidInputException): 자동차 이름 유효성 검사 추가
    - 차량 이름을 하나만 입력했을 경우 예외 처리
    - 차량 이름이 5글자를 초과하였을 경우 예외 처리
    - 차량 이름을 입력하지 않았을 경우 예외 처리
    - 차량 이름 최대 글자 수 상수화

--- src/main/java/racingcar/exception/InvalidInputException.java
+++ src/main/java/racingcar/exception/InvalidInputException.java
@@ -1,9 +1,27 @@
 package racingcar.exception;
 
 public class InvalidInputException {
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+
     public static void validateInput(String carName) {
         if (carName == null || carName.trim().isEmpty()) {
             throw new IllegalArgumentException(ExceptionMessage.INPUT_BLANK_IS_NOT_ALLOWED.getMessage());
+        }
+    }
+
+    public static void validateCarName(String[] carNames) {
+        if (carNames.length == 1) {
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_AT_LEAST_TWO_CAR.getMessage());
+        }
+
+        for (String carName : carNames) {
+            if (carName == null || carName.trim().isEmpty()) {
+                throw new IllegalArgumentException(ExceptionMessage.INPUT_BLANK_IS_NOT_ALLOWED.getMessage());
+            }
+
+            if (carName.length() > MAX_CAR_NAME_LENGTH) {
+                throw new IllegalArgumentException(ExceptionMessage.EXCEED_MAX_INPUT_LENGTH.getMessage());
+            }
         }
     }
 }
