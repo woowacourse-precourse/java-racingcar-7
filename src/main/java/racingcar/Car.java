6445b35cd7e1c7c403eab4510fd3f9b215defc73
김동진 <dongjin403@naver.com>
2024-10-24 19:36:46 +0900

    feat : Car클래스와 필요한 메소드 생성
    Car클래스와 car에 정보를 출력하는 printRacingInfo 기능 추가

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int advanceCount;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void addAdvanceCount() {
+        this.advanceCount++;
+    }
+
+    public void printRacingInfo() {
+        String sb = this.name + " : " + this.makeHyphen();
+        System.out.println(sb);
+    }
+
+    public String makeHyphen() {
+        if (this.advanceCount < 1) {
+            return "";
+        }
+        return "-".repeat(advanceCount);
+    }
+
+
+}
