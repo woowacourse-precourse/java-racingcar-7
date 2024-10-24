b8dea75727067bc136fad299516738afa0688b5f
w0dnj5 <jw991211@icloud.com>
2024-10-24 19:16:28 +0900

    feat(AttemptCount): AttemptCount 클래스 기능 추가
    - 객체 생성 시 유효성 검사
      - 범위 외 숫자가 들어오면 에러 발생
    - 단위 테스트를 통한 기능 검증 완료

---
+++ src/main/java/racingcar/model/track/AttemptCount.java
@@ -0,0 +1,22 @@
+package racingcar.model.track;
+
+public class AttemptCount {
+
+    private static final int MINIMUM = 0;
+    private final int attemptCount;
+
+    public AttemptCount(int attemptCount) {
+        validateAttemptCount(attemptCount);
+        this.attemptCount = attemptCount;
+    }
+
+    private void validateAttemptCount(int attemptCount) {
+        if (isOutOfRange(attemptCount)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isOutOfRange(int attemptCount) {
+        return attemptCount <= MINIMUM;
+    }
+}
---
+++ src/test/java/racingcar/model/track/AttemptCountTest.java
@@ -0,0 +1,15 @@
+package racingcar.model.track;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class AttemptCountTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -2, 0, -99, -84})
+    void throwExceptionIfAttemptCountIsOutOfRange(int attemptCount) {
+        Assertions.assertThatThrownBy(() -> new AttemptCount(attemptCount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
