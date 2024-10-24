fb87fdd3e002bbc6daa30173eea23709dab30e91
rladmstn <oz33177351@gmail.com>
2024-10-24 16:51:18 +0900

    test(validator) : 자동차 이름 목록 및 시도 횟수에 대한 검증 테스트 작성
    - 자동차 이름 목록 input에 대해 검증 수행 테스트 작성
    - 시도 횟수 input에 대해 검증 수행 테스트 작성

---
+++ src/test/java/racingcar/utils/InputValidatorTest.java
@@ -0,0 +1,37 @@
+package racingcar.utils;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.enums.ErrorMessage;
+
+public class InputValidatorTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"name1,name2,name3", "1,2,3", "a1,b2,c3", "Aaa,Bbb,Ccc"})
+    void 자동차_이름_입력_검증_성공(String input){
+        InputValidator.validateCarNames(input);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"*&,name2,name3", " ,2,3", "::,b2,c3", "이름-,Bbb,Ccc", ",aa,bb,"})
+    void 자동차_이름_입력_검증_실패(String input){
+        Assertions.assertThatThrownBy(() -> InputValidator.validateCarNames(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_CAR_NAMES.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1","1000","30","0"})
+    void 시도_횟수_검증_성공(String input){
+        InputValidator.validateTrialCount(input);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1","+11","aa","1001","숫자"})
+    void 시도_횟수_검증_실패(String input){
+        Assertions.assertThatThrownBy(() -> InputValidator.validateTrialCount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_TRIAL_COUNT.getMessage());
+    }
+
+}
