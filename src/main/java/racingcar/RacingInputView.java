db16b9ae8f867403e1f07f6611de4216c7043486
soojinlee <sojinlee1004@naver.com>
2024-10-24 21:20:18 +0900

    feat(view) : 자동차의 이름들과, 진행 횟수를 입력받는다.

---
+++ src/main/java/racingcar/RacingInputView.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class RacingInputView {
+    public String RacingCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    public int RacingRoundCount() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Integer.parseInt(Console.readLine());
+    }
+
+}
+
+
