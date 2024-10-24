4f7afca7f16e778864441be1b0949463827ececa
HeungY <rkrkrkrk02@naver.com>
2024-10-24 21:34:07 +0900

    feat: create user input function

---
+++ src/main/java/racingcar/User.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class User {
+
+
+    public List<String> grantCarName(){
+        List<String> cars = new ArrayList<>();
+
+        String[] carName = Console.readLine().split(",");
+
+        Collections.addAll(cars, carName);
+
+        Console.close();
+
+        return cars;
+    }
+
+    public int moves(){
+        int moves;
+
+        moves = Integer.parseInt(Console.readLine());
+
+        Console.close();
+
+        return moves;
+    }
+
+}
