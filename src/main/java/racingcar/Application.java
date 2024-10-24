89fa46f9d7cac470900afa8f652bd6db60d71fd7
박규영 <magic9282m@gmail.com>
2024-10-25 06:02:28 +0900

    fix: modify doRace method
    doRace 메서드가 마지막에 printRaceWinner 메서드를 호출하도록 수정
    - 또한, 경주를 수행하는 반복문(26번 줄)에서 cars 가 아닌 carsRaceProgress 의 길이를 참고하도록 수정
      - 해당 함수에서 실제로 값에 접근하고 수정하는 배열은 carsRaceProgress 이기 때문에 반복문도 해당 배열의 길이를 참조하도록 함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -23,8 +23,8 @@ public class Application {
     }
     public static void doRace(int round) {
         for(int i=0; i<round; i++) {
-            for(int j=0; j<cars.length; j++) {
-                if(Randoms.pickNumberInRange(0, 9) >= 4) {
+            for (int j = 0; j < carsRaceProgress.length; j++) {
+                if (Randoms.pickNumberInRange(0, 9) >= 4) {
                     // 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우 자동차는 전진
                     carsRaceProgress[j].append('-');
                 }
@@ -33,6 +33,8 @@ public class Application {
             // 각 차수별 실행 결과의 구분을 위해 빈 줄을 하나 출력한다.
             System.out.println();
         }
+        // 모든 라운드가 끝난 뒤, 최종 우승자를 출력한다.
+        printRaceWinner();
     }
     public static void printRaceRound() {
         for(int i=0; i<cars.length; i++) {
