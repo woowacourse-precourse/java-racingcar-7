5dcc052bc8ece883aade133fb276c1098458abaa
Dev-LHY <dev.leehy@gmail.com>
2024-10-24 15:02:53 +0900

    feat(racingcar): 자동차 이름 반환 추가

---
+++ src/main/java/racingcar/model/Racingcar.java
@@ -0,0 +1,13 @@
+package racingcar.model;
+
+public class Racingcar {
+    private final String carName;
+
+    public Racingcar(String carName) {
+        this.carName = carName;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+}
