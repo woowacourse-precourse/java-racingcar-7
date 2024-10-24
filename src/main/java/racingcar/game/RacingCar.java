20f0eefb6374572392a57e90a85452927c21a9f5
Revi1337 <david122123@gmail.com>
2024-10-24 14:45:57 +0900

    feat: 경주할 자동차들을 입력받는 기능 구현

--- src/main/java/racingcar/game/RacingCar.java
+++ src/main/java/racingcar/game/RacingCar.java
@@ -1,8 +1,26 @@
 package racingcar.game;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class RacingCar {
 
     public void race() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> carNames = getCarNamesFromUser();
+    }
+
+    private List<String> getCarNamesFromUser() {
+        String carNames = Console.readLine();
+        return Arrays.stream(carNames.split(","))
+                .peek(this::validateCarNameLength)
+                .toList();
+    }
+
+    private void validateCarNameLength(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름의 길이는 5를 넘을 수 없습니다.");
+        }
     }
 }
