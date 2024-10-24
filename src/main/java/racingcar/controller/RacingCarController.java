7b6077039cad0eec12ff0ae33f13c8424e1175fc
byeongdoohan <quden04@gmail.com>
2024-10-25 06:03:37 +0900

    feat: 시도 횟수 0일 때 예외처리 추가

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -34,6 +34,8 @@ public class RacingCarController {
     private void checkTryNumLength(String tryNum) {
         if (tryNum.length() == 0) {
             throw new IllegalArgumentException("시도 횟수가 입력되지 않았습니다.");
+        } else if (tryNum.equals("0")) {
+            throw new IllegalArgumentException("시도 횟수는 0이 될 수 없습니다.");
         }
     }
 
