c034bbdc25cf769c0081f4f84dc634b7e43b660c
DongYeop <s01054830455@gmail.com>
2024-10-24 15:19:20 +0900

    refactor(InputValidation): 입력값 유효성 검사 메서드 타입 변경
    - 입력값의 유효성을 boolean 타입으로 반환
    - input값에 대한 유효성 검사는 동일하다고 판단하여, 유효성 검사에 대한 참,거짓만 판단하여 검사 결과에 대한 각각의 다른
    예외 메세지를 던지기 위해
    - final 클래스로 선언하여 상속을 불가하도록 함

--- src/main/java/racingcar/exception/InputValidation.java
+++ src/main/java/racingcar/exception/InputValidation.java
@@ -1,17 +1,15 @@
 package racingcar.exception;
 
-public class InputValidation {
+public final class InputValidation {
 
-    public void validateNullOrEmpty(String input) {
-        if (input == null || input.isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름을 입력해야 합니다.");
-        }
+    private InputValidation() {
     }
 
-    public void validateBlank(String input) {
-        if (input.contains(" ")) {
-            throw new IllegalArgumentException("공백을 포함할 수 없습니다.");
-        }
+    public static boolean validateNullOrEmpty(String input) {
+        return input == null || input.isEmpty();
     }
 
+    public static boolean validateBlank(String input) {
+        return input.contains(" ");
+    }
 }
