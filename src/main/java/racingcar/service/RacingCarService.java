4397323135316ed42942b7c327b0c8a17d618bca
qyinm <qusseun@gmail.com>
2024-10-24 21:02:23 +0900

    feat(RacingCarService): RacingCar 우승자 추출 기능 작성

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -1,7 +1,28 @@
 package racingcar.service;
 
+import java.util.Comparator;
+import java.util.List;
+import racingcar.car.RacingCar;
+
 public class RacingCarService {
 
+    private List<RacingCar> racingCars;
+
     public RacingCarService() {
-    }ㅣ
+    }
+
+    public List<RacingCar> logic(List<String> carNames, int moveCount) {
+
+        return racingCars;
+    }
+
+
+    public List<RacingCar> getWinnerRacingCars(List<RacingCar> racingCars) {
+        racingCars.sort(Comparator.comparingInt(RacingCar::getDistance).reversed());
+
+        int maxDistance = racingCars.getFirst().getDistance();
+        List<RacingCar> winners = racingCars.stream().filter(car -> car.getDistance() == maxDistance).toList();
+
+        return winners;
+    }
 }
