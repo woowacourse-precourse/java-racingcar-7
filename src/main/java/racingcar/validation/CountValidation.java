dc5547a61ca10401fd5a77f01e2a05f94e99317e
garden14 <ci667@dgu.ac.kr>
2024-10-24 20:28:50 +0900

    feat(CountValidation): 횟수 입력 예외처리

---
+++ src/main/java/racingcar/validation/CountValidation.java
@@ -0,0 +1,12 @@
+package racingcar.validation;
+
+public class CountValidation {
+    public int convertToNaturalNum(String stringCount){
+        try {
+            int count = Integer.parseInt(stringCount);
+            return count;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력한 횟수가 자연수가 아닙니다.");
+        }
+    }
+}
