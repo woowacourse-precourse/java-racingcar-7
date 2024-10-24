79aaeaf8a3051dcc0397c9f58fd83b193fab196b
wogus <ind07152@naver.com>
2024-10-24 22:44:35 +0900

    feat(Validator): 자동차 이름 관련 검증 메서드 추가
    자동차 이름 관련 검증 로직을 한번에 할 수 있는 메서드를 추가함.

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -5,7 +5,13 @@ import java.util.List;
 import java.util.Set;
 
 public final class Validator {
-    public static void nameLengthValidate(List<String> names) {
+    public static void namesValidate(List<String> names) {
+        nameLengthValidate(names);
+        nameDuplicateValidate(names);
+        nameCountValidate(names);
+    }
+
+    private static void nameLengthValidate(List<String> names) {
         names.forEach((name) -> {
             if (name.length() > 5) {
                 throw new IllegalArgumentException();
@@ -13,13 +19,13 @@ public final class Validator {
         });
     }
 
-    public static void nameCountValidate(List<String> names) {
+    private static void nameCountValidate(List<String> names) {
         if (names.size() < 2) {
             throw new IllegalArgumentException();
         }
     }
 
-    public static void nameDuplicateValidate(List<String> names) {
+    private static void nameDuplicateValidate(List<String> names) {
         Set<String> checkDuplicate = new HashSet<>();
         names.forEach(name -> {
             if (!checkDuplicate.add(name)) {
