592daaa8a5429ae9b4b7619e9fd90f1911c2bcfe
Jhzlo <kjh0010703@naver.com>
2024-10-25 01:03:45 +0900

    feat(controller, view): 자동차 입력 문구를 출력한다.

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,22 @@
+package racingcar.controller;
+
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RacingController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        InputCarsName();
+    }
+
+    private void InputCarsName() {
+        inputView.inputCarNamesMessage();
+    }
+}
