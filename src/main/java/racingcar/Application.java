11229b5474a20c89b4aac473994387a655103305
Chaewon <jamjam12@ajou.ac.kr>
2024-10-24 16:36:43 +0900

    feat(input): add initial user input prompts for car race and attempt count

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,6 +10,12 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int N = Integer.parseInt(Console.readLine());
+
 
     }
 }
