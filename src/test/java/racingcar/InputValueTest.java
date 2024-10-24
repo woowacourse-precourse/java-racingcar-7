825b9703447fa15720151d30ba65ad67546273a0
kaswhy <sxshx818@naver.com>
2024-10-24 20:01:57 +0900

    test(InputValueTest): 자동차 이름이 빈 값이면 예외 발생시키는 테스트 구현
    자동차 이름이 빈 값인 것을 검증하면, 빈 값이 될 수 없다는 에러 메시지를 띄우며 예외 발생

---
+++ src/test/java/racingcar/InputValueTest.java
@@ -0,0 +1,35 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+public class InputValueTest {
+    private static final String CAR_NAME_BLANk = "";
+
+    @DisplayName("자동차 이름이 빈 값이면 예외 발생")
+    @Test
+    void ThrowExceptionIfCarNameBlank() {
+        // given
+        String carName = CAR_NAME_BLANk;
+        ValidateCarName validateCarName = new valicateCarName();
+
+        // when
+        Throwable thrown = catchThrowable(() -> {
+            validateCarName.isBlank(carName);
+        });
+
+        //then
+        assertThat(thrown)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 빈 값이 될 수 없습니다.");
+    }
+
+}
