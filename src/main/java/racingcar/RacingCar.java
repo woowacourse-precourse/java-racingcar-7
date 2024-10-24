f1e18cfd5f4640567cad16b48892ca55ce98ed94
J0YERIM <chosarah70@gmail.com>
2024-10-24 22:41:33 +0900

    refactor: 입력 기능을 InputView로 분리
    단일 책임을 위해 자동차 이름 입력과 시도 횟수 입력을 InputView 클래스로 이동
    ","를 기준으로 한 split 로직은 RacingCar 클래스에 유지

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -7,12 +7,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import racingcar.view.InputView;
 
 public class RacingCar {
 
     public void run() {
-        String[] carNames = inputCarName();
-        int numberOfAttempts = inputNumberOfAttempts();
+        String[] carNames = InputView.getCarNames().split(",");
+        int numberOfAttempts = InputView.getNumberOfAttempts();
 
         Map<String, Integer> cars = createCar(carNames);
 
@@ -47,18 +48,6 @@ public class RacingCar {
             winners.add(car.getKey());
         }
         return winners;
-    }
-
-    private String[] inputCarName() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String nameInput = Console.readLine();
-        return nameInput.split(",");
-    }
-
-    private int inputNumberOfAttempts() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String input = Console.readLine();
-        return Integer.parseInt(input);
     }
 
     private boolean canMove() {
