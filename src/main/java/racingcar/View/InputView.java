efd8d3504a69d7e83cbf4a9901d766f8ac88ed78
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-10-24 14:36:10 +0900

    feat: Add car name input feature

---
+++ src/main/java/racingcar/View/InputView.java
@@ -0,0 +1,15 @@
+package racingcar.View;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.Controller.ForwardController;
+
+import java.util.List;
+
+public class InputView {
+    public static List<ForwardController> getCars(){
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+
+    }
+}
