db654032780cbbfe4ec31bd226345cc704475041
minzzun99 <skg06123@naver.com>
2024-10-24 19:48:52 +0900

    feat(main): 자동차 경주 실행을 위한 로직 추가
    - Race와 RacingController 객체 생성
    - RacingController의 run()을 통해 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.domain.Race;
+
 public class Application {
+    private static final Race race = new Race();
+    private static final RacingController racingController = new RacingController(race);
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        racingController.run();
     }
 }
