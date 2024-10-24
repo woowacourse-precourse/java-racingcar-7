dbc8727ba30e5cba5d3a93ff7af7c5eeae1332c6
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-10-24 19:37:57 +0900

    refactor: 일급 컬렉션인 cars에서 position을 통해 우승자를 반환하도록 변경

--- src/main/java/racingcar/domain/car/Cars.java
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -31,4 +31,16 @@ public class Cars {
     public List<Car> getCars() {
         return cars;
     }
+
+    public List<String> winners() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .toList();
+    }
 }
