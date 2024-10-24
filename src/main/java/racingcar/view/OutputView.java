df76c9e5301e83bf942899da0cf243054fc9dbae
joungeun0825 <joungeun0825@gmail.com>
2024-10-24 20:35:07 +0900

    refactor(outputView): 자동차 이동 결과 출력 함수 분리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,24 +6,37 @@ import racingcar.model.Cars;
 import java.util.List;
 
 public class OutputView {
+    private static final String RACE_RESULT = "실행 결과";
+    private static final String FINAL_WINNER = "최종 우승자 : ";
+    private static final String CAR_MOVEMENT_DELIMITER = " : "; // 차량 출력 구분자
+    private static final String MOVEMENT_SYMBOL = "-";
 
     public void printMoveResultMessage() {
-        System.out.println("실행 결과");
+        System.out.println(RACE_RESULT);
     }
 
     public void printMovedCars(Cars cars) {
         for (Car car : cars.getCars()) {
-            System.out.print(car.getName() + " : ");
-            for (int i = 0; i < car.getMovedNumber(); i++) {
-                System.out.print("-");
-            }
-            System.out.println();
+            printCarMovement(car);
         }
+        System.out.println();
+    }
+
+    private void printCarMovement(Car car) {
+        String movement = generateCarMovementString(car);
+        System.out.println(movement);
+    }
+
+    private String generateCarMovementString(Car car){
+        StringBuilder movement = new StringBuilder(car.getName() + CAR_MOVEMENT_DELIMITER);
+        for (int i = 0; i < car.getMovedNumber(); i++) {
+            movement.append(MOVEMENT_SYMBOL);
+        }
+        return movement.toString();
     }
 
     public void printWinner(List<String> winners) {
-        System.out.print("최종 우승자 : ");
         String winnerNames = String.join(", ", winners);
-        System.out.println(winnerNames);
+        System.out.println(FINAL_WINNER + winnerNames);
     }
 }
