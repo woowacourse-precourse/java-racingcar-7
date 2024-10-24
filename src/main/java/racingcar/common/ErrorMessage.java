9eaab95bec6a6fc8f0e6995e350064f39a4de966
kgy1008 <gayeon1008@g.skku.edu>
2024-10-24 18:26:28 +0900

    feat: add error messages

--- src/main/java/racingcar/common/ErrorMessage.java
+++ src/main/java/racingcar/common/ErrorMessage.java
@@ -3,7 +3,8 @@ package racingcar.common;
 public enum ErrorMessage {
     NAME_LENGTH_ERROR("자동차 이름은 이름은 5자 이하만 가능합니다."),
     INVALID_TRY_COUNT("시도 횟수를 정확한 숫자로 입력해주세요."),
-    INVALID_RANDOM_NUMBER("유요하지 않은 값입니다.");
+    EMPTY_CAR_LIST("자동차 목록이 비어 있습니다."),
+    CAN_NOT_CALCULATE_DISTANCE("최대 이동 거리를 계산할 수 없습니다.");
 
     private final String message;
 
