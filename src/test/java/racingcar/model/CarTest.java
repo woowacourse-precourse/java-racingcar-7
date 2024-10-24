7780e50d80e82322392669c69c4816d5a22d3a6b
Leegeonwoo7 <workgeon07@naver.com>
2024-10-25 00:49:08 +0900

    test(CarTest): Car클래스 테스트 구현
    - winningPoint 초기화 테스트
    - moveForward()동작 테스트

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,8 +1,35 @@
+package racingcar.model;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
+
 class CarTest {
-  
+
+    @Test
+    @DisplayName("Car인스턴스 생성 시점에 winningPoint는 0점이다")
+    void whenConstructCarWinningPointZero() {
+        //given //when
+        Car car = new Car("pobi");
+
+        //then
+        assertThat(car.getWinningPoint()).isZero();
+    }
+
+    @Test
+    @DisplayName("자동차가 3번 전진하면 winningPoint는 3이어야한다")
+    void validCarWinningPoint() {
+        //given
+        Car car = new Car("pobi");
+
+        //when
+        car.moveForward();
+        car.moveForward();
+        car.moveForward();
+
+        //then
+        assertThat(car.getWinningPoint()).isSameAs(3);
+    }
 }
