38d89cab00ceaee6044049b83dfd5ae9d6fc2b0f
joshinhyeok <shtree07@naver.com>
2024-10-24 22:52:06 +0900

    feat: 레이싱 진행과 출력이 분리되도록 반복문 제거

--- src/test/java/racingcar/RacingGame.java
+++ src/test/java/racingcar/RacingGame.java
@@ -3,19 +3,16 @@ package racingcar;
 import java.util.List;
 
 public class RacingGame {
-    void playRace(List<Car> cars, int attemptCount) {
-        for (int i = 0; i < attemptCount; i++) {
-            for (Car car : cars) {
-                car.goForward();
-            }
+    void playRace(List<Car> cars) {
+        for (Car car : cars) {
+            car.goForward();
         }
     }
 
-    void playRace(List<Car> cars, int attemptCount, int fixedNumber) {
-        for (int i = 0; i < attemptCount; i++) {
-            for (Car car : cars) {
-                car.goForward(fixedNumber);
-            }
+    void playRace(List<Car> cars, int fixedNumber) {
+        for (Car car : cars) {
+            car.goForward(fixedNumber);
         }
     }
 }
+
--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -10,13 +10,15 @@ import org.junit.jupiter.api.Test;
 public class RacingGameTest {
 
     @Test
-    void 주어진_횟수_만큼_전진_조건을_실행하는지_테스트() {
+    void 자동차들이_전진_조건을_실행하는지_테스트() {
         RacingGame racingGame = new RacingGame();
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("pobi"));
+        cars.add(new Car("woni"));
+        cars.add(new Car("jun"));
         int attemptCount = 3;
 
-        racingGame.playRace(cars, attemptCount, 4);
+        racingGame.playRace(cars, 4);
 
         assertThat(cars.get(0).getPosition()).isEqualTo(3);
     }
