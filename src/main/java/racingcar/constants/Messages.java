595e7965a6949e3d2b6055e00791de9d40d2207a
suhyun9764 <suhyun9764@naver.com>
2024-10-25 02:12:14 +0900

    feat(OutputHandler): 입력 안내 메세지 출력 구현
    - 입력 안내 메세지 메서드 구현
    - Messages를 생성하여 static 변수로 메세지 관리

---
+++ src/main/java/racingcar/constants/Messages.java
@@ -0,0 +1,6 @@
+package racingcar.constants;
+
+public class Messages {
+    public static String CARS_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static String TOTAL_ROUNDS_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+}
---
+++ src/main/java/racingcar/constants/OutputHandler.java
@@ -0,0 +1,15 @@
+package racingcar.constants;
+
+import static racingcar.constants.Messages.CARS_NAME_INPUT_MESSAGE;
+import static racingcar.constants.Messages.TOTAL_ROUNDS_INPUT_MESSAGE;
+
+public class OutputHandler {
+
+    public static void displayCarsNameInputMessage() {
+        System.out.println(CARS_NAME_INPUT_MESSAGE);
+    }
+
+    public static void displayTotalRoundsInputMessage(){
+        System.out.println(TOTAL_ROUNDS_INPUT_MESSAGE);
+    }
+}
