5d19b8aa7a8320e5ccb398a6030a9a9ec30d33e3
eb9862 <luclead622@gmail.com>
2024-10-24 21:56:14 +0900

    feat : RacingCar 클래스 구현
    - 무작위 값을 생성하고 전진 조건을 확인하는 메서드 구현
    - 전진 조건에 부합한다면 전진 횟수를 증가시키는 메서드 구현
    - 생성자 구현

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RacingCar {
+
+    private final String name;
+    private int forwardCount;
+
+    public RacingCar(String name) {
+        this.name = name;
+    }
+
+    void move() {
+        int pickedNumber = Randoms.pickNumberInRange(0, 9);
+        if (canMoveForward(pickedNumber)) {
+            forwardCount++;
+        }
+    }
+
+    static boolean canMoveForward(int pickedNumber) {
+        return pickedNumber >= 4;
+    }
+}
