f93d89e54ecaa61405d3fc0170067cc0bae1475a
taek2222 <oot309@daum.net>
2024-10-24 13:19:49 +0900

    refactor(Car): 이름 길이 검증 이동 및 캡슐화
    - 오직 해당 객체에서만 사용해, 이름 길이 검증 로직을 내부로 옮겼습니다.
    - 객체 생성시 검증 로직이 활성화 되도록 개선했습니다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,11 +1,20 @@
 package racingcar.domain;
 
+import static racingcar.util.InputValidator.validateNotBlank;
+
 public class Car {
     private final String name;
     private final Integer distance;
 
     public Car(String name, Integer distance) {
+        validateNotBlank(name);
         this.name = name;
         this.distance = distance;
+        validateCarNameLength();
+    }
+
+    private void validateCarNameLength() {
+        if (this.name.length() > 5)
+            throw new IllegalArgumentException();
     }
 }
