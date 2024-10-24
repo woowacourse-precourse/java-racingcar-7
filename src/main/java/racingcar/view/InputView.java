d1a8ed9e8147af42ff636e3267a7fa5708b7d30b
Hanjjong <tim4974@naver.com>
2024-10-24 16:16:25 +0900

    feat(InputView): 게임 횟수 입력 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -15,4 +15,13 @@ public class InputView {
     private void printCarNamesInputMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
+
+    public String gameRoundInput() {
+        printGameRoundInputMessage();
+        return Console.readLine();
+    }
+
+    private void printGameRoundInputMessage() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
 }
