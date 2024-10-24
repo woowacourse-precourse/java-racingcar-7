1f6fa9001639392e1e3e6244336f545acd74d6c3
yeong-hwan <jghff700@naver.com>
2024-10-25 07:28:22 +0900

    feat(service): append winner finder by position

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -29,4 +29,16 @@ public class RacingGameService {
             }
         }
     }
+
+    public List<String> getWinners(List<Car> cars) {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
 }
