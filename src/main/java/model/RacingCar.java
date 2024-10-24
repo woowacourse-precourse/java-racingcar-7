55a596ad9aeca823a06b62dd64ef0cbd7451b653
gikhoon <ghikhoon@naver.com>
2024-10-24 23:27:34 +0900

    feat(racingCar): 레이싱 차량 생성자 구현

--- src/main/java/model/RacingCar.java
+++ src/main/java/model/RacingCar.java
@@ -1,4 +1,11 @@
 package model;
 
 public class RacingCar {
+    private int name;
+    private long distance;
+
+    public RacingCar(int name) {
+        this.name = name;
+        this.distance = 0;
+    }
 }
