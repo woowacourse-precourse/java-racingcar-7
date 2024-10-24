14ff47bb8b76121261959d4460d7bfb89d67bcba
greensnapback0229 <smdmim@gmail.com>
2024-10-25 04:14:51 +0900

    feat(Input): add input car names from user

---
+++ src/main/java/racingcar/view/Input.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.util.Validation;
+
+public class Input {
+
+    private Validation validation = new Validation();
+
+    public List<String> inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+        return validation.validateCarNameInput(input);
+    }
+}
