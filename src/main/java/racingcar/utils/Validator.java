f75201629b65ab4a8effcc2bc0bb72167200f824
sc@t0a14 <gusdn6671@gmail.com>
2024-10-24 22:02:11 +0900

    feat(Validator): 자동차의 이름이 5글자를 초과할경우 예외가 발생한다.

---
+++ src/main/java/racingcar/utils/Validator.java
@@ -0,0 +1,16 @@
+package racingcar.utils;
+
+public class Validator {
+    public static void isEmpty(String name) {
+        if(name == null || name.trim().isBlank()){
+            throw new IllegalArgumentException("자동차 이름에 빈칸은 입력할수 없습니다");
+        }
+    }
+
+    public static void isNameRange(String name){
+        if(name.length() >= 5){
+            throw new IllegalArgumentException("자동차의 이름은 5글자 이하만 가능합니다");
+        }
+    }
+
+}
