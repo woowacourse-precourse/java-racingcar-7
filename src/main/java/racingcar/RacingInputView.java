e16ed992edb3818d65e96ceda42536bb01927703
soojinlee <sojinlee1004@naver.com>
2024-10-24 22:29:57 +0900

    docs(README) : 자동차 경주 구현 기능 목록 입력 문자 처리 기능 추가

--- src/main/java/racingcar/RacingInputView.java
+++ src/main/java/racingcar/RacingInputView.java
@@ -7,7 +7,6 @@ public class RacingInputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
-
     public int RacingRoundCount() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Integer.parseInt(Console.readLine());
