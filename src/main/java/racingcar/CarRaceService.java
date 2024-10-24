1349c29e8181ebdef1f6a89e8c469bae325bfe19
Jo Myeong-geun <happy63590@gmail.com>
2024-10-25 04:43:03 +0900

    feat: 임의의 값이 4이상이면 자동차가 전진하는 기능 구현

--- src/main/java/racingcar/CarRaceService.java
+++ src/main/java/racingcar/CarRaceService.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.stream.IntStream;
+import racingcar.domain.Car;
 import racingcar.domain.CarRace;
 
 public class CarRaceService {
@@ -9,6 +11,17 @@ public class CarRaceService {
 
     public void init(String carNames) {
         carRace = new CarRace(carNames);
+    }
+
+    public void start(int attempts) {
+        IntStream.range(0, attempts)
+                .forEach(attempt -> moveCars());
+    }
+
+    public void moveCars() {
+        carRace.getCars().stream()
+                .filter(car -> canMove())
+                .forEach(Car::move);
     }
 
     private boolean canMove() {
