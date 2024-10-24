1dcc3c91264da7270a258067cd744f849151db94
taek2222 <oot309@daum.net>
2024-10-24 12:40:12 +0900

    refactor(InputView): Blank 검증 삭제 및 변환
    - 입력 검증 클래스를 활용해 Blank 검증을 재사용하도록 변경했습니다.
    - 재사용성을 위해 따로 정의한 검증 메소드로 기존은 삭제했습니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import static racingcar.util.InputValidator.validateNotBlank;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
@@ -8,7 +10,7 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
 
-        validateNull(input);
+        validateNotBlank(input);
         validateSingleComma(input);
 
         return input;
@@ -19,16 +21,10 @@ public class InputView {
         String input = Console.readLine();
         int inputNumber = Integer.parseInt(input);
 
-        validateNull(input);
+        validateNotBlank(input);
         validatePlayCount(inputNumber);
 
         return inputNumber;
-    }
-
-    private void validateNull(String input) {
-        if (input.isBlank()) {
-            throw new IllegalArgumentException();
-        }
     }
 
     private void validateSingleComma(String input) {
