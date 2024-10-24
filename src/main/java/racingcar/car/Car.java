19595c1b5436017364f57122646de1d7c596eafc
minjun <ijmtylhtf@naver.com>
2024-10-24 09:46:17 +0900

    feat : 자동차 위치 변수 추가

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -3,9 +3,12 @@ package racingcar.car;
 public class Car {
 	private final String name;
 
+	private long location;
+
 	public Car(String name) {
 		validateCarLength(name);
 		this.name = name;
+		this.location = 0;
 	}
 
 	private static void validateCarLength(String carName) {
