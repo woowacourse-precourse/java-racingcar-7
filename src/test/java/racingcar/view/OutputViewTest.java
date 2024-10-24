c22a5953853c34687f88cc1cbae1472dc5261dc2
qudcks8084 <qudcks8084@naver.com>
2024-10-24 16:37:35 +0900

    Test : Add 1 test case to check printWinnerOfRacing()
    - To check it can pring multiple winner

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import racingcar.dto.RacingCarDTO;
 
 import static racingcar.view.OutputView.printEachDistanceByRandomMovement;
+import static racingcar.view.OutputView.printWinnerOfRacing;
 
 class OutputViewTest {
 
@@ -16,6 +17,18 @@ class OutputViewTest {
         racingCarDTO.updateRacingCarDistance("park", 10);
 
         printEachDistanceByRandomMovement(racingCarDTO);
+    }
+
+    @Test
+    @DisplayName("DTO에 의한 최종 출력 결과를 확인")
+    void checKWinnerPrint(){
+        //given
+        RacingCarDTO racingCarDTO = new RacingCarDTO("park,kim,lee", "1");
+        racingCarDTO.updateRacingCarDistance("park", 10);
+        racingCarDTO.updateRacingCarDistance("lee", 10);
+        racingCarDTO.updateRacingCarDistance("kim", 5);
+
+        printWinnerOfRacing(racingCarDTO);
     }
 
 }
