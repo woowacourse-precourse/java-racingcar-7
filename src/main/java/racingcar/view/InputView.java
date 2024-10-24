7ffee5a24df931b9f54b7ce9df522532cdd3ab2f
hmlssghm <kwonse326@gmail.com>
2024-10-24 15:12:14 +0900

    feat(InputView): 입력 받는 메서드 생성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,9 @@
 package racingcar.view;
 
-public class InputView {
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
+public class InputView {
+    public static String enter() {
+        return readLine();
+    }
 }
