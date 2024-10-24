391bf9eb8e91978023ff654274f41e3f32371a03
wogus <ind07152@naver.com>
2024-10-24 22:42:13 +0900

    feat(Validator): 자동차 이름 중복 검증 로직 추가
    자동차 이름들은 중복이 있으면 안된다.

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -1,6 +1,8 @@
 package racingcar.validator;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public final class Validator {
     public static void nameLengthValidate(List<String> names) {
@@ -15,5 +17,14 @@ public final class Validator {
         if (names.size() < 2) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public static void nameDuplicateValidate(List<String> names) {
+        Set<String> checkDuplicate = new HashSet<>();
+        names.forEach(name -> {
+            if (!checkDuplicate.add(name)) {
+                throw new IllegalArgumentException();
+            }
+        });
     }
 }
