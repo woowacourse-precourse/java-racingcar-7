9c35f22300c924aa0db325aca2eb39b7e1726a8d
suhyun9764 <suhyun9764@naver.com>
2024-10-25 02:09:26 +0900

    feat(Race): 우승자 반환하기 구현
    - 우승자들의 이름을 List로 반환하기 구현
    - 테스트 코드 작성

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -16,9 +16,28 @@ public class Race {
                 .toList();
     }
 
-    public void start(){
+    public void start() {
         cars.forEach(car -> car.moveOrStop(movementDecider));
     }
 
+    public List<String> getFinalWinners() {
+        List<Car> sortedCars = sortByPositionDescending();
+        int winnerPosition = sortedCars.getFirst().getCurrentPosition();
+        List<String> winners = findWinners(winnerPosition);
+        return winners;
+    }
+
+    private List<Car> sortByPositionDescending() {
+        List<Car> cars = this.cars.stream().sorted((a, b) -> {
+            return b.getCurrentPosition() - a.getCurrentPosition();
+        }).toList();
+        return cars;
+    }
+
+    private List<String> findWinners(int winnerPosition) {
+        return cars.stream().filter(car -> {
+            return car.getCurrentPosition() == winnerPosition;
+        }).map(car -> car.getName()).toList();
+    }
 
 }
--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -1,16 +1,21 @@
 package racingcar;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class RaceTest {
     private List<Car> cars = new ArrayList<>();
@@ -25,7 +30,7 @@ class RaceTest {
 
     @DisplayName("각 회차 경기 결과 정상 반환")
     @Test
-    void getRoundResult_return_success(){
+    void getRoundResult_return_success() {
         //given
         List<Car> testCars = cars;
         race = new Race(testCars);
@@ -42,7 +47,7 @@ class RaceTest {
 
     @DisplayName("각 회차 경기 정상 작동")
     @Test
-    void start_success(){
+    void start_success() {
         assertRandomNumberInRangeTest(() -> {
                     //given
                     List<Car> testCars = cars;
@@ -50,13 +55,44 @@ class RaceTest {
                     //when
                     race.start();
                     Queue<Integer> expectedPosition = new LinkedList<>();
-                    expectedPosition.addAll(Arrays.asList(1,0,1));
+                    expectedPosition.addAll(Arrays.asList(1, 0, 1));
                     //then
                     List<CarStatus> roundResult = race.getRoundResult();
                     roundResult.stream().forEach(carStatus -> {
                         Assertions.assertThat(carStatus.getCurrentPosition()).isEqualTo(expectedPosition.poll());
                     });
                 }
-                , 4,3,4);
+                , 4, 3, 4);
     }
+
+    static Stream<Arguments> provideAdditionData() {
+        return Stream.of(
+                arguments(new int[]{4, 1, 7, 8}, new int[]{1, 2, 9, 4}, new int[]{5, 2, 8, 7})
+        );
+    }
+
+    @DisplayName("최종 우승자 출력")
+    @ParameterizedTest
+    @MethodSource("provideAdditionData")
+    void getWinners_test(int[] car1Numbers, int[] car2Numbers, int[] car3Numbers) {
+        //given
+        List<Car> testCars = cars;
+        race = new Race(testCars);
+
+        //when
+        List<String> expectedWinners = Arrays.asList("car1", "car3");
+
+        // 각 차의 번호를 각 반복마다 변경하며 레이스 실행
+        for (int i = 0; i < 4; i++) {
+            assertRandomNumberInRangeTest(() -> {
+                race.start();
+            }, car1Numbers[i], car2Numbers[i], car3Numbers[i]);
+        }
+
+        // then
+        List<String> finalWinners = race.getFinalWinners();
+        Assertions.assertThat(finalWinners).isEqualTo(expectedWinners);
+    }
+
 }
+
