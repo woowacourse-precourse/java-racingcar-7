cd387a2f5a456be1dcbb727ffed88ff4a56e65e3
DK <laancer4@gmail.com>
2024-10-24 18:08:11 +0900

    refactor : RandomMovingStrategy 의 매직넘버를 상수처리

--- src/main/java/racingcar/car/RandomMovingStrategy.java
+++ src/main/java/racingcar/car/RandomMovingStrategy.java
@@ -3,9 +3,13 @@ package racingcar.car;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomMovingStrategy implements MovingStrategy {
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
+    private static final int MOVABLE_THRESHOLD = 4;
+
     @Override
     public boolean movable() {
-        int randomNumber = Randoms.pickNumberInRange(0, 9);
-        return randomNumber >= 4;
+        int randomNumber = Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+        return randomNumber >= MOVABLE_THRESHOLD;
     }
 }
