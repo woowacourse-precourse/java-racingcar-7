7cd63f24625609a7b6cf87fb49f3ac15d62b9be9
fpem3309 <fpem3309@gmail.com>
2024-10-24 15:57:27 +0900

    feat: Vehicle class 생성, 자동자 이름 구분 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,9 +2,16 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputVehicles = Console.readLine();
+
+        List<Vehicle> vehicles = Arrays.stream(inputVehicles.split(","))
+                .map(Vehicle::new)
+                .toList();
     }
 }
---
+++ src/main/java/racingcar/Vehicle.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class Vehicle {
+    private String vehicleName;
+    private int victoryCount;
+
+    public Vehicle(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+}
