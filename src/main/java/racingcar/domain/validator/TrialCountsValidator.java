e5072377c5513bfa8237c61123dea5ba6bba6b69
bowook <mbosx316@naver.com>
2024-10-24 21:45:50 +0900

    feat(TrialCountsValidator) : 예외 메시지 기능 추가
    - 예외 발생의 명확한 이유를 보여주는 메시지 출력 기능

--- src/main/java/racingcar/domain/validator/TrialCountsValidator.java
+++ src/main/java/racingcar/domain/validator/TrialCountsValidator.java
@@ -2,9 +2,10 @@ package racingcar.domain.validator;
 
 public class TrialCountsValidator {
 
-    public void validate(String trialCounts) {
+    public int validate(String trialCounts) {
         int counts = validateIsNumber(trialCounts);
         validateIsZero(counts);
+        return counts;
     }
 
     private int validateIsNumber(String trialCounts) {
@@ -12,14 +13,14 @@ public class TrialCountsValidator {
         try {
             tempCounts = Integer.parseInt(trialCounts);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("입력한 값은 숫자가 아닙니다 : " + trialCounts);
         }
         return tempCounts;
     }
 
     private void validateIsZero(int counts) {
         if(counts <= 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("입력한 값은 0보다 커야 합니다 : " + counts);
         }
     }
 }
