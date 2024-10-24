48e2616ccdaeb607e91a8b06eb243a58f3142dc0
sunjong0214 <rlatjswhd8200@gmail.com>
2024-10-24 15:26:40 +0900

    refactor(CarRacing): forwardCarByRandomNumber 메서드 분리
    랜덤한 수를 할당받아서 앞으로 움직이는 로직 메서드로 분리

--- src/main/java/racingcar/CarRacing.java
+++ src/main/java/racingcar/CarRacing.java
@@ -8,17 +8,21 @@ public class CarRacing {
     public static List<RacingCar> start(RacingCarTrace racingCarTrace) {
         var tryCount = Prompt.racingTryCount();
         System.out.println("실행 결과");
-        IntStream.range(0, tryCount).forEach(i -> {
-                    IntStream.range(0, racingCarTrace.size()).forEach(index -> {
-                        var racingCar = racingCarTrace.getRacingCar(index);
-                        var randomNumber = Randoms.pickNumberInRange(0, 9);
-                        if (randomNumber >= 4) {
-                            racingCarTrace.forward(racingCar);
-                        }
-                    });
+        IntStream.range(0, tryCount).forEach(index -> {
+                    forwardCarByRandomNumber(racingCarTrace);
                     Prompt.tracePrint(racingCarTrace);
                 }
         );
         return racingCarTrace.getBiggestDistanceRacingCar();
+    }
+
+    public static void forwardCarByRandomNumber(RacingCarTrace racingCarTrace) {
+        IntStream.range(0, racingCarTrace.size()).forEach(index -> {
+            var racingCar = racingCarTrace.getRacingCar(index);
+            var randomNumber = Randoms.pickNumberInRange(0, 9);
+            if (randomNumber >= 4) {
+                racingCarTrace.forward(racingCar);
+            }
+        });
     }
 }
