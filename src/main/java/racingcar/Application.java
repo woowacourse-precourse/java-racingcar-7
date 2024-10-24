b4107573978b9d2bf213648d589672dcb09f0090
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-24 14:06:42 +0900

    refactor : ApplicationController 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,19 @@
 package racingcar;
 
-import racingcar.controller.ApplicationController;
+import racingcar.controller.RacingController;
+import racingcar.dto.InputDTO;
+import racingcar.dto.OutputDTO;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Application {
 
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
     public static void main(String[] args) {
-        ApplicationController.run(new InputView(), new OutputView());
+        InputDTO inputDTO = inputView.display();
+        OutputDTO outputDTO = RacingController.execute(inputDTO);
+        outputView.display(inputDTO.getTotalTrialCount(), outputDTO);
     }
 }
