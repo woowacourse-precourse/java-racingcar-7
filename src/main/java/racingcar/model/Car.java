047973afd7fe244aef697aeff7bf6795e915f7be
sanghee0820 <lsh277604@gmail.com>
2024-10-24 22:57:28 +0900

    feat: 차량 생성 시 제약사항 확인 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,11 +1,24 @@
 package racingcar.model;
 
+import racingcar.exception.CarException;
+
 public class Car {
     private final String name;
     private int position;
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
         this.position = 0;
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException(CarException.CAR_NAME_IS_NULL_OR_EMPTY.getMessage());
+        }
+
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(CarException.CAR_NAME_IS_OVER_FIVE.getMessage());
+        }
     }
 }
