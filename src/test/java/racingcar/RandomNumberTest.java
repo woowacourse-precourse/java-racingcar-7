8584f0eae0372f2b4c5953a68aba73640f957c47
GaBaljaintheroom <junsu1222@naver.com>
2024-10-25 02:25:11 +0900

    test(RandomNumberTest): 랜덤 숫자 반환 예외 처리 테스트

--- src/test/java/racingcar/RandomNumberTest.java
+++ src/test/java/racingcar/RandomNumberTest.java
@@ -1,8 +1,9 @@
 package racingcar;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
 
 class RandomNumberTest {
 
@@ -13,10 +14,45 @@ class RandomNumberTest {
         int end = 9;
 
         //when
-        int actual = RandomNumber.generateInRange(0, 9);
+        int actual = RandomNumber.generateInRange(start, end);
 
         //then
         assertThat(actual).isBetween(start, end);
     }
 
+    @Test
+    void 시작_값과_마지막_값이_같으면_예외가_발생한다() {
+        //given
+        int start = 10;
+        int end = 10;
+
+        //when & then
+        assertThatThrownBy(() -> RandomNumber.generateInRange(start, end))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("startInclusive cannot be greater than endInclusive.");
+    }
+
+    @Test
+    void 범위_내에_최댓값이_존재하면_예외가_발생한다() {
+        //given
+        int start = 0;
+        int end = Integer.MAX_VALUE;
+
+        //when & then
+        assertThatThrownBy(() -> RandomNumber.generateInRange(start, end))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("endInclusive cannot be greater than Integer.MAX_VALUE.");
+    }
+
+    @Test
+    void 최대_범위_이상이면_예외가_발생한다() {
+        //given
+        int start = -1;
+        int end = Integer.MAX_VALUE - 1;
+
+        //when & then
+        assertThatThrownBy(() -> RandomNumber.generateInRange(start, end))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("the input range is too large.");
+    }
 }
