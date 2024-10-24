7318789ead47a8479cdec2f0ec6e4f80dd44bcdd
tentenball <lth8905@gmail.com>
2024-10-25 01:38:42 +0900

    feat: 에러메시지 추가
    * RacingGameExceptionMessage: 공백입력 에러메시지 추가

--- src/main/java/racingcar/exception/racingGame/RacingGameExceptionMessage.java
+++ src/main/java/racingcar/exception/racingGame/RacingGameExceptionMessage.java
@@ -2,7 +2,8 @@ package racingcar.exception.racingGame;
 
 public enum RacingGameExceptionMessage {
 	NOT_NUMBER("숫자가 아닙니다."),
-	OUT_OF_BOUND("0보다 큰 숫자만 입력가능합니다.");
+	OUT_OF_BOUND("0보다 큰 숫자만 입력가능합니다."),
+	NOT_BLANK("공백은 입력할 수 없습니다.");
 
 	private final String message;
 
