27912b5dea55830a6eee01f419037f730b425ca5
gikhoon <ghikhoon@naver.com>
2024-10-25 00:07:26 +0900

    feat(validator): 레이싱 차량 이름이 없으면 예외 발생

--- src/main/java/exception/ErrorCode.java
+++ src/main/java/exception/ErrorCode.java
@@ -1,4 +1,16 @@
 package exception;
 
 public enum ErrorCode {
+    EMPTY_CAR_NAME("빈 문자열은 차 이름이 될 수 없습니다."),
+    ;
+
+    private final String message;
+
+    ErrorCode(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- src/main/java/validator/Validator.java
+++ src/main/java/validator/Validator.java
@@ -1,8 +1,19 @@
 package validator;
 
+import exception.ErrorCode;
 import java.util.List;
 
 public class Validator {
+    private static final String EMPTY_STRING = "";
+
     public void validateCar(List<String> carNames) {
+        checkCarNotEmpty(carNames);
+    }
+
+    private void checkCarNotEmpty(List<String> carNames) {
+        if (carNames.stream().anyMatch(
+                name -> name.trim().isEmpty())) {
+            throw new IllegalArgumentException(ErrorCode.EMPTY_CAR_NAME.getMessage());
+        }
     }
 }
