c45861426b1401b7017698dc2e5c6f5b1fe9baf5
gikhoon <ghikhoon@naver.com>
2024-10-25 00:20:12 +0900

    feat(validator): 중복하는 차 이름이 있으면 예외 발생

--- src/main/java/exception/ErrorCode.java
+++ src/main/java/exception/ErrorCode.java
@@ -2,6 +2,7 @@ package exception;
 
 public enum ErrorCode {
     EMPTY_CAR_NAME("빈 문자열은 차 이름이 될 수 없습니다."),
+    DUPLICATE_CAR_NAME("중복하는 차 이름이 있습니다.")
     ;
 
     private final String message;
--- src/main/java/validator/Validator.java
+++ src/main/java/validator/Validator.java
@@ -1,18 +1,31 @@
 package validator;
 
 import exception.ErrorCode;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validator {
 
     public void validateCar(List<String> carNames) {
         checkCarNotEmpty(carNames);
+        checkDuplicateName(carNames);
     }
 
     private void checkCarNotEmpty(List<String> carNames) {
         if (carNames.stream().anyMatch(
                 String::isEmpty)) {
             throw new IllegalArgumentException(ErrorCode.EMPTY_CAR_NAME.getMessage());
+        }
+    }
+
+    private void checkDuplicateName(List<String> carNames) {
+        Set<String> uniqueNames = new HashSet<>();
+        for (String name : carNames) {
+            String lowerCaseName = name.toLowerCase();
+            if (!uniqueNames.add(lowerCaseName)) {
+                throw new IllegalArgumentException(ErrorCode.DUPLICATE_CAR_NAME.getMessage());
+            }
         }
     }
 }
