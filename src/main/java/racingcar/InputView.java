e1a70e7f944a0444e6de80db90ba3d0dc4f94d27
임채연 <dlacodus0407@gmail.com>
2024-10-25 00:16:59 +0900

    feat(Input) : create print input message method
    - GameMessages : 유지보수 용이성을 위해, 화면에 출력할 메시지를 클래스로 모읍니다.
    - InputView : 화면에 보여주는 담당을 합니다.
        1. printNameMessage : 자동차 이름 입력요청 메시지를 출력합니다.
        2. printNumberMessage : 시도할 횟수 메시지를 출력합니다.

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class InputView {
+    public void printNameMessage() {
+        System.out.println(GameMessages.INPUT_GUIDE_NAME_MESSAGE);
+    }
+
+    public void printNumberMessage() {
+        System.out.println(GameMessages.INPUT_GUIDE_NUMBER_MESSAGE);
+    }
+}
