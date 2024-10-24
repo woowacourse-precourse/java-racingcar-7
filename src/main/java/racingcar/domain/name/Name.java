ca2f82e31ae44ffdeef3f8c7c210b504e9d96ba3
tentenball <lth8905@gmail.com>
2024-10-24 22:18:49 +0900

    feat: Name에 관련된 클래스 생성
    * Name: 이름을 입력받는 클래스 생성 및 검증로직 작성
    * Names: Name의 list를 저장하는 클래스 생성 및 검증로직 작성

---
+++ src/main/java/racingcar/domain/name/Name.java
@@ -0,0 +1,27 @@
+package racingcar.domain.name;
+
+import racingcar.exception.name.NameException;
+import racingcar.exception.name.NameExceptionMessage;
+
+public class Name {
+	private static final int NAME_LIMIT_LENGTH = 5;
+	private final String name;
+
+	public Name(String name) {
+		validate(name);
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	private void validate(String name) {
+		if (name.isBlank()) {
+			throw new NameException(NameExceptionMessage.NAME_IS_NOT_BLANK);
+		}
+		if (name.length() <= NAME_LIMIT_LENGTH) {
+			throw new NameException(NameExceptionMessage.NAME_TOO_LONG);
+		}
+	}
+}
