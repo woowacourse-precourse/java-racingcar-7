c2d7b09adfa08b49005676d254c989f4411144fc
imjunghun <dlawndgns9@naver.com>
2024-10-25 08:00:38 +0900

    feat(Winners): 우승자를 저장할 Winners 클래스 구현

---
+++ src/main/java/racingcar/domain/Winners.java
@@ -0,0 +1,36 @@
+package racingcar.domain;
+
+import java.util.List;
+import racingcar.domain.car.Car;
+import racingcar.domain.car.Cars;
+
+public class Winners {
+
+    private static final String WINNER_TITLE = "최종 우승자 : ";
+
+    private final List<Name> winners;
+
+    public Winners(Cars cars) {
+        this.winners = findWinnerIn(cars);
+    }
+
+    private List<Name> findWinnerIn(Cars cars) {
+
+        return cars.win().stream()
+                .map(Car::getName)
+                .toList();
+    }
+
+    @Override
+    public String toString() {
+        List<String> names = winners.stream()
+                .map(Name::getName)
+                .toList();
+
+        return format(names);
+    }
+
+    private String format(List<String> names) {
+        return WINNER_TITLE + String.join(", ", names);
+    }
+}
