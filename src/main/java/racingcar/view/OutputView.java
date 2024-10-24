4d18e704c0dd682a68fc25143fbf1072a88b9c82
seaniiio <cnc4750@naver.com>
2024-10-24 21:02:01 +0900

    refactor(OutputView): 상수명 자바 네이밍 컨벤션에 맞게 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,21 +2,21 @@ package racingcar.view;
 
 public class OutputView {
 
-    private static final String carInputMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String countInputMessage = "시도할 횟수는 몇 회인가요?";
-    private static final String resultMessage = "실행 결과";
-    private static final String winnerMessage = "최종 우승자 : ";
+    private static final String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String RESULT_MESSAGE_HEADER = "실행 결과";
+    private static final String WINNER_MESSAGE_PREFIX = "최종 우승자 : ";
 
     public void printCarInputMessage() {
-        System.out.println(carInputMessage);
+        System.out.println(CAR_INPUT_MESSAGE);
     }
 
     public void printCountInputMessage() {
-        System.out.println(countInputMessage);
+        System.out.println(COUNT_INPUT_MESSAGE);
     }
 
     public void printResultMessage() {
-        System.out.println(resultMessage);
+        System.out.println(RESULT_MESSAGE_HEADER);
     }
 
     public void printMessage(String message) {
@@ -24,6 +24,6 @@ public class OutputView {
     }
 
     public void printWinners(String message) {
-        System.out.println(winnerMessage + message);
+        System.out.println(WINNER_MESSAGE_PREFIX + message);
     }
 }
