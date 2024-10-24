1ea2de673f49bf0c304d43a95025d4a143d984e4
kungbi <kipper12335@gmail.com>
2024-10-24 22:19:21 +0900

    test(service): RacingCarService 테스트 수정
    RacingService의 run메소드의 반환 값을 String 에서 String[]으로 변경했습니다. & 테스트 코드 추가

--- src/test/java/racingcar/service/RacingCarServiceTest.java
+++ src/test/java/racingcar/service/RacingCarServiceTest.java
@@ -1,56 +1,62 @@
 package racingcar.service;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.TestUtils;
+import racingcar.util.RandomNumberGenerator;
 
 public class RacingCarServiceTest {
-
-    private RandomNumberGenerator mockRandomNumberGenerator(List<Integer> preDefinedNumbers) {
-        return new RandomNumberGenerator() {
-            private int index = 0;
-
-            @Override
-            public int pickRandomNumberInRange(int start, int end) {
-                int value = preDefinedNumbers.get(index);
-                index = (index + 1) % preDefinedNumbers.size();
-                return value;
-            }
-        };
-    }
 
 
     @Test
     @DisplayName("3개 자동차가 2번 이동한 결과를 확인한다.")
     void runTwoRounds() {
         // Given
-        RandomNumberGenerator randomNumberGenerator = mockRandomNumberGenerator(List.of(4, 3, 2));
+        RandomNumberGenerator randomNumberGenerator = TestUtils.mockRandomNumberGenerator(List.of(4, 3, 2));
         RacingCarService racingCarService = new RacingCarService(randomNumberGenerator);
         String userCarInput = "a,b,c";
         String userRoundInput = "2";
 
         // When
-        String raceResult = racingCarService.run(userCarInput, userRoundInput);
+        String[] raceResult = racingCarService.run(userCarInput, userRoundInput);
 
         // Then
-        Assertions.assertEquals("a", raceResult);
+        assertArrayEquals(new String[]{"a"}, raceResult);
     }
 
     @Test
     @DisplayName("3개 자동차가 5번 이동한 결과를 확인한다.")
     void runFiveRounds() {
         // Given
-        RandomNumberGenerator randomNumberGenerator = mockRandomNumberGenerator(List.of(3, 9, 2));
+        RandomNumberGenerator randomNumberGenerator = TestUtils.mockRandomNumberGenerator(List.of(3, 9, 2));
         RacingCarService racingCarService = new RacingCarService(randomNumberGenerator);
         String userCarInput = "a,b,c";
         String userRoundInput = "5";
 
         // When
-        String raceResult = racingCarService.run(userCarInput, userRoundInput);
+        String[] raceResult = racingCarService.run(userCarInput, userRoundInput);
 
         // Then
-        Assertions.assertEquals("b", raceResult);
+        assertArrayEquals(new String[]{"b"}, raceResult);
+    }
+
+    @Test
+    @DisplayName("3개 자동차가 5번 이동한 결과를 확인한다.")
+    void runFiveRoundsTwoWinner() {
+        // Given
+        RandomNumberGenerator randomNumberGenerator = TestUtils.mockRandomNumberGenerator(List.of(3, 9, 4));
+        RacingCarService racingCarService = new RacingCarService(randomNumberGenerator);
+        String userCarInput = "a,b,c";
+        String userRoundInput = "5";
+
+        // When
+        String[] raceResult = racingCarService.run(userCarInput, userRoundInput);
+
+        // Then
+        assertArrayEquals(new String[]{"b", "c"}, raceResult);
     }
 
 }
