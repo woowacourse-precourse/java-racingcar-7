9db4c566565a41782fdd37295296e2e5bfaef99c
taek2222 <oot309@daum.net>
2024-10-24 13:16:04 +0900

    feat(InputValidator): 중복 처리 검증 이동 및 추가
    - 원래 차 이름 중복 검사 로직에서 재사용성을 활용하기 위해 옮겼습니다.
    - 더 넓은 의미로 사용하기 위해 네이밍을 전체적으로 변경했습니다.

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -1,10 +1,22 @@
 package racingcar.util;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InputValidator {
 
     public static void validateNotBlank(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateNoDuplicates(List<String> list) {
+        Set<String> unique = new HashSet<>();
+        for (String input : list) {
+            if (!unique.add(input))
+                throw new IllegalArgumentException();
         }
     }
 }
