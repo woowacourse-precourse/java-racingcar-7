b4852c56e9c83a19bb349518552f80db75cf876b
Jo Myeong-geun <happy63590@gmail.com>
2024-10-25 05:54:23 +0900

    feat: 이동 거리를 `-`의 개수로 카운트하여 우승자를 구하는 기능 구현

--- src/main/java/racingcar/service/CarRaceService.java
+++ src/main/java/racingcar/service/CarRaceService.java
@@ -1,6 +1,7 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 import racingcar.domain.Car;
 import racingcar.domain.CarRace;
 
@@ -18,7 +19,21 @@ public class CarRaceService {
                 .forEach(Car::move);
     }
 
+    public List<String> getWinners() {
+        return carRace.getCars().stream()
+                .filter(car -> car.getMovingDistance().length() == getMaxDistance())
+                .map(Car::getName)
+                .toList();
+    }
+
     private boolean canMove() {
         return Randoms.pickNumberInRange(0, 9) >= 4;
+    }
+
+    private int getMaxDistance() {
+        return carRace.getCars().stream()
+                .mapToInt(car -> car.getMovingDistance().length())
+                .max()
+                .orElseThrow(() -> new IllegalArgumentException());
     }
 }
