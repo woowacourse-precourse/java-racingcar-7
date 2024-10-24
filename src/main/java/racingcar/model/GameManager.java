235d84ab32014810214269ba6d5385c56e4ed333
chyun <och2510@gmail.com>
2024-10-24 15:30:17 +0900

    feat(GameManager): 자동차의 전진 상태를 반환하는 기능을 추가

--- src/main/java/racingcar/model/GameManager.java
+++ src/main/java/racingcar/model/GameManager.java
@@ -3,13 +3,17 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import racingcar.constant.ErrorMessage;
 
 public class GameManager {
 
     private static final String CAR_NAME_DELIMITER = ",";
+    private static final String MOVEMENT_DISPLAY_CHARACTER = "-";
     private static final int RANDOM_START_NUMBER_FOR_MOVE_FORWARD = 0;
     private static final int RANDOM_END_NUMBER_FOR_MOVE_FORWARD = 9;
 
@@ -46,6 +50,16 @@ public class GameManager {
                 RANDOM_START_NUMBER_FOR_MOVE_FORWARD,
                 RANDOM_END_NUMBER_FOR_MOVE_FORWARD
         );
+    }
+
+    public Map<Car, String> getCurrentMovementStates(GameStatus gameStatus) {
+        Map<Car, String> currentMovementStates = new LinkedHashMap<>();
+
+        gameStatus.getRacersMovement().forEach((racer, moved) -> {
+            currentMovementStates.put(racer, MOVEMENT_DISPLAY_CHARACTER.repeat(moved));
+        });
+
+        return Collections.unmodifiableMap(currentMovementStates);
     }
 
     public List<Car> findWinners(GameStatus gameStatus) {
