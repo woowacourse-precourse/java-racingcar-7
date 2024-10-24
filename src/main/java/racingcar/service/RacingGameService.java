a952706956b3a174746d88625554e434e0ce70cc
dawit0905 <dawit0905@gmail.com>
2024-10-24 11:07:53 +0900

    feat: race, inputMoveCount, announceWinners 작성

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -1,14 +1,17 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.model.Car;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingGameService {
     private final InputView inputView = new InputView();
-    public List<Car> inputCars() {
+    private final OutputView outputView = new OutputView();
 
+    public List<Car> inputCars() {
         List<String> carNames = inputView.getCarNames();
         List<Car> cars = new ArrayList<>();
         for (String name : carNames) {
@@ -22,8 +25,36 @@ public class RacingGameService {
         return cars;
     }
 
-    public void announceWinners() {
-        List<String> winners = racingGameService.getWinners();
+    public void announceWinners(List<Car> cars) {
+        int maxDistance = cars.stream()
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElse(0);
+
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getDistance() == maxDistance) {
+                winners.add(car.getName());
+            }
+        }
         outputView.printWinners(winners);
     }
+
+    public int inputMoveCount() {
+        return inputView.getMoveCount();
+    }
+
+    public void race(List<Car> cars, int moveCount) {
+        outputView.printResult();
+        for (int i = 0; i < moveCount; i++) {
+            for (Car car : cars) {
+                int randomValue = Randoms.pickNumberInRange(0, 9);
+                if (randomValue >= 4) {
+                    car.move();
+                }
+            }
+            outputView.printCurrentStatus(cars);
+        }
+    }
+
 }
