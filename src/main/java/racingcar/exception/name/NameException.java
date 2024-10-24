0de3b5e0951c06252b5b7e0bad71a0183992be0d
tentenball <lth8905@gmail.com>
2024-10-24 22:17:42 +0900

    feat: Name에 대한 에러 설정
    * GlobalException: 에러를 처리하는 부모클래스
    * NameException: Name에 관련된 에러처리 클래스
    * NameExceptionMessage: Name에 관련된 에러메시지

---
+++ src/main/java/racingcar/exception/name/NameException.java
@@ -0,0 +1,9 @@
+package racingcar.exception.name;
+
+import racingcar.exception.GlobalException;
+
+public class NameException extends GlobalException {
+	public NameException(NameExceptionMessage message){
+		super(message.getMessage());
+	}
+}
