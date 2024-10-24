f49a82ebaca112523d7ffae302161c54d4316cb2
joonwan <joonwan1024@naver.com>
2024-10-24 19:01:08 +0900

    feat: OutputManager 최종 우승자 메시지 출력 기능 구현

--- src/main/java/racingcar/OutputManager.java
+++ src/main/java/racingcar/OutputManager.java
@@ -1,10 +1,19 @@
 package racingcar;
 
+import java.util.List;
+
 public class OutputManager {
 
     private static final String GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String WINNER_RESULT_MESSAGE = "최종 우승자 : ";
+    private static final String DELIMITER = ", ";
 
     public static void printGuideMessage() {
         System.out.println(GUIDE_MESSAGE);
+    }
+
+    public static void printWinner(List<String> winnerList) {
+        String joinedName = String.join(DELIMITER, winnerList);
+        System.out.println(WINNER_RESULT_MESSAGE + joinedName);
     }
 }
