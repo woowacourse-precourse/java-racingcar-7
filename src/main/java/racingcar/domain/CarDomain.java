b2a22d66a283aebfe6f20f226d57ad0c61d03e6b
reindeer002 <lsj000424@gmail.com>
2024-10-24 18:33:10 +0900

    refactor(domain): Rename incrementDistance to attemptMove and add movement logic
    - Rename incrementDistance method to attemptMove for clarity.
    - Add isMove method to determine whether the car should move based on random number generation.

--- src/main/java/racingcar/domain/CarDomain.java
+++ src/main/java/racingcar/domain/CarDomain.java
@@ -1,6 +1,10 @@
 package racingcar.domain;
 
 import static racingcar.common.constant.Constant.DEFAULT_MOVE_DISTANCE;
+import static racingcar.common.constant.Constant.INCLUSIVE_RANGE;
+import static racingcar.common.constant.Constant.MOVE_FORWARD_RANGE;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class CarDomain {
     private final String name;
@@ -23,7 +27,14 @@ public class CarDomain {
         return distance;
     }
 
-    public void incrementDistance() {
-        this.distance += DEFAULT_MOVE_DISTANCE;
+    public void attemptMove() {
+        if (isMove()) {
+            this.distance += DEFAULT_MOVE_DISTANCE;
+        }
+    }
+
+    private boolean isMove() {
+        int randomNumber = Randoms.pickNumberInRange(INCLUSIVE_RANGE.min(), INCLUSIVE_RANGE.max());
+        return MOVE_FORWARD_RANGE.contains(randomNumber);
     }
 }
