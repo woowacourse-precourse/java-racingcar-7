73aa0e26fc0c8e910312de4f0f356c13aa20a35d
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-25 02:29:25 +0900

    refactor($InputView): 최소 시도 횟수를 검증하지 않도록 변경
    - 최소 시도 횟수 검증은 Service 에서 수행될 예정임.
    - int 를 반환하는 것은 InputView 의 역할에 속하기 때문에, parseInt 의 예외는 계속 처리.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,25 +4,15 @@ import racingcar.util.io.InputConsole;
 
 public class InputView {
 
-    private final int MINIMUM_TRY_COUNT = 1;
-
     public String getCarNames() {
         return InputConsole.readLine();
     }
 
     public int getTryCount() {
         try {
-            int tryCount = Integer.parseInt(InputConsole.readLine());
-            validateTryCount(tryCount);
-            return tryCount;
+            return Integer.parseInt(InputConsole.readLine());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("유효하지 않은 시도 횟수입니다.");
-        }
-    }
-
-    private void validateTryCount(int tryCount) {
-        if (tryCount < MINIMUM_TRY_COUNT) {
-            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
         }
     }
 }
