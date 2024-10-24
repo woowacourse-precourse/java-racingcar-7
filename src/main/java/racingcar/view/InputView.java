38fcdb937211525962f0c039ad44ecd31c55b246
Jo Myeong-geun <happy63590@gmail.com>
2024-10-25 06:00:01 +0900

    chore: 메서드명 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
         return Console.readLine();
     }
 
-    public String inputRounds() {
+    public String inputAttempts() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         return Console.readLine();
     }
