0caa21a229bfcd96e42fbc0da680c833456c1398
plan11plan <tjtfy7028@gmail.com>
2024-10-24 14:38:37 +0900

    style: 자동차 이동거리 속성 네이밍 재정의
    - Distance(변경 전) -> MovementDistance(변경 후)
    
    패키지 구조에서 Distance라고만 했을때 Distance의 의도의 불명확함 문제 방지위함

--- src/main/java/racingcar/Car/Car.java
+++ src/main/java/racingcar/Car/Car.java
@@ -2,32 +2,35 @@ package racingcar.Car;
 
 public class Car {
     private final CarName carName;
-    private final Distance distance;
+    private final MovementDistance movementDistance;
 
-    private Car(CarName carName, Distance distance){
+    private Car(CarName carName, MovementDistance movementDistance){
         this.carName = carName;
-        this.distance = distance;
+        this.movementDistance = movementDistance;
     }
     public static Car createNamedCar(String carName){
         return new Car(
                 CarName.createCarName(carName),
-                Distance.initialzeDistance()
+                MovementDistance.initialzeDistance()
         );
     }
 
     public Car increaseDistanceBy(int stepCount) {
        return new Car(
                this.carName,
-               this.distance.increaseDistanceBy(stepCount)
+               this.movementDistance.increaseDistanceBy(stepCount)
        );
     }
 
     public Car maintainCurrentPosition() {
-        return new Car(this.carName,this.distance);
+        return new Car(
+                this.carName,
+                this.movementDistance
+        );
     }
 
     public int provideCurrentPosition() {
-        return this.distance.getValue();
+        return this.movementDistance.getValue();
     }
 
     public String provideCarName() {
--- src/main/java/racingcar/Car/MovementDistance.java
+++ src/main/java/racingcar/Car/MovementDistance.java
@@ -1,27 +1,27 @@
 package racingcar.Car;
 
-public class Distance {
+public class MovementDistance {
     private static final int INITIAL_POSITION = 0;
     private static final int MIN_STEP = 1;
     private static final int MAX_STEP = 100;
     private final int value;
 
-    public Distance(int value) {
+    public MovementDistance(int value) {
         this.value = value;
     }
 
-    public static Distance initialzeDistance() {
-        return new Distance(INITIAL_POSITION);
+    public static MovementDistance initialzeDistance() {
+        return new MovementDistance(INITIAL_POSITION);
     }
 
-    public Distance increaseDistanceBy(int stepCount) {
+    public MovementDistance increaseDistanceBy(int stepCount) {
         if(stepCount < MIN_STEP){
                 throw new IllegalArgumentException("이동 거리는 최소 1스텝 이상 가능 합니다.");
         }
         if(stepCount > MAX_STEP){
                 throw new IllegalArgumentException("이동 거리는 최대 100스텝 까지만 가능 합니다.");
         }
-        return new Distance(this.value + stepCount);
+        return new MovementDistance(this.value + stepCount);
     }
 
     public int getValue() {
