bb6ed9e81dcbb6a6792095c65acbe3b89f718ffc
ufo9430 <ufo9430@naver.com>
2024-10-23 22:06:21 -0400

    refactor: InputView가 이름을 반환하도록 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,7 +1,10 @@
 package racingcar.view;
 
+import racingcar.console.ConsoleReader;
+
 public class InputView {
-    public void start(){
+    public String start(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return ConsoleReader.readName();
     }
 }
