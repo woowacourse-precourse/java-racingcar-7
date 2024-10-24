b2f526bfc60bbda4808cd330506307c840dd57af
HongSeongJo <164273757+HongSeongJo@users.noreply.github.com>
2024-10-24 15:58:50 +0900

    feat(Car): Car 클래스를 생성한다

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int moveCount;
+
+    public Car(String name) {
+        this.name = name;
+        this.moveCount = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+
+    public void incrementMoveCount() {
+        moveCount++;
+    }
+}
