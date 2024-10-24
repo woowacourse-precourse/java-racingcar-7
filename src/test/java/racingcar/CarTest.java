4df4c9e6894fe9f4ddea02fe56c697ffd7b41784
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-10-24 09:11:11 +0900

    Test improve car test readability

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,25 +1,31 @@
 package racingcar;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class CarTest {
 
+    @DisplayName("자동차의 이름이 5글자를 초과하는 경우 예외를 반환하는 걸 테스트하라")
     @Test
     void test1() {
-        Assertions.assertThatThrownBy(() -> new Car("다섯글자가넘는차"))
+        assertThatThrownBy(() -> new Car("다섯글자가넘는차"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("자동차의 이름이 null 이거나 빈 경우 예외를 반환하는 걸 테스트하라")
     @ParameterizedTest
     @NullAndEmptySource
     void test2(String name) {
-        Assertions.assertThatThrownBy(() -> new Car(name))
+        assertThatThrownBy(() -> new Car(name))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("차가 전진하는 경우 포지션이 1증가하는 것을 테스트하라")
     @Test
     void test3() {
         Car car1 = new Car("car1");
@@ -28,8 +34,8 @@ class CarTest {
         int distance2 = car1.go();
         int distance3 = car1.go();
 
-        Assertions.assertThat(distance1).isEqualTo(1);
-        Assertions.assertThat(distance2).isEqualTo(2);
-        Assertions.assertThat(distance3).isEqualTo(3);
+        assertThat(distance1).isEqualTo(1);
+        assertThat(distance2).isEqualTo(2);
+        assertThat(distance3).isEqualTo(3);
     }
 }
