1397034e883b01e58d5003a614eb227256366df5
mete0rfish <sungwon326@naver.com>
2024-10-24 12:20:45 +0900

    fix(view): delete Parser and add CarInput
    CarInput acts pasing part. Input is splited in CarInput.InputCarsName().

--- src/main/java/racingcar/view/CarInput.java
+++ src/main/java/racingcar/view/CarInput.java
@@ -2,11 +2,15 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CarInput {
 
-    public static String InputCarsName() {
+    public static List<String> InputCarsName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+        String input = Console.readLine();
+        return Arrays.stream(input.split(",")).toList();
     }
 
     public static String inputCarRaceRap() {
