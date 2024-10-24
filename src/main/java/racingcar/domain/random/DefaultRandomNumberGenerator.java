1713a3fa31fc6f806088c85c4e7f627bbd92a4fc
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-10-24 19:37:25 +0900

    refactor: RandomNumberGeneratorTest가 실제 프로덕션 코드의 구현체를 검증하도록 변경

--- src/main/java/racingcar/domain/random/DefaultRandomNumberGenerator.java
+++ src/main/java/racingcar/domain/random/DefaultRandomNumberGenerator.java
@@ -1,11 +1,20 @@
 package racingcar.domain.random;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.exception.BusinessException;
+import racingcar.exception.RacingCarExceptionMessage;
 
 public class DefaultRandomNumberGenerator implements RandomNumberGenerator {
 
     @Override
     public Integer getRandomNumber(Integer min, Integer max) {
+        validateNumberRange(min, max);
         return Randoms.pickNumberInRange(min,max);
+    }
+
+    private static void validateNumberRange(Integer min, Integer max) {
+        if (min < 0 || max > 9){
+            throw new BusinessException(RacingCarExceptionMessage.INVALID_NUMBER_EXCEPTION);
+        }
     }
 }
--- src/test/java/racingcar/domain/random/RandomNumberGeneratorTest.java
+++ src/test/java/racingcar/domain/random/RandomNumberGeneratorTest.java
@@ -8,23 +8,51 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RandomNumberGeneratorTest {
 
+    public static final int TEST_FAIL_MAX_VALUE = 20;
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 9;
+    public static final int TEST_COUNT = 1000;
+    public static final int ACCEPT_VALUE = 4;
+    public static final int TEST_FAIL_MIN_VALUE = 11;
+
+
     @Test
     void 랜덤_숫자_생성_테스트() {
         // given
-        RandomNumberGenerator randomNumberGenerator = new FixedRandomNumberGenerator(4);
+        RandomNumberGenerator randomNumberGenerator = new DefaultRandomNumberGenerator();
 
         // when
-        Integer result = randomNumberGenerator.getRandomNumber(0, 9);
+        Integer result = randomNumberGenerator.getRandomNumber(MIN_VALUE, MAX_VALUE);
 
         // then
-        assertThat(result).isEqualTo(4);
+        assertThat(result).isEqualTo(ACCEPT_VALUE);
     }
 
     @Test
-    void 랜덤_숫자_생성_테스트_값이_10이상이면_Exception() {
+    void 랜덤_숫자_생성_테스트_경계값이_10이상이면_Exception() {
         // given & when & then
         assertThatThrownBy(() -> {
-            new FixedRandomNumberGenerator(15);
+            new DefaultRandomNumberGenerator().getRandomNumber(TEST_FAIL_MIN_VALUE, TEST_FAIL_MAX_VALUE);
         }).isInstanceOf(BusinessException.class);
+    }
+
+    @Test
+    void 랜덤한_숫자가_1과_9사이의_정수인지_테스트() {
+        // given
+        RandomNumberGenerator randomNumberGenerator = new DefaultRandomNumberGenerator();
+        boolean isInRange = true;
+
+        // when
+        for (int i = 0; i < TEST_COUNT; i++) {
+            Integer randomNumber = randomNumberGenerator.getRandomNumber(MIN_VALUE, MAX_VALUE);
+
+            // then
+            if (randomNumber < MIN_VALUE || randomNumber > MAX_VALUE) {
+                isInRange = false;
+                break;
+            }
+        }
+
+        assertThat(isInRange).isTrue();
     }
 }
