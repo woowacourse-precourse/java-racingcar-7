05d51db5c9e7a2cf4c9d665dc30925917d647443
ufo9430 <ufo9430@naver.com>
2024-10-23 22:07:29 -0400

    feat: 레이싱 경기를 관리하는 클래스 생성

---
+++ src/main/java/racingcar/controller/RaceManager.java
@@ -0,0 +1,19 @@
+package racingcar.controller;
+
+import racingcar.console.ConsoleReader;
+import racingcar.view.InputView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RaceManager {
+    private InputView inputView;
+
+    public RaceManager(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void getName(){
+
+    }
+}
