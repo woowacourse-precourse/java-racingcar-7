70da5ce07fe04664c138f28ed1b8095e7a3c7318
백재우 <9712jw@gmail.com>
2024-10-24 15:03:01 +0900

    feat : 사용자 입력문, 게임 시작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,21 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import racingcar.game.Game;
+import racingcar.io.Input;
+import racingcar.vehicle.Car;
 
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input in = new Input();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carName = in.nextLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String cnt = in.nextLine();
+        System.out.println();
+        Game game = new Game();
+        ArrayList<Car> result = game.gameStart(carName, cnt);
+        game.winnerPlayer(result);
     }
 }
