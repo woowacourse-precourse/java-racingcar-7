3dd969b08fa039f07d349f91c37af1a998167f59
CHISANW <keuye06380618@gmail.com>
2024-10-24 17:54:25 +0900

    feat(InputView): 사용자 입력을 처리하는 InputView 클래스 구현
    - 자동차 이름과 시도 횟수를 입력받기 위한 메서드 readCarNames()와 readTryCount() 구현.
    - 사용자에게 입력 안내 메시지를 출력하도록 구성.

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,21 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String CAR_NAME_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String COUNT_INPUT = "시도할 횟수는 몇 회인가요?";
+
+    public String readCarNames() {
+        System.out.println(CAR_NAME_INPUT);
+        return Console.readLine();
+    }
+
+    public int readTryCount() {
+        System.out.println(COUNT_INPUT);
+        String input = Console.readLine();
+        System.out.println();
+        return Integer.parseInt(input);
+    }
+}
