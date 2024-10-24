c147b1806502bdf7823ababaa64660cfa547826f
soeun2537 <soeun2537@gmail.com>
2024-10-24 11:29:19 +0900

    feat: 시도할 횟수 입력 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,6 +9,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carsNameInput = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String countInput = Console.readLine();
 
         String[] carsNameArray = parseCarsName(carsNameInput);
         List<String> carsNameList = arrayToList(carsNameArray);
