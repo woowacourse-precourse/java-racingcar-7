fba33563bbe1fe5a3242e367caa1ba2ee242ed5d
kimdevspace <ejk5148@gmail.com>
2024-10-24 20:37:30 +0900

    feat: Number input exception handling function

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,10 +7,16 @@ import java.util.List;
 
 public class Application {
 
-    public static void main(String[] args) throws CarNameInputException{
+    public static void main(String[] args) throws CarNameInputException {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int n = Integer.parseInt(Console.readLine());
+        if (n < 0) {
+            throw new NumberInputException();
+        }
 
         List<Car> cars = createParticipant(input);
 
