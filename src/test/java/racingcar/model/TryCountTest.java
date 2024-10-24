43ca7b144c1d1f565597d87143281583aeba638f
digitpic <jkw5033@gmail.com>
2024-10-24 18:36:19 +0900

    test(try-count-test): 유효성 검사 테스트 작성

--- src/test/java/racingcar/model/TryCountTest.java
+++ src/test/java/racingcar/model/TryCountTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static racingcar.constants.ErrorMessage.NOT_ALLOWED_FIRST_LAST_BLANK;
+import static racingcar.constants.ErrorMessage.NOT_ALLOWED_UNDER_MINIMUM_TRY_COUNT;
+import static racingcar.constants.ErrorMessage.TRY_COUNT_MUST_BE_NUMERIC;
 
 class TryCountTest {
     @Test
@@ -24,7 +26,7 @@ class TryCountTest {
         String rawTryCount = "1 ";
 
         // when & then
-        assertThatThrownBy(() -> new CarNames(rawTryCount))
+        assertThatThrownBy(() -> new TryCount(rawTryCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(NOT_ALLOWED_FIRST_LAST_BLANK);
     }
@@ -35,9 +37,31 @@ class TryCountTest {
         String rawTryCount = " 1 ";
 
         // when & then
-        assertThatThrownBy(() -> new CarNames(rawTryCount))
+        assertThatThrownBy(() -> new TryCount(rawTryCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(NOT_ALLOWED_FIRST_LAST_BLANK);
+    }
+
+    @Test
+    void 문장에_정수가_아닌_다른_문자열이_입력되는_경우_예외가_발생한다() {
+        // given
+        String rawTryCount = "a";
+
+        // when & then
+        assertThatThrownBy(() -> new TryCount(rawTryCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(TRY_COUNT_MUST_BE_NUMERIC);
+    }
+
+    @Test
+    void 시도할_횟수가_1보다_작은_경우_예외가_발생한다() {
+        // given
+        String rawTryCount = "0";
+
+        // when & then
+        assertThatThrownBy(() -> new TryCount(rawTryCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NOT_ALLOWED_UNDER_MINIMUM_TRY_COUNT);
     }
 
     @Test
