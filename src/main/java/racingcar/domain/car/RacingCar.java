924cdc9b7eb9acfea228a9525adef150c9a21e10
jijongkwon <help3451@naver.com>
2024-10-25 01:28:45 +0900

    feat(domain) : RacingCar 클래스 생성
    filed
    - name : 레이싱 카 이름
    
    TODO
    - 이름반환

---
+++ src/main/java/racingcar/domain/car/RacingCar.java
@@ -0,0 +1,14 @@
+package racingcar.domain.car;
+
+public class RacingCar {
+    private String name;
+
+    public RacingCar(String name) {
+        this.name = name;
+    }
+
+    // TODO : 이름 반환
+    public String getName() {
+        return null;
+    }
+}
