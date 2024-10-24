9ae24e80f420f504a4a3732742687ab641ee8b14
jangsubin <eltm521@naver.com>
2024-10-24 22:46:54 +0900

    feat : 사용자 입력을 위한 InputView 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+
+        String names = inputView.readName();
+        int tryNumber = inputView.readTryNumber();
+
     }
 }
