697435ae93907d0c80e9be9adbe16b5e39702175
lux00leo <lux00leo@gmail.com>
2024-10-24 16:52:50 +0900

    refactor : InputParser의 중복되는 기능의 메서드 제거

--- src/main/java/racingcar/util/InputParser.java
+++ src/main/java/racingcar/util/InputParser.java
@@ -22,27 +22,18 @@ public class InputParser {
     }
     public static List<RacingCar> collectRacingCarAsList() {
         final String[] racers = getRacersAsArrayFromUserInput();
-        validateRacers(racers);
 
         return Arrays.stream(racers).map(RacingCar::new)
                 .collect(Collectors.toList());
     }
-
     private static String[] getRacersAsArrayFromUserInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         final String userInput = readLine();
         final String RACERS_REGEX = "^([^,]{1,5})(,[^,]{1,5})*$";
-        validateUserInput(userInput,RACERS_REGEX,"유효하지 않은 입력입니다.");
+        validateUserInput(userInput,RACERS_REGEX,"드라이버의 이름은 5글자 이하로 해주십시오.");
 
         final String DELIMITER = ",";
         return userInput.split(DELIMITER);
-    }
-    private static void validateRacers(String[] racers) {
-        for (String racer : racers) {
-            if (racer.length() > 5) {
-                throw new IllegalArgumentException("드라이버의 이름은 5글자 이하로 해주십시오.");
-            }
-        }
     }
     private static void validateUserInput(
             final String userInput,
