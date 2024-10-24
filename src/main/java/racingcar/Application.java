463f0f556e8e673dfa16d5f4916f6d6a7632892a
KnD0715 <rlaalswo0715@naver.com>
2024-10-24 14:43:21 +0900

    이동횟수 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,13 +7,17 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("경주할 자동차 이름을 입력하시오.");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
-
         List<Name> names = Name.createNames(input);
+
+        RaceRound raceRound = RaceRound.inputRounds();
+        System.out.println("입력된 자동차 이름 : ");
         for (Name name : names) {
             System.out.println(name.getName());
         }
+
+        System.out.println("시도할 횟수 : " + raceRound.getRounds());
 
     }
 }
