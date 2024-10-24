3abaf3b7c286a709aa3192dc76749749c47f6789
kaswhy <sxshx818@naver.com>
2024-10-24 20:04:58 +0900

    style(InputValueTest): 상수명 오타 수정
    변경 전
    - 상수명 마지막 "k"가 소문자
    
    변경 후
    - 이를 대문자로 바꿈

--- src/test/java/racingcar/InputValueTest.java
+++ src/test/java/racingcar/InputValueTest.java
@@ -12,13 +12,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class InputValueTest {
-    private static final String CAR_NAME_BLANk = "";
+    private static final String CAR_NAME_BLANK = "";
 
     @DisplayName("자동차 이름이 빈 값이면 예외 발생")
     @Test
     void ThrowExceptionIfCarNameBlank() {
         // given
-        String carName = CAR_NAME_BLANk;
+        String carName = CAR_NAME_BLANK;
         ValidateCarName validateCarName = new valicateCarName();
 
         // when
