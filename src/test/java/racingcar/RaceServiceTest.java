8cbd1d3ff3c00d28ebc1a09a453503743dab11bf
GiSeok Kang <ime.rokz@gmail.com>
2024-10-24 16:54:43 +0900

    test: 중복 이름 입력 테스트 추가

---
+++ src/test/java/racingcar/RaceServiceTest.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.service.RaceService;
+
+class RaceServiceTest {
+
+    private static final RaceService raceService = new RaceService();
+
+    @DisplayName("중복되는 이름 입력")
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,pobi", "woni,jun,jun", "me,you,me"})
+    void 중복_이름_입력(String input) {
+        Assertions.assertThatThrownBy(() -> raceService.carListOf(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복되는 이름이 존재합니다.");
+    }
+}
