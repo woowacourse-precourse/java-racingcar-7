0c70d6e5095b173e8c32e5afe2053b93e8983ff5
Hyungjung Kim <gudwnd1113@gmail.com>
2024-10-24 13:25:24 +0900

    test(RacingCar): build RacingCar-related test code
    RacingCar 인스턴스 생성 시, 이름 길이에 따른 예외를 검증하는 테스트 코드와
    RacingCar의 전진함수의 정상 작동 여부를 검증하는 테스트 코드 작성.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,12 +1,13 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -15,20 +16,38 @@ class ApplicationTest extends NsTest {
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void RacingCar_construct_test() {
+        RacingCar car = new RacingCar("test");
+        car.rollDice();
+        car.rollDice();
+        car.rollDice();
+        car.rollDice();
+        car.rollDice();
+        assertThat(car.getCount()).isBetween(0, 5);
+    }
+
+    @DisplayName("자동차의 이름이 1자에서 5자 사이가 아니면 예외를 발생한다.")
+    @Test
+    void RacingCar_exception_test() {
+        assertThatThrownBy(() -> new RacingCar("invalid_name"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
