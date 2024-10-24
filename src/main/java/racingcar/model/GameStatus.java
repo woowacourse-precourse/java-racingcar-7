0e52674e4f7508fb7bcce276cc49152c8caa5d9f
chyun <och2510@gmail.com>
2024-10-24 09:24:53 +0900

    feat(GameStatus): 게임 상태에 시도할 횟수 추가

---
+++ src/main/java/racingcar/model/GameStatus.java
@@ -0,0 +1,28 @@
+package racingcar.model;
+
+import racingcar.constant.ErrorMessage;
+
+public class GameStatus {
+    private static final int ATTEMPTS_BASE = 0;
+    private static final int MINIMUM_TOTAL_ATTEMPTS = 1;
+
+    private final int totalAttempts;
+    private int currentAttempts;
+
+    private GameStatus(final int totalAttempts) {
+        validateTotalAttemptsIsPositiveNumber(totalAttempts);
+
+        this.totalAttempts = totalAttempts;
+        this.currentAttempts = ATTEMPTS_BASE;
+    }
+
+    public static GameStatus from(final int totalAttempts) {
+        return new GameStatus(totalAttempts);
+    }
+
+    private void validateTotalAttemptsIsPositiveNumber(final int totalAttempts) {
+        if (totalAttempts < MINIMUM_TOTAL_ATTEMPTS) {
+            throw new IllegalArgumentException(ErrorMessage.ATTEMPTS_SIGN_POSITIVE.getMessage());
+        }
+    }
+}
