a189fd55f886d9f502be47de89b8440120d912b4
Jo Myeong-geun <happy63590@gmail.com>
2024-10-25 05:58:42 +0900

    feat: 우승자가 여러 명일 경우 쉼표로 구분하여 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,6 @@
 package racingcar.view;
 
+import java.util.List;
 import java.util.Map;
 
 public class OutputView {
@@ -11,5 +12,10 @@ public class OutputView {
     public void printCarsStatus(Map<String, String> cars) {
         cars.forEach((key, value) -> System.out.println(key + " : " + value));
         System.out.println();
+    }
+
+    public void printWinners(List<String> winners) {
+        String winnerNames = String.join(", ", winners);
+        System.out.print("최종 우승자 : " + winnerNames);
     }
 }
