fea7de56b36fe4078a20f2fb490090fb4980ee84
rladmstn <oz33177351@gmail.com>
2024-10-24 21:24:58 +0900

    feat(application) : 시도 횟수만큼 자동차 전진 및 출력 추가
    - 시도 횟수만큼 각 자동차들에 대해 전진/멈춤 여부 판단
    - TreeMap<String, Integer> : key는 자동차 이름, value는 전진 횟수
    - 각 시도마다 자동차 이름과 함께 전진 수준을 출력함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,17 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
 import racingcar.enums.PrintMessage;
 import racingcar.utils.InputValidator;
+import racingcar.utils.Parser;
 
 public class Application {
+    public static TreeMap<String, Integer> cars = new TreeMap<>();
+
     public static void main(String[] args) {
         System.out.println(PrintMessage.ENTER_CARS_PROMPT.getMessage());
         String inputCars = Console.readLine();
@@ -14,5 +21,41 @@ public class Application {
 
         InputValidator.validateCarNames(inputCars);
         InputValidator.validateTrialCount(inputTrialCount);
+
+        initCars(inputCars);
+        int trialCount = Integer.parseInt(inputTrialCount);
+
+        System.out.println("\n"+PrintMessage.PRINT_RACE_RESULT.getMessage());
+        while(trialCount-- > 0) {
+            runRace();
+        }
+
+    }
+
+    private static void runRace() {
+        Set<String> carNames = cars.keySet();
+        for(String carName : carNames){
+            if(Randoms.pickNumberInRange(0, 9)>=4){
+                cars.put(carName, cars.getOrDefault(carName, 0)+1);
+            }
+        }
+
+        printRaceStatus();
+    }
+
+    private static void printRaceStatus() {
+        Set<String> carNames = cars.keySet();
+        for(String carName : carNames){
+            System.out.println(carName + " : " + "-".repeat(cars.get(carName)));
+        }
+
+        System.out.println();
+    }
+
+    private static void initCars(String inputCars) {
+        List<String> carNames = Parser.parseCarNames(inputCars);
+        for(String carName : carNames) {
+            cars.put(carName, 0);
+        }
     }
 }
