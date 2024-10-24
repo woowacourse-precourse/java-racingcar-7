aa6141423d37adfaec72e6296d95150b4dd56c5d
Jewan1120 <hawaii1468@gmail.com>
2024-10-24 17:15:35 +0900

    feat(Application): 사용자로부터 자동차 이름들을 입력 받아서 구분하는 기능 구현
    자동차 이름들을 쉼표(,)로 구분하여 각 객체를 생성해줍니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputString = Console.readLine();
+        StringTokenizer stringTokenizer = new StringTokenizer(inputString, ",");
+        List<Car> cars = new ArrayList<>();
+        while (stringTokenizer.hasMoreTokens()) {
+            cars.add(new Car(stringTokenizer.nextToken()));
+        }
+    }
+
+    static class Car {
+        String name;
+        int moveCount = 0;
+
+        Car(String name) {
+            this.name = name;
+        }
     }
 }
