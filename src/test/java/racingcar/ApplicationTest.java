16964c3bd09fd459b5d35471b85e79a355699efd
bk <naturalbb360@gmail.com>
2024-10-24 17:57:03 +0900

    test(ApplicationTest): 자동차 이름 테스트 추가 - 자동차 이름 길이 테스트 추가 - 영어가 아닌 이름 테스트 추가 - 자동차 대수가 2대 미만 혹은 10대 초과인 테스트 추가 - 중복 이름 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -20,6 +23,45 @@ class ApplicationTest extends NsTest {
                 assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
             },
             MOVING_FORWARD, STOP
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abcdefg,pobi", "pobi,abcdefg"})
+    void 자동차_이름_길이_테스트(String candidate) {
+        assertSimpleTest(()->
+                assertThatThrownBy(()->run(candidate))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("자동차 이름은 5자 이하만 가능합니다.")
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,포비", "포비,Pobi", "포pobi,pobi", "pobi포,pobi"})
+    void 영어가_아닌_이름_테스트(String candidate){
+        assertSimpleTest(()->
+                assertThatThrownBy(() -> run(candidate))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("자동차 이름은 알파벳으로만 구성되어야 합니다.")
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi", "a,b,c,d,e,f,g,h,i,j,k"})
+    void 자동차_대수가_2대_미만_10대_초과_테스트(String candidate){
+        assertSimpleTest(()->
+                assertThatThrownBy(() -> run(candidate))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("자동차 대수는 2대 이상 10대 이하여야 합니다.")
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,pobi", "pobi,Pobi"})
+    void 중복_이름_테스트(String candidate){
+        assertSimpleTest(() -> assertThatThrownBy(() -> run(candidate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 자동차 이름은 사용할 수 없습니다.(대문자와 소문자는 동일하게 인식됩니다.)")
         );
     }
 
