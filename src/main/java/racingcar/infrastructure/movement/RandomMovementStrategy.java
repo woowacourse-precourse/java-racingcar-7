da8f8388a6cd18f8c9c7630fda4eff2a44d959ea
jijongkwon <help3451@naver.com>
2024-10-25 01:41:17 +0900

    feat(movement) : RandomMovementStrategy 생성
    TODO
    - validOverFour : 4 이상인지 판단

---
+++ src/main/java/racingcar/infrastructure/movement/RandomMovementStrategy.java
@@ -0,0 +1,9 @@
+package racingcar.infrastructure.movement;
+
+public class RandomMovementStrategy {
+
+    // TODO: 4 이상인지 판단
+    public boolean validOverFour(int number) {
+        return false;
+    }
+}
