e6dfce79ace14be52686750dad26ea917a3ca06f
tentenball <lth8905@gmail.com>
2024-10-25 02:06:53 +0900

    feat: 자동차 경주 게임 진행 로직 작성
    * GameController: 자동차 경주 게임을 진행하는 로직 작성(car의 score를 추가하는것 까지만)

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,8 +1,11 @@
 package racingcar.controller;
 
+import java.util.stream.IntStream;
+
 import racingcar.domain.car.Cars;
 import racingcar.domain.name.Names;
 import racingcar.domain.racingGame.RacingGameCount;
+import racingcar.util.RandomNumberGenerator;
 import racingcar.view.InputView;
 
 public class GameController {
@@ -11,7 +14,20 @@ public class GameController {
 		Names carNames = InputView.inputCarNames();
 		Cars cars = Cars.from(carNames.getNames());
 		RacingGameCount count = InputView.inputRaceCount();
+
+		processRacingGame(cars, count);
 	}
 
+	private void processRacingGame(Cars cars, RacingGameCount gameCount) {
+		RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+		IntStream.range(0, gameCount.getCount())
+			.forEach(racingGame -> processEachRacingGame(cars, randomNumberGenerator));
+	}
+
+	private void processEachRacingGame(Cars cars, RandomNumberGenerator randomNumberGenerator) {
+		cars.getCars().forEach(car -> {
+			car.processGame(randomNumberGenerator);
+		});
+	}
 
 }
