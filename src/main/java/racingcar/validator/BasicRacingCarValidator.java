78fb261d9229ef5359df4c07cb3f6398d34e60cc
changuii <rhljh7410@gmail.com>
2024-10-24 17:12:31 +0900

    feat(BasicRacingCarValidator) : 3. 자동차들의 이름이 5자 초과라면 `IllegalArgumentException`을 발생시켜라
    - validateCarNameLengthLessThanMax() : 자동차의 이름이 최댓값 이하인지 검증한다.

--- src/main/java/racingcar/validator/BasicRacingCarValidator.java
+++ src/main/java/racingcar/validator/BasicRacingCarValidator.java
@@ -3,8 +3,16 @@ package racingcar.validator;
 import java.util.List;
 
 public class BasicRacingCarValidator implements RacingCarValidator {
+    private static final int CAR_NAME_MAX_LENGTH = 5;
+
     @Override
     public void validateCarNamesAndMoveCount(List<String> carNames, int MoveCount) {
 
+    }
+
+    private void validateCarNameLengthLessThanMax(String carName) {
+        if (carName.length() > CAR_NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException();
+        }
     }
 }
