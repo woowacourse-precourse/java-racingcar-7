15fb01365b6efa2a9326fcdc725695cc0404849c
5ms <oms010414@gmail.com>
2024-10-24 23:42:34 +0900

    자동차 객체 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,23 @@
 package racingcar;
 
 public class Car {
+    private String name;
+    private Integer moveCount;
+
+    public Car(String name) {
+        this.name = name;
+        this.moveCount = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getMoveCount() {
+        return moveCount;
+    }
+
+    public void move(){
+        moveCount++;
+    }
 }
