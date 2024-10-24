9a277b842402d835f04f5d877344645783094b21
yeong-hwan <jghff700@naver.com>
2024-10-25 08:22:11 +0900

    refactor(enums): append execution message at notification message

--- src/main/java/racingcar/enums/NotificationMessage.java
+++ src/main/java/racingcar/enums/NotificationMessage.java
@@ -3,7 +3,8 @@ package racingcar.enums;
 public enum NotificationMessage {
     INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     INPUT_ROUNDS("시도할 횟수는 몇 회인가요?"),
-    FINAL_WINNERS("최종 우승자 : ");
+    EXECUTION_RESULT("실행 결과"),
+    FINAL_WINNERS("최종 우승자");
 
     private final String message;
 
