551ce0d6b63f8019ab96579efe66d19f3e81c3e5
dawit0905 <dawit0905@gmail.com>
2024-10-24 09:44:42 +0900

    feat: 게임 컨트롤러 생성

---
+++ src/main/java/racingcar/RacingGameController.java
@@ -0,0 +1,27 @@
+package racingcar;
+
+public class RacingGameController {
+
+    public void start() {
+        inputCars();
+        inputMoveCount();
+        race();
+        announceWinners();
+    }
+
+    private void announceWinners() {
+    }
+
+    private void race() {
+    }
+
+    private void inputMoveCount() {
+        
+    }
+
+    private void inputCars() {
+        
+    }
+
+
+}
