0b1c5e180c88465bfcbcc4207c0967e52abdf248
hmlssghm <kwonse326@gmail.com>
2024-10-24 15:32:42 +0900

    feat(ErrorMessageEnum): 에러 출력 메시지 enum 생성

--- src/main/java/racingcar/view/messages/ErrorMessageEnum.java
+++ src/main/java/racingcar/view/messages/ErrorMessageEnum.java
@@ -1,4 +1,15 @@
 package racingcar.view.messages;
 
 public enum ErrorMessageEnum {
+    _ERROR("[ERROR] 니다.");
+
+    private final String message;
+
+    ErrorMessageEnum(String message) {
+        this.message = message;
+    }
+
+    public String getErrorMessage() {
+        return message;
+    }
 }
