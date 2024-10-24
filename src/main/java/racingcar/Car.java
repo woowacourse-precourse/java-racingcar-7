518b2462dc7b771e9725cff6fe19570cd02e0146
Park, minsu <cmwmss59@gmail.com>
2024-10-24 14:15:27 +0900

    feat : 자동차 이름을 검증하는 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,21 @@
 package racingcar;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+    private final String name;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("이름은 빈 값일수 없습니다");
+        }
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 5자 이하여야 합니다");
+        }
+    }
+
 }
