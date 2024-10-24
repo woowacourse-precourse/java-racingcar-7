755c84ed13c697f3e5544de96e2a1c96bf99a0d5
Hanjjong <tim4974@naver.com>
2024-10-24 15:55:30 +0900

    feat(Validator): 분리된 이름이 비어있는지 확인하는 기능 구현

---
+++ src/main/java/racingcar/util/Validator.java
@@ -0,0 +1,13 @@
+package racingcar.util;
+
+import java.util.List;
+
+public class Validator {
+    public void carNameValidate(List<String> carNames) {
+        for (String carName : carNames) {
+            if (carName == null || carName.isBlank()) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+}
