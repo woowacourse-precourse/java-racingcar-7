65d4ab5f03be6791d0856f32464119c3d4b14105
5ms <oms010414@gmail.com>
2024-10-25 01:44:59 +0900

    main함수 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,22 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<String> userList = IOController.userListInput();
+        String tryCount = IOController.tryCountInput();
 
+        Validator validator = new Validator();
 
+        validator.validateUsernames(userList);
+        validator.isDigit(tryCount);
+
+        IOController.printState();
+        Race race = new Race(userList, Integer.parseInt(tryCount));
+        race.raceStart();
+
+        IOController.printFinalWinner(race.getRaceWinners());
     }
 }
