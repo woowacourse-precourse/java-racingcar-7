0f2a68356c26a2cf1f244ca006e0854c608e647b
정세희 <jsh14@MacBook-Air.local>
2024-10-24 20:28:34 +0900

    feat(Application.java): 입력된 자동차 이름 검증 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,15 +8,23 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
+        // 자동차 이름 입력
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputCarName = Console.readLine();
+
+        // 입력된 자동차 이름 저장 후 검증
+        String[] names = inputCarName.split(",");
+        for (String name : names) {
+            if (name.isEmpty() || name.length() > 5) {
+                throw new IllegalArgumentException("[ERROR] 이름의 길이는 1부터 5까지만 가능합니다.");
+            }
+        }
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String inputCount = Console.readLine();
 
         System.out.println(inputCarName + " " + inputCount);
 
-        String[] names = inputCount.split(",");
         List<Car> cars = new ArrayList<>();
         for (String name : names) {
             cars.add(new Car(name));
