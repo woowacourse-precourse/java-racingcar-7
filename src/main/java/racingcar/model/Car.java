892dd6ef8365028536e46fe688ec2ad1294bf367
inha0319@naver.com <inha031913@gmail.com>
2024-10-24 10:53:05 +0900

    [feat] Car 클래스 구현
    - 자동차 이름과 위치를 관리하는 Car 클래스 구현
    - 자동차 이름은 5자 이하로 제한
    - 전진 여부에 따라 자동차 위치를 증가시키는 race 메소드 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,28 @@
 package racingcar.model;
 
 public class Car {
+    private String name;
+    private int location;
+
+    public Car(String name){
+        if (name.length() > 5) {
+            throw new IllegalStateException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+        this.name = name;
+        this.location = 0;
+    }
+
+    public void race(boolean carRace){
+        if (carRace) {
+            location++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLocation() {
+        return location;
+    }
 }
