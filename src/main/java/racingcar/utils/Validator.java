f4d4e783f781ea98be4a0531fed392ba171dbb2e
sc@t0a14 <gusdn6671@gmail.com>
2024-10-25 00:06:08 +0900

    refactor(Validator): 자동차들의 최대 이름의 범위를 수정

--- src/main/java/racingcar/utils/Validator.java
+++ src/main/java/racingcar/utils/Validator.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import racingcar.model.Car;
-import racingcar.model.Cars;
 
 public class Validator {
     public static void isEmpty(String name) {
@@ -14,17 +13,18 @@ public class Validator {
     }
 
     public static void isCarNameRange(String name){
-        if(name.length() >= 5){
+        if(name.length() > 5){
             throw new IllegalArgumentException("자동차의 이름은 5글자 이하만 가능합니다");
         }
     }
 
-    public static void containDuplicate(List<Car> cars) {
-        Set<Car> uniqueCars = new HashSet<>(cars);
-        if(uniqueCars.size() != cars.size()){
-            throw new IllegalArgumentException("자동차의 이름은 중복될수 없습니다");
+    public static void containDuplicate(List<String> names){
+        Set<String> uniqueCars = new HashSet<>(names);
+        if(uniqueCars.size() != names.size()){
+            throw new IllegalArgumentException("자동차 이름은 중복될수 없습니다.");
         }
     }
+
 
 
 }
