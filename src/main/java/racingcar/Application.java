16319130d1706d0d081b89d3a9f253b27433e9a4
박규영 <magic9282m@gmail.com>
2024-10-25 04:54:33 +0900

    feat: add new method carNameSplitter
    carNameSplitter 메서드를 Application 에 추가함
    - 입력된 input 문자열을 쉼표(,)를 기준으로 split 한 뒤 String 배열에 담아 반환함
    - 만약 split 한 문자열 중 하나라도 길이가 5 초과인게 있다면 IllegalArgumentException 을 발생시킨다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
 public class Application {
+    static String[] cars;
+    static int[] carsPosition;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+
+    public static String[] carNameSplitter(String input) {
+        String[] result = input.split(",");
+        for (String s : result) {
+            if (s.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 한다.");
+            }
+        }
+        return result;
     }
 }
