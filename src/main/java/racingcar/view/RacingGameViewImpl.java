216f69138b68d3363cba3f76ee8661f9dca3718f
LSM <leesung2925@gmail.com>
2024-10-24 21:19:52 +0900

    feat: 자동차 이름과 시도 횟수 입력 모듈 구현

--- src/main/java/racingcar/view/RacingGameViewImpl.java
+++ src/main/java/racingcar/view/RacingGameViewImpl.java
@@ -1,14 +1,18 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class RacingGameViewImpl implements RacingGameView{
     @Override
     public String getCarNames() {
-        return null;
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
     }
 
     @Override
     public String getTrialNum() {
-        return null;
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Console.readLine();
     }
 
     @Override
