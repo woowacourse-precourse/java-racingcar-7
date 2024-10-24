7138ddc17b87dc734d4bd94f7cadcf8fdb203be6
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-10-24 19:28:45 +0900

    feat: 자동차 생성하기
    -입력받은 수만큼 자동차 저장하기
    StringTokenizer를 활용하여 ',' 로 자동차 이름을 구분하여 토큰 수만큼 cars 라는 List에 저장한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,25 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String str = Console.readLine();
+
+        StringTokenizer st = new StringTokenizer(str,",");
+
+        List<String> cars = new ArrayList<>();
+
+        for (int i = 0; i < st.countTokens(); i++) {
+            cars.add(st.nextToken());
+        }
 
     }
 }
