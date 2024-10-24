8ccb1986ecc98643abb4a5f5e2328b7372fc38d4
Kim Kyeongho <sgn07124@gmail.com>
2024-10-24 22:41:00 +0900

    feat(TryCountValidator): 경주 시도 횟수를 입력하지 않은 경우에 예외 추가
    - 경주 시도 횟수를 입력하지 않았거나 공백을 입력한 경우, IllegalArgumentException를 반환하는 예외 추가

--- src/main/java/racingcar/validation/TryCountValidator.java
+++ src/main/java/racingcar/validation/TryCountValidator.java
@@ -4,6 +4,7 @@ import racingcar.enums.ErrorMessage;
 
 public class TryCountValidator {
     public static void validate(String input) {
+        validateNull(input);
         validateTryCountRange(input);
     }
 
@@ -11,6 +12,12 @@ public class TryCountValidator {
         int tryCount = Integer.parseInt(input);
         if (tryCount < 1) {
             throw new IllegalArgumentException(ErrorMessage.TRY_COUNT_UNDER_ONE.getMessage());
+        }
+    }
+
+    private static void validateNull(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_ALLOW_TRY_COUNT_BLANK.getMessage());
         }
     }
 }
