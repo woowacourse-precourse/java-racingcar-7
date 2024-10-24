aad68661c9fefc2e869d890bd47d56667323c99e
jseom <2ujs4311@gmail.com>
2024-10-24 20:09:22 +0900

    REFACTOR: Cars 파일 변경
    자동차 경주 점수판을 Race 파일에서 관리하기 위해 파일을 변경했습니다.
    기존에는 LinkedHashMap<차 이름, 점수>로 저장했지만 현재는 자동차 이름만을 담은 List형 필드가 존재합니다.

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -1,43 +1,42 @@
 package racingcar;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.List;
 
 public class Cars {
 
-    private final Map<String, Integer> cars;
+    private final List<String> cars;
     static final int INCLUDE_LAST_EMPTY_STRING = -1;
     static final int DEFAULT_MOVE_POINT = 0;
 
     public Cars(String carNameString) {
-        Map<String, Integer> cars = new LinkedHashMap<>();
+//        Map<String, Integer> cars = new LinkedHashMap<>();
 
-        String[] carNameArray = carNameString.split(",", INCLUDE_LAST_EMPTY_STRING);
-        for (String carName : carNameArray) {
-            cars.put(carName, DEFAULT_MOVE_POINT);
-        }
+        List<String> carNameArray = List.of(carNameString.split(",", INCLUDE_LAST_EMPTY_STRING));
+//        for (String carName : carNameArray) {
+//            cars.put(carName, DEFAULT_MOVE_POINT);
+//        }
 
-        validateWrongValue(cars);
-        validateNameLength(cars);
+        validateWrongValue(carNameArray);
+        validateNameLength(carNameArray);
 
-        this.cars = cars;
+        this.cars = carNameArray;
     }
 
-    public Map<String, Integer> getCars() {
+    public List<String> getCars() {
         return cars;
     }
 
-    private void validateNameLength(Map<String, Integer> cars) {
-        for (String carName : cars.keySet()) {
+    private void validateNameLength(List<String> cars) {
+        for (String carName : cars) {
             if (carName.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름이 5자를 초과했습니다.");
             }
         }
     }
 
-    private void validateWrongValue(Map<String, Integer> cars) {
-        if (cars.containsKey("")) {
-            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
+    private void validateWrongValue(List<String> cars) {
+        if (cars.contains("")) {
+            throw new IllegalArgumentException("잘못된 값이 입력됐습니다.");
         }
     }
 }
