238c57261cb9dba8e8b3fdb9acc8c36bd8434485
FhRh <yiyop@naver.com>
2024-10-24 17:45:59 +0900

    refactor(Car) : 자동차의 이름은 변경되지 않는다

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,8 +1,8 @@
 package racingcar;
 
 public class Car {
+    private final String name;
     private int position = 0;
-    private String name;
 
     public Car(String name) {
         this.name = name;
