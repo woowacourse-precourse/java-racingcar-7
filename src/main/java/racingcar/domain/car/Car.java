8f177e6ca140f20d47432e189a4c1f758af740bc
tentenball <lth8905@gmail.com>
2024-10-24 22:22:58 +0900

    feat: 자동차 생성 클래스 작성
    * Car: 자동차이름을 저장하고 있는 클래스 생성
    * Cars: Car의 list를 담고 있는 클래스 생성

---
+++ src/main/java/racingcar/domain/car/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain.car;
+
+import racingcar.domain.name.Name;
+
+public class Car {
+	private final Name name;
+
+	public Car(Name name) {
+		this.name = name;
+	}
+}
---
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -0,0 +1,11 @@
+package racingcar.domain.car;
+
+import java.util.List;
+
+public class Cars {
+	private final List<Car> cars;
+
+	public Cars(List<Car> cars) {
+		this.cars = cars;
+	}
+}
