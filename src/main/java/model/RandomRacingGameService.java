b44ccbb2e4401f798aa9c633747ab171b30f664f
gikhoon <ghikhoon@naver.com>
2024-10-25 02:06:13 +0900

    feat(view): 레이스 중간 결과 출력 메소드 구현

--- src/main/java/model/RandomRacingGameService.java
+++ src/main/java/model/RandomRacingGameService.java
@@ -1,17 +1,25 @@
 package model;
 
 import java.util.List;
+import view.RacingCarView;
 
 public class RandomRacingGameService implements RacingGameService {
     private static final int RANDOM_MIN = 0;
     private static final int RANDOM_MAX = 9;
     private static final int FORWARD_THRESHOLD = 4;
 
+    private final RacingCarView racingCarView;
+
+    public RandomRacingGameService() {
+        this.racingCarView = new RacingCarView();
+    }
+
     @Override
-    public void progress(List<RacingCar> cars, long round) {
-        long currentRound = 1;
+    public void progress(List<RacingCar> cars, int round) {
+        int currentRound = 1;
         while (currentRound < round) {
             race(cars);
+            racingCarView.printIntermediateResult(cars);
             currentRound++;
         }
     }
--- src/main/java/view/RacingCarView.java
+++ src/main/java/view/RacingCarView.java
@@ -1,10 +1,15 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import model.RacingCar;
 
 public class RacingCarView {
     private static final String INPUT_RACING_CAR_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_RACING_ROUND_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String DISTANCE_OUTPUT_TEXT = "-";
+    private static final String LINE_SEPARATOR = "\n";
+    private static final String PRINT_INTERMEDIATE_RESULT_FORMAT = "%s : %s";
 
     public String readCars() {
         System.out.println(INPUT_RACING_CAR_MESSAGE);
@@ -18,5 +23,16 @@ public class RacingCarView {
 
     public void closeInput() {
         Console.close();
+    }
+
+    public void printIntermediateResult(List<RacingCar> cars) {
+        StringBuilder result = new StringBuilder(LINE_SEPARATOR);
+        for (RacingCar car : cars) {
+            String dashes = DISTANCE_OUTPUT_TEXT.repeat(car.getDistance());
+            result.append(String.format(PRINT_INTERMEDIATE_RESULT_FORMAT, car.getName(), dashes))
+                    .append(LINE_SEPARATOR);
+        }
+
+        System.out.print(result);
     }
 }
