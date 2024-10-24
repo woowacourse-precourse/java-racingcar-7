ab7537fe58c57c3a1fc19aabb2011658d925d4cc
gikhoon <ghikhoon@naver.com>
2024-10-24 22:45:13 +0900

    feat(view): 레이싱 참여 차량 입력 메소드 구현

--- src/main/java/controller/RacingCarController.java
+++ src/main/java/controller/RacingCarController.java
@@ -1,7 +1,11 @@
 package controller;
 
+import view.RacingCarView;
+
 public class RacingCarController {
 
     public void run() {
+        RacingCarView racingCarView = new RacingCarView();
+        String cars = racingCarView.readCars();
     }
 }
--- src/main/java/view/RacingCarView.java
+++ src/main/java/view/RacingCarView.java
@@ -1,4 +1,12 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class RacingCarView {
+    private static final String INPUT_RACING_CAR_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public String readCars() {
+        System.out.println(INPUT_RACING_CAR_MESSAGE);
+        return Console.readLine();
+    }
 }
