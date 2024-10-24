c2556a09c41a68c3ae049cc8e273b4431c3462a7
KKdohyeong <dohyeong254@naver.com>
2024-10-25 03:35:29 +0900

    feat(RacingConstant): 경주 게임 관련 상수 설정
    자동차 전진 관련 숫자들 설정
    자동차 이름의 사이즈 기준치를 설정

---
+++ src/main/java/racingcar/constants/RacingConstant.java
@@ -0,0 +1,10 @@
+package racingcar.constants;
+
+public class RacingConstant {
+
+    public static final int RANDOM_MIN_NUMBER = 0;
+    public static final int RANDOM_MAX_NUMBER = 9;
+    public static final int MOVE_STANDARD_NUMBER = 4;
+
+    public static final int NAME_SIZE_LIMIT = 5;
+}
