5d0c05bc0c0ce60e4874126ef65aa3942c695bc0
fpem3309 <fpem3309@gmail.com>
2024-10-24 16:44:18 +0900

    feat: 자동자 이름이 비어있거나 5자 초과시 예외 발생

--- src/main/java/racingcar/Vehicle.java
+++ src/main/java/racingcar/Vehicle.java
@@ -1,10 +1,17 @@
 package racingcar;
 
 public class Vehicle {
+    private static final int MAX_NAME_LENGTH = 5;
     private String vehicleName;
     private int victoryCount;
 
     public Vehicle(String vehicleName) {
+        if (vehicleName.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
+        }
+        if (vehicleName.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5글자를 초과할 수 없습니다.");
+        }
         this.vehicleName = vehicleName;
     }
 }
