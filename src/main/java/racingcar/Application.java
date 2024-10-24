6010c8507db8aa3dd277483f69e780537e5202ed
zoooooz2 <92962527+zoooooz2@users.noreply.github.com>
2024-10-24 21:53:56 +0900

    feat(Application) : 자동차 이름 입력 로직 & 예외처리 로직 구현
    - 사용자에게 자동차 이름을 입력받는 로직 구현
    - 예외처리 (자동차 이름 5자 이하, 자동차 개수 10개 이하, 입력이 null)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        String carInput = Console.readLine();
+
+        if (carInput == null) {
+            throw new IllegalArgumentException("자동차 이름을 입력해주세요.\n");
+        }
+
+        String[] cars = carInput.split(",", -1);
+
+        for (String car : cars) {
+            car = car.trim();
+
+            if (car.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력해주세요.\n");
+            }
+        }
+        if (cars.length > 10) {
+            throw new IllegalArgumentException("자동차는 10개 이하로 입력해 주세요.\n");
+        }
     }
 }
