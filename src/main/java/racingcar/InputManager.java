7548c3ba6ae64df2b6533f197597cb6e8f42d1de
joonwan <joonwan1024@naver.com>
2024-10-24 17:12:19 +0900

    feat: 사용자에게 입력받은 문자열을 반환하는 기능 구현

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -1,4 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputManager {
+
+    public static String readUserInput() {
+        return Console.readLine();
+    }
+
+    public static void close() {
+        Console.close();
+    }
 }
