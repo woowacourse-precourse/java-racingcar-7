1448ca01a3431f0b81ddfb9f825745c699319317
chaerish <ccyy8432@naver.com>
2024-10-24 19:22:39 +0900

    feat(InputMessage): InputMessage Enum 생성
    InputMessage를 담는 Enum 을 생성한다.

---
+++ src/main/java/racingcar/InputMessage.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+public enum InputMessage {
+    CAR_NAME_INPUT_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    TRY_NUM_INPUT_MESSAGE("시도할 횟수는 몇 회인가요?");
+
+    private final String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    InputMessage(String message) {
+        this.message = message;
+    }
+}
