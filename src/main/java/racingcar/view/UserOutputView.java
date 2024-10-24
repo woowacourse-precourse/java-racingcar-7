4ffb5af68c4140183155baa1616c359cd5227385
nns503 <73779581+nns503@users.noreply.github.com>
2024-10-24 15:14:00 +0900

    feat: 시작 출력 및 사용자 입력 메서드 생성

--- src/main/java/racingcar/view/UserOutputView.java
+++ src/main/java/racingcar/view/UserOutputView.java
@@ -1,4 +1,12 @@
 package racingcar.view;
 
 public class UserOutputView {
+
+    public static void InputCarNameMessage(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void InputAttemptCountMessage(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
 }
