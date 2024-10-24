84842ec0d1ae686409d2abff344e383f616a33f9
Hyun jinyong <ekwls20@gmail.com>
2024-10-24 17:06:16 +0900

    test(model) : 기능 테스트 코드 수정
    기존 Cars 클래스를 Car 클래스와 CarFactory 클래스로 분할하였다. 이에 따라 올바른 테스트를 위해 해당 코드를 그에 맞게 수정하였다.

---
+++ src/test/java/racingcar/model/CarFactoryTest.java
@@ -0,0 +1,35 @@
+package racingcar.model;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CarFactoryTest {
+
+  @ParameterizedTest
+  @ValueSource(strings = {"con@kim,poi", "pobi,,yongE" , "pobi,.yongE"})
+  void 자동차이름_파라미터를_구분자_기준으로_검사한다(String input) {
+    Assertions.assertThatThrownBy(() -> new CarFactory().extractCarNames(input))
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"pobi", "pobi,java,java", ""})
+  void 자동차이름을_검사한다(String input) {
+    Assertions.assertThatThrownBy(() -> new CarFactory().extractCarNames(input))
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"pobi,yongE"})
+  void 경주에_참가하는_자동차의_갯수가_올바른지_확인한다(String input) {
+    // given
+    CarFactory carFactory = new CarFactory();
+    List<Car> cars = carFactory.extractCarNames(input);
+    // when
+    List<String> carNames = cars.stream().map(Car::getCarName).toList();
+    // then
+    Assertions.assertThat(carNames).isEqualTo(List.of("pobi", "yongE"));
+  }
+}
---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,19 @@
+package racingcar.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CarTest {
+
+  @ParameterizedTest
+  @ValueSource(ints = {4, 5, 6})
+  void 자동차가_전진하면_MOVECOUNT는_0이_아니어야_한다(int giveNumber) {
+    // given
+    Car car = new Car("yongE");
+    // when
+    car.move(giveNumber);
+    // then
+    Assertions.assertThat(car.getMoveCount()).isNotEqualTo(0);
+  }
+}
