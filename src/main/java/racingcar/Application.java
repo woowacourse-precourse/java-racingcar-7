473ae337cfe29c9d8d977c8f946699107c29d10a
Yong U Cho <yongu2000@naver.com>
2024-10-24 15:52:12 +0900

    feat: 메인함수 통합 실행 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,12 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game(User.inputCarName());
+        int gameRepeats = User.inputGameRepeat();
+
+        for (int i = 0; i < gameRepeats; i++) {
+            game.moveCar();
+            game.printProgress();
+        }
     }
 }
