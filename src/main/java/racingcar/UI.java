d3b1c6c039c8e8fe2e8a324568bea787958a81d8
making-a-scene <4936joo@naver.com>
2024-10-24 15:14:07 +0900

    feat(UI.getCarNames()): 자동차 이름을 입력받는 기능 구현

---
+++ src/main/java/racingcar/UI.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+import racingcar.Validation;
+import camp.nextstep.edu.missionutils.Console;
+
+public class UI {
+    private final Validation validation = new Validation();
+    private final String GET_CAR_NAME_STATEMENT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public String[] getCarNames() {
+        System.out.println(GET_CAR_NAME_STATEMENT);
+        String input = Console.readLine();
+        return validation.validateCarName(input);
+    }
+}
