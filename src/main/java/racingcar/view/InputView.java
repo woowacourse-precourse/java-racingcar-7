b58b8c6966765b533c7207caf2a2efbe15ccaa0b
binn <anyubin0724@naver.com>
2024-10-24 17:50:44 +0900

    fix: 예외 처리 추가
    - 시도 횟수에 대한 예외처리 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,6 +2,8 @@ package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import racingcar.exceptions.ValidateError;
+
 public class InputView {
     public static String inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -10,6 +12,8 @@ public class InputView {
 
     public static int inputTryNumber() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        return Integer.parseInt(readLine());
+        String input = readLine();
+        ValidateError.validateTryNumber(input);
+        return Integer.parseInt(input);
     }
 }
