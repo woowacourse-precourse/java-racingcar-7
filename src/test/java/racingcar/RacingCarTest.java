a3c2d687fa35056ab8726b96bd661df27c1a8338
GaBaljaintheroom <junsu1222@naver.com>
2024-10-24 12:38:04 +0900

    test(RacingCarTest): 자동차 이름 길이 검증 테스트 작성

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,10 +1,14 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class RacingCarTest {
 
@@ -45,6 +49,20 @@ class RacingCarTest {
 
         //then
         assertThat(racingCar.getPosition()).isEqualTo(position + 1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "12", "123", "1234", "12345"})
+    void 레이상카의_이름은_1에서_5_글자_사이이다(String name) {
+        assertDoesNotThrow(() -> RacingCar.from(name));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "123456"})
+    void 레이상카의_옳지_않은_이름_길이는_예외가_발생한다(String name) {
+        assertThatThrownBy(() -> RacingCar.from(name))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("자동차 이름은 1~5글자입니다.");
     }
 
 }
