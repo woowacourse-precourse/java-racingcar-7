e043a5626981bf37ba75ccf7f6db9745d2077182
reindeer002 <lsj000424@gmail.com>
2024-10-24 18:28:40 +0900

    refactor: Remove unused constant and add INCLUSIVE_RANGE
    - Remove constant that is not in use to clean up the codebase.
    - Add INCLUSIVE_RANGE constant to define the inclusive range for random number generation in the racing logic.

--- src/main/java/racingcar/common/constant/Constant.java
+++ src/main/java/racingcar/common/constant/Constant.java
@@ -3,9 +3,8 @@ package racingcar.common.constant;
 import racingcar.common.wrapper.Range;
 
 public class Constant {
-    public static final Range ROUND_VALUE_RANGE = Range.of(0, 2147483647);
+    public static final Range INCLUSIVE_RANGE = Range.of(0, 9);
     public static final Range MOVE_FORWARD_RANGE = Range.of(4, 9);
-    public static final Range STOP_RANGE = Range.of(0, 3);
     public static final Integer MAX_CAR_NAME_LENGTH = 5;
     public static final Integer DEFAULT_MOVE_DISTANCE = 1;
     public static final String DELIMITER = ",";
