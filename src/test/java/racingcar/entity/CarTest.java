a3c065c0eb6cbcdc9e3e6fe5fe8d0f8f4001a54e
yeong-hwan <jghff700@naver.com>
2024-10-25 07:40:22 +0900

    test(entity): append validation test when car name length exceed 5

--- src/test/java/racingcar/entity/CarTest.java
+++ src/test/java/racingcar/entity/CarTest.java
@@ -15,4 +15,11 @@ public class CarTest {
         assertThat(car.getPosition()).isEqualTo(1);
         assertThat(car.getPositionDisplay()).isEqualTo("-");
     }
+
+    @Test
+    void 글자수_5자_초과() {
+        assertThatThrownBy(() -> new Car("longerThanFive"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+    }
 }
