1c750776f4449cd7cc8fcba486acd0c831da0647
eusan <goodsan0330@gamil.com>
2024-10-24 15:33:27 +0900

    feat(car): create Car objects from comma-separated names and validate name length

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,27 +3,28 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
-        String carNames = Console.readLine();
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		String carNames = Console.readLine();
 
-        Car[] cars = separateNames(carNames);
+		Car[] cars = separateNames(carNames);
 
+		/*
+		 * 이동 시작 ~
+		 */
 
+	}
 
+	public static Car[] separateNames(String carNames) {
 
-    }
+		String[] names = carNames.split(",");
+		Car[] cars = new Car[names.length];
 
-    public static Car[] separateNames(String carNames) {
+		for (int i = 0; i < names.length; i++) {
+			cars[i] = new Car(names[i]);
+		}
 
-        String[] names = carNames.split(",");
-        Car[] cars = new Car[names.length];
-
-        /*
-         * 자동차 이름에 대해 자동차 객체 생성
-         */
-
-        return cars;
-    }
+		return cars;
+	}
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,9 +5,28 @@ public class Car {
 	private String name;
 	private int moveCounter;
 
-	public Car(String name) {
+	public String getName() {
+		return name;
+	}
+
+	public int getMoveCounter() {
+		return moveCounter;
+	}
+
+	public Car(String name) throws IllegalArgumentException {
+
+		validateName(name);
+
 		this.name = name;
 		this.moveCounter = 0;
 	}
 
+	private void validateName(String name) throws IllegalArgumentException {
+
+		String trimmedName = name.trim();
+
+		if (trimmedName.length() > 5) {
+			throw new IllegalArgumentException("차량의 이름은 5자 이하만 가능합니다.");
+		}
+	}
 }
