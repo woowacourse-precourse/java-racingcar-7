b7559795f28998de5c74a0ad670778c5fa259c80
jungsu <vwx1@naver.com>
2024-10-24 16:25:58 +0900

    feat: make a function to print results and winners
    - Added printRoundResult to display each round's results.
    - Added printCarResult to display individual car's progress.
    - Added printWinners to display the final winner(s) of the race.

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,5 +1,37 @@
 package racingcar.view;
 
+import static racingcar.view.Constants.ENTER_MESSAGE;
+import static racingcar.view.Constants.FINAL_RESULT_MESSAGE;
+import static racingcar.view.Constants.INTERMEDIATE_RESULT_MESSAGE;
+
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.view.Constants;
+
 public class ResultView {
 
+  public static void printRoundResult(List<Car> cars) {
+    System.out.println(ENTER_MESSAGE);
+    for (Car car : cars) {
+      printCarResult(car);
+    }
+  }
+
+  private static void printCarResult(Car car) {
+    System.out.printf(INTERMEDIATE_RESULT_MESSAGE, car.getName());
+    for (int i = 0; i < car.getPosition(); i++) {
+      System.out.print("-");
+    }
+    System.out.println();
+  }
+
+  public static void printWinners(List<Car> winners) {
+    if (winners.size() == 1) {
+      System.out.println(FINAL_RESULT_MESSAGE + winners.get(0).getName());
+    } else {
+      String winnerNames = String.join(", ", winners.stream().map(Car::getName).toArray(String[]::new));
+      System.out.println(FINAL_RESULT_MESSAGE + winnerNames);
+    }
+  }
 }
+
