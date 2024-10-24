9bb98fef6fca10fcfe6ef8297749f65a6193ffa7
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-25 02:23:02 +0900

    feat($OutputConsole): 매개변수 없는 println 함수 추가
    - 단순 줄바꿈을 위함.

--- src/main/java/racingcar/util/io/OutputConsole.java
+++ src/main/java/racingcar/util/io/OutputConsole.java
@@ -5,4 +5,8 @@ public final class OutputConsole {
     public static void println(String message) {
         System.out.println(message);
     }
+
+    public static void println() {
+        System.out.println();
+    }
 }
