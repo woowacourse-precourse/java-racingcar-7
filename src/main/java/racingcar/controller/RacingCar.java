7920e77c9ce53096f8f4c52c293541d47bee110f
sc@t0a14 <gusdn6671@gmail.com>
2024-10-25 00:37:09 +0900

    feat(RacingCar): 차들의 실행 결과 출력한다

--- src/main/java/racingcar/controller/RacingCar.java
+++ src/main/java/racingcar/controller/RacingCar.java
@@ -20,13 +20,20 @@ public class RacingCar {
             carNames.add(new Car(carInputs[i]));
         }
 
-        Cars cars = new Cars(carNames);
         OutputView.printAttemptCountPrompt();
 
         int tryCount = Integer.parseInt(inputView.input());
+        Cars cars = new Cars(carNames);
 
         for (int i = 0; i < tryCount; i++) {
-            cars.moveAll();
+            for (Car carName : carNames) {
+                carName.move();
+            }
+
+            for (Car carName : carNames) {
+                System.out.print(carName.getName() + " : ");
+                System.out.println("-".repeat(carName.getPosition()));
+            }
         }
 
 
