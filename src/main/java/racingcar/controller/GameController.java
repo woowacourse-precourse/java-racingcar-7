99a5b9db7b548df5e1272cc89eaeaba983a2783d
Kiki1875b <kevinheo0413@gmail.com>
2024-10-24 10:44:25 +0900

    feat(CarNameValidator): add feature to validate empty or null

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,15 @@
+package racingcar.controller;
+
+import racingcar.view.InputView;
+public class GameController {
+
+    public void run(){
+        String userCarInput = InputView.userInputCar();
+
+        // validate
+
+        // parse
+
+        String userTrialInput = InputView.userInputTrial();
+    }
+}
---
+++ src/main/java/racingcar/model/validator/CarNameValidator.java
@@ -0,0 +1,33 @@
+package racingcar.model.validator;
+
+public class CarNameValidator {
+
+    String input;
+    public void validateCarNames(String input){
+
+        setInput(input);
+
+        checkEmpty();
+        checkNull();
+        
+    }
+
+    private void setInput(String input){
+        this.input = input;
+    }
+
+    private void checkEmpty(){
+
+        input = input.trim();
+        if(input.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkNull(){
+
+        if(input == null){
+            throw new IllegalArgumentException();
+        }
+    }
+}
