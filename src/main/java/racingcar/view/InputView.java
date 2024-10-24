e5c0d7bf1b8aabdd4a774ca773301b7bfc4343ab
woo <ldw28517@naver.com>
2024-10-24 22:27:41 +0900

    refactor(InputView): View에서 입력값 검증하도록 수정
    - 입력에 관한 역할이니 검증의 역할을 맡아도 된다고 판단
    - 추가로 라인 별 검증이 들어가야하는데, 모든 입력을 밭고 검증하는 것보다 즉시 검증하고 예외를 반환하는게 적합하다고 판단

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,10 +1,20 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.Car;
+import racingcar.model.InputExtractor;
+import racingcar.model.InputValidator;
+
+import java.util.List;
 
 public class InputView {
     private final static String REQUEST_MESSAGE_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final static String REQUEST_MESSAGE_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+    private final InputValidator inputValidator;
+
+    public InputView(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
 
     public void printCarNameRequestMessage() {
         System.out.println(REQUEST_MESSAGE_CAR_NAME);
@@ -14,11 +24,15 @@ public class InputView {
         System.out.println(REQUEST_MESSAGE_TRY_COUNT);
     }
 
-    public String getCarName() {
-        return Console.readLine();
+    public String getCarNames() {
+        String carNames = Console.readLine();
+        inputValidator.validateCarNames(carNames);
+        return carNames;
     }
 
     public String getTryCnt() {
-        return Console.readLine();
+        String tryCount = Console.readLine();
+        inputValidator.validateTryCounts(tryCount);
+        return tryCount;
     }
 }
