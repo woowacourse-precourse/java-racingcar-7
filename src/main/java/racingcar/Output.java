19ea716e4546dc66258777f188695aabe29c9b1a
eunho <eunho1032@gmail.com>
2024-10-24 13:20:27 +0900

    refactor(Output): randomizeRace 메서드 제거 및 메서드명 변경

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -1,6 +1,5 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Output {
@@ -8,26 +7,15 @@ public class Output {
 	public static void printResult(List<Car> cars, int tryCounts) {
 		System.out.println("실행 결과");
 		for (int i = 0; i < tryCounts; i++) {
-			randomizeRace(cars);
-			printRace(cars);
+			printMovement(cars);
 		}
 		printWinner(Winner.contain(cars));
 	}
 
 	// 메서드 구분 필요
-	public static void randomizeRace(List<Car> cars) {
-		int randomNum;
-
+	public static void printMovement(List<Car> cars) {
 		for (Car car : cars) {
-			randomNum = Randoms.pickNumberInRange(0, 9);
-			if (randomNum >= 4) {
-				car.distance++;
-			}
-		}
-	}
-
-	public static void printRace(List<Car> cars) {
-		for (Car car : cars) {
+			car.drive();
 			System.out.println(car.name + " : " + displayDistance(car));
 		}
 		System.out.println();
@@ -45,7 +33,7 @@ public class Output {
 		System.out.print("최종 우승자 : ");
 
 		if (winners.size() == 1) {
-			System.out.print(winners.get(0).name);
+			System.out.print(winners.getFirst().name);
 			return;
 		}
 
