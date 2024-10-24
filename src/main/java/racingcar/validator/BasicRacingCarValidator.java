1b1a5835d44055b046239242bf9fb363404781c2
changuii <rhljh7410@gmail.com>
2024-10-24 17:06:53 +0900

    feat(RacingCarValidator) : 3. 자동차 이름과 이동 횟수를 검증한다.
    - RacingCarValidator : 자동차 경주 게임의 값들을 검증하는 validator를 추상화한 인터페이스
    - BasicRacingCarValidator : 우테코에서 주어진 미션 요구사항을 검증하는 validator 구현체

---
+++ src/main/java/racingcar/validator/BasicRacingCarValidator.java
@@ -0,0 +1,10 @@
+package racingcar.validator;
+
+import java.util.List;
+
+public class BasicRacingCarValidator implements RacingCarValidator {
+    @Override
+    public void validateCarNamesAndMoveCount(List<String> carNames, int MoveCount) {
+
+    }
+}
---
+++ src/main/java/racingcar/validator/RacingCarValidator.java
@@ -0,0 +1,7 @@
+package racingcar.validator;
+
+import java.util.List;
+
+public interface RacingCarValidator {
+    public void validateCarNamesAndMoveCount(List<String> carNames, int MoveCount);
+}
