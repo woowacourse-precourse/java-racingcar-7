a5f394ffb5cbfae83d117c910eb872f0b0baa224
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-10-24 16:07:23 +0900

    feat(car): 자동차 이름 길이 예외 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,12 +5,17 @@ public class Car {
     private int distance;
 
     public Car(String name) {
+        validateNameLength(name);
         this.name = name;
         this.distance = 0;
     }
 
     public void increaseDistance(){
         this.distance++;
+    }
+
+    public void validateNameLength(String name){
+        if(name.length() > 5) throw new IllegalArgumentException("자동차 이름은 5자 이하의 문자열만 가능합니다.");
     }
 
     public String toString(){
