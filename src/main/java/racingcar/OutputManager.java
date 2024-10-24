5c01ce6853d5021e32370c4a1d452f4922d0e073
joonwan <joonwan1024@naver.com>
2024-10-24 17:44:15 +0900

    feat: OutputManager 가이드 메시지 출력 기능 구현

--- src/main/java/racingcar/OutputManager.java
+++ src/main/java/racingcar/OutputManager.java
@@ -1,4 +1,10 @@
 package racingcar;
 
 public class OutputManager {
+
+    private static final String GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public static void printGuideMessage() {
+        System.out.println(GUIDE_MESSAGE);
+    }
 }
