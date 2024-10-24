80acfdde01c97d8e6b328d35a0c14298cd8e7069
gikhoon <ghikhoon@naver.com>
2024-10-24 23:28:28 +0900

    feat(racingGame): 레이싱 게임 생성자 구현

--- src/main/java/model/RacingGame.java
+++ src/main/java/model/RacingGame.java
@@ -1,4 +1,13 @@
 package model;
 
+import java.util.List;
+
 public class RacingGame {
+    private long round;
+    private List<RacingCar> cars;
+
+    public RacingGame(List<RacingCar> cars, long round) {
+        this.cars = cars;
+        this.round = round;
+    }
 }
