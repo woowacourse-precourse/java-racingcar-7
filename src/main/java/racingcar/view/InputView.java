9493c6f539cd0504a847fd3b9ccb662bebae3066
Hanjjong <tim4974@naver.com>
2024-10-24 16:14:25 +0900

    refactor(InputView): 자동차 이름 입력 메서드명 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,12 +7,12 @@ public class InputView {
     public InputView() {
     }
 
-    public String input() {
-        printMessage();
+    public String carNamesInput() {
+        printCarNamesInputMessage();
         return Console.readLine();
     }
 
-    private void printMessage() {
+    private void printCarNamesInputMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 }
