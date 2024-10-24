197cb486a98b4764da8c6b3707ee6eebaccfb51e
YeaChan05 <qkenrdl05@gmail.com>
2024-10-25 00:03:41 +0900

    refactor(Application): 애플리케이션 실행 간소화
    애플리케이션 추상화 클래스를 통해 호출 축소

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,25 +1,9 @@
 package racingcar;
 
-import racingcar.domain.Cars;
-import racingcar.endpoint.InputInterface;
-import racingcar.endpoint.OutputInterface;
-import racingcar.service.CarFactory;
+import racingcar.service.ApplicationRunner;
 
-public class Application {
+public class Application extends ApplicationRunner {
     public static void main(String[] args) {
-        String firstLine;
-        String secondLine;
-        try (InputInterface inputInterface=new InputInterface()) {
-            OutputInterface.printMessage(OutputInterface.ENTER_PLAYER_NAME);
-            firstLine = inputInterface.readLine();
-            OutputInterface.printMessage(OutputInterface.ENTER_NUMBER_OF_TRIALS);
-            secondLine = inputInterface.readLine();
-        }
-        CarFactory carFactory = new CarFactory();
-        Cars cars=carFactory.createCars(firstLine);
-        //TODO 2024 10 24 22:38:46 : 선수들을 주입받는 경주 서비스 만들기
-        //TODO 2024 10 24 22:39:12 : 시행 횟수만큼 경주를 실행
-        //TODO 2024 10 24 22:39:25 : 각 시행마다 거리 출력
-        //TODO 2024 10 24 22:42:24 : 경기가 끝나면 우승자 출력
+        run();
     }
 }
