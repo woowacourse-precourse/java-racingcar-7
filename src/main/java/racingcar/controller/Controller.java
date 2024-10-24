491b337498dff938807623b1d5b7182e2d1c437a
choi14123 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-10-24 21:35:20 +0900

    우승자 판단하는 기능 추가, 숫자 4 이상일 때 자동차 움직이는 기능 추가

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,5 +1,6 @@
 package racingcar.controller;
 
+import java.util.List;
 import racingcar.handler.InputHandler;
 import racingcar.handler.PrintHandler;
 import racingcar.model.Car.Cars;
@@ -12,20 +13,24 @@ public class Controller {
     public Controller() {
         this.printHandler = new PrintHandler();
         this.inputHandler = new InputHandler();
-
     }
 
     public void run() {
         printHandler.racingStartMessage();
         String[] tempCarNames = inputHandler.inputCarNames();
-        printHandler.racingNumberMessage();
         Cars cars = new Cars(tempCarNames);
+
+        printHandler.racingNumberMessage();
         int gameNumber = inputHandler.inputGameNumber();
-        int maxCarListSize = cars.getCarListSize();
-        Referee referee = new Referee(cars, maxCarListSize, gameNumber);
-        // 앞으로 구현해야 하는 거
-        // 자동차가 랜덤 숫자가 4 이상 일 때, 앞으로 간다.
-        // 그걸 어떤 방식으로 해야 자동차 이름 별 position 값을 올릴 수 있을까?
-        // position 어디에 위치해야 하는 것일까?
+        Referee referee = new Referee(cars);
+
+        printHandler.racingResultStartMessage();
+        for (int i = 0; i < gameNumber; i++) {
+            referee.playRound();
+            printHandler.printCurrentPositions(referee.getCarNames(), referee.getCarPositions());
+        }
+
+        List<String> winners = referee.getWinners();
+        printHandler.printWinners(winners);
     }
 }
--- src/main/java/racingcar/model/Referee.java
+++ src/main/java/racingcar/model/Referee.java
@@ -1,19 +1,64 @@
 package racingcar.model;
 
-import java.util.HashMap;
-import java.util.Map;
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.model.Car.Cars;
 
-public class Referee {
-    private final Map<String, Integer> refereeList = new HashMap<>();
+import java.util.ArrayList;
+import java.util.List;
 
-    public Referee(Cars cars, int maxCarListSize, int gameNumber) {
-      convertToRefereeList(cars, maxCarListSize);
+public class Referee {
+    private final int RANDOM_NUMBER_MIN_SIZE = 0;
+    private final int RANDOM_NUMBER_MAX_SIZE = 9;
+    private final int FORWARD_MOVE_LIMIT = 4;
+
+    private final Cars cars;
+
+    public Referee(Cars cars) {
+        this.cars = cars;
     }
 
-    private void convertToRefereeList(Cars cars, int maxCarListSize) {
-        for (int i = 0; i < maxCarListSize; i++) {
-            refereeList.put(cars.getCarListValue(i), 0);
+    public boolean isForwardAllowed() {
+        int stateNumber = Randoms.pickNumberInRange(RANDOM_NUMBER_MIN_SIZE, RANDOM_NUMBER_MAX_SIZE);
+        return stateNumber >= FORWARD_MOVE_LIMIT;
+    }
+
+    public void playRound() {
+        for (int i = 0; i < cars.getCarListSize(); i++) {
+            if (isForwardAllowed()) {
+                cars.getCar(i).moveForward();
+            }
         }
+    }
+
+    public List<String> getCarNames() {
+        List<String> carNames = new ArrayList<>();
+        for (int i = 0; i < cars.getCarListSize(); i++) {
+            carNames.add(cars.getCarListValue(i));
+        }
+        return carNames;
+    }
+
+    public List<Integer> getCarPositions() {
+        List<Integer> positions = new ArrayList<>();
+        for (int i = 0; i < cars.getCarListSize(); i++) {
+            positions.add(cars.getCar(i).getPosition());
+        }
+        return positions;
+    }
+
+    public List<String> getWinners() {
+        int maxPosition = 0;
+        for (int i = 0; i < cars.getCarListSize(); i++) {
+            if (cars.getCar(i).getPosition() > maxPosition) {
+                maxPosition = cars.getCar(i).getPosition();
+            }
+        }
+        List<String> winners = new ArrayList<>();
+        for (int i = 0; i < cars.getCarListSize(); i++) {
+            if (cars.getCar(i).getPosition() == maxPosition) {
+                winners.add(cars.getCar(i).getCarName());
+            }
+        }
+        return winners;
     }
 }
