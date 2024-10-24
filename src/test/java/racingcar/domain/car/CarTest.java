16f8d6ff4f26d02fb002cf7b1c93161589512b00
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-24 22:35:03 +0900

    refactor: CarTest 테스트 형식 변경
    given when then 형식으로 변경

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -12,21 +12,39 @@ class CarTest {
     @Test
     @DisplayName("자동차 저장 및 조회 테스트")
     void carBasicTest() {
+        //given
         Car car = new Car("Car");
+
+        //when
+
+        //then
         assertThat(car.getCarName()).isEqualTo("Car");
         assertThat(car.getDistance()).isEqualTo(0);
     }
 
     @Test
-    @DisplayName("자동차 이동 테스트")
-    void  carGoTest() {
-        Car car1 = new Car("Car1");
-        Car car2 = new Car("Car2");
-        car1.go(()-> true);
-        assertThat(car1.getDistance()).isEqualTo(1);
+    @DisplayName("자동차 이동case 테스트")
+    void carGoTest() {
+        //given
+        Car car = new Car("Car");
 
-        car2.go(()->false);
-        assertThat(car2.getDistance()).isEqualTo(0);
+        //when
+        car.go(() -> true);
+
+        //then
+        assertThat(car.getDistance()).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("자동차 이동 안하는case 테스트")
+    void carDoesNotGoTest() {
+        //given
+        Car car = new Car("Car");
+
+        //when
+        car.go(() -> false);
+
+        //then
+        assertThat(car.getDistance()).isEqualTo(0);
+    }
 }
