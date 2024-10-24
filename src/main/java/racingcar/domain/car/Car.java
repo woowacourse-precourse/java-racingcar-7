52c6bbaea95fa86d7fe9d552d9d7478c0e307f67
tentenball <lth8905@gmail.com>
2024-10-25 01:55:18 +0900

    feat: Car 클래스 멤버변수, 메서드 추가
    * Car: 멤버변수 score 추가, addScore, getter 추가

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -4,8 +4,18 @@ import racingcar.domain.name.Name;
 
 public class Car {
 	private final Name name;
+	private int score;
 
 	public Car(Name name) {
 		this.name = name;
+		this.score = 0;
+	}
+
+	public void addScore(int score) {
+		this.score += score;
+	}
+
+	public Name getName() {
+		return name;
 	}
 }
