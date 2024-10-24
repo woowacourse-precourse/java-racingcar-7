d982a817cb761851591ec3f0690d1aee86733eb6
hanwool <hanwe92@gmail.com>
2024-10-24 10:26:08 +0900

    feat: get try number from input

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,11 +1,16 @@
 package racingcar.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import racingcar.util.InputParser;
+import racingcar.view.InputView;
 
 public class RacingCarController {
+    public static void start() {
+        String nameString = InputView.getCarNames();
+        List<String> carNames = InputParser.nameParse(nameString);
 
-    private List<String> nameParse(String input) {
-        return Arrays.stream(input.split(",")).toList();
+        String numberString = InputView.getTryNumber();
+        int tryNumber = InputParser.tryNumberParse(numberString);
     }
+
 }
