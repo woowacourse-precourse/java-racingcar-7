8524219b28baa41e7dceef542ddc26ec56064020
JeongSu Park <bechalrang@gmail.com>
2024-10-25 00:35:46 +0900

    refactor(Mileage): 접근제한자와 compareTo 로직 개선
    - 클래스 상수의 접근제한자를 `public`에서 `private`으로 수정
    - 예외 메시지 구체적으로 개선
    - 랩 카운트 비교식 오타 수정
    - compareTo 로직을 이해하기 쉽게 개선

--- src/main/java/racingcar/object/value/Mileage.java
+++ src/main/java/racingcar/object/value/Mileage.java
@@ -4,7 +4,7 @@ import racingcar.object.enums.RaceMaxCounts;
 
 public record Mileage(Integer distance) implements Comparable<Mileage> {
 
-    public static final int LIMIT_OF_DISTANCE = RaceMaxCounts.MAX_COUNT_OF_LAP.getCount();
+    private static final Integer LIMIT_OF_DISTANCE = RaceMaxCounts.MAX_COUNT_OF_LAP.getCount();
 
     public Mileage {
         if (distance == null) {
@@ -13,8 +13,8 @@ public record Mileage(Integer distance) implements Comparable<Mileage> {
         if (distance < 0) {
             throw new IllegalArgumentException("주행거리는 음수일 수 없습니다.");
         }
-        if (LIMIT_OF_DISTANCE < distance) {
-            throw new IllegalArgumentException("주행 한계 거리를 넘어설 수 없습니다.");
+        if (LIMIT_OF_DISTANCE.compareTo(distance) < 0) {
+            throw new IllegalArgumentException(String.format("지정 가능한 최대 주행 거리는 %d입니다.", LIMIT_OF_DISTANCE));
         }
     }
 
@@ -27,7 +27,7 @@ public record Mileage(Integer distance) implements Comparable<Mileage> {
         if (other == null) {
             throw new IllegalArgumentException("null은 비교할 수 없습니다.");
         }
-        return distance - other.distance;
+        return distance.compareTo(other.distance);
     }
 
 }
