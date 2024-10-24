8f9944d3157021c335296341762c8c79df4eb791
kgy1008 <gayeon1008@g.skku.edu>
2024-10-24 18:54:45 +0900

    refactor: change error validate method

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import racingcar.common.ErrorMessage;
 
 public class InputView {
+    private static final int ZERO = 0;
     private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
@@ -17,18 +18,18 @@ public class InputView {
 
     public static int inputTryCount() {
         System.out.println(TRY_COUNT_INPUT_MESSAGE);
-        final String input = Console.readLine();
-        return getValidatedTryCount(input);
+        final int tryCount = convertStringToInt(Console.readLine());
+        validateTryCount(tryCount);
+        return tryCount;
     }
 
-    private static void validateTryCount(final String input) {
-        if (input == null || input.isBlank()) {
+    private static void validateTryCount(final int input) {
+        if (input == ZERO) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_TRY_COUNT.getMessage());
         }
     }
 
-    private static int getValidatedTryCount(final String input) {
-        validateTryCount(input);
+    private static int convertStringToInt(final String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
