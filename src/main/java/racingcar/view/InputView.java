e6998062022dd431a8ccb0ab9e8c14ec373a61c5
Jo Myeong-geun <happy63590@gmail.com>
2024-10-25 03:34:23 +0900

    Revert "feat: 사용자에게 시도할 횟수로 양의 정수를 입력받는 기능 구현"
    This reverts commit 07950b6d1a8b5266715297d830bd409777db6a93.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,9 +8,4 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
-
-    public String inputRounds() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        return Console.readLine();
-    }
 }
