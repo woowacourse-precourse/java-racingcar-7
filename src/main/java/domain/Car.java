65760e3e41e4fd8fd672446a6cf9d0364b8e8d9b
sinji <tobeabestns@naver.com>
2024-10-24 23:59:29 +0900

    feat(Car): 랜덤값이 4 이상일 시 전진 메서드 생성

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,17 +1,27 @@
 package domain;
 
 public class Car {
+    private RandomGenerator generator;
     private String carName;
-    private String carLocation;
+    private int carLocation;
+    public static final int MOVABLENUMBER = 4;
 
     // 생성자(객체)
-    public Car(final String carName, final String carLocation) {
+    public Car(final String carName, final int carLocation) {
         this.carName = carName;
         this.carLocation = carLocation;
     }
 
     // 자동차의 현재 위치
-    public String gerCarLocation() {
+    public int getCarLocation() {
         return carLocation;
+    }
+
+    // 값이 4 이상이라면 전진
+    public void move() {
+        final int value = generator.randomGenerator();
+        if(value >= MOVABLENUMBER) {
+            carLocation++;
+        }
     }
 }
