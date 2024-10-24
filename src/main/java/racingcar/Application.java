2e703b10305e1de3c9ffa7fd45188c9681039d8d
ssuukko <tlstpstpdl11@gmail.com>
2024-10-25 01:41:01 +0900

    Refactor: Application 클래스 자동차 이름 변수 변경
    - Application 클래스에서 자동차 이름 입력을 위한 변수를 carName에서 carNames로 변경하였습니다. 이는 복수의 입력값을 받아들이기 위해 코드 가독성을 높이고자 한 것입니다.
    - GameController 생성자 호출 시 attemptCount 매개변수를 제거하였습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,11 +6,11 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carName = Console.readLine();
+        String carNames = Console.readLine();
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String attemptCount = Console.readLine();
 
-        GameController gameController = new GameController(carName, attemptCount);
+        GameController gameController = new GameController(carNames);
     }
 }
