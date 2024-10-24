775045cb55baa0c8554861ca72e856925ab67478
chaerish <ccyy8432@naver.com>
2024-10-24 17:30:33 +0900

    test(InputDTOTest): InputDTO의 to 메소드 검증 테스트
    InputDTO에서 RacingCar로 변환을 위한 to메소드 검증 테스트를 진행한다.

---
+++ src/test/java/racingcar/InputDTOTest.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class InputDTOTest {
+
+    @Test
+    void INPUT_DTO를_RACING_CAR_클래스로_만든다() {
+        InputDTO inputDTO = new InputDTO(List.of("차이름1", "차이름2"), 3L);
+        List<RacingCar> racingCars = inputDTO.to();
+        assertThat(racingCars.get(0).getName()).isEqualTo("차이름1");
+        assertThat(racingCars.get(1).getName()).isEqualTo("차이름2");
+    }
+}
