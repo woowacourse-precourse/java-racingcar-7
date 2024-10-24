d3875442fcee5b66c9e63549dad54d0f5e0a6a3c
taek2222 <oot309@daum.net>
2024-10-24 11:44:11 +0900

    feat(Car): 자동차 객체 클래스 정의
    - 프로젝트의 유용하게 값을 사용할 수 있도록 Car 클래스를 정의했습니다.
    - 차에는 이름과 이동 거리 필드만을 보관하도록 설계했습니다.

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private final Integer distance;
+
+    public Car(String name, Integer distance) {
+        this.name = name;
+        this.distance = distance;
+    }
+}
