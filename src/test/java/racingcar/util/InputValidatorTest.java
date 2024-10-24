aaab20246f0dde9801c0293f017fcd6e326a9332
taek2222 <oot309@daum.net>
2024-10-24 15:39:23 +0900

    test(InputValidatorTest): 검증 테스트 추가
    - 검증 관련 성공, 예외 테스트 진행했습니다.

---
+++ src/test/java/racingcar/util/InputValidatorTest.java
@@ -0,0 +1,58 @@
+package racingcar.util;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class InputValidatorTest {
+
+    @Test
+    void 정상_문자열_입력시_예외가_발생하지_않는다() {
+        // given
+        String input = "pobi";
+
+        // when & then
+        assertDoesNotThrow(() -> InputValidator.validateNotBlank(input));
+    }
+
+    @Test
+    void 빈_문자열_입력시_예외가_발생한다() {
+        // given
+        String input = "";
+
+        // when & then
+        assertThrows(IllegalArgumentException.class,
+                () -> InputValidator.validateNotBlank(input));
+    }
+
+    @Test
+    void 공백_존재하는_문자열_입력시_예외가_발생한다() {
+        // given
+        String input = "  ";
+
+        // when & then
+        assertThrows(IllegalArgumentException.class,
+                () -> InputValidator.validateNotBlank(input));
+    }
+
+    @Test
+    void 중복이_존재_안하면_예외가_발생하지_않는다() {
+        // given
+        List<String> list = List.of("pobi", "jason", "yeontaek");
+
+        // when & then
+        assertDoesNotThrow(() -> InputValidator.validateNoDuplicates(list));
+    }
+
+    @Test
+    void 중복이_존재하면_예외가_발생한다() {
+        // given
+        List<String> list = List.of("pobi", "jason", "pobi");
+
+        // when & then
+        assertThrows(IllegalArgumentException.class,
+                () -> InputValidator.validateNoDuplicates(list));
+    }
+}
