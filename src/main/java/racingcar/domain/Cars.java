77d08af2926ddadc712497792beea3ec9b494b61
YeaChan05 <qkenrdl05@gmail.com>
2024-10-25 01:33:17 +0900

    feat(Cars): 경주에서 사용되는 차량 로직 구현
    차량 이동, 우승자 추출, 출력 메세지 결합 등 서비스에 필요한 도메인 로직 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -2,8 +2,10 @@ package racingcar.domain;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import racingcar.common.MoveCondition;
 
 public class Cars {
+    private static final String FEED = "\n";
     private final List<Car> cars;
 
     private Cars(List<Car> cars) {
@@ -14,5 +16,41 @@ public class Cars {
         return names.stream()
                 .map(Car::new)
                 .collect(Collectors.collectingAndThen(Collectors.toList(), Cars::new));
+    }
+
+    public void progressRace() {
+        cars.forEach(Cars::goOrStop);
+    }
+
+    public String toPrettyString() {
+        return cars.stream()
+                .map(Car::toString)
+                .collect(Collectors.joining(FEED)) + FEED;
+    }
+
+    public String getWinnersName() {
+        int maxPosition = getMaxPosition();
+
+        return cars.stream()
+                .filter(car -> isWinner(car, maxPosition))
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+    }
+
+    private static boolean isWinner(Car car, int maxPosition) {
+        return car.getPosition() == maxPosition;
+    }
+
+    private int getMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+
+    private static void goOrStop(Car car) {
+        if (MoveCondition.isMoveable()) {
+            car.forward();
+        }
     }
 }
