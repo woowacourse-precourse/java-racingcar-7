1c6e82dc13a66a19986466e7573f71d1dee6bd04
jijongkwon <help3451@naver.com>
2024-10-25 01:38:34 +0900

    feat(ui) : InputView 생성
    TODO
    - inputCarNameUi : 자동차 입력 메시지 출력
    - inputTryUi : 시도 횟수 입력 메시지 출력

---
+++ src/main/java/racingcar/presentation/ui/InputView.java
@@ -0,0 +1,13 @@
+package racingcar.presentation.ui;
+
+public class InputView {
+    // TODO: 자동차 입력 메시지 출력
+    public void inputCarNameUi() {
+
+    }
+
+    // TODO: 시도 횟수 입력 메시지 출력
+    public void inputTryUi() {
+
+    }
+}
