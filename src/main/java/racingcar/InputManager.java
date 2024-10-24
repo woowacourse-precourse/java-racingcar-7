e12a05ad7432ab1b229533b368f43240e875b331
joonwan <joonwan1024@naver.com>
2024-10-24 23:45:29 +0900

    fix: InputManger 빈 문자열을 입력 받을 경우 예외 발생하도록 구현

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputManager {
 
+    private static final String INVALID_USER_INPUT_MESSAGE = "빈 문자열을 입력할 수 없습니다.";
+
     public static String readUserInput() {
         OutputManager.printGuideMessage();
-        return Console.readLine();
+        String input =  Console.readLine();
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(INVALID_USER_INPUT_MESSAGE);
+        }
+        return input;
     }
 
     public static void close() {
