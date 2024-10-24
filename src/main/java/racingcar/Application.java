cfa62579cc50212f4053e97682a780e4f6024a02
IM-GYURI <kgulr0517@gmail.com>
2024-10-25 00:46:48 +0900

    feat : add AppConfig at Application
    - AppConfig를 통해 RacingGameController를 생성하고 start()를 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.config.AppConfig;
+import racingcar.controller.RacingGameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig appConfig = new AppConfig();
+        RacingGameController racingGameController = appConfig.racingGameController();
+        racingGameController.start();
     }
 }
