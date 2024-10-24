68ce462a16413490a93d4c0f536611647bed952f
mingyeongbae <bmg1203@naver.com>
2024-10-24 17:02:51 +0900

    [style]: 메소드 접근 제어자 변경
    inputAndValidate, carRacing 메소드의 접근자를 public으로 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -27,7 +27,7 @@ public class Application {
         printFinalWinners(winners);
     }
 
-    private static void inputAndValidate() {
+    public static void inputAndValidate() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> carNameListInput = List.of(Console.readLine().trim().split(","));
         System.out.println("시도할 횟수는 몇 회인가요?");
@@ -67,7 +67,7 @@ public class Application {
         }
     }
 
-    private static void carRacing(List<String> carNameList) {
+    public static void carRacing(List<String> carNameList) {
         List<Integer> scores = new ArrayList<>();
         int max = Integer.MIN_VALUE;
 
