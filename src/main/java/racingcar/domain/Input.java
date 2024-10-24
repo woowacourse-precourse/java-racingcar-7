0c561b95b6cb1e202a1b90fd660987b15363995b
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-25 01:17:47 +0900

    feat(Input): 쉼표로 구분된 영어 이름이 입력되었는지 검증하는 로직 추가

--- src/main/java/racingcar/domain/Input.java
+++ src/main/java/racingcar/domain/Input.java
@@ -21,11 +21,18 @@ public class Input {
 
         private static void validateInput(String text) {
             validateNonEmptyInput(text);
+            validateCommaSeparatedEnglishNames(text);
         }
 
         private static void validateNonEmptyInput(String text) {
             if (text == null || text.isEmpty() || text.isBlank()) {
                 throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
+            }
+        }
+
+        private static void validateCommaSeparatedEnglishNames(String text) {
+            if (!text.matches("^[a-zA-Z,]+$")) {
+                throw new IllegalArgumentException("입력은 쉼표로 구분된 영어 이름만 가능합니다.");
             }
         }
 
