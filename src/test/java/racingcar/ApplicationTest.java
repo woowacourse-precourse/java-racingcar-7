695a108ff6fe34a8b1823eb50eb26002f3287527
choi <nippert@naver.com>
2024-10-24 18:16:11 +0900

    test: 입력값에 대한 예외 검사

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -15,19 +16,73 @@ class ApplicationTest extends NsTest {
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("경주할 자동차의 이름은 문자와 쉼표 이외의 값을 가질 수 없다")
+    void 자동차이름_문자와쉼표이외의값_존재() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi@juni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("쉼표(,)로 구분했을 때 자동차 이름이 존재해야 한다")
+    void 자동차이름_존재하지_않을때() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi, ,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("자동차의 이름은 1자 이상 5자 이하여야 한다")
+    void 자동차이름길이() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobijjang,juni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("자동차의 이름은 같을 수 없다")
+    void 자동차이름동일여부() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("시도 횟수는 숫자로 이루어져야 한다")
+    void 시도횟수_형식() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,juni", "aa"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("시도 횟수는 필수로 입력되어야 한다")
+    void 시도횟수_입력여부() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,juni", "\n"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
