91bc6f07919d85cefe774d56bee75fd970dd8c36
dd-jiyun <dlwldbs8015@gmail.com>
2024-10-25 01:56:31 +0900

    test(CarTest): 자동차 생성 및 이동 테스트

---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,31 @@
+package racingcar.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    @Test
+    @DisplayName("자동차 객체가 정상적으로 생성된다.")
+    void createCar() {
+        String name = "pobi";
+
+        Car car = new Car(name);
+
+        assertEquals(car.getName(),name);
+        assertEquals(car.getPosition(),0);
+    }
+
+    @Test
+    @DisplayName("자동차가 정상적으로 움직인다.")
+    void moveCar() {
+        Car car = new Car("pobi");
+
+        car.move();
+
+        assertEquals(1, car.getPosition());
+    }
+
+}
