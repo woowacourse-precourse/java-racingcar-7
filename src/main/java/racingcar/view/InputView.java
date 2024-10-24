1d1b41d2d7b12e0b03c3ec355982eee5b4afb868
dbdb456 <adad568@naver.com>
2024-10-24 15:14:05 +0900

    feat(inputview): 시도 횟수를 입력 받는 메서드 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,4 +8,13 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
+
+    public int inputAttemptCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("입력받은 문자가 숫자가 아닌 다른 문자입니다.");
+        }
+    }
 }
