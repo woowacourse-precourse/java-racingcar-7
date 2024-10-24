0c055db21039feab47a6cbf3955bb2ecc6e6ee2d
Chan531 <chan657312@gmail.com>
2024-10-24 19:22:33 +0900

    feat(GameController): 경주할 자동차 이름 입력 메소드 호출

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,10 +1,12 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class GameController {
 
 	public void run() {
 		OutputView.printCarNamesInputMessage();
+		String carNamesInput = InputView.getCarNamesInput();
 	}
 }
