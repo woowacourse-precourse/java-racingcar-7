e3d6004c4d9ad094b5d536d76cfa4053138ae876
btongtong <leeym2615@naver.com>
2024-10-24 14:42:33 +0900

    feat(Application): Add validation for car names
    Validate carNames not to start with comma or end with a comma
        invalid example: ",A,B,C" (invalid)
    Validate for duplicated car names when split by comma
        invalid example: "A,B,C,B"
    Validate name length to not over 5 characters
        invalid example: "ABCDE"
    Validate for empty names
        invalid example: "A,,B,C"

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,9 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -9,5 +13,47 @@ public class Application {
 
         System.out.println(IOMessage.INPUT_TRY_COUNT);
         String tryCount = readLine();
+
+        isValidName(carNames);
+    }
+
+    public static boolean isValidName(String carNames) {
+        validateStartEnd(carNames);
+
+        String[] namesByComma = carNames.split(IOMessage.COMMA_SEPARATOR);
+        validateDuplication(namesByComma);
+
+        for (String name : namesByComma) {
+            validateEmpty(name);
+            validateLength(name);
+        }
+
+        return true;
+    }
+
+    public static void validateStartEnd(String carNames) {
+        if (carNames.startsWith(",") || carNames.endsWith(",")) {
+            throw new IllegalArgumentException("맨 앞 또는 뒤 이름이 없다.");
+        }
+    }
+
+    public static void validateDuplication(String[] namesByComma) {
+        Set<String> names = new HashSet<>(Arrays.stream(namesByComma).toList());
+
+        if (namesByComma.length != names.size()) {
+            throw new IllegalArgumentException("중복된 이름을 사용했다.");
+        }
+    }
+
+    public static void validateLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름 길이가 5를 초과했다.");
+        }
+    }
+
+    public static void validateEmpty(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("빈 값이다.");
+        }
     }
 }
