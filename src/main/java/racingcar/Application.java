ff03bf57b8a1031853b09edbb8799067d50d3980
unluckyboy214 <janggoon0214@naver.com>
2024-10-25 01:36:36 +0900

    입력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,34 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 경주 자동차 리스트, 시도 횟수 입력
+        List<String> carList = car_input();
+        int tryCount = count_input();
+
+    }
+
+    public static List<String> car_input(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carArray = Console.readLine().split(",");
+        List carList = Arrays.asList(carArray);
+
+        return carList;
+    }
+
+    public static int count_input(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String num = Console.readLine();
+        int count = Integer.parseInt(num);
+        return count;
     }
 }
+
+
