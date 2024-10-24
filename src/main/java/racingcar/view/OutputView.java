65d2e733d3be14d770191e608866b839a50cfe0a
dongyun <ehddbs452100@naver.com>
2024-10-24 16:24:33 +0900

    feat(Message, OutputView): add moving forward result message
    - 자동차의 전진 기록 출력 전 "실행 결과" 메시지 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,6 @@
 package racingcar.view;
 
+import static racingcar.global.Message.MOVE_FORWARD_RESULT;
 import static racingcar.global.Message.WINNER;
 
 import java.util.List;
@@ -7,7 +8,11 @@ import java.util.List;
 public class OutputView {
 
     public static void printWinner(List<String> winners) {
-        System.out.print(WINNER + " ");
+        System.out.print(WINNER.getMessage() + " ");
         System.out.print(String.join(",", winners));
+    }
+
+    public static void printMoveForwardResult() {
+        System.out.println(MOVE_FORWARD_RESULT.getMessage());
     }
 }
