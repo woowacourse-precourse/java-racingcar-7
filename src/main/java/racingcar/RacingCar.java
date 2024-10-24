8170e92ef907f59603f83d73697fd9c76b71350b
임채연 <dlacodus0407@gmail.com>
2024-10-24 23:17:00 +0900

    refactor(RacingCar) : add createRacingCar static method
    클라이언트에서 직접 객체를 생성하게 될 경우 객체 생성을 관리하기 어려울것입니다.
    따라서, 생성자를 private으로 막고 createRacingCar() 정적 팩토리 메서드를 만들어
    객체 생성을 RacingCar 클래스에서 관리합니다.

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -4,12 +4,16 @@ public class RacingCar {
     private final String name;
     private Integer score;
 
-    public RacingCar(String name) {
+    private RacingCar(String name) {
         this.name = name;
         this.score = 0;
     }
 
     public void addScore() {
         this.score += 1;
+    }
+
+    public static RacingCar createRacingCar(String name) {
+        return new RacingCar(name);
     }
 }
