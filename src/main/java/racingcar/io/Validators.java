9fea71119d3467cdc3b3ce9711ec35af65a2d28a
woong2e <tlswodnd12345@naver.com>
2024-10-25 03:14:42 +0900

    feat(validation): implement input value check
    character or number, and non-negative integer or not

--- src/main/java/racingcar/io/Validators.java
+++ src/main/java/racingcar/io/Validators.java
@@ -8,7 +8,7 @@ public class Validators implements Validator {
 
 
     @Override
-    public String validateString(String input) throws IllegalArgumentException {
+    public String[] validateString(String input) throws IllegalArgumentException {
         if (input.indexOf(",") == 0 || input.lastIndexOf(",") == input.length() - 1) {
             throw new IllegalArgumentException("잘못된 자동차 이름 입력 - 공백 이름 입력");
         }
@@ -24,7 +24,7 @@ public class Validators implements Validator {
             throw new IllegalArgumentException("잘못된 자동차 이름 입력 - 중복 이름 존재");
         }
 
-        return input;
+        return words;
     }
 
     private String[] splitToArray(String input) {
@@ -55,6 +55,14 @@ public class Validators implements Validator {
 
     @Override
     public int validateInteger(String input) throws IllegalArgumentException {
-        return 0;
+        try {
+            int num = Integer.parseInt(input.strip());
+            if (num < 0) {
+                throw new IllegalArgumentException("숫자 범위 오류");
+            }
+            return num;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닌 값 입력");
+        }
     }
 }
