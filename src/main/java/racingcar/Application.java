9f67c883c333ec094a3070f6710816b24582f9d5
YeaChan05 <qkenrdl05@gmail.com>
2024-10-24 23:58:22 +0900

    refactor(Application): 애플리케이션 로직 조합
    작성한 코드들을 main에서 순차적으로 조합

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,22 @@
 package racingcar;
 
+import racingcar.domain.Cars;
+import racingcar.endpoint.InputInterface;
+import racingcar.endpoint.OutputInterface;
+import racingcar.service.CarFactory;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO 2024 10 24 22:36:50 : 선수 이름 입력 시스템 메세지 출력
-        //TODO 2024 10 24 22:37:48 : 시행 횟수 입력 시스템 메세지 출력
-        //TODO 2024 10 24 22:38:38 : 선수 만들기
+        String firstLine;
+        String secondLine;
+        try (InputInterface inputInterface=new InputInterface()) {
+            OutputInterface.printMessage(OutputInterface.ENTER_PLAYER_NAME);
+            firstLine = inputInterface.readLine();
+            OutputInterface.printMessage(OutputInterface.ENTER_NUMBER_OF_TRIALS);
+            secondLine = inputInterface.readLine();
+        }
+        CarFactory carFactory = new CarFactory();
+        Cars cars=carFactory.createCars(firstLine);
         //TODO 2024 10 24 22:38:46 : 선수들을 주입받는 경주 서비스 만들기
         //TODO 2024 10 24 22:39:12 : 시행 횟수만큼 경주를 실행
         //TODO 2024 10 24 22:39:25 : 각 시행마다 거리 출력
