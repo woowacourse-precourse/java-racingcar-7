eb8ad77dbfb04ede9d1266beb9c2ddf8d5b8bba9
jijongkwon <help3451@naver.com>
2024-10-25 01:30:34 +0900

    feat(domain) : Winner 클래스 생성
    filed
    - name : 우승자 이름
    
    TODO
    - 이름반환

---
+++ src/main/java/racingcar/domain/winner/Winner.java
@@ -0,0 +1,14 @@
+package racingcar.domain.winner;
+
+public class Winner {
+    private final String name;
+
+    public Winner(String name) {
+        this.name = name;
+    }
+
+    // TODO : 이름 반환
+    public String getName() {
+        return null;
+    }
+}
