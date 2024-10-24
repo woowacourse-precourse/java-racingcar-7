a5624c0a385511790cadcc64d4798bdc0515e3dd
박규영 <magic9282m@gmail.com>
2024-10-25 05:36:52 +0900

    feat: add new method doRace
    doRace 메서드를 Application 에 추가함
    - 인자로 round 를 받아, 해당 int 값 만큼 race 를 진행하는 메서드
    - 각 자동차에 대해 0~9 사이 무작위 값을 뽑은 뒤, 해당 값이 4 이상이면 전진한다.
    - 각 라운드 별로 진행 결과를 출력한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     static String[] cars;
     static StringBuilder[] carsRaceProgress;
@@ -14,6 +16,19 @@ public class Application {
             }
         }
         return result;
+    }
+    public static void doRace(int round) {
+        for(int i=0; i<round; i++) {
+            for(int j=0; j<cars.length; j++) {
+                if(Randoms.pickNumberInRange(0, 9) >= 4) {
+                    // 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우 자동차는 전진
+                    carsRaceProgress[j].append('-');
+                }
+            }
+            printRaceRound();
+            // 각 차수별 실행 결과의 구분을 위해 빈 줄을 하나 출력한다.
+            System.out.println();
+        }
     }
     public static void printRaceRound() {
         for(int i=0; i<cars.length; i++) {
