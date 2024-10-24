1104b5418d9c31cd81edb421710570ec619a7e3d
sc@t0a14 <gusdn6671@gmail.com>
2024-10-24 21:51:05 +0900

    test(ValidatorTest): 자동차 이름이 빈 문자열일 경우 예외를 검증하는 테스트 추가

---
+++ src/test/java/racingcar/ValidatorTest.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.utils.Validator;
+
+public class ValidatorTest {
+    @Test
+    @DisplayName("자동차의 이름이 빈칸일 경우 예외가 발생한다")
+    void check_name_empty() {
+        String name = "";
+        assertThatThrownBy(() -> Validator.isEmpty(name)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
