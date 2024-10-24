c57302375da2c180b649c744ee036aa9821a02c4
Hojoomoney <kimhojoo51@gmail.com>
2024-10-24 22:40:53 +0900

    feat : Race(경주 관리) 클래스 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,8 +10,8 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] names = Console.readLine().split(",");
-        List<Cars> cars = Arrays.stream(names)
-                .map(Cars::new)
+        List<Car> cars = Arrays.stream(names)
+                .map(Car::new)
                 .toList();
 
 
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,12 @@
 package racingcar;
 
-public class Cars {
+public class Car {
 
     private final String name;
     private int position;
 
 
-    public Cars(String name) {
+    public Car(String name) {
         if (name.length() > 5) throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         this.name = name;
     }
