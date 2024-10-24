767bd729d1f82adff2558b8df9e92783c39be16d
gitubanana <ptyolo666@gmail.com>
2024-10-24 20:24:49 +0900

    test(ApplicationTest): 예외 테스트에서 ErrorMessage enum 확인

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,12 +1,10 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.ErrorMessage.WRONG_CAR_NAME;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Nested;
@@ -51,6 +49,7 @@ class ApplicationTest extends NsTest {
             assertSimpleTest(() ->
                     assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                             .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessage(WRONG_CAR_NAME.getMessage())
             );
         }
     }
