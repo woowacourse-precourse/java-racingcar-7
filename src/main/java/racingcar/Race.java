091877ee1a94395287992ecf6be6918bbf9daf98
jwh946 <jwh946@gmail.com>
2024-10-24 22:27:58 +0900

    최종 우승자 출력 기능

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Race {
@@ -16,11 +18,12 @@ public class Race {
             play();
             System.out.println();
         }
+        print();
     }
     private void play() {
         for (Car car : cars) {
             move(car);
-            print(car);
+            printName(car);
         }
     }
     private void move(Car car) {
@@ -31,7 +34,33 @@ public class Race {
             car.setResult(result);
         }
     }
-    private void print(Car car) {
+    private void printName(Car car) {
         System.out.println(car.getName() + " : " + car.getResult());
+    }
+    private List<String> rank() {
+        cars.sort(Comparator.comparingInt(car -> car.getResult().length()));
+        int maxLength = cars.get(cars.size() - 1).getResult().length();
+        List<String> list = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getResult().length() == maxLength) {
+                list.add(car.getName());
+            }
+        }
+        return list;
+    }
+    private void print() {
+        List<String> list = rank();
+        if (list.size() == 1) {
+            System.out.println("최종 우승자 : " + rank().get(0));
+        }
+        else {
+            String winner = "최종 우승자 : ";
+            for (int i=0; i< list.size() - 1; i++) {
+                winner += list.get(i);
+                winner += ", ";
+            }
+            winner += list.get(list.size() - 1);
+            System.out.println(winner);
+        }
     }
 }
