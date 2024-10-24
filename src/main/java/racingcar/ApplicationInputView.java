69890875c038242334ef7602c11a78fe2ceae95e
0tae <choiyt3465@naver.com>
2024-10-25 02:56:07 +0900

    feat: 시스템 입출력 기능 구현

---
+++ src/main/java/racingcar/ApplicationInputView.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ApplicationInputView {
+    public static String input() {
+        return Console.readLine();
+    }
+
+    public static int getInt(){
+        try{
+            return Integer.parseInt(input());
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("해당 입력에서는 정수를 입력해야 합니다.");
+        }
+    }
+}
---
+++ src/main/java/racingcar/ApplicationOutputView.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class ApplicationOutputView {
+    public static void outputLine(String msg) {
+        System.out.println(msg);
+    }
+
+    public static void output(String msg) {
+        System.out.print(msg);
+    }
+}
