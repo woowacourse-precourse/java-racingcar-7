f72f2923b8c95adca960554da224c99ca993a71c
leeesooha <suha630@naver.com>
2024-10-25 01:40:00 +0900

    feat(InputHandler): 자동차 이름이 5글자를 넘으면 예외 발생 기능 추가

--- src/main/java/racingcar/View/InputHandler.java
+++ src/main/java/racingcar/View/InputHandler.java
@@ -4,9 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.SimpleTimeZone;
 
 public class InputHandler {
     static private final String ALPHA_NUMERIC_PATTERN = "[a-zA-Z0-9]+";
+    static private final int CAR_MAX_LENGTH = 5;
     List<String> carNames;
 
     public InputHandler() {
@@ -23,6 +25,15 @@ public class InputHandler {
 
     private void validateCarNames(List<String> splitCarNames) {
         checkAlphaNumericName(splitCarNames);
+        checkCarNameLength(splitCarNames);
+    }
+
+    private void checkCarNameLength(List<String> splitCarNames) {
+        for (String carName : splitCarNames) {
+            if (carName.length() > CAR_MAX_LENGTH) {
+                throw new IllegalArgumentException("자동차의 이름의 길이는 5글자 이하입니다.");
+            }
+        }
     }
 
     private void checkAlphaNumericName(List<String> splitCarNames) {
