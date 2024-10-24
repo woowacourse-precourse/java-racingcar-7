a1ba6060c8770038e2e83fb451a5478e4eae7c8e
SEO-Ji-Ho <jhseo9601@gmail.com>
2024-10-24 14:08:49 +0900

    feat: 경주할 자동차의 이름 입력을 요청하는 메세지를 출력하는 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+    }
+
+    private void printRequestingCarNameInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 }
