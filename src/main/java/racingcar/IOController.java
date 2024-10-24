ad8fac71edb266918b72d2be3550cea9c324be06
5ms <oms010414@gmail.com>
2024-10-25 01:41:11 +0900

    IOController 예외처리 분리

--- src/main/java/racingcar/IOController.java
+++ src/main/java/racingcar/IOController.java
@@ -2,29 +2,40 @@ package racingcar;
 
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.List;
 
 public class IOController {
-    public static String userListInput(){
+
+    public static List<String> userListInput(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
-
+        String users = Console.readLine();
+        return getUserList(users);
     }
-    public static Integer tryCountInput(){
+
+    public static List<String> getUserList(String users){
+        return Arrays.stream(users.split(",")).toList();
+    }
+
+    public static String tryCountInput(){
         System.out.println("시도할 횟수는 몇 회인가요?");
-        String tryCount = Console.readLine();
-        for(int i = 0; i < tryCount.length(); i++){
-            if(!Character.isDigit(tryCount.charAt(i))){
-                throw new IllegalArgumentException();
-            }
-        }
-        return Integer.parseInt(tryCount);
+        return Console.readLine();
     }
 
-    public static void printOutput(ArrayList<String> winners){
+    public static void printState(){
+        System.out.println("실행 결과");
+    }
 
-        String str = winners.stream().collect(Collectors.joining(", "));
+    public static void printCurrentState(List<Car> carList){
+        for (Car car : carList) {
+            System.out.println(car.getName() + " : "+car.getMoveCount());
+        }
+        System.out.println();
+    }
+
+    public static void printFinalWinner(List<String> winners){
+
+        String str = String.join(", ", winners);
         System.out.print("최종 우승자 : "+ str);
 
     }
