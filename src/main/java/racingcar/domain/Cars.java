c28dd028f8aaae50925302ae38cf71a23c74396c
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-10-24 19:50:10 +0900

    refactor: Racing 클래스로 인한 메서드 분리
    Racing 객체를 만든 후 관심사를 분리시켰음

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,6 +1,5 @@
 package racingcar.domain;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,27 +9,6 @@ public class Cars {
 
     public Cars(List<Car> cars){
         this.cars = cars;
-    }
-
-    public List<Car> findWinner(List<Car> cars) {
-        List<Car> winner = new ArrayList<>();
-        int maxMovedDistance = findMaxMovedDistance(cars);
-        for (Car car : cars) {
-            if (car.getMovedDistance() == maxMovedDistance) {
-                winner.add(car);
-            }
-        }
-        return winner;
-    }
-
-    private int findMaxMovedDistance(List<Car> cars) {
-        int maxMovedDistance = 0;
-        for (Car car : cars) {
-            if (car.getMovedDistance() > maxMovedDistance) {
-                maxMovedDistance = car.getMovedDistance();
-            }
-        }
-        return maxMovedDistance;
     }
 
     public List<Car> getCars(){
