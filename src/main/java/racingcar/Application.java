39bd6030e0dc4d2d9a2908d852042f93a4a83897
btongtong <leeym2615@naver.com>
2024-10-24 13:28:54 +0900

    feat(Application): Receive car names and conduct count

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println(IOMessage.INPUT_CAR_NAMES);
+        String carNames = readLine();
+
+        System.out.println(IOMessage.INPUT_TRY_COUNT);
+        String tryCount = readLine();
     }
 }
