e4b548a777156d101377a253cdc5a14b14d47912
chaerish <ccyy8432@naver.com>
2024-10-24 18:30:54 +0900

    feat(RaceJudge): 우승자를 가리는 RaceJudge 클래스 구현
    경주 우승자를 가리기 위한 RaceJudge 클래스를 구현한다.

---
+++ src/main/java/racingcar/RaceJudge.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import java.util.List;
+
+public class RaceJudge {
+    public static String[] determineWinner(List<RacingCar> racingCars) {
+        return racingCars.stream()
+                .filter(compareCar -> compareCar.getDistance().length()
+                        == racingCars.stream()
+                        .mapToInt(car -> car.getDistance().length())
+                        .max()
+                        .orElseThrow(() -> new IllegalArgumentException("리스트 값이 비어있습니다.")))
+                .map(RacingCar::getName)
+                .toArray(String[]::new);
+    }
+}
