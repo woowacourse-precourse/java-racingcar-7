77e28f6d67724d65322a59972aee4851e313e7e2
YeaChan05 <qkenrdl05@gmail.com>
2024-10-24 22:57:30 +0900

    feat(OutputInterface): 출력 열거형 구현
    출력에 필요한 메세지를 열거체로하고, 출력 기능을 담당하는 메서드를 가진 열거형 구혀냐

---
+++ src/main/java/racingcar/endpoint/OutputInterface.java
@@ -0,0 +1,25 @@
+package racingcar.endpoint;
+
+public enum OutputInterface {
+    ENTER_PLAYER_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    ENTER_NUMBER_OF_TRIALS("시도할 횟수는 몇 회인가요?");
+
+    private final String message;
+
+    OutputInterface(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+
+    public static void printMessage(String systemMessage){
+        System.out.println(systemMessage);
+    }
+
+    public static void printMessage(OutputInterface systemMessage){
+        System.out.println(systemMessage);
+    }
+}
