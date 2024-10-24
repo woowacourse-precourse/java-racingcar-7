cb7ac0dae5266339efad4757e7bcce0bacf0d3b4
Jo Myeong-geun <happy63590@gmail.com>
2024-10-25 06:02:17 +0900

    feat: 자동차 경주 게임 실행을 위한 컨트롤러 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import racingcar.controller.CarRaceController;
+import racingcar.service.CarRaceService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarRaceController carRaceController = new CarRaceController(
+                new CarRaceService(),
+                new InputView(),
+                new OutputView());
+
+        carRaceController.run();
     }
 }
---
+++ src/main/java/racingcar/controller/CarRaceController.java
@@ -0,0 +1,34 @@
+package racingcar.controller;
+
+import java.util.stream.IntStream;
+import racingcar.service.CarRaceService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class CarRaceController {
+
+    private final CarRaceService carRaceService;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public CarRaceController(CarRaceService carRaceService, InputView inputView, OutputView outputView) {
+        this.carRaceService = carRaceService;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        String carNames = inputView.inputCarNames();
+        String attempts = inputView.inputAttempts();
+        carRaceService.init(carNames);
+
+        outputView.printExecutionResultHeader();
+        IntStream.range(0, Integer.parseInt(attempts))
+                .forEach(attempt -> {
+                    carRaceService.moveCars();
+                    outputView.printCarsStatus(carRaceService.getCarsStatus());
+                });
+
+        outputView.printWinners(carRaceService.getWinners());
+    }
+}
