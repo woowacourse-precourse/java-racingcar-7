6c4178dac91a11f2655a24879bfa9d1ee27cdc69
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-24 20:32:53 +0900

    refactor: CarName과 Distance equals와 hash 오버라이딩 삭제

--- src/main/java/racingcar/domain/car/CarName.java
+++ src/main/java/racingcar/domain/car/CarName.java
@@ -17,17 +17,4 @@ public class CarName {
     public String getCarName() {
         return carName;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CarName carName1 = (CarName) o;
-        return Objects.equals(carName, carName1.carName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(carName);
-    }
 }
--- src/main/java/racingcar/domain/car/Distance.java
+++ src/main/java/racingcar/domain/car/Distance.java
@@ -18,17 +18,4 @@ public class Distance {
     public int getDistance() {
         return distance;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Distance distance1 = (Distance) o;
-        return distance == distance1.distance;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(distance);
-    }
 }
