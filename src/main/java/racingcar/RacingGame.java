1228d9189bb2c2e7f63f56a6b4b361081dc90707
peanut990 <peanut1998@naver.com>
2024-10-25 01:18:15 +0900

    feat: 입력문 추가
    추가된 입력문:
    - "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
    - "시도할 횟수는 몇 회인가요?"

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -11,9 +11,11 @@ public class RacingGame {
     private int tryCount;
 
     public void ready() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputNames = readLine();
         cars = createCars(createNames(inputNames));
 
+        System.out.println("시도할 횟수는 몇 회인가요?");
         String tryCountStr = readLine();
         tryCount = changeTryCountToInt(tryCountStr);
     }
