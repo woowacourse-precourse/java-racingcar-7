9675e2121cc75c97129f94230cb6e7f8bc2301b7
dnwldla <leeemjw@naver.com>
2024-10-25 07:16:07 +0900

    refactor: outputView이 경주 과정을 출력하게 변경 Cars 일급컬렉션에는 비지니스 로직만 담기게 한다

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,6 +1,18 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
+import java.util.Iterator;
+import java.util.List;
+
 public class OutputView {
+
+    private static final String BLANK = "";
+    private static final String SPACE = " ";
+    private static final String DELIMITER = ":";
+    private static final String SPLITOR = ",";
+
     private static final String START_COMMAND="경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String RACING_COUNTS="시도할 횟수는 몇 회인가요?";
     private static final String RACING_PROCESS="실행 결과";
@@ -21,5 +33,16 @@ public class OutputView {
 
     public static void printRacingWinner(String result){
         System.out.printf(RACING_WINNER,result);
+    }
+
+    public static void printState(Cars cars) {
+        List<Car> carList=cars.getCars();
+
+        carList.
+                stream()
+                .forEach(car -> {
+                    System.out.println(car.getName() + SPACE + DELIMITER + SPACE + car.getState());
+                });
+        System.out.println(BLANK);
     }
 }
