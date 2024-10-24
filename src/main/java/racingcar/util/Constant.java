2b6412a4e0ac4c4d3a8c5867b477cc5e6c9e8476
onesay17 <onesay17@gmail.com>
2024-10-24 16:04:48 +0900

    feat: 상수를 관리하는 클래스 추가
    이름의 길이 최대값, 랜덤값으 최소/최대값, 자동차의 최소 이동거리 상수 추가

---
+++ src/main/java/racingcar/util/Constant.java
@@ -0,0 +1,9 @@
+package racingcar.util;
+
+public class Constant {
+
+    public static final Integer MAX_CAR_NAME_LENGTH = 5;
+    public static final Integer MIN_RANDOM_VALUE = 0;
+    public static final Integer MAX_RANDOM_VALUE = 9;
+    public static final Integer MIN_MOVE_VALUE = 4;
+}
