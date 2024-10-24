366aa104ed707e152286c796dc5d76fd766ddcb8
qyinm <qusseun@gmail.com>
2024-10-24 20:47:09 +0900

    feat(RacingCarController): run 함수 로직 작성

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,19 +1,26 @@
 package racingcar.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.car.RacingCar;
 import racingcar.service.RacingCarService;
 import racingcar.view.InputView;
 
 public class RacingCarController {
 
-    private final InputView inputView;
     private final RacingCarService racingCarService;
 
-    public RacingCarController(InputView inputView, RacingCarService racingCarService) {
-        this.inputView = inputView;
+    public RacingCarController(RacingCarService racingCarService) {
         this.racingCarService = racingCarService;
     }
 
     public void run() {
+        List<String> carNames = InputView.getCarNames();
+        int moveCount = InputView.getMoveCount();
 
+        List<RacingCar> winners = racingCarService.logic(carNames, moveCount);
+        String winnerNames = winners.stream().map(RacingCar::getCarName).collect(Collectors.joining(", "));
+
+        System.out.println("최종 우승자 : " + winnerNames);
     }
 }
