0a12ce166a29b41f6f6ce73c575ba037d81e0c29
GaBaljaintheroom <junsu1222@naver.com>
2024-10-24 12:42:36 +0900

    feat(Track): 트랙의 횟수 범위 검증 기능 구현

--- src/main/java/racingcar/Track.java
+++ src/main/java/racingcar/Track.java
@@ -8,6 +8,18 @@ public class Track {
         this.lap = lap;
     }
 
+    public static Track from(int lap) {
+        validateLapRange(lap);
+        return new Track(lap);
+    }
+
+    private static void validateLapRange(int lap) {
+        int minLap = 1;
+        if (lap < minLap) {
+            throw new IllegalArgumentException("최소 1회 이상 횟수를 입력해주세요.");
+        }
+    }
+
     public boolean isLapInProgress() {
         return lap > 0;
     }
