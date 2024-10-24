80ca1a4c8c9219a41475975a4f593e79fb29fadf
qudcks8084 <qudcks8084@naver.com>
2024-10-24 16:00:59 +0900

    Fix : change access modifier to Test
    - change access modifier to Test getRandomMovementResult()
    	- private -> package

--- src/main/java/racingcar/service/RacingCarServiceImpl.java
+++ src/main/java/racingcar/service/RacingCarServiceImpl.java
@@ -32,7 +32,7 @@ public class RacingCarServiceImpl {
     }
 
 
-    private static int getRandomMovementResult(){
+    static int getRandomMovementResult(){
         // 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
         if (pickNumberInRange(0, 9) >= 4) {
             return 1;
