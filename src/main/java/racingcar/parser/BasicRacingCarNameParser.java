65f5e0cc34723cdca6103464e43e16dd31d15522
changuii <rhljh7410@gmail.com>
2024-10-24 16:58:30 +0900

    feat(BasicRacingCarNameParser) : 2. 입력으로 들어온 자동차들의 이름을 분리해라
    - parseNamesStringToList() : 자동차 이름들을 문자열 배열로 분리하고, 각 이름들의 앞뒤 공백을 지운 후 List로 반환한다.

--- src/main/java/racingcar/parser/BasicRacingCarNameParser.java
+++ src/main/java/racingcar/parser/BasicRacingCarNameParser.java
@@ -1,13 +1,18 @@
 package racingcar.parser;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BasicRacingCarNameParser implements RacingCarNameParser {
     private static final String CAR_NAME_DELIMITER = ",";
 
     @Override
     public List<String> parseNamesStringToList(String carNames) {
-        return null;
+        String[] splitCarNames = splitCarNames(carNames);
+        return Arrays.stream(splitCarNames)
+                .map(this::trimLeadingAndTrailingWhitespace)
+                .collect(Collectors.toList());
     }
 
     private String trimLeadingAndTrailingWhitespace(String carName) {
