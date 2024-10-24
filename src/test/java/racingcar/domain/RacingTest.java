6d70dc0b270c33d9bbf412aab8eea04fe8f32ad9
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-10-24 20:04:06 +0900

    test: racing 객체 추가로 인한 테스트 변경
    Cars -> Racing으로 테스트 코드 이동 및 수정

--- src/test/java/racingcar/domain/RacingTest.java
+++ src/test/java/racingcar/domain/RacingTest.java
@@ -6,16 +6,17 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class CarsTest {
+public class RacingTest {
 
     @DisplayName("Cars가 주어졌을 때, 가장 많이 움직인 자동차를 우승자로 뽑는다 - 단독 우승의 경우")
     @Test
-    void find_winner_test(){
+    void find_winner_test() {
         Car car1 = new Car("pobi");
         Car car2 = new Car("bibim");
         Car car3 = new Car("even");
         Cars cars = new Cars(List.of(car1, car2, car3));
 
+        Racing racing = new Racing(cars);
         car1.move(9);
         car1.move(8);
         car2.move(0);
@@ -23,7 +24,7 @@ public class CarsTest {
         car3.move(0);
         car3.move(2);
 
-        List<Car> winner = cars.findWinner(cars.getCars());
+        List<Car> winner = racing.findWinner(cars.getCars());
         assertThat(winner.size()).isEqualTo(1);
         assertThat(winner.get(0).getName()).isEqualTo("pobi");
         assertThat(winner.get(0).getMovedDistance()).isEqualTo(2);
@@ -31,11 +32,13 @@ public class CarsTest {
 
     @DisplayName("Cars가 주어졌을 때, 가장 많이 움직인 자동차를 우승자로 뽑는다 - 복수 우승의 경우")
     @Test
-    void find_two_winner_test(){
+    void find_two_winner_test() {
         Car car1 = new Car("pobi");
         Car car2 = new Car("bibim");
         Car car3 = new Car("even");
         Cars cars = new Cars(List.of(car1, car2, car3));
+
+        Racing racing = new Racing(cars);
 
         car1.move(9);
         car1.move(8);
@@ -45,8 +48,9 @@ public class CarsTest {
         car3.move(9);
 
         List<Car> expectedWinner = List.of(car1, car3);
-        List<Car> winner = cars.findWinner(cars.getCars());
+        List<Car> winner = racing.findWinner(cars.getCars());
         assertThat(winner.size()).isEqualTo(2);
         assertThat(winner).containsAll(expectedWinner);
     }
+
 }
