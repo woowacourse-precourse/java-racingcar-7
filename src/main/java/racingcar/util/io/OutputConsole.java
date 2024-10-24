beca2d4e93696e3c10ed2fdbab8cebb3df397f27
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-25 02:22:23 +0900

    chore($OutputConsole): 사용하지 않는 함수 제거

--- src/main/java/racingcar/util/io/OutputConsole.java
+++ src/main/java/racingcar/util/io/OutputConsole.java
@@ -5,8 +5,4 @@ public final class OutputConsole {
     public static void println(String message) {
         System.out.println(message);
     }
-
-    public static void print(String message) {
-        System.out.print(message);
-    }
 }
