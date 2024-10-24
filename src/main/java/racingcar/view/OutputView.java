06255dd911418da947624927092ea61a33d7bbcb
dbdb456 <adad568@naver.com>
2024-10-24 15:33:28 +0900

    feat(outputview): 각 자동차의 경주 결과를 출력하는 메서드 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,18 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.domain.Car;
+
 public class OutputView {
 
     public void printResultMessage() {
         System.out.println("\n실행 결과");
+    }
+
+    public void printRaceResults(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + car.getCount());
+        }
+        System.out.println();
     }
 }
