466e58e7fff5588e8e798b86cc054ecb2429ed9b
SeongJunChun <csj991210@naver.com>
2024-10-24 22:46:14 +0900

    test: 자동차 생성 테스트 코드 작성

---
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -0,0 +1,38 @@
+package racingcar.domain.car;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class CarTest {
+    @DisplayName("시작 위치 자동차 생성 테스트")
+    @Test
+    void createCar() {
+        Car car = Car.createCarAtStartLine("fobi");
+        assertEquals(car.getCarName(), "fobi");
+        assertEquals(car.getCarDistance(), 0);
+    }
+
+    @DisplayName("자동차 이름 예외발생 테스트")
+    @ParameterizedTest(name = "{displayName}: {0}")
+    @MethodSource("wrongNames")
+    void validateName(String name) {
+        assertThrows(IllegalArgumentException.class, () -> Car.createCarAtStartLine(name));
+    }
+
+    static Stream<Arguments> wrongNames() {
+        return Stream.of(
+                Arguments.of(""),
+                Arguments.of(" "),
+                Arguments.of("car!"),
+                Arguments.of("longNameCar")
+        );
+    }
+
+
+}
