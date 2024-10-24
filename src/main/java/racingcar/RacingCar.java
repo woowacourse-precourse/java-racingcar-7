c4ce6f0340667403376f6a9e7607984db7022eba
sunjong0214 <rlatjswhd8200@gmail.com>
2024-10-24 19:30:44 +0900

    feat(RacingCar): RacingCar 생성 시 길이 validation 추가
    5자 이하인 문자열이 파라미터로 넘어올 시 IllegalArgumentException 발생

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -4,6 +4,9 @@ public class RacingCar {
     private final String name;
 
     public RacingCar(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
         this.name = name;
     }
 
