66d019531ccbf0d4e55174bdcfd05cb843fa8ee3
rladmstn <oz33177351@gmail.com>
2024-10-24 17:34:41 +0900

    feat(validator) : 자동차 이름 목록 검증, 시도 횟수 입력 검증하는 validator 메소드 구현
    - `validateCarNames()` : 자동차 이름 입력에 대해 아래 세 개의 케이스를 검증한다.
       1. 입력이 영어 대소문자, 숫자, 콤마로만 구성되었는지
       2. 자동차의 이름으로 공백 문자가 포함 되어있는지
       3. 자동차 이름들 중 중복이 존재하는지
    - `validateTrialCount()` : 시도 횟수 검증에 대해 아래 두 케이스를 검증한다.
       1. 시도 횟수의 입력이 숫자만을 포함하는지
       2. 시도 횟수가 0 이상 1000 이하의 정수인지
    - 잘못된 input에 대한 ErrorMessage enum 추가

---
+++ src/main/java/racingcar/enums/ErrorMessage.java
@@ -0,0 +1,17 @@
+package racingcar.enums;
+
+public enum ErrorMessage {
+    INVALID_CAR_NAMES("자동차 이름은 영어 대소문자 및 숫자로 구성해주세요."),
+    DUPLICATED_NAMES("자동차 이름 목록에 중복이 존재합니다."),
+    INVALID_TRIAL_COUNT("시도 횟수는 0 이상 1000 이하의 정수로 입력해주세요.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
