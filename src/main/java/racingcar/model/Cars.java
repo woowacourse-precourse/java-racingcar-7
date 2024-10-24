e96a90922785ac2505d48b85006a6f0cc96e733b
sc@t0a14 <gusdn6671@gmail.com>
2024-10-24 23:59:10 +0900

    feat(Cars): 자동차의 이름이 중복되었을 경우 예외를 발생한다

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -2,11 +2,29 @@ package racingcar.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import racingcar.utils.Validator;
 
 public class Cars {
     private List<Car> cars;
 
     public Cars(List<Car> cars) {
-        this.cars = new ArrayList<>();
+        this.cars = cars;
+        validate();
+    }
+
+    private void validate() {
+        Validator.containDuplicate(extractCarNames(cars));
+    }
+
+    private List<String> extractCarNames(List<Car> cars) {
+        List<String> carNames = new ArrayList<>();
+        for (Car car : cars) {
+            carNames.add(car.getName());
+        }
+        return carNames;
+    }
+
+    public List<Car> getCars() {
+        return List.copyOf(cars);
     }
 }
