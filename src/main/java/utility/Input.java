ec59b52ec990be3ed63ea669e1221749b43df1fe
youngjuKim <kjuzoojuk@naver.com>
2024-10-24 20:27:49 +0900

    feat(Input): 테스트용 메서드 추가

--- src/main/java/utility/Input.java
+++ src/main/java/utility/Input.java
@@ -2,6 +2,7 @@ package utility;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,16 +13,17 @@ public class Input {
     public static List<String> inputCars() throws IllegalArgumentException {
         // Error : 공백 입력, (,)가 아닌 구분자 사용, 5글자 초과
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        cars = Arrays.asList(Console.readLine().strip().split(","));
+        cars = new ArrayList<>(Arrays.asList(Console.readLine().strip().split(",")));
         noValueEntered(cars);
+        noValueDelete(cars);
+        System.out.println(cars.toString());
 
         for (int i = 0; i < cars.size(); i++) {
             cars.set(i, cars.get(i).strip());
             String value = cars.get(i);
 
-            noValueDelete(value);
-            over5(value);
             commaX(value);
+            over5(value);
         }
 
         return cars;
@@ -38,37 +40,48 @@ public class Input {
     }
 
     // 예외 : (,)이 아닌 구분자 사용 (ex) 모든 언어의 문자(알파벳),숫자,공백을 제외한 나머지 입력 문자들은 전부 예외처리
-    public static void commaX(String inputValue) {
+    private static void commaX(String inputValue) {
         if (!inputValue.matches("^[\\p{L}\\p{N}\\s]*$")) {
-            throw new IllegalArgumentException("쉼표(,)가 아닌 구분자가 입력되었습니다.\n");
+            throw new IllegalArgumentException("쉼표(,)가 아닌 구분자가 입력되었습니다.");
         }
     }
 
     // 예외 : 입력된 값 X
-    public static void noValueEntered(List<String> cars) {
+    private static void noValueEntered(List<String> cars) {
         if (cars.size() == 1 && cars.getFirst().isEmpty()) {
-            throw new IllegalArgumentException("입력된 값이 없습니다.\n");
+            throw new IllegalArgumentException("입력된 값이 없습니다.");
         }
     }
 
     // 예외 : 0 이하
-    public static void zeroLess(int inputValue) {
+    private static void zeroLess(int inputValue) {
         if (inputValue <= 0) {
-            throw new IllegalArgumentException("[ 시도 횟수 ] 0보다 큰 정수를 입력해야 합니다.\n");
+            throw new IllegalArgumentException("[ 시도 횟수 ] 0보다 큰 정수를 입력해야 합니다.");
         }
     }
 
     // 예외 : 5글자 초과 (ex) J Kim : 5글자로 가능 / Ju Kim : 6글자로 불가능
-    public static void over5(String inputValue) {
+    private static void over5(String inputValue) {
         if (inputValue.length() > 5) {
-            throw new IllegalArgumentException("[ 자동차 이름 ] 5글자 이하로 입려해야 합니다.\n");
+            throw new IllegalArgumentException("[ 자동차 이름 ] 5글자 이하로 입력해야 합니다.");
         }
     }
 
     // 공백일 경우 값 삭제
-    public static void noValueDelete(String inputValue) {
-        if (inputValue.isEmpty()) {
-            cars.remove(inputValue);
+    private static void noValueDelete(List<String> cars) {
+        cars.removeIf(String::isBlank);
+    }
+
+    // Test
+    public static void test(String[] args) {
+        try {
+            List<String> testNameCars = inputCars();
+            System.out.printf("Successful: 자동차 이름 %s\n", testNameCars);
+
+            int testNumTry = inputNumTry();
+            System.out.printf("Successful: 시도 횟수 %d\n", testNumTry);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Error");
         }
     }
 }
