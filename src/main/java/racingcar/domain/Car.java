a1d853c6b3e89aca01655318f5a53e0bdec9c0b7
dbdb456 <adad568@naver.com>
2024-10-24 15:48:59 +0900

    feat(domain): Car 클래스 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,29 @@
 package racingcar.domain;
 
 public class Car {
+
+    private String name;
+    private String count = "";
+
+    public Car(String name, String count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    public void addCount() {
+        this.count += "-";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
