bce629263160c5c70477f19644003183179cb8a8
chaerish <ccyy8432@naver.com>
2024-10-24 18:29:47 +0900

    test(RaceJudgeTest): RaceJudge 검증 테스트 구현
    경주 우승자를 가려내는 RaceJudge를 검증하는 테스트를 구현한다.

---
+++ src/test/java/racingcar/RaceJudgeTest.java
@@ -0,0 +1,29 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class RaceJudgeTest {
+
+    @Test
+    void 거리_길이가_가장_큰_우승자_찾기() {
+        ArrayList<RacingCar> racingCars = new ArrayList<>(
+                Arrays.asList(new RacingCar("pobi1"), new RacingCar("pobi2")));
+        racingCars.get(0).race();
+        String[] winner = RaceJudge.determineWinner(racingCars);
+        assertThat(winner).isEqualTo(new String[]{"pobi1"});
+    }
+
+    @Test
+    void 길이가_같을때_공통_우승자_찾기() {
+        ArrayList<RacingCar> racingCars = new ArrayList<>(Arrays.asList(new RacingCar("pobi"), new RacingCar("pobi2")));
+        racingCars.get(0).race();
+        racingCars.get(1).race();
+        String[] winners = RaceJudge.determineWinner(racingCars);
+        assertThat(winners).isEqualTo(new String[]{"pobi", "pobi2"});
+    }
+
+}
