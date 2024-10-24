8d47b8f21b1e3e8f227d246fccb3827e2782f803
garden14 <ci667@dgu.ac.kr>
2024-10-24 17:05:03 +0900

    feat(CarNameValidation): 자동차 이름이 5자를 초과할 때 예외처리

--- src/main/java/racingcar/validation/CarNameValidation.java
+++ src/main/java/racingcar/validation/CarNameValidation.java
@@ -20,4 +20,10 @@ public class CarNameValidation {
             throw new IllegalArgumentException("자동차 이름이 빈 값입니다.");
         }
     }
+    public void isNameLengthValid(List<String> carNameList){
+        boolean hasInvalidName = carNameList.stream().anyMatch(name -> name.length() > 5);
+        if(hasInvalidName){
+            throw new IllegalArgumentException("이름의 길이는 5자 이하여야 합니다.");
+        }
+    }
 }
