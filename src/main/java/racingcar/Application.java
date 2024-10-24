5ccdd5529a8d06038889c5a1958d0aaeb5282adb
jaewon07 <jowonjae07@gmail.com>
2024-10-25 02:01:02 +0900

    feat: Limit car name length
    Implemented functionality to restrict car names to a maximum of 5 characters.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,18 +8,21 @@ import java.util.StringTokenizer;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String CarName = Console.readLine();
+        String carNamesInput = Console.readLine();
 
         List<Car> cars = new ArrayList<>();
-        StringTokenizer tokenizer = new StringTokenizer(CarName, ",");
+        StringTokenizer tokenizer = new StringTokenizer(carNamesInput, ",");
 
         while (tokenizer.hasMoreTokens()) {
             String carName = tokenizer.nextToken();
+
+            if (carName.length() >= 6) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하이어야 합니다.");
+            }
+
             cars.add(new Car(carName));
         }
-
 
     }
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,9 +3,11 @@ package racingcar;
 public class Car {
 
     public String name;
+
     public int score=0;
 
     public Car(String name) {
         this.name = name;
     }
+
 }
