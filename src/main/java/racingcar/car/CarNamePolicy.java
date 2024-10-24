4b2c33ea0b6245a3d53e786b1132ba937cf50c9a
plan11plan <tjtfy7028@gmail.com>
2024-10-24 15:26:21 +0900

    feat: 자동차 도메인 정책 상수들을 별도 클래스로 분리
    - 자동차 이름 정책 CarNamePolicy 클래스 추가
      - 최대 길이(MAX_LENGTH) 상수화
      - 최소 길이(MIN_LENGTH) 상수화
    - 자동차 이동 정책 MovementPolicy 클래스 추가
      - 초기 위치(INITIAL_POSITION) 상수화
      - 최소/최대 이동 거리(MIN_STEP, MAX_STEP) 상수화
    
    중복 코드 방지, 유지보수성 향상을 위해 정책 상수 분리

---
+++ src/main/java/racingcar/car/CarNamePolicy.java
@@ -0,0 +1,9 @@
+package racingcar.car;
+
+public class CarNamePolicy {
+    public static final int MAX_LENGTH = 10;
+    public static final int MIN_LENGTH = 1;
+
+    private CarNamePolicy() {
+    }
+}
---
+++ src/main/java/racingcar/car/MovementPolicy.java
@@ -0,0 +1,9 @@
+package racingcar.car;
+
+public class MovementPolicy {
+    public static final int INITIAL_POSITION = 0;
+    public static final int MIN_STEP = 1;
+    public static final int MAX_STEP = 100;
+
+    private MovementPolicy() {}
+}
