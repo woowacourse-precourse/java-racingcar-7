6371eb1ba2e97535ccb9b6d33bddf702ffa29f56
jinno Yun <jinno321@naver.com>
2024-10-24 21:16:09 +0900

    feat: Car - move(전진하기) 메서드 작성

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,11 +1,15 @@
 package racingcar.model;
 
+import racingcar.util.RandomNumberGenerator;
+
 public class Car {
     private final String name;
     private int position;
+    private final RandomNumberGenerator numberGenerator;
 
-    public Car(String name) {
+    public Car(String name, RandomNumberGenerator numberGenerator) {
         this.name = name;
+        this.numberGenerator = numberGenerator;
         this.position = 0;
     }
 
@@ -16,5 +20,13 @@ public class Car {
     public int getPosition() {
         return position;
     }
+
+    public void move(){
+        int randomNumber = numberGenerator.generateRandomNumber();
+        if(4 <= randomNumber && randomNumber <= 9){
+            position++;
+        }
+    }
+
 
 }
