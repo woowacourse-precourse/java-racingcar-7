569593c0b86866f43f01e5ad0e7dd644eeab8844
Hoon <xianse10@gmail.net>
2024-10-24 14:05:32 +0900

    메인스캐너 작성|| 자동차이름입력, 시도횟수 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,7 +6,28 @@ public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("");
+        try {
+            // 자동차 이름 입력 요청
+            System.out.println("경주에 사용할 자동차 이름을 입력하시오.(이름은 쉼표(,) 기준으로 구분) ");
+            String inputNames = scanner.nextLine(); // 자동차 이름  입력
+            String[] carNames = inputNames.split(",");
 
+            // 사용자에게 시도 횟수 입력 요청
+            System.out.println("시도할 횟수는 몇 회 인가요?");
+            int attempts = Integer.parseInt(scanner.nextLine());// 몇번 시도할것인지 횟수 입력
+
+            if(attempts <= 0){
+                throw new IllegalArgumentException("시도 횟수는 1회 이상이어야 합니다"); // 1 회 이상이 아닐경우 예외발생
+            }
+
+            //  게임 객체에서 가져온후 경주
+
+
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        } finally {
+            scanner.close();
+        }
 
     }
 }
