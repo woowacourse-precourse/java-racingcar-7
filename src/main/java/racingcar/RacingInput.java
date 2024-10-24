ebcec33747c5797de1217cb0ef68a8057ef6af91
leesh <leesh7807@gmail.com>
2024-10-24 17:55:13 +0900

    docs: add Javadoc to RacingInput class

--- src/main/java/racingcar/RacingInput.java
+++ src/main/java/racingcar/RacingInput.java
@@ -4,16 +4,35 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The class {@code RacingInput} manages game input from {@code System.in}.
+ *
+ * <p>It receives two pieces of information: car names separated by commas,
+ * and the total number of rounds for the gameplay.</p>
+ */
 public class RacingInput {
     private static final int MIN_ROUNDS = 1;
     private static final int MAX_ROUNDS = 200;
 
+    /**
+     * Receives car names separated by commas from {@code System.in} and returns them as a {@code List}. Leading and
+     * trailing whitespace in each car name is automatically removed by {@link String#strip()}.
+     *
+     * @return a list of car names
+     * @throws IllegalArgumentException if a car name is empty or exceeds 5 words
+     */
     public List<String> receiveCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String rawCarNames = Console.readLine();
         return extractCarNames(rawCarNames);
     }
 
+    /**
+     * Receives the total number of rounds for the game from {@code System.in} and returns it.
+     *
+     * @return the total number of rounds
+     * @throws IllegalArgumentException if the total number of rounds is not a positive integer or exceeds 200
+     */
     public int receiveTotalRounds() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String rawTotalRounds = Console.readLine();
