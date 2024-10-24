4a662847b743fe8ccbd14080fae83c6c3871ce25
soohalee <suha630@naver.com>
2024-10-24 17:01:44 +0900

    test(ApplicationTest): 자동차 이름이 영어대소문자 및 숫자가 아니면 예외 발생 기능 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -12,24 +14,38 @@ class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
-    @Test
-    void 기능_테스트() {
-        assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "sooha,\t,good",
+            "s;oha,java,good",
+            "sooha,java,go'od",
+            "sooha,java, "
+    })
+    void 예외_테스트_자동차이름입력시_영어대소문자와숫자_아니면_예외(String input) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
+//    @Test
+//    void 기능_테스트() {
+//        assertRandomNumberInRangeTest(
+//            () -> {
+//                run("pobi,woni", "1");
+//                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+//            },
+//            MOVING_FORWARD, STOP
+//        );
+//    }
+//
+//    @Test
+//    void 예외_테스트() {
+//        assertSimpleTest(() ->
+//            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+//                .isInstanceOf(IllegalArgumentException.class)
+//        );
+//    }
 
     @Override
     public void runMain() {
