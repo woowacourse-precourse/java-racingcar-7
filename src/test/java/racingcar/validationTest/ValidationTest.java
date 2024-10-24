367a6b087d2a4a4abde554c983a5f17bc17932a0
greensnapback0229 <smdmim@gmail.com>
2024-10-25 03:27:01 +0900

    test(ValidationTest): add validation test for round count
     - condition : natural number and range in integer
     - add right case & wrong case

--- src/test/java/racingcar/validationTest/ValidationTest.java
+++ src/test/java/racingcar/validationTest/ValidationTest.java
@@ -1,31 +1,54 @@
 package racingcar.validationTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import racingcar.util.Validation;
 
 public class ValidationTest {
 
-	@Test
-	@DisplayName("차 이름 입력 유효성 검사")
-	public void validCarNameTest(){
+    private Validation validation = new Validation();
 
-		//given
-		Validation validation = new Validation();
+    @Test
+    @DisplayName("차 이름 입력 유효성 검사 테스트")
+    public void validCarNameTest() {
 
-		//when & then
-		// - right input case
-		assertDoesNotThrow(() -> validation.validateCarNameInput("name1,name2,nm3,n4,5,CarNM"));
-		assertDoesNotThrow(() -> validation.validateCarNameInput("name1"));
+        //given : this.validation
 
-		// - wrong input case
-		assertThrows(IllegalArgumentException.class, () -> validation.validateCarNameInput(",name,name2"));
-		assertThrows(IllegalArgumentException.class, () -> validation.validateCarNameInput("name,,name2"));
-		assertThrows(IllegalArgumentException.class, () -> validation.validateCarNameInput("name,name2,"));
-		assertThrows(IllegalArgumentException.class, () -> validation.validateCarNameInput("name_over_than_five,name"));
-		assertThrows(IllegalArgumentException.class, () -> validation.validateCarNameInput("sixcar"));
-	}
+        //when & then
+        // - right input case
+        assertDoesNotThrow(() -> this.validation.validateCarNameInput("name1,name2,nm3,n4,5,CarNM"));
+        assertDoesNotThrow(() -> this.validation.validateCarNameInput("name1"));
+
+        // - wrong input case
+        assertThrows(IllegalArgumentException.class, () -> this.validation.validateCarNameInput(",name,name2"));
+        assertThrows(IllegalArgumentException.class, () -> this.validation.validateCarNameInput("name,,name2"));
+        assertThrows(IllegalArgumentException.class, () -> this.validation.validateCarNameInput("name,name2,"));
+        assertThrows(IllegalArgumentException.class,
+                () -> this.validation.validateCarNameInput("name_over_than_five,name"));
+        assertThrows(IllegalArgumentException.class, () -> this.validation.validateCarNameInput("sixcar"));
+    }
+
+    @Test
+    @DisplayName("차수 개수 입력 유효성 검사 테스트")
+    public void validRoundCountTest() {
+
+        //given : this.validation
+
+        //when & then
+        // - right input case
+        assertDoesNotThrow(() -> this.validation.validateRoundCount("5"));
+        assertThrows(IllegalArgumentException.class, () -> this.validation.validateRoundCount("2147483647"));
+
+        // - wrong input case
+        assertThrows(IllegalArgumentException.class, () -> this.validation.validateRoundCount("0"));
+        assertThrows(IllegalArgumentException.class, () -> this.validation.validateRoundCount("-10"));
+        assertThrows(IllegalArgumentException.class, () -> this.validation.validateRoundCount("c"));
+        assertThrows(IllegalArgumentException.class, () -> this.validation.validateRoundCount(" "));
+        assertThrows(IllegalArgumentException.class,
+                () -> this.validation.validateRoundCount("2147483648")); //over the int range
+        assertThrows(IllegalArgumentException.class, () -> this.validation.validateRoundCount(null));
+    }
 }
