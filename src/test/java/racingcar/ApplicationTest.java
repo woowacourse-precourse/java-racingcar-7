1901aaf7dc5aebe8d2b34a092d0cc5dc8ea44021
mingyeongbae <bmg1203@naver.com>
2024-10-24 16:42:56 +0900

    [test]: 정상 기능 테스트를 위한 테스트들 추가
    정상적인 기능을 출력하는 테스트들을 추가합니다.
    - 시도 1, 우승자 1
    - 시도 1, 우승자 2
    - 시도 3, 우승자 1
    - 시도 3, 우승자 2

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,61 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 기능_테스트_우승자_여러명() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,jun", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "jun : -", "최종 우승자 : pobi, jun");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 기능_테스트_시도_여러번_우승자_한명() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "3");
+                    //첫 번째 시도 출력
+                    assertThat(output()).contains("pobi : -", "woni : -");
+
+                    //두 번째 시도 출력
+                    assertThat(output()).contains("pobi : ", "woni : -");
+
+                    //세 번째 시도 출력
+                    assertThat(output()).contains("pobi : ", "woni : ");
+
+                    //최종 우승자 출력
+                    assertThat(output()).contains("최종 우승자 : woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, STOP, MOVING_FORWARD, STOP, STOP
+        );
+    }
+
+    @Test
+    void 기능_테스트_시도_여러번_우승자_여러명() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,jun", "3");
+                    //첫 번째 시도 출력
+                    assertThat(output()).contains("pobi : -", "woni : -", "jun : ");
+
+                    //두 번째 시도 출력
+                    assertThat(output()).contains("pobi : ", "woni : ", "jun : ");
+
+                    //세 번째 시도 출력
+                    assertThat(output()).contains("pobi : ", "woni : -", "jun : ");
+
+                    //최종 우승자 출력
+                    assertThat(output()).contains("최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, STOP,
+                MOVING_FORWARD, STOP, STOP,
+                STOP, MOVING_FORWARD, STOP
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
