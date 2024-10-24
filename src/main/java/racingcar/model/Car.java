ee42d5b93267440261158a8f75455d5d93395825
w0dnj5 <jw991211@icloud.com>
2024-10-24 19:02:28 +0900

    feat(Car): Car 클래스 기능 추가
    - 객체 생성 시 유효성 검사 실행
      - 이름이 빈 문자열인지 확인
      - 이름이 5글자 이상인지 확인
      - 검사 실패 시 에러를 발생
    - Comparble 인터페이스를 통해 객체를 정렬
      - 위치(location)을 기준으로 내림차순 정렬
    - equals와 hashcode를 오버라이드
      - 이름이 같다면 서로 같은 객체로 간주

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,70 @@
+package racingcar.model;
+
+import java.util.Objects;
+
+public class Car implements Comparable<Car> {
+
+    private static final int NAME_LENGTH_LIMIT = 5;
+    private static final int MOVEABLE_LIMIT = 4;
+    private final String name;
+    private int location;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+        location = 0;
+    }
+
+    private void validateName(String name) {
+        if (isBlank(name) || isTooLong(name)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isBlank(String name) {
+        return name.isBlank();
+    }
+
+    private boolean isTooLong(String name) {
+        return name.length() > NAME_LENGTH_LIMIT;
+    }
+
+    public void move(int number) {
+        if (isMoveable(number)) {
+            location++;
+        }
+    }
+
+    private boolean isMoveable(int number) {
+        return number >= MOVEABLE_LIMIT;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Car car)) {
+            return false;
+        }
+        return Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public int compareTo(Car car) {
+        return car.location - this.location;
+    }
+}
---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,58 @@
+package racingcar.model;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CarTest {
+
+    // 올바르지 않은 이름에 대한 예외 테스트
+    @ParameterizedTest
+    @ValueSource(strings = {"abcdef", "우아한테크코스", "qwerty", "", " ", "   "})
+    void throwExceptionIfNameIsInvalid(String name) {
+        Assertions.assertThatThrownBy(() -> new Car(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    // 입력한 숫자에 따라서 위치가 제대로 변경되는 지 테스트
+    @ParameterizedTest
+    @CsvSource({
+            "1, 0",
+            "2, 0",
+            "3, 0",
+            "4, 1",
+            "5, 1",
+            "6, 1"
+    })
+    void shouldReturnCorrectLocation(int number, int location) {
+        Car car = new Car("A");
+        car.move(number);
+
+        Assertions.assertThat(car.getLocation())
+                .isEqualTo(location);
+    }
+
+    @Test
+    void shouldBeEqualWhenNamesMatch() {
+        Car car1 = new Car("A");
+        Car car2 = new Car("A");
+
+        Assertions.assertThat(car1).isEqualTo(car2);
+    }
+
+    @Test
+    void shouldSortedByLocationDesc() {
+        Car carA = new Car("A");
+        carA.move(3);
+
+        Car carB = new Car("B");
+        carB.move(5);
+
+        List<Car> cars = List.of(carA, carB);
+        Assertions.assertThat(cars.stream().sorted().toList())
+                .isEqualTo(List.of(carB, carA));
+    }
+}
