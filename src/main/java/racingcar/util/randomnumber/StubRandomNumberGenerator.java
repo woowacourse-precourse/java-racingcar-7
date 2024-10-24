f70f7323aa2e851611ec2688cd778af2d8b529b5
DongYeop <s01054830455@gmail.com>
2024-10-25 00:46:20 +0900

    test: 랜덤값에 따른 자동차 전진 테스트
    - 테스트용 Stub 객체를 생성하여 외부에서 랜덤값을 주입받아 랜덤값을 임의로 지정할 수 있다.

---
+++ src/main/java/racingcar/util/randomnumber/StubRandomNumberGenerator.java
@@ -0,0 +1,15 @@
+package racingcar.util.randomnumber;
+
+public class StubRandomNumberGenerator implements RandomNumberStrategy {
+
+    private final int randomNumber;
+
+    public StubRandomNumberGenerator(int randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
+    @Override
+    public int generateRandomNumber() {
+        return this.randomNumber;
+    }
+}
--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -5,30 +5,32 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.util.randomnumber.RandomNumberGenerator;
+import racingcar.util.randomnumber.StubRandomNumberGenerator;
 
 class CarTest {
 
-    @DisplayName("name으로 Car를 생성할 수 있다.")
+    @DisplayName("이름을 가진 Car를 생성할 수 있다.")
     @Test
     void createCarByName() {
         //given
         String carName = "pobi";
 
         //when
-        Car car = Car.from(carName);
+        Car car = Car.from(carName, new RandomNumberGenerator());
 
         //then
         assertThat(car.getName()).isEqualTo(carName);
     }
 
-    @DisplayName("Car의 초기 position은 0 이다.")
+    @DisplayName("Car가 생성될 때 position은 0 이다.")
     @Test
     void createCarSetPositionIsZero() {
         //given
         String carName = "woni";
 
         //when
-        Car car = Car.from(carName);
+        Car car = Car.from(carName, new RandomNumberGenerator());
 
         //then
         assertThat(car.getPosition()).isEqualTo(0);
@@ -41,7 +43,35 @@ class CarTest {
         String carName = "javaji";
 
         //when //then
-        assertThatThrownBy(() -> Car.from(carName))
+        assertThatThrownBy(() -> Car.from(carName, new RandomNumberGenerator()))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("랜덤값이 4보다 크다면 전진한다.")
+    @Test
+    void moveForward() {
+        //given
+        int randomNumber = 4;
+        Car car = Car.from("pobi", new StubRandomNumberGenerator(randomNumber));
+
+        //when
+        car.isMove();
+
+        //then
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("랜덤값이 4보다 작다면 전진하지 않는다.")
+    @Test
+    void notMoveForward() {
+        //given
+        int randomNumber = 3;
+        Car car = Car.from("pobi", new StubRandomNumberGenerator(randomNumber));
+
+        //when
+        car.isMove();
+
+        //then
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 }
