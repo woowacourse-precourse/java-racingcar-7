ed7d6a90ea6deac63618026b1a4d776ab3c6aeb6
onesay17 <onesay17@gmail.com>
2024-10-24 11:22:55 +0900

    feat: 자동차 이동 출력 및 우승자 가려내는 로직 구현
    - 자동차 이동 시 무작위 값이 4 이상일 때 전진하도록 수정
    - 각 자동차의 이동 거리를 '-'로 시각적으로 출력하는 기능 추가
    - 최대 이동 거리를 기준으로 우승자를 가려내는 로직 구현
    - 중복이 허용되지 않고 순서를 유지하는 우승자 컬렉션을 LinkedHashSet으로 변경하여 우승자 중복 방지 및 순서 보장
    - 우승자가 여러 명일 경우 쉼표(,)로 구분하여 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,13 +7,15 @@ package racingcar;
 [x]각 자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
 [x]자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
 [x]사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.
-[]전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
-[]자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
-[]우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.
-[]사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.
+[x]전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
+[x]자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
+[x]우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.
+[x]사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.
 */
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 
 public class Application {
@@ -21,19 +23,50 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분");
         String readLine = Console.readLine();
-        LinkedHashSet<String> members = new LinkedHashSet<>();
+        LinkedHashMap<String, Integer> members = new LinkedHashMap<>();
 
         String[] split = readLine.split(",");
         for (String s : split) {
-            if (s.length() <= 5) {
+            if (s.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능하다.");
             }
-            members.add(s);
+            members.put(s, 0);
         }
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         Integer count = Integer.valueOf(Console.readLine());
         System.out.println(count);
 
+        System.out.println("실행 결과");
+        for (int i = 0; i < count; i++) {
+            for (String s : members.keySet()) {
+                int randomCount = Randoms.pickNumberInRange(0, 9);
+                if (randomCount >= 4) {
+                    members.put(s, members.get(s) + randomCount);
+                }
+            }
+
+            for (String s : members.keySet()) {
+                System.out.println(s + " : " + "-".repeat(members.get(s)));
+            }
+            System.out.println();
+        }
+
+        int max = 0;
+        for (int carCount : members.values()) {
+            if (carCount > max) {
+                max = carCount;
+            }
+        }
+
+        LinkedHashSet<String> winners = new LinkedHashSet<>();
+        for (String name : members.keySet()) {
+            if (members.get(name) == max) {
+                winners.add(name);
+            }
+        }
+
+        // TODO: 우승자가 한명일때는 쉽표가 없도록 구현
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
