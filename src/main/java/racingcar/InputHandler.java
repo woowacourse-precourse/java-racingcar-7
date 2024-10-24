0e1408231509a77994ede6180c37c7fb080b1395
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-25 00:59:40 +0900

    style(InputHandler): remove 불필요한 주석

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -17,24 +17,22 @@ public class InputHandler {
         }
     }
 
-    // 사용자로부터 자동차 이름을 입력받음
     private String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return getSafeInput();
     }
 
-    // 사용자로부터 시도 횟수를 입력받음
     private String inputAttemptCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         return getSafeInput();
     }
 
-    // 빈 문자열 입력받기
+    // 빈 문자열도 입력 받기
     private String getSafeInput() {
         try {
             return Console.readLine();
         } catch (NoSuchElementException e) {
-            return ""; // 빈 문자열로 처리
+            return "";
         }
     }
 }
