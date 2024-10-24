95d9324d5ad8364e0518435677fe7753840998b7
bk <naturalbb360@gmail.com>
2024-10-24 15:31:26 +0900

    feat(Application): 자동차 이름 입력 검증 기능 추가
    - 자동차 이름이 영어로만 구성되었는지 검증
    - 자동차 이름이 5자 이하인지 검증
    - 경주에 참여할 자동차가 최소 2대 이상 10대 미만인지 검증

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,6 +7,10 @@ import java.util.Arrays;
 
 public class Application {
     private static final String START_TEXT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final int CAR_NAME_MAX_LENGTH = 5;
+    private static final int CAR_MAX_COUNT = 10;
+    private static final int CAR_MIN_COUNT = 2;
+
 
     private static String getInput(){
         String inputString = Console.readLine();
@@ -19,9 +23,38 @@ public class Application {
         return carNames;
     }
 
+    private static String isAlphabeticName(String carName){
+        for(char c : carName.toCharArray()){
+            if(!Character.isAlphabetic(c)){
+                throw new IllegalArgumentException("자동차 이름은 알파벳으로만 구성되어야 합니다.");
+            }
+        }
+        return carName.toLowerCase();
+    }
+
+    private static void isValidlengthName(String carName){
+        if (carName.length() > CAR_NAME_MAX_LENGTH){
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
+    private static void validCarNames(ArrayList<String> carNames){
+        for(int i=0; i<carNames.size(); i++){
+            carNames.set(i,isAlphabeticName(carNames.get(i)));
+            isValidlengthName(carNames.get(i));
+        }
+    }
+
+    private static void validCarCount(ArrayList<String> carNames){
+        if ((carNames.size() < CAR_MIN_COUNT) || (carNames.size() > CAR_MAX_COUNT)){
+            throw new IllegalArgumentException("자동차 대수는 2대 이상 10대 이하여야 합니다.");
+        }
+    }
+
     private static ArrayList<String> getCarNames(){
         String inputString = getInput();
         ArrayList<String> carNames = splitCarNames(inputString);
+        validCarCount(carNames);
+        validCarNames(carNames);
         return carNames;
     }
 
