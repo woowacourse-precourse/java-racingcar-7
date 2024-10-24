f80baa6e6ed2699124bee1e8a7aafe40108ec2ca
kjyyjk <rlawnsdud920@gmail.com>
2024-10-25 03:17:02 +0900

    feat: 자동차의 이름이 6자 이상인 경우 예외를 발생한다
    - Car를 생성할 때 생성자 내에서 이름의 길이를 확인하고,
    6글자가 이상이면 IllegalArgumentException을 발생한다

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,6 +4,10 @@ public class Car {
     private final String name;
 
     public Car(final String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+
         this.name = name;
     }
 }
