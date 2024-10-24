353d7a724d7808ade679123f6c5f9c2e454a5c63
taek2222 <oot309@daum.net>
2024-10-24 10:28:30 +0900

    test(InputViewTest): 사용자 입력 테스트 코드
    - 사용자 입력의 정상 구동과 예외 처리 부분을 검증하는 테스트 코드입니다.
    - 예외 테스트는 View 의 최소 검증 로직을 따라 설계했습니다.
    - 재사용 및 반복되는 케이스는 어노테이션으로 묶어 활용했습니다.

---
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -0,0 +1,79 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class InputViewTest {
+
+    private InputView inputView;
+
+    @BeforeEach
+    void beforeEach() {
+        inputView = new InputView();
+    }
+
+    @AfterEach
+    void afterEach() {
+        Console.close();
+    }
+
+    @Test
+    void 자동차_입력_테스트() {
+        // given
+        String input = "pobi";
+        provideSetInputStream(input);
+
+        // when
+        String inputCarsName = inputView.inputCarsName();
+
+        // then
+        Assertions.assertEquals(input, inputCarsName);
+    }
+
+    @Test
+    void 게임_횟수_입력_테스트() {
+        // given
+        String input = "5";
+        provideSetInputStream(input);
+
+        // when
+        int inputPlayCount = inputView.inputPlayCount();
+
+        // then
+        Assertions.assertEquals(input, String.valueOf(inputPlayCount));
+    }
+
+    @ParameterizedTest(name = "입력값: \"{0}\"")
+    @ValueSource(strings = {"      ", ",", "\n"})
+    void 자동차_입력_예외_테스트(String input) {
+        // given
+        provideSetInputStream(input);
+
+        // when & then
+        Assertions.assertThrows(IllegalArgumentException.class,
+                inputView::inputCarsName
+        );
+    }
+
+    @ParameterizedTest(name = "입력값: \"{0}\"")
+    @ValueSource(strings = {"0", "1000001"})
+    void 게임_횟수_입력_최소_예외_테스트(String input) {
+        // given
+        provideSetInputStream(input);
+
+        // when & then
+        Assertions.assertThrows(IllegalArgumentException.class,
+                inputView::inputPlayCount
+        );
+    }
+
+    private void provideSetInputStream(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+}
