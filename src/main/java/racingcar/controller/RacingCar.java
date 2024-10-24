cd65de30be73fb0ec6ac8e9deb30efa866fd2bb5
sc@t0a14 <gusdn6671@gmail.com>
2024-10-24 20:36:51 +0900

    feat(RacingCar): 자동차 이름 목록을 문자열 리스트로 저장한다

--- src/main/java/racingcar/controller/RacingCar.java
+++ src/main/java/racingcar/controller/RacingCar.java
@@ -1,5 +1,7 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import racingcar.model.Car;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -8,7 +10,14 @@ public class RacingCar {
     public void run(){
         OutputView.printCarNamesPrompt();
         InputView inputView = new InputView();
-        String car = inputView.userInput();
+
+        String carInput = inputView.userInput();
+        String[] carInputs = carInput.split(",");
+
+        List<String> cars = new ArrayList<>();
+        for (int i = 0; i < carInputs.length; i++) {
+            cars.add(carInputs[i]);
+        }
 
 
     }
