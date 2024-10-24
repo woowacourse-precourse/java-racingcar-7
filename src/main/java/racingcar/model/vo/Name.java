90a1572a13621130e1ef16f051d1d3941c1e3ca7
FhRh <yiyop@naver.com>
2024-10-24 18:21:31 +0900

    feat(Name) : 자동차의 이름은 영문자로 이루어져야 한다

--- src/main/java/racingcar/model/vo/Name.java
+++ src/main/java/racingcar/model/vo/Name.java
@@ -5,8 +5,20 @@ public record Name(String name) {
     private static final int MAX_NAME_LENGTH = 5;
 
     public Name {
+        validateLessThanFiveCharacters(name);
+        validateAlphabeticString(name);
+    }
+
+    private void validateLessThanFiveCharacters(String name) {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
         }
     }
+
+    private void validateAlphabeticString(String name) {
+        if (name.isBlank() || !name.matches("^[a-zA-Z]*$")) {
+            throw new IllegalArgumentException("이름은 영문자로 이루어져야 합니다.");
+        }
+    }
+
 }
--- src/test/java/racingcar/model/vo/NameTest.java
+++ src/test/java/racingcar/model/vo/NameTest.java
@@ -2,8 +2,13 @@ package racingcar.model.vo;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import java.math.BigInteger;
+import org.assertj.core.internal.Numbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("자동차 이름 테스트")
 public class NameTest {
@@ -13,6 +18,13 @@ public class NameTest {
         // given
         String name = "pobiii";
 
+        // when & then
+        assertThatIllegalArgumentException().isThrownBy(() -> new Name(name));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "pobi ", "12345", "포비", "pobi★"})
+    void 자동차의_이름은_영문자열_이어야_한다(String name) {
         // when & then
         assertThatIllegalArgumentException().isThrownBy(() -> new Name(name));
     }
