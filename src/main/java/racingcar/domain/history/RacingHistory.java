c3b0125c2ad32812109510865b34ce6c55022ff5
jijongkwon <help3451@naver.com>
2024-10-25 01:30:08 +0900

    feat(domain) : RacingHistory 클래스 생성
    filed
    - racingCarName : 레이싱 카 이름
    - distance : 이동 거리
    
    TODO
    - 이름반환
    - 거리 반환
    - 거리 출력 반환

---
+++ src/main/java/racingcar/domain/history/RacingHistory.java
@@ -0,0 +1,26 @@
+package racingcar.domain.history;
+
+public class RacingHistory {
+    private final String racingCarName;
+    private final int distance;
+
+    public RacingHistory(String racingCarName, int distance) {
+        this.racingCarName = racingCarName;
+        this.distance = distance;
+    }
+
+    // TODO : 이름 반환
+    public String getRacingCarName() {
+        return null;
+    }
+
+    // TODO : 거리 반환
+    public int getDistance() {
+        return 0;
+    }
+
+    // TODO : 거리 출력 반환
+    public String getPrintDistance() {
+        return null;
+    }
+}
