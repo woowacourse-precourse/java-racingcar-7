3c6fc8cd02776fb1fd77ef20256e04cf6f5835f4
soojinlee <sojinlee1004@naver.com>
2024-10-24 22:37:59 +0900

    feat(controller) : 분리한 각 자동차의 양쪽 공백을 제거한다.

--- src/main/java/racingcar/RacingController.java
+++ src/main/java/racingcar/RacingController.java
@@ -6,9 +6,14 @@ public class RacingController {
     public RacingController(RacingInputView inputView) {
         this.inputView = inputView;
     }
-
     public String[] SplitCarNames(String RacingCarNames) {
         return RacingCarNames.split(",");
     }
-
+    private String[] trimCarNames(String[] carNames) {
+        String[] trimNames = new String[carNames.length];
+        for(int i = 0 ; i < carNames.length ;i++) {
+            trimNames[i] = carNames[i].trim();
+        }
+        return trimNames;
+    }
 }
