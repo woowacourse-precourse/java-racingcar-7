15551fa29d7556f19d147ef8adfd271946ad5506
Yun-tellije <dbswjddms517@naver.com>
2024-10-25 00:22:27 +0900

    feat: 자동차 이름 입력 기능 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,16 +1,20 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class GameController {
 
     private final OutputView outputView;
+    private final InputView inputView;
 
     public GameController() {
         this.outputView = new OutputView();
+        this.inputView = new InputView();
     }
 
     public void startGame() {
         outputView.startMessage();
+        String carNames = inputView.userInput();
     }
 }
