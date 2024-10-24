892a675b7b6a815ad44f3c0c50f4d117ce59a266
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-10-24 14:42:35 +0900

    feat: Add car name splitting feature
    - Remove whitespace
    - Create a list of ForwardController objects

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -10,6 +10,10 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
 
+        return List.of(input.split(",")).stream()
+                .map(String::trim)
+                .map(ForwardController::new)
+                .toList();
 
     }
 }
