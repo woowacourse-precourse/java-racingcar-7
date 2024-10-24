3dec5ddcfdcbf5184d38c299e97e9b863acd40a8
joonwan <joonwan1024@naver.com>
2024-10-24 23:37:13 +0900

    test: InputManagerTest 아무것도 입력 안할 경우 예외 발생 테스트 코드 작성

--- src/test/java/racingcar/InputManagerTest.java
+++ src/test/java/racingcar/InputManagerTest.java
@@ -1,7 +1,7 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -10,8 +10,8 @@ import org.junit.jupiter.api.Test;
 
 class InputManagerTest {
 
-    @Test
     @DisplayName("사용자에게 입력받으면 입력받은 데이터를 그대로 반환해야 한다.")
+    @Test
     void getUserInput() {
         //given
         String userInput = "this is test message";
@@ -23,6 +23,19 @@ class InputManagerTest {
 
         //then
         assertThat(result).isEqualTo(userInput);
+    }
+
+    @DisplayName("빈 문자열을 입력받을 경우 예외를 발생시킨다.")
+    @Test
+    void emptyUserInput() {
+        //given
+        String userInput = "\n";
+        InputStream inputStream = getInputStream(userInput);
+        System.setIn(inputStream);
+
+        assertThatThrownBy(() -> InputManager.readUserInput())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("빈 문자열을 입력할 수 없습니다.");
     }
 
     private InputStream getInputStream(String userInput) {
