7918b83896f5f040a9983fc7685a3cfb12b053dc
kaswhy <sxshx818@naver.com>
2024-10-24 20:45:42 +0900

    test(InputValueTest): 경주할 자동차들이 null인 경우 테스트 추가
    변경 후
    - ValidateCarNames의 isNull에서 예외가 발생하는지 확인하는 테스트 작성

--- src/test/java/racingcar/InputValueTest.java
+++ src/test/java/racingcar/InputValueTest.java
@@ -1,24 +1,20 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.validator.ValidateCarNames;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class InputValueTest {
     private static final String CAR_NAMES_BLANK = "";
     private static final String CAR_NAMES_WHITE = " ";
+    private static final String CAR_NAMES_NULL = null;
 
     @DisplayName("경주할 자동차들이 빈 문자열이면 예외 발생")
     @Test
-    void ThrowExceptionIfCarNameBlank() {
+    void ThrowExceptionIfCarNamesBlank() {
         // given
         ValidateCarNames validateCarNames = new ValidateCarNames();
 
@@ -35,7 +31,7 @@ public class InputValueTest {
 
     @DisplayName("경주할 자동차들이 공백값이면 예외 발생")
     @Test
-    void ThrowExceptionIfCarNameBlankWhite() {
+    void ThrowExceptionIfCarNamesBlankWhite() {
         // given
         ValidateCarNames validateCarNames = new ValidateCarNames();
 
@@ -50,4 +46,19 @@ public class InputValueTest {
                 .hasMessageContaining("자동차 이름은 빈 값이 될 수 없습니다.");
     }
 
+    @DisplayName("")
+    @Test
+    public void ThrowExceptionIfCarNamesNULL() {
+        // given
+        ValidateCarNames validateCarNames = new ValidateCarNames();
+
+        // when
+        Throwable thrown = catchThrowable(() -> {
+            validateCarNames.isNull(CAR_NAMES_NULL);
+        });
+
+        // then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 null이 될 수 없습니다.");
+    }
 }
