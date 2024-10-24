e75dcc3c167ae129d50fcb3c0ecd968ed9c7588c
HyeonJeong <wgwjh05169@gmail.com>
2024-10-25 00:07:37 +0900

    feat(Car): 자동차 초기화 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,13 @@
 package racingcar;
 
 public class Car {
+    private final String name;
     private int score;
+
+    public Car(String name) {
+        this.name = name;
+        this.score = 0;
+    }
 
     public void go(int randomValue) {
         if (randomValue >= 4)
