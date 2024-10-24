bd7194aef3180da432f2ac15a67a1cc2c891b192
tmdcheol <leesc08392@gmail.com>
2024-10-24 22:08:05 +0900

    feat: 입력한 횟수만큼 이동시도 반복 수행 기능 추가
    - AttemptCountArgumentResolver 클래스 추가: 입력된 시도 횟수를 파싱하여 정수로 변환하는 기능 구현
    - Controller 클래스에서 시도 횟수를 입력받고 해당 횟수만큼 이동시도 수행

---
+++ src/main/java/racingcar/AttemptCountArgumentResolver.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class AttemptCountArgumentResolver {
+
+    public int parse(String parameter) {
+        return Integer.parseInt(parameter);
+    }
+
+}
--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -8,9 +8,12 @@ public class Controller {
     private final InputView inputView = new InputView();
 
     private final CarArgumentResolver carArgumentResolver = new CarArgumentResolver();
+    private final AttemptCountArgumentResolver attemptCountArgumentResolver = new AttemptCountArgumentResolver();
+
     private final SizeValidator sizeValidator = new SizeValidator();
 
     private final CarConverter carConverter = new CarConverter();
+    private final AllCarMover allCarMover = new AllCarMover();
 
     public void run() {
         outputView.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -22,6 +25,11 @@ public class Controller {
 
         outputView.println("시도할 횟수는 몇 회인가요?");
         String attemptCountRequest = inputView.read();
+        int attemptCount = attemptCountArgumentResolver.parse(attemptCountRequest);
+
+        for (int count = 0; count < attemptCount; count++) {
+            allCarMover.run(cars);
+        }
 
     }
 
