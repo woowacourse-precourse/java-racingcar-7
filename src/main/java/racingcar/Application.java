14971cb63a0496c48e9d52028c4cb1d2da34a32b
kjyyjk <rlawnsdud920@gmail.com>
2024-10-25 03:00:24 +0900

    feat: 자동차 이름에 아무 입력을 안했을 경우 예외를 발생한다
    - null이거나, 아무 입렵도 없거나, 공백 뿐이면 IllegalArgumentException을 발생한다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,11 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
+
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+
         new RacingGame().run(input);
     }
 }
