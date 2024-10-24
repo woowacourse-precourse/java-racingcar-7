06381edf105ddf0ff2a21b30dfbba8ff9d03ee80
howareyoo <newworld121@naver.com>
2024-10-24 23:02:01 +0900

    feat : 0~9사이 무작위 값 생성

---
+++ src/main/java/racingcar/DuringRacing.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class DuringRacing {
+
+    public static int getGoValue(){
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+}
