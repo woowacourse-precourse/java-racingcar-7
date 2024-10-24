0273745536fa8cf7c5296aec789f7a57d7888536
sunjong0214 <rlatjswhd8200@gmail.com>
2024-10-24 21:21:46 +0900

    refactor(RacingCar): 메서드 분리

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -4,13 +4,21 @@ public class RacingCar {
     private final String name;
 
     public RacingCar(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
-        }
+        validateLength(name);
+        validateNameRule(name);
+        this.name = name;
+    }
+
+    private static void validateNameRule(String name) {
         if (name.codePoints().anyMatch(c -> (!Character.isLetter(c) && Character.isWhitespace(c)))) {
             throw new IllegalArgumentException("이름은 문자만 가능합니다.");
         }
-        this.name = name;
+    }
+
+    private static void validateLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
     }
 
     public String name() {
