af6f6da9ed3341c0f0b2373e143f5fb3ed5d0b0d
HeungY <rkrkrkrk02@naver.com>
2024-10-24 23:03:53 +0900

    feat: print interface message

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -11,17 +11,18 @@ public class User {
     public List<String> grantCarName(){
         List<String> cars = new ArrayList<>();
 
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
         String[] carName = Console.readLine().split(",");
 
         Collections.addAll(cars, carName);
 
-        Console.close();
 
         return cars;
     }
 
     public int moves(){
-
+        System.out.println("시도할 횟수는 몇 회인가요?");
         int moves = Integer.parseInt(Console.readLine());
 
         Console.close();
