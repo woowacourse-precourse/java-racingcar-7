b273584cd08c4518c5c3b225acb76f8850ce3614
qyinm <qusseun@gmail.com>
2024-10-24 22:53:12 +0900

    feat: RandomPicker 의존 주입 객체 기능 추가
    테스트 코드 작성 시 Randoms.pickNumberInRange mock하기 위해 Wrapping 후 의존 객체 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.service.RacingCarService;
+import racingcar.util.RandomPicker;
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RandomPicker randomPicker = new RandomPicker();
+        RacingCarService racingCarService = new RacingCarService(randomPicker);
+        RacingCarController racingCarController = new RacingCarController(racingCarService);
+
+        racingCarController.run();
+
     }
 }
--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -4,12 +4,15 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Comparator;
 import java.util.List;
 import racingcar.car.RacingCar;
+import racingcar.util.RandomPicker;
 
 public class RacingCarService {
 
     private List<RacingCar> racingCars;
+    private final RandomPicker randomPicker;
 
-    public RacingCarService() {
+    public RacingCarService(RandomPicker randomPicker) {
+        this.randomPicker = randomPicker;
     }
 
     public List<RacingCar> logic(List<String> carNames, int moveCount) {
@@ -20,7 +23,7 @@ public class RacingCarService {
 
     public void process(List<RacingCar> racingCars) {
         racingCars.stream()
-                .filter(racingCar -> Randoms.pickNumberInRange(0, 9) >= 4)
+                .filter(racingCar -> randomPicker.pickNumberInRange(0, 9) >= 4)
                 .forEach(RacingCar::moveForwardOneStep);
     }
 
