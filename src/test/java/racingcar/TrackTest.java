86b0c6cf901c6ca3fc56dfd2a433be18ce2ba956
GaBaljaintheroom <junsu1222@naver.com>
2024-10-24 12:46:32 +0900

    test(TrackTest): 트랙의 횟수 범위 검증 테스트 작성

--- src/test/java/racingcar/TrackTest.java
+++ src/test/java/racingcar/TrackTest.java
@@ -1,9 +1,13 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class TrackTest {
 
@@ -29,6 +33,20 @@ class TrackTest {
 
         //then
         assertFalse(actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 100, 1000})
+    void 트랙의_횟수가_1회_이상이면_예외가_발생하지_않는다(int lap) {
+        assertDoesNotThrow(() -> Track.from(lap));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1})
+    void 트랙의_횟수가_최소값_보다_작으면_예외가_발생한다(int lap) {
+        assertThatThrownBy(() -> Track.from(lap))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("최소 1회 이상 횟수를 입력해주세요.");
     }
 
 }
