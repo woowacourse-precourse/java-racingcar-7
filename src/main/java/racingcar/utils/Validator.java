b0a1c592d7ffb02e79686b2f022dce2c6863b973
sc@t0a14 <gusdn6671@gmail.com>
2024-10-24 22:38:52 +0900

    feat(Validator): 자동차의 이름이 중복되었을 경우 예외를 발생한다

--- src/main/java/racingcar/utils/Validator.java
+++ src/main/java/racingcar/utils/Validator.java
@@ -1,5 +1,11 @@
 package racingcar.utils;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import racingcar.model.Car;
+import racingcar.model.Cars;
+
 public class Validator {
     public static void isEmpty(String name) {
         if(name == null || name.trim().isBlank()){
@@ -12,5 +18,13 @@ public class Validator {
             throw new IllegalArgumentException("자동차의 이름은 5글자 이하만 가능합니다");
         }
     }
+
+    public static void containDuplicate(List<Car> cars) {
+        Set<Car> uniqueCars = new HashSet<>(cars);
+        if(uniqueCars.size() != cars.size()){
+            throw new IllegalArgumentException("자동차의 이름은 중복될수 없습니다");
+        }
+    }
+
 
 }
