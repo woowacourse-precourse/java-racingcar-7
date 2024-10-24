6613f8e50a1b8b1b6798db19b892504be7be80a8
reindeer002 <lsj000424@gmail.com>
2024-10-24 18:48:41 +0900

    feat(main): Implement main function for racing application
    - Set up InputController and RaceController to handle user input and manage the race logic.
    - Retrieve car names and round count from user input.
    - Prepare the structure for race execution within the main function.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.controller.InputController;
+import racingcar.controller.RaceController;
+import racingcar.domain.CarDomain;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputController consoleController = new InputController();
+        RaceController raceController = new RaceController();
+
+        List<CarDomain> cars = consoleController.getCarNameRequest();
+        int round = consoleController.getRoundRequest();
+        
+        // RaceController 코드 입력
+
+        Console.close();
     }
 }
