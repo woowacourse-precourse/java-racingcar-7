43c6c65cffae3557a83bdabc8ab397eb6f9ec61e
choi <nippert@naver.com>
2024-10-24 14:57:23 +0900

    feat: 자동차 게임의 결과를 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,27 @@
 package view;
 
+import racingcar.domain.RacingCar;
+
+import java.util.List;
+
 public class OutputView {
+
+    private boolean isFirst = true;
+
+    public final void game(List<RacingCar> carNames) {
+        if (isFirst) {
+            System.out.println("실행 결과");
+            isFirst = false;
+        }
+
+        carNames.stream()
+                .map(car -> car.getName() + " : " + "-".repeat(car.getDistance())) // 자동차 이름과 거리 표시
+                .forEach(System.out::println); // 각 줄을 출력
+        System.out.println();
+    }
+
+    public final void printWinner(List<String> winners) {
+        System.out.print("최종 우승자 : ");
+        System.out.println(String.join(", ", winners));
+    }
 }
