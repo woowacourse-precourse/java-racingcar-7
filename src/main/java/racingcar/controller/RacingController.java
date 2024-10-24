a61c39a6ec8bc6aa8dbf78ea2a9f957b38358fe7
dongyun <ehddbs452100@naver.com>
2024-10-24 14:28:45 +0900

    feat(RacingController): call methods for typing car names and trying counts with controller
    - run()를 통해 프로그램을 실행하려 자동차 이름명 입력 및 검증, 시도횟수 입력 및 검증을 함
    - getSplittedCarNames()를 통해 분리한 이름들을 run 메소드 내에 변수로 따로 지정하여 전진횟수를 기록하기
    위해 따로 명시

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,5 +1,27 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.service.RacingService;
+import racingcar.view.InputView;
+
 public class RacingController {
+
+    private RacingService racingService;
+
+    public RacingController(RacingService racingService) {
+        this.racingService = racingService;
+    }
+
+    public void run() {
+        String carName = InputView.getCarName();
+        List<String> splittedCarNames = getSplittedCarNames(carName);
+        racingService.validateCarName(splittedCarNames);
+
+        String tryCounts = InputView.getTryCounts();
+        racingService.validateTryCount(tryCounts);
+    }
+    public List<String> getSplittedCarNames(String input) {
+        return racingService.splitCarName(input);
+    }
 
 }
