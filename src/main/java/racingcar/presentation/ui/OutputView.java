97649c27ac8f97f129e9fa284e911b25a094f95c
jijongkwon <help3451@naver.com>
2024-10-25 01:38:55 +0900

    feat(ui) : OutputView 생성
    TODO
    - outputRacingResultUi : 실행 결과 화면 출력
    - outputWinnerUi : 최종 우승자 화면 출력

---
+++ src/main/java/racingcar/presentation/ui/OutputView.java
@@ -0,0 +1,18 @@
+package racingcar.presentation.ui;
+
+import racingcar.domain.car.RacingCar;
+import racingcar.domain.winner.Winner;
+
+import java.util.List;
+
+public class OutputView {
+    // TODO: 실행 결과 화면 출력
+    public void outputRacingResultUi(List<RacingCar> racingCars) {
+
+    }
+
+    // TODO: 최종 우승자 화면 출력
+    public void outputWinnerUi(List<Winner> winners) {
+
+    }
+}
