d07d8d60699d66e50c825c5594dd7e0e26ac795b
박규영 <magic9282m@gmail.com>
2024-10-25 06:25:47 +0900

    fix: move loop in doRace method to main method
    indent depth 가 3을 넘지 않도록 하기 위해 doRace 메서드와 main 메서드 수정
    - round 만큼 doRace 함수를 반복적으로 호출하도록 변경
    - printRaceWinner 메서드 호출도 main 메서드에서 수행하도록 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -31,7 +31,12 @@ public class Application {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("올바른 정수를 입력해주세요.");
         }
-        doRace(round);
+
+        for(int i=0; i<round; i++) {
+            doRace();
+        }
+        // 모든 라운드가 끝난 뒤, 최종 우승자를 출력한다.
+        printRaceWinner();
     }
     public static String[] carNameSplitter(String input) {
         String[] result = input.split(",");
@@ -42,20 +47,16 @@ public class Application {
         }
         return result;
     }
-    public static void doRace(int round) {
-        for(int i=0; i<round; i++) {
-            for (int j = 0; j < carsRaceProgress.length; j++) {
-                if (Randoms.pickNumberInRange(0, 9) >= 4) {
-                    // 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우 자동차는 전진
-                    carsRaceProgress[j].append('-');
-                }
+    public static void doRace() {
+        for (int i = 0; i < carsRaceProgress.length; i++) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                // 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우 자동차는 전진
+                carsRaceProgress[i].append('-');
             }
-            printRaceRound();
-            // 각 차수별 실행 결과의 구분을 위해 빈 줄을 하나 출력한다.
-            System.out.println();
         }
-        // 모든 라운드가 끝난 뒤, 최종 우승자를 출력한다.
-        printRaceWinner();
+        printRaceRound();
+        // 각 차수별 실행 결과의 구분을 위해 빈 줄을 하나 출력한다.
+        System.out.println();
     }
     public static void printRaceRound() {
         for(int i=0; i<cars.length; i++) {
