6bf80d26e7f2b8686933e198cf8739c96a6c3039
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-25 00:01:11 +0900

    refactor($Application): 내부 로직을 함수로 분리
    - 가독성 향상.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -17,21 +17,44 @@ public class Application {
 
     private final static RacingService racingService = new RacingService();
 
+    // 필요 변수
+    private static RacingCars racingCars;
+    private static int tryCount;
+
     public static void main(String[] args) {
+        setUp();
+        RacingResult racingResult = playRacing();
+        printRacingResult(racingResult);
+    }
+
+    // core 함수
+    private static void setUp() {
+        setUpRacingCars();
+        setUpTryCount();
+    }
+
+    private static void setUpRacingCars() {
         String carNames = getCarNames();
 
         List<String> carNameList = getCarNameList(carNames);
 
-        RacingCars racingCars = createRacingCars(carNameList);
+        racingCars = createRacingCars(carNameList);
+    }
 
-        int tryCount = getTryCount();
+    private static void setUpTryCount() {
+        tryCount = getTryCount();
+    }
 
-        RacingResult racingResult = racingService.play(racingCars, tryCount);
+    private static RacingResult playRacing() {
+        return racingService.play(racingCars, tryCount);
+    }
 
+    private static void printRacingResult(RacingResult racingResult) {
         outputView.printRacingRoundResults(racingResult.roundResults());
         outputView.printWinners(racingResult.winners());
     }
 
+    // helper 함수
     private static RacingCars createRacingCars(List<String> carNameList) {
         return new RacingCars(carNameList.stream().map(RacingCar::new).toList());
     }
