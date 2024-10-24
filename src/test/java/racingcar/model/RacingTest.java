4e067797d49acb29cd7f04788a0c97e87d914589
DongYeop <s01054830455@gmail.com>
2024-10-25 01:53:38 +0900

    test: 경주 테스트
    - 자동차의 전진여부를 통해 경주가 정상적으로 진행되었는지 테스트한다.

--- src/test/java/racingcar/model/RacingTest.java
+++ src/test/java/racingcar/model/RacingTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.util.randomnumber.RandomNumberGenerator;
+import racingcar.util.randomnumber.StubRandomNumberGenerator;
 
 class RacingTest {
 
@@ -24,5 +25,28 @@ class RacingTest {
 
         //then
         assertThat(racing).isEqualTo(Racing.from(cars));
+    }
+
+    @DisplayName("경주를 진행한다.")
+    @Test
+    void runRacing() {
+        //given
+        Car pobiCar = Car.from("pobi", new StubRandomNumberGenerator(4));
+        Car woniCar = Car.from("woni", new StubRandomNumberGenerator(3));
+        Car junCar = Car.from("jun", new StubRandomNumberGenerator(1));
+
+        Cars cars = Cars.from(
+                List.of(pobiCar, woniCar, junCar)
+        );
+
+        Racing racing = Racing.from(cars);
+
+        //when
+        racing.runRacing();
+
+        //then
+        assertThat(pobiCar.getPosition()).isEqualTo(1);
+        assertThat(woniCar.getPosition()).isEqualTo(0);
+        assertThat(junCar.getPosition()).isEqualTo(0);
     }
 }
