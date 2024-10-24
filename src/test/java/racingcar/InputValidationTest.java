73e3859372e32c7e13bea529263b1f60cfb5a621
making-a-scene <4936joo@naver.com>
2024-10-24 15:16:44 +0900

    test(InputValidationTest): 자동차 이름 입력 값 검증을 위한 테스트 코드 작성
    입력받은 자동차 이름 형식이 올바른 경우 쉼표 기준으로 분할한 결과를 리턴
    자동차 이름이 한 대만 입력된 경우, 6자 이상의 자동차 이름이 존재하는 경우, 이름이 알파벳이나 숫자 외의 문자로 구성된 경우 예외 발생

---
+++ src/test/java/racingcar/InputValidationTest.java
@@ -0,0 +1,37 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+import racingcar.Validation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputValidationTest {
+    private final Validation validation = new Validation();
+
+    @Test
+    void 입력받은_자동차_이름_형식이_올바른_경우() {
+        assertThat(validation.validateCarName("pobi,woni,jun")).isEqualTo(new String[]{"pobi", "woni", "jun"});
+    }
+
+    @Test
+    void 자동차_이름이_한_대만_입력된_경우() {
+        assertThatThrownBy(
+                () -> validation.validateCarName("pobi")
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력에_여섯자_이상의_자동차_이름이_존재하는_경우() {
+        assertThatThrownBy(
+                () -> validation.validateCarName("makingascene")
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력이_알파벳이나_숫자_외의_문자를_포함하는_경우() {
+        assertThatThrownBy(
+                () -> validation.validateCarName("t_t")
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+}
