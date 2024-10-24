63db7442cc8b94d377a2e35d053ccc5fa469ee6b
dltmddus <tmddus4671@gmail.com>
2024-10-24 22:38:11 +0900

    refactor: 출력 메세지 상수화

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,12 @@
 package racingcar.view;
 
 public class OutputView {
-    public static void printCarNames(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ATTEMPT_COUNT = "시도할 횟수는 몇 회인가요?";
+    public static void printCarNamesInputMessage(){
+        System.out.println(INPUT_CAR_NAMES);
     }
-    public static void printAttemptCount(){
-        System.out.println("시도할 횟수는 몇 회인가요?");
+    public static void printAttemptCountInputMessage(){
+        System.out.println(INPUT_ATTEMPT_COUNT);
     }
 }
