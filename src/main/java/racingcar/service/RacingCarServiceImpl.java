de259e50f75769464fe9a51c0670dacba9112f76
onesay17 <onesay17@gmail.com>
2024-10-24 16:06:21 +0900

    feat: 서비스 구현체 추가
    - 자동차 객체 생성 로직 추가
    - 자동차 경주 게임 로직 추가
    - 경주가 완료된 결과 객체 받는 로직 추가

---
+++ src/main/java/racingcar/service/RacingCarServiceImpl.java
@@ -0,0 +1,42 @@
+package racingcar.service;
+
+import static racingcar.util.Constant.MAX_RANDOM_VALUE;
+import static racingcar.util.Constant.MIN_RANDOM_VALUE;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.validation.NameValidator;
+
+public class RacingCarServiceImpl implements RacingCarService {
+
+    private final NameValidator nameValidator;
+
+    public RacingCarServiceImpl(NameValidator nameValidator) {
+        this.nameValidator = nameValidator;
+    }
+
+    @Override
+    public List<Car> splitAndInitializeRacingCars(String carNames) {
+        List<String> racingCarNames = new ArrayList<>(List.of(carNames.split(",")));
+        nameValidator.validate(racingCarNames);
+
+        return getRacingCars(racingCarNames);
+    }
+
+    @Override
+    public void startRacingCarGame(List<Car> racingCars) {
+        for (Car racingCar : racingCars) {
+            racingCar.move(Randoms.pickNumberInRange(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE));
+        }
+    }
+
+    private List<Car> getRacingCars(List<String> racingCarNames) {
+        List<Car> racingCars = new ArrayList<>(racingCarNames.size());
+        for (String racingCarName : racingCarNames) {
+            racingCars.add(new Car(racingCarName));
+        }
+        return racingCars;
+    }
+}
