47a52a137a594df04965848a5b8762f861e76953
making-a-scene <4936joo@naver.com>
2024-10-24 15:43:24 +0900

    feat(UI.getRoundLimit()): 시도할 횟수를 입력받는 기능 추가

--- src/main/java/racingcar/UI.java
+++ src/main/java/racingcar/UI.java
@@ -5,10 +5,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class UI {
     private final Validation validation = new Validation();
     private final String GET_CAR_NAME_STATEMENT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-
+    private final String GET_ROUND_LIMIT_STATEMENT = "시도할 횟수는 몇 회인가요?";
     public String[] getCarNames() {
         System.out.println(GET_CAR_NAME_STATEMENT);
         String input = Console.readLine();
         return validation.validateCarName(input);
+    }
+
+    public int getRoundLimit() {
+        System.out.println(GET_ROUND_LIMIT_STATEMENT);
+        String input = Console.readLine();
+        return validation.validateRoundLimit(input);
     }
 }
