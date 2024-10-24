caaf43d83f52996d78134eddb7bc1294043c0caa
eusan <goodsan0330@gamil.com>
2024-10-24 15:16:20 +0900

    feat(input): implement input handling for car names

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String carNames = Console.readLine();
+
+        Car[] cars = separateNames(carNames);
+
+
+
+
+    }
+
+    public static Car[] separateNames(String carNames) {
+
+        String[] names = carNames.split(",");
+        Car[] cars = new Car[names.length];
+
+        /*
+         * 자동차 이름에 대해 자동차 객체 생성
+         */
+
+        return cars;
     }
 }
---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+public class Car {
+
+	private String name;
+	private int moveCounter;
+
+	public Car(String name) {
+		this.name = name;
+		this.moveCounter = 0;
+	}
+
+}
