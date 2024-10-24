f23a8718b7e5cde7996eb2ee224e568726203f77
garden14 <ci667@dgu.ac.kr>
2024-10-24 16:48:08 +0900

    feat(CarNameValidation): 자동차 이름 1개일 때 예외처리

--- src/main/java/racingcar/validation/CarNameValidation.java
+++ src/main/java/racingcar/validation/CarNameValidation.java
@@ -10,4 +10,9 @@ public class CarNameValidation {
             throw new IllegalArgumentException("중복된 이름이 존재하지 않아야 합니다.");
         }
     }
+    public void isNotOnlyOne(List<String> carNameList){
+        if(carNameList.size() == 1){
+            throw new IllegalArgumentException("자동차가 2대 이상이어야 경주할 수 있습니다.");
+        }
+    }
 }
