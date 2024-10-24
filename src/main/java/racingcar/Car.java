91bb93b745202bc7a0f463ad188fe162166ca579
eunho <eunho1032@gmail.com>
2024-10-24 13:19:04 +0900

    feat(Car): drive, stop 메서드 추가
    -drive: randomizeMove 메서드를 변경, 가져옴
    -sotp: 미션에 전진 또는 멈출 수 있다고 되어 있어 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
 	public String name;
@@ -9,4 +11,20 @@ public class Car {
 		this.name = name;
 		this.distance = 0;
 	}
+
+	public void drive() {
+		int randomNum;
+
+		randomNum = Randoms.pickNumberInRange(0, 9);
+		if (randomNum >= 4) {
+			distance++;
+			return;
+		}
+		this.stop();
+	}
+
+	public void stop() {
+		return;
+	}
+
 }
