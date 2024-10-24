4dd055190a1e58d4e6262bd625d0bf5b77d27f3d
binn <anyubin0724@naver.com>
2024-10-24 14:23:52 +0900

    feat: ParsingCarNameService 클래스 추가
    - 입력받은 문자열을 자동차 이름으로 분리하는 기능 구현

---
+++ src/main/java/racingcar/service/ParsingCarNameService.java
@@ -0,0 +1,18 @@
+package racingcar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.Car;
+
+public class ParsingCarNameService {
+    private static final String DELIMITER = ",\\s*";
+
+    public List<Car> parseCarNames(String inputCarName) {
+        String[] inputStrings = inputCarName.split(DELIMITER);
+        List<Car> cars = new ArrayList<>();
+        for (String inputString : inputStrings) {
+            cars.add(new Car(inputString));
+        }
+        return cars;
+    }
+}
