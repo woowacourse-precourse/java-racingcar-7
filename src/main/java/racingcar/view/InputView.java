fc1befbefbf99a36e180719939408edd6e00828a
장혜민 <gpals5987@gmail.com>
2024-10-24 23:23:49 +0900

    [fix] 접근제한자 수정
    - default -> public

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+import racingcar.constants.MessageConstants;
+
+public class InputView {
+
+    public void a(){
+        System.out.print(MessageConstants.INPUT_CAR_NAMES);
+    }
+
+}
