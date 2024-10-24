a7fd479efb51be9d297641c6877337fcebab492c
YeaChan05 <qkenrdl05@gmail.com>
2024-10-25 01:29:29 +0900

    fix(OutputInterface): 출력 메세지 추가
    실행 결과 및 최종 우승자 메세지 추가

--- src/main/java/racingcar/endpoint/OutputInterface.java
+++ src/main/java/racingcar/endpoint/OutputInterface.java
@@ -2,7 +2,9 @@ package racingcar.endpoint;
 
 public enum OutputInterface {
     ENTER_PLAYER_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    ENTER_NUMBER_OF_TRIALS("시도할 횟수는 몇 회인가요?");
+    ENTER_NUMBER_OF_TRIALS("시도할 횟수는 몇 회인가요?"),
+    EXECUTION_RESULT("실행 결과"),
+    FINAL_WINNER("최종 우승자 : ");
 
     private final String message;
 
@@ -15,11 +17,11 @@ public enum OutputInterface {
         return message;
     }
 
-    public static void printMessage(String systemMessage){
+    public static void printMessage(String systemMessage) {
         System.out.println(systemMessage);
     }
 
-    public static void printMessage(OutputInterface systemMessage){
+    public static void printMessage(OutputInterface systemMessage) {
         System.out.println(systemMessage);
     }
 }
