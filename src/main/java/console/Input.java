ac2a93f22218ba2a5041a06101e44ca1f7ca7ee4
김동진 <dongjin403@naver.com>
2024-10-24 21:41:07 +0900

    feat : 자동차 이름 입력시 5글자 초과되면 에러 발생 함수 추가

--- src/main/java/console/Input.java
+++ src/main/java/console/Input.java
@@ -5,10 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
-    public static final String INPUT_CAR_NAME =
-            "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public static final String INPUT_COUNT =
-            "시도할 횟수는 몇 회인가요?";
+    public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_COUNT = "시도할 횟수는 몇 회인가요?";
+    public static final String OUTPUT_WINNERS = "최종 우승자 : ";
+    public static final String NAME_LENGTH_ROLE = "이름은 5자 이하만 가능합니다.";
 
     public static String readCarName() {
         System.out.println(INPUT_CAR_NAME);
@@ -22,6 +22,12 @@ public class Input {
 
     public static void closeConsole() {
         Console.close();
+    }
+
+    public static void generateExceptionIfNameAbove5(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException(NAME_LENGTH_ROLE);
+        }
     }
 
 
