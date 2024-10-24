b457f7f6008deec4330d2a8bfa1c5b381dea3f94
jijongkwon <help3451@naver.com>
2024-10-25 01:39:57 +0900

    feat(service) : RacingCarService 생성
    TODO
    - 이름 저장하기
    - 시도횟수 입력받기
    - 시도횟수 만큼 랜덤 숫자 뽑기
    - 이상인 경우 전진하기

---
+++ src/main/java/racingcar/application/service/car/RacingCarService.java
@@ -0,0 +1,12 @@
+package racingcar.application.service.car;
+
+public class RacingCarService {
+
+    // TODO : 이름 저장하기
+
+    // TODO : 시도횟수 입력받기
+
+    // TODO : 시도횟수 만큼 랜덤 숫자 뽑기
+
+    // TODO : 4 이상인 경우 전진하기
+}
