9efe4b0cab4370490d1847c23c89b630a404564e
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-24 22:55:16 +0900

    test: RaceRoundRecordTest 테스트 코드 작성

---
+++ src/test/java/racingcar/domain/race/RaceRoundRecordTest.java
@@ -0,0 +1,33 @@
+package racingcar.domain.race;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.car.Car;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RaceRoundRecordTest {
+
+    @Test
+    @DisplayName("RaceRoundRecord 생성 테스트")
+    void raceRoundRecordCreationTest() {
+        //given
+        Car car1 = new Car("Car1",1);
+        Car car2 = new Car("Car2",2);
+
+        //when
+        RaceRoundRecord raceRoundRecord = RaceRoundRecord.from(List.of(car1,car2));
+        List<CarRecord> records = raceRoundRecord.getRaceRoundRecord();
+
+        //then
+        assertThat(records.size()).isEqualTo(2);
+        assertThat(records.get(0).getCarName()).isEqualTo("Car1");
+        assertThat(records.get(0).getDistance()).isEqualTo(1);
+        assertThat(records.get(1).getCarName()).isEqualTo("Car2");
+        assertThat(records.get(1).getDistance()).isEqualTo(2);
+    }
+
+}
