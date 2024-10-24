385acb07dc18a841cf41ea1697bb1e94818ca3c6
zeus <jj99526@naver.com>
2024-10-25 01:57:36 +0900

    feat : move에 대한 메소드 분리
    랜덤값이 4이상일시 true를 반환하고 미만일시 false를 반환하는 메소드를 추가함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,7 @@ package racingcar;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,9 +12,9 @@ import java.util.Scanner;
 //   [x] 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
 //   [x] 사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.
 //   [x] 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
-//   [x  자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
-//    우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.
-//    사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.
+//   [x]  자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
+//   [x] 우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.
+//   [x] 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.
 public class Application {
 
   public static Car[] getCarName(String inputCars) {
@@ -41,11 +42,16 @@ public class Application {
   public static void moveOrStop(Car[] cars){
     for (int j = 0; j < cars.length; j++) {
       int randomNum = (int) (Math.random() * 10);
-      if (randomNum >= 4) {
+      if (moveRule(randomNum)) {
         cars[j].setMoveCountPlus();
       }
     }
   }
+
+  public static boolean moveRule(int randomNum){
+    return randomNum>=4;
+  }
+
 
   public static void printByRound(Car[] cars) {
     for (int i = 0; i < cars.length; i++) {
@@ -76,14 +82,18 @@ public class Application {
   }
 
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    String inputCars = scanner.nextLine();
-    Car[] cars = getCarName(inputCars);
-    System.out.println("시도할 횟수는 몇 회인가요?");
-    int count = scanner.nextInt();
-    move(count, cars);
-    printWinner(cars);
+    try {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+      String inputCars = scanner.nextLine();
+      Car[] cars = getCarName(inputCars);
+      System.out.println("시도할 횟수는 몇 회인가요?");
+      int count = scanner.nextInt();
+      move(count, cars);
+      printWinner(cars);
+    }catch (InputMismatchException e){
+      throw new IllegalArgumentException("잘못된 값을 입력했습니다");
+    }
 
   }
 }
