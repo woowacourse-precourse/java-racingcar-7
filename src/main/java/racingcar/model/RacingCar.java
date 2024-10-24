b9e6aa39eabae4c0b617d57e4e488a05a40ef860
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-25 01:14:35 +0900

    refactor($RacingCar): RacingCarValidator 를 주입받아 검증하도록 변경
    - 검증은 수정 가능성이 높고, 자동차마다 다를 수 있음.
    - RacingCarValidator 를 외부에서 주입받음으로써, 유연성 증가.

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -1,31 +1,31 @@
 package racingcar.model;
 
 import racingcar.model.moving_strategy.MovingStrategy;
+import racingcar.model.validator.RacingCarValidator;
 
 import java.util.Objects;
 
 public class RacingCar {
 
     private final int DEFAULT_POSITION = 0;
-    private final int MAX_NAME_LENGTH = 5;
     private final int DEFAULT_MOVE_DISTANCE = 1;
 
     private final String name;
     private final MovingStrategy movingStrategy;
+    private final RacingCarValidator racingCarValidator;
     private int position;
 
-    public RacingCar(String name, MovingStrategy movingStrategy) {
-        validateName(name);
+    public RacingCar(String name, RacingCarValidator racingCarValidator, MovingStrategy movingStrategy) {
+        racingCarValidator.validate(name);
         this.name = name;
         this.position = DEFAULT_POSITION;
         this.movingStrategy = movingStrategy;
+        this.racingCarValidator = racingCarValidator;
     }
 
     public RacingCar(RacingCar other) {
-        validateName(other.name);
-        this.name = other.name;
+        this(other.name, other.racingCarValidator, other.movingStrategy);
         this.position = other.position;
-        this.movingStrategy = other.movingStrategy;
     }
 
     public void move() {
@@ -40,15 +40,6 @@ public class RacingCar {
 
     public int getPosition() {
         return position;
-    }
-
-    private void validateName(String name) {
-        if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
-        }
-        if (name.isBlank()) {
-            throw new IllegalArgumentException("이름은 공백일 수 없습니다.");
-        }
     }
 
     @Override
