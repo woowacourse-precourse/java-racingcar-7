cea3b85588a886890078db061eef56a910d1828c
Wooseok Jang <wowseok2774@gmail.com>
2024-10-24 14:47:44 +0900

    refactor: RacingCarService에 OutputHandler 의존성 주입

--- src/main/java/controller/RacingCarService.java
+++ src/main/java/controller/RacingCarService.java
@@ -6,12 +6,17 @@ import model.Car;
 import view.OutputHandler;
 
 public class RacingCarService {
+    private final OutputHandler outputHandler;
+
+    public RacingCarService(OutputHandler outputHandler) {
+        this.outputHandler = outputHandler;
+    }
 
     public void gameStart(List<Car> cars, int attempt) {
         for (int i = 0; i < attempt; i++) {
             for (int j = 0; j < cars.size(); j++) {
                 cars.get(j).move(Randoms.pickNumberInRange(0, 9));
-                OutputHandler.printCurrentStatus(cars.get(j));
+                outputHandler.printCurrentStatus(cars.get(j));
             }
             System.out.println();
         }
