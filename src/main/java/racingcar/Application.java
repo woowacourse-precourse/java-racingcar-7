a7fe2b5c7b3f7cec76b61e12d4ab53eabf30d0b3
choitaehoon <hugebigdream@gmail.com>
2024-10-24 18:48:08 +0900

    feat: add notification for results and single execution

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -9,6 +11,11 @@ public class Application {
         List<String> participants = divideMember(getInput());
 
         int raceTimes = Integer.parseInt(getInput());
+
+        List<Integer> countList = createParticipantCount(participants.size());
+
+        raceOneTime(countList);
+        printEachRaceResult(participants, countList);
     }
 
     public static String getInput() {
@@ -17,5 +24,34 @@ public class Application {
 
     public static List<String> divideMember(String input) {
         return List.of(input.split(","));
+    }
+
+
+    public static List<Integer> createParticipantCount(int participantSize) {
+        return new ArrayList<>() {{
+            for (int participant = 0; participant < participantSize; participant++) {
+                add(0);
+            }
+        }};
+    }
+
+    public static void raceOneTime(List<Integer> countList) {
+        for (int index = 0; index < countList.size(); index++) {
+            int random = Randoms.pickNumberInRange(0, 9);
+            if (random >= 4) {
+                countList.set(index, countList.get(index) + 1);
+            }
+        }
+    }
+
+    public static void printEachRaceResult(List<String> participantName, List<Integer> participantCount) {
+        for (int i = 0; i < participantName.size(); i++) {
+            System.out.print(participantName.get(i) + ":");
+            for (int i2 = 0; i2 < participantCount.get(i); i2++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+
     }
 }
--- src/test/java/racingcar/MethodTest.java
+++ src/test/java/racingcar/MethodTest.java
@@ -4,12 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MethodTest {
-
     @Test
     @DisplayName("입력이 정상적으로 들어오는지")
     void test_1() {
@@ -29,4 +31,51 @@ public class MethodTest {
 
         assertThat(result).hasSize(3).containsOnly("pobi", "woni", "jun");
     }
+
+    @Test
+    @DisplayName("게임 한번 플레이 진행")
+    void test_3() {
+        List<Integer> given = new ArrayList<>() {{
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+        }};
+
+        Application.raceOneTime(given);
+
+        System.out.println("given = " + given);
+        //TODO: 랜덤값으로 인하여 정확하게 테스트를 진행하기 어려우니 refactoring 하여, 테스트가 가능한 코드가 되도록 변경
+        assertThat(given).hasSize(5);
+    }
+
+    @Test
+    @DisplayName("개별 게임에 대한 결과 표시")
+    void test_4() {
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        PrintStream standardOut = System.out;
+        System.setOut(new PrintStream(outputStreamCaptor));
+
+        List<Integer> givenCount = new ArrayList<>() {{
+            add(1);
+            add(3);
+            add(2);
+            add(0);
+            add(4);
+        }};
+        List<String> givenName = new ArrayList<>() {{
+            add("nameA");
+            add("nameB");
+            add("nameC");
+            add("nameD");
+            add("nameE");
+        }};
+
+        Application.printEachRaceResult(givenName, givenCount);
+
+        assertEquals("nameA:-\nnameB:---\nnameC:--\nnameD:\nnameE:----", outputStreamCaptor.toString().trim());
+        System.setOut(standardOut);
+    }
+
 }
