bb0359d2b5204a70ada124158eb728888c126c68
dawit0905 <dawit0905@gmail.com>
2024-10-24 11:09:41 +0900

    feat: controller 에서 business 로직 호출

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -4,24 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import racingcar.model.Car;
 import racingcar.service.RacingGameService;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
 
 public class RacingGameController {
     private final RacingGameService racingGameService = new RacingGameService();
-    private final InputView inputView = new InputView();
-    private final OutputView outputView = new OutputView();
     private List<Car> cars = new ArrayList<>();
 
     public void start() {
         cars = racingGameService.inputCars();
-//        int moveCount = inputView.getMoveCount();
-//        racingGameService.race(moveCount);
-//        outputView.printCurrentStatus(racingGameService.getCars());
-//        racingGameService.announceWinners();
-    }
-
-    private void announceWinners() {
+        int moveCount = racingGameService.inputMoveCount();
+        racingGameService.race(cars, moveCount);
+        racingGameService.announceWinners(cars);
     }
 
 }
