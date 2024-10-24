7e0055d62e65735321e648c70d9cdc987b262a0e
yuntasha <jaljayo85@gmail.com>
2024-10-25 00:36:39 +0900

    feat(Car.java): add Car class
    Add function list
    - 앞으로 가기
    - 이름 반환
    - 점수 반환

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,33 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car implements Player{
+
+    static private final int DISTANCE = 1;
+    private final String name;
+    private int score;
+
+    Car(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    @Override
+    public void run() {
+        int n = Randoms.pickNumberInRange(0, 9);
+        if (n>=4) {
+            this.score+=DISTANCE;
+        }
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getScore() {
+        return score;
+    }
+}
