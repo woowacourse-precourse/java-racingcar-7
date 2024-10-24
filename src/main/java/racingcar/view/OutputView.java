ec20aa41cf82ecd8061b7cead3ed37143a2b725a
dawit0905 <dawit0905@gmail.com>
2024-10-24 10:27:58 +0900

    feat: outputview 작성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,18 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.model.Car;
+
 public class OutputView {
+
+    public void printCurrentStatus(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getDistance()));
+        }
+        System.out.println();
+    }
+
+    public void announceWinners(List<String> winners) {
+        System.out.println("우승자: " + String.join(", ", winners));
+    }
 }
