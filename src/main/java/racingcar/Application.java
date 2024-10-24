d58cc2b438c5f5c05c61a180730af24f516d6c74
soeun2537 <soeun2537@gmail.com>
2024-10-24 11:04:34 +0900

    feat: 자동차 이름 쉼표로 구분하는 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,23 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carsNameInput = Console.readLine();
+
+        String[] carsNameArray = parseCarsName(carsNameInput);
+        List<String> carsNameList = arrayToList(carsNameArray);
+    }
+
+    private static String[] parseCarsName(String carInput) {
+        return carInput.split(",");
+    }
+
+    private static List<String> arrayToList(String[] array) {
+        return Arrays.asList(array);
     }
 }
