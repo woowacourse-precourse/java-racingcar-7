e361e8ede817e63045913be7daa2e947bafe6222
Park Yu Han <pbk2312@naver.com>
2024-10-24 14:52:32 +0900

    refactor(view): static 메서드에서 인스턴스 메서드로 변경
    - InputView의 메서드를 인스턴스 메서드로 변경하여 유연성을 상승
    - OutputView의 메서드를 인스턴스 메서드로 변경하여 테스트 가능성을 향상

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,15 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static String inputCarNames() {
+    public String inputCarNames() {
         System.out.println(ViewMessage.INPUT_CAR_NAMES.getMessage());
         return Console.readLine();
     }
 
-    public static String inputTryCount() {
+    public String inputTryCount() {
         System.out.println(ViewMessage.INPUT_TRY_COUNT.getMessage());
         return Console.readLine();
     }
-
 
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,25 +6,27 @@ import racingcar.model.RacingCar;
 
 public class OutputView {
 
-    // 각 라운드 결과 출력
-    public static void printRaceResults(List<RacingCar> racingCars) {
+    public void printResultHeader() {
+        System.out.println(ViewMessage.RESULT_HEADER.getMessage());
+    }
+
+    public void printRaceResults(List<RacingCar> racingCars) {
         for (RacingCar car : racingCars) {
             printCarPosition(car);
         }
         System.out.println();
     }
 
-    public static void printWinners(List<RacingCar> winners) {
+    public void printWinners(List<RacingCar> winners) {
         String winnerNames = winners.stream()
                 .map(RacingCar::getName)
                 .collect(Collectors.joining(", "));
-        System.out.println("최종 우승자 : " + winnerNames);
+        System.out.println(ViewMessage.FINAL_WINNER.getMessage() + winnerNames);
     }
 
-    private static void printCarPosition(RacingCar car) {
+    private void printCarPosition(RacingCar car) {
         String positionDisplay = "-".repeat(car.getPosition());
         System.out.println(car.getName() + " : " + positionDisplay);
     }
-
 
 }
