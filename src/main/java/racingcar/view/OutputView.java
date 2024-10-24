5965273f0020dfe3e30276feb446cdc46bcfcdd5
Chan531 <chan657312@gmail.com>
2024-10-24 21:01:37 +0900

    feat(OutputView): 실행 결과 메세지 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -9,4 +9,8 @@ public class OutputView {
 	public static void printTrialCountInputMessage() {
 		System.out.println("시도할 횟수는 몇 회인가요?");
 	}
+
+	public static void printExecutionResultMessage() {
+		System.out.println("실행 결과");
+	}
 }
