986e62fb5df312a9d86b48962c9e395b58e614c1
dbdb456 <adad568@naver.com>
2024-10-24 21:01:17 +0900

    feat(validator): 자동차 이름 길이 검증 메서드 추가

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -7,4 +7,13 @@ public class Validator {
             throw new IllegalArgumentException("입력받은 자동차 이름이 없습니다.");
         }
     }
+
+
+    public void validateCarNameLength(String[] cars) {
+        for (String car : cars) {
+            if (car.length() >= 6) {
+                throw new IllegalArgumentException("입력받은 자동차 이름의 크기가 6이상 입니다."); //
+            }
+        }
+    }
 }
