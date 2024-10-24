97998b3e4f43b298c2aa6cd4956a17a58146cce7
yeong-hwan <jghff700@naver.com>
2024-10-25 07:25:39 +0900

    feat(service): create car by car names list

---
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -0,0 +1,16 @@
+package racingcar.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.entity.Car;
+import racingcar.enums.RacingGameCondition;
+import racingcar.view.OutputView;
+
+public class RacingGameService {
+    public List<Car> createCars(List<String> carNames) {
+        return carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+}
