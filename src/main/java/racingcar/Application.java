1416e02e7a4be59727a37039255e1c0779b88684
OhYeHyun <ohyhohyl@naver.com>
2024-10-25 01:20:07 +0900

    feat(Application): 레이싱 게임 실행하는 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String carInput = readLine();
+        int moveCountInput = Integer.parseInt(readLine());
+        RacingService racingService = new RacingService(carInput, moveCountInput);
+
+        racingService.startRaceGame();
+        String[] winner = racingService.getWinner();
+
+//        System.out.println(Arrays.toString(winner));
     }
 }
