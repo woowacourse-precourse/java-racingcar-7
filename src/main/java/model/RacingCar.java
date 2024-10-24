4a1fe76bf6e7dee9e0c6a75716536520c7c56b98
gikhoon <ghikhoon@naver.com>
2024-10-25 00:46:11 +0900

    fix(racingCar): 이름 필드를 String으로 변경

--- src/main/java/model/RacingCar.java
+++ src/main/java/model/RacingCar.java
@@ -1,10 +1,10 @@
 package model;
 
 public class RacingCar {
-    private int name;
+    private String name;
     private long distance;
 
-    public RacingCar(int name) {
+    public RacingCar(String name) {
         this.name = name;
         this.distance = 0;
     }
