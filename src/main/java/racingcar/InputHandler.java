8d37165e685876efd04c3f1e1f17df1b57764f23
johyewon <johyewon4601@gmail.com>
2024-10-24 21:49:42 +0900

    feat(InputHandler): getCarNames와 getMoveCnt, validCarNames, validMoveCnt 메소드 기능 추가

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -1,18 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputHandler {
     public String[] getCarNames() {
-
-        return null;
+        return Console.readLine().split(",");
     }
     public int getMoveCnt() {
-
-        return 0;
+        return Integer.parseInt(Console.readLine());
     }
-    public void validCarNames(String[] names) {
-
+    public void validCarNames(String[] carNames) {
+        for (String name : carNames) {
+            if (name.length() > 5) throw new IllegalArgumentException();
+        }
     }
     public void validMoveCnt(int moveCount) {
-
+        if (moveCount < 0) throw new IllegalArgumentException();
     }
 }
