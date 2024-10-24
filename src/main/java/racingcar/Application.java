d1603f86785e83e017660f772e23e8253c614bc5
gitubanana <ptyolo666@gmail.com>
2024-10-24 20:22:13 +0900

    feat(CarRacing): 자동차 경주 시작 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,7 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 다른 파일 디렉토리로 분리할지 고려
+        CarRacing.start();
     }
 }
---
+++ src/main/java/racingcar/CarRacing.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+public class CarRacing {
+    private static final String RACING_START_MESSAGE = "\n실행 결과\n";
+    private static final String PROGRESS_SEPARATOR = "\n";
+
+    private CarRacing() {
+    }
+
+    static void start() {
+        String names = RaceInput.getNames();
+        CarNameParser parser = new CarNameParser(names);
+        Cars cars = new Cars(parser);
+        int tryCount = RaceInput.getTryCount();
+
+        RaceOutput.print(RACING_START_MESSAGE);
+        while (tryCount-- > 0) {
+            cars.move();
+            RaceOutput.printProgress(cars);
+            RaceOutput.print(PROGRESS_SEPARATOR);
+        }
+
+        RaceOutput.printWinner(cars);
+    }
+}
