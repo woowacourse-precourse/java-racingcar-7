e9db0067f9494326e4f98cd4b097f04f0fba09c3
Youngchan <wdc4512@naver.com>
2024-10-24 18:44:16 +0900

    feat: Car 클래스 생성 / 기본 속성 추가
    - Car 클래스 생성
    - name과 score 속성 추가
    - Car 생성자를 통한 차 이름 초기화
    - getScore(), getName() 선언

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    public String name;
+    public int score;
+
+    // Car 객체 생성
+    public Car() {
+        this.name = name;
+        this.score = 0;
+    }
+
+    // 점수 반환
+    public int getScore() {
+        return score;
+    }
+
+    // 차 이름 반환
+    public String getName() {
+        return name;
+    }
+}
