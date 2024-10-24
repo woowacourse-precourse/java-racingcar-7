112d598f2294f5233b9fe117768b19d0980c0647
woong2e <tlswodnd12345@naver.com>
2024-10-25 02:11:14 +0900

    feat(validation): add exception handling for invalid car name input

--- src/main/java/racingcar/io/Validators.java
+++ src/main/java/racingcar/io/Validators.java
@@ -7,16 +7,37 @@ public class Validators implements Validator {
     public String validateString(String input) throws IllegalArgumentException {
         String[] words = splitToArray(input);
 
+        for (String word : words) {
+            if (!isLengthInRange(word)) {
+                throw new IllegalArgumentException("잘못된 자동차 이름 입력");
+            }
+        }
+
+
+
+
         return "";
     }
 
     private String[] splitToArray(String input) {
+        if (input.indexOf(",") == 0 || input.lastIndexOf(",") == input.length() - 1) {
+            throw new IllegalArgumentException("잘못된 자동차 이름 입력");
+        }
+
         String[] words = input.split(",");
         for (int i = 0; i < words.length; i++) {
             words[i] = words[i].strip();
         }
 
         return words;
+    }
+
+    private boolean isLengthInRange(String s) {
+        if (s.isEmpty() || s.length() > 5) {
+            return false;
+        }
+
+        return true;
     }
 
     @Override
