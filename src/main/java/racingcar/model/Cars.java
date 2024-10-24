c4302c2cc932dc0d10b898c88807e072263e6dac
FhRh <yiyop@naver.com>
2024-10-24 23:02:37 +0900

    refactor(CarCreateService) : 람다 표현식 사용

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -7,20 +7,18 @@ public class Cars {
 
     private final List<Car> cars;
 
-    public Cars(List<Car> cars) {
+    public Cars(List<Car> carList) {
         this.cars = new ArrayList<>();
-        cars.forEach(car -> {
-            this.add(car);
-        });
+        carList.forEach(this::add);
     }
 
-    private void add(Car car){
+    private void add(Car car) {
         validateDuplicate(car);
         this.cars.add(car);
     }
 
-    private void validateDuplicate(Car car){
-        if(this.cars.contains(car)){
+    private void validateDuplicate(Car car) {
+        if (this.cars.contains(car)) {
             throw new IllegalArgumentException("중복된 자동차가 존재합니다.");
         }
     }
