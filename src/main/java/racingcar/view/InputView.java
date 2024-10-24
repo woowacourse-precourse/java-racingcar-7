435c0fccff4301b6678c4b579e76495b87d8a955
dltmddus <tmddus4671@gmail.com>
2024-10-24 22:31:34 +0900

    feat: 자동차 이름, 시도 횟수 입력 메시지 출력 함수 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,18 +1,10 @@
 package racingcar.view;
 
-
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static String getUserInput(){
         return Console.readLine();
     }
-    public static String getCarNames(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return getUserInput();
-    }
-    public static String getAttemptCount(){
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        return getUserInput();
-    }
+
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,10 @@
-package racingcar.view;public class OutputView {
+package racingcar.view;
+
+public class OutputView {
+    public static void printCarNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+    public static void printAttemptCount(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
 }
