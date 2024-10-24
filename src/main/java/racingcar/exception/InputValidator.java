7bd27cf2cc028b5bb66278bd58fcebdf8747436e
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-25 05:58:22 +0900

    feat: 이름 길이 검증 기능 구현
    입력된 이름의 길이가 5를 넘어가는지 검증하는 메서드를 구현

--- src/main/java/racingcar/exception/InputValidator.java
+++ src/main/java/racingcar/exception/InputValidator.java
@@ -12,4 +12,10 @@ public class InputValidator {
             throw new IllegalArgumentException("에러: 아무것도 입력하지 않으셨습니다!");
         }
     }
+
+    public void validateInvalidCarName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("에러: 입력된 이름의 길이가 5를 넘어갑니다. 길이 5이하의 이름을 입력해주세요!");
+        }
+    }
 }
