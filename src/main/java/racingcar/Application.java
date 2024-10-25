fc3cb3af304789b487fa86cc5b8c4bec2b0b74b8
chaeyeon0130 <k84292330@gmail.com>
2024-10-25 18:17:13 +0900

    feat(Application): 프로그램 흐름 제어 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,70 +1,16 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
-        String[] tokens = input.split(",");
-        List<String> cars = new ArrayList<>();
-        for (String token : tokens) {
-            String car = token.trim();
-            if (car.isEmpty()) {
-                throw new IllegalArgumentException("자동차 이름은 비어 있지 않아야 합니다.");
-            }
-            if (car.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
-            }
-            cars.add(car);
-        }
+        List<Car> cars = InputHandler.inputCars();
+        int turns = InputHandler.inputTurns();
 
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int turns;
-        try {
-            turns = Integer.parseInt(Console.readLine());
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("시도 횟수는 정수형이어야 합니다.");
-        }
-        if (turns < 0) {
-            throw new IllegalArgumentException("시도 횟수는 0회 이상만 가능합니다.");
-        }
+        Game game = new Game(cars, turns);
+        game.play();
 
-        List<Integer> scores = new ArrayList<>();
-        for (int i = 0; i < cars.size(); i++) {
-            scores.add(0);
-        }
-        System.out.println();
-        System.out.println("실행 결과");
-        while (turns-- > 0) {
-            for (int i = 0; i < cars.size(); i++) {
-                int num = Randoms.pickNumberInRange(0, 9);
-                if (num > 3) {
-                    scores.set(i, scores.get(i) + num);
-                }
-                System.out.printf("%s : %s%n", cars.get(i), "-".repeat(scores.get(i)));
-            }
-            System.out.println();
-        }
-
-        int max = -1;
-        for (int i = 0; i < scores.size(); i++) {
-            if (scores.get(i) > max) {
-                max = scores.get(i);
-            }
-        }
-
-        List<String> winners = new ArrayList<>();
-        for (int i = 0; i < cars.size(); i++) {
-            if (scores.get(i) == max) {
-                winners.add(cars.get(i));
-            }
-        }
-
-        System.out.print("최종 우승자 : ");
-        System.out.println(String.join(", ", winners));
+        List<String> winners = game.getWinners();
+        OutputHandler.displayWinners(winners);
     }
 }
