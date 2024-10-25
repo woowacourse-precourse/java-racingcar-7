d2e03a207239341377b1ad6015de29950ddb94be
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 19:38:58 +0900

    feat: 자동차 리스트를 입력하는 기능을 추가한다

--- src/main/java/racingcar/view/inputView.java
+++ src/main/java/racingcar/view/inputView.java
@@ -1,5 +1,13 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class inputView {
 
+    private static final String INPUT_CAR_LIST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public static String inputCar() {
+        System.out.println(INPUT_CAR_LIST_MESSAGE);
+        return Console.readLine();
+    }
 }
