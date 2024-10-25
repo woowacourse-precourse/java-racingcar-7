50060bf749d8c556f312e7ebd48468f5301bba3b
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-25 17:59:56 +0900

    test(ForwardTest): 전진을 시도하는 횟수를 조건에 맞게 입력하면 통과하는 테스트 코드 작성

--- src/test/java/racingcar/domain/ForwardTest.java
+++ src/test/java/racingcar/domain/ForwardTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ForwardTest {
@@ -24,6 +25,17 @@ public class ForwardTest {
         assertThatThrownBy(() -> Forward.from(candidate))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("전진을 시도할 횟수는 0~9 사이의 숫자만 입력 가능합나다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings= {"0", "1", "5", "9"})
+    @DisplayName("전진을 시도하는 횟수에 0~9 사이의 숫자가 입력되면 테스트는 통과한다.")
+    void validateSingleDigitForwardCount(String candidate) {
+        Forward saveForwardCount = Forward.from(candidate);
+        int parseInt = Integer.parseInt(candidate);
+
+        assertThat(saveForwardCount.getForwardCount())
+                .isEqualTo(parseInt);
     }
 
 }
