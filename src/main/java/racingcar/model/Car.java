72e8e9dd7da0c8922a8f602b8cd25d8d562b32cf
jklee3409 <jklee3409@khu.ac.kr>
2024-10-25 19:51:40 +0900

    feat(car) : car 생성자 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class Car {
+    private final String name;
+    private final int distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+}
