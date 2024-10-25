fa6be67e757e625d265b222203214a1cfc972b4c
J0YERIM <chosarah70@gmail.com>
2024-10-26 04:56:41 +0900

    feat: 이동 전략 인터페이스 및 랜덤 이동 전략 구현
    다양한 이동 전략을 구현할 수 있도록 MoveStrategy 인터페이스 정의
    랜덤 값이 4 이상일 때 이동하는 RandomMoveStrategy 구현

---
+++ src/main/java/racingcar/domain/RandomMoveStrategy.java
@@ -0,0 +1,15 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomMoveStrategy implements MoveStrategy {
+
+    private static final int MOVE_SUCCESS_THRESHOLD = 4;
+    private static final int RANDOM_LOWER_BOUND = 0;
+    private static final int RANDOM_UPPER_BOUND = 9;
+
+    @Override
+    public boolean canMove() {
+        return Randoms.pickNumberInRange(RANDOM_LOWER_BOUND, RANDOM_UPPER_BOUND) >= MOVE_SUCCESS_THRESHOLD;
+    }
+}
