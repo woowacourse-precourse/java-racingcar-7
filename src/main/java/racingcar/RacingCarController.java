aa3a33289f911e52ab5b8493aded046ae4d53d34
JEANS <songkc5671@naver.com>
2024-10-25 16:17:03 +0900

    feat(controller) : Model, View 중계 하는 기능 추가
    Model, View 인스턴스화를 해서 run() 함수로 실행합니다.

--- src/main/java/racingcar/RacingCarController.java
+++ src/main/java/racingcar/RacingCarController.java
@@ -1,4 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.raceDto.RacingRequest;
+import racingcar.raceDto.RacingResponse;
+
 public class RacingCarController {
+
+    RacingCarView racingCarView = new RacingCarView();
+    RacingCarModel racingCarModel = new RacingCarModel();
+
+    RacingCarController(RacingCarView racingCarView, RacingCarModel racingCarModel) {
+        this.racingCarView = racingCarView;
+        this.racingCarModel = racingCarModel;
+    }
+
+    public void run() {
+        RacingRequest racingRequest = racingCarView.readInput();
+        RacingResponse racingResponse = racingCarModel.startRace(racingRequest);
+        racingCarView.printResult();
+    }
 }
