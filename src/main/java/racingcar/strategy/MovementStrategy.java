445c4b5e31f3f6490bce0ffcafbb9eb510ef4bfa
world-dv <goodforx0147@naver.com>
2024-10-26 03:10:50 +0900

    refactor: 클래스 이름 재정의
    - 무작위 값 생성 의도에 맞게 클래스 이름을 계산기가 아닌 생성기로 재정의합니다.

--- src/main/java/racingcar/strategy/MovementStrategy.java
+++ src/main/java/racingcar/strategy/MovementStrategy.java
@@ -1,12 +1,12 @@
 package racingcar.strategy;
 
-import racingcar.util.RandomNumberCalculator;
+import racingcar.util.RandomNumberCreator;
 
 public class MovementStrategy implements CarStrategy {
     private static final Integer MOVEMENT_CRITERION_NUMBER = 4;
 
     @Override
     public boolean move() {
-        return RandomNumberCalculator.getRandomNumber() >= MOVEMENT_CRITERION_NUMBER;
+        return RandomNumberCreator.getRandomNumber() >= MOVEMENT_CRITERION_NUMBER;
     }
 }
--- src/main/java/racingcar/util/RandomNumberCreator.java
+++ src/main/java/racingcar/util/RandomNumberCreator.java
@@ -2,7 +2,7 @@ package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomNumberCalculator {
+public class RandomNumberCreator {
     private static final Integer START_RANDOM_NUMBER = 0;
     private static final Integer END_RANDOM_NUMBER = 9;
 
