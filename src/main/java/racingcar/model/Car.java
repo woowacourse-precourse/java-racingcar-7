638affdaf32360b408ecd12eb2db47541fabce34
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-10-26 01:11:43 +0900

    feat: 자동차 움직임 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,6 +2,12 @@ package racingcar.model;
 
 public class Car {
     private String name;
+    private int position=0;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
 
     public Car(String name) {
         if(name==null) throw new IllegalArgumentException("이름이 null이 될 수 없습니다.");
@@ -16,5 +22,12 @@ public class Car {
 
     public String getName() {
         return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+    public void move(int i){
+        if(i>3) position++;
     }
 }
--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -35,4 +35,16 @@ class CarTest {
         Car car=new Car("pobi");
         assertThat(car.getName()).isEqualTo("pobi");
     }
+    @Test
+    void 정지_3이하(){
+        Car car=new Car("pobi");
+        car.move(3);
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+    @Test
+    void 이동_4이상(){
+        Car car=new Car("pobi");
+        car.move(4);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
 }
