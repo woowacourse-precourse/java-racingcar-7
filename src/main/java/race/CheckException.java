a24948e4808115461e9888460e55de0af778fc8a
ziyoungkwon <gy011003@naver.com>
2024-10-25 18:18:33 +0900

    refactor: checkNameOfCar 예외처리 오타 수정

--- src/main/java/race/CheckException.java
+++ src/main/java/race/CheckException.java
@@ -15,7 +15,7 @@ public class CheckException {
             throw new IllegalArgumentException("[잘못된 입력] 쉼표와 쉼표 사이에 자동자의 이름이 입력되지 않았습니다.");
         }
         if (name.length() > 5) {
-            throw new IllegalArgumentException("[잘못된 입력] 자동자 이름의 길이가 5자 이상입니다.");
+            throw new IllegalArgumentException("[잘못된 입력] 자동자 이름의 길이가 5자 초과입니다.");
         }
     }
 
