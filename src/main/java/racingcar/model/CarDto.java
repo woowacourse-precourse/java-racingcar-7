b40234f5ebbbf7d4e86b593b236528eb0336bf51
yooo <ghddbtl@gmail.com>
2024-10-25 15:57:20 +0900

    feat: create CarDto

---
+++ src/main/java/racingcar/model/CarDto.java
@@ -0,0 +1,35 @@
+package racingcar.model;
+
+public class CarDto {
+    private String name;
+    private int id = 0;
+    private int position = 0;
+
+    public CarDto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+}
