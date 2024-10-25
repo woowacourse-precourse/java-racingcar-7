34ac59ba6a239bed8ef29038fa4cdafd75c49c88
AYEOOON <k93853471@gmail.com>
2024-10-25 17:04:15 +0900

    refactor: 매직 상수 추가 및 적용
    - 상수 관리 클래스 Constant 추가
    - InputUtils 클래스 내 상수 사용하도록 수정

---
+++ src/test/java/racingcar/Constant.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class Constant {
+    public static final String NAME_SEPARATOR = ",";
+    public static final String NAME_INPUT_ERROR_MESSAGE = "자동차 이름은 5자 이하만 가능합니다.";
+    public static final String ATTEMPT_INPUT_ERROR_MESSAGE = "시도 횟수는 정수만 입력 가능합니다.";
+    public static final int CAR_NAME_LENGTH_LIMIT = 5;
+
+}
--- src/test/java/racingcar/InputUtils.java
+++ src/test/java/racingcar/InputUtils.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import static racingcar.Constant.*;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,7 +9,7 @@ import java.util.List;
 public class InputUtils {
     public static List<Car> inputCarName() {
         List<Car> carList = new ArrayList<>();
-        String[] carNameList = Console.readLine().split(",");
+        String[] carNameList = Console.readLine().split(NAME_SEPARATOR);
 
         for (String carName : carNameList) {
             checkNameLength(carName);
@@ -17,8 +19,8 @@ public class InputUtils {
     }
 
     private static void checkNameLength(String carName) {
-        if (carName.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        if (carName.length() > CAR_NAME_LENGTH_LIMIT) {
+            throw new IllegalArgumentException(NAME_INPUT_ERROR_MESSAGE);
         }
     }
 
@@ -31,7 +33,7 @@ public class InputUtils {
     private static void checkAttemptOK(String attempt){
         for(int i = 0; i < attempt.length(); i++){
             if(!Character.isDigit(attempt.charAt(i))){
-                throw new IllegalArgumentException("시도 횟수는 정수만 입력 가능합니다.");
+                throw new IllegalArgumentException(ATTEMPT_INPUT_ERROR_MESSAGE);
             }
         }
     }
