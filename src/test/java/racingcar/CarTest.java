17e33f1b29013656b5155b0dba60d37c8e06d7b7
kjyyjk <rlawnsdud920@gmail.com>
2024-10-25 21:17:10 +0900

    test: 자동차의 이름이 알파벳 또는 숫자가 아닌 경우 예외를 발생한다
    - 단위 테스트의 이름은 should_{}_When_{}을 따른다.
    가장 이해하기 쉽고 많이 쓰이는 네이밍 컨벤션이다.
    기존에 사용하던 한글 테스트 이름은 DisplayName 애너테이션으로 사용한다.

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,13 +1,29 @@
 package racingcar;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
 
     @Test
-    public void 이름이_6글자_이상이면_예외를_발생한다() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Car("테스트차량임"));
+    @DisplayName("이름이_5글자를_넘기면_예외를_발생한다")
+    public void should_ThrowException_When_NameLengthMoreThan5() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> new Car("testcar"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("이름에_영문자와_숫자_이외의_문자를_입력하면_예외를_발생한다")
+    public void should_ThrowException_When_NameNotOnlyEnglishAndNumber() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> new Car("ab-c"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
