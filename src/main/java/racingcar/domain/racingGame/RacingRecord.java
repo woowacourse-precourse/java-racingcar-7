be9bb4ee5a8f6f717b76585f8ee0deb23c8a4761
tentenball <lth8905@gmail.com>
2024-10-25 22:29:06 +0900

    chore: RacingRecord, RacingRecords 패키지 위치 domain으로 변경

---
+++ src/main/java/racingcar/domain/racingGame/RacingRecord.java
@@ -0,0 +1,12 @@
+package racingcar.domain.racingGame;
+
+
+public class RacingRecord {
+	private final String carName;
+	private final int score;
+
+	public RacingRecord(String carName, int score) {
+		this.carName = carName;
+		this.score = score;
+	}
+}
--- src/main/java/racingcar/domain/racingGame/RacingRecords.java
+++ src/main/java/racingcar/domain/racingGame/RacingRecords.java
@@ -1,4 +1,4 @@
-package racingcar.service;
+package racingcar.domain.racingGame;
 
 import java.util.List;
 
