6846ea173e4b138669ffe314dd6720395c0d3d2b
yooo <ghddbtl@gmail.com>
2024-10-25 21:15:42 +0900

    feat: add request method of InputView
    값이 잘못입력 됐을 때, 사용자에게 다시 입력을 요청하는 메서드 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,22 +3,24 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    public static final String CAR_NAME = "경주할 자동차 이름을 입력하세요.(쉼표(,) 기준으로 구분, 각 5자 이하)";
+    public static final String ROUNDS = "시도할 횟수는 몇 회인가요?";
 
     public static String carNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(CAR_NAME);
         return Console.readLine();
     }
 
-    public static String gameRounds(){
-        System.out.println("시도할 횟수는 몇 회인가요?");
+    public static String gameRounds() {
+        System.out.println(ROUNDS);
         return Console.readLine();
     }
 
-    public static String request(String value, String strings) {
-        String request = "";
+    public static String request(String empty, String outputStrings) {
+        String request = empty;
 
-        while (value.isEmpty()) {
-            System.out.println(strings + " 입력되지 않았습니다.");
+        while (request.isEmpty()) {
+            System.out.println(outputStrings);
             request = Console.readLine();
         }
         return request;
