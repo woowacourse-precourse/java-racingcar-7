d17ba0b26567e3a8f3128513907d08498b0f4dc9
Jiihyun <cwlgusc@gmail.com>
2024-10-25 19:47:29 +0900

    feat: 자동차 이름 길이 검증 - 1~5자만 가능, 그 외 범위는 IllegalArgumentException 반환

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -2,13 +2,21 @@ package racingcar.domain;
 
 public class CarName {
 
+    public static final int MAXIMUM = 5;
     private final String name;
 
     public CarName(String name) {
+        validateLength(name);
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    private void validateLength(String name) {
+        if (name.isBlank() || name.length() > MAXIMUM) {
+            throw new IllegalArgumentException("이름은 1~5자만 가능합니다.");
+        }
     }
 }
