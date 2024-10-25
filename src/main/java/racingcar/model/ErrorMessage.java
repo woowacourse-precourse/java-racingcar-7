c022324063708f4b3a54e4cba70d51e2f7c8782e
Leejunho <junho767@naver.com>
2024-10-26 03:52:37 +0900

    feat: 입력 검증을 위한 에러 메시지 추가

---
+++ src/main/java/racingcar/model/ErrorMessage.java
@@ -0,0 +1,9 @@
+package racingcar.model;
+
+public class ErrorMessage {
+    public static final String INVALID_CAR_NAME_MESSAGE = "차량 이름은 5자 이하여야 합니다.";
+    public static final String STRING_IS_EMPTY_MESSAGE = "문자열이 공백입니다.";
+    public static final String INVALID_DELIMITER_MESSAGE = "문자열 구분자가 쉼표(,)가 아닙니다.";
+    public static final String NON_ALPHABETIC_CHARACTER_MESSAGE = "문자열에 문자가 아닌 값이 포함되어 있습니다.";
+    public static final String INVALID_TRY_NUMBER_MESSAGE = "이동 횟수는 1 이상의 숫자여야 합니다.";
+}
