f02ceddd2bd2d950934d9183dc96664dec7b37a4
MinseokGo <rhalstjr1999@naver.com>
2024-10-25 18:30:01 +0900

    refactor: 자동차 생성 로직 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Application {
 
@@ -27,14 +24,16 @@ public class Application {
             throw new IllegalArgumentException("경주에 참가할 자동차를 최소 2개 이상 입력해주세요.");
         }
 
-        final Set<String> registeredCarNames = new HashSet<>();
-        final List<Car> cars = new ArrayList<>();
-        for (String carName : carNames) {
-            if (!registeredCarNames.add(carName)) {
-                throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
-            }
-            cars.add(new Car(carName));
-        }
+        CarManager carManager = new CarManager(carNames);
+
+//        final Set<String> registeredCarNames = new HashSet<>();
+        final List<Car> cars = carManager.getCars();
+//        for (String carName : carNames) {
+//            if (!registeredCarNames.add(carName)) {
+//                throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+//            }
+//            cars.add(new Car(carName));
+//        }
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         final String playTimesInput = Console.readLine();
---
+++ src/main/java/racingcar/CarManager.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CarManager {
+
+    private final List<Car> cars;
+
+    // 자동차 이름을 받아서 생성하는 로직으로 수정 필요
+    public CarManager(final String[] carNames) {
+        this.cars = initializeCars(carNames);
+    }
+
+    private List<Car> initializeCars(final String[] carNames) {
+        return Arrays.stream(carNames)
+                .map(Car::new)
+                .toList();
+    }
+
+    public List<Car> getCars() {
+        return Collections.unmodifiableList(cars);
+    }
+}
