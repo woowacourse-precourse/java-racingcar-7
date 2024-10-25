2e543cee34a31c80e7d3bceee0102b9f9adc533c
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 16:13:17 +0900

    feat: 시도횟수 입력하는 기능을 추가한다

--- src/main/java/racingcar/util/InputTryCount.java
+++ src/main/java/racingcar/util/InputTryCount.java
@@ -1,8 +1,20 @@
 package racingcar.util;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputTryCount {
 
-    public int parseNumber(String input) {
+    private static final String INPUT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+
+    public int inputCount() {
+        System.out.println(INPUT_TRY_COUNT);
+        String tryCount = Console.readLine();
+
+        return parseNumber(tryCount);
+    }
+
+    public int parseNumber(String tryCount) {
+
         return 0;
     }
 }
