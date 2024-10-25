2683f1e5f4a76dd5db9de733c00118a3f54a9e47
IM-GYURI <kgulr0517@gmail.com>
2024-10-25 22:31:17 +0900

    feat : revise AppConfig
    gameController()
    - new CarMaker() -> carMaker() 수정
    - newRoundNumberValidator() 추가
    
    carMaker()
    - CarMaker 생성자 추가
    - new CarNameValidator 주입

--- src/main/java/racingcar/config/AppConfig.java
+++ src/main/java/racingcar/config/AppConfig.java
@@ -3,7 +3,9 @@ package racingcar.config;
 import racingcar.controller.GameController;
 import racingcar.model.CarMaker;
 import racingcar.model.CarMover;
+import racingcar.model.CarNameValidator;
 import racingcar.model.RacingGame;
+import racingcar.model.RoundNumberValidator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -11,10 +13,15 @@ public class AppConfig {
 
     public GameController gameController() {
         return new GameController(
-                new InputView(), new OutputView(), new CarMaker(), racingGame());
+                new InputView(), new OutputView(), carMaker()
+                , racingGame(), new RoundNumberValidator());
     }
 
     public RacingGame racingGame() {
         return new RacingGame(new CarMover());
+    }
+
+    public CarMaker carMaker() {
+        return new CarMaker(new CarNameValidator());
     }
 }
