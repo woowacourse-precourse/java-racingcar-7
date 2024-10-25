76e4770c45f3601b675dfe70f5d54310cd0ee53f
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-26 02:20:54 +0900

    test: 숫자 입력시 에러 테스트 추가, 메서드 명 수정
    전체 케이스 실행 위해 AfterEach 실행한다.
    공백이나 아무것도 입력하지 않았을 때, 에러메시지 출력한다.
    메서드 명을 BBD구조로 수정한다.

--- src/test/java/racingcar/InputManagerTest.java
+++ src/test/java/racingcar/InputManagerTest.java
@@ -3,15 +3,22 @@ package racingcar;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class InputManagerTest {
 
+    @AfterEach
+    void rollback() {
+        Console.close();
+    }
+
     @DisplayName("올바른 게임 횟수가 입력되는지 확인한다.(10 이하의 숫자)")
     @Test
-    void checkCorrectGameRound() {
+    void Given_InputIsRight_When_RoundsGetInput_Then_Success() {
         // Given
         String input = "5";
         byte[] bytes = input.getBytes();
@@ -28,7 +35,7 @@ class InputManagerTest {
 
     @DisplayName("올바른 게임 횟수가 입력되는지 확인한다.(정수 최댓값)")
     @Test
-    void checkCorrectGameRoundTwo() {
+    void Given_InputIsUnderInteger_When_RoundsGetInput_Then_Success() {
         // Given
         String input = "2147483647";
         byte[] bytes = input.getBytes();
@@ -42,9 +49,40 @@ class InputManagerTest {
         assertThat(rounds).isEqualTo(2147483647);
     }
 
+    @DisplayName("공백을 입력했을 경우, 에러 메시지가 발생한다.")
+    @Test
+    void Given_InputIsBlank_Then_GetErrorMessage() {
+        // Given
+        String input = " ";
+        byte[] bytes = input.getBytes();
+        ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+        System.setIn(in);
+        // When
+
+        // Then
+        assertThatThrownBy(InputManager::getGameRound)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("게임 횟수가 입력되지 않았습니다.");
+    }
+
+    @DisplayName("아무것도 입력하지 않았을 때, 에러 메시지가 발생한다.")
+    @Test
+    void Given_InputIsNothing_Then_GetErrorMessage() {
+        // Given
+        String input = "\n";
+        byte[] bytes = input.getBytes();
+        ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+        System.setIn(in);
+        // When
+        // Then
+        assertThatThrownBy(InputManager::getGameRound)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("게임 횟수가 입력되지 않았습니다.");
+    }
+
     @DisplayName("게임 횟수가 1보다 작은 수가 들어왔을 때, 에러 메시지가 발생한다.")
     @Test
-    void checkGameRoundUnderOneTest() {
+    void Given_InputIsUnderNumberOne_Then_GetErrorMessage() {
         // Given
         String input = "0";
         byte[] bytes = input.getBytes();
@@ -60,7 +98,7 @@ class InputManagerTest {
 
     @DisplayName("게임 횟수가 정수 범위일 때, 에러 메시지가 발생한다.")
     @Test
-    void checkGameRoundRangeInteger() {
+    void Given_InputIsOverInteger_Then_GetErrorMessage() {
         // Given
         String input = "2147483648";
         byte[] bytes = input.getBytes();
@@ -76,7 +114,7 @@ class InputManagerTest {
 
     @DisplayName("숫자가 아닌 입력값만 들어왔을 때, 에러 메시지가 발생한다.")
     @Test
-    void checkGameRoundIsNotNumeric() {
+    void Given_InputIsNotNumeric_Then_GetErrorMessage() {
         // Given
         String input = "가나다라";
         byte[] bytes = input.getBytes();
@@ -92,7 +130,7 @@ class InputManagerTest {
 
     @DisplayName("숫자가 아닌 입력값이 숫자와 같이 들어왔을 때, 에러 메시지가 발생한다.")
     @Test
-    void checkGameRoundIsNotNumericAndNumeric() {
+    void Given_InputIsNumericWithNonNumeric_Then_GetErrorMessage() {
         // Given
         String input = "3a";
         byte[] bytes = input.getBytes();
