5df2415efe47ed227f06ca61b26bdc1af549c4c6
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-10-25 15:14:32 +0900

    refactor(model): 검증부 리팩토링
    - 숫자 상수화
    - 문구 상수화
    - 검증부 분리

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,14 +1,21 @@
 package racingcar.model;
 
 public class Car {
+
     private final String name;
     private int winStack;
 
     public Car(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
-        }
+        CarNameLengthLimitValidator(name);
         this.name = name;
         this.winStack = 0;
+    }
+
+    private static void CarNameLengthLimitValidator(String name) {
+        final int CAR_NAME_LENGTH_LIMIT = 5;
+        final String CAR_NAME_LENGTH_LIMIT_VALIDATION = "자동차 이름은 5자 이하여야 합니다.";
+        if (name.length() > CAR_NAME_LENGTH_LIMIT) {
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_LIMIT_VALIDATION);
+        }
     }
 }
