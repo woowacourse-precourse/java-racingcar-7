84ddb1829545629b50a1c1fbc98d5044b7cf571d
KORyounho <dordyd@naver.com>
2024-10-26 08:58:36 +0900

    feat(Application): input post process

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,33 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String inputCarName = Console.readLine();
+        int inputTry = Integer.parseInt(Console.readLine());
 
-        System.out.println("git test!");
+        String[] carListName = inputCarName.split(",");
+
+        initCarList(carListName);
+
+
     }
+
+    private static void initCarList(String[] carListName) {
+        ArrayList<Car> carList = new ArrayList<>();
+
+        for (String carName : carListName) {
+            isValidCarName(carName);
+            carList.add(new Car(carName));
+        }
+    }
+
+    private static void isValidCarName(String carName){
+        if(carName.length() < 5){
+            throw new IllegalArgumentException("차의 이름이 5글자를 넘어갑니다.");
+        }
+    }
+
 }
