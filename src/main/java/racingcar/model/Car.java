2ba9175d5c7f2c3cf0974a158d06457045b847b2
Yun-tellije <dbswjddms517@naver.com>
2024-10-26 01:48:09 +0900

    feat: 자동차 클래스 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class Car {
+    String name;
+    int forwardCount;
+
+    public Car(String name) {
+        this.name = name;
+        this.forwardCount = 0;
+    }
+}
