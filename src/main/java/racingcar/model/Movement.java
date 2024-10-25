a2b56c0bc99ab1e1d5dfdc02f10a56a692538234
Chan531 <chan657312@gmail.com>
2024-10-25 15:26:14 +0900

    feat(Movement): 이동값 상수 추가

--- src/main/java/racingcar/model/Movement.java
+++ src/main/java/racingcar/model/Movement.java
@@ -4,6 +4,7 @@ public class Movement {
 
 	private static final int INITIAL_MOVEMENT_VALUE = 0;
 	private static final int MIN_REQUIRED_VALUE_FOR_MOVEMENT = 4;
+	private static final int MOVEMENT_VALUE = 1;
 
 	private final int movement;
 
