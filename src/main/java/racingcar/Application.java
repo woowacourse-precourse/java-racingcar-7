69fde69459e2aaca2dd76fb7fa11be9b9de47281
jintakkim <mine407603@gmail.com>
2024-10-25 15:27:21 +0900

    feat(Application): 어플리케이션 작동 구현
    Input,Output,Cars,Parser 클래스를 사용하여
    자동차 경주 게임이 기능 요구 사항에 맞게 작동하도록 구현한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import racingcar.io.Input;
+import racingcar.io.Output;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //초기화
+        Input input = new Input();
+        Parser parser = new Parser(input.read());
+        Cars cars = new Cars(parser.parseCarNameInput());
+        Output output = new Output(cars);
+
+        output.printPrefix();
+
+        int raceRound = parser.parseRaceRoundInput();
+        for(int i = 0; i < raceRound; i++) {
+            cars.run();
+            output.printProcess();
+        }
+
+        output.printResult();
     }
 }
