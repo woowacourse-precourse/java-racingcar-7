de48658cd1e98b109a4f84d9d7e4480801528dd0
jklee3409 <jklee3409@khu.ac.kr>
2024-10-25 20:25:17 +0900

    feat(gamecontroller): 생성자 구현

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,11 @@
+package racingcar.controller;
+
+import javax.swing.text.View;
+
+public class GameController {
+    private final View view;
+
+    public GameController(View view) {
+        this.view = view;
+    }
+}
