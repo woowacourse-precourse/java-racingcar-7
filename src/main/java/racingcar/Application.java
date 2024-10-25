57bbd96805f53fcaa3da1fac3adcaa8635a49a19
choign3123 <choign3123@naver.com>
2024-10-25 12:33:49 +0900

    Feat(Application): 자동차 경주 게임 세팅

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,9 @@
 package racingcar;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         IInputDisplay inputDisplay = new WtcInputDisplay();
@@ -8,7 +11,14 @@ public class Application {
         String carNameMass = inputDisplay.getPromptInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
         String[] carNameList = carNameMass.split(",");
 
+        List<ICar> carList = new ArrayList<>();
+        for (final String carName : carNameList) {
+            carList.add(new CarImpl(carName));
+        }
+
         String roundStr = inputDisplay.getPromptInput("시도할 횟수는 몇 회인가요?\n");
         int round = Integer.parseInt(roundStr);
+
+        RacingGame racingGame = new RacingGame(carList, round);
     }
 }
