ed80cae00065c0c2e0636870e856acefd2c73b3a
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-10-25 23:26:45 +0900

    feat: 자동차 전진 함수 구현

--- src/main/java/racingcar/model/random/Random.java
+++ src/main/java/racingcar/model/random/Random.java
@@ -1,8 +1,16 @@
 package racingcar.model.random;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.model.car.Car;
 
 public class Random {
+    public static void goRandomForward(Car car) {
+        boolean forward = Random.randomBoolean();
+        if (forward) {
+            car.goForward();
+        }
+    }
+
     public static boolean randomBoolean() {
         final int randomNumber = Randoms.pickNumberInRange(0, 9);
         if (randomNumber >= 4) {
