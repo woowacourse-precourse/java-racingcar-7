b081b8307cac2f8c3467863ca77ff801b014bef8
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 20:34:38 +0900

    test: 자동차 이름 검증 테스트 코드를 작성한다

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,12 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.validator.CarNameValidator;
+
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -12,6 +17,7 @@ class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
+    @Disabled("완성이후 테스트할 것")
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
@@ -23,12 +29,73 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Disabled("완성이후 테스트할 것")
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @DisplayName("자동차 이름이 공백일 시 검증테스트")
+    @Test
+    void 자동차_목록_검증_테스트1() {
+        CarNameValidator carNameValidator = new CarNameValidator();
+        List<String> carList = List.of("pobi", "", "jun");
+
+        assertThatThrownBy(() -> carNameValidator.nameLength(carList))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차 이름이 5글자 이상일 시 검증테스트")
+    @Test
+    void 자동차_목록_검증_테스트2() {
+        CarNameValidator carNameValidator = new CarNameValidator();
+        List<String> carList = List.of("pobi", "woniwoni", "jun");
+
+        assertThatThrownBy(() -> carNameValidator.nameLength(carList))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차 이름이 중복일 시 검증테스트")
+    @Test
+    void 자동차_목록_검증_테스트3() {
+        CarNameValidator carNameValidator = new CarNameValidator();
+        List<String> carList = List.of("pobi", "pobi", "jun");
+
+        assertThatThrownBy(() -> carNameValidator.nameOverlap(carList))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차리스트 값이 잘못된 패턴일 시 검증테스트1")
+    @Test
+    void 자동차_목록_검증_테스트4() {
+        CarNameValidator carNameValidator = new CarNameValidator();
+        String cars = "pobi,woni,jun,";
+
+        assertThatThrownBy(() -> carNameValidator.rightNamePattern(cars))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차리스트 값이 잘못된 패턴일 시 검증테스트2")
+    @Test
+    void 자동차_목록_검증_테스트5() {
+        CarNameValidator carNameValidator = new CarNameValidator();
+        String cars = ",pobi,woni,jun";
+
+        assertThatThrownBy(() -> carNameValidator.rightNamePattern(cars))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차리스트 값이 잘못된 패턴일 시 검증테스트3")
+    @Test
+    void 자동차_목록_검증_테스트6() {
+        CarNameValidator carNameValidator = new CarNameValidator();
+        String cars = ",pobi,woni,jun,";
+
+        assertThatThrownBy(() -> carNameValidator.rightNamePattern(cars))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
