10261c7d3177546c3fbb08eb1e17114e9f3eb489
koomchang <leahpar0401@gmail.com>
2024-10-25 15:17:45 +0900

    feat: 자동차 이름을 VO로 구현

---
+++ src/main/java/racingcar/model/Name.java
@@ -0,0 +1,13 @@
+package racingcar.model;
+
+import racingcar.validator.BasicValidator;
+import racingcar.validator.NameValidator;
+
+public record Name(String name) {
+
+    private static final BasicValidator<String> carNameValidator = new NameValidator();
+
+    public Name {
+        carNameValidator.validate(name);
+    }
+}
