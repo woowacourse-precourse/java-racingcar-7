5cbe8c7164c4ae302e4c196e3be26ecd6d40f7b3
seophohoho <seop0937@gmail.com>
2024-10-26 01:50:20 +0900

    feat(Validator): validate 메소드 구현

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,6 +1,25 @@
 package racingcar;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validator {
     private final String INPUT_COUNT_PATTERN = "^[1-9][0-9]*";
     private final String INPUT_CARS_PATTERN = "^[^\\s,]{1,5}(,[^\\s,]{1,5})*";
+
+    public void validate(String inputCars, String inputCount) {
+        Pattern patternInputCount = Pattern.compile(INPUT_COUNT_PATTERN);
+        Matcher matcherInputCount = patternInputCount.matcher(inputCount);
+
+        Pattern patternInputCars = Pattern.compile(INPUT_CARS_PATTERN);
+        Matcher matcherInputCars = patternInputCars.matcher(inputCars);
+
+        if (!matcherInputCount.matches() || !matcherInputCars.matches()) {
+            throwError();
+        }
+    }
+
+    public void throwError() {
+        throw new IllegalArgumentException();
+    }
 }
