05f4f8becfd0427812e19404cb363e16a9cf702a
Chan531 <chan657312@gmail.com>
2024-10-25 17:13:58 +0900

    feat(WinnerResultGenerator): 공동 우승자 문자열 생성

--- src/main/java/racingcar/model/WinnerResultGenerator.java
+++ src/main/java/racingcar/model/WinnerResultGenerator.java
@@ -1,7 +1,13 @@
 package racingcar.model;
 
+import java.util.List;
+
 public class WinnerResultGenerator {
 
 	private static final int SINGLE_WINNER_COUNT = 1;
 	private static final String SEPARATOR = ", ";
+
+	private static String generateJointWinnersResult(List<String> winners) {
+		return String.join(SEPARATOR, winners);
+	}
 }
