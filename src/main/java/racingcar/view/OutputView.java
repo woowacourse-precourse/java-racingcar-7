cd1f35af085879d55429c821684cd804434d9a7c
dongyun <ehddbs452100@naver.com>
2024-10-25 21:18:26 +0900

    refactor(OutputView): print result
    - 프로그램 실행 시 기본 메시지 출력 뿐만 아니라 터미널에 보여지는 다른 클래스들의 결괏값을 출력
    - 동적으로 전진 횟수를 업데이트하는 메소드를 출력 클래스에 넣은 이유는 출력 클래스 역할로만 본다면 결괏값만 출력하는
    로직이있어야하는게 맞지만 중간 과정을 출력을 해야하기에 모든 과정을 기록하는 List 값을 넣는건 메모리 상 비효율적이라고 판단이
    들어서 동적으로 전진 횟수를 업데이트 하는 메소드 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,18 +1,40 @@
 package racingcar.view;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 import static racingcar.global.Message.MOVE_FORWARD_RESULT;
 import static racingcar.global.Message.WINNER;
 
 import java.util.List;
+import racingcar.domain.MoveForwardRecord;
 
 public class OutputView {
 
-    public static void printWinner(List<String> winners) {
+    private static final String COLON = " : ";
+    private static final String BAR = "-";
+
+    public void printWinner(List<String> winners) {
         System.out.print(WINNER.getMessage());
         System.out.print(String.join(",", winners));
     }
 
-    public static void printMoveForwardResult() {
+    public void printMoveForwardResult() {
         System.out.println(MOVE_FORWARD_RESULT.getMessage());
+    }
+
+    public void printMoveForwardRecord(List<MoveForwardRecord> moveForwardRecords) {
+        moveForwardRecords.stream()
+                .forEach(record -> System.out.println(record.getCarName() + COLON + BAR.repeat(record.getGoCount())));
+        System.out.println();
+    }
+
+    public void go(String tryCounts, List<MoveForwardRecord> moveForwardRecords) {
+
+        printMoveForwardResult();
+        for (int i = 0; i < Integer.parseInt(tryCounts); i++) {
+            moveForwardRecords.stream()
+                    .forEach(moveForwardRecord -> moveForwardRecord.updateGoCount(pickNumberInRange(0, 9)));
+            printMoveForwardRecord(moveForwardRecords);
+        }
+
     }
 }
