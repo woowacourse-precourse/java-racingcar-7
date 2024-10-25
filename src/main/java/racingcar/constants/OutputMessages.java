e3b3e7197a7156301feb6e42d1e9452d7f0c259a
yush <yush2202@gmail.com>
2024-10-25 19:51:09 +0900

    refactor($ OutputMessages): 상수 이름 변경
    상수 이름을 변경 사양에 맞게 수정
    before:
    - INSERT_CARS_NAME
    
    after:
    - INSERT_RACERS_NAME

--- src/main/java/racingcar/constants/OutputMessages.java
+++ src/main/java/racingcar/constants/OutputMessages.java
@@ -1,7 +1,7 @@
 package racingcar.constants;
 
 public class OutputMessages {
-    public static final String INSERT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INSERT_RACERS_NAME = "경주할 레이서 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String INSERT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
     public static final String RACING_RESULT = "실행 결과";
     public static final String FINAL_WINNER = "최종 우승자 : ";
