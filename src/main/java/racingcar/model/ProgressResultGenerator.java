cef9b4cc256d1bc4a2cd1ada3d20c763061e1b16
Chan531 <chan657312@gmail.com>
2024-10-25 16:13:03 +0900

    feat(ProgressResultGenerator): 자동차 상태 문자열 생성
    Car의 name과 이동 문자열로 이루어진 문자열 생성

--- src/main/java/racingcar/model/ProgressResultGenerator.java
+++ src/main/java/racingcar/model/ProgressResultGenerator.java
@@ -5,6 +5,10 @@ public class ProgressResultGenerator {
 	private static final String CAR_MOVEMENT_SYMBOL = "-";
 	private static final String SEPARATOR = " : ";
 
+	private static String generateCarStatus(Car car) {
+		return car.getName() + SEPARATOR + formatMovementStatus(car.getMovement());
+	}
+
 	private static String formatMovementStatus(int number) {
 		return CAR_MOVEMENT_SYMBOL.repeat(number);
 	}
