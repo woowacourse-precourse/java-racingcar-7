266fe702ac11f73ae914064237ebeaca9db44bfe
githublees <leeho_@naver.com>
2024-10-25 16:07:24 +0900

    refactor: Change OutputView from utility class to instance class

--- src/main/java/racingcar/output/OutputViewer.java
+++ src/main/java/racingcar/output/OutputViewer.java
@@ -4,22 +4,26 @@ import racingcar.constant.Constant;
 
 public class OutputViewer {
 
-    private OutputViewer() {
+    public OutputViewer() {
     }
 
-    public static void getCarNameInputMessage() {
+    public void getCarNameInputMessage() {
         System.out.println(Constant.CAR_NAME_INPUT_STRING);
     }
 
-    public static void getTryCountInputMessage() {
+    public void getTryCountInputMessage() {
         System.out.println(Constant.TRY_COUNT_INPUT_STRING);
     }
 
-    public static void getRacingProgressMessage() {
-        System.out.println(Constant.RACING_PROGRESS_STRING);
+    public void getRacingProgressMessage() {
+        System.out.println("\n" + Constant.RACING_PROGRESS_STRING);
     }
 
-    public static void getRacingResultMessage(String result) {
+    public void getSystemOutPrint(String string) {
+        System.out.print(string);
+    }
+
+    public void getRacingResultMessage(String result) {
         System.out.println(Constant.RACING_RESULT_STRING + result);
     }
 }
