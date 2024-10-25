1a3ee12d0f676439669ba3e38275c6e8598e24d7
sinminseok12 <sinminseok12@naver.com>
2024-10-26 05:05:51 +0900

    feat : 특수문자가 들어온 경우 예외처리 구현
    자동차 이름 입력시 ','이외의 특수문자가 들어온 경우 예외를 발생시킨다

--- src/main/java/racingcar/view/InputValidator.java
+++ src/main/java/racingcar/view/InputValidator.java
@@ -5,11 +5,14 @@ import java.util.regex.Pattern;
 public class InputValidator {
     private static final String ERROR_MESSAGE_EMPTY_INPUT = "입력값은 공백일 수 없습니다.";
     private static final String ERROR_MESSAGE_TRAILING_COMMA = "입력값의 마지막에 콤마(,)가 올 수 없습니다.";
+    private static final String ERROR_MESSAGE_INVALID_CHARACTERS = "입력값에 콤마(,)이외의 특수문자를 허용하지 않습니다.";
     private static final Pattern TRAILING_COMMA_PATTERN = Pattern.compile(".*,$");
+    private static final Pattern VALID_CHARACTERS_PATTERN = Pattern.compile("^[a-zA-Z가-힣0-9,\\s]+$");
 
     public static void validateCarNamesInput(String input) {
         checkEmptyInput(input);
         checkTrailingComma(input);
+        checkInvalidCharacters(input);
     }
 
     public static void validateAttemptCountInput(String input) {
@@ -25,6 +28,12 @@ public class InputValidator {
     private static void checkTrailingComma(String input) {
         if (TRAILING_COMMA_PATTERN.matcher(input).matches()) {
             throw new IllegalArgumentException(ERROR_MESSAGE_TRAILING_COMMA);
+        }
+    }
+
+    private static void checkInvalidCharacters(String input) {
+        if (!VALID_CHARACTERS_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_CHARACTERS);
         }
     }
 }
