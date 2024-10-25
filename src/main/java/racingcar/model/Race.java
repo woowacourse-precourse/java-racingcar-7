eab036289fe408f3816826d7c84684b8697dbbef
jklee3409 <jklee3409@khu.ac.kr>
2024-10-25 20:35:50 +0900

    feat(race): getWinners 메서드 구현

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -2,6 +2,7 @@ package racingcar.model;
 
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.stream.Collectors;
 
 public class Race {
     private final List<Car> cars;
@@ -15,6 +16,17 @@ public class Race {
             int randomValue = Randoms.pickNumberInRange(0, 9);
             car.move(randomValue);
         }
+    }
+
+    public List<Car> getWinners() {
+        int maxDistance = cars.stream()
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getDistance() == maxDistance)
+                .collect(Collectors.toList());
     }
 
     public List<Car> getCars() {
