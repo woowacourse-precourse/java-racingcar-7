0cb1b9ab34069da52310f8b8c3a05797d81f9b53
김예린 <annalin1004@gmail.com>
2024-10-25 23:52:17 +0900

    Modify the function to check valid input values
    Implemented a function to remove race cars with the same name.

--- src/main/java/racingcar/service/ValidService.java
+++ src/main/java/racingcar/service/ValidService.java
@@ -1,15 +1,23 @@
 package racingcar.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ValidService {
     public boolean isCarNames(String carNames){
         List<String> carList = List.of(carNames.split(","));
+        List<String> listCopy = new ArrayList<>(carList);
+
         for(String car : carList){
             if(car.isEmpty()){
                 return false;
             }
+            listCopy.remove(carList.indexOf(car));
+            if(listCopy.contains(car)){
+                return false;
+            }
         }
+
         return true;
     }
 
