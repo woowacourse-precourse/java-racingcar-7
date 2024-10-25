f71ad150cf211ab3c7ed8ef7f889e10854fb40ad
LeeDongHoon <likelion.leedh@gmail.com>
2024-10-25 15:31:08 +0900

    [TEST] : 차 이름 유효성 검사 로직 변경에 따른 테스트 코드 변경

--- src/test/java/racingcar/validationTest/CarNameValidationTest.java
+++ src/test/java/racingcar/validationTest/CarNameValidationTest.java
@@ -5,41 +5,57 @@ import org.junit.jupiter.api.Test;
 import racingcar.exception.RacingExceptionStatus;
 import racingcar.validation.CarNameValidation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CarNameValidationTest {
 
     @Test
     @DisplayName("올바른 차 이름 입력")
-    void 시도_횟수_입력_성공(){
-        String names = "  asd  ";
+    void 차_이름_입력_성공(){
+
+        List<String> names = new ArrayList<>();
+        names.add(" name ");
+        names.add("name");
         assertDoesNotThrow(() -> CarNameValidation.validate(names));
     }
 
     @Test
     @DisplayName("자동차 이름이 5자를 넘는 경우")
-    void validate_carNameTooLong() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            CarNameValidation.validate("LongName");
-        });
+    void 차_이름_5글자_넘음_실패() {
+
+        List<String> names = new ArrayList<>();
+        names.add("LongName");
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> {
+                    CarNameValidation.validate(names);
+                });
         assertEquals(RacingExceptionStatus.INVALID_NAME_LENGTH.getMessage(), exception.getMessage());
     }
-
     @Test
     @DisplayName("자동차 이름이 빈 문자열인 경우")
-    void validate_carNameBlank() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            CarNameValidation.validate("");
-        });
+    void 차_이름_빈_문자열_실패() {
+
+        List<String> names = new ArrayList<>();
+        names.add("");
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> {
+                    CarNameValidation.validate(names);
+                });
         assertEquals(RacingExceptionStatus.INVALID_NAME_BLANK.getMessage(), exception.getMessage());
     }
-
     @Test
     @DisplayName("자동차 이름이 공백만 포함된 경우")
-    void validate_carNameWithOnlySpace() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            CarNameValidation.validate(" ");
-        });
+    void 차_이름_공백만_실패() {
+
+        List<String> names = new ArrayList<>();
+        names.add(" ");
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> {
+                    CarNameValidation.validate(names);
+                });
         assertEquals(RacingExceptionStatus.INVALID_NAME_BLANK.getMessage(), exception.getMessage());
     }
 }
