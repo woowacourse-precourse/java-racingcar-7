5c2af31a54dc90cb325ae33789568f1b3fd9947d
Chan531 <chan657312@gmail.com>
2024-10-25 17:14:58 +0900

    feat(WinnerResultGenerator): 공동 우승자 여부 체크 후 만족한다면 반환

--- src/main/java/racingcar/model/WinnerResultGenerator.java
+++ src/main/java/racingcar/model/WinnerResultGenerator.java
@@ -7,6 +7,12 @@ public class WinnerResultGenerator {
 	private static final int SINGLE_WINNER_COUNT = 1;
 	private static final String SEPARATOR = ", ";
 
+	public static String generateWinnerResult(List<String> winners) {
+		if (winners.size() > SINGLE_WINNER_COUNT) {
+			return generateJointWinnersResult(winners);
+		}
+	}
+
 	private static String generateJointWinnersResult(List<String> winners) {
 		return String.join(SEPARATOR, winners);
 	}
