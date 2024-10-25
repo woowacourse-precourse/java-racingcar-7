2522770c404d189f86e6ce7cbea4d63ccd7df9f6
김예린 <annalin1004@gmail.com>
2024-10-25 23:34:47 +0900

    Implement racing functions(1)
    The random value required for the racing function is generated
    and the start function is repeated as many times as the number
     of repetitions when it exceeds a certain value.

--- src/main/java/racingcar/service/ValidService.java
+++ src/main/java/racingcar/service/ValidService.java
@@ -1,6 +1,25 @@
 package racingcar.service;
 
+import java.util.List;
+
 public class ValidService {
-    public boolean isInput(String carNams){return true;}
-    public boolean isInput(int repeatNum) {return true;}
+    public boolean isCarNames(String carNames){
+        List<String> carList = List.of(carNames.split(","));
+        for(String car : carList){
+            if(car.isEmpty()){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isRepeat(String inputNum){
+        for(int i=0; i<inputNum.length(); i++){
+            char num = inputNum.charAt(i);
+            if((int)num < (int)'0' || (int)num > (int)'9'){
+                return false;
+            }
+        }
+        return true;
+    }
 }
