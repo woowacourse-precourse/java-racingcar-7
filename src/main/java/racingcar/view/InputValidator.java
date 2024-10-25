4ef33c9d95788ca487f5bafa7ac32465dc100e15
Jin409 <sualng123@gmail.com>
2024-10-25 17:33:50 +0900

    feat(InputValidator, InputValidatorTest): 정규식의 조합을 통해 자동차의 이름 입력값에 숫자, 한글, 영어만 포함 되는지 확인해라
    정규식을 통해 숫자, 한글, 영어만 포함되었는지 확인하는 메서드의 구현:
    - 추후 허용되는 값의 종류가 달라지더라도 수정하기 용이하도록 상수로 패턴의 범위를 선언했다.

--- src/main/java/racingcar/view/InputValidator.java
+++ src/main/java/racingcar/view/InputValidator.java
@@ -3,6 +3,10 @@ package racingcar.view;
 public class InputValidator {
 
     private static final String DELIMITER = ",";
+    private static final String KOREAN_RANGE_OF_REGEX = "가-힣";
+    private static final String ENGLISH_RANGE_OF_REGEX = "a-zA-Z";
+    private static final String NUMBER_RANGE_OF_REGEX = "0-9";
+    private static final String WHITESPACE_REGEX = "\\s";
 
     public static void validateNameOfCars(String input) {
         if (input.isBlank()) {
@@ -15,6 +19,10 @@ public class InputValidator {
 
         if (input.endsWith(DELIMITER)) {
             throw new IllegalArgumentException("자동차의 이름은 구분자인 쉼표(,)로 끝나서는 안됩니다.");
+        }
+
+        if (containsInvalidCharactersForNameOfCars(input)) {
+            throw new IllegalArgumentException("자동차의 이름은 숫자, 알파벳, 한글만 가능합니다.");
         }
     }
 
@@ -30,6 +38,12 @@ public class InputValidator {
 
     private static boolean containsOnlyDelimiter(String input) {
         return input.replace(DELIMITER, "").isBlank();
+    }
+
+    private static boolean containsInvalidCharactersForNameOfCars(String input) {
+        String pattern = "[" + KOREAN_RANGE_OF_REGEX + ENGLISH_RANGE_OF_REGEX + NUMBER_RANGE_OF_REGEX + DELIMITER
+                + WHITESPACE_REGEX + "]*";
+        return !input.matches(pattern);
     }
 
     private static boolean isNotPositiveInteger(String input) {
--- src/test/java/racingcar/view/InputValidatorTest.java
+++ src/test/java/racingcar/view/InputValidatorTest.java
@@ -1,6 +1,7 @@
 package racingcar.view;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -38,6 +39,23 @@ public class InputValidatorTest {
                 .hasMessage("자동차의 이름은 구분자인 쉼표(,)로 끝나서는 안됩니다.");
     }
 
+    @ParameterizedTest
+    @DisplayName("사용자가 입력한 자동차의 이름이 숫자, 알파벳, 한글에 포함되지 않는 경우 예외를 던진다.")
+    @ValueSource(strings = {"pobi**", "jun,w*oni", "中,22"})
+    void throwExceptionWhenNameContainsInvalidCharactersForNameOfCars(String inputValue) {
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validateNameOfCars(inputValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차의 이름은 숫자, 알파벳, 한글만 가능합니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("사용자가 입력한 자동차의 이름이 숫자, 알파벳, 한글에 포함된 경우 예외를 던지지 않는다.")
+    @ValueSource(strings = {"r12n", "한글eng"})
+    void doesNotThrowExceptionWhenNameContainsOnlyValidCharactersForNameOfCars(String inputValue) {
+        // when & then
+        assertDoesNotThrow(() -> InputValidator.validateNameOfCars(inputValue));
+    }
 
     @ParameterizedTest
     @DisplayName("사용자가 입력한 총 라운드가 양의 정수가 아니면 예외를 던진다.")
