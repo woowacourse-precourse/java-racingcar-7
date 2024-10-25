825697e4062fb8a8917fa70a7818686942084734
jinukee <jinukee333@gmail.com>
2024-10-25 16:05:05 +0900

    feat(view.OutputView) : "실행 결과"를 출력하는 method 추가
    프로그램 출력 양식에 맞게 게임결과 출력 전 "실행 결과"를 출력하는 method를 추가했습니다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -13,4 +13,8 @@ public class OutputView {
     public static void printWinners(List<String> winners){
         System.out.printf("최종 우승자 : " + String.join(", " , winners));
     }
+
+    public static void printResultMessage(){
+        System.out.println("실행 결과");
+    }
 }
