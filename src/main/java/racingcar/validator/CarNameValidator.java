66610c4d2118a898793a3420d26260bb2bfc2680
Hyso <hsk7953@gmail.com>
2024-10-25 19:49:30 +0900

    refactor(CarNameValidator) : 메서드 접근 제어자 수정 - 검증 메서드들을 package-private로 수정 - 테스트를 위해 private 대신 선택

--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -22,44 +22,45 @@ public class CarNameValidator {
         validateDuplication(nameSet.size(), carNames.length);
     }
 
-    public static void validateCarNameLength(String input){
+    static void validateCarNameLength(String input){
         if(input.length() > 5){
             throw new IllegalArgumentException("자동차의 이름은 5자 이하만 가능합니다.");
         }
     }
 
-    public static void validateEnd(String input){
+    static void validateEnd(String input){
         if(input.endsWith(",")){
             throw new IllegalArgumentException("입력의 마지막은 콤마(,)로 끝날 수 없습니다.");
         }
     }
 
-    public static void validateDelimiter(String input){
+    static void validateDelimiter(String input){
         String regex = "^[^,]*$";
         if(input.matches(regex)){
             throw new IllegalArgumentException("자동차 이름들은 콤마로 구분해야합니다.");
         }
     }
 
-    public static void validateNotBlank(String input){
+    static void validateNotBlank(String input){
         if(input.isBlank()){
-            throw new IllegalArgumentException("빈 칸은 입력할 수 없습니다.");
+            throw new IllegalArgumentException("빈 값은 입력할 수 없습니다.");
         }
     }
 
-    public static void validateNoSpace(String input){
+    static void validateNoSpace(String input){
         if(input.contains(" ")){
             throw new IllegalArgumentException("입력에 공백이 포함될 수 없습니다.");
         }
     }
 
-    public static void validateDuplication(int carSetSize, int carArrayLength){
+    static void validateDuplication(int carSetSize, int carArrayLength){
         if(carSetSize != carArrayLength){
             throw new IllegalArgumentException("자동차 이름에 중복이 존재합니다.");
         }
     }
 
-    public static void validateEmptyName(String input){
+
+    static void validateEmptyName(String input){
         if(input.isBlank()){
             throw new IllegalArgumentException("자동차의 이름은 빈 값일 수 없습니다.");
         }
