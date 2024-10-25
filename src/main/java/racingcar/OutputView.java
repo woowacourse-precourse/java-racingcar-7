e21a2375ad41b8221735f321afe21f2effdc0b2d
chaerish <ccyy8432@naver.com>
2024-10-26 01:50:32 +0900

    feat(OutputView): 출력을 담당하는 OutputView 클래스 생성
    경주게임 출력을 위한 OutputView를 생성한다.

---
+++ src/main/java/racingcar/OutputView.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class OutputView {
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
+
+    public void announceWinner(String[] winners) {
+        System.out.println(WINNER_MESSAGE);
+    }
+
+}
