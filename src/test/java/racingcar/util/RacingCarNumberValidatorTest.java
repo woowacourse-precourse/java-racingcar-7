b958bbf33960a296e580d469f898481d9bb86b93
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-25 17:10:59 +0900

    test(RacingCarNumberValidatorTest): 숫자 유효성 검증 테스트
    테스트한 내용은 다음과 같습니다:
    - 숫자 형식 또는 빈문자열이 아니므로 예외가 발생한다.
    - 숫자가 최대 범위를 초과하여 예외가 발생한다.

---
+++ src/test/java/racingcar/util/RacingCarNumberValidatorTest.java
@@ -0,0 +1,42 @@
+package racingcar.util;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RacingCarNumberValidatorTest {
+
+    private RacingCarNumberValidator racingCarNumberValidator;
+
+    @BeforeEach
+    void setUp() {
+        racingCarNumberValidator = new RacingCarNumberValidator();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "10.", " ", ","})
+    @DisplayName("숫자 형식 또는 빈문자열이 아니므로 예외가 발생한다.")
+    void validateFormatTest(final String value) throws Exception {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> racingCarNumberValidator.validateFormat(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {100, 1000, 10000})
+    @DisplayName("숫자가 최대 범위를 초과하여 예외가 발생한다.")
+    void validateRangeTest(final int value) {
+        //given
+        final int maxValue = 1;
+        //when
+        //then
+        assertThatThrownBy(() -> racingCarNumberValidator.validateRange(value, maxValue))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
