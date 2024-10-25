0da11d47a5c3eb33661662143285c994495b7b8a
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 20:12:06 +0900

    refactor: 상수를 Constants 클래스로 이동한다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,10 +2,9 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class InputView {
+import static racingcar.constants.Constants.*;
 
-    private static final String INPUT_CAR_LIST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String INPUT_TRY_ROUND_MESSAGE = "시도할 횟수는 몇 회인가요?";
+public class InputView {
 
     public static String inputCar() {
         System.out.println(INPUT_CAR_LIST_MESSAGE);
