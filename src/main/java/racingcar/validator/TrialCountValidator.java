8974b9d6b0f15cc3385171015744d3cc26f5e6ba
Hyso <hsk7953@gmail.com>
2024-10-25 19:24:15 +0900

    feat(TrialCountValidator) : 시도 횟수에 대한 입력값 검증 로직 추가 - validateNotBlank  - 비어있는 값은 입력 유무 확인 - validateNumeric  - 1-9 숫자 외에 다른 값 유무 확인

---
+++ src/main/java/racingcar/validator/TrialCountValidator.java
@@ -0,0 +1,21 @@
+package racingcar.validator;
+
+public class TrialCountValidator {
+
+    private TrialCountValidator(){
+    }
+
+    public static void validateNotBlank(String input){
+        if(input.isBlank()) {
+            throw new IllegalArgumentException("빈 값은 입력할 수 없습니다.");
+        }
+    }
+
+    public static void validateNumeric(String input){
+        String regex = "^[1-9]\\d*$";
+        if(!input.matches(regex)){
+            throw new IllegalArgumentException("양의 정수만 입력할 수 있습니다.");
+        }
+    }
+
+}
