9ff242c882857bd06bc14ce31ef14a5ac5c9fad9
GS_song98 <20003204@sju.ac.kr>
2024-10-25 20:17:21 +0900

    test: 랜덤 값에 따라 움직이는지에 대한 테스트 케이스 추가

--- src/test/java/racingcar/domain/PositionTest.java
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -1,9 +1,13 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import racingcar.util.RandomDigitGenerator;
 import racingcar.util.RandomGenerator;
 
@@ -17,5 +21,36 @@ class PositionTest {
 
         //when then
         assertDoesNotThrow(() -> new Position(randomGenerator));
+    }
+
+    @Nested
+    @DisplayName("move 메서드를 실행시킬 때")
+    class whenMove {
+
+        @ParameterizedTest
+        @CsvSource({"0", "1", "2", "3"})
+        @DisplayName("랜덤 숫자가 4 미만인 경우 움직이지 않는다.")
+        void doesNotMovePositionWhenLessThanStandard(int randomNumber) {
+            //given
+            Position position = new Position(() -> randomNumber);
+            int expected = 0;
+            //when
+            position.move();
+            //then
+            assertThat(position.getPosition()).isEqualTo(expected);
+        }
+
+        @ParameterizedTest
+        @CsvSource({"4", "5", "6", "7", "8", "9"})
+        @DisplayName("랜덤 숫자가 4 이상인 경우 한칸 움직인다.")
+        void movePositionWhenEqualOrMoreThanStandard(int randomNumber) {
+            //given
+            Position position = new Position(() -> randomNumber);
+            int expected = 1;
+            //when
+            position.move();
+            //then
+            assertThat(position.getPosition()).isEqualTo(expected);
+        }
     }
 }
