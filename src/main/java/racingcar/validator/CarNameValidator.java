cbc722d288c14550f9e93895c4596b39770f2898
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 20:39:18 +0900

    feat: 자동차 입력값 패턴을 검증하는 기능을 추가한다

--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -1,10 +1,13 @@
 package racingcar.validator;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class CarNameValidator {
 
     private static final int NAME_MAX_LENGTH = 5;
+    //    private static final String CAR_LIST_PATTERN = "^([a-zA-Z0-9가-힣])(,[a-zA-Z0-9가-힣])*$";
+    private static final String CAR_LIST_PATTERN = "^[a-zA-Z0-9가-힣]+[a-zA-Z0-9가-힣" + "," + "]*[a-zA-Z0-9가-힣]";
 
     public void nameLength(List<String> carsList) {
         for (String car : carsList) {
@@ -16,6 +19,12 @@ public class CarNameValidator {
 
     public void nameOverlap(List<String> carsList) {
         if (carsList.size() != carsList.stream().distinct().count()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void rightNamePattern(String cars) {
+        if (!Pattern.compile(CAR_LIST_PATTERN).matcher(cars).matches()) {
             throw new IllegalArgumentException();
         }
     }
