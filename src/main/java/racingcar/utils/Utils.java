a7196e228fb311b3b2ca83e04955b065ab889af6
dbdb456 <adad568@naver.com>
2024-10-25 15:23:24 +0900

    feat(utils): 자동차 이름 배열을 Car 객체 리스트 저장 메서드 추가

--- src/main/java/racingcar/utils/Utils.java
+++ src/main/java/racingcar/utils/Utils.java
@@ -1,8 +1,17 @@
 package racingcar.utils;
 
+import java.util.List;
+import racingcar.domain.Car;
+
 public class Utils {
 
     public String[] convertToCarNameArray(String carNames) {
         return carNames.split(",");
+    }
+
+    public void saveCarNamesToList(String[] carNames, List<Car> cars) {
+        for (String carName : carNames) {
+            cars.add(new Car(carName, ""));
+        }
     }
 }
