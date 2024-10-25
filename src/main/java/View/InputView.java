6129b1ad01513dc1b5032a285462b7b38f523924
sinji <tobeabestns@naver.com>
2024-10-26 01:51:08 +0900

    feat(InputView): 입력 예외 처리(아무 것도 입력을 안한 경우

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -5,17 +5,29 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
 
     public static List<String> inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        final String carNames = Console.readLine();
-        return new ArrayList<>(Arrays.asList(carNames.split(",")));
+        final String carNames = Console.readLine().trim();
+
+        if (carNames.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름을 입력해야 합니다.");
+        }
+
+        return Arrays.stream(carNames.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 
     public static int inputAttemptNumber() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         return Integer.parseInt(Console.readLine());
     }
+
+    // 예외 처리: 입력된 숫자가 0 이하일 때
+    // 예외 처리: 숫자가 아닌 문자가 입력 되었을 때
+    // 예외 처리: 공백이 입력 되었을
 }
