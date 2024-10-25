2539fec1c00fa499f20dd1fb4756910324b36c1c
yujin-zero <poly30@naver.com>
2024-10-25 10:20:27 +0900

    feat(inputView) : 자동차 이동 횟수 입력 기능 구현
    inputView를 만들어 자동차 이동 횟수를 입력할 수 있도록 함.
    잘못된 입력에 대한 예외 처리는 포함하지 않음.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.controller.Controller;
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        Controller controller = new Controller(inputView);
+
+        controller.startGame();
     }
 }
---
+++ src/main/java/racingcar/controller/Controller.java
@@ -0,0 +1,15 @@
+package racingcar.controller;
+
+import racingcar.view.InputView;
+
+public class Controller {
+    private final InputView inputView;
+
+    public Controller(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void startGame() {
+    }
+
+}
