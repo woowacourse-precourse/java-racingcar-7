ed4b6ac4e51d025bbc46009bae6fba22d2f4309e
mingking2 <mingee641@gmail.com>
2024-10-25 17:11:32 +0900

    feat: 입력에 대한 상수값 추가

---
+++ src/main/java/racingcar/constant/InputConstant.java
@@ -0,0 +1,6 @@
+package racingcar.constant;
+
+public class InputConstant {
+    public static final String INPUT_CAR_NAME_VIEW = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_COUNT_VIEW = "시도할 횟수는 몇 회인가요?";
+}
