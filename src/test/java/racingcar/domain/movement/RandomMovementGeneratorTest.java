90a69f13a190940e98a341983c3c936e029ced03
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-25 19:11:02 +0900

    test: RandomMovementGeneratorTest 테스트 코드 작성

---
+++ src/test/java/racingcar/domain/movement/RandomMovementGeneratorTest.java
@@ -0,0 +1,34 @@
+package racingcar.domain.movement;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+class RandomMovementGeneratorTest {
+
+    @Test
+    @DisplayName("랜덤값이 4이상이면 움직인다")
+    void CanGoCase() {
+        //given
+        RandomMovement randomMovement = new RandomMovementGenerator(()->4);
+
+        //when
+        boolean result = randomMovement.canGo();
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("랜덤값이 3 이하면 안움직인다")
+    void CantGoCase() {
+        //given
+        RandomMovement randomMovement = new RandomMovementGenerator(()->3);
+
+        //when
+        boolean result = randomMovement.canGo();
+
+        //then
+        assertThat(result).isFalse();
+    }
+}
