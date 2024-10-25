89b8359a7a822f7eadc8326f3408757277b69a33
JunBe <wnsqowid0901@gmail.com>
2024-10-26 06:38:50 +0900

    fix: HashMap을 LinkedHashMap으로 변경
    HashMap 사용시 자동차 이름 입력 순으로 저장되지 않는 문제가 있어 입력 순서를 보장해 주는 LinkedHashMap으로 변경했다.

--- src/main/java/racingcar/model/GameModel.java
+++ src/main/java/racingcar/model/GameModel.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class GameModel {
     public Map<String, Integer> getCarMap(String[] carNames) {
-        Map<String, Integer> carMap = new HashMap<>();
+        Map<String, Integer> carMap = new LinkedHashMap<>();
         for (String carName : carNames) {
             if (carName.length() > 5) throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
             carMap.put(carName, 0);
