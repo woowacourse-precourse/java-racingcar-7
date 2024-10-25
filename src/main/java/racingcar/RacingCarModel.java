179b20f26ce3ccc983626373e9d68f2fdad3d92c
JEANS <songkc5671@naver.com>
2024-10-25 18:11:57 +0900

    feat(race) : 경주 기능 추가 및 리팩토링
    startRace로 경주를 시작하며, input값에 대한 검증을 시작합니다.
    getName으로 , 로 구분되어있던 이름을 분리하여 ArrayList<>에 저장합니다.
    실제 경주를 race함수를 통해 시작하며, 우승자를 sb에 한번에 저장합니다.
    들여쓰기 depth가 3이하로 리팩토링했습니다.

--- src/main/java/racingcar/RacingCarModel.java
+++ src/main/java/racingcar/RacingCarModel.java
@@ -11,21 +11,29 @@ public class RacingCarModel {
     static StringBuilder sb;
 
     public RacingResponse startRace(RacingRequest request) {
-        /* 객체, 이름, 숫자 존재여부 판단 */
-        if(!validateRacingInfo(request)) {
-            throw new IllegalArgumentException("입력값을 입력해주세요.");
+        /* 객체 판단 */
+        if(!validateRacingObject(request)) {
+            throw new IllegalArgumentException("객체값이 없습니다.");
         }
-        /* sb, 이름, 게임횟수 초기화 */
+        /* 이름 판단 */
+        if(!validateRacingNames(request)) {
+            throw new IllegalArgumentException("이름을 입력해주세요.");
+        }
+        /* 시도횟수 판단 */
+        if(!validateRacingTimes(request)) {
+            throw new IllegalArgumentException("시도횟수를 입력해주세요.");
+        }
         sb = new StringBuilder();
+        /* 이름, 게임횟수 초기화 */
         String name = request.name();
         int loop = request.times();
-
+        /* 이름 저장 */
         ArrayList<String> nameList = new ArrayList<>();
         getName(nameList, name);
-
+        /* 경주 시작 */
         int[] res = race(nameList, loop);
-        int max = 0;
         /* 우승자 찾기 */
+        int max = 0;
         for (int re : res) {
             max = Math.max(max, re);
         }
@@ -35,16 +43,13 @@ public class RacingCarModel {
         boolean isFirst = true;
         for(int i = 0; i < res.length; i++) {
             if(max == res[i]) {
-                if (!isFirst) {
-                    sb.append(", ");
-                }
+                addComma(isFirst);
                 sb.append(nameList.get(i));
                 isFirst = false;
             }
         }
-
-        System.out.println(sb);
-        return new RacingResponse();
+        /* 결괏값 반환 */
+        return new RacingResponse(sb.toString());
     }
 
     private int[] race(ArrayList<String> list, int loop) {
@@ -55,12 +60,8 @@ public class RacingCarModel {
         while(loop-->0) {
             /* idx == 이름 색인 */
             for(int idx = 0; idx < list.size(); idx++) {
-                /* 0-9 난수 생성 후 4 이상시 전진 */
-                int rNum = Randoms.pickNumberInRange(0, 9);
-                if(rNum >= 4) {
-                    path[idx]++;
-                }
-
+                /* 전진 */
+                forwardCar(path, idx);
                 sb.append(list.get(idx)).append(" ").append(":").append(" ");
                 sb.append("-".repeat(Math.max(0, path[idx])));
                 sb.append("\n");
@@ -69,6 +70,20 @@ public class RacingCarModel {
         }
 
         return path;
+    }
+
+    private void addComma(boolean flag) {
+        if (!flag) {
+            sb.append(", ");
+        }
+    }
+
+    private void forwardCar(int[] path, int idx) {
+        /* 0-9 난수 생성 후 4 이상시 전진 */
+        int rNum = Randoms.pickNumberInRange(0, 9);
+        if(rNum >= 4) {
+            path[idx]++;
+        }
     }
 
     private void getName(ArrayList<String> list, String s) {
@@ -91,8 +106,18 @@ public class RacingCarModel {
         return name.length() <= 5;
     }
 
-    private boolean validateRacingInfo(RacingRequest request) {
-        /* 객체, 이름, 숫자 존재여부 판단 */
-        return request != null && !request.name().isEmpty() && request.times() != 0;
+    private boolean validateRacingObject(RacingRequest request) {
+        /* 객체 유무 판단 */
+        return request != null;
+    }
+
+    private boolean validateRacingNames(RacingRequest request) {
+        /* 이름 유무 판단 */
+        return !request.name().isBlank();
+    }
+
+    private boolean validateRacingTimes(RacingRequest request) {
+        /* 횟수 유무 판단 */
+        return request.times() >= 0;
     }
 }
