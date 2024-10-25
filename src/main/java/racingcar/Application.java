e3daa6b4865a52fb4a6d7a739e536ab01d43aa60
dbdb456 <adad568@naver.com>
2024-10-25 16:35:31 +0900

    feat(application): contoller 초기화 및 시작 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.controller.Controller;
+import racingcar.utils.Utils;
+import racingcar.validator.Validator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller game = new Controller(new Validator(), new Utils(), new InputView(), new OutputView());
+        game.startRace();
     }
 }
