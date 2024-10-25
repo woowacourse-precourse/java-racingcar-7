9a3a366c719187273000c56c4b68552190855f06
daeuun <leeda51925@gmail.com>
2024-10-25 20:58:00 +0900

    refactor: 사용하지 않는 예외 처리 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,7 +3,7 @@ package racingcar;
 import racingcar.controller.RacingGameController;
 
 public class Application {
-    public static void main(String[] args) throws Throwable {
+    public static void main(String[] args) {
         RacingGameFactory racingGameFactory = new RacingGameFactory();
         RacingGameController controller = racingGameFactory.initializeController();
         controller.startGame();
