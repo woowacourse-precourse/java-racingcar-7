9b477f125370b59dafcebd93a789a5f6e3adc430
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-25 11:42:51 +0900

    fix(Validator): 테스트 중 발견된 예외 메시지 수정
    - 테스트 메서드 리팩토링 중 누락된 예외 메시지를 수정하여 발생한 문제 해결

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -3,13 +3,17 @@ package racingcar;
 public class Validator {
 
     public void validateInput(UserInput userInput) {
-        validateCars(userInput.carNames());
+        validateCarNames(userInput.carNames());
         validateAttemptCount(userInput.attemptCount());
     }
 
-    private void validateCars(String carNamesInput) {
+    private void validateCarNames(String carNamesInput) {
         if (carNamesInput.isBlank()) {
             throw new IllegalArgumentException("자동차 이름은 1개 이상 입력해야 합니다.");
+        }
+
+        if (carNamesInput.contains(" ")) {
+            throw new IllegalArgumentException("자동차 이름에 공백은 입력할 수 없습니다");
         }
     }
 
@@ -17,7 +21,7 @@ public class Validator {
         try {
             int attempts = Integer.parseInt(attemptCountInput);
             if (attempts < 1) {
-                throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+                throw new IllegalArgumentException("시도 횟수는 1 이상 입력해야 합니다.");
             }
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("시도 횟수는 숫자로 입력해야 합니다.");
