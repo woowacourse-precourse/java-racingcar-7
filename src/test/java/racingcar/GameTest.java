81c30ff59c4422981c49f833324c7b73aa696d77
Hyungjung Kim <gudwnd1113@gmail.com>
2024-10-25 18:42:48 +0900

    test(GameTest): build Game.generateRacingCarList-related test code
    Game 클래스의 static 함수 generateRacingCarList의 입력값에 따른 동작을
    검증하는 테스트 코드 작성.

---
+++ src/test/java/racingcar/GameTest.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+    @Test
+    void Game_generateRacingCarList_test() {
+        List<String> carNameList = Arrays.asList("pobi", "woni");
+        List<RacingCar> carInstanceList = Game.generateRacingCarList(carNameList);
+
+        RacingCar pobi = carInstanceList.get(0);
+        RacingCar woni = carInstanceList.get(1);
+
+        assertThat(pobi.getName()).isEqualTo("pobi");
+        assertThat(pobi.getCount()).isEqualTo(0);
+        assertThat(woni.getName()).isEqualTo("woni");
+        assertThat(woni.getCount()).isEqualTo(0);
+    }
+
+    @Test
+    void Game_generateRacingCarList_exception_test() {
+        List<String> carNameList = Arrays.asList("pobi", "javaji");
+        assertThatThrownBy(() -> Game.generateRacingCarList(carNameList))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
