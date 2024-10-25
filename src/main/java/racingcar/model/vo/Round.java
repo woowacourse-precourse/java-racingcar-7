1b7aec99a059c7e70ac878b4d00c9eeea88b955b
FhRh <yiyop@naver.com>
2024-10-25 16:13:37 +0900

    refactor(Round) : BigInteger를 사용하여 입력값 범위 파악
    - 기존 코드에서 입력값이 int범위를 넘어서면 비교도 하기전에 NumberFormatException이 발생합니다. 해당 문제를 BigInteger를 사용하여 좀 더 명시적으로 해결하였습니다.
    - 기존 예외 메시지를 보다 구체적으로 수정하였습니다.

--- src/main/java/racingcar/model/vo/Round.java
+++ src/main/java/racingcar/model/vo/Round.java
@@ -1,5 +1,7 @@
 package racingcar.model.vo;
 
+import java.math.BigInteger;
+
 public record Round(int round) {
     private static final int MAX_ROUND = Integer.MAX_VALUE;
 
@@ -11,13 +13,15 @@ public record Round(int round) {
 
     private static void validatePostiveNumber(String round) {
         if (!round.matches("^[1-9]\\d*$")) {
-            throw new IllegalArgumentException("라운드는 양수여야 합니다.");
+            throw new IllegalArgumentException("라운드의 입력값은 숫자로만 이루어진 양수여야 합니다.");
         }
     }
 
     private static void validateExceedMaxRound(String round) {
-        if (Integer.parseInt(round) > MAX_ROUND) {
-            throw new IllegalArgumentException("라운드는 100을 초과할 수 없습니다.");
+        BigInteger roundNum = new BigInteger(round);
+        BigInteger maxRound = new BigInteger(String.valueOf(MAX_ROUND));
+        if(roundNum.compareTo(maxRound) > 0) {
+            throw new IllegalArgumentException("라운드의 입력값은 2147483647을 초과할 수 없습니다.");
         }
     }
 }
