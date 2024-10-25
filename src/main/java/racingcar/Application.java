352f1e31ea16708be426f5a237a1decf0ab6ea59
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-10-25 15:01:48 +0900

    feat(Application): 예외처리 기능추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,12 +7,14 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         String[] names = input.split(",");
-        //예외처리 1 추가예정 :이름 5자 초과
+
+        Exception exception = new Exception();
+        exception.except1(names);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String count_s = Console.readLine();
         int count = Integer.parseInt(count_s);
-        //예외처리 2 추가예정: 횟수가 1회미만, 음수일경우
+        exception.except2(count);
         //우승자 출력 추가예정
     }
 }
