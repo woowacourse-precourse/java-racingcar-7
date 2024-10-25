e900813833bc434b962a884ead121afa43ccff74
unh6unh6 <ruddbs3308@naver.com>
2024-10-25 17:08:48 +0900

    feat(Car): 자동차 이름 5자 이하인지 검사

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,9 +1,18 @@
 package racingcar.model;
 
 public class Car {
+    private static final String CAR_NAME_LENGTH_EXCEPTION = "자동차 이름은 5자 이하이어야 합니다.";
+
     private final String name;
 
     public Car(String name) {
+        validateCarNameLength(name);
         this.name = name;
+    }
+
+    private void validateCarNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_EXCEPTION);
+        }
     }
 }
