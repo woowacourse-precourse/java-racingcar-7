431a7cf0f6f0a1f16fb418fd47853a302d321444
jieun <ju3391@naver.com>
2024-10-25 21:32:31 +0900

    feat(AttemptCountException): 시도 횟수 예외 처리
    - 시도 횟수 예외 처리 클래스 추가

--- src/main/java/racingcar/race/exception/AttemptCountException.java
+++ src/main/java/racingcar/race/exception/AttemptCountException.java
@@ -1,7 +1,5 @@
 package racingcar.race.exception;
 
-import racingcar.common.ExceptionEnum;
-
 // 시도 횟수 관련 예외 처리 클래스
 public class AttemptCountException extends IllegalArgumentException {
     public AttemptCountException(String message) {
