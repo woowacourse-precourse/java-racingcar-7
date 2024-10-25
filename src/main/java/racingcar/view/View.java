11bf47f7c17ffa92ea60cecd79045737ea9a53e3
wogus <ind07152@naver.com>
2024-10-25 12:41:03 +0900

    feat(View): 최종 결과 메시지 출력 기능 추가
    모든 시도를 마친 후 최종 결과에 대해 출력한다.

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.model.Car;
 import racingcar.utils.Parser;
 import racingcar.validator.Validator;
@@ -10,16 +11,18 @@ public final class View {
     private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String FINAL_RESULT_MESSAGE = "최종 우승자 : ";
 
     public static List<String> getCarNames() {
         System.out.println(NAME_INPUT_MESSAGE);
         return Parser.parse(Console.readLine());
     }
 
-    public static Integer getCount() {
+    public static int getCount() {
         System.out.println(COUNT_INPUT_MESSAGE);
         String count = Console.readLine();
         Validator.countValidate(count);
+        System.out.println();
         return Integer.parseInt(count);
     }
 
@@ -28,5 +31,12 @@ public final class View {
         cars.forEach(car -> {
             System.out.println(car.getName() + " : " + "-".repeat(car.getLocation()));
         });
+        System.out.println();
+    }
+
+    public static void showFinalResult(List<Car> cars) {
+        System.out.print(FINAL_RESULT_MESSAGE);
+        String winners = cars.stream().map(Car::getName).collect(Collectors.joining(","));
+        System.out.println(winners);
     }
 }
