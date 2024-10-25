1973218d56460d213c11dba7ac10f54e3a3ce5b1
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-10-26 00:46:28 +0900

    feat: raceRun, turnRun함수 구현
    - Race클래스에서 레이스 시작 함수, 레이스의 turn 시작 함수 구현

--- src/main/java/racingcar/model/race/Race.java
+++ src/main/java/racingcar/model/race/Race.java
@@ -3,18 +3,32 @@ package racingcar.model.race;
 import java.util.List;
 import racingcar.model.car.Car;
 import racingcar.model.car.InitCars;
+import racingcar.model.random.Random;
 
 public class Race {
     List<Car> carList;
-    public final int attemptCountNumber;
-
+    public int attemptCountNumber;
 
     public Race(String[] carNameArray, int attemptCountNumber) {
         carList = InitCars.initCars(carNameArray);
         this.attemptCountNumber = attemptCountNumber;
     }
 
-    public void raceRun() {
+    public final String turnRun() {
+        StringBuilder turnResult = new StringBuilder();
+        for (Car car : carList) {
+            Random.goRandomForward(car);
+            turnResult.append(car.toString()).append("\n");
+        }
+        return turnResult.toString();
+    }
 
+    public final String raceRun() {
+        StringBuilder raceResult = new StringBuilder();
+        while (attemptCountNumber-- > 0) {
+            String turnResult = turnRun();
+            raceResult.append(turnResult).append("\n");
+        }
+        return raceResult.toString();
     }
 }
