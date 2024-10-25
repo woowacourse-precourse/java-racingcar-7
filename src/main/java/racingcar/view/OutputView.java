9d5d8a8ce1066c9647cc22c5f650d2112fc04984
조수아 <suacho0724@gmail.com>
2024-10-25 14:15:46 +0900

    feat: OutPutView 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,8 @@
+package racingcar.view;
+
+public class OutputView {
+    public static void print(String prompt, String result) {
+        System.out.print(prompt);
+        System.out.print(result);
+    }
+}
