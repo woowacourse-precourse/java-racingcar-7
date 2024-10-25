eab121a49d5ce9f3bbbcb6058b8893ee61b02058
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-10-25 17:32:25 +0900

    feat: 입력 받은 빈 문자열 예외 발생 함수, 메시지 추가
    - 입력받은 문자열이 null이거나 blank일때의 예외 발생 함수, 메시지 추가

--- src/main/java/racingcar/message/ErrorMessage.java
+++ src/main/java/racingcar/message/ErrorMessage.java
@@ -4,4 +4,5 @@ public class ErrorMessage {
     public static final String CAR_NAME_LENGTH_EXCEED = "자동차 이름은 5자를 초과할 수 없습니다";
     public static final String ATTEMPT_NOT_NUMBER = "시도할 횟수 입력값이 숫자가 아닙니다";
     public static final String NEGATIVE_ATTEMPT_COUNT = "시도할 횟수 입력값이 음수입니다";
+    public static final String EMPTY_NULL_STRING = "null이거나 비어있는 문자열입니다";
 }
