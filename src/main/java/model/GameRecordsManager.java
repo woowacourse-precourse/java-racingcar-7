13d42f7e4e9430268193cf706db08843f5e8a5ee
shin-chaeeun <chaeeun0217@naver.com>
2024-10-25 21:41:54 +0900

    Refactor(GameRecordsManager):Extract GameRecordsManager from main class

---
+++ src/main/java/model/GameRecordsManager.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.List;
+import java.util.Map;
+
+public class GameRecordsManager {
+  public Map<String, String> carGameRecords;
+
+  public void createCarGameRecords(List<String> cars) {
+
+    // 차 이름이 5자를 초과하는지 검증
+    for (String carName : cars) {
+      if (carName.length() > 5) {
+        throw new IllegalArgumentException("차 이름이 5자를 초과했습니다: " + carName);
+      }
+      this.carGameRecords.put(carName, "");  // 초기 값은 빈 문자열_전진을 안했으니까
+    }
+  }
+}
