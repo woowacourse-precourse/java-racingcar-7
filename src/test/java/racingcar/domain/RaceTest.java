9cbcc12f3e37d98283c3960c8f9e8b443a60b21e
bowook <mbosx316@naver.com>
2024-10-25 20:10:13 +0900

    test(RaceTest) : Race 도메인 테스트
    - 최대 이동 거리 변경 테스트

--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -1,2 +1,32 @@
-package racingcar.domain;public class RaceTest {
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.validator.CarNameValidator;
+import racingcar.domain.validator.TrialCountsValidator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RaceTest {
+
+    @Test
+    void 최대_이동_거리_변경_테스트() {
+        //given
+        CarNameValidator carNameValidator = new CarNameValidator();
+        TrialCountsValidator trialCountsValidator = new TrialCountsValidator();
+        Cars cars = new Cars("bo,wook,hi", carNameValidator);
+        TrialCounts trialCounts = new TrialCounts("5", trialCountsValidator);
+        Race race = new Race(cars, trialCounts);
+
+        //when
+        for (int i = 0; i < trialCounts.getTrialCounts(); i ++) {
+            race.raceOneStep();
+        }
+        assertThat(race.isRaceOngoing()).isFalse();
+
+        //then
+        int maxPosition = race.getMaxPosition();
+        assertThat(maxPosition).isGreaterThanOrEqualTo(0);
+
+    }
+
 }
