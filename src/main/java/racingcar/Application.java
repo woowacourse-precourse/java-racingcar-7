d0f70d73f59dd1f8961147a0e5c0b6edfd6b4b12
HyeonJeong <wgwjh05169@gmail.com>
2024-10-25 21:16:27 +0900

    feat(Application): 입력 메소드 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
+
+    private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String NUMBER_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Writer.writeMessage(NAME_INPUT_MESSAGE);
+        List<String> carNames = Reader.readCarNames();
+
+        Writer.writeMessage(NUMBER_INPUT_MESSAGE);
+        int tryNumber = Reader.readNumber();
+
     }
 }
