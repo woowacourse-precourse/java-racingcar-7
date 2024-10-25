649d47daa15f68088608cdd6f217bec3819288aa
superbigwave <zlzhznzk@gmail.com>
2024-10-25 15:27:04 +0900

    [주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다.]
    각 자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
    - 이름을 입력 받을 수 있는 기능 추가
    - 입력한 값의 공백 필터링
    - 쉼표(,)로 구분 안한 입력값 오류 출력
     
    자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
    - 쉼표(,)를 기준으로 이름을 분류하고 배열값으로 변환
    - 마지막 이름 뒤에는 쉼표(,) 안붙임
    - 이름이 5글자를 넘어가면 오류 출력
    - 출력을 위해 이름 뒤에 콜론(:) 붙임
    
    사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.
    - 시도할 횟수 입력받음
    - 받은 값을 정수로 변환 후 양수 여부 점검 -> 예외일시 오류출력
    
    전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
    - 들여쓰기 3이 넘지 않기 위해 메소드로 생성
    - randomTry를 사용하여 0-9 무작위값 추출
    - 4이상이면 “-“ 추가기능
    - 값 누적시키기
    - 입력받은 시도 횟수 만큼 메소드 실행
    
    자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
    - 배열의 인덱스별 “-“를 포함한 수량 확인
    - 가장 큰 “-“ 수량 확인
    - 가장 큰 “-“ 수량이랑 일치하는 인덱스 추출
    
    우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.
    - 추출된 인덱스가 1이상이면 콤마(, )붙이는 기능 추가
    
    사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.
    - 이름입력값 및 시도횟수 입력값에 예외 추가하여 오류 발생 코드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,104 @@
 package racingcar;
+import java.util.Scanner;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner testInput = new Scanner(System.in); // 스캐너 기능 추가
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"); // 질문 출력
+        String carName = (testInput.nextLine()).replaceAll("\\s+", ""); // 콘솔 입력 및 띄어쓰기 제거
+        if (!carName.contains(",")) {   // 쉼표(,)로 구분 안할시 오류 메세지후 종료
+            throw new IllegalArgumentException();
+        }
+        String[] listOfCar = carName.split(","); // "," 기준 나누기
+
+        for (int i = 0; i < listOfCar.length; i++) { // 배열 다시하기, 이름이 5글자 넘어가는지 확인
+            if (i == listOfCar.length - 1 && listOfCar[i].length() <= 5) { // 마지막 글자는 ","안붙임
+                System.out.println(listOfCar[i]);
+            } else if  (i != listOfCar.length - 1 && listOfCar[i].length() <= 5)  {
+                System.out.print(listOfCar[i] + ",");
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+        for (int j = 0; j < listOfCar.length; j++) {    // 출력을 위해 " : " 추가
+            listOfCar[j] += " : ";
+        }
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String strRound = testInput.nextLine(); // 콘솔 입력란 추가 및 이동회수 입력
+        int round = Integer.parseInt(strRound); // 문자열 정수로 변경
+        if (round <= 0) { // 양수가 아닐때 오류 출력 후 종료
+            throw new IllegalArgumentException();
+        }
+
+        System.out.println();
+        System.out.println("실행 결과");
+        for (int f = 0; f < round; f++) {
+            moveCars(listOfCar);
+        }
+
+        // "-" 개수 카운트한 배열 만들기
+        int[] eachDashCounts = new int[listOfCar.length];
+        for (int k = 0; k < listOfCar.length; k++) {
+            eachDashCounts[k] = countDashes(listOfCar[k]); // 각 자동차의 "-" 개수를 카운트
+        }
+
+        // 제일 많은 카운트 수 확인
+        int max = 0;
+        for (int count : eachDashCounts) {  // 배열별로 카운트를 반복하여 확인
+            if (count > max) {
+                max = count; // 제일 높은 카운트 기록
+            }
+        }
+
+        // 제일 많은 카운트 수 = 인덱스 일치
+        StringBuilder winnerCar = new StringBuilder();
+        int winnerCarCount = 0;
+        for (int l = 0; l < listOfCar.length; l++) {
+            if (eachDashCounts[l] == max && winnerCarCount == 0) {
+                winnerCar.append(onlyCarName(listOfCar[l]));
+                winnerCarCount++;
+            }
+            else if (eachDashCounts[l] == max && winnerCarCount > 0) {
+                winnerCar.append(", ").append(onlyCarName(listOfCar[l]));
+            }
+
+        }
+
+        System.out.println("최종 우승자 : " + winnerCar.toString());
+    }
+
+
+    // 랜덤숫자로 전진하는 메소드
+    private static void moveCars (String[]listOfCar) {
+        for (int m = 0; m < listOfCar.length; m++) {
+            int randomTry = Randoms.pickNumberInRange(0, 9);
+            if (randomTry >= 4) {
+                System.out.println(listOfCar[m] += "-");
+            }
+            else {
+                System.out.println(listOfCar[m]);
+            }
+        }
+        System.out.println();
+    }
+
+    // "-"를 찾을 수 있는 메소드
+    private static int countDashes(String index) {
+        int count = 0; // "-" 개수 초기화
+        for (char dash : index.toCharArray()) { // 문자열을 문자로 분해해서 배열
+            if (dash == '-') {
+                count++; // "-" 발견 시 카운트 증가
+            }
+        }
+        return count; // 카운트 반환
+    }
+
+    // 마지막 배열 인덱스에서 "-"를 제외한 자동차 이름 메소드
+    private static String onlyCarName(String listOfCar) {
+        return listOfCar.substring(0,listOfCar.indexOf(" : "));
     }
 }
