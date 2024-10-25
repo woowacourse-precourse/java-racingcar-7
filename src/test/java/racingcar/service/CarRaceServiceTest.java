458eb3f5cd25cd339498f29e2bf6b3b92b612711
Jo Myeong-geun <happy63590@gmail.com>
2024-10-26 04:41:31 +0900

    test: 자동차 전진/정지 기능 테스트 추가

--- src/test/java/racingcar/service/CarRaceServiceTest.java
+++ src/test/java/racingcar/service/CarRaceServiceTest.java
@@ -2,10 +2,16 @@ package racingcar.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static racingcar.util.RandomUtil.getRandomNumberInRange;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
 
 class CarRaceServiceTest {
 
@@ -24,5 +30,70 @@ class CarRaceServiceTest {
         assertEquals(2L, carNames.size());
         assertTrue(carNames.contains("foo"));
         assertTrue(carNames.contains("bar"));
+    }
+
+    @Test
+    @DisplayName("자동차가 전진하면 이동거리에 '-'를 추가한다.")
+    void should_MoveCars_When_RandomNumberAboveThreshold() {
+        //given
+        int movingNumber = getRandomNumberInRange(4, 9);
+
+        CarRaceService mockCarRaceService = getCarRaceService(movingNumber);
+        mockCarRaceService.init("foo,bar");
+
+        //when
+        mockCarRaceService.moveCars();
+
+        //then
+        Map<String, String> carsStatus = mockCarRaceService.getCarsStatus();
+        assertEquals("-", carsStatus.get("foo"));
+        assertEquals("-", carsStatus.get("bar"));
+    }
+
+    @Test
+    @DisplayName("자동차가 정지하면 이동거리는 변하지 않는다.")
+    void should_NotMoveCars_When_RandomNumberBelowThreshold() {
+        //given
+        int stoppingNumber = getRandomNumberInRange(0, 3);
+
+        CarRaceService mockCarRaceService = getCarRaceService(stoppingNumber);
+        mockCarRaceService.init("foo,bar");
+
+        //when
+        mockCarRaceService.moveCars();
+
+        //then
+        Map<String, String> carsStatus = mockCarRaceService.getCarsStatus();
+        assertEquals("", carsStatus.get("foo"));
+        assertEquals("", carsStatus.get("bar"));
+    }
+
+    private CarRaceService getCarRaceService(int movingNumber) {
+        return new CarRaceService() {
+            List<Car> cars;
+
+            @Override
+            public void init(String input) {
+                this.cars = Arrays.stream(input.split(","))
+                        .map(Car::new)
+                        .toList();
+            }
+
+            @Override
+            public void moveCars() {
+                cars.stream()
+                        .filter(car -> movingNumber >= 4)
+                        .forEach(Car::move);
+            }
+
+            @Override
+            public Map<String, String> getCarsStatus() {
+                return cars.stream()
+                        .collect(Collectors.toMap(
+                                Car::getName,
+                                Car::getMovingDistance
+                        ));
+            }
+        };
     }
 }
