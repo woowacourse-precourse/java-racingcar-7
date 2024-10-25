54afff55748f811de4ae972e3b34e57fd3fe8a7b
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-26 01:31:26 +0900

    refactor($DefaultRacingCarValidator): 이름이 null 인 경우도 검증하도록 변경

--- src/main/java/racingcar/model/dependency/validator/DefaultRacingCarValidator.java
+++ src/main/java/racingcar/model/dependency/validator/DefaultRacingCarValidator.java
@@ -6,6 +6,9 @@ public class DefaultRacingCarValidator implements RacingCarValidator {
 
     @Override
     public void validateName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("이름은 null 이 될 수 없습니다.");
+        }
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("이름은 " + MAX_NAME_LENGTH + "자 이하여야 합니다.");
         }
