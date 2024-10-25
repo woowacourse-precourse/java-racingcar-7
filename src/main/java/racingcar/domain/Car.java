63cc674226f351e72d0748cd1e05dc0d95f3ffd0
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 21:32:18 +0900

    feat: 자동차 생성 기능을 추가한다

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,33 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+
+    private final String name;
+    private int position = 0;
+
+    public static final int GO_NUMBER = 4;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void go() {
+        if (canMove()) {
+            position++;
+        }
+    }
+
+    private boolean canMove() {
+        return Randoms.pickNumberInRange(0, 9) >= GO_NUMBER;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
