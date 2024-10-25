c6118ca7268e35257bfb2105b64c87c5651d9dbd
jhan0121 <jhan0121@gmail.com>
2024-10-25 21:52:42 +0900

    refactor(player): Car 객체 기반 메서드 리팩터링 진행

--- src/main/java/racingcar/service/PlayerService.java
+++ src/main/java/racingcar/service/PlayerService.java
@@ -1,6 +1,8 @@
 package racingcar.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import racingcar.model.Car;
 
 public class PlayerService {
 
@@ -8,5 +10,13 @@ public class PlayerService {
         List<String> players = List.of(input.split(","));
         NameValidator.validatePlayerName(players);
         return players;
+    }
+
+    public List<Car> createCarList(List<String> name) {
+        List<Car> cars = new ArrayList<>();
+        for (String s : name) {
+            cars.add(new Car(s));
+        }
+        return cars;
     }
 }
