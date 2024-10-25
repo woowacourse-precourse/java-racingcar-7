ab39c2d1e4be060b6f3579c70873fcb19fd0b82c
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-10-25 16:28:13 +0900

    refactor: 필요없는 패키지 삭제

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -2,7 +2,6 @@ package racingcar.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class RacingCars {
     List<Car> cars = new ArrayList<>();
