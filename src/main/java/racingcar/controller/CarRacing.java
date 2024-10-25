48259dc58515d775ede1e1bf78044c42628cd717
pakaheo <k70923@konkuk.ac.kr>
2024-10-25 12:14:20 +0900

    feat : Create CarRacing
    CarRacing control racing game by race method

--- src/main/java/racingcar/controller/CarRacing.java
+++ src/main/java/racingcar/controller/CarRacing.java
@@ -1,5 +1,23 @@
 package racingcar.controller;
 
-public class RacingGame {
+import java.util.List;
+import racingcar.domain.CarFactory;
+import racingcar.domain.Cars;
+import racingcar.view.InputView;
+import racingcar.view.ResultView;
 
+public class CarRacing {
+    public void race() {
+        String carNames = InputView.inputCarNames();
+        Cars cars = CarFactory.createCars(carNames);
+
+        int tryCount = InputView.inputTryCount();
+
+        for (int count = 1; count <= tryCount; count++) {
+            System.out.println(cars.decideMoving());
+        }
+
+        List<String> winners = cars.findWinners();
+        ResultView.showWinners(winners);
+    }
 }
