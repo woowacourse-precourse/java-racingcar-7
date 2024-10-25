b9341a655911b41c7298b811c02af1d9474df79f
Chan531 <chan657312@gmail.com>
2024-10-25 16:54:28 +0900

    feat(WinnerDeterminer): 우승자 이름 리스트 추출
    자동차 이동 최대값과 일치하는 자동차 이름 리스트 추출

--- src/main/java/racingcar/model/WinnerDeterminer.java
+++ src/main/java/racingcar/model/WinnerDeterminer.java
@@ -1,6 +1,15 @@
 package racingcar.model;
 
+import java.util.List;
+
 public class WinnerDeterminer {
 
 	private static final int MIN_MOVEMENT_VALUE = 0;
+
+	private static List<String> getWinner(List<Car> cars, int maxMovementValue) {
+		return cars.stream()
+				.filter(car -> car.getMovement() == maxMovementValue)
+				.map(Car::getName)
+				.toList();
+	}
 }
