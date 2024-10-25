1a2270cbbbffb06e6ca201839bc578091fe87e3c
Dev-LHY <dev.leehy@gmail.com>
2024-10-26 06:22:38 +0900

    refactor(exceptionMessage): 중복 메세지 삭제 및 메세지 재구성

--- src/main/java/racingcar/exception/ExceptionMessage.java
+++ src/main/java/racingcar/exception/ExceptionMessage.java
@@ -7,8 +7,7 @@ public enum ExceptionMessage implements Message {
     EXCEED_MAX_INPUT_LENGTH(INVALID_INPUT.getMessage() + " [자동차 이름은 5글자를 초과할 수 없습니다.]"),
     INPUT_BLANK_IS_NOT_ALLOWED(INVALID_INPUT.getMessage() + " [공백은 입력이 불가능합니다.]"),
     INPUT_AT_LEAST_TWO_CAR(INVALID_INPUT.getMessage() + "[2개 이상의 자동차를 등록해주세요.]"),
-    INPUT_ONLY_INTEGER(INVALID_INPUT.getMessage() + "[정수만 입력 가능합니다.]"),
-    INPUT_POSITIVE_INTEGER(INVALID_INPUT.getMessage() + " [1 이상의 정수를 입력해주세요.]"),
+    INPUT_ONLY_POSITIVE_INTEGER(INVALID_INPUT.getMessage() + "[1 이상의 정수만 입력 가능합니다.]"),
     DUPLICATE_INPUT(INVALID_INPUT.getMessage() + " [중복 입력은 불가능합니다.]");
 
     private final String message;
