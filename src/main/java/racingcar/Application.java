1509df15098bff8b18faab9c75a31d060407dfad
nodonghui <163218508+nodonghui@users.noreply.github.com>
2024-10-25 17:08:17 +0900

    docs: README.md 수정
    프로세스와 기능 구현 항목을 수정했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -41,6 +41,13 @@ public class Application {
     }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Input();
 
+        try {
+            String[] RaceMember=ParseInput();
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            throw e;
+        }
     }
 }
