e54ce861fa895bc78ce4b2f84cd4b5725e8bac99
khyunse0 <rjdls2549@donga.ac.kr>
2024-10-25 20:48:35 +0900

    자동차 이름, 시도할 횟수 입력 기능과 무작위 값 생성 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,36 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 자동차 이름, 시도할 횟수 입력
+        Car car = new Car();
+        car.input();
     }
 }
 
+class Car {
+    public String[] carLst;
+    public int time;
+    public int[] numLst;
+
+    public void input(){
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String cars = Console.readLine();
+        this.carLst = cars.split(",");
+        System.out.print("시도할 횟수는 몇 회인가요?");
+        try {
+            this.time = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("IllegalArgumentException");
+        }
+    }
+
+    private void giveRandomNumbers(){
+        for (int i = 0; i < carLst.length; i++){
+            numLst[i] = Randoms.pickNumberInRange(0, 9);
+        }
+    }
+}
