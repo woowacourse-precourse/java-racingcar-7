985dc8908e7dc4a3020e623a2bb9d048fc59cab0
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-26 02:24:33 +0900

    refactor($Application): RacingController 로의 클래스명 변경에 따른 코드 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,13 @@
 package racingcar;
 
-import racingcar.controller.RacingCarController;
+import racingcar.controller.RacingController;
 import racingcar.di.DefaultRacingCarTemplate;
 import racingcar.di.RacingCarTemplate;
 
 public class Application {
 
     private static final RacingCarTemplate racingCarTemplate = new DefaultRacingCarTemplate();
-    private static final RacingCarController racingCarController = new RacingCarController(racingCarTemplate);
+    private static final RacingController racingCarController = new RacingController(racingCarTemplate);
 
     public static void main(String[] args) {
         racingCarController.run();
