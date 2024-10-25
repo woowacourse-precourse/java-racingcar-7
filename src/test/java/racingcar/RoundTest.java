b9559cf225c90dd182a6442cfa1a8592a77c865f
NamJihyun99 <namjihyun9936@gmail.com>
2024-10-25 18:42:44 +0900

    test:>Round.play() 작동 테스트

---
+++ src/test/java/racingcar/RoundTest.java
@@ -0,0 +1,38 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class RoundTest {
+
+    @Test
+    @DisplayName("car목록과 각 car의 number를 입력하면 한 번 전진하거나 정지")
+    void play_success() {
+        Car car1 = new Car("AAA");
+        Car car2 = new Car("BBB");
+        List<Integer> numbers = List.of(4, 3);
+        Round.play(List.of(car1, car2), numbers);
+        assertThat(car1.getDistance()).isEqualTo(1);
+        assertThat(car2.getDistance()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("car목록과 각 car의 number목록의 크기가 일치하지 않으면 예외")
+    void play_numbersSizeMismatch() {
+        Car car1 = new Car("AAA");
+        Car car2 = new Car("BBB");
+        List<Integer> numbers = List.of(4, 3, 5);
+        assertThatThrownBy(() -> Round.play(List.of(car1, car2), numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 컬렉션의 크기와 난수 컬렉션의 크기가 일치하지 않습니다.");
+    }
+
+    @Test
+    void roundResult() {
+    }
+}
