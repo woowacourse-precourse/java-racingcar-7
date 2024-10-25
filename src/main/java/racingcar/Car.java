f3d5f793fcac2d7bc77be77d8c1c43d38cb6242d
kjyyjk <rlawnsdud920@gmail.com>
2024-10-25 20:59:11 +0900

    feat: 자동차의 이름이 알파벳 또는 숫자가 아닌 경우 예외를 발생한다
    - 메서드가 한가지 일만 하도록 Car 생성자 내 길이 검증과 문자 검증 로직을 각각의 메서드로 분리한다.
    
    - 문자 검증은 들여쓰기 depth를 준수하기 위해 다음 두 메서드에 나누어서 수행한다.
    validateNameOnlyAlphaAndNumber(): 입력 받은 이름을 구성하는 각 문자를 아래 메서드에 넘긴다.
    validateCharOnlyAlphaAndNumber(): 입력 받은 문자가 영문자 또는 숫자가 아니면 예외를 발생한다.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,10 +4,30 @@ public class Car {
     private final String name;
 
     public Car(final String name) {
+        validateNameLength(name);
+        validateNameOnlyAlphaAndNumber(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(final String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException();
         }
+    }
 
-        this.name = name;
+    private void validateNameOnlyAlphaAndNumber(final String name) {
+        char[] chars = name.toCharArray();
+
+        for (char c : chars) {
+            validateCharOnlyAlphaAndNumber(c);
+        }
+    }
+
+    private void validateCharOnlyAlphaAndNumber(final char c) {
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (Character.isDigit(c))) {
+            return;
+        }
+
+        throw new IllegalArgumentException();
     }
 }
