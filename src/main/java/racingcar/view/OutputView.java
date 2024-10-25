81fd218a5d5630bcffe83676bbd8eabbe6753c6d
Chaewon <corndots@naver.com>
2024-10-26 05:01:13 +0900

    feat(OutputView): 우승자 리스트 출력
    스트림 API의 Collectors.joining(", ")을 사용해 각 우승자의 이름을 쉼표와 공백으로 연결

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,6 +3,7 @@ package racingcar.view;
 import racingcar.domain.Car;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputView {
     public static void printRoundResult(List<Car> cars) {
@@ -14,5 +15,13 @@ public class OutputView {
 
     public static void printExecutionResultMessage() {
         System.out.println("실행 결과");
+    }
+
+    // 우승자 출력 메서드
+    public static void printWinners(List<Car> winners) {
+        String winnerNames = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println("최종 우승자 : " + winnerNames);
     }
 }
