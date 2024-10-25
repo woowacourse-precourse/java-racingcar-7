ec7bd06ca0f384e8a5b1efec5791e073a7a7f838
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 15:39:41 +0900

    feat: 입력값의 패턴이 적합한지 확인하는 기능을 추가한다

--- src/main/java/racingcar/validator/NameValidiator.java
+++ src/main/java/racingcar/validator/NameValidiator.java
@@ -1,5 +1,15 @@
 package racingcar.validator;
 
+import java.util.regex.Pattern;
+
 public class NameValidiator {
-    
+
+//    private static final String CAR_LIST_PATTERN = "^([a-zA-Z0-9가-힣])(,[a-zA-Z0-9가-힣])*$";
+    private static final String CAR_LIST_PATTERN = "^[a-zA-Z0-9가-힣]+[a-zA-Z0-9가-힣" + "," + "]*[a-zA-Z0-9가-힣]";
+
+    public void rightNameList(String cars) {
+        if (!Pattern.compile(CAR_LIST_PATTERN).matcher(cars).matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
