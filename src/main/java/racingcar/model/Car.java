268eaf067e72062bd36ecdbd32a3a8c4618822f6
Leejunho <junho767@naver.com>
2024-10-26 03:00:28 +0900

    rename: domain에서 model로 변경

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,18 @@
+package racingcar.model;
+
+public class Car {
+    private final String name;
+    public int move;
+
+    public Car(String name, int move){
+        this.name = name;
+        this.move = move;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getMove(){
+        return move;
+    }
+}
