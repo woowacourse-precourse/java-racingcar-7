8bf95b1c9ab5b8a494b9930a9ea38b4f61360747
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-10-25 19:36:58 +0900

    test(racing): 게임 진행 기능 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,12 +1,12 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -15,19 +15,47 @@ class ApplicationTest extends NsTest {
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,jun", "4");
+                    assertThat(output()).contains("pobi : --", "woni : ----", "jun : ---", "최종 우승자 : woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                STOP, MOVING_FORWARD, MOVING_FORWARD,
+                STOP, MOVING_FORWARD, STOP
+        );
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,jun", "2");
+                    assertThat(output()).contains("pobi : -", "jun : -", "최종 우승자 : pobi, jun");
+                },
+                MOVING_FORWARD, STOP,
+                STOP, MOVING_FORWARD
+        );
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("jun,pobi", "2");
+                    assertThat(output()).contains("pobi : -", "jun : -", "최종 우승자 : jun, pobi");
+                },
+                STOP, STOP,
+                MOVING_FORWARD, MOVING_FORWARD
         );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
