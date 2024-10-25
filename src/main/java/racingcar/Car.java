b176ebecfadccd0fdf15647c997f2160e180670b
leesh <leesh7807@gmail.com>
2024-10-25 14:18:43 +0900

    feat: move car and print info

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,32 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void attemptMove() {
+        int random = Randoms.pickNumberInRange(0, 9);
+        if (random >= 4) {
+            position++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name
+                + " : "
+                + "-".repeat(position)
+                + "\n";
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
