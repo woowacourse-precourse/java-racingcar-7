52315a0f5d2aedb6c836c7c79d0d78d435d2ca1b
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-25 18:25:41 +0900

    feat(RandomForwardCount): 0~9 사이에서 무작위 값을 생성하는 로직 추가

---
+++ src/main/java/racingcar/utils/RandomForwardCount.java
@@ -0,0 +1,11 @@
+package racingcar.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomForwardCount {
+
+    public static int randomSingleDigitNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+}
