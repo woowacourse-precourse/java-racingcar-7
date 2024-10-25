3ce0c18f707c8b5bd87cd6ce0086130786b9765e
dongyun <ehddbs452100@naver.com>
2024-10-25 23:32:37 +0900

    feat(Error): add a duplicated error message
    - 자동차 이름이 중복이 발생 했을 시 오류 메시지 추가

--- src/main/java/racingcar/global/error/Error.java
+++ src/main/java/racingcar/global/error/Error.java
@@ -4,6 +4,7 @@ public enum Error {
 
     NAME_BLANK_EXCEPION("[ERROR] 자동차 이름을 NULL이거나 빈칸으로 할 수 없습니다."),
     NAME_LENGTH_EXCEPION("[ERROR] 자동차 이름의 길이를 5자 초과 할 수 없습니다."),
+    NAME_DUPLICATE_EXCEPION("[ERROR] 자동차 이름이 중복 할 수 없습니다."),
     TRY_COUNT_EXCEPION("[ERROR] 시도 횟수는 양의 정수이어야합니다.");
 
     private String errorMsg;
