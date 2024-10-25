cf735ba81a8071959e4277ecaf7c777c333c3dc6
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-26 03:56:50 +0900

    refactor(RacingResult) : 코드 가독성 개선

--- src/main/java/racingcar/model/RacingResult.java
+++ src/main/java/racingcar/model/RacingResult.java
@@ -5,27 +5,27 @@ import java.util.List;
 public class RacingResult {
 
     private final List<RacingCar> racingRecords;
-    private int maxDistance;
+    private final int maxDistance;
 
     public RacingResult(List<RacingCar> racingRecords) {
         this.racingRecords = racingRecords;
+        this.maxDistance = findMaxDistance();
     }
 
-    public Integer findMaxDistance(Integer totalTrialCount) {
+    public Integer findMaxDistance() {
         return racingRecords.stream()
-                .map(racingCar -> racingCar.getDistance(totalTrialCount))
+                .map(RacingCar::getDistance)
                 .reduce(maxDistance, Integer::max);
     }
 
-    public List<String> findWinner(Integer totalTrialCount) {
-        this.maxDistance = findMaxDistance(totalTrialCount);
+    public List<String> findWinner() {
         return racingRecords.stream()
-                .filter(racingCar -> racingCar.getDistance(totalTrialCount) == maxDistance)
+                .filter(racingCar -> racingCar.getDistance() == maxDistance)
                 .map(RacingCar::getName)
                 .toList();
     }
 
-    public List<RacingCar> get() {
+    public List<RacingCar> getRecord() {
         return racingRecords;
     }
 }
