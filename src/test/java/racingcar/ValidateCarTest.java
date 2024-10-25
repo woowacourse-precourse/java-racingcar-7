148bca51da51bbc90b7b6c9fe6f818446c194401
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-25 15:38:14 +0900

    test: 자동차 입력값의 예외처리에 대한 테스트 작성

---
+++ src/test/java/racingcar/ValidateCarTest.java
@@ -0,0 +1,38 @@
+package racingcar;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+class ValidateCarTest extends NsTest {
+    @Test
+    void 예외_테스트_쉼표뒤에이름안들어옴() {
+        assertThatThrownBy(() -> ValidateCar.firstParsingOnComma("pobi,,java"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 예외_테스트_쉼표만() {
+        assertThatThrownBy(() -> ValidateCar.firstParsingOnComma(",,"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 예외_테스트_빈문자열() {
+        assertThatThrownBy(() -> ValidateCar.firstParsingOnComma(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 예외_테스트_이름길이초과() {
+        assertThatThrownBy(() -> ValidateCar.firstParsingOnComma("pobi,javaji"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
