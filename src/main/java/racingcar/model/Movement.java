ef898316f319601143243ade5ba2df3a7e3e6390
Chan531 <chan657312@gmail.com>
2024-10-25 15:32:51 +0900

    feat(Movement): 조건을 만족한다면 자동차 이동
    무작위 값이 4이상이라면 자동차 이동

--- src/main/java/racingcar/model/Movement.java
+++ src/main/java/racingcar/model/Movement.java
@@ -11,4 +11,10 @@ public class Movement {
 	public Movement() {
 		this.movement = INITIAL_MOVEMENT_VALUE;
 	}
+
+	private void move(int number) {
+		if (number >= MIN_REQUIRED_VALUE_FOR_MOVEMENT) {
+			movement += MOVEMENT_VALUE;
+		}
+	}
 }
