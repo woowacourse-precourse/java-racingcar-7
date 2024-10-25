7304b7fdc855f10cf013dcf84d13956a58b11371
mingking2 <mingee641@gmail.com>
2024-10-25 20:08:42 +0900

    refactor: 결과 출력 형식 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,21 +6,25 @@ import racingcar.model.Car;
 
 public class OutputView {
 
-    public static void outputProcessResultView(List<Car> carList) {
+    public static void outputProcessResultView() {
         System.out.println(OutputConstant.OUTPUT_PROCESS_RESULT_VIEW);
+    }
+
+    public static void outputProcessResultListView(List<Car> carList) {
         for (Car car : carList) {
             System.out.print(car.getName() + " : ");
-            for (int i = 0; i < car.getForward(); i++) {
-                System.out.println("-");
+            for (int i = 0; i < car.getMoveCount(); i++) {
+                System.out.print("-");
             }
             System.out.println();
         }
+        System.out.println();
     }
 
     public static void outputFinalWinnerView(List<Car> winnerList) {
-        System.out.println(OutputConstant.OUTPUT_FINAL_WINNER_VIEW);
+        System.out.print(OutputConstant.OUTPUT_FINAL_WINNER_VIEW);
         for (Car car : winnerList) {
-            System.out.println(car.getName() + ",");
+            System.out.print(car.getName() + ",");
         }
     }
 }
