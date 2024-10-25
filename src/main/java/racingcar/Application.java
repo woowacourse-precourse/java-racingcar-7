c0349cfb4037b995e1aa7ff7e78b1847cac9c86f
YoungGaLee <rkduddl1717@gmail.com>
2024-10-25 11:36:19 +0900

    feat: 사용자 입력
    경주할 자동차들의 이름과 이동 횟수를 입력받는다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO : refactoring
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputCar = Console.readLine();
+        String[] carName = inputCar.split(",");
+        // TODO: 글자수 체크, IllegalArgumentException 발생
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int num = Integer.parseInt(Console.readLine());
     }
 }
