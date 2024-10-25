52b46075c026f0dc62108d39789ece75bbe0e3cf
ghktndyd <hwasuyong99@naver.com>
2024-10-25 19:54:11 +0900

    refactor(OutputView): 상수의 접근 제어자 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -9,9 +9,9 @@ public class OutputView {
 
     private static final String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String REQUEST_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
-    public static final String RACING_RESULT_HEADER = "\n실행 결과";
-    public static final String RACING_RESULT_BODY = "%s : %s\n";
-    public static final String CAR_POSITION_SIGN = "-";
+    private static final String RACING_RESULT_HEADER = "\n실행 결과";
+    private static final String RACING_RESULT_BODY = "%s : %s\n";
+    private static final String CAR_POSITION_SIGN = "-";
 
     public void requestCarNames() {
         System.out.println(REQUEST_CAR_NAMES_MESSAGE);
