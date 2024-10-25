75f64ac069841c8dc432a24db93b00d9f4bf0f86
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-26 00:28:34 +0900

    refactor($OutputView): 출력 뷰 인터페이스로 변경
    - 출력을 받는 방법은 여러 가지가 있을 수 있음.
    - 하지만, 출력 방법이 변함에 따라 OutputView 코드가 수정되는 것은 확장성이 좋지 않음.
    - 따라서, OutputView 의 여러 가지 구현체를 추가하고, 갈아끼울 수 있도록 di 를 하도록 변경.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,55 +2,16 @@ package racingcar.view;
 
 import racingcar.dto.RacingRoundResult;
 import racingcar.model.RacingCars;
-import racingcar.util.io.OutputConsole;
 
 import java.util.List;
 
-public class OutputView {
+public interface OutputView {
 
-    private final String COLON = ":";
-    private final String DASH = "-";
-    private final String COMMA = ",";
-    private final String SPACE = " ";
+    void printInputCarNames();
 
-    public void printInputCarNames() {
-        String message = String.format("경주할 자동차 이름을 입력하세요.(이름은 쉼표(%s) 기준으로 구분)", COMMA);
-        OutputConsole.println(message);
-    }
+    void printInputTryCount();
 
-    public void printInputTryCount() {
-        String message = "시도할 회수는 몇회인가요?";
-        OutputConsole.println(message);
-    }
+    void printRacingRoundResults(List<RacingRoundResult> racingRoundResults);
 
-    public void printRacingRoundResults(List<RacingRoundResult> racingRoundResults) {
-        String message = "실행 결과";
-
-        OutputConsole.println();
-        OutputConsole.println(message);
-        printRoundResults(racingRoundResults);
-    }
-
-    public void printWinners(RacingCars racingCars) {
-        String message = String.format("최종 우승자 %s %s", COLON, racingCars.getJoinedNames(COMMA + SPACE));
-        OutputConsole.println(message);
-    }
-
-    private void printRoundResults(List<RacingRoundResult> racingRoundResults) {
-        racingRoundResults.forEach(this::printRoundResult);
-    }
-
-    private void printRoundResult(RacingRoundResult racingRoundResult) {
-        racingRoundResult.value().forEach((carName, position) -> {
-            String message = String.format("%s %s %s", carName, COLON, getDashes(position));
-
-            OutputConsole.println(message);
-        });
-
-        OutputConsole.println();
-    }
-
-    private String getDashes(Integer position) {
-        return DASH.repeat(position);
-    }
+    void printWinners(RacingCars racingCars);
 }
