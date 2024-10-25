0059473367022d03ce312d036b009fcbc6e29be6
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 20:35:40 +0900

    fest: 자동차 세팅 기능을 추가한다

--- src/main/java/racingcar/controller/InputController.java
+++ src/main/java/racingcar/controller/InputController.java
@@ -1,4 +1,26 @@
 package racingcar.controller;
 
+import racingcar.validator.CarNameValidator;
+import racingcar.view.InputView;
+
+import java.util.List;
+
 public class InputController {
+
+    public static final String DELIMITER = ",";
+
+    public static List<String> setCars() {
+        String cars = InputView.inputCar();
+
+        List<String> carsList = List.of(cars.split(DELIMITER));
+
+        CarNameValidator check = new CarNameValidator();
+
+        check.rightNamePattern(cars);
+        check.nameLength(carsList);
+        check.nameOverlap(carsList);
+
+        return carsList;
+    }
+
 }
