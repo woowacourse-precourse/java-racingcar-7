b6e43e42875679a68065760c56a070a7297ab8c3
youngjuKim <kjuzoojuk@naver.com>
2024-10-25 14:25:51 +0900

    style(ApplicationTest): 코드 형식 변경

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,10 +1,12 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -23,40 +25,41 @@ class ApplicationTest extends NsTest {
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @ParameterizedTest
     @MethodSource("provideNumberRandomSet")
-    void 기능_테스트_MethodSource(int num1, int num2, int num3){
+    void 기능_테스트_MethodSource(int num1, int num2, int num3) {
         assertRandomNumberInRangeTest(
                 () -> {
                     run("gene7, pobi,woni", "1");
-                    assertThat(output()).contains("gene7 : -","pobi : -", "woni : ", "최종 우승자 : gene7, pobi");
+                    assertThat(output()).contains("gene7 : -", "pobi : -", "woni : ", "최종 우승자 : gene7, pobi");
                 },
                 num1, num2, num3
         );
     }
-    public static Stream<Arguments> provideNumberRandomSet(){
+
+    public static Stream<Arguments> provideNumberRandomSet() {
         return Stream.of(
-                Arguments.of(4,5,0),
-                Arguments.of(9,6,2)
+                Arguments.of(4, 5, 0),
+                Arguments.of(9, 6, 2)
         );
     }
 
 
     @ParameterizedTest
     @CsvSource({"young,5", "ju,4", "J2085,9"})
-    void 기능_테스트_CsvSource(String nameCars, int value){
+    void 기능_테스트_CsvSource(String nameCars, int value) {
         assertRandomNumberInRangeTest(
                 () -> {
-                    run(nameCars+", pobi,woni", "1");
-                    assertThat(output()).contains(nameCars+" : -","pobi : -", "woni : ", "최종 우승자 : "+nameCars+", pobi");
+                    run(nameCars + ", pobi,woni", "1");
+                    assertThat(output()).contains(nameCars + " : -", "pobi : -", "woni : ", "최종 우승자 : " + nameCars + ", pobi");
                 },
                 MOVING_FORWARD, value, STOP
         );
@@ -77,8 +80,8 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
