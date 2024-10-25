dcc4ee2772fb333dd0c4131dcc58be32abf56676
changuii <rhljh7410@gmail.com>
2024-10-25 17:47:02 +0900

    feat(RacingCarTransporter) : 5. 자동차들을 전진시켜라
    - RacingCarTransporter : 자동차를 움직이는 역할을 수행하는 운송자 인터페이스

---
+++ src/main/java/racingcar/transporter/RacingCarTransporter.java
@@ -0,0 +1,8 @@
+package racingcar.transporter;
+
+import java.util.List;
+import racingcar.domain.Car;
+
+public interface RacingCarTransporter {
+    public void transportRacingCar(List<Car> racingCars);
+}
