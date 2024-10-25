08346003ee7d44394ccba697e1795a65fef3a184
Chaewon <corndots@naver.com>
2024-10-26 05:06:57 +0900

    feat(Application): main 실행 후 IllegalArgumentException이 발생 시 예외 메시지를 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            RacingCarController controller = new RacingCarController();
+            controller.startGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
