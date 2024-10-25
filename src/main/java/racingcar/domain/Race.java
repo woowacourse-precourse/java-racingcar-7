e4d573efd75e866c69e007ab0d649a929c5d1e5d
bowook <mbosx316@naver.com>
2024-10-25 22:00:29 +0900

    refactor(Race) : Race 도메인 리팩토링
    - maxPosition 삭제
    - updateMaxPostion 메서드 삭제
    - getRandomNumber 메서드 삭제

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -1,45 +1,26 @@
 package racingcar.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
 public class Race {
 
     private final Cars cars;
     private final TrialCounts trialCounts;
     private int currentCounts;
-    private int maxPosition;
 
     public Race(Cars cars, TrialCounts trialCounts) {
         this.cars = cars;
         this.trialCounts = trialCounts;
         this.currentCounts = 0;
-        this.maxPosition = 0;
     }
-    private int getRandomNumber() {
-        return Randoms.pickNumberInRange(0,9);
-    }
-
     private void moveCars() {
         for(Car car : cars.getCars()) {
-            car.move(getRandomNumber());
-        }
-    }
-
-    private void updateMaxPosition() {
-        for(Car car : cars.getCars()) {
-            maxPosition = Math.max(maxPosition, car.getPosition());
+            car.move(RandomNumber.getRandomNumber());
         }
     }
     public void raceOneStep() {
         moveCars();
-        updateMaxPosition();
         currentCounts++;
     }
-
     public boolean isRaceOngoing() {
         return currentCounts < trialCounts.getTrialCounts();
-    }
-
-    public int getMaxPosition() {
-        return maxPosition;
     }
 }
