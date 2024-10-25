a35c81c8ac8dca725428f64bd0cb4bf66285fa5b
joshinhyeok <shtree07@naver.com>
2024-10-25 23:05:42 +0900

    refactor: Test -> Main으로 클래스 이동

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -8,9 +8,11 @@ public class ResultView {
 
     public static void printRaceResult(List<Car> cars) {
         System.out.println();
+
         for (Car car : cars) {
             System.out.println(car.getCarName() + " : " + "-".repeat(car.getPosition()));
         }
+
         System.out.println();
     }
 
