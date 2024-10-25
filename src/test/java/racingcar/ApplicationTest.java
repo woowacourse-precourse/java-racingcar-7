03e011ba46b34c8c367065fc8a23440fc939aa2d
YeonjiIsGonji <165933276+YeonjiIsGonji@users.noreply.github.com>
2024-10-25 21:00:39 +0900

    test: 자동차 이름 유효성 검사에 대한 테스트 코드 추가
    - 유효한 입력 값이 주어졌을 때 true를 반환하는지 확인
     - @ParameterizedTest로 여러 유효한 입력에 대해 테스트 수행
     - 이름 앞뒤에 공백이 있는 경우
     - 이름 내에 공백이나 특수문자가 포함된 경우
    - 유효하지 않은 입력 값이 주어졌을 때 IllegalArgumentException 발생 여부 확인
     - 이름이 5자를 초과하는 경우
     - 이름이 누락된 경우
     - 이름이 중복된 경우
     - 이름이 1개인 경우
     - 다른 구분자를 사용하지 않은 경우

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -24,10 +26,116 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 이름_앞뒤_공백_제거_검증() {
+        String carNames = " pobi   , jun,jenny ";
+        assertThat(Application.isValidName(carNames)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {" po bi , jun,jenny", " po bi ,ju  n "})
+    void 이름_내_공백_허용_검증(String carNames) {
+        assertThat(Application.isValidName(carNames)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {" pobi* , jun1,je+ny", "**, +++,car2", "po b2 , j@@n, (**)"})
+    void 이름_내_기호_허용_검증(String carNames) {
+        assertThat(Application.isValidName(carNames)).isTrue();
+    }
+
+    @Test
+    void 예외_테스트_이름5자초과() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" pobi , junnnn,jenny ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" po  bi , jun,jenny ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" po  bi , ju   n,jenny", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_이름누락() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,,jun", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_다른구분자사용() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi.jun.jenny", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi.jun;jenny", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_이름미입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("   ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_이름중복() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,pobi,jun", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_쉼표로끝나는경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,jun,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_참가자동차1대() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("*", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
