ada1e5722c570d42b1e564e3ec7aa64016335455
soeun2537 <soeun2537@gmail.com>
2024-10-25 23:23:30 +0900

    refactor: 자동차 이름 유효성 검사 - 공백 포함 로직 분리

--- src/main/java/racingcar/model/Validator.java
+++ src/main/java/racingcar/model/Validator.java
@@ -1,5 +1,7 @@
 package racingcar.model;
 
+import static racingcar.constant.Constant.*;
+
 import java.util.List;
 
 public class Validator {
@@ -7,11 +9,18 @@ public class Validator {
     public void validateCarNames(List<String> CarNames) {
         for (String carName : CarNames) {
             validateNotEmpty(carName);
+            validateNoSpaces(carName);
         }
     }
 
     private void validateNotEmpty(String input) {
         if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNoSpaces(String input) {
+        if (input.contains(SPACE)) {
             throw new IllegalArgumentException();
         }
     }
