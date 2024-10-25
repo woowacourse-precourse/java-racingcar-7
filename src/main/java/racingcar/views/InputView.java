56d6324d9357b07b533963e357d6e3023d27d805
LeeDongHoon <likelion.leedh@gmail.com>
2024-10-25 18:48:54 +0900

    [REFACTOR] : 차 이름 입력 시 쉼표로 구분하고 리스트로 결과 반환

--- src/main/java/racingcar/views/InputView.java
+++ src/main/java/racingcar/views/InputView.java
@@ -2,13 +2,21 @@ package racingcar.views;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String RACE_ATTEMPTS_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
-    public static String inputCarNames() {
+    public static List<String> inputCarNames() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
-        return Console.readLine();
+        String input = Console.readLine();
+        return splitCarNames(input);
+    }
+
+    public static List<String> splitCarNames(String input) {
+        return Arrays.stream(input.split(",")).toList();
     }
 
     public static String inputRaceAttempts() {
