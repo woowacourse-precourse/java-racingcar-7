29e217405b4fbae03b54f40a26a0d3d57eb41e7e
unh6unh6 <ruddbs3308@naver.com>
2024-10-25 16:50:14 +0900

    feat(view): 자동차 이동 횟수가 자연수인지 검사

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
     private static final String CAR_MOVEMENT_COUNT_INPUT_GUIDE = "시도할 횟수는 몇 회인가요?";
 
     private static final String CAR_MOVEMENT_COUNT_BLANK_EXCEPTION = "자동차 이동 횟수는 빈칸이 아니어야합니다.";
+    private static final String CAR_MOVEMENT_COUNT_NOT_NATURAL_NUMBER_EXCEPTION = "자동차 이동 횟수는 자연수이어야 합니다.";
 
     public void printCarNameInputGuide() {
         System.out.println(CAR_NAME_INPUT_GUIDE);
@@ -21,12 +22,26 @@ public class InputView {
     }
 
     public String getCarMovementCount() {
-        return Console.readLine();
+        String inputString = Console.readLine();
+        validateCarMovementCountOrThrow(inputString);
+        return inputString;
     }
 
     public void validateCarMovementCountOrThrow(String inputString) {
         if (inputString.isBlank()) {
             throw new IllegalArgumentException(CAR_MOVEMENT_COUNT_BLANK_EXCEPTION);
+        }
+        if (!isNaturalNumber(inputString)) {
+            throw new IllegalArgumentException(CAR_MOVEMENT_COUNT_NOT_NATURAL_NUMBER_EXCEPTION);
+        }
+    }
+
+    public boolean isNaturalNumber(String inputString) {
+        try {
+            int number = Integer.parseInt(inputString);
+            return number > 0;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 }
