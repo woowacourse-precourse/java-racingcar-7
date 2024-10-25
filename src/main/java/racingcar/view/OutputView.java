84133feff914ca993c34e7da2d57c4a668d46edf
변우영 <likevangogh24@gmail.com>
2024-10-25 19:18:37 +0900

    feat(OutputView): 우승자 안내 문구 출력 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -8,8 +8,10 @@ public class OutputView {
 
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ROUND_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자 :";
     private static final String NAME_POSITION_SEPARATOR = " : ";
     private static final String MOVE_DASH = "-";
+    private static final String NAME_DELIMITER = ", ";
 
     public void printInputCarNameMessage() {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
@@ -24,5 +26,9 @@ public class OutputView {
             System.out.println(carStatusDto.getName() + NAME_POSITION_SEPARATOR + MOVE_DASH.repeat(carStatusDto.getPosition()));
         }
         System.out.println();
+    }
+
+    public void printWinnerNames(List<String> names) {
+        System.out.println(FINAL_WINNER_MESSAGE + NAME_POSITION_SEPARATOR + String.join(NAME_DELIMITER, names));
     }
 }
