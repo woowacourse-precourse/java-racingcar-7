fc642b7f3f0e7934500824386176dbc1b166900c
김연진 <love_homebody@naver.com>
2024-10-25 20:07:08 +0900

    test(Car): 자동차 객체 생성 예외 테스트

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -4,8 +4,32 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarTest {
+
+    @Test
+    @DisplayName("5자 이하의 이름을 입력할 경우 자동차 객체가 정상적으로 생성된다.")
+    void create() {
+        // given
+        String name = "name";
+        // when
+        Car car = new Car(name);
+        // then
+        assertThat(car).isNotNull();
+        assertThat(car.getName()).isEqualTo(name);
+    }
+
+    @Test
+    @DisplayName("자동차의 이름이 5자가 넘어갈 경우 예외가 발생한다.")
+    void throwTooLongNameException() {
+        // given
+        String name = "toolon";
+
+        // when, then
+        assertThatThrownBy(() -> new Car(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @Test
     @DisplayName("4이상의 숫자가 입력될 경우 움직인다.")
