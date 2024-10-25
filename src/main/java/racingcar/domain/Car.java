930f9acc792a62f701875609418cced0756a39f3
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-10-26 05:43:16 +0900

    refactor(Car): Car 객체 필드 키워드 수정
    * Car.java: position 은 변할 수 있는 값이기 때문에, position 객체의 final 키워드 제거. 상수에 static 키워드 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,10 +2,10 @@ package racingcar.domain;
 
 public class Car {
 
-    private final int INITIAL_POSITION = 0;
+    private static final int INITIAL_POSITION = 0;
 
     private final CarName carName;
-    private final int position;
+    private int position;
 
     public Car(CarName carName) {
         this.carName = carName;
