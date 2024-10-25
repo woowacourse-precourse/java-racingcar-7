0629e7ceb1b7914332ea9ba84ad050408eb68714
leesh <leesh7807@gmail.com>
2024-10-25 16:47:36 +0900

    feat: convert to list of Car from list of string
    RacingInput receive user input in format: List<String>.
    So, make a method about converting a list of strings to a list of Car
    instances.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The class {@code Car} represents a car with a name and a position. The car can attempt to move forward and provides
@@ -13,6 +15,20 @@ public class Car {
     public Car(String name) {
         this.name = name;
         this.position = 0;
+    }
+
+    /**
+     * Creates a new list of {@code Car} instance from a list of car names.
+     *
+     * @param carNames a list of strings representing car names
+     * @return {@code ArrayList<Car>}
+     */
+    public static List<Car> from(List<String> carNames) {
+        List<Car> result = new ArrayList<>();
+        for (String name : carNames) {
+            result.add(new Car(name));
+        }
+        return result;
     }
 
     /**
