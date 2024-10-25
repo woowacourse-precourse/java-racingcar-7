1427fa5eed2f902cf923672ce83d4c7112852e3a
dbdb456 <adad568@naver.com>
2024-10-25 15:46:44 +0900

    feat(controller): 필드, 생성자 추가

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,4 +1,24 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.utils.Utils;
+import racingcar.validator.Validator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Controller {
+
+    private List<Car> racingCarList;
+    private final Validator validator;
+    private final Utils utils;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public Controller(Validator validator, Utils utils, InputView inputView, OutputView outputView) {
+        this.validator = validator;
+        this.utils = utils;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
 }
