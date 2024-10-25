2c5f2204d6d81d2febc398c965e5f48486cfd05c
leesh <leesh7807@gmail.com>
2024-10-25 14:35:18 +0900

    docs: add Javadoc to Car class

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,6 +2,10 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+/**
+ * The class {@code Car} represents a car with a name and a position. The car can attempt to move forward and provides
+ * methods to retrieve its information.
+ */
 public class Car {
     private final String name;
     private int position;
@@ -11,6 +15,9 @@ public class Car {
         this.position = 0;
     }
 
+    /**
+     * A car attempt to move with 60% success rate.
+     */
     public void attemptMove() {
         int random = Randoms.pickNumberInRange(0, 9);
         if (random >= 4) {
@@ -18,6 +25,11 @@ public class Car {
         }
     }
 
+    /**
+     * Returns a car's information in the format: {@code "[name] : ['-' repeated by position]\n"}
+     *
+     * @return a string representing a car
+     */
     @Override
     public String toString() {
         return name
