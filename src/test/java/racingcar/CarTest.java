39511038c170845d61d133c4e76e557fc2bbaf0c
kwanse <khan67781397@gmail.com>
2024-10-25 20:30:47 +0900

    test: 자동차 이름 검증 테스트 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -22,4 +22,25 @@ class CarTest {
         // then
         Assertions.assertThat(car.getDistance()).isEqualTo(new Distance(1));
     }
+
+
+    @DisplayName("자동차 이름에 알파뱃이 아닌 다른 문자가 들어가면 예외를 발생시킨다")
+    @Test
+    void nameFormExceptionTest() {
+
+        Assertions.assertThatThrownBy(() -> Car.create("a1"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        Assertions.assertThatThrownBy(() -> Car.create("a a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차 이름이 5글자를 초과하면 예외를 발생시킨다")
+    @Test
+    void nameLengthExceptionTest() {
+
+        Assertions.assertThatThrownBy(() -> Car.create("abcdef"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
