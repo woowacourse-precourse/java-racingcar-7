b4adc1d6ba1a27d22e0e5df94aee0f636afea057
YooSejeong <nived3@naver.com>
2024-10-25 20:52:42 +0900

    feat(Application): Add user input for number of race rounds

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,7 +11,11 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carsInput = Console.readLine();
         List<String> cars = Arrays.asList(carsInput.split(","));
-        
+
+        //TODO: 다른 클래스로 분리할 수 있으면 분리하기
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int raceRounds = Integer.parseInt(Console.readLine());
+
     }
 
 }
