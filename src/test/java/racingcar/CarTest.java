622540fabb2e24e2d664178fb3dd4a39564e8de6
Lkk7380 <dlrlrhkd@naver.com>
2024-10-25 14:23:39 +0900

    feat: 자동차 이름 길이 검증 기능 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,11 +2,18 @@ package racingcar;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarTest {
     @Test
     void 자동차_이름을_가진다() {
         Car car = new Car("pobi");
         assertThat(car.getName()).isEqualTo("pobi");
+    }
+
+    @Test
+    void 자동차_이름_5글자_초과시_예외발생() {
+        assertThatThrownBy(() -> new Car("javaji"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
