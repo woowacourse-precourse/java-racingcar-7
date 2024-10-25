149d263cdce702c651b76556a033a208de6803b2
eunseobb <seobseob0810@naver.com>
2024-10-26 00:46:08 +0900

    feat(model): Car 객체 클래스 생성
    Car 객체를 생성하여 자동차의 이름과 이동 횟수를 관리할 수 있도록 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,12 @@
+package racingcar.model;
+
+public class Car {
+    public String name;
+    public Integer totalMovement = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+
+}
