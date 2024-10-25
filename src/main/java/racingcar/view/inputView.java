8ea1570142dca1e2f99473382f3ee213b4d73569
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 19:39:13 +0900

    feat: 라운드 수를 입력하는 기능을 추가한다

--- src/main/java/racingcar/view/inputView.java
+++ src/main/java/racingcar/view/inputView.java
@@ -5,9 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class inputView {
 
     private static final String INPUT_CAR_LIST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_ROUND_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
     public static String inputCar() {
         System.out.println(INPUT_CAR_LIST_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String inputRound() {
+        System.out.println(INPUT_TRY_ROUND_MESSAGE);
         return Console.readLine();
     }
 }
