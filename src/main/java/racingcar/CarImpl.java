3b6f82f58da7dd0bbbbeba3903cf8262343fd406
choign3123 <choign3123@naver.com>
2024-10-25 12:19:09 +0900

    Feat(CarImpl): 자동차 구현

---
+++ src/main/java/racingcar/CarImpl.java
@@ -0,0 +1,35 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class CarImpl implements ICar {
+
+    private final static int DRIVE_STANDARD = 4;
+    private final String name;
+    private int position;
+
+    public CarImpl(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    @Override
+    public int drive() {
+        int random = Randoms.pickNumberInRange(0, 9);
+        if (DRIVE_STANDARD <= random) {
+            position++;
+        }
+
+        return position;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getPosition() {
+        return position;
+    }
+}
