490a39c1ce23f05cda0043193bbeabcbc4056813
Chan531 <chan657312@gmail.com>
2024-10-25 15:23:45 +0900

    feat(Movement): 이동을 위한 최소값 상수 추가

--- src/main/java/racingcar/model/Movement.java
+++ src/main/java/racingcar/model/Movement.java
@@ -3,6 +3,7 @@ package racingcar.model;
 public class Movement {
 
 	private static final int INITIAL_MOVEMENT_VALUE = 0;
+	private static final int MIN_REQUIRED_VALUE_FOR_MOVEMENT = 4;
 
 	private final int movement;
 
