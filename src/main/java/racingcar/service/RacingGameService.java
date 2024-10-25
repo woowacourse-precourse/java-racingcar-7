b5ea9691e4895d05dd676882378292d8256cd763
tentenball <lth8905@gmail.com>
2024-10-25 23:03:38 +0900

    refactor: RacingGameService 수정
    * RacingGameService: 메서드 반환타입 수정

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -1,21 +1,30 @@
 package racingcar.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 import racingcar.domain.car.Cars;
 import racingcar.domain.racingGame.RacingGameCount;
+import racingcar.domain.racingGame.RacingRecord;
 import racingcar.util.RandomNumberGenerator;
 
 public class RacingGameService {
-	public void processRacingGame(Cars cars, RacingGameCount gameCount) {
+	public List<RacingRecord> processRacingGame(Cars cars, RacingGameCount gameCount) {
+		List<RacingRecord> records = new ArrayList<>();
 		IntStream.range(0, gameCount.getCount())
-			.forEach(racingGame -> processEachRacingGame(cars, RandomNumberGenerator.generateRandomNumber()));
+			.forEach(racingGame -> {
+				RacingRecord record = processEachRacingGame(cars, RandomNumberGenerator.generateRandomNumber());
+				records.add(record);
+			});
+		return records;
 	}
 
-	private void processEachRacingGame(Cars cars, int randomNumber) {
+	private RacingRecord processEachRacingGame(Cars cars, int randomNumber) {
 		cars.getCars().forEach(car -> {
 			car.processGame(randomNumber);
 		});
+		return new RacingRecord(cars);
 
 	}
 }
