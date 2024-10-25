202ffd367bda5597c02cd07028c353351304c863
StoneCAU <seokwoo1999@naver.com>
2024-10-26 00:33:56 +0900

    refactor(CarNameParser): CarNameValidator의 검증 로직 추가
    - 구분자로 끝나는지의 여부
    - 올바른 자동차 이름의 길이의 여부
    - 중복되는 자동차 이름 존재 여부

--- src/main/java/racingcar/utils/CarNameParser.java
+++ src/main/java/racingcar/utils/CarNameParser.java
@@ -1,21 +1,24 @@
-package racingcar.utils.parser;
+package racingcar.utils;
 
 import racingcar.validator.CarNameValidator;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static racingcar.validator.CarNameValidator.validateDuplicate;
+import static racingcar.validator.CarNameValidator.validateEndsWithDelimiter;
+
 public class CarNameParser {
     public static final String NAME_DELIMITER = ",";
 
     public static List<String> parse(String input) {
-        CarNameValidator.validateEndsWithDelimiter(input);
+        validateEndsWithDelimiter(input);
 
         List<String> carNames = Arrays.stream(input.split(NAME_DELIMITER))
                 .peek(CarNameValidator::validateLength)
                 .toList();
 
-        CarNameValidator.validateDuplicate(carNames);
+        validateDuplicate(carNames);
 
         return carNames;
     }
