c82f43f2f0e5839c43b9426e4daff1fa7f1d4d05
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 21:18:35 +0900

    feat: 라운드 수를 검증하는 기능을 추가한다

--- src/main/java/racingcar/validator/RoundValidator.java
+++ src/main/java/racingcar/validator/RoundValidator.java
@@ -1,4 +1,18 @@
 package racingcar.validator;
 
 public class RoundValidator {
+
+    public int parseInt(String round) {
+        try {
+            return Integer.parseInt(round);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void positiveNumber(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
