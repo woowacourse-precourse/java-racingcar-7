eee19572a1a832a1e0810d7daf9a8f71c509b0ba
githublees <leeho_@naver.com>
2024-10-25 16:14:30 +0900

    refactor: Separate constants for improved readability

--- src/main/java/racingcar/constant/Constant.java
+++ src/main/java/racingcar/constant/Constant.java
@@ -1,13 +1,14 @@
 package racingcar.constant;
 
 public class Constant {
-    public static final String CAR_NAME_INPUT_STRING = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
+    public static final String CAR_NAME_INPUT_STRING = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String TRY_COUNT_INPUT_STRING = "시도할 횟수는 몇 회인가요?";
     public static final String RACING_PROGRESS_STRING = "실행 결과";
     public static final String RACING_RESULT_STRING = "최종 우승자 : ";
 
     public static final String MAX_NAME_SIZE_STRING = "이름은 5자 이하만 가능합니다.";
     public static final String COUNT_ONLY_NATURAL_NUMBER_STRING = "시도할 횟수는 양수여야 합니다.";
+    public static final String COUNT_ONLY_INTEGER_NUMBER_STRING = "시도할 횟수는 2147483647 값을 초과해서는 안됩니다.";
     public static final String SPACE_BETWEEN_NAME_STRING = "이름 사이에 공백이 존재해서는 안됩니다.";
     public static final String INPUT_NULL_OR_EMPTY_STRING = "입력값이 Null 또는 공백이면 안됩니다.";
     public static final String RANDOM_RANGE_VALUE_ERROR_STRING = "무작위 값이 0 또는 9 사이 값이 아닙니다.";
