25c161897fd838e48542d01ed2b2a7e40ff72800
eb9862 <luclead622@gmail.com>
2024-10-25 17:38:31 +0900

    refactor : 사용자 입력 검증부를 Validator 클래스로 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import static racingcar.Validator.validateCarNames;
+import static racingcar.Validator.validateTryCount;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,48 +29,6 @@ public class Application {
             racingCarList.add(racingCar);
         }
         return racingCarList;
-    }
-
-    static void validateCarNames(String carNames) {
-        checkNameExist(carNames);
-        for (String carName : carNames.split(",")) {
-            String stripCarName = carName.strip();
-            checkEmptyName(stripCarName);
-            checkCarNameLength(stripCarName);
-        }
-    }
-
-    static void checkNameExist(String carNames) {
-        if (carNames.isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름이 존재하지 않습니다");
-        }
-        String[] splitCarNames = carNames.split(",");
-        if (splitCarNames.length == 0) {
-            throw new IllegalArgumentException("자동차 이름이 존재하지 않습니다");
-        }
-    }
-
-    static void checkEmptyName(String carName) {
-        if (carName.isEmpty()) {
-            throw new IllegalArgumentException("쉼표 이후에 자동차 이름이 존재하지 않습니다");
-        }
-    }
-
-    static void checkCarNameLength(String carName) {
-        if (carName.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다");
-        }
-    }
-
-    static void validateTryCount(String tryCount) {
-        try {
-            int tryCountNum = Integer.parseInt(tryCount);
-            if (tryCountNum <= 0) {
-                throw new IllegalArgumentException("시도할 횟수가 자연수가 아닙니다");
-            }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("시도할 횟수가 자연수가 아닙니다");
-        }
     }
 
     static String readCarNames() {
---
+++ src/main/java/racingcar/Validator.java
@@ -0,0 +1,46 @@
+package racingcar;
+
+public class Validator {
+
+    static void validateCarNames(String carNames) {
+        checkNameExist(carNames);
+        for (String carName : carNames.split(",")) {
+            String stripCarName = carName.strip();
+            checkEmptyName(stripCarName);
+            checkCarNameLength(stripCarName);
+        }
+    }
+
+    static void checkNameExist(String carNames) {
+        if (carNames.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름이 존재하지 않습니다");
+        }
+        String[] splitCarNames = carNames.split(",");
+        if (splitCarNames.length == 0) {
+            throw new IllegalArgumentException("자동차 이름이 존재하지 않습니다");
+        }
+    }
+
+    static void checkEmptyName(String carName) {
+        if (carName.isEmpty()) {
+            throw new IllegalArgumentException("쉼표 이후에 자동차 이름이 존재하지 않습니다");
+        }
+    }
+
+    static void checkCarNameLength(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다");
+        }
+    }
+
+    static void validateTryCount(String tryCount) {
+        try {
+            int tryCountNum = Integer.parseInt(tryCount);
+            if (tryCountNum <= 0) {
+                throw new IllegalArgumentException("시도할 횟수가 자연수가 아닙니다");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 횟수가 자연수가 아닙니다");
+        }
+    }
+}
