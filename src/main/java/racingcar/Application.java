3a55fafa795217e345387e4693580933f51d91cd
yoonjin <skyj217@naver.com>
2024-10-25 22:36:34 +0900

    feat: 입출력 및 예외 처리 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String[] carNames = scanner.nextLine().split(",");
+
+            System.out.println("시도할 횟수는 몇 회인가요?");
+            int raceRounds = scanner.nextInt();
+
+        } catch (IllegalArgumentException e) { // 잘못된 값 예외 처리
+            System.out.println(e.getMessage());
+        } finally {
+            scanner.close();
+        }
     }
 }
+
