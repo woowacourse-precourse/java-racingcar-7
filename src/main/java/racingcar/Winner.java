3a6bdb60dadd6d149e82cb442eb26ba2365d1ecc
eunho <eunho1032@gmail.com>
2024-10-25 16:04:27 +0900

    refactor(Winner): using tabs change spaces

--- src/main/java/racingcar/Winner.java
+++ src/main/java/racingcar/Winner.java
@@ -5,41 +5,41 @@ import java.util.List;
 
 public class Winner {
 
-	public static List<Car> winners = new ArrayList<Car>();
+    public static int findMaxDistance(List<Car> cars) {
+        int maxDistance = 0;
 
-	public static int findMaxDistance(List<Car> cars) {
-		int maxDistance = 0;
+        for (Car car : cars) {
+            if (maxDistance < car.distance) {
+                maxDistance = car.distance;
+            }
+        }
+        return maxDistance;
+    }
 
-		for (Car car : cars) {
-			if (car.distance > maxDistance) {
-				maxDistance = car.distance;
-			}
-		}
-		return maxDistance;
-	}
+    public static List<Car> find(List<Car> cars) {
+        int maxDistance = findMaxDistance(cars);
 
-	public static List<Car> contain(List<Car> cars) {
-		int maxDistance = findMaxDistance(cars);
+        List<Car> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (maxDistance == car.distance) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
 
-		for (Car car : cars) {
-			if (maxDistance == car.distance) {
-				winners.add(car);
-			}
-		}
-		return winners;
-	}
+    public static String makeList(List<Car> winners) {
+        String winnerList = "";
 
-	public static String makeList() {
-		String winnerList = "";
+        int i = 0;
 
-		int i = 0;
-		for (Car car : winners) {
-			winnerList += car.name;
-			if (i < winners.size() - 1) {
-				winnerList += ", ";
-			}
-			i++;
-		}
-		return winnerList;
-	}
+        for (Car car : winners) {
+            winnerList += car.name;
+            if (i < winners.size() - 1) {
+                winnerList += ", ";
+            }
+            i++;
+        }
+        return winnerList;
+    }
 }
