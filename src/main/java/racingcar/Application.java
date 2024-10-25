67abdc26a7daceab1092ca6f333632e66cff06ae
youjeans <dbyj2810@naver.com>
2024-10-25 22:56:28 +0900

    feat: car 클래스 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,37 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+}
+
+class Car {
+    private final String name;
+    private int movingCount;
+
+    public Car(String name) {
+        this.name = name;
+        this.movingCount = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMovingCount() {
+        return movingCount;
+    }
+
+    public void move() {
+        int randomValue = Randoms.pickNumberInRange(0, 9);
+        if (randomValue >= 4) {
+            movingCount++;
+        }
     }
 }
