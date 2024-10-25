0a0132d388db896309718ae2b0314c28df0b22f2
shin-chaeeun <chaeeun0217@naver.com>
2024-10-25 21:51:02 +0900

    Refactor(CarGame):Extract CarGame from main class

--- src/main/java/view/CarInputView.java
+++ src/main/java/view/CarInputView.java
@@ -3,14 +3,22 @@ package view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class CarInputView {
-  private String carInput ;
+  public String carInput ;
+  public int frequency ;
 
   public void CarInput() {
     System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     this.carInput = Console.readLine();
   }
 
-  public String getCarInput() {
-    return carInput;
+  public void repeatFrequency() {
+    System.out.println("시도할 횟수는 몇 회인가요");
+    String frequencyInput = Console.readLine();
+
+    if (frequencyInput.matches(".*[^0-9].*")) {
+      throw new IllegalArgumentException();
+    }
+
+    this.frequency= Integer.parseInt(frequencyInput);
   }
 }
