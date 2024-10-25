926bdf724694cda5849abb30a00987b3adba11cc
HyeonJeong <wgwjh05169@gmail.com>
2024-10-25 21:09:08 +0900

    feat(Writer): 라운드 결과 출력 구현

--- src/main/java/racingcar/Writer.java
+++ src/main/java/racingcar/Writer.java
@@ -1,8 +1,26 @@
 package racingcar;
 
+import java.util.List;
+
 public class Writer {
 
     public static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void printRoundResult(List<Car> cars) {
+
+        for (Car car : cars)
+            printCarScore(car);
+
+        System.out.println();
+    }
+
+    public static void printCarScore(Car car) {
+        System.out.print(car.getName() + " : ");
+        for (int i = 0; i < car.getScore(); i++)
+            System.out.print("-");
+
+        System.out.println("");
     }
 }
