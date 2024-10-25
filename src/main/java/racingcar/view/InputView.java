cedc26ad028636027be24728747e0cbf306fe713
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-26 00:26:04 +0900

    refactor($InputView): 입력 뷰 인터페이스로 변경
    - 입력을 받는 방법은 여러 가지가 있을 수 있음.
    - 하지만, 입력 방법이 변함에 따라 InputView 코드가 수정되는 것은 확장성이 좋지 않음.
    - 따라서, InputView 의 여러 가지 구현체를 추가하고, 갈아끼울 수 있도록 di 를 하도록 변경.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,20 +1,10 @@
 package racingcar.view;
 
-import racingcar.util.io.InputConsole;
-
 import java.util.List;
 
-public class InputView {
+public interface InputView {
 
-    public List<String> getCarNames() {
-        return List.of(InputConsole.readLine().split(","));
-    }
+    List<String> getCarNames();
 
-    public int getTryCount() {
-        try {
-            return Integer.parseInt(InputConsole.readLine());
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("유효하지 않은 시도 횟수입니다.");
-        }
-    }
+    int getTryCount();
 }
