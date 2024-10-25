9c3258b1d7edee915a6000dda0ed10d336923672
wogus <ind07152@naver.com>
2024-10-25 11:47:01 +0900

    feat(View): 각 시도마다 자동차 상태 출력 기능 추가
    각 시도마다 자동차들의 상태를 출력한다.

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -2,12 +2,14 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import racingcar.model.Car;
 import racingcar.utils.Parser;
 import racingcar.validator.Validator;
 
 public final class View {
     private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String RESULT_MESSAGE = "실행 결과";
 
     public static List<String> getCarNames() {
         System.out.println(NAME_INPUT_MESSAGE);
@@ -19,5 +21,12 @@ public final class View {
         String count = Console.readLine();
         Validator.countValidate(count);
         return Integer.parseInt(count);
+    }
+
+    public static void showResult(List<Car> cars) {
+        System.out.println(RESULT_MESSAGE);
+        cars.forEach(car -> {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getLocation()));
+        });
     }
 }
