de1f09438e103d03d23ce21642f49fcb7ad8d94a
ghktndyd <hwasuyong99@naver.com>
2024-10-25 20:09:35 +0900

    feat(OutputView): 최종 우승자를 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,11 +7,13 @@ import racingcar.car.Count;
 
 public class OutputView {
 
+    public static final String FINAL_WINNER_PREFIX = "최종 우승자 : ";
     private static final String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String REQUEST_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String RACING_RESULT_HEADER = "\n실행 결과";
     private static final String RACING_RESULT_BODY = "%s : %s\n";
     private static final String CAR_POSITION_SIGN = "-";
+    public static final String FINAL_WINNERS_SEPARATOR = ", ";
 
     public void requestCarNames() {
         System.out.println(REQUEST_CAR_NAMES_MESSAGE);
@@ -32,6 +34,7 @@ public class OutputView {
 
     public void printWinner(Cars cars) {
         List<String> winners = cars.getWinners();
+        System.out.printf(FINAL_WINNER_PREFIX + String.join(FINAL_WINNERS_SEPARATOR, winners));
     }
 
     private void printCarsNameAndPosition(Cars cars) {
