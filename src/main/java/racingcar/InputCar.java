a669b601eaee3075605fc869941055d6a36bd144
gomin0 <gomin0981113@naver.com>
2024-10-26 00:23:24 +0900

    feat: 시도 횟수 입력 기능 구현
    - 시도 횟수 입력 받는 기능 추가
    - 시도 횟수 유효성 검사 기능 추가

--- src/main/java/racingcar/InputCar.java
+++ src/main/java/racingcar/InputCar.java
@@ -9,6 +9,27 @@ public class InputCar {
     public static List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
+
         return Arrays.asList(input.split(","));
+    }
+
+    public static int readTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String input = Console.readLine();
+        validateTryCount(input);
+
+        return Integer.parseInt(input);
+    }
+
+    private static void validateTryCount(String input) {
+        try {
+            int tryCount = Integer.parseInt(input);
+
+            if (tryCount <= 0) {
+                throw new IllegalArgumentException("시도 횟수는 양의 정수여야 합니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 양의 정수여야 합니다.");
+        }
     }
 }
