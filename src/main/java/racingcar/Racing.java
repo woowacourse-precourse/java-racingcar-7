33a699bcd4315e2ae3eb166170cc6e54634602b1
mjw3723 <mjw3723@naver.com>
2024-10-25 13:52:01 +0900

    refactor: 출력 기능 OutPut 으로 분리

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import racingcar.domain.Car;
+import racingcar.ui.Output;
 import racingcar.util.RacingUtil;
 import racingcar.util.RacingValidator;
 
@@ -20,14 +21,14 @@ public class Racing {
     }
 
     private List<Car> getCarListFromUser() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        Output.printCarNameInput();
         String input = readLine();
         RacingValidator.isCarNameInput(input);
         return RacingUtil.getCarList(input);
     }
 
     private int getMoveCountFromUser() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        Output.printMoveCountInput();
         String input = readLine();
         RacingValidator.isInteger(input);
         return Integer.parseInt(input);
@@ -37,33 +38,13 @@ public class Racing {
         System.out.println("실행 결과");
         for (int i = 0; i < moveCount; i++) {
             RacingUtil.setMoveOrStop(carList);
-            printCarPosition();
+            Output.printCarPosition(carList);
         }
     }
 
     private void getWinner() {
         List<String> winners = RacingUtil.findWinner(carList);
-        printWinners(winners);
-    }
-
-    private void printCarPosition() {
-        for (Car car : carList) {
-            System.out.print(car.getName() + " : ");
-            barFromCarPosition(car.getPosition());
-        }
-        System.out.println();
-    }
-
-    private void barFromCarPosition(int position) {
-        for (int i = 0; i < position; i++) {
-            System.out.print("-");
-        }
-        System.out.println();
-    }
-
-    private void printWinners(List<String> winners) {
-        System.out.print("최종 우승자 : ");
-        System.out.println(String.join(", ", winners));
+        Output.printWinners(winners);
     }
 
 }
---
+++ src/main/java/racingcar/ui/Output.java
@@ -0,0 +1,36 @@
+package racingcar.ui;
+
+import racingcar.domain.Car;
+
+import java.util.List;
+
+public class Output {
+
+    public static void printCarNameInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void printMoveCountInput() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+    public static void printCarPosition(List<Car> carList) {
+        for (Car car : carList) {
+            System.out.print(car.getName() + " : ");
+            barFromCarPosition(car.getPosition());
+        }
+        System.out.println();
+    }
+
+    public static void barFromCarPosition(int position) {
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.print("최종 우승자 : ");
+        System.out.println(String.join(", ", winners));
+    }
+}
