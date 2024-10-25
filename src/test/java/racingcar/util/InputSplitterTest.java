1fa0d015bdbf2f848b7f17f2230b29a90f4104ba
변우영 <likevangogh24@gmail.com>
2024-10-25 14:55:11 +0900

    test(InputSplitter): 입력값을 구분자로 분리하는 테스트 코드 추가

---
+++ src/test/java/racingcar/util/InputSplitterTest.java
@@ -0,0 +1,26 @@
+package racingcar.util;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputSplitterTest {
+
+    @Test
+    @DisplayName("입력값을 쉼표(,)를 기준으로 분리한다.")
+    void splitByComma() {
+        //given
+        String input = "abc,def,ghi";
+
+        //when
+        List<String> splitInput = InputSplitter.splitByComma(input);
+        List<String> expectedInput = List.of("abc", "def", "ghi");
+
+        //then
+        Assertions.assertThat(expectedInput).hasSameElementsAs(splitInput);
+    }
+}
