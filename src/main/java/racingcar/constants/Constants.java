8500ca0a5909f381574d9d3975392fdf73bd4ec5
yush <yush2202@gmail.com>
2024-10-25 19:59:48 +0900

    style($ Constants): 상수 공백 추가
    애플리케이션 사양에 맞게 공백 추가
    before:
    - ":"
    
    after:
    - " : "

--- src/main/java/racingcar/constants/Constants.java
+++ src/main/java/racingcar/constants/Constants.java
@@ -3,6 +3,6 @@ package racingcar.constants;
 public class Constants {
     public static final String DELIMITER = ",";
     public static final String DASH = "-";
-    public static final String COLON = ":";
-    
+    public static final String COLON = " : ";
+
 }
