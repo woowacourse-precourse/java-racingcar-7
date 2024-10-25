a311afd45194148fcab8fddbaafe332ea74075bb
HyeonJeong <wgwjh05169@gmail.com>
2024-10-25 21:05:23 +0900

    feat(Reader): 자동차 입력 유효성 검사

--- src/main/java/racingcar/Reader.java
+++ src/main/java/racingcar/Reader.java
@@ -9,11 +9,26 @@ public class Reader {
 
     public static List<String> readCarNames() {
         String input = Console.readLine();
+        verifyNameInput(input);
+
         List<String> carNames = Arrays.asList(input.split(","));
+        verifyCarNames(carNames);
 
         Console.close();
 
         return carNames;
+    }
+
+    private static void verifyNameInput(String input) {
+        if (input == null || input.isBlank())
+            throw new IllegalArgumentException("자동차의 이름은 null이나 공백, 빈 칸일 수 없습니다.");
+    }
+
+    private static void verifyCarNames(List<String> carNames) {
+        for (String name: carNames) {
+            if (name.length() > 5)
+                throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
+        }
     }
 
     public static int readNumber() {
