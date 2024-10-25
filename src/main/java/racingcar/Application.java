9dbbe8a2aad3fa767bbec302def2643ce902b8dd
Ju-Hyun-J <jujung967@gmail.com>
2024-10-25 14:30:25 +0900

    자동차 구분과 잘못입력 시 예외처리 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashMap;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String RaceCarNames = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int RaceCount = Integer.parseInt(Console.readLine());
+
+
+        String[] CarNames = RaceCarNames.split(",");
+        HashMap<String, Integer> carRaceSituation = new HashMap<>();
+        for (int i = 0; i < CarNames.length; i++) {
+            if (CarNames[i].length() > 5) {
+                throw new IllegalArgumentException();
+            }
+            carRaceSituation.put(CarNames[i], 0);
+        }
+
     }
 }
