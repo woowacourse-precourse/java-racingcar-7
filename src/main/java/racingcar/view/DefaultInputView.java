6135d0a5b95f179ffa9fa58a5f61b0e15e787db1
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-26 00:26:53 +0900

    feat($DefaultInputView): 기본 입력 뷰 클래스 추가
    - 기본적으로 입력을 받는 클래스.
    - InputView 를 구현한다.

---
+++ src/main/java/racingcar/view/DefaultInputView.java
@@ -0,0 +1,20 @@
+package racingcar.view;
+
+import racingcar.util.io.InputConsole;
+
+import java.util.List;
+
+public class DefaultInputView implements InputView {
+
+    public List<String> getCarNames() {
+        return List.of(InputConsole.readLine().split(","));
+    }
+
+    public int getTryCount() {
+        try {
+            return Integer.parseInt(InputConsole.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("유효하지 않은 시도 횟수입니다.");
+        }
+    }
+}
