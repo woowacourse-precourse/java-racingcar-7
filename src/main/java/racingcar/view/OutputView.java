98d6d2dfcfedeb9ec528d084c9d3eba94f5e6d3b
codrin2 <hssang127@gmail.com>
2024-10-26 01:32:38 +0900

    feat(OutputView): 출력 로직을 담당하는 OutputView 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,25 @@
+package racingcar.view;
+
+import racingcar.model.Car;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OutputView {
+    public void printRaceRound(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    public void printWinners(List<Car> winners) {
+        String winnerNames = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println("최종 우승자 : " + winnerNames);
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.println("오류: " + message);
+    }
+}
