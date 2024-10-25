b99f3f6be5d1475cd9b264923c067e42860e1a0c
world-dv <goodforx0147@naver.com>
2024-10-25 20:56:10 +0900

    feat: 자동차 이름 검증
    - 자동차 이름의 길이를 판단해 5자 이하로 제한하기 위해 예외처리를 합니다.
    - 자동차 이름이 5자 이하라면 자동차 객체를 생성합니다.
    - 자동차 이름이 5자 초과라면 예외를 발생시킵니다.

--- src/main/java/racingcar/context/Car.java
+++ src/main/java/racingcar/context/Car.java
@@ -1,6 +1,7 @@
 package racingcar.context;
 
 import racingcar.strategy.CarStrategy;
+import racingcar.validation.CarNameValidation;
 
 public class Car {
     private final String carName;
@@ -8,6 +9,9 @@ public class Car {
     private CarStrategy carStrategy;
 
     public Car(String carName, CarStrategy carStrategy) {
+        if (CarNameValidation.validateCarNameLength(carName)) {
+            throw new IllegalArgumentException("자동차 이름 예외");
+        }
         this.carName = carName;
         this.carMovementDistance = 0;
         this.carStrategy = carStrategy;
@@ -17,6 +21,14 @@ public class Car {
         if (carStrategy.move()) {
             carMovementDistance++;
         }
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public Integer getCarMovementDistance() {
+        return carMovementDistance;
     }
 
     //결과 출력 - 자동차 이름 : -
