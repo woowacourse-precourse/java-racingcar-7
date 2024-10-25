115e68d65897618bcfa5b57bdaa8127e37ea7c5d
Chaewon <corndots@naver.com>
2024-10-26 05:17:05 +0900

    feat(Application):try-catch를 제거, IllegalArgumentException이 발생 시 상위로 전달

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,11 +4,7 @@ import racingcar.controller.RacingCarController;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            RacingCarController controller = new RacingCarController();
-            controller.startGame();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        RacingCarController controller = new RacingCarController();
+        controller.startGame();
     }
 }
