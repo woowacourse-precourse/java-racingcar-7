affdd7c2ac1a51629f0f0d12435e2490af873e56
bowook <mbosx316@naver.com>
2024-10-25 22:04:56 +0900

    test(WinnerTest) : WinnerTest 테스트
    - 우승자가 여러명이 나오는지 테스트

--- src/test/java/racingcar/domain/WinnerTest.java
+++ src/test/java/racingcar/domain/WinnerTest.java
@@ -1,2 +1,28 @@
-package racingcar.domain;public class WinnerTest {
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.validator.CarNameValidator;
+import racingcar.domain.validator.TrialCountsValidator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinnerTest {
+
+    @Test
+    void 다수의_우승자_테스트() {
+        // given
+        CarNameValidator carNameValidator = new CarNameValidator();
+        Cars cars = new Cars("bo,wook", carNameValidator);
+        TrialCountsValidator trialCountsValidator = new TrialCountsValidator();
+        TrialCounts trialCounts = new TrialCounts("5", trialCountsValidator);
+
+        // when
+        for (int i = 0; i < trialCounts.getTrialCounts(); i++) {
+            cars.getCars().forEach(car -> car.move(4));
+        }
+        Winner winner = new Winner(cars);
+
+        //then
+        assertThat(winner.getWinners().size()).isEqualTo(2);
+    }
 }
