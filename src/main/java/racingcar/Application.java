3f94819a7f00dbc7eb93b7ddf030ea173b32964d
revist8956 <lljs1113@naver.com>
2024-10-25 20:15:43 +0900

    feat(class RacingCar): implement class RacingCar
    New fields and methods for class RacingCar.
    - getter for name and moves.
    - add moves one by one.
    - decide if cars can move.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,42 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+class RacingCar {
+    private String name;
+    private int moves;
+
+    public RacingCar(String name) {
+        this.name = name;
+        this.moves = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMoves() {
+        return moves;
+    }
+
+    public void addMoves() {
+        moves++;
+    }
+
+    public boolean canMove() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= 4) {
+            return true;
+        }
+        return false;
+    }
+}
+
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
 }
