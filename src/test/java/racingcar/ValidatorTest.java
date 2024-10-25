e69b1c7ec1aef5204278ba93c961d308d9cfa071
Smith <125195487+sigmaith@users.noreply.github.com>
2024-10-25 12:29:03 +0900

    test(Validator): 자동차 이름, 시도 횟수 검증 테스트

---
+++ src/test/java/racingcar/ValidatorTest.java
@@ -0,0 +1,64 @@
+package racingcar;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.utils.Validator;
+
+class ValidatorTest {
+    @Test
+    @DisplayName("유효한 자동차이름 배열 테스트")
+    void testValidCarNames() {
+        // Given: 유효한 자동차 이름 문자열
+        String validCarNames = "car1,car2,car3";
+
+        // When: 자동차 이름 검증 메서드 실행
+        List<String> names = assertDoesNotThrow(() -> Validator.validateCarNames(validCarNames));
+
+        // Then: 변형된 배열이 null이 아니고, 올바른 길이를 가지고 있어야 함.
+        assertNotNull(names);
+        assertEquals(3, names.size());
+    }
+
+    @DisplayName("유효하지 않은 자동차이름 배열 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"car123456,car", ",,"})
+    void testInvalidCarNames(String names) {
+        // Given: 유효하지 않은 자동차 이름 문자열
+        // When: 자동차 이름 검증 메서드 실행
+        // Then: IllegalArgumentException 발생해야 함.
+        assertThatThrownBy(() -> Validator.validateCarNames(names))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("유효한 시도 횟수 테스트")
+    void testValidAttemptCount() {
+        // Given: 유효한 시도 횟수
+        String validAttemptCount = "5";
+
+        // When: 시도 횟수 검증 메서드 실행
+        int count = assertDoesNotThrow(() -> Validator.validateAttemptCount(validAttemptCount));
+
+        // Then: 반환된 횟수가 입력값과 동일해야 함.
+        assertEquals(5, count);
+    }
+
+    @DisplayName("유효하지 않은 시도 횟수 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"-200", "0"})
+    void testInvalidAttemptCount(String attempt) {
+        // Given: 유효하지 않은 시도 횟수
+        // When: 시도 횟수 검증 메서드 실행
+        // Then: IllegalArgumentException 발생해야 함.
+        assertThatThrownBy(() -> Validator.validateAttemptCount(attempt))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
