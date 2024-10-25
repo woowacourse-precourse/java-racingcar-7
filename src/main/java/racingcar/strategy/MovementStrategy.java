165675ca975c888f5e67efe55b3153f9c0b03dc0
world-dv <goodforx0147@naver.com>
2024-10-25 11:01:12 +0900

    feat: 무작위 값이 4 이상일 경우 판단
    - 가독성을 높이기 위해 숫자를 매직 넘버 처리합니다.
    - 자동차를 움직일 지 판단하기 위해 무작위 값이 4 이상일 경우 true를 반환하고, 아닐 경우 false를 반환합니다.
    - 무작위 값 생성 메서드는 클래스 내부에서만 사용되므로 접근 제어자를 private로 수정합니다.

--- src/main/java/racingcar/strategy/MovementStrategy.java
+++ src/main/java/racingcar/strategy/MovementStrategy.java
@@ -3,14 +3,16 @@ package racingcar.strategy;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class MovementStrategy implements CarStrategy {
+    private static final Integer START_RANDOM_NUMBER = 0;
+    private static final Integer END_RANDOM_NUMBER = 9;
+    private static final Integer MOVEMENT_CRITERION_NUMBER = 4;
+
     @Override
     public boolean move() {
-        //자동차 이동 로직 구현
-        //랜덤 수가 4이상이면 true 반환
-        return true;
+        return getRandomNumber() >= MOVEMENT_CRITERION_NUMBER;
     }
 
-    public Integer getRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+    private Integer getRandomNumber() {
+        return Randoms.pickNumberInRange(START_RANDOM_NUMBER, END_RANDOM_NUMBER);
     }
 }
