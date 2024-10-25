f5ef997d8d8bae0d14cf22998bcc0b735b76082c
Hyso <hsk7953@gmail.com>
2024-10-26 02:57:20 +0900

    feat : StringConvertor 유틸리티 클래스 구현 - 문자열을 숫자로 만드는 기능 구현 - 문자열을 쉼표로 구분하여 List로 만드는 기능 구현

---
+++ src/main/java/utils/StringConvertor.java
@@ -0,0 +1,18 @@
+package utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringConvertor {
+    private StringConvertor(){
+    }
+
+    public static int toInt(String input){
+        return Integer.parseInt(input);
+    }
+
+    public static List<String> toList(String input){
+        return Arrays.asList(input.split(","));
+    }
+}
+
