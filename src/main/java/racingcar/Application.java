c45065396dd81507538c275cc2b93c03e9383f86
hanwool <hanwe92@gmail.com>
2024-10-25 11:07:32 +0900

    fix: move parse logic to InputView

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,19 +3,14 @@ package racingcar;
 import java.util.List;
 import racingcar.controller.RacingCarController;
 import racingcar.domain.Cars;
-import racingcar.util.InputParser;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        String nameString = InputView.getCarNames();
-        List<String> carNames = InputParser.nameParse(nameString);
-
+        List<String> carNames = InputView.getCarNames();
         Cars cars = new Cars(carNames);
-
-        String numberString = InputView.getTryNumber();
-        int tryNumber = InputParser.tryNumberParse(numberString);
+        int tryNumber = InputView.getTryNumber();
 
         RacingCarController racingCarController = new RacingCarController();
         String winners = racingCarController.start(cars, tryNumber);
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,16 +2,20 @@ package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.List;
+import racingcar.util.InputParser;
+
 public class InputView {
-    public static String getCarNames() {
+    public static List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return readLine();
+        String nameString = readLine();
+        return InputParser.nameParse(nameString);
     }
 
-    public static String getTryNumber() {
+    public static int getTryNumber() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        String input = readLine();
+        String numberString = readLine();
         System.out.println(); // 기능요구 사항에 따른 줄바꿈 용도
-        return input;
+        return InputParser.tryNumberParse(numberString);
     }
 }
