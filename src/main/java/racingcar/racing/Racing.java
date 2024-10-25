e5cda11b152d9dc486cd5625be852b359c8d3c51
rjswjddn <rjswjddn@gmail.com>
2024-10-25 22:09:36 +0900

    feat: 자동차 검증

--- src/main/java/racingcar/racing/Racing.java
+++ src/main/java/racingcar/racing/Racing.java
@@ -10,13 +10,32 @@ import racingcar.view.Request;
 public class Racing {
 
     final String CAR_NAME_DELIMITER = ",";
+    final int MAX_CAR_NAME = 5;
 
-    public List<Car> createCar() {
+    public void start() {
+        List<String> carNameList = inputCars();
+        validateCars(carNameList);
+        List<Car> carList = createCars(carNameList);
+    }
+
+    public List<String> inputCars() {
         String carNames = Request.inputCar();
-        List<String> carNameList = List.of(carNames.split(CAR_NAME_DELIMITER));
+        return List.of(carNames.split(CAR_NAME_DELIMITER));
+    }
+
+    public void validateCars(List<String> carNameList) {
+        for(String name : carNameList) {
+            if(name.length() > MAX_CAR_NAME) {
+                throw new IllegalArgumentException("자동차 이름이 5자를 넘습니다.");
+            }
+        }
+    }
+
+    public List<Car> createCars(List<String> carNameList) {
 
         return carNameList.stream()
                 .map(Car::new)
                 .collect(Collectors.toList());
     }
+
 }
