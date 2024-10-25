02e85f4a12eb018ee6e538bf796e23863d6ce2fc
unh6unh6 <ruddbs3308@naver.com>
2024-10-25 18:09:46 +0900

    feat(controller): 입력받은 자동차 이름대로 자동차들을 생성하고 저장한다

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,20 +1,33 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
+import racingcar.model.Cars;
 import racingcar.view.InputView;
 
 public class RacingCarController {
     private final InputView inputView;
+    private final Cars cars;
 
-    public RacingCarController(InputView inputView) {
+    public RacingCarController(InputView inputView, Cars cars) {
         this.inputView = inputView;
+        this.cars = cars;
     }
 
     public void start() {
-
+        initializeCars();
     }
 
-    private void initializeRacing() {
+    private void initializeCars() {
         inputView.printCarNameInputGuide();
         String carNames = inputView.getCarNameInput();
+        registerCars(carNames);
+    }
+
+    private void registerCars(String carNames) {
+        String[] carNamesArray = carNames.split(",");
+        for (String carName : carNamesArray) {
+            Car car = new Car(carName);
+            cars.saveCar(car);
+        }
     }
 }
