5bfe14da88700ea11dab33e6370197aec017fdef
dltmddus <tmddus4671@gmail.com>
2024-10-25 20:46:58 +0900

    feat: 자동차 이름 검증 로직 validate()로 묶어서 처리

--- src/main/java/racingcar/validation/CarNameValidation.java
+++ src/main/java/racingcar/validation/CarNameValidation.java
@@ -1,10 +1,18 @@
 package racingcar.validation;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class CarNameValidation {
     public static void validate(String input) {
+        validateNotNullOrEmpty(input);
+        validateIncludeComma(input);
 
+        validateCarName(input);
+        // 쉼표 기준으로 split -> size 1이면 예외처리
+        // 빈문자열이면 무시
+        // 띄어쓰기 없애고
+        // 5글자 이하인지
     }
 
     private static void validateNotNullOrEmpty(String input) {
@@ -19,9 +27,20 @@ public class CarNameValidation {
         }
     }
 
-    private static void validateNameLength(Set<String> inputs) {
-        if (inputs.stream().anyMatch(input -> input.length() > 5)) {
+    private static void validateCarNameLength(String input) {
+        if (input.length() > 5 || input.isEmpty()) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateCarName(String input) {
+        String[] carNames = input.replaceAll("\\s+", "").split(","); // 공백 처리 완료
+
+        Set<String> carNamesSet = new HashSet<>();
+
+        for (String carName : carNames) {
+            validateCarNameLength(carName);
+            carNamesSet.add(carName);
         }
     }
 
