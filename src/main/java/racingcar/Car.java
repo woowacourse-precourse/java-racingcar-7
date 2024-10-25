ff66534da3646308ab484402a6360ac5b0b4a54a
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-10-25 11:00:11 +0900

    Style add detail error message

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -9,13 +9,12 @@ public class Car {
         this(name, 0);
     }
 
-    // TODO: 예외 메시지 처리
     public Car(String name, int position) {
         if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(String.format("자동차 이름은 null 이거나 빈 칸일 수 없습니다. 입력된 이름: (%s)", name));
         }
         if (name.length() > 5) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(String.format("자동차 이름은 쉼표(,)를 기준으로 5글자이하로만 가능합니다. 입력된 이름: (%s)", name));
         }
         this.name = name.trim();
         this.position = position;
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -13,8 +13,10 @@ class CarTest {
     @DisplayName("자동차의 이름이 5글자를 초과하는 경우 예외를 반환하는 걸 테스트하라")
     @Test
     void test1() {
-        assertThatThrownBy(() -> new Car("다섯글자가넘는차"))
-                .isInstanceOf(IllegalArgumentException.class);
+        String name = "다섯글자가넘는차";
+        assertThatThrownBy(() -> new Car(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format("자동차 이름은 쉼표(,)를 기준으로 5글자이하로만 가능합니다. 입력된 이름: (%s)", name));
     }
 
     @DisplayName("자동차의 이름이 null 이거나 빈 경우 예외를 반환하는 걸 테스트하라")
@@ -22,7 +24,8 @@ class CarTest {
     @NullAndEmptySource
     void test2(String name) {
         assertThatThrownBy(() -> new Car(name))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format("자동차 이름은 null 이거나 빈 칸일 수 없습니다. 입력된 이름: (%s)", name));
     }
 
     @DisplayName("차가 전진하는 경우 포지션이 1증가하는 것을 테스트하라")
