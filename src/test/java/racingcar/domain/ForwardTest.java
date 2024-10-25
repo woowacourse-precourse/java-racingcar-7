c6a21d0f9a9b72e9b1f5eb098bbbb77bbdb89a44
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-25 16:36:04 +0900

    test(ForwardTest): 전진을 시도하는 횟수에 문자가 입력된 경우 예외가 발생하는 테스트 코드 작성
    해당 테스트는 실패

---
+++ src/test/java/racingcar/domain/ForwardTest.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ForwardTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", ";", "15a"})
+    @DisplayName("전진을 시도하는 횟수에 숫자가 아닌 문자가 들어오면 예외가 발생한다.")
+    void validateNumber(String candidate) {
+        assertThatThrownBy(() -> Forward.from(candidate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("전진을 시도할 횟수는 숫자만 입력 가능합나다.");
+    }
+
+}
