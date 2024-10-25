a0a11de6d00d3950e73a6df0a8432e20219e3ab3
SeuneKeun <eiziong1@gmail.com>
2024-10-26 02:08:27 +0900

    feat: 사용자에게서 시도할 횟수를 입력받기 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,5 +6,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String cars = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String tryTimes = Console.readLine();
     }
 }
