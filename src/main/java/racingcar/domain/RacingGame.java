0491d48b3bbd96555ff366dfb1b93ae3e8aca130
PSW99 <steamdayz@naver.com>
2024-10-25 17:56:32 +0900

    feat(domain): implement racing game and movement rules - Added RacingGame class to manage race progress - Implemented RacingRule for defining car movement logic

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,4 +1,38 @@
 package racingcar.domain;
 
+import racingcar.util.RandomGenerator;
+import racingcar.util.RandomGeneratorImpl;
+
+import java.util.List;
+
 public class RacingGame {
+    private List<Car> carList;
+    private final RandomGenerator randomGenerator;
+    private final RacingRule racingRule;
+
+    public RacingGame() {
+        this.randomGenerator = new RandomGeneratorImpl();
+        this.racingRule = new RacingRule();
+    }
+
+    public void setCars(List<Car> carList) {
+
+        this.carList = carList;
+    }
+
+    public List<Car> getCarList() {
+        return carList;
+    }
+
+    public void play() {
+        for (Car car : carList) {
+            moveCar(car);
+        }
+    }
+
+    private void moveCar(Car car) {
+        int randomNumber = randomGenerator.generateRandomNumber();
+        car.move(randomNumber, racingRule);
+    }
+
 }
--- src/main/java/racingcar/domain/RacingRule.java
+++ src/main/java/racingcar/domain/RacingRule.java
@@ -1,4 +1,9 @@
 package racingcar.domain;
 
 public class RacingRule {
+    private static final int MINIMUM_ADVANCE_VALUE = 4;
+
+    public boolean cadAdvance(int randomValue) {
+        return randomValue >= MINIMUM_ADVANCE_VALUE;
+    }
 }
