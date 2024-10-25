1c6b72992e3490f409a7d5cc5c922ecabb513575
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-10-26 05:27:40 +0900

    feat: 자동차 이동 기능 구현
    자동차 이동 기능 구현,
    자동차가 이동 조건을 만족했는지 확인하는 기능 구현

--- src/main/java/racingcar/io/domain/Car.java
+++ src/main/java/racingcar/io/domain/Car.java
@@ -5,6 +5,7 @@ public class Car {
 	private static final int CAR_NAME_MAX_LENGTH = 5;
 	private static final String OVER_FLOW_CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 5자 이하만 가능합니다.";
 	private static final int INITIAL_MOVE_DISTANCE = 0;
+	private static final int MOVE_CONDITION = 4;
 
 	private final String name;
 	private int moveDistance;
@@ -25,6 +26,16 @@ public class Car {
 
 	public static Car create(String name) {
 		return new Car(name, INITIAL_MOVE_DISTANCE);
+	}
+
+	public void move(int randomValue) {
+		if(isPossibleToMove(randomValue)) {
+			moveDistance++;
+		}
+	}
+
+	private static boolean isPossibleToMove(int randomValue) {
+		return randomValue > MOVE_CONDITION;
 	}
 
 }
