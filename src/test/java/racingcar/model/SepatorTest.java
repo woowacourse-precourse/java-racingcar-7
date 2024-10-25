2134b0268ada560414e87f218d8f1c91ab685836
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-10-25 20:52:56 +0900

    test(model): 자동차 이름이 없는 경우 테스트
    - 4가지 예시로 테스트

--- src/test/java/racingcar/model/SepatorTest.java
+++ src/test/java/racingcar/model/SepatorTest.java
@@ -1,16 +1,16 @@
 package racingcar.model;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import racingcar.constant.Message;
 
-import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.List;
-
 
 class SepatorTest {
 
     @Test
-    public void 분리기_정상_테스트() throws Exception{
+    public void 분리기_정상_테스트() throws Exception {
         //given
         String input = "pobi,woni, jun";
 
@@ -19,9 +19,30 @@ class SepatorTest {
         List<Car> cars = sepator.separateCars(input);
 
         //then
-        Assertions.assertEquals(cars.size(), 3);
-        Assertions.assertEquals(cars.get(0).getName(), "pobi");
-        Assertions.assertEquals(cars.get(1).getName(), "woni");
-        Assertions.assertEquals(cars.get(2).getName(), " jun");
+        assertEquals(cars.size(), 3);
+        assertEquals(cars.get(0).getName(), "pobi");
+        assertEquals(cars.get(1).getName(), "woni");
+        assertEquals(cars.get(2).getName(), " jun");
     }
+
+    @Test
+    public void 자동차_이름이_없는_경우_테스트() throws Exception {
+        //given
+        String input1 = "pobi,";
+        String input2 = ",woni";
+        String input3 = "pobi,,jun";
+        String input4 = "";
+
+
+        //when
+        Sepator sepator = new Sepator();
+
+        //then
+        assertThrows(IllegalArgumentException.class, () -> sepator.separateCars(input1), Message.CAR_NAME_EMPTY_VALIDATION_MESSAGE.getMessage());
+        assertThrows(IllegalArgumentException.class, () -> sepator.separateCars(input2), Message.CAR_NAME_EMPTY_VALIDATION_MESSAGE.getMessage());
+        assertThrows(IllegalArgumentException.class, () -> sepator.separateCars(input3), Message.CAR_NAME_EMPTY_VALIDATION_MESSAGE.getMessage());
+        assertThrows(IllegalArgumentException.class, () -> sepator.separateCars(input4), Message.CAR_NAME_EMPTY_VALIDATION_MESSAGE.getMessage());
+
+    }
+
 }
