eec93913be8d8b11f9e6852cb58692efbcd30d97
minzzun99 <skg06123@naver.com>
2024-10-26 00:22:36 +0900

    feat(InputView): Null 값과 입력이 없을 경우 예외 발생 로직 추가
    - Null이 입력될 경우 IllegalArgumentException 발생
    - 입력된 값이 없을 경우 IllegalArgumentException 발생

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,10 +1,29 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class InputView {
     public static String printInputMessage(String message) {
         System.out.println(message);
-        return Console.readLine();
+
+        return getInput();
+    }
+
+    private static String getInput() {
+        try {
+            String input = Console.readLine();
+            validateInput(input);
+
+            return input;
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("Null이 입력되었습니다.");
+        }
+    }
+
+    private static void validateInput(String input) {
+        if (input.trim().isEmpty()) {
+            throw new IllegalArgumentException("입력된 값이 없습니다.");
+        }
     }
 }
