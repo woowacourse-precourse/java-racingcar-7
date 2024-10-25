6febff4f946312247d815643212677e63a0a77e5
dradnats1012 <herjebi1012@gmail.com>
2024-10-25 20:19:30 +0900

    feat: 자동차 공장 클래스 완성

--- src/main/java/racingcar/model/CarFactory.java
+++ src/main/java/racingcar/model/CarFactory.java
@@ -1,4 +1,28 @@
 package racingcar.model;
 
+import java.util.List;
+
+import racingcar.model.generator.NumberGenerator;
+
 public class CarFactory {
+
+    private final NumberGenerator numberGenerator;
+    private final List<String> names;
+    private List<Car> cars;
+
+    public CarFactory(List<String> names, NumberGenerator numberGenerator){
+        this.names = names;
+        this.numberGenerator = numberGenerator;
+        makeCars();
+    }
+
+    private void makeCars(){
+        for(String name : names){
+            cars.add(new Car(name, numberGenerator));
+        }
+    }
+
+    public List<Car> getCars(){
+        return cars;
+    }
 }
