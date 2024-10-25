360452ed63015020a58a6f8139a4a1cef33012fc
GS_song98 <20003204@sju.ac.kr>
2024-10-25 20:07:13 +0900

    test: 이름 생성 성공, 예외 케이스 추가

---
+++ src/test/java/racingcar/domain/NameTest.java
@@ -0,0 +1,41 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class NameTest {
+
+    @Nested
+    @DisplayName("Name 객체를 생성할 떄")
+    class whenCreateName {
+
+        @Test
+        @DisplayName("5글자 이하의 이름이 들어오면 성공적으로 생성한다.")
+        void successfullyCreateName() {
+            //given
+            String invalidName = "song";
+            String expected = "song";
+
+            //when then
+            Name name = assertDoesNotThrow(() -> new Name(invalidName));
+            assertThat(name.toString()).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("5글자 이상의 이름이 들어오면 예외를 반환한다.")
+        void throwsExceptionWhenCreateNameLongerThanFive() {
+            //given
+            String invalidName = "SongGwanSeok";
+
+            //when then
+            assertThatThrownBy(() -> new Name(invalidName))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("이름은 5자를 초과할 수 없습니다.");
+        }
+    }
+}
