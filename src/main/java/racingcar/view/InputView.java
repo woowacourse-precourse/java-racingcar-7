bd47da4df376634edd8d1e764851187ee032ec7a
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-25 14:28:42 +0900

    feat: InputView 클래스 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,25 @@
+package racingcar.view;
+
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+
+    public static String INPUT_CARNAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static String INPUT_GAME_TOKENS_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
+    public List<String> inputCarnames() {
+        System.out.println(INPUT_CARNAME_MESSAGE);
+        String inputLines = readLine();
+        return Arrays.asList(inputLines.split(","));
+    }
+
+    public int inputGameTokens() {
+        System.out.println(INPUT_GAME_TOKENS_MESSAGE);
+        return Integer.parseInt(readLine());
+    }
+}
