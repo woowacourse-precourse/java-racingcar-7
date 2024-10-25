61011ad83933e5e462654a1ec7c2d0bd46612980
mingyeongbae <bmg1203@naver.com>
2024-10-25 21:17:14 +0900

    [refactor]: 경주 자동차 입력 시 공백 제거 추가
    경주 자동차 입력 문자열에 공백이 포함 되었을 시에는 공백을 모두 제거해주도록 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -29,7 +29,7 @@ public class Application {
 
     public static void inputAndValidate() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        List<String> carNameListInput = List.of(Console.readLine().trim().split(","));
+        List<String> carNameListInput = List.of(Console.readLine().replaceAll(" ", "").split(","));
         System.out.println("시도할 횟수는 몇 회인가요?");
         String attemptCountInput = Console.readLine();
 
