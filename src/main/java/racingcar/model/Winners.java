a683fa6da42200b9221e3e19f69a86cb9386b904
joshinhyeok <shtree07@naver.com>
2024-10-25 23:05:08 +0900

    refactor: Winners 클래스 내부 메서드 가독성 개선

--- src/main/java/racingcar/model/Winners.java
+++ src/main/java/racingcar/model/Winners.java
@@ -5,13 +5,21 @@ import java.util.List;
 
 public class Winners {
     public static List<Car> findWinner(List<Car> cars) {
+        int maxPosition = findMaxPosition(cars);
+        return findCarsWithMaxPosition(cars, maxPosition);
+    }
+
+    private static int findMaxPosition(List<Car> cars) {
         int maxPosition = 0;
         for (Car car : cars) {
             if (car.getPosition() > maxPosition) {
                 maxPosition = car.getPosition();
             }
         }
+        return maxPosition;
+    }
 
+    private static List<Car> findCarsWithMaxPosition(List<Car> cars, int maxPosition) {
         List<Car> winners = new ArrayList<>();
         for (Car car : cars) {
             if (car.getPosition() == maxPosition) {
