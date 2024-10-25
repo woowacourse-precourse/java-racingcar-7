28cd69e373cf6d5444cc64c29db9dd9fe3a03440
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2024-10-25 12:35:02 +0900

    fix: 레이스 진행 테스트에서 불필요한 부분 삭제 및 조건 수정
    - 전진 조건은 항상 성공하므로 관련 테스트를 삭제하였습니다.
    - 자동차 경기 진행 테스트에서 잘못된 조건을 올바르게 수정하였습니다.

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -2,7 +2,6 @@ package racingcar;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
@@ -10,31 +9,19 @@ import org.junit.jupiter.api.Test;
 class RaceTest extends NsTest {
 
     @Test
-    void 전진_조건_실패_테스트() {
-        assertThatThrownBy(() -> {
-            assertRandomNumberInRangeTest(
-                    () -> {
-                        run("pobi,woni", "1");
-                    },
-                    10, 0
-            );
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
     void 자동차_경기_진행_테스트() {
-        runAndAssert(3,3,"pobi,woni", "pobi : ", "woni : ", "최종 우승자 : 없음");
+        runAndAssert(3,3,"pobi,woni", "pobi : ", "woni : ", "최종 우승자 : 무승부");
         runAndAssert(4,3,"car1,car2", "car1 : -", "car2 : ", "최종 우승자 : car1");
-        runAndAssert(3,4,"car1,car2", "car1 : -", "car2 : -", "최종 우승자 : car1, car2");
+        runAndAssert(4,4,"car1,car2", "car1 : -", "car2 : -", "최종 우승자 : car1, car2");
     }
 
-    private void runAndAssert(int aMove,int bMove,String carNames, String expectedAOutput, String expectedBOutput, String expectedWinnerOutput) {
+    private void runAndAssert(int aMove,int bMove,String carNames, String expectedFirstOutput, String expectedSecondOutput, String expectedWinnerOutput) {
         assertRandomNumberInRangeTest(
-                () -> {
-                    run(carNames, "1");
-                    assertThat(output()).contains(expectedAOutput, expectedBOutput, expectedWinnerOutput);
-                },
-                aMove,bMove
+            () -> {
+                run(carNames, "1");
+                assertThat(output()).contains(expectedFirstOutput, expectedSecondOutput, expectedWinnerOutput);
+            },
+            aMove,bMove
         );
     }
 
