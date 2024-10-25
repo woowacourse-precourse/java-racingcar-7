67e8b7315a6310c5cfa7131a3485f593e2e9547d
33jyu33 <jy04169@gmail.com>
2024-10-25 20:57:16 +0900

    feat(Car): validate for blanks in the name
    Function checkBlank:
    - check that blanks in name, and if true, throw Exception
    Function checkDoubleSeparator:
    - check using two separators at the same time
    Function checkLastBlank:
    - check there is a name after the last separator
    Throw Exception if three rules are broken

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,14 @@
 package racingcar;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class Car {
     Map<String, Integer> carMap;
 
     public void set(){
-        separateName(UserView.readCarName());
+        String inputString = UserView.readCarName();
+        separateName(inputString);
     }
 
     public Map<String, Integer> get(){
@@ -14,6 +16,8 @@ public class Car {
     }
 
     private void separateName(String input){
+        checkDoubleSeparator(input);
+        checkLastBlank(input);
         String[] splitInput = input.split(",");
         checkCount(splitInput.length);
         for (String s : splitInput) {
@@ -23,8 +27,8 @@ public class Car {
         }
     }
 
-    private void checkCount(int n){
-        if (n > 5){
+    private void checkCount(int countName){
+        if (countName > 5){
             throwArgumentException("자동차는 최대 5대까지 출전 가능합니다.");
         }
     }
@@ -33,8 +37,25 @@ public class Car {
 
     }
 
-    private void checkBlank(String name){
+    // "a,b,c,,"이면 3개까지만 저장.
+    // "a,b,,c"이면 공백 포함 4개로 저장되므로 이러한 오류 방지.
+    private void checkDoubleSeparator(String input){
+        if (input.contains(",,")){
+            throwArgumentException("구분자는 한 개씩만 사용할 수 있습니다.");
+        }
+    }
 
+    // "a,b,"이면 2개만 저장되는 오류 방지.
+    private void checkLastBlank(String input){
+        if (input.lastIndexOf(",") == input.length()-1){
+            throwArgumentException("구분자 뒤에는 자동차 이름이 지정되어야 합니다.");
+        }
+    }
+
+    private void checkBlank(String name){
+        if (!Objects.equals(name, name.replaceAll("\\s", ""))){
+            throwArgumentException("공백은 자동차의 이름에 포함될 수 없습니다.");
+        }
     }
 
     private void putCarMap(String name){
