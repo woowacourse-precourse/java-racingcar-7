b2cba02ce8c8e6dc52f1d1ed052d1825408f46ff
seophohoho <seop0937@gmail.com>
2024-10-26 01:41:26 +0900

    feat(Validator): 사용자 입력(자동차들) 정규식 작성

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -2,4 +2,5 @@ package racingcar;
 
 public class Validator {
     private final String INPUT_COUNT_PATTERN = "^[1-9][0-9]*";
+    private final String INPUT_CARS_PATTERN = "^[^\\s,]{1,5}(,[^\\s,]{1,5})*";
 }
