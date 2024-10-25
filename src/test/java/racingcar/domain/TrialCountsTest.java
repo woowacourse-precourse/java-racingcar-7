3222f36695d9a22b4c0af4a7c56b1ce68e59dece
bowook <mbosx316@naver.com>
2024-10-25 20:11:13 +0900

    test(TrialCountsTest) : TrialCounts 도메인 테스트
    - 시도 횟수 생성 테스트

--- src/test/java/racingcar/domain/TrialCountsTest.java
+++ src/test/java/racingcar/domain/TrialCountsTest.java
@@ -1,2 +1,22 @@
-package racingcar.domain;public class TrialCountsTest {
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.validator.TrialCountsValidator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TrialCountsTest {
+
+    @Test
+    void 시도_횟수_셋팅_테스트() {
+        //given
+        TrialCountsValidator validator = new TrialCountsValidator();
+        TrialCounts trialCounts = new TrialCounts("5", validator);
+
+        //when
+        int counts = trialCounts.getTrialCounts();
+
+        //then
+        assertThat(counts).isEqualTo(5);
+    }
 }
