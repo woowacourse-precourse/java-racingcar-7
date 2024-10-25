961884f4d273e9a976b5bce766307dcec8f4549f
supernovaMK <minki5336@naver.com>
2024-10-26 02:30:31 +0900

    feat: 우승자를 정하는 기능 구현
    - 레이싱 자동차들 중에서 우승자를 찾아내기 위해 WinnerFinder 클래스를 구현하였습니다.

---
+++ src/main/java/racingcar/model/WinnerFinder.java
@@ -0,0 +1,38 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinnerFinder {
+    RacingCarManager racingCarManager;
+
+    public WinnerFinder(RacingCarManager racingCarManager) {
+        this.racingCarManager = racingCarManager;
+    }
+
+    public String getWinner() {
+        int maxScore = findMaxScore(racingCarManager.getRacingCarList());
+        List<String> winners = addWinners(racingCarManager.getRacingCarList(), maxScore);
+        return String.join(",", winners);
+    }
+
+    private int findMaxScore(List<RacingCar> cars) {
+        int maxScore = 0;
+        for (RacingCar car : cars) {
+            if (car.getLocation() > maxScore) {
+                maxScore = car.getLocation();
+            }
+        }
+        return maxScore;
+    }
+
+    private List<String> addWinners(List<RacingCar> cars, int maxScore) {
+        List<String> winners = new ArrayList<>();
+        for (RacingCar car : cars) {
+            if (car.getLocation() == maxScore) {
+                winners.add(car.getCarName());
+            }
+        }
+        return winners;
+    }
+}
