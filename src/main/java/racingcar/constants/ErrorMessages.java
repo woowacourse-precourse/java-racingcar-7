49723ac1b7afb7d336ef08e77403696a0fa338ba
yush <yush2202@gmail.com>
2024-10-25 17:43:52 +0900

    feat($ ErrorMessages): 에러 메시지 상수 작성
    에러 상황 발생시 반환할 에러 메시지 작성

---
+++ src/main/java/racingcar/constants/ErrorMessages.java
@@ -0,0 +1,21 @@
+package racingcar.constants;
+
+public enum ErrorMessages {
+
+    PREFIX("[ERROR] "),
+    INVALID_USER_INPUT_BLANK("입력은 공백일 수 없습니다."),
+    INVALID_CAR_NAME_BLANK("자동차 이름은 공백일 수 없습니다."),
+    INVALID_CAR_NAME_RANGE("유효한 자동차 이름 길이가 아닙니다."),
+    INVALID_CAR_NAME_DUPLICATED("자동차 이름은 중복될 수 없습니다."),
+    INVALID_TRY_COUNT("입력횟수는 1회 이상이어야 합니다.");
+
+    private final String message;
+
+    ErrorMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return PREFIX.message + message;
+    }
+}
