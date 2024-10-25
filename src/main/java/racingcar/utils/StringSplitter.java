8c81dfde21706758732f9d889f9889b54a85008c
yush <yush2202@gmail.com>
2024-10-25 19:34:42 +0900

    feat($ StringSplitter): 구분자 나누기 유틸 기능 구현
    현재 기본 구분자인 ,로 문자열을 나누는 유틸리티 메서드 기능 구현

---
+++ src/main/java/racingcar/utils/StringSplitter.java
@@ -0,0 +1,12 @@
+package racingcar.utils;
+
+import java.util.Arrays;
+import java.util.List;
+import racingcar.constants.Constants;
+
+public class StringSplitter {
+    public static List<String> splitByDelimiter(String UserInput) {
+        String[] splittedUserInput = UserInput.split(Constants.DELIMITER);
+        return Arrays.stream(splittedUserInput).toList();
+    }
+}
