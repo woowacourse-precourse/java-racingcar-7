da1bd164881da1dd003a08549eeb4ab25fe89f85
Jeongyounghyeon <snorlax1106@naver.com>
2024-10-25 16:06:03 +0900

    refactor(output): 전진 거리 코드 리팩토링
    repeat()을 사용하여 for문을 생략하여 코드 양을 줄이고, 직관적으로 표현

--- src/main/java/racingcar/view/CarRacingView.java
+++ src/main/java/racingcar/view/CarRacingView.java
@@ -9,13 +9,7 @@ public class CarRacingView {
     public static void displayCurrentStatus(List<RacingCar> racingCars) {
         for (RacingCar racingCar : racingCars) {
             int distanceCovered = racingCar.getDistanceCovered();
-
-            String showDistanceCovered = "";
-            for (int i = 0; i < distanceCovered; i++) {
-                showDistanceCovered += "-";
-            }
-
-            System.out.println(racingCar.getName() + " : " + showDistanceCovered);
+            System.out.println(racingCar.getName() + " : " + "-".repeat(distanceCovered));
         }
     }
 
