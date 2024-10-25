06f9c7162f18a29c14ffe8fb05c106b503060fec
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 20:37:07 +0900

    fest: 자동차 이름 길이 검증 기능을 추가한다

--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -1,4 +1,17 @@
 package racingcar.validator;
 
+import java.util.List;
+
 public class CarNameValidator {
+
+    private static final int NAME_MAX_LENGTH = 5;
+
+    public void nameLength(List<String> carsList) {
+        for (String car : carsList) {
+            if (car.isEmpty() || car.length() > NAME_MAX_LENGTH) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
+
