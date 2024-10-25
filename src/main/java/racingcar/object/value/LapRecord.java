bb78a4f74495822b6818ca6c541902e7d36fa141
JeongSu Park <bechalrang@gmail.com>
2024-10-25 17:08:54 +0900

    feat(LapRecord): `랩-기록`에서 1위를 판별하는 행동 추가
    현재 `랩-기록`에서 1위를 판별하여 랩의 `우승자`로 반환하는 메서드`LapRecord#determineFirstPlace) 추가

--- src/main/java/racingcar/object/value/LapRecord.java
+++ src/main/java/racingcar/object/value/LapRecord.java
@@ -8,4 +8,16 @@ public record LapRecord(List<CarRecord> carRecords) {
         carRecords = List.copyOf(carRecords);
     }
 
+    public Winners determineFirstPlace() {
+        Mileage highestMileage = carRecords.stream()
+                .map(CarRecord::mileage)
+                .max(Mileage::compareTo)
+                .orElseThrow(() -> new IllegalArgumentException("랩에는 1위가 반드시 존재해야 합니다."));
+
+        return new Winners(new CarNames(carRecords().stream()
+                .filter(carRecord -> highestMileage.equals(carRecord.mileage()))
+                .map(CarRecord::carName)
+                .toList()));
+    }
+
 }
