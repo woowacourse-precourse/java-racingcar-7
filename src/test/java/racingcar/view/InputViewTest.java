6e3f149a0f265d5565e6cdde51c4028664f34018
songsunkook <songsunkook@gmail.com>
2024-10-25 16:28:37 +0900

    test: 잘못된 값 입력 시 예외처리

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,10 +1,13 @@
 package racingcar.view;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputViewTest {
 
@@ -12,5 +15,26 @@ public class InputViewTest {
     void 자동차_이름을_정상적으로_구분하는가() {
         List<String> names = InputView.splitNames("pobi,woni,jun");
         assertThat(names).isEqualTo(List.of("pobi", "woni", "jun"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,,woni", "pobi,woni,,jun", "pobi,,woni,,jun"})
+    void 자동차_이름_구분자가_연속으로_주어지는_경우_예외(String input) {
+        assertThatThrownBy(() -> InputView.splitNames(input))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "   ", "pobi,", ",pobi"})
+    void 자동차_이름이_빈_문자열인_경우_예외(String input) {
+        assertThatThrownBy(() -> InputView.splitNames(input))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-99", "-1", "예외케이스"})
+    void 시도_횟수가_잘못된_경우(String input) {
+        assertThatThrownBy(() -> InputView.validateTryCount(input))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
