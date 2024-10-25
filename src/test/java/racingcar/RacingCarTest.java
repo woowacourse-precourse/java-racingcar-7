e91f259661cae45f8ed52198913a20d2f0aed960
eb9862 <luclead622@gmail.com>
2024-10-25 15:39:26 +0900

    test : RacingCar 클래스의 move() 메서드에 대한 테스트 코드 추가

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,7 +1,9 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +17,20 @@ class RacingCarTest {
         assertThat(RacingCar.canMoveForward(3)).isFalse();
         assertThat(RacingCar.canMoveForward(4)).isTrue();
         assertThat(RacingCar.canMoveForward(9)).isTrue();
+    }
+
+    @Test
+    @DisplayName("전진 성공 시 전진 횟수는 +1, 아니라면 그대로")
+    void 전진_테스트() {
+        String name = "noname";
+        RacingCar racingCar = new RacingCar(name);
+
+        int notMoveForward = Randoms.pickNumberInRange(0, 3);
+        racingCar.move(notMoveForward);
+        assertEquals(racingCar.getForwardCount(), 0);
+
+        int moveForWard = Randoms.pickNumberInRange(4, 9);
+        racingCar.move(moveForWard);
+        assertEquals(racingCar.getForwardCount(), 1);
     }
 }
