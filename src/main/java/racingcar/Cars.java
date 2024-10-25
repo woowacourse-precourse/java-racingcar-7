b1cf830f65576c0168860665eeb2460da56881f6
gomin0 <gomin0981113@naver.com>
2024-10-26 00:43:57 +0900

    style: 사용하지 않는 import문 제거

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -3,9 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Cars {
