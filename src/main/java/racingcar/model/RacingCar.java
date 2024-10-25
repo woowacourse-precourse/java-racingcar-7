1bbb9037dd5752350798d276f7c0a37b16f2d46d
lejuho <zedd1324@gmail.com>
2024-10-25 21:48:24 +0900

    feat(model) : RacingCar에 게터와 세터 추가, distance 필드 추가
    - 자동차의 이름을 반환하는 getName 메서드 추가
    - 자동차가 지금까지 전진한 거리를 저장하는 distance 필드 추가
    - 자동차가 지금까지 전진한 거리를 반환하는 getDistance 메서드 추가

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -1,4 +1,6 @@
 package racingcar.model;
 
 public interface RacingCar {
+    String getName();
+    Long getDistance();
 }
--- src/main/java/racingcar/model/RacingCarEmb.java
+++ src/main/java/racingcar/model/RacingCarEmb.java
@@ -1,9 +1,20 @@
 package racingcar.model;
 
-public class RacingCarEmb {
+public class RacingCarEmb implements RacingCar {
     private final String name;
+    private final Long distance = 0L;
 
     public RacingCarEmb(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Long getDistance() {
+        return distance;
     }
 }
