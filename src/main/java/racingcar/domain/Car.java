ee5271015dca3a199076d6733cce3a77d0cd046a
PSW99 <steamdayz@naver.com>
2024-10-25 17:37:17 +0900

    feat(domain): add Car class for racing game - Added Car class to represent each car in the race - Managed car name and position

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,39 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(int randomNumber, RacingRule racingRule) {
+        if (racingRule.cadAdvance(randomNumber)) {
+            position++;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
