63a2f7ab0bca35183848a0a73ba41857017c8feb
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-10-25 15:22:27 +0900

    feat(model): 자동차 이름 검증 추가
    - 자동차의 이름을 선언하지 않은 경우
    - 검증 이름 변경(뒤에 MESSAGE 추가)

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -6,16 +6,24 @@ public class Car {
     private int winStack;
 
     public Car(String name) {
+        CarNameEmptyValidator(name);
         CarNameLengthLimitValidator(name);
         this.name = name;
         this.winStack = 0;
     }
 
+    private static void CarNameEmptyValidator(String name) {
+        final String CAR_NAME_EMPTY_VALIDATION_MESSAGE = "자동차에 이름을 부여하지 않았습니다.";
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     private static void CarNameLengthLimitValidator(String name) {
         final int CAR_NAME_LENGTH_LIMIT = 5;
-        final String CAR_NAME_LENGTH_LIMIT_VALIDATION = "자동차 이름은 5자 이하여야 합니다.";
+        final String CAR_NAME_LENGTH_LIMIT_VALIDATION_MESSAGE= "자동차 이름은 5자 이하여야 합니다.";
         if (name.length() > CAR_NAME_LENGTH_LIMIT) {
-            throw new IllegalArgumentException(CAR_NAME_LENGTH_LIMIT_VALIDATION);
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_LIMIT_VALIDATION_MESSAGE);
         }
     }
 }
