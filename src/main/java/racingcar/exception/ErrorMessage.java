ed5323403c1bc64497da1a0894226c363dc6b21e
JS <zks145@naver.com>
2024-10-25 14:51:58 +0900

    feat (exception): 경기 관련 예외 메시지 추가

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -5,7 +5,9 @@ public enum ErrorMessage {
     NULL_EMPTY_FOUND("Null 혹은 공백 문자가 포함되었습니다."),
     OVER_LENGTH_FOUND("자동차 이름 길이가 5를 초과했습니다."),
     REGEX_FOUND("특수 문자가 포함되었습니다."),
-    SAME_NAME_FOUND("동일한 이름의 자동차가 존재합니다.");
+    SAME_NAME_FOUND("동일한 이름의 자동차가 존재합니다."),
+    OVER_GAME_MATCHES("허용된 게임 횟수를 넘겼습니다."),
+    NEGATIVE_GAME_MATCHES("경기 횟수는 음수가 될 수 없습니다.");
 
     private final String message;
 
