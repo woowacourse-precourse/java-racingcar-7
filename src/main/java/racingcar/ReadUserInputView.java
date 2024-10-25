be858ea23355c8c5c48079a5d7fcb2cc3ee84fad
posite <su84053@gmail.com>
2024-10-25 22:44:29 +0900

    feat(UserInputValidator, ErrorMessage): 사용자의 입력 검증 기능 추가, 분리 및 ErrorMessage 구현
    자동차 수에 대한 검증 기능을 구현하였으며 시도 횟수에 대한 조건들을 분리하였습니다. 또한 검증 항목들에 따른 ErrorMessage를 적용하였습니다.

--- src/main/java/racingcar/ReadUserInputView.java
+++ src/main/java/racingcar/ReadUserInputView.java
@@ -1,21 +1,35 @@
 package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static racingcar.ErrorMessage.NOT_NUMBER;
+import static racingcar.ErrorMessage.NOT_POSITIVE_NUMBER;
+import static racingcar.ErrorMessage.ONE_CAR_NAME;
+import static racingcar.ErrorMessage.TOO_BIG_NUMBER;
+import static racingcar.ErrorMessage.TOO_LONG_CAR_NAME;
 import static racingcar.TextString.NAME_SEPARATOR;
 
 public class ReadUserInputView {
     public String[] readRacingCarNames() {
-        var carNames = readLine().split(NAME_SEPARATOR.getValue());
+        var carNames = readLine().split(NAME_SEPARATOR.getText());
+        if (!UserInputValidator.isOneRacingCar(carNames)) {
+            throw new IllegalArgumentException(ONE_CAR_NAME.getMessage());
+        }
         if (!UserInputValidator.isValidRacingCarName(carNames)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(TOO_LONG_CAR_NAME.getMessage());
         }
         return carNames;
     }
 
     public long readTryCount() {
         var tryCount = readLine();
-        if (!UserInputValidator.isValidTryCount(tryCount)) {
-            throw new IllegalArgumentException();
+        if (!UserInputValidator.isNumberTryCount(tryCount)) {
+            throw new IllegalArgumentException(NOT_NUMBER.getMessage());
+        }
+        if (!UserInputValidator.isNotTooBigTryCount(tryCount)) {
+            throw new IllegalArgumentException(TOO_BIG_NUMBER.getMessage());
+        }
+        if (!UserInputValidator.isPositiveTryCount(tryCount)) {
+            throw new IllegalArgumentException(NOT_POSITIVE_NUMBER.getMessage());
         }
         return Long.parseLong(tryCount);
     }
--- src/main/java/racingcar/UserInputValidator.java
+++ src/main/java/racingcar/UserInputValidator.java
@@ -1,6 +1,10 @@
 package racingcar;
 
 public class UserInputValidator {
+    public static boolean isOneRacingCar(String[] carNames) {
+        return carNames.length > 1;
+    }
+
     public static boolean isValidRacingCarName(String[] carNames) {
         for (String carName : carNames) {
             if (carName.length() > 5) {
@@ -10,7 +14,15 @@ public class UserInputValidator {
         return true;
     }
 
-    public static boolean isValidTryCount(String tryCount) {
-        return tryCount.chars().allMatch(Character::isDigit) && tryCount.length() < 20 && Long.parseLong(tryCount) > 0;
+    public static boolean isNumberTryCount(String tryCount) {
+        return tryCount.chars().allMatch(Character::isDigit);
+    }
+
+    public static boolean isNotTooBigTryCount(String tryCount) {
+        return tryCount.length() < 19;
+    }
+
+    public static boolean isPositiveTryCount(String tryCount) {
+        return Long.parseLong(tryCount) > 0;
     }
 }
