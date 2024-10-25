9b3c272d7da5a8fbdfea80850c7e6c4b80e6bab0
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-26 00:53:48 +0900

    refactor($Application): RacingCarController 를 사용하도록 변경
    - 자동차 게임의 로직과 관련된 책임은 Controller 가 진다.
    - Application 은 필요한 의존성을 구성하고 Controller 로 넘기는 역할을 한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,86 +1,15 @@
 package racingcar;
 
-import racingcar.dto.RacingResult;
-import racingcar.model.RacingCars;
-import racingcar.model.dependency.RacingCarDependency;
-import racingcar.model.dependency.moving_strategy.MovingStrategy;
-import racingcar.model.dependency.moving_strategy.random.DefaultRandomMovingStrategy;
-import racingcar.model.dependency.validator.DefaultRacingCarValidator;
-import racingcar.model.dependency.validator.RacingCarValidator;
-import racingcar.service.RacingService;
-import racingcar.view.input.DefaultInputView;
-import racingcar.view.input.InputView;
-import racingcar.view.output.DefaultOutputView;
-import racingcar.view.output.OutputView;
-
-import java.util.List;
+import racingcar.controller.RacingCarController;
+import racingcar.di.DefaultRacingCarTemplate;
+import racingcar.di.RacingCarTemplate;
 
 public class Application {
 
-    private final static InputView inputView = new DefaultInputView();
-    private final static OutputView outputView = new DefaultOutputView();
-
-    private static final RacingCarDependency racingCarDependency = buildRacingCarDependency();
-    private final static RacingService racingService = buildRacingService();
-
-    // 필요 변수
-    private static RacingCars racingCars;
-    private static int tryCount;
+    private static final RacingCarTemplate racingCarTemplate = new DefaultRacingCarTemplate();
+    private static final RacingCarController racingCarController = new RacingCarController(racingCarTemplate);
 
     public static void main(String[] args) {
-        setUp();
-        RacingResult racingResult = playRacing();
-        printRacingResult(racingResult);
-    }
-
-    // core 함수
-    private static void setUp() {
-        setUpRacingCars();
-        setUpTryCount();
-    }
-
-    private static void setUpRacingCars() {
-        List<String> carNameList = getCarNames();
-
-        racingCars = createRacingCars(carNameList);
-    }
-
-    private static void setUpTryCount() {
-        tryCount = getTryCount();
-    }
-
-    private static RacingResult playRacing() {
-        return racingService.play(racingCars, tryCount);
-    }
-
-    private static void printRacingResult(RacingResult racingResult) {
-        outputView.printRacingRoundResults(racingResult.roundResults());
-        outputView.printWinners(racingResult.winners());
-    }
-
-    // helper 함수
-    private static RacingCars createRacingCars(List<String> carNameList) {
-        return racingService.getRacingCars(carNameList);
-    }
-
-    private static List<String> getCarNames() {
-        outputView.printInputCarNames();
-        return inputView.getCarNames();
-    }
-
-    private static int getTryCount() {
-        outputView.printInputTryCount();
-        return inputView.getTryCount();
-    }
-
-    // dependency injection 을 위한 함수
-    private static RacingCarDependency buildRacingCarDependency() {
-        RacingCarValidator racingCarValidator = new DefaultRacingCarValidator();
-        MovingStrategy movingStrategy = new DefaultRandomMovingStrategy();
-        return new RacingCarDependency(racingCarValidator, movingStrategy);
-    }
-
-    private static RacingService buildRacingService() {
-        return new RacingService(racingCarDependency);
+        racingCarController.run();
     }
 }
