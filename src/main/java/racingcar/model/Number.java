bd92fba3f7cb5d4aec36e77c21e3e6193e01e7ef
GiSeok Kang <ime.rokz@gmail.com>
2024-10-25 15:04:53 +0900

    feat: 0이거나 0보다 작은 경우와 value가 비어있는 경우 예외처리 기능 추가
    - 정수 입력 외 예외처리를 메소드로 분리 및 필요없는 변수 제거
    - 양수를 판별하는 예외처리를 메소드로 분리

--- src/main/java/racingcar/model/Number.java
+++ src/main/java/racingcar/model/Number.java
@@ -5,10 +5,29 @@ public class Number {
     private final int value;
 
     public Number(String value) {
+        validateValueIsNotEmpty(value);
+
+        this.value = validateValueIsInteger(value);
+        validateValueIsPositiveNumber();
+    }
+
+    private static void validateValueIsNotEmpty(String value) {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("시도 횟수를 입력하세요.");
+        }
+    }
+
+    private int validateValueIsInteger(String value) {
         try {
-            this.value = Integer.parseInt(value);
+            return Integer.parseInt(value);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("정수만 입력하세요.");
+        }
+    }
+
+    private void validateValueIsPositiveNumber() {
+        if (this.value <= 0) {
+            throw new IllegalArgumentException("양수만 입력하세요.");
         }
     }
 
