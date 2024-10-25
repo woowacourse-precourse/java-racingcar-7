36779f1adabda4eeeee3ee394ab48cde4318eef6
bowook <mbosx316@naver.com>
2024-10-25 18:56:13 +0900

    test(CarTeset) : Car 도메인 테스트
    - move 함수가 정상 동작하는지 테스트

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,2 +1,20 @@
-package racingcar.domain;public class CarTest {
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+
+    @Test
+    void 움직이는지_테스트() {
+        //given
+        Car car = new Car("bo");
+
+        //when
+        car.move();
+
+        //then
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
 }
