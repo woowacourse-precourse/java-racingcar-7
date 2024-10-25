da899c88ca998b5ce9d6e3b8b85a7b7face136c0
dbdb456 <adad568@naver.com>
2024-10-25 15:27:32 +0900

    feat(utils): 랜덤 숫자에 따라 이동 시도하는 메서드 추가

--- src/main/java/racingcar/utils/Utils.java
+++ src/main/java/racingcar/utils/Utils.java
@@ -1,5 +1,6 @@
 package racingcar.utils;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import racingcar.domain.Car;
 
@@ -12,6 +13,12 @@ public class Utils {
     public void saveCarNamesToList(String[] carNames, List<Car> cars) {
         for (String carName : carNames) {
             cars.add(new Car(carName, ""));
+        }
+    }
+
+    public void attemptCarMove(Car car) {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            car.addCount();
         }
     }
 }
