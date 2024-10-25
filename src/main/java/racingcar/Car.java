2ba38d13efa3216415f21c52717812ecb36d1024
wodus4145@naver.com <wodus4145@naver.com>
2024-10-25 14:07:17 +0900

    feat: Creat Car class

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Car {
+    String name;
+    int advance;
+
+    public Car(String name, int advance) {
+        this.name = name;
+        this.advance = advance;
+    }
+}
