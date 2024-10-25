159fe20db91c3c493c1342e656ee5dd717e99ab6
dongyun <ehddbs452100@naver.com>
2024-10-25 20:51:04 +0900

    refactor(service package): split input and validate input
    - 기존 RacingService를 3개의 클래스로 분할하였는데 그중 하나인 입력값을 split 및 검증하는 클래스

---
+++ src/main/java/racingcar/service/RacingInputValidateService.java
@@ -0,0 +1,56 @@
+package racingcar.service;
+
+import static racingcar.global.error.Error.NAME_BLANK_EXCEPION;
+import static racingcar.global.error.Error.NAME_LENGTH_EXCEPION;
+import static racingcar.global.error.Error.TRY_COUNT_EXCEPION;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.global.error.NameError;
+import racingcar.global.error.TryCountError;
+
+public class RacingInputValidateService {
+
+    private final String COMMA = ",";
+
+    private String POSITIVE_NUMBER_REG = "\\d+";
+
+    public List<String> splitCarName(String input) {
+        return Arrays.stream(input.split(COMMA)).collect(Collectors.toList());
+    }
+
+    public void validateCarName(List<String> carNames) {
+
+        if (!validateNameNotNull(carNames)) {
+            throw new NameError(NAME_BLANK_EXCEPION);
+        }
+        if (!validateNameLength(carNames)) {
+            throw new NameError(NAME_LENGTH_EXCEPION);
+        }
+    }
+
+    public void validateTryCount(String count) {
+
+        if (!validatePositiveIntegerCount(count)) {
+            throw new TryCountError(TRY_COUNT_EXCEPION);
+        }
+    }
+
+    private boolean validateNameNotNull(List<String> carNames) {
+
+        return carNames.stream().allMatch(name -> name != null && !name.isEmpty());
+    }
+
+    private boolean validateNameLength(List<String> carNames) {
+        return carNames.stream().allMatch(name -> name.length() <= 5);
+    }
+
+    private boolean validatePositiveIntegerCount(String count) {
+        if (count.matches(POSITIVE_NUMBER_REG)) {
+            return Integer.parseInt(count) > 0;
+        }
+        return false;
+    }
+
+}
