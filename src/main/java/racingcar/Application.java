10e938b7d45d3efdf8db0eae271ab7590dc4701a
moon-akdong <problemy0913@gmail.com>
2024-10-26 03:07:30 +0900

    Create method functional of canProgressWithRandomNum

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,8 +3,6 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -18,8 +16,15 @@ public class Application {
         int repeatRaceIntNum = Integer.parseInt(repeatRaceNum);
         System.out.println("시도할 횟수는 몇 회인가요? : " + repeatRaceIntNum);
 
+        String[] carNames = splitCarsName(inputStr);
+        int[] carProgressArr = new int [carNames.length];
         for (int i = 0; i < repeatRaceIntNum; i++) {
-            int randomNum = getRandomNum();
+            for (int car_ind = 0; car_ind <carNames.length; car_ind++) {
+                int carProgress = canProgressWithRandomNum();
+//                carNames[car_ind];
+//                carProgressArr[car_ind]
+            }
+
 
         }
     }
@@ -31,10 +36,13 @@ public class Application {
         return carsName;
     }
 
-    public static int getRandomNum() {
+    public static int canProgressWithRandomNum() {
         // 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
         int randomNum = Randoms.pickNumberInRange(0, 9);
+        if (randomNum > 4) {
+            return 1;
+        }
+        return 0;
         // System.out.println(randomNum);
-        return randomNum;
     }
 }
