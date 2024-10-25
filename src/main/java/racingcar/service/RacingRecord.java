356e36020282390f87bf94eaffcfbaee0b719815
tentenball <lth8905@gmail.com>
2024-10-25 22:19:02 +0900

    feat: 경기 기록을 저장할 클래스 생성
    * RacingRecord: 매 회 자동차들의 경기결과 기록을 저장할 클래스
    * RacingRecords: RacingRecord의 list

---
+++ src/main/java/racingcar/service/RacingRecord.java
@@ -0,0 +1,11 @@
+package racingcar.service;
+
+import racingcar.domain.car.Cars;
+
+public class RacingRecord {
+	private final Cars cars;
+
+	public RacingRecord(Cars cars) {
+		this.cars = cars;
+	}
+}
---
+++ src/main/java/racingcar/service/RacingRecords.java
@@ -0,0 +1,11 @@
+package racingcar.service;
+
+import java.util.List;
+
+public class RacingRecords {
+	private final List<RacingRecord> records;
+
+	public RacingRecords(List<RacingRecord> records) {
+		this.records = records;
+	}
+}
