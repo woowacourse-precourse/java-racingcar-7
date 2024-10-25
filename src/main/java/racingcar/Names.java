c6b2eb9def0ed27ab60d2f6130512e0cbc20ee54
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-26 06:49:41 +0900

    feat: 일급 컬렉션 사용해 이름 리스트 생성

--- src/main/java/racingcar/Names.java
+++ src/main/java/racingcar/Names.java
@@ -1,2 +1,67 @@
-package racingcar;public class Names {
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Names {
+
+    private static final String INPUT_IS_EMPTY_MESSAGE = "문자열이 입력되지 않았습니다.";
+    private static final String CAN_BE_NO_CONSECUTIVE_COMMAS_MESSAGE = "연속된 쉼표(,)는 입력될 수 없습니다.";
+    private static final String SINGLE_NAME_ERROR_MESSAGE = "하나의 이름만 들어갈 수는 없습니다.";
+    private static final String INPUT_CANT_HAVE_DUPLICATED_NAME_MESSAGE = "이름은 중복될 수 없습니다.";
+
+    private final List<Name> names;
+
+    public Names(String input) {
+        this.names = parse(input);
+    }
+
+    public List<Name> getNames() {
+        return Collections.unmodifiableList(names);
+    }
+
+    public List<Name> parse(String input) {
+        validate(input);
+
+        List<String> splitNameList = Arrays.asList(input.split(","));
+        List<Name> nameList = new ArrayList<>();
+
+        for (String nameString : splitNameList) {
+            String trimName = nameString.trim();
+            append(nameList, trimName);
+        }
+
+        checkMinimumSize(nameList);
+        return nameList;
+    }
+
+    private static void validate(String input) {
+        if (input.isEmpty() || input.equals(" ")) {
+            throw new IllegalArgumentException(INPUT_IS_EMPTY_MESSAGE);
+        }
+        if (input.contains(",,")) {
+            throw new IllegalArgumentException(CAN_BE_NO_CONSECUTIVE_COMMAS_MESSAGE);
+        }
+        if (!input.contains(",")) {
+            throw new IllegalArgumentException(SINGLE_NAME_ERROR_MESSAGE);
+        }
+    }
+
+    private void append(List<Name> nameList, String nameString) {
+        Name name = new Name(nameString);
+
+        if (nameList.contains(name)) {
+            throw new IllegalArgumentException(INPUT_CANT_HAVE_DUPLICATED_NAME_MESSAGE);
+        }
+
+        nameList.add(name);
+    }
+
+    private void checkMinimumSize(List<Name> nameList) {
+        if (nameList.size() == 1) {
+            throw new IllegalArgumentException(SINGLE_NAME_ERROR_MESSAGE);
+        }
+    }
 }
