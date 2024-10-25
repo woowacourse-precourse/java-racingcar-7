323bc3603e66856a09fc98c91d5f8e2d184d2a02
JEANS <songkc5671@naver.com>
2024-10-25 16:13:23 +0900

    feat(input) : 이름과 시도횟수를 받는 기능 추가
    이름, 시도횟수를 readLine() 함수로 입력값으로 받습니다.

--- src/main/java/racingcar/RacingCarView.java
+++ src/main/java/racingcar/RacingCarView.java
@@ -1,4 +1,24 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.raceDto.RacingRequest;
+
 public class RacingCarView {
+
+    public RacingRequest readInput() {
+
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분");
+        String name = Console.readLine();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int times = Integer.parseInt(Console.readLine());
+
+        Console.close();
+
+        return new RacingRequest(name, times);
+    }
+
+    public void printResult() {
+
+    }
 }
