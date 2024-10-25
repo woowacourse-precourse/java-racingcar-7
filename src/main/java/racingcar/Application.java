dc1f7a0f56e08c1ad2c3b3336ddc63e8bc74fe5b
YooSejeong <nived3@naver.com>
2024-10-25 20:47:23 +0900

    feat(Application): Add user input for race car names

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        //TODO: 다른 클래스로 분리할 수 있으면 분리하기
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carsInput = Console.readLine();
+        List<String> cars = Arrays.asList(carsInput.split(","));
+        
     }
+
 }
