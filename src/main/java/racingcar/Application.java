f8cfff405e2a3c2525a5edd3c34e74aa01f1a618
yyysolhhh <solbaram37@gmail.com>
2024-10-26 04:45:56 +0900

    feat(Application): 입력 기능 실행 추가
    main
    - InputHandler, CarFactory, RaceController 객체 생성
    - runRace 메서드 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.io.InputHandler;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
+        CarFactory carFactory = new CarFactory();
+        RaceController raceController = new RaceController(inputHandler, carFactory);
+        raceController.runRace();
     }
 }
