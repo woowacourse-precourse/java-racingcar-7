c3d3b5e40bdff9babdcabc744d4db0af1f03e3a1
halfmoon-mind <5338095@gmail.com>
2024-10-25 13:52:39 +0900

    feat: Car 객체 정의

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move() {
+        int randomValue = Randoms.pickNumberInRange(0, 9);
+        if (randomValue >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getProgress() {
+        return "-".repeat(position);
+    }
+}
