dd98928379d354c2826c537a50fa8a416afad06d
donghyunn1 <dongdong0516@naver.com>
2024-10-26 02:36:49 +0900

    fix: 공백 입력에 대한 예외처리 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -6,7 +6,7 @@ public class Car {
 
     private static final int MOVING_STANDARD = 4;
     private static final int NAME_LENGTH_BOUND = 5;
-    private static final String NAME_LENGTH_ERROR = "[ERROR] 이름은 5글자 이하여야 합니다.";
+    private static final String NAME_LENGTH_ERROR = "[ERROR] 이름은 1~5글자 사이여야 합니다.";
 
     private final String name;
     private int position;
@@ -18,7 +18,7 @@ public class Car {
     }
 
     private void validate(String name) {
-        if (name.length() > NAME_LENGTH_BOUND) {
+        if (name.isEmpty() || name.length() > NAME_LENGTH_BOUND) {
             throw new IllegalArgumentException(NAME_LENGTH_ERROR);
         }
     }
