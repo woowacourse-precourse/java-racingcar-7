29c8e44637936c8aa726de11e93472c40b0badfd
badgelatte <saucy5939@gmail.com>
2024-10-25 21:13:27 +0900

    Car 클래스에 randomMove 메소드 추가
    주어진 횟수동안 전진 또는 멈추기를 랜덤하게 갈 수 있도록 작성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,16 +5,18 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Car {
     String name;
     int count;
+    int progress;
 
-    public Car(String name) {
+    public Car(String name, int attemptCount) {
         this.name = name;
-        this.count = getRandomCount();
+        this.count = getRandomCount() % attemptCount;
+        this.progress = 0;
     }
 
     private int getRandomCount() {
         count = 0;
-        while(count < 4) {
-            count = Randoms.pickNumberInRange(0,9);
+        while (count < 4) {
+            count = Randoms.pickNumberInRange(0, 9);
         }
         return count;
     }
@@ -25,5 +27,17 @@ public class Car {
 
     public int getCount() {
         return count;
+    }
+
+    public boolean randomMove() {
+        if (progress >= count) {
+            return false;
+        }
+
+        if (Randoms.pickNumberInRange(0, 1) == 0) {
+            return true;
+        }
+
+        return false;
     }
 }
