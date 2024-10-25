78340dc8f6ea2270067b9a8a4ae81ba822164d44
ziyoungkwon <gy011003@naver.com>
2024-10-25 15:51:01 +0900

    refactor: input 기능 안에서 빈 문자 입력 받았을 시 예외처리

--- src/main/java/IO/Input.java
+++ src/main/java/IO/Input.java
@@ -2,15 +2,31 @@ package IO;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.NoSuchElementException;
+
 public class Input {
 
     public String inputCars() {
 
-        return Console.readLine();
+        String car = null;
+        try {
+            car = Console.readLine();
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("[잘못된 입력] 자동차 이름이 입력되지 않았습니다.");
+        }
+
+        return car;
     }
 
     public String inputAttempt() {
 
-        return Console.readLine();
+        String attempt = null;
+        try {
+            attempt = Console.readLine();
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("[잘못된 입력] 시도할 횟수가 입력되지 않았습니다.");
+        }
+
+        return attempt;
     }
 }
