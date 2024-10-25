0e4b42124973c7c0a070e96d57fd7eabafd76efc
eunho <eunho1032@gmail.com>
2024-10-25 16:03:51 +0900

    refactor(Application): using tabs change spaces

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,22 +1,18 @@
 package racingcar;
 
-/*
-간단한 로직 완료, 리팩토링 필요, 예외 처리 필요, class 파일 패키지별 분리 필요
- */
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
 
-	public static void main(String[] args) {
-		String[] carNames = Input.inputCarNames();
-		int tryCounts = Input.inputTryCounts();
+    public static void main(String[] args) {
+        String[] carNames = Input.inputCarNames();
+        int tryCounts = Input.inputTryCounts();
 
-		List<Car> cars = new ArrayList<>();
-		for (int i = 0; i < carNames.length; i++) {
-			cars.add(new Car(carNames[i]));
-		}
-		Output.printResult(cars, tryCounts);
-	}
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+        Output.printResult(cars, tryCounts);
+    }
 }
