5e1e3bee1e58ddb652caf21c669271fc9eaf4d89
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-10-25 18:17:58 +0900

    feat: 자동차 클래스 생성

---
+++ src/main/java/racingcar/model/car/Car.java
@@ -0,0 +1,11 @@
+package racingcar.model.car;
+
+public class Car {
+    private String name;
+    private int forwardCount;
+
+    public Car(String name) {
+        this.name = name;
+        forwardCount = 0;
+    }
+}
