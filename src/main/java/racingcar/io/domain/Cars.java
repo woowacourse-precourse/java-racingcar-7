9414ac826cca40e379a49620536ef5ab68cb1bb9
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-10-26 08:57:15 +0900

    feat: 전체 차가 이동하는 기능 구현
    전체 차가 이동하는 기능 구현,
    랜덤 값 개수와 차량 개수가 일치하지 않으면 예외가 발생하는 기능 구현

--- src/main/java/racingcar/io/domain/Cars.java
+++ src/main/java/racingcar/io/domain/Cars.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Cars {
 
+	private static final String NOT_EQUALS_CARS_COUNT_AND_RANDOM_VALUES_COUNT_ERROR_MESSAGE = "랜덤 값과 자동차의 개수가 일치하지 않습니다.";
+
 	private final List<Car> cars;
 
 	private Cars(List<Car> cars) {
@@ -19,6 +21,20 @@ public class Cars {
 		return new Cars(cars);
 	}
 
+	public void moveAll(List<Integer> randomValues) {
 
+		validateRandomValuesCount(randomValues);
+
+		for (int carSequence = 0; carSequence < cars.size(); carSequence++) {
+			Car car = cars.get(carSequence);
+			car.move(randomValues.get(carSequence));
+		}
+	}
+
+	private void validateRandomValuesCount(List<Integer> randomValues) {
+		if (randomValues.size() != cars.size()) {
+			throw new IllegalStateException(NOT_EQUALS_CARS_COUNT_AND_RANDOM_VALUES_COUNT_ERROR_MESSAGE);
+		}
+	}
 
 }
