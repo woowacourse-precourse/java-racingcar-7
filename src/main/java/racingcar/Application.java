62b4abb2014287bf56ab2d7903c866b3068e5a1f
Changsu Park <gd12091@gmail.com>
2024-10-25 18:35:46 +0900

    feat: 메인 애플리케이션 클래스 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import racingcar.controller.CarRacingController;
+import racingcar.service.CarService;
+import racingcar.service.GameService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarService carService = new CarService();
+        GameService gameService = new GameService();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        CarRacingController carRacingController =
+                new CarRacingController(carService, gameService, inputView, outputView);
+
+        carRacingController.startRace();
     }
 }
