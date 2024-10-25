1caaddf24bd50ebaa383ee3b3bb1e7477a69f3a8
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2024-10-25 12:00:54 +0900

    :necktie: feat : getCars 메서드 추가
    - 쉼표를 기준으로 구분하여 자동차 이름을 입력받고 저장

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,6 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class Input {
+
+    public List<Car> getCars(){
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        return Arrays.stream(Console.readLine().split(","))
+                .map(String::trim)
+                .peek(this::checkValidation)
+                .map(Car::new)
+                .toList();
+    }
 
     public void checkValidation(String name) {
         checkIsEmpty(name);
