be0b7f3bb0793cc012d1bdcfb2831bc684e70d78
onesay17 <onesay17@gmail.com>
2024-10-25 12:54:15 +0900

    refactor: 자동차이름 빈값검증로직 추가

--- src/main/java/racingcar/validation/CarNamesValidatorImpl.java
+++ src/main/java/racingcar/validation/CarNamesValidatorImpl.java
@@ -1,27 +1,40 @@
 package racingcar.validation;
 
+import static racingcar.util.Constant.COMMA_DELIMITER;
 import static racingcar.util.Constant.MAX_CAR_NAME_LENGTH;
+import static racingcar.validation.ValidatorUtil.containsWhitespace;
+import static racingcar.validation.ValidatorUtil.isEmptyInput;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import racingcar.util.CommonUtil;
 
-public class CarNameValidator implements NameValidator {
+public class CarNamesValidatorImpl implements CarNamesValidator {
 
     @Override
-    public void validate(Collection<String> carNames) {
-        validateEmptyOrBlank(carNames);
-        validateDuplicateNames(carNames);
-        validateNameLength(carNames);
+    public void validate(String input) {
+        validateEmptyOrBlankInput(input);
+        List<String> ListOfCarNames = CommonUtil.splitToList(input, COMMA_DELIMITER);
+        validateEmptyOrBlank(ListOfCarNames);
+        validateDuplicateNames(ListOfCarNames);
+        validateNameLength(ListOfCarNames);
+    }
+
+    private void validateEmptyOrBlankInput(String carNames) {
+        if (isEmptyInput(carNames)) {
+            throw new IllegalArgumentException("경주할 자동차 입력은 빈 값이나 공백일 수 없습니다.");
+        }
     }
 
     private void validateEmptyOrBlank(Collection<String> carNames) {
         for (String carName : carNames) {
-            if (carName == null || carName.trim().isEmpty()) {
+            if (isEmptyInput(carName)) {
                 throw new IllegalArgumentException("자동차 이름은 빈 값이나 공백일 수 없습니다.");
             }
 
-            if (carNames.contains(" ")) {
+            if (containsWhitespace(carName)) {
                 throw new IllegalArgumentException("자동차 이름에는 공백이 포함될 수 없습니다.");
             }
         }
