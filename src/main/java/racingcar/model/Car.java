b092c452400be3e507eca54ab019ba5dbca4afc9
동영 김 <kimarang0@naver.com>
2024-10-25 22:00:14 +0900

    feat(Car): 자동차 Model 생성 - name(이름, String)과 location(위치, int)를 보유 - move 메서드로 location을 증가시킬 수 있음

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+public class Car {
+    private String name;
+    private int location;
+
+    public Car(String name) {
+        this.name = name;
+        this.location = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void move() {
+        location++;
+    }
+}
