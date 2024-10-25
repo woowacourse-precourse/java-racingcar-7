1b17176bb9071f0d80689dbe5fed4489dc0c42f8
kangsan <devstar1223@gmail.com>
2024-10-25 14:18:28 +0900

    feat: 안내 메세지 출력

--- src/main/java/racingcar/CarRacingSimulation.java
+++ src/main/java/racingcar/CarRacingSimulation.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.io.ConsoleOutputHandler;
+
 public class CarRacingSimulation {
+    private final ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
     public void run() {
-        //자동차 경주 구현
+        consoleOutputHandler.racingCarNamingMessage();
     }
 }
