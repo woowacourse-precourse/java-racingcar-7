3c892f11a7108d495de398a994f51b5d0280107c
YeonjiIsGonji <165933276+YeonjiIsGonji@users.noreply.github.com>
2024-10-25 20:44:16 +0900

    feat: 자동차 이름 유효성 검사 기능 추가
    - 자동차 이름이 유효한지 확인하는 isValidName 메서드 추가
    - 이름 유효성 검사의 구체적인 기준:
     - 유효한 이름 조건:
      - 쉼표(,)로 구분된 여러 이름을 포함할 것
      - 각 이름은 공백 제거 후 최대 5자 이하일 것
      - 앞 뒤 공백 제거 후 이름 내에 공백이나 특수 문자가 포함되어도 무관함
     - 유효하지 않은 이름 조건:
      - 이름이 비어 있거나 쉼표로만 구분된 경우
      - 이름 중복이 있는 경우
      - 이름에 공백을 제외한 문자가 전혀 없는 경우
      - 이름이 1개인 경우
      - 쉼표(,)로 끝나는 경우
    - 중복 체크를 위해 hasDuplicates 메서드를 분리 및 활용하여 코드 가독성을 높임

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,51 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNamesInput = Console.readLine();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String attemptCount = Console.readLine();
+
+        if(!isValidName(carNamesInput)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static boolean isValidName(String carNamesInput) {
+        if (carNamesInput.isEmpty()) return false;
+
+        if (!carNamesInput.contains(",")) return false;
+
+        String[] carNameList = carNamesInput.split(",", -1);
+        if (carNameList.length == 1) return false;
+
+        if (hasDuplicates(carNameList)) return false;
+
+        for (String carName : carNameList) {
+            if (carName.isBlank() || carName.trim().length() > 5) return false;
+        }
+
+        return true;
+    }
+
+    public static boolean hasDuplicates(String[] nameList) {
+        Set<String> verifiedList = new HashSet<>();
+
+        for (String name : nameList) {
+            if (!verifiedList.add(name)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
