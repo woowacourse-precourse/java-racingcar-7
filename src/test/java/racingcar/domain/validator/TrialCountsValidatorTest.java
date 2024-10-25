9c80d1feb20b5fc1c78aa512b8c92f9275d30198
bowook <mbosx316@naver.com>
2024-10-25 18:50:44 +0900

    test(TrialCountsValidatorTest) : TrialCountsValidator 테스트
    - 숫자가 아닌 값이 들어오면 예외 발생
    - 숫자가 0이면 예외 발생
    - 숫자가 음수면 예외 발생
    - 올바른 숫자 값이 들어오면 정상 처리

--- src/test/java/racingcar/domain/validator/TrialCountsValidatorTest.java
+++ src/test/java/racingcar/domain/validator/TrialCountsValidatorTest.java
@@ -1,2 +1,75 @@
-package racingcar.domain.validator;public class TrialCountsValidatorTest {
+package racingcar.domain.validator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TrialCountsValidatorTest {
+    private TrialCountsValidator trialCountsValidator;
+
+    @BeforeEach
+    void setUp() {
+        trialCountsValidator = new TrialCountsValidator();
+    }
+
+    @Test
+    void 숫자가_아닌_값이_들어오면_예외_발생() {
+        //given
+        String invalidInput = "a";
+        //when
+        try {
+            trialCountsValidator.validate(invalidInput);
+        } catch (IllegalArgumentException e) {
+            assertThat("입력한 값은 숫자가 아닙니다 : " + invalidInput).isEqualTo(e.getMessage());
+        }
+        //then
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            trialCountsValidator.validate(invalidInput);
+        });
+    }
+
+    @Test
+    void 숫자가_0이면_예외_발생() {
+        //given
+        String zeroInput = "0";
+        //when
+        try {
+            trialCountsValidator.validate(zeroInput);
+        } catch (IllegalArgumentException e) {
+            assertThat("입력한 값은 0보다 커야 합니다 : " + zeroInput).isEqualTo(e.getMessage());
+        }
+        //then
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            trialCountsValidator.validate(zeroInput);
+        });
+    }
+
+    @Test
+    void 숫자가_음수면_예외_발생() {
+        //given
+        String minusInput = "-1";
+        //when
+        try {
+            trialCountsValidator.validate(minusInput);
+        } catch (IllegalArgumentException e) {
+            assertThat("입력한 값은 0보다 커야 합니다 : " + minusInput).isEqualTo(e.getMessage());
+        }
+        //then
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            trialCountsValidator.validate(minusInput);
+        });
+    }
+
+    @Test
+    void 올바른_숫자_값이_들어오면_정상_처리() {
+        //given
+        String validInput = "5";
+        //when
+        int result = trialCountsValidator.validate(validInput);
+        //then
+        assertEquals(5, result);
+    }
 }
