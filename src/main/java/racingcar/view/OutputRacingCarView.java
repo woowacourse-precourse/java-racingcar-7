88dadf0ea21caf5625379141f59bbc23319e0e13
changuii <rhljh7410@gmail.com>
2024-10-25 17:57:38 +0900

    feat(OutputRacingCarView) : 7. 자동차 경주 게임의 결과를 출력하는 기능
    - outputGameWinners() : 자동차 경주 게임의 우승자들을 출력한다.

--- src/main/java/racingcar/view/OutputRacingCarView.java
+++ src/main/java/racingcar/view/OutputRacingCarView.java
@@ -1,9 +1,20 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.domain.Car;
+
 public class OutputRacingCarView {
     private static final String GAME_RESULT_OUTPUT = "실행 결과\n%s";
+    private static final String GAME_WINNERS_OUTPUT = "최종 우승자 : %s";
 
     public void outputGameResult(String gameResult) {
         System.out.printf(GAME_RESULT_OUTPUT, gameResult);
+    }
+
+    public void outputGameWinners(List<Car> winners) {
+        String[] gameWinners = winners.stream()
+                .map(winner -> winner.getName())
+                .toArray(String[]::new);
+        System.out.printf(GAME_WINNERS_OUTPUT, String.join(", ", gameWinners));
     }
 }
