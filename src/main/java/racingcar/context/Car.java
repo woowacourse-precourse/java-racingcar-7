8635ad9e7124810af5ef642d9471e43c52f724f4
world-dv <goodforx0147@naver.com>
2024-10-25 11:35:45 +0900

    refactor: 자동차 전략 수정
    - 자동차가 주입받는 전략을 다양하게 할 수 있도록 Interface 객체를 생성하도록 수정합니다.

--- src/main/java/racingcar/context/Car.java
+++ src/main/java/racingcar/context/Car.java
@@ -1,13 +1,13 @@
 package racingcar.context;
 
-import racingcar.strategy.MovementStrategy;
+import racingcar.strategy.CarStrategy;
 
 public class Car {
     private final String carName;
     private Integer carMovementDistance;
-    private MovementStrategy carStrategy;
+    private CarStrategy carStrategy;
 
-    public Car(String carName, MovementStrategy carStrategy) {
+    public Car(String carName, CarStrategy carStrategy) {
         this.carName = carName;
         this.carMovementDistance = 0;
         this.carStrategy = carStrategy;
