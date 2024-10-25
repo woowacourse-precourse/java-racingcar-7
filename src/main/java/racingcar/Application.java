261fa77b73e1edd55eafc1671d879ea364147a29
tentenball <lth8905@gmail.com>
2024-10-25 23:04:06 +0900

    refactor: RacingGameServic 반환타입 수정으로 인한 Application 로직 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,16 @@
 package racingcar;
 
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.domain.car.Cars;
 import racingcar.domain.name.Names;
 import racingcar.domain.racingGame.RacingGameCount;
+import racingcar.domain.racingGame.RacingRecord;
 import racingcar.service.CarService;
 import racingcar.service.RacingGameService;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
@@ -17,7 +21,8 @@ public class Application {
         Cars cars = carService.generateCars(carNames);
 
         RacingGameCount count = InputView.inputRaceCount();
-        racingGameService.processRacingGame(cars, count);
+        List<RacingRecord> records = racingGameService.processRacingGame(cars, count);
+        OutputView.printRecords(records);
         Console.close();
     }
 }
