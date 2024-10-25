d6cdb055e46bffedc5bcbdb5bc902e064022eb6c
Hyun jinyong <ekwls20@gmail.com>
2024-10-25 10:08:13 +0900

    feat(controller) : 경주 진행 관리 기능 구현
    경주 진행을 위한 controller 클래스를 구현했다.
    - 해당 클래스는 경주의 시작과 끝을 관리한다.
    - Application 클래스에서 controller 객체를 생성하여 프로그램을 시작하도록 하였다ㅏ.

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,43 @@
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.CarFactory;
+import racingcar.model.RacingCarGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+
+  private final CarFactory carFactory;
+  private final InputView inputView;
+  private final OutputView outputView;
+
+  public RacingController() {
+    this.carFactory = new CarFactory();
+    this.inputView = new InputView();
+    this.outputView = new OutputView();
+  }
+
+  public void run() {
+    List<Car> cars = getCars();
+    RacingCarGame racingCarGame = new RacingCarGame(cars);
+    playGame(racingCarGame, inputView.inputTryCount());
+    outputView.printLastWinner(racingCarGame.getWinners());
+  }
+
+  private List<Car> getCars() {
+    String carNames = inputView.inputCarNames();
+    return carFactory.extractCarNames(carNames);
+  }
+
+  private void playGame(RacingCarGame racingCarGame, int tryCount) {
+    outputView.printResult();
+    for (int i = 0; i < tryCount; i++) {
+      racingCarGame.play();
+      racingCarGame.cars()
+              .forEach(car -> outputView.printCarAndMoveCount(car.getCarName(), car.getMoveCount()));
+      System.out.println();
+    }
+  }
+}
