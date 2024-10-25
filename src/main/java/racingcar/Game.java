ec39371db96587acd52bf5bfb52a724dcfe9aad5
AYEOOON <k93853471@gmail.com>
2024-10-25 21:07:27 +0900

    feat: 구현한 메소드들을 실행할 수 있도록 구현
    - 자동차 이름과 시도 횟수를 입력받는 메서드를 initialize 메서드에서 호출
    - start 메서드에서 자동차 전진 메서드를 호출하여 실행 가능하도록 구현
    - Game 객체를 생성하여 main 에서 게임 초기화 메서드 호출

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -7,15 +7,16 @@ public class Game {
     List<Car> carList = new ArrayList<>();
 
     public void initialize(){
-        // 자동자 이름 읿력받는 함수 호출
-        // 시도 횟수 입력받는 함수 호출
-        int attempt = 0;
+        carList = InputUtils.inputCarName();
+        int attempt = InputUtils.inputAttempt();
+
+        start(attempt);
     }
 
     public void start(int attempt){
         while(attempt>0){
             for (Car car : carList){
-                // 자동차 전진 함수 호출
+                car.moveForward();
             }
             attempt--;
         }
