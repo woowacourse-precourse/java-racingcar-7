0c1b2a722eb6cdd0890e4746d5d979053faf0ea5
jhan0121 <jhan0121@gmail.com>
2024-10-25 21:53:52 +0900

    test(race): RaceService 변경으로 인한 테스트 코드 수정

--- src/test/java/racingcar/service/RaceServiceTest.java
+++ src/test/java/racingcar/service/RaceServiceTest.java
@@ -5,22 +5,29 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
 
 class RaceServiceTest {
 
     @Test
-    @DisplayName("랜덤 생성 값이 4 이상이면 progress를 1증가해야 한다.")
+    @DisplayName("랜덤 생성 값이 4 이상이면 progress를 1 증가해야 한다.")
     void move() {
-        List<Integer> progress = new ArrayList<>(List.of(0, 0, 0, 0));
+        List<String> players = List.of("a", "b", "c", "d");
+        List<Car> cars = new ArrayList<>();
+
+        for (String player : players) {
+            cars.add(new Car(player));
+        }
+
         int[] random = {1, 2, 3, 4};
         List<Integer> result = List.of(0, 0, 0, 1);
 
         for (int i = 0; i < random.length; i++) {
-            RaceService.move(progress, random[i], i);
+            RaceService.move(cars.get(i), random[i]);
         }
 
         for (int i = 0; i < 4; i++) {
-            Assertions.assertThat(progress.get(i)).isEqualTo(result.get(i));
+            Assertions.assertThat(cars.get(i).getProgress()).isEqualTo(result.get(i));
         }
     }
 
@@ -29,10 +36,15 @@ class RaceServiceTest {
     void findWinner() {
         List<String> players = List.of("a", "b", "c", "d");
         List<Integer> progress = List.of(1, 2, 3, 4);
+        List<Car> cars = new ArrayList<>();
         List<String> winner = new ArrayList<>();
 
+        for (int i = 0; i < players.size(); i++) {
+            cars.add(new Car(players.get(i), progress.get(i)));
+        }
+
         RaceService raceService = new RaceService();
-        raceService.findWinner(players, progress, winner);
+        raceService.findWinner(cars, winner);
 
         Assertions.assertThat(winner).hasSize(1).contains("d");
     }
@@ -42,10 +54,15 @@ class RaceServiceTest {
     void findDuplicatedWinner() {
         List<String> players = List.of("a", "b", "c", "d");
         List<Integer> progress = List.of(1, 2, 4, 4);
+        List<Car> cars = new ArrayList<>();
         List<String> winner = new ArrayList<>();
 
+        for (int i = 0; i < players.size(); i++) {
+            cars.add(new Car(players.get(i), progress.get(i)));
+        }
+
         RaceService raceService = new RaceService();
-        raceService.findWinner(players, progress, winner);
+        raceService.findWinner(cars, winner);
 
         Assertions.assertThat(winner).hasSize(2).containsAll(List.of("c", "d"));
     }
