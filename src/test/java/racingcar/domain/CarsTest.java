76ccc6d145b4d647acebea8b27bea72116513323
songsunkook <songsunkook@gmail.com>
2024-10-26 00:42:51 +0900

    test: 승자 판단 테스트 버그 수정

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -10,22 +10,25 @@ class CarsTest {
     @ParameterizedTest
     @CsvSource(value = {"0,0", "0,1", "2,2", "2,3", "4,3", "100,101"})
     void 정상적으로_승자를_판단하는가(int position1, int position2) {
+        // given
         Cars cars = new Cars();
         Car car1 = new Car("pobi", position1);
         Car car2 = new Car("woni", position2);
         cars.add(car1);
         cars.add(car2);
+
+        // when then
         if (position1 == position2) {
-            assertThat(cars.getWinners().get(0)).isEqualTo(car1);
-            assertThat(cars.getWinners().get(1)).isEqualTo(car2);
+            assertThat(cars.getWinners().get(0)).isEqualTo(car1.getDto());
+            assertThat(cars.getWinners().get(1)).isEqualTo(car2.getDto());
             return;
         }
         if (position1 > position2) {
-            assertThat(cars.getWinners().get(0)).isEqualTo(car1);
+            assertThat(cars.getWinners().get(0)).isEqualTo(car1.getDto());
             assertThat(cars.getWinners().size()).isEqualTo(1);
             return;
         }
-        assertThat(cars.getWinners().get(0)).isEqualTo(car2);
+        assertThat(cars.getWinners().get(0)).isEqualTo(car2.getDto());
         assertThat(cars.getWinners().size()).isEqualTo(1);
     }
 }
