e465c41cfa1efa7de7d9ef339eb0a35ce4f0a659
LeeDongHoon <likelion.leedh@gmail.com>
2024-10-25 15:23:11 +0900

    Revert "[FEAT] : 자동차 경주 게임 시작 로직 추가"
    This reverts commit 520d8aa649adb4742b673d3050196d53ef01a12f.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,7 @@
 package racingcar;
 
-import racingcar.racingGame.RacingGame;
-import racingcar.racingGame.RacingGameTemplate;
-
 public class Application {
     public static void main(String[] args) {
-        RacingGameTemplate racingGame = new RacingGame();
-        racingGame.play();
+        // TODO: 프로그램 구현
     }
 }
