be4e96c3bd42413d23ac32f66677f2b4013c4dd8
JunBe <wnsqowid0901@gmail.com>
2024-10-25 22:56:56 +0900

    refactor: Controller, Model 구조 변경
    GameController: Input과 Output 타입을 모두 생성하던 구조를 GameView 하나의 객체를 생성하여 모두 이용하게 구조를 바꿨다.
    또한 계산 로직인 getWinner()은 Model쪽에서 처리하도록 이동시켰고 필요한 값은 파라미터를 통해 모델에 넘겨주도록 개선했다.
    
    GameModel: 오직 계산만 수행하도록 입력은 View에서 담당하도록 하고 받아온 파라미터로 계산을 수행하도록 개선했다.

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,65 +1,35 @@
 package racingcar.controller;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import racingcar.view.io.Input;
-import racingcar.view.io.Output;
+import racingcar.view.GameView;
 import racingcar.model.GameModel;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class GameController {
     private final GameModel gameModel;
-    private final Input input;
-    private final Output output;
+    private final GameView gameView;
 
     public GameController() {
         this.gameModel = new GameModel();
-        this.input = new Input();
-        this.output = new Output();
+        this.gameView = new GameView();
     }
 
     public void runFullGame() {
         //입력 받기 (나중에 한 메서드로 묶을 것)
-        Map<String, Integer> carStepsMap = gameModel.getCarMap();
-        int round = gameModel.getRound();
+        Map<String, Integer> carStepsMap = gameModel.getCarMap(gameView.input.carName().split(","));
+        int round = gameModel.getRound(gameView.input.gameRound());
 
         //로직 실행
         System.out.println("실행 결과");
         for (int i = 0; i < round; i++) {
-            output.gameResult(startGame(carStepsMap));
+            gameView.output.gameResult(gameModel.startGame(carStepsMap));
         }
 
         //우승자 출력 받기
-        List<String> winnerList = getWinner(carStepsMap);
-        output.winner(winnerList);
-    }
-
-    private static List<String> getWinner(Map<String, Integer> carMap) {
-        List<String> winnerList = new ArrayList<>();
-        int top = 0;
-        for (String carName : carMap.keySet()) {
-            if (carMap.get(carName) > top) {
-                top = carMap.get(carName);
-            }
-        }
-        for (String carName : carMap.keySet()) {
-            if (carMap.get(carName) == top) {
-                winnerList.add(carName);
-            }
-        }
-        return winnerList;
+        List<String> winnerList = gameModel.getWinner(carStepsMap);
+        gameView.output.winner(winnerList);
     }
 
 
-
-    private static Map<String,Integer> startGame(Map<String,Integer> carMap) {
-        for (String key : carMap.keySet()) {
-            if (Randoms.pickNumberInRange(0, 9) >= 4) {
-                carMap.put(key, carMap.get(key) + 1);
-            }
-        }
-        return carMap;
-    }
 }
--- src/main/java/racingcar/model/GameModel.java
+++ src/main/java/racingcar/model/GameModel.java
@@ -1,6 +1,6 @@
 package racingcar.model;
 
-import racingcar.view.io.Input;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,21 +8,19 @@ import java.util.List;
 import java.util.Map;
 
 public class GameModel {
-    public static final Input input = new Input();
-    public Map<String, Integer> getCarMap() {
-        String[] splitCarName = input.carName().split(",");
+    public Map<String, Integer> getCarMap(String[] carNames) {
         Map<String, Integer> carMap = new HashMap<>();
-        for (String carName : splitCarName) {
+        for (String carName : carNames) {
             if (carName.length() > 5) throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
             carMap.put(carName, 0);
         }
         return carMap;
     }
 
-    public int getRound() {
+    public int getRound(String gameRound) {
         int numRound;
         try {
-            numRound = Integer.parseInt(input.gameRounds());
+            numRound = Integer.parseInt(gameRound);
             System.out.println();
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("정수 이외의 값은 입력이 불가능합니다.");
@@ -47,5 +45,14 @@ public class GameModel {
             }
         }
         return winnerList;
+    }
+
+    public Map<String,Integer> startGame(Map<String,Integer> carMap) {
+        for (String key : carMap.keySet()) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                carMap.put(key, carMap.get(key) + 1);
+            }
+        }
+        return carMap;
     }
 }
