e555c4a0532085c5166e61eaa3cff59b8ce715fe
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-10-25 22:57:44 +0900

    fix(InputView): 입력 로직 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,19 +1,19 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_CAR_NAMES = "자동차 이름을 쉼표(,)로 구분해 입력하세요.";
     private static final String INPUT_COUNT = "시도할 횟수는 몇 회인가요?";
-    private static final Scanner scanner = new Scanner(System.in);
 
     public static String readCarNames() {
         System.out.println(INPUT_CAR_NAMES);
-        return scanner.nextLine();
+        return Console.readLine();
     }
 
     public static String readCountOfRounds() {
         System.out.println(INPUT_COUNT);
-        return scanner.nextLine();
+        return Console.readLine();
     }
 }
