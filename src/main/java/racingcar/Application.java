ed2bcb63e56bbdfa623e4411f5647ac0b0928431
zzzyoonnn <dwsrh03@gmail.com>
2024-10-25 15:18:29 +0900

    feat: 문자열을 입력받는 inputString 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
+    static String regex = "^[a-zA-Z0-9]+(,[a-zA-Z0-9]+)*$";
+    static Pattern pattern = Pattern.compile(regex);
+    static Matcher matcher;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        inputString();
+    }
+
+    private static void inputString() {
+        String input = Console.readLine();
+        matcher = pattern.matcher(input);
+
+        if (matcher.matches()) {
+            System.out.println("문자열이 정규식에 맞습니다.");
+        } else {
+            System.out.println("문자열이 정규식에 맞지 않습니다.");
+        }
     }
 }
