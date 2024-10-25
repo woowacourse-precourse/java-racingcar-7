c5837cafa6f45ee37dfef10a5d1bba5f20933832
joonwan <joonwan1024@naver.com>
2024-10-25 15:57:24 +0900

    feat: NameExtractor 이름 파싱기능 구현

--- src/main/java/racingcar/NameExtractor.java
+++ src/main/java/racingcar/NameExtractor.java
@@ -1,4 +1,20 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NameExtractor {
+
+    private static final String REGEX = ",";
+
+    public static List<String> getNames(String userInput) {
+        List<String> splitNames = List.of(userInput.split(REGEX));
+        List<String> names = new ArrayList<>();
+
+        for (String rawName : splitNames) {
+            names.add(rawName.trim());
+        }
+
+        return names;
+    }
 }
