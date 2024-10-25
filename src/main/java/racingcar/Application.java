94e26e961ddc7f5fc322ec4be4dc47bd7513a21c
gomin0 <gomin0981113@naver.com>
2024-10-26 00:42:39 +0900

    feat: 자동차 경주 게임 실행 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            List<String> names = InputCar.readCarNames();
+            int tryCount = InputCar.readTryCount();
+
+            Cars cars = new Cars(names);
+
+            OutputCar.printStart();
+            playGame(cars, tryCount);
+            announceWinner(cars);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void playGame(Cars cars, int tryCount) {
+        for (int i = 0; i < tryCount; i++) {
+            cars.moveAll();
+            OutputCar.printRoundResult(cars.getCars());
+        }
+    }
+
+    private static void announceWinner(Cars cars) {
+        List<String> winners = cars.getWinners();
+        OutputCar.printWinners(winners);
     }
 }
