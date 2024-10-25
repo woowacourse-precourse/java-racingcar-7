0f6dc57551b638453cdf890b008443503d8a680a
jklee3409 <jklee3409@khu.ac.kr>
2024-10-25 19:58:43 +0900

    feat(race) : playRound 메서드 구현

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -1,11 +1,19 @@
 package racingcar.model;
 
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Race {
     private final List<Car> cars;
 
     public Race(List<Car> cars) {
         this.cars = cars;
+    }
+
+    public void playRound() {
+        for (Car car : cars) {
+            int randomValue = Randoms.pickNumberInRange(0, 9);
+            car.move(randomValue);
+        }
     }
 }
