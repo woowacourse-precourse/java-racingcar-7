a12a09ff8fd7e723db14ac65ebf40fafb478609c
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-10-25 12:06:42 +0900

    test: 시도할 횟수 입력 함수 테스트
    - 사용자로부터 시도할 횟수 입력 받는 기능 테스트 구현

--- src/test/java/racingcar/io/InputManagerTest.java
+++ src/test/java/racingcar/io/InputManagerTest.java
@@ -26,4 +26,14 @@ class InputManagerTest {
         String carName = inputManager.carNameInput();
         assertThat(carName).isEqualTo("pobi,woni,jun");
     }
+
+    @Test
+    @DisplayName("시도할 횟수가 올바르게 입력 되는지 확인한다")
+    void 시도할_횟수_입력_테스트() {
+        String input = "5";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        String attemptCount = inputManager.attemptCountInput();
+        assertThat(attemptCount).isEqualTo("5");
+    }
 }
