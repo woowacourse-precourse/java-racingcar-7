4cf9bd20b9e2c875b4b070f6de07acef0b1107cf
yeo-li <81564503+yeo-li@users.noreply.github.com>
2024-10-25 16:55:35 +0900

    feat(Application): 자동차 이름 추출 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -14,5 +14,8 @@ public class Application {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String repeatCount = Console.readLine();
 
+        // Extract car names
+        String[] cars = input.split(",");
+
     }
 }
