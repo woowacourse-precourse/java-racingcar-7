ee21b0a02bdfe6422227208450dd67008ebbdb16
taek105 <ht5172@gmail.com>
2024-10-26 00:44:00 +0900

    test(ApplicationTest): add test
    수정된 제약 조건을 위한 테스트 케이스를 추가했습니다

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,81 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 특이한_이름_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("!!?,:::,:::4,!.5", "1");
+                    assertThat(output()).contains("!!? : -", "::: : ",":::4 : ","!.5 : ", "최종 우승자 : !!?");
+                },
+                MOVING_FORWARD, STOP, STOP, STOP
+        );
+    }
+
+    @Test
+    void 입력_없음_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 중복_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("taek,taek", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 후미_공백_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("taek1,taek2, ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 공백_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("taek1, ,taek2", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 선두_공백_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" ,taek1,taek2", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 음수_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("taek1,taek2", "-11"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 정수_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("taek1,taek2", "3.67"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void int_범위_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("taek1,taek2", "2147483648"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
