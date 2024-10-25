cebf40990836d70989aa50da50ee391a90923e7e
김건학 <kkh10002@naver.com>
2024-10-26 01:30:12 +0900

    feat(Application): 어플리케이션 코드 구현
    핵심 기능 구현을 완료하였습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,31 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.controller.Controller;
+import racingcar.domain.Car;
+import racingcar.domain.Race;
+import racingcar.service.Service;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Service service = new Service();
+
+        Controller controller = new Controller(inputView, outputView, service);
+
+        String inputCarNames = controller.inputCarNames();
+        int attemptCount = controller.inputAttemptCount();
+
+        List<String> carNames = controller.getCarNames(inputCarNames);
+        List<Car> carList = controller.registerCar(carNames);
+
+        Race race = controller.registerRace(carList, attemptCount);
+        controller.playGame(race);
+
+        List<Car> winnerCarList = controller.getWinnerCarList(race);
+        controller.printWinners(winnerCarList);
     }
 }
