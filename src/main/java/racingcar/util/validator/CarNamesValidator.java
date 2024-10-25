6e24597a91db678c080251bc2f133c9aaca60246
junest66 <chlwndks333@gmail.com>
2024-10-25 13:09:03 +0900

    refactor(CarNamesValidator): 자동차 입력 유효성 검증 추가
    - 입력에 첫번째 혹은 마지막에 구분자가 있을 경우 예외처리
    - 연속된 구분자가 있을 경우도 예외처리

--- src/main/java/racingcar/util/validator/CarNamesValidator.java
+++ src/main/java/racingcar/util/validator/CarNamesValidator.java
@@ -7,13 +7,26 @@ public class CarNamesValidator {
     private static final int MIN_CAR_NAME_LENGTH = 1;
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final String CAR_NAME_SEPARATOR = ",";
+    private static final String INVALID_SEPARATOR_POSITION_MESSAGE = "구분자는 입력의 첫번째 또는 마지막에 올 수 없습니다";
+    private static final String INVALID_CONSECUTIVE_SEPARATOR_MESSAGE = "연속된 구분자를 사용할 수 없습니다.";
     private static final String INVALID_CAR_COUNT_MESSAGE = "자동차는 최소 1대 이상 100대 이하로만 가능합니다.";
     private static final String INVALID_CAR_NAME_MESSAGE = "자동차 이름은 최소 1자 이상 5자 이하만 가능합니다.";
 
     public static void validate(String carNames) {
+        validateSeparatorPosition(carNames);
         String[] splitCarNames = carNames.split(CAR_NAME_SEPARATOR);
         validateNumberOfCars(splitCarNames);
         validateCarNames(splitCarNames);
+    }
+
+    private static void validateSeparatorPosition(String carNames) {
+        if (!isValidSeparatorPosition(carNames)) {
+            throw new IllegalArgumentException(INVALID_SEPARATOR_POSITION_MESSAGE);
+        }
+
+        if (!isValidConsecutiveSeparator(carNames)) {
+            throw new IllegalArgumentException(INVALID_CONSECUTIVE_SEPARATOR_MESSAGE);
+        }
     }
 
     private static void validateNumberOfCars(String[] splitCarNames) {
@@ -28,6 +41,14 @@ public class CarNamesValidator {
                 throw new IllegalArgumentException(INVALID_CAR_NAME_MESSAGE);
             }
         }
+    }
+
+    private static boolean isValidSeparatorPosition(String carNames) {
+        return !(carNames.startsWith(CAR_NAME_SEPARATOR) || carNames.endsWith(CAR_NAME_SEPARATOR));
+    }
+
+    private static boolean isValidConsecutiveSeparator(String carNames) {
+        return !carNames.contains(CAR_NAME_SEPARATOR + CAR_NAME_SEPARATOR);
     }
 
     private static boolean isValidateNumberOfCars(String[] carNames) {
