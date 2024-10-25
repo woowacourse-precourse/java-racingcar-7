010aab3482a637bbaff01c76051dfad2ff2354e7
hanmh319 <hanmh319@naver.com>
2024-10-25 23:07:28 +0900

    feat: 우승자 출력 메서드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Application {
 
@@ -12,7 +14,8 @@ public class Application {
         String[] individualCarMovements = new String[names.length];
         Arrays.fill(individualCarMovements, ""); // 배열 생성시 빈 문자열로 초기화
         printCarMovements(individualCarMovements, names, attempts);
-
+        List<Integer> checkWinner = checkWinner(individualCarMovements);
+        printWinner(checkWinner, names);
     }
 
     public static void individualRandoms(String[] individualCarMovements, String[] names) { // 1회 이동 메서드
@@ -31,6 +34,38 @@ public class Application {
                                          int attempts) { // 시도
         for (int i = 0; i < attempts; i++) {
             individualRandoms(individualCarMovements, names);
+        }
+    }
+
+    public static List<Integer> checkWinner(String[] individualCarMovements) {
+        List<Integer> index = new ArrayList<>();
+        int max = 0;
+
+        for (int i = 0; i < individualCarMovements.length; i++) {
+            if (individualCarMovements[i].length() > max) {
+                max = individualCarMovements[i].length();
+                index.clear();
+                index.add(i);
+            } else if (individualCarMovements[i].length() == max) {
+                index.add(i);
+            }
+        }
+        return index;
+    }
+
+    public static void printWinner(List<Integer> checkWinner, String[] names) {
+        if (checkWinner.size() == 1) {
+            System.out.print("최종 우승자는 : " + names[checkWinner.getFirst()]);
+            return;
+        }
+        System.out.print("최종 우승자는 : ");
+        for (int i = 0; i < checkWinner.size(); i++) {
+            if (i == checkWinner.size() - 1) {
+                System.out.print(names[checkWinner.get(i)]);
+                break;
+            }
+            System.out.print(names[checkWinner.get(i)] + ", ");
+
         }
     }
 
