7d6783208a993faa36f2a74ff24eb54c15e4dee2
변우영 <likevangogh24@gmail.com>
2024-10-25 19:16:45 +0900

    feat(OutputView): 차수별 실행 결과 출력 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,9 +1,15 @@
 package racingcar.view;
 
+import racingcar.dto.CarStatusDto;
+
+import java.util.List;
+
 public class OutputView {
 
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ROUND_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String NAME_POSITION_SEPARATOR = " : ";
+    private static final String MOVE_DASH = "-";
 
     public void printInputCarNameMessage() {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
@@ -11,5 +17,12 @@ public class OutputView {
 
     public void printInputRoundMessage() {
         System.out.println(INPUT_ROUND_MESSAGE);
+    }
+
+    public void printRoundResult(List<CarStatusDto> roundResult) {
+        for (CarStatusDto carStatusDto : roundResult) {
+            System.out.println(carStatusDto.getName() + NAME_POSITION_SEPARATOR + MOVE_DASH.repeat(carStatusDto.getPosition()));
+        }
+        System.out.println();
     }
 }
