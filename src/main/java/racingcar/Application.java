771c86fb1a97feb7f0f6de0c7f66643744de02a7
wodus4145@naver.com <wodus4145@naver.com>
2024-10-25 14:46:11 +0900

    refactor: Add gap

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,10 +6,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 public class Application {
+
     public boolean stopOrGo() {
         return Randoms.pickNumberInRange(0, 9) >= 4;
     }
+
     public void printEachRound(List<Car> racingCar) {
         for (Car car : racingCar) {
             if (stopOrGo()) {
@@ -20,6 +23,7 @@ public class Application {
         }
         System.out.println();
     }
+
     public String decideWinner(List<Car> racingCar) {
         int maxValue = Integer.MIN_VALUE;
         ArrayList<String> winnerList = new ArrayList<>();
@@ -35,12 +39,14 @@ public class Application {
         }
         return String.join(",", winnerList);
     }
+
     public void playGames(List<Car> racingCar, int numberOfGames) {
         for (int i = 0; i < numberOfGames; i++) {
             printEachRound(racingCar);
         }
         System.out.println("최종 우승자 : " + decideWinner(racingCar));
     }
+
     public static void main(String[] args) throws IllegalArgumentException {
         Application application = new Application();
         InputHandler inputHandler = new InputHandler();
