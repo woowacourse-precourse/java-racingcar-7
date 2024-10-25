7f328f00a2071d59a1d96eaabe7080d5c0b386c0
이준범 <jach1206@naver.com>
2024-10-25 13:28:29 +0900

    feat: 무작위 값이 4 이상일 경우 한 칸 전진하는 기능 구현
    - 게임 진행 순서는 자동차 이름을 입력 받은 순서대로 진행했다.
    - 무작위 값이 4 이상일 경우, HashMap의 Value를 1 더해주었다. Value가 이동한 거리이기 때문이다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,7 +21,21 @@ public class Application {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String gameCount = Console.readLine();
 
+        for (int i = 0; i < Integer.parseInt(gameCount); i++) {
+            startGame(racingCar, splitRacingCarName);
+        }
 
+    }
+
+    // 게임의 순서는 자동차 이름을 입력 받은 순서대로 진행한다.
+    public static void startGame(Map<String, Integer> racingCar, String[] splitRacingCarName) {
+        for (int i = 0; i < racingCar.size(); i++) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+
+            if (randomNumber >= 4) {
+                racingCar.put(splitRacingCarName[i], racingCar.get(splitRacingCarName[i]) + 1);
+            }
+        }
     }
 
 
