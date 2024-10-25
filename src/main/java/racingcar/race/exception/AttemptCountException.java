38ce2d56dea59e576641e76c000c44d6958df2e2
jieun <ju3391@naver.com>
2024-10-25 21:27:15 +0900

    feat(AttemptCountValidator): 시도 횟수 검증 로직 구현
    - 시도 횟수 음수, 0, 숫자 검증 추가

--- src/main/java/racingcar/race/exception/AttemptCountException.java
+++ src/main/java/racingcar/race/exception/AttemptCountException.java
@@ -1,4 +1,10 @@
 package racingcar.race.exception;
 
-public class AttemptCountException {
+import racingcar.common.ExceptionEnum;
+
+// 시도 횟수 관련 예외 처리 클래스
+public class AttemptCountException extends IllegalArgumentException {
+    public AttemptCountException(String message) {
+        super(message);
+    }
 }
