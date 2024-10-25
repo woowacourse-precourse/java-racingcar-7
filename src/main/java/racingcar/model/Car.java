f85f108d0a5e5957172a25d8d6699ec0fa525836
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-10-25 23:29:39 +0900

    fix(model): 자동차의 승리 횟수증가 함수 수정
    - 횟수 증가 연산은 private으로 캡슐화하여 보호

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -24,7 +24,7 @@ public class Car {
     public int getWinStack() {
         return winStack;
     }
-    public void plusWinStack() {
+    private void plusWinStack() {
         this.winStack++;
     }
     public void goRound() {
