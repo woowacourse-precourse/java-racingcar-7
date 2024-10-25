69eac84a11aeca3c88980f7909c0dddfb1355f06
Dev-LHY <dev.leehy@gmail.com>
2024-10-25 22:00:22 +0900

    refactor(invalidInputException): 자동차 이름 유효성 검사 로직 분리 및 상위 메서드 추가
    - 자동차 이름 유효성 검사 통합 메서드 추가 validateCarNames()
    - 접근 제어자 public to private 변경
    - 입력된 자동차 이름 전체 크기 검사 분리 -> validateCarNameSize()
    - 입력된 자동차 이름 공백 입력 검사 분리 -> validateCarNameBlankInput()
    - 입력된 자동차 이름 중복 검사 분리 -> validateDuplicateName()
    - 입력된 자동차 이름 최대 길이 검사 분리 -> validateMaxCarNameLength()

--- src/main/java/racingcar/exception/InvalidInputException.java
+++ src/main/java/racingcar/exception/InvalidInputException.java
@@ -12,28 +12,37 @@ public class InvalidInputException {
         }
     }
 
-    public static void validateCarName(String[] carNames) {
-        if (carNames.length == 1) {
-            throw new IllegalArgumentException(ExceptionMessage.INPUT_AT_LEAST_TWO_CAR.getMessage());
-        }
-
+    public static void validateCarNames(String[] carNames) {
+        Set<String> test = new HashSet<>();
+        validateCarNameSize(carNames);
         for (String carName : carNames) {
-            if (carName == null || carName.trim().isEmpty()) {
-                throw new IllegalArgumentException(ExceptionMessage.INPUT_BLANK_IS_NOT_ALLOWED.getMessage());
-            }
-
-            if (carName.length() > MAX_CAR_NAME_LENGTH) {
-                throw new IllegalArgumentException(ExceptionMessage.EXCEED_MAX_INPUT_LENGTH.getMessage());
-            }
+            validateCarNameBlankInput(carName);
+            validateDuplicateName(carName, test);
+            validateMaxCarNameLength(carName);
         }
     }
 
-    public static void validateDuplicateName(String[] carNames) {
-        Set<String> test = new HashSet<>();
-        for (String carName : carNames) {
-            if (!test.add(carName)) {
-                throw new IllegalArgumentException(ExceptionMessage.DUPLICATE_INPUT.getMessage());
-            }
+    private static void validateCarNameSize(String[] carNames) {
+        if (carNames.length == 1) {
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_AT_LEAST_TWO_CAR.getMessage());
+        }
+    }
+
+    private static void validateCarNameBlankInput(String carName) {
+        if (carName == null || carName.trim().isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_BLANK_IS_NOT_ALLOWED.getMessage());
+        }
+    }
+
+    private static void validateDuplicateName(String carName, Set<String> test) {
+        if (!test.add(carName)) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATE_INPUT.getMessage());
+        }
+    }
+
+    private static void validateMaxCarNameLength(String carName) {
+        if (carName.length() > MAX_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException(ExceptionMessage.EXCEED_MAX_INPUT_LENGTH.getMessage());
         }
     }
 
