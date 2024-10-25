10373f43f5ee21a41b65d3ce3a4323b166410f13
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-26 06:41:19 +0900

    feat: equals, hashCode, toString 오버라이드 추가
    equals : name 필드를 비교하여 같은 값인지 확인
    hashCode : 객체의 고유한 정수값 반환, equals의 해시코드가 동일한
    해시코드 가지도록 보장
    toString : name이 문자열로 반환 보장

--- src/main/java/racingcar/Name.java
+++ src/main/java/racingcar/Name.java
@@ -1,18 +1,42 @@
 package racingcar;
 
+import java.util.Objects;
+
 public class Name {
 
     private final String name;
     private final String NAME_LENGTH_HAVE_TO_UNDER_FIVE = "입력한 이름의 길이는 5보다 클 수 없습니다.";
 
     public Name(String name) {
-        if (name.length() > 5){
+        if (name.length() > 5) {
             throw new IllegalArgumentException(NAME_LENGTH_HAVE_TO_UNDER_FIVE);
         }
         this.name = name;
     }
 
     public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof Name)) {
+            return false;
+        }
+        Name other = (Name) object;
+        return Objects.equals(name, other.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
         return name;
     }
 }
