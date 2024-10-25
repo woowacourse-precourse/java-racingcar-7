f67bc7a7656888b90b02b5147a19b6f316ce83ef
minjun <ijmtylhtf@naver.com>
2024-10-25 13:16:29 +0900

    fix : 공동 우승자일 경우 쉽표 누락 문제 해결

--- src/main/java/racingcar/car/Cars.java
+++ src/main/java/racingcar/car/Cars.java
@@ -2,6 +2,7 @@ package racingcar.car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
 	private List<Car> cars = new ArrayList<>();
@@ -29,9 +30,10 @@ public class Cars {
 	public void printWinner(){
 		System.out.print("최종 우승자 : ");
 		int maxLocation = getMaxLocation();
-		cars.stream()
+		String winners = cars.stream()
 			.filter(car -> car.getLocation() == maxLocation)
 			.map(Car::getName)
-			.forEach((name) -> System.out.print(name + " "));
+			.collect(Collectors.joining(", "));
+		System.out.println(winners);
 	}
 }
