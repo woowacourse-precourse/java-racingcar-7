596b15907f31fdf42997c7dcf6edea3564974cb0
unh6unh6 <ruddbs3308@naver.com>
2024-10-25 18:27:02 +0900

    feat(view): 최초 차수의 진행 현황을 출력하기 전에 `실행 결과` 라는 문구를 출력한다

---
+++ src/main/java/racingcar/view/ProgressView.java
@@ -0,0 +1,9 @@
+package racingcar.view;
+
+public class ProgressView {
+    private static final String FIRST_PROGRESS_MESSAGE = "실행 결과\n";
+
+    public void printFirstProgressMessage() {
+        System.out.println(FIRST_PROGRESS_MESSAGE);
+    }
+}
