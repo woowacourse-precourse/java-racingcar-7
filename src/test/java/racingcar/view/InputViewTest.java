91c180fdaf7893bcaad68df0cd0a2ebc4c0bba2e
SukJin Kim <rkathrwks@gmail.com>
2024-10-26 01:41:38 +0900

    test(inputview): 메서드 반환타입 및 접근 제한자 변경에 따른 테스트 수정

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,21 +1,16 @@
 package racingcar.view;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullSource;
 
 class InputViewTest {
 
-    private final PrintStream standardOut = System.out;
-    private final InputStream standardIn = System.in;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
     private final InputView inputView = new InputView();
 
@@ -26,28 +21,27 @@ class InputViewTest {
 
     @AfterEach
     public void tearDown() {
-        System.setOut(standardOut);
-        System.setIn(standardIn);
+        System.setOut(System.out);
+        System.setIn(System.in);
     }
 
     @Test
     void requestCarNames() {
-        inputView.requestCarNames();
+        String expectedInput = "pobi,woni,jun";
+        System.setIn(new ByteArrayInputStream(expectedInput.getBytes()));
+        String actualInput = inputView.requestCarNames();
+
+        assertEquals(expectedInput, actualInput);
         assertEquals("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)", outputStreamCaptor.toString().trim());
     }
 
     @Test
-    void requestNumAttempt() {
-        inputView.requestNumAttempt();
+    void requestNumAttempts() {
+        String expectedInput = "5";
+        System.setIn(new ByteArrayInputStream(expectedInput.getBytes()));
+        String actualInput = inputView.requestNumAttempts();
+
+        assertEquals(expectedInput, actualInput);
         assertEquals("시도할 횟수는 몇 회인가요?", outputStreamCaptor.toString().trim());
-    }
-
-    @Test
-    void readInput() {
-        String expected = "pobi,woni,jun";
-        System.setIn(new ByteArrayInputStream(expected.getBytes()));
-
-        String actual = inputView.readInput();
-        assertEquals(expected, actual);
     }
 }
