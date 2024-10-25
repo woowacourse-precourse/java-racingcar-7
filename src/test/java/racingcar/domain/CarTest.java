d4e49065e0761384f267f206e80fae87a5951c7e
bowook <mbosx316@naver.com>
2024-10-25 19:28:09 +0900

    test(CarTest) : Car 도메인 테스트
    - 전진기능 테스트
    - 정지기능 테스트

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,20 +1,38 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.validator.CarNameValidator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("bo");
+    }
+
     @Test
     void 움직이는지_테스트() {
         //given
-        Car car = new Car("bo");
-
+        int randomNumber = 4;
         //when
-        car.move();
+        car.move(randomNumber);
 
         //then
         assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void 안_움직이는지_테스트() {
+        //given
+        int randomNumber = 1;
+        //when
+        car.move(randomNumber);
+        //then
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 }
