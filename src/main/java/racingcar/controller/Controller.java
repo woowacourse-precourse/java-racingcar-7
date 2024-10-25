4e14d4830d40f82807e302b6d9efa8e9c3f20bc1
sieunp06 <psieun0606@gmail.com>
2024-10-25 13:15:26 +0900

    feat(Controller): 자동차 이름을 입력 받는 메서드 구현
    View의 read 메서드를 사용하여 자동차의 이름을 입력 받는 메서드를 구현함.

---
+++ src/main/java/racingcar/controller/Controller.java
@@ -0,0 +1,30 @@
+package racingcar.controller;
+
+import racingcar.view.View;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Controller {
+    private final View view;
+
+    private final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    private Controller(View view) {
+        this.view = view;
+    }
+
+    public static Controller run(View view) {
+        return new Controller(view);
+    }
+
+    public void gameStart() {
+        String carNames = inputCarNames();
+    }
+
+    private String inputCarNames() {
+        view.print(CAR_NAME_INPUT_MESSAGE);
+        return view.read();
+    }
+}
