bee7777affc5ac7db5008875e161319b22b2ac66
haeseon lee <haeseon4816@gmail.com>
2024-10-25 16:10:59 +0900

    feat: 자동차 이름 검증 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -8,6 +8,8 @@ public class Car {
     private int position;
 
     public Car(String carName) {
+        isCarNameString(carName);
+        isCarNameLengthFive(carName);
         this.carName = carName;
     }
 
@@ -15,6 +17,20 @@ public class Car {
         int randomNumber = Randoms.pickNumberInRange(0, 9);
         if(randomNumber >= 4){
             position++;
+        }
+    }
+
+    public static void isCarNameString(String carName){
+        for (int i = 0; i < carName.length(); i++) {
+            if(Character.isDigit(carName.charAt(i))){
+                throw new IllegalArgumentException("문자 이외의 값은 입력할 수 없습니다.");
+            }
+        }
+    }
+
+    public static void isCarNameLengthFive(String carName){
+        if(carName.length() > 5){
+            throw new IllegalArgumentException("자동차 이름은 5자 이하로 제한됩니다.");
         }
     }
 }
