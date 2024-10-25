e3513d670e9102fcca8aafbfd7f53582ae839a51
lchyeong <lchyeong@gmail.com>
2024-10-26 00:19:15 +0900

    feat(RacingCarService): 차 이름을 Map의 키로 매핑하는 기능 추가
    - 테스트 포함

--- src/main/java/racingcar/RacingCarService.java
+++ src/main/java/racingcar/RacingCarService.java
@@ -1,20 +1,35 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class RacingCarService {
 
-    public List<String> splitCarNames(String carNames){
+    public List<String> splitCarNames(String carNames) {
         //공백이 넘어온 경우? 구분자가 없는 경우?
         List<String> listCarNames = new ArrayList<>();
         StringTokenizer stringTokenizer = new StringTokenizer(carNames);
 
-        while (stringTokenizer.hasMoreTokens()){
+        while (stringTokenizer.hasMoreTokens()) {
             listCarNames.add(stringTokenizer.nextToken(","));
         }
 
         return listCarNames;
+    }
+
+    public Map<String, String> mapCarNames(List<String> carNames) {
+        Map<String, String> mapCarNames = new HashMap<>();
+
+        for (int i = 0; i < carNames.size(); i++) {
+            mapCarNames.put(carNames.get(i), "");
+        }
+        return mapCarNames;
+    }
+
+    public int createRandomNumber() {
+        return (int) (Math.random() * 10);
     }
 }
--- src/test/java/racingcar/RacingCarServiceTest.java
+++ src/test/java/racingcar/RacingCarServiceTest.java
@@ -1,14 +1,19 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RacingCarServiceTest {
     private final RacingCarService racingCarService = new RacingCarService();
 
     @Test
+    @DisplayName("쉼표(,) 구분자로 문자열 자르기")
     void testSplitCarNames() {
         //이름이 잘못 들어온 경우
 
@@ -16,5 +21,20 @@ class RacingCarServiceTest {
         List<String> resultCarNames = Arrays.asList("car1", "car2", "car3");
 
         Assertions.assertEquals(carNames, resultCarNames);
+    }
+
+    @Test
+    public void testMapCarNames() throws Exception {
+        //given
+        List<String> carNames = new ArrayList<>(Arrays.asList("car1", "car2", "car3"));
+        Map<String, String> expect = new HashMap<>();
+        expect.put("car1", "");
+        expect.put("car2", "");
+        expect.put("car3", "");
+
+        //when
+        Map<String, String> result = racingCarService.mapCarNames(carNames);
+        //then
+        Assertions.assertEquals(expect, result);
     }
 }
