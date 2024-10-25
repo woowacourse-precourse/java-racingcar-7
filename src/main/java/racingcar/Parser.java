af21f577ceb2d7d596a5d55e19131e1a1a7341da
jintakkim <mine407603@gmail.com>
2024-10-25 13:50:37 +0900

    feat(Parser): 파서 기능 구현
    주어진 인풋을 받아 검증 후 필요한 형식으로 변환 후 반환한다.

---
+++ src/main/java/racingcar/Parser.java
@@ -0,0 +1,50 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Parser {
+
+    private final List<String> inputs;
+
+    public Parser(List<String> inputs) {
+        this.inputs = inputs;
+    }
+
+    public List<String> parseCarNameInput() {
+        String line = inputs.getFirst();
+        List<String> carNames = Arrays.asList(line.split(","));
+
+        return carNames.stream()
+                .map(String::trim)
+                .map(this::validateCarName)
+                .collect(Collectors.toList());
+    }
+
+    public int parseRaceRoundInput() {
+        String line = inputs.get(1);
+        return parseRound(line);
+    }
+
+    private int parseRound(String secondLine) {
+        try {
+            int round = Integer.parseInt(secondLine);
+            if(round <= 0) {
+                throw new NumberFormatException();
+            }
+            return round;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자의 형식이 잘못되었습니다.");
+        }
+    }
+
+    private String validateCarName(String carName) {
+        if(carName.isEmpty() || carName.length() > 5) {
+            throw new IllegalArgumentException("올바른 자동차 이름이 아닙니다.");
+        }
+        return carName;
+    }
+}
