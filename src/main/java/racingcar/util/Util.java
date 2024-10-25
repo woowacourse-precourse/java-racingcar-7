b75473bc0d92bcceb6173245eaf7f05ef24ab630
JS <zks145@naver.com>
2024-10-25 14:43:50 +0900

    feat (Util): 전진 가능 여부 결과 메서드 추가

--- src/main/java/racingcar/util/Util.java
+++ src/main/java/racingcar/util/Util.java
@@ -3,7 +3,11 @@ package racingcar.util;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Util {
+    private static final int GO = 4;
 
+    public static boolean isCanGo() {
+        return RandomNumber() >= GO;
+    }
     public static int RandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
