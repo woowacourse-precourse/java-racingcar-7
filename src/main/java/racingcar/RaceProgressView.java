9e63738d5ba9627c670ab845fdd8c524d2ba1958
chaerish <ccyy8432@naver.com>
2024-10-26 02:54:19 +0900

    refactor(RaceProgressView): RaceProgressView에 줄바꿈 추가
    RaceProgressView에 매 회차마다 줄바꿈 출력을 위해 추가한다.

--- src/main/java/racingcar/RaceProgressView.java
+++ src/main/java/racingcar/RaceProgressView.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class RaceProgressView {
 
     public void displayRoundProgress(List<RacingCar> racingCars) {
+        System.out.println();
         for (RacingCar racingCar : racingCars) {
             System.out.println(racingCar.getName() + " : " + racingCar.getDistance());
         }
