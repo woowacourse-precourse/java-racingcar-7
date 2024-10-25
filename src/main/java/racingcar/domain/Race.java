c691c22a7ef9951f747eb26cb3709c5bce467c6e
StoneCAU <seokwoo1999@naver.com>
2024-10-25 19:04:37 +0900

    refactor(Race): Race 객체 구조 변경
    - tryCount, currentCount 변수 추가
    - 유틸리티 클래스를 통해 결괏값을 받아오는 메서드 추가
    - 생성자 매개변수 변경(List<Car> cars -> String carNames, int tryCount)

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -1,19 +1,46 @@
 package racingcar.domain;
 
+import racingcar.utils.CarNameParser;
+import racingcar.utils.RoundResultGenerator;
+import racingcar.utils.WinnerDeterminer;
+
 import java.util.List;
 
 public class Race {
     private final List<Car> cars;
+    private final int tryCount;
+    private int currentCount;
 
-    public Race(List<Car> cars) {
-        this.cars = cars;
+    public Race(String carNames, int tryCount) {
+        this.cars = convertToCars(carNames);
+        this.tryCount = tryCount;
+        this.currentCount = 0;
     }
 
     public List<Car> getCars() {
         return cars;
     }
 
-    public void start() {
+    public void play() {
         cars.forEach(Car::move);
+        currentCount++;
+    }
+
+    public boolean isRaceOver() {
+        return currentCount >= tryCount;
+    }
+
+    public List<String> getRoundResults() {
+        return RoundResultGenerator.generate(cars);
+    }
+
+    public List<String> getWinners() {
+        return WinnerDeterminer.determineWinners(cars);
+    }
+
+    private static List<Car> convertToCars(String carNames) {
+        List<String> carNameList = CarNameParser.parse(carNames);
+
+        return CarFactory.createCars(carNameList);
     }
 }
