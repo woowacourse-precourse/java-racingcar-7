532bab530b73483d41e6fa01cb32d3d98f755646
oseungtaek <tmdxor2580@naver.com>
2024-10-25 10:04:41 +0900

    feat: implement logic to print the race winners

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -60,6 +60,31 @@ public class Application {
         System.out.println();
     }
 
+    private List<String> findWinners(int maxPosition) {
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPositionLength() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
+
+    private int findMaxPosition() {
+        int maxPosition = 0;
+        for (Car car : cars) {
+            maxPosition = Math.max(maxPosition, car.getPositionLength());
+        }
+        return maxPosition;
+    }
+
+    private void printWinners() {
+        int maxPosition = findMaxPosition();
+        List<String> winners = findWinners(maxPosition);
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         new Application().start();
