ea2dd3eaba64b0a300bc71f5c5984ae17a600117
sung <salamander011017@gmail.com>
2024-10-25 20:59:31 +0900

    RaceGameController 클래스 구현
    - MVC 패턴 중 controller 구현
    - playGame()메소드에서 RaceGame 클래스를 매개변수로 받아 진행
    - run() 메소드에서 전체적인 흐름 관리

---
+++ src/main/java/racingcar/controller/RaceGameController.java
@@ -0,0 +1,32 @@
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.model.RaceGame;
+import racingcar.model.RandomNumberGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RaceGameController {
+    InputView inputView;
+    OutputView outputView;
+    RandomNumberGenerator randomNumberGenerator;
+
+    private void playGame(RaceGame game) {
+        for (int i = 0; i < game.getAttempts(); i++) {
+            game.moveAll(randomNumberGenerator);
+            outputView.printRoundResult(game.getCars());
+        }
+        outputView.printWinners(game.getWinners());
+    }
+
+    public void run(){
+        outputView.printGameStart();
+        List<String> carNames = inputView.readCarNames();
+
+        outputView.printAttemptPrompt();
+        int attempts = inputView.readAttempts();
+
+        RaceGame game = new RaceGame(carNames, attempts);
+        playGame(game);
+    }
+}
