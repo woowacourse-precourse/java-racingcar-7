11fc98b030b659ab95a095c2012332d2bf5e1240
unh6unh6 <ruddbs3308@naver.com>
2024-10-25 18:53:36 +0900

    feat(view): 매 차수마다 자동차들의 이동거리 출력

--- src/main/java/racingcar/view/ProgressView.java
+++ src/main/java/racingcar/view/ProgressView.java
@@ -1,9 +1,30 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.dto.CarDto;
+
 public class ProgressView {
     private static final String FIRST_PROGRESS_MESSAGE = "실행 결과\n";
 
     public void printFirstProgressMessage() {
         System.out.println(FIRST_PROGRESS_MESSAGE);
+    }
+
+    public void printProgressMessage(List<CarDto> CarDtoList) {
+        for (CarDto carDto : CarDtoList) {
+            String carName = carDto.name();
+            System.out.println(carName + " : ");
+
+            long position = carDto.position();
+            printDashesByPosition(position);
+
+            System.out.println();
+        }
+    }
+
+    private void printDashesByPosition(long position) {
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
     }
 }
