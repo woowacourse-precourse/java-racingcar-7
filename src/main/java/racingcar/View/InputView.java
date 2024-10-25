1e46dd96de44bad000ca2c70a7be547643633b8a
jonghwanlee <kavae550@gmail.com>
2024-10-26 01:50:54 +0900

    feat(view): 입력값 받는 기능 구현
    사용자에게 자동차들의 이름과 시도할 횟수를 입력받는 기능 구현
    이후 기존 메세지 출력 메서드와 병합

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -1,11 +1,17 @@
 package racingcar.View;
 
+import camp.nextstep.edu.missionutils.Console;
 import racingcar.View.constants.Notice;
 
 public class InputView {
-    public static void printInsertMessage(Notice notice) {
-        System.out.println(notice.getMessage());
+    public static String requestCarNames(){
+        System.out.println(Notice.ASK_CAR_NAMES);
+        return Console.readLine();
     }
 
+    public static String requestNumberOfTrials(){
+        System.out.println(Notice.ASK_NUMBER_OF_TRIALS);
+        return Console.readLine();
+    }
 
 }
--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -1,13 +1,12 @@
 package racingcar.controller;
 
 import racingcar.View.InputView;
-import racingcar.View.constants.Notice;
 
 public class Game {
 
     public void run(){
-        InputView.printInsertMessage(Notice.ASK_CAR_NAMES);
-        InputView.printInsertMessage(Notice.ASK_NUMBER_OF_TRIALS);
+        String carNames = InputView.requestCarNames();
+        String rawNumberOfTrials = InputView.requestNumberOfTrials();
 
     }
 
