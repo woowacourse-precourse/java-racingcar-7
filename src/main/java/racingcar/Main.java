3a3d2587daae38ea4d97720896e160e9ba5a7bf2
TalkingPotato90 <star901210@hanmail.net>
2024-10-25 16:51:25 +0900

    feat: 자동차 이름 입력 받기

---
+++ src/main/java/racingcar/Main.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Main {
+    public String receiveCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        return input;
+    }
+}
---
+++ src/test/java/racingcar/MainTest.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MainTest extends NsTest {
+    private Main main = new Main();
+
+    @Test
+    @DisplayName("자동차 이름을 입력받는다")
+    void carNamesInput() {
+        assertSimpleTest(() -> {
+            run("pobi,woni,jun");
+            assertThat(main.receiveCarNames()).isEqualTo("pobi,woni,jun");
+        });
+    }
+
+    @Override
+    protected void runMain() {
+
+    }
+}
