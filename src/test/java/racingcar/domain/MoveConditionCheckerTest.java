953cd21b328ac3eec067417359c8957d9e9dc40c
junest66 <chlwndks333@gmail.com>
2024-10-25 13:26:51 +0900

    test(MoveConditionCheckerTest): 테스트 숫자 생성기 숫자 지정 생성자 방식으로 인한 전진조건 테스트 수정

--- src/test/java/racingcar/domain/MoveConditionCheckerTest.java
+++ src/test/java/racingcar/domain/MoveConditionCheckerTest.java
@@ -2,26 +2,20 @@ package racingcar.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class MoveConditionCheckerTest {
     private MoveConditionChecker moveConditionChecker;
-    private TestNumberGenerator numberGenerator;
-
-    @BeforeEach
-    void setUp() {
-        this.numberGenerator = new TestNumberGenerator();
-        this.moveConditionChecker = new MoveConditionChecker(numberGenerator);
-    }
+    private NumberGenerator numberGenerator;
 
     @ParameterizedTest
     @CsvSource(value = {"4", "5", "6", "7", "8", "9"})
     @DisplayName("숫자생성기가 4이상을 생성하면 전진조건을 만족한다.")
     void canMove(int number) {
-        numberGenerator.setNumber(number);
+        numberGenerator = new TestNumberGenerator(number);
+        moveConditionChecker = new MoveConditionChecker(numberGenerator);
         assertThat(moveConditionChecker.canMove()).isTrue();
     }
 
@@ -29,7 +23,8 @@ class MoveConditionCheckerTest {
     @CsvSource(value = {"0", "1", "2", "3"})
     @DisplayName("숫자생성기가 4미만 생성하면 전진조건을 만족시키지 못한다.")
     void cannotMove(int number) {
-        numberGenerator.setNumber(number);
+        numberGenerator = new TestNumberGenerator(number);
+        moveConditionChecker = new MoveConditionChecker(numberGenerator);
         assertThat(moveConditionChecker.canMove()).isFalse();
     }
 }
