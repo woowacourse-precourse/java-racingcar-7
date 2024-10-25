2c613e83e75b3cfd3f60e23e4544a67afdef1969
kimgoat <kimgoatlove@gmail.com>
2024-10-25 17:15:55 +0900

    feat(InvalidInputExceptionMessage): 입력값 예외 처리 메시지 상수 클래스 생성

---
+++ src/main/java/racingcar/global/constant/InvalidInputExceptionMessage.java
@@ -0,0 +1,17 @@
+package racingcar.global.constant;
+
+import racingcar.global.validator.InputValidator.ValidationMode;
+
+public class InvalidInputExceptionMessage {
+    public static final String INVALID_CAR_NAME_LENGTH_MESSAGE =
+            String.format("자동차 이름은 %d자 이상 %d자 이하로 입력해 주세요.", ValidationMode.CAR_NAME.min, ValidationMode.CAR_NAME.max);
+
+    public static final String EMPTY_CAR_NAME_MESSAGE =
+            "자동차 이름을 입력해 주세요.";
+
+    public static final String INVALID_CAR_COUNT_MESSAGE =
+            String.format("자동차는 %d대 이상 %d 이하로 등록해 주세요.", ValidationMode.CAR_COUNT.min, ValidationMode.CAR_COUNT.max);
+
+    public static final String INVALID_GAME_COUNT_MESSAGE =
+            String.format("시도 횟수는 %d 이상 %d 이하의 자연수로 입력해 주세요.", ValidationMode.GAME_COUNT.min, ValidationMode.GAME_COUNT.max);
+}
