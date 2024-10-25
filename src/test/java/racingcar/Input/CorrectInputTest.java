dcf82ce4e277bea83c335e149d59670a1bb84d33
zzzyoonnn <dwsrh03@gmail.com>
2024-10-25 15:51:58 +0900

    feat: 올바른 문자열인지 확인하는 CorrectInputTest 구현

--- src/test/java/racingcar/Input/CorrectInputTest.java
+++ src/test/java/racingcar/Input/CorrectInputTest.java
@@ -1,17 +1,16 @@
 package racingcar.Input;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static racingcar.Input.InputString.inputString;
+import static racingcar.Input.InputString.isCorrectInput;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 import racingcar.Application;
 
-class InputTest extends NsTest {
+class CorrectInputTest extends NsTest {
 
     @Test
     public void 올바른_입력_값이다() {
@@ -19,10 +18,10 @@ class InputTest extends NsTest {
         String input = "pobi,woni,jun";
 
         // when
-        String result = inputString(input);
+        boolean result = isCorrectInput(input);
 
         // then
-        assertEquals("문자열이 정규식에 맞습니다.", result);            // junit assertion
+        assertTrue(result);
     }
 
     @Test
@@ -31,10 +30,10 @@ class InputTest extends NsTest {
         String input = "pobi123,woni1,jun6";
 
         // when
-        String result = inputString(input);
+        boolean result = isCorrectInput(input);
 
         // then
-        assertEquals("문자열이 정규식에 맞습니다.", result);            // junit assertion
+        assertTrue(result);
     }
 
     @Test
@@ -43,10 +42,10 @@ class InputTest extends NsTest {
         String input = "pobi!@#,woni!!!,jun^0^";
 
         // when
-        String result = inputString(input);
+        boolean result = isCorrectInput(input);
 
         // then
-        assertEquals("문자열이 정규식에 맞지 않습니다.", result);            // junit assertion
+        assertFalse(result);
     }
 
     @Test
@@ -55,10 +54,10 @@ class InputTest extends NsTest {
         String input = "pobi,woni,";
 
         // when
-        String result = inputString(input);
+        boolean result = isCorrectInput(input);
 
         // then
-        assertEquals("문자열이 정규식에 맞지 않습니다.", result);            // junit assertion
+        assertFalse(result);
     }
 
     @Override
