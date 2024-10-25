959af2dcd750bffb396be52e7f0d4d97d7cfedba
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 15:32:49 +0900

    feat: 값을 입력받는 기능을 추가한다

--- src/main/java/racingcar/util/InputCarNames.java
+++ src/main/java/racingcar/util/InputCarNames.java
@@ -1,8 +1,19 @@
 package racingcar.util;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class InputCarNames {
+
+    public static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public List<String> inputNames() {
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
+        String cars = Console.readLine();
+
+        return parsingName(cars);
+    }
 
     public List<String> parsingName(String cars) {
 
