c6181508f42cf94a36eb773cece0c287d849db7f
junest66 <chlwndks333@gmail.com>
2024-10-25 11:02:36 +0900

    feat(Application): 애플리케이션 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.RacingCarController;
+import racingcar.dto.RacingResult;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingCarController racingCarController = new RacingCarController();
+        try {
+            String carNames = InputView.inputCarNames();
+            String tryCount = InputView.inputTryCount();
+            RacingResult racingResult = racingCarController.getRacingResult(carNames, tryCount);
+            OutputView.printResult(racingResult);
+        } finally {
+            Console.close();
+        }
     }
 }
