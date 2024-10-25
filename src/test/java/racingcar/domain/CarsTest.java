48c7a01a39adcb5c1c3ad5f7397532c510a12146
songsunkook <songsunkook@gmail.com>
2024-10-25 16:07:50 +0900

    test: 승자 판정 테스트 작성

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CarsTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"0,0", "0,1", "2,2", "2,3", "4,3", "100,101"})
+    void 정상적으로_승자를_판단하는가(int position1, int position2) {
+        Cars cars = new Cars();
+        Car car1 = new Car("pobi", position1);
+        Car car2 = new Car("woni", position2);
+        cars.add(car1);
+        cars.add(car2);
+        if (position1 == position2) {
+            assertThat(cars.getWinners().get(0).getPosition()).isEqualsTo(car1);
+            assertThat(cars.getWinners().get(1).getPosition()).isEqualsTo(car2);
+            return;
+        }
+        if (position1 > position2) {
+            assertThat(cars.getWinners().get(0).getPosition()).isEqualsTo(car1);
+            return;
+        }
+        assertThat(cars.getWinners().get(0).getPosition()).isEqualsTo(car2);
+    }
+}
