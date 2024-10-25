4bd546b6037409457e1417b0b59bd33c5de92c12
eunho <eunho1032@gmail.com>
2024-10-25 16:04:11 +0900

    refactor(Input): using tabs change spaces

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -4,24 +4,24 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
-	private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-	private static final String INPUT_TRY_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
-	public static String[] inputCarNames() {
+    public static String[] inputCarNames() {
 
-		System.out.println(INPUT_NAME_MESSAGE);
-		String[] carNames = splitCarNames(Console.readLine());
-		Validation.validateCarNames(carNames);
-		return carNames;
-	}
+        System.out.println(INPUT_NAME_MESSAGE);
+        String[] carNames = splitCarNames(Console.readLine());
+        Validation.validateCarNames(carNames);
+        return carNames;
+    }
 
-	private static String[] splitCarNames(String input) {
-		return input.split(",");
-	}
+    private static String[] splitCarNames(String input) {
+        return input.split(",");
+    }
 
-	public static int inputTryCounts() {
-		System.out.println(INPUT_TRY_MESSAGE);
-		String input = Console.readLine();
-		return Validation.validateTryCounts(input);
-	}
+    public static int inputTryCounts() {
+        System.out.println(INPUT_TRY_MESSAGE);
+        String input = Console.readLine();
+        return Validation.validateTryCounts(input);
+    }
 }
