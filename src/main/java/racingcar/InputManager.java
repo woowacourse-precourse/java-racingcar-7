90ce811a8f45689dcd66ffb5297b9c02db926e3e
joonwan <joonwan1024@naver.com>
2024-10-25 20:12:15 +0900

    feat: 시도 횟수 입력 기능 구현

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputManager {
 
     private static final String INVALID_USER_INPUT_MESSAGE = "빈 문자열을 입력할 수 없습니다.";
+    private static final String INVALID_TRY_COUNT_MESSAGE = "시도 횟수는 1 부터 2147483647 까지의 정수값만 입력할 수 있습니다.";
 
     public static String readUserInput() {
         OutputManager.printGuideMessage();
@@ -13,6 +14,22 @@ public class InputManager {
             throw new IllegalArgumentException(INVALID_USER_INPUT_MESSAGE);
         }
         return input;
+    }
+
+    public static int readTryCount() {
+        OutputManager.printTryCountMessage();
+        int tryCount = 0;
+        try {
+            String userInput = Console.readLine();
+            tryCount = Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_TRY_COUNT_MESSAGE);
+        }
+
+        if (tryCount == 0) {
+            throw new IllegalArgumentException(INVALID_TRY_COUNT_MESSAGE);
+        }
+        return tryCount;
     }
 
     public static void close() {
--- src/main/java/racingcar/OutputManager.java
+++ src/main/java/racingcar/OutputManager.java
@@ -6,6 +6,7 @@ public class OutputManager {
 
     private static final String GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String WINNER_RESULT_MESSAGE = "최종 우승자 : ";
+    private static final String TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private static final String DELIMITER = ", ";
 
     public static void printGuideMessage() {
@@ -15,5 +16,9 @@ public class OutputManager {
     public static void printWinner(List<String> winnerList) {
         String joinedName = String.join(DELIMITER, winnerList);
         System.out.println(WINNER_RESULT_MESSAGE + joinedName);
+    }
+
+    public static void printTryCountMessage() {
+        System.out.println(TRY_COUNT_MESSAGE);
     }
 }
