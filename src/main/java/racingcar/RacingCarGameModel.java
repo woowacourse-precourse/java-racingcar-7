31f3180a034be18d89b5517a3e6fe5f5cc94df3a
BHC-Chicken <tij05078@gmail.com>
2024-10-25 16:39:37 +0900

    feat(init): 쉼표 기준 게임 참가자 분리 및 이름 검증
    입력된 문자열을 쉼표(,) 기준으로 분리 후 이름이 5글자 이하인지 검증합니다.

---
+++ src/main/java/racingcar/RacingCarGameModel.java
@@ -0,0 +1,27 @@
+package racingcar;
+
+import racingcar.dto.RacingGameInitializationRequest;
+
+import java.util.ArrayList;
+
+public class RacingCarGameModel {
+    ArrayList<ParticipantEntity> participantEntities = new ArrayList<>();
+
+    public void initializationGame(RacingGameInitializationRequest request) {
+        String[] participants = request.participants().split(",");
+
+        for (String participant : participants) {
+            participantEntities.add(new ParticipantEntity(nameValidation(participant)));
+        }
+    }
+
+    private String nameValidation(String name) {
+        name = name.trim();
+
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("참가자의 이름은 5자 이하만 가능합니다.");
+        }
+
+        return name;
+    }
+}
