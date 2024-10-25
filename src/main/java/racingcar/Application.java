897123e9c32880d2a7700a2eeca5b7decd70a82b
dnjswls <dnjswls6670@naver.com>
2024-10-26 02:55:40 +0900

    feat: 사용자로부터 자동차 이름 입력받기 및 유효성 검사 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 값  받아오기
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+        // 쉼표를 기준으로 분리
+        String[] carNames = input.split(",");
+
+        for(String name : carNames) {
+            if(name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
+
+        // 이름이 올바르게 입력되었음을 확인
+        System.out.println("자동차 이름이 올바르게 입력되었습니다.");
     }
 }
