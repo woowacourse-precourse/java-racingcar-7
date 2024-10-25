117d16287c3d9b0f90568a8f16e7be30d3d397ce
hanmh319 <hanmh319@naver.com>
2024-10-25 19:10:15 +0900

    feat: 이동 시도 횟수 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,6 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         String[] names = Console.readLine().split(",");
-
+        int attemtps = Integer.parseInt(Console.readLine());
     }
 }
