d8cba9ab6e4f4f2582339e04596db834ad02e024
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-10-26 01:22:28 +0900

    feat(Application): 메인 메서드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,14 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.service.RacingCarService;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingCarService racingCarService = new RacingCarService();
+        RacingCarController racingCarController = new RacingCarController(racingCarService);
+
+        racingCarController.RunRacingGame();
     }
-
-
 }
