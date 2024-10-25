46017d9cc78e6265cd7648b9f56b4f2bc03a9dfd
skilt <robot4440@naver.com>
2024-10-25 21:33:20 +0900

    feat: 최종 우승자 출력 기능

--- src/main/java/racingcar/CarRace.java
+++ src/main/java/racingcar/CarRace.java
@@ -5,28 +5,31 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class CarRace {
   public void startRace(String[] carName, int count){
-    if(count == 0){
-      // count가 0이면 공동우승으로 처리
-    }
     HashMap<Integer, Integer> carList = new HashMap<>();
+    int bestScore = 0;
     for(int i = 0; i<carName.length; i++){
       carList.put(i,0); // 초기값 세팅
     }
-    System.out.println("\n실행 결과");
-    for(int i = 0; i<count; i++){
-      currentRound(carName, carList); // 순차별 진행
+    System.out.println();
+    if(count != 0){
+      System.out.println("실행 결과");
+      for(int i = 0; i<count; i++){
+        bestScore = Math.max(currentRound(carName, carList),bestScore); // 순차별 진행
+      }
     }
-    // 최종 결과 집계
-    // 순차별 진행을 통해서 최댓값을 저장하고 아웃풋으로 같이 보낸다.
+    Output.raceResult(carName, carList, bestScore); // 최종 결과 호출
   }
 
-  private void currentRound(String[] carName, HashMap<Integer, Integer> carList){
+  private int currentRound(String[] carName, HashMap<Integer, Integer> carList){
+    int roundBestScore = -1;
     for(int i = 0; i<carList.size(); i++){
       if(Randoms.pickNumberInRange(0,9) >= 4){
-        carList.put(i,carList.getOrDefault(i,carList.get(i))+1); // 랜덤함수를 사용하여 4 이상이면 1을 더한다.
+        carList.put(i,carList.getOrDefault(i,0)+1); // 랜덤함수를 사용하여 4 이상이면 1을 더한다.
       }
       System.out.println(carName[i]+" : "+"-".repeat(carList.get(i)));
+      roundBestScore = Math.max(roundBestScore,carList.get(i)); //  순차별로 최댓값을 집계한다.
     }
     System.out.println();
+    return roundBestScore;
   }
 }
---
+++ src/main/java/racingcar/Output.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import java.util.HashMap;
+
+public class Output {
+  public static void raceResult(String[] carName, HashMap<Integer, Integer> carList, int bestScore){
+    StringBuilder sb = new StringBuilder();
+    sb.append("최종 우승자 : ");
+    for(int i = 0; i<carName.length; i++){
+      if(carList.get(i) >= bestScore){
+        sb.append(carName[i]);
+        sb.append(", ");
+      }
+    }
+    sb.delete(sb.length()-2,sb.length());
+    System.out.println(sb);
+  }
+}
