ac9745145adb26fb668babf3d44f9a3bd97ee518
이준범 <jach1206@naver.com>
2024-10-25 12:51:52 +0900

    feat: 쉼표 기준으로 자동차 이름을 구분하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
@@ -8,9 +10,18 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String racingCarName = Console.readLine();
 
+        Map<String, Integer> racingCar = new HashMap<>();
+
+        String[] splitRacingCarName = racingCarName.split(",");
+        for (String eachRacingCarName : splitRacingCarName) {
+            racingCar.put(eachRacingCarName, 0);
+        }
+
         System.out.println("시도할 횟수는 몇 회인가요?");
         String gameCount = Console.readLine();
 
 
     }
+
+
 }
