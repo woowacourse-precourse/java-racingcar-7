c29fc4cc8638484389d0874334c42e19566e84ff
Leejunho <junho767@naver.com>
2024-10-26 03:07:05 +0900

    feat: 컨트롤러에서 입력 실행

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,8 +1,21 @@
 package racingcar.controller;
 
-public class RacingController {
+import racingcar.model.Car;
+import racingcar.utils.Utils;
+import racingcar.view.InputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingController {
+    private final List<Car> cars = new ArrayList<>();
+    private int tryNumber;
     // 컨트롤러 실행
     public void run() {
+        inputInfo();
+    }
+    public void inputInfo(){
+        String input = InputView.inputCarNames();
+        tryNumber = Integer.parseInt(InputView.inputTryNumber());
     }
 }
