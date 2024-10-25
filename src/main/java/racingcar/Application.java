375f9378f1d573cb92e27ad316987f0940ec1310
KF97 <aysul0@naver.com>
2024-10-26 07:17:13 +0900

    refactor: 변수명 리팩토링

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,29 +10,29 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
-        String input = Console.readLine();
-        Validator.validateNameEmpty(input);
+        String carNamesInput = Console.readLine();
+        Validator.validateNameEmpty(carNamesInput);
 
-        String[] names = input.split(",");
+        String[] names = carNamesInput.split(",");
         List<String> nameList = new ArrayList<>();
         List<Car> cars = new ArrayList<>();
         for (String name : names) {
-            String trim_name = name.trim(); // 이름 양 끝 공백 제거
-            Validator.validateNameLength(trim_name);
-            Validator.validateNameDuplicate(trim_name, nameList);
-            nameList.add(trim_name);
-            cars.add(new Car(trim_name, ""));
+            String trimName = name.trim(); // 이름 양 끝 공백 제거
+            Validator.validateNameLength(trimName);
+            Validator.validateNameDuplicate(trimName, nameList);
+            nameList.add(trimName);
+            cars.add(new Car(trimName, ""));
         }
 
         System.out.println("시도할 횟수는 몇 회인가요?");
 
-        String input2 = Console.readLine();
-        int count = Validator.validateAndParseInteger(input2);
-        Validator.validatePositive(count);
+        String attemptCountInput = Console.readLine();
+        int attemptCount = Validator.validateAndParseInteger(attemptCountInput);
+        Validator.validatePositive(attemptCount);
 
         System.out.println("\n실행 결과");
 
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < attemptCount; i++) {
             for (Car car : cars) {
                 int random = Randoms.pickNumberInRange(0, 9);
                 if (random >= 4) {
