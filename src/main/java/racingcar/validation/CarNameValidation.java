47be59282e9f80237d3bd90d6f991dc39abf37b1
LeeDongHoon <likelion.leedh@gmail.com>
2024-10-25 15:09:32 +0900

    [REFACTOR]<CarNameValidation> : 입려된 차 이름 유효성 검사 로직 수정
    변경
    
    RacingGame 클래스에서 각 자동차 이름마다 유효성 검사
    -> validate 메서드에서 자동차 이름 리스트를 파라미터로 받아서 유효성 검사
    
    BREAKING CHANGE : RacingGame 클래스에서 자동차 이름을 리스트로 일괄 검증하므로, validate 메서드를 호출할 때 단일 이름이 아닌 리스트를 전달

--- src/main/java/racingcar/validation/CarNameValidation.java
+++ src/main/java/racingcar/validation/CarNameValidation.java
@@ -2,12 +2,16 @@ package racingcar.validation;
 
 import racingcar.exception.RacingExceptionStatus;
 
+import java.util.List;
+
 
 public class CarNameValidation {
 
-    public static void validate(String carName) {
-        checkCarNameLength(carName);
-        checkCarNameBlank(carName);
+    public static void validate(List<String> carNames){
+        for(String carName : carNames){
+            checkCarNameBlank(carName);
+            checkCarNameLength(carName);
+        }
     }
 
     private static void checkCarNameLength(String carName) {
--- src/test/java/racingcar/validationTest/CarNameValidationTest.java
+++ src/test/java/racingcar/validationTest/CarNameValidationTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import racingcar.exception.RacingExceptionStatus;
 import racingcar.validation.CarNameValidation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CarNameValidationTest {
@@ -12,15 +15,22 @@ public class CarNameValidationTest {
     @Test
     @DisplayName("올바른 차 이름 입력")
     void 시도_횟수_입력_성공(){
-        String names = "  asd  ";
+
+        List<String> names = new ArrayList<>();
+        names.add(" name ");
+        names.add("name");
         assertDoesNotThrow(() -> CarNameValidation.validate(names));
     }
 
     @Test
     @DisplayName("자동차 이름이 5자를 넘는 경우")
     void validate_carNameTooLong() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            CarNameValidation.validate("LongName");
+
+        List<String> names = new ArrayList<>();
+        names.add("LongName");
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> {
+                    CarNameValidation.validate(names);
         });
         assertEquals(RacingExceptionStatus.INVALID_NAME_LENGTH.getMessage(), exception.getMessage());
     }
@@ -28,8 +38,13 @@ public class CarNameValidationTest {
     @Test
     @DisplayName("자동차 이름이 빈 문자열인 경우")
     void validate_carNameBlank() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            CarNameValidation.validate("");
+
+        List<String> names = new ArrayList<>();
+        names.add("");
+
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> {
+                    CarNameValidation.validate(names);
         });
         assertEquals(RacingExceptionStatus.INVALID_NAME_BLANK.getMessage(), exception.getMessage());
     }
@@ -37,8 +52,13 @@ public class CarNameValidationTest {
     @Test
     @DisplayName("자동차 이름이 공백만 포함된 경우")
     void validate_carNameWithOnlySpace() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            CarNameValidation.validate(" ");
+
+        List<String> names = new ArrayList<>();
+        names.add(" ");
+
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> {
+                    CarNameValidation.validate(names);
         });
         assertEquals(RacingExceptionStatus.INVALID_NAME_BLANK.getMessage(), exception.getMessage());
     }
