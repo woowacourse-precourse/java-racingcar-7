c71e4f6ca292800b89f4e0f2b4cd8324872bbba2
binn <anyubin0724@naver.com>
2024-10-25 23:16:54 +0900

    test: 우승자 출력 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,12 +1,20 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+import racingcar.service.ParsingCarNameService;
+import racingcar.view.OutputView;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -29,6 +37,17 @@ class ApplicationTest extends NsTest {
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 우승자_출력_테스트() {
+        List<Car> winners = Arrays.asList(new Car("pobi"), new Car("crong"));
+        String expectedOutput = "최종 우승자 : pobi, crong\n";
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        OutputView.printFinalResults(winners);
+        assertEquals(expectedOutput, outputStream.toString());
     }
 
     @Override
