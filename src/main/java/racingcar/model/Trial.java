35f51e894569e5d067c9006d8cba89e27ca3aa03
dradnats1012 <herjebi1012@gmail.com>
2024-10-25 18:13:41 +0900

    feat: 시도횟수 검증 클래스 완성

--- src/main/java/racingcar/model/Trial.java
+++ src/main/java/racingcar/model/Trial.java
@@ -1,4 +1,31 @@
 package racingcar.model;
 
 public class Trial {
+
+    private final int trial;
+
+    public Trial(String trial) {
+        this.trial = convertToInteger(trial);
+    }
+
+    private int convertToInteger(String trial) {
+        try {
+            int trialNum = Integer.parseInt(trial);
+            validateNatural(trialNum);
+
+            return trialNum;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력해주세요!");
+        }
+    }
+
+    private void validateNatural(int trialNum) {
+        if (trialNum <= 0) {
+            throw new IllegalArgumentException("자연수만 입력해주세요!");
+        }
+    }
+
+    public int getTrial() {
+        return trial;
+    }
 }
