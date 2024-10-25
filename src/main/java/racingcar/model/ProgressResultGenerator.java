bfe016a9efa3863dd394019f1bc892eecb9c0760
Chan531 <chan657312@gmail.com>
2024-10-25 16:24:28 +0900

    feat(ProgressResultGenerator): 중간 결과 생성

--- src/main/java/racingcar/model/ProgressResultGenerator.java
+++ src/main/java/racingcar/model/ProgressResultGenerator.java
@@ -1,10 +1,19 @@
 package racingcar.model;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ProgressResultGenerator {
 
 	private static final String CAR_MOVEMENT_SYMBOL = "-";
 	private static final String SEPARATOR = " : ";
 	private static final String LINE_SEPARATOR = "\n";
+
+	public static String generateProgressResult(List<Car> cars) {
+		return cars.stream()
+				.map(ProgressResultGenerator::generateCarStatus)
+				.collect(Collectors.joining(LINE_SEPARATOR));
+	}
 
 	private static String generateCarStatus(Car car) {
 		return car.getName() + SEPARATOR + formatMovementStatus(car.getMovement());
