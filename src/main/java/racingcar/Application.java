6e2cfceb7bd309a9338a38828e29448bf995ac2b
thoi2 <kinggun1505@naver.com>
2024-10-25 18:24:43 +0900

    Add readme 4 실행결과 출력 기능 Revise readme 5 이동횟수에 따른 실행결과 반복 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,6 +11,8 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputName = Console.readLine();
         playerNameList(inputName);
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int inputNum = Integer.parseInt(Console.readLine());
 
     }
 
@@ -32,8 +34,13 @@ public class Application {
         } else return 0;
     }
 
-    public static void racingByCount(Map<String, Integer>playerNameList,int inputTryCount){
+    public static void racing(Map<String, Integer>playerNameList){
+        for(String key :playerNameList.keySet()){
+            playerNameList.replace(key,playerNameList.get(key)+goAndStopNum());
+            System.out.println(key + " : "+"-".repeat(playerNameList.get(key)));
 
+        }
     }
+
 
 }
