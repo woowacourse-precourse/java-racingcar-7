1b9183412b14a6ae5999ae04d0547cf5df1fae63
siwoo <k74415834@gmail.com>
2024-10-26 00:29:29 +0900

    feat(RacingServiceImpl): 승자를 선정하는 로직 구현
    - Car 객체에서 승자에 대한 정보를 보유하도록 구현

--- src/main/java/racingcar/racing/service/RacingServiceImpl.java
+++ src/main/java/racingcar/racing/service/RacingServiceImpl.java
@@ -1,7 +1,9 @@
 package racingcar.racing.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 import racingcar.global.annotation.Service;
+import racingcar.racing.domain.Car;
 import racingcar.racing.dto.request.RacingRequestDTO;
 import racingcar.racing.dto.response.RacingResponseDTO;
 
@@ -15,5 +17,15 @@ public class RacingServiceImpl implements RacingService {
 
     private Integer randomMove() {
         return Boolean.compare(4 <= Randoms.pickNumberInRange(0, 9), false);
+    }
+
+    private void setWinners(List<Car> cars) {
+        int maxPosition = cars
+                .stream().mapToInt(Car::getPosition)
+                .max()
+                .orElseThrow(IllegalStateException::new);
+        cars
+                .stream().filter(car -> car.getPosition() == maxPosition)
+                .forEach(Car::setWinner);
     }
 }
