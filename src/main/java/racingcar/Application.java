d4e256e653408a0029e31a8d7b769be4ada6e916
chaeyeon0130 <k84292330@gmail.com>
2024-10-25 16:52:12 +0900

    feat(Application): 자동차 이름 파싱
    - 쉼표(,)를 기준으로 입력된 문자열을 분리하여 각 자동차의 이름을 파싱한다.
    - 각 이름의 앞뒤 공백을 제거한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,18 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
+        String[] tokens = input.split(",");
+        List<String> cars = new ArrayList<>();
+        for (String token : tokens) {
+            String car = token.trim();
+            cars.add(car);
+        }
     }
 }
