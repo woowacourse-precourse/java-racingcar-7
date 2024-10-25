3ab42a7bcbce037f3cde3c624378bd53c3be4c25
JeongHyeon Jo <wjdgusdlekd3@yonsei.ac.kr>
2024-10-25 17:11:53 +0900

    feat(controller): CarRacingController feat create Model
    - Input되는 정보들을 받아와서 컨트롤러 기능에 맞게 처리
    - 여러개의 차량 모델 생성
    - 차량 레이싱 모델 생성

--- src/main/java/racingcar/controller/CarRacingController.java
+++ src/main/java/racingcar/controller/CarRacingController.java
@@ -1,4 +1,53 @@
 package racingcar.controller;
 
+
+import racingcar.model.Car;
+import racingcar.model.CarRacing;
+import racingcar.utils.Engine;
+import racingcar.utils.RandomEngine;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarRacingController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public CarRacingController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void run() {
+        // run 쪼개기
+        outputView.printReadCarNameMessage();
+        String readCarString = inputView.readInput();
+        inputView.parserInputString(readCarString);
+
+        List<Car> cars = createCars(inputView.getParserInputString());
+
+        outputView.printReadTotalTryMessage();
+        String totalRacingCounter = inputView.readInput();
+
+        startCarRacing(cars, totalRacingCounter);
+    }
+
+    private List<Car> createCars(ArrayList<String> carNameList) {
+        Engine randomEngine = new RandomEngine();
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNameList) {
+            Car car = new Car(carName, randomEngine);
+            cars.add(car);
+        }
+        return cars;
+    }
+
+    private void startCarRacing(List<Car> cars, String totalRacingCounter) {
+        CarRacing carRacing = new CarRacing(cars, totalRacingCounter);
+        carRacing.startRacing();
+    }
+
 }
