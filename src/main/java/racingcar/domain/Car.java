0824f1a8943a20e35c2e6da378c43ce7f438d6db
junest66 <chlwndks333@gmail.com>
2024-10-25 10:37:20 +0900

    feat(Car): Car 도메인 클래스 추가
    - 전진 메서드 추가
    - 우승자인지 확인하는 메서드 추가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,32 @@
+package racingcar.domain;
+
+public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public Car(Car other) {
+        this.name = other.name;
+        this.position = other.position;
+    }
+
+    public void move() {
+        this.position++;
+    }
+
+    public boolean isWinner(int winnerPosition) {
+        return this.position == winnerPosition;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
