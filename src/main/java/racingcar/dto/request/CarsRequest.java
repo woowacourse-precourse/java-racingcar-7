e7ce989f86403da87ee3151c810dbf7ca510de11
Jiihyun <cwlgusc@gmail.com>
2024-10-25 20:07:14 +0900

    feat: 경주 인원 검증 - 최소 2명부터 경주 가능하며 혼자일시 예외처리

--- src/main/java/racingcar/dto/request/CarsRequest.java
+++ src/main/java/racingcar/dto/request/CarsRequest.java
@@ -11,6 +11,10 @@ import java.util.List;
 public record CarsRequest(
         String carNames
 ) {
+
+    public static final String DELIMITER = ",";
+    public static final int MINIMUM_NUMBER_OF_CARS = 2;
+
     public Cars toCars() {
         List<Car> cars = parseToName(carNames).stream()
                 .map(name -> new Car(name, new CarPosition()))
@@ -21,10 +25,18 @@ public record CarsRequest(
 
     private List<CarName> parseToName(String carNames) {
         //TODO: parse메서드 위치 고민 및 콤마 상수 처리
-        return Arrays.stream(carNames.split(","))
+        List<CarName> cars = Arrays.stream(carNames.split(DELIMITER))
                 .map(String::strip)
                 .filter(name -> !name.isBlank())
                 .map(CarName::new)
                 .toList();
+        validateCarsSize(cars);
+        return cars;
+    }
+
+    private void validateCarsSize(List<CarName> cars) {
+        if (cars.size() < MINIMUM_NUMBER_OF_CARS) {
+            throw new IllegalArgumentException("최소 2명부터 경주가 가능합니다.");
+        }
     }
 }
