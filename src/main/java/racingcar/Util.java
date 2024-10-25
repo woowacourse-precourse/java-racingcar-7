f6c7206387f8060ce91f179973909a294776bba7
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-10-26 00:52:17 +0900

    feat(Util): 입력한 자동차 이름 문자열의 유효성 확인 함수 추가
    입력한 자동차 이름 문자열이 입력 형식에 맞는지 유효성을 검사하는 메소드 추가

--- src/main/java/racingcar/Util.java
+++ src/main/java/racingcar/Util.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Util {
    public static boolean isValidRoundNumber(Input input) {
        try{
@@ -9,4 +12,11 @@ public class Util {
            return false;
        }
    }
+
+    public static boolean isValidCarNamesInput(Input input) {
+       String regex = "^([\\s\\w\\uAC00-\\uD7A3\\p{Punct}]{1,5})(,(?!$)[\\s\\w\\uAC00-\\uD7A3\\p{Punct}]{1,5})*$|^([\\s\\w\\uAC00-\\uD7A3\\p{Punct}]{1,5})$";
+       Pattern pattern = Pattern.compile(regex);
+       Matcher matcher = pattern.matcher(input.getCarNames());
+       return matcher.matches();
+    }
 }
