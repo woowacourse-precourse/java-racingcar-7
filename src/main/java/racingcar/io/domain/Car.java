83147cba0b10c5e1930f1f9aee85fe4e158bbe28
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-10-26 05:16:55 +0900

    feat: 자동차 생성 기능 구현
    자동차 생성 기능 구현,
    자동차 이름 길이 검증 기능 구현

---
+++ src/main/java/racingcar/io/domain/Car.java
@@ -0,0 +1,30 @@
+package racingcar.io.domain;
+
+public class Car {
+
+	private static final int CAR_NAME_MAX_LENGTH = 5;
+	private static final String OVER_FLOW_CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 5자 이하만 가능합니다.";
+	private static final int INITIAL_MOVE_DISTANCE = 0;
+
+	private final String name;
+	private int moveDistance;
+
+	private Car(String name, int moveDistance) {
+
+		validateNameLength(name);
+
+		this.name = name;
+		this.moveDistance = moveDistance;
+	}
+
+	private void validateNameLength(String name) {
+		if (name.length() > CAR_NAME_MAX_LENGTH) {
+			throw new IllegalArgumentException(OVER_FLOW_CAR_NAME_LENGTH_ERROR_MESSAGE);
+		}
+	}
+
+	public static Car create(String name) {
+		return new Car(name, INITIAL_MOVE_DISTANCE);
+	}
+
+}
