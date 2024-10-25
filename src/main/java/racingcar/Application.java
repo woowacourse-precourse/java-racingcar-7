f7443c62576fa7caadb96f91dde30d3ede1a750e
yeo-li <81564503+yeo-li@users.noreply.github.com>
2024-10-25 17:24:14 +0900

    feat(Application): 입출력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,23 +8,23 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         // 여기서 사용되지 않는 코드는 필요 없는 코드가 아닐까?
         // 절차지향으로 TDD 한 뒤에 리팩토링 하면서 함수 쪼개고 TDD 하기
 
-        // 입력
+        // 입력 기능
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = readLine();
         System.out.println("시도할 횟수는 몇 회인가요?");
         String repeatCount = readLine();
 
-        // Extract car names
+        // 자동차 이름 추출 기능
         String[] cars = input.split(",");
 
-        // Create a repository to store how many times the car has times the car has advanced
+        // 자동차 전진 기능
         int[] stepsForward = new int[cars.length];
 
         // 실행 및 출력
+        System.out.println("실행 결과");
         for (int i = 0; i < Integer.parseInt(repeatCount); i++) {
             // 자동차 전진 기능
             for (int j = 0; j < stepsForward.length; j++) {
@@ -34,6 +34,15 @@ public class Application {
                 }
             }
 
+            // 출력 기능
+            for (int j = 0; j < stepsForward.length; j++) {
+                String forwardsViewer = "";
+                for (int k = 0; k < stepsForward[j]; k++) {
+                    forwardsViewer += "-";
+                }
+                System.out.println(cars[j] + " : " + forwardsViewer);
+            }
+            System.out.println("");
         }
 
         // 우승 차량 선별 기능
@@ -45,5 +54,8 @@ public class Application {
             }
         }
 
+        // 우승자 출력
+        String result = String.join(", ", winningCars);
+        System.out.println("최종 우승자 : " + result);
     }
 }
