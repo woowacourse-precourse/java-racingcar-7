c619be37d2da245c45585275a61a0bd216b2c2bf
suhyun9764 <suhyun9764@naver.com>
2024-10-25 16:41:11 +0900

    feat(OutputHandler): 우승자 출력하기 구현

--- src/main/java/racingcar/OutputHandler.java
+++ src/main/java/racingcar/OutputHandler.java
@@ -3,6 +3,7 @@ package racingcar;
 import static racingcar.constants.Messages.CARS_NAME_INPUT_MESSAGE;
 import static racingcar.constants.Messages.EXEC_RESULT_MESSAGE;
 import static racingcar.constants.Messages.TOTAL_ROUNDS_INPUT_MESSAGE;
+import static racingcar.constants.Messages.WINNER_INFO_HEADER;
 
 public class OutputHandler {
 
@@ -20,5 +21,9 @@ public class OutputHandler {
 
     public static void displayRoundResult(RoundResult roundResult) {
         System.out.println(DisplayConverter.convertRoundResult(roundResult));
+    }
+
+    public static void displayWinners(Winners winners) {
+        System.out.println(WINNER_INFO_HEADER+DisplayConverter.convertWinners(winners));
     }
 }
