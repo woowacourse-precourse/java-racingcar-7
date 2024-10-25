6b9a1acb0c7833a1b6d61a01202de5e92001db6e
jintakkim <mine407603@gmail.com>
2024-10-25 15:06:47 +0900

    feat(Output): 출력 기능 구현
    생성자에서 Cars객체를 받는다.
    Cars객체를 통해 미션의 요구 사항에 맞춰 경기 결과를 출력한다.

--- src/main/java/racingcar/Constants.java
+++ src/main/java/racingcar/Constants.java
@@ -3,4 +3,10 @@ package racingcar;
 public class Constants {
     public static final String INPUT_CAR_NAME_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String INPUT_RACE_ROUND_PROMPT = "시도할 횟수는 몇 회인가요?";
+
+    public static final String OUTPUT_START_PROMPT = "실행 결과";
+    public static final String OUTPUT_PROCESS_PROMPT = "%s : %s";
+    public static final String OUTPUT_FINAL_RESULT_PROMPT = "최종 우승자 : %s";
+
+    public static final String OUTPUT_BAR = "-";
 }
---
+++ src/main/java/racingcar/io/Output.java
@@ -0,0 +1,34 @@
+package racingcar.io;
+
+import racingcar.Cars;
+import racingcar.Constants;
+
+public class Output {
+    private final Cars cars;
+
+    public Output(Cars cars) {
+        this.cars = cars;
+    }
+
+    public void printPrefix() {
+        System.out.println(Constants.OUTPUT_START_PROMPT);
+    }
+
+    public void printProcess() {
+        cars.getCarsMap()
+                .forEach((carName, position)-> {
+                    String bar = convertPositionToBar(position);
+                    String processOutput = String.format(Constants.OUTPUT_PROCESS_PROMPT, carName, bar);
+                    System.out.println(processOutput);
+                });
+    }
+
+    public void printResult() {
+        String resultOutput = String.format(Constants.OUTPUT_FINAL_RESULT_PROMPT, cars.getWinner());
+        System.out.println(resultOutput);
+    }
+
+    private String convertPositionToBar(Integer position) {
+        return Constants.OUTPUT_BAR.repeat(position);
+    }
+}
