c24ce7e76d444bf63e9c1e7893846e588718e897
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-10-25 23:34:37 +0900

    feat(service): 한 라운드만 진행하는 서비스 추가

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -1,5 +1,6 @@
 package racingcar.service;
 
+import racingcar.constant.Message;
 import racingcar.dto.CarsDto;
 import racingcar.model.Separator;
 
@@ -8,5 +9,12 @@ public class RaceService {
     public CarsDto raceSetting(String inputCars) {
         Separator separator = new Separator();
         return new CarsDto(separator.separateCars(inputCars));
+    }
+
+    public CarsDto playOneRound(CarsDto carsDto) {
+        for (int i = 0; i < carsDto.getCars().size(); i++) {
+            carsDto.getCars().get(i).goRound();
+        }
+        return carsDto;
     }
 }
