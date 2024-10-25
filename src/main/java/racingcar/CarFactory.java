08d7a50e35853ac56ea1c56b63209d7956dae9bc
AwesomeMinsik <beaut3579@gmail.com>
2024-10-25 21:18:30 +0900

    refactor: 클래스의 메서드 접근 방식을 static 으로 변경하였습니다
    - 정적 메서드로 변경함에 따라 클래스 인스턴스 생성 로직을 제거하였습니다.

--- src/main/java/racingcar/CarFactory.java
+++ src/main/java/racingcar/CarFactory.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarFactory {
-    public List<RacingCar> createRacingCars(List<String> carNames) {
+    public static List<RacingCar> createRacingCars(List<String> carNames) {
         List<RacingCar> racingCars = new ArrayList<>();
         for (String carName : carNames) {
             RacingCar newRacingCar = new RacingCar(carName);
