e327ab48f8cf20cc7d3ee53aabb74507a9ce0419
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-26 06:02:24 +0900

    refactor($DefaultRandomNumberMaker): 사용하지 않는 static 키워드 제거
    - static 을 사용하고 있지 않기 때문.

--- src/main/java/racingcar/service/dependency/random_maker/DefaultRandomNumberMaker.java
+++ src/main/java/racingcar/service/dependency/random_maker/DefaultRandomNumberMaker.java
@@ -4,11 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class DefaultRandomNumberMaker implements RandomNumberMaker {
 
-    private static final int RANDOM_NUMBER_BOUND = 9;
-    private static final int RANDOM_NUMBER_MIN = 0;
+    public final int RANDOM_MAX = 9;
+    public final int RANDOM_MIN = 0;
 
     @Override
     public int makeRandomNumber() {
-        return Randoms.pickNumberInRange(RANDOM_NUMBER_MIN, RANDOM_NUMBER_BOUND);
+        return Randoms.pickNumberInRange(RANDOM_MIN, RANDOM_MAX);
     }
 }
