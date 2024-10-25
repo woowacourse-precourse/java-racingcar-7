85ddbacbf1f96ed927aac42c093e77669ddbb819
kangsan <devstar1223@gmail.com>
2024-10-25 14:58:15 +0900

    feat: 입력 문자열 리스트로 변환

--- src/main/java/racingcar/CarRacingSimulation.java
+++ src/main/java/racingcar/CarRacingSimulation.java
@@ -1,10 +1,16 @@
 package racingcar;
 
+import racingcar.io.ConsoleInputHandler;
 import racingcar.io.ConsoleOutputHandler;
+
+import java.util.List;
 
 public class CarRacingSimulation {
     private final ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
+    private final ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
     public void run() {
-        consoleOutputHandler.racingCarNamingMessage();
+        consoleOutputHandler.racingCarNamingMessage(); // "자동차 이름 입력 안내" 메세지 출력
+        String userInputString = consoleInputHandler.getUserInput(); // 유저 입력 받기
+        List<String> carNameList = UserInputStringConverter.toList(userInputString); // 입력 문자열 자동치 이름 리스트로 변환
     }
 }
