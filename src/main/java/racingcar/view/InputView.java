5da84edc1cafee98b9beba7926e6b584ca73b2ba
SukJin Kim <rkathrwks@gmail.com>
2024-10-26 01:41:16 +0900

    refactor(inputview): 메서드 반환타입 및 접근 제한자 변경
    requestCarName() : void -> String
    requestNumAttempts() : void -> String
    readInput() : public -> private

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,15 +6,17 @@ public class InputView {
     private static final String CAR_NAME_PROMPT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String NUM_ATTEMPT_PROMPT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
-    public void requestCarNames() {
+    public String requestCarNames() {
         System.out.println(CAR_NAME_PROMPT_MESSAGE);
+        return readInput();
     }
 
-    public void requestNumAttempt() {
+    public String requestNumAttempts() {
         System.out.println(NUM_ATTEMPT_PROMPT_MESSAGE);
+        return readInput();
     }
 
-    public String readInput() {
+    private String readInput() {
         String input = Console.readLine();
         Console.close();
         return input;
