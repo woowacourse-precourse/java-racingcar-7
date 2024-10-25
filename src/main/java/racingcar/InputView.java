3e49855777115bc4e089844a857b3ae44f81f39b
DocodeLee <167724096+DocodeLee@users.noreply.github.com>
2024-10-25 20:46:08 +0900

    Input 관리 자동차 숫자 시도 횟수 자동차 이름 받기 구현

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -2,18 +2,32 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
 
-    public String inputCarNames() {
-        System.out.println("경주할 차량의 이름을 입력하세요. 이름은 (,) 기준으로 구분 됩니다.이름은 5자 이하만 가능합니다.");
+    public static int getCarNum() {
+        System.out.println("자동차는 몇대인가요?");
 
-        return Console.readLine();
-
+        int carNum = Integer.parseInt(Console.readLine());
+        return carNum;
     }
 
-    public String inputMaxRound() {
-        System.out.println("시도하실 횟수를 정해주세요");
-
-        return Console.readLine();
+    public static int getTryNum() {
+        System.out.println("몇 번 시도하시겠습니까?");
+        int tryNum = Integer.parseInt(Console.readLine());
+        return tryNum;
     }
+
+    public static List<String> getCarName() {
+        System.out.println("자동차 이름을 입력하세요.(,)을 기준으로 구분: ");
+        String carName = Console.readLine();
+        String[] carArray = carName.split(",");
+
+        return Arrays.stream(carArray)
+                .map(String::trim)
+                .toList();
+    }
+
 }
