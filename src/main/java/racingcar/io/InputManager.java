c3e6ce1b41215ded6b5dc0640029ffee056d122a
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-10-25 17:33:35 +0900

    feat: 입력받은 문자열 null|blank 검증 함수 구현

--- src/main/java/racingcar/io/InputManager.java
+++ src/main/java/racingcar/io/InputManager.java
@@ -1,6 +1,7 @@
 package racingcar.io;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.exception.NullEmptyStringException;
 import racingcar.message.IOMessage;
 
 public class InputManager implements IOManager {
@@ -13,13 +14,20 @@ public class InputManager implements IOManager {
         return inputManager;
     }
 
+    private static String validateInput(final String input) {
+        if (input == null || input.isBlank()) {
+            throw new NullEmptyStringException();
+        }
+        return input;
+    }
+
     public String carNameInput() {
         printMessage(IOMessage.ENTER_CAR_NAME_INPUT);
-        return Console.readLine();
+        return validateInput(Console.readLine());
     }
 
     public String attemptCountInput() {
         printMessage(IOMessage.ENTER_ATTEMPT_COUNT_INPUT);
-        return Console.readLine();
+        return validateInput(Console.readLine());
     }
 }
