e824a9017cce175b63b34e4ee194514f19b9b3e0
sung <salamander011017@gmail.com>
2024-10-25 21:00:22 +0900

    feat : Application에 Controller 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.controller.RaceGameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RaceGameController controller = new RaceGameController();
+        controller.run();
     }
 }
+// pobi,woni,jun  -> o
+// pobibi,woni,jun -> x
+// pobi, woni,jun -> x
+// pobi,woni;jun -> x
