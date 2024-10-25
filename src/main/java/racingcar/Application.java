a3b52763a5091f867e701140d79ac9454a497bff
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-10-25 13:30:48 +0900

    feat(Application): 자동차 입력, 전진횟수 입력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] names = input.split(",");
+        //예외처리 1 추가예정 :이름 5자 초과
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String count_s = Console.readLine();
+        int count = Integer.parseInt(count_s);
+        //예외처리 2 추가예정: 횟수가 1회미만, 음수일경우
+        //우승자 출력 추가예정
     }
 }
