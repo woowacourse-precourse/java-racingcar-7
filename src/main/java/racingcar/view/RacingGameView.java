c6cef54c363af4cdcaf517141658ae9b55c514bd
CHISANW <keuye06380618@gmail.com>
2024-10-25 15:27:35 +0900

    feat(RacingGameView): 게임 종료시 출력 구현
    - OutputView 인스턴스를 통해 경주 결과와 최종 우승자의 이름을 출력하는 endGame() 메서드 추가

--- src/main/java/racingcar/view/RacingGameView.java
+++ src/main/java/racingcar/view/RacingGameView.java
@@ -1,17 +1,27 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.domain.RaceRound;
 import racingcar.dto.ViewDto;
 
 public class RacingGameView {
 
     private final InputView inputView;
+    private final OutputView outputView;
 
     public RacingGameView() {
         this.inputView = new InputView();
+        this.outputView = new OutputView();
     }
 
     public ViewDto startGame() {
         return ViewDto.of(inputView.readCarNames(), inputView.readTryCount());
+    }
+
+    public void endGame(List<RaceRound> raceRounds, String winnerCarName) {
+        outputView.printGameStart();
+        outputView.printRaceResult(raceRounds);
+        outputView.printWinners(winnerCarName);
     }
 
 }
