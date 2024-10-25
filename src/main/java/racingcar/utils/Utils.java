09870d9cf08d3def89f61441f36d144c57325539
Leejunho <junho767@naver.com>
2024-10-26 03:18:46 +0900

    feat: 전진 조건 충족 여부 검사 기능 구현

--- src/main/java/racingcar/utils/Utils.java
+++ src/main/java/racingcar/utils/Utils.java
@@ -1,8 +1,14 @@
 package racingcar.utils;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Utils {
 
     public static String[] splitDelimiterCars(String inputCarNames){
         return inputCarNames.split(",");
+    }
+
+    public static boolean advanceConditions(){
+        return Randoms.pickNumberInRange(0,9) >= 4;
     }
 }
