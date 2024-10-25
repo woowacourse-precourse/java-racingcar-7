6cd9cd91f6c0ef44994395b011707b309a589561
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-25 17:08:46 +0900

    refactor: 파싱부분 로직 분리 및 유효성 검사 로직만 남김

--- src/main/java/racingcar/ValidateCar.java
+++ src/main/java/racingcar/ValidateCar.java
@@ -1,23 +1,15 @@
 package racingcar;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ValidateCar {
-    public static List<String> firstParsingOnComma(String userInput) {
-
+    public static void validateInputCar(String userInput) {
         validateEmptyInput(userInput);
         validateExtraComma(userInput);
 
-        List<String> list = new ArrayList<>();
-
-        for (String part : userInput.split(",")) {
-            String trimmedPart = part.trim();
+        for (String carName : userInput.split(",")) {
+            String trimmedPart = carName.trim();
             validateEmptyPart(trimmedPart);
             validateNameLength(trimmedPart);
-            list.add(trimmedPart);
         }
-        return list;
     }
 
     private static void validateEmptyInput(String userInput) {
@@ -27,20 +19,20 @@ public class ValidateCar {
     }
 
     private static void validateExtraComma(String userInput) {
-        String[] splitCarNames = userInput.split(",");
-        if (splitCarNames.length == 0) {
-            throw new IllegalArgumentException("쉼표가 여러개 입력되었습니다.");
+        String[] splitUserInputTemp = userInput.split(",");
+        if (splitUserInputTemp.length == 0) {
+            throw new IllegalArgumentException("쉼표만 입력되었습니다.");
         }
     }
 
-    private static void validateEmptyPart(String part) {
-        if (part.isEmpty()) {
+    private static void validateEmptyPart(String trimmedPart) {
+        if (trimmedPart.isEmpty()) {
             throw new IllegalArgumentException("쉼표 뒤에 이름이 들어오지 않았습니다.");
         }
     }
 
-    private static void validateNameLength(String part) {
-        if (part.length() > 5) {
+    private static void validateNameLength(String trimmedPart) {
+        if (trimmedPart.length() > 5) {
             throw new IllegalArgumentException("이름이 5자를 초과합니다.");
         }
     }
