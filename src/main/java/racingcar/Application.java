462e6700c436737aebf196c5e47cd2341e66544b
moon-akdong <problemy0913@gmail.com>
2024-10-26 03:24:24 +0900

    Create method functional of printCarNameProgress()

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -17,15 +17,14 @@ public class Application {
         System.out.println("시도할 횟수는 몇 회인가요? : " + repeatRaceIntNum);
 
         String[] carNames = splitCarsName(inputStr);
-        int[] carProgressArr = new int [carNames.length];
+        int[] carProgressArr = new int[carNames.length];
         for (int i = 0; i < repeatRaceIntNum; i++) {
-            for (int car_ind = 0; car_ind <carNames.length; car_ind++) {
+            for (int car_ind = 0; car_ind < carNames.length; car_ind++) {
                 int carProgress = canProgressWithRandomNum();
-//                carNames[car_ind];
-//                carProgressArr[car_ind]
+                carProgressArr[car_ind] += carProgress;
+                printCarNameProgress(carNames[car_ind], carProgressArr[car_ind]);
             }
-
-
+            System.out.println();
         }
     }
 
@@ -39,10 +38,16 @@ public class Application {
     public static int canProgressWithRandomNum() {
         // 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
         int randomNum = Randoms.pickNumberInRange(0, 9);
-        if (randomNum > 4) {
+        System.out.println(randomNum);
+        if (randomNum >= 4) {
             return 1;
         }
         return 0;
         // System.out.println(randomNum);
+    }
+
+    public static void printCarNameProgress(String carName, int progressNum) {
+        String progressBar = "-".repeat(progressNum);
+        System.out.println(carName + " : " + progressBar);
     }
 }
