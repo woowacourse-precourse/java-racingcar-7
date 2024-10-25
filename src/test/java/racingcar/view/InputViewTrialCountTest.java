ff66a5ab906c62fb99ad9fe45efcf7bbc124a984
corpi <lj845854@gmail.com>
2024-10-26 03:01:14 +0900

    refactor(InputViewTrialCountTest): 테스트 케이스 주입 방식 변경

--- src/test/java/racingcar/view/InputViewTrialCountTest.java
+++ src/test/java/racingcar/view/InputViewTrialCountTest.java
@@ -1,14 +1,12 @@
 package racingcar.view;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.model.InputValidator;
 
 class InputViewTrialCountTest extends NsTest {
@@ -20,19 +18,12 @@ class InputViewTrialCountTest extends NsTest {
     }
 
     @DisplayName("시도 횟수 입력을 받는다.")
-    @MethodSource("inputTrialCount")
+    @ValueSource(strings = {"1", "23", "2147483647"})
     @ParameterizedTest
-    void inputTrialCount(String input, String output) {
-        run(input);
-        assertThat(output().contains(output)).isTrue();
-    }
-
-    private static Stream<Arguments> inputTrialCount() {
-        return Stream.of(
-                Arguments.of("1", "1"),
-                Arguments.of("23", "23"),
-                Arguments.of("2147483647", "2147483647")
-        );
+    void inputTrialCount(String input) {
+        assertDoesNotThrow(() -> {
+            run(input);
+        });
     }
 
     @Override
