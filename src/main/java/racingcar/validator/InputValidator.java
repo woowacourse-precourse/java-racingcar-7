c9051c8c6cf41d3dfa1e02ead4b48bced0583409
dawit0905 <dawit0905@gmail.com>
2024-10-25 12:34:18 +0900

    refactor: 함수 이름 변경 및 에러메시지 한글로 변경

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,16 +1,17 @@
 package racingcar.validator;
 
 public class InputValidator {
-    public static void checkMaxLength5(String input) {
+    public static void validateMaxLength5(String input) {
         if (input.length() > 5) {
-            throw new IllegalArgumentException("Input exceeds maximum length of 5 characters.");
+            throw new IllegalArgumentException("이름은 다섯 글자를 초과할 수 없습니다.");
         }
     }
+
     public static int parseIntWithValidation(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Input is not a valid integer: " + input);
+            throw new IllegalArgumentException("정수 값이 아닙니다 : " + input);
         }
     }
 }
