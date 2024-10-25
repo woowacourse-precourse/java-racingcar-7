d3ae3bde5ce20a83cfa415bd5af955de04c78eb7
JS <zks145@naver.com>
2024-10-25 14:42:16 +0900

    feat (Util): 랜덤 정수 생성 메서드 추가

---
+++ src/main/java/racingcar/util/Util.java
@@ -0,0 +1,11 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Util {
+
+    public static int RandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+}
