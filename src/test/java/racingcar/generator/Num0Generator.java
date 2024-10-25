5534209f5349ce5d5a8557c0136244c1097a7cb7
dradnats1012 <herjebi1012@gmail.com>
2024-10-25 17:44:00 +0900

    test: 전진, 정지 테스트를 위한 숫자 생성기 구현

---
+++ src/test/java/racingcar/generator/Num0Generator.java
@@ -0,0 +1,11 @@
+package racingcar.generator;
+
+import racingcar.model.generator.NumberGenerator;
+
+public class Num0Generator implements NumberGenerator {
+
+    @Override
+    public int generateNum() {
+        return 0;
+    }
+}
