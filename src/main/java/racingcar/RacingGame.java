f4e45b625a0ea24fa0e27d07675572ee51fd7628
kjyyjk <rlawnsdud920@gmail.com>
2024-10-25 23:32:17 +0900

    feat: 각 이동마다 자동차들은 무작위의 값을 뽑는다
    - 총 이동횟수만큼 moveCars()를 반복한다.
    moveCars()는 cars의 자동차들 별로 moveCarRandomly()를 호출한다.
    moveCarRandomly()는 자동차 별로 랜덤 값을 뽑는다.
    
    - 0~9 무작위 값을 뽑기 위해 Randoms의 pickNumberInRange()를 사용한다.
    pickNumberInRange()는 내부적으로 두 파라미터의 차이 + 1을 nextInt 인자로 넘긴다.
    이는 처음과 끝을 모두 포함하는 범위의 무작위 값을 반환한다는 의미이다.
    따라서 pickNumberInRange()에 0과 9를 인자로 넘긴다.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,6 +1,8 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class RacingGame {
@@ -10,9 +12,22 @@ public class RacingGame {
         for (String name : carNames.split(",")) {
             registerCar(new Car(name));
         }
+
+        for (int i = 0; i < totalMoves; i++) {
+            moveCars();
+        }
     }
 
-    private void registerCar(Car car) {
+    private void moveCars() {
+        Iterator<Car> iterator = cars.iterator();
+
+        while(iterator.hasNext()) {
+            Car car = iterator.next();
+            moveCarRandomly(car);
+        }
+    }
+
+    private void registerCar(final Car car) {
         boolean isAdded = cars.add(car);
         validateNameDuplicated(isAdded);
     }
@@ -21,5 +36,9 @@ public class RacingGame {
         if (!isAdded) {
             throw new IllegalArgumentException("자동차의 이름은 중복될 수 없습니다");
         }
+    }
+
+    private void moveCarRandomly(final Car car) {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
     }
 }
