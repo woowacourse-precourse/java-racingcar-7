fc4023ac08788c17cd5ecb1a5fb3ecdc6ae69913
조수아 <suacho0724@gmail.com>
2024-10-25 12:13:54 +0900

    feat: 자동차 이름들과 시도할 횟수 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String cars = InputView.input("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        String num = InputView.input("시도할 횟수는 몇 회인가요?\n");
     }
 }
