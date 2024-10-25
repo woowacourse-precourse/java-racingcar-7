7d610c06e47f5f64a4043e26914a6bb5f57f92f2
jklee3409 <jklee3409@khu.ac.kr>
2024-10-25 20:38:44 +0900

    feat(application): application 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.view.InputView;
+import racingcar.view.ResultView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+        GameController controller = new GameController(inputView, resultView);
+        controller.run();
     }
 }
