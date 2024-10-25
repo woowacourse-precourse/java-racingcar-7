55937d579f6b268a4449465ce2a49d506d8e8aa5
kimgoat <kimgoatlove@gmail.com>
2024-10-25 21:27:23 +0900

    refactor(Application): 객체를 생성하여 GameController 매개변수로 의존성 주입하도록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,14 @@
 package racingcar;
 
 import racingcar.controller.GameController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        GameController gameController = new GameController();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        GameController gameController = new GameController(inputView, outputView);
         gameController.play();
     }
 }
