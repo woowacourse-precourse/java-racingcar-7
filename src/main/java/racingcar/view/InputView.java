0e55bb96117415653c00bfd952df7cc71f9d9528
Leejunho <junho767@naver.com>
2024-10-26 03:04:43 +0900

    style: 메서드 이름 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,18 +1,18 @@
 package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
-import static racingcar.domain.Message.CAR_NAMES_MESSAGE;
-import static racingcar.domain.Message.TRY_NUMBER_MESSAGE;
+import static racingcar.model.Message.CAR_NAMES_MESSAGE;
+import static racingcar.model.Message.TRY_NUMBER_MESSAGE;
 
 public class InputView {
 
     // 입력
-    public String inputCarNames(){
+    public static String inputCarNames(){
         System.out.println(CAR_NAMES_MESSAGE);
         return Console.readLine();
     }
 
-    public String inputTryNumber(){
+    public static String inputTryNumber(){
         System.out.println(TRY_NUMBER_MESSAGE);
         return Console.readLine();
     }
