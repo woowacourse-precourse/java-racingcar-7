f59e4d45e62ec2e9d6fd2e410857f31037bbb8df
unh6unh6 <ruddbs3308@naver.com>
2024-10-25 17:18:34 +0900

    feat(Car): 자동차 전진 조건을 확인하고 자동차를 1만큼 전진

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,6 +4,7 @@ public class Car {
     private static final String CAR_NAME_LENGTH_EXCEPTION = "자동차 이름은 5자 이하이어야 합니다.";
 
     private final String name;
+    private long position = 0;
 
     public Car(String name) {
         validateCarNameLength(name);
@@ -14,5 +15,19 @@ public class Car {
         if (name.length() > 5) {
             throw new IllegalArgumentException(CAR_NAME_LENGTH_EXCEPTION);
         }
+    }
+
+    public void move() {
+        if (canMove()) {
+            position++;
+        }
+    }
+
+    private boolean canMove() {
+
+    }
+
+    private int getRandomNumber() {
+
     }
 }
