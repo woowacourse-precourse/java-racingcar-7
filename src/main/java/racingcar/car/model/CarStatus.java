9aedd7346bcc4221806f76008ee8b4c34e2746db
jieun <ju3391@naver.com>
2024-10-25 21:19:22 +0900

    feat(CarStatus): 자동차 상태 관리 추가
    - 자동차의 전진 횟수 관리

--- src/main/java/racingcar/car/model/CarStatus.java
+++ src/main/java/racingcar/car/model/CarStatus.java
@@ -1,4 +1,19 @@
 package racingcar.car.model;
 
+// 자동차의 상태를 나타내는 객체
 public class CarStatus {
+    private int position;
+
+    public CarStatus(int position) {
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void incrementPosition() {
+        position++;
+    }
 }
+
