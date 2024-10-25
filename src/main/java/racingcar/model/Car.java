2b13f1e708f5a4acc4a38cbea332f3444c748954
Smith <125195487+sigmaith@users.noreply.github.com>
2024-10-25 12:49:23 +0900

    feat(Car): 자동차 클래스 정의

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+public class Car {
+    private String name;
+    private int distance;
+
+    public Car (final String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void moveForward() {
+        distance += 1;
+    }
+}
