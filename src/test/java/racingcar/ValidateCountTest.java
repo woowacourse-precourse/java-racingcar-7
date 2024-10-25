6b5bf266f0dcfc2616a959b992eccb94080c7568
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-25 17:30:58 +0900

    style: 로직 분리로 인한 메서드명 수정

--- src/test/java/racingcar/ValidateCountTest.java
+++ src/test/java/racingcar/ValidateCountTest.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
@@ -10,19 +9,19 @@ class ValidateCountTest extends NsTest {
 
     @Test
     void 예외_테스트_빈문자열() {
-        assertThatThrownBy(() -> ValidateCount.parseCount(""))
+        assertThatThrownBy(() -> ValidateCount.validateInputCount(""))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 예외_테스트_음수가들어온경우() {
-        assertThatThrownBy(() -> ValidateCount.parseCount("-1"))
+        assertThatThrownBy(() -> ValidateCount.validateInputCount("-1"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 예외_테스트_자연수가아닌값() {
-        assertThatThrownBy(() -> ValidateCount.parseCount("a"))
+        assertThatThrownBy(() -> ValidateCount.validateInputCount("a"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
