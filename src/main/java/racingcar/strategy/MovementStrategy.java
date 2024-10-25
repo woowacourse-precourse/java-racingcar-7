63d98557fce6bbf26d7e00a0d7e961fa59ba246c
world-dv <goodforx0147@naver.com>
2024-10-26 03:36:13 +0900

    refactor: 추출한 메서드 재정의
    - 독립 기능으로 판단해 무작위 값 메서드를 추출했지만, 핵심 기능인 move와 연관관계에 있다고 판단해 다시 재정의 합니다.

--- src/main/java/racingcar/strategy/MovementStrategy.java
+++ src/main/java/racingcar/strategy/MovementStrategy.java
@@ -1,12 +1,18 @@
 package racingcar.strategy;
 
-import racingcar.util.RandomNumberCreator;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class MovementStrategy implements CarStrategy {
+    private static final Integer START_RANDOM_NUMBER = 0;
+    private static final Integer END_RANDOM_NUMBER = 9;
     private static final Integer MOVEMENT_CRITERION_NUMBER = 4;
 
     @Override
     public boolean move() {
-        return RandomNumberCreator.getRandomNumber() >= MOVEMENT_CRITERION_NUMBER;
+        return getRandomNumber() >= MOVEMENT_CRITERION_NUMBER;
+    }
+
+    private Integer getRandomNumber() {
+        return Randoms.pickNumberInRange(START_RANDOM_NUMBER, END_RANDOM_NUMBER);
     }
 }
