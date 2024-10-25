26d6a5fce383e4fc908a3aabd613d2eaede913e0
sc@t0a14 <gusdn6671@gmail.com>
2024-10-25 23:39:49 +0900

    refactor(OutputView): 모든 메서드에 static을 제거한다

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,19 +1,18 @@
 package racingcar.view;
 
 import java.util.List;
-import racingcar.model.Car;
 
 public class OutputView {
 
-    public static void printCarNamesPrompt() {
+    public void printCarNamesPrompt() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public static void printAttemptCountPrompt() {
+    public void printAttemptCountPrompt() {
         System.out.println("시도할 횟수는 몇 회인가요?");
     }
 
-    public static void printCarDistances(List<String> carNames, List<Integer> distances) {
+    public void printCarDistances(List<String> carNames, List<Integer> distances) {
         for (int i = 0; i < carNames.size(); i++) {
             System.out.print(carNames.get(i) + " : ");
             System.out.println("-".repeat(distances.get(i)));
@@ -21,14 +20,13 @@ public class OutputView {
         System.out.println();
     }
 
-    public static void printRacingResult(){
+    public void printRacingResult() {
         System.out.println();
         System.out.println("실행결과");
     }
 
-    public static void printWinners(List<String> winners){
-        System.out.println("최종 우승자 : "+ String.join(", ",winners));
+    public void printWinners(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
-
 
 }
