e667b17f8fa3d195c761fccdee71be7ce35cc7f9
DESKTOP-GRC68NA\foski <foskingson@naver.com>
2024-10-25 22:38:19 +0900

    refactor: 함수에 필요없는 부분 제거

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -3,10 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingGame {
-    public static Racingcar[] createRacingcars(){   // 입력받은 레이싱카의 이름으로 n대의 레이싱카 초기화
-        String[] carsName = InputHandler.InputCarsName(); 
-        Validator.validateCarNames(carsName);
-
+    public static Racingcar[] createRacingcars(String[] carsName){   // 입력받은 레이싱카의 이름으로 n대의 레이싱카 초기화
         Racingcar[] racingcars = new Racingcar[carsName.length];
         for (int i = 0; i < racingcars.length; i++) {
             racingcars[i] = new Racingcar(carsName[i]);
@@ -19,7 +16,7 @@ public class RacingGame {
         return Randoms.pickNumberInRange(0, 9);
     }
 
-    public static void moveRacingcar(Racingcar racingcar){ // 전진하는 조건이 맞으면 레이싱카를 전진시키고 아니면 멈춰있는다.
+    public static void checkMove(Racingcar racingcar){ // 전진하는 조건이 맞으면 레이싱카를 전진시키고 아니면 멈춰있는다.
         if (createRandom()>=4) {
             Racingcar.moveRacingcar(racingcar);
         }
