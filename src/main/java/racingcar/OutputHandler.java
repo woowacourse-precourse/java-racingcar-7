a73f8b574ff29745397d907cc4cf1c428b8af80f
howareyoo <newworld121@naver.com>
2024-10-26 00:46:39 +0900

    chors : 리네임 및 메세지 수정

--- src/main/java/racingcar/OutputHandler.java
+++ src/main/java/racingcar/OutputHandler.java
@@ -2,8 +2,8 @@ package racingcar;
 
 public class OutputHandler {
 
-    public static void startRacing(){
-        System.out.println("경주할 자동차 이름을 입력하세요.");
+    public static void outputStartInfo(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
 }
