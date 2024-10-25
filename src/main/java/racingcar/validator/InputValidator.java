a00743e95d7ceb0ec051f84017dbd2dd6da439c1
dawit0905 <dawit0905@gmail.com>
2024-10-25 12:34:55 +0900

    feat: 공백 검증, 알파벳 / 숫자 검증, 이름 중복 검증 메소드 추가

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,5 +1,8 @@
 package racingcar.validator;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class InputValidator {
     public static void validateMaxLength5(String input) {
         if (input.length() > 5) {
@@ -14,4 +17,26 @@ public class InputValidator {
             throw new IllegalArgumentException("정수 값이 아닙니다 : " + input);
         }
     }
+
+    public static void validateEmpty(String input) {
+        if (input.trim().isEmpty()) {
+            throw new IllegalArgumentException("입력이 공백입니다.");
+        }
+    }
+
+    public static void validateAlphaNumeric(String str) {
+        if (!str.matches("[a-zA-Z0-9]+")) {
+            throw new IllegalArgumentException("숫자와 알파벳 외에 다른 값이 입력되었어요 : " + str);
+        }
+    }
+
+    public static void validateForDuplicates(List<String> strings) {
+        HashSet<String> uniqueStrings = new HashSet<>();
+        for (String str : strings) {
+            if (!uniqueStrings.add(str.trim())) {
+                throw new IllegalArgumentException("중복된 이름입니다 : " + str.trim());
+            }
+        }
+    }
+
 }
