55c64e308a2f08f80b6a44abaaed7721745dd818
Hajuuu <hpt0127@naver.com>
2024-10-25 16:32:10 +0900

    fix: TestMethodOrder annotation 추가
    Order annotation이 작동하지 않아 이를 작동 시키기 위해 TestMethodOrder annotation을 작성

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -6,11 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RacingGameTest {
 
     static RacingGame racingGame;
