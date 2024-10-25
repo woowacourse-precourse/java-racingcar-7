9f8fc5e05b3d99b45fc0e0d5cd652404b40a2064
Chaewon <corndots@naver.com>
2024-10-26 04:50:30 +0900

    feat(Car): 필드 생성, 전진 메서드 구현
    전진은 랜덤값이 4이상일 경우에만 postion 1증가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    // 무작위 값이 4 이상일 때 전진
+    public void move(int randomValue) {
+        if (randomValue >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
