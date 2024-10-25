8f45d78f2f0d7f5b599d4c218df6c2696904ca25
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-10-25 22:22:56 +0900

    feat(model): 자동차 이름 중복 검증 추가
    - splitInput 자료형 Array에서 List로 수정
    - 집합 자료형과 크기 비교 후, 크기 차이 발생 시 중복 오류
    - 검증 메시지 추가

--- src/main/java/racingcar/model/Sepator.java
+++ src/main/java/racingcar/model/Sepator.java
@@ -1,8 +1,11 @@
 package racingcar.model;
 
+import racingcar.constant.Message;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 public class Sepator {
     public List<Car> separateCars(String input) {
@@ -11,9 +14,13 @@ public class Sepator {
 
         // 문자열 split 시 마지막 빈 값도 배열에 저장하기 위해 limit: -1
         // "pobi," -> ["pobi",""]
-        String[] splitInput =  input.split(DELIMITER,-1);
+        List<String> splitInput =  Arrays.stream(input.split(DELIMITER,-1)).toList();
         for (String element : splitInput) {
             cars.add(new Car(element));
+        }
+        Set<String> carsSet = Set.copyOf(splitInput);
+        if (carsSet.size() != cars.size()) {
+            throw new IllegalArgumentException(Message.CAR_NAME_DUPLICATE_VALIDATION_MESSAGE.getMessage());
         }
         return cars;
     }
