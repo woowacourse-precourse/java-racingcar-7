2662d41244ff7a367d31b4ce80ae9d4c790cab6e
sinji <tobeabestns@naver.com>
2024-10-25 17:40:00 +0900

    feat(FindWinningCar): 우승자를 구하기 위한 클래스 생성

--- src/main/java/domain/FindWinningCar.java
+++ src/main/java/domain/FindWinningCar.java
@@ -1,4 +1,28 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindWinningCar {
+    private List<Car> cars;
+    private int winnersDistance = 0;
+
+    public FindWinningCar() {
+        this.cars = cars;
+    }
+
+    public List<String> getWinningCars() {
+        List<String> winningCars = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (car.getCarLocation() > winnersDistance) {
+                winnersDistance = car.getCarLocation();
+                winningCars.clear();
+                winningCars.add(car.getCarName());
+            } else if (car.getCarLocation() == winnersDistance) {
+                winningCars.add(car.getCarName());
+            }
+        }
+        return winningCars;
+    }
 }
