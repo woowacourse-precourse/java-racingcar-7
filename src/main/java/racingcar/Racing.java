f8ecf5bd492a742f0f9bd858ce1bf4f61876ef20
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2024-10-25 14:45:39 +0900

    :necktie: feat : play, playRound 메서드 추가

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -2,13 +2,31 @@ package racingcar;
 
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Random;
 
 public class Racing {
     private final List<Car> cars;
+    private final Output output;
 
     public Racing(List<Car> cars){
         this.cars = cars;
+        this.output = new Output();
+    }
+
+    public void play(int rounds){
+        System.out.println("실행 결과");
+        for (int i = 0; i < rounds; i++){
+            playRound();
+            output.printRoundResult(cars);
+        }
+    }
+
+    public void playRound(){
+        for (Car car : cars){
+            if (canMove()){
+                car.move();
+
+            }
+        }
     }
 
     public List<Car> getWinners(){
