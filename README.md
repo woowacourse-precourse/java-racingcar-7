af6e114e6f46e33544913bece0f6d885ce874380
Local_Hwang <amongzamong@gmail.com>
2024-10-25 18:54:37 +0900

    feat(core): add init function for input handling
    add new init function to handle user input. split car names by delimiter

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # java-racingcar-precourse
+
+# init(string input)
+- input을 입력받은 후 delimiter을 ","로 설정하여 자동차를 key로 하고 int를 value로 한 map을 만들어주자.
+이름들을 arrayList에 저장해주자.
+
+# moveCar(string carName)
+- 각 차들을 Randoms.pickNumberInRange(0, 9); 를 활용해서 4이상이 나오는 경우 +1 해준다.
+
+# checkWinner
+- 차수별 실행 결과를 출력하자.
+- 각 차들이 전진한 수를 분석하고, 가장 높은 차 or 차들을 최종 우승자로 선정하고 출력해주자.
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,31 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashMap;
+
 public class Application {
+
+    static String[] names;
+    static HashMap<String, Integer> car;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        String input = Console.readLine();
+        Application app = new Application();
+        app.init(input);
+
     }
+
+    public void init(String input) {
+        // ,를 delimiter로 나눠줌
+        String delimiters = ",";
+        names = input.split("[" + delimiters + "]");
+        //현재 거리는 전부 0
+        for (String name : names) {
+            car.put(name, 0);
+        }
+    }
+
 }
