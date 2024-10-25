2db98c0ad9d0241b4f6bb6bbe17363d40cc786a6
ziyoungkwon <gy011003@naver.com>
2024-10-25 15:24:41 +0900

    feat: printCarForward, printForward 기능 작성
    자동차가 전진한 만큼 '-'를 출력하는 기능 작성

--- src/main/java/race/Race.java
+++ src/main/java/race/Race.java
@@ -2,7 +2,9 @@ package race;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Race {
@@ -17,6 +19,8 @@ public class Race {
         cars.forEach((key, value) -> {
             int newValue = playRacingCar(value);
             cars.put(key, newValue);
+
+            printCarForward(cars);
         });
 
     }
@@ -62,5 +66,21 @@ public class Race {
             return true;
         else
             return false;
+    }
+
+    private void printCarForward(Map<String, Integer> cars) {
+
+        cars.forEach((key, value) -> {
+            System.out.print(key + " : ");
+            printForward(value);
+        });
+    }
+
+    private void printForward(int value) {
+
+        for (int i = 0; i < value; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 }
