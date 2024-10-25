f1f72bd96ed4070754f267084701ae9476ba276a
dbdb456 <adad568@naver.com>
2024-10-25 15:43:18 +0900

    refactor(validator): validateInputNotBlank 메서드 매개변수 string을 carNames로 변경

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -2,8 +2,8 @@ package racingcar.validator;
 
 public class Validator {
 
-    public void validateInputNotBlank(String string) {
-        if (string.isBlank()) {
+    public void validateInputNotBlank(String carNames) {
+        if (carNames.isBlank()) {
             throw new IllegalArgumentException("입력받은 자동차 이름이 없습니다.");
         }
     }
