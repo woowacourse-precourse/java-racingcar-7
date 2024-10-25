337628fcb6741403c960199c2311bbef97240538
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-26 00:01:49 +0900

    refactor($RandomUtils): 랜덤 값 생성 전 RandomRange 의 범위를 검증하도록 변경

--- src/main/java/racingcar/util/ramdom/RandomUtils.java
+++ src/main/java/racingcar/util/ramdom/RandomUtils.java
@@ -5,6 +5,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 public final class RandomUtils {
 
     public static int generateRandomNumber(RandomRange randomRange) {
+        validate(randomRange);
         return Randoms.pickNumberInRange(randomRange.min(), randomRange.max());
+    }
+
+    private static void validate(RandomRange randomRange) {
+        if (randomRange.min() > randomRange.max()) {
+            throw new IllegalArgumentException("최소값이 최대값보다 클 수 없습니다.");
+        }
     }
 }
