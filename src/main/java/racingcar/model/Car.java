65ec7377dda49ec78f9d37e1ee3a0473b0252a07
김예린 <annalin1004@gmail.com>
2024-10-25 13:03:13 +0900

    Construct Car model

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,22 @@
+package racingcar.model;
+
+public class Car {
+    private String carName;
+    private int distance;
+
+    public Car(String carName){
+        this.carName = carName;
+        this.distance = 0;
+    }
+    
+    public String getCarName(){
+        return this.carName;
+    }
+    public int getDistance(){
+        return this.distance;
+    }
+
+    public void setDistance(int distance){
+        this.distance += distance;
+    }
+}
