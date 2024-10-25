b7daa7f355051220a77f846d56132a114645f045
sinminseok12 <sinminseok12@naver.com>
2024-10-26 04:52:15 +0900

    feat : ','가 마지막에 온 경우 예외처리 구현
    자동차 이름 입력시, ',' 가 마지막에 온 경우 예외를 발생시킨다

--- src/main/java/racingcar/view/InputValidator.java
+++ src/main/java/racingcar/view/InputValidator.java
@@ -1,10 +1,15 @@
 package racingcar.view;
 
+import java.util.regex.Pattern;
+
 public class InputValidator {
     private static final String ERROR_MESSAGE_EMPTY_INPUT = "입력값은 공백일 수 없습니다.";
+    private static final String ERROR_MESSAGE_TRAILING_COMMA = "입력값의 마지막에 콤마(,)가 올 수 없습니다.";
+    private static final Pattern TRAILING_COMMA_PATTERN = Pattern.compile(".*,$");
 
     public static void validateCarNamesInput(String input) {
         checkEmptyInput(input);
+        checkTrailingComma(input);
     }
 
     public static void validateAttemptCountInput(String input) {
@@ -14,6 +19,12 @@ public class InputValidator {
     private static void checkEmptyInput(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException(ERROR_MESSAGE_EMPTY_INPUT);
+        }
+    }
+
+    private static void checkTrailingComma(String input) {
+        if (TRAILING_COMMA_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_TRAILING_COMMA);
         }
     }
 }
