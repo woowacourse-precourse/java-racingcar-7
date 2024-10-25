37053b29f062a1d519c556733eced7689843ab0f
uniiii <126189239+fjqmqjrm@users.noreply.github.com>
2024-10-25 17:53:14 +0900

    feat: 경주에 참여하는 자동차 객체 생성 기능

---
+++ src/main/java/racingcar/CarFactory.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarFactory {
+
+    public List<Car> createCars(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+
+        for (String name : carNames) {
+            cars.add(new Car(name,0));
+        }
+
+        return cars;
+    }
+
+}
