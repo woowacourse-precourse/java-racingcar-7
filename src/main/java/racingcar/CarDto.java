83a5413c6de5abe1a7813e3c2608b91c4c92853e
howareyoo <newworld121@naver.com>
2024-10-26 01:25:53 +0900

    rename : File rename
    Refer to the feedback document

---
+++ src/main/java/racingcar/CarDto.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+public class CarDto {
+
+    private String carName;
+    private boolean isMoving;
+
+    public CarDto(String carName, boolean isMoving) {}
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public boolean isMoving() {
+        return isMoving;
+    }
+
+}
