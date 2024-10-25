eb6c5750657ca57d4eb2567e7cfc88099e9ad224
JeongHyeon Jo <wjdgusdlekd3@yonsei.ac.kr>
2024-10-25 17:15:04 +0900

    feat(model): Car, CarRacing model feature inner method
    - 각 모델에 맞게 기능 추가
    - 아직 모든 기능이 들어가진 않았다

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,26 @@
 package racingcar.model;
 
+import racingcar.utils.CarNameValidator;
+import racingcar.utils.Engine;
+import racingcar.utils.Validator;
+
 public class Car {
+    private final String CarName;
+    private final Engine engine;
+    Validator validator = new CarNameValidator();
+
+    public Car(String carName, Engine randomEngine) {
+        validator.validation(carName);
+        this.CarName = carName;
+        this.engine = randomEngine;
+    }
+
+    public String getCarName() {
+        return CarName;
+    }
+
+    public boolean getEnginePower() {
+        return engine.isEngineRunning();
+    }
+
 }
--- src/main/java/racingcar/model/CarRacing.java
+++ src/main/java/racingcar/model/CarRacing.java
@@ -1,4 +1,27 @@
 package racingcar.model;
 
+import racingcar.utils.CarNameValidator;
+import racingcar.utils.CarRacingValidator;
+import racingcar.utils.Validator;
+
+import java.util.List;
+
 public class CarRacing {
+
+    private final List<Car> cars;
+    private final String carCount;
+    Validator validator = new CarRacingValidator();
+
+    public CarRacing(List<Car> cars, String carCount) {
+        validator.validation(carCount);
+        this.cars = cars;
+        this.carCount = carCount;
+    }
+
+    public void startRacing() {
+        for (Car car : cars) {
+            System.out.println(car.getEnginePower());
+        }
+    }
+
 }
