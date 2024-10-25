fc4b4468d6dae658a30e245b6a1c213278b357c9
jkh28 <jkhkorea28@naver.com>
2024-10-25 16:50:02 +0900

    Feat: race 함수 개발

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,18 +1,41 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Application {
+
+    static void race(Map<String, Integer> cars, List<String> keys) {
+        for (String key : keys) {
+            int n = Randoms.pickNumberInRange(0, 9);
+
+            if (n >= 4) {
+                cars.put(key, cars.get(key) + 1);
+            }
+
+            System.out.println(key + " : " + "-".repeat(cars.get(key)));
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        List<String> carInput = List.of(Console.readLine().split(","));
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> keys = List.of(Console.readLine().split(","));
 
         Map<String, Integer> cars = new HashMap<>();
-        for(String car : carInput) {
-            cars.put(car, 0);
+        for (String key : keys) {
+            cars.put(key, 0);
         }
 
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int n = Integer.parseInt(Console.readLine());
+        System.out.println("\n실행 결과");
+
+        for (int i = 0; i < n; i++) {
+            race(cars, keys);
+        }
     }
 }
