702d43097f5fabd790f31c0a9e34732302d7e6c2
StoneCAU <seokwoo1999@naver.com>
2024-10-25 21:28:44 +0900

    chore(OutputView): 상수 구분 주석 추가

--- src/main/java/racingcar/view/output/OutputView.java
+++ src/main/java/racingcar/view/output/OutputView.java
@@ -5,11 +5,14 @@ import java.util.List;
 import static java.lang.String.format;
 
 public class OutputView {
+    // Output Messages
     public static final String ASK_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String ASK_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
     public static final String RESULT_TITLE = "실행 결과";
     public static final String RESULT_DETAIL = "%s : %s";
     public static final String FINAL_RESULT = "최종 우승자 : %s";
+
+    // Delimiter
     public static final String WINNER_DELIMITER = ", ";
 
     public void printMessage(String message) {
