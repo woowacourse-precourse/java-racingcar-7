4c6e9e56e08f82f28bd30a67b745093a77d15ea3
bowook <mbosx316@naver.com>
2024-10-25 22:03:12 +0900

    test(RandomNumberTest) : RandomNumberTest 테스트
    - 랜덤한 숫자가 0,9 사이에 생성되는지 검증

--- src/test/java/racingcar/domain/RandomNumberTest.java
+++ src/test/java/racingcar/domain/RandomNumberTest.java
@@ -1,2 +1,20 @@
-package racingcar.domain;public class RandomNumberTest {
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomNumberTest {
+    @Test
+    void 범위내_숫자_생성() {
+        //given
+        int randomStartNumber = 1;
+        int randomLastNumber = 10;
+
+        //when
+        int generatedNumber = RandomNumber.getRandomNumber();
+
+        //then
+        assertThat(generatedNumber).isBetween(randomStartNumber,randomLastNumber);
+    }
 }
