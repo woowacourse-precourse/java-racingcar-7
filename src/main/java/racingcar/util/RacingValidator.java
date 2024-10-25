45a120e38ce2ed175fcaabccc2679eeb950ace3c
mjw3723 <mjw3723@naver.com>
2024-10-25 14:46:20 +0900

    refactor: 에러 출력 메세지 변경

--- src/main/java/racingcar/util/RacingValidator.java
+++ src/main/java/racingcar/util/RacingValidator.java
@@ -17,7 +17,7 @@ public class RacingValidator {
 
     private static void isValidNameListSize(List<String> nameList) {
         if (nameList.isEmpty() || nameList.size() == 1) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("참여자는 2명 이상이어야 합니다.");
         }
     }
 
@@ -25,14 +25,14 @@ public class RacingValidator {
         boolean validLength = nameList.stream()
                 .anyMatch(s -> s.length() < MIN_NAME_LENGTH || s.length() > MAX_NAME_LENGTH);
         if (validLength) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("이름은 1자 이상 5자 이하로 입력해야 합니다.");
         }
     }
 
     private static void isValidDuplicateName(List<String> nameList) {
         int validLength = new HashSet<>(nameList).size();
         if (validLength != nameList.size()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("중복된 이름이 있습니다.");
         }
     }
 
@@ -40,7 +40,7 @@ public class RacingValidator {
         try {
             Integer.parseInt(input);
         } catch (Exception e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
         }
     }
 
