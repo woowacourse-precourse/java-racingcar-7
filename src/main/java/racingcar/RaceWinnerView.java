202eb0742f413d4c64407d457c77620ad77c1394
chaerish <ccyy8432@naver.com>
2024-10-26 02:55:04 +0900

    feat(RaceWinnerView): 경기 우승자 출력 기능 구현
    경기 우승자를 출력하기 위해 해당 기능을 구현한다.

--- src/main/java/racingcar/RaceWinnerView.java
+++ src/main/java/racingcar/RaceWinnerView.java
@@ -2,9 +2,21 @@ package racingcar;
 
 public class RaceWinnerView {
     private static final String WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String COMMA = ",";
+    private static final String BLANK = " ";
+    private String winner = "";
+    private static final String LINE_BLANK = "\n";
 
     public void announceWinner(String[] winners) {
-        System.out.println(WINNER_MESSAGE);
+        System.out.print(LINE_BLANK + WINNER_MESSAGE);
+        if (winners.length == 1) {
+            System.out.print(winners[0]);
+            return;
+        }
+        for (String winner : winners) {
+            this.winner += winner + COMMA + BLANK;
+        }
+        System.out.print(winner);
     }
 
 }
