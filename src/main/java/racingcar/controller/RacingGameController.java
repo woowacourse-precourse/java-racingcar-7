101732b7ee557cb13c6f6718031c38914a068193
PSW99 <steamdayz@naver.com>
2024-10-25 18:00:31 +0900

    feat(controller): implement game controller for managing the race - Added RacingGameController to manage the game flow - Linked input, output, and game logic

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,4 +1,52 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
+import racingcar.domain.RacingGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
 public class RacingGameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RacingGame racingGame;
+
+    public RacingGameController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.racingGame = new RacingGame();
+    }
+
+    public void start() {
+        List<Car> carList = initializeCars();
+        int attemptCount = initializeAttempts();
+        racingGame.setCars(carList);
+
+        playRounds(attemptCount);
+        displayResults();
+    }
+
+    private void playRounds(int attemptCount) {
+        for (int i = 0; i < attemptCount; i++) {
+            racingGame.play();
+            outputView.displayRaceProgress(racingGame.getCarList());
+        }
+    }
+
+    private void displayResults() {
+        outputView.displayWinners(racingGame.getCarList());
+        outputView.displayExecutionResult();
+    }
+
+    private List<Car> initializeCars() {
+        outputView.inputCarNames();
+        return inputView.readCarNames();
+    }
+
+    private int initializeAttempts() {
+        outputView.inputAttemptCount();
+        return inputView.inputAttemptCount();
+    }
+
 }
