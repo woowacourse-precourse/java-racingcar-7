66ddd72094de52c45763d3e60bfd23eebdc91212
githublees <leeho_@naver.com>
2024-10-26 00:32:39 +0900

    test: Update test case for InputParserTest class

--- src/test/java/racingcar/input/InputParserTest.java
+++ src/test/java/racingcar/input/InputParserTest.java
@@ -1,7 +1,10 @@
 package racingcar.input;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,21 +23,48 @@ public class InputParserTest {
     void 이름_입력값_컬렉션_반환() {
         List<String> cars = inputParser.toList("pobi,woni");
 
-        assertEquals(cars.size(), 2);
-        assertEquals(cars.get(0), "pobi");
-        assertEquals(cars.get(1), "woni");
+        assertSimpleTest(() ->
+                assertThat(cars.size()).as("cars size").isEqualTo(2)
+        );
+
+        assertSimpleTest(() ->
+                assertThat(cars).contains("pobi", "woni")
+        );
     }
 
     @Test
     void 쉼표_사이에_공백이_존재하는_경우() {
         assertThatThrownBy(() -> inputParser.toList("pobi, woni"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름 사이에 공백이 존재해서는 안됩니다.")
+                // check start
+                .hasMessageStartingWith("이름")
+                // check contain
+                .hasMessageContaining("이름 사이")
+                // check end
+                .hasMessageEndingWith("안됩니다.")
+                // check regex
+                .hasMessageMatching("이름 사이에 .*")
+                // check does not contain
+                .hasMessageNotContaining("체크");
     }
 
     @Test
     void 이름_사이에_공백이_존재하는_경우() {
-        assertThatThrownBy(() -> inputParser.toList("pobi,wo ni"))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertSimpleTest(() ->
+                assertThatExceptionOfType(IllegalArgumentException.class)
+                        .isThrownBy(() -> inputParser.toList("pobi,wo ni"))
+        );
+
+        assertSimpleTest(() ->
+                assertThatIllegalArgumentException()
+                        .isThrownBy(() -> inputParser.toList("pobi,wo ni"))
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> inputParser.toList("pobi,wo ni"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Test
