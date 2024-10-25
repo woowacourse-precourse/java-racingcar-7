c15e998ab76fbb3fff214b33d73234f3d7f0f9e9
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-10-25 15:58:00 +0900

    fix(view): 입력부 시작 메시지 추가를 위한 수정
    - 입력 전, 시작 메시지가 출력된 후 입력을 받아야 한다.
    - 시작 메시지가 상황마다 다르기 때문에 분리한다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,7 +5,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private InputView() {}
 
-    public static String input() {
+    public static String inputCars() {
+        final String INPUT_CARS_INTRO_MESSAGE ="경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+        System.out.println(INPUT_CARS_INTRO_MESSAGE);
+        String input = Console.readLine();
+        Console.close();
+        return input;
+    }
+    public static String inputRounds() {
+        final String INPUT_ROUNDS_INTRO_MESSAGE = "시도할 횟수는 몇 회인가요?";
+        System.out.println(INPUT_ROUNDS_INTRO_MESSAGE);
         String input = Console.readLine();
         Console.close();
         return input;
