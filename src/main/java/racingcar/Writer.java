34748551e92454ae843b2d3371416ee39f6180e3
HyeonJeong <wgwjh05169@gmail.com>
2024-10-25 21:12:25 +0900

    refactor(Writer): 출력 메소드명 변경

--- src/main/java/racingcar/Writer.java
+++ src/main/java/racingcar/Writer.java
@@ -4,19 +4,19 @@ import java.util.List;
 
 public class Writer {
 
-    public static void printMessage(String message) {
+    public static void writeMessage(String message) {
         System.out.println(message);
     }
 
-    public static void printRoundResult(List<Car> cars) {
+    public static void writeRoundResult(List<Car> cars) {
 
         for (Car car : cars)
-            printCarScore(car);
+            writeCarScore(car);
 
         System.out.println();
     }
 
-    public static void printCarScore(Car car) {
+    public static void writeCarScore(Car car) {
         System.out.print(car.getName() + " : ");
         for (int i = 0; i < car.getScore(); i++)
             System.out.print("-");
@@ -24,7 +24,7 @@ public class Writer {
         System.out.println("");
     }
 
-    public static void printWinner(List<Car> winners) {
+    public static void writeWinner(List<Car> winners) {
         System.out.print("최종 우승자 : ");
         for (Car car : winners) {
             System.out.print(car.getName());
