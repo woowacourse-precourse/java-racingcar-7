deeba76df6083646dfe35199ef68dceff8424818
sunjong0214 <rlatjswhd8200@gmail.com>
2024-10-25 23:04:18 +0900

    feat(Prompt): input에 빈 문자가 존재하는지 확인
    빈 문자가 있을 시 IllegalArgumentException 에러 발생

--- src/main/java/racingcar/IOPreprocessor.java
+++ src/main/java/racingcar/IOPreprocessor.java
@@ -5,7 +5,19 @@ import java.util.List;
 
 public class IOPreprocessor {
     private static List<String> parsingInput(String input) {
-        return Arrays.stream(input.split(",")).toList();
+        String[] splitInput = input.split(",");
+        validateInput(input, splitInput);
+        return Arrays.stream(splitInput).toList();
+    }
+
+    /**
+     * @param input      마지막에 ,이 되어있는지 확인
+     * @param splitInput 배열 중간에 빈 문자가 있는지 확인
+     */
+    private static void validateInput(String input, String[] splitInput) {
+        if (input.charAt(input.length() - 1) == ',' || Arrays.stream(splitInput).anyMatch(String::isBlank)) {
+            throw new IllegalArgumentException("입력된 이름 중 빈 문자가 존재합니다.");
+        }
     }
 
     private static void setRacingCarTrace(List<String> parsingInput, RacingCarTrace racingCarTrace) {
