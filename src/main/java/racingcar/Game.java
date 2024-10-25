8f9baada82dbc60d840b08c1e89d4bc409d55faa
joonwan <joonwan1024@naver.com>
2024-10-26 00:52:06 +0900

    feat: 레이싱 게임 메인 흐름 조립

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,4 +1,44 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.car.Cars;
+
 public class Game {
+
+    private static final String START_MESSAGE = "\n실행 결과";
+
+    public static void run() {
+        try {
+            String input = InputManager.readUserInput();
+            List<String> names = NameExtractor.getNames(input);
+
+            Cars cars = new Cars(names);
+
+            int tryCount = InputManager.readTryCount();
+            race(cars, tryCount);
+
+            List<String> winnerNames = cars.getWinnerNames();
+            OutputManager.printWinner(winnerNames);
+        } finally {
+            InputManager.close();
+        }
+    }
+
+    private static void race(Cars cars, int tryCount) {
+
+        printStartMessage();
+
+        for(int i = 0; i< tryCount; i++) {
+            cars.attemptMoveAllCars();
+            printNewLine();
+        }
+    }
+
+    private static void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
+
+    private static void printNewLine() {
+        System.out.println();
+    }
 }
