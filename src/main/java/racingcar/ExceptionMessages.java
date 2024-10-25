f8507423199f9c2e9b5e7a3eb58d91948a9fccca
yyysolhhh <solbaram37@gmail.com>
2024-10-26 02:18:04 +0900

    feat(ExceptionMessages): INVALID_ROUNDS_INPUT 추가
    - 이동 횟수 예외 메시지 추가

--- src/main/java/racingcar/ExceptionMessages.java
+++ src/main/java/racingcar/ExceptionMessages.java
@@ -2,6 +2,7 @@ package racingcar;
 
 public enum ExceptionMessages {
     NAME_TOO_LONG("자동차 이름은 5자 이하만 가능합니다."),
+    INVALID_ROUNDS_INPUT("이동 횟수는 양의 정수여야 합니다."),
     ;
 
     private final String message;
