02fb86e337a2bfe872baeea16c53556f68179c28
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-10-25 21:20:49 +0900

    refactor: CarNames 클래스로 분리하기
    CarName이 여러개 있음을 의미하는 CarNames

---
+++ src/main/java/domain/CarNames.java
@@ -0,0 +1,39 @@
+package domain;
+
+import java.util.List;
+
+public class CarNames {
+
+    private static final String DUPLICATE_ERROR_MESSAGE = "중복되지 않은 이름을 입력해야 합니다.";
+    private static final Integer MINIMUM_CAR_NUMBER = 2;
+    private static final String CAR_NUMBER_ERROR_MESSAGE = "자동차는 2개 이상을 입력해야 합니다.";
+
+    private List<CarName> names;
+
+    public CarNames(final List<CarName> names) {
+        validateInputSize(names);
+        validateDuplicate(names);
+        this.names = names;
+    }
+
+
+    private static void validateDuplicate(final List<CarName> names) {
+        Boolean result = names.stream()
+                .distinct()
+                .count() != names.size();
+
+        if (result) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateInputSize(final List<CarName> names) {
+        if (names.size() < MINIMUM_CAR_NUMBER) {
+            throw new IllegalArgumentException(CAR_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
+    public List<CarName> getNames() {
+        return names;
+    }
+}
