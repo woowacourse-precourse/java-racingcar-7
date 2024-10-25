11d6d524472c9631c1e66c2286addb210bf4519e
hanmh319 <hanmh319@naver.com>
2024-10-25 21:54:06 +0900

    feat: 1회 이동 함수 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,22 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
+
     public static void main(String[] args) {
         String[] names = Console.readLine().split(",");
-        int attemtps = Integer.parseInt(Console.readLine());
+        int attempts = Integer.parseInt(Console.readLine());
+        String[] individualCarMovements = new String[names.length];
     }
+
+    public static void individualRandoms(String[] individualCarMovements, String[] names) { // 1회 이동 함수
+        for (int i = 0; i < names.length; i++) {
+            if (Randoms.pickNumberInRange(0, 9) > 4) {
+                individualCarMovements[i] += "-";
+            }
+        }
+    }
+
 }
