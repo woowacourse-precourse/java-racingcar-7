934edd0fdfd2208f802693a3e0cbde0e26fbf627
yush <yush2202@gmail.com>
2024-10-25 18:32:59 +0900

    feat($ CarTest): 자동차 객체 테스트
    자동차 객체와 앞으로 나가갈지 결정하는 기능 테스트

---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,32 @@
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.Application;
+
+public class CarTest extends NsTest {
+    @ParameterizedTest
+    @DisplayName("자동차가 제대로 나아가는지 확인합니다.")
+    @CsvSource({
+            "4, 0",
+            "5, 1",
+            "9, 1",
+            "2, 0"
+    })
+    void car_테스트(int randomNumber, int answer) {
+        CarRacer carRacer = new CarRacer("거북이");
+        Car testCar = new Car(carRacer);
+        testCar.decideToGo(randomNumber);
+
+        assertThat(testCar.getDistance()).isEqualTo(answer);
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
