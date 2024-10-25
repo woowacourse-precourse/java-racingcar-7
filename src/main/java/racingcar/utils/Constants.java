1a36779d48a7d68fd5d9b24c6dc9a1193c9a9799
egaeng09 <egaeng09@gmail.com>
2024-10-26 02:22:09 +0900

    feat(Constants): 입출력 메시지 상수화

---
+++ src/main/java/racingcar/utils/Constants.java
@@ -0,0 +1,9 @@
+package racingcar.utils;
+
+public final class Constants {
+    public static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    public static final String OUTPUT_RESULT_MESSAGE = "실행 결과";
+    public static final String OUTPUT_WINNER_MESSAGE = "최종 우승자 : ";
+    public static final String DISTANCE_DASH = "-";
+}
