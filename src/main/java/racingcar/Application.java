0c38a3269d99633ac3ab4bceda44b607913debb9
k1mjunyoung <me@kimjunyoung.com>
2024-10-25 16:04:34 +0900

    feat: 자동차 이름 입력 기능 추가
    - `camp.nextstep.edu.missionutils.Console`의 `readLine()` 사용하여 사용자 입력 구현
    -입력한 문자열에서 쉼표(,)를 기준으로 구분하여 자동차 이름(5자 이하) 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static List<String> getCars() {
+        String input = Console.readLine();
+
+        String[] inputArray = input.split(",");
+
+        for (String car : inputArray) {
+            if (car.length() > 5)
+                throw new IllegalArgumentException();
+        }
+
+        List<String> cars = Arrays.asList(input.split(","));
+
+        return cars;
     }
 }
