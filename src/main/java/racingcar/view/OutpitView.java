003e79189dfd4b49751b47ff2fa361d99d61228c
dradnats1012 <herjebi1012@gmail.com>
2024-10-25 17:01:13 +0900

    feat: OutputView 클래스 구현

--- src/main/java/racingcar/view/OutpitView.java
+++ src/main/java/racingcar/view/OutpitView.java
@@ -1,4 +1,29 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.Map;
+
 public class OutpitView {
+
+    private static String RESULT = "실행 결과";
+    private static String WINNER = "최종 우승자 : ";
+
+    public static void printResult() {
+        System.out.println(RESULT);
+    }
+
+    public static void printRacing(List<Map<String, Integer>> cars) {
+        for (Map car : cars) {
+            printCarPosition(car);
+        }
+    }
+
+    private static void printCarPosition(Map<String, Integer> car) {
+        car.forEach((name, position) ->
+            System.out.println(name + " : " + "-".repeat(position)));
+    }
+
+    public static void printWinner(List<String> winners) {
+        System.out.println(WINNER + String.join(",", winners));
+    }
 }
