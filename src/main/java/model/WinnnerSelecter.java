40294918d479b993406c8772fbc0f5d43b273c26
shin-chaeeun <chaeeun0217@naver.com>
2024-10-25 21:53:04 +0900

    Refactor(WinnnerSelecter):Extract WinnnerSelecter from main class

---
+++ src/main/java/model/WinnnerSelecter.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class WinnnerSelecter {
+
+  public List<String> selectWinner(Map<String, String> carGameRecords) {
+    List<String> winnerList = new ArrayList<>();
+    int bestRecords = 0;
+    for (Map.Entry<String, String> carsRecords : carGameRecords.entrySet()) {
+      if (carsRecords.getValue().length() >= bestRecords) {
+        bestRecords = carsRecords.getValue().length();
+        winnerList.add(carsRecords.getKey());
+      }
+    }
+    return winnerList;
+  }
+}
