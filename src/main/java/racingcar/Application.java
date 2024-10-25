fe0920c28cca11604dd67d1bc8e8f132f16d636b
kimshinwoo <tlsdn67@naver.com>
2024-10-25 22:36:02 +0900

    feat: Get user's name

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String[] nameArr = Console.readLine().split(",");
     }
 }
