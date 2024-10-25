f682a65e7fad193a5aa14bf1e7eab662fbe87c3a
dradnats1012 <herjebi1012@gmail.com>
2024-10-25 20:00:29 +0900

    refactor: 자동차 위치 출력 메서드 변경, 2차 개발 시작

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,7 +3,7 @@ package racingcar.view;
 import java.util.List;
 import java.util.Map;
 
-public class OutpitView {
+public class OutputView {
 
     private static String RESULT = "실행 결과";
     private static String WINNER = "최종 우승자 : ";
@@ -12,14 +12,14 @@ public class OutpitView {
         System.out.println(RESULT);
     }
 
-    public static void printRacing(List<Map<String, Integer>> cars) {
+    /*public static void printRacing(List<Map<String, Integer>> cars) {
         for (Map car : cars) {
             printCarPosition(car);
         }
-    }
+    }*/
 
-    private static void printCarPosition(Map<String, Integer> car) {
-        car.forEach((name, position) ->
+    public static void printCarPosition(Map<String, Integer> cars) {
+        cars.forEach((name, position) ->
             System.out.println(name + " : " + "-".repeat(position)));
     }
 
