a651fecb6c498291ad14db12dcad44dd70e81dc0
unh6unh6 <ruddbs3308@naver.com>
2024-10-25 16:36:13 +0900

    feat(view): 자동차 이동 횟수가 공백이 아닌지 검사

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,6 +5,9 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String CAR_NAME_INPUT_GUIDE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String CAR_MOVEMENT_COUNT_INPUT_GUIDE = "시도할 횟수는 몇 회인가요?";
+
+    private static final String CAR_MOVEMENT_COUNT_BLANK_EXCEPTION = "자동차 이동 횟수는 빈칸이 아니어야합니다.";
+
     public void printCarNameInputGuide() {
         System.out.println(CAR_NAME_INPUT_GUIDE);
     }
@@ -19,5 +22,11 @@ public class InputView {
 
     public String getCarMovementCount() {
         return Console.readLine();
+    }
+
+    public void validateCarMovementCountOrThrow(String inputString) {
+        if (inputString.isBlank()) {
+            throw new IllegalArgumentException(CAR_MOVEMENT_COUNT_BLANK_EXCEPTION);
+        }
     }
 }
