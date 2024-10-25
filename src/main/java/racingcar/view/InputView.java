4e4442e259438cbdea7607d669b80d0924668dfc
eunseobb <seobseob0810@naver.com>
2024-10-26 01:05:35 +0900

    refactor(view): 자동차 이동여부 확인 및 이동 기능 추가
    메서드 이름을 get에서 read로 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,12 +3,12 @@ package racingcar.view;
 import static camp.nextstep.edu.missionutils.Console.*;
 
 public class InputView {
-    public String getRawCarNames(){
+    public String readRawCarNames() {
         System.out.println("자동차 이름들을 입력해주세요(쉼표로 구분).");
         return readLine();
     }
 
-    public Integer getAttemptCount(){
+    public Integer readAttemptCount() {
         System.out.println("시도할 횟수를 입력해주세요");
         return Integer.parseInt(readLine());
     }
