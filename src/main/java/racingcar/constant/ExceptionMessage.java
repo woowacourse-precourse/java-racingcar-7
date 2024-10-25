55731aa150d58746f9d9dc2ad94adaaa43ee0552
김연진 <love_homebody@naver.com>
2024-10-25 19:30:07 +0900

    feat(Car): 이름이 5자가 넘을 경우 예외 발생

--- src/main/java/racingcar/constant/ExceptionMessage.java
+++ src/main/java/racingcar/constant/ExceptionMessage.java
@@ -2,7 +2,8 @@ package racingcar.constant;
 
 public enum ExceptionMessage {
 
-    NOT_INTEGER("숫자만 입력할 수 있습니다.");
+    NOT_INTEGER("숫자만 입력할 수 있습니다."),
+    TOO_LONG_NAME("이름은 5자 이하만 가능합니다.");
 
     private final String message;
 
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,19 +1,29 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.constant.ExceptionMessage;
 
 public class Car {
 
     private static final int RUN_NUMBER = 4;
     private static final String POSITION_MARK = "-";
     private static final String NAME_MARK = " : ";
+    private static final int MAX_NAME_LENGTH = 5;
 
     private final String name;
     private int position;
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
         this.position = 0;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            ExceptionMessage message = ExceptionMessage.TOO_LONG_NAME;
+            throw new IllegalArgumentException(message.getMessage());
+        }
     }
 
     public void move() {
