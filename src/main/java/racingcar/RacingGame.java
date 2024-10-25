f27038b8ba3519d0e7c26a10ea557fbe34c222a2
DoubleKK <138849238+kwonkeonhyeong@users.noreply.github.com>
2024-10-25 16:09:25 +0900

    feat(inputView): 시도 횟수 입력
    게임 시도 횟수 입력

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -6,11 +6,20 @@ public class RacingGame {
     public void run() {
         String carNames = inputCarNames();
         System.out.println(carNames);
+
+        String tryCount = inputTryCount();
+        System.out.println(tryCount);
     }
 
     public String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputCarNames = readLine();
         return inputCarNames;
+    }
+
+    public String inputTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String inputTryCount = readLine();
+        return inputTryCount;
     }
 }
