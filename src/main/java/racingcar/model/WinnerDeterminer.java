c2f39f344a7573426e5668e2e44d2ea60652d6d2
Chan531 <chan657312@gmail.com>
2024-10-25 16:56:09 +0900

    feat(WinnerDeterminer): 자동차 이동 최대값 추출

--- src/main/java/racingcar/model/WinnerDeterminer.java
+++ src/main/java/racingcar/model/WinnerDeterminer.java
@@ -6,6 +6,13 @@ public class WinnerDeterminer {
 
 	private static final int MIN_MOVEMENT_VALUE = 0;
 
+	private static int getMaxMovementValue(List<Car> cars) {
+		return cars.stream()
+				.mapToInt(Car::getMovement)
+				.max()
+				.orElse(MIN_MOVEMENT_VALUE);
+	}
+
 	private static List<String> getWinner(List<Car> cars, int maxMovementValue) {
 		return cars.stream()
 				.filter(car -> car.getMovement() == maxMovementValue)
