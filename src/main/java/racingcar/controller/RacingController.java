dd5a2db7e0cb6e6573f1bb156fe8dffc023c10b2
변우영 <likevangogh24@gmail.com>
2024-10-25 23:13:12 +0900

    feat(RacingController): 모든 라운드의 자동차 경주를 진행하는 기능 추가

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,55 @@
+package racingcar.controller;
+
+import racingcar.domain.RacingCars;
+import racingcar.util.InputSplitter;
+import racingcar.util.RandomNumberGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingController {
+
+    private final RacingCars racingCars;
+
+    public RacingController() {
+        this.racingCars = new RacingCars();
+    }
+
+    public void startGame() {
+        registerRacingCars();
+        int round = getRound();
+
+        for (int i = 0; i < round; i++) {
+            racingCars.updatePositionsWithRandomNumbers(generateRandomNumbers(countCars()));
+            OutputView.printRoundResult(racingCars.getStatus());
+        }
+        OutputView.printWinnerNames(racingCars.selectWinners());
+    }
+
+    private void registerRacingCars() {
+        OutputView.printInputCarNameMessage();
+        List<String> carNames = InputSplitter.splitByComma(InputView.inputCarNames());
+        racingCars.registerCars(carNames);
+    }
+
+    private int getRound() {
+        OutputView.printInputRoundMessage();
+        String inputRoundNumber = InputView.inputRoundNumber();
+
+        return Integer.parseInt(inputRoundNumber);
+    }
+
+    private List<Integer> generateRandomNumbers(int racingCarCount) {
+        List<Integer> randomNumbers = new ArrayList<>();
+        for (int i = 0; i < racingCarCount; i++) {
+            randomNumbers.add(RandomNumberGenerator.generate());
+        }
+        return randomNumbers;
+    }
+
+    private int countCars() {
+        return racingCars.getStatus().size();
+    }
+}
