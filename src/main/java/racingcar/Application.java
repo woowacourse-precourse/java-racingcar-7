14d9f34b8ca564a9d606e6a316a7a2f8d012e18a
Jewan1120 <hawaii1468@gmail.com>
2024-10-25 18:30:45 +0900

    refactor(Application): 메인 함수의 로직을 컨트롤러로 이동하여 역할 분리
    절차 지향적으로 구성되어있던 로직을 객체 지향적으로 전환하고 있습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,54 +1,11 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-import racingcar.domain.Car;
-import racingcar.enums.ErrorMessage;
-import racingcar.enums.ViewMessage;
+import racingcar.controller.RacingCarController;
 
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println(ViewMessage.PROMPT_CAR_NAMES.getMessage());
-        String inputString = Console.readLine();
-        StringTokenizer stringTokenizer = new StringTokenizer(inputString, ",");
-        List<Car> cars = new ArrayList<>();
-        List<String> winners = new ArrayList<>();
-        while (stringTokenizer.hasMoreTokens()) {
-            String carName = stringTokenizer.nextToken();
-            if (carName.length() > 5) {
-                throw new IllegalArgumentException(ErrorMessage.CAR_NAME_LENGTH.getMessage());
-            }
-            cars.add(new Car(carName));
-        }
-        System.out.println(ViewMessage.PROMPT_PLAY_TIMES.getMessage());
-        inputString = Console.readLine();
-        System.out.println(ViewMessage.PRINT_BLANK.getMessage());
-        System.out.println(ViewMessage.PRINT_RESULT.getMessage());
-        int playTime = Integer.parseInt(inputString);
-        int maxMoveCount = 0;
-        while (playTime-- > 0) {
-            for (Car car : cars) {
-                int value = Randoms.pickNumberInRange(0, 9);
-                car.move(value);
-                maxMoveCount = Math.max(maxMoveCount, car.moveCount);
-            }
-            StringBuilder stringBuilder = new StringBuilder();
-            for (Car car : cars) {
-                stringBuilder.append(car);
-                stringBuilder.append(ViewMessage.PRINT_BLANK.getMessage());
-            }
-            System.out.println(stringBuilder);
-        }
-        for (Car car : cars) {
-            if (car.moveCount == maxMoveCount) {
-                winners.add(car.name);
-            }
-        }
-        String result = String.join(", ", winners);
-        System.out.printf(ViewMessage.PRINT_WINNERS.getMessage(result));
+        RacingCarController racingCarController = new RacingCarController();
+        racingCarController.play();
     }
 }
