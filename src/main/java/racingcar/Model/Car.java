11b5010b2f2256c91cf04f892bf46a05ef3cb401
이윤빈 <kanado20000@naver.com>
2024-10-25 18:57:45 +0900

    :sparkles: feat: Cat 클래스를 위한 필드와 메서드 추가
    name은 자동차 이름, location은 현재 위치를 나타낸다. goFront는 전진 조건 충족 시 이동, nameWithLocationToString은 라운드 결과 출력, compareLocation은 최종 우승자 선별을 위한 위치 비교 메서드이다.

---
+++ src/main/java/racingcar/Model/Car.java
@@ -0,0 +1,44 @@
+package racingcar.Model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Car {
+  private String name;
+  private int location;
+
+  Car(String name) {
+    this.name = name;
+    this.location = 0;
+  }
+
+  public void goFront() {
+    this.location++;
+  }
+
+  public String nameWithLocationToString() {
+    return name
+        + " : "
+        + IntStream.range(0, location).mapToObj(i -> "-").collect(Collectors.joining());
+  }
+
+  public Map<String, Object> compareLocation(int maxNumber, ArrayList<String> winners) {
+    if (location == maxNumber) {
+      winners.add(name);
+    }
+    if (location > maxNumber) {
+      winners.clear();
+      winners.add(name);
+      maxNumber = location;
+    }
+
+    Map<String, Object> result = new HashMap<>();
+    result.put("maxNumber", maxNumber);
+    result.put("winners", winners);
+
+    return result;
+  }
+}
