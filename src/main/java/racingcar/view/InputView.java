96cac90362aba3bebc5ac7024dd3b0d1c0d961c7
JGM <cnxw4570123@gmail.com>
2024-10-25 16:35:27 +0900

    refactor : 확장성을 고려해 InputView 입력 방법 수정
    - 기존 Console.readLine()으로만 입력받을 수 있던 InputView를 Supplier<String>을 사용해 텍스트로도 입력받을 수 있도록 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,16 +1,22 @@
 package racingcar.view;
 
-import camp.nextstep.edu.missionutils.Console;
+import java.util.function.Supplier;
 
 public class InputView {
+	public Supplier<String> inputSupplier;
 	public static final String ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
 	public static final String COMMA = ",";
+	public static final String INPUT_CANNOT_BE_EMPTY = "이름은 비어있을 수 없습니다.";
+
+	public InputView(Supplier<String> inputSupplier) {
+		this.inputSupplier = inputSupplier;
+	}
 
 	public String[] getCarNames() {
 		System.out.println(ENTER_CAR_NAMES);
-		String input = Console.readLine();
+		String input = inputSupplier.get();
 		if (input.isBlank()) {
-			throw new IllegalArgumentException("이름은 비어있을 수 없습니다.");
+			throw new IllegalArgumentException(INPUT_CANNOT_BE_EMPTY);
 		}
 		return input.split(COMMA);
 	}
