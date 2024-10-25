3335e1c32a287bcc0f4d994584d86ccc4186ca7c
Chanyoo Kim <cksdb012@gmail.com>
2024-10-25 18:39:00 +0900

    feat(Racing): 게임 진행 기능 구현
    무작위 값을 추출하는 기능과 전진 여부를 구분하는 기능을 구현

--- src/main/java/racingcar/model/Racing.java
+++ src/main/java/racingcar/model/Racing.java
@@ -2,6 +2,7 @@ package racingcar.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Racing {
     protected final List<Car> cars = new ArrayList<>();
@@ -9,6 +10,21 @@ public class Racing {
     public Racing(List<String> cars) {
         cars.forEach(name -> this.cars.add(new Car(name)));
     }
+    public List<Car> getCars() {
+        return cars;
+    }
 
+    public void round(){
+        for(Car car : cars){
+            int randomNumber = random();
+            if(randomNumber >= 4){
+                car.setDistance();
+            }
+        }
+    }
+
+    public int random(){
+        return Randoms.pickNumberInRange(0, 9);
+    }
 
 }
