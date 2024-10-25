63f9d7b55ab23617b6e7be925b20c11f5dd204fc
eunho <eunho1032@gmail.com>
2024-10-25 16:04:17 +0900

    refactor(Output): using tabs change spaces

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -4,39 +4,38 @@ import java.util.List;
 
 public class Output {
 
-	public static void printResult(List<Car> cars, int tryCounts) {
-		System.out.println("실행 결과");
-		for (int i = 0; i < tryCounts; i++) {
-			printMovement(cars);
-		}
-		printWinner(Winner.contain(cars));
-	}
+    public static void printResult(List<Car> cars, int tryCounts) {
+        System.out.println("실행 결과");
+        for (int i = 0; i < tryCounts; i++) {
+            printMovement(cars);
+        }
+        printWinner(Winner.find(cars));
+    }
 
-	// 메서드 구분 필요
-	public static void printMovement(List<Car> cars) {
-		for (Car car : cars) {
-			car.drive();
-			System.out.println(car.name + " : " + displayDistance(car));
-		}
-		System.out.println();
-	}
+    public static void printMovement(List<Car> cars) {
+        for (Car car : cars) {
+            car.drive();
+            System.out.println(car.name + " : " + displayDistance(car));
+        }
+        System.out.println();
+    }
 
-	public static String displayDistance(Car car) {
-		String distance = "";
-		for (int i = 0; i < car.distance; i++) {
-			distance += "-";
-		}
-		return distance;
-	}
+    public static String displayDistance(Car car) {
+        String distance = "";
+        for (int i = 0; i < car.distance; i++) {
+            distance += "-";
+        }
+        return distance;
+    }
 
-	public static void printWinner(List<Car> winners) {
-		System.out.print("최종 우승자 : ");
+    public static void printWinner(List<Car> winners) {
+        System.out.print("최종 우승자 : ");
 
-		if (winners.size() == 1) {
-			System.out.print(winners.getFirst().name);
-			return;
-		}
+        if (winners.size() == 1) {
+            System.out.print(winners.getFirst().name);
+            return;
+        }
 
-		System.out.println(Winner.makeList());
-	}
+        System.out.println(Winner.makeList(winners));
+    }
 }
