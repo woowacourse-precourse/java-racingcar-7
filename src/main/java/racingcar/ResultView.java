e0953da070223af164be98878b869be783034fdb
dltjdnjs <scw.dltjdnjs@gmail.com>
2024-10-26 03:14:26 +0900

    feat: announceWinners 메서드에서 최종 위치를 비교하여 우승자 발표

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -6,7 +6,6 @@ public class ResultView {
     public static void printPositions(List<Car> cars) {
         for (Car car : cars) {
             System.out.print(car.getName() + " : ");
-
             if (car.getPosition() > 0) {
                 System.out.println("-".repeat(car.getPosition()));
             } else {
@@ -14,6 +13,25 @@ public class ResultView {
             }
         }
         System.out.println();
+    }
+
+    public static void announceWinners(List<Car> cars) {
+        int maxPosition = 0;
+
+        for (Car car : cars) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
+            }
+        }
+
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 
 }
