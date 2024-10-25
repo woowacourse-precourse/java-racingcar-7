980f2e0e7ca31031e4f3f90e02fc3cd44249a8c9
posite <su84053@gmail.com>
2024-10-25 22:46:14 +0900

    fix(RandomNumberGenerator): 무작위 숫자 생성 오류 수정
    0만 생성하던 오류를 수정하였습니다.

--- src/main/java/racingcar/RandomNumberGenerator.java
+++ src/main/java/racingcar/RandomNumberGenerator.java
@@ -8,5 +8,5 @@ public class RandomNumberGenerator {
     }
 
     private static final int MIN_NUMBER = 0;
-    private static final int MAX_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
 }
