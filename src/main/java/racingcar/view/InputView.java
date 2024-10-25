6031e6a50862325fea8031f6e015b1e3c1a6491e
OSEUK <tpdnrzmzm3@naver.com>
2024-10-25 10:16:59 +0900

    feat(view) : 사용자 입력 클래스
    입력과 함께 검증을 진행함. 검증 과정에서 InputException이 반환되면 IllegalArgument를 반환하도록 함.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,16 +1,31 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.exception.InputException;
+import racingcar.validator.InputValidator;
+
+import java.util.List;
 
 public class InputView{
-    public String InputNames(){
+    public List<String> InputNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+        String names = Console.readLine();
+        try{
+            return InputValidator.parseNames(names);
+        } catch (InputException e){
+            throw new IllegalArgumentException();
+        }
     }
 
-    public String InputTurns() {
+    public int InputTurns() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        return Console.readLine();
+        String numberString = Console.readLine();
+        try{
+            return InputValidator.parseTurns(numberString);
+        } catch(InputException e){
+            throw new IllegalArgumentException();
+        }
+
     }
 
 
