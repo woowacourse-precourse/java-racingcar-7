344ad93427376b827d3fbf172feec527531f1b3f
SeungHui0503 <ridia1233@naver.com>
2024-10-26 05:22:06 +0900

    feat: 기본적인 자동차 이름 입력 기능 구현
    - Console.readLine()을 사용하여 쉼표로 구분된 자동차 이름 입력 기능 구현
    - 이름 글자수 제한 등 예외처리는 아직 적용하지 않음

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> carNames = new ArrayList<>();
+        carNames = getCarNames();
+    }
+
+    private static List<String>getCarNames(){
+        String input = Console.readLine();
+        return Arrays.asList(input.split(","));
     }
 }
