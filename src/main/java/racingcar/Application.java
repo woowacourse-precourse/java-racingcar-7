8d563b6a4e2001b368582e9f95ff9cbc087e437c
soobin <a01056239778@gmail.com>
2024-10-25 23:56:43 +0900

    feat: Create move car feature
    - 경주할 자동차 이름별 이동 거리 map 생성
    - 자동차 수만큼 0에서 9 사이 무작위 값 생성
    - 무작위 값 4이상일 경우 전진인지 판단
    - 전진시 해당 자동차의 이동 거리 수정
    - 시도할 횟수만큼 반복해서 자동차 이동 기능 수행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,8 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-
-
         RacingGame racingGame = new RacingGame();
         racingGame.run();
     }
