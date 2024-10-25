524fc2c11f69f116acbdf770163f990b89f7ae4e
yeo-li <81564503+yeo-li@users.noreply.github.com>
2024-10-25 16:49:21 +0900

    feat(Application): 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 여기서 사용되지 않는 코드는 필요 없는 코드가 아닐까?
+        // 절차지향으로 TDD 한 뒤에 리팩토링 하면서 함수 쪼개고 TDD 하기
+
+        // 입력
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String repeatCount = Console.readLine();
+
     }
 }
