9e799d141ec0d993e43f2ccbeeb2128657088f24
JS <zks145@naver.com>
2024-10-25 14:47:57 +0900

    chore (validation): 자동차 이름 null or empty 체크 메서드 이름 변경

--- src/main/java/racingcar/validation/CarValidation.java
+++ src/main/java/racingcar/validation/CarValidation.java
@@ -13,40 +13,39 @@ public class CarValidation {
     private static final Integer MAX_LENGTH = 5;
     private static final Pattern REGEX = Pattern.compile("[!@#$%^&*]");
 
-    public static void carValid(List<Car> carList, String carName) {
-        checkNullOrEmpty(carName);
+    public static void carValid(final List<Car> carList, final String carName) {
+        checkNullOrEmptyCarNames(carName);
         checkOverLength(carName);
         checkRegex(carName);
         checkSameCarName(carList, carName);
     }
 
-    private static void checkNullOrEmpty(String carName) {
+    private static void checkNullOrEmptyCarNames(final String carName) {
         if (carName == null || carName.isEmpty()) {
             throw new IllegalArgumentException(NULL_EMPTY_FOUND.getMessage());
         }
     }
 
-    private static void checkOverLength(String carName) {
+    private static void checkOverLength(final String carName) {
         if (carName.length() > MAX_LENGTH) {
             throw new IllegalArgumentException(OVER_LENGTH_FOUND.getMessage());
         }
     }
 
-    private static void checkRegex(String carName) {
+    private static void checkRegex(final String carName) {
         Matcher matcher = REGEX.matcher(carName);
         if (matcher.find()) {
             throw new IllegalArgumentException(REGEX_FOUND.getMessage());
         }
     }
 
-    private static void checkSameCarName(List<Car> carList, String carName) {
+    private static void checkSameCarName(final List<Car> carList, final String carName) {
         for (Car car : carList) {
             if (car.carName.equals(carName)) {
                 throw new IllegalArgumentException(SAME_NAME_FOUND.getMessage());
             }
         }
     }
-
 
 
 }
