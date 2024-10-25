b4f468eb5ababb191b5f481823247a59cb57ed29
thoi2 <kinggun1505@naver.com>
2024-10-25 17:30:10 +0900

    Refactor camp 제공 라이브러리 Console로 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,26 +1,24 @@
 package racingcar;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 import java.util.StringTokenizer;
 import java.util.Map;
 import java.util.HashMap;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Scanner scanner = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String inputName = scanner.nextLine();
+        String inputName = Console.readLine();
         playerNameSplit(inputName);
 
     }
-    public static Map playerNameSplit(String inputName){
-        StringTokenizer tokenizer = new StringTokenizer(inputName,",");
-        Map<String, Integer> player = new HashMap<>();
-        while (tokenizer.hasMoreTokens()){
-            String token =tokenizer.nextToken();
-            player.put(token,0);
-        }
-        return player;
-    }
 
+    public static void playerNameSplit(String inputName) {
+        StringTokenizer tokenizer = new StringTokenizer(inputName, ",");
+        Map<String, Integer> player = new HashMap<>();
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            player.put(token, 0);
+        }
+    }
 }
