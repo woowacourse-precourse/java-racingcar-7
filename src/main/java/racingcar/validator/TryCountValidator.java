f6415ab23813516c249d6b22146e06d29f05eb68
StoneCAU <seokwoo1999@naver.com>
2024-10-26 00:37:17 +0900

    feat(TryCountValidtor): 시도횟수에 대한 검증 로직 추가
    - 1이상의 숫자만 입력 가능
    - validateTryCount 메서드를 통해 통합 검증

--- src/main/java/racingcar/validator/TryCountValidator.java
+++ src/main/java/racingcar/validator/TryCountValidator.java
@@ -2,10 +2,24 @@ package racingcar.validator;
 
 public class TryCountValidator {
     public static void validateTryCount(String input) {
+        validateOnlyDigits(input);
+        validatePositive(input);
+        validateNotOverflow(input);
+    }
+
+    private static void validateOnlyDigits(String input) {
         if (!containsOnlyDigits(input)) {
             throw new IllegalArgumentException("[ERROR] 시도횟수는 숫자로만 입력되어야 합니다.");
         }
+    }
 
+    private static void validatePositive(String input) {
+        if (isPositive(input)) {
+            throw new IllegalArgumentException("[ERROR] 1이상의 숫자만 입력 가능합니다.");
+        }
+    }
+
+    private static void validateNotOverflow(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
@@ -15,5 +29,11 @@ public class TryCountValidator {
 
     private static boolean containsOnlyDigits(String input) {
         return input.chars().allMatch(Character::isDigit);
+    }
+
+    private static boolean isPositive(String input) {
+        int tryCount = Integer.parseInt(input);
+
+        return tryCount > 0;
     }
 }
