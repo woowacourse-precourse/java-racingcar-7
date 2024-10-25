0d87a08c689b92f26b44d13c5fbb04f6d48d207c
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 15:41:06 +0900

    feat: 자동차 이름 길이를 검증하는 기능을 추가한다

--- src/main/java/racingcar/validator/NameValidiator.java
+++ src/main/java/racingcar/validator/NameValidiator.java
@@ -1,11 +1,21 @@
 package racingcar.validator;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class NameValidiator {
 
+    private static final int NAME_MAX_LENGTH = 5;
 //    private static final String CAR_LIST_PATTERN = "^([a-zA-Z0-9가-힣])(,[a-zA-Z0-9가-힣])*$";
     private static final String CAR_LIST_PATTERN = "^[a-zA-Z0-9가-힣]+[a-zA-Z0-9가-힣" + "," + "]*[a-zA-Z0-9가-힣]";
+
+    public void nameLength(List<String> carsList) {
+        for (String car : carsList) {
+            if (car.isEmpty() || car.length() > NAME_MAX_LENGTH) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 
     public void rightNameList(String cars) {
         if (!Pattern.compile(CAR_LIST_PATTERN).matcher(cars).matches()) {
