3ce21c952be900349abe92d43ac5c5305f03b5e8
Chan531 <chan657312@gmail.com>
2024-10-25 15:51:20 +0900

    feat(CarName): 자동차 이름 반환

--- src/main/java/racingcar/model/CarName.java
+++ src/main/java/racingcar/model/CarName.java
@@ -11,6 +11,10 @@ public class CarName {
 		this.name = name;
 	}
 
+	public String getName() {
+		return name;
+	}
+
 	private void checkNameLength(String name) {
 		if (name.length() > CAR_NAME_MAX_LENGTH_LIMIT) {
 			throw new IllegalArgumentException();
