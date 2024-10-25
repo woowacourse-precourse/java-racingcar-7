d9902059df529c378fe18197361ef021adf22022
JEANS <songkc5671@naver.com>
2024-10-25 16:34:11 +0900

    feat(model) : 경주 로직 추가.
    1. startRace() : 이름과 경주횟수를 저장 후 경주 준비를 함. 끝난 후에 최종 후보자까지  sb에 저장.
    
    2. race() : path[] 배열을 사용하여 이동횟수를 저장, 이동횟수를 토대로 경기 진행 상황을 sb에 결괏값 저장.
    
    3. getName() : "," 단위로 잘라서 list에 이름 저장. 이름 길이가 5를 초과하면 예외 발생.
    
    4. validate...() : 각종 기능 명세서에 있는 검증.

--- src/main/java/racingcar/RacingCarModel.java
+++ src/main/java/racingcar/RacingCarModel.java
@@ -1,4 +1,97 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import racingcar.raceDto.RacingRequest;
+
 public class RacingCarModel {
+
+    static StringBuilder sb;
+
+    public void startRace(RacingRequest request) {
+        /* 객체, 이름, 숫자 존재여부 판단 */
+        if(!validateRacingInfo(request)) {
+            throw new IllegalArgumentException("입력값을 입력해주세요.");
+        }
+        /* sb, 이름, 게임횟수 초기화 */
+        sb = new StringBuilder();
+        String name = request.name();
+        int loop = request.times();
+
+        ArrayList<String> nameList = new ArrayList<>();
+        getName(nameList, name);
+
+        int[] res = race(nameList, loop);
+        int max = 0;
+        /* 우승자 찾기 */
+        for (int re : res) {
+            max = Math.max(max, re);
+        }
+        sb.append("최종 우승자").append(" ").append(":").append(" ");
+
+        /* 우승자 출력 */
+        boolean isFirst = true;
+        for(int i = 0; i < res.length; i++) {
+            if(max == res[i]) {
+                if (!isFirst) {
+                    sb.append(", ");
+                }
+                sb.append(nameList.get(i));
+                isFirst = false;
+            }
+        }
+
+
+        System.out.println(sb);
+    }
+
+    private int[] race(ArrayList<String> list, int loop) {
+
+        /* 몇칸 전진했는지 나타내는 배열 */
+        int[] path = new int[list.size()];
+
+        while(loop-->0) {
+            /* idx == 이름 색인 */
+            for(int idx = 0; idx < list.size(); idx++) {
+                /* 0-9 난수 생성 후 4 이상시 전진 */
+                int rNum = Randoms.pickNumberInRange(0, 9);
+                if(rNum >= 4) {
+                    path[idx]++;
+                }
+
+                sb.append(list.get(idx)).append(" ").append(":").append(" ");
+                sb.append("-".repeat(Math.max(0, path[idx])));
+                sb.append("\n");
+            }
+            sb.append("\n");
+        }
+
+        return path;
+    }
+
+    private void getName(ArrayList<String> list, String s) {
+
+        StringTokenizer st = new StringTokenizer(s, ",");
+        while(st.hasMoreTokens()) {
+            String cur = st.nextToken();
+            list.add(cur);
+        }
+
+        for(String v : list) {
+            if(!validateNameLength(v)) {
+                throw new IllegalArgumentException("이름길이 5이하로 작성해주세요.");
+            }
+        }
+    }
+
+    private boolean validateNameLength(String name) {
+        /* 이름길이가 5이하 인지 판단 */
+        return name.length() <= 5;
+    }
+
+    private boolean validateRacingInfo(RacingRequest request) {
+        /* 객체, 이름, 숫자 존재여부 판단 */
+        return request != null && !request.name().isEmpty() && request.times() != 0;
+    }
 }
