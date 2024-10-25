001e2f4949d2d61a6b3da9027fe95afd06231133
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 18:34:28 +0900

    feat: 시도횟수 입력값이 양수인지 검증하는 기능을 추가한다

--- src/main/java/racingcar/validator/TryCountValidiator.java
+++ src/main/java/racingcar/validator/TryCountValidiator.java
@@ -1,4 +1,10 @@
 package racingcar.validator;
 
 public class TryCountValidiator {
+
+    public void positiveNumber(int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
