ff43e5c88433045b300b2215b6ded4c7e34c6ca5
ChrisP-00 <jspark4151@gmail.com>
2024-10-25 18:07:47 +0900

    feat(viewer): add viewer for input view

---
+++ src/main/java/racingcar/Viewer.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class Viewer {
+    public void InputCarNameView() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)");
+    }
+
+    public void InputAttemptCountView() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+}
