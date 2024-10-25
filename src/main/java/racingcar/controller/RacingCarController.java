87072f38aee49cc63c448e4b79085bfd0fc79d12
unh6unh6 <ruddbs3308@naver.com>
2024-10-25 19:52:09 +0900

    feat(controller): OutputView를 통해 우승자를 출력
    - CarDto list를 Car name이 담긴 String list로 변환하는 메소드 추가

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,19 +1,23 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import racingcar.dto.CarDto;
 import racingcar.model.Cars;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 import racingcar.view.ProgressView;
 
 public class RacingCarController {
     private final InputView inputView;
     private final ProgressView progressView;
+    private final OutputView outputView;
     private final Cars cars;
 
-    public RacingCarController(InputView inputView, ProgressView progressView, Cars cars) {
+    public RacingCarController(InputView inputView, ProgressView progressView, OutputView outputView, Cars cars) {
         this.inputView = inputView;
         this.progressView = progressView;
+        this.outputView = outputView;
         this.cars = cars;
     }
 
@@ -21,6 +25,7 @@ public class RacingCarController {
         initializeCars();
         final long racingRound = settingRacingRound();
         startRace(racingRound);
+        finalizeRace();
     }
 
     private void initializeCars() {
@@ -52,7 +57,16 @@ public class RacingCarController {
 
     private void finalizeRace() {
         List<CarDto> winnerList = cars.calculateFarthestCars();
-
+        List<String> winnerNameList = getCarNameList(winnerList);
+        outputView.printWinnerMessage(winnerNameList);
     }
 
+    private List<String> getCarNameList(List<CarDto> carDtoList) {
+        List<String> carNameList = new ArrayList<>();
+        for (CarDto carDto : carDtoList) {
+            String carName = carDto.name();
+            carNameList.add(carName);
+        }
+        return carNameList;
+    }
 }
