f3d2b4cb95fc505e86c39cfda229b22c235cca00
sc@t0a14 <gusdn6671@gmail.com>
2024-10-25 20:51:02 +0900

    feat(OutputView): 차들의 실행 결과를 출력한다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,12 +1,24 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.model.Car;
+
 public class OutputView {
 
-    public static void printCarNamesPrompt(){
+    public static void printCarNamesPrompt() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public static void printAttemptCountPrompt(){
+    public static void printAttemptCountPrompt() {
         System.out.println("시도할 횟수는 몇 회인가요?");
     }
+
+    public static void printCarMove(List<Car> carNames) {
+        for (Car carName : carNames) {
+            System.out.print(carName.getName() + " : ");
+            System.out.println("-".repeat(carName.getPosition()));
+        }
+    }
+
+
 }
