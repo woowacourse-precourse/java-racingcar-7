c81dc953dadf432f4a6642c338cf14b321bf411c
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-25 18:34:38 +0900

    refactor(RandomForwardGenerator): 랜덤으로 숫자를 생성하는 클래스명을 명확하게 변경

--- src/main/java/racingcar/utils/RandomForwardGenerator.java
+++ src/main/java/racingcar/utils/RandomForwardGenerator.java
@@ -2,7 +2,7 @@ package racingcar.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomForwardCount {
+public class RandomForwardGenerator {
 
     public static int randomSingleDigitNumber() {
         return Randoms.pickNumberInRange(0, 9);
