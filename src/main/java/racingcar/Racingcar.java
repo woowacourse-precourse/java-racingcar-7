1c46d381bdc45f1f755037145d2c7b66d9f5886a
DESKTOP-GRC68NA\foski <foskingson@naver.com>
2024-10-25 21:49:47 +0900

    feat: 레이싱카 객체 생성 및 전진 함수 구현

---
+++ src/main/java/racingcar/Racingcar.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import java.util.HashMap;
+
+
+
+public class Racingcar {    
+    private String name;
+    private int moveCount;
+
+    public Racingcar(String name) { 
+        this.name = name;
+        this.moveCount = 0;
+    }
+
+    public static void moveRacingcar(Racingcar racingcar){  // 전진조건에 맞아 전진할 경우 이동거리 +1
+        racingcar.moveCount++;
+    }
+}
