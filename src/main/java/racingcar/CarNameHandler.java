cdb866a93284028dbfb8f6589b11a295f36c8a4f
승일 <seoung1515@naver.com>
2024-10-25 21:02:27 +0900

    feat: 우승자 반환 기능 추가

--- src/main/java/racingcar/CarNameHandler.java
+++ src/main/java/racingcar/CarNameHandler.java
@@ -1,23 +1,27 @@
 package racingcar;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class CarNameHandler {
 
-    // ','로 차 이름 분리 및 공백 제거
     private List<String> splitCarNames(String input) {
         return Arrays.stream(input.split(","))
                 .map(String::trim)
+                .filter(name -> name.length() <= 5)
                 .collect(Collectors.toList());
     }
 
-    // Car 객체 생성 및 차 이름 저장
-    public CarData storeCar(String input) {
+    public RacingCars storeCar(String input) {
         List<String> carNames = splitCarNames(input);
-        CarData carData = new CarData();
-        carData.setCarNames(carNames);
-        return carData;
+        RacingCars racingCars = new RacingCars();
+        racingCars.addCars(carNames); // 자동차 이름을 통해 Car 객체 생성 및 추가
+        return racingCars;
+    }
+
+    public void printWinCarNames(RacingCars racingCars) {
+        List<String> winners = racingCars.getWinners();
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
