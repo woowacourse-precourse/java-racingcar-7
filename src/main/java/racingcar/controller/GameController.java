900a2b28e9793d6055cee0cbf50e41ad7fec0ccb
IM-GYURI <kgulr0517@gmail.com>
2024-10-25 22:34:25 +0900

    feat : revise GameController
    - RoundNumberValidator 추가
    - roundNumber 변수에 검증된 시도 횟수가 설정되도록 함
    - 역할에 따른 함수 분리
      - getRoundNumber() : 시도 횟수를 입력받아 검증한 후 반환
      - printResults() : 결과 출력 (OutputView 호출)

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -6,6 +6,7 @@ import racingcar.dto.FinalResultDto;
 import racingcar.dto.RoundResultDto;
 import racingcar.model.CarMaker;
 import racingcar.model.RacingGame;
+import racingcar.model.RoundNumberValidator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -15,26 +16,38 @@ public class GameController {
     private final OutputView outputView;
     private final CarMaker carMaker;
     private final RacingGame racingGame;
+    private final RoundNumberValidator roundNumberValidator;
 
-    public GameController(InputView inputView, OutputView outputView, CarMaker carMaker, RacingGame racingGame) {
+    public GameController(InputView inputView, OutputView outputView, CarMaker carMaker, RacingGame racingGame,
+                          RoundNumberValidator roundNumberValidator) {
         this.inputView = inputView;
         this.outputView = outputView;
         this.carMaker = carMaker;
         this.racingGame = racingGame;
+        this.roundNumberValidator = roundNumberValidator;
     }
 
     public void start() {
         String carNames = inputView.requestCarNamesInput();
-        int roundNumber = Integer.parseInt(inputView.requestRoundNumberInput());
-        List<Car> carList = carMaker.makeCars(carNames);
+        int roundNumber = getRoundNumber();
 
+        List<Car> carList = carMaker.makeCars(carNames);
         FinalResultDto finalResultDto = racingGame.playRacingGame(roundNumber, carList);
 
+        printResults(finalResultDto, roundNumber);
+    }
+
+    private int getRoundNumber() {
+        int roundNumber = Integer.parseInt(inputView.requestRoundNumberInput());
+        roundNumberValidator.validateRoundNumber(roundNumber);
+        return roundNumber;
+    }
+
+    private void printResults(FinalResultDto finalResultDto, int roundNumber) {
         for (int i = 0; i < roundNumber; i++) {
             RoundResultDto roundResultDto = finalResultDto.roundResultList().get(i);
             outputView.printRoundResult(roundResultDto.roundNumber(), roundResultDto.carList());
         }
-
         outputView.printFinalResult(finalResultDto.winnerList());
     }
 }
