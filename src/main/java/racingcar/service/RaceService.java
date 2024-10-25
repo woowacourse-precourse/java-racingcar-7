5fa8718c51265e4ff83d02927abfe1330e619a00
jhan0121 <jhan0121@gmail.com>
2024-10-25 21:53:22 +0900

    refactor(race): Car 객체 기반 메서드로 리팩터링

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -4,35 +4,37 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
+import racingcar.model.Car;
 import racingcar.model.RandomRange;
 
 public class RaceService {
 
-    public void race(List<Integer> progress) {
-        int size = progress.size();
-        for (int i = 0; i < size; i++) {
+    public void race(List<Car> cars) {
+        for (Car car : cars) {
             int randomNumber = Randoms.pickNumberInRange(RandomRange.MIN.getNumber(), RandomRange.MAX.getNumber());
-            move(progress, randomNumber, i);
+            move(car, randomNumber);
         }
     }
 
-    public static void move(List<Integer> progress, int randomNumber, int index) {
+    public static void move(Car car, int randomNumber) {
         if (randomNumber >= 4) {
-            int currentProgress = progress.get(index);
-            progress.set(index, currentProgress + 1);
+            int currentProgress = car.getProgress();
+            car.setProgress(currentProgress + 1);
         }
     }
 
-    public void findWinner(List<String> players, List<Integer> progress, List<String> winner) {
-        int maxValue = findMaxValue(progress);
-        for (int i = 0; i < players.size(); i++) {
-            if (progress.get(i) == maxValue) {
-                winner.add(players.get(i));
+    public void findWinner(List<Car> cars, List<String> winner) {
+        int maxValue = findMaxValue(cars);
+        for (Car car : cars) {
+            if (car.getProgress() == maxValue) {
+                winner.add(car.getName());
             }
         }
     }
 
-    private int findMaxValue(List<Integer> progress) {
-        return progress.stream().max(Comparator.comparing(i -> i)).orElseThrow(NoSuchElementException::new);
+    private int findMaxValue(List<Car> progress) {
+        return progress.stream().max(Comparator.comparingInt(Car::getProgress))
+                .orElseThrow(NoSuchElementException::new)
+                .getProgress();
     }
 }
