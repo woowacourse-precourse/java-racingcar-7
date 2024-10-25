747c43cb46fd14c73459e0bf2518651091e9872e
jklee3409 <jklee3409@khu.ac.kr>
2024-10-25 20:19:24 +0900

    feat(resultview) : printWinners 메서드 구현

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -15,4 +15,10 @@ public class ResultView {
         }
         System.out.println();
     }
+
+    public void printWinners(List<Car> winners) {
+        String winnerNames = String.join(", ",
+                winners.stream().map(Car::getName).toList());
+        System.out.println("최종 우승자 : " + winnerNames);
+    }
 }
