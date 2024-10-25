4c57ed27c9b1fc8ee405f6fbac512a749189cd8e
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-10-26 01:23:42 +0900

    feat(Output): 자동차 이름과 거리를 출력하는 메소드 추가

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -4,4 +4,8 @@ public class Output {
     public static void printWinner() {
         System.out.println("최종 우승자 : ");
     }
+
+    public static void printDistance(Car car) {
+        System.out.println(car.getName() + " : " + car.getDist());
+    }
 }
