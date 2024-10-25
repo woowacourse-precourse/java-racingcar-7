c5884b63cd64de7783fc512164fbb1577d5dfc43
chaeyeon0130 <k84292330@gmail.com>
2024-10-25 18:21:08 +0900

    feat(OutputHandler): 최종 우승자 출력 문구 수정

--- src/main/java/racingcar/OutputHandler.java
+++ src/main/java/racingcar/OutputHandler.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class OutputHandler {
     public static void displayWinners(List<String> winners) {
-        System.out.print("최종 우승자 :");
+        System.out.print("최종 우승자 : ");
         System.out.println(String.join(", ", winners));
     }
 }
