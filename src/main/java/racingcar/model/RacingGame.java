0baf3c6135e35df785afed931f012b83838cf8c7
dradnats1012 <herjebi1012@gmail.com>
2024-10-25 20:51:55 +0900

    feat: 자동차 경주 클래스 완성

---
+++ src/main/java/racingcar/model/RacingGame.java
@@ -0,0 +1,43 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RacingGame {
+
+    private final List<Car> cars;
+    private final int trial;
+    private List<Map<String, Integer>> allResult = new ArrayList<>();
+
+    public RacingGame(List<Car> cars, int trial){
+        this.cars = cars;
+        this.trial = trial;
+        moveCarsByTrial();
+    }
+
+    private void moveCarsByTrial(){
+        for(int i = 0; i < trial; i++){
+            allResult.add(moveCars());
+        }
+    }
+
+    private Map<String, Integer> moveCars(){
+        Map<String, Integer> roundResult = new LinkedHashMap<>();
+
+        for(Car car : cars){
+            car.move();
+            roundResult.put(car.getName(), car.getPosition());
+        }
+        return roundResult;
+    }
+
+    public List<Map<String, Integer>> getAllResult(){
+        return allResult;
+    }
+
+    public List<Car> getCars(){
+        return cars;
+    }
+}
