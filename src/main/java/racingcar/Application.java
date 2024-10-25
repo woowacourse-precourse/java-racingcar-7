09a7a604cf6a5883d1dcad38579b5e11cadf99f6
moon-akdong <problemy0913@gmail.com>
2024-10-26 03:38:22 +0900

    Delete print for debug and Renaming parameter in splitCarsName

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -28,22 +28,19 @@ public class Application {
         }
     }
 
-    public static String[] splitCarsName(String test) {
+    public static String[] splitCarsName(String inputStr) {
         // 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
-        // 입력 예시 : pobi,woni,jun
-        String[] carsName = test.split(",");
+        String[] carsName = inputStr.split(",");
         return carsName;
     }
 
     public static int canProgressWithRandomNum() {
         // 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
         int randomNum = Randoms.pickNumberInRange(0, 9);
-        System.out.println(randomNum);
         if (randomNum >= 4) {
             return 1;
         }
         return 0;
-        // System.out.println(randomNum);
     }
 
     public static void printCarNameProgress(String carName, int progressNum) {
