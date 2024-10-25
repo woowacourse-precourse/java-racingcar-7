f7b94f7e8b7c042851340769cdef78d44872e0b5
ppparkta <pppppark2@gmail.com>
2024-10-25 20:34:34 +0900

    feat(RacingGameController): RacingGameController 생성
    추가된 내용
    - RacingGameController 추가
    - model(factory), view(input/output) 의존성 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.racing.controller.RacingGameController;
+import racingcar.racing.model.RacingGameConcreteFactory;
+import racingcar.racing.view.InputView;
+import racingcar.racing.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        RacingGameConcreteFactory racingGameFactory = new RacingGameConcreteFactory();
+
+        RacingGameController racingGameController = new RacingGameController(inputView,
+                outputView, racingGameFactory);
     }
 }
