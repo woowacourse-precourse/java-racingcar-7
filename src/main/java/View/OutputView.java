79e10c5a8b4db08886e5642eedf78c8b88689a4c
sinji <tobeabestns@naver.com>
2024-10-25 17:40:34 +0900

    feat(OutputView): 출력 양식 생성

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,4 +1,51 @@
 package View;
 
+import domain.Car;
+import domain.Cars;
+import domain.FindWinningCar;
+
+import java.util.List;
+
 public class OutputView {
+
+    // 실행 결과
+    public static void resultOfCarRacing() {
+        System.out.println("실행 결과");
+    }
+
+    // '-' 출력
+    private static String getCarsLocation(final int carLocation) {
+        final StringBuilder carsLocation = new StringBuilder();
+        for (int i = 0; i < carLocation; i++) {
+            carsLocation.append("-");
+        }
+        return carsLocation.toString();
+    }
+
+    // 차수별 실행 결과
+    //pobi : -
+    //woni :
+    public static void resultOfEveryRace(Cars cars, int inputAttemptNumber) {
+        for (int i = 0; i < inputAttemptNumber; i++) {
+            raceOnce(cars);
+            for (Car car : cars.getCars()) {
+                System.out.println(String.format("%s : %s", car.getCarName(), getCarsLocation(car.getCarLocation())));
+            }
+            System.out.println();
+        }
+    }
+
+    private static void raceOnce(Cars cars) {
+        for (Car car : cars.getCars()) {
+            car.move();
+        }
+    }
+
+    //최종 우승자 : pobi, jun
+    public static void resultOfFinalWinner() {
+        FindWinningCar findWinningCar = new FindWinningCar();
+        List<String> winningCars = findWinningCar.getWinningCars();
+
+        System.out.println("최종 우승자 : " + winningCars);
+    }
 }
