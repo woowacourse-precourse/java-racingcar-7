f2a47ba6b46624aa67e6e7d8628e4e12eb858b33
jiffyin7 <jiffyin7@gmail.com>
2024-10-25 20:46:15 +0900

    feat(cars) : List<Car>와 PriorityQueue<Car> 일급 컬렉션 구현
    - `Cars`인터페이스와 구현체
    - `findCarsByPosition`, `findMaxPosition`와 `getWinners` 메서드

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,13 @@
+package racingcar.model;
+
+import java.util.List;
+
+/**
+ * @author : jiffyin7@gmail.com
+ * @since : 24. 10. 25.
+ */
+public interface Cars {
+  List<Car> findCarsByPosition(int position);
+  int findMaxPosition();
+  List<Car> getWinners ();
+}
---
+++ src/main/java/racingcar/model/CarsList.java
@@ -0,0 +1,36 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : jiffyin7@gmail.com
+ * @since : 24. 10. 25.
+ */
+public class CarsList implements Cars{
+  private final List<Car> cars;
+
+  public CarsList(List<Car> cars) {
+    this.cars = new ArrayList<>(cars);
+  }
+
+  @Override
+  public List<Car> findCarsByPosition(int position){
+    return cars.stream()
+        .filter(car -> car.getPosition() == position)
+        .toList();
+  }
+
+  @Override
+  public int findMaxPosition(){
+    return cars.stream()
+        .mapToInt(Car::getPosition)
+        .max()
+        .orElse(0);
+  }
+
+  @Override
+  public List<Car> getWinners () {
+    return findCarsByPosition(findMaxPosition());
+  }
+}
