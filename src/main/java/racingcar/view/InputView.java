60680d6ed42273d02c4eba8441c35ce60169fe60
JeongHyeon Jo <wjdgusdlekd3@yonsei.ac.kr>
2024-10-25 17:17:42 +0900

    feat(view): InputView, OutputView feature
    - 콘솔로 들어오는 인풋 처리 완료
    - OutputView 초기 인풋에 관한 프로세스만 처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,40 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class InputView {
+    private static final String INPUT_DELIMITER_COMMA = ",";
+    private final ArrayList<String> inputDataList;
+    String inputData;
+
+    public InputView() {
+        inputDataList = new ArrayList<>();
+    }
+
+    public String readInput() {
+        inputData = Console.readLine();
+        return inputData;
+    }
+
+    public void parserInputString(String readCarString) {
+        if (readCarString != null && !readCarString.isEmpty()) {
+            String[] parsedString = readCarString.split(INPUT_DELIMITER_COMMA);
+            trimFirstElement(parsedString);
+            inputDataList.addAll(Arrays.asList(parsedString));
+        }
+    }
+
+    private void trimFirstElement(String[] parsedString) {
+        for (int i = 0; i < parsedString.length; i++) {
+            parsedString[i] = parsedString[i].trim();
+        }
+    }
+
+    public ArrayList<String> getParserInputString() {
+        return inputDataList;
+    }
+
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,24 @@
 package racingcar.view;
 
 public class OutputView {
+    private static final String PRINT_READ_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String PRINT_READ_TOTAL_TRY_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String PRINT_RACING_CAR_SUFFIX = " : ";
+
+    private void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    private void printCarName(String CarName) {
+        System.out.print(CarName + PRINT_RACING_CAR_SUFFIX);
+    }
+
+    public void printReadCarNameMessage() {
+        printMessage(PRINT_READ_CAR_NAME_MESSAGE);
+    }
+
+    public void printReadTotalTryMessage() {
+        printMessage(PRINT_READ_TOTAL_TRY_MESSAGE);
+    }
+
 }
