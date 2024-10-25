59c2a9fcba6172be2b1ebe7a70161991f24427bd
FhRh <yiyop@naver.com>
2024-10-25 14:27:22 +0900

    feat(AppConfig) : Import inputView

--- src/main/java/racingcar/config/AppConfig.java
+++ src/main/java/racingcar/config/AppConfig.java
@@ -3,6 +3,7 @@ package racingcar.config;
 import racingcar.controller.CarGameController;
 import racingcar.service.CarCreateService;
 import racingcar.service.CarGameService;
+import racingcar.view.Inputview;
 import racingcar.view.OutputView;
 
 public class AppConfig {
@@ -10,11 +11,11 @@ public class AppConfig {
     private final CarGameController carGameController;
     private final CarGameService carGameService;
     private final CarCreateService carCreateService;
-    private final InputView inputView;
+    private final Inputview inputView;
     private final OutputView outputView;
 
     public AppConfig() {
-        this.inputView = new InputView();
+        this.inputView = new Inputview();
         this.outputView = new OutputView();
         this.carCreateService = new CarCreateService();
         this.carGameService = new CarGameService();
