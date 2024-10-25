7db66f7791321eea9629b286be40e2091cbd3c9a
Chan531 <chan657312@gmail.com>
2024-10-25 17:13:17 +0900

    feat(WinnerResultGenerator): 공동 우승자 구분자 상수 추가

--- src/main/java/racingcar/model/WinnerResultGenerator.java
+++ src/main/java/racingcar/model/WinnerResultGenerator.java
@@ -1,8 +1,7 @@
 package racingcar.model;
 
-import java.util.List;
-
 public class WinnerResultGenerator {
 
 	private static final int SINGLE_WINNER_COUNT = 1;
+	private static final String SEPARATOR = ", ";
 }
