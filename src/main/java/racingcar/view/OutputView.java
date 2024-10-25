872a75f6e46af9909b18be1b26f082cc9f395ea9
sung <salamander011017@gmail.com>
2024-10-25 20:50:47 +0900

    feat : 현재 상황을 출력하는 메소드 구현
    -printRoundResult()를 활용하여 현재 진행상황 구함

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,8 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.model.Car;
+
 public class OutputView {
     public void printGameStart() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -7,5 +10,13 @@ public class OutputView {
 
     public void printAttemptPrompt() {
         System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+    public void printRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.printf("%s : %s%n",
+                    car.getCarName(), "-".repeat(car.getPosition()));
+        }
+        System.out.println();
     }
 }
