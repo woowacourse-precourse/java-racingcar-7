78aa1714105eeeb033301cfe0dd63ebbfb7bc7e7
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2024-10-25 13:29:07 +0900

    refactor : 줄바꿈 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,6 +3,7 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+
     private static final String INPUT_RACE_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_MOVE_COUNT = "시도할 횟수는 몇 회인가요?";
 
