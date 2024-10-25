4c057664273724e5d589bd04eeda872c03f82846
Hyungjung Kim <gudwnd1113@gmail.com>
2024-10-25 18:16:22 +0900

    test(UtilsTest): build enterRound-related test code
    Utils.enterRound() 함수의 입력값에 따른 동작을 검증하는 테스트코드 작성.

--- src/test/java/racingcar/UtilsTest.java
+++ src/test/java/racingcar/UtilsTest.java
@@ -1,7 +1,9 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static racingcar.Utils.enterCarList;
+import static racingcar.Utils.enterRound;
 
 import java.io.ByteArrayInputStream;
 import org.junit.jupiter.api.Test;
@@ -19,5 +21,25 @@ public class UtilsTest {
         assertThat(enterCarList())
                 .contains("pobi")
                 .contains("woni");
+    }
+
+    @Test
+    void enterRound_test() {
+        dummyInput("3");
+        assertThat(enterRound()).isEqualTo(3);
+    }
+
+    @Test
+    void enterRound_negative_exception_test() {
+        dummyInput("-1");
+        assertThatThrownBy(() -> enterRound())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void enterRound_invalid_exception_test() {
+        dummyInput("aa");
+        assertThatThrownBy(() -> enterRound())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
