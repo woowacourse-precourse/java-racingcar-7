1153a465bac03ac18f495f2e3996f5393b273bfb
Hyso <hsk7953@gmail.com>
2024-10-26 02:01:50 +0900

    feat : OutputView 클래스 기능 구현 - 경주할 자동차 입력을 요구를 나타내는 메서드 - 시도할 횟수 입력을 요구를 나타내는 메서드 - 인스턴스 방지를 위한 private 생성자

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,16 @@
+package racingcar.view;
+
+public class OutputView {
+    private OutputView() {
+    }
+
+    public static void requestCarNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void requestTrialCount(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+
+}
