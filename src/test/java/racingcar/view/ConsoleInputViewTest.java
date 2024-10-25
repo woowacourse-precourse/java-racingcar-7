9c2ef3555e73f598b2a502792585bf79562d0a09
choiminu <alsdn4590@gmail.com>
2024-10-26 01:11:14 +0900

    test(view): ConsoleInputViewTest 클래스에서 불필요한 테스트 제거
    사용자 입력 값 테스트에서 `사용자가 유효한 값을 입력하면 입력한 문자열을 반환한다` 테스트 메서드를 제거함.
    - 사용자 입력이 빈 문자열일 경우 예외가 발생하는 테스트만 유지하도록 수정.

--- src/test/java/racingcar/view/ConsoleInputViewTest.java
+++ src/test/java/racingcar/view/ConsoleInputViewTest.java
@@ -1,7 +1,5 @@
 package racingcar.view;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import org.assertj.core.api.Assertions;
@@ -11,21 +9,6 @@ import org.junit.jupiter.api.Test;
 class ConsoleInputViewTest {
 
     ConsoleInputView inputView = new ConsoleInputView();
-
-    @Test
-    @DisplayName("사용자가 유효한 값을 입력하면 입력한 문자열을 반환한다.")
-    public void 사용자_입력_기능() {
-        //given
-        String userInput = "povi,joon,minu";
-        InputStream in = new ByteArrayInputStream(userInput.getBytes());
-        System.setIn(in);
-
-        //when
-        String result = inputView.getValidatedUserInput();
-
-        //then
-        assertThat(result).isEqualTo(userInput);
-    }
 
     @Test
     @DisplayName("사용자가 빈 문자열을 입력하면 예외가 발생한다.")
