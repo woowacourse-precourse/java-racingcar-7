5f1582daba3c11b3cb04c537c5357bb76768e8bf
seahwanKim <kimkimseasea@gmail.com>
2024-10-25 21:18:19 +0900

    feat: 라운드 수를 입력받는 기능을 추가한다

--- src/main/java/racingcar/controller/InputController.java
+++ src/main/java/racingcar/controller/InputController.java
@@ -1,6 +1,7 @@
 package racingcar.controller;
 
 import racingcar.validator.CarNameValidator;
+import racingcar.validator.RoundValidator;
 import racingcar.view.InputView;
 
 import java.util.List;
@@ -12,15 +13,25 @@ public class InputController {
     public static List<String> setCars() {
         String cars = InputView.inputCar();
 
-        List<String> carsList = List.of(cars.split(DELIMITER));
+        List<String> carList = List.of(cars.split(DELIMITER));
 
         CarNameValidator check = new CarNameValidator();
 
         check.rightNamePattern(cars);
-        check.nameLength(carsList);
-        check.nameOverlap(carsList);
+        check.nameLength(carList);
+        check.nameOverlap(carList);
 
-        return carsList;
+        return carList;
     }
 
+    public static int setRound() {
+        String round = InputView.inputRound();
+
+        RoundValidator check = new RoundValidator();
+
+        int number = check.parseInt(round);
+        check.positiveNumber(number);
+
+        return number;
+    }
 }
