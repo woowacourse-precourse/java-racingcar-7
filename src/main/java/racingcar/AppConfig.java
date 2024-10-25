cc2050ad47ca9b0d57d9fda1aeed39d7d3d2d73d
onesay17 <onesay17@gmail.com>
2024-10-25 12:53:26 +0900

    refactor: 검증로직을 횟수와 자동차 이름으로 분리해서 의존성 주입 수정

--- src/main/java/racingcar/AppConfig.java
+++ src/main/java/racingcar/AppConfig.java
@@ -3,32 +3,38 @@ package racingcar;
 import racingcar.controller.RacingCarController;
 import racingcar.service.RacingCarService;
 import racingcar.service.RacingCarServiceImpl;
-import racingcar.validation.CarNameValidator;
-import racingcar.validation.NameValidator;
+import racingcar.validation.AttemptCountValidator;
+import racingcar.validation.AttemptCountValidatorImpl;
+import racingcar.validation.CarNamesValidator;
+import racingcar.validation.CarNamesValidatorImpl;
 import racingcar.view.InputView;
 import racingcar.view.InputViewImpl;
-import racingcar.view.OutPutView;
-import racingcar.view.OutPutViewImpl;
+import racingcar.view.OutputView;
+import racingcar.view.OutputViewImpl;
 
 public class AppConfig {
 
     public RacingCarController racingCarController() {
-        return new RacingCarController(racingCarService(), inputView(), outPutView());
+        return new RacingCarController(racingCarService(), inputView(), outPutView(), attemptCountValidator());
     }
 
     public RacingCarService racingCarService() {
-        return new RacingCarServiceImpl(nameValidator());
+        return new RacingCarServiceImpl(carNameValidator());
     }
 
-    public NameValidator nameValidator() {
-        return new CarNameValidator();
+    public CarNamesValidator carNameValidator() {
+        return new CarNamesValidatorImpl();
+    }
+
+    public AttemptCountValidator attemptCountValidator() {
+        return new AttemptCountValidatorImpl();
     }
 
     public InputView inputView() {
         return new InputViewImpl();
     }
 
-    public OutPutView outPutView() {
-        return new OutPutViewImpl();
+    public OutputView outPutView() {
+        return new OutputViewImpl();
     }
 }
