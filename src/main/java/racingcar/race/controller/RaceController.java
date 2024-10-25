9a74840a39cec1bf40bebaef0ccdbbb13f7edbb4
jieun <ju3391@naver.com>
2024-10-25 21:25:47 +0900

    feat(RaceController): 경주 컨트롤러 구현
    - 경주 시작 및 우승자 출력 기능 추가

--- src/main/java/racingcar/race/controller/RaceController.java
+++ src/main/java/racingcar/race/controller/RaceController.java
@@ -1,4 +1,27 @@
 package racingcar.race.controller;
 
+import java.util.List;
+import racingcar.car.model.Car;
+import racingcar.race.service.RaceService;
+import racingcar.view.Input;
+import racingcar.view.Output;
+
+
+// 경주 전체를 제어하는 컨트롤러
 public class RaceController {
+    private final RaceService raceService;
+    private final Input input;
+    private final Output output;
+
+    public RaceController(RaceService raceService, Input input, Output output) {
+        this.raceService = raceService;
+        this.input = input;
+        this.output = output;
+    }
+
+    public void startRace() {
+        raceService.startRace(input.getCarNames(), input.getAttemptCount());
+        List<Car> winners = raceService.getWinners();
+        output.printWinners(winners);
+    }
 }
