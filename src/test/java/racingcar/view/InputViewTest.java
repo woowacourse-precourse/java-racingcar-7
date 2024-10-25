7a5cf87acd4acc2d1ebc22ba4ff7f45fe6aa71e6
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-10-26 02:53:17 +0900

    refactor: 자동차 이름 입력 테스크코드 리팩토링
    여러 테스트케이스를 동일한 테스트코드에 적용시킬 때 중복되는 함수와 반복된 코드로 인해 유지보수성이 떨어짐. 따라서 @ParameterizedTest를 이용하여 해당 부분 해결

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,54 +1,69 @@
 package racingcar.view;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InputViewTest {
+    private static final String ERROR_MESSAGE_COUNT = "생성할 수 있는 자동차의 개수는 2개 이상 20이하 입니다.";
+    private static final String ERROR_MESSAGE_NAME_LENGTH = "이름은 1글자 이상, 5글자 이하 입니다.";
+    private static final String ERROR_MESSAGE_NAME_FORMAT = "이름은 숫자, 알파벳, 한글 조합만 가능합니다.";
+
     private final InputView inputView = new InputView();
 
-    private void checkValidationThrowsException(String testInput, String expectedMessage) {
-        IllegalArgumentException thrownException = assertThrows(IllegalArgumentException.class, () -> inputView.validateInput(testInput));
-        assertEquals(expectedMessage, thrownException.getMessage());
+    private static Stream<Arguments> provideCarNameCases(){
+        return Stream.of(
+                Arguments.of("abcd,abc"),
+                Arguments.of("ads,abc,dsd,123"),
+                Arguments.of("abcd,abc,2323"),
+                Arguments.of("ad,abc,12345"),
+                Arguments.of("ad,abc,김bc1")
+        );
     }
 
-    @Test
+    private static Stream<Arguments> provideCarNameExceptionCases(){
+        return Stream.of(
+                Arguments.of("abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc", ERROR_MESSAGE_COUNT),
+                Arguments.of("abc,abc", ERROR_MESSAGE_COUNT),
+                Arguments.of("abc", ERROR_MESSAGE_COUNT),
+                Arguments.of("", ERROR_MESSAGE_COUNT),
+                Arguments.of("abc,", ERROR_MESSAGE_COUNT),
+                Arguments.of("abcdef", ERROR_MESSAGE_COUNT),
+                Arguments.of("abc,abcdefg", ERROR_MESSAGE_NAME_LENGTH),
+                Arguments.of("abc,a bc", ERROR_MESSAGE_NAME_FORMAT),
+                Arguments.of("1###,1@,1\n234", ERROR_MESSAGE_NAME_FORMAT)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCarNameCases")
     @DisplayName("자동차 이름 입력 검증 테스트")
-    public void firstInputValidationTest(){
-        assertAll(
-                () -> assertTrue(inputView.validateInput("abcd,abc")),
-                () -> assertTrue(inputView.validateInput("ads,abc,dsd,123")),
-                () -> assertTrue(inputView.validateInput("abcd,abc,2323")),
-                () -> assertTrue(inputView.validateInput("ad,abc,12345")),
-                () -> assertTrue(inputView.validateInput("ad,abc,김bc1"))
-        );
+    public void validateCarNameTest(String test) {
+        assertTrue(inputView.validateCarName(test));
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("provideCarNameExceptionCases")
     @DisplayName("자동차 이름 입력 에러메시지 테스트")
-    public void firstInputValidationExceptionTest() {
-        String longTestCase = "abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc,abc";
-        assertAll(
-                () -> checkValidationThrowsException(longTestCase, "생성할 수 있는 자동차의 개수는 2개 이상 20이하 입니다."),
-                () -> checkValidationThrowsException("abc,abc", "생성할 수 있는 자동차의 개수는 2개 이상 20이하 입니다."),
-                () -> checkValidationThrowsException("abc", "생성할 수 있는 자동차의 개수는 2개 이상 20이하 입니다."),
-                () -> checkValidationThrowsException("", "생성할 수 있는 자동차의 개수는 2개 이상 20이하 입니다."),
-                () -> checkValidationThrowsException("abc,", "생성할 수 있는 자동차의 개수는 2개 이상 20이하 입니다."),
-                () -> checkValidationThrowsException("abcdef", "생성할 수 있는 자동차의 개수는 2개 이상 20이하 입니다."),
-                () -> checkValidationThrowsException("abc,abcdefg", "이름은 1글자 이상, 5글자 이하 입니다."),
-                () -> checkValidationThrowsException("abc,a bc", "이름은 숫자, 알파벳, 한글 조합만 가능합니다."),
-                () -> checkValidationThrowsException("1###,1@,1\n234", "이름은 숫자, 알파벳, 한글 조합만 가능합니다.")
-        );
+    public void validateCarNameExceptionTest(String test, String expected) {
+        IllegalArgumentException thrownException = assertThrows(IllegalArgumentException.class, () -> inputView.validateCarName(test));
+        assertEquals(expected, thrownException.getMessage());
     }
 
 //    @Test
+//    @DisplayName("시도할 횟수 입력 에러메시지 테스트")
 //    public void SecondInputExceptionTest() {
-//        assertAll("두번째 입력 검증",
+//        assertAll(
 //                () -> checkValidationThrowsException(0, "시도할 횟수는 1이상 99999이하입니다."),
-//                () -> checkValidationThrowsException(99999999, "시도할 횟수는 1이상 99999이하입니다.")
+//                () -> checkValidationThrowsException(99999999, "시도할 횟수는 1이상 99999이하입니다."),
+//                () -> checkValidationThrowsException("1")
 //        );
 //    }
-
 
 
 
