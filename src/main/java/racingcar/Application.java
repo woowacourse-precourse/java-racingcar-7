a04076d24a13f3caecaf80b3e2db1ad7b321f46e
nodonghui <163218508+nodonghui@users.noreply.github.com>
2024-10-25 16:53:08 +0900

    fect(Input) : 입력 기능과 참여 자동차 이름 분리 기능 구현
    대회에 참여하는 자동차들과 시행할 횟수를 입력받는 기능을 구현했습니다.
    입력 받은 데이터를 분리하여 배열에 저장합니다
    잘못된 데이터는 throw한 후 프로그램을 종료합니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,46 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
 public class Application {
+
+    static String inputCar;
+    static int playCount;
+
+    static void Input()  {
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        inputCar= Console.readLine();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        playCount=Integer.parseInt(Console.readLine());
+
+    }
+
+    static String[] ParseInput()  {
+
+        /* 입력받은 자동차 데이터를 쉼표를 기준으로 분리한다
+         * 자동차 이름이 5글자가 넘어가면 throw한다.
+         * 올바르지 않은 입력 데이터가 들어오면 throw한다.
+         */
+        String[] RaceMember = inputCar.split(",");
+
+        if(RaceMember.length==0) {
+            throw new IllegalArgumentException("데이터를 입력해주세요");
+        }
+
+        for(String car : RaceMember) {
+            if(car.length() >5) {
+                throw new IllegalArgumentException("차 이름의 길이가 너무 깁니다 5글자 이하로 입력해주세요");
+            }
+        }
+
+        return RaceMember;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("test");
+
     }
 }
