f47184f2aefc12bf59324d8274d20acdd25099c7
변우영 <likevangogh24@gmail.com>
2024-10-25 22:45:48 +0900

    style(OutputView): FINAL_WINNER_MESSAGE의 문자열에서 콜론 제거

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -8,7 +8,7 @@ public class OutputView {
 
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ROUND_MESSAGE = "시도할 횟수는 몇 회인가요?";
-    private static final String FINAL_WINNER_MESSAGE = "최종 우승자 :";
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자";
     private static final String NAME_POSITION_SEPARATOR = " : ";
     private static final String MOVE_DASH = "-";
     private static final String NAME_DELIMITER = ", ";
