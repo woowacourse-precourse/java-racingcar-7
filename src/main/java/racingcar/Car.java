2076c75a8ffe1619927b71a0137d7f4a94d98116
eunho <eunho1032@gmail.com>
2024-10-25 16:04:01 +0900

    refactor(Car): using tabs change spaces

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,27 +4,27 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
 
-	public String name;
-	int distance;
+    public String name;
+    int distance;
 
-	public Car(String name) {
-		this.name = name;
-		this.distance = 0;
-	}
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
 
-	public void drive() {
-		int randomNum;
+    public void drive() {
+        int randomNum;
 
-		randomNum = Randoms.pickNumberInRange(0, 9);
-		if (randomNum >= 4) {
-			distance++;
-			return;
-		}
-		this.stop();
-	}
+        randomNum = Randoms.pickNumberInRange(0, 9);
+        if (randomNum >= 4) {
+            distance++;
+            return;
+        }
+        this.stop();
+    }
 
-	public void stop() {
-		return;
-	}
+    public void stop() {
+        return;
+    }
 
 }
