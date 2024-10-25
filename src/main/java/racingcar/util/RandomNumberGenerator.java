bdeaa52a003a03b440fc8fa87f37c16c7930dd3b
변우영 <likevangogh24@gmail.com>
2024-10-25 20:11:15 +0900

    refactor(RandomNumberGenerator): RandomNumberGenerator 유틸리티 클래스로 리팩토링
    - 의미 중복을 줄이기 위해 메서드 이름을 generateRandomNumber에서 generate로 변경했다.
    - generate 메서드를 정적 메서드로 변경하여 인스턴스 없이 호출 가능하도록 수정했다.
    - 인스턴스 생성을 방지하기 위해 생성자를 private으로 설정했다.
    - 클래스 위치를 domain 패키지에서 util 패키지로 이동하여 역할에 맞는 위치로 재배치했다.

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -1,4 +1,4 @@
-package racingcar.domain;
+package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -7,7 +7,9 @@ public class RandomNumberGenerator {
     private static final int MIN_RANGE = 0;
     private static final int MAX_RANGE = 9;
 
-    public int generateRandomNumber() {
+    private RandomNumberGenerator() {}
+
+    public static int generate() {
         return Randoms.pickNumberInRange(MIN_RANGE,MAX_RANGE);
     }
 }
