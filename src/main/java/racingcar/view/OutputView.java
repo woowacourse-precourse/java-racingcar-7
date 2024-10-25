e73bf70d0797e20b4ab8029caec5794011d5e80b
coolman0123 <socool4m@naver.com>
2024-10-26 05:43:38 +0900

    feat: 횟수를 입력받을 요청문을 출력해라
    * OutputView.java: 기능 요구사항에 맞는 요청문 출력 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public static void requestCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
+
+    public static void requestRound() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
 }
