8688300741126966280626f65d157e8783ff897f
shin-chaeeun <chaeeun0217@naver.com>
2024-10-25 17:02:42 +0900

    Refacto(CarInputView):Extract CarInputView from main class

---
+++ src/main/java/view/CarInputView.java
@@ -0,0 +1,16 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class carInputView {
+  private String carInput ;
+
+  public void CarInput() {
+    System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    this.carInput = Console.readLine();
+  }
+
+  public String getCarInput() {
+    return carInput;
+  }
+}
