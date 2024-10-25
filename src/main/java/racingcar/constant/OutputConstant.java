0c6c486402f4e2564f71ab07486e5bb71b475200
mingking2 <mingee641@gmail.com>
2024-10-25 17:11:45 +0900

    feat: 출력에 대한 상수값 추가

---
+++ src/main/java/racingcar/constant/OutputConstant.java
@@ -0,0 +1,6 @@
+package racingcar.constant;
+
+public class OutputConstant {
+    public static final String OUTPUT_PROCESS_RESULT_VIEW = "실행 결과";
+    public static final String OUTPUT_FINAL_WINNER_VIEW = "최종 우승자 : ";
+}
