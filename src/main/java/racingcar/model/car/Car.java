6916fa57e23cad335746b87b6a259709bb3bb367
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-10-25 18:26:47 +0900

    feat: 자동차 이름, 전진횟수 문자열 반환 함수 구현
    - Car클래스 toString함수 구현 '(반환 문자열) pobi : --'

--- src/main/java/racingcar/model/car/Car.java
+++ src/main/java/racingcar/model/car/Car.java
@@ -1,11 +1,20 @@
 package racingcar.model.car;
 
 public class Car {
-    private String name;
+    private final String name;
     private int forwardCount;
 
     public Car(String name) {
         this.name = name;
         forwardCount = 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder progress = new StringBuilder();
+        for (int i = 0; i < forwardCount; i++) {
+            progress.append("-");
+        }
+        return name + " : " + progress;
     }
 }
