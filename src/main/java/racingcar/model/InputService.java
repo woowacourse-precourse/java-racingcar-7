58131e75f74c0cf8fc77beeb755a65513240694b
yooo <ghddbtl@gmail.com>
2024-10-25 21:17:46 +0900

    feat: create InputService and add methods
    - 자동차 이름 검증
    - 게임 횟수 검증, 최대 5000회 까지 돌릴 수 있게 함

---
+++ src/main/java/racingcar/model/InputService.java
@@ -0,0 +1,82 @@
+package racingcar.model;
+
+import racingcar.view.InputView;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class InputService {
+    private List<String> carNameList;
+
+
+    public List<String> carNamesValidation(String carNames) {
+
+        if (carNames.isEmpty()) {
+            String request = InputView.request(carNames, InputView.CAR_NAME);
+            carNamesValidation(request);
+        }
+
+        splitCarNames(carNames);
+
+        return this.carNameLengthCheck().removeNumber();
+    }
+
+
+    public int gameRoundsValidation(String gameRounds) {
+        if (isAllNumeric(gameRounds)) {
+            return overflowCheck(parseInt(gameRounds));
+        }
+
+        return gameRoundsRequest("");
+    }
+
+
+    private void splitCarNames(String carNames) {
+        carNameList = new LinkedList<>(Arrays.asList(carNames.split(",")));
+    }
+
+
+    private InputService carNameLengthCheck() {
+        for (String carName : carNameList) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("이름이 5글자가 넘었습니다.");
+            }
+        }
+        return this;
+    }
+
+
+    private List<String> removeNumber() {
+        carNameList.removeIf(carName -> carName.matches("-?\\d+(\\.\\d+)?"));
+        return carNameList;
+    }
+
+
+    private int parseInt(String str) {
+        return Integer.parseInt(str);
+    }
+
+
+    private Boolean isAllNumeric(String gameRounds) {
+        return gameRounds.matches("\\d+");
+    }
+
+
+    private int gameRoundsRequest(String gameRounds) {
+        String request = InputView.request(gameRounds, "1 ~ 5000 정수 입력만 가능합니다." + InputView.ROUNDS);
+        return gameRoundsValidation(request);
+    }
+
+
+    private int overflowCheck(int gameRounds) {
+        if (gameRounds < 1 || gameRounds > 5000) {
+            return gameRoundsRequest("");
+        }
+        return gameRounds;
+    }
+
+}
+
+
+
