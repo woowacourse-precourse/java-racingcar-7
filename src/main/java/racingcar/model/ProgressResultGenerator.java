cbf3e68e1271fd6eced945c406782c904229b608
Chan531 <chan657312@gmail.com>
2024-10-25 16:11:49 +0900

    feat(ProgressResultGenerator): 자동차 이동 문자열 생성
    Car의 movement와 "-"의 개수가 같은 이동 문자열 생성

--- src/main/java/racingcar/model/ProgressResultGenerator.java
+++ src/main/java/racingcar/model/ProgressResultGenerator.java
@@ -4,4 +4,8 @@ public class ProgressResultGenerator {
 
 	private static final String CAR_MOVEMENT_SYMBOL = "-";
 	private static final String SEPARATOR = " : ";
+
+	private static String formatMovementStatus(int number) {
+		return CAR_MOVEMENT_SYMBOL.repeat(number);
+	}
 }
