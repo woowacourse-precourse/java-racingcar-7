05c98a6081100497401ab7752e34844a90dbee7f
ryan-dia <cjfdnjs158@naver.com>
2024-10-25 18:39:56 +0900

    feat: 시도할 횟수 입력 받기

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -10,5 +10,7 @@ public class RacingcarController {
         OutputView.printMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String racingcarNames = InputView.readInput();
         Cars cars = new Cars(racingcarNames);
+        OutputView.printMessage("시도할 횟수는 몇 회인가요?");
+        String attemptCount = InputView.readInput();
     }
 }
