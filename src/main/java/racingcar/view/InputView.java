49ed21ab73d6f03ee9ef141ff3a36008b6a8be61
mingking2 <mingee641@gmail.com>
2024-10-25 17:12:02 +0900

    feat: 입력 뷰 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,14 @@
+package racingcar.view;
+
+import racingcar.constant.InputConstant;
+
+public class InputView {
+
+    public static void inputCarNameView() {
+        System.out.println(InputConstant.INPUT_CAR_NAME_VIEW);
+    }
+
+    public static void inputCountView() {
+        System.out.println(InputConstant.INPUT_COUNT_VIEW);
+    }
+}
