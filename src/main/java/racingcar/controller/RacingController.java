99ffd12d0286a43fb64e7a94ba4adf33b216392e
sc@t0a14 <gusdn6671@gmail.com>
2024-10-25 23:24:13 +0900

    Refactor(RacingController): 차량 객체 생성 방식을 개선

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,47 @@
+package racingcar.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.Cars;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private Cars cars;
+    private Car car;
+
+    public RacingController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void run() {
+        outputView.printCarNamesPrompt();
+        String carInput = inputView.input();
+        String[] carInputs = carInput.split(",");
+
+        List<Car> inputCars = new ArrayList<>();
+        for (String input : carInputs) {
+            inputCars.add(Car.of(input));
+        }
+
+
+        outputView.printAttemptCountPrompt();
+        int tryCount = Integer.parseInt(inputView.input());
+        Cars cars = new Cars(carNames);
+
+        outputView.printRacingResult();
+
+        for (int i = 0; i < tryCount; i++) {
+            cars.allMove();
+            outputView.printCarDistances(cars.getCarNames(), cars.getCarsPositions());
+        }
+
+        List<String> raceWinners = cars.findRaceWinners();
+        outputView.printWinners(raceWinners);
+    }
+
+}
