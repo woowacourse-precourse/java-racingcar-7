b4c2c4ebed762e4b9effd1c847b0b38e73eafae5
jklee3409 <jklee3409@khu.ac.kr>
2024-10-25 20:31:22 +0900

    feat(gamecontroller): initializeRace 메서드 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,6 +1,8 @@
 package racingcar.controller;
 
 import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.Race;
 import racingcar.view.InputView;
 import racingcar.view.ResultView;
 
@@ -13,5 +15,8 @@ public class GameController {
         this.resultView = resultView;
     }
 
-
+    private Race initializeRace(List<String> carNames) {
+        List<Car> cars = carNames.stream().map(Car::new).toList();
+        return new Race(cars);
+    }
 }
