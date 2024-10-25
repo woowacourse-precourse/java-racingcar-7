c17ae5831877dc566b3214e1b18dd5893550d20a
Seol-Munhyeok <abc1234568@naver.com>
2024-10-25 22:28:42 +0900

    docs(README): Add README document for project description

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,4 +1,6 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
