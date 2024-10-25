b2f8470668e7d8c9eb0ed938ebd33232191e211a
okod <okodee@naver.com>
2024-10-25 16:19:58 +0900

    feat: 경주할 자동차 이름 입력 받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        List<String> cars = getCars();
+
+    }
+
+    private static List<String> getCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        List<String> cars = new ArrayList<String>(Arrays.asList(input.split(",")));
+        return cars;
     }
 }
