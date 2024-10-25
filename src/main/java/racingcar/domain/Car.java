aba6f60b170ae110767ffc23a6c56eec623ff4ce
songsunkook <songsunkook@gmail.com>
2024-10-25 23:07:59 +0900

    refactor: Car 메서드 순서 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -23,12 +23,12 @@ public class Car {
         }
     }
 
-    public int getPosition() {
-        return position;
-    }
-
     public String getName() {
         return name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 
     public void playRound() {
