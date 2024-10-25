22176fd1ece61f60001a2e06f08749b6c97be8bb
zoaseo <huijaeseo18@google.com>
2024-10-25 09:45:35 +0900

    feat: List<Car> 생성 함수 구현

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,30 +1,42 @@
 package racingcar;
 
-import io.CarNameInputHandler;
+import camp.nextstep.edu.missionutils.Randoms;
+import car.Car;
+import io.RacingCarInputHandler;
 import io.InputHandler;
 import io.OutputHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCar {
 
     private InputHandler inputHandler = new InputHandler();
     private OutputHandler outputHandler = new OutputHandler();
-    private CarNameInputHandler carNameInputHandler = new CarNameInputHandler();
+    private RacingCarInputHandler racingCarInputHandler = new RacingCarInputHandler();
 
     public void run() {
         try {
             outputHandler.showInputCarNameMessage();
             String inputCarName = inputHandler.getInputCarName();
-            List<String> separatedCarNameList = carNameInputHandler.getSeparatedCarNameList(inputCarName);
+            List<String> separatedCarNameList = racingCarInputHandler.getSeparatedCarNameList(inputCarName);
 
             outputHandler.showInputRacingCountMessage();
-            int inputRacingCount = inputHandler.getInputRacingCount();
+            String inputRacingCount = inputHandler.getInputRacingCount();
+            int racingCount = racingCarInputHandler.getRacingCount(inputRacingCount);
 
+            List<Car> carList = getCarList(separatedCarNameList);
 
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
 
+    public List<Car> getCarList(List<String> separatedCarNameList) {
+        List<Car> carList = new ArrayList<>();
+        for (String carName : separatedCarNameList) {
+            carList.add(Car.of(carName));
+        }
+        return carList;
+    }
 }
