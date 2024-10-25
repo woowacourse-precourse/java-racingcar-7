f83781e5ad927d3f47c64a4f2dd4259c5337c88c
cherryiJuice <chldlwn711@gmail.com>
2024-10-25 16:35:13 +0900

    feat: 차 이름 검증 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,10 +1,13 @@
 package racingcar.domain;
 
 public class Car {
+    private final static int CAR_NAME_MAX_LENGTH = 5;
+    private final static int CAR_NAME_MIN_LENGTH = 1;
     private final String name;
     private int position;
 
     private Car(final String name, final int position) {
+        validateCarName(name);
         this.name = name;
         this.position = position;
     }
@@ -23,6 +26,12 @@ public class Car {
 
     public void move(final int distance) {
         position += distance;
+    }
+
+    private void validateCarName(String name) {
+        if (name.length() > CAR_NAME_MAX_LENGTH || name.length() < CAR_NAME_MIN_LENGTH) {
+            throw new IllegalArgumentException();
+        }
     }
 
 }
