b34dbe88827882b071ba66e263040347a80d0ce7
soeunnPark <carol0724@naver.com>
2024-10-25 20:53:48 +0900

    test(Car): Add test case on fail to forward car when position is bigger than INTEGER.MAX_VALUE - 2
    자동차의 위치를 나타내는 필드인 position 은 int 타입입니다. 따라서 int 범위를 벗어나는 만큼 자동차를 전진시킬 수 없습니다.

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,10 +1,13 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Field;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.common.exception.CarPositionOutOfRangeException;
 
 class CarTest {
 
@@ -23,5 +26,16 @@ class CarTest {
         Car car = new Car("전진하는 자동차");
         car.forward();
         assertThat(car.getCarPosition()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("자동차 위치가 int 범위를 벗어날 때까지 전진하면 에러를 발생하는 테스트")
+    void car_Test3() throws NoSuchFieldException, IllegalAccessException {
+        Car car = new Car("더 이상 전진할 수 없는 자동차");
+        Field carPositionField = Car.class.getDeclaredField("carPosition");
+        carPositionField.setAccessible(true);
+        carPositionField.setInt(car, Integer.MAX_VALUE - 1);
+        assertThatThrownBy(car::forward)
+                .isInstanceOf(CarPositionOutOfRangeException.class);
     }
 }
