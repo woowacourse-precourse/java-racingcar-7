680254705d59ac6d708334c9be68dbf422485dac
GS_song98 <20003204@sju.ac.kr>
2024-10-25 19:43:59 +0900

    feat: 자동차 객체에 사용할 위치 객체 기능 추가

---
+++ src/main/java/racingcar/domain/Position.java
@@ -0,0 +1,31 @@
+package racingcar.domain;
+
+import racingcar.util.RandomGenerator;
+
+public class Position {
+
+    private static final int MOVE_FORWARD_STANDARD = 4;
+
+    private final RandomGenerator randomGenerator;
+    private int position;
+
+    public Position() {
+        this.randomGenerator = new RandomGenerator();
+        position = 0;
+    }
+
+    public void move() {
+        if (randomGenerator.generate() >= MOVE_FORWARD_STANDARD) {
+            position++;
+        }
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public String toString() {
+        return "-".repeat(position);
+    }
+}
