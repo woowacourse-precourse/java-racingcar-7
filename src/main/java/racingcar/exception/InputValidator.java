788652794b1b44092cc7f4e169186c9c39e44f28
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-25 19:38:53 +0900

    feat: 이동 횟수 문자 검증 구현
    이동 횟수에 문자가 입력되었는지를 검증하는 메서드를 구현

--- src/main/java/racingcar/exception/InputValidator.java
+++ src/main/java/racingcar/exception/InputValidator.java
@@ -2,6 +2,7 @@ package racingcar.exception;
 
 public class InputValidator {
     private static final String WHITE_SPACES_REGEX = "^\\s*$";
+    private static final String DIGIT_REGEX = "^[0-9]*$";
 
     public void validateWhitespaces(String carsNames, String repeatTimes) {
         if (carsNames.matches(WHITE_SPACES_REGEX)) {
@@ -16,6 +17,12 @@ public class InputValidator {
     public void validateInvalidCarName(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("에러: 입력된 이름의 길이가 5를 넘어갑니다. 길이 5이하의 이름을 입력해주세요!");
+        }
+    }
+
+    public void validateRepeatTimesNotDigit(String repeatTimes) {
+        if (!repeatTimes.matches(DIGIT_REGEX)) {
+            throw new IllegalArgumentException("에러: 입력된 반복 횟수에 숫자가 아닌 문자가 포함되어 있습니다!");
         }
     }
 }
