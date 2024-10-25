22f9283cacbe0bf83ab54d290a222fd39880f03d
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-10-26 05:35:05 +0900

    feat: 자동차 이름 빈문자열 검증 기능 구현

--- src/main/java/racingcar/io/domain/Car.java
+++ src/main/java/racingcar/io/domain/Car.java
@@ -4,6 +4,7 @@ public class Car {
 
 	private static final int CAR_NAME_MAX_LENGTH = 5;
 	private static final String OVER_FLOW_CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 5자 이하만 가능합니다.";
+	private static final String BLANK_CAR_NAME_ERROR_MESSAGE = "자동차 이름이 존재해야 합니다.";
 	private static final int INITIAL_MOVE_DISTANCE = 0;
 	private static final int MOVE_CONDITION = 4;
 
@@ -12,10 +13,21 @@ public class Car {
 
 	private Car(String name, int moveDistance) {
 
-		validateNameLength(name);
+		validateName(name);
 
 		this.name = name;
 		this.moveDistance = moveDistance;
+	}
+
+	private void validateName(String name) {
+		validateBlankName(name);
+		validateNameLength(name);
+	}
+
+	private void validateBlankName(String name) {
+		if (name == null || name.isBlank()) {
+			throw new IllegalArgumentException(BLANK_CAR_NAME_ERROR_MESSAGE);
+		}
 	}
 
 	private void validateNameLength(String name) {
