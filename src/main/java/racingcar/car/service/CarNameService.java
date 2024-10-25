144ab52c418aec9498b4f109026faa7215170af0
jieun <ju3391@naver.com>
2024-10-25 21:21:02 +0900

    feat(CarNameService): 자동차 이름 관련 로직 처리
    - 입력된 자동차 이름 검증 로직 추가
    - 자동차 객체 생성 로직 추가

--- src/main/java/racingcar/car/service/CarNameService.java
+++ src/main/java/racingcar/car/service/CarNameService.java
@@ -1,4 +1,24 @@
 package racingcar.car.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.car.model.Car;
+import racingcar.car.validation.CarNameValidator;
+
+// 자동차 이름 관련 로직을 처리하는 서비스
 public class CarNameService {
+    private final CarNameValidator carNameValidator;
+
+    public CarNameService(CarNameValidator carNameValidator) {
+        this.carNameValidator = carNameValidator;
+    }
+
+    public List<Car> createCars(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            carNameValidator.validateCarName(carName);  // 자동차 이름 검증
+            cars.add(new Car(carName));
+        }
+        return cars;
+    }
 }
