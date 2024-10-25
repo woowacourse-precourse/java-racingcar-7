19eafae83c91d7f36a256722b2d5f6bb2063136d
coolman0123 <socool4m@naver.com>
2024-10-26 05:50:41 +0900

    refactor: 불필요한 로직, 메소드 삭제

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -4,16 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.model.Car;
-import racingcar.view.InputView;
 
 public class CarService {
 
-    public static List<Car> setParticipants() {
-        String[] carNames = InputView.getCarNames();
-        return convertToCarList(carNames);
-    }
 
-    private static List<Car> convertToCarList(String[] carNames) {
+    public static List<Car> convertToCarList(String[] carNames) {
         return Arrays.stream(carNames)
                 .map(String::trim)
                 .map(Car::new)
