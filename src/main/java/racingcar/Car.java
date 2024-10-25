0e7dd03263befa8369a6558cc9a11e7b2eb9ee45
김영록 <praoo800@konkuk.ac.kr>
2024-10-26 02:51:56 +0900

    <feat>(Car) : 자동차 객체의 전진 판단 기능 구현
      isForward()메소드를 추가구현해 줌으로써
      랜덤넘버가 4이상일때 전진해도 된다는 의미인 true값을
      반환해줌으로써 자동차의 전진을 판단할 수 있다
    
      BREAKING CHANGE : Car.java에 isForward()메소드 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
     private String name;
@@ -25,7 +27,11 @@ public class Car {
         System.out.println();
     }
 
-    public void forward(){
+    public void forward() {
         this.forwardLevel += 1;
+    }
+
+    public boolean isForward() {
+        return Randoms.pickNumberInRange(0, 9) >= 4;
     }
 }
