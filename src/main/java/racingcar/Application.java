aa3870869b613fd82e5ae7bf63d16823188cbda7
LimJaeEun0930 <je9513666@gmail.com>
2024-10-25 11:23:32 +0900

    feat(input): add input function
    add input function at Application.class

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+        //RacingGame::registerCars(input);
+        String numInput = Console.readLine();
+        //RacingGame::setTryCount();
+        //RacingGame::doGame();
+        Console.close();
     }
 }
