bbe40db1464a6d303916845171075f2e5ef1c9cb
dawit0905 <dawit0905@gmail.com>
2024-10-25 12:18:17 +0900

    refactor: 공백검사 함수 이름 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -10,6 +10,7 @@ public class InputView {
     public List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
+        InputValidator.validateEmpty(input);
         return Arrays.asList(input.split(","));
     }
 
