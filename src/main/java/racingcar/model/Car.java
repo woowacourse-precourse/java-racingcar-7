5e5663d7209c8819c8dfbaa1bcfa53df08e58b40
dradnats1012 <herjebi1012@gmail.com>
2024-10-25 17:42:19 +0900

    feat: Car 클래스 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,29 @@
 package racingcar.model;
 
+import racingcar.model.generator.NumberGenerator;
+
 public class Car {
+
+    private final String name;
+    private int position = 0;
+    private final NumberGenerator numberGenerator;
+
+    public Car(String name, NumberGenerator numberGenerator) {
+        this.name = new CarName(name).getName();
+        this.numberGenerator = numberGenerator;
+    }
+
+    public void move() {
+        if (numberGenerator.generateNum() >= 4) {
+            position += 1;
+        }
+    }
+
+    public int getPosition(){
+        return position;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
