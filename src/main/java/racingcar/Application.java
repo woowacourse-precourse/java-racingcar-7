b37b1c831dca9bd91f170be2cf15fcc203dab8de
Beomseok Noh <prodigy0831@naver.com>
2024-10-25 10:14:40 +0900

    feat(RaceCar): 자동차 이름이 5글자 이상일 때 예외처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,14 +6,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String getCarNames = Console.readLine();
+        String userInputString = Console.readLine();
         System.out.println("시도할 횟수는 몇 회인가요?");
-        String getAttemptCount = Console.readLine();
+        String userInputNumber = Console.readLine();
 
         RaceCar raceCar = new RaceCar();
-        String[] carNames = raceCar.splitNames(getCarNames);
-
-
+        String[] carNames = raceCar.splitNames(userInputString);
 
     }
 }
--- src/main/java/racingcar/RaceCar.java
+++ src/main/java/racingcar/RaceCar.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class RaceCar {
-    public String[] splitNames(String getCarNames){
-        String[] cars= getCarNames.split(",");
+    public String[] splitNames(String carNameString){
+        String[] cars= carNameString.split(",");
+        for(String car :cars){
+            if(car.length()>5){
+                throw new IllegalStateException("자동차의 이름은 5글자를 넘길 수 없습니다");
+            }
+        }
         return cars;
     }
 }
