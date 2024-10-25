23499ef01bc18f53c25a63b5fc63aa300e868e72
bowook <mbosx316@naver.com>
2024-10-25 22:01:25 +0900

    feat(RandomNumber) : RandomNumber 기능 추가
    RandomNumber를 domain에 기능 추가

--- src/main/java/racingcar/domain/RandomNumber.java
+++ src/main/java/racingcar/domain/RandomNumber.java
@@ -1,2 +1,11 @@
-package racingcar.domain;public class RandomNumber {
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumber {
+    private final static int randomStartNumber = 0;
+    private final static int randomLastNumber = 9;
+    public static int getRandomNumber() {
+        return Randoms.pickNumberInRange(randomStartNumber,randomLastNumber);
+    }
 }
