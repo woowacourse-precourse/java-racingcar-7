40fa017b1d158507cd52ff9461e96f2371a067d0
yush <yush2202@gmail.com>
2024-10-25 17:50:00 +0900

    feat($ Constants): 상수 작성
    애플리케이션에서 범용적으로 활용될 상수 작성

---
+++ src/main/java/racingcar/constants/Constants.java
@@ -0,0 +1,8 @@
+package racingcar.constants;
+
+public class Constants {
+    public static final String DELIMITER = ",";
+    public static final String DASH = "-";
+    public static final String COLON = ":";
+    
+}
