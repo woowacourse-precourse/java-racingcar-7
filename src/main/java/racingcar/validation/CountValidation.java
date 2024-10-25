d0a2baab8b5942fa0ef6345a566289a380f2b351
garden14 <ci667@dgu.ac.kr>
2024-10-25 16:36:51 +0900

    fix(CountValidation): 횟수 입력 예외처리 로직 수정

--- src/main/java/racingcar/validation/CountValidation.java
+++ src/main/java/racingcar/validation/CountValidation.java
@@ -5,6 +5,9 @@ public class CountValidation {
     public int convertToNaturalNum(String stringCount){
         try {
             int count = Integer.parseInt(stringCount);
+            if(count <= 0){
+                throw new IllegalArgumentException("입력한 횟수가 자연수가 아닙니다.");
+            }
             return count;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("입력한 횟수가 자연수가 아닙니다.");
