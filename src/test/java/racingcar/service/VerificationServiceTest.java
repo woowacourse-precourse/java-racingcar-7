1a3020241a3c55b00210a9f6e91e87ce295acc4c
이창헌 <changbill@naver.com>
2024-10-25 15:16:31 +0900

    refactor(test): add empty input exception test

--- src/test/java/racingcar/service/VerificationServiceTest.java
+++ src/test/java/racingcar/service/VerificationServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import racingcar.exception.EmptyInputException;
 import racingcar.exception.InvalidCharacterException;
 import racingcar.exception.InvalidLengthException;
 
@@ -26,6 +27,21 @@ class VerificationServiceTest {
     class IsValidLengthTests {
 
         @Test
+        void 최소_길이_예외테스트() {
+            List<String> carNames = new ArrayList<>();
+            carNames.add("");
+            assertThrows(EmptyInputException.class, () -> verificationService.isValidLength(carNames));
+        }
+
+        @Test
+        void 최대_길이_예외테스트() {
+            List<String> invalidCarNames = new ArrayList<>();
+            invalidCarNames.add("abcdef");
+            invalidCarNames.add("ghijklmnopqrstuvwxyz");
+            assertThrows(InvalidLengthException.class, () -> verificationService.isValidLength(invalidCarNames));
+        }
+
+        @Test
         void 길이_테스트() {
             List<String> carNames = new ArrayList<>();
             carNames.add("a");
@@ -35,19 +51,13 @@ class VerificationServiceTest {
             carNames.add("klmno");
             assertTrue(verificationService.isValidLength(carNames));
         }
-
-        @Test
-        void 길이_예외테스트() {
-            List<String> invalidCarNames = new ArrayList<>();
-            invalidCarNames.add("abcdef");
-            invalidCarNames.add("ghijklmnopqrstuvwxyz");
-            assertThrows(InvalidLengthException.class, () -> verificationService.isValidLength(invalidCarNames));
-        }
     }
 
     @Nested
     @DisplayName("입력값에 특수문자있는지 확인하는 테스트")
     class ContainsInvalidCharacterTests {
+
+        // TODO : String 길이 최소치, 최대치를 정해서 테스트
 
         @Test
         void 숫자_포함_테스트() {
@@ -66,6 +76,8 @@ class VerificationServiceTest {
     @Nested
     @DisplayName("숫자인지 확인하는 메소드 테스트")
     class IsNumberTests {
+
+        // TODO : 숫자 최대치 최소치 테스트
 
         @Test
         void 숫자_체크_테스트() {
