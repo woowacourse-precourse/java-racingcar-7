2c0e852393c82549e30965693c64d3b2436ae750
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-10-26 01:17:27 +0900

    feat(creatCars): 자동차 이름 문자열로 자동차 객체들을 생성하는 기능 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,5 +1,9 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Car {
 
     private String name;
@@ -13,6 +17,19 @@ public class Car {
     public Car(String name, int position) {
         this.name = name;
         this.position = position;
+    }
+
+    public static List<Car> createCars(String carNames) {
+        List<String> names = Arrays.asList(carNames.split(","));
+
+        List<Car> cars = new ArrayList<>();
+
+        for (String name : names) {
+            Car car = new Car(name);
+            cars.add(car);
+        }
+
+        return cars;
     }
 
     public int getPosition() {
