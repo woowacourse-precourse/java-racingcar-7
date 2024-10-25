b2fe01a38c532c6ea738742e1f88dafdccc789fe
world-dv <goodforx0147@naver.com>
2024-10-26 03:07:36 +0900

    refactor: 무작위 값 메서드 분리
    - 무작위 값 생성은 독립적인 기능이므로 메서드를 분리해 무작위 값 계산기로 재구성합니다.

---
+++ src/main/java/racingcar/util/RandomNumberCalculator.java
@@ -0,0 +1,12 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberCalculator {
+    private static final Integer START_RANDOM_NUMBER = 0;
+    private static final Integer END_RANDOM_NUMBER = 9;
+
+    public static Integer getRandomNumber() {
+        return Randoms.pickNumberInRange(START_RANDOM_NUMBER, END_RANDOM_NUMBER);
+    }
+}
