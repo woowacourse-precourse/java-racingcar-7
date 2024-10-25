f2d5578b8c8d53409428fb5a6dd8018792b54806
Jhzlo <kjh0010703@naver.com>
2024-10-25 18:18:40 +0900

    test(game-round): GameRound 테스트 코드 작성

--- src/main/java/racingcar/model/GameRound.java
+++ src/main/java/racingcar/model/GameRound.java
@@ -9,13 +9,25 @@ public class GameRound {
 
     private Integer validateRound(String round) {
         try {
-            return Integer.parseInt(round);
+            Integer parsedRound = Integer.parseInt(round);
+            validatePositive(parsedRound);
+            return parsedRound;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("시도 횟수는 숫자만 입력해야 합니다.");
         }
     }
 
-    public Integer getRound(){
+    private void validatePositive(Integer round) {
+        if (isNotPositive(round)) {
+            throw new IllegalArgumentException("시도 횟수는 음수일 수 없습니다.");
+        }
+    }
+
+    private boolean isNotPositive(Integer round) {
+        return round < 0;
+    }
+
+    public Integer getRound() {
         return round;
     }
 }
