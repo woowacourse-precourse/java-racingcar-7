9c964c23a01d06e334b7c3bac82aab9a3841f15d
donghyunn1 <dongdong0516@naver.com>
2024-10-25 12:32:04 +0900

    feat: 이름 최대 길이 초과시 오류 출력

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,13 +5,22 @@ import racingcar.util.RandomNumberGenerator;
 public class Car {
 
     private static final int MOVING_STANDARD = 4;
+    private static final int NAME_LENGTH_BOUND = 5;
+    private static final String NAME_LENGTH_ERROR = "[ERROR] 이름은 5글자 이하여야 합니다.";
 
     private final String name;
     private int position;
 
     public Car(String name) {
+        validate(name);
         this.name = name;
         this.position = 0;
+    }
+
+    private void validate(String name) {
+        if (name.length() > NAME_LENGTH_BOUND) {
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR);
+        }
     }
 
     public String getName() {
