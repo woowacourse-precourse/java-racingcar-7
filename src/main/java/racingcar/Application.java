5406c2da2cd1256323c0b59e9b80ef60359463be
Beomseok Noh <prodigy0831@naver.com>
2024-10-25 15:08:24 +0900

    fix(CheckInput): 중복되는 이름 입력시 예외 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,12 +7,11 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String userInputString = Console.readLine();
+        CheckInput checkInput = new CheckInput();
+        String[] carNames = checkInput.splitNames(userInputString);
+
         System.out.println("시도할 횟수는 몇 회인가요?");
         String userInputNumber = Console.readLine();
-
-        CheckInput checkInput = new CheckInput();
-
-        String[] carNames = checkInput.splitNames(userInputString);
         int raceCount = checkInput.isValidCount(userInputNumber);
 
         RaceCar car = new RaceCar();
--- src/main/java/racingcar/CheckInput.java
+++ src/main/java/racingcar/CheckInput.java
@@ -1,19 +1,28 @@
 package racingcar;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CheckInput {
     public String[] splitNames(String carNameString){
-        String[] cars= carNameString.split(",");
+        String[] cars = carNameString.split(",");
+        Set<String> nameSet = new HashSet<>();
         for(String car :cars){
-            if(car.length()>5){
-                System.out.println(car);
+            String trimmedCarName = car.trim();
+            if(trimmedCarName.length()>5){
                 throw new IllegalArgumentException("자동차의 이름은 다섯 글자를 넘을 수 없습니다.");
             }
+            if(nameSet.contains(trimmedCarName)){
+                throw new IllegalArgumentException("중복된 이름이 존재합니다.");
+            }
+            nameSet.add(trimmedCarName);
         }
-        return cars;
+
+        return nameSet.toArray(new String[0]);
     }
 
     public int isValidCount(String raceCountString) {
-        if(raceCountString==null || raceCountString.isEmpty() || !raceCountString.matches("\\d+")){
+        if(raceCountString == null || raceCountString.isEmpty() || !raceCountString.matches("\\d+")){
             throw new IllegalArgumentException("올바른 숫자를 입력해 주세요.");
         }
         return Integer.parseInt(raceCountString);
