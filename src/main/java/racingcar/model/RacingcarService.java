2818d292f21eb39118087d218b65f174ddcf23da
Dev-LHY <dev.leehy@gmail.com>
2024-10-25 17:30:43 +0900

    feat(racingcarService): 자동차 전진 추가
    - 자동차 이름을 key 값, 전진 상태를 value 값으로 사용

---
+++ src/main/java/racingcar/model/RacingcarService.java
@@ -0,0 +1,24 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Map;
+import racingcar.view.OutputView;
+
+public class RacingcarService {
+    private final OutputView outputView;
+    private static final int START_NUMBER = 0;
+    private static final int END_NUMBER = 9;
+    private static final int STANDARD_NUMBER = 4;
+
+    public RacingcarService() {
+        outputView = OutputView.getInstance();
+    }
+
+    private void movingForward(Map<String, String> player) {
+        for (String key : player.keySet()) {
+            if (Randoms.pickNumberInRange(START_NUMBER, END_NUMBER) > STANDARD_NUMBER) {
+                player.put(key, player.get(key) + "-");
+            }
+        }
+    }
+}
