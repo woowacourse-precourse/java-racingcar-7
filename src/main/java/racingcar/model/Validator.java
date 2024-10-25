8e28acd7dc1436f406f1b2336092d0014350bf5c
soeun2537 <soeun2537@gmail.com>
2024-10-25 23:32:43 +0900

    refactor: 자동차 이름 유효성 검사 - 중복된 이름 로직 분리

--- src/main/java/racingcar/model/Validator.java
+++ src/main/java/racingcar/model/Validator.java
@@ -2,15 +2,19 @@ package racingcar.model;
 
 import static racingcar.constant.Constant.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Validator {
 
     public void validateCarNames(List<String> CarNames) {
+        List<String> uniqueCarNames = new ArrayList<>();
         for (String carName : CarNames) {
             validateNotEmpty(carName);
             validateNoSpaces(carName);
             validateMaxLength(carName, MAX_CAR_NAME_LENGTH);
+            validateNoDuplicate(carName, uniqueCarNames);
+            uniqueCarNames.add(carName);
         }
     }
 
@@ -28,6 +32,12 @@ public class Validator {
 
     private void validateMaxLength(String input, int maxLength) {
         if (input.length() > maxLength) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNoDuplicate(String input, List<String> uniqueNames) {
+        if (uniqueNames.contains(input)) {
             throw new IllegalArgumentException();
         }
     }
