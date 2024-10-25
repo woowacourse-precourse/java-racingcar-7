e32ad892f00eca1f629e0f903b848b2c8bfb8983
Jo Myeong-geun <happy63590@gmail.com>
2024-10-26 04:43:25 +0900

    refactor: 랜덤 숫자 생성 메서드 위치 수정

--- src/main/java/racingcar/service/CarRaceService.java
+++ src/main/java/racingcar/service/CarRaceService.java
@@ -1,12 +1,12 @@
 package racingcar.service;
 
+import static java.util.stream.Collectors.toMap;
 import static racingcar.enums.ExceptionMessage.CAR_NOT_FOUND;
 import static racingcar.util.RandomUtil.getRandomNumberInRange;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import racingcar.domain.Car;
 
 public class CarRaceService {
@@ -21,13 +21,13 @@ public class CarRaceService {
 
     public void moveCars() {
         cars.stream()
-                .filter(car -> canMove(getRandomNumberInRange(0, 9)))
+                .filter(car -> canMove())
                 .forEach(Car::move);
     }
 
     public Map<String, String> getCarsStatus() {
         return cars.stream()
-                .collect(Collectors.toMap(
+                .collect(toMap(
                         Car::getName,
                         Car::getMovingDistance
                 ));
@@ -40,8 +40,8 @@ public class CarRaceService {
                 .toList();
     }
 
-    private boolean canMove(int randomNumber) {
-        return randomNumber >= 4;
+    private boolean canMove() {
+        return getRandomNumberInRange(0, 9) >= 4;
     }
 
     private int getMaxDistance() {
