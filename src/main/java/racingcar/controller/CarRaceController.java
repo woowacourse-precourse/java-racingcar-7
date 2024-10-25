f78cb256fdb94cb0362867650f77b3840fbf2fac
CHISANW <keuye06380618@gmail.com>
2024-10-25 15:32:37 +0900

    feat(CarRaceController): 자동차 게임 실행 및 결과 출력 기능 구현
    - CarService를 생성자 주입 방식으로 추가
    - CarService의 playGame() 메서드를 호출하여 자동차 경주 게임 실행 결과를 RaceDto로 반환하는 로직 구현
    - RacingGameView의 endGame() 메서드를 통해 각 횟수별 기록된 자동차 경주 결과와 우승자 자동차 이름을 출력하는 로직 추가

--- src/main/java/racingcar/controller/CarRaceController.java
+++ src/main/java/racingcar/controller/CarRaceController.java
@@ -1,17 +1,24 @@
 package racingcar.controller;
 
+import racingcar.dto.RaceDto;
 import racingcar.dto.ViewDto;
+import racingcar.service.CarService;
 import racingcar.view.RacingGameView;
 
 public class CarRaceController {
 
     private final RacingGameView view;
+    private final CarService service;
 
     public CarRaceController() {
         this.view = new RacingGameView();
+        this.service = new CarService();
     }
 
-    public void process(){
+    public void process() {
         ViewDto viewDto = view.startGame();
+        RaceDto raceDto = service.playGame(viewDto.getCarName(), viewDto.getCount());
+        view.endGame(raceDto.getRaceRounds(), raceDto.getWinnerCarName());
     }
+
 }
