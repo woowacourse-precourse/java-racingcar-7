2307ca988737632f559c880f470949a045410c7c
jieun <ju3391@naver.com>
2024-10-25 21:14:56 +0900

    feat(Input): 사용자 입력 기능 구현
    - 자동차 이름 입력 기능 추가
    - 시도할 횟수 입력 기능 추가
    - 입력 메시지 상수화

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -1,4 +1,29 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//출력문 상수화하기!
+//주석 필요없는 건 지우기!
+
+// 사용자 입력을 담당하는 클래스
 public class Input {
+
+    private static final String CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요 (쉼표로 구분):";
+    private static final String ATTEMPT_COUNT_MESSAGE = "시도할 횟수를 입력하세요:";
+
+    public List<String> getCarNames() {
+        System.out.println(CAR_NAMES_MESSAGE);
+        String input = Console.readLine();
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    public int getAttemptCount() {
+        System.out.println(ATTEMPT_COUNT_MESSAGE);
+        return Integer.parseInt(Console.readLine());
+    }
 }
