2068af8eb98d009ceb3f9d2e95c0825248bed6f5
김예린 <annalin1004@gmail.com>
2024-10-25 23:44:10 +0900

    Implement racing functions(2)
    Implement a function that organizes the winners of car races
     into a list and displays them on the user screen.

--- src/main/java/racingcar/controller/UserViewController.java
+++ src/main/java/racingcar/controller/UserViewController.java
@@ -1,16 +1,22 @@
 package racingcar.controller;
 
+import racingcar.service.RacingService;
+
 import java.util.List;
 
-import static racingcar.view.UserResponseView.inputTypeError;
-import static racingcar.view.UserResponseView.racingOutput;
+import static racingcar.view.UserResponseView.*;
 
 public class UserViewController {
+
     void callErrorView(){
         inputTypeError();
     }
 
     void callRacingOutputView(List<String> carName, List<Integer> start){
         racingOutput(carName, start);
+    }
+
+    void callWinnerOutput(List<String> winners){
+        finalWinner(winners);
     }
 }
