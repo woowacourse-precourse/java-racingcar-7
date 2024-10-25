eb9f5bf94d22454556267cb1f61c1a5a3c74b6ba
KangrimChoi <rkdfla100@naver.com>
2024-10-25 21:05:06 +0900

    feat: 3.1 조건 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,15 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-       run();
+        run();
     }
 
     public static void run(){
@@ -40,16 +42,39 @@ public class Application {
         return false;
     }
 
+    /* 도착했을 때, 어떻게 해야 효율적으로 도착했는지 알 수 있을까?
+    *
+    * 1. += 할 때마다, 현재 위치 저장.
+    *
+    **/
+
     public static List<String> startRacing(List<String> racers) {
         List<String> winners = null;
-        int FinishLine = Integer.parseInt(Console.readLine());
+        int finishLine = Integer.parseInt(Console.readLine());
+        int frontRacerLocation = 0;
 
+        while(frontRacerLocation < finishLine){
+            for(String racer : racers) {
+                if( isSuccesMoving() ){
+                    //출루
+                }
+            }
+        }
 
         return winners;
     }
 
     public static void printWinners(List<String> winners){
         System.out.println("승자는 우리 모두야!");
+    }
+
+    public static boolean isSuccesMoving() {
+        int randNum = Randoms.pickNumberInRange(0,9);
+        if(randNum >= 4){
+            return true;
+        }
+
+        return false;
     }
 
 
