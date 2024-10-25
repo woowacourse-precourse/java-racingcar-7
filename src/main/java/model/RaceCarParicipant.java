775323072fb8d7db2d3267a9e38ec86ba9788288
shin-chaeeun <chaeeun0217@naver.com>
2024-10-25 17:27:17 +0900

    Refactor(RaceCarParticipant):Extract RaceCarParticipant from main class

---
+++ src/main/java/model/RaceCarParicipant.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RaceCarParicipant {
+
+  private List<String> cars ;
+
+  public void validateCars(String input) {
+    // 허용된 문자들만 포함되어 있는지 검사 (콤마, 영어, 숫자)
+    if (input.matches(".*[^,A-Za-z0-9].*")) {
+      throw new IllegalArgumentException("입력에 허용되지 않은 문자가 포함되어 있습니다.");
+    }
+
+    this.cars = Arrays.asList(input.split(","));
+  }
+
+  public List<String> getValidateCars() {
+    return cars;
+  }
+}
--- src/main/java/view/WinnerOutputView.java
+++ src/main/java/view/WinnerOutputView.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class WinnerOutputView {
   public List<String> winnerList;
 
-  public static void showWinner(List<String> winnerList) {
+  public void showWinner(List<String> winnerList) {
     final String delimiter = ", ";
     final String winners = String.join(delimiter, winnerList);
     System.out.println("최종 우승자 : " + winners);
