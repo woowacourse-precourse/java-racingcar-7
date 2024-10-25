c21ac5ce446c5c753f1a60ec6d9ee2773e64aa64
kim-su-mi <ksmm9607@naver.com>
2024-10-25 16:08:41 +0900

    feat : 사용자 입력 받기(자동차이름 입력,시도횟수 입력,입력값 검증)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import racingcar.validator.InputValidator;
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    	String carNamesInput = InputView.readCarNames();
+    	String[] carNames = carNamesInput.split(",");
+    	InputValidator.validateCarNames(carNames);
+    	
+    	String trialNumInput = InputView.readTrialNum();
+    	InputValidator.validateTrilaNum(trialNumInput);
+    	int trialNum = Integer.parseInt(trialNumInput);
+    	
+    	
     }
 }
