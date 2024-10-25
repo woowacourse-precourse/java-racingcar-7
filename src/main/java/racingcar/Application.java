f6c1c9ee3f43766a8929fd1c2895e075d87aad91
halfmoon-mind <5338095@gmail.com>
2024-10-25 13:54:29 +0900

    feat: 전체 코드 흐름 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
+
+        List<String> carNames = inputHandler.getCarNames();
+        int attempts = inputHandler.getNumberOfAttempts();
+
+        List<Car> cars = CarFactory.createCars(carNames);
+        Car car = new Car("pobi");
+
+        // attemps만큼 car move
+        for (int i = 0; i < attempts; i++) {
+            for (Car c : cars) {
+                c.move();
+            }
+            for (Car c : cars) {
+                System.out.println(c.getName() + " : " + c.getProgress());
+            }
+        }
+
+        // 결과 출력
+        List<String> winners = CarUtils.getWinners(cars);
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
