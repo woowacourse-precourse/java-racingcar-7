10461172e3dea20e422575ca59156db0abb569c9
choign3123 <choign3123@naver.com>
2024-10-25 13:40:34 +0900

    Refactor(Application): 자동차 세팅 로직 main()에서 분리
    변경 이유:
    메소드가 한가지 일만 하도록 최대한 작게 만들기 위해

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,16 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+
+    private static IInputDisplay inputDisplay;
+
     public static void main(String[] args) {
-        IInputDisplay inputDisplay = new WtcInputDisplay();
+        inputDisplay = new WtcInputDisplay();
 
-        String carNameMass = inputDisplay.getPromptInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-        String[] carNameList = carNameMass.split(",");
-
-        List<ICar> carList = new ArrayList<>();
-        for (final String carName : carNameList) {
-            carList.add(new CarImpl(carName));
-        }
+        List<ICar> carList = settingCarList();
 
         String roundStr = inputDisplay.getPromptInput("시도할 횟수는 몇 회인가요?\n");
         int round = Integer.parseInt(roundStr);
@@ -23,5 +20,17 @@ public class Application {
         CarRacingGame carRacingGame = new CarRacingGame(outputDisplay, carList, round);
 
         carRacingGame.start();
+    }
+
+    private static List<ICar> settingCarList() {
+        String carNameMass = inputDisplay.getPromptInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        String[] carNames = carNameMass.split(",");
+
+        List<ICar> carList = new ArrayList<>();
+        for (final String carName : carNames) {
+            carList.add(new CarImpl(carName));
+        }
+
+        return carList;
     }
 }
