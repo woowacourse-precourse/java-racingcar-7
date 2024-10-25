d3e080b6a323e5e2fc621dac0d0e3bce5f5857bd
world-dv <goodforx0147@naver.com>
2024-10-25 21:10:09 +0900

    feat: 자동차 경주 로직 작성
    - 자동차 이름 문자열 입력 문구를 출력합니다.
    - 사용자로부터 자동차 이름 리스트를 입력받습니다.
    - 자동차 이름을 쉼표 단위로 분리한 뒤 공백을 제거하고, 배열에 저장합니다.
    - 자동차 이름을 기준으로 자동차 객체를 생성합니다.
    - 자동차 이동 시도 횟수 문구를 출력합니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,35 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.context.Car;
+import racingcar.service.CarGroup;
+import racingcar.service.CarNameGroup;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+import racingcar.view.PrintMessage;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView.printMessageWithLine(PrintMessage.INPUT_CAR_NAME_MESSAGE);
+        String carName = InputView.inputUser();
+
+        //컨트롤러 영역에서 해야 할 일
+        //1. 이름 입력 받아 쉼표 기준 분리
+        CarNameGroup splitCarName = new CarNameGroup(carName);
+        List<String> carNameGroup = splitCarName.getSplitCarNameGroup();
+
+        //2. 이름 검증(공백 제거, 5자 이하 검증)
+        //3. Car List 생성
+        CarGroup createCarGroup = new CarGroup(carNameGroup);
+        List<Car> carGroup = createCarGroup.getCarGroup();
+
+        //라운드 수 입력 받기
+        OutputView.printMessageWithLine(PrintMessage.INPUT_TRY_CAR_MOVEMENT_MESSAGE);
+        //4. 라운드 수만큼 반복하면서 자동차 이동 시도
+        //자동차 경기 현황 출력
+
+        //5. 경기 결과 판단
+        //자동차 경기 결과 출력
+
     }
 }
