4526aeeed5283bc89895597345fa8fc31cc647ad
Ju-Hyun-J <jujung967@gmail.com>
2024-10-26 01:02:55 +0900

    들여쓰기 줄임 next>> 제공된 라이브러리 이외의 외부 라이브러리 사용한듯 이거 파악헤야됨

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -21,29 +21,45 @@ public class Application {
             if (CarNames[i].length() > 5) {
                 throw new IllegalArgumentException();
             }
-            carRaceSituation.put(CarNames[i], "");
+            carRaceSituation.put(CarNames[i], ""); // 0 -> 시작지점
         }
 
+        // indent(인덴트, 들여쓰기) depth를 3이 넘지 않도록 구현한다. 2까지만 허용한다.
         System.out.println("실행 결과");
         for (int i = 0; i < RaceCount; i++) {
             for (int j = 0; j < CarNames.length; j++) {
-                if (Randoms.pickNumberInRange(0, 9) > 4) { // 4이상이면 - 추가 (전진)
-                    carRaceSituation.put(CarNames[j], carRaceSituation.get(CarNames[j]).concat("-"));
-                }
+                carRaceSituation.put(CarNames[j], carRaceSituation.get(CarNames[j]).concat(movement(CarNames[j]))); // 기본값에 movement 값 추가
                 System.out.println(CarNames[j] + " : " + carRaceSituation.get(CarNames[j]));
             }
             System.out.println();
         }
+
+
+        // indent(인덴트, 들여쓰기) depth를 3이 넘지 않도록 구현한다. 2까지만 허용한다.
         System.out.print("최종 우승자 : ");
         int winners = 0;
         for (String key : carRaceSituation.keySet()) {
             if (carRaceSituation.get(key).equals(Collections.max(carRaceSituation.values()))) {
                 winners++;
-                if (winners > 1) {
-                    System.out.print(", " + key);
-                } else System.out.print(key);
+                separator(key, winners);
             }
         }
 
     }
+
+    public static String movement(String CarNames) {
+        String movement = "";
+        if (Randoms.pickNumberInRange(0, 9) > 4) { // 4이상이면 - 추가 (전진)
+            movement = movement.concat("-");
+        }
+        return movement;
+    }
+
+    public static void separator(String key, int winners) {
+
+        if (winners > 1) {
+            System.out.print(", " + key);
+        } else System.out.print(key);
+    }
+
 }
