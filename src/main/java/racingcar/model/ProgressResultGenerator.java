de551ddb83ff788f964666a2f9669f35dc6deb64
Chan531 <chan657312@gmail.com>
2024-10-25 16:23:50 +0900

    feat(ProgressResultGenerator): 줄바꿈 상수 추가

--- src/main/java/racingcar/model/ProgressResultGenerator.java
+++ src/main/java/racingcar/model/ProgressResultGenerator.java
@@ -4,6 +4,7 @@ public class ProgressResultGenerator {
 
 	private static final String CAR_MOVEMENT_SYMBOL = "-";
 	private static final String SEPARATOR = " : ";
+	private static final String LINE_SEPARATOR = "\n";
 
 	private static String generateCarStatus(Car car) {
 		return car.getName() + SEPARATOR + formatMovementStatus(car.getMovement());
