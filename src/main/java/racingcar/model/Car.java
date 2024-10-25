58865fcb3d31299313e2708f08770720b4b8d2ba
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-10-26 00:37:38 +0900

    test: 자동차 객체 생성 5자 이상 입력 예외 처리

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -5,6 +5,9 @@ public class Car {
 
     public Car(String name) {
         if(name==null) throw new IllegalArgumentException("이름이 null이 될 수 없습니다.");
+        if(name.length()>5){
+            throw new IllegalArgumentException("이름이 5자 이상 될 수 없습니다.");
+        }
         this.name = name;
     }
 }
--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -7,11 +7,16 @@ import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
     @Test
-    void 자동차_이름_null(){
+    void 자동차_이름_null_실패(){
         assertThatThrownBy(()->new Car(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름이 null이 될 수 없습니다.");
     }
 
     @Test
+    void 자동차_이름_길이가_5글자보다_큰_입력_실패(){
+        assertThatThrownBy(()->new Car("aaaaaa"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름이 5자 이상 될 수 없습니다.");
+    }
 }
