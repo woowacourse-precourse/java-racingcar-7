15ffa605a2bb980f2384597bc1b80af938fd9fed
Leejunho <junho767@naver.com>
2024-10-26 03:16:37 +0900

    feat: 출력 메서드 수정 및 메세지 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,13 +1,26 @@
 package racingcar.view;
 
-import racingcar.domain.Car;
+import racingcar.model.Car;
+
+import java.util.List;
+
+import static racingcar.model.Message.GET_WINNER_MESSAGE;
+import static racingcar.model.Message.RUN_RESULT_MESSAGE;
 
 public class OutputView {
 
-    // 결과 출력
-    public void printWinner(Car car){
+    // 시도 횟수별 결과 출력
+    public static void printRunResult(List<Car> cars){
 
     }
-    public void printResult(Car car){
+
+    // 차량별 중간 결과 출력
+    public static void printCarRunResult(Car car){
+
+    }
+
+    // 결과 출력
+    public static void printWinner(List<String> cars){
+
     }
 }
