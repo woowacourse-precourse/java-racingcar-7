4d2c60ac037b13b9ea3e398b4f373e5423b0953e
hanwool <hanwe92@gmail.com>
2024-10-25 21:06:49 +0900

    feat: test code

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.util.ErrorMessage;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -11,23 +12,82 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+    private static final String CARS = "KIA,HONDA,BMW";
+
 
     @Test
-    void 기능_테스트() {
+    void 경주_단수_우승자_테스트() {
         assertRandomNumberInRangeTest(
             () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                run(CARS, "2");
+                assertThat(output()).contains("KIA : --", "HONDA : ", "BMW : -", "최종 우승자 : KIA");
             },
-            MOVING_FORWARD, STOP
+            MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD, STOP, STOP
         );
     }
 
     @Test
-    void 예외_테스트() {
+    void 경주_복수_우승자_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run(CARS, "2");
+                    assertThat(output()).contains("KIA : --", "HONDA : ", "BMW : --", "최종 우승자 : KIA, BMW");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 자동차_이름_자리수_5초과_예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+            assertThatThrownBy(() -> runException(CARS+"FERRARI", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.CAR_NAME_LESS_THAN_FIVE.getMessage())
+        );
+    }
+
+    @Test
+    void 자동차_이름_빈칸_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(CARS + ", ,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(ErrorMessage.CAR_NAME_CAN_NOT_BLANK_OR_EMPTY.getMessage())
+        );
+    }
+
+    @Test
+    void 자동차_이름_공백_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(CARS + ",,FERRARI", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(ErrorMessage.CAR_NAME_CAN_NOT_BLANK_OR_EMPTY.getMessage())
+        );
+    }
+
+    @Test
+    void 자동차_이름_중복_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(CARS + ",KIA", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(ErrorMessage.CAR_NAME_DUPLICATED.getMessage() + "KIA")
+        );
+    }
+
+    @Test
+    void 시도_회수_음수_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(CARS, "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(ErrorMessage.TRY_NUMBER_MORE_THAN_ZERO.getMessage())
+        );
+    }
+
+    @Test
+    void 시도_회수_문자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(CARS, "a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(ErrorMessage.TRY_NUMBER_NOT_NUMBER.getMessage() + "a")
         );
     }
 
