fea897f164a413a7a76d37732792f12f4a9cf15a
JeongSH1 <jyt001532@gmail.com>
2024-10-25 14:18:52 +0900

    test(UserInput): 시도할 횟수 입력에 대한 테스트를 진행한다.
    테스트 사항
    - 시도 횟수가 양수 시 성공
    - 시도 횟수가 빈 입력일 때 예외
    - 시도 횟수가 null 입력일 시 예외
    - 시도 횟수가 음수 입력값일 때 예외
    - 시도 횟수가 숫자로 변환 불가능할 시 예외
    - 시도 횟수가 0인 경우 예외

--- src/test/java/racingcar/user/UserInputTest.java
+++ src/test/java/racingcar/user/UserInputTest.java
@@ -21,8 +21,8 @@ class UserInputTest {
     }
 
     @Test
-    void 빈_입력값_실패_테스트() {
-        // given
+    void 자동차_이름_입력이_빈_입력값일_때_예외_테스트() {
+        // given시
         String input = "";
 
         // then
@@ -31,7 +31,7 @@ class UserInputTest {
     }
 
     @Test
-    void null_입력값_실패_테스트() {
+    void 자동차_이름_입력이_null_입력값일_때_예외_테스트() {
         // given
         String input = null;
 
@@ -41,13 +41,80 @@ class UserInputTest {
     }
 
     @Test
-    void 자동차_이름이_5글자를_초과할_경우_실패_테스트() {
+    void 자동차_이름이_5글자를_초과할_경우_예외_테스트() {
         // given
         String input = "pobi,woni,jeongseho,jun";
 
         // then
         assertThatThrownBy(() -> userInput.readNamesOfCars(input)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름(\"jeongseho\")에 대한 형식이 잘못되었습니다.");
+    }
+
+    @Test
+    void 시도_횟수가_양수시_성공_테스트() {
+        // given
+        String input = "3";
+
+        // when
+        int result = userInput.readNumberOfAttempts(input);
+
+        // then
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void 시도_횟수_입력이_빈_입력값일때_예외_테스트() {
+        // given
+        String input = "";
+
+        // then
+        assertThatThrownBy(() -> userInput.readNumberOfAttempts(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력이 비어있을 수 없습니다.");
+    }
+
+    @Test
+    void 시도_횟수_입력이_null_입력값일_때_예외_테스트() {
+        // given
+        String input = null;
+
+        // then
+        assertThatThrownBy(() -> userInput.readNumberOfAttempts(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력이 비어있을 수 없습니다.");
+    }
+
+    @Test
+    void 시도_횟수_입력이_음수_입력값일_때_예외_테스트() {
+        // given
+        String input = "-1";
+
+        // then
+        assertThatThrownBy(() -> userInput.readNumberOfAttempts(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도할 횟수(\"-1\")에 대한 형식이 잘못되었습니다.");
+    }
+
+    @Test
+    void 시도_횟수_입력이_숫자가_아닌_문자열_입력일_때_예외_테스트() {
+        // given
+        String input = "three";
+
+        // then
+        assertThatThrownBy(() -> userInput.readNumberOfAttempts(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도할 횟수(\"three\")에 대한 형식이 잘못되었습니다.");
+    }
+
+    @Test
+    void 시도_횟수가_0인_경우_예외_테스트() {
+        // given
+        String input = "0";
+
+        // then
+        assertThatThrownBy(() -> userInput.readNumberOfAttempts(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도할 횟수(\"0\")에 대한 형식이 잘못되었습니다.");
     }
 
 }
