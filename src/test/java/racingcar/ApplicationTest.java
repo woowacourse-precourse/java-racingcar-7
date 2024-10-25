3ebf97f01cec6efc804202028301c01604179310
Fletacode <bassykd@gmail.com>
2024-10-25 15:51:17 +0900

    test: 입력받는 기능 테스트 케이스 추가
    입력받는 기능에 대해 테스트 케이스를 추가하였습니다.
    자동차 문자열은 빈문자열 x, 구분자 , 이여야합니다.
    자동차 문자는 문자타입이여야합니다.
    숫자는 0이거나 음수이면 안됩니다.
    숫자는 정수타입이여야합니다.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,9 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.error.ErrorMessage;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -9,30 +11,88 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
 
-    @Test
-    void 기능_테스트() {
-        assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
-        );
-    }
+  private static final int MOVING_FORWARD = 4;
+  private static final int STOP = 3;
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
+  @Test
+  @DisplayName("자동차_빈문자열")
+  void emptyCarString() {
+    assertSimpleTest(() -> {
 
-    @Override
-    public void runMain() {
-        Application.main(new String[]{});
-    }
+      assertSimpleTest(() ->
+          assertThatThrownBy(() -> runException("\n"))
+              .isInstanceOf(IllegalArgumentException.class)
+              .hasMessage(ErrorMessage.EMPTY_CAR_STRING.getMessage())
+
+      );
+
+      assertSimpleTest(() ->
+          assertThatThrownBy(() -> runException(" "))
+              .isInstanceOf(IllegalArgumentException.class)
+
+      );
+
+    });
+  }
+
+  @Test
+  @DisplayName("자동차_구분자없는문자열")
+  void noDelimiterCarString() {
+
+    assertSimpleTest(() ->
+        assertThatThrownBy(() -> runException("kim|dong|he"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.NO_DELIMITER_CAR_STRING.getMessage())
+
+    );
+
+  }
+
+  @Test
+  @DisplayName("유효하지 않은 횟수")
+  void NoValidateNums() {
+
+    assertSimpleTest(() -> {
+
+      //숫자가 아닌 문자열
+      assertSimpleTest(() ->
+          assertThatThrownBy(() -> runException("kim,dong", "e"))
+              .isInstanceOf(IllegalArgumentException.class)
+              .hasMessage(ErrorMessage.NO_NUMBER_COUNT_STRING.getMessage())
+      );
+
+      //정수가 아님
+      assertSimpleTest(() ->
+          assertThatThrownBy(() -> runException("kim,dong", "1.24"))
+              .isInstanceOf(IllegalArgumentException.class)
+              .hasMessage(ErrorMessage.NO_NUMBER_COUNT_STRING.getMessage())
+      );
+
+      //0임
+      assertSimpleTest(() ->
+          assertThatThrownBy(() -> runException("kim,dong", "0"))
+              .isInstanceOf(IllegalArgumentException.class)
+              .hasMessage(ErrorMessage.NO_NUMBER_COUNT_STRING.getMessage())
+      );
+
+      //음수임
+      assertSimpleTest(() ->
+          assertThatThrownBy(() -> runException("kim,dong", "-2"))
+              .isInstanceOf(IllegalArgumentException.class)
+              .hasMessage(ErrorMessage.NO_NUMBER_COUNT_STRING.getMessage())
+      );
+
+    });
+
+  }
+
+
+
+
+
+  @Override
+  public void runMain() {
+    Application.main(new String[]{});
+  }
 }
