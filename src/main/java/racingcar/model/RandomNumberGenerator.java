b88c390ac07b7629ac9208acce6ec234dd2bef41
supernovaMK <minki5336@naver.com>
2024-10-26 02:19:40 +0900

    feat: 랜덤한 숫자를 만들어내는 기능 구현
    - 각 자동차가 사용할 랜덤한 숫자를 만들어내기 위해 RandomNumberGenerator 클래스를 구현하였습니다.

---
+++ src/main/java/racingcar/model/RandomNumberGenerator.java
@@ -0,0 +1,9 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+    public static int generateNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+}
