b62ddd475bfd341266e61ba7fa22e1b80bb7b1ff
woodongle <315ehdgur@gmail.com>
2024-10-25 22:27:40 +0900

    feat: 사용자 입력, 사용자 입력 검증 메서드 추가
    1. 경주할 자동차 이름을 입력받고 그 입력에 대해 특수 문자와 공백이 있는지 확인하는 메서드를 추가합니다.
      - validateInput()

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 경주할 자동차 이름 입력(참가자 수)
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+        try {
+            validateInput(input);
+            System.out.println(input);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    // 사용자 입력에 특수 문자와 공백이 있는지 확인하는 메서드
+    private static void validateInput(String input) {
+        if (input.matches(".*[ \'`%@:=>$_#+\\[;~!(*)\\]&?}\\{/.<|\\-\"^\\\\].*")) {
+            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+        }
     }
 }
