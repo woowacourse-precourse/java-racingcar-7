042a85d47e8f67893c55336958ebaa6557439c82
unh6unh6 <ruddbs3308@naver.com>
2024-10-25 15:53:10 +0900

    feat(view): 사용자로부터 경주할 자동차 이름 입력 받기

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,8 +1,14 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     private static final String CAR_NAME_INPUT_GUIDE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public void printCarNameInputGuide() {
         System.out.println(CAR_NAME_INPUT_GUIDE);
+    }
+
+    public void getCarNameInput() {
+        Console.readLine();
     }
 }
