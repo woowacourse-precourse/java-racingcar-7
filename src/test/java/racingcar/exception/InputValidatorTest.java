a9626c897bfde9400edfd5befb32490f8aef9318
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-25 21:41:40 +0900

    test: 이름 길이 검증 메서드에 대한 테스트 코드 작성 및 실행
    이름 길이 검증 메서드의 정상 작동을 확인하기 위해 validateInvalidCarName에 대한 테스트 코드를 작성 후 실행

--- src/test/java/racingcar/exception/InputValidatorTest.java
+++ src/test/java/racingcar/exception/InputValidatorTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputValidatorTest {
     private static final InputValidator inputvalidator = new InputValidator();
@@ -26,6 +27,22 @@ class InputValidatorTest {
     @DisplayName("공백이 입력되지 않으면 에러가 발생하지 않는다.")
     void whiteSpacesInputNotThrowException() {
         assertThatCode(() -> inputvalidator.validateWhiteSpaces("pobi,woni,jun", "5"))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @DisplayName("이름의 길이가 5를 넘어갈 경우 예외가 발생한다.")
+    @ValueSource(strings = {"maxwell", "pascal", "turing"})
+    void invalidCarNameThrowException(String carName) {
+        assertThatThrownBy(() -> inputvalidator.validateInvalidCarName(carName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("이름의 길이가 5를 넘어가지 않을 경우 예외가 발생하지 않는다.")
+    @ValueSource(strings = {"pobi", "woni", "jun", "tesla"})
+    void validCarNameNotThrowException(String carName) {
+        assertThatCode(() -> inputvalidator.validateInvalidCarName(carName))
                 .doesNotThrowAnyException();
     }
 
