c0a38ef77014b2570779ccc1fa20f5d3fa19d09d
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-10-27 03:13:57 +0900

    refactor(Util): 메소드의 파라미터 변경

--- src/main/java/racingcar/Util.java
+++ src/main/java/racingcar/Util.java
@@ -5,26 +5,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Util {
-   public static boolean isValidRoundNumber(Input input) {
-       try{
-           Integer.parseInt(input.getRounds());
-           return true;
-       } catch (NumberFormatException e) {
-           return false;
-       }
-   }
+    public static boolean isValidRoundNumber(String roundNumber) {
+        try {
+            Integer.parseInt(roundNumber);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
-    public static boolean isValidCarNamesInput(Input input) {
-       String regex = "^([\\s\\w\\uAC00-\\uD7A3\\p{Punct}]{1,5})(,(?!$)[\\s\\w\\uAC00-\\uD7A3\\p{Punct}]{1,5})*$|^([\\s\\w\\uAC00-\\uD7A3\\p{Punct}]{1,5})$";
-       Pattern pattern = Pattern.compile(regex);
-       Matcher matcher = pattern.matcher(input.getCarNames());
-       return matcher.matches();
+    public static boolean isValidCarNamesInput(String carNames) {
+        String regex = "^([\\s\\w\\uAC00-\\uD7A3\\p{Punct}]{1,5})(,(?!$)[\\s\\w\\uAC00-\\uD7A3\\p{Punct}]{1,5})*$|^([\\s\\w\\uAC00-\\uD7A3\\p{Punct}]{1,5})$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(carNames);
+        return matcher.matches();
     }
 
     public static boolean hasDuplicates(String[] carNames) {
         HashSet<String> set = new HashSet<>();
-        for(String str : carNames) {
-            if(!set.add(str)){
+        for (String str : carNames) {
+            if (!set.add(str)) {
                 return true;
             }
         }
