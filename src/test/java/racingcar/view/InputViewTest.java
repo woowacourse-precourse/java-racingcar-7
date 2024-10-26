2e819ba5d37dc1d57623a2de6b64e7145bdb33e9
Leegeonwoo7 <workgeon07@naver.com>
2024-10-27 02:30:54 +0900

    test(InputViewTest): InputView테스트 구현

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -12,7 +12,6 @@ import static org.assertj.core.api.Assertions.*;
 class InputViewTest {
 
     InputView inputView = new InputView();
-
     private final InputStream originalSystemIn = System.in;
 
     @AfterEach
@@ -20,6 +19,7 @@ class InputViewTest {
         System.setIn(originalSystemIn);
     }
 
+    @Disabled
     @Test
     @DisplayName("자동차 이름목록을 테스트한다")
     void test() {
@@ -36,45 +36,4 @@ class InputViewTest {
                 .containsExactlyInAnyOrder("pobi", "woni", "jun");
     }
 
-    @Test
-    @DisplayName("자동차 이름의 길이가 6글자가 넘으면 예외가 발생한다")
-    void checkLengthWithinLimits() {
-        //given
-        String input = "pobiiii,woni,jun\n";
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
-        System.setIn(inputStream);
-
-        //when //then
-        assertThatThrownBy(() -> inputView.readCarNames())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차 이름의 길이는 최대 5글자입니다");
-    }
-    
-    @Test
-    @DisplayName("중복되는 자동차 이름이 있으며 예외가 발생한다")
-    void hasDuplicatedCarName() {
-        //given
-        String input = "pobi,pobi,jun\n";
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
-        System.setIn(inputStream);
-
-        //when //then
-        assertThatThrownBy(() -> inputView.readCarNames())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("중복되는 자동차 이름이 있습니다");
-    }
-
-    @Test
-    @DisplayName("자동차 이름으로 공백이 있으면 예외가 발생한다")
-    void hasValidDelimiter() {
-        //given
-        String input = "pobi,,jun";
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
-        System.setIn(inputStream);
-
-        //when //then
-        assertThatThrownBy(() -> inputView.readCarNames())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차 이름으로 공백이 허용되지 않습니다");
-    }
 }
