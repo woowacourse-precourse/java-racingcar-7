f360dfd2360f23985e3c8979fa10bce1e6c2335d
lsj0822 <lsj0822@khu.ac.kr>
2024-10-26 20:11:00 +0900

    test(ExceptionHandlerTest) : 기능 테스트
    -ExceptionHandler의 메소들에 의해 예외 처리가 될만한 예시를 테스트

---
+++ src/test/java/exceptor/ExceptionHandlerTest.java
@@ -0,0 +1,87 @@
+package exceptor;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import constant.Constants;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.Application;
+
+public class ExceptionHandlerTest  extends NsTest {
+
+    @DisplayName("정상_입력_테스트")
+    @Test
+    void normal_input_test() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi", "1");
+                    assertThat(output()).contains(Constants.RACING_START_MESSAGE);
+                },
+                3
+        );
+    }
+
+    @DisplayName("실수_입력_예외_테스트")
+    @Test
+    void float_exception_test() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi","1.2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("문자열_입력_예외_테스트")
+    @Test
+    void letter_exception_test() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi","abc"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("음수_입력_예외_테스트")
+    @Test
+    void negative_integer_exception_test() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi","-3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("글자수_초과입력_예외_테스트")
+    @Test
+    void name_limit_exception_test() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobipobi,wonywony,junjun","3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("중복_입력_예외_테스트")
+    @Test
+    void duplicate_exception_test() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,pobi","3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("중복_입력_예외_성능_테스트")
+    @Test
+    void duplicate_exception_runtime_test() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,".repeat(1000000)+"pobi","3"))
+                        .isInstanceOf(IllegalArgumentException.class) // 약 200만개 이상의 배열을 처리할 때 limit 초과
+        );
+    }
+
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
