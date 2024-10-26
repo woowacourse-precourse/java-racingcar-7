32b403c291d4d24f45b4abb61e94702e793219bf
yooo <ghddbtl@gmail.com>
2024-10-26 19:33:53 +0900

    refactor: remove unused method from CarDto
    사용하지 않는 메서드 삭제
    setter 다 삭제함

--- src/main/java/racingcar/model/CarDto.java
+++ src/main/java/racingcar/model/CarDto.java
@@ -2,34 +2,25 @@ package racingcar.model;
 
 public class CarDto {
     private String name;
-    private int id = 0;
+    private int id;
     private int position = 0;
 
-    public CarDto(String name) {
+    public CarDto(String name, int id) {
         this.name = name;
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public int getPosition() {
         return position;
-    }
-
-    public void setPosition(int position) {
-        this.position = position;
     }
 }
