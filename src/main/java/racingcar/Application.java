95bd5fb63e3f9f4890b73ededb8987e75c1b6044
badgelatte <saucy5939@gmail.com>
2024-10-27 03:53:23 +0900

    SplitNames class 추가
    입력된 자동차 이름을 ,를 기준으로 나누는 클래스 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,12 +2,10 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
 
@@ -15,7 +13,7 @@ public class Application {
         String attemptCountString = Console.readLine();
         int attemptCount = Integer.parseInt(attemptCountString);
 
-        List<Car> carList = splitCarNames(carNames, attemptCount);
+        List<Car> carList = SplitNames.splitCarNames(carNames, attemptCount);
 
         Race race = new Race();
 
@@ -23,20 +21,5 @@ public class Application {
 
     }
 
-    public static List<Car> splitCarNames(String carNames, int attemptCount) {
-        String[] carNamesArray = carNames.split(",");
-        if (carNamesArray.length < 2) {
-            throw new IllegalArgumentException("자동차 1개 입력");
-        }
-        List<Car> carNameList = new ArrayList<>();
 
-        for (String carName : carNamesArray) {
-            if (carName.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름 5자 초과");
-            }
-            carNameList.add(new Car(carName, attemptCount));
-        }
-
-        return carNameList;
-    }
 }
---
+++ src/main/java/racingcar/SplitNames.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SplitNames {
+    public static List<Car> splitCarNames(String carNames, int attemptCount) {
+        String[] carNameArray = carNames.split(",");
+        checkOneCar(carNameArray);
+
+        List<Car> carNameList = new ArrayList<>();
+
+        for (String carName : carNameArray) {
+            checkNameLength(carName);
+            carNameList.add(new Car(carName, attemptCount));
+        }
+
+        return carNameList;
+    }
+
+    public static void checkOneCar(String[] carNameArray) {
+        if (carNameArray.length < 2) {
+            throw new IllegalArgumentException("자동차 1개 입력");
+        }
+    }
+
+    public static void checkNameLength(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름 5자 초과");
+        }
+    }
+}
