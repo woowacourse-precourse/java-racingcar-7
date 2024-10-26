898e01cc8fcbf45edbcb667afb9546f4c441028a
xeimun <hinote444@naver.com>
2024-10-26 21:38:17 +0900

    feat(Racing): 가장 많이 전진한 자동차를 구하는 기능 구현

--- src/main/java/racingcar/model/Racing.java
+++ src/main/java/racingcar/model/Racing.java
@@ -1,8 +1,15 @@
 package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 public class Racing {
+    
+    public List<Car> findWinners(List<Car> cars, int maxMarkerCount) {
+        return cars.stream()
+                   .filter(car -> car.getAdvanceMarkerCount() == maxMarkerCount)
+                   .toList();
+    }
 
     public void advance(Cars cars) {
         for (Car car : cars.getCars()) {
