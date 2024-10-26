30882ced8dfdd54af92119e0d85a2bdc8d0bc29f
mhj1205 <lightnow1205@gmail.com>
2024-10-26 22:07:27 +0900

    refactor: 경주하는 메소드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,55 +5,10 @@ import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
-            int max = -999,winner_cnt=0;
-            ArrayList<String> winner = new ArrayList<>();
-            Scanner sc = new Scanner(System.in);
 
             String[] cars = getCars();
-
             int attempts = getAttempts();
-
-            int[] racing_num = new int[cars.length];
-
-            //0~9 사이에 랜덤한 숫자 구하기
-            Random random = new Random();   
-    
-            System.out.println("실행 결과");
-            // 경주 하기
-            for (int i=0; i<attempts; i++){
-                for (int j=0; j<cars.length; j++){
-                    int advance_number = random.nextInt(10);
-                    if (advance_number > 4){
-                        racing_num[j] += 1;
-                    }
-                }
-                for (int j=0; j<cars.length; j++){
-                    System.out.print(cars[j] + " : ");
-                    for (int k=0; k<racing_num[j]; k++){
-                        System.out.print("-");
-                    }
-                    System.out.println();
-                }
-                System.out.println();
-            }
-
-            // 최대값 구하기
-            for (int i=0; i<cars.length; i++){
-                if (racing_num[i] > max){
-                    max = racing_num[i];
-                }
-            }   
-            // 최종 우승자 선정하기
-            for (int i=0; i<cars.length; i++){
-                if (racing_num[i] == max){
-                    winner.add(cars[i]);
-                    winner_cnt += 1;
-                }
-            }
-
-            // 최종 우승자 출력하기
-            String winner_str = String.join(", ", winner);
-            System.out.println("최중 우승자 : " + winner_str);
+            int[] racingNum = race(cars, attempts);
             
     }   
     // 자동차 이름 입력받기
@@ -83,5 +38,26 @@ public class Application {
             throw new IllegalArgumentException("이동 횟수는 1 이상이어야 합니다.");
         }
         return attempts;
+    }
+
+    // 경주 로직
+    private static int[] race(String[] cars, int attempts) {
+        Random random = new Random();
+        int[] racingNum = new int[cars.length];
+
+        System.out.println("실행 결과");
+        for (int i = 0; i < attempts; i++) {
+            for (int j = 0; j < cars.length; j++) {
+                if (shouldAdvance(random)) {
+                    racingNum[j]++;
+                }
+            }
+        }
+        return racingNum;
+    }
+
+    // 전진 조건 체크
+    private static boolean shouldAdvance(Random random) {
+        return random.nextInt(10) >= 4;
     }
 }
