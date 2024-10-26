ded9f451893a038361e609356e3476fa49895297
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-26 18:08:41 +0900

    feat(model): Cars wrapper 클래스 생성 - Car 객체 목록을 관리하는 Cars Wrapper 클래스 생성 - Car를 위치에 따라 내림차순 정렬하는 함수 구현 - 우승자 리스트 반환 함수 구현

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,2 +1,37 @@
-package racingcar.model;public class Cars {
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars = new ArrayList<>();
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public List<Car> getWinners() {
+        sortCarsByPosition();
+        int maxPosition = cars.getFirst().getPosition();
+        List<Car> winners = new ArrayList<>();
+        for (Car winner : cars) {
+            if (winner.getPosition() < maxPosition) {
+                break;
+            }
+            winners.add(winner);
+        }
+
+        return winners;
+
+    }
+
+    private void sortCarsByPosition() {
+        cars.sort((car1, car2) -> Integer.compare(car2.getPosition(), car1.getPosition()));
+    }
+
+
 }
