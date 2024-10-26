1de8edab537b8ce96094ac4f1fb98048ec7bc4aa
차왕준 <ckdhdkwns@gmail.com>
2024-10-26 18:04:13 +0900

    feat(InputView): 쉼표를 기준으로 각 이름을 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,12 +1,20 @@
 package racingcar.view;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
-    public void readCarNames() {
+    private static final String DELIMITER = ",";
+
+    public List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = readLine();
+        return parseCarNames(input);
+    }
+
+    private List<String> parseCarNames(String input) {
+        return Arrays.asList(input.split(DELIMITER));
     }
 }
