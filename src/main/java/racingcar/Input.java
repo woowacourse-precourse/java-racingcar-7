3b3ddb98ae7ff4eee78029f59a7afef463e8ec82
Avocado <ujubongbong@gmail.com>
2024-10-26 19:07:57 +0900

    :sparkles: feat: 이름이 5자 이하인지 확인하는 함수

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import org.junit.platform.commons.util.StringUtils;
 
+import java.util.Arrays;
+
 public class Input {
     public void validateBlank(String input) {
         if (input.contains(" ")) {
@@ -12,6 +14,13 @@ public class Input {
     public void validateEmpty(String input) {
         if (StringUtils.isBlank(input)) {
             throw new IllegalArgumentException("문자열 입력이 필요합니다.");
+        }
+    }
+
+    public void validateNameLength(String input) {
+        if (Arrays.stream(input.split(","))
+                .anyMatch(name -> name.length() > 5)) {
+            throw new IllegalArgumentException("차 이름은 5자 이하로 작성해야 합니다.");
         }
     }
 }
