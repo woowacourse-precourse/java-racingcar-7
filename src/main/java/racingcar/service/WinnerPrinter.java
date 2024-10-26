3d96849a08f5db42d23733f397137345a99f37e1
Shinjongyun <tigerjong123@gmail.com>
2024-10-27 04:21:43 +0900

    refact(WinnerPrinter): Winner를 계산하는 로직 분리

--- src/main/java/racingcar/service/WinnerPrinter.java
+++ src/main/java/racingcar/service/WinnerPrinter.java
@@ -1,39 +1,24 @@
 package racingcar.service;
 
 import racingcar.entity.Car;
+import racingcar.entity.Winner;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class WinnerPrinter {
 
     public void printWinner(List<Car> carList) {
-        int maxLength = 0;
+
         StringBuilder winnerNames = new StringBuilder();
+        Winner winner = winnerCalculator.calculateWinner(carList);
 
-        List<Car> winners = new ArrayList<>();
-
-        // 모든 자동차의 도로 길이를 비교
-        for (Car car : carList) {
-            int roadLength = car.getRoad().getLength(); // Car 객체의 Road 길이 가져오기
-
-            // 새로운 최대 길이를 찾은 경우
-            if (roadLength > maxLength) {
-                maxLength = roadLength; // 최대 길이 갱신
-                winners.clear(); // 이전의 우승자 리스트 비우기
-                winners.add(car); // 새로운 우승자 추가
-            } else if (roadLength == maxLength) {
-                winners.add(car); // 현재 길이가 최대와 같다면 우승자 리스트에 추가
-            }
-        }
-
-        System.out.print("최종우승자 : ");
-        for (int i = 0; i < winners.size(); i++) {
-            winnerNames.append(winners.get(i).getName());
-            if (i < winners.size() - 1) { // 마지막 이름이 아닐 경우에만 쉼표 추가
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < winner.getWinningCars().size(); i++) {
+            winnerNames.append(winner.getWinningCars().get(i).getName());
+            if (i < winner.getWinningCars().size() - 1) { // 마지막 이름이 아닐 경우에만 쉼표 추가
                 winnerNames.append(", ");
             }
         }
-        System.out.println(winnerNames); // 모든 우승자 이름 출력
+        System.out.println(winnerNames);
     }
 }
