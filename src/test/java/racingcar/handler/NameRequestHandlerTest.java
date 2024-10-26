b5a0446be592c71d40b1ca2ea9a28d92e3a8c1d9
lyouxsun <leedrkr323@konkuk.ac.kr>
2024-10-26 13:13:39 +0900

    test(NameRequestHandlerTest): 띄어쓰기 포함 입력 문자열 검증 단위테스트 구현
    - hasMessageContaining 메서드를 사용해서 지정한 예외 메시지와 일치하는지 검사

--- src/test/java/racingcar/handler/NameRequestHandlerTest.java
+++ src/test/java/racingcar/handler/NameRequestHandlerTest.java
@@ -4,28 +4,42 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static racingcar.message.ErrorMessage.BLANK_INPUT_ERROR;
+import static racingcar.message.ErrorMessage.CONTAINS_SPACE_ERROR;
+
 public class NameRequestHandlerTest {
 
     private final NameRequestHandler nameRequestHandler = new NameRequestHandler();
 
-    @DisplayName("빈 문자열 테스트")
+    @DisplayName("빈 문자열 검증")
     @Test
     public void blankInputValidateTest(){
         Assertions.assertThatThrownBy(() -> nameRequestHandler.validateBlank(""))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(BLANK_INPUT_ERROR.getMessage());
     }
 
-    @DisplayName("공백 문자열 테스트")
+    @DisplayName("공백 문자열 검증")
     @Test
     public void EmptyInputValidateTest(){
         Assertions.assertThatThrownBy(() -> nameRequestHandler.validateBlank(" "))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(BLANK_INPUT_ERROR.getMessage());
     }
 
-    @DisplayName("null 테스트")
+    @DisplayName("null 검증")
     @Test
     public void nullInputValidateTest(){
         Assertions.assertThatThrownBy(() -> nameRequestHandler.validateBlank(null))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(BLANK_INPUT_ERROR.getMessage());
+    }
+
+    @DisplayName("띄어쓰기 포함 문자열 검증")
+    @Test
+    public void containsSpaceValidateTest(){
+        Assertions.assertThatThrownBy(() -> nameRequestHandler.validateSpace("as d,ds"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(CONTAINS_SPACE_ERROR.getMessage());
     }
 }
