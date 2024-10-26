c3e918c18a6646b03217d416a3f65a4888e89c9a
wogus <ind07152@naver.com>
2024-10-26 17:02:33 +0900

    refactor(Parser): Parser 기본 기호 상수화
    Parser 기본 기호들을 SymbolType으로 상수화한다.

--- src/main/java/racingcar/utils/Parser.java
+++ src/main/java/racingcar/utils/Parser.java
@@ -1,12 +1,14 @@
 package racingcar.utils;
 
+import static racingcar.constant.SymbolType.CAR_NAME_DELIMITER;
+
 import java.util.Arrays;
 import java.util.List;
 import racingcar.validator.Validator;
 
 public final class Parser {
     public static List<String> parse(String input) {
-        List<String> names = Arrays.stream(input.split(",")).map(String::trim).toList();
+        List<String> names = Arrays.stream(input.split(CAR_NAME_DELIMITER.getSymbol())).map(String::trim).toList();
         Validator.namesValidate(names);
         return names;
     }
