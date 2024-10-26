22ba947635b8700646f8a48071f86e522f0de73b
MinJae00 <exceedmin@gmail.com>
2024-10-27 00:58:54 +0900

    feat: An object that stores the user's name and score

--- src/main/java/racingcar/user/UserInfo.java
+++ src/main/java/racingcar/user/UserInfo.java
@@ -1,2 +1,13 @@
-package racingcar.user;public class UserInfo {
+package racingcar.user;
+
+public class UserInfo {
+    public String name;
+    public String score;
+    public int scoreNum = 0;
+
+    public UserInfo(String name) {
+        this.name = name;
+        this.score = "";
+        this.scoreNum = 0;
+    }
 }
