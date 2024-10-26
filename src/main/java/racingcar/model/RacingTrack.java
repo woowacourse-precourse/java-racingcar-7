498a5e699c4b7c3a4a7d30c6ec9337ba3ff8806c
sanghee0820 <lsh277604@gmail.com>
2024-10-26 23:45:55 +0900

    feat: 게임 단계 진행 기능 추가

--- src/main/java/racingcar/model/RacingTrack.java
+++ src/main/java/racingcar/model/RacingTrack.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.exception.RacingTrackException;
@@ -7,22 +8,29 @@ import racingcar.exception.RacingTrackException;
 public class RacingTrack {
     private final List<Car> cars;
 
-    public RacingTrack(){
+    public RacingTrack() {
         this.cars = new ArrayList<>();
     }
 
-    public void addCar(List<String> carNames){
-        for(String carName : carNames){
+    public void addCar(List<String> carNames) {
+        for (String carName : carNames) {
             checkDuplicatedCarName(carName);
             cars.add(new Car(carName));
         }
     }
 
-    public void checkDuplicatedCarName(String name){
+    public void checkDuplicatedCarName(String name) {
         var duplicatedName = cars.stream()
                 .anyMatch(car -> car.getName().equals(name));
-        if(duplicatedName){
+        if (duplicatedName) {
             throw new IllegalArgumentException(RacingTrackException.DUPLICATED_CAR_NAME.getMessage());
+        }
+    }
+
+    public void step() {
+        for (Car car : cars) {
+            var seed = Randoms.pickNumberInRange(0, 9);
+            car.move(seed);
         }
     }
 }
