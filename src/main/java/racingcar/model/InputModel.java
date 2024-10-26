c9976c478e0fd099909bc6bb3c3dc57e676b1860
(minjae8563) <(minjae8563@naver.com)>
2024-10-26 18:44:33 +0900

    feat: 사용자에게 입력을 받는 InputModel 클래스 구현

--- src/main/java/racingcar/model/InputModel.java
+++ src/main/java/racingcar/model/InputModel.java
@@ -1,2 +1,23 @@
-package racingcar.model;public class InputModel {
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.validation.Parser;
+
+public class InputModel {
+    String carNames ;
+    public InputModel(){
+    }
+    public String getName(){
+        this.carNames = Console.readLine();
+        return carNames;
+    }
+    public String [] getInput(){
+        return carNames.split(",");
+    }
+    public int getPlayTime(){
+        Parser parser = new Parser();
+        String play_times = Console.readLine(); 
+        return(parser.stringToInt(play_times));
+    }
+
 }
