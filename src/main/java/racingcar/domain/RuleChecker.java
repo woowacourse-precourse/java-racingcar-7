78aaaac36ab76841e86b3e257df64f9c704b2027
rootTiket <l20078829@gmail.com>
2024-10-26 23:00:42 +0900

    feat: 랜덤한 수를 뽑고 조건을 판별하는 메서드 구현

---
+++ src/main/java/racingcar/domain/RuleChecker.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RuleChecker {
+
+    public static boolean canMove() {
+        return Randoms.pickNumberInRange(0, 9) > 4;
+    }
+
+}
