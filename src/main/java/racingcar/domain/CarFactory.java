bd0a334740784ca6d165eab36e383f94e63ad16f
soeunnPark <carol0724@naver.com>
2024-10-26 21:20:16 +0900

    feat(CarFactory): Generate cars by car names
    자동차 이름 리스트를 가지고 자동차 객체들을 생성하는 팩토리를 구현했습니다.

---
+++ src/main/java/racingcar/domain/CarFactory.java
@@ -0,0 +1,19 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarFactory {
+
+    public List<Car> makeCars(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        for(String carName : carNames) {
+            cars.add(makeCar(carName));
+        }
+        return cars;
+    }
+
+    private Car makeCar(String carName) {
+        return new Car(carName);
+    }
+}
