9fa05db74fa6542fe089fca39751c289981f45e8
dhwoo <wooda01@naver.com>
2024-10-26 14:12:51 +0900

    style: displayRaceStatus 출력 형식에서 콜론 주변에 공백 추가

--- src/main/java/racingcar/view/ConsoleView.java
+++ src/main/java/racingcar/view/ConsoleView.java
@@ -27,7 +27,7 @@ public class ConsoleView {
 
     public void displayRaceStatus(List<Car> cars) {
         for (Car car : cars) {
-            System.out.println(car.getName() + ": " + "-".repeat(car.getPosition()));
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
         }
         System.out.println();
     }
