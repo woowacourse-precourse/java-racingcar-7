1b0da98917e75909c1c16803e2a45b28bd715711
eundeang <111877048+eundeang@users.noreply.github.com>
2024-10-26 16:46:14 +0900

    test(CarTest): 이름 수 제한에 통과하는 테스트 작성

---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,20 @@
+package racingcar.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    private final Car car = new Car();
+
+    @Test
+    @DisplayName("자동차 이름이 5글자 이하인 경우 이름 리스트 반환")
+    void 이름_수_제한_통과_테스트() {
+        Assertions.assertThat(car.splitByDelimiter("four,fivee"))
+                .hasSize(2)
+                .extracting(car -> car.getName())
+                .contains("four", "fivee");
+    }
+
+}
