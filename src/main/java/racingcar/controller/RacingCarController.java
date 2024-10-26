b89f1291e23c298e5a8b0d2f556b8483ebc6a53f
skhnpark <skhnpark125@gmail.com>
2024-10-26 18:49:07 +0900

    feat: 자동차 이름 입력 안내 문구를 출력하는 기능 구현

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,4 +1,13 @@
 package racingcar.controller;
 
+import racingcar.view.OutputView;
+
 public class RacingCarController {
+    public void init() {
+        handleInputCarNames();
+    }
+
+    public void handleInputCarNames() {
+        OutputView.printCarNamesInputGuide();
+    }
 }
