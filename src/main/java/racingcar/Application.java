fe905d95f5b9a65c84845befb5b1ffdd41b5b5c2
hyeonjeong9911 <knk990101@naver.com>
2024-10-27 03:34:06 +0900

    feat: 자동차 이름 입력 받는 기능 구현
    사용자로부터 자동차 이름을 입력 받고, 쉼표(,) 기분으로 이름을 구분한다. 5자 이상일 경우 예외 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름을 쉼표(,) 기준으로 구분");
+
+        List<String> carNameList = List.of(readLine().split(","));
+        List<Car> carList = new ArrayList<>();
+
+        for (String name : carNameList) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException();
+            } else {
+                Car car = new Car(name);
+                carList.add(car);
+            }
+        }
     }
 }
