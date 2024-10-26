adab19ffce0f984779b8f07c3c10ddbebdb7223b
V4N1LLA <bubble1549@gmail.com>
2024-10-27 03:53:01 +0900

    feat: 게임 라운드 진행 로직 추가

---
+++ src/main/java/racingcar/OutputView.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import java.util.List;
+
+public class OutputView {
+    public static void printStartMessage() {
+        System.out.println("실행 결과");
+    }
+
+    public static void printRoundResult(List<Car> cars) {
+        // 라운드 결과 출력 로직은 다음 기능에서 구현
+    }
+}
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -9,6 +9,15 @@ public class RacingGame {
     public void start() {
         this.cars = InputView.getCars();
         this.attempts = InputView.getAttemptCount();
-        // 이후 로직은 다음 기능에서 구현
+
+        OutputView.printStartMessage();
+        runRounds();
+    }
+
+    private void runRounds() {
+        for (int i = 0; i < attempts; i++) {
+            // 각 라운드에서 자동차 이동은 다음 기능에서 구현
+            OutputView.printRoundResult(cars);
+        }
     }
 }
