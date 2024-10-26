6a8c0d304e054c63269a7dda16eec31740810295
jonghwanlee <kavae550@gmail.com>
2024-10-26 21:26:39 +0900

    feat(domain): Cars 클래스 기본 기능 구현
    - 입력값을 통해 Car 객체들을 생성해서 리스트에 담는 createCars() 구현
    - 한 라운드를 진행하는 proceedOneRound() 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,4 +1,30 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class Cars {
+    private List<Car> cars;
+
+    public Cars(List<String> carNames) {
+        this.cars = createCars(carNames);
+    }
+
+    public static Car createCar(String carName){
+        return new Car(carName);
+    }
+
+    public List<Car> createCars(List<String> carNames){
+
+        return carNames.stream()
+                .map(Cars::createCar)
+                .toList();
+    }
+
+    public void proceedOneRound(){
+        for (Car car : cars) {
+            if (MovementDecider.checkMoveOrNot()){
+                car.move();
+            }
+        }
+    }
 }
