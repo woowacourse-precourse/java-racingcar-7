8ce42559ce871c81dc2cf58a2c9c3674d84a605f
honggyu1233@naver.com <honggyu1233@naver.com>
2024-10-27 00:46:45 +0900

    feat : InputView 수정 - 반환 타입을 Object[]로 변경하여 이름과 이동 횟수 반환

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,7 +3,7 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static Object racingcarInputView() {
+    public static Object[] racingcarInputView() {
         String inputName=Console.readLine();
         int moveCount=Integer.parseInt(Console.readLine());
         return new Object[]{inputName, moveCount};
