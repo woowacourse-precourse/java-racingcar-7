7f6006186d76260e1daae0c17ee9cb026e7addbc
minseo <alstj778@gmail.com>
2024-10-27 02:05:19 +0900

    feat(InputView): 자동차 이름 입력 기능 구현

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,19 @@
+package racingcar.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import racingcar.view.InputView;
+
+public class GameController {
+    private final InputView inputView;
+
+    public GameController(final InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void run() {
+        String carRegistrationRequest = inputView.readCarRegistrationRequest();
+        List<String> cars = Arrays.asList(carRegistrationRequest.split(","));
+    }
+}
