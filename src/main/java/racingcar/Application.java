a61dc951b7567eeabbfa42f02c50935194509c84
zionhann <hanzion1108@gmail.com>
2024-10-27 00:39:34 +0900

    feat: 이동 시도 횟수 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,5 +10,9 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String rawName = Console.readLine();
         racingManager.assignCars(rawName);
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String rawTrials = Console.readLine();
+        racingManager.assignTrials(Integer.parseInt(rawTrials));
     }
 }
--- src/main/java/racingcar/RacingManager.java
+++ src/main/java/racingcar/RacingManager.java
@@ -7,10 +7,19 @@ import java.util.stream.Collectors;
 public class RacingManager {
 
     private static final String DELIM = ",";
+    private int trials;
 
     public List<Car> assignCars(String input) {
         return Arrays.stream(input.split(DELIM))
                 .map(Car::new)
                 .collect(Collectors.toList());
+    }
+
+    public void assignTrials(int trials) {
+        this.trials = trials;
+    }
+
+    public int getTrials() {
+        return trials;
     }
 }
