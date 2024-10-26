a8dd570648b62ba6ca22b251ef22c766fcf60773
김예린 <annalin1004@gmail.com>
2024-10-26 12:28:45 +0900

    Test the racing function
    Checked the operation of functions implemented in RacingService.

---
+++ src/test/java/racingcar/service/RacingServiceTest.java
@@ -0,0 +1,69 @@
+package racingcar.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RacingServiceTest {
+    RacingService racingService = new RacingService();
+
+    @Test
+    void carGoListTest(){
+        //given
+        List<String> strList = List.of("kimyelin,l1,l2".split(","));
+        //when
+        List<Boolean> result = new ArrayList<>();
+        for(String car : strList){
+            int randomNum = Randoms.pickNumberInRange(0,9);
+            result.add(racingService.isGo(randomNum));
+        }
+        //then
+        assertThat(result.size()).isEqualTo(3);
+    }
+
+    @Test
+    void isGoTest(){
+        //given
+        List<Integer> ls = List.of(1,2,3,4,5,6,7,8);
+        //when
+        List<Integer> result = new ArrayList<>();
+        for(int res : ls){
+            if(racingService.isGo(res)){
+                result.add(res);
+            }
+        }
+        //then
+        assertThat(result).contains(4);
+        assertThat(result).contains(5);
+        assertThat(result).contains(6);
+        assertThat(result).contains(7);
+        assertThat(result).contains(8);
+    }
+
+    @Test
+    void winnerCarListTest(){
+        //given
+        List<String> carList = List.of("k,k1,k2".split(","));
+        List<Integer> distance = List.of(3,2,1);
+        //when
+        List<String> res = racingService.winnerCarList(carList, distance);
+        //then
+        assertThat(res).contains("k");
+    }
+
+    @Test
+    void winnerCarListTest2(){
+        //given
+        List<String> carList = List.of("k,k1,k2".split(","));
+        List<Integer> distance = List.of(3,3,1);
+        //when
+        List<String> res = racingService.winnerCarList(carList, distance);
+        //then
+        assertThat(res).contains("k").contains("k1");
+    }
+}
