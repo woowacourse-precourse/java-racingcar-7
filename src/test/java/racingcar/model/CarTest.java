6442f9310fddbc32313c9fb72c732de7daaf5f94
ljhee92 <weilainib7@gmail.com>
2024-10-26 21:49:49 +0900

    test: position 확인 테스트 코드 수정
    getter method 사용에서 extracting method 사용으로 수정

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -11,10 +11,10 @@ class CarTest {
     @ParameterizedTest
     @CsvSource(value = {"4,1", "3,0"}, delimiter = ',')
     @DisplayName("전달된 값이 4 이상일 때만 자동차가 이동하는지")
-    void moveCar(int power, int position) {
+    void moveCar(int power, int expectedPosition) {
         Car car = new Car("pobi");
         car.move(power);
 
-        assertThat(car.getPosition()).isEqualTo(position);
+        assertThat(car).extracting("position").isEqualTo(expectedPosition);
     } // moveCar
 } // class
