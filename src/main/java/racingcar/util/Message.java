ec20cfdd53a1a1ca3ac99a376b9dc31b517f2aa1
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-26 17:06:44 +0900

    feat: 입출력과 에외처리에 필요한 메세지 클래스 생성

---
+++ src/main/java/racingcar/util/Message.java
@@ -0,0 +1,11 @@
+package racingcar.util;
+
+public class Message {
+    public static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    public static final String WINNER_MESSAGE = "최종 우승자 : ";
+
+    public static final String CAR_NAME_LENGTH_ERROR = "자동차 이름은 5자 이하이어야 합니다.";
+    public static final String TRY_COUNT_ERROR = "시도 횟수는 0 이상이어야 합니다.";
+    public static final String INVALID_NUMBER_ERROR = "유효한 숫자를 입력하세요.";
+}
