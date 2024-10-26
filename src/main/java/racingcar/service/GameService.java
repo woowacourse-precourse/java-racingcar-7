6d22204ef70eab31e9ba079b4326e46445ad88b2
minjae k <rkdalswoals@gmail.com>
2024-10-27 00:52:03 +0900

    feat: add GameService to manage game initialization, rounds, and winner determination

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,4 +1,33 @@
 package racingcar.service;
 
+import racingcar.domain.Car;
+import racingcar.domain.GameBoard;
+import racingcar.domain.RaceJudge;
+import java.util.List;
+
 public class GameService {
+    private final GameBoard gameBoard;
+    private final RaceJudge raceJudge;
+    private int totalRounds;
+
+    public GameService(GameBoard gameBoard, RaceJudge raceJudge) {
+        this.gameBoard = gameBoard;
+        this.raceJudge = raceJudge;
+    }
+
+    public void initializeGame(List<String> carNames, int totalRounds) {
+        this.totalRounds = totalRounds;
+        gameBoard.setupCars(carNames);
+    }
+
+    public List<Car> gameStart() {
+        for (int i = 0; i < totalRounds; i++) {
+            gameBoard.playRound();
+        }
+        return gameBoard.getCars();
+    }
+
+    public List<String> determineWinners() {
+        return raceJudge.getWinners(gameBoard.getCars());
+    }
 }
