3ae7114edbfb5f93dab0006d487932b155f31a3f
yumi <yumkoori@gmail.com>
2024-10-26 18:19:09 +0900

    Feat: 게임 시작 전에 필요한 처리를 하는 ready() 메세지 구현
    게임 준비를 위해 자동차의 이름을 검증하고, 검증된 자동차들을 게임에 참여 시키는 역할

--- src/main/java/racingcar/RacingGameManager.java
+++ src/main/java/racingcar/RacingGameManager.java
@@ -1,10 +1,18 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class RacingGameManager implements RacingGame {
+    private final List<Car> racingCarsList = new ArrayList<>();
+
     @Override
     public void ready(List<String> cars) {
+        for (String car : cars) {
+            InputValidator.nameValidation(car);
+
+            this.racingCarsList.add(Car.create(car));
+        }
     }
 
     @Override
