14208a70f8389a65658e0513cbb756b95214cb4f
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-10-26 18:54:41 +0900

    feat: 사용자 입력 메시지 추가 및 입력값 파싱
    - 자동차 이름 입력을 위한 안내 메시지 추가
    - 시도 횟수 입력을 위한 안내 메시지 추가
    - 자동차 이름을 쉼표로 구분하여 파싱하는 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+	private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가?";
+	
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
+        String input = Console.readLine();
+        String [] carNames = input.split(",");
+        
+        System.out.println(INPUT_TRY_COUNT_MESSAGE);
+        int count = Integer.parseInt(Console.readLine());
     }
 }
