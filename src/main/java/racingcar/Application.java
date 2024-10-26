eeebcb1d9686fe80ea429d4ce6e3f0b3630b82f0
DongHoon Lee <dhl1924@naver.com>
2024-10-27 02:20:10 +0900

    feat(application): Application 클래스에서 RacingCarGameController 초기화 및 게임 시작
    - Application 클래스에서 RacingCarGameController 인스턴스를 생성하고 ConsoleInputView와 ConsoleOutputView를 주입
    - main 메서드에서 게임 시작을 위해 startGame 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import racingcar.controller.RacingCarGameController;
+import racingcar.view.console.ConsoleInputView;
+import racingcar.view.console.ConsoleOutputView;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		RacingCarGameController racingCarGameController = new RacingCarGameController(
+			new ConsoleInputView(),
+			new ConsoleOutputView()
+		);
+
+		racingCarGameController.startGame();
+	}
 }
