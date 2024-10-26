b643d7e2663d1d05f2f5d4d1ec6e1e6d6c510bf4
seonseon1515 <seonseon1515@gmail.com>
2024-10-26 21:16:51 +0900

    feat: 무작위 값이 4이상일 경우 - 표시 자동차 이름 옆에 출력

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,18 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Car {
+    private static ArrayList<String> moveList;
+
+    ArrayList<String> move(){
+        if(RandomNumber.getRandomNum() >= 4){
+            moveList.add("-");
+        }
+        return moveList;
+    }
+
+    public static ArrayList<String> getMoveList() {
+        return moveList;
+    }
 }
