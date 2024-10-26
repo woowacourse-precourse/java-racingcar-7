cd152504dda6fc144bff72496c9c76ee043b4380
inhooo00 <chldlsgh0987@naver.com>
2024-10-27 00:14:32 +0900

    feat(model): Add Cars model
    Add Cars model :
    - 자동차의 집합을 처리하는 Cars class 생성.
    - Car 리스트에 있는 모든 자동차에 대해 carMove 메서드를 호출.

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,39 @@
+package racingcar.model;
+
+import static racingcar.util.RandomNumberGenerator.generateRandomNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.util.CarNameValidator;
+
+public class Cars {
+    
+    private final List<Car> cars;
+
+    public Cars(List<String> input) {
+        this.cars = new ArrayList<>();
+        checkInputCarNames(input);
+        addCars(input);
+    }
+
+    public List<Car> getCars() {
+        return this.cars;
+    }
+
+    public void carsMovement(List<Car> cars) {
+        for (Car car : cars) {
+            car.carMove(generateRandomNumber());
+        }
+    }
+
+    private void checkInputCarNames(List<String> input) {
+        CarNameValidator.checkEmpty(input);
+        CarNameValidator.checkDuplicate(input);
+    }
+
+    private void addCars(List<String> names) {
+        for (String carName : names) {
+            this.cars.add(new Car(carName));
+        }
+    }
+}
