a3a1ecf5925e98215babfe0696da7fd8e5a43c09
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2024-10-26 18:50:16 +0900

    refactoring : Applicaiton 입력 출력 책임 추가
    Application이 프론트엔드라 가정하여 입력과 출력 책임을 부여하였습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,11 +2,15 @@ package racingcar;
 
 import racingcar.controller.RaceController;
 import racingcar.service.RaceService;
+import racingcar.view.InputView;
 
 public class Application {
 
     public static void main(String[] args) {
+        String raceCarNames = InputView.inputRaceCarNames();
+        String moveCount = InputView.inputMoveCount();
+
         RaceController raceController = new RaceController(new RaceService());
-        raceController.raceStart();
+        System.out.println(raceController.doRace(raceCarNames,moveCount));
     }
 }
