1185a764810f7eea832f77434c601ba2f4f16eac
DongHoon Lee <dhl1924@naver.com>
2024-10-27 01:58:32 +0900

    feat(exception): CarNamesInput에 빈 문자열 유효성 검증 추가
    - CarNamesInput 생성 시 carNames 필드가 빈 문자열인 경우 예외를 발생시키도록 유효성 검증 로직 추가

--- src/main/java/racingcar/dto/CarNamesInput.java
+++ src/main/java/racingcar/dto/CarNamesInput.java
@@ -8,11 +8,18 @@ public record CarNamesInput(
 ) {
 	public CarNamesInput {
 		validateCarNamesNotNull(carNames);
+		validateCarNamesEmpty(carNames);
 	}
 
 	private void validateCarNamesNotNull(String carNames) {
 		if (carNames == null) {
 			throw RacingCarGameException.from(ErrorMessage.NULL_INPUT_ERROR);
+		}
+	}
+
+	private void validateCarNamesEmpty(String carNames) {
+		if (carNames.isEmpty()) {
+			throw RacingCarGameException.from(ErrorMessage.EMPTY_INPUT_ERROR);
 		}
 	}
 }
--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -1,8 +1,8 @@
 package racingcar.exception;
 
 public enum ErrorMessage {
-	NULL_INPUT_ERROR("null은 입력할 수 없습니다.")
-	;
+	NULL_INPUT_ERROR("null은 입력할 수 없습니다."),
+	EMPTY_INPUT_ERROR("빈 문자열은 입력할 수 없습니다.");
 
 	private final String message;
 
