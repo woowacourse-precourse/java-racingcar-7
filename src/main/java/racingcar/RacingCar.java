67ff25ae72f8d56adf07dc678e72655d4393cc68
hyunwon <khw73850@gmail.com>
2024-10-26 20:59:32 +0900

    feat: add get name, get move count function for RacingCar

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -11,6 +11,13 @@ public class RacingCar {
         this.moveCount = 0;
     }
 
+    public String getName(){
+        return this.name;
+    }
+    public int getMoveCount(){
+        return this.moveCount;
+    }
+
     public void moveForward(){
         if(Randoms.pickNumberInRange(0, 9) >= 4){
             this.moveCount++;
