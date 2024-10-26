1132355e858c67a80cb478ff1ff76c3cf6940ea2
SeoMoonk <sunghoon1216@naver.com>
2024-10-26 20:01:15 +0900

    feat: 사용자의 자동차 이름 입력에 대한 처리가 가능하다
    사용자가 자동차의 이름을 입력하면, 이에 대한 문자열 처리를 할 수 있음

--- src/main/java/racingcar/util/StringProcessor.java
+++ src/main/java/racingcar/util/StringProcessor.java
@@ -1,21 +1,25 @@
 package racingcar.util;
 
+import static racingcar.constant.RacingCarStatic.CAR_NAME_SEPARATOR;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class StringProcessor {
 
     public boolean hasMultipleCarNames(String input) {
-        //TODO : 여러 가지의 이름을 가지고 있는지 검사
-
-        return true;
+        return input.contains(CAR_NAME_SEPARATOR);
     }
     
     public List<String> splitCarNames(String input) {
 
         List<String> carNames = new ArrayList<>();
 
-        //TODO : 특정 구분자로 입력 내용을 나눠 반환하는기능
+        String[] names = input.split(CAR_NAME_SEPARATOR);
+
+        for(String carName : names) {
+            carNames.add(carName);
+        }
 
         return carNames;
     }
