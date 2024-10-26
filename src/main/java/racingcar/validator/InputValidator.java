42cc44cdf49f8771995323015a468f299190fe22
rootTiket <l20078829@gmail.com>
2024-10-27 00:10:15 +0900

    feat: 입력값이 다섯글자 이상인 경우 예외처리

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,9 +1,19 @@
 package racingcar.validator;
 
+import static racingcar.View.constant.ErrorMessage.TOO_LONG_VALUE;
+
 import java.util.List;
+import racingcar.global.exception.RacingcarException;
 
 public class InputValidator {
     public void check(List<String> carNames) {
+        carNames.stream()
+                .forEach(this::checkStringLength);
+    }
 
+    public void checkStringLength(String input) {
+        if (input.length() > 5) {
+            throw new RacingcarException(TOO_LONG_VALUE);
+        }
     }
 }
