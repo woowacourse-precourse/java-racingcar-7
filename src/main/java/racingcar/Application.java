4cb878a8d266df3e64ba432430bbcc668ed81c0a
making-a-scene <4936joo@naver.com>
2024-10-26 21:16:08 +0900

    feat: 프로그램 실행 시 새 게임 생성되도록 하는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.domain.RacingGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        UI ui = new UI();
+
+        String[] carNames = ui.getCarNames();
+        int limit = ui.getRoundLimit();
+
+        RacingGame racingGame = new RacingGame(limit);
+
+        racingGame.setParticipants(carNames);
+
+        racingGame.playGame();
     }
 }
