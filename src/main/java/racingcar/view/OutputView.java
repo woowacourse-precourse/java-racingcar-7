a8f0cd7f9b02659ba8150f3d5400ac56cf521817
slay <palgalow@gmail.com>
2024-10-26 22:20:37 +0900

    feat(OutputView): 한 라운드 결과 출력 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,20 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Race;
+
 public class OutputView {
 
     private final String RESULT_MSG = "실행 결과";
 
-    public void printResultMessage() {
+    public void outputResultMessage() {
         System.out.println(RESULT_MSG);
+    }
+
+    public void outputOneRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
     }
 }
