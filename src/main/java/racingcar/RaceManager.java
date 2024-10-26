a273292fe3edb387a89a4f09598780aec8eb9aec
maedachaeva <parkchaeyeon.b@gmail.com>
2024-10-27 00:45:32 +0900

    feat: 자동차 이동 기록을 저장하기 위한 필드 추가

--- src/main/java/racingcar/RaceManager.java
+++ src/main/java/racingcar/RaceManager.java
@@ -1,9 +1,19 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 public class RaceManager {  // raceCount만큼 반복
-    public boolean isCarMoving() {
+    private final LinkedHashMap<String, StringBuilder> carMovementRecords = new LinkedHashMap<>();
+
+    public RaceManager(List<String> racingCarNames) {
+        for (String racingCarName : racingCarNames) {
+            carMovementRecords.put(racingCarName, new StringBuilder());
+        }
+    }
+
+    private boolean isCarMoving() {
         int randomNumber = generateRandomNumber();
         return randomNumber >= 4;
     }
