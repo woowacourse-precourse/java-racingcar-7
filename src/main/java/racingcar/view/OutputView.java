dc4a8d1b25012551bd434295ee895d77bff099a6
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-26 18:11:32 +0900

    feat(view): OutputView 출력 클래스 생성 및 게임 결과 출력 기능 추가 - Cars와 Car 객체의 상태를 콘솔에 출력하여 게임 진행 상황 표시 - 각 라운드 결과와 최종 우승자 출력 기능 구현 - showRoundResult와 showWinners 메서드 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,41 @@
-package racingcar.view;public class OutputView {
+package racingcar.view;
+
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.Cars;
+
+public class OutputView {
+    private final String RESULT_MESSAGE = "실행 결과";
+    private final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
+
+    public void showRoundResult(Cars cars) {
+        System.out.println(RESULT_MESSAGE);
+        StringBuilder result = new StringBuilder();
+        for (Car car : cars.getCars()) {
+            result = addCarWithNameAndPosition(result, car);
+        }
+        System.out.println(result.toString());
+    }
+
+    private StringBuilder addCarWithNameAndPosition(StringBuilder result, Car car) {
+        result.append(car.getName()).append(" : ");
+        for (int i = 0; i < car.getPosition(); i++) {
+            result.append('-');
+        }
+        result.append("\n");
+        return result;
+    }
+
+    public void showWinners(List<Car> winners) {
+        System.out.print(FINAL_WINNER_MESSAGE);
+        StringBuilder result = new StringBuilder();
+        for (Car winner : winners) {
+            result.append(winner.getName()).append(',');
+        }
+        if (!result.isEmpty()) {
+            result.deleteCharAt(result.length() - 1); // 마지막 ,(쉼표) 제거
+        }
+
+        System.out.println(result.toString());
+    }
 }
