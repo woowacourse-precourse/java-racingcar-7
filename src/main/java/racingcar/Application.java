b971e6ff07f94bb6abc80c844e704aa553447c7d
181108-cherry <dysy9799@naver.com>
2024-10-26 23:09:26 +0900

    feat: 자동차 이름 입력 기능 추가
    자동차 이름을 입력 받고, 이름은 쉼표로 기준으로 구분짓게 함. 자동차 이름을 5자 이하로만 가능하게 하였으며 초과시
    IllegalArgumentException을 발생시킨 후 애플리케이션 종료시킴.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("Welcome racingcar game!");
+
+        System.out.println("경주할 자동차 이름을 정하시오.(이름은 쉼표(,) 기준으로 구분)");
+
+        String carName = Console.readLine();
+        System.out.println(carName);
+
+        String[] splitName = carName.split(","); // 이름은 쉼표를 기준으로 구분한다.
+
+        // 이름은 5자 이하로 가능하며 5자 초과시 IllegalArgumentException을 발생시킨 후 애플리케이션 종료시킨다.
+        for (int i = 0; i < splitName.length; i++) {
+            if (splitName[i].length() > 5) {
+                throw new IllegalArgumentException("5자 이하로 이름을 정하시오.");
+            }
+        }
+
     }
 }
