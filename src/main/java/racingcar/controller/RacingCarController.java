89370ec14ce587986ae2bf37b3d3bf4f298b7ad8
양효빈 <hyobin3026@naver.com>
2024-10-26 16:06:40 +0900

    feat(RacingCarController): Implement controller class

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,4 +1,24 @@
 package racingcar.controller;
 
+import racingcar.service.RacingCarService;
+
 public class RacingCarController {
+
+    private final RacingCarService racingCarService;
+
+    public RacingCarController(RacingCarService racingCarService){
+        this.racingCarService = racingCarService;
+    }
+
+    public void startRacing(){
+        racingCarService.inputRacingCarName();
+        racingCarService.inputRacingAttemptsCount();
+        racingCarService.passCarNameStringToModel();
+        int racingAttemptCount = racingCarService.getRacingAttemptsCount();
+        for (int i=0; i < racingAttemptCount; i++){
+            racingCarService.startRacing();
+        }
+        racingCarService.printWinnerResult();
+    }
+
 }
