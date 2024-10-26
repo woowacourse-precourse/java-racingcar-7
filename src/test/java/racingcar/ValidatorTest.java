131087184c68a7a05987819c2ba308f790a305b8
AwesomeMinsik <beaut3579@gmail.com>
2024-10-26 14:23:38 +0900

    test : 경기 회차 입력값에 대한 테스트 코드를 추가하였습니다

---
+++ src/test/java/racingcar/ValidatorTest.java
@@ -0,0 +1,44 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorTest {
+
+    @Test
+    void 정수_검증_테스트(){
+        int result = Validator.validateCycle("4");
+        assertEquals(4, result);
+    }
+    @Test
+    void 정수가_아닐때_예외_테스트() {
+        List<String> invalidInputs = List.of("A", "*", "0.1");
+
+        for (String input : invalidInputs) {
+            assertThatThrownBy(() -> Validator.validateCycle(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Test
+    void 음수값_예외_테스트(){
+        assertThatThrownBy(() -> Validator.validateCycle("-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 공백_제로입력_예외_테스트(){
+        String input1 = " ";
+        String input2 = "";
+
+        assertThatThrownBy(() -> Validator.validateCycle(input1))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> Validator.validateCycle(input2))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
