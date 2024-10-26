0cfc0cc69fd6309db0f5c566e08d096b5fccd0c3
GS_song98 <20003204@sju.ac.kr>
2024-10-27 00:30:38 +0900

    refactor: position 객체 삭제

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,38 +1,35 @@
 package racingcar.domain;
 
+import racingcar.util.RandomGenerator;
+
 public class Car {
 
+    private static final int MOVE_FORWARD_STANDARD = 4;
+
     private final Name name;
-    private final Position position;
+    private int position;
 
     public Car(String carName) {
         name = new Name(carName);
-        position = new Position();
+        position = 0;
     }
 
-    public Car(Name name, Position position) {
-        this.name = name;
-        this.position = position;
-    }
-
-    public void moveForward(int randomNumber) {
-        position.move(randomNumber);
+    public void moveForward(RandomGenerator randomGenerator) {
+        if (randomGenerator.generate() >= MOVE_FORWARD_STANDARD) {
+            position++;
+        }
     }
 
     public int getPosition() {
-        return position.getPosition();
+        return position;
     }
 
     public String getName() {
         return name.toString();
     }
 
-    public Car copy() {
-        return new Car(this.name, new Position(position.getPosition()));
-    }
-
     @Override
     public String toString() {
-        return String.format("%s : %s\n", name, position);
+        return String.format("%s : %s\n", name, "-".repeat(position));
     }
 }
