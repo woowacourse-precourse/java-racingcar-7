b7b3388ccb7ae46976225230e16372ec99cb2ffe
Nahye Choi <skgp0628@naver.com>
2024-10-26 09:33:58 +0900

    refactor: winner클래스 수정

--- src/main/java/racingcar/domain/Winners.java
+++ src/main/java/racingcar/domain/Winners.java
@@ -1,4 +1,25 @@
 package racingcar.domain;
 
-public class winners {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class Winners {
+
+    public static List<String> winner(Map<String, Integer> cars) {
+        List<String> winners = new ArrayList<>();
+        int first = Collections.max(cars.values());
+
+        for (Map.Entry<String, Integer> car : cars.entrySet()) {
+            if (car.getValue() == first) {
+                winners.add(car.getKey());
+            }
+        }
+        return winners;
+    }
+
+    public static String printWinners(List<String> winners) {
+        return String.join(", ", winners);
+    }
 }
