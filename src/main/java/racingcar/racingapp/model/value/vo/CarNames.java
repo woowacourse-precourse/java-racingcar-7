dbde22449698f3abb6727fc387042f943c1c5436
JeongSu Park <bechalrang@gmail.com>
2024-10-26 22:51:38 +0900

    fix(CarNames, CarNamesTest): 중복이름에 대한 빠뜨린 예외처리를 추가

--- src/main/java/racingcar/racingapp/model/value/vo/CarNames.java
+++ src/main/java/racingcar/racingapp/model/value/vo/CarNames.java
@@ -1,6 +1,7 @@
 package racingcar.racingapp.model.value.vo;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.racingapp.model.value.enums.RaceMaxCounts;
 
 public record CarNames(List<CarName> carNames) {
@@ -10,6 +11,12 @@ public record CarNames(List<CarName> carNames) {
     public CarNames {
         if (carNames == null) {
             throw new IllegalArgumentException("참가자 이름들이 지정되지 않았습니다.");
+        }
+        if (carNames.stream()
+                .collect(Collectors.groupingBy(CarName::name, Collectors.counting()))
+                .values().stream()
+                .anyMatch(countOfSameName -> 1 < countOfSameName)) {
+            throw new IllegalArgumentException("참가자 이름은 중복될 수 없습니다.");
         }
         if (LIMIT_OF_PARTICIPANTS < carNames.size()) {
             throw new IllegalArgumentException(String.format("지정 가능한 최대 참가자 수는 %d입니다.", LIMIT_OF_PARTICIPANTS));
--- src/test/java/racingcar/object/value/CarNamesTest.java
+++ src/test/java/racingcar/object/value/CarNamesTest.java
@@ -18,20 +18,25 @@ class CarNamesTest {
     }
 
     @Test
+    void 중복된_참가자명에대해_예외발생() {
+        assertThatThrownBy(() -> new CarNames(List.of(new CarName("dup"), new CarName("dup"))))
+                .isInstanceOf(COMMON_EXCEPTION);
+    }
+
+    @Test
     void 적절하지_않은_참가자수에대해_예외발생() {
-        assertThatThrownBy(() -> {
-            new CarNames(List.of(
-                    new CarName("1"),
-                    new CarName("2"),
-                    new CarName("3"),
-                    new CarName("4"),
-                    new CarName("5"),
-                    new CarName("6"),
-                    new CarName("7"),
-                    new CarName("8"),
-                    new CarName("9"),
-                    new CarName("10")));
-        }).isInstanceOf(COMMON_EXCEPTION);
+        assertThatThrownBy(() -> new CarNames(List.of(
+                new CarName("1"),
+                new CarName("2"),
+                new CarName("3"),
+                new CarName("4"),
+                new CarName("5"),
+                new CarName("6"),
+                new CarName("7"),
+                new CarName("8"),
+                new CarName("9"),
+                new CarName("10"))))
+                .isInstanceOf(COMMON_EXCEPTION);
     }
 
 }
