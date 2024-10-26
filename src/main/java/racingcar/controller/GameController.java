9fd10be9c3ac27a14fb6ca83b8890f5fb2e4e8b7
minjae k <rkdalswoals@gmail.com>
2024-10-27 00:56:19 +0900

    feat: add modular methods in GameController for game flow management

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,22 +1,44 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
 import racingcar.service.GameService;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
+import java.util.List;
 
 public class GameController {
     private final InputView inputView;
-    private final GameService gameService;
     private final OutputView outputView;
+    private final GameService gameService;
 
-    public GameController(InputView inputView, GameService gameService, OutputView outputView) {
+    public GameController(InputView inputView, OutputView outputView, GameService gameService) {
         this.inputView = inputView;
-        this.gameService = gameService;
         this.outputView = outputView;
+        this.gameService = gameService;
     }
     public void run() {
-        String carsString = inputView.getUserInput();
-        String[] cars = carsString.split(",");
+        initializeGame();
+        playGame();
+        displayFinalResult();
+    }
 
+    public void initializeGame() {
+        String carsString = inputView.getUserInput();
+        List<String> carNames = List.of(carsString.split(","));
+        int totalRounds = inputView.getUserInputAsInt();
+
+        gameService.initializeGame(carNames, totalRounds);
+    }
+    private void playGame() {
+        List<Car> roundResults = gameService.gameStart();
+        displayRoundResult(roundResults);
+    }
+    private void displayRoundResult(List<Car> cars) {
+        outputView.printRoundResult(cars);
+    }
+
+    private void displayFinalResult() {
+        List<String> winners = gameService.determineWinners();
+        outputView.printFinalWinners(winners);
     }
 }
