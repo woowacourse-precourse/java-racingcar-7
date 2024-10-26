1e4c6ddb76aa3ddefd11f102f54becd2fb2ab908
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-10-27 03:28:02 +0900

    feat(Application): 메인 함수에 유효성을 판별하는 부분 추가
    유효성을 판별하여 유효하지 않을 경우 에러 발생하도록 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분. 중복 불가)");
+        String carNames = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String roundNumber = Console.readLine();
+
+        Input input = new Input(carNames, roundNumber);
+        Racing racing;
+
+        if (Util.isValidCarNamesInput(input.getCarNames()) && Util.isValidRoundNumber(input.getRounds())) {
+            if (Util.hasDuplicates(input.splitCarNameInput())) {
+                throw new IllegalArgumentException("입력한 자동차 이름에 중복이 있습니다.");
+            }
+        } else {
+            throw new IllegalArgumentException("입력이 유효하지 않습니다.");
+        }
     }
 }
