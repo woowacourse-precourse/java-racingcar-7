00e204d226dbe44e0d5bf7500e67153068772bfc
Lee-Minju <mju7513@gmail.com>
2024-10-26 21:49:47 +0900

    controller\RacingGame.java | domain\Car.java: 횟수만큼 자동차를 움직이는 기능 구현

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -5,19 +5,31 @@ import java.util.List;
 import racingcar.domain.Cars;
 import racingcar.domain.Rounds;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingGame {
     InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
 
     public void doGame() {
         Cars cars = makeCars(inputView.askCarName());
         Rounds rounds = new Rounds(inputView.askNumberOfRounds());
+        for (int i = 0; i < rounds.rounds(); i++) {
+            doRace(cars);
+        }
     }
 
     public Cars makeCars(String initialInput) {
         List<String> carNames = Arrays.asList(initialInput.split(","));
         Cars cars = new Cars(carNames);
         return cars;
+    }
+
+    public void doRace(Cars cars) {
+        for (int i = 0; i < cars.size(); i++) {
+            cars.getCar(i).randomGo();
+            outputView.printProgress();
+        }
     }
 }
 
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,10 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private final int MAX_NAME_LENGTH = 5;
+    private final int NUMBER_CAN_GO = 4;
 
     private String name;
     private int progress;
@@ -21,9 +25,20 @@ public class Car {
         if (name.isBlank()) {
             throw new IllegalArgumentException("이름은 공백이 될수 없습니다.");
         }
-        if (name.length() > 5) {
+        if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("5자 이하의 이름을 입력해주세요");
         }
+    }
+
+    public void randomGo() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= NUMBER_CAN_GO) {
+            this.go();
+        }
+    }
+
+    private void go() {
+        this.progress++;
     }
 
     public String name() {
