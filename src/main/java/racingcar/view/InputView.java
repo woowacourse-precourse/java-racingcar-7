a1ff1b1ecfc7c5467d002458dea92cf0dae1dc3d
JoonKyo <joonkyolee@gmail.com>
2024-10-27 03:24:43 +0900

    feat(InputView): 문자열을 입력하지 않았을 때 유효성을 검증하는 메서드 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,14 +5,22 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String ASK_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ASK_ATTEMPT_NUMBER_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String ERROR_MESSAGE_FOR_NO_INPUT = "입력하지 않았습니다. 프로그램을 종료합니다.";
 
     public String askCarName() {
         System.out.println(ASK_CAR_NAME_MESSAGE);
-        return Console.readLine();
+        return validate(Console.readLine());
     }
 
     public String askAttemptNumber() {
         System.out.println(ASK_ATTEMPT_NUMBER_MESSAGE);
-        return Console.readLine();
+        return validate(Console.readLine());
+    }
+
+    public String validate(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_NO_INPUT);
+        }
+        return input;
     }
 }
