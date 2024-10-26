baa477a268c19e8a8abe0e75cf0f835dbbf3e842
zjhj0814 <zjhj0814@gmail.com>
2024-10-27 02:08:46 +0900

    feat(OutputView): 실행 결과 메시지 출력
    차수별 세부 실행 결과를 출력하기 전 "실행 결과" 메시지를 출력한다

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,9 @@
+package racingcar.view;
+
+public class OutputView {
+    private static final String RESULT_MESSAGE = "실행 결과";
+
+    public void printResult() {
+        System.out.println(RESULT_MESSAGE);
+    }
+}
