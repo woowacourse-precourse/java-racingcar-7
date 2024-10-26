236a4e148131848144c8d56b3f02a5ac9671801a
mhj1205 <lightnow1205@gmail.com>
2024-10-26 19:53:02 +0900

    feat: 시도할 횟수 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,6 +8,11 @@ public class Application {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             Scanner sc = new Scanner(System.in);
             String cars_str = sc.nextLine();
-            System.out.println(cars_str);
+            // System.out.println(cars_str);
+
+            System.out.println("시도할 횟수는 몇 회인가요?");
+            int cnt = sc.nextInt();
+            // System.out.println(cnt);
+            
     }
 }
