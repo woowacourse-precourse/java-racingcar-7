433df1998ad7130b589a573452d811e82620e0cb
Avocado <ujubongbong@gmail.com>
2024-10-26 19:31:23 +0900

    :sparkles: feat: 시도할 횟수 입력 안내 함수

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -4,4 +4,8 @@ public class Output {
     public void printInit() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
+
+    public void printAttemptCountMessage() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
 }
