d506d8296b142ab9773d00a93d1309b3a9e6da77
Fletacode <bassykd@gmail.com>
2024-10-26 21:49:01 +0900

    feat: 전진결정 숫자 생성 기능 추가
    전진할지 여부를 결정하는 랜덤 숫자 생성기능을 추가하였습니다. 랜덤숫자는 0~9까지 하나로 정해지고 4이하라면 전진, 4초과라면 멈춥니다.

---
+++ src/main/java/racingcar/model/decisionNum/DecisionNum.java
@@ -0,0 +1,22 @@
+package racingcar.model.decisionNum;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class DecisionNum {
+
+  private static final int MIN_RANDOM_NUM = 0;
+  private static final int MAX_RANDOM_NUM = 9;
+  private static final int GO_LIMIT = 4;
+
+  public static int GenerateDecisionNum(){
+    return Randoms.pickNumberInRange(MIN_RANDOM_NUM, MAX_RANDOM_NUM);
+  }
+
+  public static boolean isGo(int decisionNum){
+    if (decisionNum <= GO_LIMIT){
+      return true;
+    }
+    return false;
+  }
+
+}
