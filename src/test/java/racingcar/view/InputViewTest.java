738b77fa627a4d8f47f44c85ce71567d49febee3
변우영 <likevangogh24@gmail.com>
2024-10-26 22:50:29 +0900

    test(InputView): 입력받은 자동차 이름이 쉼표로 구분되어 있지 않거나, 특수문자인 경우 예외를 발생시키는 테스트 추가
    - 입력받은 자동차 이름이 쉼표(,)로 구분되지 않으면 예외를 발생시키는 테스트를 추가했다.
    - 입력받은 자동차 이름들의 사이가 아닌 바깥에 쉼표가 있는 경우 예외를 발생시키는 테스트를 추가했다.
    - 자동차 이름에 특수문자가 올 경우 예외를 발생시키는 테스트를 추가했다.
    - 자동차 이름에 알파벳, 숫자, 한글을 입력하는 테스트를 추가했다.

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.exception.BlankInputException;
+import racingcar.exception.CarNameFormatException;
 
 import java.io.ByteArrayInputStream;
 
@@ -37,5 +40,50 @@ class InputViewTest {
 
         //when & then
         Assertions.assertThrows(BlankInputException.class, InputView::inputCarNames);
+    }
+
+    @ParameterizedTest
+    @DisplayName("입력받은 자동차 이름이 쉼표(,)로 구분되지 않으면 예외가 발생한다.")
+    @ValueSource(strings = {"a.b.c,", "a;b;c", "a/b/c", "a-b-c", "a+b+c"})
+    void validateCommaDelimitedNames(String input) {
+        //given
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        //when & then
+        Assertions.assertThrows(CarNameFormatException.class, InputView::inputCarNames);
+    }
+
+    @ParameterizedTest
+    @DisplayName("입력받은 자동차 이름들의 사이가 아닌 바깥에 쉼표가 있는 경우 예외가 발생한다.")
+    @ValueSource(strings = {"a,b,c,", ",a,b,c", ",a,b,c,", ",abc", "abc,"})
+    void validateCommaBetweenCarNames(String input) {
+        //given
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        //when & then
+        Assertions.assertThrows(CarNameFormatException.class, InputView::inputCarNames);
+    }
+
+    @ParameterizedTest
+    @DisplayName("자동차 이름에 특수문자가 올 경우 예외를 발생한다.")
+    @ValueSource(strings = {"a,b,.", "a,;,c", "@,@,@", "*,*,*"})
+    void validateNonSpecialCharacter(String input) {
+        //given
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        //when & then
+        Assertions.assertThrows(CarNameFormatException.class, InputView::inputCarNames);
+    }
+
+    @ParameterizedTest
+    @DisplayName("자동차 이름에 알파벳, 숫자, 한글을 입력할 수 있다.")
+    @ValueSource(strings = {"a,b,c", "1,2,3", "가,나,다"})
+    void validateAlphabetAndNumberAndKorean(String input) {
+        //given
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        //when & then
+        org.assertj.core.api.Assertions.assertThatCode(InputView::inputCarNames)
+                .doesNotThrowAnyException();
     }
 }
