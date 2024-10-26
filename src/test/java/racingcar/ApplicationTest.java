2404a227c0c194b68cb63bee279a4f3f037fab99
je488 <pje488@naver.com>
2024-10-26 23:55:38 +0900

    test: 자동차 이름 입력 시 예외 처리에 대한 테스트 코드 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -15,20 +17,45 @@ class ApplicationTest extends NsTest {
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @DisplayName("쉼표가 없는 경우 예외가 발생하는지 검사")
+    @Test
+    void validateSeperatorPresent() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("jenn", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("입력 마지막에 쉼표가 있는 경우 예외가 발생하는지 검사")
+    @Test
+    void validateLastCharacterIsComma() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("jenn,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @DisplayName("중복된 이름이 있는 경우 예외가 발생하는지 검사")
+    @Test
+    void validateCarNameDuplicate() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("jenn,kelly,jenn", "1"))
+                        .isInstanceOf(IllegalArgumentException.class));
     }
 
     @Override
