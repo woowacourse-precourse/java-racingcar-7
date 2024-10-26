d757cd8cf0974ca03af1dc9f38378b95f7e7465f
githublees <leeho_@naver.com>
2024-10-26 15:52:29 +0900

    test: Update test cases for Cars class changes

--- src/test/java/racingcar/car/CarsTest.java
+++ src/test/java/racingcar/car/CarsTest.java
@@ -10,24 +10,24 @@ class CarsTest {
 
     @Test
     void 레이싱_참가_자동차_toString_반환() {
-        List<String> names = List.of("pobi","woni");
-        Cars cars = new Cars(names);
+        List<String> names = List.of("pobi", "woni");
+        Cars cars = Cars.createCars(names);
 
-        assertThat(cars.toString()).isEqualTo("pobi : \nwoni : ");
+        assertThat(cars.toString()).isEqualTo("pobi : \nwoni : \n");
     }
 
     @Test
     void 레이싱_위너_자동차_반환() {
-        List<String> names = List.of("pobi","woni");
-        Cars cars = new Cars(names);
+        List<String> names = List.of("pobi", "woni");
+        Cars cars = Cars.createCars(names);
 
-        assertThat(cars.getRacingWinners()).contains("pobi","woni");
+        assertThat(cars.getRacingWinners()).contains("pobi", "woni");
     }
 
     @Test
     void 참가한_자동차가_없는_경우() {
         List<String> names = List.of();
-        Cars cars = new Cars(names);
+        Cars cars = Cars.createCars(names);
 
         assertThatThrownBy(cars::getRacingWinners)
                 .isInstanceOf(IllegalArgumentException.class);
