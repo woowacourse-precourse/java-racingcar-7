9f8d73a6981e5431e1696493080e8f7c1e11658a
soeunnPark <carol0724@naver.com>
2024-10-26 21:19:44 +0900

    test(CarFactory): Add a test case for generating cars by car names
    자동차 이름 리스트를 가지고 자동차 객체들을 생성하는 팩토리에 대한 테스트를 작성했습니다.

---
+++ src/test/java/racingcar/domain/CarFactoryTest.java
@@ -0,0 +1,26 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarFactoryTest {
+
+    @DisplayName("자동차 이름을 가지고 자동차 객체들을 생성하는 테스트")
+    @Test
+    void makeCarsTest() {
+        CarFactory carFactory = new CarFactory();
+        List<String> carNames = List.of("genesis", "bmw", "bentley", "hybrid");
+        List<Car> cars = carFactory.makeCars(carNames);
+
+        assertThat(cars.size()).isEqualTo(4);
+        for(int i = 0; i < carNames.size(); i++) {
+            assertThat(cars.get(i).getCarName()).isEqualTo(carNames.get(i));
+        }
+    }
+
+}
