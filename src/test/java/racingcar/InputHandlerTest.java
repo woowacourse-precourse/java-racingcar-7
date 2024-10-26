1f2f0376dab0ed80880dbe91903179148649fe53
Seol-Munhyeok <abc1234568@naver.com>
2024-10-26 19:56:35 +0900

    refactor(inputHandlerTest): Rename test method to follow MethodName_StateUnderTest_ExpectedBehavior convention

--- src/test/java/racingcar/InputHandlerTest.java
+++ src/test/java/racingcar/InputHandlerTest.java
@@ -9,7 +9,7 @@ public class InputHandlerTest {
     private final InputHandler inputHandler = new InputHandler();
 
     @Test
-    void 올바른_입력을_받았을_때_테스트() {
+    void parseCarNames_WhenValidInputProvided_ReturnsListOfCarNames() {
         String carNames = "pobi,woni,jun";  // given
         List<String> result = inputHandler.parseCarNames(carNames);  // when
         assertThat(result).containsExactly("pobi", "woni", "jun");  // then
