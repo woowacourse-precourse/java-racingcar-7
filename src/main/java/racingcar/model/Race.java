55d0fa0ada8c4be211eb6696173567c14522b4b3
acupoframen <choe.hwan@gmail.com>
2024-10-26 15:45:02 +0900

    feat (test): create test create possible test - multiple rounds - equal score - no movement at all - name longer than 6 letters - same name

---
+++ src/main/java/racingcar/model/Race.java
@@ -0,0 +1,42 @@
+package racingcar.model;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Race {
+    private final List<Car> cars;
+    private final int rounds;
+
+    public Race(List<String> carNames, int rounds) {
+        this.cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+        this.rounds = rounds;
+    }
+    public void start() {
+        for (Car car : cars) {
+            int randomValue = pickNumberInRange(0, 9);
+            car.move(randomValue);
+        }
+    }
+    public List<Car> getCars() {
+        return new ArrayList<>(cars);
+    }
+    public List<String> getWinners() {
+        int maxPosition = 0;
+        for (Car car : cars) {
+            if (car.getLocation() > maxPosition) {
+                maxPosition = car.getLocation();
+            }
+
+        }
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getLocation() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
+}
