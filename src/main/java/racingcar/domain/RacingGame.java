6c0a707845ca8d22afcb8770236ec1698e2e78b9
Chaewon <corndots@naver.com>
2024-10-27 08:36:19 +0900

    refactor(RacingGame): 클래스를 새로 생성하여 RacingGame의 책임 분리
    CarMover 클래스를 사용해 자동차 이동 로직을 위임
    - WinnerCalculator 클래스를 사용해 우승자 결정 로직을 분리
    - RacingGame 클래스는 게임의 흐름과 라운드 진행만 관리하도록 개선

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,38 +1,35 @@
 package racingcar.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class RacingGame {
     private final List<Car> cars;
     private final int rounds;
+    private final CarMover carMover;
+    private final WinnerCalculator winnerCalculator;
 
     public RacingGame(List<Car> cars, int rounds) {
         this.cars = cars;
         this.rounds = rounds;
+        this.carMover = new CarMover();
+        this.winnerCalculator = new WinnerCalculator();
     }
 
-    // 모든 자동차가 한 라운드씩 이동
     public void playRound() {
         for (Car car : cars) {
-            int randomValue = Randoms.pickNumberInRange(0, 9);
-            car.move(randomValue); // 각 자동차의 전진 여부 결정
+            carMover.moveCar(car);
         }
-    }
-
-    // 우승자 결정 메서드
-    public List<Car> getWinners() {
-        int maxPosition = cars.stream()
-                .mapToInt(Car::getPosition)
-                .max()
-                .orElse(0);
-        return cars.stream()
-                .filter(car -> car.getPosition() == maxPosition)
-                .collect(Collectors.toList());
     }
 
     public List<Car> getCars() {
         return cars;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public List<Car> getWinners() {
+        return winnerCalculator.calculateWinners(cars);
     }
 }
