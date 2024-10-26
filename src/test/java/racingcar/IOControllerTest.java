3b51ebd664b60d33ab115dc9e0041ad7493e14cc
chyo1 <hyowon3856@gmail.com>
2024-10-27 01:49:45 +0900

    test : printResultByCar 테스트 추가

--- src/test/java/racingcar/IOControllerTest.java
+++ src/test/java/racingcar/IOControllerTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.ArrayList;
@@ -27,6 +28,14 @@ class IOControllerTest extends NsTest{
         testCar3.move();
         ioController.printResults(testCarList);
         assertThat(output()).contains("test1 : ", "test2 : ", "test3 : -");
+    }
+
+    @Test
+    void 자동차별_결과_출력() {
+        testCar1.move();
+        testCar1.move();
+        ioController.printResultByCar(testCar1.getName(), testCar1.getMoveCount());
+        assertEquals("test1 : --", output());
     }
 
     @Override
