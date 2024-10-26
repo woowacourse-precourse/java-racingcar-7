f6ef7ba169be70964d43a2bca182100c766ddb9c
rootTiket <l20078829@gmail.com>
2024-10-27 00:30:27 +0900

    feat: 빈 문자열 검증로직 구현

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,5 +1,6 @@
 package racingcar.validator;
 
+import static racingcar.View.constant.ErrorMessage.BLANK_VALUE;
 import static racingcar.View.constant.ErrorMessage.TOO_LONG_VALUE;
 
 import racingcar.global.exception.RacingcarException;
@@ -7,11 +8,18 @@ import racingcar.global.exception.RacingcarException;
 public class InputValidator {
     public void check(String input) {
         checkStringLength(input);
+        checkBlank(input);
     }
 
     public void checkStringLength(String input) {
         if (input.length() > 5) {
             throw new RacingcarException(TOO_LONG_VALUE);
+        }
+    }
+
+    public void checkBlank(String input) {
+        if (input.isBlank()) {
+            throw new RacingcarException(BLANK_VALUE);
         }
     }
 }
