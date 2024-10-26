ca7e827e18a7cbb03f98d204998154094c111d0b
SeongbinPark <hyucse22@gmail.com>
2024-10-26 22:25:21 +0900

    test(OutputHandlerTest): OutputHandler 클래스의 기능에 대한 단위 테스트
    printAttemptsMessage 메서드 테스트 테스트

--- src/test/java/racingcar/unittest/OutputHandlerTest.java
+++ src/test/java/racingcar/unittest/OutputHandlerTest.java
@@ -19,6 +19,16 @@ class OutputHandlerTest extends NsTest {
         assertThat(output()).contains("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
+    @Test
+    @DisplayName("printAttemptsMessage 메서드 테스트")
+    void printAttemptsMessage_shouldPrintCorrectMessage() {
+        // When
+        OutputHandler.printAttemptsMessage();
+
+        // Then
+        assertThat(output()).contains("시도할 횟수는 몇 회인가요?");
+    }
+
     @Override
     public void runMain() {
         // 해당 클래스에서는 runMain을 사용하지 않습니다.
