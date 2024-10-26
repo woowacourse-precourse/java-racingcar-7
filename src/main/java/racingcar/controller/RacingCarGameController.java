a035e8fac50ad60ee8676afbae3f10f8183a0214
DongHoon Lee <dhl1924@naver.com>
2024-10-27 02:19:53 +0900

    feat(controller): RacingCarGameController 클래스 추가
    - RacingCarGameController 클래스에서 InputView와 OutputView를 주입받아 게임의 입출력을 처리
    - startGame 메서드를 통해 게임의 초기 흐름 구성

---
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -0,0 +1,20 @@
+package racingcar.controller;
+
+import racingcar.dto.CarNamesInput;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingCarGameController {
+	private final InputView inputView;
+	private final OutputView outputView;
+
+	public RacingCarGameController(InputView inputView, OutputView outputView) {
+		this.inputView = inputView;
+		this.outputView = outputView;
+	}
+
+	public void startGame() {
+		outputView.printCarNamesRequest();
+		CarNamesInput carNamesInput = inputView.readCarNames();
+	}
+}
