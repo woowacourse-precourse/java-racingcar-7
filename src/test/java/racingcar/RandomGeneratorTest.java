4615ea251822ac714c47d07f896844a0827f34dd
sunwon <jsw5913@naver.com>
2024-10-26 14:53:05 +0900

    refactor(NumberGenerator): 인터페이스로 수정
    - 랜점숫자를 포함한 함수를 테스트하기 위해 변경
    - 테스트에서만 쓸 TestNumberGenerator 생성

--- src/test/java/racingcar/RandomGeneratorTest.java
+++ src/test/java/racingcar/RandomGeneratorTest.java
@@ -1,4 +1,18 @@
 package racingcar;
 
-public class RandomGenerator {
+import org.junit.jupiter.api.Test;
+import racingcar.model.RandomNumberGenerator;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RandomGeneratorTest {
+
+    @Test
+    void 숫자0에서_9까지_램덤숫자를_반환한다() {
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        int randomNumber = randomNumberGenerator.generate();
+
+        assertTrue(randomNumber >= 0);
+        assertTrue(randomNumber <= 9);
+    }
 }
