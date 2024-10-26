fb15276f5cc2990c305e5c9c4f2be22e0e8d544a
JuHyuk Lim <nyclo0705@gmail.com>
2024-10-26 22:32:18 +0900

    feat: 경주 게임의 우승자를 출력하는 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,13 +1,16 @@
 package racingcar.view;
 
 import static racingcar.common.Constant.CAR_MOVE_SYMBOL;
+import static racingcar.common.Constant.DELIMITER;
 import static racingcar.common.Constant.OUTPUT_DELIMITER;
 import static racingcar.common.Constant.WHITESPACE;
 import static racingcar.view.ViewMessage.INPUT_ATTEMPTS;
 import static racingcar.view.ViewMessage.INPUT_CAR_NAMES;
 import static racingcar.view.ViewMessage.OUTPUT_RESULT;
+import static racingcar.view.ViewMessage.OUTPUT_WINNER;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.model.Car;
 
 public class OutputView {
@@ -24,6 +27,7 @@ public class OutputView {
     }
 
     public static void printResultMessage() {
+        printMessage();
         printMessage(OUTPUT_RESULT.getMessage());
     }
 
@@ -34,6 +38,10 @@ public class OutputView {
         printMessage();
     }
 
+    public static void printWinner(List<String> winnerNames) {
+        printMessage(OUTPUT_WINNER.getMessage() + getWinnerNames(winnerNames));
+    }
+
     private static String getCarMovement(Car car) {
         StringBuilder sb = new StringBuilder(car.getName());
         sb.append(WHITESPACE.getValue()).append(OUTPUT_DELIMITER.getValue()).append(WHITESPACE.getValue());
@@ -41,6 +49,15 @@ public class OutputView {
             sb.append(CAR_MOVE_SYMBOL.getValue());
         }
         return sb.toString();
+    }
+
+    private static String getWinnerNames(List<String> winnerNames) {
+        if (winnerNames.size() == 1) {
+            return winnerNames.getFirst();
+        }
+
+        return winnerNames.stream()
+                .collect(Collectors.joining(DELIMITER.getValue() + WHITESPACE.getValue()));
     }
 
     private static void printMessage() {
