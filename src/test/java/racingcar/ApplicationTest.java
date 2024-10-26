5b9a103226043b219cdda4af460bd58b18c7519a
SeongbinPark <hyucse22@gmail.com>
2024-10-27 06:09:13 +0900

    test(ApplicationTest): ApplicationTest 초기화

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -23,50 +23,11 @@ class ApplicationTest extends NsTest {
         );
     }
 
-    /**
-     * 예외 테스트
-     */
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 자동차_이름이_5자를_초과하는_경우() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,abcdef", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining("자동차 이름은 5자 이하여야 합니다: ")
-        );
-    }
-
-    @Test
-    void 숫자가_아닌_시도횟수가_입력되는_경우() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("a,b,c", "a"))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining("시도 횟수는 '한 개'의 '숫자'여야 합니다. ")
-        );
-    }
-
-    @Test
-    void 자동차_이름이_빈_문자열인_경우() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("a,,b", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining("자동차 이름은 빈 문자열일 수 없습니다.")
-        );
-    }
-
-    @Test
-    void 둘_이상의_자동차_이름이_중복될_경우() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("a,a,b", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining("자동차 이름에 중복이 존재합니다: ")
         );
     }
 
