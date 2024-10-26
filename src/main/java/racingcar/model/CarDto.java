bccdc4c298da366a0c7784a4a89404c0f14f3db9
yooo <ghddbtl@gmail.com>
2024-10-26 19:40:43 +0900

    refactor: add constructor to CarDto
    forward를 재대입할 수 있는 생성자를 추가함. setter를 안쓰기 위함

--- src/main/java/racingcar/model/CarDto.java
+++ src/main/java/racingcar/model/CarDto.java
@@ -3,24 +3,29 @@ package racingcar.model;
 public class CarDto {
     private String name;
     private int id;
-    private int position = 0;
+    private int forward = 0;
 
     public CarDto(String name, int id) {
         this.name = name;
         this.id = id;
     }
 
+    public CarDto(String name, int id, int forward) {
+        this.name = name;
+        this.id = id;
+        this.forward = forward;
+    }
+
     public String getName() {
         return name;
     }
-
 
     public int getId() {
         return id;
     }
 
 
-    public int getPosition() {
-        return position;
+    public int getForward() {
+        return forward;
     }
 }
