5de795c4bdc92a563721e570cbcc3ff05eebb8ed
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-26 18:12:55 +0900

    feat(service): CarMoveManager 서비스 클래스 생성 및 이동 여부 판단 로직 추가 - 자동차가 이동할 수 있는지 판단하는 canMove 메서드 구현 - MOVE_THRESHOLD 상수를 사용하여 이동 기준값 설정

--- src/main/java/racingcar/service/CarMoveManager.java
+++ src/main/java/racingcar/service/CarMoveManager.java
@@ -1,5 +1,9 @@
 package racingcar.service;
 
 public class CarMoveManager {
+    private static final int MOVE_THRESHOLD = 4;
 
+    public boolean canMove(int number) {
+        return number >= MOVE_THRESHOLD;
+    }
 }
