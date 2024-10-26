38f9ff295509c12437a9ef1c12afcb78eefac20f
JuHyuk Lim <nyclo0705@gmail.com>
2024-10-26 22:07:45 +0900

    feat: 각 시도 차수마다 전진 결과를 출력하는 기능 추가

--- src/main/java/racingcar/common/Constant.java
+++ src/main/java/racingcar/common/Constant.java
@@ -2,8 +2,12 @@ package racingcar.common;
 
 public enum Constant {
     DELIMITER(","),
-    CAR_NAME_LIMIT_LENGTH("5");
-
+    CAR_NAME_LIMIT_LENGTH("5"),
+    MOVE_CONDITION("4"),
+    OUTPUT_DELIMITER(":"),
+    CAR_MOVE_SYMBOL("-"),
+    WHITESPACE(" ");
+    
     private final String value;
 
     Constant(String value) {
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,14 @@
 package racingcar.view;
 
+import static racingcar.common.Constant.CAR_MOVE_SYMBOL;
+import static racingcar.common.Constant.OUTPUT_DELIMITER;
+import static racingcar.common.Constant.WHITESPACE;
 import static racingcar.view.ViewMessage.INPUT_ATTEMPTS;
 import static racingcar.view.ViewMessage.INPUT_CAR_NAMES;
+import static racingcar.view.ViewMessage.OUTPUT_RESULT;
+
+import java.util.List;
+import racingcar.model.Car;
 
 public class OutputView {
 
@@ -16,7 +23,32 @@ public class OutputView {
         printMessage(INPUT_ATTEMPTS.getMessage());
     }
 
+    public static void printResultMessage() {
+        printMessage(OUTPUT_RESULT.getMessage());
+    }
+
+    public static void printRacingStage(List<Car> cars) {
+        for (Car car : cars) {
+            printMessage(getCarMovement(car));
+        }
+        printMessage();
+    }
+
+    private static String getCarMovement(Car car) {
+        StringBuilder sb = new StringBuilder(car.getName());
+        sb.append(WHITESPACE.getValue()).append(OUTPUT_DELIMITER.getValue()).append(WHITESPACE.getValue());
+        for (int distance = 0; distance < car.getDistance(); distance++) {
+            sb.append(CAR_MOVE_SYMBOL.getValue());
+        }
+        return sb.toString();
+    }
+
+    private static void printMessage() {
+        System.out.println();
+    }
+
     private static void printMessage(String message) {
         System.out.println(message);
     }
+
 }
