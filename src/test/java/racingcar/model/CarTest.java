7eceb638a4a2b4c2b3ea126280eca0b0536e8cc0
unh6unh6 <ruddbs3308@naver.com>
2024-10-26 17:01:23 +0900

    refactor: TestCar 클래스 분리

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -44,19 +44,3 @@ public class CarTest {
     }
 }
 
-class TestCar extends Car {
-    private int randomNumber = 0;
-
-    public TestCar(String name) {
-        super(name);
-    }
-
-    public void setRandomNumber(int randomNumber) {
-        this.randomNumber = randomNumber;
-    }
-
-    @Override
-    protected int getRandomNumber() {
-        return randomNumber;
-    }
-}
