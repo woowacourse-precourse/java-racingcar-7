c1c24b974e1f623faf746bf2259cbc6286274ac8
minSsan <msp1125@hanyang.ac.kr>
2024-10-27 01:47:18 +0900

    fix(CarNameParser): CarNameParserTest 미통과 케이스 - 중복 값 검사

--- src/main/java/racingcar/domain/util/CarNameParser.java
+++ src/main/java/racingcar/domain/util/CarNameParser.java
@@ -1,21 +1,29 @@
 package racingcar.domain.util;
 
+import racingcar.infrastructure.exception.DuplicateNameException;
 import racingcar.infrastructure.exception.EmptyInputException;
 import racingcar.domain.model.constant.CarNameDelimiter;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class CarNameParser {
     public static List<String> parse(final String input) {
         List<String> parsed = parseWithTrim(input, CarNameDelimiter.getRegex());
         validateExistEmpty(parsed);
+        validateExistDuplicate(parsed);
         return parsed;
     }
 
     private static void validateExistEmpty(final List<String> list) {
         if (list.stream().anyMatch(String::isEmpty)) {
             throw new EmptyInputException();
+        }
+    }
+
+    private static void validateExistDuplicate(final List<String> list) {
+        HashSet<String> duplicateRemoved = new HashSet<>(list);
+        if (list.size() != duplicateRemoved.size()) {
+            throw new DuplicateNameException();
         }
     }
 
