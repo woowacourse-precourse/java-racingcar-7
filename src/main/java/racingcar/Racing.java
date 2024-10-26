9a001c94127db8aee975f9f423f6fafa76a87d88
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-10-27 03:17:47 +0900

    feat(Racing): Racing게임을 실행할 클래스 추가
    Racing게임 관련 기능을 수행할 클래스 추가
    자동차 이름들을 입력 받아 Car 배열 생성
    이름이 공백일 경우 NoName이라고 보여주도록 추가

---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+public class Racing {
+    private static Car[] cars;
+
+    public Racing(String[] carNames) {
+        cars = new Car[carNames.length];
+        int i = 0;
+        for (String carName : carNames) {
+            if (carName.isEmpty()) {
+                cars[i] = new Car("NoName");
+            } else {
+                cars[i] = new Car(carName);
+            }
+            i++;
+        }
+    }
+}
