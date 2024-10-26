204444d9c0cbae1787ee1f065feeec816a586399
hyeondo <eun041212@gmail.com>
2024-10-27 00:53:43 +0900

    feat(Winner) : 레이스 종료 후 승자를 계산하는 기능 구현

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,8 +1,10 @@
 package racingcar.controller;
 
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 import racingcar.model.Race;
 import racingcar.model.RandomNumberGenerator;
+import racingcar.model.Winner;
 import racingcar.view.InputView;
 
 public class RaceController {
@@ -11,6 +13,7 @@ public class RaceController {
         ArrayList<String> carNames = InputView.readCarNames();
         int repeatCount = InputView.readRepeatCount();
         Race race = new Race(carNames,new RandomNumberGenerator());
-
+        IntStream.range(0, repeatCount).forEach(i -> race.playOneRound());
+        Winner winner = new Winner(race.getCars());
     }
 }
