44793d98cec365bbc66c0d6d91e69124ff5505eb
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-10-26 23:00:28 +0900

    refactor(Application): 출력 조건에 맞게 수정
    경주할 자동차 이름, 시도 횟수 묻는 문장을 조건에 맞게 수정함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,16 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
         // 경주 클래스
         RacingGame racingGame = new RacingGame();
-        String carNames = Console.readLine();
 
         // 경주 차량 입력
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
         racingGame.inputCars(carNames);
 
         // 시도 횟수 입력
+        System.out.println("시도할 회수는 몇회인가요?");
         int attempts = Integer.parseInt(Console.readLine());
         racingGame.inputAttempts(attempts);
 
