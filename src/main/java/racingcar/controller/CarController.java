1c97009e9c3dd94d9d9c8d5f9655adf9c1347bde
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-26 18:17:10 +0900

    feat(controller): CarController 클래스 생성 및 게임 진행 로직 구현 - 사용자 입력을 받아 자동차 경주 게임을 시작하고 결과를 출력 - CarMoveManager, InputSplitter, RandomNumberGenerator 등의 서비스 및 뷰와 연동 - 각 라운드마다 결과를 출력하고 최종 우승자를 표시

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -1,2 +1,51 @@
-package racingcar.controller;public class CarController {
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.Cars;
+import racingcar.service.CarMoveManager;
+import racingcar.service.InputSplitter;
+import racingcar.service.RandomNumberGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class CarController {
+    private final CarMoveManager carMoveManager;
+    private final InputSplitter inputSplitter;
+    private final RandomNumberGenerator randomNumberGenerator;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public CarController(CarMoveManager carMoveManager, InputSplitter inputSplitter,
+                         RandomNumberGenerator randomNumberGenerator, InputView inputView, OutputView outputView) {
+        this.carMoveManager = carMoveManager;
+        this.inputSplitter = inputSplitter;
+        this.randomNumberGenerator = randomNumberGenerator;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void play() {
+        String carNamesInput = inputView.getCarNames();
+
+        int count = Integer.parseInt(inputView.getCount());
+
+        Cars cars = new Cars();
+
+        String[] carNames = inputSplitter.getCarNmaes(carNamesInput);
+        for (String carName : carNames) {
+            cars.addCar(new Car(carName.trim()));
+        }
+
+        for (int i = 0; i < count; i++) {
+            for (Car car : cars.getCars()) {
+                int number = randomNumberGenerator.gernerateRandomNumber();
+                car.move(carMoveManager.canMove(number));
+            }
+            outputView.showRoundResult(cars);
+        }
+
+        List<Car> winners = cars.getWinners();
+        outputView.showWinners(winners);
+    }
 }
