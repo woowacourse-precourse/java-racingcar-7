dfd95aec6a27a498c420bea4aeb7a0e1c6a77aa7
Koo-EunSung <ku903@naver.com>
2024-10-26 18:58:32 +0900

    Feat: 이름 길이 검증

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -5,7 +5,15 @@ public class Car {
     private int step;
 
     public Car(String name) {
+        validateName(name);
+
         this.name = name;
         this.step = 0;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
     }
 }
