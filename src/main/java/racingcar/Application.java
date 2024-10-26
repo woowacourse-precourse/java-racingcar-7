18d3c5238016b2a2afefc318f9c2472de0693cdd
mhj1205 <lightnow1205@gmail.com>
2024-10-26 19:48:10 +0900

    feat: 경주할 자동차 이름 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
+import java.util.Scanner;
+
+import com.sun.nio.sctp.SctpChannel;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            Scanner sc = new Scanner(System.in);
+            String cars_str = sc.nextLine();
+            System.out.println(cars_str);
     }
 }
