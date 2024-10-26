a23cd58b041aa356e558272446644e0d1bc926b4
eunseobb <seobseob0810@naver.com>
2024-10-26 20:27:02 +0900

    feat(controller): RaceController에 전체적인 진행 흐름 추가
    race 메서드에 시도 횟수만큼 자동차 이동을 반복하고 각 라운드 결과와 최종 우승자 출력 로직 추가

---
+++ src/main/java/racingcar/controller/RaceController.java
@@ -0,0 +1,37 @@
+package racingcar.controller;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import racingcar.model.Car;
+import racingcar.model.CarService;
+import racingcar.model.StringParser;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RaceController {
+    private final StringParser stringParser;
+    private final CarService carService;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RaceController(StringParser stringParser, CarService carService, InputView inputView,
+                          OutputView outputView) {
+        this.stringParser = stringParser;
+        this.carService = carService;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void race() {
+        String userInput = inputView.readRawCarNames();
+        List<String> carNames = stringParser.findCarNames(userInput);
+        List<Car> cars = carService.createCars(carNames);
+
+        Integer attempt = inputView.readAttemptCount();
+        IntStream.range(0, attempt).forEach(i -> {
+            cars.forEach(carService::moveForward);
+            outputView.printEachTime(cars);
+        });
+        outputView.printWinner(carService.findWinner(cars));
+    }
+}
