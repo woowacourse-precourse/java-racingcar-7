3d31d3f98571a64290ee6c307ad8c6e2f53f7cc4
githublees <leeho_@naver.com>
2024-10-26 15:49:22 +0900

    style: Update Constant class

--- src/main/java/racingcar/constant/Constant.java
+++ src/main/java/racingcar/constant/Constant.java
@@ -1,6 +1,7 @@
 package racingcar.constant;
 
 public class Constant {
+
     public static final String CAR_NAME_INPUT_STRING = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String TRY_COUNT_INPUT_STRING = "시도할 횟수는 몇 회인가요?";
     public static final String RACING_PROGRESS_STRING = "실행 결과";
