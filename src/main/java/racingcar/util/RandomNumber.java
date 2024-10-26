25ceb96f56657468c1dae7908f064ab62781385a
양효빈 <hyobin3026@naver.com>
2024-10-26 13:55:33 +0900

    feat(RandomNumber): Implement random number generator class

--- src/main/java/racingcar/util/RandomNumber.java
+++ src/main/java/racingcar/util/RandomNumber.java
@@ -1,4 +1,13 @@
 package racingcar.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomNumber {
+    private static final int startInclusive = 0;
+    private static final int endInclusive = 9;
+
+    //TODO: 랜덤값 생성
+    public static int createRandomNumber() {
+        return Randoms.pickNumberInRange(startInclusive, endInclusive);
+    }
 }
