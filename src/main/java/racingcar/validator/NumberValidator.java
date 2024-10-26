550b3401f3fbbbf00c0871e0a6834cd0ac434e59
jeonghyeon-ohk <ok6737@naver.com>
2024-10-27 02:01:33 +0900

    feat(validator) : 경기 횟수 입력에 대한 예외 처리 기능 구현
    경기 횟수 입력값에 대한 예외 상황들을 처리하는 기능을 구현했습니다.
    - validateNumericValue : 입력값에 대한 2가지 예외 상황 검증 실행
    - isNumericValue: 숫자가 아닌 다른 값이 입력되었는지 검증
    - isPositiveInteger: 양의 정수를 입력했는지 검증

--- src/main/java/racingcar/validator/NumberValidator.java
+++ src/main/java/racingcar/validator/NumberValidator.java
@@ -1,4 +1,24 @@
 package racingcar.validator;
 
 public class NumberValidator{
+
+    public static void validateNumericValue(String input){
+        isNumericValue(input);
+        isPositiveInteger(input);
+    }
+
+    public static void isNumericValue(String input){
+        try {
+            int convert = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수 값이 아닙니다: " + input);
+        }
+    }
+
+    public static void isPositiveInteger(String input) {
+            if(Integer.parseInt(input) < 1){
+                throw new IllegalArgumentException("양의 정수 값이 아닙니다: " + input);
+            }
+    }
+
 }
