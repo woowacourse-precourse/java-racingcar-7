c786adb36b82f9908798f3d236c84771ee6c76c7
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 00:09:42 +0900

    feat(RaceProcessService): DomainFactory 의존성 처리
    - 생성자 주입 방식으로 DomainFactory 의존성 처리
    - Domain에 해당하는 세부 기능들 구현 후, 비즈니스 로직 생성 예정

---
+++ src/main/java/racingcar/Service/RaceProcessService.java
@@ -0,0 +1,13 @@
+package racingcar.Service;
+
+import racingcar.Factory.DomainFactory;
+
+public class RaceProcessService {
+    private final DomainFactory domainFactory;
+
+    public RaceProcessService(DomainFactory domainFactory) {
+        this.domainFactory = domainFactory;
+    }
+
+    // TODO - 로직 구현
+}
