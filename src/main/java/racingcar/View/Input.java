f8973cd020e66a11f5848bdad7986a1e8f109660
김호연 <yun1094@naver.com>
2024-10-26 12:18:20 +0900

    함수명 오타 수정

--- src/main/java/racingcar/View/Input.java
+++ src/main/java/racingcar/View/Input.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Input {
     private Integer trialCount;
 
-    public List<String> ListRequestCarNameMessage() {
+    public List<String> RequestCarNameMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         List<String> carNameList = List.of(Console.readLine().split(","));
         return carNameList;
