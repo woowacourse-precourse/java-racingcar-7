1ee97166fbf6a97ee0d22ff264bee92f24fe53ed
honggyu1233@naver.com <honggyu1233@naver.com>
2024-10-27 03:14:06 +0900

    feat : RacingcarController 수정 - 입력 받은 문자열 분리 및 RacingGame 객체 생성

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -1,12 +1,26 @@
 package racingcar.controller;
 
+import racingcar.model.RacingGame;
 import racingcar.model.Racingcar;
+import racingcar.view.InputView;
 
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RacingcarController {
+
+    public void start() {
+        Object[] inputNameAndCount = InputView.racingcarInputView();
+        String racingcarNames = inputNameAndCount[0].toString();
+        int moveCount = Integer.parseInt(inputNameAndCount[1].toString());
+
+        List<Racingcar> racingcarsList = createRacingcars(racingcarNames);
+        RacingGame racingGame = new RacingGame(racingcarsList, moveCount);
+
+    }
+
+
 
 
 
