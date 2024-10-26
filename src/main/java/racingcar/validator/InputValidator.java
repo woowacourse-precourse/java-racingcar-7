4d3c8d00cb16247b76f4ba87340356ae7410ef3b
jonghwanlee <kavae550@gmail.com>
2024-10-27 03:43:30 +0900

    feat(validator): 시도할 횟수 관련 유효성 검증 기능 구현
    - 입력값이 공백인 경우 오류 발생
    - 숫자가 아닌 값을 입력한 경우
    - 숫자를 입력했지만 정수가 아닌 경우

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,4 +1,40 @@
 package racingcar.validator;
 
+import java.util.ArrayList;
+
 public class InputValidator {
+
+    public static void validateCarNames(ArrayList<String> CarNames) {
+    }
+
+    public static void validateTrialNumber(String rawTrialNumber) {
+        validateNotEmpty(rawTrialNumber);
+        validateIsNumber(rawTrialNumber);
+        validateIsInteger(rawTrialNumber);
+
+    }
+
+    private static void validateNotEmpty (String rawTrialNumber){
+        if( rawTrialNumber.isEmpty() ){
+            throw new IllegalArgumentException("입력값이 공백입니다.");
+        }
+    }
+
+    private static void validateIsNumber (String rawTrialNumber){
+        try {
+            Float.parseFloat(rawTrialNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력값이 숫자가 아닙니다");
+        }
+    }
+
+    private static void validateIsInteger (String rawTrialNumber){
+        try {
+            Integer.parseInt(rawTrialNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력값이 정수가 아닙니다");
+        }
+    }
+
+
 }
