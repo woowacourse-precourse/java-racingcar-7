85be6b24f41fe8b31df86def7f398d4cc62f19e5
cherryiJuice <chldlwn711@gmail.com>
2024-10-26 20:30:58 +0900

    refactor: 접근 제한자 수정

--- src/main/java/racingcar/service/ThresholdScoreMoveRule.java
+++ src/main/java/racingcar/service/ThresholdScoreMoveRule.java
@@ -4,8 +4,8 @@ public final class ThresholdScoreMoveRule implements MoveRule {
     private static final int MIN_MOVE_NUMBER = 4;
     private static final int FORWARD = 1;
     private static final int STOP = 0;
-    public static final int MIN_NUM = 0;
-    public static final int MAX_NUM = 9;
+    private static final int MIN_NUM = 0;
+    private static final int MAX_NUM = 9;
 
     @Override
     public int tryMove() {
@@ -19,5 +19,5 @@ public final class ThresholdScoreMoveRule implements MoveRule {
     private int generateNumber() {
         return NumberGenerator.generateNumber(MIN_NUM, MAX_NUM);
     }
-    
+
 }
