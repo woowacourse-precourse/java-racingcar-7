bc6b335730407be06065909a14382f69114a6b27
차왕준 <ckdhdkwns@gmail.com>
2024-10-27 02:52:22 +0900

    test(CarsTest): 전체 자동차 이동 테스트
    해당 테스트 또한 좀 더 결정적인 방법으로 테스트하는 방법 필요

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,26 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CarsTest {
+    @Test
+    void 전체_자동차_이동 () {
+        Car car1 = new Car("pobi");
+        Car car2 = new Car("woni");
+        Car car3 = new Car("jun");
+
+        Cars cars = new Cars(Arrays.asList(car1, car2, car3));
+        cars.moveAll();
+
+        List<Car> movedCars = cars.getCars();
+
+        assertThat(movedCars)
+                .extracting(Car::getPosition)
+                .containsAnyOf(0, 1);
+    }
+}
