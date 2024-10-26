61c21e5552f2db414ebad8a20085a12159883f1e
DEV_WOONG <ywcsuwon@gmail.com>
2024-10-26 17:47:28 +0900

    feat: Car 클래스 추가
    - 생성자 추가
    - get메서드 추가

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -1,4 +1,19 @@
 package racingcar.domain.car;
 
 public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
