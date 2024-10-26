d70e5ff9c61956db9954f8cc0ee65348445707a3
chyo1 <hyowon3856@gmail.com>
2024-10-26 16:53:38 +0900

    fix : validateCarNamesInput 문자열 탐색 범위 수정

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -8,7 +8,7 @@ public class Validator {
         if (inputStr.isEmpty()) {
             throw new IllegalArgumentException("입력은 비어있을 수 없습니다");
         }
-        if (inputStr.charAt(0) == ',' || inputStr.charAt(inputStr.length()) == ',') {
+        if (inputStr.charAt(0) == ',' || inputStr.charAt(inputStr.length() - 1) == ',') {
             throw new IllegalArgumentException("구분자는 차 이름 사이에 위치할 수 있습니다.");
         }
     }
