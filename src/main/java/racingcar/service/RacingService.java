c1cc03cabf46c130e895c54101978b08c57f1737
chae02yo <chae02kim@naver.com>
2024-10-27 03:10:34 +0900

    feat: forward by random value
    - move one step when random value is 4 or more

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -8,15 +8,20 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RacingService {
     List<String> nameOfCar = new ArrayList<>();
     List<Integer> randomNum = new ArrayList<>();
+    List<Integer> movementNum = new ArrayList<>();
 
     public void separateCarNames (String namesOfCars) {
         nameOfCar = List.of(namesOfCars.split(","));
+        for (String s : nameOfCar) { // movementNum 초기화
+            movementNum.add(0);
+        }
     }
 
     public void race (int numberOfRaces) {
         while (numberOfRaces > 0) {
             randomNum.clear();
             setRandomNum();
+            decideStopOrGo();
             numberOfRaces--;
         }
     }
@@ -25,6 +30,15 @@ public class RacingService {
         for (int i=0; i<nameOfCar.size(); i++) {
             int num = Randoms.pickNumberInRange(0,9);
             randomNum.add(num);
+        }
+    }
+
+    public void decideStopOrGo () {
+        for (int i=0; i<randomNum.size(); i++) {
+            if (randomNum.get(i) >= 4) {
+                int increaseMovementNum = movementNum.get(i)+1;
+                movementNum.set(i, increaseMovementNum);
+            }
         }
     }
 }
