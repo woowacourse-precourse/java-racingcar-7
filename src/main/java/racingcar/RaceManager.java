3a99cf5b3363b2ac07d8daccdc55db54cfbd1b49
maedachaeva <parkchaeyeon.b@gmail.com>
2024-10-26 10:56:57 +0900

    feat: 자동차가 전진하는 조건을 판단하는 기능 구현

--- src/main/java/racingcar/RaceManager.java
+++ src/main/java/racingcar/RaceManager.java
@@ -3,6 +3,11 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RaceManager {  // raceCount만큼 반복
+    public boolean isCarMoving() {
+        int randomNumber = generateRandomNumber();
+        return randomNumber >= 4;
+    }
+
     private int generateRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
