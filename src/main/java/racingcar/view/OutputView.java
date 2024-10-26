aec85c5e322d6ec8b9c37e351001e33d27611a59
supernovaMK <minki5336@naver.com>
2024-10-27 01:32:32 +0900

    refactor: OutView 상수명 및 변수명 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,34 +1,34 @@
 package racingcar.view;
 
 public class OutputView {
-    private static final String STARTINGMESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String COUNTINGMESSAGE = "시도할 횟수는 몇 회인가요?";
-    private static final String MIDDLEPRINTMESSAGE = "실행 결과";
-    private static final String WINNERMESSAGE = "최종 우승자 : ";
+    private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String COIN_INSERT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String EXECUTE_RESULT_MESSAGE = "실행 결과";
+    private static final String WINNERS_MESSAGE = "최종 우승자 : ";
 
-    public static void startingMessage() {
-        System.out.println(STARTINGMESSAGE);
+    public static void printCarNameInputMessage() {
+        System.out.println(CAR_NAME_INPUT_MESSAGE);
     }
 
-    public static void insertCoinMessage() {
-        System.out.println(COUNTINGMESSAGE);
+    public static void printCoinInsertMessage() {
+        System.out.println(COIN_INSERT_MESSAGE);
     }
 
-    public static void middlePrintMessage() {
+    public static void printExecuteResultMessage() {
         System.out.println();
-        System.out.println(MIDDLEPRINTMESSAGE);
+        System.out.println(EXECUTE_RESULT_MESSAGE);
     }
 
-    public static void middleResultMessage(String name, int count) {
+    public static void printRoundResultMessage(String name, int count) {
         String location = "-".repeat(count);
         System.out.println(name + " : " + location);
     }
 
-    public static void emptyLine() {
+    public static void printEmptyLine() {
         System.out.println();
     }
 
     public static void printWinner(String winners) {
-        System.out.println(WINNERMESSAGE + winners);
+        System.out.println(WINNERS_MESSAGE + winners);
     }
 }
