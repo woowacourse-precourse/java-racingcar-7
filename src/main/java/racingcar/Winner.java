fc60c9683bdca0c4ffd2e9456705574d709abd91
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-10-26 21:32:05 +0900

    feat(Winner): 우승자 출력 구현

--- src/main/java/racingcar/Winner.java
+++ src/main/java/racingcar/Winner.java
@@ -1,4 +1,30 @@
 package racingcar;
 
 public class Winner {
+    public void win(int[] arr,String[] strings){
+        StringBuilder winners = new StringBuilder();
+        int max=0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                winners.setLength(0);
+                winners.append(strings[i]);
+            } else if (arr[i] == max) {
+                winners.append(", ").append(strings[i]);
+            }
+        }
+        for (int i = 0; i < strings.length; i++) {
+            String score;
+            if (arr[i] > 0) {
+                score = String.valueOf(arr[i]);
+            } else {
+                score = "-";
+            }
+            System.out.print(strings[i] + " : " + score);
+            if (i < strings.length) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("최종 우승자 : " + winners);
+    }
 }
