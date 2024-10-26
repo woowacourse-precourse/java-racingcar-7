84b55476cd855f685d518247488f9ae89907ca9c
Avocado <ujubongbong@gmail.com>
2024-10-26 19:07:21 +0900

    :sparkles: feat: 빈 문자열인지 확인하는 함수

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,9 +1,17 @@
 package racingcar;
 
+import org.junit.platform.commons.util.StringUtils;
+
 public class Input {
     public void validateBlank(String input) {
         if (input.contains(" ")) {
             throw new IllegalArgumentException("입력에 공백이 포함되어 있습니다.");
+        }
+    }
+
+    public void validateEmpty(String input) {
+        if (StringUtils.isBlank(input)) {
+            throw new IllegalArgumentException("문자열 입력이 필요합니다.");
         }
     }
 }
