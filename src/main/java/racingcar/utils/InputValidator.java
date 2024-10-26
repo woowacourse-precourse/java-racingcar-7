b1bc9132094fa6b0c9ed0bcb74dd7c9785337cbe
MCshin00 <sinminc7@gmail.com>
2024-10-26 19:37:59 +0900

    refactor: 시도 횟수 검증 함수 수정
    - 정규표현식을 사용해 양의 정수를 제외한 값과 10진수를 제외한 값을 받으면 예외가 발생하도록 수정

--- src/main/java/racingcar/utils/InputValidator.java
+++ src/main/java/racingcar/utils/InputValidator.java
@@ -1,16 +1,16 @@
 package racingcar.utils;
 
+import java.util.regex.Pattern;
+
 public class InputValidator {
+    private static final String NUMBER_REGEX = "^[1-9][0-9]*$";
+
     public static void validateRoundCount(String roundCountInput) {
-        try {
-            int round = Integer.parseInt(roundCountInput);
-            if(round <= 0) {
-                throw new IllegalArgumentException("시도할 횟수는 양의 정수만 입력할 수 있습니다.");
-            }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("시도할 횟수는 숫자만 입력할 수 있습니다.");
+        if (!Pattern.matches(NUMBER_REGEX, roundCountInput)) {
+            throw new IllegalArgumentException("시도할 횟수는 양의 정수만 입력할 수 있습니다.");
         }
     }
+
 
     public static void validateNullOrEmpty(String input) {
         if (input == null || input.isBlank()) {
