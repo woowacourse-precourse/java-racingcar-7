0b6574de5cb13b6b74e6cd0c79d939c462db7a1a
SeoMoonk <sunghoon1216@naver.com>
2024-10-27 06:29:27 +0900

    feat: 주행 횟수 유효성 검사 기능 구현 validator를 통해 주행 횟수가 음수나 소수인지, 숫자가 맞는지 검사함

--- src/main/java/racingcar/exception/RacingCarErrorCode.java
+++ src/main/java/racingcar/exception/RacingCarErrorCode.java
@@ -4,7 +4,10 @@ import static racingcar.constant.RacingCarStatic.MAX_CAR_NAME_LENGTH;
 
 public enum RacingCarErrorCode {
 
-    CAR_NAME_CANNOT_OVER_LIMIT("자동차의 이름은 " + MAX_CAR_NAME_LENGTH + "자를 넘을 수 없습니다.");
+    CAR_NAME_CANNOT_OVER_LIMIT("자동차의 이름은 " + MAX_CAR_NAME_LENGTH + "자를 넘을 수 없습니다."),
+    RACE_COUNT_CANNOT_BE_DECIMAL("주행 횟수에는 소수가 입력될 수 없습니다."),
+    RACE_COUNT_CANNOT_BE_NEGATIVE_NUMBER("주행 횟수에는 음수가 입력될 수 없습니다."),
+    RACE_COUNT_MUST_BE_NUMBER("주행 횟수는 반드시 숫자로 입력되어야 합니다.");
 
     private final String errorMsg;
 
--- src/main/java/racingcar/util/RacingCarValidator.java
+++ src/main/java/racingcar/util/RacingCarValidator.java
@@ -2,12 +2,45 @@ package racingcar.util;
 
 import static racingcar.constant.RacingCarStatic.MAX_CAR_NAME_LENGTH;
 import static racingcar.exception.RacingCarErrorCode.CAR_NAME_CANNOT_OVER_LIMIT;
+import static racingcar.exception.RacingCarErrorCode.RACE_COUNT_CANNOT_BE_DECIMAL;
+import static racingcar.exception.RacingCarErrorCode.RACE_COUNT_CANNOT_BE_NEGATIVE_NUMBER;
+import static racingcar.exception.RacingCarErrorCode.RACE_COUNT_MUST_BE_NUMBER;
+
+import java.math.BigInteger;
 
 public class RacingCarValidator {
 
     public void validateCarName(String carName) {
+
+        //TODO : 아래것 다른 이름으로 분리하여 호출 (EX. 길이 검사)
         if (carName.length() > MAX_CAR_NAME_LENGTH) {
             throw new IllegalArgumentException(CAR_NAME_CANNOT_OVER_LIMIT.getErrorMsg());
+        }
+    }
+
+    public void validateRaceCount(String raceCount) {
+        raceCountParsingValidate(raceCount);
+        raceCountDecimalValidate(raceCount);
+        raceCountNegativeNumberValidate(raceCount);
+    }
+
+    public void raceCountDecimalValidate(String raceCount) {
+        if (raceCount.contains(".")) {
+            throw new IllegalArgumentException(RACE_COUNT_CANNOT_BE_DECIMAL.getErrorMsg());
+        }
+    }
+
+    public void raceCountNegativeNumberValidate(String raceCount) {
+        if(raceCount.contains("-")) {
+            throw new IllegalArgumentException(RACE_COUNT_CANNOT_BE_NEGATIVE_NUMBER.getErrorMsg());
+        }
+    }
+
+    public void raceCountParsingValidate(String raceCount) {
+        try {
+            BigInteger number = new BigInteger(raceCount);
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException(RACE_COUNT_MUST_BE_NUMBER.getErrorMsg());
         }
     }
 }
