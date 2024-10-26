438ecd6f5212b6003f45d22c73d42ebe5d7acdb4
taek2222 <oot309@daum.net>
2024-10-26 22:47:17 +0900

    refactor(GameController): 게임 컨트롤러 역할 분리 작업
    - 컨트롤러의 우승자 선별, 계산등의 다양한 역할을 분리했습니다.

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,12 +1,8 @@
 package racingcar.controller;
 
-import static racingcar.util.InputValidator.validateNoDuplicates;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import racingcar.domain.Car;
-import racingcar.util.RandomUtil;
+import racingcar.domain.Cars;
+import racingcar.domain.Winner;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -20,63 +16,35 @@ public class GameController {
     }
 
     public void run() {
-        List<Car> cars = initGame();
+        Cars cars = initCars();
         startGame(cars);
 
-        Car manyMoveCar = cars.getFirst();
-
-        for (Car car : cars) {
-            if (car.compareDistance(manyMoveCar) == 1) {
-                manyMoveCar = car;
-            }
-        }
-
-        List<String> winnerPlayers = new ArrayList<>();
-        for (Car car : cars) {
-            if (car.compareDistance(manyMoveCar) == 0)
-                winnerPlayers.add(car.getName());
-        }
-
-        String join = String.join(", ", winnerPlayers);
-        outputView.printWinnerPlayers(join);
+        Winner winner = winnerPlayers(cars);
+        endGame(winner);
     }
 
-    private void startGame(List<Car> cars) {
-        int totalRoundCount = getTotalRoundCount();
+    private void endGame(Winner winner) {
+        String info = winner.winnerInfo();
+        outputView.printWinnerPlayers(info);
+    }
+
+    private Winner winnerPlayers(Cars cars) {
+        Car manyMoveCar = cars.getManyMoveCar();
+        return new Winner(cars, manyMoveCar);
+    }
+
+    private Cars initCars() {
+        String inputCarNames = inputView.inputCarNames();
+        return new Cars(inputCarNames);
+    }
+
+    private void startGame(Cars cars) {
+        int totalRoundCount = inputView.inputPlayCount();
 
         outputView.printRunResult();
         for (int round = 0; round < totalRoundCount; round++) {
-            playRound(cars);
-            outputView.printRoundResult(cars);
+            cars.playRound();
+            outputView.printRoundResult(cars.getCarStates());
         }
-    }
-
-    private int getTotalRoundCount() {
-        return inputView.inputPlayCount();
-    }
-
-    private void playRound(List<Car> cars) {
-        cars.forEach(car -> {
-            int randomNumber = RandomUtil.generateRandomNumber();
-            car.moveForward(randomNumber);
-        });
-    }
-
-    private List<Car> initGame() {
-        List<String> carList = getCarNames();
-        validateNoDuplicates(carList);
-
-        return createCars(carList);
-    }
-
-    private List<String> getCarNames() {
-        String inputCarsName = inputView.inputCarsName();
-        return Arrays.asList(inputCarsName.split(","));
-    }
-
-    private List<Car> createCars(List<String> carList) {
-        return carList.stream()
-                .map(Car::new)
-                .toList();
     }
 }
