722924982c053d22de0b8fbcbc324f465acde106
jonghwanlee <kavae550@gmail.com>
2024-10-27 00:51:31 +0900

    fix(view): InputView.java 문법 오류 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,12 +5,12 @@ import racingcar.view.constants.Notice;
 
 public class InputView {
     public static String requestCarNames(){
-        System.out.println(Notice.ASK_CAR_NAMES);
+        System.out.println(Notice.ASK_CAR_NAMES.getMessage());
         return Console.readLine();
     }
 
     public static String requestNumberOfTrials(){
-        System.out.println(Notice.ASK_NUMBER_OF_TRIALS);
+        System.out.println(Notice.ASK_NUMBER_OF_TRIALS.getMessage());
         return Console.readLine();
     }
 
