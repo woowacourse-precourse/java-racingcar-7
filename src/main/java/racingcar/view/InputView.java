a9e4a9729b897f80cf53b974bb80449a79528df7
Seungwan Yoo <ysw99@kakao.com>
2024-10-27 00:08:08 +0900

    feat: 입력 view 로직 병합

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,11 +3,7 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String getCarNames() {
+    public static String getInput() {
         return Console.readLine();
-    }
-
-    public static int getAttemptCount() {
-        return Integer.parseInt(Console.readLine());
     }
 }
