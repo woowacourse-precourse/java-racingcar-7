4e7749b4bb6b63ac69cd85e6cb92b22433cb2fb5
장시영 <sysmile77@gmail.com>
2024-10-26 17:25:19 +0900

    feat(Input) : 입력예외상황 추가

--- src/main/java/racingcar/io/Input.java
+++ src/main/java/racingcar/io/Input.java
@@ -3,34 +3,59 @@ package racingcar.io;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Input {
 
-    public static List<String> inputCarName() {
+    public static ArrayList<String> inputCarName() {
 
         System.out.println("참여할 자동차 이름을 알려주세요");
+
+        // "apple,orange,3,4"
         String input = readLine();
 
         return splitCarName(input);
-    }
-
-    public static List<String> splitCarName (String input) {
-
-        // 이름은 콤마(,) 로만 구분 - 예외 사항 없다고 가정
-        String[] carNameArray = input.split(",");
-
-        List<String> carNameList = new ArrayList<>();
-
-//        for(String name : carNameArray) {
-//            carNameList.add(name);
-//        }
-
-        carNameList.addAll(Arrays.asList(carNameArray));
-
-        return carNameList;
 
     }
 
-}
+    public static ArrayList<String> splitCarName(String input) {
+
+        String[] str = input.split(",");
+
+        carNameValidation(str);
+
+        ArrayList<String> splitInput = new ArrayList<>();
+
+        for(String name : str){
+            splitInput.add(name);
+        }
+
+        return splitInput;
+    }
+
+    public static boolean carNameValidation(String[] str) {
+
+        if(str.length == 0) {
+            throw new IllegalArgumentException("이름을 입력해주세요, 비어있는 이름이 있습니다. ");
+        }
+
+        for(String name : str) {
+            if(name.length() == 0) {
+                throw new IllegalArgumentException("이름을 입력해주세요, 비어있는 이름이 있습니다. ");
+            }
+
+            if(name.length()>5)
+                throw new IllegalArgumentException("이름을 5자 이내로 입력하세요.");
+        }
+            return true;
+    }
+
+        public static void print (ArrayList<String> prints) {
+            for (String print : prints) {
+                System.out.println("*"+print);
+            }
+        }
+
+    }
