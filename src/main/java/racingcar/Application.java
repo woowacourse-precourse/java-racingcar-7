3a435ebff74be298725cbd9733d0f87b4964d18f
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-26 18:17:42 +0900

    chore: Application 클래스에 게임 실행을 위한 구성 요소 추가 - CarController와 필요한 서비스 및 뷰 객체 생성 및 초기화 - main 메서드에서 자동차 경주 게임 실행 시작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import racingcar.controller.CarController;
+import racingcar.service.CarMoveManager;
+import racingcar.service.InputSplitter;
+import racingcar.service.RandomNumberGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CarMoveManager carMoveManager = new CarMoveManager();
+        InputSplitter inputSplitter = new InputSplitter();
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        CarController carController = new CarController(carMoveManager, inputSplitter, randomNumberGenerator,
+                inputView, outputView);
+
+        carController.play();
     }
 }
