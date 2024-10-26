ee2765b4d0af4bd3fb4e879c58d231672462d84c
hamlsy <dltmddud1122@naver.com>
2024-10-26 16:31:05 +0900

    feat: Racingcars 테스트 코드 구현

--- src/main/java/racingcar/view/ErrorView.java
+++ src/main/java/racingcar/view/ErrorView.java
@@ -2,7 +2,7 @@ package racingcar.view;
 
 public class ErrorView {
     private static final String ERROR_FLAG = "[ERROR]";
-    private static final String RACINGCAR_DUPLICATE_NAME_ERROR_MESSAGE = ERROR_FLAG + " 레이싱 자동차 이름은 중복될 수 없습니다.";
+    private static final String RACINGCAR_DUPLICATE_NAME_ERROR_MESSAGE = ERROR_FLAG + " 자동차 이름은 중복될 수 없습니다.";
     private static final String RACINGCAR_BLANK_NAME_ERROR_MESSAGE = ERROR_FLAG + " 자동차 이름에 공백을 입력할 수 없습니다.";
     private static final String RACINGCAR_LENGTH_NAME_ERROR_MESSAGE = ERROR_FLAG + " 자동차 이름의 길이는 5자 이하만 가능합니다.";
     private static final String RACINGGAME_COUNT_ERROR_MESSAGE = ERROR_FLAG + " 시도 횟수는 양의 정수를 입력해야 합니다.";
--- src/test/java/racingcar/RacingcarTest.java
+++ src/test/java/racingcar/RacingcarTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 public class RacingcarTest {
 
     @Test
-    void 이름이_공백일때_테스트(){
+    void 이름이_공백일_때_테스트(){
         //given
         String name1 = "";
         String name2 = " ";
@@ -30,7 +30,7 @@ public class RacingcarTest {
     }
 
     @Test
-    void 이름이_5글자_초과일때_테스트(){
+    void 이름이_5글자_초과일_때_테스트(){
         //given
         String name1 = "abcdef";
         String name2 = "abcdefghi";
