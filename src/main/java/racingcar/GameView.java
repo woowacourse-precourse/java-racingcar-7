3836aaf306ad4e67d79044a139548e2cc32d97dd
gorapaduckoo <jioung93@naver.com>
2024-10-27 03:32:50 +0900

    feat: 자동차명 입력 메세지 출력 기능 추가

---
+++ src/main/java/racingcar/GameView.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+import static racingcar.OutputMessage.*;
+
+public class GameView {
+    public void printCarNameInputMessage() {
+        System.out.println(CAR_NAME_INPUT_MESSAGE.getMessage());
+    }
+}
