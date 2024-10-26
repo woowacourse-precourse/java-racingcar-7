62cef6c79470878b1ec25f18c6e03e8ef184608c
김건학 <kkh10002@naver.com>
2024-10-26 22:32:46 +0900

    refactor(Application, AppConfig): 컨트롤러 및 서비스 객체 생성을 한 곳에서 관리하도록 변경
    AppConfig 클래스를 사용하여 컨트롤러 및 서비스 객체 생성을 한 곳에서 관리하도록 변경했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import java.util.List;
+import racingcar.config.AppConfig;
 import racingcar.controller.CarController;
 import racingcar.controller.Controller;
 import racingcar.controller.InputController;
@@ -8,24 +9,16 @@ import racingcar.controller.OutputController;
 import racingcar.controller.RaceController;
 import racingcar.domain.Car;
 import racingcar.domain.Race;
-import racingcar.service.CarService;
-import racingcar.service.RaceService;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
+        AppConfig appConfig = new AppConfig();
 
-        CarService carService = new CarService();
-        RaceService raceService = new RaceService();
-
-        InputController inputController = new InputController(inputView);
-        OutputController outputController = new OutputController(outputView);
-        CarController carController = new CarController(carService);
-        RaceController raceController = new RaceController(raceService);
-        Controller controller = new Controller(raceService, outputView);
+        Controller controller = appConfig.createController();
+        CarController carController = appConfig.createCarController();
+        RaceController raceController = appConfig.createRaceController();
+        InputController inputController = appConfig.createInputController();
+        OutputController outputController = appConfig.createOutputController();
 
         String inputCarNames = inputController.inputCarNames();
         int inputAttemptCount = inputController.inputAttemptCount();
@@ -35,7 +28,7 @@ public class Application {
 
         Race race = raceController.registerRace(carList, inputAttemptCount);
         controller.playGame(race);
-
+        
         List<Car> winnerCarList = raceController.getWinnerCarList(race);
         outputController.printWinners(winnerCarList);
     }
---
+++ src/main/java/racingcar/config/AppConfig.java
@@ -0,0 +1,45 @@
+package racingcar.config;
+
+import racingcar.controller.CarController;
+import racingcar.controller.Controller;
+import racingcar.controller.InputController;
+import racingcar.controller.OutputController;
+import racingcar.controller.RaceController;
+import racingcar.service.CarService;
+import racingcar.service.RaceService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class AppConfig {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final CarService carService;
+    private final RaceService raceService;
+
+    public AppConfig() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.carService = new CarService();
+        this.raceService = new RaceService();
+    }
+
+    public InputController createInputController() {
+        return new InputController(inputView);
+    }
+
+    public OutputController createOutputController() {
+        return new OutputController(outputView);
+    }
+
+    public CarController createCarController() {
+        return new CarController(carService);
+    }
+
+    public RaceController createRaceController() {
+        return new RaceController(raceService);
+    }
+
+    public Controller createController() {
+        return new Controller(raceService, outputView);
+    }
+}
