4e77cf477dc404a92511abd3efaba4f7b8044825
김동진 <dongjin403@naver.com>
2024-10-26 16:13:16 +0900

    feat : 만든 메소드들을 사용하여 프로그램 로직 작성
    여태까지 만든 메소드를 모두 사용하여 main 실행 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import console.ConsoleUtil;
+import java.util.ArrayList;
+import util.CarUtil;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String carNames = ConsoleUtil.readCarName();
+        int racingCounts = ConsoleUtil.readRacingCount();
+        ConsoleUtil.closeConsole();
+
+        ArrayList<Car> cars = CarUtil.makeCarList(carNames);
+        for (int i = 0; i < racingCounts; i++) {
+            for (Car car : cars) {
+                CarUtil.advanceSixTimesOutOfTen(car);
+                car.printRacingInfo();
+            }
+            System.out.println();
+        }
+        CarUtil.printWinners(cars);
     }
 }
