4498b3cde702deb16d81cbf13d738bfeb6c67584
min-0 <dhe08133@naver.com>
2024-10-27 01:07:47 +0900

    feat: 무작위 값 추출
    무작위 값이 4이상일 경우 전진(-) 및 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
@@ -11,6 +11,22 @@ public class Application {
         System.out.println("시도할 횟수는 몇 회인가요?");
         int num = Integer.parseInt(Console.readLine());
 
-        List<String> carList = List.of(input.split(","));
+        System.out.println("실행 결과");
+
+        String[] carList = input.split(",");
+        int[] carIndex = new int[carList.length];
+
+        for (int i = 0; i < num; i++) {
+            for (int k = 0; k < carList.length; k++) {
+                int randomValue = Randoms.pickNumberInRange(0, 9);
+                if (randomValue >= 4) {
+                    carIndex[k]++;
+                }
+            }
+            for (int j = 0; j < carList.length; j++) {
+                System.out.println(carList[j] + " : " + "-".repeat(carIndex[j]));
+            }
+            System.out.println();
+        }
     }
 }
