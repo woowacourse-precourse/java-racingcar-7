594de114da6f1edc4a2c610383e019b95907c0e2
woodongle <315ehdgur@gmail.com>
2024-10-26 15:13:01 +0900

    fix: 사용자 입력 저장, 첫 번째 메시지에 대한 입력 검증 수정
    1. 사용자 입력을 단순 문자열로 받는 것이 아니라 공백을 제거하고 쉼표로 구분해서 가변 컬렉션에 저장하도록 수정합니다.
      - String -> ArrayList
    2. 사용자 입력을 검증하는 메서드에서 빈 문자열을 제거 하고 특수 문자가 있는지 확인하는 로직으로 수정합니다.
      - 문자와 공백이 있는지만 확인 -> 빈 문자열을 제거 하고 특수 문자 있는지 확인
    수정한 이유:
      1. 입력 검증을 더 확실하게 하기 위해.
      2. List로 저장되게 하고 불변이 아닌 가변 컬렉션을 사용해 빈 문자열을 제거할 수 있게 하기 위해서.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,25 +1,44 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
 
 public class Application {
     public static void main(String[] args) {
         // 경주할 자동차 이름 입력(참가자 수)
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
+
+        // 사용자 입력에서 공백을 제거하고 쉼표로 구분해서 가변 컬렉션인 ArrayList에 저장
+        List<String> inputList = new ArrayList<>(Arrays.asList(Console.readLine().split("\\s*,\\s*")));
 
         try {
-            validateInput(input);
-            System.out.println(input);
+            validateInput(inputList);
+            System.out.println(inputList);
         } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
         }
     }
 
-    // 사용자 입력에 특수 문자와 공백이 있는지 확인하는 메서드
-    private static void validateInput(String input) {
-        if (input.matches(".*[ \'`%@:=>$_#+\\[;~!(*)\\]&?}\\{/.<|\\-\"^\\\\].*")) {
-            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+    // 사용자 입력에서 빈 문자열을 제거 하고 특수 문자가 있는지 확인하는 메서드
+    private static void validateInput(List inputList) {
+        ListIterator<String> iterator = inputList.listIterator();
+
+        while (iterator.hasNext()) {
+            String str = iterator.next();
+
+            // 공백을 제거한 후 빈 문자열이 되면 List에서 제거하고 다음 인덱스로 넘어감
+            if (str.trim().isEmpty()) {
+                iterator.remove();
+                continue;
+            }
+
+            // List에 저장된 문자열에 특수 문자가 있는지 정규 표현식을 사용해 검증
+            if (str.matches(".*[`~!@#$%^&*()\\-_=+{}\\[\\];:'\",.<>/?|\\\\].*")) {
+                throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+            }
         }
     }
 }
