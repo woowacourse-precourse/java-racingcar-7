a3b6d75b20810efd2287ef7a7d8cab684b3332cd
himinqq <alsrud0966@naver.com>
2024-10-27 02:59:01 +0900

    feat(OutputView): 게임 실행 결과를 출력하는 기능 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,29 @@
+package racingcar.view;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import racingcar.domain.Car;
+import racingcar.domain.RacingGame;
+
+public class OutputView {
+
+    public static void resultMessage(){
+        System.out.println("실행 결과");
+    }
+
+    public static String positionToDash(int position){
+        return IntStream.range(0,position)
+                .mapToObj(i->"-")
+                .collect(Collectors.joining());
+    }
+
+    public static void printCarPositions(RacingGame racingGame){
+        List<Car> cars = racingGame.getCars();
+        for(Car car : cars){
+            System.out.println(car.getName()+ " : " + positionToDash(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+}
