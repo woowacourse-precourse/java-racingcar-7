1fe0bf26c557d9d9a01d65827a2fd6658f956f59
rootTiket <l20078829@gmail.com>
2024-10-27 00:08:33 +0900

    feat: 5글자이상 입력이 있을 경우 예외 처리를 위한 에러 메시지 생성

---
+++ src/main/java/racingcar/View/constant/ErrorMessage.java
@@ -0,0 +1,16 @@
+package racingcar.View.constant;
+
+import racingcar.global.globalEnum.Message;
+
+public enum ErrorMessage implements Message {
+    TOO_LONG_VALUE("문자열은 5글자를 넘을 수 없습니다");
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
