e8b072f1871c5a0184677e651172f2088878fcf0
Yehun <yhyh4420@naver.com>
2024-10-26 12:09:49 +0900

    refactor(main):View 클래스 변화 반영

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -4,15 +4,16 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Controller {
-    View view = new View();
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
     Model model = new Model();
 
     public void run() {
-        String[] listUser = view.getUserinfo();
-        String round = view.getRoundinfo();
+        String[] listUser = inputView.getUserinfo();
+        String round = inputView.getRoundinfo();
         HashMap<String, String> initRacingHashmap = model.initRacing(listUser);
         HashMap<String, String> RacingHashmap = model.Racing(initRacingHashmap, round);
         List listWinners = model.findWinner(RacingHashmap);
-        view.displayResult(listWinners);
+        outputView.displayResult(listWinners);
     }
 }
