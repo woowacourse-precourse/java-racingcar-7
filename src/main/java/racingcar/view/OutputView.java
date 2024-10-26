2a39fe4f585e98ac01574f26f644fda559594118
eunseobb <seobseob0810@naver.com>
2024-10-26 18:27:53 +0900

    feat(view): OutputView
    입력받은 우승자 목록을 기능 요구사항에 맞는 형식으로 출력하는 메서드 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,12 +5,18 @@ import java.util.stream.IntStream;
 import racingcar.model.Car;
 
 public class OutputView {
+    public void printWinner(List<String> winners) {
+        String winnerNames = String.join(", ", winners);
+        System.out.println("최종 우승자 : " + winnerNames);
+    }
+
     public void printEachTime(List<Car> carList) {
         carList.forEach(car -> {
             System.out.print(car.getName() + " : ");
             printDash(car.getTotalMovement());
             System.out.println();
         });
+        System.out.println();
     }
 
     private void printDash(Integer movement) {
