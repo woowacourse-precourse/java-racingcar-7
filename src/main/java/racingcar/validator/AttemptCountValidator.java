d775897060c352a653bc9cd76daf31c0e6883092
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-26 23:50:21 +0900

    feat(AttemptCountValidator): 사용자 입력 값 중 시도할 횟수 유효성 검사 기능

--- src/main/java/racingcar/validator/AttemptCountValidator.java
+++ src/main/java/racingcar/validator/AttemptCountValidator.java
@@ -1,4 +1,13 @@
 package racingcar.validator;
 
-public class AttemptCountValidator {
+public class AttemptCountValidator implements Validator {
+
+    private static final String ERROR_NOT_NON_NEGATIVE_INTEGER = "Input a non-negative integer.";
+
+    @Override
+    public void validate(String input) {
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException(ERROR_NOT_NON_NEGATIVE_INTEGER);
+        }
+    }
 }
