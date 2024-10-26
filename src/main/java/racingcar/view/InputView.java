923b10dcd8e1cbc941f46522ab7acb1802ebfe10
pakaheo <k70923@konkuk.ac.kr>
2024-10-26 22:03:32 +0900

    feat : Input car names and number of attempts
    InputView input car names and number of attempts by Console.readLine

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,2 +1,20 @@
-package racingcar.view;public class InputView {
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String promptInputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        return Console.readLine();
+    }
+
+    public static String promptInputTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?\n");
+        return Console.readLine();
+    }
+
+    public static void close() {
+        Console.close();
+    }
 }
