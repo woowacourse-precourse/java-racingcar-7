7ae97fadc017f7699bab82052ba0085e8a79572b
mikekks <thdalsrb79@naver.com>
2024-10-26 18:21:21 +0900

    feat(RacingGame): 전체적인 게임 흐름 통제하는 클래스 정의

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,15 @@
+package racingcar.domain;
+
+import java.util.List;
+
+import racingcar.common.Random;
+import racingcar.view.Input;
+import racingcar.view.Output;
+
+public class RacingGame {
+	private final Random random;
+
+	public RacingGame(Random random) {
+		this.random = random;
+	}
+}
