416f9a9757558eb7afeddc15c013231ca23466a1
Seol_JY <wlsdud5654@gmail.com>
2024-10-27 03:44:43 +0900

    feat(vo): 이동 횟수에 대한 검증 기능 구현

--- src/main/java/racingcar/constant/ExceptionMessage.java
+++ src/main/java/racingcar/constant/ExceptionMessage.java
@@ -4,7 +4,8 @@ public enum ExceptionMessage {
     NULL_OR_EMPTY_INPUT("값을 입력해주세요."),
     INPUT_TOO_LONG("값이 너무 큽니다."),
     INVALID_NUMBER_FORMAT("숫자만 입력 가능합니다."),
-    INVALID_CAR_NAME_LENGTH("자동차 이름은 %d자 이상 %d자 이하만 가능합니다");
+    INVALID_CAR_NAME_LENGTH("자동차 이름은 %d자 이상 %d자 이하만 가능합니다"),
+    INVALID_ROUND_MIN("라운드는 %d 이상이어야 합니다.");
 
     private final String message;
 
--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -3,6 +3,7 @@ package racingcar.controller;
 import java.util.List;
 import racingcar.view.InputView;
 import racingcar.vo.Name;
+import racingcar.vo.Round;
 
 public class RacingController {
     private final InputView inputView;
@@ -21,11 +22,11 @@ public class RacingController {
 
     public void run() {
         String userInput = inputView.getCarNames();
-        List<String> splitNames = stringSplitter.split(userInput);
-        List<Name> names = splitNames.stream().map(Name::from).toList();
+        List<String> splitString = stringSplitter.split(userInput);
+        List<Name> names = splitString.stream().map(Name::from).toList();
 
         userInput = inputView.getTotalRace();
         Integer parsedNumber = numberParser.parse(userInput);
-
+        Round round = Round.from(parsedNumber);
     }
 }
