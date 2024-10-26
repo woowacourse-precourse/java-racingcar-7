85d63ad00d843efb5c1ce8163d662db77b15fd30
changuii <rhljh7410@gmail.com>
2024-10-26 10:53:32 +0900

    feat(RacingCarGameController) : RacingCarGame과 RacingCarView를 연결해주는 RacingCarGameController를 만들어라

---
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -0,0 +1,27 @@
+package racingcar.controller;
+
+import racingcar.domain.RacingCarGameResult;
+import racingcar.service.RacingCarGame;
+import racingcar.view.InputRacingCarView;
+import racingcar.view.OutputRacingCarView;
+
+public class RacingCarGameController {
+    private final RacingCarGame racingCarGame;
+    private final InputRacingCarView inputRacingCarView;
+    private final OutputRacingCarView outputRacingCarView;
+    public RacingCarGameController(RacingCarGame racingCarGame){
+        this.racingCarGame = racingCarGame;
+        this.inputRacingCarView = new InputRacingCarView();
+        this.outputRacingCarView = new OutputRacingCarView();
+    }
+
+    public void run(){
+        String racingCarNames = inputRacingCarView.inputCarName();
+        int moveCount = Integer.parseInt(inputRacingCarView.inputCarMoveCount());
+
+        RacingCarGameResult gameResult = racingCarGame.playRacingGame(racingCarNames, moveCount);
+
+        outputRacingCarView.outputGameResult(gameResult.getGameResult());
+        outputRacingCarView.outputGameWinners(gameResult.getGameWinnerNames());
+    }
+}
--- src/main/java/racingcar/view/OutputRacingCarView.java
+++ src/main/java/racingcar/view/OutputRacingCarView.java
@@ -1,20 +1,15 @@
 package racingcar.view;
 
-import java.util.List;
-import racingcar.domain.RacingCar;
 
 public class OutputRacingCarView {
-    private static final String GAME_RESULT_OUTPUT = "실행 결과\n%s";
+    private static final String GAME_RESULT_OUTPUT = "\n실행 결과\n%s";
     private static final String GAME_WINNERS_OUTPUT = "최종 우승자 : %s";
 
     public void outputGameResult(String gameResult) {
         System.out.printf(GAME_RESULT_OUTPUT, gameResult);
     }
 
-    public void outputGameWinners(List<RacingCar> winners) {
-        String[] gameWinners = winners.stream()
-                .map(winner -> winner.getName())
-                .toArray(String[]::new);
-        System.out.printf(GAME_WINNERS_OUTPUT, String.join(", ", gameWinners));
+    public void outputGameWinners(String winners) {
+        System.out.printf(GAME_WINNERS_OUTPUT, winners);
     }
 }
