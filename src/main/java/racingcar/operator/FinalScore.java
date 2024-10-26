1abae08cb4d4e58ff79580eff9e9faf7a010bb34
MinJae00 <exceedmin@gmail.com>
2024-10-27 01:01:55 +0900

    feat: After the game is over, scores are tallied to select a winner

--- src/main/java/racingcar/operator/FinalScore.java
+++ src/main/java/racingcar/operator/FinalScore.java
@@ -1,2 +1,40 @@
-package racingcar.operator;public class FinalScore {
+package racingcar.operator;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.user.UserInfo;
+import racingcar.view.OutputView;
+
+public class FinalScore {
+    List<UserInfo> topScores = new ArrayList<>();
+    OutputView output = new OutputView();
+    int maxScore = Integer.MIN_VALUE;
+
+    public void totalScore(UserInfo[] users) {
+
+        for (UserInfo user : users) {
+            if (user.scoreNum > maxScore) {
+                maxScore = user.scoreNum;
+            }
+        }
+
+        for (UserInfo user : users) {
+            if (user.scoreNum == maxScore) {
+                topScores.add(user);
+            }
+        }
+        Winner();
+    }
+
+    private void Winner() {
+        output.PrintWinnerSentence();
+        int n = topScores.size();
+        for (UserInfo user : topScores) {
+            n--;
+            output.PrintWinner(user.name);
+            if (n != 0) {
+                output.PrintRest();
+            }
+        }
+    }
 }
