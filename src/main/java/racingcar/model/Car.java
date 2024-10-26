c18e50b8460e696fd246391994e3e368ad5d2ba7
username0w <grandcias123@gmail.com>
2024-10-27 04:32:41 +0900

    feat: 자동차 속성 나타내는 Car 모델 클래스 추가

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,24 @@
+package racingcar.model;
+
+public class Car {
+
+    private final String name;
+    private int location;
+
+    public Car(String name, int location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
+    }
+}
