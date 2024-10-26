f83c694e351911d0cb4e2bb0a89e83719e363c8d
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-26 23:47:48 +0900

    feat(ViewFactory): view 객체 생성 기능

--- src/main/java/racingcar/factory/ViewFactory.java
+++ src/main/java/racingcar/factory/ViewFactory.java
@@ -1,4 +1,23 @@
 package racingcar.factory;
 
+import racingcar.observer.RuntimeView;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+import racingcar.view.View;
+
 public class ViewFactory {
+
+    public static final String TYPE_INPUT = "Input";
+    public static final String TYPE_RUNTIME = "Runtime";
+    public static final String TYPE_OUTPUT = "Output";
+
+
+    public View create(String type) {
+        return switch (type) {
+            case TYPE_INPUT -> new InputView();
+            case TYPE_RUNTIME -> new RuntimeView();
+            case TYPE_OUTPUT -> new OutputView();
+            default -> throw new IllegalArgumentException();
+        };
+    }
 }
