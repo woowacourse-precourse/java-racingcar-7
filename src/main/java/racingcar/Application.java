eb07f87762b82a715540b56238a48965d499fd67
Mini <mini@MiniMac.local>
2024-10-26 14:23:22 +0900

    feat: add car count input

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("자동차의 수를 입력해주세요");
+
+        int countCars = Integer.parseInt(Console.readLine());
+        
+
+
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
