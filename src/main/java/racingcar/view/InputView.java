c4a0ded4bee3765fd51503801d962e5e581f3bc4
hyeondo <eun041212@gmail.com>
2024-10-26 18:27:28 +0900

    feat(Car) : 자동차 데이터 생성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,8 +1,21 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
-    public void printCarNamePrompt() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    public String readCarNames() {
+        System.out.println(Message.INPUT_CAR_NAME.message);
+        return Console.readLine();
+    }
+
+    private enum Message {
+        INPUT_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
     }
 }
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,28 +1,56 @@
 package racingcar.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest {
 
     private InputView inputView;
-    private static ByteArrayOutputStream outputStream;
+    private ByteArrayOutputStream outputStream;
+    private ByteArrayInputStream inputStream;
 
     @BeforeEach
     void setUp() {
         inputView = new InputView();
+
         outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
     }
 
+    @AfterEach
+    void tearDown() {
+        System.setOut(System.out);
+        System.setIn(System.in);
+    }
+
     @Test
     void 자동차_입력_프롬프트_출력() {
-        inputView.printCarNamePrompt();
+        setInputStream(".");
 
-        assertThat(outputStream.toString()).isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        inputView.readCarNames();
+
+        assertThat(outputStream.toString()).contains("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    }
+
+    @Test
+    void 자동차_입력_테스트() {
+        String input = "pobi,woni,jun";
+        setInputStream(input);
+
+        String carNames = inputView.readCarNames();
+
+        assertThat(carNames).isEqualTo(input);
+    }
+
+    void setInputStream(String input) {
+        inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
     }
 }
