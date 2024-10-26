e4d1bc17f7f51ae03a45d56584c6400f3816af79
Lee-Minju <mju7513@gmail.com>
2024-10-26 17:52:14 +0900

    domain\Rounds.java: 입력받은 경주 횟수를 검증하는 기능 구현

--- src/main/java/racingcar/domain/Rounds.java
+++ src/main/java/racingcar/domain/Rounds.java
@@ -10,7 +10,14 @@ public class Rounds {
     }
 
     private void validateNumber(String number) {
-
+        try{
+            int rounds = Integer.parseInt(number);
+            if(rounds < 1) {
+                throw new IllegalArgumentException("자연수를 입력해 주세요.");
+            }
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("숫자를 입력해 주세요");
+        }
     }
 
     public int rounds() {
