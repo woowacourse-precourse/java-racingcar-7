72e7ad914a02ace4621e387ab4c5332a57cc1f9a
StoneCAU <seokwoo1999@naver.com>
2024-10-27 00:41:10 +0900

    feat(FixNumberGenerator): List를 받아와 생성하는 생성자 구현
    - 시도횟수가 여러 번일 때를 테스트하기 위함

--- src/test/java/racingcar/utils/FixedNumberGenerator.java
+++ src/test/java/racingcar/utils/FixedNumberGenerator.java
@@ -1,14 +1,26 @@
 package racingcar.utils;
 
-public class FixedNumberGenerator implements NumberGenerator {
-    private final int number;
+import java.util.LinkedList;
+import java.util.List;
 
-    public FixedNumberGenerator(int number) {
+public class FixedNumberGenerator implements NumberGenerator {
+    private Integer number;
+    private final List<Integer> queue = new LinkedList<>();
+
+    public FixedNumberGenerator(Integer number) {
         this.number = number;
+    }
+
+    public FixedNumberGenerator(List<Integer> queue) {
+        this.queue.addAll(queue);
+        number = queue.getFirst();
     }
 
     @Override
     public int getNumber() {
+        if (!queue.isEmpty()) {
+            number = queue.removeFirst();
+        }
         return number;
     }
 }
