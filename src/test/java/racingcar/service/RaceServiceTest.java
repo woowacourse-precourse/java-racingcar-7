abec38d649f2e8da77cdf6f57c33e2b0266b2c98
slay <palgalow@gmail.com>
2024-10-26 21:10:23 +0900

    feat(RaceServiceTest): 우승자 결정하는 로직 테스트 기능 추가

--- src/test/java/racingcar/service/RaceServiceTest.java
+++ src/test/java/racingcar/service/RaceServiceTest.java
@@ -2,10 +2,12 @@ package racingcar.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
 
 public class RaceServiceTest {
 
@@ -23,5 +25,21 @@ public class RaceServiceTest {
         assertEquals(carNames[0], "pobi");
         assertEquals(carNames[1], "won");
         assertEquals(carNames[2], "juni");
+    }
+
+    @DisplayName("우승자 결정하는 기능 테스트")
+    @Test
+    public void decisionWinnersTest() {
+        List<Car> Cars = new ArrayList<>();
+        Cars.add(new Car("pobi"));
+        Cars.add(new Car("won"));
+        Cars.add(new Car("juni"));
+        Cars.get(0).move();
+        Cars.get(0).move();
+        Cars.get(1).move();
+        Cars.get(2).move();
+        Cars.get(2).move();
+        String[] winners = raceService.decisionWinners();
+        assertEquals(winners, "pobi,juni");
     }
 }
