d6edcf049ad9b4feb88f3963e3d699f6ca26727b
yyysolhhh <solbaram37@gmail.com>
2024-10-26 12:48:19 +0900

    feat(Output): printWinner 메서드 추가
    printWinner
    - StringBuilder를 이용해 우승자 이름 포맷 만들어서 출력
    - 출력 형식에 필요한 문자열 상수화

--- src/main/java/racingcar/io/Output.java
+++ src/main/java/racingcar/io/Output.java
@@ -1,7 +1,38 @@
 package racingcar.io;
 
 public class Output {
+    private static final String RACE_RESULT = "실행 결과";
+    private static final String WINNER_PREFIX = "최종 우승자 : ";
+    private static final String WINNER_DELIMITER = ", ";
+
     public static void printMessage(String message) {
         System.out.println(message);
+    }
+
+//    public static void printResult() {
+//        StringBuilder resultOutput = new StringBuilder(RACE_RESULT);
+//
+//    }
+
+    public static void printWinner(String[] winnerNames) {
+        StringBuilder winnersOutput = new StringBuilder(WINNER_PREFIX);
+        for (int i = 0; i < winnerNames.length; i++) {
+            winnersOutput.append(winnerNames[i]);
+            if (i != winnerNames.length - 1) {
+                winnersOutput.append(WINNER_DELIMITER);
+            }
+        }
+        printMessage(winnersOutput.toString());
+
+////        System.out.print(WINNER_PREFIX);
+//        printMessage(WINNER_PREFIX);
+//        for (int i = 0; i < winnerNames.length; i++) {
+////            System.out.print(winnerNames[i]);
+//            printMessage(winnerNames[i]);
+//            if (i != winnerNames.length - 1) {
+////                System.out.print(WINNER_DELIMITER);
+//                printMessage(WINNER_DELIMITER);
+//            }
+//        }
     }
 }
