90af6f1d6e9372b0928ad3225588b9ae2abbc125
daeyeon <kdy4410121@naver.com>
2024-10-26 13:22:59 +0900

    feat: int, String, String to List 입력 함수 생성 및 잘못된 입력 시 예외처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,10 +9,39 @@ public class InputView {
     public List<String> inputName(){
         String input = Console.readLine();
         String[] inputs = input.split(",");
+
+        for(int i = 0; i < inputs.length; i++){
+            wrongInput(inputs[i]);
+        }
+
         List<String> inputList;
         inputList = List.of(inputs);
 
         return inputList;
+    }
+
+    public String stringInput(){
+        String input = Console.readLine();
+
+        return input;
+    }
+
+    public int integerInput(){
+        String input = Console.readLine();
+        int intInput;
+        try {
+            intInput = Integer.parseInt(input);
+        }
+        catch(IllegalArgumentException e) {
+            throw new IllegalArgumentException();
+        }
+
+        return intInput;
+    }
+
+    private void wrongInput(String input){
+        if(input.length() >= 6)
+            throw new IllegalArgumentException();
     }
 
 }
