e653f8834f2abcb5370151eae4ba2daffe27921f
seoyeon2001 <syeon1250@naver.com>
2024-10-26 16:21:06 +0900

    Fix: string 배열에서 제네릭으로 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,10 +3,13 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.validator.CarNamesValidator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
     private static final String DELIMITER = ",";
 
-    public static String[] getCarNames() {
+    public static List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
 
@@ -14,14 +17,21 @@ public class InputView {
             throw new IllegalArgumentException("자동차 이름을 입력하지 않았습니다. ,를 이용하여 두 개 이상의 자동차 이름을 입력해주세요.");
         }
 
-        return CarNamesValidator.validate(input.split(DELIMITER));
+        // 배열을 리스트로 변환하여 CarNamesValidator에 전달
+        return CarNamesValidator.validate(Arrays.asList(input.split(DELIMITER)));
     }
 
     public static int getTryTime() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        // 아무것도 입력하지 않았을 때도 처리해줘야 함.
+        String input = Console.readLine();
+
+        // 입력이 없을 때
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("시도 횟수를 입력하지 않았습니다.");
+        }
+
         try {
-            int result = Integer.parseInt(Console.readLine());
+            int result = Integer.parseInt(input);
 
             if (result < 1) {
                 throw new IllegalArgumentException("시도 횟수는 1회 이상이어야 합니다.");
@@ -31,7 +41,5 @@ public class InputView {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자로 입력해야 합니다.");
         }
-
     }
-
 }
