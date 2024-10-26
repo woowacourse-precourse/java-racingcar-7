179b5551dc5573c972b68d193b3a60b612f4cf40
jonghwanlee <kavae550@gmail.com>
2024-10-27 01:45:09 +0900

    feat(controller): 메인 로직 구현
    - 레이싱을 시작하는 run()메서드 구현
    - 정해진 횟수만큼 라운드를 진행하는 proceedRacing() 구현

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -1,13 +1,33 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import racingcar.domain.Cars;
+import racingcar.domain.Parser;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Game {
 
     public void run(){
-        String carNames = InputView.requestCarNames();
-        String rawNumberOfTrials = InputView.requestNumberOfTrials();
+        String rawCarNames = InputView.requestCarNames();
+        ArrayList<String> carNames = Parser.parse(rawCarNames);
+        Cars cars = new Cars(carNames);
 
+        String rawNumberOfTrials = InputView.requestNumberOfTrials();
+        int numberOfTrials = Integer.parseInt(rawNumberOfTrials);
+
+        proceedRacing(cars, numberOfTrials);
+
+    }
+
+    public void proceedRacing(Cars cars , int numberOfTrials ) {
+        OutputView.printProcessResult();
+
+        for (int i = 0; i < numberOfTrials; i++) {
+            cars.proceedOneRound();
+            ArrayList<String> currentStates = cars.getCurrentStates();
+            OutputView.printRoundResult(currentStates);
+        }
     }
 
 }
