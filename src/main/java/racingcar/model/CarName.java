b21a8352cf656556d9083b90f7580751f8e862f1
yush <yush2202@gmail.com>
2024-10-26 19:58:47 +0900

    refactor($ CarName): 자동차 이름 클래스 이름 리팩터링
    모호한 이름인 CarRacer에서 CarName으로 변경

--- src/main/java/racingcar/model/CarName.java
+++ src/main/java/racingcar/model/CarName.java
@@ -3,16 +3,35 @@ package racingcar.model;
 import static racingcar.constants.ErrorMessages.INVALID_RACER_NAME_BLANK;
 import static racingcar.constants.ErrorMessages.INVALID_RACER_NAME_RANGE;
 
-public class CarRacer {
+import java.util.Objects;
+
+public class CarName {
     private final String name;
 
-    public CarRacer(String name) {
+    public CarName(String name) {
         validateName(name);
         this.name = name;
     }
 
     public String getRacerName() {
         return this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CarName carName = (CarName) o;
+        return Objects.equals(name, carName.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 
     private void validateName(String name) {
