45280a04462394a6056f4406be393200efd206a3
momoysm <90163578+momoysm@users.noreply.github.com>
2024-10-26 17:18:29 +0900

    feat : 시도 횟수 검증 구현

--- src/main/java/racingcar/parser/InputParser.java
+++ src/main/java/racingcar/parser/InputParser.java
@@ -4,24 +4,24 @@ import java.util.Arrays;
 import java.util.List;
 import racingcar.dto.CarNameRequest;
 import racingcar.dto.TryNumberRequest;
-import racingcar.validation.CarNameValidator;
+import racingcar.validation.InputValidator;
 
 public class InputParser {
 
     public static CarNameRequest parseCarName(String carNames) {
-        CarNameValidator.isNull(carNames);
+        InputValidator.isCarNameNull(carNames);
 
         List<String> carNameList = Arrays.stream(carNames.split(","))
                 .toList();
 
-        CarNameValidator.isOverSize(carNameList);
+        InputValidator.isCarNameOverSize(carNameList);
 
         return CarNameRequest.of(carNameList);
     }
 
     public static TryNumberRequest parseTryNumber(String tryNumber) {
-        // TODO tryNumber 유효성 검증
-        return TryNumberRequest.of(Integer.parseInt(tryNumber));
+
+        return TryNumberRequest.of(InputValidator.isTryNumberNotInt(tryNumber));
     }
 
 }
---
+++ src/main/java/racingcar/validation/InputValidator.java
@@ -0,0 +1,32 @@
+package racingcar.validation;
+
+import java.util.List;
+import racingcar.exception.CarNameException;
+import racingcar.exception.TryNumberException;
+import racingcar.message.ErrorMessage;
+
+public class InputValidator {
+
+    public static void isCarNameNull(String carNames) throws CarNameException {
+        if(carNames.isBlank()) {
+            throw new CarNameException(ErrorMessage.CAR_NAME_NULL_ERROR.getMessage());
+        }
+    }
+
+    public static void isCarNameOverSize(List<String> carNameList) throws CarNameException {
+        carNameList.forEach(carName -> {
+            if(carName.length() > 5) {
+                throw new CarNameException(ErrorMessage.CAR_NAME_OVERSIZE_ERROR.getMessage());
+            }
+        });
+    }
+
+    public static int isTryNumberNotInt(String tryNumber) {
+        try{
+            return Integer.parseInt(tryNumber);
+        } catch (NumberFormatException e) {
+            throw new TryNumberException(ErrorMessage.TRY_NUMBER_IS_NOT_INT.getMessage());
+        }
+    }
+
+}
