3c21d076ed95bf4885b3675104fec6885023236e
hwangdaesun <hds650588@gmail.com>
2024-10-26 22:47:38 +0900

    refactor : Cars 객체 만드는 메서드 추출

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -7,16 +7,20 @@ import org.junit.jupiter.api.Test;
 class CarsTest {
 
     @Test
-    void 쉼표를_기준으로_입력을_잘라_이름_개수만큼_자동차를_만든다(){
-        String input = "pobi,woni,jun";
-        Cars cars = Cars.getInstance(input);
+    void 쉼표를_기준으로_입력을_잘라_이름_개수만큼_자동차를_만든다() {
+        Cars cars = getCars();
         assertEquals(3, cars.getCars().size());
     }
 
     @Test
-    void 쉼표를_기준으로_자른_이름이_5글자_보다_클_경우(){
+    void 쉼표를_기준으로_자른_이름이_5글자_보다_클_경우() {
         String input = "daesun,woni,jun";
-        assertThrows(IllegalArgumentException.class, () -> Cars.getInstance(input));
+        assertThrows(IllegalArgumentException.class, () -> Car.getInstance(input));
+    }
+
+    private Cars getCars() {
+        String input = "pobi,woni,jun";
+        return Cars.getInstance(input);
     }
 
 }
