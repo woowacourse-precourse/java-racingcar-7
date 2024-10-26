680b64940736946e147e91123894490b26556807
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 00:14:06 +0900

    feat(RaceProcessController): 팩토리로부터 주입받은 의존성 처리
    - ControllerFactory로부터 의존성을 받아옴
    - 생성자 주입 방식으로 RaceViewController, RaceProcessService 의존성 처리

--- src/main/java/racingcar/Controller/RaceProcessController.java
+++ src/main/java/racingcar/Controller/RaceProcessController.java
@@ -1,4 +1,16 @@
 package racingcar.Controller;
 
+import racingcar.Service.RaceProcessService;
+
 public class RaceProcessController {
+    private final RaceViewController raceViewController;
+    private final RaceProcessService raceProcessService;
+
+    public RaceProcessController(RaceViewController raceViewController, RaceProcessService raceProcessService) {
+        this.raceViewController = raceViewController;
+        this.raceProcessService = raceProcessService;
+    }
+    public void run() {
+
+    }
 }
