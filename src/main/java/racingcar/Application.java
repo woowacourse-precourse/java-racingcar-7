7349397bf8b6d67d17788c0be6d9bf11540a1e7b
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-27 02:22:24 +0900

    refactor(Application): main 메서드 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.config.AppConfig;
+import racingcar.controller.RacingCarController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final AppConfig appConfig = new AppConfig();
+        final RacingCarController racingCarController = appConfig.racingCarController();
+        racingCarController.start();
     }
 }
