a7dc2874364b52a2810eadc3ce1986ca3c830b1a
lyouxsun <leedrkr323@konkuk.ac.kr>
2024-10-26 14:19:10 +0900

    feat(NumberRequestHandler): 입력 문자열을 정수로 변환
    - 숫자가 아닌 문자열이거나 빈 문자열인 경우 parseInt에서 예외가 발생한다.

--- src/main/java/racingcar/handler/NumberRequestHandler.java
+++ src/main/java/racingcar/handler/NumberRequestHandler.java
@@ -1,8 +1,24 @@
 package racingcar.handler;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static racingcar.message.ErrorMessage.INVALID_NUMBER_ERROR;
+
 public class NumberRequestHandler {
     public int getNumber() {
         System.out.println("시도할 횟수는 몇 회인가요?");
+        String inputString = Console.readLine();
+        int rawNumber = convertToInteger(inputString);
         return 0;
+    }
+
+    public int convertToInteger(String rawNumber) {
+        try {
+            return Integer.parseInt(rawNumber);
+        } catch (NumberFormatException e) {
+            String errorMessage = INVALID_NUMBER_ERROR.getMessage();
+            System.out.println(errorMessage);
+            throw new IllegalArgumentException(errorMessage);
+        }
     }
 }
