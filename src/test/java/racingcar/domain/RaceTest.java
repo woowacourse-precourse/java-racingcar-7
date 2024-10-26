7c5bad6a77d91f55f1cd4619cbbf24bb2fa82459
@_.mung <wjdahrrla@gmail.com>
2024-10-27 00:48:57 +0900

    Test(RaceTest.java): 우승자 결정 기능에 대한 테스트 코드를 작성해라
    우승 결정 기능을 구현하기 전에 테스트 코드를 먼저 작성함.
    
    작성한 사항:
    - 자동차가 무작위로 전진하거나 멈추는지 테스트
    - 공동 우승자가 있는 경우 테스트

--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import racingcar.strategy.MovementStrategy;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -52,5 +53,50 @@ public class RaceTest {
             int position = race.getCarPosition(carName);
             assertTrue(position >= 0 && position <= attemptCount);
         }
+    }
+
+    @Test
+    @DisplayName("최종 우승자가 한 명인 경우 테스트")
+    void 단독_우승자_결정() {
+        // given
+        Set<String> carNames = new LinkedHashSet<>();
+        carNames.add("pobi");
+        carNames.add("woni");
+        carNames.add("jun");
+        int attemptCount = 5;
+
+        MovementStrategy alwaysMoveStrategy = () -> true;
+        Race race = new Race(carNames, attemptCount, alwaysMoveStrategy);
+
+        // when
+        race.start();
+        Car winner = race.getWinners().get(0);
+
+        // then
+        assertEquals("pobi", winner.getName());
+    }
+
+    @Test
+    @DisplayName("공동 우승자가 있는 경우 테스트")
+    void 공동_우승자_결정() {
+        // given
+        Set<String> carNames = new LinkedHashSet<>();
+        carNames.add("pobi");
+        carNames.add("woni");
+        carNames.add("jun");
+        int attemptCount = 5;
+
+        MovementStrategy alwaysMoveStrategy = () -> true;
+        Race race = new Race(carNames, attemptCount, alwaysMoveStrategy);
+
+        // when
+        race.start();
+        Set<Car> winners = new LinkedHashSet<>(race.getWinners());
+
+        // then
+        assertEquals(3, winners.size());
+        assertTrue(winners.stream().anyMatch(car -> car.getName().equals("pobi")));
+        assertTrue(winners.stream().anyMatch(car -> car.getName().equals("woni")));
+        assertTrue(winners.stream().anyMatch(car -> car.getName().equals("jun")));
     }
 }
