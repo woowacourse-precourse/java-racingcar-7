aadfe2b5b1c036c1a3072a07cdcea2dcfecce3bc
softmoca <acyc0909@gmail.com>
2024-10-26 12:22:57 +0900

    feat : 초기 구조 구현

---
+++ src/main/java/racingcar/controller/Controller.java
@@ -0,0 +1,17 @@
+package racingcar.controller;
+
+public class Controller {
+    public void launchRace() {
+
+        initRaceCar();
+
+
+    }
+
+
+    private void initRaceCar() {
+
+    }
+
+
+}
