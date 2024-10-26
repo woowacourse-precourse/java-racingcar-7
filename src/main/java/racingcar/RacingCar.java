581a53643941f798b4c4d0eaca5b881f186d497d
lsj0822 <lsj0822@khu.ac.kr>
2024-10-26 14:27:23 +0900

    feat(RacingCar) : RacingCar 작성
    - 차의 이름과 이동한 거리를 저장하는 클래스
    - 이후에 결과를 출력하고 거리를 갱신하는 메서드를 추가할 예정.
    - 결과를 출력하는 메서드는 OutputManager 클래스에 parameter로 이름과 거리를 전달할 예정

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class RacingCar {
+    private String carName;
+    private int distance = 0;
+
+    public RacingCar(String carName) {
+        this.carName = carName;
+    }
+}
