5e4b54cc8c2cc75b8c2b22505d9c4fa308296216
soohalee <suha630@naver.com>
2024-10-26 17:46:49 +0900

    test(Validator): 자동차 개수가 1~100개인지 확인하는 테스트

---
+++ src/test/java/racingcar/validation/ValidatorTest.java
@@ -0,0 +1,27 @@
+package racingcar.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.view.InputHandler;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ValidatorTest {
+
+    @Test
+    @DisplayName("자동차 개수가 1~100개인지 확인하는 테스트")
+    void checkCarCountTest() {
+        List<String> tooManyCars = new ArrayList<>();
+        List<String> emptyCars = new ArrayList<>();
+
+        for (int i = 0; i < 101; i++) {
+            tooManyCars.add("car" + i);
+        }
+        assertThrows(IllegalArgumentException.class, () -> Validator.checkCarCount(tooManyCars));
+        assertThrows(IllegalArgumentException.class, () -> Validator.checkCarCount(emptyCars));
+    }
+
+
+}
