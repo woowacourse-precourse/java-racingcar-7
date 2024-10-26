ff7fd980c9349d7ab25787a035b584c8a64f4751
woogym <kwj9294@gmail.com>
2024-10-27 07:11:08 +0900

    feat(main): Add main function

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.controller.RaceController;
+import racingcar.model.Cars;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Cars cars = new Cars();
+
+        RaceController raceController = new RaceController(inputView, outputView, cars);
+        raceController.run();
     }
 }
