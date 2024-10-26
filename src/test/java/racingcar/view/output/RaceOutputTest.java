4ce3f4649acad8e6ce5b44ee1f3dc0f993c426c4
@_.mung <wjdahrrla@gmail.com>
2024-10-27 00:30:50 +0900

    Test(RaceOutputTest.java): 경주 진행 사항 출력 기능에 대한 테스트 코드를 작성해라
    경주 진행 사항 출력 기능 구현 전에 테스트 코드를 먼저 작성함.
    
    작성한 사항:
    - 경주 진행 사항이 정상적으로 출력되는지 테스트

---
+++ src/test/java/racingcar/view/output/RaceOutputTest.java
@@ -0,0 +1,36 @@
+package racingcar.view.output;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Race;
+import racingcar.strategy.MovementStrategy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RaceOutputTest {
+    @Test
+    @DisplayName("경주 진행 사항이 정상적으로 출력되는지 테스트")
+    void 경주_진행_사항_출력() {
+        // given
+        Set<String> carNames = new HashSet<>();
+        carNames.add("pobi");
+        carNames.add("woni");
+        carNames.add("jun");
+        int attemptCount = 3;
+
+        MovementStrategy alwaysMoveStrategy = () -> true;
+
+        Race race = new Race(carNames, attemptCount, alwaysMoveStrategy);
+
+        // when
+        race.start();
+        String output = RaceOutput.getProgress(race);
+
+        // then
+        String expectedOutput = "pobi : ---\nwoni : ---\njun : ---\n";
+        assertEquals(expectedOutput, output);
+    }
+}
