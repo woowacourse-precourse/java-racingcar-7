863d4ad282fde3b2ba7253e4efba64c7ebc2eac4
dongri <l0o0lv@github.com>
2024-10-26 23:15:57 +0900

    feat(OutputView): 출력 관련 메서드를 포함하는 클래스 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+import static racingcar.util.Constants.*;
+
+public class OutputView {
+    public void printInputCarName(){
+        System.out.println(INPUT_CAR_NAME);
+    }
+    public void printInputMoveCount(){
+        System.out.println(INPUT_MOVE_COUNT);
+    }
+    public void printResult(){
+        System.out.println(OUTPUT_RESULT);
+    }
+    public void printWinner(){
+        System.out.println(OUTPUT_WINNER);
+    }
+}
