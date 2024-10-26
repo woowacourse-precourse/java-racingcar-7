d783eddf938948e3c7f7fc7a87012007935c66cb
cyunlee <siyunkr@gmail.com>
2024-10-27 03:26:50 +0900

    feat(view): 사용자로부터 입력받는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,13 @@
 package racingcar.view;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.constant.Constant;
 
+public class InputView {
+    public void inputGameInfo() {
+        System.out.println(Constant.INPUT_PLAYERS_PROMPT);
+        String players = Console.readLine();
+        System.out.println(Constant.INPUT_TRIAL_PROMPT);
+        int trial = Integer.parseInt(Console.readLine());
+    }
 }
