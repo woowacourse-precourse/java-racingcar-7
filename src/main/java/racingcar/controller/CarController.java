36739ef706781452d77e7b6fb189901e33691c2e
fbehddn <tom0611@naver.com>
2024-10-26 16:44:29 +0900

    feat: 게임초기화 및 시작 메서드 구현

---
+++ src/main/java/racingcar/controller/CarController.java
@@ -0,0 +1,36 @@
+package racingcar.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.CarGameManager;
+import racingcar.service.CarService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class CarController {
+    public InputView inputView;
+    public OutputView outputView;
+    public String[] names;
+    public int totalTimes;
+    public CarService carService;
+
+    public CarController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.carService = new CarService(new CarGameManager(new ArrayList<>()));
+    }
+
+    public void init() {
+        try{
+            names = inputView.inputNames();
+        }catch (Exception e) {
+            new IllegalArgumentException();
+        }
+        totalTimes = inputView.inputTimes();
+    }
+
+    public void start() {
+        carService.start(names, totalTimes);
+    }
+
+}
