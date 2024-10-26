917e020a69835a4cf0a527bc30a7d01e75edf630
Ahnsukjoo <bos98@naver.com>
2024-10-26 14:56:43 +0900

    feat: 자동차 이름 입력 기능 구현
    - 쉼표로 구분된 자동차 이름을 입력받아 처리하는 기능
    - 입력받은 자동차 이름들을 해시의 키로 넣고 해시맵을 반환

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -1,11 +1,17 @@
 package racingcar.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Input {
 
     public HashMap<String,Integer> getCarNames() {
-
+        String[] input = readLine().split(",");
+        HashMap<String,Integer> carNames = new HashMap<>();
+        Arrays.stream(input).forEach(s->carNames.put(s, 0));
+        return carNames;
     }
 
     public int getAttemptedNumber() {
