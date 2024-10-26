a1bf0f0a607ed9e71b1880ecf9d77daacc775817
rhxfkg <rhxfkg@gmail.com>
2024-10-27 00:15:09 +0900

    Update Racing.java

---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Racing {
+    public int[] racing(String[] carList, int number) {
+        int[] racingResult = new int[carList.length];
+        for (int i = 0; i < carList.length; i++) {
+            racingResult[i] = countMovements(number);
+            printRacingResult(carList[i], racingResult[i]);
+        }
+        System.out.println();
+        return racingResult;
+    }
+
+    public int countMovements(int number) {
+        int count = 0;
+        for (int i = 0; i < number; i++) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void printRacingResult(String carName, int racingResult) {
+        System.out.print(carName + " : ");
+        System.out.println("-".repeat(racingResult));
+    }
+}
