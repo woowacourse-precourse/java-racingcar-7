b859dfdf1ec66819a0723e8059e2925552f35702
jinhyeon <tiemo0708@naver.com>
2024-10-26 18:10:20 +0900

    feat: 입력 기능 구현 자동차 이름들 입력, 실행 횟수 입력

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,13 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public String getCarNames() {
+        return Console.readLine();
+    }
+
+    public String getNumberOfTrials() {
+        return Console.readLine();
+    }
 }
