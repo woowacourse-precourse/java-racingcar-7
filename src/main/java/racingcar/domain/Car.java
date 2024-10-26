a556fa765d727b748377882dfbf3fb9cd19daff9
lucku <lsw6731@naver.com>
2024-10-26 21:10:55 +0900

    feat: Car 클래스 생성 및 이동 기능 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        position = 0;
+    }
+
+    public void move(int randomNumber) {
+        if (randomNumber >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
