af7bb354a7fe6edf5772b5f25264a26ef4413702
KangrimChoi <rkdfla100@naver.com>
2024-10-26 15:25:00 +0900

    feat: 3. 경기 진행 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Assertions;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Application {
@@ -14,7 +15,7 @@ public class Application {
 
     public static void run(){
         try{
-            List<String> winners = startRacing(inputRacers());
+            String winners = startRacing(inputRacers());
             printWinners(winners);
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException();
@@ -25,7 +26,7 @@ public class Application {
 
 
     public static List<String> inputRacers(){
-        String racerNames = Console.readLine();
+        String racerNames = "ab,cd,ef"; //Console.readLine();
         List<String> racers = List.of(racerNames.split(","));
         if(isNameLengthOver5(racers)){
             throw new IllegalArgumentException();
@@ -48,24 +49,37 @@ public class Application {
     *
     **/
 
-    public static List<String> startRacing(List<String> racers) {
-        List<String> winners = null;
-        int finishLine = Integer.parseInt(Console.readLine());
+    public static String startRacing(List<String> racers) {
+        int finishLine = 5; //Integer.parseInt(Console.readLine());
         int frontRacerLocation = 0;
 
+        List<String> racerInfo = raceReady(racers);
+        List<Integer> racerLocations = initLocatoin(racers.size());
+
         while(frontRacerLocation < finishLine){
-            for(String racer : racers) {
+            for(int i=0; i<racers.size(); i++) {
                 if( isSuccesMoving() ){
-                    //출루
+                    driveCar(i, racerInfo, racerLocations);
+                    int curLocation = racerLocations.get(i);
+                    if(curLocation > frontRacerLocation) { frontRacerLocation++; }
                 }
             }
         }
 
+        String winners = announceWinner(racerInfo, finishLine);
         return winners;
     }
 
-    public static void printWinners(List<String> winners){
-        System.out.println("승자는 우리 모두야!");
+    public static List<String> raceReady(List<String> racers) {
+        List<String> preparedRacer = new ArrayList<>();
+        for(String racer : racers) {
+            preparedRacer.add(racer + " : ");
+        }
+        return preparedRacer;
+    }
+
+    public static void printWinners(String winners){
+        System.out.println(winners);
     }
 
     public static boolean isSuccesMoving() {
@@ -77,8 +91,45 @@ public class Application {
         return false;
     }
 
+    public static void driveCar(int index, List<String> racerInfo, List<Integer> racerLocations) {
+        racerInfo.set(index, racerInfo.get(index) + "-");
+        racerLocations.set(index, racerLocations.get(index) + 1);
+    }
 
+    public static String announceWinner(List<String> racerInfo, int finishLine) {
+        String winners = "최종 우승자 :";
+        for(String info : racerInfo) {
+            boolean racerResult = isWinner(info, finishLine);
+            if(racerResult){
+                winners += " " + racerName(info);
+            }
+        }
+        return winners;
+    }
 
+    public static boolean isWinner(String racerInfo, int finishLine) {
+        String driveDistance = subName(racerInfo);
+        if(driveDistance.length() == finishLine){
+            return true;
+        }
+        return false;
+    }
+
+    public static String racerName(String racerInfo) {
+        return racerInfo.substring(0, racerInfo.indexOf(" :"));
+    }
+
+    public static String subName(String racerInfo) {
+        return racerInfo.substring(racerInfo.indexOf(":") + 2);
+    }
+
+    public static List<Integer> initLocatoin(int size) {
+        List<Integer> racerLocations = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            racerLocations.add(0);
+        }
+        return racerLocations;
+    }
 }
 
 
