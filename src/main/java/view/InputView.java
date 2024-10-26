42deef8db5f9dcffa39a4a46180761854a7e545c
choi <nippert@naver.com>
2024-10-26 13:02:18 +0900

    refactor: 입력에러메시지 상수화하여 ENUM으로 분리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,25 +2,28 @@ package view;
 
 import camp.nextstep.edu.missionutils.Console;
 import view.domain.InputResult;
+import view.message.ErrorMessage;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+
+import static view.message.ErrorMessage.SHAPE_MESSAGE;
 
 public class InputView {
 
     private static final String CARS_REGEX = "^(\\s*[a-zA-Z]+\\s*)(,\\s*[a-zA-Z]+\\s*)*$";
     private static final String COUNT_REGEX = "^[0-9]+$";
 
-    private static final String EMPTY_MESSAGE = "입력 값이 존재하지 않습니다.";
-    private static final String SHAPE_MESSAGE = "입력 형태가 올바르지 않습니다.";
+    private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_COUNT = "시도할 횟수는 몇 회인가요?";
 
     private void validateCars(String cars) throws IllegalArgumentException {
         if (cars == null || cars.isEmpty()) {
-            throw new IllegalArgumentException(EMPTY_MESSAGE);
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_MESSAGE.getMessage());
         }
         if (!cars.matches(CARS_REGEX)) {
-            throw new IllegalArgumentException(SHAPE_MESSAGE);
+            throw new IllegalArgumentException(ErrorMessage.SHAPE_MESSAGE.getMessage());
         }
 
         String[] carName = cars.split(",");
@@ -29,7 +32,7 @@ public class InputView {
                 .filter(car -> car.isEmpty() || car.length() > 5)
                 .findAny()
                 .ifPresent(car -> {
-                    throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+                    throw new IllegalArgumentException(ErrorMessage.CAR_NAME_LENGTH_MESSAGE.getMessage());
                 });
 
         Set<String> duplicateCarName = new HashSet<>();
@@ -37,26 +40,26 @@ public class InputView {
                 .filter(car -> !duplicateCarName.add(car))
                 .findAny()
                 .ifPresent(car -> {
-                    throw new IllegalArgumentException("중복된 자동차 이름이 있습니다.");
+                    throw new IllegalArgumentException(ErrorMessage.EXIST_CAR_NAME_MESSAGE.getMessage());
                 });
     }
 
     private int validateCount(String count) throws IllegalArgumentException {
         if (count == null || count.isEmpty()) {
-            throw new IllegalArgumentException(EMPTY_MESSAGE);
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_MESSAGE.getMessage());
         }
         if (!count.matches(COUNT_REGEX)) {
-            throw new IllegalArgumentException(SHAPE_MESSAGE);
+            throw new IllegalArgumentException(ErrorMessage.SHAPE_MESSAGE.getMessage());
         }
 
         return Integer.parseInt(count);
     }
 
     public final InputResult input() throws IllegalArgumentException {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(INPUT_CAR_NAME);
         String cars = Console.readLine();
         validateCars(cars);
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(INPUT_COUNT);
         String count = Console.readLine();
         int gameCount = validateCount(count);
         System.out.println();
