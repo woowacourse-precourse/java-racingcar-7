84e80f347d5ec00ceb9aab3e178dd5a0199138be
swhyeon98 <smreo75@gmail.com>
2024-10-26 23:53:44 +0900

    feat: 자동차 경주 게임 입력 기능 추가
    - 사용자에게 경주할 자동차 이름과 시도 횟수를 입력받는 기능을 추가
    - 자동차 이름은 쉼표(,)로 구분하여 입력하도록 처리
    - 입력받은 시도 횟수를 문자열로 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String rawCarNames = Console.readLine();
+        List<String> racingCarNames = List.of(rawCarNames.split(","));
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String tryCount = Console.readLine();
     }
 }
