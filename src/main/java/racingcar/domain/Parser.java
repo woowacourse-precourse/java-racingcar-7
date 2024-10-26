57630fc665a558c1ead5e2e4dcee28040d246122
jonghwanlee <kavae550@gmail.com>
2024-10-26 21:14:28 +0900

    feat(domain): 입력값 parsing 기능 구현
    자동차들의 이름이 담긴 문자열을 변환하여  자동차 이름 리스트로 리턴해주는 parse() 메서드 구현

--- src/main/java/racingcar/domain/Parser.java
+++ src/main/java/racingcar/domain/Parser.java
@@ -1,4 +1,12 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class Parser {
+    public static List<String> parse(String rawCarNames) {
+
+        List<String> carNames = List.of(rawCarNames.split(","));
+
+        return carNames;
+    }
 }
