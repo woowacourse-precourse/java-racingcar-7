8d310552f7e060ab6e5fb69cfc1ea0307baa5f68
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-26 18:10:39 +0900

    feat(view): InputView 입력 클래스 생성 - 자동차 이름과 시도 횟수를 입력받기 위한 메시지 출력 및 입력 기능 구현 - CAR_NAME_INPUT_MESSAGE와 TRY_COUNT_INPUT_MESSAGE 상수를 통해 메시지 관리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,2 +1,18 @@
-package racingcar.view;public class InputView {
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRY_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
+    public String getCarNames() {
+        System.out.println(CAR_NAME_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String getCount() {
+        System.out.println(TRY_COUNT_INPUT_MESSAGE);
+        return Console.readLine();
+    }
 }
