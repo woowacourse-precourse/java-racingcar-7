3fa2d8c3436a7ce6ad8f991408ec6a18e9ebe7fc
Dohun Kim <dohun8758@naver.com>
2024-10-27 02:03:38 +0900

    feat : 입력값 검증 기능

---
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -0,0 +1,45 @@
+package racingcar.validator;
+
+import java.util.Arrays;
+
+public class InputValidator {
+
+    private static final String NOT_ALLOWED_CHARACTER_REGEX = ".*[^a-zA-Z0-9가-힣].*";
+    private static final int MAX_CAR_NAME_RANGE = 5;
+    public static final int MIN_TRIAL_COUNT = 1;
+    public static final String NUMBER_REGEX = "\\d+";
+
+    public void validateCarName(String[] names) {
+        validateCarNameLength(names);
+        validateCarNameContainsNotAllowedCharacter(names);
+    }
+
+    public void validateTrialCount(String trialCount) {
+        validateTrialCountInputType(trialCount);
+        validateTrialCountRange(trialCount);
+    }
+
+    private void validateCarNameLength(String[] names) {
+        if (Arrays.stream(names).anyMatch(s -> s.length() > MAX_CAR_NAME_RANGE)) {
+            throw new IllegalArgumentException("이름의 길이는 5자 이하여야 합니다.");
+        }
+    }
+
+    private void validateCarNameContainsNotAllowedCharacter(String[] names) {
+        if (Arrays.stream(names).anyMatch(s -> s.matches(NOT_ALLOWED_CHARACTER_REGEX))) {
+            throw new IllegalArgumentException("이름에는 알파벳, 한글, 숫자만 포함 가능합니다.");
+        }
+    }
+
+    private void validateTrialCountInputType(String trialCount) {
+        if (!trialCount.matches(NUMBER_REGEX)) {
+            throw new IllegalArgumentException("시행 횟수는 숫자로만 이루어져야 합니다");
+        }
+    }
+
+    private void validateTrialCountRange(String trialCount) {
+        if (Integer.parseInt(trialCount) < MIN_TRIAL_COUNT) {
+            throw new IllegalArgumentException("시행 횟수는 0보다 큰 정수여야 합니다.");
+        }
+    }
+}
