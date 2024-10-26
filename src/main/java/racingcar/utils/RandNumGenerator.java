590420f431ebb5835b9e1acac03bc45f034e0b8d
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-26 20:17:10 +0900

    feat: 난수 생성 모듈 구현
    RandNumGenerator에는 현재 아래와 같은 상수와 메서드가 구현되어 있다.
    - RAND_MIN: 생성될 수 있는 최소 난수
    - RAND_MAX: 생성될 수 있느 최대 난수
    - MIN_NUM_FOR_MOVE: 이동에 필요한 최소 수
    - isRandNumGreaterThanOrEqualToCriterion(): 난수를 생성하여 기준보다 크면 참을 반환, 그렇지 않으면 거짓을 반환

---
+++ src/main/java/racingcar/utils/RandNumGenerator.java
@@ -0,0 +1,15 @@
+package racingcar.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandNumGenerator {
+    private static final int RAND_MIN = 0;
+    private static final int RAND_MAX = 9;
+    private static final int MIN_NUM_FOR_MOVE = 4;
+
+    public boolean isRandNumGreaterThanOrEqualToCriterion() {
+        int randNum = Randoms.pickNumberInRange(RAND_MIN, RAND_MAX);
+
+        return randNum >= MIN_NUM_FOR_MOVE;
+    }
+}
