a6524cb629ac9484419033a5db9da4ae58639985
chae02yo <chae02kim@naver.com>
2024-10-27 02:15:50 +0900

    feat: generate random numbers of cars

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -3,16 +3,28 @@ package racingcar.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingService {
+    List<String> nameOfCar = new ArrayList<>();
+    List<Integer> randomNum = new ArrayList<>();
+
     public void separateCarNames (String namesOfCars) {
-        List<String> nameOfCar = new ArrayList<>();
         nameOfCar = List.of(namesOfCars.split(","));
     }
 
     public void race (int numberOfRaces) {
         while (numberOfRaces > 0) {
-
+            randomNum.clear();
+            setRandomNum();
             numberOfRaces--;
+        }
+    }
+
+    public void setRandomNum () {
+        for (int i=0; i<nameOfCar.size(); i++) {
+            int num = Randoms.pickNumberInRange(0,9);
+            randomNum.add(num);
         }
     }
 }
