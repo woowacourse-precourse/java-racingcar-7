01850c169fbe0253b5d26660c24e065a7b1ae13a
mhj1205 <lightnow1205@gmail.com>
2024-10-26 20:42:59 +0900

    feat: 무작위 값이 4이상일 경우 전진하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,17 +9,28 @@ public class Application {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             Scanner sc = new Scanner(System.in);
             String cars_str = sc.nextLine();
-       
+
             //시도할 횟수 입력 받기
             System.out.println("시도할 횟수는 몇 회인가요?");
             int cnt = sc.nextInt();
 
             //쉼표(,) 구분자 이용해서 배열에 넣기기
             String[] cars = cars_str.split(",");
-
+            int [] racing_num = new int[cars.length];
             //0~9 사이에 랜덤한 숫자 구하기
             Random random = new Random();   
-            int advance_number = random.nextInt(10);
+    
+            System.out.println("실행 결과");
+            // 경주 하기
+            for (int i=0; i<cnt; i++){
+                for (int j=0; j<cars.length; j++){
+                    int advance_number = random.nextInt(10);
+                    if (advance_number > 4){
+                        racing_num[j] += 1;
+                    }
+                }
+            }
     }
+
 
 }
