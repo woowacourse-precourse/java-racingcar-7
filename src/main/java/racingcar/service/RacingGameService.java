1fdb88af30d3523c0c285f2e64ce481af72c72a1
msk226 <kminseok5167@gmail.com>
2024-10-26 16:22:43 +0900

    fix(service): 메서드 책임 조정을 위해 메서드 로직 분리
    - 시도 횟수 검증 로직 메서드 분리
    - 자동차 이름 검증 로직 메서드 분리

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -1,6 +1,5 @@
 package racingcar.service;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -10,32 +9,24 @@ import racingcar.model.RacingCar;
 
 public class RacingGameService {
     private static final Integer NAME_LENGTH_LIMIT = 5;
+    private static final int MIN_CAR_COUNT = 2;
 
     public List<RacingCar> initializeRacingGame(String carNames){
-        if (!validateCarNames(carNames)){
-            throw new IllegalArgumentException("입력하신 자동차의 이름이 유효하지 않습니다.");
-        }
+        validateCarNames(carNames);
 
         String[] parseCarName = processCarName(carNames);
 
-        List<RacingCar> racingCars = new ArrayList<>();
-
-        for (String carName : parseCarName){
-            racingCars.add(new RacingCar(carName));
-        }
-
-        return racingCars;
+        return createRacingCars(parseCarName);
     }
 
     public Integer convertTryCountToInt(String tryCountInString){
 
-        if (tryCountInString.matches(".*[a-zA-Z]+.*")) {
-            throw new IllegalArgumentException("숫자 이외의 문자는 입력할 수 없습니다.");
-        }
+        validateNumericInput(tryCountInString);
+
         int tryCount = Integer.parseInt(tryCountInString);
-        if (!validateTryCount(tryCount)){
-            throw new IllegalArgumentException("입력하신 시도 횟수가 유효하지 않습니다.");
-        }
+
+        validateTryCount(tryCount);
+
         return tryCount;
     }
 
@@ -60,54 +51,88 @@ public class RacingGameService {
         return winners;
     }
 
+
+    private static List<RacingCar> createRacingCars(String[] parseCarName) {
+        List<RacingCar> racingCars = new ArrayList<>();
+
+        for (String carName : parseCarName){
+            racingCars.add(new RacingCar(carName));
+        }
+        return racingCars;
+    }
+
+
     private String[] processCarName(String carNames) {
+
         String[] parseCarName = carNames.split(",");
+
         // 이름에서 공백 제거
         for (int i = 0; i < parseCarName.length; i++) {
-            if (parseCarName[i].isBlank() || parseCarName[i].isEmpty()){
-                throw new IllegalArgumentException(i + "번 사용자의 자동차 이름이 입력 되지 않았습니다.");
-            }
-
+            checkNameIsBlankOrEmpty(parseCarName, i);
             parseCarName[i] = parseCarName[i].trim();
         }
         return parseCarName;
     }
 
+    /* -------------------------------------- 시도 횟수 검증 로직 ----------------------------------------------*/
+    private void validateNumericInput(String tryCountInString) {
+        if (tryCountInString.matches(".*[a-zA-Z]+.*")) {
+            throw new IllegalArgumentException("숫자 이외의 문자는 입력할 수 없습니다.");
+        }
+    }
 
-    private boolean validateTryCount(Integer tryCount){
+    private void validateTryCount(Integer tryCount){
         if (tryCount <= 0){
             throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
         }
-        return true;
     }
 
-    private boolean validateCarNames(String carNames){
-        if (carNames == null || carNames.isEmpty()){
-            throw new IllegalArgumentException("자동차 이름이 입력되지 않았습니다.");
-        }
+    /* -------------------------------------- 자동차 이름 검증 로직 ----------------------------------------------*/
+
+    private void validateCarNames(String carNames){
+        checkCarNameisEmpty(carNames);
 
         String[] splitCarNames = processCarName(carNames);
 
-        if (splitCarNames.length < 2) {
-            throw new IllegalArgumentException("자동차는 2대 이상 입력해야 합니다.");
-        }
+        checkCarCount(splitCarNames);
 
         for (String carName : splitCarNames){
-            if (carName.length() > NAME_LENGTH_LIMIT){
-                throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력 하셔야 합니다.");
-            }
+            checkNameLength(carName);
         }
 
         Set<String> carNameSet = new HashSet<>(Arrays.asList(splitCarNames));
 
+        checkDuplicateCarName(splitCarNames, carNameSet);
+    }
+
+    private void checkNameIsBlankOrEmpty(String[] parseCarName, int i) {
+        if (parseCarName[i].isBlank() || parseCarName[i].isEmpty()){
+            throw new IllegalArgumentException(i + "번 사용자의 자동차 이름이 입력 되지 않았습니다.");
+        }
+    }
+    private void checkDuplicateCarName(String[] splitCarNames, Set<String> carNameSet) {
         // 배열의 길이와 Set의 크기를 비교하여 중복된 이름이 있는지 확인
         if (splitCarNames.length != carNameSet.size()) {
             throw new IllegalArgumentException("자동차 이름에 중복된 이름이 있습니다.");
         }
-
-        return true;
     }
 
+    private void checkCarCount(String[] splitCarNames) {
+        if (splitCarNames.length < MIN_CAR_COUNT) {
+            throw new IllegalArgumentException("자동차는 2대 이상 입력해야 합니다.");
+        }
+    }
 
+    private void checkCarNameisEmpty(String carNames) {
+        if (carNames == null || carNames.isEmpty()){
+            throw new IllegalArgumentException("자동차 이름이 입력되지 않았습니다.");
+        }
+    }
+
+    private void checkNameLength(String carName) {
+        if (carName.length() > NAME_LENGTH_LIMIT){
+            throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력 하셔야 합니다.");
+        }
+    }
 
 }
