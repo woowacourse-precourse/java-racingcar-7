3277a2527d93d9c3ca05b3fd90d2ff94bcd8f5d3
kkmdevel <kkm.devel@gmail.com>
2024-10-26 21:23:13 +0900

    feat: StringToInteger 생성
    문자열을 숫자로 변환해주는 기능

---
+++ src/main/java/racingcar/utils/StringToInteger.java
@@ -0,0 +1,8 @@
+package racingcar.utils;
+
+public class StringToInteger {
+
+    public static Integer StringToIntegerConverter(String numberString){
+        return Integer.parseInt(numberString);
+    }
+}
