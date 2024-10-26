2f3dbfc60e63964c244dcf825768d6e5e8006043
fortune-man <wnguddl96@knou.ac.kr>
2024-10-26 12:59:49 +0900

    test(InputTest) : 입력 테스트 최소한 작성

---
+++ src/test/java/racingcar/InputTest.java
@@ -0,0 +1,29 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+
+class InputTest {
+  @DisplayName("유저가_자동차_이름을_입력한다")
+  @ParameterizedTest
+  public void 유저가_자동차_이름을_입력한다() throws Exception{
+      //given
+
+      //when
+
+      //then
+  }
+  @DisplayName("유저가_자동차_이름을_입력한다")
+  @ParameterizedTest
+  public void 유저가_게임_시도_횟수를_입력한다() throws Exception{
+    //given
+
+    //when
+
+    //then
+  }
+
+
+}
