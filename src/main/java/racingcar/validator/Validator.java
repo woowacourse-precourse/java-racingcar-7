e4c1ae595fda2ae02c17c5d95ba853e8272f1b2b
menzzi <minmin3927@naver.com>
2024-10-26 16:04:51 +0900

    refactor : Validator 공백 제거

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -1,7 +1,6 @@
 package racingcar.validator;
 
 public class Validator {
-
     public static void validateCarName(String name){
         if(name.length()>5){
             throw new IllegalArgumentException("이름은 5자 이하입니다.");
