4a911fd6c2d0d04c4517124f73cfec4de3f79393
MinJae00 <exceedmin@gmail.com>
2024-10-27 01:48:27 +0900

    refact: 정확한 역할 의미 표현을 위해 클래스 명은 명사 형으로, 메소드 이름은 동사형으로 변경

--- src/main/java/racingcar/user/UserObject.java
+++ src/main/java/racingcar/user/UserObject.java
@@ -1,11 +1,11 @@
 package racingcar.user;
 
-public class UserInfo {
+public class UserObject {
     public String name;
     public String score;
     public int scoreNum = 0;
 
-    public UserInfo(String name) {
+    public UserObject(String name) {
         this.name = name;
         this.score = "";
         this.scoreNum = 0;
