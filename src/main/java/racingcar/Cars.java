adbf362a8856dcc094dd28d22784e2a74f6ce755
= <hyeonhc328@gmail.com>
2024-10-26 20:35:36 +0900

    feat(Cars): 우승자 판별 및 반환 기능 추가
    1. 우승 점수 판별 기능 추가
    2. 우승 점수에 따른 우승자 이름 반환 기능 추가

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -16,4 +16,30 @@ public class Cars extends HashMap<String, Integer> {
         }
     }
 
+
+    public String getWinnerNames(int winnerScore) {
+        List<String> winnerNames = new ArrayList<>();
+        for (String name : this.keySet()) {
+            isWinner(winnerScore, name, winnerNames);
+        }
+        return String.join(",",winnerNames);
+    }
+
+    private void isWinner(int winnerScore, String name, List<String> winnerNames) {
+        if(this.get(name) == winnerScore) {
+            winnerNames.add(name);
+        }
+    }
+
+
+    public int findWinScore() {
+        int winnerScore = 0;
+        for (String name : this.keySet()) {
+            if (this.get(name) >= winnerScore) {
+                winnerScore = this.get(name);
+            }
+        }
+        return winnerScore;
+    }
+
 }
--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -1,25 +1,64 @@
 package racingcar;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarsTest {
 
+    Cars cars;
+
     @Test
     @DisplayName("자동차 생성")
     void 자동차_추가() {
         //given
-        Set<String> input = Set.of("pobi","ddot");
+        Set<String> input = Set.of("pobi", "ddot");
         //when
-        Cars cars = new Cars(input);
+        cars = new Cars(input);
         //then
         assertEquals(input, cars.keySet());
     }
 
-    
+    @Test
+    @DisplayName("가장 앞선 승자의 위치는?")
+    void 승자_점수_확인() {
+        //given
+        Set<String> input = Set.of("pobi", "ddot", "a");
+        cars = new Cars(input);
+        //when
+        cars.putAll(Map.of("pobi", 1, "ddot", 2, "a", 6));
+
+        //then
+        assertEquals(6, cars.findWinScore());
+    }
+
+
+    @Test
+    @DisplayName("승자가 한 명일 떄")
+    void 승자_이름() {
+        //given
+        Set<String> input = Set.of("pobi", "ddot", "a");
+        cars = new Cars(input);
+        //when
+        cars.putAll(Map.of("pobi", 1, "ddot", 2, "a", 6));
+        //then
+        assertEquals("a", cars.getWinnerNames(cars.findWinScore()));
+    }
+
+
+    @Test
+    @DisplayName("승자가 두명 이상 일 떄")
+    void 승자_여러명_이름() {
+        //given
+        Set<String> input = Set.of("pobi", "ddot", "a");
+        cars = new Cars(input);
+        //when
+        cars.putAll(Map.of("pobi", 7, "ddot", 7, "a", 7));
+        //then
+        assertEquals(String.join(",", cars.keySet()),
+                cars.getWinnerNames(cars.findWinScore()));
+    }
 }
