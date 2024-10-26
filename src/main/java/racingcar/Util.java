fb3d786954a83885daea4e2dd36f55b02b38b723
seophohoho <seop0937@gmail.com>
2024-10-26 13:31:27 +0900

    feat(Util): 사용자 입력 메소드 구현

--- src/main/java/racingcar/Util.java
+++ src/main/java/racingcar/Util.java
@@ -1,10 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
 
 public class Util {
     public int getRandom() {
         return Randoms.pickNumberInRange(0, 9);
+    }
+
+    public String getInputValue() {
+        return Console.readLine();
     }
 }
