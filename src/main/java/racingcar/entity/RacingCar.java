3e77ca0cca0c5e225b432a6e178c6c474cbc69df
SeoMoonk <sunghoon1216@naver.com>
2024-10-26 20:08:32 +0900

    feat: RacingCar 엔티티 구조 작성
    RacingCar는 이름과 이동거리를 가지고 있으며, 생성시 거리는 0으로 생성,
    move()로 1 이동이 가능

--- src/main/java/racingcar/entity/RacingCar.java
+++ src/main/java/racingcar/entity/RacingCar.java
@@ -1,7 +1,29 @@
 package racingcar.entity;
 
+import static racingcar.constant.RacingCarStatic.DEFAULT_MOVE_RANGE;
+
+import java.math.BigInteger;
+
 public class RacingCar {
 
     //TODO: 자동차 경주의 주체가 될 자동차들을 정의하는 엔티티 클래스
+    private String name;
+    private BigInteger distance;
 
+    public RacingCar(String name) {
+        this.name = name;
+        this.distance = BigInteger.ZERO;
+    }
+
+    public BigInteger getDistance() {
+        return distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void move() {
+        this.distance = distance.add(BigInteger.valueOf(DEFAULT_MOVE_RANGE));
+    }
 }
