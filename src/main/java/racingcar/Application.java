d2dfce6614642eb492a26903cd7c1de0122fe4e0
okod <okodee@naver.com>
2024-10-26 18:41:23 +0900

    refactor: getCars()에서 문자열 -> 자동차 리스트를 반환하는 parseCarList() 함수 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -30,13 +30,18 @@ public class Application {
 
     private static List<String> getCars() {
         String input = getString();
-        List<String> cars = new ArrayList<String>(Arrays.asList(input.split(",")));
+        List<String> cars = parseCarList(input);
         return cars;
     }
 
     private static String getString() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
+    }
+
+    private static List<String> parseCarList(String input) {
+        List<String> cars = new ArrayList<String>(Arrays.asList(input.split(",")));
+        return cars;
     }
 
     private static int getCount() {
