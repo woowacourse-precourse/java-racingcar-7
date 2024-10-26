bae9f851c7233845af4c5d5c736f0f7065652814
YugyeomGhim <rladbrua0207@gmail.com>
2024-10-26 22:24:50 +0900

    refactor: CarGroup클래스에서 콤마(,)단위로 Car를 구분하도록 변경
    - Game Controller 에서는 콤마(,) 단위로 구분해야하는걸 몰라도 되기 떄문에 변경

--- src/main/java/racingcar/Controller/GameController.java
+++ src/main/java/racingcar/Controller/GameController.java
@@ -1,15 +1,12 @@
 package racingcar.Controller;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
-import java.util.List;
 import racingcar.model.CarGroup;
 
 public class GameController {
     public void run() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNamesInput = Console.readLine();
-        List<String> carNames = Arrays.stream(carNamesInput.split(",")).toList();
+        String carNames = Console.readLine();
         CarGroup cars = new CarGroup(carNames);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
--- src/main/java/racingcar/model/CarGroup.java
+++ src/main/java/racingcar/model/CarGroup.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import java.util.Arrays;
 import java.util.List;
 import racingcar.utils.RandomNumberGenerator;
 
@@ -7,20 +8,29 @@ public class CarGroup {
 
     private final List<Car> cars;
 
-    public CarGroup(List<String> carNames) {
-        this.cars = carNames.stream().map(Car::new).toList();
+    public CarGroup(String carNames) {
+        this.cars = getCars(carNames);
         validateSize();
     }
 
-    private void validateSize() {
-        if (this.cars.size() < 2) {
-            throw new IllegalArgumentException("자동차 이름은 적어도 두 개 입력해야 합니다.");
-        }
+    public List<Car> getCars() {
+        return cars;
     }
 
     public void move() {
         cars.forEach(car -> {
             car.moveFront(RandomNumberGenerator.generate());
         });
+    }
+
+    private List<Car> getCars(String carNames) {
+        return Arrays.stream(carNames.split(",")).toList()
+                .stream().map(Car::new).toList();
+    }
+
+    private void validateSize() {
+        if (this.cars.size() < 2) {
+            throw new IllegalArgumentException("자동차 이름은 적어도 두 개 입력해야 합니다.");
+        }
     }
 }
