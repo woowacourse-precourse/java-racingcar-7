0cea19da9a6a35ddfde711643b0dbee10d9f98e7
MinseoKangQ <kms02171@naver.com>
2024-10-26 16:56:40 +0900

    feat: 자동차 이름 검증 로직 작성

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -7,6 +7,17 @@ public class InputHandler {
     public static String[] getCarNames() {
         String input = Console.readLine();
         String[] carNames = input.split(",");
+
+        for(String carName: carNames) {
+            checkNameIsAvailable(carName);
+        }
+
         return carNames;
+    }
+
+    public static void checkNameIsAvailable(String carName) {
+        if (carName.length() >= 6) {
+            throw new IllegalArgumentException();
+        }
     }
 }
