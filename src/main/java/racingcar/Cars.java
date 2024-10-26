785f4e58d3ee1834a40e06734aae937ea8d07ef0
yybmion <yyb400@naver.com>
2024-10-26 20:35:52 +0900

    test: 쉼표로 구분된 이름(Input)을 통한 자동차 생성 테스트 코드 추가

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    private Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars createCarsFrom(String names) {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/test/java/racingcar/CarsTest.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("자동차들 테스트")
+public class CarsTest {
+
+    @Test
+    @DisplayName("쉼표로 구분된 이름으로 자동차들을 생성한다.")
+    void 생성_자동차들() {
+        // given
+        String names = "pobi,woni,jun";
+
+        // when
+        Cars cars = Cars.createCarsFrom(names);
+
+        // then
+        assertThat(cars.size()).isEqualTo(3);
+    }
+}
