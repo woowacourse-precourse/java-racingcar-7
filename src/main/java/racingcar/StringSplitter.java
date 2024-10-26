4160dd4488a45d01a63e838a3bf14863cf39d852
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-27 01:54:12 +0900

    refactor(StringSplitter): 인스턴스화를 방지하기 위해 기본 생성자를 private으로 추가
    - 유틸리티 클래스인 StringSplitter의 인스턴스 생성을 방지하기 위해 기본 생성자를 private으로 설정

--- src/main/java/racingcar/StringSplitter.java
+++ src/main/java/racingcar/StringSplitter.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StringSplitter {
+    private StringSplitter() {
+    }
 
     public static List<String> splitByComma(String input) {
         return Arrays.asList(input.split(","));
