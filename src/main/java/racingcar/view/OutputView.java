d4cab699cd048e5c8e49988fd1eb8d20984d6c39
seahwanKim <kimkimseasea@gmail.com>
2024-10-26 12:35:03 +0900

    feat: 실행결과 출력 기능을 추가한다

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,21 @@
+package racingcar.view;
+
+import racingcar.domain.Car;
+import racingcar.domain.CarRacing;
+
+import java.util.List;
+
+public class OutputView {
+
+    public static void resultMessage() {
+        System.out.println("\n실행 결과");
+    }
+
+    public static void roundResult(CarRacing carRacing) {
+        List<Car> carList = carRacing.getCars();
+        for (Car car : carList) {
+            System.out.println(car.getName() + " : " + car.getCurrentPosition());
+        }
+        System.out.println();
+    }
+}
