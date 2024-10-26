9a97ac3f87f97c8f1f5cc0460a0cf7351fa687b6
Hanjjong <tim4974@naver.com>
2024-10-26 21:23:23 +0900

    refactor: GameRoundResult 클래스 생성으로 인한 출력 로직 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,22 +5,28 @@ import java.util.StringJoiner;
 import racingcar.model.Car;
 import racingcar.model.Cars;
 import racingcar.model.GameResult;
+import racingcar.model.GameRoundResult;
 
 public class OutputView {
     public OutputView() {
     }
 
     public void printRoundResult(GameResult gameResult) {
-        List<Car> carList = gameResult.getCars().getCars();
+        List<GameRoundResult> roundResults = gameResult.getRoundResults();
 
-        for (Car car : carList) {
-            System.out.print(car.getName() + " : ");
-            for (int i = 0; i < car.getMoveCount(); i++) {
-                System.out.print("-");
+        for (GameRoundResult roundResult : roundResults) {
+            //메서드 분리 해야함
+            Cars cars = roundResult.getCars();
+            for (Car car : cars.getCars()) {
+                System.out.print(car.getName() + " : ");
+                for (int i = 0; i < car.getMoveCount(); i++) {
+                    System.out.print("-");
+                }
+                System.out.println();
             }
             System.out.println();
+
         }
-        System.out.println();
     }
 
     public void printRoundResultMessage() {
