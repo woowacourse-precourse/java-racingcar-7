a42e3aefdffe2072fad0e9d9698c536b035623b3
codrae <hi653@naver.com>
2024-10-26 21:01:23 +0900

    feat: Output Class 작성
    Output Class 및 관련 메서드 작성.

---
+++ src/main/java/view/Output.java
@@ -0,0 +1,23 @@
+package view;
+
+import model.Car;
+
+import java.util.List;
+
+public class Output {
+
+    public static void printRaceStart(){
+        System.out.println("\n실행 결과");
+    }
+
+    public static void printRaceStatus(List<Car> cars) {
+        for(Car car : cars) {
+            System.out.println(car);
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+}
