aefdc289203cdc65e4a7774503e0ac6f343a31b4
yybmion <yyb400@naver.com>
2024-10-26 19:54:06 +0900

    feat: 자동차 이름 길이 검증 기능 구현
    자동차 이름이 5자 초과 시 예외 발생한다.
    예외 발생 시 "자동차 이름은 5자 이하로 설정해주세요" 메시지를 예외와
    함께 응답한다.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,18 @@
 package racingcar;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private final String name;
+
     public Car(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하로 설정해주세요.");
+        }
     }
 }
