733ef182a22daaae1b75b09be966691f40239d07
sinminseok12 <sinminseok12@naver.com>
2024-10-26 21:09:34 +0900

    test : 자동차 이름 길이 검증기능 테스트

--- src/test/java/racingcar/model/RaceCarTest.java
+++ src/test/java/racingcar/model/RaceCarTest.java
@@ -2,14 +2,27 @@ package racingcar.model;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class RaceCarTest {
 
     @Test
     void 자동차_이동_테스트(){
-        RaceCar raceCar = RaceCar.fromName("raceCar");
+        RaceCar raceCar = RaceCar.fromName("car");
         raceCar.move();
 
         Assertions.assertThat(raceCar.getPosition()).isEqualTo(1);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123456", ""})
+    void 자동차_이름_길이(String carName) {
+        assertThatThrownBy(() -> RaceCar.fromName(carName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
