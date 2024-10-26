31c7cb47886694bed18ce7a297b33f72e975caac
Zepelown <dbswjdqhd12@gmail.com>
2024-10-26 21:30:36 +0900

    feat: Add winner detection feature

--- src/main/java/racingcar/model/RacingGameRoundResult.java
+++ src/main/java/racingcar/model/RacingGameRoundResult.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGameRoundResult {
@@ -11,11 +12,28 @@ public class RacingGameRoundResult {
         this.cars = cars;
     }
 
-    public int getRound() {
-        return round;
-    }
-
     public List<Car> getCars() {
         return cars;
+    }
+
+    public List<String> calculateWinners() {
+        List<String> winners = new ArrayList<>();
+        int maxLocation = findMaxLocation();
+
+        for (Car car : cars) {
+            if (car.getLocation() == maxLocation) {
+                winners.add(car.getName());
+            }
+        }
+
+        return winners;
+    }
+
+    private int findMaxLocation() {
+        int maxLocation = Integer.MIN_VALUE;
+        for (Car car : cars) {
+            maxLocation = Math.max(maxLocation, car.getLocation());
+        }
+        return maxLocation;
     }
 }
