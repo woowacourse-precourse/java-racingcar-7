e9bba0496a6635c38c94db46e01ceeba9b743c2d
brandnewchanwoo <brandnewchanwoo@gmail.com>
2024-10-26 15:12:09 +0900

    feat: 자동차 무작위 전진

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(name + " is more than 5 letters.");
+        }
+        this.name = name;
+        position = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void randomGo() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            position++;
+        }
+    }
+}
