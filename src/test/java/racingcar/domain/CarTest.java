350862c7f2ea6d765f88a8a2ca1f5e4cc8dd4cef
pakaheo <k70923@konkuk.ac.kr>
2024-10-26 19:31:08 +0900

    test : Add car constant
    create constant to use where CarTest

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -5,9 +5,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
+
+    private final Car CAR_POBI = new Car("pobi", 0);
+
     @Test
     void Car_객체_생성() {
-        Car car = new Car("pobi");
-        assertThat(car).isEqualTo(new Car("pobi"));
+        assertThat(CAR_POBI).isEqualTo(new Car("pobi", 0));
+    }
+
+    @Test
+    void 전진() {
+        assertThat(CAR_POBI.move(4)).isEqualTo(new Car("pobi", 1));
+    }
+
+    @Test
+    void 정지() {
+        assertThat(CAR_POBI.move(3)).isEqualTo(new Car("pobi", 0));
     }
 }
