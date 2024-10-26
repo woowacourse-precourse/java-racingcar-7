629654548515e5b3f1161e1890b3a5a3b554cabb
dnwldla <leeemjw@naver.com>
2024-10-26 15:05:15 +0900

    refactor: 랜덤 범위를 나타내는 수를 상수로 변경

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -3,10 +3,12 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator implements NumberGenerator {
+    private static final int MINIMUM_NUM=0;
+    private static final int MAXIMUM_NUM=9;
 
     @Override
     public int generateNumber() {
-        int randomNumber=Randoms.pickNumberInRange(0, 9);
+        int randomNumber=Randoms.pickNumberInRange(MINIMUM_NUM,MAXIMUM_NUM);
 
         return randomNumber;
     }
