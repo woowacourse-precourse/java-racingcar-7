3917349441bc54c47a926d4048c4eb7aa28b020a
Seongmo Ahn <djgood780@gmail.com>
2024-10-27 01:58:58 +0900

    refactor(CarNameValidator): separateByComma 메서드 로직 분리
    - 기존 separateByComma 메서드에 사용되던 carNames.matches() 부분을
    isCommaSeparated 메서드로 분리

--- src/main/java/racingcar/CarNameValidator.java
+++ src/main/java/racingcar/CarNameValidator.java
@@ -2,8 +2,13 @@ package racingcar;
 
 public class CarNameValidator {
     public void separateByComma(String carNames){
-        if (!carNames.matches("^[a-zA-Z0-9]+(,[a-zA-Z0-9]+)*$")) {
+        if (!isCommaSeparated(carNames)) {
             throw new IllegalArgumentException("쉼표(,)로 구분하여 자동차 이름들을 입력해주세요");
         }
     }
+
+    private boolean isCommaSeparated(String carNames){
+        return carNames.matches("^[a-zA-Z0-9]+(,[a-zA-Z0-9]+)*$");
+    }
+
 }
