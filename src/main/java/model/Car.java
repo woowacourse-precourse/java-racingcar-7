4616fd5f42e0543c6debaebe9caca1b1e2749a97
codrae <hi653@naver.com>
2024-10-26 20:54:02 +0900

    feat: getName() 구현
    getName()구현 및 java convention을 위한 코드 스타일 변경

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,26 +1,34 @@
 package model;
 
+import service.MoveStrategy;
+
 public class Car {
     private final String name; // 이름을 변경할 일은 없음.
-    private int positon;
+    private int position;
 
-    public int getPositon() { // alt + insert 를 통한 getter 생성
-        return positon;
+    public int getPosition() { // alt + insert 를 통한 getter 생성
+        return position;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public Car(String name) {
         this.name = name;
-        this.positon = 0;
+        this.position = 0;
     }
 
-    public void move(boolean canMove) {
-        if(canMove) positon++;
+    public void move(MoveStrategy moveStrategy) {
+        if(moveStrategy.canMove()) {
+            this.position++;
+        }
     }
 
     // Object 클래스의 toString함수를 오버라이드
     @Override
     public String toString() {
-        return name + " : " + "-".repeat(Math.max(0, positon));
+        return name + " : " + "-".repeat(Math.max(0, position));
     }
 }
 
