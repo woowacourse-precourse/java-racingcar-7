70066aea84328bfec8e549651b3178c72744d494
Revi1337 <david122123@gmail.com>
2024-10-26 17:55:24 +0900

    refacator: RandomNumberGenerator 상수 변경
    - NUMBER 와 같은 이름보다 Java 에서 사용하는 네이밍 컨벤션으로 변경.

--- src/main/java/racingcar/game/model/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/game/model/util/RandomNumberGenerator.java
@@ -3,10 +3,10 @@ package racingcar.game.model.util;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public abstract class RandomNumberGenerator {
-    private static final int MIN_NUMBER = 0;
-    private static final int MAX_NUMBER = 9;
+    private static final int MIN_INCLUSIVE = 0;
+    private static final int MAX_INCLUSIVE = 9;
 
     public static int generate() {
-        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+        return Randoms.pickNumberInRange(MIN_INCLUSIVE, MAX_INCLUSIVE);
     }
 }
