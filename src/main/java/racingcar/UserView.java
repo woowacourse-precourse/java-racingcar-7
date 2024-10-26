8000528a2e85617bfc16105142cfa641f8633a6c
33jyu33 <jy04169@gmail.com>
2024-10-26 13:39:45 +0900

    feat(UserView): readAttemptNum print guide and return input
    Function readAttemptNum:
    - call printAttemptNumGuide
    - read and return input line
    Function printAttemptNumGuide:
    - print a guide to the number of attempts

--- src/main/java/racingcar/UserView.java
+++ src/main/java/racingcar/UserView.java
@@ -7,7 +7,17 @@ public class UserView {
         printCarNameGuide();
         return readLine();
     }
+
     private static void printCarNameGuide(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static String readAttemptNum(){
+        printAttemptNumGuide();
+        return readLine();
+    }
+
+    private static void printAttemptNumGuide(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
     }
 }
