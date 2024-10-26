57d88c7d98485c1100b2943937eb9f67db9cc7a1
ddglackrp <ddglackrp@naver.com>
2024-10-27 02:05:50 +0900

    feat "InputView 추가"

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+    public static String inputCarsName() {
+        return readLine();
+    }
+    public static String inputTryNumber() {
+        return readLine();
+    }
+}
