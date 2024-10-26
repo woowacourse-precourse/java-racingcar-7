7fa0acfe75653cba32f366d3cdeb82dbcd39c410
DongHoon Lee <dhl1924@naver.com>
2024-10-27 02:16:39 +0900

    refactor(view): InputView 및 OutputView를 인터페이스로 변경
    - View가 콘솔이 아닌 웹으로 변경될 경우를 대비해 인터페이스로 추상화하여 유연성을 확보

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,12 +1,7 @@
 package racingcar.view;
 
-import camp.nextstep.edu.missionutils.Console;
 import racingcar.dto.CarNamesInput;
 
-public class InputView {
-
-	public CarNamesInput readCarNames() {
-		String CarNames = Console.readLine();
-		return new CarNamesInput(CarNames.trim());
-	}
+public interface InputView {
+	CarNamesInput readCarNames();
 }
