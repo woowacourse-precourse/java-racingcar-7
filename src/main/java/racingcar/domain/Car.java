eac2c25a6d6cdf7edcaa9400d649eebc36bc904f
SongJaeHoonn <thdwogns121@naver.com>
2024-10-26 21:04:09 +0900

    feat: Car 도메인(자동차 관리 도메인) 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+public class Car {
+
+    private final String name;
+    private Progress progress;
+
+    public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 넘을 수 없습니다.");
+        }
+        this.name = name;
+        this.progress = new Progress("");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProgress() {
+        return progress.getValue();
+    }
+
+    public void moveForward() {
+        this.progress = progress.addStep("-");
+    }
+}
