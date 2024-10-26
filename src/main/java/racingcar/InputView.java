39718598ec27e675666db636c550d1bc1b837de4
seonseon1515 <seonseon1515@gmail.com>
2024-10-26 13:14:09 +0900

    feat: 게임 횟수 입력값 받기

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static String carNames;
+    private static int roundNumber;
 
     String inputCarName(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -11,7 +12,17 @@ public class InputView {
         return carNames;
     }
 
+    int inputRounds(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        roundNumber = Integer.parseInt(Console.readLine());
+        return roundNumber;
+    }
+
     public static String getCarNames() {
         return carNames;
+    }
+
+    public static int getRoundNumber() {
+        return roundNumber;
     }
 }
