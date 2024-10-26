76de0d3fa99e932fc48fcbe4df4c8aa3f1d7b5e6
dudxo <father8475@naver.com>
2024-10-26 19:07:22 +0900

    feat(입력값 검증기) : 입력값에 대해 검증 입력값이 null 또는 공백 값인지 확인한다. 각 자동차의 이름이 5자 이하인지 확인한다. 입력 받은 시도할 횟수 값이 숫자이며, 양수 값인지 확인한다.

---
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -0,0 +1,42 @@
+package racingcar.validator;
+
+import java.util.Arrays;
+
+public class InputValidator {
+
+    public boolean isInputEmpty(String inputCarNames) {
+        if (inputCarNames == null || inputCarNames.isBlank()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean validCarNameLength(String[] carNames) {
+        return Arrays.stream(carNames)
+                .allMatch(c -> c.length() <= 5);
+    }
+
+    public int getValidatedRacingAttempt(String inputRacingAttempt) {
+        try {
+            int attempt = convertToInteger(inputRacingAttempt);
+            isRacingAttemptPositive(attempt);
+            return attempt;
+        } catch (RuntimeException e) {
+            throw new IllegalArgumentException("양수를 입력해주세요.");
+        }
+    }
+
+    private void isRacingAttemptPositive(int inputRacingAttempt) {
+        if (inputRacingAttempt <= 0) {
+            throw new RuntimeException();
+        }
+    }
+
+    private int convertToInteger(String inputRacingAttempt) {
+        try {
+            return Integer.parseInt(inputRacingAttempt);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException();
+        }
+    }
+}
