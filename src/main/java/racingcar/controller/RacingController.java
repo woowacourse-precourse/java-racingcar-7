764faca9d43f78415a35b20162e4f40e7b0085c8
KJungW <sagakkb0707@naver.com>
2024-10-26 17:57:06 +0900

    feat(input): 전진 시도 횟수 입력 기능 구현
    구현된 기능 세부 목록 (what)
    - 사용자로부터 경주 동안 몇번의 전진 시도를 할지 입력받음
    - 전진 시도 횟수를 입력한 뒤에는 비어있는 한 줄을 출력
    
    기능을 구현한 이유 (why)
    - 경주 동안 몇번의 전진 시도를 할지 사용가 직접
      설정할 수 있도록 하기 위해 기능을 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -14,11 +14,17 @@ public class RacingController {
 
     public void startRacing() {
         List<Car> cars = prepareRacingCar();
+        int roundCount = prepareRoundCount();
     }
 
     private List<Car> prepareRacingCar() {
         String rawCarNames = inputView.inputCarNames();
         List<String> carNames = List.of(rawCarNames.split(","));
         return carNames.stream().map(Car::new).toList();
+    }
+
+    private int prepareRoundCount() {
+        String rawRoundCount = inputView.inputRoundCount();
+        return Integer.parseInt(rawRoundCount);
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,4 +8,11 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
+
+    public String inputRoundCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String rawRoundCount = Console.readLine();
+        System.out.print("\n");
+        return rawRoundCount;
+    }
 }
