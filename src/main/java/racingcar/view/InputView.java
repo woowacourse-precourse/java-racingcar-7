d862258d3374e0a2b480d387932734784e09a1c7
18hailey <hailey9459@gmail.com>
2024-10-27 05:19:17 +0900

    feat(InputView): print input message to users
    - create inputCarNames() method to input car names
    - create inputCount() method to input game rounds

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,19 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return readLine();
+    }
+
+    public static String inputCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return readLine();
+    }
+
+    public static String readLine() {
+        return Console.readLine();
+    }
 }
