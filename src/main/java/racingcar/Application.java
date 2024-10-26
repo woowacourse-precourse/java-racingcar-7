c532de26ab3747659bf19fce2f3d28c7690af76d
Mae-Hyong <maehyong02@gmail.com>
2024-10-26 23:22:55 +0900

    feat(input): Split car names by comma and store in a list

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,11 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = Console.readLine();
@@ -13,5 +17,7 @@ public class Application {
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String tryCount = Console.readLine();
+
+        List<String> carNameList = Arrays.asList(carName.split(","));
     }
 }
