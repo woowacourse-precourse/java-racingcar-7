a6f3bd72253e5d7eb6b48b1585da83c9ff4339b0
KoYunHyuk <59502729+KoYunHyuk@users.noreply.github.com>
2024-10-27 02:19:23 +0900

    feat: List에 추가하기 위한 Car 객체 생성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
