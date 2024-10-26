3d9ea6109cdafceab7d755760a04b311820cc84d
nahyunKoo <angelcindy09@gmail.com>
2024-10-27 04:57:50 +0900

    fix (tryCountValidate) : 시도 횟수 0보다 작을 시 예외 처리

--- src/main/java/racingcar/domain/TryCount.java
+++ src/main/java/racingcar/domain/TryCount.java
@@ -4,10 +4,17 @@ public class TryCount {
     private int tryCount;
 
     public TryCount(int tryCount) {
+        tryCountValidate(tryCount);
         this.tryCount = tryCount;
     }
 
     public int getTryCount() {
         return tryCount;
+    }
+
+    private void tryCountValidate(int tryCount) {
+        if(tryCount <= 0){
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 0보다 작을 수 없습니다");
+        }
     }
 }
