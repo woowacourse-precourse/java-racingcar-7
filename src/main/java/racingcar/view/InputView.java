fa9645a1ef7ef85aa2e8ef8c61a8da9642c78ced
jklee3409 <jklee3409@khu.ac.kr>
2024-10-26 17:06:07 +0900

    fix(inputview): validateCarNames 메서드 수정
    자동차 이름에 공백이나 개행문자 허용하지 않음. 또한 이름을 지정하지 않는 것도 허용하지 않음

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
     public List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
-        List<String> carNames = Arrays.asList(input.split(","));
+        List<String> carNames = Arrays.asList(input.split(",", -1));
 
         validateCarNames(carNames);
         return carNames;
@@ -25,10 +25,17 @@ public class InputView {
     }
 
     private void validateCarNames(List<String> carNames) {
-        if (carNames.isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름을 입력해야 합니다.");
-        } else if (carNames.stream().anyMatch(name -> name.length() > 5)) {
+        if (carNames.stream().anyMatch(name -> name.trim().isEmpty())) {
+            throw new IllegalArgumentException("자동차 이름은 빈 문자열이나 공백을 포함할 수 없습니다.");
+        }
+
+        if (carNames.stream().anyMatch(name -> name.matches(".*[\\s\\p{Cntrl}].*"))) {
+            throw new IllegalArgumentException("자동차 이름에 공백이나 제어 문자는 포함될 수 없습니다.");
+        }
+
+        if (carNames.stream().anyMatch(name -> name.length() > 5)) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
         }
     }
+
 }
