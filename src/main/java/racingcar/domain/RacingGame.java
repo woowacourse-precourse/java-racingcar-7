8b16e1d144c54c0524013873f12d08c27409fc89
joon <khjoon372@naver.com>
2024-10-26 13:36:40 +0900

    feat(RacingGame): 입력받은 횟수만큼 게임 실행 기능 구현

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,16 +1,29 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.view.OutputView;
 
 import java.util.List;
 
 public class RacingGame {
 
     final List<Car> playRacingCarList;
+
     public RacingGame(List<Car> playRacingCarList){
 
         this.playRacingCarList = playRacingCarList;
     }
+
+    public void runGame(int trialCount){
+
+        for(int i = 0; i < trialCount; i++){
+
+            playRacingCarList.forEach(this::moveForwardCar);
+
+            OutputView.printOneGameResult(playRacingCarList);
+        }
+    }
+
 
     private void moveForwardCar(Car car){
 
