d2bd1cd34bfadff89e379f4820a565fd896f60dc
Hanjjong <tim4974@naver.com>
2024-10-27 00:43:12 +0900

    refactor: 출력 안내 메시지 상수화

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,6 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRY_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
     public InputView() {
     }
 
@@ -13,7 +16,7 @@ public class InputView {
     }
 
     private void printCarNamesInputMessage() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(CAR_NAME_INPUT_MESSAGE);
     }
 
     public String gameRoundInput() {
@@ -22,6 +25,6 @@ public class InputView {
     }
 
     private void printGameRoundInputMessage() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(TRY_INPUT_MESSAGE);
     }
 }
