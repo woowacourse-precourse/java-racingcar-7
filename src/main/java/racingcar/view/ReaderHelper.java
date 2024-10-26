1f1a3cb69dc1854c9b579bbbf198f1e7bd23c7aa
yuntasha <jaljayo85@gmail.com>
2024-10-27 02:25:54 +0900

    refactor(ReaderHelper.java): make static field
    DELIMITER change to static field

--- src/main/java/racingcar/view/ReaderHelper.java
+++ src/main/java/racingcar/view/ReaderHelper.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ReaderHelper {
-    private final String DELIMITER = ",";
+    private static final String DELIMITER = ",";
 
     public List<String> readPlayerName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
