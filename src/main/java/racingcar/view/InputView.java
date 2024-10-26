d26b3a8f719283fda50df802cad26eb39b8dfa17
fbehddn <tom0611@naver.com>
2024-10-26 21:41:50 +0900

    style: 출력 메세지 알맞게 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,12 +4,12 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
     public String[] inputNames() {
-        System.out.println("사용자 이름을 입력해주세요 ( , 로 구분)");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return readLine().split(",");
     }
 
     public int inputTimes() {
-        System.out.println("총 시도 횟수를 입력해주세요");
+        System.out.println("시도할 횟수는 몇 회인가요?");
         return Integer.parseInt(readLine());
     }
 }
