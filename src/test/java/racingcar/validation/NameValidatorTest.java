9bf51a0ad24bc92b08911cd73fd0bc2a10804c6c
kkmdevel <kkm.devel@gmail.com>
2024-10-26 21:07:02 +0900

    test: NameValidatorTest 생성
    이름 문자열 검증에 대한 Test Case 생성
    1. 빈 문자열
    2. 11개 이상
    3. 비어있는 이름 포함
    4. 이름이 6자 이상

---
+++ src/test/java/racingcar/validation/NameValidatorTest.java
@@ -0,0 +1,41 @@
+package racingcar.validation;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NameValidatorTest {
+
+    @Test
+    @DisplayName("빈 문자열 예외")
+    public void testEmptyNameString() {
+        assertThrows(IllegalArgumentException.class, () ->
+                NameValidator.validateNameString(List.of(""))
+        );
+    }
+
+    @Test
+    @DisplayName("11개 이상의 이름 예외")
+    public void testCountNameString() {
+        assertThrows(IllegalArgumentException.class, () ->
+                NameValidator.validateNameString(List.of("1","2","3","4","5","6","7","8","9","10","11"))
+        );
+    }
+
+    @Test
+    @DisplayName("비어있는 이름 예외")
+    public void testEmptyName() {
+        assertThrows(IllegalArgumentException.class, () ->
+                NameValidator.validateNameString(List.of("a","","b"))
+        );
+    }
+
+    @Test
+    @DisplayName("이름 길이가 6개 이상 예외")
+    public void testLengthName() {
+        assertThrows(IllegalArgumentException.class, () ->
+                NameValidator.validateNameString(List.of("123456"))
+        );
+    }
+}
