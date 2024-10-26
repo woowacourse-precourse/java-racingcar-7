8fd00dcf1108e2a021eaa7396a78d37cb9b32ad4
mhj1205 <lightnow1205@gmail.com>
2024-10-26 19:56:51 +0900

    feat: 자동차 이름 쉼포(,) 구분자로 구별하여 배열에 넣기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,6 @@
 package racingcar;
 import java.util.Scanner;
 
-import com.sun.nio.sctp.SctpChannel;
-
 public class Application {
     public static void main(String[] args) {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -13,6 +11,8 @@ public class Application {
             System.out.println("시도할 횟수는 몇 회인가요?");
             int cnt = sc.nextInt();
             // System.out.println(cnt);
-            
+
+            String[] cars = cars_str.split(",");
+        
     }
 }
