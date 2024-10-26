c8f6af04550b587a78cc897ff78a85a89d7e0345
lsj0822 <lsj0822@khu.ac.kr>
2024-10-26 14:30:21 +0900

    fix(ContentParser) : 기능 수정
    - ContentParser의 경우 신뢰할 수 있는 파싱된 결과를 변환해야하기 때문에, InspectName 메서드를 racingCar 패키지가 아닌 util패키지에서 검사하도록 변경
    - 그에 따른 inspectingPlayerName 메서드를 추가하여 검사하도록 변경

--- src/main/java/utils/ContentParser.java
+++ src/main/java/utils/ContentParser.java
@@ -1,6 +1,7 @@
 package utils;
 
-import constant.Constant;
+import constant.Constants;
+import inspector.InspectName;
 
 public class ContentParser {
     public static String[] parsingContentToGroup(String targetContent){
@@ -8,14 +9,19 @@ public class ContentParser {
         for(int i = 0; i < parsedGroup.length; i++){
             parsedGroup[i] = removingWhiteSpace(parsedGroup[i]);
         }
+        inspectingPlayerName(parsedGroup);
         return parsedGroup;
     }
-
     private static String[] splitting(String targetContent){
-        return targetContent.split(Constant.CONTENT_PASRE_DELIMITER);
+        return targetContent.split(Constants.CONTENT_PASRE_DELIMITER);
     }
 
     private static String removingWhiteSpace(String targetContent){
         return targetContent.strip();
+    }
+
+    private static void inspectingPlayerName(String[] playerGroup){
+        InspectName inspectName = new InspectName();
+        inspectName.inspecting(playerGroup);
     }
 }
