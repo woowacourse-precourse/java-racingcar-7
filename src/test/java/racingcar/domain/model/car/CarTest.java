b0cae9a67bd9f7b430d2044f48ac96187a295011
minSsan <msp1125@hanyang.ac.kr>
2024-10-26 16:44:51 +0900

    test(Car): Car 객체 생성 테스트

---
+++ src/test/java/racingcar/domain/model/car/CarTest.java
@@ -0,0 +1,46 @@
+package racingcar.domain.model.car;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CarTest {
+    @Test
+    @DisplayName("다섯 글자를 넘는 이름은 설정할 수 없다.")
+    void 예외_이름_다섯_글자_초과() {
+        // given
+        final String input = "일이삼사오육";
+
+        // when & then
+        assertThatThrownBy(() -> {
+            Car.from(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("공백은 이름으로 설정할 수 없다.")
+    @ValueSource(strings = {"", " ", "      "})
+    void 예외_이름_공백(String input) {
+        // given
+        // when & then
+        assertThatThrownBy(() -> {
+            Car.from(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("앞,뒤 공백은 무시한다.")
+    @ValueSource(strings = {"   일이삼사오   ", "    123"})
+    void 정상_이름_앞_뒤_공백_무시(String input) {
+        // given
+        // when
+        Car car = Car.from(input);
+
+        // then
+        assertThat(car.getName()).isEqualTo(input.trim());
+    }
+}
