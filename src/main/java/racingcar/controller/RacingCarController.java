f4c78237971dad86d601f068da65b7f96678fb56
binn <anyubin0724@naver.com>
2024-10-26 17:50:34 +0900

    refactor: 사용 안하는 라이브러리 제거

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,8 +1,6 @@
 package racingcar.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import racingcar.exceptions.ValidateError;
 import racingcar.model.Car;
 import racingcar.service.ParsingCarNameService;
 import racingcar.service.RacingCarService;
