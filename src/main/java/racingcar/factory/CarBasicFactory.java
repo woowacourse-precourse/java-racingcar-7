7efcd950e8fe0191b9b00ca3e541541acdaa099b
jiffyin7 <jiffyin7@gmail.com>
2024-10-26 20:37:11 +0900

    refactor(car) : `CarFactory` 인터페이스 및 구현체 개선
    - `CarBasicFactory`로 구현체 이름 변경
    - `CarsType` 열거형을 별도 패키지로 이동
    - `CarFactory` 인터페이스에 `createCars` 메서드 파라미터 변경
      - `CarsType` 매개변수 추가하여 유연성 확보
    - `createDefaultCars` 메서드 추가하여 기본 `Cars` 생성 기능 분리
    - `CarDefaultFactory` 클래스 내부 로직 개선
      - 메서드 분리를 통한 단일 책임 원칙 적용
      - `validateNames`, `splitCarNames`, `getListFromRawCarNames` 등 헬퍼 메서드 추가
      - `createCars`와 `createDefaultCars` 메서드 구현 간소화

---
+++ src/main/java/racingcar/factory/CarBasicFactory.java
@@ -0,0 +1,76 @@
+package racingcar.factory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import racingcar.common.constant.CarsType;
+import racingcar.model.Car;
+import racingcar.model.Cars;
+import racingcar.strategy.CarStrategy;
+import racingcar.strategy.CarsStrategy;
+
+/**
+ * @author : jiffyin7@gmail.com
+ * @since : 24. 10. 25.
+ */
+public class CarBasicFactory implements CarFactory{
+  private final CarStrategy carStrategy;
+  private final CarsStrategy carsStrategy;
+
+  public CarBasicFactory(CarStrategy carStrategy, CarsStrategy carsStrategy) {
+    this.carStrategy = carStrategy;
+    this.carsStrategy = carsStrategy;
+  }
+
+  @Override
+  public Car createCar(String carName) {
+    return Car.from(carName);
+  }
+
+  @Override
+  public Function<List<Car>, Cars> getDefaultCollection() {
+    return carsStrategy.getDefaultCollection();
+  }
+
+  @Override
+  public Function<List<Car>, Cars> getCollection (CarsType carsType) {
+    return carsStrategy.getCollection(carsType);
+  }
+
+  @Override
+  public List<Car> createList(String[] carNames) {
+    return Arrays.stream(carNames)
+        .map(this::createCar)
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public void validateNames(String[] carNames){
+    carStrategy.validateNames(carNames);
+  }
+
+  @Override
+  public String[] splitCarNames(String rawCarNames) {
+    return carStrategy.splitNames(rawCarNames);
+  }
+
+  @Override
+  public List<Car> getListFromRawCarNames(String rawCarNames){
+    String[] carNames = splitCarNames(rawCarNames);
+    validateNames(carNames);
+    return createList(carNames);
+  }
+
+  @Override
+  public Cars createCars(String rawCarNames, CarsType carsType) {
+    return getCollection(carsType)
+        .apply(getListFromRawCarNames(rawCarNames));
+  }
+
+  @Override
+  public Cars createDefaultCars(String rawCarNames) {
+    return getDefaultCollection()
+        .apply(getListFromRawCarNames(rawCarNames));
+  }
+}
--- src/main/java/racingcar/factory/CarFactory.java
+++ src/main/java/racingcar/factory/CarFactory.java
@@ -1,5 +1,8 @@
 package racingcar.factory;
 
+import java.util.List;
+import java.util.function.Function;
+import racingcar.common.constant.CarsType;
 import racingcar.model.Car;
 import racingcar.model.Cars;
 
@@ -8,6 +11,16 @@ import racingcar.model.Cars;
  * @since : 24. 10. 25.
  */
 public interface CarFactory{
+  Function<List<Car>, Cars> getDefaultCollection ();
+  Function<List<Car>, Cars> getCollection (CarsType carsType);
+  public List<Car> createList(String[] carNames);
+
+  public void validateNames(String[] carNames);
+  public String[] splitCarNames(String rawCarNames);
+
+  public List<Car> getListFromRawCarNames(String rawCarNames);
+
   Car createCar(String carName);
-  Cars createCars(String rawCarNames);
+  Cars createCars(String rawCarNames, CarsType carsType);
+  Cars createDefaultCars(String rawCarNames);
 }
