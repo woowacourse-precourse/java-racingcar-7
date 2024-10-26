2d4f159c3dc71b3bda2f535f1e7d946e3d771250
SeungminKimdev <ksm7250432@gmail.com>
2024-10-26 16:53:33 +0900

    fix: 누락된 질문 문구 추가

--- src/main/java/racingcar/view/InputTurn.java
+++ src/main/java/racingcar/view/InputTurn.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputTurn {
     public static String getTurn(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
         return Console.readLine();
     }
 }
