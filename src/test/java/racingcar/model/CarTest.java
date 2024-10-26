d3f75d32aa815c846e8a56c763115ec0922cbc33
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-10-26 16:36:39 +0900

    refactor: Cartest CarNametest 분리

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -6,29 +6,6 @@ import static org.assertj.core.api.Assertions.*;
 
 
 class CarTest {
-
-    @Test
-    void 자동차_이름_null_실패(){
-        assertThatThrownBy(()->new Car(null))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이름이 null이 될 수 없습니다.");
-    }
-    @Test
-    void 자동차_이름_길이가_5글자보다_큰_입력_실패(){
-        assertThatThrownBy(()->new Car("aaaaaa"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-    @Test
-    void 자동차_이름_빈_문자열_입력_실패(){
-        assertThatThrownBy(()->new Car(""))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-    @Test
-    void 자동차_이름_영어_입력_실패(){
-        assertThatThrownBy(()->new Car("123"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
     @Test
     void 자동차_객체_생성_성공(){
         Car car=new Car("pobi");
