7e749579d1d813bde49f356e420595234e2a7ee3
wvssm <neoramiregi@gmail.com>
2024-10-26 23:11:55 +0900

    refactor(ValidUtilTest): Test 이름 명확하게 변경

--- src/test/java/racingcar/util/ValidUtilTest.java
+++ src/test/java/racingcar/util/ValidUtilTest.java
@@ -9,19 +9,19 @@ import static racingcar.util.ValidUtil.*;
 
 class ValidUtilTest {
     @Test
-    public void 이동_횟수는_정수여야_한다(){
+    public void 이동_횟수는_정수여야_한다_정수가_아닌_경우(){
         String moveCount = "hi";
         assertThat(isNumeric(moveCount)).isEqualTo(false);
     }
 
     @Test
-    public void 이동_횟수는_빈_값이_아니여야한다(){
+    public void 이동_횟수는_빈_값이_아니여야한다_빈_값인_경우(){
         String moveCount = "";
         assertThat(isEmpty(moveCount)).isEqualTo(true);
     }
 
     @Test
-    public void 이동_횟수는_1_이상이어야_한다(){
+    public void 이동_횟수는_1_이상이어야_한다_음수인_경우(){
         String moveCount = "-1";
         assertThat(isPositive(moveCount)).isEqualTo(false);
     }
