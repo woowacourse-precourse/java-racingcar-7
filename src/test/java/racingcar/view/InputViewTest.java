60abc2066accb0814abc885915370aa51d92b973
unh6unh6 <ruddbs3308@naver.com>
2024-10-26 14:53:38 +0900

    test(view): 자동차 이름 입력 안내 문구 출력

---
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -0,0 +1,28 @@
+package racingcar.view;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class InputViewTest {
+    InputView inputView;
+
+    @BeforeEach
+    void init() {
+        inputView = new InputView();
+    }
+
+    @Test
+    void 자동차_이름_입력_안내_문구_출력() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        inputView.printCarNameInputGuide();
+
+        assertThat(outputStream.toString().trim())
+                .isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+}
