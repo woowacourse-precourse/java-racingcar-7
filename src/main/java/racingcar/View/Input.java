21a9909a29b61dc4022adc095047df1f254078d1
김호연 <yun1094@naver.com>
2024-10-26 10:01:15 +0900

    예약어가 포함된 함수명 수정

--- src/main/java/racingcar/View/Input.java
+++ src/main/java/racingcar/View/Input.java
@@ -1,11 +1,11 @@
-package View;
+package racingcar.View;
 
 public class Input {
     public static void RequestCarNameMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
     }
 
-    public static void RequestNumberOfTryMessage() {
+    public static void RequestNumberOfTrialMessage() {
         System.out.println("시도할 횟수는 몇 회인가요?");
     }
 }
