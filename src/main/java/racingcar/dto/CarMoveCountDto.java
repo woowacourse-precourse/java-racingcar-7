af3c0713e9c809feb391c892f44a1a0143fc2ef4
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2024-10-26 19:31:10 +0900

    refactor : 사용하지 않던 static 함수 사용하도록 수정

--- src/main/java/racingcar/dto/CarMoveCountDto.java
+++ src/main/java/racingcar/dto/CarMoveCountDto.java
@@ -1,19 +1,25 @@
 package racingcar.dto;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public record CarMoveCountDto(String name, int moveCount) {
 
     public static List<String> getCollectNameWithSameMoveCount(List<CarMoveCountDto> carMoveCountDtoList, int maxMoveCount) {
-        return carMoveCountDtoList.stream()
+        if(maxMoveCount == 0) {
+            return Collections.emptyList();
+        }
+        return carMoveCountDtoList
+                .stream()
                 .filter(carMoveCountDto -> carMoveCountDto.moveCount() == maxMoveCount)
                 .map(CarMoveCountDto::name)
                 .collect(Collectors.toList());
     }
 
     public static int getMaxMoveCount(List<CarMoveCountDto> carMoveCountDtoList) {
-        int maxMoveCount = carMoveCountDtoList.stream()
+        int maxMoveCount = carMoveCountDtoList
+                .stream()
                 .mapToInt(CarMoveCountDto :: moveCount)
                 .max()
                 .orElse(0);
