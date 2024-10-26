1413fed79f7b04cb3900a9e4b10db3382df7ac1b
wogus <ind07152@naver.com>
2024-10-26 17:01:19 +0900

    refactor(Random): Random의 기본 숫자들 상수화
    Random의 기본 숫자들을 NumberType으로 상수화한다.

--- src/main/java/racingcar/utils/Random.java
+++ src/main/java/racingcar/utils/Random.java
@@ -1,9 +1,12 @@
 package racingcar.utils;
 
+import static racingcar.constant.NumberType.MAX_RANDOM_NUMBER;
+import static racingcar.constant.NumberType.MIN_RANDOM_NUMBER;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public final class Random {
     public static int createRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER.getNumber(), MAX_RANDOM_NUMBER.getNumber());
     }
 }
