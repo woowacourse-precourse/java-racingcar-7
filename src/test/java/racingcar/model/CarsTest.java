acae5ff9dbeb12b2765fcc7fe742dde2599bd6fe
yush <yush2202@gmail.com>
2024-10-26 20:17:51 +0900

    test($ CarsTest): 중복 검사 코드 테스트 추가

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -13,14 +13,14 @@ import racingcar.Application;
 
 public class CarsTest extends NsTest {
     static Cars testCars;
-    static List<CarRacer> answerWinner;
+    static List<CarName> answerWinner;
 
     @BeforeAll
     static void beforeAll() {
-        CarRacer turtle = new CarRacer("거북이");
-        CarRacer rabbit = new CarRacer("토끼");
-        CarRacer seaKing = new CarRacer("용왕");
-        CarRacer zara = new CarRacer("자라");
+        CarName turtle = new CarName("거북이");
+        CarName rabbit = new CarName("토끼");
+        CarName seaKing = new CarName("용왕");
+        CarName zara = new CarName("자라");
 
         Car car1 = new Car(turtle);
         Car car2 = new Car(rabbit);
@@ -34,9 +34,9 @@ public class CarsTest extends NsTest {
     @Test
     @DisplayName("우승자를 제대로 반환하는지 확인합니다.")
     void cars_우승자_테스트() {
-        List<CarRacer> testWinners = testCars.getFinalWinners();
-        List<String> testWinnersName = testWinners.stream().map(CarRacer::getRacerName).toList();
-        List<String> answerWinnerName = answerWinner.stream().map(CarRacer::getRacerName).toList();
+        List<CarName> testWinners = testCars.getFinalWinners();
+        List<String> testWinnersName = testWinners.stream().map(CarName::getRacerName).toList();
+        List<String> answerWinnerName = answerWinner.stream().map(CarName::getRacerName).toList();
         assertThat(testWinnersName).isEqualTo(answerWinnerName);
     }
 
@@ -46,6 +46,24 @@ public class CarsTest extends NsTest {
         Assertions.assertThatThrownBy(() -> new Cars(new ArrayList<>()))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("자동차 컬렉션의 중복을 확인합니다.")
+    void cars_중복_테스트() {
+        CarName turtle = new CarName("거북이");
+        CarName rabbit = new CarName("토끼");
+        CarName seaKing = new CarName("용왕");
+        CarName rabbit2 = new CarName("토끼");
+
+        Car car5 = new Car(turtle);
+        Car car6 = new Car(rabbit);
+        Car car7 = new Car(seaKing);
+        Car car8 = new Car(rabbit2);
+
+        Assertions.assertThatThrownBy(() -> new Cars(new ArrayList<>(List.of(car5, car6, car7, car8))))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 
     @Override
     public void runMain() {
