8931d5ff915d600990d89e2959c7422fefa0663b
softmoca <acyc0909@gmail.com>
2024-10-26 12:24:26 +0900

    feat : 사용자로부터 자동차들 이름 입력 받는 기능 구현

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -1,6 +1,14 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class View {
+    public static String displayCarNameInputPrompt() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String CarNamesInput = Console.readLine().trim();
+        return CarNamesInput;
+
+    }
 
 
 }
