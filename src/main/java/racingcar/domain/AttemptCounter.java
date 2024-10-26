2edb092940e6786d8e847c567147e7e9e8dc4a8a
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-26 13:09:22 +0900

    refactor : 시도횟수 저장 객체 이름 변경

---
+++ src/main/java/racingcar/domain/AttemptCounter.java
@@ -0,0 +1,21 @@
+package racingcar.domain;
+
+//note attemptcounter get 해오지 말고 내부에서 차감
+public class AttemptCounter {
+    private int attemptNumber;
+
+    public AttemptCounter(int attemptNumber) {
+        this.attemptNumber = attemptNumber;
+    }
+
+    public boolean isEndAttempt() {
+        return attemptNumber == 0;
+    }
+
+    public void decrease() {
+        this.attemptNumber--;
+    }
+
+    //todo 시도 횟수 유효검사ㅣ
+
+}
