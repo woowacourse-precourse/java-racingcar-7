3699f63bfeb4c0621fe054b9faaa493faa548a78
Beomseok Noh <prodigy0831@naver.com>
2024-10-26 12:07:43 +0900

    refactor(CheckInput): 큰 함수를 단일 역할을 수행하는 작은 함수로 분리

--- src/main/java/racingcar/CheckInput.java
+++ src/main/java/racingcar/CheckInput.java
@@ -11,31 +11,55 @@ public class CheckInput {
     private static final String ERROR_NAME_ALREADY_EXIST = "중복된 이름이 존재합니다.";
     private static final String ERROR_INVALID_COUNT = "올바른 숫자를 입력해 주세요.";
 
-
-
     public String[] splitNames(String carNameString) {
+        String[] carNames = splitByDelimiter(carNameString);
+        validateCarNames(carNames);
+        return checkAlreadyExist(carNames);
+    }
 
-
-        String[] cars = carNameString.split(DELIMITER);
+    private String[] checkAlreadyExist(String[] carNames) {
         Set<String> nameSet = new HashSet<>();
-        for (String car : cars) {
-            String trimmedCarName = car.trim();
-            if (trimmedCarName.length() > MAX_NAME_LENGTH) {
-                throw new IllegalArgumentException(ERROR_NAME_LENGTH);
-            }
-            if (nameSet.contains(trimmedCarName)) {
+        for (String carName : carNames) {
+            if (!nameSet.add(carName)) {
                 throw new IllegalArgumentException(ERROR_NAME_ALREADY_EXIST);
             }
-            nameSet.add(trimmedCarName);
         }
-
         return nameSet.toArray(new String[0]);
     }
 
+    private void validateCarNames(String[] carNames) {
+        for (String car : carNames) {
+            String trimmedCarName = trimCarName(car);
+            checkLength(trimmedCarName);
+        }
+    }
+
+    private String[] splitByDelimiter(String carNameString) {
+        return carNameString.split(DELIMITER);
+    }
+
+    private String trimCarName(String car) {
+        return car.trim();
+    }
+
+    private void checkLength(String trimmedCarName) {
+        if (trimmedCarName.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(ERROR_NAME_LENGTH);
+        }
+    }
+
     public int isValidCount(String raceCountString) {
-        if (raceCountString == null || raceCountString.isEmpty() || !raceCountString.matches(NUMBER_REGEX)) {
+        validateCount(raceCountString);
+        return toInt(raceCountString);
+    }
+
+    private int toInt(String raceCountString) {
+        return Integer.parseInt(raceCountString);
+    }
+
+    private void validateCount(String raceCountString) {
+        if (raceCountString == null || raceCountString.isBlank() || !raceCountString.matches(NUMBER_REGEX)) {
             throw new IllegalArgumentException(ERROR_INVALID_COUNT);
         }
-        return Integer.parseInt(raceCountString);
     }
 }
