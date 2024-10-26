4f3b8c4c9b052eb7f1f5b9995ad0eda92f56fa26
seoseunghyun1025 <seunghyun.seo101@gmail.com>
2024-10-26 21:58:54 +0900

    feat: 출력 기능 클래스

---
+++ src/main/java/InputOutput/Output.java
@@ -0,0 +1,10 @@
+package InputOutput;
+
+public class Output {
+    public static void firstPrintMessage(){
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+    }
+    public static void roundPrint(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+}
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import InputOutput.Output;
+
 public class Application {
 
     public static void main(String[] args) {
+        Output.firstPrintMessage();
+        Output.roundPrint();
     }
 }
