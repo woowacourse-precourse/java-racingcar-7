76d1bff512ae2113ad4202df272771f75cf69755
Wooseok Jang <wowseok2774@gmail.com>
2024-10-26 21:36:27 +0900

    refactor: MainController 의존성을 단순화
    CarController와 GameController로 의존성을 단순화

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,37 +1,20 @@
 package controller;
 
-import factory.CarFactory;
 import java.util.List;
 import model.Car;
-import view.InputView;
-import view.OutputView;
 
 public class MainController {
-    private final InputView inputView;
-    private final OutputView outputView;
-    private final CarFactory carFactory;
-    private final RacingCarService racingCarService;
+    private final CarController carController;
+    private final GameController gameController;
 
-    public MainController(InputView inputView, OutputView outputView, CarFactory carFactory,
-                          RacingCarService racingCarService) {
-        this.inputView = inputView;
-        this.outputView = outputView;
-        this.carFactory = carFactory;
-        this.racingCarService = racingCarService;
+    public MainController(CarController carController, GameController gameController) {
+        this.carController = carController;
+        this.gameController = gameController;
     }
 
     public void run() {
-        // 흐름 제어 로직
-        outputView.printGuide();
-        List<String> carNames = inputView.getCarNames();
-        outputView.printAttemptPrompt();
-        int attempt = inputView.getAttemptCount();
-        List<Car> cars = carFactory.createCars(carNames);
-
-        racingCarService.gameStart(cars, attempt);
-        List<String> winners = outputView.findFinalWinners(cars);
-        outputView.printFinalWinner(winners);
-        // 서비스 계층에 로직 위임
-
+        List<Car> cars = carController.getCarsFromUserInput(); // 자동차들 입력받기
+        int attempts = carController.getAttemptCount(); // 시도횟수 입력받기
+        gameController.startGame(cars, attempts); // 게임 실행하기
     }
 }
