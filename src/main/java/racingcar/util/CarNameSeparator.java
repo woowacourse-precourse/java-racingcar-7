f07c63fd11e4ed09748ad2f1af321aefb6a7ee4f
world-dv <goodforx0147@naver.com>
2024-10-26 19:43:25 +0900

    feat: split의 limit 상수 처리
    - 가독성을 높이기 위해 구분자의 limit를 상수 처리합니다.

--- src/main/java/racingcar/util/CarNameSeparator.java
+++ src/main/java/racingcar/util/CarNameSeparator.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 public class CarNameSeparator {
     private static final String CAR_NAME_SEPARATOR = ",";
+    private static final Integer EMPTY_CAR_NAME_HANDLER = -1;
 
     public static List<String> splitCarNameWithSeparator(String carNames) {
-        return List.of(carNames.split(CAR_NAME_SEPARATOR, -1));
+        return List.of(carNames.split(CAR_NAME_SEPARATOR, EMPTY_CAR_NAME_HANDLER));
     }
 
     public static List<String> splitCarNameWithoutSpace(String carNamesWithSpace) {
