8f000ed0675c51a0df5de38fd984ad8f7d667a7e
Yewon Yoo <yooyy0106@naver.com>
2024-10-27 02:51:29 +0900

    feat(racing): 자동차 전진,멈춤 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import java.util.ArrayList;
 // 각 자동차 입력받기
@@ -38,12 +39,47 @@ public class Application {
         return moveCount;
     }
 
+    private static void startRace(List<String> carNames, int moveCount) {
+        System.out.println("실행 결과");
+
+        List<Integer> positions = new ArrayList<>();
+        for (int i = 0; i < carNames.size(); i++) {
+            positions.add(0); // 각 자동차 초기 위치 설정
+        }
+
+        for (int i = 0; i < moveCount; i++) {
+            updatePositions(positions);
+            printRaceStatus(carNames, positions);
+        }
+    }
+
+    private static void updatePositions(List<Integer> positions) {
+
+        for (int j = 0; j < positions.size(); j++) {
+            if (shouldMove()) {
+                positions.set(j, positions.get(j) + 1);
+            }
+        }
+    }
+
+    private static boolean shouldMove() {
+        int randomNumber = Randoms.pickNumberInRange(0,9);
+        return randomNumber >= 4;
+    }
+
+    private static void printRaceStatus(List<String> carNames, List<Integer> positions) {
+        for (int i = 0; i < carNames.size(); i++) {
+            System.out.println(carNames.get(i) + " : ");
+            System.out.println("-".repeat(positions.get(i)));
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         // 자동차 입력받기
         List<String> carNames = registerCars();
-        System.out.println(carNames);
         // 횟수 입력받기
         int moveCount = getMoveCount();
-        System.out.println(moveCount);
+        startRace(carNames, moveCount);
     }
 }
