b975cf6fef4a926fe7b7dd61c599216da4054784
Shinjongyun <tigerjong123@gmail.com>
2024-10-27 04:34:39 +0900

    feat: WinnerCalculator 객체 추가

---
+++ src/main/java/racingcar/service/WinnerCalculator.java
@@ -0,0 +1,31 @@
+package racingcar.service;
+
+import racingcar.entity.Car;
+import racingcar.entity.Winner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinnerCalculator {
+
+    public Winner calculateWinner(List<Car> carList) {
+        int maxLength = 0;
+        List<Car> winners = new ArrayList<>();
+
+        for (Car car : carList) {
+            int roadLength = car.getRoad().getLength();
+
+            // 새로운 최대 길이를 찾은 경우
+            if (roadLength > maxLength) {
+                maxLength = roadLength;
+                winners.clear();
+                winners.add(car);
+            } else if (roadLength == maxLength) {
+                winners.add(car); // 현재 길이가 최대와 같다면 우승자 리스트에 추가
+            }
+        }
+
+        return new Winner(winners);
+    }
+}
+
--- src/main/java/racingcar/service/WinnerPrinter.java
+++ src/main/java/racingcar/service/WinnerPrinter.java
@@ -10,15 +10,21 @@ public class WinnerPrinter {
     public void printWinner(List<Car> carList) {
 
         StringBuilder winnerNames = new StringBuilder();
+        WinnerCalculator winnerCalculator = new WinnerCalculator();
+
         Winner winner = winnerCalculator.calculateWinner(carList);
 
         System.out.print("최종 우승자 : ");
         for (int i = 0; i < winner.getWinningCars().size(); i++) {
             winnerNames.append(winner.getWinningCars().get(i).getName());
-            if (i < winner.getWinningCars().size() - 1) { // 마지막 이름이 아닐 경우에만 쉼표 추가
+            if (isMoreWinner(i, winner)) { // 마지막 이름이 아닐 경우에만 쉼표 추가
                 winnerNames.append(", ");
             }
         }
         System.out.println(winnerNames);
+    }
+
+    private boolean isMoreWinner(int i, Winner winner) {
+        return i < winner.getWinningCars().size() - 1;
     }
 }
