97b1ca7c9965c7d226bd82f44efa16a7904e6f49
Avocado <ujubongbong@gmail.com>
2024-10-27 01:20:39 +0900

    :sparkles: feat: main 함수 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = new Input();
+        Output output = new Output();
+        Racing racing = new Racing();
+
+        output.printInit();
+        List<Car> cars = input.getCarName();
+        output.printAttemptCountMessage();
+        int attemptCount = input.getAttemptCount();
+
+        racing.play(cars, attemptCount);
+
+        List<String> winners = racing.getWinner(cars);
+        output.printWinner(winners);
     }
 }
