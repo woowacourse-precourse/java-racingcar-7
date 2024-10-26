8225a33ef5753d2bd85d37290e99d7d0bd2329f6
SeoMoonk <sunghoon1216@naver.com>
2024-10-27 04:51:02 +0900

    feat: inputView에서 사용자로부터 주행 횟수를 입력받음
    주행 횟수 입력 안내 문구를 출력하고, 횟수를 입력받음

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,14 +1,19 @@
 package racingcar.view;
 
 import static racingcar.constant.RacingCarInfoMsg.TYPE_CAR_NAMES;
+import static racingcar.constant.RacingCarInfoMsg.TYPE_RACE_COUNT;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    
-    //TODO: 사용자로부터의 입력을 담당하는 클래스
+
     public String inputCarNames() {
         System.out.println(TYPE_CAR_NAMES.getMsg());
+        return Console.readLine();
+    }
+
+    public String inputRaceCount() {
+        System.out.println(TYPE_RACE_COUNT.getMsg());
         return Console.readLine();
     }
 }
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static racingcar.constant.RacingCarInfoMsg.TYPE_CAR_NAMES;
+import static racingcar.constant.RacingCarInfoMsg.TYPE_RACE_COUNT;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.io.ByteArrayInputStream;
@@ -42,6 +43,37 @@ class InputViewTest extends NsTest {
 
         //then
         assertThat(result).isEqualTo("test"); // 입력받은 문자열에서 개행 제거
+    }
+
+    @Test
+    @DisplayName("경주 진행 횟수의 입력을 받을 수 있게 안내 문구가 출력된다.")
+    void t003() {
+        //given
+        String testInput = "4";
+        InputStream in = new ByteArrayInputStream(testInput.getBytes());
+        System.setIn(in);
+
+        //when
+        inputView.inputRaceCount();
+
+        //then
+        assertThat(output()).contains(TYPE_RACE_COUNT.getMsg());
+
+    }
+
+    @Test
+    @DisplayName("사용자로부터 경주 진행 횟수를 입력받는다")
+    void t004() {
+        //given
+        String testInput = "4";
+        InputStream in = new ByteArrayInputStream(testInput.getBytes());
+        System.setIn(in);
+
+        //when
+        String result = inputView.inputRaceCount();
+
+        //then
+        assertThat(result).isEqualTo(testInput);
     }
 
 
