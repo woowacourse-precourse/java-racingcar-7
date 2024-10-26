42468db437638fe30f06e46342bd6d4b1abbda1e
coolman0123 <socool4m@naver.com>
2024-10-26 20:15:52 +0900

    refactor: controller에서 view와 util 중개
    * RacingGameController.java

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -2,15 +2,22 @@ package racingcar.controller;
 
 import java.util.List;
 import racingcar.model.Car;
+import racingcar.util.CarInputConverter;
+import racingcar.util.RaceInputConverter;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingGameController {
     public static void run() {
+        //참가자 설정
         OutputView.requestCarName();
-        List<Car> raceParticipants = InputView.getCarNames();
+        String[] carNames = InputView.getCarNames();
+        List<Car> raceParticipants = CarInputConverter.setCarList(carNames);
+
+        //라운드 설정
         OutputView.requestRound();
-        int round = InputView.getRound();
+        String inputRound = InputView.getRound();
+        int round = RaceInputConverter.setIntRound(inputRound);
 
 
     }
