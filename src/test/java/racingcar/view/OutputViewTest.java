5392789a7063753053a11794b2b2cb67a7f694a1
차왕준 <ckdhdkwns@gmail.com>
2024-10-27 05:38:31 +0900

    test(OutputViewTest): 승자 출력 테스트

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -25,4 +25,19 @@ public class OutputViewTest {
 
         assertThat(outputView.generateProgress()).isEqualTo("pobi : -----\nwoni : ");
     }
+
+    @Test
+    void 승자_출력() {
+        Car car1 = new TestCar("pobi", true);
+        Car car2 = new TestCar("woni", false);
+        Car car3 = new TestCar("jun", true);
+
+        Cars cars = new Cars(Arrays.asList(car1, car2, car3));
+        OutputView outputView = new OutputView(cars);
+
+        int moveCount = 5;
+        IntStream.range(0, moveCount).forEach(i -> cars.moveAll());
+
+        assertThat(outputView.generateWinners()).isEqualTo("최종 우승자 : pobi, jun");
+    }
 }
