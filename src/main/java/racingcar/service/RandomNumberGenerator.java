a3cc6925709f8df90a5028a4ef5e21f41ad471e8
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-26 18:16:24 +0900

    feat(service): RandomNumberGenerator 클래스 생성 및 난수 생성 기능 추가 - MIN_NUMBER와 MAX_NUMBER 범위 내에서 난수를 생성하는 gernerateRandomNumber 메서드 구현

--- src/main/java/racingcar/service/RandomNumberGenerator.java
+++ src/main/java/racingcar/service/RandomNumberGenerator.java
@@ -1,2 +1,12 @@
-package racingcar.service;public class RandomNumberGenerator {
+package racingcar.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+    private static final int MAX_NUMBER = 9;
+    private static final int MIN_NUMBER = 0;
+
+    public int gernerateRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    }
 }
