203e5927693ec8dfd563365fc17e27e5a940162e
MinJae00 <exceedmin@gmail.com>
2024-10-27 01:49:14 +0900

    refact: 정확한 역할 의미 표현을 위해 클래스 명은 명사 형으로, 메소드 이름은 동사형으로 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,30 +1,30 @@
 package racingcar.view;
 
 import racingcar.user.SettingGameUser;
-import racingcar.user.UserInfo;
+import racingcar.user.UserObject;
 
 public class OutputView {
-    public void ShowResult() {
-        for (UserInfo user : SettingGameUser.user) {
+    public void printTurnResult() {
+        for (UserObject user : SettingGameUser.user) {
             System.out.println(user.name + ":" + user.score);
         }
         System.out.println("");
     }
 
-    public void PrintResultSentence() {
+    public void printResultSentence() {
         System.out.println("");
         System.out.println("실행 결과");
     }
-    
-    public void PrintWinnerSentence() {
+
+    public void printWinnerSentence() {
         System.out.print("최종 우승자 : ");
     }
 
-    public void PrintWinner(String s) {
+    public void printWinner(String s) {
         System.out.print(s);
     }
 
-    public void PrintRest() {
+    public void printRest() {
         System.out.print(" ,");
     }
 }
