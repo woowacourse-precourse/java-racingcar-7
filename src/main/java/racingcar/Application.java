b60b3cba5d8e33617e5f0df8bee4c7d75cf2b173
kmc198989 <kmc198989@naver.com>
2024-10-26 18:34:11 +0900

    feat: 자동차 이름 입력 및 검증 기능 구현
    - 자동차 이름을 쉼표로 분리하여 입력받음
    - 각 이름이 5자 이하인지 검증, 조건에 맞지 않으면 IllegalArgumentException 발생

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String input = Console.readLine();
+            List<String> carNames = Arrays.asList(input.split(","));
+            validateCarNames(carNames);
+        } catch (IllegalArgumentException e) {
+            System.out.println("오류: " + e.getMessage());
+        }
+    }
+
+    private static void validateCarNames(List<String> carNames) {
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
     }
 }
