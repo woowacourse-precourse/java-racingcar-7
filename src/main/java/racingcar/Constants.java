65891c64ce0d209037db3236abee93d4bb4130ac
kssk3 <kss8014@daum.net>
2024-10-26 14:44:00 +0900

    Constants클래스 상수

--- src/main/java/racingcar/Constants.java
+++ src/main/java/racingcar/Constants.java
@@ -3,8 +3,6 @@ package racingcar;
 
 public class Constants {
 
-
-    static final int FORWARD_MIN = 4;
-    static final int FORWARD_MAX = 10;
-    static final int CAR_NAME_LENGTH_MAX = 5;
+    private static final int FORWARD_MIN = 4;
+    private static final int FORWARD_MAX = 10;
 }
