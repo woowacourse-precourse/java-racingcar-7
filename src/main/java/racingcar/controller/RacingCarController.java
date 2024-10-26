8c5cbaa0a6f894395e74852d65905c834fba7d66
dltmddus <tmddus4671@gmail.com>
2024-10-26 18:47:18 +0900

    feat: valid한 시도할 횟수 저장하기

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -2,16 +2,18 @@ package racingcar.controller;
 
 
 import static racingcar.view.InputView.UserInput;
+import static racingcar.view.OutputView.printAttemptCountInputMessage;
 import static racingcar.view.OutputView.printCarNamesInputMessage;
 
 import racingcar.domain.CarGenerator;
 import racingcar.domain.Cars;
+import racingcar.validation.AttemptCountValidation;
 import racingcar.validation.CarNameValidation;
 
 public class RacingCarController {
     public void run() {
-
         Cars cars = getCars();
+        int attemptCount = getAttemptCount();
 
     }
 
@@ -22,4 +24,10 @@ public class RacingCarController {
         return new Cars(CarGenerator.getParticipateCars(carNames));
     }
 
+    private int getAttemptCount() {
+        printAttemptCountInputMessage();
+        String attemptCount = UserInput();
+        AttemptCountValidation.validate(attemptCount);
+        return Integer.parseInt(attemptCount);
+    }
 }
