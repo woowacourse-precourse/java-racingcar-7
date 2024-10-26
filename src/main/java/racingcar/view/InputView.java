814afea38393fde1a3b03eaecdf13e1051cd55ff
YuyoungRhee <bbclara@naver.com>
2024-10-27 03:55:24 +0900

    refactor(InputView): 입력 구분자를 매직 넘버에서 상수로 변경
    - 입력 구분자 ","를 DELIMITER 상수로 선언하여 구분자의 의미를 명확히 함
    - input을 구분자로 split할 때 의미를 정확히 알 수 있도록 함

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
+    private static final String DELIMITER = ",";
 
     public static List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -16,7 +17,7 @@ public class InputView {
     }
 
     private static List<String> parseCarNames(String input) {
-        return Arrays.asList(input.split(","));
+        return Arrays.asList(input.split(DELIMITER));
     }
 
     public static Integer getTotalRounds() {
