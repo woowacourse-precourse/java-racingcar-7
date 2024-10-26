319e47d9ae9e6c85128a358a82b81f56504ff848
yeo-li <81564503+yeo-li@users.noreply.github.com>
2024-10-27 00:14:00 +0900

    fix(RepeatCountValidator): 예외상황 추가

--- src/main/java/validator/RepeatCountValidator.java
+++ src/main/java/validator/RepeatCountValidator.java
@@ -5,6 +5,7 @@ public class RepeatCountValidator {
     public static void validateInputRepeatCount(String inputRepeatCount) {
         isNotEmpty(inputRepeatCount);
         isNotBigNumber(inputRepeatCount);
+        isNotChar(inputRepeatCount);
         isNotNegativeNumber(Integer.parseInt(inputRepeatCount));
     }
 
@@ -18,15 +19,34 @@ public class RepeatCountValidator {
         try {
             Integer.parseInt(inputRepeatCount);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("반복횟수가 너무 큽니다.");
+            if (isNumber(inputRepeatCount)) {
+                throw new IllegalArgumentException("반복횟수가 너무 큽니다.");
+            }
         }
     }
 
+    private static void isNotChar(String inputRepeatCount) {
+        try {
+            Integer.parseInt(inputRepeatCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("반복 횟수에 숫자가 아닌 문자가 입력 됐습니다.");
+        }
+    }
 
     private static void isNotNegativeNumber(int repeatCount) {
         if (repeatCount < 0) {
             throw new IllegalArgumentException("반복 횟수는 0 이상의 정수 입니다.");
         }
+    }
+
+
+    private static boolean isNumber(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
