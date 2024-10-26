bd94d0fa1916b14e6edf5af11cd9fd6102d5da3c
honggyu1233@naver.com <honggyu1233@naver.com>
2024-10-26 22:09:25 +0900

    feat : RacingGame 클래스 추가 - Racingcar 이동 및 현재 위치 반환 기능 구현

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -1,4 +1,40 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class RacingGame {
+    private List<Racingcar> racingcars;
+    private int moveCount;
+    private int moveForwardCondition=4;
+
+    public RacingGame(List<Racingcar> racingcars, int moveCount) {
+        this.racingcars = racingcars;
+        this.moveCount = moveCount;
+    }
+
+
+
+    public void MoveRacingcars() {
+       for(Racingcar racingcar : racingcars) {
+           int randomValue = Randoms.pickNumberInRange(0, 9);
+           if (randomValue >= moveForwardCondition) {
+               racingcar.moveRacingcar();
+           }
+       }
+    }
+
+    public List<String> getCarsLocation(){
+        List<String> carsLocation = new ArrayList<>();
+        for(Racingcar racingcar : racingcars) {
+            carsLocation.add(racingcar.getCarName() + " : " + "-".repeat(racingcar.getLocation()));
+        }
+        return carsLocation;
+    }
+
+
+
 }
