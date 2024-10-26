d38582e39634877df429a87ec35d797f43ed7abf
sinminseok12 <sinminseok12@naver.com>
2024-10-27 01:37:50 +0900

    feat : 레이싱 시도 횟수 초기값 검증 기능 구현
    시도 횟수 초기값이 0 이하일 경우, 게임 진행이 불가능하다. 이와 같은 경우 예외를 발생시킨다

--- src/main/java/racingcar/model/AttemptCount.java
+++ src/main/java/racingcar/model/AttemptCount.java
@@ -1,6 +1,9 @@
 package racingcar.model;
 
 public class AttemptCount {
+    private static final String ERROR_NEGATIVE_OR_ZERO_COUNT = "시도 횟수는 0 이상이어야 합니다.";
+    private static final int MIN_ATTEMPT_COUNT = 0;
+
     private int count;
 
     private AttemptCount(int count) {
@@ -8,7 +11,14 @@ public class AttemptCount {
     }
 
     public static AttemptCount from(int count) {
+        validateCountRange(count);
         return new AttemptCount(count);
+    }
+
+    private static void validateCountRange(int count) {
+        if (count < MIN_ATTEMPT_COUNT) {
+            throw new IllegalArgumentException(ERROR_NEGATIVE_OR_ZERO_COUNT);
+        }
     }
 
     public void decrease() {
