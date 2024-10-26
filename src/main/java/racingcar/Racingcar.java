1e66c5abdce847cea597d6678c9d947d33b2dfd3
jinwook <wjswlsdnr333@gmail.com>
2024-10-27 03:57:37 +0900

    docs(readme): 입력 예외사항 추가

--- src/main/java/racingcar/Racingcar.java
+++ src/main/java/racingcar/Racingcar.java
@@ -1,4 +1,5 @@
 package racingcar;
+
 import camp.nextstep.edu.missionutils.*;
 
 import java.util.Arrays;
@@ -9,13 +10,13 @@ public class Racingcar {
     private int count;
     private String[] moveResults;
 
-    public void start(){
+    public void start() {
         input();
         racingDisplay();
         winnerDisplay();
     }
 
-    public void input(){
+    public void input() {
         System.out.println("경주할 자동차의 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String rawCarName = Console.readLine();
         inputExCarName(rawCarName);
@@ -27,85 +28,78 @@ public class Racingcar {
 
     }
 
-    public boolean invCarNameLen(){
+    public boolean invCarNameLen() {
         for (String carName : carNames)
-            if(carName.length() >= 6) return true;
+            if (carName.length() >= 6) return true;
         return false;
     }
 
-    public boolean hasBlankCarName(){
+    public boolean hasBlankCarName() {
         for (String carName : carNames)
-            if(carName.contains(" ")) return true;
+            if (carName.contains(" ")) return true;
         return false;
     }
 
-    public void inputExCarName(String rawCarName){
-        if(rawCarName.isEmpty()) throw new IllegalArgumentException("자동차 입력이 없습니다.");
-        if(rawCarName.endsWith(",")) throw new IllegalArgumentException("자동차 입력시 ','로 끝날 수는 없습니다.");
+    public void inputExCarName(String rawCarName) {
+        if (rawCarName.isEmpty()) throw new IllegalArgumentException("자동차 입력이 없습니다.");
+        if (rawCarName.endsWith(",")) throw new IllegalArgumentException("자동차 입력시 ','로 끝날 수는 없습니다.");
         carNames = rawCarName.split(",");
-        if(carNames.length == 0) throw new IllegalArgumentException("쉼표만 입력될 수는 없습니다.");
-        if(carNames.length == 1) throw new IllegalArgumentException("자동차 1대로 경주를 진행 할 수 없습니다.");
-        if(invCarNameLen()) throw new IllegalArgumentException("자동차의 이름은 5자 이하만 가능합니다.");
-        if(hasBlankCarName()) throw new IllegalArgumentException("자동차 이름에 공백이 포함될 수는 없습니다.");
+        if (carNames.length == 0) throw new IllegalArgumentException("쉼표만 입력될 수는 없습니다.");
+        if (carNames.length == 1) throw new IllegalArgumentException("자동차 1대로 경주를 진행 할 수 없습니다.");
+        if (invCarNameLen()) throw new IllegalArgumentException("자동차의 이름은 5자 이하만 가능합니다.");
+        if (hasBlankCarName()) throw new IllegalArgumentException("자동차 이름에 공백이 포함될 수는 없습니다.");
     }
 
-    public void inputExCount(String count){
-        if(count.isEmpty()) throw new IllegalArgumentException("시도 횟수 입력이 없습니다.");
-        if(!count.matches("\\d+")) throw new IllegalArgumentException("공백이 포함될 수 없고 하나 이상의 정수만 입력할 수 있습니다.");
+    public void inputExCount(String count) {
+        if (count.isEmpty()) throw new IllegalArgumentException("시도 횟수 입력이 없습니다.");
+        if (!count.matches("\\d+")) throw new IllegalArgumentException("공백이 포함될 수 없고 하나 이상의 정수만 입력할 수 있습니다.");
         this.count = Integer.parseInt(count);
-        if(this.count == 0) throw new IllegalArgumentException("시도 횟수가 0이 될 수는 없습니다.");
+        if (this.count == 0) throw new IllegalArgumentException("시도 횟수가 0이 될 수는 없습니다.");
     }
 
-    public void initMoveResults(){
+    public void initMoveResults() {
         moveResults = new String[carNames.length];
         Arrays.fill(moveResults, "");
     }
 
 
-    public void moveForward(int carNameIndex){
-        if(4 <= Randoms.pickNumberInRange(0, 9)) moveResults[carNameIndex] +="-";
+    public void moveForward(int carNameIndex) {
+        if (4 <= Randoms.pickNumberInRange(0, 9)) moveResults[carNameIndex] += "-";
     }
 
-    public void racingDisplay(){
+    public void racingDisplay() {
         initMoveResults();
-        for(int i = 0; i < count; i++){
-            for(int j = 0; j < carNames.length; j++){
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < carNames.length; j++) {
                 moveForward(j);
-                System.out.println(carNames[j] + " : "+ moveResults[j]);
+                System.out.println(carNames[j] + " : " + moveResults[j]);
             }
             System.out.println();
         }
     }
 
-    public int findMaxDistance(){
+    public int findMaxDistance() {
         int maxDistance = 0;
         for (String result : moveResults) {
-            if (result.length() > maxDistance) {
-                maxDistance = result.length();
-            }
+            if (result.length() > maxDistance) maxDistance = result.length();
         }
         return maxDistance;
     }
 
-    public void addWinner(StringBuilder winners, String carName){
-        if (!winners.isEmpty()) {
-            winners.append(", ");
-        }
+    public void addWinner(StringBuilder winners, String carName) {
+        if (!winners.isEmpty()) winners.append(", ");
         winners.append(carName);
     }
 
-    public void winnerDisplay(){
+    public void winnerDisplay() {
         int maxDistance = findMaxDistance();
         StringBuilder winners = new StringBuilder();
 
         for (int i = 0; i < moveResults.length; i++) {
-            if (moveResults[i].length() == maxDistance) {
-                addWinner(winners,carNames[i]);
-            }
+            if (moveResults[i].length() == maxDistance) addWinner(winners, carNames[i]);
         }
 
         System.out.println("최종 우승자 : " + winners);
     }
-
 
 }
