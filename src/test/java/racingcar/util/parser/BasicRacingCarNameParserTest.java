8258e413bb4b9ae442ec32e7dc964ecf9d6daa9d
changuii <rhljh7410@gmail.com>
2024-10-26 11:20:28 +0900

    test(BasicRacingCarNameParserTest) : "2. 자동차들의 이름을 분리한다."를 테스트해라

---
+++ src/test/java/racingcar/util/parser/BasicRacingCarNameParserTest.java
@@ -0,0 +1,33 @@
+package racingcar.util.parser;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class BasicRacingCarNameParserTest {
+
+    private final BasicRacingCarNameParser basicRacingCarNameParser;
+
+    public BasicRacingCarNameParserTest(){
+        basicRacingCarNameParser = new BasicRacingCarNameParser();
+    }
+
+    @Test
+    void 자동차_이름_분리_테스트(){
+        assertSimpleTest(() -> {
+            // given
+            String names = "창의, 지후, 보성";
+            List<String> nameList = List.of("창의", "지후", "보성");
+
+            // when
+            List<String> result = basicRacingCarNameParser.parseNamesStringToList(names);
+
+            // then
+            assertThat(result).usingRecursiveComparison().isEqualTo(nameList);
+
+        });
+    }
+
+}
