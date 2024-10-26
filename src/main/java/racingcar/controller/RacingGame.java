440c09c9880e42cbdc31bdcbb27c63d6b3809d96
Lee-Minju <mju7513@gmail.com>
2024-10-26 18:55:48 +0900

    controller\RacingGame.java: 필요없는 import 제거

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,16 +1,12 @@
 package racingcar.controller;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.StringTokenizer;
-import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.domain.Rounds;
 import racingcar.view.InputView;
 
 public class RacingGame {
-
     InputView inputView = new InputView();
 
     public void doGame() {
