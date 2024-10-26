1f54dd8d49957ff8a772f2bf346f51c652a93a59
lsj0822 <lsj0822@khu.ac.kr>
2024-10-26 20:11:52 +0900

    test : 테스트 수정
    - 패키지 별로 관리하던 테스트를 클래스별로 구분
    - LetterCounterTest 테스트케이스 추가

--- src/test/java/utils/ContentParserTest.java
+++ src/test/java/utils/ContentParserTest.java
@@ -1,10 +1,8 @@
 package utils;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import inspector.InspectName;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.Application;
@@ -84,7 +82,6 @@ public class ContentParserTest extends NsTest {
                     assertThat(group[2]).isEqualTo("😥");
                 });
     }
-
 
     @Override
     public void runMain() {
--- src/test/java/utils/LetterCounterTest.java
+++ src/test/java/utils/LetterCounterTest.java
@@ -1,25 +1,22 @@
-package inspector;
+package utils;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static utils.LetterCounter.letterCounter;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.Application;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-class InspectorTest extends NsTest {
-
+public class LetterCounterTest extends NsTest {
     private static final String FAMILY_EMOJI = "\uD83D\uDC68\u200D\uD83D\uDC69\u200D\uD83D\uDC67\u200D\uD83D\uDC66";
-
-    InspectName inspectName = new InspectName();
 
     @DisplayName("일반적인_입력_테스트_영어")
     @Test
     void normal_input_test_english(){
         assertSimpleTest(() ->
-                assertThat(inspectName.letterCounter("english")).isEqualTo("english".length())
+                assertThat(letterCounter("english")).isEqualTo("english".length())
         );
     }
 
@@ -27,7 +24,7 @@ class InspectorTest extends NsTest {
     @Test
     void normal_input_test_korean(){
         assertSimpleTest(() ->
-                assertThat(inspectName.letterCounter("한글")).isEqualTo("한글".length())
+                assertThat(letterCounter("한글")).isEqualTo("한글".length())
         );
     }
 
@@ -43,8 +40,16 @@ class InspectorTest extends NsTest {
     @Test
     void utf8mb4_letter_counting(){
         assertSimpleTest(() ->
-                    assertThat(inspectName.letterCounter(FAMILY_EMOJI)).isEqualTo(1)
-            );
+                assertThat(letterCounter(FAMILY_EMOJI)).isEqualTo(1)
+        );
+    }
+
+    @DisplayName("utf8mb4_utf18_혼용_글자_수_테스트")
+    @Test
+    void mixed_unicode_letter_counting(){
+        assertSimpleTest(() ->
+                assertThat(letterCounter(FAMILY_EMOJI+"한글eng")).isEqualTo(6)
+        );
     }
 
     @Override
