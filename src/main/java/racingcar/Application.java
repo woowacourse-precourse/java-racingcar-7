bef2059deb6a9f51c88fa5ed68f880e2e4324498
patternplane <bss011202@naver.com>
2024-10-26 23:24:40 +0900

    refactor: 사용자 입력을 받는 기능을 메소드로 분리
    - 사유 : 이후 추가될 "시도할 횟수 입력" 기능도 고려할 때, 각 입력 기능을 별도로 관리하기 위함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        List<String> carNames = getCarNames();
+    }
+    
+    private static List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNamesStr = Console.readLine();
-        List<String> carNames = CarNameParser.parse(carNamesStr, 5);
+        return CarNameParser.parse(carNamesStr, 5);
     }
 }
