1ba0c922ee14befe087dd4932440b172060cb76c
kssk3 <kss8014@daum.net>
2024-10-26 16:19:31 +0900

    Controller클래스 추가

---
+++ src/main/java/racingcar/controller/Controller.java
@@ -0,0 +1,10 @@
+package racingcar.controller;
+
+import racingcar.GameService;
+import racingcar.repository.CarRepository;
+
+public class Controller {
+    private GameService gameService = new GameService();
+    private CarRepository carRepository = new CarRepository();
+
+}
