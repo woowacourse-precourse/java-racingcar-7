e6aa7297363bd9538e25715e81365e15c647d086
sodychoe <sodychoe@icloud.com>
2024-10-26 17:29:21 +0900

    feat : 전진 시도 횟수 입력 안내 메시지 출력 기능 구현

--- src/main/java/racingcar/io/InputConsoleHandler.java
+++ src/main/java/racingcar/io/InputConsoleHandler.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputConsoleHandler {
 
     public static final String CARNAME_INPUT_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String NUN_OF_TRIAL_GUIDE_MESSAGE = "시도할 횟수는 몇 회인가요?";
     public static final String INPUTS_SEPARATOR = ",";
 
     public void showCarNameGuideMessage() {
@@ -13,6 +14,10 @@ public class InputConsoleHandler {
 
     public String[] askCarName() {
         return Console.readLine().split(INPUTS_SEPARATOR);
+    }
+
+    public void showNumOfTrialGuideMessage() {
+        System.out.println(NUN_OF_TRIAL_GUIDE_MESSAGE);
     }
 
 }
