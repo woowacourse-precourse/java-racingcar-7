7f25e921fe9e14bc2c25823d8f96e2be1febaf02
zjhj0814 <zjhj0814@gmail.com>
2024-10-26 22:50:07 +0900

    feat(InputView): 시도 횟수 입력 받는 기능 작성
    시도 횟수를 입력 받고 InputViewValidator를 통해 입력이 빈 문자열이거나 공백이 아닌지, 숫자로만 이루어져 있는지 검증한 후, 입력을 반환한다

--- README.md
+++ README.md
@@ -47,7 +47,7 @@
 -[X] 자동차 이름 입력 받기
     - `경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)`
     - ` camp.nextstep.edu.missionutils.Console의 readLine()` 사용
--[ ] 시도할 횟수 입력 받기
+-[X] 시도할 횟수 입력 받기
     - `시도할 횟수는 몇 회인가요?`
     - ` camp.nextstep.edu.missionutils.Console의 readLine()` 사용
 
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,6 +5,7 @@ import racingcar.validator.InputViewValidator;
 
 public class InputView {
     private final String NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String TOTAL_ATTEMPTS_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private final InputViewValidator inputViewValidator = new InputViewValidator();
 
     public String getRacingCarNames() {
@@ -12,5 +13,13 @@ public class InputView {
         final String racingCarNames = Console.readLine();
         inputViewValidator.validateNotEmptyAndBlank(racingCarNames);
         return racingCarNames;
+    }
+
+    public String getTotalAttempts() {
+        System.out.println(TOTAL_ATTEMPTS_MESSAGE);
+        final String totalAttempts = Console.readLine();
+        inputViewValidator.validateNotEmptyAndBlank(totalAttempts);
+        inputViewValidator.validateNumeric(totalAttempts);
+        return totalAttempts;
     }
 }
