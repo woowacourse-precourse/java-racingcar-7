24978e9f632b5b21031c4a9f4818009f7164ffc8
taek2222 <oot309@daum.net>
2024-10-26 22:49:34 +0900

    feat(Cars): 자동차 묶음 관리 객체 정의
    - List로 컨트롤러에서 관리하던 형태를 객체로 분리해 정의했습니다.
    
    - 최대한, 컨트롤러의 담당 기능 역할을 옮겼습니다.
    
    - 역할이 집중으로 가독성이 저하되어, 리팩토링이 추가적으로 필요합니다.

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,60 @@
+package racingcar.domain;
+
+import static racingcar.util.InputValidator.validateNoDuplicates;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import racingcar.util.RandomUtil;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    public Cars(String inputCarNames) {
+        List<String> carList = splitCarNames(inputCarNames);
+        validateNoDuplicates(carList);
+
+        this.cars = createCars(carList);
+    }
+
+    public void playRound() {
+        cars.forEach(car ->
+                car.moveForward(RandomUtil.generateRandomNumber())
+        );
+    }
+
+    public List<String> getCarStates() {
+        List<String> states = new ArrayList<>();
+        cars.forEach(car -> states.add(car.generateCarState()));
+
+        return states;
+    }
+
+    public Car getManyMoveCar() {
+        return cars.stream()
+                .max(Car::compareDistance)
+                .orElse(cars.getFirst());
+    }
+
+    public List<Car> winnerCarplay(Car manyMoveCar) {
+        List<Car> winnerPlayers = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (car.compareDistance(manyMoveCar) == 0)
+                winnerPlayers.add(car);
+        }
+
+        return winnerPlayers;
+    }
+
+    private List<String> splitCarNames(String inputCarsName) {
+        return Arrays.asList(inputCarsName.split(","));
+    }
+
+    private List<Car> createCars(List<String> carList) {
+        return carList.stream()
+                .map(Car::new)
+                .toList();
+    }
+}
