320259f274d96079582d5651d77bdc69a3e09154
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-26 23:53:17 +0900

    feat(Subject): 옵저버 패턴의 Subject 인터페이스

--- src/main/java/racingcar/model/Subject.java
+++ src/main/java/racingcar/model/Subject.java
@@ -1,4 +1,11 @@
 package racingcar.model;
 
+import racingcar.observer.Observer;
+
 public interface Subject {
+    void registerObserver(Observer observer);
+
+    void unregisterObserver(Observer observer);
+
+    void notifyObservers();
 }
