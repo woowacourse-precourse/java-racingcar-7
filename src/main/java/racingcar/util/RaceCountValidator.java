abf0fde286ddb985cbb7b82eb6374ef671b94a53
Yunsu4 <ysa020428@gmail.com>
2024-10-26 23:43:44 +0900

    feat: verify that the number of races is valid

---
+++ src/main/java/racingcar/util/RaceCountValidator.java
@@ -0,0 +1,33 @@
+package racingcar.util;
+
+public class RaceCountValidator {
+
+    public static void validate(String raceCountStr) {
+        isEmpty(raceCountStr);
+        int raceCount = parseInt(raceCountStr);
+        validateRange(raceCount);
+
+    }
+
+    private static void isEmpty(String raceCount) {
+        if (raceCount == null || raceCount.trim().isEmpty()) {
+            throw new IllegalArgumentException("입력이 비어 있습니다.");
+        }
+    }
+
+    private static int parseInt(String raceCount) {
+        try {
+            return Integer.parseInt(raceCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("경주 횟수를 정수로 입력하세요.", e);
+        }
+    }
+
+    private static void validateRange(Integer raceCount) {
+        if (raceCount < 1) {
+            throw new IllegalArgumentException("경주 횟수는 한 번 이상 2147483647 이하 입니다.");
+        }
+    }
+
+
+}
