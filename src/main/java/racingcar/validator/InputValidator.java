6492b22015db17a717b914aae834db155e59042c
Chaewon <corndots@naver.com>
2024-10-27 07:51:03 +0900

    refactor(InputValidator): 메서드명 변경 및 메서드 분리
    자동차 이름의 유효성과 길이 조건에 대해 메서드 분리

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -5,12 +5,14 @@ import java.util.List;
 public class InputValidator {
     private static final int MAX_NAME_LENGTH = 5;
 
-    public static void isValidCarNameLength(List<String> carNames) {
-        for (String name : carNames) {
-            if (name.isEmpty() || name.length() > MAX_NAME_LENGTH) {
-                throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하로 입력해 주세요.");
-            }
+    public static void validateCarNames(List<String> carNames) {
+        if (carNames.stream().anyMatch(InputValidator::isInvalidName)) {
+            throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하로 입력해 주세요.");
         }
+    }
+
+    private static boolean isInvalidName(String name) {
+        return name.isEmpty() || name.length() > MAX_NAME_LENGTH;
     }
 
     public static void validateRounds(int rounds) {
