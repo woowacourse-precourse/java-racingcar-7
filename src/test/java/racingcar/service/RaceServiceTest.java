1f7c0dec482ed5f84e92dc4286e91a83fe05e4cb
slay <palgalow@gmail.com>
2024-10-26 19:18:09 +0900

    feat(RaceServiceTest): 자동차 이름을 쉼표 기준으로 분리하는 로직 테스트 기능 추가

---
+++ src/test/java/racingcar/service/RaceServiceTest.java
@@ -0,0 +1,27 @@
+package racingcar.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RaceServiceTest {
+
+    private RaceService raceService;
+
+    @BeforeEach
+    void setUp() {
+        this.raceService = new RaceService;
+    }
+
+    @DisplayName("자동차 이름을 쉼표 기준으로 분리하는 기능 테스트")
+    @Test
+    public void splitCarNamesTest() {
+        List<String> carNames = raceService.splitCarNames("pobi,won,juni");
+        assertEquals(carNames[0], "pobi");
+        assertEquals(carNames[1], "won");
+        assertEquals(carNames[2], "juni");
+    }
+}
