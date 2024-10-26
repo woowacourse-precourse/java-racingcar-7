e7a620b9c1d95f67db6c525b3189b54011bb1237
yush <yush2202@gmail.com>
2024-10-26 20:00:41 +0900

    test($ CarNameTest): 클래스 이름 변경에 따른 테스트명 변경
    CarRacer 클래스가 CarName으로 변경되면서 테스트명 변경

--- src/test/java/racingcar/model/CarNameTest.java
+++ src/test/java/racingcar/model/CarNameTest.java
@@ -7,13 +7,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.Application;
 
-public class CarRacerTest extends NsTest {
+public class CarNameTest extends NsTest {
 
     @ParameterizedTest
     @DisplayName("불가능한 레이서 이름을 확인합니다.")
     @ValueSource(strings = {"", " ", "김수한무와거북이", "반갑습니다람쥐", "하하하하하하"})
     void carRacer_테스트(String input) {
-        Assertions.assertThatThrownBy(() -> new CarRacer(input))
+        Assertions.assertThatThrownBy(() -> new CarName(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
