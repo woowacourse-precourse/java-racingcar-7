43157285061adf46835583aaa1ac5a3e01d32308
bronzehyeon <bronzehyeon@gmail.com>
2024-10-26 19:30:12 +0900

    feat : 차 이름 입력기능 구현
    - Input 클래스 생성
    - Input.readCarNames 메서드 생성
    - Application 클래스에 입력안내 문구 출력 및 메서드 호출 구문 작성

---
+++ src/main/java/racingcar/Input.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public static String[] readCarNames(){
+        String input = Console.readLine();
+
+        String[] carNames = input.split(",");
+
+        for(String carName : carNames){
+            if(carName.length() > 5){
+                throw new IllegalArgumentException("차 이름이 너무 깁니다.");
+            }
+        }
+        if(input.charAt(input.length() - 1) == ','){
+            throw new IllegalArgumentException("쉼표는 구분자로만 사용해야 합니다.");
+        }
+
+        return carNames;
+    }
+}
