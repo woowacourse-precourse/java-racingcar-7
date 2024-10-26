8dc0c22875b28338c799541deab05dff6f5763b8
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-10-26 14:49:53 +0900

    feat(view): 라운드 시작 전 출력 뷰 생성
    - 라운드 시작 전 문구를 출력하는 뷰
    - 라운드 구분은 줄바꿈을 해줘야함

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,9 +6,13 @@ import racingcar.model.Car;
 public class OutputView {
     private OutputView() {
     }
+    public static void outputBeforeRound() {
+        System.out.println("\n실행 결과");
+    }
     public static void outputEachRound(CarsDto carsDto) {
         for (Car car : carsDto.getCars()) {
             System.out.println(car.getName()+" : "+"-".repeat(car.getWinStack()));
         }
+        System.out.println();
     }
 }
