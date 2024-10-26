591076bf191babb40c219fa1137213a78afa3088
Ahnsukjoo <bos98@naver.com>
2024-10-26 15:18:30 +0900

    feat: 우승자 판별 기능 구현
    - 최종 위치를 비교하여 우승 자동차를 판별하는 기능 구현
    - 해시맵 안에서 최대값을 구해서 최대값과 같은 자동차들만 리스트에 넣어서 반환 해주는 기능 구현

--- src/main/java/racingcar/model/Winner.java
+++ src/main/java/racingcar/model/Winner.java
@@ -1,11 +1,20 @@
 package racingcar.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Winner {
 
     public List<String> informWho(HashMap<String, Integer> raceStatus) {
-
+        List<String> winners = new ArrayList<>();
+        int maxValue = raceStatus.values().stream().mapToInt(Integer::intValue).max()
+            .orElse(Integer.MIN_VALUE);
+        for (String key : raceStatus.keySet()) {
+            if (maxValue == raceStatus.get(key)) {
+                winners.add(key);
+            }
+        }
+        return winners;
     }
 }
