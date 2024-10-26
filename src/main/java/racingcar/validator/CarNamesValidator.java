4013a7326b01a328974515b569a22a34120e911f
seoyeon2001 <syeon1250@naver.com>
2024-10-26 17:12:51 +0900

    Fix: 자동차 이름에 대한 예외 처리

--- src/main/java/racingcar/validator/CarNamesValidator.java
+++ src/main/java/racingcar/validator/CarNamesValidator.java
@@ -1,12 +1,20 @@
 package racingcar.validator;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 public class CarNamesValidator {
 
-    public static String[] validate(String[] input) {
+    public static List<String> validate(List<String> input) {
 
-        if (input.length < 2) {
+        // 앞 뒤 공백 제거
+        input = input.stream()
+                .map(String::trim)
+                .collect(Collectors.toList());
+
+        if (input.size() < 2) {
             throw new IllegalArgumentException("경주 자동차는 두대 이상이어야 합니다.");
         }
 
@@ -16,19 +24,18 @@ public class CarNamesValidator {
         // 자동차 이름 중복 확인
         validateNameDuplication(input);
 
-
         return input;
     }
 
-    private static void validateNameLength(String[] input) {
-        for (String name:input) {
-            if (name.length() < 1 || name.length() > 5) {
-                throw new IllegalArgumentException(("자동차 이름은 1자 이상, 5자 이하만 가능합니다."));
+    private static void validateNameLength(List<String> input) {
+        for (String name : input) {
+            if (name.isEmpty() || name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 1자 이상, 5자 이하만 가능합니다.");
             }
         }
     }
 
-    private static void validateNameDuplication(String[] input) {
+    private static void validateNameDuplication(List<String> input) {
         Set<String> nameSet = new HashSet<>();
         for (String name : input) {
             if (!nameSet.add(name)) {
