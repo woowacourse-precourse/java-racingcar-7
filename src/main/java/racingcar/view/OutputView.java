0c2163608e71dc7170a532a2387e312c3663cd51
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-27 04:44:32 +0900

    feat: 출력을 담당하는 OutputView 클래스 구현
    차수별 실행 결과와 우승자 안내 문구 등의 출력을 담당하는 OutputView 클래스를 구현하였다.

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+public class OutputView {
+    private static OutputView instance;
+
+    private OutputView() {
+    }
+
+    public static OutputView getInstance() {
+        if (instance == null) {
+            instance = new OutputView();
+        }
+        return instance;
+    }
+}
