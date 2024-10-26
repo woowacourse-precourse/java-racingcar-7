038b753a58589670d0e2bd2ec599b633a0a1c544
JongKyeong Kim <oo7bangjk@naver.com>
2024-10-26 17:34:58 +0900

    feat: 시도할 횟수를 입력할 수 있어야 한다.

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -12,4 +12,13 @@ public class Input {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Arrays.asList(readLine().split(delimiter));
     }
+
+    public int readTryNumber() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        try {
+            return Integer.parseInt(readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
--- src/test/java/racingcar/view/InputTest.java
+++ src/test/java/racingcar/view/InputTest.java
@@ -2,11 +2,13 @@ package racingcar.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.io.ByteArrayInputStream;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
 
 public class InputTest extends NsTest {
     @Test
@@ -17,6 +19,16 @@ public class InputTest extends NsTest {
         List<String> result = new Input().readCarName();
 
         assertEquals(List.of("김종경", "김철수", "김기명"), result);
+    }
+
+    @Test
+    void 시도할_횟수를_입력할_수_있어야_한다() {
+        String input = "5\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        int result = new Input().readTryNumber();
+
+        assertEquals(5, result);
     }
 
     @Override
