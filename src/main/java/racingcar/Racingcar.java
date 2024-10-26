5b5b4ccd61d74a8c1c5d2e5564869bfc7fb7ab6d
jinwook <wjswlsdnr333@gmail.com>
2024-10-26 23:34:57 +0900

    feat(Racingcar): 입력 예외 기능 구현

--- src/main/java/racingcar/Racingcar.java
+++ src/main/java/racingcar/Racingcar.java
@@ -13,13 +13,42 @@ public class Racingcar {
     public void input(){
         System.out.println("경주할 자동차의 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String rawCarName = Console.readLine();
-        carNames = rawCarName.split(",");
+        inputExCarName(rawCarName);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String count = Console.readLine();
+        inputExCount(count);
+
+
+    }
+
+    public boolean invCarNameLen(){
+        for (String carName : carNames)
+            if(carName.length() >= 6) return true;
+        return false;
+    }
+
+    public boolean hasBlankCarName(){
+        for (String carName : carNames)
+            if(carName.contains(" ")) return true;
+        return false;
+    }
+
+    public void inputExCarName(String rawCarName){
+        if(rawCarName.isEmpty()) throw new IllegalArgumentException("자동차 입력이 없습니다.");
+        if(rawCarName.endsWith(",")) throw new IllegalArgumentException("자동차 입력시 ','로 끝날 수는 없습니다.");
+        carNames = rawCarName.split(",");
+        if(carNames.length == 0) throw new IllegalArgumentException("쉼표만 입력될 수는 없습니다.");
+        if(carNames.length == 1) throw new IllegalArgumentException("자동차 1대로 경주를 진행 할 수 없습니다.");
+        if(invCarNameLen()) throw new IllegalArgumentException("자동차의 이름은 5자 이하만 가능합니다.");
+        if(hasBlankCarName()) throw new IllegalArgumentException("자동차 이름에 공백이 포함될 수는 없습니다.");
+    }
+
+    public void inputExCount(String count){
+        if(count.isEmpty()) throw new IllegalArgumentException("시도 횟수 입력이 없습니다.");
+        if(!count.matches("\\d+")) throw new IllegalArgumentException("공백이 포함될 수 없고 하나 이상의 정수만 입력할 수 있습니다.");
         this.count = Integer.parseInt(count);
-
-
+        if(this.count == 0) throw new IllegalArgumentException("시도 횟수가 0이 될 수는 없습니다.");
     }
 
 
