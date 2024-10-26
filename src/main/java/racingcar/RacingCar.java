db20e1df446dc4db21a198bc9cdfd737406984b3
hyunwon <khw73850@gmail.com>
2024-10-26 20:19:38 +0900

    feat: create class RacingCar

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RacingCar {
+    private String name;
+    private int moveCount;
+
+    public RacingCar(String name){
+        this.name = name;
+        this.moveCount = 0;
+    }
+}
