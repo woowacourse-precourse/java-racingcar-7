a2047683d57a0e61937177897ffc193a8019c850
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-26 21:06:14 +0900

    test(RacingCarListValidator): 리스트 검증 클래스 테스트
    테스트한 내용은 다음과 같습니다:
    - 리스트에 중복 요소가 존재하여 예외가 발생한다
    - 리스트의 사이즈가 최대 범위를 초과하여 예외가 발생한다

---
+++ src/test/java/racingcar/util/RacingCarListValidatorTest.java
@@ -0,0 +1,45 @@
+package racingcar.util;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RacingCarListValidatorTest {
+
+    private RacingCarListValidator racingCarListValidator;
+
+    @BeforeEach
+    void setUp() {
+        racingCarListValidator = new RacingCarListValidator();
+    }
+
+    @Test
+    @DisplayName("리스트에 중복 요소가 존재하여 예외가 발생한다.")
+    void validateDuplicateTest() throws Exception {
+        //given
+        final List<String> values = List.of("자동차", "자동차", "자동차");
+
+        //when
+        //then
+        assertThatThrownBy(() -> racingCarListValidator.validateDuplicate(values))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("리스트의 사이즈가 최대 범위를 초과하여 예외가 발생한다.")
+    void validateRangeTest() throws Exception {
+        //given
+        final int maxRange = 3;
+        final List<String> values = List.of("자동차1", "자동차2", "자동차3", "자동차4", "자동차5");
+
+        //when
+        //then
+        assertThatThrownBy(() -> racingCarListValidator.validateSize(values, maxRange))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+}
