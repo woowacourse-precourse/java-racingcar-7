cdedcff44ddffd3ecbe26e8d9a287256b80f424c
coolman0123 <socool4m@naver.com>
2024-10-26 20:27:52 +0900

    feat: 경기 실행 결과를 출력해라
    * OutputView.java

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,8 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.model.Car;
+
 public class OutputView {
     public static void requestCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -8,4 +11,13 @@ public class OutputView {
     public static void requestRound() {
         System.out.println("시도할 횟수는 몇 회인가요?");
     }
+
+    public static void printRoundResult(List<Car> raceParticipants) {
+        for (Car car : raceParticipants) {
+            System.out.println(car.getName() + " : " + car.getPosition());
+        }
+        System.out.println();
+    }
+
+
 }
