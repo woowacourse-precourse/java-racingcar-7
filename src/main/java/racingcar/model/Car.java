7d2d9afd9e83b3dade511192e59647240063bdad
junhaa <2171326@hansung.ac.kr>
2024-10-27 02:36:04 +0900

    feat(Car): 자동차 이동 기능 구현
    - 임의의 수를 받은 후, 숫자가 4이상인 경우 자동차가 이동하도록 기능 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,24 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private final String name;
+    private int position;
+    public Car(String name) {
+        this.name = name;
+        position = 0;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(){
+        int randomInt = Randoms.pickNumberInRange(0, 9);
+        if(randomInt >= 4){
+            position ++;
+        }
+    }
+
+}
