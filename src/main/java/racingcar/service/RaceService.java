b259c4709bdb20fb99e471b348411770faa199dc
slay <palgalow@gmail.com>
2024-10-26 21:17:37 +0900

    feat(RaceService): 우승자의 위치 구하는 기능 추가

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -1,9 +1,26 @@
 package racingcar.service;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import racingcar.domain.Car;
+
 public class RaceService {
 
     public String[] splitCarNames(String carNames) {
         String[] splitNames = carNames.split(",");
         return splitNames;
+    }
+
+    public void decisionWinners(List<Car> Cars) {
+        for (Car car : Cars) {
+
+        }
+    }
+
+    public int getWinnerPosition(List<Car> Cars) {
+        Car winner = Collections.max(Cars, Comparator.comparingInt(car -> car.getPosition()));
+        int winnerPosition = winner.getPosition();
+        return winnerPosition;
     }
 }
