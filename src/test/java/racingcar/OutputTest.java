9ce3a98aa42cbc3c16a49db7e5bbb361e41784cf
MCshin00 <sinminc7@gmail.com>
2024-10-26 21:45:38 +0900

    Test: 테스트 코드 분리 및 개선
    - OutputTest 분리 및 ParameterizedTest, MethodSource, ValueSource, NullAndEmptySource, BeforeEach, AfterEach 등의 어노테이션을 활용하도록 수정

---
+++ src/test/java/racingcar/OutputTest.java
@@ -0,0 +1,80 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.RandomGenerator;
+import racingcar.domain.Winners;
+import racingcar.mock.TestRandomNumberGenerator;
+import racingcar.view.OutputView;
+
+public class OutputTest {
+    final OutputStream out = new ByteArrayOutputStream();
+    final PrintStream standardOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    static Stream<Arguments> createCarList() {
+        return Stream.of(
+                Arguments.of(List.of(new Car("car1"), new Car("car2")))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("createCarList")
+    void 차수별_실행_결과_출력_테스트(List<Car> carList) {
+        RandomGenerator testRandomNumberGenerator = new TestRandomNumberGenerator(List.of(4,3));
+        Cars cars = new Cars(carList, testRandomNumberGenerator);
+
+        cars.moveAll();
+        OutputView.printRoundResultOutput(cars.toDto());
+
+        assertThat(out.toString()).isEqualTo("car1 : -" + System.lineSeparator() +
+                "car2 : " + System.lineSeparator() + System.lineSeparator());
+    }
+
+    @ParameterizedTest
+    @MethodSource("createCarList")
+    void 단독_우승자_출력_테스트(List<Car> carList) {
+        RandomGenerator testRandomNumberGenerator = new TestRandomNumberGenerator(List.of(4,3));
+        Cars cars = new Cars(carList, testRandomNumberGenerator);
+
+        cars.moveAll();
+        Winners winners = new Winners(cars.getWinners());
+        OutputView.printWinnersOutput(winners.getWinnersList());
+
+        assertThat(out.toString().trim()).isEqualTo("최종 우승자 : car1");
+    }
+
+    @ParameterizedTest
+    @MethodSource("createCarList")
+    void 공동_우승자_출력_테스트(List<Car> carList) {
+        RandomGenerator testRandomNumberGenerator = new TestRandomNumberGenerator(List.of(4,4));
+        Cars cars = new Cars(carList, testRandomNumberGenerator);
+
+        cars.moveAll();
+        Winners winners = new Winners(cars.getWinners());
+        OutputView.printWinnersOutput(winners.getWinnersList());
+
+        assertThat(out.toString().trim()).isEqualTo("최종 우승자 : car1, car2");
+    }
+}
--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -3,9 +3,6 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 import static racingcar.utils.StringSeparator.separate;
 
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
@@ -20,29 +17,19 @@ import racingcar.domain.Winners;
 import racingcar.domain.dto.CarDto;
 import racingcar.domain.dto.CarsDto;
 import racingcar.mock.TestRandomNumberGenerator;
-import racingcar.view.OutputView;
 
 public class RacingCarTest {
+    static Stream<Arguments> createCarList() {
+        return Stream.of(
+                Arguments.of(List.of(new Car("car1"), new Car("car2")))
+        );
+    }
+
     @Test
     void 자동차_생성_테스트() {
         Car car = new Car("MCs");
 
         assertThat(car.getName()).isEqualTo("MCs");
-    }
-
-    @Test
-    void 자동차_리스트_생성_테스트() {
-        Car car = new Car("MCs");
-        Car car2 = new Car("asdf");
-        RandomGenerator randomNumberGenerator = new RandomNumberGenerator();
-        Cars cars = new Cars(List.of(car, car2), randomNumberGenerator);
-
-        CarsDto carsDto = cars.toDto();
-        List<String> carNames = List.of("MCs", "asdf");
-
-        assertThat(carsDto.getCars().stream()
-                .map(CarDto::getName))
-                .containsExactlyElementsOf(carNames);
     }
 
     @Test
@@ -63,107 +50,55 @@ public class RacingCarTest {
         assertThat(car.getPosition()).isEqualTo(0);
     }
 
-    @Test
-    void 자동차_전체_이동_테스트() {
-        Car car1 = new Car("car1");
-        Car car2 = new Car("car2");
-        List<Car> carList = List.of(car1, car2);
-        RandomGenerator testRandomNumberGenerator = new TestRandomNumberGenerator(List.of(4,3));
-        Cars cars = new Cars(carList, testRandomNumberGenerator);
+    @ParameterizedTest
+    @MethodSource("createCarList")
+    void 자동차_리스트_생성_테스트(List<Car> carList) {
+        RandomGenerator randomNumberGenerator = new RandomNumberGenerator();
+        Cars cars = new Cars(carList, randomNumberGenerator);
+        List<String> carNames = List.of("car1", "car2");
 
-        cars.moveAll();
+        CarsDto carsDto = cars.toDto();
 
-        assertThat(car1.getPosition()).isEqualTo(1);
-        assertThat(car2.getPosition()).isEqualTo(0);
+        assertThat(carsDto.getCars().stream()
+                .map(CarDto::getName))
+                .containsExactlyElementsOf(carNames);
     }
 
-    @Test
-    void 단독_우승자_결정_테스트() {
-        Car car1 = new Car("car1");
-        Car car2 = new Car("car2");
-        List<Car> carList = List.of(car1, car2);
+    @ParameterizedTest
+    @MethodSource("createCarList")
+    void 자동차_전체_이동_테스트(List<Car> carList) {
         RandomGenerator testRandomNumberGenerator = new TestRandomNumberGenerator(List.of(4,3));
         Cars cars = new Cars(carList, testRandomNumberGenerator);
-        Winners winners;
 
         cars.moveAll();
-        winners = new Winners(cars.getWinners());
+        CarsDto carsDto = cars.toDto();
+
+        assertThat(carsDto.getCars().get(0).getPosition()).isEqualTo(1);
+        assertThat(carsDto.getCars().get(1).getPosition()).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @MethodSource("createCarList")
+    void 단독_우승자_결정_테스트(List<Car> carList) {
+        RandomGenerator testRandomNumberGenerator = new TestRandomNumberGenerator(List.of(4,3));
+        Cars cars = new Cars(carList, testRandomNumberGenerator);
+
+        cars.moveAll();
+        Winners winners = new Winners(cars.getWinners());
 
         assertThat(winners.getWinnersList()).isEqualTo("car1");
     }
 
-    @Test
-    void 공동_우승자_결정_테스트() {
-        Car car1 = new Car("car1");
-        Car car2 = new Car("car2");
-        List<Car> carList = List.of(car1, car2);
+    @ParameterizedTest
+    @MethodSource("createCarList")
+    void 공동_우승자_결정_테스트(List<Car> carList) {
         RandomGenerator testRandomNumberGenerator = new TestRandomNumberGenerator(List.of(4,4));
         Cars cars = new Cars(carList, testRandomNumberGenerator);
-        Winners winners;
 
         cars.moveAll();
-        winners = new Winners(cars.getWinners());
+        Winners winners = new Winners(cars.getWinners());
 
         assertThat(winners.getWinnersList()).isEqualTo("car1, car2");
-    }
-
-    @Test
-    void 차수별_실행_결과_출력_테스트() {
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        Car car1 = new Car("car1");
-        Car car2 = new Car("car2");
-        List<Car> carList = List.of(car1, car2);
-        RandomGenerator testRandomNumberGenerator = new TestRandomNumberGenerator(List.of(4,3));
-        Cars cars = new Cars(carList, testRandomNumberGenerator);
-
-        cars.moveAll();
-
-        OutputView.printRoundResultOutput(cars.toDto());
-
-        assertThat(out.toString()).isEqualTo("car1 : -" + System.lineSeparator() +
-                "car2 : " + System.lineSeparator() + System.lineSeparator());
-    }
-
-    @Test
-    void 단독_우승자_출력_테스트() {
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        Car car1 = new Car("car1");
-        Car car2 = new Car("car2");
-        List<Car> carList = List.of(car1, car2);
-        RandomGenerator testRandomNumberGenerator = new TestRandomNumberGenerator(List.of(4,3));
-        Cars cars = new Cars(carList, testRandomNumberGenerator);
-        Winners winners;
-
-        cars.moveAll();
-        winners = new Winners(cars.getWinners());
-
-        OutputView.printWinnersOutput(winners.getWinnersList());
-
-        assertThat(out.toString().trim()).isEqualTo("최종 우승자 : car1");
-    }
-
-    @Test
-    void 공동_우승자_출력_테스트() {
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        Car car1 = new Car("car1");
-        Car car2 = new Car("car2");
-        List<Car> carList = List.of(car1, car2);
-        RandomGenerator testRandomNumberGenerator = new TestRandomNumberGenerator(List.of(4,4));
-        Cars cars = new Cars(carList, testRandomNumberGenerator);
-        Winners winners;
-
-        cars.moveAll();
-        winners = new Winners(cars.getWinners());
-
-        OutputView.printWinnersOutput(winners.getWinnersList());
-
-        assertThat(out.toString().trim()).isEqualTo("최종 우승자 : car1, car2");
     }
 
     @ParameterizedTest
