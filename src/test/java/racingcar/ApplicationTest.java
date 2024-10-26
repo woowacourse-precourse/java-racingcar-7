4a327d65f2bf4c623b37f4adb723df0660caa27f
Seongmo Ahn <djgood780@gmail.com>
2024-10-27 01:02:07 +0900

    test(ApplicationTest): 자동차 이름들을 쉼표(,)로 구분하였는지 검증 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,10 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -34,5 +37,20 @@ class ApplicationTest extends NsTest {
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+    /** 테스트 클래스 명 -> [메서드 명]_[테스트 중 상태]_[기대 행위] **/
+    @ParameterizedTest
+    @ValueSource(strings = {"Ahn;Seong;Mo", "Ahn,Seong;Mo", "Ahn/Seong,Mo"})
+    @DisplayName("자동차 이름들을 쉼표(,)로 구분하지 않았으면 예외 발생")
+    void separateByComma_NotSeparatedByComma_ExceptionThrown(String carNames){
+        //given
+        final String expectedMessage = "쉼표(,)로 구분하여 자동차 이름들을 입력해주세요";
+        final CarNameValidator carNameValidator = new CarNameValidator();
+
+        //when & then
+        assertThatThrownBy(() -> carNameValidator.separateByComma(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(expectedMessage);
     }
 }
