589e45dc4ac14ae5568b4cde2630ce547eaf3b73
woogym <kwj9294@gmail.com>
2024-10-27 06:57:19 +0900

    feat(model): Implement cars model

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private static Cars instance;
+    private final List<Car> cars;
+
+    public Cars() {
+        this.cars = new ArrayList<>();
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void addCars(final List<String> carList) {
+        for (String carName : carList) {
+            this.cars.add(new Car(carName));
+        }
+    }
+}
