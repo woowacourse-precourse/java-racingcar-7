1332fa68fca6b0dcd21296d7deaffc806507b6d0
eundeang <111877048+eundeang@users.noreply.github.com>
2024-10-27 04:27:41 +0900

    feat(OutputView): 시도횟수 관련 안내멘트 출력 메서드 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,4 +6,9 @@ public class OutputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
+    public void printTryCountProvideMessage() {
+        System.out.println("시도할 횟수");
+    }
+
+
 }
