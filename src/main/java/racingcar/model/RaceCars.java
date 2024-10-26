97232bcc695ae758ff82c055b820c1c01950592c
sinminseok12 <sinminseok12@naver.com>
2024-10-27 00:28:05 +0900

    feat : 중복된 자동차 이름 검증 기능 구현
    중복된 자동차 이름이 있을경우, 최종 우승자 판단이 어려워 예외를 발생시켰다

--- src/main/java/racingcar/model/RaceCars.java
+++ src/main/java/racingcar/model/RaceCars.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -8,6 +9,8 @@ import static java.util.stream.Collectors.toList;
 import static racingcar.utils.NumberGenerator.createRandomNumber;
 
 public class RaceCars {
+    private static final String ERROR_DUPLICATE_NAME = "중복된 자동차 이름을 허용하지 않습니다.";
+
     private final List<RaceCar> raceCars;
 
     private RaceCars(List<RaceCar> raceCars) {
@@ -15,10 +18,17 @@ public class RaceCars {
     }
 
     public static RaceCars fromNames(List<String> names) {
+        validateDuplicateNames(names);
         List<RaceCar> carList = names.stream()
                 .map(RaceCar::fromName)
                 .collect(toList());
         return new RaceCars(carList);
+    }
+
+    private static void validateDuplicateNames(List<String> names) {
+        if (names.size() != new HashSet<>(names).size()) {
+            throw new IllegalArgumentException(ERROR_DUPLICATE_NAME);
+        }
     }
 
     public void race() {
