57af3c463e39d7e2e5ed11635e6cbba3e5be6585
mingking2 <mingee641@gmail.com>
2024-10-26 23:44:57 +0900

    refactor: 문자,숫자 상수화

--- src/main/java/racingcar/constant/NumberConstant.java
+++ src/main/java/racingcar/constant/NumberConstant.java
@@ -8,4 +8,6 @@ public class NumberConstant {
     public static final int SET_CAR_NAME_LENGTH = 5;
     public static final int MIN_NAME_COUNT = 2;
     public static final int MIN_COUNT = 1;
+
+    public static final String NUMBER_REGEX = "\\d+";
 }
