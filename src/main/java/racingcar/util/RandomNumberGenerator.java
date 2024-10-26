51410436b4799366c1359eec388811e6b4e6fed6
byunsp3610@gmail.com <byunsp3610@gmail.com>
2024-10-26 23:10:21 +0900

    feat: 랜덤값 반환 함수 구현

---
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -0,0 +1,15 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Random;
+
+public class RandomNumberGenerator {
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+
+    public int generateRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    }
+
+}
