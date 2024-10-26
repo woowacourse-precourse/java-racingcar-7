054e99294b70d4d0c88665c36aecd6b2e0f562fa
양효빈 <hyobin3026@naver.com>
2024-10-26 13:58:24 +0900

    feat(RacingCarValidator): Implement validation logic for model

--- src/main/java/racingcar/validator/RacingCarValidator.java
+++ src/main/java/racingcar/validator/RacingCarValidator.java
@@ -1,4 +1,24 @@
 package racingcar.validator;
 
 public class RacingCarValidator {
+
+    public static void isValidCarName(String carName){
+        if (carName.length() > 5 || carName.isEmpty()){
+            throw new IllegalArgumentException("자동차의 이름을 잘못 입력");
+        }
+    }
+
+
+    public static boolean canForward(int randomNumber){
+        return (randomNumber >= 4);
+    }
+
+
+    public static int canStringToInt(String racingAttemptCountString){
+        try{
+            return Integer.parseInt(racingAttemptCountString);
+        } catch (Exception e){
+            throw new IllegalArgumentException("잘못된 경주 횟수 입력");
+        }
+    }
 }
