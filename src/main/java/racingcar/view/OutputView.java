ad1b0fafc247451f4d22965326282d81b5bf1332
mingking2 <mingee641@gmail.com>
2024-10-26 23:45:16 +0900

    refactor: 출력메서드 분리 및 최적화

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,6 +1,7 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.constant.io.OutputConstant;
 import racingcar.model.Car;
 
@@ -12,19 +13,23 @@ public class OutputView {
 
     public static void outputProcessResultListView(List<Car> carList) {
         for (Car car : carList) {
-            System.out.print(car.getName() + " : ");
-            for (int i = 0; i < car.getMoveCount(); i++) {
-                System.out.print("-");
-            }
-            System.out.println();
+            outputResultCarView(car);
+        }
+        System.out.println();
+    }
+
+    public static void outputResultCarView(Car car) {
+        System.out.print(car.getName() + OutputConstant.OUTPUT_COLON);
+        for (int i = 0; i < car.getMoveCount(); i++) {
+            System.out.print(OutputConstant.OUTPUT_MINUS);
         }
         System.out.println();
     }
 
     public static void outputFinalWinnerView(List<Car> winnerList) {
-        System.out.print(OutputConstant.OUTPUT_FINAL_WINNER_VIEW);
-        for (Car car : winnerList) {
-            System.out.print(car.getName() + ",");
-        }
+        String winners = winnerList.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(OutputConstant.OUTPUT_COMMA));
+        System.out.print(OutputConstant.OUTPUT_FINAL_WINNER_VIEW + winners);
     }
 }
