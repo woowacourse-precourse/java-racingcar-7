d6b3e5d8a3111a14b5962566cee8b902c013f85a
Etwashoeren <sungmin0609@nate.com>
2024-10-27 00:18:22 +0900

    feat. 중복된 자동차 이름에 대한 예외처리 추가

--- src/main/java/racingcar/UserView.java
+++ src/main/java/racingcar/UserView.java
@@ -12,7 +12,16 @@ public class UserView {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
-        return Console.readLine().split(",");
+        String[] carNames = Console.readLine().split(",");
+
+        Set<String> uniqueNames = new HashSet<>();
+        for (String name : carNames) {
+            if (!uniqueNames.add(name)) {
+                throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+            }
+        }
+
+        return carNames;
     }
 
     public int inputRoundCount() {
