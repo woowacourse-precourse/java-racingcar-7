c76fd57f7bb11756c0bd55de56731d8a75fc4f15
wogus <ind07152@naver.com>
2024-10-26 18:56:08 +0900

    feat(NumberType): 숫자 상수 추가
    자동차 이름 최소 길이에 대한 상수를 추가한다.

--- src/main/java/racingcar/constant/NumberType.java
+++ src/main/java/racingcar/constant/NumberType.java
@@ -4,6 +4,7 @@ public enum NumberType {
     MIN_CAR_SIZE(2),
     MIN_COUNT(1),
     MAX_NAME_LENGTH(5),
+    MIN_NAME_LENGTH(1),
     MIN_RANDOM_NUMBER(0),
     MAX_RANDOM_NUMBER(9),
     START_LOCATION(0),
