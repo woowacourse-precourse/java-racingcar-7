cedaa37ce5e849d9d254120f3eadb60f3166abd1
slay <palgalow@gmail.com>
2024-10-26 21:44:25 +0900

    feat(View): OutputView 클래스 기능 수정 및 일부 메소드 InputView로 이동, 차 이름 입력받는 기능과 시도할 횟수 입력받는 기능 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,27 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.service.RaceService;
+
+public class InputView {
+
+    private final String INPUT_CAR_NAMES_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표() 기준으로 구분)";
+    private final String INPUT_TRY_COUNT_MSG = "시도할 횟수는 몇 회인가요?";
+    private RaceService raceService;
+
+    public InputView() {
+        this.raceService = new RaceService();
+    }
+
+    public String inputCarNames() {
+        System.out.println(INPUT_CAR_NAMES_MSG);
+        String carNames = Console.readLine();
+        return carNames;
+    }
+
+    public int inputTryCount() {
+        System.out.println(INPUT_TRY_COUNT_MSG);
+        int tryCount = Integer.parseInt(Console.readLine());
+        return tryCount;
+    }
+}
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,17 +2,7 @@ package racingcar.view;
 
 public class OutputView {
 
-    private final String INPUT_CAR_NAMES_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표() 기준으로 구분)";
-    private final String INPUT_TRY_COUNT_MSG = "시도할 횟수는 몇 회인가요?";
     private final String RESULT_MSG = "실행 결과";
-
-    public void printCarNameMessage() {
-        System.out.println(INPUT_CAR_NAMES_MSG);
-    }
-
-    public void printTryCountMessage() {
-        System.out.println(INPUT_TRY_COUNT_MSG);
-    }
 
     public void printResultMessage() {
         System.out.println(RESULT_MSG);
