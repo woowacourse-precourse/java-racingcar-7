6718b81b8fcf9c12b442bfbef2b1c70905c62944
DongYeop <s01054830455@gmail.com>
2024-10-27 03:21:38 +0900

    test(CarTest): position과 name에 대한 유효성 검사 테스트 제거
    - position과 name 객체에서 각각의 유효성 검사가 가능하므로 Car에서의 유효성 테스트는 제거했음.

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,19 +1,18 @@
 package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.model.dto.CarStatusDto;
 import racingcar.util.randomnumber.RandomNumberGenerator;
 import racingcar.util.randomnumber.StubRandomNumberGenerator;
 
 class CarTest {
 
-    @DisplayName("이름을 가진 Car를 생성할 수 있다.")
+    @DisplayName("Car를 생성할 수 있다.")
     @Test
     void createCarByName() {
         //given
@@ -23,42 +22,7 @@ class CarTest {
         Car car = Car.of(carName, new RandomNumberGenerator());
 
         //then
-        assertThat(car.getName()).isEqualTo(carName);
-    }
-
-    @DisplayName("Car가 생성될 때 position은 0 이다.")
-    @Test
-    void createCarSetPositionIsZero() {
-        //given
-        String carName = "woni";
-
-        //when
-        Car car = Car.of(carName, new RandomNumberGenerator());
-
-        //then
-        assertThat(car.getPosition()).isEqualTo(0);
-    }
-
-    @DisplayName("CarName의 크기가 5를 초과하면 예외를 발생한다.")
-    @Test
-    void carNameLengthExceedThenFive() {
-        //given
-        String carName = "javaji";
-
-        //when //then
-        assertThatThrownBy(() -> Car.of(carName, new RandomNumberGenerator()))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("CarName의 크기가 5를 초괴하지 않으면 예외를 발생하지 않는다.")
-    @Test
-    void carNameLengthLessThenFive() {
-        //given
-        String carName = "pobii";
-
-        //when //then
-        assertThatCode(() -> Car.of(carName, new RandomNumberGenerator()))
-                .doesNotThrowAnyException();
+        assertThat(car).isEqualTo(Car.of(carName, new RandomNumberGenerator()));
     }
 
     @DisplayName("랜덤값의 크기가 4이상이라면 전진할 수 있다.")
@@ -72,12 +36,13 @@ class CarTest {
         car.isMove();
 
         //then
-        assertThat(car.getPosition()).isEqualTo(expectedPosition);
+        CarStatusDto carStatusDto = car.mapStatusToDto();
+        assertThat(carStatusDto.position()).isEqualTo(expectedPosition);
     }
 
     @DisplayName("주어진 전진값과 현재 자동차의 전진값을 비교하여 더 큰 전진값을 반환한다.")
     @Test
-    void calculateMaxPosition() {
+    void findMaxPosition() {
         //given
         Car car = Car.of("pobi", new StubRandomNumberGenerator(4));
         int position = 2;
@@ -87,9 +52,9 @@ class CarTest {
         car.isMove();
 
         //when
-        int maxPosition = car.calculateMaxPosition(position);
+        int maxPosition = car.findMaxPosition(position);
 
         //then
-        assertThat(maxPosition).isEqualTo(car.getPosition());
+        assertThat(maxPosition).isEqualTo(car.findMaxPosition(position));
     }
 }
