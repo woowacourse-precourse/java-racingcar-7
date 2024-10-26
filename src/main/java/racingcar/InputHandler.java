3993f19389a8367c175438d8686786eb5017df66
Seol-Munhyeok <abc1234568@naver.com>
2024-10-26 22:21:58 +0900

    feat(isValidMoveCount): Add isValidMoveCount()

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -6,6 +6,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputHandler {
     private static final int MAX_NAME_LENGTH = 5;
+    private static final int MAX_MOVE_COUNT = 1000;
+    private static final int MIN_MOVE_COUNT = 1;
 
     List<String> getCarNames() {
         String carNames;
@@ -46,7 +48,10 @@ public class InputHandler {
     }
 
     boolean isValidMoveCount(int moveCount) {
-        return moveCount >= 1 && moveCount <= 1000;
+        if (moveCount >= MIN_MOVE_COUNT && moveCount <= MAX_MOVE_COUNT) {
+            return true;
+        }
+        else throw new IllegalArgumentException("이동 횟수는 1회에서 1000회 사이 값만 가능");
     }
 
 
