9fc060e8f354c02399c4158e31672f7b937e8347
minjae k <rkdalswoals@gmail.com>
2024-10-26 14:01:16 +0900

    feat: 콘솔 입력 요청 출력 및 사용자 입력 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,17 +1,37 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import racingcar.controller.GameController;
 import racingcar.service.GameService;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
+/*
+[] 주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다.
+[] 각 자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
+[] 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
+[] 사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.
+[] 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
+[] 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
+[] 우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.
+[] 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.
+*/
+
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        GameService gameService = new GameService();
-        OutputView outputView = new OutputView();
+//        InputView inputView = new InputView();
+//        GameService gameService = new GameService();
+//        OutputView outputView = new OutputView();
+//
+//        GameController gameController = new GameController(inputView, gameService, outputView);
+//        gameController.run();
 
-        GameController gameController = new GameController(inputView, gameService, outputView);
-        gameController.run();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String userInput = Console.readLine();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int roundCount = Integer.parseInt(Console.readLine());
+
+
     }
 }
