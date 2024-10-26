fc56001a08c2e16799911c342fe4fa3ff64217ac
ghktndyd <hwasuyong99@naver.com>
2024-10-27 08:44:24 +0900

    test(CarTest): 자동차 이름이 5글자가 넘어갔을 때의 상황 테스트

--- src/test/java/racingcar/car/CarTest.java
+++ src/test/java/racingcar/car/CarTest.java
@@ -10,8 +10,16 @@ class CarTest {
 
     @DisplayName("자동차 이름에 빈 문자열이 포함된 경우 예외가 발생한다.")
     @Test
-    void shouldThrowExceptionWhenCarNameIsEmpty() {
+    void newCarWithEmptyName() {
         assertThatThrownBy(() -> new Cars(List.of("pobi", "", "test")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 1자 이상, 5자 이하만 가능합니다.");
+    }
+
+    @DisplayName("자동차 이름이 5자를 초과하는 경우 예외가 발생한다.")
+    @Test
+    void newCarWithOutOfRange() {
+        assertThatThrownBy(() -> new Cars(List.of("pobi", "pobipobi", "test")))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 1자 이상, 5자 이하만 가능합니다.");
     }
