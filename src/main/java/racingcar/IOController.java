442f80c7bf2f0e41869b08875c1b152bec1d0b0d
chyo1 <hyowon3856@gmail.com>
2024-10-27 00:52:40 +0900

    fix : Scanner close 추가

--- src/main/java/racingcar/IOController.java
+++ src/main/java/racingcar/IOController.java
@@ -9,6 +9,7 @@ public class IOController {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
 
+        Console.close();
         return carNames;
     }
 
