ac836d92b38bece6a4eae267fc5e61c0c7dc1ec4
dbdb456 <adad568@naver.com>
2024-10-26 15:59:33 +0900

    test(validator): 입력된 이름 문자열 빈칸 검증

--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -1,4 +1,26 @@
 package racingcar;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.validator.Validator;
+
 public class ValidatorTest {
+
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = new Validator();
+    }
+
+    @DisplayName("입력된 문자열이 빈칸인 경우 IllegalArgumentException 예외 발생")
+    @Test
+    void validateInputNotBlank_ShouldThrowException_WhenInputIsBlank() {
+        String blankInput = "   ";
+        assertThrows(IllegalArgumentException.class,
+                () -> validator.validateInputNotBlank(blankInput));
+    }
 }
