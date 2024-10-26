f2de72a1dfd2bcab419afb78e3cdedfbe3d3187c
unh6unh6 <ruddbs3308@naver.com>
2024-10-26 16:58:12 +0900

    test(Cars): 자동차를 저장한다

---
+++ src/test/java/racingcar/model/CarsTest.java
@@ -0,0 +1,31 @@
+package racingcar.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.dto.CarDto;
+
+public class CarsTest {
+    Cars cars;
+
+    @BeforeEach
+    void init() {
+        cars = new Cars();
+    }
+
+    @Test
+    void 자동차를_저장한다() {
+        cars.saveCar("pobi");
+        cars.saveCar("jun");
+        List<CarDto> carDtoList = Arrays.asList(
+                new CarDto("pobi", 0),
+                new CarDto("jun", 0)
+        );
+
+        assertThat(cars.getCarDtoList())
+                .isEqualTo(carDtoList);
+    }
+}
