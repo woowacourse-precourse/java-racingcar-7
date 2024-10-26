b770b8d9c22f5836ba6bacd839ac594dcfa92c05
changuii <rhljh7410@gmail.com>
2024-10-26 15:37:24 +0900

    test(BaiscRacingCarTransporterTest) :  4 이상이라면 전진하고 3 이하라면 멈춘다.를 테스트하라

---
+++ src/test/java/racingcar/util/transporter/BasicRacingCarTransporterTest.java
@@ -0,0 +1,56 @@
+package racingcar.util.transporter;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.RacingCar;
+
+public class BasicRacingCarTransporterTest {
+    private final BasicRacingCarTransporter basicRacingCarTransporter;
+
+    public BasicRacingCarTransporterTest(){
+        this.basicRacingCarTransporter = new BasicRacingCarTransporter();
+    }
+
+    @Test
+    public void 무작위값_4인_경우_테스트(){
+        assertSimpleTest(() -> {
+            // given
+            RacingCar car = new RacingCar("보성");
+            int location = 1;
+            List<RacingCar> racingCars = List.of(car);
+            List<Integer> randomNumbers = List.of(4);
+
+            // when
+            basicRacingCarTransporter.transportRacingCar(racingCars, randomNumbers);
+
+
+            // then
+            assertThat(car.getCurrentLocation()).isEqualTo(location);
+
+
+        });
+    }
+
+    @Test
+    public void 무작위값_3인_경우_테스트(){
+        assertSimpleTest(() -> {
+            // given
+            RacingCar car = new RacingCar("보성");
+            int location = 0;
+            List<RacingCar> racingCars = List.of(car);
+            List<Integer> randomNumbers = List.of(3);
+
+            // when
+            basicRacingCarTransporter.transportRacingCar(racingCars, randomNumbers);
+
+
+            // then
+            assertThat(car.getCurrentLocation()).isEqualTo(location);
+
+
+        });
+    }
+}
