4bc56a7e645e6e17f20cd443dfb8f4b084f58e6c
99hyeon <hyeon99@ajou.ac.kr>
2024-10-26 21:25:30 +0900

    feat: 자동차 이름 입력 받고 쉼표로 구분해 따로 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,37 @@
 package racingcar;
 
+import java.util.LinkedList;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+
+    public static class Car{
+        String name;
+        int moveDistance;
+
+        public Car(String name, int moveDistance) {
+            this.name = name;
+            this.moveDistance = moveDistance;
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = readLine();
+
+        LinkedList<Car> carList = new LinkedList<>();
+        carList = parsingCarName(carNames);
+
+    }
+
+    public static LinkedList<Car> parsingCarName(String carNames){
+        LinkedList<Car> carList = new LinkedList<>();
+
+        for(String name : carNames.split(",")){
+            carList.add(new Car(name, 0));
+        }
+
+        return carList;
     }
 }
