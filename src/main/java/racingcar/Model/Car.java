a23da424ec9dc634672044cada6f30d2e9c37de1
김호연 <yun1094@naver.com>
2024-10-26 12:14:15 +0900

    자동차 관리 클래스 완성

--- src/main/java/racingcar/Model/Car.java
+++ src/main/java/racingcar/Model/Car.java
@@ -4,15 +4,15 @@ public class Car {
     private final String carName;
     private int moveStatus;
 
-    public Car(String CarName, int Movestatus) {
-        this.carName = CarName;
-        this.moveStatus = 0;
-    }
     public String getName() {
         return this.carName;
     }
     public int getMovestatus() {
         return this.moveStatus;
+    }
+    public Car(String CarName) {
+        this.carName = CarName;
+        this.moveStatus = 0;
     }
     public void move() {
         this.moveStatus += 1;
