e008433293a25b02c2204e3e0357f597beb7a5d4
min-0 <dhe08133@naver.com>
2024-10-27 01:23:03 +0900

    feat: 우승자 출력
    시도 결과 중 가장 전진을 많이 한 우승자(들) 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -11,7 +13,7 @@ public class Application {
         System.out.println("시도할 횟수는 몇 회인가요?");
         int num = Integer.parseInt(Console.readLine());
 
-        System.out.println("실행 결과");
+        System.out.println("\n실행 결과");
 
         String[] carList = input.split(",");
         int[] carIndex = new int[carList.length];
@@ -28,5 +30,19 @@ public class Application {
             }
             System.out.println();
         }
+
+        int maxDistance = 0;
+        for (int distance : carIndex) {
+            maxDistance = Math.max(maxDistance, distance);
+        }
+
+        List<String> winners = new ArrayList<>();
+        for (int i = 0; i < carIndex.length; i++) {
+            if (carIndex[i] == maxDistance) {
+                winners.add(carList[i]);
+            }
+        }
+
+        System.out.println("최종 우승자  :  " + String.join(", ", winners));
     }
 }
