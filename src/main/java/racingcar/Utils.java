1d262b5e15ca09895309b9df5acb944e0f63ee44
Hyungjung Kim <gudwnd1113@gmail.com>
2024-10-27 02:05:27 +0900

    fix(Utils): allow space in enterCarList()
    변경 전: 입력 문자열에 대해 공백 제거 후 split(",") 수행
    변경 후: 입력 문자열에 대해 split(",") 수행 후 List 내 각 원소에 strip() 수행
    자동차 이름 내의 띄어쓰기를 허용한다. (e.g. "A BCD")

--- src/main/java/racingcar/Utils.java
+++ src/main/java/racingcar/Utils.java
@@ -8,8 +8,9 @@ public final class Utils {
     public static List<String> enterCarList() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String text = Console.readLine();
-        text = text.replaceAll(" ", "");
-        return Arrays.asList(text.split(","));
+        List<String> nameList = Arrays.asList(text.split(","));
+        nameList.replaceAll(String::strip);
+        return nameList;
     }
 
     public static int enterRound() {
