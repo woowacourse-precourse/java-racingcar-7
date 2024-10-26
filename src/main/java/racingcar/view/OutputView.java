7a7c2e515645d50be522635eeb3719207827766a
eundeang <111877048+eundeang@users.noreply.github.com>
2024-10-27 04:41:12 +0900

    feat(OutputView): 자동차 이름과 함께 전진 상태 출력 메서드 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,6 +1,14 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+
 public class OutputView {
+
+    private final Car car;
+
+    public OutputView(Car car) {
+        this.car = car;
+    }
 
     public void printCarNameProvideMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -12,6 +20,10 @@ public class OutputView {
 
     public void printExecutionResult() {
         System.out.println("실행 결과");
+    }
+
+    public void printCarNameAndMovingStatus() {
+        System.out.println(car.getName() + " : " + car.getMoveStatus());
     }
 
 }
