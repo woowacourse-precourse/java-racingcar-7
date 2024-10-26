1f5521b337c3b51ca6bf479afc676b9e3cd8d99a
fbehddn <tom0611@naver.com>
2024-10-26 16:47:47 +0900

    feat: 각 단계별 시작 메서드 구현

---
+++ src/main/java/racingcar/domain/CarGameManager.java
@@ -0,0 +1,36 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarGameManager {
+    public ArrayList<Car> cars;
+    public int totalTimes;
+
+    public CarGameManager(ArrayList<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void start(String[] names, int totalTimes) {
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
+        this.totalTimes = totalTimes;
+
+        for (int i = 0; i < totalTimes; i++) {
+            startEachStep();
+            System.out.println();
+        }
+    }
+
+    private void startEachStep() {
+        for (Car car : cars) {
+            if (car.isMoved(Randoms.pickNumberInRange(0, 9))) {
+                car.move();
+            }
+            car.printEachStep();
+        }
+    }
+
+}
