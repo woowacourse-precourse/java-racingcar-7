271444677cda5b31689d23dbfe478aea40ffd573
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-27 06:31:33 +0900

    feat: Cars에서 사용할 Car를 다루는 Class 생성
    차의 이름을 가져와 할당 후, 랜덤값을 통해 차 이동 정지 결정한다.
    또한, 테스트 코드를 유념해, 동일한 이름을 가진 move메서드를 입력값을
    차이를 둬 작성했다.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,2 +1,41 @@
-package racingcar;public class Car {
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+
+    private final Name car;
+    private int status;
+
+    public Car (Name name){
+        this.car = name;
+    }
+
+    public int getStatus(){
+        return status;
+    }
+
+    public void move() {
+        move(getRandomNum());
+    }
+
+    public void move(int num){
+        if (num >= 4) {
+            status++;
+        }
+    }
+
+    private int getRandomNum () {
+        int num = Randoms.pickNumberInRange(0, 9);
+        return num;
+    }
+
+    public String printStatus(){
+        return car + " : " + "-".repeat(status);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(car);
+    }
 }
