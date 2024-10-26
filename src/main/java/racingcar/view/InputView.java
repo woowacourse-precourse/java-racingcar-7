f3f08aa1de5169af87227cac211da6ac1b23aba3
jieunpark <jieun20626@gmail.com>
2024-10-27 02:44:25 +0900

    feat: 이름 글자수 제한 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,8 +5,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public Data inputData() {
 
-        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
+
+        String[] cars = carNames.split(",");
+        for (String car : cars) {
+            if (car.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+
         System.out.println("시도할 횟수는 몇 회인가요?");
         int trialNum = Integer.parseInt(Console.readLine());
 
