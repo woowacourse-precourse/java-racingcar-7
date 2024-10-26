bcf5a07384690b9825aa54f53421c743638c61e2
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-10-26 23:39:17 +0900

    feat : controller.RacingGameController printResult 메서드 구현

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -18,5 +18,22 @@ public class RacingGameController {
         System.out.println("경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분)");
         String[] carNames = Console.readLine().split(",");
         List<Car> cars = Arrays.stream(carNames).map(Car::new).toList();
+        System.out.println("시도할 횟수");
+        int playRound = Integer.parseInt(Console.readLine());
+
+        playRound(playRound, cars);
+    }
+
+    private void playRound(int playRound, List<Car> cars) {
+        for (int i = 0; i < playRound; i++){
+            racingGameService.play(cars);
+            printResult(cars);
+        }
+    }
+
+    private void printResult(List<Car> cars){
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
     }
 }
