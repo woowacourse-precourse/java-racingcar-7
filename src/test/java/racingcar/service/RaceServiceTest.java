731493c93aec96556900cef370d3554471f73e9f
blueconecell <95961799+blueconecell@users.noreply.github.com>
2024-10-26 19:24:39 +0900

    test(service): 시도할 횟수로 빈 값 들어온 경우 테스트

--- src/test/java/racingcar/service/RaceServiceTest.java
+++ src/test/java/racingcar/service/RaceServiceTest.java
@@ -1,6 +1,8 @@
 package racingcar.service;
 
 import org.junit.jupiter.api.Test;
+import racingcar.constant.Message;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RaceServiceTest {
@@ -14,5 +16,17 @@ class RaceServiceTest {
 
         //then
         assertEquals(raceService.roundService(inputRound),5);
+    }
+
+    @Test
+    public void 시도할_횟수로_아무것도_안들어온_경우() throws Exception {
+        //given
+        String inputRound = "";
+
+        //when
+        RaceService raceService = new RaceService();
+
+        //then
+        assertThrows(IllegalArgumentException.class, () -> raceService.roundService(inputRound), Message.ROUND_EMPTY_VALIDATION_MESSAGE.getMessage());
     }
 }
