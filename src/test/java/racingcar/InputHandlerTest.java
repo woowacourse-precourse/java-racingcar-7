7d1123d54e3a6ae83e6f86dfba48c3f2cf69c6ec
Seol-Munhyeok <abc1234568@naver.com>
2024-10-26 20:03:23 +0900

    refactor(inputHandlerTest): Rename test method using DisplayName annotation

--- src/test/java/racingcar/InputHandlerTest.java
+++ src/test/java/racingcar/InputHandlerTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,7 +10,8 @@ public class InputHandlerTest {
     private final InputHandler inputHandler = new InputHandler();
 
     @Test
-    void parseCarNames_WhenValidInputProvided_ReturnsListOfCarNames() {
+    @DisplayName("올바른 입력을 받았을 때 자동차 이름 리스트를 반환한다")
+    void parseCarNames_Test() {
         String carNames = "pobi,woni,jun";  // given
         List<String> result = inputHandler.parseCarNames(carNames);  // when
         assertThat(result).containsExactly("pobi", "woni", "jun");  // then
