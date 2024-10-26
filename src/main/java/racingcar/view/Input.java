16166041e7ff97d5b530b5252bcf9a23f95ca5cd
jaeheon0826 <jaeheon0826@jbnu.ac.kr>
2024-10-26 15:59:03 +0900

    feat: 문자와 공백이 섞여있을 때 길이를 초과하지 않도록 수정
    문자와 공백이 섞일 때 길이를 카운트하는 것을 방지하기 위해
    trim()을 적용한 이후 예외처리 로직을 수행하도록 수정

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -1,16 +1,19 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Input {
 
     public List<String> getCarNames() {
         String[] carNamesInput = Console.readLine().split(",");
+        List<String> carNames = new ArrayList<>();
 
         Arrays.stream(carNamesInput).forEach(carName -> {
+            carName = carName.trim();
+
             if (carName.equals("null") || carName == null) {
                 throw new IllegalArgumentException("자동차 이름에 null이 포함되어 있습니다.");
             } else if (carName.isEmpty() || carName.trim().isEmpty()) {
@@ -18,10 +21,9 @@ public class Input {
             } else if (carName.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
             }
-        });
 
-        List<String> carNames = Arrays.stream(carNamesInput).map(String::trim)
-            .collect(Collectors.toList());
+            carNames.add(carName);
+        });
 
         if (carNames.size() != carNames.stream().distinct().count()) {
             throw new IllegalArgumentException("중복된 자동차 이름이 있습니다.");
