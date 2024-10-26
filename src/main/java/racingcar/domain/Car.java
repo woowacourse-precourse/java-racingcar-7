78c1824508ab95491a7bf2db62c67b3a1339af5c
Lee-Minju <mju7513@gmail.com>
2024-10-26 17:48:27 +0900

    domain\Cars.java | Car.java: 입력받은 자동차의 이름을 검증하는 기능을 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -6,13 +6,24 @@ public class Car {
     private int progress;
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
         this.progress = 0;
     }
 
     public Car(String name, int progress) {
+        validateName(name);
         this.name = name;
         this.progress = progress;
+    }
+
+    private void validateName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름은 공백이 될수 없습니다.");
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("5자 이하의 이름을 입력해주세요");
+        }
     }
 
     public String name() {
---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Cars {
+    private List<Car> cars;
+
+    Cars(List<String> carNames) {
+        validate(carNames);
+        this.cars = new ArrayList<>(carNames.size());
+        for (int i = 0; i < carNames.size(); i++) {
+            this.cars.add(new Car(carNames.get(i)));
+        }
+    }
+
+    private void validate(List<String> carNames) {
+        HashSet<String> duplicateCheck = new HashSet<>();
+        for (int i = 0; i < carNames.size(); i++) {
+            duplicateCheck.add(carNames.get(i));
+        }
+        if (duplicateCheck.size() != carNames.size()) {
+            throw new IllegalArgumentException("중복된 자동차 이름은 등록이 불가능합니다.");
+        }
+    }
+}
