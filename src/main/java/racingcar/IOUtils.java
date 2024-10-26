d41ec6730fc36a595fdee30fd365c7d345cbe0ec
sunjong0214 <rlatjswhd8200@gmail.com>
2024-10-26 21:05:22 +0900

    fix(IOUtils): String null 체크 로직 추가

--- src/main/java/racingcar/IOUtils.java
+++ src/main/java/racingcar/IOUtils.java
@@ -2,20 +2,23 @@ package racingcar;
 
 import java.util.Arrays;
 import java.util.List;
+import org.junit.platform.commons.util.StringUtils;
 
 public class IOUtils {
     public static List<String> splitInput(String input) {
+        if (StringUtils.isBlank(input) || input.charAt(input.length() - 1) == ',') {
+            throw new IllegalArgumentException("입력된 이름 중 빈 문자가 존재합니다.");
+        }
         var splitInput = input.split(",");
-        validateInput(input, splitInput);
+        validateSplitInput(splitInput);
         return Arrays.stream(splitInput).toList();
     }
 
     /**
-     * @param input      마지막에 ,이 되어있는지 확인
      * @param splitInput 배열 중간에 빈 문자가 있는지 확인
      */
-    private static void validateInput(String input, String[] splitInput) {
-        if (input.charAt(input.length() - 1) == ',' || Arrays.stream(splitInput).anyMatch(String::isBlank)) {
+    private static void validateSplitInput(String[] splitInput) {
+        if (Arrays.stream(splitInput).anyMatch(String::isBlank)) {
             throw new IllegalArgumentException("입력된 이름 중 빈 문자가 존재합니다.");
         }
     }
