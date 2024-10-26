0e02a8499f7efc6a24577f7d38d333f50d62a317
making-a-scene <4936joo@naver.com>
2024-10-26 21:16:34 +0900

    style(UI): 불필요한 import문 삭제

--- src/main/java/racingcar/UI.java
+++ src/main/java/racingcar/UI.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 import racingcar.domain.Car;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class UI {
     private final Validation validation = new Validation();
