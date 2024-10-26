2afc4f519ac37c4d477e8350ee0140748b496543
daeyeon <kdy4410121@naver.com>
2024-10-26 12:58:21 +0900

    feat: carMove 함수 생성

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,13 +1,22 @@
 package racingcar.domain;
 
-public class Car {
+import random.RandomGenerate;
+
+public class Car{
 
     private String carName;
     private int position;
+    private final int carCanMove = 4;
+    private RandomGenerate randomGenerate;
 
-    public Car(String carName){
+    public Car(String carName, RandomGenerate randomGenerate){
         this.carName = carName;
+        this.randomGenerate = randomGenerate;
     }
 
+    public void carMove(){
+        if(randomGenerate.generateRandomNumber() > carCanMove)
+            this.position++;
+    }
 
 }
