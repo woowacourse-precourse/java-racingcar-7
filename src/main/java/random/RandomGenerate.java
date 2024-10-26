4a2e7604f7d2ae0ffd8eaf616f1c9437257700a5
daeyeon <kdy4410121@naver.com>
2024-10-26 12:35:05 +0900

    feat: 0~9사이으 랜덤 값 생성하는 메서드 생성

--- src/main/java/random/RandomGenerate.java
+++ src/main/java/random/RandomGenerate.java
@@ -1,4 +1,16 @@
 package random;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public interface RandomGenerate {
+
+    public final int minNumber = 0;
+    public final static int maxNumber = 9;
+    default int generateRandomNumber(){
+        int randomNumber = Randoms.pickNumberInRange(minNumber, maxNumber);
+
+        return randomNumber;
+    }
+
+
 }
