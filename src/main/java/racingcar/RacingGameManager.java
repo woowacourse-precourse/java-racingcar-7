e08a08e6e02e492ae4ea5c013cdcb30a300537ba
yumi <yumkoori@gmail.com>
2024-10-26 17:52:50 +0900

    Feat: 클라이언트에서 사용될 RacingGame인터페이스 정의
    레이싱 게임을 진행을 수행할 RacingGameManager구현 클래스 추가

---
+++ src/main/java/racingcar/RacingGameManager.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import java.util.List;
+
+class RacingGameManager implements RacingGame {
+    @Override
+    public void ready(List<String> cars) {
+    }
+
+    @Override
+    public void start(int tryNumber) {
+    }
+}
