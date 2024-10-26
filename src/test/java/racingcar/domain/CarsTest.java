e57ce39dc7fb7ab1704ebf8851248f39660466bc
StoneCAU <seokwoo1999@naver.com>
2024-10-26 23:48:32 +0900

    test(CarsTest): Cars 테스트 코드 추가
    - 객체 생성 테스트, 기능 테스트로 분리
    - 객체 생성 테스트에 자동차 2대 미만 예외처리

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,50 +1,68 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import racingcar.utils.FixedNumberGenerator;
 
 import java.util.List;
 
 public class CarsTest {
+    @Nested
+    @DisplayName("객체 생성 테스트")
+    class CarsCreateTest {
+        @Test
+        void 자동차는_2대_이상이여야_한다() {
+            // given
+            final List<String> carNames = List.of("pobi");
 
-    @Test
-    void 각_자동차에_이름을_부여할_수_있다() {
-        // given
-        final List<String> carNames = List.of("pobi", "woni", "jun");
+            // when, then
+            assertThatThrownBy(() -> Cars.createFromCarNames(carNames))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("[ERROR] 자동차의 수는 2대 이상이어야 합니다.");
+        }
+        @Test
+        void 각_자동차에_이름을_부여할_수_있다() {
+            // given
+            final List<String> carNames = List.of("pobi", "woni", "jun");
 
-        // when
-        final Cars cars = Cars.createFromCarNames(carNames);
+            // when
+            final Cars cars = Cars.createFromCarNames(carNames);
 
-        final List<Car> actual = cars.getCars();
-        final List<Car> expected = List.of(new Car("pobi"), new Car("woni"), new Car("jun"));
+            final List<Car> actual = cars.getCars();
+            final List<Car> expected = List.of(new Car("pobi"), new Car("woni"), new Car("jun"));
 
-        // then
-        assertThat(actual)
-                .usingRecursiveFieldByFieldElementComparator()
-                .containsExactlyInAnyOrderElementsOf(expected);
+            // then
+            assertThat(actual)
+                    .usingRecursiveFieldByFieldElementComparator()
+                    .containsExactlyInAnyOrderElementsOf(expected);
+        }
     }
+    @Nested
+    @DisplayName("기능 테스트")
+    class CarsMethodTest {
+        @Test
+        void 무작위_값이_4_이상일_경우_전진한다() {
+            // given
+            final List<Car> carsList = List.of(
+                    new Car("pobi", new FixedNumberGenerator(4)), new Car("woni", new FixedNumberGenerator(0)));
 
-    @Test
-    void 무작위_값이_4_이상일_경우_전진한다() {
-        // given
-        final List<Car> carsList = List.of(
-                new Car("pobi", new FixedNumberGenerator(4)), new Car("woni", new FixedNumberGenerator(0)));
+            // when
+            final Cars cars = Cars.createFromCarsList(carsList);
+            cars.play();
 
-        // when
-        final Cars cars = Cars.createFromCarsList(carsList);
-        cars.play();
+            final List<Car> actual = cars.getCars();
+            final List<Car> expected = List.of(
+                    new Car("pobi", 1), new Car("woni", 0)
+            );
 
-        final List<Car> actual = cars.getCars();
-        final List<Car> expected = List.of(
-                new Car("pobi", 1), new Car("woni", 0)
-        );
-
-        // then
-        assertThat(actual)
-                .usingRecursiveFieldByFieldElementComparatorIgnoringFields("numberGenerator")
-                .containsExactlyInAnyOrderElementsOf(expected);
-
+            // then
+            assertThat(actual)
+                    .usingRecursiveFieldByFieldElementComparatorIgnoringFields("numberGenerator")
+                    .containsExactlyInAnyOrderElementsOf(expected);
+        }
     }
 }
