748556e81d31263b544ea34babff7119747263c1
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-10-26 17:43:35 +0900

    feat: 중복된 이름의 자동차가 있을 경우 예외처리

--- src/main/java/racingcar/domain/CarManager.java
+++ src/main/java/racingcar/domain/CarManager.java
@@ -1,8 +1,12 @@
 package racingcar.domain;
 
+import static racingcar.exception.ExceptionMessage.NOT_PERMIT_SAME_CAR;
+
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import racingcar.utils.RandomNumber;
 
 public class CarManager {
@@ -11,15 +15,15 @@ public class CarManager {
 
     private List<Car> cars;
 
-    public CarManager(){
+    public CarManager() {
         this.cars = new ArrayList<>();
     }
 
-    public List<Car> getCars(){
+    public List<Car> getCars() {
         return Collections.unmodifiableList(cars);
     }
 
-    public void setupCarsFromUserInput(String userInput){
+    public void setupCarsFromUserInput(String userInput) {
         List<String> split = List.of(userInput.split(INPUT_DELIMITER));
         initCars(split);
     }
@@ -32,9 +36,17 @@ public class CarManager {
     }
 
     private void initCars(List<String> carNames) {
+        verifyDuplicateCar(carNames);
         for (String carName : carNames) {
             Car car = new Car(carName);
             cars.add(car);
+        }
+    }
+
+    private void verifyDuplicateCar(List<String> carNames) {
+        Set<String> verifyCarNames = new HashSet<>(carNames);
+        if(carNames.size() != verifyCarNames.size()) {
+            throw new IllegalArgumentException(NOT_PERMIT_SAME_CAR.getMessage());
         }
     }
 
