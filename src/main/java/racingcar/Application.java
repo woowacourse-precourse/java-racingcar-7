ae4ac0eca7159bcad53fbcffa9d12ac21b254fc2
ahma0 <dsd932@naver.com>
2024-10-27 01:23:58 +0900

    feat: 사용자에게 자동차의 이름과 횟수 등을 입력받는 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,17 @@
 package racingcar;
 
+import racingcar.io.Input;
 import racingcar.io.Output;
 
 public class Application {
 
+    private static final Input input = new Input();
     private static final Output output = new Output();
 
     public static void main(String[] args) {
         output.printInputName();
-
+        String carNames = input.getUserAnswer();
         output.printExecuteCount();
+        String executeCount = input.getUserAnswer();
     }
 }
