9008357821785699d446a2d5013b4141a54724b9
Leegeonwoo7 <workgeon07@naver.com>
2024-10-27 02:32:06 +0900

    feat(RacingGame): RacingGame구현
    레이싱게임의 진행흐름을 관리하는 객체
    
    OutputView객체를 참조하는데 분리할 필요O

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -1,2 +1,30 @@
-package racingcar.model;public class RacingGame {
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class RacingGame {
+
+    private List<Car> cars;
+    private int roofCount;
+    OutputView outputView = new OutputView();
+
+    public RacingGame(List<Car> cars, int roofCount) {
+        this.cars = cars;
+        this.roofCount = roofCount;
+    }
+
+    public void startRacing() {
+        for (int i = 0; i < roofCount; i++) {
+            for (Car car : cars) {
+                int randomNumber = Randoms.pickNumberInRange(0, 9);
+                if (randomNumber >= 4){
+                    car.moveForward();
+                }
+            }
+            outputView.printCurrentResult(cars);
+        }
+    }
 }
