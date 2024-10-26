a8023ed5d0b9e30e976551b18462bd9f88137046
rhxfkg <rhxfkg@gmail.com>
2024-10-27 00:13:54 +0900

    Update Application.java

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int number = Integer.parseInt(Console.readLine());
+
+        InputProcess inputProcess = new InputProcess();
+        String[] carList = inputProcess.splitName(carNames);
+        inputProcess.checkName(carList);
+
+        int[] winnerCount = new int[carList.length];
+        Racing racing = new Racing();
+        Winner winner = new Winner();
+        for (int i = 0; i < number; i++) {
+            int[] racingResult = racing.racing(carList, number);
+            winner.checkWinner(racingResult, winnerCount);
+        }
+        System.out.print("최종 우승자 : ");
+        winner.printFinalWinner(carList, winnerCount);
     }
 }
