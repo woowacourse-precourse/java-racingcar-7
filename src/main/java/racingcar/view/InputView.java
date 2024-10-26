ed0f29a18a6fc3eff0ff9f6eca108984e2f03cdf
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-26 23:43:59 +0900

    feat(InputView): 사용자 입력 안내 메시지 출력 기능

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,30 @@
 package racingcar.view;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView implements View {
+
+    private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ATTEMPT_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
+    @Override
+    public void print() {
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
+    }
+
+    public String inputCarNames() {
+        return Console.readLine();
+    }
+
+    public void printInputAttemptCount() {
+        System.out.println(INPUT_ATTEMPT_COUNT_MESSAGE);
+    }
+
+    public String inputAttemptCount() {
+        return removeWhitespace(Console.readLine());
+    }
+
+    private String removeWhitespace(String input) {
+        return input.trim().replace(" ", "");
+    }
 }
