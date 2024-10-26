b38ee2402feed9a4c298bd21d1e8a92edd7a7767
강승연 <syeon.dev@gmail.com>
2024-10-27 00:21:41 +0900

    feat: 게임 매니저 인터페이스 구현

---
+++ src/main/java/racingcar/GameManager.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import java.util.ArrayList;
+
+public class GameManager {
+
+    ArrayList<Car> cars = new ArrayList<>();
+    int count;
+
+    //우승자 반환
+    public String[] StartGame(String[] car_list, int count){
+        return null;
+    }
+
+    private void SetCars(String[] car_list) {
+
+    }
+
+    private void SetTryCount(int count) {
+
+    }
+
+    private void PrintGame(){
+
+    }
+}
