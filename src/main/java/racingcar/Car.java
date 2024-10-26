01fb8e2fffe4fcd13fa0a9a361d2d6b00500475f
kangsan <devstar1223@gmail.com>
2024-10-26 19:57:18 +0900

    feat: 차수마다 무작위 값에 의한 자동차 전진 또는 정지

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,9 @@
 package racingcar;
 
+import racingcar.io.ConsoleOutputHandler;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Car {
     private final String carName;
     private int distance = 0; // 달린 누적 거리
@@ -7,4 +11,9 @@ public class Car {
     public Car(String carName) {
         this.carName = carName;
     }
+
+    public void moveForwardAttempt(int RANDOM_RANGE_MIN_NUMBER, int RANDOM_RANGE_MAX_NUMBER, int MOVE_FORWARD_MIN_NUMBER) {
+        if(pickNumberInRange(RANDOM_RANGE_MIN_NUMBER, RANDOM_RANGE_MAX_NUMBER) > MOVE_FORWARD_MIN_NUMBER) distance++;
+    }
+
 }
--- src/main/java/racingcar/CarRacingSimulation.java
+++ src/main/java/racingcar/CarRacingSimulation.java
@@ -10,6 +10,9 @@ import java.util.ArrayList;
 public class CarRacingSimulation {
     private final ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
     private final ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
+    private final int RANDOM_RANGE_MIN_NUMBER = 0;
+    private final int RANDOM_RANGE_MAX_NUMBER = 9;
+    private final int MOVE_FORWARD_MIN_NUMBER = 4; // 해당 숫자 이상일때 전진
     public void run() {
         consoleOutputHandler.racingCarNamingMessage(); // "자동차 이름 입력 안내" 메세지 출력
         String userInputCarNameString = consoleInputHandler.getUserInputCarNameString(); // 유저 입력 받기 (자동차 이름 문자열)
@@ -18,6 +21,7 @@ public class CarRacingSimulation {
         List<Car> carList = createCarList(carNameList); // 자동차 객체 리스트 생성
         consoleOutputHandler.attemptCountMessage(); // "시도 횟수 입력 안내" 메세지 출력
         int attemptCount = consoleInputHandler.getUserInputAttemptCount(); // 유저 입력 받기 (시도 횟수)
+        racingStart(carList,attemptCount);
     }
 
     private List<Car> createCarList(List<String> carNameList) {
@@ -26,5 +30,13 @@ public class CarRacingSimulation {
             cars.add(new Car(carName)); // 자동차 객체 생성하여 리스트에 추가
         }
         return cars;
+    }
+
+    private void racingStart(List<Car> carList, int attemptCount) {
+        for(int attempt = 0; attempt < attemptCount; attempt++){
+            for(Car car : carList){
+                car.moveForwardAttempt(RANDOM_RANGE_MIN_NUMBER,RANDOM_RANGE_MAX_NUMBER,MOVE_FORWARD_MIN_NUMBER);
+            }
+        }
     }
 }
