02fc73fb353e6277a158634d5d4d002591e45a18
AwesomeMinsik <beaut3579@gmail.com>
2024-10-26 14:33:16 +0900

    test : 이름 입력값에 대한 테스트 코드를 추가하였습니다

--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -10,10 +10,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class ValidatorTest {
 
     @Test
-    void 정수_검증_테스트(){
+    void 이름길이_초과_입력_테스트() {
+        List<String> input = List.of("abcdef", "123456");
+        assertThatThrownBy(() -> Validator.validateName(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 공백_입력_테스트() {
+        List<String> input = List.of("", " ");
+        assertThatThrownBy(() -> Validator.validateName(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @Test
+    void 정수_검증_테스트() {
         int result = Validator.validateCycle("4");
         assertEquals(4, result);
     }
+
     @Test
     void 정수가_아닐때_예외_테스트() {
         List<String> invalidInputs = List.of("A", "*", "0.1");
@@ -25,13 +41,13 @@ class ValidatorTest {
     }
 
     @Test
-    void 음수값_예외_테스트(){
+    void 음수값_예외_테스트() {
         assertThatThrownBy(() -> Validator.validateCycle("-1"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 공백_제로입력_예외_테스트(){
+    void 공백_제로입력_예외_테스트() {
         String input1 = " ";
         String input2 = "";
 
