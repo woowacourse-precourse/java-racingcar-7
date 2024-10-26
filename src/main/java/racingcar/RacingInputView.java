12fc0b562cacedf6f0f0644fb8ce276d89207741
soojinlee <sojinlee1004@naver.com>
2024-10-26 23:59:02 +0900

    fix : 횟수 입력 코드 문구 수정

--- src/main/java/racingcar/RacingInputView.java
+++ src/main/java/racingcar/RacingInputView.java
@@ -8,7 +8,7 @@ public class RacingInputView {
         return Console.readLine();
     }
     public int RacingRoundCount() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println("시도할 횟수는 몇 회인가요?");
         return Integer.parseInt(Console.readLine());
     }
 
