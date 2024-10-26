43fc6b148cf81f1fd142a07804890aab13fd7355
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-27 00:37:32 +0900

    fix: 중복된 이름이 들어올 경우 예외처리

--- src/main/java/racingcar/ValidateCar.java
+++ src/main/java/racingcar/ValidateCar.java
@@ -1,34 +1,30 @@
 package racingcar;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ValidateCar {
-    public static List<String> manageCar(String userInputCar) {
+    /*public static List<String> manageCar(String userInputCar) {
         validateInputCar(userInputCar);
         return ParsingCar.parseInputCarToList(userInputCar);
-    }
+    }*/
 
     public static void validateInputCar(String userInput) {
         validateEmptyInput(userInput);
         validateExtraComma(userInput);
-        validateEndWithComma(userInput);
 
-        for (String carName : userInput.split(",")) {
+        Set<String> carNames = new HashSet<>();
+        for (String carName : userInput.split(",", -1)) {
             String trimmedPart = carName.trim();
             validateEmptyPart(trimmedPart);
             validateNameLength(trimmedPart);
+            validateDuplicateNames(trimmedPart, carNames);
         }
     }
 
     private static void validateEmptyInput(String userInput) {
         if (userInput == null || userInput.isEmpty()) {
             throw new IllegalArgumentException("입력값이 비어있습니다.");
-        }
-    }
-
-    private static void validateEndWithComma(String userInput) {
-        if (userInput.endsWith(",")) {
-            throw new IllegalArgumentException("쉼표로 입력이 끝났습니다.");
         }
     }
 
@@ -48,6 +44,12 @@ public class ValidateCar {
     private static void validateNameLength(String trimmedPart) {
         if (trimmedPart.length() > 5) {
             throw new IllegalArgumentException("이름이 5자를 초과합니다.");
+        }
+    }
+
+    private static void validateDuplicateNames(String carName, Set<String> carNames) {
+        if (!carNames.add(carName)) {
+            throw new IllegalArgumentException("중복된 이름이 존재합니다.");
         }
     }
 }
