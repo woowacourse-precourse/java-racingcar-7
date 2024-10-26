39100be9675fb680c337690bf84fd1cf2d9f4426
honggyu1233@naver.com <honggyu1233@naver.com>
2024-10-27 01:29:20 +0900

    feat : InputView 수정 - 시도할 횟수를 문자로 입력받도록 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,7 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public static Object[] racingcarInputView() {
         String inputName=Console.readLine();
-        int moveCount=Integer.parseInt(Console.readLine());
+        System.out.println("inputName = " + inputName);
+
+        String moveCount = Console.readLine();
+        System.out.println("moveCount = " + moveCount);
         return new Object[]{inputName, moveCount};
     }
 }
