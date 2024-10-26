a2ee3ff67a231ca35555315f56daadee5de46fdf
haeseon lee <haeseon4816@gmail.com>
2024-10-26 12:00:30 +0900

    refactor: 입력 문구 경주 메서드로 이동

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -5,7 +5,10 @@ import java.util.List;
 public class Racing {
 
     public void race(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<Car> carList = Cars.getCarList();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
         int tryNumber = User.getTryNumber();
 
         System.out.println();
