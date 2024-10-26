f541265abcc352c693ba99f1a5cd52c18ad400da
unh6unh6 <ruddbs3308@naver.com>
2024-10-26 15:50:40 +0900

    test(view): 자동차들의 이동거리 출력

--- src/test/java/racingcar/view/ProgressViewTest.java
+++ src/test/java/racingcar/view/ProgressViewTest.java
@@ -4,8 +4,11 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.dto.CarDto;
 
 public class ProgressViewTest {
     ProgressView progressView;
@@ -24,5 +27,23 @@ public class ProgressViewTest {
 
         assertThat(outputStream.toString().trim())
                 .isEqualTo("실행 결과");
+    }
+
+    @Test
+    void 자동차들의_이동거리_출력() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        List<CarDto> carDtoList = Arrays.asList(
+                new CarDto("pobi", 2),
+                new CarDto("woni", 4),
+                new CarDto("jun", 3)
+        );
+
+        progressView.printProgressMessage(carDtoList);
+
+        assertThat(outputStream.toString().trim())
+                .contains("pobi : --", "woni : ----", "jun : ---");
+
     }
 }
