9291a764559d00d42450e9f04657a612ea0ef40c
fbehddn <tom0611@naver.com>
2024-10-26 21:44:50 +0900

    refactor: 컨틀로러 초기화 및 게임 실행 흐름 간소화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,11 +4,7 @@ import racingcar.controller.CarController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         CarController carController = new CarController();
-
-        carController.init();
-        carController.start();
-        carController.printWinners();
+        carController.startGame();
     }
 }
