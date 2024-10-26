352d9b99affc1b441e23b91d16a625fc32fc0db2
steamedEggMaster <cksdud5014@naver.com>
2024-10-26 13:21:18 +0900

    test(InputParserTest): 시도 횟수 int 변환 테스트 완료
    1. 시도 횟수 문자 입력 시 예외 처리 테스트
    2. 시도 횟수 0 이하 값 입력 시 예외 처리 테스트
    3. README.md 기능 구현 완료 사항 최신화

--- src/test/java/racingcar/model/InputParserTest.java
+++ src/test/java/racingcar/model/InputParserTest.java
@@ -9,32 +9,59 @@ import org.junit.jupiter.api.Test;
 public class InputParserTest {
 
     @Test
-    @DisplayName("쉼표를 통한 자동차 이름 파싱")
-    void parseCarNames_withValidInput() {
+    @DisplayName("정상적인 자동차 이름, 시도 횟수 입력 테스트")
+    void parseCarNamesAndTryCount_withValidInput() {
         String carNames = "car1,car2,car3";
-        InputParser inputParser = new InputParser(carNames);
+        String tryCount = "5";
+        InputParser inputParser = new InputParser(carNames, tryCount);
 
         assertThat(inputParser.getCarNameList())
             .containsExactly("car1", "car2", "car3");
+        assertThat(inputParser.getTryCount())
+            .isEqualTo(5);
     }
 
     @Test
     @DisplayName("자동차 이름 5자 초과 시 예외 발생")
     void validateLength_withExceededLength() {
         String carNames = "car12345,car2";
+        String tryCount = "5";
 
-        assertThatThrownBy(() -> new InputParser(carNames))
+        assertThatThrownBy(() -> new InputParser(carNames, tryCount))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("자동차 이름의 길이가 5를 초과하였습니다.");
     }
 
     @Test
     @DisplayName("자동차 이름 빈 문자열 입력 시 예외 발생")
-    void throwExceptionWhenCarNamesIsEmpty() {
+    void throwException_whenCarNamesIsEmpty() {
         String carNames = "";
+        String tryCount = "5";
 
-        assertThatThrownBy(() -> new InputParser(carNames))
+        assertThatThrownBy(() -> new InputParser(carNames, tryCount))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("자동차 이름이 아무것도 들어오지 않았습니다.");
+    }
+
+    @Test
+    @DisplayName("시도 횟수로 문자 입력 시 예외 발생")
+    void throwException_whenTryCountIsNotANumber() {
+        String carNames = "car1,car2,car3";
+        String tryCount = "gafds";
+
+        assertThatThrownBy(() -> new InputParser(carNames, tryCount))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("알맞지 않은 시도 횟수 값이 들어왔습니다.");
+    }
+
+    @Test
+    @DisplayName("시도 횟수로 0 이하 값 입력 시 예외 발생")
+    void throwException_whenTryCountIsZeroOrNegative() {
+        String carNames = "car1,car2,car3";
+        String tryCount = "-1";
+
+        assertThatThrownBy(() -> new InputParser(carNames, tryCount))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("알맞지 않은 시도 횟수 값이 들어왔습니다.");
     }
 }
