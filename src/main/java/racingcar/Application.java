052b2a9a554bb4d2efe6cbbcd034218989b59531
DESKTOP-TMDOSVL\user <tlsekdn505@naver.com>
2024-10-26 16:19:39 +0900

    사용자에게 자동차 이름 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List <String> carNames = getCarNames();
+        System.out.println("입력한 자동차 이름"  + carNames);
     }
+
+    private static List<String> getCarNames(){
+        System.out.println("자동차 이름을 입력해주세요.");
+        String inputCarName = Console.readLine();
+        return Arrays.asList(inputCarName.split(","));
+    }
+
 }
