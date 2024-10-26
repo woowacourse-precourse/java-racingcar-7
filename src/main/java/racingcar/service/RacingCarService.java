9557c0a5e9b2b4caa1fc0779852ae03ea79a2fbf
kim jihyeon <jihyeon546@gmail.com>
2024-10-26 23:08:03 +0900

    feat(service): 자동차 이름 문자열 분리 기능 추가

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -1,11 +1,18 @@
 package racingcar.service;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class RacingCarService {
 
     public Set<String> splitCarName(String carNameList) {
+        Set<String> carNames = new HashSet<>();
+        String[] carNameArray = carNameList.split(",");
 
-        return null;
+        for(String name: carNameArray){
+            carNames.add(name.trim());
+        }
+
+        return carNames;
     }
 }
