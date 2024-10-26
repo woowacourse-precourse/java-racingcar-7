bc9fa642c9d7417af664a2497303d46b88b85384
seoyeon2001 <syeon1250@naver.com>
2024-10-26 17:08:24 +0900

    Feat: 개별 차에 대한 정보 구현

--- src/main/java/racingcar/service/Car.java
+++ src/main/java/racingcar/service/Car.java
@@ -4,8 +4,24 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
     private String name;
+    private int score = 0;
+
     public Car(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void move() {
+        if (isMove()) {
+            score++;
+        }
     }
 
     public boolean isMove() {
