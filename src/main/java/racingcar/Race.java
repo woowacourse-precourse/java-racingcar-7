bf0d271f358fac68628801ea240ef0d6db701ab2
yyysolhhh <solbaram37@gmail.com>
2024-10-27 00:59:01 +0900

    feat(Race): startRace, playRound 메서드 추가
    startRace
    - 경기 매 회차별로 진행
    
    playRound
    - 각 회차에서 세부적인 과정 진행

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import java.util.List;
+import racingcar.io.Output;
+
+public class Race {
+//    private final Cars cars; // TODO
+
+    public void startRace(List<Car> cars, int numOfRounds) {
+        Output.printRoundResultMessage();
+        for (int round = 0; round < numOfRounds; round++) {
+            playRound(cars);
+            Output.printRoundResult(cars);
+        }
+    }
+
+    private static void playRound(List<Car> cars) {
+        for (Car car : cars) {
+            car.move(RandomNumber.createRandomNumber());
+        }
+    }
+}
