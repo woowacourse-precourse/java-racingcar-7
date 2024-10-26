f5c4e4cb892c08393d51995e37dc9b35bf716520
minSsan <msp1125@hanyang.ac.kr>
2024-10-27 02:07:16 +0900

    feat(CarRacingManager): 도메인 비즈니스 로직 추가 - 자동차 경주

--- src/main/java/racingcar/domain/model/constant/CarConstants.java
+++ src/main/java/racingcar/domain/model/constant/CarConstants.java
@@ -2,4 +2,5 @@ package racingcar.domain.model.constant;
 
 public class CarConstants {
     public static final int MAX_LEN = 5;
+    public static String JOIN_DELIMITER = ", ";
 }
---
+++ src/main/java/racingcar/domain/service/CarRacingManager.java
@@ -0,0 +1,52 @@
+package racingcar.domain.service;
+
+import racingcar.domain.model.car.Car;
+import racingcar.domain.model.constant.CarConstants;
+import racingcar.domain.util.CarNameParser;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CarRacingManager {
+    private final Map<String, Car> cars = new HashMap<>();
+
+    private CarRacingManager(List<Car> carList) {
+        carList.forEach(car -> {
+            cars.put(car.getName(), car);
+        });
+    }
+
+    public static CarRacingManager from(final String input) {
+        List<String> parsed = CarNameParser.parse(input);
+        List<Car> carList = parsed.stream()
+                .map(Car::from)
+                .toList();
+        return new CarRacingManager(carList);
+    }
+
+    public void moveForward(final String name) {
+        cars.get(name).moveForward();
+    }
+
+    public String getWinners() {
+        return getFirstPrizeCarList().stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(CarConstants.JOIN_DELIMITER));
+    }
+
+    private List<Car> getFirstPrizeCarList() {
+        List<Car> ranked = getRankedCarList();
+        final int maxDistance = ranked.getFirst().getDistance();
+        return ranked.stream()
+                .filter(c->c.getDistance() == maxDistance)
+                .toList();
+    }
+
+    private List<Car> getRankedCarList() {
+        return cars.values().stream()
+                .sorted((a, b) -> b.getDistance() - a.getDistance())
+                .toList();
+    }
+}
