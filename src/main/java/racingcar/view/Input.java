f3cfa179ac5eae3cc9f4873d07b61fe363d95d05
Ahnsukjoo <bos98@naver.com>
2024-10-26 17:44:58 +0900

    feat: 자동차의 이름을 입력할 때, 5자를 초과하면 예외로 처리
    - checkCarNamesError메서드를 구현해서 readLine()으로 들어오는 값의 에러를 한번 확인하고 반환 시켜주게 수정
    - null 값이 입력될 경우 예외 처리
    - 이름에 앞,뒤의 공백을 제외하고 5자를 초과하면 예외로 처리되게 구현
    - 이름 앞,뒤에 있는 공백은 제거

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -4,14 +4,33 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import org.assertj.core.internal.IterableElementComparisonStrategy;
 
 public class Input {
 
-    public HashMap<String,Integer> getCarNames() {
-        String[] input = readLine().split(",");
-        HashMap<String,Integer> carNames = new HashMap<>();
-        Arrays.stream(input).forEach(s->carNames.put(s, 0));
+    public HashMap<String, Integer> getCarNames() {
+        String[] carNameList = checkCarNamesError(readLine());
+        HashMap<String, Integer> carNames = new HashMap<>();
+        Arrays.stream(carNameList).forEach(s -> carNames.put(s, 0));
         return carNames;
+    }
+
+    public String[] checkCarNamesError(String readLine) {
+        String inputCarNames = readLine;
+        if (inputCarNames == null) {
+            throw new IllegalArgumentException("입력값이 null입니다.");
+        }
+        String[] carNameList = inputCarNames.split(",");
+        Arrays.stream(carNameList).forEach(name -> {
+            name = name.trim();
+            if (name.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+            if (name.isEmpty()) {
+                throw new IllegalArgumentException("이름을 공백으로 할 수 없습니다.");
+            }
+        });
+        return Arrays.stream(carNameList).map(String::trim).toArray(String[]::new);
     }
 
     public int getAttemptedNumber() {
