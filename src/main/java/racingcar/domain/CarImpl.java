3044cdc1edf498f9faaf8bd888bdbfc58f9ec846
rootTiket <l20078829@gmail.com>
2024-10-26 22:44:56 +0900

    feat<Car>: Car 도메인 생성

---
+++ src/main/java/racingcar/domain/CarImpl.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+public class CarImpl implements Car{
+    private String name;
+    private int score;
+
+    private CarImpl(String name ,int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public static Car from(String name) {
+        return new CarImpl(name, 0);
+    }
+
+    public void run() {
+        this.score += 1;
+    }
+}
