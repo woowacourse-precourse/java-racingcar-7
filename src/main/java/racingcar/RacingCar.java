9d06622390e3eef0dbd5617efdd63fe1c9562cca
sseongyunn <sseongyunn@korea.ac.kr>
2024-10-27 01:16:02 +0900

    feat: RacingCar 객체의 필드, 생성자, setCarName() 메서드 구현
    - RacingCarTest 파일의 failSetCarNameTest1, failSeteCarNameTest2, successCarNameTest
    세 테스트 코드를 통과하기 위한 코드를 작성했습니다.
    -RacingCar 객체에 carName, currentLocation, currentDistance 필드 생성했습니다.
    -RacingCar의 생성자에서 각 필드를 초기화하도록 설정하였습니다.
    -RacingCar 객체의 CarName 필드에 이름을 저장하는 setCarName 메서드를 구현하였습니다.
    -setCarName 메서드는 이름 형식에 맞지 않는 문자열이 전달될 경우,
    IllegalArgumentException을 던져 예외처리합니다.

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,4 +1,27 @@
 package racingcar;
 
+import java.util.Objects;
+
 public class RacingCar {
+    String carName;
+    String currentLocation;
+    int currentDistance;
+
+    RacingCar(){
+        this.carName = "";
+        this.currentLocation = "";
+        this.currentDistance = 0;
+    }
+
+    // 한글 영어 포함 정규 표현식 /^[가-힣a-zA-Z]+$/
+    void setCarName(String name) throws Exception{
+
+        if (name.matches("^[(가-힣a-zA-Z)]{1,5}$")) {
+            this.carName = name;
+        }
+        else{
+            throw new IllegalArgumentException("이름은 5글자 이하의 한글 또는 영어로 구성되어야 합니다.");
+        }
+
+    }
 }
