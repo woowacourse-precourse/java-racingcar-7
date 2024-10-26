d3a11fda354ea9d8e7032af149d3c49c0159b8ce
yumi <yumkoori@gmail.com>
2024-10-26 19:02:28 +0900

    Feat: 사용자와 상호작용을 위한 Application 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingGame racingGame = new RacingGameManager();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> cars = Arrays.stream(Console.readLine().split(",")).toList();
+        racingGame.ready(cars);
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int tryNumber = Integer.parseInt(Console.readLine());
+        racingGame.start(tryNumber);
+
     }
 }
