296912117b9f4e1ccaccab47acaf46c1cdcce894
yushinc <cys74351@gmail.com>
2024-10-26 22:52:29 +0900

    feat(Application.main): Ramdom을 통한 레이스 진행 후 우승자 출력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,17 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import org.junit.platform.commons.util.StringUtils;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
         String carsString = Console.readLine().replaceAll(" ", "");
 
         System.out.println("시도할 횟수는 몇 회인가요?");
@@ -19,5 +23,38 @@ public class Application {
         if (carNames.stream().anyMatch(name -> name.length() > 5)) {
             throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
         }
+
+        int [] progressCounts = new int[carNames.size()];
+
+        for (int i = 0; i < tryNum; i++) {
+            for (int k = 0; k < carNames.size(); k++) {
+                if (Randoms.pickNumberInRange(0, 9) > 5) {
+                    progressCounts[k] += 1;
+                }
+                String progressBar = "-".repeat(progressCounts[k]);
+                System.out.println(carNames.get(k).toString() + " : " + progressBar);
+            }
+            System.out.println();
+        }
+
+        List<String> winner = new ArrayList<>();
+        int max = 0;
+
+        for (int i = 0; i < progressCounts.length; i++) {
+            if (progressCounts[i] > max) {
+                max = progressCounts[i];
+            }
+        }
+
+        for (int i = 0; i < progressCounts.length; i++) {
+            if (progressCounts[i] == max) {
+                winner.add(carNames.get(i));
+            }
+        }
+
+        String winnerNames = String.join(",", winner);
+
+        System.out.println("최종 우승자 : " + winnerNames);
+
     }
 }
