8145b09153d731713b6b7b493c7fc542aa85cc5e
kkonii <119216472+kkonii@users.noreply.github.com>
2024-10-27 01:46:54 +0900

    refactor : 이름 요청 문구를 출력하는 구조 변경

---
+++ src/main/java/racingcar/global/Symbol.java
@@ -0,0 +1,18 @@
+package racingcar.global;
+
+public enum Symbol {
+
+    NAME_DELIMITER(",", "쉼표");
+
+    private final String symbol;
+    private final String name;
+
+    Symbol(String symbol, String name) {
+        this.symbol = symbol;
+        this.name = name;
+    }
+
+    public String formatMessageOf(String message) {
+        return String.format(message, this.name, this.symbol);
+    }
+}
--- src/main/java/racingcar/view/RacingView.java
+++ src/main/java/racingcar/view/RacingView.java
@@ -1,13 +1,16 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.global.Symbol;
 
 public class RacingView {
 
-    private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 %s(%s) 기준으로 구분)";
 
     public String requestNamesInput() {
-        System.out.println(NAME_INPUT_MESSAGE);
+        System.out.println(Symbol.NAME_DELIMITER
+                .formatMessageOf(NAME_INPUT_MESSAGE)
+        );
 
         return Console.readLine();
     }
