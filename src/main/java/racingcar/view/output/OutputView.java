43e6d447265f549c59b43eaad8c122b88f27c6af
likerhythm <wjdalsdl0412@gmail.com>
2024-10-26 13:13:52 +0900

    refactor(OutputPrinter): OutputPrinter 기능 이전
    이전에 OutputView를 RacingCarGame으로부터 분리함으로써
    OutputView의 기능이 printer 역할만 수행
    따라서 OutputPrinter 기능을 OutputView로 이전

--- src/main/java/racingcar/view/output/OutputView.java
+++ src/main/java/racingcar/view/output/OutputView.java
@@ -1,12 +1,80 @@
 package racingcar.view.output;
 
-import racingcar.GameResult;
+import racingcar.game.GameResult;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
-    private final OutputPrinter outputPrinter = new OutputPrinter();
+    private BigInteger totalMaxPosition = BigInteger.ZERO;
+    private int attemptCount;
+    private Map<String, List<BigInteger>> carPositionLog;
 
     public void printResult(GameResult gameResult) {
-        outputPrinter.printResult(gameResult);
+        init(gameResult);
+        print();
+    }
+
+    private void init(GameResult gameResult) {
+        this.attemptCount = gameResult.getAttemptCount();
+        this.carPositionLog = gameResult.getCarPositionLog();
+    }
+
+    private void print() {
+        System.out.println("\n실행 결과");
+        printCarMovement();
+        printWinner();
+    }
+
+    public void printCarMovement() {
+        for (int step = 0; step < attemptCount; step++) {
+            printStepResult(step);
+            System.out.println();
+        }
+    }
+
+    public void printWinner() {
+        List<String> winners = new ArrayList<>();
+        for (String name : carPositionLog.keySet()) {
+            List<BigInteger> resultPositions = carPositionLog.get(name);
+            BigInteger resultPosition = resultPositions.getLast();
+            if (totalMaxPosition.compareTo(resultPosition) == 0) {
+                winners.add(name);
+            }
+        }
+        System.out.print("최종 우승자 : ");
+        System.out.println(String.join(", ", winners));
+    }
+
+    private void printStepResult(int step) {
+        for (String name : carPositionLog.keySet()) {
+            List<BigInteger> positions = carPositionLog.get(name);
+            BigInteger stepPosition = positions.get(step);
+
+            printCarName(name);
+            printStepPosition(stepPosition);
+
+            updateTotalMaxPosition(stepPosition);
+        }
+    }
+
+    private void updateTotalMaxPosition(BigInteger stepPosition) {
+        if (totalMaxPosition.compareTo(stepPosition) < 0) {
+            totalMaxPosition = stepPosition;
+        }
+    }
+
+    private void printCarName(String name) {
+        System.out.print(name + " : ");
+    }
+
+    private void printStepPosition(BigInteger stepPosition) {
+        for (BigInteger i = BigInteger.ZERO; i.compareTo(stepPosition) < 0; i = i.add(BigInteger.ONE)) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 }
