238efc8bfb741583973bbc5f0eaeff9e28c4a570
DongHoon Lee <dhl1924@naver.com>
2024-10-27 01:50:02 +0900

    feat(exception): CarNamesInput에 null 유효성 검증 추가
    - CarNamesInput 생성 시 carNames 필드가 null인 경우 예외를 발생시키도록 유효성 검증 로직 추가

--- src/main/java/racingcar/dto/CarNamesInput.java
+++ src/main/java/racingcar/dto/CarNamesInput.java
@@ -1,6 +1,18 @@
 package racingcar.dto;
 
+import racingcar.exception.ErrorMessage;
+import racingcar.exception.RacingCarGameException;
+
 public record CarNamesInput(
-	String CarNames
+	String carNames
 ) {
+	public CarNamesInput {
+		validateCarNamesNotNull(carNames);
+	}
+
+	private void validateCarNamesNotNull(String carNames) {
+		if (carNames == null) {
+			throw RacingCarGameException.from(ErrorMessage.NULL_INPUT_ERROR);
+		}
+	}
 }
