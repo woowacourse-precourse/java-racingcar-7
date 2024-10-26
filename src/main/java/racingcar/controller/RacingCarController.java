939b514fbcf0855e379c204645bc7c1a18a3e22a
Chaewon <corndots@naver.com>
2024-10-27 08:37:58 +0900

    refactor(RacingCarController): 타 클래스에 대한 의존성 감소
    InputProcessor, GameManager, ResultPrinter로 역할을 분리

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,36 +1,21 @@
 package racingcar.controller;
 
-import racingcar.domain.Car;
-import racingcar.domain.RacingGame;
-import racingcar.validator.InputValidator;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import racingcar.model.GameManager;
+import racingcar.model.InputProcessor;
+import racingcar.model.ResultPrinter;
 
 public class RacingCarController {
     public void startGame() {
-        List<String> carNames = InputView.getCarNames();
-        InputValidator.validateCarNames(carNames);
+        InputProcessor inputProcessor = new InputProcessor();
+        GameManager gameManager = new GameManager();
+        ResultPrinter resultPrinter = new ResultPrinter();
 
-        int rounds = InputView.getRounds();
-        InputValidator.validateRounds(rounds);
+        var carNames = inputProcessor.getValidatedCarNames();
+        int rounds = inputProcessor.getValidatedRounds();
 
-        List<Car> cars = carNames.stream()
-                .map(Car::new)
-                .collect(Collectors.toList());
+        gameManager.setupGame(carNames, rounds);
+        gameManager.playRounds();
 
-        RacingGame game = new RacingGame(cars, rounds);
-
-        OutputView.printExecutionResultMessage();
-        for (int i = 0; i < rounds; i++) {
-            game.playRound();
-            OutputView.printRoundResult(game.getCars());
-        }
-
-        // 최종 우승자 출력
-        List<Car> winners = game.getWinners();
-        OutputView.printWinners(winners);
+        resultPrinter.printResults(gameManager.getCars(), gameManager.getWinners());
     }
 }
