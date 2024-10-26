c42cfcd3ba0fa32a69d6e5c3675fcc08adb2bfb1
이윤빈 <kanado20000@naver.com>
2024-10-26 11:30:59 +0900

    :sparkles: feat: 자동차 이름 관련 유효성 검사를 관리는 로직 추가
    validateCarNumber 메서드가 입력된 자동차 이름이 2개 이상인지 확인하고,
    validateDuplicateCarName 메서드는 자동차 이름중에 중복이 있는지 확인한다.
    validateCarName 메서드는 비어있는 이름이 있는지, 길이가  6이상인 이름이
    있는지, 숫자로만 구성된 이름이 있는지, 특수문자를 포함한 이름이 있는지
    확인하는 역할을 하고 있다.

--- src/main/java/racingcar/Enum/ErrorMessage.java
+++ src/main/java/racingcar/Enum/ErrorMessage.java
@@ -1,9 +1,14 @@
 package racingcar.Enum;
 
+
 public enum ErrorMessage implements SystemMessage {
-  INVALID_CAR_NAME("유효하지 않은 자동차 이름이 포함되어 있습니다."),
+  INVALID_CAR_NAME_EMPTY("비어있는 자동차 이름이 존재합니다."),
+  INVALID_CAR_NAME_LENGTH("자동차 이름은 5자 이하만 가능합니다."),
+  INVALID_CAR_NAME_NUMERIC_ONLY("자동차 이름이 숫자로만 구성될 수 없습니다."),
+  INVALID_CAR_NAME_INCLUDE_SPECIAL_CHAR("자동차 이름에 특수문자가 포함될 수 없습니다."),
   INVALID_CAR_NUMBER("경기에 최소 2대 자동차가 참여할 수 있습니다."),
-  INVALID_TURN_NUMBER("자연수가 아닌 숫자입니다.");
+  INVALID_TURN_NUMBER("자연수가 아닌 숫자입니다."),
+  INVALID_DUPLICATE_CAR_NAME("중복된 자동차 이름 있습니다.");
 
   private final String message;
 
---
+++ src/main/java/racingcar/Validation/CarValidator.java
@@ -0,0 +1,41 @@
+package racingcar.Validation;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import racingcar.Enum.ErrorMessage;
+
+public class CarValidator {
+  private static final String regex = "[^a-zA-Z0-9]";
+
+  public static void validateCarNumber(List<String> splittedNames) {
+    if (splittedNames.size() == 1) {
+      throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NUMBER.getMessage());
+    }
+  }
+
+  public static void validateDuplicateCarName(List<String> splittedNames) {
+    Set<String> uniqueNames = new HashSet<>();
+    for (String name : splittedNames) {
+      if (!uniqueNames.add(name)) {
+        throw new IllegalArgumentException(ErrorMessage.INVALID_DUPLICATE_CAR_NAME.getMessage());
+      }
+    }
+  }
+
+  public static void validateCarName(String name) {
+    if (name.trim().isEmpty()) {
+      throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAME_EMPTY.getMessage());
+    }
+    if (name.length() > 5) {
+      throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAME_LENGTH.getMessage());
+    }
+    if (name.matches("\\d+")) {
+      throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAME_NUMERIC_ONLY.getMessage());
+    }
+    if (name.matches(".*" + regex + ".*")) {
+      throw new IllegalArgumentException(
+          ErrorMessage.INVALID_CAR_NAME_INCLUDE_SPECIAL_CHAR.getMessage());
+    }
+  }
+}
