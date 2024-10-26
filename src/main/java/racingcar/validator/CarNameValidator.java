8757f94df6f4d18fede355d32a30c91066578122
Jang-GO <jang8195@naver.com>
2024-10-26 18:12:58 +0900

    feat(CarNameValidator): 차 이름 검증 로직 구현

--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -1,7 +1,25 @@
 package racingcar.validator;
 
-public class CarNameValidator {
-    public static void validate(final String carName) {
+import racingcar.message.ExceptionMessage;
 
+import static racingcar.message.ExceptionMessage.*;
+
+public class CarNameValidator {
+
+    public static void validate(String name) {
+        validateBlank(name);
+        validateCarNameLength(name);
+    }
+
+    private static void validateCarNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(CAR_NAME_OVER_FIVE_LENGTH.getMessage());
+        }
+    }
+
+    private static void validateBlank(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException(CAR_NAME_NOT_BLANK.getMessage());
+        }
     }
 }
