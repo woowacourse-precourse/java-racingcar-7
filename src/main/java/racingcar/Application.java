6dc6c8c4ebd469adc059ff7da4e53be462e041fa
patternplane <bss011202@naver.com>
2024-10-26 23:34:22 +0900

    feat: 시도 횟수 입력받는 기능 추가
    - 입력값 검증 : 입력값은 0 이상의 정수여야 합니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,11 +6,30 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         List<String> carNames = getCarNames();
+        int tryCount = getTryCount();
     }
     
     private static List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNamesStr = Console.readLine();
         return CarNameParser.parse(carNamesStr, 5);
+    }
+    
+    private static int getTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int tryCount;
+        
+        try {
+            tryCount = Integer.parseInt(Console.readLine());
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+        
+        if (tryCount < 0) {
+            throw new IllegalArgumentException();
+        }
+        
+        return tryCount;
     }
 }
