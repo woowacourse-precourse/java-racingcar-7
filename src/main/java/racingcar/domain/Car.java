b6788eff55e0f44f41700a40b42d285b0ae2f37b
Chaewon <corndots@naver.com>
2024-10-27 08:30:22 +0900

    refactor(Car): 코드 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,24 +1,25 @@
 package racingcar.domain;
 
 public class Car {
-    private final String name;
-    private int position = 0;
+    private String name;
+    private int position;
 
     public Car(String name) {
         this.name = name;
+        this.position = 0;
     }
 
     public void move(int randomValue) {
         if (randomValue >= 4) {
-            position++;
+            this.position++;
         }
-    }
-
-    public String getName() {
-        return name;
     }
 
     public int getPosition() {
         return position;
+    }
+
+    public String getName() {
+        return name;
     }
 }
