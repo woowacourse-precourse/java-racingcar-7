4324ed500216c8279dd9a6ddca29b8d723e6999a
Yunsu4 <ysa020428@gmail.com>
2024-10-26 23:38:47 +0900

    feat: create the car to race as an object

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,34 @@
+package racingcar.model;
+
+import racingcar.util.CarNameValidator;
+
+public class Car {
+
+    private final String carName;
+    private int moveCount = 0;
+
+    public Car(String carName, Integer moveCount) {
+        CarNameValidator.validate(carName);
+        this.carName = carName;
+        this.moveCount = moveCount;
+    }
+
+    public Car(String carName) {
+        CarNameValidator.validate(carName);
+        this.carName = carName;
+    }
+
+    public void addMoveCount() {
+        moveCount++;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+
+
+}
