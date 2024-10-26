9127756d21426ac9853fb84f3b96fbf96743d3e0
honggyu1233@naver.com <honggyu1233@naver.com>
2024-10-27 02:43:44 +0900

    feat : 입력 받은 문자열을 활용해 Racingcar 객체를 만드는 기능 구현

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -1,4 +1,27 @@
 package racingcar.controller;
 
+import racingcar.model.Racingcar;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingcarController {
+
+
+
+
+    public List<Racingcar> createRacingcars(String racingcarNames) {
+        String[] racingcars = racingcarNames.split(",");
+        List<Racingcar> racingcarsList = new ArrayList<>();
+
+        for(String racingcarName : racingcars) {
+            racingcarName = racingcarName.trim();
+            if (racingcarName.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+            racingcarsList.add(new Racingcar(racingcarName));
+        }
+        return racingcarsList;
+    }
 }
