02dac26c8f92cc48dd805a7b583220ee48068933
wogus <ind07152@naver.com>
2024-10-26 16:20:33 +0900

    feat(ErrorMessage): 예외 메시지 상수 추가
    enum으로 예외 메시지를 상수화한다.

---
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -0,0 +1,18 @@
+package racingcar.exception;
+
+public enum ErrorMessage {
+    NOT_INTEGER("횟수 입력은 숫자만 입력할 수 있습ㄴ다."),
+    INVALID_COUNT_RANGE("횟수는 1회 이상이여야합니다."),
+    INVALID_NAME_LENGTH("자동차 이름은 5자 이하여야합니다."),
+    INVALID_NAME_COUNT("자동차 이름은 2개 이상 입력해야합니다."),
+    NAME_DUPLICATE("중복된 이름이 없어야합니다.");
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
