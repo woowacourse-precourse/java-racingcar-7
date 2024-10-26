6d186c9064dfc8da4b48148c2c80ffb167300344
nns503 <73779581+nns503@users.noreply.github.com>
2024-10-26 23:48:52 +0900

    refactor: 코드 모듈화

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -3,14 +3,12 @@ package racingcar.service;
 import racingcar.domain.Car;
 import racingcar.dto.RacingResponse;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class RacingService {
 
-    public RacingResponse racing(String[] carName, int attemptCount) {
+    public RacingResponse startRace(String[] carName, int attemptCount) {
         List<Car> cars = getCars(carName);
         List<Map<String, Integer>> moveData = getMoveData(attemptCount, cars);
         List<String> winners = getWinners(cars);
@@ -18,46 +16,46 @@ public class RacingService {
         return new RacingResponse(moveData, winners);
     }
 
+    private List<Car> getCars(String[] carName) {
+        return Arrays.stream(carName)
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
     private List<Map<String, Integer>> getMoveData(int attemptCount, List<Car> cars) {
         List<Map<String, Integer>> moveData = new ArrayList<>();
-
-        for(int i=0; i<attemptCount; i++) {
-            Map<String, Integer> move = new HashMap<>();
-            for (Car car : cars) {
-                car.move();
-                move.put(car.getName(), car.getDistance());
-            }
-            moveData.add(move);
+        while(attemptCount-- > 0) {
+            Map<String, Integer> moveSnapshot = getCarMoveSnapshot(cars);
+            moveData.add(moveSnapshot);
         }
 
         return moveData;
     }
 
-    private List<String> getWinners(List<Car> cars) {
-        List<String> winners = new ArrayList<>();
-        int max = -1;
-
-        for(Car car : cars){
-            if(max < car.getDistance()){
-                max = car.getDistance();
-                winners.clear();
-                winners.add(car.getName());
-            }
-            else if(max == car.getDistance()){
-                winners.add(car.getName());
-            }
+    private Map<String, Integer> getCarMoveSnapshot(List<Car> cars) {
+        Map<String, Integer> move = new HashMap<>();
+        for (Car car : cars) {
+            car.move();
+            move.put(car.getName(), car.getDistance());
         }
 
-        return winners;
+        return move;
     }
 
-    private List<Car> getCars(String[] carName) {
-        List<Car> cars = new ArrayList<>();
-        for(String name : carName){
-            Car car = new Car(name);
-            cars.add(car);
-        }
+    private List<String> getWinners(List<Car> cars) {
+        List<String> winners = new ArrayList<>();
+        int maxDistance = getMaxDistance(cars);
 
-        return cars;
+        return cars.stream()
+                .filter(car -> car.getDistance() == maxDistance)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private int getMaxDistance(List<Car> cars) {
+        return cars.stream()
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElse(0);
     }
 }
