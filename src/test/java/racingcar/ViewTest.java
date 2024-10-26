9c66196f8b812d9ca3153a2f43bebca41d4d632b
hyuuuun <pam4522@gmail.com>
2024-10-26 16:46:09 +0900

    fix(View) : 파라미터변수 에러처리

--- src/test/java/racingcar/ViewTest.java
+++ src/test/java/racingcar/ViewTest.java
@@ -2,34 +2,41 @@ package racingcar;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
+
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class ViewTest {
 
     @DisplayName("이름 분할 테스트")
     @ParameterizedTest
-    @CsvSource({
-        "'woni,pobi','pobi','woni'",
-        "pobi,woni,hyuuuun,test,code,singi"
-
-    })
-    void inputCarNames(String input , String... testNames) {
+    @MethodSource("expectedAndResult")
+    void inputCarNames(String input, List<String> expected) {
 
 
         View view = new View();
 
         List<String> result = view.inputCarNames(input);
 
-        List<String> test = Arrays.asList(testNames);
-
         // 결과 검증
-        assertEquals(test, result);
+        assertEquals(expected, result);
+    }
+
+    static Stream<Arguments> expectedAndResult() {
+        return Stream.of(
+            Arguments.arguments("woni,pobi", Arrays.asList("woni", "pobi")),
+            Arguments.arguments("car1", Arrays.asList("car1")),
+            Arguments.arguments("pobi ,woni ", Arrays.asList("pobi","woni"))
+        );
     }
 
 }
