88153650daae5af98f7e29efc5368e79d31176ce
이현수 <znight1020@naver.com>
2024-10-27 00:15:31 +0900

    feat: 입력값을 검증하는 기능 구현

--- src/main/java/racingcar/model/service/converter/RacingcarConverter.java
+++ src/main/java/racingcar/model/service/converter/RacingcarConverter.java
@@ -4,19 +4,28 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.model.domain.Car;
+import racingcar.model.service.validator.RacingcarValidator;
 
 public class RacingcarConverter {
 
-    private static final Character DELIMITER = ',';
+    private static final String DELIMITER = ",";
+    private final RacingcarValidator racingcarValidator;
 
-    public List<Car> convertToCars(String carNames) {
-        return Arrays.stream(carNames.split(String.valueOf(DELIMITER)))
+    public RacingcarConverter() {
+        this.racingcarValidator = new RacingcarValidator();
+    }
+
+    public List<Car> convertToCars(String carNamesInput) {
+        racingcarValidator.validateSplitCarNames(carNamesInput, DELIMITER);
+        return Arrays.stream(carNamesInput.split(DELIMITER))
                 .map(String::trim)
+                .peek(racingcarValidator::validateCarNameLength)
                 .map(Car::new)
                 .collect(Collectors.toList());
     }
 
     public Integer convertToNumber(String attemptCountInput) {
+        racingcarValidator.validateAttemptCount(attemptCountInput);
         return Integer.parseInt(attemptCountInput);
     }
 }
