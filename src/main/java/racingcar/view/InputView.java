484db44ba0b923892df72863e3ce87259575a51d
fbehddn <tom0611@naver.com>
2024-10-26 21:53:27 +0900

    refactor: 입력 메세지 enum 처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,15 +1,16 @@
 package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static racingcar.view.InputMessage.*;
 
 public class InputView {
     public String[] inputNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(INPUT_NAMES.getMessage());
         return readLine().split(",");
     }
 
     public int inputTimes() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(INPUT_TIMES.getMessage());
         return Integer.parseInt(readLine());
     }
 }
