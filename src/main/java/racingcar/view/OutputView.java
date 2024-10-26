317b86c88b98f521cb40c832cd7146c27263736d
hmlssghm <kwonse326@gmail.com>
2024-10-26 22:36:49 +0900

    fix(OutputView): enum get() 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,22 +6,22 @@ import racingcar.view.messages.InteractionMessageEnum;
 import java.util.List;
 
 public class OutputView {
-    // TODO: InteractionMessageEnum.CAR_NAMES 이거 너무 긴데 매번 이렇게 길게 써야됨??
+    // TODO: InteractionMessageEnum.CAR_NAMES.getInteractionMessage() 이거 너무 긴데 매번 이렇게 길게 써야됨??
     public static void printCarNames() {
-        System.out.println(InteractionMessageEnum.CAR_NAMES);
+        System.out.println(InteractionMessageEnum.CAR_NAMES.getInteractionMessage());
     }
 
     public static void printAttemptCount() {
-        System.out.println(InteractionMessageEnum.ATTEMPT_COUNT);
+        System.out.println(InteractionMessageEnum.ATTEMPT_COUNT.getInteractionMessage());
     }
 
     public static void printResultMessage() {
-        System.out.println(InteractionMessageEnum.RESULT_MESSAGE);
+        System.out.println(InteractionMessageEnum.RESULT_MESSAGE.getInteractionMessage());
     }
 
     public static void printResult(List<Car> cars) { // TODO: 이거 static 맞나??
         for (Car car: cars) {
-            System.out.print(car.getName() + InteractionMessageEnum.COLON +
+            System.out.print(car.getName() + InteractionMessageEnum.COLON.getInteractionMessage() +
                     InteractionMessageEnum.HYPHEN.getInteractionMessage().repeat(car.getMove())); // TODO: 넘 길다.
         }
         System.out.println(); // TODO: 줄바꿈 이렇게?
@@ -29,12 +29,12 @@ public class OutputView {
 
     public static void printWinner(List<Car> winners) {
         // TODO: 이렇게 한줄한줄 해야되나?
-        System.out.print(InteractionMessageEnum.WINNERS_MESSAGE);
-        System.out.print(InteractionMessageEnum.COLON);
+        System.out.print(InteractionMessageEnum.WINNERS_MESSAGE.getInteractionMessage());
+        System.out.print(InteractionMessageEnum.COLON.getInteractionMessage());
 
         // TODO: 아래 로직 메서드 분리 할지말지?
         for (int i = 0; i < winners.size() - 1; i++) {
-            System.out.print(winners.get(i).getName() + InteractionMessageEnum.COMMA);
+            System.out.print(winners.get(i).getName() + InteractionMessageEnum.COMMA.getInteractionMessage());
         }
         System.out.println(winners.get(winners.size() - 1).getName());
     }
