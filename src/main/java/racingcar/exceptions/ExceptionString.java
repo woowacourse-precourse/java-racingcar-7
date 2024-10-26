ef2e90f2929659c612460e6cc3411b3d2b9a6de4
byeongdoohan <quden04@gmail.com>
2024-10-26 17:37:21 +0900

    feat: exception string enum 추가

---
+++ src/main/java/racingcar/exceptions/ExceptionString.java
@@ -0,0 +1,18 @@
+package racingcar.exceptions;
+
+public enum ExceptionString {
+    NO_TRY_NUM("시도 횟수가 입력되지 않았습니다."),
+    TRY_NUM_CANNOT_BE_ZERO("시도 횟수는 0이 될 수 없습니다."),
+    CAR_NAME_CANNOT_EXCEED_FIVE("차 이름은 5자를 초과할 수 없습니다."),
+    CAR_NAME_LENGTH_CANNOT_BE_ZERO("자동차 이름의 길이는 0이 될 수 없습니다."),
+    ;
+
+    private final String text;
+    ExceptionString(String text) {
+        this.text = text;
+    }
+
+    public String getText(){
+        return this.text;
+    }
+}
