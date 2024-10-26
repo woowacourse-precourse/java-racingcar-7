4c1ae75d450d7d297724dfef10e2798044de1242
yush <yush2202@gmail.com>
2024-10-26 20:17:31 +0900

    refactor($ Cars): 중복 검사 코드 리펙터링
    전체 검증 코드에 중복 검사 코드 추가

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,20 +1,31 @@
 package racingcar.model;
 
 import java.util.List;
+import racingcar.utils.RandomNumberGenerator;
 
 public class Cars {
     private final List<Car> cars;
 
     public Cars(List<Car> cars) {
-        validateEmpty(cars);
+        validateCars(cars);
         this.cars = cars;
     }
 
-    public List<CarRacer> getFinalWinners() {
+    public List<CarName> getFinalWinners() {
         return this.cars.stream()
                 .filter(car -> car.getDistance() == findMaxDistance())
                 .map(Car::getCarRacer)
                 .toList();
+    }
+
+    public void doOneCycle() {
+        for (Car car : cars) {
+            car.decideToGo(RandomNumberGenerator.getRandomNumber());
+        }
+    }
+
+    public List<Car> getCars() {
+        return this.cars;
     }
 
     private int findMaxDistance() {
@@ -24,9 +35,24 @@ public class Cars {
                 .orElseThrow();
     }
 
+    private void validateCars(List<Car> cars) {
+        validateEmpty(cars);
+        validateDuplication(cars);
+    }
+
     private void validateEmpty(List<Car> cars) {
         if (cars.isEmpty()) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private void validateDuplication(List<Car> cars) {
+        if (isDuplicated(cars)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isDuplicated(List<Car> cars) {
+        return cars.size() != cars.stream().distinct().count();
     }
 }
