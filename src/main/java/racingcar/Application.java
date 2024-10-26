aecd56ada9c3f5923c52d7e3db6d708ba1c6d130
exena <exena01@gmail.com>
2024-10-26 16:19:03 +0900

    feat: 사용자 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input_names = readLine();
+        String[] names = input_names.split(",");
+        int[] counts = new int[names.length];
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String input_times = readLine();
+        int times = Integer.parseInt(input_times);
+
     }
 }
