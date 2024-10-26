6b618dc4e6614d58a58fd6b11e124194fd80815b
suyeong jang <jangsuyeong923@naver.com>
2024-10-26 18:51:28 +0900

    feat(RacingGame): 랜덤한 숫자를 생성하고 4이상인 경우 전진 이동 구현

---
+++ src/main/java/racingcar/model/RacingGame.java
@@ -0,0 +1,31 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+    private final List<Car> cars=new ArrayList<>();
+
+    public void initializeCars(List<String>carNames){
+        for(String name:carNames){
+            cars.add(new Car(name));
+        }
+    }
+    public void race(int count){
+        for(int i=0;i<count;i++){
+            moveCars();
+        }
+    }
+    public void moveCars(){
+        for(Car car: cars){
+            if(moveControl()){
+                car.addDash();
+            }
+        }
+    }
+    public boolean moveControl(){
+        int randomValue= Randoms.pickNumberInRange(0,9);
+        return randomValue>=4;
+    }
+}
