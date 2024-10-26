a374fb577c3f270dc590e4d55805713614c51fba
fortune-man <wnguddl96@knou.ac.kr>
2024-10-26 13:05:39 +0900

    test(ControllerTest) : 최소한의 실패하는 테스트 로직 추가

---
+++ src/test/java/racingcar/ControllerTest.java
@@ -0,0 +1,43 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ControllerTest {
+
+
+  @DisplayName("자동차 이름과 시도횟수를 전달받으면 각 자동차를 전진시킨다")
+  @Test
+  public void moveForwardCars() throws Exception{
+      //given
+
+      //when
+
+      //then
+  }
+
+  @DisplayName(" 0~9 랜덤값을 생성하여 결과가 4이상인 경우 전진을 결정한다")
+  @Test
+  public void moveOrNot() throws Exception{
+      //given
+
+      //when
+
+      //then
+  }
+
+  @DisplayName(" 자동차 게임 실행 결과를 내림차순으로 정렬한다 ")
+  @Test
+  public void reverseOrderResult() throws Exception{
+    //given
+
+    //when
+
+    //then
+  }
+
+
+
+}
