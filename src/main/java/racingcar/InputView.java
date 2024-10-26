da38919634296f3d117f3fdabb055c65926b111d
seoys0910 <seoys0910@naver.com>
2024-10-26 22:11:51 +0900

    feat : 시도할 횟수를 입력받는 기능 구현

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -7,4 +7,10 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
+
+    public static int inputTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String trycount = Console.readLine();
+        return Integer.parseInt(trycount);
+    }
 }
