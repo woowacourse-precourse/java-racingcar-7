993d8563a3a69b4dca9638149c16d0a2dd1d0454
kim jihyeon <jihyeon546@gmail.com>
2024-10-26 23:16:18 +0900

    fix(service): 자동차 이름 글자수 초과 예외 처리 추가

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -11,6 +11,10 @@ public class RacingCarService {
 
         for(String name: carNameArray){
             carNames.add(name.trim());
+
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력해 주세요.");
+            }
         }
 
         return carNames;
