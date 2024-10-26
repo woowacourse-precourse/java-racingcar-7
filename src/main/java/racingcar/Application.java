fadc34cde0c5dba3bff88df31552f45f2f190fd3
KangrimChoi <rkdfla100@naver.com>
2024-10-26 16:14:30 +0900

    feat: 4. 경기 결과 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,7 +5,6 @@ import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Assertions;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class Application {
@@ -26,7 +25,7 @@ public class Application {
 
 
     public static List<String> inputRacers(){
-        String racerNames = "ab,cd,ef"; //Console.readLine();
+        String racerNames = Console.readLine();
         List<String> racers = List.of(racerNames.split(","));
         if(isNameLengthOver5(racers)){
             throw new IllegalArgumentException();
@@ -34,7 +33,7 @@ public class Application {
         return racers;
     }
 
-    public static boolean isNameLengthOver5(List<String> racers) {
+    public static boolean isNameLengthOver5(final List<String> racers) {
         for(String racer : racers) {
             if(racer.length() > 5) {
                 return true;
@@ -43,14 +42,9 @@ public class Application {
         return false;
     }
 
-    /* 도착했을 때, 어떻게 해야 효율적으로 도착했는지 알 수 있을까?
-    *
-    * 1. += 할 때마다, 현재 위치 저장.
-    *
-    **/
 
     public static String startRacing(List<String> racers) {
-        int finishLine = 5; //Integer.parseInt(Console.readLine());
+        int finishLine = Integer.parseInt(Console.readLine());
         int frontRacerLocation = 0;
 
         List<String> racerInfo = raceReady(racers);
@@ -64,10 +58,10 @@ public class Application {
                     if(curLocation > frontRacerLocation) { frontRacerLocation++; }
                 }
             }
+            printCurRoundResult(racerInfo);
         }
 
-        String winners = announceWinner(racerInfo, finishLine);
-        return winners;
+        return announceWinner(racerInfo, finishLine);
     }
 
     public static List<String> raceReady(List<String> racers) {
@@ -78,57 +72,61 @@ public class Application {
         return preparedRacer;
     }
 
-    public static void printWinners(String winners){
+    public static void printWinners(final String winners){
         System.out.println(winners);
     }
 
     public static boolean isSuccesMoving() {
         int randNum = Randoms.pickNumberInRange(0,9);
-        if(randNum >= 4){
-            return true;
-        }
-
-        return false;
+        return randNum >= 4;
     }
 
-    public static void driveCar(int index, List<String> racerInfo, List<Integer> racerLocations) {
+    public static void driveCar(final int index, List<String> racerInfo, List<Integer> racerLocations) {
         racerInfo.set(index, racerInfo.get(index) + "-");
         racerLocations.set(index, racerLocations.get(index) + 1);
     }
 
-    public static String announceWinner(List<String> racerInfo, int finishLine) {
+    public static String announceWinner(final List<String> racerInfo, final int finishLine) {
         String winners = "최종 우승자 :";
+        int winnerCnt = 0;
         for(String info : racerInfo) {
             boolean racerResult = isWinner(info, finishLine);
-            if(racerResult){
+            if(racerResult) { winnerCnt++; }
+            if(racerResult && winnerCnt == 1){ //단독 우승자
                 winners += " " + racerName(info);
+            } else if(racerResult && winnerCnt >= 2){ // 우승자 여러명
+                winners += ", " + racerName(info);
             }
         }
         return winners;
     }
 
-    public static boolean isWinner(String racerInfo, int finishLine) {
+    public static boolean isWinner(final String racerInfo, final int finishLine) {
         String driveDistance = subName(racerInfo);
-        if(driveDistance.length() == finishLine){
-            return true;
-        }
-        return false;
+        return driveDistance.length() == finishLine;
     }
 
-    public static String racerName(String racerInfo) {
+    public static String racerName(final String racerInfo) {
         return racerInfo.substring(0, racerInfo.indexOf(" :"));
     }
 
-    public static String subName(String racerInfo) {
+    public static String subName(final String racerInfo) {
         return racerInfo.substring(racerInfo.indexOf(":") + 2);
     }
 
-    public static List<Integer> initLocatoin(int size) {
+    public static List<Integer> initLocatoin(final int size) {
         List<Integer> racerLocations = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             racerLocations.add(0);
         }
         return racerLocations;
+    }
+
+    public static void printCurRoundResult(final List<String> racerInfo ){
+        for(String Info : racerInfo) {
+            System.out.println(Info);
+        }
+        System.out.println(); //다음 라운드 결과와 구분
     }
 }
 
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -18,7 +17,7 @@ class ApplicationTest extends NsTest {
         assertRandomNumberInRangeTest(
                 () -> {
                     run("pobi,woni", "1");
-                    assertThat(output()).contains(("pobi : -\", \"woni : \", \"최종 우승자 : pobi"))
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
                 },
                 MOVING_FORWARD, STOP
         );
@@ -27,8 +26,8 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
