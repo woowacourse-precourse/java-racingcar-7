ce6e75c5b874ee89448b6f64eda9a2d4bc509135
GS_song98 <20003204@sju.ac.kr>
2024-10-27 00:35:46 +0900

    test: 자동차 move 메서드 테스트 수정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,92 +1,60 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
 
     @Test
-    @DisplayName("Car 객체를 성공적으로 생성한다.")
-    void successfullyCreateCar() {
+    @DisplayName("0-3 사이 숫자가 두번 들어오면 움직이지 않는다.")
+    void successfullyDoNotMove() {
         //given
         String carName = "song";
+        int expected = 0;
 
-        //when then
-        assertDoesNotThrow(() -> new Car(carName));
-    }
+        //when
+        Car car = new Car(carName);
+        car.moveForward(() -> 0);
+        car.moveForward(() -> 1);
+        int actual = car.getPosition();
 
-    @Nested
-    @DisplayName("position에게 2번 움직임을 요청했을 때")
-    class whenPositionMoveForward {
-
-        @Test
-        @DisplayName("0-3 사이 숫자가 두번 들어오면 움직이지 않는다.")
-        void successfullyNoMoveCarToString() {
-            //given
-            String carName = "song";
-            String expected = "song : \n";
-
-            //when
-            Car car = new Car(carName);
-            car.moveForward(0);
-            car.moveForward(1);
-            String actual = car.toString();
-
-            //then
-            assertThat(actual).isEqualTo(expected);
-        }
-
-        @Test
-        @DisplayName("0-3 사이 숫자가 한번, 4-9 사이 숫자가 한번 들어오면 한번 움직인다.")
-        void successfullyOneMoveCarToString() {
-            //given
-            String carName = "song";
-            String expected = "song : -\n";
-
-            //when
-            Car car = new Car(carName);
-            car.moveForward(0);
-            car.moveForward(9);
-            String actual = car.toString();
-
-            //then
-            assertThat(actual).isEqualTo(expected);
-        }
-
-        @Test
-        @DisplayName("4-9 사이 숫자가 두번 들어오면 두번 움직인다.")
-        void successfullyTwoMoveCarToString() {
-            //given
-            String carName = "song";
-            String expected = "song : --\n";
-
-            //when
-            Car car = new Car(carName);
-            car.moveForward(4);
-            car.moveForward(9);
-            String actual = car.toString();
-
-            //then
-            assertThat(actual).isEqualTo(expected);
-        }
+        //then
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
-    @DisplayName("copy() 깊은 복사 테스트")
-    void successfullyCopyCar() {
+    @DisplayName("0-3 사이 숫자가 한번, 4-9 사이 숫자가 한번 들어오면 한번 움직인다.")
+    void successfullyOneMove() {
         //given
         String carName = "song";
+        int expected = 1;
 
         //when
-        Car originalCar = new Car(carName);
-        Car copyCar = originalCar.copy();
-        copyCar.moveForward(9);
+        Car car = new Car(carName);
+        car.moveForward(() -> 0);
+        car.moveForward(() -> 9);
+        int actual = car.getPosition();
 
         //then
-        assertThat(originalCar.getPosition()).isNotEqualTo(copyCar.getPosition());
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("4-9 사이 숫자가 두번 들어오면 두번 움직인다.")
+    void successfullyTwoMove() {
+        //given
+        String carName = "song";
+        int expected = 2;
+
+        //when
+        Car car = new Car(carName);
+        car.moveForward(() -> 4);
+        car.moveForward(() -> 9);
+        int actual = car.getPosition();
+
+        //then
+        assertThat(actual).isEqualTo(expected);
     }
 }
