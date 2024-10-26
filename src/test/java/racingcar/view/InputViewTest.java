feaa475ff948ddccf476974fb8cc9db8ea09fcf7
unh6unh6 <ruddbs3308@naver.com>
2024-10-26 14:58:31 +0900

    test(view): 사용자로부터 경주할 자동차 이름을 입력 받기

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,5 +25,15 @@ public class InputViewTest {
 
         assertThat(outputStream.toString().trim())
                 .isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    @Test
+    void 자동차_이름_입력() {
+        String input = "poby,woni";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        String result = inputView.getCarNameInput();
+
+        assertThat(result).isEqualTo(input);
     }
 }
