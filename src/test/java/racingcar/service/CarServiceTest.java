2ba93f923b4ace7edef30fcdc7614bc5bf95fad1
Yoon-Jemin <yjm1800@naver.com>
2024-10-27 04:25:29 +0900

    test(service): 서비스 클래스 단위 테스트
    - 차수별로 전진하는 조건 만족 시 자동차를 전진시키는 로직 테스트

--- src/test/java/racingcar/service/CarServiceTest.java
+++ src/test/java/racingcar/service/CarServiceTest.java
@@ -24,7 +24,7 @@ public class CarServiceTest {
 
     @Test
     @DisplayName("자동차 경주 게임에 참가자를 추가할 수 있다.")
-    void 자동차_경주_게임에_참가자_추가() throws Exception{
+    void 자동차_경주_게임에_참가자_추가_테스트() throws Exception{
         // Given
         String carName = "Car1";
 
@@ -35,6 +35,24 @@ public class CarServiceTest {
         List<Car> participants = carService.getParticipants();
         assertEquals(participants.size(), 1);
         assertEquals(carName, participants.get(0).getCarName());
+    }
+
+    @Test
+    @DisplayName("차수별로 전진하는 조건 만족 시에 자동차는 전진할 수 있다.")
+    void 자동차_전진_테스트() throws Exception{
+        // Given
+        Car car1 = new Car("Car1", 0);
+        Car car2 = new Car("Car2", 0);
+        carGame.addParticipant(car1);
+        carGame.addParticipant(car2);
+
+        // when
+        carService.advanceCars();
+
+        // then
+        for (Car car : carGame.getParticipants()) {
+            assertTrue(car.getDistance() == 0 || car.getDistance() == 1);
+        }
     }
 
 }
