19b2d3b71485bd251088c8adb8fb1f5b9e3a04fa
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-10-26 12:46:28 +0900

    TEST(ApplicationTest): Create test code

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -28,6 +29,46 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 없는 경우")
+    void 자동차_이름_테스트() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("", "1"))
+                .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    @DisplayName("이동 횟수 입력이 음수인 경우")
+    void 이동_횟수_음수_또는_0_테스트() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi, java", "-1"))
+                .isInstanceOf(IllegalArgumentException.class));
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi, java", "0"))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    @DisplayName("이동 횟수 입력이 Integer 범위를 초과하는 경우")
+    void 이동_횟수_초과_테스트() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi, java", "9999999999"))
+                .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    @DisplayName("우승자가 여러 명인 경우")
+    void 여러_명의_우승자_테스트() {
+        assertRandomNumberInRangeTest(
+            () -> {
+                run("pobi,woni", "1");
+                assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi");
+            },
+            MOVING_FORWARD, MOVING_FORWARD
         );
     }
 
