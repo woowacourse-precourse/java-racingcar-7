7c4dc9e74a8281e699c9d5cc55fd888e4115701e
dgh06175 <dgh06175@gmail.com>
2024-10-26 16:50:21 +0900

    매직 넘버 상수화

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,22 +3,30 @@ package racingcar;
 import racingcar.util.NumberGenerator;
 
 public class Car {
+    public static final int INITIAL_POSITION = 0;
+    public static final int MAX_NAME_LENGTH = 5;
+    public static final int MIN_MOVE_VALUE = 4;
+
     private final NumberGenerator numberGenerator;
     private final String name;
     private int position;
 
     public Car(NumberGenerator numberGenerator, String name) {
-        if (name.length() > 5 || name.isBlank()) {
-            throw new IllegalArgumentException();
-        }
+        validateName(name);
         this.numberGenerator = numberGenerator;
         this.name = name;
-        this.position = 0;
+        this.position = INITIAL_POSITION;
+    }
+
+    private static void validateName(String name) {
+        if (name.length() > MAX_NAME_LENGTH || name.isBlank()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void tryMove() {
         int randomNumber = numberGenerator.generate();
-        if (randomNumber >= 4) {
+        if (randomNumber >= MIN_MOVE_VALUE) {
             position++;
         }
     }
