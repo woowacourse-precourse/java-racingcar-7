b4e62765a9abdab9a8960c6919bcd1a440398656
hseungho <hsh47607@gmail.com>
2024-10-26 19:48:34 +0900

    feat: interface Reader, class SingleLineReader 추가
    Signed-off-by: hseungho <hsh47607@gmail.com>

---
+++ src/main/java/racingcar/io/read/SingleLineReader.java
@@ -0,0 +1,12 @@
+package racingcar.io.read;
+
+public class SingleLineReader implements Reader<String> {
+
+    @Override
+    public String read() {
+        String read = camp.nextstep.edu.missionutils.Console.readLine();
+        camp.nextstep.edu.missionutils.Console.close();
+        return read;
+    }
+
+}
