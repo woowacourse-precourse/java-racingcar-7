c5631562d4fabad893df55e417f6b3724904138b
cherryiJuice <chldlwn711@gmail.com>
2024-10-26 19:55:13 +0900

    refactor: 숫자가 아닌 -를 출력하도록 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,15 +6,19 @@ import racingcar.dto.WinnerResponseDto;
 public class OutputView {
     private static final String RESULT_MESSAGE = "실행 결과";
     private static final String WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String COLON = " : ";
+    private static final String COMMA = ", ";
+    private static final String POSITION_DISPLAY = "-";
 
     public static void printRacingOutput(List<WinnerResponseDto> responseDtos) {
         System.out.println(RESULT_MESSAGE);
-        responseDtos.forEach(dto -> System.out.println(dto.name() + " : " + dto.position()));
+        responseDtos.forEach(dto -> System.out.println(dto.name() + COLON
+                + POSITION_DISPLAY.repeat(dto.position())));
         System.out.println();
     }
 
     public static void printWinnerOutput(List<String> winners) {
-        String winnerNames = String.join(", ", winners);
+        String winnerNames = String.join(COMMA, winners);
         System.out.println(WINNER_MESSAGE + winnerNames);
     }
 
