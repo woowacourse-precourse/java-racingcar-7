df77292aac4d5653902ada8ed116bded5a4fcd13
chyo1 <hyowon3856@gmail.com>
2024-10-27 00:29:42 +0900

    test : 반환값 대신 테스트에 직접 삽입

--- src/test/java/racingcar/ParserTest.java
+++ src/test/java/racingcar/ParserTest.java
@@ -12,22 +12,14 @@ class ParserTest {
 
     @Test
     void 구분자로_자동차_이름_파싱() {
-        String testStr = "one,two,three,four,five";
-        String testOperator = ",";
-        List<String> expectedResult = new ArrayList<>(List.of("one", "two", "three", "four", "five"));
-
-        List<String> result = parser.parseCarNamesByOperator(testStr, testOperator);
-
-        assertThat(result).isEqualTo(expectedResult);
+        assertThat(parser.parseCarNamesByOperator("one,two,three,four,five", ","))
+            .isEqualTo(List.of("one", "two", "three", "four", "five"));
     }
 
     @Test
     void parseRepeatCount() {
-        String testStr = "15";
-        long expected = 15;
-
-        long result = parser.parseRepeatCount(testStr);
-        assertThat(result).isEqualTo(expected);
+        assertThat(parser.parseRepeatCount("15"))
+            .isEqualTo(15);
     }
 
 }
