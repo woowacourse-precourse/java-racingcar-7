3c72415e14d1038465ce586d76b3c76a5cc04b39
Youn Sang Hyun <younsang523@gmail.com>
2024-10-27 01:33:29 +0900

    feat: 자동차 이름 입력 테스트 기능 구현

---
+++ src/test/java/racingcar/CarNameTest.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+class CarNameTest extends NsTest {
+
+    @Test
+    void 글자수_초과_테스트() {
+       assertSimpleTest(() ->
+        assertThatThrownBy(() -> runException("javaji1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 빈값_테스트() {
+        assertThatThrownBy(() -> runException(" "))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+
+    }
+}
