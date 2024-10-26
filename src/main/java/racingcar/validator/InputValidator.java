ed98a1dd54357b7dadad5765d18cd857bf675118
rootTiket <l20078829@gmail.com>
2024-10-27 00:27:02 +0900

    refactor: List가 아닌 문자열을 각각 검증하도록 변경

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -2,13 +2,11 @@ package racingcar.validator;
 
 import static racingcar.View.constant.ErrorMessage.TOO_LONG_VALUE;
 
-import java.util.List;
 import racingcar.global.exception.RacingcarException;
 
 public class InputValidator {
-    public void check(List<String> carNames) {
-        carNames.stream()
-                .forEach(this::checkStringLength);
+    public void check(String input) {
+        checkStringLength(input);
     }
 
     public void checkStringLength(String input) {
