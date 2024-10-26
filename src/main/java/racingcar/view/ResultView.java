9c17fcf4ebba88e190facc29d4fbf3b4f2597402
pakaheo <k70923@konkuk.ac.kr>
2024-10-26 22:05:37 +0900

    feat : Show race result and final winners
    ResultView show race result and final winners

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,2 +1,18 @@
-package racingcar.view;public class ResultView {
+package racingcar.view;
+
+import java.util.List;
+import racingcar.domain.Cars;
+
+public class ResultView {
+    public static void promptExecutionResult() {
+        System.out.print("실행 결과\n");
+    }
+
+    public static void showTurnResult(Cars cars) {
+        System.out.println(cars + "\n");
+    }
+
+    public static void showWinners(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(",", winners));
+    }
 }
