f3e1b1becaca72c9fa6d6c72b891de12809869be
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 00:13:27 +0900

    feat(RaceViewController): 팩토리로부터 주입받은 의존성 처리
    - ControllerFactory로부터 의존성을 받아옴
    - 생성자 주입 방식으로 InputView, OutputView 의존성 처리

--- src/main/java/racingcar/Controller/RaceViewController.java
+++ src/main/java/racingcar/Controller/RaceViewController.java
@@ -1,4 +1,14 @@
 package racingcar.Controller;
 
+import racingcar.View.InputView;
+import racingcar.View.OutputView;
+
 public class RaceViewController {
-}
+    private InputView inputView;
+    private OutputView outputView;
+
+        public RaceViewController(InputView inputView, OutputView outputView) {
+            this.inputView = inputView;
+            this.outputView = outputView;
+        }
+    }
