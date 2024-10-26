82d8780b1a985762c9f46abe1dc5e3227fe859e0
Seungwan Yoo <ysw99@kakao.com>
2024-10-26 21:17:33 +0900

    feat: 자동차 이름 입력 관련 예외 처리 - 이름을 입력하지 않거나, 하나만 입력할 경우 예외 발생

--- src/main/java/racingcar/exception/RaceException.java
+++ src/main/java/racingcar/exception/RaceException.java
@@ -2,6 +2,8 @@ package racingcar.exception;
 
 public enum RaceException {
     SPACING_NOT_ALLOWED("띄어쓰기는 입력할 수 없습니다."),
+    INPUT_VALUE_EMPTY("값이 비어있습니다."),
+    ARGUMENT_SIZE_UNSATISFIED("자동차 이름은 두 개 이상 작성해야 합니다."),
     ;
 
     private final String description;
--- src/main/java/racingcar/util/ParseStringUtil.java
+++ src/main/java/racingcar/util/ParseStringUtil.java
@@ -10,8 +10,18 @@ public class ParseStringUtil {
     private static final String SPACING_PREFIX = " ";
 
     public static List<String> parseCarNames(String input) {
+        validate(input);
+        return Arrays.stream(input.split(SPLIT_REGEX_PREFIX)).toList();
+    }
+
+    private static void validate(String input) {
+        if (input.isBlank())
+            throw new IllegalArgumentException(RaceException.INPUT_VALUE_EMPTY.getDescription());
+
         if (input.contains(SPACING_PREFIX))
             throw new IllegalArgumentException(RaceException.SPACING_NOT_ALLOWED.getDescription());
-        return Arrays.stream(input.split(SPLIT_REGEX_PREFIX)).toList();
+
+        if (!input.contains(SPLIT_REGEX_PREFIX))
+            throw new IllegalArgumentException(RaceException.ARGUMENT_SIZE_UNSATISFIED.getDescription());
     }
 }
