4373d02e442bae5cd039cb2b3d045fe5a990cbab
ksundev <kshk08@hanyang.ac.kr>
2024-10-27 03:49:05 +0900

    feat(Application): 자동차 이름 입력 받아서 Car 객체들 생성하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
+        List<Car> cars = new ArrayList<>();
+
+        for (String carName : carNames) {
+            Car car = new Car(carName);
+            cars.add(car);
+        }
+
+        // 디버깅 용
+        for (Car car : cars) {
+            System.out.println(car.getName());
+        }
     }
 }
