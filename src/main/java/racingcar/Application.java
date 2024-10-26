99b9056682172fa1d88a828297527baba8edc2c2
dnjswns98 <tngkrt2004@gmail.com>
2024-10-26 16:16:14 +0900

    feat(Application): 자동차 경주 임시 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,47 @@
 package racingcar;
 
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+    private static List<String> car = new ArrayList<>();
+    private static int count;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        outputView.printInputCar();
+        car = inputView.getInputCars();
+        outputView.printInputTryCount();
+        count = inputView.getInputCount();
+        int[] forward = new int[car.size()];
+        int Nmax = 0;
+        
+        for(int i = 0; i<count; i++){
+            for(int j = 0; j<car.size(); j++) {
+                int num = Randoms.pickNumberInRange(0, 9);
+                if(num >= 4){
+                    forward[j]++;
+                }
+                Nmax = Math.max(Nmax,forward[j]);
+                System.out.print(car.get(j) + " : ");
+                for(int h = 0; h<forward[j]; h++){
+                    System.out.print("-");
+                }
+                System.out.println();
+            }
+        }
+        System.out.print("최종 우승자 : ");
+        for(int i = 0; i<car.size(); i++){
+            if(forward[i] == Nmax){
+                System.out.print(car.get(i) + ", ");
+            }
+        }
     }
 }
