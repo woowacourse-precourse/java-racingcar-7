85b35b59c0c0094d91c0ddad913d47a917f4fb36
LSH-kw0315 <tmdgjsdl1234@naver.com>
2024-10-27 03:54:51 +0900

    fix(플레이어 입력,횟수 입력) 플레이어 입력과 횟수 입력 안내문구 누락 수정
    플레이어를 입력하는 기능과 횟수를 입력하는 기능에서 안내문구를 출력하는
    요구사항에 대한 누락을 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String playerInput=Console.readLine();
         if(playerInput==null || playerInput.isEmpty()){
             throw new IllegalArgumentException();
@@ -24,6 +25,7 @@ public class Application {
             player.put(s,0);
         }
 
+        System.out.println("시도할 횟수는 몇 회인가요?");
         String countInput=Console.readLine();
         int count;
         try {
