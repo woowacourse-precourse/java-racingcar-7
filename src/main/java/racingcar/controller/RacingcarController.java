974baff13317ab02850602d0199b80c808f0db1a
Dev-LHY <dev.leehy@gmail.com>
2024-10-26 22:13:36 +0900

    refactor(racingcarController): run 메서드, import문 수정
    - RacingcarService의 runRace 삭제 후 RacingcarController의 run과 통합
    - 여러 개로 나누어진 import문 *로 통합

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -1,21 +1,30 @@
 package racingcar.controller;
 
+import static racingcar.exception.InvalidInputException.validateCarNames;
+import static racingcar.util.Parsing.*;
+
+import java.util.List;
+import java.util.Map;
 import racingcar.exception.ExceptionMessage;
 import racingcar.exception.InvalidInputException;
+import racingcar.model.Racingcar;
 import racingcar.model.RacingcarService;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingcarController {
     private final RacingcarService racingcarService;
     private final InputView inputView;
+    private final OutputView outputView;
 
     public RacingcarController() {
         this.racingcarService = new RacingcarService();
         inputView = InputView.getInstance();
+        outputView = OutputView.getInstance();
     }
 
     public void run() {
-        String carName = inputView.getCarName();
+        Racingcar racingcar = new Racingcar(inputView.getCarName());
         int attemptCount = 0;
         try {
             attemptCount = Integer.parseInt(inputView.getAttemptCount());
@@ -23,6 +32,13 @@ public class RacingcarController {
         } catch (Exception e) {
             throw new IllegalArgumentException(ExceptionMessage.INPUT_ONLY_POSITIVE_INTEGER.getMessage());
         }
-        racingcarService.runRace(carName, attemptCount);
+
+        String[] carNames = splitCarName(racingcar.getCarName());
+        validateCarNames(carNames);
+
+        Map<String, String> player = convertStringArrToMap(carNames);
+        racingcarService.startRace(player, attemptCount);
+        List<String> winner = racingcarService.pickWinner(player, racingcarService.getMaxDashLength(player));
+        outputView.winner(joinWinner(winner));
     }
 }
