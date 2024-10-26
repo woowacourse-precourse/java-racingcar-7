df21521a60551aca2016bc041412826f45c11d07
skhnpark <skhnpark125@gmail.com>
2024-10-26 19:40:14 +0900

    feat: 자동차 이름의 길이에 대한 예외 처리를 하는 기능 구현

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -5,9 +5,18 @@ public class CarService {
 
     public void createGame(String carNames) {
         splitCarNames(carNames);
+        for (String car: cars) {
+            validateCarNames(car);
+        }
     }
 
     public void splitCarNames(String carNames) {
         cars = carNames.split(",");
+    }
+
+    public void validateCarNames(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
     }
 }
