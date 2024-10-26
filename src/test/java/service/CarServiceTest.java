8d2cdd4110793867d4357773ec2bd84c5afefd4d
SukJin Kim <rkathrwks@gmail.com>
2024-10-26 13:03:02 +0900

    test(carservice): 자동차 이름 관련 유효성 검증 기능 유닛 테스트 추가

--- src/test/java/service/CarServiceTest.java
+++ src/test/java/service/CarServiceTest.java
@@ -1,6 +1,7 @@
 package service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -32,5 +33,34 @@ class CarServiceTest {
         assertThat(carList.getFirst().getName()).isEqualTo("pobi");
         assertThat(carList.get(1).getName()).isEqualTo("woni");
         assertThat(carList.get(2).getName()).isEqualTo("jun");
+    }
+
+    @Test
+    void 빈_문자열_입력시_테스트() {
+        String carNames = "";
+
+        assertThatThrownBy(() -> {
+            carService.createCarList(carNames);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("부적절한 형식의 자동차 이름을 입력했습니다. (자동차 이름은 공백이 아닌 5자 이하 문자열)");
+    }
+
+    @Test
+    void 다섯_자_이상_차_이름_입력시_테스트() {
+        String carNames = "very_loooooooong_name, pobi,woni,jun";
+
+        assertThatThrownBy(() -> {
+            carService.createCarList(carNames);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("부적절한 형식의 자동차 이름을 입력했습니다. (자동차 이름은 공백이 아닌 5자 이하 문자열)");
+    }
+
+    @Test
+    void 중복된_이름_입력시_테스트() {
+        String carNames = "pobi,pobi,jun";
+
+        assertThatThrownBy(() -> {
+            carService.createCarList(carNames);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("중복된 자동차 이름을 입력했습니다.");
     }
 }
