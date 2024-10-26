4a400d367240ae5305fa5c08576e15a629a0a46f
githublees <leeho_@naver.com>
2024-10-26 15:54:59 +0900

    refactor: Change responsibilities from InputReader to InputParser

--- src/main/java/racingcar/input/InputParser.java
+++ src/main/java/racingcar/input/InputParser.java
@@ -7,18 +7,39 @@ import racingcar.constant.Constant;
 public class InputParser {
 
     private static final int NAME_LENGTH_MAX = 5;
+    private static final String ONLY_NUMBER = "^[1-9]\\d*$";
     private static final String COMMA = ",";
     private static final String SPACE = " ";
 
     public List<String> toList(final String input) {
+        validateInputBlank(input);
         return Arrays.stream(split(input))
-                .peek(this::validateSpaceBetweenName)
-                .peek(this::validateMaxNameSize)
+                .peek(this::validateToList)
                 .toList();
+    }
+
+    public int toPositive(final String number) {
+        validateInputBlank(number);
+        validateNegative(number);
+        return parseInteger(number);
     }
 
     private String[] split(final String input) {
         return input.split(COMMA);
+    }
+
+    private int parseInteger(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(Constant.COUNT_ONLY_INTEGER_NUMBER_STRING);
+        }
+    }
+
+    private void validateToList(final String input) {
+        validateInputBlank(input);
+        validateSpaceBetweenName(input);
+        validateMaxNameSize(input);
     }
 
     private void validateMaxNameSize(final String input) {
@@ -31,6 +52,26 @@ public class InputParser {
         if (isSpace(input)) {
             throw new IllegalArgumentException(Constant.SPACE_BETWEEN_NAME_STRING);
         }
+    }
+
+    private void validateNegative(final String number) {
+        if (isNumber(number)) {
+            throw new IllegalArgumentException(Constant.COUNT_ONLY_NATURAL_NUMBER_STRING);
+        }
+    }
+
+    private void validateInputBlank(final String input) {
+        if (isBlank(input)) {
+            throw new IllegalArgumentException(Constant.INPUT_NULL_OR_EMPTY_STRING);
+        }
+    }
+
+    private boolean isBlank(final String input) {
+        return input == null || input.isEmpty();
+    }
+
+    private boolean isNumber(final String number) {
+        return !number.matches(ONLY_NUMBER);
     }
 
     private boolean isSpace(final String input) {
--- src/main/java/racingcar/input/InputReader.java
+++ src/main/java/racingcar/input/InputReader.java
@@ -2,11 +2,8 @@ package racingcar.input;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
-import racingcar.constant.Constant;
 
 public class InputReader {
-
-    private static final String ONLY_NUMBER = "^[1-9]\\d*$";
 
     private final InputParser inputParser;
 
@@ -16,43 +13,12 @@ public class InputReader {
 
     public List<String> getNames() {
         final String input = Console.readLine();
-
-        validateInputBlank(input);
         return inputParser.toList(input);
     }
 
     public int getTryCount() {
         final String number = Console.readLine();
-
-        validateInputBlank(number);
-        validateNegative(number);
-
-        try {
-            return Integer.parseInt(number);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(Constant.COUNT_ONLY_INTEGER_NUMBER_STRING);
-        }
+        return inputParser.toPositive(number);
     }
-
-    private void validateNegative(final String number) {
-        if (isNumber(number)) {
-            throw new IllegalArgumentException(Constant.COUNT_ONLY_NATURAL_NUMBER_STRING);
-        }
-    }
-
-    private void validateInputBlank(final String input) {
-        if (isBlank(input)) {
-            throw new IllegalArgumentException(Constant.INPUT_NULL_OR_EMPTY_STRING);
-        }
-    }
-
-    private boolean isBlank(final String input) {
-        return input == null || input.isEmpty();
-    }
-
-    private boolean isNumber(final String number) {
-        return !number.matches(ONLY_NUMBER);
-    }
-
 
 }
