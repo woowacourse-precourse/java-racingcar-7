97928e5f967e0c2ce3672b33f675fd29e3bebcbc
heehun <slrkqk2@naver.com>
2024-10-27 04:05:30 +0900

    feat(InputValidator): 자동차 이름 input 검증 메서드 추가

--- src/main/java/racingcar/utils/InputValidator.java
+++ src/main/java/racingcar/utils/InputValidator.java
@@ -1,2 +1,13 @@
-package racingcar.utils;public class InputValidator {
+package racingcar.utils;
+
+public class InputValidator {
+    private static final String NAME_PATTERN = "^[a-zA-Z]+(,[a-zA-Z]+)*$";
+
+    public static void nameValidator(String input) {
+        if (!input.matches(NAME_PATTERN)) {
+            throw new IllegalArgumentException("이름 입력이 잘못되었습니다.");
+        }
+    }
+
+
 }
