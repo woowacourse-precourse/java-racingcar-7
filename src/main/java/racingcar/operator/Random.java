c7cc92bd4bd035dea0d8753e1fc51cac1847089e
MinJae00 <exceedmin@gmail.com>
2024-10-27 01:00:31 +0900

    feat: random number extraction

--- src/main/java/racingcar/operator/Random.java
+++ src/main/java/racingcar/operator/Random.java
@@ -1,2 +1,9 @@
-package racingcar.operator;public class Random {
+package racingcar.operator;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Random {
+    public int ExtractRandomNum() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
 }
