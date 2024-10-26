d6a0cbee1c29dd43f77fce10d9020d7d7841fbb3
Fletacode <bassykd@gmail.com>
2024-10-26 21:51:10 +0900

    feat: 자동차 이름 추출 기능 추가
    입력 받은 자동차 문자열을 , 구분자로 구분하고 구분된 자동차 이름의 길이가 5이상이라면 예외를 발생시킵니다. 또한, 문자가 아닐경우 숫자 문자열 (123)이면 예외를 발생시킵니다.

--- src/main/java/racingcar/model/parseCar/ParseCar.java
+++ src/main/java/racingcar/model/parseCar/ParseCar.java
@@ -1,7 +1,15 @@
 package racingcar.model.parseCar;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ParseCar {
 
   public final static String DELIMITER = ",";
+
+  public static List<String> parseCarName(String carString){
+    return Arrays.stream(carString.split(DELIMITER)).toList();
+  }
+
 
 }
