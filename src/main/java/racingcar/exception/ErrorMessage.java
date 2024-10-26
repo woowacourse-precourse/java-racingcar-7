b19b435eb7c790241c28a36e6d8d5f67126e4659
CHISANW <keuye06380618@gmail.com>
2024-10-26 15:59:40 +0900

    refactor(ErrorMessage): 예외 메시지 상수를 위한 클래스 형태로 리팩토링
    - 기존 열거형 타입 대신 클래스형 구조로 변경하여, 요구사항을 준수하고 유지보수성을 개선
    - 매직 상수를 통해 IllegalArgumentException의 오류 메시지를 관리하도록 리팩토링

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -1,18 +1,8 @@
 package racingcar.exception;
 
-public enum ErrorMessage {
+public class ErrorMessage {
 
-    CAR_NAME_TOO_LONG("자동차 이름은 5자 이하여야 합니다."),
-    INVALID_ATTEMPT_COUNT("시도 횟수는 숫자여야 합니다."),
-    ATTEMPT_COUNT_CANNOT_BE_ZERO("시도 횟수는 0 이상의 숫자여야 합니다.");
-
-    private final String message;
-
-    ErrorMessage(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+    public static final String CAR_NAME_TOO_LONG = "자동차 이름은 5자 이하여야 합니다.";
+    public static final String INVALID_ATTEMPT_COUNT = "시도 횟수는 숫자여야 합니다.";
+    public static final String ATTEMPT_COUNT_CANNOT_BE_ZERO = "시도 횟수는 0 이상의 숫자여야 합니다.";
 }
