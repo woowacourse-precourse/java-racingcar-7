71f7b2c5af2ce260d74b1fc672e390490c4a1018
ruudska6 <feels19@naver.com>
2024-10-26 12:47:37 +0900

    <feat>: 사용자에게 이동을 시도할 횟수들을 콘솔을 통해 입력 받는 InputView.askTryCount 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,10 +3,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String QUESTION_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-
+    private static final String QUESTION_TRY_COUNT = "시도할 회수는 몇회인가요?";
 
     public String askCarNames() {
         System.out.println(QUESTION_CAR_NAMES);
         return Console.readLine();
     }
+
+    public int askTryCount() {
+        System.out.println(QUESTION_TRY_COUNT);
+        return Integer.parseInt(Console.readLine());
+    }
+
 }
