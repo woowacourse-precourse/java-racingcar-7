0488b5b16e12c86a9eaed2f7329658769dc1c47a
byeongdoohan <quden04@gmail.com>
2024-10-26 18:13:38 +0900

    refactor: 출력문 상수로 변경

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -1,19 +1,19 @@
 package racingcar.view;
 
 public class RacingCarView {
-    private final String carInputString = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final String tryInputString = "시도할 횟수는 몇 회인가요?";
-    private final String outputString = "최종 우승자 : ";
+    private static final String CAR_INPUT_STRING = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRY_INPUT_STRING = "시도할 횟수는 몇 회인가요?";
+    private static final String OUTPUT_STRING = "최종 우승자 : ";
 
     public void getCarInputString() {
-        System.out.println(carInputString);
+        System.out.println(CAR_INPUT_STRING);
     }
 
     public void getTryInputString() {
-        System.out.println(tryInputString);
+        System.out.println(TRY_INPUT_STRING);
     }
 
     public void getOutputString(String carNames) {
-        System.out.println(outputString + carNames);
+        System.out.println(OUTPUT_STRING + carNames);
     }
 }
