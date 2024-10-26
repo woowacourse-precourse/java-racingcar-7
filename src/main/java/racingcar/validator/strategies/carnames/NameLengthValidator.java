8790caa3dfa143e3d201098283181cc1ed2255f8
Park Yu Han <pbk2312@naver.com>
2024-10-26 13:00:04 +0900

    refactor(validator): 단일 이름 검증에서 리스트 이름 검증으로 변경
    - NameLengthValidator의 validate 메소드를 수정하여
      단일 문자열 대신 문자열 리스트를 검증하도록 변경
    - 리스트의 각 자동차 이름 길이를 확인하여 유효성 검사 강화

--- src/main/java/racingcar/validator/strategies/carnames/NameLengthValidator.java
+++ src/main/java/racingcar/validator/strategies/carnames/NameLengthValidator.java
@@ -1,14 +1,18 @@
 package racingcar.validator.strategies.carnames;
 
+import java.util.List;
 import racingcar.constants.RacingGameConstants;
 import racingcar.validator.strategies.ValidationStrategy;
 import racingcar.view.ErrorMessage;
 
-public class NameLengthValidator implements ValidationStrategy<String> {
+public class NameLengthValidator implements ValidationStrategy<List<String>> {
+
     @Override
-    public void validate(String name) {
-        if (name.length() > RacingGameConstants.MAX_CAR_NAME_LENGTH.getValue()) {
-            throw new IllegalArgumentException(ErrorMessage.NAME_TOO_LONG.getMessage());
+    public void validate(List<String> carNames) {
+        for (String name : carNames) {
+            if (name.length() > RacingGameConstants.MAX_CAR_NAME_LENGTH.getValue()) {
+                throw new IllegalArgumentException(ErrorMessage.NAME_TOO_LONG.getMessage());
+            }
         }
     }
 
