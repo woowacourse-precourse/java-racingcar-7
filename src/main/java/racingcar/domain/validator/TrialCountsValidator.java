0dcb130ee77134b1c716242e3600c2eb7b664e87
bowook <mbosx316@naver.com>
2024-10-26 22:02:32 +0900

    refactor(TrialCountsValidator) : TrialCountsValidator 리팩토링
    - 의미 있는 숫자인 0을 상수로써 선언 후 사용하도록 기능 개선

--- src/main/java/racingcar/domain/validator/TrialCountsValidator.java
+++ src/main/java/racingcar/domain/validator/TrialCountsValidator.java
@@ -3,6 +3,7 @@ package racingcar.domain.validator;
 public class TrialCountsValidator {
     private final static String EXCEPTION_MESSAGE_NOT_NUMBER = "숫자가 아닌 값을 입력하시면 안 됩니다.";
     private final static String EXCEPTION_MESSAGE_NOT_MINUS_NUMBER = "음수와 0을 입력하시면 안 됩니다.";
+    private final static int TRIAL_COUNTS_STANDARD = 0;
 
     public int validate(String trialCounts) {
         int counts = validateIsNumber(trialCounts);
@@ -21,7 +22,7 @@ public class TrialCountsValidator {
     }
 
     private void validateIsZero(int counts) {
-        if (counts <= 0) {
+        if (counts <= TRIAL_COUNTS_STANDARD) {
             throw new IllegalArgumentException(EXCEPTION_MESSAGE_NOT_MINUS_NUMBER);
         }
     }
