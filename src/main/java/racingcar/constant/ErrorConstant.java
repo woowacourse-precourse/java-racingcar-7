a287b808c33b38136f3fbe30f9e1ed10646110dd
mingking2 <mingee641@gmail.com>
2024-10-26 22:03:30 +0900

    feat: 에러처리를 위한 상수 추가

---
+++ src/main/java/racingcar/constant/ErrorConstant.java
@@ -0,0 +1,8 @@
+package racingcar.constant;
+
+public class ErrorConstant {
+    public static final String ERROR_DUPLICATE_NAME = "중복된 자동차 이름이 존재합니다.";
+    public static final String ERROR_LENGTH_NAME = "자동차 이름의 길이는 5 이하여야 합니다.";
+    public static final String ERROR_EMPTY_NAME = "자동차 이름은 공백이 될 수 없습니다.";
+    public static final String ERROR_DELIMITER_NAME = "입력값에 쉼표가 없습니다";
+}
