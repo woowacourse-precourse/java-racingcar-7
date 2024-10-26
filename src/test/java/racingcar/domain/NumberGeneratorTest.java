f78686331024322366bd4a37a8b3d63125158359
dnwldla <leeemjw@naver.com>
2024-10-26 15:13:05 +0900

    test: RandomNumberGenerator에서 NumberGenerator로 테스트 변경 인터페이스 도입에 따라 구현이 아닌 설계에 의존하게 한다

---
+++ src/test/java/racingcar/domain/NumberGeneratorTest.java
@@ -0,0 +1,21 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NumberGeneratorTest {
+
+    NumberGenerator numberGenerator;
+
+    @BeforeEach
+    void setUp(){
+        numberGenerator=new RandomNumberGenerator();
+    }
+    @Test
+    void 랜덤수는_0과_9_사이이다(){
+        assertThat(numberGenerator.generateNumber()).isBetween(0,9);
+    }
+
+}
