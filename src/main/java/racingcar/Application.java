939cbe3d37619472d650de2fd87ed1111ee71dea
jjr7181 <jjr7181@gmail.com>
2024-10-26 16:26:48 +0900

    feat: implement car name input for racing game

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼포(,) 기준으로 구분)");
+        String players = Console.readLine();
+
     }
 }
