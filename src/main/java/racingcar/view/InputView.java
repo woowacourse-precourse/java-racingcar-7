d96eef1d5c970730c2b30efcb0e3659bd22b2bd5
honggyu1233@naver.com <honggyu1233@naver.com>
2024-10-27 00:42:22 +0900

    feat : InputView 수정 - Racingcar 이름 및 전진 횟수 반환수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,9 +3,9 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String racingcarInputView() {
+    public static Object racingcarInputView() {
         String inputName=Console.readLine();
         int moveCount=Integer.parseInt(Console.readLine());
-        return inputName;
+        return new Object[]{inputName, moveCount};
     }
 }
