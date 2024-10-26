1876252afcc008c93574c2042195e6d4c8964eb4
mikekks <thdalsrb79@naver.com>
2024-10-26 18:45:58 +0900

    fix(RealRandom): pickNumberInList -> pickNumberInRange 메서드 사용

--- src/main/java/racingcar/common/RealRandom.java
+++ src/main/java/racingcar/common/RealRandom.java
@@ -1,12 +1,10 @@
 package racingcar.common;
 
-import java.util.List;
-
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RealRandom implements Random{
 	@Override
 	public int pickRandomNumber() {
-		return Randoms.pickNumberInList(List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+		return Randoms.pickNumberInRange(0, 9);
 	}
 }
