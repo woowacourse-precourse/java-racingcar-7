b0858e7dc25d80874f1760e3ab4caa2a9fa5c824
mirolis6636 <mirolis6636@gmail.com>
2024-10-26 09:05:55 +0900

    feat: race 메서드를 RacingCar 클래스에 추가
    시도 횟수만큼 시도하고 우승자를 출력한다

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,8 +1,13 @@
 package racingcar;
 
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class RacingCar{
     private String[] carNames;
+    private HashMap<String, Integer> record;
     private int numOfTrial;
 
     public void readUserInput(){
@@ -12,6 +17,35 @@ public class RacingCar{
         System.out.println("시도할 횟수는 몇 회인가요?");
         String stringUserInput = camp.nextstep.edu.missionutils.Console.readLine();
         numOfTrial = Integer.parseInt(stringUserInput);
+    }
+
+    public void race(){
+        while(numOfTrial>0){
+            for(String car : carNames) {
+                int randomNumber = Randoms.pickNumberInRange(0, 9);
+                Integer numOfMovement = record.getOrDefault(car, 0);
+                record.put(car, numOfMovement+1);
+            }
+            System.out.println("실행 결과");
+            for(String car : carNames){
+                System.out.printf("%s : ", car);
+                for(int i=0; i<record.getOrDefault(car, 0); ++i){
+                    System.out.printf("%c", '-');
+                }
+                System.out.println();
+            }
+            --numOfTrial;
+        }
+        Optional<Integer> longestMovement = record.values().stream().max(Integer::compareTo);
+        ArrayList<String> winner = new ArrayList<>();
+        for(String car : carNames){
+            if(record.get(car).equals(longestMovement)){
+                winner.add(car);
+            }
+        }
+        System.out.printf("최종 우승자 : ");
+        String result = String.join(",", winner);
+        System.out.println(result);
     }
 
     public String[] getCarNames() {
