fade74ad811589b2caa73bca94a781a53fdd3275
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2024-10-26 18:55:08 +0900

    fix : 자동차의 개수를 테스트하기 위해 캡슐화가 깨지지 않도록 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,6 +1,6 @@
 package racingcar;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
@@ -11,7 +11,10 @@ public class CarTest extends NsTest{
     @Test
     void 자동차_개수_테스트() {
         Cars cars = new Cars("핍비,피비");
-        assertThat(cars.getCarCount()).isEqualTo(2);
+        assertThat(cars
+                .getCarMoveHistoryDtoList()
+                .size())
+                .isEqualTo(2);
     }
 
     @Override
