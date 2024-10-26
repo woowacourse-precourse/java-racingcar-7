c0c464e2b704d9fcf6ecf3f85d59d3c9210ecb89
hmlssghm <kwonse326@gmail.com>
2024-10-26 21:52:52 +0900

    feat(Application): Application.main 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import racingcar.controller.Controller;
+import racingcar.controller.validation.AttemptNumberValidator;
+import racingcar.controller.validation.InputValidator;
+import racingcar.controller.validation.NameValidator;
+import racingcar.model.InputParser;
+import racingcar.service.Service;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: Factory or 다른 방법 있는지?
+        InputParser inputParser = new InputParser();
+        InputValidator<List<String>> nameValidator = new NameValidator();
+        InputValidator<String> attemptValidator = new AttemptNumberValidator();
+        Service service = new Service();
+        Controller controller = new Controller(inputParser, nameValidator, attemptValidator, service);
+        controller.run();
     }
 }
