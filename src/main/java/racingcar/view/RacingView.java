05bc9db877e64fbaff0a739de174b50af032cc43
kungbi <kipper12335@gmail.com>
2024-10-27 00:09:42 +0900

    refactor(view): Console.readLine의 예외 처리

--- src/main/java/racingcar/view/RacingView.java
+++ src/main/java/racingcar/view/RacingView.java
@@ -3,17 +3,30 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import racingcar.exception.ExceptionUtils;
+import racingcar.exception.GeneralError;
 
 public class RacingView {
 
+    private String readline() {
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException e) {
+            ExceptionUtils.throwIllegalArgException(GeneralError.EMPTY_INPUT);
+        }
+        return null;
+    }
+
+
     public String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+        return this.readline();
     }
 
     public String inputRoundCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return Console.readLine();
+        return this.readline();
     }
 
     public void printRoundResult(Map<String, Integer> carPositions) {
