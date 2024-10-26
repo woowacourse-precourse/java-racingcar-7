28c8d83fd5d71b334e0023a9afc5a2a1c60f8aa2
gladhee <gladhee@icloud.com>
2024-10-27 00:11:35 +0900

    Test(CarsTest): Cars 테스트 작성
    생성성공시 테스트
    중복된 이름 입력시 테스트

---
+++ src/test/java/racingcar/domain/car/CarsTest.java
@@ -0,0 +1,28 @@
+package racingcar.domain.car;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.domain.Cars;
+
+public class CarsTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,woni,jun"})
+    void 자동차_생성_테스트(String carNames) {
+        Cars cars = new Cars(carNames);
+
+        assertThat(cars).isInstanceOf(Cars.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,woni,woni"})
+    void 자동차_생성시_중복된_이름_입력_예외_테스트(String carNames) {
+        assertThatThrownBy(() -> new Cars(carNames))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("중복된 이름이 존재합니다.");
+    }
+
+}
