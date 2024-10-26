d3da0c3e093cb6d224abb67c9a3c2f4b7d57e8d2
lsj0822 <lsj0822@khu.ac.kr>
2024-10-26 16:40:09 +0900

    feat(RacingCar) : 기능 추가
    RacingCar의 기능 추가
    - 랜덤한 결과에 따라서 이동하는 addDistance 추가
    - getter 추가
    - 임시적인 결과를 출력할 때 별도의 처리과정 없이 출력하기 위해 toString 메서드 오버라이딩
    - 거리 value와 결과를 비교하기 위한 메서드 compareTo 추가

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import utils.PlayerMoving;
+
 public class RacingCar {
     private String carName;
     private int distance = 0;
@@ -7,4 +9,30 @@ public class RacingCar {
     public RacingCar(String carName) {
         this.carName = carName;
     }
+
+    protected void addDistance(){
+        if(PlayerMoving.isMoving()){
+            distance++;
+        }
+    }
+
+    public String getCarName(){
+        return carName;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    @Override
+    public String toString(){
+        return carName + " : " + "-".repeat(distance);
+    }
+
+    public int compareTo(int value){
+        if(distance > value) {return 1;}
+        if(distance == value){ return 0;}
+        return -1;
+    }
+
 }
