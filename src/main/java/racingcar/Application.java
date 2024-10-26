97c812811e65861a53f92af7ef6233cb2e27d4be
koo-sangwoo <luckytkddn@gmail.com>
2024-10-27 01:38:39 +0900

    fix : 시스템 콘솔에 값을 입력하지 않았을 때 예외처리
    - 시스템 args에 두 개의 값중 하나만 들어와도 예외 발생
    - 해당 예외 포메팅 작업 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,14 +6,24 @@ import racingcar.util.game.GameUtil;
 import racingcar.vo.CarVO;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Application {
     public static void main(String[] args) {
-        String carNames = Console.readLine();
-        Integer turnNumber = Integer.valueOf(Console.readLine());
+        try {
+            System.out.println("경주할 자동차의 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)");
+            String carNames = Console.readLine();
+            System.out.println(carNames);
 
-        List<CarVO> carObjectList = CarUtil.getCarNameAndGoCountList(carNames);
-        List<CarVO> afterGameCarVOList = GameUtil.oneGamePrintAndUpdate(carObjectList, turnNumber);
-        System.out.println(GameUtil.getWinnerName(afterGameCarVOList));
+            System.out.println("시도할 횟수는 몇번인가요?");
+            Integer turnNumber = Integer.valueOf(Console.readLine());
+            System.out.println(turnNumber);
+
+            List<CarVO> carObjectList = CarUtil.getCarNameAndGoCountList(carNames);
+            List<CarVO> afterGameCarVOList = GameUtil.oneGamePrintAndUpdate(carObjectList, turnNumber);
+            System.out.println(GameUtil.getWinnerName(afterGameCarVOList));
+        } catch (NoSuchElementException noSuchElementException) {
+            new IllegalArgumentException("시스템에 값이 입력되지 않았습니다!");
+        }
     }
 }
