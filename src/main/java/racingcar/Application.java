8bdc415d8fd7baffb5d1da2c3fd0656db53421c7
DESKTOP-TMDOSVL\user <tlsekdn505@naver.com>
2024-10-26 16:38:01 +0900

    자동차 이름 유효성 검사 로직 추가(쉼표 구분 및 5글자 이하 입력)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,14 +7,30 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        List <String> carNames = getCarNames();
-        System.out.println("입력한 자동차 이름"  + carNames);
+        try {
+            List<String> carNames = getCarNames();
+            System.out.println("입력한 자동차 이름: " + carNames);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private static List<String> getCarNames(){
         System.out.println("자동차 이름을 입력해주세요.");
         String inputCarName = Console.readLine();
-        return Arrays.asList(inputCarName.split(","));
+        return validCarNames(inputCarName);
+    }
+
+    private static List<String> validCarNames(String userInput){
+        List<String> carNames = Arrays.asList(userInput.split(","));
+
+        for(String carName : carNames){
+            if(carName.length() > 5){
+                throw new IllegalArgumentException("자동차 이름은 5글자 이하만 입력 가능합니다.");
+            }
+        }
+
+        return carNames;
     }
 
 }
