dea6bacb4fd3c97c2d2f0cbe3d93666fac27ea88
lsj0822 <lsj0822@khu.ac.kr>
2024-10-26 20:08:59 +0900

    fix(PlayerMoving) : 기능 결합
    -지나치게 적은 메서드가 각 클래스마다 있다고 판단하여 기능을 결합하여 하나의 메서드로 관리하게 수정

--- src/main/java/utils/PlayerMoving.java
+++ src/main/java/utils/PlayerMoving.java
@@ -1,9 +1,17 @@
 package utils;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static constant.Constants.RACING_NUMBER_MAXIMUM;
+import static constant.Constants.RACING_NUMBER_MINIMUM;
+
 import constant.Constants;
 
 public class PlayerMoving {
     public static boolean isMoving(){
-        return (RandomNumberGenerator.createMovingNumber() >= Constants.MOVING_CRITERIA);
+        return (randomNumber() >= Constants.MOVING_CRITERIA);
+    }
+
+    public static int randomNumber(){
+        return pickNumberInRange(RACING_NUMBER_MINIMUM, RACING_NUMBER_MAXIMUM);
     }
 }
