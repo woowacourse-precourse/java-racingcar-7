700a98bcc820ff5fa8345dab7c86acee78fac8f1
wogus <ind07152@naver.com>
2024-10-26 16:59:40 +0900

    feat(NumberType): 기본 숫자 상수화를 위한 enum 타입 생성
    NumberType에 기본 숫자들을 상수화한다.

---
+++ src/main/java/racingcar/constant/NumberType.java
@@ -0,0 +1,21 @@
+package racingcar.constant;
+
+public enum NumberType {
+    MIN_CAR_SIZE(2),
+    MIN_COUNT(1),
+    MAX_NAME_LENGTH(5),
+    MIN_RANDOM_NUMBER(0),
+    MAX_RANDOM_NUMBER(9),
+    START_LOCATION(0),
+    NUMBER_THRESHOLD(4);
+
+    private final int number;
+
+    NumberType(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
