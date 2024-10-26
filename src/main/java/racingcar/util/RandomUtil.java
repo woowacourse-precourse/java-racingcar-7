6e05064a07a4f44eaa897e86fe7a7bd545b3678d
wvssm <neoramiregi@gmail.com>
2024-10-26 22:55:45 +0900

    feat(RandomUtil): 0에서 9사이의 난수를 발생시킨다

---
+++ src/main/java/racingcar/util/RandomUtil.java
@@ -0,0 +1,12 @@
+package racingcar.util;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+public class RandomUtil {
+    public static final int RANDOM_NUMBER_MIN = 0;
+    public static final int RANDOM_NUMBER_MAX = 9;
+
+    public int generateRandomNumber() {
+        return pickNumberInRange(RANDOM_NUMBER_MIN, RANDOM_NUMBER_MAX);
+    }
+}
