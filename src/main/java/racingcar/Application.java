b9c4e6f92345b3e35319d3bef5a51be1d64397f3
kssk3 <kss8014@daum.net>
2024-10-26 14:41:12 +0900

    Car 클래스

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -16,9 +16,12 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
         Scanner scanner = new Scanner(System.in);
+        Car car = new Car();
         System.out.print("경주할 자동차 이름을 입력하세.(이름은 쉼포(,) 기준으로 구분");
+
+        int n = scanner.nextInt();
+        System.out.println("시도할 횟수는 몇 회인가요?");
 
 
     }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -7,20 +7,12 @@ public class Car {
     private String name;
     private int currentPosition;
 
-    public Car(String name) {
-        this.name = name;
-        this.currentPosition = 0;
+    public void moveForward() {
+        currentPosition += 1;
     }
 
-    public void move() {
-        Random random = new Random();
-        if (name.length() > Constants.CAR_NAME_LENGTH_MAX) {
-          
-        }
-
+    public int getCurrentPosition() {
+        return currentPosition;
     }
-
-
-
 }
-}
+
