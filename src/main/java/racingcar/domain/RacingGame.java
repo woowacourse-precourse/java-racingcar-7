879b1b20f70799229a574b8c70ec850b3b1e853c
mikekks <thdalsrb79@naver.com>
2024-10-26 18:24:08 +0900

    feat(RacingGame): 게임 시작하는 기능 구현

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -13,6 +13,20 @@ public class RacingGame {
 		this.random = random;
 	}
 
+	public void start() {
+		Output.printRequestCarNames();
+		List<String> carNames = Input.readCarNames();
+		Cars cars = new Cars(carNames.stream().map(Car::new).toList());
+
+		Output.printRequestTurnCount();
+		Turn turn = new Turn(Input.readTurn());
+
+		move(cars, turn);
+
+		Winners winners = new Winners(cars.getWinners());
+
+	}
+
 	private void move(Cars cars, Turn turn) {
 		// 문자 출력
 
