780489b05f4952edff2f1d4a2c86c77213575730
yuntasha <jaljayo85@gmail.com>
2024-10-27 02:34:27 +0900

    feat(ReaderHelper.java): write PrintHelper
    Add method list:
    - printScoreBoard
    - getState
    - getState
    
    Update README.md:
    - delete 플레이어 객체를 받아 게임 상황에 맞게 반환
    - check to solved problem

---
+++ src/main/java/racingcar/view/PrintHelper.java
@@ -0,0 +1,53 @@
+package racingcar.view;
+
+import racingcar.model.Player;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+public class PrintHelper {
+    private static final String POINT = "-";
+    private static final String COLON = ":";
+    private static final String BLANK = " ";
+    private static final String LINE_BREAK = "\n";
+    private static final String WINNER_IS = "최종 우승자 : ";
+    private static final String COMMA = ",";
+
+    public void printScoreBoard(List<Player> players) {
+        StringJoiner scoreBoard = new StringJoiner(LINE_BREAK);
+
+        for (Player p : players) {
+            scoreBoard.add(getState(p));
+        }
+
+        System.out.println(scoreBoard.toString());
+    }
+
+    private String getState(Player p) {
+        StringBuilder state = new StringBuilder();
+
+        state.append(p.getName());
+
+        state.append(BLANK);
+        state.append(COLON);
+        state.append(BLANK);
+
+        for (int i = 0; i < p.getScore(); i++) {
+            state.append(POINT);
+        }
+
+        return state.toString();
+    }
+
+    public void printResult(List<Player> players) {
+        StringJoiner winner = new StringJoiner(COMMA + BLANK);
+
+        for (Player player : players) {
+            winner.add(player.getName());
+        }
+
+        String result = WINNER_IS + winner.toString();
+
+        System.out.println(result);
+    }
+}
