ed96aef4c01d549903078ac7740362c014f42af3
zionhann <hanzion1108@gmail.com>
2024-10-27 01:22:33 +0900

    feat: 무작위값에 따라 전진 혹은 정지

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,12 +3,23 @@ package racingcar;
 public class Car {
 
     private final String name;
+    private int position = 0;
 
     public Car(String name) {
         this.name = name.trim();
     }
 
+    public void attemptMove(int randomValue) {
+        if (randomValue >= 4) {
+            position++;
+        }
+    }
+
     public String getName() {
         return name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -14,4 +14,22 @@ public class CarTest {
 
         assertThat(car.getName().length()).isLessThanOrEqualTo(5);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void 무작위값이_4이상인_경우_전진(int randomValue) {
+        Car car = new Car("pobi");
+        car.attemptMove(randomValue);
+
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void 무작위값이_3이하인_경우_정지(int randomValue) {
+        Car car = new Car("pobi");
+        car.attemptMove(randomValue);
+
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
 }
