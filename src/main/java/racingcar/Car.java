83ca7e8abd9b4594326e2b84fa97d833fbf9f77f
나상연 <58220151+ense333@users.noreply.github.com>
2024-10-26 22:29:48 +0900

    feat(racingcar): add Car class with name and distance properties
    The Car class will represent individual cars in the racing game.
    Each car has a name to identify it and a distance property to track how far it has moved during the race.
    
    This will serve as a foundational class for further implementation of the racing logic.

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+public class Car {
+
+    private String carName;
+    private int distance;
+
+    public Car(String carName){
+        this.carName = carName;
+        distance = 0;
+    }
+}
