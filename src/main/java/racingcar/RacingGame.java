7900b7f8295396c6e18bf21014eb6d2da3540e3b
leesh <leesh7807@gmail.com>
2024-10-27 02:22:34 +0900

    docs: add Javadoc in RacingGame class

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,16 +4,30 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * The class {@code RacingGame} operates a racing game simulation. The game starts by receiving input from the user.
+ * Once the race begins, it progresses automatically until the results are displayed.
+ */
 public class RacingGame {
     RacingInput racingInput = new RacingInput();
     List<Car> cars;
     int totalRounds;
 
+    /**
+     * Receiving input by {@link RacingInput#receiveCarNames()} and {@link RacingInput#receiveTotalRounds()}.
+     */
     public void receiveInput() {
         cars = Car.from(racingInput.receiveCarNames());
         totalRounds = racingInput.receiveTotalRounds();
     }
 
+    /**
+     * A race progresses until the results are displayed.
+     *
+     * <p>In each round, all cars attempt to move forward, and the result is shown.
+     * After all rounds are over, the winner of the game is announced. If there are multiple winners, they are announced
+     * together.</p>
+     */
     public void play() {
         System.out.println("\n실행 결과");
         IntStream.range(0, totalRounds).forEach(i -> doRound());
