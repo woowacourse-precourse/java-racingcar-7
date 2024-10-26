e9c2f37bcc114021a15f643986f9762078eb8e6d
seonseon1515 <seonseon1515@gmail.com>
2024-10-26 20:43:22 +0900

    feat: 자동차 이름 입력값 ,를 기준으로 분할하여 ArrayList에 담기

---
+++ src/main/java/racingcar/InputHandler.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class InputHandler {
+    private static ArrayList<String> carNameList;
+
+    static ArrayList<String> handleCarNames(){
+        InputView inputView = new InputView();
+        carNameList = Arrays.stream(inputView.inputCarName().split(","))
+                .collect(Collectors.toCollection(ArrayList::new));
+        return carNameList;
+    }
+
+    public static ArrayList<String> getCarNameList() {
+        return carNameList;
+    }
+}
