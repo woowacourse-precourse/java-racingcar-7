5c824071cf764f9d417edb0b7e2ff19c7fae2098
gorapaduckoo <jioung93@naver.com>
2024-10-27 03:37:04 +0900

    feat: 시도 횟수 입력 메세지 출력 기능 추가

--- src/main/java/racingcar/GameView.java
+++ src/main/java/racingcar/GameView.java
@@ -6,4 +6,9 @@ public class GameView {
     public void printCarNameInputMessage() {
         System.out.println(CAR_NAME_INPUT_MESSAGE.getMessage());
     }
+
+    public void printRoundCountInputMessage() {
+        System.out.println(ROUND_COUNT_INPUT_MESSAGE.getMessage());
+    }
+
 }
