799495bb5b881aafa5d016198798a8e45488f95a
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-10-26 16:46:38 +0900

    feat(InputView): 경주할 자동차 이름을 입력받는 메서드 생성
    - 자동차의 이름은 콤마(,)로 구분되어야 한다.
    - 형식을 지키지 않을 시엔 예외를 던진다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -13,4 +13,14 @@ public class InputView {
             throw new IllegalArgumentException("숫자를 입력해 주세요.");
         }
     }
+
+    public final String readCarNames() {
+        String carNames = Console.readLine();
+        Console.close();
+        if (carNames.matches("^([^\\s,]+)(,[^\\s,]+)*$")) {
+            return carNames;
+        } else {
+            throw new IllegalArgumentException("잘못된 입력입니다. 이름은 쉼표(,)로 구분해주세요.");
+        }
+    }
 }
