eb70213d77f3c71294da73733852092f35be6698
woodongle <315ehdgur@gmail.com>
2024-10-26 16:03:08 +0900

    feat: 첫 번째 메시지에 대한 사용자 입력 검증 메서드 추가
    1. 사용자 입력에 중복된 자동차 이름이 있는지 확인하는 메서드를 추가합니다.
      - checkRedundancy()

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,14 +8,15 @@ import java.util.ListIterator;
 
 public class Application {
     public static void main(String[] args) {
-        // 경주할 자동차 이름 입력(참가자 수)
-        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
-
-        // 사용자 입력에서 공백을 제거하고 쉼표로 구분해서 가변 컬렉션인 ArrayList에 저장
-        List<String> inputList = new ArrayList<>(Arrays.asList(Console.readLine().split("\\s*,\\s*")));
-
         try {
+            // 경주할 자동차 이름 입력(참가자 수)
+            System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+
+            // 사용자 입력에서 공백을 제거하고 쉼표로 구분해서 가변 컬렉션인 ArrayList에 저장
+            List<String> inputList = new ArrayList<>(Arrays.asList(Console.readLine().split("\\s*,\\s*")));
             validateInput(inputList);
+            checkRedundancy(inputList);
+
             System.out.println(inputList);
         } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
@@ -39,6 +40,13 @@ public class Application {
             if (str.matches(".*[`~!@#$%^&*()\\-_=+{}\\[\\];:'\",.<>/?|\\\\].*")) {
                 throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
             }
+        }
+    }
+
+    // 중복된 자동차 이름이 있는지 확인하는 메서드
+    private static void checkRedundancy(List<String> inputList) {
+        if (inputList.size() != inputList.stream().distinct().count()) {
+            throw new IllegalArgumentException("중복된 이름이 존재합니다.");
         }
     }
 }
