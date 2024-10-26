fc8a835502ad0e389c4227d115374c9487011b3f
Taeho KIM <laferrari963@naver.com>
2024-10-26 12:21:19 +0900

    feat(Application):메인함수 구현
    메인 함수 작성.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
+import racingcar.domain.GameSettings;
+import racingcar.service.RacingGame;
+import racingcar.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameSettings settings = InputView.getGameSettings();
+        RacingGame game = new RacingGame(settings);
+        game.start();
     }
 }
