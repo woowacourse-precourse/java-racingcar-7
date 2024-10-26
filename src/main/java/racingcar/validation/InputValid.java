0e0f9e310e08870c8b2db4d9840282170a037b1f
mingking2 <mingee641@gmail.com>
2024-10-26 22:34:26 +0900

    feat: 입력 검증 클래스 추가

---
+++ src/main/java/racingcar/validation/InputValid.java
@@ -0,0 +1,51 @@
+package racingcar.validation;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import racingcar.constant.InputConstant;
+import racingcar.constant.NumberConstant;
+import racingcar.exception.input.name.DelimiterNameException;
+import racingcar.exception.input.name.DuplicateNameException;
+import racingcar.exception.input.name.EmptyNameException;
+import racingcar.exception.input.name.LengthNameException;
+import racingcar.exception.input.name.MultipleNameException;
+
+
+public class InputValid {
+
+    public static void validEmptyName(String carNames) {
+        if (carNames.isEmpty()) {
+            throw new EmptyNameException();
+        }
+    }
+
+    public static void validDelimiterName(String carNames) {
+        if (!carNames.contains(InputConstant.DELIMITER)) {
+            throw new DelimiterNameException();
+        }
+    }
+
+    public static void validMultipleName(List<String> carNameList) {
+        if (carNameList.size() < NumberConstant.MIN_NAME_COUNT) {
+            throw new MultipleNameException();
+        }
+    }
+
+    public static void validLengthName(List<String> carNameList) {
+        for (String carName : carNameList) {
+            if (carName.length() > NumberConstant.SET_CAR_NAME_LENGTH) {
+                throw new LengthNameException();
+            }
+        }
+    }
+
+    public static void validDuplicateName(List<String> carNameList) {
+        Set<String> nameSet = new HashSet<>();
+        for (String carName : carNameList) {
+            if (!nameSet.add(carName)) {
+                throw new DuplicateNameException();
+            }
+        }
+    }
+}
