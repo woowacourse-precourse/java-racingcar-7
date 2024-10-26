9748be26524bf47a8ece1a339ad1ba59a3ce4753
woogym <kwj9294@gmail.com>
2024-10-27 07:00:41 +0900

    feat(utils): Implement move decider for Race model

---
+++ src/main/java/racingcar/utils/MoveDecider.java
@@ -0,0 +1,21 @@
+package racingcar.utils;
+
+public class MoveDecider {
+    private static final int MOVE_THRESHOLD = 4;
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
+
+    private final NumberGenerator numberGenerator;
+
+    public MoveDecider(RandomNumberGenerator randomNumberGenerator) {
+        this.numberGenerator = randomNumberGenerator;
+    }
+
+    public boolean isMovable() {
+        return getRandomNumber() >= MOVE_THRESHOLD;
+    }
+
+    private int getRandomNumber() {
+        return numberGenerator.generate(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+    }
+}
