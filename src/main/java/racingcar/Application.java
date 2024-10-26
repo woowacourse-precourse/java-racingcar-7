25a2673993e0662c9a5bd8d2ffd97284b187927f
migyeong <wjdalrud5516@gmail.com>
2024-10-27 00:09:11 +0900

    첫번째 메소드 추출
    자동차 이름 입력 후 콤마(,)를 기준으로 문자열(자동차 이름) 구분 메소드 생성
    구분한 자동차 이름마다 <자동차 이름,0>으로 초기화한 LinkedHashMap을 반환하는 메소드 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,16 +9,8 @@ public class Application {
     public static final int DISTANCE_POINT = 4;
 
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
-        String inputName = Console.readLine();
-        String[] nameArray = inputName.split(",");
-        Map<String, Integer> nameMap = new LinkedHashMap<>();
-        for (String name : nameArray) {
-            if(name.length() > 5){
-                throw new IllegalArgumentException();
-            }
-            nameMap.put(name,0);
-        }
+        String[] nameArray = getStrings();
+        Map<String, Integer> nameMap = getNameMap(nameArray);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String inputCount = Console.readLine();
@@ -53,5 +45,23 @@ public class Application {
             }
         }
         System.out.println("최종 우승자 : " + String.join(", ", winnerList));
+    }
+
+    private static Map<String, Integer> getNameMap(String[] nameArray){
+        Map<String, Integer> nameMap = new LinkedHashMap<>();
+        for (String name : nameArray) {
+            if(name.length() > 5){
+                throw new IllegalArgumentException();
+            }
+            nameMap.put(name,0);
+        }
+        return nameMap;
+    }
+
+    private static String[] getStrings(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String inputName = Console.readLine();
+        String[] nameArray = inputName.split(",");
+        return nameArray;
     }
 }
