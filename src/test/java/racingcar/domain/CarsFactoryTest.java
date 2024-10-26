eab79a3d039298ccd8aa1477e5d8b67e337808ef
dnwldla <leeemjw@naver.com>
2024-10-26 21:51:19 +0900

    test: 입력 검증 테스트를 Cars에서 CarsFactory로 이동

---
+++ src/test/java/racingcar/domain/CarsFactoryTest.java
@@ -0,0 +1,52 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CarsFactoryTest {
+
+    @Test
+    void 중복된_이름을_입력했을때_예외_발생(){
+        String carsInput="지우,지우,포비";
+        assertThatThrownBy(()->CarsFactory.makeCarList(carsInput)).
+                isInstanceOf(IllegalArgumentException.class).
+                hasMessageContaining("각 차는 한번만 입력해야 합니다");
+    }
+
+    @Test
+    void 특정_이름이_여섯글자_이상일때_예외_발생(){
+        String carsInput="포비,임지우임지우임";
+        assertThatThrownBy(()->CarsFactory.makeCarList(carsInput)).
+                isInstanceOf(IllegalArgumentException.class).
+                hasMessageContaining("차는 다섯글자 이하이어야 합니다");
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateCars")
+    void 올바르게_입력하면_객체를_반환한다(String input,List<Car> carList){
+     assertThat(CarsFactory.makeCarList(input)).usingRecursiveComparison().isEqualTo(carList);
+
+    }
+
+    static Stream<Arguments> generateCars(){
+        //string 입력 시 list를 반환한다
+        return Stream.of(
+                Arguments.arguments("지우,포비,제이",List.of(new Car("지우",""),
+                        new Car("포비",""),
+                        new Car("제이","")
+                )),
+                Arguments.arguments("지우,포비       ",List.of(new Car("지우",""),
+                        new Car("포비","")))
+        );
+    }
+
+
+}
--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -13,38 +13,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarsTest {
 
-    @Test
-    void 중복된_차를_입력했을때_예외_발생(){
-        String carsInput="지우,지우,임지우";
-        assertThatThrownBy(()->new Cars(carsInput)).
-                isInstanceOf(IllegalArgumentException.class).
-                hasMessageContaining("각 차는 한번만 입력해야 합니다");
-    }
-
-    @Test
-    void 특정_차가_여섯글자_이상일때_예외_발생(){
-        String carsInput="지우,임지우임지우임";
-        assertThatThrownBy(()->new Cars(carsInput)).
-                isInstanceOf(IllegalArgumentException.class).
-                hasMessageContaining("차는 다섯글자 이하이어야 합니다");
-    }
-
-    @ParameterizedTest
-    @MethodSource("generateCars")
-    void 올바르게_입력하면_객체를_반환한다(Cars actual,Cars expected){
-        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
-
-    }
-
-    static Stream<Arguments> generateCars(){
-        Cars actualCars=new Cars("임지우,이지우,박지우");
-        return Stream.of(
-                Arguments.arguments(new Cars("임지우,이지우,박지우"),actualCars),
-                Arguments.arguments(new Cars("임지우    ,이지우  , 박지우   "),actualCars),
-                Arguments.arguments(new Cars("임지우,임 -지:"),new Cars("임지우,임-지:"))
-                );
-    }
-
     @ParameterizedTest
     @MethodSource("findWinners")
     void 우숭자를_구한다(Cars cars,String result){
