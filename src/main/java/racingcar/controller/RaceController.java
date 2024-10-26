ea10cee1c9e7209b616eb4950997e041e65419fb
acupoframen <choe.hwan@gmail.com>
2024-10-26 19:01:21 +0900

    fix (controller): fix name input issue
    invalid input check:
    - name already in the list
    - name longer than 5
    - name not in english

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,17 +1,21 @@
 package racingcar.controller;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import racingcar.model.Race;
 import racingcar.view.InputView;
 import racingcar.view.ResultView;
 
 public class RaceController {
     public void run() {
-        String[] carNames = InputView.getCarNames();
+        String carNames = InputView.getCarNames();
+
+        String[] carNamesList=wrongCheckName(carNames);
         int rounds = InputView.getRounds();
 
-        Race race = new Race(Arrays.asList(carNames), rounds);
+        Race race = new Race(Arrays.asList(carNamesList), rounds);
 
         for (int i = 0; i < rounds; i++) {
             race.start();
@@ -20,5 +24,24 @@ public class RaceController {
 
         List<String> winners = race.getWinners();
         ResultView.printWinners(winners);
+    }
+    public String[] wrongCheckName(String carname){
+        String[] CarNameList=carname.split(",");
+        Set <String> names=new HashSet<>();
+        for (String name: CarNameList){
+            name=name.trim();
+            if (!names.add(name)) {
+                throw new IllegalArgumentException("이름이 중복됨");
+            }
+            if (name.length()>5) {
+                throw new IllegalArgumentException("이름이 6글자 이상임");
+            }
+            if (!name.matches("[a-zA-Z]+")){
+                throw new IllegalArgumentException("이름 형식 오류");
+            }
+            names.add(name);
+        }
+        return names.toArray(new String[0]);
+
     }
 }
