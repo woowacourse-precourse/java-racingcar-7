5d991f1830a25dcf4d5783180cc6c16cf9f8a676
정소민 <yjk70702002@gmail.com>
2024-10-26 22:24:18 +0900

    feat: 경주할 자동차 이름 입력받는 기능

---
+++ src/main/java/racingcar/game/RacingGame.java
@@ -0,0 +1,10 @@
+package racingcar.game;
+
+import racingcar.io.InputHandler;
+
+public class RacingGame {
+    private final InputHandler inputHandler = new InputHandler();
+    public void run() {
+        String[] carNames = inputHandler.getCarNames();
+    }
+}
