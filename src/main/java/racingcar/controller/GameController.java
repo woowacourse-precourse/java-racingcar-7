11c9a72f80f5b7e055465f926bd6c25991304db8
himinqq <alsrud0966@naver.com>
2024-10-27 02:18:35 +0900

    feat(GameController): 게임 실행 횟수 입력받는 기능 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -2,10 +2,12 @@ package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import racingcar.service.GameService;
 import racingcar.view.InputView;
 
 public class GameController {
     private Validator validator = new Validator();
+    private GameService gameService = new GameService();
 
     public List<String> getCarNames(){
         InputView.carNamesMessage();
@@ -13,6 +15,10 @@ public class GameController {
         return validator.validateCarName(carNames);
     }
 
-
-
+    public int getGameCount(){
+        InputView.gameCountMessage();
+        String gameCountString = Console.readLine();
+        int gameCount = Integer.parseInt(gameCountString);
+        return validator.validateGameCount(gameCount);
+    }
 }
