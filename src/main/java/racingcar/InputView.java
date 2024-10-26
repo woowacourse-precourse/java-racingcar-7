33d829b2f4fbccfc61e4360f163585afbdfd5380
Yehun <yhyh4420@naver.com>
2024-10-26 13:24:05 +0900

    refactor(main):출력로직 수정
    - 출럭 시 줄바꿈 수정
        - InputView에서 println을 print로 수정
        - OutputView에서 println을 print로 수정

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -5,14 +5,14 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public String[] getUserinfo() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)기준으로 구분) : ");
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)기준으로 구분) : ");
         String User = Console.readLine();
         String[] listUser = User.split(",");
         return listUser;
     }
 
     public String getRoundinfo() {
-        System.out.println("시도할 횟수를 정수로 입력하세요 : ");
+        System.out.print("시도할 횟수를 정수로 입력하세요 : ");
         String round = Console.readLine();
         return round;
     }
--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -7,5 +7,4 @@ public class OutputView {
     public void displayResult(List listWinner) {
         System.out.print("최종 우승자 : " + String.join(",", listWinner));
     }
-
 }
