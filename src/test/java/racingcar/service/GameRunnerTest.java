bb15911b49d429305d37c9a83e9b0eec0fe95d76
kim jihyeon <jihyeon546@gmail.com>
2024-10-27 03:16:32 +0900

    test: 자동차 전진 조건 검증 테스트 추가

---
+++ src/test/java/racingcar/service/GameRunnerTest.java
@@ -0,0 +1,36 @@
+package racingcar.service;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import racingcar.Application;
+
+public class GameRunnerTest extends NsTest {
+    GameRunner gameRunner = new GameRunner();
+
+    @Test
+    void 자동차_전진_테스트() {
+        assertSimpleTest(() -> {
+            int randomValue = 4;
+            boolean isMoved = gameRunner.moveCarForward(randomValue);
+            assertThat(isMoved).isTrue();
+        });
+    }
+
+    @Test
+    void 자동차_멈춤_테스트() {
+        assertSimpleTest(() ->{
+            int randomValue = 1;
+            boolean isMoved = gameRunner.moveCarForward(randomValue);
+            assertThat(isMoved).isFalse();
+        });
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
