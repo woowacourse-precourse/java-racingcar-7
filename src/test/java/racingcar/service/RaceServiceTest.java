23ad9ceedafbac843eed1512936f636a627beb20
GiSeok Kang <ime.rokz@gmail.com>
2024-10-26 22:09:03 +0900

    test: 자동차 최대 범위 및 0개에 대한 테스트 코드 추가

--- src/test/java/racingcar/service/RaceServiceTest.java
+++ src/test/java/racingcar/service/RaceServiceTest.java
@@ -1,7 +1,9 @@
 package racingcar.service;
 
+import java.util.ArrayList;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -16,5 +18,30 @@ class RaceServiceTest {
         Assertions.assertThatThrownBy(() -> raceService.carListOf(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("중복되는 이름이 존재합니다.");
+    }
+
+    @DisplayName("자동차 개수 0개 되도록 입력")
+    @ParameterizedTest
+    @ValueSource(strings = {",", ",,", ",,,", ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,"})
+    void 공백_이름_입력(String input) {
+        Assertions.assertThatThrownBy(() -> raceService.carListOf(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차가 없습니다.");
+    }
+
+    @DisplayName("자동차 개수 최대 범위 입력")
+    @Test
+    void 자동차_최대개수_입력() {
+        ArrayList<String> names = new ArrayList<>();
+        for (char alpha = 'a'; alpha <= 'z'; alpha++) {
+            for (int cnt = 0; cnt < 4; cnt++) {
+                names.add(String.valueOf(alpha) + cnt);
+            }
+        }
+
+        String input = String.join(",", names);
+        Assertions.assertThatThrownBy(() -> raceService.carListOf(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차는 최대 100개까지 입력 가능합니다.");
     }
 }
