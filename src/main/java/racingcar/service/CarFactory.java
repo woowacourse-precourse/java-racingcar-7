1000dcc84dad1c455d957cce96755131396af695
YeaChan05 <qkenrdl05@gmail.com>
2024-10-26 21:31:34 +0900

    fix(CarFactory): 차량 생성 팩토리 검증 추가
    선수 수 및 이름 중복 검증 로직 추가

--- src/main/java/racingcar/service/CarFactory.java
+++ src/main/java/racingcar/service/CarFactory.java
@@ -1,12 +1,29 @@
 package racingcar.service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import racingcar.common.StringUtil;
 import racingcar.domain.Cars;
 
 public class CarFactory {
     public Cars createCars(String string) {
+        validateNoCar(string);
         List<String> names = StringUtil.splitByComma(string);
+        validateNames(names);
         return Cars.createCarsByNames(names);
+    }
+
+    private void validateNoCar(String string) {
+        if (string.trim().isBlank()) {
+            throw new IllegalArgumentException("racer must more than one");
+        }
+    }
+
+    private void validateNames(List<String> names) {
+        Set<String> set = new HashSet<>(names);
+        if (set.size() != names.size()) {
+            throw new IllegalArgumentException("duplicate names included");
+        }
     }
 }
