a2ccff041f318bfc9f4344f9065afd786414c9b4
wogus <ind07152@naver.com>
2024-10-26 16:22:46 +0900

    refactor(Validator): 커스텀 예외로 변경
    기본 IllegalArgumentException을 커스텀 예외로 변경한다.

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -1,8 +1,15 @@
 package racingcar.validator;
 
+import static racingcar.exception.ErrorMessage.INVALID_COUNT_RANGE;
+import static racingcar.exception.ErrorMessage.INVALID_NAME_COUNT;
+import static racingcar.exception.ErrorMessage.INVALID_NAME_LENGTH;
+import static racingcar.exception.ErrorMessage.NAME_DUPLICATE;
+import static racingcar.exception.ErrorMessage.NOT_INTEGER;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import racingcar.exception.RaceException;
 
 public final class Validator {
     public static void namesValidate(List<String> names) {
@@ -15,25 +22,24 @@ public final class Validator {
         try {
             int cnt = Integer.parseInt(count);
             if (cnt < 1) {
-                throw new IllegalArgumentException();
+                throw new RaceException(INVALID_COUNT_RANGE);
             }
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new RaceException(NOT_INTEGER);
         }
     }
-
 
     private static void nameLengthValidate(List<String> names) {
         names.forEach((name) -> {
             if (name.length() > 5) {
-                throw new IllegalArgumentException();
+                throw new RaceException(INVALID_NAME_LENGTH);
             }
         });
     }
 
     private static void nameCountValidate(List<String> names) {
         if (names.size() < 2) {
-            throw new IllegalArgumentException();
+            throw new RaceException(INVALID_NAME_COUNT);
         }
     }
 
@@ -41,7 +47,7 @@ public final class Validator {
         Set<String> checkDuplicate = new HashSet<>();
         names.forEach(name -> {
             if (!checkDuplicate.add(name)) {
-                throw new IllegalArgumentException();
+                throw new RaceException(NAME_DUPLICATE);
             }
         });
     }
