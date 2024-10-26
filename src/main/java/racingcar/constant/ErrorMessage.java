9c914ede8bd188ccf547f527a9cbb99bd72b6cd2
Yoon-Jemin <yjm1800@naver.com>
2024-10-27 02:05:42 +0900

    refactor(validator): 자동차 이름 검증 메서드 수정
    - 중복된 자동차 이름을 입력받은 경우 예외가 발생하도록 수정

--- src/main/java/racingcar/constant/ErrorMessage.java
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
 
     CARNAME_BLANK_EXCEPTION("자동차 이름은 공백이면 안됩니다."),
     CARNAME_LENGTH_EXCEPTION("자동차는 5자 이하만 입력이 가능합니다. 입력 받은 자동차 이름: "),
-    ATTEMPTS_FORMAT_EXCEPTION("시도 횟수 입려 형식이 잘못됐습니다. 입력 받은 횟수: ")
+    ATTEMPTS_FORMAT_EXCEPTION("시도 횟수 입려 형식이 잘못됐습니다. 입력 받은 횟수: "),
+    DUPLICATE_CARNAME_EXCCEPTION("중복된 자동차 이름을 입력할 수 없습니다. 중복된 자동차 이름 :")
     ;
 
     private final String message;
--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -1,5 +1,7 @@
 package racingcar.util;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 import static racingcar.constant.ErrorMessage.*;
@@ -8,13 +10,18 @@ public class Validator {
 
     public void checkCarNameInput(String input){
         String[] carNames = input.split(",");
+        Set<String> carNameSet = new HashSet<>();
         for(String carName : carNames){
+            if(carNameSet.contains(carName)){
+                throw new IllegalArgumentException(String.format(DUPLICATE_CARNAME_EXCCEPTION.getMessage(), carName));
+            }
             if(carName.isEmpty()){
                 throw new IllegalArgumentException(CARNAME_BLANK_EXCEPTION.getMessage());
             }
             if(carName.length() > 5){
                 throw new IllegalArgumentException(String.format(CARNAME_LENGTH_EXCEPTION.getMessage(), carName));
             }
+            carNameSet.add(carName);
         }
     }
 
