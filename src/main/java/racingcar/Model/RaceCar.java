2184856f096bb93216822e397d292592603423cd
mininiii <didals0521@gmail.com>
2024-10-26 18:48:08 +0900

    feat: 자동차 경주 기능 구현

---
+++ src/main/java/racingcar/Model/RaceCar.java
@@ -0,0 +1,29 @@
+package racingcar.Model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RaceCar {
+    private final List<Car> cars;
+
+    public RaceCar(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void race(int attempts) {
+        while (attempts-- > 0) {
+            move();
+        }
+    }
+
+    public void move() {
+        cars.forEach(this::moveEach);
+    }
+
+    public void moveEach(Car car) {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            car.moveCar();
+        }
+    }
+
+}
