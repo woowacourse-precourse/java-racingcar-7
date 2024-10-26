ebcd890afab453b32de72dfa2207c50d0ba40056
pokm321 <pokm90@gmail.com>
2024-10-26 15:19:14 +0900

    refactor: update getCarNames() to return input without a variable

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,9 +6,7 @@ public class InputView {
 
     public String getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String names = Console.readLine();
-
-        return names;
+        return Console.readLine();
     }
 
     public int getTotalRounds() {
