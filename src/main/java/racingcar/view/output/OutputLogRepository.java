778b53bfc5a70b867a6ec32dbb0a31b55bbecd2f
likerhythm <wjdalsdl0412@gmail.com>
2024-10-26 12:14:32 +0900

    refactor(folder): OutputLogRepository를 view 폴더로 이동

---
+++ src/main/java/racingcar/view/output/OutputLogRepository.java
@@ -0,0 +1,23 @@
+package racingcar.view.output;
+
+import racingcar.car.Car;
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class OutputLogRepository {
+
+    private final Map<String, List<BigInteger>> carMovementLog = new HashMap<>();
+
+    public void saveCarPositionLog(Car car) {
+        BigInteger currentPosition = car.getCurrentPosition();
+        String name = car.getName();
+
+        carMovementLog.computeIfAbsent(name, k -> new ArrayList<>());
+        carMovementLog.get(name).add(currentPosition);
+    }
+
+    public Map<String, List<BigInteger>> getCarPositionLog() {
+        return Collections.unmodifiableMap(carMovementLog);
+    }
+}
