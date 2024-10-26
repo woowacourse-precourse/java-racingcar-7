b92f6094403aeb592b30d35368fa7953bf815673
youngseo3 <wys4475@gmail.com>
2024-10-26 17:39:50 +0900

    refactor: 사용자 입력 기능 클래스 분할 사용자 입력을 받는 기능들이 더 많아질 것을 고려해 Main에서는 코드의 흐름만 집중할 수 있도록 InputView를 작성하였다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,25 +3,24 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
 
-        List<String> carNameList;
-        int n;
+        List<String> carNameList; // 자동차 이름을 입력 받는 List
+        int tryCount;
 
-        List<Car> carList = new ArrayList<>();
+        List<Car> carList = new ArrayList<>(); // Car 객체를 입력 받는 List
+        InputView inputView = new InputView();
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        carNameList = Arrays.stream(Console.readLine()
-                .split(","))
-                .toList();
+        carNameList = inputView.createCar();
 
         for (String s: carNameList) {
             carList.add(new Car(s));
         }
+        tryCount = inputView.tryCount();
+
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         n = Integer.parseInt(Console.readLine());
---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputView {
+    private final String delimeter = ",";
+
+    private String readLine() {
+        return Console.readLine();
+    }
+
+    public List<String> createCar() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = readLine();
+
+        return Arrays.stream(input
+                .split(delimeter))
+                .toList();
+    }
+
+    public int tryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String input = readLine();
+
+        return Integer.parseInt(input);
+    }
+}
