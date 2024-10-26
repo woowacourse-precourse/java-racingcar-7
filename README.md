ea6abb2964b2c4fff96a0d820c4739e4d069e5dd
Shinjongyun <tigerjong123@gmail.com>
2024-10-26 18:04:03 +0900

    feat: Car 객체 추가

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # java-racingcar-precourse
+
+- [ ] 차의 정보를 저장하는 Car 객체
+
+- [ ] 각각의 차마다 간 거리를 저장하는 Road 객체
+
+- [ ] 차가 갈 거리를 만드는 RoadCreator 인터페이스
+
+- [ ] 차가 거리를 랜덤하게 만드는 RandomRoadCreator 객체
+  - 이 부분은 RoadCreator에 의존성을 주입하여 생성
+
+- [ ] 직접 경기를 진행해주는 Racing 객체
+
+- [ ] 레이싱에서 현재 상황을 출력하는 Situation 객체
---
+++ src/main/java/racingcar/entity/Car.java
@@ -0,0 +1,20 @@
+package racingcar.entity;
+
+public class Car {
+
+    private final String name;
+    private final Road road;
+
+    private Car(String name){
+        this.name=name;
+        this.road=null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Road getRoad() {
+        return road;
+    }
+}
