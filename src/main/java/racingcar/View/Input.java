e1d921266b90db25eacd31acd51b1688b37a52c1
김호연 <yun1094@naver.com>
2024-10-26 11:49:12 +0900

    사용자에게 입력 받는 함수 완성

--- src/main/java/racingcar/View/Input.java
+++ src/main/java/racingcar/View/Input.java
@@ -1,11 +1,21 @@
 package racingcar.View;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Input {
-    public static void RequestCarNameMessage() {
+    private Integer trialCount;
+
+    public List<String> ListRequestCarNameMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        List<String> carNameList = List.of(Console.readLine().split(","));
+        return carNameList;
     }
 
-    public static void RequestNumberOfTrialMessage() {
+    public Integer RequestNumberOfTrialMessage() throws IllegalArgumentException{
         System.out.println("시도할 횟수는 몇 회인가요?");
+        this.trialCount = Integer.parseInt(Console.readLine());
+        return trialCount;
     }
 }
