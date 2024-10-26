6fd3e119625f5d5d836d477e5d77ad9466e2435e
chxghee <francis001021@gmail.com>
2024-10-27 01:32:50 +0900

    Test: 입력값 유효성 검사 테스트 코드 추가 및 수정
    반복횟수 입력에 대한 테스트 코드 추가
    자동차 이름 입력 테스트 상황별로 분리

--- src/test/java/racingcar/service/InputProcessorTest.java
+++ src/test/java/racingcar/service/InputProcessorTest.java
@@ -11,10 +11,14 @@ public class InputProcessorTest {
     InputProcessor inputProcessor = new InputProcessor();
 
     @Test
-    void 입력_유효성_테스트(){
+    void 올바른_자동차명_입력_유효성_테스트(){
 
         assertTrue(inputProcessor.isValid("asd,qwe,zxcd"));
         assertTrue(inputProcessor.isValid("asd"));
+    }
+
+    @Test
+    void 잘못된_자동차명_입력_유효성_테스트(){
 
         assertFalse( inputProcessor.isValid(",asdf"));                    // 입력 앞에 쉼표가 있는 경우
         assertFalse( inputProcessor.isValid("asdf,"));                    // 입력 마지막에 쉼표가 있는경우
@@ -22,31 +26,23 @@ public class InputProcessorTest {
         assertFalse( inputProcessor.isValid("asdf,qwe2w,zxca"));          // 이름이 대문자 소문자 알파벳이 아닌경우
         assertFalse( inputProcessor.isValid("asdf,asd,zxca,asd"));        // 자동차가 중복입력된 경우
         assertFalse( inputProcessor.isValid(null));                       // null이 입력된 경우
-
     }
 
     @Test
-    void 입력_문자열_자르기(){
+    void 입력_문자열_분리_테스트(){
         assertEquals(List.of("qwe","asd","zxc"), inputProcessor.parseCarNames("qwe,asd,zxc"));
         assertEquals(List.of("qwe"), inputProcessor.parseCarNames("qwe"));
     }
 
     @Test
-    void 에외_발생_테스트(){
-        IllegalArgumentException exception1 = assertThrows(IllegalArgumentException.class, () -> {
-            inputProcessor.parseCarNames(null);
-        });
+    void 중복_자동차명입력_에외_발생_테스트(){
         IllegalArgumentException exception2 = assertThrows(IllegalArgumentException.class, () -> {
             inputProcessor.parseCarNames("asd,asd");
         });
     }
 
     @Test
-    void 반복횟수_유효성_테스트(){
-
-        IllegalArgumentException exception1 = assertThrows(IllegalArgumentException.class, () -> {
-            inputProcessor.isValidRound(null);
-        });
+    void 잘못된_반복횟수입력_예외발생_테스트(){
         IllegalArgumentException exception2 = assertThrows(IllegalArgumentException.class, () -> {
             inputProcessor.isValidRound("A123");
         });
