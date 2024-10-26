ddd74261d9bedcda40d3867fbf4e4601f23609ec
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2024-10-27 01:30:06 +0900

    feat(service): race를 구동시킬 수 있는 RaceEngine을 구현한다
    race를 구동시키는 performRace함수를 이전하여 race domain의 책임을 분리시키는 raceEngine helper class를 service layer에 구현하다.

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -2,40 +2,31 @@ package racingcar.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import racingcar.domain.car.Car;
-import racingcar.domain.member.Member;
 import racingcar.domain.race.Race;
 import racingcar.domain.race.RaceResult;
 import racingcar.dto.RaceRequestDTO;
 import racingcar.dto.RaceResultDTO;
-import racingcar.util.RaceRandomGenerator;
+import racingcar.service.factory.CarFactory;
 
 public class RacingService {
 
-    private final RaceRandomGenerator randomGenerator;
+    private final RaceEngine raceEngine;
+    private final CarFactory carFactory;
 
-    public RacingService(RaceRandomGenerator randomGenerator) {
-        this.randomGenerator = randomGenerator;
+    public RacingService(RaceEngine raceEngine) {
+        this.carFactory = new CarFactory();
+        this.raceEngine = raceEngine;
     }
 
-    public RaceResultDTO performRace(RaceRequestDTO raceRequestDTO) {
-        List<Car> cars = convertNamesToCars(raceRequestDTO.getNames());
-        Race race = new Race(cars, raceRequestDTO.getTimes(), randomGenerator);
-        race.performRace();
+    public RaceResultDTO runAllRaces(RaceRequestDTO raceRequestDTO) {
+        List<String> names = raceRequestDTO.getNames();
+        Race race = new Race(carFactory.getCarList(names));
+        List<Race> races = new ArrayList<>(List.of(race));
 
-        RaceResult raceResult = new RaceResult(race);
-        return new RaceResultDTO(raceResult.getRaceWinnerList());
+        for (long l = 0; l < raceRequestDTO.getTimes(); l++) {
+            races.add(raceEngine.performRace(races.getLast()));
+        }
+        RaceResult raceResult = new RaceResult(races);
+        return new RaceResultDTO(raceResult.getRaces(), raceResult.getRaceWinnerList());
     }
-
-
-    private List<Car> convertNamesToCars(List<String> names) {
-        List<Car> cars = new ArrayList<>();
-        names.forEach(name -> {
-            Member member = new Member(name);
-            cars.add(Car.of(member));
-        });
-        return cars;
-    }
-
-
 }
