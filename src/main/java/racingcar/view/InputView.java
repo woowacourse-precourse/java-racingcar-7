dfdbbddfacd98a233b1a7ec68227eabf9f7b99f9
honggyu1233@naver.com <honggyu1233@naver.com>
2024-10-26 19:31:10 +0900

    feat : 사용자의 이름을 입력 받는 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,10 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static String racingcarInputView() {
+        String inputName=Console.readLine();
+        return inputName;
+    }
 }
