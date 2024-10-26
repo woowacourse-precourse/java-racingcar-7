0ffc95ef4dd34b6c6c71b52205b143d144be549b
menzzi <minmin3927@naver.com>
2024-10-26 11:26:51 +0900

    feat(validator) : Validator 구현

---
+++ src/main/java/racingcar/validator/Validator.java
@@ -0,0 +1,18 @@
+package racingcar.validator;
+
+public class Validator {
+
+    public static void validateCarName(String name){
+        if(name.length()>5){
+            throw new IllegalArgumentException("이름은 5자 이하입니다.");
+        }
+    }
+
+    public static void validateTryNumber(String tryNumber){
+        try{
+            Integer.parseInt(tryNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닙니다.");
+        }
+    }
+}
---
+++ src/test/java/racingcar/model/CarsTest.java
@@ -0,0 +1,25 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.Test;
+
+public class CarsTest {
+    @Test
+    void 자동차_등록_실페(){
+
+    }
+
+    @Test
+    void 자동차_등록_성공(){
+
+    }
+
+    @Test
+    void 자동차_전진_실패(){
+
+    }
+
+    @Test
+    void 자동차_전진_성공(){
+
+    }
+}
