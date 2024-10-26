e2a33d22cb3f0f0fb20e0ad88b4cacaaeed2bbd6
wvssm <neoramiregi@gmail.com>
2024-10-27 02:10:18 +0900

    refactor(CarTest): 메서트 테스트 순서 변경

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -8,6 +8,19 @@ import static racingcar.constant.ErrorMessages.NAME_LENGTH_ERROR_MESSAGE;
 
 class CarTest {
     @Test
+    void 자동차는_이름을_가진다(){
+        final var car = new Car("sumin");
+        assertThat(car.getName()).isEqualTo("sumin");
+    }
+
+    @Test
+    void 자동차_이름은_1자_이상_5자_이하여야_한다(){
+        assertThatThrownBy(() -> new Car("jinyoung"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NAME_LENGTH_ERROR_MESSAGE);
+    }
+
+    @Test
     void 자동차는_전진할_수_있다_4이상_숫자일_경우(){
         final var car = new Car("sumin");
         car.move(5);
@@ -19,18 +32,5 @@ class CarTest {
         final var car = new Car("sumin");
         car.move(3);
         assertThat(car.getPosition()).isEqualTo(0);
-    }
-
-    @Test
-    void 자동차는_이름을_가진다(){
-        final var car = new Car("sumin");
-        assertThat(car.getName()).isEqualTo("sumin");
-    }
-
-    @Test
-    void 자동차_이름은_1자_이상_5자_이하여야_한다(){
-        assertThatThrownBy(() -> new Car("jinyoung"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(NAME_LENGTH_ERROR_MESSAGE);
     }
 }
