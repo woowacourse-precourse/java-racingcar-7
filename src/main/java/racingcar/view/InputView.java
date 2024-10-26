fda74723efe7ed097e77bd05510eaa9b4a5878a2
junhaa <2171326@hansung.ac.kr>
2024-10-27 02:08:31 +0900

    feat(InputView): 자동차 이동 횟수 입력 기능 추가
    - 사용자가 자동차의 이동 횟수를 입력하면, int 형태로 반환

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,9 +2,6 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-/*
- - [ ] 사용자는 몇 번의 이동을 할 것인지를 입력
- */
 public class InputView {
     public String[] getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -13,16 +10,25 @@ public class InputView {
 
         String[] carNames = userInput.split(",");
 
-        if(!validateCarName(carNames)) {
+        if (!validateCarName(carNames)) {
             throw new IllegalArgumentException("입력된 자동차 이름이 올바르지 않습니다.");
         }
 
         return carNames;
     }
 
-    private boolean validateCarName(String[] carNames){
+    public int getMoveCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+
+        String userInput = Console.readLine();
+
+        return Integer.parseInt(userInput);
+    }
+
+
+    private boolean validateCarName(String[] carNames) {
         for (String carName : carNames) {
-            if(carName.isBlank() || carName.length() > 5){
+            if (carName.isBlank() || carName.length() > 5) {
                 return false;
             }
         }
