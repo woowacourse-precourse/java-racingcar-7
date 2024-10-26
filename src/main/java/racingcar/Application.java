35e3bd149b67d9a9ec65f697e3f9a915d02eef40
AppleMint98 <hong721816@konkuk.ac.kr>
2024-10-26 20:35:38 +0900

    feat : 입력 단계 구현
    자동차에 이름을 부여하고,
    사용자가 몇 번의 이동을 할 것인지 받아옴

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String names = Console.readLine();
+        String[] nameList = names.split(",");
+        for (String name : nameList) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
+        System.out.println("시도할 횟수는 몇 회 인가요?");
+        String times = Console.readLine();
+        boolean isNumeric = times.chars().allMatch(Character::isDigit);
+        if (isNumeric) {
+            throw new IllegalArgumentException("숫자만 입력 해 주세요.");
+        }
+        long time = Long.parseLong(times);
+
     }
 }
