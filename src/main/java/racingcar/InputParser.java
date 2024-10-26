1a2639a8bc930fd65b0c4eb1b6d6e76fc2521cdf
pil-go <pilbongx@naver.com>
2024-10-27 07:35:57 +0900

    feat(InputParser): 자동차 목록을 구분자로 나누는 기능 추가

---
+++ src/main/java/racingcar/InputParser.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputParser {
+    private static final String CARNAME_DELIMITER = ",";
+
+    public List<Car> stringToCarList(String input) {
+        List<Car> carList = new ArrayList<>();
+
+        for (String carName : input.split(CARNAME_DELIMITER, -1)) {
+            carList.add(new Car(carName));
+        }
+
+        return carList;
+    }
+}
