0c3bb5a988bd355a8606ad1c893341f3d9c67ff9
윤민수 <koelysa5696@naver.com>
2024-10-26 11:35:52 +0900

    Feat: 사용자 입출력 구현

---
+++ src/main/java/racingcar/View/Output.java
@@ -0,0 +1,31 @@
+package racingcar.View;
+
+import racingcar.DTO;
+
+public class Output {
+    public static final String forwardDelimiter = "-";
+
+    public static void gameResult(DTO[] player) {
+        System.out.println("실행 결과");
+
+        for (DTO dto : player) {
+            System.out.print(dto.getCarName() + " : ");
+            System.out.println(forwardDelimiter.repeat(dto.getForwardCount()));
+        }
+        System.out.println();
+    }
+
+    public static void gameWinners(int max, DTO[] participants) {
+        System.out.print("최종 우승자 : ");
+        int count = 0;
+        for (DTO participant : participants) {
+            if (count == 0 && participant.getForwardCount() == max) {
+                System.out.print(participant.getCarName());
+                count++;
+            } else if (count >= 1 && participant.getForwardCount() == max) {
+                System.out.print(", " + participant.getCarName());
+            }
+        }
+
+    }
+}
---
+++ src/main/java/racingcar/View/input.java
@@ -0,0 +1,19 @@
+package racingcar.View;
+
+import static racingcar.Validation.validateGameCount;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class input {
+    public static String carNameInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNameBuffer = Console.readLine();
+        return carNameBuffer;
+    }
+
+    public static String GameCountInput() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String gameCountBuffer = Console.readLine();
+        return gameCountBuffer;
+    }
+}
