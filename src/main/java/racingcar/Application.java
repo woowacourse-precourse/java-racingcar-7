36b158cf81d76e1dd4bfd09259c204bef82f1606
eunho <eunho1032@gmail.com>
2024-10-27 01:37:25 +0900

    refactor(Application): add new class RacingGame

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,18 +1,9 @@
 package racingcar;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Application {
 
     public static void main(String[] args) {
-        String[] carNames = Input.inputCarNames();
-        int tryCounts = Input.inputTryCounts();
-
-        List<Car> cars = new ArrayList<>();
-        for (String carName : carNames) {
-            cars.add(new Car(carName));
-        }
-        Output.printResult(cars, tryCounts);
+        RacingGame game = new RacingGame();
+        game.run();
     }
 }
