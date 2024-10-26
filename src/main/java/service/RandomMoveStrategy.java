24c86f99b6ce75eca7f7ee41902331d618c54dde
codrae <hi653@naver.com>
2024-10-26 21:05:43 +0900

    refactor: random 변수 삭제
    굳이 선언하여 사용할 필요가 없다고 느껴 삭제 함.

--- src/main/java/service/RandomMoveStrategy.java
+++ src/main/java/service/RandomMoveStrategy.java
@@ -2,16 +2,14 @@ package service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomMoveStrategy implements MoveStrategy {
-    private static final int MOVE_THRESHOLD = 5;
-    private final int random;
 
-    public RandomMoveStrategy() {
-        this.random = Randoms.pickNumberInRange(0,9);
-    }
+// stateless, 디폴트 생성자로 충분.
+public class RandomMoveStrategy implements MoveStrategy {
+    private static final int MOVE_THRESHOLD = 4;
 
     @Override
     public boolean canMove() {
+        int random = Randoms.pickNumberInRange(0,9);
         return random >= MOVE_THRESHOLD;
     }
 }
