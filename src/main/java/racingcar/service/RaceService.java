766e538caac6a665113e7791a9ebd33bc809a4ec
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2024-10-26 18:52:53 +0900

    refactoring : RaceService 경주 진행과 결과 출력 책임 추가

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -1,7 +1,9 @@
 package racingcar.service;
 
+import java.util.List;
 import racingcar.domain.Cars;
 import racingcar.domain.Race;
+import racingcar.domain.RaceFormatter;
 
 public class RaceService {
 
@@ -10,5 +12,12 @@ public class RaceService {
         Race race = new Race(moveCount,cars);
         race.doRace();
         return race;
+    }
+
+    public String getRaceResult(Race race){
+        String raceHistory = race.getRaceHistory();
+        List<String> winners = race.getRaceWinners();
+        String raceResult = RaceFormatter.formatRaceResult(winners);
+        return raceHistory + System.lineSeparator() + raceResult;
     }
 }
