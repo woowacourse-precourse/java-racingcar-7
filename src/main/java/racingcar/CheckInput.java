749ff1c8ae4f8d11606d1d2b4e6073e2503c5229
Beomseok Noh <prodigy0831@naver.com>
2024-10-26 12:44:25 +0900

    fix(CheckInput): 자동차 이름이 비어있거나 null일때 예외 처리

--- src/main/java/racingcar/CheckInput.java
+++ src/main/java/racingcar/CheckInput.java
@@ -7,14 +7,22 @@ public class CheckInput {
     private static final int MAX_NAME_LENGTH = 5;
     private static final String DELIMITER = ",";
     private static final String NUMBER_REGEX = "\\d+";
+    private static final String ERROR_EMPTY_OR_NULL_NAME = "자동차 이름은 빈 문자열이 될 수 없습니다.";
     private static final String ERROR_NAME_LENGTH = "자동차의 이름은 다섯 글자를 넘을 수 없습니다.";
     private static final String ERROR_NAME_ALREADY_EXIST = "중복된 이름이 존재합니다.";
     private static final String ERROR_INVALID_COUNT = "올바른 숫자를 입력해 주세요.";
 
     public String[] splitNames(String carNameString) {
+        validateNameString(carNameString);
         String[] carNames = splitByDelimiter(carNameString);
         validateCarNames(carNames);
         return checkAlreadyExist(carNames);
+    }
+
+    private void validateNameString(String carNameString) {
+        if (carNameString == null || carNameString.isBlank()) {
+            throw new IllegalArgumentException(ERROR_EMPTY_OR_NULL_NAME);
+        }
     }
 
     private String[] checkAlreadyExist(String[] carNames) {
