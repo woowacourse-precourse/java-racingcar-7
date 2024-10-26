dfcc7d77252630930e1750875df76c51cb222e3a
suyeong jang <jangsuyeong923@naver.com>
2024-10-26 18:23:24 +0900

    feat(ValidationUtils): 입력값 검증 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,16 +3,22 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import racingcar.Utils.ValidationUtils;
 
 public class InputView {
+    private static final ValidationUtils validationUtils=new ValidationUtils();
+
     public List<String> getCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.");
         String input= Console.readLine();
-        return Arrays.asList(input.split(","));
+        List<String> carNames = Arrays.asList(input.split(","));
+        validationUtils.validateCarNames(carNames);
+        return carNames;
     }
     public int getRaceCount(){
         System.out.println("시도할 횟수는 몇 회인가요?");
         String raceCount=Console.readLine();
+        validationUtils.validateRaceCount(raceCount);
         return Integer.parseInt(raceCount);
     }
 }
