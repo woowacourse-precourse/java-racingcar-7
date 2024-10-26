1012f665614e1d88b85838849d9886d7ff5ddef4
LSH-kw0315 <tmdgjsdl1234@naver.com>
2024-10-27 02:37:47 +0900

    feat 이름에 대한 입력 기능 추가
    이름을 입력하는 기능을 추가했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String playerInput=Console.readLine();
+        HashMap<String,Integer> player=new HashMap<>();
+        for (String s : playerInput.split(",")) {
+            player.put(s,0);
+        }
+
+
     }
 }
