770ece14a5d93fcfd4ea1240f6e22b1a1151598b
sjmmics <sjmmics@gmail.com>
2024-10-26 13:52:39 +0900

    feat : ($CarNames, $CarNamesValidator) CarNames 자동차 이름 입력값 검증 기능 로직을 CarNamesValidator로 옮김
    CarNames 객체를 생성할 때 이름 입력 검증을 CarNamesValidator의 메서드를 호출해서 수행하게 변경.

--- src/main/java/racingcar/model/entity/CarNames.java
+++ src/main/java/racingcar/model/entity/CarNames.java
@@ -1,35 +1,12 @@
 package racingcar.model.entity;
 
-import racingcar.utils.message.ErrorMessage;
+import racingcar.utils.validator.CarNamesValidator;
 
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /** 자동차 이름을 저장한 일급 컬력션 입력값 검증 기능 수행 */
 public class CarNames implements Iterable<String> {
-
-    public static final String ONLY_DIGIT_ALPHABET_HANGEUL = "[0-9a-zA-Z가-힣]";
-
-    public static final String MAX_CAR_NAME_LENGTH = "5";
-
-    public static final String CAR_NAME_LENGTH_MIN_BETWEEN_MAX =
-            "{1," + MAX_CAR_NAME_LENGTH + "}";
-
-    public static final String CAR_NAME_DELIMITER = ",";
-
-    // 자동차 이름 문자열 형식 검증 패턴
-    public static final Pattern carNamePattern =
-            Pattern.compile("^" +
-                    ONLY_DIGIT_ALPHABET_HANGEUL +
-                    CAR_NAME_LENGTH_MIN_BETWEEN_MAX +
-                    "(" + CAR_NAME_DELIMITER +
-                    ONLY_DIGIT_ALPHABET_HANGEUL +
-                    CAR_NAME_LENGTH_MIN_BETWEEN_MAX +
-                    ")*$");
 
     private final List<String> names;
 
@@ -39,35 +16,19 @@ public class CarNames implements Iterable<String> {
 
     /**
      * 자동차 이름 문자열을 받아서 형식을 검증한 후 문자열로 반환하는 메서드
+     *
      * @param 검증되지 않은 자동차 이름 문자열
      * @return CarNames 객체 반환
      * @throws 자동차 이름이 5글자 초과하거나 이름 중간에 알파벳, 한글, 숫자 이외의 문자가
      *         있으면 IllegalArgumentException 발생
      */
     public static CarNames getAfterValidateFormat(String namesToValidate) {
-        validateSpace(namesToValidate);
-        
-        Matcher matcher = carNamePattern.matcher(namesToValidate);
-        if (!matcher.matches()) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_NAME_FORMAT.toString());
-        }
-        
+        CarNamesValidator.whiteSpace(namesToValidate);
+        CarNamesValidator.illegalLetter(namesToValidate);
+        CarNamesValidator.duplicateName(namesToValidate);
+
         List<String> names = List.of(namesToValidate.split(","));
-        validateDuplicate(names);
         return new CarNames(names);
-    }
-
-    private static void validateSpace(String namesToValidate) {
-        if (namesToValidate.contains(" ")) {
-            throw new IllegalArgumentException(ErrorMessage.IN_SPACE.toString());
-        }
-    }
-
-    private static void validateDuplicate(List<String> names) {
-        Set<String> namesToCompare = new HashSet<>(names);
-        if (names.size() != namesToCompare.size()) {
-            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NAMES.toString());
-        }
     }
 
     @Override
---
+++ src/main/java/racingcar/utils/validator/CarNamesValidator.java
@@ -0,0 +1,55 @@
+package racingcar.utils.validator;
+
+import racingcar.utils.message.ErrorMessage;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class CarNamesValidator {
+
+    public static final String ONLY_DIGIT_ALPHABET_KOREAN = "[0-9a-zA-Z가-힣]";
+
+    public static final String MAX_CAR_NAME_LENGTH = "5";
+
+    public static final String CAR_NAME_LENGTH_MIN_BETWEEN_MAX =
+            "{1," + MAX_CAR_NAME_LENGTH + "}";
+
+    public static final String CAR_NAME_DELIMITER = ",";
+
+    // 자동차 이름 문자열 형식 검증 패턴
+    public static final Pattern carNamePattern =
+            Pattern.compile("^" +
+                    ONLY_DIGIT_ALPHABET_KOREAN +
+                    CAR_NAME_LENGTH_MIN_BETWEEN_MAX +
+                    "(" + CAR_NAME_DELIMITER +
+                    ONLY_DIGIT_ALPHABET_KOREAN +
+                    CAR_NAME_LENGTH_MIN_BETWEEN_MAX +
+                    ")*$");
+
+    private CarNamesValidator() {}
+
+    public static void whiteSpace(String names) {
+        if (names.contains(" ")) {
+            throw new IllegalArgumentException(ErrorMessage.IN_SPACE.toString());
+        }
+    }
+
+    public static void illegalLetter(String names) {
+        Matcher matcher = carNamePattern.matcher(names);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NAME_FORMAT.toString());
+        }
+    }
+
+    public static void duplicateName(String names) {
+        List<String> namesMayContainsDuplicate = List.of(names.split(","));
+        Set<String> namesRemovedDuplicate = new HashSet<>(namesMayContainsDuplicate);
+        if (namesMayContainsDuplicate.size() != namesRemovedDuplicate.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NAMES.toString());
+        }
+    }
+
+}
