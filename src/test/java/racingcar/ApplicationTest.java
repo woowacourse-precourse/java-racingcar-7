f4679ddab9c1f6e7d2e4089b056d3590016625ac
yereumi <lovelyyeji01@gmail.com>
2024-10-27 02:44:21 +0900

    test(logic): 단위 테스트 코드 삭제, 통합 테스트 코드 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,12 +1,14 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -30,6 +32,42 @@ class ApplicationTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void 정상_테스트1() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,wo ni", "1");
+                    assertThat(output()).contains("pobi : -", "wo ni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+    @Test
+    void 정상_테스트2() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "2");
+                    assertThat(output()).contains("pobi : -", "woni : -", "pobi : --", "woni : --", "최종 우승자 : pobi, woni");
+                },
+                4, 4, 5, 6
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "pobi,javaj,pobi | 1",
+            "pobi,javaji | 1",
+            "pobi, | 1"},
+            delimiter = '|'
+    )
+    void 이름_형식_예외_테스트(String carNames, String attemptNum) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(carNames, attemptNum))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 
     @Override
     public void runMain() {
