328d2021c5c53cb374147b5f210138e7ff7b31b8
xeimun <hinote444@naver.com>
2024-10-26 21:34:52 +0900

    feat(TextFormatter): 공동 우승자를 콤마로 구분하여 문자열로 합치는 기능 구현

--- src/main/java/racingcar/model/TextFormatter.java
+++ src/main/java/racingcar/model/TextFormatter.java
@@ -1,8 +1,17 @@
 package racingcar.model;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TextFormatter {
 
     public String[] splitCarNamesByComma(String carNames) {
         return carNames.split(",");
+    }
+
+    public String getWinnerNames(List<Car> winners) {
+        return winners.stream()
+                      .map(Car::getName)
+                      .collect(Collectors.joining(", "));
     }
 }
