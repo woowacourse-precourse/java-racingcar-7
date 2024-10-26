b0bfafbe9a333dcddd6f8e4d075d201180a08921
Fletacode <bassykd@gmail.com>
2024-10-26 23:53:30 +0900

    feat: 메인 실행 기능 구현
    메인에 애플리케이션이 동작하는 전체 로직을 추가했습니다

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,6 +1,9 @@
 package racingcar.controller;
-
+import java.util.List;
+import racingcar.model.car.Cars;
+import racingcar.model.parseCar.ParseCar;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingCarController {
 
@@ -14,6 +17,20 @@ public class RacingCarController {
   }
 
   public void run(){
+      //자동차 이름 추출
+      List<String> carsName = ParseCar.parseCarName(inputCar);
+      //자동차 객체 생성
+      Cars cars = new Cars(carsName);
+      //레이싱 시작, 현황 출력
+      OutputView.startOutput();
+      for (int i = 0; i < inputCount; i++) {
+        cars.startRacingCars();
+        OutputView.printCarRacingStatus(cars);
+        cars.endRacingCars();
+      }
+      //우승자 결정하기
+      cars.decisionWinnerCars();
+      OutputView.printWinnerCars(cars);
 
   }
 
