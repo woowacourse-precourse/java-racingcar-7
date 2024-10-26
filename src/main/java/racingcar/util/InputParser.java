4064b1e93ef4b9ad8c0a5d34c4b45d0e367d9dff
enble <enble_777@naver.com>
2024-10-26 17:47:44 +0900

    feat: InputParser 구현

---
+++ src/main/java/racingcar/util/InputParser.java
@@ -0,0 +1,52 @@
+package racingcar.util;
+
+import java.util.List;
+import racingcar.exception.ErrorStatus;
+import racingcar.exception.ExceptionUtils;
+
+public class InputParser {
+    private static final String DEFAULT_DELIMITER = ",";
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+    private static final int MAX_ROUND = 100;
+
+    public List<String> parseCars(String input) {
+        if (input == null || input.isEmpty()) {
+            ExceptionUtils.throwIllegalArgumentException(ErrorStatus.EMPTY_INPUT);
+        }
+
+        List<String> carNames = List.of(input.split(DEFAULT_DELIMITER));
+        carNames.forEach(this::validateCarName);
+
+        return carNames;
+    }
+
+    public int parseRound(String input) {
+        if (input == null || input.isEmpty()) {
+            ExceptionUtils.throwIllegalArgumentException(ErrorStatus.EMPTY_INPUT);
+        }
+
+        try {
+            int round = Integer.parseInt(input);
+            validateRound(round);
+
+            return round;
+        } catch (NumberFormatException e) {
+            ExceptionUtils.throwIllegalArgumentException(ErrorStatus.INVALID_ROUND_INPUT);
+        }
+
+        return 0;
+    }
+
+    private void validateCarName(String carName) {
+        if (carName.isEmpty() || carName.length() > MAX_CAR_NAME_LENGTH) {
+            ExceptionUtils.throwIllegalArgumentException(ErrorStatus.INVALID_CAR_NAME);
+        }
+    }
+
+    private void validateRound(int round) {
+        if (round < 1 || round > MAX_ROUND) {
+            ExceptionUtils.throwIllegalArgumentException(ErrorStatus.ROUND_OUT_OF_BOUND);
+        }
+    }
+
+}
