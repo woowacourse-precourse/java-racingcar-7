6ccad99918cbc3bc949ed4724600953ce23e1531
Yoon-Jemin <yjm1800@naver.com>
2024-10-27 01:58:11 +0900

    test(view): 자동차 이름 입력에 대한 테스트
    - 자동차의 이름이 6자 이상인 입력이 주어졌을 때 예외가 발생하는지 테스트

---
+++ src/test/java/racingcar/util/ValidatorTest.java
@@ -0,0 +1,26 @@
+package racingcar.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ValidatorTest {
+
+    @Test
+    @DisplayName("자동차의 이름이 6자 이상인 경우 예외가 발생한다")
+    void 자동차_이름_입력_테스트1() throws Exception {
+        //given
+        Validator validator = new Validator();
+
+        // when
+        String carNames = "abc,abcd,abcde,abcdef";
+
+        // then
+        assertThrows(IllegalArgumentException.class, () -> {
+            validator.checkCarNameInput(carNames);
+        });
+    }
+
+}
