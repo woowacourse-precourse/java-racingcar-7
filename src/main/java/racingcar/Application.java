c9e3a40313d9fcf8fd5b54d688cef5a1247f39dc
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-10-26 22:54:51 +0900

    feat: 전체 프로그램 동작 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import racingcar.controller.CarRaceController;
+import racingcar.util.RandomGenerator;
+import racingcar.util.RandomGeneratorImpl;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView() {};
+        OutputView outputView = new OutputView();
+
+        RandomGenerator randomGenerator = new RandomGeneratorImpl();
+
+        CarRaceController carRaceController = new CarRaceController(inputView, outputView, randomGenerator);
+        carRaceController.run();
     }
 }
