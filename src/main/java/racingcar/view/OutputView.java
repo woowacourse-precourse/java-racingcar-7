a76591ab15584c6aaae6ceeec60a4727e7a79692
minjae k <rkdalswoals@gmail.com>
2024-10-27 01:00:36 +0900

    feat: add OutputView to display game results

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,19 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+import java.util.List;
+
 public class OutputView {
+    public void printRoundResult(List<Car> cars) {
+        System.out.println("실행 결과");
+        for (Car car : cars) {
+            System.out.println(car.getName() + ":" + "-".repeat(car.getPosition()));
+        }
+    }
+
+    public void printFinalWinners(List<String> winners) {
+        String finalWinners = String.join(",", winners);
+        System.out.println("최종 우승자 : " + finalWinners);
+    }
+
 }
