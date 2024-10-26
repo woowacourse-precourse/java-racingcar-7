0e7579848a91657fb5cea3750ad0bc15bc01c9ae
JoonKyo <joonkyolee@gmail.com>
2024-10-27 03:42:29 +0900

    feat(CarName): 자동차 이름 앞뒤 및 중간에 공백이 있는지 확인하는 메서드 구현

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,6 +1,8 @@
 package racingcar.domain;
 
 public class CarName {
+    private static final String ERROR_MESSAGE_FOR_BLANK = "자동차 이름에 공백이 존재합니다. 프로그램을 종료합니다.";
+    private static final String BLANK = " ";
     private final String carName;
 
     private CarName(String carName) {
@@ -9,5 +11,11 @@ public class CarName {
 
     public static CarName from(String carName) {
         return new CarName(carName);
+    }
+
+    private static void validateBlank(String carName) {
+        if (carName.contains(BLANK)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_BLANK);
+        }
     }
 }
