800f1942bf0cb1a64b3795992d53e18b4cfb1ef5
inhooo00 <chldlsgh0987@naver.com>
2024-10-26 23:52:55 +0900

    refactor(util): refactor CarNameValidator model
    Add CarNameValidator model :
    - validator 상속 취소.

--- src/main/java/racingcar/util/CarNameValidator.java
+++ src/main/java/racingcar/util/CarNameValidator.java
@@ -1,34 +1,65 @@
-package util.validator;
+package racingcar.util;
 
-public class CarNameValidator extends Validator {
-    private static final int MAX_NAME_LENGTH = 5;
+import static racingcar.util.ErrorMessage.INPUT_CAR_NAME_BLANK;
+import static racingcar.util.ErrorMessage.INPUT_CAR_NAME_IS_INCORRECT;
+import static racingcar.util.ErrorMessage.INPUT_STRING_DUPLICATE;
+import static racingcar.util.ErrorMessage.INPUT_STRING_NOT_NULL;
 
-    @Override
-    public void validate(String name) {
-        validateCarNameLength(name);
-    }
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
 
+public class CarNameValidator {
+    private static final int MAX_DIGIT_CAR_NAME = 5;
+    private static final int MIN_DIGIT_CAR_NAME = 1;
 
-    private void validateCarNameLength(String name) {
-        if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException(Message.EXCEPTION_CAR_NAME_BLANK.message);
-        }
-        if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException(Message.EXCEPTION_CAR_NAME_LENGTH.message);
-        }
-    }
-
-    public enum Message {
-        EXCEPTION_CAR_NAME_LENGTH("5자 이하의 이름을 입력해주세요."),
-        EXCEPTION_CAR_NAME_BLANK("공백은 이름으로 사용할 수 없습니다");
-        private final String message;
-
-        Message(String message) {
-            this.message = message;
-        }
-
-        public String getMessage() {
-            return message;
+    public static void checkDuplicate(List<String> input) {
+        if (hasDuplicateCarName(input)) {
+            throw new IllegalArgumentException(INPUT_STRING_DUPLICATE.message);
         }
     }
+
+    public static void checkEmpty(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException(INPUT_STRING_NOT_NULL.message);
+        }
+    }
+
+    public static void checkEmpty(List<String> input) {
+        if (input == null) {
+            throw new IllegalArgumentException(INPUT_STRING_NOT_NULL.message);
+        }
+    }
+    public static void checkOverRange(String input) {
+        if (isOverRange(input) || isUnderRange(input)) {
+            throw new IllegalArgumentException(INPUT_CAR_NAME_IS_INCORRECT.message);
+        }
+    }
+
+    public static void checkSpace(String input) {
+        if (isSpace(input)) {
+            throw new IllegalArgumentException(INPUT_CAR_NAME_BLANK.message);
+        }
+    }
+
+    private static boolean hasDuplicateCarName(List<String> input) {
+        Set set = new HashSet(input);
+
+        return input.size() != set.size();
+    }
+
+    private static boolean isOverRange(String input) {
+        return MAX_DIGIT_CAR_NAME < input.length();
+    }
+
+    private static boolean isUnderRange(String input) {
+        return MIN_DIGIT_CAR_NAME > input.length();
+    }
+
+    private static boolean isSpace(String input) {
+        return Pattern.matches("^\\s+$", input);
+    }
+
 }
+
