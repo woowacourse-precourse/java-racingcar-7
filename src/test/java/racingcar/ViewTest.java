53fe55bbc31965ee73a21ebbd2ecff9162f5eba8
hyuuuun <pam4522@gmail.com>
2024-10-26 23:08:09 +0900

    Style : 임포트 구분

--- src/test/java/racingcar/ViewTest.java
+++ src/test/java/racingcar/ViewTest.java
@@ -4,13 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
