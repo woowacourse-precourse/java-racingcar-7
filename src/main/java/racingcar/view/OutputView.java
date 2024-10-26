64ede0f302fccf4e5f01ca80a8c48f5a56283be8
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-10-26 15:08:02 +0900

    feat(view): 최종 우승자 출력 뷰 생성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,6 +3,8 @@ package racingcar.view;
 import racingcar.dto.CarsDto;
 import racingcar.model.Car;
 
+import java.util.List;
+
 public class OutputView {
     private OutputView() {
     }
@@ -14,5 +16,14 @@ public class OutputView {
             System.out.println(car.getName()+" : "+"-".repeat(car.getWinStack()));
         }
         System.out.println();
+    }
+    public static void outputRaceResult(List<String> raceResult) {
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < raceResult.size(); i++) {
+            if (i > 0) {
+                System.out.print(", ");
+            }
+            System.out.print(raceResult.get(i));
+        }
     }
 }
