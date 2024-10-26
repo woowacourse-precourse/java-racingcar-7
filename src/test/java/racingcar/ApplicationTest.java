b11c0ec9312f8f7478658c0bea5ea831871097b6
Yehun <yhyh4420@naver.com>
2024-10-26 13:57:55 +0900

    test(test):단위테스트 추가
    - InvalidException에 해당하는 예외 옵션들 반영
        - 5글자를 초과하는 이름
        - round가 숫자가 아닌 문자의 경우
        - round가 0인 경우
        - round가 음수인 경우

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,33 +1,84 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
     @Test
-    void 기능_테스트() {
+    @DisplayName("정상적인 이름 넣었을 때 잘 돌아가는지 확인")
+    void 기능_테스트_case1() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
-    void 예외_테스트() {
+    @DisplayName("이름 앞뒤로 공백을 입력했을 때 잘 돌아가는지 확인")
+    void 기능_테스트_case2() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi, woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    @DisplayName("6글자 이상 이름 예외처리")
+    void 예외_테스트_case1() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("앞뒤 공백 제외 6글자 이상 이름 예외처리")
+    void 예외_테스트_case2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,   javaji   ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("round에 문자를 입력할 경우 예외처리")
+    void 예외_테스트_case3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "i"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("round에 0을 입력할 경우 예외처리")
+    void 예외_테스트_case4() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("round에 음수를 입력할 경우 예외처리")
+    void 예외_테스트_case5() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "-2"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
