ac56150344d4913e8a7e04da7d0f65dbcf0e4191
EunSeop <doosan1112@naver.com>
2024-10-26 17:25:31 +0900

    feat: 자동차 이름이 1자 이하일 때 검증 기능 구현

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class InputValidator {
     private static final int MAX_CAR_NAME = 5;
+    private static final int MIN_CAR_NAME = 1;
     private static final int MIN_ATTEMPTS = 1;
 
     public void isStringNull(String input) {
@@ -14,7 +15,10 @@ public class InputValidator {
 
     public void validateCarNames(List<String> carNames) {
         carNames.stream().filter(name -> name.length() > MAX_CAR_NAME).forEach(name -> {
-            throw new IllegalArgumentException("자동차 이름은 5자 이내여야 합니다.");
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        });
+        carNames.stream().filter(name -> name.length() < MIN_CAR_NAME).forEach(name -> {
+            throw new IllegalArgumentException("자동차 이름은 1자 이상이여야 합니다.");
         });
     }
 
