0976579ff7641e51bce71fd265a20966431b5117
rootTiket <l20078829@gmail.com>
2024-10-27 00:38:07 +0900

    feat<Parser>: 빈 문자열 검증로직 구현

--- src/main/java/racingcar/domain/Parser.java
+++ src/main/java/racingcar/domain/Parser.java
@@ -2,11 +2,16 @@ package racingcar.domain;
 
 import java.util.Arrays;
 import java.util.List;
+import racingcar.validator.InputValidator;
 
 public class Parser {
+    private final InputValidator inputValidator = new InputValidator();
+
     public List<String> parse(String input) {
         return Arrays.stream(input.split(","))
                 .map(String::trim)
+                .map(inputValidator::check)
                 .toList();
     }
+
 }
