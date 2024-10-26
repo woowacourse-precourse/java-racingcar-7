83c63ff2e4e7585c6b77d458617c7effa5c14a78
unh6unh6 <ruddbs3308@naver.com>
2024-10-26 16:03:32 +0900

    test(Car): 자동차 이름이 5자보다 많으면 예외

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,6 +1,8 @@
 package racingcar.model;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static racingcar.ExceptionMessage.CAR_NAME_LENGTH_EXCEPTION;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +13,13 @@ public class CarTest {
 
         assertThat(car.getName())
                 .isEqualTo("pobi");
+    }
+
+    @Test
+    void 자동차_이름이_5자보다_많으면_예외() {
+        assertThatThrownBy(() ->
+                new Car("overlength"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(CAR_NAME_LENGTH_EXCEPTION.message());
     }
 }
