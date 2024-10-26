8dd5f576c9fba8b7ebbcfe322bc97a7a65a88af9
mikekks <thdalsrb79@naver.com>
2024-10-26 18:46:27 +0900

    feat(Application): RacingGame 호출 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.common.RealRandom;
+import racingcar.domain.RacingGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        RacingGame racingGame = new RacingGame(new RealRandom());
+        racingGame.start();
     }
 }
