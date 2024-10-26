538c00fd261749e232f0daae472995989f87fbe7
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-10-26 14:50:45 +0900

    fix(view): 입력 뷰 Console.close 수정
    - 마자막으로 입력 받을 입력 뷰에서만 Console.close

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,7 +9,6 @@ public class InputView {
         final String INPUT_CARS_INTRO_MESSAGE ="경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
         System.out.println(INPUT_CARS_INTRO_MESSAGE);
         String input = Console.readLine();
-        Console.close();
         return input;
     }
     public static String inputRounds() {
