700d4c1fdaa1e85da78f138fd8ecbd73b8945922
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-10-26 14:10:27 +0900

    refactor: RacingCars의 MoveCondition 의존성주입 방식 변경

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -4,15 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCars {
-    List<Car> cars = new ArrayList<>();
 
-    public RacingCars(List<String> names) {
+    List<Car> cars = new ArrayList<>();
+    MoveCondition moveCondition;
+
+    public RacingCars(List<String> names,MoveCondition moveCondition) {
         for (String name : names) {
             cars.add(new Car(name, 0));
         }
+        this.moveCondition = moveCondition;
     }
 
-    public void moveAll(MoveCondition moveCondition) {
+    public void moveAll() {
         for (Car car : cars) {
             if(!moveCondition.canMove()) continue;
             car.moveForward(1);
