ead13639f10ec6f01f5202c8905860123dd6fd1e
fortune-man <wnguddl96@knou.ac.kr>
2024-10-26 13:09:29 +0900

    test(FrontendTest): 최소한의 실패하는 출력 테스트 추가

--- src/main/java/racingcar/Frontend.java
+++ src/main/java/racingcar/Frontend.java
@@ -2,7 +2,7 @@ package racingcar;
 
 public class Frontend {
   // 자동차 경주 실행 결과 조회
-  // 출력 문구 저장
+  // 출력 문구 생성
   // 자동차 경주 실행 결과 출력
   // 최종 우승자 출력
 
---
+++ src/test/java/racingcar/FrontendTest.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FrontendTest {
+
+  @DisplayName("자동차 경주 실행 결과를 조회한다")
+  @Test
+  void getRaceResult() {
+    // given
+    // when
+    // then
+  }
+
+  @DisplayName("사용자 입력에 근거한 자동차 경주 실행 결과를 제공하기 위해 출력한다")
+  @Test
+  void printRaceResult() {
+    // given
+    // when
+    // then
+  }
+
+  @DisplayName("게임 결과에 근거한 최종 우승자를 사용자에게 알려주기 위해 출력한다")
+  @Test
+  void printRaceWinner() {
+    // given
+    // when
+    // then
+  }
+
+
+
+}
