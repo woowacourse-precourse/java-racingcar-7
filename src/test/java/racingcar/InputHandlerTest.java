fe6059b333e98f3b5b17edb5ed155769513a8163
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-27 03:59:11 +0900

    test(InputHandlerTest): refactor System.setIn을 사용하여 입력값 모킹
    - 기존 NsTest를 사용한 방식에서 System.setIn으로 전환하여 테스트의 독립성 확보
    - 올바른 입력과 빈 문자열 입력 테스트에 대한 입력값을 모킹

--- src/test/java/racingcar/InputHandlerTest.java
+++ src/test/java/racingcar/InputHandlerTest.java
@@ -1,12 +1,13 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class InputHandlerTest extends NsTest {
+class InputHandlerTest {
 
     private static final String CAR_NAMES = "pobi,woni,jun";
     private static final String ATTEMPT_COUNT = "3";
@@ -16,10 +17,11 @@ class InputHandlerTest extends NsTest {
     @DisplayName("올바른 입력을 하면 예외가 발생하지 않고, 입력 값이 정상적으로 저장된다.")
     void 올바른입력() {
         // given
-        InputHandler inputHandler = new InputHandler();
+        InputHandler inputHandler = new InputHandler(new OutputHandler());
+        String simulatedInput = CAR_NAMES + "\n" + ATTEMPT_COUNT + "\n";
+        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
 
         // when
-        run(CAR_NAMES, ATTEMPT_COUNT);
         UserInput userInput = inputHandler.inputCarNamesAndAttemptCount();
 
         // then
@@ -31,19 +33,15 @@ class InputHandlerTest extends NsTest {
     @DisplayName("빈 문자열을 입력하면 예외가 발생하지 않고, 빈 값이 저장된다.")
     void 빈문자열입력() {
         // given
-        InputHandler inputHandler = new InputHandler();
+        InputHandler inputHandler = new InputHandler(new OutputHandler());
+        String simulatedInput = EMPTY_STRING + "\n" + EMPTY_STRING + "\n";
+        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
 
         // when
-        run(EMPTY_STRING, EMPTY_STRING);
         UserInput userInput = inputHandler.inputCarNamesAndAttemptCount();
 
         // then
         assertEquals(EMPTY_STRING, userInput.carNames());
         assertEquals(EMPTY_STRING, userInput.attemptCount());
-    }
-
-    @Override
-    public void runMain() {
-        Application.main(new String[]{});
     }
 }
