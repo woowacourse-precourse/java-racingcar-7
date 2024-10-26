8a172bbbef07c68a0fab8bf9d37991d5ea3988ff
Jang-GO <jang8195@naver.com>
2024-10-26 17:03:32 +0900

    refactor(InputValidator): 유효성 검증 로직 구현

--- src/test/java/racingcar/input/InputValidator.java
+++ src/test/java/racingcar/input/InputValidator.java
@@ -3,11 +3,15 @@ package racingcar.input;
 import static racingcar.Message.ExceptionMessage.*;
 
 public class InputValidator {
-    public void validateNameInput() {
-        throw new IllegalArgumentException(EMPTY_NAME_EXCEPTION.getMessage());
+    public void validateNameInput(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_NAME_EXCEPTION.getMessage());
+        }
     }
 
-    public void validateNumberInput() {
-        throw new IllegalArgumentException(NEGATIVE_ATTEMPT_EXCEPTION.getMessage());
+    public void validateNumberInput(int attempts) {
+        if (attempts <= 0) {
+            throw new IllegalArgumentException(NEGATIVE_ATTEMPT_EXCEPTION.getMessage());
+        }
     }
 }
--- src/test/java/racingcar/input/InputValidatorTest.java
+++ src/test/java/racingcar/input/InputValidatorTest.java
@@ -2,7 +2,6 @@ package racingcar.input;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.Message.ExceptionMessage;
 
 import static org.assertj.core.api.Assertions.*;
 import static racingcar.Message.ExceptionMessage.*;
@@ -12,20 +11,28 @@ public class InputValidatorTest {
     @DisplayName("빈 문자열만 자동차 이름으로 입력할 시 예외가 발생한다")
     @Test
     void emptyCarNameInput(){
-        InputValidator im = new InputValidator();
-        assertThatThrownBy(() -> {
-            im.validateNameInput();
-        }).isInstanceOf(IllegalArgumentException.class)
+        InputValidator validator = new InputValidator();
+        assertThatThrownBy(() -> validator.validateNameInput(" "))
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(EMPTY_NAME_EXCEPTION.getMessage());
     }
 
-    @DisplayName("양수가 아닌 값을 시도횟수로 입력하면 예외가 발상핸다")
+    @DisplayName("양수가 아닌 값을 시도횟수로 입력하면 예외가 발생한다")
     @Test
     void negativeAttemptNumberInput(){
-        InputValidator im = new InputValidator();
-        assertThatThrownBy(() -> {
-            im.validateNumberInput();
-        }).isInstanceOf(IllegalArgumentException.class)
+        InputValidator validator = new InputValidator();
+        assertThatThrownBy(() -> validator.validateNumberInput(-1))
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(NEGATIVE_ATTEMPT_EXCEPTION.getMessage());
     }
+
+    @DisplayName("0을 시도횟수로 입력하면 예외가 발생한다")
+    @Test
+    void zeroAttemptNumberInput(){
+        InputValidator validator = new InputValidator();
+        assertThatThrownBy(() -> validator.validateNumberInput(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NEGATIVE_ATTEMPT_EXCEPTION.getMessage());
+    }
+
 }
