f8f5a8be483d8fa0e71e1a1dc39c849d9dc69d2f
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-26 23:51:18 +0900

    feat(Car): Car 객체 구조 정의 기능

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,31 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+
+    private static final int START_NUMBER = 0;
+    private static final int END_NUMBER = 9;
+    private static final int MIN_NUMBER = 4;
+
+    private final String name;
+    private int advanceCount;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void attemptAdvance() {
+        if (Randoms.pickNumberInRange(START_NUMBER, END_NUMBER) >= MIN_NUMBER) {
+            advanceCount++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAdvanceCount() {
+        return advanceCount;
+    }
 }
