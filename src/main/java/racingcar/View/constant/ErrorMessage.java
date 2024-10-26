6ae4fae56d40f4c722270925e878d48777aa6a29
rootTiket <l20078829@gmail.com>
2024-10-27 00:29:59 +0900

    feat: 빈 문자열 검증을 위한 메시지 추가

--- src/main/java/racingcar/View/constant/ErrorMessage.java
+++ src/main/java/racingcar/View/constant/ErrorMessage.java
@@ -3,7 +3,8 @@ package racingcar.View.constant;
 import racingcar.global.globalEnum.Message;
 
 public enum ErrorMessage implements Message {
-    TOO_LONG_VALUE("문자열은 5글자를 넘을 수 없습니다");
+    TOO_LONG_VALUE("문자열은 5글자를 넘을 수 없습니다"),
+    BLANK_VALUE("문자열은 비어있을 수 없습니다");
     private final String message;
 
     ErrorMessage(String message) {
