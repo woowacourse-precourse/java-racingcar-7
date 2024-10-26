7f237ee0db159c7c371b20bf8860ab855ccec0ff
MinJae00 <exceedmin@gmail.com>
2024-10-27 00:59:59 +0900

    feat: Role of creating user objects when starting the game

--- src/main/java/racingcar/user/SettingGameUser.java
+++ src/main/java/racingcar/user/SettingGameUser.java
@@ -1,2 +1,12 @@
-package racingcar.operator;public class SettingGameUser {
+package racingcar.user;
+
+public class SettingGameUser {
+    public static UserInfo[] user;
+
+    public SettingGameUser(String[] result) {
+        user = new UserInfo[result.length];
+        for (int i = 0; i < result.length; i++) {
+            user[i] = new UserInfo(result[i]);
+        }
+    }
 }
