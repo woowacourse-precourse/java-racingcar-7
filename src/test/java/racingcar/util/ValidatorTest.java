c15d9f7508d7558734c0bea19239f9685ce4baf3
Yoon-Jemin <yjm1800@naver.com>
2024-10-27 02:08:16 +0900

    test(view): 자동차 이름 입력에 대한 테스트
    - 중복된 자동차 이름이 입력으로 주어졌을 때 예외가 발생하는지 테스트

--- src/test/java/racingcar/util/ValidatorTest.java
+++ src/test/java/racingcar/util/ValidatorTest.java
@@ -31,7 +31,22 @@ public class ValidatorTest {
         Validator validator = new Validator();
 
         // when
-        String carNames = ",abcd,abcde,abcdef";
+        String carNames = ",abc,abcd,abcde";
+
+        // then
+        assertThrows(IllegalArgumentException.class, () -> {
+            validator.checkCarNameInput(carNames);
+        });
+    }
+
+    @Test
+    @DisplayName("중복된 자동차 이름이 주어진 경우 예외가 발생한다")
+    void 자동차_이름_입력_테스트3() throws Exception {
+        //given
+        Validator validator = new Validator();
+
+        // when
+        String carNames = "abc,abc,abcd,abcde";
 
         // then
         assertThrows(IllegalArgumentException.class, () -> {
