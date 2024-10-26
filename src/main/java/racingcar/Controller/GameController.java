8af8ff0cc5a17a0aced334baf9a44a2a034bda9a
YugyeomGhim <rladbrua0207@gmail.com>
2024-10-26 20:18:24 +0900

    feat: 자동차 이름 검증 기능 구현
    - 자동차 이름은 적어도 두 개 입력 검증
    - 자동차 이름 입력 길이 최대 5글자 검증

--- src/main/java/racingcar/Controller/GameController.java
+++ src/main/java/racingcar/Controller/GameController.java
@@ -3,12 +3,21 @@ package racingcar.Controller;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class GameController {
     public void run() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNamesInput = Console.readLine();
         List<String> carNames = Arrays.stream(carNamesInput.split(",")).toList();
+        validateCarNames(carNames);
+    }
+
+    private void validateCarNames(List<String> carNames) {
+        if (carNames.size() < 2) {
+            throw new IllegalArgumentException("자동차 이름은 적어도 두 개 입력해야 합니다.");
+        }
+        if (carNames.stream().anyMatch(carName -> carName.length() > 5)) {
+            throw new IllegalArgumentException("자동차 이름 입력 길이를 초과하였습니다. (최대 5자)");
+        }
     }
 }
