4354ea45b4e0e23ebe6572ba6bd832cc3f044d32
wnsmir <163817041+wnsmir@users.noreply.github.com>
2024-10-26 23:50:58 +0900

    feat: Add race attempt input validation

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -1,4 +1,20 @@
 package racingcar;
 
+import  java.util.ArrayList;
+import java.util.List;
+
 public class InputHandler {
+    public static List<String> getCarNames(String input) {
+        String[] names = input.split(",");
+        List<String> carNames = new ArrayList<>();
+
+        for (String name : names) {
+            String trimmedName = name.trim();
+            if (trimmedName.isEmpty() || trimmedName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 1자이상 5자이하로 설정해야합니다!");
+            }
+            carNames.add(trimmedName);
+        }
+        return carNames;
+    }
 }
