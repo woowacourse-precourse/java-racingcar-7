fe213c95d47e167ca8a9d09e056776ebaba4aa62
mininiii <didals0521@gmail.com>
2024-10-26 19:36:33 +0900

    feat: 최종 우승자 구하는 기능 추가

--- src/main/java/racingcar/model/RaceCar.java
+++ src/main/java/racingcar/model/RaceCar.java
@@ -1,6 +1,7 @@
 package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RaceCar {
@@ -10,17 +11,33 @@ public class RaceCar {
         this.cars = cars;
     }
 
-    public void race(int attempts) {
-        while (attempts-- > 0) {
-            move();
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public List<String> getWinners() {
+        int maxStatus = 0;
+        List<String> winners = new ArrayList<>();
+
+        for (Car car : cars) {
+            int status = car.getStatus();
+
+            if (maxStatus < status) {
+                maxStatus = status;
+                winners.clear();
+                winners.add(car.getName());
+            } else if (maxStatus == status) {
+                winners.add(car.getName());
+            }
         }
+        return winners;
     }
 
     public void move() {
         cars.forEach(this::moveEach);
     }
 
-    public void moveEach(Car car) {
+    private void moveEach(Car car) {
         if (Randoms.pickNumberInRange(0, 9) >= 4) {
             car.moveCar();
         }
