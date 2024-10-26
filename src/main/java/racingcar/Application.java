f790413b22936f1bf347e829e81cbbd84d5f8bc9
fortune-man <wnguddl96@knou.ac.kr>
2024-10-26 12:38:35 +0900

    docs(README) : 유저 플로우 정리 중

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,16 @@
 package racingcar;
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Scanner scanner = new Scanner(System.in);
-        Console console = new Console();
-        Random random = new Random();
-        random.pickNumberInRange(0, 9);
-        random.
+        // 유저가 형식에 따라 자동차 이름과 시도 횟수를 입력하면
+        // 예: 경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)
+        // 시도할 횟수는 몇 회인가요?
+        // 입력값을 기반한 자동차 경주 내용과 실행 결과와 최종 우승자를 출력한다
+        // 실행 결과
+        // ...
+        // 최종 우승자 : 봉진 님
 
 }
