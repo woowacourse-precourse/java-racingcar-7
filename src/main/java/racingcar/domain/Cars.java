662a62f617f80f99f67adae1cafd75e2b14d52a2
gladhee <gladhee@icloud.com>
2024-10-27 00:13:31 +0900

    feat(Cars): Cars 도메인 로직 작성
    Car 전체 움직이기.
    Car 중에 가장 멀리간 car 찾기.
    중복이름 검사.

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,58 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import racingcar.domain.strategy.MoveStrategy;
+import racingcar.domain.strategy.RandomMoveStrategy;
+
+public class Cars {
+
+    private static final String SPLIT_DELIMITER = ",";
+    private static final String DUPLICATE_CAR_NAME = "중복된 이름이 존재합니다.";
+
+
+    private final List<Car> cars;
+    private final MoveStrategy moveStrategy;
+
+    public Cars(String carName) {
+        this.cars = createCars(carName);
+        this.moveStrategy = new RandomMoveStrategy();
+        validateDuplicateCarNames();
+    }
+
+    public void moveAllCars() {
+        cars.forEach(car -> car.move(moveStrategy));
+    }
+
+    public int getMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+
+    private List<Car> createCars(String carNames) {
+        String[] splitCarNames = splitCarNames(carNames);
+        List<Car> cars = new ArrayList<>();
+
+        for (String name : splitCarNames) {
+            cars.add(new Car(name));
+        }
+
+        return cars;
+    }
+
+    private String[] splitCarNames(final String carNames) {
+        return carNames.split(SPLIT_DELIMITER);
+    }
+
+    private void validateDuplicateCarNames() {
+        int uniqueCarNames = new HashSet<>(cars).size();
+
+        if (uniqueCarNames != cars.size()) {
+            throw new IllegalArgumentException(DUPLICATE_CAR_NAME);
+        }
+    }
+
+}
