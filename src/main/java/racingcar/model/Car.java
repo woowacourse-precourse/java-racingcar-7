cbf53cea5f78d0c8496eb5c7c5e31f1bb40d792e
Korigin99 <rldnjs3413@gmail.com>
2024-10-26 17:06:59 +0900

    feat: 자동차 전진 조건 결정 기능 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,27 @@
+package racingcar.model;
+
+public class Car {
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move(int randomValue) {
+        if (randomValue >= 4) {
+            this.position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+}
