4c31a8d411fe8f9927f041635a819dc06e359394
eunseongu <rndmstjs75@gmail.com>
2024-10-26 16:43:34 +0900

    refactor: 입력 기능 예외처리 방식 변경
    자동차 이름이나 시도 횟수에 빈 문자열이 입력될 경우 NoSuchElementException이 발생하므로
    IllegalArgumentException으로 처리 되도록 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class Application {
     public static void main(String[] args) {
@@ -11,26 +12,23 @@ public class Application {
     }
 
     public static RaceInput getInput() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = Console.readLine();
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String carNames = Console.readLine();
 
-        if (carNames == null || carNames.isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
+            System.out.println("시도할 횟수는 몇 회인가요?");
+            String inputOfAttempts = Console.readLine();
+
+            int numberOfAttempts = Integer.parseInt(inputOfAttempts.trim());
+
+            if (numberOfAttempts < 0) {
+                throw new IllegalArgumentException("시도 횟수는 0보다 작을 수 없습니다.");
+            }
+
+            return new RaceInput(carNames, numberOfAttempts);
         }
-
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String inputOfAttempts = Console.readLine();
-
-        if (inputOfAttempts == null || inputOfAttempts.isEmpty()) {
-            throw new IllegalArgumentException("시도 횟수는 비어있을 수 없습니다.");
+        catch (NoSuchElementException e){
+            throw new IllegalArgumentException("자동차 이름과 시도 횟수는 비어있을 수 없습니다.");
         }
-
-        int numberOfAttempts = Integer.parseInt(inputOfAttempts.trim());
-
-        if (numberOfAttempts < 0) {
-            throw new IllegalArgumentException("시도 횟수는 0보다 작을 수 없습니다.");
-        }
-
-        return new RaceInput(carNames, numberOfAttempts);
     }
 }
