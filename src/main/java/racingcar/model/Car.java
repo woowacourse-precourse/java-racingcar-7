62595d9cab56bc260de4658b8690118456ad4a6a
sc@t0a14 <gusdn6671@gmail.com>
2024-10-27 06:00:07 +0900

    refactor(Car): move() 메서드 이름 변경

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,38 +1,49 @@
 package racingcar.model;
 
+import java.util.Objects;
 import racingcar.utils.Validator;
 
 public class Car {
-    private final String name;
-    private int distance;
+    private final Name name;
+    private Distance distance;
 
-    private Car(String name) {
-        validate(name);
+    public Car(Name name) {
         this.name = name;
-        this.distance = 0;
+        this.distance = new Distance();
     }
 
-    public static Car from(String name) {
+    public static Car from(Name name) {
         return new Car(name);
     }
 
     public void move(MovementStrategy movingStrategy) {
         if (movingStrategy.moveable()) {
-            distance++;
+            distance.stepForward();
         }
     }
 
-    private void validate(String name) {
-        Validator.isEmpty(name);
-        Validator.isCarNameRange(name);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return Objects.equals(name, car.name) && Objects.equals(distance, car.distance);
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, distance);
     }
 
     public int getDistance() {
-        return distance;
+        return distance.getDistance();
     }
 
+    public String getName() {
+        return name.getName();
+    }
 }
