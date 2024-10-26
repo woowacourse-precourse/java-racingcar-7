7bce4bf7820bca2a4d77a98a1c3a01a42c159444
byeongdoohan <quden04@gmail.com>
2024-10-26 17:14:48 +0900

    refactor: 일급 컬렉션 적용

--- src/main/java/racingcar/model/CarGroup.java
+++ src/main/java/racingcar/model/CarGroup.java
@@ -4,46 +4,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarGroup {
-    private final List<Car> carList = new ArrayList<>();
-    private final List<String> winnerCarList = new ArrayList<>();
-    private int maxScore = 0;
+    private final List<Car> racingCarList;
 
     public CarGroup(String[] carList) {
+        List<Car> carGroup = new ArrayList<>();
         for (String carName : carList) {
             validateCarName(carName);
             Car car = new Car(carName);
-            this.carList.add(car);
+            carGroup.add(car);
         }
+        this.racingCarList = List.copyOf(carGroup);
     }
 
-    private void validateCarName(String carName){
-        if(carName.length()==0){
+    private void validateCarName(String carName) {
+        if (carName.length() == 0) {
             throw new IllegalArgumentException("자동차 이름의 길이는 0이 될 수 없습니다.");
         }
     }
 
     public List<Car> getCarList() {
-        return carList;
+        return racingCarList;
     }
 
     public void go() {
-        for (Car car : carList) {
+        for (Car car : racingCarList) {
             car.drive();
         }
     }
 
-    public void calculateMaxScore() {
-        for (Car car : carList) {
+    public int calculateMaxScore() {
+        int maxScore = 0;
+        for (Car car : racingCarList) {
             int i = car.getScore();
             if (maxScore < i) {
                 maxScore = i;
             }
         }
+        return maxScore;
     }
 
-    public List<String> getWinnerCarList(){
-        for(Car car: carList){
-            if(car.getScore()==maxScore){
+    public List<String> getWinnerCarList() {
+        int maxScore = calculateMaxScore();
+        List<String> winnerCarList = new ArrayList<>();
+        for (Car car : racingCarList) {
+            if (car.getScore() == maxScore) {
                 winnerCarList.add(car.getName());
             }
         }
