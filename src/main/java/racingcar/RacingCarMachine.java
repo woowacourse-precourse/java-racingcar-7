a27c8621c6f90806fa2f5dd01392ecf3ffb3f8e2
WARDKEY <tjrmsduq@naver.com>
2024-10-26 18:12:12 +0900

    feat(RacingCarMachine) : 자동차 입력 기능 추가

--- src/main/java/racingcar/RacingCarMachine.java
+++ src/main/java/racingcar/RacingCarMachine.java
@@ -1,8 +1,16 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class RacingCarMachine {
 
 	public void run() {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+		List<String> carList = Arrays.asList(Console.readLine().split(","));
+
 	}
 }
