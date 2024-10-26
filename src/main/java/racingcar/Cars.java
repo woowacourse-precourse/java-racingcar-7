591a713988775df382b1e1b89f6d88936065b150
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-27 06:26:39 +0900

    feat: Cars 일급 컬렉션 사용해 비지니스 로직 작성
    Names 리스트에 입력된 이름리스트를 받아 Cars에 부여한다. 차량의 움직임과
    가장 많이 움직인 우승자를 처리할 수 있는 비지니스 메서드가 존재한다.
    검증된 배열이 들어와 진행되기에 검증로직이 필요 없다.

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -1,2 +1,58 @@
-package racingcar;public class Cars {
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    public Cars(List<Name> cars) {
+        this.cars = getCars(cars);
+    }
+
+    public List<Car> getCars(List<Name> names) {
+        List<Car> carList = new ArrayList<>();
+
+        for (Name name : names) {
+            carList.add(new Car(name));
+        }
+        
+        return carList;
+    }
+
+    public void moveCars() {
+        for (Car car : cars) {
+            car.move();
+        }
+    }
+
+    public void currentStatus() {
+        for (Car car : cars) {
+            OutputManager.printCurrent(car);
+        }
+    }
+
+    public int checkMaxMovement(){
+        int maxMovement = 0;
+        for (Car car : cars) {
+            if (maxMovement < car.getStatus()){
+                maxMovement = car.getStatus();
+            }
+        }
+        return maxMovement;
+    }
+
+    public List<Car> checkWinner(){
+        List<Car> winners = new ArrayList<>();
+        int winnerMovement = checkMaxMovement();
+
+        for (Car car : cars) {
+            if (car.getStatus() == winnerMovement){
+                winners.add(car);
+            }
+        }
+
+        return winners;
+    }
 }
