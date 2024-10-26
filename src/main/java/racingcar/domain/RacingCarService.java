c6a15237aa7d1efce3bcd67bfae0a82688bce35f
KEEKE132 <dksvlfdhs1@naver.com>
2024-10-26 11:04:30 +0900

    feat: 우승자 판단 메서드 추가

--- src/main/java/racingcar/domain/RacingCarService.java
+++ src/main/java/racingcar/domain/RacingCarService.java
@@ -3,12 +3,14 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.view.RacingCarOutput;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCarService {
     private List<RacingCar> cars;
     private Integer number;
     private RacingCarOutput racingCarOutput = new RacingCarOutput();
+    private List<RacingCar> winners = new ArrayList<>();
 
     public RacingCarService(List<RacingCar> cars, Integer number) {
         this.number = number;
@@ -32,5 +34,28 @@ public class RacingCarService {
             moveRacingCars();
             racingCarOutput.printCurrentScore(this.cars);
         }
+        findWinner();
+    }
+
+    public void findWinner() {
+        winners.clear();
+        for (RacingCar car : cars) {
+            if (winners.isEmpty()) {
+                winners.add(car);
+                continue;
+            }
+            if (car.getPosition() == winners.getFirst().getPosition()) {
+                winners.add(car);
+                continue;
+            }
+            if (car.getPosition() > winners.getFirst().getPosition()) {
+                winners.clear();
+                winners.add(car);
+            }
+        }
+    }
+
+    public List<RacingCar> getWinners() {
+        return winners;
     }
 }
---
+++ src/test/java/racingcar/domain/RacingCarServiceTest.java
@@ -0,0 +1,32 @@
+package racingcar.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingCarServiceTest {
+
+    @Test
+    void 우승자_판단_테스트(){
+        RacingCar car1 = new RacingCar("Car1");
+        car1.moveForward();
+        car1.moveForward();
+
+        RacingCar car2 = new RacingCar("Car2");
+        car2.moveForward();
+
+        RacingCar car3 = new RacingCar("Car3");
+        car3.moveForward();
+        car3.moveForward();
+
+        RacingCarService racingCarService = new RacingCarService(List.of(car1,car2,car3),0);
+        racingCarService.findWinner();
+
+        assertThat(racingCarService.getWinners()).isEqualTo(List.of(car1,car3));
+    }
+
+
+}
