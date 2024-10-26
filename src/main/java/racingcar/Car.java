435091c7b3ff19cf6331483ba3e04251a5776415
dgh06175 <dgh06175@gmail.com>
2024-10-26 15:46:15 +0900

    feat: 자동차 이름 5자 초과일 경우 예외 발생

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -7,6 +7,9 @@ public class Car {
     private int position;
 
     public Car (String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
         this.name = name;
         this.position = 0;
         // TODO: 자동차 이름 검증
