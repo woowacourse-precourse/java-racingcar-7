1e172801bfb8c3b0535bcb8e0135848411862fea
DESKTOP-TMDOSVL\user <tlsekdn505@naver.com>
2024-10-26 17:42:06 +0900

    차량 위치 확인 하는 메서드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,19 +2,28 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Application {
+    private static List<Integer> carPositions;
+
     public static void main(String[] args) {
         try {
             List<String> carNames = getCarNames();
             System.out.println("입력한 자동차 이름: " + carNames);
 
+            carPositions = new ArrayList<>(Arrays.asList(new Integer[carNames.size()]));
+            for (int i = 0; i < carPositions.size(); i++) {
+                carPositions.set(i, 0); // 초기 위치 0으로 설정
+            }
+
             int tryCount = getTryCount();
             System.out.println("시도 횟수: " + tryCount);
 
             raceCars(tryCount);
+            carPosition();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
@@ -53,6 +62,13 @@ public class Application {
     private static void raceCars(int tryCount){
         for (int i = 0; i < tryCount; i++){
             System.out.println("차수 " + (i + 1) + " 진행 중...");
+        }
+    }
+
+    private static void carPosition(){
+        for(int i= 0; i < carPositions.size(); i++){
+            System.out.println("자동차 " + (i + 1) + ": " );
+            System.out.println("-".repeat(carPositions.get(i)));
         }
     }
 
