241c200c9f6111764ef42b3d09b0c99511fa9b0a
rootTiket <l20078829@gmail.com>
2024-10-27 00:53:55 +0900

    feat: 시작 메시지 출력 구현

---
+++ src/main/java/racingcar/View/OutputView.java
@@ -0,0 +1,9 @@
+package racingcar.View;
+
+import static racingcar.View.constant.OutputMessage.START_MESSAGE;
+
+public class OutputView {
+    public void printStart() {
+        System.out.println(START_MESSAGE.getMessage());
+    }
+}
