5d455a1a289c8fe431e28c95b39aacbcd12a8a86
Mini <mini@MiniMac.local>
2024-10-26 14:27:38 +0900

    feat: add car name input

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,7 +8,10 @@ public class Application {
         System.out.println("자동차의 수를 입력해주세요");
 
         int countCars = Integer.parseInt(Console.readLine());
-        
+
+        System.out.println("자동차의 이름을 쉼표(.)로 구분해 입력해주세요 (5자 이하)");
+        String[] carName = (Console.readLine()).split(",");
+
 
 
         }
