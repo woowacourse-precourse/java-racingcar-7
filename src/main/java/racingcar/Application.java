28dbb4994f564fe4f66b9266e22f5c89b8284855
JeongSu Park <bechalrang@gmail.com>
2024-10-26 15:07:46 +0900

    feat(Application): 어플리케이션 실행 구동 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,30 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.business.CarRacingService;
+import racingcar.object.carEngine.RegulationsCarEngine;
+import racingcar.object.value.CarRacingResult;
+import racingcar.view.RacingInput;
+import racingcar.view.RacingOutput;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            CarRacingService carRacingService = new CarRacingService(new RegulationsCarEngine());
+            CarRacingResult carRacingResult = carRacingService.performCarRacing(
+                    RacingInput.askCarNamesForParticipants(),
+                    RacingInput.askTotalLapCount());
+
+            RacingOutput.announceRaceDetails(carRacingResult.raceRecord());
+            RacingOutput.announceRaceWinners(carRacingResult.winners());
+
+        } catch (Error e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("예기치 못한 오류로 프로그램을 종료합니다.");
+        } finally {
+            Console.close();
+        }
     }
+
 }
