2054dd04be77fb2f9c083038abd637b10e10ec32
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-26 23:47:17 +0900

    feat(ValidatorFactory): 유효성 검사 객체 생성 기능

--- src/main/java/racingcar/factory/ValidatorFactory.java
+++ src/main/java/racingcar/factory/ValidatorFactory.java
@@ -1,4 +1,19 @@
 package racingcar.factory;
 
+import racingcar.validator.AttemptCountValidator;
+import racingcar.validator.CarNameValidator;
+import racingcar.validator.Validator;
+
 public class ValidatorFactory {
+
+    public static final String TYPE_CAR_NAMES = "Car names";
+    public static final String TYPE_ATTEMPT_COUNT = "Attempt Count";
+
+    public Validator create(String type) {
+        return switch (type) {
+            case TYPE_CAR_NAMES -> new CarNameValidator();
+            case TYPE_ATTEMPT_COUNT -> new AttemptCountValidator();
+            default -> throw new IllegalArgumentException();
+        };
+    }
 }
