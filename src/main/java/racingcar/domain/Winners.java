e610469847355672c1e16e3c9d83d9523de85114
mikekks <thdalsrb79@naver.com>
2024-10-26 17:29:43 +0900

    feat(Winners): 우승자 Car 목록을 저장하는 일급컬렉션 구현

---
+++ src/main/java/racingcar/domain/Winners.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class Winners {
+	private final List<Car> winners;
+
+	public Winners(List<Car> winners) {
+		this.winners = winners;
+	}
+}
