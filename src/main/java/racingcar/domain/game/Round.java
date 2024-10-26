32ab1bda605e0699eec0e1e18df99924a4f50698
choiminu <alsdn4590@gmail.com>
2024-10-26 16:29:15 +0900

    feat (domain): 사용자에게 입력받은 게임 횟수를 검사하는 기능 추가
    - 시도 횟수를 나타내는 Round 클래스를 추가.
    - 입력된 시도 횟수를 파싱하고 유효성 검사를 수행하도록 구현하였음
      - 최대 100회, 최소 1회로 제한.
      - 숫자가 아닌 값 입력 시 예외 처리.
    - 동작이 올바르게 동작하는지 확인하기 위하여 테스트코드 작성

---
+++ src/main/java/racingcar/domain/game/Round.java
@@ -0,0 +1,40 @@
+package racingcar.domain.game;
+
+public class Round {
+    private final int MAX_ROUNDS = 100;
+    private final int MIN_ROUNDS = 1;
+    private final int round;
+
+    public Round(String roundInput) {
+        this.round = parseAndValidateRound(roundInput);
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+    private int parseAndValidateRound(String roundInput) {
+        int parsedRound = convertToInteger(roundInput);
+        validateRound(parsedRound);
+        return parsedRound;
+    }
+
+    private void validateRound(int round) {
+        if (round > MAX_ROUNDS) {
+            throw new IllegalArgumentException("시도할 횟수는 100을 넘을 수 없습니다.");
+        }
+
+        if (round < MIN_ROUNDS) {
+            throw new IllegalArgumentException("시도할 횟수는 1이상이어야 합니다.");
+        }
+    }
+
+    private int convertToInteger(String roundInput) {
+        try {
+            return Integer.parseInt(roundInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 횟수는 숫자로 입력해주세요.");
+        }
+    }
+
+}
