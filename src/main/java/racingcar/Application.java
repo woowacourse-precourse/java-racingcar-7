3a6b19a07c2b8534f08612a622e6e77e4d3edd8f
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-27 07:01:23 +0900

    feat: 전반적인 로직 흐름을 나타내는 main로직 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        try {
+            List<Name> names = InputManager.getInput();
+            int rounds = InputManager.getGameRound();
+
+            Cars cars = new Cars(names);
+
+            Game.start(cars, rounds);
+        } finally {
+            InputManager.close();
+        }
+
     }
 }
