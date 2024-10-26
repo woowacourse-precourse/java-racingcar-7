32cf76aa643735e880a6e106228114b0545fa07f
ahpicl64 <ahpicl64@gmail.com>
2024-10-27 00:26:34 +0900

    feat(view) : 차량 이름 등록 기능 구현
    - InputView.java : 입력 문자열을 구분자로 분리,  List 컬렉션에 등록하는 기능 구현
    - README.md : 구현목록 수정

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,20 +1,22 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
 
-    public String printStart() {
+    public List<String> printStart() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-        String playerNames = Console.readLine();
+        String playerNamesInput = Console.readLine();
+        List<String> playerNamesArray = Arrays.asList(playerNamesInput.split(","));
 
-        return playerNames;
+        return playerNamesArray;
     }
 
     public int printRounds() {
         System.out.println("시도할 횟수는 몇 회인가요?\n");
-        int gameRounds = Integer.parseInt(Console.readLine());
 
-        return gameRounds;
+        return Integer.parseInt(Console.readLine());
     }
 }
