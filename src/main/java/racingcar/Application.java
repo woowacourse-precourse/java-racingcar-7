ec72554bf641970a43992af85a4f83edf414754a
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-10-27 02:32:20 +0900

    feat: 자동차 이름에 대한 정규식 검증 추가
    - 자동차 이름에 영문자, 숫자, 한글, 언더바(_)만 허용
    - 언더바가 이름의 처음과 끝에 위치할 경우 IllegalArgumentException 발생하도록 정규식 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,7 +8,8 @@ import java.util.List;
 public class Application {
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가?";
-
+    private static final String VALID_NAME_PATTERN = "^(?!_)[a-zA-Z0-9가-힣_]+(?<!_)$";
+    
     public static void main(String[] args) {
         System.out.println(INPUT_CAR_NAMES_MESSAGE);
         String input = Console.readLine();
@@ -24,6 +25,10 @@ public class Application {
 
             if (name.length() > 5) {
             	throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+            }
+            
+            if (!name.matches(VALID_NAME_PATTERN)) {
+                throw new IllegalArgumentException("자동차 이름은 영문자, 숫자, 한글만 포함하고, 언더바(_)는 처음이나 끝에 위치할 수 없습니다.");
             }
             
             if (carNames.contains(name)) {
