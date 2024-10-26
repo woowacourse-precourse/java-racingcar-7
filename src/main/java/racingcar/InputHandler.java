d2564fa6231517e91f9aa0986a923cf9ad7ba4a0
wodus4145@naver.com <wodus4145@naver.com>
2024-10-26 15:46:32 +0900

    feat: Take String input and return it as List<String>

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -6,4 +6,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputHandler {
+
+    public List<Car> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputNames = Console.readLine();
+        String[] names = devideNames(inputNames);
+        return makeNameList(names);
+    }
+
+    public List<Car> makeNameList(String[] names) throws IllegalArgumentException {
+        List<Car> racingCar = new ArrayList<>();
+        for (String name : names) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException();
+            } else {
+                racingCar.add(new Car(name, 0));
+            }
+        }
+        return racingCar;
+    }
+
+    public String[] devideNames(String names) {
+        return names.split(",");
+    }
 }
