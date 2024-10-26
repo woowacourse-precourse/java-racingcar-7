3f53c45d196e1fcdc36479df5df31db082883701
seonseon1515 <seonseon1515@gmail.com>
2024-10-26 20:59:59 +0900

    feat: 0~9 사이의 랜덤 수 구하기

---
+++ src/main/java/racingcar/RandomNumber.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumber {
+    private static int randomNum;
+
+    int randomNumber (){
+        randomNum = Randoms.pickNumberInRange(0,9);
+        return randomNum;
+    }
+
+    public static int getRandomNum() {
+        return randomNum;
+    }
+}
