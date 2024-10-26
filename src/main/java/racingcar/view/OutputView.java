6b37417ce453ac5194bf4d2eecae1a2d211b692c
MinJae00 <exceedmin@gmail.com>
2024-10-27 00:57:45 +0900

    feat: Output results while the game is in progress

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,30 @@
-package racingcar.View;public class OutputView {
+package racingcar.view;
+
+import racingcar.user.SettingGameUser;
+import racingcar.user.UserInfo;
+
+public class OutputView {
+    public void ShowResult() {
+        for (UserInfo user : SettingGameUser.user) {
+            System.out.println(user.name + ":" + user.score);
+        }
+        System.out.println("");
+    }
+
+    public void PrintResultSentence() {
+        System.out.println("");
+        System.out.println("실행 결과");
+    }
+    
+    public void PrintWinnerSentence() {
+        System.out.print("최종 우승자 : ");
+    }
+
+    public void PrintWinner(String s) {
+        System.out.print(s);
+    }
+
+    public void PrintRest() {
+        System.out.print(" ,");
+    }
 }
