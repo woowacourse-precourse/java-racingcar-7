2a43ed7aa7be95653be3e15825dc4914b74303ad
yybmion <yyb400@naver.com>
2024-10-26 21:05:43 +0900

    test: 자동차 이름 중복 검증 테스트코드 추가
    중복된 자동차 이름이 존재한다면 예외를 발생한다.

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @DisplayName("자동차들 테스트")
 public class CarsTest {
@@ -19,5 +20,13 @@ public class CarsTest {
 
         // then
         assertThat(cars.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("중복된 이름이 존재하면 에외가 발생한다.")
+    void 중복된_자동차_이름_테스트() {
+        assertThatThrownBy(() -> Cars.createCarsFrom("yoon,yoo,biny,yoon"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 중복될 수 없습니다.");
     }
 }
