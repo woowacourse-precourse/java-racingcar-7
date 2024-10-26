247cf0e3e90a929e5e9d46b012f521eacf3b076d
minSsan <msp1125@hanyang.ac.kr>
2024-10-26 17:14:49 +0900

    feat(CarNameDelimiter): 자동차 이름 구분자 enum

---
+++ src/main/java/racingcar/domain/model/constant/CarNameDelimiter.java
@@ -0,0 +1,26 @@
+package racingcar.domain.model.constant;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public enum CarNameDelimiter {
+    COMMA(",");
+
+    private final String delimiter;
+
+    private CarNameDelimiter(final String delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    public String getValue() {
+        return delimiter;
+    }
+
+    public static String getRegex() {
+        List<String> delimiters = Arrays.stream(CarNameDelimiter.values())
+                .map(c -> Pattern.quote(c.getValue()))
+                .toList();
+        return String.join("|", delimiters);
+    }
+}
