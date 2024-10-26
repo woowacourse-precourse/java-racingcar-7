d45ec5898d1ef171b4cc155f7e6c2748a01202b6
gorapaduckoo <jioung93@naver.com>
2024-10-27 03:04:33 +0900

    refactor: InputHandler에서 InputValidator에 대한 불필요한 의존성 제거

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -6,12 +6,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputHandler {
 
-    private final InputValidator inputValidator;
-
-    public InputHandler(InputValidator inputValidator) {
-        this.inputValidator = inputValidator;
-    }
-
     public List<String> getCarNames() {
         String input = Console.readLine();
         return Arrays.stream(input.split(","))
