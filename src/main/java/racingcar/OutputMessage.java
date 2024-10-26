6bed70290c9a6381631b63aecd51d584f542b0e8
gorapaduckoo <jioung93@naver.com>
2024-10-27 03:32:29 +0900

    feat: 게임 진행, 결과 관련 메세지 추가

---
+++ src/main/java/racingcar/OutputMessage.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public enum OutputMessage {
+    CAR_NAME_INPUT_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    ROUND_COUNT_INPUT_MESSAGE("시도할 횟수는 몇 회인가요?"),
+    RESULT_MESSAGE("실행 결과"),
+    WINNER_NOTICE_PREFIX("최종 우승자 : ")
+    ;
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
