e3ed116b2643dc62a736d197f71d75036901abd9
yokxim <yooksi53@gmail.com>
2024-10-27 02:31:22 +0900

    feat: 입력 값이 유효하지 않은 경우 IllegalArgumentException을 발생시키고, 애플리케이션을 종료한다.

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -3,6 +3,25 @@ package racingcar;
 public class Race {
 
     public void raceStart(String[] names, String count) {
+        int tryCount = Integer.parseInt(count);
+        checkInput(names, tryCount);
+    }
 
+    // ============= 편의 메서드 =============
+
+    private void checkInput(String[] names, int count) {
+        for (String name : names) {
+            checkIfLengthExceed(name);
+        }
+
+        if (count <= 0) {
+            throw new IllegalArgumentException("시도 횟수를 알맞게 입력해 주세요.");
+        }
+    }
+
+    private void checkIfLengthExceed(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("각 이름은 최대 5자 이하여야 합니다.");
+        }
     }
 }
