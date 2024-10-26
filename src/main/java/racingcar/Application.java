22ee7b9a69f46b108efd538f30190f56f71de694
jjr7181 <jjr7181@gmail.com>
2024-10-26 18:20:54 +0900

    refactor: move input function to RacingGame class

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,10 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.*;
+import racingcar.domain.RacingGame;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼포(,) 기준으로 구분)");
-        String players = Console.readLine();
-
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int rounds = Integer.parseInt(Console.readLine());
-
+        RacingGame game = new RacingGame();
+        game.start();
     }
 }
--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,4 +1,19 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+
 public class RacingGame {
+    private List<Car> cars;
+
+    public void start() {
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼포(,) 기준으로 구분)");
+        String inputCars = Console.readLine();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int inputRounds = Integer.parseInt(Console.readLine());
+
+    }
 }
