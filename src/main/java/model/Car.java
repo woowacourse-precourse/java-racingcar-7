be1dc9a2da032414a8396d127d0f1d559fba7fa8
codrae <hi653@naver.com>
2024-10-27 00:50:10 +0900

    style:  의미 없는 주석 삭제
    주석을 삭제하고 따로 문서로 정리함.

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -31,14 +31,3 @@ public class Car {
         return name + " : " + "-".repeat(Math.max(0, position));
     }
 }
-
-/*
-    JAVA에서의 문자열 더하기에 관하여.
-         str +=  "-";   java에서는 문자열이 불변이기에 매번 새로운 문자열 객체를 만들게 됨. (비효율적)
-         StringBuffer 멀티스레드 환경에서 안전한 방식으로 동기화가 추가되어 있음.
-         String.join() 배열이나 리스트를 특정 구분자로 연결해 문자열로 만들 때 사용
-         Collectors.joining() 리스트의 요소들을 문자열로 합칠 때 사용
-         단일 스레드, 문자열을 많이 이어붙이는 상황에서 사용.
-         더 하는 문자열이 많은 경우에는 StringBuilder를 사용하는 방식 권장.
-         여기서는 그 양이 적기에 String에 직접적으로 사용
- */
