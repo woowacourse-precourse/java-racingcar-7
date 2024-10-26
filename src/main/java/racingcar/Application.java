0009e6ea60e2aa4fb298777c6888bcbc377fa887
imscow11253 <seaweed8759@naver.com>
2024-10-27 01:30:20 +0900

    feat : Service 클래스를 Controller 클래스에 DI

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.application.RacingCarService;
+import racingcar.application.RacingCarServiceImpl;
+import racingcar.presentation.RacingCarController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingCarService racingCarService = new RacingCarServiceImpl();
+        RacingCarController racingCarController = new RacingCarController(racingCarService);
+
+        racingCarController.run();
     }
 }
