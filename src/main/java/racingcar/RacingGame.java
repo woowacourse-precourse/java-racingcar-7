32f04834db9ded7fcdaad4819efc3199e0779032
33jyu33 <jy04169@gmail.com>
2024-10-26 16:25:36 +0900

    feat(RacingGame): car moves according to random value, print results
    Function startGame:
    - printResultGuid
    - play as many games as attemptNum
    Function carMove:
    - move the car if the random value between 0 and 9 is 4 or more
    Function printAttemptResult:
    - print the results of each player's attempts according to the output format

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,5 +1,11 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
 public class RacingGame {
     Car car = new Car();
     Integer attemptNum;
@@ -7,6 +13,7 @@ public class RacingGame {
     public void run(){
         car.set();
         setAttemptNum();
+        startGame();
     }
 
     private void setAttemptNum(){
@@ -28,8 +35,32 @@ public class RacingGame {
         }
     }
 
+    private void startGame(){
+        UserView.changeLine();
+        UserView.printResultGuide();
+        List<String> cars = car.carMap.keySet().stream().toList();
+        for (int i=0; i < attemptNum; i++){
+            carMove(cars);
+            printAttemptResult(cars);
+        }
+    }
+
+    private void carMove(List<String> cars){
+        for (String car: cars){
+            if (Randoms.pickNumberInRange(0, 9) >= 4){
+                this.car.carMap.replace(car, this.car.carMap.get(car) + 1);
+            }
+        }
+    }
+
+    private void printAttemptResult(List<String> cars){
+        for (String car: cars){
+            UserView.printPlayerAttemptResult(car, this.car.carMap.get(car));
+        }
+        UserView.changeLine();
+    }
+
     private void throwArgumentException(String errorContext){
         throw new IllegalArgumentException(errorContext);
     }
-
 }
