f71b6e7fbc76be67fbeecfdea8b5c2177805b083
skhnpark <skhnpark125@gmail.com>
2024-10-26 19:35:36 +0900

    feat: 자동차 이름들을 구분하여 저장하는 기능 구현

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -1,6 +1,13 @@
 package racingcar.service;
 
 public class CarService {
+    String[] cars;
+
     public void createGame(String carNames) {
+        splitCarNames(carNames);
+    }
+
+    public void splitCarNames(String carNames) {
+        cars = carNames.split(",");
     }
 }
