cc72fc7b67cb175d9186a67688aa714ccbdda274
dltmddus <tmddus4671@gmail.com>
2024-10-26 22:52:31 +0900

    feat: 자동차 전진 기능 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,6 +1,9 @@
 package racingcar.domain;
 
+import static racingcar.view.OutputView.printCurrentResult;
+
 import java.util.List;
+import racingcar.util.RandomGeneratorUtil;
 
 public class Cars {
     private final List<Car> cars;
@@ -9,4 +12,18 @@ public class Cars {
         this.cars = cars;
     }
 
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void move() {
+        for (Car car : cars) {
+            int randomNumber = RandomGeneratorUtil.getRandomNumber();
+            if (randomNumber >= 4) {
+                car.movementOfNumber++;
+            }
+        }
+        printCurrentResult(cars);
+
+    }
 }
