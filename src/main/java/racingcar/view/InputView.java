48aac24569f83ae014c857c35e8f14e45a15b2ff
honggyu1233@naver.com <honggyu1233@naver.com>
2024-10-27 03:02:43 +0900

    feat : InputView의 println 삭제

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,10 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public static Object[] racingcarInputView() {
         String inputName=Console.readLine();
-        System.out.println("inputName = " + inputName);
 
         String moveCount = Console.readLine();
-        System.out.println("moveCount = " + moveCount);
         return new Object[]{inputName, moveCount};
     }
 }
