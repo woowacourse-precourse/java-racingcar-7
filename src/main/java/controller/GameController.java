b3fab6daae853889f90decfb66f342c5ddceea17
Wooseok Jang <wowseok2774@gmail.com>
2024-10-26 22:07:47 +0900

    feat(controller): 게임로직을 처리하는 GameController 추가

---
+++ src/main/java/controller/GameController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.util.List;
+import model.Car;
+import service.RacingCarService;
+import view.OutputView;
+
+public class GameController {
+    private final RacingCarService racingCarService;
+    private final OutputView outputView;
+
+    public GameController(RacingCarService racingCarService, OutputView outputView) {
+        this.racingCarService = racingCarService;
+        this.outputView = outputView;
+    }
+
+    public void startGame(List<Car> cars, int attempts) {
+        for (int i = 0; i < attempts; i++) {
+            racingCarService.playRound(cars);
+            outputView.printRaceStatus(cars);
+        }
+        List<String> winner = racingCarService.findFinalWinners(cars);
+        outputView.printFinalWinner(winner);
+    }
+}
