6e114ecf07618a17edebdc8fdb32cd2e2724511b
jinwook <wjswlsdnr333@gmail.com>
2024-10-27 03:19:47 +0900

    test(ApplicationTest): 자동차 3대인 경우 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -8,20 +8,24 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class ApplicationTest extends NsTest {
 
+class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
+    private static final int STOP = 0;
+
+    private static final int[] DISTANCES = {STOP, STOP, MOVING_FORWARD};
 
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
                 () -> {
-                    run("pobi,woni", "5");
-                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                    run("pobi,woni,dao", "1");
+                    assertThat(output()).contains("pobi : ", "woni : ","dao : -", "최종 우승자 : dao");
                 },
-                MOVING_FORWARD, STOP
+                DISTANCES[0],DISTANCES[1],DISTANCES[2]
         );
+
+
     }
 
     @Test
