6f2432573da6b181ae0b1ae775d45f23a6c1ee24
JoonKyo <joonkyolee@gmail.com>
2024-10-27 04:13:55 +0900

    feat(Car): 랜덤으로 얻은 정수로부터 이동할 수 있는지 여부를 결정하는 메서드 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,11 +1,20 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private static final int MIN_BOUND = 0;
+    private static final int MAX_BOUND = 9;
+    private static final int MOVE_BOUND = 4;
     private final CarName carName;
     private final int position;
 
     public Car(CarName carName) {
         this.carName = carName;
         position = 0;
+    }
+
+    private boolean canMove() {
+        return Randoms.pickNumberInRange(MIN_BOUND, MAX_BOUND) >= MOVE_BOUND;
     }
 }
