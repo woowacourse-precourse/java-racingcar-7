b74424c89170a40d0054eaf0e2a8ecc6774c7e49
jina <jinahoi@naver.com>
2024-10-27 01:46:34 +0900

    feat: 최종 우승자 출력 기능 추가
    - 해당 경주의 최종 우승자 이름 리스트를 사용해 우승자 목록을 출력합니다.
    - 우승자가 여러 명일 경우 쉼표(,)와 공백을 각 이름 사이에 추가합니다.

--- src/main/java/racingcar/handler/OutputHandler.java
+++ src/main/java/racingcar/handler/OutputHandler.java
@@ -1,5 +1,6 @@
 package racingcar.handler;
 
+import java.util.List;
 import racingcar.model.Car;
 import racingcar.model.Race;
 
@@ -8,6 +9,11 @@ public class OutputHandler {
     public void printRoundResult(Race race) {
         race.getCars().forEach(this::carStatusPrintFormat);
         System.out.println();
+    }
+
+    public void printFinalWinners(Race race) {
+        List<String> winnerNameList = race.createWinnerNameList();
+        System.out.println("최종 우승자 : " + String.join(", ", winnerNameList));
     }
 
     private void carStatusPrintFormat(Car car) {
