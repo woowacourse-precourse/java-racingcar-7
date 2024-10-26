9ecf099f0369cb4d32d0ecb8742592e6b4248d82
Byuntil <gugugu2014@jbnu.ac.kr>
2024-10-26 20:41:33 +0900

    feat(Car): Car 이동 기능 구현
    4이상의 값일 때 차량의 위치 현재 위치+1됨

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,8 +4,11 @@ public class Car {
 
     public static final String EMPTY_CAR_NAME_ERROR = "경주할 자동차 이름을 입력해주세요.";
     public static final String INVALID_CAR_NAME_LENGTH_ERROR = "경주할 자동차 이름은 5자 이하입니다.";
+    public static final int MAXIMUM_LENGTH = 5;
+    public static final int STANDARD_VALUE_CAR_CAN_MOVE = 4;
 
     private final String name;
+    private int position = 0;
 
     public Car(final String name) {
         validate(name);
@@ -16,8 +19,18 @@ public class Car {
         if (name.isEmpty()) {
             throw new IllegalArgumentException(EMPTY_CAR_NAME_ERROR);
         }
-        if (name.length() > 5) {
+        if (name.length() > MAXIMUM_LENGTH) {
             throw new IllegalArgumentException(INVALID_CAR_NAME_LENGTH_ERROR);
         }
+    }
+
+    public void move(int condition) {
+        if (condition >= STANDARD_VALUE_CAR_CAN_MOVE) {
+            this.position++;
+        }
+    }
+
+    public int getPosition() {
+        return this.position;
     }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -23,5 +24,27 @@ class CarTest {
         assertThatThrownBy(() -> new Car(""))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("경주할 자동차 이름을 입력해주세요.");
+    }
+
+    @Test
+    @DisplayName("기준값이 4이상이면 차량의 위치가 +1 된다.")
+    void carMoveTest() {
+        // given
+        Car car = new Car("pobi");
+        // when
+        car.move(4);
+        // then
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("기준값이 4미만이면 차량의 위치는 이동하지 않는다.")
+    void carNotMoveTest() {
+        // given
+        Car car = new Car("pobi");
+        // when
+        car.move(3);
+        // then
+        assertThat(car.getPosition()).isZero();
     }
 }
