ae4ebd1ece844e7d964b178654e052e19f6a29ad
sinyebin <dpqls0625@naver.com>
2024-10-26 23:14:21 +0900

    feat: 숫자가 4이상 전진, 3이하 전진 불가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,11 +3,18 @@ package racingcar.domain;
 public class Car {
     private static final String INPUT_LENGTH_EXCEEDED_ERROR = "5자 초과하였습니다.";
     private static final String EMPTY_NAME_ERROR = "이름이 없습니다.";
+    private static final int FOWARD_NUMBER = 4;
     private final String carName;
+    private Distance distance;
 
     public Car(String carName) {
+        this(carName, new Distance(0));
+    }
+
+    public Car(String carName, Distance distance) {
         validateCarName(carName);
         this.carName = carName;
+        this.distance = distance;
     }
 
     private void validateCarName(String carName) {
@@ -17,4 +24,15 @@ public class Car {
             throw new IllegalArgumentException(EMPTY_NAME_ERROR);
         }
     }
+
+    public Distance getDistance() {
+        return distance;
+    }
+
+    public void moveCar(int randomNumber) {
+        if (randomNumber >= FOWARD_NUMBER) {
+            this.distance = distance.increaseDistance();
+        }
+    }
+
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,22 @@ class CarTest {
 
         assertThatThrownBy(() -> new Car(""))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자가 4이상인 경우 전진할 수 있다.")
+    @Test
+    public void 전진_하는지() {
+        Car car = new Car("pobi");
+        car.moveCar(4);
+        assertThat(car.getDistance()).isEqualTo(new Distance(1));
+    }
+
+    @DisplayName("숫자가 3이하인 경우 전진할 수 없다.")
+    @Test
+    public void 전진_안하는지() {
+        Car car = new Car("pobi");
+        car.moveCar(1);
+        assertThat(car.getDistance()).isEqualTo(new Distance(0));
     }
 
 }
