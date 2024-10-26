413e7d76715a96ca3d7e21890c75a26c11f3835e
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-27 03:02:08 +0900

    test($ApplicationTest): 게임 결과 테스트 추가 작성
    - 게임 결과가 최종 우승자를 출려하는지 테스트.
    - 각 라운드 별 자동차의 이름과 위치를 출력하는지 테스트.
    - 기존의 테스트는 제출을 위해 남겨두기로 결정.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,35 +1,103 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import racingcar.dto.RacingRoundResult;
+import racingcar.service.PlayArguments;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.constant.Constants.RACING_CAR_MIN_MOVABLE_NUMBER;
 
 class ApplicationTest extends NsTest {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
 
+    private final int MOVING_FORWARD = RACING_CAR_MIN_MOVABLE_NUMBER;
+    private final int STOP = RACING_CAR_MIN_MOVABLE_NUMBER - 1;
+
+    static Stream<PlayArguments> providePlayArguments() {
+        return Stream.of(
+                new PlayArguments(
+                        List.of("a", "b", "c"),
+                        3,
+                        List.of(1, 2, 6, 3, 5, 6, 3, 3, 4)),
+
+                new PlayArguments(
+                        List.of("a", "b"),
+                        2,
+                        List.of(1, 4, 5, 2)),
+
+                new PlayArguments(
+                        List.of("a", "b", "c"),
+                        1,
+                        List.of(1, 2, 3))
+        );
+    }
+
+    @ParameterizedTest(name = "playArguments: {0}")
+    @MethodSource("providePlayArguments")
+    @DisplayName("main() : 게임_결과_테스트")
+    void 게임_결과_테스트(PlayArguments playArguments) {
+
+        // given
+        String carNamesInput = String.join(",", playArguments.getCarNames());
+        String tryCountInput = String.valueOf(playArguments.getTryCount());
+
+        Integer firstRandomNumber = playArguments.getRandomNumbers().getFirst();
+        Integer[] remainRandomNumbers = playArguments.getRandomNumbers().stream().skip(1).toArray(Integer[]::new);
+        String expectedWinnersNames = String.join(", ", playArguments.getExpectedWinnersNames());
+
+        List<RacingRoundResult> racingRoundResults = playArguments.getRacingRoundResults();
+
+        // when & then
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run(carNamesInput, tryCountInput);
+                    assertThat(output()).contains("최종 우승자 : " + expectedWinnersNames);
+                    racingRoundResults.forEach(racingRoundResult -> {
+                        List<String> expected = convertRacingRoundResultToString(racingRoundResult);
+                        expected.forEach(message -> assertThat(output()).contains(message));
+                    });
+                },
+                firstRandomNumber, remainRandomNumbers
+        );
+    }
+
+    private List<String> convertRacingRoundResultToString(RacingRoundResult racingRoundResult) {
+        return racingRoundResult.value().entrySet().stream()
+                .map(entry -> entry.getKey() + " : " + "-".repeat(entry.getValue()))
+                .toList();
+    }
+
+    /**
+     * 기본 테스트로, 제출을 위해 남겨놓기로 한다.
+     */
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
 
     @Override
     public void runMain() {
