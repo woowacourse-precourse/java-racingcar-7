f5046aca7f7b2cde3c0b3351452be0649f3bb260
jeonghyeon-ohk <ok6737@naver.com>
2024-10-27 02:03:30 +0900

    feat(domain) : 문자열을 정수로 변환하는 기능 구현
    - 입력받은 경기 횟수 문자열을 숫자로 변환하는 매서드를 구현했습니다.

--- src/main/java/racingcar/domain/ValueExtractor.java
+++ src/main/java/racingcar/domain/ValueExtractor.java
@@ -1,9 +1,15 @@
 package racingcar.domain;
 
+import racingcar.validator.NumberValidator;
+import java.util.ArrayList;
+
 public class ValueExtractor{
 
     public static String[] getDelimitedValue(String input){
-
         return input.split(",", -1);
+    }
+
+    public static int getNumericValue(String input){
+        return Integer.parseInt(input);
     }
 }
