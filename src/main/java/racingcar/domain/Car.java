cceb96e30e3c72bad688a4cbba925633a2d06de4
daeyeon <kdy4410121@naver.com>
2024-10-26 12:27:49 +0900

    feat: 사용자 입력에 따른 이름을 가지는 car 객체 생성자 생성

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class Car {
+
+    private String carName;
+    private int position;
+
+    public Car(String carName){
+        this.carName = carName;
+    }
+
+
+}
