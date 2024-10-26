bd4031a686fb78e618062c0926b6e641b2b259ec
eundeang <111877048+eundeang@users.noreply.github.com>
2024-10-27 03:45:45 +0900

    refactor(Car): 메서드 선언 순서 변경

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -14,19 +14,19 @@ public class Car {
         this.name = name;
     }
 
-    private boolean isValidateLength(String name, final int limitLength) {
-        if (name.length() > limitLength) {
-            throw new IllegalArgumentException("The name must be " + limitLength + " characters or fewer.");
-        }
-        return true;
-    }
-
     public List<Car> splitByDelimiter(final String userInput) {
         final String delimiter = ",";
         return Arrays.stream(userInput.split(delimiter))
                 .filter(name -> isValidateLength(name, 5))
                 .map(Car::new)
                 .toList();
+    }
+
+    private boolean isValidateLength(String name, final int limitLength) {
+        if (name.length() > limitLength) {
+            throw new IllegalArgumentException("The name must be " + limitLength + " characters or fewer.");
+        }
+        return true;
     }
 
     public String getName() {
