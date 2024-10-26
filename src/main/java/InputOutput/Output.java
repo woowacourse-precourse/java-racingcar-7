5dd40bede018ce0a42e69fbcbf25ea37b8da389f
seoseunghyun1025 <seunghyun.seo101@gmail.com>
2024-10-26 22:14:15 +0900

    refactor: Output 클래스 indent depth 리펙터링

--- src/main/java/InputOutput/Output.java
+++ src/main/java/InputOutput/Output.java
@@ -13,17 +13,20 @@ public class Output {
     public static void roundPrint(){
         System.out.println("시도할 횟수는 몇 회인가요?");
     }
+    private static void updateRaceAndPrintResult(Map<String, String> race){
+        for (String key : race.keySet()) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                race.put(key, race.get(key) + "-");
+            }
+            System.out.println(key + " : " + race.get(key));
+        }
+        System.out.println("");
+    }
     public static void raceResult(int count, Map<String, String> race){
         System.out.println("");
         System.out.println("실행 결과");
         for(int i = 0; i < count; i++){
-            for (String key : race.keySet()) {
-                if (Randoms.pickNumberInRange(0, 9) >= 4) {
-                    race.put(key, race.get(key) + "-");
-                }
-                System.out.println(key + " : " + race.get(key));
-            }
-            System.out.println("");
+            updateRaceAndPrintResult(race);
         }
         int max = 0;
         List<String> maxKeys = new ArrayList<>();
