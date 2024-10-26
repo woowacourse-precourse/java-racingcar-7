224197ffdab75a5bd8cfbb51060e46eb8153fb5e
Seungwan Yoo <ysw99@kakao.com>
2024-10-26 20:01:19 +0900

    feat: 자동차 이름 입력 구현 - 입력 받은 문자열 파싱 - 자동차 이름마다 Car 객체 생성 - 생성된 Car들로 Race 객체 생성 (하나의 객체로 관리 위함)

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,9 +1,18 @@
 package racingcar.controller;
 
+import racingcar.domain.Race;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class RaceController {
 
     public void run() {
+        Race race = setupRace();
+    }
 
+    private Race setupRace() {
+        OutputView.renderFirstRun();
+        return Race.setupRace(InputView.getCarNames());
     }
 
 }
--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import racingcar.util.ParseStringUtil;
+
 import java.util.List;
 
 public class Race {
@@ -12,6 +14,20 @@ public class Race {
 
     public List<Car> getRaceCars() {
         return raceCars;
+    }
+
+    public static Race setupRace(String carNames) {
+        return new Race(getCarNamesList(carNames).stream()
+                .map(Race::createCar)
+                .toList());
+    }
+
+    private static List<String> getCarNamesList(String input) {
+        return ParseStringUtil.parseCarNames(input);
+    }
+
+    private static Car createCar(String carName) {
+        return new Car(carName, 0);
     }
 
 }
