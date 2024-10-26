ebce3b9f838ed683752e459745fcb7b168b09840
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-27 07:03:12 +0900

    feat($RacingCarsUtil): RacingCars 의 values 를 설정하는 메서드 추가

--- src/test/java/racingcar/util/RacingCarsUtil.java
+++ src/test/java/racingcar/util/RacingCarsUtil.java
@@ -24,4 +24,19 @@ public class RacingCarsUtil {
             throw new RuntimeException("RacingCars 객체의 values 필드 접근에 실패했습니다.");
         }
     }
+
+    /*
+        RacingCars 객체의 values 필드에 접근하여 값을 설정한다.
+        Reflection 을 사용하여 private 필드에 접근한다.
+     */
+    public static void setValues(RacingCars racingCars, List<RacingCar> values) {
+        try {
+            Field valuesField = RacingCars.class.getDeclaredField("values");
+            valuesField.setAccessible(true);
+
+            valuesField.set(racingCars, values);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException("RacingCars 객체의 values 필드 접근에 실패했습니다.");
+        }
+    }
 }
