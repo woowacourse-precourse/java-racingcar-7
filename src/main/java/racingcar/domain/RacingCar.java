4a2aee0b16975ddfde476ad7a11243ca0323ccf3
Taekgil <taekgil273@gmail.com>
2024-10-26 10:04:59 +0900

    feat($RacingCar): RacingCar 객체 구현
    RacingCar 구현

---
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+public class RacingCar {
+
+    private final String name;
+    private int score;
+
+    public RacingCar(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
