292a5d781ff2cf757da998dce6c2891cf3af832a
unknown <qwer1234@mail.ulsan.ac.kr>
2024-10-27 02:34:04 +0900

    자동차 이름 입력 및 검증 기능 추가 - 사용자로부터 자동차 이름을 입력받는 기능 구현 - 자동차 이름 길이를 5자 이하로 제한하는 검증 추가 - 유효하지 않은 이름일 경우 IllegalArgumentException 발생

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String cars = getCarNames();
+    }
+    private static String getCarNames() {
+        System.out.println("자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+    private static void validation(String carName){
+        if(carName.length()>5) throw new IllegalArgumentException();
     }
 }
