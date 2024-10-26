f00682f074cc4d76429abc094f3fcc1afe3de992
SukJin Kim <rkathrwks@gmail.com>
2024-10-26 17:43:10 +0900

    feat(game): 게임 진행 기능 구현

--- src/main/java/racingcar/model/Game.java
+++ src/main/java/racingcar/model/Game.java
@@ -1,8 +1,13 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Game {
+    private static final int START_INCLUSIVE = 0;
+    private static final int END_INCLUSIVE = 9;
+    private static final int MOVE_THRESHOLD = 4;
+
     private final List<Car> players;
     private final int totalRound;
     private int currentRound;
@@ -12,7 +17,36 @@ public class Game {
 
         this.players = players;
         this.totalRound = Integer.parseInt(totalRound);
-        this.currentRound = 0;
+        this.currentRound = 1;
     }
 
+    public List<Car> getPlayers() {
+        return players;
+    }
+
+    public int getTotalRound() {
+        return totalRound;
+    }
+
+    public int getCurrentRound() {
+        return currentRound;
+    }
+
+    public void play() {
+        for (Car player : players) {
+            if (canMove()) {
+                player.move();
+            }
+        }
+
+        currentRound += 1;
+    }
+
+    private boolean canMove() {
+        return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE) >= MOVE_THRESHOLD;
+    }
+
+    public boolean isNotFinished() {
+        return currentRound <= totalRound;
+    }
 }
