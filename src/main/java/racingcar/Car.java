26e64f0302dc02db3ec726647176a8f429c31e1e
kellyihyeon <kellykang13@gmail.com>
2024-10-27 02:15:14 +0900

    feat: 파라미터로 전달한 거리만큼 자동차를 전진시키는 기능

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,6 +4,7 @@ public class Car {
 
     private final int NAME_MAX_LENGTH  = 5;
     private String name;
+    private int location;
 
     public Car(String name) {
         validateName(name);
@@ -14,5 +15,13 @@ public class Car {
         if (name.length() > NAME_MAX_LENGTH) {
             throw new IllegalArgumentException("이름이 5글자를 초과할 수 없어요.");
         }
+    }
+
+    public void moveForward(int distance) {
+        this.location = distance;
+    }
+
+    public int currentLocation() {
+        return this.location;
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -7,6 +7,15 @@ import org.junit.jupiter.api.Test;
 class CarTest {
 
     @Test
+    void 자동차는_앞으로_갈_수_있다() {
+        Car yh = new Car("yh");
+
+        yh.moveForward(5);
+
+        assertEquals(5, yh.currentLocation());
+    }
+
+    @Test
     void 자동차_이름이_5자를_초과하면_IllegalArgumentException이_발생한다() {
         assertThrows(IllegalArgumentException.class, () -> new Car("javaji"));
     }
