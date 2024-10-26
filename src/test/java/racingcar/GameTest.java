52fd70985c6ff113a68ca42504e44eaf62fc97ad
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-27 06:56:50 +0900

    test: Game이 시작되었을 때, 차의 움직임 확인위한 테스트 작성

---
+++ src/test/java/racingcar/GameTest.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    private Cars cars;
+
+    @BeforeEach
+    void setUp(){
+        List<Name> names = Arrays.asList(new Name("a"), new Name("bb"), new Name("ccc"));
+        cars = new Cars(names);
+    }
+
+    @DisplayName("게임 시작 되었을 때, 라운드마다 차가 움직인다.")
+    @Test
+    void Given_StartGame_When_FinishGame_Then_IsNotEqual() {
+        // Given
+        int round = 3;
+        List<Name> names = Arrays.asList(new Name("a"), new Name("bb"), new Name("ccc"));
+        List<Car> initCars = cars.getCars(names);
+        // When
+        Game.start(cars, round);
+        // Then
+        List<Car> finishCars = cars.getCars(names);
+        assertThat(initCars).isNotEqualTo(finishCars);
+    }
+}
