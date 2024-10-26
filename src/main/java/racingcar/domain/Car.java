d5f595cd1922d5b37e857d0db75fc6fd0aa3f095
mikekks <thdalsrb79@naver.com>
2024-10-26 17:11:19 +0900

    feat(Car): 자동차 전진 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -17,6 +17,17 @@ public class Car {
 		this.position = START_POSITION;
 	}
 
+	public void tryMove(Random random) {
+		boolean canMove = random.pickRandomNumber() >= MOVE_CONDITION;
+		if (canMove) {
+			move();
+		}
+	}
+
+	private void move() {
+		position += MOVE_AMOUNT;
+	}
+
 	private void validateCarName(String name){
 		if(name.length() > CAR_NAME_MAX_LENGTH){
 			throw new IllegalArgumentException("차 이름은 5글자를 초과할 수 없습니다.");
