5d9828acde1293b4964a6f1656f12f997b04529e
rootTiket <l20078829@gmail.com>
2024-10-27 00:53:28 +0900

    feat: 시작 메시지를 위한 enum 생성

---
+++ src/main/java/racingcar/View/constant/OutputMessage.java
@@ -0,0 +1,14 @@
+package racingcar.View.constant;
+
+public enum OutputMessage {
+    START_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
