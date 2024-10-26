4d489bdf1d90f339a5bb458ad694af61470ce57d
lvalentine6 <ysr1227@naver.com>
2024-10-26 09:31:06 +0900

    feat: implement car

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,31 @@
 package racingcar;
 
 public class Car {
+	private static final int TARGET_NUMBER = 4;
+
+	private final String name;
+	private int moveDistance;
+
+	private Car(String name) {
+		this.name = name;
+		this.moveDistance = 0;
+	}
+
+	public static Car from(String name) {
+		return new Car(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getMoveDistance() {
+		return moveDistance;
+	}
+
+	public void move(int randomNumber) {
+		if (TARGET_NUMBER <= randomNumber) {
+			moveDistance++;
+		}
+	}
 }
