fefae5192864b89f899e9dccf4306610de7ee85b
YugyeomGhim <rladbrua0207@gmail.com>
2024-10-27 01:58:49 +0900

    feat: 입력값 처음과 끝에 쉼표가 오는 경우 예외처리 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,9 +6,20 @@ import java.util.List;
 
 public class InputView {
 
+    private static final String DELIMITER = ",";
+
     public static List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Arrays.stream(Console.readLine().split(",")).toList();
+        String carNames = Console.readLine();
+        validateCarNames(carNames);
+
+        return Arrays.stream(carNames.split(DELIMITER)).toList();
+    }
+
+    private static void validateCarNames(String carNames) {
+        if (carNames.startsWith(DELIMITER) || carNames.endsWith(DELIMITER)) {
+            throw new IllegalArgumentException("구분자는 처음과 끝에 존재할 수 없습니다.");
+        }
     }
 
     public static int inputTryCount() {
