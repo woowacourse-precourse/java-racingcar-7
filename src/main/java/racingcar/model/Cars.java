1ef0e0f4979e62f63b112a035321a4fc65c87f7f
menzzi <minmin3927@naver.com>
2024-10-26 11:20:17 +0900

    feat(model) : Cars 구현

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,26 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Cars {
+    private static Map<String,String> cars = new HashMap<>();
+
+    public static void register(List<String> names){
+        for(String name : names){
+            cars.put(name,"");
+        }
+    }
+
+    public static Map<String,String> moveForward(){
+        for(String key:cars.keySet()){
+            int random = Randoms.pickNumberInRange(0, 9);
+            if(random>=4){
+                cars.put(key,cars.get(key)+"-");
+            }
+        }
+        return cars;
+    }
+}
