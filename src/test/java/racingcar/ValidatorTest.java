c753081c92a805580181376445aa995dace9c067
chyo1 <hyowon3856@gmail.com>
2024-10-26 17:33:22 +0900

    test : validateCarNamesLength 테스트 추가
    자동차 이름이 비어있거나, 기준보다 길 때 검증

--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -3,6 +3,8 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class ValidatorTest {
@@ -27,5 +29,21 @@ class ValidatorTest {
         assertThatThrownBy(() -> validator.validateCarNamesInput("one,two,"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("구분자는 차 이름 사이에 위치할 수 있습니다.");
+    }
+
+    @Test
+    void 자동차_이름_기준_검증() {
+        Validator validator = new Validator();
+
+        List<String> testEmptyStr = new ArrayList<>(List.of(""));
+        List<String> testLongStr = new ArrayList<>(List.of("onetwo"));
+
+        assertThatThrownBy(() -> validator.validateCarNamesLength(testEmptyStr, 5))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 비어있지 않습니다.");
+
+        assertThatThrownBy(() -> validator.validateCarNamesLength(testLongStr, 5))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 기준을 넘길 수 없습니다.");
     }
 }
