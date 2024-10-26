5cb7fc6084eb194002879dc36ec4bae5074409e6
장시영 <sysmile77@gmail.com>
2024-10-26 19:26:25 +0900

    feat(Output) : 최종 우승자 뽑기

--- src/main/java/racingcar/io/Output.java
+++ src/main/java/racingcar/io/Output.java
@@ -1,2 +1,26 @@
-package racingcar.Io;public class output {
+package racingcar.io;
+
+import java.util.ArrayList;
+
+import static racingcar.util.Racing.judgementWhoIsWin;
+
+public class Output {
+
+    public static String out(String str) {
+        return "test";
+    }
+
+    public static void printWinner() {
+
+        System.out.print("최종 우승자 : ");
+        ArrayList<String> strings = judgementWhoIsWin();
+        for (int i = 0; i < strings.size(); i++) {
+            System.out.println(strings.get(i));
+            if(strings.size()-1>i) {
+                System.out.print(", ");
+            }
+        }
+
+    }
+
 }
