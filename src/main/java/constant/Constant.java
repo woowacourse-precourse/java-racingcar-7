9b1d9d8bca4f910f6053530ca0b11431ecff7d9e
lsj0822 <lsj0822@khu.ac.kr>
2024-10-26 13:00:27 +0900

    feat(Constant): Constant 클래스 작성
    상수를 따로 관리하는 Constant 메서드를 작성하였습니다
    - 생성자를 private로 만들어 Constant를 생성할 수 없도록 설정
    - 이후에 상수가 많아지면 패키지별, 메서드별로 분리 예정

---
+++ src/main/java/constant/Constant.java
@@ -0,0 +1,13 @@
+package constant;
+
+public final class Constant {
+    public static final String CONTENT_PASRE_DELIMITER = ",";
+    public static final String RACING_CAR_INPUT_MASSEAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String RACING_TRIAL_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    public static final int RACING_CAR_NAME_LIMIT = 5;
+    public static final int RANDOM_NUMBER_MINIMUM = 0;
+    public static final int RANDOM_NUMBER_MAXIMUM = 9;
+    private Constant(){
+
+    }
+}
