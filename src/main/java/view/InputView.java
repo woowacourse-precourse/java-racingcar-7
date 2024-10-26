db8cdb4634cd493159ef28b3e50db937c18f700e
Wooseok Jang <wowseok2774@gmail.com>
2024-10-26 21:51:28 +0900

    refactor: 함수를 하나의 일만 하도록 간소화

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,30 +1,19 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
-import java.util.List;
 
 public class InputView {
 
-    public List<String> getCarNames() {
-        String carNames = Console.readLine();
-        return parseCarNames(carNames);
+    public String getCarNames() {
+        String CarNames = Console.readLine();
+        return CarNames;
+
     }
 
     public int getAttemptCount() {
         return Integer.parseInt(Console.readLine());
     }
 
-    public List<String> parseCarNames(String carNames) {
-        String[] nameArray = carNames.split(",");  // 쉼표로 나누어 배열 생성
-        List<String> carNameList = new ArrayList<>();
-
-        for (String name : nameArray) {
-            carNameList.add(name);
-        }
-
-        return carNameList;
-    }
 }
 
 
