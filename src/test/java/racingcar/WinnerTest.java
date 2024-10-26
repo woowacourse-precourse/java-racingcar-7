ecc14eae0740d2f04bb769a695bfa16f2d1bfaad
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-27 02:30:26 +0900

    test: 공동우승자와 단독우승자에 대한 테스트케이스 추가

--- src/test/java/racingcar/WinnerTest.java
+++ src/test/java/racingcar/WinnerTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 class WinnerTest {
     @DisplayName("게임이 진행되는 동안 가장 많이 움직인 차가 우승자이다.")
     @Test
-    void winnerTest() {
+    void 우승자_확인_총괄_테스트() {
         String firstname = "pobi";
         GameCar firstGameCar = new GameCar(firstname);
         String secondName = "java";
@@ -23,9 +23,57 @@ class WinnerTest {
         gameCarList.add(secondGameCar);
 
         GameCarProcess gameCarProcess = new GameCarProcess(gameCarList, 1);
-        gameCarProcess.gameProgress();
+        //gameCarProcess.gameProgress();
 
         List<String> winners = gameCarProcess.findWinner();
         assertThat(winners).hasSize(1).containsExactly("java");
+    }
+
+    @DisplayName("공동 우승자를 확인한다.")
+    @Test
+    void 공동_우승자_확인_테스트() {
+        String firstname = "pobi";
+        GameCar firstGameCar = new GameCar(firstname);
+        String secondName = "java";
+        GameCar secondGameCar = new GameCar(secondName);
+        String thirdName = "yi";
+        GameCar thirdGameCar = new GameCar(thirdName);
+        firstGameCar.move(5);
+        secondGameCar.move(5);
+        thirdGameCar.move(5);
+
+        List<GameCar> gameCarList = new ArrayList<>();
+        gameCarList.add(firstGameCar);
+        gameCarList.add(secondGameCar);
+        gameCarList.add(thirdGameCar);
+
+        GameCarProcess gameCarProcess = new GameCarProcess(gameCarList, 1);
+
+        List<String> winners = gameCarProcess.findWinner();
+        assertThat(winners).hasSize(3).containsExactly("pobi", "java", "yi");
+    }
+
+    @DisplayName("단독 우승자를 확인한다.")
+    @Test
+    void 단독_우승자_확인_테스트() {
+        String firstname = "pobi";
+        GameCar firstGameCar = new GameCar(firstname);
+        String secondName = "java";
+        GameCar secondGameCar = new GameCar(secondName);
+        String thirdName = "yi";
+        GameCar thirdGameCar = new GameCar(thirdName);
+        firstGameCar.move(2);
+        secondGameCar.move(3);
+        thirdGameCar.move(5);
+
+        List<GameCar> gameCarList = new ArrayList<>();
+        gameCarList.add(firstGameCar);
+        gameCarList.add(secondGameCar);
+        gameCarList.add(thirdGameCar);
+
+        GameCarProcess gameCarProcess = new GameCarProcess(gameCarList, 1);
+
+        List<String> winners = gameCarProcess.findWinner();
+        assertThat(winners).hasSize(1).containsExactly("yi");
     }
 }
