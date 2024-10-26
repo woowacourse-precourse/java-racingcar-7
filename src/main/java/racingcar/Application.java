a19637da63e7c65c0927669d239ad946c8076b9e
Mini <mini@MiniMac.local>
2024-10-26 17:30:28 +0900

    refactor: remove car count input

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,15 +7,11 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("자동차의 수를 입력해주세요");
-        int countCars = Integer.parseInt(Console.readLine());
 
         System.out.println("자동차의 이름을 쉼표(,)로 구분해 입력해주세요 (5자 이하)");
         String[] carNames = (Console.readLine()).split(",");
 
-        if (carNames.length != countCars) {
-            throw new IllegalArgumentException("자동차의 수와 이름의 개수가 일치하지 않습니다");
-        }
+        int countCars = carNames.length;
 
         Car[] cars = new Car[countCars];
 
@@ -30,6 +26,7 @@ public class Application {
 
         System.out.println("이동횟수를 정수로 입력해주세요");
         int moveCount;
+        
         try {
             moveCount = Integer.parseInt(Console.readLine());
 
@@ -53,6 +50,7 @@ public class Application {
                 System.out.println(cars[c].getPosition());
             }
         }
+
         String winner = "";
         int maxPosition = 0;
 
@@ -68,7 +66,7 @@ public class Application {
             }
         }
 
-        System.out.println("우승자는 " + winner + " 입니다");
+        System.out.println("최종 우승자 : " + winner);
 
 
 
