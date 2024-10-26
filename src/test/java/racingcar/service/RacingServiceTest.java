b95067f56b47f6506c7ed42402161c3ac18b73da
msk226 <kminseok5167@gmail.com>
2024-10-26 14:12:57 +0900

    test(service): RacingService 테스트 코드 작성
    - 자동차 이름이 정상적으로 입력된 경우
    - 자동차 이름이 비어있는 경우
    - 동일한 이름이 중복된 경우
    - 다섯자 이상의 이름이 입력된 경우
    - 시도 횟수가 0인 경우

---
+++ src/test/java/racingcar/service/RacingServiceTest.java
@@ -0,0 +1,75 @@
+package racingcar.service;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RacingServiceTest extends NsTest {
+
+    private RacingGameService racingGameService;
+
+    @BeforeEach
+    void setUp() {
+        racingGameService = new RacingGameService();
+    }
+    @Test
+    void 자동차_이름이_정상적으로_입력된_경우_1(){
+        String input = "pobi,woni";
+        Integer tryCount = 3;
+        Assertions.assertDoesNotThrow(() -> racingGameService.startRace(carNames, tryCount));
+    }
+
+    @Test
+    void 자동차_이름이_정상적으로_입력된_경우_2(){
+        String input = "pobi, woni";
+        Integer tryCount = 3;
+        Assertions.assertDoesNotThrow(() -> racingGameService.startRace(carNames, tryCount));
+    }
+    @Test
+    void 자동차_이름이_비어_있는_입력된_경우(){
+        String input = "";
+        Integer tryCount = 3;
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> racingGameService.startRace(input, tryCount))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 동일한_이름이_중복되어_입력된_경우(){
+        String input = "pobi,pobi";
+        Integer tryCount = 3;
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> racingGameService.startRace(input, tryCount))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 다섯자_이상의_자동차_이름이_입력된_경우(){
+        String input = "abceda,pobi";
+        Integer tryCount = 3;
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> racingGameService.startRace(input, tryCount))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 시도_횟수가_0인_경우(){
+        String input = "pobi,wani";
+        Integer tryCount = 0;
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> racingGameService.startRace(input, tryCount))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    protected void runMain() {
+
+    }
+}
