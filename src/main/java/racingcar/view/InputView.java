f69d03642a77433dcdfec427ff6a740487ab5e97
taek2222 <oot309@daum.net>
2024-10-26 23:35:24 +0900

    style(InputView): 매직넘버, 문자열 상수로 정의
    - 하드 코딩된, 코드를 상수로 분리했습니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,8 +6,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_PLAY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String COMMA = ",";
+    private static final int MIN_PLAY_COUNT = 1;
+    private static final int MAX_PLAY_COUNT = 1000000;
+
     public String inputCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
         String input = Console.readLine();
 
         validateNotBlank(input);
@@ -17,7 +23,7 @@ public class InputView {
     }
 
     public int inputPlayCount() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(INPUT_PLAY_COUNT_MESSAGE);
         String input = Console.readLine();
         int inputNumber = Integer.parseInt(input);
 
@@ -28,13 +34,13 @@ public class InputView {
     }
 
     private void validateSingleComma(String input) {
-        if (input.equals(",")) {
+        if (input.equals(COMMA)) {
             throw new IllegalArgumentException();
         }
     }
 
     private void validatePlayCount(int input) {
-        if (input < 1 || input > 1000000)
+        if (input < MIN_PLAY_COUNT || input > MAX_PLAY_COUNT)
             throw new IllegalArgumentException();
     }
 }
