ed4bddd39793c321377d924b5db256caf04e686c
cyunlee <siyunkr@gmail.com>
2024-10-27 03:38:40 +0900

    feat(view): 구분자(,)를 기준으로 플레이어 이름 파싱 기능 구현

--- src/main/java/racingcar/constant/Constant.java
+++ src/main/java/racingcar/constant/Constant.java
@@ -4,6 +4,7 @@ public class Constant {
     public static final String INPUT_PLAYERS_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String INPUT_TRIAL_PROMPT = "시도할 횟수는 몇 회인가요?";
     public static final String WINNER_PROMPT = "최종 우승자 : ";
+    public static final String DELIMITER = ",";
     public static final int MAX_CAR_NAME = 5;
     public static final int NUMBER_FOR_MOVE = 4;
     public static final String STEP_UNIT = "-";
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,13 +1,31 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 import racingcar.constant.Constant;
+import java.util.ArrayList;
 
 public class InputView {
+    private String players;
+    private int trial;
+
     public void inputGameInfo() {
         System.out.println(Constant.INPUT_PLAYERS_PROMPT);
-        String players = Console.readLine();
+        players = Console.readLine();
         System.out.println(Constant.INPUT_TRIAL_PROMPT);
-        int trial = Integer.parseInt(Console.readLine());
+        trial = Integer.parseInt(Console.readLine());
+    }
+
+    private ArrayList<String> parseCarNames(String inputStr){
+        ArrayList<String> carNames = new ArrayList<String>(List.of(inputStr.split(Constant.DELIMITER)));
+        return carNames;
+    }
+
+    public ArrayList<String> getPlayers(){
+        return parseCarNames(players);
+    }
+
+    public int getTrial(){
+        return trial;
     }
 }
