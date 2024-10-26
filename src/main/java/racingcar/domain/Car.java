0b8be9110e54da31c415aa5f461fd399c07e50d5
leeseongsil <leegwichan@naver.com>
2024-10-26 22:20:36 +0900

    feat(Car): 자동차는 전진 또는 멈추는 기능
    - 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우 전진
    
    (cherry picked from commit 5607d14bf5688ecdce5da016fccbc1e891e2901f)

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,16 +3,38 @@ package racingcar.domain;
 
 public class Car {
 
+    private static final int INITIAL_POSITION = 0;
+    private static final int MOVE_FORWARD_BOUND = 4;
+
+    private final NumberSupplier numberSupplier;
     private final String name;
+    private int position;
+
+    public Car(String name, NumberSupplier numberSupplier) {
+        validate(name);
+
+        this.numberSupplier = numberSupplier;
+        this.name = name;
+        this.position = INITIAL_POSITION;
+    }
 
     public Car(String name) {
-        validate(name);
-        this.name = name;
+        this(name, new RandomNumberSupplier());
     }
 
     private void validate(String name) {
         if (name == null || name.isBlank() || name.length() > 5) {
             throw new IllegalArgumentException("이름은 1글자 이상 5글자 이하이어야 합니다");
         }
+    }
+
+    public void move() {
+        if (numberSupplier.supply() >= MOVE_FORWARD_BOUND) {
+            position++;
+        }
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.params.ParameterizedTest;
@@ -7,6 +8,8 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
+
+    private static final String DEFAULT_NAME = "name";
 
     @ParameterizedTest
     @NullAndEmptySource
@@ -23,5 +26,29 @@ class CarTest {
         assertThatThrownBy(() -> new Car(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("이름은 1글자 이상 5글자 이하이어야 합니다");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 9})
+    void moveTest_whenSuppliedNumberIsOverBound_moveForward(int overBoundNumber) {
+        NumberSupplier overBoundSupplier = () -> overBoundNumber;
+        Car car = new Car(DEFAULT_NAME, overBoundSupplier);
+        int expectedPosition = car.getPosition() + 1;
+
+        car.move();
+
+        assertThat(car.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 3})
+    void moveTest_whenSuppliedNumberIsUnderBound_stop(int underBoundNumber) {
+        NumberSupplier overBoundSupplier = () -> underBoundNumber;
+        Car car = new Car(DEFAULT_NAME, overBoundSupplier);
+        int expectedPosition = car.getPosition();
+
+        car.move();
+
+        assertThat(car.getPosition()).isEqualTo(expectedPosition);
     }
 }
