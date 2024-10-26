3193a89387733f6f81b3b693dfe589acc3fed84a
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-27 02:17:55 +0900

    feat(ErrorType): 자동차 이름 중복 메세지 정의

--- src/main/java/racingcar/constant/ErrorType.java
+++ src/main/java/racingcar/constant/ErrorType.java
@@ -10,8 +10,9 @@ public class ErrorType {
             PREFIX + String.format("자동차의 대수는 %d이하여야 합니다.", Rule.CAR_MAX_SIZE);
     public static final String EXCEEDED_MAX_COUNT = PREFIX + String.format("시도 횟수는 %d이하여야 합니다.", Rule.COUNT_MAX);
     public static final String INVALID_COUNT = PREFIX + "시도 횟수는 숫자여야 합니다.";
+    public static final String DUPLICATE_CAR_NAME = PREFIX + "자동차 이름은 중복될 수 없습니다.";
 
     private ErrorType() {
-        
+
     }
 }
