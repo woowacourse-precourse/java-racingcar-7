4b12b23f32255b330dc4a2fe1e9b50a3dfff6c16
seahwanKim <kimkimseasea@gmail.com>
2024-10-26 12:35:30 +0900

    feat: RaceController 클래스를 생성하고 시작 기능을 추가한다

---
+++ src/main/java/racingcar/controller/RaceController.java
@@ -0,0 +1,30 @@
+package racingcar.controller;
+
+import racingcar.domain.CarRacing;
+
+import static racingcar.controller.InputController.*;
+import static racingcar.view.OutputView.*;
+
+public class RaceController {
+
+    private CarRacing carRacing;
+    private int round;
+
+    public void start() {
+        set();
+        play();
+    }
+
+    private void set() {
+        carRacing = setCars();
+        round = setRound();
+    }
+
+    private void play() {
+        resultMessage();
+        for (int i = 0; i < round; i++) {
+            carRacing.go();
+            roundResult(carRacing);
+        }
+    }
+}
