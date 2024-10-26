8d850ee91ad00fbe7cfe315aa8cab817a5ded8f7
Leegeonwoo7 <workgeon07@naver.com>
2024-10-26 20:23:34 +0900

    feat(Winner): Winner클래스 구현
    addWinner()메서드를 통하여 우승자 목록을 추가한다
    외부에서는 getWinners()메서드를 통하여 우승자 목록을 조회한다

--- src/main/java/racingcar/model/Winner.java
+++ src/main/java/racingcar/model/Winner.java
@@ -1,10 +1,15 @@
 package racingcar.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Winner {
 
     private List<Car> winners;
+
+    public Winner() {
+        winners = new ArrayList<>();
+    }
 
     public Winner(List<Car> winners) {
         this.winners = winners;
@@ -12,5 +17,21 @@ public class Winner {
 
     public List<Car> getWinners() {
         return winners;
+    }
+
+    public void addWinner(List<Car> cars) {
+        int highestPoint = 0;
+
+        for (Car car : cars) {
+            if (car.getWinningPoint() > highestPoint) {
+                highestPoint = car.getWinningPoint();
+            }
+        }
+
+        for (Car car : cars) {
+            if (car.getWinningPoint() == highestPoint) {
+                winners.add(car);
+            }
+        }
     }
 }
