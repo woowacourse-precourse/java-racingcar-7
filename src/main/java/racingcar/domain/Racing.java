9308d7b81c6e6fc1783ab00f34696cde1612a313
himitery <himitery@gmail.com>
2024-10-26 15:53:38 +0900

    feat(racing): 레이싱 실행 및 기록 기능 추가
    - 레이싱 라운드 실행
    - 레이싱 기록 추가 및 관리
    - 레이싱 우승자 찾기

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -1,18 +1,55 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
-import jdk.jshell.spi.ExecutionControl.NotImplementedException;
+import java.util.stream.Collectors;
 
 public class Racing {
 
-    private Racing() {
+    private final List<Car> cars;
+    private final List<String> history = new ArrayList<>();
+
+    private Racing(List<Car> cars) {
+        this.cars = cars;
     }
 
-    public static Racing of() throws NotImplementedException {
-        throw new NotImplementedException("아직 구현되지 않았습니다.");
+    public static Racing of(List<Car> cars) {
+        return new Racing(cars);
     }
 
-    public List<Car> getWinners() throws NotImplementedException {
-        throw new NotImplementedException("아직 구현되지 않았습니다.");
+    public void start() {
+        runRound();
+        recordHistory();
+    }
+
+    public void printHistory() {
+        System.out.println(String.join("\n\n", history));
+    }
+
+    public List<Car> getWinners() {
+        int longDistance = findLongDistance();
+
+        return cars.stream()
+                .filter(it -> it.getDistance() == longDistance)
+                .collect(Collectors.toList());
+    }
+
+    private void runRound() {
+        cars.forEach(Car::moveForward);
+    }
+
+    private void recordHistory() {
+        String roundHistory = cars.stream()
+                .map(Car::toString)
+                .collect(Collectors.joining("\n"));
+
+        history.add(roundHistory);
+    }
+
+    private int findLongDistance() {
+        return cars.stream()
+                .map(Car::getDistance)
+                .max(Integer::compare)
+                .orElse(0);
     }
 }
