9de33b2cbd0f956e58bd7c0e572086933993eea4
YugyeomGhim <rladbrua0207@gmail.com>
2024-10-26 22:07:22 +0900

    test: 랜덤 숫자 생성 테스트코드 작성

---
+++ src/test/java/racingcar/utils/RandomNumberGeneratorTest.java
@@ -0,0 +1,20 @@
+package racingcar.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RandomNumberGeneratorTest {
+
+    @Test
+    public void 여러번_테스트하여_랜덤_숫자가_0부터_9_사이에_있는지_확인() {
+        // given
+        int testCount = 1000;
+
+        // when & then
+        for (int i = 0; i < testCount; i++) {
+            int randomNumber = RandomNumberGenerator.generate();
+            assertTrue(randomNumber >= 0 && randomNumber < 10);
+        }
+    }
+}
