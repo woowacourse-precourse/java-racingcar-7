f18bc8a21711a3da275364fc0d8c887251a7f473
daeyeon <kdy4410121@naver.com>
2024-10-26 12:57:04 +0900

    fix: default로 구현한 함수 abstarct로 바꾸어줌

--- src/main/java/random/RandomGenerate.java
+++ src/main/java/random/RandomGenerate.java
@@ -1,16 +1,9 @@
 package random;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 public interface RandomGenerate {
 
     public final int minNumber = 0;
     public final static int maxNumber = 9;
-    default int generateRandomNumber(){
-        int randomNumber = Randoms.pickNumberInRange(minNumber, maxNumber);
-
-        return randomNumber;
-    }
-
+    public abstract int generateRandomNumber();
 
 }
