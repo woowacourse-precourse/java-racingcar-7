6f2ca8cdbc4a2f7e7a1f37d7b8c8373094acdcbf
daeyeon <kdy4410121@naver.com>
2024-10-26 14:07:31 +0900

    feat: car객체 모아둔 cars객체 생성

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+import random.RandomGenerate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private final int startIndex = 0;
+    private List<Car> carList;
+    private RandomGenerate randomGenerate;
+    private final int maxCarNumber = 5;
+
+    public Cars(List<String> inputNames, RandomGenerate randomGenerate){
+        this.randomGenerate = randomGenerate;
+        Car[] carArray = new Car[maxCarNumber];
+
+        for(int i = startIndex; i < inputNames.size(); i++){
+            Car car = new Car(inputNames.get(i), randomGenerate);
+            carArray[i] = car;
+        }
+
+        carList = List.of(carArray);
+    }
+
+}
