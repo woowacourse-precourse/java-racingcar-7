e948d34494750b8459f1300b28af370edc1d6ea3
minjae k <rkdalswoals@gmail.com>
2024-10-27 00:38:39 +0900

    feat: Add ObjectFactory for dependency injection

---
+++ src/main/java/racingcar/config/ObjectFactory.java
@@ -0,0 +1,23 @@
+package racingcar.config;
+
+import racingcar.controller.GameController;
+import racingcar.domain.GameBoard;
+import racingcar.domain.RaceJudge;
+import racingcar.service.GameService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+//Application -> GameController -> (View, GameService -> Gameboard, Racejudge)
+public class ObjectFactory {
+    public static GameController createGameController() {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        GameBoard gameBoard = new GameBoard();
+        RaceJudge raceJudge = new RaceJudge();
+
+        GameService gameService = new GameService(gameBoard, raceJudge);
+
+        return new GameController(inputView, outputView, gameService);
+    }
+}
