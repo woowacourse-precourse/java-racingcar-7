133d7002260ee4e2937955c8aa67977413586b75
username0w <grandcias123@gmail.com>
2024-10-27 04:37:16 +0900

    feat: 라운드 결과와 우승자 출력을 처리하는 InputView 클래스 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+public class OutputView {
+
+    public void printRoundResults () {
+        System.out.println("실행 결과");
+    }
+
+    public void printWinner() {
+        System.out.println("최종 우승자 : ");
+    }
+}
