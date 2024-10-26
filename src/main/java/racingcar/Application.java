e84f79ba331b6b77c8182323af36643cfb193f1b
JeongYeonSeung <asas4654@daum.net>
2024-10-26 23:12:02 +0900

    feat: implement main function to execute racing car game

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        List<String> carNames = inputView.readCarNames();
+        int number = inputView.readNumber();
+
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        Racing racing = new Racing();
+        OutputView outputView = new OutputView();
+        for (int i = 0; i < number; i++) {
+            racing.runRace(cars);
+            outputView.printRoundStatus(cars);
+        }
+
+        outputView.printFinalWinners(racing.getWinners(cars));
     }
 }
