a922bc4f9e20dc3a3e1b5aa1f06c1a284576e4d9
yyysolhhh <solbaram37@gmail.com>
2024-10-27 00:36:49 +0900

    feat(Output): printRoundResult, printWinners 메서드 추가
    printRoundResult
    - 매 회 결과 형식에 맞춰 출력
    
    printWinners
    - 최종 우승자 이름 형식에 맞춰 출력
    
    printRoundResultMessage
    - 중간 결과 출력 전 메시지 출력

--- src/main/java/racingcar/io/Output.java
+++ src/main/java/racingcar/io/Output.java
@@ -1,24 +1,37 @@
 package racingcar.io;
 
+import java.util.List;
+import racingcar.Car;
+
 public class Output {
     private static final String RACE_RESULT = "실행 결과";
     private static final String WINNER_PREFIX = "최종 우승자 : ";
     private static final String WINNER_DELIMITER = ", ";
+    private static final String NAME_POSITION_DELIMITER = " : ";
+    private static final String NEW_LINE = "\n";
 
     public static void printMessage(String message) {
         System.out.println(message);
     }
 
-//    public static void printResult() {
-//        StringBuilder resultOutput = new StringBuilder(RACE_RESULT);
-//
-//    }
+    public static void printRoundResultMessage() {
+        printMessage(RACE_RESULT);
+    }
 
-    public static void printWinner(String[] winnerNames) {
+    public static void printRoundResult(List<Car> cars) {
+//        StringBuilder roundResult = new StringBuilder(RACE_RESULT);
+        StringBuilder roundResult = new StringBuilder();
+        for (Car car : cars) {
+            roundResult.append(car.getName() + NAME_POSITION_DELIMITER + car.getPositionAsString() + NEW_LINE);
+        }
+        printMessage(roundResult.toString());
+    }
+
+    public static void printWinners(List<String> winnerNames) {
         StringBuilder winnersOutput = new StringBuilder(WINNER_PREFIX);
-        for (int i = 0; i < winnerNames.length; i++) {
-            winnersOutput.append(winnerNames[i]);
-            if (i != winnerNames.length - 1) {
+        for (int i = 0; i < winnerNames.size(); i++) {
+            winnersOutput.append(winnerNames.get(i));
+            if (i != winnerNames.size() - 1) {
                 winnersOutput.append(WINNER_DELIMITER);
             }
         }
