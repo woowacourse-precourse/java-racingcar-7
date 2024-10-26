6e171de06eace416ba530b5ea77759809691d9ad
Devheun <dudwo5600@korea.ac.kr>
2024-10-26 23:55:09 +0900

    feat: Separate the implementation logic into several methods.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import java.util.LinkedHashMap;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LinkedHashMap<String, StringBuffer> cars = InputHandler.getNames();
+        int raceCount = InputHandler.getNumber();
+        Race.startRace(cars, raceCount);
     }
 }
