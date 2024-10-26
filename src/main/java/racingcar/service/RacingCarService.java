085cef1383a37f937779f850429b460e6f89b122
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-26 21:12:13 +0900

    feat(RacingCarService): 서비스 레이어 구현
    메서드들의 기능은 다음과 같습니다:
    - 자동차들을 생성한다
    - 시도횟수를 생성한다
    - 경주 결과를 가져온다
    - 우승자를 가져온다

---
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -0,0 +1,81 @@
+package racingcar.service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import racingcar.constant.Rule;
+import racingcar.domain.Car;
+import racingcar.domain.CarName;
+import racingcar.domain.Count;
+import racingcar.domain.Racing;
+import racingcar.domain.Score;
+import racingcar.dto.RacingCarProgress;
+import racingcar.dto.RacingCarResult;
+import racingcar.dto.RacingCarWinner;
+import racingcar.util.ListValidator;
+import racingcar.util.NumberGenerator;
+import racingcar.util.NumberValidator;
+import racingcar.util.StringValidator;
+
+public class RacingCarService {
+
+    private final ListValidator<String> listValidator;
+    private final NumberGenerator numberGenerator;
+    private final NumberValidator<Integer> numberValidator;
+    private final StringValidator stringValidator;
+
+    public RacingCarService(final ListValidator<String> listValidator, final NumberGenerator numberGenerator,
+                            final NumberValidator<Integer> numberValidator, final StringValidator stringValidator) {
+        this.listValidator = listValidator;
+        this.numberGenerator = numberGenerator;
+        this.numberValidator = numberValidator;
+        this.stringValidator = stringValidator;
+    }
+
+    public List<Car> generateCars(final String values) {
+        final List<String> carNames = parseToList(values);
+        listValidator.validateSize(carNames, Rule.CAR_MAX_SIZE);
+        listValidator.validateDuplicate(carNames);
+        return carNames.stream()
+                .map(this::createCar)
+                .toList();
+    }
+
+    public Count<Integer> generateCount(final String value) {
+        return Count.of(value, numberValidator);
+    }
+
+    public List<RacingCarResult> getRacingCarResult(final Racing racing, final Count<Integer> count) {
+        final List<RacingCarResult> racingCarResult = new ArrayList<>();
+        final Integer value = count.getValue();
+        for (int turn = 0; turn < value; turn++) {
+            final List<Car> cars = racing.move();
+            racingCarResult.add(RacingCarResult.of(mapToRacingCarProgresses(cars)));
+        }
+        return racingCarResult;
+    }
+
+    public RacingCarWinner getRacingCarWinner(final Racing racing) {
+        final List<Car> winners = racing.getWinners();
+        return RacingCarWinner.of(winners);
+    }
+
+    private List<RacingCarProgress> mapToRacingCarProgresses(final List<Car> cars) {
+        return cars.stream()
+                .map(RacingCarProgress::of)
+                .toList();
+    }
+
+
+    private Car createCar(final String value) {
+        final CarName carName = CarName.of(value, stringValidator);
+        return new Car(carName, new Score(0), numberGenerator);
+    }
+
+    private List<String> parseToList(final String values) {
+        final String[] carNames = values.split(",");
+        return Arrays.stream(carNames)
+                .toList();
+    }
+
+}
