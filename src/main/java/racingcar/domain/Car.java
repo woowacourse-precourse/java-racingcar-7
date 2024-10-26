4844ecb14e99e4ee457f6dd8cd262aaa27c40093
mikekks <thdalsrb79@naver.com>
2024-10-26 17:10:46 +0900

    feat(Car): 자동차 이름, 위치를 저장할 클래스 정의

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,26 @@
+package racingcar.domain;
+
+import racingcar.common.Random;
+
+public class Car {
+	private static final int START_POSITION = 0;
+	private static final int MOVE_CONDITION = 4;
+	private static final int MOVE_AMOUNT = 1;
+	private static final int CAR_NAME_MAX_LENGTH = 5;
+
+	private final String name;
+	private int position;
+
+	public Car(String name) {
+		validateCarName(name);
+		this.name = name;
+		this.position = START_POSITION;
+	}
+
+	private void validateCarName(String name){
+		if(name.length() > CAR_NAME_MAX_LENGTH){
+			throw new IllegalArgumentException("차 이름은 5글자를 초과할 수 없습니다.");
+		}
+	}
+
+}
