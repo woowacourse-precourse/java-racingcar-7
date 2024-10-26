282d805d40f376a633f4fe24520cc09e541f0130
seoseunghyun1025 <seunghyun.seo101@gmail.com>
2024-10-26 22:03:00 +0900

    feat: race 시작 전 map 초기화 및 count 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,17 @@
 package racingcar;
 
+import InputOutput.Input;
 import InputOutput.Output;
+import race.Race;
+import java.util.Map;
 
 public class Application {
 
     public static void main(String[] args) {
         Output.firstPrintMessage();
+        Map<String, String> race;
+        race = Race.insertEnteredCarNamesIntoMap();
         Output.roundPrint();
+        int count = Input.numberOfRounds();
     }
 }
