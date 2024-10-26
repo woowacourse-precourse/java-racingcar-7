3ab190d27bc73d81715376dd004113e0ad4c8e7a
chyo1 <hyowon3856@gmail.com>
2024-10-26 23:45:38 +0900

    test : car 객체 생성 BeforeEach로 변경

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,25 +2,29 @@ package racingcar;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
+    Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("test");
+    }
 
     @Test
     void 이동_횟수_확인() {
-        Car car = new Car("test");
         assertThat(car.getMoveCount()).isEqualTo(0);
     }
 
     @Test
     void 자동차_이름_확인() {
-        Car car = new Car("test");
         assertThat(car.getName()).isEqualTo("test");
     }
 
     @Test
     void 자동차_이동() {
-        Car car = new Car("test");
 
         for (int i = 0; i < 5; i++) {
             car.move();
