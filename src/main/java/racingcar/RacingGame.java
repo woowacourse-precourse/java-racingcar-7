1440264026102a3ee012e153e605dbe5e65ecfe1
navangs <43688529+insertend23@users.noreply.github.com>
2024-10-27 01:40:06 +0900

    Feat: make RacingGame class

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+public class RacingGame {
+    private final RacingInputView inputView;
+    private final RacingOutputView outputView;
+
+    RacingGame(RacingInputView inputView, RacingOutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    void play() {
+        String carNamesInput = inputView.getCarNamesInput();
+        CompeteCars competeCars = new CompeteCars(carNamesInput);
+
+        String moveCount = inputView.getMoveCount();
+        Round round = new Round(moveCount, competeCars.getAll());
+        round.progress(competeCars.getAll());
+
+        outputView.printRoundResult(round);
+        outputView.finalWinners(round);
+    }
+}
