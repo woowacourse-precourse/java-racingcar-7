f25b6ad8131496455e98060cd239fcc07f5c8460
soeunnPark <carol0724@naver.com>
2024-10-26 22:22:31 +0900

    refactor(Car): Remove unnecessary constant
    불필요한 상수 선언을 삭제했습니다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -7,7 +7,6 @@ public class Car {
 
     private static final int CAR_POSITION_INIT = 0;
     private static final int CAR_POSITION_MAX_CRITERIA = Integer.MAX_VALUE - 2;
-    private static final int MIN_RANDOM_NUMBER_TO_FORWARD = 4;
 
     private final String carName;
     private int carPosition;
