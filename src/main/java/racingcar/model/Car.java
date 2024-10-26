828b5038653bb7b8055c0f178f66072bc4d66f52
Koo-EunSung <ku903@naver.com>
2024-10-26 18:57:12 +0900

    Feat: Car 객체 생성

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class Car {
+    private String name;
+    private int step;
+
+    public Car(String name) {
+        this.name = name;
+        this.step = 0;
+    }
+}
