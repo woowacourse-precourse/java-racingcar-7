ba2f7bc35c142fd8a448d84defffbef2b3b91ca8
jonghwanlee <kavae550@gmail.com>
2024-10-26 18:18:36 +0900

    feat(domain): MovementDecider 클래스 기능 구현
    - 무작위 숫자 생성 후 숫자에 따라 전진 여부를 boolean값으로 리턴하는 checkMoveOrNot() 메서드 생성

--- src/main/java/racingcar/domain/MovementDecider.java
+++ src/main/java/racingcar/domain/MovementDecider.java
@@ -1,4 +1,18 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class MovementDecider {
+
+    public static boolean checkMoveOrNot(){
+
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+
+        if (randomNumber >= 4) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
