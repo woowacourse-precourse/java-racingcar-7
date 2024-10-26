5cdd68a3ab9e23ad89f071311042d298c5662aa1
rootTiket <l20078829@gmail.com>
2024-10-27 00:39:14 +0900

    test<ParserTest>: 다섯글자 넘는 자동차명이 있을경우 예외처리

--- src/test/java/racingcar/domain/ParserTest.java
+++ src/test/java/racingcar/domain/ParserTest.java
@@ -1,6 +1,8 @@
 package racingcar.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static racingcar.View.constant.ErrorMessage.TOO_LONG_VALUE;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -13,5 +15,15 @@ class ParserTest {
         String input = "자동차1, 자동차2, 자동차3";
         List<String> expected = List.of("자동차1", "자동차2", "자동차3");
         assertEquals(expected, parser.parse(input));
+    }
+
+    @Test
+    void 다섯글자_넘을경우_예외처리() {
+        String input = "자동차1,다섯글자넘어가는자동차, 자동차3";
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> parser.parse(input)
+        );
+        assertEquals(TOO_LONG_VALUE.getMessage(), exception.getMessage());
     }
 }
