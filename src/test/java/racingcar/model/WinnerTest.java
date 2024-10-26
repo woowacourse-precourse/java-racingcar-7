1679be8f2c0ffe175d6ede9cf1d91f5681d5fb97
Leegeonwoo7 <workgeon07@naver.com>
2024-10-26 20:25:06 +0900

    test(WinnerTest): Winner테스트 구현
    addWinner()메서드 테스트

---
+++ src/test/java/racingcar/model/WinnerTest.java
@@ -0,0 +1,38 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+class WinnerTest {
+
+    @Test
+    @DisplayName("test")
+    void test() {
+        //given
+        Car pobi = new Car("pobi");
+        Car woni = new Car("woni");
+        Car jun = new Car("jun");
+
+        pobi.moveForward();
+        pobi.moveForward();
+        woni.moveForward();
+        jun.moveForward();
+
+        List<Car> cars = new ArrayList<>(List.of(pobi, woni, jun));
+        Winner winner = new Winner();
+
+        //when
+        winner.addWinner(cars);
+        List<Car> winners = winner.getWinners();
+
+        //then
+        assertThat(winners).hasSize(1)
+                .containsExactlyInAnyOrder(pobi);
+    }
+}
