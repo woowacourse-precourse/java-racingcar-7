aab4996bb85da549437779136f9b1f8c168c5474
unknown <qwer1234@mail.ulsan.ac.kr>
2024-10-27 02:40:35 +0900

    시도 횟수 입력 처리 추가 - getTries() 메서드를 통해 사용자로 부터 시도할 횟수를 입력 받음 - 입력된 횟수를 정수로 변환하여 반환

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String cars = getCarNames();
+        int tries = getTries();
     }
     private static String getCarNames() {
         System.out.println("자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -13,5 +14,10 @@ public class Application {
     }
     private static void validation(String carName){
         if(carName.length()>5) throw new IllegalArgumentException();
+    }
+    private static int getTries(){
+        System.out.println("시도할 횟수는 몇 회 인가요?");
+        String triesInput = Console.readLine();
+        return Integer.parseInt(triesInput);
     }
 }
