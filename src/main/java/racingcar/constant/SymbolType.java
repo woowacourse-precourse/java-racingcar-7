72f298c443eec934cfe6d623c32155c7c52e82ae
wogus <ind07152@naver.com>
2024-10-26 16:21:51 +0900

    feat(SymbolType): 기호 상수화
    기본 기호들을 상수화한다.

---
+++ src/main/java/racingcar/constant/SymbolType.java
@@ -0,0 +1,17 @@
+package racingcar.constant;
+
+public enum SymbolType {
+    RESULT_POSITION("-"),
+    RESULT_TOKEN(" : "),
+    CAR_NAME_DELIMITER(","),
+    RESULT_DELIMITER(", ");
+    private final String symbol;
+
+    SymbolType(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+}
