44687c3cb7d7f478c27c5b6649e5c10946b90e2a
pakaheo <k70923@konkuk.ac.kr>
2024-10-26 20:27:14 +0900

    test : Create Car constant
    create Car constant CAR_POBI, CAR_NANA, CAR_JUN to use in CarsTest

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -6,10 +6,33 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class CarsTest {
+
+    private final Car CAR_POBI = new Car("pobi", 3);
+    private final Car CAR_NANA = new Car("nana", 1);
+    private final Car CAR_JUN = new Car("jun", 3);
+
     @Test
     void Cars_객체_생성() {
-        List<Car> carList = List.of(new Car("pobi"), new Car("pobi"));
+        List<Car> carList = List.of(new Car("pobi"), new Car("nana"));
         Cars cars = new Cars(carList);
         assertThat(cars).isEqualTo(new Cars(carList));
+    }
+
+    @Test
+    void 우승자_한_명_찾기() {
+        List<Car> carList = List.of(CAR_POBI, CAR_NANA);
+        Cars cars = new Cars(carList);
+
+        List<Car> winners = List.of(CAR_POBI);
+        assertThat(cars.findWinners()).isEqualTo(winners);
+    }
+
+    @Test
+    void 우승자_여러_명_찾기() {
+        List<Car> carList = List.of(CAR_POBI, CAR_NANA, CAR_JUN);
+        Cars cars = new Cars(carList);
+
+        List<Car> winners = List.of(CAR_POBI, CAR_JUN);
+        assertThat(cars.findWinners()).isEqualTo(winners);
     }
 }
