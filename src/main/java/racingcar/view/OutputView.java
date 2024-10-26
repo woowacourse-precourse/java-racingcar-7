a748f62fefb3b4abb1af8aca5b7706a7c521942a
zjhj0814 <zjhj0814@gmail.com>
2024-10-27 02:51:31 +0900

    feat(OutputView): 우승자 안내 문구 출력 기능 작성
    우승자 이름을 리스트로 받아서 separator ', '로 구분한 후 정해진 형식에 맞춰 출력한다

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,6 +7,8 @@ public class OutputView {
     private static final String RESULT_MESSAGE = "실행 결과";
     private static final String ATTEMPT_RESULT_MESSAGE_SEPARATOR = " : ";
     private static final String ATTEMPT_RESULT_MESSAGE_ADVANCE_INDICATOR = "-";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String WINNER_FORMAT_SEPARATOR = ", ";
 
     public void printResult() {
         System.out.println(RESULT_MESSAGE);
@@ -21,5 +23,13 @@ public class OutputView {
     private String getAttemptResult(RacingCar car) {
         return car.getCarName() + ATTEMPT_RESULT_MESSAGE_SEPARATOR
                 + ATTEMPT_RESULT_MESSAGE_ADVANCE_INDICATOR.repeat(car.getAdvanceRecord());
+    }
+
+    public void printWinner(List<String> winners) {
+        System.out.println(WINNER_MESSAGE + getWinnerFormat(winners));
+    }
+
+    private String getWinnerFormat(List<String> winners) {
+        return String.join(WINNER_FORMAT_SEPARATOR, winners);
     }
 }
