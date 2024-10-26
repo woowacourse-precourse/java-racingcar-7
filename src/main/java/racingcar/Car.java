03bfce42609422c69b0ca28cd98a75556bc70414
yumi <yumkoori@gmail.com>
2024-10-26 16:56:22 +0900

    Feat: 자동차에 전진 기능 추가
    - printMovingResult: 추가 예정인 정지 기능에서도 재사용 예정이기 때문에 메서드 분리

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class Car {
+    private int forwardNumber = 0;
+    private String carName;
+    
+    public void movingForward() {
+        this.forwardNumber++;
+        printMovingResult(this.forwardNumber);
+    }
+
+    private void printMovingResult(int forwardNumber) {
+        System.out.print(carName + " : ");
+        for (int i = 1; i <= forwardNumber; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+}
