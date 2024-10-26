5d94ff352316ab476bde8982f9a3041bf915ed20
rootTiket <l20078829@gmail.com>
2024-10-27 00:55:40 +0900

    feat: 실행결과 출력 메시지 enum 추가

--- src/main/java/racingcar/View/constant/OutputMessage.java
+++ src/main/java/racingcar/View/constant/OutputMessage.java
@@ -1,7 +1,8 @@
 package racingcar.View.constant;
 
 public enum OutputMessage {
-    START_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    START_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    ROUND_START_MESSAGE("실행 결과");
     private final String message;
 
     OutputMessage(String message) {
