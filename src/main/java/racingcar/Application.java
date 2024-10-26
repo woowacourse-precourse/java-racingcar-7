31479b07be33f60d0ce818b2d831f9700a64db20
haeun <haeun9634@naver.com>
2024-10-26 12:28:15 +0900

    Feat: 자동차 객체 생성 및 자동차 String 배열을 객체로 초기화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,11 +4,24 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.Input;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         String[] carNames = Input.inputCarNames();
         int tryCount = Input.inputTryCount();
+        List<Car> cars = initializeCars(carNames);//자동차 초기화
+
+    }
+
+    public static List<Car> initializeCars(String[] carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));//자동차를 자동차 객체로 만드는 과정
+        }
+        return cars;
     }
 
 }
