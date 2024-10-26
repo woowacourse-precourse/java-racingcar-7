31f4b6d207b26f3d4bcae0c307db7eecc913a4a6
nns503 <73779581+nns503@users.noreply.github.com>
2024-10-26 23:46:46 +0900

    refactor: 변수명 변경 및 상수화

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,24 +4,25 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
 
+    private static final int LIMIT = 4;
     private final String name;
-    private int len;
+    private int distance;
 
     public Car(String name) {
         this.name = name;
     }
 
     public void move(){
-        int randomNumber = Randoms.pickNumberInRange(0, 9);
-        if(randomNumber >= 4 && randomNumber <= 9){
-            len++;
+        if(Randoms.pickNumberInRange(0, 9) >= LIMIT ){
+            distance++;
         }
     }
 
     public String getName(){
         return name;
     }
-    public int getLen() {
-        return len;
+
+    public int getDistance() {
+        return distance;
     }
 }
