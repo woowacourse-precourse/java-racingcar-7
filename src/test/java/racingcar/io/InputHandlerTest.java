e26f9aa00153b81e1ba7f4f7c64ca84cb6dff171
정소민 <yjk70702002@gmail.com>
2024-10-26 22:27:30 +0900

    refactor: 경주할 자동차 이름 배열에 저장하는 메서드 따로 뺌

--- src/test/java/racingcar/io/InputHandlerTest.java
+++ src/test/java/racingcar/io/InputHandlerTest.java
@@ -1,15 +1,10 @@
 package racingcar.io;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.stream.Stream;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
