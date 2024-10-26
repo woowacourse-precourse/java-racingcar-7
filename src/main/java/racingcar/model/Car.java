619d88d7a05456604ae9a9287c1ed846bf96e4fd
hmlssghm <kwonse326@gmail.com>
2024-10-26 22:24:02 +0900

    feat(CarManager): car 객체 관리하는 모델 생성

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,12 +1,13 @@
 package racingcar.model;
 
 public class Car {
+    final int START_POSITION = 0;
     private String name;
     private int move;
 
-    public Car(String name, int move) {
+    public Car(String name) {
         this.name = name;
-        this.move = move;
+        this.move = START_POSITION;
     }
 
     public String getName() {
@@ -21,7 +22,7 @@ public class Car {
         this.move = move;
     }
 
-    public void addMove(Car car) {
-        car.setMove(car.getMove() + 1);
+    public void addMove() {
+        this.setMove(this.getMove() + 1);
     }
 }
