265226dc0fa2a6713f8d97cf5eddf7516ac789bd
lsj0822 <lsj0822@khu.ac.kr>
2024-10-26 20:10:14 +0900

    feat(ExceptionHandler) : 예외사항 추가
    중복된 이름이 추가되었을 경우, 이를 set로 만들어서 길이가 일치하지 않는다면 중복된 값이 있는 것으로 판단하고 예외처리함

--- src/main/java/exceptor/ExceptionHandler.java
+++ src/main/java/exceptor/ExceptionHandler.java
@@ -1,6 +1,10 @@
 package exceptor;
 
 import constant.Constants;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static utils.LetterCounter.letterCounter;
 
 public class ExceptionHandler {
@@ -12,15 +16,23 @@ public class ExceptionHandler {
             throw new IllegalArgumentException("입력한 값이 정수가 아님");
         }
     }
+
     public static void detectNegativeIntegerPlayerTurn(int turn){
         if(turn < 0){
             throw new IllegalArgumentException("입력한 값이 음수임");
         }
     }
 
-    public static void detectNameLengthSatisfyingCriteria(String name){
+    public static void detectNameLengthLimit(String name){
         if(letterCounter(name) > Constants.RACING_CAR_NAME_LIMIT){
             throw new IllegalArgumentException("글자 수가 기준보다 많음");
+        }
+    }
+
+    public static void detectDuplicatedName(String[] group){
+        Set<String> removedDuplicategroup = new HashSet<>(List.of(group));
+        if(removedDuplicategroup.size() != group.length){
+            throw new IllegalArgumentException("중복된 이름이 들어갔음");
         }
     }
 }
