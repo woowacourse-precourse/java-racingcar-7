ed743a437c4f9e7b3f26af3eb5e3ef9394bab024
wvssm <neoramiregi@gmail.com>
2024-10-27 02:02:04 +0900

    refactor(CarFactory): 변수명 오타 수정 Nmae -> Name

--- src/main/java/racingcar/domain/CarFactory.java
+++ src/main/java/racingcar/domain/CarFactory.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class CarFactory {
     public static List<Car> generateCars(List<String> carNames){
         List<Car> cars = new ArrayList<>();
-        for(String carNmae : carNames){
-            cars.add(new Car(carNmae));
+        for(String carName : carNames){
+            cars.add(new Car(carName));
         }
         return cars;
     }
