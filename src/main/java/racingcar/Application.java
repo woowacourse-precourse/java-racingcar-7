0f5b11170bd4a325b914955964c12416914cc144
lvalentine6 <ysr1227@naver.com>
2024-10-26 10:41:48 +0900

    feat: implement main logic

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		RaceManager raceManager = RaceManager.of(
+			IoHandler.of(InputHandler.create(), OutputHandler.create(), ValidatingParser.create()),
+			RandomNumberProvider.create());
+
+		raceManager.run();
+	}
 }
--- src/main/java/racingcar/RaceManager.java
+++ src/main/java/racingcar/RaceManager.java
@@ -1,4 +1,34 @@
 package racingcar;
 
+import java.util.List;
+
 public class RaceManager {
+	private final IoHandler ioHandler;
+	private final RandomNumberProvider randomNumberProvider;
+
+	private RaceManager(IoHandler ioHandler, RandomNumberProvider randomNumberProvider) {
+		this.ioHandler = ioHandler;
+		this.randomNumberProvider = randomNumberProvider;
+	}
+
+	public static RaceManager of(IoHandler ioHandler, RandomNumberProvider randomNumberProvider) {
+		return new RaceManager(ioHandler, randomNumberProvider);
+	}
+
+	public void run() {
+		RaceInfo raceInfo = ioHandler.handleRaceInfoForIo();
+		Cars cars = Cars.from(raceInfo.getCarNames());
+		int gameCount = 0;
+
+		ioHandler.showResult();
+		while (gameCount < raceInfo.getAttemptCount()) {
+			List<Integer> randomNumbers = randomNumberProvider.generateRandomNumbers(cars.getCarsCount());
+			cars.moveAll(randomNumbers);
+			ioHandler.showGameStatus(cars);
+			gameCount++;
+		}
+
+		List<String> winners = cars.getWinners();
+		ioHandler.showWinners(winners);
+	}
 }
