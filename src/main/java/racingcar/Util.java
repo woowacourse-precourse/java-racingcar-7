0a3fc8c38e2490fdc304e3b8c6c2203c936be561
seophohoho <seop0937@gmail.com>
2024-10-26 13:29:29 +0900

    feat(Util): 랜덤 값 추출 메소드 구현

---
+++ src/main/java/racingcar/Util.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+
+public class Util {
+    public int getRandom() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+}
