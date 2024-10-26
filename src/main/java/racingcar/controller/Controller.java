664dc786a724c719383b5ccdcccb9eddaf1b71e7
(minjae8563) <(minjae8563@naver.com)>
2024-10-26 19:30:48 +0900

    feat: InputController와 RacingController을 이어주는 Controller 클래스 구현

---
+++ src/main/java/racingcar/controller/Controller.java
@@ -0,0 +1,29 @@
+package racingcar.controller;
+
+import racingcar.model.Cars;
+import racingcar.view.OutputView;
+
+public class Controller {
+    private final OutputView outputView = new OutputView();
+    private final Cars cars = new Cars();
+    private final InputController inputController = new InputController();
+    private final RacingController racingController = new RacingController();
+    public Controller(){
+
+    }
+
+    public void gameStart(){
+
+        inputController.validateInputFormat();
+        inputController.validateNameLength();
+        inputController.validateNonEmptyNames();
+        inputController.addCarsFromInput(cars);
+
+        racingController.playGame(cars);
+
+        cars.calculateMaxMove();
+        cars.determineWinners();
+        outputView.printWinners(cars.getWinners());
+    }
+
+}
