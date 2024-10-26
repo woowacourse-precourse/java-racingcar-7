5dd4a28fc71d6cc8df2f7d07afad35b4293ec823
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-10-26 23:01:57 +0900

    refactor(RacingGame): 출력 조건에 맞게 수정
    printResult에서 자동차들의 위치 현황을 조건에 맞에 출력하도록 수정

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -35,8 +35,13 @@ public class RacingGame {
 
     public void printResult(){
         for(Car car : cars){
-            System.out.println(car.getName() +": "+ car.getPosition());
+            System.out.print(car.getName() + " : ");
+            for(int i = 0; i < car.getPosition(); i++){
+                System.out.print("-");
+            }
+            System.out.println();
         }
+        System.out.println();
     }
 
     public void printWinners(){
