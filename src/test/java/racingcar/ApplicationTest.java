8a205cf95b7cce0cf913a84464d36982950eb334
geonwoo <kysub99@naver.com>
2024-10-26 21:56:13 +0900

    test: 추가 고려사항 통합테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -44,6 +44,30 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 쉼표_사이_공백_제거_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi, woni, jun", "2");
+                    assertThat(output()).contains("pobi : --", "woni : --", "jun : --", "최종 우승자 : pobi, woni, jun");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 숫자_및_기호_포함_정상_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi1, woni$, jun!", "2");
+                    assertThat(output()).contains("pobi1 : --", "woni$ : --", "jun! : --", "최종 우승자 : pobi1, woni$, jun!");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
     void 자동차_이름_길이_예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi123,woni,jun", "3"))
@@ -78,6 +102,16 @@ class ApplicationTest extends NsTest {
                         .hasMessageContaining("잘못된 이동 횟수입니다. 양의 정수를 입력해주세요.")
         );
     }
+
+    @Test
+    void 자동차_이름_중복_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni,pobi", "3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("자동차 이름은 중복될 수 없습니다.")
+        );
+    }
+
 
     @Override
     public void runMain() {
