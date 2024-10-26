e610ce237571704d82127195eb718126a0002e77
dltmddus <tmddus4671@gmail.com>
2024-10-26 22:54:32 +0900

    feat: 자동차 경주 진행상태 출력 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,8 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.domain.Car;
+
 public class OutputView {
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ATTEMPT_COUNT = "시도할 횟수는 몇 회인가요?";
@@ -16,6 +19,13 @@ public class OutputView {
 
     public static void printExecutionResult() {
         System.out.println(EXECUTION_RESULT);
+    }
+
+    public static void printCurrentResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.name + " : " + "-".repeat(car.movementOfNumber));
+        }
+        System.out.println();
     }
 
     public static void printFinalWinner() {
