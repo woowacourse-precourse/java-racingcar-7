fc9aa08d6dabe8c21b6319d6322e9cc08c88af26
SeoMoonk <sunghoon1216@naver.com>
2024-10-26 18:27:40 +0900

    feat: 사용자로부터 자동차 이름을 입력받는다
    사용자에게 입력 안내문구를출력하고, 자동차 이름을 입력받음

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,11 +1,14 @@
 package racingcar.view;
 
+import static racingcar.constant.RacingCarInfoMsg.TYPE_CAR_NAMES;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     
     //TODO: 사용자로부터의 입력을 담당하는 클래스
     public String inputCarNames() {
-        //TODO : 사용자로부터 자동차들의 이름을 입력받는다
-
-        return "";
+        System.out.println(TYPE_CAR_NAMES.getMsg());
+        return Console.readLine();
     }
 }
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,9 +1,11 @@
 package racingcar.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static racingcar.constant.RacingCarInfoMsg.TYPE_CAR_NAMES;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.Application;
@@ -13,32 +15,35 @@ class InputViewTest extends NsTest {
     private final InputView inputView = new InputView();
 
     @Test
-    @DisplayName("사용자로부터 자동차들의 이름을 입력받는 문구가 출력된다")
+    @DisplayName("사용자로부터 자동차들의 이름을 입력받을 수 있게 안내 문구가 출력된다")
     void t001() {
         //given
-        String testInput = "seo";
-        System.setIn(new ByteArrayInputStream(testInput.getBytes()));
+        String testInput = "test";
+        InputStream in = new ByteArrayInputStream(testInput.getBytes());
+        System.setIn(in);
 
         //when
         inputView.inputCarNames();
 
         //then
-        assertThat(output()).contains("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        assertThat(output()).contains(TYPE_CAR_NAMES.getMsg());
     }
 
     @Test
-    @DisplayName("사용자는 자동차들의 이름을 입력할 수 있다")
+    @DisplayName("사용자로부터 자동차의 이름을 입력받는다")
     void t002() {
         //given
-        String testInput = "seo";
-        System.setIn(new ByteArrayInputStream(testInput.getBytes()));
+        String testInput = "test";
+        InputStream in = new ByteArrayInputStream(testInput.getBytes());
+        System.setIn(in);
 
         //when
         String result = inputView.inputCarNames();
 
         //then
-        assertThat(result).isEqualTo(testInput);
+        assertThat(result).isEqualTo("test"); // 입력받은 문자열에서 개행 제거
     }
+
 
     @Override
     public void runMain() {
