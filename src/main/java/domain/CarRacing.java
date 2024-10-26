2a36e7735c89132e9de5791410f3c08d5dbb9532
jayjae97 <skyroof97@gmail.com>
2024-10-26 12:53:44 +0900

    feat: InputHandler, OutputHandler, MoveStrategy의 종속성을 주입한 후 값 출력 기능 구현

--- src/main/java/domain/CarRacing.java
+++ src/main/java/domain/CarRacing.java
@@ -1,5 +1,34 @@
 package domain;
 
-public class CarRacing {
+import iohandler.InputHandler;
+import iohandler.OutputHandler;
+import strategy.MoveStrategy;
 
+import java.util.List;
+
+public class CarRacing {
+    private final InputHandler inputHandler;
+    private final OutputHandler outputHandler;
+    private final MoveStrategy moveStrategy;
+    private List<Car> cars;
+
+    public CarRacing(InputHandler inputHandler, OutputHandler outputHandler, MoveStrategy moveStrategy) {
+        this.inputHandler = inputHandler;
+        this.outputHandler = outputHandler;
+        this.moveStrategy = moveStrategy;
+    }
+
+    public void run(){
+        cars = inputHandler.inputCars();
+        int attempts = inputHandler.inputAttempt();
+
+        for(int i = 0 ; i<attempts; i++){
+            for(Car car: cars){
+                car.move(moveStrategy.isPossibleToMove());
+            }
+            outputHandler.printResult(cars);
+        }
+
+        outputHandler.printWinners(cars);
+    }
 }
