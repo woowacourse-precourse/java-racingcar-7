f3c059ad73cd923c6b6d2be83eb544899bd90be4
ouvir <kilhyunii@naver.com>
2024-10-26 22:00:30 +0900

    feat: application 구현
    - 사용자 입력 받기(자동차 이름, 시도 횟수)
    - 텍스트에서 자동차 이름을 구분해서 저장
    - 전진 프로세스를 통해 앞으로 전진할지 멈출지 결정
    - 자동차마다, 진행 상황을 기록
    - 시도횟수를 반복하며, 진행 상황을 출력
    - 우승자를 판별하여 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,39 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import determiner.WinnerDeterminer;
+import process.MovePolicy;
+import race.Race;
+import separator.Separator;
+import view.RaceView;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String text = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int count = Integer.parseInt(Console.readLine());
+
+        List<String> carNames = Separator.split(text);
+        MovePolicy policy = new MovePolicy();
+        Race race = new Race(carNames);
+        for (int i = 0; i < count; i++) {
+
+            for (String car : carNames) {
+                if (policy.run()) {
+                    race.updateProgress(car);
+                }
+            }
+
+            RaceView.view(race.getAllProgress());
+        }
+
+        WinnerDeterminer determiner = new WinnerDeterminer();
+        List<String> winners = determiner.run(race.getAllProgress());
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
