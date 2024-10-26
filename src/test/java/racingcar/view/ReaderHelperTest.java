2633e4f83d91446375f02fe91813ca0f025cfd8e
yuntasha <jaljayo85@gmail.com>
2024-10-27 02:11:03 +0900

    test(ReaderHelperTest.java): write test code
    Write just one code because conflict System.in

---
+++ src/test/java/racingcar/view/ReaderHelperTest.java
@@ -0,0 +1,33 @@
+package racingcar.view;
+
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ReaderHelperTest {
+
+    @Test
+    @DisplayName("두명의 선수, 3회 입력")
+    void twoPlayer() {
+        // given
+        ReaderHelper readerHelper = new ReaderHelper();
+        setInput("player1,player2\n3");
+
+        // when
+        List<String> result = readerHelper.readPlayerName();
+        int playTime = readerHelper.readPlayTime();
+
+        // then
+        assertEquals(2, result.size());
+        assertEquals("player1", result.get(0));
+        assertEquals("player2", result.get(1));
+        assertEquals(3, playTime);
+    }
+
+    private void setInput(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+}
