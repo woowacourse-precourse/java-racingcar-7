89d5a41624e47292d35f3989161e91c85878b55f
yush <yush2202@gmail.com>
2024-10-26 20:26:48 +0900

    feat($ OutputView): 사용자 출력 UI 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,42 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.constants.Constants;
+import racingcar.constants.OutputMessages;
+
+public class OutputView {
+    private OutputView() {
+
+    }
+
+    public static void printInsertRacersName() {
+        System.out.println(OutputMessages.INSERT_RACERS_NAME);
+    }
+
+    public static void printInsertTryCount() {
+        System.out.println(OutputMessages.INSERT_TRY_COUNT);
+    }
+
+    public static void printGameResultMessage() {
+        System.out.println(OutputMessages.RACING_RESULT);
+    }
+
+    public static void printFinalWinner(List<String> finalWinners) {
+        System.out.print(OutputMessages.FINAL_WINNER);
+        StringBuilder result = new StringBuilder();
+
+        for (String winner : finalWinners) {
+            result.append(winner).append(", ");
+        }
+
+        System.out.println(result.substring(0, result.length() - 2));
+    }
+
+    public static void printGameResult(String racerName, int distance) {
+        System.out.println(racerName + Constants.COLON + Constants.DASH.repeat(distance));
+    }
+
+    public static void printEmptyLine() {
+        System.out.println();
+    }
+}
