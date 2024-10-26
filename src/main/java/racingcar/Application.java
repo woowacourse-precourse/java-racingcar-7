610fe107c2542622a745c40a8787f99c3ca713be
Mae-Hyong <maehyong02@gmail.com>
2024-10-27 01:53:54 +0900

    feat(car): 차량 초기화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -31,5 +32,35 @@ public class Application {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String tryCountInput = Console.readLine();
 
+        List<Car> cars = initializeCars(carNameList);
+   }
+
+    private static List<Car> initializeCars(List<String> carNameList) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNameList) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+
+    static class Car {
+        private final String carName;
+        private int position = 0;
+
+        public Car(String carName) {
+            this.carName = carName;
+        }
+
+        public String getCarName() {
+            return carName;
+        }
+
+        public int getPosition() {
+            return position;
+        }
+
+        public void advance() {
+            position++;
+        }
     }
 }
