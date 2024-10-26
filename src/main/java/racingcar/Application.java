25fce22561be7c1c99bf479d34f4bb048a86b9e3
dldb-chamchi <dladkfl49@gmail.com>
2024-10-26 17:48:28 +0900

    feat: add type<RacingCar> ArrayList. name is cars

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,12 +7,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    private List<RacingCar> cars = new ArrayList<>();
+
+    public String[] setCarName(String input) {
+        return input.split(",");
+    }
 
 
 
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        Application racingGame = new Application();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNameInput = Console.readLine();  //사용자 입력
+        String[] carNames = racingGame.setCarName(carNameInput);
     }
 }
