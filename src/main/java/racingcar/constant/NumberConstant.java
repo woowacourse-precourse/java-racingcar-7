6437c70fcf4e4d81ef06f5a083d1fb8d5fda7733
mingking2 <mingee641@gmail.com>
2024-10-26 22:04:10 +0900

    refactor: 자동차 이름 길이 상수 추가

--- src/main/java/racingcar/constant/NumberConstant.java
+++ src/main/java/racingcar/constant/NumberConstant.java
@@ -4,4 +4,6 @@ public class NumberConstant {
     public static final int MIN_NUMBER = 0;
     public static final int MAX_NUMBER = 9;
     public static final int SET_VALUE = 4;
+
+    public static final int SET_CAR_NAME_LENGTH = 5;
 }
