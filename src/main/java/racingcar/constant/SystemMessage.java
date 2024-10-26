96c768f5e7461b009361c5837923e983a3e99cb4
chyun <och2510@gmail.com>
2024-10-26 21:51:13 +0900

    feat(OutputView): 입력 안내 메세지와 게임 결과 출력 기능 추가

---
+++ src/main/java/racingcar/constant/SystemMessage.java
@@ -0,0 +1,11 @@
+package racingcar.constant;
+
+public class SystemMessage {
+    public static final String LINE_FEED = "\n";
+    public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_ATTEMPTS = "시도할 횟수는 몇 회인가요?";
+    public static final String WINNER_DELIMITER = ", ";
+    public static final String RESULT_PREFIX = "실행 결과";
+
+    public static final String MOVEMENT_STATE_FORMAT = "%s : %s";
+}
