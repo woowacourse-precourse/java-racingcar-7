e3a65920a3ddf5514d6c5c04ac55595ebaa0e26f
홍성문 <2020041412@hanyang.ac.kr>
2024-10-26 11:53:18 +0900

    feat: implemet D and modify print func

--- src/main/java/racingcar/Challenge.java
+++ src/main/java/racingcar/Challenge.java
@@ -6,9 +6,27 @@ import java.util.Comparator;
 public class Challenge {
 
     private ArrayList<Car> partCars;
+    ArrayList<String> winners;
 
     public Challenge() {
         partCars = new ArrayList<>();
+        winners = new ArrayList<>();
+    }
+
+    public void addCar(Car car) {
+        partCars.add(car);
+    }
+
+    public void makeWinnerList()
+    {
+        partCars.sort(Comparator.comparingInt(Car::getDistance).reversed());
+        int tmp = partCars.get(0).getDistance(); // 가장 앞에 있는(가장 큰 distance)
+
+        for (Car car : partCars) {
+            if(car.getDistance() == tmp) {
+                winners.add(car.getCarName());
+            }
+        }
     }
 
     public void printStep() {
@@ -22,15 +40,6 @@ public class Challenge {
     }
 
     public void printWinner() {
-        partCars.sort(Comparator.comparingInt(Car::getDistance).reversed());
-        int tmp = partCars.get(0).getDistance(); // 가장 앞에 있는(가장 큰 distance)
-        ArrayList<String> winners = new ArrayList<>();
-        for (Car car : partCars) {
-            if(car.getDistance() == tmp) {
-                winners.add(car.getCarName());
-            }
-        }
-
         System.out.println(String.join(", ", winners));
     }
 }
