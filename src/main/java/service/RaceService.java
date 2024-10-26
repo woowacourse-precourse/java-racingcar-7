c7a7088016e8d38078b5a51a133d612c9aeea15f
codrae <hi653@naver.com>
2024-10-26 21:05:01 +0900

    feat: callback 함수 사용으로 변경
    기존 출력을 callback함수를 사용하는 방식으로 변경함.

--- src/main/java/service/RaceService.java
+++ src/main/java/service/RaceService.java
@@ -6,20 +6,16 @@ import java.util.List;
 
 public class RaceService {
     private final Race race;
+
     public RaceService(List<String> carNames, int attempts, MoveStrategy moveStrategy) {
         this.race = new Race(carNames, attempts, moveStrategy);
     }
 
-    public void startRace(){
-        race.startRace();
+    public void startRace(RaceStatusCallback callback) {
+        race.startRace(callback);
     }
 
-    public List<String> getWinnner(){
-        return race.getWinner();
+    public List<String> getWinners(){
+        return race.getWinners();
     }
-
-    public void printWinnner(){
-        race.printWinner();
-    }
-
 }
