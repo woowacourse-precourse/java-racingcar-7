d090024282eef377723dad9069eb65c59897a1c1
Chaewon <corndots@naver.com>
2024-10-27 07:37:44 +0900

    feat(InputValidator): 메서드명 수정
    명확한 의미 전달을 위해 validateCarNames -> isValidNameLength로 수정

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class InputValidator {
     private static final int MAX_NAME_LENGTH = 5;
 
-    public static void validateCarNames(List<String> carNames) {
+    public static void isValidCarNameLength(List<String> carNames) {
         for (String name : carNames) {
             if (name.isEmpty() || name.length() > MAX_NAME_LENGTH) {
                 throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하로 입력해 주세요.");
