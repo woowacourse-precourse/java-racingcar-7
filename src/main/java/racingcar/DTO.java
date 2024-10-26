539ae6aabe1aad9d9207496654f99f84522f2002
윤민수 <koelysa5696@naver.com>
2024-10-26 11:48:58 +0900

    Feat: DTO 클래스 생성
    - 차 이름과 전진 횟수에 대한 데이터 생성

---
+++ src/main/java/racingcar/DTO.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+public class DTO {
+    private final String carName;
+    private int forwardCount;
+
+    public DTO(String carName, int forWardCount) {
+        this.carName = carName;
+        this.forwardCount = forWardCount;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getForwardCount() {
+        return forwardCount;
+    }
+
+
+    public void setForwardCount(int forwardCount) {
+        this.forwardCount = forwardCount;
+    }
+}
