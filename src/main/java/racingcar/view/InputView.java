bf9c73eede95cab2a166d904ae3e4ee477061268
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-10-26 16:20:10 +0900

    feat: InputView구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,33 +2,43 @@ package racingcar.view;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-public class InputView {
+public abstract class InputView {
+    protected String readInput() {
+        return readLine();
+    }
 
-    public static List<String> inputCarNames(){
+    public List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분");
-        String input=readLine();
+        String input = readInput();
         return Arrays.asList(input.split(","));
     }
 
-    public static int inputAttemptCount(){
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        while (true){
-            try {
-                int count=Integer.parseInt(readLine());
-                validateAttemptCount(count);
-                return count;
-            }catch(NumberFormatException e){
-                System.out.println("유효한 숫자를 입력하세요.(ex 양수)");
-            }
+    public int inputCount() {
+        String input=inputAttemptCount();
+        return validateAttemptCount(input);
+    }
+
+    private  String inputAttemptCount() {
+        String count;
+        System.out.println("시도할 회수는 몇회인가요?");
+        try {
+            count = readInput();
+            return count;
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("숫자를 입력해주세요");
         }
     }
 
-    private static void validateAttemptCount(int count) {
-        if(count<=0){
+
+    private static int validateAttemptCount(String input) {
+        int count=Integer.parseInt(input);
+        if (count <= 0) {
             throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
         }
+        return count;
     }
 }
