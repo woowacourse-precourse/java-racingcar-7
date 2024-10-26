6f9ddbe09f7f0c275487c1262c9346923b234c95
DongHoon Lee <dhl1924@naver.com>
2024-10-27 01:43:22 +0900

    feat(exception): 예외 처리 기능 추가 (ErrorMessage enum 및 RacingCarGameException)
    - ErrorMessage enum을 추가하여 예외 메시지를 일관되게 관리
    - RacingCarGameException을 추가해 게임 전반의 예외를 처리하고, ErrorMessage를 통한 메시지 전달을 지원
    - from() 정적 팩토리 메서드를 사용해 예외 생성 과정을 간결하게 구현
    - null 예외 처리를 위한 상수를 ErrorMessage에 추가

---
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -0,0 +1,16 @@
+package racingcar.exception;
+
+public enum ErrorMessage {
+	NULL_INPUT_ERROR("null은 입력할 수 없습니다.")
+	;
+
+	private final String message;
+
+	ErrorMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
