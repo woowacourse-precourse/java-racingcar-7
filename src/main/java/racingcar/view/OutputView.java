592f3bb785876a0de93b710f8192bf8171c6fcdc
dd-jiyun <dlwldbs8015@gmail.com>
2024-10-26 14:56:25 +0900

    feat(OutputView): 실행 결과 및 우승자 출력 기능

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,43 @@
+package racingcar.view;
+
+import java.util.List;
+import java.util.Map;
+import racingcar.model.MovementRecord;
+
+public class OutputView {
+
+    private static final String PROCESS_RESULT = "실행 결과";
+    private static final String WINNER_ANNOUNCEMENT = "최종 우승자 : ";
+
+    private static final String WINNER_DELIMITER = ", ";
+    private static final String PROCESS_POSITION_MARK = "-";
+
+    public static void printProcess(MovementRecord movementRecord) {
+        System.out.println(PROCESS_RESULT);
+
+        for (Map<String, Integer> round : movementRecord.getRoundRecords()) {
+            printRoundResult(round);
+
+            System.out.println();
+        }
+    }
+
+    private static void printRoundResult(Map<String, Integer> round) {
+        for (Map.Entry<String, Integer> entry : round.entrySet()) {
+            String carName = entry.getKey();
+            int position = entry.getValue();
+
+            String progress = PROCESS_POSITION_MARK.repeat(position);
+            String result = String.format("%s : %s", carName, progress);
+
+            System.out.println(result);
+        }
+    }
+
+    public static void printWinners(List<String> winners) {
+        String winnerNames = String.join(WINNER_DELIMITER, winners);
+
+        System.out.println(WINNER_ANNOUNCEMENT + winnerNames);
+    }
+
+}
