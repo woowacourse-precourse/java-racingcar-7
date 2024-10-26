0078b56edfdd26476356464a897ee49c8d3b7a1b
Etwashoeren <sungmin0609@nate.com>
2024-10-26 23:13:45 +0900

    refactor. Application.java 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,70 +10,8 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
-
-        System.out.println("시도할 횟수는 몇 회인가요?");
-
-        int count;
-
-        try {
-            count = Integer.parseInt(Console.readLine());
-            if (count <= 0) {
-                throw new IllegalArgumentException();
-            }
-        } catch (Exception e) {
-            throw new IllegalArgumentException("잘못된 입력값 입니다.");
-        }
-
-
-        String[] cars = input.split(",");
-
-        for (String car : cars) {
-            if (car.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5글자 이하이어야 합니다.");
-            }
-        }
-
-        int countOfCars = cars.length;
-
-        String[][] carArray = new String[countOfCars][2];
-
-        for (int i = 0 ; i < countOfCars ; i++) {
-            carArray[i][0] = cars[i];
-            carArray[i][1] = "";
-        }
-
-        while (count-- > 0) {
-            for (int i = 0; i < countOfCars; i++) {
-
-                int randNum = Randoms.pickNumberInRange(0,9);
-
-                if (randNum >= 4) {
-                    carArray[i][1] += "-";
-                }
-                System.out.println(carArray[i][0] + " : " + carArray[i][1]);
-            }
-            System.out.println();
-        }
-
-        int result = 0;
-
-        for (int i = 0; i < countOfCars; i++) {
-            if (carArray[i][1].length() > result) {
-                result = carArray[i][1].length();
-            }
-        }
-
-        List<String> winners = new ArrayList<>();
-
-        for (int i = 0; i < countOfCars; i++) {
-            if (carArray[i][1].length() == result) {
-                winners.add(carArray[i][0]);
-            }
-        }
-
-        System.out.println("최종 우승자 : " + String.join(",", winners));
+        UserView userView = new UserView();
+        RaceController raceController = new RaceController(userView);
+        raceController.run();
     }
 }
