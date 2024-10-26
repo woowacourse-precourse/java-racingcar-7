66c458b2dc7e64a10ccbfeef0e115af595c997b1
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-26 23:22:58 +0900

    feat(Application): main 메서드 진입점 구성
    - ControllerFactory 객체 생성 및 의존성 주입
    - ControllerFactory 의 컨트롤러 객체를 생성하는 메서드들 호출부 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import racingcar.Controller.RaceProcessController;
+import racingcar.Controller.RaceViewController;
+import racingcar.Factory.ControllerFactory;
+import racingcar.Factory.DomainFactory;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ControllerFactory controllerFactory = new ControllerFactory();
+
+        RaceViewController viewController = controllerFactory.createRaceViewController();
+
+        RaceProcessController processController = controllerFactory.createRaceProcessController(viewController);
+
+        processController.run();
     }
 }
