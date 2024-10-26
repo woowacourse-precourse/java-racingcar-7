11d2997c933108f0e088a950c8365fed0bff1961
sodychoe <sodychoe@icloud.com>
2024-10-26 15:45:15 +0900

    feat : 자동차 이름 입력 받기 기능 구현

--- src/main/java/racingcar/io/InputConsoleHandler.java
+++ src/main/java/racingcar/io/InputConsoleHandler.java
@@ -1,11 +1,18 @@
 package racingcar.io;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputConsoleHandler {
 
     public static final String CARNAME_INPUT_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUTS_SEPARATOR = ",";
 
     public void showCarNameGuideMessage() {
         System.out.println(CARNAME_INPUT_GUIDE_MESSAGE);
+    }
+
+    public String[] askCarName() {
+        return Console.readLine().split(INPUTS_SEPARATOR);
     }
 
 }
