88f3198c05dc31e688e6016ab74bd4bcdb95344d
pokm321 <pokm90@gmail.com>
2024-10-26 19:09:21 +0900

    test: add more test cases
    works: run("pobi,woni,준팍,ㅇ       ㅇ", "2")
    exceptions: when the second input is not a natural number (negative, zero, alphabet, etc.)

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,12 +1,12 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -15,11 +15,11 @@ class ApplicationTest extends NsTest {
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
         assertRandomNumberInRangeTest(
                 () -> {
@@ -28,13 +28,23 @@ class ApplicationTest extends NsTest {
                 },
                 MOVING_FORWARD, STOP, STOP, MOVING_FORWARD
         );
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,준팍,ㅇ       ㅇ", "2");
+                    assertThat(output()).contains("pobi : -", "woni : ", "준팍 : ", "ㅇㅇ : -",
+                            "pobi : -", "woni : ", "준팍 : -", "ㅇㅇ : --",
+                            "최종 우승자 : ㅇㅇ");
+                },
+                MOVING_FORWARD, STOP, STOP, MOVING_FORWARD,
+                STOP, STOP, MOVING_FORWARD, MOVING_FORWARD
+        );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,でja", "1"))
@@ -48,6 +58,26 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("woni,woni", "2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+//        assertSimpleTest(() ->
+//                assertThatThrownBy(() -> runException("pobi,woni", ""))
+//                        .isInstanceOf(IllegalArgumentException.class)
+//        );
     }
 
     @Override
