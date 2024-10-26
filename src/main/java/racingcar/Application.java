e31c382299b51ff73f4e9629c0f71bf0cb5c64da
pakaheo <k70923@konkuk.ac.kr>
2024-10-27 00:19:08 +0900

    refactor : Separate method of RacingGame class

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,6 +4,6 @@ import racingcar.controller.RacingGame;
 
 public class Application {
     public static void main(String[] args) {
-        RacingGame.race();
+        new RacingGame().race();
     }
 }
--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -9,20 +9,41 @@ import racingcar.view.InputView;
 import racingcar.view.ResultView;
 
 public class RacingGame {
-    public static void race() {
+
+    private final String inputCarNames = initializeCarNames();
+    private final Long tryCount = initializeTryCount();
+    private final Cars cars = registerCars();
+
+    public void race() {
+        executeRacing();
+        announceWinners();
+    }
+
+    private String initializeCarNames() {
         String inputCarNames = InputView.promptInputCarNames();
         InputValidator.validateCarNames(inputCarNames);
-        long tryCount = InputView.promptInputTryCount();
+        return inputCarNames;
+    }
 
+    private Long initializeTryCount() {
+        return InputView.promptInputTryCount();
+    }
+
+    private Cars registerCars() {
         CarFactory carFactory = CarFactory.getInstance();
-        Cars cars = carFactory.createCars(CarNamesCreator.createCarNames(inputCarNames));
+        return carFactory.createCars(CarNamesCreator.createCarNames(inputCarNames));
+    }
 
+    private void executeRacing() {
         ResultView.promptExecutionResult();
+
         for (long count = 1L; count <= tryCount; count++) {
             cars.takeTurn();
             ResultView.showTurnResult(cars);
         }
+    }
 
+    private void announceWinners() {
         List<String> winners = cars.findWinners();
         ResultView.showWinners(winners);
     }
