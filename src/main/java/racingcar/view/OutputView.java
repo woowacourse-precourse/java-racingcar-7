9e60a223f44ee4131eacb0ba9b634e01826dc453
양효빈 <hyobin3026@naver.com>
2024-10-26 14:39:29 +0900

    feat(view): Implement InputView and OutputView

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,14 @@
 package racingcar.view;
 
 public class OutputView {
+
+    public static void printRacingResult(String racingResult){
+        System.out.println(racingResult);
+    }
+
+    public static void printWinner(String winnerResult){
+        System.out.println(winnerResult);
+    }
+
+
 }
