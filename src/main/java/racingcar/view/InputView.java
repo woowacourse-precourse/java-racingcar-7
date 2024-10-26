c944a654cbb2166f9a44aac00808c4a741d89d5b
Jinu Yi <dionisos198@naver.com>
2024-10-26 23:14:15 +0900

    feat: 사용자 입력 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,7 +1,29 @@
 package racingcar.view;
 
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.exception.ErrorCode;
+import racingcar.exception.IllegalArgumentException;
+
 public class InputView {
+
+    public String racingCarNamesWithDelimeter(){
+
+        String carNamesWithDelimeter = Console.readLine();
+
+        return carNamesWithDelimeter;
+    }
+
+    public int countOfTry(){
+
+        String countOfTry = Console.readLine();
+
+        try{
+            return Integer.valueOf(countOfTry);
+        }catch(Exception e){
+            throw new IllegalArgumentException(ErrorCode.CANT_CONVERT_TO_INTEGER);
+        }
+    }
 
 
 }
