02f9579e770ec69e0ddc5e0e8354626b80cbf76b
lchyeong <lchyeong@gmail.com>
2024-10-27 08:56:51 +0900

    refactor(controller): 가독성과 관리 용이성을 위해 디렉토리 구조 변경 및 메서드 분리

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,58 @@
+package racingcar.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import java.util.Map;
+import racingcar.service.RacingCarService;
+import racingcar.validator.CarNamesValidator;
+import racingcar.validator.TryNumberValidator;
+import racingcar.viewer.RacingCarViewer;
+
+public class RacingCarController {
+
+    private final RacingCarViewer racingCarViewer = new RacingCarViewer();
+    private final RacingCarService racingCarService = new RacingCarService();
+    private final CarNamesValidator carNamesValidator = new CarNamesValidator();
+    private final TryNumberValidator tryNumberValidator = new TryNumberValidator();
+
+    public void startRacing() {
+
+        List<String> splitCarNames = getValidatedCarNames();
+        int tryNumber = getValidatedTryNumber();
+
+        Map<String, String> racingResult = racingCarService.initializeCarPositions(splitCarNames);
+
+        System.out.println("실행 결과");
+        for (int i = 0; i < tryNumber; i++) {
+            List<String> canMoveCarList = racingCarService.getListCanMoveCarNames(splitCarNames);
+            racingResult = racingCarService.updateCarPositions(racingResult, canMoveCarList);
+            racingCarViewer.showRacingResult(racingResult);
+        }
+
+        String maxMove = racingCarService.findMaxMove(racingResult);
+        List<String> winners = racingCarService.getListRacingWinners(racingResult, maxMove);
+
+        racingCarViewer.showWinners(winners);
+    }
+
+    private List<String> getValidatedCarNames() {
+        racingCarViewer.promptCarNamesInput();
+
+        String carNames = Console.readLine();
+        carNamesValidator.validateCarNamesInput(carNames);
+
+        List<String> splitCarNames = racingCarService.splitCarNames(carNames);
+        carNamesValidator.validateCarNamesList(splitCarNames);
+
+        return splitCarNames;
+    }
+
+    private int getValidatedTryNumber() {
+        racingCarViewer.promptTryNumber();
+
+        String tryNumber = Console.readLine();
+        tryNumberValidator.validateTryNumber(tryNumber);
+
+        return Integer.parseInt(tryNumber);
+    }
+}
