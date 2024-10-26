b07923a9da675cc594aed31f04fabe5c5393a536
seahwanKim <kimkimseasea@gmail.com>
2024-10-26 12:33:51 +0900

    feat: 모든 자동차의 전진 기능을 추가한다

--- src/main/java/racingcar/domain/CarRacing.java
+++ src/main/java/racingcar/domain/CarRacing.java
@@ -1,4 +1,25 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarRacing {
+
+    private final List<Car> carList = new ArrayList<>();
+
+    public CarRacing(List<String> cars) {
+        for (String car : cars) {
+            carList.add(new Car(car));
+        }
+    }
+
+    public List<Car> getCars() {
+        return carList;
+    }
+
+    public void go() {
+        for (Car car : carList) {
+            car.go();
+        }
+    }
 }
