c4b04295919b95fcda3ac83019f5bcc742d2a338
honggyu1233@naver.com <honggyu1233@naver.com>
2024-10-27 00:37:23 +0900

    feat : InputView 수정 - 전진 횟수를 입력 받도록 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public static String racingcarInputView() {
         String inputName=Console.readLine();
+        int moveCount=Integer.parseInt(Console.readLine());
         return inputName;
     }
 }
