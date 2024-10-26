52c965845ceb566c1be3826b7ffdfa7a496ba2b7
김동진 <dongjin403@naver.com>
2024-10-26 16:10:49 +0900

    refactor : 메시지 파일 생성
    전역 변수만 담을 파일 생성

---
+++ src/main/java/console/Message.java
@@ -0,0 +1,16 @@
+package console;
+
+public class Message {
+
+    public static final String PROMPT_FOR_RACING_CARS =
+            "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String PROMPT_FOR_ATTEMPTS = "시도할 횟수는 몇 회인가요?";
+    public static final String WINNER_ANNOUNCEMENT_PREFIX = "최종 우승자 : ";
+    public static final String CAR_NAME_LENGTH_ERROR = "이름은 5자 이하만 가능합니다.";
+    public static final String NUMERIC_INPUT_PATTERN = "[0-9]+";
+    public static final String NATURAL_NUMBER_VALIDATION_ERROR = "1이상의 정수만 입력 가능합니다.";
+    public static final String NUMBER_TOO_LARGE_ERROR = "숫자가 너무 큽니다.";
+    public static final String CAR_NAME_FORMAT_ERROR = "경주할 자동차 이름은 공백없이 쉼표(,)로 구분해주세요.";
+    public static final String CAR_NAME_SEPARATOR_ERROR = "각 자동차는 쉼표(,)로 구분되어야 합니다.";
+    public static final String VALUE_REQUIRED_ERROR = "값을 입력해주세요";
+}
