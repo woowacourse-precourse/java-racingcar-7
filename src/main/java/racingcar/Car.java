5e93fd5c354d8179d2c4a43a73f9fcdbe569273d
seoyeoning <minalunpark@naver.com>
2024-10-26 20:58:10 +0900

    feat : Car 클래스 생성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+public class Car {
+
+    String name;
+    long moveCount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getMoveCount() {
+        return moveCount;
+    }
+
+    public void setMoveCount(long moveCount) {
+        this.moveCount = moveCount;
+    }
+
+    public Car(String name, long moveCount) {
+        this.name = name;
+        this.moveCount = moveCount;
+    }
+}
