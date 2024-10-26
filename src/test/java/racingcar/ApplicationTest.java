438e332bc8524b86b1654bd5394aac3fac9ee8b2
jinwook <wjswlsdnr333@gmail.com>
2024-10-27 02:09:15 +0900

    test(ApplicationTest): 우승자 출력 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -10,52 +10,27 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
 
-    @Test
-    void 입력테스트(){
-        assertSimpleTest(() -> {
-                    run("pobi,woni,jun","1");
-                    assertThat(output()).contains(
-                            "pobi : -",
-                            "woni : -",
-                            "jun : -"
-                            );
-                }
-        );
-        assertSimpleTest(() -> {
-                    run("pobi,woni,jun","2");
-                    assertThat(output()).contains(
-                            "pobi : -",
-                            "woni : -",
-                            "jun : -"
-                            ,"pobi : --",
-                            "woni : --",
-                            "jun : --"
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
 
-                    );
-                }
-        );
-        assertSimpleTest(() -> {
-                    run("pobi,woni,jun","3");
-                    assertThat(output()).contains(
-                            "pobi : -",
-                            "woni : -",
-                            "jun : -"
-                            ,"pobi : --",
-                            "woni : --",
-                            "jun : --"
-                            ,"pobi : ---",
-                            "woni : ---",
-                            "jun : ---"
-                    );
-                }
+    @Test
+    void 기능_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "5");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
     void 예외_테스트() {
-
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
-
 
     @Override
     public void runMain() {
