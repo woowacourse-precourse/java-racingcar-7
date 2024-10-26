be10393edd4db0f695266967a7663f77ae9549d5
haebyun <bhk4392@gmail.com>
2024-10-26 22:42:20 +0900

    feat(ConsoleInputView): 이동 횟수 입력 기능 및 예외 처리 추가
    - 이동 횟수 입력 요청 메시지 `REQUEST_TRY_NUMBER_MESSAGE` 추가
    - `inputMoveCount` 메서드를 통해 이동 횟수를 입력받고, 정수로 변환 후 반환
    - 잘못된 입력이 있을 경우 `INVALID_TRY_NUMBER_MESSAGE`와 함께 `IllegalArgumentException`을 발생시키도록 예외 처리

--- src/main/java/racingcar/view/input/ConsoleInputView.java
+++ src/main/java/racingcar/view/input/ConsoleInputView.java
@@ -6,11 +6,24 @@ import java.util.List;
 
 public class ConsoleInputView implements InputView {
     private static final String REQUEST_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String REQUEST_TRY_NUMBER_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String INVALID_TRY_NUMBER_MESSAGE = "잘못된 시도 횟수 입력입니다.";
 
     @Override
     public List<String> inputCarNames() {
         System.out.println(REQUEST_CAR_NAME_MESSAGE);
         String carNames = Console.readLine();
         return Arrays.asList(carNames.split(",",-1));
+    }
+
+    @Override
+    public Integer inputMoveCount() {
+        System.out.println(REQUEST_TRY_NUMBER_MESSAGE);
+        String tryNumber = Console.readLine();
+        try {
+            return Integer.parseInt(tryNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_TRY_NUMBER_MESSAGE);
+        }
     }
 }
