f5d757d0fc8466100ac8282326dce961f24fe02c
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-10-26 21:56:05 +0900

    feat(Application): 메인클래스에 실행 로직 구현
    사용자로부터 자동차 이름, 시도횟수 입력받음, 게임 진행, 최종 우승자 출력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 경주 클래스
+        RacingGame racingGame = new RacingGame();
+        String carNames = Console.readLine();
+
+        // 경주 차량 입력
+        racingGame.inputCars(carNames);
+
+        // 시도 횟수 입력
+        int attempts = Integer.parseInt(Console.readLine());
+        racingGame.inputAttempts(attempts);
+
+        // 경주 시작
+        racingGame.start();
+
+        // 우승자 출력
+        racingGame.printWinners();
     }
 }
