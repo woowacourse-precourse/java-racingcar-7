a361b8fa710ab1b7db6f9d89e31a5b6d7f7342d6
hunko <rhwlgns4386@naver.com>
2024-10-26 12:29:56 +0900

    feat : Car이름 빈값 검증로직 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,8 +2,20 @@ package racingcar;
 
 public class Car {
     public Car(String name) {
-        if (6 <= name.length()) {
+        if (isInvalid(name)) {
             throw new IllegalArgumentException("이름의 길이는 6을 넘길 수 없습니다.");
         }
+    }
+
+    private boolean isInvalid(String name) {
+        return isBlank(name) || isTooLong(name);
+    }
+
+    private boolean isBlank(String name) {
+        return name.isBlank();
+    }
+
+    private boolean isTooLong(String name) {
+        return 6 <= name.length();
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
 
@@ -12,8 +14,9 @@ public class CarTest {
         assertThatCode(() -> new Car("javaj")).doesNotThrowAnyException();
     }
 
-    @Test
-    public void 자동차의_이름은_6자_이상이면_예외가_발생한다() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Car("javaji"));
+    @ParameterizedTest
+    @ValueSource(strings = {"javaji", "", " "})
+    public void 자동차이름에_빈값_혹은_6자이상이면_예외가_발생한다(String name) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Car(name));
     }
 }
