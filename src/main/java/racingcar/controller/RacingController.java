6b4f3f59dd4faa7892472d252c526c5748c6d972
Seol_JY <wlsdud5654@gmail.com>
2024-10-26 22:23:18 +0900

    feat(view): 자동차 이름을 입력 받는 기능 구현
    - 안내 메세지 출력, 사용자 입력값 받기
    - 전체 문자열은 stripe 수행

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,17 @@
+package racingcar.controller;
+
+import racingcar.view.InputView;
+
+public class RacingController {
+    private final InputView inputView;
+
+    public RacingController(
+            InputView inputView
+    ) {
+        this.inputView = inputView;
+    }
+
+    public void run() {
+        String userInput = inputView.getCarNames();
+    }
+}
---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,16 @@
+package racingcar.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+    private static final String GET_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public String getCarNames() {
+        return readLineWithPrompt(GET_CAR_NAMES_MESSAGE);
+    }
+
+    private String readLineWithPrompt(String prompt) {
+        System.out.println(prompt);
+        return readLine().trim();
+    }
+}
