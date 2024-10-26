c5ddb5232f5c8832cc36e73ec856346c2707ba1b
HanJun Lee <137124830+LeeHanJun00@users.noreply.github.com>
2024-10-26 20:42:01 +0900

    test: 빈 이름이 포함되어 있을 경우 예외 발생

--- src/test/java/racingcar/ValidateTest.java
+++ src/test/java/racingcar/ValidateTest.java
@@ -14,4 +14,13 @@ class ValidateTest {
         });
         assertEquals("자동차 이름의 시작이나 끝에 콤마가 올 수 없습니다.", exception.getMessage());
     }
+
+    @Test
+    void testCarNameEmpty() {
+        String input = "Tom,,Jake";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            Validate.carName(input);
+        });
+        assertEquals("자동차 이름이 비어있습니다.", exception.getMessage());
+    }
 }
