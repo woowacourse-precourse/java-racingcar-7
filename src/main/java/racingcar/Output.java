e55759bfa75ed37eca85e529836467d7005b2c1b
Avocado <ujubongbong@gmail.com>
2024-10-26 20:05:13 +0900

    :sparkles: feat: 실행 결과를 출력하는 함수

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.List;
+
 public class Output {
     public void printInit() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -7,5 +9,13 @@ public class Output {
 
     public void printAttemptCountMessage() {
         System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+    
+    public void printRacingResult(List<Car> cars) {
+        System.out.println("실행 결과");
+        for(Car car : cars) {
+           car.printMoveResult();
+        }
+        System.out.println();
     }
 }
