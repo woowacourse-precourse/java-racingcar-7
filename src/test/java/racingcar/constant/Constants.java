4588c6f2d0d8addcd09e9ba29d0a320ed6935b45
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-27 01:58:40 +0900

    feat($Constants): 테스트 전역에서 사용할 상수 클래스 추가
    - RacingCar 의 최소 이동 가능 숫자.
    - RacingCar 의 기본 위치.

---
+++ src/test/java/racingcar/constant/Constants.java
@@ -0,0 +1,10 @@
+package racingcar.constant;
+
+public final class Constants {
+
+    // 레이싱 자동차의 최소 이동 가능 숫자
+    public static final int RACING_CAR_MIN_MOVABLE_NUMBER = 4;
+
+    // 레이싱 자동차의 기본 위치
+    public static final int RACING_CAR_DEFAULT_POSITION = 0;
+}
