38216cd32c3f6ef4a5a59fc2e41470a108b2fa2d
dongri <l0o0lv@github.com>
2024-10-26 23:03:15 +0900

    feat(Car): 경주에 필요한 자동차 객체 추가

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+public class Car {
+    private String name;
+    private int position = 0;
+
+    public Car(String name){
+        this.name = name;
+    }
+
+    public void moveForward(){
+        this.position++;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+}
