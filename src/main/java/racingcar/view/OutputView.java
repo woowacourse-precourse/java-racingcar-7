03f4d9510481f484c9dc6a0dd56987859980a151
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-26 23:44:36 +0900

    feat(OutputView): 결과 안내 메시지 출력 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,15 @@
 package racingcar.view;
 
-public class OutputView {
+public class OutputView implements View {
+
+    private static final String OUTPUT_WINNER_MESSAGE = "최종 우승자 : ";
+
+    @Override
+    public void print() {
+        System.out.print(OUTPUT_WINNER_MESSAGE);
+    }
+
+    public void printWinner(String winnerList) {
+        System.out.println(winnerList);
+    }
 }
