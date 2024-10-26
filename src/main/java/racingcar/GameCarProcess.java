335dc1627c33e0641ba32c57ad056aad3f74a988
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-26 16:38:04 +0900

    feat: 게임이 시행되는 클래스 생성
    -게임 진행을 위해 자동차 객체리스트와 시도횟수를 파라미터로 받아옴
    -출력문구를 출력하고 게임 진행
    -자동차 객체리스트를 돌며 난수를 받고 전진하고 과정을 출력
    -이 과정을 차수별로 진행함
    -객체별 리스트의 길이를 계산하여 최종 우승자를 출력함

---
+++ src/main/java/racingcar/GameCarProcess.java
@@ -0,0 +1,49 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameCarProcess {
+    private List<GameCar> gameCarList;
+    private int count;
+
+    public GameCarProcess(List<GameCar> gameCarList, int count) {
+        this.gameCarList = gameCarList;
+        this.count = count;
+    }
+
+    void gameProgress() {
+        System.out.println("실행 결과");
+        for (int i = 0; i < count; i++) {
+            gameCarProgress();
+            System.out.println("\n");
+        }
+        findWinner();
+    }
+
+    void gameCarProgress() {
+        for (GameCar gameCar : gameCarList) {
+            int randNum = RandNumPick.pickRandomNumber();
+            gameCar.move(randNum);
+            gameCar.displayPrint();
+        }
+    }
+
+    void findWinner() {
+        int maxWinner = 0;
+        for (GameCar gameCar : gameCarList) {
+            int current = gameCar.getForwardMarksSize();
+            if (current > maxWinner) {
+                maxWinner = current;
+            }
+        }
+        List<String> winners = new ArrayList<>();
+        for (GameCar gameCar : gameCarList) {
+            if (gameCar.getForwardMarksSize() == maxWinner) {
+                winners.add(gameCar.getCarName());
+            }
+        }
+        System.out.print("최종 우승자 : ");
+        System.out.println(String.join(", ", winners));
+    }
+}
