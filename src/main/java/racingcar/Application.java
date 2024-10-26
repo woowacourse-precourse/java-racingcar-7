a1ad65ed5011a78968c6013795c0b0fa7afaa928
MinGwan Kim <fpem3309@gmail.com>
2024-10-26 12:27:43 +0900

    feat: 자동차 이름 중복 제거 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Application {
@@ -12,11 +13,7 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputVehicles = Console.readLine();
 
-        List<Vehicle> vehicles = Arrays.stream(inputVehicles.split(","))
-                .map(String::trim)
-                .filter(name -> !name.isEmpty())
-                .map(Vehicle::new)
-                .toList();
+        List<Vehicle> vehicles = createUniqueVehicles(inputVehicles);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
 
@@ -26,6 +23,23 @@ public class Application {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("경주 횟수는 숫자여야 합니다. 올바른 값을 입력해주세요.");
         }
+    }
+
+    private static List<Vehicle> createUniqueVehicles(String inputVehicles) {
+        List<String> vehicles = Arrays.stream(inputVehicles.split(","))
+                .map(String::trim)
+                .filter(name -> !name.isEmpty())
+                .toList();
+
+
+        Set<String> uniqueNames = Set.copyOf(vehicles);
+        if (uniqueNames.size() < vehicles.size()) {
+            throw new IllegalArgumentException("중복된 이름이 있습니다.");
+        }
+
+        return vehicles.stream()
+                .map(Vehicle::new)
+                .toList();
     }
 
     private static void raceStart(List<Vehicle> vehicles, int raceRounds) {
