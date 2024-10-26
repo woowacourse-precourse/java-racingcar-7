2209e60baf5c1c42c0a8f2a8be673c9d2c205aa8
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2024-10-27 01:35:36 +0900

    refactor(dto): raceResultDTO의 데이터변환 로직을 제거한다
    result을 반환하는 raceResultDTO의 데이터 변환 로직을 제거하여 dto의 역할 책임을 줄인다. domain value를 string으로 변환하는 로직은 controller에서 시행하도록 한다.
    
    BREAKING CHANGE: dto에서 반환하는 value는 controller에서 formatting한다

--- src/main/java/racingcar/dto/RaceResultDTO.java
+++ src/main/java/racingcar/dto/RaceResultDTO.java
@@ -2,26 +2,23 @@ package racingcar.dto;
 
 import java.util.List;
 import racingcar.domain.car.Car;
+import racingcar.domain.race.Race;
 
 public class RaceResultDTO {
 
+    private final List<Race> races;
     private final List<Car> raceWinners;
 
-    public RaceResultDTO(List<Car> raceWinners) {
+    public RaceResultDTO(List<Race> races, List<Car> raceWinners) {
+        this.races = races;
         this.raceWinners = raceWinners;
     }
 
-    public String convertWinnersToString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (Car car : raceWinners) {
-            stringBuilder.append(" ");
-            stringBuilder.append(car.getMember().getName());
-            stringBuilder.append(",");
-        }
-        if (!stringBuilder.isEmpty()) {
-            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-        }
-        return stringBuilder.toString();
+    public List<Car> getRaceWinners() {
+        return raceWinners;
     }
 
+    public List<Race> getRaces() {
+        return races;
+    }
 }
