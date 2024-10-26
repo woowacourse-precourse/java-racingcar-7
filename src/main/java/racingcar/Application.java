6532267a0eb6cafae580352f8890999511beed58
bill291104 <bill29123@gmail.com>
2024-10-26 20:49:41 +0900

    feat(RacingcarUser): make RacingcarUser for demo
    Make RacingcarUser class to demonstrate the racingcar system
    - What RacingcarUser do
    -- Set race
    -- Start race
    -- See result of the game

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,14 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        RacingcarUser racingcarUser = new RacingcarUser();
+
+        racingcarUser.setRace();
+
+        racingcarUser.startRace();
+
+        racingcarUser.showResult();
     }
+
 }
