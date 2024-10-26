68e3951a49ddc859781aceb255bf81637f865289
imscow11253 <seaweed8759@naver.com>
2024-10-27 01:28:07 +0900

    feat : 우승자 출력 로직 구현

--- src/main/java/racingcar/common/OutputController.java
+++ src/main/java/racingcar/common/OutputController.java
@@ -1,5 +1,9 @@
 package racingcar.common;
 
+import racingcar.model.CarName;
+
+import java.util.List;
+
 public class OutputController {
     public static void printCarNamePromptMessage(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -7,5 +11,13 @@ public class OutputController {
 
     public static void printRaceCountPromptMessage(){
         System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+    public static void printWinnerName(List<CarName> carNameList){
+        List<String> carNameStringList = carNameList.stream()
+                .map(CarName::carName)
+                .toList();
+
+        System.out.println(String.join(", ", carNameStringList));
     }
 }
