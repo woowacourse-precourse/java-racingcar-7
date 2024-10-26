efbc52a8f7514a6e86c76e890334c1e33a2831b9
ratatuwee <pkqwaszx@gmail.com>
2024-10-26 18:46:19 +0900

    feat(car race controller): 자동차 레이싱 컨트롤러 기능 추가
    리드미에 정의한 내용에 맞춰 중복된 값을 제한하고, 우승자를 반환하는 기능을 추가하였습니다.
    추가적으로 이후에 viewout클래스 개발 시 중간과정을 출력하는 함수를 구현해야 합니다.

--- src/main/java/racingcar/controller/CarRaceController.java
+++ src/main/java/racingcar/controller/CarRaceController.java
@@ -25,6 +25,7 @@ public class CarRaceController {
 
 
     public CarRaceController(List<String> carsName){
+        checkDuplicateCarsName(carsName);
         playCars = setCars(carsName);
         checkMinMAXCarCount();
     }
@@ -41,12 +42,22 @@ public class CarRaceController {
         if(playCars.size() < CarCountBoundary.MIN_CAR_COUNT.getMessage()) throw new IllegalArgumentException("경주에 필요한 최소 차량이 부족합니다.");
         if(playCars.size() > CarCountBoundary.MAX_CAR_COUNT.getMessage()) throw new IllegalArgumentException("경주에 사용되는 차량이 너무 많습니다.");
     }
+    private void checkDuplicateCarsName(List<String> carsName){
+        if(carsName.stream().distinct().count() != carsName.size()) throw new IllegalArgumentException("중복된 자동차 이름이 있습니다.");
+    }
     private int getRandomSingleDigit(){
         return Randoms.pickNumberInRange(0, 9);
     }
     private boolean checkStepForward(int randomDigit){
         if(randomDigit >= 4) return true;
         return false;
+    }
+    private int getWinnerStepCount(){
+        int winnerStep = 0;
+        for(Car car : playCars){
+            if(car.getStepCount()>winnerStep) winnerStep = car.getStepCount();
+        }
+        return winnerStep;
     }
 
     public void playRaceOnce(){
@@ -55,5 +66,16 @@ public class CarRaceController {
                 car.nextStep();
             }
         }
+    }
+    public List<String> getRaceWinners(){
+        List<String> winners = new ArrayList<>();
+        int winnerStep = getWinnerStepCount();
+        for(Car car : playCars){
+            if(car.getStepCount() == winnerStep) winners.add(car.getCarName());
+        }
+        return winners;
+    }
+    public void raceDisplay(){
+        //이후 중간 과정 출력 시 사용
     }
 }
