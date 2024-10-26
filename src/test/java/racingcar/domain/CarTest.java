fd21ad9aaf99c2657a26cf2ceb8594f350e7a8fb
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-26 15:54:18 +0900

    test : 예외 메시지 상수로 변경

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,6 +1,8 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.domain.ErrorMessage.CAR_NAME_BLANK;
+import static racingcar.domain.ErrorMessage.CAR_NAME_MAX;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,9 +11,9 @@ class CarTest {
     @Test
     void 자동차_이름길이_범위_초과_예외처리() {
 
-        assertThatThrownBy(() -> new Car("가마바사아아아"))
+        assertThatThrownBy(() -> new Car("123456"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름은 5자 이하까지 입력 가능합니다.");
+                .hasMessageContaining(CAR_NAME_MAX.getMessage());
     }
 
     @Test
@@ -19,7 +21,7 @@ class CarTest {
 
         assertThatThrownBy(() -> new Car(""))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("공백으로 입력된 이름이 있습니다.");
+                .hasMessageContaining(CAR_NAME_BLANK.getMessage());
     }
 
     @Test
