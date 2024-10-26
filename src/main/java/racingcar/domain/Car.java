26e8d5c92945c79174b9ada62d5a29bf3f5f1b85
차왕준 <ckdhdkwns@gmail.com>
2024-10-26 21:12:53 +0900

    feat(Car): 무작위 숫자가 4 이상인지 판단하는 기능

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,10 +1,16 @@
 package racingcar.domain;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Car {
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    private boolean isMovable() {
+        return pickNumberInRange(0, 9) >= 4;
     }
 }
