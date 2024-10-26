4379ce0f31ea58a9184bb2e8ee70f840fd4b6e67
@_.mung <wjdahrrla@gmail.com>
2024-10-26 22:54:49 +0900

    Feat(CarNameInput.java): 자동차 이름 입력 기능을 구현해라
    앞에서 작성한 자동차 이름 입력 테스트 코드 기반으로 기능을 구현함.
    
    구현한 내용:
    - 입력 값에 대한 유효성을 검사하는 기능 구현
    - 입력 값을 쉼표를 기준으로 자동차 이름을 분리하는 기능 구현
    - 자동차 이름에 대한 유효성을 검사하는 기능 구현
    - 자동차 이름 유효성 통과가 된다면 Set에 저장하는 기능 구현
    - 명확한 예외 처리를 위해 CarNameInputErrorMessage.java 추가
    - CarNameInputTest.java를 위 ErrorMessage 기반으로 리팩토링

--- src/test/java/racingcar/view/input/CarNameInputTest.java
+++ src/test/java/racingcar/view/input/CarNameInputTest.java
@@ -2,86 +2,105 @@ package racingcar.view.input;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.exception.view.CarNameInputErrorMessage;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CarNameInputTest {
     @Test
     @DisplayName("입력된 자동차 이름이 유효하면 자동차 리스트 생성")
     void 입력된_자동차_이름이_유효하면_자동차_리스트_생성() {
         // given
-        String input = "pobi,woni,jun";
+        String carName = "pobi,woni,jun";
 
         // when
         CarNameInput carNameInput = new CarNameInput();
 
         // then
-        assertDoesNotThrow(() -> carNameInput.processInput(input));
+        assertDoesNotThrow(() -> carNameInput.process(carName));
+    }
+
+    @Test
+    @DisplayName("입력된 자동차 이름이 빈 문자열이면 예외 처리")
+    void 입력된_자동차_이름이_빈_문자열이면_예외_처리() {
+        // given
+        String carName = "";
+
+        // when
+        CarNameInput carNameInput = new CarNameInput();
+
+        // then
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> carNameInput.process(carName));
+        assertEquals(CarNameInputErrorMessage.CAR_NAME_IS_EMPTY.getMessage(), exception.getMessage());
     }
 
     @Test
     @DisplayName("자동차 이름이 5자를 초과하면 예외 발생")
     void 자동차_이름이_5자를_초과하면_예외_발생() {
         // given
-        String input = "pobi,woni,junnnnn";
+        String carName = "pobi,woni,junnnnn";
 
         // when
         CarNameInput carNameInput = new CarNameInput();
 
         // then
-        assertThrows(IllegalArgumentException.class, () -> carNameInput.processInput(input));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> carNameInput.process(carName));
+        assertEquals(CarNameInputErrorMessage.CAR_NAME_TOO_LONG.getMessage(), exception.getMessage());
     }
 
     @Test
     @DisplayName("자동차 이름이 소문자 영어가 아니라면 예외 발생")
     void 자동차_이름이_소문자_영어가_아니라면_예외_발생() {
         // given
-        String input = "pobi,wonI,junnnnn";
+        String carName = "pobi,wonI,junnnnn";
 
         // when
         CarNameInput carNameInput = new CarNameInput();
 
         // then
-        assertThrows(IllegalArgumentException.class, () -> carNameInput.processInput(input));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> carNameInput.process(carName));
+        assertEquals(CarNameInputErrorMessage.CAR_NAME_NOT_LOWERCASE.getMessage(), exception.getMessage());
     }
 
     @Test
     @DisplayName("자동차 이름에 공백이 있으면 예외 발생")
     void 자동차_이름에_공백이_있으면_예외_발생() {
-        // give
-        String input = "pobi,wo n";
+        // given
+        String carName = "pobi,wo n";
 
         // when
         CarNameInput carNameInput = new CarNameInput();
 
         // then
-        assertThrows(IllegalArgumentException.class, () -> carNameInput.processInput(input));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> carNameInput.process(carName));
+        assertEquals(CarNameInputErrorMessage.CAR_NAME_CONTAINS_WHITESPACE.getMessage(), exception.getMessage());
     }
 
     @Test
     @DisplayName("중복된 자동차 이름이 있으면 예외 발생")
     void 중복된_자동차_이름이_있으면_예외_발생() {
         // given
-        String input = "pobi,jun,pobi";
+        String carName = "pobi,jun,pobi";
 
         // when
         CarNameInput carNameInput = new CarNameInput();
 
         // then
-        assertThrows(IllegalArgumentException.class, () -> carNameInput.processInput(input));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> carNameInput.process(carName));
+        assertEquals(CarNameInputErrorMessage.CAR_NAME_DUPLICATE.getMessage(), exception.getMessage());
     }
 
     @Test
     @DisplayName("쉼표가 아닌 다른 구분자가 있으면 예외 발생")
     void 쉼표가_아닌_다른_구분자가_있으면_예외_발생() {
         // given
-        String input = "pobi,wonI:jin";
+        String carName = "pobi,wonI:jin";
 
         // when
         CarNameInput carNameInput = new CarNameInput();
 
         // then
-        assertThrows(IllegalArgumentException.class, () -> carNameInput.processInput(input));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> carNameInput.process(carName));
+        assertEquals(CarNameInputErrorMessage.INVALID_DELIMITER.getMessage(), exception.getMessage());
     }
 }
