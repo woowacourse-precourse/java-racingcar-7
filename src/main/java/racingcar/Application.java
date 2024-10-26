dc5f2b32499bf34d9d307b99968210d82118044f
jjr7181 <jjr7181@gmail.com>
2024-10-26 16:28:21 +0900

    feat: implement movement count input for racing game

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,5 +7,8 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼포(,) 기준으로 구분)");
         String players = Console.readLine();
 
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int rounds = Integer.parseInt(Console.readLine());
+
     }
 }
