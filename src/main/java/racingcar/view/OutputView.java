13339c5a56b2f23f9e4efb6edd68f9a00eede760
zjhj0814 <zjhj0814@gmail.com>
2024-10-27 02:39:52 +0900

    feat(OutputView): 차수별 실행 결과 출력 기능 작성
    RacingCar 리스트를 받아서 리스트의 모든 RacingCar에 대한 실행 결과를 정해진 형식대로 출력한다

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,9 +1,25 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.domain.RacingCar;
+
 public class OutputView {
     private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String ATTEMPT_RESULT_MESSAGE_SEPARATOR = " : ";
+    private static final String ATTEMPT_RESULT_MESSAGE_ADVANCE_INDICATOR = "-";
 
     public void printResult() {
         System.out.println(RESULT_MESSAGE);
+    }
+
+    public void printAttemptResult(List<RacingCar> cars) {
+        for (RacingCar car : cars) {
+            System.out.println(getAttemptResult(car));
+        }
+    }
+
+    private String getAttemptResult(RacingCar car) {
+        return car.getCarName() + ATTEMPT_RESULT_MESSAGE_SEPARATOR
+                + ATTEMPT_RESULT_MESSAGE_ADVANCE_INDICATOR.repeat(car.getAdvanceRecord());
     }
 }
