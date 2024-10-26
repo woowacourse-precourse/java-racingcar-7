237ede160f2c1a81a30bd88d11f1141e0933c70f
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-10-26 22:05:58 +0900

    refactor: 랜덤값 테스트를 위한 수정

--- src/main/java/racingcar/util/RandomGenerator.java
+++ src/main/java/racingcar/util/RandomGenerator.java
@@ -1,10 +1,5 @@
 package racingcar.util;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-
-public class RandomGenerator {
-    public static int picNumber(){
-        return Randoms.pickNumberInRange(0, 9);
-    }
+public interface RandomGenerator {
+    int nextInt(int bound);
 }
