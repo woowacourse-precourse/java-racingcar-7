0099c197fcecbc6f430325bc687940db1a8e965d
GS_song98 <20003204@sju.ac.kr>
2024-10-27 00:28:39 +0900

    feat: 공백 문자열 예외 추가

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -7,8 +7,19 @@ public class Name {
     private final String name;
 
     public Name(String name) {
-        validateNameLength(name);
+        validateName(name);
         this.name = name;
+    }
+
+    private void validateName(String name) {
+        validateBlank(name);
+        validateNameLength(name);
+    }
+
+    private void validateBlank(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름에는 빈 문자만으로 구성된 문자열이 들어올 수 없습니다.");
+        }
     }
 
     private void validateNameLength(String name) {
