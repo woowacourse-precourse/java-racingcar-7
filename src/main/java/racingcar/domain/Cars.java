cd9510d4138f3d415bc047fa01a3f7482558a8bd
bowook <mbosx316@naver.com>
2024-10-26 19:41:21 +0900

    refactor(Cars) : Cars 도메인 리팩토링
    - 변수명 수정

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,18 +1,17 @@
 package racingcar.domain;
 
-import racingcar.domain.validator.CarNameValidator;
-
 import java.util.List;
 import java.util.stream.Collectors;
+import racingcar.domain.validator.CarNameValidator;
 
 public class Cars {
     private static final String COMMA = ",";
     private final List<Car> cars;
 
     public Cars(String carNames, CarNameValidator carNameValidator) {
-        List<String> convertedStringToList = List.of(carNames.split(COMMA));
-        carNameValidator.validate(convertedStringToList);
-        this.cars = convertedStringToList.stream().map(Car::new).collect(Collectors.toList());
+        List<String> toList = List.of(carNames.split(COMMA));
+        carNameValidator.validate(toList);
+        this.cars = toList.stream().map(Car::new).collect(Collectors.toList());
     }
 
     public List<Car> getCars() {
