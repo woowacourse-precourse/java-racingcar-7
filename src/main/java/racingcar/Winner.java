70707bb8c4da337772c171de4b70d67b38ec9dee
rhxfkg <rhxfkg@gmail.com>
2024-10-27 00:15:27 +0900

    Update Winner.java

---
+++ src/main/java/racingcar/Winner.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+public class Winner {
+    public void checkWinner(int[] racingResult, int[] winnerCount) {
+        int maxValue = findMaxValue(racingResult);
+        for (int i = 0; i < racingResult.length; i++) {
+            if (maxValue == racingResult[i]) {
+                winnerCount[i]++;
+            }
+        }
+    }
+
+    public void printFinalWinner(String[] carList, int[] winnerCount) {
+        int maxValue = findMaxValue(winnerCount);
+        for (int i = 0; i < winnerCount.length; i++) {
+            if (maxValue == winnerCount[i]) {
+                System.out.println(carList[i]);
+            }
+        }
+    }
+
+    public int findMaxValue(int[] racingResult) {
+        int maxValue = -1;
+        for (int i = 0; i < racingResult.length; i++) {
+            if (maxValue < racingResult[i]){
+                maxValue = racingResult[i];
+            }
+        }
+        return maxValue;
+    }
+}
