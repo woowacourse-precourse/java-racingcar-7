6378571f275418b9308c2ba70df8c1165a743b39
Ahnsukjoo <bos98@naver.com>
2024-10-26 18:17:41 +0900

    style: 출력시 불필요한 줄바꿈 삭제
    - 우테코 미션에 맞게 출력문에 나오는 줄바꿈을 수정

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -22,9 +22,9 @@ public class GameController {
     public void start() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         raceStatus = input.getCarNames();
-        System.out.println("시도할 횟수는 몇 회인가요?\n");
+        System.out.println("시도할 횟수는 몇 회인가요?");
         attemptedNumber = input.getAttemptedNumber();
-        System.out.println("실행 결과");
+        System.out.println("\n실행 결과");
         for (int i = 0; i < attemptedNumber; i++) {
             raceStatus = game.play(raceStatus);
             output.printProgressByStage(raceStatus);
