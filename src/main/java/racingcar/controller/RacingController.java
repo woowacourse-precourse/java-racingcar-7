7e1b55002f6141d15e1e565d9ecf11dcf385b35c
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-10-26 14:57:49 +0900

    feat(controller, application): 콘솔 프로그램 동작 구현
    * RacingController.java: 작성된 domain 과 view 에 따라, 실행 결과처럼 나올 수 있도록 구현한다.
    * Application.java: 최종 프로그램 구현 동작을 위한 코드를 구현한다.

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,24 +1,64 @@
 package racingcar.controller;
 
 import java.util.List;
+import racingcar.domain.Car;
 import racingcar.domain.CarName;
 import racingcar.domain.CarNameParser;
 import racingcar.domain.Cars;
+import racingcar.domain.RacingChance;
+import racingcar.domain.random.RandomNumberGenerator;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingController {
 
     private final InputView inputView;
+    private final OutputView outputView;
+    private final RandomNumberGenerator randomNumberGenerator;
 
-    public RacingController(InputView inputView) {
+    public RacingController(InputView inputView, OutputView outputView, RandomNumberGenerator randomNumberGenerator) {
         this.inputView = inputView;
+        this.outputView = outputView;
+        this.randomNumberGenerator = randomNumberGenerator;
     }
 
-    private Cars makeCars(){
+    public void run() {
+        Cars cars = makeCars();
+        RacingChance racingChance = getChance();
+        outputView.raceGuide();
+        raceRoundsByChance(cars, racingChance);
+        findWinner(cars);
+    }
+
+    private void findWinner(Cars cars) {
+        outputView.findWinner(cars.getWinner());
+    }
+
+    private void raceRoundsByChance(Cars cars, RacingChance racingChance) {
+        for (int i = 0; i < racingChance.getRacingChance(); i++) {
+            raceOneRound(cars);
+        }
+    }
+
+    private void raceOneRound(Cars cars) {
+        for (Car car : cars.getCars()) {
+            car.moveCar(randomNumberGenerator.generateRandomNumber());
+            outputView.raceResult(car.getCarName().getCarName(), car.getPosition());
+        }
+        outputView.separateLine();
+    }
+
+    private Cars makeCars() {
         return new Cars(getCarNames());
     }
 
     private List<CarName> getCarNames() {
+        outputView.getCarNames();
         return CarNameParser.parseCarNames(inputView.getCarName());
+    }
+
+    private RacingChance getChance() {
+        outputView.getRacingChance();
+        return new RacingChance(inputView.getRacingChance());
     }
 }
