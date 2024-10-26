f68d35a318cf9a7007adb63c8a93f414c438d444
sinminseok12 <sinminseok12@naver.com>
2024-10-27 01:43:39 +0900

    test : 레이싱 시도 횟수 초기값 검증 기능 테스트
    AttemptCount 의 MIN_ATTEMPT_COUNT 를 0 -> 1로 변경했다

--- src/main/java/racingcar/model/AttemptCount.java
+++ src/main/java/racingcar/model/AttemptCount.java
@@ -2,7 +2,7 @@ package racingcar.model;
 
 public class AttemptCount {
     private static final String ERROR_NEGATIVE_OR_ZERO_COUNT = "시도 횟수는 0 이상이어야 합니다.";
-    private static final int MIN_ATTEMPT_COUNT = 0;
+    private static final int MIN_ATTEMPT_COUNT = 1;
 
     private int count;
 
