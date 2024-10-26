46592463dfd14bc85db60d5851987330c14ff20c
Taekgil <taekgil273@gmail.com>
2024-10-26 09:41:02 +0900

    feat($OutputView): OutputView 구현
    사용자에게 보여줄 수 있는 문자열을 출력하는 클래스 구현
    - 기본 문자 출력 기능
    - 입력 요청 문자 출력
    - 라운드 별 결과 출력
    - 최종 경기 결과 출력

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,33 @@
+package racingcar.view;
+
+public class OutputView {
+
+    private static final String ASK_INPUT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+    private static final String ROUND_RESULT = "실행 결과";
+    private static final String GAME_RESULT = "최종 우승자 : ";
+
+    public void printAskInput(InputType type) {
+        if (type == InputType.CAR_NAME) {
+            String ASK_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+            printMessage(ASK_INPUT_CAR_NAME);
+        } else if (type == InputType.TRY_COUNT) {
+            printMessage(ASK_INPUT_TRY_COUNT);
+        }
+    }
+
+    public void responseRoundResult(StringBuilder sb) {
+        System.out.println(ROUND_RESULT + "\n" + sb.toString());
+    }
+
+    public void responseGameResult(String winner) {
+        System.out.println(GAME_RESULT + winner);
+    }
+
+    private void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public enum InputType {
+        CAR_NAME, TRY_COUNT;
+    }
+}
