dec048b2be00ee5b16dd395fde59aa399aa7163b
Etwashoeren <sungmin0609@nate.com>
2024-10-26 23:02:35 +0900

    feat. RaceController 클래스 내 메소드 추가

--- src/main/java/racingcar/RaceController.java
+++ src/main/java/racingcar/RaceController.java
@@ -2,5 +2,21 @@ package racingcar;
 
 public class RaceController {
 
+    private Race race;
+    private UserView userView;
 
+    public RaceController(UserView userView) {
+        this.userView = userView;
+    }
+
+    public void run() {
+        String[] carNames = userView.createCars();
+        int round = userView.inputRoundCount();
+
+        race = new Race(carNames);
+        race.proceedRace(round);
+
+        userView.displayRace(race.getCars());
+        userView.displayWinner(race.getWinners());
+    }
 }
