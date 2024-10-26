dfb69f667372de225bc02edc0ee053fda38263da
JoonKyo <joonkyolee@gmail.com>
2024-10-27 03:51:44 +0900

    feat(CarName): 자동차 이름의 길이가 허용가능한 길이를 초과하는지 검증하는 메서드 구현

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -3,7 +3,9 @@ package racingcar.domain;
 public class CarName {
     private static final String ERROR_MESSAGE_FOR_BLANK = "자동차 이름에 공백이 존재합니다. 프로그램을 종료합니다.";
     private static final String ERROR_MESSAGE_LESS_THAN_MIN_LENGTH = "자동차 이름의 길이가 0입니다. 프로그램을 종료합니다.";
+    private static final String ERROR_MESSAGE_OVER_MAX_LENGTH = "자동차 이름이 5글자보다 깁니다. 프로그램을 종료합니다.";
     private static final String BLANK = " ";
+    private static final int MAX_LENGTH = 5;
     private final String carName;
 
     private CarName(String carName) {
@@ -23,6 +25,12 @@ public class CarName {
     private static void validateNoName(String carName) {
         if (carName.length() == 0) {
             throw new IllegalArgumentException(ERROR_MESSAGE_LESS_THAN_MIN_LENGTH);
+        }
+    }
+
+    private static void validateOverLength(String carName) {
+        if (carName.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_OVER_MAX_LENGTH);
         }
     }
 }
