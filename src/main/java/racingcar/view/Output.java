50d6958fc52694d013eb080cc25228b588ed22bd
Ahnsukjoo <bos98@naver.com>
2024-10-26 15:15:55 +0900

    feat: 단계별 경주 진행 상태 출력
    - 각 시도 후 현재 진행 상태를 출력하는 기능
    - 자동차가 진행된 횟수를 "-"의 갯수로 표현하도록 바꿔주는 기능 구현

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -6,11 +6,18 @@ import java.util.List;
 public class Output {
 
     public void printProgressByStage(HashMap<String, Integer> raceStatus) {
-
+        for (String key : raceStatus.keySet()) {
+            System.out.println(key + " : " + returnString(raceStatus.get(key)));
+        }
+        System.out.println();
     }
 
     public String returnString(int length) {
-
+        StringBuilder hyphen = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            hyphen.append("-");
+        }
+        return hyphen.toString();
     }
 
     public void printWinners(List<String> winners) {
