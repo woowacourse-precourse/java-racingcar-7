df56491c96a943df3390bafd354583b4c701107b
jimmy0524 <10jmin04@naver.com>
2024-10-27 03:15:39 +0900

    refactor: 예외처리 기능 리팩토링
    - 예외처리 기능 메서드 세분화 작업

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -4,44 +4,72 @@ import racingcar.exception.ExceptionMessage;
 
 public class InputValidator {
     public void validateCarNames(String carNames) {
-        if (carNames == null || carNames.trim().isEmpty()) {
-            throw new IllegalArgumentException(ExceptionMessage.EMPTY_CAR_NAMES.getMessage());
-        }
-        if (!carNames.contains(",")) {
-            throw new IllegalArgumentException(ExceptionMessage.INSUFFICIENT_CARS.getMessage());
-        }
-        if (carNames.trim().startsWith(",") || carNames.trim().endsWith(",")) {
-            throw new IllegalArgumentException(ExceptionMessage.BOUNDARY_DELIMITER.getMessage());
-        }
+        checkEmptyCarNames(carNames);
+        checkMinimumCarCount(carNames);
+        checkBoundary(carNames);
         String[] splitCarNames = carNames.split(",");
+
         for (String name : splitCarNames) {
-            if (name.trim().isEmpty()) {
-                throw new IllegalArgumentException(ExceptionMessage.INVALID_CAR_NAME.getMessage());
-            }
-            if (name.trim().length() > 5) {
-                throw new IllegalArgumentException(ExceptionMessage.TOO_LONG_NAME.getMessage());
-            }
+            checkCarNameValid(name);
+            checkCarNameLength(name);
         }
     }
 
     public void validateAttemptCount(String count) {
-        if (count == null || count.trim().isEmpty()) {
-            throw new IllegalArgumentException(ExceptionMessage.EMPTY_ATTEMPT_COUNT.getMessage());
-        }
+        validateEmptyCount(count);
 
         try {
             int parsedCount = Integer.parseInt(count);
-
-            if (parsedCount < 1) {
-                throw new IllegalArgumentException(ExceptionMessage.OUT_OF_BOUNDS_ATTEMPT_COUNT.getMessage());
-            }
-            // 1000을 초과하는 경우 예외 발생
-            if (parsedCount > 1000) {
-                throw new IllegalArgumentException(ExceptionMessage.OUT_OF_BOUNDS_ATTEMPT_COUNT.getMessage());
-            }
+            checkCountBounds(parsedCount);
 
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ExceptionMessage.INVALID_ATTEMPT_COUNT.getMessage());
+            throw checkCountValid();
         }
+    }
+
+    private void checkEmptyCarNames(String carNames) {
+        if (carNames == null || carNames.trim().isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessage.EMPTY_CAR_NAMES.getMessage());
+        }
+    }
+
+    private void checkMinimumCarCount(String carNames) {
+        if (!carNames.contains(",")) {
+            throw new IllegalArgumentException(ExceptionMessage.INSUFFICIENT_CARS.getMessage());
+        }
+    }
+
+    private void checkBoundary(String carNames) {
+        if (carNames.trim().startsWith(",") || carNames.trim().endsWith(",")) {
+            throw new IllegalArgumentException(ExceptionMessage.BOUNDARY_DELIMITER.getMessage());
+        }
+    }
+
+    private void checkCarNameValid(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_CAR_NAME.getMessage());
+        }
+    }
+
+    private void checkCarNameLength(String name) {
+        if (name.trim().length() > 5) {
+            throw new IllegalArgumentException(ExceptionMessage.TOO_LONG_NAME.getMessage());
+        }
+    }
+
+    private static void validateEmptyCount(String count) {
+        if (count == null || count.trim().isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessage.EMPTY_ATTEMPT_COUNT.getMessage());
+        }
+    }
+
+    private static void checkCountBounds(int parsedCount) {
+        if (parsedCount < 1 || parsedCount > 1000) {
+            throw new IllegalArgumentException(ExceptionMessage.OUT_OF_BOUNDS_ATTEMPT_COUNT.getMessage());
+        }
+    }
+
+    private static IllegalArgumentException checkCountValid() {
+        return new IllegalArgumentException(ExceptionMessage.INVALID_ATTEMPT_COUNT.getMessage());
     }
 }
