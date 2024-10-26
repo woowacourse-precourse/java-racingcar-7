9c4bb747eb276be612df69121751aa3bf95f0ace
un-known0 <nam0221@naver.com>
2024-10-27 01:21:25 +0900

    feat(Race): 자동차 목록 생성 기능 추가
    Race 클래스에 자동차 목록을 생성하는 생성자를 추가하였습니다.
    - 문자열 입력을 받아 자동차 이름으로 Car 객체를 생성하는 기능 구현
    - List<String>을 인자로 받아 Car 객체 목록으로 변환

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+
+    List<Car> cars = new ArrayList<>();
+
+    public Race(List<String> names){
+        cars = names.stream().map(Car::new).toList();
+    }
+
+    public Race(String input){
+        this(List.of(input.split(",")));
+    }
+
+}
