2d2527055b52d604396f632043a05bed0647037e
hjg1473 <hjg1473@naver.com>
2024-10-26 20:29:12 +0900

    feat: add go or stop function
    If the value is 4 or more, it returns false, which means stop, if it is not true, which means advance.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
@@ -14,8 +15,12 @@ public class Application {
         String[] cars = input.split(",");
         VaildInput(cars, stringNumber);
         int number = Integer.parseInt(stringNumber);
+        int random = Randoms.pickNumberInRange(0, 9);
+        System.out.println(random);
+        System.out.println(DecideStopOrNot(random));
     }
 
+    // 1. 잘못된 입력 판단 함수
     private static void VaildInput(String[] cars, String stringNumber) {
         for(int i = 0; i < cars.length; i++) {
             // 5글자 이상이거나, 공백 입력인 경우
@@ -29,4 +34,13 @@ public class Application {
             throw new IllegalArgumentException("IllegalArgumentException");
         }
     }
+
+    // 3. 특정 값을 넣었을 때, 전진할지 멈출지 판단하는 함수
+    // 특정 값이 4 이상일 때 전진 (TRUE) . 아니면 스탑 (FALSE)
+    private static boolean DecideStopOrNot(int value) {
+        return value >= 4;
+    }
+
+
+
 }
