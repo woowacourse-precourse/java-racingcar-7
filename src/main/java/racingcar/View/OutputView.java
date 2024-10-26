66a1f6384a624fecd1528c99c544b06d972a525b
rootTiket <l20078829@gmail.com>
2024-10-27 00:56:12 +0900

    feat<OutputView>: 실행결과 출력 구현

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -1,9 +1,14 @@
 package racingcar.View;
 
+import static racingcar.View.constant.OutputMessage.ROUND_START_MESSAGE;
 import static racingcar.View.constant.OutputMessage.START_MESSAGE;
 
 public class OutputView {
     public void printStart() {
         System.out.println(START_MESSAGE.getMessage());
+    }
+
+    public void printRoundStart() {
+        System.out.println(ROUND_START_MESSAGE.getMessage());
     }
 }
