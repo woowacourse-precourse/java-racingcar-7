49d0014fed310733001c34f17b22294619f47058
Jang-GO <jang8195@naver.com>
2024-10-26 18:39:44 +0900

    feat(CarFactory): 문자열을 추출하여 Car객체 생성

--- src/main/java/racingcar/car/factory/CarFactory.java
+++ src/main/java/racingcar/car/factory/CarFactory.java
@@ -2,10 +2,18 @@ package racingcar.car.factory;
 
 import racingcar.car.Car;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class CarFactory {
     public static Set<Car> createCars(String input) {
+        String[] names = input.split(",");
+        Set<Car> cars = new LinkedHashSet<>();
 
+        for (String name : names) {
+            cars.add(new Car(name.trim()));
+        }
+
+        return cars;
     }
 }
