5cb067bbad29bf7e68b0994e4ca6c490fddfef63
seonwoonam <potatomoney716@gmail.com>
2024-10-27 03:52:48 +0900

    feat(InputTest) : input 클래스 테스트 추가

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -4,19 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Input {
-    private static String inputNameString(){
+    public static List<String> getNameList(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+        String input = Console.readLine();
+        return splitName(input);
     }
 
     public static int inputNumber(){
         System.out.println("시도할 횟수는 몇 회인가요?");
-        return Integer.parseInt(Console.readLine());
+        String input = Console.readLine();
+        if (input == null || input.isEmpty()){
+            throw new IllegalArgumentException("시도할 횟수를 입력해주세요.");
+        }
+        return Integer.parseInt(input);
     }
 
-    public static List<String> getNameList(){
-        String inputNameString = inputNameString();
-
+    public static List<String> splitName(String inputNameString){
         if (!inputNameString.contains(",")) {
             throw new IllegalArgumentException("쉼표 기준으로 구분해주세요.");
         }
