7f99999472eff30cbeff27c56ada04082174843d
Fletacode <bassykd@gmail.com>
2024-10-26 21:57:30 +0900

    feat: 자동차 생성, 전진 기능 추가
    자동차를 생성하면 이름과 거리 클래스를 생성하고 총 전진횟수를 0으로 초기화 합니다.

---
+++ src/main/java/racingcar/model/car/Car.java
@@ -0,0 +1,42 @@
+package racingcar.model.car;
+
+import racingcar.model.decisionNum.DecisionNum;
+
+public class Car {
+
+  private Name name;
+  private Integer totalForwordCount;
+  private Distance distance;
+
+  public Car(String name) {
+    this.name = new Name(name);
+    this.totalForwordCount = 0;
+    this.distance = new Distance();
+  }
+
+  public void moveCar() {
+
+    //앞으로 갈지 안 갈지 결정하는 숫자
+    int decision = GenerateDecisionNum();
+
+    //전진하면 전체 전진횟수 증가
+    if (isGo(decision)) {
+      totalForwordCount++;
+    }
+
+    //거리 문자열 붙이기
+    for (int i = 0; i < decision; i++) {
+      distance.increaseDistance();
+    }
+
+  }
+
+  private int GenerateDecisionNum() {
+    return DecisionNum.GenerateDecisionNum();
+  }
+
+  private boolean isGo(int decisionNum){
+    return DecisionNum.isGo(decisionNum);
+  }
+
+}
