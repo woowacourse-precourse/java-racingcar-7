e1dff16ea132740b13c21dc991e2a92c54b1d9b3
LeeJaeHoon <dlwogns3413@naver.com>
2024-10-26 23:18:19 +0900

    refactor: InputView 상수 사용하게 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
 
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_RACE_ROUND_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String NOT_NUMBER_ERROR_MESSAGE = "숫자를 입력하세요";
 
     public String inputCarNames() {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
@@ -19,7 +20,7 @@ public class InputView {
             String round = Console.readLine();
             return Integer.parseInt(round);
         } catch (NumberFormatException | NoSuchElementException e) {
-            throw new IllegalArgumentException("숫자를 입력하세요", e);
+            throw new IllegalArgumentException(NOT_NUMBER_ERROR_MESSAGE, e);
         }
     }
 }
