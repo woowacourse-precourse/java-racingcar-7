b9dd132ecb76f1b204c1169311b467478e3d1978
Hanjjong <tim4974@naver.com>
2024-10-26 22:37:57 +0900

    test: CarTest 클래스 명 수정 carTest -> CarTest

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class carTest {
+public class CarTest {
 
     private Car car;
 
