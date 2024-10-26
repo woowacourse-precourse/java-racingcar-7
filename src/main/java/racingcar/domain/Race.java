3c9889ad074323081b977541de4a835720a79918
slay <palgalow@gmail.com>
2024-10-27 00:59:59 +0900

    feat(refactor): 우승자 위치 반환하는 기능 및 최종 우승자 반환하는 기능 Race 클래스로 이동
    Race 클래스가 자신의 상태와 행동을 직접 관리하도록 하여 응집도를 높이고, 단일 책임 원칙을 따르도록 개선

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -1,6 +1,8 @@
 package racingcar.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import racingcar.util.RandomNumber;
 import racingcar.util.Validator;
@@ -19,7 +21,30 @@ public class Race {
         return this.tryCount;
     }
 
-    public void playOneRound(List<Car> cars) {
+    public int getWinnerPosition(List<Car> cars) {
+        Car winner = Collections.max(cars, Comparator.comparingInt(car -> car.getPosition()));
+        int winnerPosition = winner.getPosition();
+        return winnerPosition;
+    }
+
+    public List<String> getWinners() {
+        List<String> winners = new ArrayList<>();
+        int winnerPosition = getWinnerPosition(cars);
+        for (Car car : cars) {
+            if (car.getPosition() == winnerPosition) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
+
+    public void playRace() {
+        for (int i = 0; i < tryCount; i++) {
+            playOneRound(cars);
+        }
+    }
+
+    private void playOneRound(List<Car> cars) {
         for (Car car : cars) {
             int randomNumber = RandomNumber.generateRandomNumber();
             if (Validator.isMoreFour(randomNumber)) {
@@ -28,10 +53,6 @@ public class Race {
         }
     }
 
-    public void playRace() {
-        for (int i = 0; i < tryCount; i++) {
-            playOneRound(cars);
-        }
-    }
+
 
 }
--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import racingcar.domain.Car;
+import racingcar.domain.Race;
 import racingcar.util.RandomNumber;
 import racingcar.util.Validator;
 
@@ -13,23 +14,6 @@ public class RaceService {
     public String[] splitCarNames(String carNames) {
         String[] splitNames = carNames.split(",");
         return splitNames;
-    }
-
-    public List<String> decisionWinners(List<Car> cars) {
-        List<String> winners = new ArrayList<>();
-        int winnerPosition = getWinnerPosition(cars);
-        for (Car car : cars) {
-            if (car.getPosition() == winnerPosition) {
-                winners.add(car.getName());
-            }
-        }
-        return winners;
-    }
-
-    public int getWinnerPosition(List<Car> cars) {
-        Car winner = Collections.max(cars, Comparator.comparingInt(car -> car.getPosition()));
-        int winnerPosition = winner.getPosition();
-        return winnerPosition;
     }
 
     public List<Car> createCars(String[] carNames) {
