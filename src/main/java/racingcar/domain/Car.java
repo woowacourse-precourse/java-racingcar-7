e09cbfdb3053436ce9f44b5a03122c4eab46c634
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-10-27 00:02:07 +0900

    fix : move 숫자 랜덤로직 move 메서드 안으로 이동

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,20 +1,25 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+
+    private static final int MOVE_THRESHOLD = 4;
+    private static final int MAX_NAME_LENGTH = 5;
     private final String name;
 
     private int position;
 
     public Car(String name) {
-        if (name.length() > 5) {
+        if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
         }
         this.name = name;
         this.position = 0;
     }
 
-    public void move(boolean canMove){
-        if(canMove){
+    public void move(){
+        if (Randoms.pickNumberInRange(0, 9) >= MOVE_THRESHOLD) {
             position++;
         }
     }
@@ -26,4 +31,5 @@ public class Car {
     public String getName(){
         return name;
     }
+    
 }
