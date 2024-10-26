be12a9fb67ce2e6afd5d4cd9d4f221f9354bb5a9
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-26 17:09:21 +0900

    feat : 입력 문구 출력 추가

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -7,6 +7,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
 
     public static String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         try {
             return Console.readLine();
         } catch (NullPointerException e) {
@@ -15,6 +16,7 @@ public class Input {
     }
 
     public static int inputAttemptNumber() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
         try {
             return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
