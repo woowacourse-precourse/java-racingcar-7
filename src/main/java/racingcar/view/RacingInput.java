f2fdc3f70f806e5df901218b0765a6eddcafcac7
JeongSu Park <bechalrang@gmail.com>
2024-10-26 15:09:35 +0900

    fix(RacingInput): 삭제된 클래스에 대한 의존 코드 제거
    삭제된 `ConsoleOutput`에 대한 의존성과 코드를 콘솔 표준출력 메서드`System.out#println`로 교체

--- src/main/java/racingcar/view/RacingInput.java
+++ src/main/java/racingcar/view/RacingInput.java
@@ -1,7 +1,6 @@
 package racingcar.view;
 
 import racingcar.common.view.ConsoleInput;
-import racingcar.common.view.ConsoleOutput;
 import racingcar.object.value.CarName;
 import racingcar.object.value.CarNames;
 import racingcar.object.value.TotalLapCount;
@@ -9,14 +8,14 @@ import racingcar.object.value.TotalLapCount;
 public class RacingInput {
 
     public static CarNames askCarNamesForParticipants() {
-        ConsoleOutput.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return new CarNames(ConsoleInput.readStrings().stream()
                 .map(CarName::new)
                 .toList());
     }
 
     public static TotalLapCount askTotalLapCount() {
-        ConsoleOutput.println("시도할 횟수는 몇 회인가요?");
+        System.out.println("시도할 횟수는 몇 회인가요?");
         return new TotalLapCount(ConsoleInput.readInteger());
     }
 
