65529fe91bc043ce25f8684fb52d124ec2a4c6a2
차왕준 <ckdhdkwns@gmail.com>
2024-10-27 05:35:29 +0900

    test(OutputViewTest): 진행 상황 출력 테스트

---
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -0,0 +1,28 @@
+package racingcar.view;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.TestCar;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class OutputViewTest {
+    @Test
+    void 진행_상황_출력() {
+        Car car1 = new TestCar("pobi", true);
+        Car car2 = new TestCar("woni", false);
+
+        Cars cars = new Cars(Arrays.asList(car1, car2));
+        OutputView outputView = new OutputView(cars);
+
+        int moveCount = 5;
+        IntStream.range(0, moveCount).forEach(i -> cars.moveAll());
+
+        assertThat(outputView.generateProgress()).isEqualTo("pobi : -----\nwoni : ");
+    }
+}
