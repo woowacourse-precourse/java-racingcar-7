10784cd9b08c1586f8120d50c044aad638a36f26
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-10-26 23:16:13 +0900

    feat : service.RacingGameService play 메서드 구현

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -1,12 +1,17 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import racingcar.domain.Car;
 
 public class RacingGameService {
     public void play(List<Car> cars){
         for (Car car : cars) {
-            if
+            randomMove(car);
         }
+    }
+
+    private void randomMove(Car car){
+        car.move(Randoms.pickNumberInRange(0, 9) >= 4);
     }
 }
