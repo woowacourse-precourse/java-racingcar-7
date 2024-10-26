3f57f8e878313bbd2860a96626931e1b8bb7a685
Fletacode <bassykd@gmail.com>
2024-10-26 22:47:24 +0900

    feat: 실행결과 기능 추가
    자동차 경주 실행결과를 출력하는 기능을 추가했습니다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,23 @@
 package racingcar.view;
 
+import racingcar.model.car.Car;
+import racingcar.model.car.Cars;
+
 public class OutputView {
+    private static String StartOutputMessage = "실행 결과";
+
+    public static void startOutput(){
+      System.out.println(StartOutputMessage);
+    }
+
+    public static void printCarRacingStatus(Cars cars){
+        for (Car car : cars.getCars()) {
+          System.out.println(toStringCarResult(car));
+        }
+    }
+
+    private static String toStringCarResult(Car car){
+      return car.getName().getName() + " : " + car.getDistance().toString();
+    }
 
 }
