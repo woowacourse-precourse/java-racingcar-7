2db577ef45519fef10efe5a941864567814f406c
GO-TE <alsrudxo9810@naver.com>
2024-10-26 15:59:46 +0900

    refactor(InputView): View 패키지로 이동

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,63 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class InputView {
+    private static final String INPUT_CAR_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TURN_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String DELIMITER = ",";
+
+    public List<String> inputCarName() {
+        DisplayInputPrompt(INPUT_CAR_MESSAGE);
+        String carNamePart = readUserInput();
+        final List<String> carNames = parseCarNames(carNamePart);
+        validateCarName(carNames);
+
+        return carNames;
+    }
+
+    public int inputRacingTurn() {
+        DisplayInputPrompt(INPUT_TURN_MESSAGE);
+        String rawTurn = readUserInput();
+        validateTurn(rawTurn);
+
+        return parseTurn(rawTurn);
+    }
+
+    private String readUserInput() {
+        return Console.readLine();
+    }
+
+    private void DisplayInputPrompt(String prompt) {
+        System.out.println(prompt);
+    }
+
+    private List<String> parseCarNames(String carNamePart) {
+        return List.of(carNamePart.split(DELIMITER));
+    }
+
+    private void validateCarName(List<String> carNames) {
+        for (String carName : carNames) {
+            if (carName == null || carName.isBlank()) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void validateTurn(String rawTurn) {
+        for (char digit : rawTurn.toCharArray()) {
+            if (!Character.isDigit(digit)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private int parseTurn(String rawTurn) {
+        try {
+            return Integer.parseInt(rawTurn);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
