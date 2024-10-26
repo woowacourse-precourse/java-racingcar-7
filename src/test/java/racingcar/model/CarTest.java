592de788d7b649c83e4d143d933029800a8d0eb2
longrunpc <pcjmo4050@gmail.com>
2024-10-27 00:57:39 +0900

    test(Car): Car 이름 유효성 검사 테스트 추가
    - Car 생성 성공 테스트 추가
    - Car 이름이 5자 초과일 때 예외 발생 테스트 추가
    - Car 이름이 null일 때 예외 발생 테스트 추가
    - Car 이름이 빈 문자열일 때 예외 발생 테스트 추가

---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,61 @@
+package racingcar.model;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CarTest {
+
+    @DisplayName("car 생성")
+    @Nested
+    class CarNameTest {
+        @DisplayName("car 생성 성공")
+        @Test
+        public void carNameSuccess() {
+            //given
+            String carName = "name";
+            //when
+            Car car = new Car(carName);
+            //Then
+            assertThat(car.getName).isEqualTo("name");
+
+        }
+
+        @DisplayName("car 생성시 이름 5글자 초과")
+        @Test
+        public void carNameException_Length() {
+            //given
+            String carName = "longName";
+            //when && then
+            assertThrows(IllegalArgumentException.class, () -> {
+                new Car(carName);
+            });
+        }
+
+        @DisplayName("car 생성시 이름 null")
+        @Test
+        public void carNameException_Null() {
+            //given
+            String carName = null;
+            //when && then
+            assertThrows(IllegalArgumentException.class, () -> {
+                new Car(carName);
+            });
+        }
+
+        @DisplayName("car 생성시 이름 빈칸")
+        @Test
+        public void carNameException_Empty() {
+            //given
+            String carName = "";
+            //when && then
+            assertThrows(IllegalArgumentException.class, () -> {
+                new Car(carName);
+            });
+        }
+    }
+}
