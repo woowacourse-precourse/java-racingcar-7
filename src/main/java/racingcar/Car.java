caec213047946f36a2af77f4b38985f904cc22ab
Hun0914 <bkh010914@naver.com>
2024-10-26 19:21:35 +0900

    feat : 전체 함수 구현
    객체지향적으로 분리하기 전 전체적으로 돌아가는 로직을 구현했습니다

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+public class Car {
+
+
+    public Car(String name, int length, String distance) {
+        this.name = name;
+        this.length = length;
+        this.distance = distance;
+    }
+
+    String name;
+    int length;
+    String distance;
+
+    @Override
+    public String toString() {
+        return name + " : " + distance;
+    }
+
+    public void move(){
+        this.length++;
+        this.distance += "-";
+    }
+}
