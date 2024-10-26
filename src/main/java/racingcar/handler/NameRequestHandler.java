a99fb5e10acd5cc8e12d11f1ef808930689b8051
lyouxsun <leedrkr323@konkuk.ac.kr>
2024-10-26 12:59:04 +0900

    feat(NameRequestHandler): 입력 문자열에 띄어쓰기가 포함되어 있는지 검사
    - 단위 테스트 작성을 위해 validateSpace 메서드의 접근제어자를 public으로 설정

--- src/main/java/racingcar/handler/NameRequestHandler.java
+++ src/main/java/racingcar/handler/NameRequestHandler.java
@@ -3,6 +3,7 @@ package racingcar.handler;
 import camp.nextstep.edu.missionutils.Console;
 
 import static racingcar.message.ErrorMessage.BLANK_INPUT_ERROR;
+import static racingcar.message.ErrorMessage.CONTAINS_SPACE_ERROR;
 
 public class NameRequestHandler {
 
@@ -10,12 +11,20 @@ public class NameRequestHandler {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String rawInput = Console.readLine();
         validateBlank(rawInput);
+        validateSpace(rawInput);
         return null;
     }
 
     public void validateBlank(String rawInput) {
         if(rawInput == null || rawInput.isBlank()){
             System.out.println(BLANK_INPUT_ERROR.getMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateSpace(String rawInput) {
+        if(rawInput.contains(" ")){
+            System.out.println(CONTAINS_SPACE_ERROR.getMessage());
             throw new IllegalArgumentException();
         }
     }
--- src/main/java/racingcar/message/ErrorMessage.java
+++ src/main/java/racingcar/message/ErrorMessage.java
@@ -2,7 +2,8 @@ package racingcar.message;
 
 public enum ErrorMessage {
 
-    BLANK_INPUT_ERROR("[ERROR] 빈 문자열을 입력하실 수 없습니다. 다시 입력해주세요.");
+    BLANK_INPUT_ERROR("[ERROR] 빈 문자열을 입력하실 수 없습니다. 다시 입력해주세요."),
+    CONTAINS_SPACE_ERROR("[ERROR] 문자열은 띄어쓰기를 포함할 수 없습니다. 다시 입력해주세요.");
 
     private String message;
 
