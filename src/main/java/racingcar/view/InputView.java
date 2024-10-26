74d1c9e5020a97bbb3c1fcf1eef0e7db3baf9efc
SongJaeHoonn <thdwogns121@naver.com>
2024-10-26 21:08:48 +0900

    feat: 자동차 이름 입력받는 로직 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,21 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
+
+    public static List<Car> getCars() {
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+        return Arrays.stream(input.split(","))
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
 }
