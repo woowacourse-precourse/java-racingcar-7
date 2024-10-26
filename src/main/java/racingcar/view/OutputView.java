dec81b3927083f0c408a8658fb72aadbc4c340b0
StoneCAU <seokwoo1999@naver.com>
2024-10-26 15:41:18 +0900

    refactor(OutputView): Round, Winner 출력 로직 메서드 변경
    - printRound: 이제 이 메서드에서 결과를 포맷팅하여 출력함
    - printWinners: 불필요한 format 호출 대신 printf 사용

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,19 +1,20 @@
 package racingcar.view;
 
-import java.util.List;
+import racingcar.domain.Car;
 
-import static java.lang.String.format;
+import java.util.List;
 
 public class OutputView {
     // Output Messages
     public static final String ASK_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String ASK_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
     public static final String RESULT_TITLE = "실행 결과";
-    public static final String RESULT_DETAIL = "%s : %s";
+    public static final String RESULT_DETAIL = "%s : %s\n";
     public static final String FINAL_RESULT = "최종 우승자 : %s";
 
     // Delimiter
-    public static final String WINNER_DELIMITER = ", ";
+    private static final String WINNER_DELIMITER = ", ";
+    private static final String POSITION_INDICATOR = "-";
 
     public void printMessage(String message) {
         System.out.println(message);
@@ -23,16 +24,18 @@ public class OutputView {
         System.out.println();
     }
 
-    public void printRound(List<String> roundResults) {
-        for (String roundResult : roundResults) {
-            System.out.println(roundResult);
+    public void printRound(List<Car> carsList) {
+        for (Car car : carsList) {
+            String carName = car.getName();
+            int position = car.getPosition();
+
+            System.out.printf(RESULT_DETAIL, carName, POSITION_INDICATOR.repeat(position));
         }
     }
 
-    public void printWinners(List<String> winnerList) {
-        String winners = String.join(WINNER_DELIMITER, winnerList);
-        String finalResultFormat = format(FINAL_RESULT, winners);
+    public void printWinners(List<String> winnersList) {
+        String winners = String.join(WINNER_DELIMITER, winnersList);
 
-        System.out.println(finalResultFormat);
+        System.out.printf(FINAL_RESULT, winners);
     }
 }
