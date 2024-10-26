beed7e1f6e8415776354c5608333e351e64714c4
dltmddus <tmddus4671@gmail.com>
2024-10-26 18:31:47 +0900

    feat: 0-9 랜덤 정수 추출 함수 재구현

--- src/main/java/racingcar/util/RandomGeneratorUtil.java
+++ src/main/java/racingcar/util/RandomGeneratorUtil.java
@@ -1,2 +1,12 @@
-package racingcar.util;public class RandomGeneratorUtil {
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomGeneratorUtil {
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
+
+    public static Integer getRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+    }
 }
