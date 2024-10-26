bcda26fc99049f5971c3b003d7a02a470ea65a08
sinminseok12 <sinminseok12@naver.com>
2024-10-27 04:26:37 +0900

    feat : 잔여 시도 횟수 검증 기능 구현
    decrease() 메서드를 안전하게 사용할 수 있게 하기 위해, 시도 횟수가 1보다 작을 경우 예외를 반환시킨다

--- src/main/java/racingcar/model/AttemptCount.java
+++ src/main/java/racingcar/model/AttemptCount.java
@@ -1,6 +1,7 @@
 package racingcar.model;
 
 public class AttemptCount {
+    private static final String ERROR_ATTEMPT_COUNT_DEPLETED = "시도 횟수가 부족합니다. 더 이상 감소할 수 없습니다.";
     private static final String ERROR_NEGATIVE_OR_ZERO_COUNT = "시도 횟수는 0 이상이어야 합니다.";
     private static final int MIN_ATTEMPT_COUNT = 1;
 
@@ -22,7 +23,14 @@ public class AttemptCount {
     }
 
     public void decrease() {
+        checkRemainingAttempts();
         count--;
+    }
+
+    private void checkRemainingAttempts(){
+        if(count < MIN_ATTEMPT_COUNT){
+            throw new IllegalArgumentException(ERROR_ATTEMPT_COUNT_DEPLETED);
+        }
     }
 
     public boolean canRace() {
