7bcaf9b0c420e521c82502335a100f3250576fa8
pakaheo <k70923@konkuk.ac.kr>
2024-10-26 23:37:05 +0900

    feat : Validate input try count
    create InputValidator to validate input try count, car names

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,2 +1,20 @@
-package racingcar.validator;public class InputValidator {
+package racingcar.validator;
+
+public class InputValidator {
+
+    private static final String INPUT_CAR_NAMES_REGEX = "(\\s*\\w+\\s*)(,\\s*\\w+\\s*)*";
+
+    public static void validateCarNames(String inputCarNames) {
+        if (!inputCarNames.matches(INPUT_CAR_NAMES_REGEX)) {
+            throw new IllegalArgumentException("자동차 이름은 콤마로 구분해야 합니다");
+        }
+    }
+
+    public static void validateTryCount(String inputTryCount) {
+        try {
+            Long.valueOf(inputTryCount);
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException("시도 횟수는 아라비아 숫자로 입력해야 합니다");
+        }
+    }
 }
--- src/test/java/racingcar/validator/InputValidatorTest.java
+++ src/test/java/racingcar/validator/InputValidatorTest.java
@@ -1,2 +1,21 @@
-package racingcar.validator;public class InputValidatorTest {
+package racingcar.validator;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.Test;
+
+public class InputValidatorTest {
+    @Test
+    void 콤마로_split_할_수_없음() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputValidator.validateCarNames("pobi:nana,jun"))
+                .withMessageMatching("자동차 이름은 콤마로 구분해야 합니다");
+    }
+
+    @Test
+    void 시도_횟수_숫자로_변경_불가() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputValidator.validateTryCount("a"))
+                .withMessageMatching("시도 횟수는 아라비아 숫자로 입력해야 합니다");
+    }
 }
