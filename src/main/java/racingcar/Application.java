5f681810a520e71c2caf97b7ff0f1ef43b6a3b4c
Jaehwi Song <wotns0319@gmail.com>
2024-10-28 03:38:45 +0900

    feat(racingcar): implement first feature for handling inputs
    1. Input car names with delimiter (,) and manage them with LinkedListMap (key=name, value=0) initial value = 0
    2. Input tryCount to repeat the racing process later

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Map<String, Integer> cars = new LinkedHashMap<>();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String nameInput = Console.readLine();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int tryCount = Integer.parseInt(Console.readLine());
+
+        // set initial value for each car name as 0
+        for(String name : nameInput.split(",")) {
+            cars.put(name, 0);
+        }
+
     }
 }
