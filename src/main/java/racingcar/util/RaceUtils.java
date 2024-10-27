a6ffb787e3914eac7ef083c2ea0bbb5052fb48a4
Hyun0828 <spdlqj4818@naver.com>
2024-10-27 15:50:49 +0900

    fix(RaceUtils): 우승자를 결정하는 로직 변경

--- src/main/java/racingcar/util/RaceUtils.java
+++ src/main/java/racingcar/util/RaceUtils.java
@@ -5,19 +5,24 @@ import racingcar.domain.Car;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class RaceUtils {
 
     public static String getWinners(final List<Car> carList) {
         List<String> winnerList = new ArrayList<>();
 
-        Collections.sort(carList);
-        long maxMoves = carList.get(0).getTotalMoves();
+        PriorityQueue<Car> maxHeap = new PriorityQueue<>();
+        maxHeap.addAll(carList);
 
-        for (int i = 1; i < carList.size(); i++) {
-            if (carList.get(i).getTotalMoves() < maxMoves)
-                break;
-            winnerList.add(carList.get(i).getName());
+        if (!maxHeap.isEmpty()) {
+            Car car = maxHeap.poll();
+            long maxMoves = car.getTotalMoves();
+            winnerList.add(car.getName());
+
+            while (!maxHeap.isEmpty() && maxHeap.peek().getTotalMoves() == maxMoves) {
+                winnerList.add(maxHeap.poll().getName());
+            }
         }
 
         return String.join(", ", winnerList);
