5e8dd4bf1a12cddea66cb12cf631f351f7e8039f
woongjee <jeewoong7753@naver.com>
2024-10-28 01:03:18 +0900

    feat(Race):자동차 이름 배열 생성 기능 추가

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -10,5 +10,11 @@ public class Race {
     }
 
     private Car[] createCars(String[] carNames) {
+        Car[] cars = new Car[carNames.length];
+        for(int i = 0; i < carNames.length; i++) {
+            cars[i] = new Car(carNames[i]);
+        }
+        return cars;
     }
+
 }
