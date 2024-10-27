7d31f314d59933861094791e086e90cbf0433b74
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-28 06:01:09 +0900

    feat(Racing): 처음 이동 횟수 기록 기능 구현
    - 처음 이동 횟수를 기록합니다.

--- src/main/java/racingcar/model/Racing.java
+++ src/main/java/racingcar/model/Racing.java
@@ -1,8 +1,11 @@
 package racingcar.model;
 
 public class Racing {
-    // TODO: 처음 이동 횟수를 기록한다.
+    private int tryCount;
 
+    public void saveTryCount(int tryCount) {
+        this.tryCount = tryCount;
+    }
     // TODO: 이동횟수를 차감한다.
 
     // TODO: 이동 횟수에 따라 경기 진행 및 종료 여부를 판단한다.
