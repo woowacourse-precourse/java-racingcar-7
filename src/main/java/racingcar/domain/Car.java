148635e076263b2a0454746909d953a7e818df7b
jae woo Jung <awfjol2008@gmail.com>
2024-10-27 15:48:41 +0900

    feat: Car 클래스 생성자 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,4 +3,9 @@ package racingcar.domain;
 public class Car {
 	String name;
 	String distance;
+
+	public Car(String name, String distance) {
+		this.name = name;
+		this.distance = distance;
+	}
 }
