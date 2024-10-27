d6b2c75f1a845385f8d2b401691166c24825822e
eoehd1ek <eoehd2ek@gmail.com>
2024-10-28 02:50:56 +0900

    test(Car): Car 테스트 코드 추가
    자동차 이름 정보를 반환하는 메소드의 테스트를 구현합니다.
    자동차 상태 정보를 반환하는 메소드의 테스트를 구현합니다.
    자동차 이동 메소드의 정지 상황의 테스트를 구현합니다.
    자동차 이동 메소드의 전진 상황의 테스트를 구현합니다.

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,71 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @Test
+    void getName_테스트() {
+        // given
+        Car car = new Car("dong");
+
+        // when
+        String name = car.getName();
+
+        // then
+        assertEquals(name, "dong");
+    }
+
+    @Test
+    void getStatus_위치0_테스트() {
+        // given
+        Car car = new Car("dong");
+
+        // when
+        String initStatus = car.getStatus();
+
+        // then
+        assertEquals(initStatus, "dong : ");
+    }
+
+    @Test
+    void getStatus_위치1_테스트() {
+        // given
+        Car car = new Car("dong");
+        car.moveIfPossible(4);
+
+        // when
+        String status = car.getStatus();
+
+        // then
+        assertEquals(status, "dong : -");
+    }
+
+    @Test
+    void moveIfPossible_정지_테스트() {
+        // given
+        Car car = new Car("dong");
+
+        // when
+        car.moveIfPossible(3);
+
+        // then
+        assertThat(car).extracting("position").isEqualTo(0);
+    }
+
+    @Test
+    void moveIfPossible_이동_테스트() {
+        // given
+        Car car = new Car("dong");
+
+        // when
+        car.moveIfPossible(4);
+
+        // then
+        assertThat(car).extracting("position").isEqualTo(1);
+    }
+
+}
