0cf7ed8f94313c395766c8cbc0c695cfc6a05291
Seungwan Yoo <ysw99@kakao.com>
2024-10-27 13:44:54 +0900

    feat: 검증 로직 개선, 분리

--- src/main/java/racingcar/util/ValidateUtil.java
+++ src/main/java/racingcar/util/ValidateUtil.java
@@ -7,17 +7,22 @@ public class ValidateUtil {
     private static final String SPACING_PREFIX = " ";
 
     public static void validateCar(String input) {
-        if (input.isBlank() || input.contains(SPACING_PREFIX) || !input.contains(SPLIT_REGEX_PREFIX))
+        if (input.isBlank() || input.contains(SPACING_PREFIX) || !input.contains(SPLIT_REGEX_PREFIX) || input.split(SPLIT_REGEX_PREFIX).length == 1)
             throw new IllegalArgumentException(RaceException.WRONG_INPUT.getDescription());
     }
 
     public static void validateAttemptCount(String attemptCount) {
+        if (attemptCount == null || attemptCount.isBlank())
+            throw new IllegalArgumentException(RaceException.WRONG_INPUT.getDescription());
         try {
-            if (Integer.parseInt(attemptCount) < 1)
-                throw new IllegalArgumentException(RaceException.WRONG_INPUT.getDescription());
+            validateNum(Integer.parseInt(attemptCount));
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(RaceException.WRONG_INPUT.getDescription());
         }
+    }
 
+    public static void validateNum(int attemptCount) {
+        if (attemptCount < 1)
+            throw new IllegalArgumentException(RaceException.WRONG_INPUT.getDescription());
     }
 }
