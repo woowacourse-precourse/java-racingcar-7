941fb2878d4a5cacd1f831ddfd8d8c88ddae5bbd
JS <zks145@naver.com>
2024-10-27 15:41:21 +0900

    feat (exception) : 경주 횟수 입력에 숫자가 아닌 값이 입력된 경우 예외 메시지 추가

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -7,7 +7,8 @@ public enum ErrorMessage {
     REGEX_FOUND("특수 문자가 포함되었습니다."),
     SAME_NAME_FOUND("동일한 이름의 자동차가 존재합니다."),
     OVER_GAME_MATCHES("허용된 게임 횟수를 넘겼습니다."),
-    NEGATIVE_GAME_MATCHES("경기 횟수는 음수가 될 수 없습니다.");
+    NEGATIVE_GAME_MATCHES("경기 횟수는 음수가 될 수 없습니다."),
+    GAME_MATCHES_NOT_NUMBER("경기 횟수는 숫자가 입력되어야 합니다.");
 
     private final String message;
 
