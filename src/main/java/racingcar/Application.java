d259edb7c5b799d2808de8cd9896ed50e0655d1c
Jungwoo Chu <cjw980930@gmail.com>
2024-10-27 22:34:41 +0900

    feat: 시도할 횟수 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("자동차 이름을 쉼표(,)로 구분하여 입력해 주세요.");
         String input = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int attemptsNum = Integer.parseInt(Console.readLine());
 
         Console.close();
     }
