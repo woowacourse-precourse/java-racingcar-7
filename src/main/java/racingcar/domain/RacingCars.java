6b704d33e8225cd2c1563e55e0d156e76dbecb63
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-10-27 19:13:22 +0900

    reafactor: RacingCars와 MoveCondition 느슨한결합으로 변경

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -5,17 +5,15 @@ import java.util.List;
 
 public class RacingCars {
 
-    List<Car> cars = new ArrayList<>();
-    MoveCondition moveCondition;
+    private final List<Car> cars = new ArrayList<>();
 
-    public RacingCars(List<String> names,MoveCondition moveCondition) {
+    public RacingCars(List<String> names) {
         for (String name : names) {
             cars.add(new Car(name, 0));
         }
-        this.moveCondition = moveCondition;
     }
 
-    public void moveAll() {
+    public void moveAll(MoveCondition moveCondition) {
         for (Car car : cars) {
             if(!moveCondition.canMove()) continue;
             car.moveForward(1);
