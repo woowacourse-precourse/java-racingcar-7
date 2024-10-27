ee807986675ef85fa57cb1a4d7a000a6fbc74e3b
Hun0914 <bkh010914@naver.com>
2024-10-27 14:32:56 +0900

    refactor : 우승자 저장 기능 분리
    우승자를 저장하는 기능을 분리하였습니다.

--- src/main/java/racingcar/service/MoveService.java
+++ src/main/java/racingcar/service/MoveService.java
@@ -1,5 +1,6 @@
 package racingcar.service;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 import racingcar.Car;
 
@@ -16,6 +17,22 @@ public class MoveService {
         distanceMaximum = getMaximum(cars, distanceMaximum);
 
         return distanceMaximum;
+    }
+
+    public List<String> winnerSave(List<Car> cars, int maximum){
+        List<String> winners = new ArrayList<>();
+
+        checkWinner(cars, maximum, winners);
+
+        return winners;
+    }
+
+    private static void checkWinner(List<Car> cars, int maximum, List<String> winners) {
+        for (Car car : cars) {
+            if(car.lengthSize()== maximum){
+                winners.add(car.getName());
+            }
+        }
     }
 
     private static int getMaximum(List<Car> cars, int maximum) {
