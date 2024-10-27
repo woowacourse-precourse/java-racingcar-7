41bbc1ebbb0e1d2060fbd2f8fb05af3060bfeb93
신관규 <soundbar91@gmail.com>
2024-10-27 20:34:19 +0900

    feat: 입력 안내 메시지 클래스 추가

---
+++ src/main/java/racingcar/message/UserInputPrompt.java
@@ -0,0 +1,6 @@
+package racingcar.message;
+
+public class UserInputPrompt {
+    public static String CAR_NAME_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static String TRY_COUNT_PROMPT = "시도할 횟수는 몇 회인가요?";
+}
