ec312cdff2632dda3ff9d6e696057e6a28c16e8a
조수아 <suacho0724@gmail.com>
2024-10-28 05:33:38 +0900

    test: CarRace 테스트 구현

--- src/test/java/racingcar/controller/CarRaceTest.java
+++ src/test/java/racingcar/controller/CarRaceTest.java
@@ -1,38 +1,45 @@
-package racingcar;
+package racingcar.controller;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
-class ApplicationTest extends NsTest {
+class CarRaceTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
     @Test
-    void 기능_테스트() {
+    void startRace_SingleWinner() {
         assertRandomNumberInRangeTest(
                 () -> {
-                    run("pobi,woni", "1");
+                    CarRace carRace = new CarRace("pobi,woni", 1);
+                    carRace.startRace();
+
                     assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
                 },
                 MOVING_FORWARD, STOP
         );
+
     }
 
     @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
+    void startRace_MultipleWinner() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    CarRace carRace = new CarRace("pobi,woni,jun", 1);
+                    carRace.startRace();
+
+                    assertThat(output()).contains("최종 우승자 : pobi, woni");
+                }
+                , MOVING_FORWARD, MOVING_FORWARD, STOP
         );
+
     }
 
     @Override
-    public void runMain() {
-        Application.main(new String[]{});
+    protected void runMain() {
+
     }
 }
