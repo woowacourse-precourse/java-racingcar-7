6b5137b55a742abddaa59a0e56d975fffd8f9612
ffineapple <imffineapple@gmail.com>
2024-10-27 23:04:50 +0900

    feat: 자동차의 이름과 현위치 저장 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -11,5 +14,16 @@ public class Application {
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         int gameRoundInput = Integer.parseInt(Console.readLine());
+
+        List<String> carNames = List.of(carNamesInput.split(","));
+        HashMap<String, Integer> cars = new HashMap<String, Integer>();
+
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+            }
+
+            cars.put(carName, 0);
+        }
     }
 }
