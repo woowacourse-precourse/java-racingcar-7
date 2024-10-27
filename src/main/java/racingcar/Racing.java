333cbc2ed6ef9f4e9feab6a7629fc7cc35746929
Yeongjae Kim <sees77770@gmail.com>
2024-10-27 23:05:17 +0900

    refactor: 레이싱 객체 생성
    Racing 객체를 생성한다.
    - Racing 로직을 main에서 분리한다.
    - main은 Racing을 시작하는 역할을 수행한다.

---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,35 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Racing {
+
+    public void run() {
+        List<String> carNames = List.of("pobi", "woni");
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        int lap = 5;
+        for (int i = 0; i < lap; i++) {
+            for (Car car : cars) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (randomNumber >= 4) {
+                    car.move();
+                }
+            }
+        }
+
+        int maxDistance = cars.stream()
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElse(0);
+
+        List<Car> winners = cars.stream()
+                .filter(car -> car.getDistance() == maxDistance)
+                .toList();
+    }
+}
