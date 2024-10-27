07ae15fe7013e715f03a28b4f537336af8d1c733
kwon <rnjsdndud00@naver.com>
2024-10-27 12:22:01 +0900

    feat(Racer): Racer가 가지는 Utils 메소드 분리

--- src/main/java/racingcar/racer/RacerUtils.java
+++ src/main/java/racingcar/racer/RacerUtils.java
@@ -1,6 +1,5 @@
 package racingcar.racer;
 
 public interface RacerUtils {
-    public boolean isMovable(int randomNumber);
-    public int pickNumber(int minNumber, int maxNumber);
+    public boolean isMovable(int randomNum);
 }
