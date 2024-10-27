ab75d1ab06a07e9657dd2a43b04439aac7ac285c
MinJae00 <exceedmin@gmail.com>
2024-10-28 03:43:58 +0900

    feat: User객체 생성을 담당하는 컨트롤러 도입. 이로써 각자의 독립적인 기능을 하는 두개의 컨트롤러 운영. username에 대한 예외처리를 담당

--- src/main/java/racingcar/controller/UserController.java
+++ src/main/java/racingcar/controller/UserController.java
@@ -1,2 +1,39 @@
-package racingcar.controller;public class UserController {
+package racingcar.controller;
+
+import racingcar.user.UserObject;
+import racingcar.validator.Validator;
+
+public class UserController {
+    private String inputString;
+    private UserObject[] user;
+    private Validator validator;
+
+    public UserController(String inputString) {
+        this.inputString = inputString;
+        this.validator = new Validator(inputString);
+    }
+
+    public UserObject[] settingUser() {
+        validator.isEmptyInput();
+        validator.isCommaInFrontorBack();
+        parsingInputString();
+        return user;
+    }
+
+    private void parsingInputString() {
+        String[] parsingUser = inputString.split(",");
+        createUser(parsingUser);
+    }
+
+    private void createUser(String[] parsingUser) {
+        user = new UserObject[parsingUser.length];
+        createUserObject(parsingUser);
+    }
+
+    private void createUserObject(String[] parsingUser) {
+        for (int i = 0; i < parsingUser.length; i++) {
+            validator.userNameValidate(parsingUser[i].trim());
+            user[i] = new UserObject(parsingUser[i]);
+        }
+    }
 }
