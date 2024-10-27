20de64389c1e01f37216e601d712a60395828139
jijongkwon <help3451@naver.com>
2024-10-28 01:42:16 +0900

    feat(service) : RacingCarService 추가 메서드 구현
    - startGame : 랜덤 숫자 뽑기 및 이동
    - moveRacingCar : 자동차 이동
    - saveRacingHistory : 기록 저장

--- src/main/java/racingcar/application/service/car/RacingCarService.java
+++ src/main/java/racingcar/application/service/car/RacingCarService.java
@@ -1,5 +1,7 @@
 package racingcar.application.service.car;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.application.service.history.RacingHistoryService;
 import racingcar.domain.car.RacingCar;
 import racingcar.infrastructure.persistence.InMemoryRacingCarRepository;
 import racingcar.infrastructure.util.NameParser;
@@ -10,14 +12,15 @@ public class RacingCarService {
 
     private final NameParser nameParser;
     private final InMemoryRacingCarRepository racingRepository;
+    private final RacingHistoryService racingHistoryService;
     private int tryNumber;
 
-    public RacingCarService(NameParser nameParser) {
-        this.nameParser = nameParser;
+    public RacingCarService() {
+        this.nameParser = new NameParser();
         this.racingRepository = InMemoryRacingCarRepository.getInstance();
+        this.racingHistoryService = new RacingHistoryService();
     }
 
-    // TODO : 이름 입력받기
     public void inputCarName(String racingCars) {
         List<String> names = nameParser.parsingName(racingCars);
 
@@ -26,12 +29,10 @@ public class RacingCarService {
         }
     }
 
-    // TODO : 레이싱 카 초기화
     private RacingCar initRacingCar(String name) {
         return new RacingCar(name);
     }
 
-    // TODO : 이름 저장하기
     private void saveName(RacingCar racingCar) {
         racingRepository.saveRacingCar(racingCar);
     }
@@ -42,6 +43,25 @@ public class RacingCarService {
     }
 
     // TODO : 시도횟수 만큼 랜덤 숫자 뽑기
+    public void startGame(){
+        for (int i = 0; i < tryNumber ; i++) {
+            for (RacingCar racingCar : racingRepository.getRacingCars()) {
+                int number = Randoms.pickNumberInRange(0, 9);
+                moveRacingCar(racingCar, number);
+                saveRacingHistory(racingCar);
+            }
+        }
+    }
 
     // TODO : 4 이상인 경우 전진하기
+    public void moveRacingCar(RacingCar racingCar, int number){
+        if(number > 4){
+            racingCar.move();
+            racingRepository.saveRacingCar(racingCar);
+        }
+    }
+
+    private void saveRacingHistory(RacingCar racingCar){
+        racingHistoryService.saveHistory(racingCar);
+    }
 }
