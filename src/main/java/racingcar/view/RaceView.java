ef271ce20b35724667ebd2165679c7784d75721a
조윤재 <devilishvoice@inha.edu>
2024-10-28 03:15:01 +0900

    feat(view) : printResults, printWinner, printInputPrompt 구현

--- src/main/java/racingcar/view/RaceView.java
+++ src/main/java/racingcar/view/RaceView.java
@@ -5,13 +5,14 @@ import racingcar.model.RacingCar;
 public class RaceView {
     public void printResults(RacingCar[] cars) {
         for (RacingCar car : cars) {
-            System.out.print(car.getName() + ": " + car.getResults() + " "); // 자동차 이름과 누적 결과 출력
+            System.out.print(car.getName() + " : " + car.getResults() + " "); // 자동차 이름과 누적 결과 출력
+            System.out.println();
         }
         System.out.println(); // 줄바꿈
     }
 
     public void printWinner(String winners) {
-        System.out.println("최종 우승자: " + winners);
+        System.out.println("최종 우승자 : " + winners);
     }
 
     public void printInputPrompt(String message) {
