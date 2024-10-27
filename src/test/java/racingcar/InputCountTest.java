3077b511307b020877620ebc944d4be8974c1101
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-27 18:16:14 +0900

    test: 시도횟수 출력 문구 테스트 확인

---
+++ src/test/java/racingcar/InputCountTest.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class InputCountTest {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void 시도횟수_출력_확인_테스트() {
+        // given
+        String input = "5\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        // when
+        String result = InputCount.getInputCountData();
+
+        // then
+        String expectedOutput = "시도할 횟수는 몇 회인가요?";
+        String actualOutput = outputStreamCaptor.toString().trim();
+
+        // 출력 확인
+        assertThat(actualOutput).isEqualTo(expectedOutput);
+        assertThat(result).isEqualTo("5");
+    }
+}
