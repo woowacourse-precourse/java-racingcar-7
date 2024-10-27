66e82cef4be4a967264a41a98a4bdc8f1801bce6
kimdev <kim__dev@hufs.ac.kr>
2024-10-27 21:06:05 +0900

    refactor(Input): 턴 입력 예외 메세지를 상수 처리하라

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
     private static String GET_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static String GET_RACE_TURN_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static String TURN_INPUT_TYPE_ERROR_MESSAGE = "숫자를 입력해 주세요.";
 
     public static String inputCarName() {
         System.out.println(GET_CAR_NAME_MESSAGE);
@@ -16,7 +17,7 @@ public class Input {
         try {
             return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException exception) {
-            throw new IllegalArgumentException("숫자를 입력해 주세요.");
+            throw new IllegalArgumentException(TURN_INPUT_TYPE_ERROR_MESSAGE);
         }
     }
 }
