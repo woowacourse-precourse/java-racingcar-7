63e98ff38e034ed26b2c5ae9f9217521e90ee3fd
MinJae00 <exceedmin@gmail.com>
2024-10-27 19:28:30 +0900

    docs: 기능 세분화에 따른 기능 문서 업데이트

---
+++ src/main/java/racingcar/Controller/GameController.java
@@ -0,0 +1,25 @@
+package racingcar.Controller;
+
+import racingcar.operator.GameEngine;
+import racingcar.user.SettingGameUser;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class GameController {
+    public void runGame() {
+        new SettingGameUser();
+        OutputView.printResultSentence();
+        GameEngine gameEngine = new GameEngine();
+        for (int i = 0; i < InputView.trynum; i++) {
+            gameEngine.progressGame();
+        }
+    }
+
+    public static void displayProgress() {
+        OutputView.printTurnResult();
+    }
+
+    public static void displayWinner() {
+
+    }
+}
