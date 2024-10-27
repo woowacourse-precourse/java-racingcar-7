8da497d370b7c9b6ea85dc18ea83f92ee1e50cd8
JEN <ekffuclxk@gmail.com>
2024-10-27 21:11:59 +0900

    feat(car): 자동차 전진 조건 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,8 +1,8 @@
 package racingcar;
 
 public class Car {
-    /* 자동차 클래스 생성
-     *  - 자동차 전진 조건 추가(무작위 값이 4 이상일 경우 전진)
+    /* 자동차 클래스 생성 - 0
+     *  - 자동차 전진 조건 추가(무작위 값이 4 이상일 경우 전진) - 0
      *  - 게임 회차별 현재 자동차들 위치 출력 기능
      */
     private String name;
@@ -19,5 +19,11 @@ public class Car {
 
     public int getMoveDisctance() {
         return moveDistance;
+    }
+
+    public void move(int randomNumber){
+        if(randomNumber >= 4){
+            moveDistance++;
+        }
     }
 }
