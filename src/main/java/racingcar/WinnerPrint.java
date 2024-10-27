9ce7ca649f135c6b5ca99072854e665a156c95ca
Spinichi <spinichi@naver.com>
2024-10-28 03:20:09 +0900

    refactor: WinnerPrint 코드 리팩토링
    Test 코드 작성을 위한 코드 리팩토링

--- src/main/java/racingcar/WinnerPrint.java
+++ src/main/java/racingcar/WinnerPrint.java
@@ -4,30 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WinnerPrint {
-    public static void winnerPrint() {
+
+    protected static List<String> getWinners() {
 
         int maxNumber = Application.moveCount[0];
 
         for (int i : Application.moveCount) {
 
             if (i > maxNumber) {
-
                 maxNumber = i;
-
             }
         }
 
         List<String> result = new ArrayList<>();
-
         for (int i = 0; i < Application.moveCount.length; i++) {
 
             if (Application.moveCount[i] == maxNumber) {
-
                 result.add(Application.nameList[i]);
-
             }
+
         }
 
-        System.out.println("최종 우승자 : " + String.join(", ",result));
+        return result;
+    }
+
+    public static void winnerPrint() {
+        List<String> winners = getWinners();
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
