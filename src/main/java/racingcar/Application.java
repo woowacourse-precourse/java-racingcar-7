c523c240fbaf8d773d40ac54274c61f509b103fa
Jungwoo Chu <cjw980930@gmail.com>
2024-10-28 00:15:54 +0900

    feat: 우승자 출력
    메인 메소드에서 게임 실행 및 결과 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,8 +7,19 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
+
         System.out.println("시도할 횟수는 몇 회인가요?");
         int attemptsNum = Integer.parseInt(Console.readLine());
+
+        Game game = new Game(CarName.carNameFilter(input));
+
+        System.out.println("실행 결과");
+        for (int i = 1; i <= attemptsNum; i++) {
+            game.attemptStatus();
+        }
+
+        List<String> winners = game.findWinners();
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
 
         Console.close();
     }
