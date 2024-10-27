bc39d123016cbcd98e0e837f41f4d7841cbb94d9
Yeongjae Kim <sees77770@gmail.com>
2024-10-27 22:25:50 +0900

    feat(Car) : 자동차 전진
    경주를 위해 자동차 전진 기능을 추가한다.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,8 +2,21 @@ package racingcar;
 
 public class Car {
     private final String name;
+    private int distance = 0;
 
     public Car(String carName) {
         this.name = carName;
+    }
+
+    public void move() {
+        distance += 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
     }
 }
