5dc438a518c84c9681625d652c991885a1e0d479
rootTiket <l20078829@gmail.com>
2024-10-27 14:12:51 +0900

    fix<RuleChecker>: 전진조건이 맞지 않아 수정
    전진조건이 4 이상인데 4 초과로 되어있어 수정

--- src/main/java/racingcar/domain/RuleChecker.java
+++ src/main/java/racingcar/domain/RuleChecker.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RuleChecker {
 
     public static boolean canMove() {
-        return Randoms.pickNumberInRange(0, 9) > 4;
+        return Randoms.pickNumberInRange(0, 9) >= 4;
     }
 
 }
