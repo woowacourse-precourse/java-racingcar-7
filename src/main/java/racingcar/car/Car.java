e10cc3efa8d5f74bd3a7af4c3327e540a2037a06
wlsdlf2 <wlsdlfzg@naver.com>
2024-10-28 00:48:03 +0900

    feat: Car 클래스 생성 및 이동 기능 추가

---
+++ src/main/java/racingcar/car/Car.java
@@ -0,0 +1,28 @@
+package racingcar.car;
+
+public class Car {
+    private static final int MOVE_TRIGGER = 4;
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(int randomNum) {
+        if (randomNum >= MOVE_TRIGGER) {
+            this.position++;
+        }
+    }
+
+}
