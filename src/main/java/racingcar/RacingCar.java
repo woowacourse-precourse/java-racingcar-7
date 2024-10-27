ba266383988848b6b017d5d0976134e8ec013ecd
mirolis6636 <mirolis6636@gmail.com>
2024-10-28 00:10:55 +0900

    refactor: RacingCar클래스를 차 이름과 기록을 가지고 동작하는 클래스로 변경

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -6,49 +6,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 public class RacingCar{
-    private String carNames;
-    private HashMap<String, Integer> record = new HashMap<>();
-    private int numOfTrial;
-    private InputReader inputReader;
 
-    public RacingCar(InputReader inputReader){
-        this.inputReader = inputReader;
+    private String carName;
+    private int record;
+
+    public RacingCar(String carName){
+        this.carName = carName;
+        record = 0;
     }
-
     public void race(){
-        while(numOfTrial>0){
-            for(String car : carNames) {
-                int randomNumber = Randoms.pickNumberInRange(0, 9);
-                Integer numOfMovement = record.getOrDefault(car, 0);
-                record.put(car, numOfMovement+1);
-            }
-            System.out.println("실행 결과");
-            for(String car : carNames){
-                System.out.printf("%s : ", car);
-                for(int i=0; i<record.getOrDefault(car, 0); ++i){
-                    System.out.printf("%c", '-');
-                }
-                System.out.println();
-            }
-            --numOfTrial;
-        }
-        Optional<Integer> longestMovement = record.values().stream().max(Integer::compareTo);
-        ArrayList<String> winner = new ArrayList<>();
-        for(String car : carNames){
-            if(record.get(car).equals(longestMovement.get())){
-                winner.add(car);
-            }
-        }
-        System.out.printf("최종 우승자 : ");
-        String result = String.join(",", winner);
-        System.out.println(result);
+
     }
 
-    public String[] getCarNames() {
-        return carNames;
-    }
-
-    public int getNumOfTrial() {
-        return numOfTrial;
-    }
 }
