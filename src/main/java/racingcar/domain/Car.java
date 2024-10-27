713a29a65b4db4d92c27eb5551b60471a0b8b12e
MCshin00 <sinminc7@gmail.com>
2024-10-28 03:36:57 +0900

    refactor: 자동차 이름 검증 함수 분리

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -14,18 +14,26 @@ public class Car {
     private int position;
 
     public Car(String name) {
-        validateName(name);
+        validateNullOrEmpty(name);
+        validateLength(name);
+        validateContainEmpty(name);
         this.name = name;
         this.position = 0;
     }
 
-    private void validateName(String name) {
+    private void validateNullOrEmpty(String name) {
         if (name == null || name.isBlank()) {
             throw new CarNameNullExeption(CAR_NAME_NULL_OR_EMPTY_MESSAGE.getMessage());
         }
+    }
+
+    private void validateLength(String name) {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new CarNameLengthExeption(CAR_NAME_LENGTH_MESSAGE.getMessage());
         }
+    }
+
+    private void validateContainEmpty(String name) {
         if (name.contains(" ")) {
             throw new CarNameContainEmptyException(CAR_NAME_CONTAIN_EMPTY_MESSAGE.getMessage());
         }
