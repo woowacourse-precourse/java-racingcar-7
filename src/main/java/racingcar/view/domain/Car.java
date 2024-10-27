8205e7d2bff8c319f2c63a7e47c6ca596dde8aa9
TaegeunYou <dbxorms2310@naver.com>
2024-10-27 11:50:16 +0900

    feat(Car): 자동차 이름이 5자 이하인지 검증

--- src/main/java/racingcar/view/domain/Car.java
+++ src/main/java/racingcar/view/domain/Car.java
@@ -3,9 +3,14 @@ package racingcar.view.domain;
 import racingcar.view.global.Validator;
 
 public class Car {
+    private static int NAME_MAX_LENGTH = 5;
     private String name;
+
     public Car(String str) {
         Validator.validString(str);
+        if (str.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException();
+        }
         this.name = str;
     }
 }
