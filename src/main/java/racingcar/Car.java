4f106a4046a02ac6a647ee1dbff82dbc93663ef9
mins-git <m2insapp@gmail.com>
2024-10-27 15:37:56 +0900

    feat(Car): Car 모델 유효성 검사 로직 구현 - validateName()로 유효성 검사를 진행하여 글자수 5자 이하인지 체크.     - 뷰에서 차량 이름 뽑을때(printRaceProgress) 진행시 IllegalArgumentException 예외처리 발생시킴.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,8 +5,15 @@ public class Car {
     private int position;
 
     public Car(String name){
+        validateName(name);
         this.name = name;
         this.position = 0;
+    }
+
+    private void validateName(String name){
+        if (name.length() > 5){
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
     }
 
     public String getName() {
