87eebe4a68f3864ee5462ccbf8ed0335d1078b30
dltmddus <tmddus4671@gmail.com>
2024-10-27 14:53:04 +0900

    refactor: 자동차 이름 에러 메시지 상수 적용, 코드 리팩토링

--- src/main/java/racingcar/validation/CarNameValidation.java
+++ src/main/java/racingcar/validation/CarNameValidation.java
@@ -1,5 +1,11 @@
 package racingcar.validation;
 
+import static racingcar.constant.ErrorMessage.CAR_NAME_CANNOT_BE_NULL_OR_EMPTY;
+import static racingcar.constant.ErrorMessage.CAR_NAME_CANNOT_END_WITH_COMMA;
+import static racingcar.constant.ErrorMessage.CAR_NAME_LENGTH_INVALID;
+import static racingcar.constant.ErrorMessage.CAR_NAME_MUST_BE_SEPARATED_BY_COMMAS;
+import static racingcar.constant.ErrorMessage.DUPLICATE_CAR_NAME_FOUND;
+import static racingcar.constant.ErrorMessage.MINIMUM_CAR_NUMBER_IS_ONE;
 import static racingcar.constant.SeparatorConstant.COMMA;
 import static racingcar.constant.SeparatorConstant.EMPTY;
 import static racingcar.constant.SeparatorConstant.SPACE;
@@ -20,19 +26,19 @@ public class CarNameValidation {
 
     private static void validateNotNullOrEmpty(String input) {
         if (input == null || input.trim().isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(CAR_NAME_CANNOT_BE_NULL_OR_EMPTY.getMessage());
         }
     }
 
     private static void validateIncludeComma(String input) {
         if (!input.contains(",")) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(CAR_NAME_MUST_BE_SEPARATED_BY_COMMAS.getMessage());
         }
     }
 
     private static void validateNotEndWithComma(String input) {
         if (input.endsWith(",")) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(CAR_NAME_CANNOT_END_WITH_COMMA.getMessage());
         }
     }
 
@@ -52,20 +58,20 @@ public class CarNameValidation {
 
     private static void validateCarNameLength(String input) {
         if (input.isEmpty() || input.length() > MAXIMUM_CAR_NAME_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_INVALID.getMessage());
         }
     }
 
     private static void validateDuplicateName(int arraySize, int setSize) {
         if (arraySize != setSize) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(DUPLICATE_CAR_NAME_FOUND.getMessage());
         }
     }
 
 
     private static void validateCarCount(Set<String> inputs) {
         if (inputs.size() <= MINIMUM_CAR_NUMBER) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(MINIMUM_CAR_NUMBER_IS_ONE.getMessage());
         }
 
     }
