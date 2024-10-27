96ee83eab1994054cf1adcaeb4be42c5c3f12999
seoyoungjin23 <ascd050@gmail.com>
2024-10-27 21:11:05 +0900

    fix(InputHandler): print문 수정

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -5,12 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputHandler {
 
     public static String getRacingCarNames() {
-        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
 
     public static int getNumberOfAttempts() {
-        System.out.print("시도할 횟수는 몇 회인가요?");
+        System.out.println("시도할 횟수는 몇 회인가요?");
         return Integer.parseInt(Console.readLine());
     }
 }
