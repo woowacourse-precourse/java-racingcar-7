cc99f536039344a8167bdee1f74cb4f89ea17fa3
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-10-28 04:24:21 +0900

    refactor(CarNameParser): 구분자와 이름최대길이 상수로 선언

--- src/main/java/racingcar/CarNameParser.java
+++ src/main/java/racingcar/CarNameParser.java
@@ -2,10 +2,11 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class CarNameParser {
+    private static final String DELIMITER = ",";
+    private static final int MAX_CAR_NAME_LENGTH = 5;
 
     /**
      * 자동차 이름을 입력받는 메서드
@@ -46,7 +47,6 @@ public class CarNameParser {
 
     /**
      * 자동차 이름 배열을 검증하고 리스트로 반환하는 메서드
-     *
      * @param splitCarNames 쉼표 기준으로 분리한 문자열 배열
      * @return 유효성이 검증된 자동차 이름 리스트
      * */
