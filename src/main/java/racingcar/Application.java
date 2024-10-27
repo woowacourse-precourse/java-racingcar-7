1785155e5c741ee426c750b152f87f19d6c85a38
dpfls0922 <dpfls0922@naver.com>
2024-10-27 15:05:14 +0900

    feat: 사용자로부터 자동치 이름 및 실행 횟수 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    private static String[] getCarNames() {
+        String carNames;
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        carNames = Console.readLine();
+        return splitCarNameByDelimiter(carNames);
+    }
+
+    private static int getGameCount() {
+        String count;
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        count = Console.readLine();
+        return Integer.parseInt(count);
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String[] carNames;
+        int gameCount;
+
+        carNames = getCarNames();
+        gameCount = getGameCount();
+
+        // 최종 우승자 출력
     }
 }
