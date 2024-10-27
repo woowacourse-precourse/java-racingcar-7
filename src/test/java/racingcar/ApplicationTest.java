01d5d2496918af98201c0fc7cc82c85c0abddeb9
siyoung <wfs0502@gmail.com>
2024-10-28 05:32:35 +0900

    test: 자동차 경주 게임 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -23,11 +24,50 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @DisplayName("최종 우승자가 여려명인 경우")
+    @Test
+    void manyWinnersTest() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni, java", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "java : -", "최종 우승자 : pobi, java");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("자동차 이름 중 빈 문자열이 포함된 경우")
+    @Test
+    void emptyCarNameTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("a,,b"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("시도 횟수가 음수인 경우")
+    @Test
+    void negativeRunLimitTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("a, b", "-2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("자동차 이름이 중복된 경우")
+    @Test
+    void duplicateCarNameTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("a, a", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
