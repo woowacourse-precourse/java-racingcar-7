3c340631865c0191c3a4f23aff16b76665da4170
yeo-li <81564503+yeo-li@users.noreply.github.com>
2024-10-27 23:41:15 +0900

    style(Application): 클래스 호출 방법 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,6 @@ import controller.RacingcarController;
 
 public class Application {
     public static void main(String[] args) {
-        RacingcarController racingcarController = new RacingcarController();
-        racingcarController.run();
+        new RacingcarController().run();
     }
 }
