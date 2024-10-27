a0ab03a24fd88201ce28bbe510277306a01b81ad
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-28 04:44:04 +0900

    feat(RacingController): 자동차 리스트를 만드는 기능 추가

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,16 +1,23 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import racingcar.model.Car;
+import racingcar.model.CarFactory;
 import racingcar.view.InputView;
 
 public class RacingController {
 
     private final InputView inputView;
+    private final CarFactory carFactory;
 
-    public RacingController(InputView inputView) {
+    public RacingController(InputView inputView, CarFactory carFactory) {
         this.inputView = inputView;
+        this.carFactory = carFactory;
     }
 
     public void startGame() {
         String carNames = inputView.inputCarNames();
+
+        ArrayList<Car> carList = carFactory.createCar(carNames);
     }
 }
