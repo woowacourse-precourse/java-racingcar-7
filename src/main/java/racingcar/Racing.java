98f74a32b63732cb7601f35c1e6fa648a80d45f0
gayoung228 <103436798+gayoung228@users.noreply.github.com>
2024-10-28 03:03:38 +0900

    우승자 계산 및 출력 기능 추가
    경주가 완료된 후, 가장 멀리 이동한 자동차를 찾아 우승자로 선언하고, 우승자의 이름을 출력하는 기능을 구현.

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -41,5 +41,16 @@ public class Racing {
         System.out.println();
     }
 
+    public List<String> getWinners() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
 
 }
