559648e20423b5d091fe4da051674aff9f4c399b
Youn Sang Hyun <younsang523@gmail.com>
2024-10-27 20:01:51 +0900

    refactor: 클래스 분할

--- src/main/java/racingcar/Domain/CarName.java
+++ src/main/java/racingcar/Domain/CarName.java
@@ -1,38 +1,19 @@
 package racingcar.Domain;
 
-import racingcar.Message.ErrorMessage;
-
 public class CarName {
     private final String value;
-    private static final int MAX_NAME_LENGTH = 5;
 
     private CarName(String value) {
-        validateName(value);
+        CarNameValidatator.validate(value);
         this.value = value;
-    }
-
-    private void validateBlank(String name) {
-        if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_MESSAGE_BLANK);
-        }
-    }
-
-    private void validateLength(String name) {
-        if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_MESSAGE_LENGTH);
-        }
-    }
-
-    private void validateName(String name) {
-        validateLength(name);
-        validateBlank(name);
     }
 
     public static CarName from(String value) {
         return new CarName(value);
     }
 
-    public String getValue() {
+    @Override
+    public String toString() {
         return value;
     }
 
---
+++ src/main/java/racingcar/Domain/CarNameValidatator.java
@@ -0,0 +1,25 @@
+package racingcar.Domain;
+
+import racingcar.Message.ErrorMessage;
+
+public class CarNameValidatator {
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private static void validateBlank(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_MESSAGE_BLANK);
+        }
+    }
+
+    private static void validateLength(String name) {
+        if (name.trim().length() > MAX_NAME_LENGTH) {  // trim() 추가
+            throw new IllegalArgumentException(ErrorMessage.ERROR_MESSAGE_LENGTH);
+        }
+    }
+
+    public static void validate(String name) {
+        validateBlank(name);
+        validateLength(name);
+    }
+
+}
