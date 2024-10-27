a8ed69c53c7ac9cdf7d49f9ba7b18ddfe57a939d
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-28 06:09:22 +0900

    feat(OutputView): 경주 시작 메시지 출력 및 각 스테이지 결과 반환 출력 기능 구현
    - 경주 시작 메시지를 출력합니다.
    - cars 클래스를 통해 각 스테이지 결과를 반환합니다.
    - todo list 및 기능 작성에서 잘못된 부분을 수정했습니다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,9 +1,35 @@
 package racingcar.view;
 
-public class OutputView {
-    // TODO: 경주 시작 메시지를 출력한다.
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.Cars;
 
-    // TODO: 각 스테이지마다 이동 결과를 출력한다.
+public class OutputView {
+    private static final String CLONE_STRING_FORMAT = " : ";
+    private static final String MOVE_EXPRESSION = "-";
+    private static final String WINNER_PROMPT = "최종 우승자 : ";
+    private static final String RACING_START_PROMPT = "실행 결과";
+
+    public void printRacingStart() {
+        System.out.println();
+        System.out.println(RACING_START_PROMPT);
+    }
+
+    public void printStageResult(Cars cars) {
+        for (Car car : cars.getCars()) {
+            System.out.println(car.getCarName() + CLONE_STRING_FORMAT + getMoveDistance(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    private String getMoveDistance(int position) {
+        String moveDistance = "";
+        for (int i = 0; i<position; i++) {
+            moveDistance += MOVE_EXPRESSION;
+        }
+        return moveDistance;
+    }
+
 
     // TODO: 최종 우승 자동차를 출력한다.
 }
