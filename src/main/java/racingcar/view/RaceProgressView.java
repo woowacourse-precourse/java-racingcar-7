058739c398d2b8360c79acea787fd77e11c5f4fa
chaerish <ccyy8432@naver.com>
2024-10-27 16:53:49 +0900

    refactor(RaceProgressView): 콜론 상수로 변경

--- src/main/java/racingcar/view/RaceProgressView.java
+++ src/main/java/racingcar/view/RaceProgressView.java
@@ -4,11 +4,12 @@ import java.util.List;
 import racingcar.RacingCar;
 
 public class RaceProgressView {
+    private final String COLON_DELIMITER = " : ";
 
     public void displayRoundProgress(List<RacingCar> racingCars) {
         System.out.println();
         for (RacingCar racingCar : racingCars) {
-            System.out.println(racingCar.getName() + " : " + racingCar.getDistance());
+            System.out.println(racingCar.getName() + COLON_DELIMITER + racingCar.getDistance());
         }
     }
 
