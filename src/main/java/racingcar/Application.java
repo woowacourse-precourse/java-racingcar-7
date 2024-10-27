7f694c7f930ac4b1bd2128fc77a0c39c1d146cc7
장영재 <urinaner@naver.com>
2024-10-28 02:02:18 +0900

    feat: 메인에 초기 세팅

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import java.io.IOException;
+import racingcar.controller.GameController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IOException {
+        GameController gameController = new GameController();
+        gameController.startGame();
     }
 }
