99ce59e001ae4624559887bb397215376d255f52
etd93 <etd93@naver.com>
2024-10-27 20:50:00 +0900

    refactor : CarNameValidation 클래스 메서드 분리

--- src/main/java/racingcar/validate/CarNameValidation.java
+++ src/main/java/racingcar/validate/CarNameValidation.java
@@ -2,11 +2,20 @@ package racingcar.validate;
 
 public class CarNameValidation {
     public static void validateName(String name) {
+        validateIfCarNameIsNUllOrEmpty(name);
+        validateIfCarNameLengthWithinFive(name);
+    }
+
+    public static void validateIfCarNameIsNUllOrEmpty(String name) {
         if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("자동차 이름을 지어주세요.");
         }
+    }
+
+    public static void validateIfCarNameLengthWithinFive(String name) {
         if (name.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.");
+            throw new IllegalArgumentException(
+                    "자동차 이름은 5글자 이하여야 합니다.");
         }
     }
 }
