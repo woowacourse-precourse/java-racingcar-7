bd01914a54acfb7f18b1c643a2aabcbc1316247a
yunhacandy <yunhacandy@gmail.com>
2024-10-27 22:28:10 +0900

    fix (OutputMessage): 실행 결과 출력 메시지 수정

--- src/main/java/racingcar/constant/OutputMessage.java
+++ src/main/java/racingcar/constant/OutputMessage.java
@@ -3,7 +3,7 @@ package racingcar.constant;
 public class OutputMessage {
     public static final String ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String ENTER_ATTEMPTS = "시도할 횟수는 몇 회인가요?";
-    public static final String RESULT_PREFIX = "실행결과";
+    public static final String RESULT_PREFIX = "실행 결과";
     public static final String WINNERS_ANNOUNCEMENT = "최종 우승자 : ";
     public static final String ERROR_PREFIX = "[오류] ";
 }
