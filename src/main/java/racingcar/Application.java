67af31ab4952f6b89458b07618a37ca37b6af25c
woodongle <315ehdgur@gmail.com>
2024-10-27 13:15:36 +0900

    feat: 자동차 경주 프로세스 추가, 메인 함수 간략화
    1. 자동차 경주 과정부터 최종 우승자까지 자동차 경주 게임을 완성합니다.
      - 게임을 시작하는 메서드(startGame())
      - 무작위 값을 이용해 전진하는 메서드(moveForwardWithRandomValues())
      - 중간 결과를 출력하는 메서드(printInterimResults())
      - 우승자를 판별해서 List에 저장하는 메서드(getWinner())
      - 게임 결과를 출력하는 메서드(printExecutionResults())
    2. 메인 함수를 간략하게 해서 복잡하지 않게 수정합니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,38 +1,90 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.ListIterator;
 
 public class Application {
     public static void main(String[] args) {
         try {
-            // 경주할 자동차 이름 입력(참가자 수)
-            System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
-
-            // 사용자 입력에서 공백을 제거하고 쉼표로 구분해서 가변 컬렉션인 ArrayList에 저장
-            List<String> inputList = new ArrayList<>(Arrays.asList(Console.readLine().split("\\s*,\\s*")));
-            validateInput(inputList);
-            checkRedundancy(inputList);
-            checkCarNameLength(inputList);
-
-            List<Participant> participantList = new ArrayList<>();
-            createParticipantList(inputList, participantList);
-            checkParticipantSize(participantList);
-            System.out.println(participantList);
-
-            System.out.println("시도할 횟수는 몇 회인가요?");
-            String input2 = Console.readLine();
-            checkAttemptCount(input2);
-            int attemptCount = Integer.parseInt(input2);
-            System.out.println(attemptCount);
-
+            startGame();
         } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
             throw e;
         }
+    }
+
+    private static void startGame() {
+        // 경주할 자동차 이름 입력(참가자 수)
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+
+        // 사용자 입력에서 공백을 제거하고 쉼표로 구분해서 가변 컬렉션인 ArrayList에 저장
+        List<String> inputList = new ArrayList<>(Arrays.asList(Console.readLine().split("\\s*,\\s*")));
+        validateInput(inputList);
+        checkRedundancy(inputList);
+        checkCarNameLength(inputList);
+
+        List<Participant> participantList = new ArrayList<>();
+        createParticipantList(inputList, participantList);
+        checkParticipantSize(participantList);
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String inputAttemptCount = Console.readLine();
+        checkAttemptCount(inputAttemptCount);
+        int attemptCount = Integer.parseInt(inputAttemptCount);
+
+        System.out.println("\n실행 결과");
+        printExecutionResults(attemptCount, participantList);
+    }
+
+    // 게임 결과를 출력하는 메서드
+    private static void printExecutionResults(int attemptCount, List<Participant> participantList) {
+        for (int i = 0; i < attemptCount; i++) {
+            moveForwardWithRandomValues(participantList);
+            printInterimResults(participantList);
+            System.out.println();
+        }
+
+        List<String> winnerList = getWinner(participantList);
+
+        System.out.println("최종 우승자 : " + String.join(", ", winnerList));
+    }
+
+    // 중간 결과를 출력하는 메서드
+    private static void printInterimResults(List<Participant> participantList) {
+        for (Participant participant : participantList) {
+            System.out.println(participant);
+        }
+    }
+
+    private static void moveForwardWithRandomValues(List<Participant> participantList) {
+        for (Participant participant : participantList) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                participant.plusMoveCount();
+            }
+        }
+    }
+
+    // 우승자를 판별해서 List에 저장하는 메서드
+    private static List<String> getWinner(List<Participant> participantList) {
+        List<Integer> moveCountList = new ArrayList<>();
+
+        for (int i = 0; i < participantList.size(); i++) {
+            moveCountList.add(participantList.get(i).getMoveCount());
+        }
+
+        List<String> winnerList = new ArrayList<>();
+
+        for (int i = 0; i < participantList.size(); i++) {
+            if (Collections.max(moveCountList) == participantList.get(i).getMoveCount()) {
+                winnerList.add(participantList.get(i).getCarName());
+            }
+        }
+        return winnerList;
     }
 
     // 시도할 횟수가 숫자 형식, 양수인지 확인하는 메서드
