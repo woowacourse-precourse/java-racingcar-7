37d2f9b6dd00337c0ca1f7f73d14b66822d76a3e
한관희 <noop103@naver.com>
2024-10-27 15:17:42 +0900

    feat: Main 메소드에 자동차 경주 게임 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.RacingController;
+import racingcar.input.ConsoleInputManager;
+import racingcar.input.InputManager;
+import racingcar.output.ConsoleOutputManager;
+import racingcar.output.OutputManager;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputManager inputManager = new ConsoleInputManager();
+        OutputManager outputManager = new ConsoleOutputManager();
+        RacingController controller = new RacingController(inputManager, outputManager);
+
+        try {
+            controller.setupGame();
+            controller.startRace();
+            controller.displayWinners();
+        } finally {
+            Console.close();
+        }
     }
 }
