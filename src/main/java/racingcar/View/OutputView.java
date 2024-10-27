44b5ce21baa4a8016772b1ffad02eca3d348d0d2
YunCheol1 <naro2727@naver.com>
2024-10-27 14:01:20 +0900

    feat(OutputView): 라운드 결과를 출력하는 OutputView 작성 - 라운드마다 결과를 출력하는 기능 구현.

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -1,4 +1,18 @@
 package racingcar.View;
 
+import java.util.List;
+import racingcar.Model.Car;
+
 public class OutputView {
+    public static void printRoundResults(List<Car> cars) {
+        for (var car : cars) {
+            System.out.print(car.getName() + " : ");
+            for (int i = 0; i < car.getPosition(); i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
+
