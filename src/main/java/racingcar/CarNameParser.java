cac38d5ead67579670263a73d0df33b63e8f28fc
RawBrain <95132660+RawBrain@users.noreply.github.com>
2024-10-28 01:02:21 +0900

    feat(parsing): implement car name parsing and validation
    쉼표(,)로 구분된 자동차 이름 문자열을 파싱하여 List로 변환
    자동차 이름 유효성 검사 구현
     - 양 끝 공백을 제외한 이름 길이 5자 초과시 예외 처리
     - null 입력 시 예외 처피
    공백이나 빈 문자열인 경우 무명1, 무명2 등으로 자동 할당

--- src/main/java/racingcar/CarNameParser.java
+++ src/main/java/racingcar/CarNameParser.java
@@ -1,4 +1,49 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CarNameParser {
+    private static int emptyCarNameCount = 1;
+
+    public static List<String> parse(String input) {
+        validateInput(input);
+        return processInput(input);
+    }
+
+    private static void validateInput(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("경기에 진행할 자동차가 존재하지 않습니다");
+        }
+    }
+
+    private static List<String> processInput(String input) {
+        List<String> carList = Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(CarNameParser::processCarName)
+                .collect(Collectors.toList());
+        return carList;
+    }
+
+    private static String processCarName(String carName) {
+        validateCarName(carName);
+        carName = replaceEmptyCarName(carName);
+
+        return carName;
+    }
+
+    private static void validateCarName(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 5자를 초과할 수 없습니다: " + carName);
+        }
+    }
+
+    private static String replaceEmptyCarName(String carName) {
+        if (carName.isBlank()) {
+            return "무명" + emptyCarNameCount++;
+        }
+        return carName;
+    }
+
 }
