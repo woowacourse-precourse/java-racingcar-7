341adf2cc58b47f7d02fb9658e46d73cee4ede58
wodus4145@naver.com <wodus4145@naver.com>
2024-10-27 14:35:01 +0900

    refactor: Modify InputHandler
    InputHandler에서 받는 자동차 이름과 경기 횟수를 검증하는 기능 추가

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -10,29 +10,41 @@ public class InputHandler {
     public List<Car> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputNames = Console.readLine();
-        String[] names = devideNames(inputNames);
+        String[] names = divideNames(inputNames);
+        validateNames(names);
         return makeNameList(names);
     }
 
-    public List<Car> makeNameList(String[] names) throws IllegalArgumentException {
+    public String[] divideNames(String names) {
+        return names.split(",");
+    }
+
+    private void validateNames(String[] names) throws IllegalArgumentException {
+        for(String singleName : names) {
+            if(singleName.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public List<Car> makeNameList(String[] names) {
         List<Car> racingCar = new ArrayList<>();
         for (String name : names) {
-            if (name.length() > 5) {
-                throw new IllegalArgumentException();
-            } else {
-                racingCar.add(new Car(name, 0));
-            }
+            racingCar.add(new Car(name, 0));
         }
         return racingCar;
     }
 
-    public String[] devideNames(String names) {
-        return names.split(",");
-    }
-
     public int getNumberOfGames() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        int numberOfGames = Integer.parseInt(Console.readLine());
-        return numberOfGames;
+        return validateNumberOfGames(Console.readLine());
+    }
+
+    private int validateNumberOfGames(String input) throws IllegalArgumentException {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
