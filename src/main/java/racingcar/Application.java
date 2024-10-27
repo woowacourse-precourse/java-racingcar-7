1f791c702a42f33e6bc156f06d93d90e64e2fb3b
김주언 <wndjs803@naver.com>
2024-10-27 16:38:04 +0900

    feat: 시도 횟수 입력 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -24,6 +24,8 @@ public class Application {
             cars.add(car);
         }
 
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        Integer tryCount = Integer.parseInt(Console.readLine());
 
     }
 
