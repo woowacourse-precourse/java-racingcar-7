727e47432082c58989914be008c8f304b1cf3450
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 23:43:00 +0900

    feat(CarInfo): 자동차의 상태와 동작을 정의하는 CarInfo 클래스 구현
    - HashMap을 사용해서 자동차의 이름과 현재 위치에 대한 상태를 정의함
    - 생성자를 이용해서 문자열 리스트에 담긴 자동차들의 이름을 HashMap에 대입함
    - moveForward() 메서드를 이용해서 자동차의 현재 위치를 한 칸 전진시키는 동작을 정의함

--- src/main/java/racingcar/Domain/CarInfo.java
+++ src/main/java/racingcar/Domain/CarInfo.java
@@ -1,4 +1,21 @@
 package racingcar.Domain;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class CarInfo {
+    HashMap<String, Integer> carList = new HashMap();
+
+    public CarInfo(List<String> carNames) {
+        for (String carName : carNames) {
+            carList.put(carName, 0);
+        }
+    }
+
+    public void moveForward(String carName, Integer randomNumber) {
+        if (randomNumber >= 4) {
+            int currentPosition = carList.get(carName);
+            carList.put(carName, currentPosition + 1);
+        }
+    }
 }
