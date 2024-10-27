d0d3760439f9bf200ecbc34aeb7a2d077a901c90
sall <keinpark1@naver.com>
2024-10-27 23:12:27 +0900

    feat : 사용자 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        Map<String,Integer> dictionary = new HashMap<>();
+
+        for (String name : input.split(",")) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
+            }
+            dictionary.put(name, 0);
+        }
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int attemptCount = Integer.parseInt(Console.readLine());
+    
     }
 }
