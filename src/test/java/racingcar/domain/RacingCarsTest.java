5bd0c8bd3889ecebe4e596b1f53f925a536cdc21
babyfox225 <babyfox225@naver.com>
2024-10-27 15:48:16 +0900

    test: 중복되지 않는 이름의 자동차 추가 가능 테스트 작성

--- src/test/java/racingcar/domain/RacingCarsTest.java
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -3,9 +3,11 @@ package racingcar.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class RacingCarsTest {
 
@@ -30,6 +32,21 @@ class RacingCarsTest {
     public void cannotCreateRacingCarListWithNull() {
         assertThatThrownBy(() -> new RacingCars(null))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("중복되지 않은 이름을 가진 자동차는 추가할 수 있다.")
+    public void addRacingCarWithNotDuplicatedName() {
+        List<RacingCar> createdCars = new ArrayList<>(List.of(
+                new RacingCar("이미"),
+                new RacingCar("만들어진"),
+                new RacingCar("자동차들")
+        ));
+        RacingCars racingCars = new RacingCars(createdCars);
+
+        RacingCar racingCar = new RacingCar("new");
+
+        assertDoesNotThrow(() -> racingCars.addRacingCar(racingCar));
     }
 
 }
