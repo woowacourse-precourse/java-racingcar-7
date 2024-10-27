77dbe3f39c72bfa679f42e852112db102659db75
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-27 16:02:18 +0900

    Feat: 자동차 이름 입력시 콤마 이외의 구분자 사용할 경우 예외 발생시키는 검증 로직 추가

--- src/main/java/racingcar/service/exception/CarNamesExceptionMessage.java
+++ src/main/java/racingcar/service/exception/CarNamesExceptionMessage.java
@@ -5,6 +5,7 @@ import racingcar.exception.ExceptionMessage;
 public enum CarNamesExceptionMessage implements ExceptionMessage {
 
     NO_INPUT("입력값이 없습니다"),
+    INVALID_SEPARATOR("구분자는 콤마를 사용해야 합니다."),
     ;
 
     private final String message;
