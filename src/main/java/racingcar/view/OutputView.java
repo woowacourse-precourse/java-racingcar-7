c95e78d62b3d5c287bcc2045d33938abb26480b4
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-28 03:20:19 +0900

    feat(OutputView): OutputView 생성 및 자동차 이름 입력 안내 메시지 출력 메서드 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,8 @@
+package racingcar.view;
+
+public class OutputView {
+
+    public void printCarNamesInstruction() {
+        System.out.println(InfoMessage.CAR_NAMES_INSTRUCTION.getMessage());
+    }
+}
