18f52627671df254316e2da5a9e51e9b648fea12
junghun00 <ji8657447@gmail.com>
2024-10-27 16:12:13 +0900

    refactor: 게임 실행 로직 정리

--- src/main/java/racingcar/GameStart.java
+++ src/main/java/racingcar/GameStart.java
@@ -1,13 +1,14 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameStart {
-    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String RESULT_MESSAGE = "\n실행 결과";
     private static final String PRINT_WINNER = "최종 우승자 : ";
 
     public static void run(List<RacingCar> racingCarList, int tryCount) {
-        System.out.println('\n' + RESULT_MESSAGE);
+        System.out.println(RESULT_MESSAGE);
         for (int i = 0; i < tryCount; i++) {
             for (RacingCar racingCar : racingCarList) {
                 racingCar.move();
@@ -16,19 +17,32 @@ public class GameStart {
         }
 
         System.out.print(PRINT_WINNER);
-        int maxmove = 0;
-        String winner = "";
+        IOHandler.printWinner(getWinner(racingCarList));
+    }
+
+    private static String getWinner(List<RacingCar> racingCarList) {
+        List<String> winnerList = new ArrayList<>();
+        int maxMove = getMaxMove(racingCarList);
+
         for (RacingCar racingCar : racingCarList) {
-            if (maxmove < racingCar.getCount()) {
-                maxmove = racingCar.getCount();
-                winner = racingCar.getName();
-            } else if (maxmove == racingCar.getCount()) {
-                winner += ", " + racingCar.getName();
+            if (maxMove == racingCar.getCount()) {
+                winnerList.add(racingCar.getName());
             }
         }
-        if (maxmove == 0) {
+        return String.join(", ", winnerList);
+    }
+
+    private static int getMaxMove(List<RacingCar> racingCarList) {
+        int maxMove = 0;
+        for (RacingCar racingCar : racingCarList) {
+            if (maxMove < racingCar.getCount()) {
+                maxMove = racingCar.getCount();
+            }
+        }
+
+        if (maxMove == 0) {
             throw new IllegalArgumentException();
         }
-        IOHandler.printWinner(winner);
+        return maxMove;
     }
 }
