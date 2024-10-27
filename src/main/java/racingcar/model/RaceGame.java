54ec9ba36444ecefe31097432e1e47d212821646
Parkjyun <jaeyun0507@naver.com>
2024-10-27 22:24:23 +0900

    feat(RaceGame): 1회 round의 race를 할 수 있다.

--- src/main/java/racingcar/model/RaceGame.java
+++ src/main/java/racingcar/model/RaceGame.java
@@ -1,9 +1,19 @@
 package racingcar.model;
 
 public class RaceGame {
-    private RaceCars raceCars;
+    private final RaceCars raceCars;
+    private final Movement movement;
 
-    public RaceGame(RaceCars raceCars) {
+    public RaceGame(RaceCars raceCars, Movement movement) {
         this.raceCars = raceCars;
+        this.movement = movement;
+    }
+
+    public void moveAllCars() {
+        raceCars.moveAllCars(movement);
+    }
+
+    public RaceCars getRaceCars() {
+        return raceCars;
     }
 }
--- src/test/java/racingcar/model/RaceGameTest.java
+++ src/test/java/racingcar/model/RaceGameTest.java
@@ -1,6 +1,7 @@
 package racingcar.model;
 
 import java.util.Arrays;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,9 +17,33 @@ class RaceGameTest {
         Car car2 = new Car(name2);
         RaceCars raceCars = new RaceCars(Arrays.asList(car, car2));
         // when
-        RaceGame raceGame = new RaceGame(raceCars);
+        RaceGame raceGame = new RaceGame(raceCars, new RandomMovement());
         // then
         Assertions.assertThat(raceGame).isNotNull();
+    }
+
+    @Test
+    @DisplayName("매 라운드마다 자동차를 이동시킬 수 있다.")
+    void moveAllCars() {
+        // given
+        Name name1 = new Name("우테코");
+        Name name2 = new Name("박재연");
+        Car car = new Car(name1);
+        Car car2 = new Car(name2);
+        List<Car> cars = Arrays.asList(car, car2);
+        RaceCars raceCars = new RaceCars(cars);
+        List<Car> beforeMovement = raceCars.getCars();
+        Movement alwaysTrueMovement = () -> true;
+        RaceGame raceGame = new RaceGame(raceCars, alwaysTrueMovement);
+
+        //when
+        raceGame.moveAllCars();
+        List<Car> afterMovement = raceGame.getRaceCars().getCars();
+
+        //then
+        for (int i = 0; i < cars.size(); i++) {
+            Assertions.assertThat(afterMovement.get(i).hasBiggerPositionThan(beforeMovement.get(i))).isTrue();
+        }
     }
 
 }
