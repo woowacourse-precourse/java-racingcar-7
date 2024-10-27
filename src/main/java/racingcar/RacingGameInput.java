b38e1ed4b65283b050dc06f672dbfdd5c6fa9d87
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-10-27 20:22:20 +0900

    Docs update README

--- src/main/java/racingcar/RacingGameInput.java
+++ src/main/java/racingcar/RacingGameInput.java
@@ -10,6 +10,7 @@ public class RacingGameInput {
     private static final String CAR_NAMES_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_COUNT_PROMPT = "시도할 횟수는 몇 회인가요?";
 
+    // TODO 참가자들을 나누지 말고 입력을 받기만 할지 고려하기
     public List<String> readCarNames() {
         System.out.println(CAR_NAMES_PROMPT);
         String carsString = Console.readLine();
