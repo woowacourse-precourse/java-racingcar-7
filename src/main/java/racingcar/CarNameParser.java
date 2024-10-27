4fa08cdff2c8342a662258d7bbf8e7e36e2b53a9
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-10-27 21:49:43 +0900

    feat(CarNameParser): 경주할 자동차 이름 입력 기능 구현
    -inputCarNames: 자동차 이름을 입력받음
    -splitCarNamesByComma: 자동차 이름을 쉼표를 기준으로 구분하고 배열로 반환
    -validateCarName: 자동차 이름의 유효성을 검증
    -getValidatedCarNames: 자동차 이름 배열을 검증하고 리스트로 반환

---
+++ src/main/java/racingcar/CarNameParser.java
@@ -0,0 +1,57 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CarNameParser {
+
+    /**
+     * 자동차 이름을 입력받는 메서드
+     *
+     * @return 사용자가 입력한 자동차 이름 문자열
+     * */
+    public String inputCarNames(){
+        return Console.readLine();
+    }
+
+    /**
+     * 자동차 이름을 쉼표를 기준으로 구분하고 배열로 반환하는 메서드
+     *
+     * @param carNames 사용자가 입력한 자동차 이름 문자열
+     * @return 쉼표를 기준으로 분리한 자동차 이름 배열
+     */
+    public String[] splitCarNamesByComma(String carNames){
+        return carNames.split(",");
+    }
+
+    /**
+     * 자동차 이름의 유효성을 검증하는 메서드
+     *
+     * @param carName: 유효성 검증할 자동차 이름
+     * @throws IllegalStateException 자동차 이름이 빈 문자열 인 경우
+     * @throws IllegalStateException 자동차 이름이 5자를 초과할 경우
+     * */
+    public void validateCarName(String carName){
+        if(carName.isEmpty()){
+            throw new IllegalArgumentException("자동차 이름이 빈 문자열 입니다.");
+        }
+
+        if(carName.length() > 5){
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+
+    /**
+     * 자동차 이름 배열을 검증하고 리스트로 반환하는 메서드
+     *
+     * @param splitCarNames 쉼표 기준으로 분리한 문자열 배열
+     * @return 유효성이 검증된 자동차 이름 리스트
+     * */
+    public List<String> getValidatedCarNames(String[] splitCarNames) {
+        return Arrays.stream(splitCarNames).peek(this::validateCarName).toList();
+    }
+
+}
