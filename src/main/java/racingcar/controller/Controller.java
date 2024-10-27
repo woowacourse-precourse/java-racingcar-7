9a5e98519adbfe8b314d2b2f9c5b3b83030f08bc
wooqqq <sge0225@naver.com>
2024-10-27 16:43:08 +0900

    refactor: 컨트롤러 내 기능 메서드 서비스로 이동

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -2,44 +2,21 @@ package racingcar.controller;
 
 import racingcar.model.Car;
 import racingcar.model.RaceService;
-import racingcar.util.Converter;
-import racingcar.view.InputView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
 
-    private final InputView inputView = new InputView();
-    private final Converter converter = new Converter();
     private final RaceService raceService = new RaceService();
 
     public void run() {
-        List<Car> carList = createCarList();
-        String inputTryCnt = inputView.inputTryCnt();
-        int tryCnt = converter.convertTryCnt(inputTryCnt);
+        List<Car> carList = raceService.createCarList();
+        int tryCnt = raceService.getTryCount();
 
         // 테스트용
         for (Car car : carList) {
             System.out.println(car.getName());
         }
-
         System.out.println("시도할 횟수: " + tryCnt);
-    }
-
-    private List<Car> createCarList() {
-        String inputCarList = inputView.inputCarList();
-        String[] carNameList = converter.parseCarList(inputCarList);
-        return convertToCar(carNameList);
-    }
-
-    private List<Car> convertToCar(String[] carNameList) {
-        List<Car> carList = new ArrayList<>();
-        for (String carName : carNameList) {
-            Car car = new Car(carName);
-            carList.add(car);
-        }
-
-        return carList;
     }
 }
--- src/main/java/racingcar/model/RaceService.java
+++ src/main/java/racingcar/model/RaceService.java
@@ -1,8 +1,38 @@
 package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.util.Converter;
+import racingcar.view.InputView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RaceService {
+
+    private final Converter converter = new Converter();
+    private final InputView inputView = new InputView();
+
+    public List<Car> createCarList() {
+        String inputCarList = inputView.inputCarList();
+        String[] carNameList = parseCarNames(inputCarList);
+        return convertToCarList(carNameList);
+    }
+
+    public int getTryCount() {
+        String inputTryCnt = inputView.inputTryCnt();
+        return converter.convertTryCnt(inputTryCnt);
+    }
+
+    private String[] parseCarNames(String inputCarList) {
+        return converter.parseCarList(inputCarList);
+    }
+
+    private List<Car> convertToCarList(String[] carNameList) {
+        return Arrays.stream(carNameList)
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
 
     private int pickRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
