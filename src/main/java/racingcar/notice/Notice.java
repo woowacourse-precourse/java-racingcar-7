d9d0cc28085a16376fbf2cc557fb6cefb83bb6f3
CheonMinJae <cmjcmj1011@gmail.com>
2024-10-27 19:53:03 +0900

    feature: Notice implement
    implement: inputName
    자동차 문자열을 읽어, 검증 후 List<String> 으로 반환
    
    implement: inputGameTime
    게임 횟수를 읽어, 검증 후 Integer 로 반환

---
+++ src/main/java/racingcar/notice/Notice.java
@@ -0,0 +1,37 @@
+package racingcar.notice;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.regex.Pattern;
+
+public class Notice {
+    static public List<String> inputName(Supplier<String> read, List<Predicate<String>> policies) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String content = read.get();
+
+        return Pattern.compile(",")
+                .splitAsStream(content)
+                .map((name) -> isValid(name, policies))
+                .toList();
+    }
+    static public Integer inputGameTime(Supplier<String> read) {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String content = read.get();
+
+        String reg = "\\d+";
+         if(! content.matches(reg)) {
+             throw new IllegalArgumentException("invalid input: " + content);
+         }
+
+         return Integer.parseInt(content);
+    }
+    static private String isValid(String content, List<Predicate<String>> policies) {
+        for (Predicate<String> p : policies) {
+            if (!p.test(content)) {
+                throw new IllegalArgumentException("invalid content: " + content);
+            }
+        }
+        return content;
+    }
+}
