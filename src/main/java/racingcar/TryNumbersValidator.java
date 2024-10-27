044c1c2a4a783ae2194d102150b929b6673b783e
chaerish <ccyy8432@naver.com>
2024-10-27 15:19:55 +0900

    refactor(TryNumbersValidator): TryNumbersValidator로 클래스 이름 수정

--- src/main/java/racingcar/TryNumbersValidator.java
+++ src/main/java/racingcar/TryNumbersValidator.java
@@ -1,7 +1,7 @@
 package racingcar;
 
-public class ValidateTryNums {
-    public static void isNumeric(String tryNums) {
+public class TryNumbersValidator {
+    public static void validateNumeric(String tryNums) {
         for (char num : tryNums.toCharArray()) {
             if (!Character.isDigit(num)) {
                 throw new IllegalArgumentException("숫자가 아닌 값은 올 수 없습니다.");
--- src/test/java/racingcar/TryNumbersValidatorTest.java
+++ src/test/java/racingcar/TryNumbersValidatorTest.java
@@ -6,12 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class ValidateTryNumsTest {
+class TryNumbersValidatorTest {
     @ParameterizedTest
     @ValueSource(strings = {"a", "1a", "2나", "$3"})
     void 시도횟수가_정수가_아닐때(String userInput) {
         assertThrows(IllegalArgumentException.class, () -> {
-            ValidateTryNums.isNumeric(userInput);
+            TryNumbersValidator.isNumeric(userInput);
         });
     }
 
@@ -19,7 +19,7 @@ class ValidateTryNumsTest {
     @ValueSource(strings = {"111", "12", "23", "12321312"})
     void 시도횟수가_정수_일때(String userInput) {
         assertDoesNotThrow(() -> {
-            ValidateTryNums.isNumeric(userInput);
+            TryNumbersValidator.isNumeric(userInput);
         });
     }
 
