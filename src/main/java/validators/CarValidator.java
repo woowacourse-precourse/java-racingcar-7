6e882be3e6272029a79701ab92aee808aac43296
dongyeopwoo <dongyeopwoo@naver.com>
2024-10-27 20:04:42 +0900

    feat(validators): RoundValidator에 라운드 입력 유효성 검사 로직 추가
    - RoundValidator 클래스를 추가하여 사용자로부터 입력받은 라운드 수의 유효성을 검증하는 기능을 구현하였습니다.
    - countValidate(List<Integer> counts) 메서드에서 라운드 수가 0 이하일 경우 예외를 발생시키고, StringValidate(List<String> inputs) 메서드로 빈 입력 및 숫자 형식을 검증합니다.
    - 이러한 유효성 검사를 통해 사용자의 잘못된 입력으로 인한 오류를 사전에 방지합니다.

--- src/main/java/validators/CarValidator.java
+++ src/main/java/validators/CarValidator.java
@@ -1,37 +1,23 @@
 package validators;
 
-import java.util.List;
+import static constant.ErrorValidators.NAME_EMPTY;
+import static constant.ErrorValidators.NAME_LONGER;
+import static constant.ErrorValidators.NAME_OVERLAP;
+import static constant.GameSettings.NAME_MAX;
+
 import java.util.Set;
 
-public class EmptyValidator {
+public class CarValidator {
 
-    private static final int Empty = 0;
-
-    // 횟수 입력 값이 0이거나 0보다 작을 때 오류 메세지 터트리기
-    public static void countValidate(List<Integer> counts) {
-        for (Integer count : counts) {
-            if (count == Empty || count <= Empty) {
-                throw new IllegalArgumentException("횟수는 0 이상이어야 합니다.");
-            }
-        }
-    }
-
-    // 차 이름을 입력 안 했을 때 오류 메세지 터트리기
     public static void validateCarName(String name, Set<String> uniqueNames) {
         if (name.isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름은 비어 있을 수 없습니다.");
+            throw new IllegalArgumentException(NAME_EMPTY.getMessage());
         }
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5글자를 넘을 수 없습니다.");
+        if (name.length() > NAME_MAX.getMessage()) {
+            throw new IllegalArgumentException(NAME_LONGER.getMessage());
         }
         if (!uniqueNames.add(name)) {
-            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다 : " + name);
-        }
-    }
-
-    public static void otherWinners(String name){
-        if(name == name){
-            System.out.println("우승자가 한 명 입니다.");
+            throw new IllegalArgumentException(NAME_OVERLAP.getMessage() + name);
         }
     }
 }
