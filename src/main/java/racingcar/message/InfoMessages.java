85851536b40e71351f52ba849784eed58b5dd00f
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 11:57:29 +0900

    :sparkles: feat(InfoMessages): 경주할 자동차 이름을 입력하라는 메시지 제공

--- src/main/java/racingcar/message/InfoMessages.java
+++ src/main/java/racingcar/message/InfoMessages.java
@@ -1,4 +1,7 @@
 package racingcar.message;
 
 public class InfoMessages {
+
+    // Input related info messages
+    public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 }
