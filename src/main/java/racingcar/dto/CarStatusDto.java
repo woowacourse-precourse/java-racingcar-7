9be52be19864c7194030203d3b6a14a589369d4e
변우영 <likevangogh24@gmail.com>
2024-10-27 15:04:24 +0900

    style(CarStatusDto): MOVE_DASH -> POSITION_MAKER 로 상수 이름 변경

--- src/main/java/racingcar/dto/CarStatusDto.java
+++ src/main/java/racingcar/dto/CarStatusDto.java
@@ -3,10 +3,10 @@ package racingcar.dto;
 public record CarStatusDto(String name, int position) {
 
     private static final String NAME_POSITION_SEPARATOR = " : ";
-    private static final String MOVE_DASH = "-";
+    private static final String POSITION_MAKER = "-";
 
     @Override
     public String toString() {
-        return name + NAME_POSITION_SEPARATOR + MOVE_DASH.repeat(position);
+        return name + NAME_POSITION_SEPARATOR + POSITION_MAKER.repeat(position);
     }
 }
