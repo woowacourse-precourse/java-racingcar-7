b05be2ddecd808ce59d9c6b4d757a73eff8a17f4
chahyunsoo <chahs1012@naver.com>
2024-10-27 22:45:17 +0900

    feat(OutputViewImpl) : OutputView 구현체 OutputViewImpl 클래스 추가
    * OutputView 인터페이스를 구현하여 실행 결과와 우승자를 출력하는 OutputViewImpl 클래스를 정의했습니다.
    
    * 이전에 상수로 정의해두었던 Message 클래스의 상수를 이용합니다.

---
+++ src/main/java/racingcar/view/impl/OutputViewImpl.java
@@ -0,0 +1,22 @@
+package racingcar.view.impl;
+
+import racingcar.constant.Message;
+import racingcar.dto.Result;
+import racingcar.view.OutputView;
+
+public class OutputViewImpl implements OutputView {
+    @Override
+    public void printExecutionMessage() {
+        System.out.println(Message.EXECUTION_RESULT_HEADER);
+    }
+
+    @Override
+    public void printResult(Result result) {
+        System.out.println(result.toIntermediateResult());
+    }
+
+    @Override
+    public void printFinalWinner(String finalWinner) { // String 타입으로 변경
+        System.out.println(finalWinner);
+    }
+}
