61b8ac5082f9df632eef8d61fe87521877416ed1
Changhyun <tabloni.dev@gmail.com>
2024-10-28 02:16:14 +0900

    feat : 최종 우승자 출력 기능 추가

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -3,18 +3,22 @@ package racingcar.view;
 
 import java.util.List;
 import racingcar.model.Car;
+import java.util.stream.Collectors;
 
 
 public class ResultView {
-   public static void printRaceResult(List<Car> cars) {
-       for (Car car : cars) {
-           System.out.println(car.getName() + " : " + "-".repeat(car.getPosition().intValue()));
-       }
-       System.out.println();
-   }
+    public static void printRaceResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition().intValue()));
+        }
+        System.out.println();
+    }
 
 
-   public static void printWinners(List<Car> winners) {
-   }
-  
+    public static void printWinners(List<Car> winners) {
+        String winnerNames = winners.stream()
+                                    .map(Car::getName)
+                                    .collect(Collectors.joining(", "));
+        System.out.println("최종 우승자 : " + winnerNames);
+    }
 }
