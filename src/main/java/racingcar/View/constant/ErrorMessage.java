ff105a3a651f6fc0bcc3eefdf25d54cd65586a97
rootTiket <l20078829@gmail.com>
2024-10-27 16:20:24 +0900

    feat: 중복 예외처리를 위한 메시지 구현

--- src/main/java/racingcar/View/constant/ErrorMessage.java
+++ src/main/java/racingcar/View/constant/ErrorMessage.java
@@ -5,7 +5,8 @@ import racingcar.global.globalEnum.Message;
 public enum ErrorMessage implements Message {
     TOO_LONG_VALUE("문자열은 5글자를 넘을 수 없습니다"),
     BLANK_VALUE("문자열은 비어있을 수 없습니다"),
-    INVALID_INTEGER("시도횟수는 정수만 입력할 수 있습니다");
+    INVALID_INTEGER("시도횟수는 정수만 입력할 수 있습니다"),
+    DUPLICATED("이름은 중복될 수 없습니다.");
     private final String message;
 
     ErrorMessage(String message) {
