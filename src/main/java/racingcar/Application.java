3d7f688031fc103ebfcb25d94a000eab362d9a86
고경우 <dfdfg11@gmail.com>
2024-10-27 21:22:50 +0900

    feat: cars 이름 리스트로 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,31 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+
+
+    List<String>getCarNames(String cars){
+        return Arrays.stream(cars.split(","))
+                .map(String::trim).toList();
+    };
+
+
+/*    initializeCars(List<String> carNames)
+    generateRandomNumber()
+
+    canMove(int randomNumber)
+    moveCar(Car car, int randomNumber)
+    playRound(List<Car> cars)
+    getWinners(List<Car> cars)
+
+    validateCarName(String carName)
+    validateAttemptCount(String attemptCount)*/
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
     }
 }
