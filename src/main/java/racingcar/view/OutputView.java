9de978c73c20fdde7cc62f232d9d9f15c9c44d8b
jae woo Jung <awfjol2008@gmail.com>
2024-10-27 22:34:45 +0900

    feat: 경주 시도 횟수 입력 프롬프트 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
 	public void printCarNameInputPrompt() {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 	}
+
+	public void printAttemptCountInputPrompt() {
+		System.out.println("시도할 횟수는 몇 회인가요?");
+	}
 }
