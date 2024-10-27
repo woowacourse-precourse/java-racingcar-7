f2f8d2673a2f5910e2bc22a2d7e781391205a204
bellmin <whdlas011203@naver.com>
2024-10-27 16:53:33 +0900

    feat(Car): nameValidation 메소드 추가
    - 이름의 길이가 5자 초과일 경우, IllegalArgumentException throw

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -6,11 +6,16 @@ public class Car {
     private int location;
 
     public Car(String name) {
+        nameValidation(name);
         this.name = name;
         this.location = 0;
     }
 
     public void forwardOne(){
         this.location++;
+    }
+
+    private void nameValidation(String name){
+        if(name.length() > 5) throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
     }
 }
