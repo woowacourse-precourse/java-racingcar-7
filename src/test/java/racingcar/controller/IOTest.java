fd713f20f0c8925a71a7ebebb16d8bafaebd2f54
ljhee92 <weilainib7@gmail.com>
2024-10-27 20:35:18 +0900

    test: 출력값 테스트 콘솔로 확인할 수 있는 기능 추가
    실제 출력값이 어떻게 나오는지 콘솔로 확인할 수 있는 기능 추가
    경기 라운드를 추가하여 랜덤값도 추가하여 테스트

--- src/test/java/racingcar/controller/IOTest.java
+++ src/test/java/racingcar/controller/IOTest.java
@@ -1,5 +1,6 @@
 package racingcar.controller;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.ByteArrayOutputStream;
@@ -7,12 +8,20 @@ import java.io.PrintStream;
 
 public abstract class IOTest {
     private ByteArrayOutputStream outputStream;
+    private PrintStream printStream;
 
     @BeforeEach
     void setUp() {
+        printStream = System.out;
         outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
     } // setUp
+
+    @AfterEach
+    protected void printResult() {
+        System.setOut(printStream);
+        System.out.println(getOutput());
+    } // printResult
 
     protected String getOutput() {
         return outputStream.toString();
--- src/test/java/racingcar/controller/RacingCarControllerTest.java
+++ src/test/java/racingcar/controller/RacingCarControllerTest.java
@@ -16,23 +16,23 @@ class RacingCarControllerTest extends IOTest {
     @Test
     @DisplayName("경주별 결과 출력")
     void printRaceResult() {
-        RaceCount raceCount = new RaceCount(1);
+        RaceCount raceCount = new RaceCount(2);
         Cars cars = new Cars(List.of(new Car("pobi"), new Car("woni")));
         RacingCarController racingCarController = new RacingCarController();
 
         assertRandomNumberInRangeTest(
                 () -> {
                     racingCarController.playRace(raceCount, cars);
-                    assertThat(getOutput()).contains("pobi : -", "woni : ");
+                    assertThat(getOutput()).contains("pobi : -", "woni : ", "pobi : --", "woni : -");
                 },
-                4, 3
+                4, 3, 4, 4
         );
     } // printRaceResult
 
     @Test
     @DisplayName("최종 우승자 출력 - 단독")
     void printWinner() {
-        RaceCount raceCount = new RaceCount(1);
+        RaceCount raceCount = new RaceCount(2);
         Cars cars = new Cars(List.of(new Car("pobi"), new Car("woni")));
         RacingCarController racingCarController = new RacingCarController();
 
@@ -42,7 +42,7 @@ class RacingCarControllerTest extends IOTest {
                     racingCarController.judgeWinnerCars(cars);
                     assertThat(getOutput()).contains("최종 우승자 : pobi");
                 },
-                4, 3
+                4, 3, 4, 4
         );
     } // printWinner
 
