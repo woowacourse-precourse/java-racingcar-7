57715aeb3d34a79646134d60040919e60bd314ad
yunhacandy <yunhacandy@gmail.com>
2024-10-27 22:42:35 +0900

    feat (Application): 자동차 경주 전체 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import racingcar.controller.RaceController;
+import racingcar.service.RacingGameService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        RacingGameService racingGameService = new RacingGameService();
+        RaceController controller = new RaceController(inputView, outputView, racingGameService);
+
+        controller.run();
     }
 }
