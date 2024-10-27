dd76860977bb30236486fd1a089f1a224c2167f1
YujinJuhn <holsya@naver.com>
2024-10-28 08:15:16 +0900

    feat: 사용자 입력 처리 및 예외 처리 기능 구현(자동차 이름, 시도 횟수)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.utils.InputHandler;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
+
+        List<String> carNames = inputHandler.getCarNames();
+        int attemptCount = inputHandler.getAttemptCount();
     }
 }
--- src/main/java/racingcar/utils/InputHandler.java
+++ src/main/java/racingcar/utils/InputHandler.java
@@ -1,4 +1,39 @@
 package racingcar.utils;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class InputHandler {
+
+    public List<String> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        List<String> carNames = Arrays.asList(input.split(","));
+
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력해야 합니다.");
+            }
+        }
+
+        return carNames;
+    }
+
+    public int getAttemptCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int attemptCount;
+
+        try {
+            attemptCount = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 숫자로 입력해야 합니다.");
+        }
+
+        if (attemptCount <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+        }
+        return attemptCount;
+    }
 }
