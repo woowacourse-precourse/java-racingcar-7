e2a184f920bf2b4134f4e37bae471fff872af71d
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-10-27 15:27:53 +0900

    feat : Assign each car a random value between 0 and 9
    - 랜덤값 부여를 위해 Randoms 라이브러리를 사용함.
    
    add :
    Lis의 요소를 동적으로 변경하기 위해 new ArrayList<> 추가.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,18 +1,23 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         String carName = Console.readLine();
-        List<String> splitName = List.of(carName.split(","));
+        List<String> splitName = new ArrayList<>(List.of(carName.split(",")));
 
         for (int i = 0; i < splitName.size(); i++) {
+
             if (splitName.get(i).length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 최대 5자입니다.");
             }
+
+            splitName.set(i, String.valueOf((Randoms.pickNumberInRange(0, 9))));
         }
     }
 }
