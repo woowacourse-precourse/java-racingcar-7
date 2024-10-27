553e25347269d264c3ce1016e4f6d427589433b5
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-10-27 22:41:50 +0900

    test(ApplicationTest) : 기능 1.1 테스트 코드 추가
    - 자동차 목록을 입력하라는 출력문이 콘솔에 정상적으로 출력되는지 확인
    - 입력으로 들어온 문자열이 정상적으로 저장되는지 확인

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -30,6 +32,19 @@ class ApplicationTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void 테스트_1_1_자동차_이름_입력() {
+        String simulatedInput = "a,b,c,d\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes(StandardCharsets.UTF_8));
+        System.setIn(inputStream);
+
+        String carNamesInputString = Application.getCarNamesInputString();
+
+        assertThat(output()).contains("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        assertThat(carNamesInputString).isEqualTo("a,b,c,d");
+    }
+
 
     @Override
     public void runMain() {
