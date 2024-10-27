4d2c1c14a31de18270f981f8e8d06ad1de5f10a6
rootTiket <l20078829@gmail.com>
2024-10-27 17:12:09 +0900

    feat: 정수범위 예외처리를 위한 메시지 구현

--- src/main/java/racingcar/View/constant/ErrorMessage.java
+++ src/main/java/racingcar/View/constant/ErrorMessage.java
@@ -6,7 +6,8 @@ public enum ErrorMessage implements Message {
     TOO_LONG_VALUE("문자열은 5글자를 넘을 수 없습니다"),
     BLANK_VALUE("문자열은 비어있을 수 없습니다"),
     INVALID_INTEGER("시도횟수는 정수만 입력할 수 있습니다"),
-    DUPLICATED("이름은 중복될 수 없습니다.");
+    DUPLICATED("이름은 중복될 수 없습니다."),
+    OVER_VALUE("정수값 이상으로 입력할 수 없습니다.");
     private final String message;
 
     ErrorMessage(String message) {
