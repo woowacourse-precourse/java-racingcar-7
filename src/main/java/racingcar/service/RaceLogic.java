5a08c59659b0c454d943557af93b5b456fcec747
DSRN0000 <dsrn0000@gmail.com>
2024-10-27 23:40:37 +0900

    (feat) 전진 결정 기능 개발

---
+++ src/main/java/racingcar/service/RaceLogic.java
@@ -0,0 +1,35 @@
+package racingcar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RaceLogic {
+
+    private static List<Integer> raceResult;
+
+    private static List<String> names;
+    private static int number;
+    private MoveDecision moveDecision;
+
+    public RaceLogic(List<String> names, int number, MoveDecision moveDecision) {
+        this.raceResult = new ArrayList<>();
+        this.number = number;
+        this.names = names;
+        this.moveDecision = moveDecision;
+        for (int i = 0; i < names.size(); i++) {
+            raceResult.add(0);
+        }
+    }
+
+    public void race(List<Boolean> isFourResult) {
+        for (int i = 0; i < raceResult.size(); i++) {
+            if (isFourResult.get(i)) {
+                raceResult.set(i, raceResult.get(i) + 1);
+            }
+        }
+    }
+
+    public List<Integer> getRaceResult() {
+        return raceResult;
+    }
+}
