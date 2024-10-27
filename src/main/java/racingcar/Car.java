a21830127f72284011effb0861c91f53a0932008
minjoon-98 <4kmj54321@gmail.com>
2024-10-28 03:24:16 +0900

    feat: create Car class for racing car game

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move() {
+        position++;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + "-".repeat(position);
+    }
+}
