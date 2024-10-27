be0ac553cf37333b66d5df780d9f29c83559d961
GaBaljaintheroom <junsu1222@naver.com>
2024-10-27 18:35:25 +0900

    feat(Car): equals 메서드 오버라이딩

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -1,5 +1,7 @@
 package racingcar.car;
 
+import java.util.Objects;
+
 public abstract class Car {
 
     protected final String name;
@@ -20,5 +22,22 @@ public abstract class Car {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return Objects.equals(getName(), car.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getName());
     }
 }
