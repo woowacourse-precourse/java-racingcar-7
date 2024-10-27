a822879a5c7b4188fd2c39020ae7dab969f9fe10
Koo-EunSung <ku903@naver.com>
2024-10-27 16:16:10 +0900

    Test: 자동차 전진

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -15,4 +15,23 @@ public class CarTest {
             Car car = new Car(name);
         });
     }
+
+    @Test()
+    @DisplayName("난수가 4이상이면 전진한다")
+    void 전진_테스트_4이상() {
+        Car car = new Car("temp");
+
+        car.go(4);
+
+        assertThat(car).extracting("step").isEqualTo(1);
+    }
+
+    @Test()
+    @DisplayName("난수가 4미만이면 전진하지 않는다")
+    void 전진_테스트_4미만() {
+        Car car = new Car("temp");
+
+        car.go(3);
+        assertThat(car).extracting("step").isNotEqualTo(1);
+    }
 }
