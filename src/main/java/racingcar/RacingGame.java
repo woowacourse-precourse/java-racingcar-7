cd9b7ba51c16934757d3a5c259d50489dcd60b98
soobin <a01056239778@gmail.com>
2024-10-28 03:10:38 +0900

    chore: Delete unused import
    불필요한 import 삭제

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -3,7 +3,6 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
