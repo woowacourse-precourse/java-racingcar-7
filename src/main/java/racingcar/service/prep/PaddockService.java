5b56a8588d40b0bc18c2b6a4b5c47f97fbc894d7
sytTest <syt1128@naver.com>
2024-10-28 03:22:06 +0900

    :sparkles: (PaddockService) add createPaddock()
    입력받은 경기 정보를 토대로 모든 자동차를 세팅하여 저장된 Paddock을 반환하는 creatPaddock 추가

--- src/main/java/racingcar/service/prep/PaddockService.java
+++ src/main/java/racingcar/service/prep/PaddockService.java
@@ -13,6 +13,16 @@ public class PaddockService {
 
     private final EngineService engineService = new EngineService();
 
+    public Paddock createPaddock(Registration gameInfo) {
+        List<Car> openField = new ArrayList<>();
+        Long oilProvision = gameInfo.getLapCount();
+
+        for (String carName : gameInfo.getLineup()) {
+            openField.add(calibrate(carName, oilProvision));
+        }
+        return new Paddock(openField);
+    }
+
     private Car calibrate(String carName, Long oilProvision) {
         Engine tunedEngine = engineService.tunedEngine(
                 RacingRules.BASIC_ENGINE_PERFORMANCE_REGULATION.status()
