0cc7125b89fab036cdf78446f6b883f6ef4e6d58
haebyun <bhk4392@gmail.com>
2024-10-27 14:46:07 +0900

    feat(Car): 자동차 이름 검증 로직 추가
    - 정적 메서드 `validateName`을 통해 자동차 이름에 대한 검증 로직 추가
    - `validateEmptyName`으로 빈 문자열일 경우 예외 발생하도록 처리
    - `validateNameLength`로 이름의 최대 길이를 초과할 경우 예외 발생하도록 구현

--- src/main/java/racingcar/domain/model/Car.java
+++ src/main/java/racingcar/domain/model/Car.java
@@ -1,6 +1,10 @@
 package racingcar.domain.model;
 
 public class Car {
+    private final static String EMPTY_CAR_NAME_MESSAGE = "자동차 이름이 비어있습니다.";
+    private final static String CAR_NAME_TOO_LONG_MESSAGE = "자동차 이름 %d자 초과 되었습니다.";
+    private final static Integer MAX_CAR_NAME_LENGTH = 5;
+
     private final String name;
     private Integer moveCount;
 
@@ -10,7 +14,27 @@ public class Car {
     }
 
     static Car of(String name) {
+        validateName(name);
         return new Car(name, 0);
+    }
+
+    private static void validateName(String name) {
+        validateEmptyName(name);
+        validateNameLength(name);
+    }
+
+    private static void validateEmptyName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_CAR_NAME_MESSAGE);
+        }
+    }
+
+    private static void validateNameLength(String name) {
+        if (name.length() > MAX_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException(
+                    String.format(CAR_NAME_TOO_LONG_MESSAGE, MAX_CAR_NAME_LENGTH)
+            );
+        }
     }
 
     public String getName() {
