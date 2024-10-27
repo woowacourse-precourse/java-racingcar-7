15abb15921e9f692386a1c1561283bfd028cc18c
jiyun <tomatoziiilll@gmail.com>
2024-10-27 17:48:26 +0900

    feat: 자동차 이름과 횟수 입력받기
    입력받고 유효성 검사를 진행합니다
    자동차 이름은 Map 자료구조의 key값으로 저장합니다
        - 자동차별 진행 상황에 대한 데이터를 쌓아야하기 때문입니다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,39 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Application {
     public static void main(String[] args) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        carNames = (carNames == null) ? "" : carNames;
+        carNames = carNames.trim();
 
+        Map<String, StringBuilder> racingStatus = new HashMap<>();
+        Arrays.stream(carNames.split(","))
+                .forEach(s -> {
+                    if (s.isEmpty() || s.length() > 5) {
+                        throw new IllegalArgumentException("자동차 이름을 입력해주세요");
+                    } else {
+                        racingStatus.put(s.trim(), new StringBuilder());
+                    }
+                });
+
+        System.out.println("시도 할 횟수 몇 회인가요?");
+        String round = Console.readLine();
+        round = (round == null) ? "" : round;
+        round = round.trim();
+        if (round.isEmpty() || !round.matches("\\d+")) {
+            throw new IllegalArgumentException("유효하지 않은 숫자 입니다");
+        }
+
+        System.out.println(racingStatus);
+        System.out.println(round);
     }
 }
