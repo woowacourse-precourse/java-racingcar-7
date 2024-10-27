77b0da8e951b328120efc46b000ef757277bc037
jae woo Jung <awfjol2008@gmail.com>
2024-10-27 23:58:32 +0900

    refactor: OutputView 클래스의 프롬프트 기능 관련 메서드명 일괄 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,11 +1,11 @@
 package racingcar.view;
 
 public class OutputView {
-	public void printCarNameInputPrompt() {
+	public void promptCarName() {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 	}
 
-	public void printAttemptCountInputPrompt() {
+	public void promptAttemptCount() {
 		System.out.println("시도할 횟수는 몇 회인가요?");
 	}
 
