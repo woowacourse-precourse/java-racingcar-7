a5ee6f68bf47d7aa7776c2ad420da427d464a6e0
mete0rfish <sungwon326@naver.com>
2024-10-27 15:30:02 +0900

    fix: add CarMapper parameter
    lapCount parameter is added.

--- src/main/java/racingcar/domain/CarMapper.java
+++ src/main/java/racingcar/domain/CarMapper.java
@@ -5,11 +5,11 @@ import java.util.stream.Collectors;
 
 public class CarMapper {
 
-    public static CarRace mapCarRaceByName(List<String> names) {
+    public static CarRace mapCarRaceByName(List<String> names, String lapCount) {
 
         final List<Car> carList = names.stream()
                 .map(name -> new Car(new CarName(name),0))
                 .toList();
-        return new CarRace(carList);
+        return new CarRace(carList, Integer.parseInt(lapCount));
     }
 }
