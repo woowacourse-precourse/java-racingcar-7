e26c89874337101788693171c432bf114632951e
Lucerna00 <pjs3859811@naver.com>
2024-10-27 16:49:56 +0900

    feat(Output): add car status output method

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -8,8 +8,8 @@ public class Output {
     private static final String NAME_INPUT_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String COUNT_INPUT_PROMPT = "시도할 횟수는 몇 회인가요?";
     private static final String RESULT_PROMPT = "\n실행 결과";
-    private static final String WINNER_PROMPT = "최종 우승자 : ";
-    private static final String WINNER_DELIMITER = ", ";
+    private static final String WINNERS_PROMPT = "최종 우승자 : ";
+    private static final String WINNERS_DELIMITER = ", ";
 
     public void printNameInputPrompt() {
         System.out.println(NAME_INPUT_PROMPT);
@@ -23,8 +23,12 @@ public class Output {
         System.out.println(RESULT_PROMPT);
     }
 
-    public void printWinnerPrompt(List<String> winners) {
-        System.out.println(WINNER_PROMPT
-                + winners.stream().collect(Collectors.joining(WINNER_DELIMITER)));
+    public void printCarStatus(String name, int distance) {
+        System.out.println(name + " : " + "-".repeat(distance));
+    }
+
+    public void printWinnersPrompt(List<String> winners) {
+        System.out.println(WINNERS_PROMPT
+            + winners.stream().collect(Collectors.joining(WINNERS_DELIMITER)));
     }
 }
