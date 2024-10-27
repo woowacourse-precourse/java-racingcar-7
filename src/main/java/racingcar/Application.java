0845f8a6f18880d56bad72b3a516e22ea667159d
@_.mung <wjdahrrla@gmail.com>
2024-10-27 12:26:17 +0900

    Refactor(Application.java): SOLID 원칙에 맞게 의존성 주입 및 객체 재사용 구조 개선해라
    SOLID 원칙에 맞게 의존성 주입 및 객체 재사용 구조 개선함.
    
    수정된 사항:
    - 의존성 주입을 통한 구조 변경
    - 전략 객체 재사용을 통해 불필요한 객체 생성 방지
    - 책임 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,23 +1,38 @@
 package racingcar;
 
 import racingcar.domain.Race;
+import racingcar.strategy.move.MovementStrategy;
 import racingcar.strategy.move.RandomMovementStrategy;
 import racingcar.strategy.winner.MaxPositionWinnerStrategy;
+import racingcar.strategy.winner.WinnerStrategy;
 import racingcar.view.input.InputHandler;
 import racingcar.view.output.RaceResultOutput;
 
 import java.util.Set;
 
 public class Application {
+    private final InputHandler inputHandler;
+    private final MovementStrategy movementStrategy;
+    private final WinnerStrategy winnerStrategy;
+
+    public Application(InputHandler inputHandler, MovementStrategy movementStrategy, WinnerStrategy winnerStrategy) {
+        this.inputHandler = inputHandler;
+        this.movementStrategy = movementStrategy;
+        this.winnerStrategy = winnerStrategy;
+    }
+
     public static void main(String[] args) {
         InputHandler inputHandler = new InputHandler();
+        MovementStrategy movementStrategy = new RandomMovementStrategy();
+        WinnerStrategy winnerStrategy = new MaxPositionWinnerStrategy();
 
+        Application application = new Application(inputHandler, movementStrategy, winnerStrategy);
+        application.run();
+    }
+
+    private void run() {
         Set<String> carNames = inputHandler.inputCarNames();
-
         int attemptCount = inputHandler.inputAttemptCount();
-
-        RandomMovementStrategy movementStrategy = new RandomMovementStrategy();
-        MaxPositionWinnerStrategy winnerStrategy = new MaxPositionWinnerStrategy();
 
         Race race = new Race(carNames, attemptCount, movementStrategy, winnerStrategy);
         race.start();
--- src/main/java/racingcar/view/input/InputHandler.java
+++ src/main/java/racingcar/view/input/InputHandler.java
@@ -5,12 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Set;
 
 public class InputHandler {
-    public static Set<String> inputCarNames() {
+    public Set<String> inputCarNames() {
         System.out.println(InputMessages.INPUT_CAR_NAMES);
         return new CarNameInput().process(Console.readLine());
     }
 
-    public static int inputAttemptCount() {
+    public int inputAttemptCount() {
         System.out.println(InputMessages.INPUT_ATTEMPT_COUNT);
         return new RaceAttemptNumberInput().process(Console.readLine());
     }
