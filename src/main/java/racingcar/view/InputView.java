a567aa6b336610ea235885cff200004565a4e5b9
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-28 05:51:07 +0900

    feat(InputView): 이동 횟수를 입력받는 기능 구현
    - 이동횟수(스테이지)를 입력받습니다.
    - 입력은 string으로 이루어져서 int 형으로 변환하여 반환합니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,16 +4,25 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import racingcar.validator.InputCarNamesValidator;
+import racingcar.validator.InputCountValidator;
 
 public class InputView {
     private static final String INPUT_CAR_NAMES_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_COUNT_PROMPT = "시도할 횟수는 몇 회인가요?";
     private final InputCarNamesValidator inputCarNamesValidator = new InputCarNamesValidator();
+    private final InputCountValidator inputCountValidator = new InputCountValidator();
 
-    // TODO: 경주할 자동차를 입력받는다.
     public List<String> getInputCarNames() {
         System.out.println(INPUT_CAR_NAMES_PROMPT);
         String InputCarNames = Console.readLine();
         return convertCarNamesToList(InputCarNames);
+    }
+
+    public int getInputTryCount() {
+        System.out.println(INPUT_TRY_COUNT_PROMPT);
+        String InputTryCount = Console.readLine();
+        Console.close();
+        return convertTryCountToInt(InputTryCount);
     }
 
     private List<String> convertCarNamesToList (String inputCarNames) {
@@ -25,6 +34,9 @@ public class InputView {
                 .toList();
     }
 
+    private int convertTryCountToInt (String inputTryCount) {
+        int tryCount = Integer.parseInt(inputTryCount);
+        return tryCount;
+    }
 
-    // TODO: 이동할 횟수를 입력받는다.
 }
