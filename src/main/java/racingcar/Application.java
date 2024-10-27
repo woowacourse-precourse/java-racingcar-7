49c0be81eabd367777b3bee8026b935f11a66b51
hekim52 <acrossnightsky@gmail.com>
2024-10-27 20:58:27 +0900

    feat: 자동차 이름, 시도 횟수 입력, 차수별 실행 결과, 우승자 안내 문구 출력
    주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다.
    각 자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
    사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.
    전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
    자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
    우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,63 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carName = readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String countStr = readLine();
+        int count = Integer.parseInt(countStr);
+        System.out.println();
+
+        String[] carlist = carName.split(",");
+        int carListLength = carlist.length;
+        int[] moveList = new int[carListLength];
+
+        System.out.println("실행 결과");
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < carListLength; j++) {
+                int goNum = Randoms.pickNumberInRange(0, 9);
+                if (goNum >= 4) {
+                    moveList[j] += 1;
+                }
+            }
+            for (int k = 0; k < carListLength; k++) {
+                System.out.print(carlist[k] + " : ");
+                for (int l = 0; l < moveList[k]; l++) {
+                    System.out.print("-");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+
+        int maxMove = 0;
+        for (int m = 0; m < carListLength; m++) {
+            if (moveList[m] > maxMove) {
+                maxMove = moveList[m];
+            }
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int n = 0; n < carListLength; n++) {
+            if (moveList[n] == maxMove) {
+                list.add(carlist[n]);
+            }
+        }
+
+        System.out.print("최종 우승자 : ");
+        for (int o = 0; o < list.size() - 1; o++) {
+            System.out.print(list.get(o));
+            System.out.print(", ");
+        }
+        System.out.print(list.get(list.size() - 1));
     }
 }
