fae162c752250b312eb659cc98c1762b5d87708b
seonseon1515 <seonseon1515@gmail.com>
2024-10-28 02:40:25 +0900

    feat: outputProcess() 메서드 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,18 @@
 package racingcar.view;
 
 import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.Car;
 
 public class OutputView {
 
-
+    public void outputProcess(int roundNumber,Car car){
+        for (int i = 0; i < roundNumber; i++) {
+            System.out.print(Car.getCarList().get(i).getName() + " : ");
+            List<String> processList = Car.getCarList().get(i).getMoveList();
+            System.out.print(String.join(", " ,processList.subList(0,i)));
+        }
+    }
     public void outputWinnerList(ArrayList<String> winnerList){
         System.out.print("최종 우승자 : ");
         System.out.print(winnerList.get(0));
