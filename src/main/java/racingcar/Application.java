007bc5a5579cca3f579799ce093b9880fefdbe02
shinebyul <qufdl8382@gmail.com>
2024-10-28 02:07:16 +0900

    [Feat] 최종 자동차 경주 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.controller.RacingManager;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingManager.run();
     }
 }
--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -2,18 +2,21 @@ package racingcar.view;
 
 import java.util.*;
 import racingcar.global.io.Condition;
+import racingcar.global.io.InputMessages;
 import racingcar.model.Car;
 import racingcar.model.Cars;
 
 public class Input {
-    public Cars readCarName(){
+    public static Cars readCarName(){
+        System.out.println(InputMessages.CAR_INPUT_MESSAGE);
+
         Scanner scanner = new Scanner(System.in);
         String carsInput = scanner.nextLine();
         List<Car> cars = carSaparator(carsInput);
 
         return new Cars(cars);
     }
-    private List<Car> carSaparator(String carsInput){
+    private static List<Car> carSaparator(String carsInput){
         String saparator = Condition.SAPARATOR;
 
         String[] carNames = carsInput.split(saparator);
@@ -21,7 +24,7 @@ public class Input {
         List<Car> cars = new ArrayList<>();
 
         for (String name : carNames) {
-            cars.add(new Car(name, 0));
+            cars.add(new Car(name));
         }
 
         return cars;
