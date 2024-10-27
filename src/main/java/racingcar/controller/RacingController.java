6668a5c2759a3bcc78b3a8e7f8edbfaa6a856e15
KSJ0128 <ksj01128@naver.com>
2024-10-27 20:41:42 +0900

    refactor: RacingController 수정
    컨트롤러 내 코드를 가독성 좋게 수정했습니다. InputView, RacingService, WinnerService의
    도입으로 인해 컨트롤러 자체에서는 함수 호출만 수행합니다.
    각 클래스에서 관련 로직을 수행하는 방식으로 코드를 수정했습니다.

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,99 +1,29 @@
 package racingcar.controller;
 
-import racingcar.message.ErrorMessage;
-import racingcar.model.Car;
-import racingcar.model.ProcessedInput;
-import racingcar.model.OriginInput;
-import racingcar.model.Winner;
-import racingcar.util.Splitter;
+import racingcar.dto.RacingInfoDTO;
+import racingcar.dto.RacingInputReq;
+import racingcar.service.RacingService;
+import racingcar.service.WinnerService;
 import racingcar.util.Validator;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.math.BigInteger;
-import java.util.List;
-
+import racingcar.view.InputView;
 public class RacingController {
     public void run() {
         /*
         입력, 레이싱, 최종 우승 자동차 출력 수행
         */
-        OriginInput originInput = new OriginInput();
+        InputView inputView = new InputView();
+        Validator validator = new Validator();
+        RacingService racingService = new RacingService();
+        WinnerService winnerService = new WinnerService();
 
         // 유저 입력 받기
-        originInput.inputCarNames();
-        originInput.inputRaceRounds();
+        RacingInputReq racingInputReq = new RacingInputReq(inputView.inputCarNames(), inputView.inputRaceRounds());
+        RacingInfoDTO racingInfoDTO = validator.validInput(racingInputReq);
 
-        Validator validator = new Validator();
-
-        // 아무 값도 입력되지 않은 경우
-        if (validator.validBlank(originInput.getCarNames(), originInput.getRaceRound())) {
-            throw new IllegalArgumentException(ErrorMessage.EMPTY_ARG.getMessage());
-        }
-
-        Splitter separator = new Splitter(originInput.getCarNames());
-
-        // 시도할 횟수로 수가 아닌 값이 입력된 경우
-        if (validator.validNumber(originInput.getRaceRound())) {
-            throw new IllegalArgumentException(ErrorMessage.NON_NUMBER.getMessage());
-        }
-
-        ProcessedInput processedInput = new ProcessedInput(separator.splitSeparator(), new BigInteger(originInput.getRaceRound()));
-
-        // 자동차 이름 길이가 5보다 큰 경우
-        if (validator.validNameLength(processedInput.getCarList())) {
-            throw new IllegalArgumentException(ErrorMessage.NAME_LENGTH.getMessage());
-        }
-
-        // 자동차 경주 수행
-        racing(processedInput);
-
-        Winner winner = new Winner();
+        // 유저 입력 검증 및 레이싱 수행
+        racingService.racing(racingInfoDTO);
 
         // 우승 자동차 찾기
-        findWinner(processedInput.getCarList(), winner.getWinner());
-
-        // 최종 우승자 출력
-        winner.printWinner();
-    }
-
-    // 자동차 경주
-    public void racing(ProcessedInput processedInput) {
-        System.out.println();
-        for (BigInteger i = BigInteger.ZERO; i.compareTo(processedInput.getRaceRounds()) < 0; i = i.add(BigInteger.ONE)) {
-            raceSingleRound(processedInput.getCarList());
-        }
-    }
-
-    // 레이싱 한 라운드 수행
-    public void raceSingleRound(List<Car> carList) {
-        for (Car car : carList) {
-            if (getRandom() > 3) {
-                car.moveForward();
-            }
-            car.printPosition();
-        }
-        System.out.println();
-    }
-
-    // 무작위 수 구하기
-    public int getRandom() {
-        return Randoms.pickNumberInRange(0,9);
-    }
-
-    public void findWinner(List<Car> carList, List<Car> winner) {
-        BigInteger maxPosition = BigInteger.ZERO;
-
-        for (Car car : carList) {
-            BigInteger curPosition = car.getPosition();
-
-            if (curPosition.compareTo(maxPosition) > 0){
-                maxPosition = curPosition;
-                winner.clear();
-                winner.add(car);
-            }
-            else if (maxPosition.compareTo(curPosition) == 0) {
-                winner.add(car);
-            }
-        }
+        winnerService.findWinner(racingInfoDTO);
     }
 }
