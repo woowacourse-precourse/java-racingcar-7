22028aa0a18d81aabd658f28418dea4c1da17ff6
kimdev <kim__dev@hufs.ac.kr>
2024-10-27 15:47:59 +0900

    feat(Race): 턴 개수 검증 기능을 추가하라

--- src/main/java/racingcar/model/RaceTurn.java
+++ src/main/java/racingcar/model/RaceTurn.java
@@ -4,6 +4,16 @@ public class RaceTurn {
     private int turn;
 
     public RaceTurn(int turn) {
+        validTurn(turn);
         this.turn = turn;
+    }
+
+    private void validTurn(int turn) {
+        if (turn <= 0) {
+            throw new IllegalArgumentException("0 이하의 수는 입력할 수 없습니다.");
+        }
+        if (turn >= 6) {
+            throw new IllegalArgumentException("6 이상의 수는 입력할 수 없습니다.");
+        }
     }
 }
