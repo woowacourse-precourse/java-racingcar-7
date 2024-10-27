5e9e6e3706f0e19145a46a59b0c39d3e7df5afff
jijongkwon <help3451@naver.com>
2024-10-28 01:40:31 +0900

    feat(infrastructure) : InMemoryRacingHistoryRepository 싱글톤 패턴 및 메서드 구현
    - saveRacingHistory : 기록 저장
    - getRacingHistory : 기록반환

--- src/main/java/racingcar/infrastructure/persistence/InMemoryRacingHistoryRepository.java
+++ src/main/java/racingcar/infrastructure/persistence/InMemoryRacingHistoryRepository.java
@@ -7,20 +7,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryRacingHistoryRepository implements RacingHistoryRepository {
-
+    private static final InMemoryRacingHistoryRepository instacne = new InMemoryRacingHistoryRepository();
     private final List<RacingHistory> racingHistorys;
 
-    public InMemoryRacingHistoryRepository() {
+    private InMemoryRacingHistoryRepository() {
         this.racingHistorys = new ArrayList<>();
+    }
+
+    public static InMemoryRacingHistoryRepository getInstance(){
+        return instacne;
     }
 
     // TODO : 레이싱 결과 저장
     public void saveRacingHistory(RacingHistory racingHistory) {
-
+        racingHistorys.add(racingHistory);
     }
 
     // TODO : 레이싱 결과 반환
     public List<RacingHistory> getRacingHistory() {
-        return null;
+        return racingHistorys;
     }
 }
