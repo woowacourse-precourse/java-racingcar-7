d25286092f2f03c60f61f64bac9d5cab3287cee6
EHCTH <lees90896@gmail.com>
2024-10-27 11:43:16 +0900

    feat(ErrorMessage): ErrorMessage static constant

--- src/main/java/racingcar/constant/errormessage/ErrorMessage.java
+++ src/main/java/racingcar/constant/errormessage/ErrorMessage.java
@@ -1,8 +1,10 @@
-package racingcar.constant;
+package racingcar.constant.errormessage;
 
 public class ErrorMessage {
     public static final String ERROR_MESSAGE = "[ERROR] ";
     public static final String WITHIN_RANGE_ERROR_MESSAGE = ERROR_MESSAGE + "이름 길이가 5초과입니다";
     public static final String REGEX_ERROR = ERROR_MESSAGE + "입력 형식이 잘못되었습니다";
     public static final String DUPLICATE_ERROR = ERROR_MESSAGE + "중복된 값이 있습니다";
+    public static final String UNEXPECTED_ERROR = ERROR_MESSAGE + "예상치 못한 에러 발생";
+    public static final String INTEGER_MAX_VALUE_ERROR = ERROR_MESSAGE + "정수형 최대 범위를 넘었습니다";
 }
