33821633c103b65d284bc837b6799959b9445a18
choiyoung69 <choiyoung609@gmail.com>
2024-10-28 04:13:38 +0900

    feat(RacingCarNameDTO): 경주할 자동차가 1개 이하로 입력된 경우 에러 처리

--- src/main/java/racingcar/dto/RacingCarNameDTO.java
+++ src/main/java/racingcar/dto/RacingCarNameDTO.java
@@ -9,6 +9,8 @@ public class RacingCarNameDTO {
     public RacingCarNameDTO(String carNamesInput){
         validateCarNamesInputString(carNamesInput);
         this.carNames = Arrays.asList(carNamesInput.split(","));
+        validateNonEmptyCarNames();
+        validateMinimumCarCount();
     }
 
     private void validateCarNamesInputString(String carNamesInput){
@@ -16,15 +18,16 @@ public class RacingCarNameDTO {
             throw new IllegalArgumentException("자동차 이름을 입력해야 합니다");
     }
 
-    private void validateCarNames(){
-
-    }
-
     private void validateNonEmptyCarNames(){
         for(String carName : carNames){
             if(isBlank(carName))
                 throw new IllegalArgumentException("자동차 이름은 비어 있거나 공백일 수 없습니다.");
         }
+    }
+
+    private void validateMinimumCarCount(){
+        if(carNames.size() < 2)
+            throw new IllegalArgumentException("경주할 자동차는 두 대 이상 입력해야 합니다.");
     }
 
     private boolean isBlank(String inputString){
