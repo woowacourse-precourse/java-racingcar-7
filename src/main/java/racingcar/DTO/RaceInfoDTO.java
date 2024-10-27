4d84eec6795bb801e5aa4e2336cee1fd2af15b92
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 20:12:34 +0900

    feat(RaceInfoDTO): 경주에 사용되는 정보 관련 객체 정의
    - 생성자를 통해서 값을 검증한 뒤에 값을 할당하기 때문에 변경 불가능한 객체로 유지할 수 있게됨

--- src/main/java/racingcar/DTO/RaceInfoDTO.java
+++ src/main/java/racingcar/DTO/RaceInfoDTO.java
@@ -1,4 +1,22 @@
 package racingcar.DTO;
 
+import java.util.List;
+
 public class RaceInfoDTO {
+    private List<String> carNames;
+    private int trialCount;
+
+    public RaceInfoDTO(List<String> carNames, int trialCount) {
+        // TODO - 검증부 구현
+        this.carNames = carNames;
+        this.trialCount = trialCount;
+    }
+
+    public List<String> getCarNames() {
+        return carNames;
+    }
+
+    public int getTrialCount() {
+        return trialCount;
+    }
 }
