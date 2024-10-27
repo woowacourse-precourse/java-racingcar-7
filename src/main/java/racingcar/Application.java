618a5138605c12ef33caf96742c18b9a7aa968f0
alpin87 <qortmdals94@naver.com>
2024-10-27 18:01:46 +0900

    feat: RaceUtil에서 추가한 검증 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,13 +8,20 @@ public class Application {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             String carNames = Console.readLine();
 
+            for (String name : RaceUtil.parseCarNames(carNames)) {
+                Car.validateName(name);
+            }
+
             System.out.println("시도할 횟수는 몇 회인가요?");
-            int attempts = Integer.parseInt(Console.readLine());
+            String attemptsInput = Console.readLine();
+            int attempts = RaceUtil.parseAttempts(attemptsInput);
+            RaceUtil.validateAttempts(attempts);
 
             RaceGame game = new RaceGame(carNames);
             game.playGame(attempts);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
+            throw e;
         }
     }
 }
