6ce2624dc1510f3d15afa8565346a12e23d4cfdc
gyuseon <mn5592@naver.com>
2024-10-27 17:09:39 +0900

    feat: 사용자로부터 자동차 이름 문자열을 입력받는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,7 +3,12 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    
+
+    public String readCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
     public void close() {
         Console.close();
     }
