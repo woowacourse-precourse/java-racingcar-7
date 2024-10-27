09afbc74ec902e7724e411f294fd16d444062311
euni <kongeuni1004@gmail.com>
2024-10-28 00:09:47 +0900

    feat: 자동차 객체 배열 생성 및 이동 기능 구현
    - 문자열 배열을 Car 객체 배열로 변환하는 `mapToCarArray` 메서드 추가
    - 시도 횟수만큼 각 자동차를 이동시키는 `moveCars` 메서드 구현
    - 자동차 이동 로직을 GameController 클래스에서 통제하여 게임 흐름 관리

---
+++ src/main/java/racingcar/GameController.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameController {
+
+    public List<Car> mapToCarArray(String[] cars) {
+        List<Car> carList = new ArrayList<>();
+        for(String car : cars) {
+            carList.add(new Car(car));
+        }
+        return carList;
+    }
+
+    public void moveCars(List<Car> cars, int attemptNumber) {
+        for (int i = 0; i < attemptNumber; i++) {
+            for (Car car : cars) {
+                car.move();
+            }
+        }
+    }
+}
