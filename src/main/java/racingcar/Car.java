04e65cdafef9f5351b4ea74910a0e05263f6942e
euni <kongeuni1004@gmail.com>
2024-10-27 23:27:02 +0900

    feat: 자동차 클래스 구현 및 전진 기능 추가
    - 자동차 이름과 전진한 횟수 초기화 변수 추가 (이름: String, 전진 횟수: int, 초기값 0)
    - 0-9 사이의 랜덤 숫자를 생성하는 메서드 구현
    - 랜덤 숫자가 4 이상일 경우 전진 횟수를 증가시키는 로직 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+
+    private String carName;
+    private int forwardCount;
+
+    public Car(String carName) {
+        this.carName = carName;
+        this.forwardCount = 0;
+    }
+
+    public void move() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= 4) {
+            forwardCount += 1;
+        }
+    }
 }
