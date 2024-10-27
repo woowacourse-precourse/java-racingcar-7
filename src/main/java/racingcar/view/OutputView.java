98e3561867cc273bb3ea2a94b9d4f0f13a49c82c
young-in <hardworkingini@gmail.com>
2024-10-28 00:53:11 +0900

    feat : 우승자(들)을 출력하기 위한 view 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,6 +3,8 @@ package racingcar.view;
 import racingcar.domain.RacingCar;
 import racingcar.domain.RacingCars;
 
+import java.util.List;
+
 public class OutputView {
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
@@ -10,7 +12,8 @@ public class OutputView {
     private static final String EXECUTION_RESULT = "실행 결과";
     private static final String COLON_SIGN = " : ";
     private static final String DISTANCE_SIGN = "-";
-    private static final String SPACE = " ";
+    private static final String FINAL_WINNER = "최종 우승자";
+    private static final String COMMA_SIGN = ", ";
 
     public void printRequestCarNames(){
         System.out.println(INPUT_CAR_NAMES);
@@ -29,12 +32,28 @@ public class OutputView {
         for (RacingCar racingCar : racingCars.getRacingCars()) {
             resultByRound
                     .append(racingCar.getRacingCarName())
-                    .append(SPACE)
                     .append(COLON_SIGN)
-                    .append(SPACE)
                     .append(DISTANCE_SIGN.repeat(racingCar.getLocation()))
                     .append(NEXT_LINE);
         }
         System.out.println(resultByRound);
+    }
+
+    public void printRacingCarWinnerResult(final RacingCars racingCars){
+        List<String> winners = racingCars.findRacingWinners();
+        StringBuilder winnerRacingCars = new StringBuilder();
+        winnerRacingCars
+                .append(FINAL_WINNER)
+                .append(COLON_SIGN);
+        for (int i = 0; i < winners.size(); i++) {
+            winnerRacingCars.append(winners.get(i));
+            if (isJointWinner(i, winners)){
+                winnerRacingCars.append(COMMA_SIGN);
+            }
+        }
+        System.out.println(winnerRacingCars);
+    }
+    private boolean isJointWinner(int idx, List<String> winners) {
+        return idx < (winners.size() - 1);
     }
 }
