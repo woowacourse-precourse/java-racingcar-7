e30a5d34b6d907c3ca8ca35e5e4c2e661ab8e9e5
soeunnPark <carol0724@naver.com>
2024-10-27 20:30:47 +0900

    refactor(RandomRacingStrategyTest): Delete unnecessary annotation
    불필요한 주석 제거

--- src/test/java/racingcar/interfaces/racing/RandomRacingStrategyTest.java
+++ src/test/java/racingcar/interfaces/racing/RandomRacingStrategyTest.java
@@ -1,7 +1,6 @@
 package racingcar.interfaces.racing;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
