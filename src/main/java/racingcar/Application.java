3205e28aa4a26784e1b3844090c009ba8fe5625d
양효빈 <hyobin3026@naver.com>
2024-10-27 12:51:31 +0900

    feat(Application): Implement core functionality in main class

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.factory.RacingCarFactory;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingCarController racingCarController = RacingCarFactory.createController();
+        racingCarController.startRacing();
     }
 }
