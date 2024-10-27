2a81dff529dd8f3f73d5ea25dfc00363e8678bf2
tjdrbs0712 <102494118+tjdrbs0712@users.noreply.github.com>
2024-10-28 01:54:32 +0900

    refactor : Car클래스를 하나의 자동차만 관리하도록 수정

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,45 +3,29 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.validation.Validator;
 
-import java.util.*;
-
 public class Car {
 
-    Map<String, Integer> cars = new HashMap<>();
-    List<String> winnerCars = new ArrayList<>();
+    private String name;
+    private int position;
 
-    public Map<String, Integer> carNameSplit(String carNames){
-        String[] tokens = carNames.split(",");
-
-        for(String car : tokens){
-            Validator.checkNameLength(car);
-            cars.put(car, 0);
-        }
-
-        return cars;
+    public Car(String name) {
+        Validator.checkNameLength(name);
+        this.name = name;
+        this.position = 0;
     }
 
-    public Map<String, Integer> racingTry(Map<String, Integer> cars){
-
-        for (Map.Entry<String, Integer> entry : cars.entrySet()) {
-            int number = Randoms.pickNumberInRange(0, 9);
-            if (number >= 4) {
-                cars.put(entry.getKey(), entry.getValue() + 1);
-            }
+    public void move() {
+        int number = Randoms.pickNumberInRange(0, 9);
+        if (number >= 4) {
+            position++;
         }
-
-        return cars;
     }
 
-    public List<String> getWinningCars(Map<String, Integer> cars){
-        Optional<Integer> maxDistance = cars.values().stream()
-                .max(Integer::compare);
+    public int getPosition() {
+        return position;
+    }
 
-        for (Map.Entry<String, Integer> entry : cars.entrySet()) {
-            if (Objects.equals(entry.getValue(), maxDistance.get())) {
-                winnerCars.add(entry.getKey());
-            }
-        }
-        return winnerCars;
+    public String getName() {
+        return name;
     }
 }
