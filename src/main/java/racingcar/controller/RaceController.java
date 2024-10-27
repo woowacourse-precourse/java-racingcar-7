f82ee34c2b1e62317b1782ba2d99b6fec279f488
kymin <park48664866@gmail.com>
2024-10-27 14:59:32 +0900

    fix($trial): 시도 횟수 입력에 대한 예외처리 추가
    시도 횟수 입력에 대한 예외처리 추가:
    - 시도 횟수에 숫자가 아닌 값이 입력되었을 경우에 대한 예외처리 추가

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -4,11 +4,17 @@ import java.util.List;
 
 import racingcar.domain.Car;
 import racingcar.domain.Race;
+import racingcar.util.ErrorMessage;
 
 public class RaceController {
     // 문자열로 입력된 시도 횟수를 int로 변경
     private int convertTrial(String inputTrial) {
-        return Integer.parseInt(inputTrial);
+        try {
+            return Integer.parseInt(inputTrial);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(ErrorMessage.TRIAL_IS_NOT_NUMBER.getMessage());
+        }
+
     }
 
     // 레이스 생성
