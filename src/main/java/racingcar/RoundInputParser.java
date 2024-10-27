653bbdcc466dbc5fcd9a0fbe7f3b5cda57deaa33
RawBrain <95132660+RawBrain@users.noreply.github.com>
2024-10-28 01:48:51 +0900

    feat(parsing): implement number of round parsing and validation
    입력받은 라운드 횟수 문자열을 파싱하여 int로 변환
    라운드 횟수 유효성 검사 구현
     - 아라비아 숫자가 아닐 시 예외처리
     - 0보다 작은 값 입력 시 예외 처리
     - 값이 입력되지 않거나 공백일 경우 예외 처리

--- src/main/java/racingcar/RoundInputParser.java
+++ src/main/java/racingcar/RoundInputParser.java
@@ -1,4 +1,36 @@
 package racingcar;
 
 public class RoundInputParser {
+
+    public static int parse(String input) {
+        validateInput(input);
+        return processInput(input);
+    }
+
+    private static void validateInput(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("진행할 라운드 값이 입력되지 않았습니다.");
+        }
+    }
+
+    private static int processInput(String input) {
+        int roundNum = inputToRoundNum(input);
+        return validateRoundNum(roundNum);
+    }
+
+    private static int inputToRoundNum(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("아라비아 숫자값을 입력해야 합니다.");
+        }
+    }
+
+    private static int validateRoundNum(int roundNum) {
+        if(roundNum < 0) {
+            throw new IllegalArgumentException("0이상의 횟수를 입력해야 합니다.");
+        }
+        return roundNum;
+    }
+
 }
