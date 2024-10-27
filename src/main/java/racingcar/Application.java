fcbe12d01ff80185e6e67daa3005a4fad1124b2f
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2024-10-27 16:06:16 +0900

    feat : 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,44 @@
 package racingcar;
 
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            // 자동차 이름 입력 받기
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String inputNames = scanner.nextLine();
+            List<String> carNames = Arrays.asList(inputNames.split(","));
+
+            checkCarNames(carNames);
+
+            // 시도 횟수 입력 받기
+            System.out.println("시도할 횟수는 몇 회인가요?");
+            int tryCount = Integer.parseInt(scanner.nextLine());
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("입력 오류: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+
+        
+
+
     }
+
+    private static void checkCarNames(List<String> carNames) {
+        // 자동차 이름 유효성 검사 (5자 이하 확인)
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+            }
+        }
+    }
+
+
 }
