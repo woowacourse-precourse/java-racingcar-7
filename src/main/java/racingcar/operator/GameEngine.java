448ba4181343d3a316ea627e273219d5960de0ad
MinJae00 <exceedmin@gmail.com>
2024-10-28 03:45:12 +0900

    refact: 랜덤 숫자를 뽑고 점수를 업데이트 하는 게임의 실질 동작에 집중하는 클래스로 리팩토링

--- src/main/java/racingcar/operator/GameEngine.java
+++ src/main/java/racingcar/operator/GameEngine.java
@@ -1,45 +1,24 @@
 package racingcar.operator;
 
-import racingcar.controller.GameController;
-import racingcar.user.SettingGameUser;
 import racingcar.user.UserObject;
-import racingcar.view.InputView;
 
 public class GameEngine {
-    UserObject[] user;
-    public static int NUMBER_OF_USERS = 0;
-    public static int TURN_NUMBER = 0;
+    public int randomNum;
 
-    public void progressGame() {
-        startNewTurn();
-        GameController.displayProgress();
+    public void extractRandom() {
+        randomNum = RandomNumberGenerator.generateRandomNumber();
     }
 
-    private void startNewTurn() {
-        for (UserObject users : user) {
-            playUserTurn(users);
-        }
-        GameController.displayProgress();
-    }
-
-    private void playUserTurn(UserObject user) {
-        int randomNum = RandomNumberGenerator.generateRandomNumber();
-        if (checkScoreEligibility(randomNum)) {
+    public void scoreUpdate(UserObject user) {
+        if (checkScoreEligibility()) {
             user.updateUserScore();
         }
     }
 
-    private boolean checkScoreEligibility(int randomNum) {
+    private boolean checkScoreEligibility() {
         if (randomNum >= 4) {
             return true;
-        } else {
-            return false;
         }
-    }
-
-    public GameEngine() {
-        this.user = SettingGameUser.user;
-        NUMBER_OF_USERS = user.length;
-        TURN_NUMBER = InputView.trynum;
+        return false;
     }
 }
