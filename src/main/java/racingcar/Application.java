f3e5d6f2121a0d8783331caf39fa6632663fad71
moon <0917jong@gmail.com>
2024-10-28 00:05:05 +0900

    feat: RacingCar 이동 및 출력
    RacingCar를 임의의 수를 기준으로 이동하는 기능 추가, 현재 이동상태 출력 함수 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,14 @@
 package racingcar;
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Application {
+
+    public static HashMap<String, Integer> carMoveStatus = new HashMap<>();
 
     public static ArrayList<String> splitCarNames(String userInput) {
         ArrayList<String> carNames = new ArrayList<>(Arrays.asList(userInput.split(",")));
@@ -17,7 +21,7 @@ public class Application {
         return carNames;
     }
 
-    public static ArrayList<String> getRacingCars() {
+    public static ArrayList<String> getCarNames() {
         String userInput = Console.readLine();
         return splitCarNames(userInput);
     }
@@ -31,12 +35,46 @@ public class Application {
         }
     }
 
+    public static void initializeCarStatus(ArrayList<String> carNames) {
+        carMoveStatus.clear();
+
+        for (String carName : carNames) {
+            carMoveStatus.put(carName, 0);
+        }
+    }
+
+    public static boolean canMoveCar() {
+        return Randoms.pickNumberInRange(0, 9) >= 4;
+    }
+
+    public static void moveCars(ArrayList<String> carNames) {
+        for (String carName : carNames) {
+            int currentPosition = carMoveStatus.get(carName);
+            if (canMoveCar()) carMoveStatus.put(carName, currentPosition + 1);
+        }
+    }
+
+    public static void printCarStatus(ArrayList<String> carNames) {
+        for (String carName: carNames) {
+            int currentPosition = carMoveStatus.get(carName);
+            System.out.println(carName + " : " + "-".repeat(currentPosition));
+        }
+        System.out.println();
+    }
+
     public static void racingCarGame() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        ArrayList<String> racingCars = getRacingCars();
+        ArrayList<String> carNames = getCarNames();
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         int iteration = getIteration();
+
+        System.out.println("실행 결과");
+        initializeCarStatus(carNames);
+        while (iteration-- > 0) {
+            moveCars(carNames);
+            printCarStatus(carNames);
+        }
     }
 
     public static void main(String[] args) {
