e1aa7a344067fcfa709aea46e0a2a6fd24ac759c
Hyso <hsk7953@gmail.com>
2024-10-28 01:07:15 +0900

    feat(OutputView) : 차수별 진행사항과 우승자 안내 문구 기능 추가 - 차수별 진행 사항을 출력해주는 printRacing 메서드 구현 - 우승자를 안내 해주는 printWinner 메서드 구현 - 코드 포매팅 적용

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,16 +1,35 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+
+import java.util.List;
+
 public class OutputView {
     private OutputView() {
     }
 
-    public static void requestCarNames(){
+    public static void requestCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public static void requestTrialCount(){
+    public static void requestTrialCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
     }
 
+    public static void printRacing(List<Car> racingCars) {
+        StringBuilder currentPosition = new StringBuilder();
+        for (Car car : racingCars) {
+            currentPosition.append(car.getName()).append(" : ")
+                    .append("-".repeat(car.getPosition()))
+                    .append("\n");
+        }
+        System.out.println(currentPosition.toString());
+    }
 
+    public static void printWinner(List<String> winners){
+        StringBuilder result = new StringBuilder();
+        result.append("최종 우승자 : ")
+                .append(String.join(", ", winners));
+        System.out.println(result);
+    }
 }
