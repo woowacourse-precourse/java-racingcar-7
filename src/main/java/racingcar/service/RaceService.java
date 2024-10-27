1198ffd294c6ebec37b3ffe1e5114f1828cf7c49
kaswhy <sxshx818@naver.com>
2024-10-28 01:16:11 +0900

    feat(RaceService): 자동차에 대한 무작위 값 생성 로직 구현
    변경 후
    - 제공된 API 이용하여 0~9 사이의 랜덤값 반환

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -1,10 +1,26 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.LinkedHashMap;
+import racingcar.model.CarName;
 import racingcar.model.CarScores;
+import racingcar.model.Distance;
 
 public class RaceService {
-    public static CarScores playGame(CarScores prevCarScores) {
-        return prevCarScores;
+    public static CarScores playGame(CarScores previousCarScores) {
+        LinkedHashMap<CarName, Distance> nowScores = new LinkedHashMap<>();
+
+        // 전체 맵 돌면서
+        // 랜덤 값 구하기
+        // 전진이 필요한지 확인 후 전진
+        // 결과 출력
+        return previousCarScores;
     }
+
+    private int getRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+
 
 }
