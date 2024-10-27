da4cd2f99273e9d4fc2d54b580b67eaa057307ee
kwon <rnjsdndud00@naver.com>
2024-10-27 12:20:27 +0900

    refactor(IO): 입출력 메소드 추가 확장

--- src/main/java/racingcar/io/CustomIO.java
+++ src/main/java/racingcar/io/CustomIO.java
@@ -1,14 +1,44 @@
 package racingcar.io;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.racer.car.Car;
 
 public class CustomIO {
+
+    private static final int NAME_LENGTH_LIMIT = 5;
+
     public String[] inputCarNames() {
-        String s = Console.readLine();
-        return s.split(",");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String names = Console.readLine();
+        String[] splitNames = names.split(",");
+        for (String name : splitNames) {
+            if (name.length() > NAME_LENGTH_LIMIT) {
+                throw new IllegalArgumentException("이름은 5자 이하로만 입력 가능합니다.");
+            }
+        }
+        return splitNames;
     }
 
     public int inputRepeatNum() {
-        return Integer.parseInt(Console.readLine());
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int num = Integer.parseInt(Console.readLine());
+        System.out.println();
+        return num;
+    }
+
+    public void print(String message) {
+        System.out.println(message);
+    }
+
+    public void printResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + car.getDistance());
+        }
+        System.out.println();
+    }
+
+    public void printWinner(List<String> winners) {
+        System.out.print("최종 우승자 : " + String.join(", ", winners));
     }
 }
