7c845ac4ce25e902b1bac7d99ecea82a3839a4f5
heehunjung <slrkqk2@naver.com>
2024-10-27 16:57:02 +0900

    feat(Car): Car 클래스 생성, move() 메서드 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,2 +1,24 @@
-package racingcar.domain;public class Car {
+package racingcar.domain;
+
+public class Car {
+
+    private String name;
+    private int Location;
+
+    public Car(String name, int Location) {
+        this.name = name;
+        this.Location = Location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLocation() {
+        return Location;
+    }
+
+    public void move() {
+        this.Location++;
+    }
 }
