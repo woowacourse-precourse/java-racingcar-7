3305add8f3ad8d0a0c6b7ee1aee655253b69bc51
jongseo <jongseo0703@naver.com>
2024-10-27 20:07:22 +0900

    feat: 시도할 횟수를 입력하는 기능 구현

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -8,4 +8,23 @@ public class InputView {
         return Console.readLine();
     }
 
+    public int InputTryCounts() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+
+        String input = Console.readLine();
+
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (!isNumeric(input) || Integer.parseInt(input) < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return Integer.parseInt(input);
+    }
+
+    private boolean isNumeric(String input) {
+        return input.matches("\\d+");
+    }
 }
