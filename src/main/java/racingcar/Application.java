8265f4a82eb256e9710077ac9334c0b8100fe322
kwon <rnjsdndud00@naver.com>
2024-10-27 21:09:21 +0900

    refactor(main): 사용자 입력 -> main에서 동작
    내부 로직이 돌아가기 전 사용자 입력을 먼저 받는다.
    입력 받은 값으로 트랙을 생성한 후 경기를 시작한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,17 @@
 package racingcar;
 
+import racingcar.io.CarInputHandler;
 import racingcar.race.track.CarRacetrack;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        new CarRacetrack().raceStart();
+        CarInputHandler inputHandler = new CarInputHandler();
+
+        String[] names = inputHandler.inputRacerNames();
+        int repeat = inputHandler.inputRepeatNum();
+
+        CarRacetrack carRacetrack = new CarRacetrack(names, repeat);
+        carRacetrack.raceStart();
     }
 }
