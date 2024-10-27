77d0b70a75cf2c7020d336e2e800801754a0b97d
josephuk77 <josephuk77@gmail.com>
2024-10-27 17:39:57 +0900

    feat(canMove): 4 이상일 때 전진하는 조건 구현
    자동차가 움직일 수 있는지 판단하는 기능을 추가했습니다. 우아한테크코스에서 제공한는 함수를 사용해서 0~9사이의 랜담한 정수를 받아 4이상인지 판별하는 메서드를 구현했습니다.

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,8 +1,14 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingService {
 
     public int parseAttemptCount(String inputAttemptCount) {
         return Integer.parseInt(inputAttemptCount);
+    }
+
+    public boolean canMove() {
+        return Randoms.pickNumberInRange(0, 9) >= 4;
     }
 }
