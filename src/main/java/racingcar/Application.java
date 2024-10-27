f7c72bad5f0966d2b409530f42d2354e358480b3
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-10-27 21:32:08 +0900

    feat : Accumulate "-" output for cars with vaild movements
    - 전진이 가능한 차는 입력한 이동횟수만큼 각각 "-" 출력을 누적해서 반복함.
    
    - 이동횟수를 추가하고, 이동횟수마다 자동차에 "-"를 출력할때 가독성 및 기능 분리를 위해 round 메서드를 추가함.
    - 자동차마다 무작위 값이 4이상이면 moveValue 값에 1을 더하고 그 값으로 바꿈.
    이 값은 "-"의 횟수를 의미함.
    - 누적된 "-" 횟수만큼 실제 "-"으로 반복해서 출력함.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,9 +8,12 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+
         String carName = Console.readLine();
         List<String> splitName = new ArrayList<>(List.of(carName.split(",")));
-        ArrayList<Integer> moveValue = new ArrayList<>();
+        List<Integer> moveValue = new ArrayList<>();
+
+        int trial = Integer.parseInt(Console.readLine());
 
         for (int i = 0; i < splitName.size(); i++) {
 
@@ -18,12 +21,29 @@ public class Application {
                 throw new IllegalArgumentException("자동차 이름은 최대 5자입니다.");
             }
 
-            int randomNumber = Randoms.pickNumberInRange(0, 9);
-            moveValue.add(randomNumber);
+            moveValue.add(0); // 각 이름의 초기 이동 값을 0으로 설정
+        }
 
-            if (moveValue.get(i) >= 4) {
-                System.out.println(splitName.get(i) + ":" + "-");
+        // 각 라운드별 "-" 출력
+        for (int i = 1; i <= trial; i++) {
+            round(splitName, moveValue);
+            System.out.println(); // 각 라운드 후 빈 줄 추가
+        }
+    }
+
+    // 주어진 라운드에 따라 '-' 출력
+    private static void round(List<String> names, List<Integer> moveValue) {
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.print(names.get(i) + " : ");
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+
+            if (randomNumber >= 4) {
+                moveValue.set(i, moveValue.get(i) + 1); // 누적 이동 값 증가
             }
+
+            // 누적된 "-" 값 출력
+            System.out.println("-".repeat(moveValue.get(i)));
         }
     }
 }
