5d3b9cf9d6778b710d9b1ca7483d92e40ea0aaff
SwnBae <128581113+SwnBae@users.noreply.github.com>
2024-10-28 03:08:15 +0900

    fix: InputView 수정
     Validator 구현에 따른 입력 검증 기능 추가

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -1,6 +1,7 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.validator.Validator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,21 +12,14 @@ public class Input {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
 
-        //Validator 과정 추가 예정
-        List<String> cars = new ArrayList<>();
-
-        for (final String s : input.split(",")) {
-            cars.add(s);
-        }
-        return cars;
+        return Validator.checkName(input);
     }
 
     public int counts() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String input = Console.readLine();
 
-        //Validator 과정 추가 예정
-        return Integer.parseInt(input);
+        return Validator.checkCount(input);
     }
 
 
