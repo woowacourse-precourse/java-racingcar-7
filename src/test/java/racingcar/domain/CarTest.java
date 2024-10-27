1b82197b19ab0a486607f1176bea9158cbf41010
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-27 18:29:51 +0900

    test: Car 클래스의 위치 비교 메서드에 대한 테스트 코드 작성 및 실행
    두 Car 객체의 위치값을 비교하여 boolean형을 반환하는 isCarSamePosition 메서드에 대한 테스트 코드를 작성 및 실행하였다.

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -22,4 +22,31 @@ public class CarTest {
         car.moveForward(true);
         assertThat(car.getCurrentStatus()).isEqualTo("woni : --\n");
     }
+
+    @Test
+    @DisplayName("두 차량의 위치가 같을 경우 메서드는 참을 반환한다.")
+    void compareTwoCarsPositionNotThrowException() {
+        Car pobiCar = new Car("pobi");
+        Car woniCar = new Car("woni");
+
+        pobiCar.moveForward(true);
+        pobiCar.moveForward(true);
+        woniCar.moveForward(true);
+        woniCar.moveForward(true);
+
+        assertThat(pobiCar.isCarSamePosition(woniCar)).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("두 차량의 위치가 다를 경우 메서드는 거짓을 반환한다.")
+    void compareTwoCarsPositionThrowException() {
+        Car pobiCar = new Car("pobi");
+        Car woniCar = new Car("woni");
+
+        pobiCar.moveForward(true);
+        pobiCar.moveForward(true);
+        pobiCar.moveForward(true);
+
+        assertThat(pobiCar.isCarSamePosition(woniCar)).isEqualTo(false);
+    }
 }
