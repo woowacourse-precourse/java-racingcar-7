abb7dbe38974fd4960e101a180a4159a33fa2137
llcodingll <ll.o.llwhkr@gmail.com>
2024-10-28 00:40:38 +0900

    feat: Car 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        position++;
+    }
+}
