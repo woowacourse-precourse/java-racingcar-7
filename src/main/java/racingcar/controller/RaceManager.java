1ea875c5ac48f6c86b2b8ffa11a6be66b639bff0
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:11:27 +0900

    :sparkles: feat(RaceManager): 입력받은 자동차 이름과 실행 횟수를 바탕으로 경주를 진행한다

--- src/main/java/racingcar/controller/RaceManager.java
+++ src/main/java/racingcar/controller/RaceManager.java
@@ -1,4 +1,28 @@
 package racingcar.controller;
 
+import racingcar.model.Cars;
+import racingcar.model.TryCount;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class RaceManager {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void run() {
+        String carNamesInput = inputView.readCarNames();
+        String tryCountInput = inputView.readTryCount();
+
+        Cars cars = new Cars(carNamesInput);
+        TryCount tryCount = new TryCount(tryCountInput);
+
+        outputView.displayRaceResult();
+        int currentTryCount = 0;
+        while (tryCount.isNotSame(currentTryCount)) {
+            cars.race();
+
+            outputView.displayRaceProgress(cars.getCars());
+            currentTryCount++;
+        }
+    }
 }
