2f5e34d45754fd41aa8d33a2809f810f23e07590
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-10-27 18:50:29 +0900

    test: domain 테스트코드 작성
    moveTest: 한칸 전진하는지 테스트
    generateRandomInRangeTest: 지정된 범위 랜덤값 추출테스트
    getWinnerTest: 우승자 테스트
    runGameTest: 게임 진행 테스트

--- src/test/java/racingcar/domain/GameTest.java
+++ src/test/java/racingcar/domain/GameTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -10,30 +11,47 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
-    Game game = new Game(10, null);
+    Game game;
+
+    @BeforeEach
+    public void setUp() {
+        int gameCount = 10;
+        Map<String, Integer> cars = new HashMap<>();
+        cars.put("pobi", 0);
+        cars.put("woni", 0);
+        cars.put("jun", 0);
+        game = new Game(gameCount, cars);
+    }
 
     @Test
     public void moveTest() {
-        assertFalse(game.move(3));
-        assertTrue(game.move(4));
+        game.move("pobi", 3);
+        game.move("woni", 4);
+        Map<String, Integer> cars =  game.getCars();
+
+        assertEquals(0, cars.get("pobi"));
+        assertEquals(1, cars.get("woni"));
+
     }
 
     @Test
     public void generateRandomInRangeTest(){
         int randomNumber = game.generateRandomInRange(0, 9);
+
         assertTrue(randomNumber >= 0 && randomNumber <= 9);
     }
 
     @Test
     public void getWinnerTest() {
+        int gameCount = 10;
         Map<String, Integer> cars = new HashMap<>();
         cars.put("pobi", 3);
         cars.put("woni", 3);
         cars.put("jun", 1);
-        game = new Game(10, cars);
+        game = new Game(gameCount, cars);
+        List<String> winners = game.getWinners();
 
-        List<String> winnerList = game.getWinner();
-        assertTrue(winnerList.containsAll(Arrays.asList("pobi", "woni")));
+        assertTrue(winners.containsAll(Arrays.asList("pobi", "woni")));
 
     }
 
