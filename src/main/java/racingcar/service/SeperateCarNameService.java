a42020d4498549f6630e298a208dc683d52288e4
Jinu Yi <dionisos198@naver.com>
2024-10-27 22:31:58 +0900

    refactor: 피드백 반영, 배열 대신 컬렉션 사용

--- src/main/java/racingcar/service/SeperateCarNameService.java
+++ src/main/java/racingcar/service/SeperateCarNameService.java
@@ -2,6 +2,8 @@ package racingcar.service;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import racingcar.model.Cars;
 
 public class SeperateCarNameService {
@@ -12,7 +14,7 @@ public class SeperateCarNameService {
 
         String[] carNames = carNamesWithDelimeter.split(BASIC_DELIMETER);
 
-        return new Cars(carNames);
+        return new Cars(Arrays.stream(carNames).collect(Collectors.toList()));
 
     }
 
