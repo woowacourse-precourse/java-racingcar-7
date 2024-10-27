d31ab0a67130e8a9e17d92c61455d7e7da9f87bd
Seungwan Yoo <ysw99@kakao.com>
2024-10-27 17:05:24 +0900

    feat: exception enum 제거

--- src/main/java/racingcar/util/ValidateUtil.java
+++ src/main/java/racingcar/util/ValidateUtil.java
@@ -1,28 +1,24 @@
 package racingcar.util;
 
-import racingcar.exception.RaceException;
-
 public class ValidateUtil {
     private static final String SPLIT_REGEX_PREFIX = ",";
     private static final String SPACING_PREFIX = " ";
 
     public static void validateCar(String input) {
         if (input.isBlank() || input.contains(SPACING_PREFIX) || !input.contains(SPLIT_REGEX_PREFIX) || input.split(SPLIT_REGEX_PREFIX).length == 1)
-            throw new IllegalArgumentException(RaceException.WRONG_INPUT.getDescription());
+            throw new IllegalArgumentException("Error in Car name input.");
     }
 
     public static void validateAttemptCount(String attemptCount) {
-        if (attemptCount == null || attemptCount.isBlank())
-            throw new IllegalArgumentException(RaceException.WRONG_INPUT.getDescription());
         try {
             validateNum(Integer.parseInt(attemptCount));
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(RaceException.WRONG_INPUT.getDescription());
+            throw new IllegalArgumentException("Error in attemptCount input.");
         }
     }
 
     public static void validateNum(int attemptCount) {
         if (attemptCount < 1)
-            throw new IllegalArgumentException(RaceException.WRONG_INPUT.getDescription());
+            throw new IllegalArgumentException("Inserted attemptCount is not a number.");
     }
 }
