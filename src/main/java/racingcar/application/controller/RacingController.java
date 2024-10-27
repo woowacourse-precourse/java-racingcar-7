13940b08f220559be8f86b8b432fb1970601fefa
jody816 <81346079+jody816@users.noreply.github.com>
2024-10-27 21:24:32 +0900

    feat: 컨트롤러 의존성 주입 로직 추가

--- src/main/java/racingcar/application/controller/RacingController.java
+++ src/main/java/racingcar/application/controller/RacingController.java
@@ -1,5 +1,18 @@
 package racingcar.application.controller;
 
+import racingcar.application.service.RacingService;
+import racingcar.application.view.input.InputView;
+import racingcar.application.view.output.OutputView;
+
 public class RacingController {
 
+  private final RacingService racingService;
+  private final InputView inputView;
+  private final OutputView outputView;
+
+  public RacingController(RacingService racingService, InputView inputView, OutputView outputView) {
+    this.racingService = racingService;
+    this.inputView = inputView;
+    this.outputView = outputView;
+  }
 }
