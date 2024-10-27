92a80abf14c066c1adfa8695cc5a947d4e261846
bronzehyeon <bronzehyeon@gmail.com>
2024-10-27 20:47:18 +0900

    feat : InputTest 생성
    - 잘못된 입력 테스트 클래스 및 메서드 생성

---
+++ src/test/java/racingcar/InputTest.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class InputTest extends NsTest {
+
+    @Test
+    @DisplayName("긴 차 이름이 입력되는 경우 오류 테스트")
+    void 입력_테스트1() {
+        Assertions.assertThatThrownBy(() -> Input.parseCarNames("pobi,abcdef"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("차 이름이 너무 깁니다.");
+    }
+
+    @Test
+    @DisplayName("쉼표 뒤에 아무것도 오지 않는 경우 입력테스트")
+    void 입력_테스트2() {
+        Assertions.assertThatThrownBy(() -> Input.parseCarNames("pobi,abcde,"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("쉼표는 구분자로만 사용해야 합니다.");
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
