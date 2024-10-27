fb386436682837b038ed4ec19682bc3304e49e6c
psihyeong <qkrtlgud3286@naver.com>
2024-10-27 23:00:40 +0900

    refactor : 패키지명 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,6 @@
 package racingcar;
 
-import racingcar.Controller.CarController;
+import racingcar.controller.CarController;
 
 public class Application {
     public static void main(String[] args) {
--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -1,4 +1,4 @@
-package racingcar.Controller;
+package racingcar.controller;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
