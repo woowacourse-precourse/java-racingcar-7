7e26d158a48fe7bcdc0232998ab85a48ceda15fa
jae woo Jung <awfjol2008@gmail.com>
2024-10-28 01:01:51 +0900

    feat: RacingController 클래스 필드 추가

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,4 +1,9 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class RacingController {
+	private final InputView inputView;
+	private final OutputView outputView;
 }
