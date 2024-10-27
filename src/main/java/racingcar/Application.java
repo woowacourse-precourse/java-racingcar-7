fd82f3b37ba35f779d7af81fa6819f3d829e0e93
Hujin0322 <hongyuj0322@naver.com>
2024-10-27 15:25:41 +0900

    feat: InputHandler class 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
+
+        List<String> carNames = inputHandler.getCarNames();
+        int attemptCount = inputHandler.getAttempCount();
     }
 }
