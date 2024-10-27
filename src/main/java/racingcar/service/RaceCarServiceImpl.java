29f0dbaf255912d1d2690e8914145ef161480a11
18hailey <hailey9459@gmail.com>
2024-10-27 19:09:05 +0900

    refactor(RaceCarServiceImpl): replace for-statement with stream API
    - simplify iteration logic using stream API
    - enhance readability and maintainability of code

--- src/main/java/racingcar/service/RaceCarServiceImpl.java
+++ src/main/java/racingcar/service/RaceCarServiceImpl.java
@@ -2,7 +2,9 @@ package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.domain.Car;
 
 public class RaceCarServiceImpl implements RaceCarService {
@@ -31,24 +33,18 @@ public class RaceCarServiceImpl implements RaceCarService {
 
     @Override
     public String announceWinner() {
-        List<String> winners = new ArrayList<>();
-        int max_dist = 0;
-        for (Car car : carNames) {
-            int car_dist = car.getDist();
-            if (car_dist > max_dist) {
-                max_dist = car_dist;
-            }
-        }
+        final int max_dist = carNames.stream()
+                .map(Car::getDist)
+                .max(Comparator.naturalOrder())
+                .orElse(0);
 
         if (max_dist == 0) {
             throw new IllegalArgumentException("[ERROR] 전진 거리가 모두 0이므로 우승자가 없습니다.");
         }
 
-        for (Car car : carNames) {
-            if (max_dist == car.getDist()) {
-                winners.add(car.getName());
-            }
-        }
-        return String.join(", ", winners);
+        return carNames.stream()
+                .filter(car -> max_dist == car.getDist())
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
     }
 }
