5f99762abc47913ed5b848d7aea38634994cd30a
pokm321 <pokm90@gmail.com>
2024-10-27 18:12:18 +0900

    style: ensure consistency in variable names and java conventions
    cars_list -> carsListed
    cars_parsed -> carsParsed
    names -> cars
    input -> rounds

--- src/main/java/racingcar/model/InputParser.java
+++ src/main/java/racingcar/model/InputParser.java
@@ -8,17 +8,17 @@ import racingcar.validators.InputValidator;
 public class InputParser {
 
     public LinkedHashMap<String, Integer> parseCars(String cars) {
-        List<String> cars_list = Arrays.asList(cars.replace(" ", "").split(",", -1));
+        List<String> carsListed = Arrays.asList(cars.replace(" ", "").split(",", -1));
 
         InputValidator validator = new InputValidator();
-        validator.validateCars(cars_list);
+        validator.validateCars(carsListed);
 
-        LinkedHashMap<String, Integer> cars_parsed = new LinkedHashMap<>();
-        for (String car : cars_list) {
-            cars_parsed.put(car, 0);
+        LinkedHashMap<String, Integer> carsParsed = new LinkedHashMap<>();
+        for (String car : carsListed) {
+            carsParsed.put(car, 0);
         }
 
-        return cars_parsed;
+        return carsParsed;
     }
 
     public int parseRounds(String rounds) {
