b98f6dd6b7f13756f91fc62eae66d8d0dece718b
LEEJaeHyeok97 <hazardous10@naver.com>
2024-10-27 13:46:01 +0900

    feat: 게임 실행 기능 구현

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 public class Game {
     private final int tryCount;
+    private final List<Car> cars;
 
-    public Game(int tryCount) {
+    public Game(int tryCount, List<Car> cars) {
         this.tryCount = tryCount;
+        this.cars = cars;
     }
 
     public void run() {
+        cars.forEach(Car::move);
     }
 }
