c1264809811723c70b343ab0a0d010c72781b273
gyuoo <yeomky98@naver.com>
2024-10-28 02:46:19 +0900

    feat(error): add error message for empty race count input

--- src/main/java/racingcar/error/ErrorMessage.java
+++ src/main/java/racingcar/error/ErrorMessage.java
@@ -3,7 +3,8 @@ package racingcar.error;
 public enum ErrorMessage {
 	INVALID_CAR_NAME("자동차 이름은 1자 이상 5자 이하여야 합니다."),
 	INVALID_RACE_COUNT("이동 시도 횟수는 1 이상, 100 이하의 정수여야 합니다."),
-	EMPTY_CAR_NAME("자동차 이름을 입력해 주세요.");
+	EMPTY_CAR_NAME("자동차 이름을 입력해 주세요."),
+	EMPTY_RACE_COUNT("이동 시도 횟수를 입력해 주세요.");
 
 	private final String message;
 
