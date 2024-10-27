43ac1fbbf0763720070dd5c635970c93d2ec5950
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-28 05:19:17 +0900

    feat(InfoMessage): 시도 횟수 입력 안내 메시지 추가

--- src/main/java/racingcar/view/InfoMessage.java
+++ src/main/java/racingcar/view/InfoMessage.java
@@ -2,7 +2,8 @@ package racingcar.view;
 
 public enum InfoMessage {
 
-    CAR_NAMES_INSTRUCTION("자동차 이름을 입력해주세요. (이름은 쉼표(,) 기준으로 구분)");
+    CAR_NAMES_INSTRUCTION("자동차 이름을 입력해주세요. (이름은 쉼표(,) 기준으로 구분)"),
+    MOVING_COUNT_INSTRUCTION("시도할 회수는 몇 회인가요?");
 
     private final String message;
 
