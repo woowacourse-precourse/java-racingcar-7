be07c312e7d35b98b80c19175fb23d63665ad7d5
young-in <hardworkingini@gmail.com>
2024-10-27 23:17:57 +0900

    feat : racingCar 의 전진 및 정지 조건 추가

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,6 +1,8 @@
 package racingcar.domain;
 
 public class RacingCar {
+    private static final int MOVING_CONDITION = 4;
+
     private RacingCarName racingCarName;
     private Location location;
 
@@ -9,8 +11,18 @@ public class RacingCar {
         this.location = location;
     }
 
+    public void move(final int randomNumber){
+        if(randomNumber >= MOVING_CONDITION){
+            this.location = location.increase();
+        }
+    }
+
     public RacingCarName getRacingCarName() {
         return racingCarName;
+    }
+
+    public Location getLocation() {
+        return location;
     }
 }
 
