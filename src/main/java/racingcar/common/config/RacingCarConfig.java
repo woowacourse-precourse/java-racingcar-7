edea95c38a72854adfa143396630631f76042cb7
soeunnPark <carol0724@naver.com>
2024-10-27 20:32:20 +0900

    Config(Application): Add a Config file for Dependency Injection
    Application 의 실행을 위해 RacingCarConfig 파일을 정의해 각각의 클래스에 의존성을 주입해줍니다.

---
+++ src/main/java/racingcar/common/config/RacingCarConfig.java
@@ -0,0 +1,25 @@
+package racingcar.common.config;
+
+import racingcar.domain.car.CarFactory;
+import racingcar.domain.car.CarService;
+import racingcar.domain.racing.JudgeService;
+import racingcar.domain.racing.RacingService;
+import racingcar.interfaces.input.InputHandlerImpl;
+import racingcar.interfaces.output.OutputHandler;
+import racingcar.interfaces.racing.RacingController;
+import racingcar.interfaces.racing.RandomRacingStrategy;
+
+public class RacingCarConfig {
+
+    public RacingController getRacingController() {
+        CarFactory carFactory = new CarFactory();
+        CarService carService = new CarService(carFactory);
+        RandomRacingStrategy randomRacingStrategy = new RandomRacingStrategy();
+        RacingService racingService = new RacingService(randomRacingStrategy);
+        JudgeService judgeService = new JudgeService();
+        InputHandlerImpl inputHandler = new InputHandlerImpl();
+        OutputHandler outputHandler = new OutputHandler();
+
+        return new RacingController(carService, racingService, inputHandler, outputHandler, judgeService);
+    }
+}
