3264b9e51f775b08050142591a643cccdd38b926
donggundg <rlaehdrjs98@g.skku.edu>
2024-10-27 12:43:25 +0900

    style(Application) : 코드 포맷팅

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -17,7 +17,7 @@ public class Application {
     public static final String COLON = " : ";
     public static final String TRIAL_INPUT = "시도할 횟수는 몇 회인가요?";
     public static final String CARNAME_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    
+
     public static void main(String[] args) {
         System.out.println(CARNAME_INPUT);
         String input = readInput();
