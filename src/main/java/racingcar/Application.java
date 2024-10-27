9f7d23b6de820143edb80ec3a2776e6239d648f9
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-10-27 20:28:20 +0900

    feat(Application) : 1.1 경주할 자동차 목록 입력 기능 구현
    - camp.nextstep.edu.missionutils.Console의 readLine()을 통해 문자열을 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 1.1 경주할 자동차 목록을 입력받는 기능
+        String carNamesInputString = getCarNamesInputString();
+    }
+
+    public static String getCarNamesInputString() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
     }
 }
