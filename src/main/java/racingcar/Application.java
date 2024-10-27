c00fbd4bf6459b8f36df2134c7dc13250f25535d
wvssm <neoramiregi@gmail.com>
2024-10-27 15:16:18 +0900

    feat(Application): RacingGameController 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+import racingcar.service.InputService;
+import racingcar.service.RacingGameService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingGameService racingGameService = new RacingGameService();
+        InputService inputService = new InputService();
+
+        RacingGameController racingGameController = new RacingGameController(racingGameService,inputService);
+        racingGameController.run();
     }
 }
