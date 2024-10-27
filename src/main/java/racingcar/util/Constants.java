70061f017b175bab2cb272ae4ccd738bc71b822b
dongri <l0o0lv@github.com>
2024-10-28 02:30:28 +0900

    rafactor(Constants): 라운드 수 입력 후 줄바꿈 추가

--- src/main/java/racingcar/util/Constants.java
+++ src/main/java/racingcar/util/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
     public static int MAX_NAME_LENGTH = 5;
     public static String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static String INPUT_MOVE_COUNT = "시도할 횟수는 몇 회인가요?";
-    public static String OUTPUT_RESULT = "실행 결과";
+    public static String OUTPUT_RESULT = "\n실행 결과";
     public static String OUTPUT_WINNER = "최종 우승자 : ";
 }
