18feda282a4cf2ea4c2b9566b40816355f9d60eb
JoonKyo <joonkyolee@gmail.com>
2024-10-28 00:22:52 +0900

    feat(GameBoard): 자동차의 위치를 파악하고 결과를 문자열로 반환하는 메서드 구현

--- src/main/java/racingcar/domain/GameBoard.java
+++ src/main/java/racingcar/domain/GameBoard.java
@@ -6,6 +6,8 @@ import java.util.stream.Collectors;
 
 public class GameBoard {
     private static final String DELIMITER_OF_CAR_NAMES = ",";
+    private static final String SYMBOL_OF_EACH_TRY_FORMAT = " : ";
+    private static final String NEW_LINE = "\n";
     private final Cars cars;
 
     private GameBoard(Cars cars) {
@@ -16,5 +18,12 @@ public class GameBoard {
         List<String> carNames = Arrays.asList(input.split(DELIMITER_OF_CAR_NAMES));
         List<Car> cars = carNames.stream().map(Car::from).toList();
         return new GameBoard(Cars.from(cars));
+    }
+
+    public String makeCarPositions() {
+        cars.moveEachCar();
+        return cars.getCars().stream()
+                .map(car -> car.getName() + SYMBOL_OF_EACH_TRY_FORMAT + car.getMoveStatus())
+                .collect(Collectors.joining(NEW_LINE));
     }
 }
