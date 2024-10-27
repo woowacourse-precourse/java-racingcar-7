e4d2d9ee782d2f823808c5d73ed5c09183ebc1dd
Seol-Munhyeok <abc1234568@naver.com>
2024-10-27 21:00:30 +0900

    refactor(RacingGameTest): Clean up code formatting

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -2,23 +2,19 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
 import java.util.List;
 
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class RacingGameTest {
     @Test
-    @DisplayName("playGame() 메서드를 테스트한다")
+    @DisplayName("playGame()의 동작을 테스트한다")
     void playGameTest() {
         List<Car> cars = Arrays.asList(
                 new Car("pobi"),
                 new Car("woni"),
                 new Car("jun")
         );
-        int moveCount = 5;
+        int moveCount = 10;
         RacingGame racingGame = new RacingGame(cars, moveCount);
         racingGame.playGame();
     }
