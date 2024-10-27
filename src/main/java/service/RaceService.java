8745a4ffeb712b96df7a4678c2443b6b01731019
jjjjjoon <mjj7343@gmail.com>
2024-10-27 20:44:25 +0900

    feat(RaceService): 자동차 경주의 로직 구현

---
+++ src/main/java/service/RaceService.java
@@ -0,0 +1,46 @@
+package service;
+
+import model.Car;
+import model.Cars;
+import util.randomnumber.RandomNumberHandler;
+import view.OutputView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RaceService {
+    private final RandomNumberHandler randomNumberHandler;
+    private final OutputView outputView;
+
+    public RaceService(RandomNumberHandler randomNumberHandler, OutputView outputView) {
+        this.randomNumberHandler = randomNumberHandler;
+        this.outputView = outputView;
+    }
+
+    public void startRace(Cars cars, int rounds) {
+        for (int i = 0; i < rounds; i++) {
+            moveAllCars(cars);
+            outputView.printCurrentStatus(cars);
+        }
+    }
+
+    private void moveAllCars(Cars cars) {
+        for (Car car : cars.getCars()) {
+            if (randomNumberHandler.canMove()) {
+                car.moveForward();
+            }
+        }
+    }
+
+    public List<String> getWinners(Cars cars) {
+        int maxPosition = cars.getCars().stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.getCars().stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+}
