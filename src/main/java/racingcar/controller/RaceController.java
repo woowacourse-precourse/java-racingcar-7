f32013aefe29e28cc22099fe6013a4a0cbf0c9f4
gyuoo <yeomky98@naver.com>
2024-10-28 03:26:54 +0900

    feat(controller): integrate race functionality

---
+++ src/main/java/racingcar/controller/RaceController.java
@@ -0,0 +1,46 @@
+package racingcar.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import racingcar.domain.car.Car;
+import racingcar.domain.car.RandomMoveStrategy;
+import racingcar.domain.race.Race;
+import racingcar.domain.race.RaceResult;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RaceController {
+	private final InputView inputView;
+	private final OutputView outputView;
+
+	public RaceController(InputView inputView, OutputView outputView) {
+		this.inputView = inputView;
+		this.outputView = outputView;
+	}
+
+	public void startRace() {
+		List<String> carNames = inputView.getCarNames();
+		int raceCount = inputView.getRaceCount();
+		List<Car> cars = createCars(carNames);
+
+		Race race = new Race(cars, raceCount);
+		race.start();
+
+		RaceResult raceResult = new RaceResult(cars);
+		outputView.printCurrentPositions(cars);
+
+		List<Car> winners = raceResult.getWinners();
+		outputView.printWinners(winners);
+	}
+
+	private List<Car> createCars(List<String> carNames) {
+		List<Car> cars = new ArrayList<>();
+
+		for (String name : carNames) {
+			Car car = new Car(name.trim(), new RandomMoveStrategy());
+			cars.add(car);
+		}
+		return cars;
+	}
+}
