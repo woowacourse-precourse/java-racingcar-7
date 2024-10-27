a74e6368ca24334423fe14056688e8b4db032071
jinhyeon <tiemo0708@naver.com>
2024-10-28 02:58:34 +0900

    feat: 오류 메시지를  static 상수로 관리하는 클래스 생성

---
+++ src/main/java/racingcar/utils/ErrorMessages.java
@@ -0,0 +1,9 @@
+package racingcar.utils;
+
+public class ErrorMessages {
+    public static final String CAR_NAME_NULL_OR_EMPTY = "자동차 이름은 비어 있을 수 없습니다.";
+    public static final String CAR_NAME_TOO_LONG = "자동차 이름은 5자를 넘을 수 없습니다: ";
+    public static final String CAR_NAME_EMPTY = "자동차 이름은 빈 값이 될 수 없습니다.";
+    public static final String INVALID_TRIALS_NUMBER = "시도 횟수는 0보다 커야 합니다.";
+    public static final String TRIALS_NOT_A_NUMBER = "시도 횟수는 유효한 정수여야 합니다.";
+}
