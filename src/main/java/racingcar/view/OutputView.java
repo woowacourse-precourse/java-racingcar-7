8e69b6dce144d13021143eef2da0502dae0a0b97
JeongHyeon Jo <cisxo@naver.com>
2024-10-28 03:21:42 +0900

    refactor(view): OutputView winner print
     - 결과 출력 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,17 +1,16 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
     private static final String PRINT_READ_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String PRINT_READ_TOTAL_TRY_MESSAGE = "시도할 횟수는 몇 회인가요?";
-    private static final String PRINT_RACING_CAR_SUFFIX = " : ";
+    private static final String PRINT_WINNER_MESSAGE = "최종 우승자 : ";
 
     private void printMessage(String message) {
         System.out.println(message);
     }
 
-    private void printCarName(String CarName) {
-        System.out.print(CarName + PRINT_RACING_CAR_SUFFIX);
-    }
 
     public void printReadCarNameMessage() {
         printMessage(PRINT_READ_CAR_NAME_MESSAGE);
@@ -21,4 +20,7 @@ public class OutputView {
         printMessage(PRINT_READ_TOTAL_TRY_MESSAGE);
     }
 
+    public void printWinners(List<String> winners) {
+        System.out.println(PRINT_WINNER_MESSAGE + String.join(", ", winners));
+    }
 }
