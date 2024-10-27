445afea7c0a5c473b1865b0371fddef75465e3b8
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-28 05:41:33 +0900

    feat(Cars): 자동차 객체 리스트 생성 및 조회, 이동 요청 기능 구현
    - 자동차 객체로 이루어진 list를 생성합니다.
    - 이름을 기준으로 자동차 객체를 생성해서 list로 만듭니다.
    - RandomIntGenerator는 각 자동차마다 없어도 되기 때문에 여기서 생성해서 재사용했습니다.
    - 자동차들에게 이동을 요청합니다.
    - 경주에 참가하는 자동차 객체 리스트를 조회합니다.

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,11 +1,35 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cars {
-    // TODO: 자동차 리스트를 초기화하고 생성한다.
+    private final List<Car> cars;
 
-    // TODO: 각 자동차의 이동을 요청한다.
+    public Cars(List<String> carNames) {
+        this.cars = new ArrayList<>();
+        initCars(carNames);
+    }
 
-    // TODO: 자동차들의 리스트를 조회한다.
+    public void requestCarMove() {
+        for (Car car : cars) {
+            car.tryMove();
+        }
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    private void initCars(List<String> carNames) {
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        for (String carName : carNames) {
+            Car car = new Car(carName, randomIntGenerator);
+            cars.add(car);
+        }
+    }
+
+    // TODO: 각 자동차에 이동을 요청한다.
 
     // TODO: 가장 멀리 간 자동차의 이동 거리를 반환한다.
 }
