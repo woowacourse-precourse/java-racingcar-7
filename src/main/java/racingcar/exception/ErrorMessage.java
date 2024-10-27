d0b6d9e1ecc3f67cee2347cc5a974bb864a8c175
wogus <ind07152@naver.com>
2024-10-27 21:50:40 +0900

    fix(ErrorMessage): 에러 메시지 오타 수정

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -1,7 +1,7 @@
 package racingcar.exception;
 
 public enum ErrorMessage {
-    NOT_INTEGER("횟수 입력은 숫자만 입력할 수 있습ㄴ다."),
+    NOT_INTEGER("횟수 입력은 숫자만 입력할 수 있습니다."),
     INVALID_COUNT_RANGE("횟수는 1회 이상이여야합니다."),
     INVALID_NAME_LENGTH("자동차 이름은 1글자 이상, 5글자 이하여야합니다."),
     INVALID_NAME_COUNT("자동차 이름은 2개 이상 입력해야합니다."),
