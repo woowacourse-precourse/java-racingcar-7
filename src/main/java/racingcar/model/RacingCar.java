cb09b9dbc5ea39617ee4b6f6c63ad39d5fbcdb3d
yunhacandy <yunhacandy@gmail.com>
2024-10-27 22:33:25 +0900

    feat (RacingCar): 자동차 클래스 생성 및 이동 메소드 추가

---
+++ src/main/java/racingcar/model/RacingCar.java
@@ -0,0 +1,40 @@
+package racingcar.model;
+
+import java.util.List;
+import racingcar.constant.Movement;
+
+public class RacingCar {
+    private static final int INITIAL_LOCATION = 0;
+
+    private final String name;
+    private int location;
+
+    public RacingCar(String name) {
+        this.name = name;
+        this.location = INITIAL_LOCATION;
+    }
+
+    public static List<RacingCar> createRacingCars(List<String> names) {
+        return names.stream()
+                .map(RacingCar::new)
+                .toList();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void increaseLocation() {
+        this.location++;
+    }
+
+    public void move(Movement movement) {
+        if (movement == Movement.FORWARD) {
+            increaseLocation();
+        }
+    }
+}
