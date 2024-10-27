5bd5af793c902252d6cb53010a22648f6cc29c94
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-10-27 15:10:47 +0900

    feat : Limit car name to 5 characters to less
    - 자동차 이름이 5자 이상일 때, IllegalArgumentException 발생함.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,9 +2,17 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         String carName = Console.readLine();
-        carName.split(",");
+        List<String> splitName = List.of(carName.split(","));
+
+        for (int i = 0; i < splitName.size(); i++) {
+            if (splitName.get(i).length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 최대 5자입니다.");
+            }
+        }
     }
 }
