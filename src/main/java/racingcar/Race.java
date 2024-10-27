d5a73803998792f312ec7525116c0ce43cf1bdd4
joon <junkyu83@gmail.com>
2024-10-27 17:10:10 +0900

    feat : race 정보 입력 기능

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Race {
     public void runRace() {
+        String racingCarsName = inputCarName(); // 자동차 이름 문자열을 입력받는다.
+        int raceRound = inputRaceRound(); // 자동차 race round를 입력받는다.
+    }
 
+    public String inputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.");
+        return Console.readLine();
+    }
+
+    public int inputRaceRound() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(Console.readLine());
     }
 }
