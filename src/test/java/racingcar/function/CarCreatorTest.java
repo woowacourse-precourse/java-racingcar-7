8f0bb28e5ab0763a2d903d44ea89bc823c903ce5
world-dv <goodforx0147@naver.com>
2024-10-27 14:23:13 +0900

    test: 자동차 객체 생성자 테스트 추가

---
+++ src/test/java/racingcar/function/CarCreatorTest.java
@@ -0,0 +1,21 @@
+package racingcar.function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+import racingcar.util.CarNameSeparator;
+import racingcar.util.CarsCreator;
+
+public class CarCreatorTest {
+    @Test
+    void creator_자동차_객체_생성() {
+        String input = "pobi,woni";
+        List<String> splitInput = CarNameSeparator.splitCarNameWithoutSpace(input);
+        List<Car> result = CarsCreator.createCarNameToCar(splitInput);
+
+        assertThat(result.get(0).getCarName()).isEqualTo("pobi");
+        assertThat(result.get(1).getCarName()).isEqualTo("woni");
+    }
+}
