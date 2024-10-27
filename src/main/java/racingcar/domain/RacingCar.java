810f3033f1e9da03ffd6fc15e76f6226b181e633
leejh7 <777joonho@kookmin.ac.kr>
2024-10-27 23:34:18 +0900

    feat: 조건에 맞춰 자동차를 움직이는 로직 작성

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,14 +1,19 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCar {
 
     private static final int MAX_NAME_LENGTH = 5;
+    private static final int MOVE_THRESHOLD = 4;
 
     private final String name;
+    private int moveDistance;
 
     public RacingCar(String name) {
         validateName(name);
         this.name = name;
+        this.moveDistance = 0;
     }
 
     private void validateName(String name) {
@@ -17,9 +22,30 @@ public class RacingCar {
         }
     }
 
+    public void move() {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
+
+        if (isMove(randomNum)) {
+            this.moveDistance += 1;
+        }
+    }
+
+    public boolean isMove(int randomNum) {
+        return randomNum >= MOVE_THRESHOLD;
+    }
+
+    // -- Getter -- //
     public String getName() {
         return name;
     }
 
+    public int getMoveDistance() {
+        return moveDistance;
+    }
 
+    // -- toString -- //
+    @Override
+    public String toString() {
+        return String.format("%s : %s%n", name, "-".repeat(moveDistance));
+    }
 }
