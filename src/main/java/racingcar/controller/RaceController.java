3cd706ce154ec79c0cdeffa17d823cf16f2f33cb
HJ <gmlwnchlrh@gmail.com>
2024-10-28 00:38:18 +0900

    feat: 경주 횟수 입력 기능 추가
    - 사용자가 경주할 횟수를 입력할 수 있음
    - 가능한 경주 횟수는 1과 1000 사이로 제한함
    - 범위를 벗어나면 IllegalArgumentException 발생, 프로그램 종료

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -8,9 +8,11 @@ import java.util.List;
 
 public class RaceController {
     private final List<Car> cars = new ArrayList<>();
+    private static final int MAX_GAME_NUMBER = 1000;
 
     public void startRace() {
         getCarNames();
+        getMoveCount();
     }
 
     private void getCarNames() {
@@ -19,6 +21,33 @@ public class RaceController {
         String[] names = input.split(",");
         for (String name : names) {
             cars.add(new Car(name.trim()));
+        }
+    }
+
+    private int getMoveCount() {
+        System.out.println("시도할 횟수는 몇 회인가요? (최대 경주 횟수는 1000회입니다.)");
+        int count;
+        try {
+            String input = Console.readLine();
+            count = Integer.parseInt(input);
+            validateMoveCount(count);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.", e);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        } catch (Exception e) {
+            System.out.println("예상치 못한 에러가 발생하였습니다." + e.getMessage());
+            throw e;
+        }
+        return count;
+    }
+
+    private void validateMoveCount(int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("경주 횟수는 1 이상의 숫자여야 합니다.");
+        }
+        if (count > MAX_GAME_NUMBER) {
+            throw new IllegalArgumentException("최대 경주 횟수(1000회)를 벗어났습니다.");
         }
     }
 }
