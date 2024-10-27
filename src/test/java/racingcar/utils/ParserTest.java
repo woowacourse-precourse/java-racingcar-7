e7147e46fb988e6a414a0b2f6cf537252af772cb
조수아 <suacho0724@gmail.com>
2024-10-28 05:39:18 +0900

    test: Parser 테스트 구현

---
+++ src/test/java/racingcar/utils/ParserTest.java
@@ -0,0 +1,31 @@
+package racingcar.utils;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+
+class ParserTest {
+
+    @Test
+    void parseCarList_success() {
+        String cars = "pobi,woni,jun";
+
+        List<Car> carList = Parser.parseCarList(cars);
+
+        assertEquals(carList.get(0).getName(), "pobi");
+        assertEquals(carList.get(1).getName(), "woni");
+        assertEquals(carList.get(2).getName(), "jun");
+    }
+
+    @Test
+    void parseCarList_name_length_exception() {
+        String cars = "pobi,wonddi,jun";
+
+        assertThatThrownBy(() -> Parser.parseCarList(cars))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
