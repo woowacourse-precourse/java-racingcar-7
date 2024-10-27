c40a86e5c427592d5c717a36d6342291330feb53
jijongkwon <help3451@naver.com>
2024-10-28 00:57:47 +0900

    feat(service) : RacingCarService 이름 입력받기 메서드 구현
    - inputCarName : 이름 입력받기
    - initRacingCar : 레이싱 카 초기화
    - saveName : 이름 저장하기

--- src/main/java/racingcar/application/service/car/RacingCarService.java
+++ src/main/java/racingcar/application/service/car/RacingCarService.java
@@ -1,8 +1,39 @@
 package racingcar.application.service.car;
 
+import racingcar.domain.car.RacingCar;
+import racingcar.infrastructure.persistence.InMemoryRacingCarRepository;
+import racingcar.infrastructure.util.NameParser;
+
+import java.util.List;
+
 public class RacingCarService {
 
+    private final NameParser nameParser;
+    private final InMemoryRacingCarRepository racingRepository;
+
+    public RacingCarService(NameParser nameParser) {
+        this.nameParser = nameParser;
+        this.racingRepository = InMemoryRacingCarRepository.getInstance();
+    }
+
+    // TODO : 이름 입력받기
+    public void inputCarName(String racingCars) {
+        List<String> names = nameParser.parsingName(racingCars);
+
+        for (String name : names) {
+            saveName(initRacingCar(name));
+        }
+    }
+
+    // TODO : 레이싱 카 초기화
+    private RacingCar initRacingCar(String name) {
+        return new RacingCar(name);
+    }
+
     // TODO : 이름 저장하기
+    private void saveName(RacingCar racingCar) {
+        racingRepository.saveRacingCar(racingCar);
+    }
 
     // TODO : 시도횟수 입력받기
 
