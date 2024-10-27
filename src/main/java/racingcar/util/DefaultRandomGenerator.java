17c17be1680ac4f630b8fbdfbb998142b4928f2b
Chanwon-Seo <scwonn60@gmail.com>
2024-10-28 02:39:40 +0900

    refactor : 랜덤값 범위 상수 변경

--- src/main/java/racingcar/util/DefaultRandomGenerator.java
+++ src/main/java/racingcar/util/DefaultRandomGenerator.java
@@ -3,8 +3,11 @@ package racingcar.util;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class DefaultRandomGenerator implements RandomNumberGenerator {
+    private static final int MIN_RANGE = 0;
+    private static final int MAX_RANGE = 9;
+
     @Override
     public int move() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE);
     }
 }
