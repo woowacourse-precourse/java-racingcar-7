4356d0f119f69afd2787dde7eea894498fef002f
jody816 <81346079+jody816@users.noreply.github.com>
2024-10-28 01:07:44 +0900

    fix: 출력문 실행 예시 내용에 맞게 수정

--- src/main/java/racingcar/application/view/output/OutputViewImpl.java
+++ src/main/java/racingcar/application/view/output/OutputViewImpl.java
@@ -2,8 +2,8 @@ package racingcar.application.view.output;
 
 public class OutputViewImpl implements OutputView {
 
-  private static final String CAR_NAME_INPUT_PROMPT = "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분)";
-  private static final String ATTEMPT_COUNT_INPUT_PROMPT = "시도할 횟수";
+  private static final String CAR_NAME_INPUT_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+  private static final String ATTEMPT_COUNT_INPUT_PROMPT = "시도할 횟수는 몇 회인가요?";
 
   @Override
   public void printCarNamesInputPrompt() {
