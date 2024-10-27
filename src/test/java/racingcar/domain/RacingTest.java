ecdb1799a2c6a419cc5c0dcb4b05d860cba1d094
rladmstn <oz33177351@gmail.com>
2024-10-28 03:25:06 +0900

    test(racing) : Racing 관련 public 메소드 테스트 작성
    - runRace() : 경주를 진행하면서 출력 형태가 정상적인지 확인하는 테스트 작성
    - createWinnerList() : 경주 후, 결과 기반으로 정상적으로 승자 목록 String을 리턴하는지 확인하는 테스트 작성

---
+++ src/test/java/racingcar/domain/RacingTest.java
@@ -0,0 +1,53 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class RacingTest {
+    @Test
+    void 경주_정상_수행_출력_성공() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Racing racing = new Racing("car1,car2,car3", "10");
+        racing.runRace();
+
+        String[] lines = outContent.toString().split("\n");
+
+        for (int i = 0; i < lines.length; i++) {
+            if ((i+1) % 4 != 0) {
+                assertThat(lines[i]).contains("car" + (i+1)%4 + " : ");
+            } else {
+                assertThat(lines[i]).isEqualTo("\n");
+            }
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "0 / 2 / 0 / car2",
+            "1 / 1 / 0 / car1, car2"
+    }, delimiter = '/')
+    void 승자_목록_문자열_생성_성공(int move1, int move2, int move3, String expected){
+        Racing racing = new Racing("car1,car2,car3", "10");
+
+        for(int i=0; i<move1; i++) {
+            racing.cars.get(0).move();
+        }
+        for(int i=0; i<move2; i++) {
+            racing.cars.get(1).move();
+        }
+        for(int i=0; i<move3; i++) {
+            racing.cars.get(2).move();
+        }
+
+        String winnerList = racing.createWinnerList();
+
+        assertThat(winnerList).isEqualTo(expected);
+    }
+}
