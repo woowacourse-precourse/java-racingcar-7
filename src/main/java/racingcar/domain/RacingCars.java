d6f45c506c42bcbd58d8fbc740b6c2455d0b58c9
zjhj0814 <zjhj0814@gmail.com>
2024-10-27 20:50:12 +0900

    feat(RacingCars): RacingCars 인스턴스 변수에 우승자 정보 추가
    추후 여러 메소드에서 사용할 여지가 있으므로 유지보수를 위해 우승자 정보를 인스턴스 변수에 추가한다

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -7,16 +7,22 @@ public class RacingCars {
     private List<RacingCar> cars;
     private Integer currentAttempts;
     private final Integer totalAttempts;
+    private List<RacingCar> winners;
     private static final Integer INITIAL_CURRENT_ATTEMPTS = 0;
 
     private RacingCars(Integer totalAttempts) {
         this.cars = new ArrayList<>();
         this.currentAttempts = INITIAL_CURRENT_ATTEMPTS;
         this.totalAttempts = totalAttempts;
+        this.winners = new ArrayList<>();
     }
 
     private List<RacingCar> getCars() {
         return cars;
+    }
+
+    private List<RacingCar> getWinners() {
+        return winners;
     }
 
     public static RacingCars createRacingCars(List<String> carNames, Integer totalAttempts) {
