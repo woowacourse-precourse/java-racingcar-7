f42cbab30491e7116ac7dd41f5227aef9d6a013c
나상연 <58220151+ense333@users.noreply.github.com>
2024-10-27 14:17:54 +0900

    test(racingcar): add unit tests for RacingProgram class
    Added unit tests for RacingProgram class to validate input handling:
    - Valid input test: Ensures correct car creation from user input.
    - Blank input test: Throws an exception for empty or blank car names.
    - Duplicate input test: Throws an exception for duplicate car names.
    - Name length test: Throws an exception if a car name exceeds 5 characters.

---
+++ src/test/java/racingcar/RacingProgramTest.java
@@ -0,0 +1,57 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingProgramTest {
+
+    private RacingProgram racingProgram;
+
+    @BeforeEach
+    void setUp(){
+        // 테스트마다 새로운 인스턴스 생성
+        racingProgram = new RacingProgram();
+    }
+
+    @Test
+    void 올바른_입력_테스트(){
+        String input = "test1,test2,test3";
+
+        List<Car> cars = racingProgram.splitName(input);
+
+        assertThat(cars).hasSize(3);
+    }
+
+    @Test
+    void 공백_예외_테스트(){
+        //문자열 내에 공백이 입력되는 경우
+        String input = "test1,,test2";
+
+        Assertions.assertThatThrownBy(() -> racingProgram.splitName(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 중복_예외_테스트(){
+        //차량 이름이 중복되는 경우
+        String input = "test1,test1,test2";
+
+        Assertions.assertThatThrownBy(() -> racingProgram.splitName(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 글자수_예외_테스트(){
+        //차량 이름중에 5글자를 넘어가는 경우
+        String input = "test1,test12,test2";
+
+        Assertions.assertThatThrownBy(() -> racingProgram.splitName(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
