df10830ed28a56e242154ef28c79bd451e1e0e45
gahyeon <1000ji1@naver.com>
2024-10-27 23:49:38 +0900

    feat: 이름을 입력하지 않은 경우 예외처리 테스트

--- src/test/java/racingcar/utils/ValidateTest.java
+++ src/test/java/racingcar/utils/ValidateTest.java
@@ -9,12 +9,23 @@ import racingcar.domain.Cars;
 public class ValidateTest {
     @DisplayName("이름이 5자를 초과하는 경우 예외 발생")
     @Test
-    void validateNameLength() {
+    void throwsExceptionWhenNameExceedsLengthLimit() {
         String carNames = "pobi,woni,junjun";
         Cars cars = new Cars();
 
         assertThatThrownBy(() -> cars.registerCars(carNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(Validator.NAME_LENGTH_OVER_LIMIT_MESSAGE);
+    }
+
+    @DisplayName("이름을 입력하지 않은 경우 예외 발생")
+    @Test
+    void throwsExceptionWhenNameIsEmpty() {
+        String carNames = "";
+        Cars cars = new Cars();
+
+        assertThatThrownBy(() -> cars.registerCars(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Validator.INPUT_NAME_MESSAGE);
     }
 }
