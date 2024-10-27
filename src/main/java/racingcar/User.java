1d4eb9c46a63f2130442b779082ace38da5f52f6
Yong U Cho <yongu2000@naver.com>
2024-10-28 02:04:50 +0900

    style: 불필요한 import 제거

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -3,7 +3,6 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class User {
