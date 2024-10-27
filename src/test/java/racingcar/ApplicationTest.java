ab9af36a2ba58575b99de331233293949ac8b923
LeeJE12 <jjungeun2013@gmail.com>
2024-10-28 04:34:07 +0900

    feat: Test 7 Cases 정리

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -25,20 +25,52 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 커스텀_구분자_사용() {
+    void 기능_자동차_띄어쓰기() {
         assertSimpleTest(() -> {
             run("pobi,woni ,  jun", "5");
             assertThat(output()).contains("");
         });
     }
 
-//    @Test
-//    void 예외_테스트() {
-//        assertSimpleTest(() ->
-//            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-//                .isInstanceOf(IllegalArgumentException.class)
-//        );
-//    }
+    @Test
+    void 기능_자동차_이름_띄어쓰기() {
+        assertSimpleTest(() -> {
+            run("po bi,wo ni ,  j un", "5");
+            assertThat(output()).contains("");
+        });
+    }
+
+    @Test
+    void 기능_시도횟수_0회() {
+        assertSimpleTest(() -> {
+            run("pobi , woni , jun", "0");
+            assertThat(output()).contains("");
+        });
+    }
+
+    @Test
+    void 예외_자동차_5자초과() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_시도횟수_입력없음() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", ""))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_자동차_입력없음() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
     @Override
     public void runMain() {
