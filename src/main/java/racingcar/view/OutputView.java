59241b4ea9447d5487ab0dc0c7652e247edd35f9
Chan Mi <rkdcksal02@gmail.com>
2024-10-28 02:35:51 +0900

    feat: 결과 출력 기능 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,19 @@
+package racingcar.view;
+
+import racingcar.model.Car;
+
+import java.util.List;
+
+public class OutputView {
+
+    public static void printRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+}
