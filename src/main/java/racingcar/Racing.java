e0ba641247192334406bc25b0d3ea896e717868e
181108-cherry <dysy9799@naver.com>
2024-10-27 15:17:34 +0900

    feat: 랜덤 숫자 생성 후 멈춤 or 전진에 따라 출력
    - 4에서 9일 경우 '-'로 전진 표시, 0에서 3일 경우 아무런 표시 없이 출력

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,12 +1,39 @@
 package racingcar;
 
-
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+
+public class Racing {
 
     private List<String> carNames;
 
     public Racing(List<String> carNames) {
         this.carNames = carNames;
+    }
+    public void startRace() {
+        System.out.println("실행 결과");
+        for (String name : carNames) {
+            System.out.print(name.trim() + ": ");
+            int randomNumber = randomNumber();
+
+            // 0에서 3일 경우는 멈춤, 4에서 9일 경우 전진
+            if (randomNumber >= 4) {
+                printProgress(1);  // 전진일 때 '-' 출력
+            }
+
+            System.out.println();
+        }
+    }
+
+    // 0에서 9사이의 랜덤 값 구하기
+    public int randomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+    // 전진 시 '-' 출력
+    public void printProgress(int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.print("-");
+        }
     }
 }
