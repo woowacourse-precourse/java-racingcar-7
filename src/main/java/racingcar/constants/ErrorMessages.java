e51dbc7fd6b92d7dba1b2288e8b02799cc0e0086
yush <yush2202@gmail.com>
2024-10-28 02:24:04 +0900

    feat($ ErrorMessages): 에러 메시지 추가
    에러 사항 변경에 따라 메시지 추가

--- src/main/java/racingcar/constants/ErrorMessages.java
+++ src/main/java/racingcar/constants/ErrorMessages.java
@@ -4,10 +4,11 @@ public enum ErrorMessages {
 
     PREFIX("[ERROR] "),
     INVALID_USER_INPUT_BLANK("입력은 공백일 수 없습니다."),
-    INVALID_RACER_NAME_BLANK("레이서 이름은 공백일 수 없습니다."),
-    INVALID_RACER_NAME_RANGE("유효한 레이서 이름 길이가 아닙니다."),
-    INVALID_RACER_NAME_DUPLICATED("레이서 이름은 중복될 수 없습니다."),
-    INVALID_TRY_COUNT("입력횟수는 1회 이상이어야 합니다.");
+    INVALID_RACER_NAME_BLANK("자동차 이름은 공백일 수 없습니다."),
+    INVALID_RACER_NAME_RANGE("유효한 자동차 이름 길이가 아닙니다."),
+    INVALID_RACER_NAME_DUPLICATED("자동차 이름은 중복될 수 없습니다."),
+    INVALID_RANGE_TRY_COUNT("입력횟수는 1회 이상이어야 합니다."),
+    INVALID_NOT_NUMBER_TRY_COUNT("입력 횟수는 숫자여야 합니다.");
 
     private final String message;
 
