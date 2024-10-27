2f11909f1ea40b34e8f6cda0c5fb33f16a9d972f
TaegeunYou <dbxorms2310@naver.com>
2024-10-27 12:34:26 +0900

    refactor(InputView): 입력받은 문자열을 검증하는 로직을 함수로 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,7 +2,8 @@ package racingcar.view;
 
 import racingcar.view.console.ConsoleReader;
 import racingcar.view.console.ConsoleWriter;
-import racingcar.view.global.Validator;
+import racingcar.view.global.exception.CustomException;
+import racingcar.view.global.exception.ErrorMessage;
 
 public class InputView {
     private static final String REQUEST_CARS_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -10,8 +11,14 @@ public class InputView {
     public String requestCarsName() {
         ConsoleWriter.printlnMessage(REQUEST_CARS_MESSAGE);
         String input = ConsoleReader.enterMessage();
-        Validator.validString(input);
+        if (isEmptyOrBlank(input)) {
+            throw CustomException.of(ErrorMessage.BLANK_INPUT_ERROR);
+        }
         return input;
+    }
+
+    private boolean isEmptyOrBlank(String str) {
+        return str == null || str.isBlank();
     }
 
 }
--- src/main/java/racingcar/view/global/Validator.java
+++
@@ -1,9 +0,0 @@
-package racingcar.view.global;
-
-public class Validator {
-    public static void validString(String str) {
-        if (str == null || str.isBlank()) {
-            throw new IllegalArgumentException();
-        }
-    }
-}
