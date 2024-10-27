5dd02a49f27de31d284d02fcba96ff8e632a3c1e
jae woo Jung <awfjol2008@gmail.com>
2024-10-27 16:10:17 +0900

    feat: 범위 내의 랜덤한 숫자를 뽑는 기능 구현

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -1,10 +1,16 @@
 package racingcar.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomNumberGenerator {
 	private static final int START_INCLUSIVE = 0;
 	private static final int END_INCLUSIVE = 9;
 
 	private RandomNumberGenerator() {
 
+	}
+
+	public static int pickNumber() {
+		return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
 	}
 }
