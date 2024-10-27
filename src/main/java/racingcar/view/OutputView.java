ddabb7ab411c79a1546c716e783dd19198abc369
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-10-27 19:05:02 +0900

    feat: 입력클래스 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,29 @@
 package racingcar.view;
 
-public class OutputViw {
+import racingcar.domain.Car;
+import racingcar.dto.GameResult;
+import racingcar.dto.RoundResults;
+
+import java.util.List;
+
+public class OutputView {
+
+    public static void printResult(GameResult gameResult) {
+        printRoundResults(gameResult.getRoundResults());
+        printWinners(gameResult.getWinnerNames());
+    }
+
+    private static void printRoundResults(RoundResults roundResults) {
+        System.out.println();
+        for (List<Car> cars : roundResults.getResult()) {
+            for (Car car : cars) {
+                System.out.println(car.getCarName() +" : "+"-".repeat(car.getPosition()));
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printWinners(List<String> winnerNames) {
+        System.out.print("최종 우승자 : "+ String.join(", ", winnerNames));
+    }
 }
