709abcac4b05687649292a6467f05a3ddb37e84f
sunmok <tjsahr9191@naver.com>
2024-10-27 20:24:30 +0900

    feat : Car 전진기능, 이름 생성
    - 우승자인지 판단 할 수 있다.
    - 전진 횟수만큼 -로 변환 할 수 있다.
    - 이름을 반환한다.
    - 전진 횟수를 반환한다.
    - 가장 전진횟수가 많은지 확인한다.
    - 전진할 수 있는지 확인한다.
    - 전진한다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,14 +3,17 @@ package racingcar.model;
 import racingcar.config.GameConfig;
 import racingcar.util.RandomNumberGenerator;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Car {
 
     private final String name;
-    private final int forward;
+    private final int forwardNumber;
 
-    private Car(String name, int forward) {
+    private Car(String name, int forwardNumber) {
         this.name = name;
-        this.forward = forward;
+        this.forwardNumber = forwardNumber;
     }
 
     public static Car of(String name, int forward) {
@@ -24,13 +27,40 @@ public class Car {
     public Car play() {
         int randomNumber = RandomNumberGenerator.generateNumber();
         if (isForward(randomNumber)) {
-            return of(name, forward + 1);
+            return of(name, forwardNumber + 1);
         }
         return this;
     }
 
-    private boolean isForward(int randomNumber) {
-        return randomNumber > GameConfig.POSSIBLE_FORWARD_NUMBER;
+    public boolean isMaxForwardNumber(int forwardNumber) {
+        return this.forwardNumber > forwardNumber;
     }
 
+    public int getForwardNumber() {
+        return this.forwardNumber;
+    }
+
+    public boolean isWinner(int maxForwardNumber) {
+        return this.forwardNumber == maxForwardNumber;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        String forwardNumberToString = getForwardNumberToString();
+        return this.name + " : " + forwardNumberToString;
+    }
+
+    private String getForwardNumberToString() {
+        return IntStream.range(0, forwardNumber)
+                .mapToObj(i -> "-")
+                .collect(Collectors.joining());
+    }
+
+    private boolean isForward(int randomNumber) {
+        return randomNumber >= GameConfig.POSSIBLE_FORWARD_NUMBER;
+    }
 }
