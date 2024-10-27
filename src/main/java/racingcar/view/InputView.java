270543fdcd08b7d4b63f7e9d293fb8c715661cb9
steamedEggMaster <cksdud5014@naver.com>
2024-10-27 15:45:29 +0900

    refactor(InputView): 가독성 및 복잡성 줄이기 위한 코드 리팩토링
    1. 바로 입력값을 받아오게 하여 set,get 메서드 삭제
    2. 불필요한 필드 삭제

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,8 +6,6 @@ public class InputView {
     private static final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private static InputView inputView;
-    private String carNames;
-    private String tryCount;
 
     private InputView() {
     }
@@ -19,35 +17,14 @@ public class InputView {
         return inputView;
     }
 
-    public void readCarRacingSetup() {
-        readCarNames();
-        readTryCount();
-    }
-
-    private void readCarNames() {
+    public String readCarNames() {
         System.out.println(CAR_NAME_MESSAGE);
-        setCarNames(Console.readLine());
+        return Console.readLine();
     }
 
-    private void readTryCount() {
+    public String readTryCount() {
         System.out.println(TRY_COUNT_MESSAGE);
-        setTryCount(Console.readLine());
-    }
-
-    private void setCarNames(String carNames) {
-        this.carNames = carNames;
-    }
-
-    public String getCarNames() {
-        return carNames;
-    }
-
-    private void setTryCount(String tryCount) {
-        this.tryCount = tryCount;
-    }
-
-    public String getTryCount() {
-        return tryCount;
+        return Console.readLine();
     }
 
     public void close() {
