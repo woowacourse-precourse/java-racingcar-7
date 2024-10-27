7a968210b3381959f2ee11f3121c4edc229d00b0
JiHyeonSu <itip1@mju.ac.kr>
2024-10-28 03:34:35 +0900

    feat(Input): 입력 기능 구현 -getCarNames 메소드는 사용자에게 경주할 자동차 이름을 입력받습니다. -getAttempts 메소드는 사용자가 자동차를 이동을 시도할 횟수를 입력받습니다.

---
+++ src/main/java/racingcar/common/io/Input.java
@@ -0,0 +1,28 @@
+package racingcar.common.io;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.common.validator.InputValidator;
+
+public class Input {
+
+  Output output = new Output();
+
+  public String[] getCarNames() {
+    output.printMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    String input = Console.readLine().trim();
+    String[] names = input.split(",");
+
+    InputValidator.validateCarNames(names);
+    return names;
+  }
+
+  public int getAttempts() {
+    output.printMessage("시도할 횟수는 몇 회인가요?");
+    String input = Console.readLine().trim();
+
+    int attempts = InputValidator.parseAndValidateNumber(input);
+    InputValidator.validateAttempts(attempts);
+
+    return attempts;
+  }
+}
