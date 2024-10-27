aa51edd4477565808066ffd5920619a5e9d2c886
YugyeomGhim <rladbrua0207@gmail.com>
2024-10-27 15:41:15 +0900

    test: 예외 테스트에 메시지 검증 추가 및 공백 자동차 이름 예외 테스트 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,6 +1,7 @@
 package racingcar.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +13,24 @@ class CarTest {
         String name = "여섯글자이름";
 
         // when & then
-        assertThrows(IllegalArgumentException.class, () -> new Car(name));
+        assertThatThrownBy(() -> new Car(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름 입력 길이를 초과하였습니다. (최대 5자)");
+    }
+
+    @Test
+    void 자동차_이름이_공백일_경우_예외_발생() {
+        // given
+        String whiteSpaceName = " ";
+        String emptyName = "";
+
+        // when & then
+        assertThatThrownBy(() -> new Car(whiteSpaceName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름이 공백으로 입력되었습니다.");
+        assertThatThrownBy(() -> new Car(emptyName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름이 공백으로 입력되었습니다.");
     }
 
     @Test
