652eccb9e74edca263bb84a6af005a2d73e42168
정재웅 <jweidon0821@gmail.com>
2024-10-28 08:22:53 +0900

    refactor: 입출력 관련 코드 view 폴더로 분리
    - InputView: 입력 관련 컨트롤러
    - OutputView: 출력 관련 컨트롤러

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -1,8 +1,9 @@
 package racingcar.controller;
 
-import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.domain.Car;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,38 +13,15 @@ public class RacingcarController {
     private static final int MOVE_THRESHOLD = 4;
 
     public void playGame() {
-        String carNames = inputCarNames();
+        String carNames = InputView.inputCarNames();
         cars = Car.createCarsFromNames(carNames);
-        int attemptCount = inputAttemptCount();
+        int attemptCount = InputView.inputAttemptCount();
 
         for (int i = 0; i < attemptCount; i++) {
             raceCars();
-            displayCurrentPositions();
+            OutputView.displayCurrentPositions(cars);
         }
-        announceWinners();
-    }
-
-    private String inputCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
-    }
-
-    private int inputAttemptCount() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String input = Console.readLine();
-        return validateAttemptCount(input);
-    }
-
-    private int validateAttemptCount(String input) {
-        try {
-            int count = Integer.parseInt(input);
-            if (count <= 0) {
-                throw new IllegalArgumentException("시도 횟수는 0보다 커야 합니다.");
-            }
-            return count;
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("시도 횟수는 숫자로 입력해야 합니다.");
-        }
+        OutputView.announceWinners(cars);
     }
 
     private void raceCars() {
@@ -56,26 +34,5 @@ public class RacingcarController {
 
     private boolean shouldMoveForward() {
         return Randoms.pickNumberInRange(0, 9) >= MOVE_THRESHOLD;
-    }
-
-    private void displayCurrentPositions() {
-        System.out.println("실행 결과");
-        for (Car car : cars) {
-            System.out.print(car.getName() + " : " + "-".repeat(car.getPosition()) + "\n");
-        }
-        System.out.println();
-    }
-
-    private void announceWinners() {
-        int maxPosition = cars.stream().mapToInt(Car::getPosition).max().orElse(0);
-        List<String> winners = new ArrayList<>();
-
-        for (Car car : cars) {
-            if (car.getPosition() == maxPosition) {
-                winners.add(car.getName());
-            }
-        }
-
-        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
