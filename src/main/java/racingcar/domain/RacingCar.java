454af789c941a62847ae27c88e490fc2f5124979
young-in <hardworkingini@gmail.com>
2024-10-27 23:15:43 +0900

    feat : racingCar 의 위치를 나타내기 위해서 Location 추가

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -2,9 +2,11 @@ package racingcar.domain;
 
 public class RacingCar {
     private RacingCarName racingCarName;
+    private Location location;
 
-    public RacingCar(final String racingCarName){
+    public RacingCar(final String racingCarName, final Location location){
         this.racingCarName = new RacingCarName(racingCarName);
+        this.location = location;
     }
 
     public RacingCarName getRacingCarName() {
