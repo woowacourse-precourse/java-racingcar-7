a80e5fa09857ba5354338214cc48e093d7515bdd
JiHyeonSu <itip1@mju.ac.kr>
2024-10-28 02:41:14 +0900

    test(OutputTest): 출력 테스트 코드 작성 - 진행상황_출력_테스트 메소드는 경주 진행상황 출력값을 검증하기 위한 테스트입니다. - 우승자_출력_테스트 메소드는 우승자가 정해졌을 때 출력값을 검증하기 위한 테스트입니다. - 공동_우승자_출력_테스트 메소드는 우승자가 2명 이상일 때 출력값을 검증하기 위한 테스트입니다.

---
+++ src/test/java/racingcar/OutputTest.java
@@ -0,0 +1,46 @@
+package racingcar;
+
+import common.io.Output;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+
+class OutputTest {
+
+  private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+  @BeforeEach
+  void setUp() {
+    System.setOut(new PrintStream(outputStream));
+  }
+
+  @Test
+  void 진행상황_출력_테스트() {
+    Output outputHandler = new Output();
+    outputHandler.printRaceProgress("hsj", 3);
+
+    assertThat(outputStream.toString().trim()).isEqualTo("hsj : ---");
+  }
+
+  @Test
+  void 우승자_출력_테스트() {
+    Output outputHandler = new Output();
+    outputHandler.printWinners(List.of("hsj"));
+
+    assertThat(outputStream.toString().trim()).isEqualTo("최종 우승자 : hsj");
+  }
+
+  @Test
+  void 공동_우승자_출력_테스트() {
+    Output outputHandler = new Output();
+    outputHandler.printWinners(Arrays.asList("jsh", "hsj", "jwj"));
+
+    assertThat(outputStream.toString().trim()).isEqualTo("최종 우승자 : jsh, hsj, jwj");
+  }
+}
