6ba887e08c0b1069e5159b3ab9b780107acd8c46
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-27 11:32:04 +0900

    feat: 주어진 반복 횟수 만큼 경주 수행 기능 구현
    - 서비스 계층을 생성하여 전체적인 경주를 관할하는 startRacing 메서드를 기반으로 아래와 같은 기능을 구현하였다.
    - 주어진 반복 횟수만큼 라운드가 반복되고 결과가 출력되도록 Cars 클래스의 roundProcess 메서드와 roundStatus 메서드를 호출한다.
    - 사용자의 입력을 검증하는 validateInputs 메서드를 startRacing 메서드에서 호출한다.

---
+++ src/main/java/racingcar/service/RaceService.java
@@ -0,0 +1,31 @@
+package racingcar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Cars;
+import racingcar.exception.InputValidator;
+import racingcar.utils.RandNumGenerator;
+
+public class RaceService {
+    public List<String> startRacing(String carsNames, String repeatTimes) {
+        validateInputs(carsNames, repeatTimes);
+
+        Cars cars = new Cars(carsNames);
+        RandNumGenerator randNumGenerator = new RandNumGenerator();
+
+        for (int i = 0; i < Integer.parseInt(repeatTimes); i++) {
+            cars.roundProcess(randNumGenerator);
+            cars.roundStatus();
+        }
+
+        return new ArrayList<>();
+    }
+
+    private static void validateInputs(String carsNames, String repeatTimes) {
+        InputValidator inputValidator = InputValidator.getInstance();
+
+        inputValidator.validateWhiteSpaces(carsNames, repeatTimes);
+        inputValidator.validateRepeatTimesNotDigit(repeatTimes);
+        inputValidator.validateOutOfRangeRepeatTimes(repeatTimes);
+    }
+}
