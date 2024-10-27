ce38485b3306be6c9c64aa7d713f60f6f1a54491
theundergroundt <139340193+theundergroundt@users.noreply.github.com>
2024-10-27 18:04:50 +0900

    test(ApplicationTest) : 기능테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -24,6 +24,38 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 기능_테스트2() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,sam", "3");
+                    assertThat(output()).contains("pobi : -", "woni : ", "sam : -",
+                            "pobi : -", "woni : ", "sam : - -",
+                            "pobi : -", "woni : ", "sam : - - -",
+                            "최종 우승자 : sam");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD,
+                STOP, STOP, MOVING_FORWARD,
+                STOP, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 기능_테스트3_다수의우승자() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,sam", "3");
+                    assertThat(output()).contains("pobi : -", "woni : ", "sam : -",
+                            "pobi : - -", "woni : ", "sam : - -",
+                            "pobi : - - -", "woni : ", "sam : - - -",
+                            "최종 우승자 : pobi, sam");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD,
+                MOVING_FORWARD, STOP, MOVING_FORWARD,
+                MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
