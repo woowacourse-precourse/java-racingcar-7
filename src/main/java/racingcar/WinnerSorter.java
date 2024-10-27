e504ff7e7fbbe10095dd2d4eb9831ceb564a4b55
minitiated <minitiated@gmail.com>
2024-10-27 16:29:10 +0900

    refactor(WinnerSorter): refactor winnerList function
    added logic into empty getJointWinner() method in WinnerSorter
    added new addJointWinner() to WinnerSorter to compare forwardCount
    values

--- src/main/java/racingcar/WinnerSorter.java
+++ src/main/java/racingcar/WinnerSorter.java
@@ -1,17 +1,32 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class WinnerSorter {
 
-    public List<String> getJointWinners(Map<String, Integer> sortedTotalForwardCount) {
-//        맵은 우승자 판별 어케하는지 모르겠네
-        for (int i = 0; i < sortedTotalForwardCount.size(); i++) {
-            sortedTotalForwardCount.get(i).compareTo(sortedTotalForwardCount.get(i+1));
-//            뭐 이런거 써야하나?
+    public List<String> getJointWinners(TreeMap<String, Integer> forwardCounter) {
+
+        List<String> winnerList = new ArrayList<>();
+        String winner = forwardCounter.lastKey();
+        Integer winnerValue = forwardCounter.get(winner);
+
+        winnerList.add(winner);
+
+        for (Map.Entry<String, Integer> entry : forwardCounter.entrySet()) {
+            String carName = entry.getKey();
+            Integer forwardCount = entry.getValue();
+            addJointWinner(carName, forwardCount, winnerValue, winnerList);
         }
-//        return List<String> winnerList;
-        return null;
+
+        return winnerList;
+    }
+
+    private void addJointWinner(String carName, Integer forwardCount, Integer winnerValue, List<String> winnerList) {
+        if(forwardCount.equals(winnerValue)) {
+            winnerList.add(carName);
+        }
     }
 }
