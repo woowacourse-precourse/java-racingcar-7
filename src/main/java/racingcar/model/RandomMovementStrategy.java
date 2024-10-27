b89e6693b162f758862ffaf6b092837e101912d3
sc@t0a14 <gusdn6671@gmail.com>
2024-10-27 11:17:39 +0900

    refactor(RandomMovementStrategy): RandomMovementStrategy 의 매직넘버를 상수로 추출하고 리팩토링

--- src/main/java/racingcar/model/RandomMovementStrategy.java
+++ src/main/java/racingcar/model/RandomMovementStrategy.java
@@ -3,13 +3,17 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomMovementStrategy implements MovementStrategy {
+    private static final int MOVE_THRESHOLD = 4;
+    private static final int RANDOM_MIN = 0;
+    private static final int RANDOM_MAX = 9;
+
     @Override
     public boolean moveable() {
-        return makeRandomNumber() >= 4;
+        return makeRandomNumber() >= MOVE_THRESHOLD;
     }
 
     protected int makeRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(RANDOM_MIN, RANDOM_MAX);
     }
 
 }
