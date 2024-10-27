dc51edb0191d5337401da8c2de8679f36f4b0c6e
CheChe903 <cheche903@naver.com>
2024-10-27 21:17:17 +0900

    refactor(ConsoleOutputHandler): 전체적인 메서드 리팩터링 진행
    들여쓰기를 최소화합니다.

--- src/main/java/racingcar/io/Output/ConsoleOutputHandler.java
+++ src/main/java/racingcar/io/Output/ConsoleOutputHandler.java
@@ -8,6 +8,7 @@ public class ConsoleOutputHandler implements OutputHandler {
     private static final String CAR_NAME_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ROUND_REQUEST_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private static final String WINNERS_MESSAGE = "최종 우승자 : ";
+    private static final String DELIMITER = ", ";
 
     @Override
     public void printCarNameRequest() {
@@ -22,22 +23,22 @@ public class ConsoleOutputHandler implements OutputHandler {
     @Override
     public void printCarProgress(String carName, int position) {
         System.out.print(carName + " : ");
+
         for (int i = 0; i < position; i++) {
             System.out.print("-");
         }
-        System.out.println();
+        printNewLine();
     }
 
     @Override
     public void printWinners(List<Car> winners) {
         System.out.print(WINNERS_MESSAGE);
-        for (int i = 0; i < winners.size(); i++) {
-            System.out.print(winners.get(i).getName());
-            if (i < winners.size() - 1) {
-                System.out.print(", ");
-            }
+
+        for (int i = 0; i < winners.size() - 1; i++) {
+            System.out.print(winners.get(i).getName() + DELIMITER);
         }
-        System.out.println();
+        System.out.println(winners.getLast().getName());
+        printNewLine();
     }
 
     @Override
