45e3163871e2ae7d1e69e96df378282c10072423
woongjee <jeewoong7753@naver.com>
2024-10-28 01:32:24 +0900

    feat(Application):메인 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.sql.SQLOutput;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try{
+            String inputNames = Console.readLine();
+            String[] carNames = parseCarNames(inputNames);
 
+            int rounds = Integer.parseInt(Console.readLine());
+
+            Race race = new Race(carNames, rounds);
+            race.start();
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
     private static String[] parseCarNames(String inputNames){
         return inputNames.split(",");
