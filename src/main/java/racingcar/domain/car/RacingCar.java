6733430bb31a0d56772b67d7512ed4f0daf97546
jijongkwon <help3451@naver.com>
2024-10-28 01:39:07 +0900

    feat(domain) : RacingCar filed 추가 및 메서드 추가 구현
    - distance : 거리
    - move : 거리를 증가하는 메서드

--- src/main/java/racingcar/domain/car/RacingCar.java
+++ src/main/java/racingcar/domain/car/RacingCar.java
@@ -2,13 +2,26 @@ package racingcar.domain.car;
 
 public class RacingCar {
     private String name;
+    private int distance = 0;
 
     public RacingCar(String name) {
         this.name = name;
     }
 
-    // TODO : 이름 반환
+    public RacingCar(String name, int distance){
+        this.name = name;
+        this.distance = distance;
+    }
+
+    public void move(){
+        distance++;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public int getDistance(){
+        return distance;
     }
 }
