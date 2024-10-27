244eace1bc053e30e6480c151542f9cefd4853c9
EHCTH <lees90896@gmail.com>
2024-10-27 12:16:47 +0900

    feat: InputView 기능 추가 및 수정

--- src/main/java/racingcar/view/input/service/impl/CarInputView.java
+++ src/main/java/racingcar/view/input/service/impl/CarInputView.java
@@ -2,6 +2,7 @@ package racingcar.view.input.service.impl;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.view.input.service.InputService;
+import racingcar.view.input.validator.racingcar.RacingCarValidator;
 import racingcar.view.output.service.InfoOutputViewService;
 
 public class CarInputView implements InputService {
@@ -13,6 +14,7 @@ public class CarInputView implements InputService {
     public String input() {
         infoOutputViewService.info();
         String input = Console.readLine();
+        RacingCarValidator.validator(input);
         return input;
     }
 }
