6aaa9eda0cfedbc0deeb0210cfa0a837944dffd8
권장순 <jsoonworld@gmail.com>
2024-10-27 15:04:37 +0900

    refactor(RaceSetupFacade): InputView와 OutputView 의존성 제거 및 초기화 로직 수정
    - initializeCars 메서드가 List<String> 형식의 자동차 이름을 받아 객체를 생성하도록 수정
    - InputView와 OutputView 의존성 제거로 초기화 작업에 집중할 수 있도록 개선
    - 불필요한 필드와 생성자 삭제로 클래스의 책임을 명확히 함

--- src/main/java/racingcar/config/RaceSetupFacade.java
+++ src/main/java/racingcar/config/RaceSetupFacade.java
@@ -5,37 +5,17 @@ import racingcar.domain.RacingCar;
 import racingcar.domain.RandomNumberGenerator;
 import racingcar.domain.WinnerDecider;
 import racingcar.domain.MovementPolicy;
-import racingcar.dto.AttemptCount;
 import racingcar.dto.MovementNumber;
-import racingcar.dto.RacingCarNames;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class RaceSetupFacade {
 
-    private final InputView inputView;
-    private final OutputView outputView;
-
-    public RaceSetupFacade(InputView inputView, OutputView outputView) {
-        this.inputView = inputView;
-        this.outputView = outputView;
-    }
-
-    public List<RacingCar> initializeCars() {
-        outputView.printCarNameInputPrompt();
-        RacingCarNames carNames = inputView.readRacingCarNames();
-        return carNames.carNames().stream()
+    public List<RacingCar> initializeCars(List<String> carNames) {
+        return carNames.stream()
                 .map(RacingCar::new)
                 .collect(Collectors.toList());
-    }
-
-    public int initializeAttemptCount() {
-        outputView.printNumberOfAttemptCountMessage();
-        AttemptCount attemptCount = inputView.readNumberOfAttemptCount();
-        return attemptCount.value();
     }
 
     public RaceProgressManager createRaceProgressManager(List<RacingCar> cars) {
