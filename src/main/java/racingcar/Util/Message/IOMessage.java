3b876657544994aa0b4c515f73efd0a950057cb9
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 20:13:42 +0900

    feat(IOMessage): 입출력 관련 메시지 상수화
    - 입출력과 관련해서 사용되는 문자열들을 상수화

--- src/main/java/racingcar/Util/Message/IOMessage.java
+++ src/main/java/racingcar/Util/Message/IOMessage.java
@@ -1,4 +1,18 @@
 package racingcar.Util.Message;
 
-public class IOMessage {
+public enum IOMessage {
+    INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    INPUT_TRIAL_COUNT("시도할 횟수는 몇 회인가요?"),
+    RACING_RESULT("\n실행 결과"),
+    RESULT_MESSAGE("최종 우승자 : ");
+
+    private final String message;
+
+    IOMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
