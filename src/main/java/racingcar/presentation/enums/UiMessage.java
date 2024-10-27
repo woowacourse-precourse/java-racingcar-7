fe48fc260fe2614ab1369d1c10728e43ab285f3c
jijongkwon <help3451@naver.com>
2024-10-27 23:51:37 +0900

    feat(view) : InputView 입력 메시지 메서드 구현
    - inputCarNameUi : 자동차 입력 메시지 출력
    - inputTryUi : 시도 횟수 입력 메시지 출력

--- src/main/java/racingcar/presentation/enums/UiMessage.java
+++ src/main/java/racingcar/presentation/enums/UiMessage.java
@@ -1,4 +1,16 @@
 package racingcar.presentation.enums;
 
 public enum UiMessage {
+    UI_INPUT_CAR_NAME_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    UI_INPUT_TRY_NUMBER_MESSAGE("시도할 횟수는 몇 회인가요?");
+
+    private final String message;
+
+    UiMessage(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
 }
--- src/main/java/racingcar/presentation/ui/InputView.java
+++ src/main/java/racingcar/presentation/ui/InputView.java
@@ -1,13 +1,15 @@
 package racingcar.presentation.ui;
 
+import racingcar.presentation.enums.UiMessage;
+
 public class InputView {
     // TODO: 자동차 입력 메시지 출력
     public void inputCarNameUi() {
-
+        System.out.println(UiMessage.UI_INPUT_CAR_NAME_MESSAGE.getMessage());
     }
 
     // TODO: 시도 횟수 입력 메시지 출력
     public void inputTryUi() {
-
+        System.out.println(UiMessage.UI_INPUT_TRY_NUMBER_MESSAGE.getMessage());
     }
 }
