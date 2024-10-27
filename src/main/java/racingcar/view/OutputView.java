d9483c8e774c69997448d6de2a08c46571ce40d1
Su-daa <subin.kk019@gmail.com>
2024-10-28 01:39:06 +0900

    feat: 우승자 출력 로직 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,11 +3,20 @@ package racingcar.view;
 import racingcar.model.Car;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class OutputView {
     public void printCarPosition(ArrayList<Car> cars) {
         for (Car car : cars) {
             System.out.println(car);
         }
+    }
+
+    public void printWinners(ArrayList<Car> winners) {
+        String winnerNames = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+
+        System.out.println("최종 우승자 : " + winnerNames);
     }
 }
