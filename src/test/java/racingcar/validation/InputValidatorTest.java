cd9b45ae0035fc69e96ab407523e68760be711d2
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-10-28 00:37:30 +0900

    refactor: 사용되지 않는 import문 제거

--- src/test/java/racingcar/validation/InputValidatorTest.java
+++ src/test/java/racingcar/validation/InputValidatorTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import racingcar.view.InputView;
 
 import java.util.List;
 import java.util.stream.Stream;
