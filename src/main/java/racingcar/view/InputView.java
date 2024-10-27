4f945a82417fe1a472556d6242c870979fbec8c6
qyinm <qusseun@gmail.com>
2024-10-28 00:06:40 +0900

    refactor: throws 키워드 추가 예외 발생 명시 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -13,7 +13,7 @@ public class InputView {
         return Console.readLine();
     }
 
-    public static List<String> getCarNames() {
+    public static List<String> getCarNames() throws IllegalArgumentException {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = readLine();
         if (!isValidCarInput(input)) {
@@ -30,7 +30,7 @@ public class InputView {
         return VALID_CAR_INPUT_PATTERN.matcher(input).matches();
     }
 
-    public static int getMoveCount() {
+    public static int getMoveCount() throws IllegalArgumentException {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String input = readLine();
         if (!isValidMoveCountInput(input)) {
