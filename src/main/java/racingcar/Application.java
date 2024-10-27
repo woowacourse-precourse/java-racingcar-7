331eef3178b68c0be6283d78f9ebf015cd7ef72c
wjdgkrud1 <wjdgkrud1@naver.com>
2024-10-27 23:37:44 +0900

    5글자를 넘으면 예외처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,6 +10,7 @@ public class Application {
 		System.out.println("이름은 5글자 이하 쉼표(,) 기준으로 구분");
 		String input = Console.readLine();
 		String[] arr = splitInput(input);
+		n_lenCheck(arr); 
 	}
 
 	public static String[] splitInput(String input) {
@@ -17,6 +18,18 @@ public class Application {
 			return input.split(",");
 		}
 		return new String[] { input };
+	}
+
+	public static void n_lenCheck(String[] arr) {
+		for (String name : arr) {
+			checkNameLength(name);
+		}
+	}
+
+	public static void checkNameLength(String name) {
+		if (name.length() > 5) {
+			throw new IllegalArgumentException("이름은 5글자 이하로 입력해주세요");
+		}
 	}
 
 	
