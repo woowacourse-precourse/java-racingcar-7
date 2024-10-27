f526547d9308ee7db842857f1a8c8dbcae711ea9
Parkjyun <jaeyun0507@naver.com>
2024-10-28 02:56:16 +0900

    feat(Controller): 자동차 경주게임을 실행한다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import racingcar.Controller.Controller;
+import racingcar.model.Movement;
+import racingcar.model.RandomMovement;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Movement movement = new RandomMovement();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Controller controller = new Controller(inputView, outputView, movement);
+        controller.run();
     }
 }
