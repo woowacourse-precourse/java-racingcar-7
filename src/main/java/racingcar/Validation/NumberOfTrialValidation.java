6709bf2f7dcba45a0b0f8b199e6b97e11c0bd91f
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 21:29:56 +0900

    feat(NumberOfTrialValidation): 시행 횟수과 관련된 검증 로직 구현
    - 시행 횟수과 관련된 검증 로직 구현
    - 뷰에서 바로 예외를 처리할 수 있는 입력 관련 예외와 내용을 검증해야 하는 내용 관련 예외로 나눔
    - 입력 관련 예외 : 시행 횟수가 정수가 아닌 경우
    - 내용 관련 예외 : 시행 횟수가 음수인 경우, 시행 횟수가 0인 경우

--- src/main/java/racingcar/Validation/NumberOfTrialValidation.java
+++ src/main/java/racingcar/Validation/NumberOfTrialValidation.java
@@ -1,4 +1,20 @@
 package racingcar.Validation;
 
 public class NumberOfTrialValidation {
+    public static boolean isNotInteger(String input) {
+        try {
+            Integer.parseInt(input);
+            return false;
+        } catch (NumberFormatException e) {
+            return true;
+        }
+    }
+
+    public static boolean isNegative(int trialCount) {
+        return trialCount < 0;
+    }
+
+    public static boolean isZero(int trialCount) {
+        return trialCount == 0;
+    }
 }
