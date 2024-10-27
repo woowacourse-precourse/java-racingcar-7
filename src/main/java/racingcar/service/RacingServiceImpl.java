c0a511d4ca27a78552227518bec98949a8498537
haebyun <bhk4392@gmail.com>
2024-10-27 18:09:21 +0900

    feat(RacingServiceImpl): 라운드 결과 생성 메서드 추가
    - `Car` 리스트를 `CarRoundStateDTO`로 변환하여 `RoundResultDTO`를 생성하는 메서드 구현

--- src/main/java/racingcar/service/RacingServiceImpl.java
+++ src/main/java/racingcar/service/RacingServiceImpl.java
@@ -1,9 +1,13 @@
 package racingcar.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.model.Car;
 import racingcar.domain.model.RacingManager;
 import racingcar.dto.request.RacingRequestDTO;
+import racingcar.dto.response.CarRoundStateDTO;
 import racingcar.dto.response.RacingResponseDTO;
+import racingcar.dto.response.RoundResultDTO;
 
 public class RacingServiceImpl implements RacingService {
 
@@ -16,5 +20,12 @@ public class RacingServiceImpl implements RacingService {
 
     private RacingManager initializeParticipants(List<String> carNames) {
         return RacingManager.createWithCars(carNames);
+    }
+
+    private RoundResultDTO createRoundResult(List<Car> cars) {
+        List<CarRoundStateDTO> carStates = cars.stream()
+                .map(CarRoundStateDTO::from)
+                .collect(Collectors.toList());
+        return RoundResultDTO.of(carStates);
     }
 }
