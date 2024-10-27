613a75c2276f5730ef7e0cfa8d4295dab770f11f
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-10-28 01:05:19 +0900

    refactor: carNames로 복수형으로 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class InputView {
 
-    public List<String> getCarNameInput(){
+    public List<String> getCarNamesInput(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         return InputValidator.validateCarNameAndReturn(input);
