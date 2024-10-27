e7a8f988538d3b2a012dd9e398c52531a976e0d9
qyinm <qusseun@gmail.com>
2024-10-27 19:57:42 +0900

    feat: 유저 입력 시 기본 출력문 출력 기능 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -14,6 +14,7 @@ public class InputView {
     }
 
     public static List<String> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = readLine();
         if (!isValidCarInput(input)) {
             throw new IllegalArgumentException("자동차 이름 입력이 올바르지 않습니다.");
@@ -30,6 +31,7 @@ public class InputView {
     }
 
     public static int getMoveCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
         String input = readLine();
         if (!isValidMoveCountInput(input)) {
             throw new IllegalArgumentException("이동 횟수 입력이 올바르지 않습니다.");
