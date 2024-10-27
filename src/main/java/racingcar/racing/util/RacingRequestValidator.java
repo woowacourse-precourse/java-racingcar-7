043733243b531fd92a827d88f704a879b4eab54f
siwoo <k74415834@gmail.com>
2024-10-27 19:51:56 +0900

    fix(RacingRequestValidator): 빈 문자열 뿐만 아니라 공백이 들어가 있는 문자열도 허용하지 않도록 변경

--- src/main/java/racingcar/racing/util/RacingRequestValidator.java
+++ src/main/java/racingcar/racing/util/RacingRequestValidator.java
@@ -14,7 +14,7 @@ public class RacingRequestValidator {
 
     private static void validateCarNameLength(List<String> carNames) {
         carNames.stream()
-                .filter(carName -> carName.isEmpty() || (5 < carName.length()))
+                .filter(carName -> carName.isBlank() || (5 < carName.length()))
                 .findAny()
                 .ifPresent(carName -> {
                     throw new IllegalArgumentException("자동차 이름은 1자 이상, 5자 이하여야 합니다.");
