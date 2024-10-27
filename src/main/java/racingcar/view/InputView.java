037a9483c51c93bfe683e44589a8cdda573eda12
Chan Mi <rkdcksal02@gmail.com>
2024-10-28 04:49:17 +0900

    refactor: 자동차 이름 공백 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,13 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
 
     public static List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
-        List<String> carNames = Arrays.asList(input.split(","));
+
+        List<String> carNames = Arrays.stream(input.split(","))
+                .map(String::trim) // 공백 제거
+                .collect(Collectors.toList());
         return carNames;
     }
 
