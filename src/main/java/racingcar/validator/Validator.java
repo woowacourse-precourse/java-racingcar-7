e5954ece1e6f32c8eee37cc5d844ef273c54357b
MinJae00 <exceedmin@gmail.com>
2024-10-28 03:48:56 +0900

    feat: 예외처리 담당 클래스 생성

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -1,2 +1,36 @@
-package racingcar.validator;public class Validator {
+package racingcar.validator;
+
+public class Validator {
+    private static String inputString;
+    private static int inputSize;
+    public String userName;
+
+    public void userNameValidate(String userName) {
+        this.userName = userName;
+        isExceedMaxLen();
+    }
+
+    public void isCommaInFrontorBack() {
+        if ((inputString.charAt(0) == ',') || (inputString.charAt(inputSize - 1) == ',')) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void isExceedMaxLen() {
+        if (userName.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+    public void isEmptyInput() {
+        if (inputString.equals("")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Validator(String inputString) {
+        this.inputString = inputString;
+        this.inputSize = inputString.length();
+    }
 }
