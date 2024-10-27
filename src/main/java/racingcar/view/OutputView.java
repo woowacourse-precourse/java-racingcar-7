9dfa07c67b26a256d03a86b3081a2ba6da893f31
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-28 05:19:52 +0900

    feat(OutputView): 시도 횟수 입력 안내 메시지 출력 메서드 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public void printCarNamesInstruction() {
         System.out.println(InfoMessage.CAR_NAMES_INSTRUCTION.getMessage());
     }
+
+    public void printMovingCountInstruction() {
+        System.out.println(InfoMessage.MOVING_COUNT_INSTRUCTION.getMessage());
+    }
 }
