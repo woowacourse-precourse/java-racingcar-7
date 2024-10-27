e57495199602dd44aa9a181476fc0b5b25c33f7c
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-27 22:34:08 +0900

    feat(RacingGame): add 레이싱 게임 실행 로직
    - 게임의 시작을 담당하는 start() 메서드 구현
    - 입력 검증을 수행하는 getValidatedUserInput() 메서드 추가
    - 자동차 목록을 초기화하는 initCars() 메서드 구현
    - 경주 시도 횟수만큼 라운드를 반복하는 runRaceRounds() 메서드 추가
    - 각 라운드마다 자동차를 이동시키는 runRaceRound() 메서드 구현
    - 결과 출력은 OutputHandler를 통해 수행하며, 레이싱 게임의 흐름을 제어

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,57 @@
+package racingcar;
+
+import racingcar.observer.CarMovePrinter;
+
+import java.util.List;
+
+public class RacingGame {
+
+    private final InputHandler inputHandler;
+    private final Validator validator;
+    private final RandomValueGenerator randomValueGenerator;
+    private final OutputHandler outputHandler;
+
+    public RacingGame(InputHandler inputHandler,
+                      Validator validator,
+                      RandomValueGenerator randomValueGenerator,
+                      OutputHandler outputHandler) {
+        this.inputHandler = inputHandler;
+        this.validator = validator;
+        this.randomValueGenerator = randomValueGenerator;
+        this.outputHandler = outputHandler;
+    }
+
+    public void start() {
+        UserInput userInput = getValidatedUserInput();
+        Cars cars = initCars(userInput.carNames());
+        int attemptCount = Integer.parseInt(userInput.attemptCount());
+
+        outputHandler.outputMessageWithNewLine("실행결과");
+        runRaceRounds(cars, attemptCount);
+        outputHandler.displayWinners(cars.getWinners());
+    }
+
+    private UserInput getValidatedUserInput() {
+        UserInput userInput = inputHandler.inputCarNamesAndAttemptCount();
+        validator.validateInput(userInput);
+        return userInput;
+    }
+
+    private Cars initCars(String carNames) {
+        Cars cars = new Cars(StringSplitter.splitByComma(carNames));
+        cars.addObserverToAll(CarMovePrinter.class, outputHandler);
+        return cars;
+    }
+
+    private void runRaceRounds(Cars cars, int attemptCount) {
+        for (int i = 0; i < attemptCount; i++) {
+            runRaceRound(cars);
+            outputHandler.outputNewLine();
+        }
+    }
+
+    private void runRaceRound(Cars cars) {
+        List<Integer> randomValues = randomValueGenerator.generateMultiple(0, 9, cars.size());
+        cars.moveAll(randomValues);
+    }
+}
