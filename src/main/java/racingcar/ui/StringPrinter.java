7509ec7c09b58d96dd4d862d20aa3dd8c1b93089
Jang <sdsd090811@gmail.com>
2024-10-27 10:47:55 +0900

    feat: 기본 입출력 클래스 추가

---
+++ src/main/java/racingcar/ui/StringPrinter.java
@@ -0,0 +1,7 @@
+package racingcar.ui;
+
+public class StringPrinter {
+    public void print(String value) {
+        System.out.println(value);
+    }
+}
---
+++ src/main/java/racingcar/ui/StringReader.java
@@ -0,0 +1,9 @@
+package racingcar.ui;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class StringReader {
+    public String readLine() {
+        return Console.readLine();
+    }
+}
