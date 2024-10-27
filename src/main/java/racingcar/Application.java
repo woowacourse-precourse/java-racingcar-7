625caa2762291e42409db010916eab3a96012ed5
선경훈 <seongyeonghun@MacBook-M3-Pro.local>
2024-10-27 22:02:25 +0900

    feat: 이동 횟수 입력 및 검증 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,6 +9,9 @@ public class Application {
     public static void main(String[] args) {
         // 자동차 이름 입력 및 검증
         List<String> nameList = inputCarNames();
+
+        // 이동 횟수 입력 및 검증
+        int times = inputRaceTimes();
     }
 
     private static List<String> inputCarNames() {
@@ -21,5 +24,14 @@ public class Application {
             }
         }
         return nameList;
+    }
+
+    private static int inputRaceTimes() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        try {
+            return Integer.parseInt(readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("횟수는 숫자만 입력할 수 있습니다.");
+        }
     }
 }
