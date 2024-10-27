0bfc098d5a4f347b126f2dbe9c6550a0b1fd89a7
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 20:17:03 +0900

    feat(InputView): 사용자 입력 받기 기능 구현부 추가
    - inputCarName() 메서드를 통해서 사용자에게 자동차 이름들을 입력받는 기능 구현
    - splitNames() 메서드를 통해서 이름 앞 뒤의 공백을 제거하는 기능을 구현
    - inputTrialCount() 메서드를 통해서 사용자에게 시행 횟수를 입력받는 기능 구현

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -1,4 +1,33 @@
 package racingcar.View;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.Util.Message.IOMessage;
+
 public class InputView {
+    public List<String> inputCarName() {
+        System.out.println(IOMessage.INPUT_CAR_NAMES.getMessage());
+        String names = Console.readLine();
+
+        return splitNames(names);
+    }
+
+    public int inputTrialCount() {
+        System.out.println(IOMessage.INPUT_TRIAL_COUNT.getMessage());
+        int count = Integer.parseInt(Console.readLine());
+
+        return count;
+    }
+
+    public List<String> splitNames(String names) {
+        String[] splitNames = names.split(",");
+        List<String> carNames = new ArrayList<>();
+
+        for (String name : splitNames) {
+            carNames.add(name.trim());
+        }
+
+        return carNames;
+    }
 }
