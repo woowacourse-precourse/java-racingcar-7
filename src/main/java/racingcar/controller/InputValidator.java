35b2a6c246c3f7ec4511b8409067b9497417e47d
LeeJiWon <dlwldnjs1009@gmail.com>
2024-10-27 21:43:11 +0900

    feat: 검증 기능 구현

---
+++ src/main/java/racingcar/controller/InputValidator.java
@@ -0,0 +1,16 @@
+package racingcar.controller;
+
+public class InputValidator {
+
+    public static void validateCarName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능하다.");
+        }
+    }
+
+    public static void validateAttemptCount(int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+        }
+    }
+}
