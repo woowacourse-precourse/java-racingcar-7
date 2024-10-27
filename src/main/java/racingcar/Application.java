40297997081b137d1de4ee4f944807d0ff93ed73
Minseok <saltishDev@gmail.com>
2024-10-27 20:05:26 +0900

    feat: 문자열 입력받는 기능 구현
    1. 인자 없이 사용하면, 단순히 문자열만 입력받음
    2. 인자를 주고 사용하면, 사용자에게 보일 메세지를 출력한 후 문자열을
       입력받음

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static String inputString() {
+        return inputString(null);
+    }
+
+    private static String inputString(String message) {
+        if (message == null) {
+            return Console.readLine();
+        }
+        System.out.println(message);
+        return Console.readLine();
     }
 }
