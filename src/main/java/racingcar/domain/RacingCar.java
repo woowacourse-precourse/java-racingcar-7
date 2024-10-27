1b8950e4d1c8e583990f063ac2d7af7459d7cb1d
201912025 <michi2012@naver.com>
2024-10-27 13:46:14 +0900

    feat(RacingCar): createRacingCarsBySplitCarsName() 메서드 구현

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,18 +1,15 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCar {
     private final String carName;
-    private final List<String> advanceResult;
+    private final List<String> advanceResults;
 
     public RacingCar(String carName, List<String> advanceResult) {
         this.carName = carName;
-        this.advanceResult = advanceResult;
-    }
-
-    public void createRacingCars(String inputCarsName) {
-        String[] carsName = splitCarsName(inputCarsName);
+        this.advanceResults = advanceResult;
     }
 
     public String[] splitCarsName(String inputCarsName) {
@@ -21,5 +18,11 @@ public class RacingCar {
         return carsNames;
     }
 
-
+    public List<RacingCar> createRacingCarsBySplitCarsName(String[] carsNames) {
+        List<RacingCar> racingCars = new ArrayList<>();
+        for (String carName : carsNames) {
+            racingCars.add(new RacingCar(carName, new ArrayList<>()));
+        }
+        return racingCars;
+    }
 }
