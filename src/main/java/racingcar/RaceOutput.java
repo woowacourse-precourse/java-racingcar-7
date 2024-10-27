378fc478400dd0379d5952f93134d81b241140ff
maedachaeva <parkchaeyeon.b@gmail.com>
2024-10-27 11:49:05 +0900

    feat: 현재 차수의 경주 결과를 출력하는 기능 구현

--- src/main/java/racingcar/RaceOutput.java
+++ src/main/java/racingcar/RaceOutput.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import java.util.LinkedHashMap;
+
 public class RaceOutput {
-    public void printRaceStatus() {
+    public void printRaceStatus(LinkedHashMap<String, StringBuilder> carMovementRecords) {
             System.out.println("실행 결과");
+        for (String racingCarName : carMovementRecords.keySet()) {
+            System.out.println(racingCarName + " : " + carMovementRecords.get(racingCarName));
+        }
     }
 }
