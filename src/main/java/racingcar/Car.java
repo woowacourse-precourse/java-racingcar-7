6220f6afe9f8ec1979ba331a8280aa9867dada4a
genmu5 <genmu4@skuniv.ac.kr>
2024-10-28 02:10:32 +0900

    feat : 자동차 이름 입력, 자동차 이름과 이동 횟수검증, 자동차 만들기, 자동차 전진기능 추가

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    public void move(int randomValue) {
+        if (randomValue >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
