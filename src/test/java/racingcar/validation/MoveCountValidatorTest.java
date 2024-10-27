c094bd7b7dab706b777cd7a3f7d11c2bc1a5df4d
Sinorang <sinholang@naver.com>
2024-10-27 19:55:18 +0900

    test: 이동 횟수 숫자가 아닌 경우 예외 테스트 구현
    - 이동 횟수 입력값이 숫자가 아닌 경우 예외 테스트 코드 작성

--- src/test/java/racingcar/validation/MoveCountValidatorTest.java
+++ src/test/java/racingcar/validation/MoveCountValidatorTest.java
@@ -2,6 +2,7 @@ package racingcar.validation;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.constants.ErrorMessageConstants;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -13,5 +14,13 @@ public class MoveCountValidatorTest {
         assertThatThrownBy(() -> MoveCountValidator.validateMoveCount(moveCountInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessageConstants.ERROR_EMPTY_MOVE_COUNT);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"five", "5.5", "abc"})
+    void 이동_횟수_숫자가_아닌_경우_예외_테스트(String moveCountInput) {
+        assertThatThrownBy(() -> MoveCountValidator.validateMoveCount(moveCountInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessageConstants.ERROR_MOVE_COUNT_NOT_NUMBER);
     }
 }
