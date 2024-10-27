03424394b42099f4cb564dc48879032e001ffe0b
bowook <mbosx316@naver.com>
2024-10-27 19:39:45 +0900

    refactor(OutputView) : OutputView 리팩토링
    - static으로 수정하였습니다.
    - 상태를 가지고 있지 않고 행위만을 수행하기 때문입니다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,32 +1,30 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class OutputView {
 
-    public void executionResultMessage() {
+    public static void executionResultMessage() {
         System.out.println("\n실행 결과");
     }
 
-    public void currentStatusMessage(Cars cars) {
+    public static void currentStatusMessage(Cars cars) {
         for (Car car : cars.getCars()) {
             System.out.println(car.getCarName() + " : " + "-".repeat(car.getPosition()));
         }
         System.out.println();
     }
 
-    public void finalWinnerMessage(List<Car> winners) {
+    public static void finalWinnerMessage(List<Car> winners) {
         List<String> carNames = winners.stream()
                 .map(Car::getCarName)
                 .collect(Collectors.toList());
         String message = "최종 우승자 : " + String.join(", ", carNames);
         System.out.println(message);
     }
-
 
 
 }
