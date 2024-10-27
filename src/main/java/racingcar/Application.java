32a512f175a0466b150b78c038374c187af2eed3
iceHood <hanyo3477@gmail.com>
2024-10-28 02:26:38 +0900

    feat: 차량 읽어오기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        String readed = Console.readLine();
+
+        String[] cars = readed.split(",");
+        
     }
 }
