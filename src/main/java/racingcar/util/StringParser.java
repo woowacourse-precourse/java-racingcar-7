a1cd9970bcf2321dac58714d292391dc38f49ae8
gbtwld <gbtwld@gmail.com>
2024-10-27 21:22:59 +0900

    fix: trim whitespace from car names during parsing
    StringParser 클래스에서 자동차 이름을 파싱할 때, 각 이름의 앞뒤 공백을 제거하도록 로직을 수정했습니다.
    이를 통해 입력된 자동차 이름에 불필요한 공백이 포함되지 않도록 개선했습니다.
    메서드 이름도 `parseTryNumberString`에서 `parseTryCountString`으로 변경하여 의미를 더 명확하게 표현했습니다.

--- src/main/java/racingcar/util/StringParser.java
+++ src/main/java/racingcar/util/StringParser.java
@@ -1,15 +1,20 @@
 package racingcar.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class StringParser {
     public List<String> parseCarNameString(String input) {
+        List<String> carNameList = Arrays.asList(input.split(","));
+        for (String carName : carNameList) {
+            carNameList.set(carNameList.indexOf(carName), carName.trim());
+        }
 
-        return Arrays.asList(input.split(","));
+        return carNameList;
     }
 
-    public int parseTryNumberString(String input) {
+    public int parseTryCountString(String input) {
         return Integer.parseInt(input);
     }
 }
