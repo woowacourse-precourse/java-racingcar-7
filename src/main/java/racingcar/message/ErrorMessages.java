b215601951fa580f235a59640c20a550b495eb46
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 11:52:28 +0900

    :sparkles: feat(ErrorMessages): 이름이 빈 문자열일 때 오류 메시지 제공

--- src/main/java/racingcar/message/ErrorMessages.java
+++ src/main/java/racingcar/message/ErrorMessages.java
@@ -5,4 +5,7 @@ public class ErrorMessages {
     // Car names related error messages
     public static final String UNPROVIDED_NAMES = "[ERROR] 입력된 이름이 없습니다. 이름을 1개 이상 입력해주세요.";
     public static final String DUPLICATED_NAMES = "[ERROR] 중복된 이름이 있습니다. 각 이름이 고유하도록 입력해주세요.";
+
+    // Car name related errors messages
+    public static final String CLEAR_NAME = "[ERROR] 이름은 비워둘 수 없습니다. 이름을 입력해주세요.";
 }
