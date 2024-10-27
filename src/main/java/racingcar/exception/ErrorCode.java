25204cc5b726abdb0adc503caecacd2b7296ba2e
Jinu Yi <dionisos198@naver.com>
2024-10-27 22:39:48 +0900

    refactor: 중복된 차량 이름 입력 시 예외 처리

--- src/main/java/racingcar/exception/ErrorCode.java
+++ src/main/java/racingcar/exception/ErrorCode.java
@@ -10,7 +10,9 @@ public enum ErrorCode {
 
     CANT_CONVERT_CAR_NAME_WITH_NULL_OR_BLANK("NULL 혹은 빈 값은 차량의 이름이 되지 못합니다"),
 
-    CANT_TRY_COUNT_UNDER_ZERO("시도 횟수는 0 이하가 될 수 없습니다");
+    CANT_TRY_COUNT_UNDER_ZERO("시도 횟수는 0 이하가 될 수 없습니다"),
+
+    CANT_INSERT_DUPLICATE_CAR_NAME("중복된 차량의 이름은 입력할 수 없습니다");
 
    private final String message;
 
