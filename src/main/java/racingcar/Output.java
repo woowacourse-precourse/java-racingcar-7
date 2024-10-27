c4bfdf3742c01353ec14f01dbb2fc4f9cd825ca7
KoYunHyuk <59502729+KoYunHyuk@users.noreply.github.com>
2024-10-27 14:31:01 +0900

    refactor: Modify Enhanced for loop

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -1,36 +1,37 @@
 package racingcar;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Output {
-    List<Car> carList = new ArrayList<>();
+    List<Car> carList;
 
     public Output(List<Car> carList) {
         this.carList = carList;
     }
 
     public void printOutput() {
-        int max = -1;
-        int count = 0;
+        int max = 0;
+        List<String> resultList = new ArrayList<>();
 
-        for (int i = 0; i < carList.size(); i++) {
-            if (carList.get(i).getPosition() > max) {
-                max = carList.get(i).getPosition();
-                count++;
-            } else if (carList.get(i).getPosition() == max) {
-                count++;
+        for(Car car : carList) {
+            if(car.getPosition() > max) {
+                max = car.getPosition();
+            }
+        }
+
+        for(Car car : carList) {
+            if(car.getPosition() == max) {
+                resultList.add(car.getName());
             }
         }
 
         System.out.print("최종 우승자 : ");
-        for (int i = 0; i < carList.size(); i++) {
-            if (carList.get(i).getPosition() == max) {
-                System.out.print(carList.get(i).getName());
-
-                if (--count != 0) {
-                    System.out.print(", ");
-                }
+        for (int i = 0; i < resultList.size(); i++) {
+            System.out.print(resultList.get(i));
+            if (i != resultList.size() - 1) {
+                System.out.print(", ");
             }
         }
     }
