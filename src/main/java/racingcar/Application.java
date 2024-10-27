329995205c0cec319d887ce5ef021e2376dd4103
CheChe903 <cheche903@naver.com>
2024-10-27 10:06:45 +0900

    feat: 라운드 횟수 입력 기능 구현
    자동차가 몇 번 이동할 것인지 입력합니다.
    이는 양의 정수만 가능하며 이외에는 예외처리합니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
 
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
         String input = Console.readLine();
 
         String[] carNameList = input.split(",");
@@ -13,6 +15,18 @@ public class Application {
             if(car.isEmpty() || car.length() >5) {
                 throw new IllegalArgumentException();
             }
+        }
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+
+        try {
+            int roundNumber = Integer.parseInt(Console.readLine());
+
+            if(roundNumber < 1) {
+                throw new IllegalArgumentException();
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
         }
 
     }
