3065e91d515e6140175ebd003d54118df800bb39
Shinjongyun <tigerjong123@gmail.com>
2024-10-28 03:44:10 +0900

    feat: WinnerCalculatorTest 추가

---
+++ src/test/java/racingcar/domain/winner/WinnerCalculatorTest.java
@@ -0,0 +1,53 @@
+package racingcar.domain.winner;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.car.entity.Car;
+import racingcar.domain.road.entity.Road;
+import racingcar.domain.winner.entity.Winner;
+import racingcar.domain.winner.service.WinnerCalculator;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WinnerCalculatorTest {
+
+    private final WinnerCalculator calculator = new WinnerCalculator();
+
+    @Test
+    @DisplayName("가장 멀리 이동한 자동차 한 대를 우승자로 계산한다")
+    void calculateSingleWinner() {
+        // given
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        Car car3 = new Car("car3");
+        List<Car> carList = Arrays.asList(car1, car2, car3);
+        car1.getRoad().addRoad(1);
+
+        // when
+        Winner winner = calculator.calculateWinner(carList);
+
+        // then
+        assertThat(winner.getWinningCars()).containsExactly(car1);
+    }
+
+    @Test
+    @DisplayName("동일한 거리로 이동한 자동차 여러 대를 우승자로 계산한다")
+    void calculateMultipleWinners() {
+        // given
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        Car car3 = new Car("car3");
+        List<Car> carList = Arrays.asList(car1, car2, car3);
+        car1.getRoad().addRoad(1);
+        car2.getRoad().addRoad(1);
+
+        // when
+        Winner winner = calculator.calculateWinner(carList);
+
+        // then
+        assertThat(winner.getWinningCars()).containsExactlyInAnyOrder(car1, car2);
+    }
+}
