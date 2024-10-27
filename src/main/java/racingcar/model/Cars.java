f7c5064768c669912d3b78b1b1cf6942df36cec6
xeimun <hinote444@naver.com>
2024-10-27 14:05:08 +0900

    feat(Cars): 자동차 이름이 중복되었는지 검사하는 기능 구현

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,13 +1,16 @@
 package racingcar.model;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Cars {
 
     private final List<Car> cars;
 
     private Cars(List<Car> cars) {
+        validateUniqueCarNames(cars);
         this.cars = cars;
     }
 
@@ -28,5 +31,16 @@ public class Cars {
 
     public List<Car> getCars() {
         return cars;
+    }
+
+    public void validateUniqueCarNames(List<Car> cars) {
+        Set<String> uniqueNames = new HashSet<>();
+
+        for (Car car : cars) {
+            String carName = car.getName();
+            if (!uniqueNames.add(carName)) {
+                throw new IllegalArgumentException("이름은 중복될 수 없습니다. 중복된 이름 : " + carName);
+            }
+        }
     }
 }
