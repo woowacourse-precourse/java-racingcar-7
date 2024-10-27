b7fdf4029a9a07e612d019e464b78919d71076df
jeong-baechoo <baechoo0905@naver.com>
2024-10-28 04:09:37 +0900

    refactor: DI를 위해서 static 제거

--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -19,22 +19,26 @@ class InputViewTest {
     @Test
     @DisplayName("자동차 이름 입력을 정상적으로 반환한다")
     void readCarNames() {
+        InputView inputView = new InputView();
+
         String input = "pobi,woni,jun\n";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
-        String result = InputView.readCarNames();
+        String result = inputView.readCarNames();
         assertThat(result).isEqualTo("pobi,woni,jun");
     }
 
     @Test
     @DisplayName("시도 횟수 입력을 정상적으로 반환한다")
     void readAttempts() {
+        InputView inputView = new InputView();
+
         String input = "5\n";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
-        String result = InputView.readAttempts();
+        String result = inputView.readAttempts();
         assertThat(result).isEqualTo("5");
     }
 }
