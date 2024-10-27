6163d341400bf582943497ef59b6dfdf13652b2b
gyuoo <yeomky98@naver.com>
2024-10-28 03:27:10 +0900

    feat(application): set up main application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import racingcar.controller.RaceController;
+import racingcar.util.Parser;
+import racingcar.util.Validator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView(new Validator(), new Parser());
+        OutputView outputView = new OutputView();
+        RaceController raceController = new RaceController(inputView, outputView);
+
+        raceController.startRace();
     }
 }
