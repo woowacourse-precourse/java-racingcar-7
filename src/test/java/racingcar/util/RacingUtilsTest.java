c76eb77b9eab7741ee251bb644f60b7e75ccc8b4
ahma0 <dsd932@naver.com>
2024-10-27 18:22:17 +0900

    test: 유틸 클래스 테스트
    자동차 이름에 중복이 있는 경우, 띄어쓰기가 있을 경우, 5자를 넘어가는 경우, 잘못된 실행 횟수를 입력받은 경우

---
+++ src/test/java/racingcar/util/RacingUtilsTest.java
@@ -0,0 +1,59 @@
+package racingcar.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class RacingUtilsTest {
+
+    @Test
+    @DisplayName("입력받은 자동차 이름에 중복 값이 있는 경우 예외를 발생시킨다.")
+    void duplicateCarNameTest() {
+        assertThatThrownBy(() -> RacingUtils.divideComma("123,123,456"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 입력입니다.");
+    }
+
+    @Test
+    @DisplayName("쉼표 옆에 띄어쓰기가 있을 경우 띄어쓰기를 제거하고 분리한다.")
+    void commaAroundWhiteSpaceTest() {
+        assertThat(RacingUtils.divideComma("123 , 456, 789"))
+                .containsExactlyElementsOf(List.of("123", "456", "789"));
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 5자를 넘어가는 경우 예외를 발생시킨다.")
+    void ExceedCarNameLengthTest() {
+        assertAll(
+                () -> assertThat(RacingUtils.divideComma("12345, 6789"))
+                        .containsExactlyElementsOf(List.of("12345", "6789")),
+                () -> assertThatThrownBy(() -> RacingUtils.divideComma("123456, 789"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("잘못된 입력입니다.")
+        );
+    }
+
+    @Test
+    @DisplayName("입력받은 실행 횟수가 정수형이 아니거나 양수가 아닐 경우 예외를 발생시킨다.")
+    void executeCountTest() {
+        assertAll(
+                () -> assertThatThrownBy(() -> RacingUtils.parseInt("test"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("잘못된 입력입니다."),
+                () -> assertThatThrownBy(() -> RacingUtils.parseInt("6.2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("잘못된 입력입니다."),
+                () -> assertThatThrownBy(() -> RacingUtils.parseInt("-9"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("잘못된 입력입니다."),
+                () -> assertThat(RacingUtils.parseInt("3"))
+                        .isEqualTo(3)
+        );
+    }
+
+}
