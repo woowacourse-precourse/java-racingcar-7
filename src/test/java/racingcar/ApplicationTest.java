94ca0e6005713ff0ad2b35b9f07be48ee44df7a2
parksaegyeol <dmsgkrkt1@naver.com>
2024-10-27 19:04:21 +0900

    test : 입력받은 자동차 문자열 자르는 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -11,6 +12,20 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+
+    @Test
+    void 입력_문자열_자르기() {
+        String inputCarNames = "박새결,김주아,조성민";
+        String[] carNames = RacingCar.splitInputCarNames(inputCarNames);
+        RacingCar.checkInputCarNames(carNames);
+
+        for (String carName : carNames) {
+            System.out.println("carName = " + carName);
+        }
+
+        Assertions.assertThat(carNames.length).isEqualTo(3);
+
+    }
 
     @Test
     void 기능_테스트() {
