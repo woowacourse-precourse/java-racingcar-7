42b5354735d54086be1f98d61059d3e885c7482c
soyoon19 <96063941+soyoon19@users.noreply.github.com>
2024-10-28 05:52:36 +0900

    Feat : Add Race function
    createParticipant 메서드, start 메서드 구현

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,13 +1,41 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Race {
+    private InputManager inputManager;
+    private OutputManager outputManager;
+    private Referee referee;
+    public Race(InputManager inputManager, OutputManager outputManager, Referee referee){
+        this.inputManager = inputManager;
+        this.outputManager = outputManager;
+        this.referee = referee;
+    }
     public List<Car> createParticipant(List<String> carName) {
-        return null;
+        List<Car> carList = new ArrayList<>();
+        for(int i = 0; i < carName.size(); i++){
+            String name = carName.get(i);
+            carList.add(new RacingCar(name));
+        }
+        return carList;
     }
 
     public void start() {
+        inputManager.whatIsYourName();
+        List<String> carName = inputManager.splitName(inputManager.getName());
+        List<Car> carList = createParticipant(carName);
 
+        inputManager.HowManyAttempt();
+        int count = inputManager.getNumberOfAttempt();
+
+        List<Car> result = null;
+        for(int i = 0; i < count; i++){
+            result = referee.checkDrivingDistance(carList);
+            outputManager.printExecutionResult(result);
+        }
+
+        List<Car> winners = referee.judgeWinner(result);
+        outputManager.printWinner(winners);
     }
 }
