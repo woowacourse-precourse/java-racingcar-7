d88084e5c2837f73c5c3a95c18879694d86ce14b
chyo1 <hyowon3856@gmail.com>
2024-10-27 15:34:38 +0900

    test : 변수명 수정

--- src/test/java/racingcar/IOControllerTest.java
+++ src/test/java/racingcar/IOControllerTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,20 +11,20 @@ import org.junit.jupiter.api.Test;
 class IOControllerTest extends NsTest {
     IOController ioController = new IOController();
     Car testCar1, testCar2, testCar3;
-    List<Car> testCarList;
+    List<Car> testCars;
 
     @BeforeEach
     void setUp() {
         testCar1 = new Car("test1");
         testCar2 = new Car("test2");
         testCar3 = new Car("test3");
-        testCarList = new ArrayList<>(List.of(testCar1, testCar2, testCar3));
+        testCars = List.of(testCar1, testCar2, testCar3);
     }
 
     @Test
     void 결과_출력(){
         testCar3.move();
-        ioController.printResults(testCarList);
+        ioController.printResults(testCars);
         assertThat(output()).contains("test1 : ", "test2 : ", "test3 : -");
     }
 
