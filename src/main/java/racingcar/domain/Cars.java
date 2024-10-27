0308b63003ef14a45f038ddd8614247fbffe788b
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-10-28 05:07:31 +0900

    feat : 자동차 이름 중복 예외 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -3,24 +3,52 @@ package racingcar.domain;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntFunction;
 import java.util.stream.Collectors;
 
 public class Cars {
     private final List<Car> cars;
 
     public Cars(String[] players) {
+        validDuplicate(players);
         this.cars = Arrays.stream(players)
                 .map(Car::new)
-                .collect(Collectors.toUnmodifiableList());
+                .toList();
     }
 
-    public int getSize(){
+    private void validDuplicate(String[] players) {
+        long duplicateCount = calculateDuplicate(players);
+        if (duplicateCount >= 1) {
+            throw new IllegalArgumentException("같은 이름을 사용할 수 없습니다");
+        }
+    }
+
+    private long calculateDuplicate(String[] players) {
+        return Arrays.stream(players)
+                .filter(player -> Arrays.stream(players).filter(player::equals).count() > 1)
+                .distinct()
+                .count();
+    }
+
+    private static IntFunction<String> nameOfPlayer(String[] players) {
+        return i -> getString(players, i);
+    }
+
+    private static String getString(String[] players, int i) {
+        if (players[i].isEmpty()) {
+            return String.format("%d 번째 선수", (i + 1));
+        }
+        return players[i];
+    }
+
+    public int getSize() {
         return cars.size();
     }
 
     public List<Car> getCars() {
         return new ArrayList<>(cars);
     }
+
 
     public void tryMove(NumberGenerator numberGenerator) {
         cars.forEach(car -> car.move(numberGenerator.generateNumber()));
--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -9,8 +9,19 @@ public class CarsTest {
     @Test
     @DisplayName("자동차 생성 테스트")
     void 자동차_생성_테스트() {
-        Cars cars = new Cars(new String[] {"pobi","woni"});
+        Cars cars = new Cars(new String[]{"pobi", "woni"});
 
-        Assertions.assertThat(cars.getSize()).isEqualTo(2);
+        Assertions.assertThat(cars.getCars()).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("자동차 이름 중복 시 예외 발생")
+    void 자동차_이름_중복_시_예외_발생() {
+
+        Assertions.assertThatThrownBy(() -> new Cars(new String[]{"pobi", "pobi"}))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("같은 이름을 사용할 수 없습니다");
+
+        ;
     }
 }
