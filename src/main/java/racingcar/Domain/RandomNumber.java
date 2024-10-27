ce1186a5bb0794907e0dce6df500926333c0fc0c
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 23:55:32 +0900

    feat(RandomNumber): 랜덤하게 전진하기 위한 로직 구현
    - camp.nextstep.edu.missionutils.Randoms 에 위치한 Randoms API를 사용해서 랜덤하게 정수를 반환하는 로직 구현

--- src/main/java/racingcar/Domain/RandomNumber.java
+++ src/main/java/racingcar/Domain/RandomNumber.java
@@ -1,4 +1,9 @@
 package racingcar.Domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomNumber {
+    public Integer pickNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
 }
