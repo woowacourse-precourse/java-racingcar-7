8dba3ce544cae1ba925822c08b30aa71804e36b3
dpfls0922 <dpfls0922@naver.com>
2024-10-27 16:13:01 +0900

    feat: 자동차 이름 유효성 검사

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,14 +9,30 @@ public class Application {
     private static final int MIN_VALUE = 0;
     private static final int MAX_VALUE = 9;
     private static final int FORWARD_CRITERIA = 4;
+    private static final String CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 5자 이하이어야 합니다.";
     private static int[] carPositions;
 
     private static String[] getCarNames() {
         String carNames;
+        String[] splitCarNames;
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         carNames = Console.readLine();
+        splitCarNames = splitCarNamesByDelimiter(carNames);
+        validateCarNames(splitCarNames);
+        return splitCarNames;
+    }
+
+    private static String[] splitCarNamesByDelimiter(String carNames) {
         return carNames.split(",");
+    }
+    
+    private static void validateCarNames(String[] carNames) {
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR_MESSAGE);
+            }
+        }
     }
 
     private static int getGameCount() {
