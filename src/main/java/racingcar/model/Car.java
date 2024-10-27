018c29bb20c5c647640b3703f53126ef882d1e27
steamedEggMaster <cksdud5014@naver.com>
2024-10-27 13:58:41 +0900

    refactor(Model): Car 클래스를 통해 자동차 관련 로직 분리
    1. 자동차 객체를 표현하는 Car 클래스 생성
    2. 자동차 이동 및 출력 기능 -> Car 클래스에 추가
    3. Car 객체 활용을 위해 InputParser 코드 리팩토링
       1) List에 String 객체가 아닌 Car 객체 추가로 변경

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,22 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private static final String START_POSITION = "";
+    private final String name;
+    private String position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = START_POSITION;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+}
--- src/main/java/racingcar/model/InputParser.java
+++ src/main/java/racingcar/model/InputParser.java
@@ -1,42 +1,50 @@
 package racingcar.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class InputParser {
     private static final String NAME_DELIMITER = ",";
     private static final String ERROR_EMPTY_CAR_NAME = "자동차 이름이 아무것도 들어오지 않았습니다.";
     private static final String ERROR_CAR_NAME_TOO_LONG = "자동차 이름의 길이가 5를 초과하였습니다.";
+    private static final String ERROR_SAME_CAR_NAME = "동일한 자동차 이름이 존재합니다.";
     private static final String ERROR_INVALID_TRY_COUNT = "알맞지 않은 시도 횟수 값이 들어왔습니다.";
-    private final List<String> carNames;
+    private final List<Car> cars;
     private final int tryCount;
 
     public InputParser(String carNamesInput, String tryCountInput) {
-        carNames = new ArrayList<>();
+        cars = new ArrayList<>();
         parseCarNames(carNamesInput);
         tryCount = parseTryCount(tryCountInput);
     }
 
-    private void parseCarNames(String carNamesInput){
+    private void parseCarNames(String carNamesInput) {
         validateCarNamesNotEmpty(carNamesInput);
+        Set<String> uniqueCarNames = new HashSet<>();
+
         for (String carName : carNamesInput.split(NAME_DELIMITER)) {
             validateLength(carName);
-            carNames.add(carName);
+            validateUniqueCarName(carName, uniqueCarNames);
+            cars.add(new Car(carName));
         }
     }
 
     private int parseTryCount(String tryCountInput) {
         try {
-            int parseIntValue = Integer.parseInt(tryCountInput);
-            validateInteger(parseIntValue);
-            return parseIntValue;
+            int parsedTryCount = Integer.parseInt(tryCountInput);
+            validateTryCount(parsedTryCount);
+            return parsedTryCount;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_INVALID_TRY_COUNT);
         }
     }
 
     private void validateCarNamesNotEmpty(String carNamesInput) {
-        if(carNamesInput.isEmpty()) {
+        if (carNamesInput.isEmpty()) {
             throw new IllegalArgumentException(ERROR_EMPTY_CAR_NAME);
         }
     }
@@ -47,17 +55,23 @@ public class InputParser {
         }
     }
 
-    private void validateInteger(int parseIntValue) {
-        if (parseIntValue <= 0) {
-            throw new NumberFormatException();
+    private void validateUniqueCarName(String carName, Set<String> uniqueCarNames) {
+        if (!uniqueCarNames.add(carName)) {
+            throw new IllegalArgumentException(ERROR_SAME_CAR_NAME);
         }
     }
 
-    public List<String> getCarNameList() {
-        return carNames;
+    private void validateTryCount(int parseIntValue) {
+        if (parseIntValue <= 0) {
+            throw new IllegalArgumentException(ERROR_INVALID_TRY_COUNT);
+        }
     }
 
-    public int getTryCount(){
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public int getTryCount() {
         return tryCount;
     }
 }
