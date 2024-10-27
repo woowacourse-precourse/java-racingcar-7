d5352b0e5baa20ff1daf10493be018d7497db10f
Hun0914 <bkh010914@naver.com>
2024-10-27 16:02:34 +0900

    refactor : 게임 클래스 리팩토링
    각각의 클래스에서 받은 값들을 이용하여 게임을 진행하도록 구현함

--- src/main/java/racingcar/Controller/MoveProcessController.java
+++ src/main/java/racingcar/Controller/MoveProcessController.java
@@ -3,17 +3,34 @@ package racingcar.Controller;
 import java.util.List;
 import racingcar.Car;
 import racingcar.service.MoveService;
+import racingcar.view.GameView;
+import racingcar.view.GetInputView;
+import racingcar.view.GetOutputView;
 
 public class MoveProcessController {
 
     private final MoveService moveService = new MoveService();
+    private final GetInputView getInputView = new GetInputView();
+    private final GetOutputView getOutputView = new GetOutputView();
+    private final GameView gameView = new GameView();
+
+    public void raceStart(){
+        List<Car> cars = getInputView.getCars();
+        int count = getInputView.getAttemptCount();
+        moveProcess(count, cars);
+        int maxmimudistance = moveService.checkMaximumMove(cars);
+
+        List<String> winners =moveService.winnerSave(cars, maxmimudistance);
+
+        getOutputView.getOutput(winners);
+    }
+
 
     public void moveProcess(int count, List<Car> cars){
-
         for(int i=0; i<count; i++){
             moveControl(cars);
+            gameView.LiveScore(cars);
         }
-
     }
 
     private void moveControl(List<Car> cars) {
