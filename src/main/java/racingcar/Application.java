a7396cdc1e8771104da93512da01e9822b7717ea
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-10-27 20:29:50 +0900

    feat(Application): 사용자명을 입력받고 이름을 분리하여 배열에 넣어준다
    사용자명을 입력받은 값을 ','를 기준으로 분리한다. 그리고 분리한 값들을 배열에 넣어준다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,37 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    static List<String> participantList = new ArrayList<>();
+
+    public static boolean isValidName(String name) {
+
+        return true;
+    }
+
+    public static void splitParticipantString(String rawString) {
+        for (final String name : rawString.split(",")) {
+            if (isValidName(name))
+                participantList.add(name);
+        }
+
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String participants = Console.readLine();
+            splitParticipantString(participants);
+        } catch (Error e) {
+            throw new IllegalArgumentException("입력이 잘못되었습니다");
+        } finally {
+            Console.close();
+        }
     }
 }
