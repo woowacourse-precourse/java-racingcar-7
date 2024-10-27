9bafc56e31388418733e4ef4f21a43c33758a7ac
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-27 17:38:09 +0900

    feat: 게임 진행 및 실행 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String userInput = InputData.getInputData();
+        ValidateCar.validateInputCar(userInput);
+        String userInputCount = InputCount.getInputCountData();
+        ValidateCount.validateInputCount(userInputCount);
+
+        List<GameCar> doGame = GameCarParsingList.createGameCarList(userInput);
+        int tryCount = ParsingCount.parseInputCountToInt(userInputCount);
+
+        GameCarProcess gameCarProcess = new GameCarProcess(doGame, tryCount);
+        gameCarProcess.gameProgress();
     }
 }
