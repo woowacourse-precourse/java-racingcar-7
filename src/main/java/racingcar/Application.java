3b84acb315efb429ac59737aa062fcdfca6dc6a4
정재민 <jeongjaemin@jeongjaemin-ui-noteubug.local>
2024-10-27 14:28:30 +0900

    feat: 시도 횟수 입력 및 검증 기능 추가
    - 사용자로부터 시도할 횟수를 입력받는 기능 구현
    - 음수 또는 숫자가 아닌 입력에 대해 IllegalArgumentException 발생 처리
    - 유효한 숫자 입력 시 시도 횟수를 저장하는 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,20 +6,37 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
         String nameInput = Console.readLine();
-        String [] cars = nameInput.split(",");
+        String[] cars = nameInput.split(",");
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String attemptInt = (Console.readLine());
 
         // 각 자동차 이름이 5자 이하인지 검증
         ValidateCarNames(cars);
+
+        // 시도 횟수 검증
+        int attemptCount = ValidateAttemptCount(attemptInt);
+
+
     }
 
-
-    private static void ValidateCarNames(String [] carNames){
-        for(String carName : carNames){
+    private static void ValidateCarNames(String[] carNames) {
+        for (String carName : carNames) {
             carName = carName.trim(); // 공백 제거
-
-            if (carName.length() > 5 || carName.isEmpty()){
+            if (carName.length() > 5 || carName.isEmpty()) {
                 throw new IllegalArgumentException();
             }
+        }
+    }
+
+    private static int ValidateAttemptCount(String attemptInput){
+        try{
+            int attemptCount = Integer.parseInt(attemptInput);
+            if (attemptCount <= 0){
+                throw new IllegalArgumentException();
+            }
+            return attemptCount;
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException();
         }
     }
 }
