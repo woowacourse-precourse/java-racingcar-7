116810c7f1cdf02d5267df3be7cd8a42d63ad3ff
jaepyo99 <jaepyo99@naver.com>
2024-10-27 15:30:46 +0900

    feat: Vehicle 추상클래스 행동 추가 구현과 Car의 오버라이드

--- src/main/java/racingcar/model/domain/Vehicle.java
+++ src/main/java/racingcar/model/domain/Vehicle.java
@@ -1,7 +1,26 @@
 package racingcar.model.domain;
 
 public abstract class Vehicle {
+    private String name;
+    private int distance = 0;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
     public abstract void move();
 
     protected abstract int play();
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    protected void incrementDistance(int increment) {
+        distance++;
+    }
 }
---
+++ src/main/java/racingcar/model/infrastructure/Car.java
@@ -0,0 +1,28 @@
+package racingcar.model.infrastructure;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.model.domain.Decider;
+import racingcar.model.domain.Vehicle;
+
+public class Car extends Vehicle {
+    private final Decider moveDecider;
+    private final static Integer MOVE_DISTANCE = 1;
+
+    public Car(String name, Decider moveDecider) {
+        super(name);
+        this.moveDecider = moveDecider;
+    }
+
+    @Override
+    public void move() {
+        int playNum = play();
+        if (moveDecider.canMove(playNum)) {
+            incrementDistance(MOVE_DISTANCE);
+        }
+    }
+
+    @Override
+    protected int play() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+}
