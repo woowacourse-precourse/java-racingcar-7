41fa66c056cde0848df28aa26efc721b35c458e9
yereumi <lovelyyeji01@gmail.com>
2024-10-27 22:25:30 +0900

    feat(validator):  errorMessage 변수 삭제

--- src/main/java/racingcar/validator/CarValidator.java
+++ src/main/java/racingcar/validator/CarValidator.java
@@ -5,23 +5,17 @@ import racingcar.message.Message;
 
 public class CarValidator {
 
-    private String errorMessage;
-
     public void validateCarName(String car) {
 
-        errorMessage = String.format(Message.ERROR_INVALID_CAR_NAME.getMessage());
-
         if (car.length() > 5 || car.isEmpty()) {
-            throw new IllegalArgumentException(errorMessage);
+            throw new IllegalArgumentException(Message.ERROR_INVALID_CAR_NAME.getMessage());
         }
     }
 
     public void checkDuplicateCarName(HashMap<String, Integer> result, String car) {
 
-        errorMessage = String.format(Message.ERROR_DUPLICATE_CAR_NAME.getMessage());
-
         if (result.containsKey(car)) {
-            throw new IllegalArgumentException(errorMessage);
+            throw new IllegalArgumentException(Message.ERROR_DUPLICATE_CAR_NAME.getMessage());
         }
     }
 }
