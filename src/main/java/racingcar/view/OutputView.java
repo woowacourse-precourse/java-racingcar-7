88a8bf98676ca18b6bd51279a0b54255a549ca12
sihyun10 <hyhy200170@gmail.com>
2024-10-28 01:31:35 +0900

    feat(결과 출력) : 게임 진행상태, 최종 우승자 출력한다.
    OutputView에서 게임 진행 상태와 최종 우승자를 출력해줄 수 있도록
    View화면을 구현하였다.

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.Car;
+
+public class OutputView {
+    public static void printRoundResult(List<Car> cars) {
+        System.out.println("실행 결과");
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getLocation()));
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+}
