efb52adec1f4a4444ef0cc6077866ec36b965ab4
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-10-28 06:47:52 +0900

    feat(RacingGame): 자동차 생성 기능 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -9,10 +10,21 @@ public class RacingGame {
     private final AttemptCountParser attemptCountParser;
     private int attemptCount;
 
+    // 경주에 참가한 자동차 리스트
+    private final List<Car> cars = new ArrayList<>();
 
     public RacingGame(CarNameParser carNameParser, AttemptCountParser attemptCountParser){
         this.carNameParser = carNameParser;
         this.attemptCountParser = attemptCountParser;
+    }
+
+    /**
+     * 자동차 객체들을 생성하고 자동차 리스트에 추가하는 객체
+     * */
+    public void addCars(List<String> validatedCarNames){
+        for(String carName : validatedCarNames){
+            cars.add(Car.createCar(carName));
+        }
     }
 
 
@@ -31,5 +43,9 @@ public class RacingGame {
         String[] splitCarName = carNameParser.splitCarNamesByComma(input);
 
         return carNameParser.getValidatedCarNames(splitCarName);
+    }
+
+    public List<Car> getCars() {
+        return cars;
     }
 }
