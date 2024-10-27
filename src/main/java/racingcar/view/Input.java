6c3aff6e46597e78b012e9a46d297e04a40fbfc3
Lucerna00 <pjs3859811@naver.com>
2024-10-27 17:11:24 +0900

    feat(Input): specify maximum count

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -4,13 +4,19 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
+    private static final int MAXIMUM_COUNT = 10;
+
     public String readNames() {
         return Console.readLine();
     }
 
     public int readCount() {
         try {
-            return Integer.parseInt(Console.readLine());
+            int count = Integer.parseInt(Console.readLine());
+            if (count > MAXIMUM_COUNT) {
+                throw new IllegalArgumentException();
+            }
+            return count;
         } catch (Exception e) {
             throw new IllegalArgumentException();
         }
