1495c24c7f1f0b10d3c9715c48fe1dfb5515d8bb
송지웅 <thdwldnd7@dgu.ac.kr>
2024-10-28 01:57:53 +0900

    feat: 자동차 게임 객체 추가

---
+++ src/main/java/racingcar/CarGame.java
@@ -0,0 +1,55 @@
+package racingcar;
+
+import racingcar.decider.Decider;
+
+import java.util.List;
+
+public class CarGame {
+    private final int numberOfTurn;
+    private List<Car> entryCars;
+    private Decider decider;
+
+    public CarGame(int numberOfTurn, List<Car> entryCars, Decider decider) {
+        this.numberOfTurn = numberOfTurn;
+        this.entryCars = entryCars;
+        this.decider = decider;
+    }
+    private void moveCars() {
+        for (Car car: entryCars) {
+            boolean move = decider.decideMoveOrNot();
+            if (move) {
+                car.forward();
+            }
+        }
+    }
+
+    private void printCars() {
+        for (Car car: entryCars) {
+            System.out.printf("%s : ", car.getName());
+            for (int i = 0; i < car.getPosition(); i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private List<String> getWinners() {
+        int maxPosition = entryCars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+        return entryCars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .toList();
+    }
+
+    public List<String> play() {
+        for (int i = 0 ; i < numberOfTurn; i++) {
+            moveCars();
+            printCars();
+        }
+        return getWinners();
+    }
+}
