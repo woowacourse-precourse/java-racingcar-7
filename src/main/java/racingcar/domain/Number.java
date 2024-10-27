54d935da13e8b65e773b24e0a60a41ab505909c1
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-10-27 18:28:45 +0900

    refactor: 매직넘버 상수로 관리

--- src/main/java/racingcar/domain/Number.java
+++ src/main/java/racingcar/domain/Number.java
@@ -3,11 +3,14 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Number {
+    private int MAX_RANDOM_NUMBER_RANGE = 9;
+
     public Number() {
 
     }
 
     public int getRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        int num = Randoms.pickNumberInRange(0, MAX_RANDOM_NUMBER_RANGE);
+        return num;
     }
 }
