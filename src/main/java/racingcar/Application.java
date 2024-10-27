fc254d65e07bdb4daf88b8bde9e54703a918b5b2
Silbet <65993038+Silbet@users.noreply.github.com>
2024-10-27 15:53:58 +0900

    Feat: 입력값 다듬기
    첫 번째 입력 String을 이름 단위로 나눠 배열에 저장한다.
    차수별 실행 결과를 기록하기 위해 차의 개수 만큼 배열 값을 0으로 초기화 한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static void race(final String text) {
+        String[] name = text.split(",");
+        // 이름은 5자 이하만 가능
+        final int numberOfCar = name.length;
+        ArrayList<Integer> racingDistance = new ArrayList<>(numberOfCar);
+
+        for (int i = 0; i < numberOfCar; i++) {
+            racingDistance.add(0);
+        }
     }
 }
