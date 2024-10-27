3e693ee6b3bde2851335a0253d9839e30b6452d8
yunjeooong <yunjeongssu@gmail.com>
2024-10-27 21:17:58 +0900

    refactor: 이름 검증 관련 코드 상수화 적용

--- src/main/java/racingcar/util/validator/CarNameValidator.java
+++ src/main/java/racingcar/util/validator/CarNameValidator.java
@@ -4,25 +4,27 @@ import java.util.Arrays;
 import java.util.List;
 import racingcar.util.ExceptionMessage;
 
-public class CarNameValidator extends Validator{
+public class CarNameValidator extends Validator {
+    private static final int MIN_CARS_COUNT = 2;
+    private static final int MAX_NAME_LENGTH = 5;
 
     @Override
     public void validate(String input) throws IllegalArgumentException {
         List<String> carNames = Arrays.asList(removeSpace(input).split(","));
-       validateNumberofCars(carNames);
-       for (String carName : carNames) {
-           validateCarNameLength(carName);
-       }
+        validateNumberOfCars(carNames);
+        for (String carName : carNames) {
+            validateCarNameLength(carName);
+        }
     }
 
     private static void validateCarNameLength(String carName) {
-        if(carName.length() >=6) {
+        if (carName.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_CAR_NAME_LENGTH.getMessage());
         }
     }
 
-    private static void validateNumberofCars(List<String> carNames) {
-        if(carNames.size() < 2) {
+    private static void validateNumberOfCars(List<String> carNames) {
+        if (carNames.size() < MIN_CARS_COUNT) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_CARS_COUNT.getMessage());
         }
     }
