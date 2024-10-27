8cdff0c3b552197d200b6b815a58f827fcf497c4
hekim52 <acrossnightsky@gmail.com>
2024-10-28 01:18:31 +0900

    feat: 이름 5자 이하, Exception 발생
    자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
    사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,13 +11,24 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = readLine();
+        String[] carList = carName.split(",");
+        int carListLength = carList.length;
+        for (int q = 0; q < carListLength; q++) {
+            if (carList[q].length() > 5) {
+                throw new IllegalArgumentException("잘못된 값이 입력되었습니다.");
+            }
+        }
+
         System.out.println("시도할 횟수는 몇 회인가요?");
         String countStr = readLine();
+        for (int p = 0; p < countStr.length(); p++) {
+            if (!Character.isDigit(countStr.charAt(p))) {
+                throw new IllegalArgumentException("잘못된 값이 입력되었습니다.");
+            }
+        }
         int count = Integer.parseInt(countStr);
         System.out.println();
 
-        String[] carlist = carName.split(",");
-        int carListLength = carlist.length;
         int[] moveList = new int[carListLength];
 
         System.out.println("실행 결과");
@@ -29,7 +40,7 @@ public class Application {
                 }
             }
             for (int k = 0; k < carListLength; k++) {
-                System.out.print(carlist[k] + " : ");
+                System.out.print(carList[k] + " : ");
                 for (int l = 0; l < moveList[k]; l++) {
                     System.out.print("-");
                 }
@@ -49,7 +60,7 @@ public class Application {
         ArrayList<String> list = new ArrayList<>();
         for (int n = 0; n < carListLength; n++) {
             if (moveList[n] == maxMove) {
-                list.add(carlist[n]);
+                list.add(carList[n]);
             }
         }
 
