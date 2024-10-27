8831bbeb84e7b76b568f250671bc24300336dc1a
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-10-27 12:01:54 +0900

    feat: 시도 횟수만큼 게임반복 하는 로직 추가

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -5,30 +5,41 @@ import racingcar.domain.Number;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class RacingCarGame {
-    private InputView inputView;
-    private OutputView outputView;
-    private Number number;
-    private List<String> carName;
-    private List<Car> carList;
-    private String inputString;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Number number;
+    private final List<Car> carList;
 
     public RacingCarGame(InputView inputView, OutputView outputView, Number number) { //의존성 주입 다시 공부하기
         this.inputView = inputView;
         this.outputView = outputView;
         this.number = number;
+        this.carList = new ArrayList<>();
     }
 
     public void startRacingGame() {
         outputView.printStartMessage();
-        inputString = inputView.getInput();
-        carName = Arrays.asList(inputString.split(","));
+        String inputString = inputView.getInput();
+        outputView.printTryCountMessage();
+        int tryCount = Integer.parseInt(inputView.getInput());
+
+        List<String> carName = Arrays.asList(inputString.split(","));
         for (String name : carName) {
             carList.add(new Car(name));
         }
+
+        outputView.printResultMessage();
+        while (tryCount > 0) {
+            playOneRound();
+            tryCount--;
+        }
+        
+        outputView.printWinner();
     }
 
     private void playOneRound() {
@@ -36,10 +47,9 @@ public class RacingCarGame {
             car.setDistance(number.getRandomNumber());
         }
 
-        for (Car car:carList){
+        for (Car car : carList) {
             outputView.printCarDistance(car.getName(), car.getDistance());
         }
-
+        System.out.println();
     }
-
 }
