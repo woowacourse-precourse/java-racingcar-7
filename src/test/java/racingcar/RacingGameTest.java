32af7806bc26d60bad0523ec199f926f5c807515
Seol-Munhyeok <abc1234568@naver.com>
2024-10-27 20:56:43 +0900

    test(RacingGame): Add playGameTest()

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,5 +1,25 @@
 package racingcar;
 
-public class RacingGameTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingGameTest {
+    @Test
+    @DisplayName("playGame() 메서드를 테스트한다")
+    void playGameTest() {
+        List<Car> cars = Arrays.asList(
+                new Car("pobi"),
+                new Car("woni"),
+                new Car("jun")
+        );
+        int moveCount = 5;
+        RacingGame racingGame = new RacingGame(cars, moveCount);
+        racingGame.playGame();
+    }
 }
