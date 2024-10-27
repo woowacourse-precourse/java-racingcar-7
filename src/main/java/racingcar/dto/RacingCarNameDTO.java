4a3f38072b69422aa3cc8ca93622c329b4120aa6
choiyoung69 <choiyoung609@gmail.com>
2024-10-28 04:05:40 +0900

    feat(RacingCarNameDTO): 사용자로부터 입력된 자동차 이름 목록 문자열이 공백인 경우 처리

--- src/main/java/racingcar/dto/RacingCarNameDTO.java
+++ src/main/java/racingcar/dto/RacingCarNameDTO.java
@@ -1,1 +1,26 @@
+package racingcar.dto;
 
+import java.util.Arrays;
+import java.util.List;
+
+public class RacingCarNameDTO {
+    private List<String> carNames;
+
+    public RacingCarNameDTO(String carNamesInput){
+        validateString(carNamesInput);
+        this.carNames = Arrays.asList(carNamesInput.split(","));
+    }
+
+    private void validateString(String carNamesInput){
+        if(carNamesInput == null || carNamesInput.trim().isEmpty())
+            throw new IllegalArgumentException("자동차 이름을 입력해주세요");
+    }
+
+    private void validateCarNames(){
+
+    }
+
+    private void checkEmptyCarNames(){
+
+    }
+}
