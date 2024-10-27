74d492ccdf8c9fd6ab2e6665a68c843342cc5359
Hun0914 <bkh010914@naver.com>
2024-10-27 16:04:35 +0900

    refactor : 메인 함수 간소화
    분리한 클래스를 이용하여 메인 함수를 간소화하였습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,73 +3,14 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import racingcar.Controller.MoveProcessController;
+import racingcar.service.MoveService;
 
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
-        int maximum=0;
-        int checkingnum=0;
-        List<String> winnerList = new ArrayList<>();
-
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String nameset= Console.readLine();
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int count= Integer.parseInt(Console.readLine());
-
-        String[] checkedname = nameset.split(",");
-        for (String name : checkedname) {
-            if(name.length()>5){
-                throw new IllegalArgumentException();
-            }
-        }
-        //List<String> name= Arrays.asList(nameset.split(","));
-        List<Car> cars= new ArrayList<>();
-
-        for (String s : checkedname) {
-            cars.add(new Car(s.trim(),0,""));
-        }
-
-        System.out.println("실행 결과");
-        for(int i=0; i<count; i++) {
-
-            for (Car car : cars) {
-                if(Randoms.pickNumberInRange(0, 9)>=4){
-                    car.length++;
-                    car.distance+="-";
-                }
-                System.out.println(car);
-            }
-            System.out.println();
-        }
-
-        for (Car car : cars) {
-            if(car.length>maximum){
-                maximum=car.length;
-            }
-        }
-
-        System.out.print("최종 우승자 : ");
-        for(int i=0; i<cars.size(); i++) {
-
-            if (maximum == cars.get(i).length) {
-                winnerList.add(cars.get(i).name);
-            }
-        }
-
-        if(winnerList.size()>1){
-            for(int i=0; i<winnerList.size(); i++){
-                System.out.print(winnerList.get(i));
-                if(i<winnerList.size()-1){
-                    System.out.print(", ");
-                }
-            }
-        }
-        if(winnerList.size()==1){
-            System.out.print(winnerList.get(0));
-        }
+        MoveProcessController controller = new MoveProcessController();
+        controller.raceStart();
     }
 }
