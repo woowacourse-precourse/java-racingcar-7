c7de8e9fae725cd1e13a8f9983a37e5717be5de1
minSsan <msp1125@hanyang.ac.kr>
2024-10-27 15:38:25 +0900

    feat(Application): Application 클래스 main 메서드 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import racingcar.adapter.inbound.cli.InputAdapter;
+import racingcar.adapter.outbound.cli.OutputAdapter;
+import racingcar.application.port.inbound.GameUseCase;
+import racingcar.application.service.CliInputPortImpl;
+import racingcar.application.service.GameService;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameUseCase gameUseCase = new GameService(new CliInputPortImpl(), new OutputAdapter());
+        InputAdapter inputAdapter = new InputAdapter(gameUseCase);
+        inputAdapter.run();
     }
 }
