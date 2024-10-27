0a9df2e86b02ce9d7d1f6c7e7288fa266594af0c
Park, Sojeong <sojeong2101@gmail.com>
2024-10-28 02:20:00 +0900

    test($RefereeTest): 우승자 판단 테스트 구현
    1. 단일 우승자 판단 테스트
    2. 공동 우승자 판단 테스트

--- src/test/java/racingcar/RefereeTest.java
+++ src/test/java/racingcar/RefereeTest.java
@@ -1,9 +1,13 @@
 package racingcar;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class RefereeTest {
+
 
     @Test
     void 라운드_입력이_Null인지_검사() {
@@ -43,5 +47,43 @@ class RefereeTest {
                     new Referee(round);
                 }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("라운드 입력은 숫자만 가능합니다.");
+    }
+
+    @Test
+    void 단일_우승자_판단() {
+        // give
+        Referee referee = new Referee("3");
+
+        Map<String, Integer> carsDecision = new LinkedHashMap<>();
+        carsDecision.put("소정", 3);
+        carsDecision.put("pobi", 2);
+        carsDecision.put("woni", 1);
+
+        List<String> expectedWinners = List.of("소정");
+
+        // when
+        List<String> actualWinner = referee.judgmentWinner(carsDecision);
+
+        // then
+        Assertions.assertThat(actualWinner).isEqualTo(expectedWinners);
+    }
+
+    @Test
+    void 공동_우승자_판단() {
+        // give
+        Referee referee = new Referee("3");
+
+        Map<String, Integer> carsDecision = new LinkedHashMap<>();
+        carsDecision.put("소정", 3);
+        carsDecision.put("pobi", 3);
+        carsDecision.put("woni", 2);
+
+        List<String> expectedWinners = List.of("소정", "pobi");
+
+        // when
+        List<String> actualWinner = referee.judgmentWinner(carsDecision);
+
+        // then
+        Assertions.assertThat(actualWinner).isEqualTo(expectedWinners);
     }
 }
