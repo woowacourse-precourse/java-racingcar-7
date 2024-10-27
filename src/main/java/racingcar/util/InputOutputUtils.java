96a99d980f6b50923ed77cd38893d4dcab776292
enble <enble_777@naver.com>
2024-10-27 15:01:39 +0900

    feat: 사용자 입출력 담당 클래스 구현

---
+++ src/main/java/racingcar/util/InputOutputUtils.java
@@ -0,0 +1,25 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class InputOutputUtils {
+
+    private final InputParser inputParser;
+
+    public InputOutputUtils() {
+        this.inputParser = new InputParser();
+    }
+
+    public List<String> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        return inputParser.parseCars(input);
+    }
+
+    public int getRound() {
+        System.out.println("시도할 회수는 몇 회인가요?");
+        String input = Console.readLine();
+        return inputParser.parseRound(input);
+    }
+}
