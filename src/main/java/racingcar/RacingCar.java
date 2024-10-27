36d25c5ab5a28fb51d4c61b326d111f3167ff848
skadjs <skadjs11@gmail.com>
2024-10-28 00:37:40 +0900

    feat(RacingCar): 자동차 이름 추출 기능 추가
    공백을 제거한 문자열에서 ","를 기준으로 자동차 이름을 구분해 리스트에 담는 extractCarNames() 함수 추가

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,9 +1,13 @@
 package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import java.util.Arrays;
+import java.util.List;
 
 public class RacingCar {
     String carNames;
+    List<String> carNameList;
+    int carNamesSize;
 
     public void getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -14,5 +18,8 @@ public class RacingCar {
         this.carNames = carNames.replaceAll(" ", "");
     }
 
-
+    public void extractCarNames() {
+        this.carNameList = Arrays.asList(carNames.split(","));
+        carNamesSize = carNameList.size();
+    }
 }
