e312e90dd6e4486b4e8836aca69e62f05013df3e
김세희 <sehee0954@gmail.com>
2024-10-27 22:33:12 +0900

    fix($): 파일 구조 오류 수정
    $ 이전에 생성했던 전체 파일 디렉토리의 위치를 test에 잘못올려서 main으로 옮겼습니다.

--- src/main/java/racingcar/Service/RacingGameService.java
+++ src/main/java/racingcar/Service/RacingGameService.java
@@ -1,16 +1,16 @@
-package racingcar.Entity;
+package racingcar.Service;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.Entity.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class RacingGame {
+public class RacingGameService {
     private final List<Car> cars;
     private final int attemptCount;
 
-    public RacingGame(List<String> carNames, int attemptCount) {// Car 객체 생성, 시도횟수 초기화
+    public RacingGameService(List<String> carNames, int attemptCount) {// Car 객체 생성, 시도횟수 초기화
         this.cars = carNames.stream()
                 .map(Car::new)
                 .collect(Collectors.toList());
