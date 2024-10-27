a98fc069912ced0947692aa350193fe06aad9d3f
MinJae00 <exceedmin@gmail.com>
2024-10-27 20:47:53 +0900

    refact: 내부 라이브러리를 활용하여 우승자 선별 로직 단순화

--- src/main/java/racingcar/operator/WinnerSelector.java
+++ src/main/java/racingcar/operator/WinnerSelector.java
@@ -1,42 +1,26 @@
 package racingcar.operator;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.user.SettingGameUser;
 import racingcar.user.UserObject;
-import racingcar.view.OutputView;
 
 public class WinnerSelector {
-    List<UserObject> topScores = new ArrayList<>();
-    OutputView output = new OutputView();
-    int maxScore = Integer.MIN_VALUE;
+    UserObject[] user;
 
-    public void selectWinner(UserObject[] users) {
-
-        for (UserObject user : users) {
-            if (user.scoreNum > maxScore) {
-                maxScore = user.scoreNum;
-            }
-        }
-
-        for (UserObject user : users) {
-            if (user.scoreNum == maxScore) {
-                topScores.add(user);
-            }
-        }
-        extractWinner();
+    public List<String> getTopScores() {
+        int maxScore = Arrays.stream(user)
+                .mapToInt(UserObject::getUserScore)
+                .max()
+                .orElse(Integer.MIN_VALUE);
+        return Arrays.stream(user)
+                .filter(u -> u.getUserScore() == maxScore)
+                .map(UserObject::getUserName)
+                .collect(Collectors.toList());
     }
 
-    private void extractWinner() {
-        output.printWinnerSentence();
-        int n = topScores.size();
-        for (UserObject user : topScores) {
-            n--;
-            output.printWinner(user.name);
-            if (n != 0) {
-                output.printWinner(user.name);
-                break;
-            }
-            output.printRest();
-        }
+    public WinnerSelector() {
+        this.user = SettingGameUser.user;
     }
 }
