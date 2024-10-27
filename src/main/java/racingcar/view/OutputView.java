94e924b0f8e12f0a725b8cb4ba93515020bcb9e8
Kyuwon-Choi <kyuwon30@naver.com>
2024-10-27 20:41:53 +0900

    feat: OutputView 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,24 @@
 package racingcar.view;
 
+import racingcar.domain.Name;
+
+import java.util.List;
+
 public class OutputView {
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String DASH = "-";
+
+    public static void startPrintResult() {
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public static void printResult(Name name, int distance) {
+        System.out.println(name + " : " + DASH.repeat(distance));
+    }
+
+    public static void printWinners(List<Name> winners) {
+        System.out.println(WINNER_MESSAGE + winners);
+    }
+
 }
