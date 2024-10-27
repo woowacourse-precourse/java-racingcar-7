56bcd64e53fca0318de80a86ec4c07927f99f32d
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:26:57 +0900

    :sparkles: feat(Name): 이름이 빈 문자열일 경우, 예외를 발생시킨다

--- src/main/java/racingcar/model/Name.java
+++ src/main/java/racingcar/model/Name.java
@@ -1,14 +1,29 @@
 package racingcar.model;
 
+import static racingcar.message.ErrorMessages.CLEAR_NAME;
+
+import racingcar.exception.InvalidInputException;
+
 public class Name {
     private final String name;
 
     public Name(String name) {
+        validate(name);
         this.name = name;
     }
 
     @Override
     public String toString() {
         return this.name;
+    }
+
+    private void validate(String name) {
+        validateClearName(name);
+    }
+
+    private void validateClearName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new InvalidInputException(CLEAR_NAME);
+        }
     }
 }
