ff24745460e4d6abacc63dc293116d2b050be28e
PSW99 <steamdayz@naver.com>
2024-10-27 22:12:25 +0900

    refactor(view): remove static import of InputValidator methods in InputView
    - Replaced static import of InputValidator methods with explicit class references
    - Improved readability by clarifying method origins in InputView

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,35 +6,34 @@ import racingcar.domain.Car;
 import java.util.ArrayList;
 import java.util.List;
 
-import static racingcar.view.InputValidator.*;
 
 public class InputView {
     private static final String CAR_NAME_DELIMITER = ",";
 
     public List<Car> readCarNames() {
         String inputCarNames = Console.readLine();
-        validateEmptyInput(inputCarNames);
+        InputValidator.validateEmptyInput(inputCarNames);
         String[] carsArray = inputCarNames.split(CAR_NAME_DELIMITER);
 
         validateAllCarNames(carsArray);
 
         List<Car> carList = convertToCarList(carsArray);
 
-        validateCarCounts(carList);
+        InputValidator.validateCarCounts(carList);
 
         return carList;
     }
 
     public int inputAttemptCount() {
         String inputAttemptCount = Console.readLine();
-        validateAttemptCountIsNumber(inputAttemptCount);
+        InputValidator.validateAttemptCountIsNumber(inputAttemptCount);
 
         return Integer.parseInt(inputAttemptCount);
     }
 
     private void validateAllCarNames(String[] carsArray) {
         for (String car : carsArray) {
-            validateCarNameLength(car);
+            InputValidator.validateCarNameLength(car);
         }
     }
 
@@ -43,7 +42,7 @@ public class InputView {
 
         for (String carName : cars) {
             Car targetCar = new Car(carName);
-            validateDuplicateCarName(carList, targetCar);
+            InputValidator.validateDuplicateCarName(carList, targetCar);
             carList.add(targetCar);
         }
 
