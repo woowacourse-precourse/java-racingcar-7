5994179736eec1916719ff4886358704a3be0c93
junhyunpark <junhyunpark305@gmail.com>
2024-10-27 19:44:35 +0900

    feat: create InputValidator, RandomValidator
    - InputValidator에서 구분자 체크 및 자동자 이름 확인
    - 시도횟수 확인
    - 랜덤 값 유효 확인

---
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -0,0 +1,51 @@
+package racingcar.validator;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+// 완료
+public class InputValidator {
+  private static final String DELIMITER = ",";
+
+  public List<String> checkCarNames(String carNames) {
+
+    List<String> carNameList;
+    try {
+      carNameList = Stream.of(carNames.split(DELIMITER))
+              .map(String::trim)
+              .toList();
+    } catch (IllegalArgumentException e) {
+      throw new IllegalArgumentException("유효하지 않은 구분자입니다.", e);
+    }
+    for (String carName : carNameList) {
+      if (carName.trim().isBlank()) {
+        throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+      }
+    }
+    for (String carName : carNameList) {
+      if (carName.length() > 5) {
+        throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+      }
+    }
+
+    return carNameList;
+  }
+
+  public int isValidAtmp(String numberOfAttempts) {
+
+    int parseAtmp;
+
+    try {
+      parseAtmp = Integer.parseInt(numberOfAttempts);
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("유효한 시도 횟수가 아닙니다", e);
+    }
+
+    if (parseAtmp < 1 || parseAtmp > 99) {
+      throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+    }
+    return parseAtmp;
+  }
+}
+
+
