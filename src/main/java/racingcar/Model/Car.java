db9e0a9b98aa2a1796cc3d9261ad7d596e179171
jongseo <jongseo0703@naver.com>
2024-10-27 23:55:47 +0900

    feat: 전진하는 기능 추가

--- src/main/java/racingcar/Model/Car.java
+++ src/main/java/racingcar/Model/Car.java
@@ -2,14 +2,25 @@ package racingcar.Model;
 
 public class Car {
     private String carName;
+    private int distance = 0;
 
     public Car(String carName) {
         lengthValidation(carName);
         this.carName = carName;
     }
 
+    public void run(int number) {
+        if (isMove(number)) {
+            this.distance++;
+        }
+    }
+
     public String getCarName() {
         return carName;
+    }
+
+    public int getDistance() {
+        return distance;
     }
 
     private void lengthValidation(String carName) {
@@ -19,5 +30,9 @@ public class Car {
         else if (carName.length() < Rule.MIN_LENGTH.value() || carName.length() > Rule.MAX_LENGTH.value()) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private boolean isMove(int number) {
+        return number >= Rule.THRESHOLD.value();
     }
 }
