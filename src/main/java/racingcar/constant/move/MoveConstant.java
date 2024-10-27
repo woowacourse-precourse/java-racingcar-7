49bf60330b3544454ef175f9774e47bcd1453765
EHCTH <lees90896@gmail.com>
2024-10-27 11:51:13 +0900

    feat(MoveConstant): MoveConstant static

---
+++ src/main/java/racingcar/constant/move/MoveConstant.java
@@ -0,0 +1,7 @@
+package racingcar.constant.move;
+
+public class MoveConstant {
+    public static final int MOVE_THRESHOLD = 4;
+    public static final int MIN_MOVE_RANGE = 0;
+    public static final int MAX_MOVE_RANGE = 9;
+}
