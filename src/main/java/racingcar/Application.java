97ebc15f68de0efb9acc6a496787259c663fb082
angrychimpanzee <136212007+angrychimpanzee@users.noreply.github.com>
2024-10-28 05:52:40 +0900

    feat : 자동차 이름입력 기능 추가
    자동차의 이름을 쉼표로 구분하여 입력받고, 5글자 이상이라면 예외처리 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,41 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            List<Car> cars = initializeCars();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static List<Car> initializeCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
+
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            if (name.trim().length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+            cars.add(new Car(name.trim()));
+        }
+        return cars;
+    }
+}
+
+class Car {
+    private final String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
