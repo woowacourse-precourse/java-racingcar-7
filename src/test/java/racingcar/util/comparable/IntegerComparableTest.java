578bce07b702dbc742f03356cab48ac39cc362ae
Mint <audwl03071@gmail.com>
2024-10-27 19:54:01 +0900

    test: util 패키지 테스트 작성

---
+++ src/test/java/racingcar/util/comparable/IntegerComparableTest.java
@@ -0,0 +1,19 @@
+package racingcar.util.comparable;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("정수 비교 테스트")
+class IntegerComparableTest {
+    @Test
+    @DisplayName("두 정수를 비교한다")
+    void 성공_비교() {
+        // Given
+        IntegerComparable numberComparable = new IntegerComparable();
+
+        // When & Then
+        assertThat(numberComparable.compare(2, 1)).isEqualTo(1);
+    }
+}
---
+++ src/test/java/racingcar/util/random/RandomIntegerGeneratorTest.java
@@ -0,0 +1,26 @@
+package racingcar.util.random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("랜덤 정수 생성 테스트")
+class RandomIntegerGeneratorTest {
+
+    @Test
+    @DisplayName("정해진 범위 내에서 랜덤값을 생성한다")
+    void 성공_랜덤값생성() {
+        // Given
+        int startInclusive = 0;
+        int endInclusive = 9;
+        RandomIntegerGenerator randomNumberGenerator = new RandomIntegerGenerator(startInclusive, endInclusive);
+
+        // When
+        Integer number = randomNumberGenerator.pickNumber();
+
+        // Then
+        assertThat(number).isGreaterThanOrEqualTo(0)
+                .isLessThanOrEqualTo(9);
+    }
+}
