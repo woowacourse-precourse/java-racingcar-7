5d52daba742719911f5895435c78bbdf0aade0cc
JoonKyo <joonkyolee@gmail.com>
2024-10-27 11:15:38 +0900

    feat(AttemptNumber): 시도 횟수가 음수인 경우를 처리하는 메서드 구현

--- src/main/java/racingcar/domain/AttemptNumber.java
+++ src/main/java/racingcar/domain/AttemptNumber.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 public class AttemptNumber {
     private static final String ERROR_MESSAGE_FOR_NOT_INTEGER = "숫자가 아닌 문자를 입력했습니다. 프로그램을 종료합니다.";
+    private static final String ERROR_MESSAGE_FOR_NEGATIVE_NUMBER = "음수를 입력했습니다. 프로그램을 종료합니다.";
     private final int attemptNumber;
 
     private AttemptNumber(int attemptNumber) {
@@ -9,8 +10,8 @@ public class AttemptNumber {
     }
 
     public static AttemptNumber from(String input) {
-        int tryNumber = Integer.parseInt(input);
-        return new AttemptNumber(tryNumber);
+        int attemptNumber = Integer.parseInt(input);
+        return new AttemptNumber(attemptNumber);
     }
 
     private static int StringToInt(String input) {
@@ -18,6 +19,12 @@ public class AttemptNumber {
             return Integer.parseInt(input);
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException(ERROR_MESSAGE_FOR_NOT_INTEGER);
+        }
+    }
+
+    private static void validateNegativeNumber(int attemptNumber) {
+        if (attemptNumber < 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_NEGATIVE_NUMBER);
         }
     }
 }
