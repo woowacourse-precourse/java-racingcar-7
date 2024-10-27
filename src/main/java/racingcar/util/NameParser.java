712c6b7c4995f670ef4f538f5f71f11a71695d9b
kimdev <kim__dev@hufs.ac.kr>
2024-10-27 18:54:23 +0900

    refactor(NameParser): 빈 이름 검증 로직을 분리하라

--- src/main/java/racingcar/util/NameParser.java
+++ src/main/java/racingcar/util/NameParser.java
@@ -5,12 +5,21 @@ import java.util.Collections;
 
 public class NameParser {
     public static ArrayList<String> parse(String names) {
-        if (names == null || names.isBlank()) {
-            throw new IllegalArgumentException("이름을 입력해 주세요.");
-        }
-
         ArrayList<String> nameList = new ArrayList<>();
         Collections.addAll(nameList, names.split(","));
+        validName(nameList);
         return nameList;
+    }
+
+    public static void validName(ArrayList<String> nameList) {
+        if (nameList.size() == 0) {
+            throw new IllegalArgumentException("빈 이름은 허용되지 않습니다.");
+        }
+
+        nameList.stream()
+                .filter(String::isBlank)
+                .forEach(name -> {
+                    throw new IllegalArgumentException("빈 이름은 허용되지 않습니다.");
+                });
     }
 }
