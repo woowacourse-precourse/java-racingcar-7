5d0453a4262badacd6dcced6336c01fa89fc4fde
Zrfv <youngoh07015@gmail.com>
2024-10-27 20:15:03 +0900

    style: 미사용 import 제거

--- README.md
+++ README.md
@@ -49,3 +49,4 @@
 - 중복된 코드 제거
 - print 기능은 OutPutView 클래스로 이동
 - GameTryCount 클래스는 객체를 만들 필요가 없기 때문에 private 생성자 선언
+- 미사용 import 제거
--- src/test/java/racingcar/model/WinnerTest.java
+++ src/test/java/racingcar/model/WinnerTest.java
@@ -3,7 +3,6 @@ package racingcar.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class WinnerTest {
