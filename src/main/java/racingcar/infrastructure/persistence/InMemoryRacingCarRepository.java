81e0e0cd91dad1b7f5fa864fe899a986717cd6ef
jijongkwon <help3451@naver.com>
2024-10-28 00:56:45 +0900

    feat(infrastructure) : InMemoryRacingCarRepository 싱글톤 패턴 변경 및 메서드 구현
    - saveRacingCar : racing car 저장
    - getRacingCars : racing cars 반환
    - clear : racing car 초기화

--- src/main/java/racingcar/infrastructure/persistence/InMemoryRacingCarRepository.java
+++ src/main/java/racingcar/infrastructure/persistence/InMemoryRacingCarRepository.java
@@ -7,19 +7,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryRacingCarRepository implements RacingCarRepository {
+    private static final InMemoryRacingCarRepository instance = new InMemoryRacingCarRepository();
     private final List<RacingCar> racingCars;
 
-    public InMemoryRacingCarRepository() {
+    private InMemoryRacingCarRepository() {
         this.racingCars = new ArrayList<>();
+    }
+
+    public static InMemoryRacingCarRepository getInstance(){
+        return instance;
     }
 
     // TODO : RacingCar 저장
     public void saveRacingCar(RacingCar racingCar) {
-
+        racingCars.add(racingCar);
     }
 
     // TODO : RacingCar 반환
     public List<RacingCar> getRacingCars() {
-        return null;
+        return racingCars;
+    }
+
+    public void clear() {
+        racingCars.clear();
     }
 }
