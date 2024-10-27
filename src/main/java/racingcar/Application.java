4e09470ba6ce68f84ff8f8b18bd76a8baa8f8c30
ksundev <kshk08@hanyang.ac.kr>
2024-10-27 14:56:45 +0900

    feat(Application): 시도횟수 입력 및 검증기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,16 +10,20 @@ import java.util.HashSet;
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
-        List<String> carNames = List.of(input.split(","));
+        String carNamesInput = Console.readLine();
+        List<String> carNames = List.of(carNamesInput.split(","));
         validateCarNames(carNames);
 
         List<Car> cars = new ArrayList<>();
-
         for (String carName : carNames) {
             Car car = new Car(carName);
             cars.add(car);
         }
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String triesInput = Console.readLine();
+        int numTries = validateAndParseTries(triesInput);
+
     }
 
     static void validateCarNames(List<String> carNames) {
@@ -33,6 +37,21 @@ public class Application {
             } else {
                 uniqueCarNames.add(carName);
             }
+        }
+    }
+
+    static int validateAndParseTries(String triesInput) {
+        if (triesInput.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        try {
+            int numTries = Integer.parseInt(triesInput);
+            if (numTries < 1) {
+                throw new IllegalArgumentException();
+            }
+            return numTries;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
         }
     }
 
