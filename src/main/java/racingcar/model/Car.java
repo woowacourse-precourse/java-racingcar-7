70033a5df76efc2e7f044f648afa28c5be8043e3
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:32:42 +0900

    :sparkles: feat(Car): 자동차 이름 설정 및 반환

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,13 @@
 package racingcar.model;
 
 public class Car {
+    private final Name name;
+
+    public Car(String name) {
+        this.name = new Name(name);
+    }
+
+    public String getName() {
+        return this.name.toString();
+    }
 }
