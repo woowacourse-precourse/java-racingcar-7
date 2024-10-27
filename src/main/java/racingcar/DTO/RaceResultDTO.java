c712ff46e8dd297b6b37910d1af4ce7b70df186c
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-28 01:25:23 +0900

    feat(RaceResultDTO): 레이스 진행 상황에 따라서 현재 위치를 전달하는 DTO 객체 추가
    - 현재시점에서의 레이스 결과가 담긴 carInfo로 부터 반환받은 HashMap을 생성자를 통해서 대입
    - getCarList() 메서드로 레이스 중간 결과를 집계할 수 있는 정보를 반환

--- src/main/java/racingcar/DTO/RaceResultDTO.java
+++ src/main/java/racingcar/DTO/RaceResultDTO.java
@@ -1,4 +1,15 @@
 package racingcar.DTO;
 
+import java.util.HashMap;
+
 public class RaceResultDTO {
+    HashMap<String, Integer> carList = new HashMap<>();
+
+    public RaceResultDTO(HashMap<String, Integer> carList) {
+        this.carList = carList;
+    }
+
+    public HashMap<String, Integer> getCarList() {
+        return carList;
+    }
 }
