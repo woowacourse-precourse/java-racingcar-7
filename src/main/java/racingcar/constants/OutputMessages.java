83a564df2b9a87c617f80b417cdd13eced2fcbdc
yush <yush2202@gmail.com>
2024-10-28 05:23:10 +0900

    refactor($ OutputMessages): 변수명 변경
    모호한 이름 변경

--- src/main/java/racingcar/constants/OutputMessages.java
+++ src/main/java/racingcar/constants/OutputMessages.java
@@ -9,5 +9,5 @@ public class OutputMessages {
     public static final String INSERT_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String INSERT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
     public static final String RACING_RESULT = "실행 결과";
-    public static final String FINAL_WINNER = "최종 우승자 : ";
+    public static final String WINNER_ANNOUNCEMENT = "최종 우승자 : ";
 }
