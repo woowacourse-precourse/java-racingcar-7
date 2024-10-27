30982b538fd7c86d7183056886a29e4ad5a2e65d
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-10-27 19:44:40 +0900

    feat: Racing 시작 메소드, 전진 메소드 구현

--- src/main/java/racingcar/model/Racing.java
+++ src/main/java/racingcar/model/Racing.java
@@ -1,5 +1,8 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.view.OutputView;
+
 public class Racing {
     private Car[] cars;
     private int attempts;
@@ -10,5 +13,20 @@ public class Racing {
             this.cars[i] = new Car(cars[i]);
         }
         this.attempts = attempts;
+    }
+
+    public void start() {
+        for (int i = 0; i < attempts; i++) {
+            moveCars();
+            OutputView.printCarsPostions(cars);
+        }
+    }
+
+    private void moveCars() {
+        for (Car car : cars) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                car.move();
+            }
+        }
     }
 }
