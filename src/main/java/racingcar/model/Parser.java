8cd439ebfe63f5b62217781811f2a672e74a629c
gyuseon <mn5592@naver.com>
2024-10-27 17:38:00 +0900

    feat: 자동차 이름 파싱하여 리스트로 반환하는 기능 구현

---
+++ src/main/java/racingcar/model/Parser.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.validation.InputValidator;
+
+public class Parser {
+
+    public List<String> parseCarName(String input) {
+        InputValidator.validateCarNames(input);
+
+        List<String> result = new ArrayList<>();
+        String[] names = input.split(",");
+
+        for (String name : names) {
+            result.add(name);
+        }
+
+        return result;
+    }
+
+
+}
