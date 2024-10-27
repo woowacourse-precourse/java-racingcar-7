3508f138c321231db291354a59b8119966fb22aa
Goohong Chung <9rrong@uoslife.team>
2024-10-27 16:25:49 +0900

    feat(InputView): `시도할 횟수`를 입력받는다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,11 +7,17 @@ public class InputView {
 
     private static final String DELIMITER = ",";
     private static final String CAR_NAME_INPUT_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ATTEMPT_COUNT_INPUT_PROMPT = "시도할 횟수는 몇 회인가요?";
 
     public List<String> readCarNames() {
         System.out.println(CAR_NAME_INPUT_PROMPT);
         String input = Console.readLine();
         return parseToStrings(input);
+    }
+
+    public String readAttemptCount() {
+        System.out.println(ATTEMPT_COUNT_INPUT_PROMPT);
+        return Console.readLine();
     }
 
     private static List<String> parseToStrings(String input) {
