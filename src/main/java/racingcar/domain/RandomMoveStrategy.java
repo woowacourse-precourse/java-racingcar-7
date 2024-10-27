70581819a68c52de24854bd7f1c395b6444dc5b9
J0YERIM <chosarah70@gmail.com>
2024-10-28 07:52:19 +0900

    refactor: 변수명 MOVE_SUCCESS_THRESHOLD를 MOVE_THRESHOLD로 변경

--- src/main/java/racingcar/domain/RandomMoveStrategy.java
+++ src/main/java/racingcar/domain/RandomMoveStrategy.java
@@ -4,12 +4,12 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomMoveStrategy implements MoveStrategy {
 
-    private static final int MOVE_SUCCESS_THRESHOLD = 4;
+    private static final int MOVE_THRESHOLD = 4;
     private static final int RANDOM_LOWER_BOUND = 0;
     private static final int RANDOM_UPPER_BOUND = 9;
 
     @Override
     public boolean canMove() {
-        return Randoms.pickNumberInRange(RANDOM_LOWER_BOUND, RANDOM_UPPER_BOUND) >= MOVE_SUCCESS_THRESHOLD;
+        return Randoms.pickNumberInRange(RANDOM_LOWER_BOUND, RANDOM_UPPER_BOUND) >= MOVE_THRESHOLD;
     }
 }
