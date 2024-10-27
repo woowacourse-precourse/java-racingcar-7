0208bad3b06716ba179c8631399b9e5761083bbd
KoYunHyuk <aes2036@daum.net>
2024-10-27 16:38:45 +0900

    refactor: 결과 출력 클래스(Output)의 기능별 메서드 분리

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -1,10 +1,10 @@
 package racingcar;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Output {
+    private int max = 0;
     List<Car> carList;
 
     public Output(List<Car> carList) {
@@ -12,26 +12,35 @@ public class Output {
     }
 
     public void printOutput() {
-        int max = 0;
         List<String> resultList = new ArrayList<>();
 
-        for(Car car : carList) {
-            if(car.getPosition() > max) {
-                max = car.getPosition();
-            }
-        }
+        setMax();
+        addList(resultList);
+        printWinner(resultList);
+    }
 
-        for(Car car : carList) {
-            if(car.getPosition() == max) {
-                resultList.add(car.getName());
-            }
-        }
-
+    private void printWinner(List<String> resultList) {
         System.out.print("최종 우승자 : ");
         for (int i = 0; i < resultList.size(); i++) {
             System.out.print(resultList.get(i));
             if (i != resultList.size() - 1) {
                 System.out.print(", ");
+            }
+        }
+    }
+
+    private void setMax() {
+        for(Car car : carList) {
+            if(car.getPosition() > max) {
+                max = car.getPosition();
+            }
+        }
+    }
+
+    private void addList(List<String> resultList) {
+        for(Car car : carList) {
+            if(car.getPosition() == max) {
+                resultList.add(car.getName());
             }
         }
     }
