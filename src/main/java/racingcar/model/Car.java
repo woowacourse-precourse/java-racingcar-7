255bc49925df8c4113623634b9b4b1376c38d78e
kimdev <kim__dev@hufs.ac.kr>
2024-10-27 17:42:10 +0900

    feat(Race): 턴을 진행시키는 기능을 추가하라

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,5 +1,9 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashMap;
+
 public class Car {
     private CarName name;
     private int position;
@@ -7,6 +11,23 @@ public class Car {
     public Car(String name) {
         this.name = new CarName(name);
         this.position = 0;
+    }
+
+    public HashMap<String,Integer> randomlyMoveForward() {
+        if (checkBiggerThanFour()) {
+            moveForward();
+        }
+
+        HashMap<String, Integer> nameAndPosition = new HashMap<>();
+        nameAndPosition.put(name.getCarName(), position);
+        return nameAndPosition;
+    }
+
+    public boolean checkBiggerThanFour() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            return true;
+        }
+        return false;
     }
 
     public int moveForward() {
--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -1,10 +1,11 @@
 package racingcar.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Race {
     private RaceTurn turn;
-    private ArrayList<Car> carList;
+    private ArrayList<Car> carList = new ArrayList<>();
 
     public Race(int turn) {
         this.turn = new RaceTurn(turn);
@@ -12,5 +13,15 @@ public class Race {
 
     public void join(Car car) {
         this.carList.add(car);
+    }
+
+    public HashMap<String, Integer> playOneTurn() {
+        HashMap<String, Integer> allCarsNameAndPosition = new HashMap<>();
+
+        carList.stream()
+                .map(Car::randomlyMoveForward)
+                .forEach(map -> allCarsNameAndPosition.putAll(map));
+
+        return allCarsNameAndPosition;
     }
 }
