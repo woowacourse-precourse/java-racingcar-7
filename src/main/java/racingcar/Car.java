46ee74aa798b1ddaeb418fb5f96a1b542a648c47
jeong-baechoo <baechoo0905@naver.com>
2024-10-28 02:21:12 +0900

    feat: 자동차 객체 생성 및 이동 기능 구현
    - Car 클래스 생성
    - 자동차 이름과 위치 상태 관리
    - 무작위 값에 따른 자동차 이동 기능 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import static camp.nextstep.edu.missionutils.Randoms.*;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move(){
+        if (isMovable()) position++;
+    }
+
+    private boolean isMovable() {
+        return pickNumberInRange(0,9) >= 4;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
