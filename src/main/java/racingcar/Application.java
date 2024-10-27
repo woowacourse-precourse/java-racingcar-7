f01fccb85fe6cb6e7b3f0171b30aee5592b06723
Su-daa <subin.kk019@gmail.com>
2024-10-28 02:18:21 +0900

    feat: 게임 실행 흐름 설정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.controller.CarController;
+import racingcar.controller.GameController;
+import racingcar.service.CarService;
+import racingcar.service.GameService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarService carService = new CarService();
+        CarController carController = new CarController(carService);
+        carController.createCars();
+
+        GameService gameService = new GameService(carService.getCars());
+        GameController gameController = new GameController(carService, gameService);
+        gameController.startGame();
     }
 }
