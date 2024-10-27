c01ee4d5d9efb06bc3b12c05565b9cf87de5c92f
sangjun <sangjoon12345@gmail.com>
2024-10-27 14:01:33 +0900

    feat: 최종 우승자를 판별하는 기능 구현

--- src/main/java/racingcar/model/RacingManager.java
+++ src/main/java/racingcar/model/RacingManager.java
@@ -1,23 +1,40 @@
 package racingcar.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 
 public class RacingManager {
-    public List<Car>parseCarNamesToCar(String carNames){
+    public List<Car> parseCarNamesToCar(String carNames) {
         return Arrays.stream(carNames.split(","))
                 .map(String::trim)
                 .map(Car::new)
                 .collect(Collectors.toList());
     }
 
-    public void moveOrStop(List<Car>carList){
-        for(Car car : carList){
-            car.move(pickNumberInRange(0,9));
+    public void moveOrStop(List<Car> carList) {
+        for (Car car : carList) {
+            car.move(pickNumberInRange(0, 9));
         }
     }
 
+    public List<Car> getWinners(List<Car> carList) {
+        int maxDistance = 0;
+        List<Car> winnerList = new ArrayList<>();
+        for (Car car : carList) {
+            int distance = car.getDistance();
+            if (distance > maxDistance) {
+                maxDistance = distance;
+                winnerList.clear();
+                winnerList.add(car);
+            } else if (distance == maxDistance) {
+                winnerList.add(car);
+            }
+        }
+        return winnerList;
+    }
 }
