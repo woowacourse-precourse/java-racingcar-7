51cb5a990539fc3aac3621c6e15ac82c4b7b210f
pil-go <pilbongx@naver.com>
2024-10-27 10:40:37 +0900

    feat(Race): 무작위 값에 따라 자동차 전진 기능 추가

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,12 +1,24 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Race {
     private List<Car> carList;
+    private int round;
 
-    Race(List<Car> carList) {
+    Race(List<Car> carList, int round) {
         this.carList = carList;
+        this.round = round;
+    }
+
+    private void executeRound() {
+        for (Car car : carList) {
+            int dice = Randoms.pickNumberInRange(0, 9);
+            if (dice >= 4) {
+                car.forward();
+            }
+        }
     }
 
 }
