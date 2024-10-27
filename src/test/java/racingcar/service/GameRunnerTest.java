e81a75dae5b94ef6629ffff5d2d7301459abe10c
kim jihyeon <jihyeon546@gmail.com>
2024-10-28 01:16:56 +0900

    test: 게임 우승자 판별 테스트 추가

--- src/test/java/racingcar/service/GameRunnerTest.java
+++ src/test/java/racingcar/service/GameRunnerTest.java
@@ -4,6 +4,8 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import racingcar.Application;
 
@@ -11,7 +13,7 @@ public class GameRunnerTest extends NsTest {
     GameRunner gameRunner = new GameRunner();
 
     @Test
-    void 자동차_전진_테스트() {
+    void 자동차_전진_기능_테스트() {
         assertSimpleTest(() -> {
             int randomValue = 4;
             boolean isMoved = gameRunner.isMoved(randomValue);
@@ -20,11 +22,33 @@ public class GameRunnerTest extends NsTest {
     }
 
     @Test
-    void 자동차_멈춤_테스트() {
+    void 자동차_멈춤_기능_테스트() {
         assertSimpleTest(() ->{
             int randomValue = 1;
             boolean isMoved = gameRunner.isMoved(randomValue);
             assertThat(isMoved).isFalse();
+        });
+    }
+
+    @Test
+    void 우승자_판별_기능_테스트() {
+        assertSimpleTest(() ->{
+            Map<String, Integer> carState= new HashMap<>();
+            carState.put("win", 4);
+            carState.put("lose", 1);
+            String winner = gameRunner.getWinner(carState);
+            assertThat(winner).isEqualTo("win");
+        });
+    }
+
+    @Test
+    void 공동_우승자_기능_테스트() {
+        assertSimpleTest(() ->{
+            Map<String, Integer> carState= new HashMap<>();
+            carState.put("win1", 4);
+            carState.put("win2", 4);
+            String winner = gameRunner.getWinner(carState);
+            assertThat(winner).isEqualTo("win1, win2");
         });
     }
 
