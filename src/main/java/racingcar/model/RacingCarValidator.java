516fbe60c0399536a375a70e49433446e2183595
SukJin Kim <rkathrwks@gmail.com>
2024-10-28 00:21:43 +0900

    style(racingCarValidator): 가독성 높아지도록 포매팅

--- src/main/java/racingcar/model/RacingCarValidator.java
+++ src/main/java/racingcar/model/RacingCarValidator.java
@@ -4,7 +4,9 @@ import java.util.HashSet;
 import java.util.List;
 
 public class RacingCarValidator {
-    private static final String INVALID_CAR_NAME_ERROR_MESSAGE = "부적절한 형식의 자동차 이름을 입력했습니다. (자동차 이름은 공백이 아닌 5자 이하 문자열)";
+
+    private static final String INVALID_CAR_NAME_ERROR_MESSAGE =
+            "부적절한 형식의 자동차 이름을 입력했습니다. (자동차 이름은 공백이 아닌 5자 이하 문자열)";
     private static final String DUPLICATED_CAR_NAME_ERROR_MESSAGE = "중복된 자동차 이름을 입력했습니다.";
     private static final int NAME_LENGTH_LIMIT = 5;
 
