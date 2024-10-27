2a66ec5b34b5d72b3858b4dddf3fb158b6ce95b7
181108-cherry <dysy9799@naver.com>
2024-10-27 14:55:36 +0900

    refactor: 자동차 이름 출력 후 랜덤 숫자를 생성하도록 수정

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -4,26 +4,9 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
-public class Racing {
-
-    public static int randomNumber(int move) {
-
-        // 0에서 9사이의 랜덤 값 구하기
-        int randomNumber = Randoms.pickNumberInRange(0, 9);
-        System.out.println("Random number: " + randomNumber);
-
-        return randomNumber;
-    }
-
     private List<String> carNames;
 
     public Racing(List<String> carNames) {
         this.carNames = carNames;
-    }
-    public void printCarNames() {
-        System.out.println("실행 결과");
-        for (String name : carNames) {
-            System.out.println(name.trim() + ":");
-        }
     }
 }
