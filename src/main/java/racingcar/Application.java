f45a5907249138af316b6c788f8853eebf8e46ba
minseojeong <jgoldstone7@gmail.com>
2024-10-27 15:36:00 +0900

    docs: 추가 요구사항 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,23 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String carNames = getCarNames();
+        int count = getCount();
+    }
 
+    private static int getCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String tryCountInputValue = Console.readLine();
+        //입력값 검증
+        //return count;
+    }
+
+    private static String getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String carNames = Console.readLine();
+        //차이름이 5자 이하인지 검사
+        return carNames;
     }
 }
