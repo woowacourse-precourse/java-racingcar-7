b73dcfafbae17505f82a20a4b31b254493f97241
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 21:45:01 +0900

    feat(Application): 예외를 캐치하는 로직 추가
    - 최상위 호출부이자 진입점인 main 메서드가 예외를 캐치할 수 있도록 로직을 변경함
    - 예외를 캐치하면 예외 메시지를 출력함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,14 +8,16 @@ import racingcar.Factory.DomainFactory;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        DomainFactory domainFactory = new DomainFactory();
+        try {
+            DomainFactory domainFactory = new DomainFactory();
+            ControllerFactory controllerFactory = new ControllerFactory(domainFactory);
 
-        ControllerFactory controllerFactory = new ControllerFactory(domainFactory);
+            RaceViewController viewController = controllerFactory.createRaceViewController();
+            RaceProcessController processController = controllerFactory.createRaceProcessController(viewController);
 
-        RaceViewController viewController = controllerFactory.createRaceViewController();
-
-        RaceProcessController processController = controllerFactory.createRaceProcessController(viewController);
-
-        processController.run();
+            processController.run();
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
