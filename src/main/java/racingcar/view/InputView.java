69ad9a10060e9e93bc11019b1993836fb6754ede
hyebin <shin378378@naver.com>
2024-10-28 04:16:31 +0900

    feat : 시도횟수 입력값이 숫자가 아닐 때 오류 발생시키는 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,13 +5,20 @@ import java.util.Scanner;
 public class InputView {
     Scanner scanner = new Scanner(System.in);
 
-    public String InputCarNames(){
+    public String InputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return scanner.nextLine();
     }
 
-    public Integer InputTrialCount(){
+    public Integer InputTrialCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        return scanner.nextInt();
+        Integer trialCount = null;
+        try {
+            trialCount = scanner.nextInt();
+
+        } catch (Exception e) {
+            System.out.println("오류: 입력값이 숫자가 아닙니다.");
+        }
+        return trialCount;
     }
 }
