ad2013b4a173858d7900626c888d4005c9e15de5
jae woo Jung <awfjol2008@gmail.com>
2024-10-27 18:36:25 +0900

    feat: 자동차 이름을 입력받을 떄, 빈 칸이 있는지 검증하도록 로직 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,6 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 	public String readCarNameInput() {
-		return Console.readLine();
+		String input = Console.readLine();
+		validateBlank(input);
+
+		return input;
+	}
+
+	private void validateBlank(String input) {
+		if (input.isBlank()) {
+			throw new IllegalArgumentException("[ERROR] 아무것도 입력되지 않았습니다.");
+		}
 	}
 }
