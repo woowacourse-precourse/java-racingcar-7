fb1f6d5041dcc31e7fcb52ab40296d1ef1bffe8f
Lee Jeong Woo <sysliar@gmail.com>
2024-10-27 19:22:10 +0900

    feat(Track): 필드 추가
    - 차 이름 ArrayList로 트랙 생성

--- src/main/java/racingcar/Track.java
+++ src/main/java/racingcar/Track.java
@@ -1,5 +1,15 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Track {
+    private final ArrayList<Car> cars;
+
+    public Track(ArrayList<String> carNames) {
+        cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+    }
 
 }
