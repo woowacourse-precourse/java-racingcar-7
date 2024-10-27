2c1af065f47d8947e65699a0ab4406aa609febc3
rlaehd62 <rlaehd62@gmail.com>
2024-10-27 16:13:51 +0900

    feat(application): Client 추가 - Client 호출을 통하여 서비스 연결

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.client.Client;
+import racingcar.client.RacingClient;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Client client = new RacingClient();
+        client.request();
     }
 }
