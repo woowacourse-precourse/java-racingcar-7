90c4cfe00d3f15d56d837be9e7793328f7293acd
KJungW <sagakkb0707@naver.com>
2024-10-27 15:19:59 +0900

    test(generator): Car 테스트 데이터 생성기 추가
    추가 내용 (what)
    - Model인 Car를 생성하는 생성기 추가
    - 생성기를 기존의 테스트에 적용
    
    추가 이유 (why)
    - Model인 Car는 여러 테스트에서 자주 사용되기 때문에
       생성 메서드도 테스트마다 중복으로 존재
    -  이러한 중복코드를 없애기 위해 Car 테스트 메서드를 생성하는
       생성기를 추가

--- src/test/java/racingcar/service/RacingServiceTest.java
+++ src/test/java/racingcar/service/RacingServiceTest.java
@@ -3,8 +3,6 @@ package racingcar.service;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +10,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import racingcar.model.Car;
+import racingcar.test_data_generator.CarGenerator;
 
 class RacingServiceTest {
 
@@ -21,7 +20,7 @@ class RacingServiceTest {
     @ParameterizedTest(name = "3개의 자동차에 {0} 무작위 값이 주어졌을 경우 각 자동차의 전진 거리는 {1}")
     @MethodSource()
     public void 자동차_경주의_라운드_진행(List<Integer> randomNumberSet, List<Integer> expectedCarMovedDistance) {
-        List<Car> givenCars = makeCars(List.of("car1", "car2", "car3"));
+        List<Car> givenCars = CarGenerator.makeCars(List.of("car1", "car2", "car3"));
         assertRandomNumberInRangeTest(
                 () -> {
                     racingService.processRound(givenCars);
@@ -41,11 +40,6 @@ class RacingServiceTest {
         );
     }
 
-    private List<Car> makeCars(List<String> names) {
-        return names.stream().map(Car::new).toList();
-    }
-
-
     @DisplayName("자동차 경주 진행 기능 - 가장 많이 전진한 자동차들이 우승자")
     @ParameterizedTest(name = "우승자가 {1}일 경우")
     @MethodSource()
@@ -57,34 +51,10 @@ class RacingServiceTest {
 
     private static Stream<Arguments> 우승자_찾기() {
         return Stream.of(
-                Arguments.of(makeCars(List.of("r0", "r1", "r2"), List.of(5, 3, 2)), List.of("r0")),
-                Arguments.of(makeCars(List.of("r0", "r1", "r2"), List.of(5, 5, 1)), List.of("r0", "r1")),
-                Arguments.of(makeCars(List.of("r0", "r1", "r2"), List.of(5, 5, 5)), List.of("r0", "r1", "r2"))
+                Arguments.of(CarGenerator.makeCars(List.of("r0", "r1", "r2"), List.of(5, 3, 2)), List.of("r0")),
+                Arguments.of(CarGenerator.makeCars(List.of("r0", "r1", "r2"), List.of(5, 5, 1)), List.of("r0", "r1")),
+                Arguments.of(CarGenerator.makeCars(List.of("r0", "r1", "r2"), List.of(5, 5, 5)),
+                        List.of("r0", "r1", "r2"))
         );
-    }
-
-    private static List<Car> makeCars(List<String> names, List<Integer> movedDistance) {
-        if (names.size() != movedDistance.size()) {
-            throw new IllegalArgumentException("Car 리스트를 생성하기 위한 이름과 이동거리의 개수가 맞지 않습니다.");
-        }
-        List<Car> cars = new ArrayList<>();
-        for (int i = 0; i < names.size(); i++) {
-            cars.add(makeCar(names.get(i), movedDistance.get(i)));
-        }
-        return cars;
-    }
-
-    private static Car makeCar(String name, int movedDistance) {
-        try {
-            Car car = new Car(name);
-            Field movedDistanceField = Car.class.getDeclaredField("movedDistance");
-            movedDistanceField.setAccessible(true);
-            movedDistanceField.set(car, movedDistance);
-            return car;
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException("Car 클래스에서 movedDistance 필드를 찾을 수 없습니다.");
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException("Car 클래스 movedDistance 필드에 접근할 권한이 없습니다.");
-        }
     }
 }
