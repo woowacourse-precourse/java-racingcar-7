6c08d57e3b29d1d1c14f21de0fa2c58a755449c3
soeunnPark <carol0724@naver.com>
2024-10-27 17:05:09 +0900

    test(CarFactory): Add a test case for throwing exception when register duplicate car
    자동차는 중복되어 등록할 수가 없습니다. 같은 이름의 자동차를 등록하려고 할 때 예외를 발생하는 테스트를 추가했습니다.

--- src/test/java/racingcar/domain/car/CarFactoryTest.java
+++ src/test/java/racingcar/domain/car/CarFactoryTest.java
@@ -1,16 +1,18 @@
 package racingcar.domain.car;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.common.exception.CarDuplicateException;
 
 class CarFactoryTest {
 
-    @DisplayName("자동차 이름을 가지고 자동차 객체들을 생성하는 테스트")
+    @DisplayName("자동차를 생성하고 등록하는데 성공하는 테스트")
     @Test
-    void makeCarsTest() {
+    void registerCarTest() {
         CarFactory carFactory = new CarFactory();
         List<String> carNames = List.of("genesis", "bmw", "bentley", "hybrid");
         Cars cars = carFactory.makeCars(carNames);
@@ -19,6 +21,15 @@ class CarFactoryTest {
         for(int i = 0; i < carNames.size(); i++) {
             assertThat(cars.getCars().get(i).getCarName()).isEqualTo(carNames.get(i));
         }
+    }
+
+    @DisplayName("중복된 자동차를 등록 시 예외를 발생하는 테스트")
+    @Test
+    void carRegisterFailTest() {
+        CarFactory carFactory = new CarFactory();
+        List<String> carNames = List.of("genesis", "genesis");
+        assertThatThrownBy(() -> carFactory.makeCars(carNames))
+                .isInstanceOf(CarDuplicateException.class);
     }
 
 }
