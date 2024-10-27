933618b5ade51c8153e3c0342839227be1340a91
OSEUK <tpdnrzmzm3@naver.com>
2024-10-27 14:07:26 +0900

    refactor(view) : 입력 검증 방식 변경
    기존 방식은 컨트롤러에서 뷰를 통해 입력받고 검증을 따로 하게 구성.
    개선 방식은 뷰 내에서 검증 로직을 실행

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,6 +7,7 @@ import racingcar.validator.InputValidator;
 import java.util.List;
 
 public class InputView{
+
     public List<String> InputNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String names = Console.readLine();
