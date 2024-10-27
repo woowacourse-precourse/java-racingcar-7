77e0cb01dea26fc3c3e3e71bad5e815317d909fd
201912025 <michi2012@naver.com>
2024-10-27 14:50:26 +0900

    feat(OutputView): printCarRacingResult() 메서드 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,8 @@
+package racingcar.view;
+
+public class OutputView {
+
+    public void printCarRacingResult(String carName, String advanceResult) {
+        System.out.println(carName + " : " + advanceResult);
+    }
+}
