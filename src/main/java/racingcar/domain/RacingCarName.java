3a934c6d0d670a9253b0caa0214765ca33901f41
young-in <hardworkingini@gmail.com>
2024-10-27 15:06:41 +0900

    feat : 경주용 자동차 이름 길이 제한 및 불변성 유지
    - 자동차 이름이 1자 이상 5자 이하의 길이를 갖지 않으면 IllegalArgumentException을 발생시키고 프로그램을 종료하도록 구현.
    
    - record를 사용해 자동차 이름을 불변하게 유지하여 변경 불가능한 객체로 처리.

---
+++ src/main/java/racingcar/domain/RacingCarName.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+public record RacingCarName(String carName) {
+    private static final int CAR_NAME_MAX_LENGTH = 5;
+
+    public RacingCarName {
+        ValidateCarNameLength(carName);
+    }
+    private void ValidateCarNameLength(final String carName){
+        if (carName.isEmpty() || carName.length() > CAR_NAME_MAX_LENGTH){
+            throw new IllegalArgumentException("자동차 이름은 1자리 이상 5자 이하만 가능합니다.");
+        }
+    }
+}
