0397eb39cd39e3f01a35b5b023a3b391f0772422
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-27 12:33:44 +0900

    feat: 우승자 안내 문구 출력 기능 구현
    OutputView 클래스에 우승자 안내 문구를 출력하는 기능을 구현
    - 문자열을 가공할 때 사용하는 대괄호 기호를 상수로 지정
    - 문자열을 가공할 때 사용하는 매직넘버를 상수로 지정
    - String형 List를 매개변수로 받아 가공하여 우승자 안내 문구를 출력하는 printFinalResult 메서드
    - 문자열을 가공하는 removeBracket 메서드

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,13 @@
 package racingcar.view;
 
+import java.util.List;
 import racingcar.enums.IOMessages;
 
 public class OutputView {
+    private static final char PRE_ELEMS = '[';
+    private static final int FOR_NEXT_IDX = 1;
+    private static final char POST_ELEMS = ']';
+
     private static OutputView instance;
 
     private OutputView() {
@@ -21,5 +26,15 @@ public class OutputView {
 
     public void printRoundResult(String currentStatus) {
         System.out.println(currentStatus);
+    }
+
+    public void printFinalResult(List<String> result) {
+        System.out.print(IOMessages.RESULT_HEADER.getMsg());
+        String finalWinners = result.toString();
+        System.out.println(removeBracket(finalWinners));
+    }
+
+    private String removeBracket(String finalWinners) {
+        return finalWinners.substring(finalWinners.indexOf(PRE_ELEMS) + FOR_NEXT_IDX, finalWinners.indexOf(POST_ELEMS));
     }
 }
