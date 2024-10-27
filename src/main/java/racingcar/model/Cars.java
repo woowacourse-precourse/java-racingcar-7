322654db2c06687d43028eea20069c41b19a7132
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:45:46 +0900

    :sparkles: feat(Cars): 중복된 자동차 이름이 있을 경우, 예외를 발생시킨다

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,16 +1,28 @@
 package racingcar.model;
 
+import static racingcar.message.ErrorMessages.DUPLICATED_NAMES;
 import static racingcar.message.ErrorMessages.UNPROVIDED_NAMES;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.exception.InvalidInputException;
 
 public class Cars {
+    private static final String NAME_DELIMITER = ",";
+
     private final List<Car> cars;
 
     public Cars(String inputCarNames) {
         String inputNames = inputCarNames.trim();
         validateUnprovidedNames(inputNames);
+
+        String[] nameArray = inputNames.split(NAME_DELIMITER);
+        validateDuplicatedNames(nameArray);
+
+        this.cars = Arrays.stream(nameArray)
+                .map(name -> new Car(name.trim()))
+                .collect(Collectors.toList());
     }
 
     public List<Car> getCars() {
@@ -20,6 +32,12 @@ public class Cars {
     private void validateUnprovidedNames(String names) {
         if (names == null || names.isEmpty()) {
             throw new InvalidInputException(UNPROVIDED_NAMES);
+        }
+    }
+
+    private void validateDuplicatedNames(String[] names) {
+        if (Arrays.stream(names).distinct().count() != names.length) {
+            throw new InvalidInputException(DUPLICATED_NAMES);
         }
     }
 }
