dcadc0db4b3559361efff283e6adcaa6a7893db3
shinebyul <qufdl8382@gmail.com>
2024-10-28 00:48:12 +0900

    [Feat] 랜덤값 범위, 전진 조건 추가

---
+++ src/main/java/racingcar/global/io/SettingCondition.java
@@ -0,0 +1,7 @@
+package racingcar.global.io;
+
+public class SettingCondition {
+    public static final int RANDOM_MIN = 0;
+    public static final int RANDOM_MAX = 9;
+    public static final int ADVANCE_CONDITION = 4;
+}
