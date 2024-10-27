4772d58caba34030fed7c283260feb5994c3ac66
Park - Jeonghwan <990706leo@gmail.com>
2024-10-28 07:04:21 +0900

    feat(Game): 이름의 중복을 검사할 수 있다

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -7,17 +7,21 @@ public class Game {
 
     private List<Car> cars = new ArrayList<>();
 
-    /*
-    1. 입력받은거 건네받음
-    2. 이름만큼 차 생성
-    3. 매번 난수 세개씩 생성
-    4. 경주해서 결과 확인
-     */
     public List<Car> createCars(List<String> names) {
+        validateDuplicated(names);
         for (String name : names) {
             cars.add(new Car(name));
         }
 
         return cars;
+    }
+
+    private void validateDuplicated(List<String> names) {
+        long nonDuplicatedSize = names.stream()
+                .distinct()
+                .count();
+        if (nonDuplicatedSize != names.size()) {
+            throw new IllegalArgumentException("이름이 중복되었습니다.");
+        }
     }
 }
