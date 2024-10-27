2ef651121bafe6b70eb959eaab5b0fb3a62de73a
pil-go <pilbongx@naver.com>
2024-10-27 10:55:14 +0900

    feat(Race): 시도 횟수만큼 라운드 진행 기능 추가

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -4,6 +4,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Race {
+    private static final int DICE_START_NUMBER = 0;
+    private static final int DICE_END_NUMBER = 9;
+    private static final int FORWARD_CONDITION = 4;
+
     private List<Car> carList;
     private int round;
 
@@ -12,10 +16,16 @@ public class Race {
         this.round = round;
     }
 
+    public void play() {
+        for (int i = 0; i < round; i++) {
+            executeRound();
+        }
+    }
+
     private void executeRound() {
         for (Car car : carList) {
-            int dice = Randoms.pickNumberInRange(0, 9);
-            if (dice >= 4) {
+            int dice = Randoms.pickNumberInRange(DICE_START_NUMBER, DICE_END_NUMBER);
+            if (dice >= FORWARD_CONDITION) {
                 car.forward();
             }
         }
