8ac6fbe21b9653e1dbbdc913db0c01b7c67ec054
seoyoungjin23 <ascd050@gmail.com>
2024-10-27 21:05:46 +0900

    feat(RacingController): 경주 실행 기능 추가
    input으로 실제 경주를 실행해서 output을 내는 기능 추가

---
+++ src/main/java/racingcar/RacingController.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingController {
+    private Race race;
+
+    public void run() {
+        String carNamesInput = InputHandler.getRacingCarNames();
+        int numberOfAttempts = InputHandler.getNumberOfAttempts();
+
+        List<RacingCar> racingCars = createRacingCars(carNamesInput);
+
+        race = new Race(racingCars);
+        race.startRacing(numberOfAttempts);
+
+        OutputHandler.printCars(race.getRacingCars());
+        OutputHandler.printWinners(race.getWinners());
+    }
+
+    private List<RacingCar> createRacingCars(String carNamesInput) {
+        String[] names = carNamesInput.split(",");
+        List<RacingCar> racingCars = new ArrayList<>();
+
+        for (String name : names) {
+            racingCars.add(new RacingCar(name.trim()));
+        }
+
+        return racingCars;
+    }
+}
