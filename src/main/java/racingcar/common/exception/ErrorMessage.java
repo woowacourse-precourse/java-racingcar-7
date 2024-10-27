0ace2feca980531739c76fa44e0abaf3a905a319
soeunnPark <carol0724@naver.com>
2024-10-27 19:04:29 +0900

    refactor(ErrorMessage): Add a error message for Empty Car Exception
    자동차가 비어있을 경우 예외를 발생하기 위해 에러 메시지를 추가했습니다.

--- src/main/java/racingcar/common/exception/ErrorMessage.java
+++ src/main/java/racingcar/common/exception/ErrorMessage.java
@@ -5,12 +5,17 @@ public enum ErrorMessage {
     CAR_POSITION_OUT_OF_RANGE("자동차가 더 이상 전진할 수 없습니다."),
     DUPLICATE_CAR("중복된 자동차를 등록할 수 없습니다."),
     CAR_NAME_EMPTY("자동차 이름이 비어 있어서는 안됩니다."),
-    CAR_NAME_LENGTH_OVER_FIVE("자동차 이름이 5글자를 넘어서는 안됩니다.")
+    CAR_NAME_LENGTH_OVER_FIVE("자동차 이름이 5글자를 넘어서는 안됩니다."),
+    CARS_EMPTY("자동차 리스트가 비어있습니다.")
     ;
 
     private final String description;
 
     ErrorMessage(String description) {
         this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
