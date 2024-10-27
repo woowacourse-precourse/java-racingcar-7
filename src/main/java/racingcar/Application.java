3ad06a3c15f7a942b35c5625801e8677dc02e3ff
Minuring <mw9505@gmail.com>
2024-10-27 14:14:38 +0900

    feat(main): 애플리케이션
    - 자동차 이름과 횟수 입력을 위임
    - 출력 전략을 주입해 게임 생성
    - 전진 정책을 주입해 게임 시작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.domain.car.Car;
+import racingcar.domain.car.ForwardPolicy;
+import racingcar.domain.game.Display;
+import racingcar.domain.game.Game;
+import racingcar.io.InputReader;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig appConfig = AppConfig.getInstance();
+        InputReader inputReader = appConfig.inputReader();
+
+        List<Car> cars = inputReader.readCarNames();
+        int count = inputReader.readCount();
+
+        Display display = appConfig.display();
+        Game game = Game.of(cars, display);
+
+        ForwardPolicy forwardPolicy = appConfig.forwardPolicy();
+        game.start(count, forwardPolicy);
     }
 }
