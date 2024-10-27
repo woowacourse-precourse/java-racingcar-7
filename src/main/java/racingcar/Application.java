b322c8f0d4ab3190766d1e72f3f381309b546c4e
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-28 04:44:28 +0900

    refactor(Application): RacingController 생성 로직 분리 및 CarFactory 의존성 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,21 @@
 package racingcar;
 
 import racingcar.controller.RacingController;
+import racingcar.model.CarFactory;
 import racingcar.view.InputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        RacingController racingController = new RacingController(new InputView());
+        RacingController racingController = setUpController();
 
         racingController.startGame();
+    }
+
+    private static RacingController setUpController() {
+        InputView inputView = new InputView();
+        CarFactory carFactory = new CarFactory();
+
+        return new RacingController(inputView, carFactory);
     }
 }
