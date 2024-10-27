e88542dd6ed5f3df717c1de220cc34c425f5db60
aj4941@naver.com <aj4941@naver.com>
2024-10-27 22:22:02 +0900

    :pencil2: [FIX] Application, InterView 코드 일부 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,14 +4,10 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            List<String> carNames = InputView.getCarNames();
-            int rounds = InputView.getRounds();
-            RacingGame racingGame = new RacingGame(carNames, rounds);
-            racingGame.start();
-            OutputView.printWinners(racingGame.getWinners());
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        List<String> carNames = InputView.getCarNames();
+        int rounds = InputView.getRounds();
+        RacingGame racingGame = new RacingGame(carNames, rounds);
+        racingGame.start();
+        OutputView.printWinners(racingGame.getWinners());
     }
 }
--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -8,6 +8,9 @@ public class InputView {
     public static List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
         List<String> carNames = Arrays.asList(input.split(","));
         carNames.forEach(InputView::validateCarName);
         return carNames;
