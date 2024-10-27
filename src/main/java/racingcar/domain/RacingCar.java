10b7dc219da7a9ae604b702a2a790418eb4199e6
babyfox225 <babyfox225@naver.com>
2024-10-27 14:52:31 +0900

    refactor: 자동차 이름 유효성 검사 메서드 분리

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -4,6 +4,11 @@ public class RacingCar {
     private final String name;
 
     public RacingCar(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
         if (name == null || name.trim().isEmpty()) {
             throw new IllegalArgumentException("자동차 이름은 필수입니다.");
         }
@@ -11,8 +16,6 @@ public class RacingCar {
         if (name.length() > 5) {
             throw new IllegalArgumentException("자동차는 5자리 이하 이름만 지정할 수 있습니다.");
         }
-
-        this.name = name;
     }
 
     public String getName() {
