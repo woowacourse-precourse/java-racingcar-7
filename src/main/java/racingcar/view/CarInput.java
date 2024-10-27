f84447a2c98c572efb10ec638fd92ef80382ea3e
mete0rfish <sungwon326@naver.com>
2024-10-27 15:28:41 +0900

    chore(view): fix typo of CarInput
    change InputCarsName to inputCarsName

--- src/main/java/racingcar/view/CarInput.java
+++ src/main/java/racingcar/view/CarInput.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class CarInput {
 
-    public static List<String> InputCarsName() {
+    public static List<String> inputCarsName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         return Arrays.stream(input.split(",")).toList();
