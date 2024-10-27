ce82fb90df195aa5c92438c4976004ea0fa98d36
kimsehw <seunghwan9703@gmail.com>
2024-10-28 03:24:13 +0900

    refactor(RacingJudge): 메소드 추출
    메소드 추출을 통해 기존 decideWinnerBy 로직의 가독성을 높입니다.
    메소드들의 파라미터 개수를 줄이기 위해 전역 변수들을 추가합니다.

--- src/main/java/racingcar/model/RacingJudge.java
+++ src/main/java/racingcar/model/RacingJudge.java
@@ -4,28 +4,51 @@ import java.util.List;
 
 public class RacingJudge {
 
+    private String winners = "";
+    private int maxDistance = 0;
+
     public String decideWinnerBy(List<RacingRecord> records) {
-        String winners = "";
-        int maxDistance = 0;
-
         for (RacingRecord record : records) {
-            int moveDistance = record.getMoveDistance();
-            String name = record.getCarName();
-
-            if (moveDistance == maxDistance) {
-                if (!winners.isEmpty()) {
-                    winners += "," + name;
-                }
-                if (winners.isEmpty()) {
-                    winners += name;
-                }
-            }
-
-            if (moveDistance > maxDistance) {
-                maxDistance = moveDistance;
-                winners = name;
-            }
+            decideWinnerBy(record);
         }
         return winners;
+    }
+
+    private void decideWinnerBy(RacingRecord record) {
+        int moveDistance = record.getMoveDistance();
+        String name = record.getCarName();
+
+        checkNewWinnerUsing(moveDistance, name);
+        checkNewSoloWinnerUsing(moveDistance, name);
+    }
+
+    private void checkNewSoloWinnerUsing(int moveDistance, String name) {
+        if (moveDistance > maxDistance) {
+            maxDistance = moveDistance;
+            winners = name;
+        }
+    }
+
+    private void checkNewWinnerUsing(int moveDistance, String name) {
+        if (moveDistance == maxDistance) {
+            addAnotherWinner(name);
+            addFirstWinner(name);
+        }
+    }
+
+    private void addFirstWinner(String name) {
+        if (isFirstWinner()) {
+            winners += name;
+        }
+    }
+
+    private void addAnotherWinner(String name) {
+        if (!isFirstWinner()) {
+            winners += "," + name;
+        }
+    }
+
+    private boolean isFirstWinner() {
+        return winners.isEmpty();
     }
 }
