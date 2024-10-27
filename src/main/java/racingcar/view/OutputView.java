c1dcdf295a5da455028bd8a0f238a3e385a884a3
jae woo Jung <awfjol2008@gmail.com>
2024-10-28 00:04:04 +0900

    feat: 경주 결과를 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,9 @@
 package racingcar.view;
 
+import java.util.List;
+
+import racingcar.dto.RaceResultResponse;
+
 public class OutputView {
 	public void promptCarName() {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -11,5 +15,12 @@ public class OutputView {
 
 	public void printRaceResultPrompt() {
 		System.out.println("실행 결과");
+	}
+
+	public void printRaceStatus(List<RaceResultResponse> raceResultResponses) {
+		raceResultResponses.forEach(raceResultResponse -> {
+			String result = raceResultResponse.name() + " : " + raceResultResponse.distance();
+			System.out.println(result);
+		});
 	}
 }
