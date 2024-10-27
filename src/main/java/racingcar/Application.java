13eb7d6082deb7fde19b054d689c50258e46d005
sjsk3232 <sjsk3232@gmail.com>
2024-10-28 01:22:07 +0900

    feat : 경주할 자동차 이름 문자열 파싱 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,19 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class Application {
+
+    private static final String DELIMITER = ",";
 
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputNames = Console.readLine();
+        List<String> carNames = List.of(inputNames.split(DELIMITER));
 
         System.out.println("시도할 횟수는 몇 회인가요?");
-        int trialCount = Integer.parseInt(Console.readLine());
+        String inputRaceTime = Console.readLine();
+        int raceTime = Integer.parseInt(inputRaceTime);
     }
 }
