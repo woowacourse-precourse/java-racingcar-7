6a737538ffc0d1dfd029ca56c64343527e1da9d3
thsghdud13 <thsgdud25@naver.com>
2024-10-28 05:33:46 +0900

    feat: 의존성 주입 기능
    * AppConfig에서 의존성 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.config.AppConfig;
+import racingcar.controller.RacingCarController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        AppConfig appConfig = new AppConfig();
+        RacingCarController racingCarController = appConfig.racingCarController();
+        racingCarController.run();
     }
 }
