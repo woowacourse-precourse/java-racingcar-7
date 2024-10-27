12a615f921f09ac832da28a3792c9a3a7c3dc500
Dohun Kim <dohun8758@naver.com>
2024-10-27 15:45:16 +0900

    feat: 입력값 검증 기능
    - 자돋차 이름에 중복이 있는경우
    - 자동차 이름의 개수가 2개 미만인 경우

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,17 +1,21 @@
 package racingcar.validator;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class InputValidator {
 
     private static final String NOT_ALLOWED_CHARACTER_REGEX = ".*[^a-zA-Z0-9가-힣].*";
     private static final int MAX_CAR_NAME_RANGE = 5;
+    public static final int MIN_CAR_NAME_COUNT = 2;
     public static final int MIN_TRIAL_COUNT = 1;
     public static final String NUMBER_REGEX = "\\d+";
 
     public void validateCarName(String[] names) {
         validateCarNameLength(names);
         validateCarNameContainsNotAllowedCharacter(names);
+        validateCarNameDuplication(names);
+        validateCarNameCount(names);
     }
 
     public void validateTrialCount(String trialCount) {
@@ -22,6 +26,18 @@ public class InputValidator {
     private void validateCarNameLength(String[] names) {
         if (Arrays.stream(names).anyMatch(s -> s.length() > MAX_CAR_NAME_RANGE)) {
             throw new IllegalArgumentException("이름의 길이는 5자 이하여야 합니다.");
+        }
+    }
+
+    private void validateCarNameDuplication(String[] names) {
+        if (Arrays.stream(names).anyMatch(new HashSet<>()::add)) {
+            throw new IllegalArgumentException("같은 이름의 자동차 이름은 허용되지 않습니다.");
+        }
+    }
+
+    private void validateCarNameCount(String[] names) {
+        if (names.length < MIN_CAR_NAME_COUNT) {
+            throw new IllegalArgumentException("자동차의 최소 개수는 2개 입니다.");
         }
     }
 
