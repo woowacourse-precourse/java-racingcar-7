ccce849f3d5d6aa0320662144b85ec59e94679f2
RawBrain <95132660+RawBrain@users.noreply.github.com>
2024-10-28 04:33:24 +0900

    feat: impliment Car class
    자동차 경주의 기본 도메인인 Car 클래스 구현
    - MovementCondition에 따른 자동차 이동 로직 구현
    - 이동시 position 증가하는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,28 @@
 package racingcar;
 
 public class Car {
+    private final String carName;
+    private int position = 0;
+
+    public Car(String name) {
+        this.carName = name;
+    }
+
+    public void chanceToMove() {
+        if (MovementCondition.isMovementAllowed().equals("move")) {
+            move();
+        }
+    }
+
+    private void move() {
+        position++;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
