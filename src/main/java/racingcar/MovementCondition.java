d7036ac40c2e6b5ccd6b6b85d9d9edbfe7a4382a
RawBrain <95132660+RawBrain@users.noreply.github.com>
2024-10-28 04:35:07 +0900

    feat(movement): implement random movement condition logic
    자동차 이동 조건을 결정하는 로직 구현

--- src/main/java/racingcar/MovementCondition.java
+++ src/main/java/racingcar/MovementCondition.java
@@ -1,4 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class MovementCondition {
+    public static String isMovementAllowed() {
+        return checkWithRandom();
+    }
+
+    private static String checkWithRandom() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            return "move";
+        }
+        return "stop";
+    }
 }
