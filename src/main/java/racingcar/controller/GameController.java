7f5d00a042732d30bc2b794ef657a03166a4fffd
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-10-28 01:07:47 +0900

    feat: GameContoller로 전체 게임 흐름 제작

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,29 @@
+package racingcar.controller;
+
+import racingcar.service.GameService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+import java.util.Map;
+
+public class GameController {
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
+    public static void executeGame() {
+        // 사용자 입력 받기
+        List<String> carNames = inputView.getCarNamesInput();
+        int gameCount = inputView.getGameCountInput();
+
+        // 사용자 입력 정보를 토대로 게임을 실행
+        GameService gameService = new GameService(gameCount, carNames);
+        gameService.runGame();
+
+        // 게임 실행 결과를 출력
+        List<Map<String, Integer>> record = gameService.getRecord();
+        List<String> winners = gameService.getWinners();
+        outputView.printRecord(record);
+        outputView.printWinners(winners);
+    }
+}
