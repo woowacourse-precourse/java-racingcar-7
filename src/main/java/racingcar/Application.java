b806ad2ab768c9149aca2e23549860d3e71f8e44
bellmin <whdlas011203@naver.com>
2024-10-27 17:40:18 +0900

    feat(Application): main 메소드, inputCarNames, inputLoopCnt 메소드 구현
    - main() : Game 객체 생성, 생성 후 start 메소드 호출
    - inputCarNames() : 사용자로부터 자동차 이름 입력
    - inputLoopCnt() : 사용자로부터 Loop 횟수 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.game.Game;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Game game = new Game(inputCarNames());
+        game.start(inputLoopCnt());
+
+    }
+
+    private static String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    private static int inputLoopCnt() {
+        try {
+            System.out.println("시도할 횟수는 몇 회인가요?");
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
     }
 }
