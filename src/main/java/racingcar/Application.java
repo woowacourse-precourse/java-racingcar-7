c3e393720b4901597fa85dc7d9665b1471314f34
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-28 01:53:26 +0900

    style : add comment and erase unused variable

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,13 +11,13 @@ public class Application {
         try {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             String cars = Console.readLine();
-
             System.out.println("시도할 횟수는 몇 회인가요?");
             int tryN = Integer.parseInt(Console.readLine());
 
+            // 이름에 대한 길이 제한조건으로 예외 던지기
             String[] carList = cars.split(",");
             for (String s : carList) {
-                if (s.length()>5) throw new IllegalArgumentException("illegal");
+                if (s.length()>5) throw new IllegalArgumentException("Wrong Input, car name length should be under 6");
             }
 
             // 각 자동차의 이동횟수 저장할 변수 선언 및 초기화
@@ -26,6 +26,7 @@ public class Application {
                 result.put(s,0);
             }
 
+            // 각 단계별로 경기 진행 후 결과 도출
             System.out.println("실행 결과");
             for (int k=0; k<tryN; k++){
                 for (String car : carList) {
@@ -46,6 +47,7 @@ public class Application {
                 System.out.println();
             }
 
+            // 우승자 점수 구하기
             List<String> winners = new ArrayList<>();
             int max=0;
             for (String s : result.keySet()) {
@@ -54,16 +56,13 @@ public class Application {
                 }
             }
 
+            // 우승자 점수에 해당하는 사람울 최종 출력 변수에 추가
             for (String s : result.keySet()) {
                 if (max == result.get(s)) winners.add(s);
             }
 
-            String join="";
-            for (String winner : winners) {
-                System.out.println("max is "+max + " and winner is "+winner +" ");
-                join = String.join(", ", winners);
-            }
-
+            // 2명 이상이 우승자 일때 콤마로 연결한 후 출력
+            String join = String.join(", ", winners);
             System.out.println("최종 우승자 : "+join);
 
         } catch (IllegalStateException e){
