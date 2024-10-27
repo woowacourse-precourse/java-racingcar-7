d0efe8d811b999de384624dda39a09412c0ba4e2
wjdgkrud1 <wjdgkrud1@naver.com>
2024-10-27 23:32:19 +0900

    각 자동차에 이름을 부여(입력 받을 수 있게)한다. 이름은 5글자 이하 쉼표(,) 기준으로 구분.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		System.out.println("경주할 자동차 이름을 입력해주세요");
+		System.out.println("이름은 5글자 이하 쉼표(,) 기준으로 구분");
+		String input = Console.readLine();
+	}
+
 }
