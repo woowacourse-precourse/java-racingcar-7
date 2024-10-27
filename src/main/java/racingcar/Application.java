260f4b508b4f0762f169442640cedd81e250ebff
mete0rfish <sungwon326@naver.com>
2024-10-27 15:27:15 +0900

    feat(main): implement Program in Main

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import racingcar.controller.CarController;
+import racingcar.domain.CarMapper;
+import racingcar.domain.CarRace;
+import racingcar.view.CarInput;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        final var inputName = CarInput.inputCarsName();
+        final var inputLap = CarInput.inputCarRaceRap();
+
+        final CarRace carRace = CarMapper.mapCarRaceByName(inputName, inputLap);
+        final CarController carController = new CarController();
+
+        carController.race(carRace);
     }
 }
