83797cad489c65a40fdc00edfd2e67f44a50f5c7
dudxo <father8475@naver.com>
2024-10-27 15:07:56 +0900

    feat(Main) : 프로그램 실행
    - 프로그램 실행에 따른 클래스들을 의존한다.
    - 경주 프로그램 컨트롤러에 입력화면 실행을 요청한다.
    - 경주 프로그램 컨트롤러에 경주 시작을 요청한다.
    - 경주 프로그램 컨트롤러에 결과 출력화면 실행을 요청한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.service.RacingCarService;
+import racingcar.util.SeparatorParser;
+import racingcar.validator.InputValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        SeparatorParser separatorParser = new SeparatorParser();
+        InputValidator inputValidator = new InputValidator();
+        RacingCarService racingCarService = new RacingCarService(separatorParser, inputValidator);
+        RacingController racingController = new RacingController(inputView, outputView, racingCarService);
+
+        racingController.input();
+        String winnerMessage = racingController.racing();
+        racingController.output(winnerMessage);
     }
 }
