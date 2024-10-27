d769f312005c1d002b978729e09a61300afbebea
junho767 <junho767@naver.com>
2024-10-27 17:44:50 +0900

    style: 입력 검증 순서 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,8 +3,8 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.validator.InputValidator;
 
-import static racingcar.model.ViewMessage.CAR_NAMES_MESSAGE;
-import static racingcar.model.ViewMessage.TRY_NUMBER_MESSAGE;
+import static racingcar.message.ViewMessage.CAR_NAMES_MESSAGE;
+import static racingcar.message.ViewMessage.TRY_NUMBER_MESSAGE;
 
 public class InputView {
 
@@ -13,8 +13,8 @@ public class InputView {
         System.out.println(CAR_NAMES_MESSAGE);
         String input = Console.readLine().trim();
 
-        InputValidator.validateCharacter(input); // 문자열에 알맞지 않는 문자 여부 검증
-        InputValidator.validateNotEmpty(input); // 문자열 공백인지 검증
+        InputValidator.validateEmpty(input);
+        InputValidator.validateCharacter(input);
 
         return input;
     }
@@ -23,7 +23,7 @@ public class InputView {
         System.out.println(TRY_NUMBER_MESSAGE);
         String tryNumber = Console.readLine().trim();
 
-        InputValidator.validateInteger(tryNumber); // 시도 횟수가 숫자인지 검증
+        InputValidator.validateInteger(tryNumber);
 
         return Integer.parseInt(tryNumber);
     }
