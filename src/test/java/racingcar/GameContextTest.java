373b817171697651c84490a08a250e1092145d72
ksundev <kshk08@hanyang.ac.kr>
2024-10-27 21:47:32 +0900

    test(GameContext,Application): 테스트 코드 수정 및 추가

--- src/test/java/racingcar/GameContextTest.java
+++ src/test/java/racingcar/GameContextTest.java
@@ -1,27 +1,44 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 import java.util.function.Supplier;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameContextTest {
-    @Test
-    void 전진_조건_만족_테스트() {
-        Supplier<Integer> customSupplier = () -> 4;
-        GameContext gameConText = new GameContext(null, 0, customSupplier);
-        boolean result = gameConText.shouldMove();
+    private Car car1;
+    private Car car2;
+    private List<Car> cars;
 
-        assertTrue(result);
+    @BeforeEach
+    void setUp() {
+        car1 = new Car("pobi");
+        car2 = new Car("woni");
+        cars = List.of(car1, car2);
     }
 
     @Test
-    void 전진_조건_불만족_테스트() {
-        Supplier<Integer> customSupplier = () -> 3;
-        GameContext gameConText = new GameContext(null, 0, customSupplier);
-        boolean result = gameConText.shouldMove();
+    void playGame_자동차이동_테스트() {
+        Supplier<Integer> customSupplier = () -> 5;
+        GameContext gameContext = new GameContext(cars, 3, customSupplier);
 
-        assertFalse(result);
+        gameContext.playGame();
+
+        assertEquals(car1.getPosition(), 3);
+        assertEquals(car2.getPosition(), 3);
+    }
+
+    @Test
+    void playGame_자동차정지_테스트() {
+        Supplier<Integer> customSupplier = () -> 3;
+        GameContext gameContext = new GameContext(cars, 3, customSupplier);
+
+        gameContext.playGame();
+
+        assertEquals(car1.getPosition(), 0);
+        assertEquals(car2.getPosition(), 0);
     }
 }
