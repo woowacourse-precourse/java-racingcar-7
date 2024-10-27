08d1624e578fd33e9d7ebc70b8e46d6ee5d3f620
gyuoo <yeomky98@naver.com>
2024-10-28 03:40:33 +0900

    feat(util): implement random number generation

---
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -0,0 +1,12 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+	private static final int MIN_VALUE = 0;
+	private static final int MAX_VALUE = 9;
+
+	public int pick() {
+		return Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE);
+	}
+}
