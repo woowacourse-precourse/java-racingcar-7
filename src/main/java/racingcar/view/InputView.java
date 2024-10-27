c28af1260e5990face34a61233303a247cc27df7
ruudska6 <feels19@naver.com>
2024-10-27 12:45:05 +0900

    <feat>: InputView List에  자동차 이름들을 저장 하기 위한 메서드 convertINputCarnamesToList() 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,18 +1,32 @@
 package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
     private static final String QUESTION_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String QUESTION_TRY_COUNT = "시도할 회수는 몇회인가요?";
 
-    public String askCarNames() {
-        System.out.println(QUESTION_CAR_NAMES);
-        return Console.readLine();
-    }
+    public List<String> askCarNames() {
+            System.out.println(QUESTION_CAR_NAMES);
+            String inputCarNames = Console.readLine();
+            return convertInputCarNamesToList(inputCarNames);
+        }
 
-    public int askTryCount() {
+    public int askTryCount () {
         System.out.println(QUESTION_TRY_COUNT);
         return Integer.parseInt(Console.readLine());
+    }
+
+    private List<String> convertInputCarNamesToList(String inputCarNames) {
+        String[] CarNames = inputCarNames.split(",");
+        List<String> carNames = new ArrayList<>();
+
+        for (String CarName : CarNames) {
+            String trimmedCarName = CarName.trim();
+            carNames.add(trimmedCarName);
+        }
+        return carNames;
     }
 
 }
