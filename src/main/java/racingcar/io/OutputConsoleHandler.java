25e0c6ad6f71d797fe12c0facbaa4a61838c16d6
sodychoe <sodychoe@icloud.com>
2024-10-28 08:39:18 +0900

    fix : 콘솔에 출력하는 형식 수정
    콘솔 줄바꿈 추가
    OutputConsoleHandler.showResultPrefixMessage 로 메서드화

--- src/main/java/racingcar/io/OutputConsoleHandler.java
+++ src/main/java/racingcar/io/OutputConsoleHandler.java
@@ -7,8 +7,14 @@ public class OutputConsoleHandler {
     public static final String FINAL_WINNERS_DELIMITER = ",";
     public static final String WINNERS_PREFIX = "최종 우승자 : ";
 
+    public void showResultPrefixMessage() {
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
     public void showCurrentCarStatus(String view) {
         System.out.println(view);
+        System.out.println();
     }
 
     public void showFinalWinnerNames(List<String> winnerNames) {
