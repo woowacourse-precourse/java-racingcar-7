979ee5b140ae99cd10e9ebd2a464cee2d100f44e
MinJae00 <exceedmin@gmail.com>
2024-10-27 19:45:13 +0900

    refact: 출력 양식에 맞게 기능 분리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,27 +4,27 @@ import racingcar.user.SettingGameUser;
 import racingcar.user.UserObject;
 
 public class OutputView {
-    public void printTurnResult() {
+    public static void printTurnResult() {
         for (UserObject user : SettingGameUser.user) {
             System.out.println(user.name + ":" + user.score);
         }
         System.out.println("");
     }
 
-    public void printResultSentence() {
+    public static void printResultSentence() {
         System.out.println("");
         System.out.println("실행 결과");
     }
 
-    public void printWinnerSentence() {
+    public static void printWinnerSentence() {
         System.out.print("최종 우승자 : ");
     }
 
-    public void printWinner(String s) {
+    public static void printWinner(String s) {
         System.out.print(s);
     }
 
-    public void printRest() {
+    public static void printRest() {
         System.out.print(" ,");
     }
 }
