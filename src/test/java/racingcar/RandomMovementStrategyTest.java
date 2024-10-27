7a17c642f73a6cbf6d20dd6cd352488aa525d252
sc@t0a14 <gusdn6671@gmail.com>
2024-10-27 10:39:03 +0900

    test(RandomMovementStrategy) : RandomMovementStrategy가 4 이상일 때 이동 가능한지 검증하는 테스트 추가

---
+++ src/test/java/racingcar/RandomMovementStrategyTest.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.model.RandomMovementStrategy;
+
+public class RandomMovementStrategyTest {
+
+    @Test
+    @DisplayName("RandomMovementStrategy 는 4 이상일 때 이동 가능하다")
+    void moveableWhenRandomNumberAtLeastFour(){
+        RandomMovementStrategy randomMovementStrategy =new RandomMovementStrategy(){
+            @Override
+            protected int makeRandomNumber() {
+                return 4;
+            }
+        };
+        assertThat(randomMovementStrategy.moveable()).isTrue();
+    }
+
+}
