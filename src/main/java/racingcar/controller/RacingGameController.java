455c0a0652c2cf2040966ebed6d75d5340f41092
wvssm <neoramiregi@gmail.com>
2024-10-28 00:20:27 +0900

    refactor(RacingGameController): run 메서드 내부의 로직 메서드 추출

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -3,32 +3,41 @@ package racingcar.controller;
 import racingcar.domain.RacingCarManager;
 import racingcar.util.CarNameParser;
 import racingcar.util.RandomUtil;
-import racingcar.util.ValidationUtil;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
 
 import java.util.List;
 
+import static racingcar.util.ValidationUtil.validateTrial;
+import static racingcar.view.InputView.readConsoleInput;
+import static racingcar.view.OutputView.*;
+
 public class RacingGameController {
     public void run(){
-        OutputView.printCarNameInputPrompt();
-        String carNameStr = InputView.readConsoleInput();
+        RacingCarManager racingCarManager = initializeCarsFromUserInput();
+        int trialCount = getTrialCountFromUser();
+        executeRace(trialCount, racingCarManager);
+        displayWinners(racingCarManager.findWinners());
+    }
+
+    private static RacingCarManager initializeCarsFromUserInput() {
+        promptCarNameInput();
+        String carNameStr = readConsoleInput();
         List<String> racingCarNames = CarNameParser.parse(carNameStr);
+        return new RacingCarManager(racingCarNames);
+    }
 
-        OutputView.printTrialInputPrompt();
-        String trialStr = InputView.readConsoleInput();
-        ValidationUtil.validateTrial(trialStr);
-        int trial = Integer.parseInt(trialStr);
+    private static int getTrialCountFromUser() {
+        promptTrialCountInput();
+        String trialStr = readConsoleInput();
+        validateTrial(trialStr);
+        return Integer.parseInt(trialStr);
+    }
 
-        RacingCarManager racingCarManager = new RacingCarManager(racingCarNames);
-
-        OutputView.printResultHeader();
-        for(int i=0; i<trial; i++){
+    private static void executeRace(int trial, RacingCarManager racingCarManager) {
+        displayResultHeader();
+        for(int i = 0; i< trial; i++){
             List<Integer> randomNumbers = RandomUtil.generateRandomNumberForCars(racingCarManager.getCars().size());
             racingCarManager.moveCars(randomNumbers);
-            OutputView.printRoundResult(racingCarManager.getCars());
+            displayRoundResult(racingCarManager.getCars());
         }
-
-        OutputView.printWinners(racingCarManager.findWinners());
     }
 }
