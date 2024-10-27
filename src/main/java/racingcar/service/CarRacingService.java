516f8a3225b23b29533fa4c4ee3a925308ac1b27
Willy <willy_1020@naver.com>
2024-10-27 20:03:33 +0900

    [✨ feat] Service 인터페이스 분리
    - 확장성을 고려하여, 서비스단을 인터페이스와 구현체로 분리했습니다.

--- src/main/java/racingcar/service/CarRacingService.java
+++ src/main/java/racingcar/service/CarRacingService.java
@@ -1,36 +1,12 @@
 package racingcar.service;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import racingcar.domain.Car;
 
-/**
- * CarRacingService
- * - 자동차 경주 라운드 실행, 우승자 계산 같은 비즈니스 로직 수행
- */
-public class CarRacingService {
+public interface CarRacingService {
+    void runRound(List<Car> cars);
 
-    public void runRound(List<Car> cars) {
-        for (Car car : cars) {
-            if (isMovable()) {
-                car.move();
-            }
-        }
-    }
+    boolean isMovable();
 
-    private boolean isMovable() {
-        int randomValue = Randoms.pickNumberInRange(0, 9);
-        return randomValue >= 4;
-    }
-
-    public List<Car> findWinners(List<Car> cars) {
-        int maxPosition = cars.stream()
-                .mapToInt(Car::getPosition)
-                .max()
-                .orElse(0);
-
-        return cars.stream()
-                .filter(car -> car.getPosition() == maxPosition)
-                .toList();
-    }
+    List<Car> findWinners(List<Car> cars);
 }
