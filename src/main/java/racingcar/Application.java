b760e7efe8d0d36b1b47db0651b9a5e09c8ba10d
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-10-28 08:41:12 +0900

    feat: Add get carNames and racingTime in Application.java

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import static racingcar.RacingHelper.checkCarNamesValidate;
+import static racingcar.RacingHelper.checkRacingTimeValidate;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        List<String> carNames = checkCarNamesValidate(Console.readLine());
+
+        System.out.println("시도할 횟수는 몇 회인가요?\n");
+        int racingTime = checkRacingTimeValidate(Console.readLine());
     }
+
 }
