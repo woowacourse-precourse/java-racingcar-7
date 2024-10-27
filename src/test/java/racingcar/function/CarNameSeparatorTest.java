e05bf01d2ce49b409f78a8a35913ddc669c4ff8e
world-dv <goodforx0147@naver.com>
2024-10-27 13:58:32 +0900

    test: 자동차 이름 구분자 테스트 추가

---
+++ src/test/java/racingcar/function/CarNameSeparatorTest.java
@@ -0,0 +1,45 @@
+package racingcar.function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.util.CarNameSeparator;
+
+public class CarNameSeparatorTest {
+    @Test
+    void Separator_주어진_값을_구분자_기준_구분() {
+        String input = "pobi,woni";
+        List<String> result = CarNameSeparator.splitCarNameWithSeparator(input);
+
+        assertThat(result).contains("pobi", "woni");
+        assertThat(result).containsExactly("pobi", "woni");
+    }
+
+    @Test
+    void Separator_주어진_값의_공백_인식_구분() {
+        String input = "pobi, ";
+        List<String> result = CarNameSeparator.splitCarNameWithSeparator(input);
+
+        assertThat(result).contains("pobi", " ");
+        assertThat(result).containsExactly("pobi", " ");
+    }
+
+    @Test
+    void Separator_주어진_값의_공백_제거해_구분() {
+        String input = "pobi, woni ";
+        List<String> result = CarNameSeparator.splitCarNameWithoutSpace(input);
+
+        assertThat(result).contains("pobi", "woni");
+        assertThat(result).containsExactly("pobi", "woni");
+    }
+
+    @Test
+    void Separator_주어진_값의_공백_인식_제거해_구분() {
+        String input = "pobi, ";
+        List<String> result = CarNameSeparator.splitCarNameWithoutSpace(input);
+
+        assertThat(result).contains("pobi", "");
+        assertThat(result).containsExactly("pobi", "");
+    }
+}
