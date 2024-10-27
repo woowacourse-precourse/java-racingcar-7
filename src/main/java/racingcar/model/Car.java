e7ca175a4110cd9522055dd4403619709a443d16
MinjuKwak01 <mkwak1125@gmail.com>
2024-10-27 22:07:38 +0900

    feat : 자동차를 앞으로 전진시키는 기능

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,25 @@
+package racingcar.model;
+
+public class Car {
+    private final String name;
+    private String curLocation;
+
+    public Car(String name, String curLocation) {
+        validateName(name);
+        this.name = name;
+        this.curLocation = curLocation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCurLocation() {
+        return curLocation;
+    }
+
+    public void moveForward() {
+        curLocation += "-";
+    }
+
+}
