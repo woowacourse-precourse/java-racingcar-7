a72c95245d565ddbc2134c663d2d49808e60e413
yunsseong <aster20@hanyang.ac.kr>
2024-10-27 23:56:47 +0900

    feat: 시도할 횟수 받기 기능 구현

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -16,6 +16,9 @@ public class Controller {
     public void run() {
         outputView.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<Car> cars = readyCar();
+        outputView.print("시도할 횟수는 몇 회인가요?");
+        final Long times = inputView.longInput();
+        outputView.print("");
     }
 
     private List<Car> readyCar() {
