b7ef02ee58817475b427f8ee9d32df0c6c515fe4
hwangdaesun <hds650588@gmail.com>
2024-10-27 12:13:32 +0900

    feat : 자동차들이 움직이는 메서드 구현
    무작위 값들에 따라 자동차들이 움직일 수 있다.
    무작위 값의 개수와 자동차들의 개수가 다르면 예외가 발생한다.

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -3,6 +3,7 @@ package racingcar.model;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Cars {
 
@@ -19,5 +20,24 @@ public class Cars {
 
     public List<Car> getCars(){
         return Collections.unmodifiableList(cars);
+    }
+
+    public void moveAll(List<Integer> randomNumbers){
+        validateSize(randomNumbers);
+        for(int i=0; i<randomNumbers.size(); i++){
+            Integer randomCount = randomNumbers.get(i);
+            cars.get(i).move(randomCount);
+        }
+        print();
+    }
+
+    private void validateSize(List<Integer> randomNumbers) {
+        if(!Objects.equals(randomNumbers.size(), cars.size())){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void print() {
+        System.out.println();
     }
 }
--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -2,6 +2,8 @@ package racingcar.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarsTest {
@@ -16,6 +18,23 @@ class CarsTest {
     void 쉼표를_기준으로_자른_이름이_5글자_보다_클_경우() {
         String input = "daesun,woni,jun";
         assertThrows(IllegalArgumentException.class, () -> Car.getInstance(input));
+    }
+
+    @Test
+    @DisplayName("랜덥값은 4이상일 때 전진한다.")
+    void 랜덤값들에_따라_자동차들이_움직일수있다(){
+        List<Integer> randomNumbers = List.of(5, 6, 7);
+        Cars cars = getCars();
+        cars.moveAll(randomNumbers);
+        assertEquals(1, cars.getCars().get(0).getDistance());
+        assertEquals(1, cars.getCars().get(1).getDistance());
+        assertEquals(1, cars.getCars().get(2).getDistance());
+    }
+
+    @Test
+    void 랜덥값들의_개수와_자동차들의_개수가_다르면_예외_발생(){
+        List<Integer> randomNumbers = List.of(5, 6);
+        assertThrows(IllegalArgumentException.class, () -> getCars().moveAll(randomNumbers));
     }
 
     private Cars getCars() {
