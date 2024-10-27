08878c18e733489e22401a903899a78a136f3e73
jeonghyeon-ohk <ok6737@naver.com>
2024-10-27 18:18:26 +0900

    feat(controller): 자동차 경주를 실행하여 결과를 반환하는 play 매서드 구현
    - random 변수 값에 따라 자동차를 1회 전진
    - 각 자동차 별 이동 거리를 콘솔에 출력
    - 최대 이동거리 값을 가진 자동차들을 저장
    - 최종 우승자를 콘솔에 출력

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,4 +1,37 @@
 package racingcar.controller;
 
+import racingcar.domain.Forward;
+import racingcar.domain.ValueExtractor;
+import racingcar.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame{
+    String[] cars;
+    int[] forwardDistance;
+    int racingCount;
+    List<String> winner;
+
+    public RacingGame(String[] cars, int racingCount) {
+        this.cars = cars;
+        this.racingCount = racingCount;
+        forwardDistance = new int[cars.length];
+        winner = new ArrayList<>();
+    }
+
+    public void play() {
+        System.out.println("실행 결과");
+
+        for (int count = 0; count < racingCount; count++) {
+            Forward.move(forwardDistance);
+            OutputView.printForwardResult(cars, forwardDistance);
+        }
+
+        int max = ValueExtractor.getMaxValue(forwardDistance);
+        ValueExtractor.getWinner(cars, forwardDistance, winner, max);
+
+        String result = String.join(", ", winner);
+        OutputView.printResult(result);
+    }
 }
