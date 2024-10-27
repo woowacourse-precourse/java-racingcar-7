d1ad7535def25f5b75b619246be451a3a32e352e
Jin409 <sualng123@gmail.com>
2024-10-27 23:03:32 +0900

    feat(TotalRounds, TotalRoundsTest):라운드 수에 대한 입력값이 최소 라운드 수보다 큰지 검증해라

--- src/main/java/racingcar/model/game/TotalRounds.java
+++ src/main/java/racingcar/model/game/TotalRounds.java
@@ -3,6 +3,7 @@ package racingcar.model.game;
 public class TotalRounds {
 
     private static final int MAX_TOTAL_ROUNDS = 10;
+    private static final int MIN_TOTAL_ROUNDS = 1;
     private final int totalRounds;
 
     public TotalRounds(int totalRounds) {
@@ -12,6 +13,10 @@ public class TotalRounds {
     }
 
     private void validate(int totalRounds) {
+        if (totalRounds < MIN_TOTAL_ROUNDS) {
+            throw new IllegalArgumentException("총 라운드 수는 2회 이상이어야 합니다.");
+        }
+
         if (totalRounds > MAX_TOTAL_ROUNDS) {
             throw new IllegalArgumentException("총 라운드 수는 10회 이하여야 합니다.");
         }
--- src/test/java/racingcar/model/TotalRoundsTest.java
+++ src/test/java/racingcar/model/TotalRoundsTest.java
@@ -9,6 +9,7 @@ import racingcar.model.game.TotalRounds;
 public class TotalRoundsTest {
 
     private static final int MAX_TOTAL_ROUNDS = 10;
+    private static final int MIN_TOTAL_ROUNDS = 1;
 
     @Test
     @DisplayName("최대 라운드 수를 초과하는 경우 예외를 던진다.")
@@ -21,4 +22,17 @@ public class TotalRoundsTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("총 라운드 수는 10회 이하여야 합니다.");
     }
+
+    @Test
+    @DisplayName("최소 라운드 수보다 작은 경우 예외를 던진다.")
+    void throwExceptionWhenTotalRoundsIsLessThanMinimum() {
+        // given
+        int invalidTotalRounds = MIN_TOTAL_ROUNDS - 1;
+
+        // when & then
+        assertThatThrownBy(() -> new TotalRounds(invalidTotalRounds))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("총 라운드 수는 2회 이상이어야 합니다.");
+    }
+
 }
