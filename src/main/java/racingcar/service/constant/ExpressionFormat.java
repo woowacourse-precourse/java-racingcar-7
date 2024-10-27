18ad7364b5b7c58e2bddac7ab74638badd24921c
sytTest <syt1128@naver.com>
2024-10-27 22:15:55 +0900

    :sparkles: create ExpressionFormat
    표현식을 상수 값으로 모아둔 ExpressionFormat 추가

---
+++ src/main/java/racingcar/service/constant/ExpressionFormat.java
@@ -0,0 +1,17 @@
+package racingcar.service.constant;
+
+public enum ExpressionFormat {
+    SEPARATOR(","),
+    GHOST_CAR(",,"),
+    ONLY_NUMBERS("[0-9]+"),
+    WINNER_SEPARATOR(", ");
+
+    private final String rule;
+
+    ExpressionFormat(String format) {
+        this.rule = format;
+    }
+    public String form() {
+        return rule;
+    }
+}
