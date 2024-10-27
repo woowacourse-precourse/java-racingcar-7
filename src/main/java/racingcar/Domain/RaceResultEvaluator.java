01dba9c8fe926d87a42529ed77c12eb7e0bdad94
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-28 03:01:42 +0900

    feat(RaceResultEvaluator): 최종 우승자 선정 로직 구현 및 최대 거리 계산 구현
    - 각 자동차의 최대 거리를 계산하여 우승자를 선정하는 로직 구현
    - 최대 거리를 구한 후 해당 거리와 같은 값을 가진 자동차 탐색
    - 최대 거리에 도달한 모든 자동차를 리스트에 추가
    - 우승자 리스트를 반환하는 getWinners() 구현

--- src/main/java/racingcar/Domain/RaceResultEvaluator.java
+++ src/main/java/racingcar/Domain/RaceResultEvaluator.java
@@ -1,4 +1,34 @@
 package racingcar.Domain;
 
+import static racingcar.Util.Constants.Constant.DISTANCE_OF_WINNER;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import racingcar.DTO.RaceResultDTO;
+
 public class RaceResultEvaluator {
+    private final List<String> winnerList = new ArrayList<>();
+    private HashMap<String, Integer> carList;
+
+    public void evaluateFinalWinners(RaceResultDTO raceResultDTO) {
+        this.carList = raceResultDTO.getCarList();
+        Integer maxDistance = DISTANCE_OF_WINNER.getValue();
+
+        for (Integer distance : carList.values()) {
+            if (distance > maxDistance) {
+                maxDistance = distance;
+            }
+        }
+
+        for (String carName : carList.keySet()) {
+            if (carList.get(carName).equals(maxDistance)) {
+                winnerList.add(carName);
+            }
+        }
+    }
+
+    public List<String> getWinners() {
+        return new ArrayList<>(winnerList);
+    }
 }
