c80881087f06cfedc8fe34fc94badd4e8c7ce8d9
Parkjyun <jaeyun0507@naver.com>
2024-10-28 02:37:07 +0900

    fix(ViewMessage): 입력메시지 요구사항에 맞도록 변경

--- src/main/java/racingcar/view/ViewMessage.java
+++ src/main/java/racingcar/view/ViewMessage.java
@@ -1,7 +1,7 @@
 package racingcar.view;
 
 public enum ViewMessage {
-    INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)"),
+    INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     INPUT_ROUND_COUNT("시도할 횟수는 몇 회인가요?"),
     RESULT_MESSAGE("실행 결과"),
     WINNERS_MESSAGE("최종 우승자 : ");
