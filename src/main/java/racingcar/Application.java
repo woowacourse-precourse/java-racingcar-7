66efed431949ebd46dad8e001260ee156e40ff39
Minseok <saltishDev@gmail.com>
2024-10-27 23:28:37 +0900

    feat: 쉼표를 기준으로, 입력받은 문자열을 분리하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,10 +2,14 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String racingCarNameInput = inputString("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> racingCars = Arrays.asList(racingCarNameInput.split(","));
     }
 
     private static String inputString() {
