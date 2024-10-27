5a0be20f70aeaee4369913d68b252e704ad8e9cc
이창근 <dlckdrms0517@gmail.com>
2024-10-28 01:03:10 +0900

    feat: Application 개발
    - 애플리케이션의 시작점입니다.
    - RaceGameConfig를 구성한 후, 자동차 경주를 시작합니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import racingcar.config.RaceGameConfig;
+import racingcar.config.moveProvider.DefaultMoveProviderConfig;
+import racingcar.config.reader.DefaultReaderConfig;
+import racingcar.config.writer.DefaultWriterConfig;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RaceGameConfig raceGameConfig = new RaceGameConfig(
+                new DefaultReaderConfig(),
+                new DefaultWriterConfig(),
+                new DefaultMoveProviderConfig()
+        );
+        RaceGame raceGame = new RaceGame(raceGameConfig);
+
+        raceGame.play();
     }
 }
