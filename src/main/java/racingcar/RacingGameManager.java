3eb01e78955b71c75a082fc7043a2507683d81aa
bokyeong <wntjdals0412@gmail.com>
2024-10-27 21:20:44 +0900

    fix : 횟수는 최소 1번부터 입력 가능 제시된 테스트 상으로는 1번부터 가능

--- src/main/java/racingcar/RacingGameManager.java
+++ src/main/java/racingcar/RacingGameManager.java
@@ -34,8 +34,8 @@ public class RacingGameManager {
     }
 
     private void validateInputCount(int count) {
-        if (count < 2 || count > 1000) {
-            throw new IllegalArgumentException("횟수는 최소 2번부터 최대 1000번까지 가능합니다. 입력값 :" + count);
+        if (count < 1 || count > 1000) {
+            throw new IllegalArgumentException("횟수는 최소 1번부터 최대 1000번까지 가능합니다. 입력값 :" + count);
         }
     }
 }
