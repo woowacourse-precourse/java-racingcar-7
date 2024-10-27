6242ee2e7561193eb992430f28adc4f53807da9e
ssggii <haan7525@gmail.com>
2024-10-27 16:20:58 +0900

    feat: implement moveCarsForwardRandomly method in CarRacing
    CarRacing 클래스의 moveCarsForwardRandomly 메서드로 랜덤값에 따라
    자동차들의 전진 여부를 결정하는 기능을 구현했습니다.

---
+++ src/main/java/racingcar/model/CarRacing.java
@@ -0,0 +1,50 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarRacing {
+
+    private final List<Car> racingCars;
+
+    public CarRacing(List<Car> racingCars) {
+        this.racingCars = racingCars;
+    }
+
+    // 전진 여부 결정
+    public void moverCarsForwardRandomly() {
+        for (Car car : racingCars) {
+            int randomValue = Randoms.pickNumberInRange(0, 9);
+            if (randomValue >= 4) {
+                car.goForward();
+            }
+        }
+    }
+
+    // 우승자 결정
+    public List<Car> getWinners() {
+        List<Car> winners = new ArrayList<>();
+        winners.add(racingCars.getFirst());
+        int maxDistance = racingCars.getFirst().getTotalDistance();
+
+        // 총 전진 거리가 최대인 자동차가 우승
+        for (int i = 1; i < racingCars.size(); i++) {
+            Car car = racingCars.get(i);
+            int carDistance = car.getTotalDistance();
+            if (carDistance == maxDistance) {
+                winners.add(car);
+            } else if (carDistance > maxDistance) {
+                maxDistance = carDistance;
+                winners.clear();
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
+
+    public List<Car> getRacingCars() {
+        return racingCars;
+    }
+
+}
