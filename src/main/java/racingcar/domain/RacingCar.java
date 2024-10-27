717415cde8beefddf516c56f4519509860340530
leejh7 <777joonho@kookmin.ac.kr>
2024-10-27 20:14:25 +0900

    feat: RacingCar 클래서 작성

---
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+public class RacingCar {
+
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private String name;
+
+    public RacingCar(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank() || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(String.format("자동차 이름으로 %s는 적절하지 않습니다.", name));
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    
+}
