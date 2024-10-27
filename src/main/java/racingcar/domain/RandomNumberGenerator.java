5ab983c2d8e053d6f355d4379cf4fda28e93c869
YoonSeungHyun2 <dbstmdgus5050@gmail.com>
2024-10-27 20:02:00 +0900

    feat: 예외처리 기능 발생 시 애플리케이션이 종료되도록 수정

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -2,11 +2,19 @@ package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+/**
+ * RandomNumberGenerator 클래스는 NumberGenerator 인터페이스를 구현하여,
+ * - 지정된 범위의 난수를 생성하는 역할을 한다.
+ */
 public class RandomNumberGenerator implements NumberGenerator {
 
-    private static final int RANDOM_LOWER_INCLUSIVE = 0;
-    private static final int RANDOM_UPPER_INCLUSIVE = 9;
+    private static final int RANDOM_LOWER_INCLUSIVE = 0;   // 난수 생성 범위의 하한값
+    private static final int RANDOM_UPPER_INCLUSIVE = 9;   // 난수 생성 범위의 상한값
 
+    /**
+     * 난수를 생성하여 반환한다.
+     * - 범위: 0 ~ 9
+     */
     @Override
     public int generate() {
         return Randoms.pickNumberInRange(RANDOM_LOWER_INCLUSIVE, RANDOM_UPPER_INCLUSIVE);
