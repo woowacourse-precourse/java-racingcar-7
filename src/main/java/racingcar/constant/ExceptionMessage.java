9ceefdefa3e26e47d120f096a9b37b2eae0a6ec5
eunseobb <seobseob0810@naver.com>
2024-10-27 13:10:44 +0900

    refactor(constant): 에러 메세지 수정
    - 일관성 있는 이름으로 변경
    - 양수가 아닐 때와 정수가 아닐 때의 에러메세지 분리

--- src/main/java/racingcar/constant/ExceptionMessage.java
+++ src/main/java/racingcar/constant/ExceptionMessage.java
@@ -2,9 +2,11 @@ package racingcar.constant;
 
 public enum ExceptionMessage {
     CAR_NAME_DELIMITER_ERROR("쉼표가 아닌 문자표가 있습니다."),
-    CAR_NAME_EMPTY_ERROR("자동차의 이름 중 공백이 있습니다."),
-    CAR_NAME_NO_INPUT_ERROR("자동차의 이름이 입력되지 않았습니다."),
-    NOT_POSITIVE_INTEGER("양의 정수를 입력해주세요."),
+    CAR_NAME_EMPTY("자동차의 이름 중 공백이 있습니다."),
+    CAR_NAME_NO_INPUT("자동차의 이름이 입력되지 않았습니다."),
+    CAR_NAME_LENGTH_EXCEEDED("자동차의 이름은 5자 이내로 입력해주세요."),
+    NOT_POSITIVE("양수를 입력해주세요."),
+    NOT_INTEGER("정수를 입력해주세요."),
     WRONG_FORMAT("출력 형식이 잘못되었습니다.");
 
     private final String message;
