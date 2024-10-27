7086e9bc9d40c1cc4452adbd769388720d2f0502
taek2222 <oot309@daum.net>
2024-10-28 00:04:56 +0900

    test(CarTest): 추가 메소드 및 반복 테스트 추가
    - 테스트 케이스의 여러 값의 할당 메소드를 반복문을 통해 모든 범위를 테스트할 수 있도록 변경했습니다.
    
    - 추가된 getName() 메소드를 테스트 하기 위해 추가했습니다.
    
    - 추가된 compareDistance() 메소드를 테스트 하기 위해 추가했습니다.

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -11,13 +11,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
 
+    private static final String NORMAL_NAME = "pobi";
+    private static final String ABNORMAL_NAME = "yeonTaek";
+
     @Test
     void 이름이_정상일_경우_객체가_생성된다() {
-        // given
-        String name = "pobis";
-
-        // when
-        Car car = new Car(name);
+        // given & when
+        Car car = new Car(NORMAL_NAME);
 
         // then
         assertNotNull(car);
@@ -32,35 +32,87 @@ class CarTest {
 
     @Test
     void 이름이_5자_초과하면_예외가_발생한다() {
-        // given
-        String name = "yeonTaek";
-
-        // when & then
         assertThrows(IllegalArgumentException.class,
-                () -> new Car(name));
+                () -> new Car(ABNORMAL_NAME));
     }
 
     @Test
-    void 무작위_값이_4이상_이면_전진한다() {
+    void 자동차_이름을_가져온다() {
         // given
-        Car car = new Car("pobis");
+        Car car = new Car(NORMAL_NAME);
+
+        // when
+        String name = car.getName();
+
+        // then
+        assertEquals(NORMAL_NAME, name);
+    }
+
+    @Test
+    void 차의_거리가_크면_1을_반환한다() {
+        // given
+        Car car = new Car(NORMAL_NAME);
+        Car otherCar = new Car(NORMAL_NAME);
+
+        car.moveForward(4);
+
+        // when
+        int result = car.compareDistance(otherCar);
+
+        // then
+        assertEquals(result, 1);
+    }
+
+    @Test
+    void 차의_거리가_같으면_0을_반환한다() {
+        // given
+        Car car = new Car(NORMAL_NAME);
+        Car otherCar = new Car(NORMAL_NAME);
+
+        // when
+        int result = car.compareDistance(otherCar);
+
+        // then
+        assertEquals(result, 0);
+    }
+
+    @Test
+    void 차의_거리가_적으면_마이너스_1을_반환한다() {
+        // given
+        Car car = new Car(NORMAL_NAME);
+        Car otherCar = new Car(NORMAL_NAME);
+
+        otherCar.moveForward(4);
+        // when
+        int result = car.compareDistance(otherCar);
+
+        // then
+        assertEquals(result, -1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void 무작위_값이_4이상_이면_전진한다(int randomNumber) {
+        // given
+        Car car = new Car(NORMAL_NAME);
         String beforeDistance = car.toString();
 
         // when
-        car.moveForward(4);
+        car.moveForward(randomNumber);
 
         // then
         assertNotEquals(beforeDistance, car.toString());
     }
 
-    @Test
-    void 무작위_값이_4미만_이면_전진하지_않는다() {
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void 무작위_값이_4미만_이면_전진하지_않는다(int randomNumber) {
         // given
-        Car car = new Car("pobis");
+        Car car = new Car(NORMAL_NAME);
         String beforeDistance = car.toString();
 
         // when
-        car.moveForward(3);
+        car.moveForward(randomNumber);
 
         // then
         assertEquals(beforeDistance, car.toString());
