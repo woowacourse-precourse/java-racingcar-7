a5bf371d2a3a07a113dc3415001c60305a58341d
Lucerna00 <pjs3859811@naver.com>
2024-10-27 17:21:44 +0900

    refactor(Output): treat strings as constants

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -8,8 +8,10 @@ public class Output {
     private static final String NAME_INPUT_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String COUNT_INPUT_PROMPT = "시도할 횟수는 몇 회인가요?";
     private static final String RESULT_PROMPT = "\n실행 결과";
+    private static final String NAME_DISTANCE_SEPARATOR = " : ";
+    private static final String DISTANCE_UNIT = "-";
     private static final String WINNERS_PROMPT = "최종 우승자 : ";
-    private static final String WINNERS_DELIMITER = ", ";
+    private static final String WINNERS_SEPARATOR = ", ";
 
     public void printNameInputPrompt() {
         System.out.println(NAME_INPUT_PROMPT);
@@ -24,12 +26,12 @@ public class Output {
     }
 
     public void printCarStatus(String name, int distance) {
-        System.out.println(name + " : " + "-".repeat(distance));
+        System.out.println(name + NAME_DISTANCE_SEPARATOR + DISTANCE_UNIT.repeat(distance));
     }
 
     public void printWinnersPrompt(List<String> winners) {
         System.out.println(WINNERS_PROMPT
-                + winners.stream().collect(Collectors.joining(WINNERS_DELIMITER)));
+                + winners.stream().collect(Collectors.joining(WINNERS_SEPARATOR)));
     }
 
     public void printNewLine() {
