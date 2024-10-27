803a9ca10c049496602d3665ff01ecf121dee032
Lee Chae Rin <chaerin0916@gmail.com>
2024-10-27 12:12:37 +0900

    feat: 자동차 이름 쉼표(,) 기준으로 나누고 예외 발생
    - 공백 있을 경우 예외 발생
    - 6글자 이상일 경우 예외 발생

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import com.sun.jdi.ArrayReference;
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 자동차 입력 받고 쉼표 기준으로 나누기
+        String inputCarNames = Console.readLine();
+        String[] carNamesList = inputCarNames.split(",");
+
+        for (String carName : carNamesList) {
+            // 나눌 때 앞뒤로 있을 수 있는 공백 제거
+            carName = carName.trim();
+            // 공백이 있다면 예외 발생
+            if (carName == null || carName.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름이 공백입니다.");
+            } else if (carName.length() >= 6) {
+                throw new IllegalArgumentException("자동차 이름이 6글자 이상입니다.");
+            }
+        }
+
     }
 }
