d5e1698ec1f44c061c5f09b5cdaf42bb71fc76ea
taek2222 <oot309@daum.net>
2024-10-27 17:35:11 +0900

    style(RandomUtil): 매직 넘버 상수 분리
    - 매직 넘버를 상수로 분리해, 재사용성과 가독성을 향상시켰습니다.

--- src/main/java/racingcar/util/RandomUtil.java
+++ src/main/java/racingcar/util/RandomUtil.java
@@ -4,7 +4,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomUtil {
 
+    public static final int RANDOM_MIN_RANGE = 0;
+    public static final int RANDOM_MAX_RANGE = 9;
+
     public static int generateRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(RANDOM_MIN_RANGE, RANDOM_MAX_RANGE);
     }
 }
