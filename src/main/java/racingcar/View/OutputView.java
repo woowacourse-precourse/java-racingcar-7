67324e0b9668aa4c94fd8443d73d4187c3579f8a
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-28 01:33:14 +0900

    feat(OutputView): 시행 횟수 라운드별 중간 결과 출력 기능 구현
    - printRaceResult() : 기존 콘솔의 내용과 중간 결과를 분리해주는 "실행 결과" 문자열을 출력
    - displayRaceProgress() : 라운드별 중간 결과 출력
    - convertPositionToDashes() : 현재 위치에 따라서 '-' 의 개수를 반화해주는 로직 구현

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -1,4 +1,34 @@
 package racingcar.View;
 
+import java.util.HashMap;
+import racingcar.DTO.RaceResultDTO;
+import racingcar.Util.Message.IOMessage;
+
 public class OutputView {
+    HashMap<String, Integer> carList;
+
+    public void printRaceResult() {
+        System.out.println(IOMessage.RACING_RESULT.getMessage());
+    }
+
+    public void displayRaceProgress(RaceResultDTO raceResultDTO) {
+        carList = raceResultDTO.getCarList();
+
+        for (String car : carList.keySet()) {
+            String position = convertPositionToDashes(carList.get(car));
+            System.out.println(car + IOMessage.SEPARATOR.getMessage() + position);
+        }
+        System.out.print(IOMessage.NEW_LINE.getMessage());
+    }
+
+    public String convertPositionToDashes(Integer currentPosition) {
+        StringBuilder raceBar = new StringBuilder();
+
+        while(currentPosition > 0) {
+            raceBar.append("-");
+            currentPosition--;
+        }
+
+        return raceBar.toString();
+    }
 }
