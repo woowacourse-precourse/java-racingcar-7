61307547a431ba0c97590e260aed5ca0f6d4ca5c
ahpicl64 <ahpicl64@gmail.com>
2024-10-28 08:28:48 +0900

    feat(controller): GameController 클래스에 게임 진행 및 결과 출력기능 구현
    - 생성자를 통해 이름이 저장된 List를 CarManager 모델로 저장
    - playGame 메서드로 라운드수를 호출하여 게임 진행
    - 각 라운드별 roundResult 메서드로 결과 출력
    - README 구현내용 저장

--- src/main/java/racingcar/GameController.java
+++ src/main/java/racingcar/GameController.java
@@ -1,23 +1,32 @@
 package racingcar;
 
+import java.util.List;
+
 public class GameController {
     public final InputView input;
-    public final OutputView output;
-    public final Car car;
+    public final CarsManager manager;
 
-    public GameController(InputView input, OutputView output, Car car) {
+    public GameController(InputView input) {
         this.input = input;
-        this.output = output;
-        this.car = car;
+        this.manager = new CarsManager(input.printStart());
     }
 
-//    public void setCarName() {
-//        input.printStart();
-//        List<String> carNames = input.printStart();
-//        car.setCarName(String.valueOf(carNames));
-//    }
-
-    public void getResult() {
-
+    public void playGame() {
+        input.printStart();
+        int rounds = input.printRounds();
+        for (int i = 0; i < rounds; i++) {
+            manager.moveAll();
+            roundResult();
+        }
     }
+
+    public void roundResult() {
+        List<Car> cars = manager.getCars();
+        for (Car car : cars) {
+            String carName = car.getCarName();
+            int distance = car.getDistance();
+            System.out.print(carName + " : " + "-".repeat(distance));
+        }
+    }
+
 }
