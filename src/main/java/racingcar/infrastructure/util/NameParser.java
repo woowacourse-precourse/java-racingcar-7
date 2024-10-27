452ddad6cdbb4374074f155d0550582381ababb7
jijongkwon <help3451@naver.com>
2024-10-28 00:24:11 +0900

    feat(util) : ',' 로 이름 구별하는 메서드 구현

--- src/main/java/racingcar/infrastructure/util/NameParser.java
+++ src/main/java/racingcar/infrastructure/util/NameParser.java
@@ -1,11 +1,27 @@
 package racingcar.infrastructure.util;
 
+import racingcar.presentation.enums.ExceptionMessage;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NameParser {
 
+    private final NameValidator nameValidator = new NameValidator();
+
     // TODO : ',' 로 이름 구별하기
     public List<String> parsingName(String name) {
-        return null;
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessage.EMPTY_NAME_EXCEPTION.getMessage());
+        }
+
+        List<String> names = Arrays.stream(name.split(","))
+                .map(String::trim)
+                .filter(s -> !s.isEmpty())
+                .collect(Collectors.toList());
+
+        nameValidator.validRacingCarName(names);
+        return names;
     }
 }
