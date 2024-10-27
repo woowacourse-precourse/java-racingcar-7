6296b6f0520ecc0d0dfebb5a4d331c5329efc860
MinJae00 <exceedmin@gmail.com>
2024-10-28 03:38:46 +0900

    refact: 게임의 진행 역할에 충실한 컨트롤러로 업데이트. 턴 진행과 유저 한명씩 게임을 진행할 수 있도록 도와주는 컨트롤러로 리팩토링

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -3,37 +3,56 @@ package racingcar.controller;
 import java.util.List;
 import racingcar.operator.GameEngine;
 import racingcar.operator.WinnerSelector;
-import racingcar.user.SettingGameUser;
+import racingcar.user.UserObject;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class GameController {
+    private UserObject[] user;
+    private int NUMBER_OF_USERS;
+    GameEngine gameEngine = new GameEngine();
+
     public void runGame() {
-        new SettingGameUser();
         OutputView.printResultSentence();
-        GameEngine gameEngine = new GameEngine();
-        for (int i = 0; i < InputView.trynum; i++) {
-            gameEngine.progressGame();
-        }
+        progressGame();
         displayWinner();
     }
 
-    public static void displayProgress() {
-        OutputView.printTurnResult();
+    private void progressGame() {
+        for (int i = 0; i < InputView.trynum; i++) {
+            gameEngine.extractRandom();
+            gameEngine.scoreUpdate(user[i]);
+        }
+        displayProgress();
     }
 
-    public static void displayWinner() {
+    private void displayProgress() {
+        for (int i = 0; i < NUMBER_OF_USERS; i++) {
+            String name = user[i].getUserName();
+            String score = user[i].getUserScoreString();
+            OutputView.printTurnResult(name, score);
+        }
+        OutputView.printEmpty();
+    }
+
+    private void displayWinner() {
         OutputView.printWinnerSentence();
-        List<String> topScoreList = new WinnerSelector().getTopScores();
+        List<String> topScoreList = new WinnerSelector(user).getTopScores();
         for (int i = 0; i < topScoreList.size(); i++) {
             displayRest(i);
             OutputView.printWinner(topScoreList.get(i));
         }
     }
 
-    public static void displayRest(int index) {
+    private void displayRest(int index) {
         if (index != 0) {
             OutputView.printRest();
         }
     }
+
+    public GameController(UserObject[] user) {
+        this.user = user;
+        NUMBER_OF_USERS = user.length;
+    }
+
 }
