e7813e71dfed4cb56ced9287068b8cc2857b07f2
송지웅 <thdwldnd7@dgu.ac.kr>
2024-10-28 00:37:37 +0900

    feat: 자동차 객체 추가 - 자동차의 이름과 위치 관리 - 움직이는 메서드

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+    public void forward() {
+        this.position += 1;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+}
