3c2f02ee948cb58148990f875936b867e499fc73
EHCTH <lees90896@gmail.com>
2024-10-27 11:59:41 +0900

    feat(NameValidator): 예외 기능 작성 * feat: 중복 이름 검사 * feat: 이름 길이가 5이하인지 검사

--- src/main/java/racingcar/racingcar/validator/name/NameValidator.java
+++ src/main/java/racingcar/racingcar/validator/name/NameValidator.java
@@ -1,7 +1,21 @@
 package racingcar.racingcar.validator.name;
 
+import racingcar.constant.errormessage.ErrorMessage;
+import racingcar.racingcar.domain.RacingCar;
+import racingcar.racingcar.dto.RacingCarInfo;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class NameValidator {
+    private static final int MAX_WITHIN_RANGE = 5;
     public static void withingRangeNameLength(String name) {
-        if (name.length() > 5) throw new IllegalArgumentException("[ERROR] 이름 길이가 5초과입니다");
+        if (name.length() > MAX_WITHIN_RANGE) throw new IllegalArgumentException(ErrorMessage.WITHIN_RANGE_ERROR_MESSAGE);
+    }
+    public static void isDuplicateName(RacingCarInfo racingCarInfo) {
+        List<RacingCar> racingCarList = racingCarInfo.getRacingCarInfo();
+        Set<RacingCar> racingCarSet = new HashSet<>(racingCarList);
+        if (racingCarSet.size() != racingCarList.size()) throw new IllegalArgumentException(ErrorMessage.DUPLICATE_ERROR);
     }
 }
