b9483552dde53504ed1b3f5861ede45fb893965f
gyuoo <yeomky98@naver.com>
2024-10-28 02:50:12 +0900

    feat(parser): implement method for parsing race count

--- src/main/java/racingcar/util/Parser.java
+++ src/main/java/racingcar/util/Parser.java
@@ -13,4 +13,15 @@ public class Parser {
 		}
 		return new ArrayList<>(Arrays.asList(input.split(",")));
 	}
+
+	public int stringToInt(String input) {
+		if (input == null || input.isBlank()) {
+			throw new IllegalArgumentException(ErrorMessage.EMPTY_RACE_COUNT.getMessage());
+		}
+		try {
+			return Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(ErrorMessage.INVALID_RACE_COUNT.getMessage());
+		}
+	}
 }
