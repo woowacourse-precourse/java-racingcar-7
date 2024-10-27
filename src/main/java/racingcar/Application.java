e202d8f5678be53aff6c10693556a9f8b1479e41
na0th <105775683+na0th@users.noreply.github.com>
2024-10-27 16:07:01 +0900

    feat : Validator 클래스 구현
    추가한 내용 :
    1. 자동차 이름 형식 검증 메서드
    - 자동차 이름 형식은 ','을 기준으로 구분
    2. 자동차 이름 글자수 검증 메서드
    - 자동차 이름은 5글자 이내
    3. 이동 횟수 입력 검증 메서드
    - 양의 정수만 가능(0 포함)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -31,9 +34,45 @@ public class Application {
     }
 
     public static class Validator {
-        // 자동차 이름 입력 형식 을 검증 (쉼표로 구분)
+        private static final String VALID_NAME_PATTERN = "^[a-zA-Z0-9]+(,[a-zA-Z0-9]+)*$";
+        private static final int NAME_MAX_LENGTH = 5;
+        private static final String POSITIVE_INTEGER_PATTERN = "^[0-9]+$";
+
+
+        private static final String VALIDATE_NAME_PATTERN_MSG = "자동차 이름 입력 형식은 , 단위로 작성해주십시오";
+        private static final String MAX_NAME_SIZE_MSG = "이름은 최대 5자까지 가능합니다.";
+        private static final String VALIDATE_POSITIVE_INTEGER_MSG = "이동 횟수는 0 이상의 정수만 가능합니다.";
+
+        private void validateAll(String inputCarNames, String inputCarRoundCount) {
+            validateNameForm(inputCarNames);
+            validateNameCount(inputCarNames);
+            validateInputCarRoundCount(inputCarRoundCount);
+        }
+
+
+        // 자동차 이름 입력 형식을 검증 (쉼표로 구분), 아니면 throw
+        private void validateNameForm(String input) {
+            if (!input.matches(VALID_NAME_PATTERN)) {
+                throw new IllegalArgumentException(VALIDATE_NAME_PATTERN_MSG);
+            }
+        }
         // 자동차 이름이 5자 이내인지 검증
-        // 이동 횟수 입력을 검증
+        private void validateNameCount(String input) {
+            String[] names = input.split(",");
+            for (String name : names) {
+                if (name.length() > NAME_MAX_LENGTH) {
+                    throw new IllegalArgumentException(MAX_NAME_SIZE_MSG);
+                }
+            }
+        }
+
+        // 이동 횟수 입력을 검증 (양의 정수만 가능.. 음수, 음의 소수, 숫자가 아니거나, 너무 큰 수)
+        private void validateInputCarRoundCount(String inputCarRoundCount) {
+            if (!inputCarRoundCount.matches(POSITIVE_INTEGER_PATTERN)) {
+                throw new IllegalArgumentException(VALIDATE_POSITIVE_INTEGER_MSG);
+            }
+        }
+        
     }
 
     public static class Car {
