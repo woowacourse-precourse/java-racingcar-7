520f8567ed6070247f9030a7d602a185739c9961
JoonKyo <joonkyolee@gmail.com>
2024-10-27 12:31:17 +0900

    feat(Cars): 자동차 이름에 중복이 있을 경우 처리하는 메서드 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,16 +1,18 @@
 package racingcar.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
     private static final String ERROR_MESSAGE_FOR_NUMBER_OF_CARS = "경주할 자동차가 부족합니다. 프로그램을 종료합니다.";
-
+    private static final String ERROR_MESSAGE_FOR_DUPLICATED_CAR = "중복된 자동차 이름이 존재합니다. 프로그램을 종료합니다.";
     private static final int MINIMUM_NUMBER_OF_CARS = 2;
     private final List<Car> cars;
 
     private Cars(List<Car> cars) {
         this.cars = cars;
     }
+
     public static Cars from(List<Car> cars) {
         return new Cars(cars);
     }
@@ -19,5 +21,19 @@ public class Cars {
         if (cars.size() < MINIMUM_NUMBER_OF_CARS) {
             throw new IllegalArgumentException(ERROR_MESSAGE_FOR_NUMBER_OF_CARS);
         }
+    }
+
+    private static void validateDuplicatedCar(List<Car> cars) {
+        if (afterSizeRemoveDuplication(cars) != cars.size()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_DUPLICATED_CAR);
+        }
+    }
+
+    private static int afterSizeRemoveDuplication(List<Car> cars) {
+        return cars.stream()
+                .map(Car::getName)
+                .distinct()
+                .toList()
+                .size();
     }
 }
