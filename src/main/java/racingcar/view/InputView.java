e924230946737aee9b79bf29da82f9f9ca4183cb
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-10-27 19:54:17 +0900

    feat: 입력에 대한 유효성 검증

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,18 +3,48 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-
+    // 자동차 이름 입력 메소드
     public static String[] inputCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String text = Console.readLine();
         String[] carNames = text.split(",");
+        validateCarNames(carNames);
         return carNames;
     }
-
+    // 시도 횟수 입력 메소드
     public static int inputAttempts() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String input = Console.readLine();
-        int attempts = Integer.parseInt(input);
+        int attempts = validateAttemptsInput(input);
         return attempts;
     }
+
+    // 자동차 이름 유효성 검사
+    private static void validateCarNames(String[] carNames) {
+        for (String name : carNames) {
+            if (name.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+            }
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
+    }
+
+    // 시도 횟수 유효성 검사
+    private static int validateAttemptsInput(String input) {
+        try {
+            int attempts = Integer.parseInt(input);
+            validateAttempts(attempts);
+            return attempts;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 횟수는 숫자로 입력해야 합니다.");
+        }
+    }
+    private static void validateAttempts(int attempts) {
+        if (attempts <= 0) {
+            throw new IllegalArgumentException("시도할 횟수는 양수를 입력해주세요");
+        }
+    }
+
 }
