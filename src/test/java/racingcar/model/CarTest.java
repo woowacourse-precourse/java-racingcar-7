9aa8e7935eb91a13e6dbb5fa707392a4ee264f07
Goohong Chung <9rrong@uoslife.team>
2024-10-27 18:11:00 +0900

    refactor(CarTest): `@ParameterizedTest` 문법 적용

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -3,21 +3,21 @@ package racingcar.model;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 class CarTest {
 
     @Test
     void 이름_길이가_5이상이면_예외를_반환한다() {
-        assertThatThrownBy(() -> new Car("exceed"))
+        assertThatThrownBy(() -> new Car("123456"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 이름에_공백이나_null이_들어오면_예외를_반환한다() {
-        assertThatThrownBy(() -> new Car(""))
-                .isInstanceOf(IllegalArgumentException.class);
-
-        assertThatThrownBy(() -> new Car(null))
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 이름에_공백이나_null이_들어오면_예외를_반환한다(String name) {
+        assertThatThrownBy(() -> new Car(name))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
