08a7aa263740c3591e972af3fdcce7a6ffb15e26
jaehwannnnnn <seanjung0915@gmail.com>
2024-10-28 01:23:28 +0900

    fix: 예제에 나와있는 문구로 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,12 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public static String[] getCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요 : ");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine().split(",");
     }
 
     public static String getRaceAttempts() {
-        System.out.println("시도할 횟수를 입력하세요 :");
+        System.out.println("시도할 횟수는 몇 회인가요?");
         return Console.readLine();
     }
 }
