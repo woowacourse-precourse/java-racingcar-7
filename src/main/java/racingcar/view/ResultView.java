03214aab9e520f4229bf0dbeb23fde1c82ec3960
alswnsp411 <alswnsp411@naver.com>
2024-10-28 02:17:17 +0900

    Refactor: racing 진행 상황 출력 구현 시 repeat 메서드를 사용

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -8,18 +8,14 @@ public class ResultView {
 
     public void printResultView(final List<Car> carList, final int inputTurns) {
         for (int i = 1; i <= inputTurns; i++) {
-            for (final Car car : carList) {
-                printProcessView(car.getName(), car.getDistance(i));
-            }
+            printTurnView(carList, i);
             System.out.println();
         }
     }
 
-    private void printProcessView(String carName, int distance) {
-        StringBuilder visualizeDistance = new StringBuilder();
-        for (int d = 0; d < distance; d++) {
-            visualizeDistance.append("-");
+    private void printTurnView(final List<Car> carList, final int i) {
+        for (final Car car : carList) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getDistance(i)));
         }
-        System.out.println(carName + " : " + visualizeDistance);
     }
 }
