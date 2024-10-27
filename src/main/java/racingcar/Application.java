00ebee32435b6fe4c3f842df8ea5caa35f805ef4
Jungwoo Chu <cjw980930@gmail.com>
2024-10-27 22:35:44 +0900

    fix: 시작 문구 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("자동차 이름을 쉼표(,)로 구분하여 입력해 주세요.");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         System.out.println("시도할 횟수는 몇 회인가요?");
         int attemptsNum = Integer.parseInt(Console.readLine());
