aed89cb5c4f1b66306cc8e2415019a03dfd088b7
sseen2 <kse10023@naver.com>
2024-10-27 23:10:03 +0900

    feat: 우승자 출력 기능 구현

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -3,11 +3,15 @@ package racingcar;
 import java.util.List;
 
 public class OutputView {
-    public static void outputRacingResult(List<Car> cars) {
+    public static void printRacingResult(List<Car> cars) {
         for(Car car : cars) {
             System.out.println(car.toString());
         }
 
         System.out.println();
+    }
+
+    public static void printWinner(List<String> winner) {
+        System.out.println("최종 우승자 : " + String.join(",", winner));
     }
 }
--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Race {
@@ -14,13 +15,40 @@ public class Race {
     public void playRacing() {
         for (int i = 0; i < moveCount; i++) {
             carsMove();
-            OutputView.outputRacingResult(cars);
+            OutputView.printRacingResult(cars);
         }
+
+        OutputView.printWinner(getWinner());
     }
 
     private void carsMove() {
         for (Car car : cars) {
             car.moveCar();
         }
+    }
+
+    private List<String> getWinner() {
+        int maxPosition = 0;
+        List<String> winner = new ArrayList<>();
+
+        for (Car car : cars) {
+            maxPosition = getMaxPosition(car.getPosition(), maxPosition);
+        }
+
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winner.add(car.getName());
+            }
+        }
+
+        return winner;
+    }
+
+    private int getMaxPosition(int position, int maxPosition) {
+        if (position >= maxPosition) {
+            return position;
+        }
+
+        return maxPosition;
     }
 }
