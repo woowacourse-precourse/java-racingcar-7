dbc1d9efe1c71efa3b2f283cc6504e9859b6f8d8
wvssm <neoramiregi@gmail.com>
2024-10-27 23:18:07 +0900

    feat(RandomUtil): 자동차 수만큼 난수를 발생하는 메서드 추가

--- src/main/java/racingcar/util/RandomUtil.java
+++ src/main/java/racingcar/util/RandomUtil.java
@@ -1,12 +1,19 @@
 package racingcar.util;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class RandomUtil {
     public static final int RANDOM_NUMBER_MIN = 0;
     public static final int RANDOM_NUMBER_MAX = 9;
 
-    public static int generateRandomNumber() {
-        return pickNumberInRange(RANDOM_NUMBER_MIN, RANDOM_NUMBER_MAX);
+    public static List<Integer> generateRandomNumberForCars(int carSize) {
+        return IntStream.range(0,carSize)
+                .map(i->pickNumberInRange(RANDOM_NUMBER_MIN, RANDOM_NUMBER_MAX))
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
