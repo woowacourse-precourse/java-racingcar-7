653d16781dd5182ed2d7e0528eb1af942d0c2dc5
201912025 <michi2012@naver.com>
2024-10-27 18:04:52 +0900

    feat(Application): 객체 생성 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,12 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.service.RacingCarService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 public class Application {
     public static void main(String[] args) {
+        RacingCarController racingCarController = new RacingCarController(new InputView(), new OutputView(), new RacingCarService());
+        racingCarController.run();
     }
 }
