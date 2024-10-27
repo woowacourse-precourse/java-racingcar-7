f5baaaf7a30cfd1d179c33ceb569681ce6ffe964
Sinorang <sinholang@naver.com>
2024-10-27 18:38:14 +0900

    test: 자동차 이름 null, 빈 값 예외 테스트 코드 구현
    - 자동차 이름에 null 혹은 빈 문자열이 있는 경우 예외 처리 테스트 코드
      작성

--- src/test/java/racingcar/validation/CarNameValidatorTest.java
+++ src/test/java/racingcar/validation/CarNameValidatorTest.java
@@ -2,6 +2,7 @@ package racingcar.validation;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.constants.ErrorMessageConstants;
 
@@ -18,5 +19,14 @@ public class CarNameValidatorTest {
         assertThatThrownBy(() -> CarNameValidator.validateCarNames(carNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessageConstants.ERROR_CAR_NAME_CONTAINS_BLANK);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 자동차_이름_null_또는_빈_문자열_예외_테스트(String carName) {
+        List<String> carNames = Arrays.asList(carName, "woni", "jun");
+        assertThatThrownBy(() -> CarNameValidator.validateCarNames(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessageConstants.ERROR_EMPTY_CAR_NAME);
     }
 }
