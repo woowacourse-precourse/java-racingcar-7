df8df589413f7744bf09ebfd72b00d80cf3dc3ad
JoonKyo <joonkyolee@gmail.com>
2024-10-27 13:14:08 +0900

    feat(Car): 자동차의 이동 결과를 -를 이용해 나타내는 방법도 추가 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,19 +3,22 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
+    private static final String MOVE_STATE = "-";
     private static final int MIN_BOUND = 0;
     private static final int MAX_BOUND = 9;
     private static final int MOVE_BOUND = 4;
     private final CarName carName;
     private int position;
+    private String moveStatus;
 
-    private Car(CarName carName, int position) {
+    private Car(CarName carName, int position, String moveStatus) {
         this.carName = carName;
         this.position = position;
+        this.moveStatus = moveStatus;
     }
 
     public static Car from(String input) {
-        return new Car(CarName.from(input), 0);
+        return new Car(CarName.from(input), 0, "");
     }
 
     public String getName() {
@@ -25,6 +28,7 @@ public class Car {
     public void move() {
         if (canMove()) {
             position++;
+            moveStatus += MOVE_STATE;
         }
     }
 
