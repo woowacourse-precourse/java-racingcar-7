9509b774520a928e4d706639a3a2bc331fa94114
jungsehui <jungsehui@naver.com>
2024-10-28 00:27:01 +0900

    feat(Application.java): 우승자인 최대 전진 자동차를 전부 찾고 출력하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -65,5 +65,12 @@ public class Application {
                 .mapToInt(Car::getPosition)
                 .max()
                 .orElse(0);
+
+        // 최대 전진 자동차의 포지션(전진 횟수)을 통해 그외의 최대 전진 자동차 전부 찾기, 출력
+        List<String> winners = cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .toList();
+        System.out.print(String.join(", ", winners));
     }
 }
