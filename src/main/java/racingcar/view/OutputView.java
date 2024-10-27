c249581cde1f077ed9f243550b1d82c909a008ca
jungsehui <jungsehui@naver.com>
2024-10-28 01:52:04 +0900

    refactor(OutputView.java): 패키지 구조 변경과 메서드 분리 작업

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,29 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
+import java.util.List;
+
 public class OutputView {
+
+    public void printCurrentCarPositions(final List<Car> cars) {
+        for (Car car : cars) {
+            StringBuilder result = new StringBuilder(car.getName() + " : ");
+            int carPosition = car.getPosition();
+            for (int j = 0; j < carPosition; j++) {
+                result.append("-");
+            }
+            System.out.println(result.toString());
+        }
+        System.out.println();
+    }
+
+    public void printWinners(int maxPosition, final List<Car> cars) {
+        // 최대 전진 자동차의 포지션(전진 횟수)을 통해 그외의 최대 전진 자동차 전부 찾기, 출력
+        List<String> winners = cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .toList();
+        System.out.print(String.join(", ", winners));
+    }
 }
