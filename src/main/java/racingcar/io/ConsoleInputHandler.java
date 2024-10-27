0388b902b898a1a9e56fa5ad8b5446b93ea34f7a
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-10-27 19:12:43 +0900

    feat : 사용자 입력 시 아무것도 입력되지 않은 경우 예외 처리

--- src/main/java/racingcar/io/ConsoleInputHandler.java
+++ src/main/java/racingcar/io/ConsoleInputHandler.java
@@ -6,6 +6,8 @@ public class ConsoleInputHandler {
 
     public String getCarListInput(){
         String carListInput = Console.readLine();
+        if(carListInput == null | carListInput.isBlank())
+            throw new IllegalArgumentException("잘못된 입력입니다.");
 
         return carListInput;
     }
@@ -13,6 +15,10 @@ public class ConsoleInputHandler {
     public int getMoveTryCount(){
         try {
             String moveTryCount = Console.readLine();
+
+            if(moveTryCount == null | moveTryCount.isBlank())
+                throw new IllegalArgumentException("잘못된 입력입니다");
+
             return Integer.valueOf(moveTryCount);
         } catch(NumberFormatException e){
             throw new IllegalArgumentException("숫자를 입력해야 합니다.");
