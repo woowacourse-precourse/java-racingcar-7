1e6c11dee4244d728c16b348e9f1eefdb879318f
Bae-JunSeok <qownstjr77@gmail.com>
2024-10-28 00:34:53 +0900

    feat: 자동차 이름 및 위치 관리

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,31 @@
+package racingcar.model;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        if (name == null || name.isBlank() || name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 1~5자 사이여야 합니다.");
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(boolean shouldMove) {
+        if (shouldMove) {
+            position++;
+        }
+    }
+
+    public String getPositionRepresentation() {
+        return "-".repeat(position);
+    }
+}
