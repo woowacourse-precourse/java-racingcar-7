e042b36e7fac373e928fc4e4816a4fc3e65667bb
bokyeong <wntjdals0412@gmail.com>
2024-10-27 20:32:55 +0900

    test : 자동차 이동 테스트

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,8 +1,8 @@
 package racingcar;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,8 +12,9 @@ class CarTest {
     void 알파벳_이름이_아니면_오류() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> {
-                    new Car("abc124!");
+                    new Car("ab24!");
                 }).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("이름은 영어(알파벳)로만 생성할 수 있습니다.")
         );
     }
 
@@ -23,6 +24,7 @@ class CarTest {
                 assertThatThrownBy(() -> {
                     new Car(" ");
                 }).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("공백은 이름이 될 수 없습니다.")
         );
     }
 
@@ -32,10 +34,21 @@ class CarTest {
                 assertThatThrownBy(() -> {
                     new Car("abcdefghi");
                 }).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("이름은 5글자 이상이 될 수 없습니다.")
         );
     }
 
     @Test
-    void moveOrStop() {
+    void 숫자가_4보다_작으면_움직이면_안된다() {
+        Car car = new Car("test");
+        car.moveOrStop(3);
+        assertThat(car.getDistance()).isEqualTo(0);
+    }
+
+    @Test
+    void 숫자가_4이상이면_앞으로_움직인다() {
+        Car car = new Car("test");
+        car.moveOrStop(6);
+        assertThat(car.getDistance()).isEqualTo(1);
     }
 }
