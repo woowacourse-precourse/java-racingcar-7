304f2266166e016e6a740148d5b7c8ec5c65cd19
smyeongL <sangmyeonglee123@gmail.com>
2024-10-27 22:00:49 +0900

    fix(message): modify WINNER_MESSAGE

--- src/main/java/racingcar/message/Message.java
+++ src/main/java/racingcar/message/Message.java
@@ -4,5 +4,5 @@ public class Message {
     public static final String CAR_NAMES_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
     public static final String GAME_ROUND_REQUEST_MESSAGE = "시도할 횟수는 몇 회인가요?\n";
     public static final String EXECUTION_MESSAGE = "실행 결과\n";
-    public static final String WINNER_MESSAGE = "실행 결과\n";
+    public static final String WINNER_MESSAGE = "최종 우승자 : \n";
 }
