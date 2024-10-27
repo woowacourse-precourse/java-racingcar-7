dc7eafae8c6e13eb3230f45c772453b5d71ef159
Hujin0322 <hongyuj0322@naver.com>
2024-10-28 00:57:47 +0900

    feat(InputCarNameHandler): isValidCarName 메서드 생성

--- src/main/java/racingcar/InputCarNameHandler.java
+++ src/main/java/racingcar/InputCarNameHandler.java
@@ -9,15 +9,36 @@ public class InputCarNameHandler {
         String inputCarNames;
 
         System.out.print("경주할 자동차 이름을 2대 이상 입력하세요. : ");
-        inputCarNames = Console.readLine().trim();
+        inputCarNames = Console.readLine();
 
         String[] names = inputCarNames.split(",");
 
         for (String name : names) {
             String trimmedName = name.trim();
+            isValidCarName(trimmedName, carNames);
             carNames.add(trimmedName);
         }
+
+        if (carNames.size() < 2 || carNames.size() > 11) {
+            throw new IllegalArgumentException("최소 2개부터 최대 10개까지 입력이 가능합니다.");
+        }
+
         return carNames;
+    }
+
+    private void isValidCarName(String name, List<String> carNames) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("빈 칸 혹은 공백은 사용이 불가합니다.");
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 합니다.");
+        }
+        if (!name.matches("[a-zA-Z0-9가-힣]*")) {
+            throw new IllegalArgumentException("특수기호는 사용이 불가합니다.");
+        }
+        if (carNames.contains(name)) {
+            throw new IllegalArgumentException("중복된 자동차 이름입니다.");
+        }
     }
 }
 
