2679da6c2149f59d80e3659e39ea982a358017dd
sc@t0a14 <gusdn6671@gmail.com>
2024-10-27 10:27:21 +0900

    refactor : of -> from 네이밍 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -5,14 +5,13 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
-import racingcar.model.Distance;
-import racingcar.model.Name;
+import racingcar.model.CarName;
 
 public class CarTest {
     @Test
     @DisplayName("차가 이동한다")
     void move(){
-        Car car = Car.from(Name.from("kiri"));
+        Car car = Car.from(CarName.from("kiri"));
         car.move(()-> true);
         assertThat(car.getDistance()).isEqualTo(1);
     }
@@ -20,10 +19,11 @@ public class CarTest {
     @Test
     @DisplayName("차가 정지한다")
     void stop(){
-        Car car = Car.from(Name.from("kiri"));
+        Car car = Car.from(CarName.from("kiri"));
         car.move(()-> false);
         assertThat(car.getDistance()).isEqualTo(0);
     }
+
 
 }
 
