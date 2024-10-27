14a812b43505b24050efeb4191007f1fb9a8c747
wvssm <neoramiregi@gmail.com>
2024-10-27 11:56:58 +0900

    feat(OutputView): 우승자를 출력한다

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,23 +5,28 @@ import racingcar.domain.Car;
 import java.util.List;
 
 public class OutputView {
-    public static void printCarNameInputPrompt(){
+    public static void printCarNameInputPrompt() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public static void printTrialInputPrompt(){
+    public static void printTrialInputPrompt() {
         System.out.println("시도할 횟수는 몇 회인가요?");
     }
 
-    public static void printResultHeader(){
+    public static void printResultHeader() {
         System.out.println("실행 결과");
     }
 
-    public static void printRoundResult(List<Car> cars){
-        for(Car car : cars){
+    public static void printRoundResult(List<Car> cars) {
+        for (Car car : cars) {
             System.out.print(car.getName() + " : ");
             System.out.println("-".repeat(car.getPosition()));
         }
         System.out.println();
+    }
+
+    public static void printWinners(List<String> winnerNames) {
+        String winnerNamesStr = String.join(", ", winnerNames);
+        System.out.println("최종 우승자 : " + winnerNamesStr);
     }
 }
