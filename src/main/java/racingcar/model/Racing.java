befec7304bd5fdcb92041db448b3375f488b56f5
gyuseon <mn5592@naver.com>
2024-10-27 18:10:56 +0900

    refactor: 자동차 이름 순서 유지를 위해 LinkedHashMap으로 변경

--- src/main/java/racingcar/model/Racing.java
+++ src/main/java/racingcar/model/Racing.java
@@ -1,18 +1,31 @@
 package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Racing {
 
-    private final Map<String, Integer> raceStatus = new HashMap<>();
+    private final Map<String, Integer> raceStatus = new LinkedHashMap<>();
 
     public void initialize(List<String> input) {
         for (String name : input) {
             raceStatus.put(name, 0);
         }
     }
+
+    public void race(int count) {
+        for (int i = 0; i < count; i++) {
+
+        }
+    }
+
+    private boolean checkCondition() {
+        return Randoms.pickNumberInRange(0, 9) >= 4;
+    }
+
 
 }
