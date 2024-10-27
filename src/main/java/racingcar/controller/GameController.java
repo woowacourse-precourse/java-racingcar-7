fe8c6e89463dd7126612c59798b445dd1e075f41
MinJae00 <exceedmin@gmail.com>
2024-10-27 20:49:12 +0900

    refact: WinnerSelector 클래스의 리턴값이 String 형식의 리스트로 달라짐에 따라 로직 변경

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,6 +1,8 @@
 package racingcar.controller;
 
+import java.util.List;
 import racingcar.operator.GameEngine;
+import racingcar.operator.WinnerSelector;
 import racingcar.user.SettingGameUser;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -13,6 +15,7 @@ public class GameController {
         for (int i = 0; i < InputView.trynum; i++) {
             gameEngine.progressGame();
         }
+        displayWinner();
     }
 
     public static void displayProgress() {
@@ -20,6 +23,17 @@ public class GameController {
     }
 
     public static void displayWinner() {
+        OutputView.printWinnerSentence();
+        List<String> topScoreList = new WinnerSelector().getTopScores();
+        for (int i = 0; i < topScoreList.size(); i++) {
+            displayRest(i);
+            OutputView.printWinner(topScoreList.get(i));
+        }
+    }
 
+    public static void displayRest(int index) {
+        if (index != 0) {
+            OutputView.printRest();
+        }
     }
 }
