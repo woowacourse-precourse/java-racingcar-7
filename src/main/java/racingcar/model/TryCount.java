6a653fbcd3b126b4626a4193e34ae3b9ee123e4b
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:57:50 +0900

    :sparkles: feat(TryCount): 실행 횟수가 유효한 양의 정수가 아닐 경우, 예외를 발생시킨다

--- src/main/java/racingcar/model/TryCount.java
+++ src/main/java/racingcar/model/TryCount.java
@@ -1,9 +1,14 @@
 package racingcar.model;
 
+import static racingcar.message.ErrorMessages.TRY_COUNT_NOT_VALID_NUMBER;
+
+import racingcar.exception.InvalidInputException;
+
 public class TryCount {
     private final int tryCount;
 
     public TryCount(String count) {
+        validate(count);
         this.tryCount = convertStringToInt(count);
     }
 
@@ -13,5 +18,15 @@ public class TryCount {
 
     public boolean isNotSame(int tryCount) {
         return this.tryCount != tryCount;
+    }
+
+    private void validate(String count) {
+        validatIntegerNumber(count);
+    }
+
+    private void validatIntegerNumber(String count) {
+        if (!count.matches("^\\d+$")) {
+            throw new InvalidInputException(TRY_COUNT_NOT_VALID_NUMBER);
+        }
     }
 }
