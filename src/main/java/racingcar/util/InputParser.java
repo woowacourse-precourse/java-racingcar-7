ae03b413a92a9c824dfa37baa12248282d188978
sanghee0820 <lsh277604@gmail.com>
2024-10-28 00:08:42 +0900

    feat: Input 유효성 검증 로직 추가

--- src/main/java/racingcar/util/InputParser.java
+++ src/main/java/racingcar/util/InputParser.java
@@ -2,6 +2,7 @@ package racingcar.util;
 
 import java.util.Arrays;
 import java.util.List;
+import racingcar.exception.InputException;
 
 public final class InputParser {
     private static final String DELIMITER = ",";
@@ -10,8 +11,15 @@ public final class InputParser {
     }
 
     public static List<String> parseName(String input) {
+        validateInput(input);
         return Arrays.stream(input.split(DELIMITER))
                 .map(String::trim)
                 .toList();
+    }
+
+    private static void validateInput(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException(InputException.INPUT_IS_NULL_OR_EMPTY.getMessage());
+        }
     }
 }
