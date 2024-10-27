8cc7b7a015bb378e51cf518c4106c67cb6f33adc
choiyoung69 <choiyoung609@gmail.com>
2024-10-28 04:11:10 +0900

    feat(RacingCarNameDTO): ㅈ동차 이름이 공백인 경우 처리

--- src/main/java/racingcar/dto/RacingCarNameDTO.java
+++ src/main/java/racingcar/dto/RacingCarNameDTO.java
@@ -7,20 +7,27 @@ public class RacingCarNameDTO {
     private List<String> carNames;
 
     public RacingCarNameDTO(String carNamesInput){
-        validateString(carNamesInput);
+        validateCarNamesInputString(carNamesInput);
         this.carNames = Arrays.asList(carNamesInput.split(","));
     }
 
-    private void validateString(String carNamesInput){
-        if(carNamesInput == null || carNamesInput.trim().isEmpty())
-            throw new IllegalArgumentException("자동차 이름을 입력해주세요");
+    private void validateCarNamesInputString(String carNamesInput){
+        if(isBlank(carNamesInput))
+            throw new IllegalArgumentException("자동차 이름을 입력해야 합니다");
     }
 
     private void validateCarNames(){
 
     }
 
-    private void checkEmptyCarNames(){
+    private void validateNonEmptyCarNames(){
+        for(String carName : carNames){
+            if(isBlank(carName))
+                throw new IllegalArgumentException("자동차 이름은 비어 있거나 공백일 수 없습니다.");
+        }
+    }
 
+    private boolean isBlank(String inputString){
+        return inputString == null || inputString.trim().isEmpty();
     }
 }
