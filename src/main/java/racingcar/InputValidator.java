065d84271b0b62b19c0317e9c9362ccce9f3b40d
maedachaeva <parkchaeyeon.b@gmail.com>
2024-10-28 01:18:01 +0900

    feat: 자동차 이름 입력에 대한 예외 처리 추가

---
+++ src/main/java/racingcar/InputValidator.java
@@ -0,0 +1,39 @@
+package racingcar;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class InputValidator {
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final int MAX_NAME_LENGTH = 5;
+
+    public void checkCarCount(List<String> racingCarNames) {
+        if (racingCarNames.size() < 2) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void checkNameLength(List<String> racingCarNames) {
+        for (String racingCarName : racingCarNames) {
+            if (racingCarName.length() < MIN_NAME_LENGTH || racingCarName.length() > MAX_NAME_LENGTH) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public void checkForWhitespace(List<String> racingCarNames) {
+        for (String racingCarName : racingCarNames) {
+            if (racingCarName.contains(" ")) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public void checkForDuplicates(List<String> racingCarNames) {
+        Set<String> distinctCarNames = new HashSet<>(racingCarNames);
+        if (distinctCarNames.size() < racingCarNames.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
--- src/main/java/racingcar/UserInput.java
+++ src/main/java/racingcar/UserInput.java
@@ -5,10 +5,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class UserInput {
+    private final InputValidator inputValidator = new InputValidator();
+
     public List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String rawCarNames = Console.readLine();
-        return parseCarNames(rawCarNames);
+        List<String> racingCarNames = parseCarNames(rawCarNames);
+
+        inputValidator.checkCarCount(racingCarNames);
+        inputValidator.checkNameLength(racingCarNames);
+        inputValidator.checkForWhitespace(racingCarNames);
+        inputValidator.checkForDuplicates(racingCarNames);
+
+        return racingCarNames;
     }
 
     private List<String> parseCarNames(String rawCarNames) {
