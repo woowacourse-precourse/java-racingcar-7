0a7ef0db7c34ae9f65bb03afa11aad991f9a510a
eunseobb <seobseob0810@naver.com>
2024-10-27 13:15:22 +0900

    feat(view): 입력 관련 검증 로직 추가
    - 자동차 이름 입력 관련 검증 로직 추가
    - 시도 횟수 입력 관련 검증 로직 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,16 +2,28 @@ package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.*;
 
+import racingcar.model.Validation;
+
 public class InputView {
+    private final Validation validation;
+
+    public InputView(Validation validation) {
+        this.validation = validation;
+    }
+
     public String readRawCarNames() {
         System.out.println("자동차 이름들을 입력해주세요(쉼표로 구분).");
         String userInput = readLine();
+        validation.validCarNameDelimiter(userInput);
+        validation.checkEmptyInput(userInput);
         return userInput;
     }
 
     public Integer readAttemptCount() {
-        System.out.println("시도할 횟수를 입력해주세요");
-        Integer attemptCount = Integer.parseInt(readLine());
-        return attemptCount;
+        System.out.println("시도할 횟수를 입력해주세요.");
+        String attemptCount = readLine();
+        validation.validateAttemptCount(attemptCount);
+
+        return Integer.parseInt(attemptCount);
     }
 }
