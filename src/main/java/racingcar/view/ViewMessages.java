2c52701e3b4429a72184be8573306ce583190597
choiyoung69 <choiyoung609@gmail.com>
2024-10-28 03:16:53 +0900

    feat(ViewMessages): 출력할 메세지를 관리할 객체 생성

---
+++ src/main/java/racingcar/view/ViewMessages.java
@@ -0,0 +1,8 @@
+package racingcar.view;
+
+public class ViewMessages {
+    public static final String REQUIRE_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String REQUIRE_NUMBER_OF_ATTEMPTS = "시도할 횟수는 몇 회인가요?";
+    public static final String PRINT_EXECUTION_RESULT = "실행 결과";
+    public static final String PRINT_FINAL_WINNER = "최종 우승자 : ";
+}
