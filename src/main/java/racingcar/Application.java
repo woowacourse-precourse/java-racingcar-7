815bf9481e95e5b597e46a19107e0b26ee9adc32
wjdgkrud1 <wjdgkrud1@naver.com>
2024-10-27 23:34:30 +0900

    경주에 참여하는 사람이 여러명일 경우 ","로 구분.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,6 +9,15 @@ public class Application {
 		System.out.println("경주할 자동차 이름을 입력해주세요");
 		System.out.println("이름은 5글자 이하 쉼표(,) 기준으로 구분");
 		String input = Console.readLine();
+		String[] arr = splitInput(input);
 	}
 
+	public static String[] splitInput(String input) {
+		if (input.contains(",")) {
+			return input.split(",");
+		}
+		return new String[] { input };
+	}
+
+	
 }
