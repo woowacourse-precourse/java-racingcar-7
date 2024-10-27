f89b38bdf1286c8f9f6183d5c8cca3c1999e4a05
고성태 <v12rhtjdxo@naver.com>
2024-10-27 18:09:38 +0900

    fix : Error 처리 추가

--- src/main/java/racingcar/service/InputServiceImpl.java
+++ src/main/java/racingcar/service/InputServiceImpl.java
@@ -18,15 +18,24 @@ public class InputServiceImpl implements InputService {
     @Override
     public String[] inputRacingUsersAndSplit() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] inputArr = Console.readLine().split(",");
 
-        return Console.readLine().split(",");
+        if (Arrays.stream(inputArr).allMatch(x -> x.length() <= 5)) {
+            return inputArr;
+        } else {
+            throw new IllegalArgumentException("플레이어 이름이 5자 이하여야 합니다.");
+        }
     }
 
     @Override
     public int inputMoveCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
 
-        return Integer.parseInt(Console.readLine());
+        try {
+            return checkCollectSequence(Integer.parseInt(Console.readLine()));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수를 입력해주세요!");
+        }
     }
 
 
@@ -39,5 +48,13 @@ public class InputServiceImpl implements InputService {
 
     private RacingCar strToRacingCar(String str) {
         return new RacingCar(str, 0);
+    }
+
+    private int checkCollectSequence(int sequence) {
+        if (sequence >= 1) {
+            return sequence;
+        }
+
+        throw new IllegalArgumentException("1이상 값을 입력해주세요!");
     }
 }
