8f84ef27a4d2770262c4bc510635542d03e4b1ec
gyuoo <yeomky98@naver.com>
2024-10-28 02:28:43 +0900

    feat(error): add error messages

---
+++ src/main/java/racingcar/error/ErrorMessage.java
@@ -0,0 +1,17 @@
+package racingcar.error;
+
+public enum ErrorMessage {
+	INVALID_CAR_NAME("자동차 이름은 1자 이상 5자 이하여야 합니다."),
+	INVALID_RACE_COUNT("이동 시도 횟수는 1 이상의 정수여야 합니다."),
+	EMPTY_CAR_NAME("자동차 이름을 입력해 주세요.");
+
+	private final String message;
+
+	ErrorMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
