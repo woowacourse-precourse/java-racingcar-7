15e36eb590b4fa89453626a0c51f1191bf2a2968
강승연 <syeon.dev@gmail.com>
2024-10-27 21:26:24 +0900

    fix: 입력 문구 수정(')' 누락)

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputManager {
 
     public String[] GetCarList(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
 
         return input.split(",");
