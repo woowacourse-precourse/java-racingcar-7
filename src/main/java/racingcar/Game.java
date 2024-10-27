d225db28592d408821dfe1447a34bdd305564e1e
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-27 18:44:41 +0900

    style: 함수 변수명 수정
    게임 시작후 winner변수명에서 printWinner로 변수명 수정

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -7,23 +7,25 @@ public class Game {
     public static void start(Cars cars, int rounds){
         OutputManager.printStartMessage();
         race(cars, rounds);
-        winner(cars);
+        printWinner(cars);
     }
 
     private static void race(Cars cars, int rounds){
-        int round = 1;
 
-        while (round <= rounds){
+        for (int round = 0; round < rounds; round++){
             cars.moveCars();
             cars.currentStatus();
-            System.out.println();
-            round++;
+            printNextLine();
         }
+
     }
     
-    private static void winner(Cars cars) {
-        List<Car> winners = cars.checkWinner();
+    private static void printWinner(Cars cars) {
+        List<Car> winners = cars.getWinner();
         OutputManager.printWinner(winners);
     }
 
+    private static void printNextLine(){
+        System.out.println();
+    }
 }
