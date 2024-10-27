007a37ce778a79707a6681ec4805c50afd1d25ad
spiky-haired-Pyramid-head <qns0147@gmail.com>
2024-10-28 01:08:33 +0900

    feat: Application 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.manager.RacingManager;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingManager manager = new RacingManager();
+        manager.run();
     }
 }
