9df4f1cb718561895f6378bf68a311af890f8ce8
ouvir <kilhyunii@naver.com>
2024-10-27 15:54:30 +0900

    fix: InputSeparator 자동차 이름 길이 초과 확인 로직 추가

--- src/main/java/separator/InputSeparator.java
+++ src/main/java/separator/InputSeparator.java
@@ -4,37 +4,38 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Separator {
 public class InputSeparator {
+    private static final Integer MAX_NAME_LENGTH = 5;
 
     public static List<String> split(String text) throws IllegalArgumentException {
         if (text == null || text.isEmpty()) {
             throw new IllegalArgumentException("input is empty");
         }
 
-        List<String> splitItems  = List.of(text.split(","));
+        List<String> splitItems = List.of(text.split(","));
 
-        if (isContainEmpty(splitItems)) {
+        if (containsEmpty(splitItems)) {
             throw new IllegalArgumentException("split value is empty");
         }
-        if (isNameDuplicate(splitItems)) {
+        if (hasDuplicateNames(splitItems)) {
             throw new IllegalArgumentException("duplicate input");
+        }
+        if (!areNameLengthsValid(splitItems)) {
+            throw new IllegalArgumentException("name is too long. max length is " + MAX_NAME_LENGTH);
         }
 
         return splitItems;
     }
 
-    private static boolean isContainEmpty(List<String> splitTexts) {
-        for (String text : splitTexts) {
-            if (text.trim().isEmpty()) {
-                return true;
-            }
-        }
-        return false;
+    private static boolean containsEmpty(List<String> splitTexts) {
+        return splitTexts.stream().anyMatch(text -> text.trim().isEmpty());
     }
 
-    private static boolean isNameDuplicate(List<String> nameList) {
-        Set<String> uniqueNames = new HashSet<>(nameList);
-        return uniqueNames.size() != nameList.size();
+    private static boolean hasDuplicateNames(List<String> names) {
+        return names.stream().distinct().count() != names.size();
+    }
+
+    private static boolean areNameLengthsValid(List<String> nameList) {
+        return nameList.stream().allMatch(string -> string.length() <= MAX_NAME_LENGTH);
     }
 }
