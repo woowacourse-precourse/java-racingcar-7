248569080837ffa9b825966e2645af1ea91b81f2
navangs <43688529+insertend23@users.noreply.github.com>
2024-10-28 02:52:25 +0900

    Refactor(CompeteCars): change duplication validation logic

--- src/main/java/racingcar/CompeteCars.java
+++ src/main/java/racingcar/CompeteCars.java
@@ -1,14 +1,17 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CompeteCars {
     private static final String ERROR_CAR_NAME_DUPLICATION = "자동차 이름은 중복될 수 없습니다.";
     private final List<Car> cars = new ArrayList<>();
 
-    private void validateDuplicatedCar(Car car) {
-        if (cars.contains(car)) {
+    private void validateDuplicatedCar() {
+        Set<Car> uniqueCars = new HashSet<>(cars);
+        if (uniqueCars.size() < cars.size()) {
             throw new IllegalArgumentException(ERROR_CAR_NAME_DUPLICATION);
         }
     }
@@ -16,9 +19,9 @@ public class CompeteCars {
     public CompeteCars(List<String> carNames) {
         for (String carName : carNames) {
             Car car = new Car(carName);
-            validateDuplicatedCar(car);
             cars.add(car);
         }
+        validateDuplicatedCar();
     }
 
     public List<Car> getAll() {
