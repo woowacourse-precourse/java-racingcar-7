2aab4609824dda83d5c807a3e5d30acedc4c6eaa
minSsan <msp1125@hanyang.ac.kr>
2024-10-28 00:03:53 +0900

    test(CarRacingManager): 불필요한 메소드 사용 개선

--- src/test/java/racingcar/domain/service/CarRacingManagerTest.java
+++ src/test/java/racingcar/domain/service/CarRacingManagerTest.java
@@ -2,29 +2,15 @@ package racingcar.domain.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.domain.model.car.Car;
+import racingcar.infrastructure.constant.Boundary;
 import racingcar.infrastructure.constant.CarConstants;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarRacingManagerTest {
-    @Test
-    @DisplayName("자동차를 움직일 수 있다.")
-    void 자동차_움직이기() {
-        // given
-        final CarRacingManager carRacingManager = CarRacingManager.from("1,2,3");
-
-        // when
-        carRacingManager.moveForward("1");
-
-        // then
-        final Car movedCar = carRacingManager.getList().stream()
-                .filter(car -> car.getName().equals("1"))
-                .findFirst()
-                .get();
-        assertThat(movedCar.getDistance()).isEqualTo(1);
-    }
+    private static final int MOVING_FORWARD = Boundary.MOVE_CONDITION_MIN;
+    private static final int STOP = Boundary.MOVE_CONDITION_MIN - 1;
 
     @Test
     @DisplayName("우승자를 출력할 수 있다.")
@@ -32,25 +18,33 @@ class CarRacingManagerTest {
         // given
         final CarRacingManager carRacingManager = CarRacingManager.from("1,2,3");
 
-        // when
-        carRacingManager.moveForward("1");
-
-        // then
-        assertThat(carRacingManager.getWinners()).isEqualTo("1");
+        assertRandomNumberInRangeTest(
+                () -> {
+                    // when
+                    carRacingManager.moveAllCar();
+                    final String winners = carRacingManager.getWinners();
+                    // then
+                    assertThat(winners.split(CarConstants.JOIN_DELIMITER).length).isEqualTo(1);
+                },
+                MOVING_FORWARD, STOP, STOP
+        );
     }
 
     @Test
     @DisplayName("우승자가 여러 명일 수 있다.")
     void 우승자_여러명() {
         // given
-        final CarRacingManager carRacingManager = CarRacingManager.from("우승자1, 우승자2, loser");
-        carRacingManager.moveForward("우승자1");
-        carRacingManager.moveForward("우승자2");
+        final CarRacingManager carRacingManager = CarRacingManager.from("1, 2, 3");
 
-        // when
-        final String winners = carRacingManager.getWinners();
-
-        // then
-        assertThat(winners.split(CarConstants.JOIN_DELIMITER)).containsExactly("우승자1", "우승자2");
+        assertRandomNumberInRangeTest(
+                () -> {
+                    // when
+                    carRacingManager.moveAllCar();
+                    final String winners = carRacingManager.getWinners();
+                    // then
+                    assertThat(winners.split(CarConstants.JOIN_DELIMITER).length).isEqualTo(2);
+                },
+                MOVING_FORWARD, MOVING_FORWARD, STOP
+        );
     }
 }
