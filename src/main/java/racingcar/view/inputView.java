249ae0628d82f9ef84fb4ab9d8b975ea8d58afb1
kaswhy <sxshx818@naver.com>
2024-10-27 15:27:20 +0900

    feat(InputView): 입력을 받는 InputView 클래스 구현
    변경 후
    - 자동차 이름 입력받는  메서드 구현
    - 경주 시도 횟수 입력받는 메서드 구현

---
+++ src/main/java/racingcar/view/inputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class inputView {
+    public static String getCarNames(){
+        return Console.readLine();
+    }
+
+    public static String getRacingNumber() {
+        return Console.readLine();
+    }
+}
