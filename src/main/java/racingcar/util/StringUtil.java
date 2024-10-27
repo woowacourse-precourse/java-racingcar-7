93291046f8266b0de41cfda383323abb5218631f
Su-daa <subin.kk019@gmail.com>
2024-10-27 23:16:45 +0900

    feat: 배열을 ArrayList로 변환하는 기능 구현

--- src/main/java/racingcar/util/StringUtil.java
+++ src/main/java/racingcar/util/StringUtil.java
@@ -1,7 +1,14 @@
 package racingcar.util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StringUtil {
     public static String[] splitByComma(String input) {
         return input.split(",");
+    }
+
+    public static ArrayList<String> toArrayList(String[] strings) {
+        return new ArrayList<>(Arrays.asList(strings));
     }
 }
