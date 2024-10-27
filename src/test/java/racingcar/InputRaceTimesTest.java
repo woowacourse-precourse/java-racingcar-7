0d529d9c20ae16e2934f56319a1d1e3094f3dcc1
kaswhy <sxshx818@naver.com>
2024-10-27 19:50:55 +0900

    test(InputRaceTimesTest): 시도 횟수가 빈 값이면 예외 발생
    변경 후
    - NullAndEmptySource를 이용하여 빈 값인 경우("", null)인 경우 예외 발생 테스트

--- src/test/java/racingcar/InputRaceTimesTest.java
+++ src/test/java/racingcar/InputRaceTimesTest.java
@@ -6,16 +6,13 @@ import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.validator.ValidateRaceTimes;
 
 public class InputRaceTimesTest {
     private static final String RACE_TIMES_ALPHABET = "abc";
     private static final String RACE_TIMES_KOREAN = "가나다";
-    private static final String RACE_TIMES_BLANK = "";
-    private static final String RACE_TIMES_WHITE = " ";
-    private static final String RACE_TIMES_NULL = null;
     private static final String RACE_TIMES_ZERO = "0";
     private static final String RACE_TIMES_NEGATIVE_NUMBER = "-3";
     private static final String RACE_TIMES_REAL_NUMBER = "3.3";
@@ -27,17 +24,26 @@ public class InputRaceTimesTest {
     @ValueSource(strings = {RACE_TIMES_ALPHABET, RACE_TIMES_KOREAN,
             RACE_TIMES_NEGATIVE_NUMBER, RACE_TIMES_REAL_NUMBER})
     public void ThrowExceptionIfRaceTimesContainsCharacters(String raceTimes) {
-        // given
-
-        // when
         Throwable thrown = catchThrowable(() -> {
             validateRaceTimes.containsCharacters(raceTimes);
         });
 
-        // then
         assertThat(thrown)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자 이외의 문자가 포함되어 있습니다.");
+    }
+
+    @DisplayName("시도 횟수가 빈 값이면 예외 발생")
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void ThrowExceptionIfRaceTimesEmpty(String raceTimes) {
+        Throwable thrown = catchThrowable(() -> {
+            validateRaceTimes.isBlank(raceTimes);
+        });
+
+        assertThat(thrown)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도 횟수는 빈 값이 될 수 없습니다.");
     }
 
 }
