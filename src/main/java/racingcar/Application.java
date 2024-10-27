c4878d0f8099bf59644e90708bd91460d20ef14b
LeeJiWon <dlwldnjs1009@gmail.com>
2024-10-27 21:51:48 +0900

    feat: 전체 게임 흐름 제어 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.controller.CarRacingGameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarRacingGameController gameController = new CarRacingGameController();
+        gameController.run();
     }
 }
---
+++ src/main/java/racingcar/controller/CarRacingGameController.java
@@ -0,0 +1,50 @@
+package racingcar.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.WinnerDecision;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class CarRacingGameController {
+
+    private final List<Car> cars = new ArrayList<>();
+    private int attemptCount;
+
+    public void run() {
+        initializeGame();
+        playRounds();
+        showWinners();
+    }
+
+    private void initializeGame() {
+        List<String> carNames = InputView.getCarNames();
+        carNames.forEach(name -> {
+            InputValidator.validateCarName(name);
+            cars.add(Car.create(name));
+        });
+
+        attemptCount = InputView.getAttemptCount();
+        InputValidator.validateAttemptCount(attemptCount);
+    }
+
+    private void playRounds() {
+        for (int round = 0; round < attemptCount; round++) {
+            playOneRound();
+            OutputView.displayRaceProgress(cars);
+        }
+    }
+
+    private void playOneRound() {
+        cars.forEach(car -> {
+            int randomNumber = Car.randomNumberGenerator();
+            car.move(randomNumber);
+        });
+    }
+
+    private void showWinners() {
+        List<Car> winners = WinnerDecision.determineWinners(cars);
+        OutputView.displayWinners(winners);
+    }
+}
