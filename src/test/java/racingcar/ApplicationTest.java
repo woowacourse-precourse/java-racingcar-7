fc1a5f44bf629e24a24636b8595c174f852c743f
sso9594 <sso9594@naver.com>
2024-10-27 17:45:20 +0900

    test(ApplicationTest): 테스트 케이스 추가를 통한 어플리케이션 검증

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -30,6 +31,61 @@ class ApplicationTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    @DisplayName("자동차가 0대 생성 된 경우")
+    void meetsCarCountZero_Then_Exception() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("", "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("자동차의 이름이 5자를 초과할 경우")
+    void meetsCarNameOverFive_Then_Exception() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi,woni,javaji", "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("정상(1대 이상)적으로 생성 될 경우")
+    void meetsCarCountOneOrMore_Then_Success() {
+        assertSimpleTest(() -> {
+            run("pobi", "1");
+            assertThat(output()).contains("pobi : -");
+        });
+    }
+
+    @Test
+    @DisplayName("0번 횟수 동안 반복 할 경우 예외를 던짐")
+    void meetsZeroRound_Then_Exception() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi", "0"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("음수 횟수를 반복 할 경우 예외를 던짐")
+    void meetsMinusRound_Then_Exception() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("반복 횟수에 문자열이 들어 올 경우 예외를 던짐")
+    void meetsStringRound_Then_Exception() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi", "hi"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 
     @Override
     public void runMain() {
