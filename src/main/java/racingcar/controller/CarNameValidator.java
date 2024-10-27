675c07ff18dacef5d1873989f6ae71a2afd4eb74
byegu <rkdqudrn07@naver.com>
2024-10-27 20:53:15 +0900

    fix : 자동차 이름 중복된 경우 예외 추가

--- src/main/java/racingcar/controller/CarNameValidator.java
+++ src/main/java/racingcar/controller/CarNameValidator.java
@@ -2,15 +2,13 @@ package racingcar.controller;
 
 import racingcar.model.Car;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CarNameValidator {
     private static final String EMPTY_INPUT_ERROR_MESSAGE = "자동차 이름이 비어있습니다.";
     private static final String NAME_TOO_LONG_ERROR_MESSAGE = "자동차 이름은 5글자를 초과할 수 없습니다.";
+    private static final String DUPLICATE_NAME_ERROR_MESSAGE = "자동차 이름이 중복되었습니다 : ";
     public List<Car> validate(String carName) {
         if(!isEmpty(carName)) {
             throw new IllegalArgumentException(EMPTY_INPUT_ERROR_MESSAGE);
@@ -24,15 +22,22 @@ public class CarNameValidator {
     }
 
     private List<Car> convert(String inputString) {
+        Set<String> carNameSet = new HashSet<>();
         return Arrays.stream(inputString.split(","))
                 .map(carName -> {
                     String car = carName.trim();
                     if (car.isBlank()) {
                         throw new IllegalArgumentException(EMPTY_INPUT_ERROR_MESSAGE);
                     }
-                    else if (car.length() > 5) {
+
+                    if (car.length() > 5) {
                         throw  new IllegalArgumentException(NAME_TOO_LONG_ERROR_MESSAGE);
                     }
+
+                    if (!carNameSet.add(car)) {
+                        throw new IllegalArgumentException(DUPLICATE_NAME_ERROR_MESSAGE + car);
+                    }
+
                     return new Car(car);
                 }).collect(Collectors.toList());
     }
