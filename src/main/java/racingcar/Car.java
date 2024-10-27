29518e6452e9d84995c33aac0a6e94a3d9dc9ae9
jieunsse <jieunsse@gmail.com>
2024-10-28 00:02:04 +0900

    Refactor(Car) : 자동차 이름의 글자수 한계치를 상수화

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,12 @@
 package racingcar;
 
 public class Car {
-
    private String name;
    private int position;
 
-   public void giveCarName(String name) {
-      if (name.length() > 5) {
+    public void giveCarName(String name) {
+        final int MAXIMUM_LETTERS = 5;
+        if (name.length() > MAXIMUM_LETTERS) {
          throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
       }
       this.name = name;
