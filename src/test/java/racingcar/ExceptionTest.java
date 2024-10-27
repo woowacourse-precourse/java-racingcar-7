bd782fb69247f030ce0b946b57584cb3a6c3db3d
OhYeHyun <ohyhohyl@naver.com>
2024-10-28 01:16:34 +0900

    test: 자동차 이름이 5글자 초과인 경우 에러 발생하는 테스트

---
+++ src/test/java/racingcar/ExceptionTest.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ExceptionTest {
+    private RacingService racingService;
+
+    @Test
+    @DisplayName("자동차 이름이 5글자 초과인 경우 에러 발생")
+    void 자동차_이름_5글자_초과_테스트() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            racingService = new RacingService("aaaaaaa,bbbbbbb,ccccccc,ddddddd", "4");
+            racingService.startRaceGame();
+        });
+    }
+}
