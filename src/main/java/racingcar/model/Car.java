1db91b8facd2f5f64454b0133b54fc7fffe944a2
JeongHyeon Jo <cisxo@naver.com>
2024-10-28 03:16:17 +0900

    feat(model): feat Car model design
     - 차 움직임 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -7,20 +7,31 @@ import racingcar.utils.Validator;
 public class Car {
     private final String CarName;
     private final Engine engine;
+    private int position;
+
     Validator validator = new CarNameValidator();
 
     public Car(String carName, Engine randomEngine) {
         validator.validation(carName);
         this.CarName = carName;
         this.engine = randomEngine;
+        this.position = 0;
     }
 
     public String getCarName() {
         return CarName;
     }
 
-    public boolean getEnginePower() {
-        return engine.isEngineRunning();
+    public boolean move() {
+        if (engine.isEngineRunning()) {
+            position++;
+            return true;
+        }
+        return false;
+    }
+
+    public int getPosition() {
+        return position;
     }
 
 }
