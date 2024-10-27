59cb9e6690c05f67b306a30850f12e8575118b64
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-10-27 21:59:23 +0900

    feat: 자동차 경주 게임 진행 기능 추가 및 입력 처리 변경
    - runRace: 주어진 시도 횟수만큼 경주를 실행하는 메서드 추가
    - playRound: 각 라운드에서 자동차의 전진 여부를 결정하는 기능 추가
    - displayRoundResult: 라운드별 자동차 위치를 출력하는 기능 추가
    - inputCarNames: 자동차 이름 입력 및 Car 객체 리스트 생성 로직으로 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,38 +1,69 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
-    private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가?";
-  
-    private static List<String> inputCarNames() {
-    	System.out.println(INPUT_CAR_NAMES_MESSAGE);
-    	String input = Console.readLine();
-    	
-    	List<String> carNames = new ArrayList<>(Arrays.asList(input.split(",")));
-    	Validator.validateCarNames(carNames);
-    	return carNames;    	
-    }
-    
-    private static int inputTryCount() {
-    	System.out.println(INPUT_TRY_COUNT_MESSAGE);
-    	String input = Console.readLine();
-        
-        Validator.validateNumericInput(input);
-        int tryCount = Integer.parseInt(input);
-        
-        Validator.validatePositiveTryCount(tryCount);
-        
-        return tryCount;
-    			
-    }
-    
-    public static void main(String[] args) {
-    	List<String> carNames = inputCarNames();
-        int tryCount = inputTryCount();
-    }
+	private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가?";
+
+	private static List<Car> inputCarNames() {
+		System.out.println(INPUT_CAR_NAMES_MESSAGE);
+		String input = Console.readLine();
+
+		List<String> carNames = new ArrayList<>(Arrays.asList(input.split(",")));
+		Validator.validateCarNames(carNames);
+
+		List<Car> cars = new ArrayList<>();
+		for (String name : carNames) {
+			cars.add(new Car(name.trim()));
+		}
+
+		return cars;
+	}
+
+	private static int inputTryCount() {
+		System.out.println(INPUT_TRY_COUNT_MESSAGE);
+		String input = Console.readLine();
+
+		Validator.validateNumericInput(input);
+		int tryCount = Integer.parseInt(input);
+
+		Validator.validatePositiveTryCount(tryCount);
+
+		return tryCount;
+
+	}
+
+	private static void runRace(List<Car> cars, int tryCount) {
+		for (int i = 0; i < tryCount; i++) {
+			playRound(cars);
+			displayRoundResult(cars);
+		}
+	}
+
+	private static void playRound(List<Car> cars) {
+		for (Car car : cars) {
+			int randomValue = Randoms.pickNumberInRange(0, 9);
+			car.move(randomValue);
+		}
+	}
+
+	private static void displayRoundResult(List<Car> cars) {
+		for (Car car : cars) {
+			System.out.println(car.displayPosition());
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		List<Car> cars = inputCarNames();
+		int tryCount = inputTryCount();
+
+		runRace(cars, tryCount);
+	}
 }
