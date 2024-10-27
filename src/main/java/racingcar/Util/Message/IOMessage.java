52c77c142f22213a37133dc900282bf32a6e9084
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-28 01:34:22 +0900

    feat(IOMessage): 입출력 관련 문자열 상수화 추가
    - SEPARATOR(" : ") : 출력 시에 구분자로 사용
    - NEW_LINE("\n") : 개행 문자 대신 사용

--- src/main/java/racingcar/Util/Message/IOMessage.java
+++ src/main/java/racingcar/Util/Message/IOMessage.java
@@ -4,6 +4,8 @@ public enum IOMessage {
     INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     INPUT_TRIAL_COUNT("시도할 횟수는 몇 회인가요?"),
     RACING_RESULT("\n실행 결과"),
+    SEPARATOR(" : "),
+    NEW_LINE("\n"),
     RESULT_MESSAGE("최종 우승자 : ");
 
     private final String message;
