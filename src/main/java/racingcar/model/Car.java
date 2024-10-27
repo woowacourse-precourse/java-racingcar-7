5079ebe7e177fff5ae957205ef00b13574ea6546
wgrgwg <kyoeng14697@naver.com>
2024-10-27 18:02:57 +0900

    feat(model): Add Car class
    Added Car class to represent cars as objects.

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,18 @@
+package racingcar.model;
+
+public final class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
