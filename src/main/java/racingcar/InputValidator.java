210d9cd7c2cc56862d279f37c3adc15dc3b077a9
문고은 <>
2024-10-27 16:05:26 +0900

    feat(validator) : 글자 수 예외 처리 기능 추가
    글자 수가 5자가 넘어갈 경우 예외 발생

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -1,15 +1,21 @@
 package racingcar;
 
+import java.util.LinkedHashMap;
+
 public class InputValidator {
     final String inputText;
+    final LinkedHashMap<Car, Integer> splitInputText;
 
     public InputValidator(String inputText) {
         this.inputText = inputText;
+        splitInputText = new InputParser(inputText).splitByComma();
     }
 
     public void carNameValidator() {
         if (isBlankInput()) {
             throw new IllegalArgumentException("경주할 자동차 이름을 입력하세요.");
+        } else if (isCarNameTooLong()) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         }
     }
 
@@ -21,5 +27,14 @@ public class InputValidator {
 
     private boolean isBlankInput() {
         return inputText.isBlank();
+    }
+
+    private boolean isCarNameTooLong() {
+        for (Car car : splitInputText.keySet()) {
+            if (car.getCarName().length() > 5) {
+                return true;
+            }
+        }
+        return false;
     }
 }
