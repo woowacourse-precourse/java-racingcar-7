a2b9f304c19183fe93dbacbcfb4cf8fb4aeb7aa2
문혜교 <3hilney@gmail.com>
2024-10-27 22:57:40 +0900

    feat: Add move method to print car movements

--- src/main/java/racingcar/MoveDecider.java
+++ src/main/java/racingcar/MoveDecider.java
@@ -4,20 +4,43 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class MoveDecider {
 
+
     public static int pickRandomNum (){
-        int randomNum = pickNumberInRange(0, 9);
-        System.out.println(randomNum);
-        return randomNum;
+        //System.out.println(randomNum);
+        return pickNumberInRange(0, 9);
     }
 
     public static String isMovable(int randomNum) {
+
         String moved = "";
         if (randomNum > 3) {
             moved = moved + "-";
         }
-        System.out.println(moved);
+        //System.out.println(moved);
         return moved;
     }
 
+    public static String move() {
+        StringBuilder result = new StringBuilder();
+
+        try {
+            String[] carNames = InputView.inputCar();
+
+            for (String name : carNames) {
+                int randomNum = MoveDecider.pickRandomNum();
+                String moved = MoveDecider.isMovable(randomNum);
+                String movement = name + " : " + moved;
+                System.out.println(movement);
+                result.append(movement).append("\n");
+            }
+
+        } catch (IllegalAccessException e) {
+            System.out.println(e.getMessage());
+            return e.getMessage();
+
+        }
+
+        return result.toString();
+    }
 
 }
