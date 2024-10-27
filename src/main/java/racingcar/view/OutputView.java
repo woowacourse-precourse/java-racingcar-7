6b15ed751fc414dbe6d9eb36f1e1783f5028dcab
JaeGyun-Yoo <dbworbs15@gmail.com>
2024-10-27 19:57:46 +0900

    feat(OutputView): 자동차 경주 상태 출력 기능 구현
    각 자동차의 이름과 이동 거리를 출력하는 함수를 생성한다.
    - 자동차 이름과 현재 위치를 입력받아 출력
    - 위치는 "-" 문자의 개수로 표현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public void printGameStart() {
         System.out.println("\n실행 결과");
     }
+
+    public void printCarStatus(string carName, int position) {
+        System.out.println(name + " : " + "-".repeat(position));
+    }
 }
