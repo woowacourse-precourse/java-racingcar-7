0a402f26c26264e2839fa18e7039ab68f3b55dd6
KJungW <sagakkb0707@naver.com>
2024-10-27 15:35:17 +0900

    test(output): 경주 출력 기능 관련 테스트 추가
    테스트하고자 하는 내용 (what)
    - 경주 진행 상황 출력 기능: 자동차의 진행 상황을 문자열로 생성
    
    테스트 추가 이유 (why)
    - 경주의 진행 상황을 출력할 때, 각 자동차의 한 줄 진행 상황이
       잘 출력되는지 테스트하기 위해 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -3,9 +3,13 @@ package racingcar.model;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import racingcar.test_data_generator.CarGenerator;
 
 class CarTest {
 
@@ -20,6 +24,22 @@ class CarTest {
                     assertThat(car.getMovedDistance()).isEqualTo(expectedMovedDistance);
                 },
                 randomNumber
+        );
+    }
+
+    @DisplayName("자동차 경주 진행 상황 출력 기능 - 자동차의 진행 상황을 문자열로 생성")
+    @ParameterizedTest(name = "\"{1}\" 이 생성되는 경우")
+    @MethodSource()
+    public void 자동차_세부내요_출력(Car car, String expectedCarDetail) {
+        String actualCarDetail = car.makeDetail();
+        assertThat(actualCarDetail).isEqualTo(expectedCarDetail);
+    }
+
+    private static Stream<Arguments> 자동차_세부내요_출력() {
+        return Stream.of(
+                Arguments.of(CarGenerator.makeCar("rc1", 5), "rc1 : -----"),
+                Arguments.of(CarGenerator.makeCar("rc2", 0), "rc2 : "),
+                Arguments.of(CarGenerator.makeCar("rc3", 3), "rc3 : ---")
         );
     }
 }
