b61a771dc4f2cde8103663d4aadff66ab39ddb46
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-27 22:45:35 +0900

    refactor(RacingControllerTests) : 테스트 코드 가독성 개선

--- src/test/java/racingcar/controller/RacingControllerTests.java
+++ src/test/java/racingcar/controller/RacingControllerTests.java
@@ -18,23 +18,6 @@ class RacingControllerTests extends NsTest {
     private static final int STOP = 3;
 
     @Test
-    @DisplayName("실행 결과에서의 이름의 순서가 최대 이동 거리에 관계없이 항상 입력 당시의 순서를 유지하는지 확인")
-    void testNameOrderSameToInputOrder() {
-        List<String> input = List.of("alice", "bob", "john");
-        int inputTrialCount = 1;
-
-        assertRandomNumberInRangeTest(
-                () -> {
-                    RacingResult result = RacingController.startRace(input, inputTrialCount);
-                    OutputDTO output = RacingController.announceResult(result, inputTrialCount);
-                    List<RacingCar> racingResult = output.getResult();
-                    assertThat(racingResult).extracting(RacingCar::getName).containsSequence("alice", "bob", "john");
-                },
-                STOP, MOVING_FORWARD, STOP
-        );
-    }
-
-    @Test
     @DisplayName("이동 가능 횟수가 여러 번일 때, 마지막 실행 결과가 입력한 순서를 유지하면서 올바르게 출력되는지 확인")
     void testRaceResultOrderSameToInputOrder() {
         assertRandomNumberInRangeTest(
@@ -52,16 +35,33 @@ class RacingControllerTests extends NsTest {
     }
 
     @Test
-    @DisplayName("최종 우승자 출력 시, 출력되는 이름의 순서가 입력된 순서를 따르는지 확인")
+    @DisplayName("실행 결과에서 출력되는 이름의 순서가 항상 입력된 순서를 유지하는지 확인")
+    void testNameOrderSameToInputOrder() {
+        List<String> racerNames = List.of("alice", "bob", "john");
+        int totalTrialCount = 1;
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    RacingResult result = RacingController.startRace(racerNames, totalTrialCount);
+                    OutputDTO output = RacingController.announceResult(result, totalTrialCount);
+                    List<RacingCar> racingResult = output.getResult();
+                    assertThat(racingResult).extracting(RacingCar::getName).containsSequence("alice", "bob", "john");
+                },
+                STOP, MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    @DisplayName("최종 우승자를 출력하는 순서가 입력된 순서를 유지하는지 확인")
     void testWinnerNameSameToInputOrder() {
-        List<String> input = List.of("alice", "bob", "john", "paul");
-        int inputTrialCount = 1;
+        List<String> racerNames = List.of("alice", "bob", "john", "paul");
+        int totalTrialCount = 1;
         String expected = "alice, john";
 
         assertRandomNumberInRangeTest(
                 () -> {
-                    RacingResult result = RacingController.startRace(input, inputTrialCount);
-                    OutputDTO output = RacingController.announceResult(result, inputTrialCount);
+                    RacingResult result = RacingController.startRace(racerNames, totalTrialCount);
+                    OutputDTO output = RacingController.announceResult(result, totalTrialCount);
                     assertThat(output.getWinners()).isEqualTo(expected);
                 },
                 MOVING_FORWARD, STOP, MOVING_FORWARD, STOP
