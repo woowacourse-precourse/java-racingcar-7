c408125dd5792dd41df9814408938aea86254bc3
jbilee <julishahn@gmail.com>
2024-10-27 14:55:13 +0900

    test(validator): Validator 관련 테스트 코드 수정
    - ParameterizedTest를 사용해 한 줄의 코드로 여러 인풋값을 테스트하도록 수정

--- src/test/java/racingcar/CarInputValidatorTest.java
+++ src/test/java/racingcar/CarInputValidatorTest.java
@@ -1,7 +1,8 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.validators.CarInputValidator;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -10,11 +11,9 @@ class CarInputValidatorTest {
     private final CarInputValidator validator = new CarInputValidator();
 
     @DisplayName("자동차 이름으로 입력한 값이 유효하지 않으면 예외를 발생시킨다.")
-    @Test
-    void 자동차_입력값_검증_테스트() {
-        assertThrows(IllegalArgumentException.class, () -> validator.validate("car1"));
-        assertThrows(IllegalArgumentException.class, () -> validator.validate("car111,car2"));
-        assertThrows(IllegalArgumentException.class, () -> validator.validate("car,car"));
-        assertThrows(IllegalArgumentException.class, () -> validator.validate("1,2,3,4,5,6,7,8,9,10,11"));
+    @ParameterizedTest
+    @ValueSource(strings = {"car1", "car111,car2", "car,car", "1,2,3,4,5,6,7,8,9,10,11"})
+    void 자동차_입력값_검증_테스트(String input) {
+        assertThrows(IllegalArgumentException.class, () -> validator.validate(input));
     }
 }
--- src/test/java/racingcar/RoundInputValidatorTest.java
+++ src/test/java/racingcar/RoundInputValidatorTest.java
@@ -1,7 +1,8 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.validators.RoundInputValidator;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -10,12 +11,9 @@ class RoundInputValidatorTest {
     private final RoundInputValidator validator = new RoundInputValidator();
 
     @DisplayName("시도 횟수로 입력한 값이 유효하지 않으면 예외를 발생시킨다.")
-    @Test
-    void 횟수_입력값_검증_테스트() {
-        assertThrows(IllegalArgumentException.class, () -> validator.validate("asdf"));
-        assertThrows(IllegalArgumentException.class, () -> validator.validate("0"));
-        assertThrows(IllegalArgumentException.class, () -> validator.validate("-10"));
-        assertThrows(IllegalArgumentException.class, () -> validator.validate("1.23"));
-        assertThrows(IllegalArgumentException.class, () -> validator.validate("301"));
+    @ParameterizedTest
+    @ValueSource(strings = {"asdf", "0", "-10", "1.23", "301"})
+    void 횟수_입력값_검증_테스트(String input) {
+        assertThrows(IllegalArgumentException.class, () -> validator.validate(input));
     }
 }
