849a0e2e0c51f705396c661a99ccaaa0189d6d43
KangGeonyoung <kky6335@gmail.com>
2024-10-27 21:32:07 +0900

    feat(OutputView): 설명 문구 출력 OutputView - printDescription: 실행 결과 출력 전, 설명 문구를 출력하는 메서드

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,6 +7,14 @@ import java.util.List;
 public class OutputView {
 
     /**
+     * 실행 결과 출력 전, 설명 문구 출력
+     */
+    public static void printDescription() {
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
+    /**
      * 게임 1회 실행 결과 출력
      */
     public static void printGameResult(List<Car> cars) {
