dedebb1724fa8992fa08b7b5060b94489a1ab8ed
jungsehui <jungsehui@naver.com>
2024-10-28 02:27:40 +0900

    test(CarNameInputTest.java): 자동차 이름 정상 입력 및 예외 입력 테스트

--- src/test/java/racingcar/view/CarNameInputTest.java
+++ src/test/java/racingcar/view/CarNameInputTest.java
@@ -1,4 +1,37 @@
 package racingcar.view;
 
+import org.junit.jupiter.api.Test;
+import racingcar.Message;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class CarNameInputTest {
+
+    private final InputView inputViewValidator = new InputView();
+
+    @Test
+    void 자동차_이름_정상_입력_테스트() {
+        String inputCarName = "pobi, java";
+        inputViewValidator.validateNameLength(inputCarName);
+    }
+
+    @Test
+    void 자동차_이름_예외_입력_테스트() {
+        String inputCarName = "pobi, javaji";
+        assertThatThrownBy(() -> inputViewValidator.validateNameLength(inputCarName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Message.ILLEGAL_NAME_LENGTH.getMessage());
+    }
+
+    @Test
+    void 자동차_이름_정상_변환_테스트() {
+        String inputCarName = "pobi, java";
+
+        List<String> names = inputViewValidator.changeStringToList(inputCarName);
+
+        assertThat(names).containsExactly("pobi", "java");
+    }
 }
