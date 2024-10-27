973a75b427b41b6dab1d943dc3e714fac8fb5dfb
hwangdaesun <hds650588@gmail.com>
2024-10-27 23:06:32 +0900

    refactor : 메서드명을 더 구체적으로 수정

--- src/main/java/racingcar/model/Name.java
+++ src/main/java/racingcar/model/Name.java
@@ -3,13 +3,14 @@ public class Name {
     private final String name;
     private Name(String name){
         validate(name);
+        validateLength(name);
         this.name = name;
     }
     public static Name from(String name){
         return new Name(name);
     }
 
-    private void validate(String name){
+    private void validateLength(String name){
         if(name.length() > 5){
             throw new IllegalArgumentException("자동차 이름은 5자 이하입니다.");
         }
