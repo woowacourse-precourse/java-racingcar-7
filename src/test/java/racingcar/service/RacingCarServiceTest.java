7f398c404b4d5e7a1b9883c865b8b4b7cc12fbf8
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-10-27 14:16:50 +0900

    test : 레이싱카 전진기능 테스트

---
+++ src/test/java/racingcar/service/RacingCarServiceTest.java
@@ -0,0 +1,18 @@
+package racingcar.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.view.InputView;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RacingCarServiceTest {
+    private RacingCarService racingCarService = new RacingCarService(new InputView());
+
+
+
+}
