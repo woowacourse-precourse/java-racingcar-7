c8ff663e49a1ade78eb4891166c129c80e110295
Byuntil <gugugu2014@jbnu.ac.kr>
2024-10-27 23:33:25 +0900

    test(CarRacingGameTest): 자동차 게임 테스트 추가

--- src/test/java/racingcar/domain/CarRacingGameTest.java
+++ src/test/java/racingcar/domain/CarRacingGameTest.java
@@ -7,26 +7,37 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarRacingGameTest {
-    @Test
-    @DisplayName("차량들이 없으면 false를 반환한다.")
-    void canNotPlay1() {
-        // given
-        List<Car> carList = List.of();
-        Cars cars = Cars.from(carList);
-        CarRacingGame carRacingGame = new CarRacingGame(cars, new RandomNumberGenerator(), 1);
-        // then // when
-        assertThat(carRacingGame.canPlay()).isFalse();
-    }
 
     @Test
     @DisplayName("게임횟수가 0이면 false를 반환한다.")
-    void canNotPlay2() {
+    void canNotPlay() {
         // given
-        List<Car> carList = List.of();
+        List<String> carList = List.of();
         Cars cars = Cars.from(carList);
         CarRacingGame carRacingGame = new CarRacingGame(cars, new RandomNumberGenerator(), 0);
         // then // when
         assertThat(carRacingGame.canPlay()).isFalse();
     }
 
+    @Test
+    @DisplayName("자동차 게임 start() 메서드는 게임이 시작된다.")
+    void carRacingGameStartTest() {
+        // given
+        List<String> carList = List.of("pobi", "woni", "jun");
+        Cars cars = Cars.from(carList);
+
+        CarRacingGame carRacingGame = new CarRacingGame(cars, new TestNumberGenerator(), 1);
+        // when
+        List<Car> carlist = carRacingGame.start();
+
+        // then
+        assertThat(carlist).hasSize(3);
+    }
+
+    static class TestNumberGenerator implements NumberGenerator {
+        @Override
+        public int generate() {
+            return 4;
+        }
+    }
 }
