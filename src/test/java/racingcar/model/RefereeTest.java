29c654869b3a640c8e9574fc74b8d846049f0f18
inhooo00 <chldlsgh0987@naver.com>
2024-10-27 22:32:51 +0900

    test(model): refactor RefereeTest
    refactor RefereeTest:
    - 입력값 검증 테스트 추가.

--- src/test/java/racingcar/model/RefereeTest.java
+++ src/test/java/racingcar/model/RefereeTest.java
@@ -1,6 +1,10 @@
 package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.util.validator.ErrorMessage.EXCEPTION_INT_RANGE;
+import static racingcar.util.validator.ErrorMessage.EXCEPTION_NUMBER_RANGE;
+import static racingcar.util.validator.ErrorMessage.EXCEPTION_NUMERIC;
 
 import java.util.Arrays;
 import java.util.List;
@@ -8,6 +12,33 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RefereeTest {
+
+    @Test
+    @DisplayName("1회 이상 라운드를 입력하지 않으면 예외를 발생시킨다.")
+    void shouldThrowExceptionWhenRoundIsLessThanOne() {
+
+        assertThatThrownBy(() -> new Referee("0"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage(EXCEPTION_NUMBER_RANGE.message);
+    }
+
+    @Test
+    @DisplayName("자연수를 입력하지 않으면 예외를 발생시킨다.")
+    void shouldThrowExceptionWhenRoundIsNotNaturalNumber() {
+
+        assertThatThrownBy(() -> new Referee("1.1"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage(EXCEPTION_NUMERIC.message);
+    }
+
+    @Test
+    @DisplayName("int 범위보다 큰 값을 입력하면 예외를 발생시킨다.")
+    void shouldThrowExceptionWhenRoundExceedsIntRange() {
+
+        assertThatThrownBy(() -> new Referee("33333333333"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage(EXCEPTION_INT_RANGE.message);
+    }
 
     @Test
     @DisplayName("한 명의 우승자를 판단할 수 있다.")
