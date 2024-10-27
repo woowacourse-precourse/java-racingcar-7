1bbad96290df8085de3020fb8e0bc76029194be6
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:59:01 +0900

    :sparkles: feat(TryCount): 실행 횟수가 설정된 범위를 벗어날 경우, 예외를 발생시킨다

--- src/main/java/racingcar/model/TryCount.java
+++ src/main/java/racingcar/model/TryCount.java
@@ -5,6 +5,8 @@ import static racingcar.message.ErrorMessages.TRY_COUNT_NOT_VALID_NUMBER;
 import racingcar.exception.InvalidInputException;
 
 public class TryCount {
+    private static final int MAX_TRY_COUNT = 1000;
+
     private final int tryCount;
 
     public TryCount(String count) {
@@ -22,10 +24,18 @@ public class TryCount {
 
     private void validate(String count) {
         validatIntegerNumber(count);
+        validateValidNumber(count);
     }
 
     private void validatIntegerNumber(String count) {
         if (!count.matches("^\\d+$")) {
+            throw new InvalidInputException(TRY_COUNT_NOT_VALID_NUMBER);
+        }
+    }
+
+    private void validateValidNumber(String count) {
+        long countLong= Long.parseLong(count);
+        if (countLong < 0 || countLong > MAX_TRY_COUNT) {
             throw new InvalidInputException(TRY_COUNT_NOT_VALID_NUMBER);
         }
     }
