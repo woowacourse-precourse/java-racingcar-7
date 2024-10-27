f52bb64153b7f5df9fd17c4892e01f39c8b69a9d
sihyun10 <hyhy200170@gmail.com>
2024-10-27 20:44:26 +0900

    feat(이동횟수 예외처리) : 음수, 숫자 외의 값 입력
    사용자에게 몇 번의 이동을 할 것인지 입력받는데, 그 상황 속에서 발생할 수
    있는 예외들을 처리해주었다. 잘못입력할 경우 사용자에게 알맞은 문구를
    보여주어 프로그램 진행에 있어 도움을 주었다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,10 +2,13 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import java.util.regex.Pattern;
+import racingcar.exception.UserInputException;
 
 public class InputView {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_NUMBER_OF_ATTEMPTS_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("^\\d+$");
 
     public InputView() {
     }
@@ -20,6 +23,23 @@ public class InputView {
     public static int getNumberOfAttempts() {
         System.out.println(INPUT_NUMBER_OF_ATTEMPTS_MESSAGE);
         String inputNumber = Console.readLine();
+        validateNumberOfAttempts(inputNumber);
+
         return Integer.parseInt(inputNumber);
+    }
+
+    private static void validateNumberOfAttempts(String inputNum) {
+        if (inputNum == null || inputNum.isBlank()) {
+            throw new UserInputException("이동횟수를 입력해주세요.");
+        }
+        if (inputNum.equals("0")) {
+            throw new UserInputException("이동횟수는 1번 이상이어야합니다.");
+        }
+        if (inputNum.contains("-")) {
+            throw new UserInputException("이동횟수는 음수가 될 수 없습니다.");
+        }
+        if (!NUMBER_PATTERN.matcher(inputNum).matches()) {
+            throw new UserInputException("이동횟수는 숫자로만 입력가능합니다.");
+        }
     }
 }
