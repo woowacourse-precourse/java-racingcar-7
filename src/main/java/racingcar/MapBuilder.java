d74919c7dc48bfdcbfd6346a1883a74cf4ef6ee3
minitiated <minitiated@gmail.com>
2024-10-27 12:34:17 +0900

    refactor(MapBuilder): add logic to mapCarNamesToForwardCounts()
    add mapping logic to empty mapCarNamesToForwardCounts() method in
    MapBuilder;

--- src/main/java/racingcar/MapBuilder.java
+++ src/main/java/racingcar/MapBuilder.java
@@ -1,11 +1,16 @@
 package racingcar;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapBuilder {
     public void mapCarNamesToForwardCounts(List<String> carNames) {
-//        이 부분은 어떻게 할지 아직 모르겠다.
+        Map<String, Integer> forwardCounter = new TreeMap<>();
+        for (int i = 0; i < carNames.size(); i++) {
+            forwardCounter.put(carNames.get(i), 0);
+        }
     }
 
     public void mapCarNamesToStringForwardLines(List<String> carNames) {
