c51dacdfaa0a82592fc8df13427570ad79981a92
yush <yush2202@gmail.com>
2024-10-28 05:15:54 +0900

    refactor($ OutputMessages): 상수명 변경
    출력 사양에 맞게 출력 메시지 변경

--- src/main/java/racingcar/constants/OutputMessages.java
+++ src/main/java/racingcar/constants/OutputMessages.java
@@ -6,7 +6,7 @@ public class OutputMessages {
 
     }
 
-    public static final String INSERT_RACERS_NAME = "경주할 레이서 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INSERT_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String INSERT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
     public static final String RACING_RESULT = "실행 결과";
     public static final String FINAL_WINNER = "최종 우승자 : ";
