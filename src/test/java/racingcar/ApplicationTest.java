a2db37d6d9fa055a91b6c1ae4bbbda489c809b93
YongLeeCode <yonghwan311@gmail.com>
2024-10-27 21:19:05 +0900

    add test cases

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,6 +3,9 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,45 +24,112 @@ class ApplicationTest extends NsTest {
                 },
                 MOVING_FORWARD, STOP);
     }
-    
-    @Test
-    void 기능_테스트_2명() {
-        assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni, june", "2");
-                assertThat(output()).contains("pobi : -", "woni : --", "june : --", "최종 우승자 : woni, june");
-            },
-            MOVING_FORWARD, MOVING_FORWARD,MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD
-        );
-    }
 
     @Test
-    void 기능_테스트_3명() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("pobi,woni, june", "2");
-                    assertThat(output()).contains("pobi : --", "woni : --", "june : --", "최종 우승자 : pobi, woni, june");
-                },
-                MOVING_FORWARD, MOVING_FORWARD,MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
-        );
-    }
-
-    @Test
-    void 이동_없음() {
+    void 테스트_No_Move() {
         assertRandomNumberInRangeTest(
                 () -> {
                     run("pobi,woni", "1");
-                    assertThat(output()).contains("pobi :", "woni :", "최종 우승자 :");
+                    assertThat(output()).contains("우승자가 없습니다.");
                 },
                 STOP, STOP
         );
     }
 
     @Test
-    void 예외_테스트() {
+    void 테스트_입력_이름_공백_제거와_중복_제거() {
+        // given
+        Input input = new Input();
+        String carNames = "pobi, pobi , pobi, woni";
+        List<String> result = Stream.of(carNames.split(",")).map(String::trim).distinct().toList();
+
+        // when, then
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    void 테스트_중복이름_5개_초과() {
+        // given
+        Input input = new Input();
+        String carNames = "pobi, pobi , pobi, woni, pobi  , june, pobi, pobi,pobi";
+        List<String> result = Stream.of(carNames.split(",")).map(String::trim).distinct().toList();
+
+        // when, then
+        assertThat(result.size()).isEqualTo(3);
+    }
+
+    @Test
+    void 테스트_단수_우승() {
+        // given
+        List<String> carNames = List.of("woni", "pobi");
+        int rounds = 2;
+        RacingGame racingGame = new RacingGame(carNames);
+
+        // when
+        assertRandomNumberInRangeTest(
+                () -> {
+                    for (int round = 1; round <= rounds; round += 1){
+                        racingGame.playNextRound();
+                    }
+                    RoundStatus finalRoundStatus = racingGame.getRoundStatus();
+
+                    // then
+                    CarStatus firstCarStatus = finalRoundStatus.getCarStatusList().get(0);
+                    assertThat(firstCarStatus.getName()).isEqualTo("woni");
+                    assertThat(firstCarStatus.getPosition()).isEqualTo(0);
+
+                    CarStatus secondCardStatus = finalRoundStatus.getCarStatusList().get(1);
+                    assertThat(secondCardStatus.getName()).isEqualTo("pobi");
+                    assertThat(secondCardStatus.getPosition()).isEqualTo(2);
+                },
+                STOP, MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 테스트_다수_우승자() {
+        // given
+        List<String> carNames = List.of("woni", "pobi", "mary");
+        int rounds = 3;
+        RacingGame racingGame = new RacingGame(carNames);
+
+        // when
+        assertRandomNumberInRangeTest(
+                () -> {
+                    for (int round = 1; round <= rounds; round += 1) {
+                        racingGame.playNextRound();
+                    }
+                    RoundStatus finalRoundStatus = racingGame.getRoundStatus();
+
+                    // then
+                    assertThat(finalRoundStatus.getCarStatusList())
+                            .allMatch(it -> it.getPosition() == 3);
+                },
+                MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 예외_테스트_이름_5글자_이상() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_출전_가능_차량_최대_5대() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java,woni,june,bobb,joe", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_최대_10라운드() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java,woni,june", "11"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
