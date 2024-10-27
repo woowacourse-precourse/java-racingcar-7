1e0bbd87da4432c49c070ba1ecb887b1ca2b0b26
Su-daa <subin.kk019@gmail.com>
2024-10-27 23:52:56 +0900

    docs: 필요없는 주석 삭제

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -5,13 +5,15 @@ import racingcar.model.Car;
 import java.util.ArrayList;
 
 public class CarService {
-    private final ArrayList<Car> cars; // 자동차 목록
+    private final ArrayList<Car> cars;
 
     public CarService() {
         this.cars = new ArrayList<>();
     }
 
     public void addCars(ArrayList<Car> newCars) {
-        cars.addAll(newCars); // 새로운 자동차들을 기존 목록에 추가
+        cars.addAll(newCars);
     }
+
+
 }
