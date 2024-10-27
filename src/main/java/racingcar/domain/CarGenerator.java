39355e1e8dee2cad7ab68beeebfb8a13d7129260
dltmddus <tmddus4671@gmail.com>
2024-10-27 11:47:59 +0900

    refactor: SeparatorConstant 클래스의 구분자 상수 적용

--- src/main/java/racingcar/domain/CarGenerator.java
+++ src/main/java/racingcar/domain/CarGenerator.java
@@ -1,15 +1,18 @@
 package racingcar.domain;
 
+import static racingcar.constant.SeparatorConstant.COMMA;
+import static racingcar.constant.SeparatorConstant.EMPTY;
+import static racingcar.constant.SeparatorConstant.SPACE;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CarGenerator {
     public static List<Car> getParticipateCars(String input) {
-        return Arrays.stream(input.replaceAll("\\s+", "")
-                        .split(","))
+        return Arrays.stream(input.replaceAll(SPACE.getSeparator(), EMPTY.getSeparator())
+                        .split(COMMA.getSeparator()))
                 .map(Car::new)
                 .collect(Collectors.toList());
     }
-
 }
