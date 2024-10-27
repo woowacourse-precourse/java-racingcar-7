914dcbf6dcd36994edd6a5ecdc82c2d7f61b74a4
kimdev <kim__dev@hufs.ac.kr>
2024-10-27 16:35:39 +0900

    feat(Car): 자동차를 전진시키는 기능을 추가하라

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,8 +2,14 @@ package racingcar.model;
 
 public class Car {
     private CarName name;
+    private int position;
 
     public Car(String name) {
         this.name = new CarName(name);
+        this.position = 0;
+    }
+
+    public int moveForward() {
+        return ++position;
     }
 }
