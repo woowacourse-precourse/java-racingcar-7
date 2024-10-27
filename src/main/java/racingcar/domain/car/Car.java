66a22b6ec5dceb1a165aa88832fe9071dd8f38c9
gyuoo <yeomky98@naver.com>
2024-10-28 03:00:42 +0900

    feat(car): implement Car class

---
+++ src/main/java/racingcar/domain/car/Car.java
@@ -0,0 +1,27 @@
+package racingcar.domain.car;
+
+public class Car {
+	private final String name;
+	private int position;
+	private final MoveStrategy moveStrategy;
+
+	public Car (String name, MoveStrategy moveStrategy) {
+		this.name = name;
+		this.position = 0;
+		this.moveStrategy = moveStrategy;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void move() {
+		if (moveStrategy.isMovable()) {
+			position++;
+		}
+	}
+}
