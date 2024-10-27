83fba6a8882092de1dab8e37e8f2a6c2b7e6fca5
Goohong Chung <9rrong@uoslife.team>
2024-10-27 17:22:04 +0900

    feat(AttemptCount): `남은 시도 횟수가 0인지 확인`한다.

--- src/main/java/racingcar/model/AttemptCount.java
+++ src/main/java/racingcar/model/AttemptCount.java
@@ -3,10 +3,15 @@ package racingcar.model;
 import java.math.BigInteger;
 
 public class AttemptCount {
+    public static final BigInteger ZERO = BigInteger.ZERO;
     private BigInteger count;
 
     public AttemptCount(String count) {
         this.count = convertToBigInteger(count);
+    }
+
+    public boolean isCountZero() {
+        return count.equals(ZERO);
     }
 
     private BigInteger convertToBigInteger(String count) {
