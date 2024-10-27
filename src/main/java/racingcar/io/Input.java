1f5d22016345ccae9293a36058044b33154e2d33
eeun1225 <eeun9108@gmail.com>
2024-10-28 02:30:20 +0900

    fix: 사용자 입력관련 메서드 static으로 변경 - 메서드 static화로 인스턴스 없이 호출 가능

--- src/main/java/racingcar/io/Input.java
+++ src/main/java/racingcar/io/Input.java
@@ -5,8 +5,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 import static racingcar.validation.ValidateInput.*;
 
 public class Input {
-
-    public String inputCarName(){
+    public static String inputCarName(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String CarName = readLine();
 
@@ -15,7 +14,7 @@ public class Input {
         return CarName;
     }
 
-    public String inputNumberOfRound() {
+    public static String inputNumberOfRound() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String numberOfRounds = readLine();
 
