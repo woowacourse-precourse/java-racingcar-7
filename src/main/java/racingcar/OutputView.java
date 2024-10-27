27ca2b74224f9c3a79e9b2d04903fb25dacbd221
JiEung2 <181757@jnu.ac.kr>
2024-10-28 00:23:09 +0900

    fix(outputView): 최종 우승자 오타 수정

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class OutputView {
     private final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final String ROUND_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
-    private final String FINAL_WINNER_MESSAGE = "최송 우승자 : ";
+    private final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
 
     public void printCarNameInputMessage() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
