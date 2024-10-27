fc20dfd86833600405cfce94685c253466f20e6d
Chanwoo yang <moomsss@naver.com>
2024-10-27 15:18:33 +0900

    refactor: 자동차 이름에 숫자가 들어가도록 리팩토링
    기능요구사항을 다시 읽어보니 이름이 꼭 문자여야 한다는 언급은 없기에 숫자도 허용하도록 변경합니다

--- src/main/java/racingcar/validation/InputValidator.java
+++ src/main/java/racingcar/validation/InputValidator.java
@@ -3,11 +3,11 @@ package racingcar.validation;
 public class InputValidator {
 
     public static boolean isNullOrBlank(String input) {
-        return input.isBlank() || input == null;
+        return input == null || input.isBlank();
     }
 
     public static boolean isValidFormat(String input) {
-        return input.matches("^([a-zA-Z]{1,5})(,[a-zA-Z]{1,5})*$");
+        return input.matches("^([a-zA-Z1-9]{1,5})(,[a-zA-Z1-9]{1,5})*$");
     }
 
     public static boolean isValidNumber(String input) {
