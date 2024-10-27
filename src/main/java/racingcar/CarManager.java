f795895afc5aa8cf2c48f1ed9c130e63bfa9920a
threeony <threeknot1250@cau.ac.kr>
2024-10-27 22:44:06 +0900

    feat: update class 'CarManager'
    * implement moveCars
    * refactor duplicate code

--- src/main/java/racingcar/CarManager.java
+++ src/main/java/racingcar/CarManager.java
@@ -2,32 +2,40 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class CarManager {
-    private List<Car> cars = new ArrayList<>();
+    private final List<Car> cars = new ArrayList<>();
 
     public void addCar(String name, int advance){
         Car car = new Car(name, advance);
         cars.add(car);
     }
 
-    public void moveCars(List<String> participants){
+    public Car findCar(String name){
+        return cars.stream()
+                .filter(car1 -> car1.getName().equals(name))
+                .findAny()
+                .orElse(null);
+    }
 
+    public void moveCars(List<String> participants){
+        for(String name: participants){
+            Car car = findCar(name);
+
+            int randomNo = pickNumberInRange(0, 9);
+            if(randomNo >= 4) car.move();
+        }
     }
 
     public void printState(List<String> participants){
         for(String name: participants){
-            Car car = cars.stream()
-                    .filter(car1 -> car1.getName().equals(name))
-                    .findAny()
-                    .orElse(null);
-            if (car != null) {
-                System.out.print(car.getName() + " : ");
-                for(int i=0; i<car.getAdvance(); i++){
-                    System.out.print("-");
-                }
-                System.out.println();
-            }
+            Car car = findCar(name);
+
+            System.out.print(car.getName() + " : ");
+            IntStream.range(0, car.getAdvance()).mapToObj(i -> "-").forEach(System.out::print);
         }
     }
 }
