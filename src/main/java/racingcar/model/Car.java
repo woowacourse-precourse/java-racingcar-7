17650c6bf0eed3c2327ab1fe1b2aa27ce4a28a84
dk7648 <dk7648@korea.ac.kr>
2024-10-27 16:33:30 +0900

    feat: 자동차 객체 생성

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,10 @@
+package racingcar.model;
+
+public class Car {
+    private String name;
+    private int movedDistance;
+    public Car(String name) {
+        this.name = name;
+        movedDistance = 0;
+    }
+}
