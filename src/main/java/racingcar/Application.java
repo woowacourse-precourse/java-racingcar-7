a5fbc87f462385355afcae0f8c075de820c026cc
llcodingll <ll.o.llwhkr@gmail.com>
2024-10-28 00:45:04 +0900

    프로그램 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            List<String> carNames = InputView.inputCarNames();
+            int rounds = InputView.inputRounds();
+            RacingGame game = new RacingGame(carNames, rounds);
+            game.startRace();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
