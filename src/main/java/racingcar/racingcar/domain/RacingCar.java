7a3c6db66e069e0fe8ef6d40eaf2194462d0df68
EHCTH <lees90896@gmail.com>
2024-10-27 11:56:08 +0900

    feat(RacingCar): 이름 반환, 이동횟수, 이동횟수 증가 기능 추가 * feat: getName() * feat: getMove() * feat: increaseMoveCount()

--- src/main/java/racingcar/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/racingcar/domain/RacingCar.java
@@ -4,17 +4,28 @@ import java.util.Objects;
 
 public class RacingCar {
     private final Name name;
+    private final Move move;
     private RacingCar(Name name) {
         this.name = name;
+        this.move = new Move();
     }
     public static RacingCar of(String name) {
         return new RacingCar(
                 new Name(name)
         );
     }
+    public int getMove() {
+        return move.getMove();
+    }
+    public String getName() {
+        return name.getName();
+    }
+    public void increaseMoveCount() {
+        move.increaseMoveCount();
+    }
     @Override
     public String toString() {
-        return name.toString();
+        return String.format("%s : %s", name, "-".repeat(move.getMove()));
     }
     @Override
     public int hashCode() {
