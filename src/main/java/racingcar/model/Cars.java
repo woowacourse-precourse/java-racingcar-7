c8ef3b0b521038dcab6f5279c1e55eaf05b59fd3
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:41:37 +0900

    :sparkles: feat(Cars): 모든 자동차 정보 설정 및 반환

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,4 +1,14 @@
 package racingcar.model;
 
+import java.util.List;
+
 public class Cars {
+    private final List<Car> cars;
+
+    public Cars(String inputCarNames) {
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
 }
