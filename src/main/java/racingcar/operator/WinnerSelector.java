b612925d075260c3aa1dee7b20c7afaaa1b31936
MinJae00 <exceedmin@gmail.com>
2024-10-28 03:46:19 +0900

    refact: UserObject의 달라진 메소드 명에 따른 호출 메소드 명 변경

--- src/main/java/racingcar/operator/WinnerSelector.java
+++ src/main/java/racingcar/operator/WinnerSelector.java
@@ -3,7 +3,6 @@ package racingcar.operator;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import racingcar.user.SettingGameUser;
 import racingcar.user.UserObject;
 
 public class WinnerSelector {
@@ -11,16 +10,16 @@ public class WinnerSelector {
 
     public List<String> getTopScores() {
         int maxScore = Arrays.stream(user)
-                .mapToInt(UserObject::getUserScore)
+                .mapToInt(UserObject::getUserScoreNum)
                 .max()
                 .orElse(Integer.MIN_VALUE);
         return Arrays.stream(user)
-                .filter(u -> u.getUserScore() == maxScore)
+                .filter(u -> u.getUserScoreNum() == maxScore)
                 .map(UserObject::getUserName)
                 .collect(Collectors.toList());
     }
 
-    public WinnerSelector() {
-        this.user = SettingGameUser.user;
+    public WinnerSelector(UserObject[] user) {
+        this.user = user;
     }
 }
