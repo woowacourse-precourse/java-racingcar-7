d5cefaf81370eca2c2c7ba84725564340a05a900
DSRN0000 <dsrn0000@gmail.com>
2024-10-28 00:44:05 +0900

    (feat) 이름 입력에서 이름 5글자 초과, 시도할 횟수에서 음수 입력, 숫자가 아닌 입력에 대한 예외처리 기능 개발

--- src/main/java/racingcar/view/InputUser.java
+++ src/main/java/racingcar/view/InputUser.java
@@ -8,14 +8,35 @@ public class InputUser {
 
     public List<String> inputNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-
-        return List.of(Console.readLine().split(","));
+        List<String> names = List.of(Console.readLine().split(","));
+        nameLengthValidation(names);
+        return names;
     }
 
     public int inputNumber() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        String S = Console.readLine();
+        String number = Console.readLine();
+        isNumber(number);
+        return Integer.parseInt(number);
+    }
 
-        return Integer.parseInt(S);
+    private void nameLengthValidation(List<String> names) {
+        for(String name : names) {
+            if (5 < name.length()) {
+                throw new IllegalArgumentException("이름은 5글자 이하로 입력해주세요.");
+            }
+        }
+    }
+
+    private int isNumber(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            if (number < 1) {
+                throw  new IllegalArgumentException("1 이상의 수를 입력해주세요.");
+            }
+            return number;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
     }
 }
