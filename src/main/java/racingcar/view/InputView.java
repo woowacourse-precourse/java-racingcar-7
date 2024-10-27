b118c1c13f7e0183a3d0720916033edc0944fc78
sihyun10 <hyhy200170@gmail.com>
2024-10-27 17:23:58 +0900

    feat(이동 횟수 입력받기) : int타입으로 변환 후 이동횟수 값 반환
    사용자에게 몇번의 이동을 시도할지 입력받는다. 이때 string타입의 숫자를
    입력받으므로 int타입으로 변환해준다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class InputView {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_NUMBER_OF_ATTEMPTS_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
     public InputView() {
     }
@@ -14,5 +15,11 @@ public class InputView {
         String inputCarNames = Console.readLine();
         List<String> carNames = List.of(inputCarNames.split(","));
         return carNames;
+    }
+
+    public static int getNumberOfAttempts() {
+        System.out.println(INPUT_NUMBER_OF_ATTEMPTS_MESSAGE);
+        String inputNumber = Console.readLine();
+        return Integer.parseInt(inputNumber);
     }
 }
