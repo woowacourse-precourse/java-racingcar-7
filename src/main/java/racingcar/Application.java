9c7d8e71e4acfc1971b981dc8feb86bc1dddf80f
jiyun <tomatoziiilll@gmail.com>
2024-10-27 21:35:46 +0900

    feat: 자동차 전진 추가
    자동차들이 저장되어있는 Map 자료구조 순회

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,13 +6,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();
-        carNames = (carNames == null) ? "" : carNames;
+        if (carNames == null) {
+            carNames = "";
+        }
         carNames = carNames.trim();
 
         Map<String, StringBuilder> racingStatus = new HashMap<>();
@@ -27,13 +30,22 @@ public class Application {
 
         System.out.println("시도 할 횟수 몇 회인가요?");
         String round = Console.readLine();
-        round = (round == null) ? "" : round;
+        if (round == null) {
+            round = "";
+        }
         round = round.trim();
         if (round.isEmpty() || !round.matches("\\d+")) {
             throw new IllegalArgumentException("유효하지 않은 숫자 입니다");
         }
 
-        System.out.println(racingStatus);
-        System.out.println(round);
+
+        int roundToInt = Integer.parseInt(round);
+        for(int i = 0; i < roundToInt; i++) {
+
+            for (String key : racingStatus.keySet()) {
+                if (Randoms.pickNumberInRange(0, 9) >= 4)
+                    racingStatus.get(key).append("-");
+            }
+        }
     }
 }
