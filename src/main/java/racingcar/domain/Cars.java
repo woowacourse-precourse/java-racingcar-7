7f2aa1f77b0739994f59df0533d10485074da19a
dltmddus <tmddus4671@gmail.com>
2024-10-27 10:31:18 +0900

    feat: 최종 우승자 이름 추출 함수 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -4,6 +4,7 @@ import static racingcar.view.OutputView.printCurrentResult;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.util.RandomGeneratorUtil;
 
 public class Cars {
@@ -33,6 +34,10 @@ public class Cars {
                 .max(Comparator.comparingInt(car -> car.movementOfNumber)).map(car -> car.movementOfNumber).orElse(0);
     }
 
+    public List<String> calculateFinalWinner() {
+        int maxMovement = findMaxMovement();
+        return cars.stream().filter(car -> car.movementOfNumber == maxMovement).map(carMovement -> carMovement.name)
+                .collect(Collectors.toList());
 
     }
 }
