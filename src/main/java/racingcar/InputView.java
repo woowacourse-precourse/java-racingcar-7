3fca3c7b8fe6f04681cb08e24471f924bc7dd88b
threepebbles <threepebbles@naver.com>
2024-10-28 05:57:56 +0900

    feat: 사용자 입력에 대한 검증 기능 추가

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -10,19 +10,66 @@ public class InputView {
     public List<String> enterCarNames() {
         System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         System.out.print(LINE_SEPARATOR);
-        String input = Console.readLine();
-        return parseCarNames(input);
-    }
+        String userInput = Console.readLine();
 
-    public int enterRoundCount() {
-        System.out.print("시도할 횟수는 몇 회인가요?");
-        System.out.print(LINE_SEPARATOR);
-        String input = Console.readLine();
-        return Integer.parseInt(input);
+        List<String> carNames = parseCarNames(userInput);
+        validateCarNames(carNames);
+        return carNames;
     }
 
     private List<String> parseCarNames(String carNames) {
         return Arrays.stream(carNames.split(","))
                 .toList();
+    }
+
+    private void validateCarNames(List<String> carNames) {
+        for (String carName : carNames) {
+            validateCarNameLength(carName);
+        }
+        validateDuplicates(carNames);
+    }
+
+    private void validateCarNameLength(String carName) {
+        if (carName.length() < 1 || carName.length() > 5) {
+            throw new IllegalArgumentException("이름의 길이는 1글자 이상 5글자 이하만 가능합니다.");
+        }
+    }
+
+    private void validateDuplicates(List<String> carNames) {
+        if (carNames.stream()
+                .distinct()
+                .count() != carNames.size()) {
+            throw new IllegalArgumentException("중복된 자동차 이름이 있습니다.");
+        }
+    }
+
+    public int enterRoundCount() {
+        System.out.print("시도할 횟수는 몇 회인가요?");
+        System.out.print(LINE_SEPARATOR);
+        String userInput = Console.readLine();
+        validateRoundCount(userInput);
+
+        int roundCount = Integer.parseInt(userInput);
+        return roundCount;
+    }
+
+    private void validateRoundCount(String userInput) {
+        validateInteger(userInput);
+        int roundCount = Integer.parseInt(userInput);
+        validateRoundCountRange(roundCount);
+    }
+
+    private void validateInteger(String maybeInteger) {
+        try {
+            Integer.parseInt(maybeInteger);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("정수를 입력해주세요.");
+        }
+    }
+
+    private void validateRoundCountRange(int roundCount) {
+        if (roundCount < 0 || roundCount > 100_0000) {
+            throw new IllegalArgumentException("1이상 100만 이하의 숫자를 입력해주세요.");
+        }
     }
 }
