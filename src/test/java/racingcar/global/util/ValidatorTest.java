2535b93f71a79c3df8abf25343605ae0c3b65cc0
633jinn <judyg00dy68@gmail.com>
2024-10-27 19:57:06 +0900

    style: 사용하지 않는 import 삭제

--- src/test/java/racingcar/global/util/ValidatorTest.java
+++ src/test/java/racingcar/global/util/ValidatorTest.java
@@ -1,16 +1,12 @@
 package racingcar.global.util;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ValidatorTest {
     static Validator validator;
