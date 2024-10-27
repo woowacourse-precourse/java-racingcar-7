c6e329a174aabf4aa5152d3ba1845ffdaf8f25be
Seol_JY <wlsdud5654@gmail.com>
2024-10-27 10:10:44 +0900

    refactor(CarTest): CarTest 가 테스트용 Strategy 를 사용하도록 변경

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -4,24 +4,12 @@ import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import org.junit.jupiter.api.Test;
 import racingcar.strategy.MovingStrategy;
+import racingcar.support.AlwaysMovingStrategy;
+import racingcar.support.NeverMovingStrategy;
 import racingcar.vo.Name;
 import racingcar.vo.Position;
 
 class CarTest {
-
-    private static class AlwaysMoveStrategy implements MovingStrategy {
-        @Override
-        public boolean shouldMove() {
-            return true;
-        }
-    }
-
-    private static class NeverMoveStrategy implements MovingStrategy {
-        @Override
-        public boolean shouldMove() {
-            return false;
-        }
-    }
 
     @Test
     void 이름으로_자동차를_생성한다() {
@@ -42,7 +30,7 @@ class CarTest {
     void 이동_조건이_참이면_전진한다() {
         // given
         Car car = Car.withName(Name.from("pobi"));
-        MovingStrategy alwaysMove = new AlwaysMoveStrategy();
+        MovingStrategy alwaysMove = new AlwaysMovingStrategy();
 
         // when
         car.attemptMove(alwaysMove);
@@ -58,7 +46,7 @@ class CarTest {
     void 이동_조건이_거짓이면_정지한다() {
         // given
         Car car = Car.withName(Name.from("pobi"));
-        MovingStrategy neverMove = new NeverMoveStrategy();
+        MovingStrategy neverMove = new NeverMovingStrategy();
         Position initialPosition = car.getPosition();
 
         // when
@@ -75,8 +63,8 @@ class CarTest {
     void 여러번_이동을_시도한다() {
         // given
         Car car = Car.withName(Name.from("pobi"));
-        MovingStrategy alwaysMove = new AlwaysMoveStrategy();
-        MovingStrategy neverMove = new NeverMoveStrategy();
+        MovingStrategy alwaysMove = new AlwaysMovingStrategy();
+        MovingStrategy neverMove = new NeverMovingStrategy();
 
         // when
         car.attemptMove(alwaysMove);  // 1
@@ -97,7 +85,7 @@ class CarTest {
         Car car1 = Car.withName(Name.from("car1"));
         Car car2 = Car.withName(Name.from("car2"));
         Car car3 = Car.withName(Name.from("car3"));
-        MovingStrategy alwaysMove = new AlwaysMoveStrategy();
+        MovingStrategy alwaysMove = new AlwaysMovingStrategy();
 
         car2.attemptMove(alwaysMove);
         car3.attemptMove(alwaysMove);
@@ -145,7 +133,7 @@ class CarTest {
     void 자동차는_이동_전략에_따라_한_칸씩만_이동한다() {
         // given
         Car car = Car.withName(Name.from("pobi"));
-        MovingStrategy alwaysMove = new AlwaysMoveStrategy();
+        MovingStrategy alwaysMove = new AlwaysMovingStrategy();
 
         // when
         car.attemptMove(alwaysMove);
