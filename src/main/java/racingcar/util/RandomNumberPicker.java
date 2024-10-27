6f97634b9c03be9d5e978f282f5f59830dc3aed4
jae woo Jung <awfjol2008@gmail.com>
2024-10-27 19:53:53 +0900

    refactor: 난수 생성기 내부의 상수 제거

--- src/main/java/racingcar/util/RandomNumberPicker.java
+++ src/main/java/racingcar/util/RandomNumberPicker.java
@@ -3,14 +3,11 @@ package racingcar.util;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberPicker {
-	private static final int START_INCLUSIVE = 0;
-	private static final int END_INCLUSIVE = 9;
-
 	private RandomNumberPicker() {
 
 	}
 
 	public static int pickNumber() {
-		return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+		return Randoms.pickNumberInRange(0, 9);
 	}
 }
