c0313e5bbf8159575751c0affda8523175f549a8
seophohoho <seop0937@gmail.com>
2024-10-27 21:24:02 +0900

    feat(Race): 생성자 추가

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -4,7 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Race {
+    private final Validator validator;
+    private final Judge judge;
     private List<Car> carList = new ArrayList<>();
+
+    public Race() {
+        this.validator = new Validator();
+        this.judge = new Judge();
+    }
 
     public void setup(String inputCars) {
         String[] strCars = separateInputCars(inputCars);
