f5441708c39cf2febfc3c801e5b60cc904f0ce0c
dia218 <sang218@kyonggi.ac.kr>
2024-10-28 01:11:28 +0900

    feat(getWinner): Implement final winner
    - Calculate the maximum distance
    - Identify the cars of the maximum distance
    - additionally improvements and fixes

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,6 @@
 package racingcar;
 
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Application {
     static Map<String, Integer> carList = new LinkedHashMap<>(); //name, distance
@@ -11,18 +9,32 @@ public class Application {
     public static void main(String[] args) {
         init();
         moveCar();
-
+        getWinner();
     }
     static void init() { //자동차 이름 n개, 턴 수 입력받기
-        String input = camp.nextstep.edu.missionutils.Console.readLine();
-        String[] nameArray = input.split(",");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        try {
+            String input1 = camp.nextstep.edu.missionutils.Console.readLine();
+            String[] nameArray = input1.split(",");
+            checkName(nameArray);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        try {
+            String input2 = camp.nextstep.edu.missionutils.Console.readLine();
+            turn = Integer.parseInt(input2);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+    static void checkName(String[] nameArray) {
         for(int i = 0; i < nameArray.length; i++) {
             if(nameArray[i].length() > 5) { throw new IllegalArgumentException(); }
             else { carList.put(nameArray[i], 0); }
         }
-
-        input = camp.nextstep.edu.missionutils.Console.readLine();
-        turn = Integer.parseInt(input);
     }
     static void moveCar() {
         for(int t = 0 ; t < turn; t++) {
@@ -48,5 +60,23 @@ public class Application {
             System.out.print("-");
             currentMove++;
         }
+    }
+    static void getWinner() {
+        List<String> winners = new ArrayList<>();
+        int maxDistance = 0;
+
+        for (Map.Entry<String, Integer> entry : carList.entrySet()) {
+            if (entry.getValue() > maxDistance) {
+                maxDistance = entry.getValue();
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : carList.entrySet()) {
+            if (entry.getValue() == maxDistance) {
+                winners.add(entry.getKey());
+            }
+        }
+
+        System.out.print("최종 우승자 : " + String.join(", ",winners));
     }
 }
