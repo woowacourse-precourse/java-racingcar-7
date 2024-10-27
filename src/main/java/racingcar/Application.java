3fe13e809d4c6767063d2946b41ad093d8457a17
송지웅 <thdwldnd7@dgu.ac.kr>
2024-10-28 04:38:14 +0900

    feat: 유저 입력 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,32 @@
 package racingcar;
 
 
+import java.util.Arrays;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
+    public static void showInputCarNameDialogue() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+    public static List<String> getCarNames() {
+        String names = Console.readLine().trim();
+        return Arrays.stream(names.split(",")).toList();
+    }
+    public static void showInputNumberOfTurnDialogue() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+    public static int getNumberOfTurn() {
+        String number = Console.readLine().trim();
+        return Integer.parseInt(number);
+    }
     public static void main(String[] args) {
-        CarGame game = GameFactory.of(List.of("1", "2", "3"), 5);
+        showInputCarNameDialogue();
+        List<String> names = getCarNames();
+        showInputNumberOfTurnDialogue();
+        int numberOfTurn = getNumberOfTurn();
+        CarGame game = GameFactory.of(names, numberOfTurn);
         game.play();
     }
 }
