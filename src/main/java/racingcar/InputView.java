a1622775dfa0fe228fa59d13dd447d4d3e10fd65
JiEung2 <181757@jnu.ac.kr>
2024-10-27 21:10:02 +0900

    feat(inputView): 구분자 ","를 기준으로 파싱하는 메서드 추가
    - 파싱 메서드 추가
    - inputCarNames() 메서드에서 해당 메서드 실행

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -2,12 +2,21 @@ package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.*;
 
+import java.util.List;
+
 public class InputView {
-    public String inputCarNames() {
-        return readLine();
+    private final String CAR_NAME_DELIMITER = ",";
+
+    public List<String> inputCarNames() {
+        String carNames = readLine();
+        return parseCarNames(carNames);
     }
 
     public int inputRoundCount() {
         return Integer.parseInt(readLine());
+    }
+
+    private List<String> parseCarNames(String carNames) {
+        return List.of(carNames.split(CAR_NAME_DELIMITER));
     }
 }
