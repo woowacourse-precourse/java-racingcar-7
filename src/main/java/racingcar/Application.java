8290ed50eb28f9f98ef008e1d67ab8907d607e96
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-10-27 20:40:03 +0900

    feat(Application) : 1.3 경주할 자동차 목록이 형식에 맞지 않으면 예외 발생시키는 기능 구현
    - 분리한 String 배열에 길이가 5를 초과하는 문자열이 존재하면, IllegalArgumentException 발생

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,11 +3,15 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+
     public static void main(String[] args) {
         // 1.1 경주할 자동차 목록을 입력받는 기능
         String carNamesInputString = getCarNamesInputString();
         // 1.2 경주할 자동차 목록을 쉼표(,)를 기준으로 구분하는 기능
         String[] carNames = getCarNames(carNamesInputString);
+        // 1.3 경주할 자동차 목록이 형식에 맞지 않으면 예외를 발생시키는 기능
+        validateCarNames(carNames);
     }
 
     public static String getCarNamesInputString() {
@@ -19,4 +23,11 @@ public class Application {
         return carNamesInputString.split(",");
     }
 
+    public static void validateCarNames(String[] carNames) {
+        for (String carName : carNames) {
+            if (carName.length() > MAX_CAR_NAME_LENGTH) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
