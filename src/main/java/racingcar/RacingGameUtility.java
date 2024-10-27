2e9d6ab974d628b3e816542e153374877e739b16
mirinae700 <mirinae219@gmail.com>
2024-10-28 00:14:29 +0900

    feat(gameutility) : 자동차 이름 분리 및 유효성 검사 메서드 추가
    checkNameLength 에서 이름이 유효한 길이(1~5자)인지 확인합니다
    splitStringToArray 에서 사용자 입력 값을 구분자(,)에따라 분리하여 이름 배열을 반환합니다

--- src/main/java/racingcar/RacingGameUtility.java
+++ src/main/java/racingcar/RacingGameUtility.java
@@ -1,6 +1,9 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingGameUtility {
 
@@ -8,5 +11,19 @@ public class RacingGameUtility {
         if(Randoms.pickNumberInRange(0, 9) >= 4) {
             car.raiseMoveCount();
         }
+    }
+
+    public static void checkNameLength(String input) {
+        if(input.isEmpty() || input.length() > 5) {
+            throw new IllegalArgumentException(input + " ::: 이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    public static List<String> splitStringToArray(String input, String delimiter) {
+        String[] list = input.split(delimiter);
+
+        Arrays.stream(list).forEach(RacingGameUtility::checkNameLength);
+
+        return Arrays.stream(list).collect(Collectors.toList());
     }
 }
