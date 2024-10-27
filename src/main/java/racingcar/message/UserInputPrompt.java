8154f04ed385f8a8c6140407c795109c9756d829
신관규 <soundbar91@gmail.com>
2024-10-27 20:37:37 +0900

    chore: final 키워드 추가

--- src/main/java/racingcar/message/UserInputPrompt.java
+++ src/main/java/racingcar/message/UserInputPrompt.java
@@ -1,6 +1,6 @@
 package racingcar.message;
 
 public class UserInputPrompt {
-    public static String CAR_NAME_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public static String TRY_COUNT_PROMPT = "시도할 횟수는 몇 회인가요?";
+    public static final String CAR_NAME_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String TRY_COUNT_PROMPT = "시도할 횟수는 몇 회인가요?";
 }
