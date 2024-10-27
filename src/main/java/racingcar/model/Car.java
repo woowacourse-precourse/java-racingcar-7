1136524778b0e98485fba9864ed26b1c26c60102
Yunsu4 <ysa020428@gmail.com>
2024-10-27 14:51:39 +0900

    style: add a blank line after the declaration of a function for readability

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -8,12 +8,14 @@ public class Car {
     private int moveCount = 0;
 
     public Car(String carName, Integer moveCount) {
+
         CarNameValidator.validate(carName);
         this.carName = carName;
         this.moveCount = moveCount;
     }
 
     public Car(String carName) {
+
         CarNameValidator.validate(carName);
         this.carName = carName;
     }
