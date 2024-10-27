84d9d324d7275894a447866c0bc68fd656b2ad04
young-in <hardworkingini@gmail.com>
2024-10-27 23:23:52 +0900

    feat : 경주 게임을 위한 controller 구현

---
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -0,0 +1,13 @@
+package racingcar.controller;
+
+import racingcar.view.InformationView;
+import racingcar.view.InputView;
+
+public class RacingGameController {
+    private InformationView informationView = new InformationView();
+    private InputView inputView = new InputView();
+
+    public void playGame() {
+
+    }
+}
