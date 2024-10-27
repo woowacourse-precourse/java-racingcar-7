46df5db81632523921c83347ba987d54b1724d4a
fbehddn <tom0611@naver.com>
2024-10-27 16:45:04 +0900

    feat: 자동차 이름 공백 검증 기능 구현

--- src/main/java/racingcar/validation/InputValidator.java
+++ src/main/java/racingcar/validation/InputValidator.java
@@ -6,11 +6,22 @@ public class InputValidator {
     private InputValidator() {
     }
 
-    public static void validateName(String[] inputNames) {
+    public static void validateNameLength(String[] inputNames) {
         for (String inputName : inputNames) {
             if (inputName.length() > MAX_NAME_LENGTH) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력해주세요.");
             }
         }
     }
+
+    public static void validateNameEmpty(String[] inputNames) {
+        if (inputNames.length > 0) {
+            String lastName = inputNames[inputNames.length - 1];
+
+            if (lastName.trim().isEmpty()) {
+                throw new IllegalArgumentException("이름 입력이 공백일 수 없습니다.");
+            }
+        }
+    }
+    
 }
