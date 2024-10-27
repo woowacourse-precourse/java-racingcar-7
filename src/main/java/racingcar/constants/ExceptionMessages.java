69e1f77eba314e3f88f364cf9fa56b76ec5a74ba
이창근 <dlckdrms0517@gmail.com>
2024-10-28 00:52:27 +0900

    feat: Constants 개발
    - 여러 상수 문자열들을 관리합니다.
    - 용도에 따라 Exception, Input, Output, String으로 분리됩니다.

---
+++ src/main/java/racingcar/constants/ExceptionMessages.java
@@ -0,0 +1,13 @@
+package racingcar.constants;
+
+public class ExceptionMessages {
+
+    public static final String CAR_NAME_LENGTH_EXCEED = "자동차 이름은 5자 이하여야 합니다.";
+    public static final String CAR_NAME_EMPTY = "자동차 이름은 비어있을 수 없습니다.";
+    public static final String LAP_COUNT_LACK = "시도는 1회 이상이어야 합니다.";
+    public static final String ILLEGAL_INPUT = "잘못된 입력입니다.";
+    public static final String ILLEGAL_NUMBER_FORMAT = "잘못된 숫자 형식입니다.";
+    public static final String RACE_RESULT_NOT_EXIST = "경주 결과가 존재하지 않습니다.";
+    public static final String LAP_RESULT_NOT_EXIST = "랩 결과가 존재하지 않습니다.";
+    public static final String WINNER_NOT_EXIST = "최종 우승자가 존재하지 않습니다.";
+}
---
+++ src/main/java/racingcar/constants/OutputMessages.java
@@ -0,0 +1,7 @@
+package racingcar.constants;
+
+public class OutputMessages {
+
+    public static final String EXECUTION_RESULT = "실행 결과";
+    public static final String FINAL_WINNER = "최종 우승자 : ";
+}
