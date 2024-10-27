0597407a0cbd2f0d7ceadd1aea7c4fa8ca52b7b6
장영재 <urinaner@naver.com>
2024-10-28 01:03:58 +0900

    랜덤 숫자 기반 move 로직 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 public class Car {
+    private static final int MOVED_BOUND = 4;
     private String name;
     private int position;
 
@@ -9,8 +10,10 @@ public class Car {
         this.position = 0;
     }
 
-    public void move() {
-        position++;
+    public void move(int randomNumber) {
+        if (randomNumber >= MOVED_BOUND) {
+            position++;
+        }
     }
 
     public int getPosition() {
