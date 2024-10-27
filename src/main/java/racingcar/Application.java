4893a60bd01479c514dbc887e07943cf750b66b3
JiHyeonSu <itip1@mju.ac.kr>
2024-10-28 05:38:45 +0900

    feat(Application): 프로그램 실행 구현 -main메소드에 Controller객체를 생성하고 startGame을 통해 게임을 시작할 수 있다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.presentation.RacingController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingController racingController = new RacingController();
+        racingController.startGame();
     }
 }
