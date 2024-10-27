8810dd59286a2fc9aaf68f960d6cd6fb42049aac
Byuntil <gugugu2014@jbnu.ac.kr>
2024-10-27 23:54:32 +0900

    feat(Application): main 함수 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.controller.CarRacingController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarRacingController carRacingController = new CarRacingController(new InputView(), new OutputView());
+        carRacingController.run();
     }
 }
