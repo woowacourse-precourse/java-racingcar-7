8d613006e52a8d4c50118fb138d0ad2a17bbe7a3
yuntasha <jaljayo85@gmail.com>
2024-10-27 20:11:07 +0900

    feat(GameController): write GameController
    Add method list:
    - run
    - initGame
    - addPlayers
    - progress
    - endGame

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.factory.CarRacingFactory;
+import racingcar.validate.Validator;
+import racingcar.view.PrintHelper;
+import racingcar.view.ReaderHelper;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameController gameController = new GameController(new ReaderHelper(), new PrintHelper(),
+                new Validator(), new CarRacingFactory());
+        gameController.run();
     }
 }
