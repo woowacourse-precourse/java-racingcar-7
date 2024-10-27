86a0bc0d9d9c9f1585f724dc8de38748379827f3
SongMinhyuk <dnfldpden32@gmail.com>
2024-10-27 21:27:25 +0900

    feat: Game userInput 에러 처리 구현
    1. 5글자 이하가 아닌경우
    2. 공백
    3. 쉼표만 입력

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -12,11 +12,8 @@ import java.util.Set;
 
 
 public class Game {
-    //TODO:
-    // 1. `시도할 횟수` 마다 아래를 반복
-    //    2. 각 자동차에 대해 해당 로직을 적용한다
-    //    3. `pickNumberInRange()`값이 4이상 인지 판단
-    //        - 4이상이라면 1회 전진
+    //게임이 관심있어 하는것?
+
     private final Map<String, Integer> playerScoreBoard;
     private final Integer totalTurn;
 
@@ -26,13 +23,29 @@ public class Game {
         totalTurn=getTurnFromInput();
     }
 
+
     private void mapPlayerNamesFromInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String userInput=Console.readLine();
         //TODO: 추후 예외처리 필요
+        //아무값 없음,공백
+        if(userInput.isBlank()|| userInput.isEmpty()) {
+            throw new IllegalArgumentException("플레이어 이름이 입력되지 않았습니다");
+        }
         List<String> splitted= List.of(userInput.split(","));
         for (String player:splitted){
+            //1. 5글자 이하 [o]
+            if(player.length()>5){
+                throw new IllegalArgumentException("5글자 이하만 가능합니다");
+            }
             playerScoreBoard.put(player,0);
+        }
+        //2. 중복된 이름X
+        if(playerScoreBoard.size()!= splitted.size()){
+            throw new IllegalArgumentException("중복된 이름은 허용하지 않습니다.");
+        }
+        if(splitted.isEmpty()){
+            throw new IllegalArgumentException("플레이어 이름이 입력되지 않았습니다");
         }
     }
 
