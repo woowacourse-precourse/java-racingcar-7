e6d1db27ff42283363c1a2c86f5a865adac3cec0
jongsic17 <qkrwhdtlr178@naver.com>
2024-10-28 08:31:38 +0900

    refactor: replace HashMap with LinkedHashMap

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,64 +1,71 @@
 package racingcar;
 
 import java.util.*;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        HashMap<String, Integer> cars = new HashMap<>();
+        LinkedHashMap<String, Integer> cars = new LinkedHashMap<>();
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] carNameList = readLine().split(",");
         validateCarNames(carNameList);
-        for(String carName : carNameList) {
+        for (String carName : carNameList) {
             cars.put(carName, 0);
         }
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         int count = Integer.parseInt(readLine());
 
-        for(int i=0; i < count; i++) {
+        for (int i = 0; i < count; i++) {
             randomMoveCars(cars);
             printRoundResults(cars);
             System.out.println();
         }
         printWinner(cars);
     }
+
     public static void validateCarNames(String[] carNames) {
-        for(String carName : carNames) {
-            if(carName.length() > 5){
-                throw new IllegalArgumentException(ExceptionCode.NAME_LENGTH_EXCEEDED.getMessage() );
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException(ExceptionCode.NAME_LENGTH_EXCEEDED.getMessage());
             }
         }
     }
-    public static void printRoundResults(HashMap<String, Integer> cars ) {
-        for(String carName : cars.keySet()) {
+
+    public static void printRoundResults(LinkedHashMap<String, Integer> cars) {
+        for (String carName : cars.keySet()) {
             String travelDistance = "";
-            for(int i=0; i < cars.get(carName); i++) {
+            for (int i = 0; i < cars.get(carName); i++) {
                 travelDistance += "- ";
             }
-            System.out.println(carName + " : " + travelDistance );
+            System.out.println(carName + " : " + travelDistance);
         }
     }
-    public static void randomMoveCars(HashMap<String, Integer> cars) {
-        for(String carName : cars.keySet()) {
-            int random = Randoms.pickNumberInRange(0,9);
-            if(random >= 4){
+
+    public static void randomMoveCars(LinkedHashMap<String, Integer> cars) {
+        for (String carName : cars.keySet()) {
+            int random = Randoms.pickNumberInRange(0, 9);
+            if (random >= 4) {
                 cars.put(carName, cars.get(carName) + 1);
             }
         }
     }
-    public static void printWinner(HashMap<String, Integer> cars) {
+
+    public static void printWinner(LinkedHashMap<String, Integer> cars) {
         int maxValue = Collections.max(cars.values());
         String championsList = "";
 
-        for(Map.Entry<String, Integer> car : cars.entrySet()) {
-            if(car.getValue() == maxValue){
-                championsList += car.getKey()+" ";
+        for (Map.Entry<String, Integer> car : cars.entrySet()) {
+            if (car.getValue() == maxValue) {
+                championsList += car.getKey() + " ";
             }
         }
-        championsList = championsList.trim().replace(" ",", ");
-        System.out.println("최종 우승자 : "+ championsList);
+        championsList = championsList.trim().replace(" ", ", ");
+        System.out.println("최종 우승자 : " + championsList);
     }
 }
