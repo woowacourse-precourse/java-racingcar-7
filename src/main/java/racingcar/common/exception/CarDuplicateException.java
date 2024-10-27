bbba5e112638fa89e7a8a5b57299c5f5f810319d
soeunnPark <carol0724@naver.com>
2024-10-27 16:22:12 +0900

    feat(Cars): Add a exception class for duplicate car exception
    자동차는 이름으로 식별됩니다. 경주를 할 자동차는 이름으로 식별할 수 있기 때문에 고유한 이름을 사용해야 합니다. 만약 이미 존재하는 자동차가 또 등록될 경우 예외가 발생합니다.

---
+++ src/main/java/racingcar/common/exception/CarDuplicateException.java
@@ -0,0 +1,8 @@
+package racingcar.common.exception;
+
+public class CarDuplicateException extends IllegalArgumentException{
+
+    public CarDuplicateException(String carName) {
+        super(ErrorMessage.DUPLICATE_CAR + "[자동차 이름 : " + carName + "]");
+    }
+}
--- src/main/java/racingcar/common/exception/ErrorMessage.java
+++ src/main/java/racingcar/common/exception/ErrorMessage.java
@@ -2,7 +2,8 @@ package racingcar.common.exception;
 
 public enum ErrorMessage {
 
-    CAR_POSITION_OUT_OF_RANGE("자동차가 더 이상 전진할 수 없습니다.");
+    CAR_POSITION_OUT_OF_RANGE("자동차가 더 이상 전진할 수 없습니다."),
+    DUPLICATE_CAR("중복된 자동차를 등록할 수 없습니다.");
 
     private final String description;
 
