fb6e460a412cca616e796cf4b97861b950c575f1
jae woo Jung <awfjol2008@gmail.com>
2024-10-28 00:01:57 +0900

    feat: RaceResultResponse 클래스 생성자 추가

--- src/main/java/racingcar/dto/RaceResultResponse.java
+++ src/main/java/racingcar/dto/RaceResultResponse.java
@@ -1,4 +1,9 @@
 package racingcar.dto;
 
+import racingcar.domain.Car;
+
 public record RaceResultResponse(String name, int distance) {
+	public RaceResultResponse(Car car) {
+		this(car.getName(), car.getDistance());
+	}
 }
