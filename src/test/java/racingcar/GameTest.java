51001eb34d9c963b36df114dba8ab8aaa4254e53
임채연 <dlacodus0407@gmail.com>
2024-10-28 01:07:22 +0900

    refactor(GameServiceTest): refactor GameServiceTest
    - GameTest 이름을 GameServiceTest로 변경합니다.
    - GameService에 존재하는 메소드를 테스트하기 위해 Nested로 클래스를 묶어 가독성을 높입니다.

--- src/test/java/racingcar/GameTest.java
+++ src/test/java/racingcar/GameTest.java
@@ -5,73 +5,74 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-class GameTest {
+class GameServiceTest {
+    @Nested
+    @DisplayName("selectWinner()를 테스트합니다.")
+    class SelectWinnerTest {
+        private final Integer ROUND = 5;
+        private final List<String> racingCarsName = new ArrayList<>();
 
-    private final Integer ROUND = 5;
-    private final List<String> racingCarsName = new ArrayList<>();
+        @Test
+        void score가_가장_높은사람이_1명일_경우_우승자는_1명이다() {
+            // given
+            GameService gameService = new GameService();
+            racingCarsName.add("woodz");
+            racingCarsName.add("dean");
+            racingCarsName.add("yuze");
+            gameService.init(ROUND, racingCarsName);
 
-    @Test
-    @DisplayName("score가 가장 높은사람이 1명일 경우, 우승자는 1명이다.")
-    void selectWinner_one() {
-        // given
-        GameService gameService = new GameService();
-        racingCarsName.add("woodz");
-        racingCarsName.add("dean");
-        racingCarsName.add("yuze");
-        gameService.init(ROUND, racingCarsName);
+            // when
+            gameService.getGame().getRacingCars().get(0).addScore();
+            List<String> winners = gameService.selectWinner();
 
-        // when
-        gameService.getGame().getRacingCars().get(0).addScore();
-        List<String> winners = gameService.selectWinner();
+            // then
+            assertThat(winners.size()).isEqualTo(1);
+            assertThat(winners.get(0)).isEqualTo("woodz");
+        }
 
-        // then
-        assertThat(winners.size()).isEqualTo(1);
-        assertThat(winners.get(0)).isEqualTo("woodz");
-    }
+        @Test
+        void score가_가장_높은사람이_2명일_경우_우승자는_2명이다() {
+            // given
+            GameService gameService = new GameService();
+            racingCarsName.add("woodz");
+            racingCarsName.add("dean");
+            racingCarsName.add("yuze");
+            gameService.init(ROUND, racingCarsName);
 
-    @Test
-    @DisplayName("score가 가장 높은사람이 2명일 경우 우승자는 2명이다.")
-    void selectWinner_two() {
-        // given
-        GameService gameService = new GameService();
-        racingCarsName.add("woodz");
-        racingCarsName.add("dean");
-        racingCarsName.add("yuze");
-        gameService.init(ROUND, racingCarsName);
+            // when
+            gameService.getGame().getRacingCars().get(0).addScore();
+            gameService.getGame().getRacingCars().get(1).addScore();
+            List<String> winners = gameService.selectWinner();
 
-        // when
-        gameService.getGame().getRacingCars().get(0).addScore();
-        gameService.getGame().getRacingCars().get(1).addScore();
-        List<String> winners = gameService.selectWinner();
+            // then
+            assertThat(winners.size()).isEqualTo(2);
+            assertThat(winners.get(0)).isEqualTo("woodz");
+            assertThat(winners.get(1)).isEqualTo("dean");
+        }
 
-        // then
-        assertThat(winners.size()).isEqualTo(2);
-        assertThat(winners.get(0)).isEqualTo("woodz");
-        assertThat(winners.get(1)).isEqualTo("dean");
-    }
+        @Test
+        void 참가자의_score가_모두_동일하면_우승자는_전체이다() {
+            // given
+            GameService gameService = new GameService();
+            racingCarsName.add("woodz");
+            racingCarsName.add("dean");
+            racingCarsName.add("yuze");
+            gameService.init(ROUND, racingCarsName);
 
-    @Test
-    @DisplayName("모든 참가자의 score가 동점일 경우 우승자는 전체다.")
-    void selectWinner_all() {
-        // given
-        GameService gameService = new GameService();
-        racingCarsName.add("woodz");
-        racingCarsName.add("dean");
-        racingCarsName.add("yuze");
-        gameService.init(ROUND, racingCarsName);
+            // when
+            gameService.getGame().getRacingCars().get(0).addScore();
+            gameService.getGame().getRacingCars().get(1).addScore();
+            gameService.getGame().getRacingCars().get(2).addScore();
+            List<String> winners = gameService.selectWinner();
 
-        // when
-        gameService.getGame().getRacingCars().get(0).addScore();
-        gameService.getGame().getRacingCars().get(1).addScore();
-        gameService.getGame().getRacingCars().get(2).addScore();
-        List<String> winners = gameService.selectWinner();
-
-        // then
-        assertThat(winners.size()).isEqualTo(3);
-        assertThat(winners.get(0)).isEqualTo("woodz");
-        assertThat(winners.get(1)).isEqualTo("dean");
-        assertThat(winners.get(2)).isEqualTo("yuze");
+            // then
+            assertThat(winners.size()).isEqualTo(3);
+            assertThat(winners.get(0)).isEqualTo("woodz");
+            assertThat(winners.get(1)).isEqualTo("dean");
+            assertThat(winners.get(2)).isEqualTo("yuze");
+        }
     }
 }
