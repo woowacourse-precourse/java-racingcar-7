06066f64c5bbae614f3a4bd35374f42409e804c5
강승연 <syeon.dev@gmail.com>
2024-10-27 20:25:04 +0900

    refactor: 입출력 형식 구현 책임 InputManager로 이전

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,21 +6,14 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
-        String input = Console.readLine();
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int try_count = Integer.parseInt(Console.readLine());
-
-        String[] cars = input.split(",");
-
+        InputManager inputManager = new InputManager();
         GameManager gameManager = new GameManager();
 
-        String[] winners = gameManager.StartGame(cars, try_count);
+        String[] car_list = inputManager.GetCarList();
+        int try_count = inputManager.GetTryCount();
 
-        System.out.print("최종 우승자 : ");
-        for(int i = 0; i < winners.length - 1; i++) {
-            System.out.print(winners[i] + ", ");
-        }
-        System.out.print(winners[winners.length - 1]);
+        inputManager.PrintResult(
+                gameManager.StartGame(car_list, try_count));
+
     }
 }
---
+++ src/main/java/racingcar/InputManager.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputManager {
+
+    public String[] GetCarList(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String input = Console.readLine();
+
+        return input.split(",");
+    }
+
+    public int GetTryCount(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public void PrintResult(String[] winners){
+        System.out.print("최종 우승자 : ");
+        for(int i = 0; i < winners.length - 1; i++) {
+            System.out.print(winners[i] + ", ");
+        }
+        System.out.print(winners[winners.length - 1]);
+    }
+}
