b4ce68f5b5bdd23d2c3bd1f2df6dd8b9606029a9
RawBrain <95132660+RawBrain@users.noreply.github.com>
2024-10-28 04:46:26 +0900

    feat(main): implement main application start point
    자동차 경주 게임의 메인 실행 흐름 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,10 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String carName = InputView.readCarName();
+        String roundInput = InputView.readRoundCount();
+
+        RaceManager raceManager = new RaceManager();
+        raceManager.raceStart(CarNameParser.parse(carName), RoundInputParser.parse(roundInput));
     }
 }
