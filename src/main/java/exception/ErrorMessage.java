0a8d8e67eae5baccd79ed4d00528eb6f060a2d1c
aaahyunseo <hs030608@naver.com>
2024-10-27 17:13:29 +0900

    feat(ErrorMessage): IllegalArgumentException 발생 시 에러 메세지 추가

---
+++ src/main/java/exception/ErrorMessage.java
@@ -0,0 +1,17 @@
+package exception;
+
+public enum ErrorMessage {
+    NAME_LENGTH_EXCESS("[ERROR] 자동차 이름이 5글자를 초과하였습니다."),
+    NAME_EMPTY("[ERROR] 자동차 이름이 비어있습니다."),
+    NAME_DUPLICATE("[ERROR] 자동차 이름이 중복되었습니다.");
+
+    private final String errorMessage;
+
+    ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage(){
+        return this.errorMessage;
+    }
+}
