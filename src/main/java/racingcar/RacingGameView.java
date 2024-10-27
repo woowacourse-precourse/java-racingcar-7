49cddc929812aac2692ec55e9c1a0c917722d3fa
sun <50648835+kimseongyu@users.noreply.github.com>
2024-10-27 12:09:58 +0900

    feat: display game progress to user

--- src/main/java/racingcar/RacingGameView.java
+++ src/main/java/racingcar/RacingGameView.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class RacingGameView {
 
@@ -12,5 +13,25 @@ public class RacingGameView {
     public String getTurn() {
         System.out.println("시도할 횟수는 몇 회 인가요?");
         return Console.readLine();
+    }
+
+    public void displayStartRacing() {
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
+    public void displayCarState(String name, int distance) {
+        String output = String.format("%s : %s", name, "-".repeat(distance));
+        System.out.println(output);
+    }
+
+    public void displayCurrentTurnOver() {
+        System.out.println();
+    }
+
+    public void displayWinners(List<String> winnersName) {
+        String output = String.join(", ", winnersName);
+        output = String.format("최종 우승자 : %s", output);
+        System.out.println(output);
     }
 }
