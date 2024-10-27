aff79041aa3aebe05a01f3f293682acb7273e1ec
gyuho <kyuhokim12@gmail.com>
2024-10-27 16:05:06 +0900

    chore(InputView): 메소드 이름 네이밍 컨벤션에 맞게 수정
    - InputView 클래스의 GetlistOfCarNames()메소드의 이름을 getListOfCarNaems로 수정하였습니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_CAR_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public static String GetlistOfCarNames(){
+    private static final String INPUT_NUMBER_OF_ATTEMPTS = "시도할 횟수는 몇 회인가요?";
+
+    public static String getListOfCarNames(){
         System.out.println(INPUT_CAR_MESSAGE);
         String CarNames = Console.readLine();
         return CarNames;
