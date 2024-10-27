7c7828b5fa2b71bce4d05c8d618102b9480c154e
임채연 <dlacodus0407@gmail.com>
2024-10-28 00:28:02 +0900

    style(InputValidator): add import static
    - import static으로 길게 늘어진 코드를 줄입니다.

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import static racingcar.RegexPatterns.*;
+
 import java.util.List;
 
 public class InputValidator {
@@ -10,7 +12,7 @@ public class InputValidator {
     }
 
     public Boolean isValidInputFormat(String input) {
-        if (input.startsWith(RegexPatterns.DELIMITER) || input.endsWith(RegexPatterns.DELIMITER)) {
+        if (input.startsWith(DELIMITER) || input.endsWith(DELIMITER)) {
             throw new IllegalArgumentException(ExceptionMessages.INVALID_INPUT_FORMAT);
         }
         return true;
@@ -30,7 +32,7 @@ public class InputValidator {
     }
 
     public Integer isValidRound(String input) {
-        if (!input.matches(RegexPatterns.DIGIT_REGEX)) {
+        if (!input.matches(DIGIT_REGEX)) {
             throw new IllegalArgumentException(ExceptionMessages.ONLY_NUMERIC_ATTEMPTS);
         }
         return Parser.toInteger(input);
