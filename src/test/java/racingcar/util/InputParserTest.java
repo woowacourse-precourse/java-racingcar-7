e70e1d02353ffcda736be81833147ec8cc9d02a7
sanghee0820 <lsh277604@gmail.com>
2024-10-28 00:10:30 +0900

    test: null과 비어있는 값에 대한 테스트 추가

--- src/test/java/racingcar/util/InputParserTest.java
+++ src/test/java/racingcar/util/InputParserTest.java
@@ -1,12 +1,14 @@
 package racingcar.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.exception.InputException;
 
 class InputParserTest {
 
@@ -25,16 +27,28 @@ class InputParserTest {
     }
 
     @Test
-    @DisplayName("입력값이 빈값일 경우 파싱 테스트")
-    void parseNameEmpty() {
+    @DisplayName("입력값이 빈값일 경우 오류 발생 테스트")
+    void parseNameEmptyTest() {
         // given
         final String input = "";
         List<String> expected = List.of("");
 
-        // when
-        var parseResponse = InputParser.parseName(input);
+        // when && then
+        assertThatThrownBy(() -> InputParser.parseName(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(InputException.INPUT_IS_NULL_OR_EMPTY.getMessage());
+    }
 
-        // then
-        assertThat(parseResponse).isEqualTo(expected);
+    @Test
+    @DisplayName("입력값이 null일 경우 오류 발생 테스트")
+    void parseNameNullTest() {
+        // given
+        final String input = null;
+        List<String> expected = List.of("");
+
+        // when && then
+        assertThatThrownBy(() -> InputParser.parseName(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(InputException.INPUT_IS_NULL_OR_EMPTY.getMessage());
     }
 }
