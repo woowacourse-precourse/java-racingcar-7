15cd5322cd2e060c764a7f67718f12bf574548eb
dd-jiyun <dlwldbs8015@gmail.com>
2024-10-27 17:00:52 +0900

    test(ApplicationTest): 게임 정상 실행 테스트
    - 게임이 우승자가 한 명일 때와 여러 명일 때의 경우에 정상적으로 실행되는지 확인합니다.
    - 사용자에게 자동차의 이름과 시도 횟수를 입력받을 때의 예외 상황을 확인합니다.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -9,11 +10,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
     @Test
-    void 기능_테스트() {
+    @DisplayName("자동차 경주 게임이 정상적으로 시작되어 결과가 반환된다.(우승자가 한명일 때)")
+    void racingGameAndWinner() {
         assertRandomNumberInRangeTest(
             () -> {
                 run("pobi,woni", "1");
@@ -24,10 +27,32 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    @DisplayName("자동차 경주 게임이 정상적으로 시작되어 결과가 반환된다.(우승자가 여러 명일 때)")
+    void racingGameAndWinners() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    @DisplayName("올바르지 않은 자동차의 이름을 입력하면 예외가 발생한다.")
+    void carNameException() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("올바르지 않은 시도 횟수를 입력하면 예외가 발생한다.")
+    void tryCountException() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "a"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
@@ -35,4 +60,5 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+
 }
