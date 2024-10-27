1de7c9284136353eb57bd1d9a375190f52c6e840
jody816 <81346079+jody816@users.noreply.github.com>
2024-10-28 00:47:16 +0900

    feat: 입력값에 따른 초기 세팅 메서드 및 로직 구현

--- src/main/java/racingcar/application/service/RacingService.java
+++ src/main/java/racingcar/application/service/RacingService.java
@@ -1,5 +1,19 @@
 package racingcar.application.service;
 
+import java.util.List;
+import racingcar.application.dto.InputCarNamesDTO;
+import racingcar.application.model.Car;
+
 public class RacingService {
 
+  private int attemptCount;
+  private List<Car> cars;
+
+  public void setup(String inputCarNames, int inputAttemptCount) {
+
+    InputCarNamesDTO inputCarNamesDTO = new InputCarNamesDTO(inputCarNames);
+    cars = inputCarNamesDTO.toModel();
+
+    this.attemptCount = inputAttemptCount;
+  }
 }
