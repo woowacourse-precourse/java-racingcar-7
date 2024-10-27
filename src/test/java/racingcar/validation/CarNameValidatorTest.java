1e8c16148fee58e34fba64809207ead8c21f5020
Sinorang <sinholang@naver.com>
2024-10-27 18:06:15 +0900

    test: 자동차 이름 공백 포함 예외 테스트 코드 구현
    - @ParameterizedTest, @ValueSource 어노테이션을 사용하여 동일한 로직을
      여러 입력값으로 반복해서 테스트할 수 있도록 테스트 코드 작성

---
+++ src/test/java/racingcar/validation/CarNameValidatorTest.java
@@ -0,0 +1,22 @@
+package racingcar.validation;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.constants.ErrorMessageConstants;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CarNameValidatorTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi, woni,jun ", "po bi,woni,jun"})
+    void 자동차_이름_공백_포함_예외_테스트(String input) {
+        List<String> carNames = Arrays.asList(input.split(","));
+        assertThatThrownBy(() -> CarNameValidator.validateCarNames(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessageConstants.ERROR_CAR_NAME_CONTAINS_BLANK);
+    }
+}
