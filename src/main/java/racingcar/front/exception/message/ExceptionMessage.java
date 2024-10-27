8b3ecf3f08e6b49e9697113f573a4f60625646f3
whatsgoodg <rlwjddl1596@naver.com>
2024-10-27 19:57:56 +0900

    feat(ExceptionMessage): ExceptionMessage 클래스 내 예외 메세지 수정
    수정 내용
    - 차 이름에 대한 예외 메세지 변경

--- src/main/java/racingcar/front/exception/message/ExceptionMessage.java
+++ src/main/java/racingcar/front/exception/message/ExceptionMessage.java
@@ -1,7 +1,7 @@
 package racingcar.front.exception.message;
 
 public enum ExceptionMessage {
-    INVALID_CAR_NAME("자동차 이름은 1글자 이상, 5글자 이하여야 하며 알파벳 대소문자만 입력 가능합니다."),
+    INVALID_CAR_NAME("자동차 이름은 1글자 이상, 5글자 이하여야 하며 쉼표와 공백은 허용되지 않습니다."),
     INVALID_PLAY_COUNT("시도 횟수는 양의 정수값만 입력 가능합니다.");
     private final String message;
 
