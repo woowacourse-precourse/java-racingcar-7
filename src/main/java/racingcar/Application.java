7a4f5544e4460cd5d27f2ba381abeb6deed56970
jungsehui <jungsehui@naver.com>
2024-10-27 17:06:25 +0900

    feat(Application.java): 메서드 분리를 위한 구분용 주석 추가, 레이싱 게임을 위한 자동차 움직임 기능 실행 및 중간 실행 결과 출력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,25 +9,45 @@ public class Application {
 
     public static void main(String[] args) {
         // 자동차 이름 입력
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(Message.RACING_CAR_NAME_INPUT.getMessage());
         String inputCarName = Console.readLine();
 
         // 입력된 자동차 이름 저장 후 검증
         String[] names = inputCarName.split(",");
         for (String name : names) {
             if (name.isEmpty() || name.length() > 5) {
-                throw new IllegalArgumentException("[ERROR] 이름의 길이는 1부터 5까지만 가능합니다.");
+                throw new IllegalArgumentException(Message.ILLEGAL_NAME_LENGTH.getMessage());
             }
         }
 
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String inputCount = Console.readLine();
+        // 시도 횟수 입력
+        System.out.println(Message.TRY_COUNT.getMessage());
+        String inputTryCount = Console.readLine();
 
-        System.out.println(inputCarName + " " + inputCount);
-
+        // 입력된 이름을 기반으로 Car 객체를 생성하여 리스트에 저장
         List<Car> cars = new ArrayList<>();
         for (String name : names) {
             cars.add(new Car(name));
+        }
+
+        // 레이싱 게임 전체 사이클
+        System.out.println(Message.RESULT.getMessage());
+        for (int i = 0; i < Integer.parseInt(inputTryCount); i++) {
+            // 자동차 무빙 기능
+            for (Car car : cars) {
+                car.move();
+            }
+
+            // 중간 실행 결과 출력
+            for (Car car : cars) {
+                StringBuilder result = new StringBuilder(car.getName() + " : ");
+                int carPosition = car.getPosition();
+                for (int j = 0; j < carPosition; j++) {
+                    result.append("-");
+                }
+                System.out.println(result.toString());
+            }
+            System.out.println();
         }
     }
 }
