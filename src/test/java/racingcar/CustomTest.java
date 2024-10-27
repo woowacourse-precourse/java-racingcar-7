12bc9613b48abe46e5a1d42dcca4491a88b420f1
minyeongg <1030pmy@gmail.com>
2024-10-27 23:05:11 +0900

    test: 자동차 1개 예외 테스트

--- src/test/java/racingcar/CustomTest.java
+++ src/test/java/racingcar/CustomTest.java
@@ -26,7 +26,14 @@ public class CustomTest {
                 .hasMessage("빈 문자열을 입력할 수 없습니다.");
     }
 
-
+    @Test
+    @DisplayName("자동차 이름이 두 개 이상이 아닐 때 예외 발생")
+    void testMinimumCarException() {
+        Map<String, Integer> race = new HashMap<>();
+        assertThatThrownBy(() -> Application.getNames(race, "pobi"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차는 2개 이상이어야 합니다.");
+    }
 
 
 }
