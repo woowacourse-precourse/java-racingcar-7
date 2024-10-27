0eb7e311eedde620eae2131e6c676353ce6e05ab
Hyso <hsk7953@gmail.com>
2024-10-28 03:24:33 +0900

    refactor(CarNameValidatorTest) : 테스트 코드를 인스턴스 기반으로 변경 - @BeforeEach로 객체 초기화 추가

--- src/test/java/racingcar/validator/CarNameValidatorTest.java
+++ src/test/java/racingcar/validator/CarNameValidatorTest.java
@@ -1,5 +1,6 @@
 package racingcar.validator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,50 +10,57 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarNameValidatorTest {
+    private CarNameValidator carNameValidator;
+
+    @BeforeEach
+    void setup() {
+        carNameValidator = new CarNameValidator();
+    }
+
     @DisplayName("자동차 이름이 5자 이하면 통과한다")
     @ParameterizedTest
     @ValueSource(strings = {"car1", "car2", "car3", "car4", "kim", "pobi"})
     void validateCarNameLength_success(String input) {
-        assertDoesNotThrow(() -> CarNameValidator.validateCarNameLength(input));
+        assertDoesNotThrow(() -> carNameValidator.validateCarNameLength(input));
     }
 
     @DisplayName("자동차 이름이 5자 이상이면 예외가 발생한다")
     @Test
     void validateCarNameLength_throwsException() {
-        assertThrows(IllegalArgumentException.class, () -> CarNameValidator.validateCarNameLength("kimhyunsoo"));
+        assertThrows(IllegalArgumentException.class, () -> carNameValidator.validateCarNameLength("kimhyunsoo"));
     }
 
     @DisplayName("문장의 마지막이 콤마(,)로 끝나면 예외가 발생한다")
     @Test
     void validateEnd_throwsException() {
-        assertThrows(IllegalArgumentException.class, () -> CarNameValidator.validateEnd("kim,pobi,"));
+        assertThrows(IllegalArgumentException.class, () -> carNameValidator.validateEnd("kim,pobi,"));
     }
 
     @DisplayName("자동차 이름들의 구분자는 콤마(,)다.")
     @ParameterizedTest
     @ValueSource(strings = {"zzz4,zz3,zzz1", "kim,pobi,ori-3", "kim, hyun, soo"})
     void validateDelimiter_throwsException(String input) {
-        assertDoesNotThrow(() -> CarNameValidator.validateDelimiter(input));
+        assertDoesNotThrow(() -> carNameValidator.validateDelimiter(input));
     }
 
     @DisplayName("자동차 이름들의 구분자가 콤마가 아니면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"zz3;zz2;zz1", "kim hyun soo", "pobi 3412; 332"})
     void validateDelimiter_success(String input) {
-        assertThrows(IllegalArgumentException.class, () -> CarNameValidator.validateDelimiter(input));
+        assertThrows(IllegalArgumentException.class, () -> carNameValidator.validateDelimiter(input));
     }
 
     @DisplayName("자동차 이름이 빈칸이거나 공백일 경우 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {" ", ""})
     void validateNotBlank_throwsException(String input) {
-        assertThrows(IllegalArgumentException.class, () -> CarNameValidator.validateNotBlank(input));
+        assertThrows(IllegalArgumentException.class, () -> carNameValidator.validateNotBlank(input));
     }
 
     @DisplayName("자동차 이름에 공백이 있을 경우 예외가 발생한다.")
     @Test
     void validateNoSpace_throwsException() {
-        assertThatThrownBy(() -> CarNameValidator.validateNoSpace("pobi kim"))
+        assertThatThrownBy(() -> carNameValidator.validateNoSpace("pobi kim"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -60,13 +68,13 @@ class CarNameValidatorTest {
     @ParameterizedTest
     @ValueSource(strings = {"pobi,june,kim", "ori-4,kimm,aax2"})
     void validateCarNames_success(String input) {
-        assertDoesNotThrow(() -> CarNameValidator.validateCarNames(input));
+        assertDoesNotThrow(() -> carNameValidator.validateCarNames(input));
     }
 
     @DisplayName("자동차 이름 검증 통합테스트 예외발생")
     @ParameterizedTest
     @ValueSource(strings = {"pobi, june, kim", "aa,,d3,4", "", " ", "ooooo3,dd3,jje", "zzz34,kk32,", "kim, hyye, ek2"})
     void validateCarNames_throwsException(String input) {
-        assertThrows(IllegalArgumentException.class, () -> CarNameValidator.validateCarNames(input));
+        assertThrows(IllegalArgumentException.class, () -> carNameValidator.validateCarNames(input));
     }
 }
