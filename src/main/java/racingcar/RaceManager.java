241d2c5f8cd5509ed60d4b803c187ec2cc81e678
RawBrain <95132660+RawBrain@users.noreply.github.com>
2024-10-28 04:40:21 +0900

    feat: impliment race management system logic
    자동차 경주의 전체 흐름을 관리하는 로직 구현
    -참가 차량 목록과 라운드 수를 입력받아 경주 시작
    -매 라운드마다 전체 차량에게 이동기회 제공
    -OutPutView를 통해 진행상황과 우승자 결과 출력

--- src/main/java/racingcar/RaceManager.java
+++ src/main/java/racingcar/RaceManager.java
@@ -1,4 +1,20 @@
 package racingcar;
 
+import java.util.List;
+
 public class RaceManager {
+    private RaceMember raceMember;
+
+    public void raceStart(List<String> carList, int round) {
+        raceMember = new RaceMember(carList);
+        race(round);
+    }
+
+    private void race(int round) {
+        for (int i = 0; i < round; i++) {
+            raceMember.moveAll();
+            OutputView.reportProgress(raceMember.getRaceMember());
+        }
+        OutputView.reportRaceResult(raceMember.getWinner());
+    }
 }
