6d72d201b89f5d76bde31a16f7ef704d74e7a384
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-27 17:34:26 +0900

    refactor: 에러 메시지만 상수화로 남김

--- src/main/java/racingcar/ErrorMessage.java
+++ src/main/java/racingcar/ErrorMessage.java
@@ -1,10 +1,6 @@
 package racingcar;
 
-public class ConstantMessage {
-    public static final String FINAL_RESULT = "\n실행 결과";
-    public static final String FINAL_WINNER = "최종 우승자 : ";
-    public static final String COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
-    public static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+public class ErrorMessage {
     public static final String INPUT_IS_EMPTY = "입력값이 비어있습니다.";
     public static final String ONLY_COMMA_HERE = "쉼표만 입력되었습니다.";
     public static final String INCORRECT_TYPE_COMMA_N_NAME = "쉼표 뒤에 이름이 들어오지 않았습니다.";
@@ -13,7 +9,7 @@ public class ConstantMessage {
     public static final String BE_NATURAL_NUMBER = "입력값은 1 이상의 자연수여야 합니다.";
     public static final String BE_DIGIT = "입력값은 숫자여야 합니다.";
 
-    private ConstantMessage() {
+    private ErrorMessage() {
 
     }
 }
