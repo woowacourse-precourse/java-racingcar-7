287f538798c3ec7759d62a5a8de0a3a5368b602a
minzzun99 <skg06123@naver.com>
2024-10-27 23:01:44 +0900

    refactor(view): printWinners 메서드 리팩토링 및 상수 처리, newLinePrint 메서드 추가
    - 구분자 상수처리
    - newLinePrint() 메서드 추가
    - printMessage() 메서드 이름 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,12 +3,18 @@ package racingcar.view;
 import java.util.List;
 
 public class OutputView {
-    public static void printMessage(String message) {
-        System.out.print(message);
+    private static final String WINNER_DELIMITER = ", ";
+
+    public static void print(String output) {
+        System.out.print(output);
+    }
+
+    public static void newLinePrint(String output) {
+        System.out.println(output);
     }
 
     public static void printWinners(String message, List<String> winners) {
-        String result = String.join(",", winners);
-        System.out.println(message + result);
+        String result = String.join(WINNER_DELIMITER, winners);
+        newLinePrint(message + result);
     }
 }
