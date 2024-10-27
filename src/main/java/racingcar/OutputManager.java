dec40daf327d6a6538c087b52d0f29c288361eec
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-27 19:06:33 +0900

    style: 가독성위해 print함수 분리, 변수명 수정

--- src/main/java/racingcar/OutputManager.java
+++ src/main/java/racingcar/OutputManager.java
@@ -6,6 +6,7 @@ public class OutputManager {
 
     private static final String ROUND_RESULT = "실행 결과";
     private static final String WINNER_RESULT = "최종 우승자 : ";
+    private static final String COMMA_WITH_BLANK = ", ";
 
     public static void printStartMessage() {
         System.out.println();
@@ -13,22 +14,23 @@ public class OutputManager {
     }
 
     public static void printCurrent(Car car) {
-        System.out.println(car.printStatus());;
+        System.out.println(car.printStatus());
     }
 
-    public static void printWinner(List<Car> winnerNames) {
+    public static void printWinner(List<Car> winners) {
         System.out.print(WINNER_RESULT);
         StringBuilder sb = new StringBuilder();
 
         int i = 0;
 
-        for (int idx = i; idx < winnerNames.size() - 1; idx++){
-            sb.append(winnerNames.get(idx));
-            sb.append(", ");
+        for (int idx = i; idx < winners.size() - 1; idx++){
+            sb.append(winners.get(idx));
+            sb.append(COMMA_WITH_BLANK);
             i++;
         }
 
-        sb.append(winnerNames.get(i));
+        sb.append(winners.get(i));
         System.out.println(sb);
+        System.out.println();
     }
 }
