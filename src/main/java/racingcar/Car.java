4fafea94f981db1c1b0c25ef38191e3b42e8dc77
soyoon19 <96063941+soyoon19@users.noreply.github.com>
2024-10-28 03:27:02 +0900

    Feat : Add Car function
    judge(), random() 구현
    자동차 이름과 이동 거리는 메서드가 아니므로 체크박스에서 제외

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     public String name;
     public int distance;
+    public Car (String name){
+        this.name = name;
+    }
     public boolean judge (int random){
+        if(random >= 4){
+            return true;
+        }
         return false;
     };
+
     public int random (){
-        return 0;
+        return Randoms.pickNumberInRange(0, 9);
     };
 }
