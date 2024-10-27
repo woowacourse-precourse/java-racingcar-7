b5c2f0dd5dd522e7560b43423797f8126e63745d
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-10-27 12:09:36 +0900

    feat : 경주할 자동차 목록 쉼표 기준 구분하기 기능 추가
    RacingCar.java : carList로 쉼표 기준으로 구분된 자동차 이름을 관리하게 하였습니다.
    
    Separtor.java : 쉼표를 기준으로 String을 분리해 List를 반환하는 함수 separateCarList를 구현하였습니다.

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,14 +1,18 @@
 package racingcar;
 
+import java.util.List;
 import racingcar.io.ConsoleInputHandler;
+import racingcar.util.Separator;
 
 public class RacingCar {
 
     private final ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
+    private final Separator separator = new Separator();
 
     public void run(){
         String carListInput = consoleInputHandler.getCarListInput();
         int tryCount = consoleInputHandler.getMoveTryCount();
+        List<String> carList = separator.separateCarList(carListInput);
     }
 
 }
---
+++ src/main/java/racingcar/util/Separator.java
@@ -0,0 +1,14 @@
+package racingcar.util;
+
+import java.util.List;
+
+public class Separator {
+
+    String delimiter = "\'";
+
+    public List<String> separateCarList(String carListInput){
+        List<String> carList= List.of(carListInput.split(delimiter));
+        return carList;
+    }
+
+}
