433ba99634928f98da958f2aae2bf4088930a504
Hyso <hsk7953@gmail.com>
2024-10-27 20:28:32 +0900

    feat : RandomNumber 클래스 기능 구현 - 0 - 9 사이에 랜덤한 숫자를 생성

---
+++ src/main/java/utils/RandomNumber.java
@@ -0,0 +1,12 @@
+package utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumber {
+    private RandomNumber(){
+    }
+
+    public static int getNumber(){
+        return Randoms.pickNumberInRange(0,9);
+    }
+}
