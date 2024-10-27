e80392bb36772269a078b65bccb97699969c30ba
yxoni <psy337250@gmail.com>
2024-10-27 15:52:39 +0900

    feat: 프로그램의 동작을 관리하는 OperationManager 클래스 생성

---
+++ src/main/java/racingcar/OperationManager.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+public class OperationManager {
+    private IO io = new IO();
+    private Race race = new Race();
+
+    public void initProgram() {
+    }
+
+    public void execute() {
+    }
+}
