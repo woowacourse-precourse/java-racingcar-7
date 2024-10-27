744c55ea7746de881528be42da678a8dbcaad930
JiEung2 <181757@jnu.ac.kr>
2024-10-27 21:28:32 +0900

    feat(inputValidator): 자동차 이름 길이 제한 검증 메서드 추가

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -1,9 +1,18 @@
 package racingcar;
 
+import java.util.List;
+
 public class InputValidator {
     private final int CAR_NAME_LENGTH_LIMIT = 5;
+    private final String CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 5자 이하만 가능합니다.";
 
-    public void validateCarNames() {
+    public void validateCarNames(List<String> carNames) {
 
+    }
+
+    private void validateCarNameLength(String carName) {
+        if (carName.length() > CAR_NAME_LENGTH_LIMIT) {
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR_MESSAGE);
+        }
     }
 }
