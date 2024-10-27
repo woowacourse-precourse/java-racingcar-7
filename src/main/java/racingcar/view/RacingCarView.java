c8405a60f5964c908f306622b5c370e01cedf4e2
byeongdoohan <quden04@gmail.com>
2024-10-27 15:45:05 +0900

    rename: getOutputString -> printOutputString 변경

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -1,10 +1,15 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class RacingCarView {
     private static final String CAR_INPUT_STRING = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_INPUT_STRING = "시도할 횟수는 몇 회인가요?";
     private static final String OUTPUT_STRING = "최종 우승자 : ";
 
+    public String readInput(){
+        return Console.readLine();
+    }
     public void printCarInputString() {
         System.out.println(CAR_INPUT_STRING);
     }
@@ -13,7 +18,8 @@ public class RacingCarView {
         System.out.println(TRY_INPUT_STRING);
     }
 
-    public void getOutputString(String carNames) {
+    public void printOutputString(String carNames) {
         System.out.println(OUTPUT_STRING + carNames);
     }
+
 }
