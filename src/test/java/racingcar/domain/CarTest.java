5b7b572b1a52e64e5e32b578253dad7af61fda61
장영재 <urinaner@naver.com>
2024-10-28 01:04:36 +0900

    test(Car): 바뀐 로직 테스트

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,14 +16,33 @@ class CarTest {
         assertThat(car.getPosition()).isEqualTo(0);
     }
 
-    @DisplayName("자동차를 앞으로 한 칸 움직인다.")
+    @DisplayName("0 ~ 3일때는 자동차를 안 움직인다.")
     @Test
-    void moveTest() {
+    void move_Zero_To_Three_Test() {
         //given
+        List<Integer> numbers = List.of(0, 1, 2, 3);
         Car car = new Car("yj");
         //when
-        car.move();
+        for (int number : numbers) {
+            car.move(number);
+        }
         //then
-        assertThat(car.getPosition()).isEqualTo(1);
+        assertThat(car.getPosition()).isEqualTo(0);
     }
+
+    @DisplayName("4 ~ 9일때는 자동차를 움직인다.")
+    @Test
+    void move_Four_To_Nine_Test() {
+        //given
+        List<Integer> numbers = List.of(4, 5, 6, 7, 8, 9);
+        Car car = new Car("yj");
+        //when
+        for (int number : numbers) {
+            car.move(number);
+        }
+        //then
+        assertThat(car.getPosition()).isEqualTo(6);
+    }
+
+
 }
