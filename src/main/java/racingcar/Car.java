d9f600e3d74126e2bb6d5d3bce106654d1416f8e
ahpicl64 <ahpicl64@gmail.com>
2024-10-28 06:58:19 +0900

    refactor(model): Car 클래스 생성자 및 이동 로직 단순화, CarTest 수정
    - Car.java: 불필요 생성자와 파라미터 제거(move 메서드 간소화 및 가독성 향상)
    - CarTest.java: move 메서드 테스트 수정, getter 메서드 테스트 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,39 +4,25 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
     private String carName;
-    private int distance;
+    private int distance = 0;
 
-    public Car() {
+    public Car(String name) {
+        this.carName = name;
     }
 
-    public Car(int distance) {
-        this.distance = distance;
-    }
-
-    public Car(String carName, int count) {
-        this.carName = carName;
-    }
-
-    public boolean isMove() {
+    public void move() {
         int powerOfCars = Randoms.pickNumberInRange(0, 9);
-        if (5 >= powerOfCars) {
-            return false;
-        }
-        return true;
-    }
-
-    private void setDistance() {
-        distance = 0;
-        if (isMove()) {
+        if (powerOfCars >= 5) {
             distance++;
         }
+    }
+
+    public String getCarName() {
+        return carName;
     }
 
     public int getDistance() {
         return distance;
     }
 
-    public String getCarName() {
-        return carName;
-    }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -6,14 +6,27 @@ import org.junit.jupiter.api.Test;
 
 
 class CarTest {
-    Car car = new Car();
+
+    Car car = new Car("pobi");
 
     @Test
-    void 무브랜덤에_따라_거리의_값_증가() {
-        int result = car.getDistance();
-        if (car.isMove()) {
-            assertThat(result).isEqualTo(1);
+    void 이름_설정_확인() {
+        assertThat(car.getCarName()).isEqualTo("pobi");
+    }
+
+    @Test
+    void 초기_거리_확인() {
+        assertThat(car.getDistance()).isEqualTo(0);
+    }
+
+    @Test
+    void 호출에_따라_거리_증가() {
+        int initialDistance = car.getDistance();
+
+        for (int i = 0; i < 10; i++) {
+            car.move();
         }
-        assertThat(result).isEqualTo(0);
+
+        assertThat(car.getDistance()).isGreaterThanOrEqualTo(initialDistance);
     }
 }
