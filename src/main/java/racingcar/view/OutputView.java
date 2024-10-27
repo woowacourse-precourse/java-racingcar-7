e8f5de7c0af1568360d0526819f806b327332885
Chan531 <chan657312@gmail.com>
2024-10-27 16:16:08 +0900

    chore(OutputView): 자동차 이름 입력 메세지 문자열 상수 분리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,10 +2,11 @@ package racingcar.view;
 
 public class OutputView {
 
+	private static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	private static final String LINE_SEPARATOR = "\n";
 
 	public static void printCarNamesInputMessage() {
-		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		System.out.println(CAR_NAMES_INPUT_MESSAGE);
 	}
 
 	public static void printTrialCountInputMessage() {
