d82fa113221cce0e73da3565f66337df2cd9c952
Mint <audwl03071@gmail.com>
2024-10-27 20:02:09 +0900

    test: splitter 테스트 작성

---
+++ src/test/java/racingcar/util/splitter/SplitterTest.java
@@ -0,0 +1,29 @@
+package racingcar.util.splitter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("구분기 테스트")
+class SplitterTest {
+
+    @Test
+    @DisplayName("문자열을 구분한다")
+    void 성공_구분() {
+        // Given
+        String delimiter = ",";
+        Splitter splitter = new Splitter(delimiter);
+        String text = "mint,dobby";
+
+        // When
+        String[] names = splitter.splitFrom(text);
+
+        // Then
+        assertAll(
+                () -> assertThat(names[0]).isEqualTo("mint"),
+                () -> assertThat(names[1]).isEqualTo("dobby")
+        );
+    }
+}
