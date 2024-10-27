de74f7abff34d529e7ec6c2eeb67742684dd2a5b
haebyun <bhk4392@gmail.com>
2024-10-27 14:59:57 +0900

    feat(RacingParticipants): 우승자 찾기 기능 추가
    - 최대 이동 횟수를 기준으로 우승자 이름을 반환하는 `findWinnerNames` 메서드 추가
    - 참가 자동차 중 최대 이동 횟수를 찾는 `findMaxMoveCount` 메서드 추가
    - 최대 이동 횟수가 없을 경우 예외를 발생시키도록 처리

--- src/main/java/racingcar/domain/model/RacingParticipants.java
+++ src/main/java/racingcar/domain/model/RacingParticipants.java
@@ -3,6 +3,8 @@ package racingcar.domain.model;
 import java.util.List;
 
 public class RacingParticipants {
+    private static final String FIND_MAX_FAILURE_MESSAGE = "Move Count의 최대값을 찾을 수 없습니다.";
+
     private final List<Car> cars;
 
     private RacingParticipants(List<Car> cars) {
@@ -23,5 +25,22 @@ public class RacingParticipants {
 
     public List<Car> getCars() {
         return cars;
+    }
+
+    public List<String> findWinnerNames() {
+        int maxMoveCount = findMaxMoveCount();
+        return cars.stream()
+                .filter(car -> car.getMoveCount() == maxMoveCount)
+                .map(Car::getName)
+                .toList();
+    }
+
+    private int findMaxMoveCount() {
+        return cars.stream()
+                .mapToInt(Car::getMoveCount)
+                .max()
+                .orElseThrow(
+                        () -> new IllegalArgumentException(FIND_MAX_FAILURE_MESSAGE)
+                );
     }
 }
