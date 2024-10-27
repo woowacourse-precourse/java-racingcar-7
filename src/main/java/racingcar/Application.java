0addd2aec5f52ae83a0c3a754471533c75ce8e5e
jaepyo99 <jaepyo99@naver.com>
2024-10-27 15:33:50 +0900

    refactor: GameController 인터페이스로 교체

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import racingcar.controller.domain.GameController;
 import racingcar.controller.infrastructure.RaceController;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -7,7 +8,7 @@ import racingcar.view.OutputView;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RaceController controller = new RaceController(new InputView(), new OutputView());
+        GameController controller = new RaceController(new InputView(), new OutputView());
         controller.run();
     }
 }
