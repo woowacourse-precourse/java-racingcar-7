75c27dfaf40ef3d515d458f1daf21ae3352f5340
hwangdaesun <hds650588@gmail.com>
2024-10-27 16:20:05 +0900

    feat : 시도할 횟수를 입력받는 메서드 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,11 +5,19 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public static final InputView INSTANCE = new InputView();
     private static final String PROMPT_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String PROMPT_TIME = "시도할 횟수는 몇 회인가요?";
     private InputView(){}
 
     public String readNames(){
         System.out.println(PROMPT_NAME);
         String input = Console.readLine();
         return input;
+    }
+
+    public Integer readTime(){
+        System.out.println(PROMPT_TIME);
+        String input = Console.readLine();
+        Console.close();
+        return Integer.valueOf(input);
     }
 }
