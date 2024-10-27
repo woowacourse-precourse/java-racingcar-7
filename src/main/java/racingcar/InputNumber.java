8e30bd66849ec4d2e73c3a7b397c61a14cf2736b
seyeon22 <sywolleych@gmail.com>
2024-10-27 23:59:17 +0900

    feat(InputNumber): 시도할 횟수 입력받는 클래스 작성

---
+++ src/main/java/racingcar/InputNumber.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputNumber {
+    public int TryNumber() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int input;
+        try {
+            input = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+
+        if (input<1)
+            throw new IllegalArgumentException();
+
+        return input;
+    }
+}
