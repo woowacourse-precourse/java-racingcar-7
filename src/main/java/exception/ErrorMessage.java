bd43b65c063e86e08921fe8a4b7d78db42e1eecf
aaahyunseo <hs030608@naver.com>
2024-10-27 23:50:58 +0900

    feat(ErrorMessage): 에러 메세지 추가

--- src/main/java/exception/ErrorMessage.java
+++ src/main/java/exception/ErrorMessage.java
@@ -3,7 +3,8 @@ package exception;
 public enum ErrorMessage {
     NAME_LENGTH_EXCESS("[ERROR] 자동차 이름이 5글자를 초과하였습니다."),
     NAME_EMPTY("[ERROR] 자동차 이름이 비어있습니다."),
-    NAME_DUPLICATE("[ERROR] 자동차 이름이 중복되었습니다.");
+    NAME_DUPLICATE("[ERROR] 자동차 이름이 중복되었습니다."),
+    INVALID_ATTEMPTS_RANGE("[ERROR] 시도 횟수는 1회 이상이어야 합니다.");
 
     private final String errorMessage;
 
