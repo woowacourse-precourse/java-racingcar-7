8f2309837ea5f402ce0e04cf113e021339c74e62
JEN <ekffuclxk@gmail.com>
2024-10-27 21:33:02 +0900

    refactor(application): cars -> carNames 변수명 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -26,7 +26,7 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputCarNames = Console.readLine();
 
-        List<String> cars = splitCarNames(inputCarNames);
+        List<String> carNames = splitCarNames(inputCarNames);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String inputGameCount = Console.readLine();
