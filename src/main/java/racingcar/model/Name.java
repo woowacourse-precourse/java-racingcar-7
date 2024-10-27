b621b703875c1d40d593f8672cb46cc426538e7e
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:27:30 +0900

    :sparkles: feat(Name): 이름이 5자를 초과할 경우, 예외를 발생시킨다

--- src/main/java/racingcar/model/Name.java
+++ src/main/java/racingcar/model/Name.java
@@ -1,10 +1,12 @@
 package racingcar.model;
 
 import static racingcar.message.ErrorMessages.CLEAR_NAME;
+import static racingcar.message.ErrorMessages.LONG_NAME;
 
 import racingcar.exception.InvalidInputException;
 
 public class Name {
+    private static final int MAX_NAME_LENGTH = 5;
     private final String name;
 
     public Name(String name) {
@@ -19,11 +21,18 @@ public class Name {
 
     private void validate(String name) {
         validateClearName(name);
+        validateLongName(name);
     }
 
     private void validateClearName(String name) {
         if (name == null || name.isBlank()) {
             throw new InvalidInputException(CLEAR_NAME);
+        }
+    }
+
+    private void validateLongName(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new InvalidInputException(LONG_NAME);
         }
     }
 }
