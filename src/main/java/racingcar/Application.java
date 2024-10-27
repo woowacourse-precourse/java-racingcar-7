ed3c80675ed38d4633f1a986b0b0d644f9cff282
MinJae00 <exceedmin@gmail.com>
2024-10-28 03:51:59 +0900

    refact: 의미 없는 input 변수 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,6 +4,6 @@ import racingcar.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
-        InputView input = new InputView();
+        new InputView();
     }
 }
