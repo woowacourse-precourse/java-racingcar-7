d0e50b50f9fe63b865fb6a0b3d86ede5767aece4
DongYeop <s01054830455@gmail.com>
2024-10-27 15:40:58 +0900

    refactor: 예외처리 로직을 InputValidation 클래스로 이동

--- src/main/java/racingcar/exception/InputValidation.java
+++ src/main/java/racingcar/exception/InputValidation.java
@@ -2,14 +2,40 @@ package racingcar.exception;
 
 public final class InputValidation {
 
+    private static final String NOT_CONTAIN_BLANK_MESSAGE = "공백을 입력할 수 없습니다.";
+    private static final String BLANK = " ";
+
     private InputValidation() {
     }
 
-    public static boolean validateNullOrEmpty(String input) {
+    public static void validateCarNames(String carNames) {
+        validate(carNames, "자동차 이름을 입력해야 합니다.");
+    }
+
+    public static void validateDelimiter(String[] splitCarNames) {
+        for (String splitCarName : splitCarNames) {
+            validate(splitCarName, "구분자를 연속해서 입력할 수 없습니다.");
+        }
+    }
+
+    public static void validateRacingCount(String racingCount) {
+        validate(racingCount, "시도 횟수를 입력해야 합니다.");
+    }
+
+    private static void validate(String input, String exceptionMessage) {
+        if (InputValidation.validateNullOrEmpty(input)) {
+            throw new IllegalArgumentException(exceptionMessage);
+        }
+        if (InputValidation.validateBlank(input)) {
+            throw new IllegalArgumentException(NOT_CONTAIN_BLANK_MESSAGE);
+        }
+    }
+
+    private static boolean validateNullOrEmpty(String input) {
         return input == null || input.isEmpty();
     }
 
-    public static boolean validateBlank(String input) {
-        return input.contains(" ");
+    private static boolean validateBlank(String input) {
+        return input.contains(BLANK);
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,46 +7,21 @@ import racingcar.util.Convertor;
 
 public class InputView {
 
-    private static final String NOT_CONTAIN_BLANK_MESSAGE = "공백을 입력할 수 없습니다.";
-
     public List<String> inputCarNames() {
         String carNames = Console.readLine();
-        validateCarNames(carNames);
+        InputValidation.validateCarNames(carNames);
 
         String[] splitCarNames = carNameSplit(carNames);
-        validateDelimiter(splitCarNames);
+        InputValidation.validateDelimiter(splitCarNames);
 
         return Convertor.toList(splitCarNames);
     }
 
     public int inputRacingCount() {
         String racingCount = Console.readLine();
-        validateRacingCount(racingCount);
+        InputValidation.validateRacingCount(racingCount);
 
         return toInt(racingCount);
-    }
-
-    private void validateCarNames(String carNames) {
-        validateInput(carNames, "자동차 이름을 입력해야 합니다.");
-    }
-
-    private void validateDelimiter(String[] splitCarNames) {
-        for (String splitCarName : splitCarNames) {
-            validateInput(splitCarName, "구분자를 연속해서 입력할 수 없습니다.");
-        }
-    }
-
-    private void validateRacingCount(String racingCount) {
-        validateInput(racingCount, "시도 횟수를 입력해야 합니다.");
-    }
-
-    private void validateInput(String input, String exceptionMessage) {
-        if (InputValidation.validateNullOrEmpty(input)) {
-            throw new IllegalArgumentException(exceptionMessage);
-        }
-        if (InputValidation.validateBlank(input)) {
-            throw new IllegalArgumentException(NOT_CONTAIN_BLANK_MESSAGE);
-        }
     }
 
     private String[] carNameSplit(String carNames) {
