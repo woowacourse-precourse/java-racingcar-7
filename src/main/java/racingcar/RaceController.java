8f2ed5b28f623dd1d18cbb5b3610f8967e279967
문고은 <>
2024-10-27 18:26:07 +0900

    feat(controller) : 레이스 진행 상황 출력 및 전진 기능 추가
    무작위 값이 4 이상일 경우 1씩 전진하는 기능 추가
    매 횟수마다 진행 상황을 출력하는 기능 추가

--- src/main/java/racingcar/RaceController.java
+++ src/main/java/racingcar/RaceController.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 
 public class RaceController {
@@ -11,6 +12,24 @@ public class RaceController {
     public RaceController(LinkedHashMap<Car, Integer> cars, int attemptCount) {
         this.attemptCount = attemptCount;
         this.cars = cars;
+    }
+
+    public void start() {
+        OutputPrinter printer = new OutputPrinter();
+        for (int i = 0; i < attemptCount; i++) {
+            forward();
+            printer.showRace(cars);
+        }
+    }
+
+    private void forward() {
+        Iterator<Car> carIterator = cars.keySet().iterator();
+        while (carIterator.hasNext()) {
+            Car car = carIterator.next();
+            if (getRandomNumber() >= 4) {
+                cars.put(car, cars.get(car) + 1);
+            }
+        }
     }
 
     private int getRandomNumber() {
