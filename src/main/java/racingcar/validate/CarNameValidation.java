f87e6d1ec24c6b2812946ce57dcfe73fc15759b9
etd93 <etd93@naver.com>
2024-10-27 20:56:42 +0900

    feat : 자동차 이름의 길이가 5이하인지 유효성 검증 메서드 구현

--- src/main/java/racingcar/validate/CarNameValidation.java
+++ src/main/java/racingcar/validate/CarNameValidation.java
@@ -1,21 +1,30 @@
 package racingcar.validate;
 
+import racingcar.validate.enums.ValidateMessages;
+
 public class CarNameValidation {
     public static void validateName(String name) {
         validateIfCarNameIsNUllOrEmpty(name);
+        validateIfCarNameHasNewline(name);
         validateIfCarNameLengthWithinFive(name);
     }
 
     public static void validateIfCarNameIsNUllOrEmpty(String name) {
         if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("자동차 이름을 지어주세요.");
+            throw new IllegalArgumentException(ValidateMessages.CAR_CANNOT_BE_NULL_OR_EMPTY.getValidateMessage());
+        }
+    }
+
+    public static void validateIfCarNameHasNewline(String name) {
+        if (name.contains("\n")) {
+            throw new IllegalArgumentException(ValidateMessages.CAR_NAME_CANNOT_CONTAIN_NEWLINE.getValidateMessage());
         }
     }
 
     public static void validateIfCarNameLengthWithinFive(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException(
-                    "자동차 이름은 5글자 이하여야 합니다.");
+                    ValidateMessages.CAR_NAME_LENGTH_CANNOT_BE_OVER_FIVE.getValidateMessage());
         }
     }
 }
---
+++ src/main/java/racingcar/validate/enums/ValidateMessages.java
@@ -0,0 +1,18 @@
+package racingcar.validate.enums;
+
+public enum ValidateMessages {
+
+    CAR_CANNOT_BE_NULL_OR_EMPTY("자동차 이름을 지어주세요."),
+    CAR_NAME_LENGTH_CANNOT_BE_OVER_FIVE("자동차 이름은 5글자 이하여야 합니다."),
+    CAR_NAME_CANNOT_CONTAIN_NEWLINE("자동차 이름에 개행 문자가 존재할 수 없습니다.");
+
+    private final String validateMessage;
+
+    ValidateMessages(String validateMessage) {
+        this.validateMessage = validateMessage;
+    }
+
+    public String getValidateMessage() {
+        return validateMessage;
+    }
+}
