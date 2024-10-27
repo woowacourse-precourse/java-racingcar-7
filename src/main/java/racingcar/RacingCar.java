864373cbbe562c7043fc08c20b2b1f93ad52f7bb
skadjs <skadjs11@gmail.com>
2024-10-28 00:32:21 +0900

    feat(RacingCar): 사용자 입력값 공백 제거 기능 추가
    사용자로부터 받아온 자동차 이름 문자열에서 공백을 제거하는 removeBlank() 함수 추가

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -9,4 +9,10 @@ public class RacingCar {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         this.carNames = readLine();
     }
+
+    public void removeBlank() {
+        this.carNames = carNames.replaceAll(" ", "");
+    }
+
+
 }
