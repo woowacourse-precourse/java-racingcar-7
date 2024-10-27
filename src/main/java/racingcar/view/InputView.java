8b977e2620898f5a1d519f04edf7ece77793f0cc
slay <palgalow@gmail.com>
2024-10-27 23:48:28 +0900

    refactor(InputView): InputView에서 RaceService 삭제
    InputView는 입력만 담당하게 함

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,11 +7,6 @@ public class InputView {
 
     private final String INPUT_CAR_NAMES_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final String INPUT_TRY_COUNT_MSG = "시도할 횟수는 몇 회인가요?";
-    private RaceService raceService;
-
-    public InputView() {
-        this.raceService = new RaceService();
-    }
 
     public String inputCarNames() {
         System.out.println(INPUT_CAR_NAMES_MSG);
