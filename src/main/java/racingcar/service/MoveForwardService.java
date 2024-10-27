a4deb1b9383598bfb0abbb640d08e105e638a8de
dongyun <ehddbs452100@naver.com>
2024-10-27 23:44:22 +0900

    refactor(OutputView, MoveForwardService): seperate update movingforward method from OutputView to MoveForwardService
    - OutputView 클래스는 출력만 담당하는 메소드가 맞다고 판단이 들어 MoveForwardService 클래스에서
    전진횟수만큼 반복문을 실행하여 업데이트를하고 출력을하는게 각 클래스별 역할에 맞다고 판단
    - 인스턴스 메서드를 사용 할 일이 없다고 판단이 들고 객체의 성질을 이용하지 않는다고 생각하여 static으로 변경

--- src/main/java/racingcar/service/MoveForwardService.java
+++ src/main/java/racingcar/service/MoveForwardService.java
@@ -1,8 +1,11 @@
 package racingcar.service;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.domain.MoveForwardRecord;
+import racingcar.view.OutputView;
 
 public class MoveForwardService {
     private List<MoveForwardRecord> moveForwardRecords;
@@ -16,4 +19,21 @@ public class MoveForwardService {
     public List<MoveForwardRecord> getMoveForwardRecords() {
         return moveForwardRecords;
     }
+
+    public void printMovingForwardProcess(String tryingCount) {
+        OutputView.printMoveForwardResult();
+
+        for (int i = 0; i < Integer.parseInt(tryingCount); i++) {
+            increaseMoveForwardCount();
+            moveForwardRecords.stream().map(MoveForwardRecord::getGoCount).forEach(System.out::println);
+            OutputView.printMoveForwardRecord(moveForwardRecords);
+            System.out.println();
+        }
+    }
+
+    private void increaseMoveForwardCount() {
+        moveForwardRecords.stream()
+                .forEach(moveForwardRecord -> moveForwardRecord.updateGoCount(pickNumberInRange(0, 9)));
+    }
+
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,6 +1,5 @@
 package racingcar.view;
 
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 import static racingcar.global.Message.MOVE_FORWARD_RESULT;
 import static racingcar.global.Message.WINNER;
 
@@ -12,29 +11,17 @@ public class OutputView {
     private static final String COLON = " : ";
     private static final String BAR = "-";
 
-    public void printWinner(List<String> winners) {
+    public static void printWinner(List<String> winners) {
         System.out.print(WINNER.getMessage());
         System.out.print(String.join(",", winners));
     }
 
-    public void printMoveForwardResult() {
+    public static void printMoveForwardResult() {
         System.out.println(MOVE_FORWARD_RESULT.getMessage());
     }
 
-    public void printMoveForwardRecord(List<MoveForwardRecord> moveForwardRecords) {
+    public static void printMoveForwardRecord(List<MoveForwardRecord> moveForwardRecords) {
         moveForwardRecords.stream()
                 .forEach(record -> System.out.println(record.getCarName() + COLON + BAR.repeat(record.getGoCount())));
-        System.out.println();
-    }
-
-    public void go(String tryCounts, List<MoveForwardRecord> moveForwardRecords) {
-        printMoveForwardResult();
-
-        for (int i = 0; i < Integer.parseInt(tryCounts); i++) {
-            moveForwardRecords.stream()
-                    .forEach(moveForwardRecord -> moveForwardRecord.updateGoCount(pickNumberInRange(0, 9)));
-            printMoveForwardRecord(moveForwardRecords);
-        }
-
     }
 }
