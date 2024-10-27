f05bdf281cf51ed7b7157c001e8a9b10c74cd032
taek2222 <oot309@daum.net>
2024-10-27 17:37:29 +0900

    style(RandomUtilTest): 상수 재사용과 Static Import 간소화
    - 상수를 활용해 숫자를 재사용하는 방향으로 변경했습니다.
    
    - Static Import 내용을 간소화해 가독성을 향상시켰습니다.

--- src/test/java/racingcar/util/RandomUtilTest.java
+++ src/test/java/racingcar/util/RandomUtilTest.java
@@ -1,6 +1,8 @@
 package racingcar.util;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+import static racingcar.util.RandomUtil.*;
+
 import org.junit.jupiter.api.RepeatedTest;
 
 class RandomUtilTest {
@@ -8,9 +10,9 @@ class RandomUtilTest {
     @RepeatedTest(value = 10, name = "반복 검증 :: {currentRepetition}/{totalRepetitions}")
     void 무작위_값은_범위내_숫자만_추출한다() {
         // given
-        int randomNumber = RandomUtil.generateRandomNumber();
+        int randomNumber = generateRandomNumber();
 
         // when & then
-        Assertions.assertTrue(randomNumber >= 0 && randomNumber <= 9);
+        assertTrue(randomNumber >= RANDOM_MIN_RANGE && randomNumber <= RANDOM_MAX_RANGE);
     }
 }
