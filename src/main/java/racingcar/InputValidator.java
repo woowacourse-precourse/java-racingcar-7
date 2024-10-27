051c71b637ad6926ec2943573006f5bb85d355c7
JiEung2 <181757@jnu.ac.kr>
2024-10-27 21:44:01 +0900

    feat(inputValidator): 자동차 이름 공백 검증 메서드 추가

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class InputValidator {
     private final int CAR_NAME_LENGTH_LIMIT = 5;
     private final String CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 5자 이하만 가능합니다.";
+    private final String CAR_NAME_EMPTY_ERROR_MESSAGE = "자동차 이름은 공백으로 구성될 수 없습니다.";
 
     public void validateCarNames(List<String> carNames) {
 
@@ -13,6 +14,12 @@ public class InputValidator {
     private void validateCarNameLength(String carName) {
         if (carName.length() > CAR_NAME_LENGTH_LIMIT) {
             throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateCarNameNotEmpty(String carName) {
+        if (carName.trim().isEmpty()) {
+            throw new IllegalArgumentException(CAR_NAME_EMPTY_ERROR_MESSAGE);
         }
     }
 }
