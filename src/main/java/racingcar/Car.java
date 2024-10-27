1e4451c60c28fe482cdc99f78470cc794dd8c34f
동원 <ai06021@gmail.com>
2024-10-27 23:26:56 +0900

    feat : introduce message

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int location;
+
+    public Car(String name) {
+        this.name = name;
+        this.location = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+}
