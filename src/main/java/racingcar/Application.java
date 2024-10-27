2438ceec2bf698b6e22d3df4095f0ad280d952a0
wlsdlf2 <wlsdlfzg@naver.com>
2024-10-27 23:15:30 +0900

    feat: 사용자 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.validator.InputValidator;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String strPlayer = Console.readLine();
+        InputValidator.validatePlayer(strPlayer);
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String strRound = Console.readLine();
+        InputValidator.validateRound(strRound);
     }
 }
