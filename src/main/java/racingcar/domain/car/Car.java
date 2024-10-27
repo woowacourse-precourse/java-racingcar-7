edc94c01c339334213f80897cfbbe26188a11dff
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-10-27 22:31:02 +0900

    feat(Car): 이동 전략을 주입받아 자동차 생성, 이름 및 위치 조회 구현

---
+++ src/main/java/racingcar/domain/car/Car.java
@@ -0,0 +1,24 @@
+package racingcar.domain.car;
+
+import racingcar.domain.car.strategy.MoveStrategy;
+
+public class Car {
+    private final String name;
+    private final MoveStrategy moveStrategy;
+
+    private long position;
+
+    public Car(String name, MoveStrategy moveStrategy) {
+        this.name = name;
+        this.moveStrategy = moveStrategy;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getPosition() {
+        return position;
+    }
+}
