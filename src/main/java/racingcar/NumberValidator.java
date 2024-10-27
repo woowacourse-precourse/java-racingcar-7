42aae4e421e2baaefa3ef34aa8ea7472c04542c7
Seongmo Ahn <djgood780@gmail.com>
2024-10-27 17:56:16 +0900

    refactor(NumberValidator): inputPositiveNumber 메서드 로직 분리
    - inputPositiveNumber 메서드에서 음수인지 판단하는 부분을 isNegativeNumber 메서드로 분리

--- src/main/java/racingcar/NumberValidator.java
+++ src/main/java/racingcar/NumberValidator.java
@@ -2,8 +2,12 @@ package racingcar;
 
 public class NumberValidator {
     public void inputPositiveNumber(int number) {
-        if (number <= 0) {
+        if (isNegativeNumber(number)) {
             throw new IllegalArgumentException("시도할 횟수를 양수로 입력해주세요");
         }
+    }
+
+    private boolean isNegativeNumber(int number) {
+        return number <= 0;
     }
 }
