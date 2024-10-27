4c63c33086e4ba2f7a8a514f7a477145ba440b7d
Parkjyun <jaeyun0507@naver.com>
2024-10-28 00:54:47 +0900

    feat(RoundCount): 1 이상의 정수가 들어오지 않으면 예외를 발생시킨다

--- src/main/java/racingcar/model/RoundCount.java
+++ src/main/java/racingcar/model/RoundCount.java
@@ -8,13 +8,24 @@ public class RoundCount {
     private final int roundCount;
 
     public RoundCount(String roundCount) {
-        validateNumberFormat(String.valueOf(roundCount));
+        validateRoundCount(roundCount);
         this.roundCount = Integer.parseInt(roundCount);
+    }
+
+    private void validateRoundCount(String input) {
+        validateNumberFormat(input);
+        validatePositiveRoundCount(input);
     }
 
     private void validateNumberFormat(String input) {
         if (!NUMBER_REGEX.matcher(input).matches()) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_ROUND_COUNT.getMessage());
+        }
+    }
+
+    private void validatePositiveRoundCount(String input) {
+        if (Integer.parseInt(input) <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.ROUND_COUNT_OUT_OF_RANGE.getMessage());
         }
     }
 }
--- src/test/java/racingcar/model/RoundCountTest.java
+++ src/test/java/racingcar/model/RoundCountTest.java
@@ -3,6 +3,8 @@ package racingcar.model;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.ErrorMessage;
 
 class RoundCountTest {
@@ -26,5 +28,18 @@ class RoundCountTest {
         Assertions.assertThatThrownBy(() -> new RoundCount(invalidInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.INVALID_ROUND_COUNT.getMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("양수가 아닌 수를 입력하면 예외가 발생한다.")
+    @ValueSource(strings = {"-1", "0"})
+    void createRoundCountWithNegativeOrZero() {
+        // given
+        String negativeInput = "-1";
+        String zeroInput = "0";
+        // when, then
+        Assertions.assertThatThrownBy(() -> new RoundCount(zeroInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.ROUND_COUNT_OUT_OF_RANGE.getMessage());
     }
 }
