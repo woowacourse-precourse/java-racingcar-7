169be43e7b44ed1ccf80024b228cd948e22d2251
longrunpc <pcjmo4050@gmail.com>
2024-10-28 03:10:39 +0900

    feat(RacingGameController): 자동차 경주 게임 전체 진행 로직 구현
    - 자동차 이름과 시도 횟수를 입력받고 게임을 진행하는 run 메서드 추가
    - 각 라운드에서 MoveStrategy에 따라 자동차를 전진시키고 결과를 출력
    - 최종 우승자 출력 기능 구현
    - InputView와 OutputView를 통해 사용자 입력과 출력 처리 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+import racingcar.model.MoveStrategy;
+import racingcar.model.RandomMoveStrategy;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        MoveStrategy moveStrategy = new RandomMoveStrategy();
+        RacingGameController controller = new RacingGameController(moveStrategy,inputView, outputView);
+        controller.run();
     }
 }
---
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -0,0 +1,36 @@
+// RacingGameController.java
+package racingcar.controller;
+
+import racingcar.model.MoveStrategy;
+import racingcar.model.RacingGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class RacingGameController {
+    private final MoveStrategy moveStrategy;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RacingGameController(MoveStrategy moveStrategy,InputView inputView, OutputView outputView) {
+        this.moveStrategy = moveStrategy;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        List<String> carNames = inputView.getCarNames();
+        int raceCount = inputView.getRaceCount();
+        RacingGame racingGame = new RacingGame(carNames);
+
+        outputView.printStartMessage();
+
+        for (int i = 0; i < raceCount; i++) {
+            racingGame.playRound(moveStrategy);
+            outputView.printRoundResult(racingGame.getCars());
+        }
+
+        outputView.printWinners(racingGame.getWinners());
+    }
+}
