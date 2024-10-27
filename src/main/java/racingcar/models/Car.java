858380964aa8d0e04bcc128e4108561ec9d5a2bf
sangxxjin <sj_0368@naver.com>
2024-10-27 23:05:44 +0900

    feat: Car 모델 구현
    본문:
    자동차를 모델화해서 이름과 거리를 상태로 갖게 만들었습니다.

---
+++ src/main/java/racingcar/models/Car.java
@@ -0,0 +1,12 @@
+package racingcar.models;
+
+public class Car {
+    private int distance;
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+}
