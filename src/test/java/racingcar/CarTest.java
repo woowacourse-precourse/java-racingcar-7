83f7855b98262fb688f73ec3fa9c37e1657811b9
김동진 <dongjin403@naver.com>
2024-10-27 16:48:53 +0900

    test : Car 객체의 메소드 테스트 작성

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,25 +1,47 @@
 package racingcar;
 
+import static console.Message.OUTPUT_SEPARATOR;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
 
-    @DisplayName("Car의 메소드들이 정상적으로 작동하는지 확인한다.")
+    @DisplayName("makeHyphen 함수가 advanceCount과 동일한 Hyphen을 출력하는지 확인")
     @Test
-    void carMethodTest() {
+    void makeHyphenTest() {
         String carName = "testCar";
+
         Car car = new Car(carName);
 
-        assertThat(carName).isEqualTo(car.getName());
-        assertThat(0).isEqualTo(car.getAdvanceCount());
         assertThat("").isEqualTo(car.makeHyphen());
+
         car.addAdvanceCount();
-        assertThat(1).isEqualTo(car.getAdvanceCount());
+
         assertThat("-").isEqualTo(car.makeHyphen());
+
+        car.addAdvanceCount();
+
+        assertThat("--").isEqualTo(car.makeHyphen());
     }
 
+    @DisplayName("printRacingInfo 함수가 중간출력을 정상적으로 하는지 확인")
+    @Test
+    void printRacingInfoTest() {
+        String carName = "testCar";
+        Car car = new Car(carName);
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        String expectedOutput = carName + OUTPUT_SEPARATOR + car.makeHyphen() + "\r\n";
+
+        car.printRacingInfo();
+
+        assertThat(outContent.toString()).isEqualTo(expectedOutput);
+        // 원래 출력 스트림 복원
+        System.setOut(System.out);
+    }
 
 }
