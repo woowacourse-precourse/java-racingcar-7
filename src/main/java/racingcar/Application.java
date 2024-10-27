264f5eb7167d7822857e663e76c36ae4f8e8560d
sso9594 <sso9594@naver.com>
2024-10-27 17:37:28 +0900

    feat(Application): 자동차 경주 프로그램 초기화 및 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.service.RacingService;
+import racingcar.view.RacingInputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingInputView racingInputView = new RacingInputView();
+        RacingService racingService = new RacingService();
+
+        RacingController racingController = new RacingController(racingInputView, racingService);
+        racingController.run();
     }
 }
