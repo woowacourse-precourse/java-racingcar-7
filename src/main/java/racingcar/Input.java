ed2563893d29ef4fcad96b95bf72ba2fbdf8d42c
동원 <ai06021@gmail.com>
2024-10-27 23:29:28 +0900

    refactor : input cars Class add

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -5,22 +5,28 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Input {
-    private List<String> cars;
+    private List<Car> cars;
     public int tryCount() {
-        int count = Integer.parseInt(getReadLine());
-        if (count < 0) {
-            throw new IllegalArgumentException("잘못된 시도횟수 입니다");
+        System.out.println(Message.INPUT_TRY_COUNT);
+        try {
+            int count = Integer.parseInt(getReadLine());
+            if (count < 0) {
+                throw new IllegalArgumentException("잘못된 시도횟수 입니다");
+            }
+            return count;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("숫자를 입력해야 합니다");
         }
-        return count;
     }
 
-    public List<String> carNames() {
+    public List<Car> carNames() {
+        System.out.println(Message.INPUT_CAR_NAMES);
         String[] names = getReadLine().split(",");
         for (String name : names) {
             if (name.length() > 5) {
                 throw new IllegalArgumentException("잘못된 차량이름 입니다");
             }
-            cars.add(name);
+            cars.add(new Car(name));
         }
 
         return cars;
