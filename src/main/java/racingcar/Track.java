2e71d2c2677ff2a6ce1c1af24b3971fb92a3a0e0
Lee Jeong Woo <sysliar@gmail.com>
2024-10-27 21:02:50 +0900

    feat(Track): 우승자 출력

--- src/main/java/racingcar/Track.java
+++ src/main/java/racingcar/Track.java
@@ -38,4 +38,15 @@ public class Track {
         }
         return winners;
     }
+
+    public void printWinners() {
+        ArrayList<Car> winners = getWinners();
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < winners.size(); i++) {
+            System.out.print(winners.get(i).getName());
+            if (i != winners.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
 }
