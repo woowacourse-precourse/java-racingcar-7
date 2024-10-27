96e9d8341849c4e27cb692b34cd5f5fbd313f164
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 13:02:43 +0900

    :sparkles: feat(Winners): 가장 멀리 이동한 자동차를 우승자 또는 공동 우승자로 결정

--- src/main/java/racingcar/model/Winners.java
+++ src/main/java/racingcar/model/Winners.java
@@ -1,4 +1,25 @@
 package racingcar.model;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Winners {
+    private static final int MIN_POSITION = 0;
+
+    private final int maxPosition;
+    private final List<Car> winners;
+
+    public Winners(List<Car> cars) {
+        maxPosition = getMaxPosition(cars);
+        winners = cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .collect(Collectors.toList());
+    }
+
+    private int getMaxPosition(List<Car> cars) {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(MIN_POSITION);
+    }
 }
