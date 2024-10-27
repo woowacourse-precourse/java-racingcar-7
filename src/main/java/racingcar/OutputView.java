227a251b888db8644a424b0713ba177f224ac80d
JiEung2 <181757@jnu.ac.kr>
2024-10-28 00:08:25 +0900

    feat(outputView): 라운드별 진행 상황을 출력하는 메서드 추가

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.List;
+
 public class OutputView {
     private final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final String ROUND_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
@@ -10,5 +12,11 @@ public class OutputView {
 
     public void printRoundCountInputMessage() {
         System.out.println(ROUND_COUNT_INPUT_MESSAGE);
+    }
+
+    public void printRoundResult(List<Car> carList) {
+        for (Car car : carList) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
     }
 }
