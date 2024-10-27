b431b4ea7faca45ced86d69d0e6c0c53d780b373
wodus4145@naver.com <wodus4145@naver.com>
2024-10-27 14:41:08 +0900

    feat: Creat ResultPrinter class

---
+++ src/main/java/racingcar/ResultPrinter.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import java.util.List;
+
+public class ResultPrinter {
+
+    public void printRoundResult(List<Car> racingCar) {
+        for(Car car : racingCar) {
+            String statusBar = "-".repeat(car.advance);
+            System.out.println(car.name + " : " + statusBar);
+        }
+        System.out.println();
+    }
+
+    public void printWinners(String winners) {
+        System.out.println("최종 우승자 : " + winners);
+    }
+}
