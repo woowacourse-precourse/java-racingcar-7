c41b085056ea8d26490fc5a106f70eaca1db6a0e
youngchai <80930129+eahc00@users.noreply.github.com>
2024-10-27 17:43:48 +0900

    feat : 경주 횟수 입력 받기 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,12 +5,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         Cars cars = inputCars();
+        int count = inputCount();
     }
 
     private static Cars inputCars() {
         Cars cars = new Cars(Console.readLine());
         return cars;
+    }
+
+    private static int inputCount() {
+        return Integer.parseInt(Console.readLine());
     }
 }
