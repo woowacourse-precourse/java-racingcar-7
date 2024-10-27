84c175e734db647cd797b90fecb8f7fd238fd74b
Park, Sojeong <sojeong2101@gmail.com>
2024-10-27 16:17:53 +0900

    feat: 입력 받는 Input 객체 구현
    1. 자동차 이름을 입력 받는 getCarNames() 메서드 구현
    2. 경기 횟수를 입력 받는 getRound() 메서드 구현

---
+++ src/main/java/racingcar/Input.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public static String getCarNames() {
+        return Console.readLine();
+    }
+
+    public static String getRound() {
+        return Console.readLine();
+    }
+}
