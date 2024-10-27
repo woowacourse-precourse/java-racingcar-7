b5836f46c0e2e2f40e4a9cd18bc729c14e90fbd1
Su-daa <subin.kk019@gmail.com>
2024-10-28 00:06:51 +0900

    feat: 사용자 입력 부분 질문 나오도록 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,7 +3,13 @@ package racingcar.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
-    public String input() {
+    public String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return readLine();
+    }
+
+    public String inputAttemptCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
         return readLine();
     }
 }
