ca5cf02054e03be3624f964980b0f9df15bc63ff
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:02:57 +0900

    :sparkles: feat(InputView): 경주할 자동차 이름들을 입력받는다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,13 @@
 package racingcar.view;
 
+import static racingcar.message.InfoMessages.INPUT_CAR_NAMES;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public String readCarNames() {
+        System.out.println(INPUT_CAR_NAMES);
+        return Console.readLine();
+    }
 }
