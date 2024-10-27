5ee02aa9b8ef8cb404adcf17a9ee1efc32d65544
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-28 03:08:48 +0900

    feat(RaceResultDTO): 경주를 한 라운드 진행할 때마다 객체를 생성했던 기존 코드 수정
    - 기존에는 경주가 진행될 때마다 매 시행 후 객체를 재생성했었는데, 성능적으로 좋지 않을 것이라고 판단해서 updateResults() 메서드를 추가

--- src/main/java/racingcar/DTO/RaceResultDTO.java
+++ src/main/java/racingcar/DTO/RaceResultDTO.java
@@ -3,7 +3,7 @@ package racingcar.DTO;
 import java.util.HashMap;
 
 public class RaceResultDTO {
-    HashMap<String, Integer> carList = new HashMap<>();
+    HashMap<String, Integer> carList;
 
     public RaceResultDTO(HashMap<String, Integer> carList) {
         this.carList = carList;
@@ -11,5 +11,9 @@ public class RaceResultDTO {
 
     public HashMap<String, Integer> getCarList() {
         return carList;
+    }
+
+    public void updateResults(HashMap<String, Integer> carList) {
+        this.carList = carList;
     }
 }
