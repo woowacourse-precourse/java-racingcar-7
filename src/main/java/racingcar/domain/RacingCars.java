02ca93c31697e8d900e7d20f927b85dcf4153e06
young-in <hardworkingini@gmail.com>
2024-10-27 21:57:57 +0900

    feat : 2대 이상의 경주용 자동차 등록을 위한 예외 구현

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -5,10 +5,12 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class RacingCars {
+private static final int MINIMUM_CAR_COUNT = 2;
     private final List<RacingCar> racingCars;
 
     public RacingCars(List<RacingCar> racingCars) {
         validateDuplicateRacingCarNames(racingCars);
+        validateMinimumCarCount(racingCars);
         this.racingCars = racingCars;
     }
 
@@ -19,6 +21,12 @@ public class RacingCars {
 
         if (racingCarNames.size() != racingCars.size()) {
             throw new IllegalArgumentException("자동차 이름을 중복으로 사용할 수 없습니다.");
+        }
+    }
+
+    private void validateMinimumCarCount(List<RacingCar> racingCars) {
+        if (racingCars.size() < MINIMUM_CAR_COUNT) {
+            throw new IllegalArgumentException("자동차 경주를 위해서는 최소 2대 이상의 자동차를 만들어야 합니다.");
         }
     }
 }
