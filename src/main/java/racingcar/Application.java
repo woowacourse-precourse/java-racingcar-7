2b7bf3de97c4a67d585c2d5bd3ede3cb88080184
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-10-27 20:57:45 +0900

    feat(Application): 게임을 진행하는 함수인 playGame함수를 정의한다
    playGame함수에 입력받은 게임 횟수를 파라미터로 받는다. while문 안에서 게임 진행
    코드를 작성하도록 하고, 남은 게임 횟수가 0이 되면 종료한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,11 +3,15 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 public class Application {
 
     static List<String> participantList = new ArrayList<>();
+    static Map<String, Integer> map = new HashMap<>();
 
     public static boolean isValidName(String name) {
 
@@ -22,6 +26,25 @@ public class Application {
 
     }
 
+    public static void goRandomPlay(){
+
+
+    }
+
+    //주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다.
+    //  - 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
+    //- 자동차 경주 게임을 완료한 후 누가 우승했는지를 출력한다.
+    //    - 우승자는 여러 명일 수 있다.
+    //    - 우승자가 여러 명일 경우, 쉼표를 이용하여 구분한다.
+    public static void playGame(String number) {
+        int playNumber = Integer.parseInt(number);
+        while (playNumber != 0) {
+            goRandomPlay();
+            playNumber--;
+        }
+
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         try {
@@ -29,7 +52,8 @@ public class Application {
             String participants = Console.readLine();
             splitParticipantString(participants);
             System.out.println("시도할 횟수는 몇 회인가요?");
-            String input= Console.readLine();
+            String playNumber = Console.readLine();
+            playGame(playNumber);
         } catch (Error e) {
             throw new IllegalArgumentException("입력이 잘못되었습니다");
         } finally {
