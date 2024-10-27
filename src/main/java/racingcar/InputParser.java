44333e17bcdf520a1c7ea4aaf222c196fbc44ee1
문고은 <>
2024-10-27 16:36:47 +0900

    feat(validator) : 자동차 이름 중복 시 예외 처리 기능 추가

--- src/main/java/racingcar/InputParser.java
+++ src/main/java/racingcar/InputParser.java
@@ -12,8 +12,20 @@ public class InputParser {
     public LinkedHashMap<Car, Integer> splitByComma() {
         LinkedHashMap<Car, Integer> result = new LinkedHashMap<>();
         for (String carName : carNames.split(",")) {
+            if (isDuplicateCarName(result, carName)) {
+                throw new IllegalArgumentException("자동차 이름이 중복되었습니다.");
+            }
             result.put(new Car(carName), 0);
         }
         return result;
+    }
+
+    private boolean isDuplicateCarName(LinkedHashMap<Car, Integer> cars, String carName) {
+        for (Car car : cars.keySet()) {
+            if (car.getCarName().equals(carName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
