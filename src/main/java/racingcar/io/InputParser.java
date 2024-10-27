b9463914ef603936ba11d3721e5cfb6d28a03554
yyysolhhh <solbaram37@gmail.com>
2024-10-28 03:22:51 +0900

    fix(InputParser): split 인자에 limit -1 추가
    parseCarNames
    - 마지막에 구분자가 왔을 때 예외처리하기 위해 추가

--- src/main/java/racingcar/io/InputParser.java
+++ src/main/java/racingcar/io/InputParser.java
@@ -4,6 +4,6 @@ public class InputParser {
     private static final String NAME_DELIMITER = ",";
 
     public String[] parseCarNames(String input) {
-        return input.split(NAME_DELIMITER);
+        return input.split(NAME_DELIMITER, -1);
     }
 }
