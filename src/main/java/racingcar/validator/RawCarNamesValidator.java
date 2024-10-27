fcb5abe6d122ff43eeaf07a04c938bc62209e5f3
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-28 04:27:32 +0900

    refactor(RawCarNamesValidator): 예외 처리 방식 변경
    - 정적 팩토리 메서드 사용으로 `IllegalArgumentException` 처리 방식 개선

--- src/main/java/racingcar/validator/RawCarNamesValidator.java
+++ src/main/java/racingcar/validator/RawCarNamesValidator.java
@@ -1,12 +1,13 @@
 package racingcar.validator;
 
+import racingcar.exception.RacingException;
 import racingcar.view.ErrorMessage;
 
 public class RawCarNamesValidator {
 
     public void isEmpty(String rawCarNames) {
         if (rawCarNames == null || rawCarNames.isBlank()) {
-            throw new IllegalArgumentException(ErrorMessage.NO_INPUT.getMessage());
+            throw RacingException.from(ErrorMessage.NO_INPUT);
         }
     }
 }
