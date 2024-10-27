6005b3397e52ec8f69887d5a36968da7b541bcc5
minzzun99 <skg06123@naver.com>
2024-10-27 23:02:47 +0900

    refactor(InputView): 상수 처리 및 printInputMessage 메서드 이름 수정
    - 예외 메시지 상수 처리
    - printInputMessage() 메서드 이름 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,7 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.NoSuchElementException;
 
 public class InputView {
-    public static String printInputMessage(String message) {
+    private static final String NULL_INPUT_ERROR = "Null이 입력되었습니다.";
+    private static final String EMPTY_INPUT_ERROR = "입력된 값이 없습니다.";
+
+    public static String requestInput(String message) {
         System.out.println(message);
 
         return getInput();
@@ -17,13 +20,13 @@ public class InputView {
 
             return input;
         } catch (NoSuchElementException e) {
-            throw new IllegalArgumentException("Null이 입력되었습니다.");
+            throw new IllegalArgumentException(NULL_INPUT_ERROR);
         }
     }
 
     private static void validateInput(String input) {
         if (input.trim().isEmpty()) {
-            throw new IllegalArgumentException("입력된 값이 없습니다.");
+            throw new IllegalArgumentException(EMPTY_INPUT_ERROR);
         }
     }
 }
