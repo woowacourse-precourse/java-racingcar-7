55ed7daa3a3be34d65588a3dec91f356eab2f549
woong2e <tlswodnd12345@naver.com>
2024-10-27 20:13:42 +0900

    feat: implement Application class for car racing game with exception handling

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Race;
+import racingcar.io.InputReader;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputReader inputReader = new InputReader();
+        try {
+            String[] carNames = inputReader.readLine1();
+            int num = inputReader.readLine2();
+            Race race = new Race(carNames, num);
+            race.start();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } finally {
+            inputReader.close();
+        }
     }
 }
