f74e190da9acb713a601e5595639d9b87c904778
steamedEggMaster <cksdud5014@naver.com>
2024-10-27 15:39:09 +0900

    feat(OutputView): 우승자 출력을 위한 OutputView 클래스 정의
    1. DI를 구현하기 위해 싱글톤 방식으로 OutputView 구성
    2. 종료 시 인스턴스 해제를 위해 close() 메서드를 통해 null 처리

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,26 @@
+package racingcar.view;
+
+public class OutputView {
+    private static final String RESULT_MESSAGE = "최종 우승자 : ";
+    private static OutputView outputView;
+
+    private OutputView(){
+    }
+
+    public static OutputView getInstance() {
+        if (outputView == null) {
+            outputView = new OutputView();
+        }
+        return outputView;
+    }
+
+    public void printResult(String result) {
+        System.out.print(RESULT_MESSAGE + result);
+    }
+
+    public void closeOutputView() {
+        if (outputView != null) {
+            outputView = null;
+        }
+    }
+}
