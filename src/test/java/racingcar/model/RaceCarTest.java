59d75237fbc1542e7d17916292146196ef834ee6
sinminseok12 <sinminseok12@naver.com>
2024-10-27 17:19:37 +0900

    test : RaceCar 우승 자동차 판별 기능 테스트
    파라미터로 전달받은 우승자 위치와 동일하면 true, 다르면, false 를 반환한다

--- src/test/java/racingcar/model/RaceCarTest.java
+++ src/test/java/racingcar/model/RaceCarTest.java
@@ -3,6 +3,7 @@ package racingcar.model;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -10,6 +11,14 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.*;
 
 public class RaceCarTest {
+
+    @ParameterizedTest
+    @MethodSource("provideRaceCarNames")
+    void 자동차_이름_생성_테스트(String inputName, String expectedName) {
+        RaceCar raceCar = RaceCar.fromName(inputName);
+
+        assertThat(raceCar.getName()).isEqualTo(expectedName);
+    }
 
     @Test
     void 자동차_이동_테스트() {
@@ -19,12 +28,12 @@ public class RaceCarTest {
         assertThat(raceCar.getPosition()).isEqualTo(1);
     }
 
-
     @ParameterizedTest
-    @MethodSource("provideRaceCarNames")
-    void 자동차_이름_생성_테스트(String inputName, String expectedName) {
-        RaceCar raceCar = RaceCar.fromName(inputName);
-        assertThat(raceCar.getName()).isEqualTo(expectedName);
+    @CsvSource(value = {"0, true", "1, false"})
+    void 우승자_판별_테스트(int winPosition, boolean result) {
+        RaceCar raceCar = RaceCar.fromName("car");
+
+        assertThat(raceCar.isWinner(winPosition)).isEqualTo(result);
     }
 
     static Stream<Arguments> provideRaceCarNames() {
