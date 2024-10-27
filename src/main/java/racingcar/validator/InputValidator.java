47c31a5efe11f9eabe77a7fbed1181a3d43ced97
Jewoo Lee <jaewoo0254@gmail.com>
2024-10-28 08:59:56 +0900

    refactor: refactor InputValidator and fix typos in README

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -7,6 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class InputValidator {
+  private static final BigInteger MAX_ATTEMPTS = new BigInteger("100");
+
   private void validateNotEmpty(String input) {
     if (input.isEmpty()) {
       throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT);
@@ -62,7 +64,6 @@ public class InputValidator {
   }
   private void validateMaxAttemptCount(String attemptsInput) {
     BigInteger attemptsCount = new BigInteger(attemptsInput);
-    BigInteger MAX_ATTEMPTS = new BigInteger("1000");
     if (attemptsCount.compareTo(MAX_ATTEMPTS) > 0) {
       throw new IllegalArgumentException("Error: 시도 횟수는 " + MAX_ATTEMPTS + " 이하여야 합니다.");
     }
