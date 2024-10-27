affa961e046ea7367666e4805c5ca61fc7b81351
jeonghyeon-ohk <ok6737@naver.com>
2024-10-27 18:08:49 +0900

    feat(validator) : Random 변수가 4 이상인지 판단하는 기능 구현
    - isNumberOverFour : 입력된 숫자가 4 이상인지 판단

--- src/main/java/racingcar/validator/NumberValidator.java
+++ src/main/java/racingcar/validator/NumberValidator.java
@@ -2,12 +2,12 @@ package racingcar.validator;
 
 public class NumberValidator{
 
-    public static void validateNumericValue(String input){
+    public static void validateNumericValue(String input) {
         isNumericValue(input);
         isPositiveInteger(input);
     }
 
-    public static void isNumericValue(String input){
+    public static void isNumericValue(String input) {
         try {
             int convert = Integer.parseInt(input);
         } catch (NumberFormatException e) {
@@ -16,9 +16,12 @@ public class NumberValidator{
     }
 
     public static void isPositiveInteger(String input) {
-            if(Integer.parseInt(input) < 1){
-                throw new IllegalArgumentException("양의 정수 값이 아닙니다: " + input);
-            }
+        if (Integer.parseInt(input) < 1) {
+            throw new IllegalArgumentException("양의 정수 값이 아닙니다: " + input);
+        }
     }
 
+    public static boolean isNumberOverFour(int input) {
+        return input >= 4;
+    }
 }
