f1a0c1f874b7c7d4d4415710191d083666ac71bc
Wooseok Jang <wowseok2774@gmail.com>
2024-10-27 17:23:49 +0900

    feat: printEmptyLine, printExecutionResult 함수 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,19 +5,27 @@ import model.Car;
 
 public class OutputView {
 
-    static public void printGuide() {
+    public static void printGuide() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    static public void printAttemptPrompt() {
+    public static void printAttemptPrompt() {
         System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+    public void printEmptyLine() {
+        System.out.println();
+    }
+
+    public void printExecutionResult() {
+        System.out.println("실행결과");
     }
 
     public void printRaceStatus(List<Car> cars) {
         for (Car car : cars) {
             System.out.println((getPositionStatus(car)));
         }
-        System.out.println();
+        printEmptyLine();
     }
 
     public void printFinalWinner(List<String> finalWinners) {
