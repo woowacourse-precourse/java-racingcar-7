ba01ece4817818d780743aaaaa42db2b5b7c8349
Lee-KwanBeom <dmtmqhdkk@naver.com>
2024-10-27 20:24:29 +0900

    feat : 자동차 이름 받기
    -String로 받은 문자열 공백제거
    -String => String[] 변환 작업

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    public static final int NAME_HANDLING_CRITERIA = 5;
+    public static final int RANDOM_CONDITIONS = 4;
+    public static final String BAR = "-";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 자동차 이름 받기
+        // String => String[] 을 위한 split
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] carNames = input.replaceAll(" ", "").split(",");
+
     }
 }
