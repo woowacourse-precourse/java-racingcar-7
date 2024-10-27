30c220526806648c0e9867b475089b301162d2c2
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 11:57:54 +0900

    :sparkles: feat(InfoMessages): 실행 횟수를 입력하라는 메시지 제공

--- src/main/java/racingcar/message/InfoMessages.java
+++ src/main/java/racingcar/message/InfoMessages.java
@@ -4,4 +4,5 @@ public class InfoMessages {
 
     // Input related info messages
     public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    public static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
 }
