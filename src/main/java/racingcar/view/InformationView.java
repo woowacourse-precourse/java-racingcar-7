f49eec3aeee2330789eb59915c9d9587da216ea1
young-in <hardworkingini@gmail.com>
2024-10-27 23:20:45 +0900

    feat : 실행 결과를 위한 안내 문구 작성 완료

--- src/main/java/racingcar/view/InformationView.java
+++ src/main/java/racingcar/view/InformationView.java
@@ -3,6 +3,8 @@ package racingcar.view;
 public class InformationView {
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+    private static final String NEXT_LINE = "\n";
+    private static final String EXECUTION_RESULT = "실행 결과";
 
     public void printRequestCarNames(){
         System.out.println(INPUT_CAR_NAMES);
@@ -10,5 +12,9 @@ public class InformationView {
 
     public void printRequestTryCount(){
         System.out.println(INPUT_TRY_COUNT);
+    }
+
+    public void printExecutionResult(){
+        System.out.println(NEXT_LINE + EXECUTION_RESULT);
     }
 }
