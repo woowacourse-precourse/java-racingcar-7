bde2b57ff369e7ebc396c77b5465677a72f994de
parksaegyeol <dmsgkrkt1@naver.com>
2024-10-27 20:42:43 +0900

    style : 테스트용 최종 코드 재제출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,6 @@ package racingcar;
 
 
 public class Application {
-
     public static void main(String[] args) {
         String inputCarNames = RacingCar.inputCarNames();
         int inputTrial = RacingCar.inputTrial();
--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -27,11 +27,9 @@ class Car {
         return this.go;
     }
 
-
 }
 
 public class RacingCar {
-
 
     public static String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
