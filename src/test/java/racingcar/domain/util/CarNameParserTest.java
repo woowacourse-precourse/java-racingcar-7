416ccddbe33be900407687c1401fde0da2c877f0
minSsan <msp1125@hanyang.ac.kr>
2024-10-28 00:59:18 +0900

    test(CarNameParser): 정상 케이스 추가

--- src/test/java/racingcar/domain/util/CarNameParserTest.java
+++ src/test/java/racingcar/domain/util/CarNameParserTest.java
@@ -5,10 +5,25 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarNameParserTest {
+    @Test
+    @DisplayName("올바른 입력 값을 파싱할 수 있다.")
+    void 정상_케이스() {
+        // given
+        final String input = "철수, 영희, 맹구";
+
+        // when
+        List<String> parsed = CarNameParser.parse(input);
+
+        // then
+        assertThat(parsed).containsSequence("철수", "영희", "맹구");
+    }
+
     @ParameterizedTest
     @DisplayName("빈 문자열은 자동차 이름으로 입력할 수 없다")
     @ValueSource(strings = {"", "철수,,영희", "     , 철수"})
