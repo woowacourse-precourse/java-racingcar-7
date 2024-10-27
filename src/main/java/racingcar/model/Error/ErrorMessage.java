26d1819b18533d6cc6fa9b378639420b0d1a7dd5
minju <minju9711@naver.com>
2024-10-27 16:03:36 +0900

    feat: 에러 발생시 함께 보여줄 에러 메시지 클래스 추가

---
+++ src/main/java/racingcar/model/Error/ErrorMessage.java
@@ -0,0 +1,11 @@
+package racingcar.model.Error;
+
+public class ErrorMessage {
+    public static final String CarNameMaxis5Length = "자동차 이름은 최대 5글자 입니다.";
+    public static final String IsNotNumber = "숫자 값을 입력해 주세요.";
+    public static final String RaceNumIsMinus = "경주 횟수는 0 이상 이어야 합니다.";
+    public static final String CarNameMustBeUnique = "자동차 이름은 중복이 불가능 합니다.";
+    public static final String InputMustHaveValue = "값을 입력해 주세요.";
+    public static final String NotAllowedDelimiter = "정해진 문자 외 특수 문자는 입력 하실 수 없습니다.";
+    public static final String NotValidType = "유효하지 않은 타입입니다.";
+}
