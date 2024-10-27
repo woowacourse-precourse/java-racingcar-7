f7019988106d35062090c820a1cd4b2009270db0
pil-go <pilbongx@naver.com>
2024-10-28 05:24:50 +0900

    refactor(Validator): 라운드 입력값 유효 검증 수정

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class Validator {
     private static final String ERROR_MESSAGE_INPUT_EMPTY = "자동차 이름을 입력해주세요.";
-    private static final String ERROR_MESSAGE_ROUND_FORMAT = "숫자만 입력 가능해요.";
-    private static final String ERROR_MESSAGE_ROUND_RANGE = "자연수만 입력 가능해요";
+    private static final String ERROR_MESSAGE_ROUND_FORMAT = "정수만 입력 가능해요.";
+    private static final String ERROR_MESSAGE_ROUND_RANGE = "0이상만 입력 가능해요";
     private static final String ERROR_MESSAGE_UNIQUE_NAME = "자동차 이름은 중복될 수 없어요.";
     private static final String ERROR_MESSAGE_NAME_LENGTH = "자동차 이름은 5자 이하만 가능해요.";
     private static final String ERROR_MESSAGE_NAME_EMPTY = "자동차 이름은 공백이 불가능해요.";
@@ -18,14 +18,18 @@ public class Validator {
     }
 
     public static void checkRound(String input) {
-        int round;
+        inputRoundDigit(input);
+        roundRange(Integer.parseInt(input));
+    }
 
-        try {
-            round = Integer.parseInt(input);
-        } catch (NumberFormatException e) {
+    private static void inputRoundDigit(String input) {
+        if (!input.matches("\\d+")) {
             throw new IllegalArgumentException(ERROR_MESSAGE_ROUND_FORMAT);
         }
-        if (round <= 0) {
+    }
+
+    private static void roundRange(int round){
+        if(round <= 0 ){
             throw new IllegalArgumentException(ERROR_MESSAGE_ROUND_RANGE);
         }
     }
@@ -36,7 +40,7 @@ public class Validator {
         carNamesBlank(carList);
     }
 
-    public static void carNamesUnique(List<Car> carList) {
+    private static void carNamesUnique(List<Car> carList) {
         long uniqueCount = carList.stream()
                 .map(Car::getName)
                 .distinct()
@@ -47,7 +51,7 @@ public class Validator {
         }
     }
 
-    public static void carNamesLength(List<Car> carList) {
+    private static void carNamesLength(List<Car> carList) {
         for (Car car : carList) {
             if (car.getName().length() > carList.size()) {
                 throw new IllegalArgumentException(ERROR_MESSAGE_NAME_LENGTH);
@@ -55,7 +59,7 @@ public class Validator {
         }
     }
 
-    public static void carNamesBlank(List<Car> carList) {
+    private static void carNamesBlank(List<Car> carList) {
         for (Car car : carList) {
             if (car.getName().isBlank()) {
                 throw new IllegalArgumentException(ERROR_MESSAGE_NAME_EMPTY);
