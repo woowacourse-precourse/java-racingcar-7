ca7e683ec90ce38f14f329255f5806d8f5f6b32f
나상연 <58220151+ense333@users.noreply.github.com>
2024-10-27 21:20:38 +0900

    feat(racingcar): add playCar logic and distance visualization to Car class
    - Added the playCar method to determine movement based on random number.
    - Implemented distance tracking and visualization using the 'view' string.
    - Added the printDistance method to display car name and its movement visually.
    - Introduced getters to retrieve car name and distance for further use.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,37 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
     private String carName;
     private int distance;
+    //얼마나 이동했는지 표시하기 위한 문자열 변수
+    private String view;
 
     public Car(String carName){
         this.carName = carName;
         distance = 0;
+        view = "";
+    }
+
+    public void playCar(){
+        if(Randoms.pickNumberInRange(0,9) >= 4){
+            distance++;
+            view += "-";
+        }
+        printDistance();
+    }
+
+    private void printDistance(){
+        System.out.println(carName + " : " + view);
+    }
+
+    public int getInstance(){
+        return distance;
+    }
+
+    public String getCarName(){
+        return carName;
     }
 }
