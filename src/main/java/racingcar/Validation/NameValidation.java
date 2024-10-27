6ab077d77d503d33360f8fa642846c40c065b5a2
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 21:28:07 +0900

    feat(NameValidation): 자동차 이름과 관련된 검증 로직 구현
    - 자동차 이름과 관련된 검증 로직 구현
    - 뷰에서 바로 예외를 처리할 수 있는 입력 관련 예외와 내용을 검증해야 하는 내용 관련 예외로 나눔
    - 입력 관련 예외 : 이름이 빈 문자열인 경우, 이름이 공백인 경우
    - 내용 관련 예외 : 이름이 5글자 초과인 경우, 이름이 중복되는 경우

--- src/main/java/racingcar/Validation/NameValidation.java
+++ src/main/java/racingcar/Validation/NameValidation.java
@@ -1,4 +1,52 @@
 package racingcar.Validation;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import racingcar.Util.Exception.ErrorMessage;
+
 public class NameValidation {
+    public static boolean isLongName(List<String> names) {
+        for (String name : names)
+        {
+            if (name.length() > 5) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isWhitespace(List<String> names) {
+        for (String name : names)
+        {
+            if (name != null && name.trim().isEmpty()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isEmpty(List<String> names) {
+        for (String name : names)
+        {
+            if (name != null && name.isEmpty()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean hasDuplicate(List<String> carNames) {
+        Set<String> uniqueNames = new HashSet<>(carNames);
+        return uniqueNames.size() != carNames.size();
+    }
+
+    public static void validateName(String name) {
+        if (NameValidation.isEmpty(List.of(name))) {
+            throw new IllegalArgumentException(ErrorMessage.NAME_EMPTY.getMessage());
+        }
+        if (NameValidation.isWhitespace(List.of(name))) {
+            throw new IllegalArgumentException(ErrorMessage.NAME_WHITESPACE.getMessage());
+        }
+    }
 }
