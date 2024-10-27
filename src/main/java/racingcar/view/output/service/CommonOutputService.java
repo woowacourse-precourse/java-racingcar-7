c9cd60509865208b4c6cad3ffdc1af4b85a72725
EHCTH <lees90896@gmail.com>
2024-10-27 12:18:55 +0900

    feat(CommonOutputService): interface 공통의 출력

--- src/main/java/racingcar/view/output/service/CommonOutputService.java
+++ src/main/java/racingcar/view/output/service/CommonOutputService.java
@@ -1,4 +1,10 @@
 package racingcar.view.output.service;
 
+import racingcar.racingcar.dto.RacingCarInfo;
+import racingcar.trynumber.domain.TryNumber;
+
 public interface CommonOutputService {
+    void carOutputView(RacingCarInfo racingCarInfo);
+    void tryNumberOutputView(TryNumber tryNumber);
+
 }
