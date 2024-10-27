5ac9715333770da7c6afff5c349b92d1cfe3f324
Seongmo Ahn <djgood780@gmail.com>
2024-10-27 13:44:26 +0900

    test(ApplicationTest): 이름을 5자 이하로 입력하였는지 검증 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -85,4 +85,18 @@ class ApplicationTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(expectedMessage);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123456", "BMW X5 M", "Porsche", "Ferrari"})
+    @DisplayName("이름을 5자 이하로 작성하지 않았으면 예외 발생")
+    void inputNotMoreThanFiveCharacter_InputtedOverFiveCharacter_ExceptionThrown(String carNames){
+        //given
+        final String expectedMessage = "이름을 5자 이하로 입력해주세요";
+        final CarNameValidator carNameValidator = new CarNameValidator();
+
+        //when & then
+        assertThatThrownBy(() -> carNameValidator.inputNotMoreThanFiveCharacter(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(expectedMessage);
+    }
 }
