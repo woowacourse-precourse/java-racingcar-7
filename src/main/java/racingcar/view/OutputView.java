b6592cbffaf6c334010a7c0a01563edb7d0b5893
Hyso <hsk7953@gmail.com>
2024-10-28 03:46:29 +0900

    refactor(OutputView) : OutputView 를 static 에서 인스턴스 기반으로 변경 - static 메서들을 인스턴스 메서드로 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,18 +5,16 @@ import racingcar.model.Car;
 import java.util.List;
 
 public class OutputView {
-    private OutputView() {
-    }
 
-    public static void requestCarNames() {
+    public void requestCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public static void requestTrialCount() {
+    public void requestTrialCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
     }
 
-    public static void printRacing(List<Car> racingCars) {
+    public void printRacing(List<Car> racingCars) {
         StringBuilder currentPosition = new StringBuilder();
         for (Car car : racingCars) {
             currentPosition.append(car.getName()).append(" : ")
@@ -26,7 +24,7 @@ public class OutputView {
         System.out.println(currentPosition.toString());
     }
 
-    public static void printWinner(List<String> winners){
+    public void printWinner(List<String> winners) {
         StringBuilder result = new StringBuilder();
         result.append("최종 우승자 : ")
                 .append(String.join(", ", winners));
