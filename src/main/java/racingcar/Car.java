63663e2398ac0ca25fa385a47111d900ac6f6275
HyeonJeong <wgwjh05169@gmail.com>
2024-10-27 18:46:23 +0900

    refactor(Car): 자동차에서 사용되는 숫자를 정적 불변 값으로 수정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,16 +1,18 @@
 package racingcar;
 
 public class Car {
+    private static final int INITIAL_SCORE = 0;
+    private static final int BOUND_VALUE = 0;
     private final String name;
     private int score;
 
     public Car(String name) {
         this.name = name;
-        this.score = 0;
+        this.score = INITIAL_SCORE;
     }
 
     public void go(int randomValue) {
-        if (randomValue >= 4) {
+        if (randomValue >= BOUND_VALUE) {
             score++;
         }
     }
