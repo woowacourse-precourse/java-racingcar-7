7fcc21ccaa801511d3b57d0b9fcf3962c83b665d
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-10-28 03:19:44 +0900

    feat: 자동차 이름과 시도 횟수 입력 로직 생성

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,5 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Racing {
+    private String cars;
+    private Integer attemptsCnt;
+
     Racing() {}
+
+    public void input() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        cars = Console.readLine();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        attemptsCnt = Integer.valueOf(Console.readLine());
+    }
 }
