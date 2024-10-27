e9a23ee42c5f54f9ac52854beee3f0834a1ff052
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-10-27 22:32:45 +0900

    test(CarTest): 위치이동 후 위치 확인 테스트
    - 항상 이동하는 전략
    - 항상 이동하지 않는 전략

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.car.strategy.FixedMoveStrategy;
+import racingcar.domain.car.strategy.StopMoveStrategy;
 
 class CarTest {
     @Test
@@ -32,6 +33,36 @@ class CarTest {
         long actual = car.getPosition();
 
         //then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("위치 이동 - 항상 이동하는 전략")
+    void moveWithAlwaysMoveStrategy() {
+        //given
+        Car car = new Car("testName", new FixedMoveStrategy());
+        long expected = 1L;
+
+        //when
+        car.move();
+
+        //then
+        long actual = car.getPosition();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("위치 이동 - 항상 이동하지 않는 전략")
+    void moveWithNeverMoveStrategy() {
+        //given
+        Car car = new Car("testName", new StopMoveStrategy());
+        long expected = 0L;
+
+        //when
+        car.move();
+
+        //then
+        long actual = car.getPosition();
         assertThat(actual).isEqualTo(expected);
     }
 }
