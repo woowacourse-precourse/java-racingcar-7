fe424b95c62517e8bc5c062934bc69b611941d7f
RawBrain <95132660+RawBrain@users.noreply.github.com>
2024-10-28 04:44:40 +0900

    feat(view/output): implement race result output view
    경주 진행상황과 결과 출력 기능 구현

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,4 +1,22 @@
 package racingcar;
 
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OutputView {
+    public static void reportProgress(List<Car> raceMember) {
+        for (Car car : raceMember) {
+            System.out.print(car.getCarName() + " : ");
+            System.out.println("-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    public static void reportRaceResult(List<Car> winners) {
+        String winnerNames = winners.stream()
+                .map(Car::getCarName)
+                .collect(Collectors.joining(", "));
+        System.out.println("최종 우승자 : " + winnerNames);
+    }
 }
