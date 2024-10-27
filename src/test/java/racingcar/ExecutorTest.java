168d2a81e4cd09cda4db0228ea62003178c8df91
chyo1 <hyowon3856@gmail.com>
2024-10-28 03:28:04 +0900

    chore : 변수명 변경

--- src/test/java/racingcar/ExecutorTest.java
+++ src/test/java/racingcar/ExecutorTest.java
@@ -20,61 +20,23 @@ class ExecutorTest extends NsTest {
     Executor executor = new Executor(ioController, parser, validator);
 
     Car testCar1, testCar2, testCar3;
-    List<Car> testCarList;
+    List<Car> testCars;
 
     @BeforeEach
     void setUp() {
         testCar1 = new Car("test1");
         testCar2 = new Car("test2");
         testCar3 = new Car("test3");
-        testCarList = new ArrayList<>(List.of(testCar1, testCar2, testCar3));
-    }
-
-    @Test
-    void 특정_횟수만큼_반복해서_자동차_이동() {
-        assertRandomNumberInRangeTest(
-            () -> {
-                executor.executeForCount(testCarList, 2);
-                assertThat(testCar1.getMoveCount()).isEqualTo(1);
-                assertThat(testCar2.getMoveCount()).isEqualTo(1);
-                assertThat(testCar3.getMoveCount()).isEqualTo(2);
-            },
-            MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
-            STOP, STOP, MOVING_FORWARD
-        );
-    }
-
-    @Test
-    void 자동차_이동() {
-        assertRandomNumberInRangeTest(
-            () -> {
-                executor.moveCar(testCar1);
-                assertThat(testCar1.getMoveCount()).isEqualTo(1);
-            },
-            MOVING_FORWARD, MOVING_FORWARD
-        );
-    }
-
-    @Test
-    void 자동차_여러대_이동() {
-        assertRandomNumberInRangeTest(
-            () -> {
-                executor.moveCars(testCarList);
-                assertThat(testCar1.getMoveCount()).isEqualTo(1);
-                assertThat(testCar2.getMoveCount()).isEqualTo(1);
-                assertThat(testCar3.getMoveCount()).isEqualTo(0);
-            },
-            MOVING_FORWARD, MOVING_FORWARD, STOP
-        );
+        testCars = new ArrayList<>(List.of(testCar1, testCar2, testCar3));
     }
 
     @Test
     void 우승자_반환() {
         testCar1.move();
-        assertThat(executor.getWinners(testCarList)).isEqualTo(List.of(testCar1));
+        assertThat(executor.getWinners(testCars)).isEqualTo(List.of(testCar1));
 
         testCar2.move();
-        assertThat(executor.getWinners(testCarList)).isEqualTo(List.of(testCar1, testCar2));
+        assertThat(executor.getWinners(testCars)).isEqualTo(List.of(testCar1, testCar2));
     }
 
     @Test
