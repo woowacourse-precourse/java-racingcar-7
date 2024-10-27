2d23c2133a316fffa9809154f271d28b0d1757eb
bellmin <whdlas011203@naver.com>
2024-10-27 16:29:20 +0900

    feat(Car) : Car 클래스 정의
    - name : 자동차 이름
    - location : 자동차의 현재 위치 또는 얼마나 전진하였는지 나타내는 정수

---
+++ src/main/java/racingcar/car/Car.java
@@ -0,0 +1,12 @@
+package racingcar.car;
+
+public class Car {
+
+    private final String name;
+    private final int location;
+
+    public Car(String name, int location) {
+        this.name = name;
+        this.location = location;
+    }
+}
