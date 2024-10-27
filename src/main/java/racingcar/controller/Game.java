00367ea292fcdd413046d5838bdf8bb336438949
JoonKyo <joonkyolee@gmail.com>
2024-10-28 01:14:35 +0900

    feat(Game): 시도 횟수를 입력받고 시도 횟수를 초기화하는 메서드 구현

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -1,15 +1,22 @@
 package racingcar.controller;
 
+import racingcar.domain.AttemptNumber;
 import racingcar.domain.GameBoard;
 import racingcar.view.InputView;
 
 public class Game {
     public void run() {
         GameBoard gameBoard = initializeGameBoard();
+        AttemptNumber attemptNumber = initializeAttemptNumber();
     }
 
     private GameBoard initializeGameBoard() {
         String carNames = InputView.askCarName();
         return GameBoard.from(carNames);
+    }
+
+    private AttemptNumber initializeAttemptNumber() {
+        String attemptNumberString = InputView.askAttemptNumber();
+        return AttemptNumber.from(attemptNumberString);
     }
 }
