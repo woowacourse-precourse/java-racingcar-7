6b819a9e65aecb097a4b82819f63eb270869daa3
dd-jiyun <dlwldbs8015@gmail.com>
2024-10-27 17:02:45 +0900

    refactor(MovementRecord): 각 라운드 저장 시 순서 보장을 위해 LinkedHashMap으로 변경

--- src/main/java/racingcar/model/MovementRecord.java
+++ src/main/java/racingcar/model/MovementRecord.java
@@ -1,7 +1,7 @@
 package racingcar.model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,7 +10,7 @@ public class MovementRecord {
     private final List<Map<String, Integer>> roundRecords = new ArrayList<>();
 
     public void recordRound(final Cars cars) {
-        roundRecords.add(new HashMap<>(cars.getCarPositions()));
+        roundRecords.add(new LinkedHashMap<>(cars.getCarPositions()));
     }
 
     public List<Map<String, Integer>> getRoundRecords() {
