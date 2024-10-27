35cd22aba2125d8f2c9acae99d77b7fe918ba986
Goohong Chung <9rrong@uoslife.team>
2024-10-27 17:25:18 +0900

    feat(OutputView): `우승자 안내 문구`를 출력한다.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,10 +4,17 @@ import java.util.List;
 
 public class OutputView {
 
+    private static final String DELIMITER = ", ";
     private static final String EXECUTION_RESULT_TITLE = "\n실행 결과";
+    private static final String TOTAL_RESULT_PREFIX = "\n최종 우승자 : ";
 
     public void printExecutionResult(List<String> executionResult) {
         System.out.println(EXECUTION_RESULT_TITLE);
         executionResult.forEach(System.out::println);
+    }
+
+    public void printTotalResult(List<String> totalResult) {
+        String result = TOTAL_RESULT_PREFIX + String.join(DELIMITER, totalResult);
+        System.out.println(result);
     }
 }
