dd923844f43bb8a397dfb9bdc7d595a2c1fc3a7c
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-10-28 05:47:01 +0900

    feat(RacingGame): 사용자의 시도 횟수 입력받는 기능 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,16 +1,29 @@
 package racingcar;
 
 import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 
 
 public class RacingGame {
     private final CarNameParser carNameParser;
+    private final AttemptCountParser attemptCountParser;
+    private int attemptCount;
 
-    public RacingGame(CarNameParser carNameParser) {
+    public RacingGame(CarNameParser carNameParser, AttemptCountParser attemptCountParser){
         this.carNameParser = carNameParser;
+        this.attemptCountParser = attemptCountParser;
     }
 
+    /**
+     * 사용자가 시도할 횟수를 입력받는 메서드
+     **/
+    public void getAttemptCount(){
+        this.attemptCount = attemptCountParser.getAttemptCount();
+    }
 
+    /**
+     * 입력받은 자동차 이름 리스트를 반환하는 메서드
+     * */
     public List<String> validatedCarName(){
         String input = carNameParser.inputCarNames();
         String[] splitCarName = carNameParser.splitCarNamesByComma(input);
