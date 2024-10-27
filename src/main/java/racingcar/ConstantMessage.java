de2efc439e7ee660c543bb18f1c38c1cf27d2a27
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-10-27 15:15:31 +0900

    feat: 메시지를 상수화한 클래스 생성

---
+++ src/main/java/racingcar/ConstantMessage.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class ConstantMessage {
+    public static final String FINAL_RESULT = "\n실행 결과";
+    public static final String FINAL_WINNER = "최종 우승자 : ";
+    public static final String COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    public static final String GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_IS_EMPTY = "입력값이 비어있습니다.";
+    public static final String ONLY_COMMA_HERE = "쉼표만 입력되었습니다.";
+    public static final String INCORRECT_TYPE_COMMA_N_NAME = "쉼표 뒤에 이름이 들어오지 않았습니다.";
+    public static final String OVER_5_LENGTH = "이름이 5자를 초과합니다.";
+    public static final String DUPLICATE_NAME = "중복된 이름이 존재합니다.";
+    public static final String BE_NATURAL_NUMBER = "입력값은 1 이상의 자연수여야 합니다.";
+    public static final String BE_DIGIT = "입력값은 숫자여야 합니다.";
+
+    private ConstantMessage() {
+
+    }
+}
