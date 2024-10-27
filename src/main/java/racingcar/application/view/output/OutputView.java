1857bf09d405ee9aea4a02dd893470d23ae91fee
jody816 <81346079+jody816@users.noreply.github.com>
2024-10-28 01:30:47 +0900

    feat: 실행 결과에 대한 출력 메서드 추가

--- src/main/java/racingcar/application/view/output/OutputView.java
+++ src/main/java/racingcar/application/view/output/OutputView.java
@@ -1,8 +1,17 @@
 package racingcar.application.view.output;
 
+import java.util.List;
+import racingcar.application.model.Car;
+
 public interface OutputView {
 
   void printCarNamesInputPrompt();
 
   void printAttemptCountInputPrompt();
+
+  void printStartMessage();
+
+  void printRaceState(List<Car> cars);
+
+  void printEmptyLine();
 }
