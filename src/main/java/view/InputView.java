d1839cb388dbfc493f22bccfd6c05b284409aaea
jjjjjoon <mjj7343@gmail.com>
2024-10-27 23:21:58 +0900

    refactor(InputView): enum을 통해 입력값 받는 메세지 출력

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -13,15 +13,30 @@ public class InputView {
         this.raceCountValidator = raceCountValidator;
     }
 
+    private enum InputMessage {
+        CAR_NAME_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+        RACE_COUNT_MESSAGE("시도할 횟수는 몇 회인가요?");
+
+        private final String message;
+
+        InputMessage(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
     public String getCarName(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(InputMessage.CAR_NAME_MESSAGE.getMessage());
         String carName = Console.readLine();
         carNameValidator.validate(carName);
         return carName;
     }
 
     public int getRaceCount() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(InputMessage.RACE_COUNT_MESSAGE.getMessage());
         String number = Console.readLine();
         Console.close();
         int raceCount = Integer.parseInt(number);
