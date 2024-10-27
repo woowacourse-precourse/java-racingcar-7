e869487759280633ea2d29625ddf8e437035182b
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-27 15:29:32 +0900

    feat(CarNameParser): 자동차 중복 검증 적용

--- src/main/java/racingcar/parser/CarNamesParser.java
+++ src/main/java/racingcar/parser/CarNamesParser.java
@@ -1,5 +1,7 @@
 package racingcar.parser;
 
+import racingcar.validator.InputValidator;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,8 +10,14 @@ public class CarNamesParser {
     private static final String CAR_NAMES_DELIMITER = ",";
 
     public static List<String> parseCarNames(final String input) {
-        return Arrays
-                .stream(input.split(CAR_NAMES_DELIMITER))
+        List<String> names = getCarNames(input);
+        InputValidator.validateDuplicate(names);
+        return names;
+    }
+
+    private static List<String> getCarNames(final String input) {
+        return Arrays.stream(input.split(CAR_NAMES_DELIMITER))
+                .map(String::trim)
                 .toList();
     }
 }
