580be2657cc8547df2a90fb50887d7c7401467db
min429 <lsm990429@gmail.com>
2024-10-27 18:47:04 +0900

    feat: 사용자로부터 자동차 이름 및 시도할 횟수를 입력받는 기능을 구현한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,32 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+    public static List<String> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        List<String> carNames = Arrays.asList(Console.readLine().split(","));
+        validateCarNames(carNames);
+
+        return carNames;
+    }
+
+    public static int getRounds() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해 주세요.");
+        }
+    }
+
+    public static void validateCarNames(List<String> carNames) {
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
+            }
+        }
+    }
     }
 }
