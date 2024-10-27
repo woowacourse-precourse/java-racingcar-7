f21298c40f549503aa2ae00b72c27c35ccb734af
Hyso <hsk7953@gmail.com>
2024-10-28 03:21:58 +0900

    refactor(CarNameValidator) : static 메서드들을 인스턴스 메서드로 변경 - 모든 검증 메서드들을 인스턴스 메서드로 변경

--- src/main/java/racingcar/validator/CarNameValidator.java
+++ src/main/java/racingcar/validator/CarNameValidator.java
@@ -4,10 +4,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class CarNameValidator {
-    private CarNameValidator() {
+    public CarNameValidator() {
     }
 
-    public static void validateCarNames(String input) {
+    public void validateCarNames(String input) {
         validateNotBlank(input);
         validateNoSpace(input);
         validateDelimiter(input);
@@ -22,45 +22,44 @@ public class CarNameValidator {
         validateDuplication(nameSet.size(), carNames.length);
     }
 
-    static void validateCarNameLength(String input) {
+    void validateCarNameLength(String input) {
         if (input.length() > 5) {
             throw new IllegalArgumentException("자동차의 이름은 5자 이하만 가능합니다.");
         }
     }
 
-    static void validateEnd(String input) {
+    void validateEnd(String input) {
         if (input.endsWith(",")) {
             throw new IllegalArgumentException("입력의 마지막은 콤마(,)로 끝날 수 없습니다.");
         }
     }
 
-    static void validateDelimiter(String input) {
+    void validateDelimiter(String input) {
         String regex = "^[^,]*$";
         if (input.matches(regex)) {
             throw new IllegalArgumentException("자동차 이름들은 콤마로 구분해야합니다.");
         }
     }
 
-    static void validateNotBlank(String input) {
+    void validateNotBlank(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException("빈 값은 입력할 수 없습니다.");
         }
     }
 
-    static void validateNoSpace(String input) {
+    void validateNoSpace(String input) {
         if (input.contains(" ")) {
             throw new IllegalArgumentException("입력에 공백이 포함될 수 없습니다.");
         }
     }
 
-    static void validateDuplication(int carSetSize, int carArrayLength) {
+    void validateDuplication(int carSetSize, int carArrayLength) {
         if (carSetSize != carArrayLength) {
             throw new IllegalArgumentException("자동차 이름에 중복이 존재합니다.");
         }
     }
 
-
-    static void validateEmptyName(String input) {
+    void validateEmptyName(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException("자동차의 이름은 빈 값일 수 없습니다.");
         }
