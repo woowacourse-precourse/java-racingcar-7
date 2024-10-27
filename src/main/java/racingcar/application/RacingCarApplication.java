0973cbd65e55c9562d615fb785d20520f1ee8be8
jody816 <81346079+jody816@users.noreply.github.com>
2024-10-27 21:24:16 +0900

    refactor: 의존성 주입을 위한 로직 수정

--- src/main/java/racingcar/application/RacingCarApplication.java
+++ src/main/java/racingcar/application/RacingCarApplication.java
@@ -1,5 +1,7 @@
 package racingcar.application;
 
+import racingcar.application.controller.RacingController;
+import racingcar.application.service.RacingService;
 import racingcar.application.view.input.InputView;
 import racingcar.application.view.input.InputViewImpl;
 import racingcar.application.view.output.OutputView;
@@ -8,12 +10,14 @@ import racingcar.application.view.output.OutputViewImpl;
 public class RacingCarApplication {
 
   public void run() {
+    RacingService racingService = new RacingService();
     InputView inputView = new InputViewImpl();
     OutputView outputView = new OutputViewImpl();
 
-    outputView.printCarNamesInputPrompt();
-    inputView.readInputCarNames();
-    outputView.printAttemptCountInputPrompt();
-    inputView.readInputAttemptCount();
+    RacingController racingController = new RacingController(
+        racingService,
+        inputView,
+        outputView
+    );
   }
 }
