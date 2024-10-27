dec1189bfbf59cc8937aa1409d7878d8cda9a7c6
Seol-Munhyeok <abc1234568@naver.com>
2024-10-27 20:26:08 +0900

    refactor(CarTest): Remove unused import statement

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,10 +1,7 @@
 package racingcar;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
