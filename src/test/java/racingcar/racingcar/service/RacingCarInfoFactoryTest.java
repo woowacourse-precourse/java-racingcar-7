17dd8a782aa2f1338800df46e2d71b8ccd889cef
EHCTH <lees90896@gmail.com>
2024-10-27 12:27:07 +0900

    feat(RacingCarINfoFactoryTest): 중복이름 테스트

--- src/test/java/racingcar/racingcar/service/RacingCarInfoFactoryTest.java
+++ src/test/java/racingcar/racingcar/service/RacingCarInfoFactoryTest.java
@@ -1,4 +1,20 @@
 package racingcar.racingcar.service;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.constant.errormessage.ErrorMessage;
+import racingcar.racingcar.service.create.RacingCarFactory;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class RacingCarInfoFactoryTest {
+    @Test
+    @DisplayName("중복 에러")
+    void duplicateTest() {
+        String sentence = "123,123,456,789";
+        assertThatThrownBy(() ->
+                RacingCarFactory.create(sentence))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.DUPLICATE_ERROR);
+    }
 }
