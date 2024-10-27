2f2894943d1bbbc4eea6e6b3d8ff634801a02380
DevBigOne <a62387272@gmail.com>
2024-10-27 22:09:09 +0900

    [feat] 게임 시작 기능, 시작 메세지 구현

---
+++ src/main/java/racingcar/GameController.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+public class GameController {
+    private final View view;
+
+    public GameController() {
+        this.view = new View();
+    }
+
+    public void run() {
+    }
+}
---
+++ src/main/java/racingcar/View.java
@@ -0,0 +1,7 @@
+package racingcar;
+
+public class View {
+    public void Message() {
+        System.out.println("자동차 대수는 몇 대 인가요?(쉼표(,)를 기준으로 구분)");
+    }
+}
