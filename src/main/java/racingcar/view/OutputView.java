9e0ffd4ba22d996d6f2aa65ef8e034eabfed1ef9
JoonKyo <joonkyolee@gmail.com>
2024-10-28 01:35:51 +0900

    feat(OutputView): 최종 결과를 출력하는 메서드 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 public class OutputView {
     private static final String PLAY_RESULT_HEADING = "\n실행 결과";
+    private static final String END_RESULT_HEADER = "최종 우승자 : ";
 
     public static void printGameResultHeader() {
         System.out.println(PLAY_RESULT_HEADING);
@@ -9,5 +10,9 @@ public class OutputView {
 
     public static void printEachGameResult(String eachGameResult) {
         System.out.println(eachGameResult);
+    }
+
+    public static void printEndResult(String endResult) {
+        System.out.println(END_RESULT_HEADER + endResult);
     }
 }
