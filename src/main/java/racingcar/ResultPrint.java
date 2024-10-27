77013f5268c74605a363611b65ef5826fc728397
Spinichi <spinichi@naver.com>
2024-10-28 03:15:29 +0900

    refactor: ResultPrint 코드 리팩토링
    Test 코드 작성을 위한 코드 리팩토링

--- src/main/java/racingcar/ResultPrint.java
+++ src/main/java/racingcar/ResultPrint.java
@@ -1,14 +1,22 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResultPrint {
-    public static void resultPrint() {
 
+    public static List<String> getResultStrings() {
+        List<String> results = new ArrayList<>();
         for (int i = 0; i < RacingProcess.carNumber; i++) {
-
-            System.out.println(Application.nameList[i] + " : " +Application.processResult[i]);
-
+            results.add(Application.nameList[i] + " : " + Application.processResult[i]);
         }
+        return results;
+    }
 
+    public static void resultPrint() {
+        for (String result : getResultStrings()) {
+            System.out.println(result);
+        }
         System.out.println();
     }
 }
