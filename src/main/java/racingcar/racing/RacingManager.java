0bfb82d71d83f6b32e870267631d948d531226b6
신관규 <soundbar91@gmail.com>
2024-10-27 22:01:01 +0900

    refactor: RacingManager 스태틱 임포트 진행

--- src/main/java/racingcar/racing/RacingManager.java
+++ src/main/java/racingcar/racing/RacingManager.java
@@ -1,14 +1,18 @@
 package racingcar.racing;
 
+import static racingcar.message.ResultPrompt.WINNER_PROMPT;
+import static racingcar.message.UserInputPrompt.CAR_NAME_PROMPT;
+import static racingcar.message.UserInputPrompt.TRY_COUNT_PROMPT;
+import static racingcar.racing.RaceSimulator.getRacingWinner;
+import static racingcar.racing.RaceSimulator.startRace;
+import static racingcar.util.SplitString.SplitCarNames;
+
 import java.util.List;
 import racingcar.car.Car;
 import racingcar.car.CarFactory;
 import racingcar.io.InputUtil;
 import racingcar.io.OutputUtil;
-import racingcar.message.ResultPrompt;
-import racingcar.message.UserInputPrompt;
 import racingcar.util.IntConverter;
-import racingcar.util.SplitString;
 
 public class RacingManager {
     private final InputUtil inputUtil = InputUtil.getInstance();
@@ -20,16 +24,16 @@ public class RacingManager {
     }
 
     public void startRacing() {
-        outputUtil.println(UserInputPrompt.CAR_NAME_PROMPT);
-        List<String> carNames = SplitString.SplitCarNames(inputUtil.InputString());
+        outputUtil.println(CAR_NAME_PROMPT);
+        List<String> carNames = SplitCarNames(inputUtil.InputString());
         List<Car> cars = carNames.stream()
                 .map(carFactory::createOperation)
                 .toList();
 
-        outputUtil.println(UserInputPrompt.TRY_COUNT_PROMPT);
+        outputUtil.println(TRY_COUNT_PROMPT);
         long tryCount = IntConverter.StringToInt(inputUtil.InputString());
 
-        RaceSimulator.startRace(cars, tryCount);
-        outputUtil.print(ResultPrompt.WINNER_PROMPT + RaceSimulator.getRacingWinner(cars));
+        startRace(cars, tryCount);
+        outputUtil.print(WINNER_PROMPT + getRacingWinner(cars));
     }
 }
