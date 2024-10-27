dd8dbf8f1a71c154476b9507b6c8c81d6a207101
wodus4145@naver.com <wodus4145@naver.com>
2024-10-28 00:31:26 +0900

    refactor: Modify InputHandler class
    잘못된 입력값이 들어왔을 때 IllegalArgumentException을 발생시킬 수 있도록 수정

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -11,7 +11,6 @@ public class InputHandler {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputNames = Console.readLine();
         String[] names = divideNames(inputNames);
-        validateNames(names);
         return makeNameList(names);
     }
 
@@ -19,15 +18,16 @@ public class InputHandler {
         return names.split(",");
     }
 
-    private void validateNames(String[] names) throws IllegalArgumentException {
+    public void validateNames(String[] names) throws IllegalArgumentException {
         for(String singleName : names) {
-            if(singleName.length() > 5) {
+            if(singleName.length() > 5 || singleName.isBlank()) {
                 throw new IllegalArgumentException();
             }
         }
     }
 
-    public List<Car> makeNameList(String[] names) {
+    public List<Car> makeNameList(String[] names) throws IllegalArgumentException {
+        validateNames(names);
         List<Car> racingCar = new ArrayList<>();
         for (String name : names) {
             racingCar.add(new Car(name, 0));
@@ -40,9 +40,13 @@ public class InputHandler {
         return validateNumberOfGames(Console.readLine());
     }
 
-    private int validateNumberOfGames(String input) throws IllegalArgumentException {
+    public int validateNumberOfGames(String input) throws IllegalArgumentException {
         try {
-            return Integer.parseInt(input);
+            if(Integer.parseInt(input) <= 0) {
+                throw new IllegalArgumentException();
+            } else {
+                return Integer.parseInt(input);
+            }
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
         }
