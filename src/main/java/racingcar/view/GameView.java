6f64833c2fb39d1650d1b08af3b11a488a08afa2
SeriousBug98 <martin1156@konkuk.ac.kr>
2024-10-28 00:51:28 +0900

    fix(GameView): 사용자의 입력을 받기 위한 문자열 출력 기능 추가

--- src/main/java/racingcar/view/GameView.java
+++ src/main/java/racingcar/view/GameView.java
@@ -7,6 +7,14 @@ import java.util.stream.Collectors;
 
 public class GameView {
 
+    public void displayInputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void displayInputRoundNum() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
     public void displayRoundResult(List<Car> cars) {
         System.out.println("실행결과");
         for (Car car : cars) {
