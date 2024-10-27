96dbd95967f78fb8c08fc768b0d87253da844384
Kim mingyu <minq0302@naver.com>
2024-10-28 01:18:56 +0900

    feat: 입력받은 자동차 이름들로 자동차 객체 생성

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,22 +1,28 @@
 package racingcar.controller;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
+import racingcar.model.Car;
 import racingcar.view.RacingGameView;
 
 public class RacingGameController {
 
     RacingGameView view = new RacingGameView();
+    List<Car> cars = new ArrayList<>();
 
     public void run(){
         view.carNameInputMessage();
         createCarInstances();
         view.trialCountInputMessage();
+
     }
 
-    private List<String> createCarInstances(){
+    private void createCarInstances(){
         List<String> carNames = new ArrayList<>(List.of(view.getCarNames().split(",")));
-        return carNames;
+        for(String name:carNames){
+            cars.add(new Car(name));
+        }
     }
 
 }
