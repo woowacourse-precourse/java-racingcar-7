2723f3b8b86ea9dee7e8569b8ae3832dd59da7a6
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:33:59 +0900

    :sparkles: feat(Car): 자동차 현재 위치 설정 및 반환

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,12 +2,18 @@ package racingcar.model;
 
 public class Car {
     private final Name name;
+    private final Position position;
 
     public Car(String name) {
         this.name = new Name(name);
+        this.position = new Position();
     }
 
     public String getName() {
         return this.name.toString();
+    }
+
+    public int getPosition() {
+        return this.position.toInteger();
     }
 }
