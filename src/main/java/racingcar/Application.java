27b75c7b1e6e516272d000e624fdbc90ac31a60b
Jungwoo Chu <cjw980930@gmail.com>
2024-10-27 22:15:18 +0900

    feat: 자동차 이름 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("자동차 이름을 쉼표(,)로 구분하여 입력해 주세요.");
+        String input = Console.readLine();
+
+        Console.close();
     }
 }
