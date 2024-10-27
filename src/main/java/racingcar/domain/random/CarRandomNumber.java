309431924ad88df95e4472c19bc7e221b87d08bd
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-10-27 19:59:52 +0900

    refactor(CarRandomNumber): 요구사항에 맞추어 1~9 사이의 랜덤 숫자 반환 메서드 수정
    * CarRandomNumber.java: 현재 최댓값을 10으로 설정해서 0~10 사이의 랜덤 숫자가 반환되고 있었음. 따라서 최댓값 9로 수정

--- src/main/java/racingcar/domain/random/CarRandomNumber.java
+++ src/main/java/racingcar/domain/random/CarRandomNumber.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class CarRandomNumber implements RandomNumberGenerator{
 
     private static final int MIN_NUMBER = 0;
-    private static final int MAX_NUMBER = 10;
+    private static final int MAX_NUMBER = 9;
 
     @Override
     public int generateRandomNumber() {
