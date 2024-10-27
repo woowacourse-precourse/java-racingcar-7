ef05ca9a0b79484cd7276f2e6403a22904cc7492
JongKyeong Kim <oo7bangjk@naver.com>
2024-10-27 13:53:00 +0900

    feat: 우승자는 한 명 이상일 수 있다.

--- src/test/java/racingcar/view/OutputTest.java
+++ src/test/java/racingcar/view/OutputTest.java
@@ -41,6 +41,24 @@ public class OutputTest extends NsTest {
         assert expectedOutput.trim().equals(outputStream.toString().trim()) : "출력 값이 예상과 다릅니다!";
     }
 
+    @Test
+    void 우승자는_한_명_이상일_수_있다() {
+        Car car1 = new Car("김종경");
+        car1.move();
+        Car car2 = new Car("송강");
+        car2.move();
+        List<Car> carList = new ArrayList<>();
+        carList.add(car1);
+        carList.add(car2);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        new Output().printWinner(new CarService().getWinners(carList));
+
+        String expectedOutput = "최종 우승자 : 김종경, 송강\n";
+        assert expectedOutput.trim().equals(outputStream.toString().trim()) : "출력 값이 예상과 다릅니다!";
+    }
+
     @Override
     protected void runMain() {
 
