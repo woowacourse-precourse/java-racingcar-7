5cc7840459dfa35379cf55e64c9ff7f9c14fd1f6
201912025 <michi2012@naver.com>
2024-10-27 18:02:33 +0900

    feat(OutputView): printLineSpace() 메서드 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,6 +3,10 @@ package racingcar.view;
 public class OutputView {
 
     public void printCarRacingResult(String carName, String advanceResult) {
-        System.out.println(carName + " : " + advanceResult);
+            System.out.println(carName + " : " + advanceResult);
+    }
+
+    public void printLineSpace() {
+        System.out.println("\n");
     }
 }
