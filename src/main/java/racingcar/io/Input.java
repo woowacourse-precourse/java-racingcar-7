3fd2214bfeb684da7d8e63a98a3eb643e4e6cc70
eeun1225 <eeun9108@gmail.com>
2024-10-28 01:51:38 +0900

    feat: 자동차 이름과 이동 횟수 입력받는 기능 구현

---
+++ src/main/java/racingcar/io/Input.java
@@ -0,0 +1,28 @@
+package racingcar.io;
+
+import racingcar.validation.ValidateInput;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static racingcar.validation.ValidateInput.*;
+
+public class Input {
+
+    public String inputCarName(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String CarName = readLine();
+
+        validateName(CarName);
+
+        return CarName;
+    }
+
+    public String inputNumberOfRound() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String numberOfRounds = readLine();
+
+        validateMoveCount(numberOfRounds);
+
+        return numberOfRounds;
+    }
+
+
+}
