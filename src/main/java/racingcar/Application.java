aedc1d97709ad9112d6dc04dfe6eac65eb3fff10
b2etr <102886338+betterb2etr@users.noreply.github.com>
2024-10-27 18:36:41 +0900

    feat: 메인 프로그램 실행을 위한 Application 클래스 구현
    - 사용자 입력을 받아 경주를 시작하는 메인 흐름 구성
    - Race 클래스와 InputView, ResultView 연동하여 게임 진행
    - 입력값 검증 및 예외 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,18 @@
-//Application.java
+//Application.java 
 package racingcar;
 
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> carNames = InputView.inputCarNames();
+        int rounds = InputView.inputRounds();
 
+        Race race = new Race(carNames, rounds);
+        System.out.println("실행 결과");
+
+        race.start();
+        List<String> winners = race.getWinners();
+        ResultView.printWinners(winners);
     }
 }
-
