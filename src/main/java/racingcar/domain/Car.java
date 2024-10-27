1e33c887d754d389aa6d15b99218ee23fad75791
Park - Jeonghwan <990706leo@gmail.com>
2024-10-28 07:18:54 +0900

    feat(Car): 자동차는 전진할 수 있다

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,15 +2,32 @@ package racingcar.domain;
 
 public class Car {
 
+    private static final int INITIAL_POSITION = 0;
     private final String name;
+    private int position;
 
     public Car(String name) {
         validateLength(name);
         this.name = name;
+        position = INITIAL_POSITION;
+    }
+
+    public void move(boolean canMove) {
+        if (canMove) {
+            position++;
+        }
+    }
+
+    public boolean hasHigherPositionThan(Car another) {
+        return this.position >= another.position;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 
     private void validateLength(String name) {
