0fdb0c12aac0f007ce94a3009ad124093827b863
kang yeon joo <rkdduswndi@naver.com>
2024-10-28 03:19:42 +0900

    test: 통합 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,13 +2,11 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.game.constant.ExceptionMessage.*;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -29,43 +27,51 @@ class ApplicationTest extends NsTest {
     void 자동차이름이_빈문자열일때_예외() {
         assertThatThrownBy(() -> runException("", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름은 빈 문자열이거나 공백일 수 없습니다.");
+                .hasMessageContaining(CAR_NAME_IS_BLANK.getMessage());
+    }
+
+    @Test
+    void 자동차_개수가_11개_들어왔을때_예외() {
+        assertThatThrownBy(() -> runException("pobi,jun,woni,yeonjoo,soeun,somin,chaeyoung,chaerin,seohyun,jinho,seongjin", "5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INVALID_CAR_COUNT.getMessage());
     }
 
     @Test
     void 자동차이름에_공백이_포함될때_예외() {
         assertThatThrownBy(() -> runException("pobi,,jun", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름은 빈 문자열이거나 공백일 수 없습니다.");
+                .hasMessageContaining(CAR_NAME_IS_BLANK.getMessage());
     }
 
     @Test
     void 자동차이름이_5자를_초과할때_예외() {
         assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름은 1자 이상 5자 이하로 입력해야 합니다: javaji");
+                .hasMessageContaining(INVALID_CAR_NAME_LENGTH.getMessage());
     }
 
     @Test
     void 시도횟수가_1미만일때_예외() {
         assertThatThrownBy(() -> runException("pobi,woni,jun", "0"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("시도할 횟수는 1 이상 10 이하의 정수여야 합니다.");
+                .hasMessageContaining(INVALID_RACING_COUNT_RANGE.getMessage());
     }
 
     @Test
     void 시도횟수가_10초과할때_예외() {
         assertThatThrownBy(() -> runException("pobi,woni,jun", "11"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("시도할 횟수는 1 이상 10 이하의 정수여야 합니다.");
+                .hasMessageContaining(INVALID_RACING_COUNT_RANGE.getMessage());
     }
 
     @Test
     void 중복된자동차이름이_있을때_예외() {
         assertThatThrownBy(() -> runException("pobi,pobi,jun", "5"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름은 중복될 수 없습니다: pobi");
+                .hasMessageContaining(DUPLICATE_CAR_NAME.getMessage());
     }
+
 
     @Override
     public void runMain() {
