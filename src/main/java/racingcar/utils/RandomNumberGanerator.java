8b10cce4a1662cc2d57db67798cd445288fa429b
sese2204 <sese2204@naver.com>
2024-10-27 20:38:55 +0900

    feat(): 난수 생성기 구현

---
+++ src/main/java/racingcar/utils/RandomNumberGanerator.java
@@ -0,0 +1,12 @@
+package racingcar.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGanerator {
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
+
+    public static int generate(){
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER,MAX_RANDOM_NUMBER);
+    }
+}
