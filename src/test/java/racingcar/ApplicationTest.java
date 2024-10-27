7d672c9985062ef18b712c602c2799318a57ca2f
eeun1225 <eeun9108@gmail.com>
2024-10-28 02:01:28 +0900

    test: 기능 및 예외 처리 테스트 추가
    - 레이싱 게임 기능 테스트: 정상적으로 우승자를 출력하는지 확인
    - 이름 앞뒤 공백 허용 테스트 추가
    - 여러 명의 우승자 케이스 테스트 추가
    - 다양한 예외 상황에 대한 테스트 추가:
      - 빈 문자열 입력 시 예외 발생 확인
      - 이름에 공백 포함 시 예외 발생 확인
      - 음수 라운드 입력 시 예외 발생 확인
      - 라운드 입력에 공백 포함 시 예외 발생 확인

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -24,12 +24,70 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 이름앞뒤_공백은_허용() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,   woni  ", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 우승자가_여러명인_경우() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi, woni  ", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void 빈문자열이_입력된_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 이름_띄어쓰기한_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("Ann,D an,pobi", "3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 음수를_입력한_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("Ann,Dan,pobi", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자_사이에_공백이_있는_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("Ann,Dan,pobi", "1 1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
 
     @Override
     public void runMain() {
