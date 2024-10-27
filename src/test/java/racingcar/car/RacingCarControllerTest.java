baa8169c8d1957aa0f63bd036907762c2d1c182f
rjswjddn <rjswjddn@gmail.com>
2024-10-27 19:30:14 +0900

    test: 자동차 생성 테스트 코드 추가

---
+++ src/test/java/racingcar/car/RacingCarControllerTest.java
@@ -0,0 +1,33 @@
+package racingcar.car;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RacingCarControllerTest {
+
+    private final RacingCarController racingCarController = new RacingCarController();
+
+    @Test
+    void 자동차_생성() {
+        List<Car> carList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Car car = new Car("name" + i);
+            carList.add(car);
+        }
+
+        String input = "name0,name1,name2";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        List<Car> result = racingCarController.registerRacingCars();
+
+        for(int num = 0; num < 3; num++) {
+            Assertions.assertThat(result.get(num).getName()).isEqualTo(carList.get(num).getName());
+        }
+    }
+}
