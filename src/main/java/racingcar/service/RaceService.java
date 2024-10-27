b91d8a447f05fdc48e0b33d111c1d1b408acb5e2
kaswhy <sxshx818@naver.com>
2024-10-28 01:27:58 +0900

    feat(RaceService): 차수별 경주 진행 로직 구현
    변경 후
    - 한 회차에서 진행하는 무작위값 생성 -> 전진 로직 구현
    - 출력 구현 필요

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -2,6 +2,7 @@ package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.LinkedHashMap;
+import java.util.Map;
 import racingcar.model.CarName;
 import racingcar.model.CarScores;
 import racingcar.model.Distance;
@@ -10,18 +11,21 @@ public class RaceService {
     public static CarScores playGame(CarScores previousCarScores) {
         LinkedHashMap<CarName, Distance> nowScores = new LinkedHashMap<>();
 
-        // 전체 맵 돌면서
-        // 랜덤 값 구하기
-        // 전진이 필요한지 확인 후 전진
-        // 결과 출력
-        return previousCarScores;
+        for (Map.Entry<CarName, Distance> nowCarScore : previousCarScores.getCarScores().entrySet()) {
+            int randomNumber = getRandomNumber();
+            Distance newDistance = moveOrStop(nowCarScore.getValue(), randomNumber);
+            nowScores.put(nowCarScore.getKey(), newDistance);
+            // 출력 로직 필요
+        }
+
+        return new CarScores(nowScores);
     }
 
-    private int getRandomNumber() {
+    private static int getRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
 
-    private Distance moveOrStop(Distance previousDistance, int randomNumber) {
+    private static Distance moveOrStop(Distance previousDistance, int randomNumber) {
         if (randomNumber >= 4 && randomNumber <= 9) {
             return new Distance(previousDistance.move(randomNumber));
         }
