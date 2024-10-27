9b700ce54c8bf345c6decdc428742b48b8ff5f45
YugyeomGhim <rladbrua0207@gmail.com>
2024-10-27 15:32:35 +0900

    refactor: test를 위해 입력을 파싱 및 검증하는 클래스 분리

---
+++ src/main/java/racingcar/view/InputParser.java
@@ -0,0 +1,35 @@
+package racingcar.view;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputParser {
+
+    private static final String ERROR_INVALID_DELIMITER_MESSAGE = "구분자는 처음과 끝에 존재할 수 없습니다.";
+    private static final String DELIMITER = ",";
+    private static final String ERROR_INVALID_TRY_COUNT_MESSAGE = "시도 횟수는 양수여야 합니다.";
+    private static final int MIN_TRY_COUNT = 1;
+
+    public static List<String> parseCarNames(String carNames){
+        validateCarNames(carNames);
+        return Arrays.stream(carNames.split(DELIMITER)).toList();
+    }
+
+    private static void validateCarNames(String carNames) {
+        if (carNames.startsWith(DELIMITER) || carNames.endsWith(DELIMITER)) {
+            throw new IllegalArgumentException(ERROR_INVALID_DELIMITER_MESSAGE);
+        }
+    }
+
+    public static Integer parseTryCount(String tryCountString) {
+        int tryCount = Integer.parseInt(tryCountString);
+        validateTryCount(tryCount);
+        return tryCount;
+    }
+
+    private static void validateTryCount(int tryCount) {
+        if (tryCount < MIN_TRY_COUNT) {
+            throw new IllegalArgumentException(ERROR_INVALID_TRY_COUNT_MESSAGE);
+        }
+    }
+}
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,36 +8,15 @@ public class InputView {
 
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
-    private static final String ERROR_INVALID_DELIMITER_MESSAGE = "구분자는 처음과 끝에 존재할 수 없습니다.";
-    private static final String ERROR_INVALID_TRY_COUNT_MESSAGE = "시도 횟수는 양수여야 합니다.";
-    private static final String DELIMITER = ",";
-    private static final int MIN_TRY_COUNT = 1;
 
     public static List<String> inputCarNames() {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
-        String carNames = Console.readLine();
-        validateCarNames(carNames);
-
-        return Arrays.stream(carNames.split(DELIMITER)).toList();
+        return InputParser.parseCarNames(Console.readLine());
     }
 
-    private static void validateCarNames(String carNames) {
-        if (carNames.startsWith(DELIMITER) || carNames.endsWith(DELIMITER)) {
-            throw new IllegalArgumentException(ERROR_INVALID_DELIMITER_MESSAGE);
-        }
-    }
 
     public static int inputTryCount() {
         System.out.println(INPUT_TRY_COUNT_MESSAGE);
-        int tryCount = Integer.parseInt(Console.readLine());
-        validateTryCount(tryCount);
-
-        return tryCount;
-    }
-
-    private static void validateTryCount(int tryCount) {
-        if (tryCount < MIN_TRY_COUNT) {
-            throw new IllegalArgumentException(ERROR_INVALID_TRY_COUNT_MESSAGE);
-        }
+        return InputParser.parseTryCount(Console.readLine());
     }
 }
