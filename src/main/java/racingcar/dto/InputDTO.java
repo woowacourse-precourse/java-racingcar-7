141fed5a2c4e97b455542a55801eb0c830512d4a
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-27 21:51:32 +0900

    refactor : 입력에 관한 유효성 검증 로직 수정 - InputView는 이름에 관한 문자열에서 쉼표를 올바르게 사용했는지와 입력한 숫자 뽑기 횟수가 정수로 변환 가능한 문자열인지만 검사 - 중복된 이름 사용 여부와 횟수가 양수인지에 관한 검사는 InputDTO에서 수행하도록 변경

--- src/main/java/racingcar/dto/InputDTO.java
+++ src/main/java/racingcar/dto/InputDTO.java
@@ -1,15 +1,18 @@
 package racingcar.dto;
 
+import static racingcar.util.Validator.validateDuplicatedName;
+import static racingcar.util.Validator.validatePositive;
+
 import java.util.List;
 
 public class InputDTO {
 
     private final List<String> racerNames;
-    private final int totalTrialCount;
+    private final Integer totalTrialCount;
 
     public InputDTO(List<String> racerNames, Integer totalTrialCount) {
-        this.racerNames = racerNames;
-        this.totalTrialCount = totalTrialCount;
+        this.racerNames = validateDuplicatedName(racerNames);
+        this.totalTrialCount = validatePositive(totalTrialCount);
     }
 
     public List<String> getRacerNames() {
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,9 +2,8 @@ package racingcar.view;
 
 import static racingcar.util.message.IOMessage.INPUT_TRIAL_COUNT;
 import static racingcar.util.message.IOMessage.INPUT_NAMES;
-import static racingcar.util.InputValidator.validateInputString;
-import static racingcar.util.InputValidator.validateInteger;
-import static racingcar.util.InputValidator.validateRacingCarCount;
+import static racingcar.util.Validator.validateInputString;
+import static racingcar.util.Validator.validateIntegerInput;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
@@ -12,7 +11,7 @@ import racingcar.dto.InputDTO;
 
 public class InputView {
 
-    public InputDTO display() {
+    public InputDTO input() {
         List<String> racingCarNames = inputRacerNames();
         int trialCount = inputTrialCount();
         Console.close();
@@ -22,16 +21,11 @@ public class InputView {
 
     public List<String> inputRacerNames() {
         System.out.println(INPUT_NAMES);
-        String input = Console.readLine();
-
-        List<String> racingCarNames = validateInputString(input);
-        validateRacingCarCount(racingCarNames.size());
-
-        return racingCarNames;
+        return validateInputString(Console.readLine());
     }
 
     public Integer inputTrialCount() {
         System.out.println(INPUT_TRIAL_COUNT);
-        return validateInteger(Console.readLine());
+        return validateIntegerInput(Console.readLine());
     }
 }
