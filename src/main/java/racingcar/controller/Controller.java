29b4aeda0c9c0381569713ab66beec7f209a331c
wooqqq <sge0225@naver.com>
2024-10-27 16:26:38 +0900

    feat: 자동차 이름 별 자동차 객체 생성 기능 구현

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,8 +1,12 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
 import racingcar.model.RaceService;
 import racingcar.util.Converter;
 import racingcar.view.InputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
@@ -11,16 +15,31 @@ public class Controller {
     private final RaceService raceService = new RaceService();
 
     public void run() {
-        String inputCarList = inputView.inputCarList();
+        List<Car> carList = createCarList();
         String inputTryCnt = inputView.inputTryCnt();
-        String[] carNameList = converter.parseCarList(inputCarList);
         int tryCnt = converter.convertTryCnt(inputTryCnt);
 
         // 테스트용
-        for (String car : carNameList) {
-            System.out.println(car);
+        for (Car car : carList) {
+            System.out.println(car.getName());
         }
 
         System.out.println("시도할 횟수: " + tryCnt);
+    }
+
+    private List<Car> createCarList() {
+        String inputCarList = inputView.inputCarList();
+        String[] carNameList = converter.parseCarList(inputCarList);
+        return convertToCar(carNameList);
+    }
+
+    private List<Car> convertToCar(String[] carNameList) {
+        List<Car> carList = new ArrayList<>();
+        for (String carName : carNameList) {
+            Car car = new Car(carName);
+            carList.add(car);
+        }
+
+        return carList;
     }
 }
--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -5,7 +5,7 @@ public class Car {
     private String name;
     private int distance;
 
-    public Car(String name, int distance) {
+    public Car(String name) {
         this.name = name;
         this.distance = 0;
     }
