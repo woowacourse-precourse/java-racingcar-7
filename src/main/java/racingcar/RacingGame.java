2e3fa2c242d601dcd791838740bb8a7df5358845
taehyun <xogus5070318@gmail.com>
2024-10-27 16:33:59 +0900

    feat(RacingGame): 레이싱 게임 자동차 이름 입력 예외처리 추가
    자동차 이름을 입력받는 과정에서 요구사항에 맞게 예외 처리를 추가하였습니다.
    요구사항
    1. n의 값은 2~8까지의 숫자만 입력 가능하다.
    2. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
    3. 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 2자 이상, 5자 이하만 가능하다.
    4. 자동차의 이름에 들어올 수 있는 문자는 알파벳 대소문자로 한정한다.
    5. 자동차의 이름은 중복될 수 없다.
    
    BREAKING CHANGE: None

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import java.util.ArrayList;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class RacingGame {
@@ -9,7 +10,7 @@ public class RacingGame {
     ArrayList<String> winners = new ArrayList<>();
     int maxTurn = 0;
 
-    public void runGame(){
+    public void runGame() {
 
         setGame();
         playGame();
@@ -17,23 +18,23 @@ public class RacingGame {
     }
 
     /// 레이싱게임 입력부분
-    private void setGame(){
+    private void setGame() {
         //Todo: 한 가지 기능을 하는 함수로 나누기
 
         String[] carNameArray = inputCarName(); // 자동차 이름 입력받아서 배열에 저장
-        // Todo: 자동차 입력 예외처리
+        validateCarName(carNameArray); // 자동차 입력부분 예외처리
 
         // 자동차 정보 저장
-        for(String carName : carNameArray){
+        for (String carName : carNameArray) {
 
-            // Todo: 중복확인 예외처리
+            checkDuplicateCar(carName); // 자동차 이름 중복 예외처리
             cars.add(new Car(carName));
         }
         //Todo: 사용자로부터 maxTurn 입력받기
     }
 
     /// 자동차 이름을 입력받아 문자열 배열을 리턴
-    private String[] inputCarName(){
+    private String[] inputCarName() {
 
         String carNameString = Console.readLine(); // 사용자로부터 자동차 이름 입력받기
         carNameString = carNameString.trim();
@@ -42,15 +43,54 @@ public class RacingGame {
         return carNameString.split(",");
     }
 
+    /// 자동차 입력부분 예외처리
+    void validateCarName(String[] carNameArray) {
+
+        if (carNameArray.length > 8) {
+            throw new IllegalArgumentException("자동차 이름은 최대 8개까지 입력 가능합니다.");
+        }
+        if (carNameArray.length < 2){
+            throw new IllegalArgumentException("자동차 이름은 최소 2개 이상 입력해야합니다.");
+        }
+
+        for (String carName : carNameArray) {
+
+            carName = carName.trim();
 
 
-    private void playGame(){
+            if (carName.length() < 2) {
+                throw new IllegalArgumentException("자동차 이름은 2자 이상으로 입력해야합니다.");
+            }
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력해야입니다.");
+            }
+
+            // 자동차 이름이 알파벳 대소문자로만 이루어져있는지 확인
+            if (!carName.matches("[a-zA-Z]+")) {
+                throw new IllegalArgumentException("자동차 이름은 알파벳 대소문자만 가능합니다.");
+            }
+
+        }
+    }
+
+    ///자동차 이름 중복 예외처리
+    void checkDuplicateCar(String carName) {
+
+        for (Car car : cars) {
+            if (carName.equals(car.getCarName())) {
+                throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+            }
+        }
+    }
+
+
+    private void playGame() {
         //Todo: maxTurn번 반복하기
         //Todo: 무작위 수를 이용하여 자동차 움직이기
         //Todo: 한 가지 기능을 하는 함수로 나누기
     }
 
-    private void afterGame(){
+    private void afterGame() {
         //Todo: 각 자동차별 최종 거리를 이용하여, 1등 거리 구하기
         //Todo: 1등 거리에 해당하는 자동차 이름을 winners 배열에 저장
         //Todo: winners 배열을 통해 우승자 출력하기
