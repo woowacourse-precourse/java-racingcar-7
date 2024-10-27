ca64d50c10fa485f51414195985aec470478ce6a
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 11:55:32 +0900

    :sparkles: feat(ErrorMessages): 유효하지 않은 실행 횟수에 대해 오류 메시지 제공

--- src/main/java/racingcar/message/ErrorMessages.java
+++ src/main/java/racingcar/message/ErrorMessages.java
@@ -9,4 +9,7 @@ public class ErrorMessages {
     // Car name related errors messages
     public static final String CLEAR_NAME = "[ERROR] 이름은 비워둘 수 없습니다. 이름을 입력해주세요.";
     public static final String LONG_NAME = "[ERROR] 이름이 너무 깁니다. 5자 이하로 입력해주세요.";
+
+    // Try count related errors messages
+    public static final String TRY_COUNT_NOT_VALID_NUMBER = "[ERROR] 시도 횟수가 유효하지 않습니다. 시도 횟수는 0 이상 1000이하로 입력해주세요.";
 }
