466cbf87968191736f181d4566957ae8adcf7999
DevBigOne <a62387272@gmail.com>
2024-10-28 00:29:26 +0900

    [feat] GameController에 자동차 이름 검증 구현 추가

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -2,20 +2,25 @@ package racingcar.controller;
 
 import racingcar.service.Separation;
 import racingcar.view.View;
+import racingcar.service.Verification;
 
 public class GameController {
     private final View view;
     private final Separation separation;
+    private final Verification verification;
 
     public GameController() {
         this.view = new View();
         this.separation = new Separation();
+        this.verification = new Verification();
     }
 
     public void run() {
         view.Message();
         String userInput = view.userInput();
+        verification.special_characters(userInput);
 
         String[] splitUserInput = separation.split(userInput);
+
     }
 }
