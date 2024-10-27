ea851dc1f0bbdf936edc16e6df2e442cb6b66887
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 21:33:55 +0900

    feat(InputView): 자동차 이름과 시행 횟수에 관한 검증 로직 호출부 추가
    - 각 메서드에 try-catch 구문을 적용해서 예외를 검사하고 상위 호출부로 예외를 던지는 로직 추가
    - splitName() 메서드에서 Indent Depth가 3이 넘지 않도록 하기위해서 예외 검증 부분을 분리함
    - NameValidation.validateName() 메서드로 이름이 공백인 경우와 이름이 없는 경우에 대한 검증을 실시

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -3,21 +3,33 @@ package racingcar.View;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
+import racingcar.Util.Exception.ErrorMessage;
 import racingcar.Util.Message.IOMessage;
+import racingcar.Validation.NameValidation;
+import racingcar.Validation.NumberOfTrialValidation;
 
 public class InputView {
     public List<String> inputCarName() {
-        System.out.println(IOMessage.INPUT_CAR_NAMES.getMessage());
-        String names = Console.readLine();
-
-        return splitNames(names);
+        try {
+            System.out.println(IOMessage.INPUT_CAR_NAMES.getMessage());
+            String names = Console.readLine();
+            return splitNames(names);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
     }
 
     public int inputTrialCount() {
-        System.out.println(IOMessage.INPUT_TRIAL_COUNT.getMessage());
-        int count = Integer.parseInt(Console.readLine());
-
-        return count;
+        try {
+            System.out.println(IOMessage.INPUT_TRIAL_COUNT.getMessage());
+            String count = Console.readLine();
+            if (NumberOfTrialValidation.isNotInteger(count)) {
+                throw new IllegalArgumentException(ErrorMessage.TRIAL_NOT_INTEGER.getMessage());
+            }
+            return Integer.parseInt(count);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
     }
 
     public List<String> splitNames(String names) {
@@ -25,9 +37,9 @@ public class InputView {
         List<String> carNames = new ArrayList<>();
 
         for (String name : splitNames) {
+            NameValidation.validateName(name);
             carNames.add(name.trim());
         }
-
         return carNames;
     }
 }
