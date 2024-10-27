2e655586fc466f1363dbf605613d1fba736c3e06
Ahnsukjoo <bos98@naver.com>
2024-10-27 16:36:44 +0900

    refactor: 테스트 코드로 테스트 하기 쉽게 코드 수정
    - 예외를 처리하는 코드를 한곳으로 묶어줌으로써 테스트 코드를 짜기 쉬워짐
    - readLine()이 안들어있고 String을 매개변수로 갖는 함수를 만들어주니까 테스트가 쉬워짐

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -19,6 +19,9 @@ public class Input {
             throw new IllegalArgumentException("입력값이 null 입니다.");
         }
         String[] carNameList = readLine.split(",");
+        if (Arrays.stream(carNameList).allMatch(String::isBlank)) {
+            throw new IllegalArgumentException("올바른 이름이 입력되지 않았습니다.");
+        }
         Arrays.stream(carNameList).forEach(name -> {
             name = name.trim();
             if (name.length() > 5) {
@@ -28,12 +31,23 @@ public class Input {
                 throw new IllegalArgumentException("이름을 공백으로 할 수 없습니다.");
             }
         });
+        if (Arrays.stream(carNameList).distinct().count() != carNameList.length) {
+            throw new IllegalArgumentException("중복된 이름이 존재합니다.");
+        }
         return Arrays.stream(carNameList).map(String::trim).toArray(String[]::new);
     }
 
     public int getAttemptedNumber() {
+        return checkAttemptedNumberError(readLine());
+    }
+
+    public int checkAttemptedNumberError(String readLine) {
         try {
-            return Integer.parseInt(readLine());
+            int attemptedNumber = Integer.parseInt(readLine);
+            if (attemptedNumber < 0) {
+                throw new IllegalArgumentException("음수를 입력하였습니다.");
+            }
+            return attemptedNumber;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("잘못 입력되었습니다.");
         }
