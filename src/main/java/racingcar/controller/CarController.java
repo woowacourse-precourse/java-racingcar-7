db9fadd6d480a0d76fb1279dfa39e934212a1fce
mete0rfish <sungwon326@naver.com>
2024-10-27 15:27:35 +0900

    feat(controller): add CarController class

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -1,4 +1,24 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
+import racingcar.domain.CarRace;
+import racingcar.view.CarInput;
+import racingcar.view.CarOutput;
+
+import java.util.List;
+
 public class CarController {
+
+    public void race(CarRace carRace) {
+
+        for(int i=0;i<carRace.getLapCount();i++) {
+            carRace.getCars().forEach(Car::move);
+            CarOutput.outputResultPerRap(carRace);
+        }
+
+        int maxPosition = carRace.findMaxPosition();
+        List<Car> winners = carRace.findCarsByPosition(maxPosition);
+
+        CarOutput.outputWinnersName(winners);
+    }
 }
