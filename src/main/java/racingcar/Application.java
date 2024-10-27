50343699058f2746f0e81bf42c2356076dfb3aab
kjyyjk <rlawnsdud920@gmail.com>
2024-10-27 21:32:11 +0900

    refactor: main()의 입력 로직을 메서드로 분리한다
    - 입력 관련 메서드만을 처리하는 입력 메서드를 생성한다.
    기존 main()은 입력, 출력, 게임 실행 등 많은 일을 한다.
    이 경우 어떤 한 가지가 변경되면 다른 관련 없는 로직에도 영향을 줄 위험이 크다.
    따라서 우선 입력 로직을 분리한다.
    input()은 사용자로부터 입력을 받고 null과 공백을 체크한다.
    inputCarNames()는 input()을 사용하며 자동차 이름에 맞는 로직을 추가로 수행한다.
    inputTotalMoves()는 input()을 사용하며 이동 횟수에 맞는 로직을 추가로 수행한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,16 +6,8 @@ import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] carNames = input().split(",");
-
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int totalMoves;
-        try {
-            totalMoves = Integer.parseInt(Console.readLine());
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("정수를 올바르게 입력해주세요");
-        }
+        String[] carNames = inputCarNames();
+        int totalMoves = inputTotalMoves();
 
         List<Car> winners = new RacingGame().run(carNames, totalMoves);
         List<String> winnerNames = winners.stream()
@@ -26,10 +18,24 @@ public class Application {
         System.out.println("최종 우승자 : " + output);
     }
 
+    private static String[] inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return input().split(",");
+    }
+
+    private static int inputTotalMoves() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        try {
+            return Integer.parseInt(input());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("이동 횟수를 올바르게 입력해주세요");
+        }
+    }
+
     private static String input() {
         String input = Console.readLine();
         if (input == null || input.isBlank()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("입력은 필수입니다");
         }
         return input;
     }
