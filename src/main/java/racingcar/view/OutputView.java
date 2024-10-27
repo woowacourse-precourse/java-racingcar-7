ebefc0d7bba2add867b51f39c4f6e15a20beff2e
choiyoung69 <choiyoung609@gmail.com>
2024-10-28 03:19:20 +0900

    feat(OutputView): 사용자로부터 입력을 받기 전 출력 메세지를 출력하는 함수 생성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,14 @@
 package racingcar.view;
 
+import static racingcar.view.ViewMessages.REQUIRE_CARS_NAME;
+import static racingcar.view.ViewMessages.REQUIRE_NUMBER_OF_ATTEMPTS;
+
 public class OutputView {
+    public static void requireCarsName(){
+        System.out.println(REQUIRE_CARS_NAME);
+    }
+
+    public static void requireNumberOfAttempts(){
+        System.out.println(REQUIRE_NUMBER_OF_ATTEMPTS);
+    }
 }
