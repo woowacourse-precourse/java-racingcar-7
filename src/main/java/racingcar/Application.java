3028256694ca060c2d425d8560dfa0b8fcf0cd9d
KNU-K <knukang334@gmail.com>
2024-10-27 20:29:00 +0900

    refactor(app): View 에서 UI로 폴더 이름 및 클래스이름 변경 반영

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,27 +1,28 @@
 package racingcar;
 
-import racingcar.config.ApplicationConfig;
-import racingcar.constant.MessageConstants;
+import racingcar.common.config.ApplicationConfig;
+import racingcar.common.constant.MessageConstants;
 import racingcar.controller.RaceController;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
+import racingcar.ui.InputUI;
+import racingcar.ui.OutputUI;
 
 public class Application {
+
+
     public static void main(String[] args) {
 
         // 1. ApplicationConfig로부터 RaceController를 생성
         ApplicationConfig config = new ApplicationConfig();
-        RaceController raceController = config.getRaceController();
+        RaceController controller = config.getRaceController();
 
         // 2. 사용자로 부터 입력(Client)
-        String command = InputView.inputString(MessageConstants.INPUT_TRY_COUNT);
-        Integer iterations = InputView.inputInteger(MessageConstants.INPUT_CAR_NAMES);
+        String command = InputUI.inputString(MessageConstants.INPUT_TRY_COUNT);
+        Integer iterations = InputUI.inputInteger(MessageConstants.INPUT_CAR_NAMES);
 
         // 3. 경주 시작
-        String winners = raceController.startRace(command, iterations);
+        String winners = controller.startRace(command, iterations);
 
         // 4. 사용자에게 출력 (Client)
-        OutputView.printResult(MessageConstants.OUTPUT_RESULT_TEMPLATE, winners);
-
+        OutputUI.printResult(MessageConstants.OUTPUT_RESULT_TEMPLATE, winners);
     }
 }
