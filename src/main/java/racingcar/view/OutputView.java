5d2a0e142a6a8d8afacd2f43d5be605cb0b66bcc
JaeGyun-Yoo <dbworbs15@gmail.com>
2024-10-27 19:59:25 +0900

    feat(OutputView): 라운드 종료 후 줄바꿈 기능 구현
    - 라운드 종료 후 한줄 띄우는 기능 구현
    - 자동차 상태 기능 출력함수에 주석 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,6 +6,10 @@ public class OutputView {
     }
 
     public void printCarStatus(string carName, int position) {
-        System.out.println(name + " : " + "-".repeat(position));
+        System.out.println(name + " : " + "-".repeat(position)); //라운드별 자동차 상태 출력
+    }
+
+    public void printRoundEnd() {
+        System.out.println(); //라운드 종료 후 한 줄 띄우기
     }
 }
