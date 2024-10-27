d11d74fbbde2343c50f155b16bff5e075f9aff3c
soonhong99 <nanayah99@naver.com>
2024-10-27 14:42:54 +0900

    feat(WinnerOfFinalScore): 최종 점수로 우승자를 추출하는 기능을 구현하라
    - map 형식으로 이루어진 최종 점수를 리스트로 바꾸는 기능 구현
    - 기존 map을 사용했던 이유는 직관적으로 누구가 전진했는지 알기 위함
    - map을 리스트로 바꿈으로써 쉽게 우승자를 추출하기 위함
    - 가장 높은 점수를 추출하는 기능 구현
    - 해당 점수를 토대로 최종 우승자 리스트를 추출하는 기능 구현
    
    Signed-off-by: soonhong99 <nanayah99@naver.com>

--- src/main/java/racingGame/Score.java
+++ src/main/java/racingGame/Score.java
@@ -1,5 +1,6 @@
 package racingGame;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,5 +28,54 @@ public class Score {
         }
 
         return currentScore;
+    }
+
+    public List<String> makeFinalWinnerList(Map<String, String> currentScore) {
+        List<List<String>> finalParticipantsAndScoreList = makeFinalScoreMapToList(currentScore);
+        List<String> finalWinnerList = extractFinalWinnerList(finalParticipantsAndScoreList);
+
+        return finalWinnerList;
+    }
+
+    private List<List<String>> makeFinalScoreMapToList(Map<String, String> currentScore) {
+        List<List<String>> finalParticipantsAndScoreList = new ArrayList<>();
+
+        for (Map.Entry<String, String> entry : currentScore.entrySet()) {
+            List<String> participantResult = new ArrayList<>();
+
+            participantResult.add(entry.getKey());
+            participantResult.add(entry.getValue());
+            finalParticipantsAndScoreList.add(participantResult);
+        }
+
+        return finalParticipantsAndScoreList;
+    }
+
+    private List<String> extractFinalWinnerList(List<List<String>> finalParticipantsAndScoreList) {
+        List<String> finalWinnerList = new ArrayList<>();
+        int maxScore = findMaxScore(finalParticipantsAndScoreList);
+
+        for (List<String> participantAndScore : finalParticipantsAndScoreList) {
+            String participantName = participantAndScore.get(0);
+            String stringOfScore = participantAndScore.get(1);
+
+            if (stringOfScore.length() == maxScore) {
+                finalWinnerList.add(participantName);
+            }
+        }
+
+        return finalWinnerList;
+    }
+
+    private int findMaxScore(List<List<String>> finalParticipantsAndScoreList) {
+        int maxScore = 0;
+
+        for (List<String> participant : finalParticipantsAndScoreList) {
+            String stringOfScore = participant.get(1);
+            int thisScore = stringOfScore.length();
+            maxScore = Math.max(maxScore, thisScore);
+        }
+
+        return maxScore;
     }
 }
