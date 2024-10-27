7722388ab84e5baa79835677d85926911c523fd7
Hyso <hsk7953@gmail.com>
2024-10-28 05:14:36 +0900

    refactor(GameController) : GameController 클래스 단일 책임 원칙 적용 - InputView, OutputView 를 생성자 주입으로 변경 - startGame 메소드의 책임을 분리하여 메서드 추출   - receiveCarNames: 자동차 이름 입력 처리   - receiveTrialCount: 시도 횟수 입력 처리   - playRace: 게임 진행 로직   - racingResult: 결과 출력 처리

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -6,23 +6,41 @@ import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class GameController {
-
-    public void startGame(){
-        OutputView.requestCarNames();
-        String registerdCars = InputView.inputCarName();
-
-        OutputView.requestTrialCount();
-        int trialCount = StringConvertor.toInt(InputView.inputTrialCount());
-
-        RacingCar racingCar = new RacingCar(StringConvertor.toCarList(registerdCars));
-
-        while(trialCount > 0){
-            racingCar.moving();
-            OutputView.printRacing(racingCar.getCars());
-            trialCount--;
-        }
-
-        OutputView.printWinner(racingCar.getWinner());
+    private final InputView inputView;
+    private final OutputView outputView;
+    public GameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
     }
 
+    public void startGame() {
+        String receiveCarNames = receiveCarNames();
+        RacingCar racingCar = new RacingCar(StringConvertor.toCarList(receiveCarNames));
+        int trialCount = receiveTrialCount();
+        playRace(racingCar, trialCount);
+        racingResult(racingCar);
+
+    }
+
+    private String receiveCarNames() {
+        outputView.requestCarNames();
+        return inputView.inputCarName();
+    }
+
+    private int receiveTrialCount() {
+        outputView.requestTrialCount();
+        String inputTrialCount = inputView.inputTrialCount();
+        return StringConvertor.toInt(inputTrialCount);
+    }
+
+    private void playRace(RacingCar racingCar, int trialCount) {
+        for (int i = 0; i < trialCount; i++) {
+            racingCar.moving();
+            outputView.printRacing(racingCar.getCars());
+        }
+    }
+
+    private void racingResult(RacingCar cars) {
+        outputView.printWinner(cars.getWinner());
+    }
 }
