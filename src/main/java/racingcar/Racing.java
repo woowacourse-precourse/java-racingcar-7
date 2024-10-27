c938f15c78886b32f9ae5e22c763416ffc7d5de6
gayoung228 <103436798+gayoung228@users.noreply.github.com>
2024-10-28 03:11:50 +0900

    Car 클래스 구현 및 기본 속성과 이동 기능 추가
    자동차의 기본 속성인 이름과 위치를 관리하는 Car 클래스를 추가. 또한, 자동차의 위치를 한 칸 증가시키는 move 메서드를 통해 경주 중 자동차의 이동을 시뮬레이션한다.

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -53,4 +53,25 @@ public class Racing {
                 .collect(Collectors.toList());
     }
 
+    private static class Car {
+        private final String name;
+        private int position;
+
+        public Car(String name) {
+            this.name = name;
+            this.position = 0;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getPosition() {
+            return position;
+        }
+
+        public void move() {
+            position++;
+        }
+    }
 }
