e013868b682ca27a19bf2c5e32c75d6fa940344d
sc@t0a14 <gusdn6671@gmail.com>
2024-10-27 11:20:09 +0900

    test(DistanceTest) : 자동차의 distance 가 여러 번 증가하면 증가 테스트 코드 추가

--- src/test/java/racingcar/DistanceTest.java
+++ src/test/java/racingcar/DistanceTest.java
@@ -17,5 +17,14 @@ public class DistanceTest {
         assertThat(distance.getDistance()).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("자동차의 distance 가 여러 번 증가하면 증가한다")
+    void multipleStepForward(){
+        CarDistance distance = new CarDistance();
+        distance.stepForward();
+        distance.stepForward();
+        distance.stepForward();
+        assertThat(distance.getDistance()).isEqualTo(3);
+    }
 
 }
