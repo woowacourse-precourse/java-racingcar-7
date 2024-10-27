14b14c5725be127d94d97a5cc8284ef5d03c8f7f
xeimun <hinote444@naver.com>
2024-10-28 07:26:04 +0900

    test(CarsTest): 중복된 코드를 제거하고 초기화 작업을 생성

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -4,26 +4,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CarsTest {
 
+    private Cars cars;
+    private Car pobi;
+    private Car woni;
+    private Car jun;
+
+    @BeforeEach
+    void setUp() {
+        String[] carNames = {"pobi", "woni", "jun"};
+        cars = Cars.from(carNames);
+
+        pobi = cars.getCars()
+                   .get(0);
+        woni = cars.getCars()
+                   .get(1);
+        jun = cars.getCars()
+                  .get(2);
+
+        pobi.addAdvanceMarker();  // pobi: 1회 전진
+        woni.addAdvanceMarker();  // woni: 1회 전진
+        woni.addAdvanceMarker();  // woni: 2회 전진
+        jun.addAdvanceMarker();   // jun: 1회 전진
+        jun.addAdvanceMarker();   // jun: 2회 전진
+    }
+
     @Test
     void 최대_전진_횟수를_구한다() {
-        // given
-        String[] carNames = {"pobi", "woni"};
-        Cars cars = Cars.from(carNames);
-
-        cars.getCars()
-            .get(0)
-            .addAdvanceMarker(); // pobi: 1회 전진
-        cars.getCars()
-            .get(1)
-            .addAdvanceMarker(); // woni: 1회 전진
-        cars.getCars()
-            .get(1)
-            .addAdvanceMarker(); // woni: 2회 전진
-
         // when
         int maxAdvanceMarkerCount = cars.getMaxAdvanceMarkerCount();
 
@@ -34,22 +45,6 @@ public class CarsTest {
     @Test
     void 가장_많이_전진한_우승자를_찾는다() {
         // given
-        String[] carNames = {"pobi", "woni", "jun"};
-        Cars cars = Cars.from(carNames);
-
-        Car pobi = cars.getCars()
-                       .get(0);
-        Car woni = cars.getCars()
-                       .get(1);
-        Car jun = cars.getCars()
-                      .get(2);
-
-        pobi.addAdvanceMarker();  // pobi: 1회 전진
-        woni.addAdvanceMarker();  // woni: 1회 전진
-        woni.addAdvanceMarker();  // woni: 2회 전진
-        jun.addAdvanceMarker();   // jun: 1회 전진
-        jun.addAdvanceMarker();   // jun: 2회 전진
-
         int maxAdvanceMarkerCount = cars.getMaxAdvanceMarkerCount();
 
         // when
