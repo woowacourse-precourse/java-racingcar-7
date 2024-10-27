7fce3daaff288d1c80ad69db41e0afd5499bdbaa
SISU <atgsisu@gmail.com>
2024-10-27 15:05:28 +0900

    test(Race): Exception을 발생시키는 예외 상황 테스트 케이스 추가

--- src/test/java/racingcar/model/RaceTest.java
+++ src/test/java/racingcar/model/RaceTest.java
@@ -1,36 +1,88 @@
 package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class RaceTest {
 
-        /*
-        객체의 값을 일일히 다 비교하는 방법을 모르겠어서 우선 나중에 하기로 함...
-         */
-//    @Test
-//    void 자동차_이름이_주어졌을_때() {
-//        Race race = new Race("해건,수미,영재");
-//        List<Car> expectedResult = new ArrayList<>();
-//        expectedResult.add(new Car("해건"));
-//        expectedResult.add(new Car("수미"));
-//        expectedResult.add(new Car("영재"));
-//
-//    }
+    /*
+    객체의 값을 일일히 다 비교하는 방법을 모르겠어서 우선 나중에 하기로 함...
+     */
+    @Test
+    void 자동차_이름이_주어졌을_때() {
+        Race race = new Race("sisu,java,pobi", "3");
+        assertThat(race.getRacingCars().stream().map(Car::getName).toList()).containsExactly("sisu", "java", "pobi");
+
+    }
 
     @Test
     void 자동차_이름이_5자_초과일_때() {
         assertThrows(IllegalArgumentException.class,
                 () -> {
-            Race race = new Race("OverFiveLength,test,수미,해건", "3");
-        });
+                    Race race = new Race("longNameTest,test,java,sisu", "3");
+                });
     }
 
+    @Test
+    void 이름이_없는_입력값이_주어질_때() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    Race race = new Race("", "3");
+                });
+    }
+
+    @Test
+    void 중복되는_이름이_있을_때() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    Race race = new Race("sisu,sisu", "3");
+                });
+    }
+
+    @Test
+    void 정수가_아닌_값이_round_인자로_주어졌을_때() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    Race race = new Race("sisu", "I'm not an integer");
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    Race race = new Race("sisu", "0.999999999999999999999999999999999999999999");
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    Race race = new Race("sisu", "3.1");
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    Race race = new Race("sisu", "3e");
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    Race race = new Race("sisu", "3!");
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    Race race = new Race("sisu",
+                            "100000000000000000000000000000000000000000000000000000000000000000000000");
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    Race race = new Race("sisu",
+                            "");
+                });
+    }
+
+    @Test
+    void 양수가_아닌_값이_round_인자로_주어졌을_때() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    Race race = new Race("sisu", "-1");
+                });
+
+    }
 //    @Test
 //    void 우승자_확인() {
 //        Race race = new Race("해건,수미,영재");
