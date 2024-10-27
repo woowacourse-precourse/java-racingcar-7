4c28d033f523b03e115d8d0c603e13b2a7b8e320
조수아 <suacho0724@gmail.com>
2024-10-28 04:50:42 +0900

    feat: 차수마다 각 차별 이동 거리 출력하기

--- src/main/java/racingcar/carrace/CarRace.java
+++ src/main/java/racingcar/carrace/CarRace.java
@@ -1,6 +1,7 @@
 package racingcar.carrace;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Comparator;
 import java.util.List;
 import racingcar.model.Car;
 import racingcar.utils.Parser;
@@ -21,6 +22,19 @@ public class CarRace {
             OutputView.printResult(createOutputMessage());
             remainNum--;
         }
+        OutputView.printResult(createWinnerMessage());
+    }
+
+    private void moveCar() {
+        for (Car car : carList) {
+            if (getRandomNumber() >= 4) {
+                car.move();
+            }
+        }
+    }
+
+    private int getRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
     }
 
     private String createOutputMessage() {
@@ -36,17 +50,29 @@ public class CarRace {
         return sb.toString();
     }
 
-    private void moveCar() {
+    private String createWinnerMessage() {
+        StringBuilder sb = new StringBuilder("최종 우승자 : ");
+        int winnerCnt = getWinnerCnt();
         for (Car car : carList) {
-            if (getRandomNumber() >= 4) {
-                car.move();
+            if (winnerCnt == 0) {
+                break;
             }
+            sb.append(car.getName()).append(", ");
+            winnerCnt--;
         }
+        return sb.toString();
     }
 
-    private int getRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+    private int getWinnerCnt() {
+        carList.sort(Comparator.comparingInt(Car::getDistance).reversed());
+        int cnt = 0;
+        int maxDistance = carList.getFirst().getDistance();
+        for (Car car : carList) {
+            if (car.getDistance() != maxDistance) {
+                return cnt;
+            }
+            cnt++;
+        }
+        return cnt;
     }
-
-
 }
