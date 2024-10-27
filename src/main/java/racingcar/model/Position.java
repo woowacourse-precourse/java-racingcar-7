92d2be5bce7637643525285d4a4f59d0fd2f708f
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:29:10 +0900

    :sparkles: feat(Position): 초기 위치 설정 및 현재 위치 반환

--- src/main/java/racingcar/model/Position.java
+++ src/main/java/racingcar/model/Position.java
@@ -1,4 +1,15 @@
 package racingcar.model;
 
 public class Position {
+    private static final int INITIAL_POSITION = 0;
+
+    private int position;
+
+    public Position() {
+        this.position = INITIAL_POSITION;
+    }
+
+    public int toInteger() {
+        return this.position;
+    }
 }
