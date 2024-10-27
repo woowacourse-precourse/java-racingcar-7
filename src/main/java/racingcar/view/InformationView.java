8dac5da0ebebba4da6fafe2ce0c45ce09cb6bec3
young-in <hardworkingini@gmail.com>
2024-10-27 22:44:52 +0900

    feat : 이동 횟수를 입력받기 위한 view 구현

--- src/main/java/racingcar/view/InformationView.java
+++ src/main/java/racingcar/view/InformationView.java
@@ -2,8 +2,13 @@ package racingcar.view;
 
 public class InformationView {
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
 
     public void printRequestCarNames(){
         System.out.println(INPUT_CAR_NAMES);
+    }
+
+    public void printRequestTryCount(){
+        System.out.println(INPUT_TRY_COUNT);
     }
 }
