047024fa089f1b1ede0357e984eccb2d5b693095
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-27 23:59:03 +0900

    chore(ErrorMessage): 예외 메시지 오타 수정
    FORWARD_COUNT_MUST_BE_IN_RANGE 메시지 오타 수정

--- src/main/java/racingcar/constants/exception/ErrorMessage.java
+++ src/main/java/racingcar/constants/exception/ErrorMessage.java
@@ -6,7 +6,7 @@ public enum ErrorMessage {
     DUPLICATE_CAR_NAME_NOT_ALLOWED("자동차 이름은 중복될 수 없습니다."),
     EMPTY_CAR_NAME_NOT_ALLOWED("자동차 이름은 비어있을 수 없습니다."),
     INVALID_CAR_NAME_FORMAT("입력은 쉼표로 구분된 영어 이름만 가능합니다."),
-    FORWARD_COUNT_MUST_BE_IN_RANGE("시도할 횟수는 1~100 사이의 숫자만 입력 가능합나다.");
+    FORWARD_COUNT_MUST_BE_IN_RANGE("시도할 횟수는 1~100 사이의 숫자만 입력 가능합니다.");
 
     private final String message;
 
