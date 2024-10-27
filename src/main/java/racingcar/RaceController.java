2adbcacbcb247b6df9ba579d60ebb0cd935f1963
문고은 <>
2024-10-27 18:20:37 +0900

    feat(controller) : 무작위 값을 반환하는 메서드 추가
    0에서 9사이 무작위 값을 반환하는 메서드 생성

--- src/main/java/racingcar/RaceController.java
+++ src/main/java/racingcar/RaceController.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.LinkedHashMap;
 
 public class RaceController {
@@ -9,5 +11,9 @@ public class RaceController {
     public RaceController(LinkedHashMap<Car, Integer> cars, int attemptCount) {
         this.attemptCount = attemptCount;
         this.cars = cars;
+    }
+
+    private int getRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
     }
 }
