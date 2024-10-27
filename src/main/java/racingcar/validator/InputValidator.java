df207f10445259ca202a93b2ba5f22330e3ba360
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-27 17:14:32 +0900

    feat(InputValidator): 입력 값 검증 로직 추가
    자동차가 2개 미만일 경우와 시도할 횟수가 음수인 경우 예외 처리하는 로직을 추가하였습니다.

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class InputValidator {
 
+    private static final int MINIMUM_CAR_COUNT = 2;
+
     public static void validateEmpty(final String input) {
         if (input == null || input.isBlank()) {
             throw RacingException.of(ErrorMessage.EMPTY_INPUT);
@@ -14,6 +16,7 @@ public class InputValidator {
     }
 
     public static void validateDuplicate(final List<String> names) {
+        validateMinimumCarCount(names);
         if (isDuplicated(names)) {
             throw RacingException.of(ErrorMessage.DUPLICATE_CAR_NAME);
         }
@@ -25,11 +28,24 @@ public class InputValidator {
                 .count();
     }
 
+    private static void validateMinimumCarCount(final List<String> names) {
+        if (names.size() < MINIMUM_CAR_COUNT) {
+            throw RacingException.of(ErrorMessage.MINIMUM_NUMBER_OF_CARS);
+        }
+    }
+
     public static void validateNumeric(final String input) {
         try {
-            Integer.parseInt(input);
+            int number = Integer.parseInt(input);
+            validateNegativeNumber(number);
         } catch (NumberFormatException e) {
             throw RacingException.of(ErrorMessage.INVALID_ATTEMPT_COUNT_FORMAT);
+        }
+    }
+
+    private static void validateNegativeNumber(final int number) {
+        if (number < 0) {
+            throw RacingException.of(ErrorMessage.NEGATIVE_NUMBER);
         }
     }
 }
