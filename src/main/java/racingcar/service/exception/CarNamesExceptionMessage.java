758c37e63e10aec08454fa4bf826c6bcae1a256a
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-27 18:09:26 +0900

    Fix: 전체 입력값(carNames)를 검증하는 것으로 검증 로직 단순화

--- src/main/java/racingcar/service/exception/CarNamesExceptionMessage.java
+++ src/main/java/racingcar/service/exception/CarNamesExceptionMessage.java
@@ -5,10 +5,10 @@ import racingcar.exception.ExceptionMessage;
 public enum CarNamesExceptionMessage implements ExceptionMessage {
 
     NO_INPUT("입력값이 없습니다"),
-    INVALID_SEPARATOR("구분자는 콤마를 사용해야 합니다."),
-    CAR_NAME_MUST_ENG_KOR_DIGIT("자동차 이름은 영어, 한글, 숫자이어야 합니다."),
+    INVALID_CAR_NAMES_OR_SEPARATOR("자동차 이름은 한글, 영어, 숫자를, 구분자는 콤마를 사용해야 합니다."),
+    CAR_NAMES_ONLY_COMMA("콤마만 입력했습니다."),
     CAR_NAME_MUST_UNDER_LENGTH_FIVE("자동차 이름은 5자 이하이어야 합니다."),
-    CAR_NAME_DUPLICATE("중복된 자동차 이름이 존재합니다."),
+    CAR_NAME_DUPLICATED("중복된 자동차 이름이 존재합니다."),
     ;
 
     private final String message;
