bbaaaecefd8a2521a987874f7a65b4e2273299d9
ksundev <kshk08@hanyang.ac.kr>
2024-10-27 18:56:33 +0900

    feat(Car): 자동차 위치(position) 속성 및 전진(move) 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,6 +2,7 @@ package racingcar;
 
 public class Car {
     private final String name;
+    private int position = 0;
 
     public Car(String name) {
         this.name = name;
@@ -9,5 +10,11 @@ public class Car {
 
     public String getName() {
         return name;
+    }
+    public int getPosition() {
+        return position;
+    }
+    public void move() {
+        position++;
     }
 }
