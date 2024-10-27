9478e053a56458815d453f05fffff55e82699f26
song hyeongyu <lit3207@ajou.ac.kr>
2024-10-27 16:04:27 +0900

    feat: add Car class with Constructor

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,17 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private final String name;
+    private int distance = 0;
+    static final String SYMBOL = "-";
+
+    public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름이 5글자 이상입니다.");
+        }
+        this.name = name;
+    }
+
+}
