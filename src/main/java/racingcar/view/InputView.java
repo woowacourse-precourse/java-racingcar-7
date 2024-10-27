89cf2ee137a3e765768067b6182c6f90d481d783
ruudska6 <feels19@naver.com>
2024-10-27 12:55:43 +0900

    <feat>: InputView 사용자 입력 값 예외 처리 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,11 +6,25 @@ import java.util.List;
 public class InputView {
     private static final String QUESTION_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String QUESTION_TRY_COUNT = "시도할 회수는 몇회인가요?";
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final int MIN_CAR = 2;
 
     public List<String> askCarNames() {
-            System.out.println(QUESTION_CAR_NAMES);
-            String inputCarNames = Console.readLine();
-            return convertInputCarNamesToList(inputCarNames);
+        System.out.println(QUESTION_CAR_NAMES);
+        String inputCarNames = Console.readLine();
+        List<String> carNames = convertInputCarNamesToList(inputCarNames);
+
+        for (String name : carNames) {
+            if (name.length() > MAX_NAME_LENGTH) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
+
+        if (carNames.size() < MIN_CAR) {
+            throw new IllegalArgumentException("경주를 하기 위해서는 최소 2대 이상의 자동차가 필요합니다.");
+        }
+
+        return convertInputCarNamesToList(inputCarNames);
         }
 
     public int askTryCount () {
