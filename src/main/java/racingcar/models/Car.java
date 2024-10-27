53b1fcef2cbd61edf4320ba07ec4f39eda9d0037
sangxxjin <sj_0368@naver.com>
2024-10-27 23:07:53 +0900

    feat: 자동차 거리 증가 메서드 구현
    본문:
    제공 받은 Randoms 사용해서 랜덤값이 4이상일때 거리 증가하게 구현했습니다.
    숫자 4는 매직넘버 처리 했습니다.

--- src/main/java/racingcar/models/Car.java
+++ src/main/java/racingcar/models/Car.java
@@ -1,12 +1,21 @@
 package racingcar.models;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private static final int MOVE_THRESHOLD = 4;
     private int distance;
     private String name;
 
     public Car(String name) {
         this.name = name;
         this.distance = 0;
+    }
+
+    public void incrementDistance() {
+        if (Randoms.pickNumberInRange(0, 9) >= MOVE_THRESHOLD) {
+            this.distance++;
+        }
     }
 
 }
