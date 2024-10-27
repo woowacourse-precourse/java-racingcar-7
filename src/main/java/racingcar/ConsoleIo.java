9c5105c4c47461df16e1091d87d3fc665d420b8f
bokyeong <wntjdals0412@gmail.com>
2024-10-27 23:12:28 +0900

    fix : 결과 출력 형식 수정

--- src/main/java/racingcar/ConsoleIo.java
+++ src/main/java/racingcar/ConsoleIo.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class ConsoleIo {
     private static final String NAME_INPUT_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String COUNT_INPUT_MSG = "시도할 횟수는 몇 회인가요?";
-    private static final String OUTPUT_MSG = "실행결과";
+    private static final String OUTPUT_MSG = "실행 결과";
 
     public String getNameInput() {
         System.out.println(NAME_INPUT_MSG);
@@ -19,6 +19,7 @@ public class ConsoleIo {
     }
 
     public void printResultMessage() {
+        System.out.println();
         System.out.println(OUTPUT_MSG);
     }
 
