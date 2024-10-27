b0914cdefcc0636d35cde43491d6c384e8cf6195
MinJae00 <exceedmin@gmail.com>
2024-10-28 03:50:16 +0900

    refact: userController 선 호출하고 유저 객체 생성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,20 +2,24 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.controller.GameController;
+import racingcar.controller.UserController;
+import racingcar.user.UserObject;
 
 public class InputView {
     public static int trynum;
-    public static String[] result;
+    public static String result;
+    public UserObject[] user;
 
     public InputView() {
         result = getUserInput();
         trynum = getTryCount();
-        new GameController().runGame();
+        user = new UserController(result).settingUser();
+        new GameController(user).runGame();
     }
-    
-    private String[] getUserInput() {
-        System.out.println("경주할 자동차 이름을 입력하세.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine().split(",");
+
+    private String getUserInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine().trim();
     }
 
     private int getTryCount() {
