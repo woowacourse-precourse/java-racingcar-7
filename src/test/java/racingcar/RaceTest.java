630c858847480bccd30e285a0883d7bf8330ee87
kimdev <kim__dev@hufs.ac.kr>
2024-10-27 16:24:40 +0900

    test(Race): 자동차 등록 테스트 케이스를 추가하라

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -1,19 +1,33 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.model.Car;
 import racingcar.model.Race;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RaceTest {
+
+    private Race race;
+
+    @BeforeEach
+    public void Car_객체들과_함께_Race_객체를_생성한다() {
+        race = new Race(5);
+        race.join(new Car("firstCar"));
+        race.join(new Car("secondCar"));
+        race.join(new Car("thirdCar"));
+    }
+
     @Test
     @DisplayName("일정한_턴을_가진_게임이_생성된다")
     public void 일정한_턴을_가진_게임이_생성된다() {
-        int turnCount = 5;
-
         Race race = new Race(5);
         assertTrue(true);
     }
