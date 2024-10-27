afe0b4ff168ec37543fbbb2a8006e3e9e21cad19
wanbinchoi <cwb6278@gmail.com>
2024-10-27 16:10:42 +0900

    add input method for car names

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> cars = getCarNames();
+    }
+
+    // 자동차 이름 입력
+    private static List<String> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] names = input.split(",");
+        List<String> cars = new ArrayList<>();
+
+        for (String name : names) {
+            cars.add(name.trim());
+        }
+        return cars;
     }
 }
