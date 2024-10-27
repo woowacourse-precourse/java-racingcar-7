60ccb42c070b6a1cc9f2b6c935024d4e3a378661
pakaheo <k70923@konkuk.ac.kr>
2024-10-28 01:07:18 +0900

    feat : Create constants to use in global

---
+++ src/main/java/racingcar/constants/Constants.java
@@ -0,0 +1,11 @@
+package racingcar.constants;
+
+public class Constants {
+    public static final String EXECUTION_RESULT_PROMPT = "\n실행 결과";
+
+    
+    public static final String FINAL_WINNERS_PROMPT = "최종 우승자 : ";
+    public static final String NOT_CAR_NAMES_DIVIDED_COMMA = "자동차 이름은 콤마로 구분해야 합니다";
+    public static final String DUPLICATE_CAR_NAMES = "자동차 이름은 콤마로 구분해야 합니다";
+    public static final String INVALID_NAME_LENGTH = "자동차 이름은 1자 이상 5자 이하여야 합니다";
+}
