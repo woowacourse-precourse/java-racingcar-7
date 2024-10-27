810d6f333390afe0bf05fdef71936d9f5b0917d5
jeonghyeon-ohk <ok6737@naver.com>
2024-10-27 18:10:21 +0900

    feat(view) : 최종 결과 출력 기능 및 자동차 전진 거리 출력 기능 구현
    - printResult : 최종 우승자 문자열을 콘솔에 출력
    - printForwardResult : 각각의 자동차 이름과 현재 이동거리를 콘솔에 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,17 @@
 package racingcar.view;
 
 public class OutputView{
+    public static void printResult(String result) {
+        System.out.print("최종 우승자 : " + result);
+    }
+
+    public static void printForwardResult(String[] cars, int[] forwardDistance) {
+        for (int index = 0; index < cars.length; index++) {
+            String forwardResult = cars[index] + " : "
+                    + "-".repeat(forwardDistance[index]);
+
+            System.out.println(forwardResult);
+        }
+        System.out.println();
+    }
 }
