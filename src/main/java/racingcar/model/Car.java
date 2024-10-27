755fa41c74861ad36ff9380adb84f2e180669887
Jinu Yi <dionisos198@naver.com>
2024-10-27 21:46:22 +0900

    refactor(Car): 차의 이름 검증 로직 순서 변경

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -16,13 +16,14 @@ public class Car {
 
     private void checkValidateName(final String name) {
 
+        if(name==null || name.isBlank()){
+            throw new IllegalArgumentException(ErrorCode.CANT_CONVERT_CAR_NAME_WITH_NULL_OR_BLANK.getMessage());
+        }
+
         if(name.length()>5){
             throw new IllegalArgumentException(ErrorCode.CANT_CAR_NAME_LENGTH_EXCEED_FIVE.getMessage());
         }
 
-        if(name==null || name.isBlank()){
-            throw new IllegalArgumentException(ErrorCode.CANT_CONVERT_CAR_NAME_WITH_NULL_OR_BLANK.getMessage());
-        }
     }
 
     public void moveForward(){
