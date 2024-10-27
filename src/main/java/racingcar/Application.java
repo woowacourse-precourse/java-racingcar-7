c98905f920a5fb748e4a9f54323e4a2fa7ee7bfe
LaiTial <asna90@naver.com>
2024-10-27 16:44:24 +0900

    Validate Test

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,7 +8,10 @@ public class Application {
         String racingName = Console.readLine();  // 레이싱 카 이름 목록
         String tryNum = Console.readLine(); // 시도 횟수
 
-        //Validate.check(racingName, tryNum); // 입력 값 검증
+        Validate.isNameLengthValid(racingName); // 이름이 5글자 이하인지 검증
+        Validate.isNameCommaSeparated(racingName); // 정확한 형식을 갖추고 있는지
+        Validate.isNumeric(tryNum); // 시도횟수가 숫자인지
+
         //Racing race = new Racing(racingName) // 레이싱 카 생성
         //race.run(tryNum) // 레이싱 카 게임 시작
     }
