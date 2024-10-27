5f8dcb26212879c2022c43378b4359e8df1ebcd0
USER <cji513@naver.com>
2024-10-27 20:42:11 +0900

    refactor(RacingController): 코드 가독성을 위한 코드 정렬

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -14,22 +14,22 @@ public class RacingController {
     private final OutputView outputView;
 
     public RacingController() {
-    this.inputView = new InputView();
-    this.outputView = new OutputView();
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
     }
 
-    public void run(){
+    public void run() {
         List<Car> carList = makeCarList(inputView.names());
         int repeat = stringToInt(inputView.repeat());
-        raceStart(carList,repeat);
+        raceStart(carList, repeat);
         outputView.winners(carList);
     }
 
-    List<Car> makeCarList(String names){
+    List<Car> makeCarList(String names) {
         ArrayList<String> nameList = new ArrayList<>(Arrays.asList(names.split(",")));
         ArrayList<Car> carList = new ArrayList<>();
 
-        for (String name : nameList){
+        for (String name : nameList) {
             checkNameCondition(name);
             carList.add(new Car(name));
         }
@@ -37,32 +37,32 @@ public class RacingController {
         return carList;
     }
 
-    public void raceStart(List<Car> carList,int repeat){
+    public void raceStart(List<Car> carList, int repeat) {
         outputView.resultText();
-        while (repeat-- != 0){
+        while (repeat-- != 0) {
             raceOneCircle(carList);
             outputView.resultPerOneCircle(carList);
         }
     }
 
-    void raceOneCircle(List<Car> carList){
-        for (Car car : carList){
+    void raceOneCircle(List<Car> carList) {
+        for (Car car : carList) {
             car.race();
         }
     }
 
-    int stringToInt(String string){
+    int stringToInt(String string) {
         checkNumberCondition(string);
         return Integer.parseInt(string);
     }
 
-    void checkNameCondition(String name){
-        if (name.length() > 5){
+    void checkNameCondition(String name) {
+        if (name.length() > 5) {
             throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
         }
     }
 
-    void checkNumberCondition(String number){
+    void checkNumberCondition(String number) {
         String regex = "^(0|[1-9]\\d*)$";
         if (!number.matches(regex)) {
             throw new IllegalArgumentException("0또는 양수를 입력해주세요.");
