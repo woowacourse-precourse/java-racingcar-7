466fad510fa1075fda5eb1c7ceddb128326ecffc
EP45 <5510816+EP45@users.noreply.github.com>
2024-10-28 01:09:29 +0900

    feat(Application): add basic carNamesRaw parser
    - Basic input / splitter for carNamesRaw
    - Need more validation
    - Update docs accordingly

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Map<String, Integer> cars = new LinkedHashMap<>();
+
+        // Input carNamesRaw
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNamesRaw = Console.readLine();
+
+        // Parse names and register
+        for (String carName : carNamesRaw.split("\\s*,\\s*")) {
+            // scenario 1: process leading/trailing non-standard space
+            carName = carName.strip();
+            // TODO: validate more
+            cars.put(carName, 0);
+        }
+
+        // Temporary output
+        System.out.println(cars);
+
+        // TODO: Process iterationNumberRaw
+
+        // TODO: Roll and output middle results
+
+        // TODO: Output final winner
     }
 }
