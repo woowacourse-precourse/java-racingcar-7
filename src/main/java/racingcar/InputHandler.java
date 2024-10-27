d3721f2eaffc666a6e6304d26f8c18476a5f41a0
kimchijinju <hanbimkim@gmail.com>
2024-10-28 07:10:35 +0900

    feat: 자동차 이름과 횟수 입력받기

---
+++ src/main/java/racingcar/InputHandler.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+
+    private static void validateInput(final String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+        }
+    }
+
+    public static String readLine(final String printMessage) {
+        System.out.println(printMessage);
+
+        String input = Console.readLine();
+        validateInput(input);
+
+        return Console.readLine();
+    }
+}
