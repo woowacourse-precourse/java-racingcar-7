537f17420cf2b5c332364ad138e5caacd51f0015
Seongho Kim <kksshh0612@gmail.com>
2024-10-28 01:25:00 +0900

    refactor: 게임에서 자동차 움직이는 로직 분리

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -1,37 +1,39 @@
 package racingcar.service;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import racingcar.dto.RacingResult;
 import racingcar.model.Car;
 
 public class RacingCarService {
 
-    private static final int MINIMUM_TRY_COUNT = 1;
+    private static final int MINIMUM_GO_NUMBER = 4;
 
     public RacingResult startRacingGame(List<Car> cars, int tryCount) {
 
         StringBuilder racingResultBuilder = new StringBuilder();
 
-        while (tryCount-- >= MINIMUM_TRY_COUNT) {
-            for (Car car : cars) {
-                int ramdomNumber = Randoms.pickNumberInRange(0, 9);
-
-                racingResultBuilder.append(car.getName()).append(" : ");
-                if (ramdomNumber >= 4) {      // 전진
-                    car.go();
-                }
-                for (int i = 0; i < car.getPos(); i++) {
-                    racingResultBuilder.append("-");
-                }
-                racingResultBuilder.append("\n");
-            }
-            racingResultBuilder.append("\n");
+        while (tryCount-- > 0) {
+            moveCars(cars, racingResultBuilder);
         }
 
         List<String> winnerNames = generateWinnerResult(cars);
 
         return RacingResult.of(racingResultBuilder.toString(), winnerNames);
+    }
+
+    private void moveCars(List<Car> cars, StringBuilder racingResultBuilder) {
+
+        for (Car car : cars) {
+            racingResultBuilder.append(car.getName()).append(" : ");
+            if (RandomUtil.generateRandomNumber() >= MINIMUM_GO_NUMBER) {      // 전진
+                car.go();
+            }
+            for (int i = 0; i < car.getPos(); i++) {
+                racingResultBuilder.append("-");
+            }
+            racingResultBuilder.append("\n");
+        }
+        racingResultBuilder.append("\n");
     }
 
     private List<String> generateWinnerResult(List<Car> cars) {
