4a9087a409584dde7221e79016d1405dd8506112
023-dev <0_2_3@naver.com>
2024-10-27 22:26:17 +0900

    feat(domain): 가장 먼 거리를 이동한 자동차를 우승자로 판별하는 기능 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -16,6 +16,15 @@ public class Cars {
         this.cars.forEach(Car::move);
     }
 
+    public List<Car> getWinner() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max().orElseThrow(IllegalArgumentException::new);
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .collect(Collectors.toList());
+    }
+
     public List<Car> getCars() {
         return cars;
     }
