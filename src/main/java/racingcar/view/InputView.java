3166f08829b37d8f5b2d2eea8711b1fcecd25b28
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:04:06 +0900

    :sparkles: feat(InputView): 경주 실행 횟수를 입력받는다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,7 @@
 package racingcar.view;
 
 import static racingcar.message.InfoMessages.INPUT_CAR_NAMES;
+import static racingcar.message.InfoMessages.INPUT_TRY_COUNT;
 
 import camp.nextstep.edu.missionutils.Console;
 
@@ -8,6 +9,11 @@ public class InputView {
 
     public String readCarNames() {
         System.out.println(INPUT_CAR_NAMES);
+        return Console.readLine();
+    }
+
+    public String readTryCount() {
+        System.out.println(INPUT_TRY_COUNT);
         return Console.readLine();
     }
 }
