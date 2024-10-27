71caba8682cabfd6a274d872f5320b1fcd09b00d
YoungGaLee <rkduddl1717@gmail.com>
2024-10-27 15:49:05 +0900

    fix: 사용자 입력 수정
    프로그래밍 요구사항에 따라 메소드가 한가지 일만 하도록 수정하였습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,13 +2,36 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO : refactoring
+    Car[] cars;
+    int attempt;
+    int carCount;
+
+    void inputCar() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputCar = Console.readLine();
         String[] carName = inputCar.split(",");
-        // TODO: 글자수 체크, IllegalArgumentException 발생
+
+        this.carCount = carName.length;
+        this.cars = new Car[carCount];
+
+        for(int i=0;i<carCount;i++){
+            this.cars[i] = new Car(carName[i].trim());
+        }
+    }
+
+    void inputNum() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        int num = Integer.parseInt(Console.readLine());
+        this.attempt = Integer.parseInt(Console.readLine());
+        System.out.println();
+    }
+
+
+    public void start(){
+        inputCar();
+        inputNum();
+    }
+
+    public static void main(String[] args) {
+        new Application().start();
     }
 }
