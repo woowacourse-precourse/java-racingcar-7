cabe97e53a6e82a00a6498f1bb97328be83113e8
threepebbles <threepebbles@naver.com>
2024-10-28 05:21:28 +0900

    feat(RacingGame.java): 자동차 경주 게임을 진행하는 기능 추가

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,56 @@
+package racingcar.domain;
+
+import java.util.Collections;
+import java.util.List;
+import racingcar.dto.RoundResult;
+import racingcar.utils.CustomNumberGenerator;
+import racingcar.utils.NumberGenerator;
+
+public class RacingGame {
+    private final List<Player> players;
+    private final int roundCount;
+    private final CustomNumberGenerator customNumberGenerator;
+    private List<RoundResult> roundResults;
+
+    public RacingGame(List<Player> players, int roundCount) {
+        this.players = players;
+        this.roundCount = roundCount;
+        customNumberGenerator = new CustomNumberGenerator();
+    }
+
+    public void play() {
+        for (int round = 1; round <= roundCount; round++) {
+            playOneRound();
+            roundResults.add(RoundResult.createRoundResult(round, players));
+        }
+    }
+
+    public void playOneRound() {
+        for (Player player : players) {
+            moveCarByRule(player, customNumberGenerator);
+        }
+    }
+
+    public void moveCarByRule(Player player, NumberGenerator numberGenerator) {
+        int randomNumber = numberGenerator.generateInteger();
+        int step = 1;
+
+        if (randomNumber >= 4) {
+            player.moveCar(step);
+        }
+    }
+
+    public List<RoundResult> getRoundResults() {
+        return roundResults;
+    }
+
+    public List<String> getWinners() {
+        int maxPosition = Collections.max(
+                players.stream().map(Player::getPosition).toList()
+        );
+        return players.stream()
+                .filter(player -> player.getPosition() == maxPosition)
+                .map(Player::getCarName)
+                .toList();
+    }
+}
