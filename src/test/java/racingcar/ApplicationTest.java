0a436e677db7add8a5ffbc6893a34acb281159b4
jungsehui <jungsehui@naver.com>
2024-10-27 17:04:20 +0900

    feat(ApplicationTest.java): 자동차 입력 테스트 코드 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -13,21 +13,33 @@ class ApplicationTest extends NsTest {
     private static final int STOP = 3;
 
     @Test
+    void 자동차_이름_입력_테스트() {
+        // Given
+        String inputCarName = "pobi,java";
+
+        // When
+        String[] carNames = inputCarName.split(",");
+
+        // Then
+        assertThat(carNames).contains("pobi", "java");
+    }
+
+    @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
