d9f8cf053fe6ae21897bb27e87004a21bba9554e
KIM MINSEOK <mskim.glenn@gmail.com>
2024-10-27 17:22:52 +0900

    refactor: 불필요한 주석 제거

--- src/main/java/racingcar/domain/WinnerSelector.java
+++ src/main/java/racingcar/domain/WinnerSelector.java
@@ -3,14 +3,11 @@ package racingcar.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 우승자 선정 기능을 제공하는 클래스
- */
 public class WinnerSelector {
 
     /**
-     * 주어진 자동차 리스트에서 최종 우승자를 선정하여 반환합니다.
-     * 여러 자동차가 공동 우승일 경우, 모두 반환합니다.
+     * 주어진 자동차 리스트에서 최종 우승자를 선정하여 반환한다.
+     * 여러 자동차가 공동 우승일 경우, 모두 반환한다.
      *
      * @param cars 경주에 참여한 자동차 리스트
      * @return 우승자 이름 리스트
