617934f730d2be57345f3ff3b8f171d405255fdd
apple-mint <174938449+apple-mint@users.noreply.github.com>
2024-10-28 02:16:25 +0900

    feat: 경주할 자동차 이름 입력 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 1. 문자열 입력받기
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String words = Console.readLine();
     }
 }
