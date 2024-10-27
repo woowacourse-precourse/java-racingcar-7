87fce0d32c624ab6b49d15789333a4388235c79d
dongjae <harold306122@gmail.com>
2024-10-27 17:14:55 +0900

    refactor(Application):사용하지 않는 import문 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 import racingcar.controller.CarRaceController;
-import racingcar.view.CarRaceView;
 public class Application {
     public static void main(String[] args) {
         CarRaceController carRaceController = new CarRaceController();
