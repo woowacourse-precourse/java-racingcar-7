b377975b5aabd17339fb763cd6273bd8c63e9aa1
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-28 05:24:56 +0900

    feat(InputView): 시도 횟수를 입력받아 Long으로 변환하는 메서드 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,12 +1,14 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.validator.MovingCountValidator;
 import racingcar.validator.RawCarNamesValidator;
 
 public class InputView {
 
     private final OutputView outputView = new OutputView();
     private final RawCarNamesValidator rawCarNamesValidator = new RawCarNamesValidator();
+    private final MovingCountValidator movingCountValidator = new MovingCountValidator();
 
     public String inputCarNames() {
         outputView.printCarNamesInstruction();
@@ -15,5 +17,12 @@ public class InputView {
         rawCarNamesValidator.isEmpty(rawCarNames);
 
         return rawCarNames;
+    }
+
+    public Long inputMovingCount() {
+        outputView.printMovingCountInstruction();
+        String rawMovingCount = Console.readLine();
+
+        return movingCountValidator.validateMovingCount(rawMovingCount);
     }
 }
