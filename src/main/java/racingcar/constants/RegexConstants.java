1fb729372effe7d2f027b70a2e5242c4e665e800
hyeondo <eun041212@gmail.com>
2024-10-27 10:06:26 +0900

    style(RegexConstants) : 정규식 상수 클래스의 가독성 향상을 위한 JavaDoc 주석 추가 및 문자열 포맷 사용

--- src/main/java/racingcar/constants/RegexConstants.java
+++ src/main/java/racingcar/constants/RegexConstants.java
@@ -1,16 +1,33 @@
 package racingcar.constants;
 
+/**
+ * 정규식 패턴 상수를 정의하는 클래스
+ */
 public class RegexConstants {
 
     public static final String COMMA = ",";
     public static final String COMMA_SPACE = ", ";
     public static final String COLON = ":";
 
-    private static final String HANGUL_PATTERN = "가-힣";
-    private static final String ALPHABET_PATTERN = "a-zA-Z";
-    private static final String NUMBER_PATTERN = "0-9";
-    private static final String CAR_NAME_PATTERN = "[" + HANGUL_PATTERN + ALPHABET_PATTERN + NUMBER_PATTERN + "]+";
-    public static final String INPUT_CARS_PATTERN = "^(" + CAR_NAME_PATTERN + "(" + COMMA + CAR_NAME_PATTERN + ")*)$";
+    private static final String KOREAN_CHAR_RANGE = "가-힣";
+    private static final String ALPHABET_RANGE = "a-zA-Z";
+    private static final String DIGIT_RANGE = "0-9";
 
+    /**
+     * 자동차 이름은 한글, 알파벳, 숫자로 이루어질 수 있다.
+     * 예: "ABC", "123", "가나다"
+     */
+    private static final String CAR_NAME_PATTERN = String.format("[%s%s%s]+", KOREAN_CHAR_RANGE, ALPHABET_RANGE, DIGIT_RANGE);
+
+    /**
+     * 자동차 이름은 쉼표(,)로 구분되며, 각 이름은 한글, 알파벳, 숫자 조합으로 이루어져야 한다.
+     * 예: "가나다,ABC,123"
+     */
+    public static final String INPUT_CARS_PATTERN = String.format("^(%s(%s%s)*)$", CAR_NAME_PATTERN, COMMA, CAR_NAME_PATTERN);
+
+    /**
+     * 양의 정수를 나타내는 패턴. 반복 횟수 검증에 사용
+     * 예: "1", "10", "123"
+     */
     public static final String INTEGER_PATTERN = "^[1-9][0-9]*$";
 }
