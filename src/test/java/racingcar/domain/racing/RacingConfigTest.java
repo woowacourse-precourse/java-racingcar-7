768dee59c6f6f696b3dc73311da301d55c7ce994
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-10-27 22:41:21 +0900

    test(RacingConfigTest): 객체 생성 및 잘못된 입력값에 대한 테스트
    - 자동차 이름이 5자 보다 많은 경우
    - 자동차 이름이 공백인 경우
    - 자동차 이름이 중복된 경우
    - 시도할 횟수에 숫자가 아닌 문자가 들어온 경우
    - 시도할 횟수가 음수인 경우

--- src/test/java/racingcar/domain/racing/RacingConfigTest.java
+++ src/test/java/racingcar/domain/racing/RacingConfigTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain.racing;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -47,5 +48,76 @@ class RacingConfigTest {
 
         //then
         assertThat(actualRounds).isEqualTo(Long.valueOf(rounds));
+    }
+
+    @Test
+    @DisplayName("객체 생성")
+    void initWith() {
+        //given
+        List<String> expectedCars = List.of("test1", "test2", "test3");
+
+        //when
+        RacingConfig racingConfig = new RacingConfig(cars, rounds, cartype);
+        List<String> actualCars = racingConfig.getCars().stream()
+                .map(Car::getName).toList();
+
+        //then
+        assertThat(actualCars).isEqualTo(expectedCars);
+        assertThat(racingConfig.getRounds()).isEqualTo(Long.valueOf(rounds));
+    }
+
+    @Test
+    @DisplayName("객체 생성 - 자동차 이름이 5자보다 많은 경우")
+    void initWithTooLongCarName() {
+        //given
+        cars = "test1,test2,test3,TooLongCarName";
+
+        //when & then
+        assertThatThrownBy(() -> new RacingConfig(cars, rounds, cartype))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("객체 생성 - 자동차 이름이 공백인 경우")
+    void initWithEmptyCarName() {
+        //given
+        cars = "test1,,test3";
+
+        //when & then
+        assertThatThrownBy(() -> new RacingConfig(cars, rounds, cartype))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("객체 생성 - 자동차 이름이 중복된 경우")
+    void initWithDuplicatedCarName() {
+        //given
+        cars = "test1,test1,test3";
+
+        //when & then
+        assertThatThrownBy(() -> new RacingConfig(cars, rounds, cartype))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("객체 생성 - 시도할 횟수에 문자가 들어온 경우")
+    void initWithCharacterRounds() {
+        //given
+        rounds = "a";
+
+        //when & then
+        assertThatThrownBy(() -> new RacingConfig(cars, rounds, cartype))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("객체 생성 - 시도할 횟수가 음수인 경우")
+    void initWithNegativeRounds() {
+        //given
+        rounds = "-1";
+
+        //when & then
+        assertThatThrownBy(() -> new RacingConfig(cars, rounds, cartype))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
