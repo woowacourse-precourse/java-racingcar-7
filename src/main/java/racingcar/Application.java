6f6500eb395a5845a8ee643d45dc02116a2927bd
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2024-10-27 16:12:54 +0900

    feat : Car 클래스 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,30 +4,28 @@ package racingcar;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
 
         try {
             // 자동차 이름 입력 받기
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-            String inputNames = scanner.nextLine();
+            String inputNames = Console.readLine();
             List<String> carNames = Arrays.asList(inputNames.split(","));
 
             checkCarNames(carNames);
 
             // 시도 횟수 입력 받기
             System.out.println("시도할 횟수는 몇 회인가요?");
-            int tryCount = Integer.parseInt(scanner.nextLine());
+            int tryCount = Integer.parseInt( Console.readLine());
 
         } catch (IllegalArgumentException e) {
             System.out.println("입력 오류: " + e.getMessage());
-        } finally {
-            scanner.close();
         }
-
-        
-
 
     }
 
