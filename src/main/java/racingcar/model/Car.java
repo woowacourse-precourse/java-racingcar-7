4b90897aa7218fd63de8c484becabba386ec4ef0
Goohong Chung <9rrong@uoslife.team>
2024-10-27 21:01:27 +0900

    refactor(Car): `position`의 기본값 상수화(DEFAULT_POSITION)

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,10 +4,11 @@ import org.junit.platform.commons.util.StringUtils;
 
 public class Car {
 
-    public static final int INCREMENT_VALUE = 1;
-    private final int NAME_MAX_LENGTH = 5;
+    private static final int INCREMENT_VALUE = 1;
+    private static final int DEFAULT_POSITION = 0;
+    private static final int NAME_MAX_LENGTH = 5;
+    private static int position = DEFAULT_POSITION;
     private final String name;
-    private int position = 0;
 
     public Car(String name) {
         this.name = name;
