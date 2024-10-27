4023a28f9aceb1f7c027827f23ba6c25d167389b
tjdrbs0712 <102494118+tjdrbs0712@users.noreply.github.com>
2024-10-28 05:14:03 +0900

    test : 테스트 코드 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -28,6 +29,81 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("우승자가 여러명일 경우")
+    void test1(){
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi,woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    @DisplayName("이름을 입력하지 않은 경우")
+    void test2(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("\n"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("마지막 자동차 이름을 입력하지 않은 경우")
+    void test3(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni,"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("이름 구분자를 잘못 사용한 경우")
+    void test4(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",pobi,woni"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("이름이 5글자 초과한 경우")
+    void test5(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("aaaaaa"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("시도 횟수가 0이하인 경우")
+    void test6(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("시도 횟수가 숫자가 아닌 경우")
+    void test7(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "aa"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("시도 횟수가 int 범위를 넘어간 경우")
+    void test8(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "999999999999"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
---
+++ src/test/java/racingcar/util/WinnerCalculatorTest.java
@@ -0,0 +1,45 @@
+package racingcar.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WinnerCalculatorTest {
+
+    @Test
+    @DisplayName("우승자가 한명인 경우")
+    public void test1(){
+        List<Car> cars = List.of(
+                new Car("car1"),
+                new Car("car2"),
+                new Car("car3")
+        );
+        cars.get(0).move(5);
+        cars.get(1).move(0);
+        cars.get(2).move(0);
+
+        List<String> winners = WinnerCalculator.calculateWinners(cars);
+        assertThat(List.of("car1")).isEqualTo(winners);
+    }
+
+    @Test
+    @DisplayName("우승자가 여러명인 경우")
+    public void test2(){
+        List<Car> cars = List.of(
+                new Car("car1"),
+                new Car("car2"),
+                new Car("car3")
+        );
+        cars.get(0).move(5);
+        cars.get(1).move(5);
+        cars.get(2).move(0);
+
+        List<String> winners = WinnerCalculator.calculateWinners(cars);
+        assertThat(List.of("car1", "car2")).isEqualTo(winners);
+    }
+
+}
