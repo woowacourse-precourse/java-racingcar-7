9afe3d7cd78ac412a0ac18d3fd4feeb42651e6b4
minseojeong <jgoldstone7@gmail.com>
2024-10-27 15:58:54 +0900

    refactor: 입력값 받을때 출력문 상수화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,10 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
+    private static final String CARNAMES_INPUTMESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
+    private static final String COUNT_INPUTMESSAGE = "시도할 횟수는 몇 회인가요?";
+
     public static void main(String[] args) {
         String carNames = getCarNames();
         int count = getCount();
@@ -10,7 +14,7 @@ public class Application {
     }
 
     private static int getCount() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(COUNT_INPUTMESSAGE);
         String CountInputValue = Console.readLine();
         int count = validateCountInputValue(CountInputValue);
         return count;
@@ -30,7 +34,7 @@ public class Application {
     }
 
     private static String getCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println(CARNAMES_INPUTMESSAGE);
         String carNames = Console.readLine();
         //차이름이 5자 이하인지 검사
         return carNames;
