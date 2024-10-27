36ac49a6cda4a7f5749bdadf07de3a0795635593
Gyuri <haha0888@naver.com>
2024-10-27 17:55:48 +0900

    feat. 자동차 정보 클래스 구현

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,27 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+
+public class Car {
+    private final String name;
+    private int distance;
+
+    public Car(String name){
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public void move(){
+            this.distance++;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getDistance(){
+        return this.distance;
+    }
+
+}
