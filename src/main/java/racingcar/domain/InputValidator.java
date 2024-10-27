ed6129075534cbe22848aa49f79cc423ea151672
KangGeonyoung <kky6335@gmail.com>
2024-10-27 19:59:06 +0900

    feat(InputValidator): 자동차 이름 유효성 검사 InputValidator - splitCarName: 문자열을 쉼표(,)를 구분자로 하여 분리하는 메서드 - checkValidLength: 자동차 이름이 5자 이하인지 검사하는 메서드

---
+++ src/main/java/racingcar/domain/InputValidator.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class InputValidator {
+
+    /**
+     * 문자열을 쉼표(,)를 구분자로 하여 분리
+     */
+    public static List<String> splitCarName(String input) {
+        List<String> cars = Stream.of(input.split(",")).collect(Collectors.toList());
+        return cars;
+    }
+
+    /**
+     * 자동차 이름이 5자 이하인지 검사
+     */
+    public static void checkValidLength(List<String> cars) {
+        for (String car : cars) {
+            if (car.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+}
