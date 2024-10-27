2b3d400bde72ed50e61e5bc8fdc533abaca364c0
tjdrbs0712 <102494118+tjdrbs0712@users.noreply.github.com>
2024-10-28 05:05:30 +0900

    refactor : 에러 메세지 수정

--- src/main/java/racingcar/message/ErrorMessage.java
+++ src/main/java/racingcar/message/ErrorMessage.java
@@ -2,7 +2,7 @@ package racingcar.message;
 
 public enum ErrorMessage implements MessageManager{
     INVALID_NAME_LENGTH("자동차 이름은 5글자를 초과할 수 없습니다."),
-    INVALID_NUMBER("정수를 입력해주세요."),
+    INVALID_NUMBER("잘못된 입력입니다."),
     INVALID_NAME_INPUT("이름은 비어있거나 공백일 수 없습니다."),
     DUPLICATE_NAME("자동차 이름은 중복될 수 없습니다."),
     NEGATIVE_NUMBER("1 이상의 숫자를 입력해주세요."),
