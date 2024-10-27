043cd91542950a4f605fda96b8caf6a656c7062b
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 22:13:11 +0900

    feat(DomainFactory): 도메인 객체 생성
    - DomainFactory 가 도메인 객체 생성의 책임을 가질 수 있도록 각 도메인 객체들을 생성해주는 구현부 추가

--- src/main/java/racingcar/Factory/DomainFactory.java
+++ src/main/java/racingcar/Factory/DomainFactory.java
@@ -1,4 +1,19 @@
 package racingcar.Factory;
 
+import racingcar.Domain.CarInfo;
+import racingcar.Domain.RaceResultEvaluator;
+import racingcar.Domain.RandomNumber;
+
 public class DomainFactory {
+    public CarInfo createCarInfo() {
+        return new CarInfo();  // 팩토리 내부에서 객체 생성
+    }
+
+    public RaceResultEvaluator createRaceResultEvaluator() {
+        return new RaceResultEvaluator();
+    }
+
+    public RandomNumber createRandomNumber() {
+        return new RandomNumber();
+    }
 }
