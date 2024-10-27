5bd3232d784b6bd1567a40bffa87d1bdc287a0fc
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:07:58 +0900

    :sparkles: feat(OutputView): 각 경주 턴마다 자동차의 현재 위치를 출력한다

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,27 @@
 package racingcar.view;
 
+import static racingcar.message.InfoMessages.OUTPUT_RUN_RESULT;
+
+import java.util.List;
+import racingcar.model.Car;
+
 public class OutputView {
+    private static final String ONE_STEP = "-";
+
+    public void displayRaceResult() {
+        System.out.println(System.lineSeparator() + OUTPUT_RUN_RESULT);
+    }
+
+    public void displayRaceProgress(List<Car> cars) {
+        StringBuilder raceProgress = new StringBuilder();
+
+        cars.forEach(car ->
+                raceProgress.append(car.getName())
+                        .append(" : ")
+                        .append(ONE_STEP.repeat(car.getPosition()))
+                        .append(System.lineSeparator())
+        );
+
+        System.out.println(raceProgress);
+    }
 }
