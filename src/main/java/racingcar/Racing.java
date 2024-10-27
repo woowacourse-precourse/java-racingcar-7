68c5572138d5411c4aa66ad2fa211d977e8b244e
gayoung228 <103436798+gayoung228@users.noreply.github.com>
2024-10-28 02:59:59 +0900

    경주 기능 구현
    각 자동차에 대해 무작위로 움직임을 결정하고, 이동할 때마다 자동차의 상태를 업데이트하는 경주 로직을 구현.

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,4 +1,31 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class Racing {
+    private final List<Car> cars;
+    private static final int FORWARD_THRESHOLD = 4;
+    private final Random random;
+    private boolean isFirstRound = true; // 첫 번째 라운드인지 확인하는 변수
+
+    public Racing(List<String> carNames) {
+        this.cars = carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+        this.random = new Random();
+    }
+
+    public void startRace(int moves) {
+        for (int i = 0; i < moves; i++) {
+            for (Car car : cars) {
+                if (random.nextInt(10) >= FORWARD_THRESHOLD) {
+                    car.move();
+                }
+            }
+            displayRaceStatus();
+        }
+    }
 }
