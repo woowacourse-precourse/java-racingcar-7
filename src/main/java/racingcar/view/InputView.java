ea34f14dfce8c72298257016ec722c5912735f6e
kymin <park48664866@gmail.com>
2024-10-27 12:00:06 +0900

    refactor($input): enum을 이용한 메시지 출력으로 변경
    enum을 이용한 메시지 출력으로 변경:
    - 기존에 상수를 선언하고 출력하던 방식에서 enum이 관리하는 메시지 출력으로 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,14 +1,14 @@
 package racingcar.view;
 
+import racingcar.util.IOMessage;
+
 public class InputView {
-    private final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final String INPUT_TRIALMESSAGE = "시도할 횟수는 몇 회인가요?";
 
     public void printInputCarNameMessage() {
-        System.out.println(INPUT_CAR_NAME_MESSAGE);
+        System.out.println(IOMessage.INPUT_CAR_NAME_MESSAGE.getMessage());
     }
 
     public void printInputTrialMessage() {
-        System.out.println(INPUT_TRIALMESSAGE);
+        System.out.println(IOMessage.INPUT_TRIAL_MESSAGE.getMessage());
     }
 }
