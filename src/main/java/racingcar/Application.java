72a738722cb85699ff625f1b715d6f3379ccd7d4
hyunjilee1132 <hyunji.lee1132@gmail.com>
2024-10-27 14:27:45 +0900

    feat: 입력 받는 함수 구현
    - 자동차 이름 입력 받는 함수
    - 시도할 횟수 입력 받는 함수

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Objects;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static java.lang.Integer.parseInt;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputParser inputParser = new InputParser(",", 5);
+        InputResult inputResult = inputParser.process();
+
+        System.out.println();
     }
 }
