5b30f5035a21455b96cc392ff06c9259ad7244c9
SECHANG1412 <binaryblitz1412@gmail.com>
2024-10-27 23:14:58 +0900

    feat: 사용자 입력을 담당하는 InputView 클래스 생성

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return scanner.nextLine();
+    }
+
+    public static int getTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(scanner.nextLine());
+    }
+}
