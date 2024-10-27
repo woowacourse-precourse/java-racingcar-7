5989704c90729bdf99f2b3a486fa559b6387ce27
seoys0910 <seoys0910@naver.com>
2024-10-27 18:16:03 +0900

    feat : 자동차 자신의 이름과 위치를 반환하는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,12 +3,16 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
-    private String name;
+    private final String name;
     private int position;
 
     public Car(String name) {
         this.name = name;
         this.position = 0;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getPosition() {
@@ -19,5 +23,9 @@ public class Car {
         if (Randoms.pickNumberInRange(0, 9) >= 4) {
             this.position++;
         }
+    }
+
+    public String displayStatus() {
+        return name + " : " + "-".repeat(position);
     }
 }
