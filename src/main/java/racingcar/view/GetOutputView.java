23406f37159ea14c8998aeeae1da41a283726ae5
Hun0914 <bkh010914@naver.com>
2024-10-27 16:01:34 +0900

    refactor : Output 기능 분리
    Output 기능만 하도록 분리하였습니다.

--- src/main/java/racingcar/view/GetOutputView.java
+++ src/main/java/racingcar/view/GetOutputView.java
@@ -1,4 +1,12 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class GetOutputView {
+
+    public void getOutput(List<String> winners) {
+        String result = String.join(", ", winners);
+        System.out.println("최종 우승자 : " + result);
+    }
+
 }
