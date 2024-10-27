d499540ae8e326496a158a4a69c1b9c5c9db153d
gyongcode <gyongcode@gmail.com>
2024-10-27 16:17:41 +0900

    입출력 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+public class OutputView {
+    public void printResult(){
+        System.out.println("실행 결과");
+    }
+
+    public void printString(String s){
+        System.out.println(s);
+    }
+
+    public void printFinalWinner(String s){
+        System.out.println("최종 우승자 : "+ s);
+    }
+}
