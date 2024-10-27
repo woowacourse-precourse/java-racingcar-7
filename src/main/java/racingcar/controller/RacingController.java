9b58fdad97c88a57858d50f656520b17e943bd27
aprilnineteen <dencoding@naver.com>
2024-10-28 01:23:28 +0900

    feat: 컨트롤러 생성 및 시작 메서드 추가

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,15 @@
+package racingcar.controller;
+
+import racingcar.exception.RaceCountsException;
+import racingcar.validation.CarNamesValidation;
+import racingcar.validation.RaceCountsValidation;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class RacingController {
+
+    public void run() {
+    }
+}
