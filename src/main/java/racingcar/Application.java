6433ecc7a9804fa367368cfc0cb2dacbe1794633
dongyun <ehddbs452100@naver.com>
2024-10-28 01:20:21 +0900

    style(Application): delete unused class
    - InputView와 OutputView 클래스를 Application에서 필요하지 않으니 import에서 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,8 +4,6 @@ import racingcar.controller.RacingController;
 import racingcar.service.MoveForwardService;
 import racingcar.service.RacingInputValidateService;
 import racingcar.service.WinnerService;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
