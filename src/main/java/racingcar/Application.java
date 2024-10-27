4f0f88129de46e3afa2d00e4f5c21eb46f0aa73c
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-10-27 18:34:49 +0900

    refactor: InputManager의 역할 감소위해  Names에서 검증하도록 수정
    이전에는 InputManager에서 검증된 NamesList를 반환했다면,
        InputManager에서 오직 입력값만 받아오도록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,13 @@
 package racingcar;
 
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
 
         try {
-            List<Name> names = InputManager.getInput();
-            int rounds = InputManager.getGameRound();
+            String name = InputManager.readInput();
+            int rounds = InputManager.readGameRound();
 
+            Names names = new Names(name);
             Cars cars = new Cars(names);
 
             Game.start(cars, rounds);
