d5071bb2f5a84c0e8529facb815b6dbdce7851ab
JiEung2 <181757@jnu.ac.kr>
2024-10-27 21:53:15 +0900

    feat(inputValidator): 자동차 이름 중복 검증 메서드 추가

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -1,11 +1,14 @@
 package racingcar;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class InputValidator {
     private final int CAR_NAME_LENGTH_LIMIT = 5;
     private final String CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 5자 이하만 가능합니다.";
     private final String CAR_NAME_EMPTY_ERROR_MESSAGE = "자동차 이름은 공백으로 구성될 수 없습니다.";
+    private static final String CAR_NAME_DUPLICATE_ERROR_MESSAGE = "자동차 이름은 중복될 수 없습니다.";
 
     public void validateCarNames(List<String> carNames) {
 
@@ -20,6 +23,13 @@ public class InputValidator {
     private void validateCarNameNotEmpty(String carName) {
         if (carName.trim().isEmpty()) {
             throw new IllegalArgumentException(CAR_NAME_EMPTY_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateDuplicateNames(List<String> carNames) {
+        Set<String> uniqueNames = new HashSet<>(carNames);
+        if (uniqueNames.size() != carNames.size()) {
+            throw new IllegalArgumentException(CAR_NAME_DUPLICATE_ERROR_MESSAGE);
         }
     }
 }
