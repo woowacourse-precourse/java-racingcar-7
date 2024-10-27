b2eb743d7cc02500806f381ce572148e1d5837be
JEONGHUNJEON <jjhoon06@gmail.com>
2024-10-28 04:40:23 +0900

    chore: Car, RequestRaceDto commit 추가

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,22 @@
+package racingcar.domain;
+
+public class Car {
+
+    private final String carName;
+    private int location;
+
+    public Car(String carName) {
+        this.carName = carName;
+        this.location = 0;
+    }
+
+    public String getCarName () {
+        return carName;
+    }
+    public int getLocation () {
+        return location;
+    }
+    public void increaseLocation () {
+        this.location++;
+    }
+}
---
+++ src/main/java/racingcar/domain/RequestRaceDto.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RequestRaceDto {
+
+    private final List<Car> carList;
+    private final int tryNumber;
+
+    public RequestRaceDto (List<Car> carList, int tryNumber) {
+        this.carList = carList;
+        this.tryNumber = tryNumber;
+    }
+
+    public List<Car> getCarList() {
+        return carList;
+    }
+
+    public int getTryNumber() {
+        return tryNumber;
+    }
+}
