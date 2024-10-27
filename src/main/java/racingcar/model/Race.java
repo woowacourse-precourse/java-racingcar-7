b2931ec54270dc75abb2e12b015981edd20244e4
kimdev <kim__dev@hufs.ac.kr>
2024-10-27 16:26:24 +0900

    feat(Race): 경주에 자동차 등록 기능을 추가하라

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -1,9 +1,16 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+
 public class Race {
     private RaceTurn turn;
+    private ArrayList<Car> carList;
 
     public Race(int turn) {
         this.turn = new RaceTurn(turn);
+    }
+
+    public void join(Car car) {
+        this.carList.add(car);
     }
 }
