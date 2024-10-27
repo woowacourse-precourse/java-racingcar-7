e6b938ca291bf5f0eae17ebd0227941eb0304658
wvssm <neoramiregi@gmail.com>
2024-10-27 11:27:18 +0900

    feat(InputView): 사용자에게 입력을 받는다

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,8 @@
+package racingcar.view;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String readConsoleInput(){
+        return Console.readLine();
+    }
+}
