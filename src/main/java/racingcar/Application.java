987a45aafd11422286744085ee84ffb2fa61f496
minju <minju9711@naver.com>
2024-10-27 15:59:17 +0900

    feat: Application 클래스에 Controller 초기화 및 레이스 실행 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.config.ControllerConfig;
+import racingcar.controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ControllerConfig controllerConfig = ControllerConfig.getInstance();
+        Controller controller = controllerConfig.getController();
+
+        controller.ready();
+        controller.runRace();
+        controller.RaceResult();
     }
 }
