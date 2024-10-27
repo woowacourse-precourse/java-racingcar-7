74a9f6392df20e76b38a833e2a226f97690b97ec
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-27 15:53:27 +0900

    test(ApplicationTest): 통합 테스트 추가
    테스트한 내용은 다음과 같습니다:
    - 자동차 이름에 빈문자열이 존재하여 예외가 발생한다
    - 자동차 이름이 5글자를 초과하여 예외가 발생한다
    - 자동차 이름에 중복이 존재하여 예외가 발생한다
    - 시도 횟수가 숫자가 아니므로 예외가 발생한다
    - 시도 횟수가 10_000_000 초과하여 예외가 발생한다

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,9 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.constant.ErrorType;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -15,19 +17,79 @@ class ApplicationTest extends NsTest {
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("자동차 이름에 빈문자열이 존재하여 예외가 발생한다.")
+    void carNameFormatTest() throws Exception {
+        //given
+        //when
+        //then
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorType.INVALID_CAR_NAME)
+        );
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 5글자를 초과하여 예외가 발생한다.")
+    void carNameLengthTest() throws Exception {
+        //given
+        //when
+        //then
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobiiii,gyunnnn", "1"))
+                        .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorType.EXCEEDED_MAX_CAR_NAME_LENGTH)
+        );
+    }
+
+    @Test
+    @DisplayName("자동차 이름에 중복이 존재하여 예외가 발생한다.")
+    void carNameDuplicateTest() throws Exception {
+        //given
+        //when
+        //then
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorType.DUPLICATE_CAR_NAME)
+        );
+    }
+
+    @Test
+    @DisplayName("시도 횟수가 숫자가 아니므로 예외가 발생한다.")
+    void countFormatTest() throws Exception {
+        //given
+        //when
+        //then
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,gyun", "a"))
+                        .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorType.INVALID_COUNT)
+        );
+    }
+
+    @Test
+    @DisplayName("시도 횟수가 10_000_000 초과하여 예외가 발생한다.")
+    void countTest() throws Exception {
+        //given
+        //when
+        //then
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,gyun", "100_000_000"))
+                        .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorType.EXCEEDED_MAX_COUNT)
         );
     }
 
