f786ab2ad76ce5bcbeb3fb65300503a8d5ffc987
iamdonghyuk <iamdonghyuk@gmail.com>
2024-10-27 22:17:16 +0900

    [프리코스 2주차] 자동차 경주 - 클래스 Car 수정 - 클래스내의 변수 수정 - 클래스내의 메서드 이름 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,16 +2,13 @@ package racingcar;
 import camp.nextstep.edu.missionutils.*;
 
 public class Application {
-    public static car[] list;
+    public static Car[] list;
     public static String[] member = {};
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Application ap = new Application();
-        ap.start(ap);
-    }
-    
-    public void start(Application ap) {
-    	System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         try{
             member = Console.readLine().split(",");
         }catch (IllegalArgumentException e){
@@ -30,16 +27,11 @@ public class Application {
 
         System.out.println();
         System.out.println("실행 결과");
-
-
-
         for(int i = 0; i < try_count; i++){
-            ap.r_print(ap);
+            ap.r_print();
         }
-
-        ap.winner_print(ap.winner_score());
+        ap.result_print(ap.winner_score());
     }
-    
     public void mem_trim() {
         for(String str : member) {
             if(str.trim().isEmpty()) {
@@ -47,23 +39,22 @@ public class Application {
             }
         }
     }
-    
-    public void r_print(Application ap){
+    public void r_print(){
+        Application ap = new Application();
         ap.add();
         ap.print_result();
         System.out.println();
     }
-    
-    public void winner_print(int max){
+    public void result_print(int max){
         System.out.print("최종 우승자 : ");
         boolean check = true;
-        for(car i : list) {
-        	int len = i.car_count.length();
+        for(int i = 0; i < list.length; i++){
+            int len = list[i].racer_point.length();
             if(len == max && check){
-                System.out.print(i.getCar_name());
+                System.out.print(list[i].racer_name);
                 check = false;
             }else if(len == max) {
-                System.out.print(","+i.getCar_name());
+                System.out.print(","+list[i].racer_name);
             }
         }
     }
@@ -72,17 +63,17 @@ public class Application {
         for(int i = 0; i < list.length; i++){
             int random = Randoms.pickNumberInRange(0,9);
             if(random >= 4){
-                list[i] = new car(list[i].getCar_name(),true,list[i].getCar_count());
+                list[i] = new Car(list[i].racer_name,true,list[i].racer_point);
             }else{
-                list[i] = new car(list[i].getCar_name(),false,list[i].getCar_count());
+                list[i] = new Car(list[i].racer_name,false,list[i].racer_point);
             }
         }
     }
 
     public int winner_score(){
         int max = 0;
-        for(car strb : list){
-            int len = strb.car_count.length();
+        for(Car strb : list){
+            int len = strb.racer_point.length();
             if(max < len){
                 max = len;
             }
@@ -91,36 +82,34 @@ public class Application {
     }
 
     public void print_result(){
-    	for(car i : list) {
-    		System.out.println(i.getCar_name()+" : "+i.getCar_count());
-    	}
+        for(int i = 0; i < list.length; i++){
+            System.out.println(list[i].racer_name+" : "+list[i].racer_point);
+        }
     }
 
-    public car [] initial(String [] member){
-        car list[] = new car[member.length];
+    public Car [] initial(String [] member){
+        Car list[] = new Car[member.length];
         for(int i = 0; i < list.length; i++){
-            list[i] = new car(member[i],false, new StringBuffer(""));
+            list[i] = new Car(member[i],false, new StringBuffer(""));
         }
         return list;
     }
 }
 
-class car{
-    public String getCar_name() {
-		return car_name;
-	}
-	public StringBuffer getCar_count() {
-		return car_count;
-	}
-	String car_name;
-    StringBuffer car_count = new StringBuffer();
-    public car(String car_name, boolean go, StringBuffer prev_count){
-        this.car_name = car_name;
+class Car{
+    public String getRacer_name() {
+        return racer_name;
+    }
+    public StringBuffer getRacer_point() {
+        return racer_point;
+    }
+    String racer_name;
+    StringBuffer racer_point;
+    public Car(String racer_name, boolean go, StringBuffer prev_point){
+        this.racer_name = racer_name;
+        this.racer_point = new StringBuffer(prev_point);
         if(go){
-            car_count.append(prev_count).append("-");
-        }else{
-            car_count.append(prev_count);
+            racer_point.append("-");
         }
     }
-
 }
