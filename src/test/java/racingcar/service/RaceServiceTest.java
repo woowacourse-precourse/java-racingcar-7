4c4006020df0d1e7e981672c3e900746ecc9e7ce
ufo9430 <ufo9430@naver.com>
2024-10-27 01:05:15 -0400

    test: 이름의 개수만큼 자동차가 만들어지는 기능 테스트

--- src/test/java/racingcar/service/RaceServiceTest.java
+++ src/test/java/racingcar/service/RaceServiceTest.java
@@ -1,4 +1,27 @@
+package racingcar.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class RaceServiceTest {
-  
+    private static RaceService service;
+    @BeforeAll
+    static void init(){
+        service = new RaceService();
+    }
+    @Test
+    void 이름의_개수만큼_자동차가_만들어진다(){
+        String input = "abc,bcd,cde";
+        service.init(input);
+        List<Car> cars = service.getCars();
+        Assertions.assertThat(cars.size()).isEqualTo(3);
+    }
+
 }
