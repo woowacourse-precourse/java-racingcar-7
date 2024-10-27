ebcb7bbc7aa99aeeb07204a1a73bfcc9cddf942d
minitiated <minitiated@gmail.com>
2024-10-27 12:52:46 +0900

    refactor(MapBuilder): change varible name lineCounter
    changed variable name of mapCarNamesToStringForwardLines() from
    forwardCounter to lineCounter
    moved Map forwardCounter, lineCounter instant statement to above level

--- src/main/java/racingcar/MapBuilder.java
+++ src/main/java/racingcar/MapBuilder.java
@@ -6,22 +6,24 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class MapBuilder {
+
+    Map<String, Integer> forwardCounter = new TreeMap<>();
+    Map<String, String> lineCounter = new TreeMap<>();
+
     public void mapCarNamesToForwardCounts(List<String> carNames) {
-        Map<String, Integer> forwardCounter = new TreeMap<>();
         for (int i = 0; i < carNames.size(); i++) {
             forwardCounter.put(carNames.get(i), 0);
         }
     }
 
     public void mapCarNamesToStringForwardLines(List<String> carNames) {
-        Map<String, String> forwardCounter = new TreeMap<>();
         for (int i = 0; i < carNames.size(); i++) {
-            forwardCounter.put(carNames.get(i), "");
+            lineCounter.put(carNames.get(i), "");
         }
     }
 
     public void mapSingleCarForwardResult(String carNames) {
-//        이 부분은 어떻게 할지 아직 모르겠다.
+
     }
 
     public void mapSingleRandomRoundResult(int i) {
