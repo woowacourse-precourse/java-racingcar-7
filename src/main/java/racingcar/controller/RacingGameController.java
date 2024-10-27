6c1b40cccce9fb776909e89403c3bc59f35a5381
young-in <hardworkingini@gmail.com>
2024-10-27 23:36:31 +0900

    feat : racing car 생성을 위한 method 구현

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,5 +1,7 @@
 package racingcar.controller;
 
+import racingcar.domain.RacingCars;
+import racingcar.util.CreatingRacingCar;
 import racingcar.view.InformationView;
 import racingcar.view.InputView;
 
@@ -8,6 +10,12 @@ public class RacingGameController {
     private InputView inputView = new InputView();
 
     public void playGame() {
+        RacingCars racingCars = createRacingCars();
+    }
 
+    private RacingCars createRacingCars() {
+        informationView.printRequestCarNames();
+        String[] carNames = inputView.inputCarNames();
+        return CreatingRacingCar.createCars(carNames);
     }
 }
