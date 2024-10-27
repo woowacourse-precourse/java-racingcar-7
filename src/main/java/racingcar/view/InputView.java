4690a4e80114db686834511f9b27403179acb20e
young-in <hardworkingini@gmail.com>
2024-10-27 22:27:58 +0900

    feat : 경주용 자동차들의 이름 입력을 위한 안내사항 출력 view 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,8 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 import racingcar.util.SplittingRacingCarName;
 
 public class InputView {
+
     public String[] inputCarNames(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine();  // Scanner 대신 Console 사용
         return SplittingRacingCarName.splitRacingCarName(carNames);
     }
