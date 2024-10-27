afc7967bd2600b7c43b33e1126a1856a159faae0
jaepyo99 <jaepyo99@naver.com>
2024-10-27 15:33:22 +0900

    feat: Controller 구현 및 Controller 호출을 통한 프로그램 시작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.infrastructure.RaceController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RaceController controller = new RaceController(new InputView(), new OutputView());
+        controller.run();
     }
 }
---
+++ src/main/java/racingcar/controller/infrastructure/RaceController.java
@@ -0,0 +1,71 @@
+package racingcar.controller.infrastructure;
+
+import racingcar.controller.domain.GameController;
+import racingcar.model.domain.Race;
+import racingcar.model.domain.RaceFind;
+import racingcar.model.infrastructure.CarRace;
+import racingcar.util.Splitter;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+import racingcar.view.enumerate.OutputConstant;
+
+import java.util.List;
+
+public class RaceController implements GameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RaceController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    @Override
+    public void run() {
+        RaceFind race = createRacing();
+        playRace(race);
+        announce(race);
+    }
+
+    @Override
+    public List<String> process(String in) {
+        List<String> split = Splitter.split(in);
+        for (String s : split) {
+            validateNameLength(s);
+        }
+        return split;
+    }
+
+    private void announce(RaceFind race) {
+        List<String> winners = race.getWinners();
+        outputView.printWinners(winners);
+    }
+
+    private void playRace(Race race) {
+        race.runFullRace(outputView);
+    }
+
+    private int requestTryTimes() {
+        outputView.print(OutputConstant.PRINT_INPUT_TRY_TIMES_SENTENCE);
+        return inputView.inputRaceTryTimes();
+    }
+
+    private String requestCarNames() {
+        outputView.print(OutputConstant.PRINT_INPUT_CAR_NAMES_SENTENCE);
+        return inputView.inputCarName();
+    }
+
+    private RaceFind createRacing() {
+        String carNamesInput = requestCarNames();
+        int tryTimes = requestTryTimes();
+        List<String> carNames = process(carNamesInput);
+        return CarRace.init(carNames, tryTimes);
+    }
+
+
+    private void validateNameLength(String s) {
+        if (s.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
