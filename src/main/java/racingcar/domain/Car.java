804c7d1c08431caca33c0479a1c3f3ef654056f7
023-dev <0_2_3@naver.com>
2024-10-27 19:22:53 +0900

    feat(domain): Car 전진 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,11 +1,20 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
-    private final int position = 0;
+    private int position = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public void move() {
+        int random = Randoms.pickNumberInRange(0, 9);
+        if (random >= 4) {
+            this.position++;
+        }
     }
 
     public String getName() {
