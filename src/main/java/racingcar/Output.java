15eb25b9e0ebf4403d96a79649d58d67bc400a3a
YongLeeCode <yonghwan311@gmail.com>
2024-10-27 21:18:13 +0900

    indentation

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -3,13 +3,14 @@ package racingcar;
 import java.util.List;
 
 public class Output {
-  public static void showRound(RoundStatus roundStatus) {
-    System.out.println("라운드 " + (roundStatus.getRound()));
-    for (CarStatus carStatus : roundStatus.getCarStatusList()) {
-      System.out.println(carStatus.getName() + " : " + "-".repeat(carStatus.getPosition()));
+
+    public static void showRound(RoundStatus roundStatus) {
+        System.out.println("라운드 " + (roundStatus.getRound()));
+        for (CarStatus carStatus : roundStatus.getCarStatusList()) {
+            System.out.println(carStatus.getName() + " : " + "-".repeat(carStatus.getPosition()));
+        }
+        System.out.println();
     }
-    System.out.println();
-  }
 
     public static void showWinner(WinnerChoicer winnerChoicer) {
         List<String> winners = winnerChoicer.getWinners();
