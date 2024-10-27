c8e5b92984b2b6666010840e9f5a4f9d4734ef62
seungheejang <justinholla908@gmail.com>
2024-10-27 22:07:48 +0900

    feat: Add function to main() method of Application class
    The main() method starts the program :
    - Calls CarNameInput to get car names and create Car objects
    - Calls TrialNumInput to get the number of trials
    - Repeatedly runs the game using the Play class
    - Displays the final results using ResultPrinter

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,26 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 자동차 이름 입력 메소드 호출 후 반환값 저장
+        String[] carNames = CarNameInput.settingCarName();
+
+        // Car 객체 배열 생성
+        Car[] cars = new Car[carNames.length];
+
+        // 각 자동차 이름으로 Car 객체 생성
+        for (int i=0; i < carNames.length; i++ ) {
+            cars[i] = new Car(carNames[i]);
+        }
+
+        // 시도 횟수 입력 메소드 호출 후 반환값 저장
+        int trialNum = TrialNumInput.settingTrialNum();
+
+        // 시도 횟수 만큼 게임 메소드 반복 호출
+        for (int gameTrial=0; gameTrial < trialNum; gameTrial++) {
+            Play.play(cars);
+        }
+        
+        // 최종결과 출력 메소드 호출
+        ResultPrinter.printWinner(cars);
     }
 }
