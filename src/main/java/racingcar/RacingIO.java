461b5c5a93d5db6b790b52b1b4ac1025bb258dfb
OhYeHyun <ohyhohyl@naver.com>
2024-10-27 21:45:58 +0900

    feat(RacingIO): 실행 결과 부분 입출력 기능 구현

--- src/main/java/racingcar/RacingIO.java
+++ src/main/java/racingcar/RacingIO.java
@@ -2,9 +2,15 @@ package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class RacingIO {
     private static final String CAR_NAME_INPUT_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String MOVE_COUNT_INPUT_PROMPT = "시도할 횟수는 몇 회인가요?";
+    private static final String TURN_RESULT_PROMPT = "실행 결과";
+    private static final String WINNER_PROMPT = "최종 우승자 : ";
+    private static final String WINNER_DELIMITER = ", ";
 
     public static void promptCarNamesInput() {
         System.out.println(CAR_NAME_INPUT_PROMPT);
@@ -16,5 +22,24 @@ public class RacingIO {
 
     public static String getInput() {
         return readLine();
+    }
+
+    public static void promptTurnResult(ArrayList<HashMap<String, Integer>> turnResults) {
+        System.out.println();
+        System.out.println(TURN_RESULT_PROMPT);
+
+        for (HashMap<String, Integer> turnResult : turnResults) {
+            formatTurnResult(turnResult);
+            System.out.println();
+        }
+    }
+
+    private static void formatTurnResult(HashMap<String, Integer> turnResult) {
+        for (HashMap.Entry<String, Integer> entry : turnResult.entrySet()) {
+            String carName = entry.getKey();
+            int carPosition = entry.getValue();
+
+            System.out.println(carName + " : " + "-".repeat(carPosition));
+        }
     }
 }
