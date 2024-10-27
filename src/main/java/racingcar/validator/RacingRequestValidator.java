fa840afc228e3b2daaa6632e33abe152702dff81
haebyun <bhk4392@gmail.com>
2024-10-27 19:47:08 +0900

    refactor(RacingRequestValidator): String.format을 통해 오류 메시지 모두 상수로 관리하도록 수정

--- src/main/java/racingcar/validator/RacingRequestValidator.java
+++ src/main/java/racingcar/validator/RacingRequestValidator.java
@@ -7,7 +7,7 @@ public class RacingRequestValidator {
     private final static String BLANK_CAR_NAME_MESSAGE = "자동차 이름이 비어있습니다.";
     private final static String CAR_NAME_TOO_LONG_MESSAGE = "자동차 이름 %d자 초과 되었습니다.";
     private final static Integer MAX_CAR_NAME_LENGTH = 5;
-    private final static String INVALID_MOVE_COUNT_MESSAGE = "이동 횟수는 1 이상이어야 합니다.";
+    private final static String INVALID_MOVE_COUNT_MESSAGE = "이동 횟수는 %d 이상이어야 합니다.";
     private final static Integer MIN_MOVE_COUNT = 1;
 
     public static void validate(RacingRequestDTO racingRequestDTO) {
@@ -28,7 +28,9 @@ public class RacingRequestValidator {
 
     private static void validateMoveCount(Integer moveCount) {
         if (moveCount < MIN_MOVE_COUNT) {
-            throw new IllegalArgumentException(INVALID_MOVE_COUNT_MESSAGE);
+            throw new IllegalArgumentException(
+                    String.format(INVALID_MOVE_COUNT_MESSAGE, MIN_MOVE_COUNT)
+            );
         }
     }
 }
