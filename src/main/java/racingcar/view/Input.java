38e2ac9097fd2b407ec167a5423b6a3827c3979c
Yeongjae Kim <sees77770@gmail.com>
2024-10-28 04:00:09 +0900

    feat(Input): 시도할 횟수를 입력

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -5,9 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
 
     public static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String LAP_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
     public String readCarNames() {
         System.out.println(CAR_NAMES_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String readLap() {
+        System.out.println(LAP_INPUT_MESSAGE);
         return Console.readLine();
     }
 }
