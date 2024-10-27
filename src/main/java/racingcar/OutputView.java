664dd1eb7d6d295bba9052e1748eac7c139658a0
ye-one222 <101704967+ye-one222@users.noreply.github.com>
2024-10-27 20:18:01 +0900

    feat(OutputView): Create OutputView class

---
+++ src/main/java/racingcar/OutputView.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+public class OutputView {
+    public void notifyEnterNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void notifyEnterTryingNumber(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+    public void notifyPrintResult(){
+        System.out.println("실행 결과");
+    }
+}
