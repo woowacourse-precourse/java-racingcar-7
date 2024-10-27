044efc0bc885a581ee00e608efbb9378585e191c
babyfox225 <babyfox225@naver.com>
2024-10-27 11:12:29 +0900

    feat: reset project

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,5 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        ConsoleCarRacingStadium consoleStadium = new ConsoleCarRacingStadium();
-        consoleStadium.startRace();
     }
 }
