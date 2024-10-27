f5e66d81b4de70bc3a79159bc0f6cef3b2508408
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-10-27 16:13:09 +0900

    feat: 전체 프로그램 동작 구현

--- src/main/java/racingcar/util/RandomGeneratorImpl.java
+++ src/main/java/racingcar/util/RandomGeneratorImpl.java
@@ -2,7 +2,7 @@ package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomGeneratorImpI implements RandomGenerator{
+public class RandomGeneratorImpl implements RandomGenerator{
     @Override
     public int pickNumber(int bound) {
         return Randoms.pickNumberInRange(0,bound);
