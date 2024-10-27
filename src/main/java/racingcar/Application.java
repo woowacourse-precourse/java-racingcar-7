d1998ec16bcb710cf1c42414d69a797bcf85ff3e
babyfox225 <babyfox225@naver.com>
2024-10-27 17:01:20 +0900

    refactor: 자동차 이름 입력 메서드 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,10 +10,7 @@ import java.util.*;
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
-        String[] names = input.split(",");
-
-        // -------------
+        List<String> names = inputNames();
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String count = Console.readLine();
@@ -51,5 +48,10 @@ public class Application {
                 .toList();
 
         System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+
+    private static List<String> inputNames() {
+        String input = Console.readLine();
+        return List.of(input.split(","));
     }
 }
