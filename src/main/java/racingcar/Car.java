5b1db65da853c2194fc1501cda6c3feb42d4cab7
yjinlee99 <yjinlee99@gmail.com>
2024-10-27 23:22:27 +0900

    feat: 게임 실행 - 매 시도마다 랜덤 값 생성 및 전진 여부 결정

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Car {
+    String name;
+    int count;
+
+    public Car(String name) {
+        this.name = name;
+        this.count = 0;
+    }
+}
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,10 +1,35 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame {
     private InputHandler inputHandler = new InputHandler();
 
     public void run() {
         String[] cars = inputHandler.getCarNames();
         int tryCount = inputHandler.getTryCount();
+
+        List<Car> carList = new ArrayList<>();
+
+        for(String car : cars) {
+            carList.add(new Car(car));
+        }
+
+        System.out.println();
+        System.out.println("실행 결과");
+
+        for(int i=0; i<tryCount; i++) {
+            for (Car car : carList) {
+                if(Randoms.pickNumberInRange(0, 9) >= 4) {
+                    car.count ++;
+                }
+                System.out.print(car.name + " : ");
+                for(int j=0;j<car.count;j++){
+                    System.out.print("-");
+                }
+            }
+        }
     }
 }
