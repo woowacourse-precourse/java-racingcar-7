29c7546323beb1a066e3c54158acfd9d48a38ecc
Chan Mi <rkdcksal02@gmail.com>
2024-10-28 08:51:34 +0900

    test: 자동차 이름이 5자를 초과할 경우 예외가 발생하는지 테스트

---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,26 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTest {
+
+    @BeforeEach
+    void setUp() {
+        Car.resetNames();
+    }
+
+    @Test
+    void 자동차_이름이_5자초과일_경우_예외발생() {
+        assertThatThrownBy(() -> new Car("CarTest"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5자 이하만 가능합니다.");
+    }
+
+    @Test
+    void move() {
+    }
+}
