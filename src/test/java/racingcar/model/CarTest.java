90ae339649e11beb1198bc874390a291902d41f4
xeimun <hinote444@naver.com>
2024-10-28 06:20:46 +0900

    test(CarTest): 전진 조건을 검사하는 테스트 구현

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -17,6 +18,30 @@ public class CarTest {
     void 이름이_5자_이하이면_예외가_발생하지_않는다() {
         assertThatCode(() -> Car.from("배고픈포비"))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 숫자가_4_이상이면_전진한다() {
+        // given
+        Car car = Car.from("pobi");
+
+        // when
+        car.advance(4);
+
+        // then
+        assertThat(car.getAdvanceMarkerCount()).isEqualTo(1);
+    }
+
+    @Test
+    void 숫자가_3_이하이면_움직이지_않는다() {
+        // given
+        Car car = Car.from("pobi");
+
+        // when
+        car.advance(3);
+
+        // then
+        assertThat(car.getAdvanceMarkerCount()).isEqualTo(0);
     }
 
 }
