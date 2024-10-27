80f1cfdba474c28e06d43276e5e79c5cd48287ef
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-10-27 20:36:36 +0900

    refactor: 메소드명 리팩토링에 따른 수정

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -4,11 +4,12 @@ import racingcar.model.Racing;
 import racingcar.view.OutputView;
 
 import static racingcar.view.InputView.inputAttempts;
-import static racingcar.view.InputView.inputCarNames;
+import static racingcar.view.InputView.inputCars;
+
 
 public class RacingController {
     public static void run() {
-        String[] cars = inputCarNames();
+        String[] cars = inputCars();
         int attempts = inputAttempts();
 
         Racing racing = new Racing(cars, attempts);
