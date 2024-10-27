0604cfe22801c81daba10290c376bf494fbc16ed
euni <kongeuni1004@gmail.com>
2024-10-27 23:17:27 +0900

    chore: 임시 리턴값 추가하여 컴파일 에러 해결
    - 메서드의 컴파일 에러 방지를 위해 임의의 리턴값 설정
    - 향후 기능 구현 시 실제 리턴값으로 수정 예정
    - 코드 구조 작업 중간 상태를 저장

--- src/main/java/racingcar/InputParser.java
+++ src/main/java/racingcar/InputParser.java
@@ -5,10 +5,10 @@ import java.util.List;
 public class InputParser {
 
     public List<Car> parseCarName(String carName){
-
+        return null;
     }
 
     public int parseAttemptNumber(String attemptNumber){
-
+        return 0;
     }
 }
