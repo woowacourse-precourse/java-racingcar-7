62786a9b9ec7c697022b9d57751e4fcdbf617250
KangGeonyoung <kky6335@gmail.com>
2024-10-27 20:43:13 +0900

    feat(Car): 경주 게임 1번 실행 Car - startOneGame: 경주 게임을 1번 실행하는 메서드

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -12,13 +12,27 @@ public class Car {
         moveCount = 0;
     }
 
+    /**
+     * 무작위 숫자가 4이상이라면 전진
+     */
     public void forwardCar(int randomNumber) {
         if (randomNumber >= 4) {
             moveCount += 1;
         }
     }
 
+    /**
+     * 0-9 사이의 무작위 숫자 생성
+     */
     public int generateRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
+    }
+
+    /**
+     * 경주 게임 1번 실행
+     */
+    public void startOneGame() {
+        int randomNumber = generateRandomNumber();
+        forwardCar(randomNumber);
     }
 }
