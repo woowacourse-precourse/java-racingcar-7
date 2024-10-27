9f1e5d9e0c716d70295f92e986aca50790b683ec
kwon <rnjsdndud00@naver.com>
2024-10-27 12:22:01 +0900

    feat(IO): Racer가 가지는 Utils 메소드 분리

--- src/main/java/racingcar/racer/car/CarUtils.java
+++ src/main/java/racingcar/racer/car/CarUtils.java
@@ -1,21 +1,19 @@
-package racingcar.car;
+package racingcar.racer.car;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.racer.RacerUtils;
 
 public class CarUtils implements RacerUtils {
-    @Override
     public boolean isMovable(int randomNum) {
-        if (0 <= randomNum && randomNum <= 4) {
+        if (0 <= randomNum && randomNum <= 3) {
             return false;
-        } else if (5 <= randomNum && randomNum <= 9) {
+        } else if (4 <= randomNum && randomNum <= 9) {
             return true;
         } else {
             throw new RuntimeException("랜덤값이 0 ~ 9 범위를 벗어났습니다.");
         }
     }
 
-    @Override
     public int pickNumber(int minNum, int maxNum) {
         return Randoms.pickNumberInRange(minNum, maxNum);
     }
