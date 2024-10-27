2e0e86c7cfbfb74aa52e6d268c3b21de427f6ddb
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2024-10-27 21:32:50 +0900

    refactor(util): random package를 추가한다
    random package를 추가하여 package 간 책임을 분리한다.

--- src/main/java/racingcar/util/random/RaceRandomGenerator.java
+++ src/main/java/racingcar/util/random/RaceRandomGenerator.java
@@ -1,4 +1,4 @@
-package racingcar.util;
+package racingcar.util.random;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
--- src/main/java/racingcar/util/random/RaceRandomGeneratorImpl.java
+++ src/main/java/racingcar/util/random/RaceRandomGeneratorImpl.java
@@ -1,4 +1,4 @@
-package racingcar.util;
+package racingcar.util.random;
 
 public class RaceRandomGeneratorImpl implements RaceRandomGenerator {
 
