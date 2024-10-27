ef378de716469c397299630f4063712d2be13946
junhyunpark <junhyunpark305@gmail.com>
2024-10-27 19:43:06 +0900

    feat: create RacingCarService, RandomService
    - 랜덤값 생성 메서드 호출
    - 반복문으로 결과 출력
    - getRaceWinner 메서드 호출

---
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -0,0 +1,48 @@
+package racingcar.service;
+
+import racingcar.validator.RandomValidator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RacingCarService {
+
+  RandomValidator randomValidator = new RandomValidator();
+
+  public List<String> startRace(List<String> carList, int parsedAtmp){
+    System.out.println("실행결과\n");
+    // 랜덤 값 생성
+    String[] carRaceResults = new String[carList.size()];
+    Arrays.fill(carRaceResults, "");
+
+    for (int i = 0; i < parsedAtmp; i++) {
+      for (int j = 0 ; j < carList.size(); j++) {
+        if (randomValidator.checkRandomThreshold()) {
+          carRaceResults[j] += "-";
+        }
+        else {
+          carRaceResults[j] += "";
+        }
+        System.out.println(carList.get(j) + " : " + carRaceResults[j]);
+      }
+      System.out.println("\n");
+    }
+    List<String> gameResult = Arrays.asList(carRaceResults);
+    return getRaceWinner(carList, gameResult);
+  }
+
+  public List<String> getRaceWinner(List<String> carList, List<String> gameResult){
+    int[] carRaceResults = new int[gameResult.size()];
+    for (int i = 0; i < gameResult.size(); i++) {
+      carRaceResults[i] = gameResult.get(i).length();
+    }
+
+    String[] winners = new String[carList.size()];
+    for (int i = 0; i < carRaceResults.length; i++) {
+      if (carRaceResults[i] == Arrays.stream(carRaceResults).max().getAsInt()) {
+        winners = new String[]{carList.get(i)};
+      }
+    }
+    return Arrays.asList(winners);
+  }
+}
