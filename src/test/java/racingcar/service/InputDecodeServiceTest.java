c3a7b0a89a14dba0834a885c4504ce195ac5cbe8
KJungW <sagakkb0707@naver.com>
2024-10-27 20:17:07 +0900

    test(raname): 파라미터 테스트의 소스 메서드명 통일
    변경 내용 (what)
    - 파라미터 테스트의 소스 메서드명은 해당 테스트명과
      동일하도록 변경
    - InputDecodeService.makeRawCarNamesAndCars()
      소스 메서드명을 테스트명으로 변경
    
    변경 이유 (why)
    - 파라미터 테스트와 소스 메서드를 하나의 세트로 묶어
      가독성을 높이기 위해 테스트명과 소스 메서드명을 통일

--- src/test/java/racingcar/service/InputDecodeServiceTest.java
+++ src/test/java/racingcar/service/InputDecodeServiceTest.java
@@ -17,7 +17,7 @@ class InputDecodeServiceTest {
 
     @DisplayName("자동차 이름 입력 기능 - 쉼표(,)를 기준으로 n 대의 자동차 이름입력")
     @ParameterizedTest(name = "\"{0}\"를 입력했을 경우")
-    @MethodSource("makeRawCarNamesAndCars")
+    @MethodSource()
     void n대의_자동차_이름_입력(String rawCarNames, List<Car> expectedCars) {
         List<Car> actualCars = inputDecodeService.decodeRawCarNames(rawCarNames);
         assertThat(actualCars)
@@ -25,16 +25,7 @@ class InputDecodeServiceTest {
                 .containsExactlyElementsOf(expectedCars);
     }
 
-    @DisplayName("전진 시도 횟수 입력 기능 - 올바른 전진 시도 횟수 입력")
-    @ParameterizedTest(name = "\"{0}\"를 입력했을 경우")
-    @CsvSource({"5,5", "10,10", "15,15"})
-    void 올바른_전진_시도_횟수_입력(String rawRoundCount, int expectedRoundCount) {
-        int actualRoundCount = inputDecodeService.decodeRawRoundCount(rawRoundCount);
-        assertThat(actualRoundCount).isEqualTo(expectedRoundCount);
-    }
-
-
-    private static Stream<Arguments> makeRawCarNamesAndCars() {
+    private static Stream<Arguments> n대의_자동차_이름_입력() {
         return Stream.of(
                 Arguments.of("car1,car2",
                         List.of(new Car("car1"), new Car("car2"))),
@@ -43,5 +34,13 @@ class InputDecodeServiceTest {
                 Arguments.of("rc1,rc2,rc3,rc4",
                         List.of(new Car("rc1"), new Car("rc2"), new Car("rc3"), new Car("rc4")))
         );
+    }
+
+    @DisplayName("전진 시도 횟수 입력 기능 - 올바른 전진 시도 횟수 입력")
+    @ParameterizedTest(name = "\"{0}\"를 입력했을 경우")
+    @CsvSource({"5,5", "10,10", "15,15"})
+    void 올바른_전진_시도_횟수_입력(String rawRoundCount, int expectedRoundCount) {
+        int actualRoundCount = inputDecodeService.decodeRawRoundCount(rawRoundCount);
+        assertThat(actualRoundCount).isEqualTo(expectedRoundCount);
     }
 }
