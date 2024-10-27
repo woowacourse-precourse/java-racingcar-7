b106f921aeef0048afb682dfed934bd399e0c1e0
xeimun <hinote444@naver.com>
2024-10-27 15:52:23 +0900

    feat(InputView): 입력값이 비어있거나 빈 공백인지 검사하는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,20 +6,29 @@ public class InputView {
 
     public String readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+        String carNames = Console.readLine();
+        validateNotBlank(carNames);
+        return carNames;
     }
 
     public String readTotalRounds() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String roundNumber = Console.readLine();
+        validateNotBlank(roundNumber);
         validatePositiveRoundNumber(roundNumber);
         return roundNumber;
     }
 
-    public void validatePositiveRoundNumber(String number) {
-        int roundNumber = Integer.parseInt(number);
+    public void validatePositiveRoundNumber(String input) {
+        int roundNumber = Integer.parseInt(input);
         if (roundNumber < 1) {
             throw new IllegalArgumentException("총 라운드 수는 1 이상이어야 합니다.");
+        }
+    }
+
+    public void validateNotBlank(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("입력값이 빈 공백이거나 비어있으면 안 됩니다.");
         }
     }
 }
