8a1436be6dc674eb7e08c330e6008a34b32e91f7
Hojoomoney <kimhojoo51@gmail.com>
2024-10-27 22:10:00 +0900

    refactor : Source(입력) 클래스 생성 및 메인 리팩토링

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,13 +9,13 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] names = Console.readLine().split(",");
+        String[] names = Source.inputCarNames(Console.readLine());
         List<Car> cars = Arrays.stream(names)
                 .map(Car::new)
                 .toList();
 
         System.out.println("시도할 횟수는 몇 회인가요?");
-        int rounds = Integer.parseInt(Console.readLine());
+        int rounds = Source.inputAttempts(Console.readLine());
 
         Race race = new Race(cars);
         race.run(rounds);
