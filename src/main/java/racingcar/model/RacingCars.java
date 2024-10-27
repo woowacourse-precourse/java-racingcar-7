b7dbbd114721cf87949e90b92e1c9969d120c3a8
이희수 <hslee9234@naver.com>
2024-10-28 01:45:23 +0900

    feat: RacingCars 예외처리 추가
    빈 입력에 대한 RacingCars 객체 예외처리

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -1,14 +1,16 @@
 package racingcar.model;
 
 import java.util.List;
+import racingcar.enums.ExceptionMessages;
+import racingcar.exception.CarNameException;
 import racingcar.util.MoveStrategy;
-import racingcar.util.RandomMoveStrategy;
 
 public class RacingCars implements Cars {
     private final MoveStrategy moveStrategy;
     private final List<Car> cars;
 
     public RacingCars(List<Car> cars, MoveStrategy moveStrategy) {
+        validateEmpty(cars);
         this.cars = cars;
         this.moveStrategy = moveStrategy;
     }
@@ -25,5 +27,11 @@ public class RacingCars implements Cars {
     @Override
     public List<Car> getCars() {
         return cars;
+    }
+
+    private void validateEmpty(List<Car> cars) {
+        if (cars == null || cars.isEmpty()) {
+            throw new CarNameException(ExceptionMessages.EMPTY_INPUT.getMessage());
+        }
     }
 }
