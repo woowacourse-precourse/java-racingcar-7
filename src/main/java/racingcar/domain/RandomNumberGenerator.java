93dd1f9f22ed8f0d4a02110a7764821d23d9bca3
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-10-27 19:23:52 +0900

    feat : 랜덤 값 생성 기능 구현

---
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -0,0 +1,21 @@
+package racingcar.domain;
+
+public class RandomNumberGenerator implements NumberGenerator{
+
+    private static final int RANDOM_MIN = 0;
+    private static final int RANDOM_MAX = 9;
+
+    private static RandomNumberGenerator randomGenerator;
+
+    public static RandomNumberGenerator RandomNumberGenerator() {
+        if (randomGenerator == null) {
+            randomGenerator = new RandomNumberGenerator();
+        }
+        return randomGenerator;
+    }
+
+    @Override
+    public int generateNumber() {
+        return camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(RANDOM_MIN, RANDOM_MAX);
+    }
+}
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -9,8 +9,9 @@ public class CarTest {
     @DisplayName("자동차가 움직일 수 있는 조건을 만족할 때 움직이는지 확인한다")
     void 움직임_테스트() {
         Car car = new Car("pobi");
+        NumberGenerator numberGenerator = () -> 4;
 
-        car.move(4);
+        car.move(numberGenerator.generateNumber());
 
         Assertions.assertThat(car.getLocation()).isEqualTo(2);
     }
