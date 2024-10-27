b8db8c242cdcc77606ffcf80661c25abb8d4fa05
heehunjung <slrkqk2@naver.com>
2024-10-27 19:37:37 +0900

    feat(RacingGame): RacingGame 클래스 생성, game 메서드 추가
    Car객체 리스트를 순회하며 무작위 숫자를 Car에 전달하며 move 메서드 실행

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,2 +1,24 @@
-package racingcar.domain;public class Game {
+package racingcar.domain;
+
+import java.util.List;
+import racingcar.utils.RandomNoGenarator;
+
+public class RacingGame {
+
+    private Cars cars;
+
+    public RacingGame(Cars cars) {
+        this.cars = cars;
+    }
+
+    public void game() {
+
+        List<Car> cars = this.cars.getCars();
+
+        for (Car car : cars) {
+            int randNo = RandomNoGenarator.getRandomNo();
+            car.move(randNo);
+        }
+
+    }
 }
