28ed066f7ab8149d102db4b36f1e2de3ca0193ee
Parkjyun <jaeyun0507@naver.com>
2024-10-27 19:23:20 +0900

    feat(Name): 이름이 같다면 같은 이름으로 취급한다

--- src/main/java/racingcar/model/Name.java
+++ src/main/java/racingcar/model/Name.java
@@ -1,9 +1,28 @@
 package racingcar.model;
 
-public class Name {
-    private final String name;
+import java.util.Objects;
 
-    public Name(final String name) {
-        this.name = name;
+public class Name {
+    private final String value;
+
+    public Name(final String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof Name)) {
+            return false;
+        }
+        Name name = (Name) object;
+        return Objects.equals(this.value, name.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
