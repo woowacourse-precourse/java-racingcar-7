74ccfd0989d2fad4e7b09b19b8edfb5a7d523ca9
jjjjjoon <mjj7343@gmail.com>
2024-10-27 13:17:38 +0900

    refactor(CarNameValidator): split 이후 유효성 검증하는 메소드 분리

--- src/main/java/util/CarNameValidator.java
+++ src/main/java/util/CarNameValidator.java
@@ -1,13 +1,9 @@
 package util;
 
-import java.util.List;
-import java.util.Arrays;
-
 public class CarNameValidator {
     public enum ErrorMessage {
         EMPTY_OR_NULL_NAME("자동차 이름에 공백이나 null 값이 포함될 수 없습니다."),
-        INVALID_DELIMITER("자동차 이름은 쉼표로 구분되어야 합니다."),
-        INVALID_NAME_LENGTH("자동차 이름은 5자를 초과할 수 없습니다.");
+        INVALID_DELIMITER("자동차 이름은 쉼표로 구분되어야 합니다.");
 
         private final String message;
 
@@ -25,22 +21,12 @@ public class CarNameValidator {
             throw new IllegalArgumentException(ErrorMessage.EMPTY_OR_NULL_NAME.getMessage());
         }
 
-        List<String> carNames = Arrays.asList(input.split(","));
-        validateDelimiter(carNames, input);
-        validateNameLength(carNames);
+        validateDelimiter(input);
     }
 
-    public void validateDelimiter(List<String> carNames, String input) {
-        if (carNames.size() == 1 || !input.contains(",")) {
+    private void validateDelimiter(String input) {
+        if (!input.contains(",")) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_DELIMITER.getMessage());
-        }
-    }
-
-    public void validateNameLength(List<String> carNames) {
-        for (String carName : carNames) {
-            if (carName.trim().length() > 5) {
-                throw new IllegalArgumentException(ErrorMessage.INVALID_NAME_LENGTH.getMessage());
-            }
         }
     }
 }
