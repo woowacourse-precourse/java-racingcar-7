8b5cc0d646c191e812135a86232d8fa1c5827645
전석균 <jsg998877@gmail.com>
2024-10-28 00:04:16 +0900

    feat($Utils.java): removeWhitespace(String name) 구현
    - String Class의 replaceAll() 메소드를 이용해 name안에 있는 반복되는 공백을 모두 제거하는 메소드 구현

--- src/main/java/racingcar/utils/Utils.java
+++ src/main/java/racingcar/utils/Utils.java
@@ -1,9 +1,14 @@
 package racingcar.utils;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Utils {
+    public static String removeWhitespace(String name) {
+        return name.replaceAll("\\s+", ""); // "정규표현식" \\s: 모든 공백 문자, +: 앞의 패턴 반복됨
+    }
+
     public static List<String> splitNames(String input) {
         return Arrays.stream(input.split(","))
                 .map(Utils::removeWhitespace)
