63ddb73372c0949c45e0e7626c3bcf1871028122
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 21:37:42 +0900

    feat(RaceInfoDTO): 자동차 이름과 시행 횟수에 관한 검증 로직 호출부 추가
    - 생성자에 의해 RaceInfoDTO 객체가 생성될 때, 예외 검증 로직들을 호출함
    - InputView 클래스와는 다르게 내용과 관련된 예외들을 검증
    - 이름이 5글자 초과인 경우, 이름이 중복인 경우, 시행 횟수가 음수인 경우, 시행 횟수가 0인 경우를 검증
    - 해당 구조로 불변성을 보장받을 수 있음

--- src/main/java/racingcar/DTO/RaceInfoDTO.java
+++ src/main/java/racingcar/DTO/RaceInfoDTO.java
@@ -1,13 +1,27 @@
 package racingcar.DTO;
 
 import java.util.List;
+import racingcar.Util.Exception.ErrorMessage;
+import racingcar.Validation.NameValidation;
+import racingcar.Validation.NumberOfTrialValidation;
 
 public class RaceInfoDTO {
     private List<String> carNames;
     private int trialCount;
 
     public RaceInfoDTO(List<String> carNames, int trialCount) {
-        // TODO - 검증부 구현
+        if (NameValidation.isLongName(carNames)) {
+            throw new IllegalArgumentException(ErrorMessage.NAME_TOO_LONG.getMessage());
+        }
+        if (NameValidation.hasDuplicate(carNames)) {
+            throw new IllegalArgumentException(ErrorMessage.NAME_DUPLICATE.getMessage());
+        }
+        if (NumberOfTrialValidation.isNegative(trialCount)) {
+            throw new IllegalArgumentException(ErrorMessage.TRIAL_NEGATIVE.getMessage());
+        }
+        if (NumberOfTrialValidation.isZero(trialCount)) {
+            throw new IllegalArgumentException(ErrorMessage.TRIAL_ZERO.getMessage());
+        }
         this.carNames = carNames;
         this.trialCount = trialCount;
     }
