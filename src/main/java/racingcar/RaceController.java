3fdd40460ad35bde7423817f53ec5365091459e8
seoys0910 <seoys0910@naver.com>
2024-10-27 18:47:08 +0900

    feat : 자동차 경주를 진행하는 전체적인 로직 구현

--- src/main/java/racingcar/RaceController.java
+++ src/main/java/racingcar/RaceController.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import java.util.List;
+
 public class RaceController {
     public void run() {
+        String carNames = InputView.inputCarNames();
+        Cars cars = new Cars();
+        cars.createCars(carNames);
+        int tryCount = InputView.inputTryCount();
 
+        System.out.println();
+        System.out.println("실행 결과");
+
+        for (int n = 0; n < tryCount; n++) {
+            cars.moveAllCars();
+            OutputView.printCarStatus(cars);
+        }
+
+        int maxPosition = cars.findMaxPosition();
+        List<Car> winner = cars.findWinner(maxPosition);
+        OutputView.printWinner(winner);
     }
 }
