22a082214f035916ed5fc589b7a312c7195f946b
seophohoho <seop0937@gmail.com>
2024-10-27 21:48:01 +0900

    feat(Application): 입력 값 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,5 +4,9 @@ public class Application {
     public static void main(String[] args) {
         Race race = new Race();
 
+        String inputCars = Util.getInputValue();
+        String inputCount = Util.getInputValue();
+
+        race.run(inputCars, inputCount);
     }
 }
