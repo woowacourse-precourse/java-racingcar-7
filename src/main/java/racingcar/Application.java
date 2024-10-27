e6261a96df04adb0d98d7a4a5fafb5c7b1a7b5d3
mohyerolo <garam2101@naver.com>
2024-10-27 16:15:01 +0900

    style: 필요없는 주석 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,9 @@
 package racingcar;
 
 import racingcar.controller.RacingGame;
-import racingcar.parser.InputParserFactory;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-//        RacingGame racingGame = new RacingGame(new InputView(), InputParserFactory.create(), new OutputView());
         AppConfig appConfig = new AppConfig();
         RacingGame racingGame = appConfig.racingGame();
         racingGame.startRace();
