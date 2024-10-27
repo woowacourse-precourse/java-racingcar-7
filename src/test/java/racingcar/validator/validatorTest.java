5e0d78e4f26e16b18ba09acef5e5981c03fe40aa
yunsseong <aster20@hanyang.ac.kr>
2024-10-27 22:16:11 +0900

    test: 이름 검증 예외 테스트 추가

--- src/test/java/racingcar/validator/validatorTest.java
+++ src/test/java/racingcar/validator/validatorTest.java
@@ -18,4 +18,13 @@ class validatorTest {
         // then
         Assertions.assertThat(result).isEqualTo(true);
     }
+
+    @Test
+    void 이름_검증_예외_테스트() {
+        String names = "a,b,c,defghi";
+
+        Assertions.assertThatThrownBy(() -> {
+            validator.validateNames(names);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 }
