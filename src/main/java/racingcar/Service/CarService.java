931295604d3efdb90c9b6dba0c986792ed30f2a5
Seonjeongg <sj030205@naver.com>
2024-10-27 09:57:57 +0900

    feat: 자동차 경주 기능

--- src/main/java/racingcar/Service/CarService.java
+++ src/main/java/racingcar/Service/CarService.java
@@ -6,6 +6,11 @@ import racingcar.Model.Car;
 import java.util.ArrayList;
 
 public class CarService {
+    public void Racing_Car(ArrayList<Car> cars,int Count) {
+        for(int i=0;i<Count;i++){
+            Move(cars);
+        }
+    }
     private int Generate_RandomValue(){
         return Randoms.pickNumberInRange(0,9);
     }
