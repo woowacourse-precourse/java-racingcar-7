cc97e0d9144704066acdc69288b09986c73d07de
Mint <audwl03071@gmail.com>
2024-10-27 17:59:03 +0900

    test: 시도 횟수 VO 테스트 작성

---
+++ src/test/java/racingcar/model/attempt/AttemptTest.java
@@ -0,0 +1,41 @@
+package racingcar.model.attempt;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import racingcar.exception.InvalidAttemptException;
+
+@DisplayName("시도 횟수 테스트")
+class AttemptTest {
+
+    @Nested
+    @DisplayName("시도 횟수 생성 시,")
+    class createTest {
+
+        @Test
+        @DisplayName("0 이상의 수이면 성공적으로 생성한다")
+        void 성공_생성_유효한파라미터() {
+            // Given
+
+            // When & Then
+            assertThatCode(() -> {
+                new Attempt(0);
+            }).doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("음수이면 예외를 발생한다")
+        void 실패_생성_음수() {
+            // Given
+
+            // When & Then
+            assertThatThrownBy(() -> new Attempt(-1))
+                    .isExactlyInstanceOf(InvalidAttemptException.class)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("시도 횟수는 0 또는 양수여야 합니다.");
+        }
+    }
+}
