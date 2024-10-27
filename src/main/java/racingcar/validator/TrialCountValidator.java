78f07d343e18d2d7fab19953d05092be143e06be
Hyso <hsk7953@gmail.com>
2024-10-28 03:26:17 +0900

    refactor(TrialCountValidator) : static 메서드들을 인스턴스 메서드로 변경 - 모든 검증 메서드들을 인스턴스 메서드로 변경

--- src/main/java/racingcar/validator/TrialCountValidator.java
+++ src/main/java/racingcar/validator/TrialCountValidator.java
@@ -2,21 +2,21 @@ package racingcar.validator;
 
 public class TrialCountValidator {
 
-    private TrialCountValidator() {
+    public TrialCountValidator() {
     }
 
-    public static void validateTrialCount(String input) {
+    public void validateTrialCount(String input) {
         validateNotBlank(input);
         validateNumeric(input);
     }
 
-    static void validateNotBlank(String input) {
+    void validateNotBlank(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException("빈 값은 입력할 수 없습니다.");
         }
     }
 
-    static void validateNumeric(String input) {
+    void validateNumeric(String input) {
         String regex = "^[1-9]\\d*$";
         if (!input.matches(regex)) {
             throw new IllegalArgumentException("양의 정수만 입력할 수 있습니다.");
