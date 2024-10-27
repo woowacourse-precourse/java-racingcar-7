bb0beb91d5cfc6e11296f0c1b9c56dc88f348d61
Cheong Ye Jun <113380447+LooanCheong@users.noreply.github.com>
2024-10-27 20:41:52 +0900

    feat: 유저 입력부 구현 (Application, UserInput)
    - 자동차 이름 입력 기능 구현
    - 레이싱 시도 횟수 입력 기능 구현
    - 자동차 이름 검증 기능 구현 (미입력, 빈 문자열, 5자 이상의 이름)
    - 레이싱 시도 횟수 검증 기능 구현 (미입력, 양수 입력값)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,10 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        UserInput userInput = new UserInput();
+
+        String[] racingCars = userInput.racingCarNameInput();
+        Integer raceCount = userInput.raceCountInput();
     }
 }
