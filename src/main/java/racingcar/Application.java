342feeecef4c2dec29419bb2261e0aa5dc28abee
sjsk3232 <sjsk3232@gmail.com>
2024-10-28 01:14:00 +0900

    feat : 경주할 자동차 이름 문자열 및 경주 시도 횟수 입력 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
-public class Application {
-    public static void main(String[] args) {
+import camp.nextstep.edu.missionutils.Console;
 
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputNames = Console.readLine();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int trialCount = Integer.parseInt(Console.readLine());
     }
 }
