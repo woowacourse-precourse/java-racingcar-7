c6be0aaa0af0c551d161c7fa0f7eeaf452e4b104
Lee Chae Rin <chaerin0916@gmail.com>
2024-10-27 12:28:51 +0900

    feat: 자동차 이름별로 진행 사항 저장
    - <String, StringBuilder> 형태의 해시맵에 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,13 @@
 package racingcar;
 
-import com.sun.jdi.ArrayReference;
-import java.util.ArrayList;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashMap;
 
 public class Application {
     public static void main(String[] args) {
+        // 맵 형태로 자동차 상황 기록
+        HashMap<String, StringBuilder> carStates = new HashMap<>();
+
         // 자동차 입력 받고 쉼표 기준으로 나누기
         String inputCarNames = Console.readLine();
         String[] carNamesList = inputCarNames.split(",");
@@ -13,13 +15,13 @@ public class Application {
         for (String carName : carNamesList) {
             // 나눌 때 앞뒤로 있을 수 있는 공백 제거
             carName = carName.trim();
-            // 공백이 있다면 예외 발생
+            // 공백이 있거나 자동차 이름이 6글자 이상이면 예외 발생
             if (carName == null || carName.isEmpty()) {
                 throw new IllegalArgumentException("자동차 이름이 공백입니다.");
             } else if (carName.length() >= 6) {
                 throw new IllegalArgumentException("자동차 이름이 6글자 이상입니다.");
             }
+            carStates.put(carName, new StringBuilder());
         }
-
     }
 }
