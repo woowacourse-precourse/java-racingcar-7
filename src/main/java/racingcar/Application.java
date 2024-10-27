dbc2a010b28988a33c12acb6bfbf9b73eb0b2b6e
DY <ldy092000@gmail.com>
2024-10-27 09:36:18 +0900

    feat(game) : update Application.java
    Application.java 코드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,35 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.Game;
+import racingcar.view.GameView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        GameView view = new GameView();
+
+        List<Car> cars = parseCars(view.getCarNamesInput());
+        int attemptCount = view.getAttemptCountInput();
+
+        Game game = new Game(cars, attemptCount);
+
+        view.displayBeforeGameProcess();
+
+        for (int i = 0; i < game.getAttemptCount(); i++) {
+            game.playRound();
+            view.displayRoundResult(game.getRoundResult());
+        }
+
+        view.displayWinners(game.getWinners());
+
+    }
+
+    private static List<Car> parseCars(List<String> carNamesInput) {
+        return carNamesInput.stream()
+                            .map(String::trim)
+                            .map(Car::new)
+                            .toList();
     }
 }
