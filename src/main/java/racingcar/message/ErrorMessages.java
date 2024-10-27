c47eb1c6349b125dd7cf0d4ab38a9b60fa54b735
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 11:49:59 +0900

    :sparkles: feat(ErrorMessages): 입력된 이름이 없을 때 오류 메시지 제공

--- src/main/java/racingcar/message/ErrorMessages.java
+++ src/main/java/racingcar/message/ErrorMessages.java
@@ -1,4 +1,7 @@
 package racingcar.message;
 
 public class ErrorMessages {
+
+    // Car names related error messages
+    public static final String UNPROVIDED_NAMES = "[ERROR] 입력된 이름이 없습니다. 이름을 1개 이상 입력해주세요.";
 }
