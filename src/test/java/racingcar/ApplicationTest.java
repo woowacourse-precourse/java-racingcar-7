0f2d2f40e92087f3df709c00976e6a670a850ca5
함지현 <gkawlgus01@naver.com>
2024-10-27 20:22:42 +0900

    test: 테스트코드 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,7 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.model.RacingGame;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -14,7 +16,8 @@ class ApplicationTest extends NsTest {
     private static final int STOP = 3;
 
     @Test
-    void 기능_테스트() {
+    @DisplayName("자동차 이름 테스트")
+    void carNameTest() {
         assertRandomNumberInRangeTest(
             () -> {
                 run("pobi,woni", "1");
@@ -22,13 +25,62 @@ class ApplicationTest extends NsTest {
             },
             MOVING_FORWARD, STOP
         );
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,pobi", "1");
+                    assertThat(output()).contains("pobi1 : -", "pobi2 : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
     }
 
     @Test
-    void 예외_테스트() {
+    @DisplayName("자동차 이름 예외 테스트")
+    void carNameErrorTest() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,12", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("자동차 이름에는 숫자를 포함할 수 없습니다.")
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,,woni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("자동차 이름에 공백이 들어갈 수 없습니다.")
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("자동차 이름은 최소 2개 이상 입력해야 합니다.")
+        );
+    }
+
+    @Test
+    @DisplayName("라운드 입력 예외 테스트")
+    void roundErrorTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("횟수는 정수여야 합니다.")
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("1 이상의 정수여야 합니다.")
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("1 이상의 정수여야 합니다.")
         );
     }
 
