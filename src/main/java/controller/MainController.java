6b756030f8dfd976b40f743c88c033fcbb9e27f3
Wooseok Jang <wowseok2774@gmail.com>
2024-10-27 20:20:54 +0900

    refactor(controller): 예외 처리를 추가하여 게임 실행 안정성을 개선
    - MainController의 run 메서드에 try-catch 블록을 추가해 예외 발생 시 처리합니다.

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -13,9 +13,13 @@ public class MainController {
     }
 
     public void run() {
-        List<Car> cars = carController.getCarsFromUserInput(); // 자동차들 입력받기
-        int attempts = carController.getAttemptCount(); // 시도횟수 입력받기
-        gameController.startGame(cars, attempts); // 게임 실행하기
-        gameController.announceWinners(cars); // 우승자 출력하기
+        try {
+            List<Car> cars = carController.getCarsFromUserInput(); // 자동차들 입력받기
+            int attempts = carController.getAttemptCount(); // 시도횟수 입력받기
+            gameController.startGame(cars, attempts); // 게임 실행하기
+            gameController.announceWinners(cars); // 우승자 출력하기
+        } catch (Exception e) {
+            throw new IllegalArgumentException("게임이 정상적으로 실행되지 않았습니다", e);
+        }
     }
 }
