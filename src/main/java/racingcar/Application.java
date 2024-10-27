79c1085fcc7f7d816cdca6d72829f9dbfaf96418
o-jeong <ljj020304@gmail.com>
2024-10-27 17:29:17 +0900

    feat: main에서 게임 시작 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import racingcar.controller.CarRaceController;
+import racingcar.service.CarRaceService;
+import racingcar.service.RaceResultService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        CarRaceService carRaceService = new CarRaceService();
+        RaceResultService raceResultService = new RaceResultService();
+        CarRaceController carRaceController = new CarRaceController(carRaceService, inputView, outputView, raceResultService);
+
+        carRaceController.startRace();
     }
 }
