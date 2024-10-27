6a132dd8f7984d2dea9ac326bfb5c12b6596504b
dk7648 <dk7648@korea.ac.kr>
2024-10-27 19:32:09 +0900

    자동차 이름 길이 예외 처리 기능 추가

--- src/main/java/racingcar/global/InputValidator.java
+++ src/main/java/racingcar/global/InputValidator.java
@@ -9,9 +9,17 @@ public class InputValidator {
     public static List<String> convertInputToCarNames(String input) {
         List<String> carNames = new ArrayList<>();
         for(String carName : input.split(",")) {
+            String name = carName.trim();
+            checkNameLength(name);
             carNames.add(carName.trim());
         }
         return carNames;
+    }
+
+    public static void checkNameLength(String name) {
+        if(name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public static int convertInputToInteger(String input) throws IllegalArgumentException {
