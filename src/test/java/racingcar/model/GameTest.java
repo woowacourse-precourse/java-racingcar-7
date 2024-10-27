a78f65e87581fb85f24861074e5da099ccbe7847
Park - Jeonghwan <990706leo@gmail.com>
2024-10-28 07:04:29 +0900

    test(Game): 이름의 중복을 검사할 수 있다

--- src/test/java/racingcar/model/GameTest.java
+++ src/test/java/racingcar/model/GameTest.java
@@ -3,6 +3,7 @@ package racingcar.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
 import racingcar.domain.Game;
@@ -17,5 +18,14 @@ public class GameTest {
         List<Car> cars = game.createCars(names);
 
         assertEquals(cars.size(), 3);
+    }
+
+    @Test
+    void 이름의_중복을_검사할_수_있다() {
+        List<String> names = List.of("hwan", "hwan", "pobi");
+
+        Game game = new Game();
+
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> game.createCars(names));
     }
 }
