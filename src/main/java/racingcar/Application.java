b0004c8690837aaad0f6ca0ac4efd165c2754c76
Minseok <saltishDev@gmail.com>
2024-10-27 20:22:52 +0900

    feat: 경주할 자동차 이름을 입력받기 위해 inputString() 사용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String racingCarNameInput = inputString("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     private static String inputString() {
