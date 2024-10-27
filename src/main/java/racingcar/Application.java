8785147f75a6ad5ae7dde25a71e64700665a5185
mskim <msung98@naver.com>
2024-10-28 00:10:46 +0900

    feat: 기능요구사항 2 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -26,55 +26,90 @@ public class Application {
 
     public static void DataTrans(RaceDto data){
         data.racers = data.carName.split(",");
-        for(int i = 0; i < data.racers.length; i++){
-            data.racers[i] = data.racers[i].trim();
-            if(data.racers[i].length() > 5){
-                throw new IllegalArgumentException();
-            }
-        }
+        validateRacers(data.racers);
         data.forwardNum = new int[data.racers.length];
     }
 
+    private static void validateRacers(String[] racers) {
+        for (int i = 0; i < racers.length; i++) {
+            racers[i] = racers[i].trim();
+            validateRacerLength(racers[i]);
+        }
+    }
+
+    private static void validateRacerLength(String racer) {
+        if (racer.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public static void StartRace(RaceDto data){
-        for(int i = 0; i < data.num; i++){
-            for(int j = 0; j < data.racers.length; j++){
-                if(Randoms.pickNumberInRange(0,9) >= 4) {
-                    data.forwardNum[j]++;
-                }
+        for (int i = 0; i < data.num; i++) {
+            moveCars(data);
+        }
+    }
+
+    private static void moveCars(RaceDto data) {
+        for (int j = 0; j < data.racers.length; j++) {
+            if (shouldMove()) {
+                data.forwardNum[j]++;
             }
         }
     }
 
+    private static boolean shouldMove() {
+        return Randoms.pickNumberInRange(0, 9) >= 4;
+    }
+
     public static void CheckResult(RaceDto data){
-        for(int i = 0; i < data.racers.length; i++){
+        for (int i = 0; i < data.racers.length; i++){
             System.out.println(data.racers[i] + " : " + "-".repeat(data.forwardNum[i]));
         }
     }
 
-    public  static void Winner(RaceDto data){
+    public static void Winner(RaceDto data){
+        int max = findMaxDistance(data.forwardNum);
+        int[] winners = findWinners(data, max);
+        printWinners(data, winners);
+    }
 
+    private static int findMaxDistance(int[] forwardNum) {
         int max = 0;
-        int[] winners = new int [data.racers.length];
-        int count = 0;
+        for (int distance : forwardNum) {
+            if (distance > max) {
+                max = distance;
+            }
+        }
+        return max;
+    }
 
+    private static int[] findWinners(RaceDto data, int max) {
+        int[] winners = new int[data.racers.length];
+        int count = 0;
         for (int i = 0; i < data.racers.length; i++) {
-            if (data.forwardNum[i] > max) {
-                max = data.forwardNum[i];
-                count = 0;
-                winners[count] = i;
-                count++;
-            } else if (data.forwardNum[i] == max) {
+            if (data.forwardNum[i] == max) {
                 winners[count] = i;
                 count++;
             }
         }
+        return trimWinners(winners, count);
+    }
+
+    private static int[] trimWinners(int[] winners, int count) {
+        int[] result = new int[count];
+        System.arraycopy(winners, 0, result, 0, count);
+        return result;
+    }
+
+    private static void printWinners(RaceDto data, int[] winners) {
         System.out.print("최종 우승자 : ");
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < winners.length; i++) {
             System.out.print(data.racers[winners[i]]);
-            if (i < count - 1) {
+            if (i < winners.length - 1) {
                 System.out.print(", ");
             }
         }
+        System.out.println();
     }
 }
 
