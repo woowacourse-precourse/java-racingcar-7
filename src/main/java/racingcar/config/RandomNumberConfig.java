b5719e682612564d25410a7de6418d4efd65ab54
신관규 <soundbar91@gmail.com>
2024-10-27 22:05:55 +0900

    refactor: RandomNumberConfig 스태틱 게터로 변경

--- src/main/java/racingcar/config/RandomNumberConfig.java
+++ src/main/java/racingcar/config/RandomNumberConfig.java
@@ -1,22 +1,17 @@
 package racingcar.config;
 
 public class RandomNumberConfig {
-    private static final RandomNumberConfig instance = new RandomNumberConfig();
-    private final int RANDOM_NUMBER_MAX_RANGE = 9;
-    private final int RANDOM_NUMBER_MIN_RANGE = 0;
+    private static final int RANDOM_NUMBER_MAX_RANGE = 9;
+    private static final int RANDOM_NUMBER_MIN_RANGE = 0;
 
     private RandomNumberConfig() {
     }
 
-    public static RandomNumberConfig getInstance() {
-        return instance;
-    }
-
-    public int getRANDOM_NUMBER_MAX_RANGE() {
+    public static int getRandomNumberMaxRange() {
         return RANDOM_NUMBER_MAX_RANGE;
     }
 
-    public int getRANDOM_NUMBER_MIN_RANGE() {
+    public static int getRandomNumberMinRange() {
         return RANDOM_NUMBER_MIN_RANGE;
     }
 }
--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -1,13 +1,12 @@
 package racingcar.util;
 
+import static racingcar.config.RandomNumberConfig.getRandomNumberMaxRange;
+import static racingcar.config.RandomNumberConfig.getRandomNumberMinRange;
+
 import camp.nextstep.edu.missionutils.Randoms;
-import racingcar.config.RandomNumberConfig;
 
 public class RandomNumberGenerator {
-    private static final RandomNumberConfig randomNumberConfig = RandomNumberConfig.getInstance();
-
     public static int getRandomNumber() {
-        return Randoms.pickNumberInRange(randomNumberConfig.getRANDOM_NUMBER_MIN_RANGE(),
-                randomNumberConfig.getRANDOM_NUMBER_MAX_RANGE());
+        return Randoms.pickNumberInRange(getRandomNumberMinRange(), getRandomNumberMaxRange());
     }
 }
