6c55de48c055bb3d68f628990b9b038751677622
전석균 <jsg998877@gmail.com>
2024-10-28 00:45:55 +0900

    refactor($InputView.java): change method name
    - getCarNames라는 함수가 하는 역할이 readLine()이기 때문에 입력을 받을 때 계속 사용해야함. 따라서 더 보편적인 이름인 getStrInput()으로 바꿈

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,7 +3,7 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public String inputCarNames(){
+    public String getStrInput(){
         return Console.readLine();
     }
 }
