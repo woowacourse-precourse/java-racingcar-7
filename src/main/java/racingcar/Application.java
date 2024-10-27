116e5e6cf6b0073107bf0f21f3e68c5382274229
정재민 <jeongjaemin@jeongjaemin-ui-noteubug.local>
2024-10-27 13:57:06 +0900

    feat: 자동차 이름 입력 및 5자 제한 검증 기능 추가
    - 사용자로부터 쉼표(,)로 구분된 자동차 이름을 입력받는 기능 구현
    - 각 자동차 이름의 길이가 5자를 초과할 경우 예외 처리
    - 입력된 자동차 이름에서 공백을 제거하고 검증하는 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String nameInput = Console.readLine();
+        String [] cars = nameInput.split(",");
+
+        // 각 자동차 이름이 5자 이하인지 검증
+        ValidateCarNames(cars);
+    }
+
+
+    private static void ValidateCarNames(String [] carNames){
+        for(String carName : carNames){
+            carName = carName.trim(); // 공백 제거
+
+            if (carName.length() > 5 || carName.isEmpty()){
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
+
+
