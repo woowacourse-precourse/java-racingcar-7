323fbd46433cd412b7ce4f9355280041209daa9c
skadjs <skadjs11@gmail.com>
2024-10-28 00:50:21 +0900

    feat(RacingCar): 경주 현황을 나타내는 기능 추가
    HashMap을 이용해 경기 현황을 나타내는 makeRaceStatus() 함수 추가

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class RacingCar {
@@ -9,6 +10,7 @@ public class RacingCar {
     List<String> carNameList;
     int carNamesSize;
     int tryCount;
+    HashMap<String, Integer> raceStatus = new HashMap<>();
 
     public void getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -23,9 +25,15 @@ public class RacingCar {
         this.carNameList = Arrays.asList(carNames.split(","));
         carNamesSize = carNameList.size();
     }
+
     public void getTryCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         this.tryCount = Integer.parseInt(readLine());
     }
 
+    public void makeRaceStatus() {
+        for (String carName : carNameList) {
+            raceStatus.put(carName, 0);
+        }
+    }
 }
