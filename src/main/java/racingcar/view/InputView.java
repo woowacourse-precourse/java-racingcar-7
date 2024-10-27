b7f826380fb3613f9e4bdbd561220ccfeaf0906e
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-28 05:44:34 +0900

    feat(InputView): 경주할 자동차 입력 기능 구현
    - 경주할 자동차를 입력받습니다.
    - 경주할 자동차는 ','를 기준으로 나누어 list를 생성했습니다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,7 +1,26 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import racingcar.validator.InputCarNamesValidator;
+
 public class InputView {
+    private static final String INPUT_CAR_NAMES_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final InputCarNamesValidator inputCarNamesValidator = new InputCarNamesValidator();
+
     // TODO: 경주할 자동차를 입력받는다.
+    public List<String> getInputCarNames() {
+        System.out.println(INPUT_CAR_NAMES_PROMPT);
+        String InputCarNames = Console.readLine();
+        return convertCarNamesToList(InputCarNames);
+    }
+
+    private List<String> convertCarNamesToList (String inputCarNames) {
+        return Arrays.stream(inputCarNames.split(","))
+                .map(String::trim)
+                .toList();
+    }
 
     // TODO: 이동할 횟수를 입력받는다.
 }
