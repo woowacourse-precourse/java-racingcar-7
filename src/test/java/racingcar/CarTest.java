d57231ce0222aada469b92eca73145e235ef18a7
김성민 <klg1425@gmail.com>
2024-10-27 22:51:28 +0900

    test: CarTest 추가
    Car 클래스 생성 및 테스트코드 작성

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,56 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    private Car car;
+
+    @BeforeEach
+    public void setUp() {
+        car = new Car("test");
+    }
+
+    @Test
+    @DisplayName("차가 처음 생성되면 위치는 0이어야 한다")
+    void 차_초기_위치_테스트() {
+        Assertions.assertThat(car.getCarPosition()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("무작위 값이 4 이상인 경우 차의 위치가 증가해야 한다")
+    void 값_증가_자동차_위치_전진_테스트() {
+        car.carMove();
+        int initialPosition = car.getCarPosition();
+        car.carMove();
+        if (car.getCarPosition() > initialPosition) {
+            Assertions.assertThat(car.getCarPosition()).isEqualTo(initialPosition + 1);
+        } else {
+            Assertions.assertThat(car.getCarPosition()).isEqualTo(initialPosition);
+        }
+    }
+
+    @Test
+    @DisplayName("무작위 값이 4 미만일 경우 차의 위치가 감소해야 한다")
+    void 값_감소_자동차_후진_테스트() {
+        car.carPosition = 1;
+        car.carMove();
+        int newPosition = car.getCarPosition();
+
+        if (newPosition < 1) {
+            Assertions.assertThat(newPosition).isEqualTo(0);
+        } else {
+            Assertions.assertThat(newPosition).isEqualTo(1);
+        }
+    }
+
+    @Test
+    @DisplayName("차의 위치를 '-'로 표시해야 한다")
+    void 자동차_위치_출력_테스트() {
+        car.carPosition = 3;
+        String expectedDisplay = "---";
+        Assertions.assertThat(car.displayPosition()).isEqualTo(expectedDisplay);
+    }
+}
