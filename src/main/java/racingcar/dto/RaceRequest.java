b54a40b4d54c8793a84535a0f2cdbc28973f4a17
KNU-K <knukang334@gmail.com>
2024-10-27 20:26:17 +0900

    refactor(dto): DTO 를 class 에서 record 객체로 변경
    DTO 의 불변성을 지키기 위해서 해당 객체를 record형식으로 생성함.

--- src/main/java/racingcar/dto/RaceRequest.java
+++ src/main/java/racingcar/dto/RaceRequest.java
@@ -2,33 +2,9 @@ package racingcar.dto;
 
 import racingcar.model.CarList;
 
-public class RaceRequest {
-    private final String command;
-    private final int iterations;
-    private CarList carList;
-
+public record RaceRequest(String command, int iterations, CarList carList) {
     public RaceRequest(String command, Integer iterations) {
-        this.command = command;
-        this.iterations = iterations;
-        this.carList = new CarList();
-    }
-
-    public RaceRequest(String command, int iterations, CarList carList) {
-        this.command = command;
-        this.iterations = iterations;
-        this.carList = carList;
-    }
-
-    public CarList getCarList() {
-        return carList;
-    }
-
-    public int getIterations() {
-        return iterations;
-    }
-
-    public String getCommand() {
-        return command;
+        this(command, iterations, new CarList());
     }
 
     public RaceRequest withCarList(CarList carList) {
--- src/main/java/racingcar/dto/RaceResponse.java
+++ src/main/java/racingcar/dto/RaceResponse.java
@@ -1,20 +1,7 @@
 package racingcar.dto;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class RaceResponse {
-    private List<String> winners;
+public record RaceResponse(List<String> winners) {
 
-    public RaceResponse() {
-        this.winners = new ArrayList<>();
-    }
-
-    public void updateWinners(List<String> winners) {
-        this.winners = winners;
-    }
-
-    public List<String> winners() {
-        return winners;
-    }
 }
