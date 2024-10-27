cb83915aacf6212138bb4d9b788705a05a9cd056
jijongkwon <help3451@naver.com>
2024-10-28 01:42:33 +0900

    feat(service) : RacingHistoryService 실행 결과 저장하기

--- src/main/java/racingcar/application/service/history/RacingHistoryService.java
+++ src/main/java/racingcar/application/service/history/RacingHistoryService.java
@@ -1,8 +1,20 @@
 package racingcar.application.service.history;
 
+import racingcar.domain.car.RacingCar;
+import racingcar.domain.history.RacingHistory;
+import racingcar.infrastructure.persistence.InMemoryRacingHistoryRepository;
+
 public class RacingHistoryService {
+    private final InMemoryRacingHistoryRepository historyRepository;
+
+    public RacingHistoryService() {
+        this.historyRepository = InMemoryRacingHistoryRepository.getInstance();
+    }
 
     // TODO : 실행결과 저장하기
+    public void saveHistory(RacingCar racingCar) {
+        historyRepository.saveRacingHistory(new RacingHistory(racingCar.getName(), racingCar.getDistance()));
+    }
 
     // TODO : 최대 거리 갱신하기
 
