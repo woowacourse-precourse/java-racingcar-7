a955e3d35785db33f1f691dd51c99d3c064cc2e4
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-10-28 00:31:33 +0900

    feat: 자동차 이름이 6자 이상인 경우 예외 처리 메서드 추가

--- src/main/java/racingcar/util/Constants.java
+++ src/main/java/racingcar/util/Constants.java
@@ -3,4 +3,5 @@ package racingcar.util;
 public class Constants {
     public static final String CAR_NAME_INPUT_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String EMPTY_INPUT_ERROR_MESSAGE = "입력값은 비어있을 수 없습니다.";
+    public static final String CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 5자 이하만 가능합니다.";
 }
--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -1,9 +1,19 @@
 package racingcar.util;
 
+import java.util.List;
+
 public class Validator {
     public void validateEmptyString(String input) {
         if (input == null || input.equals("")) {
             throw new IllegalArgumentException(Constants.EMPTY_INPUT_ERROR_MESSAGE);
+        }
+    }
+
+    public void validateStringLength(List<String> carNamesList) {
+        for (String splitInput: carNamesList) {
+            if (splitInput.length() >= 6) {
+                throw new IllegalArgumentException(Constants.CAR_NAME_LENGTH_ERROR_MESSAGE);
+            }
         }
     }
 }
