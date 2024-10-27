ec2d12b3be541585fb7b97906cf31a99071f907b
delena0702 <delena0702@gmail.com>
2024-10-27 23:43:58 +0900

    refactor: 문자열 상수 클래스 전역으로 이동 및 포맷 문자열 함수 상단으로 분리
    - 프로그램 내에 사용되는 문자열 상수를 클래스 전역 변수로 이동.
    - 포맷 관련 문자열은 해당 함수의 상단에 별도로 선언하도록 수정.
        - 포맷 문자열은 코드 수정 시의 가독성 향상을 위해 함수 내부에 정의함.

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -3,24 +3,33 @@ package racingcar;
 import java.util.List;
 
 public class OutputView {
+	private static final String PROMPT_FOR_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String PROMPT_FOR_ITERATION_COUNT = "시도 횟수는 몇 회인가요?";
+	private static final String RESULT_HEADER = "실행 결과";
+
+	private static final String WINNER_NAME_SEPARATOR = ", ";
+	private static final String CAR_POSITION_MARKER = "-";
+
 	public static void promptForCarName() {
-		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		System.out.println(PROMPT_FOR_CAR_NAMES);
 	}
 
 	public static void promptForIterationCount() {
-		System.out.println("시도 횟수는 몇 회인가요?");
+		System.out.println(PROMPT_FOR_ITERATION_COUNT);
 	}
 
 	public static void printResultTitle() {
 		System.out.println();
-		System.out.println("실행 결과");
+		System.out.println(RESULT_HEADER);
 	}
 
 	public static void printResultData(List<String> carNames, List<Long> carPosition) {
+		final String CAR_POSITION_FORMAT = "%s : %s%n";
+
 		for (int i = 0; i < carNames.size(); i++) {
 			try {
 				int repeatCount = Math.toIntExact(carPosition.get(i));
-				System.out.printf("%s : %s%n", carNames.get(i), "-".repeat(repeatCount));
+				System.out.printf(CAR_POSITION_FORMAT, carNames.get(i), CAR_POSITION_MARKER.repeat(repeatCount));
 			} catch (ArithmeticException e) {
 				throw new IllegalArgumentException("The input value exceeds the maximum value for an int");
 			}
@@ -30,6 +39,8 @@ public class OutputView {
 	}
 
 	public static void printWinners(List<String> winners) {
-		System.out.printf("최종 우승자 : %s", String.join(", ", winners));
+		final String WINNER_MESSAGE_FORMAT = "최종 우승자 : %s";
+
+		System.out.printf(WINNER_MESSAGE_FORMAT, String.join(WINNER_NAME_SEPARATOR, winners));
 	}
 }
--- src/main/java/racingcar/StringParser.java
+++ src/main/java/racingcar/StringParser.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StringParser {
-	public static List<String> parseCarNames(String input) {
-		List<String> names = Arrays.asList(input.split(",", -1));
+	private static final String CAR_NAME_SEPARATOR = ",";
 
+	public static List<String> parseCarNames(String input) {
+		List<String> names = Arrays.asList(input.split(CAR_NAME_SEPARATOR, -1));
 		Validator.validateCarNames(names);
 		return names;
 	}
