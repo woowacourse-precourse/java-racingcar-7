3e9b0d496f7c6c4988cd9709be2c6e0a4ebc380c
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-10-27 13:43:51 +0900

    feat : 출력 관리 클래스 생성
    ConsoleOutputHandler.java : 자동차 이름 입력, 시도 횟수 안내 문구 출력 함수를 추가하였다.
    
    RacingCar.java : run() 함수에 위 두 함수를 추가하였다.

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -3,15 +3,19 @@ package racingcar;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.io.ConsoleInputHandler;
+import racingcar.io.ConsoleOutputHandler;
 import racingcar.util.Separator;
 
 public class RacingCar {
 
     private final ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
+    private final ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
     private final Separator separator = new Separator();
 
     public void run(){
+        consoleOutputHandler.printCarNameInstruction();
         String carListInput = consoleInputHandler.getCarListInput();
+        consoleOutputHandler.printTryCountInstruction();
         int tryCount = consoleInputHandler.getMoveTryCount();
         List<String> carList = separator.separateCarList(carListInput);
         List<Car> cars = new ArrayList<>();
