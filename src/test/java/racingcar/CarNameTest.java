b7fe627d624441db9d60fed0a7701219adbafe8d
sc@t0a14 <gusdn6671@gmail.com>
2024-10-27 09:01:31 +0900

    test(CarNameTest) : 빈 자동차 이름을 생성하려고 하면 예외가 발생한다

---
+++ src/test/java/racingcar/CarNameTest.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.model.CarName;
+
+public class CarNameTest {
+
+    @Test
+    @DisplayName("빈 자동차 이름을 생성하려고 하면 예외가 발생한다")
+    void createEmptyCarName() {
+        String name = "";
+        assertThatThrownBy(() -> CarName.from(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름에 빈칸은 입력할수 없습니다");
+    }
+
+}
