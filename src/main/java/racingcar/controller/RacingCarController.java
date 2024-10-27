b6d901c9d1415eb472818da87eb358e2dce1806f
201912025 <michi2012@naver.com>
2024-10-27 16:43:06 +0900

    feat(RacingCarController): run() 메서드 구현

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,34 @@
+package racingcar.controller;
+
+import racingcar.service.RacingCarService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class RacingCarController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RacingCarService racingCarService;
+
+    public RacingCarController(InputView inputView, OutputView outputView, RacingCarService racingCarService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.racingCarService = racingCarService;
+    }
+
+    public void run() {
+        String inputCarsName = inputView.inputCarsName();
+        int inputCarRacingRepeatCount = inputView.inputCarRacingRepeatCount();
+
+        racingCarService.createRacingCars(inputCarsName);
+
+        for (int i = 0; i < inputCarRacingRepeatCount; i++) {
+            racingCarService.advanceRacingCarByRandomOrZero();
+            List<String[]> extractedCarNameAndAdvanceResults = racingCarService.extractCarNameAndAdvanceResult();
+            for (String[] carNameAndAdvanceResult : extractedCarNameAndAdvanceResults) {
+                outputView.printCarRacingResult(carNameAndAdvanceResult[0], carNameAndAdvanceResult[1], extractedCarNameAndAdvanceResults.size());
+            }
+        }
+    }
+}
