824784ac1893673c295b0cb79b4a3f987677bf0a
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-27 17:01:43 +0900

    Feat: RaceService 에서 IO 로직과 자동차 초기화 로직 구현

---
+++ src/main/java/racingcar/service/RaceService.java
@@ -0,0 +1,20 @@
+package racingcar.service;
+
+import racingcar.domain.race.RaceManager;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RaceService {
+
+    private final RaceManager raceManager;
+
+    public RaceService() {
+        this.raceManager = new RaceManager();
+    }
+
+    public void setCars() {
+        OutputView.printCarNamesMessage();
+        String carNames = InputView.readCarNames();
+        raceManager.setCars(carNames);
+    }
+}
