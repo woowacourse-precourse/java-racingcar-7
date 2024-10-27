3156b514ed858a296388b38dbda74f3aaed234c7
KangGeonyoung <kky6335@gmail.com>
2024-10-27 20:36:08 +0900

    feat(Car): 자동차 클래스 생성 Car - 필드, 생성자 설정

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+public class Car {
+
+    private String carName;
+    private int moveCount;
+
+    public Car(String carName) {
+        this.carName = carName;
+        moveCount = 0;
+    }
+}
