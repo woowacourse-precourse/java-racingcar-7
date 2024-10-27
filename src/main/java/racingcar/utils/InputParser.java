19c9c23e831220bf380e179cb964a5aa7baab495
heehunjung <slrkqk2@naver.com>
2024-10-27 21:54:17 +0900

    feat(InputPaser): getNames()메서드 작성

---
+++ src/main/java/racingcar/utils/InputParser.java
@@ -0,0 +1,12 @@
+package racingcar.utils;
+
+import static racingcar.view.InputView.SEPARATOR;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputParser {
+    public static List<String> getNames(String input) {
+        return Arrays.asList(input.split(SEPARATOR ));
+    }
+}
