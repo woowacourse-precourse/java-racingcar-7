7c73978f781af904c2682be99ae3c2fbd655d918
Seol-Munhyeok <abc1234568@naver.com>
2024-10-27 19:55:56 +0900

    feat(Car) : Add Car class and canMove() method

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    String carName;
+    int currentDistance = 0;
+
+    Car(String name) {
+        this.carName = name;
+    }
+
+    boolean canMove() {
+        int randomValue = Randoms.pickNumberInRange(0, 9);
+        return randomValue >= 4;
+    }
+
+    void move() {
+
+
+    }
+
+}
