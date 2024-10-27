ff2197323b7ca998df994a2778b9582db6df2e41
LEEJaeHyeok97 <hazardous10@naver.com>
2024-10-27 13:22:51 +0900

    feat: 게임 작동 구현

---
+++ src/main/java/racingcar/domain/Game.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class Game {
+    private final int tryCount;
+
+    public Game(int tryCount) {
+        this.tryCount = tryCount;
+    }
+
+    public void run() {
+    }
+}
