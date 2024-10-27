47bf89bffa18c72cc78dd895ec3d247dd9e2578b
lkdcode <dlrlejr1@naver.com>
2024-10-27 20:47:14 +0900

    Feat(:RacingCar) 레이싱카 값 객체

---
+++ src/main/java/racingcar/value/RacingCarList.java
@@ -0,0 +1,36 @@
+package racingcar.value;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class RacingCarList {
+    private final List<RacingCar> list;
+
+    public RacingCarList(List<RacingCar> list) {
+        this.list = list;
+    }
+
+    public void action(final Consumer<RacingCar> action) {
+        list.forEach(action);
+    }
+
+    public void actionPrint(final Consumer<RacingCarList> action) {
+        action.accept(this);
+    }
+
+    public WinnerPlayerList getWinnersList() {
+        return list.stream()
+            .filter(e -> e.getDistance().equals(getWinnerDistance()))
+            .map(RacingCar::getPlayer)
+            .collect(Collectors.collectingAndThen(Collectors.toList(), WinnerPlayerList::new));
+    }
+
+    private Distance getWinnerDistance() {
+        return this.list.stream()
+            .map(RacingCar::getDistance)
+            .max(Comparator.comparingLong(Distance::getValue))
+            .orElseThrow(IllegalArgumentException::new);
+    }
+}
