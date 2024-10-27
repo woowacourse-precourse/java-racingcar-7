097f9e683c6f22706f24d8a34a4ffefc3f0c4c8b
LEEJaeHyeok97 <hazardous10@naver.com>
2024-10-27 17:48:50 +0900

    feat: 출력 기능 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,37 @@
+package racingcar.view;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Game;
+
+public class OutputView {
+
+    public void printRequestName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void printRequestTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+    public void printResultMessage() {
+        System.out.println("실행 결과");
+    }
+
+    public void printRoundResult(HashMap<String, Integer> roundResult) {
+        for (HashMap.Entry<String, Integer> entry : roundResult.entrySet()) {
+            System.out.print(entry.getKey() + " : ");
+            for (int i = 0; i < entry.getValue(); i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printWinner(List<String> winner) {
+        String result = String.join(", ", winner);
+        System.out.println("최종 우승자 = " + result);
+    }
+}
