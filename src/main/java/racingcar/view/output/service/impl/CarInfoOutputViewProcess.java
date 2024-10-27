23d3c97a11b64a66410eff387d58d2337dfa1fe9
EHCTH <lees90896@gmail.com>
2024-10-27 12:21:07 +0900

    feat: InfoOutputService 구현체

--- src/main/java/racingcar/view/output/service/impl/CarInfoOutputViewProcess.java
+++ src/main/java/racingcar/view/output/service/impl/CarInfoOutputViewProcess.java
@@ -2,7 +2,7 @@ package racingcar.view.output.service.impl;
 
 import racingcar.view.output.service.InfoOutputViewService;
 
-public class CarInfoOutputView implements InfoOutputViewService {
+public class CarInfoOutputViewProcess implements InfoOutputViewService {
     @Override
     public void info() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
--- src/main/java/racingcar/view/output/service/impl/TryNumberInfoOutputViewProcess.java
+++ src/main/java/racingcar/view/output/service/impl/TryNumberInfoOutputViewProcess.java
@@ -2,7 +2,7 @@ package racingcar.view.output.service.impl;
 
 import racingcar.view.output.service.InfoOutputViewService;
 
-public class TryNumberInfoOutputView implements InfoOutputViewService {
+public class TryNumberInfoOutputViewProcess implements InfoOutputViewService {
     @Override
     public void info() {
         System.out.println("시도할 횟수는 몇 회인가요?");
