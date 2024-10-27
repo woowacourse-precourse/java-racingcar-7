76f38f15e6d72b7607c9e47703e786c7c3f3d0f5
m2nseop <pms246@naver.com>
2024-10-28 00:49:17 +0900

    feat(Car): 자동차 이름 글자수 검증 로직 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,10 +3,13 @@ package racingcar.domain;
 public class Car implements Raceable {
     private final static int STEP_SIZE = 1;
     private final static char DISTANCE_SYMBOL = '-';
+    private final static int NAME_MAX_LEGNTH = 5;
+    private final static int NAME_MIN_LEGNTH = 1;
     private final String name;
     private int moveCount;
 
     public Car(String name) {
+        validateNameLength(name);
         this.name = name;
         this.moveCount = 0;
     }
@@ -23,5 +26,13 @@ public class Car implements Raceable {
             progressBuilder.append(DISTANCE_SYMBOL);
         }
         return progressBuilder.toString();
+    }
+
+    private void validateNameLength(String name){
+        if(name.length() > NAME_MAX_LEGNTH) {
+            throw new IllegalArgumentException("이름은 5글자 이하만 가능합니다.");
+        }else if(name.length() < NAME_MIN_LEGNTH){
+            throw new IllegalArgumentException("이름은 1글자 이상만 가능합니다.");
+        }
     }
 }
