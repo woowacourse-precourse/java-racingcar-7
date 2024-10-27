0ca5d52f5c62f887d271cc073e1c1f10d4d70379
SukJin Kim <rkathrwks@gmail.com>
2024-10-27 17:11:39 +0900

    feat(outputView): 최종 우승자 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -10,6 +10,8 @@ public class OutputView {
     private static final String PLAYER_POSITION_TEMPLATE = "%s : %s\n";
     private static final String FOOTER = "\n";
     private static final String ONE_STEP = "-";
+    private static final String FINAL_WINNER_TEMPLATE = "최종 우승자 : %s";
+    private static final String WINNERS_DELIMITER = ", ";
 
     public void printRoundResults(RacingGame racingGame) {
         if (racingGame.isFirstRound()) {
@@ -17,7 +19,7 @@ public class OutputView {
         }
 
         List<RacingCar> players = racingGame.getPlayers();
-        
+
         for (RacingCar player : players) {
             String name = player.getName();
             String position = printPosition(player.getPosition());
@@ -30,5 +32,26 @@ public class OutputView {
 
     private String printPosition(int position) {
         return ONE_STEP.repeat(position);
+    }
+
+    public void printFinalWinners(RacingGame racingGame) {
+        List<RacingCar> finalWinners = racingGame.findFinalWinners();
+
+        if (finalWinners.size() == 1) {
+            printSingleWinner(finalWinners);
+        }
+        if (finalWinners.size() > 1) {
+            printMultipleWinners(finalWinners);
+        }
+    }
+
+    private void printSingleWinner(List<RacingCar> finalWinners) {
+        String name = finalWinners.getFirst().getName();
+        System.out.printf(FINAL_WINNER_TEMPLATE, name);
+    }
+
+    private void printMultipleWinners(List<RacingCar> finalWinners) {
+        List<String> names = finalWinners.stream().map(RacingCar::getName).toList();
+        System.out.printf(FINAL_WINNER_TEMPLATE, String.join(WINNERS_DELIMITER, names));
     }
 }
