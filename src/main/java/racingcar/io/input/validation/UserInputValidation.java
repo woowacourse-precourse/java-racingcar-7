dba3aaf54477a2aabb0454cb6c8abf56b90adc62
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-10-27 10:03:03 +0900

    refactor: 자동자 이름 검증 기능 구분자를 파라미터로 주입 받게 변경

--- src/main/java/racingcar/io/input/validation/UserInputValidation.java
+++ src/main/java/racingcar/io/input/validation/UserInputValidation.java
@@ -4,19 +4,18 @@ import java.util.regex.Pattern;
 
 public class UserInputValidation {
 
-	private static final String CAR_NAME_DELIMITER = ",";
 	private static final Pattern NUMBER_PATTERN = Pattern.compile("[0-9]*");
 	private static final String BLANK_INPUT_ERROR_MESSAGE = "빈 문자열은 입력할 수 없습니다.";
 	private static final String NOT_CONTAIN_CAR_NAME_DELIMITER_ERROR_MESSAGE = "2대 이상의 자동차를 쉼표를 이용해 구분해 입력해주세요";
 	private static final String NOT_NUMBER_TYPE_ERROR_MESSAGE = "숫자만 입력 가능합니다.";
 
-	public void validateInputCarNames(String inputCarNames) {
+	public void validateInputCarNames(String inputCarNames, String delimiter) {
 		validateBlankInput(inputCarNames);
-		validateNotContainsCarNameDelimiter(inputCarNames);
+		validateNotContainsCarNameDelimiter(inputCarNames, delimiter);
 	}
 
-	private void validateNotContainsCarNameDelimiter(String userInput) {
-		if(!userInput.contains(CAR_NAME_DELIMITER)) {
+	private void validateNotContainsCarNameDelimiter(String userInput, String delimiter) {
+		if(!userInput.contains(delimiter)) {
 			throw new IllegalArgumentException(NOT_CONTAIN_CAR_NAME_DELIMITER_ERROR_MESSAGE);
 		}
 	}
