ba987c91d53ae8433dff89c051de4898b2133daa
Seol_JY <wlsdud5654@gmail.com>
2024-10-27 09:43:35 +0900

    fix(Name): Name vo가 예외적으로 참조 동등성만을 만족하도록 수정
    - 동일 이름을 가진 자동차도 서로 다른 것으로 판별되도록

--- src/main/java/racingcar/vo/Name.java
+++ src/main/java/racingcar/vo/Name.java
@@ -2,8 +2,12 @@ package racingcar.vo;
 
 import static racingcar.constant.ExceptionMessage.INVALID_CAR_NAME_LENGTH;
 
-import java.util.Objects;
-
+/**
+ * 자동차 이름을 나타내는 Value Object 입니다. 주의: 이 클래스는 일반적인 VO와 달리 참조 동등성(reference equality)을 사용합니다. 이는 동일한 이름의 서로 다른 자동차를 구분하기
+ * 위한 의도적인 설계입니다.
+ *
+ * @implSpec 이 클래스는 의도적으로 equals와 hashCode를 구현하지 않아 Object 클래스의 참조 기반 동등성 검사를 사용합니다.
+ */
 public class Name {
     private static final int MIN_LENGTH = 1;
     private static final int MAX_LENGTH = 5;
@@ -29,21 +33,5 @@ public class Name {
 
     public String getValue() {
         return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Name name)) {
-            return false;
-        }
-        return Objects.equals(value, name.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(value);
     }
 }
