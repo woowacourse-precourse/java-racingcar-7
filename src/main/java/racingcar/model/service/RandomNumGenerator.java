59ec9b8e6dd7d0ee885d5aeccb293f1524e094c7
Sujin Kim <su10jin11@khu.ac.kr>
2024-10-27 18:05:55 +0900

    feat : 0에서 9까지 중 핸덤 숫자 생성 기능 구현
    camp.nextstep.edu.missionutils.Randoms.pickNumberInRange를 이용해 0에서 9까지의 랜덤 숫자를 반환하는 메서드를 구현했습니다.

---
+++ src/main/java/racingcar/model/service/RandomNumGenerator.java
@@ -0,0 +1,10 @@
+package racingcar.model.service;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+public class RandomNumGenerator {
+
+    public static int generateRandomNumber() {
+        return pickNumberInRange(0, 9);
+    }
+}
