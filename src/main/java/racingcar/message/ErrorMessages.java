f281a2529e2004154269cb960e7c1817a133975a
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 11:50:32 +0900

    :sparkles: feat(ErrorMessages): 중복된 이름이 있을 때 오류 메시지 제공

--- src/main/java/racingcar/message/ErrorMessages.java
+++ src/main/java/racingcar/message/ErrorMessages.java
@@ -4,4 +4,5 @@ public class ErrorMessages {
 
     // Car names related error messages
     public static final String UNPROVIDED_NAMES = "[ERROR] 입력된 이름이 없습니다. 이름을 1개 이상 입력해주세요.";
+    public static final String DUPLICATED_NAMES = "[ERROR] 중복된 이름이 있습니다. 각 이름이 고유하도록 입력해주세요.";
 }
