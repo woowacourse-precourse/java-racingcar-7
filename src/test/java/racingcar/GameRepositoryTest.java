2d8ebbf51cc2bafd064b11eaf289e52dc06cb329
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-10-27 21:48:53 +0900

    feat(GameRepositoryTest) : 이름 입력 및 저장 기능 테스트

--- src/test/java/racingcar/GameRepositoryTest.java
+++ src/test/java/racingcar/GameRepositoryTest.java
@@ -1,4 +1,26 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 public class GameRepositoryTest {
+
+    GameRepository gameRepository = new GameRepository();
+
+    @Test
+    void 자동차_이름_설정_테스트() {
+        // given
+        List<String> names = List.of("tokki", "pobi", "nori");
+
+        // when
+        gameRepository.setCarName(names);
+        List<String> allNames = gameRepository.getAllNames();
+
+        // then
+        assertThat(allNames).containsExactly("tokki", "pobi", "nori");
+    }
+
+    
 }
