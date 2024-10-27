ddcea66ef50d6ddf6c12125f46b842f2452630bd
dev-joeysos <joeysos@naver.com>
2024-10-27 21:01:27 +0900

    feat: Add race result output handling

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,35 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static javax.management.Query.or;
 
 public class Application {
+    public static int moveCar(int random, int movements) {
+        if (random >= 4) {
+            movements += 1;
+        }
+        return movements;
+    }
+
+    public static void calculateMovement(String[] cars, Map<String, Integer> carMoves) {
+        for (String car: cars) {
+            int random = Randoms.pickNumberInRange(0, 9);
+            int movements = carMoves.get(car);
+            movements = moveCar(random, movements);
+
+            System.out.println(car + " : " + "-".repeat(movements));
+            carMoves.put(car, movements);
+        }
+    }
     public static void main(String[] args) {
+        Map<String, Integer> carMoves = new HashMap<>();
+        int count;
+
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] cars = Console.readLine().split(",");
 
@@ -13,18 +37,24 @@ public class Application {
             if (car.length() > 5 || car.trim().isEmpty()) {
                 throw new IllegalArgumentException("잘못된 입력 형식입니다.");
             }
-            System.out.println(car);
+            carMoves.put(car, 0);
+            // System.out.println(car);
         }
         System.out.println("시도할 횟수는 몇 회인가요?");
         try {
-            int count = Integer.parseInt(Console.readLine());
-            System.out.println(count);
+            count = Integer.parseInt(Console.readLine());
+            //System.out.println(count);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("잘못된 입력 형식입니다. 숫자만 입력해주세요.");
         }
 
-        // TODO: 출력부 구현
         System.out.println("\n실행 결과");
+        for (int i = 0; i < count; i++) {
+            calculateMovement(cars, carMoves);
+            System.out.println();
+        }
 
+        // TODO: 최종 우승자 출력
+        System.out.println("최종 우승자 : ");
     }
 }
