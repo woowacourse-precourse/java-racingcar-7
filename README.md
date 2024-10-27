d2ea89aa8bc7555cc83aecd88f0ad37c0fd1ae96
Yoo SeungHyeon <effort-result@naver.com>
2024-10-27 13:04:09 +0900

    fix(Library) : Change the library according to conditions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # java-racingcar-precourse
+
+## 자동차 경주 게임 구현
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,2 +1,33 @@
-package racingcar;public class Car {
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private String carName;
+    private String moveNum;
+    private int moveCount;
+
+    public Car(String carName) {
+        this.carName = carName;
+        this.moveNum = "";
+    }
+
+    public void show(){
+        System.out.println(carName + " : " + moveNum);
+    }
+
+    public void randomMove(){
+        if(Randoms.pickNumberInRange(0,9)>=4){
+            moveNum += "-";
+            moveCount++;
+        };
+    }
+
+    public int getMoveCount(){
+        return moveCount;
+    }
+
+    public String getCarName(){
+        return carName;
+    }
 }
