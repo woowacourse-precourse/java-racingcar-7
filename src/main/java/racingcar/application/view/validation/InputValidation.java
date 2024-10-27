0421148fe61706c3c291dca9c1e3784d1b57ae3b
jody816 <81346079+jody816@users.noreply.github.com>
2024-10-28 01:36:56 +0900

    feat: 차 이름 입력값 중복에 대한 유효성 검사 로직 추가

--- src/main/java/racingcar/application/view/validation/InputValidation.java
+++ src/main/java/racingcar/application/view/validation/InputValidation.java
@@ -1,5 +1,6 @@
 package racingcar.application.view.validation;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -8,6 +9,7 @@ public class InputValidation {
   private static final String EMPTY_INPUT_ERROR = "자동차 이름은 비어있을 수 없습니다.";
   private static final String INVALID_CAR_NAME_LENGTH_ERROR = "자동차 이름은 1자 이상, 5자 이하로 입력해야 합니다.";
   private static final String INVALID_ATTEMPT_COUNT_ERROR = "시도할 횟수는 0 이상의 숫자로 입력해 주세요.";
+  private static final String DUPLICATE_CAR_NAME_ERROR = "자동차 이름에 중복된 값이 있습니다.";
 
   public static void validateInputCarNames(String inputCarNames) {
 
@@ -31,6 +33,10 @@ public class InputValidation {
 
     if (carNameList.isEmpty()) {
       throw new IllegalArgumentException(EMPTY_INPUT_ERROR);
+    }
+
+    if (carNameList.size() != new HashSet<>(carNameList).size()) {
+      throw new IllegalArgumentException(DUPLICATE_CAR_NAME_ERROR);
     }
 
     for (String carName : carNameList) {
