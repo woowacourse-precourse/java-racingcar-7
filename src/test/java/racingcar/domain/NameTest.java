b31e8d06d803b549472ef0837f7b43037eb31d20
GS_song98 <20003204@sju.ac.kr>
2024-10-27 21:38:24 +0900

    test: NameTest @Nested를 이용해 구분

--- src/test/java/racingcar/domain/NameTest.java
+++ src/test/java/racingcar/domain/NameTest.java
@@ -5,43 +5,54 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class NameTest {
 
-    @Test
-    @DisplayName("5글자 이하의 이름이 들어오면 성공적으로 생성한다.")
-    void successfullyCreateName() {
-        //given
-        String invalidName = "song";
-        String expected = "song";
+    @Nested
+    @DisplayName("Name 생성 테스트")
+    class whenCreateName {
 
-        //when then
-        Name name = assertDoesNotThrow(() -> new Name(invalidName));
-        assertThat(name.toString()).isEqualTo(expected);
-    }
+        @Test
+        @DisplayName("5글자 이하의 이름이 들어오면 성공적으로 생성한다.")
+        void successfullyCreateName() {
+            //given
+            String invalidName = "song";
+            String expected = "song";
 
-    @Test
-    @DisplayName("5글자 이상의 이름이 들어오면 예외를 발생시킨다.")
-    void throwsExceptionWhenCreateNameLongerThanFive() {
-        //given
-        String invalidName = "SongGwanSeok";
+            //when then
+            Name name = assertDoesNotThrow(() -> new Name(invalidName));
+            assertThat(name.toString()).isEqualTo(expected);
+        }
 
-        //when then
-        assertThatThrownBy(() -> new Name(invalidName))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("이름은 5자를 초과할 수 없습니다.");
-    }
+        @Nested
+        @DisplayName("Name 생성 실패")
+        class whenInvalidName {
 
-    @Test
-    @DisplayName("빈 문자열만으로 구성된 이름이 들어오면 예외를 발생시킨다.")
-    void throwsExceptionWhenCreateNameIsBlank() {
-        //given
-        String invalidName = "  ";
+            @Test
+            @DisplayName("5글자 이상의 이름이 들어오면 예외를 발생시킨다.")
+            void throwsExceptionWhenCreateNameLongerThanFive() {
+                //given
+                String invalidName = "SongGwanSeok";
 
-        //when then
-        assertThatThrownBy(() -> new Name(invalidName))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("이름에는 빈 문자만으로 구성된 문자열이 들어올 수 없습니다.");
+                //when then
+                assertThatThrownBy(() -> new Name(invalidName))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("이름은 5자를 초과할 수 없습니다.");
+            }
+
+            @Test
+            @DisplayName("빈 문자열만으로 구성된 이름이 들어오면 예외를 발생시킨다.")
+            void throwsExceptionWhenCreateNameIsBlank() {
+                //given
+                String invalidName = "  ";
+
+                //when then
+                assertThatThrownBy(() -> new Name(invalidName))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("이름에는 빈 문자만으로 구성된 문자열이 들어올 수 없습니다.");
+            }
+        }
     }
 }
