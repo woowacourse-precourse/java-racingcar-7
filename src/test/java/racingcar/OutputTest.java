7f74b887224358d4a3e6a64ddd0d44c19d8e9e52
JinHyung-Park <trupjh@gmail.com>
2024-10-27 23:02:47 +0900

    test(OutputTest) : 라운드 결과 출력 테스트 수정 및 완료

--- src/test/java/racingcar/OutputTest.java
+++ src/test/java/racingcar/OutputTest.java
@@ -33,8 +33,8 @@ public class OutputTest {
         cars.getCarList().forEach(car -> car.moveIfTrue(5));
         round.saveRoundResult(cars);
 
-        gameResult = racingGame.getGameResult();
+        gameResult = racingGame.getEachRoundResult();
         OutputView.roundResult(round.toStringRoundResult());
-        assertEquals("emily: 5\\ntomas: 5\\ngina: 5\\n", round.toStringRoundResult());
+        assertEquals("emily: 5\ntomas: 5\ngina: 5\n", round.toStringRoundResult());
     }
 }
