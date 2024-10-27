7b8dd26c52725b220df1ae6253a1999e42106e3f
JEONGHUNJEON <jjhoon06@gmail.com>
2024-10-28 05:03:54 +0900

    feat: main에서 기능 실행 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+
+import racingcar.controller.RaceController;
+import racingcar.domain.RequestRaceDto;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RaceController raceController = new RaceController();
+        RequestRaceDto requestRaceDto = raceController.parseInput();
+        raceController.play(requestRaceDto);
+        raceController.declareWinners(requestRaceDto);
     }
 }
