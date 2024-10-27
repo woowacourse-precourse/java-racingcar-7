a6adc6a51df094d2650aec9f8c8694e696ca7f9d
softmoca <acyc0909@gmail.com>
2024-10-27 11:40:23 +0900

    test : 자동차 이름에 알파벳 이외의 문자가 포함된 경우 예외 발생 테스트 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -15,4 +15,11 @@ class CarTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.CAR_NAME_TOO_LONG.getMessage());
     }
+
+    @Test
+    void 자동차_이름_유효성_예외_알파벳이_아닌_경우() {
+        assertThatThrownBy(() -> new Car("1234"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.CAR_NAME_INVALID_CHARACTERS.getMessage());
+    }
 }
