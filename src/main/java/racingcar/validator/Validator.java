2e80f49e270c1eaf8430c8fba7e639c5fef01eb8
yunsseong <aster20@hanyang.ac.kr>
2024-10-27 22:16:25 +0900

    feat: 이름 검증 기능 구현

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -1,8 +1,26 @@
 package racingcar.validator;
 
+import java.util.List;
+import racingcar.util.Util;
+
 public class Validator {
 
-    public boolean validateNames(String names) {
+    public boolean validateNames(String string) {
+        if (string.isEmpty() || isOverOne(string)) {
+            isOverOne(string);
+        }
+        List<String> names = Util.splitByComma(string);
+        names.forEach(name -> isLimitExceed(name, 5));
         return true;
+    }
+
+    private boolean isOverOne(String string) {
+        return Util.splitByComma(string).size() <= 1;
+    }
+
+    private void isLimitExceed(String string, int limit) {
+        if (string.length() > limit) {
+            throw new IllegalArgumentException();
+        }
     }
 }
