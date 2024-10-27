77d544cf05e89c2cad7dbddefde7f268b36ea78c
waffle <nate.crema@gmail.com>
2024-10-28 03:06:54 +0900

    feat: Car 객체 및 관련로직 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.*;
+
 public class Car {
     private static long carCnt;
     private long carId;
@@ -11,5 +13,19 @@ public class Car {
 
         this.name = name;
         this.loc = 0;
+    }
+
+    public String getName() { return this.name; }
+
+    public int getLoc() { return this.loc; }
+
+    /**
+     * 자동차가 랜덤값에 따라 앞으로 이동하도록 합니다
+     * @return 자동차가 랜덤값에 따라 이동한 후 위치
+     */
+    public int run() {
+        if ( Randoms.pickNumberInRange(0, 9) < 4 )
+            return this.getLoc();
+        return ++this.loc;
     }
 }
