bd2d18557a78ccb8499ce377b006ab43143507e0
wanbinchoi <cwb6278@gmail.com>
2024-10-27 20:35:33 +0900

    add validation method for car name and round

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,9 +10,13 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        List<Car> cars = getCarNames();
-        int rounds = getRounds();
-        playRacingCar(cars,rounds);
+        try{
+            List<Car> cars = getCarNames();
+            int rounds = getRounds();
+            playRacingCar(cars,rounds);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 
     // 자동차 이름 입력
@@ -23,15 +27,37 @@ public class Application {
         List<Car>cars = new ArrayList<>();
 
         for (String name : names) {
+            validateCarName((name.trim()));
             cars.add(new Car(name.trim()));
         }
         return cars;
+    }
+
+    //자동차 이름 입력 유효성 검사
+    private static void validateCarName(String name){
+        if(name.isEmpty() || name.length() > 5){
+            throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력해야 합니다.");
+        }
+    }
+
+    //입력 횟수 입력 유효성 검사
+    private static void validateRound(String round){
+        try{
+            Integer.parseInt(round);
+        }catch(Exception e){
+            throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+        }
+        if(Integer.parseInt(round) < 1){
+            throw new IllegalArgumentException("시도 횟수는 1회 이상이어야 합니다.");
+        }
     }
 
     // 시도할 횟수 입력
     private static int getRounds(){
         System.out.println("시도할 횟수는 몇 회인가요?");
         String input = Console.readLine();
+
+        validateRound(input);
 
         return Integer.parseInt(input);
     }
@@ -47,7 +73,6 @@ public class Application {
             printCurrentPosition(cars);
         }
         getWinner(cars);
-
     }
 
     //현재 위치 출력
