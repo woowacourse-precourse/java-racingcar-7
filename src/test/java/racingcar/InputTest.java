50eeb64d63dba03ca5de58e79f69dca3cd8c7e3c
bronzehyeon <bronzehyeon@gmail.com>
2024-10-27 21:16:56 +0900

    refactor : InputTest
    - 메서드명 영어로 변경

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -9,7 +9,7 @@ public class InputTest {
 
     @Test
     @DisplayName("긴 차 이름이 입력되는 경우 오류 테스트")
-    void 입력_테스트1() {
+    void input_test_1() {
         Assertions.assertThatThrownBy(() -> Input.parseCarNames("pobi,abcdef"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("차 이름이 너무 깁니다.");
@@ -17,7 +17,7 @@ public class InputTest {
 
     @Test
     @DisplayName("쉼표 뒤에 아무것도 오지 않는 경우 입력테스트")
-    void 입력_테스트2() {
+    void input_test_2() {
         Assertions.assertThatThrownBy(() -> Input.parseCarNames("pobi,abcde,"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("쉼표는 구분자로만 사용해야 합니다.");
