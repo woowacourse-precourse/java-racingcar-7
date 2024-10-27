a9d77db60daab0a2232c92444c9dc4206b25a626
dongyeopwoo <dongyeopwoo@naver.com>
2024-10-27 20:07:31 +0900

    feat(view): OutputView에 게임 결과 출력 기능 추가
    - OutputView 클래스를 추가하여 게임 실행 결과 및 최종 우승자를 출력하는 기능을 구현하였습니다.
    - gameResultMessage(String roundResults) 메서드를 통해 모든 라운드의 결과를 출력하며, gameWinnerMessage() 메서드를 통해 최종 우승자를 표시합니다.
    - Comment 상수 메시지를 사용하여 출력 메시지의 일관성을 유지하고, 유지보수를 용이하게 합니다.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,2 +1,22 @@
-package view;public class OutputView {
+package view;
+
+import constant.Comment;
+import service.ResultService;
+
+public class OutputView {
+
+    private final ResultService resultService;
+
+    public OutputView(ResultService resultService) {
+        this.resultService = resultService;
+    }
+
+    public void gameResultMessage(String roundResults) {
+        System.out.printf(Comment.RESULT_MESSAGE.getMessage() + roundResults);
+    }
+
+    public void gameWinnerMessage() {
+        System.out.printf(Comment.WINNER_MESSAGE.getMessage() + resultService.displayWinners());
+    }
 }
+
