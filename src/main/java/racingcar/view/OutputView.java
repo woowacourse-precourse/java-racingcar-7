c55d43c797e52f92522e38850dbcda954a028980
young-in <hardworkingini@gmail.com>
2024-10-28 00:10:48 +0900

    feat : 각 라운드의 결과를 출력하는 method 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,16 @@
 package racingcar.view;
 
+import racingcar.domain.RacingCar;
+import racingcar.domain.RacingCars;
+
 public class OutputView {
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
     private static final String NEXT_LINE = "\n";
     private static final String EXECUTION_RESULT = "실행 결과";
+    private static final String COLON_SIGN = " : ";
+    private static final String DISTANCE_SIGN = "-";
+    private static final String SPACE = " ";
 
     public void printRequestCarNames(){
         System.out.println(INPUT_CAR_NAMES);
@@ -16,5 +22,19 @@ public class OutputView {
 
     public void printExecutionResult(){
         System.out.println(NEXT_LINE + EXECUTION_RESULT);
+    }
+
+    public void printRoundByExecutionResults(final RacingCars racingCars){
+        StringBuilder resultByRound = new StringBuilder();
+        for (RacingCar racingCar : racingCars.getRacingCars()) {
+            resultByRound
+                    .append(racingCar.getRacingCarName())
+                    .append(SPACE)
+                    .append(COLON_SIGN)
+                    .append(SPACE)
+                    .append(DISTANCE_SIGN.repeat(racingCar.getLocation()))
+                    .append(NEXT_LINE);
+        }
+        System.out.println(resultByRound);
     }
 }
