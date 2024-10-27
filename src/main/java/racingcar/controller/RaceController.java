f816d2188fb63bbd0b11f348ce16d541d664c173
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-10-27 13:38:40 +0900

    feat: 컨트롤러 구현
    뷰와 서비스 계층을 통해 전반적인 경주를 진행하는 컨트롤러를 구현하였다.

---
+++ src/main/java/racingcar/controller/RaceController.java
@@ -0,0 +1,21 @@
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.service.RaceService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RaceController {
+    public void start() {
+        InputView inputView = new InputView();
+        OutputView outputView = OutputView.getInstance();
+
+        String carsNames = inputView.inputCarName();
+        String repeatTimes = inputView.inputRepeatTimes();
+
+        outputView.roundResultHeader();
+        RaceService raceService = new RaceService();
+        List<String> finalWinners = raceService.startRacing(carsNames, repeatTimes);
+        outputView.printFinalResult(finalWinners);
+    }
+}
