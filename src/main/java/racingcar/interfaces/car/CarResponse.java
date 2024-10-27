b39142386ef0cd8af36a767a27f70d28c4bdaf8e
soeunnPark <carol0724@naver.com>
2024-10-27 17:35:41 +0900

    feat(CarResponse): Make Car Response to print Car Status every round in race
    자동차 경주가 진행되는 동안 각 round 에 대해 자동차의 상태(자동차 이름 + 자동차 위치)를 출력해주기 위해 response 를 구현했습니다.

---
+++ src/main/java/racingcar/interfaces/car/CarResponse.java
@@ -0,0 +1,16 @@
+package racingcar.interfaces.car;
+
+import racingcar.domain.car.Car;
+
+public record CarResponse(
+        String carStatus
+) {
+
+    private final static String CAR_POSITION_INDEX = "-";
+    private final static String CAR_NAME_AND_POSITION_SEPARATOR = " : ";
+
+    public static CarResponse from(Car car) {
+        String carPosition = CAR_POSITION_INDEX.repeat(car.getCarPosition());
+        return new CarResponse(car.getCarName() + CAR_NAME_AND_POSITION_SEPARATOR + carPosition);
+    }
+}
---
+++ src/main/java/racingcar/interfaces/car/CarsResponse.java
@@ -0,0 +1,15 @@
+package racingcar.interfaces.car;
+
+import java.util.List;
+import racingcar.domain.car.Cars;
+
+public record CarsResponse(
+        List<CarResponse> carResponses
+) {
+    public static CarsResponse from(Cars cars) {
+        return new CarsResponse(cars.getCars().stream()
+                .map(CarResponse::from)
+                .toList());
+    }
+
+}
