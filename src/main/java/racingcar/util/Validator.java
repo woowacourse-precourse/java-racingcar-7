fe5ab0589a8b462f8ec034a11b00b669618adc42
KSJ0128 <ksj01128@naver.com>
2024-10-27 20:28:56 +0900

    refactor: validInput() 구현
    검증 메서드를 호출할 상위 메서드인 validInput()을 구현했습니다.
    해당 메서드 내부에서 validBlank(), validNumber(), validNameLength()를
    호출해 검증을 진행합니다.

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -1,9 +1,40 @@
 package racingcar.util;
 
+import racingcar.dto.RacingInputReq;
+import racingcar.message.ErrorMessage;
 import racingcar.model.Car;
+import racingcar.dto.RacingInfoDTO;
+
+import java.math.BigInteger;
 import java.util.List;
 
 public class Validator {
+
+    public RacingInfoDTO validInput(RacingInputReq racingInputReq) {
+        Validator validator = new Validator();
+
+        // 아무 값도 입력되지 않은 경우
+        if (validator.validBlank(racingInputReq.getCarNames(), racingInputReq.getRaceRounds())) {
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_ARG.getMessage());
+        }
+
+        Splitter separator = new Splitter(racingInputReq.getCarNames());
+
+        // 시도할 횟수로 수가 아닌 값이 입력된 경우
+        if (validator.validNumber(racingInputReq.getRaceRounds())) {
+            throw new IllegalArgumentException(ErrorMessage.NON_NUMBER.getMessage());
+        }
+
+        RacingInfoDTO racingInfoDTO = new RacingInfoDTO(separator.splitSeparator(), new BigInteger(racingInputReq.getRaceRounds()));
+
+        // 자동차 이름 길이가 5보다 큰 경우
+        if (validator.validNameLength(racingInfoDTO.getCarList())) {
+            throw new IllegalArgumentException(ErrorMessage.NAME_LENGTH.getMessage());
+        }
+
+        return racingInfoDTO;
+    }
+
     public Boolean validBlank(String carNames, String raceRounds) {
         return carNames.isBlank() || raceRounds.isBlank();
     }
