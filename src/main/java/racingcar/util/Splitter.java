f98920b0b0484c95aa79825062946f51d674fec2
KSJ0128 <ksj01128@naver.com>
2024-10-27 20:54:52 +0900

    refactor: Splitter 내 불필요한 필드 삭제 및 로직 개선
    Splitter 내에 필요치 않은 변수를 삭제 처리하고, 구분자로 자동차 이름을 분리하는 로직을 개선했습니다.
    기존에는 네 개의 함수를 사용해 자동차 이름을 분리한 List<Car>를 생성해 리턴하였습니다.
    현재는 하나의 함수에서 Java의 API를 활용해 로직을 개선한 상태입니다.

--- src/main/java/racingcar/util/Splitter.java
+++ src/main/java/racingcar/util/Splitter.java
@@ -1,40 +1,18 @@
 package racingcar.util;
 
+import racingcar.dto.RacingInputReq;
 import racingcar.model.Car;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Splitter {
-    private final String carNames;
-    private static final String COMMA = ",";
+    public List<Car> splitSeparator(RacingInputReq racingInputReq) {
+        final String COMMA = ",";
 
-    public Splitter(String carNames) {
-        this.carNames = carNames;
-    }
-
-    public List<Car> splitSeparator() {
-        List<String> splitCarNames = Arrays.stream(carNames.split(COMMA)).toList();
-        List<String> trimCarNames = trimCarNames(splitCarNames);
-        List<String> carNames = removeEmpty(trimCarNames);
-        return createCarList(carNames);
-    }
-
-    public List<String> trimCarNames(List<String> splitCarNames) {
-        return new ArrayList<>(splitCarNames.stream()
+        return Arrays.stream(racingInputReq.getCarNames().split(COMMA))
                 .map(String::trim)
-                .toList());
-    }
-
-    public List<String> removeEmpty(List<String> trimCarNames) {
-        trimCarNames.removeIf(String::isBlank);
-        return trimCarNames;
-    }
-
-    public List<Car> createCarList(List<String> carNames) {
-        return carNames.stream()
+                .filter(name -> !name.isBlank())
                 .map(Car::new)
                 .collect(Collectors.toList());
     }
