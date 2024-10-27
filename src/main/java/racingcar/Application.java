b93c0fbd347155b981bdea14ddedddaa2f2325c2
OhYeHyun <ohyhohyl@naver.com>
2024-10-27 21:47:41 +0900

    feat(Application): 레이싱 게임 실행 결과를 출력하는 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,18 +1,20 @@
 package racingcar;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
-
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Application {
     public static void main(String[] args) {
-        String carInput = readLine();
-        int moveCountInput = Integer.parseInt(readLine());
-        RacingService racingService = new RacingService(carInput, moveCountInput);
+        RacingIO.promptCarNamesInput();
+        String carNameInput = RacingIO.getInput();
 
+        RacingIO.promptMoveCountInput();
+        int moveCountInput = Integer.parseInt(RacingIO.getInput());
+
+        RacingService racingService = new RacingService(carNameInput, moveCountInput);
         racingService.startRaceGame();
-        String[] winner = racingService.getWinner();
 
-//        System.out.println(Arrays.toString(winner));
+        ArrayList<HashMap<String, Integer>> turnResults = racingService.getTurnResult();
+        RacingIO.promptTurnResult(turnResults);
     }
 }
