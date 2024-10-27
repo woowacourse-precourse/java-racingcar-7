e4724dba34fc9d46f9e3cf677f5b967926cadcb9
jae woo Jung <awfjol2008@gmail.com>
2024-10-27 16:14:31 +0900

    feat: 각 자동차들을 일괄적으로 랜덤하게 이동시키는 기능 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -2,10 +2,16 @@ package racingcar.domain;
 
 import java.util.List;
 
+import racingcar.util.RandomNumberPicker;
+
 public class Cars {
 	private final List<Car> cars;
 
 	public Cars(List<Car> cars) {
 		this.cars = cars;
+	}
+
+	public void moveEachCars() {
+		cars.forEach(car -> car.move(RandomNumberPicker.pickNumber()));
 	}
 }
