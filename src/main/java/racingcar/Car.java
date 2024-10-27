2c536ae8dfe93deba07e0a39567a160956131086
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-10-28 08:18:03 +0900

    feat(Car): 자동차 전진 기능 구현
    랜덤 값이 4 이상일 경우 전진
    전진시, Car의 moveCount + 1

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,15 +3,28 @@ package racingcar;
 // 시도횟수 입력 - 5,
 public class Car {
     private String name;
+    private int moveCount;
 
+    /**
+     * 자동차 전진 메서드
+     *  - 랜덤 값이 4 이상일 경우에만 전진
+     * */
+    public void move(int randomNum){
+        if(randomNum>=4){
+            this.moveCount += 1;
+        }
+    }
 
     public Car(String name){
         this.name = name;
+        this.moveCount = 0;
     }
 
     public String getName() {
         return name;
     }
 
-
+    public int getMoveCount() {
+        return moveCount;
+    }
 }
