a20d9e829ffa808ead24fa9a72c6441de2301e7e
sytTest <syt1128@naver.com>
2024-10-28 03:20:09 +0900

    :sparkles: (PaddockService) add calibrate()
    경기 참여 전 대기 장소에서 자동차 이름, 기름 규정, 엔진을 입력 받아 새로운 경주 자동차 객체로 반환하는 calibrate() 추가

--- src/main/java/racingcar/service/constant/RacingRules.java
+++ src/main/java/racingcar/service/constant/RacingRules.java
@@ -2,7 +2,12 @@ package racingcar.service.constant;
 
 public enum RacingRules {
 
-    CAR_NAME_LENGTH_LIMIT(5L);
+    CAR_NAME_LENGTH_LIMIT(5L),
+
+    BASIC_ENGINE_PERFORMANCE_REGULATION(1L),
+    BASIC_OIL_CONSUMPTION_REGULATION(1L),
+
+    MIN_LAP_COUNT(1L);
 
     private final Long status;
 
---
+++ src/main/java/racingcar/service/prep/PaddockService.java
@@ -0,0 +1,23 @@
+package racingcar.service.prep;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Engine;
+import racingcar.domain.Paddock;
+import racingcar.domain.RacingCar;
+import racingcar.domain.Registration;
+import racingcar.service.constant.RacingRules;
+
+public class PaddockService {
+
+    private final EngineService engineService = new EngineService();
+
+    private Car calibrate(String carName, Long oilProvision) {
+        Engine tunedEngine = engineService.tunedEngine(
+                RacingRules.BASIC_ENGINE_PERFORMANCE_REGULATION.status()
+                ,RacingRules.BASIC_OIL_CONSUMPTION_REGULATION.status());
+
+        return new RacingCar(carName, oilProvision, tunedEngine);
+    }
+}
