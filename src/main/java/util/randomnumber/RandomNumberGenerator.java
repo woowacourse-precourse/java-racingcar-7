4e5b12fc4cdd2dca1a5e7c0cf5572c0c56dc72ed
jjjjjoon <mjj7343@gmail.com>
2024-10-27 14:44:02 +0900

    feat(RandomNumberGenerator): canMove()를 통해 전진 조건 설정

--- src/main/java/util/randomnumber/RandomNumberGenerator.java
+++ src/main/java/util/randomnumber/RandomNumberGenerator.java
@@ -3,11 +3,17 @@ package util.randomnumber;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
+    private static final int MINIMUM_MOVEMENT_VALUE = 4;
     private static final int START_INCLUSIVE = 0;
     private static final int END_INCLUSIVE = 9;
 
     public int getRandomNumber() {
         int randomnumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
         return randomnumber;
+    }
+
+    public boolean canMove() {
+        int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+        return randomNumber >= MINIMUM_MOVEMENT_VALUE;
     }
 }
