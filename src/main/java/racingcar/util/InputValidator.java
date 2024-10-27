b67e701b76d0ff84d2a91a5c748c74240b090b9b
taek2222 <oot309@daum.net>
2024-10-27 17:28:12 +0900

    refactor(InputValidator): 중복 검증 인덴트 감소 및 로직 변경
    - 중복 검증을 삽입 기준이 아닌, 크기 비교 형식으로 변경해 가독성을 향상 시키고 인덴트를 감소했습니다.
    
    - if 문의 조건식을 메소드 분리해 가독성을 향상시켰습니다.

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -13,10 +13,13 @@ public class InputValidator {
     }
 
     public static void validateNoDuplicates(List<String> list) {
-        Set<String> unique = new HashSet<>();
-        for (String input : list) {
-            if (!unique.add(input))
-                throw new IllegalArgumentException();
+        Set<String> unique = new HashSet<>(list);
+        if (hasDuplicates(list, unique)) {
+            throw new IllegalArgumentException();
         }
+    }
+
+    private static boolean hasDuplicates(List<String> list, Set<String> unique) {
+        return unique.size() != list.size();
     }
 }
