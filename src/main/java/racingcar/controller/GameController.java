7740490019e1c6463487237b10a23b7ea5c2f536
wgrgwg <kyoeng14697@naver.com>
2024-10-27 17:35:14 +0900

    feat: Implement parsing feature to get car names
    Implemented feature to extract car names from user input string.

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,5 +1,7 @@
 package racingcar.controller;
 
+import java.util.Arrays;
+import java.util.List;
 import racingcar.view.GameView;
 
 public final class GameController {
@@ -11,5 +13,14 @@ public final class GameController {
 
     public void run() {
         String carNamesInput = gameView.getCarNamesInput();
+        List<String> carNameList = parseCarNames(carNamesInput);
+    }
+
+    private List<String> parseCarNames(String carNamesInput) {
+        List<String> carNameList;
+
+        carNameList = Arrays.asList(carNamesInput.split(","));
+
+        return carNameList;
     }
 }
