19ac5bbf0ae7c0a00c2def0ac843bb74803d3bf1
고경우 <dfdfg11@gmail.com>
2024-10-27 21:43:22 +0900

    feat : MoveHandler 추가 generateRandomNumber메서드 추가

---
+++ src/main/java/racingcar/MoveHandler.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class MoveHandler {
+
+
+    public int generateRandomNumber(){
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+}
