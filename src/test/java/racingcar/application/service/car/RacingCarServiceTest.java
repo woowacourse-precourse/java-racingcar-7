2362aa8a95c33b1dcae33eadb659458b47b27f75
jijongkwon <help3451@naver.com>
2024-10-28 01:43:25 +0900

    test(service) : RacingHistoryService 게임을 정상적으로 진행할 수 있다 테스트 진행

--- src/test/java/racingcar/application/service/car/RacingCarServiceTest.java
+++ src/test/java/racingcar/application/service/car/RacingCarServiceTest.java
@@ -1,9 +1,10 @@
 package racingcar.application.service.car;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.car.RacingCar;
 import racingcar.infrastructure.persistence.InMemoryRacingCarRepository;
-import racingcar.infrastructure.util.NameParser;
 
 import static org.junit.jupiter.api.Assertions.*;
 class RacingCarServiceTest {
@@ -15,7 +16,7 @@ class RacingCarServiceTest {
     public void before(){
         repository = InMemoryRacingCarRepository.getInstance();
         repository.clear();
-        racingCarService = new RacingCarService(new NameParser());
+        racingCarService = new RacingCarService();
     }
 
     @Test
@@ -25,5 +26,17 @@ class RacingCarServiceTest {
         racingCarService.inputCarName(names);
 
         assertEquals(repository.getRacingCars().getFirst().getName(), "name");
+    }
+
+    @Test
+    void 게임을_정상적으로_진행할_수_있다() {
+        racingCarService.inputCarName("car1,car2");
+        racingCarService.inputTryNumber(3);
+
+        racingCarService.startGame();
+
+        for (RacingCar car : repository.getRacingCars()) {
+            assertTrue(car.getDistance() >= 0);
+        }
     }
 }
