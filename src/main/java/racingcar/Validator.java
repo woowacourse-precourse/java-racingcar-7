4aa656d1019aa3e3394e8d684b42172a9c94942a
pil-go <pilbongx@naver.com>
2024-10-27 10:09:40 +0900

    feat(Validator): 시도 횟수 입력값 유효성 검사 기능 추가

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import java.util.List;
+
 public class Validator {
     private static final String ERROR_MESSAGE_INPUT_EMPTY = "자동차 이름을 입력해주세요.";
+    private static final String ERROR_MESSAGE_ROUND_FORMAT = "숫자만 입력 가능해요.";
+    private static final String ERROR_MESSAGE_ROUND_RANGE = "자연수만 입력 가능해요";
 
     public void inputSting(String input) {
         if (input.isEmpty()) {
@@ -9,4 +13,22 @@ public class Validator {
         }
     }
 
+    public void CheckCarList(List<Car> carList) {
+        for (Car car : carList) {
+
+        }
+    }
+
+    public void checkRound(String input) {
+        int round;
+
+        try {
+            round = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_ROUND_FORMAT);
+        }
+        if (round <= 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_ROUND_RANGE);
+        }
+    }
 }
