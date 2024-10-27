c439f9227779617b737df1ac27a4c171b7a040c0
Nahye Choi <skgp0628@naver.com>
2024-10-27 22:20:48 +0900

    refactor: 클래스명 변경 및 리턴값 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,7 +6,7 @@ import racingcar.domain.RacingCarGame;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RacingCarGame.startRacingCarGame();
+        RacingCarGame.start();
         Console.close();
     }
 }
--- src/main/java/racingcar/domain/RacingCarGame.java
+++ src/main/java/racingcar/domain/RacingCarGame.java
@@ -7,11 +7,11 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-public class RacingCarGame { //게임 컨트롤러
+public class RacingCarGame {
     static Map<String, Integer> cars = new LinkedHashMap<>();
     static int tryNumber = 0;
 
-    public static void startRacingCarGame() {
+    public static void start() {
         cars = Cars.registerCars();
         inputTryNumber();
         OutputView.promptMoveCar();
@@ -24,12 +24,11 @@ public class RacingCarGame { //게임 컨트롤러
         tryNumber = InputView.inputTryNumber();
     }
 
-    public static Map<String, Integer> carRacing(Map<String, Integer> cars, int tryNumber) {
+    public static void carRacing(Map<String, Integer> cars, int tryNumber) {
         for (int i = 0; i < tryNumber; i++) {
             Cars.moveCar(cars);
             Cars.carsPostion(cars);
         }
-        return cars;
     }
 
     private static void winner(Map<String, Integer> cars) {
