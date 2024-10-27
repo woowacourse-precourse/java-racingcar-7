adab7d3cd2527322da47d93658414f1fafb1433a
o-jeong <ljj020304@gmail.com>
2024-10-27 14:57:23 +0900

    feat: 경주 진행상황 및 결과 출력 클래스 구현

---
+++ src/main/java/racingcar/View/OutputView.java
@@ -0,0 +1,16 @@
+package racingcar.View;
+
+import java.util.List;
+import racingcar.domain.Car;
+
+public class OutputView {
+    public void printRaceStatus(List<Car> cars){
+        for (Car car : cars) {
+            System.out.println(car.getCarName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+    public void printWinners(List<String> winners){
+        System.out.println("우승자: " + String.join(",", winners));
+    }
+}
