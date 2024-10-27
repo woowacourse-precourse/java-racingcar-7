23f0ed8cb84dbd950c26e6b370e84d8771826325
pakaheo <k70923@konkuk.ac.kr>
2024-10-28 00:45:50 +0900

    feat : Create test about InputView, ResultView

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,2 +1,36 @@
-package racingcar.view;public class InputViewTest {
+package racingcar.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class InputViewTest {
+
+    @BeforeEach
+    void 콘솔_닫기() {
+        Console.close();
+    }
+
+    @Test
+    void 자동차_이름_입력() {
+        String inputCarNames = "pobi,nana,jun";
+        System.setIn(new ByteArrayInputStream(inputCarNames.getBytes()));
+
+        String carNames = InputView.inputCarNames();
+
+        assertThat(carNames).isEqualTo("pobi,nana,jun");
+    }
+
+    @Test
+    void 시도_횟수_입력() {
+        String inputTryCount = "5";
+        System.setIn(new ByteArrayInputStream(inputTryCount.getBytes()));
+
+        String tryCount = InputView.inputTryCount();
+
+        assertThat(tryCount).isEqualTo("5");
+    }
 }
--- src/test/java/racingcar/view/ResultViewTest.java
+++ src/test/java/racingcar/view/ResultViewTest.java
@@ -1,2 +1,40 @@
-package racingcar.view;public class ResultViewTest {
+package racingcar.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ResultViewTest {
+
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @BeforeEach
+    void tearDown() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void 실행_결과_문구_출력() {
+        ResultView.promptExecutionResult();
+
+        assertThat(outputStream.toString().trim()).isEqualTo("실행 결과");
+    }
+
+    @Test
+    void 최종_우승자_출력() {
+        List<String> winners = List.of("pobi", "nana");
+        ResultView.showWinners(winners);
+
+        assertThat(outputStream.toString().trim()).isEqualTo("최종 우승자 : pobi, nana");
+    }
 }
