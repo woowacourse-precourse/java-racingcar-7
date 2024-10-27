cabb6dcd186b9cc2e2c505b4d394a610771d52b0
woongjee <jeewoong7753@naver.com>
2024-10-28 01:46:15 +0900

    fix(Application):오류 이중 처리 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,17 +7,13 @@ import java.sql.SQLOutput;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        try{
-            String inputNames = Console.readLine();
-            String[] carNames = parseCarNames(inputNames);
+        String inputNames = Console.readLine();
+        String[] carNames = parseCarNames(inputNames);
 
-            int rounds = Integer.parseInt(Console.readLine());
+        int rounds = Integer.parseInt(Console.readLine());
 
-            Race race = new Race(carNames, rounds);
-            race.start();
-        }catch(IllegalArgumentException e){
-            System.out.println(e.getMessage());
-        }
+        Race race = new Race(carNames, rounds);
+        race.start();
     }
     private static String[] parseCarNames(String inputNames){
         return inputNames.split(",");
