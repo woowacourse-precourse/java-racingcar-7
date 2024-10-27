c1bd296db7931ec10102eff37c3f7328dcff6060
jungsehui <jungsehui@naver.com>
2024-10-28 02:42:13 +0900

    chore(RacingGameController.java): 패키지 구조 변경으로 인한 import문 변경

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,7 +1,7 @@
 package racingcar.controller;
 
-import racingcar.Message;
 import racingcar.domain.Car;
+import racingcar.global.Message;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
