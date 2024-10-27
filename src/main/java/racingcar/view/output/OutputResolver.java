0138296876827fe718d5d4758caafb766d8f88fc
haebyun <bhk4392@gmail.com>
2024-10-27 18:55:32 +0900

    feat(OutputResolver): 개별 자동차 상태 포맷팅 기능 추가
    - `formatCarRoundState` 메서드를 통해 자동차 이름과 이동 횟수에 따른 상태를 문자열로 변환

--- src/main/java/racingcar/view/output/OutputResolver.java
+++ src/main/java/racingcar/view/output/OutputResolver.java
@@ -1,5 +1,7 @@
 package racingcar.view.output;
 
+import racingcar.dto.response.CarRoundStateDTO;
+
 final class OutputResolver {
     private static final String WINNER_MESSAGE_PREFIX = "최종 우승자 : ";
     private static final String NAME_MOVE_COUNT_SEPARATOR = " : ";
@@ -7,5 +9,12 @@ final class OutputResolver {
     private static final String LINE_SEPARATOR = System.lineSeparator();
 
     private OutputResolver() {
+    }
+
+    private static String formatCarRoundState(CarRoundStateDTO carRoundStateDTO) {
+        return String.format("%s%s%s",
+                carRoundStateDTO.name(),
+                NAME_MOVE_COUNT_SEPARATOR,
+                "-".repeat(carRoundStateDTO.moveCount()));
     }
 }
