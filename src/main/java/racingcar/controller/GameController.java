4e29b5a96e7539b9e38d1102c75994bc8ae5a3f8
DevBigOne <a62387272@gmail.com>
2024-10-27 23:09:42 +0900

    [feat] 자동차 이름 분리 기능 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -9,8 +9,14 @@ public class GameController {
         this.view = new View();
     }
 
+    public String[] split(String userInput) {
+        return userInput.split(",");
+    }
+
     public void run() {
         view.Message();
         String userInput = view.userInput();
+
+        String[] splitUserInput = split(userInput);
     }
 }
