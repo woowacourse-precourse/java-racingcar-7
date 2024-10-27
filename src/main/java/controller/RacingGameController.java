1e7785dc4d8228d2c311590bfa802c0e117d01fb
aaahyunseo <hs030608@naver.com>
2024-10-27 17:04:22 +0900

    feat(RacingGameController): view와 service를 연결하는 controller 생성

---
+++ src/main/java/controller/RacingGameController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import model.Car;
+import service.RacingGameService;
+import view.InputView;
+import view.PlayView;
+import view.ResultView;
+
+import java.util.List;
+
+public class RacingGameController {
+    private final InputView inputView = new InputView();
+    private final PlayView playView = new PlayView();
+    private final ResultView resultView = new ResultView();
+    private final RacingGameService racingGameService = new RacingGameService();
+
+    public void play(){
+        String names = inputView.carNameInput();
+        List<Car> cars = racingGameService.splitCarsName(names);
+        int attempts = inputView.numberOfAttemptsInput();
+
+        for (int i = 0; i < attempts; i++) {
+            racingGameService.racing(cars);
+            playView.playResult(cars);
+        }
+
+        List<String> winners = racingGameService.findWinners(cars);
+        resultView.getWinners(winners);
+    }
+}
