26c7197db7ca681b666d6821d8ec688e7f1188e0
min-0 <dhe08133@naver.com>
2024-10-27 19:41:42 +0900

    feat: 예외 처리 및 메서드 추출
    사용자 입력값 예외 처리 및 기능 메서드 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,13 +9,34 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
+        String[] carList = checkInput(input);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         int num = Integer.parseInt(Console.readLine());
 
         System.out.println("\n실행 결과");
+        runRacing(num, carList);
+    }
+
+    static public String[] checkInput(String input) throws IllegalArgumentException {
+        if(input.isEmpty()) {
+            throw new IllegalArgumentException("경주에 참여할 자동차를 입력해주세요. 예) pobi,woni,jun");
+        }
 
         String[] carList = input.split(",");
+        if(carList.length < 1) {
+            throw new IllegalArgumentException("경주에 참여할 자동차를 입력해주세요. 예) pobi,woni,jun");
+        }
+
+        for(String car : carList) {
+            if(car.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력해주세요. 예) pobi,woni,jun");
+            }
+        }
+        return carList;
+    }
+
+    static public void runRacing(int num, String[] carList) {
         int[] carIndex = new int[carList.length];
 
         for (int i = 0; i < num; i++) {
@@ -25,12 +46,16 @@ public class Application {
                     carIndex[k]++;
                 }
             }
+
             for (int j = 0; j < carList.length; j++) {
                 System.out.println(carList[j] + " : " + "-".repeat(carIndex[j]));
             }
             System.out.println();
         }
+        maxDistance(carIndex, carList);
+    }
 
+    static public void maxDistance(int[] carIndex, String[] carList) {
         int maxDistance = 0;
         for (int distance : carIndex) {
             maxDistance = Math.max(maxDistance, distance);
@@ -42,7 +67,10 @@ public class Application {
                 winners.add(carList[i]);
             }
         }
+        printWinner(winners);
+    }
 
+    static public void printWinner(List<String> winners) {
         System.out.println("최종 우승자  :  " + String.join(", ", winners));
     }
 }
