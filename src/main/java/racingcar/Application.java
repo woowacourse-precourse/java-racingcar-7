5a13140bcba57ca7e8abf342d28d812cedac3d71
yeseong33 <yeseong.oh@kt.com>
2024-10-27 15:37:57 +0900

    feat: 입력 유효성 검증 기능
    - 이름을 , 로 구분해서 받음
    - 공백이 없는지 확인
    - 길이가 5 이하인지 확인
    - n이 양수인지 확인

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,15 @@
 package racingcar;
 
 import racingcar.controller.RacingCarController;
+import racingcar.service.RacingCarService;
+import racingcar.view.InstructionView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RacingCarController controller = new RacingCarController();
+        InstructionView instructionView = new InstructionView();
+        RacingCarService racingCarService = new RacingCarService();
+        RacingCarController controller = new RacingCarController(instructionView, racingCarService);
         controller.run();
     }
 }
--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,22 +1,27 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.service.RacingCarService;
 import racingcar.view.InstructionView;
 
 public class RacingCarController {
 
     InstructionView instructionView;
+    RacingCarService racingCarService;
 
-    public RacingCarController() {
-        instructionView = new InstructionView();
+    public RacingCarController(InstructionView instructionView, RacingCarService racingCarService) {
+        this.instructionView = instructionView;
+        this.racingCarService = racingCarService;
     }
 
     public void run() {
 
         instructionView.printInstruction("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String names = Console.readLine();
+        racingCarService.validateName(names);
         instructionView.printInstruction("시도할 횟수는 몇 회인가요?");
         String raceCount = Console.readLine();
+        racingCarService.validateRaceCount(raceCount);
 
     }
 
