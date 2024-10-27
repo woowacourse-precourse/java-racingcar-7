4d16727164a69d75e7270fad4295e105058c9707
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:43:19 +0900

    :sparkles: feat(Cars): 입력된 자동차 이름이 0개일 경우, 예외를 발생시킨다

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,14 +1,25 @@
 package racingcar.model;
 
+import static racingcar.message.ErrorMessages.UNPROVIDED_NAMES;
+
 import java.util.List;
+import racingcar.exception.InvalidInputException;
 
 public class Cars {
     private final List<Car> cars;
 
     public Cars(String inputCarNames) {
+        String inputNames = inputCarNames.trim();
+        validateUnprovidedNames(inputNames);
     }
 
     public List<Car> getCars() {
         return cars;
+    }
+
+    private void validateUnprovidedNames(String names) {
+        if (names == null || names.isEmpty()) {
+            throw new InvalidInputException(UNPROVIDED_NAMES);
+        }
     }
 }
