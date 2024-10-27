883902b1557cdc43d0d8d1c0dade7aced3ac1b36
Park, Sojeong <sojeong2101@gmail.com>
2024-10-27 17:45:25 +0900

    test($CarNameExtractorTest): 이름 문자열 예외 처리 테스트 구현
    1. null 예외 처리하는 checkNull() 메서드 테스트
    2. 빈 문자열, 공백 예외 처리하는 checkBlank()
    메서드 테스트

--- src/test/java/racingcar/CarNameExtractorTest.java
+++ src/test/java/racingcar/CarNameExtractorTest.java
@@ -5,13 +5,13 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class CarNameExtractorTest {
+class CarNameExtractorTest {
 
     @Test
     void 이름문자열에서_이름_추출해서_배열_반환() {
         // give
-        CarNameExtractor carNameExtractor = new CarNameExtractor();
         String carNames = "소정,pobi,woni,jun";
+        CarNameExtractor carNameExtractor = new CarNameExtractor(carNames);
 
         List<String> expectedCarNameList = new ArrayList<>();
         expectedCarNameList.add("소정");
@@ -24,5 +24,35 @@ public class CarNameExtractorTest {
 
         // then
         Assertions.assertThat(actualCarNameList).isEqualTo(expectedCarNameList);
+    }
+
+    @Test
+    void 이름문자열이_Null인지_검사() {
+        String carNames = null;
+
+        Assertions.assertThatThrownBy(() -> {
+                    new CarNameExtractor(carNames);
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("null은 입력할 수 없습니다.");
+    }
+
+    @Test
+    void 이름문자열이_빈문자열인지_검사() {
+        String carNames = "";
+
+        Assertions.assertThatThrownBy(() -> {
+                    new CarNameExtractor(carNames);
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("빈 문자열과 공백은 입력할 수 없습니다.");
+    }
+
+    @Test
+    void 이름문자열이_공백인지_검사() {
+        String carNames = " ";
+
+        Assertions.assertThatThrownBy(() -> {
+                    new CarNameExtractor(carNames);
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("빈 문자열과 공백은 입력할 수 없습니다.");
     }
 }
