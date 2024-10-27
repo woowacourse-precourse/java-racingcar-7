13b39fe214e76ebd7b1ac264fceb92bd74a6269c
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-10-27 22:13:52 +0900

    refactor: 자동차 이름이 빈칸인지 검사하는 함수 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -16,6 +16,9 @@ public class Car {
         if (name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 최대 5자까지만 입력 가능합니다.");
         }
+        if(name.isBlank()){
+            throw new IllegalArgumentException("자동차 이름은 빈칸일 수 없습니다.");
+        }
     }
 
     public void setDistance(int randomNumber) {
