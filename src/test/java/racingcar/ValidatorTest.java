68158c9f8cbdc91d54d4614f99b8876763733173
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-27 22:58:32 +0900

    test(ValidatorTest): add 시도 횟수에 대한 숫자 유효성 검사 테스트
    - 시도 횟수가 숫자가 아닌 경우 예외가 발생하는 테스트 케이스 추가
    - 간단한 오타 수정 포함

--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -29,7 +29,7 @@ class ValidatorTest {
     }
 
     @Test
-    @DisplayName("자동차 이름을 1개 이상 입력하지 않으면 예외 발생한다.")
+    @DisplayName("자동차 이름을 1개 이상 입력하지 않으면 예외가 발생한다.")
     void 자동차입력_최소1개() {
         // given
         UserInput userInput = new UserInput("", ATTEMPT_COUNT);
@@ -44,7 +44,7 @@ class ValidatorTest {
     }
 
     @Test
-    @DisplayName("자동차 이름에 공백을 포함하면 예외 발생한다.")
+    @DisplayName("자동차 이름에 공백을 포함하면 예외가 발생한다.")
     void 자동차입력_공백() {
         // given
         UserInput userInput = new UserInput(CAR_NAMES_WITH_SPACE, ATTEMPT_COUNT);
@@ -59,7 +59,7 @@ class ValidatorTest {
     }
 
     @Test
-    @DisplayName("자동차 이름을 5글자 이상 입력하면 예외 발생한다.")
+    @DisplayName("자동차 이름을 5글자 이상 입력하면 예외가 발생한다.")
     void 자동차입력_5글자이상() {
         // given
         UserInput userInput = new UserInput(CAR_NAME_OVER_5,  ATTEMPT_COUNT);
@@ -74,7 +74,7 @@ class ValidatorTest {
     }
 
     @Test
-    @DisplayName("시도 횟수가 1 이상 입력하지 않으면 이면 예외 발생한다.")
+    @DisplayName("시도 횟수가 1 이상 입력하지 않으면 예외가 발생한다.")
     void 시도횟수_최소1() {
         // given
         UserInput userInput = new UserInput(CAR_NAMES, "0");
@@ -86,5 +86,20 @@ class ValidatorTest {
         });
 
         assertEquals("시도 횟수는 1 이상 입력해야 합니다.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("시도 횟수가 숫자가 아니라면 예외가 발생한다.")
+    void 시도횟수_숫자만가능() {
+        // given
+        UserInput userInput = new UserInput(CAR_NAMES, "shouldBeNumber");
+        Validator validator = new Validator();
+
+        // when & then
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            validator.validateInput(userInput);
+        });
+
+        assertEquals("시도 횟수는 숫자로 입력해야 합니다.", exception.getMessage());
     }
 }
