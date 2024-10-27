47ecd0887ca3cae3f5bc4fce3a6b6b77d7811b2c
heehunjung <slrkqk2@naver.com>
2024-10-28 02:08:59 +0900

    feat(InputView): 입력 처리 메서드 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,16 +7,19 @@ import racingcar.utils.InputValidator;
 
 public class InputView {
     public static final String SEPARATOR = ",";
+    public static final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
     private InputView(){
     }
 
-    public static List<String> getName() {
+    public static List<String> getCarNames() {
 
-        System.out.println("~~~");
+        System.out.println(INPUT_MESSAGE);
+
         String input = Console.readLine();
         InputValidator.nameValidator(input);
 
-       return InputParser.getNames(input);
+        return InputParser.getNames(input);
     }
+
 }
