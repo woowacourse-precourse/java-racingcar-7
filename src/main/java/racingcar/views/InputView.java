7a557b212c2b2317bf576875b05ddbc4fb4c2a23
sangxxjin <sj_0368@naver.com>
2024-10-27 23:04:05 +0900

    feat: 시도할 횟수 입력 메세지 출력
    본문:
    시도할 횟수 입력 메세지 출력 메서드 구현했습니다.
    메세지는 상수화 했습니다.

--- src/main/java/racingcar/views/InputView.java
+++ src/main/java/racingcar/views/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String INPUT_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRY_COUNT_PROMPT = "시도할 횟수는 몇 회인가요?";
     private static Scanner sc = new Scanner(System.in);
 
     public static String readStringInput() {
@@ -19,4 +20,7 @@ public class InputView {
         System.out.println(INPUT_PROMPT);
     }
 
+    public static void displayTryCountPrompt() {
+        System.out.println(TRY_COUNT_PROMPT);
+    }
 }
