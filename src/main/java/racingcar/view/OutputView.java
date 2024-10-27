26e44cc765d8067e287740a64a545712490cd79d
ariimo <159447466+ariimo@users.noreply.github.com>
2024-10-27 22:39:54 +0900

    feat(view): 각 자동차의 현재 위치 출력 기능 추가
    - printCurrentLocationOf() 생성
    - printEndOfOneRace() 생성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,19 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+
 public class OutputView {
 
     public void printStartMessage() {
         System.out.println("실행 결과");
+    }
+
+    public void printCurrentLocationOf(Car car) {
+        final String ONE_STEP = "-";
+        System.out.println(car.getName() + " : " + ONE_STEP.repeat(car.getCurrentLocation()));
+    }
+
+    public void printEndOfOneRace() {
+        System.out.println();
     }
 }
