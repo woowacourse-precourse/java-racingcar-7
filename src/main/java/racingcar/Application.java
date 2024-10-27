350d3b464c3d352bd780f52fed0616b36f4f89c9
wvssm <neoramiregi@gmail.com>
2024-10-27 23:54:01 +0900

    feat(Application): 레이싱 게임을 실행점 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,15 +1,10 @@
 package racingcar;
 
 import racingcar.controller.RacingGameController;
-import racingcar.service.InputService;
-import racingcar.service.RacingGameService;
 
 public class Application {
     public static void main(String[] args) {
-        RacingGameService racingGameService = new RacingGameService();
-        InputService inputService = new InputService();
-
-        RacingGameController racingGameController = new RacingGameController(racingGameService,inputService);
+        RacingGameController racingGameController = new RacingGameController();
         racingGameController.run();
     }
 }
