e8f8da7716780bc4d96ffd3f12eca93a7b5372bd
smyeongL <sangmyeonglee123@gmail.com>
2024-10-28 03:11:55 +0900

    feat(application): call input handlers and run game logic

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,40 @@
 package racingcar;
 
+import racingcar.dto.CarNamesInputHandler;
+import racingcar.dto.GameRoundInputHandler;
+import racingcar.message.Message;
+import racingcar.model.RacingCar;
+import racingcar.model.RacingCars;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // Car Names
+        CarNamesInputHandler carNamesInputHandler = new CarNamesInputHandler();
+        carNamesInputHandler.getUserResponse();
+        List<String> carNames = carNamesInputHandler.validateUserResponse();
+        // Game Round
+        GameRoundInputHandler gameRoundInputHandler = new GameRoundInputHandler();
+        gameRoundInputHandler.getUserResponse();
+        int gameRound = gameRoundInputHandler.validateUserResponse();
+
+        List<RacingCar> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new RacingCar(carName));
+        }
+        RacingCars racingCars = new RacingCars(cars);
+
+        System.out.println(Message.EXECUTION_MESSAGE);
+        for(int i = 0; i < gameRound; i++) {
+            racingCars.moveCars();
+            System.out.println(racingCars.toString());
+        }
+
+        System.out.print(Message.WINNER_MESSAGE);
+        System.out.println(racingCars.getWinners());
+
     }
+
 }
