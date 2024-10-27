4d72216287b4a0c4c24ec2dd95ef6e9b395205a3
Park - Jeonghwan <990706leo@gmail.com>
2024-10-28 07:36:02 +0900

    feat(Game): 자동차 경주를 진행할 수 있다

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -1,11 +1,20 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Game {
 
-    private List<Car> cars = new ArrayList<>();
+    private final List<Car> cars;
+    private final Map<Car, String> results;
+
+    public Game() {
+        this.cars = new ArrayList<>();
+        this.results = new HashMap<>();
+    }
 
     public List<Car> createCars(List<String> names) {
         validateDuplicated(names);
@@ -14,6 +23,25 @@ public class Game {
         }
 
         return cars;
+    }
+
+    public Map<Car, String> play(List<String> names) {
+        createCars(names);
+        for (Car car : cars) {
+            int randNum = Randoms.pickNumberInRange(0, 9);
+            car.move(randNum >= 4);
+        }
+        return judge();
+    }
+
+    private Map<Car, String> judge() {
+        cars.stream()
+                .filter(car -> car.getPosition() == cars.stream()
+                        .mapToInt(Car::getPosition)
+                        .max()
+                        .orElse(0))
+                .forEach(car -> results.put(car, car.getName()));
+        return results;
     }
 
     private void validateDuplicated(List<String> names) {
