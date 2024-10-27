e75113be83204ead2cd71692578f771b8b892fbe
yush <yush2202@gmail.com>
2024-10-28 02:01:33 +0900

    feat($ Constants): 애플리케이션 상수 추가
    애플리케이션에서 사용될 상수 추가

--- src/main/java/racingcar/constants/Constants.java
+++ src/main/java/racingcar/constants/Constants.java
@@ -5,5 +5,14 @@ public class Constants {
     public static final String DASH = "-";
     public static final String COLON = " : ";
     public static final String ENTER = "\n";
+    public static final String COMMA_SPACE = ", ";
 
+    public static final int MIN_TRY_NUMBER = 0;
+    public static final int MAX_NAME_LENGTH = 5;
+    public static final int ONE_STEP = 1;
+    public static final int ADVANCE_LIMIT = 4;
+    public static final int MIN_RANDOM_NUMBER = 0;
+    public static final int MAX_RANDOM_NUMBER = 9;
+    public static final int START_INDEX = 0;
+    public static final int TRIM_LENGTH = 2;
 }
