66cd416ba5540a6bdd7aaebe83038b58b99623b8
ksundev <kshk08@hanyang.ac.kr>
2024-10-27 19:22:10 +0900

    test(CarTest): 자동차 위치 얻기 및 전진 기능 테스트 코드 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -10,4 +10,20 @@ class CarTest {
         Car car = new Car("pobi");
         assertThat(car.getName()).isEqualTo("pobi");
     }
+
+    @Test
+    void 자동차_위치_얻기_테스트() {
+        Car car = new Car("pobi");
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    void 자동차_전진_테스트() {
+        Car car = new Car("pobi");
+        int initialPosition = car.getPosition();
+
+        car.move();
+
+        assertThat(car.getPosition()).isEqualTo(initialPosition + 1);
+    }
 }
