068a7e91874d3639954682c8d723338c0d583563
jody816 <81346079+jody816@users.noreply.github.com>
2024-10-28 00:45:55 +0900

    feat: 모델 구현

--- src/main/java/racingcar/application/model/Car.java
+++ src/main/java/racingcar/application/model/Car.java
@@ -2,4 +2,23 @@ package racingcar.application.model;
 
 public class Car {
 
+  private final String name;
+  private int forward;
+
+  public Car(String name) {
+    this.name = name;
+    this.forward = 0;
+  }
+
+  public void move() {
+    this.forward++;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getForward() {
+    return forward;
+  }
 }
