de4038d935da08f092f82adb7874307144da38a6
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-10-27 14:11:06 +0900

    feat: 자동차 이름이 숫자로만 구성되었을 때, 예외를 발생해라
    숫자 사이에 중복이 들어간 경우에도 예외가 발생합니다.

--- src/main/java/racingcar/car/CarName.java
+++ src/main/java/racingcar/car/CarName.java
@@ -10,6 +10,7 @@ public class CarName {
     public CarName(final String rawContent) {
         String content = rawContent.strip();
         validateContentLength(content);
+        validateContent(content);
         this.content = content;
     }
 
@@ -28,6 +29,23 @@ public class CarName {
         if (content.length() < MIN_LENGTH) {
             throw new IllegalArgumentException("이름은 " + MIN_LENGTH + "자 이상이어야 합니다.");
         }
+    }
+
+    private void validateContent(final String content) {
+        if (isOnlyContainNumber(content)) {
+            throw new IllegalArgumentException("이름은 숫자로만 구성될 수 없습니다. 최소 1자 이상의 문자를 포함해주세요.");
+        }
+    }
+
+    private boolean isOnlyContainNumber(final String content) {
+        int countOfNumber = 0;
+        String removedWhiteSpace = content.replace(" ", "");
+        for (char c : removedWhiteSpace.toCharArray()) {
+            if (c >= '1' && c <= '9') {
+                countOfNumber++;
+            }
+        }
+        return removedWhiteSpace.length() == countOfNumber;
     }
 
     public String getContent() {
