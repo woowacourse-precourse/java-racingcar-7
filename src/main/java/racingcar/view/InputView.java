f70508f72e7af11c535dee28f312824342b844f5
DongYeop <s01054830455@gmail.com>
2024-10-27 15:07:44 +0900

    feat: 연속된 구분자 입력 예외처리
    - 구분자를 연속해서 입력한 경우 예외가 발생한다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -11,16 +11,16 @@ public class InputView {
 
     public List<String> inputCarNames() {
         String carNames = Console.readLine();
-
         validateCarNames(carNames);
 
         String[] splitCarNames = carNameSplit(carNames);
+        validateDelimiter(splitCarNames);
+
         return Convertor.toList(splitCarNames);
     }
 
     public int inputRacingCount() {
         String racingCount = Console.readLine();
-
         validateRacingCount(racingCount);
 
         return toInt(racingCount);
@@ -30,13 +30,19 @@ public class InputView {
         validateInput(carNames, "자동차 이름을 입력해야 합니다.");
     }
 
+    private void validateDelimiter(String[] splitCarNames) {
+        for (String splitCarName : splitCarNames) {
+            validateInput(splitCarName, "구분자를 연속해서 입력할 수 없습니다.");
+        }
+    }
+
     private void validateRacingCount(String racingCount) {
         validateInput(racingCount, "시도 횟수를 입력해야 합니다.");
     }
 
-    private void validateInput(String input, String emptyMessage) {
+    private void validateInput(String input, String exceptionMessage) {
         if (InputValidation.validateNullOrEmpty(input)) {
-            throw new IllegalArgumentException(emptyMessage);
+            throw new IllegalArgumentException(exceptionMessage);
         }
         if (InputValidation.validateBlank(input)) {
             throw new IllegalArgumentException(NOT_CONTAIN_BLANK_MESSAGE);
