c30dec82eae61cfeec5df6dc2d195ce9efe204d0
Seongmo Ahn <djgood780@gmail.com>
2024-10-27 11:20:12 +0900

    refactor(CarNameValidator): inputMoreThanTwo 메서드 로직 분리
    - 기존 inputMoreThanTwo 메서드에서 split()을 통해 리스트를 생성하고
    리스트의 크기가 2개 이상인지 검사 하는 부분을 isMoreThanTwo 메서드로 분리

--- src/main/java/racingcar/CarNameValidator.java
+++ src/main/java/racingcar/CarNameValidator.java
@@ -1,21 +1,24 @@
 package racingcar;
 
 public class CarNameValidator {
-    public void separateByComma(String carNames){
+    public void separateByComma(String carNames) {
         if (!isCommaSeparated(carNames)) {
             throw new IllegalArgumentException("쉼표(,)로 구분하여 자동차 이름들을 입력해주세요");
         }
     }
 
-    public void inputMoreThanTwo(String carNames){
-        String[] carNameList = carNames.split(",");
-        if(carNameList.length < 2) {
+    public void inputMoreThanTwo(String carNames) {
+        if (!isMoreThanTwo(carNames)) {
             throw new IllegalArgumentException("자동차 이름을 두 개 이상으로 입력해주세요");
         }
     }
 
-    private boolean isCommaSeparated(String carNames){
+    private boolean isCommaSeparated(String carNames) {
         return carNames.matches("^[a-zA-Z0-9]+(,[a-zA-Z0-9]+)*$");
     }
 
+    private boolean isMoreThanTwo(String carNames) {
+        String[] carNameList = carNames.split(",");
+        return carNameList.length > 2;
+    }
 }
