45e11b79c6185c659b2239aabc8615a751bb7ad8
whatsgoodg <rlwjddl1596@naver.com>
2024-10-27 20:12:38 +0900

    feat(ExceptionMessage): ExceptionMessage 클래스 메세지 추가
    추가 내용
    - 중복된 차 이름이 들어왔을 때 사용할 예외 메세지 추가

--- src/main/java/racingcar/front/exception/message/ExceptionMessage.java
+++ src/main/java/racingcar/front/exception/message/ExceptionMessage.java
@@ -2,7 +2,8 @@ package racingcar.front.exception.message;
 
 public enum ExceptionMessage {
     INVALID_CAR_NAME("자동차 이름은 1글자 이상, 5글자 이하여야 하며 쉼표와 공백은 허용되지 않습니다."),
-    INVALID_PLAY_COUNT("시도 횟수는 양의 정수값만 입력 가능합니다.");
+    INVALID_PLAY_COUNT("시도 횟수는 양의 정수값만 입력 가능합니다."),
+    DUPLICATED_CAR_NAME("중복된 차 이름은 허용되지 않습니다.");
     private final String message;
 
     ExceptionMessage(String message) {
