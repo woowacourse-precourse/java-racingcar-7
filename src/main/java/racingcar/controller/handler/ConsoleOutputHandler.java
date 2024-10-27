1392168623c528e698bf814509393004f2f5830b
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2024-10-27 21:31:32 +0900

    refactor(controller): formatter를 handler로 위임하는 방식으로 변경한다
    기존 controller에서 formatter를 이용하여 value를 변환해 handler로 반환하는 방식에서 formatter를 handler로 위임하여 controller의 책임을 줄인다.

--- src/main/java/racingcar/controller/handler/ConsoleOutputHandler.java
+++ src/main/java/racingcar/controller/handler/ConsoleOutputHandler.java
@@ -1,10 +1,29 @@
 package racingcar.controller.handler;
 
-import racingcar.dto.RaceResultDTO;
+import racingcar.controller.format.RaceResultFormatter;
+import racingcar.domain.race.RaceResult;
 
 public class ConsoleOutputHandler {
 
-    public void displayResult(RaceResultDTO raceResultDTO) {
-        System.out.println(raceResultDTO.convertWinnersToString());
+    private final RaceResultFormatter resultFormatter;
+
+    public ConsoleOutputHandler(RaceResultFormatter resultFormatter) {
+        this.resultFormatter = resultFormatter;
+    }
+
+    public void displayResults(RaceResult raceResult) {
+        displayRaceProgress(raceResult);
+        displayWinners(raceResult);
+    }
+
+    private void displayRaceProgress(RaceResult raceResult) {
+        String raceProgress = resultFormatter.convertRaceProgressToString(raceResult.getRaces());
+        System.out.print(raceProgress);
+    }
+
+    private void displayWinners(RaceResult raceResult) {
+        String raceWinners = resultFormatter.convertWinnersToString(raceResult.getRaceWinnerList());
+        System.out.print("최종 우승자 :");
+        System.out.print(raceWinners);
     }
 }
