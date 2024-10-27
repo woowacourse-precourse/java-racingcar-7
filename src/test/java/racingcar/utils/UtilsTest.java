18b6129e976572175e89c4e9fce8c975d87aac6a
softmoca <acyc0909@gmail.com>
2024-10-27 23:00:46 +0900

    test : 연속된 쉼표, 빈 이름 예외 발생 테스트 추가

--- src/test/java/racingcar/utils/UtilsTest.java
+++ src/test/java/racingcar/utils/UtilsTest.java
@@ -1,9 +1,11 @@
 package racingcar.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.global.message.ErrorMessage;
 import racingcar.global.utils.ParseStringUtils;
 
 class UtilsTest {
@@ -16,6 +18,23 @@ class UtilsTest {
         assertThat(carNames).containsExactly("pobi", "woni", "jun");
     }
 
+    @Test
+    void 연속된_쉼표_예외() {
+        String carNamesInput = "pobi,,jun";
+
+        assertThatThrownBy(() -> ParseStringUtils.splitCarNames(carNamesInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.CONSECUTIVE_COMMAS.getMessage());
+    }
+
+    @Test
+    void 빈_이름_입력_예외() {
+        String carNamesInput = "pobi, ,jun";
+
+        assertThatThrownBy(() -> ParseStringUtils.splitCarNames(carNamesInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.EMPTY_CAR_NAME.getMessage());
+    }
 
     @Test
     void 이름_양쪽_공백_제거_테스트() {
