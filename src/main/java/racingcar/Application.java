2786263e51143aa8fb084191a37368805ae50e0a
KoYunHyuk <aes2036@daum.net>
2024-10-27 17:00:14 +0900

    refactor: Remove comment

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,7 +3,6 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        // pobi,woni,jun
         Input input = new Input();
         input.inputNameAndAttemptCount();
 
