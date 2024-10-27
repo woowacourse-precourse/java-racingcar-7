881ba9a8fe9ac183bc642f3b48db7607b2ce88c1
Gyuri <haha0888@naver.com>
2024-10-27 18:45:40 +0900

    refactor. 결과 출력 형식 수정

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -14,6 +14,7 @@ public class RacingController {
         Cars cars = setCars(inputView.inputCarNames());
         int count = inputView.inputCount();
 
+        System.out.println("실행 결과");
         runRacing(cars, count);
 
         List<String> winnerList= cars.getWinner();
@@ -25,8 +26,6 @@ public class RacingController {
     }
 
     public void runRacing(Cars cars, int count){
-        System.out.println("실행 결과 ");
-
         while(count > 0){
             cars.moveCars();
             outputView.printResult(cars);
