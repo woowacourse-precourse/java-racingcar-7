fa4cc0c7a646d653f6a65665d993d61ba8592430
parksomii <somi020605@gmail.com>
2024-10-28 03:12:23 +0900

    feat(Application): 예외 처리 및 게임 시작 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,12 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            startRacingGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     }
 }
