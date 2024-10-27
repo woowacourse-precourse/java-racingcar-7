f331f6d9c685b43bafc875ed0c0913b8e95f1687
dia218 <sang218@kyonggi.ac.kr>
2024-10-27 23:51:24 +0900

    feat(car movement): Implement car movement
    - moveCar(): Randomly generate how far each car moves per turn
    - displayMove(): Output the movement of the cars

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,19 +4,40 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
-        List<String> nameList;
-        int turn = 0;
+    static List<String> carList;
+    static int turn = 0;
 
-        //자동차 이름 n개, 턴 수 입력받기
+    public static void main(String[] args) {
+        init();
+        moveCar();
+
+    }
+    static void init() { //자동차 이름 n개, 턴 수 입력받기
         String input = camp.nextstep.edu.missionutils.Console.readLine();
-        nameList = Arrays.asList(input.split(","));
+        carList = Arrays.asList(input.split(","));
         input = camp.nextstep.edu.missionutils.Console.readLine();
         turn = Integer.parseInt(input);
 
-        for(int i = 0; i < nameList.size(); i++) {
-            if(nameList.get(i).length() > 5) { throw new IllegalArgumentException(); }
+        for(int i = 0; i < carList.size(); i++) {
+            if(carList.get(i).length() > 5) { throw new IllegalArgumentException(); }
         }
-
+    }
+    static void moveCar() {
+        for(int t = 0 ; t < turn; t++) {
+            for(int i = 0; i < carList.size(); i++) {
+                System.out.print(carList.get(i) + " : ");
+                int requireMove = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 9);
+                int currentMove = 0;
+                displayMove(currentMove, requireMove);
+                System.out.print("\n");
+            }
+            System.out.print("\n");
+        }
+    }
+    static void displayMove(int currentMove, int requireMove) {
+        while(currentMove < requireMove) {
+            System.out.print("-");
+            currentMove++;
+        }
     }
 }
