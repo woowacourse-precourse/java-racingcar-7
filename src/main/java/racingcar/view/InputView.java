8dc7dbf1828b9b71facce5aaa3bae1b2b6d4a139
xeimun <hinote444@naver.com>
2024-10-27 15:42:20 +0900

    feat(InputView): 총 라운드 수가 1 이상인지 검사하는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -11,6 +11,15 @@ public class InputView {
 
     public String readTotalRounds() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        return Console.readLine();
+        String roundNumber = Console.readLine();
+        validatePositiveRoundNumber(roundNumber);
+        return roundNumber;
+    }
+
+    public void validatePositiveRoundNumber(String number) {
+        int roundNumber = Integer.parseInt(number);
+        if (roundNumber < 1) {
+            throw new IllegalArgumentException("총 라운드 수는 1 이상이어야 합니다.");
+        }
     }
 }
