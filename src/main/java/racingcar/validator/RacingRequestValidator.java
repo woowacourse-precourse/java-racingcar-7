f70b01bbe15b42b01782378682c3332b3b29b984
haebyun <bhk4392@gmail.com>
2024-10-27 20:10:01 +0900

    feat(RacingRequestValidator): 자동차 이름 중복 검증 로직 추가
    - `validateCarNames` 메서드에 중복된 자동차 이름이 있을 경우 예외를 발생시키는 로직 추가
    - 중복된 이름이 포함된 경우를 식별하기 위한 `hasDuplicateNames` 메서드 구현
    - 중복된 이름이 있을 때 예외 메시지로 사용할 `DUPLICATE_CAR_NAME_MESSAGE` 상수 추가

--- src/main/java/racingcar/validator/RacingRequestValidator.java
+++ src/main/java/racingcar/validator/RacingRequestValidator.java
@@ -1,6 +1,8 @@
 package racingcar.validator;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import racingcar.dto.request.RacingRequestDTO;
 
 public final class RacingRequestValidator {
@@ -9,6 +11,7 @@ public final class RacingRequestValidator {
     private final static Integer MAX_CAR_NAME_LENGTH = 5;
     private final static String INVALID_MOVE_COUNT_MESSAGE = "이동 횟수는 %d 이상이어야 합니다.";
     private final static Integer MIN_MOVE_COUNT = 1;
+    private final static String DUPLICATE_CAR_NAME_MESSAGE = "중복된 자동차 이름이 있습니다.";
 
     private RacingRequestValidator() {
     }
@@ -27,6 +30,14 @@ public final class RacingRequestValidator {
                     String.format(CAR_NAME_TOO_LONG_MESSAGE, MAX_CAR_NAME_LENGTH)
             );
         }
+        if (hasDuplicateNames(carNames)) {
+            throw new IllegalArgumentException(DUPLICATE_CAR_NAME_MESSAGE);
+        }
+    }
+
+    private static boolean hasDuplicateNames(List<String> carNames) {
+        Set<String> uniqueNames = new HashSet<>(carNames);
+        return uniqueNames.size() < carNames.size();
     }
 
     private static void validateMoveCount(Integer moveCount) {
