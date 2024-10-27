7dfcae5c90e0f4adcc0412f3ef909feaf32d010c
Seongho Kim <kksshh0612@gmail.com>
2024-10-27 22:53:15 +0900

    refactor: 결과 출력 시 Console.close() 호출

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -4,20 +4,27 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class RacingCarView {
 
-    public String getCarNameInput(){
+    public String getCarNameInput() {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
 
-    public String getTryCount(){
+    public String getTryCount() {
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         return Console.readLine();
     }
 
-    public void printResult(String result){
+    public void printResult(String result, String winnerInfo) {
 
-        System.out.println("최종 우승자 : " + result);
+        StringBuilder resultBuilder = new StringBuilder()
+                .append("실행 결과").append("\n")
+                .append(result)
+                .append("최종 우승자 : ").append(winnerInfo);
+
+        System.out.println(resultBuilder.toString());
+
+        Console.close();
     }
 }
