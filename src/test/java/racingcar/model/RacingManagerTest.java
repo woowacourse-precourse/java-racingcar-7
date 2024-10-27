932377964b7f02787ebfb7f4754ba85bb0371ca8
steamedEggMaster <cksdud5014@naver.com>
2024-10-27 15:04:08 +0900

    test(RacingManagerTest): 차수별 자동차 전진 처리 테스트 완료
    1. 최소 position 값을 isGreaterThanOrEqualTo(0)로 확인
       1) 랜덤 조건에 의해 1로 테스트 시 실패 가능성 있어, 0으로 commit

---
+++ src/test/java/racingcar/model/RacingManagerTest.java
@@ -0,0 +1,36 @@
+package racingcar.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RacingManagerTest {
+
+    private RacingManager racingManager;
+    private List<Car> cars;
+
+    @BeforeEach
+    void setUp() {
+        racingManager = new RacingManager();
+        cars = new ArrayList<>();
+        cars.add(new Car("car1"));
+        cars.add(new Car("car2"));
+        cars.add(new Car("car3"));
+    }
+
+    @Test
+    @DisplayName("시도 횟수 값만큼 라운드 실행")
+    void playRounds_byTryCount() {
+        int tryCount = 5;
+
+        racingManager.playRounds(cars, tryCount);
+
+        for (Car car : cars) {
+            assertThat(car.getPosition().length()).isGreaterThanOrEqualTo(0);
+        }
+    }
+}
