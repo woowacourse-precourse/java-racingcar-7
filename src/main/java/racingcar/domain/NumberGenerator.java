b7a75187e9ca088d03de9782eff6676dd0536e95
YoonSeungHyun2 <dbstmdgus5050@gmail.com>
2024-10-27 20:01:18 +0900

    feat: 예외처리를 위한 기능 추가

--- src/main/java/racingcar/domain/NumberGenerator.java
+++ src/main/java/racingcar/domain/NumberGenerator.java
@@ -1,7 +1,14 @@
 package racingcar.domain;
 
+/**
+ * NumberGenerator 인터페이스는 난수를 생성하는 메서드를 정의한다.
+ * - 이동 여부를 결정하기 위한 난수 생성에 사용된다.
+ */
 @FunctionalInterface
 public interface NumberGenerator {
 
+    /**
+     * 난수를 생성하는 메서드.
+     */
     int generate();
 }
