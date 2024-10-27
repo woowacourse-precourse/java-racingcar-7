cafe6c618487f0e1b300280d248519b9fcd22aa9
exena <exena01@gmail.com>
2024-10-27 17:44:33 +0900

    fix: 인풋이 0으로 들어오는 경우도 예외 처리되도록 추가함.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -26,7 +26,7 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+            assertThatThrownBy(() -> runException("pobi,javaji", "0"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
