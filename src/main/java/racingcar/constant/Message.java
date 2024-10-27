693bdcc909458803edc8593f2e38379b9091b8dc
ajroot5685 <anjeong8577@naver.com>
2024-10-27 17:23:34 +0900

    docs($Message): 출력 메세지의 마지막 공백은 println으로 대신한다

--- src/main/java/racingcar/constant/Message.java
+++ src/main/java/racingcar/constant/Message.java
@@ -1,5 +1,5 @@
 package racingcar.constant;
 
 public class Message {
-    public static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+    public static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 }
