1b20ac39beb7cc15a8ae3fffc0c3327fac9374aa
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-10-27 20:32:59 +0900

    feat(Application) : 1.2 경주할 자동차 목록을 쉼표(,)를 기준으로 구분하는 기능 구현
    - 입력된 문자열을 쉼표(,)를 기준으로 분리하여 String 배열 형태로 반환

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,10 +6,17 @@ public class Application {
     public static void main(String[] args) {
         // 1.1 경주할 자동차 목록을 입력받는 기능
         String carNamesInputString = getCarNamesInputString();
+        // 1.2 경주할 자동차 목록을 쉼표(,)를 기준으로 구분하는 기능
+        String[] carNames = getCarNames(carNamesInputString);
     }
 
     public static String getCarNamesInputString() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
+
+    public static String[] getCarNames(String carNamesInputString) {
+        return carNamesInputString.split(",");
+    }
+
 }
