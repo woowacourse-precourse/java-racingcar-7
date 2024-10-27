df94cd51e2d763161b428df56a998b6cb4003c13
yunho_yun <yunho_yun@naver.com>
2024-10-28 01:49:46 +0900

    feat: 시도(라운드) 횟수 유효성 검사 기능 구현
    InputValidator

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -6,4 +6,12 @@ public class InputValidator {
             throw new IllegalArgumentException("자동차의 이름은 5자 이하만 가능합니다.");
         }
     }
+
+    public static void validRound(String input) {
+        try {
+            int time = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 횟수는 정수여야 합니다.");
+        }
+    }
 }
