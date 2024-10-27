8ffc03f231b325d7bd92c064b9a610ca1c25ccd1
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-27 17:04:51 +0900

    Feat: 경주에 따른 게임 결과 출력 메서드 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,18 @@
 package racingcar.view;
 
+import racingcar.domain.car.Cars;
+import racingcar.domain.car.Name;
+import racingcar.domain.car.Position;
+
 public class OutputView {
 
     private static final String CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String RACE_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private static final String RACE_RESULT_MESSAGE = "\n실행 결과";
+
+    private static final String NEW_LINE = "\n";
+    private static final String RACE_RESULT_FORMAT = "%s : %s";
+    private static final String POSITION_VALUE = "-";
 
     public static void printCarNamesMessage() {
         System.out.println(CAR_NAMES_MESSAGE);
@@ -16,5 +24,18 @@ public class OutputView {
 
     public static void printResultMessage() {
         System.out.println(RACE_RESULT_MESSAGE);
+    }
+
+    public static void printCarsWithPosition(Cars cars) {
+        cars.getValues().forEach(car -> System.out.println(formatCar(car.getName(), car.getPosition())));
+        System.out.print(NEW_LINE);
+    }
+
+    private static String formatCar(Name name, Position position) {
+        return String.format(
+                RACE_RESULT_FORMAT,
+                name.getValue(),
+                POSITION_VALUE.repeat(position.getValue())
+        );
     }
 }
