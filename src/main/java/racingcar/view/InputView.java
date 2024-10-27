013635e9c495cd2308ccc9fd5f4b1add1c8f53ea
young-in <hardworkingini@gmail.com>
2024-10-27 22:45:08 +0900

    feat : 이동 횟수를 입력 view 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,7 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.MovingCount;
 import racingcar.util.SplittingRacingCarName;
 
 public class InputView {
@@ -8,5 +9,10 @@ public class InputView {
     public String[] inputCarNames(){
         String carNames = Console.readLine();  // Scanner 대신 Console 사용
         return SplittingRacingCarName.splitRacingCarName(carNames);
+    }
+
+    public MovingCount inputTrialCount(){
+        String trialCount = Console.readLine();
+        return new MovingCount(trialCount);
     }
 }
