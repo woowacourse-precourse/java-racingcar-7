1125657de1281643be207514dbe00ae88c05aa3a
parksomii <somi020605@gmail.com>
2024-10-28 02:48:08 +0900

    feat(RacingGame): 자동차 이름 입력 기능 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -14,4 +14,9 @@ public class RacingGame {
         this.cars = cars;
         this.rounds = rounds;
     }
+    private static String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
 }
