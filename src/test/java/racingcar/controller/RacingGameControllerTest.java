dbe7c4191ab2f1148beb4f2b26e289db503db10e
jungsehui <jungsehui@naver.com>
2024-10-28 02:28:55 +0900

    test(RacingGameControllerTest.java): 자동차 설정 테스트

--- src/test/java/racingcar/controller/RacingGameControllerTest.java
+++ src/test/java/racingcar/controller/RacingGameControllerTest.java
@@ -1,4 +1,38 @@
 package racingcar.controller;
 
-public class RacingGameControllerTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingGameControllerTest {
+
+    private InputView inputView;
+    private OutputView outputView;
+    private RacingGameController racingGameController;
+
+    @BeforeEach
+    void setUp() {
+        inputView = new InputView();
+        outputView = new OutputView();
+        racingGameController = new RacingGameController(inputView, outputView);
+    }
+
+    @Test
+    void 차량_설정_테스트() {
+        List<String> inputNames = Arrays.asList("pobi", "java");
+
+        List<Car> cars = racingGameController.setCars(inputNames);
+
+        assertThat(cars).hasSize(2);
+        assertThat(cars.get(0).getName()).isEqualTo("pobi");
+        assertThat(cars.get(1).getName()).isEqualTo("java");
+    }
 }
+
