e35f5345a663b9f16a9ae199654bc11b189a8fc1
jjjjjoon <mjj7343@gmail.com>
2024-10-27 21:09:03 +0900

    feat(Application): 기능 구현 완료
    전체적인 의존성 관계 정리 필요

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+
+import controller.RacingGameController;
+import service.RaceService;
+import util.randomnumber.RandomNumberHandler;
+import util.splitter.CarNameSplitter;
+import view.InputView;
+import view.OutputView;
+import util.validator.InputValidator;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        InputValidator inputValidator = new InputValidator();
+        CarNameSplitter carNameSplitter = new CarNameSplitter(inputValidator);
+        RandomNumberHandler randomNumberHandler = new RandomNumberHandler();
+
+        RaceService raceService = new RaceService(randomNumberHandler, outputView);
+        RacingGameController controller = new RacingGameController(inputView, outputView, carNameSplitter, raceService);
+        controller.startGame();
+
+
     }
 }
