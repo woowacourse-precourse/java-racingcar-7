1bc0c111e6d5e111131785af8311ed04451f2e31
jae woo Jung <awfjol2008@gmail.com>
2024-10-27 19:35:01 +0900

    feat: 자동차 목록 입력값을 구분하여 집합의 형태로 반환하는 기능 구현

--- src/main/java/racingcar/parser/CarNameInputParser.java
+++ src/main/java/racingcar/parser/CarNameInputParser.java
@@ -1,4 +1,11 @@
 package racingcar.parser;
 
+import java.util.Set;
+
 public class CarNameInputParser {
+	public Set<String> parseInput(String input) {
+		String[] carNames = input.split(",");
+
+		return Set.of(carNames);
+	}
 }
