d6953ddd09c3d0166b431ac4a7bc92f049fc3982
dongri <l0o0lv@github.com>
2024-10-28 03:44:48 +0900

    refactor(Constants): 상수들을 static final 로 변경

--- src/main/java/racingcar/util/Constants.java
+++ src/main/java/racingcar/util/Constants.java
@@ -2,9 +2,9 @@ package racingcar.util;
 
 public class Constants {
     private Constants() {}
-    public static int MAX_NAME_LENGTH = 5;
-    public static String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public static String INPUT_MOVE_COUNT = "시도할 횟수는 몇 회인가요?";
-    public static String OUTPUT_RESULT = "\n실행 결과";
-    public static String OUTPUT_WINNER = "최종 우승자 : ";
+    public static final int MAX_NAME_LENGTH = 5;
+    public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_MOVE_COUNT = "시도할 횟수는 몇 회인가요?";
+    public static final String OUTPUT_RESULT = "\n실행 결과";
+    public static final String OUTPUT_WINNER = "최종 우승자 : ";
 }
