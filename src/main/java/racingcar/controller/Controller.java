84abc9047929e9f24bb40ca144dba44585f32ecb
yunsseong <aster20@hanyang.ac.kr>
2024-10-27 22:37:37 +0900

    feat: 자동차 생성 컨트롤러 구현

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,11 +1,27 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.util.Util;
+import racingcar.validator.Validator;
 import racingcar.view.InputView;
 
 public class Controller {
     InputView inputView = new InputView();
+    Validator validator = new Validator();
 
     public void run() {
-        String names = inputView.input();
+        List<Car> cars = readyCar();
+    }
+
+    private List<Car> readyCar() {
+        String string = inputView.input();
+        validator.validateNames(string);
+        List<Car> cars = new ArrayList<>();
+        for (String name : Util.splitByComma(string)) {
+            cars.add(new Car(name));
+        }
+        return cars;
     }
 }
