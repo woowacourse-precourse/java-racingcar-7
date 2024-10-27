c2199c33ae8d4f601aa83ba30f01095207f8fff0
Seung Ik Lee <natwo98@gmail.com>
2024-10-27 14:45:22 +0900

    feat: 자동차 이름들과 시도 횟수 입력 받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표 기준으로 구분)");
+        String names = Console.readLine();
+
+        StringTokenizer tokenizer = new StringTokenizer(names, ",");
+        List<Car> cars = new ArrayList<>();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String attempts = Console.readLine();
+
+        while (tokenizer.hasMoreTokens()) {
+            String name = tokenizer.nextToken().trim(); // 공백 제거
+            cars.add(new Car(name));
+            System.out.println(name);
+        }
+        System.out.println(attempts);
+
     }
 }
---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int moveCount;
+
+    public Car(String name) {
+        this.name = name;
+        this.moveCount = 0; // 초기 이동 횟수는 0으로 설정
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+
+    public void move() {
+        this.moveCount++;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{name='" + name + "', moveCount=" + moveCount + "}";
+    }
+}
