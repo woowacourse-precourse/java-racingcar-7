ab82b79465640cd491f901e08b4e165b3ae2358d
dia218 <sang218@kyonggi.ac.kr>
2024-10-27 23:15:30 +0900

    feat(input): Implement {input car names and turn count}
    - Allow users to input car names in a single line
    - Allow users to input turn count in a next line
    - Error handling with a 5-character limit for names

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> nameList;
+        int turn = 0;
+
+        //자동차 이름 n개, 턴 수 입력받기
+        String input = camp.nextstep.edu.missionutils.Console.readLine();
+        nameList = Arrays.asList(input.split(","));
+        input = camp.nextstep.edu.missionutils.Console.readLine();
+        turn = Integer.parseInt(input);
+
+        for(int i = 0; i < nameList.size(); i++) {
+            if(nameList.get(i).length() > 5) { throw new IllegalArgumentException(); }
+        }
+
     }
 }
