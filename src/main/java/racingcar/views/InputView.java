c0015f7ad8068df520ac415a452aeade2dd626c8
sangxxjin <sj_0368@naver.com>
2024-10-27 23:03:25 +0900

    feat: 자동차 입력 메세지 출력
    본문:
    자동차 이름 입력 메세지 출력 메서드 구현했습니다.
    메세지는 상수화 했습니다.

--- src/main/java/racingcar/views/InputView.java
+++ src/main/java/racingcar/views/InputView.java
@@ -3,6 +3,8 @@ package racingcar.views;
 import java.util.Scanner;
 
 public class InputView {
+
+    private static final String INPUT_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static Scanner sc = new Scanner(System.in);
 
     public static String readStringInput() {
@@ -12,4 +14,9 @@ public class InputView {
     public static int readNumberInput() {
         return sc.nextInt();
     }
+
+    public static void displayInputPrompt() {
+        System.out.println(INPUT_PROMPT);
+    }
+
 }
