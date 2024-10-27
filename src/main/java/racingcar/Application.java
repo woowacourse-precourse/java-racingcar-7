c89ad37cad06aaf3855a0c4ba6ab900ee2ebebf6
taegyunB <taegyunb99@gmail.com>
2024-10-27 23:41:04 +0900

    feat(CarNames): 자동차 이름 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        String[] carNames = getCarNames();
+    }
+
+    private static String[] getCarNames() {
+        System.out.println("자동차 이름을 쉼표로 구분하여 입력하세요.");
+        String[] carNames = Console.readLine().split(",");
+        validateCarNames(carNames);
+        return carNames;
+    }
+
+    private static void validateCarNames(String[] carNames) {
+        for (String carName : carNames) {
+            if (carName.length() > 5 || carName.trim().isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
