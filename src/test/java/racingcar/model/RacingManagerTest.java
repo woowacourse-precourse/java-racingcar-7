1f470bf4df0754eeb2047b23f12b7184b35223da
sangjun <sangjoon12345@gmail.com>
2024-10-27 12:18:03 +0900

    feat: 랜덤값에 따라 자동차 이동 결정하는 기능 테스트 코드 구현

--- src/test/java/racingcar/model/RacingManagerTest.java
+++ src/test/java/racingcar/model/RacingManagerTest.java
@@ -1,14 +1,12 @@
 package racingcar.model;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RacingManagerTest {
     private RacingManager racingManager;
@@ -35,6 +33,27 @@ class RacingManagerTest {
         assertThat(carList).hasSize(3);
         assertThat(carList).extracting(Car::getName)
                 .containsExactly("a", "bb", "ccc");
+    }
+
+    @Test
+    void 랜덤값에_따라_자동차_이동_결정(){
+        Car car = new Car("Test",2);
+
+        car.move(4);
+        assertThat(car.getDistance()).isEqualTo(3);
+
+        car.move(3);
+        assertThat(car.getDistance()).isEqualTo(3);
+    }
+
+    @Test
+    void 모든_자동차가_이동_또는_정지한다(){
+        List<Car> carList = Arrays.asList(new Car("A"), new Car("B"), new Car("C"));
+
+        racingManager.moveOrStop(carList);
+        for (Car car : carList) {
+            assertThat(car.getDistance()).isBetween(0, 1); // 자동차가 이동했거나 그대로일 수 있음
+        }
     }
 
 }
