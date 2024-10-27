548648be5d70524f7d97733fc2cf794c1f0f79e7
JunwooPark <wnsdn1994@gmail.com>
2024-10-28 01:38:40 +0900

    fix(racinggame): output 형식 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -13,6 +13,6 @@ public class OutputView {
     }
 
     public void displayWinners(List<String> winners) {
-        System.out.println("최종우승자 : " + String.join(", ", winners));
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
