35abd05496124e60eb7e576b3930285659058463
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-10-27 17:39:14 +0900

    style: java style 적용

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,15 +7,15 @@ public class InputView {
     private static final String INPUT_CAR_STRING = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_REPEAT_COUNT = "시도할 횟수는 몇 회인가요?";
 
-    private InputView(){
+    private InputView() {
     }
 
-    public static String getInputCarString(){
+    public static String getInputCarString() {
         System.out.println(INPUT_CAR_STRING);
         return Console.readLine();
     }
 
-    public static String getInputRepeatCount(){
+    public static String getInputRepeatCount() {
         System.out.println(INPUT_REPEAT_COUNT);
         return Console.readLine();
     }
