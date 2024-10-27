f89149d8899d957c0397607f9e4bf19ded655d9e
wodus4145@naver.com <wodus4145@naver.com>
2024-10-27 14:40:35 +0900

    feat: Creat RacingGame class
    - 경기를 진행하는 RacingGame 클래스를 추가
    - 입력된 경기 횟수만큼 무작위 값을 받아서 전진하고 최종 승자(들)를 정함
    - 각 경기 결과와 최종 승자의 출력은 ResultPrinter 클래스로 넘김

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,48 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+
+    ResultPrinter printer = new ResultPrinter();
+    private static final int MOVEMENT_THRESHOLD = 4;
+
+    public void playGames(List<Car> racingCar, int numberOfGames) {
+        for (int i = 0; i < numberOfGames; i++) {
+            updateResult(racingCar);
+            printer.printRoundResult(racingCar);
+        }
+        printer.printWinners(decideWinner(racingCar));
+    }
+
+    public void updateResult(List<Car> racingCar) {
+        for (Car car : racingCar) {
+            if (moveForward()) {
+                car.advance += 1;
+            }
+        }
+    }
+
+    public boolean moveForward() {
+        return Randoms.pickNumberInRange(0, 9) >= MOVEMENT_THRESHOLD;
+    }
+
+    public String decideWinner(List<Car> racingCar) {
+        int maxValue = Integer.MIN_VALUE;
+        ArrayList<String> winnerList = new ArrayList<>();
+
+        for (Car car : racingCar) {
+            if (car.advance > maxValue) {
+                maxValue = car.advance;
+                winnerList.clear();
+                winnerList.add(car.name);
+            } else if (car.advance == maxValue) {
+                winnerList.add(car.name);
+            }
+        }
+        return String.join(",", winnerList);
+    }
+}
