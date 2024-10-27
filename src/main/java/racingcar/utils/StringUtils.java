6231dd5f4e623d719be6efed33e80691e41abe0a
moongua404 <hogun.soongit@gmail.com>
2024-10-27 19:00:52 +0900

    feat(test): test code 작성
    - 예외처리 테스트코드 작성
    - 정상 작동 테스트코드 작성
    - 개별 테스트에서는 이상이 없지만 파일로 실행하면 에러가 발생함
    - 모킹의 처리가 제대로 되지 않음

--- src/main/java/racingcar/utils/StringUtils.java
+++ src/main/java/racingcar/utils/StringUtils.java
@@ -1,23 +1,35 @@
 package racingcar.utils;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class StringUtils {
     public List<String> separateName(String nameString) {
         try {
-            return Arrays.stream(nameString.split(","))
-                    .peek(this::validateName)
+            List<String> nameList = Arrays.stream(nameString.split(","))
+                    .peek(this::validateNameLength)
                     .collect(Collectors.toList());
+
+            validateNameDuplication(nameList);
+            return nameList;
         } catch (Exception exception) {
             throw new IllegalArgumentException(exception);
         }
     }
 
-    private void validateName(String name) {
+    private void validateNameLength(String name) {
         if (name.length() > ConstantNumber.MAX_CAR_NAME_LENGTH) {
             throw new IllegalArgumentException(ConstantMessage.NAME_LENGTH_EXCEPTION);
+        }
+    }
+
+    private void validateNameDuplication(List<String> names) {
+        Set<String> uniqueNames = new HashSet<>(names);
+        if (uniqueNames.size() < names.size()) {
+            throw new IllegalArgumentException(ConstantMessage.DUPLICATED_NAME_EXCEPTION);
         }
     }
 }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,33 +1,97 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
+    //모킹이 작동하지 않고 모두 1으로 들어감
     @Test
-    void 기능_테스트() {
+    @DisplayName("랜덤 값 테스트 (모킹)")
+    void featureTest() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
+    }
+    /*
+    //모킹이 작동하지 않고 모두 1으로 들어감
+    @Test
+    @DisplayName("랜덤 값 테스트 (공동 우승, 실행 결과 예시)")
+    void DoubleWinnerTest() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,jun", "5");
+                    assertThat(output()).contains(
+                            "pobi : -----",
+                            "woni : ----",
+                            "jun : -----",
+                            "최종 우승자 : pobi, jun");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+     */
+
+    @ParameterizedTest
+    @DisplayName("여러 사용자(차) 입력 테스트")
+    @ValueSource(strings = {"pobi,woni,jun", "pobi,woni,jun", "pobi,woni,jun,hogun,gamja,be,A"})
+    void PlentyCarTest(String names) {
+        run(names, "10");
+        assertThat(output()).contains("최종 우승자 : ");
+    }
+
+    @ParameterizedTest
+    @DisplayName("이름 중복 예외처리")
+    @ValueSource(strings = {"pobi,woni,pobi", "A,B,C,C", "A,Babo,C,Babo"})
+    void InputDuplicatedNameException(String names) {
+        assertThatThrownBy(() -> {
+            Application.main(new String[]{names, "10"});
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("음수 라운드 입력 예외처리")
+    @ValueSource(ints = {Integer.MAX_VALUE, -40, -1, 0})
+    void InputMinusRoundException(int value) {
+        assertThatThrownBy(() -> {
+            Application.main(new String[]{"pobi,woni", String.valueOf(value)});
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("빈 입력값 예외처리")
+    @NullAndEmptySource
+    void InputEmptyNameException(String names) {
+        assertThatThrownBy(() -> {
+            Application.main(new String[]{names, "1"});
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 예외_테스트() {
+    @DisplayName("긴 이름 입력 예외처리")
+    void InputInvalidNameException() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
