1a19d957acce029754cca667290c8541257c2491
youngkwanglim <richard08@naver.com>
2024-10-27 22:08:44 +0900

    feat: 우승자 출력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Application {
@@ -11,18 +12,17 @@ public class Application {
         int tryCount = inputTryCount();
 
         gameStart(cars, tryCount);
+
+        printWinner(cars);
     }
 
-    private static List<Car> inputCarNames() {
-        List<Car> cars = new ArrayList<>();
+    private static void printWinner(List<Car> cars) {
+        int maxLocation = findMaxLocation(cars);
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
-        List<String> carNames = Arrays.asList(input.split(","));
-        for (String carName : carNames) {
-            cars.add(new Car(carName));
-        }
-        return cars;
+        List<String> winners = findWinners(cars, maxLocation);
+
+        String finalWinner = String.join(", ", winners);
+        System.out.println("최종 우승자 : " + finalWinner);
     }
 
     private static int inputTryCount() {
@@ -50,5 +50,35 @@ public class Application {
             System.out.println();
         }
         System.out.println();
+    }
+
+    private static List<String> findWinners(List<Car> cars, int maxLocation) {
+        List<String> winners = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (car.getLocation() == maxLocation) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
+
+    private static int findMaxLocation(List<Car> cars) {
+        return cars.stream()
+                .max(Comparator.comparingInt(Car::getLocation))
+                .get()
+                .getLocation();
+    }
+
+    private static List<Car> inputCarNames() {
+        List<Car> cars = new ArrayList<>();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        List<String> carNames = Arrays.asList(input.split(","));
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+        return cars;
     }
 }
