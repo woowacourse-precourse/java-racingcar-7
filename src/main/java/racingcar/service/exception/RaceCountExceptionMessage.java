8d3813c3e8b57cfeb78f9afd2f7bb91d3edaeebb
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-27 16:11:33 +0900

    Feat: 게임 횟수가 int 최소 미만일 경우 예외 발생시키는 검증 로직 추가

--- src/main/java/racingcar/service/exception/RaceCountExceptionMessage.java
+++ src/main/java/racingcar/service/exception/RaceCountExceptionMessage.java
@@ -7,6 +7,7 @@ public enum RaceCountExceptionMessage implements ExceptionMessage {
     RACE_COUNT_CANNOT_BLANK("게임 횟수는 빈 문자열 또는 공백문자일 수 없습니다."),
     RACE_COUNT_MUST_DIGIT("게임 횟수는 숫자이어야 합니다."),
     RACE_COUNT_OVERFLOW("게임 횟수 오버플로우입니다."),
+    RACE_COUNT_UNDERFLOW("게임 횟수를 언더플로우입니다."),
     ;
 
     private final String message;
--- src/main/java/racingcar/validation/RaceCountValidator.java
+++ src/main/java/racingcar/validation/RaceCountValidator.java
@@ -3,6 +3,7 @@ package racingcar.validation;
 import static racingcar.service.exception.RaceCountExceptionMessage.RACE_COUNT_CANNOT_BLANK;
 import static racingcar.service.exception.RaceCountExceptionMessage.RACE_COUNT_MUST_DIGIT;
 import static racingcar.service.exception.RaceCountExceptionMessage.RACE_COUNT_OVERFLOW;
+import static racingcar.service.exception.RaceCountExceptionMessage.RACE_COUNT_UNDERFLOW;
 
 import racingcar.service.exception.RaceCountException;
 
@@ -11,6 +12,8 @@ public class RaceCountValidator {
     public static String validateRaceCount(String raceCount) {
         validateRaceCountBlank(raceCount);
         validateRaceCountDigit(raceCount);
+        validateRaceCountOverFlow(raceCount);
+        validateRaceCountUnderFlow(raceCount);
         return raceCount;
     }
 
@@ -31,6 +34,12 @@ public class RaceCountValidator {
     private static void validateRaceCountOverFlow(String raceCount) {
         if (Long.parseLong(raceCount) > Integer.MAX_VALUE) {
             throw new RaceCountException(RACE_COUNT_OVERFLOW);
+        }
+    }
+
+    private static void validateRaceCountUnderFlow(String raceCount) {
+        if (Long.parseLong(raceCount) < Integer.MIN_VALUE) {
+            throw new RaceCountException(RACE_COUNT_UNDERFLOW);
         }
     }
 
