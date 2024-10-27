6768dce0280521dade2bba7dc381892a17e1522c
jungsehui <jungsehui@naver.com>
2024-10-28 02:42:33 +0900

    chore(InputView.java): 패키지 구조 변경으로 인한 import문 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,7 +1,7 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.Message;
+import racingcar.global.Message;
 
 import java.util.Arrays;
 import java.util.List;
