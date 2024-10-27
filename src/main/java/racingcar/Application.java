9ebedca378cde77df8a1c240fe783c4ec7ce708c
김주언 <wndjs803@naver.com>
2024-10-27 16:32:02 +0900

    자동차 이름 부여 및 제약사항 체크

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,30 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+        // Car 라는 클래스를 만들어 List<Car>로 처리할까 -> 확장성 고려해서(나중에 후진 같은 것이 생길지도)
+        // Map<String, Integer>로 처리할까
+
+        List<Car> cars = new ArrayList<>();
+        for (String carName : input.split(",")) {
+            if(carName.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+            Car car = new Car(carName, 0);
+            cars.add(car);
+        }
+
+
     }
+
 }
---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private Integer forward;
+
+    public Car(String carName, int forward) {
+        this.name = carName;
+        this.forward = forward;
+    }
+}
