55e8994b36093cfac30f526061211dd9a29aa91c
hojae <ghwo336@naver.com>
2024-10-28 04:55:30 +0900

    feat: Valid 수정

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,11 +1,16 @@
 package racingcar.controller;
 import racingcar.seperator.Seperator;
+import racingcar.valid.Valid;
 import racingcar.view.InputView;
 
 public class Controller {
     public void start(){
         String s= InputView.getInput();
         Seperator seperator=new Seperator(s);
+        Valid valid=new Valid(s);
+        for (String name : seperator.getNames()) {
+
+        }
 
     }
 
--- src/main/java/racingcar/valid/Valid.java
+++ src/main/java/racingcar/valid/Valid.java
@@ -1,20 +1,14 @@
 package racingcar.valid;
 
 public class Valid {
-    private String s;
-    private int num;
 
-    public Valid(String s) {
-        this.s = s;
-    }
-
-    public void valid(){
+    public void valid(String s){
         if (s.length()>5){
             throw new IllegalArgumentException("이름은 5자를 초과할 수 없습니다.");
         }
     }
     
-    public void numValid(){
+    public void numValid(String s){
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
@@ -22,9 +16,7 @@ public class Valid {
                 throw new IllegalArgumentException("숫자가 아닙니다");
             }
         }
-        num=Integer.parseInt(s);
         }
-
-
+        
 
 }
