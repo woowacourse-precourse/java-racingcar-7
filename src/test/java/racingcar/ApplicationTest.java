988cc6990f2ae1b1c78215f8503005a8b5af79d4
pokm321 <pokm90@gmail.com>
2024-10-27 09:37:32 +0900

    test: add more test cases
    exceptions for a big number of rounds or cars

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -78,6 +78,23 @@ class ApplicationTest extends NsTest {
 //                assertThatThrownBy(() -> runException("pobi,woni", ""))
 //                        .isInstanceOf(IllegalArgumentException.class)
 //        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(
+                        "aa,ab,ac,ad,ae,af,ag,ah,ai,aj,ak,al,am,an,ao,ap,aq,ar,as,at,au,av,aw,ax,ay,az,ba,bb,bc,"
+                                + "bd,be,bf,bg,bh,bi,bj,bk,bl,bm,bn,bo,bp,bq,br,bs,bt,bu,bv,bw,bx,by,bz,ca,cb,cc,"
+                                + "cd,ce,cf,cg,ch,ci,cj,ck,cl,cm,cn,co,cp,cq,cr,cs,ct,cu,cv,cw,cx,cy,cz,da,db,dc,"
+                                + "dd,de,df,dg,dh,di,dj,dk,dl,dm,dn,do,dp,dq,dr,ds,dt,du,dv,dw", // 101 cars
+                        "2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "10001"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "9999999999999999999999999999999999"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Override
