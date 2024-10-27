e73a29941c39d574db8e25f02980117fab68d575
YooSejeong <nived3@naver.com>
2024-10-27 19:49:07 +0900

    refactor(Application, InputView): Separate user input input into a dedicated class

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,17 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
+import controller.RacingController;
 import java.util.List;
+import view.InputView;
 
 public class Application {
     public static void main(String[] args) {
 
-        //TODO: 다른 클래스로 분리할 수 있으면 분리하기
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carsInput = Console.readLine();
-        List<String> cars = Arrays.asList(carsInput.split(","));
+        List<String> carsList = InputView.getCarNames();
+        int raceRounds = InputView.getRaceRounds();
 
-        //TODO: 다른 클래스로 분리할 수 있으면 분리하기
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int raceRounds = Integer.parseInt(Console.readLine());
+        RacingController controller = new RacingController(carsList, raceRounds);
+        controller.startRace();
 
     }
 
