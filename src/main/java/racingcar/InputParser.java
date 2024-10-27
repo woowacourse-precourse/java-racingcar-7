51a62ad160160f63a598e7d262238c4156023b52
euni <kongeuni1004@gmail.com>
2024-10-27 23:15:15 +0900

    chore: 자동차 이름 및 시도 횟수 파싱 메서드 추가 (구현 예정)
    - 자동차 이름을 파싱하는 `parseCarNames` 메서드 이름 정의
    - 시도할 횟수를 파싱하는 `parseAttemptCount` 메서드 이름 정의
    - 기능 구현 없이 메서드 틀만 작성하여 향후 개발 준비 완료

---
+++ src/main/java/racingcar/InputParser.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import java.util.List;
+
+public class InputParser {
+
+    public List<Car> parseCarName(String carName){
+
+    }
+
+    public int parseAttemptNumber(String attemptNumber){
+
+    }
+}
