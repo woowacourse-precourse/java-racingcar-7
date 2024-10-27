c40cc24b0d9722265e13a394c3ff309038d67c4e
jungsehui <jungsehui@naver.com>
2024-10-28 01:50:51 +0900

    refactor(RacingGameController.java): 패키지 구조 변경과 메서드 분리 작업

---
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -0,0 +1,55 @@
+package racingcar.controller;
+
+import racingcar.Message;
+import racingcar.domain.Car;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RacingGameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        String carName = inputView.getRacingCarName();
+        int tryCount = inputView.getTrial();
+        List<String> carNames = inputView.changeStringToList(carName);
+        List<Car> cars = setCars(carNames);
+        race(tryCount, cars);
+        outputView.printWinners(getMaxPosition(cars), cars);
+    }
+
+    public int getMaxPosition(final List<Car> cars) {
+        System.out.print(Message.WINNER.getMessage());
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+
+    public void race(int tryCount, final List<Car> cars) {
+        System.out.println();
+        for (int i = 0; i < tryCount; i++) {
+            for (Car car : cars) {
+                car.move();
+            }
+            outputView.printCurrentCarPositions(cars);
+        }
+    }
+
+    public List<Car> setCars(final List<String> input) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : input) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+}
