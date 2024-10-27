a8550caa30773c643b5440be10616e3c0da8cd81
RawBrain <95132660+RawBrain@users.noreply.github.com>
2024-10-28 04:42:29 +0900

    feat: implement race participants management logic
    경주 참가자들의 생성 및 관리 로직 구현
    -입력받은 참가자들의 Car객체를 생성하여 한번에 관리

--- src/main/java/racingcar/RaceMember.java
+++ src/main/java/racingcar/RaceMember.java
@@ -1,4 +1,33 @@
 package racingcar;
 
+import java.util.List;
+
 public class RaceMember {
+    private final List<Car> raceMember;
+
+    public RaceMember(List<String> carNames) {
+        this.raceMember = carNames.stream()
+                .map(Car::new)
+                .toList();
+    }
+
+    public void moveAll() {
+        raceMember.forEach(Car::chanceToMove);
+    }
+
+    public List<Car> getRaceMember() {
+        return raceMember;
+    }
+
+    public List<Car> getWinner() {
+        int maxPosition = raceMember.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return raceMember.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .toList();
+
+    }
 }
