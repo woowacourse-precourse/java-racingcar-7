f3bf1c1807d88f57065af2ffa93efaee772ac372
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-27 21:24:58 +0900

    feat(ErrorMessage): 예외 처리 문자열 상수화
    - 예외 처리와 관련해서 사용되는 문자열들을 상수화

--- src/main/java/racingcar/Util/Exception/ErrorMessage.java
+++ src/main/java/racingcar/Util/Exception/ErrorMessage.java
@@ -1,4 +1,21 @@
 package racingcar.Util.Exception;
 
-public class ErrorMessage {
+public enum ErrorMessage {
+    NAME_WHITESPACE("이름에 공백만 입력할 수 없습니다."),
+    NAME_EMPTY("이름은 비어 있을 수 없습니다."),
+    NAME_TOO_LONG("이름이 5글자 이상입니다."),
+    NAME_DUPLICATE("이름이 중복되었습니다."),
+    TRIAL_NOT_INTEGER("시도 횟수는 정수여야 합니다."),
+    TRIAL_NEGATIVE("시도 횟수는 음수일 수 없습니다."),
+    TRIAL_ZERO("시도 횟수는 0일 수 없습니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
