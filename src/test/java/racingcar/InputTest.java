3fa0b041b27800763f99a0df48b4a8f7a268fd9f
yyysolhhh <solbaram37@gmail.com>
2024-10-28 00:29:41 +0900

    test(InputTest): 입력값 관련 테스트 추가
    - 테스트케이스 여러개일 때 @ParameterizedTest, @ValueSource 이용
    - 입력값 유효성 검사 테스트 추가

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -1,10 +1,18 @@
 package racingcar;
 
+import static racingcar.messages.ExceptionMessages.EMPTY_NAME;
 import static racingcar.messages.ExceptionMessages.INVALID_CAR_NAME_FORMAT;
+import static racingcar.messages.ExceptionMessages.INVALID_INTEGER_RANGE;
+import static racingcar.messages.ExceptionMessages.INVALID_ROUNDS_INPUT;
 import static racingcar.messages.ExceptionMessages.NAME_TOO_LONG;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.io.InputHandler;
 import racingcar.io.InputParser;
 import racingcar.io.InputValidator;
@@ -14,31 +22,70 @@ public class InputTest {
     private final InputValidator inputValidator = new InputValidator();
     private final InputHandler inputHandler = new InputHandler();
 
+    @AfterAll
+    static void close() {
+        Console.close();
+    }
+
+    @Test
+    void 입력_테스트() {
+        System.setIn(new ByteArrayInputStream("pobi,woni,jun\n".getBytes()));
+        String[] carNames = inputHandler.inputCarNamesProcess();
+        Assertions.assertThat(carNames).containsExactly("pobi", "woni", "jun");
+    }
+
     @Test
     void 입력값_파싱_테스트() {
         String inputString = "pobi,woni,jun";
+//        provideInput(inputString);
 //        Assertions.assertArrayEquals(new String[]{"pobi", "woni", "jun"}, inputParser.parseCarNames(inputString));
         Assertions.assertThat(inputParser.parseCarNames(inputString)).containsExactly("pobi", "woni", "jun");
+//        Assertions.assertThat(inputHandler.inputCarNamesProcess()).containsExactly("pobi", "woni", "jun");
     }
 
-    @Test
-    void 입력값_파싱_예외_테스트() {
-        String[] inputStrings = {"pobi, woni, jun", "pobi,woni, jun", "pobi, woni,jun", " pobi,woni,jun",
-                "pobi,woni,jun ", " pobi, woni, jun ", "pobi, woni, j un"};
-        for (String inputString : inputStrings) {
-            String[] carNames = inputString.split(",");
-            Assertions.assertThatThrownBy(() -> inputValidator.validateCarNames(carNames))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessage(INVALID_CAR_NAME_FORMAT.getMessage());
-        }
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "pobi, woni, jun", "pobi,woni, jun", "pobi, woni,jun", " pobi,woni,jun",
+            "pobi,woni,jun ", " pobi, woni, jun ", "pobi, woni, j un"
+    })
+    void 입력값_공백_포함_예외_테스트(String inputString) {
+        String[] carNames = inputParser.parseCarNames(inputString);
+        Assertions.assertThatThrownBy(() -> inputValidator.validateCarNames(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_CAR_NAME_FORMAT.getMessage());
     }
 
-    @Test
-    void 자동차_이름_길이_제한_테스트() {
-        String[] carNames = {"pobii, woni, jun", "pobi, wonni, jun", "pobi, woni, jjunn"};
+    @ParameterizedTest
+    @ValueSource(strings = {"pobiii,woni,jun", "pobi,wonnii,jun", "pobi,woni,jjjunn"})
+    void 자동차_이름_5자_이하만_가능_예외_테스트(String inputString) {
+        String[] carNames = inputParser.parseCarNames(inputString);
         Assertions.assertThatThrownBy(() -> inputValidator.validateCarNames(carNames))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(NAME_TOO_LONG.getMessage());
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"", ",woni,jun", "jun,", ",jun,"})
+    void 빈문자열_이름_예외_테스트(String inputString) {
+        String[] carNames = inputParser.parseCarNames(inputString);
+        Assertions.assertThatThrownBy(() -> inputValidator.validateCarNames(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(EMPTY_NAME.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "0", "-100"})
+    void 이동횟수_양의정수만_가능_예외_테스트(String inputString) {
+        Assertions.assertThatThrownBy(() -> inputValidator.validateNumberOfRound(inputString))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_ROUNDS_INPUT.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2147483648", "-2147483649"})
+    void 이동횟수_정수범위만_가능_예외_테스트(String inputString) {
+        Assertions.assertThatThrownBy(() -> inputValidator.validateNumberOfRound(inputString))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_INTEGER_RANGE.getMessage());
+    }
 }
