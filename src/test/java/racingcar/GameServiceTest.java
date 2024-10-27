1f8a5a70cc6cdeecd0211886ffcdf624c828e08b
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-10-27 22:05:21 +0900

    feat(GameServiceTest) : 우승자 출력 테스트 및 중복 출력 테스트 기능 구현

--- src/test/java/racingcar/GameServiceTest.java
+++ src/test/java/racingcar/GameServiceTest.java
@@ -1,4 +1,46 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
 public class GameServiceTest {
+    GameService gameService = new GameService();
+
+    @Test
+    void 우승자_출력_테스트() {
+
+        //given
+        Map<String, Integer> resultOfRacingGame = new HashMap<>();
+        resultOfRacingGame.put("wozi", 6);
+        resultOfRacingGame.put("woni", 4);
+        resultOfRacingGame.put("jepp", 5);
+
+        //when
+        String winner = gameService.findingWinner(resultOfRacingGame);
+
+        //then
+        assertThat(winner).isEqualTo("wozi");
+
+    }
+
+    @Test
+    void 중복_우승자_출력_테스트() {
+
+        //given
+        Map<String, Integer> resultOfRacingGame = new HashMap<>();
+        resultOfRacingGame.put("pobi", 5);
+        resultOfRacingGame.put("woni", 5);
+
+        //when
+        String winner = gameService.findingWinner(resultOfRacingGame);
+
+        //then
+        assertThat(winner).isEqualTo("pobi,woni");
+
+    }
+
+
 }
