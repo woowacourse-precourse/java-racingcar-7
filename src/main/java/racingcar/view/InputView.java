6555ae1889213692fbadf19655633dff43590072
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-10-27 20:34:32 +0900

    fix: 자동차 중복입력 예외 처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,14 +2,17 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InputView {
     // 자동차 이름 입력 메소드
-    public static String[] inputCarNames(){
+    public static String[] inputCars(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String text = Console.readLine();
-        String[] carNames = text.split(",");
-        validateCarNames(carNames);
-        return carNames;
+        String[] cars = text.split(",");
+        validateCars(cars);
+        return cars;
     }
     // 시도 횟수 입력 메소드
     public static int inputAttempts() {
@@ -20,14 +23,12 @@ public class InputView {
     }
 
     // 자동차 이름 유효성 검사
-    private static void validateCarNames(String[] carNames) {
-        for (String name : carNames) {
-            if (name.isEmpty()) {
-                throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
-            }
-            if (name.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
-            }
+    private static void validateCars(String[] cars) {
+        Set<String> cheakUnique = new HashSet<>();
+        for (String name : cars) {
+            if (name.isEmpty()) throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+            if (name.length() > 5) throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            if (!cheakUnique.add(name)) throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
         }
     }
 
