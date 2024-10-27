84a315d5a0c24c30cea89d2914b1efd5d52b604a
yereumi <lovelyyeji01@gmail.com>
2024-10-27 22:24:33 +0900

    feat(input):  inputMessage 변수 삭제

--- src/main/java/racingcar/io/Input.java
+++ src/main/java/racingcar/io/Input.java
@@ -5,20 +5,16 @@ import racingcar.message.Message;
 
 public class Input {
 
-    private String inputMessage;
-
     public String getCarList() {
 
-        inputMessage = String.format(Message.INPUT_CAR_NAMES.getMessage());
-        System.out.println(inputMessage);
+        System.out.println(Message.INPUT_CAR_NAMES.getMessage());
 
         return Console.readLine();
     }
 
     public Integer getAttemptCount() {
 
-        inputMessage = String.format(Message.INPUT_ATTEMPT_COUNT.getMessage());
-        System.out.println(inputMessage);
+        System.out.println(Message.INPUT_ATTEMPT_COUNT.getMessage());
 
         String num = Console.readLine();
         return Integer.parseInt(num);
