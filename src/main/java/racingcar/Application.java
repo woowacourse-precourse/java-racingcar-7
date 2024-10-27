1416fa3dee96c4f946dfdc7c59dce08d33faf411
정재민 <jeongjaemin@jeongjaemin-ui-noteubug.local>
2024-10-27 16:04:01 +0900

    feat: 우승자 판별 기능 추가 - 가장 멀리 이동한 자동차를 우승자로 결정하고, 우승자가 여러 명일 경우 쉼표로 구분하여 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,6 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
@@ -15,8 +14,10 @@ public class Application {
         String attemptInt = (Console.readLine());
         int attemptCount = ValidateAttemptCount(attemptInt);// 시도 횟수 검증
         // 자동차 경주 시작
-        carRacing(cars, attemptCount);
+        String [] carPosition = carRacing(cars, attemptCount);
 
+        //우승자 판별 및 결과출력
+        announceWinners(cars, carPosition);
     }
     // 자동차 이름 검증
     private static void ValidateCarNames(String[] carNames) {
@@ -42,7 +43,7 @@ public class Application {
     }
 
     // 자동차 경주 시작
-    private static void carRacing(String [] cars, int attemptCount){
+    private static String [] carRacing(String [] cars, int attemptCount){
         String [] carPosition = new String[cars.length];
         for(int i=0; i < cars.length; i++){
             carPosition[i] = ""; // 빈문자열로 초기화
@@ -50,7 +51,7 @@ public class Application {
 
         System.out.println("\n실행 결과");
         for(int i = 0; i < attemptCount; i++){
-            for(int j =0; j< cars.length; j++){
+            for(int j =0; j < cars.length; j++){
                 int randomInt = Randoms.pickNumberInRange(0,9);
                 // 4이상일 때 전진
                 if(randomInt >= 4){
@@ -60,6 +61,37 @@ public class Application {
             }
             System.out.println(); // 각 시도마다 줄바꿈
         }
+        return carPosition;
+    }
+
+    //우승자 발표
+    private static void announceWinners(String [] cars, String [] carPosition){
+        int maxLength = 0;
+        // 가장 긴 이동거리 구하기
+        for(String position : carPosition){
+            maxLength = Math.max(maxLength, position.length());
+        }
+
+        //우승자 수 세기
+        int winnersCount = 0;
+        for(String position : carPosition){
+            if(maxLength == position.length()){
+                winnersCount++;
+            }
+        }
+
+        //우승자 찾기
+        String[] winnersList = new String[winnersCount];
+        int index = 0;
+        for(int i = 0; i <cars.length; i++){
+            if(carPosition[i].length() == maxLength){
+                winnersList[index] = cars[i];
+                index++;
+            }
+        }
+
+        //우승자 출력
+        System.out.println("최종 우승자 : " + String.join(",", winnersList));
     }
 }
 
