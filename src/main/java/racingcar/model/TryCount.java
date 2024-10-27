284befd9c4736bd99d8650283228c9ee4dc24c65
yeonnhuu <yeonnhuu@gmail.com>
2024-10-27 12:55:15 +0900

    :sparkles: feat(TryCount): 실행 횟수 문자열을 숫자로 변환

--- src/main/java/racingcar/model/TryCount.java
+++ src/main/java/racingcar/model/TryCount.java
@@ -1,4 +1,13 @@
 package racingcar.model;
 
 public class TryCount {
+    private final int tryCount;
+
+    public TryCount(String count) {
+        this.tryCount = convertStringToInt(count);
+    }
+
+    private int convertStringToInt(String input) {
+        return Integer.parseInt(input);
+    }
 }
