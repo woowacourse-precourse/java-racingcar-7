803f03726779e97fa7fe76422b432fe90d594059
yunju <juile0827@naver.com>
2024-10-27 20:59:15 +0900

    docs(Application): Application.java main 메서드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+import racingcar.converter.GameOverDTOConverter;
+import racingcar.service.CarService;
+import racingcar.service.InputProcessService;
+import racingcar.view.GameOverView;
+import racingcar.view.GameStartView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingGameController racingGameController = new RacingGameController(
+                new GameStartView(),
+                new GameOverView(),
+                new InputProcessService(),
+                new CarService(),
+                new GameOverDTOConverter()
+        );
+
+        racingGameController.startGame();
     }
 }
