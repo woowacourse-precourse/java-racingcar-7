2101a1e0077e4f9380e0c9f69cc7f7ad7f0acb8b
lkdcode <dlrlejr1@naver.com>
2024-10-27 21:34:50 +0900

    Refactor(:TotalLaps) 생성자에 최소 바큇 수 스펙 적용

--- src/main/java/racingcar/value/TotalLaps.java
+++ src/main/java/racingcar/value/TotalLaps.java
@@ -3,4 +3,11 @@ package racingcar.value;
 public record TotalLaps(
     Long count
 ) {
+    private static final Long MIN_SIZE = 1L;
+
+    public TotalLaps {
+        if (count < MIN_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
