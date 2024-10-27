5a8d6315770abb2a30a77ae849db24fa9885ec30
woongjee <jeewoong7753@naver.com>
2024-10-28 01:26:43 +0900

    fix(Car):랜덤한 수 추출하는 방식 수정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
     private int position = 0;
@@ -21,8 +23,7 @@ public class Car {
         return position;
     }
     public boolean canMove(){
-        long currentTime = System.currentTimeMillis();
-        int number = (int)(currentTime%10);
+        int number = Randoms.pickNumberInRange(0,9);
         return number>=4;
     }
     public void printPosition() {
