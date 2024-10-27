c29e42d5fed124ac4a776ab7f38c64ac8a3b2c28
023-dev <0_2_3@naver.com>
2024-10-27 19:11:06 +0900

    feat(domain): Cars 일급 컬렉션 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,13 @@
 package racingcar;
 
 import java.util.List;
+import racingcar.domain.Cars;
 import racingcar.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         List<String> carNames = InputView.getCarNames();
         int rounds = InputView.getRounds();
+        Cars cars = new Cars(carNames);
     }
 }
---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,18 @@
+package racingcar.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<String> cars) {
+        this.cars = cars.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
