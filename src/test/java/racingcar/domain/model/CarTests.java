11f89a70fd58eec5f2c0b08dc0ea2a65da2eb6f6
haebyun <bhk4392@gmail.com>
2024-10-27 16:08:40 +0900

    test(CarTests): 일반 Test에서 ParameterizedTest로 변경 및 테스트 코드 추가
    - 공백문자로 이루어진 자동차 이름에 대한 예외 처리 테스트 추가
    - isCanMove에 따른 전진 로직 확인 테스트 추가

--- src/test/java/racingcar/domain/model/CarTests.java
+++ src/test/java/racingcar/domain/model/CarTests.java
@@ -2,27 +2,55 @@ package racingcar.domain.model;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTests {
-    private static final String EMPTY_NAME = "";
-    private static final String LONG_NAME = "우아한테크코스라는매우긴이름";
-
-    @Test
-    void carInitialMoveCountIsZero() {
-        Car car = Car.of("car");
-        assertThat(car.getMoveCount()).isZero();
+    static Stream<Arguments> invalidCarNames() {
+        return Stream.of(
+                Arguments.of("  ", "자동차 이름이 비어있습니다."),
+                Arguments.of("", "자동차 이름이 비어있습니다."),
+                Arguments.of("우아한테크코스라는매우긴이름", "자동차 이름 5자 초과 되었습니다.")
+        );
     }
 
-    @Test
-    void carThrowsExceptionWhenNameIsEmpty() {
-        assertThatThrownBy(() -> Car.of(EMPTY_NAME))
-                .isInstanceOf(IllegalArgumentException.class);
+    @ParameterizedTest
+    @DisplayName("잘못된 자동차 이름 입력 시 예외 발생")
+    @MethodSource("invalidCarNames")
+    public void invalidCarNameThrowException(String carName, String errorMessage) {
+        assertThatThrownBy(() -> Car.of(carName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(errorMessage);
     }
 
-    @Test
-    void carThrowsExceptionWhenNameIsTooLong() {
-        assertThatThrownBy(() -> Car.of(LONG_NAME))
-                .isInstanceOf(IllegalArgumentException.class);
+    @ParameterizedTest
+    @DisplayName("유효한 자동차 이름 입력 시 정상 작동")
+    @ValueSource(strings = {"해광", "우테코", "떡볶이최고"})
+    public void validCarNameDoesNotThrowException(String carName) {
+        assertThatCode(() -> Car.of(carName))
+                .doesNotThrowAnyException();
+    }
+
+
+    static Stream<Arguments> isCanMove() {
+        return Stream.of(
+                Arguments.of(true, 1),
+                Arguments.of(false, 0)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("isCanMove가 true이면 전진, false이면 전진하지 않는다.")
+    @MethodSource("isCanMove")
+    void move(boolean isCanMove, int expected) {
+        Car car = Car.of("우테코");
+
+        car.tryMove(isCanMove);
+
+        assertThat(car.getMoveCount()).isEqualTo(expected);
     }
 }
