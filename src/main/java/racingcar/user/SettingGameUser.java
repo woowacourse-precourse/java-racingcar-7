5cd072dd11de161603e790a4cdb23b81e6a816a9
MinJae00 <exceedmin@gmail.com>
2024-10-27 19:40:27 +0900

    refact: 코드 가독성을 위해 유저 객체를 생성하는 기능을 두개의 기능으로 분리

--- src/main/java/racingcar/user/SettingGameUser.java
+++ src/main/java/racingcar/user/SettingGameUser.java
@@ -1,13 +1,20 @@
 package racingcar.user;
 
+import racingcar.view.InputView;
+
 public class SettingGameUser {
     public static UserObject[] user;
 
-    public SettingGameUser(String[] result) {
-        user = new UserObject[result.length];
+    public SettingGameUser() {
+        user = new UserObject[InputView.result.length];
+        CreateUser(InputView.result);
+    }
+
+    private void CreateUser(String[] result) {
         for (int i = 0; i < result.length; i++) {
             user[i] = new UserObject(result[i]);
         }
     }
 }
+
 
