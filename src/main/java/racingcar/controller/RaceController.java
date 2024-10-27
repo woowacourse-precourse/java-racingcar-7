918b807334de6f74d77659aa00164647a93b6961
kimdev <kim__dev@hufs.ac.kr>
2024-10-27 18:23:43 +0900

    feat(Input): 턴을 입력받는 기능을 추가하라

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,4 +1,16 @@
 package racingcar.controller;
 
+import racingcar.util.NameParser;
+import racingcar.view.Input;
+
+import java.util.ArrayList;
+
 public class RaceController {
+    public void playRace() {
+        String names = Input.inputCarName();
+        ArrayList<String> nameList = NameParser.parse(names);
+
+        int turn = Input.inputRaceTurn();
+
+    }
 }
--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -4,9 +4,19 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
     private static String getCarNameMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static String getRaceTurnMessage = "시도할 횟수는 몇 회인가요?";
 
     public static String inputCarName() {
         System.out.println(getCarNameMessage);
         return Console.readLine();
+    }
+
+    public static int inputRaceTurn() {
+        System.out.println(getRaceTurnMessage);
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("숫자를 입력해 주세요.");
+        }
     }
 }
