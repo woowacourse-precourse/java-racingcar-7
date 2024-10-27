6c605d47206cb89f789d21ee179c42ae22bef9dc
Youn Sang Hyun <younsang523@gmail.com>
2024-10-28 01:26:37 +0900

    feat: 자동차 전진 기능 구현

--- src/main/java/racingcar/Domain/Car.java
+++ src/main/java/racingcar/Domain/Car.java
@@ -2,9 +2,12 @@ package racingcar.Domain;
 
 public class Car {
     private final String name;
+    private final Position position;
 
     public Car(String name) {
         this.name = CarName.from(name).toString();
+        this.position = new Position();
     }
+
 
 }
