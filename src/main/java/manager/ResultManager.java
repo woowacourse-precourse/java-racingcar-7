0c44deaf30ed53e54b764e76594f690eacf04929
belight2 <chlwnsgur090@gmail.com>
2024-10-28 18:01:18 +0900

    feat: 결과 문자열을 반환하는 메소드 추가
    - getWinners() : 최종 우승 결과를 문자열로 반환하는 함수

--- src/main/java/manager/ResultManager.java
+++ src/main/java/manager/ResultManager.java
@@ -1,13 +1,21 @@
 package manager;
 
 import car.Car;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultManager {
     private final List<Car> carList;
 
     public ResultManager(List<Car> carList){
         this.carList = carList;
+    }
+
+    public String getWinners(){
+        return "최종 우승자 : " + carList.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
     }
 
 }
