b4ecd4492f7b01d408654c16d8256e1b6f8f7855
kim-se-hee <saree98@naver.com>
2024-10-28 17:00:30 +0900

    feat(Racing): 자동차 경주 진행 클래스 구현
    Racing에 경주 진행 핵심 로직 추가
    - 각 스테이지 마다 플레이어 전지 여부 판단
    - 랜덤 숫자를 골라 플레이어 전지
    - 우승자 선별

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,2 +1,76 @@
-package racingcar;public class Racing {
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class Racing {
+    private static final ConsoleOutputHandler outputHandler = new ConsoleOutputHandler();
+    private static final ConsoleInputHandler inputHandler = new ConsoleInputHandler();
+    private static int maxDistance;
+    private static int totalStage;
+
+    public void race() {
+
+        outputHandler.showStartComment();
+        List<String> names = inputHandler.getUserNameInput();
+        List<Player> players = names.stream()
+                .map(Player::new)
+                .toList();
+
+        outputHandler.showCommentForChoosingStageSize();
+        totalStage = inputHandler.getTotalStageSizeInput();
+
+        outputHandler.showCommentForResult();
+        startRacing(players);
+
+        List<Player> winners = findWinnerFrom(players);
+        outputHandler.showWinners(winners);
+
+    }
+
+    private static void startRacing(List<Player> players) {
+
+        for (int i = 0; i < totalStage; i++) {
+
+            checkAndMove(players);
+            outputHandler.showSnapshotOfCurrentStage(players);
+
+        }
+
+    }
+
+    private static void updateMaxDistance(Player player) {
+        maxDistance = Math.max(player.getDistance(), maxDistance);
+    }
+
+    private static List<Player> findWinnerFrom(List<Player> players) {
+
+        return players.stream()
+                .filter(player -> player.isWinner(maxDistance))
+                .toList();
+
+    }
+
+    private static void checkAndMove(List<Player> players) {
+
+        for (Player player : players) {
+
+            if (canMoveThisRound()) {
+                player.move();
+            }
+
+            updateMaxDistance(player);
+
+        }
+
+    }
+
+    private static boolean canMoveThisRound() {
+
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        return randomNumber >= 4;
+
+    }
+
 }
