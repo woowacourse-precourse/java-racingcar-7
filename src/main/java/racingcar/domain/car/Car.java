6b074b9f7410b36b823498811cae7e882cca52e3
hootzz <wjqwjql3@gmail.com>
2024-10-28 19:41:24 +0900

    feat(car): 자동차 기본 기능 구현

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -1,4 +1,23 @@
 package racingcar.domain.car;
 
 public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move(int number) {
+        if (number >= 4) {
+            position++;
+        }
+    }
+
+    public String showStatus() {
+        String progress = "-".repeat(position);
+        String format = "%s : %s";
+
+        return String.format(format, name, progress);
+    }
 }
