3a63786b7d2c10e0070bcec551d27ad55e51d698
HyejinYoo <yyyhj0114@gmail.com>
2024-10-28 18:18:41 +0900

    feat: add Application class

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    	try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String inputNames = Console.readLine();
+            String[] carNames = inputNames.split(",");
+
+            System.out.println("시도할 횟수는 몇 회인가요?");
+            int rounds = Integer.parseInt(Console.readLine());
+
+            Race race = new Race(Arrays.asList(carNames));
+            race.play(rounds);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
     }
 }
