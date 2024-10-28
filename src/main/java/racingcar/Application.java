196748fb7cd715f9892bae1b22fae9271efb7099
Seung Ik Lee <natwo98@gmail.com>
2024-10-28 21:17:55 +0900

    feat: 사용자 입력 받는 부분 분리 및 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException 발생시키는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,32 +1,17 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String names = Console.readLine();
-
-        StringTokenizer tokenizer = new StringTokenizer(names, ",");
-        List<Car> cars = new ArrayList<>();
-
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String attempts = Console.readLine();
-
-        while (tokenizer.hasMoreTokens()) {
-            String name = tokenizer.nextToken().trim(); // 공백 제거
-            cars.add(new Car(name));
-        }
+        List<Car> cars = InputHandler.getCars();  // 입력된 자동차 목록 받기
+        int attempts = InputHandler.getAttempts();  // 입력된 시도 횟수 받기
 
         RaceController raceController = new RaceController();
         System.out.println();
         System.out.println("실행 결과");
-        raceController.runRace(Integer.parseInt(attempts), cars);
+        raceController.runRace(attempts, cars);
 
         raceController.getWinner(cars);
     }
