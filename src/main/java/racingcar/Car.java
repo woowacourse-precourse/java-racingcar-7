3789a8c7e803c52144d578c67d67828a57b6c429
seoys0910 <seoys0910@naver.com>
2024-10-28 14:59:20 +0900

    feat: 자동차 생성 시 이름 유효성 검증 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,8 +5,15 @@ public class Car {
     private int position;
 
     public Car(String name) {
+        this(name, 0);
+    }
+
+    public Car(String name, int position) {
+        validateEmpty(name);
+        validateName(name);
+        validateLength(name);
         this.name = name;
-        this.position = 0;
+        this.position = position;
     }
 
     public String getName() {
@@ -24,6 +31,24 @@ public class Car {
     }
 
     public String displayStatus() {
-        return name + " : " + "-".repeat(position);
+        return name.trim() + " : " + "-".repeat(position);
+    }
+
+    private void validateEmpty(String name) {
+        if (name.isEmpty() || name.isBlank()) {
+            throw new IllegalArgumentException("이름이 비어있습니다.");
+        }
+    }
+
+    private void validateName(String name) {
+        if (!name.matches("[a-zA-Z0-9가-힣 ]+")) {
+            throw new IllegalArgumentException("이름은 문자나 숫자만 가능합니다.");
+        }
+    }
+
+    private void validateLength(String name) {
+        if (!(name.length() <= 5)) {
+            throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
+        }
     }
 }
