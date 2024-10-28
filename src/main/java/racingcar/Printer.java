bcce8d57f929c3a2541c4512bb1242297f6641d9
eunho <eunho1032@gmail.com>
2024-10-28 15:13:21 +0900

    refactor(Printer): rename class, rename methods

--- src/main/java/racingcar/Printer.java
+++ src/main/java/racingcar/Printer.java
@@ -2,19 +2,19 @@ package racingcar;
 
 import java.util.List;
 
-public class Output {
+public class Printer {
 
-    public static void printResult(List<Car> cars, int tryCounts) {
+    public static void displayResult(List<Car> cars, int tryCounts) {
         System.out.println("\n실행 결과");
         for (int i = 0; i < tryCounts; i++) {
-            printMovement(cars);
+            displayMovement(cars);
         }
-        printWinner(Winner.find(cars));
+        displayWinner(Winner.find(cars));
     }
 
-    public static void printMovement(List<Car> cars) {
+    public static void displayMovement(List<Car> cars) {
         for (Car car : cars) {
-            car.drive();
+            car.move();
             System.out.println(car.name + " : " + displayDistance(car));
         }
         System.out.println();
@@ -23,19 +23,19 @@ public class Output {
     public static String displayDistance(Car car) {
         String distance = "";
         for (int i = 0; i < car.distance; i++) {
-            distance += "-";
+            distance = distance.concat("-");
         }
         return distance;
     }
 
-    public static void printWinner(List<Car> winners) {
+    public static void displayWinner(List<Car> winners) {
         System.out.print("최종 우승자 : ");
+        Car singleWinner = winners.getFirst();
 
         if (winners.size() == 1) {
-            System.out.print(winners.getFirst().name);
+            System.out.print(singleWinner.name);
             return;
         }
-
-        System.out.println(Winner.makeList(winners));
+        System.out.println(Winner.createList(winners));
     }
 }
