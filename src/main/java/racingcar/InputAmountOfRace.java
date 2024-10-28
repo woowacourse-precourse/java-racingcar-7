08281a0d521233cf5d54415d569a2e044d0673a4
jmlee <p2achtrii@gmail.com>
2024-10-28 23:13:46 +0900

    refactor: raceAmount edit

--- src/main/java/racingcar/InputAmountOfRace.java
+++ src/main/java/racingcar/InputAmountOfRace.java
@@ -3,10 +3,16 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputAmountOfRace {
-    public static void main(String[] args) {
+    private int raceAmount;
+
+    public void inputRaceAmount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String raceString = Console.readLine();
 
-        int raceAmount = Integer.parseInt(raceString);
+        raceAmount = Integer.parseInt(raceString);
+    }
+
+    public int getRaceAmount() {
+        return raceAmount;
     }
 }
