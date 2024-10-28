0c1842551e52964f9f21123c4b028396d8d27cac
Limwngur <ovg07047@naver.com>
2024-10-28 20:13:11 +0900

    feat: RacingGame 실행을 위한 main메서드 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.service.RacingCarService;
+import racingcar.view.ConsoleInputView;
+import racingcar.view.ConsoleOutputView;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        InputView inputView = new ConsoleInputView();
+        RacingCarService racingCarService = new RacingCarService();
+        OutputView outputView = new ConsoleOutputView();
+        RacingCarController racingCarController = new RacingCarController(inputView, racingCarService, outputView);
+        racingCarController.runGame();
     }
 }
