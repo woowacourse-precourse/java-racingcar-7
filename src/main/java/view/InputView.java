86fd347ad62384edeab12afb87f0d46575223b1c
sseopsseop <ponde119@outlook.com>
2024-10-28 18:39:52 +0900

    fix: 입력 기능 예외 처리 코드 적용
    올바른 형태의 입력이 아닌 경우 StringValidator의 메서드로 검증.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,10 +2,19 @@ package view;
 
 import camp.nextstep.edu.missionutils.Console;
 import util.Constants;
+import util.StringValidator;
 
 public class InputView {
     public static String[] inputWindow(){
-        return new String[]{inputString(Constants.NOTICE_INPUT_CAR_NAME), inputString(Constants.NOTICE_INPUT_TRY_COUNT)};
+        String carNames = inputString(Constants.NOTICE_INPUT_CAR_NAME);
+        String tryCount = inputString(Constants.NOTICE_INPUT_TRY_COUNT);
+
+        StringValidator.checkNotNull(carNames);
+        StringValidator.checkNotNull(tryCount);
+        StringValidator.checkCarNames(carNames);
+        StringValidator.checkPositiveIntValue(tryCount);
+
+        return new String[]{carNames, tryCount};
     }
 
     private static String inputString(String prompt) {
