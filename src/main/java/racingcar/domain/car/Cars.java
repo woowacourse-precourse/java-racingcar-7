df9ff093c9aec3f23f240443c03b5a4b9b79f641
hootzz <wjqwjql3@gmail.com>
2024-10-28 20:27:40 +0900

    feat(cars): 자동차 일급 컬렉션 생성

--- src/main/java/racingcar/domain/car/Cars.java
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -1,4 +1,21 @@
 package racingcar.domain.car;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<String> names) {
+        this.cars = new ArrayList<>();
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
+    }
+
+    public void race() {
+        for (Car car : cars) {
+            car.move();
+        }
+    }
 }
