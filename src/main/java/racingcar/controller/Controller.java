7c632fe2faac9fd15a42bb52c294fa727ebb10ee
박성준 <sungjunp1208@gmail.com>
2024-10-28 22:07:17 +0900

    feat: Parser 구현

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -2,6 +2,10 @@ package racingcar.controller;
 
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
+import racingcar.model.Parser;
+import racingcar.model.RacingGame;
+
+import java.util.ArrayList;
 
 public class Controller {
 
@@ -9,6 +13,10 @@ public class Controller {
     private final OutputView outputView = new OutputView();
 
     public void run(){
-        String input=inputView.input();
+        String input=inputView.inputName();
+        int num = inputView.inputNum();
+        ArrayList<String> participants = Parser.parse(input);
+        String winner=RacingGame.race();
+        outputView.output(winner);
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,7 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String input(){
+    private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_NUM_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
+    public String inputName(){
+        System.out.println(INPUT_NAME_MESSAGE);
         return Console.readLine();
+    }
+
+    public int inputNum(){
+        System.out.println(INPUT_NUM_MESSAGE);
+        return Integer.parseInt(Console.readLine());
+
     }
 }
