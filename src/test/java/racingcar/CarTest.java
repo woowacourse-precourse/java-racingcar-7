93cb4962048aacf365f56a3f644747b70e89ad9a
dongri <l0o0lv@github.com>
2024-10-28 21:27:08 +0900

    test: Car 객체 생성 테스트 추가

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,39 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.model.Car;
+import racingcar.util.validator.PlayerNameValidator;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class CarTest {
+
+    private PlayerNameValidator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = new PlayerNameValidator();
+    }
+    @DisplayName("자동차 객체 생성 성공 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "pobi", "a2", "abcd", "12345"})
+    void carNameSuccessTest(String name) {
+        Car car = new Car(name);
+
+        Assertions.assertThat(car.getName()).isEqualTo(name);
+    }
+
+    @DisplayName("자동차 객체 생성 실패 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "     ", "123456", "thisIsMoreThenFour"})
+    void carNameFailTest(String name) {
+        assertThatThrownBy(() -> validator.validateName(List.of(name)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
