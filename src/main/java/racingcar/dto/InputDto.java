791dcd8b719d3cd8d011a57bb33e3dda2856553b
Limwngur <ovg07047@naver.com>
2024-10-28 20:07:17 +0900

    feat: 사용자 입력 Data transfer object 구현

---
+++ src/main/java/racingcar/dto/InputDto.java
@@ -0,0 +1,24 @@
+package racingcar.dto;
+
+import java.util.List;
+
+public class InputDto {
+
+    public static class RequestInputDto {
+        private final List<String> inputCars;
+        private final int inputCnt;
+
+        public RequestInputDto(List<String> inputCars, int inputCnt) {
+            this.inputCars = inputCars;
+            this.inputCnt = inputCnt;
+        }
+
+        public List<String> getInputCars() {
+            return this.inputCars;
+        }
+
+        public int getInputCnt() {
+            return this.inputCnt;
+        }
+    }
+}
