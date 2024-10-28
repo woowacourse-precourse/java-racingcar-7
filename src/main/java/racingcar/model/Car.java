5a0a1b64e6eb1c5d5e37d972e8e9c4f42034dde6
youjae <youjae0803@gmail.com>
2024-10-28 15:39:16 +0900

    feat(Car): 자동차 이름을 검증한다.

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,24 @@
+package racingcar.model;
+
+import racingcar.utils.ErrorCode;
+
+public class Car {
+    private MoveCondition moveCondition;
+    private final String name;
+    private Long position;
+
+    public Car(String name) {
+        validateName(name);
+
+        this.moveCondition = new MoveCondition(4);
+        this.name = name;
+        this.position = 0L;
+    }
+
+    private Boolean validateName(String name){
+        if(name == "")
+            throw new IllegalArgumentException(ErrorCode.NAME_EMPTY_ERROR.getMessage());
+
+        return Boolean.TRUE;
+    }
+}
