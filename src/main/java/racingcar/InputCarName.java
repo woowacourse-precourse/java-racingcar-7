c551850d50060604c8de839f8cf9ce1af261be2a
jmlee <p2achtrii@gmail.com>
2024-10-28 22:03:14 +0900

    feat: update InputCar - edit List

--- src/main/java/racingcar/InputCarName.java
+++ src/main/java/racingcar/InputCarName.java
@@ -3,11 +3,12 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputCarName {
-    public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분");
-        String car = Console.readLine();
+    public String[] getCarList(){
+            System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분");
+            String car = Console.readLine();
 
-        DelimiterName delimiterCar = new DelimiterName();
-        String[] CarList = car.split(delimiterCar.delimiter);
+            DelimiterName delimiterCar = new DelimiterName();
+
+            return car.split(delimiterCar.delimiter);
+        }
     }
-}
