a54a472eb234d835fe8550bd7e39943768c7376a
parksomii <somi020605@gmail.com>
2024-10-28 21:02:11 +0900

    fix(Application): 테스트에서 IllegalArgumentException이 발생하도록 예외 처리 방식 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,17 +4,18 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            startRacingGame();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        startRacingGame();
     }
 
     private static void startRacingGame() {
-        List<Car> cars = RacingGame.initializeCars();
-        int rounds = RacingGame.initializeRounds();
-        RacingGame game = new RacingGame(cars, rounds);
-        game.startRace();
+        try {
+            List<Car> cars = RacingGame.initializeCars();
+            int rounds = RacingGame.initializeRounds();
+            RacingGame game = new RacingGame(cars, rounds);
+            game.startRace();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
 }
