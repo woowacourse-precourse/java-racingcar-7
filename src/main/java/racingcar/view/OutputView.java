e7a03c44a44f49dcae7dd7debb7d0673cea2bb1f
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-28 14:21:23 +0900

    refactor(view): OutputView의 출력 로직 리팩토링
    - showWinners 메서드에서 StringBuilder 대신 Stream API 활용으로 코드 간결화

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,51 +1,46 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.model.Car;
 import racingcar.model.Cars;
 
 public class OutputView {
     private static final OutputView INSTANCE = new OutputView();
-    private final String RESULT_MESSAGE = "실행 결과";
-    private final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
 
     private OutputView() {
     }
-
-    ;
 
     public static OutputView getInstance() {
         return INSTANCE;
     }
 
-    public void showRoundResult(Cars cars) {
+    public void showResultMessage() {
         System.out.println(RESULT_MESSAGE);
-        StringBuilder result = new StringBuilder();
-        for (Car car : cars.getCars()) {
-            result = addCarWithNameAndPosition(result, car);
-        }
-        System.out.println(result.toString());
     }
 
-    private StringBuilder addCarWithNameAndPosition(StringBuilder result, Car car) {
-        result.append(car.getName()).append(" : ");
-        for (int i = 0; i < car.getPosition(); i++) {
-            result.append('-');
+    public void showRoundResult(Cars cars) {
+        StringBuilder result = new StringBuilder();
+        for (Car car : cars.getCars()) {
+            addCarWithNameAndPosition(result, car);
         }
+        System.out.println(result);
+    }
+
+    private void addCarWithNameAndPosition(StringBuilder result, Car car) {
+        result.append(car.getName()).append(" : ");
+        result.append("-".repeat(car.getPosition()));
         result.append("\n");
-        return result;
     }
 
     public void showWinners(List<Car> winners) {
         System.out.print(FINAL_WINNER_MESSAGE);
-        StringBuilder result = new StringBuilder();
-        for (Car winner : winners) {
-            result.append(winner.getName()).append(',');
-        }
-        if (!result.isEmpty()) {
-            result.deleteCharAt(result.length() - 1); // 마지막 ,(쉼표) 제거
-        }
 
-        System.out.println(result.toString());
+        String result = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println(result);
     }
 }
