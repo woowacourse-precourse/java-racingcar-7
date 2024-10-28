038e62bbdd5afdbc0f44bccf67055992935e5efa
hyunwon <khw73850@gmail.com>
2024-10-28 20:39:41 +0900

    test: test selectWinners

--- src/test/java/racingcar/RacingCarGameTest.java
+++ src/test/java/racingcar/RacingCarGameTest.java
@@ -1,12 +1,13 @@
 package racingcar;
 
-
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCarGameTest {
     @Test
@@ -16,15 +17,26 @@ public class RacingCarGameTest {
         System.setOut(new PrintStream(outputStream));
 
         try{
-            camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest(
+            assertRandomNumberInRangeTest(
                     () -> {
                         RacingCarGame game = new RacingCarGame(Arrays.asList("faker", "keria"), 1);
                         game.round();
-                        Assertions.assertThat(outputStream.toString().trim()).contains("faker : -", "keria :");
+                        assertThat(outputStream.toString().trim()).contains("faker : -", "keria :");
                     }, 5, 3
             );
         }finally{
             System.setOut(originalOut);
         }
+    }
+
+    @Test
+    void 우승자_고르기_테스트(){
+        assertRandomNumberInRangeTest(
+                ()->{
+                    RacingCarGame game = new RacingCarGame(Arrays.asList("faker", "keria"), 1);
+                    game.start();
+                    assertThat(game.selectWinners().toString()).isEqualTo("[faker]");
+                }, 5, 3
+        );
     }
 }
