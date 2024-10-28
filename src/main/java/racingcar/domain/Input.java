1ae920a08dc5c0bef01826331bd5e6711d8b5979
sytTest <syt1128@naver.com>
2024-10-28 15:48:27 +0900

    :construction: (Input) fix and add
    요청과 그에 대한 응답을 쌍으로 가지고 있도록 필드 추가 및 수정

--- src/main/java/racingcar/domain/Input.java
+++ src/main/java/racingcar/domain/Input.java
@@ -1,19 +1,30 @@
 package racingcar.domain;
 
 import java.lang.reflect.Type;
+import racingcar.view.constant.Request;
 
 public class Input {
 
+    private String request;
     private final String input;
 //    private final Type type;
 
     public Input(String input) {
+        this.input = input;
+    }
+
+    public Input(String request, String input) {
+        this.request = request;
         this.input = input;
 //        type = input.getClass();
     }
 
     public String getInput() {
         return input;
+    }
+
+    public String getRequest() {
+        return request;
     }
 //
 //    public Type getType() {
