43769949f7f5e4fed7142c35f39a9c184c3e7b7b
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-10-28 20:11:35 +0900

    feat: Controller 작성 - 자동차 이름 입력 받는 handleInputCarNames() - 이동할 횟수 입력 받는 handleInputMoveCount() - 전체 경주게임 시작 startGame()

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,31 @@
+package racingcar.controller;
+
+import racingcar.service.CarService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingCarController {
+    private final CarService service = new CarService();
+
+    public void handleInputCarNames() {
+        InputView.printCarNamesInputGuide();
+        service.createGame(InputView.inputCarNames());
+    }
+
+    public void handleInputMoveCount() {
+        InputView.printMoveCountInputGuide();
+        service.setMoveCount(InputView.inputMoveCount());
+    }
+
+    public void startGame() {
+        handleInputCarNames();
+        handleInputMoveCount();
+        OutputView.printRunMessage();
+        for (int i = 0; i < service.getMoveCount(); i++) {
+            service.playGame();
+            OutputView.printGameStatus(service.getGameBoard().getCars());
+        }
+        service.determineWinner();
+        OutputView.printGameResult(service.getWinners());
+    }
+}
