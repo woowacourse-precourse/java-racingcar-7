138b3d5cb294a382cc046acb05b34ffab7f3bba4
YugyeomGhim <rladbrua0207@gmail.com>
2024-10-28 20:25:19 +0900

    test: RacingGame의 게임 종료 조건 테스트 추가

--- src/test/java/racingcar/model/RacingGameTest.java
+++ src/test/java/racingcar/model/RacingGameTest.java
@@ -2,30 +2,33 @@ package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
-class CarGroupTest {
+class RacingGameTest {
 
     @Test
     void 자동차_수_테스트() {
         // given
         List<String> carNames = Arrays.stream("ghim,yu,gyeom".split(",")).toList();
-        CarGroup carGroup = new CarGroup(carNames);
+        int tryCount = 5;
+        RacingGame racingGame = new RacingGame(carNames, tryCount);
 
         // when & then
-        assertEquals(3, carGroup.getCars().size());
+        assertEquals(3, racingGame.getCars().size());
     }
 
     @Test
     void 자동차_수가_2보다_작을_경우_예외_발생() {
         // given
         List<String> carNames = Arrays.stream("gyeom".split(",")).toList();
+        int tryCount = 5;
 
         // when & then
-        assertThatThrownBy(() -> new CarGroup(carNames))
+        assertThatThrownBy(() -> new RacingGame(carNames, tryCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 적어도 두 개 입력해야 합니다.");
     }
@@ -34,9 +37,10 @@ class CarGroupTest {
     void 자동차_이름이_중복될_경우_예외_발생() {
         // given
         List<String> carNames = Arrays.stream("gyeom,gyeom".split(",")).toList();
+        int tryCount = 5;
 
         // when & then
-        assertThatThrownBy(() -> new CarGroup(carNames))
+        assertThatThrownBy(() -> new RacingGame(carNames, tryCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("중복된 자동차 이름이 존재합니다.");
     }
@@ -44,16 +48,17 @@ class CarGroupTest {
     @Test
     void 우승자_여러명_테스트() {
         // given
-        List<String> carNames = Arrays.stream("gyeom,yu,ghim".split(",")).toList();
-        CarGroup carGroup = new CarGroup(carNames);
+        List<String> carNames = Arrays.stream("ghim,yu,gyeom".split(",")).toList();
+        int tryCount = 5;
+        RacingGame racingGame = new RacingGame(carNames, tryCount);
 
         int movableNumber = 4;
-        List<Car> cars = carGroup.getCars().stream().toList();
+        List<Car> cars = racingGame.getCars().stream().toList();
         cars.get(0).moveFront(movableNumber);
         cars.get(2).moveFront(movableNumber);
 
         // when
-        List<Car> winners = carGroup.getWinners();
+        List<Car> winners = racingGame.getWinners();
 
         // then
         assertEquals(cars.get(0), winners.get(0));
@@ -63,17 +68,18 @@ class CarGroupTest {
     @Test
     void 우승자_모두_테스트() {
         // given
-        List<String> carNames = Arrays.stream("gyeom,yu,ghim".split(",")).toList();
-        CarGroup carGroup = new CarGroup(carNames);
+        List<String> carNames = Arrays.stream("ghim,yu,gyeom".split(",")).toList();
+        int tryCount = 5;
+        RacingGame racingGame = new RacingGame(carNames, tryCount);
 
         int movableNumber = 4;
-        List<Car> cars = carGroup.getCars().stream().toList();
+        List<Car> cars = racingGame.getCars().stream().toList();
         cars.get(0).moveFront(movableNumber);
         cars.get(1).moveFront(movableNumber);
         cars.get(2).moveFront(movableNumber);
 
         // when
-        List<Car> winners = carGroup.getWinners();
+        List<Car> winners = racingGame.getWinners();
 
         // then
         assertEquals(cars.get(0), winners.get(0));
@@ -84,17 +90,32 @@ class CarGroupTest {
     @Test
     void 우승자_한명_테스트() {
         // given
-        List<String> carNames = Arrays.stream("gyeom,yu,ghim".split(",")).toList();
-        CarGroup carGroup = new CarGroup(carNames);
+        List<String> carNames = Arrays.stream("ghim,yu,gyeom".split(",")).toList();
+        int tryCount = 5;
+        RacingGame racingGame = new RacingGame(carNames, tryCount);
 
         int movableNumber = 4;
-        List<Car> cars = carGroup.getCars().stream().toList();
+        List<Car> cars = racingGame.getCars().stream().toList();
         cars.get(2).moveFront(movableNumber);
 
         // when
-        List<Car> winners = carGroup.getWinners();
+        List<Car> winners = racingGame.getWinners();
 
         // then
         assertEquals(cars.get(2), winners.get(0));
+    }
+
+    @Test
+    void 게임_진행_테스트() {
+        // given
+        List<String> carNames = Arrays.stream("ghim,yu,gyeom".split(",")).toList();
+        int tryCount = 1;
+        RacingGame racingGame = new RacingGame(carNames, tryCount);
+
+        // when
+        racingGame.moveAll();
+
+        // then
+        assertTrue(racingGame.isFinished());
     }
 }
