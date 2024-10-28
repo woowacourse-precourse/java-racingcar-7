896770ed90c6b1bd49f0c375a4c166924c6a7472
RedJunKi <bing8821@naver.com>
2024-10-28 17:23:15 +0900

    refactor: 자동차 이름 입력 예외처리를 꼼꼼히 해라
    InputView.java: 자동차 이름 입력시 구분자로 시작하는 경우 혹은 구분자로 끝나는 경우를 예외처리 했다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,13 +2,18 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static racingcar.domain.StringConstant.*;
 import static racingcar.exception.ExceptionMessage.*;
 
 public class InputView {
 
     public String carName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+        String input = Console.readLine();
+        if (input.startsWith(NAME_DELIMITER.getValue()) || input.endsWith(NAME_DELIMITER.getValue())) {
+            throw new IllegalArgumentException(CAR_NAME_VALIDATION);
+        }
+        return input;
     }
 
     public int tryCount() {
