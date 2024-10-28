1e02ae8ad0ccde66471fff507127c319c0201638
jijongkwon <help3451@naver.com>
2024-10-28 22:00:54 +0900

    feat(controller) : 경기 흐름 제어 구현
    - 입력: inputCarName, inputTryNumber
    - 게임: startGame
    - 출력: outputRacingResultUi, outputWinnerUi

--- src/main/java/racingcar/presentation/controller/RacingCarController.java
+++ src/main/java/racingcar/presentation/controller/RacingCarController.java
@@ -1,8 +1,29 @@
 package racingcar.presentation.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.application.service.car.RacingCarService;
+import racingcar.presentation.ui.InputView;
+import racingcar.presentation.ui.OutputView;
+
 public class RacingCarController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RacingCarService racingCarService;
+
+    public RacingCarController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.racingCarService = new RacingCarService();
+    }
+
     // TODO : 경기 흐름 제어
     public void run() {
-
+        inputView.inputCarNameUi();
+        racingCarService.inputCarName(Console.readLine());
+        inputView.inputTryUi();
+        racingCarService.inputTryNumber(Console.readLine());
+        racingCarService.startGame();
+        outputView.outputRacingResultUi();
+        outputView.outputWinnerUi();
     }
 }
