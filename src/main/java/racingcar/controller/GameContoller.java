948de35213b682cb8ba6c774e9ac5c5ee0ee2c2d
SongMinhyuk <dnfldpden32@gmail.com>
2024-10-28 15:03:54 +0900

    refactor: Controller 함수 service단으로 분리

--- src/main/java/racingcar/controller/GameContoller.java
+++ src/main/java/racingcar/controller/GameContoller.java
@@ -1,47 +1,38 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import racingcar.model.Car;
+import racingcar.service.GameService;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 
 public class GameContoller {
 
-    private static final int INITIAL_SCORE=0;
+    public static final int INITIAL_SCORE=0;
     private final Map<String, Integer> playerScoreBoard;
     private final Integer totalTurn;
+    private final GameService gameService=new GameService();
     private final InputView inputView=new InputView();
     private final OutputView outputView=new OutputView();
 
     public GameContoller() {
-        playerScoreBoard = new LinkedHashMap<>();
-        mapPlayerNamesFromInput();
+        List<String> playerNames= inputView.readPlayerNameList();
+        playerScoreBoard=gameService.mapPlayerNames(playerNames);
         totalTurn=inputView.readTurnFromInput();
     }
 
     public void run() {
         for (int curTurn = 0; curTurn < totalTurn; curTurn++) {
-            moveCarsByRandomVal();
+            gameService.movePlayersRandomly(playerScoreBoard);
             outputView.printCurResult(playerScoreBoard);
         }
         outputView.printFinalResult(playerScoreBoard);
     }
-    private void mapPlayerNamesFromInput() {
-        List<String> playerNames= inputView.readPlayerNameList();
-        playerNames.forEach((playerName)->{
-            playerScoreBoard.put(playerName,INITIAL_SCORE);
-        });
-    }
 
-    private void moveCarsByRandomVal() {
-        playerScoreBoard.forEach((player,score)->{
-            int randomVal= Randoms.pickNumberInRange(0,9);
-            if(randomVal>=4){
-                playerScoreBoard.put(player, score + 1);
-            }
-        });
-    }
 }
---
+++ src/main/java/racingcar/service/GameService.java
@@ -0,0 +1,27 @@
+package racingcar.service;
+
+import static racingcar.controller.GameContoller.INITIAL_SCORE;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GameService {
+
+    public Map<String, Integer> mapPlayerNames(List<String> playerNames) {
+        Map<String, Integer> playerScoreBoard=new LinkedHashMap<>();
+        playerNames.forEach((playerName)->{
+            playerScoreBoard.put(playerName,INITIAL_SCORE);
+        });
+        return playerScoreBoard;
+    }
+    public void movePlayersRandomly(Map<String, Integer> playerScoreBoard) {
+        playerScoreBoard.forEach((player,score)->{
+            int randomVal= Randoms.pickNumberInRange(0,9);
+            if(randomVal>=4){
+                playerScoreBoard.put(player, score + 1);
+            }
+        });
+    }
+}
