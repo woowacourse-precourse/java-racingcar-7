1d7590acd460e795e2d4315dba441c362f3cd95d
JoonKyo <joonkyolee@gmail.com>
2024-10-28 11:50:50 +0900

    test(CarNameTest): 자동차 이름 앞뒤 또는 중간에 스페이스가 존재할 때 예외처리 진행하는 테스트 구현

--- src/test/java/racingcar/domain/CarNameTest.java
+++ src/test/java/racingcar/domain/CarNameTest.java
@@ -4,10 +4,21 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarNameTest {
+    @ParameterizedTest
+    @ValueSource(strings = {" pobi", "pobi ", "po bi"})
+    void 자동차_이름에_스페이스가_존재할_때_예외_발생(String carName) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> CarName.from(carName))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Test
-    void 자동차_이름이_없을_때_예외_발생() {
+    void 자동차_이름이_없_때_예외_발생() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> CarName.from(""))
                         .isInstanceOf(IllegalArgumentException.class)
