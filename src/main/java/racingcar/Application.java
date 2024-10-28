22829941a0f1f0278da1a8e6906aba6bf45798ee
minjcho <117441212+minjcho@users.noreply.github.com>
2024-10-28 23:09:21 +0900

    fix: Application.java 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,8 +3,13 @@ package racingcar;
 import java.util.List;
 
 public class Application {
-	public static void main(String[] args) {
-		List<String> carNames = InputView.readCarNames();
-		int trialCount = InputView.readTrialCount();
-	}
+    public static void main(String[] args) {
+        List<String> carNames = InputView.readCarNames();
+        int trialCount = InputView.readTrialCount();
+        RacingGame racingGame = new RacingGame(carNames, trialCount);
+        System.out.println("실행 결과");
+        racingGame.play();
+        List<String> winners = racingGame.getWinners();
+        ResultView.printWinners(winners);
+    }
 }
