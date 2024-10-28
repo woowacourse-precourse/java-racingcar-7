642b92974efddc54269fa1846851460d0981d917
wlsdlf2 <wlsdlfzg@naver.com>
2024-10-28 11:46:01 +0900

    test: 동일 메서드 테스트임을 명확히 하기 위해 테스트 이름 변경

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -10,13 +10,13 @@ public class CarTest {
 
     @ParameterizedTest
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    void 자동차_전진_성공_4이상(int randomNum) {
+    void 자동차_전진_성공_4이상(int randomNumber) {
         // given
         Car car = new Car("car1");
         int position = car.getPosition();
 
         // when
-        car.move(randomNum);
+        car.move(randomNumber);
 
         // then
         assertEquals(position + 1, car.getPosition());
@@ -24,13 +24,13 @@ public class CarTest {
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3})
-    void 자동차_정지_성공_4미만(int randomNum) {
+    void 자동차_전진_실패_4미만(int randomNumber) {
         // given
         Car car = new Car("car1");
         int position = car.getPosition();
 
         // when
-        car.move(randomNum);
+        car.move(randomNumber);
 
         // then
         assertEquals(position, car.getPosition());
