41e88844588070616f46a86b61b856482e551464
yyysolhhh <solbaram37@gmail.com>
2024-10-28 15:58:37 +0900

    test(RaceTest): 레이스 관련 테스트 추가
    - Race, Rule 객체를 사용하여 메서드 정상 동작하는지 테스트

---
+++ src/test/java/racingcar/RaceTest.java
@@ -0,0 +1,44 @@
+package racingcar;
+
+import java.util.Arrays;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.car.Car;
+import racingcar.race.Race;
+import racingcar.race.Rule;
+
+public class RaceTest {
+
+    @Test
+    void 레이스_지정된_라운드_수만큼_진행되는지_테스트() {
+        Race race = new Race();
+        List<Car> cars = Arrays.asList(
+                new Car("bmw"),
+                new Car("audi")
+        );
+        int numOfRounds = 3;
+
+        race.startRace(cars, numOfRounds);
+
+        for (Car car : cars) {
+            Assertions.assertThat(car.getPosition()).isBetween(0, numOfRounds);
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void 숫자가_4_이상일_경우_전진_규칙_충족_테스트(int number) {
+        Rule rule = new Rule();
+        Assertions.assertThat(rule.canMove(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void 숫자가_4_미만일_경우_전진_규칙_미달_테스트(int number) {
+        Rule rule = new Rule();
+        Assertions.assertThat(rule.canMove(number)).isFalse();
+    }
+}
