349c081c49ce5a09bfd83c1f35e3a39e7dbde572
HEX <123macanic@naver.com>
2024-10-28 22:02:23 +0900

    test: List<String>에서 List<Car> 형으로의 변환 테스트 설명 추가 및 불필요한 주석 제거
    @DisplayName으로 Test에 대한 설명 추가 및 불필요한 주석을 제거함

--- src/test/java/racingcar/DataConverterTest.java
+++ src/test/java/racingcar/DataConverterTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.entity.Car;
 import racingcar.executor.util.DataConverter;
@@ -10,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DataConverterTest {
 
+    @DisplayName("제대로 입력한 자동차 이름 문자열을 쉼표를 기준으로 쪼개서 반환해주는지 Test")
     @Test
     public void convertFromCarNameListToCar_ShouldReturnListOfCars_WhenInputIsValid() {
         String[] carNames = {"Car1", "Car2", "Car3"};
@@ -25,13 +27,14 @@ public class DataConverterTest {
                 .containsOnly(0);
     }
 
+    @DisplayName("변환하는 과정에서 빈 자동차 이름 배열을 보냈을 떄 빈 배열을 반환하는지 Test")
     @Test
     public void convertFromCarNameListToCar_ShouldReturnEmptyList_WhenInputIsEmpty() {
         String[] carNames = {};
 
         List<Car> carList = DataConverter.convertFromCarNameListToCarList(carNames);
 
-        assertThat(carList).isEmpty(); // 리스트가 비어있는지 확인
+        assertThat(carList).isEmpty();
     }
 }
 
