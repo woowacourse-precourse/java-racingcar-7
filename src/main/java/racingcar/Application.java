e1a1205de73451d73a3f15a8081f2439d7d570a2
jarvis0720 <gktkddms9631@gmail.com>
2024-10-28 16:35:11 +0900

    refactor: 각 함수가 한 가지 작업만 수행하도록 코드 리팩토링

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,27 +6,53 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
+        String[] carNames = inputCarNames();
+        int raceCount = inputRaceCount();
+        Car[] cars = createCars(carNames);
+        progressRace(raceCount, cars);
+        printWinnner(cars);
+        Console.close();
+    }
+
+    public static String[] inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNamesInput = readLine();
+        String[] carNames = readLine().split(",");
+        return carNames;
+    }
+
+    public static int inputRaceCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         int raceCount = Integer.parseInt(readLine());
-        String[] carNames = carNamesInput.split(",");
+        return raceCount;
+    }
+
+    public static Car[] createCars(String[] carNames) {
         Car[] cars = new Car[carNames.length];
         for (int i = 0; i < carNames.length; i++) {
             cars[i] = new Car(carNames[i]);
         }
+        return cars;
+    }
+
+    public static void progressRace(int raceCount, Car[] cars) {
         RacingController racingController = new RacingController(cars);
-        System.out.println("실행 결과");
+        System.out.println("\n실행 결과");
         for (int i = 0; i < raceCount; i++) {
             racingController.startRace();
-            for (int j = 0; j < carNames.length; j++) {
-                String raceRoundResult = cars[j].getName() + ":" + "-".repeat(cars[j].getForwardCount());
-                System.out.println(raceRoundResult);
-            }
+            printRaceResult(cars);
             System.out.println();
         }
+    }
+
+    public static void printRaceResult(Car[] cars) {
+        for (Car car : cars) {
+            String raceResult = car.getName() + ":" + "-".repeat(car.getForwardCount());
+            System.out.println(raceResult);
+        }
+    }
+
+    public static void printWinnner(Car[] cars) {
         WinnerDecider winnerDecider = new WinnerDecider(cars);
         System.out.println("최종 우승자 : " + winnerDecider.decideWinner());
-        Console.close();
     }
 }
--- src/main/java/racingcar/WinnerDecider.java
+++ src/main/java/racingcar/WinnerDecider.java
@@ -10,19 +10,28 @@ public class WinnerDecider {
         this.cars = cars;
     }
 
-    public String decideWinner() {
+    private int findMaxForwardCount() {
         int maxForwardCount = 0;
         for (Car car : cars) {
             if (car.getForwardCount() > maxForwardCount) {
                 maxForwardCount = car.getForwardCount();
             }
         }
+        return maxForwardCount;
+    }
+
+    private List<String> selectWinner(int maxForwardCount) {
         List<String> winners = new ArrayList<>();
         for (Car car : cars) {
             if (car.getForwardCount() == maxForwardCount) {
                 winners.add(car.getName());
             }
         }
+        return winners;
+    }
+
+    public String decideWinner() {
+        List<String> winners = selectWinner(findMaxForwardCount());
         if (winners.size() == 1) {
             return winners.getFirst();
         } else {
