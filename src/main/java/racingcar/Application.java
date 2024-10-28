d4eb491bbddf5d84627325b6bb268df175ca5f8a
halfmoon-mind <5338095@gmail.com>
2024-10-28 22:26:35 +0900

    feat: 각 단계를 game의 실행코드로 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,29 +1,14 @@
 package racingcar;
 
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
         InputHandler inputHandler = new InputHandler();
 
-        List<String> carNames = inputHandler.getCarNames();
-        int attempts = inputHandler.getNumberOfAttempts();
+        Game game = new Game(inputHandler);
 
-        List<Car> cars = CarFactory.createCars(carNames);
-        Car car = new Car("pobi");
-
-        // attemps만큼 car move
-        for (int i = 0; i < attempts; i++) {
-            for (Car c : cars) {
-                c.move();
-            }
-            for (Car c : cars) {
-                System.out.println(c.getName() + " : " + c.getProgress());
-            }
-        }
-
-        // 결과 출력
-        List<String> winners = CarUtils.getWinners(cars);
-        System.out.println("최종 우승자 : " + String.join(", ", winners));
+        game.initializeCars();
+        game.initializeRemainAttempts();
+        game.startGame();
+        game.printWinners();
     }
 }
