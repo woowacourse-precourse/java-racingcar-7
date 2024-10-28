12ce975bf731853e2103f88150e8ec14cf119d7d
wogus <ind07152@naver.com>
2024-10-28 11:17:11 +0900

    refactor(ErrorMessage): 자바 컨벤션 관련 수정

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -6,6 +6,7 @@ public enum ErrorMessage {
     INVALID_NAME_LENGTH("자동차 이름은 1글자 이상, 5글자 이하여야합니다."),
     INVALID_NAME_COUNT("자동차 이름은 2개 이상 입력해야합니다."),
     NAME_DUPLICATE("중복된 이름이 없어야합니다.");
+
     private final String message;
 
     ErrorMessage(String message) {
