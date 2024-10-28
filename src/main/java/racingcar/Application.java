ec2c099a64a0e22b34843793cf567a462d05051d
문준일 <answnsdlfwkd@naver.com>
2024-10-28 21:42:31 +0900

    feat: 이동 횟수 입력 및 유효성 검사 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Application {
     private static List<String> carNames;
+    private static int tryCount;
 
     public static void inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -22,7 +23,24 @@ public class Application {
         }
     }
 
+    public static void inputTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        try {
+            tryCount = Integer.parseInt(Console.readLine());
+            validateTryCount();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("이동 횟수는 양의 정수여야 합니다.");
+        }
+    }
+
+    private static void validateTryCount() {
+        if (tryCount <= 0) {
+            throw new IllegalArgumentException("이동 횟수는 양의 정수여야 합니다.");
+        }
+    }
+
     public static void main(String[] args) {
         inputCarNames();
+        inputTryCount();
     }
 }
