0574ae68489960b37a3adcfae74134215c7c314c
dayaelee <dayae.dev@gmail.com>
2024-10-28 21:40:59 +0900

    feat:1-1. 자동차 이름이 5자를 초과한 경우 에러 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import static java.lang.Integer.parseInt;
 
@@ -11,28 +14,12 @@ public class Application {
         String inputCar;
         String[] carArr = null;
 
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        inputCar = Console.readLine();
+        String car = inputCar;
+        carArr = car.split(",");
 
-        try{
-            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-            inputCar = Console.readLine();
-//            System.out.println("inputCar: "+ inputCar);
-            String car = inputCar;
-            carArr = car.split(",");
-
-            //1-1. 자동차 이름이 5자를 초과한 경우 에러 출력
-            for (String checkCar : carArr) {
-                if (checkCar.length()>5){
-                    throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
-                }
-                if (isNmberic(checkCar)){
-                    throw new IllegalArgumentException("자동차 이름은 숫자일 수 없습니다.");
-                }
-            }
-
-        } catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-            throw e;
-        }
+        isValidCars(carArr);
 
         //feat:2. 시도할 횟수 변수에 횟수 값 입력 받기
 
@@ -48,47 +35,47 @@ public class Application {
 
         //3. 전체 자동차를 포함하는 해시맵을 만들고, 키는 자동차의 이름으로 하고 값은 0으로 초기화 한다.
         HashMap<String, String> map = new HashMap<>();
-        for (String car : carArr){
-            map.put(car, "");
+        for (String targetCar : carArr){
+            map.put(targetCar, "");
         }
 
         System.out.println("실행 결과");
         //7. 시도할 횟수동안 다음의 4-6과정을 반복한다.
         for (int i = 0; i < parseInt(inputTry); i++) {
-            for (String car : carArr) {
+            for (String targetCar : carArr) {
                 //4. 각 자동차의 전진 조건에 대해 정하기 위해 0-9사이의 무작위 값을 구한다.
                 Integer pickNum = Randoms.pickNumberInRange(0, 9);
                 //5. 만약 무작위 값이 4이상일 경우 전진해야하니, 해당 자동차의 키 값에 대한 value에 +1을 해준다.
                 if (pickNum >= 4) {
-                    String value = map.get(car);
+                    String value = map.get(targetCar);
                     value += "-";
-                    map.put(car, value);
+                    map.put(targetCar, value);
                 }
 
             }
 
             //6. 전진 시도 차수별 실행 결과를 출력한다. 자동차 이름 키 값에 대한 밸류값만큼 '-'를 출력한다.
-            for (String car : carArr) {
-                System.out.println(car + " : " + map.get(car));
+            for (String targetCar : carArr) {
+                System.out.println(targetCar + " : " + map.get(targetCar));
             }
             System.out.println();
         }
 
         //8. 시도가 끝나면 우승자를 뽑는다. 해시맵에서 value값이 가장 큰 키 값을 최종 우승자로 출력한다.
         Integer totalLength=0;
-        for (String car : carArr) {
-            if (totalLength <= map.get(car).length()){
-                totalLength= map.get(car).length();
+        for (String targetCar : carArr) {
+            if (totalLength <= map.get(targetCar).length()){
+                totalLength= map.get(targetCar).length();
             }
         }
 
         String totalWinner = null;
-        for (String car : carArr) {
-            if (totalLength == map.get(car).length()){
+        for (String targetCar : carArr) {
+            if (totalLength == map.get(targetCar).length()){
                 if (totalWinner == null){
-                    totalWinner = car;
+                    totalWinner = targetCar;
                 } else{
-                    totalWinner += (", " + car);
+                    totalWinner += (", " + targetCar);
                 }
             }
         }
@@ -107,6 +94,29 @@ public class Application {
         return  inputTry != null && inputTry.matches("[+-]?\\d*(\\.\\d+)?");
     }
 
+    public static boolean isValidCars(String[] carArr){
+        try{
+            //1-1. 자동차 이름이 5자를 초과한 경우 에러 출력
+            for (String checkCar : carArr) {
+                checkLengthValidCars(checkCar);
+                checkNumericValidCars(checkCar);
+            }
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            throw e;
+        }
+        return true;
+    }
 
+    public static boolean checkLengthValidCars(String checkCar){
+        if (checkCar.length()>5) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }else{
+            return true;
+        }
+
+    }
+
+    
 
 }
