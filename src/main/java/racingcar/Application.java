5177951e268f6249a96d91f029e22316514792a3
Shin1012 <83046264+Eunoia24@users.noreply.github.com>
2024-10-28 19:55:19 +0900

    1번 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String carNamesInput = getCarNames();
+        // 이후 기능 구현
+    }
+
+    private static String getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+        // 입력 유효성 검증
+        String[] carNames = input.split(",");
+        for (String name : carNames) {
+            if (name.trim().length() == 0 || name.trim().length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+            }
+        }
+        return input;
     }
 }
