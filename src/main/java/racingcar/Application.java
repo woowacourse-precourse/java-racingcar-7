d2c25d00566399112e9c6f92cea5f844c09587d8
smk412 <whu1477@naver.com>
2024-10-28 20:36:03 +0900

    feat: 쉼표 구분자를 이용하여 입력한 자동차를 구별하고 배열에 넣기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,9 +8,13 @@ public class Application {
         Scanner sc = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = sc.nextLine();
+
         System.out.println("시도할 횟수는 몇 회인가요?");
         int count = sc.nextInt();
-        System.out.println(count);
 
+        String[] cars = carName.split(",");
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
     }
 }
