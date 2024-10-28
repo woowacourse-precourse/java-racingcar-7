d1a32479e85da8e3bdc88377644644ad19b0d117
persi <jina8151010@gmai.com>
2024-10-28 17:18:09 +0900

    chore: 필요없는 주석 제거

--- src/main/java/racingcar/RaceResult.java
+++ src/main/java/racingcar/RaceResult.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class RaceResult {
-
-    // 우승자를 계산하는 메서드
     public static List<String> determineWinners(List<Car> cars) {
         int maxPosition = cars.stream()
                 .mapToInt(Car::getPosition)
