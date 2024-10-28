164029acb4791d6d29501a2f9833925aa8b69695
haennni <ggowater33@kangnam.ac.kr>
2024-10-28 17:29:21 +0900

    feat(application): 메인 메서드에 게임 실행 로직 추가
    Application 클래스의 main 메서드에 게임 실행 로직을 추가했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
+        InputParser inputParser = new InputParser();
+
+        GameConfiguration gameConfiguration = new GameConfiguration(inputHandler, inputParser);
+
+        // 자동차 리스트와 Result 객체 생성
+        List<Car> cars = new ArrayList<>();
+        Result result = new Result();
+
+        // RacingGame 객체 생성 및 실행
+        RacingGame racingGame = new RacingGame(gameConfiguration, cars, result);
+        racingGame.run();
+
     }
 }
