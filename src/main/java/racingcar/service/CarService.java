782441a9ea2e6945ceb88e9feafb51bcdeab7baf
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-10-28 20:27:30 +0900

    feat: 시도 횟수가 음수일 때, **예외처리**

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -55,6 +55,25 @@ public class CarService {
         this.moveCount = moveCount;
     }
 
+    public void determineWinner() {
+        int maxMoved = getMaxMoved();
+        for (Car car : carList.getCars()) {
+            if (car.getCarMoved() == maxMoved) {
+                winners.add(car.getCarName());
+            }
+        }
+    }
 
+    public int getMaxMoved() {
+        int maxMoved = 0;
+        for (Car car : carList.getCars()) {
+            maxMoved = Math.max(maxMoved, car.getCarMoved());
+        }
+        return maxMoved;
+    }
+
+    public List<String> getWinners() {
+        return winners;
+    }
 
 }
