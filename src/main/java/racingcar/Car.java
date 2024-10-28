dce52493002b0f666df219b227fa1cb5e81e8aad
heeyeop Kim <ssucgoo@gmail.com>
2024-10-28 20:12:15 +0900

    feat: 이름 유효성 검사 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,7 +5,14 @@ public class Car {
     private int position;
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
         this.position = 0;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("5글자 이상입니다.");
+        }
     }
 }
