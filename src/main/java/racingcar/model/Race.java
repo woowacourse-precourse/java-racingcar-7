76b2dbd1aa8d68ac345128a0625611159d4ef2ca
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-28 12:22:01 +0900

    feat : findWinners 메소드 구현

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -12,5 +12,24 @@ public class Race {
         this.attempts = attempts;
     }
 
+    public List<Car> findWinners() {
+        List<Car> winners = new ArrayList<>();
+        int maxPosition = 0;
+
+        // 자동차들의 최대 거리 구하기
+        for(Car car : cars) {
+            if(car.getPosition() > maxPosition){
+                maxPosition = car.getPosition();
+            }
+        }
+
+        // 최대거리에 위치한 자동차들 찾고 리스트에 추가
+        for(Car car : cars) {
+            if(car.getPosition() == maxPosition) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
 
 }
