e919e950884d38c9b5561e5497393d95e2569f2b
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-28 18:54:06 +0900

    feat: 입력 메시지 상수 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,9 @@
+package racingcar.view;
+
+public class InputView {
+    private static final String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ROUND_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String ROUND_FORMAT = "\\d+";
+
+
+}
