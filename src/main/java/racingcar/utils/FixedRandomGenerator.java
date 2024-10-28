d12412a84b7a581b8de8995f0fba1cd3a4c6c361
rtaeho <ryou0920@naver.com>
2024-10-28 15:04:06 +0900

    feat: 지정한 랜덤값 반환 클래스 구현

---
+++ src/main/java/racingcar/utils/FixedRandomGenerator.java
@@ -0,0 +1,14 @@
+package racingcar.utils;
+
+public class FixedRandomGenerator implements RandomGenerator {
+    private final int fixedValue;
+
+    public FixedRandomGenerator(int fixedValue) {
+        this.fixedValue = fixedValue;
+    }
+
+    @Override
+    public int generate() {
+        return fixedValue;
+    }
+}
