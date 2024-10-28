7546649f0325ea75c73a0b02d8cb86ae448a4b33
DaehoGo <rualneox@dgu.ac.kr>
2024-10-28 19:56:09 +0900

    feat: 시도할 횟수 입력

--- src/main/java/racingcar/domain/Validator.java
+++ src/main/java/racingcar/domain/Validator.java
@@ -4,6 +4,7 @@ public class Validator {
     public static final String INVALID_INPUT_ERROR = "값이 입력되지 않았습니다.";
     public static final String INVALID_NAME_LENGTH_ERROR = "이름은 1~5자 사이여야 합니다.";
     public static final String EMPTY_NAME_ERROR = "이름이 비어있을 수 없습니다.";
+    public static final String INVALID_TRY_COUNT_ERROR = "시도 횟수는 1 이상의 정수여야 합니다.";
 
     public static void validateInput(String input) {
         if(input.isEmpty()) {
@@ -25,6 +26,17 @@ public class Validator {
     private static void validateNameLength(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException(INVALID_NAME_LENGTH_ERROR);
+        }
+    }
+
+    public static void validateTryCount(String input) {
+        try {
+            int tryCount = Integer.parseInt(input);
+            if (tryCount < 1) {
+                throw new IllegalArgumentException(INVALID_TRY_COUNT_ERROR);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_TRY_COUNT_ERROR);
         }
     }
 }
--- src/test/java/racingcar/domain/ValidatorTest.java
+++ src/test/java/racingcar/domain/ValidatorTest.java
@@ -14,4 +14,31 @@ class ValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(Validator.INVALID_INPUT_ERROR);
     }
+
+    @Test
+    void 시도_횟수가_유효하지_않을_때_예외발생() {
+        // given && when
+        assertThatThrownBy(() -> Validator.validateTryCount("-1"))
+        // then
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Validator.INVALID_TRY_COUNT_ERROR);
+
+        // given && when
+        assertThatThrownBy(() -> Validator.validateTryCount("0"))
+        // then
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Validator.INVALID_TRY_COUNT_ERROR);
+
+        // given && when
+        assertThatThrownBy(() -> Validator.validateTryCount("abc"))
+        // then
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Validator.INVALID_TRY_COUNT_ERROR);
+
+        // given && when
+        assertThatThrownBy(() -> Validator.validateTryCount("   "))
+        // then
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Validator.INVALID_TRY_COUNT_ERROR);
+    }
 }
