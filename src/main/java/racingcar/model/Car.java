d4ae09f98a01dc577083e328c23a3f1b58cf6a53
박성춘 <songchoon10@naver.com>
2024-10-28 14:23:10 +0900

    refactor: 자동차 이름에 영어, 한글, 숫자만 들어가도록 변경
    - docs: '자동차 이름이 `영어, 한글, 숫자`외의 글자가 포함된 경우' 항목 완료

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,7 +2,11 @@ package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Car {
+    private static final Pattern VALID_CAR_NAME_REGEXP = Pattern.compile("[a-zA-Z가-힣0-9]+");
     private static final int MIN_RANDOM_NUM = 0;
     private static final int MAX_RANDOM_NUM = 9;
     private static final int CAR_NAME_MAX_LENGTH = 5;
@@ -46,9 +50,18 @@ public class Car {
         if (carName.contains(" ")) {
             throw new IllegalArgumentException("이름에 공백을 제외시켜주세요.");
         }
+
+        if (!isMatchesCarNamePattern(carName)) {
+            throw new IllegalArgumentException("한글, 영어, 숫자로만 입력해주세요.");
+        }
     }
 
     private boolean isForwardable() {
         return Randoms.pickNumberInRange(MIN_RANDOM_NUM, MAX_RANDOM_NUM) >= MIN_FORWARD_NUM;
+    }
+
+    private boolean isMatchesCarNamePattern(String carName) {
+        Matcher matcher = VALID_CAR_NAME_REGEXP.matcher(carName);
+        return matcher.matches();
     }
 }
