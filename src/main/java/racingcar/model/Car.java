c70c31c2edd1a7938ddb45a893c7ad5d0fc3dec4
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-28 15:05:27 +0900

    style: Car 클래스의 들여쓰기 수정

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -13,12 +13,15 @@ public class Car {
         this.name = name;
         this.position = 0;
     }
+
     public String getName() {
         return name;
     }
+
     public int getPosition() {
         return position;
     }
+
     public void moveForward() {
         position++;
     }
