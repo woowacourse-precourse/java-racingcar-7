21f0de27910aa2bf299670607603dd7f966f2c4b
kaswhy <sxshx818@naver.com>
2024-10-28 22:23:40 +0900

    refactor(RacingGameController): 입력받은 횟수만큼 경주하는 부분 메서드 분리
    변경 후
    - 기존 컨트롤러의 run() 코드가 길어 입력받은 횟수만큼 경주하는 부분을 메서드로 분리

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -8,19 +8,25 @@ import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingGameController {
+    private static int INIT_TURN_TO_ZERO = 0;
+
     public static void run() {
         OutputView.printCarNameGuide();
         CarScores carScores = new CarScores(InputView.getCarNames());
-
         OutputView.printRaceCountGuide();
         RaceCount raceCount = new RaceCount(InputView.getRaceCount());
 
+        carScores = raceForRaceCountTimes(carScores, raceCount);
+
+        RaceWinnerService.findLastWinner(carScores);
+    }
+
+    private static CarScores raceForRaceCountTimes(CarScores carScores, RaceCount raceCount) {
         OutputView.printExecutionResult();
-        for (int nowRaceTurn = 0; nowRaceTurn < raceCount.getValue(); nowRaceTurn++) {
+        for (int nowRaceTurn = INIT_TURN_TO_ZERO; nowRaceTurn < raceCount.getValue(); nowRaceTurn++) {
             carScores = RoundRaceService.playGame(carScores);
             System.out.println();
         }
-
-        RaceWinnerService.findLastWinner(carScores);
+        return carScores;
     }
 }
