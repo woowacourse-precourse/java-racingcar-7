2edf6406154686c873fe0514054b8a5f74f9eb93
honga <honga@BOOK-5SF7AISI00>
2024-10-28 11:22:01 +0900

    FEAT : 플레이어의 현재 정보를 저장하는 함수 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -24,8 +24,24 @@ class Function {
 		if (checkNameDuplicationList.contains(inputName)) {
 			throw new IllegalArgumentException();
 		}
-
 	}
+	
+	public static ArrayList<Player> makePlayerList(String nameString) {
+		String[] name = nameString.split(",");
+		ArrayList<Player> playerList = new ArrayList<Player>(); 
+		ArrayList<String> checkNameDuplicationList = new ArrayList<String>(); //중복된 이름이 있는지 확인을 위한 ArrayList
+
+		for (int i = 0; i < name.length; i++) {
+			Function.checkNameLengthOverFive(name[i].trim());
+			Function.checkNameDuplication(checkNameDuplicationList, name[i].trim());
+			checkNameDuplicationList.add(name[i].trim());
+			
+			playerList.add(new Player(name[i].trim()));
+		}
+
+		return playerList;
+	}
+
 
 }
 
