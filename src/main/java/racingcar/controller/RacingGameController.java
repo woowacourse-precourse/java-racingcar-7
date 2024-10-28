511b2a09782eedfd89ca511b35bc87c752d7d42d
hooniverse <sung010326@gmail.com>
2024-10-28 22:33:59 +0900

    feat: 라운드별 자동차 움직임 기능 추가

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,17 +1,19 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.model.Car;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingGameController {
 
     Validation validation = new Validation();
     InputView inputView = new InputView();
-
+    OutputView outputView = new OutputView();
     public List<Car> cars = new ArrayList<>();
     int rounds = 0;
 
@@ -22,7 +24,11 @@ public class RacingGameController {
         System.out.println("시도할 횟수는 몇 회 인가요?");
         rounds = inputView.getRoundNumber();
 
-
+        System.out.println("\n실행 결과");
+        for (int round = 0; round < rounds; round++) {
+            moveCar();
+            outputView.roundResult(cars);
+        }
     }
 
     public List<Car> splitCar(String carList) {
@@ -35,5 +41,9 @@ public class RacingGameController {
                 .collect(Collectors.toList());
     }
 
-
+    public void moveCar() {
+        for (Car car : cars) {
+            car.move(Randoms.pickNumberInRange(0, 9));
+        }
+    }
 }
