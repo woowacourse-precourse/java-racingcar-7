a5defcacd47bafd44a955ad65ccc24bdf9c3bd0d
Limwngur <ovg07047@naver.com>
2024-10-28 23:35:31 +0900

    refactor: calculateWinners 메서드에서 보조 메서드로 findRacingCarWithMaxLocation와 getMaxLocationAfterGame 분리
    코드의 의도를 명확히 하기 위해 메서드로 분리하여 네이밍을 할 수 있도록 분리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -8,16 +8,26 @@ public abstract class OutputView {
 
     public void calculateWinners(final List<RacingCar> carStateAfterGame) {
 
+        final int maxLocation = getMaxLocationAfterGame(carStateAfterGame);
+
+        final List<RacingCar> winners = findRacingCarWithMaxLocation(carStateAfterGame, maxLocation);
+
+        displayOutput(winners);
+    }
+
+    private List<RacingCar> findRacingCarWithMaxLocation(final List<RacingCar> carStateAfterGame, final int maxLocation) {
+        final List<RacingCar> winners = carStateAfterGame.stream()
+                .filter(car -> car.getLocation() == maxLocation)
+                .toList();
+        return winners;
+    }
+
+    private int getMaxLocationAfterGame(final List<RacingCar> carStateAfterGame) {
         final int maxLocation = carStateAfterGame.stream()
                 .mapToInt(RacingCar::getLocation)
                 .max()
                 .getAsInt();
-
-        final List<RacingCar> winners = carStateAfterGame.stream()
-                .filter(car -> car.getLocation() == maxLocation)
-                .toList();
-
-        displayOutput(winners);
+        return maxLocation;
     }
 
     abstract void displayOutput(final List<RacingCar> winners);
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.dto.InputDto;
 
-public class ViewTest {
+public class InputViewTest {
 
 
     @AfterEach
