6602b5ed1affe2cb28e54db4878ed8b9b53bc990
yejin923 <yejin923@naver.com>
2024-10-28 16:52:01 +0900

    FEAT: 시도 횟수 입력 메소드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,15 +2,22 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static java.lang.Integer.parseInt;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
 
-    public static String[] input(){
+    public static String[] inputName(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String line = Console.readLine().trim();
-        String[] carName = line.split(",");
-        return carName;
+        return line.split(",");
+    }
+
+    public static int inputNum(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String input = Console.readLine().trim();
+        return Integer.parseInt(input);
     }
 }
