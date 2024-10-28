9e8cd687d4ced0fc3bae1dd2c3fc5f6a84df3442
JinHyung-Park <trupjh@gmail.com>
2024-10-28 14:29:42 +0900

    feat(WinnerCalculator) : Car 리스트에서 최대 거리를 추출하는 로직 구현

---
+++ src/main/java/racingcar/model/WinnerCalculator.java
@@ -0,0 +1,13 @@
+package racingcar.model;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class WinnerCalculator {
+    public static int findMaxDistance(List<Car> cars){
+        return Collections.max(cars, Comparator.comparingInt(car -> car.getDistance().getDistanceValue()))
+                .getDistance().getDistanceValue();
+    }
+}
