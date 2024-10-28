5cdbcf1b412445244c55ec7f4f7c5e186d257c40
Si Yeong Park <psy915@swu.ac.kr>
2024-10-28 21:58:47 +0900

    feat(GameController): 게임실행 시 유효성 검증 추가

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -3,6 +3,7 @@ package racingcar.controller;
 import racingcar.model.service.GameService;
 import racingcar.model.RandomNumberGenerator;
 import racingcar.model.domain.GameResult;
+import racingcar.validator.Validator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -11,11 +12,15 @@ import java.util.List;
 
 public class GameController {
 
-    public void runGame() {
+    public void runGame() throws IllegalArgumentException {
+
         String carNamesInput = InputView.inputNames();
         List<String> carNames = Arrays.asList(carNamesInput.split("\\s*,\\s*"));
 
         int attemptCount = InputView.inputGameCnt();
+        Validator.validateAttemptCount(String.valueOf(attemptCount));
+
+        Validator.validateCarNames(carNames);
 
         GameService gameService = new GameService(carNames, attemptCount, new RandomNumberGenerator());
         GameResult gameResult = gameService.playGame();
