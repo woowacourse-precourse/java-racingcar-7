c520165f869e37807ec4be21d5dfd3b972412d07
sanghee0820 <lsh277604@gmail.com>
2024-10-28 21:16:43 +0900

    feat: 차량 대수 확인 오류 추가

--- src/main/java/racingcar/exception/RacingTrackException.java
+++ src/main/java/racingcar/exception/RacingTrackException.java
@@ -1,7 +1,9 @@
 package racingcar.exception;
 
 public enum RacingTrackException {
-    DUPLICATED_CAR_NAME("중복된 이름이 존재합니다.");
+    DUPLICATED_CAR_NAME("중복된 이름이 존재합니다."),
+    NOT_ENOUGH_CAR("자동차는 2대 이상이어야 합니다."),
+    TOO_MANY_CAR("자동차는 10대 이하이어야 합니다.");
 
     private final String message;
 
