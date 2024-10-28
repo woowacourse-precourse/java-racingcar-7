892601912c5008b25e5456c58189b1a8a0f9e305
mingyeongwon <wonmingy@gmail.com>
2024-10-28 22:25:05 +0900

    feat: 자동차 전진 조건
    무작위 값 생성
    전진 조건 : 무작위 값이 4 이상일 경우만 전진

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,12 +1,26 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position;
+    private static final int MINIMUM_MOVE_VALUE = 4;
 
     public Car(String name, int position) {
         this.name = name;
         this.position = 0;
+    }
+
+    public void move() {
+        if (canMove()) {
+            position++;
+        }
+    }
+
+    private boolean canMove() {
+        int randomValue = Randoms.pickNumberInRange(0, 9);
+        return randomValue >= MINIMUM_MOVE_VALUE;
     }
 
     public String getName() {
