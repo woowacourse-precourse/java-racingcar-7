a19d1a1038e79e4f990cd53b8815d2e20c11ffba
Seol-Munhyeok <abc1234568@naver.com>
2024-10-28 15:41:28 +0900

    test(RacingGameTest): Add test method for announce winners

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,5 +19,29 @@ public class RacingGameTest {
         WinnerManager winnerManager = new WinnerManager();
         RacingGame racingGame = new RacingGame(cars, moveCount, winnerManager);
         racingGame.playGame();
+    }
+
+    @Test
+    @DisplayName("announceWinners()가 제대로 우승자를 출력하는지 확인한다")
+    void announceWinnersTest() {
+        Car car1 = new Car("pobi");
+        Car car2 = new Car("woni");
+        Car car3 = new Car("jun");
+        Car car4 = new Car("jason");;
+        car1.setDistance(10);
+        car2.setDistance(4);
+        car3.setDistance(7);
+        car4.setDistance(10);
+
+        List<Car> cars = Arrays.asList(car1, car2, car3, car4);;
+        WinnerManager winnerManager = new WinnerManager();
+        int moveCount = 10;
+        RacingGame racingGame = new RacingGame(cars, moveCount, winnerManager);
+        String expectedWinners = "pobi, jason";
+
+        racingGame.announceWinners();  // 출력 결과 확인
+        List<Car> winners = winnerManager.determineWinners(cars);
+        String winnerNames = winnerManager.getWinnerNames(winners);
+        assertEquals(expectedWinners, winnerNames);
     }
 }
