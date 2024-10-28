56f15be0baa6f862990174c913d508515cd9c75b
dltmddus <tmddus4671@gmail.com>
2024-10-28 11:07:09 +0900

    refactor: Car 클래스의 필드를 getter를 이용해 접근하는 방식으로 수정

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -10,7 +10,6 @@ import static racingcar.view.OutputView.printCurrentResult;
 
 public class Cars {
     private final List<Car> cars;
-    private static final int MINIMUM_MOVEMENT_THRESHOLD = 4;
 
     public Cars(List<Car> cars) {
         this.cars = cars;
@@ -18,26 +17,23 @@ public class Cars {
 
     public void move() {
         for (Car car : cars) {
-            if (getNumberOfMoves()) {
-                car.movementOfNumber++;
-            }
+            car.move(getNumberOfMoves());
         }
         printCurrentResult(cars);
     }
 
-    private boolean getNumberOfMoves() {
-        int randomNumber = RandomGeneratorUtil.getRandomNumber();
-        return randomNumber >= MINIMUM_MOVEMENT_THRESHOLD;
+    private int getNumberOfMoves() {
+        return RandomGeneratorUtil.getRandomNumber();
     }
 
     private int findMaxMovement() {
         return cars.stream()
-                .max(Comparator.comparingInt(car -> car.movementOfNumber)).map(car -> car.movementOfNumber).orElse(0);
+                .max(Comparator.comparingInt(Car::getMovementOfNumber)).map(Car::getMovementOfNumber).orElse(0);
     }
 
     public List<String> calculateFinalWinner() {
         int maxMovement = findMaxMovement();
-        return cars.stream().filter(car -> car.movementOfNumber == maxMovement).map(carMovement -> carMovement.name)
+        return cars.stream().filter(car -> car.getMovementOfNumber() == maxMovement).map(Car::getName)
                 .collect(Collectors.toList());
 
     }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,8 @@
 package racingcar.view;
 
-import java.util.List;
 import racingcar.domain.Car;
+
+import java.util.List;
 
 public class OutputView {
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
@@ -24,7 +25,7 @@ public class OutputView {
 
     public static void printCurrentResult(List<Car> cars) {
         for (Car car : cars) {
-            System.out.printf("%s : %s\n", car.name, MOVING_COUNT.repeat(car.movementOfNumber));
+            System.out.printf("%s : %s\n", car.getName(), MOVING_COUNT.repeat(car.getMovementOfNumber()));
         }
         System.out.println();
     }
