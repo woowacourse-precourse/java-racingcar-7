d977690e80d9697ee4ca4941f39f0e25b3b8e15b
youjae <youjae0803@gmail.com>
2024-10-28 15:37:49 +0900

    refactor(NumberGenerator): 상수를 나타내는 변수 이름 대문자로 변경

--- src/main/java/racingcar/model/NumberGenerator.java
+++ src/main/java/racingcar/model/NumberGenerator.java
@@ -3,14 +3,14 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberGenerator {
-    private final int startValue;
-    private final int endValue;
+    private final int START_VALUE;
+    private final int END_VALUE;
     public NumberGenerator(int startValue, int endValue) {
-        this.startValue = startValue;
-        this.endValue = endValue;
+        this.START_VALUE = startValue;
+        this.END_VALUE = endValue;
     }
 
     public int getRandomNumber(){
-        return Randoms.pickNumberInRange(startValue, endValue);
+        return Randoms.pickNumberInRange(START_VALUE, END_VALUE);
     }
 }
