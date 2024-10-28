1781d3bf0bc83c0a9ff1bc958b3c801e36d5f35b
gladhee <gladhee@icloud.com>
2024-10-28 13:35:59 +0900

    feat(CarsService): 자동차 생성 및 우승자를 도메인에서 가져오는 서비스 작성

---
+++ src/main/java/racingcar/service/CarsService.java
@@ -0,0 +1,25 @@
+package racingcar.service;
+
+import racingcar.domain.Cars;
+import racingcar.domain.RaceResult;
+import racingcar.domain.strategy.MoveStrategy;
+import racingcar.dto.WinnerDto;
+
+public class CarsService {
+
+    public Cars createCars(String carNames, MoveStrategy moveStrategy) {
+        return new Cars(carNames, moveStrategy);
+    }
+
+    public WinnerDto getWinnerNames(Cars cars) {
+        RaceResult raceResult = getRaceResult(cars);
+
+        return new WinnerDto(raceResult.getWinners());
+
+    }
+
+    private RaceResult getRaceResult(Cars cars) {
+        return new RaceResult(cars);
+    }
+
+}
