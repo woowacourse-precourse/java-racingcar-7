fb81eed59d71eda2633db02dcf8c22bb303c07a1
장영재 <urinaner@naver.com>
2024-10-28 14:37:23 +0900

    feat(CarService): 자동차 중복 예외 처리

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -1,14 +1,27 @@
 package racingcar.service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import racingcar.domain.Car;
+import racingcar.utils.ErrorMessage;
 import racingcar.utils.Util;
 
 public class CarService {
     public List<Car> createCars(String carsName) {
-        return Util.splitByComma(carsName).stream()
+        List<String> carNames = Util.splitByComma(carsName);
+        validateCarNames(carNames);
+        return carNames.stream()
                 .map(Car::new)
                 .collect(Collectors.toList());
+
+    }
+
+    private void validateCarNames(List<String> carNames) {
+        Set<String> uniqueNames = new HashSet<>(carNames);
+        if (uniqueNames.size() < carNames.size()) {
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_DUPLICATED.message());
+        }
     }
 }
