f8e59d14a2170d0e5732122abd783c7bee06c138
young-in <hardworkingini@gmail.com>
2024-10-28 16:49:47 +0900

    feat : 경주용 자동차 이름이 중복되는 경우의 예외 처리 테스트 구현

---
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -0,0 +1,21 @@
+package racingcar.domain;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RacingCarsTest {
+    @DisplayName("자동차 이름은 중복 사용 예외 확인")
+    @Test
+    void validateDuplicateRacingCarNames() throws Exception{
+        //given
+        String racingCarName = "yoni";
+        RacingCar car = new RacingCar(racingCarName, 1);
+        List<RacingCar> cars = List.of(car, car);
+        //when //then
+        Assertions.assertThatThrownBy(() -> new RacingCars(cars))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 중복으로 사용될 수 없습니다.");
+    }
+}
