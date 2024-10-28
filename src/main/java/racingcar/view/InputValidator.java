6e34f4dcf754f7a1699db09ec84a149f92ca25e7
gyuho <kyuhokim12@gmail.com>
2024-10-28 15:07:37 +0900

    refactor(InputValidator): 변수를 상수로 변경
    -EMPTY_VALUE_ERROR_MESSAGE는 프로그램 실행 중 변경될 일이 없으므로 상수로 변경

--- src/main/java/racingcar/view/InputValidator.java
+++ src/main/java/racingcar/view/InputValidator.java
@@ -1,7 +1,7 @@
 package racingcar.view;
 
 public class InputValidator {
-    private static String EMPTY_VALUE_ERROR_MESSAGE = "모든 입력은 비어있을 수 없습니다.";
+    private static final String EMPTY_VALUE_ERROR_MESSAGE = "모든 입력은 비어있을 수 없습니다.";
     public static void validateInput(String inputString){
         if(inputString.isBlank()){
             throw new IllegalArgumentException(EMPTY_VALUE_ERROR_MESSAGE);
