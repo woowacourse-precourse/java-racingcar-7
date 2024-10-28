9508e2591d407d3ea98081174d8566c82f83567b
woody6624 <woody6624@gmail.com>
2024-10-28 14:55:55 +0900

    fix(AdvanceAction) : depth2 조건을 맞추기 위한 함수 분리

--- src/main/java/racingcar/AdvanceAction.java
+++ src/main/java/racingcar/AdvanceAction.java
@@ -1,60 +1,74 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class AdvanceAction {
-    public Map<String,String> setCarList = new HashMap<>(); // 받아올 변수 선언
-    public String [] carList;
+    public Map<String, String> setCarList = new HashMap<>(); // 받아올 변수 선언
+    public String[] carList;
     private int maxDistance = 0;
     private String winner;
-    public void setFirst(String [] carList){
-        this.carList =carList;
-        for(String car:carList){
-            setCarList.put(car,"");
+
+    public void setFirst(String[] carList) {
+        this.carList = carList;
+        for (String car : carList) {
+            setCarList.put(car, "");
         }
     }
-    public boolean checkUpperFour(){
-            int randNum = Randoms.pickNumberInRange(0,9);
-            if(randNum >= 4){
-                return true;
-            }
-            return false;
+
+    public boolean checkUpperFour() {
+        int randNum = Randoms.pickNumberInRange(0, 9);
+        return randNum >= 4;
     }
 
-    public void moveCar(int num){
-        boolean isUpperFour;
-        for(int i=0; i<num; i++){
+    public void updateCarDistance(String car) {
+        String tmp = setCarList.get(car);
+        tmp += "-";
+        setCarList.put(car, tmp);
+    }
+
+    public void moveEachCar() {
+        for (String car : carList) {
+            if (checkUpperFour()) {
+                updateCarDistance(car);
+            }
+            System.out.println(car + " : " + setCarList.get(car));
+        }
+    }
+
+    public void printCarProgress(int num) {
+        for (int i = 0; i < num; i++) {
             System.out.println("실행 결과\n");
-            for(int j=0; j<carList.length; j++){
-                // 만약 4이상이라면...
-                isUpperFour = checkUpperFour();
-                if(isUpperFour){
-                    String tmp = setCarList.get(carList[j]);
-                    tmp += "-";
-                    setCarList.put(carList[j],tmp);
-                }
-                System.out.println(carList[j]+" : "+setCarList.get(carList[j]));
-            }
-
+            moveEachCar();
         }
+    }
+
+    public int findMaxDistance() {
+        return setCarList.values().stream().mapToInt(String::length).max().orElse(0);
+    }
+
+    private void appendWinnerName(StringBuffer stringBuffer, Map.Entry<String, String> entry) {
+        if (entry.getValue().length() == maxDistance) {
+            if (stringBuffer.length() > 0) {
+                stringBuffer.append(", ");
+            }
+            stringBuffer.append(entry.getKey());
+        }
+    }
+
+    private String getWinnerNames() {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (Map.Entry<String, String> entry : setCarList.entrySet()) {
+            appendWinnerName(stringBuffer, entry);
+        }
+        return stringBuffer.toString();
 
     }
 
-    public void printWinner(){
-        maxDistance = setCarList.values().stream().mapToInt(String::length).max().orElse(0);
-        StringBuffer stringBuffer = new StringBuffer();
-        for(Map.Entry<String,String> entry : setCarList.entrySet()){
-            if(entry.getValue().length() == maxDistance){
-                if(stringBuffer.length() > 0){
-                    stringBuffer.append(", ");
-                }
-                stringBuffer.append(entry.getKey().toString());
-            }
-        }
-        System.out.println("최종 우승자 : " + stringBuffer.toString());
+    public void printWinner() {
+        maxDistance = findMaxDistance();
+        String winnerNames = getWinnerNames();
+        System.out.println("최종 우승자 : " + winnerNames);
     }
 }
