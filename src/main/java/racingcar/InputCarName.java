924715830fe2dea666d1be4ebb8b544b697442ef
jmlee <p2achtrii@gmail.com>
2024-10-28 23:11:54 +0900

    edit: some typo

--- src/main/java/racingcar/InputCarName.java
+++ src/main/java/racingcar/InputCarName.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputCarName {
     public String[] getCarList(){
-            System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분");
+            System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
             String car = Console.readLine();
 
             DelimiterName delimiterCar = new DelimiterName();
