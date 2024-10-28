c6108b5de3056e6e2b2a25487cba3dcaca474a16
dragonwaterr <yongsu626@naver.com>
2024-10-28 19:51:16 +0900

    feat : 이동 시도할 횟수 유효성 검증 조건 추가

--- src/main/java/racingcar/error/ErrorMessage.java
+++ src/main/java/racingcar/error/ErrorMessage.java
@@ -6,6 +6,8 @@ public class ErrorMessage {
     private static final String NAME_WITH_BLANK_ERROR_MESSAGE = "입력에 공백이 있으면 안됩니다.";
     private static final String NAME_LENGTH_OVER_FIVE_ERROR_MESSAGE = "이름은 5자 이하만 가능합니다.";
     private static final String TRIAL_TIME_IS_NOT_NUMBER_MESSAGE = "입력할 시도 횟수는 숫자만 가능합니다.";
+    private static final String TRIAL_TIME_IS_NEGATIVE_NUMBER_MESSAGE = "입력할 시도 횟수는 0 이상의 숫자만 가능합니다.";
+
     public static void printNameWithBlankError() {
         System.out.println(NAME_WITH_BLANK_ERROR_MESSAGE);
     }
@@ -16,5 +18,9 @@ public class ErrorMessage {
 
     public static void printTrialTimeIsNotNumberError() {
         System.out.println(TRIAL_TIME_IS_NOT_NUMBER_MESSAGE);
+    }
+
+    public static void printTrialTimeIsNegaviveNumberError() {
+        System.out.println(TRIAL_TIME_IS_NEGATIVE_NUMBER_MESSAGE);
     }
 }
--- src/main/java/racingcar/validation/TrialTimesValidator.java
+++ src/main/java/racingcar/validation/TrialTimesValidator.java
@@ -8,8 +8,16 @@ public class TrialTimesValidator {
     public static void validate(String trialTimes) {
         try {
             Integer.parseInt(trialTimes);
+            checkNegativeNumber(trialTimes);
         } catch (NumberFormatException e) {
             ErrorMessage.printTrialTimeIsNotNumberError();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void checkNegativeNumber(String trialTimes) {
+        if(Integer.parseInt(trialTimes) < 0) {
+            ErrorMessage.printTrialTimeIsNegaviveNumberError();
             throw new IllegalArgumentException();
         }
     }
