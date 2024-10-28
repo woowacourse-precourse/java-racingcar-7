dea9ea1a2f0d58be857f83cfdd5e4fe21eaeb388
Hyoseop Song <crad_on25@naver.com>
2024-10-28 22:16:42 +0900

    feat: 자동차명 입력 이름 분리 기능 구현

---
+++ src/main/java/racingcar/view/CarNameParser.java
@@ -0,0 +1,19 @@
+package racingcar.view;
+
+import java.util.List;
+
+public class CarNameParser {
+	private static final String DELIMITER = ",";
+	private CarValidator carValidator;
+
+	public List<String> parseCarNameList(String carNames) {
+		return splitCarNameList(carNames).stream()
+				.map(carValidator::validateNameEmpty)
+				.map(carValidator::validateCarNameLimit)
+				.toList();
+	}
+
+	private List<String> splitCarNameList(String carNameList) {
+		return List.of(carNameList.split(DELIMITER));
+	}
+}
