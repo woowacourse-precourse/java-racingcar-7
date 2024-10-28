6418483b2d40044e486741b8a05bced44d9408ca
G-Lake <note125@naver.com>
2024-10-28 17:36:15 +0900

    Feat : console, randoms API 적용
    기본 입출력 양식, 무작위 값 요구사항 일부, 자동차 클래스 메서드 정의

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,59 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNamesInput = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int attemptCount = Integer.parseInt(Console.readLine());
+
+        RacingGame game = new RacingGame();
+        game.play();
     }
+
+    static class RacingGame {
+        private static final int forwardThreshold = 4;
+
+        public void play() {
+            System.out.println("\n실행 결과");
+        }
+
+        private void playRound() {
+
+        }
+
+        private boolean isMovable() {
+            return Randoms.pickNumberInRange(0, 9) >= forwardThreshold;
+        }
+    }
+
+    static class Car {
+        private final String name;
+        private int position = 0;
+
+        public Car(String name) {
+            this.name = name;
+        }
+
+        public void move() {
+            position++;
+        }
+
+        public int getPosition() {
+            return position;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return name + " : " + "-".repeat(position);
+        }
+    }
+
 }
