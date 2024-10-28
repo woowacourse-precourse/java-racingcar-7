e5e48cb51c97b20184341fb6e41663d351caf4e2
BusanGukbap <dong2857210@gmail.com>
2024-10-28 16:01:22 +0900

    feat($Util) : create class - InputView

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.console;
+
+public class InputView {
+
+    private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_NUMBER_OF_ATTEMPTS_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
+    public static String inputCarNaems() {
+
+    }
+
+    public static int inputNumberOfAttempts() {
+
+    }
+}
