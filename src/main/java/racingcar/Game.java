739e5c72e9f547e5bf50604545a9e5b56b43e213
MinseoKangQ <kms02171@naver.com>
2024-10-28 15:12:25 +0900

    feat: 반복문 돌면서 차수별 실행 로직 작성

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,5 +20,24 @@ public class Game {
         for(String carName : carNames) {
             cars.add(new Car(carName));
         }
+
+        // 2. 반복문 돌면서 실행 한번씩 하기
+        for(int i = 0; i < times; i++) {
+
+            for(int j = 0; j < cars.toArray().length; j++) {
+
+                Car car = cars.get(j);
+
+                // 2-1. 각 객체에 대해 랜덤값 구하기
+                int randomNumber = Randoms.pickNumberInRange(0, 9);
+
+                // 2-2. 값이 4 이상이면 count 필드 1 증가
+                if (randomNumber >= 4) {
+                    car.increaseCount();
+                }
+            }
+
+        }
+
     }
 }
