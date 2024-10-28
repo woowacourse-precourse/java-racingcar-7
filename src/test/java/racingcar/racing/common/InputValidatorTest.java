1a0ebec2a469225aa1abee830c127005d16ee437
ppparkta <pppppark2@gmail.com>
2024-10-28 19:03:22 +0900

    test(InputValidatorTest): 레이싱카이름 5자이하 테스트 추가
    추가사항
    - 레이싱카 이름이 5자 이하일 때 유효성 검사 성공
    - 레이싱카 이름이 5자 초과할 때 유효성 검사 실패

--- src/test/java/racingcar/racing/common/InputValidatorTest.java
+++ src/test/java/racingcar/racing/common/InputValidatorTest.java
@@ -1,47 +1,99 @@
 package racingcar.racing.common;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.Assertions;
 import java.util.Arrays;
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class InputValidatorTest {
 
     @Test
-    void 빈_문자열_검사() {
-        //given
-        //when
-        //then
+    void 빈_문자열이_아니면_통과() {
+        Assertions.assertSimpleTest(() -> {
+            //given
+            String one = ".";
+
+            //when
+
+            //then
+            assertThatCode(() -> {
+                InputValidator.validateInputNotEmpty(one);
+            }).doesNotThrowAnyException();
+        });
     }
 
     @Test
-    void 레이싱카_배열이_비었는지_유효성검사(){
-        //given
-        //when
-        //then
+    void 빈_문자열_예외처리() {
+        Assertions.assertSimpleTest(() -> {
+            //given
+            String empty = "";
+
+            //when
+
+            //then
+            assertThatThrownBy(() -> {
+                InputValidator.validateInputNotEmpty(empty);
+            }).isInstanceOf(IllegalArgumentException.class);
+        });
     }
 
     @Test
-    void 레이싱카_이름_길이_유효성검사() {
-        // given
-        List<String> carNames1 = Arrays.asList("HelloCal", "hiCar", "Hihic");
-        List<String> carNames2 = Arrays.asList("Hello", "hiCar", "Hihic");
-        // when
-        // then
-//        Assertions.assertThat(InputValidator.validateCarNameLength(carNames1));
-//        Assertions.assertThat(InputValidator.validateCarNameLength(carNames2));
+    void 레이싱카_배열이_비었는지_유효성검사() {
+        Assertions.assertSimpleTest(() -> {
+            //given
+            //when
+            //then
+        });
+    }
+
+    @Test
+    void 레이싱카_이름_5자이하_유효성검사_통과() {
+        Assertions.assertSimpleTest(() -> {
+            //given
+            List<String> carNames = Arrays.asList("Hello", "hiCar", "Hihic");
+
+            //when
+
+            //then
+            assertThatCode(() -> {
+                InputValidator.validateCarNameLength(carNames);
+            }).doesNotThrowAnyException();
+        });
+    }
+
+    @Test
+    void 레이싱카_이름_5자이하_유효성검사_실패() {
+        Assertions.assertSimpleTest(() -> {
+            //given
+            List<String> carNames = Arrays.asList("HelloCal", "hiCar", "Hihic");
+
+            //when
+
+            //then
+            assertThatThrownBy(() -> {
+                InputValidator.validateCarNameLength(carNames);
+            }).isInstanceOf(IllegalArgumentException.class);
+        });
     }
 
     @Test
     void 시도횟수_유효성검사() {
-        // given
-        int zero = 0;
-        int minus = -1;
-        int one = 1;
-        // when
-        // then
-//        Assertions.assertThat(InputValidator.validateAttemptNumber(zero);)();
-//        Assertions.assertThat(InputValidator.validateAttemptNumber(minus);).isFalse();
-//        Assertions.assertThat(InputValidator.validateAttemptNumber(one);).isTrue();
+        Assertions.assertSimpleTest(() -> {
+            //given
+            int zero = 0;
+            int minus = -1;
+            int one = 1;
+
+            //when
+            InputValidator.validateAttemptNumber(zero);
+            //then
+//            assertThat();
+//            assertThat(InputValidator.validateAttemptNumber(minus));
+//            assertThat(InputValidator.validateAttemptNumber(one));
+        });
     }
 }
