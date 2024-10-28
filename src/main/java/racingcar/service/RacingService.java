5faec745bb3432b92fd54dd8204c0f139ef68de3
HIDE_MAIN <ra___be@naver.com>
2024-10-28 22:15:22 +0900

    feat: 최고 점수로 우승자 얻기

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -21,4 +21,19 @@ public class RacingService {
     public int getRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
     }
+
+    public int getTopScore() {
+        return cars.stream()
+                .mapToInt(Car::getScore)
+                .max()
+                .orElse(Integer.MIN_VALUE);
+    }
+
+    public List<String> getWinner() {
+        int topScore = getTopScore();
+        return cars.stream()
+                .filter(car -> car.getScore() == topScore)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
 }
