31016a22b86990775776d1a9ee031feb43f9995b
lucku <lsw6731@naver.com>
2024-10-28 22:08:53 +0900

    refactor: 결과 출력 문자열 상수화

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -4,19 +4,23 @@ import java.util.List;
 import racingcar.domain.Car;
 
 public class ResultView {
+    private static final String CAR_POSITION_FORMAT = " : ";
+    private static final String POSITION_MARK = "-";
+    private static final String WINNER_MESSAGE_PREFIX = "최종 우승자 : ";
+    private static final String DELIMITER = ", ";
+
     private ResultView() {
     }
 
     public static void printRoundResult(List<Car> cars) {
         for (Car car : cars) {
-            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+            System.out.println(car.getName() + CAR_POSITION_FORMAT + POSITION_MARK.repeat(car.getPosition()));
         }
         System.out.println();
     }
 
     public static void printWinners(List<Car> winners) {
         List<String> winnerNames = winners.stream().map(Car::getName).toList();
-
-        System.out.println("최종 우승자 : " + String.join(", ", winnerNames));
+        System.out.println(WINNER_MESSAGE_PREFIX + String.join(DELIMITER, winnerNames));
     }
 }
