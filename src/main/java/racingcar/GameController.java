00b8f9f9f96055ed912320e5546988385e751ac7
Cheoroo <195822@jnu.ac.kr>
2024-10-28 15:19:13 +0900

    refactor: 예외 처리 로직을 개선한다
    - try catch블록을 제거하여 예외를 최상위로 전파하도록 변경

--- src/main/java/racingcar/GameController.java
+++ src/main/java/racingcar/GameController.java
@@ -15,23 +15,17 @@ public class GameController {
     }
 
     public void start() {
-        try {
-            List<String> carNames = inputView.getCarNames();
-            int moveCount = inputView.getMoveCount();
+        List<String> carNames = inputView.getCarNames();
+        int moveCount = inputView.getMoveCount();
 
-            RacingGame racingGame = new RacingGame(
-                    carNames, moveCount, moveStrategy);
+        RacingGame racingGame = new RacingGame(carNames, moveCount, moveStrategy);
 
-            outputView.printMessage("\n실행 결과");
-            while (racingGame.hasNextRound()) {
-                racingGame.playNextRound();
-                outputView.printRoundResult(racingGame.getCars());
-            }
-            List<Car> winners = racingGame.getWinners();
-            outputView.printWinners(winners);
-        } catch (IllegalArgumentException e) {
-            outputView.printMessage(e.getMessage());
-            throw e;
+        outputView.printMessage("\n실행 결과");
+        while (racingGame.hasNextRound()) {
+            racingGame.playNextRound();
+            outputView.printRoundResult(racingGame.getCars());
         }
+        List<Car> winners = racingGame.getWinners();
+        outputView.printWinners(winners);
     }
 }
