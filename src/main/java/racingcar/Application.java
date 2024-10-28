076a8f204c799c70526ac9dd7c3445f5d566b22b
ChoiDongJoon <adong0808@naver.com>
2024-10-28 20:56:56 +0900

    refactor(racingcar): RacingCarGame에서 동작 코드를 가지고 있고, main에서는 start() 호출만 하도록 수정
    RacingCarGame에서 동작 코드를 가지고 있고, main에서는 RacingCarGame 객체를 생성하고 start()
    호출만 하도록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -12,29 +12,7 @@ import racingcar.controller.OutputController;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        Validator validator = new Validator();
-        InputController inputController = new InputController();
-        OutputController outputController = new OutputController();
-        RacingCarService racingCarService = new RacingCarService();
-
-        InputContainer inputContainer = inputController.readConsole();
-        String inputCars = inputContainer.getCars();
-        int attempts = inputContainer.getAttempts();
-
-        validator.attemptValidate(attempts);
-
-        String[] cars = inputCars.split(",");
-
-        Map<String, String> carMoveMap = racingCarService.getCarMoveMap();
-        racingCarService.makeCarMap(cars, carMoveMap);
-
-
-        outputController.printResultSentence();
-        racingCarService.executeRaceRounds(cars, carMoveMap, attempts);
-
-        ArrayList<String> winners = racingCarService.findWinners(carMoveMap);
-
-        outputController.printWinner(winners);
+        RacingCarGame racingCarGame = new RacingCarGame();
+        racingCarGame.start();
     }
 }
