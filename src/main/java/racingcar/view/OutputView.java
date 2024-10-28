863f4876fad9ecade20ce19974f8c61d5a445cf6
kymin <park48664866@gmail.com>
2024-10-28 09:07:32 +0900

    style: 함수 이름 변경
    함수 이름 변경:
    - OutputView의 printWinner -> printWinnerNames로 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,11 +4,10 @@ import racingcar.util.IOMessage;
 
 public class OutputView {
     public void printRaceProcedure(String name, String bar) {
-        // String distanceBar = drewBar(distance);
         System.out.println(name + " : " + bar);
     }
 
-    public void printWinner(String winners) {
+    public void printWinnerNames(String winners) {
         System.out.println(IOMessage.OUTPUT_RESULT_MESSAGE.getMessage() + winners);
     }
 }
