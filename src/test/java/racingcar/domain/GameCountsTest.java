02d66137abc8028fe265658d2142454af4c17915
dnwldla <leeemjw@naver.com>
2024-10-28 17:46:52 +0900

    test: @Test에서 @ParameterizedTest로 변경

--- src/test/java/racingcar/domain/GameCountsTest.java
+++ src/test/java/racingcar/domain/GameCountsTest.java
@@ -1,6 +1,5 @@
 package racingcar.domain;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -17,20 +16,18 @@ class GameCountsTest {
                 hasMessageContaining("게임은 한 번 이상 실행되어야 합니다");
     }
 
-    @Test
-    void 양수정수_범위_초과_입력시_예외_발생(){
-        String gameCounts = String.valueOf(Long.MAX_VALUE);
-
+    @ParameterizedTest
+    @ValueSource(strings={"-1j","skjfk"})
+    void 숫자를_입력하지_않았을때_예외_발생(String gameCounts){
         assertThatThrownBy(()->new GameCounts(gameCounts)).
                 isInstanceOf(IllegalArgumentException.class).
                 hasMessageContaining("게임은 양수인 정수 범위에서 실행되어야 합니다");
     }
 
-    @Test
-    void 정상_작동_확인(){
-        GameCounts expectedCounts=new GameCounts("3");
-
-        assertThat(new GameCounts("3")).usingRecursiveComparison().isEqualTo(expectedCounts);
+    @ParameterizedTest
+    @ValueSource(strings={"3","+3","2"})
+    void 정상_작동_확인(String gameCounts){
+        assertThat(new GameCounts(gameCounts)).usingRecursiveComparison().isEqualTo(new GameCounts(gameCounts));
     }
 }
 
