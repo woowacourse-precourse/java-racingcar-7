ad473d2cdcee502b8f70a9a973816eb8ce83467e
settle54 <settle54@kangwon.ac.kr>
2024-10-28 14:46:58 +0900

    feat: 인풋뷰와 아웃뷰 메소드가 있는 뷰 클래스 생성

--- src/main/java/racingcar/View.java
+++ src/main/java/racingcar/View.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+
 public class View {
 
     public View() {
@@ -9,11 +11,17 @@ public class View {
     }
 
     public void showInputView() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] names = Console.readLine().trim().split(",");
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String strCount = Console.readLine().trim();
-        int count = Integer.parseInt(strCount);
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String[] names = Console.readLine().trim().split(",");
+            checkValidNames(names);
+            System.out.println("시도할 횟수는 몇 회인가요?");
+            String strCount = Console.readLine().trim();
+            checkValidCount(strCount);
+            int count = Integer.parseInt(strCount);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 
     public void showOutputView(String[] names) {
@@ -22,5 +30,21 @@ public class View {
             winners.append(name).append(", ");
         }
         System.out.print(winners);
+    }
+
+    private void checkValidNames(String[] names) throws Exception {
+        for (String name : names) {
+            if (name.length() > 5 || name.isEmpty()) {
+                throw new Exception("잘못된 이름 입력입니다.");
+            }
+        }
+    }
+
+    private void checkValidCount(String strCount) throws Exception {
+        for (char c : strCount.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new Exception("잘못된 횟수 입력입니다.");
+            }
+        }
     }
 }
