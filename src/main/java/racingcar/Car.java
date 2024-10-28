00e06956c89803e000d84c9eceb1284d9ba976de
leehenson <hslee7231@gmail.com>
2024-10-28 13:09:29 +0900

    feat: 랜덤 값으로 Car 객체 전진시키는 move 메서드 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private String forwardProgress;
@@ -8,5 +10,12 @@ public class Car {
     public Car(String name) {
         this.name = name;
         this.forwardProgress = "";
+    }
+
+    public void move() {
+        pickNumber = Randoms.pickNumberInRange(0, 9);
+        if (pickNumber >= 4) {
+            forwardProgress += "-";
+        }
     }
 }
