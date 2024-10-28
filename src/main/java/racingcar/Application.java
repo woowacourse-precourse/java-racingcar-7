08ad2c9f45b665ad15dd1ee356b812dae1230d31
KKdohyeong <dohyeong254@naver.com>
2024-10-28 18:53:58 +0900

    feat(Application): 프로그램 시작 Application 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.service.ParseCarNames;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        ParseCarNames parseCarNames = new ParseCarNames();
+        RacingController racingController = new RacingController(inputView, outputView, parseCarNames);
+
+        racingController.run();
     }
 }
