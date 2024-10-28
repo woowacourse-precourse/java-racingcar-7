4a5d6fe17ab7a68f27b17ff248ac93e247b6c141
Chan531 <chan657312@gmail.com>
2024-10-28 16:35:49 +0900

    feat(ErrorMessage): 자동차 이름 관련 에러 메세지 추가

--- src/main/java/racingcar/model/message/ErrorMessage.java
+++ src/main/java/racingcar/model/message/ErrorMessage.java
@@ -2,6 +2,8 @@ package racingcar.model.message;
 
 public enum ErrorMessage {
 
+	INVALID_CAR_NAME_LENGTH("자동차 이름의 길이는 1 이상 5 이하여야 합니다.");
+
 	private final String message;
 
 	ErrorMessage(String message) {
