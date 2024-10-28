cca18a0a45903fad8c0f075fb5a3814ceaa62b9d
chino <tnehd83@naver.com>
2024-10-28 14:26:56 +0900

    feat(RacingPlayingCycle): 경주 시작 구현

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -2,6 +2,7 @@ package racingcar.controller;
 
 import java.util.List;
 import racingcar.gameuserdata.RacingUserData;
+import racingcar.model.RacingPlayingCycle;
 import racingcar.model.RacingcarDataVerification;
 import racingcar.splitter.RacingcarSplitter;
 import racingcar.view.InputView;
@@ -9,11 +10,13 @@ import racingcar.view.InputView;
 public class RacingcarController {
     private final InputView inputView;
     private final RacingcarDataVerification racingcarDataVerification;
+    private final RacingPlayingCycle racingPlayingCycle;
     private final RacingcarSplitter splitter;
 
     public RacingcarController() {
         inputView = new InputView();
         racingcarDataVerification = new RacingcarDataVerification();
+        racingPlayingCycle = new RacingPlayingCycle();
         splitter = new RacingcarSplitter();
     }
 
@@ -24,5 +27,7 @@ public class RacingcarController {
         String[] RacingCarSplitName = RacingCarNames.split(",");
         racingcarDataVerification.validateRacingCarNames(RacingCarSplitName);
         List<RacingUserData> racingUserDataList = splitter.racingUserDataSplitter(RacingCarSplitName);
+
+        racingUserDataList = racingPlayingCycle.racingPlayingCycle(RacingCount, racingUserDataList);
     }
 }
