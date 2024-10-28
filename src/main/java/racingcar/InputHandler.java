5b42965453a67edec0cca59c2030b0fbeae38717
MinseoKangQ <kms02171@naver.com>
2024-10-28 14:22:23 +0900

    feat: 리스트로 변경

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -1,14 +1,18 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputHandler {
 
     public static String[] getCarNames() {
         String input = Console.readLine();
         String[] carNames = input.split(",");
+        List<String> carNamesList = new ArrayList<>(Arrays.asList(carNames));
 
-        for(String carName: carNames) {
+        for(String carName: carNamesList) {
             checkNameLength(carName);
         }
 
