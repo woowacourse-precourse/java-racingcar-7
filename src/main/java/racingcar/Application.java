4d934f0dcd5925d83482ae6d0cabeae42303fcea
mjk <mjk3220@ajou.ac.kr>
2024-10-28 11:31:38 +0900

    refactor: Car class
    클래스 위치를 이동

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,20 +3,18 @@ package racingcar;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
-public class Application {
+class Car {
+    String name;
+    int move_cnt;
 
-    public static class Car {
-        String name;
-        int move_cnt;
-
-        Car(String name, int move_cnt){
-            this.name = name;
-            this.move_cnt = move_cnt;
-        }
+    Car(String name, int move_cnt) {
+        this.name = name;
+        this.move_cnt = move_cnt;
     }
+}
 
-
-    
+public class Application {
+    Car car_list[];
 
     public static void inputCar() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
