446e77020ce3c8a71777e44a8c67c342ff798a09
kimsongie12 <song3078@naver.com>
2024-10-28 16:19:21 +0900

    feat: 우승자 이름 찾기 기능 수정

--- src/main/java/racingcar/RaceView.java
+++ src/main/java/racingcar/RaceView.java
@@ -1,5 +1,35 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
 
-public class RaceView
-{
+import java.util.List;
+
+public class RaceView {
+    public List<String> getCarNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        return splitCarNames(input);
+    }
+
+    private List<String> splitCarNames(String input){
+        String [] names = input.split(",");
+        return List.of(names);
+    }
+    public int getTotalRounds(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
+    public void displayRoundResults(List<Car> cars){
+        System.out.println("실행 결과");
+        for (Car car: cars){
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+    public void displayWinners(List<Car> winners){
+        System.out.println("최종 우승자 : " + getWinnersNames(winners));
+    }
+
+    public String getWinnersNames(List<Car> winners){
+        return String.join(",",winners.stream().map(Car::getName).toList());
+    }
 }
