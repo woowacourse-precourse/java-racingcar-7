da46b25fee73a03156663932d7e7222a81487baf
SSU <hshwan0406@gmail.com>
2024-10-28 20:34:23 +0900

    refactor: 레이싱 카 생성 메서드 수정
    테스트를 위해 private -> public으로 변경
    입력값으로 받은 racingCarNames의 null과 빈 값 처리
    자동차 이름이 5자 초과시 예외 발생

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -12,12 +12,25 @@ public class Application {
 
     }
 
-    private static List<Car> createCar(String racingCarsNames) {
+    public static List<Car> createCar(String racingCarsNames) {
+        if(racingCarsNames == null || racingCarsNames.isBlank()) {
+            throw new IllegalArgumentException();
+        }
 
         List<Car> carArray =  new ArrayList<>();
-        String[] carsNames = racingCarsNames.split(":");
+        String[] carsNames = racingCarsNames.split(",");
 
         for (String carName : carsNames) {
+            if(carName.isBlank()) {
+                continue;
+            }
+
+            carName = carName.trim();
+
+            if(carName.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+
             carArray.add(new Car(carName));
         }
 
