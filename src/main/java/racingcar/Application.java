8397e5e512a07d462ad312f2a4b3ab9573a8d91a
soobzzi <ehfdkrkwk101@naver.com>
2024-10-28 16:54:34 +0900

    feat : 메소드 실행 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
 
@@ -9,15 +11,13 @@ public class Application {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputCarNames = Console.readLine();
+        List<String> nameList =  methods.nameCheck(inputCarNames);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String inputTryCount = Console.readLine();
+        methods.carRacing(inputTryCount, nameList);
 
-
-
-
+        methods.printWinners();
 
     }
-
-
 }
