5fb3dbd9baea8a3181edc05dee100aa228cab62c
jae woo Jung <awfjol2008@gmail.com>
2024-10-28 21:02:46 +0900

    feat: 자동차 이름의 길이를 검사하는 테스트 구현

--- src/test/java/racingcar/domain/NameTest.java
+++ src/test/java/racingcar/domain/NameTest.java
@@ -19,4 +19,16 @@ class NameTest {
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessageContaining(CAR_NAME_BLANK.getMessage());
 	}
+
+	@Test
+	@DisplayName("자동차 이름이 최대 길이를 초과할 경우, 예외를 반환한다.")
+	void Throw_Exception_When_Car_Name_Length_Exceeded() {
+		// given
+		String input = "holyPigeon";
+
+		// when, then
+		assertThatThrownBy(() -> new Name(input))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(CAR_NAME_LENGTH_EXCEEDED.getMessage());
+	}
 }
