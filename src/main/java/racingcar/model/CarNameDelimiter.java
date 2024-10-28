7834a030ee986a9d4856abfc30ed52d2cba3eafe
jihyun <jihyun3066@naver.com>
2024-10-28 19:20:10 +0900

    [ feat ]  : 자동차 이름에 공백 입력시 예외

--- src/main/java/racingcar/model/CarNameDelimiter.java
+++ src/main/java/racingcar/model/CarNameDelimiter.java
@@ -1,6 +1,7 @@
 package racingcar.model;
 
 import racingcar.exception.ExceptionCode;
+import static racingcar.model.Number.MAX_NAME_LENGTH;
 
 
 public class CarNameDelimiter {
@@ -10,14 +11,39 @@ public class CarNameDelimiter {
 
 
     public String[] NameDelimiter(String name) {
+        validateDelimiter(name);
         carNames = name.trim().split("\\s*" + DELIMITER + "\\s*");
+        checkValidAll(carNames);
 
         return carNames;
     }
 
+    public void checkValidAll(String[] validCarName) {
+        validStringLength(validCarName);
+        for (String name : validCarName) {
+            validCarNameNull(name);
+        }
+    }
+
+
     public void validateDelimiter(String name) {
         if (name.matches(".*[^\\w\\s,가-힣].*")) {
             throw new IllegalArgumentException(String.valueOf(ExceptionCode.INVALID_DELIMITER));
+        }
+    }
+
+    //, , , 이런식으로 들어와도 예외 처리(공백이 들어오면 처리해준다)
+    public void validStringLength(String[] validCarName) {
+        if (validCarName.length == 0) {
+            throw new IllegalArgumentException(ExceptionCode.NAME_LIST_NULL.getMessage());
+        }
+    }
+
+
+    //, , pobi 에서 자동차 이름 배열에 공백문자열이 있으면 예외처리 해준다. (문자열 분리 후 size가 0이면 예외처리)
+    public void validCarNameNull(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException(ExceptionCode.NAME_NULL.getMessage());
         }
     }
 
