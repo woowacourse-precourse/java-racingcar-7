58e6cda2e06d051e7910580af9941f701bc89eab
Jun2-Lee <ljh19980823@gmail.com>
2024-10-28 16:00:19 +0900

    feat : 중복된 이름 입력시 에러 발생

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -1,13 +1,25 @@
 package racingcar.util;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Validator {
 
-    public void checkNameFormat(String input) {
+    public void checkInputFormat(String input) {
         Pattern pattern = Pattern.compile("^[a-zA-Z0-9,\\s가-힣ㄱ-ㅎㅏ-ㅣ]*$");
         if(!input.matches(pattern.pattern())) {
             throw new IllegalArgumentException("입력에 올 수 없는 문자가 포함되었습니다");
+        }
+    }
+
+    public void checkDuplicateName(String input) {
+        List<String> nameList = Arrays.stream(input.split(",")).map(String::strip).toList();
+        Set<String> nameSet = new HashSet<>(nameList);
+        if(nameList.size() != nameSet.size()) {
+            throw new IllegalArgumentException("중복된 이름이 포함되었습니다.");
         }
     }
 }
