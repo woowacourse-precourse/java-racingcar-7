596a26bd097b2e503f67a00f42e1a642b5095f1f
woohyuk <7dngur7@naver.com>
2024-10-28 17:40:43 +0900

    chore: 예외 메시지 상수를 관리하기 위한 ExceptionMessages 클래스 추가
    - 예외 메시지를 상수로 관리하여 코드의 일관성을 유지함

---
+++ src/main/java/racingcar/ExceptionMessages.java
@@ -0,0 +1,7 @@
+package racingcar;
+
+public class ExceptionMessages {
+
+    public static final String INVALID_CAR_NAME = "자동차 이름은 1자 이상 5자 이하만 가능합니다.";
+    public static final String INVALID_ATTEMPTS = "시도 횟수는 1회 이상이어야 합니다.";
+}
