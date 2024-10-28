da02384505b3535a326a2eae57cdd3b0aff67a5e
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-10-28 19:52:33 +0900

    feat: 자동차 이름, 이동할 횟수 입력 받기

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,25 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static void printCarNamesInputGuide() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void printMoveCountInputGuide() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+    public static String inputCarNames() {
+        return Console.readLine();
+    }
+
+    public static int inputMoveCount() {
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 횟수는 자연수만 가능합니다.");
+        }
+    }
+}
