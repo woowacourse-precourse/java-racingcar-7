ae02730f2566601db9c4d7f38f4ffa66ad5285f7
eb9862 <luclead622@gmail.com>
2024-10-28 19:25:03 +0900

    refactor : InputManager 클래스의 사용자 입력 메시지를 상수로 변경

--- src/main/java/racingcar/InputManager.java
+++ src/main/java/racingcar/InputManager.java
@@ -4,13 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputManager {
 
+    private static final String INPUT_MESSAGE_FOR_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_MESSAGE_FOR_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+
     static String readCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(INPUT_MESSAGE_FOR_CAR_NAME);
         return Console.readLine();
     }
 
     static String readTryCount() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(INPUT_MESSAGE_FOR_TRY_COUNT);
         return Console.readLine();
     }
 }
