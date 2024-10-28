e6f28b1249ce6046f204ca83aebe898e9d874d96
Dongjun Lee <dongjle2@student.42heilbronn.de>
2024-10-28 15:10:20 +0100

    Feat: implement main function

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String inputNames = Console.readLine();
+        String rounds = Console.readLine();
+
+        InputValidator inputValidator = new InputValidator(inputNames, rounds);
+        try {
+            inputValidator.validateName();
+            inputValidator.validateRounds();
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException();
+        }
+
+        GameController gameController = new GameController(inputNames, rounds);
+        gameController.playGame();
     }
 }
