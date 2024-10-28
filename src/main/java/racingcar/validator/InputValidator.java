e91e4f680cbdf1baaf86f2dbb92fdd099b49ff40
junhyunpark <junhyunpark305@gmail.com>
2024-10-28 22:51:13 +0900

    feat: update InputValidator
    - 자동차 이름의 갯수가 1개 뿐이면 예외처리

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -5,6 +5,7 @@ import java.util.stream.Stream;
 
 // 완료
 public class InputValidator {
+
   private static final String DELIMITER = ",";
 
   public List<String> checkCarNames(String carNames) {
@@ -14,7 +15,6 @@ public class InputValidator {
     if (carNames.endsWith(",") || carNames.startsWith(",")) {
       throw new IllegalArgumentException("구분자로 끝나는 문자열은 입력할 수 없습니다");
     }
-
     try {
       carNameList = Stream.of(carNames.split(DELIMITER))
               .map(String::trim)
@@ -22,6 +22,11 @@ public class InputValidator {
     } catch (IllegalArgumentException e) {
       throw new IllegalArgumentException("유효하지 않은 구분자입니다.", e);
     }
+
+    if (carNameList.size() < 2) {
+      throw new IllegalArgumentException("자동차 이름은 2개 이상이어야 합니다.");
+    }
+
     for (String carName : carNameList) {
       if (carName.trim().isBlank()) {
         throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
@@ -32,7 +37,6 @@ public class InputValidator {
         throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
       }
     }
-    // 자동자 이름 중복 확인(메서드로 분리 예정)
     for (int i = 0; i < carNameList.size(); i++) {
       for (int j = i + 1; j < carNameList.size(); j++) {
         if (carNameList.get(i).equals(carNameList.get(j))) {
@@ -40,7 +44,6 @@ public class InputValidator {
         }
       }
     }
-
 
     return carNameList;
   }
@@ -56,7 +59,7 @@ public class InputValidator {
     }
 
     if (parseAtmp < 1 || parseAtmp > 99) {
-      throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+      throw new IllegalArgumentException("시도 횟수는 0과 100 사이여야 합니다.");
     }
     return parseAtmp;
   }
