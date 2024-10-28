adc4b8f5711521293b2a4a1fd11854de58bc7ac1
sytTest <syt1128@naver.com>
2024-10-28 15:51:46 +0900

    :construction: (InputController) fix and add
    컨트롤러의 역할에 보다 충실하도록 inputService에만 의존하도록 수정

---
+++ src/main/java/racingcar/controller/InputController.java
@@ -0,0 +1,34 @@
+package racingcar.controller;
+
+import racingcar.domain.Input;
+import racingcar.service.input.InputService;
+import racingcar.service.input.Validation;
+import racingcar.view.UserInput;
+import racingcar.view.constant.Request;
+
+public class InputController {
+
+    private static InputController instance;
+    private final InputService inputService = new InputService();
+
+    private InputController() {}
+
+    public void getInputs()  {
+        Input carNamesInput = rawInput(Request.CAR_NAMES.toString());
+        inputService.saveInput(carNamesInput, Validation::carNames);
+        Input LapCountInput = rawInput(Request.ATTEMPT_NUMBER.toString());
+        inputService.saveInput(LapCountInput, Validation::lapCount);
+    }
+
+    private Input rawInput(String requestMessage) {
+         return inputService.receive(requestMessage
+                 , UserInput.request(requestMessage));
+    }
+
+    public static InputController getInstance() {
+        if (instance == null) {
+            return new InputController();
+        }
+        return instance;
+    }
+}
