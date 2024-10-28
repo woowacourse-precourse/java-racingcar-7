b4530c19145fdf7ed6ae140770852c678e732bd3
kimchanho97 <nh0903@pusan.ac.kr>
2024-10-28 21:24:16 +0900

    feat(RacingCarGameInputView): 라운드 숫자 입력

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,11 +1,13 @@
 package racingcar.controller;
 
 import racingcar.model.Cars;
+import racingcar.util.NumberParser;
 import racingcar.view.RacingCarGameInputView;
 
 public class RacingGameController {
 
     public void run() {
         Cars cars = new Cars(RacingCarGameInputView.inputCarNames());
+        int totalRounds = NumberParser.parsePositiveInteger(RacingCarGameInputView.inputTotalRound());
     }
 }
