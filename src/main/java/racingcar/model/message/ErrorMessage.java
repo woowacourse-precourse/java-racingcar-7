2b3772673920a5cba83fd2b7cd8682ff4ea5d302
Chan531 <chan657312@gmail.com>
2024-10-28 17:06:36 +0900

    feat(ErrorMessage): 시도할 횟수 관련 에러 메세지 추가

--- src/main/java/racingcar/model/message/ErrorMessage.java
+++ src/main/java/racingcar/model/message/ErrorMessage.java
@@ -2,7 +2,8 @@ package racingcar.model.message;
 
 public enum ErrorMessage {
 
-	INVALID_CAR_NAME_LENGTH("자동차 이름의 길이는 1 이상 5 이하여야 합니다.");
+	INVALID_CAR_NAME_LENGTH("자동차 이름의 길이는 1 이상 5 이하여야 합니다."),
+	INVALID_TRIAL_COUNT("시도할 횟수 입력이 유효하지 않습니다.");
 
 	private final String message;
 
