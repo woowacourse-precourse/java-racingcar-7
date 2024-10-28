b653ca1aca5dbf5040b9fbd3bfffa90ddd52f550
yushinc <cys74351@gmail.com>
2024-10-28 22:02:26 +0900

    feat: 자동차 이름 중복 검증 로직 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,11 @@
 package racingcar;
 
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Car {
     private static final int CAR_NAME_MAX_LENGTH = 5;
     private String name;
@@ -28,5 +33,23 @@ public class Car {
 
     public int getDistance() {
         return distance;
+    }
+
+    public static List<Car> createCars(String carsName) {
+
+        List<String> carNames = List.of(carsName.split(","));
+        validateDuplicate(carNames);
+
+        return carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    private static void validateDuplicate(List<String> carNames) {
+        Set<String> uniqueCarNames = new HashSet<>(carNames);
+
+        if (uniqueCarNames.size() != carNames.size()) {
+            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+        }
     }
 }
