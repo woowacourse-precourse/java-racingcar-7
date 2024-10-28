83555b82180909988425f844b05d9283d54ae8f8
youngkwanglim <richard08@naver.com>
2024-10-28 16:12:39 +0900

    refactor: 게임 진행 부분 새로운 클래스로 이동

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,32 +9,9 @@ public class Application {
         List<Car> cars = InputHandler.inputCarNames();
         int tryCount = InputHandler.inputTryCount();
 
-        gameStart(cars, tryCount);
+        GameHandler.gameStart(cars, tryCount);
 
         printWinner(cars);
-    }
-
-    private static void gameStart(List<Car> cars, int tryCount) {
-        System.out.println("실행 결과");
-
-        for (int i = 0; i < tryCount; i++) {
-            for (Car car : cars) {
-                car.move();
-            }
-
-            printProgress(cars);
-        }
-    }
-
-    private static void printProgress(List<Car> cars) {
-        for (Car car : cars) {
-            System.out.print(car.getName() + " : ");
-            for (int i = 0; i < car.getLocation(); i++) {
-                System.out.print("-");
-            }
-            System.out.println();
-        }
-        System.out.println();
     }
 
     private static void printWinner(List<Car> cars) {
---
+++ src/main/java/racingcar/GameHandler.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+import java.util.List;
+
+public class GameHandler {
+    public static void gameStart(List<Car> cars, int tryCount) {
+        System.out.println("실행 결과");
+
+        for (int i = 0; i < tryCount; i++) {
+            for (Car car : cars) {
+                car.move();
+            }
+
+            printProgress(cars);
+        }
+    }
+
+    private static void printProgress(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.print(car.getName() + " : ");
+            for (int i = 0; i < car.getLocation(); i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
