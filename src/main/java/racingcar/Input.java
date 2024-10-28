18ab008b049a8d4859610a77aa9c5fc196bf57c7
dev-Rhea <dev.rhea4444@gmail.com>
2024-10-28 21:18:04 +0900

    :bug: fix : 공백입력 예외 처리

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,11 +1,20 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class Input {
 
     public static String inputCarName() {
-        return Console.readLine();
+        try {
+            String input = Console.readLine();
+            if (input == null || input.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+            }
+            return input;
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("자동차 이름 입력이 필요합니다.");
+        }
     }
 
     public static int inputTryCount() {
