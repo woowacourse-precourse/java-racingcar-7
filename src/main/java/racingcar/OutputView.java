238ca9f778755694d09342db07f0084d057fe7f3
ssuukko <tlstpstpdl11@gmail.com>
2024-10-28 21:53:38 +0900

    Refactor: OutputView 클래스 수정 및 코드 간소화
    - `printResultMessage` 메서드를 제거하고 불필요한 상수(`EXECUTION_RESULT`)를 삭제하였습니다.
    - `printRacingStatus` 메서드에서 `printLineBreak` 메서드를 호출하여 코드 간소화를 진행하였습니다.

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -4,12 +4,7 @@ import java.util.List;
 
 public class OutputView {
 
-    private static final String EXECUTION_RESULT = "실행 결과";
-    private static final String CAR_SYMBOL = "-"; // 자동차 이동을 나타내는 기호
-
-    public static void printResultMessage() {
-        System.out.println(EXECUTION_RESULT);
-    }
+    private static final String CAR_SYMBOL = "-";
 
     public static void printRacingStatus(List<Car> carList) {
         for (Car car : carList) {
@@ -17,7 +12,7 @@ public class OutputView {
             for (int i = 0; i < car.getPosition(); i++) {
                 System.out.print(CAR_SYMBOL);
             }
-            System.out.println();
+            printLineBreak();
         }
     }
 
