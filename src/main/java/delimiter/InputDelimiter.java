5d19f8f126566860c649d9a4ce1390a9366bf12b
soonhong99 <nanayah99@naver.com>
2024-10-28 14:38:13 +0900

    fix(Input Error): input으로 입력된 내용에서 예외 발생 사항을 추가하라
    - 자동차 이름이 중복될 시 예외 발생
    - comma로 구분된 자동차의 이름이 빈값일 시 예외 발생
    - 1000이 초과된 시도 횟수는 에외 발생
    - 시도 횟수가 1000이 초과될 시 프로그램 실행 시간 및 사용자 경험에 부정적이라고 판단
    - 자동차의 이름은 빈값이 될 수 없다고 판단
    - 자동차 이름이 중복될 시 Map을 사용하는 입장에서 순서가 바뀔 수 있어
     - 중복된 이름을 구별하기 힘들기 때문
    
    Signed-off-by: soonhong99 <nanayah99@naver.com>

--- src/main/java/delimiter/InputDelimiter.java
+++ src/main/java/delimiter/InputDelimiter.java
@@ -1,10 +1,12 @@
 package delimiter;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class InputDelimiter {
-    public List<String> delimiterWithComma(String inputCarName) {
+    public List<String> activateDelimiterWithComma(String inputCarName) {
         List<String> delimiterWithCommaList = new ArrayList<>();
 
         for (String carName : inputCarName.split(",")) {
@@ -12,6 +14,8 @@ public class InputDelimiter {
         }
 
         isCorrectCarName(delimiterWithCommaList);
+        isDuplicateCarName(delimiterWithCommaList);
+
         return delimiterWithCommaList;
     }
 
@@ -20,6 +24,23 @@ public class InputDelimiter {
             if (i.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하로 적어주세요.");
             }
+            if (i.length() == 0) {
+                throw new IllegalArgumentException("comma(\",\")는 자동차 이름 구분자입니다. 자동차 이름 사이에만 하나의 comma가 올 수 있도록 해주세요.");
+            }
         });
+    }
+
+    private void isDuplicateCarName(List<String> delimiterWithCommaList) {
+        int sizeWithDeleteDuplicateInList = deleteDuplicateStringInList(delimiterWithCommaList);
+
+        if (sizeWithDeleteDuplicateInList != delimiterWithCommaList.size()) {
+            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다. 중복되지 않는 자동차 이름을 입력해주세요.");
+        }
+    }
+
+    private int deleteDuplicateStringInList(List<String> delimiterWithCommaList) {
+        Set<String> delimiterWithCommaListToSet = new HashSet<>(delimiterWithCommaList);
+
+        return delimiterWithCommaListToSet.size();
     }
 }
--- src/main/java/input/MoveInput.java
+++ src/main/java/input/MoveInput.java
@@ -21,14 +21,18 @@ public class MoveInput {
 
     private int isNumberOfMove(String isNumberOfMoveString) {
         int numberOfMove = -1;
-
         try {
             numberOfMove = Integer.parseInt(isNumberOfMoveString);
         } catch (Exception e) {
-            throw new IllegalArgumentException("입력하신 문자열은 시도 횟수 형식이 아닙니다. 숫자로 입력해주세요.");
+            throw new IllegalArgumentException("입력하신 문자열은 시도 횟수 형식이 아닙니다. 1000이하의 숫자를 입력해주세요.");
         }
+
         if (numberOfMove <= 0) {
             throw new IllegalArgumentException("입력하신 숫자로는 자동차를 움직일 수 없습니다. 1 이상의 숫자를 입력해주세요.");
+        }
+
+        if (numberOfMove > 1000) {
+            throw new IllegalArgumentException("입력하신 숫자는 레이싱 시도횟수에 적합하지 않습니다. 1000이하의 숫자를 입력해주세요.");
         }
 
         return numberOfMove;
