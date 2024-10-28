de8e04f16518db19cb982919121eca7d2cd16fbb
SinnoLn <sintmtn69@gmail.com>
2024-10-28 13:47:04 +0900

    test(controller): GameController 통합 테스트 추가
    - GameController가 경주를 올바르게 초기화하고 진행하는지 테스트
    - 통합 테스트 환경에서 입력과 출력을 검증

---
+++ src/test/java/racingcar/controller/GameControllerIntegrationTest.java
@@ -0,0 +1,24 @@
+package racingcar.controller;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.service.RaceService;
+
+@DisplayName("GameController 통합 테스트")
+public class GameControllerIntegrationTest extends NsTest {
+    @Test
+    @DisplayName("GameController 는 경주를 성공적으로 초기화하고 진행한다")
+    void gameControllerShouldInitializeRaceAndMoveCars() {
+        run("Car1,Car2", "5");
+
+        assert output().contains("경주 초기화 완료");
+    }
+
+    @Override
+    protected void runMain() {
+        RaceService raceService = new RaceService();
+        GameController controller = new GameController(raceService);
+        controller.start();
+    }
+}
