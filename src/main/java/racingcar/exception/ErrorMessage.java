eb2e0ca580fe05419b46243be6246a619aeb45e0
JS <zks145@naver.com>
2024-10-28 11:54:46 +0900

    feat (exception) : , 미입력 예외처리 메시지 추가

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -8,7 +8,8 @@ public enum ErrorMessage {
     SAME_NAME_FOUND("동일한 이름의 자동차가 존재합니다."),
     OVER_GAME_MATCHES("허용된 게임 횟수를 넘겼습니다."),
     NEGATIVE_GAME_MATCHES("경기 횟수는 음수가 될 수 없습니다."),
-    GAME_MATCHES_NOT_NUMBER("경기 횟수는 숫자가 입력되어야 합니다.");
+    GAME_MATCHES_NOT_NUMBER("경기 횟수는 숫자가 입력되어야 합니다."),
+    NO_COMMA(", 를 이용해 자동차 이름을 구분해주세요.");
 
     private final String message;
 
