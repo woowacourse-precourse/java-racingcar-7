f6d5a58dedfee1186ba16cc708fc245d403df696
KNU-K <knukang334@gmail.com>
2024-10-28 17:03:42 +0900

    refactor(util): Proxy가 상속으로 바뀌면서 타입 변경
    interface로써 주입이 불가능해졌기 때문에, 조금 더 목적성이 뚜렷한 CarList만을 위한 Collector로써 구성

--- src/main/java/racingcar/common/util/CarListCollector.java
+++ src/main/java/racingcar/common/util/CarListCollector.java
@@ -1,32 +1,47 @@
 package racingcar.common.util;
 
 import java.util.stream.Collector;
-import racingcar.model.CarList;
 import racingcar.model.Drivable;
+import racingcar.model.RacingCar;
+import racingcar.model.RacingCarList;
 
 public class CarListCollector {
 
     /**
-     * Drivable 인터페이스의 구현 객체를 CarList로 수집하는 Collector를 반환합니다.
+     * Drivable 인터페이스의 구현 객체를 RacingCarList로 수집하는 Collector를 반환합니다.
      *
-     * @return Drivable 인터페이스의 구현체를 CarList로 수집하기 위한 Collector
+     * @return Drivable 객체를 RacingCarList로 수집하기 위한 Collector
      */
-    public static Collector<Drivable, ?, CarList> toCarList() {
+    public static Collector<Drivable, ?, RacingCarList> toCarList() {
         return Collector.of(
-                CarList::new, // 수집기의 결과를 위한 새로운 CarList 생성
-                CarList::addCar, // 요소를 CarList에 추가하는 accumulator
-                CarListCollector::mergeCarLists // 두 개의 CarList를 병합하는 combiner
+                RacingCarList::new,
+                CarListCollector::addCarIfRacingCar,
+                CarListCollector::combineCarLists
         );
     }
 
     /**
-     * 두 개의 CarList를 병합합니다.
+     * RacingCar로 캐스팅하여 RacingCarList에 추가합니다.
      *
-     * @param cl1 첫 번째 CarList
-     * @param cl2 두 번째 CarList
-     * @return 병합된 CarList
+     * @param carList  RacingCarList
+     * @param drivable Drivable 객체
      */
-    private static CarList mergeCarLists(CarList cl1, CarList cl2) {
+    private static void addCarIfRacingCar(RacingCarList carList, Drivable drivable) {
+        if (drivable instanceof RacingCar racingCar) {
+            carList.addCar(racingCar);
+        } else {
+            throw new IllegalArgumentException("Expected a RacingCar instance");
+        }
+    }
+
+    /**
+     * 두 개의 RacingCarList를 병합합니다.
+     *
+     * @param cl1 첫 번째 RacingCarList
+     * @param cl2 두 번째 RacingCarList
+     * @return 병합된 RacingCarList
+     */
+    private static RacingCarList combineCarLists(RacingCarList cl1, RacingCarList cl2) {
         cl1.getCarsCopy().addAll(cl2.getCarsCopy());
         return cl1;
     }
