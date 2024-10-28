b496ff9c8f1bb8bf2edf99a9fb19621097b05e73
박철현 <park82602@gmail.com>
2024-10-28 17:02:03 +0900

    Refactor : InputHandler.java 클래스에서 이름을 받아 처리하는 로직을 수정함
    - 사용자로부터 입력을받자마자 검증을 마친 후 데이터를 반환하는 형식이 다른 클래스에서 검증 된 데이터를 쓴다는 느낌을 주고 InputHandler.java에서 해당 입력 및 데이터 검증 작업을 처리하는 과정이 SRP에 맞는거 같아서 변경함

--- src/main/java/racingcar/util/InputHandler.java
+++ src/main/java/racingcar/util/InputHandler.java
@@ -1,12 +1,18 @@
 package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.exception.Validator;
+
+import java.util.List;
 
 public abstract class InputHandler {
 
-    public static String readCarNames() {
+    public static List<String> readCarNames() {
         Printer.print(SystemMessage.START_MESSAGE);
-        return Console.readLine();
+        String carNames = Console.readLine();
+        return Splitor.split(carNames).stream()
+                .map(Validator::validateNameLength)
+                .toList();
     }
 
     public static String readAttemptCount() {
