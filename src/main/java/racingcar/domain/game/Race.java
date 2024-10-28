87304c1eab338d95e82fde31a9ffa0dca447158f
hootzz <wjqwjql3@gmail.com>
2024-10-28 22:16:19 +0900

    feat(domain): Race 상태 관리 기능 구현

--- src/main/java/racingcar/domain/game/Race.java
+++ src/main/java/racingcar/domain/game/Race.java
@@ -1,4 +1,20 @@
 package racingcar.domain.game;
 
+import racingcar.domain.car.Cars;
+import racingcar.domain.strategy.MovingStrategy;
+
 public class Race {
+    private final Cars cars;
+    private final int attempts;
+
+    public Race(Cars cars, int attempts) {
+        this.cars = cars;
+        this.attempts = attempts;
+    }
+
+    public void start(MovingStrategy strategy) {
+        for (int i = 0; i < attempts; i++) {
+            cars.race(strategy);
+        }
+    }
 }
