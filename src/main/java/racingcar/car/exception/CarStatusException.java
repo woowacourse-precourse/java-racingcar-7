75ad0175d1493e0bf64a2d701b4141301949b367
jieun <ju3391@naver.com>
2024-10-28 20:27:56 +0900

    remove : 불필요한 파일들 삭제

--- src/main/java/racingcar/car/exception/CarStatusException.java
+++
@@ -1,8 +0,0 @@
-package racingcar.car.exception;
-
-// 자동차 상태 관련 예외 처리 클래스
-public class CarStatusException extends IllegalArgumentException {
-    public CarStatusException(String message) {
-        super(message);
-    }
-}
