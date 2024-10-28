c94d7ca4eca8d57bc0367559c2caa6b1ba97939b
jae woo Jung <awfjol2008@gmail.com>
2024-10-28 18:30:57 +0900

    refactor: RaceResultResponse 클래스 제거

--- src/main/java/racingcar/dto/RaceResultResponse.java
+++
@@ -1,9 +0,0 @@
-package racingcar.dto;
-
-import racingcar.domain.Car;
-
-public record RaceResultResponse(String name, int distance) {
-	public RaceResultResponse(Car car) {
-		this(car.getName(), car.getDistance());
-	}
-}
