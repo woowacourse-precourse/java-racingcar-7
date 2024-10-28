0911ac4bc83f3d015aa1f01508facbcbf7e37a79
better.gyeom <better.gyeom@gmail.com>
2024-10-28 10:55:11 +0900

    feat: 자동차 이름 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] input = scanner.nextLine().split(",");
+
+        Map<String, Integer> cars = new HashMap<>();
+        for (int i = 0; i < input.length; i++) {
+            if (input[i].length() <= 5) {
+                cars.put(input[i], 0);
+            } else {
+                throw new IllegalArgumentException("자동차의 이름은 5문자를 넘을 수 없습니다.");
+            }
+        }
+
+
     }
+
+
 }
