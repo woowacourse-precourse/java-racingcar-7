c307285a13b044cfea50d21356589403edc6e5de
bk <naturalbb360@gmail.com>
2024-10-28 13:12:21 +0900

    refactor(Car): 메소드명 수정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,27 +1,27 @@
 package racingcar;
 
 class Car {
-    private String name;
-    private int currentLocation;
 
-    public Car(String name){
+    private String name;
+    private int currentLocation = 0;
+
+    public Car(String name) {
         this.name = name;
-        currentLocation = 0;
     }
 
-    public void move(){
+    public void move() {
         currentLocation++;
     }
 
-    public void rename(String newName){
+    public void rename(String newName) {
         name = newName;
     }
 
-    public String name(){
+    public String name() {
         return name;
     }
 
-    public int currentLocation(){
+    public int getcurrentLocation() {
         return currentLocation;
     }
 }
