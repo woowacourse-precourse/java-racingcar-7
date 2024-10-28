e665d769260f799d9507ce5dd41caae03d420ba0
ChoiDongJoon <adong0808@naver.com>
2024-10-28 20:24:35 +0900

    feat(racingcar): 레이싱카 이름이 공백일 경우 예외 처리
    레이싱카 이름이 공백일 경우 예외 처리

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -11,6 +11,12 @@ public class Validator {
         }
     }
 
+    public void carNameEmptyValidate(String car) {
+        if (car.isBlank()) {
+            throw new IllegalArgumentException("레이싱카 이름은 공백일 수 없습니다.");
+        }
+    }
+
     // ','가 아닌 다른 것으로 구분되어 있을 경우
     public Boolean delimiterValidate(String car) {
         for (char c : car.toCharArray()) {
