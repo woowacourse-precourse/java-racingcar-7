b749ef908f9c20d985792c358adb039889ff1df3
DongHoon Lee <dhl1924@naver.com>
2024-10-28 17:50:52 +0900

    refactor(util): TryCountParser에서 양의 정수 검증 로직 삭제
    - 양의 정수 여부 검증은 `Count` 도메인에서 수행하는 것이 책임 분리에 더 적합하다고 판단

--- src/main/java/racingcar/utils/TryCountParser.java
+++ src/main/java/racingcar/utils/TryCountParser.java
@@ -4,23 +4,13 @@ import racingcar.exception.ErrorMessage;
 import racingcar.exception.RacingCarGameException;
 
 public class TryCountParser {
-    private static final int MINIMUM_TRY_COUNT = 1;
-
     private TryCountParser() {
     }
 
     public static int parseTryCount(String tryCountInput) {
         try {
-            int parsedTryCount = Integer.parseInt(tryCountInput);
-            validatePositiveIntegerForTryCount(parsedTryCount);
-            return parsedTryCount;
+            return Integer.parseInt(tryCountInput);
         } catch (NumberFormatException e) {
-            throw RacingCarGameException.from(ErrorMessage.TRY_COUNT_INVALID);
-        }
-    }
-
-    private static void validatePositiveIntegerForTryCount(int tryCount) {
-        if (tryCount < MINIMUM_TRY_COUNT) {
             throw RacingCarGameException.from(ErrorMessage.TRY_COUNT_INVALID);
         }
     }
