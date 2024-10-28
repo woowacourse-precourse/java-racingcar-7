0c45bd5a860c57db97174f7d3d2c0275b37b9bd5
GitDohyun <GitDohyun@gmail.com>
2024-10-28 16:59:31 +0900

    fix(Application): Apllication의 main 메서드의 try-catch 블록 삭제
    try-catch 블록이 없어도 테스트 코드 통과에 문제가 없으므로 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,18 +4,14 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            InputReader inputReader = new InputReader();
-            List<String> carNames = inputReader.readCarNames();
-            int tryCount = inputReader.readTryCount();
+        InputReader inputReader = new InputReader();
+        List<String> carNames = inputReader.readCarNames();
+        int tryCount = inputReader.readTryCount();
 
-            Racing racing = new Racing(carNames);
-            racing.start(tryCount);
+        Racing racing = new Racing(carNames);
+        racing.start(tryCount);
 
-            Winner winner = new Winner();
-            winner.printWinner(racing.positions());
-        } catch (RuntimeException e) {
-            throw new IllegalArgumentException(e);
-        }
+        Winner winner = new Winner();
+        winner.printWinner(racing.positions());
     }
 }
