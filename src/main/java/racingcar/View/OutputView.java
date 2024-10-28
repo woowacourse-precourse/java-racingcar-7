bdd2512ca5d5164262ae3dceb975079afd3ea62c
woojin cheon <qpqpqp0102@naver.com>
2024-10-28 16:38:00 +0900

    feat(OutputView) : add method for attempt number - add method that asks attempt number

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -2,11 +2,11 @@ package racingcar.View;
 
 public class OutputView {
 
-    InputView inputView = new InputView();
-
     public void askCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-        inputView.inputRacingCarName();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
+    public void askNumber() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
 }
