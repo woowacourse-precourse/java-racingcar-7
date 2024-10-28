7bb55745352670ec8e16c50b5b57aa715a8d84a1
wooqqq <sge0225@naver.com>
2024-10-28 20:21:54 +0900

    test: RaceService 내 시도 횟수 입력 테스트 추가

--- src/test/java/racingcar/RaceServiceTest.java
+++ src/test/java/racingcar/RaceServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
 import racingcar.model.RaceService;
+import racingcar.util.Converter;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RaceServiceTest {
 
     private RaceService raceService = new RaceService();
+    private Converter converter = new Converter();
 
     @Test
     @DisplayName("자동차 리스트 생성 테스트")
@@ -25,6 +27,16 @@ public class RaceServiceTest {
         assertThat(carList.get(0).getName()).isEqualTo("pobi");
         assertThat(carList.get(1).getName()).isEqualTo("woni");
         assertThat(carList.get(2).getName()).isEqualTo("conn");
+    }
+
+    @Test
+    @DisplayName("시도 횟수 입력 테스트")
+    void getTryCount() {
+        // 직접 입력값 설정
+        String inputTryCnt = "5";
+        int tryCount = converter.convertTryCnt(inputTryCnt);
+
+        assertThat(tryCount).isEqualTo(5);
     }
 
 }
