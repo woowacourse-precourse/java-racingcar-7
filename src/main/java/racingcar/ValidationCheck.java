159a148a88d41667e865209f2eadff24a9f3a007
mirolis6636 <mirolis6636@gmail.com>
2024-10-28 20:51:51 +0900

    feat: 입력값 유효성 검사 메서드 추가
    isNullOrEmptyName 자동차 이름입력이 null이거나 ""일때
    hasMoreThanFiveCharacters 자동차 이름 6자 이상일 때
    hasDuplicatNames 자동차 이름이 중복될 때
    endsWithComma 입력 마지막이 구분자로 끝날 때
    
    isNullOrEmptyNumber 시도횟수가 null이거나 "" 일 때
    hasNumberFormatException parseInt 때 오류 확인
    isNegativeTrial 시도횟수가 음수일 때

--- src/main/java/racingcar/ValidationCheck.java
+++ src/main/java/racingcar/ValidationCheck.java
@@ -1,5 +1,10 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ValidationCheck {
@@ -7,13 +12,62 @@ public class ValidationCheck {
 
     public void validateCarName(String carName){
         isNullOrEmptyName(carName);
-        hasDuplicateNames(carName);
         hasMoreThanFiveCharacters(carName);
+        hasDuplicateNames(carName);
         endsWithComma(carName);
     }
-    public void validateNumberOfTrial(String numberOfTrial){
-        hasNumberFormatException(numberOfTrial);
-        isNegetiveTrial(numberOfTrial);
-        isNullOrEmptyNumber(numberOfTrial);
+
+    private void isNullOrEmptyName(String carName) {
+        if(carName.isBlank() || carName.isEmpty())
+            throw new IllegalArgumentException("입력값이 없습니다.");
     }
+
+    private void hasMoreThanFiveCharacters(String carName) {
+        for(String car : carName.split(",")){
+            Matcher matcher = pattern.matcher(car);
+            if(matcher.matches()==false)
+                throw new IllegalArgumentException("이름이 다섯글자가 넘거나 이름에 허용되지 않는 특수문자가 있습니다.");
+        }
+    }
+
+    private void hasDuplicateNames(String carName) {
+        String[] splitCarName = carName.split(",");
+        Set<String> carNameSet = new HashSet<>(Arrays.asList(splitCarName));
+        ArrayList<String> carNameArray = new ArrayList<>(Arrays.asList(splitCarName));
+        if(carNameSet.size() != carNameArray.size())
+            throw new IllegalArgumentException("중복된 자동차 이름이 있습니다.");
+    }
+
+
+    private void endsWithComma(String carName) {
+        if(carName.endsWith(",")){
+            throw new IllegalArgumentException("구분자 뒤에 자동차 이름을 입력하세요.");
+        }
+    }
+
+    public void validateNumberOfTrial(String numberOfTrial){
+        isNullOrEmptyNumber(numberOfTrial);
+        hasNumberFormatException(numberOfTrial);
+        isNegativeTrial(numberOfTrial);
+    }
+
+    private void isNullOrEmptyNumber(String numberOfTrial) {
+        if(numberOfTrial.isEmpty() || numberOfTrial.isBlank())
+            throw new IllegalArgumentException("시도횟수가 올바르지 않습니다.");
+    }
+
+    private void hasNumberFormatException(String numberOfTrial){
+        try{
+            int intNumberOfTrial = Integer.parseInt(numberOfTrial);
+        }
+        catch(NumberFormatException e){
+            throw new IllegalArgumentException("시도 횟수가 올바르지 않습니다.");
+        }
+    }
+    private void isNegativeTrial(String numberOfTrial) {
+        if(Integer.parseInt(numberOfTrial) <= 0){
+            throw new IllegalArgumentException("최소 한 번의 시도를 해야합니다.");
+        }
+    }
+
 }
