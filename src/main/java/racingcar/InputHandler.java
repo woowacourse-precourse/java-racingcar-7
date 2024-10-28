d3faaf25cbf23489d8eb22e7ed79243c88a9b71d
seongsiyeong <ssy2681q@naver.com>
2024-10-28 19:58:47 +0900

    refactor : InputHandler 리팩토링

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -2,18 +2,51 @@ package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputHandler {
 
-    public static void readRacingInfo(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String nameString = readLine();
+    private static final String NAME_DIAMETER = ",";
 
+    public static void readRacingInfo(){
+
+        List<String> nameList = splitNames(getNameString());
+
+        int tryCount = getTryCount();
+
+    }
+
+    private static int getTryCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
+
         try {
-            int tryCount = Integer.parseInt(readLine());
+            return Integer.parseInt(readLine());
         } catch (NumberFormatException numberFormatException){
             throw new IllegalArgumentException();
         }
 
+    }
+
+    private static String getNameString() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        String nameString = readLine();
+        return nameString;
+    }
+
+    private static List<String> splitNames(String nameString) {
+        String[] participants = nameString.split(NAME_DIAMETER);
+        validateNames(participants);
+
+        return Arrays.stream(participants).toList();
+    }
+
+    private static void validateNames(String[] participants) {
+        for (String participant : participants) {
+            if (participant.length() < 5) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
