0ba1dfb68d6ce63275c052cccfdd24976e48b6d0
davemins <davemins4125@naver.com>
2024-10-28 20:24:05 +0900

    test(Domain): 자동차 전진 시도 기능 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,13 +1,12 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position;
 
     public Car(String name) {
-        if (name == null || name.isEmpty() || name.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하로 입력해야 합니다.");
-        }
         this.name = name;
         this.position = 0;
     }
@@ -18,5 +17,12 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    public void tryMove() {
+        int randomValue = Randoms.pickNumberInRange(1, 9);
+        if (randomValue >= 4) {
+            position++;
+        }
     }
 }
