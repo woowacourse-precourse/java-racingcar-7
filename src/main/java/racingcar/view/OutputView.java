de83a11e67a7007425ac7c27ab7b7544caa6b922
김주언 <wndjs803@naver.com>
2024-10-28 22:34:29 +0900

    feat: OutputView printPlayResults 메서드 작성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,17 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
     public void printPlayMessage(){
         System.out.println();
         System.out.println("실행결과");
+    }
+
+    public void printPlayResults(List<String> results){
+        for (String result : results) {
+            System.out.println(result);
+        }
+        System.out.println();
     }
 }
