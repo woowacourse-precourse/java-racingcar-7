fa4c4e1ee3ca4921d3e4529ef3866ea5a7f41bb2
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2024-10-28 17:57:46 +0900

    feat : Make information for users
    Make showCarInputInfo and showRoundTimeInfo for show information to users

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -21,6 +21,14 @@ public class RacingCarView {
         return Integer.parseInt(input);
     }
 
+    public void showCarInputInfo(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void showRoundTimeInfo(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
 }
 
 
