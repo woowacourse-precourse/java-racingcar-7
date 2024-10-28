f79a51bb81d2d7ab94a0f472c7cb161cc5b64d55
임채연 <dlacodus0407@gmail.com>
2024-10-28 20:04:13 +0900

    refactor(OutputView): change access controller range
    - 메소드의 접근제어자를 public 으로 선언합니다.
    - 다른 패키지에서도 사용이 가능하게 합니다.

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,31 +1,31 @@
 package racingcar;
 
-import static racingcar.GameMessages.*;
-import static racingcar.RegexPatterns.*;
+import static racingcar.common.constant.GameMessages.*;
+import static racingcar.common.constant.RegexPatterns.*;
 
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class OutputView {
-    void printResultGuideMessage() {
+    public void printResultGuideMessage() {
         System.out.println(OUTPUT_RESULT_GUIDE_MESSAGE);
     }
 
-    void resultMessageLoop(List<RacingCar> racingCars) {
+    public void resultMessageLoop(List<RacingCar> racingCars) {
         racingCars.forEach(racingCar -> {
             printResultMessage(racingCar.getName(), racingCar.getScore());
         });
         System.out.println();
     }
 
-    void printResultMessage(String name, Integer number) {
+    public void printResultMessage(String name, Integer number) {
         System.out.print(name + " : ");
         IntStream.range(0, number)
                 .forEach(i -> System.out.print(OUTPUT_SCORE_CHARACTER));
         System.out.println();
     }
 
-    void printWinnerMessage(List<String> winners) {
+    public void printWinnerMessage(List<String> winners) {
         System.out.print(OUTPUT_WINNER_GUIDE_MESSAGE);
         String result = String.join(DELIMITER + " ", winners);
         System.out.print(result);
