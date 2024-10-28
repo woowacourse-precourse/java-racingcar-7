c887f7a9c9759dc1df1f698cd8d8a225a8a79a4a
cksgud0403 <cksgud0403@naver.com>
2024-10-28 23:19:28 +0900

    feat: add function to set car names

---
+++ src/main/java/racingcar/domain/Race.java
@@ -0,0 +1,26 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+    private final int MIN_VALUE = 0;
+    private final int MAX_VALUE = 9;
+    private int tryCount;
+    private List<Car> cars = new ArrayList<>();
+
+    public void setCars(String names) {
+
+        String[] carNames = names.split(",");
+
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.");
+            }
+
+            cars.add(new Car(carName));
+        }
+    }
+}
