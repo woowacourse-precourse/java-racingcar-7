4954afd61f0b79bebd2a68fbfa21aa0d908cbf7c
Leegeonwoo7 <workgeon07@naver.com>
2024-10-28 21:29:46 +0900

    refactor: OutputView출력 메시지 분리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,12 +5,9 @@ import racingcar.domain.Winner;
 
 import java.util.List;
 
+import static racingcar.constants.PrintMessages.*;
+
 public class OutputView {
-    //OutputView 내부에 상수를 정의해서 사용하는 장점이 무엇인지, 메서드이름으로도 충분히 식별을 가능
-    private final static String SET_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final static String SET_TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
-    private final static String CURRENT_RESULT = "실행 결과";
-    private final static String FINAL_WINNER = "최종 우승자 : ";
 
     public void printInputCarNamesMessage() {
         System.out.println(SET_CAR_NAME_MESSAGE);
