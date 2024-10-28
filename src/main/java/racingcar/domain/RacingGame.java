749eae7bfdaf75503801f7f01af552967deeddb5
Jun2-Lee <ljh19980823@gmail.com>
2024-10-28 16:46:33 +0900

    feat : 한 턴 진행후 결과 출력

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,10 +1,12 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.view.Output;
 
 import java.util.List;
 
 public class RacingGame {
+    private final Output output = new Output();
     private final List<Car> carList;
     private final int number;
     public RacingGame(List<Car> carList, int num) {
@@ -15,10 +17,9 @@ public class RacingGame {
     public void start() {
         for(int i = 0; i < number; i++) {
             playTurn();
-            // 현재 턴의 상황 출력
-
+            output.printTurn(carList);
         }
-        // 결과 출력
+
     }
 
     public void playTurn() {
