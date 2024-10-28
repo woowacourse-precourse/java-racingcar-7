e88acf6d4b0ce81a22645bdd875cd8f842f94057
J-1ac <tkdwns26@naver.com>
2024-10-28 15:14:00 +0900

    feat: 자동차 이름 입력 받기 기능 구현

---
+++ src/main/java/racingcar/model/RacingCar.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class RacingCar {
+    private int position;
+    private final String carName;
+
+    public RacingCar(String carName) {
+        this.position = 0;
+        this.carName = carName;
+    }
+}
---
+++ src/main/java/racingcar/service/GameService.java
@@ -0,0 +1,43 @@
+package racingcar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.RacingCar;
+import racingcar.view.GameView;
+
+public class GameService {
+    private List<RacingCar> racingCars;
+    private int numRounds;
+    private final GameView gameView = new GameView();
+
+    public void getCarNames() {
+        String inputCarNames = gameView.inputCarNames();
+        validateCarNames(inputCarNames);
+        createCarList(inputCarNames);
+    }
+
+    private void validateCarNames(String inputCarNames) {
+        if (inputCarNames.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름을 입력해 주세요.");
+        }
+        String[] carNames = inputCarNames.split(",");
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+            if (carName.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 비워둘 수 없습니다.");
+            }
+        }
+    }
+
+    private void createCarList(String inputCarNames) {
+        racingCars = new ArrayList<>();
+        String[] carNames = inputCarNames.split(",");
+        for (String carName : carNames) {
+            RacingCar racingCar = new RacingCar(carName);
+            racingCars.add(racingCar);
+        }
+    }
+
+}
