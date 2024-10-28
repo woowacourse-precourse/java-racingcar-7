f51f62c6ad76cf60e244f019d0ae6322eda60003
jieun <ju3391@naver.com>
2024-10-28 21:01:29 +0900

    refactor(ExceptionEnum): 에러 메시지 수정

--- src/main/java/racingcar/common/ExceptionEnum.java
+++ src/main/java/racingcar/common/ExceptionEnum.java
@@ -7,8 +7,7 @@ public enum ExceptionEnum {
     INVALID_MIN_CAR_NAME_LENGTH("자동차 이름은 최소 1글자 이상이어야 합니다."),
     INVALID_MAX_CAR_NAME_LENGTH("자동차 이름은 5글자를 초과할 수 없습니다."),
     EMPTY_CAR_NAME("자동차 이름은 빈 입력일 수 없습니다."),
-    DUPLICATE_CAR_NAME("자동차 이름은 중복될 수 없습니다."),
-    //    DUPLICATE_CAR_NAME("자동차 이름은 중복될 수 없습니다. 중복된 자동차 이름 :"),
+    DUPLICATE_CAR_NAME("자동차 이름은 중복될 수 없습니다. 중복된 자동차 이름 : "),
     INVALID_CAR_NAME_FORMAT("자동차 이름으로 특수문자, 숫자 또는 한글은 사용할 수 없습니다."),
     NULL_CAR_NAME("자동차 이름은 null일 수 없습니다."),
 
@@ -16,7 +15,6 @@ public enum ExceptionEnum {
     NEGATIVE_ATTEMPT_COUNT("시도 횟수는 음수일 수 없습니다."),
     ZERO_ATTEMPT_COUNT("시도 횟수는 0일 수 없습니다."),
     NON_INTEGER_ATTEMPT_COUNT("시도 횟수는 정수여야 합니다."),
-    NON_NUMERIC_ATTEMPT_COUNT("시도 횟수에는 숫자만 입력 가능합니다."),
     EMPTY_ATTEMPT_COUNT("시도 횟수는 빈 입력일 수 없습니다."),
     NULL_ATTEMPT_COUNT("시도 횟수는 null일 수 없습니다.");
 
