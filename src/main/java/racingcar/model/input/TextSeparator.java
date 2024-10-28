bdb324a428cf4cb521c170774e05721923293b81
Chan531 <chan657312@gmail.com>
2024-10-28 18:17:18 +0900

    refactor(TextSeparator): 중복된 자동차 이름 입력 시 예외 발생

--- src/main/java/racingcar/model/input/TextSeparator.java
+++ src/main/java/racingcar/model/input/TextSeparator.java
@@ -1,5 +1,8 @@
 package racingcar.model.input;
 
+import static racingcar.model.message.ErrorMessage.INVALID_CAR_NAMES_INPUT;
+
+import java.util.HashSet;
 import java.util.List;
 
 public class TextSeparator {
@@ -7,6 +10,14 @@ public class TextSeparator {
 	private static final String DELIMITER = ",";
 
 	public static List<String> separateCarNames(String text) {
-		return List.of(text.split(DELIMITER));
+		List<String> carNames = List.of(text.split(DELIMITER));
+		checkCarNamesDuplicated(carNames);
+		return carNames;
+	}
+
+	private static void checkCarNamesDuplicated(List<String> carNames) {
+		if (new HashSet<>(carNames).size() != carNames.size()) {
+			throw new IllegalArgumentException(INVALID_CAR_NAMES_INPUT.getMessage());
+		}
 	}
 }
