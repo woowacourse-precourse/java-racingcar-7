790dec41cfc1aedd76eed678a7c2f6f64b30de43
Bell-isHere <quf417@gmail.com>
2024-10-28 20:58:15 +0900

    feat: 자동차 이름 Console 입력 구현
    - 자동차 이름 Console 입력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("이름을 입력해주세요: ");
+        String names = Console.readLine();
     }
 }
