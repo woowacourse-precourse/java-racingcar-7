face22e788cebdce3d0c0c253b7748df9005191e
sanghee0820 <lsh277604@gmail.com>
2024-10-28 20:57:48 +0900

    feat: 이름이 비어있는 경우 오류코드 추가

--- src/main/java/racingcar/exception/InputException.java
+++ src/main/java/racingcar/exception/InputException.java
@@ -1,7 +1,8 @@
 package racingcar.exception;
 
 public enum InputException {
-    INPUT_IS_NULL_OR_EMPTY("입력값이 비어있거나 null입니다.");
+    INPUT_IS_NULL_OR_EMPTY("입력값이 비어있거나 null입니다."),
+    NAME_IS_EMPTY("이름이 빈 값이 포함되어 있습니다.");
 
     private final String message;
 
