a6d4545952c695604981fe2fdd57435cca52349d
mirolis6636 <mirolis6636@gmail.com>
2024-10-28 20:14:13 +0900

    refactor: validateCarNames 이름을 재정의

--- src/main/java/racingcar/InputReader.java
+++ src/main/java/racingcar/InputReader.java
@@ -1,7 +1,5 @@
 package racingcar;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,7 +7,7 @@ import java.util.regex.Pattern;
 public class InputReader {
     public String readCarNames() throws NoSuchElementException{
         String userInput = camp.nextstep.edu.missionutils.Console.readLine();
-        validateInput(userInput);
+        validateCarNames(userInput);
         return userInput;
     }
     public int readNumberOfTrial() throws NoSuchElementException{
@@ -17,7 +15,7 @@ public class InputReader {
         return Integer.parseInt(userInput);
     }
     private static Pattern pattern = Pattern.compile("[A-Za-z][A-Za-z0-9]{0,4}");
-    private void validateInput(String userInput) throws IllegalArgumentException{
+    private void validateCarNames(String userInput) throws IllegalArgumentException{
         for(String name : userInput.split(",")){
             Matcher matcher = pattern.matcher(name.trim());
             if(!matcher.matches())
