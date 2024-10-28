ef6c276f2d6ee5f0d21af0f720c903f82629e2ff
kellyihyeon <kellykang13@gmail.com>
2024-10-28 19:25:55 +0900

    fix: 사용자 입력값을 한번에 입력 받게끔 수정

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -7,13 +7,29 @@ public class InputView {
     private static String requestName = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static String requestRound = "시도할 횟수는 몇 회인가요?";
 
-    public static String requestCarNames() {
+    private static String carNames;
+    private static String round;
+
+    public static void requestCarNames() {
         System.out.println(requestName);
-        return Console.readLine();
+        carNames = Console.readLine();
     }
 
-    public static String requestRound() {
+    public static void requestRound() {
         System.out.println(requestRound);
-        return Console.readLine();
+        round = Console.readLine();
+    }
+
+    public static void requestInput() {
+        requestCarNames();
+        requestRound();
+    }
+
+    public static String round() {
+        return round;
+    }
+
+    public static String carNames() {
+        return carNames;
     }
 }
