86ea9fc26922bf7a897067fd0968bc20189d96f2
greensnapback0229 <smdmim@gmail.com>
2024-10-28 22:05:20 +0900

    feat(Controller): add process for racing game on controller

--- src/main/java/racingcar/controller/CarRacingController.java
+++ src/main/java/racingcar/controller/CarRacingController.java
@@ -1,22 +1,31 @@
 package racingcar.controller;
 
 import java.util.List;
+import racingcar.service.AwardService;
+import racingcar.service.GameService;
 import racingcar.view.Input;
 import racingcar.view.Output;
 
 public class CarRacingController {
 
-    private Input input;
-    private Output output;
+    private static CarRacingController instance;
 
-    public CarRacingController(Input input, Output output) {
+    private final Input input;
+    private final Output output;
+    private final GameService gameService;
+    private final AwardService awardService;
+
+    public CarRacingController(Input input, Output output, GameService gameService, AwardService awardService) {
         this.input = input;
         this.output = output;
+        this.gameService = gameService;
+        this.awardService = awardService;
     }
 
     public void startCarRacing() {
         List<String> carNames = input.inputCarNames();
         int roundCount = input.inputRoundCount();
-
+        Integer gameId = gameService.startGame(carNames, roundCount);
+        awardService.presentAward(gameId);
     }
 }
