9bee4636f0fd7b474ca2841c9b19e34201234493
honga <honga@BOOK-5SF7AISI00>
2024-10-28 11:36:32 +0900

    FEAT : 메인 함수에서 다른 함수를 호출하도록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -110,6 +110,19 @@ class Function {
 
 public class Application {
 	public static void main(String[] args) {
+		try {
+			System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+			String nameString = camp.nextstep.edu.missionutils.Console.readLine();
+			System.out.println("시도할 횟수는 몇 회인가요?");
+			int carryoutCount = Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
+
+			System.out.println("실행결과");
+			ArrayList<Player> playerList = Function.makePlayerList(nameString);
+			Function.processSteps(playerList, carryoutCount);
+			Function.printWinner(Function.searchWinner(playerList, carryoutCount));
+		}catch(NumberFormatException e) {
+			throw new IllegalArgumentException();
+		}
 
 	}
 }
