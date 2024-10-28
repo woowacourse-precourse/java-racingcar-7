f5c4e079c0fbd07314e9cb0fef0fd4896131036f
김호연 <yun1094@naver.com>
2024-10-28 13:46:58 +0900

    자동차 이름의 길이 5자 제한 추가

--- src/main/java/racingcar/View/Input.java
+++ src/main/java/racingcar/View/Input.java
@@ -8,8 +8,13 @@ public class Input {
     private Integer trialCount;
 
     public List<String> RequestCarNameMessage() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> carNameList = List.of(Console.readLine().split(","));
+        for(String carName : carNameList) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름의 길이는 5자 이하여야 합니다.");
+            }
+        }
         return carNameList;
     }
 
