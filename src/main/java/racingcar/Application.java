b7709efcccab1a884f8479a85da32cf508c330f2
Park - Jeonghwan <990706leo@gmail.com>
2024-10-28 14:45:19 +0900

    refactor(Application): 메서드 및 변수 네이밍 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,25 @@
 package racingcar;
 
 import racingcar.controller.RacingController;
+import racingcar.domain.Movement;
+import racingcar.domain.NumberGenerator;
 import racingcar.domain.NumberOverFour;
 import racingcar.domain.RandomNumberGenerator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Application {
+
+    public final static int RANDOM_NUMBER_END_INCLUSIVE = 9;
+
     public static void main(String[] args) {
-        new RacingController(new InputView(), new OutputView(), new NumberOverFour(new RandomNumberGenerator(9))).run();
+        NumberGenerator randomNumberGenerator = new RandomNumberGenerator(RANDOM_NUMBER_END_INCLUSIVE);
+        Movement numberOverFour = new NumberOverFour(randomNumberGenerator);
+
+        new RacingController(
+                new InputView(),
+                new OutputView(),
+                numberOverFour
+        ).run();
     }
 }
