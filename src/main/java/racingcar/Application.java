d0df3814c1d8c20bc1de86c950f0b4fc472115c5
pang0916 <qrv22@naver.com>
2024-10-28 17:41:19 +0900

    feat(Application, ApplicationTest): add input car names and run count parser
    Added parser logics to inputCarNames and inputRunCount:
    - car name must be between 1 to 5 characters
    - run count must be positive integer or zero
    - all inputs must be provided within the time limit, or an IllegalArgumentException will be thrown

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,50 @@
 package racingcar;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.NoSuchElementException;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.setOut(new PrintStream(new PrintStream(System.out, true, StandardCharsets.UTF_8)));
+        String[] carNames = inputCarNames();
+        int runCount = inputRunCount();
+
+        // input check
+        for (int i = 0; i < carNames.length; i++) {
+            System.out.println("#" + i + " " + carNames[i]);
+        }
+        System.out.println("runCount : " + runCount);
+    }
+
+    public static String[] inputCarNames() throws IllegalArgumentException {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carNames = Console.readLine().split(",");
+
+        for (String carName : carNames) {
+            if (carName.isEmpty() || carName.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        return carNames;
+    }
+
+    public static int inputRunCount() throws IllegalArgumentException {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int runCount;
+
+        try{
+            runCount = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException | NoSuchElementException e) {
+            throw new IllegalArgumentException();
+        }
+
+        if (runCount < 0) {
+            throw new IllegalArgumentException();
+        }
+        return runCount;
     }
 }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,31 +3,86 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
+//    private static final int MOVING_FORWARD = 4;
+//    private static final int STOP = 3;
+//
+//    @Test
+//    void 기능_테스트() {
+//        assertRandomNumberInRangeTest(
+//            () -> {
+//                run("pobi,woni", "1");
+//                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+//            },
+//            MOVING_FORWARD, STOP
+//        );
+//    }
 
-    @Test
-    void 기능_테스트() {
-        assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
-        );
-    }
-
-    @Test
+    @Test // 5글자 이상
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 빈_자동차입력_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "5"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 빈_자동차이름_테스트1() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",pobi", "5"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 빈_자동차이름_테스트2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,,woni", "5"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 빈_시도횟수_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", ""))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자가아닌_시도횟수_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 음수_시도횟수_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void overflow_시도횟수_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "2200000000"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
