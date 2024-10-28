416fc29eda629666cdc95fbebe1804356154b7cc
username0w <grandcias123@gmail.com>
2024-10-28 22:32:21 +0900

    feat: 경기 결과 및 우승자 출력 메서드 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,12 +1,23 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.model.Car;
+
 public class OutputView {
 
-    public void printRoundResults () {
+    public void printRoundResultsHeader() {
         System.out.println("실행 결과");
     }
 
-    public void printWinner() {
-        System.out.println("최종 우승자 : ");
+    public void printRoundResults(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.print(car.getName() + " : ");
+            System.out.println("-".repeat(car.getDistance()));
+        }
+        System.out.println();
+    }
+
+    public void printWinner(String winners) {
+        System.out.println("최종 우승자 : " + winners);
     }
 }
