298cc6acfdadf8005b6ebc4d5a7596df3427419f
jihyun <jihyun3066@naver.com>
2024-10-28 20:10:49 +0900

    [ refactor ]  : 입력 메시지의 접근 제어자를 private으로 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,8 +2,8 @@ package racingcar.view;
 
 public class InputView {
 
-    public static String PRINT_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public static String PRINT_LIMIT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static String PRINT_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static String PRINT_LIMIT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public static void printInputMessage() {
         System.out.println(PRINT_INPUT_MESSAGE);
