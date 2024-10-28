c58350e89b0aac4da1df1aa745005ca6140af6b8
JeongHyeon Jo <wjdgusdlekd3@yonsei.ac.kr>
2024-10-28 12:18:45 +0900

    feat(view): feature view added logic
    - InputView: validate 로직 추가
    - OutputView: 공동 승자 추가 및 출력 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,9 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class InputView {
     private static final String INPUT_DELIMITER_COMMA = ",";
+    private static final String INPUT_DUPLICATE_VALIDATE_MESSAGE = "ERROR: 중복된 값이 존재합니다.";
     private final ArrayList<String> inputDataList;
     String inputData;
 
@@ -23,6 +26,7 @@ public class InputView {
         if (readCarString != null && !readCarString.isEmpty()) {
             String[] parsedString = readCarString.split(INPUT_DELIMITER_COMMA);
             trimFirstElement(parsedString);
+            validateNoDuplicates(parsedString);
             inputDataList.addAll(Arrays.asList(parsedString));
         }
     }
@@ -37,4 +41,10 @@ public class InputView {
         return inputDataList;
     }
 
+    private void validateNoDuplicates(String[] elements) {
+        Set<String> uniqueElements = new HashSet<>(Arrays.asList(elements));
+        if (uniqueElements.size() != elements.length) {
+            throw new IllegalArgumentException(INPUT_DUPLICATE_VALIDATE_MESSAGE);
+        }
+    }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,6 +6,7 @@ public class OutputView {
     private static final String PRINT_READ_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String PRINT_READ_TOTAL_TRY_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private static final String PRINT_WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String PRINT_RUN_RESULT_MESSAGE = "실행 결과 : ";
 
     private void printMessage(String message) {
         System.out.println(message);
@@ -20,7 +21,10 @@ public class OutputView {
         printMessage(PRINT_READ_TOTAL_TRY_MESSAGE);
     }
 
+    public void printRunResult() {printMessage(PRINT_RUN_RESULT_MESSAGE);}
+
     public void printWinners(List<String> winners) {
-        System.out.println(PRINT_WINNER_MESSAGE + String.join(", ", winners));
+        printMessage(PRINT_WINNER_MESSAGE + String.join(", ", winners));
     }
+
 }
