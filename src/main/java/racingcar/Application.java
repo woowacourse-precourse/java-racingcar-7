5a082450b09ac0cf3b5cfeee735dfa149c0beaca
kryptonite43 <sonyj0430@gmail.com>
2024-10-28 15:20:06 +0900

    fix: 예외 처리 구현 - , 이외 구분자/이동횟수 오류/6자 이상의 이름 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,16 +7,33 @@ import java.util.*;
 public class Application {
     static HashMap<String, Integer> carScores = new HashMap<String, Integer>();
     public static void main(String[] args) {
+        int repeat = 0;
         // 차 이름 입력받고 이름을 하나씩 나누어 이름 배열에 저장 -> getCarNames()
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String names = Console.readLine();
+
+        if (!names.matches("^[a-zA-Z가-힣]+(,[a-zA-Z가-힣]+)*$")) {
+            throw new IllegalArgumentException("잘못된 입력 값입니다: , 외의 구분자를 사용했습니다. 이름은 한글, 영문만으로 이루어져야 합니다.");
+        }
         String[] cars = names.split(",");
 
+        for (int i = 0; i<cars.length; i++) {
+            if (cars[i].length() > 5) {
+                throw new IllegalArgumentException("잘못된 입력 값입니다: 이름은 5자 이하여야 합니다.");
+            }
+        }
         // 시도 횟수 -> getRepeatNum()
         System.out.println("시도할 횟수는 몇 회인가요?");
         String repeatStr = Console.readLine();
-        int repeat = Integer.parseInt(repeatStr);
-
+        try {
+            repeat = Integer.parseInt(repeatStr);
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 입력 값입니다: 이동 횟수에 문자가 입력되었습니다.");
+        }
+        if (repeat < 0) {
+            throw new IllegalArgumentException("잘못된 입력 값입니다: 이동 횟수가 음수입니다.");
+        }
         // 자동차 스코어 초기화해서 해시맵에 저장
         for (String car: cars) {
             carScores.put(car, 0);
@@ -62,6 +79,7 @@ public class Application {
         List<String> winners = new ArrayList<>();
         int maxScore = 0;
 
+        // 내림차순 정렬 -> DescendingSortByValue()
         List<Map.Entry<String, Integer>> entryList = new LinkedList<>(carScores.entrySet());
         entryList.sort(new Comparator<Map.Entry<String, Integer>>() {
             @Override
@@ -69,6 +87,8 @@ public class Application {
                 return o2.getValue() - o1.getValue(); // 내림차순 정렬
             }
         });
+
+        // 우승자 목록 저장 -> saveWinners()
         for(Map.Entry<String, Integer> entry : entryList){
             int curScore = entry.getValue();
             if (maxScore <= curScore) { // 처음엔 0<3이므로 해당, 두번째는 3=3이므로 해당, 2<3이면 종료
@@ -80,10 +100,7 @@ public class Application {
             }
         }
         result = String.join(", ", winners);
-        /*
-        * var members = List.of("pobi", "jason");
-          var result = String.join(",", members); // pobi,jason
-        * */
+
         return result;
     }
 }
