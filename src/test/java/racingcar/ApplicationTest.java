9e6dbafe4bcb381fb03dabde2698a63d90635b70
jjun522 <tjwowns522@>
2024-10-28 19:32:46 +0900

    test: ApplicationTest 클래스 수정 및 테스트 케이스 추가
    - 새로운 테스트 케이스 추가로 입력 검증 강화
    - 기존 테스트 케이스 수정 및 예외 처리 테스트 추가
    - 코드 리팩토링에 따른 테스트 메서드 수정

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,10 +3,17 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.validator.CarNameValidator.validate;
+import static racingcar.validator.CarNameValidator.validateNames;
+import static racingcar.validator.RaceValidator.validateRounds;
+import static racingcar.validator.RaceValidator.validateRoundsInput;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -30,6 +37,36 @@ class ApplicationTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void 자동차_이름이_빈_문자열일_때_예외발생() {
+        assertThatThrownBy(() -> validate(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 올바른_자동차_이름_리스트는_예외_발생하지_않음() {
+        List<String> carNames = Arrays.asList("pobi", "woni", "jun");
+        validateNames(carNames); // 예외가 발생하지 않으면 테스트 통과
+    }
+
+    @Test
+    void 시도_횟수가_숫자가_아닐_때_예외발생() {
+        assertThatThrownBy(() -> validateRoundsInput("abc"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 시도_횟수가_음수일_때_예외발생() {
+        assertThatThrownBy(() -> validateRounds(-5))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 시도_횟수가_올바른_숫자일_때_예외_발생하지_않음() {
+        validateRounds(5); // 예외가 발생하지 않으면 테스트 통과
+    }
+
 
     @Override
     public void runMain() {
