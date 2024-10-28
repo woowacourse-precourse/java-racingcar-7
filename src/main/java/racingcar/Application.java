40ec4a03e3ab46681ef4be5cf2769b0ced2c5a51
nsb329 <nsb329@naver.com>
2024-10-28 17:50:25 +0900

    자동차 이름 별 전진 값 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,14 +8,15 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
 	public static void main(String[] args) {
 		String car[] = input_name();
-		int[] result =new int[car.length];
+		int[] result = new int[car.length];
 		int time = try_count();
 		System.out.println("실행 결과");
 		while (time > 0) {
 			time--;
-			dash_print(car, time,result);
+			dash_print(car, time, result);
 			System.out.println();
 		}
+		winner(car, result);
 	}
 
 	public static String[] input_name() {
@@ -48,7 +49,7 @@ public class Application {
 		return 1;
 	}
 
-	public static int[] random_value(String[] car, int time,int[] result) {
+	public static int[] random_value(String[] car, int time, int[] result) {
 		int random = 0;
 		for (int i = 0; i < result.length; i++) {
 			random = Randoms.pickNumberInRange(0, 9);
@@ -60,6 +61,17 @@ public class Application {
 	}
 
 	public static void dash_print(String[] car, int time, int[] result) {
-		int[] count = random_value(car, time,result);
+		int[] count = random_value(car, time, result);
+		for (int i = 0; i < car.length; i++) {
+			print_result(car[i], count[i]);
 		}
 	}
+
+	public static void print_result(String car, int count) {
+		System.out.print(car + " : ");
+		for (int j = 0; j < count; j++) {
+			System.out.print("-");
+		}
+		System.out.println();
+	}
+}
