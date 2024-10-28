5d46c0b8b7051a5d37cf2f552ef20356bccec947
kmc198989 <kmc198989@naver.com>
2024-10-28 15:46:28 +0900

    Feat: 자동차 전진 조건 구현
    - Randoms.pickNumberInRange를 사용하여 무작위 값 생성
    - 값이 4 이상일 경우 자동차 전진

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -12,6 +14,12 @@ public class Car {
     private void validateName(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    public void move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            position++;
         }
     }
 
