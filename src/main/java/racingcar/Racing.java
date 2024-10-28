5c106fb932fcfe6ab623114008708f80e3d9c44d
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-10-28 18:10:45 +0900

    feat : 5. ì최종 웃ì우승자 확인 후 출력하기

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -3,10 +3,10 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Racing {
-//    ### 3. 무작위 값 생성하기
-//    ### 4. 생성된 랜덤수를 통한 전진여부 판단하기
-//    ### 5. 최종 우승자 확인후 출력하기
 
     public void run() {
         // # 1. 경주할 자동차 입력받기
@@ -23,13 +23,57 @@ public class Racing {
         // # 4. 생성된 랜덤수를 통한 전진여부 판단하기
         System.out.println("실행 결과");
         for (int i = 0; i < repeat; i++) {
-            for (int j = 0; j < racer.length; j++) {
-                if (isMove(randomGenerator())) racerMoved[j]++;
-                System.out.println(racer[j] + " : " + "-".repeat(racerMoved[j]));
-            }
+            defineMove(racer, racerMoved);
         }
 
+        // #5. 최종 우승자 확인 후 출력하기
+        List<String> winners = findWinner(racer, racerMoved);
+        int size = winners.size();
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < size; i++) {
+            printWinner(winners, size, i);
+        }
+    }
 
+    // 랜덤수 생성 후 전진여부 판단
+    public void defineMove(String[] racer, int[] racerMoved) {
+        for (int j = 0; j < racer.length; j++) {
+            if (isMove(randomGenerator())) racerMoved[j]++;
+            System.out.println(racer[j] + " : " + "-".repeat(racerMoved[j]));
+        }
+        System.out.println();
+    }
+
+    // 출력분리
+    public void printWinner(List<String> winners, int size, int idx) {
+        if (size == 1) {
+            System.out.println(winners.get(idx));
+        } else {
+            if (idx < size - 1) {
+                System.out.print(winners.get(idx) + ", ");
+            } else {
+                System.out.println(winners.get(idx));
+            }
+        }
+    }
+
+    // 최종 우승자 찾기
+    public List<String> findWinner(String[] racer, int[] racerMoved) {
+        List<String> winners = new ArrayList<>();
+        int MAX = findMax(racerMoved);
+        for (int i = 0; i < racer.length; i++) {
+            if (racerMoved[i] == MAX) winners.add(racer[i]);
+        }
+        return winners;
+    }
+
+    // 최대값 찾기
+    public int findMax(int[] racerMoved) {
+        int max = 0;
+        for (int racer : racerMoved) {
+            max = Math.max(racer, max);
+        }
+        return max;
     }
 
     // , 기준으로 분리하기
