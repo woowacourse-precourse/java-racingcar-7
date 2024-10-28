415b11fda8e8d7512f246c637a60bdffe734f676
AntsThatWander <anubspider@gmail.com>
2024-10-28 17:02:10 +0900

    fix($Application, $Racing) : move an input from Application.main() to Racing.startRace()
    Move an input functionality from Application.main() to Racing.startRace(). This is to reduce a waste of input.
    ex) If an exception occurs in carNames, an input on carNames would not happen.

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,11 +1,20 @@
 package racingcar;
 
 import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 
 public class Racing {
-    public static void startRace(String carNames, String tryCount) {
+    public static void startRace() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String carNames = Console.readLine();
+
         List<Car> cars = nameCars(carNames);
+        
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String tryCount = Console.readLine();
         
         Play play = new Play(cars);
         play.play(tryCount);
