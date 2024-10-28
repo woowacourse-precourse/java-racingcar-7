72d565093a7998e7d459defe3b098b983613f55b
seaniiio <cnc4750@naver.com>
2024-10-28 12:28:51 +0900

    refactor(MessageFormatter): 매직 넘버 직관적인 이름으로 상수화

--- src/main/java/racingcar/util/MessageFormatter.java
+++ src/main/java/racingcar/util/MessageFormatter.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 public class MessageFormatter {
 
+    private final static String NAME_STATUS_DELIMITER = " : ";
+    private final static String STATUS_BAR = "-";
+
     public static String getWinnersName(List<String> winners) {
         return String.join(", ", winners);
     }
 
     public static String getCarInformation(String name, int status) {
-        return name + " : " + "-".repeat(status);
+        return name + NAME_STATUS_DELIMITER + STATUS_BAR.repeat(status);
     }
 }
