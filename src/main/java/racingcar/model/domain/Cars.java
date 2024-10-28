981b6732f5ac2d49d65be2869c535f8031046657
Si Yeong Park <psy915@swu.ac.kr>
2024-10-28 20:23:39 +0900

    feat(Cars): 자동차의 그룹을 관리 기능

---
+++ src/main/java/racingcar/model/domain/Cars.java
@@ -0,0 +1,69 @@
+package racingcar.model.domain;
+
+import racingcar.model.NumberGenerator;
+import racingcar.model.RandomNumberGenerator;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Cars {
+
+    private final List<Car> cars;
+    private final NumberGenerator numberGenerator;
+
+    private Cars(List<Car> cars, NumberGenerator numberGenerator) {
+        this.cars = cars;
+        this.numberGenerator = numberGenerator;
+    }
+
+    public static Cars from(List<String> names) {
+        List<Car> cars = names.stream()
+                .map(Car::from)
+                .toList();
+
+        return new Cars(cars, new RandomNumberGenerator());
+    }
+
+    public List<String> getNames() {
+        return cars.stream()
+                .map(Car::getName)
+                .toList();
+    }
+
+    public Cars moveForward() {
+        List<Car> movedCars = cars.stream()
+                .map(car -> car.moveForward(numberGenerator.generate()))
+                .toList();
+        return new Cars(movedCars, this.numberGenerator);
+    }
+
+    public List<Integer> getPositions() {
+        return cars.stream()
+                .map(Car::getPosition)
+                .toList();
+    }
+
+    public List<String> findWinners() {
+        int maxPosition = findMaxPosition();
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .toList();
+    }
+
+    private int findMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+
+    public Map<String, Integer> mapNamesToPositions() {
+        return cars.stream()
+                .collect(Collectors.toMap(
+                        Car::getName,
+                        Car::getPosition
+                ));
+    }
+}
