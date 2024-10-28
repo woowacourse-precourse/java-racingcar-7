45ab910564b95815a01354e15ee1e2fd64c9a8f3
Hyunuk <dksgusdnr17@naver.com>
2024-10-28 21:07:46 +0900

    feat(service): implement main game logic

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.service.RacingCarService;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingCarService racingCarService = RacingCarService.getInstance();
+        racingCarService.run();
     }
 }
