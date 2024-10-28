f985da280b7bf810a290e6197664a2b832828c89
Hyoseop Song <crad_on25@naver.com>
2024-10-28 23:51:47 +0900

    feat: 경기 결과 출력 기능 구현

--- src/main/java/racingcar/RaceResultParser.java
+++ src/main/java/racingcar/RaceResultParser.java
@@ -1,23 +1,22 @@
 package racingcar;
 
-import static racingcar.OutputPrefix.DISPLAY_NAME_FORMAT;
-import static racingcar.OutputPrefix.DISPLAY_WINNER_FORMAT;
-
 import java.util.List;
 import racingcar.domain.Car;
 
 public class RaceResultParser {
 
 	public void displayRoundProgress(List<Car> cars) {
-		cars.forEach(this::displayCarPosition);
+		cars.forEach(this::getCarPositionBar);
 	}
 
-	public void displayWinners(List<String> winnerNames) {
-		String joinedWinners = String.join(", ", winnerNames);
-		System.out.printf(DISPLAY_WINNER_FORMAT.getMessage(), joinedWinners);
+	public String getWinnersName(List<Car> winnerCars) {
+		List<String> winnerNames = winnerCars.stream()
+				.map(Car::getName)
+				.toList();
+		return String.join(", ", winnerNames);
 	}
 
-	private void displayCarPosition(Car car) {
-		System.out.printf((DISPLAY_NAME_FORMAT) + "%n", car.getName(), "-".repeat(Math.max(0, car.getPosition())));
+	private void getCarPositionBar(Car car) {
+		System.out.println("-".repeat(Math.max(0, car.getPosition())));
 	}
 }
