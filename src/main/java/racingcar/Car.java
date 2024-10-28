7a814e6ffdaf7e478cc249f14a924a4d543fdd59
Hyejeong <thswjd0609@gmail.com>
2024-10-28 14:27:02 +0900

    feat: Car 클래스 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,2 +1,19 @@
-package racingcar;public class Car {
+package racingcar;
+
+public class Car {
+    private String name;
+    private int score;
+
+    public Car(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
