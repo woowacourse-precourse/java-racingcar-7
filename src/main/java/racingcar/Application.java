ef9e51f87aa11acac84de1f2dd7c164c65f349ca
전민정 <minj_1203@naver.com>
2024-10-28 19:59:04 +0900

    refact: 자료구조변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,7 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.*;
 
 public class Application {
-    public static List<String> cars;
+    public static Map<String, String> cars = new LinkedHashMap<>();
+    public static int N;
     public static void main(String[] args) {
         try {
             input();
@@ -16,17 +17,19 @@ public class Application {
 
     public static void input(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
-        cars = Arrays.asList(input.split(","));
+        String[] input = Console.readLine().split(",");
+        for(String car: input){
+            cars.put(car, "");
+        }
 
         if(cars.isEmpty() || !isValid()) throw new IllegalArgumentException();
 
         System.out.println("시도할 횟수는 몇 회인가요?");
-        int N = Integer.parseInt(Console.readLine());
+        N = Integer.parseInt(Console.readLine());
     }
 
     public static boolean isValid(){
-        for (String car : cars) {
+        for (String car : cars.keySet()) {
             if (car.length() > 5) return false;
         }
 
