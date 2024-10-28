7dca64106f1cecb043636d2f4b351de3b4cdbade
lee-haeseung <lhser123@cau.ac.kr>
2024-10-28 18:52:28 +0900

    feat: 자동차 생성시 이름 부여 가능하게 구현
    자동차 생성자 및 기본 구현 수행
    이름 부여 가능하도록 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,31 @@
 package racingcar;
 
 public class Car {
+    private Integer location;
+    private String name;
+
+    public Car(String name) {
+        location = 0;
+        this.name = name;
+    }
+
+    public Car() {
+        location = 0;
+    }
+
+    public void move() {
+        // 이후 구현
+    }
+
+    public void randomMove() {
+        // 이후 구현
+    }
+
+    public Integer getLocation() {
+        return location;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
