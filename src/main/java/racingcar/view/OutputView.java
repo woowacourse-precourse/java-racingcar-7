da54903459dc00cc62568f32daea17dddf99aa7d
ryan-dia <cjfdnjs158@naver.com>
2024-10-28 21:33:48 +0900

    refactor: string format으로 가독성 확보

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,7 +6,7 @@ import racingcar.model.CarSnapshot;
 public class OutputView {
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ATTEMPT_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
-    private static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자";
 
     private OutputView() {
     }
@@ -42,6 +42,6 @@ public class OutputView {
     }
 
     private static void printWinners(List<String> winners) {
-        printMessage(FINAL_WINNER_MESSAGE + String.join(", ", winners));
+        printMessage(String.format("%s : %s", FINAL_WINNER_MESSAGE, String.join(", ", winners)));
     }
 }
