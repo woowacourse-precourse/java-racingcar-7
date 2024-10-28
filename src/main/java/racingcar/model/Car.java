9ed302a564d1800d1a2a897e75766f2f18151c66
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-10-28 17:47:35 +0900

    feat: 자동차 이름 구분 결과가 5자 초과 시 예외 발생시키는 기능 구현
    - validateName(String name) 메소드로 name의 길이 검사
    - name의 길이가 5를 초과할 시 IllegalArgumentException 발생

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -10,8 +10,15 @@ public class Car {
     private int position;
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
         this.position = 0;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void move() {
