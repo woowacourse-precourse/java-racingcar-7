21052d522fa086971e8ba5abd0e37585fd7e844d
jungsu <vwx1@naver.com>
2024-10-28 12:22:56 +0900

    feat: trim whitespace from car names
    - Added trimming of leading and trailing whitespace for each car name in the input

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,14 +1,17 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 import racingcar.view.enums.Constants;
 
 public class InputView {
 
   public static String[] inputCarNames() {
     System.out.println(Constants.START_MESSAGE.getMessage());
-    String input =Console.readLine();
-    return input.split(",");
+    String input = Console.readLine();
+    return Arrays.stream(input.split(","))
+        .map(String::trim) // 이름 사이 공백 제거
+        .toArray(String[]::new);
   }
 
   public static int inputTrialCount() {
