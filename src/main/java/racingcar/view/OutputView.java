9ef07455d9c6bba0397ca827363b58f3ff70c241
jinhyeon <tiemo0708@naver.com>
2024-10-28 14:32:02 +0900

    refactor: 변수이름과 메서드 이름을 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,17 +3,17 @@ package racingcar.view;
 import java.util.List;
 
 public class OutputView {
-    private static final String WELCOME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String CAR_NAMES_PROMPT = "시도할 횟수는 몇 회인가요?";
+    private static final String ENTER_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ENTER_TRIALS_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private static final String ROUND_RESULTS_PREFIX = "실행 결과";
     private static final String WINNERS_MESSAGE = "최종 우승자 : ";
 
-    public static void printWelcomeMessage() {
-        System.out.println(WELCOME_MESSAGE);
+    public static void printEnterCarNamesMessage() {
+        System.out.println(ENTER_CAR_NAMES_MESSAGE);
     }
 
-    public static void printCarNamesPrompt() {
-        System.out.println(CAR_NAMES_PROMPT);
+    public static void printEnterTrialsCountMessage() {
+        System.out.println(ENTER_TRIALS_COUNT_MESSAGE);
     }
 
     public static void printInitialStatus() {
