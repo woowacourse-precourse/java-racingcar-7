ca808498a604dc8e78097d665a2f8e27081185db
jijongkwon <help3451@naver.com>
2024-10-28 21:47:36 +0900

    test(ui) : 우승자 출력 콘솔 화면 테스트

--- src/test/java/racingcar/presentation/ui/OutputViewTest.java
+++ src/test/java/racingcar/presentation/ui/OutputViewTest.java
@@ -6,25 +6,34 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.application.service.winner.WinnerService;
 import racingcar.domain.car.RacingCar;
 import racingcar.domain.history.RacingHistory;
+import racingcar.domain.winner.Winner;
+import racingcar.domain.winner.repository.WinnerRepository;
 import racingcar.infrastructure.persistence.InMemoryRacingCarRepository;
 import racingcar.infrastructure.persistence.InMemoryRacingHistoryRepository;
+import racingcar.infrastructure.persistence.InMemoryWinnerRepository;
 import racingcar.presentation.enums.UiMessage;
 
 class OutputViewTest {
     private OutputView outputView;
     private ByteArrayOutputStream outputStream;
+    private WinnerService winnerService;
     private InMemoryRacingCarRepository racingCarRepository;
     private InMemoryRacingHistoryRepository historyRepository;
+    private WinnerRepository winnerRepository;
 
     @BeforeEach
     void before(){
         outputView = new OutputView();
         outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
+        winnerService = new WinnerService();
+
         racingCarRepository = InMemoryRacingCarRepository.getInstance();
         historyRepository = InMemoryRacingHistoryRepository.getInstance();
+        winnerRepository = InMemoryWinnerRepository.getInstance();
         racingCarRepository.clear();
     }
 
@@ -41,5 +50,18 @@ class OutputViewTest {
         assertTrue(result.contains("pobi : -"));
         assertTrue(result.contains("woni : --"));
         assertTrue(result.contains("jun : ---"));
+    }
+
+    @Test
+    void 우승자_결과_화면_출력_테스트(){
+        winnerRepository.saveWinner(new Winner("pobi"));
+        winnerRepository.saveWinner(new Winner("woni"));
+
+        winnerService.saveWinners();
+        outputView.outputWinnerUi();
+        String result = outputStream.toString();
+
+        assertTrue(result.contains(UiMessage.UI_OUTPUT_WINNER_RESULT_MESSAGE.getMessage()));
+        assertTrue(result.contains("pobi"));
     }
 }
