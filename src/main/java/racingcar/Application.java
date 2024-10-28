44b8076d06325a085198eae9faf4bcaff860ab67
SUNSETBREEZE <ghubkit2060@gmail.com>
2024-10-28 11:13:11 +0900

    FEAT : Player 클래스 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,40 @@
 package racingcar;
 
+import java.util.ArrayList;
+
+class Player {
+	String name;
+	int racingProcessCount;
+
+	Player(String name) {
+		this.name = name;
+		this.racingProcessCount = 0;
+	}
+
+	public void canMoveForward() {
+		if (camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 9) >= 4) {
+			this.racingProcessCount++;
+		}
+	}
+
+	public String exchangeRacingProcessCountToString() {
+		String racingString = "";
+		for (int i = 0; i < this.racingProcessCount; i++) {
+			racingString = racingString + "-";
+		}
+
+		return racingString;
+	}
+
+	public void printPlayerPresentState() {
+		System.out.println(this.name + " : " + this.exchangeRacingProcessCountToString());
+	}
+}
+
+
+	
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		
+	}
 }
