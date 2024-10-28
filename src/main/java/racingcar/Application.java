18197fd6aef0a101e09294c8b7f2a7a95dc24399
jungsu <vwx1@naver.com>
2024-10-28 14:06:45 +0900

    refactor: update main application
    - Refactor main application to create and inject instances of RacingGameService, InputView, and ResultView into RacingGameController.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,19 @@
 package racingcar;
 
 import racingcar.controller.RacingGameController;
+import racingcar.service.RacingGameService;
+import racingcar.view.InputView;
+import racingcar.view.ResultView;
 
 public class Application {
-    public static void main(String[] args) {
-        RacingGameController controller = new RacingGameController();
-        controller.run();
-    }
+
+  public static void main(String[] args) {
+    RacingGameService racingGameService = new RacingGameService();
+    InputView inputView = new InputView();
+    ResultView resultView = new ResultView();
+
+    RacingGameController controller = new RacingGameController(
+        racingGameService, inputView, resultView);
+    controller.run();
+  }
 }
