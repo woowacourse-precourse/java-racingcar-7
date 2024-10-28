f1bf2eddf815f802a40cc53b20b16fac66b12d93
bk <naturalbb360@gmail.com>
2024-10-28 13:16:19 +0900

    refactor(RegistrationManager): 클래스명 수정, 인스턴스변수에 접근제어자 추가, 불필요한 메소드 삭제
    - 등록된 선수들의 수를 나타내는 getRegisteredPlayersCount 메소드 삭제

--- src/main/java/racingcar/RegistrationManager.java
+++ src/main/java/racingcar/RegistrationManager.java
@@ -2,23 +2,25 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
 
-public class registraionManager {
+public class RegistrationManager {
+
     private static final int MAX_CAR_COUNT = 10;
     private static final int MIN_CAR_COUNT = 2;
     private static final int MAX_CAR_NAME_LENGTH = 5;
 
-    Car[] cars;
+    private Car[] cars;
 
-    public registraionManager(ArrayList<String> carNames){
+    public RegistrationManager(List<String> carNames) {
         cars = new Car[carNames.size()];
         enroll(carNames);
     }
 
-    public void enroll(ArrayList<String> name){
-        for(int index=0; index < cars.length; index++){
+    public void enroll(List<String> name) {
+        for (int index = 0; index < cars.length; index++) {
             cars[index] = new Car(name.get(index));
         }
     }
@@ -30,44 +32,39 @@ public class registraionManager {
         return carName.toLowerCase();
     }
 
-    private static void isValidlengthName(String carName){
-        if (carName.length() > MAX_CAR_NAME_LENGTH){
+    private static void isValidlengthName(String carName) {
+        if (carName.length() > MAX_CAR_NAME_LENGTH) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         }
     }
 
-    public void checkValidCarNames(){
-        for(int i=0; i<cars.length; i++){
+    public void checkValidCarNames() {
+        for (int i = 0; i < cars.length; i++) {
             String curCarName = cars[i].name();
             cars[i].rename(getLowerCaseName(curCarName));
             isValidlengthName(curCarName);
         }
     }
 
-    public void checkDuplicatePlayer(){
-        ArrayList<String> nameList= new ArrayList<>();
-        for (int i=0; i<cars.length; i++){
+    public void checkDuplicatePlayer() {
+        ArrayList<String> nameList = new ArrayList<>();
+        for (int i = 0; i < cars.length; i++) {
             nameList.add(cars[i].name());
         }
         Set<String> nameSet = new HashSet<>(nameList);
-        if(nameList.size() != nameSet.size()){
+        if (nameList.size() != nameSet.size()) {
             throw new IllegalArgumentException("중복된 자동차 이름은 사용할 수 없습니다.(대문자와 소문자는 동일하게 인식됩니다.)");
-
         }
     }
 
-    public void checkValidCarCount(){
-        if ((cars.length > MAX_CAR_COUNT) || (cars.length < MIN_CAR_COUNT)){
+    public void checkValidCarCount() {
+        if ((cars.length > MAX_CAR_COUNT) || (cars.length < MIN_CAR_COUNT)) {
             throw new IllegalArgumentException("자동차 대수는 2대 이상 10대 이하여야 합니다.");
         }
     }
 
-    public Car[] getRegisteredPlayers(){
+    public Car[] getRegisteredPlayers() {
         return cars;
-    }
-
-    public int getRegisteredPlayersCount(){
-        return cars.length;
     }
 
 }
