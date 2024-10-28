6d5949b013cfb793ba0bc561d97450d87327cc6b
jieun <ju3391@naver.com>
2024-10-28 20:31:37 +0900

    refactor(Car): 불변 객체로 수정
    - 이동할 때마다 새로운 객체 생성

--- src/main/java/racingcar/car/model/Car.java
+++ src/main/java/racingcar/car/model/Car.java
@@ -3,18 +3,26 @@ package racingcar.car.model;
 // 자동차 객체를 나타내는 클래스
 public class Car {
     private final String name;
-    private int position = 0;  // 초기 위치는 0
+    private final int position; // 초기 위치는 0
 
     public Car(String name) {
         this.name = name;
+        this.position = 0;
+    }
+
+    private Car(String name, int position) {  // private 생성자
+        this.name = name;
+        this.position = position;
     }
 
     // 자동차를 전진시키는 메서드
-    public void move(int randomNumber) {
+    public Car move(int randomNumber) {
         if (randomNumber >= 4) {
-            position++;  // 랜덤 숫자가 4 이상일 때 전진
+            return new Car(this.name, this.position + 1);
         }
+        return this;  // 이동이 없을 경우 현재 객체 반환
     }
+
 
     public String getName() {
         return name;
@@ -24,3 +32,5 @@ public class Car {
         return position;
     }
 }
+
+
