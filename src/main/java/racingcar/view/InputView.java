dbf82e98fc458f3039177f9b7d61f62074493c7f
BusanGukbap <dong2857210@gmail.com>
2024-10-28 16:08:01 +0900

    feat($util) : add inputCarNames in inputView

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,14 +1,15 @@
 package racingcar.view;
 
-import camp.nextstep.edu.missionutils.console;
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_NUMBER_OF_ATTEMPTS_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
-    public static String inputCarNaems() {
-
+    public static String inputCarNames() {
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
+        return Console.readLine();
     }
 
     public static int inputNumberOfAttempts() {
