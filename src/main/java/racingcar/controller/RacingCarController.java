3da04c1ee8e9422b581a49136f669de3fff2a59d
jinhyeon <tiemo0708@naver.com>
2024-10-28 13:13:24 +0900

    feat: 컨트롤러 클래스 생성

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,6 +1,25 @@
+// RacingCarController.java
 package racingcar.controller;
 
+import racingcar.service.RacingCarService;
+import racingcar.utils.InputValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class RacingCarController {
+    private final RacingCarService racingCarService;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final InputValidator carNameValidator;
+    private final InputValidator numberOfTrialsValidator;
+
+    public RacingCarController(RacingCarService racingCarService, InputView inputView, OutputView outputView, InputValidator carNameValidator, InputValidator numberOfTrialsValidator) {
+        this.racingCarService = racingCarService;
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.carNameValidator = carNameValidator;
+        this.numberOfTrialsValidator = numberOfTrialsValidator;
+    }
 
     public void run() {
     }
