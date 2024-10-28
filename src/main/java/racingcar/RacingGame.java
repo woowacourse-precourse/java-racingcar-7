cd9f4661d1bfb63b966eb0f7bfaf82050e3777ee
kmc198989 <kmc198989@naver.com>
2024-10-28 16:31:50 +0900

    Feat: 자동차 경주 진행 기능 구현
    - 사용자가 입력한 횟수만큼 경주를 반복하며 각 자동차의 전진 여부 결정
    - 각 회차 종료 후 결과 출력

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -3,4 +3,19 @@ package racingcar;
 import java.util.List;
 
 public record RacingGame(List<Car> cars, int rounds) {
+    public void startRace() {
+        for (int i = 0; i < rounds; i++) {
+            for (Car car : cars) {
+                car.move();
+            }
+            printRoundResults();
+        }
+    }
+
+    private void printRoundResults() {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
 }
