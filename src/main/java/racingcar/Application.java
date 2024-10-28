9134886c7e0e2ee0e9b185ea3087f211e357f75f
allavo <hjchokr2018@gmail.com>
2024-10-28 20:54:09 +0900

    feat(main): 애플리케이션 시작점 재설정
    애플리케이션의 시작점 역할을 하는 Application 클래스를 생성했습니다.
    - main() 메서드에서 RacingController를 호출하여 경주 게임을 시작하도록 구성했습니다.
    - 사용자가 프로그램을 실행할 때 자동으로 경주 흐름이 시작되도록 설정했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,18 +1,10 @@
 package racingcar;
-//
-//public class Application {
-//    public class Application {
-//        public static void main(String[] args) {
-//            SpringApplication.run(racingcar.Application.class, args);
-//        }
-//    }
-//
 
 import racingcar.controller.RacingController;
 
 public class Application {
     public static void main(String[] args) {
-
+        RacingController racingController = new RacingController();
+        racingController.startRace();
     }
 }
-
