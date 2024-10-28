d46696b2e62b80d462405faaa741e79e2ad10fdd
조윤성 <yunsung1206@gmail.com>
2024-10-28 21:56:15 +0900

    test: 자동차 이동 테스트 케이스 추가

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -4,8 +4,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import racingcar.util.RandomGenerator;
 
 public class CarsTest {
+    static Object[][] randomNumberAndExpected() {
+        return new Object[][]{
+                {0, 0},
+                {3, 0},
+                {4, 1},
+                {9, 1}
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource("randomNumberAndExpected")
+    void 랜덤_값이_4_이상이면_전진_미만이면_정지한다(int random, int expected) {
+        RandomGenerator randomGenerator = () -> random;
+        List<Car> carList = new CarList("pobi,woni");
+        Car car = carList.getFirst();
+        Cars cars = new Cars(carList, randomGenerator);
+
+        cars.tryMoveAll();
+
+        assertThat(car.getPosition()).isEqualTo(expected);
+    }
+
     @Test
     void 자동차_경주_최종_우승자가_2명() {
         List<Car> carList = new CarList("pobi,woni,jun");
