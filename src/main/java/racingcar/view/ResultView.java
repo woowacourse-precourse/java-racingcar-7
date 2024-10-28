4c7c85c8f230c9f593f70aaea619ca76711ca2a2
hootzz <wjqwjql3@gmail.com>
2024-10-28 22:22:39 +0900

    feat(view): ResultView 경주 결과 출력 기능 구현

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,4 +1,25 @@
 package racingcar.view;
 
+import racingcar.domain.car.Car;
+import racingcar.domain.car.Cars;
+
+import java.util.List;
+
 public class ResultView {
+
+    public void printStartMessage() {
+        System.out.println("실행 결과");
+    }
+
+    public void printRaceResult(Cars cars) {
+        List<Car> carList = cars.getCars();
+        for (Car car : carList) {
+            System.out.println(car.toString());
+        }
+        System.out.println();
+    }
+
+    public void printWinners(String winners) {
+        System.out.println("최종 우승자: " + winners);
+    }
 }
