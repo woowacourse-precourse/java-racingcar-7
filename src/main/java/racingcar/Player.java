daf1a01333889541d582823d14b7f54ec85cfb99
kim-se-hee <saree98@naver.com>
2024-10-28 16:58:00 +0900

    feat(Player): 경주 참가자를 나타내는  클래스 구현
    Player에 참가자 관련 로직 추가
    - 한 칸 이동
    - 우승자인지 판단
    - 이름과 이동 거리를 위한 getter

--- src/main/java/racingcar/Player.java
+++ src/main/java/racingcar/Player.java
@@ -1,2 +1,28 @@
-package racingcar;public class Player {
+package racingcar;
+
+public class Player {
+    private final String name;
+    private int distance;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public void move() {
+        this.distance++;
+    }
+
+    public boolean isWinner(int maxDistance) {
+        return maxDistance == this.distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+
 }
