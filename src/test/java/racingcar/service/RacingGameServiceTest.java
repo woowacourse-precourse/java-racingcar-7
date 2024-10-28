183771fee6485a72bde3ba92c0413ce7ed94a33d
Sinorang <sinholang@naver.com>
2024-10-28 13:55:40 +0900

    test: 라운드별 자동차 위치 업데이트 테스트코드 구현
    - raceOneRound() 메서드를 호출하는 방법은 난수 제어가 불가능하기 때문에
      Car 클래스의 decideToMove() 메서드를 호출하여 테스트 코드 작성
    - 모든 자동차가 전진, 멈춤일 때의 경계 조건 테스트 코드도 함께 구현

--- src/test/java/racingcar/service/RacingGameServiceTest.java
+++ src/test/java/racingcar/service/RacingGameServiceTest.java
@@ -1,2 +1,50 @@
-package racingcar.service;public class RacingGameServiceTest {
+package racingcar.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingGameServiceTest {
+    private RacingGameService racingGameService;
+
+    @BeforeEach
+    void setUp() {
+        racingGameService = new RacingGameService();
+    }
+
+    @Test
+    void 라운드별_자동차_위치_업데이트_테스트() {
+        String carNamesInput = "pobi,woni,jun";
+        List<Car> cars = racingGameService.createCars(carNamesInput);
+
+        cars.get(0).decideToMove(4);
+        cars.get(1).decideToMove(5);
+        cars.get(2).decideToMove(3);
+
+        assertThat(cars.get(0).getPosition()).isEqualTo(1);
+        assertThat(cars.get(1).getPosition()).isEqualTo(1);
+        assertThat(cars.get(2).getPosition()).isZero();
+    }
+
+    @Test
+    void 모든_자동차_정지_테스트() {
+        String carNamesInput = "pobi,woni,jun";
+        List<Car> cars = racingGameService.createCars(carNamesInput);
+
+        cars.forEach(car -> car.decideToMove(3));
+        cars.forEach(car -> assertThat(car.getPosition()).isZero());
+    }
+
+    @Test
+    void 모든_자동차_전진_테스트() {
+        String carNamesInput = "pobi,woni,jun";
+        List<Car> cars = racingGameService.createCars(carNamesInput);
+
+        cars.forEach(car -> car.decideToMove(4));
+        cars.forEach(car -> assertThat(car.getPosition()).isEqualTo(1));
+    }
 }
