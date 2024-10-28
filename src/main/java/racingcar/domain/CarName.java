c450d5ea2652c43bdd109e0188c5eda3c524b001
gyuho <kyuhokim12@gmail.com>
2024-10-28 15:23:41 +0900

    fix(CarName): 차량의 이름이 공백이거나 비어있을 시 에러를 발생시키도록 수정
    - 차량의 이름이 공백일 수 없다는 예외 사항 처리
    - 예외 사항 처리하는 메소드 분리

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -3,17 +3,29 @@ package racingcar.domain;
 public class CarName {
     private final int MAX_LENGTH = 5;
     private final String NAME_LENGTH_OVER_ERROR_MESSAGE = "자동차 이름의 길이는 5 이하여야만 합니다";
+    private final String EMPTY_NAME_ERROR_MESSAGE = "자동차의 이름은 비어있을 수 없습니다."
 
     private String name;
 
     public CarName(String name){
-        if(name.length() > MAX_LENGTH){
-            throw new IllegalArgumentException(NAME_LENGTH_OVER_ERROR_MESSAGE);
-        }
+        checkCarNameisBlank(name);
+        checkCarNamelength(name);
         this.name = name;
     }
 
     public String getName(){
         return this.name;
+    }
+
+    public void checkCarNamelength(String name){
+        if(name.length() > MAX_LENGTH){
+            throw new IllegalArgumentException(NAME_LENGTH_OVER_ERROR_MESSAGE);
+        }
+    }
+
+    public void checkCarNameisBlank(String name){
+        if(name.isBlank()){
+            throw new IllegalArgumentException(EMPTY_NAME_ERROR_MESSAGE);
+        }
     }
 }
