2f5ef5e79bd15ddee10b8adab0e53d3a393c2a70
Cmk1031 <maple1031@naver.com>
2024-10-28 16:41:35 +0900

    feat: 자동차 정보 클래스

---
+++ src/main/java/run/Run.java
@@ -0,0 +1,11 @@
+package run;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.Car;
+
+public class Run {
+    public void start() {
+        List<Car> racingCar = new ArrayList<>();
+    }
+}
