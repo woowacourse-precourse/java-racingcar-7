11046fe3a1da93b4ee4d80f6a0ef85b96b6e336d
zjhj0814 <zjhj0814@gmail.com>
2024-10-28 12:47:05 +0900

    test(InputValidatorTest): InputViewValidator 테스트 작성
    에러가 발생하지 않는 경우는 너무 자명하여 에러가 발생하는 경우에만 테스트를 작성한다

---
+++ src/test/java/racingcar/validator/InputViewValidatorTest.java
@@ -0,0 +1,42 @@
+package racingcar.validator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static racingcar.validator.ErrorMessage.EMPTY_OR_BLANK_INPUT_ERROR;
+import static racingcar.validator.ErrorMessage.NON_NUMERIC_INPUT_ERROR;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class InputViewValidatorTest {
+    private InputViewValidator inputViewValidator;
+
+    @BeforeEach
+    void setUp() {
+        inputViewValidator = new InputViewValidator();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 문자열이_널이거나_공백이면_에러가_발생한다(String userInput) {
+        IllegalArgumentException e = assertThrowsExactly(IllegalArgumentException.class,
+                () -> {
+                    inputViewValidator.validateNotEmptyAndBlank(userInput);
+                }
+        );
+        assertEquals(EMPTY_OR_BLANK_INPUT_ERROR.getMessage(), e.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "3D"})
+    void 문자열이_숫자가_아니면_에러가_발생한다(String userInput) {
+        IllegalArgumentException e = assertThrowsExactly(IllegalArgumentException.class,
+                () -> {
+                    inputViewValidator.validateNumeric(userInput);
+                }
+        );
+        assertEquals(NON_NUMERIC_INPUT_ERROR.getMessage(), e.getMessage());
+    }
+}
