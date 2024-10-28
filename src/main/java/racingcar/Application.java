81565be4491027b5ecd9d56d0b75d305317afdb5
yejin923 <yejin923@naver.com>
2024-10-28 17:45:10 +0900

    FEAT: 출력 메소드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,9 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static java.lang.Integer.parseInt;
 
 public class Application {
@@ -25,5 +28,35 @@ public class Application {
     public static boolean Movement(){ //true:전진 false:정지
        int randomNum = Randoms.pickNumberInRange(0, 9);
        return randomNum >= 4;
+    }
+
+    public static void display(String[] arr){ // 실행 결과 출력
+
+        System.out.println("실행 결과");
+
+        String[] move = new String[arr.length];
+
+        for(int i = 0; i < arr.length; i++){
+            if(Movement())
+                move[i] += "-";
+            else
+                move[i] += "";
+            System.out.println(arr[i] + " : " + move[i]);
+        }
+
+        List<String> longestStrings = new ArrayList<>();
+        int maxLength = 0;
+
+        for(String str: arr){
+            if(str.length() > maxLength){
+                maxLength = str.length();
+                longestStrings.clear();
+                longestStrings.add(str);
+            }
+            else if(str.length() == maxLength){
+                longestStrings.add(str);
+            }
+        }
+        System.out.println("최종 우승자 : " + longestStrings);
     }
 }
