60f621876f26474598becddcedb1fff8632bc630
Junad-Park <qkrwnsgk1624@icloud.com>
2024-10-28 14:27:06 +0900

    test(InputValidatorTest): 시도할 횟수로 null, 빈 문자열, 공백으로만 이루어진 문자열을 입력했을 경우 유효성 검증 테스트 추가

--- src/test/java/racingcar/validator/InputValidatorTest.java
+++ src/test/java/racingcar/validator/InputValidatorTest.java
@@ -17,4 +17,13 @@ class InputValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.BLANK_INPUT_NOT_ALLOWED.getMessage());
     }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" "})
+    void 시도할_횟수는_비거나_공백으로_이루어진_문자열일_수_없다(String input) {
+        assertThatThrownBy(() -> InputValidator.validateTryCount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.BLANK_INPUT_NOT_ALLOWED.getMessage());
+    }
 }
