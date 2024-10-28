6f6241d8016a844450ca2d58f85bd66ba3051bc8
jihyun <jihyun3066@naver.com>
2024-10-28 19:04:45 +0900

    [ feat ]  : 자동차 경주에 사용할 상수 정리

---
+++ src/main/java/racingcar/model/Number.java
@@ -0,0 +1,11 @@
+package racingcar.model;
+
+public class Number {
+
+    public static int MAX_NAME_LENGTH = 5;
+    public static int MIN_NUMBER = 0;
+    public static int MAX_NUMBER = 9;
+    public static int RESTRICT_NUMBER = 4;
+    public static int LIMIT_NUMBER;
+
+}
