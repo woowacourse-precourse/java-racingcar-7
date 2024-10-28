865faa498fa1dfd32c20406668ba1ad29e6e3452
jae woo Jung <awfjol2008@gmail.com>
2024-10-28 21:59:28 +0900

    feat: 모든 자동차들의 전진 상태가 이름과 이동 거리를 통해 반환되는지 검사하는 테스트 구현

--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -49,4 +49,42 @@ class RaceTest {
 			.toList())
 			.isEqualTo(List.of(1, 1, 1));
 	}
+
+	@Test
+	@DisplayName("모든 자동차의 랜덤 숫자가 기준값 미만일 때, 모든 자동차가 정지한다.")
+	void stopEachCar() {
+		// given
+		int randomNumber = RaceConfig.MOVE_CONDITION_NUMBER.getNumber() - 1;
+
+		// when
+		race.moveEachCars(randomNumber);
+
+		// then
+		assertThat(cars.stream()
+			.map(Car::getDistance)
+			.toList())
+			.isEqualTo(List.of(0, 0, 0));
+	}
+
+	@Test
+	@DisplayName("모든 자동차들의 전진 상태를 이름과 이동 거리를 통해 반환한다.")
+	void findRaceStatus() {
+		// given
+		int randomNumber = RaceConfig.MOVE_CONDITION_NUMBER.getNumber();
+
+		// when
+		race.moveEachCars(randomNumber);
+		List<CarMovementResponse> carMovementResponses = race.findRaceStatus();
+
+		// then
+		assertThat(carMovementResponses.stream()
+			.map(CarMovementResponse::name)
+			.toList())
+			.containsExactly("pobi", "woni", "jun");
+
+		assertThat(carMovementResponses.stream()
+			.map(CarMovementResponse::distance)
+			.toList())
+			.containsExactly(1, 1, 1);
+	}
 }
