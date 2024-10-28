922426f5adf005bae6531c6a1acc9d769dabcd00
seha9631 <gktkddms9631@gmail.com>
2024-10-28 21:44:12 +0900

    feat(Validater): 입력 검증 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -23,17 +23,15 @@ public class Application {
     public static String[] inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] carNames = readLine().split(",");
-        for (String carName : carNames){
-            if (carName.length() > 5){
-                throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
-            }
-        }
+        Validater.validateCarNames(carNames);
         return carNames;
     }
 
     public static int inputRaceCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        int raceCount = Integer.parseInt(readLine());
+        String userInputCount = readLine();
+        Validater.validateRaceCount(userInputCount);
+        int raceCount = Integer.parseInt(userInputCount);
         return raceCount;
     }
 }
---
+++ src/main/java/racingcar/Validater.java
@@ -0,0 +1,42 @@
+package racingcar;
+
+public class Validater {
+    public static void validateCarNames(String[] carNames){
+        validateCarsCount(carNames);
+        validateCarNameLength(carNames);
+        validateDuplicateCarName(carNames);
+    }
+
+    public static void validateRaceCount(String userInputCount){
+        char[] tmp = userInputCount.toCharArray();
+        for (char c : tmp) {
+            if (!(c >= '0' && c <= '9')){
+                throw new IllegalArgumentException("[ERROR] 실행 횟수는 숫자여야 합니다");
+            }
+        }
+    }
+
+    private static void validateCarNameLength(String[] carNames) {
+        for (String carName : carNames){
+            if (carName.length() > 5){
+                throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
+    }
+
+    private static void validateDuplicateCarName(String[] carNames) {
+        for (String carName : carNames){
+            if (carName.length() > 5){
+                throw new IllegalArgumentException("[ERROR] 중복된 이름은 입력 불가능합니다.");
+            }
+        }
+    }
+
+    private static void validateCarsCount(String[] carNames) {
+        if (carNames.length <= 1) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름을 하나 이상 입력해야 합니다.");
+        }
+    }
+
+
+}
