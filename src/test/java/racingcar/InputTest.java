c5b0f1863a69ec501259b5f3a2c3dfb5ed318726
yooo <ghddbtl@gmail.com>
2024-10-28 16:20:57 +0900

    test: add user input handling tests
    -콤마만 들어갔을 경우 콤마를 기준으로 나눠 자동차를 생성하도록 했는데 잘 작동 하는지 확인함
    
    -이름이 하나일 경우 임의로 car(1)를 생성해서 게임을 돌리는지 확인함

---
+++ src/test/java/racingcar/InputTest.java
@@ -0,0 +1,42 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+
+    @Test
+    void inputEmptyAndCommaTest() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run(",", "1");
+                    assertThat(output()).contains("car(1) : -", "car(2) : ", "최종 우승자 : car(1)");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void inputSingleNameTest() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi", "1");
+                    assertThat(output()).contains("pobi : -", "car : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
