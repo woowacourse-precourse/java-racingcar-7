97802bceb2697ce28ce5543f067d280dedff01de
hmlssghm <kwonse326@gmail.com>
2024-10-28 17:59:13 +0900

    refactor(CarTest): 코드 중복 제거

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -6,15 +6,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
 
-
     @Test
     void addMove_test() {
         Car car = new Car("TestCar");
 
         car.addMove();
-        car.addMove();
-        car.addMove();
 
-        assertEquals(3, car.getMove());
+        assertEquals(1, car.getMove());
     }
 }
