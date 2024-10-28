23606aeb5de0602ddc682939a8950538d9a5c6bc
Chan531 <chan657312@gmail.com>
2024-10-28 17:07:23 +0900

    refactor(NumberValidator): 시도할 횟수 관련 에러 메세지 추가

--- src/main/java/racingcar/model/input/NumberValidator.java
+++ src/main/java/racingcar/model/input/NumberValidator.java
@@ -1,12 +1,14 @@
 package racingcar.model.input;
 
+import static racingcar.model.message.ErrorMessage.INVALID_TRIAL_COUNT;
+
 public class NumberValidator {
 
 	public static int validateTrialCountIsDigit(String count) {
 		try {
 			return Integer.parseInt(count);
 		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException(INVALID_TRIAL_COUNT.getMessage());
 		}
 	}
 }
