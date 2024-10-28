77f35517d93d45c19d2912006d517c742f122680
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-28 14:32:35 +0900

    test(RaceTest): Race 실행 테스트

--- src/test/java/racingcar/model/RaceTest.java
+++ src/test/java/racingcar/model/RaceTest.java
@@ -1,4 +1,19 @@
 package racingcar.model;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 public class RaceTest {
+
+    @Test
+    void 레이스_우승자_선정() {
+        // given
+        Race race = new Race("pobi,woni,jun");
+
+        // when
+
+        // then
+        assertThat(race.runRace(3)).isNotEmpty();
+    }
 }
