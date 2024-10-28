d75e70fdee71d1298211568f759fee3b8a3ad8e3
woohyuk <7dngur7@naver.com>
2024-10-28 17:38:58 +0900

    refactor(Application): 입력 및 출력 로직을 분리, 예외 처리 개선
    - InputView와 OutputView를 사용하여 입력과 출력을 분리
    - 예외 처리를 추가하여 프로그램의 안정성을 향상

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,36 +1,20 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] carNames = Console.readLine().split(",");
+        List<String> carNames = InputView.getCarNames();
         validateCarNames(carNames);
 
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int attempts = Integer.parseInt(Console.readLine());
-        validateAttempts(attempts);
+        int attempts = InputView.getAttempts();
 
         RacingGame game = new RacingGame(carNames, attempts);
         game.play();
     }
 
-    // 자동차 이름 유효성 검사 메서드
-    private static void validateCarNames(String[] carNames) {
-        for (String name : carNames) {
-            if (name.length() > 5) {
-                System.out.println("chk");
-                throw new IllegalArgumentException("자동차 이름은 최대 5자 이하만 가능합니다.");
-            }
-        }
-    }
-
-    // 시도 횟수 유효성 검사 메서드
-    private static void validateAttempts(int attempts) {
-        if (attempts <= 0) {
-            throw new IllegalArgumentException("시도 횟수는 1회 이상이어야 합니다.");
-        }
+    private static void validateCarNames(List<String> carNames) {
+        carNames.forEach(Car::validateName);
     }
 }
