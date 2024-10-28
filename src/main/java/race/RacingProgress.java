72e6a281287d197947581ca4c73c9a499feaefdd
belight2 <chlwnsgur090@gmail.com>
2024-10-28 17:48:20 +0900

    feat: 이동 횟수만큼 자동차들이 이동하게 구현
    - raceStart() : roundCount만큼 반복하면서 레이싱을 진행
    
    - moveCars() : 자동차들이 이동을 함.
    
    - printCars() : 자동차들을 출력 함.

--- src/main/java/race/RacingProgress.java
+++ src/main/java/race/RacingProgress.java
@@ -11,4 +11,26 @@ public class RacingProgress {
         this.carList = carList;
         this.roundCount = roundCounter;
     }
+
+    public void raceStart(){
+        System.out.println("실행 결과");
+        while(roundCount-- > 0){
+            moveCars();
+            printCars();
+        }
+    }
+
+    private void moveCars(){
+        for(Car car : carList){
+            car.move();
+        }
+    }
+
+    private void printCars(){
+        for(Car car : carList){
+            System.out.println(car);
+        }
+        System.out.println();
+    }
+
 }
