9c4a406b93def7d2e2a54ad79893ce22cca7a0a3
sseopsseop <ponde119@outlook.com>
2024-10-28 12:21:00 +0900

    feat: 상수 저장 클래스 생성 값을 입력하기 위해서 안내하는 문구 초기화했음. 차량이름 입력 안내 문구, 횟수 입력 안내 문구 따로 구분했음.

---
+++ src/main/java/util/Constants.java
@@ -0,0 +1,7 @@
+package util;
+
+public final class Constants {
+    private Constants() {}
+    public static final String NOTICE_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String NOTICE_INPUT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+}
