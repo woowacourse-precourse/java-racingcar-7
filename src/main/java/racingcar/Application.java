fa4516dd4ea012c3da5b5a24341b8910a2737b62
youngkwanglim <richard08@naver.com>
2024-10-28 14:57:14 +0900

    feat: 자동차 이름에 공복이 포함되는 경우 예외 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -35,6 +35,9 @@ public class Application {
         }
 
         for (String carName : carNames) {
+            if (carName == null || carName.isBlank()) {
+                throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+            }
             if (carName.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
             }
