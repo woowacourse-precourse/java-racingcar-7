afadc03fd17e0fd83d8a60df00c0a6d5757dab94
dayeondayeon <dayeon2438@gmail.com>
2024-10-28 19:52:21 +0900

    feat : 최종 우승자 출력 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static racingcar.Utils.*;
@@ -26,5 +29,27 @@ public class Application {
                 car.printLocation();
             }
         }
+
+        // 최종 우승자
+        findWinner(vehicles);
+    }
+
+    private static void findWinner(List<Car> vehicles) {
+        int max = vehicles.stream().mapToInt(Car::getLocation).max().orElse(0);
+        List<String> winners = vehicles.stream()
+                                       .filter(car -> car.getLocation() == max)
+                                       .map(Car::getName)
+                                       .toList();
+
+
+        System.out.print("최종 우승자 : ");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < winners.size()-1; i++) {
+            sb.append(winners.get(i));
+            sb.append(", ");
+        }
+        sb.append(winners.get(winners.size()-1));
+        System.out.print(sb);
     }
 }
