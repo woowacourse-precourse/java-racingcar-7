e644608ca1040f363792b7cc048f9671fb8e6339
jmlee <p2achtrii@gmail.com>
2024-10-28 22:55:13 +0900

    refactor: RaceOrNot function

--- src/main/java/racingcar/RandomRaceOrNot.java
+++ src/main/java/racingcar/RandomRaceOrNot.java
@@ -7,25 +7,30 @@ import java.util.List;
 
 
 public class RandomRaceOrNot {
-    public static void main(String[] args) {
-        InputCarName CarNameInput = new InputCarName();
-        String[] CarList = CarNameInput.getCarList();
-        int carNum = CarList.length;
+    private List<StringBuilder> progressList = new ArrayList<>();
+    private String[] carList;
 
-        List<StringBuilder> progressList = new ArrayList<>();
+    public void performRace() {
+        InputCarName CarNameInput = new InputCarName();
+        carList = CarNameInput.getCarList();
+        int carNum = carList.length;
+
         for (int i = 0; i < carNum; i++) {
             progressList.add(new StringBuilder());
         }
+
         List<Integer> raceAmount = new ArrayList<>();
         for (int i = 0; i < carNum; i++) {
             raceAmount.add(i, Randoms.pickNumberInRange(0, 9));
-
-            if (raceAmount.get(i) >= 4){
+            if (raceAmount.get(i) >= 4) {
                 progressList.get(i).append("-");
             }
         }
-
-
-
+    }
+    public List<StringBuilder> getProgressList() {
+        return progressList;
+    }
+    public String[] getCarList() {
+        return carList;
     }
 }
