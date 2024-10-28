9e2023f35a0c3919579923f6ec6fd070c17e9113
soyoon19 <96063941+soyoon19@users.noreply.github.com>
2024-10-28 11:28:09 +0900

    Test : Fix the TestCar
    distance를 조작할 수 있는 생성자 추가

--- src/test/java/racingcar/TestCar.java
+++ src/test/java/racingcar/TestCar.java
@@ -2,12 +2,19 @@ package racingcar;
 
 public class TestCar extends Car {
     private int fixedRandomValue;
-
+    private int testDistance;
     public TestCar(String name, int fixedRandomValue) {
         super(name);
         this.fixedRandomValue = fixedRandomValue;
     }
+    public TestCar(int testDistance, String name) {
+        super(name);
+        this.testDistance = testDistance;
 
+        for (int i = 0; i < testDistance; i++) {
+            addDistance();
+        }
+    }
     @Override
     public int random() {
         return fixedRandomValue;
