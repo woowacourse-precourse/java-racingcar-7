3d2bb8651c0d04187464ccd25b323d0ae4e0695d
신관규 <soundbar91@gmail.com>
2024-10-28 09:59:10 +0900

    refactor: CarNameSplit 예외 처리 추가

--- src/main/java/racingcar/message/exception/CarNameSplitExceptionMessage.java
+++ src/main/java/racingcar/message/exception/CarNameSplitExceptionMessage.java
@@ -1,7 +1,10 @@
 package racingcar.message.exception;
 
-public class SplitStringExceptionMessage {
+public class CarNameSplitExceptionMessage {
     public static final String CAR_NAME_INCLUDE_BLANK = "자동차 이름은 공백 혹은 빈 문자열은 허용되지 않습니다";
+    public static final String CAR_NAMES_LAST_COMMA = "자동차 이름 문자열은 콤마로 끝날 수 없습니다.";
+    public static final String CAR_NAME_COUNT_IS_ONE = "자동차는 한 개만 등록될 수 없습니다.";
+    public static final String CAR_NAME_DUPLICATE = "자동차 이름은 중복될 수 없습니다.";
     public static final String CAR_NAME_LENGTH_OVER = "자동차 이름은 5글자 이하입니다.";
     public static final String CAR_NAME_INCLUDE_SPECIAL_CHARACTERS = "자동차 이름은 한글, 영어, 숫자로만 구성될 수 있습니다";
 }
--- src/main/java/racingcar/util/SplitString.java
+++ src/main/java/racingcar/util/SplitString.java
@@ -1,10 +1,14 @@
 package racingcar.util;
 
-import static racingcar.message.exception.SplitStringExceptionMessage.CAR_NAME_INCLUDE_BLANK;
-import static racingcar.message.exception.SplitStringExceptionMessage.CAR_NAME_INCLUDE_SPECIAL_CHARACTERS;
-import static racingcar.message.exception.SplitStringExceptionMessage.CAR_NAME_LENGTH_OVER;
+import static racingcar.message.exception.CarNameSplitExceptionMessage.CAR_NAMES_LAST_COMMA;
+import static racingcar.message.exception.CarNameSplitExceptionMessage.CAR_NAME_COUNT_IS_ONE;
+import static racingcar.message.exception.CarNameSplitExceptionMessage.CAR_NAME_DUPLICATE;
+import static racingcar.message.exception.CarNameSplitExceptionMessage.CAR_NAME_INCLUDE_BLANK;
+import static racingcar.message.exception.CarNameSplitExceptionMessage.CAR_NAME_INCLUDE_SPECIAL_CHARACTERS;
+import static racingcar.message.exception.CarNameSplitExceptionMessage.CAR_NAME_LENGTH_OVER;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class SplitString {
@@ -15,19 +19,34 @@ public class SplitString {
 
     public static List<String> SplitCarNames(String carNames) {
         List<String> carNameList = Arrays.stream(carNames.split(",")).toList();
-        checkCarNameValid(carNameList);
+        checkCarNameValid(carNameList, carNames);
 
         return carNameList;
     }
 
-    private static void checkCarNameValid(List<String> carNameList) {
+    private static void checkCarNameValid(List<String> carNameList, String carNames) {
+        checkCarNames(carNames);
         checkCarNameList(carNameList);
         carNameList.forEach(SplitString::checkCarName);
+    }
+
+    private static void checkCarNames(String carNames) {
+        if (carNames.charAt(carNames.length() - 1) == ',') {
+            throw new IllegalArgumentException(CAR_NAMES_LAST_COMMA);
+        }
     }
 
     private static void checkCarNameList(List<String> carNameList) {
         if (carNameList.isEmpty()) {
             throw new IllegalArgumentException(CAR_NAME_INCLUDE_BLANK);
+        }
+
+        if (carNameList.size() == 1) {
+            throw new IllegalArgumentException(CAR_NAME_COUNT_IS_ONE);
+        }
+
+        if (new HashSet<>(carNameList).size() != carNameList.size()) {
+            throw new IllegalArgumentException(CAR_NAME_DUPLICATE);
         }
     }
 
