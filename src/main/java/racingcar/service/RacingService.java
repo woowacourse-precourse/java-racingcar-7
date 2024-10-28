00ed5a687b8d7a66411b88960abc4d88d55296ef
josephuk77 <josephuk77@gmail.com>
2024-10-28 12:24:41 +0900

    feat(racingWinner): 최종 우승자를 판별하는 메서드 구현
    경기 종료 후 가장 앞에 있는 자동차들을 찾아 List에 담아 반환하는 메소드를 구현했습니다

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,6 +1,10 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RacingService {
 
@@ -10,5 +14,24 @@ public class RacingService {
 
     public boolean canMove() {
         return Randoms.pickNumberInRange(0, 9) >= 4;
+    }
+
+    public List<String> racingWinner(HashMap<String, Integer> cars) {
+        List<String> winner = new ArrayList<>();
+
+        int maxValue = 0;
+        for (String name : cars.keySet()) {
+            if (cars.get(name) > maxValue) {
+                maxValue = cars.get(name);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : cars.entrySet()) {
+            if (entry.getValue() == maxValue) {
+                winner.add(entry.getKey());
+            }
+        }
+
+        return winner;
     }
 }
