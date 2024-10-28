feefbee92438972cd5a5986a126959794a61c15c
githyj-jang <hoo9268@gmail.com>
2024-10-28 22:06:02 +0900

    feat: 사용자 입력 처리 기능 추가
    - 사용자가 경주할 자동차 이름을 입력할 수 있도록 기능 구현
    - 사용자가 시도할 횟수를 입력할 수 있도록 기능 구현
    - 잘못된 값이 입력될 경우 IllegalArgumentException을 발생시키고 애플리케이션을 종료하도록 예외 처리 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,37 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String[] carNames = getCarNames();;
+        int tryCount = getTryCount();
+
+    }
+    private static String[] getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String[] carNames = Console.readLine().split(",");
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+            }
+        }
+        return carNames;
+    }
+
+    private static int getTryCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String tryCountStr = Console.readLine();
+        int tryCount;
+        try {
+            tryCount = Integer.parseInt(tryCountStr);
+            if (tryCount <= 0) {
+                throw new IllegalArgumentException("시도 횟수는 양의 정수여야 합니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 정수여야 합니다.");
+        }
+        return tryCount;
     }
 }
