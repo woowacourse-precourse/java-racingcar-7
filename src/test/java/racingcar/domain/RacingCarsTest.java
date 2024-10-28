12308ea2852c6cc5f908c2c5f30d481a6f253156
young-in <hardworkingini@gmail.com>
2024-10-28 17:20:21 +0900

    feat : 1대 이하의 자동차 만들었을 때 예외 확인

--- src/test/java/racingcar/domain/RacingCarsTest.java
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RacingCarsTest {
-    @DisplayName("자동차 이름은 중복 사용 예외 확인")
+    @DisplayName("자동차 이름을 중복으로 사용할 수 없습니다.")
     @Test
     void validateDuplicateRacingCarNames() throws Exception{
         //given
@@ -16,6 +16,19 @@ public class RacingCarsTest {
         //when //then
         Assertions.assertThatThrownBy(() -> new RacingCars(cars))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차 이름은 중복으로 사용될 수 없습니다.");
+                .hasMessage("자동차 이름을 중복으로 사용할 수 없습니다.");
+    }
+
+    @DisplayName("자동차 경주를 하기위해 자동차는 최소 2대이상 생성해야 한다.")
+    @Test
+    void validateMinimumCarCount() throws Exception{
+        //given
+        String racingCarName = "pobi";
+        RacingCar car = new RacingCar(racingCarName, 1);
+        List<RacingCar> cars = List.of(car);
+        //when //then
+        Assertions.assertThatThrownBy(() -> new RacingCars(cars))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 경주를 위해서는 최소 2대 이상의 자동차를 만들어야 합니다.");
     }
 }
