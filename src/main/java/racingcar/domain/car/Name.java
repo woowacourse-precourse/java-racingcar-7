bbe9ccd8482b9e262658658f39664ac846d83ba7
hootzz <wjqwjql3@gmail.com>
2024-10-28 20:04:19 +0900

    feat(car): 자동차 이름 원시값 포장

--- src/main/java/racingcar/domain/car/Name.java
+++ src/main/java/racingcar/domain/car/Name.java
@@ -1,4 +1,19 @@
 package racingcar.domain.car;
 
+import racingcar.domain.validator.CarNameValidator;
+
 public class Name {
+    private final String value;
+    private final CarNameValidator validator;
+
+    public Name(String value) {
+        this.validator = new CarNameValidator();
+        this.validator.validate(value);
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
