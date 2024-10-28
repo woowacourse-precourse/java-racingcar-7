c5d773e0fcf9814ce82d9aa64ba11143034fb25a
김호연 <yun1094@naver.com>
2024-10-28 21:15:56 +0900

    예외 처리 클래스를 만들어 코드 단순화

--- src/main/java/racingcar/Model/CarCreate.java
+++ src/main/java/racingcar/Model/CarCreate.java
@@ -3,13 +3,13 @@ package racingcar.Model;
 import java.util.ArrayList;
 import java.util.List;
 
-import racingcar.View.Output;
+import racingcar.Util.Validator;
 
 public class CarCreate {
-    public static List<Car> createCar(String[] carNames) {
+    public static List<Car> createCar(List<String> carNames) {
         List<Car> cars = new ArrayList<Car>();
         for (String name : carNames) {
-            Output.validatorCarNames(name);
+            Validator.carName(name);
             cars.add(new Car(name));
         }
         return cars;
