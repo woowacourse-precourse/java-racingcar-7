45a8495527bc7b113c44ff94584174ffdf509b70
DongYeop <s01054830455@gmail.com>
2024-10-28 21:32:32 +0900

    refactor: RacingCount 객체 primitive 패키지로 이동
    - RacingCount 객체는 원시값 포장 객체에 가깝다.

--- src/main/java/racingcar/model/primitive/RacingCount.java
+++ src/main/java/racingcar/model/primitive/RacingCount.java
@@ -1,4 +1,4 @@
-package racingcar.model;
+package racingcar.model.primitive;
 
 import java.util.Objects;
 import racingcar.exception.ErrorMessage;
--- src/test/java/racingcar/model/RacingCountTest.java
+++ src/test/java/racingcar/model/RacingCountTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.model.primitive.RacingCount;
 
 class RacingCountTest {
 
