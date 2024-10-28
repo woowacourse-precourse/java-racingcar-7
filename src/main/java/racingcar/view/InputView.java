208c4757471cc42b56d0ba8c5b0bf8218199a19f
hyosang <billy04100410@gmail.com>
2024-10-28 14:08:32 +0900

    refactor(InputView): 자바 코드 컨벤션 반영

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,6 +3,7 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+
     private static final String ASK_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ASK_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
 
