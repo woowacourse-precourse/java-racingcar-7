afe3f7e4cd3221918a0a229e92ba7d945bc02ce8
JoonKyo <joonkyolee@gmail.com>
2024-10-28 13:46:58 +0900

    test(InputViewTest): 입력이 비어있을 때 예외를 발생하는지 확인하는 테스트 구현

---
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -0,0 +1,16 @@
+package racingcar.view;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class InputViewTest {
+    @Test
+    void 입력이_비어있을때_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> InputView.validate(""))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+}
