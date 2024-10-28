78627289b340a90a6f9d72984d826e96f4189aa9
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-28 13:16:29 +0900

    feat(InfoMessage): 실행 결과 안내 메시지 추가

--- src/main/java/racingcar/view/InfoMessage.java
+++ src/main/java/racingcar/view/InfoMessage.java
@@ -3,7 +3,8 @@ package racingcar.view;
 public enum InfoMessage {
 
     CAR_NAMES_INSTRUCTION("자동차 이름을 입력해주세요. (이름은 쉼표(,) 기준으로 구분)"),
-    MOVING_COUNT_INSTRUCTION("시도할 회수는 몇 회인가요?");
+    MOVING_COUNT_INSTRUCTION("시도할 회수는 몇 회인가요?"),
+    RESULT("실행 결과");
 
     private final String message;
 
