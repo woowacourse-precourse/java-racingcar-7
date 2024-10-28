28163de8b38f9d1b4122e10ac445a8524d7b7f8e
wkdlrn <kkjjgg97@gmail.com>
2024-10-28 20:26:42 +0900

    feat: 사용자 입력 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String carNames = getCarNames();
+        int moveCount = getMoveCount();
+        System.out.println("입력받은 자동차 이름: " + carNames);
+        System.out.println("입력받은 이동 횟수: " + moveCount);
+    }
+
+    private static String getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    private static int getMoveCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(Console.readLine());
     }
 }
