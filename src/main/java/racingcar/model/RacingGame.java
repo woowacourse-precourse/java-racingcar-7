8b9bbb6eed32d94cbb5aaf6cde11b31b07c29da1
Koo-EunSung <ku903@naver.com>
2024-10-28 17:59:10 +0900

    Feat: 우승자 반환

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -1,13 +1,16 @@
 package racingcar.model;
 
+import java.util.Comparator;
 import java.util.List;
 import racingcar.dto.Result;
+import racingcar.dto.Result.Round;
 import racingcar.util.RandomUtil;
 
 public class RacingGame {
     private List<Car> racer;
     private int round;
     private RandomUtil randomUtil = new RandomUtil();
+    private List<Result.Round> resultOfRound;
 
     public RacingGame(List<Car> racer, int round) {
         validateRound(round);
@@ -22,8 +25,25 @@ public class RacingGame {
     }
 
     public List<Result.Round> progress() {
-        return racer.stream()
+        List<Result.Round> resultOfRound = racer.stream()
                 .map(racer -> racer.go(randomUtil.generateNumber()))
                 .toList();
+
+        this.resultOfRound = resultOfRound;
+
+        return resultOfRound;
+    }
+
+    public List<String> selectWinner() {
+        Result.Round maxStep = resultOfRound.stream()
+                .max(Comparator.comparingInt(Result.Round::getStep))
+                .orElseThrow();
+
+        return resultOfRound.stream()
+                        .filter(result -> Comparator
+                                .comparing(Result.Round::getStep)
+                                .compare(result, maxStep) == 0 )
+                        .map(Round::getName)
+                        .toList();
     }
 }
