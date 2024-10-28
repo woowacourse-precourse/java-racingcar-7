54721b7ecab11a873ca75b62b20bd3f6e8d738d6
heeyeop Kim <ssucgoo@gmail.com>
2024-10-28 21:45:18 +0900

    feat: 횟수 입력 받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,5 +8,8 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         String[] splitInput = input.split(",");
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String trialInput = Console.readLine();
     }
 }
