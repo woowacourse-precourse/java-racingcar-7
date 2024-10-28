6219d6018eefac04dac3936e044d571f29e2e989
seoys0910 <seoys0910@naver.com>
2024-10-28 14:31:51 +0900

    feat : 빈 문자열이 입력되었는지 검증하는 기능 구현

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
     public static List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
+        validateEmpty(input);
         return Utils.splitByDelimiter(input);
     }
 
@@ -15,4 +16,11 @@ public class InputView {
         String trycount = Console.readLine();
         return Integer.parseInt(trycount);
     }
+
+    private static void validateEmpty(String input) {
+        if (input.isEmpty() || input.isBlank()) {
+            throw new IllegalArgumentException("빈 값이 입력되었습니다.");
+        }
+    }
+
 }
