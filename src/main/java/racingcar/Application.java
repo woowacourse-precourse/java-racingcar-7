822754eb4a8bfc457fb31d9aec8aeb727e9d5404
doronggyu <rbehddl13@gmail.com>
2024-10-28 18:29:22 +0900

    feat:입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        String carNameText;
+        int tryNum;
+
+        System.out.println("경주할 자동차 이름을 입력하세요.");
+        carNameText = Console.readLine();
+        System.out.println("횟수를 입력하세요.");
+        tryNum = Integer.parseInt(Console.readLine());
     }
+
+
 }
