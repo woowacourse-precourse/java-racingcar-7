8ef54e64b001330cc5122ea0cdb9049535a20986
jungsu <vwx1@naver.com>
2024-10-28 14:06:24 +0900

    refactor: update RacingGameController
    - Modify RacingGameController to accept RacingGameServiceInterface, InputViewInterface, and ResultViewInterface via constructor.

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,68 +1,43 @@
 package racingcar.controller;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import racingcar.model.Car;
-import racingcar.view.InputView;
-import racingcar.view.ResultView;
+import racingcar.service.RacingGameServiceInterface;
+import racingcar.view.InputViewInterface;
+import racingcar.view.ResultViewInterface;
+
+import java.util.List;
 
 public class RacingGameController {
+
+  private final RacingGameServiceInterface racingGameService;
+  private final InputViewInterface inputView;
+  private final ResultViewInterface resultView;
+
+  public RacingGameController(
+      RacingGameServiceInterface racingGameService,
+      InputViewInterface inputView,
+      ResultViewInterface resultView
+  ) {
+    this.racingGameService = racingGameService;
+    this.inputView = inputView;
+    this.resultView = resultView;
+  }
+
   public void run() {
-    String[] carNames = InputView.inputCarNames();
-    int trialCount = InputView.inputTrialCount();
+    String[] carNames = inputView.inputCarNames();
+    int trialCount = inputView.inputTrialCount();
 
-    validateCarNames(carNames);
-    validateTrialCount(trialCount);
+    racingGameService.validateCarNames(carNames);
+    racingGameService.validateTrialCount(trialCount);
 
-    List<Car> cars = createCars(carNames);
+    List<Car> cars = racingGameService.createCars(carNames);
 
     for (int i = 0; i < trialCount; i++) {
-      playRound(cars);
-      ResultView.printRoundResult(cars);
+      racingGameService.playRound(cars);
+      resultView.printRoundResult(cars);
     }
 
-    List<Car> winners = findWinners(cars);
-    ResultView.printWinners(winners);
-  }
-  private void validateCarNames(String[] carNames) {
-    Set<String> uniqueNames = new HashSet<>();
-    for (String name : carNames) {
-      if (!uniqueNames.add(name)) {
-        throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
-      }
-    }
-  }
-
-  private void validateTrialCount(int trialCount) {
-    if (trialCount < 1) {
-      throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
-    }
-  }
-  private List<Car> createCars(String[] carNames) {
-    List<Car> cars = new ArrayList<>();
-    for (String name : carNames) {
-      cars.add(new Car(name));
-    }
-    return cars;
-  }
-
-  private void playRound(List<Car> cars) {
-    for (Car car : cars) {
-      car.move();
-    }
-  }
-
-  private List<Car> findWinners(List<Car> cars) {
-    int maxPosition = cars.stream().mapToInt(Car::getPosition).max().orElse(0);
-
-    List<Car> winners = new ArrayList<>();
-    for (Car car : cars) {
-      if (car.getPosition() == maxPosition) {
-        winners.add(car);
-      }
-    }
-    return winners;
+    List<Car> winners = racingGameService.findWinners(cars);
+    resultView.printWinners(winners);
   }
 }
