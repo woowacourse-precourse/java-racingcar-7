78380a4444507e31b8d2adc0d5181d4412aca730
junho <wnsgh732@gmail.com>
2024-10-28 20:22:25 +0900

    feat: 결과를 받고 입력 출력하는 부분 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,30 @@
 package racingcar;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
+        List<Car> carList = inputHandler.getCarName();
+        RaceTrack raceTrack = new RaceTrack(carList);
+        int attemptNumber = inputHandler.getAttemtNumber();
+
+        for(int i = 0; i < attemptNumber; i ++) {
+            for(Car car : carList) {
+                car.move();
+            }
+            for(Car car : carList) {
+                car.displayPosition();
+            }
+            System.out.println();
+        }
+
+        List<Car> winnerList = raceTrack.getWinner();
+        String result = winnerList.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println("최종 우승자 : " + result);
     }
 }
