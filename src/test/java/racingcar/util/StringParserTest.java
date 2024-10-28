c3b51af75480e9e38423305fdef3205037ec79b4
seunghee <seunghee_chae3@tmax.co.kr>
2024-10-28 19:28:08 +0900

    refactor(StringParserTest): given, when, then 적용

--- src/test/java/racingcar/util/StringParserTest.java
+++ src/test/java/racingcar/util/StringParserTest.java
@@ -11,9 +11,11 @@ public class StringParserTest {
 
     @Test
     void 차_이름_파싱하기_테스트() {
+        // given
         String input = "pobi,woni,jun";
-        List<String> result = Arrays.asList("pobi", "woni", "jun");
+        List<String> expectedCarNames = Arrays.asList("pobi", "woni", "jun");
 
-        assertEquals(result, StringParser.parseCarName(input));
+        // when, then
+        assertEquals(expectedCarNames, StringParser.parseCarName(input));
     }
 }
