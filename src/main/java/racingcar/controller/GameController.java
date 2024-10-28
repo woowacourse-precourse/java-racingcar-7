0caa0b41873df9ddc3866402e8e91ac5aaa3cda0
Si Yeong Park <psy915@swu.ac.kr>
2024-10-28 20:51:15 +0900

    feat(GameController): 최종 우승자 출력 기능 추가

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -5,17 +5,23 @@ import racingcar.model.RandomNumberGenerator;
 import racingcar.model.domain.GameResult;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class GameController {
 
     public void runGame() {
-        List<String> carNames = InputView.inputNames();
+        String carNamesInput = InputView.inputNames();
+        List<String> carNames = Arrays.asList(carNamesInput.split("\\s*,\\s*"));
+
         int attemptCount = InputView.inputGameCnt();
 
         GameService gameService = new GameService(carNames, attemptCount, new RandomNumberGenerator());
         GameResult gameResult = gameService.playGame();
 
         gameResult.getRoundResults().forEach(OutputView::printRecord);
+
+        OutputView.printWinner(gameResult.getWinners());
     }
 }
