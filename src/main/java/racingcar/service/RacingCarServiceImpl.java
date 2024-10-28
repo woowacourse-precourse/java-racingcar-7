269912d9d5039eb68ddd4eddcd570962a1f56234
jinhyeon <tiemo0708@naver.com>
2024-10-28 13:12:32 +0900

    feat: 서비스에 RandomNumberGenerator 연결

--- src/main/java/racingcar/service/RacingCarServiceImpl.java
+++ src/main/java/racingcar/service/RacingCarServiceImpl.java
@@ -1,8 +1,12 @@
 package racingcar.service;
 
 import racingcar.domain.RacingGame;
+import racingcar.utils.RandomNumberGenerator;
 
 public class RacingCarServiceImpl implements RacingCarService{
+    public RacingCarServiceImpl(RandomNumberGenerator randomNumberGenerator) {
+    }
+
     @Override
     public void startRace(String carNames, int tryCount) {
 
