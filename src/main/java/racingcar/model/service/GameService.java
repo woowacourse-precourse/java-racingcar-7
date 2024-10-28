1b2c431431d99fda7d062ae0a08d1199bb15cf9d
Si Yeong Park <psy915@swu.ac.kr>
2024-10-28 20:40:24 +0900

    feat(GameService): 각 라운드의 결과를 기록하고 최종 우승자를 관리 기능

---
+++ src/main/java/racingcar/model/service/GameService.java
@@ -0,0 +1,32 @@
+package racingcar.model.service;
+
+import racingcar.model.domain.Cars;
+import racingcar.model.NumberGenerator;
+import racingcar.model.domain.GameResult;
+
+import java.util.List;
+
+public class GameService {
+    private final Cars cars;
+    private final int attemptCount;
+    private final GameResult gameResult;
+
+    public GameService(List<String> carNames, int attemptCount, NumberGenerator numberGenerator) {
+        this.cars = Cars.from(carNames, numberGenerator);
+        this.attemptCount = attemptCount;
+        this.gameResult = new GameResult();
+    }
+
+    public GameResult playGame() {
+        for (int i = 0; i < attemptCount; i++) {
+            playRound();
+        }
+        gameResult.recordWinner(cars);
+        return gameResult;
+    }
+
+    private void playRound() {
+        cars.moveForward();
+        gameResult.recordRound(cars);
+    }
+}
