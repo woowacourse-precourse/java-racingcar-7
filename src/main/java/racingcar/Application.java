1ce6234a08d5cd63089d9ed56564e38eddcf4fa6
Cheoroo <195822@jnu.ac.kr>
2024-10-28 09:43:32 +0900

    feat : 자동차 경기 실행 로직을 구현한다
    - 입출력 View를 사용하여 게임 실행 흐름 구현
    - 이동 전략 및 예외처리를 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            List<String> carNames = InputView.getCarNames();
+            int moveCount = InputView.getMoveCount();
+
+            MoveStrategy moveStrategy = new RandomMoveStrategy(new DefaultRandomGenerator());
+            RacingGame racingGame = new RacingGame(carNames, moveCount, moveStrategy);
+
+            System.out.println("\n실행 결과");
+            racingGame.startRace();
+            for (int i = 0; i < moveCount; i++) {
+                OutputView.printRoundResult(racingGame.getCars());
+            }
+
+            List<Car> winners = racingGame.getWinners();
+            OutputView.printWinners(winners);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
 }
