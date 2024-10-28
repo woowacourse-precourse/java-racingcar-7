5b9e9f5192f9790ff0c4bb0621cb395bcfe47dbf
[CLOUD4] 백승주 <bsj5967@naver.com>
2024-10-28 16:51:49 +0900

    feat(application): 자동차 이름 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println(" 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> cars_name = getCarsName();
+
+    }
+
+    public static List<String> getCarsName() {
+        String input = Console.readLine();
+        return List.of(input.split(","));
     }
 }
+
+
