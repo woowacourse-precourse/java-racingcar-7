dbc934ee615434d83074345285cbd4bc8f16e05b
Jihyun3478 <leejihyun3478@gmail.com>
2024-10-28 23:03:15 +0900

    feat : OutputMessage 구현
    OutputMessage 구현
    - 자동차 이름 입력 안내 메시지
    - 시도 횟수 입력 안내 메시지
    - 실행 결과 출력 메시지
    - 우승자 출력 메시지

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,23 +7,19 @@ import racingcar.model.domain.Delimiter;
 
 public class OutputView {
 
-	private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-	private static final String INPUT_ATTEMPT_NUMBER = "시도할 횟수는 몇 회인가요?";
-	private static final String RACE_RESULT = "\n실행 결과";
 	private static final String SEMI_COLON = " : ";
 	private static final String HYPHEN = "-";
-	private static final String FINAL_WINNER = "최종 우승자 : ";
 
 	public static void carName() {
-		System.out.println(INPUT_CAR_NAME);
+		System.out.println(OutputMessage.INPUT_CAR_NAME.getMessage());
 	}
 
 	public static void attemptNumber() {
-		System.out.println(INPUT_ATTEMPT_NUMBER);
+		System.out.println(OutputMessage.INPUT_ATTEMPT_NUMBER.getMessage());
 	}
 
 	public static void runResultGuide() {
-		System.out.println(RACE_RESULT);
+		System.out.println(OutputMessage.RACE_RESULT.getMessage());
 	}
 
 	public static void runResult(List<Car> cars) {
@@ -33,6 +29,6 @@ public class OutputView {
 	}
 
 	public static void winners(List<String> winners) {
-		System.out.println(FINAL_WINNER + String.join(Delimiter.COMMA.getDelimiter() + " ", winners));
+		System.out.println(OutputMessage.FINAL_WINNER.getMessage() + String.join(Delimiter.COMMA.getDelimiter() + " ", winners));
 	}
 }
