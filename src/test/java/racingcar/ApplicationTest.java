ef0758af778e6cb0a660b054ed389a47c9959dfc
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-28 17:25:57 +0900

    feat(ApplicationTest): 기능 테스트 및 예외 테스트 케이스 추가
    - 총 18개의 테스트
    - 기능 테스트는 실제로 동작시켰을 때, 기대하는 값과 결과값이 일치하는지 판별하는 테스트
    - 예외 테스트는 잘못된 값 등의 사용으로 IllegalArgumentException 예외가 발생하는지 판별하는 테스트
    - 예외 테스트는 크게 두 가지로 나뉨
    - 사용자가 입력 조건에 맞지 않는 값을 입력했을 때 (뷰에서 바로 사용자에게 알려줄 수 있는 예외 즉, 비밀번호와 비밀번호 확인이 서로 다름, 비밀번호에 특수문자가 없음 등)
    - 사용자가 입력한 값의 내용이 올바르지 않을 때 (서버와의 통신을 거쳐야만 확인할 수 있는 예외 즉, 아이디 중복, 이메일 중복, 인증 실패 등)

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -13,7 +13,7 @@ class ApplicationTest extends NsTest {
     private static final int STOP = 3;
 
     @Test
-    void 기능_테스트() {
+    void 기능_테스트_한명이_우승자() {
         assertRandomNumberInRangeTest(
             () -> {
                 run("pobi,woni", "1");
@@ -24,10 +24,164 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 기능_테스트_동시_우승자() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,hun", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 기능_테스트_전원_우승_전부_전진() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,hun", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "hun : -", "최종 우승자 : hun, pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 기능_테스트_전원_우승_전부_전진실패() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,hun", "1");
+                    assertThat(output()).contains("pobi : ", "woni : ", "hun : ", "최종 우승자 : hun, pobi, woni");
+                },
+                STOP, STOP, STOP
+        );
+    }
+
+    @Test
+    void 기능_테스트_자동차_이름_구분을_쉼표로_사용하지않음() {
+        System.out.println("자동차 이름 구분자로 쉼표가 아닌 문자를 사용했을 때는 '.'도 이름으로 판단");
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("po.bi,woni,hun", "1");
+                    assertThat(output()).contains("po.bi : -", "woni : ", "hun : ", "최종 우승자 : po.bi");
+                },
+                MOVING_FORWARD, STOP, STOP
+        );
+    }
+
+    @Test
+    void 기능_테스트_자동차_이름_앞에_공백() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run(" pobi,woni,      hun", "1");
+                    assertThat(output()).contains("pobi : ", "woni : ", "hun : -", "최종 우승자 : hun");
+                },
+                STOP, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 기능_테스트_자동차_이름_뒤에_공백() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run(" pobi,woni,hun      ", "1");
+                    assertThat(output()).contains("pobi : ", "woni : ", "hun : -", "최종 우승자 : hun");
+                },
+                STOP, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 기능_테스트_자동차_이름_앞_뒤에_공백() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run(" pobi,woni,  hun  ", "1");
+                    assertThat(output()).contains("pobi : ", "woni : ", "hun : -", "최종 우승자 : hun");
+                },
+                STOP, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 기능_테스트_매_차수마다_출력() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run(" pobi,woni", "2");
+                    assertThat(output()).contains("pobi : -", "woni : ", "pobi : --", "woni : ","최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD, STOP, MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 예외_테스트_자동차_이름이_5글자_초과() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_자동차_이름이_중복() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_시행_횟수가_음수() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_시행_횟수가_0() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_자동차_이름이_공백() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi, ,woni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_자동차_이름이_빈_문자열() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,,woni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트_자동차_이름이_개행문자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,\n,woni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_자동차_이름_구분을_쉼표로_사용하지않음() {
+        System.out.println("자동차 이름 구분자로 쉼표가 아닌 문자를 사용했을 때는 '.'도 이름으로 판단");
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi.woni,hun", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_시행_횟수가_정수가_아님() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "hun"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
