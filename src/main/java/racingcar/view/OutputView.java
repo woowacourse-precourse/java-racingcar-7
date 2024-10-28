d24efeb0ed3c91a487d9b27e93a9e6143b97a710
Koo-EunSung <ku903@naver.com>
2024-10-28 18:30:38 +0900

    Feat: 우승자 안내 문구 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,13 +4,19 @@ import java.util.List;
 import racingcar.dto.Result;
 
 public class OutputView {
+
     public void inform() {
         System.out.println("실행 결과");
     }
+
     public void resultOfRound(List<Result.Round> result) {
         result.forEach(res -> {
             System.out.println(res.getName() + " : " + "-".repeat(res.getStep()));
         });
         System.out.println();
+    }
+
+    public void winner(List<String> winner) {
+        System.out.println("최종 우승자 : " + String.join(", ", winner));
     }
 }
