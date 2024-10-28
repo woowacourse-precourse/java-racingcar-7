8a1de20bf407cf55538810005cc36bafbcc7a7c0
yunju <juile0827@naver.com>
2024-10-28 14:43:19 +0900

    fix(InputProcessService): 자동차 이름 검증 메서드에 추가 예외 케이스 처리 로직 추가
    - 입력이 없는 경우
    - ',' 문자로 시작하거나 끝이 나는 경우

--- src/main/java/racingcar/service/InputProcessService.java
+++ src/main/java/racingcar/service/InputProcessService.java
@@ -17,19 +17,29 @@ public class InputProcessService {
     }
 
     public void carsInputValidation(String carsInput){
-        String[] carArray = carsInput.split(",");
+        carsInput = carsInput.trim();
 
+        if(carsInput.length() == 0){
+            throw new IllegalArgumentException("[자동차 이름 입력 오류] 입력이 없습니다.");
+        }
+
+        // , 로 시작하거나 끝이 나는 경우
+        if(carsInput.charAt(0) == ',' || carsInput.charAt(carsInput.length()-1) == ','){
+            throw new IllegalArgumentException("[자동차 이름 입력 오류] 자동차의 이름은 한 글자 이상만 가능합니다.");
+        }
+
+        String[] carArray = carsInput.split(",");
         List<String> carList = new ArrayList<>(Arrays.asList(carArray));
 
         for(String car : carList){
             car = car.trim();
 
             if(car.length() == 0){
-                throw new IllegalArgumentException("자동차의 이름은 한 글자 이상만 가능합니다.");
+                throw new IllegalArgumentException("[자동차 이름 입력 오류] 자동차의 이름은 한 글자 이상만 가능합니다.");
             }
 
             if(car.length() > 5){
-                throw new IllegalArgumentException(car + ": 자동차의 이름은 다섯 글자 이하만 가능합니다.");
+                throw new IllegalArgumentException("[자동차 이름 입력 오류] 자동차의 이름은 다섯 글자 이하만 가능합니다. : "+car);
             }
         }
     }
@@ -41,11 +51,11 @@ public class InputProcessService {
             roundInput.trim();
             round = Integer.parseInt(roundInput);
         } catch (Exception e){
-            throw new IllegalArgumentException("시도 횟수는 띄어쓰기 없는 정수로 입력해주세요.");
+            throw new IllegalArgumentException("[시도 횟수 입력 오류] 시도 횟수는 띄어쓰기 없는 정수로 입력해주세요.");
         }
 
-        if(round == 0){
-            throw new IllegalArgumentException("시도 횟수는 0회 이상 가능합니다.");
+        if(round <= 0){
+            throw new IllegalArgumentException("[시도 횟수 입력 오류] 시도 횟수는 0회 이상 가능합니다.");
         }
     }
 
