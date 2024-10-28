275a0b732e7548769c3cc7bb9f76915f14b381fd
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-10-28 19:12:29 +0900

    test(car-test): remove unnecessary test case
    - removed obsolete or redundant test case that are no longer relevent

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,7 +1,6 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.common.exception.CarValidationError;
 import racingcar.model.Car;
-import racingcar.model.CarInformation;
 import racingcar.model.CarMovementPolicy;
 import racingcar.model.Cars;
 import racingcar.model.DefaultCarMovementPolicy;
@@ -58,27 +56,5 @@ class CarsTest {
         assertThatThrownBy(() -> new Cars(cars))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(CarValidationError.DUPLICATED_CARS_EXISTS_EXCEPTION.getMessage());
-    }
-
-    @Test
-    @DisplayName("모든 자동차의 이름과 위치를 반환해야 한다.")
-    void getAllCarInformationTest() {
-        // given
-        Car pobi = new Car(Name.from("pobi"), movementPolicy);
-        Car woni = new Car(Name.from("woni"), movementPolicy);
-        Cars cars = new Cars(List.of(pobi, woni));
-        woni.race(6);
-
-        // when
-        List<CarInformation> informations = cars.getAllCarInformation();
-
-        // then
-        assertAll(
-                () -> assertEquals(2, informations.size()),
-                () -> assertEquals("pobi", informations.getFirst().name()),
-                () -> assertEquals("woni", informations.get(1).name()),
-                () -> assertEquals(0, informations.getFirst().position()),
-                () -> assertEquals(1, informations.get(1).position())
-        );
     }
 }
