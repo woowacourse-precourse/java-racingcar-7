812d04ab5c73def85f3f6e4f523f6349f8fcd93a
박철현 <park82602@gmail.com>
2024-10-28 17:56:30 +0900

    Style : 구분자 검증 로직을 삭제함에 따라 변경

--- src/main/java/racingcar/exception/Validator.java
+++ src/main/java/racingcar/exception/Validator.java
@@ -4,29 +4,21 @@ import camp.nextstep.edu.missionutils.Console;
 
 public abstract class Validator {
     private static final int MAX_LENGTH = 5;
-    private static final String DEFAULT_DELIMITER = ",";
 
-    public static String validateNameLength(String names) {
-        if (names.length() > MAX_LENGTH) {
+    public static String validateNameLength(String carNames) {
+        if (carNames.length() > MAX_LENGTH) {
             Console.close();
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("자동차의 이름은 5글자 이하여야 합니다");
         }
-        return names;
+        return carNames;
     }
 
     public static int validateCircleCountIfNegative(String circleCount) {
         int countAsInt = Integer.parseInt(circleCount);
         if (countAsInt < 0) {
             Console.close();
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("순회 횟수 입력은 1이상 이여야 합니다.");
         }
         return countAsInt;
-    }
-
-    public static void validateDelimiterFormatAndSingleRacer(String carNames) {
-        if (!carNames.contains(DEFAULT_DELIMITER)) {
-            Console.close();
-            throw new IllegalArgumentException();
-        }
     }
 }
