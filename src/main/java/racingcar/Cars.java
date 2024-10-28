91627c449529e8be68c063aaba38c1228a8d29be
JEN <ekffuclxk@gmail.com>
2024-10-28 13:56:40 +0900

    refactor: 미사용 주석,import 제거

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -3,15 +3,8 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Cars {
-    /* 자동차 클래스 관리 클래스 생성 - 0
-    * - 우승 자동차 결정 기능
-    * - 우승 자동차 출력 기능
-    * - 우승자가 여러 명일 경우, 쉼표(,)를 이용하여 구분한다.
-    * 잘못된 값이 입력될 경우 IllegalArgumentException을 발생시키고 에플리케이션을 종료한다.
-     */
 
     private List<Car> cars;
 
