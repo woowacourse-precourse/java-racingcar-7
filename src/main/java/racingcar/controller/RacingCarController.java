001c940656117beca4f71986ed2c9d72947d74a3
sese2204 <sese2204@naver.com>
2024-10-28 11:34:20 +0900

    feat(controller): RacingCarController의 게임을 시작하는 부분 구현
    - start 메소드 구현
    - InputView의 메소드를 세분화하여 구분

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,20 @@
+package racingcar.controller;
+
+import racingcar.service.RacingCarService;
+import racingcar.view.InputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingCarController {
+    private RacingCarService racingCarService;
+
+        public void start(){
+            List<String> nameList;
+            nameList = InputView.getCarNames();
+            int tryCount = InputView.getTryCount();
+            this.racingCarService = new RacingCarService(nameList);
+        }
+
+
+}
