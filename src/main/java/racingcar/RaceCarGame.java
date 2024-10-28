f244b0b2822105d65c2a55921bf43c2a0e646293
CheBeomHee <tigerfrom2@naver.com>
2024-10-28 19:02:56 +0900

    feat(RaceCarGame): 마지막 위너 출력 메소드 구현

--- src/main/java/racingcar/RaceCarGame.java
+++ src/main/java/racingcar/RaceCarGame.java
@@ -20,15 +20,16 @@ public class RaceCarGame {
     }
 
     /***
-     * 레이스 게임을 진행
+     * 레이스 게임을 진행하고 각 시도마다 상태 업데이트 마지막에 우승자 출력
      *
      */
     public void race(){
         System.out.println("실행 결과");
         for(int i = 0; i < this.cnt; i++){
             forwardUpdate();
-
+            showGameStatus();
         }
+        selectWinners();
     }
 
     /***
@@ -38,7 +39,6 @@ public class RaceCarGame {
     private void forwardUpdate(){
         for(RaceCar raceCar : this.cars){
             raceCar.updateForward(Randoms.pickNumberInRange(0, 9));
-            showGameStatus();
         }
     }
 
@@ -62,5 +62,23 @@ public class RaceCarGame {
     private String makeStatus(RaceCar raceCar){
         return raceCar.getName() + " : " +
                 "-".repeat(Math.max(0, raceCar.getForward()));
+    }
+
+    private void selectWinners(){
+        int winnerScore = 0;
+        for(RaceCar raceCar : this.cars) {
+            winnerScore = Math.max(winnerScore, raceCar.getForward());
+        }
+        System.out.println("최종 우승자 : " + makeWinnerStr(winnerScore));
+    }
+
+    private String makeWinnerStr(int winnerScore){
+        StringJoiner joiner = new StringJoiner(", ");
+        for (RaceCar raceCar : cars) {
+            if (winnerScore == raceCar.getForward()) {
+                joiner.add(raceCar.getName());
+            }
+        }
+        return joiner.toString();
     }
 }
