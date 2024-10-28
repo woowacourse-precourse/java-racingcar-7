bcbb9fc512cbec9cf871869bbb418f431273c298
Park-Min-Jun <gomdole3963@gmail.com>
2024-10-28 23:09:39 +0900

    feat : 자동차 이름이 5글자 이하여야함.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,6 +10,21 @@ public class Application {
 
     private static String[] getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine().split(",");
+        String[] carNames = Console.readLine().split(",");
+        validateCarNames(carNames);
+        return carNames;
+    }
+
+    // 자동차 이름 유효성 검사
+    private static void validateCarNames(String[] carNames) {
+        for (String name : carNames) {
+            if (isInvalidCarName(name)) {
+                throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하로 설정해야 합니다.");
+            }
+        }
+    }
+
+    private static boolean isInvalidCarName(String name) {
+        return name.trim().length() > 5 || name.trim().isEmpty();
     }
 }
