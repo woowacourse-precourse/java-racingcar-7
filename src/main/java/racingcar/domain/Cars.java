c18a8928a1ed240977529b4f0c3a1d5394eb83ad
Park - Jeonghwan <990706leo@gmail.com>
2024-10-28 10:11:03 +0900

    feat(Cars): 전략에 따라 자동차들을 움직일 수 있다

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -13,6 +13,10 @@ public class Cars {
         this.cars = new ArrayList<>(cars);
     }
 
+    public void move(final Movement movement) {
+        cars.forEach(car -> car.move(movement.canMove()));
+    }
+
     private void validateDuplicated(List<Car> cars) {
         if (cars.size() > new HashSet<>(cars).size()) {
             throw new IllegalArgumentException("자동차는 중복될 수 없습니다.");
