11f4556d7c2382a2abf4fa0537286ebd6b54979f
Ek, Kyungbin <eukyungbin0@gaml.com>
2024-10-28 21:10:52 +0900

    feat: 자동차 이름 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,39 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+class Car {
+    private final String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+    public String getNma() {
+        return name;
+    }
+}
 public class Application {
+    private final List<Car> cars;
+    private final int rounds;
+
+    public Application(List<String> carNames, int rounds) {
+        this.cars = new ArrayList<>();
+        for (String name : carNames) {
+            if (name.length() > 5) throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            cars.add(new Car(name));
+        }
+        this.rounds = rounds;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carNamesInput = sc.nextLine().split(",");
+        List<String> carNames = new ArrayList<>();
+        for (String name : carNamesInput) {
+            carNames.add(name.trim());
+        }
     }
 }
