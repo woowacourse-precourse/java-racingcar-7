45780b0014f7ff789ad3af38240d63145fa854b9
dnwldla <leeemjw@naver.com>
2024-10-28 17:39:48 +0900

    feat: 문장이 쉼표로 시작하거나 끝나는 경우 예외사항 추가

--- src/main/java/racingcar/domain/CarsFactory.java
+++ src/main/java/racingcar/domain/CarsFactory.java
@@ -6,13 +6,14 @@ import java.util.List;
 public class CarsFactory {
     private static final String DUPLICATED_NAME = "각 차는 한번만 입력해야 합니다";
     private static final String OUT_OF_RANGE_NAME = "차는 다섯글자 이하이어야 합니다";
-    private static final String BLANK_NAME = "차 이름은 공백일 수 없습니다";
+    private static final String BLANK_NAME = "차 이름에 공백이 존재할 수 없습니다";
+    private static final String COMMA_ON_START_END_POINT = "입력의 시작과 끝은 쉼표가 될 수 없습니다";
     private static final String SPLITOR = ",";
+    private static final String BLANK = ",";
     private static final Integer LONGEST_NAME_LENGTH = 5;
 
     public static List<Car> makeCarList(String carsInput) {
-        carsInput = removeBlank(carsInput);
-
+        validateStartEndPoint(carsInput);
         validateBlankName(carsInput);
         validateDuplicatedName(carsInput);
         validateOutOfRangeName(carsInput);
@@ -24,17 +25,22 @@ public class CarsFactory {
         return carList;
     }
 
+    private static void validateStartEndPoint(String carsInput) {
+        char startPoint = carsInput.charAt(0);
+        char endPoint = carsInput.charAt(carsInput.length() - 1);
+
+        if (startPoint == ',' || endPoint == ',') {
+            throw new IllegalArgumentException(COMMA_ON_START_END_POINT);
+        }
+    }
+
     private static void validateBlankName(String carsInput) {
-        boolean hasBlank = Arrays.stream(carsInput.split(SPLITOR))
+        boolean hasBlank = Arrays.stream(carsInput.split(BLANK))
                 .anyMatch((car) -> car.isBlank());
 
         if (hasBlank) {
             throw new IllegalArgumentException(BLANK_NAME);
         }
-    }
-
-    private static String removeBlank(String carsInput) {
-        return carsInput.replace(" ", "");
     }
 
     private static void validateDuplicatedName(String carsInput) {
