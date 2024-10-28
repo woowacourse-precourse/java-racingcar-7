567a671c6f079ffbd738473e5dab556581c2ed61
Jungwoo Chu <cjw980930@gmail.com>
2024-10-28 23:27:46 +0900

    feat: 자동차이름 앞뒤 공백 제거

--- src/main/java/racingcar/CarValidator.java
+++ src/main/java/racingcar/CarValidator.java
@@ -6,11 +6,13 @@ import java.util.List;
 public class CarValidator {
     public static List<String> validateNames(String input){
         List<String> carList = Arrays.asList(input.split(","));
-        for (String car : carList) {
-            if (car.length() > 5) {
-                throw new IllegalArgumentException();
+        for (int i = 0; i < carList.size(); i++) {
+            carList.set(i, carList.get(i).trim());
+            if (carList.get(i).length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 최대 5자 이내여야 합니다. ");
             }
         }
         return carList;
     }
+
 }
