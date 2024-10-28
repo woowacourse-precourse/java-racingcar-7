75ae50a1038a8a079457cb5d26b593663a34ba13
hooniverse <sung010326@gmail.com>
2024-10-28 22:29:39 +0900

    feat: 입력 검증 클래스 작성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,16 +1,21 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.Validation;
 
 public class InputView {
 
+    Validation validation = new Validation();
+
     public String getCarsName() {
         String CarsName = Console.readLine();
+        validation.isNullOrEmpty(CarsName);
         return CarsName;
     }
 
     public int getRoundNumber() {
         String RoundNumber = Console.readLine();
+        validation.isNullOrEmpty(RoundNumber);
         return Integer.parseInt(RoundNumber);
     }
 }
