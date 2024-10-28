e1d8a65ba5438b3f88f9ed2e4b1c9ecb73e69edd
halfmoon-mind <5338095@gmail.com>
2024-10-28 22:27:10 +0900

    feat: Game 객체 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,2 +1,35 @@
-package racingcar;public class Game {
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Game {
+    private final InputHandler inputHandler;
+
+    Game(InputHandler inputHandler) {
+        this.inputHandler = inputHandler;
+    }
+
+    private List<Car> carList = new ArrayList<>();
+    private int remainAttempts = 0;
+
+    public void initializeCars() {
+        List<String> result = inputHandler.getCarNames();
+        carList.addAll(result.stream().map(c -> new Car(c)).collect(Collectors.toList()));
+    }
+
+    public void initializeRemainAttempts() {
+        remainAttempts = inputHandler.getNumberOfAttempts();
+    }
+
+    public void startGame() {
+        for (int i = 0; i < remainAttempts; i++) {
+            for (int j = 0; j < carList.size(); j++) {
+                carList.get(j).move();
+                carList.get(j).printCurrentStatus();
+            }
+        }
+    }
+
 }
--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -3,20 +3,43 @@ package racingcar;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class InputHandler {
-    public List<String> getCarNames() {
+    public List<String> getCarNames() throws IllegalArgumentException {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         List<String> carNames = parseCarNames(input);
-        validateCarNames(carNames);
+        validateCarNames(carNames, input);
         return carNames;
     }
 
-    private void validateCarNames(List<String> carNames) {
+    private void validateCarNames(List<String> carNames, String input) throws IllegalArgumentException {
+        final long carNameCount = input.chars().filter(c -> c == ',').count() + 1;
+
+        if (carNameCount != carNames.size()) {
+            throw new IllegalArgumentException("이름을 입력해야 합니다.");
+        }
+
+        if (carNames.isEmpty()) {
+            throw new IllegalArgumentException("빈 문자열은 입력할 수 없습니다.");
+        }
+
         for (String name : carNames) {
             if (name.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+            if (name.isEmpty()) {
+                throw new IllegalArgumentException("이름을 입력해야 합니다.");
+            }
+
+            String regex = "[^a-zA-Z0-9가-힣ㄱ-ㅎㅏ-ㅣ]";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(name);
+
+            if (matcher.find()) {
+                throw new IllegalArgumentException("특수문자는 허용되지 않습니다.");
             }
         }
     }
@@ -25,7 +48,8 @@ class InputHandler {
         return List.of(input.split(","));
     }
 
-    public int getNumberOfAttempts() {
+
+    public int getNumberOfAttempts() throws IllegalArgumentException {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String input = Console.readLine();
         try {
@@ -38,5 +62,4 @@ class InputHandler {
             throw new IllegalArgumentException("올바른 숫자를 입력하세요.");
         }
     }
-
 }
