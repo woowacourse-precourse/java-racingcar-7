476f29aa58f1a29d024ca482095340876683e0c5
sytTest <syt1128@naver.com>
2024-10-28 17:16:26 +0900

    :sparkles: (ScreenController) add scoreBoardUpdate()
    경기 결과를 시각화해주는 정보를 받아서 점수판을 갱신해주는 scoreBoardUpdate() 추가

--- src/main/java/racingcar/controller/ScreenController.java
+++ src/main/java/racingcar/controller/ScreenController.java
@@ -3,6 +3,7 @@ package racingcar.controller;
 import java.util.List;
 import racingcar.domain.LapScore;
 import racingcar.domain.Race;
+import racingcar.service.race.LapScoreService;
 import racingcar.view.ScoreBoard;
 import racingcar.view.constant.Output;
 
@@ -23,6 +24,11 @@ public class ScreenController {
     public void connect(Race race) {
         System.out.println(ScoreBoard.initMessage());
         this.race = race;
+    }
+
+    public void scoreBoardUpdate() {
+        LapScoreService lapScoreService = new LapScoreService(race.getEntry());
+        printLap(lapScoreService.createEntryScores(race.getEntry()));
     }
 
     private void printLap(List<LapScore> lapScores) {
