72d69fc3060104b3f9a4192e3b80b6011961830c
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-10-28 23:16:50 +0900

    fix: 출력문 오타 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,7 +8,7 @@ import static racingcar.validator.InputValidator.validateCars;
 public class InputView {
     // 자동차 이름 입력 메소드
     public static String inputCars(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String text = Console.readLine();
         return text;
     }
