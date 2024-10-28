65b5d0887fde331ac2509d04a0296b12439c737b
Seongho Kim <kksshh0612@gmail.com>
2024-10-28 11:35:06 +0900

    feat: 컨트롤러를 통해 게임 시작

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,10 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
-import racingcar.model.Car;
+import racingcar.controller.RacingCarController;
+import racingcar.service.InputValidator;
+import racingcar.service.RacingCarService;
+import racingcar.service.RandomNumberGenerator;
+import racingcar.service.StringHandler;
 import racingcar.view.RacingCarView;
 
 public class Application {
@@ -12,16 +12,10 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        RacingCarView carView = new RacingCarView();
+        RacingCarController racingCarController = new RacingCarController(new RacingCarView(), new InputValidator(),
+                new StringHandler(), new RacingCarService(new RandomNumberGenerator()));
 
-        String carNameInput = carView.getCarNameInput();
-        int tryCount = Integer.parseInt(carView.getTryCount());
-
-        String[] cars = carNameInput.split(",");
-        for(String car : cars) {
-            System.out.println("여기:" + car + "사이즈 : " + car.length());
-        }
-        System.out.println(cars.length);
+        racingCarController.start();
 
     }
 }
