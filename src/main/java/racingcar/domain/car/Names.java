420adc2546caeeca811e6f727526f94508a67246
Gyulguma <ybg653924@gmail.com>
2024-10-28 20:39:01 +0900

    fix($domain): 입력 최댓값 수정
    Names의 MAX_SIZE는 500으로, RepeatCount의 MAX_VALUE는 1000으로 수정하였습니다.

--- src/main/java/racingcar/domain/car/Names.java
+++ src/main/java/racingcar/domain/car/Names.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 public class Names {
 
-    private final int MAX_SIZE = 1000;
+    private final int MAX_SIZE = 500;
 
     private final List<Name> names;
 
--- src/main/java/racingcar/domain/racing/RepeatCount.java
+++ src/main/java/racingcar/domain/racing/RepeatCount.java
@@ -2,6 +2,8 @@ package racingcar.domain.racing;
 
 public class RepeatCount {
 
+    private final int MAX_VALUE = 1000;
+
     private final int value;
 
     public RepeatCount(String value) {
@@ -13,8 +15,8 @@ public class RepeatCount {
         if (value == null || value.isEmpty() || !value.matches("^[1-9][0-9]*$")) {
             throw new IllegalArgumentException("자연수만 입력할 수 있습니다.");
         }
-        if (value.length() > 2) {
-            throw new IllegalArgumentException("입력 값이 100 미만이어야합니다.");
+        if (Integer.parseInt(value) > MAX_VALUE) {
+            throw new IllegalArgumentException("입력 값이 " + MAX_VALUE + " 이하여야합니다.");
         }
     }
 
