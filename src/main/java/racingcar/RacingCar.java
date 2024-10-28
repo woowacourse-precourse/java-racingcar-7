8b52f7b337e3c3515382ff92e7500445f64ac14b
yhkee0404 <yhkee0404@gmail.com>
2024-10-28 21:34:37 +0900

    feat(racingcar): add name

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,6 +1,10 @@
 package racingcar;
 
 public class RacingCar {
+    public RacingCar(final String name) {
+        this.name = name;
+    }
+
     public void move() {
         mileage++;
     }
@@ -9,5 +13,10 @@ public class RacingCar {
         return mileage;
     }
 
+    public String getName() {
+        return name;
+    }
+
     private int mileage;
+    private String name;
 }
