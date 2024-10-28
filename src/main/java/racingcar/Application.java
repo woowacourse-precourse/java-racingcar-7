6ad4caf9edc3e6c48fdb4e86c83f56f972d1bd04
dongyeop <ds06041@gmail.com>
2024-10-28 22:26:11 +0900

    feat: 자동차 이름의 길이 검사 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String cars_name = Console.readLine();
+        // 자동차 이름 길이 검사
+        ValidateCarNames.validateCarNames(cars_name);
     }
+
+
 }
