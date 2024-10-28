ec0ca70b80f2a74629fdc2c35f19e14e2e4ca2d4
ham8962 <zzing199842@gmial.com>
2024-10-28 21:20:15 +0900

    Racing Game 클래스 제작 setRound, getCarNames 메서드 제작

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,19 +1,41 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class RacingGame {
 
     public void start() {
-
     }
 
     private int setRound() {
-
+        System.out.println("시도할 회수는 몇 회인가요?");
+        int round =  Integer.parseInt(Console.readLine());
+        if (round < 1) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+        }
+        return round;
     }
 
     private Set<String> getCarNames() {
-
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        String[] carNames = Console.readLine().split(",");
+        Set<String> carNameSet = new LinkedHashSet<>();
+        for (String carName : carNames) {
+            carNameSet.add(carName);
+            System.out.println(carNameSet);
+        }
+        if (carNameSet.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+        }
+        for (String carName : carNameSet) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
+        return carNameSet;
     }
 
     private void play(int round, Set<Car> playingCars) {
