9d93c8d35bf656aaba702f2a49850f74a623dbca
aprilnineteen <dencoding@naver.com>
2024-10-28 14:56:13 +0900

    feat: RacingController 구현해 자동차 경주 게임 흐름 관리

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,15 +1,32 @@
 package racingcar.controller;
 
-import racingcar.exception.RaceCountsException;
+import racingcar.model.Car;
+import racingcar.model.Cars;
+import racingcar.model.RaceGame;
 import racingcar.validation.CarNamesValidation;
 import racingcar.validation.RaceCountsValidation;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final CarNamesValidation carNamesValidation = new CarNamesValidation();
+    private final RaceCountsValidation raceCountsValidation = new RaceCountsValidation();
 
     public void run() {
+        List<String> carNames = carNamesValidation.validateCarNames(inputView.readCarNames());
+        int raceCount = raceCountsValidation.validateRaceCounts(inputView.readRaceCounts());
+
+        List<Car> carObjects = carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+
+        Cars cars = new Cars(carObjects);
+        RaceGame raceGame = new RaceGame(cars, raceCount);
+        raceGame.startRace(outputView);
     }
 }
