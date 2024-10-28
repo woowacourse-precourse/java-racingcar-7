7642c7a1e311b1fa9f8af63c14d84bbdd634d8fe
JINSU <bluesea9519@gmail.com>
2024-10-28 12:00:34 +0900

    feat: add class to run th car racing game

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Race;
+import racingcar.io.InputReader;
+import racingcar.io.OutputWriter;
+import racingcar.strategy.RandomMoveStrategy1;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        InputReader inputReader = new InputReader();
+        OutputWriter outputWriter = new OutputWriter();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> carNames = inputReader.carName();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int attemptCount = inputReader.attemptCount();
+
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        Race race = new Race(cars, attemptCount, outputWriter);
+        race.start(new RandomMoveStrategy1());
+
     }
 }
