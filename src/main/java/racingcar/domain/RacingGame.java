7c7c63b06e5234dcf4eb4bf8c30655148c883547
Jun2-Lee <ljh19980823@gmail.com>
2024-10-28 16:37:30 +0900

    feat : 한개의 턴 진행

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class RacingGame {
@@ -8,5 +10,19 @@ public class RacingGame {
     public RacingGame(List<Car> carList, int num) {
         this.carList = carList;
         this.number = num;
+    }
+
+    public void start() {
+        for(int i = 0; i < number; i++) {
+            // 현재 턴 진행
+            playTurn();
+            // 현재 턴의 상황 출력
+
+        }
+        // 결과 출력
+    }
+
+    public void playTurn() {
+        carList.forEach(car -> car.move(Randoms.pickNumberInRange(0, 9)));
     }
 }
