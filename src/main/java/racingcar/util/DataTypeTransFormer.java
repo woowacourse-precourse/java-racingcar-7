88ac819cf43091b85e9983cf8e6ddf0ab77e4f9d
dragonwaterr <yongsu626@naver.com>
2024-10-28 20:46:50 +0900

    refactor : 자주 등장하는 형변환 기능을 다른 패키지로 분리

---
+++ src/main/java/racingcar/util/DataTypeTransFormer.java
@@ -0,0 +1,10 @@
+package racingcar.util;
+
+
+public class DataTypeTransFormer {
+    private DataTypeTransFormer() {}
+
+    public static int makeStringToInt(String string) {
+        return Integer.parseInt(string);
+    }
+}
