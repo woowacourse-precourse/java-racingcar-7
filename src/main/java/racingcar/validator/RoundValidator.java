9abcb9edc7097f67548e2a68d7b7e26534439f30
jehyuck <wpgur07@gmail.com>
2024-10-28 15:28:37 +0900

    fix: roundValidation에서 잘못된 에러로그 알맞게 수정

--- src/main/java/racingcar/validator/RoundValidator.java
+++ src/main/java/racingcar/validator/RoundValidator.java
@@ -2,8 +2,8 @@ package racingcar.validator;
 
 public class RoundValidator {
     public static void validate(int value) {
-        if (value < 0) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        if (value < 1) {
+            throw new IllegalArgumentException("자연수가 아닌 값이 들어왔습니다.");
         }
     }
 }
