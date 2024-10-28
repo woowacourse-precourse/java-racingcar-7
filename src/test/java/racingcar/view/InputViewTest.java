e42074d77eeb0a62201aae3c60fdf7c073eb605d
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-10-28 20:12:35 +0900

    test: 예외처리 코드 수정

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -9,36 +9,34 @@ class InputViewTest {
     @DisplayName("차 이름 입력 테스트")
     public void inputCarsTest() {
         String input = "pobi,woni,jun";
-        String[] cars = input.split(",");
+        String[] cars = InputView.parseCars(input);
         Assertions.assertThat(cars).containsExactly("pobi", "woni", "jun");
     }
 
     @Test
     @DisplayName("자동차 이름 최소 1개 이상")
     public void minimumOneCarNameTest() {
-        String cars = "pobi";
-        String[] splitCars = cars.split(",");
-        Assertions.assertThatThrownBy(() -> InputView.testValidateCars(splitCars))
+        String input = "";
+
+        Assertions.assertThatThrownBy(() -> InputView.parseCars(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차는 최소 2대 이상 입력해야 합니다.");
+                .hasMessageContaining("자동차는 최소 1대이상 입력해야합니다.");
     }
 
     @Test
-    @DisplayName("자동차 이름 공백")
+    @DisplayName("자동차 이름 , 로 구분")
     public void emptyCarNameTest() {
         String input = "pobi,,jun";
-        String[] cars = input.split(",");
-        Assertions.assertThatThrownBy(() -> InputView.testValidateCars(cars))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차 이름은 공백일 수 없습니다.");
+        String[] cars = InputView.parseCars(input);
+        Assertions.assertThat(cars).containsExactly("pobi", "jun");
     }
 
     @Test
     @DisplayName("자동차 이름 5자 초과")
     public void lengthCarNameTest() {
-        String cars = "wooaah,wooaah,wooaah";
-        String[] splitCars = cars.split(",");
-        Assertions.assertThatThrownBy(() -> InputView.testValidateCars(splitCars))
+        String input = "wooaaaaahancorse";
+
+        Assertions.assertThatThrownBy(() -> InputView.parseCars(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름은 5자 이하만 가능합니다.");
     }
@@ -47,27 +45,37 @@ class InputViewTest {
     @DisplayName("자동차 이름 중복")
     public void duplicateCarNameTest() {
         String input = "pobi,woni,pobi";
-        String[] cars = input.split(",");
-        Assertions.assertThatThrownBy(() -> InputView.testValidateCars(cars))
+
+        Assertions.assertThatThrownBy(() -> InputView.parseCars(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름은 중복될 수 없습니다.");
     }
 
     @Test
-    @DisplayName("자동차 이름 영문자 및 숫자 이외의 문자 포함")
+    @DisplayName("자동차 이름에 영문자 및 숫자 이외의 문자 포함")
     public void invalidCarNameCharactersTest() {
         String input = ";,woni,jun";
-        String[] cars = input.split(",");
-        Assertions.assertThatThrownBy(() -> InputView.testValidateCars(cars))
+        
+        Assertions.assertThatThrownBy(() -> InputView.parseCars(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름은 영문자와 숫자만 포함해야 합니다.");
     }
 
     @Test
+    @DisplayName("앞이나 뒤에 쉼표 들어오는경우")
+    public void trimLeadingAndTrailingCommasTest() {
+        String input = ",pobi,woni,jun,";
+        String cars[] = InputView.parseCars(input);
+
+        Assertions.assertThat(cars).containsExactly("pobi", "woni", "jun");
+    }
+
+    @Test
     @DisplayName("시도 횟수 공백")
     public void emptyAttemptsTest() {
-        String attempts = "";
-        Assertions.assertThatThrownBy(() -> InputView.testValidateAttemptsInput(attempts))
+        String input = "";
+
+        Assertions.assertThatThrownBy(() -> InputView.parseAttempts(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("시도할 횟수는 숫자로 입력해야 합니다.");
     }
@@ -75,8 +83,9 @@ class InputViewTest {
     @Test
     @DisplayName("시도 횟수 숫자 X")
     public void invalidAttemptsTest() {
-        String attempts = "abc";
-        Assertions.assertThatThrownBy(() -> InputView.testValidateAttemptsInput(attempts))
+        String input = "abc";
+
+        Assertions.assertThatThrownBy(() -> InputView.parseAttempts(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("시도할 횟수는 숫자로 입력해야 합니다.");
     }
@@ -84,21 +93,10 @@ class InputViewTest {
     @Test
     @DisplayName("시도 횟수 양수 X")
     public void positiveAttemptsTest() {
-        String attempts = "-1";
-        Assertions.assertThatThrownBy(() -> InputView.validateAttempts(Integer.parseInt(attempts)))
+        String input = "-1";
+
+        Assertions.assertThatThrownBy(() -> InputView.parseAttempts(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("시도할 횟수는 양수를 입력해주세요");
-    }
-
-    @Test
-    @DisplayName("앞뒤 쉼표 제거 테스트")
-    public void trimLeadingAndTrailingCommasTest() {
-        String input = ",pobi,woni,as,";
-        String[] splitCars = input.replaceAll("^,|,$", "").split(",");
-
-        Assertions.assertThat(splitCars).containsExactly("pobi", "woni", "as");
-
-        Assertions.assertThatCode(() -> InputView.testValidateCars(splitCars))
-                .doesNotThrowAnyException();
     }
 }
