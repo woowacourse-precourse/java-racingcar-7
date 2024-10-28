288aeb08fbe75c2db855b3dc3cfec2a1e07caf1c
AbsoluteValues <heejun1708@gmail.com>
2024-10-28 22:51:30 +0900

    refactor: follow code style

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -22,7 +22,7 @@ public class RacingcarController {
         printResultRun(trialCount, cars);
         racingcarView.printWinners(cars);
     }
-    
+
     public List<String> carNameRun() {
         String rawCarName = racingcarView.carNameInput();
         List<String> parsedCarName = racingcarService.parseCarName(rawCarName);
