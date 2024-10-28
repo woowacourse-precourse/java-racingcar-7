a6e532331c77794e5215889de85a49db9be95f17
WARDKEY <tjrmsduq@naver.com>
2024-10-28 16:56:02 +0900

    style(TryNumberTest) : 불필요한 import 제거 및 줄간격 조정

--- src/test/java/racingcar/domain/TryNumberTest.java
+++ src/test/java/racingcar/domain/TryNumberTest.java
@@ -1,9 +1,7 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,12 +10,12 @@ class TryNumberTest {
 	@DisplayName("시도 횟수가 음수가 입력됐는지 확인한다.")
 	@Test
 	void doesTryNmuberNegativeTest() {
-	    // given
-	    TryNumber tryNumber = TryNumber.from(-1);
+		// given
+		TryNumber tryNumber = TryNumber.from(-1);
 
-	    // when
+		// when
 
-	    // then
+		// then
 		assertThat(tryNumber.doesTryNumberNegative())
 			.isTrue();
 	}
