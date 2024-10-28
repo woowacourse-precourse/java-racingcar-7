76afeea7db2af77d45f43d44d86a1268e532668a
aprilnineteen <dencoding@naver.com>
2024-10-28 17:45:22 +0900

    refactor: 예외에 따른 상수 이름 변경

--- src/main/java/racingcar/exception/CarNamesException.java
+++ src/main/java/racingcar/exception/CarNamesException.java
@@ -1,7 +1,7 @@
 package racingcar.exception;
 
 public class CarNamesException extends IllegalArgumentException {
-    public static final String MESSAGE_CAR_NAME_EMPTY = "자동차 이름이 비어있습니다.";
+    public static final String MESSAGE_CAR_NAME_EMPTY = "자동차 이름이 비어 있습니다.";
     public static final String MESSAGE_INVALID_DELIMITER = "잘못된 구분자입니다.";
     public static final String MESSAGE_CAR_NAME_OVER_FIVE = "자동차 이름은 5자를 초과할 수 없습니다.";
     public static final String MESSAGE_DUPLICATE_CAR_NAME = "자동차 이름이 중복됩니다.";
