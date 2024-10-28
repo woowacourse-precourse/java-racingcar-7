fe333b6abcb84b337fd6d6976c4540b2023cd33f
dayaelee <dayae.dev@gmail.com>
2024-10-28 19:27:23 +0900

    feat: 경주할 자동차의 이름을 쉼표 기준으로 구분하여 한줄에 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //1. 경주할 자동차의 이름을 쉼표 기준으로 구분하여 한줄에 받기
+        String inputCar;
+        try{
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            inputCar = Console.readLine();
+        } catch (IllegalArgumentException e){
+            return;
+        }
+
+
+
+
     }
 }
