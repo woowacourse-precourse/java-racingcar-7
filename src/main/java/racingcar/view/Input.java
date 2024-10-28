6dfc00a14004dde35fcac7b9fcce1168cf8b4c42
rndnjsgud <00kwh@naver.com>
2024-10-28 17:39:54 +0900

    feat: Input 예외처리 구현 자동차의 이름이 6자 이상인 경우, 자동차 이름이 정상적으로 입력되지 않는 경우, 게임 횟수가 정수가 아닌 경우 예외처리 구현 이를 위해 isInteger 메소드와 isNameShorterThan5 메소드 구현

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -9,17 +9,46 @@ public class Input {
     private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분";
     private static final String INPUT_GAME_NUMBER = "시도할 횟수는 몇 회인가요?";
 
-    public static List<String> inputCarName(){
+    public static List<String> inputCarName() throws IllegalArgumentException{
         System.out.println(INPUT_CAR_NAME);
-        List<String> carNames = Arrays.stream(readLine()
+        List<String> nameOfCars = Arrays.stream(readLine()
                         .split(","))
                 .toList();
-        return carNames;
+        if(nameOfCars.size() == 0){
+            throw new IllegalArgumentException("자동차 입력이 올바르지 않습니다.");
+        }
+
+        for(String nameOfCar: nameOfCars){
+           if(!isNameShorterThan5(nameOfCar)){
+               throw new IllegalArgumentException("자동차의 이름은 다섯 글자 이하여야 합니다.");
+           }
+        }
+
+        return nameOfCars;
     }
 
     public static int inputGameNumber(){
         System.out.println(INPUT_GAME_NUMBER);
         String numberOfGames = readLine();
+        if(!isInteger(numberOfGames)){
+            throw new IllegalArgumentException("정수만 입력하여야 합니다.");
+        }
         return Integer.parseInt(numberOfGames);
+    }
+
+    public static boolean isInteger(String number) {
+        try {
+            Integer.parseInt(number);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public static boolean isNameShorterThan5(String carName){
+        if(carName.length() > 5){
+            return false;
+        }
+        return true;
     }
 }
