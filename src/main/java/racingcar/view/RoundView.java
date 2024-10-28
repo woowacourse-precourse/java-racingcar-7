e68b7838f82a822fd27380fb390b3820ddcaa04a
kimsehw <seunghwan9703@gmail.com>
2024-10-28 19:10:08 +0900

    refactor(RoundView): RacingGame 변경에 따른 구조 변경
    RacingGame 이 records 를 외부로 전달 받음에 따라 RoundView 도 외부에서 전달 받아 처리하도록 수정합니다.
    RacingGame 에 대한 의존도를 낮춥니다.

--- src/main/java/racingcar/view/RoundView.java
+++ src/main/java/racingcar/view/RoundView.java
@@ -3,20 +3,18 @@ package racingcar.view;
 import static racingcar.message.RoundMessage.ROUND_START_MESSAGE;
 
 import java.util.List;
-import racingcar.model.RacingGame;
 import racingcar.model.RacingRecord;
 
 public class RoundView implements View {
 
-    private final RacingGame racingGame;
+    private final List<RacingRecord> records;
     private boolean isStart = true;
 
-    public RoundView(RacingGame racingGame) {
-        this.racingGame = racingGame;
+    public RoundView(List<RacingRecord> records) {
+        this.records = records;
     }
 
     public void printRound() {
-        List<RacingRecord> records = racingGame.getRecords();
         StringBuilder printResult = new StringBuilder();
 
         for (RacingRecord record : records) {
