cc0202ae377437ad34e06e28a8c1f80d8f508d17
youjae <youjae0803@gmail.com>
2024-10-28 17:10:28 +0900

    feat(OutputView): 우승자를 출력하는 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -9,24 +9,25 @@ public class OutputView {
     private static final String NOTICE_STATEMENT_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String NOTICE_STATEMENT_CNT = "시도할 횟수는 몇 회인가요?";
     private static final String NOTICE_STATEMENT_RESULTS = "실행 결과";
+    private static final String NOTICE_STATEMENT_WINNER = "최종 우승자";
 
     public void printNoticeStatementName() {
         System.out.println(NOTICE_STATEMENT_NAME);
     }
 
-    public void printNoticeStatementCnt(){
+    public void printNoticeStatementCnt() {
         System.out.println(NOTICE_STATEMENT_CNT);
     }
 
-    public void printRaceResults(List<RaceResult> raceResults){
-        for (RaceResult raceResult : raceResults) {
+    public void printRaceResults(List<RaceResult> raceResults) {
+        System.out.println(NOTICE_STATEMENT_RESULTS);
 
+        for (RaceResult raceResult : raceResults) {
+            printRaceResult(raceResult);
         }
     }
 
     public void printRaceResult(RaceResult raceResult) {
-        System.out.println(NOTICE_STATEMENT_RESULTS);
-
         List<Car> cars = raceResult.getCars();
 
         for (Car car : cars) {
@@ -34,6 +35,7 @@ public class OutputView {
 
             System.out.println(line);
         }
+
         System.out.println();
     }
 
@@ -42,6 +44,24 @@ public class OutputView {
         for (int i = 0; i < car.getPosition(); i++) {
             display.append("-");
         }
+        return display.toString();
+    }
+
+    public void printWinner(List<String> names) {
+        String line = getWinnerDisplay(names);
+        System.out.println(line);
+    }
+
+    private String getWinnerDisplay(List<String> names) {
+        StringBuilder display = new StringBuilder(NOTICE_STATEMENT_WINNER + " :");
+
+        for (int i = 0; i < names.size(); i++) {
+            display.append(" ").append(names.get(i));
+            if (i < names.size() - 1) {
+                display.append(",");
+            }
+        }
+
         return display.toString();
     }
 }
