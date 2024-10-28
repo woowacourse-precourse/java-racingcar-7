19ce1bfd9bd561db286c1537791749bd85bdc6da
Jinyshin <iwbm312@gmail.com>
2024-10-28 12:53:02 +0900

    feat(output): 자동차 경주 차수별 실행 결과 출력

--- src/main/java/racingcar/io/OutputHandler.java
+++ src/main/java/racingcar/io/OutputHandler.java
@@ -1,4 +1,16 @@
 package racingcar.io;
 
+import java.util.List;
+import racingcar.Car;
+
 public class OutputHandler {
+    // TODO: 차수별 실행 결과를 출력할 때 자동차 이름을 같이 출력한다.
+
+    public void printTrialResult(List<Car> cars) {
+        System.out.println("실행 결과");
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
 }
