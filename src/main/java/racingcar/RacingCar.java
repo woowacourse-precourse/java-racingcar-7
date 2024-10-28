afeb1cd03d3fd02c8b9a1231cc8e6fa6a64554a1
dldb-chamchi <dladkfl49@gmail.com>
2024-10-28 20:20:06 +0900

    fix: RacingCar constructor throws exception condition

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -4,7 +4,7 @@ public class RacingCar {
     public final String carName;
     public int numberOfForward = 0;
     public RacingCar(String carName){
-        if(carName == null || carName.length() > 5 || carName.trim().isEmpty()) throw new IllegalArgumentException();
+        if(carName == null || carName.length() > 5 || carName.trim().isEmpty()) throw new IllegalArgumentException(); //null, 5초과 길이, 빈 문자열
         this.carName = carName;
     }
 
