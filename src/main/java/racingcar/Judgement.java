8fdd02cfe93f52a364a3a8f9fd66f44a00f72ab7
bk <naturalbb360@gmail.com>
2024-10-28 13:14:59 +0900

    refactor(Judgement): 클래스명 수정 및 인스턴스변수에 접근제어자 추가

--- src/main/java/racingcar/Judgement.java
+++ src/main/java/racingcar/Judgement.java
@@ -3,26 +3,27 @@ package racingcar;
 import java.util.ArrayList;
 import java.util.List;
 
-public class judgement {
-    Car[] cars;
+public class Judgement {
 
-    public judgement(Car[] cars){
+    private Car[] cars;
+
+    public Judgement(Car[] cars) {
         this.cars = cars;
     }
 
-    private void printResult(Car curCar){
-        int curCarLocation = curCar.currentLocation();
+    private void printResult(Car curCar) {
+        int curCarLocation = curCar.getcurrentLocation();
         System.out.print(curCar.name() + " : ");
-        for(int count = 0; count < curCarLocation; count++){
+        for (int count = 0; count < curCarLocation; count++) {
             System.out.print("-");
         }
         System.out.println();
     }
 
-    public void updateResult(ArrayList<Integer> randomNumbers){
-        for(int carIndex = 0; carIndex < cars.length; carIndex++){
+    public void updateResult(List<Integer> randomNumbers) {
+        for (int carIndex = 0; carIndex < cars.length; carIndex++) {
             Car curCar = cars[carIndex];
-            if(randomNumbers.get(carIndex) > 3){
+            if (randomNumbers.get(carIndex) > 3) {
                 curCar.move();
             }
             printResult(curCar);
@@ -30,32 +31,32 @@ public class judgement {
         System.out.println();
     }
 
-    private List<String> judgeWinner(){
+    private List<String> judgeWinner() {
         List<String> winnerList = new ArrayList<>();
-        int maxLocationNum = cars[0].currentLocation();
+        int maxLocationNum = cars[0].getcurrentLocation();
         winnerList.add(cars[0].name());
 
-        for(int i =1; i<cars.length; i++){
+        for (int i = 1; i < cars.length; i++) {
             Car curCar = cars[i];
-            if (curCar.currentLocation() > maxLocationNum){
-                maxLocationNum = curCar.currentLocation();
+            if (curCar.getcurrentLocation() > maxLocationNum) {
+                maxLocationNum = curCar.getcurrentLocation();
                 winnerList.clear();
                 winnerList.add(curCar.name());
                 continue;
             }
-            if(curCar.currentLocation() == maxLocationNum){
+            if (curCar.getcurrentLocation() == maxLocationNum) {
                 winnerList.add(curCar.name());
             }
         }
         return winnerList;
     }
 
-    public void printWinner(){
-        System.out.print("최종 우승자 : " );
+    public void printWinner() {
+        System.out.print("최종 우승자 : ");
         List<String> winnerList = judgeWinner();
-        for(int i=0; i < winnerList.size(); i++){
+        for (int i = 0; i < winnerList.size(); i++) {
             System.out.print(winnerList.get(i));
-            if(i != winnerList.size() - 1){
+            if (i != winnerList.size() - 1) {
                 System.out.print(", ");
             }
         }
