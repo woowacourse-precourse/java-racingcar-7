4338277bb8190f43e2d76a6eda85661a9ad5e73e
mikekks <thdalsrb79@naver.com>
2024-10-28 20:33:41 +0900

    refactor(Turn): 시도횟수가 자연수가 아닌 경우 예외 처리

--- src/main/java/racingcar/domain/Turn.java
+++ src/main/java/racingcar/domain/Turn.java
@@ -1,13 +1,21 @@
 package racingcar.domain;
 
 public class Turn {
+	private static final int ZERO = 0;
 	private final int count;
 
 	public Turn(int count) {
+		validateTurn(count);
 		this.count = count;
 	}
 
-	public int getTurnCount(){
+	public int getTurnCount() {
 		return count;
+	}
+
+	private void validateTurn(int count) {
+		if(count <= ZERO){
+			throw new IllegalArgumentException("시도 횟수는 자연수만 가능합니다.");
+		}
 	}
 }
