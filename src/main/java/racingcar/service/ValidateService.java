9c31b1bb6150e76369a5aa474e08bde82f16899f
yeseong33 <ohyeesung@naver.com>
2024-10-28 20:16:13 +0900

    refect: 예외 사항 및 구조
    - 이름 중복 금지 추가
    - validateService 구조 변경
    - 한 메서드에 하나의 역할만 하도록 재구성
    - 의미 없는 주석 제거

--- src/main/java/racingcar/service/ValidateService.java
+++ src/main/java/racingcar/service/ValidateService.java
@@ -1,65 +1,81 @@
 package racingcar.service;
 
-import racingcar.util.ConditionConstant;
-import racingcar.util.ErrorMessage;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static racingcar.util.ConditionConstant.MAX_LENGTH;
 import static racingcar.util.ErrorMessage.*;
 
 public class ValidateService {
 
-    long validateRaceCount;
-    List<String> rawNames, validatedNames;
-
-    public ValidateService() {
-        this.validatedNames = new ArrayList<>();
-        this.rawNames = new ArrayList<>();
-    }
 
     public List<String> validateName(String names) {
-        rawNames = Arrays.stream(names.split(",")).toList();
+        List<String> validatedNames = new ArrayList<>();
+
+        List<String> rawNames = Arrays.stream(names.split(",")).toList();
+
+        validateBlankInput(rawNames);
 
         rawNames.forEach(rawName -> {
             String trimmedName = rawName.trim();
-            // 공백처리
-            if (trimmedName.isEmpty()) {
-                throw new IllegalArgumentException(ERROR_BLANK_CAR_NAME);
-            }
 
-            // 길이 제한
-            if (trimmedName.length() > MAX_LENGTH) {
-                throw new IllegalArgumentException(ERROR_CAR_NAME_LENGTH);
-            }
+            validateBlankInput(trimmedName);
 
-            // 유효한 이름
+            validateNameLength(trimmedName);
+
             validatedNames.add(trimmedName);
         });
         return validatedNames;
     }
 
+    public List<String> validateDuplicateNames(List<String> processingName) {
+        Set<String> nameSet = new HashSet<>();
+        for (String name : processingName) {
+            if (!nameSet.add(name)) {
+                throw new IllegalArgumentException(ERROR_DUPLICATE_NAME);
+            }
+        }
+        return processingName;
+    }
+
+    public void validateNameLength(String name) {
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(ERROR_CAR_NAME_LENGTH);
+        }
+    }
+
     public long validateRaceCount(String raceCount) {
         String trimmedRaceCount = raceCount.trim();
 
-        // 공백 입력
-        if (trimmedRaceCount.isEmpty()) {
-            throw new IllegalArgumentException(ERROR_BLANK_INPUT);
-        }
+        validateBlankInput(raceCount);
 
+        return validateNumber(trimmedRaceCount);
+
+    }
+
+    private long validateNumber(String trimmedRaceCount) {
         try {
             int rawRaceCount = Integer.parseInt(trimmedRaceCount);
 
             if (rawRaceCount <= 0) {
                 throw new IllegalArgumentException();
             }
-            validateRaceCount = rawRaceCount;
+            return rawRaceCount;
         } catch (Exception e) {
             // 음수 확인, 숫자로 변경 할 수 없을 경우
             throw new IllegalArgumentException(ERROR_INVALID_NUMBER_INPUT);
         }
-        return validateRaceCount;
     }
+
+    private void validateBlankInput(List<String> values) {
+        if (values == null || values.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_BLANK_INPUT);
+        }
+    }
+
+    private void validateBlankInput(String value) {
+        if (value == null || value.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_BLANK_INPUT);
+        }
+    }
+
 }
