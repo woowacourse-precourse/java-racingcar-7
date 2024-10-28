fe82b4296b261c16e8d972cde91041280f68f12d
woojin cheon <qpqpqp0102@naver.com>
2024-10-28 18:34:28 +0900

    fix(InputView) : correct method for car names input - correct method to receive multiple names

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -2,14 +2,20 @@ package racingcar.View;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import racingcar.View.Model.RacingCar;
+import racingcar.Model.RacingCar;
 
 public class InputView {
 
     public List<RacingCar> inputCarNames() {
-        List<RacingCar> carList = new ArrayList<RacingCar>();
-        carList.add(new RacingCar(0, Console.readLine()));
+        List<RacingCar> carList = new ArrayList<>();
+
+        Arrays.stream(Console.readLine().split(","))
+            .map(name -> new RacingCar(0, name))
+            .distinct()
+            .forEach(carList::add);
+
         return carList;
     }
 
