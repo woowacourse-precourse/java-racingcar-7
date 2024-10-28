976030a633610a06ec60d57f43fe7105a382ea29
masee1994 <masee1994@gmail.com>
2024-10-28 20:29:04 +0900

    feat: 유효성 검사 메소드 생성 및 예외 처리 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,11 +7,17 @@ import java.util.Collections;
 import java.util.List;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        // 경주에 참가하는 자동차이름 입력
+        // 경주에 참가하는 자동차 이름 입력
         String input_players = Console.readLine();
+
+        // 입력된 자동차 이름에 대한 유효성 검사, 잘못된 값 입력시 IllegalArgumentException을 발생
+        if (!ValidChk_players(input_players)) {
+            throw new IllegalArgumentException();
+        }
 
         // 쉼표(,)기준으로 이름을 구분한 후 players에 저장 후 각각의 전진 횟수를 기록하는 scores 생성
         ArrayList<String> players = new ArrayList<>(List.of(input_players.split(",")));
@@ -20,6 +26,12 @@ public class Application {
         System.out.println("시도할 횟수는 몇 회인가요?");
         // 경주 횟수 입력
         String input_game = Console.readLine();
+
+        // 입력된 경주 횟수에 대한 유효성 검사, 잘못된 값 입력시 IllegalArgumentException을 발생
+        if (!ValidChk_game(input_game)) {
+            throw new IllegalArgumentException();
+        }
+
         int n = Integer.parseInt(input_game);
         // 우승 스코어 초기화
         int win_score = 0;
@@ -53,7 +65,42 @@ public class Application {
         }
         // 최송 우승자 혹은 공동 우승자 뒤에 붙은 쉼표(',') 제거
         winner = winner.substring(0, winner.length() - 2);
+
         // 최종 우승자 혹은 공동 우승자 출력
         System.out.println(winner);
     }
+
+    // 자동차 이름 입력에 대한 유효성 검사 메소드
+    private static boolean ValidChk_players(String input) {
+        // 널값(Null)이거나 공백을 제거했는데도 빈 문자열("")일 경우 false를 반환
+        if (input == null || input.trim().isEmpty()) {
+            return false;
+        }
+        // 입력받은 값중에 이름이 5자를 초과할 경우 false를 반환
+        ArrayList<String> players = new ArrayList<>(List.of(input.split(",")));
+        for (String player : players) {
+            if (player.trim().isEmpty() || player.length() > 5) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 경기 횟수 입력에 대한 유효성 검사 메소드
+    private static boolean ValidChk_game(String input) {
+        try {
+            // 공백을 제거했는데 0 이상의 정수일 경우 true, 음의 정수일 경우 false를 반환
+            int number = Integer.parseInt(input.trim());
+            if (number >= 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        //숫자가 아닐경우 false를 반환
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
 }
