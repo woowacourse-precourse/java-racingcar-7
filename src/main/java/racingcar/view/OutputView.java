4af9c84f5659ecb6c4abf9519a416146de725a21
rbgusdlza <rbgusdlza@naver.com>
2024-10-28 22:06:57 +0900

    feat: 실행 결과를 나타내는 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,19 @@
-package calculator.view;
+package racingcar.view;
 
-public class OutputView {
+import racingcar.Cars;
 
-    public static void show(int result) {
-        System.out.println("결과 : " + result);
+import java.util.List;
+
+public abstract class OutputView {
+
+    private OutputView() {
+    }
+
+    public static void showRacing(Cars cars) {
+        System.out.println("실행 결과 : ");
+    }
+
+    public static void showResult(List<String> winnerNames) {
+        System.out.println("최종 우승자 : " + String.join(", ", winnerNames));
     }
 }
