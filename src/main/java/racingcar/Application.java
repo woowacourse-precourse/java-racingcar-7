c2977fc499c3a09b948da63b396f33a592562de5
201912025 <michi2012@naver.com>
2024-10-28 23:46:18 +0900

    refactor(Application): 필요없는 의존성 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,11 +2,7 @@ package racingcar;
 
 import racingcar.controller.RacingCarController;
 import racingcar.factory.RacingCarFactory;
-import racingcar.service.RacingCarService;
-import racingcar.validator.RacingCarNameValidator;
-import racingcar.validator.RacingCarRepeatCountValidator;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         RacingCarController racingCarController = RacingCarFactory.createRacingCarController();
