c686e05489eda13dca1065fbbbb3f488a0233b9b
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-10-28 21:10:02 +0900

    feat: Add race logic into separate methods
    - runRace 메서드 생성
    - 업데이트와 실행 내역을 보여줄updateAndDisplayRaceResults 메서드를 생성
    - runRace 내부 함수를 통해 시도 횟수만큼 updateAndDisplayRaceResults 를 통해 race를 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,27 @@ public class Application {
         String[] carNames = getCarNames();
         Integer attempts = getNumberOfAttempts();
         List<StringBuilder> raceResults = initializeRaceResults(carNames);
+
+        runRace(attempts, carNames, raceResults);
+    }
+
+    private static void runRace(int attempts, String[] carNames, List<StringBuilder> raceResults) {
+        System.out.println("실행 결과");
+        Integer carNamesLength = carNames.length;
+        for (int i = 0; i < attempts; i++) {
+            updateAndDisplayRaceResults(carNames, raceResults);
+        }
+    }
+
+    private static void updateAndDisplayRaceResults(String[] carNames, List<StringBuilder> raceResults) {
+        int carNamesLength = carNames.length;
+        for (int i = 0; i < carNamesLength; i++) {
+            StringBuilder nowRaceCarResult = raceResults.get(i);
+            if (Randoms.pickNumberInRange(0, 9) >= 4) { // 4 이상일 때 전진
+                nowRaceCarResult.append("-");
+            }
+            System.out.println(carNames[i] + " : " + nowRaceCarResult);
+        }
     }
 
     private static String[] getCarNames(){
