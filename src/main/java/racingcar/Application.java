22b6a425a253ace524879c6ad0022f9750d4d2f4
GitDohyun <GitDohyun@gmail.com>
2024-10-28 15:56:10 +0900

    fix(Application): main 메서드에 try-catch 블록 추가
    Application파일의 main 메서드에 뺴먹은 try-catch 블럭을 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,14 +4,18 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        InputReader inputReader = new InputReader();
-        List<String> carNames = inputReader.readCarNames();
-        int tryCount = inputReader.readTryCount();
+        try {
+            InputReader inputReader = new InputReader();
+            List<String> carNames = inputReader.readCarNames();
+            int tryCount = inputReader.readTryCount();
 
-        Racing racing = new Racing(carNames);
-        racing.start(tryCount);
+            Racing racing = new Racing(carNames);
+            racing.start(tryCount);
 
-        Winner winner = new Winner();
-        winner.printWinner(racing.positions());
+            Winner winner = new Winner();
+            winner.printWinner(racing.positions());
+        } catch (RuntimeException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 }
