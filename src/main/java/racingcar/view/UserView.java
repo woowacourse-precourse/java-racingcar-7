d32c0ad80e53c2cf4bc2e01fe746d3207f3b8e1d
Gothax <scott990328@gmail.com>
2024-10-28 17:18:08 +0900

    fix: 이름 문자열 공백 예외 추가
    test 코드 통과못한 부분 예외처리 - view처리

--- src/main/java/racingcar/view/UserView.java
+++ src/main/java/racingcar/view/UserView.java
@@ -3,6 +3,7 @@ package racingcar.view;
 import racingcar.controller.PlayStation;
 import racingcar.service.Game;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
@@ -20,11 +21,30 @@ public class UserView {
 
     public static List<String> printAndGetNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)");
-        return List.of(readLine().split(","));
+        return getListAndCheck(readLine());
     }
+
+    private static List<String> getListAndCheck(String s) {
+        List<String> names = Arrays.asList(s.split(","));
+        for (String name : names) {
+            if (name.length() > 5 || name.isEmpty() || name.isBlank()) {
+                throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+            }
+        }
+        return names;
+    }
+
 
     public static Integer printAndGetRound() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return Integer.parseInt(readLine());
+        return getParsedIntAndCheck();
+    }
+
+    private static int getParsedIntAndCheck() {
+        int parsedInt = Integer.parseInt(readLine());
+        if (parsedInt < 1) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+        }
+        return parsedInt;
     }
 }
