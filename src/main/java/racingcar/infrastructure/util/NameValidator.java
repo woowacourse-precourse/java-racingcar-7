b2dfbe82b2144288defecf789feb327008538e6a
jijongkwon <help3451@naver.com>
2024-10-28 21:59:12 +0900

    feat(util) : 숫자 형식이 맞는지 확인하는 메서드 구현

--- src/main/java/racingcar/infrastructure/util/NameValidator.java
+++ src/main/java/racingcar/infrastructure/util/NameValidator.java
@@ -6,11 +6,20 @@ import java.util.List;
 
 public class NameValidator {
 
-    public void validRacingCarName(List<String> names){
+    public static void validRacingCarName(List<String> names){
         for(String carName : names){
             if(carName.length() > 5){
                 throw new IllegalArgumentException(ExceptionMessage.OVER_5_CAR_NAME_LENGTH.getMessage());
             }
+        }
+    }
+
+    public static int validNumber(String number){
+        try{
+            return Integer.parseInt(number);
+        }
+        catch (NumberFormatException e){
+            throw new IllegalArgumentException(ExceptionMessage.NOT_VALID_NUMBER.getMessage());
         }
     }
 }
--- src/main/java/racingcar/presentation/enums/ExceptionMessage.java
+++ src/main/java/racingcar/presentation/enums/ExceptionMessage.java
@@ -2,7 +2,8 @@ package racingcar.presentation.enums;
 
 public enum ExceptionMessage {
     EMPTY_NAME_EXCEPTION("이름이 비어있습니다."),
-    OVER_5_CAR_NAME_LENGTH("이름의 길이가 5 초과입니다.");
+    OVER_5_CAR_NAME_LENGTH("이름의 길이가 5 초과입니다."),
+    NOT_VALID_NUMBER("유효하지 않은 숫자 입니다.");
 
     private final String message;
 
