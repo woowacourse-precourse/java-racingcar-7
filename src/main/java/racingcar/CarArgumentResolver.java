e7f746632129151959521c97dcaaf81b5cf93f80
tmdcheol <leesc08392@gmail.com>
2024-10-28 20:14:17 +0900

    feat: 자동차 이름 입력이 존재하지 않을 때 검증 및 기존 split 함수 일부 기능 수정
    - 입력값이 null이거나 비어 있을 경우 예외 발생하도록 checkBlank 메서드 추가
    - splitWithBlankAndTrim 메서드에서 입력 문자열을 ','로 분리하고, 각 항목의 공백을 제거하여 리스트 반환하도록 수정
    - 구분자가 앞 뒤에 있는 경우는 빈 문자열을 포함하여 리턴하도록 작성
      - 빈 문자열은 다른 클래스에서 예외 처리
    
    - CarArgumentResolverTest 테스트 코드 작성
      - null 및 빈 문자열 입력 시 예외 발생 테스트
      - 일반적인 ',' 구분자 기준 파싱, 앞뒤 공백 제거 후 파싱, 구분자가 앞/뒤에 있는 경우의 파싱 테스트

--- src/main/java/racingcar/CarArgumentResolver.java
+++ src/main/java/racingcar/CarArgumentResolver.java
@@ -5,9 +5,23 @@ import java.util.List;
 
 public class CarArgumentResolver {
 
+    private static final int NEGATIVE = -1;
+
     public List<String> parse(String parameter) {
-        String[] carNameArray = parameter.split(",");
-        return Arrays.asList(carNameArray);
+        checkBlank(parameter);
+        return splitWithBlankAndTrim(parameter);
+    }
+
+    private List<String> splitWithBlankAndTrim(String parameter) {
+        return Arrays.stream(parameter.split(",", NEGATIVE))
+                .map(String::trim)
+                .toList();
+    }
+
+    private void checkBlank(String parameter) {
+        if (parameter == null || parameter.isBlank()) {
+            throw new IllegalArgumentException("비어있는 입력은 불가능합니다.");
+        }
     }
 
 }
--- src/test/java/racingcar/CarArgumentResolverTest.java
+++ src/test/java/racingcar/CarArgumentResolverTest.java
@@ -1,13 +1,15 @@
 package racingcar;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarArgumentResolverTest {
+
+    private static final String ERROR_MESSAGE = "비어있는 입력은 불가능합니다.";
 
     CarArgumentResolver carArgumentResolver = new CarArgumentResolver();
 
@@ -21,9 +23,69 @@ class CarArgumentResolverTest {
         List<String> carNames = carArgumentResolver.parse(rawCarNames);
 
         // then
-        Assertions.assertThat(carNames).contains("a");
-        Assertions.assertThat(carNames).contains("b");
-        Assertions.assertThat(carNames).contains("c");
+        assertThat(carNames).contains("a");
+        assertThat(carNames).contains("b");
+        assertThat(carNames).contains("c");
+    }
+
+    @Test
+    @DisplayName("앞뒤로 공백이 있을 경우, 공백을 제거하고 정상 parse")
+    public void parse_trim_테스트() {
+        // given
+        String rawCarNames = " a , b , c ";
+
+        // when
+        List<String> carNames = carArgumentResolver.parse(rawCarNames);
+
+        // then
+        assertThat(carNames).contains("a");
+        assertThat(carNames).contains("b");
+        assertThat(carNames).contains("c");
+    }
+
+    @Test
+    @DisplayName("구분자가 앞에 있을 경우 살려서 정상 parse")
+    public void parseSplitWithBlankFront_테스트() {
+        // given
+        String rawCarNames = ",b";
+
+        // when
+        List<String> carNames = carArgumentResolver.parse(rawCarNames);
+
+        // then
+        assertThat(carNames).contains("");
+        assertThat(carNames).contains("b");
+    }
+
+    @Test
+    @DisplayName("구분자가 뒤에 있을 경우 살려서 정상 parse")
+    public void parseSplitWithBlankBack_테스트() {
+        // given
+        String rawCarNames = "b,";
+
+        // when
+        List<String> carNames = carArgumentResolver.parse(rawCarNames);
+
+        // then
+        assertThat(carNames).contains("");
+        assertThat(carNames).contains("b");
+    }
+
+    @Test
+    @DisplayName("비어있거나 null 값이 입력으로 주어질 때 에러 발생")
+    void parse_비어있는_값이_입력으로_주어질_때() {
+        // given
+        String nullString = null;
+        String emptyString = "";
+
+        // when & then
+        assertThatThrownBy(() -> carArgumentResolver.parse(nullString))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE);
+
+        assertThatThrownBy(() -> carArgumentResolver.parse(emptyString))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE);
     }
 
 }
