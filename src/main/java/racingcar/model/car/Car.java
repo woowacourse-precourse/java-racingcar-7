46b081dc17e418954e2864e0fc6d49b24fa20d02
SinnoLn <sintmtn69@gmail.com>
2024-10-28 12:50:22 +0900

    feat(model): Car 객체 생성 및 이동 기능 구현

---
+++ src/main/java/racingcar/model/car/Car.java
@@ -0,0 +1,33 @@
+package racingcar.model.car;
+
+import racingcar.utils.ValidationUtils;
+
+public class Car {
+    private final String name;
+    private int position;
+    private final MoveStrategy moveStrategy;
+
+    public Car(
+            String name,
+            MoveStrategy moveStrategy
+    ) {
+        ValidationUtils.validateCarNames(new String[]{name});
+        this.name = name;
+        this.moveStrategy = moveStrategy;
+        this.position = 0;
+    }
+
+    public void move() {
+        if(moveStrategy.canMove()) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
