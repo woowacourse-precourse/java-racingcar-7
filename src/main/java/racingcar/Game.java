70bfc46fae02484eccf449fdbbd4195ef4dbd14b
songkyung <min.songkyung@gmail.com>
2024-10-28 18:49:23 +0900

    small fix2

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -18,6 +18,7 @@ public class Game {
 
     // 경주 시작
     public void startRace() {
+        System.out.println();
         System.out.println("실행 결과");
         for (int i=0; i<raceCount; i++) {
             randomMove();
