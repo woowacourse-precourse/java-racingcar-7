37dbe082fdf359c1fbc1e3f98a254b435271e04c
GaBaljaintheroom <junsu1222@naver.com>
2024-10-28 16:11:18 +0900

    docs(Car): 추상 클래스 javadoc 추가

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -12,8 +12,22 @@ public abstract class Car {
         this.position = position;
     }
 
+    /**
+     * 자동차의 위치를 변경합니다. 이 메서드는 구체적인 자동차 클래스에서 구현되어야 합니다.
+     *
+     * @implSpec
+     * 구현 클래스는 자동차의 이동 거리를 결정하여 position 필드의 값을 변경해야 합니다.
+     */
     abstract public void move();
 
+    /**
+     * 자동차의 우승 여부를 반환합니다. 이 메서드는 구체적인 자동차 클래스에서 구현되어야 합니다.
+     *
+     * @param maxPosition 현재 레이스의 최대 위치
+     * @return 최대 위치와 비교하여 우승했으면 True, 못했으면 False
+     * @implSpec
+     * 구현 클래스는 자동차의 최대 위치를 확인하여 판별합니다.
+     */
     abstract public boolean isWinner(int maxPosition);
 
     public int getPosition() {
