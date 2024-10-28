ceb6092e4e7d37e982e576aea4bfcdc59906a1b6
na0th <105775683+na0th@users.noreply.github.com>
2024-10-28 17:09:55 +0900

    feat : 이동 횟수 0은 예외처리
    추가한 내용 :
    정규표현식으로 이동 횟수 0은 예외처리
    -> 혼자 달리면 Race가 아니라 생각

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -11,15 +11,15 @@ public class Validator {
     private static final String NAME_DELIMITER = ",";
 
     private static final String VALID_NAME_PATTERN = "^[a-zA-Z0-9]+(,[a-zA-Z0-9]+)*$";
-    private static final String POSITIVE_INTEGER_PATTERN = "^[0-9]+$";
+    private static final String POSITIVE_INTEGER_PATTERN = "^[1-9][0-9]*$";
 
     private static final String VALIDATE_NAME_PATTERN_MSG = "자동차 이름 입력 형식은 , 단위로 작성해주십시오";
+    private static final String DUPLICATE_NAME_MSG = "자동차 이름은 중복될 수 없습니다.";
     private static final String MAX_NAME_SIZE_MSG = "이름은 최대 5자까지 가능합니다.";
     private static final String VALIDATE_POSITIVE_INTEGER_MSG = "이동 횟수는 0 이상의 정수만 가능합니다.";
-    private static final String DUPLICATE_NAME_MSG = "자동차 이름은 중복될 수 없습니다.";
     private static final String MINIMUM_CAR_COUNT_MSG = "최소한 두 대 이상의 자동차가 필요합니다.";
     private static final String MAX_ROUND_LIMIT_DUE_TO_PERFORMANCE = "성능의 문제상 최대 라운드인 10000 라운드를 넘을 수 없습니다.";
-    //이름 중복
+
     public void validateAllInput(String inputCarNames, String inputCarRoundCount) {
         validateDelimitedNameFormat(inputCarNames);
         validateDuplicateNames(inputCarNames);
