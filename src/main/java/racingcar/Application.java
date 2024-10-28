547aa1644667f1fa2867fa3bee16cb9135d25f29
huipadyam <cekim3197@ewhain.net>
2024-10-28 20:45:32 +0900

    Feat(car): Add Car class with name, position fields and movement methods

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,4 +4,30 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    private static class Car {
+        private final String name;
+        private int position;
+
+        public Car(String name) {
+            this.name = name;
+            this.position = 0;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getPosition() {
+            return position;
+        }
+
+        public void move() {
+            position++;
+        }
+
+        public String getPositionDisplay() {
+            return "-".repeat(position);
+        }
+    }
 }
