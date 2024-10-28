9c6f5b0442ba067e966c8097c6860dee0022fe7f
Jungwoo Chu <cjw980930@gmail.com>
2024-10-28 23:39:08 +0900

    test: 자동차 이름 같을 경우, 자동차 공백 처리 테스트 작성

--- src/main/java/racingcar/CarValidator.java
+++ src/main/java/racingcar/CarValidator.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,14 +10,15 @@ public class CarValidator {
         for (int i = 0; i < carList.size(); i++) {
             carList.set(i, carList.get(i).trim());
             if (carList.get(i).length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 최대 5자 이내여야 합니다. ");
+                throw new IllegalArgumentException("자동차 이름은 최대 5자 이내여야 합니다.");
             }
         }
+        validateSameName(carList);
         return carList;
     }
 
-    public void validateSameName(List<String> carList){
-        List<String> nameCheck = carList;
+    public static void validateSameName(List<String> carList){
+        List<String> nameCheck = new ArrayList<>();
         for (String car : carList){
             if (nameCheck.contains(car)){
                 throw new IllegalArgumentException("자동차 이름 중 겹치는 이름이 있습니다.");
--- src/test/java/racingcar/CarValidatorTest.java
+++ src/test/java/racingcar/CarValidatorTest.java
@@ -27,6 +27,35 @@ class CarValidatorTest {
 
 
         // Then
-        Assertions.assertThatThrownBy(() -> CarValidator.validateNames(input)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> CarValidator.validateNames(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 최대 5자 이내여야 합니다.");
+    }
+
+    @Test
+    void blankValidator() {
+        // Given
+        String input = "pobi , jun ";
+
+        // When
+        List<String> result = CarValidator.validateNames(input);
+
+        // Then
+        Assertions.assertThat(result).contains("pobi", "jun");
+    }
+
+
+    @Test
+    void sameNameException() {
+        // Given
+        String input = "pobi,pobi";
+
+        // When
+
+
+        // Then
+        Assertions.assertThatThrownBy(() -> CarValidator.validateNames(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름 중 겹치는 이름이 있습니다.");
     }
 }
