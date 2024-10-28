e3d99d4e388965cf628d116af2a5e86d213a60a5
gladhee <gladhee@icloud.com>
2024-10-28 15:25:59 +0900

    fix(ViewMessages): 개행 위치 수정

--- src/main/java/racingcar/view/ViewMessages.java
+++ src/main/java/racingcar/view/ViewMessages.java
@@ -3,8 +3,8 @@ package racingcar.view;
 public enum ViewMessages {
 
     CAR_INPUT_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    TRY_COUNT_INPUT_MESSAGE("\n시도할 회수는 몇 회인가요?"),
-    OUTPUT_MESSAGE("실행 결과"),
+    TRY_COUNT_INPUT_MESSAGE("시도할 회수는 몇 회인가요?"),
+    OUTPUT_MESSAGE("\n실행 결과"),
     WINNER_OUTPUT_MESSAGE("최종 우승자 : ");
 
     private final String message;
