4ab0642eb2c54a898e549075a3b15f7e0f239eb9
yejin923 <yejin923@naver.com>
2024-10-28 16:47:48 +0900

    FEAT: 입력 메소드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static String[] input(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String line = Console.readLine().trim();
+        String[] carName = line.split(",");
+        return carName;
     }
 }
