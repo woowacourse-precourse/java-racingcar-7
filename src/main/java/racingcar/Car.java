52ff815cb29fe24c6257028ff24c4463b053b7b8
kimch <rlacogk6577@daum.net>
2024-10-28 20:32:13 +0900

    feat(Car): 자동차 생성 기능 추가

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    public int move;
+    public String name;
+
+    public Car(String name){
+        this.name = name;
+        move = 0;
+    }
+
+}
