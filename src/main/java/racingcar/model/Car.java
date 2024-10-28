a0e6bd2b640da34b27bf832f1a686018c3c6feb6
mingyeongwon <wonmingy@gmail.com>
2024-10-28 23:14:48 +0900

    feat: 우승자 결정 및 출력
    우승자 : 가장 멀리 전진한 자동차
    우승자 출력

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,12 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
     private final String name;
-    private int position;
+    private int position = 0;
     private static final int MINIMUM_MOVE_VALUE = 4;
 
-    public Car(String name, int position) {
+    public Car(String name) {
         this.name = name;
-        this.position = 0;
     }
 
     public void move() {
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,11 +6,19 @@ import java.util.List;
 
 public class OutputView {
 
-    public static void printGameStatus(List<Car> cars) {
+    public static void printGameStart() {
         System.out.println("\n실행 결과");
+    }
+
+    public static void printGameStatus(List<Car> cars) {
         for (Car car : cars) {
             System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
         }
         System.out.println();
+    }
+
+    public static void printWinners(List<String> winners) {
+        String winnerNames = String.join(".", winners);
+        System.out.println("최종 우승자 : " + winnerNames);
     }
 }
