2f5508ae6b2854714c61f5bf21534eb9d4643d0e
song hyeongyu <lit3207@ajou.ac.kr>
2024-10-28 20:18:08 +0900

    refactor: add validate

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,25 +3,42 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
+    private static final String SYMBOL = "-";
+
     private final String name;
     private int distance = 0;
-    static final String SYMBOL = "-";
+    private StringBuilder sb = new StringBuilder();
+
 
     public Car(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("이름이 5글자 이상입니다.");
+        if (validateName(name)) {
+            throw new IllegalArgumentException("자동차가 존재하지 않습니다.");
         }
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public String getSymbol() {
+        return sb.toString();
     }
 
     public void drive() {
         int random = Randoms.pickNumberInRange(0, 9);
         if (random >= 4) {
             distance += 1;
+            sb.append(SYMBOL);
         }
     }
 
-    public String getSymbol() {
-        return SYMBOL.repeat(distance);
+
+    private boolean validateName(String name) {
+        return name == null || name.trim().isEmpty() || name.length() >= 5;
     }
 }
