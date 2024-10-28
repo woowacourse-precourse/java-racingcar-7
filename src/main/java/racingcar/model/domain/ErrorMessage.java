ed3f8f689046c289525e41bba4e4a0cdcbe4983b
Jihyun3478 <leejihyun3478@gmail.com>
2024-10-28 20:30:16 +0900

    refactor : 사용하지 않는 상수 제거
    사용하지 않는 상수 제거
    - NOT_NUMBER_FORMAT 제거

--- src/main/java/racingcar/model/domain/ErrorMessage.java
+++ src/main/java/racingcar/model/domain/ErrorMessage.java
@@ -5,8 +5,7 @@ public enum ErrorMessage {
 	SINGLE_CAR_NAME("자동차는 최소 2대 이상이어야 합니다."),
 	OVER_CAR_NAME_LENGTH("자동차 이름은 5자 이하만 가능합니다."),
 	DUPLICATE_CAR_NAME("자동차 이름은 중복되지 않아야 합니다."),
-	NEGATIVE_ATTEMPT_NUMBER("시도 횟수는 1 이상이어야 합니다."),
-	NOT_NUMBER_FORMAT("횟수는 숫자로 입력해 주세요.");
+	NEGATIVE_ATTEMPT_NUMBER("시도 횟수는 1 이상이어야 합니다.");
 
 	private final String message;
 
