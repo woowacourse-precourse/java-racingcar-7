4e377ebb8247260ed815921c6b4ddaaf4875615a
downfa11 <downfa11@naver.com>
2024-10-28 12:12:48 +0900

    refactor: 클래스의 책임 분리, 폴더 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,12 @@
 package racingcar;
 
+import racingcar.controller.Controller;
+
 public class Application {
 
     public static void main(String[] args) {
-        Controller controller = new Controller();
+        final Controller controller = new Controller();
         controller.run();
     }
-
-
 
 }
