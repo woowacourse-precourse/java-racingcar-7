7bcccf52bfadf023c6eccf7d4123eb990d2cdd10
wobrorry <128110758+wobrorry@users.noreply.github.com>
2024-10-28 22:22:19 +0900

    Feat add playGame(), printWinners method
    게임 시작 및 우승자 출력 메소드 제작

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -12,6 +12,8 @@ public class Racing {
         String namesInput = getUserName();
         attempts = getUserAttempts();
         cars = splitCarsName(namesInput);
+        playGame();
+        printWinners();
     }
 
     private String getUserName() {
@@ -35,5 +37,41 @@ public class Racing {
             carList.add(new Car(name.trim()));
         }
         return carList;
+    }
+    private void playGame() {
+        for (int i = 0; i < attempts; i++) {
+            for (Car car : cars) {
+                car.move();
+            }
+            printRoundResults();
+        }
+    }
+
+    private void printRoundResults() {
+        for (Car car : cars) {
+            System.out.println(car);
+        }
+        System.out.println();
+    }
+
+    private int getMaxPosition(){
+        int maxPosition = -1;
+        for (Car car : cars) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
+            }
+        }
+        return maxPosition;
+    }
+
+    private void printWinners() {
+        int winnerPosition = getMaxPosition();
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == winnerPosition) {
+                winners.add(car.getName());
+            }
+        }
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
