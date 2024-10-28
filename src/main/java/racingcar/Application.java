0bdb86a78d84473b09aa4153b61c278d799d1a7a
[CLOUD4] 백승주 <bsj5967@naver.com>
2024-10-28 19:46:14 +0900

    feat(validateCarNames): 자동차 이름 입력 예외 처리 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,9 @@
 package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,7 +14,37 @@ public class Application {
 
     public static List<String> getCarsName() {
         String input = Console.readLine();
-        return List.of(input.split(","));
+
+        // 빈 문자열 예외 처리
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름은 빈 문자열일 수 없다.");
+        }
+        // 이름 분리
+        List<String> carNames = List.of(input.split(","));
+        validateCarNames(carNames);
+        return carNames;
+    }
+
+    public static void validateCarNames(List<String> carNames) {
+        Set<String> nameSet = new HashSet<>();
+
+        for (String name : carNames) {
+            name = name.trim();
+            // 개별 이름이 빈 문자열일 경우 예외 처리
+            if (name.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름에 빈 이름이 포함될 수 없다.");
+            }
+
+            // 이름이 5자를 초과할 경우 예외 처리
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없다 ");
+            }
+
+            // 중복된 이름 예외 처리
+            if (!nameSet.add(name)) {
+                throw new IllegalArgumentException("자동차 이름은 중복될 수 없다");
+            }
+        }
     }
 }
 
