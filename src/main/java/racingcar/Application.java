5996309c8921ca53e8026861771a782b41d85b9b
HwangBBang <h1009218@hufs.ac.kr>
2024-10-28 15:13:03 +0900

    feat(구현 목록#1) : n회, 수행 레이싱 참가 차량 1개에 대해서 구현
    - 참가 차량 1대에 대한 입력으로 수정
    - 차량 전진 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,31 +6,29 @@ import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
+
+        // todo : 1회, 수행 레이싱 참가 차량 1개에 대해서 구현
         // 사용자로부터 자동차 이름을 입력받음
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNameStr = Console.readLine();
+        System.out.println("경주할 자동차 이름을 입력하세요.");
+        String carName = Console.readLine();
+        int carPosition = 0;
 
         // 사용자로부터 시도할 횟수를 입력받음
         System.out.println("시도할 횟수는 몇 회인가요?");
         int n = Integer.parseInt(Console.readLine());
 
-        /*라이브러리 확인*/
-        /* pickNumberInRange 기능 확인 */
-        int randomNum = Randoms.pickNumberInRange(0, 9);
+        for (int i = 0; i < n; i++) {
+            if (isForward()) carPosition++;
+            System.out.println(carName + ":" + drawLogLine(carPosition));
+        }
 
-        /* pickNumberInList 기능 확인 */
-        List<Integer> numList = Arrays.asList(1, 2, 3, 4, 5);
-        int selectedNum = Randoms.pickNumberInList(numList);
+    }
+    private static boolean isForward() {
+        return Randoms.pickNumberInRange(0, 9) >= 4;
+    }
 
-        /*pickUniqueNumbersInRange 기능 확인 (0에서 9 사이의 고유한 숫자 중 3개 선택)*/
-        List<Integer> selectedNumList = Randoms.pickUniqueNumbersInRange(0, 9, 3);
-
-        /*shuffle 기능 확인 (리스트를 랜덤하게 셔플)*/
-        List<Integer> shuffledNumList = Randoms.shuffle(numList);
-
-        /*출력 확인*/
-        System.out.println("I/O,라이브러리 체크 종료");
-
+    private static String drawLogLine(int k) {
+        return "-".repeat(Math.max(0, k));
     }
 
 }
