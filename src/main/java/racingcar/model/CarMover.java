294c5c55afe1a7ada9266f1748059e53d711dcdc
IM-GYURI <kgulr0517@gmail.com>
2024-10-28 11:43:25 +0900

    refactor : add START_OF_RANGE, END_OF_RANGE and MOVE_STANDARD
    - START_OF_RANGE : 범위 시작 값 (0)
    - END_OF_RANGE : 범위 끝 값 (9)
    - MOVE_STANDARD : 전진 기준 값 (4)

--- src/main/java/racingcar/model/CarMover.java
+++ src/main/java/racingcar/model/CarMover.java
@@ -6,17 +6,21 @@ import racingcar.domain.Car;
 
 public class CarMover {
 
+    private static final int START_OF_RANGE = 0;
+    private static final int END_OF_RANGE = 9;
+    private static final int MOVE_STANDARD = 4;
+
     public void moveCars(List<Car> carList) {
         carList.forEach(this::tryMoveCar);
     }
 
     private void tryMoveCar(Car car) {
-        if (canMove(Randoms.pickNumberInRange(0, 9))) {
+        if (canMove(Randoms.pickNumberInRange(START_OF_RANGE, END_OF_RANGE))) {
             car.move();
         }
     }
 
     private boolean canMove(int randomValue) {
-        return randomValue >= 4;
+        return randomValue >= MOVE_STANDARD;
     }
 }
