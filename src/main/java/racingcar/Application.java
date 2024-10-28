14c40229857fceb214b5e33dd37a8fd53a2d2664
kangrae <kangrae0501@gmail.com>
2024-10-28 16:24:26 +0900

    refactor: Code refactor
    의미없는 주석 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,33 +10,26 @@ import view.OutputView;
 public class Application {
     public static void main(String[] args) {
 
-        // 1. 입력
         InputView inputView = new InputView();
         String inputCarNames = inputView.getCarNames();
         String inputMoveCount = inputView.getMoveCount();
         inputView.closeScanner();
 
-        // 2. 구분 및 에러 처리
         InputHandler inputHandler = new InputHandler();
         String[] carNames = inputHandler.splitCarNames(inputCarNames);
         int moveCount = inputHandler.toInt(inputMoveCount);
 
-        // 3. 자동차 객체 생성
         CarsHandler carsHandler = new CarsHandler(carNames);
 
-        // 4. 전진 & 5. 차수별 실행 결과 출력
         OutputView outputView = new OutputView();
-
         outputView.showRaceStarter();
         for (int i = 0; i < moveCount; i++) {
             carsHandler.moveCars();
             outputView.showRaceStatus(carsHandler.getCars());
         }
 
-        // 6. 우승자 판단
         List<Car> winners = carsHandler.getWinners();
 
-        // 7. 우승자 출력
         outputView.showWinners(winners);
     }
 }
