b51161bde27c22431420f15e474c639d73d6f00f
gorapaduckoo <jioung93@naver.com>
2024-10-28 21:18:46 +0900

    refactor: 시도 횟수 관련 메세지 변경
    - ROUND_COUNT를 ROUND_SIZE로 변경

--- src/main/java/racingcar/OutputMessage.java
+++ src/main/java/racingcar/OutputMessage.java
@@ -2,10 +2,9 @@ package racingcar;
 
 public enum OutputMessage {
     CAR_NAME_INPUT_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    ROUND_COUNT_INPUT_MESSAGE("시도할 횟수는 몇 회인가요?"),
+    ROUND_SIZE_INPUT_MESSAGE("시도할 횟수는 몇 회인가요?"),
     RESULT_MESSAGE("실행 결과"),
-    WINNER_NOTICE_PREFIX("최종 우승자 : ")
-    ;
+    WINNER_NOTICE_PREFIX("최종 우승자 : ");
 
     private final String message;
 
