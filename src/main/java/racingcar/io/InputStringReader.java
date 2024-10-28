37260e9a72385b06d95aa9c5870b8047577cfbb3
Minuring <mw9505@gmail.com>
2024-10-28 16:35:54 +0900

    feat(input): 사용자 입력을 요구할 때 콜론 제거

--- src/main/java/racingcar/io/InputStringReader.java
+++ src/main/java/racingcar/io/InputStringReader.java
@@ -8,12 +8,12 @@ public class InputStringReader {
     private final AppConfig appConfig = AppConfig.getInstance();
 
     public String readCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) : ");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
 
     public String readCount() {
-        System.out.println("시도할 횟수는 몇 회인가요? : ");
+        System.out.println("시도할 횟수는 몇 회인가요?");
         return Console.readLine();
     }
 }
