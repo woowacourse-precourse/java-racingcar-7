055b1dd993e652287a241fb073e2834cd2e02628
hyunn522 <tjgus52213@gmail.com>
2024-10-28 20:54:11 +0900

    feat: Add validation for car input

--- src/main/java/racingcar/model/car/CarGenerator.java
+++ src/main/java/racingcar/model/car/CarGenerator.java
@@ -1,14 +1,19 @@
 package racingcar.model.car;
 
+import static racingcar.constant.ExceptionMessage.*;
+
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CarGenerator {
 
     public List<String> extractCarNames(String carInput) {
-        String[] splitResult = carInput.split(",");
-        return Arrays.asList(splitResult);
+        List<String> splitResult = List.of(carInput.split(","));
+        validateCarInput(splitResult);
+
+        return splitResult;
     }
 
     public Cars generateCars(List<String> carNames) {
@@ -19,5 +24,22 @@ public class CarGenerator {
         }
 
         return cars;
+    }
+
+    private void validateCarInput(List<String> splitResult) {
+        if (splitResult.size() < 2) {
+            throw new IllegalArgumentException(INVALID_CAR_COUNT.getValue());
+        }
+
+        Set<String> uniqueCarNames = new HashSet<>();
+
+        for (String carName : splitResult) {
+            if (carName.isEmpty() || carName.length() > 5) {
+                throw new IllegalArgumentException(INVALID_CAR_NAME_LENGTH.getValue());
+            }
+            if (!uniqueCarNames.add(carName)) {
+                throw new IllegalArgumentException(INVALID_CAR_DUPLICATE.getValue());
+            }
+        }
     }
 }
