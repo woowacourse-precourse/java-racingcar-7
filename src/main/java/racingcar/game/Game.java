1dc43779683ca7b562f6065884f4d9869dcfbce3
CheonMinJae <cmjcmj1011@gmail.com>
2024-10-28 20:52:38 +0900

    fix: Game class 수정
    play(): 테스트코드 작성에 용이하게 하기 위해,
    Player 객체를 점수 받는지 판정에 사용할 수 있게 변경
    실제 application 에서는 필요없다
    
    winner(): 최댓값을 찾고, 이와 같은 player 를 찾는 로직에서
    오류 수정

--- src/main/java/racingcar/game/Game.java
+++ src/main/java/racingcar/game/Game.java
@@ -1,29 +1,33 @@
 package racingcar.game;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import racingcar.player.Player;
 
 public class Game {
     final List<Player> players;
-    Integer turn;
 
-    public Game(List<Player> players, Integer turn) {
+    public Game(List<Player> players) {
         this.players = players;
-        this.turn = turn;
     }
 
     public List<String> winner() {
+        Integer highestScore = players.stream()
+                .max(Comparator.<Player>comparingInt(Player::getScore))
+                .map(Player::getScore)
+                .orElse(0);
+
         return players.stream()
-                .sorted()
-                .filter(player -> player.getScore().equals(players.getFirst().getScore()))
+                .filter(player -> player.getScore().equals(highestScore))
                 .map(Player::getName)
                 .toList();
     }
 
-    public List<Player> play(Integer Score, Supplier<Boolean> scoreRule) {
+    public List<Player> play(Integer Score, Predicate<Player> scoreRule) {
         for(Player player : players) {
-            if (scoreRule.get()) {
+            if (scoreRule.test(player)) {
                 player.winGame(Score);
             }
         }
