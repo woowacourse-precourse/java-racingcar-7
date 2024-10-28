a602ee0f6d91907c2a8b4309ea90080c4dc48497
byunghyunkim0 <bb05qudgus@gamil.com>
2024-10-28 22:08:52 +0900

    test: input test코드 작성

--- src/test/java/racingcar/util/InputCarNameTest.java
+++ src/test/java/racingcar/util/InputCarNameTest.java
@@ -1,19 +1,16 @@
 package racingcar.util;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputCarNameTest {
 
-//  예외 케이스 = {"a,b,a", "A,a,B", "aaaaaa,abc,d", "\n", ",", "a,b,"}
-    @Test
-    void 입력_예외_테스트() {
-        String input = "a,b,B";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-
-        Assertions.assertThrows(IllegalArgumentException.class, InputUtils::readCarNameInput);
+    @ParameterizedTest
+    @ValueSource(strings = {"a,b,a", "A,a,B", "aaaaaa,abc,d", ",", "a,b,"})
+    void 입력_예외_테스트(String input) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            InputUtils.parseCarNameInput(input);
+        });
     }
 }
--- src/test/java/racingcar/util/InputRaceCountTest.java
+++ src/test/java/racingcar/util/InputRaceCountTest.java
@@ -1,19 +1,16 @@
 package racingcar.util;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputRaceCountTest {
 
-    //  예외 케이스 = {"-1", "a", " ", "01"}
-    @Test
-    void 숫자_입력_예외_테스트() {
-        String input = " ";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-
-        Assertions.assertThrows(IllegalArgumentException.class, InputUtils::readRaceCountInput);
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "a", " ", "\n", "01"})
+    void 숫자_입력_예외_테스트(String input) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            InputUtils.parseRaceCountInput(input);
+        });
     }
 }
