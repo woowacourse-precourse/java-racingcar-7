a3d0e3d93db1b60c565ba4f988055a976bd14876
AppleMint98 <hong721816@konkuk.ac.kr>
2024-10-28 13:17:06 +0900

    test : 객체 생성 메서드 테스트 구현
    createCar 메서드 호출 시 예외처리 테스트

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.Car.createCar;
+
+class CarTest {
+
+    @DisplayName("차 이름이 5 이하이면 차가 정상적으로 생성된다.")
+    @Test
+    void createCarWithValidNameLength() {
+
+        Car car = createCar("apple");
+
+        assertThat(car.getCarName()).isEqualTo("apple");
+        assertThat(car.getScore()).isZero();
+    }
+
+    @DisplayName("차 이름이 5가 넘으면 예외를 발생한다.")
+    @Test
+    void createCarWithInvalidNameLength() {
+
+        assertThatThrownBy(() -> createCar("banana"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("자동차 이름은 5자 이하만 가능합니다.");
+    }
+
+}
