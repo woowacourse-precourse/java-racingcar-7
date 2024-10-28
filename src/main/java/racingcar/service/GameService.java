d9ebe394ee0a13756f49f9c1b4be2b63aaeb0f39
rlaehd62 <rlaehd62@gmail.com>
2024-10-28 17:11:40 +0900

    refactor(service): RaceService 인터페이스 적용 - 서비스의 유연성을 위해서 인터페이스 적용 - 의미를 보다 명확하게 만들기 위해 이름 변경

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,27 +1,8 @@
 package racingcar.service;
 
-import java.util.List;
 import racingcar.dto.RaceRequest;
 import racingcar.dto.RaceResponse;
-import racingcar.model.car.Car;
-import racingcar.model.game.Race;
 
-public class GameService {
-
-    private CarService carService;
-
-    public GameService() {
-        this.carService = new CarService();
-    }
-
-    public RaceResponse startRace(RaceRequest request) {
-
-        String carInput = request.getCarInput();
-        int tryCount = request.getTryCount();
-
-        List<Car> cars = carService.createCars(carInput);
-        Race track = new Race(tryCount);
-        cars.forEach(track::addCar);
-        return new RaceResponse(track.simulate());
-    }
+public interface GameService<T, R> {
+    R startGame(T request);
 }
