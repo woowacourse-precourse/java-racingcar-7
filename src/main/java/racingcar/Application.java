0f68414c849108a5cde38fb332ab1e50c0b61c21
SUNSETBREEZE <ghubkit2060@gmail.com>
2024-10-28 11:18:07 +0900

    FEAT : Function 클래스에 이름의 길이의 초과여부를 판단하는 함수 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,30 +11,19 @@ class Player {
 		this.racingProcessCount = 0;
 	}
 
-	public void canMoveForward() {
-		if (camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 9) >= 4) {
-			this.racingProcessCount++;
-		}
-	}
-
-	public String exchangeRacingProcessCountToString() {
-		String racingString = "";
-		for (int i = 0; i < this.racingProcessCount; i++) {
-			racingString = racingString + "-";
-		}
-
-		return racingString;
-	}
-
-	public void printPlayerPresentState() {
-		System.out.println(this.name + " : " + this.exchangeRacingProcessCountToString());
-	}
 }
 
+class Function {
+	public static void checkNameLengthOverFive(String name) {
+		if (name.length() > 5) {
+			throw new IllegalArgumentException();
+		}
+	}
 
-	
+}
+
 public class Application {
 	public static void main(String[] args) {
-		
+
 	}
 }
