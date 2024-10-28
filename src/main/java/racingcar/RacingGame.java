50f55c7a0b7c8396eff3c44f92e7c17a7ba85635
kmc198989 <kmc198989@naver.com>
2024-10-28 16:47:38 +0900

    Feat: 우승자 결정 및 최종 결과 출력 기능 구현
    - 가장 멀리 이동한 자동차를 우승자로 결정하여 출력
    - 우승자가 여러 명일 경우 쉼표로 구분하여 출력

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record RacingGame(List<Car> cars, int rounds) {
     public void startRace() {
@@ -17,5 +18,19 @@ public record RacingGame(List<Car> cars, int rounds) {
             System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
         }
         System.out.println();
+    }
+
+    public void printFinalResults() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        List<String> winners = cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
