d01e074eb30d38730b431272f9938bc800a617fc
kyung-Park <ruddnjs5816@gmail.com>
2024-10-28 20:37:24 +0900

    feat: Car 클래스, RacingGame 클래스 추가

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+    private List<Car> cars = new ArrayList<>();
+    private int attempts;
+
+}
