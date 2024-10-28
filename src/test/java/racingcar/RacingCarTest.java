33855ed7f90d752036fc5df02b3e128aa192d188
jeonjuho <wjswngh23@daum.net>
2024-10-28 19:57:31 +0900

    test(RacingCarTest): 여러번 이동하는 테스트 케이스 추가
    - 여러번 이동했을 때의 결과 값을 검증

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class RacingCarTest {
 
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
     @ParameterizedTest
     @ValueSource(strings = {"다섯글자이상", "포르쉐카이엔"})
     public void registWithOverLength(String racingCarName) throws Exception {
@@ -31,7 +34,7 @@ class RacingCarTest {
         String racingCarString = racingCar.toString();
 
         //then
-        assertThat(racingCarString).isEqualTo(expected+" ");
+        assertThat(racingCarString).isEqualTo(expected + " ");
     }
 
     @ParameterizedTest
@@ -41,7 +44,26 @@ class RacingCarTest {
         RacingCar racingCar = new RacingCar(racingCarName);
         Assertions.assertRandomNumberInRangeTest(() -> {
             racingCar.moveOrNot();
-        }, 4, 3);
+        }, MOVING_FORWARD, STOP);
+
+        //when
+        String racingCarString = racingCar.toString();
+
+        //then
+        assertThat(racingCarString).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"자동차,자동차 : ---", "레이싱카,레이싱카 : ---"})
+    public void toStringWithMoveForwardThreeTimes(String racingCarName, String expected)
+            throws Exception {
+        //given
+        RacingCar racingCar = new RacingCar(racingCarName);
+        Assertions.assertRandomNumberInRangeTest(() -> {
+            racingCar.moveOrNot();
+            racingCar.moveOrNot();
+            racingCar.moveOrNot();
+        }, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, STOP);
 
         //when
         String racingCarString = racingCar.toString();
