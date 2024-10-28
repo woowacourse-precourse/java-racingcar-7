6d5357423a22588ccb0e448affbfa9bd718da260
smyeongL <sangmyeonglee123@gmail.com>
2024-10-28 15:54:19 +0900

    feat(message): add error message for empty user input

--- src/main/java/racingcar/message/Message.java
+++ src/main/java/racingcar/message/Message.java
@@ -5,6 +5,7 @@ public class Message {
     public static final String GAME_ROUND_REQUEST_MESSAGE = "시도할 횟수는 몇 회인가요?\n";
     public static final String EXECUTION_MESSAGE = "실행 결과\n";
     public static final String WINNER_MESSAGE = "최종 우승자 : ";
+    public static final String USER_INPUT_EMPTY_ERROR_MESSAGE = "No User Response found\n";
     public static final String CAR_NAME_EMPTY_ERROR_MESSAGE = "Invalid car name length: length = 0\n";
     public static final String CAR_NAME_EXCEED_ERROR_MESSAGE = "Invalid car name length: length > 5\n";
     public static final String INVALID_GAME_ROUND_INT_ERROR_MESSAGE = "Invalid Game Round Input: value must be 1 or greater\n";
