00da38bc19b51497563efd056425e5e9b0044512
rlaehd62 <rlaehd62@gmail.com>
2024-10-28 17:11:14 +0900

    refactor(service): CarService 인터페이스 적용 - 서비스의 유연성을 위해서 인터페이스 적용 - 의미를 보다 명확하게 만들기 위해 이름 변경

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -1,16 +1,8 @@
 package racingcar.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import racingcar.model.car.Car;
-import racingcar.model.car.RaceCar;
 
-public class CarService {
-
-    public List<Car> createCars(String input) {
-        List<String> items = List.of(input.split(","));
-        return items.stream()
-                .map(RaceCar::new)
-                .collect(Collectors.toUnmodifiableList());
-    }
+public interface CarService {
+    List<Car> createCars(String input);
 }
