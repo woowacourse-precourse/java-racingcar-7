bf28d9ea5fc6ef703db832997be5d542334e6f4b
lee-haeseung <lhser123@cau.ac.kr>
2024-10-28 21:28:14 +0900

    feat: 같은 이름 중복 예외 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,10 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -10,8 +13,13 @@ public class Application {
 
         // model 생성
         List<Car> cars = new ArrayList<>();
+        Set<String> carNamesSet = new HashSet<>();
         for (String carName : carNameStrings.split(",")) {
+            if (carNamesSet.contains(carName)) {
+                throw new IllegalArgumentException();
+            }
             cars.add(new Car(carName));
+            carNamesSet.add(carName);
         }
 
         System.out.println("시도할 횟수는 몇 회인가요?");
