44e65e91121ca9ecdd227c8136cd99b89f28d8c4
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2024-10-28 21:31:07 +0900

    feat(Cars):
    - List 형태의 자동차 이름을 입력받아 List<Car> collection 생성
    - 이동과 현재 상태를 출력
    - 우승 자동차 이름 반환

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,56 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> CAR_LIST;
+
+    public Cars(List<String> carNameList) {
+        this.CAR_LIST = generateCarList(carNameList);
+    }
+
+    private List<Car> generateCarList(List<String> carNameList) {
+        List<Car> carList = new ArrayList<>();
+        for (String carName : carNameList) {
+            carList.add(new Car(carName));
+        }
+        return carList;
+    }
+
+    public void move() {
+        for (Car car : CAR_LIST) {
+            car.move();
+        }
+    }
+
+    private int getWinnerDistance() {
+        int winnerDistance = 0;
+        for (Car car : CAR_LIST) {
+            winnerDistance = car.maxDistanceAndOtherDistance(winnerDistance);
+        }
+        return winnerDistance;
+    }
+
+    public String getWinnersCarName() {
+        int winnerDistance = getWinnerDistance();
+        List<String> winnersCarName = new ArrayList<>();
+        for (Car car : CAR_LIST) {
+            if (car.isEqualToWinnerDistance(winnerDistance)) {
+                winnersCarName.add(car.getCAR_NAME());
+            }
+        }
+        return String.join(",", winnersCarName);
+    }
+
+    public void printCarsStatus() {
+        for (Car car : CAR_LIST) {
+            car.visualizeDistance();
+        }
+        printNewLine();
+    }
+
+    private void printNewLine() {
+        System.out.println();
+    }
+}
