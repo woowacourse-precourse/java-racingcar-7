6d5f7a11d8e8b246f8893794fe04a89a087329bc
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-10-28 09:06:42 +0900

    Style fix constant to upper case

--- src/main/java/racingcar/RandomMoveCondition.java
+++ src/main/java/racingcar/RandomMoveCondition.java
@@ -4,10 +4,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomMoveCondition implements MoveCondition {
 
-    private static final int standard_number = 4;
+    private static final int STANDARD_NUMBER = 4;
 
     @Override
     public boolean canMove() {
-        return Randoms.pickNumberInRange(0, 9) >= standard_number;
+        return Randoms.pickNumberInRange(0, 9) >= STANDARD_NUMBER;
     }
 }
