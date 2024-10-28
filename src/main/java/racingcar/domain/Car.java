796339ce6a4f2f05436a70e6eca1c8ec259f00b4
gibumgo <gibumgo@gmail.com>
2024-10-28 11:51:55 +0900

    feat : 자동차 전진 기능 및 무작위 정수 반환 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,9 +1,28 @@
 package racingcar.domain;
 
 public class Car {
+    private Position position;
     private Name name;
 
     public Car(String name) {
         this.name = Name.create(name);
+        this.position = Position.create();
+    }
+
+    public void move(MovingStrategy movingStrategy) {
+        if (movingStrategy.isMoving()) {
+            position = position.move();
+        }
+    }
+
+    public Position getMaxPosition(Position otherPosition) {
+        if (this.position.isGreater(otherPosition)) {
+            return this.position;
+        }
+        return otherPosition;
+    }
+
+    public Position getPosition() {
+        return this.position;
     }
 }
---
+++ src/main/java/racingcar/domain/Position.java
@@ -0,0 +1,43 @@
+package racingcar.domain;
+
+import java.util.Objects;
+
+public class Position {
+    private static final int FORWARD_COUNT = 1;
+    private static final int INIT_POSITION_VALUE = 0;
+    private static final int COMPARE_RESULT_EQUAL_VALUE = 0;
+    private int position;
+
+    private Position(int position) {
+        this.position = position;
+    }
+
+    public static Position create() {
+        return new Position(INIT_POSITION_VALUE);
+    }
+
+    public Position move() {
+        return new Position(position + FORWARD_COUNT);
+    }
+
+    public boolean isGreater(Position OtherPosition) {
+        return this.position - OtherPosition.getPosition() > COMPARE_RESULT_EQUAL_VALUE;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+}
