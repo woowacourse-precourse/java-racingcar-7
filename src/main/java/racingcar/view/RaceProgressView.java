17b9721be68f1db145404aa0b62710afbb76ed24
chaerish <ccyy8432@naver.com>
2024-10-28 22:11:09 +0900

    feat(RaceProgressView): 출력 결과 메세지 추가
    RaceProgressView에 출력 결과 메세지를 추가한다.

--- src/main/java/racingcar/view/RaceProgressView.java
+++ src/main/java/racingcar/view/RaceProgressView.java
@@ -5,12 +5,19 @@ import racingcar.model.RacingCar;
 
 public class RaceProgressView {
     private final String COLON_DELIMITER = " : ";
+    private final String OUTPUT_MESSAGE = "실행 결과";
+    private final String LINE_SPACE = "\n";
+
+    public void displayOutputMessage() {
+        System.out.println(LINE_SPACE + OUTPUT_MESSAGE);
+    }
 
     public void displayRoundProgress(List<RacingCar> racingCars) {
-        System.out.println();
         for (RacingCar racingCar : racingCars) {
-            System.out.println(racingCar.getName() + COLON_DELIMITER + racingCar.getDistance());
+            String result = racingCar.getName() + COLON_DELIMITER + racingCar.getDistance();
+            System.out.println(result);
         }
+        System.out.print(LINE_SPACE);
     }
 
 }
