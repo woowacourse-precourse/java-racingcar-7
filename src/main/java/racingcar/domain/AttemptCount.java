efddb7484c5f0f4d4c9e633e060229886250e469
jae woo Jung <awfjol2008@gmail.com>
2024-10-28 19:33:29 +0900

    refactor: AttemptCount 클래스 getter 추가

--- src/main/java/racingcar/domain/AttemptCount.java
+++ src/main/java/racingcar/domain/AttemptCount.java
@@ -3,7 +3,7 @@ package racingcar.domain;
 public class AttemptCount {
 	private final Integer attemptCount;
 
-	protected AttemptCount(int attemptCount) {
+	public AttemptCount(int attemptCount) {
 		validatePositive(attemptCount);
 		this.attemptCount = attemptCount;
 	}
@@ -12,5 +12,9 @@ public class AttemptCount {
 		if (attemptCount <= 0) {
 			throw new IllegalArgumentException("[ERROR] 시도 횟수는 0보다 커야 합니다.");
 		}
+	}
+
+	public Integer getAttemptCount() {
+		return attemptCount;
 	}
 }
