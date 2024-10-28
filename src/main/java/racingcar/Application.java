c72937c4b5ae34858dc1b4920d6688de9e296a0f
junho <wnsgh732@gmail.com>
2024-10-28 20:42:47 +0900

    refactor: 단일 책임 원칙을 위한 객체 분할

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,29 +2,17 @@ package racingcar;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Application {
     public static void main(String[] args) {
         InputHandler inputHandler = new InputHandler();
         List<Car> carList = inputHandler.getCarName();
-        RaceTrack raceTrack = new RaceTrack(carList);
         int attemptNumber = inputHandler.getAttemtNumber();
 
-        for(int i = 0; i < attemptNumber; i ++) {
-            for(Car car : carList) {
-                car.move();
-            }
-            for(Car car : carList) {
-                car.displayPosition();
-            }
-            System.out.println();
-        }
+        RaceTrack raceTrack = new RaceTrack(carList);
+        RaceController raceController = new RaceController(raceTrack, carList);
 
-        List<Car> winnerList = raceTrack.getWinner();
-        String result = winnerList.stream()
-                .map(Car::getName)
-                .collect(Collectors.joining(", "));
-        System.out.println("최종 우승자 : " + result);
+        raceController.run(attemptNumber);
+        raceController.printWinnerList();
     }
 }
