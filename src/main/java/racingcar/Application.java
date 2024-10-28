ae754890ecd59bb98b1252a02f4f08dcecda9f00
nsb329 <nsb329@naver.com>
2024-10-28 17:44:14 +0900

    실행 횟수 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,21 +9,32 @@ public class Application {
 	public static void main(String[] args) {
 		String car[] = input_name();
 		int[] result = new int[car.length];
+		int time = try_count();
 		System.out.println("실행 결과");
+		while (time > 0) {
+			time--;
 			System.out.println();
 		}
+	}
 
 	public static String[] input_name() {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		String name_input = Console.readLine();
 		if (name_input.contains(",")) {
 			String[] arr = name_input.split(",");
-				System.out.println(name_input);
+			System.out.println(name_input);
 			return arr;
 		} else {
 			String[] arr = { name_input };
 			System.out.println(name_input);
 			return arr;
 		}
+	}
+
+	public static int try_count() {
+		System.out.println("시도할 횟수를 입력하세요 : ");
+		String count = Console.readLine();
+		int number = Integer.parseInt(count);
+		return number;
 	}
 }
