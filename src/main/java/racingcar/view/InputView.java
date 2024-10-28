77efbcb36b346e84778906cae7287e9661498711
DongHoon Lee <dhl1924@naver.com>
2024-10-28 13:44:50 +0900

    feat(view): 시도할 횟수 입력 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,7 +1,10 @@
 package racingcar.view;
 
 import racingcar.dto.CarNamesInput;
+import racingcar.dto.TryCountInput;
 
 public interface InputView {
     CarNamesInput readCarNames();
+
+    TryCountInput readTryCount();
 }
--- src/main/java/racingcar/view/console/ConsoleInputView.java
+++ src/main/java/racingcar/view/console/ConsoleInputView.java
@@ -2,11 +2,19 @@ package racingcar.view.console;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.dto.CarNamesInput;
+import racingcar.dto.TryCountInput;
 import racingcar.view.InputView;
 
 public class ConsoleInputView implements InputView {
+    @Override
     public CarNamesInput readCarNames() {
         String carNames = Console.readLine();
         return new CarNamesInput(carNames.trim());
+    }
+
+    @Override
+    public TryCountInput readTryCount() {
+        String TryCount = Console.readLine();
+        return new TryCountInput(TryCount.trim());
     }
 }
