8f92bc18119d5f77bd039beafa5639222c68cc8a
Trillion_Impulse <tkdduq2897@naver.com>
2024-10-28 15:23:51 +0900

    feat: update code for feature requirement

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,50 +2,62 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        // 입력 요구 사항 1-1
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         String input = Console.readLine();
 
         String[] names = input.split(",");
 
-        if(names.length == 0){//구분자만 넣었을 경우
+        if (names.length == 0) {// 구분자만 넣었을 경우
             throw new IllegalArgumentException("자동차의 이름을 1글자 이상 입력하세요.");
         }
-        for(String name : names){
-            if(name.length() > 5){
+        for (String name : names) {
+            if (name.length() > 5) {
                 throw new IllegalArgumentException("자동차의 이름은 5글자 이하만 가능합니다.");
-            }
-            else if(name.isEmpty()){
+            } else if (name.isEmpty()) {
                 throw new IllegalArgumentException("자동차의 이름을 1글자 이상 입력하세요.");
-            }
-            else if(name.isBlank()){
+            } else if (name.isBlank()) {
                 throw new IllegalArgumentException("공백으로만 이루어진 자동차의 이름은 불가능합니다.");
             }
-            System.out.println(name.length());
-            
         }
 
-
+        // 입력 요구 사항 1-2
         System.out.println("시도할 횟수는 몇 회인가요?");
 
         String num = Console.readLine();
 
-        try{
-            int number = Integer.parseInt(num);
-            if(number == 0){
+        int repeat = 0;
+        try {
+            repeat = Integer.parseInt(num);
+            if (repeat < 1) {
                 throw new IllegalArgumentException("1 이상의 숫자만 가능합니다.");
             }
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자만 입력가능합니다.");
         }
 
-
-        int random = 0;
+        // 기능 요구 사항
+        ArrayList<Integer> numOfGo = new ArrayList<>();
+        int j = 0;
+        int lengthOfNames = names.length;
+        for (j = 0; j < lengthOfNames; j++) {
+            int i = 0;
+            int go = 0;
+            for (i = 0; i < repeat; i++) {
+                int random = Randoms.pickNumberInRange(0, 9);
+                if (random >= 4) {
+                    go += 1;
+                }
+            }
+            numOfGo.add(go);
+        }
 
     }
 }
