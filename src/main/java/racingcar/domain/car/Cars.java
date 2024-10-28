218441eea3ae17e85d6f11d9c7c6eb9729d80ffc
hootzz <wjqwjql3@gmail.com>
2024-10-28 22:15:16 +0900

    feat(domain): Cars 우승자 찾기 기능 구현

--- src/main/java/racingcar/domain/car/Cars.java
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -1,21 +1,40 @@
 package racingcar.domain.car;
 
+import racingcar.domain.strategy.MovingStrategy;
+
 import java.util.List;
-import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Cars {
     private final List<Car> cars;
 
     public Cars(List<String> names) {
-        this.cars = new ArrayList<>();
-        for (String name : names) {
-            cars.add(new Car(name));
+        cars = names.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public void race(MovingStrategy strategy) {
+        for (Car car : cars) {
+            car.move(strategy);
         }
     }
 
-    public void race() {
-        for (Car car : cars) {
-            car.move();
-        }
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public int findMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+
+    public List<Car> findWinners() {
+        int maxPosition = findMaxPosition();
+        return cars.stream()
+                .filter(car -> car.isAtPosition(maxPosition))
+                .collect(Collectors.toList());
     }
 }
