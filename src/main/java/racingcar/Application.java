96df93cad88e5f47018bbaabe204b90b225c10ff
AntsThatWander <anubspider@gmail.com>
2024-10-28 16:01:22 +0900

    feat($Racing) : add a method to name cars
    Add a method named nameCars to name cars, and return a List that contains cars.
    
    An exception handling included:
    - 이름이 5글자 이상일 때

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입려갛세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String carNames = Console.readLine();
 
         System.out.println("시도할 횟수는 몇 회인가요?");
--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,8 +1,25 @@
 package racingcar;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Racing {
     public static void startRace(String carNames, String tryCount) {
-        
+        List<String> cars = nameCars(carNames);
+    }
+
+    private static List<String> nameCars(final String carNames) {
+        List<String> cars = new ArrayList<String>();
+
+        for (String car : carNames.split(",")) {
+            if (car.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+
+            cars.add(car);
+        }
+
+        return cars;
     }
 
 }
