fff44088f97f6e4f70edea8350b41cba3ea85584
yongminKwon1 <ym.kwon@lipis.kr>
2024-10-28 11:51:48 +0900

    feat(RacingCar): name 프로퍼티, getter 추가
    name 프로퍼티를 추가했습니다.
    getter 메서드를 추가했습니다.

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -3,16 +3,24 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingCar {
+    private String name;
     private int position;
 
     public RacingCar(String name) {
+        this.name = name;
         this.position = 0;
     }
 
-    public int move() {
+    public String getName() {
+        return name;
+    }
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
         if (4 <= Randoms.pickNumberInRange(0, 9)) {
             position++;
         }
-        return position;
     }
 }
