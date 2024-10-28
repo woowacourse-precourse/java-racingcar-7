1a77db54b0c8325ca9b1eeab9a2120b3ee889780
sytTest <syt1128@naver.com>
2024-10-28 12:32:51 +0900

    :sparkles: (WinnerService) add pickWinners()
    가장 먼 주행 거리를 가진 차량의 이름을 모아 우승 차량 이름 리스트에 넣는 pickWinners() 추가

--- src/main/java/racingcar/service/race/WinnerService.java
+++ src/main/java/racingcar/service/race/WinnerService.java
@@ -2,6 +2,7 @@ package racingcar.service.race;
 
 import java.util.List;
 import racingcar.domain.Car;
+import racingcar.service.constant.ExpressionFormat;
 
 public class WinnerService {
     private List<String> winners;
@@ -9,6 +10,7 @@ public class WinnerService {
 
     public WinnerService(List<Car> finishers) {
         findWinningPosition(finishers);
+        pickWinners(finishers);
     }
 
     private void findWinningPosition(List<Car> finishers) {
@@ -16,5 +18,12 @@ public class WinnerService {
                 .mapToLong(Car::getMileage)
                 .max()
                 .orElse(0L);
+    }
+
+    private void pickWinners(List<Car> finishers) {
+        winners = finishers.stream()
+                .filter(car -> car.getMileage().equals(farthest))
+                .map(Car::getName)
+                .toList();
     }
 }
