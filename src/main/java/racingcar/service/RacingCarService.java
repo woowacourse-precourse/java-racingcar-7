b8aaaa6d7ad4248d35e28d2c9dece2b432509e59
Seongho Kim <kksshh0612@gmail.com>
2024-10-28 11:31:35 +0900

    refactor: 랜덤 숫자 생성기 인터페이스에 의존하도록 리팩토링

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -6,7 +6,13 @@ import racingcar.model.Car;
 
 public class RacingCarService {
 
-    private static final int MINIMUM_GO_NUMBER = 4;
+    private final NumberGenerator numberGenerator;
+
+    private static final int MINIMUM_MOVE_NUMBER = 4;
+
+    public RacingCarService(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
 
     public RacingResult startRacingGame(List<Car> cars, int tryCount) {
 
@@ -16,7 +22,8 @@ public class RacingCarService {
             moveCars(cars, racingResultBuilder);
         }
 
-        List<String> winnerNames = generateWinnerResult(cars);
+        int winnerPos = getWinnerPos(cars);
+        List<String> winnerNames = generateWinnerResult(cars, winnerPos);
 
         return RacingResult.of(racingResultBuilder.toString(), winnerNames);
     }
@@ -24,7 +31,7 @@ public class RacingCarService {
     private void moveCars(List<Car> cars, StringBuilder racingResultBuilder) {
 
         for (Car car : cars) {
-            if (RandomUtil.generateRandomNumber() >= MINIMUM_GO_NUMBER) {      // 전진
+            if (numberGenerator.generateRandomNumber() >= MINIMUM_MOVE_NUMBER) {      // 전진
                 car.go();
             }
 
@@ -34,15 +41,20 @@ public class RacingCarService {
         racingResultBuilder.append("\n");
     }
 
-    private List<String> generateWinnerResult(List<Car> cars) {
-        int max = cars.stream()
+    private int getWinnerPos(List<Car> cars) {
+
+        return cars.stream()
                 .mapToInt(Car::getPos)
                 .max()
                 .orElse(0);
+    }
+
+    private List<String> generateWinnerResult(List<Car> cars, int winnerPos) {
 
         return cars.stream()
-                .filter(car -> car.getPos() == max)
+                .filter(car -> car.getPos() == winnerPos)
                 .map(Car::getName)
                 .toList();
     }
+
 }
