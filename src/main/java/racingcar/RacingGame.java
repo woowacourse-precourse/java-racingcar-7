f5aeb3fb13d0de773924282fb9e565e95535cbc3
AhnJoonSung <tjdtna01@naver.com>
2024-10-28 13:19:22 +0900

    Feature: Car, RacingGame 클래스 뼈대 구현

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import java.util.List;
+
+public class RacingGame {
+    private List<Car> cars;
+
+    public RacingGame(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void start() {
+
+    }
+
+}
