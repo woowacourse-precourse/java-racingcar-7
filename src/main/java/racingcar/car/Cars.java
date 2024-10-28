2dcd7a623e125417cb44c4ba9b7707dd0cd8f66d
wlsdlf2 <wlsdlfzg@naver.com>
2024-10-28 11:40:34 +0900

    chore: RandomNumberGenerator 클래스에서 상수 제거

--- src/main/java/racingcar/car/Cars.java
+++ src/main/java/racingcar/car/Cars.java
@@ -35,11 +35,8 @@ public class Cars {
     }
 
     private static class RandomNumberGenerator {
-        private static final int MIN_RANDOM_VALUE = 0;
-        private static final int MAX_RANDOM_VALUE = 9;
-
         public static int generate() {
-            return Randoms.pickNumberInRange(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE);
+            return Randoms.pickNumberInRange(0, 9);
         }
     }
 }
