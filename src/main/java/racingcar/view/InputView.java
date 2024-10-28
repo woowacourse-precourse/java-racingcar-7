0fcd512e7560b3a552f140cdc0025fdc1d289d93
장영재 <urinaner@naver.com>
2024-10-28 14:03:05 +0900

    feat(InputView): 시도 횟수 예외처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,6 +3,7 @@ package racingcar.view;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import racingcar.utils.ErrorMessage;
 
 public class InputView {
     private static final String GET_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
@@ -16,6 +17,20 @@ public class InputView {
 
     public int getTryNumbers() throws IOException {
         System.out.println(GET_TRY_NUMBERS);
-        return Integer.parseInt(br.readLine());
+        return validateTrialNumber(Integer.parseInt(br.readLine()));
+    }
+
+    private int validateTrialNumber(int trials) {
+        try {
+            if (trials < 1) {
+                throw new IllegalArgumentException(ErrorMessage.TRIAL_NUMBER_UNDER_RANGE.message());
+            }
+            if (trials > 100) {
+                throw new IllegalArgumentException(ErrorMessage.TRIAL_NUMBER_OVER_RANGE.message());
+            }
+            return trials;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_TRIAL_NUMBER.message());
+        }
     }
 }
