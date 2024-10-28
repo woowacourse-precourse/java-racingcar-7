bda84a1621f624f4ad031dbd3bd96bade214a11c
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-28 12:30:09 +0900

    refactor(Car): Strategy 패턴 사용으로 인한 참조 변경 및 선언명 변경
    - strategy 패턴 사용으로 변경된 클래스명에 맞춰 변경했습니다.
    - 인터페이스를 선언하고 구현체를 받습니다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,12 +3,12 @@ package racingcar.model;
 public class Car {
     private static final int MINIMUM_CAN_MOVE_CONDITION = 4;
     private static final int ONE_FORWARD_MOVE_DISTANCE = 1;
-    private final RandomIntGenerator randomIntGenerator;
+    private final IntGeneratorStrategy intGeneratorStrategy;
     private final String carName;
     private int position;
 
-    public Car(String carName, RandomIntGenerator randomIntGenerator) {
-        this.randomIntGenerator = randomIntGenerator;
+    public Car(String carName, IntGeneratorStrategy randomIntGeneratorStrategy) {
+        this.intGeneratorStrategy = randomIntGeneratorStrategy;
         this.carName = carName;
         this.position = 0;
     }
@@ -28,7 +28,7 @@ public class Car {
     }
 
     private boolean canMove() {
-        return randomIntGenerator.getRandomNumber() >= MINIMUM_CAN_MOVE_CONDITION;
+        return intGeneratorStrategy.getIntValue() >= MINIMUM_CAN_MOVE_CONDITION;
     }
 
     private void doMove() {
