90d4cf782f9a8dc3381c7799b4f2814cce1db584
seaniiio <cnc4750@naver.com>
2024-10-28 11:46:51 +0900

    fix(ValidatorTest): 테스트 대상 클래스명 수정

--- src/test/java/racingcar/util/ValidatorTest.java
+++ src/test/java/racingcar/util/ValidatorTest.java
@@ -1,4 +1,4 @@
-package racingcar.validator;
+package racingcar.util;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -11,13 +11,13 @@ import racingcar.exception.CarNameFormatException;
 import racingcar.exception.CountFormatException;
 import racingcar.exception.CountNegativeException;
 
-class InputValidatorTest {
+class ValidatorTest {
 
     @Test
     public void 자동차_이름_공백_예외_테스트() {
         List<String> carNames = List.of(" ");
 
-        assertThatThrownBy(() -> InputValidator.validateCarNames(carNames))
+        assertThatThrownBy(() -> Validator.validateCarNames(carNames))
                 .isInstanceOf(CarNameFormatException.class);
     }
 
@@ -26,7 +26,7 @@ class InputValidatorTest {
         List<String> carNames = new ArrayList<>();
         carNames.add(null);
 
-        assertThatThrownBy(() -> InputValidator.validateCarNames(carNames))
+        assertThatThrownBy(() -> Validator.validateCarNames(carNames))
                 .isInstanceOf(CarNameFormatException.class);
     }
 
@@ -34,7 +34,7 @@ class InputValidatorTest {
     public void 자동차_이름_5글자_초과_예외_테스트() {
         List<String> carNames = List.of("seanii");
 
-        assertThatThrownBy(() -> InputValidator.validateCarNames(carNames))
+        assertThatThrownBy(() -> Validator.validateCarNames(carNames))
                 .isInstanceOf(CarNameFormatException.class);
     }
 
@@ -42,7 +42,7 @@ class InputValidatorTest {
     public void 자동차_이름_중복_예외_테스트() {
         List<String> carNames = List.of("sean", "car3", "sean");
 
-        assertThatThrownBy(() -> InputValidator.validateCarNames(carNames))
+        assertThatThrownBy(() -> Validator.validateCarNames(carNames))
                 .isInstanceOf(CarNameDuplicatedException.class);
     }
 
@@ -50,7 +50,7 @@ class InputValidatorTest {
     public void 시도_횟수_음수_예외_테스트() {
         String countInput = "-1";
 
-        assertThatThrownBy(() -> InputValidator.validateCount(countInput))
+        assertThatThrownBy(() -> Validator.validateCount(countInput))
                 .isInstanceOf(CountNegativeException.class);
     }
 
@@ -58,7 +58,7 @@ class InputValidatorTest {
     public void 시도_횟수_공백_예외_테스트() {
         String countInput = "";
 
-        assertThatThrownBy(() -> InputValidator.validateCount(countInput))
+        assertThatThrownBy(() -> Validator.validateCount(countInput))
                 .isInstanceOf(CountFormatException.class);
     }
 
@@ -66,7 +66,7 @@ class InputValidatorTest {
     public void 시도_횟수_문자열_예외_테스트() {
         String countInput = "5번";
 
-        assertThatThrownBy(() -> InputValidator.validateCount(countInput))
+        assertThatThrownBy(() -> Validator.validateCount(countInput))
                 .isInstanceOf(CountFormatException.class);
     }
 
@@ -74,7 +74,7 @@ class InputValidatorTest {
     public void 시도_횟수_NULL_예외_테스트() {
         String countInput = null;
 
-        assertThatThrownBy(() -> InputValidator.validateCount(countInput))
+        assertThatThrownBy(() -> Validator.validateCount(countInput))
                 .isInstanceOf(CountFormatException.class);
     }
 
@@ -82,7 +82,7 @@ class InputValidatorTest {
     public void 시도_횟수_범위_초과_예외_테스트() {
         BigInteger bigInteger = BigInteger.valueOf(Integer.MAX_VALUE).add(BigInteger.valueOf(1));
 
-        assertThatThrownBy(() -> InputValidator.validateCount(String.valueOf(bigInteger)))
+        assertThatThrownBy(() -> Validator.validateCount(String.valueOf(bigInteger)))
                 .isInstanceOf(CountFormatException.class);
     }
 }
