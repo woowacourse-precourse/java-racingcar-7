d648939414cce182ee18e94ad88aef3f51e3989a
BusanGukbap <dong2857210@gmail.com>
2024-10-28 18:37:15 +0900

    feat($Model) : add RacingGame

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -8,7 +8,10 @@ public class RacingGame {
     private final List<Car> cars;
 
     public RacingGame(List<String> carNames) {
-
+        cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
     }
 
     public List<Car> getCars() {
