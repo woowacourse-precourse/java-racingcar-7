2b79fdb5a874cb0fc709fb1433463fa74ec058f2
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-28 13:59:27 +0900

    feat: 시도 횟수 문자 유효성 검사 메소드 추가

--- src/main/java/racingcar/validator/RaceValidator.java
+++ src/main/java/racingcar/validator/RaceValidator.java
@@ -1,6 +1,7 @@
 package racingcar.validator;
 
 import racingcar.model.Car;
+
 import java.util.List;
 
 public class RaceValidator {
@@ -11,9 +12,19 @@ public class RaceValidator {
         }
     }
 
-    public static void validateAttempts(int attempts) {
+    public static void checkPositiveAttempts(int attempts) {
         if (attempts <= 0) {
             throw new IllegalArgumentException("시도 횟수는 1회 이상이어야 합니다.");
+        }
+    }
+
+    public static int checkAttemptsInput(String input) {
+        try {
+            int attempts = Integer.parseInt(input);
+            checkPositiveAttempts(attempts);
+            return attempts;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 숫자로 입력해야 합니다.");
         }
     }
 }
