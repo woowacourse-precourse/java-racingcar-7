f1075090f979e78305b8b27c6a1017c48df5d248
mjk <mjk3220@ajou.ac.kr>
2024-10-28 11:17:25 +0900

    feat: create Car class
    자동차 클래스를 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,22 @@
 package racingcar;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Application {
+
+    public static class Car {
+        String name;
+        int move_cnt;
+
+        Car(String name, int move_cnt){
+            this.name = name;
+            this.move_cnt = move_cnt;
+        }
+    }
+
+
+    
 
     public static void inputCar() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
