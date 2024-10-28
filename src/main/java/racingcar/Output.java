6289a207702a5fa4e04ed816d472fc1b6988cdc1
Seongmo Ahn <djgood780@gmail.com>
2024-10-28 12:39:15 +0900

    feat(Output): 경주할 자동차 이름 입력 요청 메세지 출력 기능 추가

---
+++ src/main/java/racingcar/Output.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Output {
+    public static void printCarNameRequestMessage(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+}
