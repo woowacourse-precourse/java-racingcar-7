64f3eb69d8de1ff3c6a1f7628b1b245e9acf448f
jiyoung <jiyoung990601@gmail.com>
2024-10-28 22:00:02 +0900

    fix: 예외 상황 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -13,7 +13,9 @@ public class Application {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carsNameInput = Console.readLine();
-
+        if(Character.isDigit(carsNameInput.charAt(0))) {
+            throw new IllegalArgumentException();
+        }
         System.out.println("시도할 횟수는 몇 회인가요?");
         String chanceInput = Console.readLine();
 
