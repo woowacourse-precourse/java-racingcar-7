9b1bbfa88973c8bd9695479b149a65c6810fbf6e
hancrysta1 <hancrystal@sookmyung.ac.kr>
2024-10-28 23:28:31 +0900

    feat: 사용자로부터 자동차 이름 및 시도 횟수 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> carNames = getCarNames();
+        int attempts = getAttempts();
+    }
+
+    // 자동차 이름 입력 받기
+    private static List<String> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        return Arrays.asList(input.split(","));
+    }
+
+    // 시도 횟수 입력 받기
+    private static int getAttempts() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(Console.readLine());
     }
 }
