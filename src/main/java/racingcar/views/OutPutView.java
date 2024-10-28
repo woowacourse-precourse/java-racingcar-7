12c1aa5a4e439a4f34bcf0ebe2e27fa9abc42833
sangxxjin <sj_0368@naver.com>
2024-10-28 16:15:36 +0900

    feat: 중간 진행단계 출력 메서드 구현
    본문:
    Map을 사용해서 자동차들의 현재 진행상태를 받아오고 출력하게 구현했습니다.

--- src/main/java/racingcar/views/OutPutView.java
+++ src/main/java/racingcar/views/OutPutView.java
@@ -1,10 +1,24 @@
 package racingcar.views;
 
+import java.util.Map;
+
 public class OutPutView {
     private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String DISTANCE_SYMBOL = "-";
 
     public static void displayResultMessage() {
         System.out.println(RESULT_MESSAGE);
+    }
+
+    public static void displayCurrentCarsInfo(Map<String, Integer> carsInfo) {
+        carsInfo.forEach((carName, distance) -> {
+            System.out.println(formatCarInfo(carName, distance));
+        });
+        System.out.println();
+    }
+
+    private static String formatCarInfo(String carName, int distance) {
+        return carName + " : " + DISTANCE_SYMBOL.repeat(distance);
     }
 
 }
