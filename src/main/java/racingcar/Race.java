835b4318105eff4cb0fa3d39884b1b1e53e67a67
leehenson <hslee7231@gmail.com>
2024-10-28 12:58:16 +0900

    feat: Race 클래스 만들기

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Race {
+    private String carNames;
+    private final String tryCount;
+
+    public Race(String carNames, String tryCount) {
+        this.carNames = carNames;
+        this.tryCount = tryCount;
+    }
+}
