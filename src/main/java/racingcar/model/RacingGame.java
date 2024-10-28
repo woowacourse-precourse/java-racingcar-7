b1f617f7ccd7b8a04f34a758efd1fd6476205445
sanghee0820 <lsh277604@gmail.com>
2024-10-28 22:03:33 +0900

    feat: 라운드 진행 로직 추가

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -6,7 +6,7 @@ import racingcar.exception.RacingGameException;
 public class RacingGame {
     private static final Integer MINIMUM_ROUND = 1;
     private final RacingTrack racingTrack;
-    private final Integer round;
+    private Integer round;
 
     public RacingGame(Integer round) {
         this.racingTrack = new RacingTrack();
@@ -18,9 +18,23 @@ public class RacingGame {
         racingTrack.addCar(names);
     }
 
+    public List<String> playRound() {
+        if (isEnd()) {
+            throw new IllegalArgumentException(RacingGameException.GAME_IS_END.getMessage());
+        }
+        var stepInfo = racingTrack.step();
+        round--;
+
+        return stepInfo;
+    }
+
     private void validateRound(Integer round) {
         if (round < MINIMUM_ROUND) {
             throw new IllegalArgumentException(RacingGameException.ROUND_IS_LESS_THAN_ONE.getMessage());
         }
+    }
+
+    public boolean isEnd() {
+        return round == 0;
     }
 }
