6ff23de5ce21f5c6a756960e502057278839a097
persi <jina8151010@gmai.com>
2024-10-28 16:42:42 +0900

    chore: parseAndValidateCarNames 호출 시 불필요한 반환값 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,17 +1,14 @@
 package racingcar;
 
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
 
         String carNamesInput = InputView.getCarNames();
         RaceController raceController = new RaceController();
-        List<String> carNames = raceController.parseAndValidateCarNames(carNamesInput);
+        raceController.parseAndValidateCarNames(carNamesInput);
 
         int attempts = InputView.getAttempts();
 
         raceController.startRace(attempts);
-
     }
 }
