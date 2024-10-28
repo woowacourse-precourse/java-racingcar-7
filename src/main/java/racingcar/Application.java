5d822c8a21dfb093090142bd436502c04ec022c3
nsb329 <nsb329@naver.com>
2024-10-28 17:49:30 +0900

    자동차 이름 별 전진 값 저장 및 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,15 +8,14 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
 	public static void main(String[] args) {
 		String car[] = input_name();
-		int[] result = new int[car.length];
+		int[] result =new int[car.length];
 		int time = try_count();
 		System.out.println("실행 결과");
 		while (time > 0) {
 			time--;
-			dash_print(car, time, result);
+			dash_print(car, time,result);
 			System.out.println();
 		}
-		winner(car, result);
 	}
 
 	public static String[] input_name() {
@@ -49,7 +48,7 @@ public class Application {
 		return 1;
 	}
 
-	public static int[] random_value(String[] car, int time, int[] result) {
+	public static int[] random_value(String[] car, int time,int[] result) {
 		int random = 0;
 		for (int i = 0; i < result.length; i++) {
 			random = Randoms.pickNumberInRange(0, 9);
@@ -59,4 +58,8 @@ public class Application {
 		}
 		return result;
 	}
-}
+
+	public static void dash_print(String[] car, int time, int[] result) {
+		int[] count = random_value(car, time,result);
+		}
+	}
