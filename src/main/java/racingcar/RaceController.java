6e3aca1413d2361484c6dbe87ad7a791a0b26529
kimsongie12 <song3078@naver.com>
2024-10-28 16:20:37 +0900

    feat: 컨트롤러 기능 수정

--- src/main/java/racingcar/RaceController.java
+++ src/main/java/racingcar/RaceController.java
@@ -1,5 +1,30 @@
 package racingcar;
 
-public class RaceController {
+import java.util.List;
 
+public class RaceController {
+    private RaceGame game;
+    private RaceView view;
+
+    public RaceController(RaceView view){
+        this.view = view;
+    }
+    public void startGame(){
+        initializeGame();
+        playGame();
+        displayResults();
+    }
+    private void initializeGame(){
+        List<String> carNames = view.getCarNames();
+        int totalRounds = view.getTotalRounds();
+        this.game = new RaceGame(carNames, totalRounds, view);
+    }
+    private void playGame(){
+        game.playRounds();
+        view.displayRoundResults(game.getCars());
+    }
+    private void displayResults(){
+        List<Car> winners = game.getWinners();
+        view.displayWinners(winners);
+    }
 }
