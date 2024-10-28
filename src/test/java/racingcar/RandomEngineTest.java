82e9bd09634aa1f0243b331f270b7debb91019c3
david-parkk <ji0513ji@naver.com>
2024-10-28 21:48:46 +0900

    test: 요구사항 2 관련 테스트 추가
    - 랜덤_엔진은_확률이_100이면_무조건_전진한다
    - 랜덤_엔진은_확률이_0이면_무조건_정지한다

--- src/test/java/racingcar/RandomEngineTest.java
+++ src/test/java/racingcar/RandomEngineTest.java
@@ -19,4 +19,34 @@ public class RandomEngineTest {
         //then
         Assertions.assertThat(accelerate).isIn(0, moveDistance);
     }
+
+    @Test
+    void 랜덤_엔진은_확률이_100이면_무조건_전진한다() {
+        //given
+        int moveDistance = 11;
+        int randomRangeEnd = 0;
+        int accelerationThreshold = 0;
+        Engine engine = new RandomEngine(moveDistance, randomRangeEnd, accelerationThreshold);
+
+        //when
+        int accelerate = engine.accelerate();
+
+        //then
+        Assertions.assertThat(accelerate).isEqualTo(moveDistance);
+    }
+
+    @Test
+    void 랜덤_엔진은_확률이_0이면_무조건_정지한다() {
+        //given
+        int moveDistance = 1;
+        int randomRangeEnd = 1;
+        int accelerationThreshold = 2;
+        Engine engine = new RandomEngine(moveDistance, randomRangeEnd, accelerationThreshold);
+
+        //when
+        int accelerate = engine.accelerate();
+
+        //then
+        Assertions.assertThat(accelerate).isEqualTo(0);
+    }
 }
