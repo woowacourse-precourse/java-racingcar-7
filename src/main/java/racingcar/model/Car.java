e1b9bd5de3e22fa15a895fe01ae030ff5aa8b388
kimchanho97 <nh0903@pusan.ac.kr>
2024-10-28 21:16:55 +0900

    feat(Car): 난수에 따라 이동 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,11 +2,19 @@ package racingcar.model;
 
 public class Car {
 
+    public static final int MOVING_FORWARD = 4;
+
     private final CarName name;
     private int position;
 
     public Car(CarName name) {
         this.name = name;
+    }
+
+    public void moveRandomly(int randomNumber) {
+        if (randomNumber >= MOVING_FORWARD) {
+            position++;
+        }
     }
 
     public String getName() {
