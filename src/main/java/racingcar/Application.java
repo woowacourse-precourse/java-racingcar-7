e89ab6ee552d1c9ae49baf49ab6f4207547ae697
Choi Min Young <codingmy@naver.com>
2024-10-28 19:31:05 +0900

    feat<Application>: 순수 입력값 차 명으로 분리
    ,기준 pure String 분리해서 ArrayList로 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -12,6 +12,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String pureInput = getInputCarNames();
+        ArrayList<String> carName = new ArrayList<>(List.of(pureInput.split(",")));
     }
 
 
