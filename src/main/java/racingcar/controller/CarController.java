9bfc173131df508da6ce557c7b83d757084c9918
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-28 14:24:03 +0900

    refactor(Controller): ValidatorFacade와 RacingService를 통한 의존성 간소화
    - CarController에서 개별 검증 및 자동차 초기화 로직을 ValidatorFacade와 RacingService로 분리

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -4,24 +4,24 @@ import java.util.List;
 import racingcar.model.Car;
 import racingcar.model.CarNames;
 import racingcar.model.Cars;
-import racingcar.util.generator.Generator;
-import racingcar.util.movement.MoveStrategy;
+import racingcar.service.RacingService;
 import racingcar.util.parser.InputParser;
+import racingcar.validator.ValidatorFacade;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class CarController {
-    private final MoveStrategy moveStrategy;
+    private final RacingService racingService;
+    private final ValidatorFacade validatorFacade;
     private final InputParser inputParser;
-    private final Generator<Integer> generator;
     private final InputView inputView;
     private final OutputView outputView;
 
-    public CarController(MoveStrategy moveStrategy, InputParser inputParser,
-                         Generator<Integer> generator) {
-        this.moveStrategy = moveStrategy;
+    public CarController(RacingService racingService, ValidatorFacade validatorFacade,
+                         InputParser inputParser) {
+        this.racingService = racingService;
+        this.validatorFacade = validatorFacade;
         this.inputParser = inputParser;
-        this.generator = generator;
         this.inputView = InputView.getInstance();
         this.outputView = OutputView.getInstance();
     }
@@ -29,23 +29,29 @@ public class CarController {
     public void play() {
         String carNamesInput = inputView.getCarNames();
 
-        int count = Integer.parseInt(inputView.getCount());
-
-        Cars cars = new Cars();
-
-        CarNames carNames = inputParser.parse(carNamesInput);
-        for (String carName : carNames.getNames()) {
-            cars.addCar(new Car(carName.trim()));
+        // 입력 파싱 및 개별 이름 유효성 검사
+        CarNames names = inputParser.parse(carNamesInput);
+        for (String carName : names.getNames()) {
+            validatorFacade.validateCarNames(carName); // 개별 이름에 대한 유효성 검사
         }
 
+        validatorFacade.validateCarCount(names);
+
+        String countInput = inputView.getCount();
+
+        // 라운드 수 유효성 검사
+        validatorFacade.validateRoundCount(countInput);
+        int count = Integer.parseInt(countInput);
+
+        // 유효성 검사를 통과한 경우 게임 시작
+        Cars cars = racingService.initializeCars(names);
+
+        outputView.showResultMessage();
+        // 각 라운드 결과 표시
         for (int i = 0; i < count; i++) {
-            for (Car car : cars.getCars()) {
-                int number = generator.generate();
-                car.move(moveStrategy.canMove(number));
-            }
-            outputView.showRoundResult(cars);
+            racingService.playRound(cars); // 매 라운드 진행
+            outputView.showRoundResult(cars); // 매 라운드 결과 표시
         }
-
         List<Car> winners = cars.getWinners();
         outputView.showWinners(winners);
     }
