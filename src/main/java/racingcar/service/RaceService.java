9c31db1055456766f1dff20cdb8d3e1933ebc997
SinnoLn <sintmtn69@gmail.com>
2024-10-28 13:16:26 +0900

    feat(service): RaceService 경주 초기화 및 진행 로직 구현

---
+++ src/main/java/racingcar/service/RaceService.java
@@ -0,0 +1,39 @@
+package racingcar.service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.model.car.Car;
+import racingcar.model.car.CarFactory;
+import racingcar.model.car.MoveStrategy;
+import racingcar.model.race.RaceManager;
+
+public class RaceService {
+    private RaceManager raceManager;
+
+    public void initializeRace(
+            String[] carNames,
+            MoveStrategy strategy,
+            int tryCount
+    ) {
+        List<Car> cars = initializeCars(carNames, strategy);
+        this.raceManager = new RaceManager(cars, tryCount);
+    }
+
+    private List<Car> initializeCars(
+            String[] carNames,
+            MoveStrategy strategy
+    ) {
+        return Arrays.stream(carNames)
+                .map(name -> CarFactory.createCar(name, strategy))
+                .collect(Collectors.toList());
+    }
+
+    public void moveCars() {
+        raceManager.startRace();
+    }
+
+    public List<String> getWinners() {
+        return raceManager.getWinners();
+    }
+}
