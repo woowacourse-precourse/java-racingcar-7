2166ec059684eef8b56cc2832f0ea88cd817c91d
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-28 19:24:05 +0900

    refactor: 입력 메시지를 InputMessages enum으로 분리하여 관리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,25 +1,24 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.message.ExceptionMessages;
+import racingcar.message.ErrorMessage;
+import racingcar.message.InputMessage;
 import racingcar.exception.RoundException;
 
 public class InputView {
-    private static final String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String ROUND_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private static final String ROUND_FORMAT = "\\d+";
 
     public String getCarNames() {
-        System.out.println(CAR_INPUT_MESSAGE);
+        System.out.println(InputMessage.CAR_INPUT.getMessage());
         return Console.readLine();
     }
 
     public int getRound() {
-        System.out.println(ROUND_INPUT_MESSAGE);
+        System.out.println(InputMessage.ROUND_INPUT.getMessage());
         String input = Console.readLine();
 
         if (!input.matches(ROUND_FORMAT)) {
-            throw new RoundException(ExceptionMessages.INVALID_NUMBER_FORMAT.getMessage());
+            throw new RoundException(ErrorMessage.INVALID_NUMBER_FORMAT.getMessage());
         }
 
         return Integer.parseInt(input);
