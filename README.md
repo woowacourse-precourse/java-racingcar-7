b5febc1eadf40568bb738660e2a1bcd8fc5d57a9
heehun <slrkqk2@naver.com>
2024-10-28 12:51:48 +0900

    feat(RacingCarController): 프로젝트 내 클래스를 이용하여 게임을 진행

--- README.md
+++ README.md
@@ -1,19 +1,6 @@
 # java-racingcar-precourse
 
--------
-# 공통 피드백에서 잘 못지키는 것들 
-- 디버거 사용 
-- 이름을 통해 의도를 드러내자 
-- 코드 포매팅 사용
-  - Ctrl + Alt + L
-# java-racingcar-precourse
-
 ---
-# 공통 피드백에서 잘 못지키는 것들
-- 디버거 사용
-- 이름을 통해 의도를 드러내자
-- 코드 포매팅 사용
-  - `Ctrl + Alt + L`
 # 👷기능 요구사항 정의
 ## Utils
 - [x] **InputValidator**
@@ -22,8 +9,9 @@
       - [x] 적절한 입력을 제외하고 모두 에러처리 - 적절한 입력을 찾는 정규식 찾고 그 외의 경우 `IllegalArgumentException`
     - [x] 횟수
       - [x] 잘못된 입력을 받았을 때 `IllegalArgumentException`이 나오게 문자열을 입력으로 받고 정수인지 검증
-- [] **InputParser**
-  - [] 사용자 입력에서 CarName 을 파싱
+- [x] **InputParser**
+  - [x] 사용자 입력에서 CarName 을 파싱
+  - [x] 사용자 입력에서 tryNum 을 파싱
 ## Domain
 - [x] **Car**
   - [x] 이름, 위치
@@ -52,12 +40,18 @@
     - [x] 'Winner' List 전달
 
 ## Controller
-- [ ] **GameController**
-  - [ ] Game 을 실행
+- [x] **GameController**
+  - [x] Game 을 실행
 
 ---
 
 ## 추가된 코드 컨벤션 지키기
-- `indent` <= 2
-- 3항 연산자 사용 금지
-- 함수(메서드)는 최대한 하는 일 작게 유지하기
+-[x] `indent` <= 2 
+-[x] 3항 연산자 사용 금지
+-[x] 함수(메서드)는 최대한 하는 일 작게 유지하기
+
+# 공통 피드백에서 평소 잘 지키지 않는 것
+- 디버거 사용
+- 이름을 통해 의도를 드러내자
+- 코드 포매팅 사용
+  - `Ctrl + Alt + L`
--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,2 +1,39 @@
-package racingcar.controller;public class RacingCarController {
+package racingcar.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.GameRule;
+import racingcar.domain.RacingGame;
+import racingcar.domain.RandomRule;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingCarController {
+
+    public void run() {
+        List<Car> carList = new ArrayList<>();
+        List<String> winners = new ArrayList<>();
+        GameRule gameRule = new RandomRule();
+
+        List<String> carNames = InputView.getCarNames();
+        int tryNum = InputView.getTryNum();
+
+        for (String carName : carNames) {
+            Car car = new Car(carName,0);
+            carList.add(car);
+        }
+        Cars cars = new Cars(carList);
+        RacingGame racingGame = new RacingGame(cars);
+
+        OutputView.printExecutionResult();
+        for (int i = 0; i < tryNum; i++) {
+            racingGame.game(gameRule);
+            OutputView.printMidResult(cars);
+        }
+
+        winners = cars.findWinner();
+        OutputView.printResult(winners);
+    }
 }
