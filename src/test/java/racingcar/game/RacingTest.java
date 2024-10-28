747c23399fdb9b651f2b48a90b86b9588c9e583e
ikjeong <jipark7937@gmail.com>
2024-10-28 12:33:17 +0900

    test: add testcase for racing
    난수가 지정된 상황의 테스트케이스 추가
    단일 우승자, 다중 우승자, 이동 상태 검증 테스트케이스 추가

--- src/test/java/racingcar/game/RacingTest.java
+++ src/test/java/racingcar/game/RacingTest.java
@@ -5,36 +5,73 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingTest {
 
-    @DisplayName("우승자 검증")
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    @DisplayName("단일 우승자 검증")
     @Test
-    void validateWinner() {
+    void validateSingleWinner() {
+        //given
+        List<Car> cars = CarFactory.createCars("pobi,woni");
+        Racing racing = new Racing(cars, 1);
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    // when
+                    racing.start();
+
+                    // then
+                    List<Car> winners = racing.getWinners();
+                    assertThat(winners).hasSize(1);
+                    assertThat(winners.get(0).getName()).isEqualTo("pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @DisplayName("다중 우승자 검증")
+    @Test
+    void validateMultipleWinner() {
         // given
         List<Car> cars = CarFactory.createCars("pobi,woni,jun");
-        Racing racing = new Racing(cars, 5);
+        Racing racing = new Racing(cars, 1);
 
-        // when
-        racing.start();
+        assertRandomNumberInRangeTest(
+                () -> {
+                    // when
+                    racing.start();
 
-        // then
-        List<Car> winners = racing.getWinners();
+                    // then
+                    List<Car> winners = racing.getWinners();
+                    assertThat(winners).hasSize(2);
+                    assertThat(winners.get(0).getName()).isEqualTo("pobi");
+                    assertThat(winners.get(1).getName()).isEqualTo("woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, STOP
+        );
+    }
 
-        // winner 위치가 서로 같아야 함
-        int winnerPosition = winners.get(0).getPosition();
-        for (Car car : winners) {
-            assertEquals(winnerPosition, car.getPosition());
-        }
+    @DisplayName("이동 상태 검증")
+    @Test
+    void validateMove() {
+        // given
+        List<Car> cars = CarFactory.createCars("pobi");
+        Racing racing = new Racing(cars, 2);
 
-        // winner를 제외한 나머지 차량은 winner보다 위치가 작아야 함
-        for (Car car : cars) {
-            if (winners.contains(car)) {
-                continue;
-            }
-            assertTrue(car.getPosition() < winnerPosition);
-        }
+        assertRandomNumberInRangeTest(
+                () -> {
+                    // when
+                    racing.start();
 
+                    // then
+                    assertThat(cars.get(0).getPosition()).isEqualTo(1);
+                },
+                MOVING_FORWARD, STOP
+        );
     }
 }
