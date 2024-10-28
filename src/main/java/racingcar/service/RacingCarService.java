7f5c49afe43a7b91086ec5de7a2094d0472d87db
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-10-28 17:49:56 +0900

    feat : 메인로직: 자동차 경주 구현

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -1,23 +1,26 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.model.RacingCar;
 import racingcar.validator.Validator;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCarService {
     private final InputView inputView;
-
-    public RacingCarService(InputView inputView) {
+    private final OutputView outputView;
+    Validator validator = new Validator();
+    public RacingCarService(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
+        this.outputView = outputView;
     }
 
-    public List<RacingCar> init(){
+    public List<RacingCar> inputCars(){
         String names = inputView.readNames();
         String[] nameList=names.split(",");
-        Validator validator = new Validator();
         List<RacingCar> racingCars = new ArrayList<>();
 
         for (String name : nameList) {
@@ -28,4 +31,50 @@ public class RacingCarService {
         return racingCars;
     }
 
+    public Integer inputTrial() {
+        String trial = inputView.readTrial();
+        validator.trialValidate(trial);
+        return Integer.parseInt(trial);
+    }
+
+    public void racing(List<RacingCar> cars, int trial) {
+        int randomNum=0;
+        for (int i=0; i<trial; i++){
+            carGoAhead(cars);
+            outputView.printResult(cars);
+        }
+    }
+
+    private void carGoAhead(List<RacingCar> cars) {
+
+        for (RacingCar car : cars) {
+            car.goAhead(Randoms.pickNumberInRange(0,9));
+        }
+    }
+
+    public List <String> getResult(List<RacingCar> cars) {
+        List<String> result = new ArrayList<>();
+        int maxDistance = getMaxDistance(cars);
+        for (RacingCar car : cars) {
+            if (car.getDistance() == maxDistance) {
+                result.add(car.getName());
+            }
+        }
+        return result;
+    }
+
+    public void printWinners(List<String> winners) {
+        outputView.printWinner(winners);
+    }
+
+
+    private int getMaxDistance(List<RacingCar> cars) {
+        int max=0;
+        for (RacingCar car : cars) {
+            if (car.getDistance() > max) {
+                max = car.getDistance();
+            }
+        }
+        return max;
+    }
 }
