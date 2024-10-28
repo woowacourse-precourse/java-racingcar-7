13ebec91e3fb741b9acc958378dfb2837e03889b
mirolis6636 <mirolis6636@gmail.com>
2024-10-28 14:23:24 +0900

    feat: readCarNames 메서드에 빈 입력에 대한 예외 대응문 추가

--- src/main/java/racingcar/InputReader.java
+++ src/main/java/racingcar/InputReader.java
@@ -1,20 +1,20 @@
 package racingcar;
 
 import java.util.HashMap;
+import java.util.NoSuchElementException;
 
 public class InputReader {
     public String readCarNames(){
-        String userInput = camp.nextstep.edu.missionutils.Console.readLine();
-        return userInput;
+        try {
+            String userInput = camp.nextstep.edu.missionutils.Console.readLine();
+            return userInput;
+        }
+        catch(NoSuchElementException e){
+            throw new NoSuchElementException("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        }
     }
     public int readNumberOfTrial(){
         String userInput = camp.nextstep.edu.missionutils.Console.readLine();
         return Integer.parseInt(userInput);
-    }
-    public void showNumberOfMovement(String[] carNames, HashMap<String, Integer> movementRecord){
-        for(String car : carNames){
-            System.out.printf("%s : %s\n", car, "-".repeat(movementRecord.get(car)));
-        }
-        System.out.println();
     }
 }
