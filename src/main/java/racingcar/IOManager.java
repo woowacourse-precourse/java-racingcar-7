52d6ed25f5043bf5dad6be85d3eae6c3a2b14bf0
moeun9309 <moeun9309@gmail.com>
2024-10-28 12:08:03 +0900

    refactor: input library change

--- src/main/java/racingcar/IOManager.java
+++ src/main/java/racingcar/IOManager.java
@@ -1,24 +1,23 @@
 package racingcar;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class IOManager {
     static public void carInput() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = scanner.nextLine();
+        String input = Console.readLine();
 
         input = input.trim();
         carNameInputCheck(input);
 
-        turnInput(scanner);
+        turnInput();
 
-        scanner.close();
+        Console.close();
     }
 
-    static private void turnInput(Scanner scanner) {
+    static private void turnInput() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        String input = scanner.nextLine();
+        String input = Console.readLine();
 
         try {
             int turn = Integer.parseInt(input);
