6dbaa0474e5574fa618b6cceaf2c148fa6e75c36
jody816 <81346079+jody816@users.noreply.github.com>
2024-10-28 18:41:02 +0900

    test: 초기 세팅 메서드 로직 구현

--- src/test/java/racingcar/application/service/RacingServiceTest.java
+++ src/test/java/racingcar/application/service/RacingServiceTest.java
@@ -8,6 +8,14 @@ class RacingServiceTest {
 
   @Test
   void setup() {
+    RacingService racingService = new RacingService();
+    String inputCarNames = "pobi,woni,jun";
+    int inputAttemptCount = 5;
+
+    racingService.setup(inputCarNames, inputAttemptCount);
+
+    assertEquals(5, racingService.getAttemptCount());
+    assertEquals(3, racingService.playRound().size());
   }
 
   @Test
