23d7a40bcc3bd322c745ba183d8ac4923e524ae8
hojae <ghwo336@naver.com>
2024-10-28 17:05:11 +0900

    fix: 유저이름이 중복되는 경우 해결RacingGame 수정

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -3,10 +3,13 @@ package racingcar.model;
 import racingcar.config.Config;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RacingGame {
     private final List<Car> cars =new ArrayList<>();
+    private final Set<String> userNames= new HashSet<>();
     Config config=new Config();
 
     public List<Car> getCars() {
@@ -14,8 +17,12 @@ public class RacingGame {
     }
 
     public void addCar(String s){
+        if (userNames.contains(s)){
+            throw new IllegalArgumentException("이미 존재하는 이름 입니다.");
+        }
         Car car=config.car(s);
         cars.add(car);
+        userNames.add(s);
     }
 
 }
