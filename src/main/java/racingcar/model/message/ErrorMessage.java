60f454e885f6ec71fb72ae5406ba271c0f8cd7e8
Chan531 <chan657312@gmail.com>
2024-10-28 18:13:18 +0900

    feat(ErrorMessage): 중복 자동차 이름 관련 에러 메세지 추가

--- src/main/java/racingcar/model/message/ErrorMessage.java
+++ src/main/java/racingcar/model/message/ErrorMessage.java
@@ -2,6 +2,7 @@ package racingcar.model.message;
 
 public enum ErrorMessage {
 
+	INVALID_CAR_NAMES_INPUT("자동차 이름은 중복될 수 없습니다."),
 	INVALID_CAR_NAME_LENGTH("자동차 이름의 길이는 1 이상 5 이하여야 합니다."),
 	INVALID_TRIAL_COUNT("시도할 횟수 입력이 유효하지 않습니다.");
 
