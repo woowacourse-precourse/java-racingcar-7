dc44a3d8afb612d0221e31643a53e36f9a759192
qudcks8084 <qudcks8084@naver.com>
2024-10-28 21:17:47 +0900

    Fix : Refactor RacingCarController
    - Devide each method by minumum role
    - Add 3 static method

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -11,30 +11,42 @@ import static racingcar.service.RacingCarServiceImpl.*;
 import static racingcar.util.RacingCarNameValidator.*;
 import static racingcar.util.RacingNumberValidator.*;
 import static racingcar.view.InputView.*;
+import static racingcar.view.InputView.getNumberOfRaceFromUser;
 import static racingcar.view.OutputView.*;
 
 public class RacingCarController {
     public static void run(){
 
-        // 사용자의 이름을 입력받고, 올바른지 확인
+        // DTO 객체 생성 (View - Controller - Util)
+        RacingCarDTO racingCarDTO = new RacingCarDTO(getRacingCarNameFromUserInController(), getNumberOfRaceFromUser());
+
+        // N회차 반복 실행 (Controller - Service)
+        RaceNTimeInController(racingCarDTO);
+
+        // 최종 우승자 출력 (Controller - View)
+        printWinnerOfRacing(racingCarDTO);
+
+    }
+
+    // 사용자에게 이름을 입력받고, 올바른지 확인
+    static String getRacingCarNameFromUserInController(){
         String racingCarNameString = getRacingCarNameFromUser();
         checkRacingCarNameListIsAvailable(racingCarNameString);
+        return racingCarNameString;
+    }
 
-        // 사용자의 실행 횟수를 입력받고, 올바른지 확인
+    // 사용자에게 실행 횟수를 입력받고, 올바른지 확인
+    static String getNumberOfRaceFromUserInController(){
         String numberOfRacingString = getNumberOfRaceFromUser();
         checkNumberOfRacingIsAvailable(numberOfRacingString);
+        return numberOfRacingString;
+    }
 
-        // DTO 인스턴스 생성
-        RacingCarDTO racingCarDTO = new RacingCarDTO(racingCarNameString, numberOfRacingString);
-
-        // 사용자가 입력한 실행 횟수를 기반으로 반복 실행
+    // 사용자가 입력한 실행 횟수를 기반으로 반복 실행
+    static void RaceNTimeInController(RacingCarDTO racingCarDTO){
         for (int i = 0; i < racingCarDTO.getNumberOfRacing(); i++) {
             moveRacingCarByRandom(racingCarDTO);
             printEachDistanceByRandomMovement(racingCarDTO);
         }
-
-        // 최종 우승자 출력
-        printWinnerOfRacing(racingCarDTO);
-
     }
 }
