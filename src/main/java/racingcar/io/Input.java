cef019457e5b4920ada731999684fed40678777e
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-10-28 12:02:34 +0900

    feat: 경주할 자동차 입력 기능 구현
    경주할 자동차 입력 문자열을 콘솔에서 받아오는 기능을 구현했습니다.

--- src/main/java/racingcar/io/Input.java
+++ src/main/java/racingcar/io/Input.java
@@ -1,8 +1,11 @@
 package racingcar.io;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
     public String readCar() {
-        return null; // 경주할 자동차 반환
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
     }
 
     public int readNumberOfMove() {
