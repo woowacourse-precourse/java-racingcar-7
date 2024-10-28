aee8b066daded056b07a4784b87d926ce1d5ad98
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-28 19:14:32 +0900

    feat: 게임 실행 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.controller.RacingGameController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        RacingController controller = new RacingController(inputView, outputView);
+
+        controller.playGame();
     }
 }
