d42ce7bc06361ef9f62967e936a85b3550079090
rndnjsgud <00kwh@naver.com>
2024-10-28 20:53:48 +0900

    feat: 출력 관련 기능 구현 각 racingCar의 중간 결과를 출력, 우승자의 위치 확인, 최종 결과 출력 구현

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -1,5 +1,45 @@
 package racingcar.view;
 
-public class Output {
+import racingcar.Domain.RacingCar;
+import racingcar.Domain.RacingCars;
 
+import java.util.List;
+
+public class Output {
+    private static final String RESULT = "실행결과\n";
+    private static final String FINAL_RESULT = "가 최종 우승했습니다";
+    public static void printResults(RacingCars racingCars){
+        System.out.print(RESULT);
+        List<RacingCar> racingCarList = racingCars.getRacingCars();
+        racingCarList.stream()
+                .forEach(cars -> System.out.println(cars.getCarName() + " : " + "-".repeat( cars.getCarPosition())));
+        System.out.println();
+    }
+
+    public static int findWinnersPosition(RacingCars racingCars){
+        List<RacingCar> racingCarList = racingCars.getRacingCars();
+        int maxPosition = 0;
+        for(int i = 0 ; i < racingCarList.size() ; i++){
+            maxPosition = findBigger(racingCarList.get(i).getCarPosition(), maxPosition);
+        }
+        return maxPosition;
+    }
+
+    public static int findBigger(int n, int m){
+        if(n > m){
+            return n;
+        }
+        return m;
+    }
+
+    public static void printFinalResult(RacingCars racingCars){
+        List<RacingCar> racingCarList = racingCars.getRacingCars();
+        int winnersPosition = findWinnersPosition(racingCars);
+        for(int i = 0 ; i < racingCarList.size() ; i++){
+            if(racingCarList.get(i).getCarPosition() == winnersPosition){
+                System.out.print(racingCarList.get(i).getCarName() + ", ");
+            }
+        }
+        System.out.print("\b\b" + FINAL_RESULT);
+    }
 }
