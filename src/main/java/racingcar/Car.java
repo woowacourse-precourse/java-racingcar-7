98cbb48b4e7af2e9e281419c12ecb0bb5a8a7378
woohyuk <7dngur7@naver.com>
2024-10-28 17:41:13 +0900

    refactor(Car): 이동 로직을 캡슐화하고 이름 유효성 검사 추가
    - move 메서드에 랜덤 숫자를 인자로 받아 이동 로직을 캡슐화
    - 자동차 이름의 유효성을 검사하는 메서드 추가
    - 예외 처리를 통해 잘못된 이름 입력을 방지

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,16 +1,27 @@
 package racingcar;
 
 public class Car {
+    private static final int FORWARD_THRESHOLD = 4;
+
     private final String name;
     private int position;
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
         this.position = 0;
     }
 
-    public void move() {
-        this.position++;
+    public void move(int randomNumber) {
+        if (randomNumber >= FORWARD_THRESHOLD) {
+            position++;
+        }
+    }
+
+    public static void validateName(String name) {
+        if (name.length() == 0 || name.length() > 5) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_CAR_NAME);
+        }
     }
 
     public int getPosition() {
