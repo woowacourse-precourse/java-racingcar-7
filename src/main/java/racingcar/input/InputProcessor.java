1a8a0d1e559672c07fda89b59b5d64ff6875ff63
Hyunuk <dksgusdnr17@naver.com>
2024-10-28 21:02:29 +0900

    feat(input): add method to validate empty input

--- src/main/java/racingcar/input/InputProcessor.java
+++ src/main/java/racingcar/input/InputProcessor.java
@@ -3,6 +3,7 @@ package racingcar.input;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import racingcar.domain.Car;
+import racingcar.exception.ErrorMessage;
 
 public class InputProcessor {
     private static InputProcessor instance;
@@ -24,6 +25,17 @@ public class InputProcessor {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputString = Console.readLine().trim();
 
+        validateEmpty(inputString);
+        if (inputString.isEmpty()) {
+            throw new IllegalStateException(ErrorMessage.EMPTY_INPUT.getValue());
+        }
+
         return stringParser.parse(inputString);
+    }
+
+    private void validateEmpty(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalStateException(ErrorMessage.EMPTY_INPUT.getValue());
+        }
     }
 }
