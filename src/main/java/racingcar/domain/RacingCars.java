278aae7b818e893c3a8c8489f9a84251477d0b79
변우영 <likevangogh24@gmail.com>
2024-10-28 18:32:42 +0900

    refactor: RacingCars 클래스의 일부를 다른 클래스로 분리
    - RacingCars가 너무 많은 책임을 지고 있는 것 같아 몇 가지 기능을 분리했다.
    - RacingCars는 랜덤 숫자에 따라 경주 자동차들을 움직이고 자동차 상태 반환을 담당하도록 했다.
    - RacingCenter는 경주 참가 등록을 담당하도록 했다.
    - RacingResult는 최종 우승자를 선정을 담당하도록 했다.

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -1,24 +1,15 @@
 package racingcar.domain;
 
 import racingcar.dto.CarStatusDto;
-import racingcar.exception.DuplicateNameException;
-import racingcar.exception.MaxRacingCarCountException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCars {
 
-    private static final int CAR_COUNT_THRESHOLD = 10;
+    private final List<Car> racingCars;
 
-    private final List<Car> racingCars = new ArrayList<>();
-
-    public void registerCars(List<String> carNames) {
-        validateUniqueName(carNames);
-        validateRacingCarCount(carNames);
-        carNames.stream()
-                .map(Car::new)
-                .forEach(racingCars::add);
+    public RacingCars(List<Car> cars) {
+        racingCars = cars;
     }
 
     public void updatePositionsWithRandomNumbers(List<Integer> randomNumbers) {
@@ -32,34 +23,5 @@ public class RacingCars {
         return racingCars.stream()
                 .map(Car::toCarStatusDto)
                 .toList();
-    }
-
-    public List<String> selectWinners() {
-        List<CarStatusDto> raceResults = getStatus();
-        int maxPosition = getMaxPosition(raceResults);
-
-        return raceResults.stream()
-                .filter(raceResult -> raceResult.position() == maxPosition)
-                .map(CarStatusDto::name)
-                .toList();
-    }
-
-    private int getMaxPosition(List<CarStatusDto> raceResults) {
-        return raceResults.stream()
-                .mapToInt(CarStatusDto::position)
-                .max()
-                .orElse(0);
-    }
-
-    private void validateUniqueName(List<String> names) {
-        if (names.size() != names.stream().distinct().count()) {
-            throw new DuplicateNameException();
-        }
-    }
-
-    private void validateRacingCarCount(List<String> carNames) {
-        if (carNames.size() > CAR_COUNT_THRESHOLD) {
-            throw new MaxRacingCarCountException();
-        }
     }
 }
