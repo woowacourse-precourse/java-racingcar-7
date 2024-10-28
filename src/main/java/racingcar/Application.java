da4792a6a2ab0d5c19a0a2bc1af4f454c6dbbc27
jeong-baechoo <baechoo0905@naver.com>
2024-10-28 20:15:17 +0900

    refactor: 메인 메서드에서 컨트롤러 메서드 호출 변경
    - play에서 start로 변경된 메서드명 적용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,6 @@ import racingcar.controller.RacingGameController;
 public class Application {
     public static void main(String[] args) {
         RacingGameController controller = new RacingGameController();
-        controller.play();
+        controller.start();
     }
 }
