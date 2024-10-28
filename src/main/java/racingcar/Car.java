e7a036882a00b0e923fdaebe7e160a58c32182c8
heeyeop Kim <ssucgoo@gmail.com>
2024-10-28 21:36:22 +0900

    feat: position 참조 함수 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -34,4 +34,8 @@ public class Car implements Comparable<Car> {
     public int compareTo(Car o) {
         return o.position - position;
     }
+
+    public int getPosition() {
+        return position;
+    }
 }
