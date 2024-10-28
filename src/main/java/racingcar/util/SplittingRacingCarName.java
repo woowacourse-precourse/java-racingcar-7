d18954dc3e3afd1017925b73702c0e46e086bc0e
young-in <hardworkingini@gmail.com>
2024-10-28 20:45:21 +0900

    refactor : SplittingRacingCarName을 정적 유틸리티 클래스로 변경하여 인스턴스 생성 없이 사용 가능하게 리팩토링

--- src/main/java/racingcar/util/SplittingRacingCarName.java
+++ src/main/java/racingcar/util/SplittingRacingCarName.java
@@ -3,6 +3,9 @@ package racingcar.util;
 public class SplittingRacingCarName {
     private static final String STRING_SPLITTER = ",";
 
+    private SplittingRacingCarName() {
+
+    }
     public static String[] splitRacingCarName(String racingCarNames) {
         return racingCarNames.split(STRING_SPLITTER);
     }
