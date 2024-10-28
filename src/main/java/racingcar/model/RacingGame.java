d8c8a23c1c0559b26b59e3e57c1bfb52267007e1
BusanGukbap <dong2857210@gmail.com>
2024-10-28 15:27:24 +0900

    feat($model) : create class - RacingGame

---
+++ src/main/java/racingcar/model/RacingGame.java
@@ -0,0 +1,17 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+
+    private final List<Car> cars;
+
+    public RacingGame(List<String> carNames) {
+
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
