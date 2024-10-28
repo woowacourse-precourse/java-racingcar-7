65677906ef94ca5d702ee11b5e751d1a4b623766
JEANS <songkc5671@naver.com>
2024-10-28 23:02:00 +0900

    refactor : validate, 테스트 부분
    validate 부분 시각 간편화, 테스트 코드 재작성.

--- src/main/java/racingcar/RacingCarController.java
+++ src/main/java/racingcar/RacingCarController.java
@@ -1,13 +1,12 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
 import racingcar.raceDto.RacingRequest;
 import racingcar.raceDto.RacingResponse;
 
 public class RacingCarController {
 
-    RacingCarView racingCarView = new RacingCarView();
-    RacingCarModel racingCarModel = new RacingCarModel();
+    RacingCarView racingCarView;
+    RacingCarModel racingCarModel;
 
     RacingCarController(RacingCarView racingCarView, RacingCarModel racingCarModel) {
         this.racingCarView = racingCarView;
