726dd6bdc9f80a0df549fe32d799dec87edac16b
JiHyeonSu <itip1@mju.ac.kr>
2024-10-28 18:20:22 +0900

    feat(InputValidator):  추가 검증 메소드 추가 -자동차 이름이 동일할 수 없도록 메소드를 추가했습니다. -대소문자 구분 없이 자동차 이름은 동일할 수 없습니다.

--- src/main/java/racingcar/common/validator/InputValidator.java
+++ src/main/java/racingcar/common/validator/InputValidator.java
@@ -1,14 +1,21 @@
 package racingcar.common.validator;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class InputValidator {
 
   public static void validateCarNames(List<String> carNames) {
-    if (carNames == null || carNames.stream().allMatch(String::isEmpty)) {
+    if (carNames == null) {
       throw new IllegalArgumentException("자동차 이름은 빈 문자열이 올 수 없습니다.");
     }
 
+    if (carNames.stream().anyMatch(String::isBlank)) {
+      throw new IllegalArgumentException("자동차 이름에 빈 문자열이나 공백만 포함된 이름이 올 수 없습니다.");
+    }
+
+    validateUniqueNames(carNames);
     carNames.forEach(InputValidator::validateCarName);
   }
 
@@ -22,6 +29,14 @@ public class InputValidator {
   public static void validateAttempts(int attempts) {
     if (attempts <= 0) {
       throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+    }
+  }
+
+  private static void validateUniqueNames(List<String> carNames) {
+    Set<String> uniqueNames = carNames.stream().map(String::toLowerCase)
+        .collect(Collectors.toSet());
+    if (uniqueNames.size() < carNames.size()) {
+      throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
     }
   }
 }
