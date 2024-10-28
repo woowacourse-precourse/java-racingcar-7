78a48337734eeae68e06efbcd89e3ebb38c0acbe
gyuho <kyuhokim12@gmail.com>
2024-10-28 14:42:34 +0900

    refactor(OutputView): 메소드와 변수의 이름을 직관적으로 변경
    -printExecutionStart 메소드의 이름은 게임 시작 메시지이므로 printStartGame으로, EXECUTION_RESULT_MESSAGE 변수를 RACE_START_MESSAGE로 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,11 +5,11 @@ import racingcar.domain.Car;
 import java.util.List;
 
 public class OutputView {
-    private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
+    private static final String RACE_START_MESSAGE = "실행 결과";
     private static final String WINNER_MESSAGE = "최종 우승자 : ";
 
-    public static void printExecutionStart() {
-        System.out.println(EXECUTION_RESULT_MESSAGE);
+    public static void printStartGame() {
+        System.out.println(RACE_START_MESSAGE);
     }
 
     public static void printCarPositions(List<Car> cars) {
