726c0cc9c5915dabd4da014262e3cd69112094bb
kim-se-hee <saree98@naver.com>
2024-10-28 16:55:05 +0900

    feat(RacingSnapshotProvider): 플레이어의 경주 상황 제공 클래스 구현
    RacingSnapshotProvider에 플레이어의 현재 경주 상태 반환 로직 추가
    - 플레이어의 이동 거리를 이용해 이동 경로를 반환

--- src/main/java/racingcar/RacingSnapshotProvider.java
+++ src/main/java/racingcar/RacingSnapshotProvider.java
@@ -1,2 +1,17 @@
-package racingcar;public class RacingSnapshotProvider {
+package racingcar;
+
+public class RacingSnapshotProvider {
+
+    public static final String PATH_SIGN = "-";
+
+    public String getSnapshotOf(Player player) {
+        int distance = player.getDistance();
+
+        String path = "";
+        for (int i = 0; i < distance; i++) {
+            path = path.concat(PATH_SIGN);
+        }
+
+        return player.getName() + " : " + path;
+    }
 }
