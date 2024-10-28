d44972be8d61bd0e382456ed5b037fae39e3de86
minseo <alstj778@gmail.com>
2024-10-28 23:23:34 +0900

    test(ApplicationTest): 잘못된 사용자 입력 테스트 케이스 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,9 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.constant.ExceptionMessage;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -28,6 +30,36 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("자동차 이름에 빈 문자열을 입력시 예외가 발생한다.")
+    void testInputEmptyStringNames() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(ExceptionMessage.INPUT_EMPTY_STRING.getMessage())
+        );
+    }
+
+    @Test
+    @DisplayName("횟수에 숫자가 아닌 값을 입력시 예외가 발생한다.")
+    void testInputNonDigitTrialCount() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("test1,test2", "a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(ExceptionMessage.INPUT_NON_DIGIT_CHARACTER.getMessage())
+        );
+    }
+
+    @Test
+    @DisplayName("횟수에 정수 범위를 초과하는 값을 입력시 예외가 발생한다.")
+    void testInputOverFlowTrialCount() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("test1,test2", "99999999999"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(ExceptionMessage.INPUT_INVALID_INTEGER_RANGE.getMessage())
         );
     }
 
