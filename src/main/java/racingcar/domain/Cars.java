df870b6795ba7a8817f13c91d2be5da1724c5be0
yseo14 <lmslys14@naver.com>
2024-10-28 17:34:46 +0900

    feat: 우승자 결정 기능 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 import racingcar.validation.NameValidator;
 
-public class Cars implements Iterable<Car>{
+public class Cars implements Iterable<Car> {
     private List<Car> cars;
 
     public Cars() {
@@ -17,6 +17,21 @@ public class Cars implements Iterable<Car>{
         for (String name : nameList) {
             cars.add(new Car(name));
         }
+    }
+
+    public List<Car> getWinners() {
+        List<Car> winners = new ArrayList<>();
+        int maxDistance = cars.stream()
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElse(0);
+
+        for (Car car : cars) {
+            if (car.getDistance() == maxDistance) {
+                winners.add(car);
+            }
+        }
+        return winners;
     }
 
     @Override
