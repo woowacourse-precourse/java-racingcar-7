affc4417ab901945cb8c73e93dcc9dd2fea4e565
JMJ <ghtntkdnsk@naver.com>
2024-10-28 21:04:41 +0900

    feat(RacingGameController) : 게임 흐름을 관리하는 RacingGameController 추가

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,2 +1,30 @@
-package racingcar.controller;public class RacingGameController {
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.RacingGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingGameController {
+
+    public void runGame() {
+        OutputView.printCarNamePrompt();
+        List<String> carNames = InputView.inputCarNames();
+        List<Car> cars = carNames.stream().map(Car::new).toList();
+
+        OutputView.printTryCountPrompt();
+        int tryCount = InputView.inputTryCount();
+
+        RacingGame racingGame = new RacingGame(cars, tryCount);
+
+        OutputView.printRoundResultsHeader();
+        for (int i = 0; i < tryCount; i++) {
+            racingGame.start();
+            OutputView.printRoundResults(racingGame.getCars());
+        }
+
+        List<Car> winners = racingGame.getWinners();
+        OutputView.printWinners(winners);
+    }
 }
