57f333a67cbf380534888b42c61ccc5b3798cb78
yallo428 <yallo428@naver.com>
2024-10-28 14:14:20 +0900

    feat(Car) : 자동차 구현
    자동차 구현:
    - 이름의 길이 검증
    - 4이상 나올 시 앞으로 이동 후 점수 증가
    - 현재 점수 반환
    - 우승자 이름 반환

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,54 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.vo.ExceptionMessage;
+
+public class Car {
+
+    private static final int MINIMUM_MOVE_VALUE = 4;
+    private static final int DEFAULT_MOVE_COUNT = 0;
+
+    private final String name;
+    private int moveCount;
+
+    public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_CAR_NAME_LENGTH.getMessage());
+        }
+        this.name = name;
+        this.moveCount = DEFAULT_MOVE_COUNT;
+    }
+
+    public void addScore() {
+        this.moveCount++;
+    }
+
+    public boolean isForwardMovable() {
+        return Randoms.pickNumberInRange(0, 9) >= MINIMUM_MOVE_VALUE;
+    }
+
+    public boolean isMoveCountHigher(int maxNum) {
+        return moveCount >= maxNum;
+    }
+
+    public int compareScore(int maxScore) {
+        if (moveCount > maxScore) {
+            return moveCount;
+        }
+        return maxScore;
+    }
+
+    public String currentScore() {
+        StringBuilder content = new StringBuilder(name).append(" : ");
+
+        for (int i = 0; i < moveCount; i++) {
+            content.append("-");
+        }
+
+        return content.toString();
+    }
+
+    public String getWinner() {
+        return name;
+    }
+}
