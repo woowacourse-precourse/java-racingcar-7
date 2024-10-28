80cb7e7c15b301f5635a11a8a9ff0a4ca77903c3
jae woo Jung <awfjol2008@gmail.com>
2024-10-28 21:03:00 +0900

    feat: 자동차 이름의 알파벳 외의 다른 문자가 있는지 검사하는 테스트 구현

--- src/test/java/racingcar/domain/NameTest.java
+++ src/test/java/racingcar/domain/NameTest.java
@@ -6,7 +6,7 @@ import static racingcar.exception.ErrorMessage.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("NameTest")
+@DisplayName("Name 클래스 테스트")
 class NameTest {
 	@Test
 	@DisplayName("빈 자동차 이름을 입력할 경우, 예외를 반환한다.")
@@ -30,5 +30,17 @@ class NameTest {
 		assertThatThrownBy(() -> new Name(input))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessageContaining(CAR_NAME_LENGTH_EXCEEDED.getMessage());
+	}
+
+	@Test
+	@DisplayName("자동차 이름에 알파벳이 아닌 다른 문자가 있는 경우, 예외를 반환한다.")
+	void Throw_Exception_When_Car_Name_Not_Alphabet() {
+		// given
+		String input = "비둘기";
+
+		// when, then
+		assertThatThrownBy(() -> new Name(input))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(CAR_NAME_NOT_ALPHABET.getMessage());
 	}
 }
