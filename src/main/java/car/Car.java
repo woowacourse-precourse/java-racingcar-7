48f7c11a035b305a568ece645fe51a6141602dc1
belight2 <chlwnsgur090@gmail.com>
2024-10-28 17:12:12 +0900

    feat: 자동차 객체 구현 완료
    toString() : 자동차의 이름과 전진 횟수가 출력이 된다.
    
    move() : 자동차가 한 칸 전진한다.

--- src/main/java/car/Car.java
+++ src/main/java/car/Car.java
@@ -1,11 +1,20 @@
 package car;
 
 public class Car {
-    private String name;
+    private final String name;
     private Integer distance;
 
     public Car(String name) {
         this.name = name;
         this.distance = 0;
+    }
+
+    public void move(){
+        distance++;
+    }
+
+    @Override
+    public String toString(){
+        return name + " : " + "-".repeat(distance);
     }
 }
