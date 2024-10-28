5be70772fb2869abb27a4c8a49bd835fc4cf144a
greensnapback0229 <smdmim@gmail.com>
2024-10-28 22:06:42 +0900

    feat(main): integrate all components

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import racingcar.controller.CarRacingController;
+import racingcar.service.AwardService;
+import racingcar.service.GameService;
+import racingcar.util.DependencyFactory;
+import racingcar.util.Validation;
+import racingcar.view.Input;
+import racingcar.view.Output;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        DependencyFactory.initialize();
+
+        final Validation validation = new Validation();
+        final Input input = new Input(validation);
+        final Output output = new Output();
+        final GameService gameService = new GameService(output);
+        final AwardService awardService = new AwardService(output);
+
+        final CarRacingController carRacingController = new CarRacingController(input, output, gameService, awardService);
+
+        carRacingController.startCarRacing();
+
     }
 }
