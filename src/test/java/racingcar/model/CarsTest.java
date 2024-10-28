3520130a736c0e8f47fadcacaec64340f1c2907c
Goohong Chung <9rrong@uoslife.team>
2024-10-28 23:19:55 +0900

    test(Cars): `@ParameterizedTest` 문법 적용

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.model.valuegenerator.ValueGenerator;
 
 class CarsTest {
@@ -29,23 +31,25 @@ class CarsTest {
         assertThat(cars.getTotalResult()).isEqualTo(List.of("pobi", "woni", "jun"));
     }
 
-    @Test
-    void 값이_4이상이면_전진한다() {
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void 값이_4이상이면_전진한다(int value) {
         List<String> input = List.of("pobi");
         Cars cars = Cars.valueOf(input);
 
-        ValueGenerator moveGenerator = () -> 4;
+        ValueGenerator moveGenerator = () -> value;
         cars.moveEachRandomly(moveGenerator);
 
         assertThat(cars.getExecutionResult()).isEqualTo(List.of("pobi : -"));
     }
 
-    @Test
-    void 값이_4미만이면_이동하지_않는다() {
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void 값이_4미만이면_이동하지_않는다(int value) {
         List<String> input = List.of("pobi");
         Cars cars = Cars.valueOf(input);
 
-        ValueGenerator stayGenerator = () -> 3;
+        ValueGenerator stayGenerator = () -> value;
         cars.moveEachRandomly(stayGenerator);
 
         assertThat(cars.getExecutionResult()).isEqualTo(List.of("pobi : "));
