4fe701a46591bfc7f6856cba0746efc17882b3cc
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-10-28 22:45:01 +0900

    test: 기능 TEST 생성

--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -5,8 +5,22 @@ import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class RacingTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    @Test
+    void 기능_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,eqq,qwe", "10");
+                    assertThat(output()).contains("pobi : -", "woni : ", "eqq : ", "qwe : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
 
     @Test
     void 자동차_개수_1개_예외처리() {
