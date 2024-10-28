92f86461abf546e1427da4d18f60328ff7dc8800
GeonHui Lee <spongging@gmail.com>
2024-10-28 12:40:45 +0900

    [fix]예외 처리 기능 수정

--- src/main/java/racingcar/console/InputView.java
+++ src/main/java/racingcar/console/InputView.java
@@ -12,9 +12,6 @@ public class InputView {
 
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             String input = Console.readLine();
-            if (input.length()>5){
-                throw new IllegalArgumentException("차 이름은 5글자 이하이여야 합니다.");
-            }
             return parseCarNames(input);
         }catch (IllegalStateException e){
             throw new IllegalArgumentException("차 이름은 5글자 이하이여야 합니다.");
@@ -24,8 +21,14 @@ public class InputView {
     private static List<String> parseCarNames(String input) {
         return Arrays.stream(input.split(","))
                 .map(String::trim)
+                .peek(name -> {
+                    if (name.length() > 5) {
+                        throw new IllegalArgumentException("차 이름은 5글자 이하이어야 합니다: " + name);
+                    }
+                })
                 .collect(Collectors.toList());
     }
+
 
     public static int readAttemptCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
