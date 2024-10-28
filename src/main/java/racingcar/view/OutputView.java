ad67f3088ccd4b6559d5ea2fe780c0ae75d3a364
Jihyun3478 <leejihyun3478@gmail.com>
2024-10-28 21:07:00 +0900

    refactor : 출력 메시지 상수화
    출력 메시지 상수화
    - 자동차 이름 입력 안내 메시지
    - 시도 횟수 입력 안내 메시지
    - 실행 결과 안내 메시지
    - 세미콜론, 하이픈
    - 최종 우승자 안내 메시지

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,27 +3,36 @@ package racingcar.view;
 import java.util.List;
 
 import racingcar.model.domain.Car;
+import racingcar.model.domain.Delimiter;
 
 public class OutputView {
+
+	private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String INPUT_ATTEMPT_NUMBER = "시도할 횟수는 몇 회인가요?";
+	private static final String RACE_RESULT = "\n실행 결과";
+	private static final String SEMI_COLON = " : ";
+	private static final String HYPHEN = "-";
+	private static final String FINAL_WINNER = "최종 우승자 : ";
+
 	public static void carName() {
-		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		System.out.println(INPUT_CAR_NAME);
 	}
 
 	public static void attemptNumber() {
-		System.out.println("시도할 횟수는 몇 회인가요?");
+		System.out.println(INPUT_ATTEMPT_NUMBER);
 	}
 
 	public static void raceResultGuide() {
-		System.out.println("\n실행 결과");
+		System.out.println(RACE_RESULT);
 	}
 
 	public static void raceResult(List<Car> cars) {
 		cars
-			.forEach(car -> System.out.println(car.getName() + " : " + "-".repeat(car.getPosition())));
+			.forEach(car -> System.out.println(car.getName() + SEMI_COLON + HYPHEN.repeat(car.getPosition())));
 		System.out.println();
 	}
 
 	public static void winners(List<String> winners) {
-		System.out.println("최종 우승자 : " + String.join(", ", winners));
+		System.out.println(FINAL_WINNER + String.join(Delimiter.COMMA + " ", winners));
 	}
 }
