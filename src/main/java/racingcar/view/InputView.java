08a379d10dacf22d52a88119f1f82d1abc06fe2d
Gahyeon <best11gh@naver.com>
2024-10-28 21:17:29 +0900

    feat(InputView): ApplicationTest null 들어올 경우

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,17 +1,23 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class InputView {
-    public static String getCarNames(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+    private static String getInput(String prompt) {
+        System.out.println(prompt);
+        try {
+            return Console.readLine().trim();
+        } catch (NoSuchElementException e) {
+            System.out.println("입력 과정에서 오류가 발생했습니다. 빈 문자열을 처리합니다.");
+            return "";
+        }
+    }
+    public static String getCarNames() {
+        return getInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public static String getTotalRounds(){
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        return Console.readLine();
+    public static String getTotalRounds() {
+        return getInput("시도할 횟수는 몇 회인가요?");
     }
-
-
 }
