8d861d04aa58a40e5d7adf76e5941060affa352a
Park - Jeonghwan <990706leo@gmail.com>
2024-10-28 19:26:39 +0900

    test(Car): 테스트 작성

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -2,11 +2,16 @@ package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
+import racingcar.domain.Movement;
 
 public class CarTest {
+
+    private final Movement movableMovement = () -> true;
+    private final Movement notMovableMovement = () -> false;
 
     @Test
     void 자동차는_이름을_가질_수_있다() {
@@ -27,11 +32,20 @@ public class CarTest {
 
     @Test
     void 자동차는_전진할_수_있다() {
-        boolean canMove = true;
         Car car = new Car("hwan");
 
-        car.move(canMove);
+        car.move(movableMovement.canMove());
 
         assertEquals(1, car.getPosition());
+    }
+
+    @Test
+    void 자동차_위치를_비교할_수_있다() {
+        Car one = new Car("hwan");
+        Car another = new Car("leo");
+
+        one.move(movableMovement.canMove());
+
+        assertTrue(one.isFartherThan(another));
     }
 }
