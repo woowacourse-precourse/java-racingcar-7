57848007ac41df8e869b4fe70900914323341ecf
jin66 <dbf6609@naver.com>
2024-10-28 20:54:04 +0900

    chore : 테스트 통과시키도록 main 문 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,22 +7,17 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
 
-        try {
-            System.out.println("경주할 자동차 이름을 입력하세요. 이름은 쉼표(,) 기준으로 구분됩니다");
-            String input = Console.readLine();
-            List<Car> cars = new ArrayList<>();
+        System.out.println("경주할 자동차 이름을 입력하세요. 이름은 쉼표(,) 기준으로 구분됩니다");
+        String input = Console.readLine();
+        List<Car> cars = new ArrayList<>();
 
-            for (String name : input.split(",")) {
-                Car newCar = new Car(name.trim()); // 문자열 시작과 끝의 공백을 제거
-                cars.add(newCar);
-            }
-
-            Game game = new Game(cars);
-            game.StartGame(); // 게임 시작
-
-        } catch (IllegalArgumentException e) {
-            System.out.println("입력 오류: " + e.getMessage());
+        for (String name : input.split(",")) {
+            Car newCar = new Car(name.trim()); // 문자열 시작과 끝의 공백을 제거
+            cars.add(newCar);
         }
+
+        Game game = new Game(cars);
+        game.StartGame(); // 게임 시작
 
     }
 }
