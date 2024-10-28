5a493f14943b0bd6086527be665ba3f0f80d8116
임채연 <dlacodus0407@gmail.com>
2024-10-28 14:37:16 +0900

    refactor(ValidateRoundTest): refactor test code
    - BeforeEach를 통해 중복되는 코드를 setUp에 넣어서 가독성을 높입니다.

--- src/test/java/racingcar/inputvalidator/ValidateRoundTest.java
+++ src/test/java/racingcar/inputvalidator/ValidateRoundTest.java
@@ -2,16 +2,19 @@ package racingcar.inputvalidator;
 
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.InputValidator;
 
 class ValidateRoundTest {
+    private InputValidator inputValidator;
+    @BeforeEach
+    void setUp() {
+        inputValidator = new InputValidator();
+    }
 
     @Test
     void 시도횟수가_숫자이면_올바른_입력입니다() {
-        // given
-        InputValidator inputValidator = new InputValidator();
-
         // when
         Integer result = inputValidator.isValidRound("58");
 
@@ -21,9 +24,6 @@ class ValidateRoundTest {
 
     @Test
     void 시도횟수로_문자가_올_수_없습니다() {
-        // given
-        InputValidator inputValidator = new InputValidator();
-
         // then
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             inputValidator.isValidRound("57d");
