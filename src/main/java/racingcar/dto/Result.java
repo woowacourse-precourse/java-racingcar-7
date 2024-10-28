9a035d6856acef462890166d84eb3c2a8856b04b
chahyunsoo <chahs1012@naver.com>
2024-10-28 09:16:58 +0900

    feat(Result) : 중간/최종 결과 출력 기능, Result 클래스 추가
    * Result 클래스는 자동차 경주의 중간 및 최종 결과를 관리합니다.
    
    * of 메서드는 Car 객체 리스트를 받아 자동차 이름과 현재 위치를 매핑하여 Result 객체를 생성합니다.
    
    * middleResult 메서드는 각 자동차의 이름과 위치를 '-'로 표시하여 중간 결과 문자열을 생성합니다.
    
    * finalWinner 메서드는 최종 우승자를 포맷에 맞게 문자열로 변환하며, 여러 명일 경우 쉼표로 구분합니다.
    
    * System.lineSeparator()를 LINE_SEPARATOR 상수로 분리하여 일관된 출력 형식을 유지합니다.

---
+++ src/main/java/racingcar/dto/Result.java
@@ -0,0 +1,34 @@
+package racingcar.dto;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import racingcar.constant.Message;
+import racingcar.model.Car;
+
+public class Result {
+    private final Map<String, Integer> result;
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
+    private Result(Map<String, Integer> result) {
+        this.result = result;
+    }
+
+    public static Result of(List<Car> cars) {
+        Map<String, Integer> result = cars.stream()
+                .collect(Collectors.toMap(Car::getName, Car::getCurrentPosition,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+        return new Result(result);
+    }
+
+    public String middleResult() {
+        return result.entrySet().stream()
+                .map(entry -> String.format(Message.RESULT_DISPLAY_FORMAT, entry.getKey(), "-".repeat(entry.getValue())))
+                .collect(Collectors.joining(LINE_SEPARATOR, "", LINE_SEPARATOR));
+    }
+
+    public String finalWinner() {
+        return String.format(Message.FINAL_WINNER_MESSAGE, String.join(", ", result.keySet()));
+    }
+}
