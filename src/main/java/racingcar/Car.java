f7fac5bad8aed73d145b848e3efbd8374e6b92e6
kimsongie12 <song3078@naver.com>
2024-10-28 16:21:13 +0900

    feat: Car객체 수정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,30 @@
 package racingcar;
 
 public class Car {
+    private String name;
+    private int position =0;
+
+    public Car(String name){
+        validateCarName(name);
+        this.name = name;
+    }
+
+    private void validateCarName(String name){
+        if(name == null || name.isEmpty() || name.length() > 5 || !name.matches("[a-zA-Z]+")){
+            throw new
+                    IllegalArgumentException("자동차 이름은 1자 이상 5자이하");
+        }
+    }
+    public String getName(){
+        return name;
+    }
+    public int getPosition(){
+        return position;
+    }
+    public void moveForward(){
+        position++;
+    }
+    public boolean isAtPosition(int position){
+        return this.position == position;
+    }
 }
