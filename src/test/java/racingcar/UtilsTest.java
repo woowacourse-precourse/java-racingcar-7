68386e9c04ef60ff843c0b657e9f2821d03bb813
전석균 <jsg998877@gmail.com>
2024-10-28 22:15:30 +0900

    test($UtilsTest.java): test Utils Methods
    - Utils의 메소드를 테스트함.
    - splitNames를 테스트해보니 메소드의 로직 상 오류를 발견함!

---
+++ src/test/java/racingcar/UtilsTest.java
@@ -0,0 +1,72 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import racingcar.exception.ErrorCode;
+import racingcar.utils.Utils;
+
+public class UtilsTest {
+
+    @Test
+    void 공백_제거_테스트() {
+        String inputWithWhitespace = " 1 2 3    4";
+        String expected = "1234";
+
+        String result = Utils.removeWhitespace(inputWithWhitespace);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void 이름_공백_제거_테스트() {
+        String inputWithWhitespace = "po bi";
+        String expected = "pobi";
+
+        String result = Utils.removeNameWhitespace(inputWithWhitespace);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void 이름_길이_유효성_테스트() {
+        String invalidName = "po bi ";
+
+        assertThatThrownBy(() -> Utils.validateName(invalidName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.INVALID_NAME_LENGTH.getMessage());
+    }
+
+    @Test
+    void splitNames_테스트() {
+        String input = "pobi, java,  woni"; // 공백 포함
+        List<String> expectedNames = List.of("pobi", "java", "woni");
+
+        List<String> result = Utils.splitNames(expectedNames);
+
+        assertThat(result).isEqualTo(expectedNames);
+    }
+
+    @Test
+    void 시도_횟수_입력_테스트() {
+        String input = " 12 "; // 공백 포함
+        int expected = 12;
+        
+        int result = Utils.inputToInt(input);
+        
+        assertThat(result).isEqualTo(expected);
+    }
+    
+    @Test
+    void 시도_횟수_입력_예외_테스트() {
+        String input = "1a3";
+
+        assertThatThrownBy(() -> Utils.inputToInt(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.INVALID_NUMBER.getMessage());
+    }
+}
