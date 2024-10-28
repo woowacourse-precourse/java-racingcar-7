ff6a707cad68f5f83767b70ae583ce01b8575bd2
김도현 <hot99009@gmail.com>
2024-10-28 22:25:44 +0900

    fix: 자동차 이름 공백 처리 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,14 +5,18 @@ import racingcar.validator.InputValidator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
     public List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         String[] carNames = input.split(",");
-        InputValidator.validateCarNames(carNames);
-        return Arrays.asList(carNames);
+        List<String> trimmedCarNames = Arrays.stream(carNames)
+                .map(String::trim)
+                .collect(Collectors.toList());
+        InputValidator.validateCarNames(trimmedCarNames.toArray(new String[0]));
+        return trimmedCarNames;
     }
 
     public int readAttempts() {
