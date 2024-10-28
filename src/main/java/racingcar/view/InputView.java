dab11bcdc97de7d469f0715f7e9dfc6e1217edca
codrin2 <hssang127@gmail.com>
2024-10-28 19:18:14 +0900

    feat(InputView): 예외 처리 추가에 따른 검증 로직 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,33 +2,54 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class InputView {
-
     private static final String DELIMITER = ",";
+    private static final int MAX_ATTEMPTS = 10;
+    private static final int MAX_CAR_COUNT = 5;
 
     public List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         validateDelimiter(input);
-        return Arrays.asList(input.split(DELIMITER));
+
+        List<String> carNames = Arrays.stream(input.split(DELIMITER))
+                .map(String::trim)
+                .collect(Collectors.toList());
+
+        validateCarNames(carNames);
+        return carNames;
     }
 
     public int getNumberOfAttempts() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String input = Console.readLine();
         validateIsNumeric(input);
-        return convertToInt(input);
+
+        int attempts = convertToInt(input);
+        validateAttemptCount(attempts);
+        return attempts;
     }
 
     public void close() {
         Console.close();
     }
 
+    private int convertToInt(String input) {
+        return Integer.parseInt(input);
+    }
+
     private void validateDelimiter(String input) {
-        if (!input.contains(DELIMITER)) {
-            throw new IllegalArgumentException("구분자는 쉼표로 작성해야 합니다.");
+        if (input.contains(",,") ) {
+            throw new IllegalArgumentException("구분자는 연속으로 사용될 수 없습니다.");
+        }
+
+        if (input.startsWith(DELIMITER) || input.endsWith(DELIMITER)) {
+            throw new IllegalArgumentException("구분자는 입력의 시작이나 끝에 위치할 수 없습니다.");
         }
     }
 
@@ -38,7 +59,24 @@ public class InputView {
         }
     }
 
-    private int convertToInt(String input) {
-        return Integer.parseInt(input);
+    private void validateAttemptCount(int attempts) {
+        if (attempts > MAX_ATTEMPTS) {
+            throw new IllegalArgumentException("이동 횟수는 최대 10회까지 입력 가능합니다.");
+        }
+    }
+
+    private void validateCarNames(List<String> carNames) {
+        if (carNames.size() > MAX_CAR_COUNT) {
+            throw new IllegalArgumentException("자동차 이름은 최대 5개까지 입력 가능합니다.");
+        }
+
+        if (carNames.contains("")) {
+            throw new IllegalArgumentException("이름값이 비어있습니다.");
+        }
+
+        Set<String> uniqueNames = new HashSet<>(carNames);
+        if (uniqueNames.size() < carNames.size()) {
+            throw new IllegalArgumentException("중복된 이름이 존재합니다.");
+        }
     }
 }
