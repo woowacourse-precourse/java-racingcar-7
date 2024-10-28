c5227c7bfcdfe2d362f40dbc858107788529ca94
sytTest <syt1128@naver.com>
2024-10-28 15:47:44 +0900

    :construction: (InputService) fix and add
    서비스의 역할에 충실할 수 있도록 구조 변경

--- src/main/java/racingcar/service/input/InputService.java
+++ src/main/java/racingcar/service/input/InputService.java
@@ -1,9 +1,28 @@
 package racingcar.service.input;
 
+import java.util.function.Function;
 import racingcar.domain.Input;
+import racingcar.repository.InputRepository;
+import racingcar.view.constant.Request;
 
 public class InputService {
-    public Input receive(String input) {
-        return new Input(input);
+
+    private final InputRepository inputRepository;
+
+    public InputService() {
+        this.inputRepository = InputRepository.connect();
+    }
+
+    public void saveInput(Input userInput, Function<String, String> validation) {
+        validatedInput(userInput, validation);
+        inputRepository.save(userInput.getRequest(), userInput);
+    }
+
+    public Input receive(String request, String input) {
+        return new Input(request, input);
+    }
+
+    private void validatedInput(Input userInput, Function<String, String> validation) {
+        validation.apply(userInput.getInput());
     }
 }
