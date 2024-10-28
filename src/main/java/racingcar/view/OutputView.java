0eb7690ea8ea77ec7301a95ad14bbaecb405fa52
YJ9985 <lucy1486@naver.com>
2024-10-28 22:14:07 +0900

    fix: OutputView 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,6 @@
 package racingcar.view;
 
 import racingcar.domain.Car;
-
 import java.util.List;
 
 public class OutputView {
@@ -20,5 +19,9 @@ public class OutputView {
 
     public void printWinners(List<String> winners) {
         System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+
+    public void printError(String message) {
+        System.out.println("[ERROR] " + message);
     }
 }
