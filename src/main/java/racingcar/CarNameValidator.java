bbd5b8e61bfdff9f8c74fa9288b943ea9307338a
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-10-28 19:56:22 +0900

    refactor: optimize CarNameValidator to separate validation logic and meet single responsibility

--- src/main/java/racingcar/CarNameValidator.java
+++ src/main/java/racingcar/CarNameValidator.java
@@ -8,12 +8,13 @@ public class CarNameValidator {
     // 첫 번째 기능: 자동차 이름 입력 및 검증
     public static List<String> getCarNames(String input) {
         List<String> carNames = Arrays.asList(input.split(","));
-
-        for (String name : carNames) {
-            if (name.length() > 5) {
-                throw new IllegalArgumentException();
-            }
-        }
+        carNames.forEach(CarNameValidator::validateCarName);
         return carNames;
+    }
+
+    private static void validateCarName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
     }
 }
