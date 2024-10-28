caa26acb1c8776bc4348cc367d7fa266c48104e5
leehenson <hslee7231@gmail.com>
2024-10-28 12:59:58 +0900

    feat: 자동차 이름 분리하기

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,11 +1,16 @@
 package racingcar;
 
 public class Race {
-    private String carNames;
+    private String[] carNames;
     private final String tryCount;
 
     public Race(String carNames, String tryCount) {
-        this.carNames = carNames;
+        this.carNames = splitString(carNames);
         this.tryCount = tryCount;
+    }
+
+    private String[] splitString(String string) {
+        String[] carNames = string.split(",");
+        return carNames;
     }
 }
