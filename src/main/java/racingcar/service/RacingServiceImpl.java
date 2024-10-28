fa03cf214b16bfc553a866962c1949e6f7788adc
haebyun <bhk4392@gmail.com>
2024-10-28 13:22:04 +0900

    refactor(RacingServiceImpl): 코드를 간결하게 표현하기 위해 수정
    - for 루프 대신 스트림을 통해 로직을 구현하도록 수정
    - 복잡성을 올리는 메서드 삭제

--- src/main/java/racingcar/service/RacingServiceImpl.java
+++ src/main/java/racingcar/service/RacingServiceImpl.java
@@ -1,8 +1,7 @@
 package racingcar.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import racingcar.domain.model.Car;
 import racingcar.domain.model.RacingManager;
 import racingcar.dto.request.RacingRequestDTO;
@@ -14,42 +13,23 @@ public class RacingServiceImpl implements RacingService {
 
     @Override
     public RacingResponseDTO race(RacingRequestDTO racingRequestDTO) {
-        RacingManager racingManager = initializeParticipants(racingRequestDTO.carNames());
+        RacingManager racingManager = RacingManager.createWithCars(racingRequestDTO.carNames());
         int moveCount = racingRequestDTO.moveCount();
 
-        List<RoundResultDTO> roundResultDTOs = performRaceMoves(racingManager, moveCount);
+        List<RoundResultDTO> roundResultDTOs = IntStream.range(0, moveCount)
+                .mapToObj(i -> {
+                    racingManager.moveCars();
+                    return createRoundResult(racingManager.getCars());
+                }).toList();
 
-        List<String> winnerNames = determineWinners(racingManager);
-
+        List<String> winnerNames = racingManager.findWinnerNames();
         return RacingResponseDTO.of(roundResultDTOs, winnerNames);
-    }
-
-    private RacingManager initializeParticipants(List<String> carNames) {
-        return RacingManager.createWithCars(carNames);
-    }
-
-    private List<RoundResultDTO> performRaceMoves(RacingManager racingManager, int moveCount) {
-        List<RoundResultDTO> roundResults = new ArrayList<>();
-        for (int i = 0; i < moveCount; i++) {
-            roundResults.add(executeMove(racingManager));
-        }
-        return roundResults;
-    }
-
-    private RoundResultDTO executeMove(RacingManager racingManager) {
-        racingManager.moveCars();
-        List<Car> movedCars = racingManager.getCars();
-        return createRoundResult(movedCars);
     }
 
     private RoundResultDTO createRoundResult(List<Car> cars) {
         List<CarRoundStateDTO> carStates = cars.stream()
                 .map(CarRoundStateDTO::from)
-                .collect(Collectors.toList());
+                .toList();
         return RoundResultDTO.of(carStates);
-    }
-
-    private List<String> determineWinners(RacingManager racingManager) {
-        return racingManager.findWinnerNames();
     }
 }
