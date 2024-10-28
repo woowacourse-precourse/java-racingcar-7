2f9e51a89c4d08e83ddcb09fd15397ce15170086
AbsoluteValues <heejun1708@gmail.com>
2024-10-28 20:55:53 +0900

    refactor: convert field to final

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -5,8 +5,8 @@ import racingcar.view.RacingcarView;
 
 public class RacingcarController {
 
-    private RacingcarView racingcarView;
-    private RacingcarService racingcarService;
+    private final RacingcarView racingcarView;
+    private final RacingcarService racingcarService;
 
     public RacingcarController(RacingcarView racingcarView, RacingcarService racingcarService) {
         this.racingcarView = racingcarView;
