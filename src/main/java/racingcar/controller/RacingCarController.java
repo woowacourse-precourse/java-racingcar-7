fcf759a40555acf4f523abb81f096f3cf1745a55
양효빈 <hyobin3026@naver.com>
2024-10-28 13:29:37 +0900

    refactor(RacingCarController): Update codes to accommodate ResultGeneratorService creation

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -3,17 +3,20 @@ package racingcar.controller;
 import racingcar.service.RacingCarService;
 
 public class RacingCarController {
-
     private final RacingCarService racingCarService;
+
 
     public RacingCarController(RacingCarService racingCarService){
         this.racingCarService = racingCarService;
     }
 
-    public void startRacing(){
-        racingCarService.inputRacingCarName();
+
+    public void startCarRacingContest(){
+        String carNameString = racingCarService.inputRacingCarName();
+        racingCarService.updateCarNameListInModel(carNameString);
         racingCarService.inputRacingAttemptsCount();
-        racingCarService.passCarNameStringToModel();
+        racingCarService.initializeCarHashMap();
+
         int racingAttemptCount = racingCarService.getRacingAttemptsCount();
         for (int i=0; i < racingAttemptCount; i++){
             racingCarService.startRacing();
