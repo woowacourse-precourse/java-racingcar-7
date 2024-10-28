a3631d76b19f60db4472dd4d6c20d5388ffc41ca
caboodles <sanghyup2000@gmail.com>
2024-10-28 18:55:22 +0900

    feat: implement Car Class
    Car 클래스가 이름을 받아서 이름 조건을 검토하고 저장
    자동차의 이동 진행 현황을 저장하고 필요시 이동

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,26 @@
+package racingcar.model;
+
+public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private final String name;
+    private int progress = 0;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름이 비어 있으면 안됩니다.");
+        }
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+        }
+    }
+    
+    public void move() {
+        progress++;
+    }
+}
