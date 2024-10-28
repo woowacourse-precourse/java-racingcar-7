67e5aea51cf69b4367744c9522f29792c93770e6
shortboy7 <thdwldnd7@naver.com>
2024-10-28 21:24:54 +0900

    feat: 이름 validation 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,9 @@
 package racingcar;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 public class Application {
     public static void showInputCarNameDialogue() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -14,12 +12,14 @@ public class Application {
         String names = Console.readLine();
         String [] splited = names.split(",");
         List<String> carNames = new ArrayList<>();
-        for (int i = 0 ; i < splited.length ; i++) {
-            String name = splited[i];
+        for (String name : splited) {
             if (name.isBlank()) {
-                throw new IllegalArgumentException("사람 이름으로 공백은 허용되지 않습니다.");
+                throw new IllegalArgumentException("자동차 이름으로 공백은 허용되지 않습니다.");
             }
             carNames.add(name);
+        }
+        if (carNames.isEmpty()) {
+            throw new IllegalArgumentException("반드시 하나 이상의 자동차 이름이 필요합니다.");
         }
         return carNames;
     }
