407f1644d8568193aef0ab588849337e44bc3743
JiEung2 <181757@jnu.ac.kr>
2024-10-28 23:11:55 +0900

    feat(inputValidator): 입력된 roundCount를 검증하고 숫자로 변환된 값을 반환하는 기능 추가
    - 숫자로 변환이 가능한지 확인하기 위해 변환해보고 변환이 된다면 그 변환 값을 반환
    - 반환을 두 번 하는 것보다는 한번에 해서 검증하고 보내는 식으로 수정

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -8,7 +8,9 @@ public class InputValidator {
     private final int CAR_NAME_LENGTH_LIMIT = 5;
     private final String CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 5자 이하만 가능합니다.";
     private final String CAR_NAME_EMPTY_ERROR_MESSAGE = "자동차 이름은 공백으로 구성될 수 없습니다.";
-    private static final String CAR_NAME_DUPLICATE_ERROR_MESSAGE = "자동차 이름은 중복될 수 없습니다.";
+    private final String CAR_NAME_DUPLICATE_ERROR_MESSAGE = "자동차 이름은 중복될 수 없습니다.";
+    private final String ROUND_COUNT_ERROR_MESSAGE = "시도할 횟수는 1 이상 숫자여야 합니다.";
+    private final String ROUND_COUNT_NOT_DIGIT = "시도할 횟수는 숫자여야 합니다.";
 
     public void validateCarNames(List<String> carNames) {
         carNames.forEach(carName -> {
@@ -17,6 +19,12 @@ public class InputValidator {
         });
 
         validateDuplicateNames(carNames);
+    }
+
+    public int validateRoundCount(String input) {
+        int roundCount = parseNumber(input);
+        validatePositiveRoundCount(roundCount);
+        return roundCount;
     }
 
     private void validateCarNameLength(String carName) {
@@ -35,6 +43,20 @@ public class InputValidator {
         Set<String> uniqueNames = new HashSet<>(carNames);
         if (uniqueNames.size() != carNames.size()) {
             throw new IllegalArgumentException(CAR_NAME_DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+
+    private int parseNumber(String roundCount) {
+        try {
+            return Integer.parseInt(roundCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ROUND_COUNT_NOT_DIGIT);
+        }
+    }
+
+    private void validatePositiveRoundCount(int roundCount) {
+        if (roundCount <= 0) {
+            throw new IllegalArgumentException(ROUND_COUNT_ERROR_MESSAGE);
         }
     }
 }
