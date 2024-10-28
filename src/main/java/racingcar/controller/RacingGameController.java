1129ecaaa10a4e92b29e62333a3f58940ee013c2
jaehwannnnnn <seanjung0915@gmail.com>
2024-10-28 14:38:39 +0900

    fix: controller 클래스에서 indent depth를 맞추기 위해 메서드 분리

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -16,23 +16,30 @@ public class RacingGameController {
         CarValidator.validateAttempts(attempts);
         int attemptCount = Integer.parseInt(attempts);
 
+        List<Car> cars = createCars(carNames);
+        RacingGame game = new RacingGame(cars);
+        runRounds(game, attemptCount);
+
+        List<String> winners = determineWinners(game.getCars());
+        OutputView.printFinalWinners(winners);
+    }
+
+    private List<Car> createCars(String[] carNames) {
         List<Car> cars = new ArrayList<>();
         for (String name : carNames) {
             String trimmedName = name.trim();
             CarValidator.validateCarName(trimmedName);
             cars.add(new Car(trimmedName));
         }
+        return cars;
+    }
 
-        RacingGame game = new RacingGame(cars);
-
+    private void runRounds(RacingGame game, int attemptCount) {
         for (int i = 0; i < attemptCount; i++) {
             game.playRound();
             OutputView.printRoundResult(game);
         }
-        List<String> winners = determineWinners(game.getCars());
-        OutputView.printFinalWinners(winners);
     }
-
     private List<String> determineWinners(List<Car> cars) {
         int maxPosition = cars.stream().mapToInt(Car::getPosition).max().orElse(0);
         List<String> winners = new ArrayList<>();
