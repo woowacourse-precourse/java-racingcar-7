4ddd78e8565cc5511dcfe1b0f8c6ad013ef1a005
염규영 <yeomky98@naver.com>
2024-10-28 17:01:58 +0900

    refactor(validator): replace hard-coded race count limits with constants

--- src/main/java/racingcar/util/RaceValidator.java
+++ src/main/java/racingcar/util/RaceValidator.java
@@ -4,8 +4,11 @@ import racingcar.error.ErrorMessage;
 
 public class RaceValidator {
 
+    private static final int MIN_RACE_COUNT = 1;
+    private static final int MAX_RACE_COUNT = 100;
+
     public void checkRaceCount(int raceCount) {
-        if (raceCount <= 0 || 100 < raceCount) {
+        if (raceCount < MIN_RACE_COUNT || MAX_RACE_COUNT < raceCount) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_RACE_COUNT.getMessage());
         }
     }
