b11b6f976b3c42bd583419cc73c9c94520a41a94
Jewan1120 <hawaii1468@gmail.com>
2024-10-28 17:13:36 +0900

    refactor(Validator): 각 역할 별 메서드로 분리

--- src/main/java/racingcar/validator/RacingCarValidator.java
+++ src/main/java/racingcar/validator/RacingCarValidator.java
@@ -10,46 +10,77 @@ public class RacingCarValidator {
     private final Set<String> uniqueCarNames = new HashSet<>();
 
     public void validateInputString(String inputString) {
+        checkNullOrBlank(inputString);
+    }
+
+    public void validateCarNames(String[] carNames) {
+        checkZeroLength(carNames);
+        checkNumberOfCars(carNames);
+    }
+
+    public void validateCarName(String carName) {
+        checkCarNameEmpty(carName);
+        checkCarNameLength(carName);
+        checkCarNameUnique(carName);
+    }
+
+    public void validateRounds(String roundString) {
+        checkNullOrBlank(roundString);
+        checkDigit(roundString);
+        checkBound(roundString);
+    }
+
+    private static void checkNullOrBlank(String inputString) {
         if (inputString == null || inputString.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.INPUT_NOTHING.getMessage());
         }
     }
 
-    public void validateCarNames(String[] carNames) {
+    private static void checkZeroLength(String[] carNames) {
         if (carNames.length == 0) {
             throw new IllegalArgumentException(ErrorMessage.CAR_NAME_EMPTY.getMessage());
         }
+    }
+
+    private static void checkNumberOfCars(String[] carNames) {
         if (carNames.length == Constants.CAR_COUNT_MINIMUM.getValue()) {
             throw new IllegalArgumentException(
                     ErrorMessage.CAR_COUNT_MINIMUM.getMessage(Constants.CAR_COUNT_MINIMUM.getValue()));
         }
     }
 
-    public void validateCarName(String carName) {
-        if (carName.isEmpty()) {
-            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_EMPTY.getMessage());
-        }
+    private static void checkCarNameLength(String carName) {
         if (carName.length() > Constants.NAME_LENGTH_LIMIT.getValue()) {
             throw new IllegalArgumentException(ErrorMessage.CAR_NAME_LENGTH.getMessage());
         }
+    }
+
+    private void checkCarNameUnique(String carName) {
         if (uniqueCarNames.contains(carName)) {
             throw new IllegalArgumentException(ErrorMessage.CAR_NAME_UNIQUE.getMessage());
         }
         uniqueCarNames.add(carName);
     }
 
-    public void validateRounds(String roundString) {
-        if (roundString == null || roundString.isBlank()) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_NOTHING.getMessage());
+    private static void checkCarNameEmpty(String carName) {
+        if (carName.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_EMPTY.getMessage());
         }
+    }
+
+    private static void checkBound(String roundString) {
+        int rounds = Integer.parseInt(roundString);
+        if (rounds < Constants.ROUND_MINIMUM.getValue() || rounds > Constants.ROUND_MAXIMUM.getValue()) {
+            throw new IllegalArgumentException(ErrorMessage.ROUND_LIMIT.getMessage(Constants.ROUND_MINIMUM.getValue(),
+                    Constants.ROUND_MAXIMUM.getValue()));
+        }
+    }
+
+    private static void checkDigit(String roundString) {
         for (int i = 0; i < roundString.length(); i++) {
             if (!Character.isDigit(roundString.charAt(i))) {
                 throw new IllegalArgumentException(ErrorMessage.ROUND_FORMAT.getMessage());
             }
-        }
-        int rounds = Integer.parseInt(roundString);
-        if (rounds < Constants.ROUND_MINIMUM.getValue() || rounds > Constants.ROUND_MAXIMUM.getValue()) {
-            throw new IllegalArgumentException(ErrorMessage.ROUND_LIMIT.getMessage(Constants.ROUND_MINIMUM.getValue(), Constants.ROUND_MAXIMUM.getValue()));
         }
     }
 }
