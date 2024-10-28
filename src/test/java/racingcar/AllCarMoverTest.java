becfdc1501d04fda902879628575c6002e95fa11
tmdcheol <leesc08392@gmail.com>
2024-10-28 23:27:33 +0900

    test: AllCarMover 테스트에 Mock MovePolicy 적용
    - TrueMockMovePolicy, FalseMockMovePolicy Mock 클래스 추가
    - AllCarMover가 CarMover에 주입된 Mocking 정책에 따라 자동차의 이동을 정확히 수행하는지 검증

--- src/test/java/racingcar/AllCarMoverTest.java
+++ src/test/java/racingcar/AllCarMoverTest.java
@@ -1,28 +1,67 @@
 package racingcar;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class AllCarMoverTest {
 
-    AllCarMover allCarMover = new AllCarMover();
-    List<Car> cars = List.of(
-            new Car("a"),
-            new Car("b")
-    );
+    private CarMover trueCarMover;
+    private CarMover falseCarMover;
+    private List<Car> cars;
 
-    /*
-     * Todo: CarMover, RandomNumberGenerator를 Mocking 하여 테스트 코드 작성하기
-     *
-     * 현재는 Mocking 불가능 한 상태
-     * 우선 요구사항 구현에 집중하자
-     */
+    @BeforeEach
+    void setUp() {
+        cars = List.of(
+                new Car("a"),
+                new Car("b")
+        );
+
+        trueCarMover = new CarMover(new TrueMockMovePolicy());
+        falseCarMover = new CarMover(new FalseMockMovePolicy());
+    }
+
     @Test
-    @DisplayName("모든 차 한번 이동 수행 시도 성공 테스트")
-    void run_테스트() {
+    @DisplayName("모든 차가 이동하는 경우 테스트")
+    void 모든_차_이동_테스트() {
+        // given
+        AllCarMover allCarMover = new AllCarMover(trueCarMover);
+
         // when
         allCarMover.run(cars);
+
+        // then
+        assertThat(cars).allMatch(car -> car.getMoveCount() == 1);
+    }
+
+    @Test
+    @DisplayName("모든 차가 이동하지 않는 경우 테스트")
+    void 모든_차_정지_테스트() {
+        // given
+        AllCarMover allCarMover = new AllCarMover(falseCarMover);
+
+        // when
+        allCarMover.run(cars);
+
+        // then
+        assertThat(cars).allMatch(car -> car.getMoveCount() == 0);
+    }
+
+    static class TrueMockMovePolicy implements MovePolicy {
+        @Override
+        public boolean canMove() {
+            return true;
+        }
+    }
+
+    static class FalseMockMovePolicy implements MovePolicy {
+        @Override
+        public boolean canMove() {
+            return false;
+        }
     }
 
 }
