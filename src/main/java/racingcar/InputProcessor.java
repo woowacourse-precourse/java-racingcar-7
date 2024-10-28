8c67791ef57041a26f1ff8c5d898f36992f105d2
Hyejeong <thswjd0609@gmail.com>
2024-10-28 13:12:53 +0900

    feat: Car name 길이, 공백 check 메소드 작성

--- src/main/java/racingcar/InputProcessor.java
+++ src/main/java/racingcar/InputProcessor.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class InputProcessor {
@@ -10,7 +11,21 @@ public class InputProcessor {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         String[] cars = input.split(",");
-        return List.of(cars);
+        return checkCarNames(cars);
+    }
+
+    private static List<String> checkCarNames(String[] cars) {
+        List<String> carList = new ArrayList<>();
+        for(String car : cars) {
+            if (car.isEmpty()) {
+                continue;
+            }
+            if (car.length() > 5) {
+                throw new IllegalArgumentException("Invalid String length");
+            }
+            carList.add(car);
+        }
+        return carList;
     }
 
 }
