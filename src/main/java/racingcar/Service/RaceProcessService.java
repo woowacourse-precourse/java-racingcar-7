36a2b602c2e639c2c93dc233531da81318fdfafe
dbehdgns1215 <dbehdgns1215@naver.com>
2024-10-28 18:06:31 +0900

    refactor(RaceProcessService): indent depth가 2를 넘지 않도록 메서드 분리
    - 기존 startRace() 메서드에 while문(depth 1), for문(depth 2), 메서드 호출(depth 3)으로 indent depth가 3 이었기에 메서드를 분리
    - 기존 startRace() 메서드에 라운드별로 실행하던 로직들을 stratSingleRound() 메서드로 분리

--- src/main/java/racingcar/Service/RaceProcessService.java
+++ src/main/java/racingcar/Service/RaceProcessService.java
@@ -11,13 +11,11 @@ import racingcar.Factory.DomainFactory;
 
 public class RaceProcessService {
     private final DomainFactory domainFactory;
-
     private CarInfo carInfo;
-    private Integer trialCount;
     private RaceInfoDTO raceInfoDTO;
-    private RaceResultEvaluator raceResultEvaluator;
     private RaceViewController raceViewController;
     private RandomNumber randomNumber;
+    private int trialCount;
 
     public RaceProcessService(DomainFactory domainFactory) {
         this.domainFactory = domainFactory;
@@ -26,38 +24,39 @@ public class RaceProcessService {
     public void readyRace(RaceInfoDTO raceInfoDTO, RaceViewController raceViewController) {
         this.raceInfoDTO = raceInfoDTO;
         this.raceViewController = raceViewController;
-
-        carInfo = domainFactory.createCarInfo(raceInfoDTO.getCarNames());
-        randomNumber = domainFactory.createRandomNumber();
-        raceResultEvaluator = domainFactory.createRaceResultEvaluator();
-
-        trialCount = raceInfoDTO.getTrialCount();
+        this.carInfo = domainFactory.createCarInfo(raceInfoDTO.getCarNames());
+        this.randomNumber = domainFactory.createRandomNumber();
+        this.trialCount = raceInfoDTO.getTrialCount();
 
         startRace();
     }
 
-    public void startRace() {
+    private void startRace() {
         RaceResultDTO raceResultDTO = new RaceResultDTO(carInfo.exportCarList());
-
         raceViewController.printRaceResult();
 
-        while (trialCount > 0) {
-            for (String carName : raceInfoDTO.getCarNames()) {
-                Integer number = randomNumber.pickNumber();
-                carInfo.moveForward(carName, number);
-            }
-            trialCount--;
-            raceResultDTO.updateResults(carInfo.exportCarList());
-            raceViewController.gatherRaceResult(raceResultDTO);
+        while (trialCount-- > 0) {
+            stratSingleRound(raceResultDTO);
         }
 
         endRace(raceResultDTO);
     }
 
-    public void endRace(RaceResultDTO raceResultDTO) {
-        raceResultEvaluator.evaluateFinalWinners(raceResultDTO);
-        RaceFinalWinnerDTO raceFinalWinnerDTO = new RaceFinalWinnerDTO(raceResultEvaluator.getWinners());
+    private void stratSingleRound(RaceResultDTO raceResultDTO) {
+        for (String carName : raceInfoDTO.getCarNames()) {
+            int number = randomNumber.pickNumber();
+            carInfo.moveForward(carName, number);
+        }
 
-        raceViewController.printFinalWinners(raceFinalWinnerDTO);
+        raceResultDTO.updateResults(carInfo.exportCarList());
+        raceViewController.gatherRaceResult(raceResultDTO);
+    }
+
+    private void endRace(RaceResultDTO raceResultDTO) {
+        RaceResultEvaluator evaluator = domainFactory.createRaceResultEvaluator();
+        evaluator.evaluateFinalWinners(raceResultDTO);
+        RaceFinalWinnerDTO finalWinnerDTO = new RaceFinalWinnerDTO(evaluator.getWinners());
+
+        raceViewController.printFinalWinners(finalWinnerDTO);
     }
 }
