5bc6e0153f6034059e1e3ff638973ea56e4364d3
jjjjjoon <mjj7343@gmail.com>
2024-10-28 17:44:21 +0900

    feat(CarTest): Car클래스 테스트
    자동차 이름과 위치 정상적으로 입력되는지 테스트

---
+++ src/test/java/util/model/CarTest.java
@@ -0,0 +1,38 @@
+package util.model;
+
+import model.Car;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CarTest {
+
+    private Car car;
+    private String name;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car(name);
+    }
+
+    @ParameterizedTest
+    @DisplayName("이름이 정상적으로 설정되었습니다.")
+    @ValueSource(strings = {"pobi"})
+    void testCarNameLength(String carName) {
+        Car car = new Car(carName);
+        assertEquals(carName, car.getName());
+    }
+
+    @Test
+    void testMoveForward() {
+        Car car = new Car("Test");
+        int initialPosition = car.getPosition();
+        car.moveForward();
+        assertEquals(initialPosition + 1, car.getPosition());
+    }
+}
