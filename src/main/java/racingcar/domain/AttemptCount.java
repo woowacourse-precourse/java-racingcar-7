bf27b5cc554ac1ebc1dfa197c2cafd0623bb547e
jae woo Jung <awfjol2008@gmail.com>
2024-10-28 19:32:56 +0900

    refactor: AttemptCount 클래스 생성자 추가

--- src/main/java/racingcar/domain/AttemptCount.java
+++ src/main/java/racingcar/domain/AttemptCount.java
@@ -2,4 +2,8 @@ package racingcar.domain;
 
 public class AttemptCount {
 	private final Integer attemptCount;
+
+	protected AttemptCount(int attemptCount) {
+		this.attemptCount = attemptCount;
+	}
 }
