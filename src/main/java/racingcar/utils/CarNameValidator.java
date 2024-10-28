b579f9d75e54e1939bafeafdd4de1535367651a6
JeongHyeon Jo <wjdgusdlekd3@yonsei.ac.kr>
2024-10-28 13:28:57 +0900

    refactor(utils): CarNameValidator class manage responsibilities
    - 중복 관리 validator 책임에 맡게 분배
    - 싱글턴 패턴을 이용하여 구현

--- src/main/java/racingcar/utils/CarNameValidator.java
+++ src/main/java/racingcar/utils/CarNameValidator.java
@@ -1,14 +1,29 @@
 package racingcar.utils;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CarNameValidator implements Validator {
     private static final int RESTRICTED_CAR_LENGTH = 4;
-    private static final String RESTRICTED_CAR_NAME_LENGTH_ERROR = ERROR_PREFIX + "차량 이름은 "+RESTRICTED_CAR_LENGTH+"자리 까지만 허용합니다.";
-    private static final String EMPTY_CAR_NAME_ERROR = ERROR_PREFIX + "차량 이름은 빈 문자열이나 공백은 포함할 수 없습니다.";
+    private static final String RESTRICTED_CAR_NAME_LENGTH_ERROR = ERROR_PREFIX + "이름은 " + RESTRICTED_CAR_LENGTH + "자리 까지만 허용합니다.";
+    private static final String EMPTY_CAR_NAME_ERROR = ERROR_PREFIX + "이름은 빈 문자열이나 공백은 포함할 수 없습니다.";
+    private static final String INPUT_DUPLICATE_VALIDATE_MESSAGE = ERROR_PREFIX + "중복된 이름이 존재합니다.";
+    private final Set<String> carNames = new HashSet<>();
+    private static CarNameValidator instance;
+
+    public static CarNameValidator getInstance() {
+        if (instance == null) {
+            instance = new CarNameValidator();
+        }
+        return instance;
+    }
 
     @Override
     public void validation(String carName) {
         checkRestrictedLength(carName);
         checkEmptyCarName(carName);
+        checkDuplicateCarName(carName);
+        carNames.add(carName);
     }
 
     private void checkRestrictedLength(String carName) {
@@ -20,6 +35,12 @@ public class CarNameValidator implements Validator {
     private void checkEmptyCarName(String carName) {
         if (carName == null || carName.contains(" ") || carName.isEmpty()) {
             throw new IllegalArgumentException(EMPTY_CAR_NAME_ERROR);
+        }
+    }
+
+    private void checkDuplicateCarName(String carName) {
+        if (carNames.contains(carName)) {
+            throw new IllegalArgumentException(INPUT_DUPLICATE_VALIDATE_MESSAGE);
         }
     }
 
