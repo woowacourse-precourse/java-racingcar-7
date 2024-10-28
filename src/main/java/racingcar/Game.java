9e4cd4875a3129bf8e440210152457999d090346
MinseoKangQ <kms02171@naver.com>
2024-10-28 14:37:54 +0900

    feat: 안내 문구 출력

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -3,7 +3,10 @@ package racingcar;
 public class Game {
 
     public Game() {
+        OutputHandler.printInstructionForCarNames();
         String[] carNames = InputHandler.getCarNames();
+
+        OutputHandler.printInstructionForTimes();
         int times = InputHandler.getTimes();
     }
 }
