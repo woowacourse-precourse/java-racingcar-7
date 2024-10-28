30a3524c258e927a47abc840a02b1873069bea53
hgh1472 <hgh1472@naver.com>
2024-10-28 12:50:06 +0900

    refactor: 단일 역할을 하도록 메서드 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -25,48 +25,50 @@ public class InputView {
     public List<String> readCarInput() {
         System.out.println(View.CAR_NAME_INPUT.getMessage());
         String input = Console.readLine();
-        return splitNames(input);
+        String regex = getRegexFromSeparator();
+        return splitNames(input, regex);
     }
 
-    public Integer readCountInput() {
-        System.out.println(View.COUNT_INPUT.getMessage());
-        String input = Console.readLine();
-        return parseCount(input);
-    }
-
-    private Integer parseCount(String input) {
-        if (!isNumber(input)) {
-            throw new IllegalArgumentException(ErrorStatus.NOT_NUMBER.getMessage());
-        }
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            // Overflow
-            throw new IllegalArgumentException(ErrorStatus.INVALID_NUMBER_RANGE.getMessage());
-        }
-    }
-
-    private Boolean isNumber(String input) {
-        for (char c : input.toCharArray()) {
-            if (!Character.isDigit(c)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private List<String> splitNames(String input) {
-        String regex = String.join(DELIMITER,
-                Arrays.stream(Separator.values()).map(Separator::getSeparator).toList());
-
+    private List<String> splitNames(String input, String regex) {
         return Arrays.stream(input.split(regex))
                 .peek(this::isBlankCarName)
                 .toList();
     }
 
+    private String getRegexFromSeparator() {
+        return String.join(DELIMITER,
+                Arrays.stream(Separator.values())
+                        .map(Separator::getSeparator)
+                        .toList());
+    }
+
     private void isBlankCarName(String carName) {
         if (carName.isBlank()) {
             throw new IllegalArgumentException(ErrorStatus.INVALID_CAR_NAME.getMessage());
+        }
+    }
+
+    public Integer readCountInput() {
+        System.out.println(View.COUNT_INPUT.getMessage());
+        String input = Console.readLine();
+        isOnlyNumber(input);
+        return parseCount(input);
+    }
+
+    private void isOnlyNumber(String input) {
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException(ErrorStatus.NOT_NUMBER.getMessage());
+            }
+        }
+    }
+
+    private Integer parseCount(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            // Overflow
+            throw new IllegalArgumentException(ErrorStatus.INVALID_NUMBER_RANGE.getMessage());
         }
     }
 }
