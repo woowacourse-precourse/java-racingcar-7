49f2cbe9a7217a97a14e69cc4e9f712dd8367600
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-28 10:54:45 +0900

    refactor(InputCountValidator): 열거형에 맞춰 에러 메시지 코드 리팩토링
    - 열거형을 사용하기 위해 static으로 import 했습니다.
    - 메시지를 직접 입력이 아닌 상수형으로 사용했습니다.

--- src/main/java/racingcar/validator/InputCountValidator.java
+++ src/main/java/racingcar/validator/InputCountValidator.java
@@ -1,17 +1,22 @@
 package racingcar.validator;
 
+import static racingcar.exception.InputCountExceptionMessage.BLANK_COUNT;
+import static racingcar.exception.InputCountExceptionMessage.IMPOSSIBLE_CONVERT_NUMBER;
+import static racingcar.exception.InputCountExceptionMessage.INVALID_INTEGER_SCOPE;
+import static racingcar.exception.InputCountExceptionMessage.UNDER_MINIMUM_COUNT;
+
 public class InputCountValidator {
     private static final int MINMUM_TRY_COUNT = 1;
 
     public void isAboveMinimumTryCount(int tryCount) {
         if (tryCount < MINMUM_TRY_COUNT) {
-            throw new IllegalArgumentException("0 이하의 숫자입니다.");
+            throw new IllegalArgumentException(UNDER_MINIMUM_COUNT.toString());
         }
     }
 
     public void isPossibleConvertNumber(String tryCount) {
         if (!tryCount.matches("^%d")) {
-            throw new IllegalArgumentException("숫자가 아닌 값이 입력되었습니다.");
+            throw new IllegalArgumentException(IMPOSSIBLE_CONVERT_NUMBER.toString());
         }
     }
 
@@ -19,13 +24,13 @@ public class InputCountValidator {
         try {
             Integer.parseInt(tryCount);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("int 자료형의 범위를 넘어서는 입력값입니다.");
+            throw new IllegalArgumentException(INVALID_INTEGER_SCOPE.toString());
         }
     }
 
     public void isNotBlankTryCount(String tryCount) {
         if (tryCount.isEmpty()) {
-            throw new IllegalArgumentException("공백이 입력되었습니다.");
+            throw new IllegalArgumentException(BLANK_COUNT.toString());
         }
     }
 }
