fe0a7eb50c2b29b645943c19b46346ce9af8f02d
Seongho Kim <kksshh0612@gmail.com>
2024-10-28 11:30:01 +0900

    refactor: 랜덤 숫자 생성하는 클래스 추상화
    - 테스트에서 랜덤값을 지정하기 위해 NumberGenerator 인터페이스 생성
    - RandomUtil이 하나의 작업을 진행하기 때문에 RandomNumberGenerator로 변경

--- src/main/java/racingcar/service/RandomNumberGenerator.java
+++ src/main/java/racingcar/service/RandomNumberGenerator.java
@@ -2,12 +2,13 @@ package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomUtil {
+public class RandomNumberGenerator implements NumberGenerator {
 
     public static final int RANDOM_MIN = 0;
     public static final int RANDOM_MAX = 9;
 
-    public static int generateRandomNumber() {
+    @Override
+    public int generateRandomNumber() {
         return Randoms.pickNumberInRange(RANDOM_MIN, RANDOM_MAX);
     }
 }
