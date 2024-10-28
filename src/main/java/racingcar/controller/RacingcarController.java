451a460ca513e6f50b9f06d727049a92280431b2
chino <tnehd83@naver.com>
2024-10-28 14:13:34 +0900

    feat(InputView): 사용자 입력받는 기능 추가

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -1,4 +1,26 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.gameuserdata.RacingUserData;
+import racingcar.model.FindManyMovingRacingcar;
+import racingcar.model.RacingPlayingCycle;
+import racingcar.model.RacingcarDataVerification;
+import racingcar.splitter.RacingcarSplitter;
+import racingcar.view.InputView;
+import racingcar.view.OuputView;
+
 public class RacingcarController {
+    private final InputView inputView;
+
+    public RacingcarController() {
+        inputView = new InputView();
+    }
+
+    public void run() {
+        String RacingCarNames = inputView.getRacingCarInput();
+        int RacingCount = inputView.getRaingCountInput();
+
+        String[] RacingCarSplitName = RacingCarNames.split(",");
+    }
 }
