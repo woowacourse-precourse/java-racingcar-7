a296ebc1d91e2fffd98e562e7fdd4d79a84da2a4
rbgusdlza <rbgusdlza@naver.com>
2024-10-28 22:08:46 +0900

    feat: 게임 실행을 담당하는 기능 구현

--- src/main/java/racingcar/RacingController.java
+++ src/main/java/racingcar/RacingController.java
@@ -1,2 +1,22 @@
-package racingcar;public class RacingController {
+package racingcar;
+
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+
+    public void run() {
+        Cars cars = InputView.readCars();
+        int count = InputView.readCount();
+        OutputView.showRacing(cars);
+        startRacingGame(count, cars);
+        OutputView.showResult(cars.getWinners());
+    }
+
+    private void startRacingGame(int count, Cars cars) {
+        for (int i = 0; i < count; i++) {
+            cars.moveAll();
+            cars.showAll();
+        }
+    }
 }
