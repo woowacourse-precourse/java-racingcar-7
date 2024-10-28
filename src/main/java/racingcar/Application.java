9258c14e02f938a21c3e0f6c6a9bf3560c70c6fa
AndBlack99 <jwoo1687@icloud.com>
2024-10-28 17:46:47 +0900

    feat: 게임준비 단계 기능 구현- 자동차 이름을 입력받고 5자 제한을 확인하는 기능 추가, 다수의 자동차 이름 구분은 쉼표(,)로 구분.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,38 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+	
+	private static final int MAX_NAME_LENGTH = 5;
+	
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+      
+    	String[] carName = inputCarNames();
     }
+    
+    private static String[] inputCarNames() {
+    	System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ");
+    	String inputNames = Console.readLine();
+    	return validateCarNames(inputNames);
+    }
+  
+    private static String[] validateCarNames(String inputNames) {
+    	String[] carNames = inputNames.split(",");
+    	carNamesData(carNames);
+    	return carNames;
+    }
+    
+    private static void carNamesData(String[] carNames) {
+    	for (String name : carNames) {
+    		lengthCheck(name);
+    	}
+    }
+    private static void lengthCheck(String name) {
+    	if (name.length() > MAX_NAME_LENGTH) 
+    		throw new IllegalArgumentException();
+    	
+    }
+   
 }
