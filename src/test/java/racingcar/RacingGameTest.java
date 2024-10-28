174ecb9e08c5509d4c83c888e8fb2ce22f9f1b16
Hajuuu <hpt0127@naver.com>
2024-10-28 23:02:36 +0900

    test: test 추가
    이름이 null이거나 공백 문자일 경우, 빈 문자열일 경우 에러가 발생하는지 확인하는 테스트 작성

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -35,15 +35,39 @@ class RacingGameTest {
     @Test
     @DisplayName("이름의 길이가 5를 초과하는 경우 예외 발생")
     @Order(1)
-    void nameCheck() {
+    void nameMaxLength() {
         cars.add(new Car("abcdef", 0));
+        assertThatThrownBy(() -> racingGame.nameCheck(cars)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("이름이 null일 경우 예외 발생")
+    @Order(2)
+    void nameIsNull() {
+        cars.add(new Car(null, 0));
+        assertThatThrownBy(() -> racingGame.nameCheck(cars)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("이름을 공백 문자로 입력할 경우 예외 발생")
+    @Order(3)
+    void nameIsBlank() {
+        cars.add(new Car(" ", 0));
+        assertThatThrownBy(() -> racingGame.nameCheck(cars)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("이름을 빈 문자열로 입력할 경우 예외 발생")
+    @Order(4)
+    void nameIsEmpty() {
+        cars.add(new Car("", 0));
         assertThatThrownBy(() -> racingGame.nameCheck(cars)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
     @DisplayName("횟수를 0 이하의 숫자 또는 문자로 입력할 경우 예외 발생")
     @ValueSource(strings = {"0", "-1", "a"})
-    @Order(2)
+    @Order(5)
     void countCheck(String input) {
         assertThatThrownBy(() -> racingGame.countCheck(input)).isInstanceOf(IllegalArgumentException.class);
     }
@@ -51,7 +75,7 @@ class RacingGameTest {
     @ParameterizedTest
     @DisplayName("랜덤값이 4 이상일 경우 전진")
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    @Order(3)
+    @Order(6)
     void movingCar(int num) {
         Car car = cars.get(0);
         car.move(num);
@@ -61,7 +85,7 @@ class RacingGameTest {
     @ParameterizedTest
     @DisplayName("랜덤값이 4 미만일 경우 전진 X")
     @ValueSource(ints = {0, 1, 2, 3})
-    @Order(4)
+    @Order(7)
     void findMovingCar(int num) {
         Car car = cars.get(0);
         car.move(num);
@@ -70,7 +94,7 @@ class RacingGameTest {
 
     @Test
     @DisplayName("우승자가 1명인 경우")
-    @Order(5)
+    @Order(8)
     void printWinner() {
         cars.add(new Car("carA", 3));
         OutputStream output = getOutputStream();
@@ -80,7 +104,7 @@ class RacingGameTest {
 
     @Test
     @DisplayName("우승자가 2명인 경우")
-    @Order(6)
+    @Order(9)
     void printWinners() {
         cars.add(new Car("carA", 5));
         cars.add(new Car("carB", 5));
