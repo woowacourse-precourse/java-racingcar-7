db54c141d9cdfb3696fce163c140eaa89f633340
shortboy7 <thdwldnd7@naver.com>
2024-10-28 21:52:23 +0900

    fix: 실행결과 Observer가 Car가 아닌 전체 Car를 관찰하도록 변경 - CarUpdater에서 Observer에 대한 참조 제거 - CarFactory에서 실행결과 Observer 종속성 제거 - 실행결과 옵저버에서 생성자 메서드 제거

--- src/main/java/racingcar/observer/ExecutionResultOutputObserver.java
+++ src/main/java/racingcar/observer/ExecutionResultOutputObserver.java
@@ -1,35 +1,30 @@
 package racingcar.observer;
 
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
 import racingcar.car.Car;
 import racingcar.car.CarView;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-
 
 public class ExecutionResultOutputObserver implements Observer{
-    private final Car car;
+    private final List<Car> cars;
     private final Writer writer;
 
-    ExecutionResultOutputObserver(Car car, Writer writer) {
-        this.car = car;
+    public ExecutionResultOutputObserver(List<Car> cars, Writer writer) {
+        this.cars = cars;
         this.writer = writer;
     }
 
     @Override
     public void notifyUpdate() {
         try {
-            writer.write(CarView.executionResult(car));
+            for (Car car: cars) {
+                writer.write(CarView.executionResult(car));
+            }
             writer.flush();
         } catch (IOException exception) {
             System.err.println(exception.getMessage());
         }
-    }
-
-    public static ExecutionResultOutputObserver stdOutput(Car car) {
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        return new ExecutionResultOutputObserver(car, writer);
     }
 }
