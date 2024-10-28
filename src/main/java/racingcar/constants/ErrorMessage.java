b5f34ed55ccb1afef08dfff6cfef1c65705712b4
kaswhy <sxshx818@naver.com>
2024-10-28 16:27:33 +0900

    feat(ErrorMessage): 에러 메시지 enum으로 관리
    변경 전
    - 검증 메소드와 테스트 코드에서 에러메시지 하드 코딩
    
    변경 후
    - 하드코딩한 에러메시지를 enum에서 관리

---
+++ src/main/java/racingcar/constants/ErrorMessage.java
@@ -0,0 +1,18 @@
+package racingcar.constants;
+
+public enum ErrorMessage {
+    CAR_NAME_CAN_NOT_BE_EMPTY("자동차 이름은 빈 값이 될 수 없습니다."),
+    RACE_COUNT_DOES_NOT_HAVE_CHARACTER("숫자 이외의 문자가 포함되어 있습니다."),
+    RACE_COUNT_CAN_NOT_BE_EMPTY("시도 횟수는 빈 값이 될 수 없습니다."),
+    RACE_COUNT_CAN_NOT_BE_ZERO("시도 횟수는 0이 될 수 없습니다.");
+
+    private final String errorMessage;
+
+    private ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
