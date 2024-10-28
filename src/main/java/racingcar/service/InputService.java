a20d22aff058b99bdcf923785e1bbcab348d5046
o-jeong <ljj020304@gmail.com>
2024-10-28 17:37:18 +0900

    feat: 사용자 입력 처리 구현

---
+++ src/main/java/racingcar/service/InputService.java
@@ -0,0 +1,37 @@
+package racingcar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.CarValidator;
+import racingcar.domain.TrialCountValidator;
+import racingcar.view.InputView;
+
+public class InputService {
+    private final InputView inputView;
+
+    public InputService(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public List<Car> getCars(){
+        String carNamesInput = inputView.getCarNames();
+        List<Car> cars = createCars(carNamesInput);
+        CarValidator.validate(cars);
+        return cars;
+    }
+
+    public int getTrialCount(){
+        String trialCountInput = inputView.getTrialCount();
+        TrialCountValidator.validate(trialCountInput);
+        return Integer.parseInt(trialCountInput);
+    }
+
+    private List<Car> createCars(String carNamesInput){
+        List<Car> cars = new ArrayList<>();
+        for(String carName : carNamesInput.split(",")){
+            cars.add(new Car(carName.trim()));
+        }
+        return cars;
+    }
+}
