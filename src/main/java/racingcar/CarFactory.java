e52824f1aa8a47a825509a735c4aba5307dccfdb
AwesomeMinsik <beaut3579@gmail.com>
2024-10-28 15:17:05 +0900

    refactor : CarFactory 내부 메서드의 객체생성로직을 제외한 나머지 수행로직을 제거하였습니다

--- src/main/java/racingcar/CarFactory.java
+++ src/main/java/racingcar/CarFactory.java
@@ -3,14 +3,9 @@ package racingcar;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CarFactory {
-    public static List<RacingCar> createRacingCars(List<String> carNames) {
-        List<RacingCar> racingCars = new ArrayList<>();
-        for (String carName : carNames) {
-            RacingCar newRacingCar = new RacingCar(carName);
-            racingCars.add(newRacingCar);
-        }
-        return racingCars;
 public abstract class CarFactory {
+
+    public static RacingCar createRacingCars(String carName) {
+        return new RacingCar(carName);
     }
 }
