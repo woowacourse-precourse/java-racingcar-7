60c487b6799dc08865177e4cd9a14b909390a2b1
Seyoung Goo <0917sy@gmail.com>
2024-10-28 23:07:02 +0900

    refactor: OutputView 상수명 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,25 +6,25 @@ import racingcar.domain.dto.CarDto;
 
 public class OutputView {
 
-    private static final String INPUT_CAR_NAMES_PROMPT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String INPUT_ATTEMPTS_PROMPT_MESSAGE = "시도할 횟수는 몇 회인가요?";
-    private static final String PROMPT_RUN_RESULT_TITLE = "실행 결과";
-    private static final String LOCATION_FORMAT = "-";
+    private static final String MESSAGE_INPUT_CAR_NAMES_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String MESSAGE_INPUT_ATTEMPTS_PROMPT = "시도할 횟수는 몇 회인가요?";
+    private static final String MESSAGE_RUN_RESULT_TITLE = "실행 결과";
+    private static final String CAR_LOCATION_SYMBOL = "-";
     private static final String CAR_INFO_FORMAT = "%s : %s\n";
     private static final String MESSAGE_FINAL_WINNER_TITLE = "최종 우승자 : ";
     private static final String WINNER_DELIMITER = ", ";
 
     public void displayInputCarNamesPrompt() {
-        System.out.println(INPUT_CAR_NAMES_PROMPT_MESSAGE);
+        System.out.println(MESSAGE_INPUT_CAR_NAMES_PROMPT);
     }
 
     public void displayAttemptsPrompt() {
-        System.out.println(INPUT_ATTEMPTS_PROMPT_MESSAGE);
+        System.out.println(MESSAGE_INPUT_ATTEMPTS_PROMPT);
     }
 
     public void displayRoundResultHeader() {
         System.out.println();
-        System.out.println(PROMPT_RUN_RESULT_TITLE);
+        System.out.println(MESSAGE_RUN_RESULT_TITLE);
     }
 
     public void displayCars(List<CarDto> cars) {
@@ -35,7 +35,7 @@ public class OutputView {
     }
 
     private String formatCarInfo(CarDto car) {
-        String location = LOCATION_FORMAT.repeat(car.location());
+        String location = CAR_LOCATION_SYMBOL.repeat(car.location());
         return CAR_INFO_FORMAT.formatted(car.name(), location);
     }
 
