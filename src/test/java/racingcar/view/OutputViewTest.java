31011dc6b872dc102cf0718516ef31525ad24876
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-28 15:34:47 +0900

    test(OutputViewTest): 자동차 이동이 연속적일 때 출력 테스트
    - 자동차 이동이 두 번 일어났을 떄, 출력이 알맞게 되는지 테스트한다.

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -43,10 +43,42 @@ public class OutputViewTest {
 
         OutputView outputView = new OutputView();
         outputView.printStageResult(cars);
-        assertThat(byteArrayOutputStream.toString().trim()).isEqualTo(
-                cars.getCars().get(0).getCarName() + " : " + "--" + "\n"
-                + cars.getCars().get(1).getCarName() + " : " + "-" + "\n"
-                + cars.getCars().get(2).getCarName() + " : " + "-"
-        );
+        assertThat(byteArrayOutputStream
+                .toString()
+                .trim())
+                .isEqualTo(
+                        cars.getCars().get(0).getCarName() + " : " + "--" + "\n"
+                        + cars.getCars().get(1).getCarName() + " : " + "-" + "\n"
+                        + cars.getCars().get(2).getCarName() + " : " + "-"
+                );
+    }
+
+    @Test
+    public void Given_Cars_객체_생성_When_자동차_이동이_두_번_있을시_Expect_메시지를_출력한다() {
+        final List<String> carNames = Arrays.asList("car1", "car2", "car3");
+        final Cars cars = new Cars(carNames, new MaxValueIntGeneratorStrategy());
+
+        cars.getCars().get(0).tryMove();
+        cars.requestCarMove();
+
+        OutputView outputView = new OutputView();
+        outputView.printStageResult(cars);
+
+        cars.getCars().get(1).tryMove();
+
+        outputView.printStageResult(cars);
+
+        assertThat(byteArrayOutputStream
+                .toString()
+                .trim())
+                .isEqualTo(
+                        cars.getCars().get(0).getCarName() + " : " + "--" + "\n"
+                        + cars.getCars().get(1).getCarName() + " : " + "-" + "\n"
+                        + cars.getCars().get(2).getCarName() + " : " + "-" + "\n"
+                        + "\n"
+                        + cars.getCars().get(0).getCarName() + " : " + "--" + "\n"
+                        + cars.getCars().get(1).getCarName() + " : " + "--" + "\n"
+                        + cars.getCars().get(2).getCarName() + " : " + "-"
+                );
     }
 }
