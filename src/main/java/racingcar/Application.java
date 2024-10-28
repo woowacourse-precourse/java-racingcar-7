4ad2f761eb04c31478210c69e5e91f730c477974
KIM MINSEOK <mskim.glenn@gmail.com>
2024-10-28 21:25:15 +0900

    refactor: Application 클래스 내 불필요한 공백 라인 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,10 +3,9 @@ package racingcar;
 import racingcar.domain.RacingCarGame;
 
 public class Application {
-
     public static void main(String[] args) {
         RacingCarGame game = new RacingCarGame();
         game.play();
     }
-
 }
+
