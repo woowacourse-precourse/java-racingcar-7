11872cd1733058fcf60ce1fa012f13606dc30ac8
lucku <lsw6731@naver.com>
2024-10-28 22:06:57 +0900

    fix: 기본 제공 테스트 코드로 수정

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -15,83 +15,18 @@ class ApplicationTest extends NsTest {
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
-        );
-        assertRandomNumberInRangeTest(
                 () -> {
                     run("pobi,woni", "1");
-                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
                 },
-                MOVING_FORWARD, MOVING_FORWARD
-        );
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("pobi", "1");
-                    assertThat(output()).contains("pobi : -", "최종 우승자 : pobi");
-                },
-                MOVING_FORWARD
-        );
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("pobi,woni", "0");
-                    assertThat(output()).contains("최종 우승자 : pobi, woni");
-                },
-                MOVING_FORWARD
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
-    void 자동차_이름이_5자_초과_예외() {
+    void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 시도할_횟수가_음수_예외() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,java", "-1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 시도할_횟수가_숫자가_아닌_입력_예외() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,java", "kimchi"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 자동차_이름이_빈_문자열_예외() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,,java", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 시도할_횟수가_빈_문자열_예외() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,java", "", ""))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 중복된_자동차_이름_예외() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi,pobi", "1"))
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
