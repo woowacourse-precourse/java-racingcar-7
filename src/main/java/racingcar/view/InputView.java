7cd4bb7448d4c94042e8e8f7d2e995734132a04b
J0YERIM <chosarah70@gmail.com>
2024-10-28 19:08:01 +0900

    refactor: InputView에서 시도 횟수 trim 하도록 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,14 +9,14 @@ public class InputView {
 
     public static String getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        final String input = Console.readLine();
+        final String input = Console.readLine().trim();
         validateNotBlank(input);
-        return input.trim();
+        return input;
     }
 
     public static int getNumberOfAttempts() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        final String input = Console.readLine();
+        final String input = Console.readLine().trim();
         validateNotBlank(input);
         validateInteger(input);
         return Integer.parseInt(input);
