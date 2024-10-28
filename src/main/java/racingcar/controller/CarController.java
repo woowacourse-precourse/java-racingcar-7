a32769df188ee40986c665745c07e5edf039cb8d
hyeonjeong9911 <knk990101@naver.com>
2024-10-28 23:39:33 +0900

    refactor: 메소드 분리
    무작위 값 검사 후 전진, 자동차 전진 상태 출력, 우승자 찾기 로직을 함수로 분리

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -5,11 +5,8 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 import java.util.List;
 import racingcar.domain.Car;
 import racingcar.repository.CarRepository;
-import racingcar.service.CarService;
 
 public class CarController {
-    private static CarService carService = new CarService();
-
     public static void saveCarNames() {
         List<String> carNameList = List.of(readLine().split(","));
         for (String name : carNameList) {
--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -1,5 +1,41 @@
 package racingcar.service;
 
-public class CarService {
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.repository.CarRepository;
+
+public class CarService {
+    public void moveCars() {
+        for (int j = 1; j <= CarRepository.carList.size(); j++) {
+            int randomValue = pickNumberInRange(0, 9);
+            if (randomValue >= 4) {
+                CarRepository.carList.get(j - 1).go();
+            }
+        }
+    }
+
+    public void printCarsStatus() {
+        for (int j = 1; j <= CarRepository.carList.size(); j++) {
+            CarRepository.carList.get(j - 1).printStatus();
+            System.out.println();
+        }
+    }
+
+    public List<String> findWinners() {
+        List<String> winnerList = new ArrayList<>();
+        int maxStatus = CarRepository.carList.stream()
+                .mapToInt(Car::getRacingStatus)
+                .max()
+                .orElse(0);
+
+        for (int i = 0; i < CarRepository.carList.size(); i++) {
+            if (CarRepository.carList.get(i).getRacingStatus() == maxStatus) {
+                winnerList.add(CarRepository.carList.get(i).getName());
+            }
+        }
+        return winnerList;
+    }
 }
