19a34ebe8bcd369cc07cb3a0948c1239cab989ec
kim-se-hee <saree98@naver.com>
2024-10-28 16:47:48 +0900

    feat(ConsoleInputHandler): 입력 처리 클래스 구현
    ConsoleInputHandler에 사용자 입력을 받는 기능 추가
    - 사용자 입력 받음
    - 검증기를 이용해 입력 검증

--- src/main/java/racingcar/ConsoleInputHandler.java
+++ src/main/java/racingcar/ConsoleInputHandler.java
@@ -1,2 +1,28 @@
-package racingcar;public class ConsoleInputHandler {
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ConsoleInputHandler {
+
+    private static final Validator validator = new Validator();
+    private static final String DELIMITER = ",";
+
+    public List<String> getUserNameInput() {
+
+        String[] names = Console.readLine().split(DELIMITER);
+        validator.checkIfNameInputIsLegal(names);
+
+        return Arrays.asList(names);
+    }
+
+    public int getTotalStageSizeInput() {
+
+        String userInput = Console.readLine();
+        validator.checkStageSizeIsLegal(userInput);
+
+        return Integer.parseInt(userInput);
+    }
 }
