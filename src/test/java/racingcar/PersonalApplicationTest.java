fef69e7628b5df7808451290625b0c4a6510a117
parkdamin <ferrorist@naver.com>
2024-10-28 19:01:19 +0900

    test(TimeOut): 수행 시간 테스트 케이스 생성 기능 및 테스트 함수 구현
    수행 시간이 특정 시간 미만으로 수행하는 지 테스트하기 위한 함수를 구현했습니다.
    또한, 테스트 케이스로 사용할 csv 파일을 랜덤으로 생성하는 기능을 구현했습니다.

--- src/test/java/racingcar/PersonalApplicationTest.java
+++ src/test/java/racingcar/PersonalApplicationTest.java
@@ -1,13 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import generator.TestCSVFileGenerator;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class PersonalApplicationTest {
+@TestInstance(Lifecycle.PER_CLASS)
+public class PersonalApplicationTest extends NsTest {
 
     private static CarInitializer carInitializer;
 
@@ -18,6 +31,15 @@ public class PersonalApplicationTest {
         Assertions.assertEquals(expected, nameList.toString());
     }
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/TimeOutTestFile.csv")
+    void 수행_시간_테스트(String inputValue, String progressCount) {
+        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(1L), () -> {
+            run(inputValue, progressCount);
+        });
+    }
+
+
     @BeforeAll
     static void setup() {
         carInitializer = new CarInitializer();
@@ -26,5 +48,10 @@ public class PersonalApplicationTest {
     @AfterAll
     static void afterAll() {
         carInitializer = null; // 참조 해제를 통한 GC 대상 지정.
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
     }
 }
