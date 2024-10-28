a1a78cab2db80a2b44081024febe8f63bc2dc455
ppparkta <pppppark2@gmail.com>
2024-10-28 11:50:45 +0900

    refactor(RacingGameManager): input 관련 메서드명 수정
    변경사항
    - inputCarNames, inputAttemptNumber 메서드명 get~로 수정
    - Console.close() 명시적 호출

--- src/main/java/racingcar/racing/controller/RacingGameManager.java
+++ src/main/java/racingcar/racing/controller/RacingGameManager.java
@@ -1,7 +1,6 @@
 package racingcar.racing.controller;
 
-import static racingcar.racing.utils.ExceptionMessages.INVALID_ATTEMPT_EXCEPTION_MESSAGE;
-
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import racingcar.racing.model.Car;
 import racingcar.racing.model.RacingGame;
@@ -15,31 +14,30 @@ public class RacingGameManager {
     private RacingGame game;
 
     public void run() {
-        List<String> carNames = inputCarNames();
-        int attemptNumber = inputAttemptNumber();
+        List<String> carNames = getCarNames();
+        int attemptNumber = getAttemptNumber();
         createRacingGame(attemptNumber, carNames);
         List<List<RoundResult>> allRoundResult = game.start();
         OutputView.printAllRoundResult(allRoundResult);
         List<Car> winners = game.selectWinners();
         OutputView.printWinner(winners);
+        Console.close();
     }
 
     private void createRacingGame(int attemptNumber, List<String> carNames) {
         this.game = RacingGame.of(attemptNumber, carNames);
     }
 
-    private int inputAttemptNumber() {
-        OutputView.printMessage("시도할 횟수는 몇 회인가요?");
-        String inputAttemptNumber = InputView.inputConsole();
+    private int getAttemptNumber() {
+        String inputAttemptNumber = InputView.inputConsole("시도할 횟수는 몇 회인가요?");
         InputValidator.validateInputNotEmpty(inputAttemptNumber);
         int attemptNumber = InputParser.parseAttemptNumber(inputAttemptNumber);
         InputValidator.validateAttemptNumber(attemptNumber);
         return attemptNumber;
     }
 
-    private List<String> inputCarNames() {
-        OutputView.printMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String inputCar = InputView.inputConsole();
+    private List<String> getCarNames() {
+        String inputCar = InputView.inputConsole("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         InputValidator.validateInputNotEmpty(inputCar);
         List<String> carNames = InputParser.parseCarNames(inputCar);
         InputValidator.validateCarNameLength(carNames);
