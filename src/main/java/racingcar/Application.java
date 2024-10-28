8f7747fcb150470afe9ad838b0bb7ba53ca3498a
bongj9 <lollypark501@naver.com>
2024-10-28 17:16:02 +0900

    feat:stop & go 기능구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -12,11 +12,21 @@ public class Application {
         String car1 = tokenizer.nextToken();
         String car2 = tokenizer.nextToken();
         String car3 = tokenizer.nextToken();
+        String[] cars = new String[]{car1, car2, car3};
         System.out.println("시도할 홧수는 몇 회인가요?");
         int trial = Integer.parseInt(Console.readLine());
 
         System.out.println("실행 결과");
 
+        //전진 멈추는 방법(4이상일때만 전진)
+        for (int i = 0; i < trial; i++) {
+            for (String car : cars) {
+                int value = Randoms.pickNumberInRange(0, 9);
+                if (value >= 4) {
+                    car.go();
+                }
+            }
+        }
 
     }
 }
