b5dc76a998d008c319877dc5ea1985a13ea793a9
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-28 19:40:41 +0900

    refactor: Application 클래스 코드 최적화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,16 +1,9 @@
 package racingcar;
 
 import racingcar.controller.RacingController;
-import racingcar.controller.RacingGameController;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        RacingController controller = new RacingController(inputView, outputView);
-
-        controller.playGame();
+        new RacingController().playGame();
     }
 }
