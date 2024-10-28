c3d02dd6be361e51207b89430cd37e4950efa39d
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-10-28 09:03:46 +0900

    feat: 경주할 자동차 생성 기능 구현
    입력 받은 자동차 문자열을 구분자로 구분하여 경주할 자동차 객체를 각각 생성할 수 있도록 구현했습니다.

--- src/main/java/racingcar/car/CarService.java
+++ src/main/java/racingcar/car/CarService.java
@@ -1,13 +1,31 @@
 package racingcar.car;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CarService {
-    public void createCar() {
+    private final String CAR_DELIMITER = ",";
+
+    public List<Car> createCar(String raceCar) {
+        List<Car> cars = new ArrayList<>();
+        List<String> raceCars = Arrays.asList(raceCar.split(CAR_DELIMITER));
+
+        for (String car : raceCars) {
+            cars.add(new Car(car));
+        }
+
+        return cars;
     }
 
-    public boolean validateName(/*자동차 이름*/) {
+    public void validateName(String car) {
+    }
+
+    public boolean isUsableName(/*자동차 이름*/) {
         // 이름 5자 이하면 true 반환
         return false;
     }
+
 
     public void countTotalMove() {
         // 움직인 자동차 개수 세기
