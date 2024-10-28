ab47f7080aa0ea63948cae95ac28fc2cd864fb36
sytTest <syt1128@naver.com>
2024-10-28 13:17:56 +0900

    :sparkles: (RegistrationController) add lineupSetting()
    입력 받은 자동차 이름 입력을 특정한 구분자를 기준으로 나눈 리스트의 형태로 반환해주는 lineupSetting() 추가

--- src/main/java/racingcar/controller/RegistrationController.java
+++ src/main/java/racingcar/controller/RegistrationController.java
@@ -3,6 +3,7 @@ package racingcar.controller;
 import java.util.List;
 import java.util.function.Function;
 import racingcar.domain.Input;
+import racingcar.service.constant.ExpressionFormat;
 import racingcar.service.input.InputService;
 import racingcar.view.UserInput;
 
@@ -29,5 +30,9 @@ public class RegistrationController {
 
     private String validatedInput(Input userInput, Function<String, String> validation) {
         return validation.apply(userInput.getInput());
+    }
+
+    private List<String> lineupSetting(String carNames) {
+        return List.of(carNames.split(ExpressionFormat.SEPARATOR.form()));
     }
 }
