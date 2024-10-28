354c37556e54ef6c46ebb9c866f2ddde2ca0649e
GrO0vy <lmc2819@naver.com>
2024-10-28 22:43:25 +0900

    feat: 자동차 리스트 입력 값 파싱 기능 추가

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,6 +1,7 @@
 package racingcar.controller;
 
 import racingcar.model.Car;
+import racingcar.model.CarListParser;
 import racingcar.model.WinnerSelector;
 import racingcar.view.inputView.CarNameInputView;
 import racingcar.view.inputView.InputView;
@@ -13,24 +14,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RacingController {
-    private final List<Car> carList;
-    private final WinnerSelector winnerSelector;
+    private List<Car> carList;
     private final InputView carNameInputView;
     private final InputView moveCountInputView;
-    private final OutputView progressOutputView;
-    private final OutputView winnersOutputView;
 
     public RacingController(){
-        this.carList = new ArrayList<>();
-        this.winnerSelector = new WinnerSelector();
         this.carNameInputView = new CarNameInputView();
         this.moveCountInputView = new MoveCountInputView();
-        this.progressOutputView = new ProgressOutputView(carList);
-        this.winnersOutputView = new WinnersOutputView(winnerSelector);
     }
 
     public void startRacing(){
-        carNameInputView.input();
-        moveCountInputView.input();
+        String carListInput = carNameInputView.input();
+        Integer moveCountInput = Integer.parseInt(moveCountInputView.input());
+
+        CarListParser parser = new CarListParser(carListInput);
+        carList = parser.parsingData();
     }
 }
---
+++ src/main/java/racingcar/model/CarListParser.java
@@ -0,0 +1,22 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarListParser {
+    private final String input;
+
+    public CarListParser(String input){
+        this.input = input;
+    }
+
+    public List<Car> parsingData(){
+        List<Car> carList = new ArrayList<>();
+
+        for(String carName: input.split(",")){
+            carList.add(new Car(carName));
+        }
+
+        return carList;
+    }
+}
