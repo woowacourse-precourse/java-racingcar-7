c0dccae4d849554ff4064108605b767fe5280cec
kimsehw <seunghwan9703@gmail.com>
2024-10-28 19:03:32 +0900

    refactor(RacingGame): generate 책임 분리
    generate 분리에 따라 records 를 외부로부터 생성자로 전달 받도록 합니다.
    기존 generator 와의 의존도를 낮춥니다.

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -2,40 +2,12 @@ package racingcar.model;
 
 import static racingcar.message.ExceptionMessage.INVALID_RANDOM_NUMBER_EXCEPTION_MESSAGE;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
 import racingcar.GameSettings;
 
 public class RacingGame {
 
-    private final List<RacingRecord> records = new ArrayList<>();
-
-    public RacingGame(String names) {
-        List<Car> cars = CarsGenerator.generateFrom(names);
-        for (Car car : cars) {
-            records.add(new RacingRecord(car));
-        }
-    }
-
-    public List<RacingRecord> getRecords() {
-        return records;
-    }
-
-    private int generateRandomNumber() {
-        return Randoms.pickNumberInRange(GameSettings.MIN_RANDOM_NUMBER.getNumber()
-                , GameSettings.MAX_RANDOM_NUMBER.getNumber());
-    }
-
-    public List<Integer> generateRandomNumbers() {
-        List<Integer> randomNumbers = new ArrayList<>();
-        for (int i = 0; i < records.size(); i++) {
-            randomNumbers.add(generateRandomNumber());
-        }
-        return randomNumbers;
-    }
-
-    public void racing(List<Integer> randomNumbersOfRound) {
+    public void racing(List<Integer> randomNumbersOfRound, List<RacingRecord> records) {
         checkingRandomNumbers(randomNumbersOfRound);
 
         for (int i = 0; i < randomNumbersOfRound.size(); i++) {
