6b795f194cec52627156168052c0dc0a6f0ac9fd
bbandm <hjerachel@gmail.com>
2024-10-28 23:07:48 +0900

    feat(CarMover): 4 이상일 경우 자동차 전진

--- src/main/java/racingcar/domain/CarMover.java
+++ src/main/java/racingcar/domain/CarMover.java
@@ -3,6 +3,10 @@ package racingcar.domain;
 import java.util.List;
 
 public class CarMover {
+    private static void moveCarIfNeeded(Car car, int randomNumber) {
+        if (isMoveAllowed(randomNumber))
+            car.move();
+    }
     private static boolean isMoveAllowed(int randomNumber) {
         return randomNumber >= 4;
     }
