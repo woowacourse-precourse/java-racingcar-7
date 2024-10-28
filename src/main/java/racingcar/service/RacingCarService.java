1cae6616b1cbeb4e7935c890dbf068392bde71d1
Limwngur <ovg07047@naver.com>
2024-10-28 20:11:22 +0900

    feat: RacingGame 실행 로직 서비스 코드 구현

---
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -0,0 +1,46 @@
+package racingcar.service;
+
+import racingcar.domain.RacingCar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingCarService {
+
+    public List<RacingCar> runGame(final List<String> inputCars, final int trial) {
+
+        List<RacingCar> racingRacingCars = generateCar(inputCars);
+
+        System.out.println("실행 결과");
+        for (int trialIndex = 0; trialIndex < trial; ++trialIndex) {
+            for (RacingCar racingCar : racingRacingCars) {
+                // 자동차 이동
+                racingCar.conditionalMove();
+
+                //현재 상황 출력
+                printCurState(racingCar);
+            }
+            System.out.println();
+        }
+
+        return racingRacingCars;
+    }
+
+    private void printCurState(final RacingCar racingCar) {
+        // 현재 상황 출력
+        System.out.print(racingCar.getName() + " : ");
+        for (int j = 0; j < racingCar.getLocation(); ++j) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    private List<RacingCar> generateCar(final List<String> inputCars) {
+        List<RacingCar> racingCars = new ArrayList<>();
+        for (String inputCar : inputCars) {
+            racingCars.add(new RacingCar(inputCar));
+        }
+
+        return racingCars;
+    }
+}
