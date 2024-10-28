46d1544f87a7db86960fe76594d59a2ac9b1a442
BusanGukbap <dong2857210@gmail.com>
2024-10-28 16:01:02 +0900

    feat($View) : create class - OutputView

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,23 @@
+package racingcar.view;
+
+import racingcar.model.Car;
+
+import java.util.list;
+
+public class OutputView {
+
+    private static final String RESULT_MESSAE = "실행 결과";
+    private static final String WINNER_ANNOUNCEMENT_MESSAGE = "최종 우승자 : ";
+
+    public static void printExecutionResult() {
+
+    }
+
+    public static void printCars(List<Car> cars) {
+
+    }
+
+    public static void printWinners(List<String> Winners) {
+
+    }
+}
