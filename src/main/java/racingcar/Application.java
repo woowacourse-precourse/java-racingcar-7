cde7f9ff5010be42c41674c4c02646a353f1b43d
LeeJU <thirdspirit@naver.com>
2024-10-28 11:53:39 +0900

    build(App): implement main Application
    Implements: Application.java

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.model.RacingCarRepository;
+import racingcar.validator.Validator;
+import racingcar.view.RacingCarIO;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        RacingCarIO racingCarIO = new RacingCarIO();
+        RacingCarRepository raceRepository = new RacingCarRepository();
+        Validator validator = new Validator();
+        RacingCarController racingCarController = new RacingCarController(racingCarIO, raceRepository, validator);
+
+        racingCarController.getCarList();
+        racingCarController.getTryCount();
+        racingCarController.calculateRandomNumber();
+        racingCarController.calculateWinner();
     }
 }
