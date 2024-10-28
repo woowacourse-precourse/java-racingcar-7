19704ff5e4c1e8667e25d6de9ce6280145f4f325
jjun522 <tjwowns522@>
2024-10-28 19:30:12 +0900

    feat: InputView, ResultView 클래스 추가
    - InputView: 사용자 입력을 처리하는 클래스 (자동차 이름 및 시도 횟수)
    - ResultView: 게임 결과를 출력하는 클래스 (각 라운드 결과와 우승자 정보)

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,33 @@
-package racingcar.View;
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.validator.CarNameValidator;
+import racingcar.validator.RaceValidator;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
+    public static List<String> getCarNames() {
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String input = Console.readLine();
+            List<String> carNames = Arrays.asList(input.split(","));
+            CarNameValidator.validateNames(carNames);
+            return carNames; 
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("잘못된 자동차 이름 입력입니다.");
+        }
+    }
+
+    public static int getNumberOfRounds() {
+        try {
+            System.out.println("시도할 횟수는 몇 회인가요?");
+            String input = Console.readLine();
+            RaceValidator.validateRoundsInput(input);
+            return Integer.parseInt(input);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("잘못된 시도 횟수 입력입니다.");
+        }
+    }
 }
