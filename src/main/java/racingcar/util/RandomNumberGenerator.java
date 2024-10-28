b1df67c9687f7e6510260911b6a095e502f9db23
BusanGukbap <dong2857210@gmail.com>
2024-10-28 16:00:32 +0900

    feat($Util) : create class - RandomNumberGenerator

---
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -0,0 +1,14 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+
+    private static final int RANDOM_START_INCLUSIVE = 0;
+    private static final int RANDOM_END_INCLUSIVE = 9;
+    private static final int MOVE_THRESHOLD = 4;
+
+    public static boolean shouldMove() {
+
+    }
+}
