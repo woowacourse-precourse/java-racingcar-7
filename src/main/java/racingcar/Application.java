0f98d8cb90635ecf10c33070e28e843b2338cd5e
nsb329 <nsb329@naver.com>
2024-10-28 17:42:24 +0900

    자동차 이름 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import java.util.ArrayList;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		String car[] = input_name();
+		int[] result = new int[car.length];
+		System.out.println("실행 결과");
+			System.out.println();
+		}
+
+	public static String[] input_name() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		String name_input = Console.readLine();
+		if (name_input.contains(",")) {
+			String[] arr = name_input.split(",");
+				System.out.println(name_input);
+			return arr;
+		} else {
+			String[] arr = { name_input };
+			System.out.println(name_input);
+			return arr;
+		}
+	}
 }
