2a2297cd9aaa43f622ee1d93c61fef0a8857f289
SolfE <love23869318@gmail.com>
2024-10-28 18:49:23 +0900

    feat: add Car create function

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private int position = 0;
+    private String carName;
+
+    public Car(String carName) {
+        this.carName = carName;
+    }
+
+    public void move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            position++;
+        }
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+}
