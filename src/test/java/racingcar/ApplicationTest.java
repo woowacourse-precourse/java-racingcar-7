65c331e476c3710f616ffb85aefcfd22bd8f0d09
kimch <rlacogk6577@daum.net>
2024-10-28 23:01:49 +0900

    test: 입력값 오류 테스트케이스 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -28,6 +29,48 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    @DisplayName("공백만으로 생성된 이름")
+    void 이름공백_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("   ,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    @DisplayName("공백으로 시작된 이름")
+    void 이름공백시작_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" user,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("이름 길이가 5이상 일때")
+    void 이름길이5이상_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("hello world,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    @DisplayName("이름에 이스케이프 문자가 포함되었을때")
+    void 이름이스케이프포함_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("\njun,may", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("이동횟수가 문자일때")
+    void 이동횟수문자_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" user,pobi", "hello"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
