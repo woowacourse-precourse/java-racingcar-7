6ff3b37ce3cc85c897b3eb40e09ab2c64f5947ea
cjh-19 <gns8812@naver.com>
2024-10-28 17:39:07 +0900

    feat(): add input function
    - input car name(1~N)
    - input round

---
+++ src/main/java/racingcar/view/ConsoleView.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ConsoleView {
+
+    public String[] inputCar() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carName = Console.readLine();
+        return carName.split(",");
+    }
+
+    public int inputRound() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
+}
