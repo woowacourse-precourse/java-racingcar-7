c92438b6fce1986602f34007ccb04d65e8340188
Hyejeong <thswjd0609@gmail.com>
2024-10-28 21:19:41 +0900

    feat: Race 클래스 추가

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,2 +1,19 @@
-package racingcar;public class Race {
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+    private final List<Car> carList;
+    private final int moveCount;
+    private int maxScore;
+
+    public Race(List<String> carNames, int moveCount) {
+        this.carList = new ArrayList<>();
+        for (String carName : carNames) {
+            this.carList.add(new Car(carName));
+        }
+        this.moveCount = moveCount;
+        this.maxScore = 0;
+    }
 }
