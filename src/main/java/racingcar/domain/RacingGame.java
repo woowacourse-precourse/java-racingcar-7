24a2c6ebc153b7219b37f1ed273737136468ec2e
JMJ <ghtntkdnsk@naver.com>
2024-10-28 17:34:30 +0900

    feat(RacingGame) : 자동차 경주의 진행 및 시도 횟수를 관리하는 RacingGame 클래스 추가

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,41 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RacingGame {
+
+    private final List<Car> cars;
+    private final int tryCount;
+
+    public RacingGame(List<Car> cars, int tryCount) {
+        this.cars = cars;
+        this.tryCount = tryCount;
+    }
+
+    public void start() {
+        for (int i = 0; i < tryCount; i++) {
+            playRound();
+        }
+    }
+
+    private void playRound() {
+        for (Car car : cars) {
+            int randomValue = Randoms.pickNumberInRange(0, 9);
+            car.move(randomValue);
+        }
+    }
+
+    public List<Car> getWinners() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        List<Car> winners = cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .toList();
+
+        return winners;
+    }
+}
