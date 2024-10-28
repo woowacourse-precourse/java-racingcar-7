fa8ffca66435fd51732ec2b34ff3c698db417a2d
gyuho <kyuhokim12@gmail.com>
2024-10-28 15:29:45 +0900

    test: 구현할 기능 목록에서 식별되어 있는 기능 및 예외 사항에 대한 테스트 코드 추가
    -구현할 기능 목록에서 식별한 기능 및 예외사항에 대한 테스트 코드를 추가하여 더욱 견고한 소프트웨어를 제작

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Attempts;
+import racingcar.domain.CarName;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -29,6 +31,81 @@ class ApplicationTest extends NsTest {
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 우승자_여러명_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,jun", "2");
+                    assertThat(output()).contains(
+                            "최종 우승자 : pobi,woni"
+                    );
+                },
+                // 첫 번째 시도
+                MOVING_FORWARD, MOVING_FORWARD, STOP,
+                // 두 번째 시도
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 예외_테스트_자동차_이름_길이_초과() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("자동차 이름의 길이는 5 이하여야만 합니다");
+        });
+    }
+
+    @Test
+    void 예외_테스트_자동차_이름_중복() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("pobi,pobi", "1"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("자동차 이름은 중복될 수 없습니다");
+        });
+    }
+
+    @Test
+    void 예외_테스트_시도_횟수_음수() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("pobi,woni", "-1"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("시도 할 횟수는 1 이상의 정수여야 합니다");
+        });
+    }
+
+    @Test
+    void 예외_테스트_시도_횟수_숫자아님() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("pobi,woni", "three"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("시도 할 횟수는 1 이상의 정수여야 합니다");
+        });
+    }
+
+    @Test
+    void 빈_자동차_이름_예외_테스트() {
+        assertThatThrownBy(() -> new CarName("   "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차의 이름은 비어있을 수 없습니다.");
+    }
+
+    @Test
+    void 시도횟수_공백_예외_테스트() {
+        assertThatThrownBy(() -> new Attempts("   "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도 할 횟수는 1 이상의 정수여야 합니다");
+    }
+
+    @Test
+    void 예외_테스트_시도_횟수_0() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("pobi,woni", "0"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("시도 할 횟수는 1 이상의 정수여야 합니다");
+        });
     }
 
     @Override
