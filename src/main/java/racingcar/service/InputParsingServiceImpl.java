24727cab376380eeae1a2348272edc18d46b417f
thsghdud13 <thsgdud25@naver.com>
2024-10-28 20:12:41 +0900

    refactor: 주석 제거
    * InputParsingServiceImpl의 주석 제거

--- src/main/java/racingcar/service/InputParsingServiceImpl.java
+++ src/main/java/racingcar/service/InputParsingServiceImpl.java
@@ -5,15 +5,11 @@ import java.util.List;
 public class InputParsingServiceImpl implements InputParsingService {
     @Override
     public List<String> parseCarNameListString(String input) {
-        /** 예외처리 로직**/
-
         return List.of(input.split(","));
     }
 
     @Override
     public int parseAttemptCountString(String input) {
-        /** 예외처리 로직**/
-
         return Integer.parseInt(input);
     }
 }
