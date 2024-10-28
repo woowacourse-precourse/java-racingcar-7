fb31e65eb4aee3cc350b437753225031d658acf1
Lee Ye Jin <jene0028@gmail.com>
2024-10-28 18:58:34 +0900

    feat: 자동차 이름, 시도 횟수 입력 받기

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,9 @@
+package racingcar.controller;
+
+import racingcar.view.InputView;
+
+public class RacingCarController {
+    public void run() {
+        String carNames = InputView.getCarNames();
+    }
+}
