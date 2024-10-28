e6d999d2faafe17b2fad642e8d8feb1a9227012a
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-10-28 20:14:46 +0900

    feat: Extract number of attempts from user input
    - 사용자 input을 통해 시도 횟수를 받습니다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,19 +1,21 @@
 package racingcar;
 
-import java.util.ArrayList;
-import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
         String[] carNames = getCarNames();
-
-
+        Integer attempts = getNumberOfAttempts();
     }
+
     private static String[] getCarNames(){
         System.out.println("경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분)");
         String carNamesInput = Console.readLine();
         return carNamesInput.split(",");
+    }
+
+    private static int getNumberOfAttempts(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(Console.readLine());
     }
 }
