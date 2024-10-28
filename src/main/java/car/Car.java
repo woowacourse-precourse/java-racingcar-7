f73cec2ffa02fe96576a29e4bbb18108c0b1c29a
belight2 <chlwnsgur090@gmail.com>
2024-10-28 17:44:14 +0900

    feat: canMove() 함수 추가
    - canMove() : 0~9 중 무작위로 값을 받아와 4 이상이면 true를 반환
    true를 반환하면 한 칸 이동을 할 수 있다는 의미임

--- src/main/java/car/Car.java
+++ src/main/java/car/Car.java
@@ -1,8 +1,10 @@
 package car;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
-    public static Integer maxDistance;
+    public static Integer maxDistance = 0;
     private final String name;
     private Integer distance;
 
@@ -11,7 +13,13 @@ public class Car {
         this.distance = 0;
     }
 
+    private boolean canMove(){
+        return Randoms.pickNumberInRange(0, 9) >= 4;
+    }
     public void move(){
+        if(!canMove()){
+            return;
+        }
         distance++;
         maxDistance = Math.max(distance, maxDistance);
     }
