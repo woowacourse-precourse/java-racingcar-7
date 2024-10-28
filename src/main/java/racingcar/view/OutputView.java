11dbf0bca3b47e7a3914b32c984fee8191f09c4b
DongHoon Lee <dhl1924@naver.com>
2024-10-28 19:05:06 +0900

    feat(view): OutputView 인터페이스에 printRoundResult 및 printWinners 메서드 추가 및 ConsoleOutputView에서 구현
    - OutputView 인터페이스에 자동차의 진행 상태를 출력하는 printRoundResult 메서드와 우승자를 출력하는 printWinners 메서드 추가
    - ConsoleOutputView 클래스에서 해당 메서드들을 구현하여 자동차의 진행 상태와 최종 우승자를 콘솔에 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,7 +1,14 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.domain.Car;
+
 public interface OutputView {
     void printCarNamesRequest();
 
     void printTryCountRequest();
+
+    void printRoundResult(List<Car> cars);
+
+    void printWinners(List<Car> winners);
 }
--- src/main/java/racingcar/view/console/ConsoleOutputView.java
+++ src/main/java/racingcar/view/console/ConsoleOutputView.java
@@ -1,16 +1,48 @@
 package racingcar.view.console;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Car;
 import racingcar.view.OutputView;
 
 public class ConsoleOutputView implements OutputView {
     private static final String CAR_NAMES_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_COUNT_REQUEST_MESSAGE = "시도할 횟수를 입력하세요.";
+    private static final String NAME_POSITION_SEPARATOR = " : ";
+    private static final String POSITION_MARKER = "-";
+    private static final String WINNER_ANNOUNCEMENT_MESSAGE = "최종 우승자 : ";
 
+    @Override
     public void printCarNamesRequest() {
         System.out.println(CAR_NAMES_REQUEST_MESSAGE);
     }
 
+    @Override
     public void printTryCountRequest() {
         System.out.println(TRY_COUNT_REQUEST_MESSAGE);
+    }
+
+    @Override
+    public void printRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(formatCarPosition(car));
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void printWinners(List<Car> winners) {
+        String winnerNames = formatWinnerNames(winners);
+        System.out.println(WINNER_ANNOUNCEMENT_MESSAGE + winnerNames);
+    }
+
+    private String formatCarPosition(Car car) {
+        return car.getName().carName() + NAME_POSITION_SEPARATOR + POSITION_MARKER.repeat(car.getPosition().position());
+    }
+
+    private String formatWinnerNames(List<Car> winners) {
+        return winners.stream()
+                .map(car -> car.getName().carName())
+                .collect(Collectors.joining(", "));
     }
 }
