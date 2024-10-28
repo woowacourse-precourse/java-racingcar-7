fdf39195171b7f886280f8b6022ba703de1a4461
진민규 <jinmingyu@jinmingyuui-MacBookPro.local>
2024-10-28 20:03:04 +0900

    feat: 시도 횟수 입 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,9 +7,11 @@ public class Application {
         // TODO: 프로그램 구현
         try {
             String inputNames = readCarNames();
-            GameService game = new GameService(inputNames);
-            System.out.println("입력된 자동차 이름:");
+            String inputCount = readCount();
+            GameService game = new GameService(inputNames, inputCount);
+            System.out.println("입력된 자동차 이름 : ");
             game.getCars().forEach(car -> System.out.println(car.getName()));
+            System.out.println("시도 횟수 : " + game.getCount());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
@@ -17,6 +19,11 @@ public class Application {
 
     private static String readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return camp.nextstep.edu.missionutils.Console.readLine();
+    }
+
+    private static String readCount() {
+        System.out.println("시도할 횟수는 몇 회 인가요?");
         return camp.nextstep.edu.missionutils.Console.readLine();
     }
 }
--- src/main/java/racingcar/GameService.java
+++ src/main/java/racingcar/GameService.java
@@ -6,12 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 // 레이싱게임의 서비스 로직
-// 차 목록
+// 차 목록, 시도 횟수
 public class GameService {
     private final List<Car> cars;
+    private final int tryNumber;
 
-    public GameService(String inputNames) {
+    public GameService(String inputNames, String inputCount) {
         this.cars = parseCarNames(inputNames);
+        this.tryNumber = parseCount(inputCount);
     }
 
     private List<Car> parseCarNames(String inputNames) {
@@ -29,7 +31,23 @@ public class GameService {
         return carList;
     }
 
+    private int parseCount(String inputCount) {
+        try {
+            int count = Integer.parseInt(inputCount);
+            if (count <= 0) {
+                throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+            }
+            return count;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
+        }
+    }
+
     public List<Car> getCars() {
         return cars;
+    }
+
+    public int getCount() {
+        return tryNumber;
     }
 }
