df245c89af7ac0bc1c8a82741cd2e9d84a9c332d
hlmg <devhlmg@gmail.com>
2024-10-28 13:27:54 +0900

    refactor: 입출력 담당하는 클래스 생성

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -1,45 +1,34 @@
 package racingcar.view;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import racingcar.model.RacingCarSnapShot;
+import racingcar.view.io.InputHandler;
+import racingcar.view.io.OutputHandler;
 
 public class RacingCarView {
+    public InputHandler inputHandler = new InputHandler();
+    public OutputHandler outputHandler = new OutputHandler();
 
     public List<String> getRacingCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
-        return List.of(input.split(","));
+        outputHandler.showCarNameInputComment();
+        return inputHandler.getCarNamesFromUser();
     }
 
     public int getAttemptCount() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String input = Console.readLine();
-        return convertToNumber(input);
-    }
-
-    private int convertToNumber(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자를 입력해주세요.");
-        }
+        outputHandler.showAttemptCountInputComment();
+        return inputHandler.getAttemptCount();
     }
 
     public void showRaceResultComment() {
-        System.out.println();
-        System.out.println("실행 결과");
+        outputHandler.showRaceResultComment();
     }
 
     public void showRaceResult(List<RacingCarSnapShot> snapShots) {
-        for (RacingCarSnapShot snapShot : snapShots) {
-            System.out.printf("%s : %s%n", snapShot.name(), "-".repeat(snapShot.position()));
-        }
-        System.out.println();
+        outputHandler.showRaceResult(snapShots);
     }
 
     public void showWinners(List<String> winners) {
-        System.out.printf("최종 우승자 : %s", String.join(", ", winners));
+        outputHandler.showWinners(winners);
     }
 
 }
---
+++ src/main/java/racingcar/view/io/OutputHandler.java
@@ -0,0 +1,31 @@
+package racingcar.view.io;
+
+import java.util.List;
+import racingcar.model.RacingCarSnapShot;
+
+public class OutputHandler {
+    public void showCarNameInputComment() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void showAttemptCountInputComment() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+    public void showRaceResultComment() {
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
+    public void showRaceResult(List<RacingCarSnapShot> snapShots) {
+        for (RacingCarSnapShot snapShot : snapShots) {
+            System.out.printf("%s : %s%n", snapShot.name(), "-".repeat(snapShot.position()));
+        }
+        System.out.println();
+    }
+
+    public void showWinners(List<String> winners) {
+        System.out.printf("최종 우승자 : %s", String.join(", ", winners));
+    }
+
+}
