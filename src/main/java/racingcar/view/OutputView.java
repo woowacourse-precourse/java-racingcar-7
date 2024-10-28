c3282ca4299a0636e14ae2c407f115a5751c59f0
ryan-dia <cjfdnjs158@naver.com>
2024-10-28 17:04:20 +0900

    refactor: format 기능 메서드로 추

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,6 +6,7 @@ import racingcar.model.CarSnapshot;
 public class OutputView {
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ATTEMPT_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
 
     private OutputView() {
     }
@@ -30,13 +31,17 @@ public class OutputView {
     private static void printRoundResults(List<List<CarSnapshot>> roundResults) {
         for (List<CarSnapshot> roundResult : roundResults) {
             for (CarSnapshot car : roundResult) {
-                printMessage(car.carName() + " : " + "-".repeat(car.position()));
+                printMessage(formatCarStatus(car));
             }
             System.out.println();
         }
     }
 
+    private static String formatCarStatus(CarSnapshot car) {
+        return car.carName() + " : " + "-".repeat(car.position());
+    }
+
     private static void printWinners(List<String> winners) {
-        printMessage("최종 우승자 : " + String.join(", ", winners));
+        printMessage(FINAL_WINNER_MESSAGE + String.join(", ", winners));
     }
 }
