75eaefeaebec1b09a0f44a9c9d3b77a1b7eb1f6a
MinseoKangQ <kms02171@naver.com>
2024-10-28 15:07:17 +0900

    feat: 자동차 객체 생성

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
@@ -10,5 +11,13 @@ public class Game {
 
         OutputHandler.printInstructionForTimes();
         int times = InputHandler.getTimes();
+
+        // 핵심 로직
+        List<Car> cars = new ArrayList<>();
+
+        // 1. 객체 생성
+        for(String carName : carNames) {
+            cars.add(new Car(carName));
+        }
     }
 }
