8745ce768b6718cc5b224178d12bf01d47de0c19
gorapaduckoo <jioung93@naver.com>
2024-10-28 21:01:48 +0900

    refactor: main함수의 GameView를 OutputHandler로 이름 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,7 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        Game game = new Game(new InputHandler(), new GameView());
+        Game game = new Game(new InputHandler(), new OutputHandler());
         game.start();
     }
 }
