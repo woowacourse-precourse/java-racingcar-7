bfa944a6009eb84fa87a9dc6f5a3ac7dc2e211f3
박성춘 <songchoon10@naver.com>
2024-10-28 14:59:52 +0900

    chore: 필요없는 import문 삭제

--- src/test/java/racingcar/model/RacingTest.java
+++ src/test/java/racingcar/model/RacingTest.java
@@ -1,13 +1,10 @@
 package racingcar.model;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.dto.RacingInfo;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RacingTest {
     private Racing racing;
