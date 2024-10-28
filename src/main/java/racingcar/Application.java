3ea08ec720f7ed6ead2f82971c448f5909761cec
BusanGukbap <dong2857210@gmail.com>
2024-10-28 21:24:58 +0900

    refactor($Application) : try-catch문 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,12 +5,7 @@ import racingcar.controller.RacingGameController;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
-        try {
-            RacingGameController controller = new RacingGameController();
-            controller.startGame();
-        } catch(IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        RacingGameController controller = new RacingGameController();
+        controller.startGame();
     }
 }
