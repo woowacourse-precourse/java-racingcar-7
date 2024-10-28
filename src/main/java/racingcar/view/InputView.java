07411ace7ad7313d83e2ee5da5e8ca55624a1d16
EgPwl <egpwl07@gmail.com>
2024-10-28 14:30:24 +0900

    feat(InputView): 자동차 이름, 시도 횟수 입력

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,22 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+
+public class InputView {
+
+    private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ATTEMPT_NUMBER_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
+    public static String getCarNames() {
+        System.out.println(INPUT_CAR_NAME_MESSAGE);
+        String input = Console.readLine();
+        return input;
+    }
+
+    public static int getAttemptNumber() {
+        System.out.println(INPUT_ATTEMPT_NUMBER_MESSAGE);
+        int input = Integer.parseInt(Console.readLine());
+        return input;
+    }
+}
