5d51e591abb25c1c29bd4a6eeed7debca27e3171
JiEung2 <181757@jnu.ac.kr>
2024-10-28 22:33:19 +0900

    refactor(outputView): RoundHistory 도입에 따른 OutputView 수정
    - 모든 history를 한번에 받아서 처리하기 위한 로직 수정

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,11 +1,13 @@
 package racingcar;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     private final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final String ROUND_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
 
     public void printCarNameInputMessage() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
@@ -15,10 +17,18 @@ public class OutputView {
         System.out.println(ROUND_COUNT_INPUT_MESSAGE);
     }
 
-    public void printRoundResult(List<Car> carList) {
-        for (Car car : carList) {
-            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
-        }
+    public void printRoundResults(List<RoundHistory> roundHistories) {
+        System.out.println();
+        System.out.println(EXECUTION_RESULT_MESSAGE);
+
+        roundHistories.forEach(this::printRoundHistory);
+    }
+
+    private void printRoundHistory(RoundHistory roundHistory) {
+        roundHistory.nameAndPosition().forEach((name, position) ->
+                System.out.println(name + " : " + "-".repeat(position))
+        );
+        System.out.println();
     }
 
     public void printWinner(List<String> winnerList) {
