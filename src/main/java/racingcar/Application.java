4d522d76f36ca00f57f783d959cafa34558d9976
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-10-28 14:48:09 +0900

    feat(Application): 컨트롤러를 통해 게임을 진행하도록 구현
    - 불필요한 주석을 제거하였다.
    - 게임은 컨트롤러를 통해 수행된다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,5 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -12,28 +8,8 @@ public class Application {
     public static void main(String[] args) {
         OutputView outputView = new OutputView();
         InputView inputView = new InputView();
+        GameController gameController = new GameController(outputView, inputView);
 
-        //outputView.printRacingCarNameFormat();
-        //String names = inputView.readCarNames();
-        //outputView.printRacingRoundRequest();
-        //int round = inputView.readRacingRound();
-
-        //String[] splitNames = names.split(",");
-        //List<Car> cars = Arrays.stream(splitNames).map(Car::from).toList();
-
-        //while(round-- > 0) {
-        //    cars.forEach(car -> car.move(Randoms.pickNumberInRange(0, 9)));
-        //    outputView.printMovingCarsWithPositions(cars);
-        //}
-
-        //우승자 결정
-//        List<Integer> positions = cars.stream().map(Car::getPosition).toList();
-//        int maxPosition = Collections.max(positions);
-//        List<String> winners = cars.stream()
-//            .filter(car -> car.getPosition() == maxPosition)
-//            .map(Car::getName)
-//            .toList();
-//
-//        outputView.printRacingWinners(winners);
+        gameController.run();
     }
 }
