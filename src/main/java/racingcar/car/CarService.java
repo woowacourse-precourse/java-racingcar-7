c57a22f4123f4b4404def4c0d3aeb6913d77824f
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-10-28 22:36:47 +0900

    refactor: 자동차 이름 예외 발생 문구 코드 수정
    자동차 이름의 유효성을 검사한 후 예외 발생시 출력하는 문구를 문자열 상수로 변경했습니다.

--- src/main/java/racingcar/car/CarService.java
+++ src/main/java/racingcar/car/CarService.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 public class CarService {
     private final String CAR_DELIMITER = ",";
+    private final String CAR_NAME_EXCEPTION = "자동차 이름이 5자를 초과했습니다.";
 
     public List<Car> createCar(String raceCar) {
         return Arrays.stream(raceCar.split(CAR_DELIMITER))
@@ -18,7 +19,7 @@ public class CarService {
 
     public void validateName(String carName) {
         if (!isUsableName(carName)) {
-            throw new IllegalArgumentException("자동차 이름이 5자를 초과했습니다.");
+            throw new IllegalArgumentException(CAR_NAME_EXCEPTION);
         }
     }
 
