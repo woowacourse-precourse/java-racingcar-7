bfffbe0418a082934d551b2ecd5614635fc38b9f
minjcho <117441212+minjcho@users.noreply.github.com>
2024-10-28 22:30:58 +0900

    feat: 결과 출력 기능 구현

---
+++ src/main/java/racingcar/ResultView.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import racingcar.Car;
+import java.util.List;
+
+public class ResultView {
+
+    public static void printRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + getPositionString(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    private static String getPositionString(int position) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < position; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+}
