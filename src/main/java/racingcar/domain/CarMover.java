0f8c3a8e112fb2f1d17429bfb88ede46cec46a77
bbandm <hjerachel@gmail.com>
2024-10-28 23:08:18 +0900

    feat(CarMover): 자동자 전진 설정 기능 구현

--- src/main/java/racingcar/domain/CarMover.java
+++ src/main/java/racingcar/domain/CarMover.java
@@ -3,6 +3,13 @@ package racingcar.domain;
 import java.util.List;
 
 public class CarMover {
+    public static void decideMoveOrNot(List<Car> racingCarList) {
+        for(Car car : racingCarList) {
+            RandomNumberGenerator numberGenerator = new RandomNumberGenerator();
+            int randomNumber = numberGenerator.generate();
+            moveCarIfNeeded(car, randomNumber);
+        }
+    }
     private static void moveCarIfNeeded(Car car, int randomNumber) {
         if (isMoveAllowed(randomNumber))
             car.move();
