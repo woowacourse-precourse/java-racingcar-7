7da7547d02e3769f8c786e2bd259d28b88573cf2
sese2204 <sese2204@naver.com>
2024-10-28 14:28:21 +0900

    feat(controller): 게임 진행과 결과를 담당하는 기능 구현
    - 게임을 진행하는 run 메소드
    - 게임을 종료하고 우승자를 출력하는 finish 메소드

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -2,17 +2,31 @@ package racingcar.controller;
 
 import racingcar.service.RacingCarService;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 import java.util.List;
 
 public class RacingCarController {
     private RacingCarService racingCarService;
+    private  int tryCount;
 
         public void start(){
             List<String> nameList;
             nameList = InputView.getCarNames();
-            int tryCount = InputView.getTryCount();
+            tryCount = InputView.getTryCount();
             this.racingCarService = new RacingCarService(nameList);
         }
 
+        public void run(){
+            OutputView.printResultCommand();
+            for (int i = 0; i < tryCount; i++) {
+                racingCarService.playRound();
+                OutputView.printRoundResult(racingCarService.getCarList());
+                System.out.println();
+            }
+        }
+
+        public void finish(){
+            OutputView.printWinner(racingCarService.getWinningCarList());
+        }
 }
