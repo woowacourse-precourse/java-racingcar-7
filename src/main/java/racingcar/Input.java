e5bec3581b7abe5b235c13eafc766687eb5d5b8b
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2024-10-28 22:36:51 +0900

    refactor(Input):
    - 자동차 이름 입력 시 마지막에 ,로 끝나는 경우 exception 발생

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -5,10 +5,11 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
     private static final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRIAL_NUMBER_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String CAR_NAME_DELIMITER = ",";
     public static String inputCarName() {
         printCarNameMessage();
         String inputCarName = Console.readLine();
-        validateNull(inputCarName);
+        validateInputCarName(inputCarName);
         return inputCarName;
     }
 
@@ -27,8 +28,19 @@ public class Input {
         System.out.println(TRIAL_NUMBER_MESSAGE);
     }
 
-    private static void validateNull(final String input) {
-        if (isNull(input)) {
+    private static void validateNull(final String inputCarName) {
+        if (isNull(inputCarName)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateInputCarName(String inputCarName) {
+        validateNull(inputCarName);
+        validateEndsWithComma(inputCarName);
+    }
+
+    private static void validateEndsWithComma(String inputCarName) {
+        if (inputCarName.endsWith(CAR_NAME_DELIMITER)) {
             throw new IllegalArgumentException();
         }
     }
