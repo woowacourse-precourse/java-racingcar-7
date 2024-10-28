c25959bdb89e4fe08fff0e8c334a9ae4d45b16ac
dayaelee <dayae.dev@gmail.com>
2024-10-28 20:29:28 +0900

    feat:4. 각 자동차의 전진 조건에 대해 정하기 위해 0-9사이의 무작위 값을 구한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.HashMap;
 
@@ -41,10 +42,16 @@ public class Application {
         }
 
         //3. 전체 자동차를 포함하는 해시맵을 만들고, 키는 자동차의 이름으로 하고 값은 0으로 초기화 한다.
-
         HashMap<String, Integer> map = new HashMap<>();
         for (String car : carArr){
             map.put(car, 0);
+        }
+
+
+        for (String car : carArr){
+            //4. 각 자동차의 전진 조건에 대해 정하기 위해 0-9사이의 무작위 값을 구한다.
+            Integer pickNum = Randoms.pickNumberInRange(0, 9);
+//            map.put(car, 0);
         }
 
 
