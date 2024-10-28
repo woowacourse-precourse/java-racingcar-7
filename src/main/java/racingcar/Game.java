6a34e77de1ccf92d29a28f8f9358fe83cdd50d2d
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2024-10-28 18:54:48 +0900

    fix(Game.java) : fix parseCars() exception handling
    Add exception " "

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -45,7 +45,7 @@ public class Game {
             if(carName.length()>5){
                 throw new IllegalArgumentException("이름이 5글자 이상입니다.");
             }
-            else if(carName.isEmpty()){
+            else if(carName.isEmpty()||carName.equals(" ")){
                 throw new IllegalArgumentException("잘못된 입력입니다.");
             }
             cars.add(new Car(carName));
