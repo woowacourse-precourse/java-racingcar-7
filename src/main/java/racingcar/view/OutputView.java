f50da3e2cf5adaa940ecb334d194fc322b7a625c
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-10-28 23:17:01 +0900

    fix: 출력문 오타 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,7 +4,7 @@ import racingcar.model.Car;
 
 public class OutputView {
     public static void printWinners(String winners) {
-        System.out.println("최종 우승자: " + winners);
+        System.out.println("최종 우승자 : " + winners);
     }
 
     public static void printCarsPostions(Car[] cars) {
