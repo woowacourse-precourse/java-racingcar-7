33ef8147a4cf3c69a88786c2b5fbba0e82e9c3ab
SSU <hshwan0406@gmail.com>
2024-10-28 22:02:22 +0900

    feat: 레이싱 진행 상태와 우승자 출력 함수 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,10 +7,20 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("경주할 자동차 이름을 입력하세요.이름은 쉼표(,) 기준으로 구분");
         String racingCarsNames = Console.readLine();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String attemptCount = Console.readLine();
+
         List<Car> carArray = createCar(racingCarsNames);
 
+        printRacing(attemptCount, carArray);
+
+        printWinner(carArray);
     }
+
+
 
     public static List<Car> createCar(String racingCarsNames) {
         if(racingCarsNames == null || racingCarsNames.isBlank()) {
@@ -39,5 +49,35 @@ public class Application {
         }
 
         return carArray;
+    }
+
+    public static void printRacing(String attemptCount, List<Car> carArray) {
+        System.out.println("\n실행 결과");
+        for(int i = 0; i < Integer.parseInt(attemptCount); i++) {
+            for(Car car : carArray) {
+                car.move();
+                System.out.println(car.getName()+" : " + "-".repeat(car.getMoveCount()));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printWinner(List<Car> carArray) {
+        int maxMoveCount = 0;
+        List<String> winnerNames = new ArrayList<>();
+
+        for (Car car : carArray) {
+            maxMoveCount = Math.max(maxMoveCount, car.getMoveCount());
+        }
+
+        for (Car car : carArray) {
+            if(car.getMoveCount() >= maxMoveCount) {
+                winnerNames.add(car.getName());
+            }
+        }
+
+        String winner = String.join(", ", winnerNames);
+
+        System.out.println("최종 우승자 : " + winner);
     }
 }
