c21ed1b3902601bf43c32cbcd2cbeb57cf257bd8
rookedsysc <rookedsysc36@gmail.com>
2024-10-28 20:57:18 +0900

    feat: RacingGameController를 생성해서 게임 시작 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+import racingcar.model.RacingCar;
+import racingcar.service.CarNameParser;
+import racingcar.service.RacingGamePresenter;
+import racingcar.service.RacingGame;
+import racingcar.service.RandomMovementStrategy;
 import racingcar.view.ConsoleInput;
 import racingcar.view.ConsoleOutput;
+
+import java.util.List;
 
 import static racingcar.common.constant.OutputConstant.*;
 
@@ -16,5 +24,18 @@ public class Application {
         consoleOutput.print(INPUT_TRY_COUNT);
         Integer carTryCount = consoleInput.inputNumber();
 
+        CarNameParser carNameParser = new CarNameParser(carName);
+        List<String> carNames = carNameParser.parse();
+
+        List<RacingCar> racingCarList = carNames.stream().map(RacingCar::new).toList();
+
+        RandomMovementStrategy randomMovementStrategy = new RandomMovementStrategy();
+        RacingGame racingGame = new RacingGame(racingCarList, randomMovementStrategy, carTryCount);
+        RacingGamePresenter racingGamePresenter = new RacingGamePresenter();
+
+        RacingGameController racingGameController = new RacingGameController(racingGame, racingGamePresenter);
+        String racingResult = racingGameController.playGame();
+
+        consoleOutput.print(racingResult.toString());
     }
 }
