7fb55faf9ad5387ba911739b62b1319f0e2c8c10
leejunnyeop <daffodil8620@naver.com>
2024-10-28 16:24:13 +0900

    [refactor] RacingController(controller) - 코드정리

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,14 +1,14 @@
 package racingcar.controller;
 
 import racingcar.dto.RaceRequest;
-import racingcar.dto.RaceResponse;
 import racingcar.model.RacingModel;
 import racingcar.view.RacingView;
 
 public class RacingController {
 
-    private RacingModel model;
-    private RacingView view;
+    private final RacingModel model;
+    private final RacingView view;
+
 
     // Controller 생성자 (Model과 View를 주입받음)
     public RacingController(RacingModel model, RacingView view) {
@@ -18,20 +18,22 @@ public class RacingController {
 
     // 게임 실행
     public void run(RaceRequest request) {
+        try {
 
-        // 1. 자동차 이름을 Model에 초기화
-        model.initializeCars(request.getCarName());
+            // 1. 자동차 이름을 Model에 초기화
+            model.initializeCars(request.getCarName());
 
-        // 2. 시도 횟수만큼 경주 진행
-        for (int i = 0; i < request.getRoundCount(); i++) {
-            System.out.println("차시 : " + (i + 1));
-            model.raceOneRound();
+            // 2. 시도 횟수만큼 경주 진행
+            for (int i = 0; i < request.getRoundCount(); i++) {
+                System.out.println("차시 : " + (i + 1));
+                view.printCarStates(model.getCarStates());
+                model.raceOneRound();
+            }
+
+            // 3. 최종 우승자
+            view.printWinners(model.getWinners());
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("오류");
         }
-
-        //3. 경주 결과 생성 (자동차 상태와 우승자를 포함한 Response 객체)
-        RaceResponse response = new RaceResponse(model.getCarStates(), model.getWinners());
-
-        // 4. 우승자 결정 및 출력
-        view.printRaceResult(response);
     }
 }
