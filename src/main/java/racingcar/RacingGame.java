f752fd6bc8e5597d860ae8f81c0b8bd98caabf76
kyung-Park <ruddnjs5816@gmail.com>
2024-10-28 20:45:18 +0900

    feat: 시도 횟수 입력 기능 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -23,7 +23,6 @@ public class RacingGame {
             cars.add(new Car(name.trim()));
         }
     }
-
     // 시도 횟수 입력
     private void inputAttempts() {
         System.out.println("시도할 횟수는 몇 회인가요?");
