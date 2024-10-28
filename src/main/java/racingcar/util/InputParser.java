2a8f4f0f802da4bf7dffc3ba0bfd4d584171d74a
sanghee0820 <lsh277604@gmail.com>
2024-10-28 20:58:27 +0900

    feat: 이름이 비어있는 경우 유효성 검증 로직 추가

--- src/main/java/racingcar/util/InputParser.java
+++ src/main/java/racingcar/util/InputParser.java
@@ -12,14 +12,28 @@ public final class InputParser {
 
     public static List<String> parseName(String input) {
         validateInput(input);
-        return Arrays.stream(input.split(DELIMITER))
+        var names = Arrays.stream(input.split(DELIMITER))
                 .map(String::trim)
                 .toList();
+        validateNameCount(names, input);
+
+        return names;
     }
 
     private static void validateInput(String input) {
         if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException(InputException.INPUT_IS_NULL_OR_EMPTY.getMessage());
         }
+    }
+
+    private static void validateNameCount(List<String> names, String input) {
+        var delimiterCount = (int) input.chars().filter(ch -> ch == DELIMITER.charAt(0)).count();
+        if (isNameSameWithDelimiterCount(names, delimiterCount)) {
+            throw new IllegalArgumentException(InputException.NAME_IS_EMPTY.getMessage());
+        }
+    }
+
+    private static boolean isNameSameWithDelimiterCount(List<String> names, int delimiterCount) {
+        return delimiterCount + 1 != names.size();
     }
 }
