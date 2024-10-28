3d2e0e28ae5d1cea7667c90aa4b167ba100ef54f
Sooyong Yoon <ybh6703@naver.com>
2024-10-28 20:54:00 +0900

    feat: 무작위 값이 4이상일 경우 n대의 자동차 전진 혹은 멈춤 기능 추가

--- src/main/java/racingcar/Racingcar.java
+++ src/main/java/racingcar/Racingcar.java
@@ -1,5 +1,23 @@
 package racingcar;
 
-public class Racingcar {
+import java.util.HashMap;
 
+import camp.nextstep.edu.missionutils.*;
+
+public class Racingcar {
+    // 주어진 횟수 동안 n대 자동차는 전진 or 멈춤
+    public static HashMap<String, Integer> racing(int nums, HashMap<String, Integer> cars) {
+
+        for (int i = 0; i < nums; i++) {
+            // 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우
+            for (String s : cars.keySet()) {
+                int random_num = Randoms.pickNumberInRange(0, 9);
+                if (random_num >= 4) {
+                    cars.put(s, cars.get(s) + 1);
+                }
+            }
+        }
+
+        return cars;
+    }
 }
