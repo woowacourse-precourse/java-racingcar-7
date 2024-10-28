7fe5473e8019d0d1be420be24d3b363cd5d09f5a
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-10-28 09:42:48 +0900

    feat: 자동차 이름 입력 받기
    쉼표 기준으로 구분하여 이름 분리
    이름이 5자 이하인지 검증
    5자 이하이면 IllegalArgumentException 발생
    빈 이름이나 null 이여도 똑같이 exception 발생

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,31 @@
+package racingcar.model;
+
+public class Car {
+	private static final int MAX_NAME_LENGTH = 5;
+	private final String name;
+	private int position;
+
+	public Car(String name, int position) {
+		validateName(name);
+		this.name = name;
+		this.position = position;
+	}
+
+	private void validateName(String name) {
+		if (isInvalidName(name)) {
+			throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하여야 합니다.");
+		}
+	}
+
+	private boolean isInvalidName(String name) {
+		return name == null || name.trim().isEmpty() || name.length() > MAX_NAME_LENGTH;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+}
