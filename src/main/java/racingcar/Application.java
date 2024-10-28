15f0cd45b95ffbbfba8e188fa0f047fecc2eda70
hightuv <hightuv@gmail.com>
2024-10-28 22:00:46 +0900

    feat(Application): update Application
    Creates racing car instance and runs it

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Car> cars = RacingCarInput.carNameInput();
+        int round = RacingCarInput.roundInput();
+        RacingCar racingCar = new RacingCar(cars);
+        racingCar.run(round);
     }
 }
