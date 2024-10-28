b725dc522b605326ff5d503c1a4f5ed0e36247de
MinjuKwak01 <mkwak1125@gmail.com>
2024-10-28 21:37:50 +0900

    refactor : 랜덤 값 추출 범위 상수로 관리하게끔 리팩토링

--- src/main/java/racingcar/model/RandomGenerator.java
+++ src/main/java/racingcar/model/RandomGenerator.java
@@ -3,11 +3,12 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomGenerator {
-
+    private static final int MIN_RANDOM_NUM = 0;
+    private static final int MAX_RANDOM_NUM = 9;
     private static final int MOVABLE_VALUE = 4;
 
     public int generateRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUM, MAX_RANDOM_NUM);
     }
 
     public Boolean canMoveForward(int randomNumber) {
