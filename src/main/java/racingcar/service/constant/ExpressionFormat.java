ebf10631a30ba55653f83d8ffe8b9d76e0921771
sytTest <syt1128@naver.com>
2024-10-28 11:03:27 +0900

    :sparkles: (LapScoreService) add carPosition()
    자동차 객체를 입력받아 주행거리만큼 정해진 위치표시 문자를 반복해서 더한 뒤 문자열을 반환하는 carPosition() 을 추가

--- src/main/java/racingcar/service/constant/ExpressionFormat.java
+++ src/main/java/racingcar/service/constant/ExpressionFormat.java
@@ -6,7 +6,11 @@ public enum ExpressionFormat {
     SEPARATOR(","),
     MID_PART_GHOST_CAR(",,"),
     ONLY_NUMBERS("[0-9]+"),
+
+    POSITION_MARK("-"),
+
     WINNER_SEPARATOR(", ");
+
 
     private final String rule;
 
---
+++ src/main/java/racingcar/service/race/LapScoreService.java
@@ -0,0 +1,22 @@
+package racingcar.service.race;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.LapScore;
+import racingcar.service.constant.ExpressionFormat;
+import racingcar.view.constant.Output;
+
+public class LapScoreService {
+    private final List<LapScore> lapScores;
+
+    public LapScoreService(List<Car> entry) {
+        lapScores = new ArrayList<>();
+
+    }
+
+    private String carPosition(Car racingCar) {
+        return ExpressionFormat.POSITION_MARK.toString()
+                .repeat(racingCar.getMileage().intValue());
+    }
+}
