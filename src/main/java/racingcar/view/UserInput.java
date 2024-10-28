fb76dedf26cd1d53bc8f084d67cae17ad2d113f6
ratatuwee <pkqwaszx@gmail.com>
2024-10-28 16:37:29 +0900

    refactor(user input): 내부 enum private 변경
    사용자 입력 뷰에서 사용하는 내부 enum 클래스를 private로 변경하였습니다.
    해당 클래스를 함수로 실행하여 외부에서 접근할 필요가 없어 변경하였습니다.

--- src/main/java/racingcar/view/UserInput.java
+++ src/main/java/racingcar/view/UserInput.java
@@ -4,7 +4,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class UserInput {
 
-    public enum InputMessage{
+    private enum InputMessage{
         INPUT_CAR_NAME_PROMPT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
         INPUT_TRY_NUM_PROMPT("시도할 횟수는 몇 회인가요?");
 
