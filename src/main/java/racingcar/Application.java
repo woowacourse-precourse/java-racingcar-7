6792a3ad2328efdd72ffeb9c9ac41b7412b0dd6f
SSU <hshwan0406@gmail.com>
2024-10-28 22:40:19 +0900

    refactor: Application 클래스 메서드 수정
    Application 클래스 메서드를 RacingCar 클래스에서 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,6 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -13,71 +12,9 @@ public class Application {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String attemptCount = Console.readLine();
 
-        List<Car> carArray = createCar(racingCarsNames);
-
-        printRacing(attemptCount, carArray);
-
-        printWinner(carArray);
+        List<Car> carArray = RacingCar.createCar(racingCarsNames);
+        RacingCar.printRacing(attemptCount, carArray);
+        RacingCar.printWinner(carArray);
     }
 
-
-
-    public static List<Car> createCar(String racingCarsNames) {
-        if(racingCarsNames == null || racingCarsNames.isBlank()) {
-            throw new IllegalArgumentException();
-        }
-
-        List<Car> carArray =  new ArrayList<>();
-        String[] carsNames = racingCarsNames.split(",");
-
-        for (String carName : carsNames) {
-            if(carName.isBlank()) {
-                continue;
-            }
-
-            carName = carName.trim();
-
-            if(carName.length() > 5) {
-                throw new IllegalArgumentException();
-            }
-
-            carArray.add(new Car(carName));
-        }
-
-        if(carArray.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-
-        return carArray;
-    }
-
-    public static void printRacing(String attemptCount, List<Car> carArray) {
-        System.out.println("\n실행 결과");
-        for(int i = 0; i < Integer.parseInt(attemptCount); i++) {
-            for(Car car : carArray) {
-                car.move();
-                System.out.println(car.getName()+" : " + "-".repeat(car.getMoveCount()));
-            }
-            System.out.println();
-        }
-    }
-
-    public static void printWinner(List<Car> carArray) {
-        int maxMoveCount = 0;
-        List<String> winnerNames = new ArrayList<>();
-
-        for (Car car : carArray) {
-            maxMoveCount = Math.max(maxMoveCount, car.getMoveCount());
-        }
-
-        for (Car car : carArray) {
-            if(car.getMoveCount() >= maxMoveCount) {
-                winnerNames.add(car.getName());
-            }
-        }
-
-        String winner = String.join(", ", winnerNames);
-
-        System.out.println("최종 우승자 : " + winner);
-    }
 }
