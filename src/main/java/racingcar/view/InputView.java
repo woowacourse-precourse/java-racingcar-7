3975a6f023ac7a3f935b0d2656d2a43a592204c6
장영재 <urinaner@naver.com>
2024-10-28 16:36:28 +0900

    fix(InputView): 출력 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import racingcar.utils.ErrorMessage;
 
 public class InputView {
-    private static final String GET_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분)";
+    private static final String GET_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String GET_TRY_NUMBERS = "시도할 횟수는 몇 회인가요?";
 
     public String getCarNames() {
