d7fc4c24e9277c7a5492764747ba74c28f04ddc0
hongjinsu <kaameo12@gmail.com>
2024-10-28 14:14:38 +0900

    feat(Application): 우승자를 출력하는 기능 추가
    단독 또는 공동 우승자 안내 문구를 출력
    - 단독 우승자일 경우 해당 이름만 출력
    - 공동 우승자가 존재할 경우 쉼표(,)와 공백을 넣어 공동 우승자 이름을 모두 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -19,8 +19,13 @@ public class Application {
         System.out.println("실행 결과");
         for (int i = 0; i < times; i++) {
             carRun(racingCars);
-            print(racingCars);
+            printResult(racingCars);
         }
+
+        // 최종 우승자 출력
+        System.out.print("최종 우승자 : ");
+        List<RacingCar> winners = winners(racingCars);
+        printWinners(winners);
     }
 
     // 사용자로부터 입력을 받아 경주할 자동차 이름 배열 반환
@@ -49,7 +54,7 @@ public class Application {
         return racingCars;
     }
 
-    public static void print(List<RacingCar> racingCars) {
+    public static void printResult(List<RacingCar> racingCars) {
         StringBuilder sb = new StringBuilder();
         for (RacingCar racingCar : racingCars) {
             sb.append(racingCar.getName())
@@ -66,5 +71,38 @@ public class Application {
             sb.append("-");
         }
         return sb.toString();
+    }
+
+    public static List<RacingCar> winners(List<RacingCar> racingCars) {
+        List<RacingCar> winners = new ArrayList<>();
+        int maxProceed = maxProceed(racingCars);
+        for (RacingCar racingCar : racingCars) {
+            if (racingCar.getPosition() == maxProceed) {
+                winners.add(racingCar);
+            }
+        }
+        return winners;
+    }
+
+    public static int maxProceed(List<RacingCar> racingCars) {
+        int maxProceed = 0;
+        for (RacingCar racingCar : racingCars) {
+            if (maxProceed < racingCar.getPosition()) {
+                maxProceed = racingCar.getPosition();
+            }
+        }
+        return maxProceed;
+    }
+
+    public static void printWinners(List<RacingCar> winners) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < winners.size(); i++) {
+            sb.append(winners.get(i).getName());
+            // 우승자의 마지막이 아닌경우 쉼표를 추가
+            if (i != winners.size() - 1) {
+                sb.append(", ");
+            }
+        }
+        System.out.println(sb);
     }
 }
