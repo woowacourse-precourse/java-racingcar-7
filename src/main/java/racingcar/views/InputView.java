8e046fcc13e2424036dbf6900976d3c6c0e542c8
sangxxjin <sj_0368@naver.com>
2024-10-28 16:31:43 +0900

    fix: 요구사항의 Console.readLine 사용하게 구현
    본문:
    요구사항대로 Scanner를 사용하는게 아닌 제공받은 Console을 사용하게 구현했습니다.

--- src/main/java/racingcar/views/InputView.java
+++ src/main/java/racingcar/views/InputView.java
@@ -1,19 +1,15 @@
 package racingcar.views;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.Scanner;
 
 public class InputView {
 
     private static final String INPUT_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_COUNT_PROMPT = "시도할 횟수는 몇 회인가요?";
-    private static Scanner sc = new Scanner(System.in);
 
-    public static String readStringInput() {
-        return sc.nextLine();
-    }
-
-    public static int readNumberInput() {
-        return sc.nextInt();
+    public static String readInput() {
+        return Console.readLine();
     }
 
     public static void displayInputPrompt() {
