ab30d37222f31a3b4d62464a01d39a6b20f6b5b5
hakSick <koukai@kakao.com>
2024-10-28 22:41:31 +0900

    test: 테스트 케이스 작성
    구분자에 ","가 아닌 것이 섞인 경우, 이름 문자열에 공백이 섞인 경우, 우승자가 여러명인 경우, 자동차의 이름이 중복되는 경우의 테스트 케이스 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,16 +1,52 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+
+    @Test
+    void 쉼표가_아닌_다른_구분자_포함_경우() {
+        String input = "pobi,woni.jun";
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validator.InputStringValidator(input);
+        });
+    }
+
+    @Test
+    void 이름_문자열_공백이_섞인_경우() {
+        String input = "pobi,,jun";
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validator.InputStringValidator(input);
+        });
+    }
+
+    @Test
+    void 우승자_여러명() {
+        Cars.makeCarslist(List.of("pobi", "woni", "jun"));
+        Cars.cars.get(0).count = 3;
+        Cars.cars.get(1).count = 3;
+        Cars.cars.get(2).count = 2;
+
+        Cars.printWinner();
+    }
+
+    @Test
+    void 자동차_이름_중복() {
+        String input = "pobi,pobi,jun";
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validator.InputStringValidator(input);
+        });
+    }
 
     @Test
     void 기능_테스트() {
