1a5e78f5ccdddf2f3827e4ba7a0f25306668af63
belight2 <chlwnsgur090@gmail.com>
2024-10-28 15:46:08 +0900

    feat: InputManager class 구현
    - inputCars(): 자동차 이름을 쉼표(,)를 기준으로 구분한 문자열을
    입력받아 반환한다.
    
    - inputMoveCount() : 몇 번의 이동을 시도할 것인지를 입력받아 반
    환한다.
    
    InputValidator 구현 후 유효성 검사 후 반환하도록 구현할 예정임

--- src/main/java/manager/InputManager.java
+++ src/main/java/manager/InputManager.java
@@ -1,5 +1,10 @@
 package manager;
 
+import camp.nextstep.edu.missionutils.Console;
+import car.Car;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import validation.InputValidator;
 
 public class InputManager {
@@ -8,4 +13,17 @@ public class InputManager {
         this.inputValidator = inputValidator;
     }
 
+    public List<Car> inputCars(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String cars = Console.readLine();
+        return Arrays.stream(cars.split(","))
+                .map(String::trim)
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public Integer inputMoveCount(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
 }
