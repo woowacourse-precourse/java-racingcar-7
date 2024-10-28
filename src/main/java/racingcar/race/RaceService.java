8efc9e5d568bfc3458efc891a3621d37188f0afb
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-10-28 10:50:56 +0900

    feat: 무작위 숫자 생성 기능 구현
    무작위로 숫자 생성하는 기능을 구현했습니다.

--- src/main/java/racingcar/race/RaceService.java
+++ src/main/java/racingcar/race/RaceService.java
@@ -1,12 +1,14 @@
 package racingcar.race;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import racingcar.car.Car;
 
 public class RaceService {
 
     public int createRandomNumber() {
-        return 0; // 생성한 무작위 숫자 반환
+        // 생성한 무작위 숫자 반환
+        return Randoms.pickNumberInRange(0, 9);
     }
 
     public boolean isMove(/*int randomNumber*/) {
