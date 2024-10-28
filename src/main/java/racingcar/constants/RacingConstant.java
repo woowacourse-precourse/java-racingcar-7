eca050f4db5505e8989efd2a77955d2d0b724ac5
KKdohyeong <dohyeong254@naver.com>
2024-10-28 18:48:43 +0900

    feat(RacingConstant): 경주 게임 관련 상수 설정
    경주 게임에서 값이 변할 수 있는 상수들을 관리하는 클래스

--- src/main/java/racingcar/constants/RacingConstant.java
+++ src/main/java/racingcar/constants/RacingConstant.java
@@ -1,6 +1,9 @@
 package racingcar.constants;
 
 public class RacingConstant {
+    public static final String CAR_MOVE_CHARACTER = "-";
+
+    public static final String CAR_NAME_PARSING_DELIMETER = ",";
 
     public static final int RANDOM_MIN_NUMBER = 0;
     public static final int RANDOM_MAX_NUMBER = 9;
