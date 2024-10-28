477caea418e651a0ffd5c72fe108584f4a4406ca
scc9811 <wjsrudtjr98@naver.com>
2024-10-28 18:06:54 +0900

    test(CarNameValidatorTest) : 자동차 이름 검증 테스트 기능 추가

---
+++ src/test/java/racingcar/validation/CarNameValidatorTest.java
@@ -0,0 +1,24 @@
+package racingcar.validation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class CarNameValidatorTest {
+    @Test
+    void 자동차_이름_6자_이상일_경우_예외() {
+        List<String> carNames = List.of("pobi", "jun", "GyeongSoek");
+        assertThatThrownBy(() -> CarNameValidator.validateCarNameLength(carNames))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 자동차_이름_5자_이하일_경우_정상() {
+        List<String> carNames = List.of("pobi", "jun", "seok");
+        assertThatCode(() -> CarNameValidator.validateCarNameLength(carNames))
+                .doesNotThrowAnyException();
+    }
+}
