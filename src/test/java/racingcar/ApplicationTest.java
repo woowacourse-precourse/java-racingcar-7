3115e8e1560ea94427874352dabb43e31ab4106d
sangxxjin <sj_0368@naver.com>
2024-10-28 16:40:28 +0900

    test: 자동차 이름 공백 방지 테스트 구현
    본문:
    자동차의 이름을 공백으로 입력하였을 경우 예외처리하는 테스트 구현했습니다.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 공백_이름_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
