3c22f8874fdf69721bb2a601b04263b7facab0bd
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-10-28 14:54:39 +0900

    feat: 개별 자동차 컨테이너 정의 및 초기 입력에 대한 split 기능 구현
    - 개별 자동차에 대한 컨테이너를 Map 형태로 선언
    - 초기 입력 문자열에 split 함수를 이용하여 String 배열 저장
    - String 배열 검사, 5 이하일 경우에만 cars 컨테이너에 입력 값 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,16 +1,30 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carsName = Console.readLine();
+        String carsInput = Console.readLine();
         System.out.println("시도할 횟수는 몇 회인가요?");
+        int raceRoundCount;
         try {
-            int raceRoundCount = Integer.parseInt(Console.readLine());
+            raceRoundCount = Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
+        }
+
+        Map<String, Integer> cars = new HashMap<>();
+
+        String[] carsName = carsInput.split(",");
+
+        for (String car : carsName) {
+            if (car.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+            cars.put(car, 0);
         }
     }
 }
