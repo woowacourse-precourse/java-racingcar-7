8cfe22a05768b12787bfdeedcee307ab77bf869b
gichan222 <>
2024-10-28 16:24:01 +0900

    feat(Application): Application에서의 controller 설정
    컨트롤러단 시작 메서드 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.model.RacingService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
+
+    private static final InputView inputView = InputView.getInstance();
+    private static final OutputView outputView = OutputView.getInstance();
+    private static final RacingService RACING_SERVICE = new RacingService();
+    private static final Validator validator = Validator.getInstance();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingController racingController = new RacingController(inputView, outputView, RACING_SERVICE, validator);
+        racingController.start();
     }
 }
