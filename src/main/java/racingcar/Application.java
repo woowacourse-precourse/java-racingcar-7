20d6c58c00b87a15e8189f9cad4c81f68df06121
wouldu <jhw664089@gmail.com>
2024-10-28 10:54:52 +0900

    feat: 우승자 계산 및 출력 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -33,5 +33,21 @@ public class Application {
             }
             System.out.println();
         }
+
+        int maxPosition = 0;
+        for (Car car : cars) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
+            }
+        }
+
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
