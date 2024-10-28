49544170acc87484cec0f8a3495a38c172eaa8cb
Juyeon Kim <airin00100@gmail.com>
2024-10-28 20:04:34 +0900

    feat. 입력 처리 기능 및 예외처리 추가함

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # java-racingcar-precourse
+
+### 1. 자동차 경주 게임의 입력처리 로직 구현
+    ## a. 사용자가 경주할 자동차 이름을 입력할 수 있도록 요청할 수 있다. 입력 형식은 쉼표(,)로 구분된 리스트이다.
+            a-1) Scanner를 사용해 사용자 입력을 받고, 쉼표를 기준으로 문자열을 분할해 자동차 이름 뮨자열 배열 (carNames)에 저장한다.
+    ## b. 각 자동차 이름의 길이를 검사해서 입력받은 문자열이 5글자를 초과하면 IllegalArgumentException을 발생시킨다.
+            b-1) trt - catch 문 사용
+            b-2) finally 에서 Scanner 객체를 해제함
+
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,33 @@
 package racingcar;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        String input = scanner.nextLine();
+
+        try {
+            String[] carNames = splitNames(input);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+
+    static String[] splitNames(String input) {
+        String[] carNames = input.split(",");
+
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자를 넘을 수 없습니다!");
+            }
+        }
+
+        return carNames;
     }
 }
+
