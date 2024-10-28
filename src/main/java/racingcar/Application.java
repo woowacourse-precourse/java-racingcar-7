26c69db898968dea1937b8449e8208d255221d6c
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-10-28 16:13:17 +0900

    feat : 초기 입력 프린트 출력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
 public class Application {
+    private final static String INPUT_PRINT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+    public static void inputCarNamePrint(){
+        System.out.println(INPUT_PRINT);
+    }
+
     }
 }
