0e9f1b95d0298e593d3530e378dca61fe83ed3d9
scc9811 <wjsrudtjr98@naver.com>
2024-10-28 18:34:48 +0900

    test(InputViewTest) : 입력 문자열 처리 로직 테스트 기능 추가

---
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -0,0 +1,24 @@
+package racingcar.view;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class InputViewTest {
+    @Test
+    void 문자열_쉼표_구분하여_분리() {
+        String input = "pobi,jun,seok";
+        List<String> tokens = InputView.splitStringByDelimiters(input, ",");
+        assertThat(tokens).containsExactly("pobi", "jun", "seok");
+    }
+
+    @Test
+    void 공백_제거() {
+        String input = "  pobi,   ju  n  , s e o k   ";
+        String result = InputView.removeSpace(input);
+        assertThat(result).isEqualTo("pobi,jun,seok");
+    }
+}
