ce33ab3223648b7781d44b20c93e08633719f5d4
Seongho Kim <kksshh0612@gmail.com>
2024-10-28 11:32:00 +0900

    test: 랜덤 숫자가 0~9 사이 생성되는지 테스트 작성

--- src/test/java/racingcar/service/RandomNumberGeneratorTest.java
+++ src/test/java/racingcar/service/RandomNumberGeneratorTest.java
@@ -5,20 +5,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class RandomUtilTest {
+class RandomNumberGeneratorTest {
 
-    private RandomUtil randomUtil;
+    private RandomNumberGenerator randomNumberGenerator;
 
     @BeforeEach
     void setUp() {
-        randomUtil = new RandomUtil();
+        randomNumberGenerator = new RandomNumberGenerator();
     }
 
     @DisplayName("랜덤으로 생성되는 값은 0~9 사이이다")
     @Test
     void generateRandomNumber() {
         // given & when
-        int randomNumber = randomUtil.generateRandomNumber();
+        int randomNumber = randomNumberGenerator.generateRandomNumber();
 
         // then
         Assertions.assertThat(randomNumber).isBetween(0, 9);
