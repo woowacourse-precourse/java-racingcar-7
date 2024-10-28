7b1af32161c2e596b358c9a8ed517adb32da722e
sjsk3232 <sjsk3232@gmail.com>
2024-10-28 16:03:23 +0900

    test : 경주 시도 횟수 입력 예외 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -35,10 +36,18 @@ class ApplicationTest extends NsTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"pobi,jun,sjsk32", "pobi,jun, ", ""})
+    @ValueSource(strings = {"pobi,jun,sjsk32", "pobi,jun, ", " "})
     @DisplayName("자동차 이름이 5자를 초과하거나 공백일 경우 예외 발생")
     void invalidCarNameThrowsException(String invalidCarNames) {
         assertThatThrownBy(() -> runException(invalidCarNames, "1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1", "-100", "sdg", " "})
+    @DisplayName("시도 횟수가 자연수가 아닐 경우 예외 발생")
+    void invalidRaceTimeThrowsException(String invalidRaceTime) {
+        assertThatThrownBy(() -> runException("pobi,jun,sjsk", invalidRaceTime))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
