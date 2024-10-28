cc0dbb399bf607b043c9c212b7e7f0b5bcee702e
sanikani <sanikani@naver.com>
2024-10-28 13:42:17 +0900

    feat: 자동차 이동 거리 출력
    - Car 값을 가져오기 위해 getter 정의
    - view에서 model의 의존성을 없애기 위해 name과 position을 controller에서 얻어서 전달

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,6 +1,8 @@
 package racingcar.controller;
 
 import java.util.List;
+import racingcar.model.Car;
+import racingcar.service.RacingCarService;
 import racingcar.util.InputParser;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -9,15 +11,34 @@ public class RacingCarController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final RacingCarService racingCarService;
 
     public RacingCarController() {
         inputView = new InputView();
         outputView = new OutputView();
+        racingCarService = new RacingCarService();
     }
 
     public void run() {
         List<String> carNameList = getCarNames();
         int tryCount = getTryCount();
+        List<Car> carList = racingCarService.initializeCars(carNameList);
+
+        outputView.printResult();
+        executeRace(carList, tryCount);
+    private void executeRace(List<Car> carList, int tryCount) {
+        for (int i = 0; i < tryCount; i++) {
+            racingCarService.moveCars(carList);
+
+            printAllCarPosition(carList);
+        }
+    }
+
+    private void printAllCarPosition(List<Car> carList) {
+        for (Car car : carList) {
+            outputView.printCarPosition(car.getName(),car.getPosition());
+        }
+        outputView.printEnter();
     }
 
     public List<String> getCarNames() {
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,6 +4,13 @@ public class OutputView {
 
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String TRY_COUNT_INPUT_MESSAGE = "시도할 회수는 몇 회인가요?";
+    private static final String RESULT_MESSAGE = "실행 결과";
+    public static final String NAME_POSITION_SEPARATOR = " : ";
+    private static final String POSITION_MARKER = "-";
+    public static final String WINNER_DELIMITER = ", ";
+    public static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
+
+
 
     public void printCarNameInputMessage() {
         System.out.println(CAR_NAME_INPUT_MESSAGE);
@@ -11,5 +18,22 @@ public class OutputView {
 
     public void printTryCountInputMessage() {
         System.out.println(TRY_COUNT_INPUT_MESSAGE);
+    }
+
+    public void printResult() {
+        System.out.println();
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public void printCarPosition(String name, int position) {
+        System.out.print(name + NAME_POSITION_SEPARATOR);
+        for (int i = 0; i < position; i++) {
+            System.out.print(POSITION_MARKER);
+        }
+        System.out.println();
+    }
+
+    public void printEnter() {
+        System.out.println();
     }
 }
