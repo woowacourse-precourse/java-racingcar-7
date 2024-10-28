8c90330844c1c50b823a6bec5f5bb4d9ff412ed4
lee-haeseung <lhser123@cau.ac.kr>
2024-10-28 21:24:12 +0900

    feat: 입력시 자동차 객체 생성
    model 구현시 구현한 예외 처리로 인해 해결된 예외 사항 기록

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,18 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNameStrings = Console.readLine();
+
+        // model 생성
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNameStrings.split(",")) {
+            cars.add(new Car(carName));
+        }
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String playTimeString = Console.readLine();
