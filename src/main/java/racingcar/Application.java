b9de3b0312d41408d34603ba03bcfa3ca77bf8c7
bongj9 <lollypark501@naver.com>
2024-10-28 18:25:55 +0900

    refactor : Line formating &  출력 구문 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,7 +10,7 @@ public class Application {
         // TODO: 프로그램 구현
 
         try {
-            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             String[] carNames = Console.readLine().split(",");
             List<Car> cars = new ArrayList<>();
             for (String name : carNames) {
@@ -22,55 +22,57 @@ public class Application {
             }
             System.out.println("시도할 횟수는 몇 회인가요?");
             int trial = Integer.parseInt(Console.readLine());
-            if(trial < 0) {
-                throw new IllegalArgumentException("시도 횟수는 1회 이상이어야 합니다.")
+            if (trial < 0) {
+                throw new IllegalArgumentException("시도 횟수는 1회 이상이어야 합니다.");
             }
             System.out.println("실행 결과");
 
             //게임 실행
-            for (int i = 0; i <trial; i++) {
+            for (int i = 0; i < trial; i++) {
                 playOneRound(cars);
                 playResult(cars);
             }
             winner(cars);
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println("잘못된 입력입니다: " + e.getMessage());
         }
-        //매판 결과값
-        private static void playResult(List<Car> cars){
-            for (Car car : cars) {
-                System.out.println(car.getName() + " : " + car.exchangeDistance());
-            }
-            System.out.println();
-        }
+    }
 
-        //전진 멈추는 방법(4이상일때만)
-        private static void playOneRound(List<Car> cars) {
-            for (int i = 0; i < trial; i++) {
-                for (Car car : cars) {
-                    if (Randoms.pickNumberInRange(0, 9) >= 4) {
-                          car.go();
-                    }
-                }
-            }
+    //매판 결과값
+    private static void playResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + car.exchangeDistance());
         }
-        //우승자를 뽑는 방식
-        private static void winner(List<Car> cars){
-            int maxDistatnce = 0;
-            for (Car car : cars) {
-                if (car.getDistance() >= maxDistatnce) {
-                    maxDistatnce = car.getDistance();
-                }
+        System.out.println();
+    }
+
+    //전진 멈추는 방법(4이상일때만)
+    private static void playOneRound(List<Car> cars) {
+
+        for (Car car : cars) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                car.go();
             }
-            List<String> winners = new ArrayList<>();
-            for (Car car : cars) {
-                if (car.getDistance() == maxDistatnce) {
-                    winners.add(car.getName());
-                }
-            }
-            //우승자 출력
-            System.out.println("최종 우승자 : " + String.join(", ", winners));
         }
 
     }
+
+    //우승자를 뽑는 방식
+    private static void winner(List<Car> cars) {
+        int maxDistatnce = 0;
+        for (Car car : cars) {
+            if (car.getDistance() >= maxDistatnce) {
+                maxDistatnce = car.getDistance();
+            }
+        }
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getDistance() == maxDistatnce) {
+                winners.add(car.getName());
+            }
+        }
+        //우승자 출력
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+
 }
