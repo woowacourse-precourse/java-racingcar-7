d0ef451ea7117920ace00f60a3bcb6c81763264d
threeony <threeknot1250@cau.ac.kr>
2024-10-28 16:12:45 +0900

    feat: update class 'CarManager'
    * add 'getWinners' method

--- src/main/java/racingcar/CarManager.java
+++ src/main/java/racingcar/CarManager.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
@@ -9,33 +10,46 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 public class CarManager {
     private final List<Car> cars = new ArrayList<>();
 
-    public void addCar(String name, int advance){
+    public void addCar(String name, int advance) {
         Car car = new Car(name, advance);
         cars.add(car);
     }
 
-    public Car findCar(String name){
+    public Car findCar(String name) {
         return cars.stream()
                 .filter(car1 -> car1.getName().equals(name))
                 .findAny()
                 .orElse(null);
     }
 
-    public void moveCars(List<String> participants){
-        for(String name: participants){
+    public void moveCars(List<String> participants) {
+        for (String name : participants) {
             Car car = findCar(name);
 
             int randomNo = pickNumberInRange(0, 9);
-            if(randomNo >= 4) car.move();
+            if (randomNo >= 4) car.move();
         }
     }
 
-    public void printState(List<String> participants){
-        for(String name: participants){
+    public void printState(List<String> participants) {
+        for (String name : participants) {
             Car car = findCar(name);
 
             System.out.print(car.getName() + " : ");
             IntStream.range(0, car.getAdvance()).mapToObj(i -> "-").forEach(System.out::print);
+            System.out.println();
         }
+    }
+
+    public List<String> getWinners() {
+        int maxAdvance = cars.stream()
+                .mapToInt(Car::getAdvance)
+                .max()
+                .orElseThrow();
+
+        return cars.stream()
+                .filter(car -> car.getAdvance() == maxAdvance)
+                .map(Car::getName)
+                .collect(Collectors.toList());
     }
 }
