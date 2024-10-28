04ddaad22f5550f425afdddbce1f5e440f2ad026
junsoo0719 <junsoo0719@gmail.com>
2024-10-28 15:11:43 +0900

    feat: 자동차 이름 예외 기능 추가 빈 문자열/ 이름이 빈 값/ 이름의 길이가 5를 초과한 경우

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -6,11 +6,25 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Input {
-    public List<String> inputCarName() {
+    public static List<String> inputCarName() {
         String input = Console.readLine();
-
+        checkInput(input);
         List<String> carList = Arrays.asList(input.split(","));
-
+        checkName(carList);
         return carList;
+    }
+
+    private static void checkInput(String input) {
+        if(input == null || input.trim().isEmpty())
+            throw new IllegalArgumentException("빈 문자열입니다.");
+    }
+
+    private static void checkName(List<String> carList) {
+        for(String carName : carList) {
+            if(carName == null || carName.trim().isEmpty())
+                throw new IllegalArgumentException("이름이 빈 값입니다.");
+            if(carName.length() > 5)
+                throw new IllegalArgumentException("각 이름은 5자 이하여야 합니다.: " + carName);
+        }
     }
 }
