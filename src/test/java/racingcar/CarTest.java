d6da63d4a04b4d80654d49bda062764e167cb1d4
persi <jina8151010@gmai.com>
2024-10-28 15:56:52 +0900

    test: 랜덤 숫자에 따른 자동차 전진 여부 테스트 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -13,4 +13,15 @@ public class CarTest {
         assertEquals("pobi", car.getName());
         assertEquals(0, car.getPosition());
     }
+
+    @Test
+    void 랜덤_숫자에_따른_자동차_전진_여부_확인() {
+        Car car = new Car("pobi");
+
+        car.moveIfPossible(4);
+        assertEquals(1, car.getPosition(), "랜덤 숫자가 4 이상일 때 전진해야 합니다.");
+
+        car.moveIfPossible(3);
+        assertEquals(1, car.getPosition(), "랜덤 숫자가 3 이하일 때 전진하지 않아야 합니다.");
+    }
 }
