17737123ef5381126454f88ed695a64d8da8b3a9
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-10-28 22:58:09 +0900

    refactor: 안내 메시지 & 문자 기호 모두 문자열 상수로 변경

--- src/main/java/racingcar/io/Output.java
+++ src/main/java/racingcar/io/Output.java
@@ -4,13 +4,19 @@ import java.util.List;
 import racingcar.car.Car;
 
 public class Output {
+    private final static String RESULT_MESSAGE = "\n실행 결과";
+    private final static String COLON = " : ";
+    private final static String CHAMPION_MESSAGE = "최종 우승자";
+    private final static String JOIN_DELIMITER = ", ";
+    private final static String MOVE_SYMBOL = "-";
+
     public void printResult() {
-        System.out.println("\n실행 결과");
+        System.out.println(RESULT_MESSAGE);
     }
 
     public void printCar(List<Car> cars) {
         for (Car car : cars) {
-            System.out.print(car.getName() + " : ");
+            System.out.print(car.getName() + COLON);
             printMoveByOrder(car);
         }
         System.out.println();
@@ -18,12 +24,12 @@ public class Output {
 
     public void printMoveByOrder(Car car) {
         for (int i = 0; i < car.getTotalMove(); i++) {
-            System.out.print("-");
+            System.out.print(MOVE_SYMBOL);
         }
         System.out.println();
     }
 
     public void printChampion(List<String> champions) {
-        System.out.println("최종 우승자 : " + String.join(", ", champions));
+        System.out.println(CHAMPION_MESSAGE + COLON + String.join(JOIN_DELIMITER, champions));
     }
 }
