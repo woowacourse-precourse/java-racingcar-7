aefed87c8493df279e0f7170656f480396bcf8f0
Si Yeong Park <psy915@swu.ac.kr>
2024-10-28 19:38:33 +0900

    feat(InputView): 사용자로부터 입력받는 뷰 기능

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+public class InputView {
+    public static String inputNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return readLine();
+    }
+    public static int inputGameCnt() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(readLine());
+    }
+}
