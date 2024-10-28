58cd250fc9f001948285403a3e365d86abc55d21
전민정 <minj_1203@naver.com>
2024-10-28 19:37:46 +0900

    feat: 입력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.*;
+
 public class Application {
+    public static List<String> cars;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+
+    public static void input(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        cars = Arrays.asList(input.split(","));
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int N = Integer.parseInt(Console.readLine());
     }
 }
