2a9ac10f9dacae235db7e3bd530e87d7d4f9b352
JEN <ekffuclxk@gmail.com>
2024-10-28 22:04:27 +0900

    test(validatetest): 시도할 숫자 입력 테스트 추가

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
@@ -21,4 +23,22 @@ class InputTest {
         int gameCount = input.convertStrToInt("5");
         assertThat(gameCount).isEqualTo(5);
     }
+
+    @Test
+    void 숫자_형식의_시도_횟수_검사() {
+        assertThatCode(() -> Validate.validateGameCountIsNumber("56"))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 문자_형식의_시도_횟수_검사() {
+        assertThatThrownBy(() -> Validate.validateGameCountIsNumber("5a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시도할 횟수는 숫자여야 합니다.");
+
+        assertThatThrownBy(() -> Validate.validateGameCountIsNumber("abc"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시도할 횟수는 숫자여야 합니다.");
+    }
+
 }
