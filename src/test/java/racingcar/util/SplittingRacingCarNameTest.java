2a0e69837880977406d3b61ae4d6d27975172c18
young-in <hardworkingini@gmail.com>
2024-10-28 20:44:36 +0900

    test : 상수를 사용해서 더미데이터 변경이 쉽도록 리팩토링

--- src/test/java/racingcar/util/SplittingRacingCarNameTest.java
+++ src/test/java/racingcar/util/SplittingRacingCarNameTest.java
@@ -5,16 +5,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class SplittingRacingCarNameTest {
+    private static final String DUMMY_RACING_CAR_NAME = "ini,yoni,nini,bye";
+    private static final int DUMMY_RACING_CAR_COUNT = 4;
+    private static final String[] DUMMY_RACING_CAR_NAMES = {"ini", "yoni", "nini", "bye"};
+
     @DisplayName("자동차는 쉼표(,)를 기준으로 구분하여 생성된다.")
     @Test
     void splitRacingCarName() throws Exception{
-        //given
-        String playerInput = "ini,yoni,nini,bye";
-        SplittingRacingCarName splittingRacingCarName = new SplittingRacingCarName();
-        String[] strings = splittingRacingCarName.splitRacingCarName(playerInput);
-        //when //then
-        assertThat(strings).hasSize(4)
-                .containsExactly(new String[]{"ini","yoni","nini","bye"});
+        // given
+        String playerInput = DUMMY_RACING_CAR_NAME;
+
+        // when
+        String[] result = SplittingRacingCarName.splitRacingCarName(playerInput);
+
+        // then
+        assertThat(result).hasSize(DUMMY_RACING_CAR_COUNT)
+                .containsExactly(DUMMY_RACING_CAR_NAMES);
     }
 
 }
