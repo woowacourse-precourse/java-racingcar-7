707819fce5bcd3a798233f3b4aa8452b98d10d0e
LouiIII3 <jikdsunghuilee@naver.com>
2024-10-28 19:52:19 +0900

    feat: 자동차 이름 입력 및 유효성 검사 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String[] carNames = inputCarNames();
+    }
+
+    private static String[] inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        validateCarNames(input);
+        return input.split(",");
+    }
+
+    private static void validateCarNames(String input) {
+        String[] names = input.split(",");
+        for (String name : names) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자를 넘을 수 없습니다.");
+            }
+        }
     }
 }
