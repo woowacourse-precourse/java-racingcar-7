5209275dbedcd8c973bcc976a6bef68edd386932
BusanGukbap <dong2857210@gmail.com>
2024-10-28 13:15:33 +0900

    feat(Applicatoin) : add controller

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,12 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        try {
+            RacingGameController controller = new RacingGameController();
+            controller.startGame();
+        } catch() {
+        }
+
     }
 }
