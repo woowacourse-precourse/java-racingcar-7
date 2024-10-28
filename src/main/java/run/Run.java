226983270999752aeffc807195fde950fbcf9c08
Cmk1031 <maple1031@naver.com>
2024-10-28 16:53:43 +0900

    feat: 자동차 이름 문자열 입력

--- src/main/java/run/Run.java
+++ src/main/java/run/Run.java
@@ -3,9 +3,44 @@ package run;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.Car;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Run {
     public void start() {
         List<Car> racingCar = new ArrayList<>();
+        getCar(racingCar);
+    }
+
+    private void getCar(List<Car> racingCar) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String names = Console.readLine();
+        String[] splitCarName = getCarName(names);
+
+        for (String carName : splitCarName) {
+            racingCar.add(new Car(carName,0));
+        }
+    }
+
+    private String[] getCarName(String names) {
+        String[] carNames = names.split(",");
+        if (carNames.length == 0) {
+            throw new IllegalStateException();
+        }
+        for(String carName : carNames) {
+            if(carName.length() > 5 || carName.isBlank()) {
+                throw new IllegalStateException();
+            }
+            isDigit(carName);
+        }
+        return carNames;
+    }
+
+    private void isDigit(String carName) {
+        for (int i = 0; i < carName.length(); i++) {
+            char c = carName.charAt(i);
+            if (c < 65 || (c > 90 && c < 97) || c > 122) {
+                throw new IllegalStateException();
+            }
+        }
     }
 }
