dc1fe97f940518d3fd225c042b44a5801970ad04
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-10-28 19:49:18 +0900

    chore : 테스트코드 파라미터 네이밍 변경

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -10,9 +10,9 @@ public class CarTest {
     @ParameterizedTest
     @ValueSource(ints = {4, 9})
     @DisplayName("자동차가 움직일 수 있는 조건을 만족할 때 움직이는지 확인한다")
-    void 움직일_수_있는_경우_테스트(int i) {
+    void 움직일_수_있는_경우_테스트(int testCase) {
         Car car = new Car("pobi");
-        NumberGenerator numberGenerator = () -> i;
+        NumberGenerator numberGenerator = () -> testCase;
 
         car.move(numberGenerator.generateNumber());
 
@@ -22,9 +22,9 @@ public class CarTest {
     @ParameterizedTest
     @ValueSource(ints = {0, 3})
     @DisplayName("자동차가 움직일 수 있는 조건을 만족하지 않을 때 움직이는지 확인한다")
-    void 움직일_수_없는_경우_테스트(int i) {
+    void 움직일_수_없는_경우_테스트(int testCase) {
         Car car = new Car("pobi");
-        NumberGenerator numberGenerator = () -> i;
+        NumberGenerator numberGenerator = () -> testCase;
 
         car.move(numberGenerator.generateNumber());
 
@@ -42,8 +42,8 @@ public class CarTest {
     @ParameterizedTest
     @ValueSource(strings = {"", " ","    "," pobi"})
     @DisplayName("자동차 이름이 공백문자열을 포함하는 경우")
-    void 자동차_이름_공백_테스트(String input) {
-        Assertions.assertThatThrownBy(() -> new Car(input))
+    void 자동차_이름_공백_테스트(String testCase) {
+        Assertions.assertThatThrownBy(() -> new Car(testCase))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 공백문자열을 포함할 수 없습니다");
     }
