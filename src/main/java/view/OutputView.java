b3a4a816ce3bf95c7429e4ff93ce8aafaac271e9
sseopsseop <ponde119@outlook.com>
2024-10-28 17:41:48 +0900

    feat: 출력 기능 클래스 추가 출력할 문자열을 받으면 양식에 맞게 출력함

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,16 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
+import util.Constants;
+
 public class OutputView {
+    public static void outputWindow(String[] results){
+        for(String result : results){
+            printOutputString(result + "\n");
+        }
+    }
+
+    private static void printOutputString(String prompt) {
+        System.out.println(prompt);
+    }
 }
