8d9380aaabf5cf5dfcc70cd06ecac4fb198640f3
rbgusdlza <rbgusdlza@naver.com>
2024-10-28 22:07:46 +0900

    feat: 우승자 이름을 담는 메서드 구현

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
 
@@ -17,7 +18,18 @@ public class Cars {
 
     public void showAll() {
         carList.forEach(Car::show);
+        System.out.println();
     }
 
+    public List<String> getWinners() {
+        int maxDistance = carList.stream()
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElse(0);
 
+        return carList.stream()
+                .filter(car -> car.getDistance() == maxDistance)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
 }
