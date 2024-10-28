d8b566422e472334893892c7a7a274015f44f6be
AbsoluteValues <heejun1708@gmail.com>
2024-10-28 22:27:33 +0900

    refactor: separate run logic

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -16,21 +16,31 @@ public class RacingcarController {
     }
 
     public void run() {
+        List<String> carNames = carNameRun();
+        int trialCount = trialCountRun();
+        List<Car> cars = racingcarService.createCar(carNames);
+        printResultRun(trialCount, cars);
+        racingcarView.printWinners(cars);
+    }
+    
+    public List<String> carNameRun() {
         String rawCarName = racingcarView.carNameInput();
         List<String> parsedCarName = racingcarService.parseCarName(rawCarName);
         racingcarService.validateCarName(parsedCarName);
+        return parsedCarName;
+    }
 
+    public int trialCountRun() {
         String trialCount = racingcarView.trialCountInput();
         racingcarService.validateNumber(trialCount);
+        return Integer.parseInt(trialCount);
+    }
 
-        List<Car> cars = racingcarService.createCar(parsedCarName);
-
+    public void printResultRun(int trialCount, List<Car> cars) {
         System.out.println("실행결과");
-        for (int i = 1; i <= Integer.parseInt(trialCount); i++) {
+        for (int i = 1; i <= trialCount; i++) {
             List<Car> result = racingcarService.race(cars);
             racingcarView.printResult(result);
         }
-
-        racingcarView.printWinners(cars);
     }
 }
