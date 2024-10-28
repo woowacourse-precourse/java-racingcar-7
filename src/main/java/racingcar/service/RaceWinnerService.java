a7ddc04c706a66c58d8a8b6b145f48dbee6dbac2
kaswhy <sxshx818@naver.com>
2024-10-28 21:10:18 +0900

    refactor(RaceWinnerService): 최종 우승자 목록 구하는 부분 메서드 분리
    변경 전
    - findLastWinner()에서 최종 우승자 이름을 담는 List<String> lastWinners 구하는 부분 직접 구현
    
    변경 후
    - 메소드 길이가 길어 최종 우승자 이름을 담는 목록 반환하는 부분을 메소드로 분리

--- src/main/java/racingcar/service/RaceWinnerService.java
+++ src/main/java/racingcar/service/RaceWinnerService.java
@@ -14,24 +14,25 @@ public class RaceWinnerService {
 
     public static void findLastWinner(CarScores carScores) {
         int max = INIT_MAX_TO_ZERO;
-
         for (Map.Entry<CarName, Distance> nowCarScore : carScores.getCarScores().entrySet()) {
             Distance nowDistance = nowCarScore.getValue();
             max = getNewMax(max, nowDistance.getDistance());
         }
 
-        LinkedHashMap<CarName, Distance> nowCarScores = carScores.getCarScores();
-        Distance maxDistance = new Distance(max);
-        List<String> lastWinners = nowCarScores.entrySet().stream()
-                .filter(entry -> entry.getValue().equals(maxDistance))
-                .map(Map.Entry::getKey)
-                .map(CarName::getCarName)
-                .collect(Collectors.toList());
+        List<String> lastWinners = getLastWinners(carScores.getCarScores(), new Distance(max));
 
         OutputView.printFinalWinner(lastWinners);
     }
 
     private static int getNewMax(int max, int nowScore) {
         return Math.max(nowScore, max);
+    }
+
+    private static List<String> getLastWinners(LinkedHashMap<CarName, Distance> nowCarScores, Distance maxDistance) {
+        return nowCarScores.entrySet().stream()
+                .filter(entry -> entry.getValue().equals(maxDistance))
+                .map(Map.Entry::getKey)
+                .map(CarName::getCarName)
+                .collect(Collectors.toList());
     }
 }
