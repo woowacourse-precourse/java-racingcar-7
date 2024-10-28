18b7235b59455caa4ad9d8241ec384bf84bb0d35
Silbet <65993038+Silbet@users.noreply.github.com>
2024-10-28 18:49:04 +0900

    Test: 테스트 코드 추가
    기능의 정확성을 위한 기능 테스트 1개와 예외테스트 5개를 추가하였다.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -15,19 +16,85 @@ class ApplicationTest extends NsTest {
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @DisplayName("참가 자동차가 1대인 경우")
+    @Test
+    void 기능_테스트_자동차1개() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("JB", "1");
+                    assertThat(output()).contains("JB : -", "최종 우승자 : JB");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("자동차 이름이 공백 일 때")
+    @Test
+    void 예외_테스트_공백() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("자동차 이름의 쉼표 사이에 공백이 있을 때")
+    @Test
+    void 예외_테스트_쉼표사이공백() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("A, ,B", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("자동차 이름의 쉼표가 2개 연속으로 있을 때")
+    @Test
+    void 예외_테스트_연속쉼표() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("A,,B", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("경주 횟수 입력이 양이 아닌 정수 일 때")
+    @Test
+    void 예외_테스트_양이아닌정수() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi, woni", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi, woni", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("경주 횟수 입력이 정수형이 아닐 때")
+    @Test
+    void 예외_테스트_not정수() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi, woni", "three"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi, woni", "3.5"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
