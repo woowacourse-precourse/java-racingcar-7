c32105faeb2eb3c7b27a3782a2cbfab0a4ea72fe
이건희 <dnzp74@g.eulji.ac.kr>
2024-10-28 22:30:49 +0900

    feat(ResultView) : 최종 우승자 출력 기능 구현
    - 최종 우승자 이름을 쉼표로 구분하여 일관된 형식으로 출력

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -5,6 +5,10 @@ import racingcar.model.Car;
 
 public class ResultView {
 
+    public static void printWinners(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+
     public static void printRoundResults(List<Car> cars) {
         for (Car car : cars) {
             System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
