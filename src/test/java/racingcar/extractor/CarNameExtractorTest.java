e8a89927cca3fb639d02b1f846b10b802cac0ff4
username0w <grandcias123@gmail.com>
2024-10-28 22:48:10 +0900

    chore: import 문 정리

--- src/test/java/racingcar/extractor/CarNameExtractorTest.java
+++ src/test/java/racingcar/extractor/CarNameExtractorTest.java
@@ -1,12 +1,8 @@
 package racingcar.extractor;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CarNameExtractorTest {
