4fc0d8393eef4d3b1b6cb866f09880f23bbf7ce3
SinnoLn <sintmtn69@gmail.com>
2024-10-28 13:30:41 +0900

    feat(app): 애플리케이션 시작점 설정 및 게임 실행 로직 초기화
    - Application 클래스에 main 메서드 추가로 애플리케이션 실행 시작점 설정
    - RaceService와 GameController 인스턴스를 생성하고 게임 시작 메서드 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.service.RaceService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RaceService raceService = new RaceService();
+        GameController gameController = new GameController(raceService);
+        gameController.start();
     }
 }
