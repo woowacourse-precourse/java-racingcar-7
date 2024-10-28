0c8832fc98e655a7c3a59d989197f5b1c5a56f05
choiyoung69 <choiyoung609@gmail.com>
2024-10-28 09:16:21 +0900

    feat(RacingGame): 레이싱 게임에 대한 내용을 저장할 객체 생성

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,1 +1,19 @@
+package racingcar.domain;
 
+public class RacingGame {
+    private int[] distances;
+
+    public RacingGame(int size) {
+        distances = new int[size];
+    }
+
+    public void startGame(Car[] cars) {
+        for(int i = 0; i < cars.length; i++){
+            distances[i] = cars[i].move();
+        }
+    }
+
+    public int[] getDistances() {
+        return distances;
+    }
+}
