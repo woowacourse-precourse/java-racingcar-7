8d61faf8db7023e47715ab431984461bbe6a369b
caboodles <sanghyup2000@gmail.com>
2024-10-28 18:39:14 +0900

    feat: Get and parse user inputs
    InputView 클라스가 유저의 입력을 받고 GameFlow 클라스로 전달
    GameFlow 클라스가 유저의 입력을 해석하고 처리하여 저장

--- src/main/java/racingcar/controller/GameFlow.java
+++ src/main/java/racingcar/controller/GameFlow.java
@@ -1,14 +1,46 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+import java.util.Arrays;
+import java.util.List;
+
 public class GameFlow {
+    private int numberOfMoves;
 
     public GameFlow() {}
 
     public void start() {
-        createCars();
-        inputNumberOfSteps();
-        runRace();
-        displayWinners();
+        List<String> carNames = parseCarNames();
+        // createCars(carNames);
+        setNumberOfMoves();
+        // runRace();
+        // displayWinners();
     }
+
+    private List<String> parseCarNames() {
+        String carNameInput = InputView.inputCarNames();
+        List<String> carNames = Arrays.asList(carNameInput.split(","));
+        return carNames;
+    }
+
+    private void setNumberOfMoves() {
+        String NumberInput = InputView.inputNumberOfMoves();
+        validateNumberOfMoves(NumberInput);
+        numberOfMoves = Integer.parseInt(NumberInput);
+    }
+    
+    private void validateNumberOfMoves(String NumberInput) {
+        try {
+            int NumberOfMoves = Integer.parseInt(NumberInput);
+            if (NumberOfMoves <= 0) {
+                throw new IllegalArgumentException("이동 횟수는 1 이상의 양수여야 합니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("이동 횟수는 아라비아 숫자여야 합니다.");
+        }
+    }
+    
+    
+
 
 }
