960a66689b315df920a7513c0dfd1cf2089006e7
Goohong Chung <9rrong@uoslife.team>
2024-10-28 23:35:54 +0900

    refactor(RandomValueGenerator): 최댓값, 최솟값 상수화

--- src/main/java/racingcar/model/valuegenerator/RandomValueGenerator.java
+++ src/main/java/racingcar/model/valuegenerator/RandomValueGenerator.java
@@ -4,8 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomValueGenerator implements ValueGenerator {
 
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 9;
+
     @Override
     public int getValue() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE);
     }
 }
