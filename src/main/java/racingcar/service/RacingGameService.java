deda5e8045963e4e4960eccaab2a9790f3909d86
choiyoung69 <choiyoung609@gmail.com>
2024-10-28 09:52:54 +0900

    feat(RacingGameService):R RacingGames 객체를 이용하도록 수정

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -3,18 +3,20 @@ package racingcar.service;
 import java.util.ArrayList;
 import racingcar.domain.Cars;
 import racingcar.domain.RacingGame;
+import racingcar.domain.RacingGames;
 import racingcar.dto.RacingAttemptDTO;
 import racingcar.dto.RacingCarNamesDTO;
 
 public class RacingGameService {
     public static void runGame(RacingCarNamesDTO racingCarNamesDTO, RacingAttemptDTO racingAttemptDTO){
-        ArrayList<RacingGame> racingGames = new ArrayList<RacingGame>();
+        RacingGames racingGames = new RacingGames();
         Cars cars = new Cars(racingCarNamesDTO);
         int attempts = racingAttemptDTO.getRacingAttempts();
 
         for(int i = 0; i < attempts; i++){
-            racingGames.add(new RacingGame());
-            racingGames.get(i).startGame(cars);
+            RacingGame racingGame = new RacingGame();
+            racingGame.startGame(cars);
+            racingGames.addRacingGame(racingGame);
         }
     }
 }
