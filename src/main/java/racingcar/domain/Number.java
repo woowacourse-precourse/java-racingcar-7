6c5e2a1299e559f94cb77587d572064044be5e22
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-10-28 21:38:36 +0900

    refactor: 매직 넘버 상수로 수정

--- src/main/java/racingcar/domain/Number.java
+++ src/main/java/racingcar/domain/Number.java
@@ -3,9 +3,10 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Number {
+    private static final int MIN_RANDOM_NUMBER_RANGE = 0;
     private static final int MAX_RANDOM_NUMBER_RANGE = 9;
 
     public int getRandomNumber() {
-        return Randoms.pickNumberInRange(0, MAX_RANDOM_NUMBER_RANGE);
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER_RANGE, MAX_RANDOM_NUMBER_RANGE);
     }
 }
