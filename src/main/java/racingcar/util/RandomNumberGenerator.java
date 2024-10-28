02526ab6683fec2746cf162fdc41817bad115d72
jjun522 <tjwowns522@>
2024-10-28 19:29:58 +0900

    feat: RandomNumberGenerator 유틸리티 클래스 추가
    - 0에서 9 사이의 랜덤 숫자를 생성하여 자동차 전진 여부 결정
    - 추후 테스트를 위한 유연한 숫자 생성 로직 구현

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -1,4 +1,11 @@
-package racingcar.Util;
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
+    private RandomNumberGenerator() {}
+
+    public static int generate() {
+        return Randoms.pickNumberInRange(0, 9); // 0과 9 사이의 임의의 값 반환
+    }
 }
