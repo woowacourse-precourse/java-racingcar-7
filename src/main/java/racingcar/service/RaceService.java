e3a9fb5cf939bccccab2a4103f22d0b1dc7288d4
KNU-K <knukang334@gmail.com>
2024-10-28 16:58:57 +0900

    refactor(service): RaceService interface에 javaDoc 추가
    인터페이스의 목적성을 명확하게 명시

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -2,6 +2,20 @@ package racingcar.service;
 
 import racingcar.dto.RaceRequest;
 
+/**
+ * 경주 서비스 인터페이스입니다.
+ * <p>
+ * 이 인터페이스는 경주를 시작하는 기능을 정의합니다.
+ * </p>
+ */
 public interface RaceService {
-    String startRace(RaceRequest request);
+
+    /**
+     * 경주를 시작합니다.
+     *
+     * @param request 경주를 시작하기 위한 요청 객체
+     * @return 경주 시작에 대한 결과 메시지
+     * @throws IllegalArgumentException 요청이 유효하지 않은 경우 발생하는 예외
+     */
+    String startRace(RaceRequest request) throws IllegalArgumentException;
 }
