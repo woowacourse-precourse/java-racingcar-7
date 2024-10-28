d7330473d383278c0e44c81e3c46ac9df46bb6dd
ChrisP <jspark4151@gmail.com>
2024-10-28 19:06:46 +0900

    feat(application): add main application logic

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import javax.swing.text.View;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Viewer viewer = new Viewer();
+        viewer.InputCarNameView();
+
+        String nameInputs = Console.readLine();
+        InputHandler inputHandler = new InputHandler();
+        String[] carNames = inputHandler.InputCarNames(nameInputs);
+
+        viewer.InputAttemptCountView();
+
+        String attemptCountInput = Console.readLine();
+        int rounds = inputHandler.GetRound(attemptCountInput);
+
+        viewer.OutputRaceResult();
+
+        GameManager gameManager = new GameManager(carNames, rounds);
+        gameManager.startRace();
+
     }
 }
