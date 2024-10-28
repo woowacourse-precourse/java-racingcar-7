50df8b2188f4eed5c5215cc4893cbc1f7b452a76
HongSeongJo <164273757+HongSeongJo@users.noreply.github.com>
2024-10-28 09:08:29 +0900

    feat(forward): 난수 조건에 따른 전진 메서드를 생성한다

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,14 +1,30 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class Racing {
     public void run(String[] names, int count) {
-        ArrayList<Car> cars = new ArrayList<>();
+        List<Car> cars = new ArrayList<>();
 
         for (String name : names) {
             Car car = new Car(name);
             cars.add(car);
+        }
+
+        for (int i = 0; i < count; i++) {
+            forwardCondition(cars);
+        }
+    }
+
+    public void forwardCondition(List<Car> cars) {
+        for (Car car : cars) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            if (randomNumber >= 4) {
+                car.incrementMoveCount();
+            }
         }
     }
 }
