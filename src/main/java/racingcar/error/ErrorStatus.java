4cb8fae06350308de08ef133cc25292935d61d75
88dldl <chltnrgml0999@gmail.com>
2024-10-28 09:33:46 +0900

    feat: 입력값이 비어있을시 예외처리

--- src/main/java/racingcar/error/ErrorStatus.java
+++ src/main/java/racingcar/error/ErrorStatus.java
@@ -2,4 +2,7 @@ package racingcar.error;
 
 public class ErrorStatus {
     public static final String OVERSIZE_CAR_NAME = "자동차 이름은 5자 이하로만 가능합니다.";
+    public static final String EMPTY_CAR_NAME = "자동차 이름은 비어있을 수 없습니다.";
+    public static final String EMPTY_ATTEMPTS = "시도 횟수는 비어있을 수 없습니다.";
+
 }
--- src/main/java/racingcar/handler/InputHandler.java
+++ src/main/java/racingcar/handler/InputHandler.java
@@ -1,6 +1,7 @@
 package racingcar.handler;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.error.ErrorStatus;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,10 +11,19 @@ public class InputHandler {
         Map<String, String> inputData = new HashMap<>();
 
         System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        inputData.put("carName", Console.readLine());
+        String carNames = Console.readLine();
+        System.out.println(carNames);
+        if (carNames == null || carNames.isEmpty()) {
+            throw new IllegalArgumentException(ErrorStatus.EMPTY_CAR_NAME);
+        }
+        inputData.put("carName", carNames);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
-        inputData.put("attemptCount", Console.readLine());
+        String attemptCount = Console.readLine();
+        if (attemptCount == null || attemptCount.isEmpty()) {
+            throw new IllegalArgumentException(ErrorStatus.EMPTY_ATTEMPTS);
+        }
+        inputData.put("attemptCount", attemptCount);
 
         return inputData;
     }
