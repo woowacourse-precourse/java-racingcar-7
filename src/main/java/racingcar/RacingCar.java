420cb543a610070242cf3f6c5e0b6a2651100bdd
yhkee0404 <yhkee0404@gmail.com>
2024-10-28 21:08:13 +0900

    feat(racingcar): add RacingCar

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+public class RacingCar {
+    public void move() {
+        mileage++;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    private int mileage;
+}
