3ee2f2609d1fd483f83d5feeb8e5b2758d729025
Park, Sojeong <sojeong2101@gmail.com>
2024-10-28 12:19:45 +0900

    feat($Garage): 자동차 최소 조건 위반과 이름 중복 예외 처리 구현
    1. 자동차 개수가 적어도 2개가 되지 않을 경우 예외 처리
    2. 이름이 중복 될 시 예외 처리

--- src/main/java/racingcar/Garage.java
+++ src/main/java/racingcar/Garage.java
@@ -7,9 +7,13 @@ import java.util.Map;
 
 public class Garage {
 
+    private static final int CAR_COUNT_MIN = 2;
+
     private List<Car> carList;
 
     public Garage(List<String> carNames) {
+        checkMinimumCarCount(carNames);
+        checkDuplicateName(carNames);
         nameCars(carNames);
     }
 
@@ -32,5 +36,18 @@ public class Garage {
         carList = carNames.stream()
                 .map(Car::new)
                 .toList();
+    }
+
+    private void checkMinimumCarCount(List<String> carNames) {
+        if (carNames.size() < CAR_COUNT_MIN) {
+            throw new IllegalArgumentException("자동차는 적어도 2개 이상이어야 합니다.");
+        }
+    }
+
+    private void checkDuplicateName(List<String> carNames) {
+        long distinctCount = carNames.stream().distinct().count();
+        if (distinctCount < carNames.size()) {
+            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+        }
     }
 }
