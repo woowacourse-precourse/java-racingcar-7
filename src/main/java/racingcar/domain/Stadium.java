482b3a2bb9a8cff221c2f142985f06e7df9a515a
yallo428 <yallo428@naver.com>
2024-10-28 14:22:29 +0900

    feat(Stadium) : 경기장 구현
    경기장 구현:
    - 입력값으로 Car 생성
    - 자동차들 이동 기능
    - 최고 점수 찾은 후, 우승자 반환 기능

---
+++ src/main/java/racingcar/domain/Stadium.java
@@ -0,0 +1,53 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stadium {
+
+    private final List<Car> cars;
+
+    public Stadium(List<String> carNames) {
+        this.cars = createCars(carNames);
+    }
+
+    public List<String> updateCarPositions() {
+        List<String> scores = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.isForwardMovable()) {
+                car.addScore();
+            }
+            scores.add(car.currentScore());
+        }
+
+        return scores;
+    }
+
+    public List<String> getWinners() {
+        List<String> winners = new ArrayList<>();
+        int maxNum = findMaxScore();
+
+        for (Car car : cars) {
+            if (car.isMoveCountHigher(maxNum)) {
+                winners.add(car.getWinner());
+            }
+        }
+
+        return winners;
+    }
+
+    private List<Car> createCars(List<String> carNames) {
+        return carNames.stream().map(Car::new).toList();
+    }
+
+    private int findMaxScore() {
+        int maxScore = 0;
+        for (Car car : cars) {
+            if (car.isMoveCountHigher(maxScore)) {
+                maxScore = car.compareScore(maxScore);
+            }
+        }
+        return maxScore;
+    }
+
+}
