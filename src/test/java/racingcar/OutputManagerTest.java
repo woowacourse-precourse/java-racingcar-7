12329cb51443dcd98f0c3cd22e402b0cfababa47
eb9862 <luclead622@gmail.com>
2024-10-28 17:38:08 +0900

    test : OutputManagerTest 테스트 코드 수정
    - DisplayName 애노테이션을 사용하여 테스트를 구체적으로 명시
    - 테스트 코드 내의 테스트 변수명 또한 더 구체적으로 명시

--- src/test/java/racingcar/OutputManagerTest.java
+++ src/test/java/racingcar/OutputManagerTest.java
@@ -4,22 +4,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class OutputManagerTest extends NsTest {
 
     @Test
+    @DisplayName("리스트에 이름이 1개일 경우, 단독 우승으로 쉼표로 구분없이 결과 출력")
     void 기능_테스트_단독우승() {
-        List<String> testList = List.of("java");
-        OutputManager.displayWinner(testList);
+        List<String> testWinnerList = List.of("java");
+        OutputManager.displayWinner(testWinnerList);
 
         assertThat(output()).isEqualTo("최종 우승자 : java");
     }
 
     @Test
+    @DisplayName("리스트에 이름 여러 개일 경우, 공동 우승으로 쉼표로 구분하여 결과 출력")
     void 기능_테스트_공동우승() {
-        List<String> testList = List.of("java", "pobi");
-        OutputManager.displayWinner(testList);
+        List<String> testWinnerList = List.of("java", "pobi");
+        OutputManager.displayWinner(testWinnerList);
 
         assertThat(output()).isEqualTo("최종 우승자 : java, pobi");
     }
