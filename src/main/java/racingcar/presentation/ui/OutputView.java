47892b1865256d596e838d694237c4dd9bdbfed9
jijongkwon <help3451@naver.com>
2024-10-28 17:45:47 +0900

    feat(ui) : OutputView 실행 결과 출력 메서드 구현

--- src/main/java/racingcar/presentation/ui/OutputView.java
+++ src/main/java/racingcar/presentation/ui/OutputView.java
@@ -1,18 +1,29 @@
 package racingcar.presentation.ui;
 
-import racingcar.domain.car.RacingCar;
+import racingcar.application.service.history.RacingHistoryService;
+import racingcar.domain.car.repository.RacingCarRepository;
 import racingcar.domain.winner.Winner;
 
 import java.util.List;
+import racingcar.infrastructure.persistence.InMemoryRacingCarRepository;
+import racingcar.presentation.enums.UiMessage;
 
 public class OutputView {
-    // TODO: 실행 결과 화면 출력
-    public void outputRacingResultUi(List<RacingCar> racingCars) {
+    private final RacingHistoryService racingHistoryService;
+    private final RacingCarRepository racingCarRepository;
 
+    public OutputView() {
+        this.racingHistoryService = new RacingHistoryService();
+        this.racingCarRepository = InMemoryRacingCarRepository.getInstance();
+    }
+
+    // TODO: 실행 결과 화면 출력
+    public void outputRacingResultUi() {
+        System.out.println(UiMessage.UI_OUTPUT_EXECUTION_RESULT_MESSAGE.getMessage());
+        racingHistoryService.printRacingResult();
     }
 
     // TODO: 최종 우승자 화면 출력
     public void outputWinnerUi(List<Winner> winners) {
-
     }
 }
