3677ea0735713078d3205f2b5a4b30e18e97c4db
gyuho <kyuhokim12@gmail.com>
2024-10-28 15:55:42 +0900

    refactor(main): 코딩 컨벤션에 맞춰 코드 수정
    -raceController=에서 raceController = 으로 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,7 +4,7 @@ import racingcar.controller.RaceController;
 
 public class Application {
     public static void main(String[] args) {
-        RaceController raceController= new RaceController();
+        RaceController raceController = new RaceController();
         raceController.startGame();
     }
 }
