d5be014546afc3370ff75941f9c655ba9d7e84f6
baesh <baesh6778@gmail.com>
2024-10-28 20:22:14 +0900

    feat: 자동차는 무작위 값에 따라 전진하거나 멈춤

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
@@ -8,6 +9,9 @@ public class Application {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             String carNamesInput = Console.readLine();
             String[] carNames = validateCarNames(carNamesInput);
+            
+            // 자동차 이동
+            moveCars(carNames);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
@@ -22,5 +26,15 @@ public class Application {
         }
         return carNames;
     }
-    
+
+    static void moveCars(String[] carNames) {
+        for (String car : carNames) {
+            int randomValue = Randoms.pickNumberInRange(0, 9);
+            if (randomValue >= 4) {
+                System.out.println(car + "가 전진합니다.");
+            } else {
+                System.out.println(car + "가 멈춥니다.");
+            }
+        }
+    }
 }
