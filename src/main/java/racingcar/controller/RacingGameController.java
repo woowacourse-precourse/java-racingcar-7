02a45439979704085fca30e7a251fc484f2deb54
BusanGukbap <dong2857210@gmail.com>
2024-10-28 13:14:43 +0900

    feat($controller) : create class - RacingGameController

---
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -0,0 +1,15 @@
+package racingcar.controller;
+
+import racingcar.service.RacingGameService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.list;
+
+public class RacingGameController {
+
+    public void startGame() {
+
+    }
+
+}
