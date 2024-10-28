71a25c468e66e89d539fddda8cb677261f89f17b
jijongkwon <help3451@naver.com>
2024-10-28 12:53:07 +0900

    feat(service) : WinnerService 메서드 구현
    - saveWinners : 최대값과 일치하는 자동차 저장하기
    - selectWinners : 이동거리가 최대값과 일치하는 지 비교하기
    - getWinners : 우승자 반환하기

--- src/main/java/racingcar/application/service/winner/WinnerService.java
+++ src/main/java/racingcar/application/service/winner/WinnerService.java
@@ -1,10 +1,41 @@
 package racingcar.application.service.winner;
 
+import java.util.List;
+import racingcar.domain.car.RacingCar;
+import racingcar.domain.car.repository.RacingCarRepository;
+import racingcar.domain.history.repository.RacingHistoryRepository;
+import racingcar.domain.winner.Winner;
+import racingcar.domain.winner.repository.WinnerRepository;
+import racingcar.infrastructure.persistence.InMemoryRacingCarRepository;
+import racingcar.infrastructure.persistence.InMemoryRacingHistoryRepository;
+import racingcar.infrastructure.persistence.InMemoryWinnerRepository;
+
 public class WinnerService {
 
-    // TODO : 이동거리가 최대값과 일치하는 지 비교하기
+    private final RacingHistoryRepository racingHistoryRepository;
+    private final RacingCarRepository racingCarRepository;
+    private final WinnerRepository winnerRepository;
 
-    // TODO : 최대값과 일치하는 자동차 저장하기
+    public WinnerService() {
+        this.racingHistoryRepository = InMemoryRacingHistoryRepository.getInstance();
+        this.racingCarRepository = InMemoryRacingCarRepository.getInstance();
+        this.winnerRepository = InMemoryWinnerRepository.getInstance();
+    }
 
-    // TODO : 우승자 반환하기
+    public void saveWinners(){
+        for(RacingCar racingCar : racingCarRepository.getRacingCars()){
+            if(selectWinners(racingCar)){
+                winnerRepository.saveWinner(new Winner(racingCar.getName()));
+            }
+        }
+    }
+
+    public boolean selectWinners(RacingCar racingCar){
+        int maxDistance = racingHistoryRepository.getMaxDistance();
+        return maxDistance == racingCar.getDistance();
+    }
+
+    public List<Winner> getWinners(){
+        return winnerRepository.getWinners();
+    }
 }
