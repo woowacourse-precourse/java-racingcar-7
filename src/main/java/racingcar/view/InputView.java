fe4bc6b7735a856c4b0744565cb1f6b62a3381b3
hooniverse <sung010326@gmail.com>
2024-10-28 22:24:00 +0900

    feat: 자동차 이름 입력 기능 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String getCarsName() {
+        String CarsName = Console.readLine();
+        return CarsName;
+    }
+    
+}
