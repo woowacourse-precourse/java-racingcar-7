ca15a29cdfe864f40b0178fad69f908555befed7
Lee Ye Jin <jene0028@gmail.com>
2024-10-28 19:32:01 +0900

    feat: 진행 단계별 실행 결과 출력

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+import racingcar.model.Car;
+import java.util.List;
+
+public class OutputView {
+    public static void printStartMessage() {
+        System.out.println("실행 결과");
+    }
+
+    public static void printRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+}
