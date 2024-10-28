139e260c80835dba5ab14a6c62842aa23930dd1e
aprilnineteen <dencoding@naver.com>
2024-10-28 14:54:18 +0900

    feat: Application 프로그램 시작 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.model.Cars;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingController racingController = new RacingController();
+        racingController.run();
     }
 }
