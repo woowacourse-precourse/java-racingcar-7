a42972561256a635d8c2692ba13c07eb07cbce57
Hyunuk <dksgusdnr17@naver.com>
2024-10-28 21:00:34 +0900

    feat(input): add method to validate car name

--- src/main/java/racingcar/input/StringParser.java
+++ src/main/java/racingcar/input/StringParser.java
@@ -3,6 +3,7 @@ package racingcar.input;
 import java.util.Arrays;
 import java.util.List;
 import racingcar.domain.Car;
+import racingcar.exception.ErrorMessage;
 
 public class StringParser {
     private static StringParser instance;
@@ -26,6 +27,13 @@ public class StringParser {
     }
 
     private Car createCar(String name) {
+        validateCarName(name);
         return new Car(name, 0);
+    }
+
+    private void validateCarName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(ErrorMessage.EXCEED_NAME_LENGTH.getValue());
+        }
     }
 }
