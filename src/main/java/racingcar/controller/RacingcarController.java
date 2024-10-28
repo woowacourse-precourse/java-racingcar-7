031ffbcd00d35e7a299da7c9efa12bce7432b998
chino <tnehd83@naver.com>
2024-10-28 14:24:17 +0900

    feat(RacingcarSplitter): 문자열 분리 및 검증 구현

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -1,15 +1,20 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.gameuserdata.RacingUserData;
 import racingcar.model.RacingcarDataVerification;
+import racingcar.splitter.RacingcarSplitter;
 import racingcar.view.InputView;
 
 public class RacingcarController {
     private final InputView inputView;
     private final RacingcarDataVerification racingcarDataVerification;
+    private final RacingcarSplitter splitter;
 
     public RacingcarController() {
         inputView = new InputView();
         racingcarDataVerification = new RacingcarDataVerification();
+        splitter = new RacingcarSplitter();
     }
 
     public void run() {
@@ -18,5 +23,6 @@ public class RacingcarController {
 
         String[] RacingCarSplitName = RacingCarNames.split(",");
         racingcarDataVerification.validateRacingCarNames(RacingCarSplitName);
+        List<RacingUserData> racingUserDataList = splitter.racingUserDataSplitter(RacingCarSplitName);
     }
 }
