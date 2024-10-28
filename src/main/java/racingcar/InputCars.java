39197c4d7d3427cdb7ea7d3adf5e64a4b32deeb4
seyeon22 <sywolleych@gmail.com>
2024-10-28 17:58:20 +0900

    fix(InputCars): 공백 포함한 입력 예외로 추가

--- src/main/java/racingcar/InputCars.java
+++ src/main/java/racingcar/InputCars.java
@@ -8,8 +8,11 @@ public class InputCars {
     public Map<String, Integer> receiveCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
+        if (input.contains(" ")) {
+            throw new IllegalArgumentException();
+        }
         List<String> tempCars = Arrays.stream(input.split(",")).toList();
-        Set<String> cars = new HashSet<>(tempCars);
+        Set<String> cars = new HashSet<>(tempCars);  // 중복된 차 이름 제거 위해 Set으로 변환
         for (var car : cars) {
             if (car.length() > 5) {
                 throw new IllegalArgumentException();
