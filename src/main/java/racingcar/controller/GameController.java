2696fdd35f74c8f9ea98087ba404cc58e9bebc7e
Si Yeong Park <psy915@swu.ac.kr>
2024-10-28 20:47:15 +0900

    feat(GameController): GameService를 통한 게임 실행 및 결과 반환

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,5 +1,8 @@
 package racingcar.controller;
 
+import racingcar.model.service.GameService;
+import racingcar.model.RandomNumberGenerator;
+import racingcar.model.domain.GameResult;
 import racingcar.view.InputView;
 import java.util.List;
 
@@ -8,5 +11,8 @@ public class GameController {
     public void runGame() {
         List<String> carNames = InputView.inputNames();
         int attemptCount = InputView.inputGameCnt();
+
+        GameService gameService = new GameService(carNames, attemptCount, new RandomNumberGenerator());
+        GameResult gameResult = gameService.playGame();
     }
 }
