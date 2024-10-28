5028d08dbb4a2a784713ddc9549fae160b3ed857
dldb-chamchi <dladkfl49@gmail.com>
2024-10-28 20:15:15 +0900

    fix: change function's name. setCarName -> splitCarName

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,18 +4,29 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
     private List<RacingCar> cars = new ArrayList<>();
 
-    public String[] setCarName(String input) {
+    public String[] splitCarName(String input) {
         return input.split(",");
     }
 
-    public void setCar(String[] carNames) {
-        List<String> checknames = new ArrayList<>();
+    public Set<String> setCarName(String[] carNames) {
+        Set<String> carNamesSet = new HashSet<>();
+
         for (String name : carNames) {
+            carNamesSet.add(name.trim()); // 이름 양쪽 공백 제거 후 추가
+        }
+        if(carNamesSet.isEmpty()) throw new IllegalArgumentException();
+        return carNamesSet;
+    }
+
+    public void setCar(Set<String> carNamesSet) {
+        for (String name : carNamesSet) {
             cars.add(new RacingCar(name.trim()));
         }
     }
@@ -56,12 +67,13 @@ public class Application {
         Application racingGame = new Application();
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNameInput = Console.readLine();
-        String[] carNames = racingGame.setCarName(carNameInput);
-        racingGame.setCar(carNames);
+        String[] carNames = racingGame.splitCarName(carNameInput);
+        racingGame.setCar(racingGame.setCarName(carNames));
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String numberOfGames = Console.readLine();
         int gameCount = Integer.parseInt(numberOfGames);
+        if(gameCount <= 0) throw new IllegalArgumentException();
 
         System.out.println("\n실행 결과");
         for(int i = 0; i<gameCount; ++i){
