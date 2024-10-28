c8849f27f53e34edb7ccc10787f1a9878914ceed
inha0319@naver.com <inha031913@gmail.com>
2024-10-28 11:18:53 +0900

    [feat] OutputView 클래스 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,23 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+
+import java.util.List;
+
 public class OutputView {
+
+    public static void printRaceStatus(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.print(car.getName() + " : ");
+            System.out.print("-".repeat(car.getLocation()));
+            System.out.println();
+        }
+    }
+
+    public static void printWinners(List<Car> winners) {
+        String winnerNames = String.join(", ", winners.stream()
+                .map(Car::getName)
+                .toArray(String[]::new));
+        System.out.println("최종 우승자 : " + winnerNames);
+    }
 }
