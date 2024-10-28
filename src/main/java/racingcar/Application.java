a8f78c09cc393a0485a2f43ad3afe989c039fd15
dayeondayeon <dayeon2438@gmail.com>
2024-10-28 16:06:43 +0900

    feat : 각 이름들로 자동차 객체 생성 코드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static racingcar.Utils.*;
 
@@ -7,5 +10,10 @@ public class Application {
     public static void main(String[] args) {
         String[] carNames = getNamesFromString(readLine());
         int tryCount = getTryCountFromString(readLine());
+
+        List<Vehicle> vehicles = new ArrayList<>(carNames.length);
+        for (String carName : carNames) {
+            vehicles.add(new Car(carName, 0));
+        }
     }
 }
