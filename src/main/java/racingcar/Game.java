958e38fa661a279a97d6b112916bff2b6cdaee69
kimch <rlacogk6577@daum.net>
2024-10-28 19:46:49 +0900

    refactor(Game): 사용자 입력 받는 방법 수정

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,14 +1,12 @@
 package racingcar;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import camp.nextstep.edu.missionutils.Console;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class Game {
-    BufferedReader bufferedReader;
     int moveNumber;
     ArrayList<Car> Cars;
 
@@ -16,7 +14,7 @@ public class Game {
         System.out.println("게임을 진행할 자동차들의 이름을 쉼표(,)로 구분하여 입력 해주세요.");
         List<String> carNames = new ArrayList<>();
         try{
-            String readString= bufferedReader.readLine();
+            String readString= Console.readLine();
             String regex = "^([A-Za-z0-9][A-Za-z0-9 ]{0,4})(,[A-Za-z0-9][A-Za-z0-9 ]{0,4})*$";
             if(Pattern.matches(regex, readString))
                 throw new IllegalArgumentException();
@@ -29,8 +27,8 @@ public class Game {
     }
 
 
+
     public Game(){
-        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         readCarName();
     }
 
