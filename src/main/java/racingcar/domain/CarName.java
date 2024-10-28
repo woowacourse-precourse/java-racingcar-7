c58bf8c825eddd5549decf38bffeeff62ac176e5
gyuho <kyuhokim12@gmail.com>
2024-10-28 14:43:41 +0900

    refactor(CarName): 변수의 이름을 직관적으로 변경
    -NAME_LENGTH_OVER_ERROR를 NAME_LENGTH_OVER_ERROR_MESSAGE로 명칭 변경하여 더욱 직관적으로 변경

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -2,13 +2,13 @@ package racingcar.domain;
 
 public class CarName {
     private final int MAX_LENGTH = 5;
-    private final String NAME_LENGTH_OVER_ERROR = "자동차 이름의 길이는 5 이하여야만 합니다";
+    private final String NAME_LENGTH_OVER_ERROR_MESSAGE = "자동차 이름의 길이는 5 이하여야만 합니다";
 
     private String name;
 
     public CarName(String name){
         if(name.length() > MAX_LENGTH){
-            throw new IllegalArgumentException(NAME_LENGTH_OVER_ERROR);
+            throw new IllegalArgumentException(NAME_LENGTH_OVER_ERROR_MESSAGE);
         }
         this.name = name;
     }
