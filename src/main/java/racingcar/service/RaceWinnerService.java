0e349fa175e65dc75c1bf6e51e12eb6c78b6f8e0
kaswhy <sxshx818@naver.com>
2024-10-28 21:14:15 +0900

    refactor(RaceWinnerService): 원시값 포장을 위해 구현 방법 변경
    변경 전
    - int로 max 값 초기화, max값 업데이트
    
    변경 후
    - Distance로 max값 초기화 및 업데이트

--- src/main/java/racingcar/service/RaceWinnerService.java
+++ src/main/java/racingcar/service/RaceWinnerService.java
@@ -13,19 +13,18 @@ public class RaceWinnerService {
     private static final int INIT_MAX_TO_ZERO = 0;
 
     public static void findLastWinner(CarScores carScores) {
-        int max = INIT_MAX_TO_ZERO;
+        Distance maxDistance = new Distance(INIT_MAX_TO_ZERO);
         for (Map.Entry<CarName, Distance> nowCarScore : carScores.getCarScores().entrySet()) {
             Distance nowDistance = nowCarScore.getValue();
-            max = getNewMax(max, nowDistance.getDistance());
+            maxDistance = getNewMax(maxDistance.getDistance(), nowDistance.getDistance());
         }
 
-        List<String> lastWinners = getLastWinners(carScores.getCarScores(), new Distance(max));
-
+        List<String> lastWinners = getLastWinners(carScores.getCarScores(), maxDistance);
         OutputView.printFinalWinner(lastWinners);
     }
 
-    private static int getNewMax(int max, int nowScore) {
-        return Math.max(nowScore, max);
+    private static Distance getNewMax(int max, int nowScore) {
+        return new Distance(Math.max(nowScore, max));
     }
 
     private static List<String> getLastWinners(LinkedHashMap<CarName, Distance> nowCarScores, Distance maxDistance) {
