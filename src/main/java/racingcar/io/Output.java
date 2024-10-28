5862b5cfe86aa8e43000fd8884afb81c0dac14df
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-10-28 16:08:44 +0900

    style: 줄바꿈 방식 변경
    코드 한줄로 줄바꿈까지 실행할 수 있도록 개행 문자를 사용하여 수정했습니다.

--- src/main/java/racingcar/io/Output.java
+++ src/main/java/racingcar/io/Output.java
@@ -5,8 +5,7 @@ import racingcar.car.Car;
 
 public class Output {
     public void printResult() {
-        System.out.println();
-        System.out.println("실행 결과");
+        System.out.println("\n실행 결과");
     }
 
     public void printCar(List<Car> cars) {
