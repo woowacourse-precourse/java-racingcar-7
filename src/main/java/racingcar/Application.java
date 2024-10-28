7b4720189e57de9e6299f482003deb34a5acc6df
smk412 <whu1477@naver.com>
2024-10-28 20:40:34 +0900

    feat: 0~9 사이에 무작위 값 생성하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,27 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Scanner;
 
 
 public class Application {
     public static void main(String[] args) {
+
+        // 자동차 이름 입력하기
         Scanner sc = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = sc.nextLine();
 
+        //이동을 시도할 횟수 입력하기
         System.out.println("시도할 횟수는 몇 회인가요?");
         int count = sc.nextInt();
 
+        //쉼표 구분자를 이용하여 입력한 자동차를 구별하고 배열에 넣기
         String[] cars = carName.split(",");
-        for (int i = 0; i < cars.length; i++) {
-            System.out.println(cars[i]);
-        }
+
+        // 0~9 사이에 무작위 값 생성하기
+        int ranNum;
+        ranNum = Randoms.pickNumberInRange(0, 9);
+        System.out.println(ranNum);
     }
 }
