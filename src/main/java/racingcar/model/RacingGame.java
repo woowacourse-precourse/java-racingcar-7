14c572b17253df1c062a1334dbac0e7b918a347e
Sujin Kim <su10jin11@khu.ac.kr>
2024-10-28 18:40:36 +0900

    feat : 게임을 진행하는 RacingGame 클래스 구현

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -1,6 +1,24 @@
 package racingcar.model;
 
+import java.util.List;
+
 public class RacingGame {
+    private final Cars cars;
+    private final int tryCount;
 
+    public RacingGame(Cars cars, int tryCount) {
+        this.cars = cars;
+        this.tryCount = tryCount;
+    }
 
+    public void play(Runnable roundResultCallback) {
+        for (int i = 0; i < tryCount; i++) {
+            cars.moveCars();
+            roundResultCallback.run();
+        }
+    }
+
+    public List<String> getWinners() {
+        return cars.getWinners();
+    }
 }
