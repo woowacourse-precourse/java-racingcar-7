cfbdd3746971e08436f96327a689196c6e040a69
AhnJoonSung <tjdtna01@naver.com>
2024-10-28 13:49:45 +0900

    Feature: main에서 전체 흐름 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -11,5 +14,11 @@ public class Application {
         int turn = Integer.parseInt(Console.readLine());
 
         String[] carNames = input.split(",");
+        List<Car> cars = Arrays.stream(carNames)
+                .map(Car::new)
+                .toList();
+
+        RacingGame game = new RacingGame(cars, turn);
+        game.start();
     }
 }
