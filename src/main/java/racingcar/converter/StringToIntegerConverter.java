97c1986f2d95890cbce31b7aa0c5a3c24897b058
Junad-Park <qkrwnsgk1624@icloud.com>
2024-10-28 15:16:56 +0900

    feat(StringToIntegerConverter): String을 Integer 타입으로 변환할 수 없는 경우 예외 처리 추가

--- src/main/java/racingcar/converter/StringToIntegerConverter.java
+++ src/main/java/racingcar/converter/StringToIntegerConverter.java
@@ -1,5 +1,7 @@
 package racingcar.converter;
 
+import racingcar.enums.ErrorMessage;
+
 public class StringToIntegerConverter implements Converter<String, Integer> {
 
     public StringToIntegerConverter() {
@@ -7,6 +9,10 @@ public class StringToIntegerConverter implements Converter<String, Integer> {
 
     @Override
     public Integer convert(String input) {
-        return Integer.parseInt(input);
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_TRY_COUNT.getMessage());
+        }
     }
 }
