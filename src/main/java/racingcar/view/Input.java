7afab0beeaa81752a0540577df80268b8a5e0151
bbandm <hjerachel@gmail.com>
2024-10-28 22:59:55 +0900

    feat(Input): 자동차 이름 입력받는 기능 구현

---
+++ src/main/java/racingcar/view/Input.java
@@ -0,0 +1,20 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Input {
+    private static final String SYSTEM_CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String SYSTEM_ATTEMPT_NUMBER_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
+    public List<Car> getCarName() {
+        System.out.println(SYSTEM_CAR_NAME_INPUT_MESSAGE);
+        String carNameInput = Console.readLine();
+        validateCarNameInput(carNameInput);
+
+        return makeCarList(carNameInput);
+    }
+}
