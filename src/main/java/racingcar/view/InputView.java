ed22eb9fed563b190147efc878c19a228c2ccd84
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-28 13:26:25 +0900

    feat: 사용자 입력을 처리하는 InputView 클래스 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,31 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.Car;
+import racingcar.validator.CarValidator;
+
+import java.util.List;
+
+public class InputView {
+    private static String getCarNamesInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    private static List<String> validateAndCreateCarList(String input) {
+        List<String> carNames = List.of(input.split(","));
+
+        // 각 자동차 이름 유효성 검사
+        for (String carName : carNames) {
+            CarValidator.validateName(carName);
+        }
+        CarValidator.validateUniqueNames(carNames);
+
+        return carNames;
+    }
+
+    public static List<String> getCarNames() {
+        String input = getCarNamesInput();
+        return validateAndCreateCarList(input);
+    }
+}
