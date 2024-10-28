d54a11121eb51e1c9d1e8a73fd746346ac9a4180
jihyun <jihyun3066@naver.com>
2024-10-28 19:18:27 +0900

    [ feat ]  : 입력에 "," 이외에 다른 구분자 존재 시 예외 처리

--- src/main/java/racingcar/model/CarNameDelimiter.java
+++ src/main/java/racingcar/model/CarNameDelimiter.java
@@ -1,5 +1,7 @@
 package racingcar.model;
 
+import racingcar.exception.ExceptionCode;
+
 
 public class CarNameDelimiter {
 
@@ -8,11 +10,15 @@ public class CarNameDelimiter {
 
 
     public String[] NameDelimiter(String name) {
-
         carNames = name.trim().split("\\s*" + DELIMITER + "\\s*");
 
         return carNames;
     }
 
+    public void validateDelimiter(String name) {
+        if (name.matches(".*[^\\w\\s,가-힣].*")) {
+            throw new IllegalArgumentException(String.valueOf(ExceptionCode.INVALID_DELIMITER));
+        }
+    }
 
 }
