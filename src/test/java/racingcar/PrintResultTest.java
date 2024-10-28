b62886b8bda0b54c8627f0bfb1f9059236bf895b
euni <kongeuni1004@gmail.com>
2024-10-28 16:50:08 +0900

    test: PrintResult 클래스의 전진 및 우승자 출력 테스트 추가
    - 자동차의 전진 횟수만큼 "-"로 출력되는지 확인하는 `전진횟수만큼_bar출력_테스트` 추가
    - 최종 우승자 목록이 올바르게 출력되는지 검증하는 `최종우승자_출력_테스트` 추가
    - 출력 결과 검증을 통해 게임 결과 표시 기능의 완성도 향상

---
+++ src/test/java/racingcar/PrintResultTest.java
@@ -0,0 +1,46 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+
+public class PrintResultTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+
+    PrintResult printResult = new PrintResult();
+
+    @Test
+    void 전진횟수만큼_bar출력_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    Car pobi = new Car("pobi");
+
+                    pobi.move();
+                    pobi.move();
+
+                    printResult.printCar(pobi);
+
+                    assertThat(output()).contains("pobi : --");
+
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 최종우승자_출력_테스트() {
+        List<String> carName = List.of("pobi","woni");
+        printResult.printWinner(carName);
+        assertThat(output()).contains("최종 우승자 : pobi, woni");
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
