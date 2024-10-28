c3920cdac06ef0222762188098160789ea9d0d82
seohee-P <p03061180@gmail.com>
2024-10-28 22:57:57 +0900

    test: 예외테스트 추가
    - 차 이름 글자수가 5보다 클 때 예외처리
    - 차 이름이 중복됐을 때 예외처리
    - 차 이름이 공백일 때 예외처리
    - 시도할 횟수가 숫자가 아닐 때 예외처리

---
+++ src/test/java/racingcar/RacingCarTest.java
@@ -0,0 +1,64 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+public class RacingCarTest {
+    private RacingCar racingCar;
+
+    @BeforeEach
+    public void setup() {
+        racingCar = new RacingCar();
+    }
+
+    @AfterEach
+    void tearDown() {
+        Console.close();
+    }
+
+    @Test
+    public void 글자수가_5보다_클_때_예외테스트() {
+        String input = "pobi,woni,jackson/n2";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            racingCar.run();
+        });
+    }
+
+    @Test
+    public void 시도할_횟수가_숫자가_아닐_때_예외테스트() {
+        String input = "pobi,woni,jun/naaa";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            racingCar.run();
+        });
+    }
+
+    @Test
+    public void 차_이름이_중복됐을_때_예외테스트() {
+        String input = "pobi,pobi/n3";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            racingCar.run();
+        });
+    }
+
+    @Test
+    public void 차_이름이_공백일_때_예외테스트() {
+        String input = ",pobi/n3";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            racingCar.run();
+        });
+    }
+
+}
