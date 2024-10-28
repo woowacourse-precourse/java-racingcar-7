7be00c0422642cbc8f0b643de86480a55f138a27
hhh88261 <hhh88261@gmail.com>
2024-10-28 20:08:19 +0900

    FIX : 랜덤한 숫자가 경기현황에 반영되는 것을 전진 현황을 출력하도록 수정

--- src/main/java/racingcar/caculator/WinnerCaculator.java
+++ src/main/java/racingcar/caculator/WinnerCaculator.java
@@ -1,5 +1,8 @@
 package racingcar.caculator;
 
+import racingcar.input.CarInput;
+import racingcar.output.WinnerOutput;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
@@ -7,8 +10,10 @@ import java.util.Map;
 public class WinnerCaculator {
     // 누적 우승자를 저장할 리스트
     private final List<String> totalWinners = new ArrayList<>();
+    WinnerOutput winnerOutput = new WinnerOutput();
+    CarDistance carDistance = new CarDistance();
 
-    public void WinnerResult(List<String> carName, List<Integer> carRandomNumber) {
+    public List<String> WinnerResult(List<String> carName, List<Integer> carRandomNumber, int randomNumber, int repeat) {
         Map<String, Integer> playReusult = new HashMap<>();
 
         // 두 리스트를 value=key 형태로 매핑
@@ -22,20 +27,27 @@ public class WinnerCaculator {
 
         // HashMap을 순회하며 최대값을 가진 우승자 찾기
         for (Map.Entry<String, Integer> entry : playReusult.entrySet()) {
-            if (entry.getValue() > maxNumber) {
-                // 새로운 최대값을 찾으면 리스트를 초기화하고 우승자 추가
-                winnerName.clear();
-                winnerName.add(entry.getKey());
-                maxNumber = entry.getValue();
-            } else if (entry.getValue() == maxNumber) {
-                // 최대값과 동일한 값이 있을 경우 우승자 추가
-                winnerName.add(entry.getKey());
+            //랜덤값이 4이상인 경우에만 우승자 후보에 추가
+            if (entry.getValue() >= 4) {
+                if (entry.getValue() > maxNumber) {
+                    // 새로운 최대값을 찾으면 리스트를 초기화하고 우승자 추가
+                    winnerName.clear();
+                    winnerName.add(entry.getKey());
+                    maxNumber = entry.getValue();
+                } else if (entry.getValue() == maxNumber) {
+                    // 최대값과 동일한 값이 있을 경우 우승자 추가
+                    winnerName.add(entry.getKey());
+                }
             }
         }
         // 현재 실행의 우승자를 누적 리스트에 추가
         totalWinners.addAll(winnerName);
 
-        // 결과 출력
-        System.out.println("우승자 : " + totalWinners);
+        carDistance.CarDistanceCaculator(carName, totalWinners);
+
+        if(repeat + 1 == randomNumber) {
+            winnerOutput.FinallyWinner(totalWinners);
+        }
+        return winnerName;
     }
 }
