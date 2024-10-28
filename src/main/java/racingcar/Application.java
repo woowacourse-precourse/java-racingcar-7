5fd1e4c65aafe0f5c6a3e4fb27552662a6086884
SangWookie <swcube520@gmail.com>
2024-10-29 02:20:47 +0900

    docs($main): add comments

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,5 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,17 +7,21 @@ public class Application {
     public static void main(String[] args) {
         InputHandler inputHandler = new InputHandler();
 
+        // 입력 받기
         inputHandler.inputNames();
         inputHandler.inputCount();
 
+        // 입력을 기반으로 Car 인스턴스 생성
         List<Car> cars = new ArrayList<>();
         for(String name : inputHandler.getNames()){
             Car car = new Car(name);
             cars.add(car);
         }
 
+        // Race 인스턴스 생성
         Race race = new Race(cars, inputHandler.getRunCount());
 
+        // 경주 진행
         race.runRace();
         race.showWinners();
     }
