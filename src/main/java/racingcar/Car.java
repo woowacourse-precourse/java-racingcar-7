ecd5e8b9ebd13a8679d6e72d8d05c300f8538e7f
SUIN <65757661+whtndls@users.noreply.github.com>
2024-10-28 22:04:46 +0900

    feat: Car 객체

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,40 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car implements Comparable<Car> {
+    private final static int startNum = 0;
+    private final static int endNum = 9;
+
+    private String name;
+    private int randomNumber;
+    private int currentPos;
+
+    public Car(String name) {
+        this.name = name;
+        this.randomNumber = 0;
+        this.currentPos = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCurrentPos() {
+        return currentPos;
+    }
+
+    public void incCurrentPos() {
+        this.currentPos++;
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return o.currentPos - this.getCurrentPos();
+    }
+
+    public int setAndReturnRandomNumber() {
+        this.randomNumber = Randoms.pickNumberInRange(startNum, endNum);
+        return this.randomNumber;
+    }
+}
