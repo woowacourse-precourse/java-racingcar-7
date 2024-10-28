dbaae870089693e7dd1b7e731836773b4dc58a3e
조윤성 <yunsung1206@gmail.com>
2024-10-28 15:29:15 +0900

    refactor: InputView에서만 사용하는 상수를 클래스 내부로 이동

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,18 +2,20 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.constant.ErrorMessages;
-import racingcar.constant.Messages;
 import racingcar.model.Cars;
 import racingcar.model.TryCount;
 
 public class InputView {
+    public static final String CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+
     public static Cars readCars() {
-        String carNames = read(Messages.INPUT_CAR_NAMES);
+        String carNames = read(CAR_NAMES);
         return new Cars(carNames);
     }
 
     public static TryCount readTryCount() {
-        String tryCount = read(Messages.INPUT_TRY_COUNT);
+        String tryCount = read(TRY_COUNT);
         return new TryCount(tryCount);
     }
 
