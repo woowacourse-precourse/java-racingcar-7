005507245ade22274f99067aa826b149971b0b1e
hyuntae99 <hyuntae9912@naver.com>
2024-10-28 09:14:16 +0900

    feat: 오류 메세지 생성

---
+++ src/main/java/racingcar/Exception/ErrorMessage.java
@@ -0,0 +1,10 @@
+package racingcar.Exception;
+
+public class ErrorMessage {
+    public static final String INVALID_CAR_NAME = "자동차 이름은 1자 이상 5자 이하여야 합니다.";
+    public static final String DUPLICATE_CAR_NAMES = "자동차 이름이 중복될 수 없습니다.";
+    public static final String EMPTY_CAR_NAME = "자동차 이름을 입력해야 합니다.";
+    public static final String INVALID_MOVE_COUNT = "이동 횟수는 숫자여야 합니다.";
+    public static final String NEGATIVE_MOVE_COUNT = "이동 횟수는 음수가 될 수 없습니다.";
+    public static final String EMPTY_MOVE_COUNT = "이동 횟수를 입력해야 합니다.";
+}
