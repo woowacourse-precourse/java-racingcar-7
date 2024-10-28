0e61f9502a4f53910338397970c6c8803af3c2f9
esfuerzo <dkdldrha@naver.com>
2024-10-28 18:30:00 +0900

    feat(Application) : implement racing car

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,38 +1,62 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+
+import java.util.Arrays;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static racingcar.Application.ToNameArray;
+
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
-
+    private static final int STOP = 2;
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
             () -> {
                 run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                assertThat(output()).contains("pobi : -", "woni : ","최종 우승자 : pobi");
             },
             MOVING_FORWARD, STOP
         );
     }
 
+
     @Test
-    void 예외_테스트() {
+    void 이름이_5자_초과시() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
-
+    @Test
+    void 중복이름_처리() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,pobi", "1");
+                    assertThat(output()).contains("최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+    @Test
+    void 잘못된_숫자_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
     @Override
     public void runMain() {
         Application.main(new String[]{});
     }
+
+
 }
