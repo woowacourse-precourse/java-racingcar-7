da8da925ec68a4302a7736c2e26bf3685171e37b
HyejinYoo <yyyhj0114@gmail.com>
2024-10-28 18:32:49 +0900

    refactor: apply MoveStrategy in Car class

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,13 +3,15 @@ package racingcar;
 public class Car {
     private String name;
     private int position;
+    private MoveStrategy moveStrategy;
 
-    public Car(String name) {
+    public Car(String name, MoveStrategy moveStrategy) {
     	if (name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         }
         this.name = name;
         this.position = 0;
+        this.moveStrategy = moveStrategy;
     }
 
     public String getName() {
@@ -20,8 +22,8 @@ public class Car {
         return position;
     }
 
-    public void move(boolean canMove) {
-        if (canMove) {
+    public void move() {
+        if (moveStrategy.canMove()) {
             position++;
         }
     }
