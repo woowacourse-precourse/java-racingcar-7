888a025c40211454dfdbdca7dbd0e14ab8edd95a
Minseok <saltishDev@gmail.com>
2024-10-28 11:37:14 +0900

    test: 자동차 이름 테스트
    1. 중복된 자동차 이름을 포함하는 경우
    2. 자동차 이름이 0글자인 경우
    3. 자동차 이름이 5글자 초과인 경우
    4. 자동차 이름이 1글자 이상 5글자 이하로만 구성된 경우
    5. 자동차 이름의 첫 글자가 영문자로 시작하지 않는 경우
    6. 자동차 이름의 첫 글자가 영문자로 시작하는 경우
    7. 자동차 이름이 쉼표로만 구성된 경우
    8. 자동차 이름을 입력하지 않은 경우
    9. 자동차 이름이 영문자와 숫자로만 구성되지 않은 경우
    10. 자동차 이름이 영문자와 숫자로만 구성된 경우

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
@@ -28,6 +29,83 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 중복된_자동차_이름을_포함하는_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,jun,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름이_0글자인_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,,pobi", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름이_5글자_초과인_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,banana", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름이_1글자_이상_5글자_이하인_경우() {
+        assertSimpleTest(() ->
+                assertThatCode(() -> run("pobi,cool,sun", "4")).doesNotThrowAnyException()
+        );
+    }
+
+    @Test
+    void 자동차_이름의_첫_글자가_영문자로_시작하지_않는_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("123", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름의_첫_글자가_영문자로_시작하는_경우() {
+        assertSimpleTest(() ->
+                assertThatCode(() -> run("Sun", "4")).doesNotThrowAnyException()
+        );
+    }
+
+    @Test
+    void 자동차_이름이_쉼표로만_구성된_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",,,,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름을_입력하지_않은_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름이_영문자와_숫자로만_구성되지_않은_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("a b", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름이_영문자와_숫자로만_구성된_경우() {
+        assertSimpleTest(() ->
+                assertThatCode(() -> run("a2b", "2")).doesNotThrowAnyException()
         );
     }
 
