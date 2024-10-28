37ea099eaa3de987789354c99955b7110c5d0ed5
plan11plan <tjtfy7028@gmail.com>
2024-10-28 15:54:45 +0900

    feat: Service 플레이어 등록하기 기능 구현
    메서드 구현 및 테스트 구현
        - 정상적인 플레이어 등록
        - 부적절한 이름으로 등록 시도

---
+++ src/main/java/racingcar/service/PlayerService.java
@@ -0,0 +1,45 @@
+package racingcar.service;
+
+import static racingcar.exception.player.PlayerException.PlayerExceededException;
+import static racingcar.exception.player.PlayerException.PlayerUnderstaffedException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.constant.GameConstants;
+import racingcar.domain.player.Player;
+import racingcar.repository.PlayerRepository;
+import racingcar.util.IdGenerator;
+
+public class PlayerService {
+    private final PlayerRepository repository;
+    private final IdGenerator idGenerator;
+
+    public PlayerService(PlayerRepository repository, IdGenerator idGenerator) {
+        this.repository = repository;
+        this.idGenerator = idGenerator;
+    }
+
+    public List<Player> register(List<String> names) {
+        validateNames(names);
+        return names.stream()
+                .map(this::createPlayer)
+                .collect(Collectors.toList());
+    }
+
+    private void validateNames(List<String> names) {
+        if (names.size() > GameConstants.MAXIMUM_PLAYERS) {
+            throw new PlayerExceededException();
+        }
+
+        if (names.size() < GameConstants.MINIMUM_PLAYERS) {
+            throw new PlayerUnderstaffedException();
+        }
+    }
+
+    private Player createPlayer(String name) {
+        Long id = idGenerator.generate();
+        Player player = Player.of(id, name);
+        return repository.save(player);
+    }
+
+}
---
+++ src/test/java/racingcar/service/PlayerServiceTest.java
@@ -0,0 +1,54 @@
+package racingcar.service;
+
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import racingcar.exception.car.InvalidCarNameException;
+import racingcar.repository.MemoryPlayerRepository;
+import racingcar.repository.PlayerRepository;
+import racingcar.util.IdGenerator;
+
+@DisplayName("플레이어 서비스(PlayerService) 유스케이스")
+class PlayerServiceTest {
+
+    @DisplayName("플레이어 등록하기")
+    @Nested
+    class 플레이어_등록하기 {
+
+        @DisplayName("정상적인 플레이어 등록")
+        @Test
+        void 정상적인_플레이어_등록() {
+            // given
+            PlayerRepository playerRepository = new MemoryPlayerRepository();
+            IdGenerator idGenerator = new IdGenerator();
+            PlayerService playerService = new PlayerService(playerRepository, idGenerator);
+            List<String> playerNames = List.of("p1", "p2", "p3");
+
+            // when
+            Assertions.assertThatCode(() -> playerService.register(playerNames))
+                    .doesNotThrowAnyException();
+        }
+
+        @DisplayName("부적절한 이름으로 등록 시도")
+        @Test
+        void 부적절한_이름으로_등록_시도() {
+            // given
+            PlayerRepository playerRepository = new MemoryPlayerRepository();
+            IdGenerator idGenerator = new IdGenerator();
+            PlayerService playerService = new PlayerService(playerRepository, idGenerator);
+            List<String> playerNames = List.of("", "", "");
+
+            // when
+            Assertions.assertThatThrownBy(() -> playerService.register(playerNames))
+                    .isInstanceOf(InvalidCarNameException.class)
+                    .hasMessage("자동차 이름은 최소 1자부터 가능합니다.");
+            
+        }
+
+
+    }
+
+}
