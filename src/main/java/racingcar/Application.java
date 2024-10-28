95c5e228d9e6a2894c5b00705253bd9d633bfeb1
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-10-28 16:14:51 +0900

    feat : 자동차 이름 초기화 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Application {
+
+    private static final String COMMA = ",";
     private final static String INPUT_PRINT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final static String INPUT_VALIDATION_EXCEPTION_MESSAGE = "자동차 이름 형식을 맞춰주세요. 자동차 이름은 필수 값입니다.";
+
+    public static List<String> carList;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        carList = inputCar();
+
+    public static List<String> inputCar(){
+        inputCarNamePrint();
+        String input = Console.readLine();
+        validateInput(input);
+        return Arrays.stream(input.split(COMMA))
+                .map(String::trim)
+                .toList();
+    }
 
     public static void validateInput(String input){
         if(input == null || input.trim().isEmpty()){
