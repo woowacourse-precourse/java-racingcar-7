4734584b625e79de83ca86657bcd02e00fc932ff
mjk <mjk3220@ajou.ac.kr>
2024-10-28 11:11:52 +0900

    feat: create inputCar
    자동차 입력을 위한 함수 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+
+    public static void inputCar() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String user_input = readLine();
+        saveCar(user_input);
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        inputCar();
     }
 }
