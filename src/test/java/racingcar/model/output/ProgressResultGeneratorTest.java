e0466881d87133fee0530d1f60993de31fb8e023
Chan531 <chan657312@gmail.com>
2024-10-28 17:21:55 +0900

    test(ProgressResultGeneratorTest): 다수 자동차 중간 결과 테스트

--- src/test/java/racingcar/model/output/ProgressResultGeneratorTest.java
+++ src/test/java/racingcar/model/output/ProgressResultGeneratorTest.java
@@ -13,6 +13,7 @@ class ProgressResultGeneratorTest {
 
 	private final String carMovementSymbol = "-";
 	private final String separator = " : ";
+	private final String lineSeparator = "\n";
 	private final int minRequiredValueForMovement = 4;
 
 	@Test
@@ -26,6 +27,25 @@ class ProgressResultGeneratorTest {
 
 		// when
 		String progressResult = ProgressResultGenerator.generateProgressResult(car);
+
+		// then
+		assertEquals(expectedResult, progressResult);
+	}
+
+	@Test
+	@DisplayName("다수의 자동차가 게임에 참여한 경우 중간 결과를 생성할 수 있다.")
+	void 다수의_자동차의_중간_결과를_생성한다() {
+		// given
+		String carName1 = "name1";
+		String carName2 = "name2";
+		List<Car> cars = List.of(new Car(carName1), new Car(carName2));
+		cars.getFirst().move(minRequiredValueForMovement);
+		cars.getLast().move(minRequiredValueForMovement);
+		String expectedResult = carName1 + separator + carMovementSymbol + lineSeparator
+				+ carName2 + separator + carMovementSymbol;
+
+		// when
+		String progressResult = ProgressResultGenerator.generateProgressResult(cars);
 
 		// then
 		assertEquals(expectedResult, progressResult);
