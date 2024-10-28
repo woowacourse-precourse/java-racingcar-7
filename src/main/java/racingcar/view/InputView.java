67cb66178bf11e74f7cd67d467f29dca8ce8f4d0
o-jeong <ljj020304@gmail.com>
2024-10-28 17:31:33 +0900

    refactor: 시도 횟수 입력 반환값 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,11 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String getCarNames(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)");
         return Console.readLine();
     }
-    public int getTrialCount(){
+    public String getTrialCount(){
         System.out.println("시도할 횟수는 몇 회인가요?");
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 }
