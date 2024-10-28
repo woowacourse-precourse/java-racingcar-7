df33e56635584a8b8c0fa6d539fd9472ef6033ea
Smith <125195487+sigmaith@users.noreply.github.com>
2024-10-28 19:01:57 +0900

    test(Race): 무작위 값에 따라 전진 테스트

---
+++ src/test/java/racingcar/model/RaceTest.java
@@ -0,0 +1,33 @@
+package racingcar.model;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RaceTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 0;
+
+    @Test
+    @DisplayName("랜덤값에 따라 전진하는 자동차 테스트")
+    void operateRaceTest() {
+        assertRandomNumberInRangeTest(() -> {
+                    // Given: Car 배열과 시도횟수가 입력된 상황
+                    List<Car> cars = List.of(new Car[]{new Car("sisu"), new Car("sumi"), new Car("kali")});
+                    Race race = new Race(cars, 3);
+
+                    // When: Race가 동작했을 시
+                    race.operate();
+
+                    // Then: 무작위 값이 4 이상이면 전진하고, 3 이하이면 전진하지 않는다.
+                    assertEquals(cars.stream().map(Car::getDistance).toList(), List.of(1, 2, 3));
+                }, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                STOP, MOVING_FORWARD, MOVING_FORWARD,
+                STOP, STOP, MOVING_FORWARD);
+    }
+
+
+}
