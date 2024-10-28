516392c145998271a9fcfefa7c1d0987a242e918
himinqq <alsrud0966@naver.com>
2024-10-28 17:01:43 +0900

    feat(Validator): 자동차 이름 쉼표로 구분되지 않으면 예외 발생시키는 기능 구현

--- src/main/java/racingcar/controller/Validator.java
+++ src/main/java/racingcar/controller/Validator.java
@@ -3,20 +3,34 @@ package racingcar.controller;
 import java.util.List;
 
 public class Validator {
-    public List<String> validateCarName(String input){
+    public List<String> validateCarName(String input) {
+
+        checkDelimiter(input);
+
         List<String> carNameList = List.of(input.split(","));
-        for(String name : carNameList){
-            if(name.length() > 5){
-                throw new IllegalArgumentException("이름은 5글자 이하로 입력해주세요.");
-            }
+
+        for (String name : carNameList) {
+            checkNameLength(name);
         }
         return carNameList;
     }
 
-    public int validateGameCount(int input){
-        if(input <= 0){
+    public void checkDelimiter(String name) {
+        if (!name.contains(",")) {
+            throw new IllegalArgumentException("(,)로 구분하여 입력해주세요.");
+        }
+    }
+
+    private void checkNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5글자 이하로 입력해주세요.");
+        }
+    }
+
+    public int validateGameCount(int gameCount) {
+        if (gameCount <= 0) {
             throw new IllegalArgumentException("게임횟수는 1부터 입력 가능합니다");
         }
-        return input;
+        return gameCount;
     }
 }
