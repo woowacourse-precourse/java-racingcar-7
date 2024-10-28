669c4d8f192ea14eb5b44a2adbd57e9709eaa8e7
KORyounho <dordyd@naver.com>
2024-10-28 16:03:30 +0900

    feat(Car): car update

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String carName;
     private int distance;
 
     public Car(String carName) {
         this.carName = carName;
+        isValidCarName();
     }
 
     public String getCarName() {
@@ -20,13 +23,31 @@ public class Car {
         System.out.println(getCarName() + " : " + getDistanceAsDash());
     }
 
-    private String getDistanceAsDash(){
+    private String getDistanceAsDash() {
         StringBuilder dash = new StringBuilder();
-        for(int i = 0; i<distance; i++){
+        for (int i = 0; i < distance; i++) {
             dash.append("-");
         }
         return dash.toString();
     }
 
+    public void goStraight() {
+        if (Randoms.pickNumberInRange(0, 9) > 3) {
+            distance++;
+        }
+    }
 
+    @Override
+    public String toString() {
+        return "Car{" +
+                "carName='" + carName + '\'' +
+                ", distance=" + distance +
+                '}';
+    }
+
+    private void isValidCarName() {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException(carName + "차의 이름이 5글자를 넘어갑니다.");
+        }
+    }
 }
