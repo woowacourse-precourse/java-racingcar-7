9b058cfd4d3056c5a34f335eaffebd477d7d535f
minseo <alstj778@gmail.com>
2024-10-28 16:53:12 +0900

    feat(Car): 자동차 이름 길이 검증 기능 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,12 +3,20 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
     private final String name;
     private int position;
 
     public Car(String name) {
+        validateNameLength(name);
         this.name = name;
         this.position = 0;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void tryMoveForward() {
