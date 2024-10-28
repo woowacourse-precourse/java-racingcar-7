22fb78173f9e72104be16442d96c0289e0fa3a58
Hyso <hsk7953@gmail.com>
2024-10-28 10:29:57 +0900

    refactor(OutputView) : 하드코딩된 출력문들을 private final static 으로 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,13 +6,16 @@ import racingcar.model.Car;
 import java.util.List;
 
 public class OutputView {
+    private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRIAL_COUNT = "시도할 횟수는 몇 회인가요?";
+    private static final String WINNER_VIEWPOINT = "최종 우승자 : ";
 
     public void requestCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(INPUT_CAR_NAMES);
     }
 
     public void requestTrialCount() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(INPUT_TRIAL_COUNT);
     }
 
     public void printRacing(List<Car> racingCars) {
@@ -32,7 +35,7 @@ public class OutputView {
 
     public void printWinner(List<String> winners) {
         StringBuilder result = new StringBuilder();
-        result.append("최종 우승자 : ")
+        result.append(WINNER_VIEWPOINT)
                 .append(joinWinner(winners));
         System.out.println(result);
     }
