472d1f9ee20f17b3b39853795e79c00fe40401db
ahpicl64 <ahpicl64@gmail.com>
2024-10-28 11:37:15 +0900

    feat(controller): GameController를 통한 우승자 찾기 구현
    - Application 클래스로부터 실행을 위한 run 메서드 수정
    - getWinner 메서드로부터 최대 거리를 산출, 우승자 리스트 추출

--- src/main/java/racingcar/GameController.java
+++ src/main/java/racingcar/GameController.java
@@ -1,22 +1,45 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameController {
     public final InputView input;
+    public final OutputView output;
     public final CarsManager manager;
 
-    public GameController(InputView input) {
+    public GameController(InputView input, OutputView output) {
         this.input = input;
+        this.output = output;
         this.manager = new CarsManager(input.printStart());
     }
 
-    public void playGame() {
+    public void run() {
         input.printStart();
+        input.printRounds();
+        output.showResultMessage();
+    }
+
+    public void playGame() {
         int rounds = input.printRounds();
         for (int i = 0; i < rounds; i++) {
             manager.moveAll();
             roundResult();
+        }
+    }
+
+    public void getWinner(List<Car> cars) {
+        List<Car> winner = new ArrayList<>();
+        int maxRange = 0;
+        // 승자 거리 찾기
+        for (Car car : cars) {
+            maxRange = Math.max(maxRange, car.getDistance());
+        }
+        // 승자 거리와 일치하는 사람 찾기
+        for (Car car : cars) {
+            if (maxRange == car.getDistance()) {
+                winner.add(car);
+            }
         }
     }
 
@@ -28,5 +51,4 @@ public class GameController {
             System.out.print(carName + " : " + "-".repeat(distance));
         }
     }
-
 }
