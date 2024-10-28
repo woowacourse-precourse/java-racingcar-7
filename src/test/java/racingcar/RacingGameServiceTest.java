7d470d024fda2959c6a557d9b0f59d31b814172c
KIM TAE HYEONG <thk12378905@gmail.com>
2024-10-28 23:29:01 +0900

    test: 우승자가 한명일 때 테스트 추가

--- src/test/java/racingcar/RacingGameServiceTest.java
+++ src/test/java/racingcar/RacingGameServiceTest.java
@@ -29,4 +29,24 @@ public class RacingGameServiceTest {
         assertEquals("Car2, Car3", winners); // 예상 우승자 목록을 확인합니다.
     }
 
+    @Test
+    public void testUniWinners() {
+        // Given
+        RacingGameService winnerDecider = new RacingGameService();
+        Car car1 = new Car("Car1", "------");
+        Car car2 = new Car("Car2", "------");
+        Car car3 = new Car("Car3", "--------"); // 이 차가 가장 앞선다고 가정
+
+        CarRepository.getInstance().save(car1);
+        CarRepository.getInstance().save(car2);
+        CarRepository.getInstance().save(car3);
+
+        // When
+        String winners = winnerDecider.decideWinner();
+
+        // Then
+        System.out.println("Winners: " + winners); // 반환된 우승자를 출력합니다.
+        assertEquals("Car3", winners); // 예상 우승자 목록을 확인합니다.
+    }
+
 }
