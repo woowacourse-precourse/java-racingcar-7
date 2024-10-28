617bf8ec891e6b3f0c41a02ff40860b3cf6ed397
minseo <alstj778@gmail.com>
2024-10-28 20:49:52 +0900

    test(RaceTrackTest): 우승자 선정 기능 테스트 케이스 추가

--- src/test/java/racingcar/model/RaceTrackTest.java
+++ src/test/java/racingcar/model/RaceTrackTest.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -10,6 +11,9 @@ import org.junit.jupiter.api.Test;
 import racingcar.constant.ExceptionMessage;
 
 public class RaceTrackTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
     private RaceTrack raceTrack;
 
     @BeforeEach
@@ -34,5 +38,20 @@ public class RaceTrackTest {
         assertThatThrownBy(() -> raceTrack.registerCars(names))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessage.CAR_NAMES_DUPLICATE.getMessage());
+    }
+
+    @Test
+    @DisplayName("가장 직진을 많이 한 자동차들을 우승자로 선정한다.")
+    void testDetermineWinners() {
+        List<String> names = List.of(new String[]{"test1", "test2", "test3"});
+        raceTrack.registerCars(names);
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    raceTrack.tryMoveAllCars();
+                    assertThat(raceTrack.determineWinners().size()).isEqualTo(2);
+                },
+                MOVING_FORWARD, MOVING_FORWARD, STOP
+        );
     }
 }
