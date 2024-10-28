b095502e856bc60fe90c50ae06c9b8d1ad8d9753
AppleMint98 <hong721816@konkuk.ac.kr>
2024-10-28 16:47:54 +0900

    test : 수행횟수 입력 테스트 작성
    0, 양수, 음수, 문자 별 테스트 작성

--- src/main/java/racingcar/iohandler/InputHandler.java
+++ src/main/java/racingcar/iohandler/InputHandler.java
@@ -12,9 +12,12 @@ public class InputHandler {
 
     public int getMoveCountFromUser() {
         String input = Console.readLine();
+        if (input.equals("0")) {
+            throw new IllegalArgumentException("1 이상의 숫자를 입력 해 주세요");
+        }
         boolean isNumeric = input.chars().allMatch(Character::isDigit);
         if (!isNumeric) {
-            throw new IllegalArgumentException("숫자만 입력 해 주세요.");
+            throw new IllegalArgumentException("양수만 입력 해 주세요.");
         }
         return Integer.parseInt(input);
     }
---
+++ src/test/java/racingcar/iohandler/InputHandlerTest.java
@@ -0,0 +1,73 @@
+package racingcar.iohandler;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputHandlerTest {
+
+    @AfterEach
+    void tearDown() {
+        Console.close();
+    }
+
+    @DisplayName("수행 횟수 양수 입력시 정상적으로 추출한다..")
+    @Test
+    void getMoveCountFromUserWithValidInput() {
+
+        String input = "5\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        InputHandler inputHandler = new InputHandler();
+
+        int moveCount = inputHandler.getMoveCountFromUser();
+
+        assertThat(moveCount).isEqualTo(5);
+    }
+
+    @DisplayName("수행 횟수 0 입력시 예외를 발생시킨다.")
+    @Test
+    void getMoveCountFromUserWithZeroInput() {
+
+        String input = "0\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        InputHandler inputHandler = new InputHandler();
+
+        assertThatThrownBy(() -> inputHandler.getMoveCountFromUser())
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("1 이상의 숫자를 입력 해 주세요");
+    }
+
+    @DisplayName("수행 횟수 음수 입력시 예외를 발생시킨다.")
+    @Test
+    void getMoveCountFromUserWithNegativeInput() {
+
+        String input = "-1\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        InputHandler inputHandler = new InputHandler();
+
+        assertThatThrownBy(() -> inputHandler.getMoveCountFromUser())
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("양수만 입력 해 주세요.");
+    }
+
+    @DisplayName("수행 횟수 문자 입력시 예외를 발생시킨다.")
+    @Test
+    void getMoveCountFromUserContainsCharacterInput() {
+
+        String input = "-1de\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        InputHandler inputHandler = new InputHandler();
+
+        assertThatThrownBy(() -> inputHandler.getMoveCountFromUser())
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("양수만 입력 해 주세요.");
+    }
+
+
+}
