1f8aec6057ba93cdc3c6a0c380c971bd929060e2
TalkingPotato90 <star901210@hanmail.net>
2024-10-28 11:34:00 +0900

    refactor: 테스트코드 중복 제거

--- src/test/java/racingcar/controller/CarControllerTest.java
+++ src/test/java/racingcar/controller/CarControllerTest.java
@@ -3,9 +3,13 @@ package racingcar.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import racingcar.model.Car;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
@@ -38,32 +42,34 @@ class CarControllerTest {
         assertThat(carController.createAllCars(List.of("pobi", "woni", "jun")).size()).isEqualTo(3);
     }
 
-    @Test
-    @DisplayName("자동차 최대 숫자 초과")
-    void exceedMaxCar() {
-        assertThatThrownBy(() -> carController.validateCarNames(List.of("pobi", "woni", "jun", "four", "five", "six")))
-                .isInstanceOf(IllegalArgumentException.class);
+    static Stream<Arguments> createValidCar() {
+        return Stream.of(
+                Arguments.of(List.of("pobi", "woni", "jun", "four", "five"), "자동차의 최대 숫자 이내"),
+                Arguments.of(List.of("pobi", "povi", "pob1"), "자동차 이름 중복 없음")
+        );
     }
 
-    @Test
-    @DisplayName("자동차 최대 숫자 이내")
-    void withinMaxCar() {
-        assertThatCode(() -> carController.validateCarNames(List.of("pobi", "woni", "jun", "four", "five")))
+    @ParameterizedTest(name = "{1}")
+    @MethodSource("createValidCar")
+    @DisplayName("자동차 이름이 유효하면 예외가 발생하지 않는다")
+    void validCarName(List<String> car, String testCase) {
+        assertThatCode(() -> carController.validateCarNames(car))
                 .doesNotThrowAnyException();
     }
 
-    @Test
-    @DisplayName("자동차 이름 중복")
-    void duplicateCarName() {
-        assertThatThrownBy(() -> carController.validateCarNames(List.of("pobi", "pobi")))
-                .isInstanceOf(IllegalArgumentException.class);
+    static Stream<Arguments> createInvalidCar() {
+        return Stream.of(
+                Arguments.of(List.of("pobi", "woni", "jun", "four", "five", "six"), "자동차의 최대 숫자 초과"),
+                Arguments.of(List.of("pobi", "pobi"), "자동차 이름 중복")
+        );
     }
 
-    @Test
-    @DisplayName("자동차 이름 중복없음")
-    void uniqueCarName() {
-        assertThatCode(() -> carController.validateCarNames(List.of("pobi", "povi", "pob1")))
-                .doesNotThrowAnyException();
+    @ParameterizedTest(name = "{1}")
+    @MethodSource("createInvalidCar")
+    @DisplayName("자동차 이름이 잘못되면 예외가 발생한다")
+    void invalidCarName(List<String> car, String testCase) {
+        assertThatThrownBy(() -> carController.validateCarNames(car))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
