54b19e4520038100832ffc7544a348904116647a
dayeondayeon <dayeon2438@gmail.com>
2024-10-28 21:49:59 +0900

    chores : 불필요한 주석 제거

--- src/main/java/racingcar/service/CarCountService.java
+++ src/main/java/racingcar/service/CarCountService.java
@@ -6,7 +6,7 @@ public class CarCountService {
             throw new IllegalArgumentException("유효하지 않은 시도 횟수 입니다.");
         }
         try {
-            return Integer.parseInt(count); // 정수와 실수 모두 처리
+            return Integer.parseInt(count);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("시도 횟수는 숫자 형태의 입력만 가능합니다. ");
         }
