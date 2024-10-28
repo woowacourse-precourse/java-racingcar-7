af2ca826640621e71a56c514de7d0d096b2bda93
MinJae00 <exceedmin@gmail.com>
2024-10-28 23:01:14 +0900

    feat: Validator 테스트 코드

--- src/test/java/racingcar/validator/ValidatorTest.java
+++ src/test/java/racingcar/validator/ValidatorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.view.InputView;
 
-class ValidatorTest2 {
+class ValidatorTest {
     private Validator validator;
 
     @BeforeEach
@@ -17,21 +17,21 @@ class ValidatorTest2 {
 
     @Test
     @DisplayName("유저 이름이 빈 문자열일 때 예외 발생")
-    void testIsEmptyName_ShouldThrowException_WhenUserNameIsEmpty() {
+    void testIsEmptyName() {
         assertThatThrownBy(() -> validator.userNameValidate(""))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("유저 이름이 최대 길이를 초과할 때 예외 발생")
-    void testIsExceedMaxNameSize_ShouldThrowException_WhenUserNameExceedsMaxLength() {
-        assertThatThrownBy(() -> validator.userNameValidate("use"))
+    void testIsExceedMaxNameSize() {
+        assertThatThrownBy(() -> validator.userNameValidate("username_too_long"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("입력 문자열이 빈 문자열일 때 예외 발생")
-    void testIsEmptyInput_ShouldThrowException_WhenInputStringIsEmpty() {
+    void testIsEmptyInput() {
         validator = new Validator("");
         assertThatThrownBy(validator::userInputValidate)
                 .isInstanceOf(IllegalArgumentException.class);
@@ -39,7 +39,7 @@ class ValidatorTest2 {
 
     @Test
     @DisplayName("입력 문자열의 처음이나 끝에 쉼표가 있을 때 예외 발생")
-    void testIsCommaInFrontOrBack_ShouldThrowException_WhenCommaAtStartOrEnd() {
+    void testIsCommaInFrontOrBack() {
         validator = new Validator(",user1,user2,user3,");
         assertThatThrownBy(validator::userInputValidate)
                 .isInstanceOf(IllegalArgumentException.class);
@@ -47,7 +47,7 @@ class ValidatorTest2 {
 
     @Test
     @DisplayName("입력 문자열이 최대 길이를 초과할 때 예외 발생")
-    void testIsExceedMaxInputSize_ShouldThrowException_WhenInputStringExceedsMaxLength() {
+    void testIsExceedMaxInputSize() {
         // 5100자 이상의 문자열을 생성하여 테스트
         String longInputString = "a".repeat(5100);
         validator = new Validator(longInputString);
@@ -57,7 +57,7 @@ class ValidatorTest2 {
 
     @Test
     @DisplayName("tryNumber가 0 이하 또는 1000 초과일 때 예외 발생")
-    void testIsValidateNumber_ShouldThrowException_WhenTryNumberOutOfRange() {
+    void testIsValidateNumber() {
         // InputView.tryNumber를 0 이하 또는 1000 초과로 설정하고 테스트
         InputView.tryNumber = 0;
         assertThatThrownBy(Validator::isValidateNumber)
@@ -70,14 +70,14 @@ class ValidatorTest2 {
 
     @Test
     @DisplayName("유효한 입력이 주어졌을 때 예외 발생하지 않음")
-    void testUserInputValidate_ShouldNotThrowException_WhenInputIsValid() {
+    void testUserInputValidate_True() {
         validator = new Validator("user1,user2,user3");
         validator.userInputValidate();
     }
 
     @Test
     @DisplayName("유효한 유저 이름이 주어졌을 때 예외 발생하지 않음")
-    void testUserNameValidate_ShouldNotThrowException_WhenUserNameIsValid() {
+    void testUserNameValidate_True() {
         validator.userNameValidate("user1");
     }
 }
