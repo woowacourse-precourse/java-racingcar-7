5d5becf5fba5e956e4c236019226a2bc4d9c952d
capDoYeonLee <ghtyu21@naver.com>
2024-10-28 14:27:15 +0900

    refactor: 사용하지 않는 인스턴스 변수 제거

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,18 +1,12 @@
 package racingcar;
 
-import java.util.*;
-
 public class Car {
 
     private String carName;
     private int racingCount = 0;
-    private Map<Integer, List<String>> result = new HashMap<>();
 
     public Car(String carName) {
         this.carName = carName;
-    }
-
-    public Car() {
     }
 
     public int getRacingCount() {
