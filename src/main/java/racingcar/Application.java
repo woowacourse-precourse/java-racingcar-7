fa23dff2a467e1174d9686b2a38582d9816a619a
hojooo <ghwn5833@gmail.com>
2024-10-28 15:32:02 +0900

    feat: Implement final result display and winner determination in GameLogic and combine in application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputNames = Console.readLine();
+        List<String> carNames = inputHandler.getNames(inputNames);
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String inputCount = Console.readLine();
+        int count = Integer.parseInt(inputCount);
+
+        GameLogic gameLogic = new GameLogic(carNames, count);
+        gameLogic.race();
+        gameLogic.finalResults();
     }
 }
--- src/main/java/racingcar/GameLogic.java
+++ src/main/java/racingcar/GameLogic.java
@@ -3,6 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameLogic {
     private final List<Car> cars = new ArrayList<>();
@@ -37,5 +38,22 @@ public class GameLogic {
             System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
         }
         System.out.println();
+    }
+
+    public void finalResults() {
+        List<String> winners = determineWinners();
+        System.out.println("최종 우승자 : " + String.join(", ", winners));  // 우승자를 쉼표로 구분하여 출력
+    }
+
+    private List<String> determineWinners() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)  // 최장 거리와 일치하는 자동차 필터
+                .map(Car::getName)                               // 해당 자동차의 이름 반환
+                .collect(Collectors.toList());                   // 리스트로 수집
     }
 }
