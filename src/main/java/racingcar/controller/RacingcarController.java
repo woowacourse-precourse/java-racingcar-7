44faadf2e79d5215f8a3c49f808c6e80e8b46a7f
chae02yo <chae02kim@naver.com>
2024-10-28 17:19:41 +0900

    feat: handle exception for user input
    - handle exception where car name is left blank

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -11,9 +11,12 @@ public class RacingcarController {
         this.racingService = racingService;
     }
 
-    public void getUserInput() {
+    public void getUserInput() throws IllegalArgumentException{
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String namesOfCars = Console.readLine();
+        if (namesOfCars.isBlank()) {
+            throw new IllegalArgumentException("경주할 자동차 이름을 입력하세요.");
+        }
         racingService.separateCarNames(namesOfCars);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
