34f82ba8090551d53c9d05946adc5a234cb6e4ca
seohee-P <p03061180@gmail.com>
2024-10-28 23:22:21 +0900

    test: 우승자 및 실행 결과 출력 형식 테스트 추가

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,24 +1,51 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RacingCarTest {
     private RacingCar racingCar;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @BeforeEach
     public void setup() {
         racingCar = new RacingCar();
+        System.setOut(new PrintStream(outContent));
     }
 
     @AfterEach
     void tearDown() {
+        System.setOut(System.out);
         Console.close();
+    }
+
+    @Test
+    void 실행_결과_출력_테스트() {
+        String input = "pobi,woni\n1\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        racingCar.run();
+
+        String output = outContent.toString();
+        assertTrue(output.contains("pobi : "));
+        assertTrue(output.contains("woni : "));
+    }
+
+    @Test
+    void 우승자_출력_테스트() {
+        String input = "pobi,woni\n1\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        racingCar.run();
+
+        String output = outContent.toString();
+        assertTrue(output.contains("최종 우승자 : "));
     }
 
     @Test
