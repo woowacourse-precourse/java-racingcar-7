0aa162b776ed1093451e3f80e7f7225ee0a30a6e
SuJeong <sujenghong@gmail.com>
2024-10-28 23:07:54 +0900

    feat(winner Display): implement final winner(s) display after all rounds

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -17,7 +17,7 @@ public class Application {
             racingGame.printRaceStatus(cars);
         }
 
-
+        racingGame.printWinners(cars);
     }
 }
 
@@ -64,7 +64,15 @@ class RacingGame {
         System.out.println();
     }
 
+    public void printWinners(List<Car> cars) {
+        int maxPosition = cars.stream().mapToInt(Car::getPosition).max().orElse(0);
+        List<String> winners = cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
 
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
 }
 
 class Car {
