a68f9017f9620013468570fbfa84499fcd104d92
belight2 <chlwnsgur090@gmail.com>
2024-10-28 16:50:05 +0900

    feat: 입력된 자동차이름의 유효성 검사 함수 추가
    - validateCarName(): 자동차이름이 유효한지 검사하는 함수
    - isEmptyList(): 자동차 리스트에 아무것도 없을 시 예외발생
    - isNullorEmpty(): 자동차이름이 null이거나 빈 문자열이면 예외발생
    - isValidCarNameLength(): 자동차이름의 길이가 5 초과일 시 예외발생
    - hasDuplicateCarName(): 중복인 자동차 이름이 있을 시 예외발생

--- src/main/java/validation/InputValidator.java
+++ src/main/java/validation/InputValidator.java
@@ -1,7 +1,50 @@
 package validation;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InputValidator {
 
     public InputValidator(){
     }
+
+    public void validateCarName(final String cars){
+        List<String> carNameList = Arrays.asList(cars.split(","));
+        isEmptyList(carNameList);
+        for(String carName : carNameList){
+            isNullOrEmpty(carName);
+            isValidCarNameLength(carName);
+        }
+        hasDuplicateCarName(carNameList);
+    }
+
+    private void isEmptyList(List<String> carList){
+        if(carList.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isNullOrEmpty(String carName){
+        if(carName == null || carName.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isValidCarNameLength(String carName){
+        if(carName.length() > 5){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void hasDuplicateCarName(List<String> carList){
+        Set<String> nameSet = new HashSet<>();
+        for(String carName : carList){
+            if(!nameSet.add(carName)){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
 }
