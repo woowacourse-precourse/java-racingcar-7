951f178a8d245c822e253fecf6661ac86764a202
yallo428 <yallo428@naver.com>
2024-10-28 13:26:53 +0900

    feat(Input) : 플레이 횟수 입력 구현
    플레이 횟수 입력 구현:
    - 입력값이 음수인지 검증
    - 입력값이 숫자인지 검증

---
+++ src/main/java/racingcar/io/Input.java
@@ -0,0 +1,33 @@
+package racingcar.io;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
+import racingcar.vo.ExceptionMessage;
+
+public class Input {
+
+    public int readPlayCount() {
+        String playCount = Console.readLine();
+
+        validateNotNumber(playCount);
+        validateNotPositive(playCount);
+
+        return Integer.valueOf(playCount);
+    }
+
+    private void validateNotNumber(String playCount) {
+        boolean isNotNumber = !Pattern.matches("^(0|[0-9]+)$", playCount);
+
+        if (isNotNumber) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_NOT_NUMBER.getMessage());
+        }
+    }
+
+    private void validateNotPositive(String playCount) {
+        boolean isNotPositive = Integer.parseInt(playCount) < 1;
+
+        if (isNotPositive) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_NEGATIVE_NUMBER.getMessage());
+        }
+    }
+}
