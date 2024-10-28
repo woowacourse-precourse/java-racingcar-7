fdde6196f287e55f1c7769010f279b3c4529281b
yooo <ghddbtl@gmail.com>
2024-10-28 16:08:58 +0900

    feat: add RacingGameService and methods
    - 라운드 수 만큼 각 자동차에 숫자 돌리기
    - 자동차 리스트를 전진수 기준으로 내림차순 정령
    - 우승자 추출하기

---
+++ src/main/java/racingcar/model/RacingGameService.java
@@ -0,0 +1,52 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class RacingGameService {
+    List<CarDto> cars;
+
+
+    public RacingGameService(List<CarDto> cars) {
+        this.cars = cars;
+    }
+
+    public List<CarDto> getCars() {
+        return cars;
+    }
+
+    public void gameStart() {
+        cars.forEach(car -> {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                int index = cars.indexOf(car);
+                CarDto movementCar = CarService.forwardMovement(car, car.getForward() + 1);
+                cars.set(index, movementCar);
+            }
+        });
+    }
+
+    private void sortByForwardMovement() {
+        cars.sort(Comparator.comparingInt(CarDto::getForward).reversed());
+    }
+
+    public List<CarDto> whoIsWinners() {
+        List<CarDto> winners;
+        sortByForwardMovement();
+        int highScore = cars.getFirst().getForward();
+
+        winners = cars.stream()
+                      .filter(car -> car.getForward() == highScore)
+                      .toList();
+
+        return winners;
+    }
+
+    public static Boolean isWinnersSameName(List<CarDto> cars) {
+        Map<String, Long> nameCollect = cars.stream()
+                                               .collect(Collectors.groupingBy(CarDto::getName, Collectors.counting()));
+
+        return nameCollect.values().stream().anyMatch(count -> count > 1);
+    }
+}
