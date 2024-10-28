bbadbc352468330980e0d299721556fe393a38d9
kingjinyong <wlsdydwhswkf@gmail.com>
2024-10-28 09:36:48 +0900

    refactor(CarRacingControllerTest): 의존성 주입 리팩토링으로 인한 테스트 코드 수정

--- src/test/java/racingcar/controller/CarRacingControllerTest.java
+++ src/test/java/racingcar/controller/CarRacingControllerTest.java
@@ -3,12 +3,18 @@ package racingcar.controller;
 import java.util.ArrayList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import racingcar.model.Register;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class CarRacingControllerTest {
 
     @Test
     void 자동차_문자열_쉼표_기준_분리() {
-        CarRacingController carRacingController = new CarRacingController();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Register register = new Register();
+        CarRacingController carRacingController = new CarRacingController(inputView, outputView, register);
         ArrayList<String> expected = new ArrayList<>();
         expected.add("진용1");
         expected.add("진용2");
