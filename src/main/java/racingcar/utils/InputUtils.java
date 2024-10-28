7fd3591ac7d3d7bf9d261cb8387defa72623edac
sonshn <sonshn@naver.com>
2024-10-28 21:18:32 +0900

    feat(ParseUtils): 자동차 이름/시도 횟수 문자열 분리 구현

---
+++ src/main/java/racingcar/utils/InputUtils.java
@@ -0,0 +1,58 @@
+package racingcar.utils;
+
+import racingcar.exception.InputException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class InputUtils {
+
+    public List<String> parseCarNames(String carName) {
+
+        if (carName.isEmpty()) {
+            throw new IllegalArgumentException(String.valueOf(InputException.EMPTY_RACING_CAR_NAME));
+        }
+
+        String[] carNames = carName.split(",");
+        List<String> listOfCarNames = new ArrayList<>(Arrays.asList(carNames));
+        List<String> finalListOfCarNames = new ArrayList<>();
+
+        for (String individualCarName : listOfCarNames) {
+            if (individualCarName.length() > 5) {
+                throw new IllegalArgumentException(String.valueOf(InputException.TOO_LONG_RACING_CAR_NAME));
+            }
+
+            if (finalListOfCarNames.contains(individualCarName)) {
+                throw new IllegalArgumentException(String.valueOf(InputException.DUPLICATE_RACING_CAR_NAME));
+            }
+
+            finalListOfCarNames.add(individualCarName);
+        }
+
+        return finalListOfCarNames;
+    }
+
+    public int parseAttemptValue(String attemptValue) {
+
+        if (attemptValue.isEmpty()) {
+            throw new IllegalArgumentException(String.valueOf(InputException.ATTEMPT_VALUE_IS_EMPTY));
+        }
+
+        try {
+            int finalAttemptValue = Integer.parseInt(attemptValue);
+
+            if (finalAttemptValue == 0) {
+                throw new IllegalArgumentException(String.valueOf(InputException.ATTEMPT_VALUE_IS_ZERO));
+            } else if (finalAttemptValue < 0) {
+                throw new IllegalArgumentException(String.valueOf(InputException.ATTEMPT_VALUE_IS_NEGATIVE));
+            } else {
+                return finalAttemptValue;
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(String.valueOf(InputException.ATTEMPT_VALUE_IS_INVALID));
+        }
+
+    }
+
+}
