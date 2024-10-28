d029a22bf8b4dd151ac275b2af4a9286372200e2
Seol-Munhyeok <abc1234568@naver.com>
2024-10-28 16:00:30 +0900

    typo : 출력문 오타 수정

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -11,7 +11,7 @@ public class InputHandler {
 
     List<String> getCarNames() {
         String carNames;
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         carNames = Console.readLine();
         return parseCarNames(carNames);
     }
