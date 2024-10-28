3214170740eb33ddb8d6071f2bdc22b2c96d0ef5
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-10-28 18:03:21 +0900

    feat(Application): initCollections함수 추가하기
    collection값들을 초기화 해주는 initCollections함수를 추가하여 적용한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -129,10 +129,16 @@ public class Application {
         calculateFinal();
         displayWinners();
     }
+    public static void initCollections(){
+        Application.participantList.clear();
+        Application.participantScore.clear();
+        Application.winners.clear();
+    }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         try {
+            initCollections();
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             String participants = Console.readLine();
             splitParticipantString(participants);
