8af7df08b7ca4e5f2a90b5067011b86379b66962
임채연 <dlacodus0407@gmail.com>
2024-10-28 14:31:57 +0900

    refactor(GameServiceTest): refactor test code
    - BeforeEach를 사용해서 반복되는 코드를 삭제합니다.
    - 전역적으로 사용하는 변수를 상수처리합니다.
    - containsExactlyInAnyOrder메서드를 사용해서 크기와 내부 요소 한번에 확인하도록 합니다.

--- src/test/java/racingcar/GameServiceTest.java
+++ src/test/java/racingcar/GameServiceTest.java
@@ -5,24 +5,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class GameServiceTest {
+    private final Integer ROUND = 5;
+    private GameService gameService;
+    private List<String> racingCarsName;
+
+    @BeforeEach
+    public void setUp() {
+        gameService  = new GameService();
+        racingCarsName = new ArrayList<>();
+        racingCarsName.add("woodz");
+        racingCarsName.add("yuze");
+        racingCarsName.add("karina");
+    }
+
     @Nested
     @DisplayName("게임 로직을 실행합니다.")
     class ExecuteLogicTest {
-        private final Integer ROUND = 5;
-        private final List<String> racingCarsName = new ArrayList<>();
         @Test
         void 게임핵심로직을_실행합니다() {
-            //given
-            GameService gameService = new GameService();
-            racingCarsName.add("woodz");
-            racingCarsName.add("dean");
-            racingCarsName.add("yuze");
-
             // when
             gameService.init(ROUND, racingCarsName);
             List<RacingCar> racingCars = gameService.executeLogic();
@@ -35,17 +41,8 @@ class GameServiceTest {
     @Nested
     @DisplayName("게임을 초기화 합니다.")
     class InitGameTest {
-        private final Integer ROUND = 5;
-        private final List<String> racingCarsName = new ArrayList<>();
-
         @Test
         void 게임을_초기화합니다() {
-            // given
-            GameService gameService = new GameService();
-            racingCarsName.add("woodz");
-            racingCarsName.add("dean");
-            racingCarsName.add("yuze");
-
             // when
             gameService.init(ROUND, racingCarsName);
 
@@ -56,12 +53,6 @@ class GameServiceTest {
 
         @Test
         void 차를_초기화합니다() {
-            // given
-            GameService gameService = new GameService();
-            racingCarsName.add("woodz");
-            racingCarsName.add("dean");
-            racingCarsName.add("yuze");
-
             // when
             List<RacingCar> racingCars = gameService.createRacingCars(racingCarsName);
 
@@ -76,67 +67,46 @@ class GameServiceTest {
     @Nested
     @DisplayName("selectWinner()를 테스트합니다.")
     class SelectWinnerTest {
-        private final Integer ROUND = 5;
-        private final List<String> racingCarsName = new ArrayList<>();
-
         @Test
         void score가_가장_높은사람이_1명일_경우_우승자는_1명이다() {
             // given
-            GameService gameService = new GameService();
-            racingCarsName.add("woodz");
-            racingCarsName.add("dean");
-            racingCarsName.add("yuze");
             gameService.init(ROUND, racingCarsName);
+            gameService.getGame().getRacingCars().get(0).addScore();
 
             // when
-            gameService.getGame().getRacingCars().get(0).addScore();
             List<String> winners = gameService.selectWinner();
 
             // then
-            assertThat(winners.size()).isEqualTo(1);
-            assertThat(winners.get(0)).isEqualTo("woodz");
+            assertThat(winners).containsExactlyInAnyOrder("woodz");
         }
 
         @Test
         void score가_가장_높은사람이_2명일_경우_우승자는_2명이다() {
             // given
-            GameService gameService = new GameService();
-            racingCarsName.add("woodz");
-            racingCarsName.add("dean");
-            racingCarsName.add("yuze");
             gameService.init(ROUND, racingCarsName);
-
-            // when
             gameService.getGame().getRacingCars().get(0).addScore();
             gameService.getGame().getRacingCars().get(1).addScore();
+
+            // when
             List<String> winners = gameService.selectWinner();
 
             // then
-            assertThat(winners.size()).isEqualTo(2);
-            assertThat(winners.get(0)).isEqualTo("woodz");
-            assertThat(winners.get(1)).isEqualTo("dean");
+            assertThat(winners).containsExactlyInAnyOrder("woodz", "yuze");
         }
 
         @Test
         void 참가자의_score가_모두_동일하면_우승자는_전체이다() {
             // given
-            GameService gameService = new GameService();
-            racingCarsName.add("woodz");
-            racingCarsName.add("dean");
-            racingCarsName.add("yuze");
             gameService.init(ROUND, racingCarsName);
+            for (RacingCar car : gameService.getGame().getRacingCars()) {
+                car.addScore();
+            }
 
             // when
-            gameService.getGame().getRacingCars().get(0).addScore();
-            gameService.getGame().getRacingCars().get(1).addScore();
-            gameService.getGame().getRacingCars().get(2).addScore();
             List<String> winners = gameService.selectWinner();
 
             // then
-            assertThat(winners.size()).isEqualTo(3);
-            assertThat(winners.get(0)).isEqualTo("woodz");
-            assertThat(winners.get(1)).isEqualTo("dean");
-            assertThat(winners.get(2)).isEqualTo("yuze");
+            assertThat(winners).containsExactlyInAnyOrder("woodz", "yuze", "karina");
         }
     }
 }
