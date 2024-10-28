a4b1ec8a367825fa70ccb4ae6b2070eb48e3ece7
youjae <youjae0803@gmail.com>
2024-10-28 15:38:19 +0900

    refactor(MoveCondition): 상수를 나타내는 변수 이름 대문자로 변경

--- src/main/java/racingcar/model/MoveCondition.java
+++ src/main/java/racingcar/model/MoveCondition.java
@@ -1,16 +1,16 @@
 package racingcar.model;
 
 public class MoveCondition {
-    private final int minNumber;
+    private final int MIN_NUMBER;
     private NumberGenerator numberGenerator;
 
     public MoveCondition(int minNumber){
-        this.minNumber = minNumber;
+        this.MIN_NUMBER = minNumber;
         this.numberGenerator = new NumberGenerator(0, 9);
     }
 
     public Boolean isSatisfied(){
         int randomNumber = this.numberGenerator.getRandomNumber();
-        return randomNumber >= this.minNumber;
+        return randomNumber >= this.MIN_NUMBER;
     }
 }
