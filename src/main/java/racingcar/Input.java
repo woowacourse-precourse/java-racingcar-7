c2d7f7758d0dcacd8b8db9bab35da321c3c1fd13
JEN <ekffuclxk@gmail.com>
2024-10-28 14:29:54 +0900

    feat(input): trim 메서드 구현

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Input {
@@ -8,17 +9,26 @@ public class Input {
     public List<String> getCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputCarNames = Console.readLine();
-        return splitCarNames(inputCarNames);
+        List<String> carNames = splitCarNames(inputCarNames);
+        return trimCarNames(carNames);
     }
 
     public int getGameCount(){
         System.out.println("시도할 횟수는 몇 회인가요?");
         String inputGameCount = Console.readLine();
-        return Integer.parseInt(inputGameCount);
+        return convertStrToInt(inputGameCount);
     }
 
     public static List<String> splitCarNames(String inputCarNames){
         return List.of(inputCarNames.split(","));
+    }
+
+    public static List<String> trimCarNames(List<String> carNames){
+        List<String> trimNames = new ArrayList<>();
+        for (String carName : carNames) {
+            trimNames.add(carName.trim());
+        }
+        return trimNames;
     }
 
     public static int convertStrToInt(String inputGameCount){
