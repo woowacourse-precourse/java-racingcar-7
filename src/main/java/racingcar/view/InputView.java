d954774124a020d781bf60203ad994336398ebbe
rbgusdlza <rbgusdlza@naver.com>
2024-10-28 21:35:58 +0900

    feat: 사용자의 입력을 받는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,11 +1,82 @@
-package calculator.view;
+package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.Car;
+import racingcar.Cars;
 
-public class InputView {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
-    public static String read() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return Console.readLine();
+import static racingcar.error.RacingException.*;
+
+public abstract class InputView {
+
+    private InputView() {
+    }
+
+    public static Cars readCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        String carNames = Console.readLine();
+        checkIfCarNamesValid(carNames);
+        Cars cars = new Cars();
+        Arrays.stream(carNames.split(","))
+                .map(Car::new)
+                .forEach(cars::add);
+        return cars;
+    }
+
+    public static int readCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+
+        String count = Console.readLine();
+        checkIfCarNamesValid(count);
+        return Integer.parseInt(count);
+    }
+
+    public void checkIfCountValid(String count) {
+        isInputEmpty(count);
+        if (!count.matches("^[1-9][0-9]*$")) {
+            throw new IllegalArgumentException(WRONG_COUNT_ERROR.getDescription());
+        }
+    }
+
+    private static void checkIfCarNamesValid(String carNames) {
+        isInputEmpty(carNames);
+        isWrongFormat(carNames);
+        isCarNameLengthValid(carNames);
+        isCarNameDuplicate(carNames);
+    }
+
+    private static void isInputEmpty(String carNames) {
+        if (carNames.isEmpty()) {
+            throw new IllegalArgumentException(NO_INPUT_ERROR.getDescription());
+        }
+    }
+
+    private static void isWrongFormat(String carNames) {
+        if (!carNames.matches("^(?!,)(?!.*,,)[a-zA-Z0-9,]+(?<! ,)$")) {
+            throw new IllegalArgumentException(WRONG_FORMAT_ERROR.getDescription());
+        }
+    }
+
+    private static void isCarNameLengthValid(String carNames) {
+        String[] names = carNames.split(",");
+        for (String name : names) {
+            if (name.isEmpty() || name.length() > 5) {
+                throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR.getDescription());
+            }
+        }
+    }
+
+    private static void isCarNameDuplicate(String carNames) {
+        String[] names = carNames.split(",");
+        Set<String> set = new HashSet<>();
+        for (String name : names) {
+            if (!set.add(name)) {
+                throw new IllegalArgumentException(DUPLICATE_CAR_NAME_ERROR.getDescription());
+            }
+        }
     }
 }
