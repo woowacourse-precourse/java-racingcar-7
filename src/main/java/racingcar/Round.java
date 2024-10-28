285684b36bff18c0dd001f2a070d0f65bcfa8788
sseopsseop <ponde119@outlook.com>
2024-10-28 16:53:13 +0900

    fix: Round 결과 출력 메서드 추가 및 기존 코드 수정 차의 인덱스 관리를 위해서 CarList 저장하는 필드 추가함. 해당 라운드의 결과 값을 출력하기 위해 String 형태로 반환하는 printResult 메서드 추가

--- src/main/java/racingcar/Round.java
+++ src/main/java/racingcar/Round.java
@@ -1,16 +1,27 @@
 package racingcar;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Round {
+    private List<Car> cars;
     private Map<Car, Integer> results;
 
     public Round(){
         this.results = new HashMap<Car, Integer>();
     }
 
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
     public void addResult(Car car){
+        this.cars.add(car);
         this.results.put(car, car.getMoveCount());
     }
 
@@ -20,5 +31,21 @@ public class Round {
 
     public Map<Car, Integer> getResults(){
         return this.results;
+    }
+
+    public String printResult(){
+        StringBuilder result = new StringBuilder();
+        for(Car car : cars){
+            result.append(car.getCarName())
+                    .append(" : ")
+                    .append(printMovingTextureFromMoveCount(results.get(car)))
+                    .append("\n");
+        }
+
+        return result.toString();
+    }
+
+    private String printMovingTextureFromMoveCount(int moveCount){
+        return "-".repeat(Math.max(0, moveCount));
     }
 }
