f29d06306a229423faef2382ae8533d5dc146c8d
jijongkwon <help3451@naver.com>
2024-10-28 21:46:31 +0900

    feat(service) : 레이싱 실행결과 출력 메서드 줄바꿈 추가

--- src/main/java/racingcar/application/service/history/RacingHistoryService.java
+++ src/main/java/racingcar/application/service/history/RacingHistoryService.java
@@ -3,6 +3,7 @@ package racingcar.application.service.history;
 import java.util.List;
 import racingcar.domain.car.RacingCar;
 import racingcar.domain.history.RacingHistory;
+import racingcar.infrastructure.persistence.InMemoryRacingCarRepository;
 import racingcar.infrastructure.persistence.InMemoryRacingHistoryRepository;
 
 public class RacingHistoryService {
@@ -20,12 +21,16 @@ public class RacingHistoryService {
     // TODO : 실행결과 출력하기
     public void printRacingResult(){
         List<RacingHistory> racingHistories =  historyRepository.getRacingHistory();
+        int numberOfCars = InMemoryRacingCarRepository.getInstance().getSize();
 
-        for(RacingHistory racingHistory : racingHistories) {
+        for(int i = 0; i < racingHistories.size(); i++) {
+            RacingHistory racingHistory = racingHistories.get(i);
             System.out.print(racingHistory.getRacingCarName() + " : ");
             System.out.println(racingHistory.getPrintDistance());
+
+            if((i + 1) % numberOfCars == 0) {
+                System.out.println();
+            }
         }
     }
-
-
 }
