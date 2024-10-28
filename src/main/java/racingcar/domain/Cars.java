8179fe0b0d61cdfb8995bc3e4113162fd1b0becd
younyoung <00younyoung@naver.com>
2024-10-28 19:44:46 +0900

    Feat: 최종적으로 가장 많이 전진한 자동차(들)를 우승자로 확정 Cars 클래스에 getWinnerPosition과 getWinnerList 메서드를 통해 가장 많이 전진한 자동차(들)를 구하고, 우승자로 확정한다.

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -23,6 +23,20 @@ public record Cars(List<Car> carList) {
         );
     }
 
+    public List<Car> getWinnerList() {
+        Integer winnerPosition = getWinnerPosition();
+        return carList.stream()
+            .filter(car -> car.isSamePosition(winnerPosition))
+            .collect(Collectors.toList());
+    }
+
+    private Integer getWinnerPosition() {
+        return carList.stream()
+            .map(Car::getPosition)
+            .max(Integer::compareTo)
+            .orElse(0);
+    }
+
     @Override
     public String toString() {
         return carList.stream()
