e6323dc9beb7346f753f0725f8ce465bde94b075
SSU <hshwan0406@gmail.com>
2024-10-28 19:49:58 +0900

    feat: 받은 입력 값으로 레이싱 차 생성 메서드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String racingCarsNames = Console.readLine();
+        List<Car> carArray = createCar(racingCarsNames);
+
+    }
+
+    private static List<Car> createCar(String racingCarsNames) {
+
+        List<Car> carArray =  new ArrayList<>();
+        String[] carsNames = racingCarsNames.split(":");
+
+        for (String carName : carsNames) {
+            carArray.add(new Car(carName));
+        }
+
+        return carArray;
     }
 }
