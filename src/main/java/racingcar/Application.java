0243e53fef12fa13046312fa90bb1d50465369d3
GeonHui Lee <spongging@gmail.com>
2024-10-28 13:01:20 +0900

    [fix] 예외사항 발생 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,17 +10,13 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            List<String> carNames = InputView.readCarNames();
-            int attemptCount = InputView.readAttemptCount();
+        List<String> carNames = InputView.readCarNames();
+        int attemptCount = InputView.readAttemptCount();
 
-            RacingGame racingGame = new RacingGame(carNames, attemptCount);
-            racingGame.run();
+        RacingGame racingGame = new RacingGame(carNames, attemptCount);
+        racingGame.run();
 
-            List<Car> winners = racingGame.getWinners();
-            OutputView.printWinners(winners);
-        } catch (IllegalArgumentException e) {
-            ExceptionHandler.handleException(e);
-        }
+        List<Car> winners = racingGame.getWinners();
+        OutputView.printWinners(winners);
     }
 }
