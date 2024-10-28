901f0722c2cccb0133210f4502b54d994d3e8ae5
AhnJoonSung <tjdtna01@naver.com>
2024-10-28 14:09:14 +0900

    Refactoring: input 로직 리팩토링

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,19 +7,35 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
-
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int turn = Integer.parseInt(Console.readLine());
-        System.out.println();
-
-        String[] carNames = input.split(",");
-        List<Car> cars = Arrays.stream(carNames)
-                .map(Car::new)
-                .toList();
+        List<Car> cars = inputCarNames();
+        int turn = inputTurnCount();
 
         RacingGame game = new RacingGame(cars, turn);
         game.start();
+    }
+
+    private static List<Car> inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNamesInput = Console.readLine();
+
+        if (carNamesInput.isEmpty()) {
+            throw new IllegalArgumentException("빈 문자열을 입력하였습니다.");
+        }
+
+        return Arrays.stream(carNamesInput.split(","))
+                .map(Car::new)
+                .toList();
+    }
+
+    private static int inputTurnCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String turnInput = Console.readLine();
+        System.out.println();
+
+        try {
+            return Integer.parseInt(turnInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 횟수는 정수형으로 입력하여야 합니다.");
+        }
     }
 }
