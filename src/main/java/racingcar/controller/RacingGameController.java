ce1486b51e12a127bee424fcd1c1540715742556
coolman0123 <socool4m@naver.com>
2024-10-28 19:48:14 +0900

    refactor: 불필요한 주석 삭제
    * RacingGameController.java

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -4,29 +4,26 @@ import java.util.List;
 import racingcar.model.Car;
 import racingcar.model.Race;
 import racingcar.util.CarInputConverter;
-import racingcar.util.RaceInputConverter;
+import racingcar.util.RoundInputConverter;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingGameController {
     public static void run() {
-        //참가자 설정
+
         OutputView.requestCarName();
         String[] carNames = InputView.getCarNames();
         List<Car> raceParticipants = CarInputConverter.setCarList(carNames);
 
-        //라운드 설정
         OutputView.requestRound();
         String inputRound = InputView.getRound();
-        int round = RaceInputConverter.setIntRound(inputRound);
+        int round = RoundInputConverter.setIntRound(inputRound);
 
-        //참가자 리스트와 라운드로 경기 생성
         Race race = new Race(raceParticipants, round);
-        //시작
+
         race.start();
         OutputView.startRacePrint();
 
-        //승자를 구함
         List<Car> winner = race.getWinner();
         OutputView.printWinners(winner);
 
