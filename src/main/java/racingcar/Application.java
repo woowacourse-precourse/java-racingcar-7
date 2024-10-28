f6fdf81efcbb206e3e8ee52ac6ee5f1b6fda6a59
hongjungin <jungini0000@gmail.com>
2024-10-28 10:33:40 +0900

    feat: main 함수에 자동차 이름 및 시도 횟수 입력 흐름 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingGameController controller = new RacingGameController();
+
+        // 자동차 이름 입력 + 검증
+        List<String> carNames = controller.parseCarNames();
+
+        // 시도 횟수 입력 + 검증
+        int attemptCount = controller.getAttemptCount();
     }
 }
