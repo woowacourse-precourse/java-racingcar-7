8299d4c50437075f830860a17371f9b9ccb7068d
hongjinsu <kaameo12@gmail.com>
2024-10-28 11:07:50 +0900

    feat(Application): Add car name input from user
    Added some methods to Application:
    - User로부터 경주할 자동차 이름을 쉼표(,)기준으로 입력받음
    - 입력받은 이름을 쉼표(,)기준으로 나눠 List에 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,30 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<String> names = userInput();
+//        System.out.println(names);
+    }
+
+    // 사용자로부터 입력을 받아 경주할 자동차 이름 배열 반환
+    public static List<String> userInput(){
+        String names = Console.readLine();
+        return concatNames(names);
+    }
+
+    public static List<String> concatNames(String names){
+        List<String> nameList = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(names,",");
+        while (st.hasMoreTokens()){
+            nameList.add(st.nextToken());
+        }
+        return nameList;
     }
 }
