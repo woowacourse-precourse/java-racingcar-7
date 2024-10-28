353e4b05715e13cdd0971f3a8a231ba3929c57d3
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-10-28 20:11:34 +0900

    feat: Extract car names from user input by getCarNames function
    - 사용자 input을 통해 자동차 이름을 받습니다
    - input을 통해 받은 자동차 이름을 list로 변환해 return 합니다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String[] carNames = getCarNames();
+
+
+    }
+    private static String[] getCarNames(){
+        System.out.println("경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분)");
+        String carNamesInput = Console.readLine();
+        return carNamesInput.split(",");
     }
 }
