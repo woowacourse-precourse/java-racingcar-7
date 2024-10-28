b0285074a61eb63f60d676488102f139583be752
sodychoe <sodychoe@icloud.com>
2024-10-28 18:57:52 +0900

    feat : 전진 시도 횟수 입력 예외처리 구현

--- src/main/java/racingcar/io/InputConsoleHandler.java
+++ src/main/java/racingcar/io/InputConsoleHandler.java
@@ -21,9 +21,42 @@ public class InputConsoleHandler {
     }
 
     public int askNumOfTrial() {
-        String input = Console.readLine();
+        String input = Console.readLine()
+                .trim();
 
-        return Integer.parseInt(input);
+        validateNegativeInput(input);
+        validateIntegerInput(input);
+        validateNumberInput(input);
+
+        try {
+            int result = Integer.parseInt(input);
+            return result;
+
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("허용 가능한 시도 횟수를 초과하였습니다.", e);
+        }
     }
 
+    private void validateNegativeInput(String input) {
+        if (input.matches("-\\d+")) {
+            throw new IllegalArgumentException("시도할 횟수는 0보다 커야 합니다.");
+        }
+    }
+
+    private void validateIntegerInput(String input) {
+        if (input.matches("\\d+\\.\\d+")) {
+            throw new IllegalArgumentException(
+                    String.format("시도할 횟수는 정수여야 합니다. 입력 : %s", input)
+            );
+        }
+    }
+
+    private void validateNumberInput(String input) {
+
+        if (input.matches("\\D+.")) {
+            throw new IllegalArgumentException(
+                    String.format("시도할 횟수에 숫자가 아닌 값이 입력되었습니다. 입력 : %s", input)
+            );
+        }
+    }
 }
