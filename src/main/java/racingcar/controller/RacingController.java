a4154a961ef5334ed2f79d77e9a8a19c6df3c787
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-28 13:32:09 +0900

    feat(RacingController): 자동차 경주 진행 기능 추가

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -3,16 +3,23 @@ package racingcar.controller;
 import java.util.ArrayList;
 import racingcar.model.Car;
 import racingcar.model.CarFactory;
+import racingcar.model.Judge;
+import racingcar.model.RandomNumberGenerator;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingController {
 
     private final InputView inputView;
+    private final OutputView outputView;
     private final CarFactory carFactory;
+    private final Judge judge;
 
-    public RacingController(InputView inputView, CarFactory carFactory) {
+    public RacingController(InputView inputView, OutputView outputView, CarFactory carFactory, Judge judge) {
         this.inputView = inputView;
+        this.outputView = outputView;
         this.carFactory = carFactory;
+        this.judge = judge;
     }
 
     public void startGame() {
@@ -21,5 +28,17 @@ public class RacingController {
         ArrayList<Car> carList = carFactory.createCar(carNames);
 
         Long movingCount = inputView.inputMovingCount();
+
+        outputView.printRacingResultTitle();
+
+        for (int i = 0; i < movingCount; i++) {
+            for (Car car : carList) {
+                if (judge.judgeGoOrStop(RandomNumberGenerator.generateRandomNumber())) {
+                    car.move();
+                }
+            }
+
+            outputView.printRacingResult(carList);
+        }
     }
 }
