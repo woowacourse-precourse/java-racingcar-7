1852067319db75f13d2ea2d1e79229a597f9c197
jungsu <vwx1@naver.com>
2024-10-28 14:10:48 +0900

    style: align code with Java conventions

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,6 +3,7 @@ package racingcar.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
+
   private final String name;
   private int position;
 
@@ -16,14 +17,16 @@ public class Car {
   public String getName() {
     return name;
   }
+
   private void validateName(String name) {
     if (name.length() > 5) {
       throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
     }
-    if (name.isEmpty()){
+    if (name.isEmpty()) {
       throw new IllegalArgumentException("자동차 이름에는 빈 스페이스가 포함될 수 없습니다.");
     }
   }
+
   public int getPosition() {
     return position;
   }
@@ -33,7 +36,8 @@ public class Car {
       position++;
     }
   }
-  private boolean canMove(){
+
+  private boolean canMove() {
     return Randoms.pickNumberInRange(0, 9) >= 4;
   }
 }
