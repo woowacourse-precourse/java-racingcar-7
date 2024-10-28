15eda007c047ab6db30b47a6a71646e80a3747bc
soonvro <soonvro@gmail.com>
2024-10-28 20:40:10 +0900

    feat: 입력에 공백이 포함될 시 에러 메시지 추가

--- src/main/java/racingcar/ErrorMessages.java
+++ src/main/java/racingcar/ErrorMessages.java
@@ -1,7 +1,8 @@
 package racingcar;
 
 public enum ErrorMessages {
-    EMPTY_CAR_NAMES("자동차 이름은 비어있을 수 없습니다.");
+    EMPTY_CAR_NAMES("자동차 이름은 비어있을 수 없습니다."),
+    WHITE_SPACE_INPUT("입력에 공백이 포함되어 있습니다.");
 
     private final String message;
 
