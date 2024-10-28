4ad066e8f46dabec0ef717322b0c924e64ad49b1
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-10-28 11:11:10 +0900

    chore : 움직일 수 있는 경우 추가 테스트 케이스 작성

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,18 +3,22 @@ package racingcar.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {4, 9})
     @DisplayName("자동차가 움직일 수 있는 조건을 만족할 때 움직이는지 확인한다")
-    void 움직임_테스트() {
+    void 움직일_수_있는_경우_테스트(int i) {
         Car car = new Car("pobi");
-        NumberGenerator numberGenerator = () -> 5;
+        NumberGenerator numberGenerator = () -> i;
 
         car.move(numberGenerator.generateNumber());
 
         Assertions.assertThat(car.getLocation()).isEqualTo(1);
     }
+
 
     @Test
     @DisplayName("이름 길이 테스트")
