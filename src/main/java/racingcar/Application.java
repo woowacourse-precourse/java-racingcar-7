54a2eaf19d82cfd46d9a0406a0a2517d46b6df16
Jhsysng <ysng1118@naver.com>
2024-10-28 11:39:51 +0900

    feat: 자동차 정보 입력 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Car> cars = inputCars();
+
+    }
+
+    private static List<Car> inputCars(){
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
+
+        List<Car> cars = new ArrayList<>();
+
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        return cars;
     }
 }
