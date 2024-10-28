7f94920e3ab06c7f9b01163b1110237c8d8e6f17
wowseok <wowseok2774@gmail.com>
2024-10-28 22:44:16 +0900

    refactor: 컨트롤러가 상태를 가지지 않도록 수정

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -8,7 +8,6 @@ import view.OutputView;
 public class GameController {
     private final RacingCarService racingCarService;
     private final OutputView outputView;
-    private boolean warningPrinted = false;
 
     public GameController(RacingCarService racingCarService, OutputView outputView) {
         this.racingCarService = racingCarService;
@@ -16,7 +15,7 @@ public class GameController {
     }
 
     public void startGame(List<Car> cars, int attempts) {
-        showGameStartMessage();
+        outputView.showGameStartMessage();
         runGameRounds(cars, attempts);
     }
 
@@ -32,26 +31,21 @@ public class GameController {
         outputView.printFinalWinner(winners);
     }
 
-    // 게임 시작 메시지 출력
-    private void showGameStartMessage() {
-        outputView.printEmptyLine();
-        outputView.printExecutionResult();
-    }
 
     private void printRaceStatusIfAllowed(List<Car> cars, int attempts) {
-        if (!racingCarService.isPrintAllowed(attempts)) {
-            printWarningMessageOnce();
-            return;  // 조건에 맞지 않으면 즉시 종료
+        if (!racingCarService.isPrintAllowed(attempts)) { // 시도횟수가 1000보다 크면
+            printWarningMessageOnce();                  // 경고 메세지를 한번만 출력
+            return;
         }
 
-        outputView.printRaceStatus(cars);  // 조건에 맞을 때만 실행
+        outputView.printRaceStatus(cars);  // 시도횟수가 1001보다 작은 일반적인 경우, 레이스 상황 출력
     }
 
     private void printWarningMessageOnce() {
-        if (warningPrinted) {
-            return;  // 이미 출력된 경우 아무것도 하지 않음
+        if (racingCarService.hasPrintedWarning()) { //경고가 이미 출력된 경우 아무것도 하지않음
+            return;
         }
         outputView.printWarningMessage();
-        warningPrinted = true;  // 한 번 출력되면 다시 출력되지 않도록 설정
+        racingCarService.setWarningFlag(); // 한 번 출력되면 다시 출력되지 않도록 설정
     }
 }
