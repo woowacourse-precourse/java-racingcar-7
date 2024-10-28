3791f252917b0b6d9dd7958933a2330dfc473ab5
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-10-28 10:18:01 +0900

    Test write beforeeach

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,11 +8,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
 
+    Car car1;
+
+    @BeforeEach
+    void setUp() {
+        car1 = new Car("car1");
+    }
+
     @DisplayName("차가 전진하는 경우 포지션이 1증가하는 것을 테스트하라")
     @Test
     void test3() {
-        Car car1 = new Car("car1");
-
         car1.move(() -> true);
         car1.move(() -> true);
         car1.move(() -> true);
@@ -22,8 +28,6 @@ class CarTest {
     @DisplayName("차가 정지하는 경우 포지션이 그대로인 것을 테스트하라")
     @Test
     void test4() {
-        Car car1 = new Car("car1");
-
         car1.move(() -> false);
         car1.move(() -> false);
         car1.move(() -> false);
@@ -34,8 +38,6 @@ class CarTest {
     @DisplayName("차가 2번 전진, 1번 정지하는 경우 포지션이 2인 것을 테스트하라")
     @Test
     void test5() {
-        Car car1 = new Car("car1");
-
         car1.move(() -> true);
         car1.move(() -> true);
 
@@ -47,8 +49,6 @@ class CarTest {
     @DisplayName("차가 1번 전진, 2번 정지하는 경우 포지션이 1인 것을 테스트하라")
     @Test
     void test6() {
-        Car car1 = new Car("car1");
-
         car1.move(() -> true);
 
         car1.move(() -> false);
