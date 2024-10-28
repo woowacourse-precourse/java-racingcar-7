56d9989207d2401e37d99beec3372b56074929ad
unknown <pjy2163@sookmyung.ac.kr>
2024-10-28 21:16:34 +0900

    feat: RacingGame 전반 기능 구현, main application에 구현한 클래스들 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import java.util.List;
+
+import racingcar.controller.RacingGame;
+import racingcar.view.input;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        input input = new input();
+        List<String> carNameList = input.getCarNames();
+        String[] carNames = carNameList.toArray(new String[0]); // List를 String[]로 변환
+        int attemptCount = input.getAttemptCount();
+
+        try {
+            RacingGame racingGame = new RacingGame(carNames, attemptCount);
+            racingGame.start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
