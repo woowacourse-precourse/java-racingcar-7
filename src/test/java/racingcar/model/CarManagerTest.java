2721af1081883d5419d301923e9c83d4f6b34a34
hmlssghm <kwonse326@gmail.com>
2024-10-28 18:14:51 +0900

    test(CarManagerTest): CarManager 비즈니스 로직 메서드들 테스트 코드 작성

--- src/test/java/racingcar/model/CarManagerTest.java
+++ src/test/java/racingcar/model/CarManagerTest.java
@@ -1,24 +1,55 @@
-package java.racingcar.model;
+package racingcar.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarManagerTest {
 
-    @Test
-    void initialize() {
+    private CarManager carManager;
+
+    @BeforeEach
+    void setUp() {
+        carManager = new CarManager();
     }
 
     @Test
-    void advanceCars() {
+    void initialize_test() {
+        List<String> names = List.of("pobi", "woni", "jun");
+
+        carManager.initialize(names);
+
+        assertEquals(3, carManager.getCars().size());
+        assertEquals("pobi", carManager.getCars().get(0).getName());
+        assertEquals("woni", carManager.getCars().get(1).getName());
+        assertEquals("jun", carManager.getCars().get(2).getName());
     }
 
     @Test
-    void getCars() {
+    void advanceCars_test() {
+        List<String> names = List.of("pobi", "woni");
+        carManager.initialize(names);
+
+        carManager.advanceCars(Arrays.asList(true, false));
+
+        assertEquals(1, carManager.getCars().get(0).getMove());
+        assertEquals(0, carManager.getCars().get(1).getMove());
     }
 
     @Test
-    void findWinners() {
+    void findWinners_test() {
+        List<String> names = List.of("pobi", "woni", "jun");
+        carManager.initialize(names);
+        carManager.advanceCars(Arrays.asList(true, true, false));
+
+        List<Car> winners = carManager.findWinners();
+
+        assertEquals("pobi", winners.get(0).getName());
+        assertEquals("woni", winners.get(1).getName());
     }
 }
