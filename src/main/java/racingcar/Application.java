6dd61e5e74077798afd86a43f81f5bcca9d17abd
김재형 <kjhstar971124@gmail.com>
2024-10-28 21:07:54 +0900

    feat(Application): 자동차 이름 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.NoSuchElementException;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String carNames = promptCarName();
+
+    }
+
+    private static String promptCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames;
+        try {
+            carNames = Console.readLine();
+        } catch (NoSuchElementException e) {
+            System.out.println("올바른 자동차 이름을 입력해 주세요");
+            throw new IllegalArgumentException();
+        }
+        return carNames;
     }
 }
