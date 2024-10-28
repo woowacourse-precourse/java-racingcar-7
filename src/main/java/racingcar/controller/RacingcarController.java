bb5796fbf4c3a01b534da60f9f97202e5edfe9da
chino <tnehd83@naver.com>
2024-10-28 14:16:17 +0900

    feat(RacingcarController): 컨트롤러 추가

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -1,14 +1,6 @@
 package racingcar.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import racingcar.gameuserdata.RacingUserData;
-import racingcar.model.FindManyMovingRacingcar;
-import racingcar.model.RacingPlayingCycle;
-import racingcar.model.RacingcarDataVerification;
-import racingcar.splitter.RacingcarSplitter;
 import racingcar.view.InputView;
-import racingcar.view.OuputView;
 
 public class RacingcarController {
     private final InputView inputView;
