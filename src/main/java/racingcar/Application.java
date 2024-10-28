1e497be232f9cd3775a000da09dd4cc1a6fa4623
kim-su-mi <ksmm9607@naver.com>
2024-10-28 13:47:06 +0900

    feat : 예외처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,17 +8,24 @@ import racingcar.view.ResultView;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-    	String carNamesInput = InputView.readCarNames();
-    	String[] carNames = carNamesInput.split(",");
-    	InputValidator.validateCarNames(carNames);
     	
-    	String trialNumInput = InputView.readTrialNum();
-    	InputValidator.validateTrilaNum(trialNumInput);
-    	int trialNum = Integer.parseInt(trialNumInput);
+    	try {
+    		String carNamesInput = InputView.readCarNames();
+        	String[] carNames = carNamesInput.split(",");
+        	InputValidator.validateCarNames(carNames);
+        	
+        	String trialNumInput = InputView.readTrialNum();
+        	InputValidator.validateTrilaNum(trialNumInput);
+        	int trialNum = Integer.parseInt(trialNumInput);
+        	
+        	GameController game = new GameController(carNames, trialNum);
+            game.play();
+            ResultView.printWinners(game.getCars());
     	
-    	GameController game = new GameController(carNames, trialNum);
-        game.play();
-        ResultView.printWinners(game.getCars());
+    	}catch(IllegalArgumentException e){
+    		System.out.println(e.getMessage());
+    	}
+    	
     	
     }
 }
