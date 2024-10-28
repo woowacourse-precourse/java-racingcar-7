954243429ea1b700e19f9c2fde1c2b6fc9515362
gobchang7 <anchovyzzang@gmail.com>
2024-10-28 21:07:06 +0900

    feat: 자동차 이름 유효성 검사 기능 구현

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -13,13 +13,32 @@ public class InputView {
         String CarName = Console.readLine();
         List<String> names = Arrays.asList(CarName.split(","));
 
+        validateCarNames(names);
+
         return names;
     }
 
-    public int inputTryCount() {
+    private void validateCarNames(List<String> names) {
+        for (String name : names) {
+            validateCarName(name);
+        }
+    }
+
+    private void validateCarName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 최대 5글자 입니다.");
+        }
+
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("자동차의 이름을 입력해주세요.");
+        }
+    }
+
+    public static int inputTryCount() {
 
         System.out.println(("시도할 횟수는 몇 회인가요?"));
         String cnt = Console.readLine();
+
         return Integer.parseInt(cnt);
     }
 }
