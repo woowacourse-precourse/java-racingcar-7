6f514cfb3ef2f02aca972c1bb924ae20c561fdee
kaswhy <sxshx818@naver.com>
2024-10-28 14:55:19 +0900

    style(InputView): 띄어쓰기 추가
    변경 전
    - getCarNames(){}
    
    변경 후
    - getCarNames() {}

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,7 +3,7 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String getCarNames(){
+    public static String getCarNames() {
         return Console.readLine();
     }
 
