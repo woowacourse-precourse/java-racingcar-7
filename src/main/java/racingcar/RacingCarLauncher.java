d6c8d27446bd725280dc7efc1e6906cdde8f8e1d
sobaman <sobaman522@gmail.com>
2024-10-28 23:37:01 +0900

    refactoring(racing-car-launcher): 오타 수정 "(이름은 쉼표(,) 기준으로 구분" -> "이름은 쉼표(,) 기준으로 구분"

--- src/main/java/racingcar/RacingCarLauncher.java
+++ src/main/java/racingcar/RacingCarLauncher.java
@@ -12,7 +12,7 @@ public class RacingCarLauncher {
     }
 
     public void start() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요. 이름은 쉼표(,) 기준으로 구분");
         String carNames = Console.readLine();
         racingManager.registerCars(carNames);
 
