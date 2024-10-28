4513754dc05ca251ea5da10e3160a1faeb3f75b2
yeong-hwan <jghff700@naver.com>
2024-10-28 15:25:31 +0900

    test(view): should display correctly at outputview

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -7,23 +7,10 @@ import racingcar.entity.Car;
 
 public class OutputViewTest {
     @Test
-    void printWinners_ShouldDisplayCorrectly() {
+    void 우승자_출력_정상적으로_표시되는지_확인() {
         OutputView outputView = new OutputView();
         List<String> winners = List.of("pobi", "jun");
 
         outputView.printWinners(winners);
-    }
-
-    @Test
-    void printRoundResults_ShouldDisplayCarProgress() {
-        OutputView outputView = new OutputView();
-        List<Car> cars = List.of(new Car("pobi"), new Car("woni"), new Car("jun"));
-
-        cars.get(0).move();
-        cars.get(1).move();
-        cars.get(1).move();
-        cars.get(2).move();
-
-        OutputView.printRoundResults(cars);
     }
 }
