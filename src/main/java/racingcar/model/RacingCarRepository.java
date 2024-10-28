fc247d831d836e049ef69ad7a555ee845d9759ca
LeeJU <thirdspirit@naver.com>
2024-10-28 11:37:03 +0900

    build(App): Implement Controller
    Implement Random Number Calculation in controller

---
+++ src/main/java/racingcar/model/RacingCarRepository.java
@@ -0,0 +1,36 @@
+package racingcar.model;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class RacingCarRepository {
+    ArrayList<String> cars;
+    ArrayList<BigInteger> values;
+    Long tryCount;
+
+    public RacingCarRepository() {
+        this.cars = new ArrayList<>();
+        this.values = new ArrayList<>();
+    }
+
+    public ArrayList<String> getCars() {
+        return cars;
+    }
+
+    public void addCar(String carName) {
+        this.cars.add(carName);
+        this.values.add(BigInteger.ZERO);
+    }
+
+    public void plusValue(int idx) {
+        this.values.set(idx, this.values.get(idx).add(BigInteger.ONE));
+    }
+
+    public void setTryCount(Long tryCount) {
+        this.tryCount = tryCount;
+    }
+
+    public Long getTryCount() {
+        return tryCount;
+    }
+}
