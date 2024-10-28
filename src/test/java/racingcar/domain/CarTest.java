ff964fcac089e97d73f6f172d241a94a4212febd
JoonKyo <joonkyolee@gmail.com>
2024-10-28 12:21:01 +0900

    refactor(CarTest): 움직이지 않고 테스트했던 부분 수정 및 예측 값 변경

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -20,6 +20,7 @@ public class CarTest {
     void 랜덤_숫자가_0에서_3까지_일때_이동_상황_테스트(int number) {
         assertRandomNumberInRangeTest(
                 () -> {
+                    car.move();
                     assertThat(car.getPosition()).isEqualTo(0);
                     assertThat(car.getMoveStatus()).isEqualTo("");
                 },
@@ -32,8 +33,9 @@ public class CarTest {
     void 랜덤_숫자가_4이상_일때_이동_상황_테스트(int number) {
         assertRandomNumberInRangeTest(
                 () -> {
-                    assertThat(car.getPosition()).isEqualTo(0);
-                    assertThat(car.getMoveStatus()).isEqualTo("");
+                    car.move();
+                    assertThat(car.getPosition()).isEqualTo(1);
+                    assertThat(car.getMoveStatus()).isEqualTo("-");
                 },
                 number
         );
