fdbc57cc406a57db539c34fcc2bc3841df8ca9cc
heehun <slrkqk2@naver.com>
2024-10-28 12:48:17 +0900

    feat(view): InputView,OutputVIew 추가
    사용자 입력, 게임 결과 처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,14 +7,13 @@ import racingcar.utils.InputValidator;
 
 public class InputView {
     public static final String SEPARATOR = ",";
-    public static final String INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-
+    public static final String INPUT_MESSAGE_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_MESSAGE_TRY_NUM = "시도할 횟수는 몇 회인가요?";
     private InputView(){
     }
 
     public static List<String> getCarNames() {
-
-        System.out.println(INPUT_MESSAGE);
+        System.out.println(INPUT_MESSAGE_CAR_NAME);
 
         String input = Console.readLine();
         InputValidator.nameValidator(input);
@@ -22,4 +21,12 @@ public class InputView {
         return InputParser.getNames(input);
     }
 
+    public static int getTryNum() {
+        System.out.println(INPUT_MESSAGE_TRY_NUM);
+
+        String input = Console.readLine();
+        InputValidator.numberValidator(input);
+
+        return InputParser.getRepeatCount(input);
+    }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,16 +6,23 @@ import racingcar.domain.Cars;
 
 public class OutputView {
     public static final String WINNERS = "최종 우승자 : ";
+    public static final String EXECUTION_RESULT = "실행 결과";
+
+    public static void printExecutionResult() {
+        System.out.println();
+        System.out.println(EXECUTION_RESULT);
+    }
 
     public static void printMidResult(Cars cars) {
         List<Car> carList = cars.getCars();
 
-        for (Car car: carList) {
+        for (Car car : carList) {
             String path = locationToString(car.getLocation());
             String name = car.getName();
 
             System.out.println(name + " : " + path);
         }
+        System.out.println();
     }
 
     public static void printResult(List<String> carNames) {
