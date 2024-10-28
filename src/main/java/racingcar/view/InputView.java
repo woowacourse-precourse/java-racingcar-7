7f7ab89efa2953011367023253049906255bb80f
Seyoung Goo <0917sy@gmail.com>
2024-10-28 21:22:43 +0900

    feat: 사용자 입력 기능
    - 프롬프트 메시지 출력
    - 문자열 입력

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+    public String getCarNamesInput() {
+        return readLine();
+    }
+
+    public String getAttemptsInput() {
+        return readLine();
+    }
+}
---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,20 @@
+package racingcar.view;
+
+public class OutputView {
+
+    private static final String INPUT_CAR_NAMES_PROMPT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ATTEMPTS_PROMPT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
+    public void displayInputCarNamesPrompt() {
+        showPrompt(INPUT_CAR_NAMES_PROMPT_MESSAGE);
+    }
+
+    public void displayAttemptsPrompt() {
+        showPrompt(INPUT_ATTEMPTS_PROMPT_MESSAGE);
+    }
+
+
+    private void showPrompt(String message) {
+        System.out.println(message);
+    }
+}
