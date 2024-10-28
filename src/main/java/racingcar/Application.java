fcc3aa2b0e400d15254c3d5af1af2f20ef081946
dayaelee <dayae.dev@gmail.com>
2024-10-28 20:35:14 +0900

    feat:6. 전진 시도 차수별 실행 결과를 출력한다. 자동차 이름 키 값에 대한 밸류값만큼 '-'를 출력한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -42,24 +42,28 @@ public class Application {
         }
 
         //3. 전체 자동차를 포함하는 해시맵을 만들고, 키는 자동차의 이름으로 하고 값은 0으로 초기화 한다.
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, String> map = new HashMap<>();
         for (String car : carArr){
-            map.put(car, 0);
+            map.put(car, "");
         }
 
-
+        System.out.println("실행 결과");
         for (String car : carArr){
             //4. 각 자동차의 전진 조건에 대해 정하기 위해 0-9사이의 무작위 값을 구한다.
             Integer pickNum = Randoms.pickNumberInRange(0, 9);
             //5. 만약 무작위 값이 4이상일 경우 전진해야하니, 해당 자동차의 키 값에 대한 value에 +1을 해준다.
             if (pickNum >= 4){
-                int value = map.get(car);
-                value += 1;
+                String value = map.get(car);
+                value += "-";
                 map.put(car, value);
             }
 
         }
 
+        //6. 전진 시도 차수별 실행 결과를 출력한다. 자동차 이름 키 값에 대한 밸류값만큼 '-'를 출력한다.
+        for (String car : carArr){
+            System.out.println(car+" : "+map.get(car));
+        }
 
 
     }
