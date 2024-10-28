2a275390ea3de989c3eaab55f692a319e0f0ddd2
rlaehd62 <rlaehd62@gmail.com>
2024-10-28 17:14:05 +0900

    refactor(controller): 인터페이스에 의존하도록 변경 - 서비스의 인터페이스를 사용하도록 변경 - 생성자를 통해서 구현체를 변경할 수 있도록함 - 구현체가 변경되어도 사용이 바뀌지 않도록함

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -3,19 +3,20 @@ package racingcar.controller;
 import racingcar.dto.RaceRequest;
 import racingcar.dto.RaceResponse;
 import racingcar.service.GameService;
+import racingcar.service.RaceService;
 import racingcar.view.RaceView;
 import racingcar.view.View;
 
 public class RaceController implements Controller<RaceRequest> {
 
-    private GameService gameService;
+    private GameService<RaceRequest, RaceResponse> gameService;
 
     public RaceController() {
-        this.gameService = new GameService();
+        this.gameService = new RaceService();
     }
 
     public View run(RaceRequest request) {
-        RaceResponse response = gameService.startRace(request);
+        RaceResponse response = gameService.startGame(request);
         return RaceView.createView(response);
     }
 
--- src/main/java/racingcar/controller/RaceProxy.java
+++ src/main/java/racingcar/controller/RaceProxy.java
@@ -1,6 +1,7 @@
 package racingcar.controller;
 
 import racingcar.dto.RaceRequest;
+import racingcar.service.InputValidationService;
 import racingcar.service.ValidationService;
 import racingcar.view.View;
 
@@ -10,13 +11,13 @@ public class RaceProxy implements Controller<RaceRequest> {
     private ValidationService validationService;
 
     public RaceProxy(Controller<RaceRequest> controller) {
-        this.validationService = new ValidationService();
+        this.validationService = new InputValidationService();
         this.controller = controller;
     }
 
     @Override
     public View run(RaceRequest request) {
-        validationService.verifyInput(request.getCarInput());
+        validationService.verify(request.getCarInput());
         return controller.run(request);
     }
 }
