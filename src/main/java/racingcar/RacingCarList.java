20dd25bf8882b77008b98afe7153e9ba0cf54888
ssuukko <tlstpstpdl11@gmail.com>
2024-10-28 21:56:50 +0900

    Refactor: RacingCarList 클래스를 개선
    - 자동차 이름 검증 로직을 `Validation.validateLength`로 변경하였습니다.
    - 이동 로직에서 각 자동차가 차례대로 이동하도록 개선하였습니다.
    - 우승자 판별 로직을 단순화하고, 최종 우승자 출력을 추가하였습니다.
    - 최대 위치 찾기 기능을 `findMaxPosition` 메서드로 분리하여 가독성을 높였습니다.

--- src/main/java/racingcar/RacingCarList.java
+++ src/main/java/racingcar/RacingCarList.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCarList {
+    private static final String WINNER_RESULT = "최종 우승자 : ";
     private final String DELIMITER = ",";
     private final List<Car> carList;
 
@@ -14,19 +15,16 @@ public class RacingCarList {
     public void add(String carNames) {
         String[] splitNames = carNames.split(DELIMITER);
         for (String splitName : splitNames) {
-            Validation.validateCarName(splitName);
+            Validation.validateLength(splitName);
             carList.add(new Car(splitName));
         }
     }
 
     public List<Car> moveCar(String attemptCount) {
-        int count = Validation.validateAttemptCount(attemptCount);
+        int count = Validation.validateIfAttemptCount(attemptCount);
         for (int i = 0; i < count; i++) {
-            for (Car car : carList) {
-                if (MoveCondition.getCondition() >= 4) {
-                    car.move();
-                }
-            }
+            Car car = carList.get(i % carList.size());
+            car.move();
             OutputView.printRacingStatus(carList);
             OutputView.printLineBreak();
         }
@@ -34,19 +32,26 @@ public class RacingCarList {
     }
 
     public String findWinner(List<Car> cars) {
-        int maxPosition = 0;
+        int maxPosition = findMaxPosition(cars);
+
         List<String> winners = new ArrayList<>();
-
+        System.out.print(WINNER_RESULT);
         for (Car car : cars) {
-            if (car.getPosition() > maxPosition) {
-                maxPosition = car.getPosition();
-                winners.clear();
-            }
-
             if (car.getPosition() == maxPosition) {
                 winners.add(car.getName());
             }
         }
+        if (winners.size() == 1) {
+            return winners.getFirst();
+        }
         return String.join(", ", winners);
     }
+
+    private static int findMaxPosition(List<Car> cars) {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .getAsInt();
+    }
 }
+
