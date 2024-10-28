f2ae20a29e39c2ba6602fc92150a96328662ace3
thsghdud13 <thsgdud25@naver.com>
2024-10-28 17:18:17 +0900

    refactor(dto): dto들을 record로 변경
    * 변경 전 : class
    * 변경 후 : record

--- src/main/java/racingcar/dto/CarDto.java
+++ src/main/java/racingcar/dto/CarDto.java
@@ -1,16 +1,7 @@
 package racingcar.dto;
 
 public class CarDto {
-    public static class CarOutputDto {
-        private final String carName;
-
-        public CarOutputDto(String carName) {
-            this.carName = carName;
-        }
-
-        public String getCarName() {
-            return carName;
-        }
+    public record CarOutputDto(String carName) {
     }
 
 }
--- src/main/java/racingcar/dto/RacingHistoryDto.java
+++ src/main/java/racingcar/dto/RacingHistoryDto.java
@@ -1,27 +1,6 @@
 package racingcar.dto;
 
 public class RacingHistoryDto {
-    public static class RacingHistoryOutputDto {
-        private final String carName;
-        private final int roundNumber;
-        private final int totalMovedDistance;
-
-        public RacingHistoryOutputDto(String carName, int roundNumber, int totalMovedDistance) {
-            this.carName = carName;
-            this.roundNumber = roundNumber;
-            this.totalMovedDistance = totalMovedDistance;
-        }
-
-        public String getCarName() {
-            return carName;
-        }
-
-        public int getRoundNumber() {
-            return roundNumber;
-        }
-
-        public int getTotalMovedDistance() {
-            return totalMovedDistance;
-        }
+    public record RacingHistoryOutputDto(String carName, int roundNumber, int totalMovedDistance) {
     }
 }
