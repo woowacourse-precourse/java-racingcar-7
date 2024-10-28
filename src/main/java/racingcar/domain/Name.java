0f3cd2557bba43d6d2d5a0ad181aaec795c113fd
jae woo Jung <awfjol2008@gmail.com>
2024-10-28 17:52:38 +0900

    feat: Name 클래스에 검증 로직 추가

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -1,11 +1,36 @@
 package racingcar.domain;
 
+import racingcar.config.RaceConfig;
+
 public class Name {
 	private final String name;
 
 	public Name(String name) {
+		validateName(name);
 		this.name = name;
 	}
 
+	private void validateName(String name) {
+		validateNotBlank(name);
+		validateLength(name);
+		validateAlphabet(name);
+	}
 
+	private void validateNotBlank(String name) {
+		if (name == null || name.isBlank()) {
+			throw new IllegalArgumentException("[ERROR] 자동차 이름은 비어있을 수 없습니다.");
+		}
+	}
+
+	private void validateLength(String name) {
+		if (name.length() > RaceConfig.MAX_NAME_LENGTH.getNumber()) {
+			throw new IllegalArgumentException("[ERROR] 자동차 이름이 최대 길이를 초과했습니다.");
+		}
+	}
+
+	private void validateAlphabet(String name) {
+		if (!name.matches("^[a-zA-Z]+")) {
+			throw new IllegalArgumentException("[ERROR] 입력값은 알파벳으로만 구성되어야 합니다.");
+		}
+	}
 }
