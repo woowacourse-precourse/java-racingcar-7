d78c191bb66b0bfe29247dd05e4fcd3f5dfcc2aa
2jin2.1031 <2jin2.1031@gmail.com>
2024-10-28 20:16:52 +0900

    test: 불필요한 코드 및 메서드 삭제

--- src/test/java/racingcar/CarRacerTest.java
+++ src/test/java/racingcar/CarRacerTest.java
@@ -17,26 +17,8 @@ class CarRacerTest {
         carRacer.moveCars(3);
 
         // then
-        for (int i = 0; i < carRacer.getCarPositions().length; i++) {
-            System.out.println(carRacer.getCarNames()[i] + " : " + carRacer.getCarPositions()[i]);
-        }
+
         assertTrue(Arrays.stream(carRacer.getCarPositions()).allMatch(position -> position >= 0));
         assertTrue(Arrays.stream(carRacer.getCarPositions()).allMatch(position -> position <= 3));
-    }
-
-    @Test
-    void getWinners() {
-
-        // given
-        CarRacer carRacer = new CarRacer(new String[]{"pobi", "crong", "honux"});
-
-        // when
-        carRacer.moveCars(3);
-
-        // then
-        for (int i = 0; i < carRacer.getCarPositions().length; i++) {
-            System.out.println(carRacer.getCarNames()[i] + " : " + carRacer.getCarPositions()[i]);
-        }
-        System.out.println(carRacer.getWinners());
     }
 }
