6eaec1878f7932b88823730087c249097f37a501
gabean13 <choegabin171@gmail.com>
2024-10-28 10:35:59 +0900

    feat(Car) : Car 객체 기능 구현
    - 차 랜덤으로 움직일지 말지 기능 구현
    - 차 현 이동 상태 String으로 반환 기능 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,43 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private String name;
+    private Long distance;
+
+    public Car(String name) {
+        this.name = name;
+        distance = 0L;
+    }
+
+    public void moveOrNot() {
+        if (isCarMove()) {
+            distance++;
+        }
+    }
+
+    public boolean isCarMove() {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
+        if (randomNum >= 4) {
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(name).append(" : ");
+
+        for (int i = 0; i < distance; i++) {
+            sb.append("-");
+        }
+
+        sb.append("\n");
+        return sb.toString();
+    }
+
+    public Long getDistance() {
+        return distance;
+    }
+}
