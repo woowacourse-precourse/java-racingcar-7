5502957db21194484020a4f23be8e6584fab1fc4
seyeon22 <sywolleych@gmail.com>
2024-10-28 15:16:58 +0900

    fix(InputCars): 입력받는 부분 수정

--- src/main/java/racingcar/InputCars.java
+++ src/main/java/racingcar/InputCars.java
@@ -1,12 +1,14 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.*;
 
 public class InputCars {
     public Map<String, Integer> receiveCars() {
         List<String> tempCars;
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = System.console().readLine();
+        String input = Console.readLine();
         tempCars = Arrays.stream(input.split(",")).toList();
         Set<String> cars = new HashSet<>(tempCars);
         for (var car : cars) {
