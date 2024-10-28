19e5aa8ef22575574f9771174fd30a16d67dde1c
hmlssghm <kwonse326@gmail.com>
2024-10-28 18:50:14 +0900

    test(ServiceTest): 서비스 로직 테스트 코드 작성

--- src/test/java/racingcar/service/ServiceTest.java
+++ src/test/java/racingcar/service/ServiceTest.java
@@ -1,16 +1,35 @@
-package java.racingcar.service;
+package racingcar.service;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.model.CarManager;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
-class ServiceTest {
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ServiceTest extends NsTest {
+
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    Service service = new Service();
 
     @Test
     void operate() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run();
+                    assertThat(output()).contains("pobi : -", "pobi : --", "woni : -");
+                },
+                MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, STOP
+        );
     }
 
-    @Test
-    void getWinner() {
+    @Override
+    protected void runMain() {
+        service.operate(List.of("pobi", "woni"), 2);
     }
 }
