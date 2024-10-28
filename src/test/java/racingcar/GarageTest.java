0db56fad7723c6d4adf689c125a0ce1e89f5cb9b
Park, Sojeong <sojeong2101@gmail.com>
2024-10-28 12:21:26 +0900

    test($GarageTest): 자동차 최소 조건 위반과 이름 중복 예외 처리 테스트 구현
    1. 자동차 최소 개수 위반 예외 처리 테스트
    2. 이름이 중복 시 예외 처리 테스트

--- src/test/java/racingcar/GarageTest.java
+++ src/test/java/racingcar/GarageTest.java
@@ -9,6 +9,26 @@ import org.junit.jupiter.api.Test;
 class GarageTest {
 
     @Test
+    void 자동차_개수_2개_미만인지_검사() {
+        List<String> carName = List.of("소정");
+
+        Assertions.assertThatThrownBy(() -> {
+                    new Garage(carName);
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차는 적어도 2개 이상이어야 합니다.");
+    }
+
+    @Test
+    void 자동차_이름_중복_검사() {
+        List<String> carNameList = List.of("소정", "소정");
+
+        Assertions.assertThatThrownBy(() -> {
+                    new Garage(carNameList);
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 중복될 수 없습니다.");
+    }
+
+    @Test
     void 자동차_이름_배열로_자동차_배열_생성() {
         // give
         List<String> carNameList = List.of("소정", "pobi", "woni", "jun");
