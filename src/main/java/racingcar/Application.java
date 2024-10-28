bc3cc97ae16027a325559b0f7104bbd777854cb2
dayaelee <dayae.dev@gmail.com>
2024-10-28 19:34:19 +0900

    feat:1-1. 자동차 이름이 5자를 초과한 경우 에러 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,9 +6,20 @@ public class Application {
     public static void main(String[] args) {
         //1. 경주할 자동차의 이름을 쉼표 기준으로 구분하여 한줄에 받기
         String inputCar;
+
         try{
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             inputCar = Console.readLine();
+            String car = inputCar;
+            String[] carArr = car.split(",");
+
+            //1-1. 자동차 이름이 5자를 초과한 경우 에러 출력
+            for (String checkCar : carArr) {
+                if (checkCar.length()>5){
+                    throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+                }
+            }
+
         } catch (IllegalArgumentException e){
             return;
         }
