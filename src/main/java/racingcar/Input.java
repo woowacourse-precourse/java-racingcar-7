ff20218d68cf7b03c77478c2ce00d87bfab56d85
Seongmo Ahn <djgood780@gmail.com>
2024-10-28 12:15:32 +0900

    feat(Input): 자동차 이름 문자열 입력 받기 기능 추가

---
+++ src/main/java/racingcar/Input.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+    private static final InputValidator inputValidator;
+
+    static {
+        CarNameValidator carNameValidator = new CarNameValidator();
+        NumberValidator numberValidator = new NumberValidator();
+
+        inputValidator = new InputValidator(carNameValidator, numberValidator);
+    }
+
+    public static String getCarNames(){
+        String carNames = Console.readLine();
+        inputValidator.validateCarNames(carNames);
+        inputValidator.validateCarName(carNames);
+        return carNames;
+    }
+}
