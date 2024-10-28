30cb53602d5bb330adaf15327d9237d2a05a63c1
unknown <qwer1234@mail.ulsan.ac.kr>
2024-10-28 23:00:10 +0900

    refactor: printPositions 메서드를 CarList로 이동 - 'printPositions' 메서드를 'CarList' 클래스에 추가하여 자동차 목록의 위치를 출력하도록 수정했습니다. - 'Car' 클래스의 책임을 분리하고, 자동차 목록 출력 기능을 'CarList'에서 관리하도록 개선했습니다. - 메인 로직 에서는 'carList.printPositions()' 호출로 변경하여 코드 구조를 명확하게 했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -10,6 +13,9 @@ public class Application {
 
         CarList carlist = new CarList();
         addCarsToList(cars, carlist);
+
+        raceCars(carlist, tries);
+        printWinners(carlist);
     }
 
     private static String getCarNames() {
@@ -17,23 +23,57 @@ public class Application {
         return Console.readLine();
     }
 
-    private static void validation(String carName){
-        if(carName.length()>5) throw new IllegalArgumentException();
+    private static void validation(String carName) {
+        if (carName.length() > 5) throw new IllegalArgumentException();
     }
 
-    private static int getTries(){
+    private static int getTries() {
         System.out.println("시도할 횟수는 몇 회 인가요?");
         String triesInput = Console.readLine();
         return Integer.parseInt(triesInput);
     }
 
-    private static void addCarsToList(String cars, CarList carlist){
+    private static void addCarsToList(String cars, CarList carlist) {
         String[] carNames = cars.split(",");
-        for(String carName : carNames){
+        for (String carName : carNames) {
             validation(carName);
-            Car car = new Car(carName,0);
+            Car car = new Car(carName, 0);
             carlist.getCars().add(car);
         }
     }
 
+    private static void moveCars(CarList carlist) {
+        for (Car car : carlist.getCars()) {
+            car.move_car();
+        }
+    }
+
+    private static void raceCars(CarList carList, int tries) {
+        while (tries-- > 0) {
+            moveCars(carList);
+            carList.printPositions(carList);
+            System.out.println();
+        }
+    }
+
+    private static List<String> findWinners(CarList carList) {
+        int max_move = 0;
+        List<String> winners = new ArrayList<>();
+
+        for (Car car : carList.getCars()) {
+            if (car.getMovevalue() > max_move) {
+                winners.clear();
+                max_move = car.getMovevalue();
+                winners.add(car.getName());
+            } else if (car.getMovevalue() == max_move) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
+    private static void printWinners(CarList carList) {
+        List<String> winners = findWinners(carList);
+        System.out.print("최종 우승자 : ");
+        System.out.println(String.join(", ", winners));
+    }
 }
--- src/main/java/racingcar/CarList.java
+++ src/main/java/racingcar/CarList.java
@@ -13,4 +13,13 @@ public class CarList {
     public void addCar(Car car) {
         cars.add(car);
     }
+    static void printPositions(CarList carlist) {
+        for (Car car : carlist.getCars()) {
+            System.out.print(car.getName() + " : ");
+            for (int i = 0; i < car.getMovevalue(); i++) {
+                System.out.print('-');
+            }
+            System.out.println();
+        }
+    }
 }
