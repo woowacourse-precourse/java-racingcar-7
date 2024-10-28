41c0b5c70278e40184171ba6cfbfa3d84bdcc1aa
encoreJeong <zzaibean@gmail.com>
2024-10-28 18:28:28 +0900

    feat: 결과 출력
    1. 자동차 위치의 최댓값 구하기
    2. 최종 위치가 자동차 위치의 최댓값인 경우 우승자로 선정
    3. 우승자 콘솔 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,20 +3,11 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * 주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다.
- * 각 자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
- * 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
- * 사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.
- * 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
- * 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
- * 우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.
- * 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.
- */
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -43,6 +34,7 @@ public class Application {
             cars.putIfAbsent(carName, 0);
         });
 
+        int maxPosition = 0;
         for(int i = 0; i < tryCount; i++) {
 
             for(String car : cars.keySet()) {
@@ -51,6 +43,7 @@ public class Application {
                 if(randomValue >= 4) {
                     int prevPosition = cars.get(car);
                     cars.put(car, prevPosition + 1);
+                    maxPosition = Math.max(maxPosition, prevPosition + 1);
                 }
             }
 
@@ -66,8 +59,15 @@ public class Application {
             System.out.println(carStatus);
         }
 
+        int finalMaxPosition = maxPosition;
+        List<String> winnerName = new ArrayList<>();
+        cars.forEach((carname, position) -> {
+            if(position == finalMaxPosition) {
+                winnerName.add(carname);
+            }
+        });
 
-
-
+        String winner = String.join(", ", winnerName);
+        System.out.println("최종 우승자 : " + winner);
     }
 }
