0318f4b1354e68dd4b1f50a729731cb1f95b74d1
이태구 <alexlee9841@gmail.com>
2024-10-28 21:31:46 +0900

    feat: Car 클래스 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,3 +5,27 @@ public class Application {
         // TODO: 프로그램 구현
     }
 }
+
+class Car {
+    String name;
+    int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void moveForward(int randomNumber) {
+        if (randomNumber >= 4) {
+            position++;
+        }
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
