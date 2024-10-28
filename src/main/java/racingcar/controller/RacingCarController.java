8df95db1a98add08a19b8bd7ee1b1061950bf073
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2024-10-28 22:51:11 +0900

    feat : Add new throw exceptions
    Make new  IllegalArgumentException for exceptions

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -20,9 +20,19 @@ public class RacingCarController {
     public void CarRacingStart() {
         racingCarView.showCarInputInfo();
         InputDTO inputDTO = racingCarView.getInputCarName();
-        racingCarView.showRoundTimeInfo();
-        int roundTime = racingCarView.getRoundTime();
 
+        // 입력값이 공백이거나 null인 경우 예외 처리
+        if (inputDTO == null || inputDTO.getInput().trim().isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+        }
+
+        racingCarView.showRoundTimeInfo();
+        int roundTime = racingCarView.getInputRoundTime().getRoundTime();
+
+        // 자동차 실행횟수가 공백인 경우, 음수인 경우에 대한 예외 처리
+        if (roundTime < 0) {
+            throw new IllegalArgumentException("시행횟수는 공백이나 음수가 들어와서는 안됩니다.");
+        }
         List<CarDTO> carDTOList = racingCarService.splitCarName(inputDTO);
 
         racingCarView.showResultInfo();
