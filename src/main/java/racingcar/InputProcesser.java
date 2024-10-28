d6ea499b2bd9c3e89bceace556ee417248b74100
yushinc <cys74351@gmail.com>
2024-10-28 22:02:43 +0900

    feat: 양수 검증 로직 추가

--- src/main/java/racingcar/InputProcesser.java
+++ src/main/java/racingcar/InputProcesser.java
@@ -1,12 +1,13 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class InputProcesser {
     public String getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
         String carNames = Console.readLine().replaceAll(" ", "");
-        validateEmpty(carNames);
+        validateBlank(carNames);
 
         return carNames;
     }
@@ -14,19 +15,33 @@ public class InputProcesser {
     public int getTryNums() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String tryNumString = Console.readLine().replaceAll(" ", "");
-        validateEmpty(tryNumString);
 
-        try {
-            int tryNum = Integer.parseInt(tryNumString);
-            return tryNum;
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자를 입력해 주세요.");
+        validateBlank(tryNumString);
+        validateInteger(tryNumString);
+
+        int tryNum = Integer.parseInt(tryNumString);
+        validatePositive(tryNum);
+
+        return tryNum;
+    }
+
+    private void validateBlank(String inputString) {
+        if (inputString.isEmpty() || inputString == null) {
+            throw new IllegalArgumentException("아무런 내용이 입력되지 않았습니다.");
         }
     }
 
-    private void validateEmpty(String inputString) {
-        if (inputString.isEmpty() || inputString == null) {
-            throw new IllegalArgumentException("아무런 내용이 입력되지 않았습니다.");
+    private void validateInteger(String tryNumString) {
+        try {
+            Integer.parseInt(tryNumString);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("숫자를 입력해 주세요.");
+        }
+    }
+
+    private void validatePositive(int inputNum) {
+        if (inputNum <= 0) {
+            throw new IllegalArgumentException("실행횟수는 양수만 입력 가능합니다.");
         }
     }
 }
