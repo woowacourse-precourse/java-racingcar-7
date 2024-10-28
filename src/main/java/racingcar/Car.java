bfbb3e36af37f773d2fcc674cb3ba51432b5cf1b
bongj9 <lollypark501@naver.com>
2024-10-28 17:36:38 +0900

    feat: Car객체 만들기 및 기능구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int distance = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void go() {
+        distance++;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getDistance() {
+        return distance;
+    }
+    //거리 -치환
+    public String exchangeDistance(int distance) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < distance; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
+    }
+
+}
