1f12e06ed407d02e6f65f8fc9a9638ce2feee50f
장영재 <urinaner@naver.com>
2024-10-28 15:36:20 +0900

    fix(InputView): Console.readLine 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,23 +1,21 @@
 package racingcar.view;
 
-import java.io.BufferedReader;
+import camp.nextstep.edu.missionutils.Console;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import racingcar.utils.ErrorMessage;
 
 public class InputView {
     private static final String GET_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String GET_TRY_NUMBERS = "시도할 회수는 몇회인가요?";
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public String getCarNames() throws IOException {
         System.out.println(GET_CAR_NAMES);
-        return br.readLine();
+        return Console.readLine();
     }
 
     public int getTryNumbers() throws IOException {
         System.out.println(GET_TRY_NUMBERS);
-        return validateTrialNumber(Integer.parseInt(br.readLine()));
+        return validateTrialNumber(Integer.parseInt(Console.readLine()));
     }
 
     private int validateTrialNumber(int trials) {
