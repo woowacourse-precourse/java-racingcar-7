dd8614ba164ce8a50c6fb9a64a43cd0a6f3714d3
unknown <kjjs2670@naver.com>
2024-10-28 09:45:36 +0900

    feat:결과 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class Application {
     private static final int MOVE_THRESHOLD = 4;
+
     public static void main(String[] args) {
         // 입력 받기
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
