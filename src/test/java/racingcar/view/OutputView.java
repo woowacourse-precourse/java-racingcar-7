bcd5187c61c5d5995316b28f9119a37e7521d8b8
JMJ <ghtntkdnsk@naver.com>
2024-10-28 19:08:07 +0900

    feat(OutputView) : 게임의 출력 메시지 담당 OutputView 클래스 추가

---
+++ src/test/java/racingcar/view/OutputView.java
@@ -0,0 +1,29 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.domain.Car;
+
+public class OutputView {
+
+    private OutputView() {}
+
+    public static void printCarNamePrompt() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void printTryCountPrompt() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+    public static void printRoundResults(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<Car> winners) {
+        String winnerNames = String.join(", ", winners.stream().map(Car::getName).toList());
+        System.out.println("최종 우승자: " + winnerNames);
+    }
+}
