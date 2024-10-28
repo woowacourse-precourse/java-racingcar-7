531ebdba09f579398f9abd148acc13c2c646bf25
Junad-Park <qkrwnsgk1624@icloud.com>
2024-10-28 15:17:27 +0900

    test(StringToIntegerConverterTest): String을 Integer 타입으로 변환할 수 없는 경우 유효성 검증 테스트 추가

--- src/test/java/racingcar/converter/StringToIntegerConverterTest.java
+++ src/test/java/racingcar/converter/StringToIntegerConverterTest.java
@@ -1,21 +1,41 @@
 package racingcar.converter;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import racingcar.enums.ErrorMessage;
 
 class StringToIntegerConverterTest {
-
-    @Test
-    void String_타입의_입력을_Integer_타입으로_변활할_수_있다() {
-        // given
+    
+    @Nested
+    @DisplayName("String을 Integer로 변환 시")
+    class ConvertStringToIntegerTests {
         Converter<String, Integer> converter = new StringToIntegerConverter();
-        String input = "5";
 
-        // when
-        Integer convertedInput = converter.convert(input);
+        @Test
+        void String_타입의_입력을_Integer_타입으로_변환할_수_있다() {
+            // given
+            String input = "5";
 
-        // then
-        assertThat(convertedInput).isInstanceOf(Integer.class);
+            // when
+            Integer convertedInput = converter.convert(input);
+
+            // then
+            assertThat(convertedInput).isInstanceOf(Integer.class);
+        }
+
+        @Test
+        void 양의_정수가_아닌_String_값을_변환할_수_없다() {
+            // given
+            String input = "5.5";
+
+            // when & then
+            assertThatThrownBy(() -> converter.convert(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_TRY_COUNT.getMessage());
+        }
     }
 }
