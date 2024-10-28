bf88114968fe1ec85c79b3658f7f0017d269ddbd
making-a-scene <4936joo@naver.com>
2024-10-28 12:57:50 +0900

    refactor(Validation): 예외 발생에 대한 detailMessage 추가

--- src/main/java/racingcar/Validation.java
+++ src/main/java/racingcar/Validation.java
@@ -7,22 +7,26 @@ public class Validation {
 
     public String[] validateCarName(String input) {
         if (!input.contains(",")) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("최소 2개 이상의 자동차 이름을 입력해야 합니다.");
         }
         String[] carNames = input.split(",");
         for (String carName : carNames) {
             if (!Pattern.matches(CAR_NAME_INPUT_PATTERN, carName)) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("자동차 이름은 알파벳 대소문자와 숫자로만 이루어져 있어야 하며, 1자 이상 5자 이하여야 합니다.");
             }
         }
         return carNames;
     }
 
     public int validateRoundLimit(String input) {
-        int limit = Integer.parseInt(input);
-        if (limit <= 0 || limit > 100) {
-            throw new IllegalArgumentException();
+        try {
+            int limit = Integer.parseInt(input);
+            if (limit <= 0 || limit > 100) {
+                throw new IllegalArgumentException("입력된 수가 범위를 벗어납니다. 1번 이상, 100번 이하로만 시도 가능합니다.");
+            }
+            return limit;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력이 숫자 형태가 아닙니다.");
         }
-        return limit;
     }
 }
