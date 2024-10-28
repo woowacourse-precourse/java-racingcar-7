b9a283fab26c399979f73e2aaf491833a5162a30
Kim-dong-kyun <kdk942@gmail.com>
2024-10-28 17:49:41 +0900

    feat: 자동차 이름 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String[] carNames = inputCarNames();
+    }
+
+    public static String[] inputCarNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine().split(",");
     }
 }
