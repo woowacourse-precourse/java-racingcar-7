c0b7fc10d79a5495f14501566a859184a600fe01
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-28 15:17:29 +0900

    refactor: 반복문 로직 중복으로 인한 출력오류 개선

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -6,30 +6,23 @@ import racingcar.validator.RaceValidator;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Race {
-    private final List<Car> cars;
-    private final int attempts;
-
-    public Race(List<Car> cars, int attempts) {
-        RaceValidator.validateCars(cars);
-        RaceValidator.validateAttempts(attempts);
-
-        this.cars = cars;
-        this.attempts = attempts;
+public record Race(List<Car> cars, int attempts) {
+    public Race {
+        RaceValidator.validateCars(cars); // 생성자에서 자동차 리스트 검증
     }
 
     public List<Car> findWinners() {
         List<Car> winners = new ArrayList<>();
         int maxPosition = 0;
 
-        // 자동차들의 최대 거리 구하기
+        // 최대 위치 찾기
         for (Car car : cars) {
             if (car.getPosition() > maxPosition) {
                 maxPosition = car.getPosition();
             }
         }
 
-        // 최대거리에 위치한 자동차들 찾고 리스트에 추가
+        // 우승자 찾기
         for (Car car : cars) {
             if (car.getPosition() == maxPosition) {
                 winners.add(car);
@@ -39,17 +32,15 @@ public class Race {
     }
 
     public void startRace() {
-        for (int i = 0; i < attempts; i++) {
-            for (Car car : cars) {
-                int randomValue = pickRandomValue();
-                if (randomValue >= 4) {
-                    car.moveForward();
-                }
+        for (Car car : cars) {
+            int randomValue = pickRandomValue();
+            if (randomValue >= 4) {
+                car.moveForward(); // 자동차 전진
             }
         }
     }
 
     private int pickRandomValue() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(0, 9); // 랜덤 값 생성
     }
 }
