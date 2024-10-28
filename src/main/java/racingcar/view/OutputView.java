ca0be09136578ff0a3772a43aee9489ca8cec24f
making-a-scene <4936joo@naver.com>
2024-10-28 12:34:49 +0900

    refactor: 실행 결과 메시지 상수 선언에 줄바꿈 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,12 +6,12 @@ import racingcar.domain.Car;
 import java.util.List;
 
 public class OutputView {
-    private final String PRINT_RESULT_STATEMENT = "실행 결과";
+    private final String PRINT_RESULT_STATEMENT = "\n실행 결과";
     private final String PRINT_WINNERS_STATEMENT = "최종 우승자 : ";
 
     public void printRoundResult(boolean isFirstRound, List<Car> participants) {
         if (isFirstRound) {
-            System.out.println("\n" + PRINT_RESULT_STATEMENT);
+            System.out.println(PRINT_RESULT_STATEMENT);
         }
         for (Car car : participants) {
             System.out.println(car.getName() + " : " + "-".repeat(car.getMovedCnt()));
