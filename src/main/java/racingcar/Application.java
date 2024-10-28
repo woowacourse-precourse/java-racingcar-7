c5a974dc27c1f64fd6e56acaba2ac64876767f25
tdddt <doto1116@ewhain.net>
2024-10-28 21:25:21 +0900

    feat: 자동차 이름 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
+class Car{
+    private String name;
+    private int now = 0;
+    public Car(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getNow(){
+        return this.name;
+    }
+}
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 1. 자동차 이름 입력
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> carNames = List.of(Console.readLine().split(","));
+        // System.out.println(carNames);
     }
 }
