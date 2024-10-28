6bf5b8c2dbe1e892e32b5b8ac0e0c47e30f39a66
HwangSgHn <dolphin33@jbnu.ac.kr>
2024-10-28 18:23:43 +0900

    feat: 시도할 횟수 입력 받는 기능
    Input 클래스에 추가.

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -4,12 +4,21 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
     private String carNamesWithComa;
+    private String tryCountString;
 
     public void carNamesWithComa() {
         carNamesWithComa = Console.readLine();
     }
 
+    public void tryCount() {
+        tryCountString = Console.readLine();
+    }
+
     public String getNamesWithComa() {
         return carNamesWithComa;
+    }
+
+    public String getTryCount() {
+        return tryCountString;
     }
 }
