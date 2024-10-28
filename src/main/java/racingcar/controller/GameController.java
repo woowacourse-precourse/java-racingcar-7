fc17ecc829623d8b9baee0cf74448f60ae24b1ab
username0w <grandcias123@gmail.com>
2024-10-28 22:33:31 +0900

    refactor: GameController 로직 개선

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -2,30 +2,61 @@ package racingcar.controller;
 
 import java.util.List;
 import racingcar.model.Car;
+import racingcar.model.CarManager;
 import racingcar.model.Race;
-import racingcar.utils.CarNameValidator;
-import racingcar.utils.RaceRoundsValidator;
+import racingcar.validator.RaceRoundsValidator;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class GameController {
 
     private final InputView inputView;
-    private final CarNameValidator carNameValidator;
     private final RaceRoundsValidator raceRoundsValidator;
+    private final CarManager carManager;
+    private final OutputView outputView;
+    private Race race;
 
-    public GameController() {
-        this.inputView = new InputView();
-        this.carNameValidator = new CarNameValidator();
-        this.raceRoundsValidator = new RaceRoundsValidator();
+    public GameController(InputView inputView, RaceRoundsValidator raceRoundsValidator, CarManager carManager,
+                          OutputView outputView) {
+        this.inputView = inputView;
+        this.raceRoundsValidator = raceRoundsValidator;
+        this.carManager = carManager;
+        this.outputView = outputView;
     }
 
     public void startGame() {
-        String carNames = inputView.inputCarNames();
-        List<Car> cars = carNameValidator.processCarName(carNames);
-        String raceRounds = inputView.inputRaceRounds();
-        int validRaceRounds = raceRoundsValidator.validateRoundsInput(raceRounds);
+        List<Car> cars = processCarNames();
+        int validRaceRounds = processRaceRounds();
 
-        Race race = new Race(cars, validRaceRounds);
-        race.startRace();
+        race = new Race(cars, validRaceRounds);
+        processRace(race);
+    }
+
+    public Race getRace() {
+        return race;
+    }
+
+    private List<Car> processCarNames() {
+        String carNames = inputView.inputCarNames();
+        return carManager.processCarName(carNames);
+    }
+
+    private int processRaceRounds() {
+        String raceRounds = inputView.inputRaceRounds();
+        return raceRoundsValidator.validateRoundsInput(raceRounds);
+    }
+
+    private void processRace(Race race) {
+        outputView.printRoundResultsHeader();
+        int totalRounds = race.getTotalRounds();
+        List<Car> participatingCars = race.getParticipatingCars();
+
+        for (int i = 0; i < totalRounds; i++) {
+            List<Car> roundCarResult = race.startEachRound(participatingCars);
+            outputView.printRoundResults(roundCarResult);
+        }
+
+        List<String> winners = race.getWinners(participatingCars);
+        outputView.printWinner(String.join(", ", winners));
     }
 }
