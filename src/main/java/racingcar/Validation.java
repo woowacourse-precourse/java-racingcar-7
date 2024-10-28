7cff1ee5c44f2d4b016f66fd0767a9ba59fba7c7
making-a-scene <4936joo@naver.com>
2024-10-28 14:20:25 +0900

    refactor(Validation): 상수 값 final 키워드로 상수화
    자동차 이름을 구분하는 구분자 ",",
    경주 진행 최소 횟수 1,
    최대 횟수 100

--- src/main/java/racingcar/Validation.java
+++ src/main/java/racingcar/Validation.java
@@ -7,13 +7,16 @@ import java.util.regex.Pattern;
 
 public class Validation {
     private static final String CAR_NAME_INPUT_PATTERN = "[a-zA-Z0-9]{1,5}";
+    private static final String CAR_NAME_DELIMITER = ",";
+    private static final int MIN_LIMIT = 1;
+    private static final int MAX_LIMIT = 100;
 
     public String[] validateCarName(String input) {
         if (!validateTheNumberOfCarNames(input)) {
             throw new IllegalArgumentException("최소 2개 이상의 자동차 이름을 입력해야 합니다.");
         }
 
-        String[] splited = input.split(",");
+        String[] splited = input.split(CAR_NAME_DELIMITER);
         if (!validateCarNamesUniqueness(splited)) {
             throw new IllegalArgumentException("모든 자동차는 서로 다른 이름을 가져야 합니다.");
         }
@@ -26,7 +29,7 @@ public class Validation {
     }
 
     private boolean validateTheNumberOfCarNames(String input) {
-        if (!input.contains(",")) {
+        if (!input.contains(CAR_NAME_DELIMITER)) {
             return false;
         }
         return true;
@@ -52,7 +55,7 @@ public class Validation {
     public int validateRoundLimit(String input) {
         try {
             int limit = Integer.parseInt(input);
-            if (limit <= 0 || limit > 100) {
+            if (!(limit >= MIN_LIMIT && limit <= MAX_LIMIT)) {
                 throw new IllegalArgumentException("입력된 수가 범위를 벗어납니다. 1번 이상, 100번 이하로만 시도 가능합니다.");
             }
             return limit;
