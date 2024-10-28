f2b7a75bc6ab12abccb3de4b36e95d327d932c26
Seongmo Ahn <djgood780@gmail.com>
2024-10-28 12:55:19 +0900

    refactor(Output): 시도할 횟수 입력 요청 메세지를 상수로 선언

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -8,12 +8,14 @@ import java.util.StringJoiner;
 public class Output {
     private static final String CAR_NAME_REQUEST_MESSAGE =
             "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String NUMBER_REQUEST_MESSAGE =
+            "시도할 횟수는 몇 회인가요?";
 
     public static void printCarNameRequestMessage(){
         System.out.println(CAR_NAME_REQUEST_MESSAGE);
     }
 
     public static void printNumberRequestMessage(){
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(NUMBER_REQUEST_MESSAGE);
     }
 }
