09df807f20d51607452f22ef6e22110cddd3e8b3
sanikani <sanikani@naver.com>
2024-10-28 13:14:14 +0900

    feat: 자동차 이동 판별 기능
    - Service 단에서 이동을 판별하고 Car 의 상태 변경

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -6,9 +6,22 @@ import racingcar.model.Car;
 
 public class RacingCarService {
 
-    public void race(List<String> carNameList, int tryCount) {
-        List<Car> carList = carNameList.stream()
+    public List<Car> initializeCars(List<String> carNameList) {
+        return carNameList.stream()
                 .map(Car::new)
+                .toList();
+    }
+
+    public void moveCars(List<Car> carList) {
+        carList.forEach(this::tryToMoveCar);
+    }
+
+    private void tryToMoveCar(Car car) {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            car.move();
+        }
+    }
+
                 .toList();
     }
 }
