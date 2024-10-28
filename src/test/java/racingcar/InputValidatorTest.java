bf72635aad7ac817a0845d403ba582770163d885
Seongmo Ahn <djgood780@gmail.com>
2024-10-28 22:06:31 +0900

    test(Validator): 검증 클래스 분리에 따라 검증 테스트 클래스도 분리

--- src/test/java/racingcar/InputValidatorTest.java
+++ src/test/java/racingcar/InputValidatorTest.java
@@ -3,19 +3,24 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.Validator.CarNameValidator;
+import racingcar.Validator.CarNamesValidator;
+import racingcar.Validator.InputValidator;
+import racingcar.Validator.NumberValidator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputValidatorTest {
     /** 테스트 클래스 명 -> [메서드 명]_[테스트 중 상태]_[기대 행위] **/
     @ParameterizedTest
-    @ValueSource(strings = {"Ahn;Seong;Mo", "Ahn", "Ahn,Seong,Ahn"})
+    @ValueSource(strings = {""," ","Ahn;Seong;Mo", "Ahn", "Ahn,Seong,Ahn"})
     @DisplayName("잘못된 자동차 이름 문자열 입력 예외 발생")
     void validateCarNames_InputtedWrongCarNames_ExceptionThrown(String carNames){
         //given
+        final CarNamesValidator carNamesValidator = new CarNamesValidator();
         final CarNameValidator carNameValidator = new CarNameValidator();
         final NumberValidator numberValidator = new NumberValidator();
-        final InputValidator inputValidator = new InputValidator(carNameValidator, numberValidator);
+        final InputValidator inputValidator = new InputValidator(carNamesValidator, carNameValidator, numberValidator);
 
         //when & then
         assertThatThrownBy(() -> inputValidator.validateCarNames(carNames))
@@ -27,9 +32,10 @@ public class InputValidatorTest {
     @DisplayName("잘못된 자동차 이름 입력 예외 발생")
     void validateCarName_InputtedWrongCarName_ExceptionThrown(String carNames){
         //given
+        final CarNamesValidator carNamesValidator = new CarNamesValidator();
         final CarNameValidator carNameValidator = new CarNameValidator();
         final NumberValidator numberValidator = new NumberValidator();
-        final InputValidator inputValidator = new InputValidator(carNameValidator, numberValidator);
+        final InputValidator inputValidator = new InputValidator(carNamesValidator, carNameValidator, numberValidator);
 
         //when & then
         assertThatThrownBy(() -> inputValidator.validateCarName(carNames))
@@ -37,13 +43,14 @@ public class InputValidatorTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {0, -1, -2, -3, -10, -100})
+    @ValueSource(strings = {"", " ", "0", "-1", "-2", "-3", "-10", "-100"})
     @DisplayName("잘못된 횟수 입력 예외 발생")
-    void validateNumber_InputtedWrongNumber_ExceptionThrown(int number){
+    void validateNumber_InputtedWrongNumber_ExceptionThrown(String number){
         //given
+        final CarNamesValidator carNamesValidator = new CarNamesValidator();
         final CarNameValidator carNameValidator = new CarNameValidator();
         final NumberValidator numberValidator = new NumberValidator();
-        final InputValidator inputValidator = new InputValidator(carNameValidator, numberValidator);
+        final InputValidator inputValidator = new InputValidator(carNamesValidator, carNameValidator, numberValidator);
 
         //when & then
         assertThatThrownBy(() -> inputValidator.validateNumber(number))
