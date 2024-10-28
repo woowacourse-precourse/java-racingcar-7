dab1b22efdc72967c1449f663bbb53876b442492
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-10-28 13:36:13 +0900

    feat(Judge): 최종 우승자 판단 메서드 구현

--- src/main/java/racingcar/model/Judge.java
+++ src/main/java/racingcar/model/Judge.java
@@ -1,8 +1,27 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+
 public class Judge {
 
     public Boolean judgeGoOrStop(Integer randomNumber) {
         return randomNumber >= 4;
+    }
+
+    public ArrayList<String> getWinners(ArrayList<Car> carList) {
+        ArrayList<String> winners = new ArrayList<>();
+        Long maxPosition = 0L;
+
+        for (Car car : carList) {
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+
+        for (Car car : carList) {
+            if (car.getPosition().equals(maxPosition)) {
+                winners.add(car.getName());
+            }
+        }
+
+        return winners;
     }
 }
