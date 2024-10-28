63409af7bff4ff45ea2fb3a79a06f7f550d832a7
youngseo3 <wys4475@gmail.com>
2024-10-28 22:22:01 +0900

    feat: 자동차 이름이 null 혹은 공백 포함 예외 처리

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -2,9 +2,25 @@ package racingcar;
 
 public class InputValidator {
 
+    private static final int MAX_NAME_LENGTH = 5;
+
     public static void validateCarName(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException();
+        validateNameIsNull(name);
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("차의 이름은 5글자 이하여야 합니다.");
+        }
+    }
+
+    private static void validateNameIsNull(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("차의 이름이 null 이면 안됩니다.");
+        }
+        validateNameHasSpaces(name);
+    }
+
+    private static void validateNameHasSpaces(String name) {
+        if (name.contains(" ")) {
+            throw new IllegalArgumentException("차의 이름에 공백을 포함하면 안됩니다.");
         }
     }
 
