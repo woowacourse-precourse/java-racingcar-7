c477278e94b15ec980121eeb50c336fd83762f58
김주언 <wndjs803@naver.com>
2024-10-28 23:07:45 +0900

    fix: getter public으로 수정

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -19,44 +19,16 @@ public class Car {
     }
 
     public String getResult() {
-        StringBuilder result = new StringBuilder(getName() + " : ");
+        StringBuilder result = new StringBuilder(name + " : ");
         result.append("-".repeat(Math.max(0, forward)));
         return String.valueOf(result);
     }
 
-    public void printForward(){
-        System.out.print(getName() + " : ");
-        for(int i=0; i<forward; i++){
-            System.out.print("-");
-        }
-        System.out.println();
-    }
-
-    public void printCarName(){
-        System.out.print(getName());
-    }
-
-    public void printCarNameWithComma(){
-        System.out.print(", " + getName());
-    }
-
-    private String getName(){
+    public String getName(){
         return name;
     }
-    private Integer getForward(){
+    public Integer getForward(){
         return forward;
     }
 
-    public static List<Car> getMaxByForward(List<Car> cars) {
-        // 가장 큰 forward 값을 가진 Car 객체 찾기
-        int maxForward = cars.stream()
-                .mapToInt(Car::getForward)
-                .max() // 최대값 찾기
-                .orElseThrow(() -> new IllegalArgumentException("최대값을 찾을 수 없습니다."));
-
-        // 최대값을 가진 Car 객체들 필터링
-        return cars.stream()
-                .filter(car -> car.getForward() == maxForward) // 최대값과 같은 Car 객체들
-                .toList(); // 결과 리스트로 변환
-    }
 }
