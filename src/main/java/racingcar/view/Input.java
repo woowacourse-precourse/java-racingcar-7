fe4dcdc859164e2e85c45a092a0a71579e99719a
mikekks <thdalsrb79@naver.com>
2024-10-28 19:41:35 +0900

    refactor(Input): 자동차 이름이 없거나 빈 문자열일 경우 예외 처리

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -7,9 +7,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
+	private static final String DELIMITER = ",";
+
 	public static List<String> readCarNames() {
 		String carNames = Console.readLine();
-		String[] split = carNames.split(",");
+		String[] split = carNames.split(DELIMITER);
+
+		if (split.length == 0 || Arrays.stream(split).allMatch(String::isBlank)) {
+			throw new IllegalArgumentException("자동차 이름을 하나 이상 입력해주세요.");
+		}
 
 		return Arrays.stream(split)
 			.toList();
@@ -18,7 +24,7 @@ public class Input {
 	public static int readTurn() {
 		String turn = Console.readLine();
 
-		if (isNumeric(turn) == false) {
+		if (!isNumeric(turn)) {
 			throw new IllegalArgumentException("숫자를 입력해주세요.");
 		}
 
