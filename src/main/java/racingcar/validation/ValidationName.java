9921e949d4c4ade65f55a763bc18bed1d8fc0fb2
윤민수 <koelysa5696@naver.com>
2024-10-28 19:28:57 +0900

    Refactor: 이름과 시도횟수 검증 로직 메서드 분리

--- src/main/java/racingcar/validation/ValidationName.java
+++ src/main/java/racingcar/validation/ValidationName.java
@@ -7,12 +7,28 @@ public class Validation {
 
     public static void validateName(String carName) {
         String regex = "^[a-zA-Z0-9가-힣]+$";
+        try {
+            isNameEmpty(carName);
+            validateNameInput(carName, regex);
+            validateNameLength(carName);
+        } catch (IllegalArgumentException e) {}
 
+    }
+
+    public static void isNameEmpty(String carName) {
         if (carName.isEmpty()) {
             throw new IllegalArgumentException("이름이 제대로 입력되지 않았습니다.");
-        } else if (!carName.matches(regex)) {
+        }
+    }
+
+    public static void validateNameInput(String carName, String regex) {
+        if (!carName.matches(regex)) {
             throw new IllegalArgumentException("이름은 영문, 한글, 숫자로 띄어쓰기없이 입력하십시오.");
-        } else if (carName.length() > 5) {
+        }
+    }
+
+    public static void validateNameLength(String carName) {
+        if (carName.length() > 5) {
             throw new IllegalArgumentException("이름은 5글자를 넘기면 안됩니다.");
         }
     }
