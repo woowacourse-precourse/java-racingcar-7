cfc7923a0684534ab857cb23a46d36f1cf833bc0
JiEung2 <181757@jnu.ac.kr>
2024-10-28 22:59:27 +0900

    fix(application): inputView에 validator 의존성 주입으로 인한 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,8 +2,8 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        Game game = new Game(new InputView(), new OutputView(), new CarGenerator(), new WinnerDecider(),
-                new InputValidator(), new RandomNumberGenerator());
+        Game game = new Game(new InputView(new InputValidator()), new OutputView(), new CarGenerator(),
+                new WinnerDecider(), new RandomNumberGenerator());
 
         game.run();
     }
