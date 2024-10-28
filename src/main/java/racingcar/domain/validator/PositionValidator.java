3c9115faa138bac3d980ae77f93686b8e021bba3
hootzz <wjqwjql3@gmail.com>
2024-10-28 22:53:30 +0900

    style(validator): 예외 처리 문구 수정

--- src/main/java/racingcar/domain/validator/PositionValidator.java
+++ src/main/java/racingcar/domain/validator/PositionValidator.java
@@ -3,7 +3,7 @@ package racingcar.domain.validator;
 public class PositionValidator {
     public void validate(int position) {
         if (position < 0) {
-            throw new IllegalArgumentException("위치는 0 이상의 정수여야 합니다.");
+            throw new IllegalArgumentException("시도 회수는 0 이상의 정수여야 합니다.");
         }
     }
 }
