c9902026bc17d1f5dc82a0367c711a33eb698a02
ppparkta <pppppark2@gmail.com>
2024-10-28 11:51:41 +0900

    refactor(InputView): inputConsole에 출력 추가
    변경사항
    - inputConsole 메서드에 placeholder 출력 추가
    
    사용자 관점에서 입력에는 입력 설명도 포함되므로 inputView로 이동

--- src/main/java/racingcar/racing/view/InputView.java
+++ src/main/java/racingcar/racing/view/InputView.java
@@ -5,8 +5,9 @@ import static racingcar.racing.utils.ExceptionMessages.IO_EXCEPTION_MESSAGE;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String inputConsole() {
+    public static String inputConsole(String placeholder) {
         try {
+            System.out.println(placeholder);
             return Console.readLine().replace(" ", "");
         } catch (Exception e) {
             throw new IllegalArgumentException(IO_EXCEPTION_MESSAGE);
