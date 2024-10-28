1af97b12342bb59902bea35c18d5f8e5eb4ddafb
hootzz <wjqwjql3@gmail.com>
2024-10-28 22:32:23 +0900

    style(view): 출력 문자열에 공백 추가

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -20,6 +20,6 @@ public class ResultView {
     }
 
     public void printWinners(String winners) {
-        System.out.println("최종 우승자: " + winners);
+        System.out.println("최종 우승자 : " + winners);
     }
 }
