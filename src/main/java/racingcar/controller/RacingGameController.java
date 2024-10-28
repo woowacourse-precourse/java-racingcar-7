57e9d2df724189753fd19481fd58d052d908d9a1
onesay17 <onesay17@gmail.com>
2024-10-28 12:31:17 +0900

    refactor: 경주 과정, 결과 로직 서비스 클래스로 분리

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,8 +1,6 @@
 package racingcar.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import racingcar.domain.Car;
 import racingcar.service.RacingGameService;
 import racingcar.validation.RaceCountValidator;
@@ -32,11 +30,11 @@ public class RacingGameController {
         String raceCount = inputView.getRaceCountFormView();
         raceCountValidator.validate(raceCount);
 
-        runCarRaceAndPrintResults(Integer.valueOf(raceCount), racingCars);
+        runRaceAndPrintRoundResults(Integer.valueOf(raceCount), racingCars);
         findWinnersAndPrintResults(racingCars);
     }
 
-    private void runCarRaceAndPrintResults(Integer raceCount, List<Car> racingCars) {
+    private void runRaceAndPrintRoundResults(Integer raceCount, List<Car> racingCars) {
 
         System.out.println();
         System.out.println("실행 결과");
@@ -52,32 +50,6 @@ public class RacingGameController {
     }
 
     private void findWinnersAndPrintResults(List<Car> racingCars) {
-        Integer maxDistance = getMaxDistance(racingCars);
-
-        List<String> winnerCars = getWinners(racingCars, maxDistance);
-        String winners = String.join(", ", winnerCars);
-        outPutView.printWinners(winners);
-    }
-
-    private Integer getMaxDistance(List<Car> racingCars) {
-
-        Integer maxDistance = 0;
-
-        for (Car car : racingCars) {
-            if (car.getDistance() > maxDistance) {
-                maxDistance = car.getDistance();
-            }
-        }
-        return maxDistance;
-    }
-
-    private List<String> getWinners(List<Car> racingCars, Integer maxDistance) {
-        List<String> winners = new ArrayList<>();
-        for (Car car : racingCars) {
-            if (Objects.equals(car.getDistance(), maxDistance)) {
-                winners.add(car.getName());
-            }
-        }
-        return winners;
+        outPutView.printWinners(String.join(", ", racingGameService.getWinners(racingCars)));
     }
 }
