695acf616fbc3236c7089a36474128cbda9d3fc7
dayeondayeon <dayeon2438@gmail.com>
2024-10-28 16:59:51 +0900

    feat : 안내문 콘솔에 출력하는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,12 +8,23 @@ import static racingcar.Utils.*;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분");
         String[] carNames = getNamesFromString(readLine());
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
         int tryCount = getTryCountFromString(readLine());
 
-        List<Vehicle> vehicles = new ArrayList<>(carNames.length);
+        List<Car> vehicles = new ArrayList<>(carNames.length);
         for (String carName : carNames) {
             vehicles.add(new Car(carName, 0));
+        }
+
+        System.out.println("실행 결과");
+        for (int i = 0; i <tryCount; i++) {
+            for (Car car : vehicles) {
+                car.move();
+                car.printLocation();
+            }
         }
     }
 }
