474bdcb6fba9521b0c19157ca51f5a23756f9bf4
Chan531 <chan657312@gmail.com>
2024-10-28 17:55:40 +0900

    refactor(WinnerResultGenerator): 다수 우승자 체크 메소드 분리

--- src/main/java/racingcar/model/output/WinnerResultGenerator.java
+++ src/main/java/racingcar/model/output/WinnerResultGenerator.java
@@ -8,10 +8,14 @@ public class WinnerResultGenerator {
 	private static final String SEPARATOR = ", ";
 
 	public static String generateWinnerResult(List<String> winners) {
-		if (winners.size() > SINGLE_WINNER_COUNT) {
+		if (checkJointWinners(winners)) {
 			return generateJointWinnersResult(winners);
 		}
 		return winners.getFirst();
+	}
+
+	private static boolean checkJointWinners(List<String> winners) {
+		return winners.size() > SINGLE_WINNER_COUNT;
 	}
 
 	private static String generateJointWinnersResult(List<String> winners) {
