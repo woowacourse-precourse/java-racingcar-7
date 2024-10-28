238d69abaa647d44133ecd005c7bd14a8b5f264d
eundeang <111877048+eundeang@users.noreply.github.com>
2024-10-28 10:42:37 +0900

    feat(OutputView): 우승자 출력 메서드 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,21 +1,17 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.model.Car;
 
 public class OutputView {
-
-    private final Car car;
-
-    public OutputView(Car car) {
-        this.car = car;
-    }
 
     public void printCarNameProvideMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public void printTryCountProvideMessage() {
-        System.out.println("시도할 횟수");
+        System.out.println("시도할 횟수는 몇 회인가요?");
     }
 
     public void printExecutionResult() {
@@ -24,6 +20,14 @@ public class OutputView {
 
     public void printCarNameAndMovingStatus() {
         System.out.println(car.getName() + " : " + car.getMoveStatus());
+    }
+
+    public void printWinningPeople(List<Car> winningCars) {
+        System.out.print("최종 우승자 : ");
+        final String delimiter = ",";
+        final String listToStringWithDelimiter = winningCars.stream().map(Car::getName)
+                .collect(Collectors.joining(delimiter));
+        System.out.println(listToStringWithDelimiter);
     }
 
 }
