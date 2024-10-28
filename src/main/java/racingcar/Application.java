73da1fe8f9b9d7e9a5c9743ddd32cac37f325984
이태구 <alexlee9841@gmail.com>
2024-10-28 22:08:48 +0900

    메인 클래스에서 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,7 +8,12 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingGame racingGame = new RacingGame();
+
+        racingGame.inputCars();
+        racingGame.inputRound();
+        racingGame.runGame();
+        racingGame.printWinners();
     }
 }
 
