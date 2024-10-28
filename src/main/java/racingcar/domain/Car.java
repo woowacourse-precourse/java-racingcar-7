219576ae11c496d5d961af269110e27afe703915
dltmddus <tmddus4671@gmail.com>
2024-10-28 11:05:02 +0900

    refactor: Car 클래스의 변수 접근 제어자 public -> private 수정, getter 함수 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,10 +1,25 @@
 package racingcar.domain;
 
 public class Car {
-    public String name;
-    public int movementOfNumber = 0;
+    private String name;
+    private int movementOfNumber = 0;
+    private static final int MINIMUM_MOVEMENT_THRESHOLD = 4;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public void move(int NumberOfMoves) {
+        if (NumberOfMoves >= MINIMUM_MOVEMENT_THRESHOLD) {
+            movementOfNumber++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMovementOfNumber() {
+        return movementOfNumber;
     }
 }
