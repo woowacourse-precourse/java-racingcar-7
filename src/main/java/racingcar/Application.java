cac17c63427a661747c7d2e9672ed96f9f63520c
bongj9 <lollypark501@naver.com>
2024-10-28 18:41:42 +0900

    feat: 입력문을 메서드화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,21 +8,12 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
         try {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-            String[] carNames = Console.readLine().split(",");
-            List<Car> cars = new ArrayList<>();
-            for (String name : carNames) {
-                name = name.trim();
-                if (name.length() > 5) {
-                    throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다");
-                }
-                cars.add(new Car(name));
-            }
+            List<Car> cars = inputName();
             System.out.println("시도할 횟수는 몇 회인가요?");
             int trial = Integer.parseInt(Console.readLine());
-            if (trial < 0) {
+            if (trial <= 0) {
                 throw new IllegalArgumentException("시도 횟수는 1회 이상이어야 합니다.");
             }
             System.out.println("실행 결과");
@@ -36,6 +27,20 @@ public class Application {
         } catch (IllegalArgumentException e) {
             System.out.println("잘못된 입력입니다: " + e.getMessage());
         }
+    }
+
+    //입력문 기능으로 빼기
+    private static List<Car> inputName() {
+        String[] carNames = Console.readLine().split(",");
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            name = name.trim();
+            if(name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다. ");
+            }
+            cars.add(new Car(name));
+        }
+        return cars;
     }
 
     //매판 결과값
