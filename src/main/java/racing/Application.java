f4885e6e5348b78c8c0fccc885f35708ce6a0229
HwangBBang <h1009218@hufs.ac.kr>
2024-10-28 16:27:17 +0900

    feat(구현 목록#2) : 1회, 수행 레이싱 참가 차량 k개에 대해서 구현
    - 입력 검증 로직 추가
    - 여러 라운드 로직 주석 처리
    - 변수명 수정
    - 기능 구현 목록 수정

--- src/main/java/racing/Application.java
+++ src/main/java/racing/Application.java
@@ -4,25 +4,28 @@ import camp.nextstep.edu.missionutils.Console;
 import racing.validation.InputValidator;
 
 public class Application {
-    private static InputValidator validator = new InputValidator();
+    private final static InputValidator validator = new InputValidator();
     public static void main(String[] args) {
 
         // todo : n회, 수행 레이싱 참가 차량 1개에 대해서 구현
         // 사용자로부터 자동차 이름을 입력받음
         System.out.println("경주할 자동차 이름을 입력하세요.");
-        String carName = Console.readLine();
-        validator.validateCarName(carName);
+        String carNameStr = Console.readLine();
+        String[] carNames = carNameStr.split(",");
+        validator.validateCarNames(carNames);
 
-        int carPosition = 0;
+        int carCnt = carNames.length;
+
+        int[] carPositions = new int[carCnt];
 
         // 사용자로부터 시도할 횟수를 입력받음
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int gameRound = Integer.parseInt(Console.readLine());
-        validator.validateGameRound(gameRound);
+//        System.out.println("시도할 횟수는 몇 회인가요?");
+//        int gameRound = Integer.parseInt(Console.readLine());
+//        validator.validateGameRound(gameRound);
 
-        for (int i = 1; i <= gameRound; i++) {
-            if (isForward()) carPosition++;
-            System.out.println(carName + ":" + drawLogLine(carPosition));
+        for (int carIdx = 0; carIdx < carCnt; carIdx++) {
+            if (isForward()) carPositions[carIdx]++;
+            System.out.println(carNames[carIdx] + ":" + drawLogLine(carPositions[carIdx]));
         }
 
     }
