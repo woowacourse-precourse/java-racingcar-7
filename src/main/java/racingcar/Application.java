1f718fa2c6e2dad038a2349c0b0f80d261a96d16
Grayson1999 <bbt1250912@gmail.com>
2024-10-28 21:08:32 +0900

    fix: 사용자 입력 시 질문 내용이 나오지 않는 오류 수정
    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)","시도할 횟수는 몇 회인가요?" 같은 입력 질문이 나오지 않는 오류를 수정함.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -15,7 +15,9 @@ public class Application {
         List<String> winners;
 
         //사용자 입력
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         inputCarNames = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
         String inputNumber = Console.readLine();
 
         //사용자 입력 검증
