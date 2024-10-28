e1e43802c8233aa8c293a0b648a39e9cfa1bef30
sseopsseop <ponde119@outlook.com>
2024-10-28 17:41:06 +0900

    feat: 상수 저장 클래스 내용 추가 출력 안내 문구 2가지 추가함.

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -4,4 +4,6 @@ public final class Constants {
     private Constants() {}
     public static final String NOTICE_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String NOTICE_INPUT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+    public static final String NOTICE_OUTPUT_ROUND_RESULT = "실행 결과";
+    public static final String NOTICE_OUTPUT_FINAL_RESULT = "최종 우승자 : ";
 }
