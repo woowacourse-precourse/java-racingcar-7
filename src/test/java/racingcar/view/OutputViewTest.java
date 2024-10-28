043b1dc8458176f650a648eef74827e1f35713d0
imjanghyeok <qkdrhkgn23@naver.com>
2024-10-28 15:02:04 +0900

    test(OutputViewTest): 경주 시작시 출력 테스트 및 자동차 이동후 진행 결과 출력 테스트
    - 컨트롤러에서 자동차 이동 loop 진입 전 출력을 테스트합니다.
    - 자동차가 이동한 후에 결과물 출력을 테스트합니다.

---
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -0,0 +1,52 @@
+package racingcar.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Cars;
+import racingcar.model.strategy.MaxValueIntGeneratorStrategy;
+
+public class OutputViewTest {
+    private final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(byteArrayOutputStream));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void Given_경주_시작_When_호출시_Expect_메시지를_출력한다() {
+        OutputView outputView = new OutputView();
+        outputView.printRacingStart();
+
+        assertThat(byteArrayOutputStream.toString().trim()).isEqualTo("실행 결과");
+    }
+
+    @Test
+    public void Given_Cars_객체_생성_When_자동차_이동시_Expect_메시지를_출력한다() {
+        final List<String> carNames = Arrays.asList("car1", "car2", "car3");
+        final Cars cars = new Cars(carNames, new MaxValueIntGeneratorStrategy());
+
+        cars.getCars().get(0).tryMove();
+        cars.requestCarMove();
+
+        OutputView outputView = new OutputView();
+        outputView.printStageResult(cars);
+        assertThat(byteArrayOutputStream.toString().trim()).isEqualTo(
+                cars.getCars().get(0).getCarName() + " : " + "--" + "\n"
+                + cars.getCars().get(1).getCarName() + " : " + "-" + "\n"
+                + cars.getCars().get(2).getCarName() + " : " + "-"
+        );
+    }
+}
