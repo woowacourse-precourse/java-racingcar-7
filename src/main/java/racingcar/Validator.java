90d207d59711bae40d3e0132898ec3fe76446215
gichan222 <>
2024-10-28 15:56:44 +0900

    feat(Validator): 자동차 입력에 대한 검증 코드 작성
    입력된 자동차에 대한 검증 진행
    자동차 이름이 5글자를 초과하는 경우, 자동차가 중복되는 경우에 대한 검증

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Validator {
     private static final Validator instance = new Validator();
 
@@ -13,6 +16,12 @@ public class Validator {
         validateBlankTryCount(input);
         int numberOfRounds = parseNumber(input);
         validatePositiveNumber(numberOfRounds);
+    }
+
+    public void validateCarNames(String input) {
+        List<String> carList = Arrays.asList(input.split(","));
+        validateDuplicatedCarName(carList);
+        validateCarNameLength(carList);
     }
 
     private void validatePositiveNumber(int tryCounts) {
@@ -34,4 +43,18 @@ public class Validator {
             throw new IllegalArgumentException("시도할 횟수를 입력해야합니다.");
         }
     }
+
+    private void validateCarNameLength(List<String> carList) {
+        if (carList.stream().anyMatch(carName -> carName.length() > 5)) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하이어야 합니다");
+        }
+    }
+
+
+    private void validateDuplicatedCarName(List<String> carList) {
+        if (carList.size() != carList.stream().distinct().count()) {
+            throw new IllegalArgumentException("중복된 자동차가 있습니다");
+        }
+    }
+
 }
