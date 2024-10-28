bad76ae06a16a2bc5edc64d29529873a3693456b
Sujin Kim <su10jin11@khu.ac.kr>
2024-10-28 18:54:30 +0900

    feat : 출력 메시지 상수로 변경

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -5,19 +5,26 @@ import racingcar.model.Car;
 import racingcar.model.Cars;
 
 public class Output {
+    private static final String OUTPUT_MESSAGE = "실행 결과";
+    private static final String DELIMITER = " : ";
+    private static final String MOVE_DELIMITER = "-";
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String WINNER_DELIMETER = ", ";
+
+
     public void printExecutionResult() {
-        System.out.println("실행 결과");
+        System.out.println(OUTPUT_MESSAGE);
     }
 
     public void printRoundResult(Cars cars) {
         for (Car car : cars.getCars()) {
-            System.out.print(car.getName() + " : ");
-            System.out.println("-".repeat(car.getPosition()));
+            System.out.print(car.getName() + DELIMITER);
+            System.out.println(MOVE_DELIMITER.repeat(car.getPosition()));
         }
         System.out.println();
     }
 
     public void printWinners(List<String> winners) {
-        System.out.println("최종 우승자 : " + String.join(", ", winners));
+        System.out.println(FINAL_WINNER_MESSAGE + String.join(WINNER_DELIMETER, winners));
     }
 }
