f361d8dfb74fd7b7267f26d9ec67e0062af7dbff
sangxxjin <sj_0368@naver.com>
2024-10-28 16:17:00 +0900

    feat: 자동차 모델들 생성 메서드 구현
    본문:
    자동차들의 이름을 받아서 자동차 모델들을 생성하게 구현했습니다.

---
+++ src/main/java/racingcar/services/CarService.java
@@ -0,0 +1,14 @@
+package racingcar.services;
+
+import java.util.Arrays;
+import java.util.List;
+import racingcar.models.Cars;
+
+public class CarService {
+    private Cars cars;
+
+    public void createCarModel(String carNames) {
+        List<String> carNameList = Arrays.asList(carNames.split(","));
+        this.cars = new Cars(carNameList);
+    }
+}
