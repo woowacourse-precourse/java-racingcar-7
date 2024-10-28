ff06f0a5ccd51ea3b93a7d9d90753e8b7c70c1b9
Jihyun3478 <leejihyun3478@gmail.com>
2024-10-28 20:32:14 +0900

    test : AttemptNumberTest 추가
    AttemptNumberTest 추가
    - 시도 횟수가 음수일 경우
    - 시도 횟수가 0일 경우

---
+++ src/test/java/racingcar/model/domain/AttemptNumberTest.java
@@ -0,0 +1,31 @@
+package racingcar.model.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import camp.nextstep.edu.missionutils.test.Assertions;
+
+class AttemptNumberTest {
+
+	@ParameterizedTest
+	@ValueSource(ints = { -50, -10, -1 })
+	void 시도_횟수가_음수일_경우_예외(int attemptNumber) {
+		Assertions.assertSimpleTest(() -> {
+			assertThatThrownBy(() -> Validator.validateAttemptNumber(attemptNumber))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage(ErrorMessage.NEGATIVE_ATTEMPT_NUMBER.getMessage());
+		});
+	}
+
+	@Test
+	void 시도_횟수가_0일_경우_예외() {
+		Assertions.assertSimpleTest(() -> {
+			assertThatThrownBy(() -> Validator.validateAttemptNumber(0))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage(ErrorMessage.NEGATIVE_ATTEMPT_NUMBER.getMessage());
+		});
+	}
+}
