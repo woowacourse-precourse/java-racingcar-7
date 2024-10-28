11cc46d3c0122ab203d6b7570dadef19ae6c1813
장영재 <urinaner@naver.com>
2024-10-28 12:56:27 +0900

    feat(OutputView): 현재 상태 출력 메서드

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,21 +1,22 @@
 package racingcar.view;
 
 import java.util.List;
+import racingcar.domain.Car;
 
 public class OutputView {
     private static final String WINNER_MESSAGE = "최종 우승자 : ";
     private static final String LINE = "\n";
 
-    public void printCarsResult(String carsPositionFormat) {
-        System.out.println(carsPositionFormat + LINE);
-    }
 
     public void printWinners(List<String> winners) {
         String winnerNames = String.join(", ", winners);
         System.out.println(WINNER_MESSAGE + winnerNames);
     }
 
-    public void printInitialCarPosition(String carsPositionFormat) {
-        System.out.println(carsPositionFormat + LINE);
+    public void printStatus(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getCurrentStateFormat()); // 포맷된 상태 문자열을 출력
+        }
+        System.out.println();
     }
 }
