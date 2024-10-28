1b2f51a22321c977721678da8078a200380df4de
gahyeon <1000ji1@naver.com>
2024-10-28 21:45:30 +0900

    feat: 최종 우승자 출력

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -32,6 +32,7 @@ public class RacingGameController {
         playRounds(round, cars);
 
         List<Car> winners = winnerService.getWinners(cars);
+        OutputView.finalWinner(winners);
     }
 
     private Round setupRound() {
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,10 +1,14 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
 
 public class OutputView {
     private static final String EXECUTION_RESULTS = "실행 결과";
+    private static final String FINAL_WINNER = "최종 우승자 : ";
+
 
     public static void executionResult() {
         System.out.println();
@@ -16,5 +20,12 @@ public class OutputView {
             System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
         }
         System.out.println();
+    }
+
+    public static void finalWinner(List<Car> winners) {
+        var winnerNames = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println(FINAL_WINNER + winnerNames);
     }
 }
