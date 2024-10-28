f2ea01f78149f4b947365e3e5dee057e351e18ca
sanghun <vitash1215@gmail.com>
2024-10-28 20:42:40 +0900

    feat(main) : main 함수 작성
    생성해야할 객체 생성 및 의존성 주입, Game 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.validator.Validator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Validator validator = new Validator();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        GameController gameController = new GameController(validator, inputView, outputView);
+        gameController.startGame();
     }
 }
