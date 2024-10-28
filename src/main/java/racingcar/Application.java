e64952b7059a46bdfad70dd1434f7d16bb97f0b4
zerocat36 <Seo.james2020@gmail.com>
2024-10-28 20:29:08 +0900

    feat($Application): add getRacingCarNames function
    Added new function to $Application
    - get name of racing cars in String type
    - convert names into List<String> type and return it

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.Map;
+import java.util.Collections;
+
 public class Application {
+    private static List<String> getRacingCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        try {
+            return Stream.of(Console.readLine().split("[\\s,]+")).toList();
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
 }
