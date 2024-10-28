77332fc905d6021125aae909f9eed732cfd81d37
ouvir <kilhyunii@naver.com>
2024-10-28 17:04:47 +0900

    refactor: Application main 로직에서 메서드 분리
    가독성을 위해, main 메서드 분리
    
    Before:
    main()
    - 시도 횟수만큼 반복
    - 모든 차량들에 대해, 진행 시도
    
    After:
    main()
    - 시도 횟수만큼 반복
    runProcess()
    - 모든 차량들에 대해, 진행 시도

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.process.MovePolicy;
+import racingcar.policy.MovePolicy;
+import racingcar.policy.RandomMovePolicy;
 import racingcar.race.Race;
 import racingcar.separator.InputSeparator;
 import racingcar.view.RaceView;
@@ -19,18 +20,21 @@ public class Application {
 
         List<String> carNames = InputSeparator.split(text);
         Race race = new Race(carNames);
-        for (int i = 0; i < count; i++) {
-            for (String car : carNames) {
-                if (policy.canMove()) {
-                    race.updateProgress(car);
-                }
-            }
 
+        for (int i = 0; i < count; i++) {
+            runProcess(race, carNames);
             RaceView.displayProgress(race.getRaceProgress());
         }
 
         List<String> winners = race.getWinners();
         RaceView.displayWinners(winners);
+    }
 
+    private static void runProcess(Race race, List<String> carNames) {
+        for (String car : carNames) {
+            if (policy.canMove()) {
+                race.updateProgress(car);
+            }
+        }
     }
 }
