70f8311e9d8ddea512eff048ce2ef3ee2644a7f4
taehyun <xogus5070318@gmail.com>
2024-10-28 21:38:25 +0900

    refactor(Validation,RacingGame): 레이싱게임 예외처리 메소드 구조 개선
    예외처리 테스트를 가능하게하되, 레이싱게임의 캡슐화를 유지시키기 위해
    레이싱게임의 예외처리 메소드를 Validation 클래스를 생성하여 이동시켰습니다.
    이렇게하면, 레이싱게임 메소드에서 제어자 변경을 통해 캡슐화를 약화시키지 않습니다.
    
    BREAKING CHANGE: None

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -9,6 +9,7 @@ public class RacingGame {
     ArrayList<Car> cars = new ArrayList<>();
     ArrayList<String> winners = new ArrayList<>();
     int maxTurn = 0;
+    Validation validation = new Validation();
 
     public void runGame() {
 
@@ -21,17 +22,17 @@ public class RacingGame {
     private void setGame() {
 
         String[] carNameArray = inputCarName(); // 자동차 이름 입력받아서 배열에 저장
-        validateCarName(carNameArray); // 자동차 입력부분 예외처리
+        validation.validateCarName(carNameArray); // 자동차 입력부분 예외처리
 
         // 자동차 정보 저장
         for (String carName : carNameArray) {
 
-            checkDuplicateCar(carName); // 자동차 이름 중복 예외처리
+            validation.checkDuplicateCar(cars, carName); // 자동차 이름 중복 예외처리
             cars.add(new Car(carName));
         }
 
         String maxTurnString = inputMaxTurn(); // 이동횟수 입력받아서 문자열에 저장
-        validateMaxTurn(maxTurnString); // 이동횟수 입력부분 예외처리
+        validation.validateMaxTurn(maxTurnString); // 이동횟수 입력부분 예외처리
         maxTurn = Integer.parseInt(maxTurnString);
 
     }
@@ -66,45 +67,6 @@ public class RacingGame {
         return carNameString.split(",");
     }
 
-    /// 자동차 입력부분 예외처리
-    void validateCarName(String[] carNameArray) {
-
-        if (carNameArray.length > 8) {
-            throw new IllegalArgumentException("자동차 이름은 최대 8개까지 입력 가능합니다.");
-        }
-        if (carNameArray.length < 2) {
-            throw new IllegalArgumentException("자동차 이름은 최소 2개 이상 입력해야합니다.");
-        }
-
-        for (String carName : carNameArray) {
-
-            carName = carName.trim();
-
-
-            if (carName.length() < 2) {
-                throw new IllegalArgumentException("자동차 이름은 2자 이상으로 입력해야합니다.");
-            }
-            if (carName.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력해야입니다.");
-            }
-
-            // 자동차 이름이 알파벳 대소문자로만 이루어져있는지 확인
-            if (!carName.matches("[a-zA-Z]+")) {
-                throw new IllegalArgumentException("자동차 이름은 알파벳 대소문자만 가능합니다.");
-            }
-
-        }
-    }
-
-    ///자동차 이름 중복 예외처리
-    void checkDuplicateCar(String carName) {
-
-        for (Car car : cars) {
-            if (carName.equals(car.getCarName())) {
-                throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
-            }
-        }
-    }
 
     private String inputMaxTurn() {
 
@@ -116,26 +78,6 @@ public class RacingGame {
         return maxTrunString;
     }
 
-    void validateMaxTurn(String maxTurnString) {
-
-        int turn = 0;
-
-        try {
-            turn = Integer.parseInt(maxTurnString);
-
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("이동횟수는 숫자만 입력 가능합니다.");
-        }
-
-        if (turn < 1) {
-            throw new IllegalArgumentException("이동횟수는 최소 1 이상입니다.");
-        }
-
-        if (turn > 20) {
-            throw new IllegalArgumentException("이동횟수는 최대 20 이하입니다.");
-        }
-
-    }
 
     /// 자동차 이동
     private void moveCar() {
--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -2,39 +2,43 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("레이싱 게임 테스트")
 public class RacingGameTest {
-
+    Validation validation = new Validation();
     @Test
     @DisplayName("자동차 이름 밸리디에이션 테스트")
     public void testCarNameValidation() {
-        RacingGame game = new RacingGame();
+
 
         // 자동차 수가 9개 이상일 때 오류발생
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validateCarName(new String[]{"car1", "car2", "car3", "car4", "car5", "car6", "car7", "car8", "car9"});
+            validation.validateCarName(new String[]{"car1", "car2", "car3", "car4", "car5", "car6", "car7", "car8", "car9"});
         }, "자동차 이름은 최대 8개까지 입력 가능합니다.");
 
         // 자동차 수가 1개 이하일 때 오류발생
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validateCarName(new String[]{"car1"});
+            validation.validateCarName(new String[]{"car1"});
         },"자동차 이름은 최소 1개 이상 입력 가능합니다.");
 
         // 자동차 이름이 2자 이하일 때 오류발생
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validateCarName(new String[]{"c", "def"});
+            validation.validateCarName(new String[]{"c", "def"});
         }, "자동차 이름은 2자 이상으로 입력해야합니다.");
 
         // 자동차 이름이 6자 이상일 때 오류발생
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validateCarName(new String[]{"carnameiswaytoolong", "abe"});
+            validation.validateCarName(new String[]{"carnameiswaytoolong", "abe"});
         }, "자동차 이름은 5자 이하로 입력해야입니다.");
 
         // 자동차 이름이 알파벳이 아닌 경우 오류발생
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validateCarName(new String[]{"1234"});
+            validation.validateCarName(new String[]{"1234"});
         }, "자동차 이름은 알파벳 대소문자만 가능합니다.");
 
 
@@ -45,7 +49,7 @@ public class RacingGameTest {
     public void testCarNameValidation_TooManyCars() {
         RacingGame game = new RacingGame();
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validateCarName(new String[]{"car1", "car2", "car3", "car4", "car5", "car6", "car7", "car8", "car9"});
+            validation.validateCarName(new String[]{"car1", "car2", "car3", "car4", "car5", "car6", "car7", "car8", "car9"});
         });
     }
 
@@ -54,7 +58,7 @@ public class RacingGameTest {
     public void testCarNameValidation_TooFewCars() {
         RacingGame game = new RacingGame();
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validateCarName(new String[]{"car1"});
+            validation.validateCarName(new String[]{"car1"});
         });
     }
 
@@ -63,7 +67,7 @@ public class RacingGameTest {
     public void testCarNameValidation_TooShortName() {
         RacingGame game = new RacingGame();
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validateCarName(new String[]{"c", "def"});
+            validation.validateCarName(new String[]{"c", "def"});
         });
     }
 
@@ -72,7 +76,7 @@ public class RacingGameTest {
     public void testCarNameValidation_TooLongName() {
         RacingGame game = new RacingGame();
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validateCarName(new String[]{"carnameiswaytoolong", "abe"});
+            validation.validateCarName(new String[]{"carnameiswaytoolong", "abe"});
         });
     }
 
@@ -81,7 +85,7 @@ public class RacingGameTest {
     public void testCarNameValidation_NonAlphabetic() {
         RacingGame game = new RacingGame();
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validateCarName(new String[]{"1234"});
+            validation.validateCarName(new String[]{"1234", "cde"});
         });
     }
 
@@ -89,14 +93,12 @@ public class RacingGameTest {
     @DisplayName("자동차 이름 중복 확인 테스트")
     public void testCarNameDuplication() {
 
-        RacingGame game = new RacingGame();
-
-
-        game.cars.add(new Car("testCar"));
+        ArrayList<Car> cars = new ArrayList<>();
+        cars.add(new Car("testCar"));
 
         // 중복된 자동차 이름이 있는 경우 오류발생
         assertThrows(IllegalArgumentException.class, () -> {
-            game.checkDuplicateCar("testCar");
+            validation.checkDuplicateCar(cars,"testCar");
         }, "자동차 이름은 중복될 수 없습니다.");
     }
 
@@ -107,32 +109,32 @@ public class RacingGameTest {
 
         // 이동횟수가 숫자가 아닐 때
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validateMaxTurn("twenty");
+            validation.validateMaxTurn("twenty");
         }, "이동횟수는 숫자만 입력 가능합니다.");
 
         //이동횟수가 1보다 적을 때
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validateMaxTurn("0");
+            validation.validateMaxTurn("0");
         }, "이동횟수는 최소 1 이상입니다.");
 
 
         // 범위 내 태스트
         assertDoesNotThrow(() -> {
-            game.validateMaxTurn("1");
+            validation.validateMaxTurn("1");
         }, "이동횟수가 최소값 1일 때 예외가 발생하지 않아야 합니다.");
 
         assertDoesNotThrow(() -> {
-            game.validateMaxTurn("10");
+            validation.validateMaxTurn("10");
         }, "이동횟수가 중간값 10일 때 예외가 발생하지 않아야 합니다.");
 
         assertDoesNotThrow(() -> {
-            game.validateMaxTurn("20");
+            validation.validateMaxTurn("20");
         }, "이동횟수가 최대값 20일 때 예외가 발생하지 않아야 합니다.");
 
 
         //이동횟수가 20보다 클 때
         assertThrows(IllegalArgumentException.class, () -> {
-            game.validateMaxTurn("21");
+            validation.validateMaxTurn("21");
         }, "이동횟수는 최대 20 이하입니다.");
 
     }
