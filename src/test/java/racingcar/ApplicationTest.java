7df931a84d7e9c2e0d4b925ac90fe3e0a224d61b
yushinc <cys74351@gmail.com>
2024-10-28 22:04:49 +0900

    feat: 양수 검증 로직 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -7,12 +7,10 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberI
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.Assertions.in;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
-
 
     @Test
     void 기능_테스트() {
@@ -24,41 +22,6 @@ class ApplicationTest extends NsTest {
             MOVING_FORWARD, STOP
         );
     }
-
-    @Test
-    void 자동차_이름_5자_이상_입력() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차의 이름은 5자 이하여야 합니다.")
-        );
-    }
-
-    @Test
-    void 자동차목록_빈문자열_입력() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("", "1"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessage("아무런 내용이 입력되지 않았습니다."));
-    }
-
-    @Test
-    void 실행횟수_빈문자열_입력() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi, woni", ""))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessage("아무런 내용이 입력되지 않았습니다."));
-    }
-
-
-    @Test
-    void 숫자가_아닌_실행횟수_입력() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("pobi, woni", "a"))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessage("숫자를 입력해 주세요."));
-    }
-
 
     @Override
     public void runMain() {
