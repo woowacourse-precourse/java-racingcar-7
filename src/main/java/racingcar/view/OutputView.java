a6c8770fcfc51c5a907ac3a72f73cb8dfc9ef1ba
kaswhy <sxshx818@naver.com>
2024-10-28 16:02:38 +0900

    fix(OutputView): 실행 결과 나타내는 상수 앞부분에 개행 추가
    변경 전
    - "실행 결과"
    
    변경 후
    - "\n실행 결과"

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,7 +7,7 @@ import racingcar.model.Distance;
 public class OutputView {
     private static final String INPUT_CAR_NAME_GUIDE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_RACE_COUNT_GUIDE = "시도할 횟수는 몇 회인가요?";
-    private static final String EXECUTION_RESULT = "실행 결과";
+    private static final String EXECUTION_RESULT = "\n실행 결과";
     private static final String ROAD_EXPRESSION = "-";
     private static final String NAME_SCORE_DELIMITER = " : ";
     private static final String LAST_WINNER = "최종 우승자 : ";
