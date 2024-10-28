a0d23f00c9258e96e9a3e0f248ad78b77158cc00
SeuneKeun <eiziong1@gmail.com>
2024-10-28 21:08:08 +0900

    refactor: 입력 콘솔 클래스 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,10 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String cars = Console.readLine();
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String tryTimesString = Console.readLine();
-        System.out.println("\n실행 결과");
+        String cars = InputConsole.inputCarNames();
+        String tryTimesString = InputConsole.inputTryTimes();
+        InputConsole.resultGide();
 
         RacingCar racingCar = new RacingCar();
         racingCar.start(cars, tryTimesString);
