59c9b2fb1b7bfca575e390a66e86e7c009e7e56b
hmlssghm <kwonse326@gmail.com>
2024-10-28 18:25:21 +0900

    test(InputParserTest): 파싱 테스트 생성

--- src/test/java/racingcar/model/InputParserTest.java
+++ src/test/java/racingcar/model/InputParserTest.java
@@ -1,12 +1,34 @@
-package java.racingcar.model;
+package racingcar.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputParserTest {
 
+    private final InputParser inputParser = new InputParser();
+
     @Test
-    void parseName() {
+    void 단일_이름_파싱() {
+        String initialInput = "pobi";
+
+        List<String> parsed = inputParser.parseName(initialInput);
+
+        assertEquals(1, parsed.size());
+        assertEquals("pobi", parsed.get(0));
+    }
+
+    @Test
+    void 복수_이름_파싱() {
+        String initialInput = "pobi,woni";
+
+        List<String> parsed = inputParser.parseName(initialInput);
+
+        assertEquals(2, parsed.size());
+        assertEquals("pobi", parsed.get(0));
+        assertEquals("woni", parsed.get(1));
     }
 }
