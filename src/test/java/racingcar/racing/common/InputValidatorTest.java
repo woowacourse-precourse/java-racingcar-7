0fe2d4ca7cdbe811a636bdfebcce29098841e0e1
ppparkta <pppppark2@gmail.com>
2024-10-28 19:11:54 +0900

    test(InputValidatorTest): 레이싱카 빈배열 테스트 추가

--- src/test/java/racingcar/racing/common/InputValidatorTest.java
+++ src/test/java/racingcar/racing/common/InputValidatorTest.java
@@ -1,10 +1,10 @@
 package racingcar.racing.common;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.Assertions;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -12,25 +12,26 @@ import org.junit.jupiter.api.Test;
 class InputValidatorTest {
 
     @Test
-    void 빈_문자열이_아니면_통과() {
+    void 비지_않은_문자열_검사_성공() {
         Assertions.assertSimpleTest(() -> {
             //given
-            String one = ".";
+            String full = "abcd";
 
             //when
 
             //then
             assertThatCode(() -> {
-                InputValidator.validateInputNotEmpty(one);
+                InputValidator.validateInputNotEmpty(full);
             }).doesNotThrowAnyException();
         });
     }
 
     @Test
-    void 빈_문자열_예외처리() {
+    void 비지_않은_문자열_검사_실패() {
         Assertions.assertSimpleTest(() -> {
             //given
             String empty = "";
+            String nullString = null;
 
             //when
 
@@ -38,20 +39,44 @@ class InputValidatorTest {
             assertThatThrownBy(() -> {
                 InputValidator.validateInputNotEmpty(empty);
             }).isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> {
+                InputValidator.validateInputNotEmpty(nullString);
+            }).isInstanceOf(IllegalArgumentException.class);
         });
     }
 
     @Test
-    void 레이싱카_배열이_비었는지_유효성검사() {
+    void 레이싱카_배열이_비었는지_유효성검사_성공() {
         Assertions.assertSimpleTest(() -> {
             //given
+            List<String> carNames = Arrays.asList("car1", "car2", "car3");
+
             //when
+
             //then
+            assertThatCode(() -> {
+                InputValidator.validateCarArrayNotEmpty(carNames);
+            }).doesNotThrowAnyException();
         });
     }
 
     @Test
-    void 레이싱카_이름_5자이하_유효성검사_통과() {
+    void 레이싱카_배열이_비었는지_유효성검사_실패() {
+        Assertions.assertSimpleTest(() -> {
+            //given
+            List<String> carNames1 = new ArrayList<>();
+
+            //when
+
+            //then
+            assertThatThrownBy(() -> {
+                InputValidator.validateCarArrayNotEmpty(carNames1);
+            }).isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 레이싱카_이름_5자이하_유효성검사_성공() {
         Assertions.assertSimpleTest(() -> {
             //given
             List<String> carNames = Arrays.asList("Hello", "hiCar", "Hihic");
@@ -81,19 +106,36 @@ class InputValidatorTest {
     }
 
     @Test
-    void 시도횟수_유효성검사() {
+    void 시도횟수_유효성검사_성공() {
+        Assertions.assertSimpleTest(() -> {
+            //given
+            int one = 1;
+
+            //when
+
+            //then
+            assertThatCode(() -> {
+                InputValidator.validateAttemptNumber(one);
+            }).doesNotThrowAnyException();
+        });
+    }
+
+    @Test
+    void 시도횟수_유효성검사_실패() {
         Assertions.assertSimpleTest(() -> {
             //given
             int zero = 0;
             int minus = -1;
-            int one = 1;
 
             //when
-            InputValidator.validateAttemptNumber(zero);
+
             //then
-//            assertThat();
-//            assertThat(InputValidator.validateAttemptNumber(minus));
-//            assertThat(InputValidator.validateAttemptNumber(one));
+            assertThatThrownBy(() -> {
+                InputValidator.validateAttemptNumber(zero);
+            }).isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> {
+                InputValidator.validateAttemptNumber(minus);
+            }).isInstanceOf(IllegalArgumentException.class);
         });
     }
 }
