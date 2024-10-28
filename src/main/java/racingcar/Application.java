e8964d6163aa2da3ce7311a417398a08527f8cf2
dack3 <joyunjeong75@gmail.com>
2024-10-28 22:06:07 +0900

    feat: 랜덤숫자와 전진 바 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,20 +7,32 @@ public class Application {
     public static void main(String[] args) {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] nameList = splitName(input());
+        String[] nameList = splitName(getName());
         System.out.println("시도할 횟수는 몇 회인가요?");
-        int num = Integer.parseInt(input());
-
+        int num = Integer.parseInt(getNum());
         result(nameList, num);
 
     }
 
-    public static String input() {
+    public static String getName() {
+        return Console.readLine();
+    }
+    public static String getNum() {
         return Console.readLine();
     }
 
     public static int randomNum(){
-        return Randoms.pickNumberInRange(0,9);
+        int a = Randoms.pickNumberInRange(0,9);
+        System.out.println("랜덤숫자" + a); // 디버깅용
+        return a;
+    }
+
+    public static String numBar(int randomNum){
+        String bar;
+        if(randomNum>3){
+            return "-";
+        }
+        return "";
     }
 
     public static String[] splitName(String name){
@@ -30,11 +42,18 @@ public class Application {
     public static void result(String[] nameList, int num){
         System.out.println("실행 결과");
 
+        String[] barList = new String[nameList.length];
+        for(int j = 0; j< barList.length; j ++){
+            barList[j] = "";
+        }
+
         int i = 0;
         while(i<num){
-            for(String name : nameList){
-                System.out.println(name + " : ");
+            for(int j =0; j< nameList.length; j++){
+                barList[j] +=numBar(randomNum());
+                System.out.println(nameList[j] +" : " + barList[j]);
             }
+            System.out.println();
             i++;
         }
 
