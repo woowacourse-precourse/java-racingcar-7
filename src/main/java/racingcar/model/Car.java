fdeada0fa96606a56afd4dd78e9e291efc924d87
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-28 13:06:49 +0900

    feat: 자동차 이름 및 시도 횟수에 대한 유효성 검증 로직 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,11 +1,15 @@
 package racingcar.model;
 
+import racingcar.validator.CarValidator;
+
 public class Car {
     private final String name;
 
     private int position;
 
     public Car(String name) {
+        CarValidator.validateName(name);
+
         this.name = name;
         this.position = 0;
     }
---
+++ src/main/java/racingcar/validator/CarValidator.java
@@ -0,0 +1,19 @@
+package racingcar.validator;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CarValidator {
+    public static void validateName(String name) {
+        if (name == null || name.isBlank() || name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하로 입력해야 합니다.");
+        }
+    }
+    public static void validateUniqueNames(List<String> names) {
+        Set<String> uniqueNames = new HashSet<>(names);
+        if (uniqueNames.size() < names.size()) {
+            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+        }
+    }
+}
