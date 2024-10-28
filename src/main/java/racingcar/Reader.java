de43fc26220cf7fc9f77d7c234c3fc1802e6ec1d
eunho <eunho1032@gmail.com>
2024-10-28 15:15:06 +0900

    refactor(Reader): remove splitCarNames

--- src/main/java/racingcar/Reader.java
+++ src/main/java/racingcar/Reader.java
@@ -2,26 +2,22 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class Input {
+public class Reader {
 
     private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
-    public static String[] inputCarNames() {
-
+    public static String[] createCarNames() {
         System.out.println(INPUT_NAME_MESSAGE);
-        String[] carNames = splitCarNames(Console.readLine());
-        Validation.validateCarNames(carNames);
+        String input = Console.readLine();
+        String[] carNames = input.split(",");
+        Validation.checkCarNames(carNames);
         return carNames;
     }
 
-    private static String[] splitCarNames(String input) {
-        return input.split(",");
-    }
-
-    public static int inputTryCounts() {
+    public static int createTryCounts() {
         System.out.println(INPUT_TRY_MESSAGE);
         String input = Console.readLine();
-        return Validation.validateTryCounts(input);
+        return Validation.checkTryCounts(input);
     }
 }
