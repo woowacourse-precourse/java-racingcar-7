c7f28baac9a4a6fd201c473a7b6d1083b376964d
moeun9309 <moeun9309@gmail.com>
2024-10-28 13:34:01 +0900

    feat: find the winner

--- src/main/java/racingcar/CarManager.java
+++ src/main/java/racingcar/CarManager.java
@@ -40,4 +40,23 @@ public class CarManager {
     private boolean numCheck(int num) {
         return num >= 4;
     }
+
+    public void checkResult() {
+        List<String> winners = new ArrayList<>();
+        int maxNum = 0;
+
+        for (Car car : carList) {
+            int dist = car.getCarPosition();
+            if (dist > maxNum) {
+                winners.clear();
+                winners.add(car.getCarName());
+                maxNum = dist;
+            }
+            else if (dist == maxNum) {
+                winners.add(car.getCarName());
+            }
+        }
+
+        IOManager.printResult(winners);
+    }
 }
