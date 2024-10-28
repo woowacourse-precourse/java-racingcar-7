fa68492197626df36ed7b13e3a564679bf1200c5
DongHoon Lee <dhl1924@naver.com>
2024-10-28 19:09:15 +0900

    feat(controller): RacingCarGameController 클래스 구현 및 레이스 시작과 종료 기능 추가
    - 게임 설정을 초기화하는 initializeGameSettings 메서드 추가.
    - 레이스 시작 및 진행 상황을 출력하는 startRace 메서드 구현.
    - 레이스 종료 후 우승자를 출력하는 endRace 메서드 구현.

--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -1,8 +1,15 @@
 package racingcar.controller;
 
 import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.Count;
+import racingcar.domain.NumberGenerator;
+import racingcar.domain.Race;
+import racingcar.domain.RandomNumberGenerator;
 import racingcar.dto.CarNamesInput;
-import racingcar.utils.CarNameSplitter;
+import racingcar.dto.GameSettingsInput;
+import racingcar.dto.TryCountInput;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -16,9 +23,34 @@ public class RacingCarGameController {
     }
 
     public void startGame() {
+        GameSettingsInput gameSettingsInput = initializeGameSettings();
+        Cars cars = new Cars(gameSettingsInput.carNames());
+        Count count = Count.newInstance(gameSettingsInput.tryCount());
+        RandomNumberGenerator numberGenerator = new RandomNumberGenerator();
+        Race race = new Race(cars, count);
+
+        startRace(race, numberGenerator);
+        endRace(race);
+    }
+
+    private GameSettingsInput initializeGameSettings() {
         outputView.printCarNamesRequest();
         CarNamesInput carNamesInput = inputView.readCarNames();
-        List<String> carNames = CarNameSplitter.separateCarNames(carNamesInput.carNames());
 
+        outputView.printTryCountRequest();
+        TryCountInput tryCountInput = inputView.readTryCount();
+
+        return GameSettingsInput.of(carNamesInput, tryCountInput);
+    }
+
+    private void startRace(Race race, NumberGenerator numberGenerator) {
+        while (!race.isRaceComplete()) {
+            List<Car> raceCars = race.moveAllCarsAndGetStatus(numberGenerator);
+            outputView.printRoundResult(raceCars);
+        }
+    }
+
+    private void endRace(Race race) {
+        outputView.printWinners(race.getWinners());
     }
 }
