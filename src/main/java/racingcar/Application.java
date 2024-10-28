9947ef063c5bdf2fa452ec52dfd5fdbca938fef3
jieun <ju3391@naver.com>
2024-10-28 22:55:33 +0900

    refactor(Application): 예외 처리 수정
    - try-catch 문 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -12,22 +12,19 @@ import racingcar.view.Output;
 public class Application {
     public static void main(String[] args) {
 
-        try {
-            CarNameValidator carNameValidator = new CarNameValidator();
-            RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-            CarNameService carNameService = new CarNameService();
-            AttemptCountValidator attemptCountValidator = new AttemptCountValidator();
+        CarNameValidator carNameValidator = new CarNameValidator();
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        CarNameService carNameService = new CarNameService();
+        AttemptCountValidator attemptCountValidator = new AttemptCountValidator();
 
-            Input input = new Input(carNameValidator, attemptCountValidator);
-            Output output = new Output();
+        Input input = new Input(carNameValidator, attemptCountValidator);
+        Output output = new Output();
 
-            RaceService raceService = new RaceService(carNameService, randomNumberGenerator, output);
-            RaceController raceController = new RaceController(raceService, input, output);
+        RaceService raceService = new RaceService(carNameService, randomNumberGenerator, output);
+        RaceController raceController = new RaceController(raceService, input, output);
 
-            // 경주 시작
-            raceController.startRace();
-        } catch (Exception e) {
-            System.out.println("예외 발생: " + e.getMessage());
-        }
+        // 경주 시작
+        raceController.startRace();
+
     }
 }
