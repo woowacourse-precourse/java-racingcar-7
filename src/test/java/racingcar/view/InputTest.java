c849aeb46cfd9abb60dbbd401d7a809b341ffdab
mikekks <thdalsrb79@naver.com>
2024-10-28 19:58:42 +0900

    test(Input): 자동차이름 입력 부분에서 빈문자열 관련 테스트 코드 작성

--- src/test/java/racingcar/view/InputTest.java
+++ src/test/java/racingcar/view/InputTest.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -39,6 +40,49 @@ class InputTest {
 			.hasMessageContaining("숫자를 입력해주세요.");
 	}
 
+	@Test
+	@DisplayName("자동차 이름을 ','로 구분하여 입력하면 문자열 목록이 반환된다.")
+	void normal_readCarNames_strings() {
+		// given
+		System.setIn(createUserInput("car1,car2,car3"));
+
+		// when, then
+		Assertions.assertThat(Input.readCarNames()).isEqualTo(List.of("car1", "car2", "car3"));
+	}
+
+	@Test
+	@DisplayName("자동차 이름 간의 공백문자는 무시하고 문자열 목록이 반환된다.")
+	void namesBetweenBlank_readCarNames_strings() {
+		// given
+		System.setIn(createUserInput("car1, car2, car3"));
+
+		// when, then
+		Assertions.assertThat(Input.readCarNames()).isEqualTo(List.of("car1", "car2", "car3"));
+	}
+
+	@Test
+	@DisplayName("구분자를 기준으로 빈문자열일 경우 예외가 발생한다.")
+	void blankCarName_readCarNames_exception() {
+		// given
+		System.setIn(createUserInput("car1, ,car3"));
+
+		// when, then
+		Assertions.assertThatThrownBy(Input::readCarNames)
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("자동차 이름을 빈 값 없이 입력해주세요.");
+	}
+
+	@Test
+	@DisplayName("빈문자열을 입력할 경우 예외가 발생한다.")
+	void blank_readCarNames_exception() {
+		// given
+		System.setIn(createUserInput(" "));
+
+		// when, then
+		Assertions.assertThatThrownBy(Input::readCarNames)
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("자동차 이름을 하나 이상 입력해주세요.");
+	}
 
 	private InputStream createUserInput(String input) {
 		return new ByteArrayInputStream(input.getBytes());
