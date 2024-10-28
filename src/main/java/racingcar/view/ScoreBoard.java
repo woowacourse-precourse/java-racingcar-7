b8baf1fef13eadaf9e36ba119c474ce71f2d167b
sytTest <syt1128@naver.com>
2024-10-28 12:16:18 +0900

    :sparkles: (ScoreBoard) add carStatus()
    자동차의 이름과 시각화 된 이동거리를 입력받아 정해진 폼에 맞춰 반환하는 carStatus() 추가

--- src/main/java/racingcar/view/ScoreBoard.java
+++ src/main/java/racingcar/view/ScoreBoard.java
@@ -8,4 +8,8 @@ public class ScoreBoard {
         System.out.print(Output.SEPARATOR_BLANK_LINES);
         return Output.RESULT_HEADLINE.toString();
     }
+
+    public static String carStatus (String carName, String mileages) {
+        return String.format(Output.CAR_POSITION_FORMAT.toString(), carName, mileages);
+    }
 }
