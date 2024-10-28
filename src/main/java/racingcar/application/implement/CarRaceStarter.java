19ea109c84fddb029fb56b4952a957e9a70782f5
koseonje <kosunje1344@naver.com>
2024-10-28 21:57:24 +0900

    refactor : 사용하지 않은 import 제거

--- src/main/java/racingcar/application/implement/CarRaceStarter.java
+++ src/main/java/racingcar/application/implement/CarRaceStarter.java
@@ -1,10 +1,5 @@
 package racingcar.application.implement;
 
-import static racingcar.common.constant.RacingCarConstant.MAX_RANDOM_BOUND;
-import static racingcar.common.constant.RacingCarConstant.MIN_RANDOM_BOUND;
-import static racingcar.common.constant.RacingCarConstant.MOVE_THRESHOLD;
-
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import racingcar.domain.CarRacer;
 
