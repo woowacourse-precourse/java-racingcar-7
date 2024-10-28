e86498ac401cdf5c8475836d78075744ed03f54c
junhyunpark <junhyunpark305@gmail.com>
2024-10-28 22:52:16 +0900

    test: create test
    - 자동자 이름 개수 테스트 추가
    - 유효하지 않은 구분자 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,22 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 자동차이름개수테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("car1", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 유효하지않은구분자테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",car1", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
