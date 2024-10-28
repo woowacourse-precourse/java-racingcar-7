60f7ae320c81f34a1fea26c00f8908c1eb6a1157
leestana01 <leestana01@naver.com>
2024-10-28 17:48:56 +0900

    feat(InputController): 사용자 입력 처리 구현
    사용자 입력 처리
    - 자동차 목록을 받아 List로 변환
    - 시행 횟수를 받아 Integer로 변환
    
    예외 처리
    - 이름 공백
    - 이름 5글자 초과
    - 이름 중복

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,17 +1,84 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.io.ByteArrayInputStream;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
+    private InputController inputController = new InputController();
+
+    // 입력 테스트 ----------------------------------------------
+    // nsTest의 command 메서드만 따로 가져옴
+    private void command(String... args) {
+        byte[] buf = String.join("\n", args).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+    }
+
+    @Test
+    void 입력_테스트_자동차목록() {
+        assertSimpleTest(() -> {
+            command("pobi,woni");
+            assertThat(inputController.readCarNames()).containsExactly("pobi", "woni");
+        });
+    }
+
+    @Test
+    void 입력_테스트_자동차목록_예외_빈칸() {
+        assertSimpleTest(() -> {
+            command("pobi,,woni");
+            assertThatThrownBy(() -> inputController.readCarNames())
+                .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 입력_테스트_자동차목록_예외_글자초과() {
+        assertSimpleTest(() -> {
+            command("pobi,woni,123456");
+            assertThatThrownBy(() -> inputController.readCarNames())
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 입력_테스트_자동차목록_예외_중복() {
+        assertSimpleTest(() -> {
+            command("pobi,woni,pobi");
+            assertThatThrownBy(() -> inputController.readCarNames())
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 입력_테스트_시도횟수() {
+        assertSimpleTest(() -> {
+            command("5");
+            assertThat(inputController.readTryCount()).isEqualTo(5);
+        });
+    }
+
+    @Test
+    void 입력_테스트_시도횟수_예외_문자() {
+        assertSimpleTest(() -> {
+            command("a");
+            assertThatThrownBy(() -> inputController.readTryCount())
+                .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    // 랜덤 테스트 ----------------------------------------------
+
+    // 출력 결과 테스트 ----------------------------------------------
+
+    // 통합 테스트 ----------------------------------------------
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
