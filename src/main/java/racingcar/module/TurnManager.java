3f38d6addf828375d909258042b34b459d2ea179
MinJae00 <exceedmin@gmail.com>
2024-10-28 19:44:57 +0900

    feat: 컨트롤러의 역할을 덜기 위해 턴수 조절 및 유저의 게임 로직 실행을 담당하는 TurnManager 클래스 생성

--- src/main/java/racingcar/module/TurnManager.java
+++ src/main/java/racingcar/module/TurnManager.java
@@ -1,2 +1,36 @@
-package racingcar.module;public class TurnManager {
+package racingcar.module;
+
+import racingcar.controller.GameController;
+import racingcar.user.UserObject;
+import racingcar.view.InputView;
+
+public class TurnManager {
+    private UserObject[] user;
+    private int TURN;
+    private GameEngine gameEngine = new GameEngine();
+
+    public void progressTurn() {
+        for (int i = 0; i < TURN; i++) {
+            executeEachTurn();
+        }
+        endTurn();
+    }
+
+    private void executeEachTurn() {
+        for (UserObject users : user) {
+            gameEngine.extractRandom();
+            gameEngine.scoreUpdate(users);
+        }
+        GameController.displayProgress(user);
+    }
+
+    public void endTurn() {
+        GameController.displayWinner(new WinnerSelector(user).getTopScores());
+    }
+
+    public TurnManager(UserObject[] user) {
+        this.user = user;
+        this.TURN = InputView.tryNumber;
+    }
+
 }
