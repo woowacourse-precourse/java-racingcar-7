6aac7fc2ee2ed7e3f5cc778a5e6491ecbcde0a97
Goohong Chung <9rrong@uoslife.team>
2024-10-28 23:50:57 +0900

    feat(Car): VO `Name`, `Position` 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,41 +1,26 @@
 package racingcar.model;
 
-import org.junit.platform.commons.util.StringUtils;
+import racingcar.model.vo.Name;
+import racingcar.model.vo.Position;
 
 public class Car {
 
-    private static final int INCREMENT_VALUE = 1;
-    private static final int DEFAULT_POSITION = 0;
-    private static final int NAME_MAX_LENGTH = 5;
-    private int position = DEFAULT_POSITION;
-    private final String name;
+    private final Position position = new Position();
+    private final Name name;
 
     public Car(String name) {
-        this.name = name;
-        validateNameValue(name);
-        validateNameLength(name);
+        this.name = new Name(name);
     }
 
     public String getName() {
-        return name;
+        return name.value();
     }
 
     public int getPosition() {
-        return position;
+        return position.getValue();
     }
 
     public void increasePosition() {
-        position += INCREMENT_VALUE;
-    }
-
-    private void validateNameLength(String name) {
-        if (name.length() > NAME_MAX_LENGTH) {
-            throw new IllegalArgumentException("이름의 길이는 5자를 넘을 수 없습니다.");
-        }
-    }
-    private void validateNameValue(String name) {
-        if (StringUtils.isBlank(name)) {
-            throw new IllegalArgumentException("이름은 비어있을 수 없습니다.");
-        }
+        position.increment();
     }
 }
