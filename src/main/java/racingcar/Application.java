1acb574f02a37b1e29080562093161d3795a4f58
woohyuk <7dngur7@naver.com>
2024-10-28 17:16:42 +0900

    feat(application): 레이싱 게임의 메인 진입점 및 입력 추가
    - 자동차 이름과 시도 횟수를 사용자로부터 입력받아 게임을 시작하는 로직 구현
    - 사용자 입력에 대한 기본 안내 메시지 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,36 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carNames = Console.readLine().split(",");
+        validateCarNames(carNames);
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int attempts = Integer.parseInt(Console.readLine());
+        validateAttempts(attempts);
+
+        RacingGame game = new RacingGame(carNames, attempts);
+        game.play();
+    }
+
+    // 자동차 이름 유효성 검사 메서드
+    private static void validateCarNames(String[] carNames) {
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                System.out.println("chk");
+                throw new IllegalArgumentException("자동차 이름은 최대 5자 이하만 가능합니다.");
+            }
+        }
+    }
+
+    // 시도 횟수 유효성 검사 메서드
+    private static void validateAttempts(int attempts) {
+        if (attempts <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 1회 이상이어야 합니다.");
+        }
     }
 }
