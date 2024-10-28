06461e0e86c00197ec2d7608bc24379fbcfdf41a
Hyoseop Song <crad_on25@naver.com>
2024-10-28 23:52:54 +0900

    refactor: 경주 자동차명 파싱 메소드명 수정

--- src/main/java/racingcar/CarNameParser.java
+++ src/main/java/racingcar/CarNameParser.java
@@ -6,7 +6,7 @@ public class CarNameParser {
 	private static final String DELIMITER = ",";
 	private CarValidator carValidator;
 
-	public List<String> getCarNames(String input) {
+	public List<String> parse(String input) {
 		List<String> carNames = splitCarNames(input);
 		return carValidator.validateCarNameList(carNames);
 	}
