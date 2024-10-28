44893d88908e13651a36189db22785763851cc8c
kim-se-hee <saree98@naver.com>
2024-10-28 16:50:30 +0900

    feat(Validator): 입력 검증 클래스 구현
    Validator에 검증 로직들을 추가
    - 플레이어 이름 입력 검증
    - 스테이지 크기 입력 검증

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,2 +1,56 @@
-package racingcar;public class Validator {
+package racingcar;
+
+public class Validator {
+
+    public boolean checkIfNameInputIsLegal(String[] names) {
+
+        if(hasNoName(names)){
+            throw new IllegalArgumentException();
+        }
+
+        for (String name : names) {
+
+            if (isIllegalName(name)) {
+                throw new IllegalArgumentException();
+            }
+
+        }
+
+        return true;
+    }
+
+    public boolean checkStageSizeIsLegal(String input) {
+        if (isPositiveNumber(input)) {
+            return true;
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    private static boolean hasNoName(String[] names) {
+        return names.length == 0;
+    }
+
+    private static boolean isNameTooLong(String name) {
+        return name.length() > 5;
+    }
+
+    private static boolean isPositiveNumber(String input) {
+
+        for(int i = 0; i < input.length(); i++){
+
+            char letter = input.charAt(i);
+            if(Character.isDigit(letter)){
+                continue;
+            }
+
+            throw new IllegalArgumentException();
+        }
+
+        return true;
+    }
+
+    private static boolean isIllegalName(String name) {
+        return isNameTooLong(name) || name.isEmpty();
+    }
 }
