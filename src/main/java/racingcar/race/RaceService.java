e0053b9fc7e3fd52a6104adfbce8768a238293d3
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-10-28 11:54:55 +0900

    feat: 우승자 확인 기능 구현
    가장 많이 전진한 자동차를 구하는 우승자 확인 기능을 구현했습니다.

--- src/main/java/racingcar/race/RaceService.java
+++ src/main/java/racingcar/race/RaceService.java
@@ -1,7 +1,9 @@
 package racingcar.race;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.car.Car;
 
 public class RaceService {
@@ -16,7 +18,14 @@ public class RaceService {
         return randomNumber >= 4;
     }
 
-    public List<String> getChampion(/*List<Car> participant*/) {
-        return null; // 경주 우승자 반환
+    public List<Car> getChampion(List<Car> participant) {
+        return participant.stream()
+                .collect(Collectors.teeing(
+                        Collectors.maxBy(Comparator.comparingInt(Car::getTotalMove)),
+                        Collectors.toList(),
+                        (maxTotalMove, cars) -> cars.stream()
+                                .filter(car -> maxTotalMove.orElseThrow().getTotalMove() == car.getTotalMove())
+                                .collect(Collectors.toList())
+                ));
     }
 }
