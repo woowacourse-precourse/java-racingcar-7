12d15fcce81f0cc35983a97ab9f2641e0d9c6494
HwangSgHn <dolphin33@jbnu.ac.kr>
2024-10-28 19:00:29 +0900

    feat: 출력 기능
    Ouput 클래스 생성.
    입력을 받을 때 사용되는 출력과 '실행 결과' 출력 구현.

---
+++ src/main/java/racingcar/Output.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+public class Output {
+    public void carInputComment() {
+        final String CAR_INPUT_COMMENT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+        print(CAR_INPUT_COMMENT);
+    }
+
+    public void tryCountComment() {
+        final String Try_COUNT_COMMENT = "시도할 횟수는 몇 회인가요?";
+        print(Try_COUNT_COMMENT);
+    }
+
+    public void executionResultComment() {
+        final String EXECUTION_RESULT_COMMENT = "실행 결과";
+        print(EXECUTION_RESULT_COMMENT);
+    }
+
+    private void print(String comment) {
+        System.out.println(comment);
+    }
+}
