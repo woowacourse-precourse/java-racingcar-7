26f420734f0d1f33dfaa373c8906a091b4b316b5
jmlee <p2achtrii@gmail.com>
2024-10-28 21:39:23 +0900

    feat: Add Delimiter & InputAmountOfRace class

--- src/main/java/racingcar/InputAmountOfRace.java
+++ src/main/java/racingcar/InputAmountOfRace.java
@@ -1,4 +1,12 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputAmountOfRace {
+    public static void main(String[] args) {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String raceString = Console.readLine();
+
+        int raceAmount = Integer.parseInt(raceString);
+    }
 }
