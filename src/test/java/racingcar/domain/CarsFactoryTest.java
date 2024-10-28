0130f8952e0411a36c358ef7381828393b6eb598
dnwldla <leeemjw@naver.com>
2024-10-28 17:40:47 +0900

    test: 입력의 시작 또는 끝이 쉼표인 경우 테스트 추가

--- src/test/java/racingcar/domain/CarsFactoryTest.java
+++ src/test/java/racingcar/domain/CarsFactoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -30,6 +31,22 @@ class CarsFactoryTest {
     }
 
     @ParameterizedTest
+    @ValueSource(strings = {"지우, 포비","지우, "," 지우,포비"})
+    void 이름에_공백이_존재하면_예외_발생(String carsInput){
+        assertThatThrownBy(()->CarsFactory.makeCarList(carsInput)).
+                isInstanceOf(IllegalArgumentException.class).
+                hasMessageContaining("차 이름에 공백이 존재할 수 없습니다");
+
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {",","지우,",",,"})
+    void 입력_시작과_끝에_쉼표가_존재하면_예외_발생(String carsInput){
+        assertThatThrownBy(()->CarsFactory.makeCarList(carsInput)).
+                isInstanceOf(IllegalArgumentException.class).
+                hasMessageContaining("입력의 시작과 끝은 쉼표가 될 수 없습니다");
+    }
+
+    @ParameterizedTest
     @MethodSource("generateCars")
     void 올바르게_입력하면_객체를_반환한다(String input,List<Car> carList){
      assertThat(CarsFactory.makeCarList(input)).usingRecursiveComparison().isEqualTo(carList);
@@ -40,9 +57,7 @@ class CarsFactoryTest {
                 Arguments.arguments("지우,포비,제이",List.of(new Car("지우",""),
                         new Car("포비",""),
                         new Car("제이","")
-                )),
-                Arguments.arguments("지우,포비       ",List.of(new Car("지우",""),
-                        new Car("포비","")))
+                ))
         );
     }
 }
