4a86c4035523b8c65ce57c29bc51928a3cac6ef5
Hyeo_Ni__ <148944993+Mae-Hyong@users.noreply.github.com>
2024-10-28 23:58:58 +0900

    feat(object): Car 객체 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -133,3 +133,24 @@ public class Application {
         return winners;
     }
 }
+
+class Car {
+    private final String carName;
+    private int position = 0;
+
+    public Car(String carName) {
+        this.carName = carName;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void advance() {
+        position++;
+    }
+}
