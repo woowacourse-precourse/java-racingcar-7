d29189d851ac21fd00f2057dbc1146e3eb435a94
making-a-scene <4936joo@naver.com>
2024-10-28 12:35:44 +0900

    refactor(InputView, OuputView): 상수 선언한 모든 변수 값에서 'STATEMENT' 삭제
    변수의 타입이 String인 것으로 충분히 statement라는 것이 전달되기에 불필요하다고 판단

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,17 +5,17 @@ import racingcar.Validation;
 
 public class InputView {
     private final Validation validation = new Validation();
-    private final String GET_CAR_NAME_STATEMENT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final String GET_ROUND_LIMIT_STATEMENT = "시도할 횟수는 몇 회인가요?";
+    private final String GET_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String GET_ROUND_LIMIT = "시도할 횟수는 몇 회인가요?";
 
     public String[] getCarNames() {
-        System.out.println(GET_CAR_NAME_STATEMENT);
+        System.out.println(GET_CAR_NAME);
         String input = Console.readLine();
         return validation.validateCarName(input);
     }
 
     public int getRoundLimit() {
-        System.out.println(GET_ROUND_LIMIT_STATEMENT);
+        System.out.println(GET_ROUND_LIMIT);
         String input = Console.readLine();
         return validation.validateRoundLimit(input);
     }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,12 +6,12 @@ import racingcar.domain.Car;
 import java.util.List;
 
 public class OutputView {
-    private final String PRINT_RESULT_STATEMENT = "\n실행 결과";
-    private final String PRINT_WINNERS_STATEMENT = "최종 우승자 : ";
+    private final String PRINT_RESULT = "\n실행 결과";
+    private final String PRINT_WINNERS = "최종 우승자 : ";
 
     public void printRoundResult(boolean isFirstRound, List<Car> participants) {
         if (isFirstRound) {
-            System.out.println(PRINT_RESULT_STATEMENT);
+            System.out.println(PRINT_RESULT);
         }
         for (Car car : participants) {
             System.out.println(car.getName() + " : " + "-".repeat(car.getMovedCnt()));
@@ -20,6 +20,6 @@ public class OutputView {
     }
 
     public void printWinners(List<String> winners) {
-        System.out.print(PRINT_WINNERS_STATEMENT + String.join(", ", winners));
+        System.out.print(PRINT_WINNERS + String.join(", ", winners));
     }
 }
