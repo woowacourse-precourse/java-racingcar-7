2c85f5a32d42d3240517c9bb63ba9e7ea5487feb
sodychoe <sodychoe@icloud.com>
2024-10-28 19:11:07 +0900

    feat : Application 실행 성공 여부에 관계없이 Console이 닫히도록 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,22 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.io.InputConsoleHandler;
+import racingcar.io.OutputConsoleHandler;
+import racingcar.provider.NumberProvider;
+import racingcar.provider.RandomNumberProvider;
+
 public class Application {
     public static void main(String[] args) {
-        RacingGame racingGame = new RacingGame();
+        NumberProvider numberProvider = new RandomNumberProvider();
+        InputConsoleHandler inputConsoleHandler = new InputConsoleHandler();
+        OutputConsoleHandler outputConsoleHandler = new OutputConsoleHandler();
 
-        racingGame.runGame();
+        try {
+            RacingGame racingGame = new RacingGame(numberProvider, inputConsoleHandler, outputConsoleHandler);
+            racingGame.runGame();
+        } finally {
+            Console.close();
+        }
     }
 }
