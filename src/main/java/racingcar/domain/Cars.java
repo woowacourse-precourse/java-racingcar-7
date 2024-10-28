cf5575ccee29c76486a91eb59d12643ae71b5576
gladhee <gladhee@icloud.com>
2024-10-28 16:21:41 +0900

    refactor(SplitterUtil): String carNames 를 split 하는 메소드를 static 으로 util 에 추출

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -5,10 +5,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.domain.strategy.MoveStrategy;
+import racingcar.util.SplitterUtil;
 
 public class Cars {
 
-    private static final String SPLIT_DELIMITER = ",";
     private static final String DUPLICATE_CAR_NAME = "중복된 이름이 존재합니다.";
 
 
@@ -33,7 +33,7 @@ public class Cars {
     }
 
     private List<Car> createCars(String carNames) {
-        String[] splitCarNames = splitCarNames(carNames);
+        String[] splitCarNames = SplitterUtil.splitCarNames(carNames);
 
         return Arrays.stream(splitCarNames)
                 .map(Car::new)
@@ -41,17 +41,13 @@ public class Cars {
     }
 
     private void validateDuplicateCarNames(String carNames) {
-        String[] splitCarNames = splitCarNames(carNames);
+        String[] splitCarNames = SplitterUtil.splitCarNames(carNames);
         List<String> carNameList = Arrays.asList(splitCarNames);
         int uniqueCarNames = new HashSet<>(carNameList).size();
 
         if (uniqueCarNames != cars.size()) {
             throw new IllegalArgumentException(DUPLICATE_CAR_NAME);
         }
-    }
-
-    private String[] splitCarNames(final String carNames) {
-        return carNames.split(SPLIT_DELIMITER);
     }
 
     public List<Car> getCars() {
---
+++ src/main/java/racingcar/util/SplitterUtil.java
@@ -0,0 +1,9 @@
+package racingcar.util;
+
+public class SplitterUtil {
+
+    public static String[] splitCarNames(final String carNames) {
+        return carNames.split(",");
+    }
+
+}
