90d68269d3bd9d67ed352b167dcee6a6d5bb3383
Seongmo Ahn <djgood780@gmail.com>
2024-10-28 14:25:05 +0900

    refactor(MovementGenerator): 나올 수 있는 최소, 최대 랜덤 값을 상수로 선언

--- src/main/java/racingcar/MovementGenerator.java
+++ src/main/java/racingcar/MovementGenerator.java
@@ -3,7 +3,10 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class MovementGenerator {
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+
     public static int getRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 }
