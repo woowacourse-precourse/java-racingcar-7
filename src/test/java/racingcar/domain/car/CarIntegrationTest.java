f1ed33290bed6090f3fa7cc54c9e1789d9059465
inti0 <142914095+inti0@users.noreply.github.com>
2024-10-28 20:42:24 +0900

    refactor : 종합테스트 테스트코드 리팩토링
    ParamiterizeTest로 변경

--- src/test/java/racingcar/domain/car/CarIntegrationTest.java
+++ src/test/java/racingcar/domain/car/CarIntegrationTest.java
@@ -2,24 +2,57 @@ package racingcar.domain.car;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.IntSupplier;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class CarIntegrationTest {
 
-    @Test
-    void 종합_테스트() {
-        List<String> carNames = List.of("stopCarA", "stopCarB", "stopCarC", "stopCarD");
+    static Stream<Arguments> provideStopEngine() {
+        return Stream.of(
+                arguments(List.of("stopCarA", "stopCarB"), 0),
+                arguments(List.of("stopCarA", "stopCarB"), 1),
+                arguments(List.of("stopCarA", "stopCarB"), 2),
+                arguments(List.of("stopCarA", "stopCarB"), 3)
+        );
+    }
 
-        Cars cars = CarGenerator.generateCars(carNames, () -> 0);
+    @ParameterizedTest
+    @MethodSource("provideStopEngine")
+    void 종합_테스트_멈춤(List<String> carNames, final int engineValue) {
+        Cars cars = CarGenerator.generateCars(carNames, () -> engineValue);
         List<Car> carList = cars.getCars();
 
         IntStream.range(0, 50).forEach(i -> cars.move());
 
         IntStream.range(0, carList.size())
                 .forEach(i -> Assertions.assertThat(carList.get(i).getPosition()).isEqualTo(0));
+    }
+
+    static Stream<Arguments> provideMoveEngine() {
+        return Stream.of(
+                arguments(List.of("moveCarA", "moveCarB"), 4),
+                arguments(List.of("moveCarA", "moveCarB"), 9)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideMoveEngine")
+    void 종합_테스트_이동(List<String> carNames, final int engineValue) {
+        Cars cars = CarGenerator.generateCars(carNames, () -> engineValue);
+        List<Car> carList = cars.getCars();
+
+        IntStream.range(0, 50).forEach(i -> cars.move());
+
+        IntStream.range(0, carList.size())
+                .forEach(i -> Assertions.assertThat(carList.get(i).getPosition()).isEqualTo(50));
     }
 }
