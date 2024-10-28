8abf76396c9b38083b6046273ccd5e0eaa0dcd6d
sonshn <sonshn@naver.com>
2024-10-28 18:17:10 +0900

    feat(InputView): 멤버 함수를 static에서 instance method로 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,17 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static String getCarNames() {
+    public String getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
 
-    public static String getAttemptValue() {
+    public String getAttemptValue() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         return Console.readLine();
     }
 
-    public static void closeConsole() {
+    public void closeConsole() {
         Console.close();
     }
 
