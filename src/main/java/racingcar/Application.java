5c9a749b435d4c53c1f227203021d6710a2c9ea0
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-10-28 16:16:31 +0900

    feat : 경기 횟수 입력창 출력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,6 +9,7 @@ public class Application {
 
     private static final String COMMA = ",";
     private final static String INPUT_PRINT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final static String ATTEMPT_PRINT = "시도할 횟수는 몇 회인가요?";
     private final static String INPUT_VALIDATION_EXCEPTION_MESSAGE = "자동차 이름 형식을 맞춰주세요. 자동차 이름은 필수 값입니다.";
     private final static String INPUT_VALIDATION_SIZE_EXCEPTION_MESSAGE = "자동차 이름 형식을 맞춰주세요. 자동차 이름은 5자 이하입니다.";
 
@@ -43,6 +44,10 @@ public class Application {
 
     public static void inputCarNamePrint(){
         System.out.println(INPUT_PRINT);
+    }
+
+    public static void inputAttemptPrint(){
+        System.out.println(ATTEMPT_PRINT);
     }
 
     }
