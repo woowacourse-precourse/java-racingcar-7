080308c60c35a7ce6bed85388b194800e07ca43f
inha0319@naver.com <inha031913@gmail.com>
2024-10-28 10:51:02 +0900

    [feat] Winner 클래스 구현

--- src/main/java/racingcar/model/Winner.java
+++ src/main/java/racingcar/model/Winner.java
@@ -1,4 +1,25 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Winner {
+    public static List<Car> getWinners(List<Car> cars) {
+        int maxLocation = 0;
+
+        for (Car car : cars) {
+            if (car.getLocation() > maxLocation) {
+                maxLocation = car.getLocation();
+            }
+        }
+
+        List<Car> winners = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (car.getLocation() == maxLocation) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
 }
