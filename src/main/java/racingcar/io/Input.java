272d46570cdb5bbe6f1a6627f5375f9186339fbf
yallo428 <yallo428@naver.com>
2024-10-28 13:28:27 +0900

    feat(Input) : 자동차 이름 입력 구현
    자동차 이름 입력 구현:
    - 입력값의 처음과 끝이 ,인지 검증
    - 입력값이 알파벳,숫자로 구성됐는지 검증

--- src/main/java/racingcar/io/Input.java
+++ src/main/java/racingcar/io/Input.java
@@ -1,6 +1,8 @@
 package racingcar.io;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 import racingcar.vo.ExceptionMessage;
 
@@ -28,6 +30,35 @@ public class Input {
 
         if (isNotPositive) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_NEGATIVE_NUMBER.getMessage());
+        }
+    }
+
+    public List<String> readCarNames() {
+        String[] carNames = Console.readLine().split(",");
+
+        return Arrays.stream(carNames)
+                .map(String::trim)
+                .filter(this::isReadCarNameValidate)
+                .toList();
+    }
+
+    private boolean isReadCarNameValidate(String carName) {
+        validateAllowedName(carName);
+        validateCommaPosition(carName);
+        return true;
+    }
+
+    private void validateCommaPosition(String carName) {
+        if (carName.startsWith(",") || carName.endsWith(",")) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_NOT_NAME.getMessage());
+        }
+    }
+
+    private void validateAllowedName(String carName) {
+        boolean isNotAllowedName = !Pattern.matches("[a-zA-z\\d,]+", carName);
+
+        if (isNotAllowedName) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_NOT_ALLOWED_NAME.getMessage());
         }
     }
 }
