dbe55e116370241e6d397ef550f673c4b78b8b1e
w0dnj5 <jw991211@icloud.com>
2024-10-28 15:16:22 +0900

    test(AttemptCount): AttemptCount 유효성 검사에 대한 테스트 추가
    - 올바른 형식으로 들어왔을 때 유효성 검사를 통과하는지 검증

--- src/test/java/racingcar/model/AttemptCountTest.java
+++ src/test/java/racingcar/model/AttemptCountTest.java
@@ -8,8 +8,17 @@ public class AttemptCountTest {
 
     @ParameterizedTest
     @ValueSource(ints = {-1, -2, 0, -99, -84})
-    void throwExceptionIfAttemptCountIsOutOfRange(int attemptCount) {
-        Assertions.assertThatThrownBy(() -> new AttemptCount(attemptCount))
+    void throwExceptionIfAttemptCountIsOutOfRange(int input) {
+        Assertions.assertThatThrownBy(() -> new AttemptCount(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 99, 84})
+    void shouldReturnCorrectNumber(int input) {
+        AttemptCount attemptCount = new AttemptCount(input);
+
+        Assertions.assertThat(attemptCount.getAttemptCount())
+                .isEqualTo(input);
     }
 }
