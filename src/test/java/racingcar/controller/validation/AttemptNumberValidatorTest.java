9b5c60509cfdbc10c636a07d531706f97e3b9a6a
hmlssghm <kwonse326@gmail.com>
2024-10-28 16:59:23 +0900

    test(AttemptNumberValidatorTest): 시도 회수 예외 테스트

--- src/test/java/racingcar/controller/validation/AttemptNumberValidatorTest.java
+++ src/test/java/racingcar/controller/validation/AttemptNumberValidatorTest.java
@@ -1,12 +1,45 @@
-package java.racingcar.controller.validation;
+package racingcar.controller.validation;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.view.messages.ErrorMessageEnum;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class AttemptNumberValidatorTest {
+class AttemptNumberValidatorTest extends NsTest {
+
+    private final AttemptNumberValidator validator = new AttemptNumberValidator();
 
     @Test
-    void validate() {
+    void 유효값_통과_테스트() {
+        int result = validator.validate("3");
+        assertEquals(3, result);
     }
+
+    @Test
+    void 공백_테스트() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            validator.validate("");
+        });
+        assertEquals(ErrorMessageEnum.EMPTY_ERROR.getErrorMessage(), exception.getMessage());
+    }
+
+    @Test
+    void 시도값_0_테스트() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            validator.validate("0");
+        });
+        assertEquals(ErrorMessageEnum.NOT_NATURAL_NUMBER_ERROR.getErrorMessage(), exception.getMessage());
+    }
+
+    @Test
+    void 시도값_음수_테스트() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            validator.validate("-1");
+        });
+        assertEquals(ErrorMessageEnum.NOT_NATURAL_NUMBER_ERROR.getErrorMessage(), exception.getMessage());
+    }
+
+    @Override
+    protected void runMain() {}
 }
