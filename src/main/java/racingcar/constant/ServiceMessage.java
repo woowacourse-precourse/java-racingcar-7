87e8e809062307b54f4b1dda4d68e43e39f53316
chino <tnehd83@naver.com>
2024-10-28 14:09:02 +0900

    feat(constat): 메시지 추가

--- src/main/java/racingcar/constant/ServiceMessage.java
+++ src/main/java/racingcar/constant/ServiceMessage.java
@@ -4,6 +4,8 @@ public enum ServiceMessage {
     START_MESSAGE_RACING_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분"),
     START_MESSAGE_RACING_COUNT("시도할 횟수는 몇 회인가요?"),
     PLAYING_RACING("실행 결과"),
+    PLAYING_RACING_COMMA(" : "),
+    PLAYING_RACING_MOVE_LENGTH("-"),
     WINNER_USER("최종 우승자 : ");
 
 
