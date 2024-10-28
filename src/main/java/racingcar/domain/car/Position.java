baa515acd87003483df7a5ed36b208adf7e0b465
hootzz <wjqwjql3@gmail.com>
2024-10-28 20:20:54 +0900

    feat(car): 자동차 위치 관련 기능 구현

--- src/main/java/racingcar/domain/car/Position.java
+++ src/main/java/racingcar/domain/car/Position.java
@@ -1,4 +1,27 @@
 package racingcar.domain.car;
 
+import racingcar.domain.validator.PositionValidator;
+
 public class Position {
+    private static final int INITIAL_POSITION = 0;
+    private final int value;
+    private final PositionValidator validator;
+
+    public Position() {
+        this(INITIAL_POSITION);
+    }
+
+    private Position(int value) {
+        this.validator = new PositionValidator();
+        this.validator.validate(value);
+        this.value = value;
+    }
+
+    public Position forward() {
+        return new Position(value + 1);
+    }
+
+    public boolean isSameAs(Position other) {
+        return this.value == other.value;
+    }
 }
