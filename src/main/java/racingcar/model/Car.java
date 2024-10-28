f9e9ba58a4822cfa27d09b03601cfb6922898391
inha0319@naver.com <inha031913@gmail.com>
2024-10-28 18:38:10 +0900

    [fix] 자동차 이름 길이 예외처리 수정

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -6,7 +6,7 @@ public class Car {
 
     public Car(String name){
         if (name.length() > 5) {
-            throw new IllegalStateException("자동차 이름은 5자 이하만 가능합니다.");
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         }
         this.name = name;
         this.location = 0;
