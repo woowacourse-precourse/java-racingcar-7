dd17a6e859be2a409071fb38bf20e6d3d6d18267
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-28 19:12:43 +0900

    feat: 경주 상태 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,28 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.model.RacingCars;
+
+public class OutputView {
+
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String RESULT_SEPARATOR = " : ";
+    private static final String MOVE_MARKER = "-";
+    private static final String COMMA = ",";
+    private static final String FINAL_WINNER = "최종 우승자 : ";
+    private static final String NO_WINNER = "우승자가 없습니다.";
+
+    public void printRaceStart() {
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public void printRaceStatus(RacingCars racingCars) {
+        racingCars.getCars().forEach(car -> printCarStatus(car.getName(), car.getPosition()));
+        System.out.println();
+    }
+
+    private void printCarStatus(String name, int position) {
+        String progress = MOVE_MARKER.repeat(position);
+        System.out.println(name + RESULT_SEPARATOR + progress);
+    }
+}
