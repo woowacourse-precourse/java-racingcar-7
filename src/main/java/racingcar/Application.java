da5611c4029b8ca30a52d4d59fbc2b0e09bbf68a
AwesomeMinsik <beaut3579@gmail.com>
2024-10-28 16:05:39 +0900

    refactor: 무작위 값 정책 인터페이스의 의존성 주입 로직 추가에 따른 Application 의 구현체 인스턴스 생성로직을 업데이트 하였습니다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,15 @@
 package racingcar;
 
+import racingcar.policy.RandomNumGeneratorImpl;
+
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         List<String> racerList = IOHandler.setRacerList();
-        List<RacingCar> racingCars = CarFactory.createRacingCars(racerList);
-        raceManager.startRace(racingCars,IOHandler.setRaceCycle());
+        RandomNumGeneratorImpl randomNumGenerator = new RandomNumGeneratorImpl();
+
+        RaceManager raceManager = new RaceManager(racerList,randomNumGenerator);
+        raceManager.startRace(IOHandler.setRaceCycle());
     }
 }
