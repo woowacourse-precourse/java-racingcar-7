46ad7baed2102e3974e9ab595ee26a8114bbf583
yanghyen <sk0310803@naver.com>
2024-10-28 19:08:57 +0900

    feat(car): 자동차 이름 유효성 검사 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,8 +1,16 @@
 package racingcar;
 
 public class Car {
-  public Car(String name) {
+  private final String name;
 
+  public Car(String name) {
+    validateName(name);
+    this.name = name;
   }
 
+  private void validateName(String name) {
+    if (name.length() > 5) {
+      throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+    }
+  }
 }
