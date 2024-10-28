d64b205d57880d04e1d528e62bb2625a84091aa9
kimchanho97 <nh0903@pusan.ac.kr>
2024-10-28 21:22:02 +0900

    feat(RacingCarGameInputView): 자동차 이름 입력

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,8 +1,11 @@
 package racingcar.controller;
 
+import racingcar.model.Cars;
+import racingcar.view.RacingCarGameInputView;
+
 public class RacingGameController {
 
     public void run() {
-
+        Cars cars = new Cars(RacingCarGameInputView.inputCarNames());
     }
 }
