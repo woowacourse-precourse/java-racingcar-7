bbdcc37f28c5a073c5e5b6f9c04042e050aa317a
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-10-28 21:38:19 +0900

    refactor: Controller에서 Service 호출하도록 수정

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -1,55 +1,34 @@
 package racingcar.controller;
 
-import racingcar.domain.Car;
-import racingcar.domain.Number;
 import racingcar.domain.Cars;
+import racingcar.domain.GameRounds;
+import racingcar.service.GameService;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
-
-import java.util.List;
 
 public class RacingCarGame {
     private final InputView inputView;
     private final OutputView outputView;
-    private final Number number;
-    private final Cars cars;
-    private String inputString;
-    private int tryCount;
+    private final GameService gameService;
 
-    public RacingCarGame(InputView inputView, OutputView outputView, Number number, Cars cars) { //의존성 주입 다시 공부하기
+    public RacingCarGame(InputView inputView, OutputView outputView, GameService gameService) {
         this.inputView = inputView;
         this.outputView = outputView;
-        this.number = number;
-        this.cars = cars;
-        this.inputString = "";
-        this.tryCount = 0;
+        this.gameService = gameService;
     }
 
-    public void initGame() {
+    public void run() {
         outputView.printStartMessage();
-        this.inputString = inputView.getInput();
+        String inputString = inputView.getInput();
+        Cars cars = gameService.generateCarList(inputString);
 
         outputView.printTryCountMessage();
-        this.tryCount = Integer.parseInt(inputView.getInput());
+        int tryCount = Integer.parseInt(inputView.getInput());
 
-        cars.setCarList(inputString);
+        GameRounds gameRounds = gameService.startGame(cars, tryCount);
+        outputView.printGameRounds(gameRounds);
+        outputView.printWinner(cars.getWinnerList());
     }
 
-    public void startGame() {
-        outputView.printResultMessage();
-        while (tryCount-- > 0) {
-            playOneRound();
-        }
 
-        List<String> winnerList = cars.getWinnerList();
-        outputView.printWinner(winnerList);
-    }
-
-    private void playOneRound() {
-        for (Car car : cars.getCarList()) {
-            car.setDistance(number.getRandomNumber());
-            outputView.printCarDistance(car.getName(), car.getDistance());
-        }
-        outputView.printEmptyLine();
-    }
 }
