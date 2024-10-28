25d8d60e4efd2039fcb9eb9d7ccc5935cdb96bbc
LeeDongWoo99 <dleh684@gmail.com>
2024-10-28 22:40:45 +0900

    test(application): 유효성 검사 및 기능 테스트 추가
    - 유효한 입력에 대한 정수 반환 테스트 추가
    - 음수, 0, 잘못된 문자 입력 시 예외 발생 테스트 추가
    - 게임 동작 확인을 위한 기능 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,11 +2,13 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.model.Validator;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -29,6 +31,37 @@ class ApplicationTest extends NsTest {
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 유효한_양수_입력시_정수반환() {
+        String input = "5";
+        int result = Validator.trialCount(input);
+        assertThat(result).isEqualTo(5);
+    }
+
+    @Test
+    void 음수_입력시_예외발생() {
+        String input = "-1";
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Validator.trialCount(input))
+                .withMessage("이동 횟수는 양수여야 합니다.");
+    }
+
+    @Test
+    void 영입력시_예외발생() {
+        String input = "0";
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Validator.trialCount(input))
+                .withMessage("이동 횟수는 양수여야 합니다.");
+    }
+
+    @Test
+    void 잘못된_문자입력시_예외발생() {
+        String input = "abc";
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Validator.trialCount(input))
+                .withMessage("유효하지 않은 입력입니다. 숫자를 입력해주세요.");
     }
 
     @Override
