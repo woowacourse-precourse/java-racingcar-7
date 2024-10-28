fdb12d2d98b732978711397285abd23bd2124e16
haneul0509 <haneul0509@naver.com>
2024-10-28 22:35:46 +0900

    feat: 빈 문자열 이름 입력에 대한 IllegalArgumentException 예외 처리

--- src/main/java/racingcar/IO/CarNamesInput.java
+++ src/main/java/racingcar/IO/CarNamesInput.java
@@ -9,6 +9,10 @@ public class CarNamesInput {
             if (carName.length() > 5) {
                 throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
             }
+
+            else if (carName.length() == 0){
+                throw new IllegalArgumentException("이름은 한 글자 이상 작성해 주세요.");
+            }
         }
         return carNames;
     }
