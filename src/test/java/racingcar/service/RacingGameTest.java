96f2aa72069f3c7b0c4dab3fa8d9f58578966a00
kim-do-kyun <zetgi7dk5359@gmail.com>
2024-10-28 14:30:11 +0900

    test : validator만 수행하도록 테스트코드 수정

--- src/test/java/racingcar/service/RacingGameTest.java
+++ src/test/java/racingcar/service/RacingGameTest.java
@@ -2,35 +2,16 @@ package racingcar.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.domain.Car;
-import racingcar.repository.CarRepository;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RacingGameTest {
 
     private final Validator validator = new Validator();
-    private final CarRepository carRepository = new CarRepository();
 
     @Test
     @DisplayName("입력을 콤마 기준으로 분할")
     void 입력테스트() {
         validator.checkCarName("pobi,woni,jun");
-    }
-
-    @Test
-    @DisplayName("검증 후 carDB에 저장되는지 확인")
-    void 저장테스트() {
-        carRepository.save(new Car("pobi"));
-        carRepository.save(new Car("woni"));
-        carRepository.save(new Car("jun"));
-        List<Car> carList = carRepository.findAll();
-
-        assertThat(carList).hasSize(3); // 저장된 자동차의 개수를 검증
-        assertThat(carList).extracting(Car::getName).contains("pobi", "woni", "jun"); // 이름 검증
     }
 
     @Test
