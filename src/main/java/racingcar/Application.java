a5f5b4e05e04a6f4acd87608e77bc2f5ee5d3072
spotydol7 <spotydol7@gmail.com>
2024-10-28 22:19:12 +0900

    feat: Application 실행 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
+import racingcar.view.InputView;
+import racingcar.domain.RacingGame;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            String carNamesInput = InputView.getCarNames();
+            int trialCount = InputView.getTrialCount();
+
+            RacingGame game = new RacingGame(carNamesInput, trialCount);
+            game.start();
+        } catch (IllegalArgumentException e) {
+            // 예외 메시지 출력 없이 프로그램 종료
+        }
     }
 }
