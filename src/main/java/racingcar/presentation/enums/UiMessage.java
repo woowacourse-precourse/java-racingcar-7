1e96c06c9f77ad318d035f1e27980d5a3711d7b4
jijongkwon <help3451@naver.com>
2024-10-28 17:43:17 +0900

    feat(enum) : UI Message 추가
    - UI_OUTPUT_EXECUTION_RESULT_MESSAGE
    - UI_OUTPUT_WINNER_RESULT_MESSAGE

--- src/main/java/racingcar/presentation/enums/UiMessage.java
+++ src/main/java/racingcar/presentation/enums/UiMessage.java
@@ -1,9 +1,13 @@
 package racingcar.presentation.enums;
 
 public enum UiMessage {
+    //=====================INPUT MESSAGE=====================//
     UI_INPUT_CAR_NAME_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    UI_INPUT_TRY_NUMBER_MESSAGE("시도할 횟수는 몇 회인가요?");
+    UI_INPUT_TRY_NUMBER_MESSAGE("시도할 횟수는 몇 회인가요?"),
 
+    //=====================OUTPUT MESSAGE=====================//
+    UI_OUTPUT_EXECUTION_RESULT_MESSAGE("실행 결과"),
+    UI_OUTPUT_WINNER_RESULT_MESSAGE("최종 우승자 :");
     private final String message;
 
     UiMessage(String message){
