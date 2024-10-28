837949a5d3562ef3ab125c87d52c9ba397fb666b
yu-k-m <karen0806@naver.com>
2024-10-28 16:40:03 +0900

    fix: fix duplicated InputView

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,10 +4,13 @@ import racingcar.controller.RacingGameController;
 import racingcar.service.RacingGameService;
 import racingcar.view.InputView;
 
+import java.util.zip.InflaterInputStream;
+
 public class Application {
     public static void main(String[] args) {
-        String carNames = InputView.getRacingCarsAndTryNumber()[0];
-        String tryNumber= InputView.getRacingCarsAndTryNumber()[1];
+        String[] inputs = InputView.getRacingCarsAndTryNumber();
+        String carNames = inputs[0];
+        String tryNumber= inputs[1];
         RacingGameService racingGameService = new RacingGameService();
         RacingGameController racinggameController = new RacingGameController(racingGameService);
         racinggameController.startRace(carNames,tryNumber);
