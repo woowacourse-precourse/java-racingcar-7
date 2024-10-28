5aee2fd7eb48d03f921a824f6129ce85abe85b3f
Sujin Kim <su10jin11@khu.ac.kr>
2024-10-28 18:40:56 +0900

    feat : Application.java 코드 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.view.Input;
+import racingcar.view.Output;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = new Input();
+        Output output = new Output();
+        RacingCarController controller = new RacingCarController(input, output);
+        controller.startGame();
     }
 }
