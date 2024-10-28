6c7e223c93374e3917fc42cc1b86337db332b202
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-28 14:33:11 +0900

    test(AttemptCountValidatorTest): 시도 횟수 입력 테스트

--- src/test/java/racingcar/validator/AttemptCountValidatorTest.java
+++ src/test/java/racingcar/validator/AttemptCountValidatorTest.java
@@ -1,4 +1,38 @@
 package racingcar.validator;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+import org.junit.jupiter.api.Test;
+
 public class AttemptCountValidatorTest {
+
+    private AttemptCountValidator attemptCountValidator = new AttemptCountValidator();
+
+    @Test
+    void 음수_입력() {
+        assertThatCode(() -> attemptCountValidator.validate("-1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Input a non-negative integer.");
+    }
+
+    @Test
+    void 소수_입력() {
+        assertThatCode(() -> attemptCountValidator.validate("0.1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Input a non-negative integer.");
+    }
+
+    @Test
+    void 식_입력() {
+        assertThatCode(() -> attemptCountValidator.validate("1+3"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Input a non-negative integer.");
+    }
+
+    @Test
+    void 문자열_입력() {
+        assertThatCode(() -> attemptCountValidator.validate("hello"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Input a non-negative integer.");
+    }
 }
