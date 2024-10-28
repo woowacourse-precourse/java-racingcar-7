045fac6a3d558abb21d9b132cacf01b621d78f05
Goohong Chung <9rrong@uoslife.team>
2024-10-28 23:20:04 +0900

    test(AttemptCount): `@ParameterizedTest` 문법 적용

--- src/test/java/racingcar/model/AttemptCountTest.java
+++ src/test/java/racingcar/model/AttemptCountTest.java
@@ -3,18 +3,21 @@ package racingcar.model;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class AttemptCountTest {
 
     @Test
-    void 시도_횟수에_음수가_들어오면_예외를_반환한다() {
+    void 음수가_들어오면_예외를_반환한다() {
         assertThatThrownBy(() -> new AttemptCount("-1"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 시도_횟수에_문자가_들어오면_예외를_반환한다() {
-        assertThatThrownBy(() -> new AttemptCount("a"))
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "abc", "!", "123abc", "-5"})
+    void 숫자가_아닌_문자가_들어오면_예외를_반환한다(String input) {
+        assertThatThrownBy(() -> new AttemptCount(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
