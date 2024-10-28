0e889c4c76c2084a4e86a1a77994ab87051a77e4
JEONGHUNJEON <jjhoon06@gmail.com>
2024-10-28 11:07:31 +0900

    refactor: 예외처리 추가

--- src/main/java/racingcar/service/Validate.java
+++ src/main/java/racingcar/service/Validate.java
@@ -2,26 +2,33 @@ package racingcar.service;
 
 public class Validate {
     public static String[] validateCarNames (String carNames) {
-        if (carNames.isEmpty()) {
+        if (carNames.isBlank()) {
             throw new IllegalArgumentException("이름을 입력해주세요.");
         }
         String[] carNamesList = carNames.split(",");
         for (String carName : carNamesList) {
             if (carName.length() > 5) {
                 throw new IllegalArgumentException("이름이 6자 이상입니다.");
+            } else if (carName.isBlank()) {
+                throw new IllegalArgumentException("이름을 입력해주세요.");
             }
         }
         return carNamesList;
     }
 
     public static int validateTryNumber (String tryNumber) {
+        int tryNumberToInt;
         if (tryNumber.isBlank()) {
             throw new IllegalArgumentException("게임 횟수를 입력해주세요.");
         }
         try {
-            return Integer.parseInt(tryNumber);
+            tryNumberToInt = Integer.parseInt(tryNumber);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자만 입력해주세요.");
+            throw new IllegalArgumentException("유효하지 않은 입력값입니다.");
         }
+        if (tryNumberToInt <= 0) {
+            throw new IllegalArgumentException("1이상의 숫자를 입력해주세요.");
+        }
+        return tryNumberToInt;
     }
 }
