bb178095912902b03b39299e1aa4e3e0fff8c759
JiEung2 <181757@jnu.ac.kr>
2024-10-28 22:41:56 +0900

    feat(game): 게임을 실행하는 run 메서드 추가

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,21 +1,48 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     private final InputView inputView;
+    private final OutputView outputView;
     private final CarGenerator carGenerator;
+    private final WinnerDecider winnerDecider;
     private final InputValidator inputValidator;
     private final RandomNumberGenerator randomNumberGenerator;
 
-    public Game(InputView inputView, CarGenerator carGenerator, InputValidator inputValidator,
+    public Game(InputView inputView, OutputView outputView, CarGenerator carGenerator, WinnerDecider winnerDecider,
+                InputValidator inputValidator,
                 RandomNumberGenerator randomNumberGenerator) {
         this.inputView = inputView;
+        this.outputView = outputView;
         this.carGenerator = carGenerator;
+        this.winnerDecider = winnerDecider;
         this.inputValidator = inputValidator;
         this.randomNumberGenerator = randomNumberGenerator;
     }
 
     public void run() {
+        outputView.printCarNameInputMessage();
+        List<Car> carList = carGenerator.generateCars(inputView.inputCarNames());
+        outputView.printRoundCountInputMessage();
+        int tryCount = inputView.inputRoundCount();
 
+        List<RoundHistory> roundHistoryList = new ArrayList<>();
+
+        for(int i = 0; i < tryCount; i++){
+            for (Car car : carList) {
+                if(isMovable(randomNumberGenerator.generate())){
+                    car.move();
+                }
+            }
+            roundHistoryList.add(RoundHistory.saveCarPosition(carList));
+        }
+
+        outputView.printRoundResults(roundHistoryList);
+
+        List<String> winners = winnerDecider.decideWinner(carList);
+        outputView.printWinner(winners);
     }
 
     private boolean isMovable(int randomNumber) {
