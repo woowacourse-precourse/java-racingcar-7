14976d40f4f1c302867bf3859fb85a2298b66555
hjg1473 <hjg1473@naver.com>
2024-10-28 09:11:27 +0900

    refactor: Use code formatting

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -18,40 +18,42 @@ public class Application {
 
         String[] carScore = new String[cars.length];
         InitializeToSpace(carScore);
+
         System.out.println("실행 결과");
-        for(int i = 0; i < number; i++) {
+        for (int i = 0; i < number; i++) {
             StartRaceOnetime(cars, carScore);
             PrintCurrentScore(cars, carScore);
             System.out.println();
         }
+
         int[] carScoreInt = new int[cars.length];
         StringtoIntArray(carScore, carScoreInt);
         int max = FindLargestValue(carScoreInt);
         int countWinners = CountWinners(carScoreInt, max);
         int[] winners = new int[countWinners];
         StoreWinnerIndex(winners, carScoreInt, max);
+
         System.out.print("최종 우승자 : ");
-        for(int i = 0; i < countWinners; i++) {
-            if(i == countWinners - 1) {
+        for (int i = 0; i < countWinners; i++) {
+            if (i == countWinners - 1) {
                 System.out.print(cars[winners[i]]);
-            }
-            else {
-                System.out.print(cars[winners[i]]+", ");
+            } else {
+                System.out.print(cars[winners[i]] + ", ");
             }
         }
     }
 
     // 1. 잘못된 입력 판단 함수
     private static void VaildInput(String[] cars, String stringNumber) {
-        for(int i = 0; i < cars.length; i++) {
+        for (int i = 0; i < cars.length; i++) {
             // 5글자 이상이거나, 공백 입력인 경우
-            if(cars[i].length() > 5 || cars[i].isEmpty()) {
+            if (cars[i].length() > 5 || cars[i].isEmpty()) {
                 throw new IllegalArgumentException("IllegalArgumentException");
             }
         }
 
         // stringNumber 가 양의 정수가 아니면
-        if(!stringNumber.matches("\\d+")) {
+        if (!stringNumber.matches("\\d+")) {
             throw new IllegalArgumentException("IllegalArgumentException");
         }
     }
@@ -64,7 +66,7 @@ public class Application {
 
     // 4. 참여자들의 경주를 1회 시행했을 때의 결과 계산.
     private static void StartRaceOnetime(String[] cars, String[] carScore) {
-        for(int i = 0; i < cars.length; i++) {
+        for (int i = 0; i < cars.length; i++) {
             int random = Randoms.pickNumberInRange(0, 9);
             if (DecideStopOrNot(random)) {
                 carScore[i] += "-";
@@ -74,21 +76,21 @@ public class Application {
 
     // Additionally, 공백으로 문자열 배열 초기화.
     private static void InitializeToSpace(String[] str) {
-        for(int i = 0; i < str.length; i++) {
+        for (int i = 0; i < str.length; i++) {
             str[i] = "";
         }
     }
 
     // 5. 현재 자동차들의 상황을 출력하는 함수
     private static void PrintCurrentScore(String[] cars, String[] carScore) {
-        for(int i = 0; i < cars.length; i++) {
-            System.out.println(cars[i]+" : "+carScore[i]);
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i] + " : " + carScore[i]);
         }
     }
 
     // Additionally, 문자열 배열을 해당 문자열 길이의 정수 배열로 변환하는 함수
     private static void StringtoIntArray(String[] str, int[] ary) {
-        for(int i = 0; i < str.length; i++) {
+        for (int i = 0; i < str.length; i++) {
             ary[i] = str[i].length();
         }
     }
@@ -97,7 +99,7 @@ public class Application {
     // 점수가 같으면 입력한 순서대로 출력
     private static void StoreWinnerIndex(int[] ary, int[] carScoreInt, int maxValue) {
         int index = 0;
-        for(int i = 0; i < carScoreInt.length; i++) {
+        for (int i = 0; i < carScoreInt.length; i++) {
             if (carScoreInt[i] == maxValue) {
                 ary[index] = i;
                 index++;
@@ -108,7 +110,7 @@ public class Application {
     // 6-1. 배열에서 가장 큰 값을 찾는 함수
     private static int FindLargestValue(int[] carScoreInt) {
         int maxValue = carScoreInt[0];
-        for(int i = 1; i < carScoreInt.length; i++) {
+        for (int i = 1; i < carScoreInt.length; i++) {
             maxValue = Math.max(maxValue, carScoreInt[i]);
         }
         return maxValue;
@@ -117,7 +119,7 @@ public class Application {
     // 6-2. 우승자의 인원을 반환하는 함수
     private static int CountWinners(int[] carScoreInt, int maxValue) {
         int index = 0;
-        for(int i = 0; i < carScoreInt.length; i++) {
+        for (int i = 0; i < carScoreInt.length; i++) {
             if (carScoreInt[i] == maxValue) {
                 index++;
             }
