60cd456e5e929e83d463c048d2ece63141834862
Si Yeong Park <psy915@swu.ac.kr>
2024-10-28 22:42:10 +0900

    refactor(Application): GameController의 runGame() 메서드를 호출하도록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,7 +3,11 @@ package racingcar;
 import racingcar.controller.GameController;
 public class Application {
     public static void main(String[] args) {
-        GameController controller = new GameController();
-        controller.runGame();
+        GameController gameController = new GameController();
+        try {
+            gameController.runGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println("게임 실행 중 오류 발생: " + e.getMessage());
+        }
     }
 }
