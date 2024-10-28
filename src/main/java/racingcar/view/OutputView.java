ed979c98095836e948ee3c24877e7d195c2587e1
spotydol7 <spotydol7@gmail.com>
2024-10-28 21:59:44 +0900

    feat: 결과 출력 기능 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,22 @@
+package racingcar.view;
+
+import racingcar.domain.Car;
+
+import java.util.List;
+
+public class OutputView {
+    public static void printResultMessage() {
+        System.out.println("\n실행 결과");
+    }
+
+    public static void printCarPositions(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.println("최종 우승자: " + String.join(", ", winners));
+    }
+}
