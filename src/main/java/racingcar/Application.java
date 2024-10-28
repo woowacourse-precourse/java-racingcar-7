da77e464b32621dbf5011b6b0523cba3946701c3
Grayson1999 <bbt1250912@gmail.com>
2024-10-28 19:00:03 +0900

    feat: 사용자 입력 및 입력 검증 기능 추가
    입력에 null, 빈 값이 입력되었을 때, 오류를 반환하는 예외 처리
    반복 횟수에 문자가 입력되었을 때, 오류를 반환하는 예외 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -8,8 +9,35 @@ import java.util.LinkedHashMap;
 import java.util.List;
 
 public class Application {
+    //쉼표가 아닌 구분자 예외처리
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //변수 초기화
+        String inputCarNames;
+        int NumberOfRounds;
+        List<String> carNames;
+        List<String> winner;
+
+        //사용자 입력
+        inputCarNames = Console.readLine();
+        String inputNumber = Console.readLine();
+
+        //사용자 입력 검증
+        validateInput(inputCarNames, inputNumber);
+
+        carNames = splitCarNames(inputCarNames);
+        NumberOfRounds = Integer.parseInt(inputNumber);
+
+        winner = playGame(carNames, NumberOfRounds);
+    }
+
+    public static void validateInput(String inputCarName, String inputNumber) throws IllegalArgumentException {
+        //사용자 입력 값 검증
+        if (inputNumber == null || inputNumber.isEmpty() || !inputNumber.matches("\\d+")) {
+            throw new IllegalArgumentException("반복 횟수 " + inputNumber + " 는 숫자만 입력 가능합니다.");
+        }
+        if (inputCarName == null || inputCarName.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름을 입력 해야 합니다.");
+        }
     }
 
     public static List<String> playGame(List<String> carNames, int numberOfMoves) {
@@ -21,12 +49,12 @@ public class Application {
             carDistanceResults.put(carName, 0);
         }
 
+        System.out.println("실행 결과");
         for (int i = 0; i < numberOfMoves; i++) {
             for (String carName : carDistanceResults.keySet()) {
                 int randomNumber = Randoms.pickNumberInRange(0, 9);
                 updateDistance(carDistanceResults, carName, randomNumber);
             }
-            System.out.println("실행 결과");
             printRoundScores(carDistanceResults);
             System.out.println();
         }
@@ -58,8 +86,11 @@ public class Application {
         System.out.println();
     }
 
-    //쉼표가 아닌 구분자 예외처리
-    //공백 처리
+
+    //공백 처리 "woni, jun,pobi "
+    //이름이 같을 경우 "woni,woni"
+    //한 개만 입력 될 때 처리 "woni"
+    //이름에 숫자만 있을 경우 처리 "1234"
     public static List<String> splitCarNames(String userInputNames) throws IllegalAccessError {
         List<String> carNames = Arrays.asList(userInputNames.split(","));
         for (String carName : carNames) {
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -11,6 +12,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberI
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -28,40 +30,76 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 자동차_이동_테스트() {
-        // Given
-        List<String> carNames = Arrays.asList("pobi", "woni", "jun");
-        LinkedHashMap<String, Integer> carDistanceResults = new LinkedHashMap<>();
+    void 입력값_검증_테스트() {
+        assertAll(
+                () -> assertThatThrownBy(() -> Application.validateInput("woni,pobi", "1a"))
+                        .isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(() -> Application.validateInput("", "1"))
+                        .isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(() -> Application.validateInput(null, "1"))
+                        .isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(() -> Application.validateInput("woni,pobi", ""))
+                        .isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(() -> Application.validateInput("woni,pobi", null))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        for (String carName : carNames) {
-            carDistanceResults.put(carName, 0);
+    @Nested
+    class 자동차_경주_테스트 {
+
+        @Test
+        void 자동차_이동_테스트() {
+            // Given
+            List<String> carNames = Arrays.asList("pobi", "woni", "gray");
+            LinkedHashMap<String, Integer> carDistanceResults = new LinkedHashMap<>();
+
+            for (String carName : carNames) {
+                carDistanceResults.put(carName, 0);
+            }
+
+            // When
+            Application.updateDistance(carDistanceResults, "pobi", MOVING_FORWARD);
+            Application.updateDistance(carDistanceResults, "woni", MOVING_FORWARD);
+            Application.updateDistance(carDistanceResults, "gray", STOP);
+
+            // Then: 결과 검증
+            assertThat(carDistanceResults.get("pobi")).isEqualTo(1);
+            assertThat(carDistanceResults.get("woni")).isEqualTo(1);
+            assertThat(carDistanceResults.get("gray")).isEqualTo(0);
+
         }
 
-        // When
-        Application.updateDistance(carDistanceResults, "pobi", MOVING_FORWARD);
-        Application.updateDistance(carDistanceResults, "woni", MOVING_FORWARD);
-        Application.updateDistance(carDistanceResults, "jun", STOP);
+        @Test
+        void 자동차_경주_결과_테스트() {
+            // Given
+            List<String> carNames = Arrays.asList("pobi", "woni", "gray");
+            int numberOfMoves = 5;
 
-        // Then: 결과 검증
-        assertThat(carDistanceResults.get("pobi")).isEqualTo(1);
-        assertThat(carDistanceResults.get("woni")).isEqualTo(1);
-        assertThat(carDistanceResults.get("jun")).isEqualTo(0);
+            // When
+            List<String> winners = Application.playGame(carNames, numberOfMoves);
 
+            // Then
+            assertThat(winners).isNotEmpty();
+            assertThat(output()).contains("pobi", "woni", "gray");
+        }
+
+        @Test
+        void 자동차_경주_출력_테스트() {
+            //Given
+            LinkedHashMap<String, Integer> carDistanceResults = new LinkedHashMap<>() {{
+                put("pobi", 3);
+                put("woni", 5);
+            }};
+            //When
+            Application.printRoundScores(carDistanceResults);
+
+            //Then
+            String expectedOutput = "pobi : ---\n" + "woni : -----";
+            assertThat(output()).contains(expectedOutput);
+        }
     }
 
-    @Test
-    void 자동차_경주_결과_테스트() {
-        // Given: 자동차 이름과 이동 횟수 설정
-        List<String> carNames = Arrays.asList("pobi", "woni", "jun");
-        int numberOfMoves = 5;
-
-        // When: playGame() 메서드 호출
-        List<String> winners = Application.playGame(carNames, numberOfMoves);
-
-        // Then: 결과 검증
-        assertThat(winners).isNotEmpty();
-        assertThat(output()).contains("pobi", "woni", "jun");
-    }
 
     @Test
     void 예외_테스트() {
