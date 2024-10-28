3eaa88541ae8989def9a08322e92c49f18a47005
belight2 <chlwnsgur090@gmail.com>
2024-10-28 18:13:01 +0900

    feat: main문 작성 완료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import manager.InputManager;
+import manager.ResultManager;
+import race.RacingProgress;
+import validation.InputValidator;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputManager inputManager = new InputManager(new InputValidator());
+        RacingProgress racing = new RacingProgress(inputManager.inputCars(), inputManager.inputMoveCount());
+
+        racing.raceStart();
+
+        ResultManager resultManager = new ResultManager(racing.getWinnerCarList());
+        System.out.println(resultManager.getWinners());
     }
 }
