9a64488bed66dadbf0af27da134c679cbe4ae998
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-10-28 20:24:40 +0900

    feat: Add initializeRaceResults
    - 새로운 initializeRaceResults를 생성
    - race 결과를 담기 위해 carNames 갯수 만큼 List를 생성해 return 하는 함수

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,10 +2,14 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         String[] carNames = getCarNames();
         Integer attempts = getNumberOfAttempts();
+        List<StringBuilder> raceResults = initializeRaceResults(carNames);
     }
 
     private static String[] getCarNames(){
@@ -17,5 +21,13 @@ public class Application {
     private static int getNumberOfAttempts(){
         System.out.println("시도할 횟수는 몇 회인가요?");
         return Integer.parseInt(Console.readLine());
+    }
+
+    private static List<StringBuilder> initializeRaceResults(String[] carNames){
+        List<StringBuilder> raceResults = new ArrayList<>();
+        for (String carName : carNames) {
+            raceResults.add(new StringBuilder(carName));
+        }
+        return raceResults;
     }
 }
