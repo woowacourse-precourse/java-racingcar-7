e56c5d28bcfa4ef3118a7afc5e913f9cde7013a8
chahyunsoo <chahs1012@naver.com>
2024-10-28 09:04:52 +0900

    feat(ValidatorFactory) : 유효성 검사기 생성 기능 구현, ValidatorFactory 클래스 추가
    * ValidatorFactory 클래스는 유효성 검사기(Validator)를 생성하고 관리하는 역할을 합니다.
    
    * buildFactory 메서드를 통해 기본 유효성 검사기(CarValidator, RacerValidator, RoundValidator)들을 포함한 팩토리를 생성합니다.
    
    * 싱글톤 패턴을 적용하여 동일한 ValidatorFactory 인스턴스를 재사용합니다.
    
    * getValidator 메서드는 주어진 클래스 타입에 맞는 유효성 검사기를 반환하며, 지원하지 않는 타입일 경우 예외를 발생시킵니다.

---
+++ src/main/java/racingcar/validation/ValidatorFactory.java
@@ -0,0 +1,27 @@
+package racingcar.validation;
+
+import java.util.Set;
+
+public class ValidatorFactory {
+    private static ValidatorFactory validatorFactory;
+    private final Set<Validator> validators;
+
+    private ValidatorFactory(Set<Validator> validators) {
+        validatorFactory = this;
+        this.validators = validators;
+    }
+
+    public static ValidatorFactory buildFactory() {
+        if (validatorFactory != null) {
+            return validatorFactory;
+        }
+        return new ValidatorFactory(Set.of(new CarValidator(), new RacerValidator(), new RoundValidator()));
+    }
+
+    public Validator getValidator(Class<?> clazz) {
+        return validators.stream()
+                .filter(validator -> validator.supportsType().isAssignableFrom(clazz))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
