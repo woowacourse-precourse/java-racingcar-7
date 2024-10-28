82edef8c5845bab00aa1d0f8d267a86d2f11c799
gibumgo <gibumgo@gmail.com>
2024-10-28 11:06:14 +0900

    feat : 시도할 횟수 입력 기능

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final String CAR_NAME_QUESTION = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRY_COUNT_QUESTION = "시도할 횟수는 몇 회인가요?";
     private static final String DELIMITER_COMMA = ",";
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -19,5 +20,10 @@ public class InputView {
     private List<String> splitCarsName(String inputNames) {
         return Arrays.stream(inputNames.split(DELIMITER_COMMA))
                 .collect(Collectors.toList());
+    }
+
+    public int inputTryCount() {
+        System.out.println(TRY_COUNT_QUESTION);
+        return scanner.nextInt();
     }
 }
