7f08fe9ef32dad223bf6c35d6a0a5d649534c337
geonu <kgo000926@gmail.com>
2024-10-28 20:43:10 +0900

    feat: 입력받기 위한 요청 메시지 출력 기능

--- src/main/java/racingcar/io/OutputView.java
+++ src/main/java/racingcar/io/OutputView.java
@@ -9,4 +9,12 @@ public class OutputView {
     public void printWinners(String winnerMessage) {
         System.out.println("최종 우승자 : " + winnerMessage);
     }
+
+    public void printRequestInputCarNameMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void printRequestInputNumberOfTimesMessage() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
 }
