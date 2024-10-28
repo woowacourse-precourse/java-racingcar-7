6a72702ee7684f0fbdac7f36ec85bfd39b34c576
이건희 <dnzp74@g.eulji.ac.kr>
2024-10-28 23:05:47 +0900

    refactor(Car) : 자동차 객체 생성 로직을 이동
    - Car 클래스에 generate 메서드를 추가하여 자동차 리스트 생성 책임을 캡슐화

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,6 +1,5 @@
 package racingcar.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import racingcar.model.Car;
 import racingcar.model.RacingGame;
@@ -13,12 +12,9 @@ public class RacingGameController {
         String[] carNames = InputView.getCarNames();
         int tryCount = InputView.getTryCount();
 
-        List<Car> cars = new ArrayList<>();
-        for (String name : carNames) {
-            cars.add(new Car(name.trim()));
-        }
-
+        List<Car> cars = Car.generate(carNames);
         RacingGame game = new RacingGame(cars);
+
         for (int i = 0; i < tryCount; i++) {
             List<Car> roundCars = game.playRound();
             ResultView.printRoundResults(roundCars);
--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,5 +1,8 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
     private final String name;
     private int position;
@@ -7,6 +10,14 @@ public class Car {
     public Car(String name) {
         this.name = name;
         this.position = 0;
+    }
+
+    public static List<Car> generate(String[] carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name.trim()));
+        }
+        return cars;
     }
 
     public void move(int randomValue) {
