3f7dab12de196210fcae7c22848486b796e2904c
jijongkwon <help3451@naver.com>
2024-10-28 21:47:21 +0900

    feat(ui) : 우승자 출력 콘솔 화면 구현

--- src/main/java/racingcar/presentation/ui/OutputView.java
+++ src/main/java/racingcar/presentation/ui/OutputView.java
@@ -1,29 +1,25 @@
 package racingcar.presentation.ui;
 
 import racingcar.application.service.history.RacingHistoryService;
-import racingcar.domain.car.repository.RacingCarRepository;
-import racingcar.domain.winner.Winner;
-
-import java.util.List;
-import racingcar.infrastructure.persistence.InMemoryRacingCarRepository;
+import racingcar.application.service.winner.WinnerService;
 import racingcar.presentation.enums.UiMessage;
 
 public class OutputView {
     private final RacingHistoryService racingHistoryService;
-    private final RacingCarRepository racingCarRepository;
+    private final WinnerService winnerService;
 
     public OutputView() {
         this.racingHistoryService = new RacingHistoryService();
-        this.racingCarRepository = InMemoryRacingCarRepository.getInstance();
+        this.winnerService = new WinnerService();
     }
 
-    // TODO: 실행 결과 화면 출력
     public void outputRacingResultUi() {
         System.out.println(UiMessage.UI_OUTPUT_EXECUTION_RESULT_MESSAGE.getMessage());
         racingHistoryService.printRacingResult();
     }
 
-    // TODO: 최종 우승자 화면 출력
-    public void outputWinnerUi(List<Winner> winners) {
+    public void outputWinnerUi() {
+        System.out.println(UiMessage.UI_OUTPUT_WINNER_RESULT_MESSAGE.getMessage() + winnerService.getWinnerToString());
+
     }
 }
