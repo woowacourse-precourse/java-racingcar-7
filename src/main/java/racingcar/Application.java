b87892c25522ce93f5d3d522f40d1f18a53880d8
chul5 <och7164@gmail.com>
2024-10-28 17:31:37 +0900

    feat: 메인 문에 carController초기화 후 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.controller.CarController;
+import racingcar.service.CarService;
+import racingcar.util.InputView;
+import racingcar.util.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CarController carController = new CarController(new CarService(), new InputView(), new OutputView());
+        carController.run();
     }
 }
