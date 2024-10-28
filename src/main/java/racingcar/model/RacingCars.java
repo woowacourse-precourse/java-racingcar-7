9b8e45bdc9fd6f5affa282e43f1dbf1e08a9d3eb
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-28 19:06:11 +0900

    feat: 자동차 리스트 유효성 검사 추가

--- src/main/java/racingcar/model/RacingCars.java
+++ src/main/java/racingcar/model/RacingCars.java
@@ -10,7 +10,7 @@ public class RacingCars implements Cars {
     private final List<Car> cars;
 
     public RacingCars(List<Car> cars, MoveStrategy moveStrategy) {
-        
+        validateEmpty(cars);
         this.cars = cars;
         this.moveStrategy = moveStrategy;
     }
@@ -29,4 +29,9 @@ public class RacingCars implements Cars {
         return cars;
     }
 
+    private void validateEmpty(List<Car> cars) {
+        if (cars == null || cars.isEmpty()) {
+            throw new CarNameException(ExceptionMessages.EMPTY_INPUT.getMessage());
+        }
+    }
 }
