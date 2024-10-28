3a0ce837968547ab1b8db690d1ccd6ce2cc943b5
ahpicl64 <ahpicl64@gmail.com>
2024-10-28 12:41:26 +0900

    feat(view): 실행 결과값 출력을 위한 OutputView 구현
    - showResultMessage 메서드로 실행 결과 출력
    - 레이싱 진행 경과 출력을 위한 printRacing 구현
    - 최종 우승자 출력

---
+++ src/main/java/racingcar/OutputView.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+public class OutputView {
+
+    public void showResultMessage() {
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
+    public void printRacing(String carName, int distance) {
+        System.out.println(carName + " : " + "-".repeat(distance));
+    }
+
+    public void printWinners(String winners) {
+        System.out.print("최종 우승자 : " + winners);
+    }
+}
