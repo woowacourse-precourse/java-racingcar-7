60a5e4d23535cdbef2b949f511d4cf41bee2ccdd
Seol-Munhyeok <abc1234568@naver.com>
2024-10-28 15:59:16 +0900

    test(Application): Add main application logic

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,9 +2,26 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
 
+        List<String> carNames = inputHandler.getCarNames();
+        inputHandler.areAllCarNameLengthsValid(carNames);
+
+        List<Car> cars = carNames.stream()
+                .map(Car::new)
+                .toList();
+
+        int moveCount = inputHandler.getMoveCount();
+
+        WinnerManager winnerManager = new WinnerManager();
+        RacingGame racingGame = new RacingGame(cars, moveCount, winnerManager);
+
+        racingGame.playGame();
+
+        racingGame.announceWinners();
     }
 }
