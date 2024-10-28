cfb1a6319888c3fea5ed0c1ec730a9688c8d8928
Koo-EunSung <ku903@naver.com>
2024-10-28 18:00:45 +0900

    Test: 단독 우승자 반환

--- src/test/java/racingcar/model/RacingGameTest.java
+++ src/test/java/racingcar/model/RacingGameTest.java
@@ -2,10 +2,15 @@ package racingcar.model;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
+import org.junit.platform.commons.util.ReflectionUtils.HierarchyTraversalMode;
 
 public class RacingGameTest {
     @Test
@@ -17,5 +22,21 @@ public class RacingGameTest {
         assertThatIllegalArgumentException().isThrownBy(() -> {
             RacingGame racingGame = new RacingGame(racer, round);
         });
+    }
+
+    @Test
+    void 우승자_반환_테스트_단독() {
+        Car car1 = new Car("temp1");
+        Car car2 = new Car("temp2");
+        RacingGame racingGame = new RacingGame(new ArrayList<>(Arrays.asList(car1, car2)), 1);
+
+        car1.go(5);
+        car1.go(5);
+        car1.go(5);
+
+        racingGame.progress();
+        List<String> winner = racingGame.selectWinner();
+
+        assertThat(winner).containsExactly("temp1");
     }
 }
