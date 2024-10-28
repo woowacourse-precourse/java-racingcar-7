e18087eb00bb9a167fbb8706f7d394c802d2ef91
seonmin <w.hand715@gmail.com>
2024-10-28 09:29:33 +0900

    fix: Validator 메서드명 변경에 따른 테스트 코드 오류 수정

--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -9,36 +9,36 @@ class ValidatorTest {
     @Test
     void 정상() {
         String input = "pobi,woni,jun";
-        assertTrue(Validator.validate(input));
+        assertTrue(Validator.validateCarNames(input));
     }
 
     @Test
     void 공백포함_글자제한_넘어가는_자동차() {
         String input = "pobi   ,test,jun";
-        assertTrue(Validator.validate(input));
+        assertTrue(Validator.validateCarNames(input));
     }
 
     @Test
     void 널() {
         String input = null;
-        assertFalse(Validator.validate(input));
+        assertFalse(Validator.validateCarNames(input));
     }
 
     @Test
     void 전체_Blank() {
         String input = " ";
-        assertFalse(Validator.validate(input));
+        assertFalse(Validator.validateCarNames(input));
     }
 
     @Test
     void 중간_Blank() {
         String input = "pobi,  ,jun";
-        assertFalse(Validator.validate(input));
+        assertFalse(Validator.validateCarNames(input));
     }
 
     @Test
     void 글자제한_넘어가는_자동차이름() {
         String input = "pobi,test5123,jun";
-        assertFalse(Validator.validate(input));
+        assertFalse(Validator.validateCarNames(input));
     }
 }
