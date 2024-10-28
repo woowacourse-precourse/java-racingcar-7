485b513b5a7e96d131291ab8385d6c4920b2c1fe
ruudska6 <feels19@naver.com>
2024-10-28 14:54:11 +0900

    <refactor>: InputView에 생성자 주입을 도입하여 테스트 용이성 개선과 그에 따른 askTryCount() 수정 & 예외 처리 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,17 +1,30 @@
 package racingcar.view;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class InputView {
     private static final String QUESTION_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String QUESTION_TRY_COUNT = "시도할 회수는 몇회인가요?";
     private static final int MAX_NAME_LENGTH = 5;
-    private static final int MIN_CAR = 2;
+    private static final int MIN_CAR_SIZE = 2;
+
+    private final Supplier<String> inputProvider;
+
+    public InputView() {
+        this(Console::readLine);
+    }
+
+    // 테스트 할 때 사용함
+    public InputView(Supplier<String> inputProvider) {
+        this.inputProvider = inputProvider;
+    }
 
     public List<String> askCarNames() {
         System.out.println(QUESTION_CAR_NAMES);
-        String inputCarNames = Console.readLine();
+        String inputCarNames = inputProvider.get();
         List<String> carNames = convertInputCarNamesToList(inputCarNames);
 
         for (String name : carNames) {
@@ -20,27 +33,40 @@ public class InputView {
             }
         }
 
-        if (carNames.size() < MIN_CAR) {
+        if (carNames.size() < MIN_CAR_SIZE) {
             throw new IllegalArgumentException("경주를 하기 위해서는 최소 2대 이상의 자동차가 필요합니다.");
         }
 
-        return convertInputCarNamesToList(inputCarNames);
-        }
-
-    public int askTryCount () {
-        System.out.println(QUESTION_TRY_COUNT);
-        return Integer.parseInt(Console.readLine());
+        return carNames;
     }
 
+    public int askTryCount() {
+        System.out.println(QUESTION_TRY_COUNT);
+        String input = inputProvider.get();
+        int tryCount;
+
+        try {
+            tryCount = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+        }
+
+        if (tryCount <= 0) {
+            throw new IllegalArgumentException("양의 정수를 입력해야 합니다.");
+        }
+
+        return tryCount;
+    }
+
+
     private List<String> convertInputCarNamesToList(String inputCarNames) {
-        String[] CarNames = inputCarNames.split(",");
+        String[] carNamesArray = inputCarNames.split(",");
         List<String> carNames = new ArrayList<>();
 
-        for (String CarName : CarNames) {
-            String trimmedCarName = CarName.trim();
+        for (String carName : carNamesArray) {
+            String trimmedCarName = carName.trim();
             carNames.add(trimmedCarName);
         }
         return carNames;
     }
-
 }
