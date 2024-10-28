576f3ad9886ef474275f845fd2833665d84a919e
taegyunB <taegyunb99@gmail.com>
2024-10-28 22:10:01 +0900

    fix(CarClass): Car 클래스 따로 구별

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -110,35 +110,3 @@ public class Application {
     }
 }
 
-class Car {
-    private String name;
-    private int position = 0;
-
-    public Car(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPosition() {
-        return position;
-    }
-
-    public void move() {
-        int randomValue = Randoms.pickNumberInRange(0, 9);
-        if (randomValue >= 4) {
-            position++;
-        }
-    }
-
-    public void printPosition() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(name).append(" : ");
-        for (int i = 0; i < position; i++) {
-            sb.append("-");
-        }
-        System.out.println(sb);
-    }
-}
