5a0a436e39079ebde7eb63cc30a13047fdd9545e
sangxxjin <sj_0368@naver.com>
2024-10-28 16:16:02 +0900

    feat: 최종 우승자 출력 메서드 구현

--- src/main/java/racingcar/views/OutPutView.java
+++ src/main/java/racingcar/views/OutPutView.java
@@ -1,9 +1,11 @@
 package racingcar.views;
 
+import java.util.List;
 import java.util.Map;
 
 public class OutPutView {
     private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
     private static final String DISTANCE_SYMBOL = "-";
 
     public static void displayResultMessage() {
@@ -15,6 +17,10 @@ public class OutPutView {
             System.out.println(formatCarInfo(carName, distance));
         });
         System.out.println();
+    }
+
+    public static void displayWinner(List<String> winners) {
+        System.out.println(WINNER_MESSAGE + String.join(", ", winners));
     }
 
     private static String formatCarInfo(String carName, int distance) {
