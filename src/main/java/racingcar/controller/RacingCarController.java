84504e9b97c83ef7c7b9466976727c325e26abca
byunghyunkim0 <bb05qudgus@gamil.com>
2024-10-28 22:24:04 +0900

    feat: racingCar 컨트롤러 기능 구현

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,28 @@
+package racingcar.controller;
+
+import racingcar.domain.Race;
+import racingcar.service.RacingCarService;
+import racingcar.view.RacingCarInputView;
+import racingcar.view.RacingCarOutputView;
+
+public class RacingCarController {
+    private final Race race;
+    private final RacingCarService racingCarService;
+    private final RacingCarInputView racingCarInputView;
+    private final RacingCarOutputView racingCarOutputView;
+
+
+    public RacingCarController() {
+        this.race = new Race();
+        this.racingCarService = new RacingCarService();
+        this.racingCarInputView = new RacingCarInputView();
+        this.racingCarOutputView = new RacingCarOutputView();
+    }
+
+    public void run() {
+        String[] racingCarNames = racingCarInputView.readRacingCarNameInput();
+        int racingCarCount = racingCarInputView.readRacingCountInput();
+        racingCarOutputView.printRacingCarResultMessage();
+        racingCarService.createRace(race, racingCarNames);
+    }
+}
