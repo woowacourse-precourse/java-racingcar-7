7fd3963bba9cd38017caafbb5b1d0b84a4fd131f
JEN <ekffuclxk@gmail.com>
2024-10-28 13:13:22 +0900

    reafactor(application): 자동차 이름 입력 기능 Input 클래스로 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -23,9 +23,9 @@ public class Application {
     */
     public static void main(String[] args) {
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String inputCarNames = Console.readLine();
+        Input input = new Input();
 
+        String inputCarNames = input.getCarNames();
         List<String> carNames = splitCarNames(inputCarNames);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
