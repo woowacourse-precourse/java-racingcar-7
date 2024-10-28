442448b6e1d2f98f4e90f99cd56ac902ba4e864d
jieun <ju3391@naver.com>
2024-10-28 20:35:57 +0900

    refactor(view): 출력 메시지 상수화

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import racingcar.car.validation.CarNameValidator;
 
 //출력문 상수화하기!
 //주석 필요없는 건 지우기!
@@ -11,19 +12,28 @@ import java.util.stream.Collectors;
 // 사용자 입력을 담당하는 클래스
 public class Input {
 
-    private static final String CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요 (쉼표로 구분):";
-    private static final String ATTEMPT_COUNT_MESSAGE = "시도할 횟수를 입력하세요:";
+    private static final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ATTEMPT_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String CAR_NAME_DELIMITER = ",";
+
+    private final CarNameValidator carNameValidator;
+
+    public Input(CarNameValidator carNameValidator) {
+        this.carNameValidator = carNameValidator;
+    }
 
     public List<String> getCarNames() {
-        System.out.println(CAR_NAMES_MESSAGE);
+        System.out.println(CAR_NAME_MESSAGE);
         String input = Console.readLine();
-        return Arrays.stream(input.split(","))
+
+        // 입력받은 이름들을 리스트로 변환
+        return Arrays.stream(input.split(CAR_NAME_DELIMITER))
                 .map(String::trim)
                 .collect(Collectors.toList());
     }
 
-    public int getAttemptCount() {
+    public String getAttemptCount() {
         System.out.println(ATTEMPT_COUNT_MESSAGE);
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 }
--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -6,12 +6,15 @@ import racingcar.car.model.Car;
 
 // 게임 결과를 출력하는 클래스
 public class Output {
-    private static final String WINNER_MESSAGE = "최종 우승자 : %s";
+    private static final String CAR_POSITION_RESULT_FORMAT = "%s : %s";
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자 : %s";
+    private static final String CAR_NAME_DELIMITER = ", ";
 
     // 경주 결과를 출력하는 메서드
     public void printRaceResults(List<Car> cars) {
         cars.forEach(car -> {
-            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+            System.out.printf(CAR_POSITION_RESULT_FORMAT, car.getName(), "-".repeat(car.getPosition()));
+            System.out.println();
         });
         System.out.println();  // 차수 결과 구분을 위해 빈 줄 추가
     }
@@ -20,7 +23,7 @@ public class Output {
     public void printWinners(List<Car> winners) {
         String winnerNames = winners.stream()
                 .map(Car::getName)
-                .collect(Collectors.joining(", "));
-        System.out.printf(WINNER_MESSAGE, winnerNames);
+                .collect(Collectors.joining(CAR_NAME_DELIMITER));
+        System.out.printf(FINAL_WINNER_MESSAGE, winnerNames);
     }
 }
