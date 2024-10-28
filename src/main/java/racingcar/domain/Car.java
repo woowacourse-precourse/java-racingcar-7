e7da00628587d9ff9a46335b704977f66f534148
MyStraw <kikongdosa@gmail.com>
2024-10-28 21:59:07 +0900

    feat(domain): Implement Car domain class
    - Create Car class with name and position fields
    - Implement movement logic based on random number
    - Add position representation with hyphens
    - Override equals and hashCode for proper object comparison

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,48 @@
+package racingcar.domain;
+
+import racingcar.Constants;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move(int randomNumber) {
+        if (randomNumber >= Constants.MOVE_THRESHOLD) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getMoveHyphen() {
+        StringBuilder hyphen = new StringBuilder();
+        for (int i = 0; i < position; i++) {
+            hyphen.append("-");
+        }
+        return hyphen.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return name.equals(car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+}
