89f130cc3e1cf8df0d06bb47b46f4fb9b5c3dfce
Koo-EunSung <ku903@naver.com>
2024-10-28 18:25:02 +0900

    Test: 공동 우승자 반환

--- src/test/java/racingcar/model/RacingGameTest.java
+++ src/test/java/racingcar/model/RacingGameTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.util.ReflectionUtils;
 import org.junit.platform.commons.util.ReflectionUtils.HierarchyTraversalMode;
+import racingcar.dto.Result;
+import racingcar.dto.Result.Round;
 
 public class RacingGameTest {
     @Test
@@ -38,5 +40,30 @@ public class RacingGameTest {
         List<String> winner = racingGame.selectWinner();
 
         assertThat(winner).containsExactly("temp1");
+    }
+
+    @Test
+    void 우승자_반환_테스트_공동() {
+        Result.Round car1 = new Result.Round("car1", 1);
+        Result.Round car2 = new Result.Round("car2", 1);
+        RacingGame racingGame = new RacingGame(new ArrayList<>(), 1);
+
+
+        Field field;
+        try {
+            field = racingGame.getClass().getDeclaredField("resultOfRound");
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+
+        field.setAccessible(true);
+        try {
+            field.set(racingGame, Arrays.asList(car1, car2));
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        List<String> winner = racingGame.selectWinner();
+        assertThat(winner).containsExactly("car1", "car2");
     }
 }
