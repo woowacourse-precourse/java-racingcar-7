d79b3e765628c96b932c65ced63c3441d503e80e
smk412 <whu1477@naver.com>
2024-10-28 20:31:49 +0900

    feat: 이동을 시도할 횟수 입력하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,7 +8,9 @@ public class Application {
         Scanner sc = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = sc.nextLine();
-        System.out.println(carName);
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int count = sc.nextInt();
+        System.out.println(count);
 
     }
 }
