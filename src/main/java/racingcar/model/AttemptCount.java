9d001dd416ade76494bfbb9d926eff1aa68a86af
ryan-dia <cjfdnjs158@naver.com>
2024-10-28 18:37:02 +0900

    refactor: count 유효성 검사를 먼저한 뒤 인스턴스 변수에 할당

--- src/main/java/racingcar/model/AttemptCount.java
+++ src/main/java/racingcar/model/AttemptCount.java
@@ -1,15 +1,14 @@
 package racingcar.model;
 
 public class AttemptCount {
-
     public static final int MIN_ATTEMPT_COUNT = 1;
     public static final int MAX_ATTEMPT_COUNT = 10;
 
     private final int racingAttemptCount;
 
     public AttemptCount(int count) {
+        validateValidCount(count);
         this.racingAttemptCount = count;
-        validateValidCount();
     }
 
     public static AttemptCount fromString(String count) {
@@ -24,8 +23,8 @@ public class AttemptCount {
         }
     }
 
-    private void validateValidCount() {
-        if (racingAttemptCount < MIN_ATTEMPT_COUNT || racingAttemptCount > MAX_ATTEMPT_COUNT) {
+    private void validateValidCount(int count) {
+        if (count < MIN_ATTEMPT_COUNT || count > MAX_ATTEMPT_COUNT) {
             throw new IllegalArgumentException("시도 횟수는 " + MIN_ATTEMPT_COUNT + " ~ " + MAX_ATTEMPT_COUNT + "사이여야합니다.");
         }
     }
