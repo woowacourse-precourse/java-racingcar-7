01993d105c2258aa146f23597445b77ffeaeaf3f
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-10-28 14:40:45 +0900

    feat: 자동차 이름 출력 & 이동 횟수 출력 기능 구현
    - 자동차 이름 출력하는 기능을 구현했습니다.
    - 자동차 이동 횟수 출력하는 기능을 구현했습니다.

--- src/main/java/racingcar/io/Output.java
+++ src/main/java/racingcar/io/Output.java
@@ -7,12 +7,14 @@ public class Output {
         System.out.println("실행 결과");
     }
 
-    public void printCar(/*String car*/) {
+    public void printCar(String car) {
         // 자동차 이름 출력
+        System.out.print(car + " : ");
     }
 
     public void printMoveByOrder() {
         // 차수별 실행 결과 출력
+        System.out.print("-");
     }
 
     public void printChampion(List<String> champions) {
