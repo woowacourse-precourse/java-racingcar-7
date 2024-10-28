9c8240aafc50f073cf0d6d682022a383df58f6b4
DongHoon Lee <dhl1924@naver.com>
2024-10-28 15:59:49 +0900

    feat(domain): 자동차 이름을 VO로 구현한 Name 클래스 추가
    - Name 클래스를 값 객체(VO)로 구현하여 자동차 이름의 불변성과 유효성 검사 수행
    - 최대 길이 제한(5자) 및 빈 문자열 검사를 통해 자동차 이름의 유효성을 보장

---
+++ src/main/java/racingcar/domain/Name.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+import racingcar.exception.ErrorMessage;
+import racingcar.exception.RacingCarGameException;
+
+public record Name(
+        String carName
+) {
+    private static final int MAX_NAME_LENGTH = 5;
+
+    public Name {
+        validateCarNameNotEmpty(carName);
+        validateCarNameLength(carName);
+    }
+
+    private static void validateCarNameNotEmpty(String carName) {
+        if (carName.trim().isEmpty()) {
+            throw RacingCarGameException.from(ErrorMessage.EMPTY_CAR_NAME_ERROR);
+        }
+    }
+
+    private static void validateCarNameLength(String carName) {
+        if (carName.length() > MAX_NAME_LENGTH) {
+            throw RacingCarGameException.from(ErrorMessage.NAME_TOO_LONG);
+        }
+    }
+}
--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -3,7 +3,9 @@ package racingcar.exception;
 public enum ErrorMessage {
     NULL_INPUT_ERROR("null은 입력할 수 없습니다."),
     EMPTY_INPUT_ERROR("빈 문자열은 입력할 수 없습니다."),
-    TRY_COUNT_INVALID("시도할 횟수는 최소 1 이상의 양의 정수이어야 합니다.");
+    TRY_COUNT_INVALID("시도할 횟수는 최소 1 이상의 양의 정수이어야 합니다."),
+    EMPTY_CAR_NAME_ERROR("자동차 이름은 빈 문자열일 수 없습니다."),
+    NAME_TOO_LONG("자동차 이름은 5자 이하이어야 합니다.");
 
     private final String message;
 
