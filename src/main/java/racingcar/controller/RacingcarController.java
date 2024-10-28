ed8a280b457d153a42d571e280aa36cb89b32b41
ryan-dia <cjfdnjs158@naver.com>
2024-10-28 16:51:12 +0900

    refactor: outputPrintMessage를 상수화

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -10,10 +10,10 @@ import racingcar.view.OutputView;
 public class RacingcarController {
 
     public void run() {
-        OutputView.printMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        OutputView.printInputCarNamesMessage();
         Cars cars = new Cars(InputView.readInput(), new RandomMoveStrategy());
 
-        OutputView.printMessage("시도할 횟수는 몇 회인가요?");
+        OutputView.printInputAttemptCountMessage();
         AttemptCount attemptCount = AttemptCount.fromString(InputView.readInput());
 
         RacingGame racingGame = new RacingGame(cars, attemptCount);
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,11 +4,21 @@ import java.util.List;
 import racingcar.model.CarSnapshot;
 
 public class OutputView {
+    private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ATTEMPT_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
     private OutputView() {
     }
 
-    public static void printMessage(String message) {
+    public static void printInputCarNamesMessage() {
+        printMessage(INPUT_CAR_NAMES_MESSAGE);
+    }
+
+    public static void printInputAttemptCountMessage() {
+        printMessage(INPUT_ATTEMPT_COUNT_MESSAGE);
+    }
+
+    private static void printMessage(String message) {
         System.out.println(message);
     }
 
@@ -20,13 +30,13 @@ public class OutputView {
     private static void printRoundResults(List<List<CarSnapshot>> roundResults) {
         for (List<CarSnapshot> roundResult : roundResults) {
             for (CarSnapshot car : roundResult) {
-                System.out.println(car.carName() + " : " + "-".repeat(car.position()));
+                printMessage(car.carName() + " : " + "-".repeat(car.position()));
             }
             System.out.println();
         }
     }
 
     private static void printWinners(List<String> winners) {
-        System.out.println("최종 우승자 : " + String.join(", ", winners));
+        printMessage("최종 우승자 : " + String.join(", ", winners));
     }
 }
