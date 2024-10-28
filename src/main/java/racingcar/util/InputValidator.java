94526ba2649d87f9f9a8ef587c5e1b021c18dc5b
BusanGukbap <dong2857210@gmail.com>
2024-10-28 18:22:07 +0900

    feat($Util) : add splitCarNames

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -8,8 +8,20 @@ public class InputValidator {
     private static final String NAME_DELIMITER = ",";
     private static final int MAX_NAME_LENGTH = 5;
 
-    public static List<String> splitCarNames() {
+    public static List<String> splitCarNames(String carNamesInput) {
+        String[] namesArray = carNamesInput.split(NAME_DELIMITER);
+        List<String> names = new ArrayList<>();
 
+        for (String name : namesArray) {
+            String seperatedName = name.trim();
+            validateCarName(seperatedName);
+            names.add(seperatedName);
+        }
+
+        if (names.isEmpty()) {
+            throw new IllegalArgumentException("[error] 자동차 이름을 입력하세요");
+        }
+        return names;
     }
 
     private static void validateCarName(String name) {
