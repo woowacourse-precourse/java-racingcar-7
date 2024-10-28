fc9532fb271feb0fd8e96c2effe5fbb7b5824a9a
JoonKyo <joonkyolee@gmail.com>
2024-10-28 12:12:39 +0900

    test(CarTest): 랜덤 숫자가 4 이상일 때 이동했는지 확인하는 테스트 구현

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -17,7 +17,19 @@ public class CarTest {
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3})
-    void 랜덤_숫자가_0에서_3까지_일때_이동_가능한지_테스트(int number) {
+    void 랜덤_숫자가_0에서_3까지_일때_이동_상황_테스트(int number) {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThat(car.getPosition()).isEqualTo(0);
+                    assertThat(car.getMoveStatus()).isEqualTo("");
+                },
+                number
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void 랜덤_숫자가_4이상_일때_이동_상황_테스트(int number) {
         assertRandomNumberInRangeTest(
                 () -> {
                     assertThat(car.getPosition()).isEqualTo(0);
