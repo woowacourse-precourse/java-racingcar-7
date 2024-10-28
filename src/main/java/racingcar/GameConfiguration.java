a7ce36247d9664efc27040bca85c1b9556cd53e9
haennni <ggowater33@kangnam.ac.kr>
2024-10-28 16:07:27 +0900

    refactor(configuration): 입력 핸들러와 파서 이용해 생성자 리팩토링
    - GameConfiguration 클래스의 생성자를 리팩토링하여 입력 핸들러와 파서를 사용해 자동차 이름과 시도 횟수를 초기화하도록 수정했습니다.

--- src/main/java/racingcar/GameConfiguration.java
+++ src/main/java/racingcar/GameConfiguration.java
@@ -1,2 +1,25 @@
-package racingcar;public class GameConfiguration {
+package racingcar;
+
+import racingcar.domain.Car;
+
+import java.util.List;
+
+public class GameConfiguration {
+    private String[] carsNames;
+    private int attemptCount;
+
+    public GameConfiguration(InputHandler inputHandler, InputParser inputParser) {
+        String carNames = inputHandler.inputCarNames();
+        this.carsNames = inputParser.parser(carNames);
+        this.attemptCount = inputHandler.inputGameCount();
+    }
+
+    public String[] carNames() {
+        return carsNames;
+    }
+
+    public int attemptCount() {
+        return attemptCount;
+    }
+
 }
