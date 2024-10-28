7387102198fe557385519ed7a72282dd30a2f0c9
IM-GYURI <kgulr0517@gmail.com>
2024-10-28 11:44:22 +0900

    refactor : add ROUND_NUMBER_STANDARD
    - ROUND_NUMBER_STANDARD 상수화 : 1
    - 시도 횟수의 최소 기준

--- src/main/java/racingcar/model/RoundNumberValidator.java
+++ src/main/java/racingcar/model/RoundNumberValidator.java
@@ -2,8 +2,10 @@ package racingcar.model;
 
 public class RoundNumberValidator {
 
+    private static final int ROUND_NUMBER_STANDARD = 1;
+
     public void validateRoundNumber(int roundNumber) {
-        if (roundNumber <= 0) {
+        if (roundNumber < ROUND_NUMBER_STANDARD) {
             throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
         }
     }
