888286b02e16096a69eb49341b1481a0d41967ab
AhnJoonSung <tjdtna01@naver.com>
2024-10-28 13:21:56 +0900

    Feature: Car randomMove 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,13 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
+    private int position;
 
     public Car(String name) {
         this.name = name;
+        this.position = 0;
     }
 
     public void randomMove() {
+        int rand = Randoms.pickNumberInRange(0, 9);
 
+        if (rand >= 4)
+            position++;
     }
 }
