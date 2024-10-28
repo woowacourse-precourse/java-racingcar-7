39e8016309057fb888088c10476fa0e1008ae57b
jody816 <81346079+jody816@users.noreply.github.com>
2024-10-28 18:40:36 +0900

    test: 테스트 메서드 목록 추가

---
+++ src/test/java/racingcar/application/service/RacingServiceTest.java
@@ -0,0 +1,20 @@
+package racingcar.application.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RacingServiceTest {
+
+  @Test
+  void setup() {
+  }
+
+  @Test
+  void playRound() {
+  }
+
+  @Test
+  void getWinners() {
+  }
+}
