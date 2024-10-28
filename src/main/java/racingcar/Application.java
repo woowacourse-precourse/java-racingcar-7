61777d8fb1f66b033c5921ea8d8ffbb2846587c6
baesh <baesh6778@gmail.com>
2024-10-28 20:18:56 +0900

    feat: 각 자동차에는 최대 5자의 이름이 할당

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String carNamesInput = Console.readLine();
+            String[] carNames = validateCarNames(carNamesInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
+
+    static String[] validateCarNames(String carNamesInput) {
+        String[] carNames = carNamesInput.split(",");
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
+        return carNames;
+    }
+    
 }
