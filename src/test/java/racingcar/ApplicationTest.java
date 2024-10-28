6854465d1cb44c04f793a2b740aa17bb6545f592
heeyeop Kim <ssucgoo@gmail.com>
2024-10-28 22:12:58 +0900

    test: move 함수 테스트 코드 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -29,6 +31,24 @@ class ApplicationTest extends NsTest {
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void move는_4_미만의_숫자에선_전진하지_않는다(int randomNumber) {
+        Car car = new Car("david");
+        car.move(randomNumber);
+
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void move는_4_이상의_숫자에선_전진한다(int randomNumber) {
+        Car car = new Car("david");
+        car.move(randomNumber);
+
+        assertThat(car.getPosition()).isEqualTo(1);
     }
 
     @Override
