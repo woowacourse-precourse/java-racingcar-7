6c04c8cd35a34191a7d9862d9eb5f319ad44ba82
SSU <hshwan0406@gmail.com>
2024-10-28 22:01:46 +0900

    feat: Car move 메서드 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private String name;
+    private int moveCount;
 
     public Car(String name) {
         this.name = name;
@@ -9,5 +12,17 @@ public class Car {
 
     public String getName() {
         return name;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+
+    public void move() {
+        int move = Randoms.pickNumberInRange(0, 9);
+
+        if(move >= 4) {
+            moveCount += 1;
+        }
     }
 }
