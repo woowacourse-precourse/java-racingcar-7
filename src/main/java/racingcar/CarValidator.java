f9b3ccec9250fdf0377d42c05ce7d10c740361da
Hyoseop Song <crad_on25@naver.com>
2024-10-28 22:28:39 +0900

    refactor: 자동차 이름 길이 제한 상수화 정의

--- src/main/java/racingcar/CarValidator.java
+++ src/main/java/racingcar/CarValidator.java
@@ -1,10 +1,10 @@
 package racingcar;
 
 public class CarValidator {
-	private static final String DELIMITER = ",";
+	private static final int CAR_NAME_LIMIT = 5;
 
 	public String validateCarNameLimit(String carName) {
-		if (carName.length() > 5) {
+		if (carName.length() > CAR_NAME_LIMIT) {
 			throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
 		}
 		return carName;
