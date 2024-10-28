dc0e8bb3081b47ee42e63be7cd6d4f12b241753f
HongSeongJo <164273757+HongSeongJo@users.noreply.github.com>
2024-10-28 10:25:23 +0900

    fix(separator): 이름이 5자를 넘길 때 예외가 발생하도록 수정

--- src/main/java/racingcar/Separator.java
+++ src/main/java/racingcar/Separator.java
@@ -1,14 +1,23 @@
 package racingcar;
 
 public class Separator {
+    private static final String TOKEN = ",";
+
     public String[] separate(String inputNames) {
-        String tokenizer = ",";
-        String[] names = inputNames.split(tokenizer);
+        String[] names = inputNames.split(TOKEN);
 
         for (int i = 0; i < names.length; i++) {
-            names[i] = names[i].trim();
+            names[i] = checkNameLength(names[i].trim());
         }
 
         return names;
+    }
+
+    private String checkNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자를 넘길 수 없습니다");
+        }
+
+        return name;
     }
 }
