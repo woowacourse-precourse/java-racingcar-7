ea947e840ddeb42d746b0c8e4e7ae04b1c12a845
youngkwanglim <richard08@naver.com>
2024-10-28 12:04:08 +0900

    feat: 자동차 이름이 5자 초과일 경우 예외 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -28,6 +28,9 @@ public class Application {
         }
 
         for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
             cars.add(new Car(carName));
         }
         return cars;
