04b188e84aa08f1d6ddaa5c22b6dcb2060c38611
HyejinYoo <yyyhj0114@gmail.com>
2024-10-28 19:00:05 +0900

    refactor: apply Printer in Application class

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -12,7 +12,7 @@ public class Application {
             System.out.println("시도할 횟수는 몇 회인가요?");
             int rounds = Integer.parseInt(Console.readLine());
 
-            Race race = new Race(Arrays.asList(carNames), new DefaultMoveStrategy());
+            Race race = new Race(Arrays.asList(carNames), new DefaultMoveStrategy(), new RacePrinter());
             race.play(rounds);
         } catch (IllegalArgumentException e) {
             throw e;
