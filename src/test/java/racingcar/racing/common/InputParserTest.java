80b6e2b24b91c5bdb3cf2502083f64aeccb08602
ppparkta <pppppark2@gmail.com>
2024-10-28 19:22:30 +0900

    test(InputParser): 레이싱카 이름 , 기준 파싱 테스트

--- src/test/java/racingcar/racing/common/InputParserTest.java
+++ src/test/java/racingcar/racing/common/InputParserTest.java
@@ -1,19 +1,40 @@
 package racingcar.racing.common;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.Assertions;
 import java.util.Arrays;
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class InputParserTest {
     @Test
-    void 레이싱카_파싱_성공() {
-        //given
-        String input = "hello,hi,ho,,";
-        List<String> array = Arrays.asList("hello", "hi", "ho");
-        //when
-        //then
-        Assertions.assertThat(InputParser.parseCarNames(input)).isEqualTo(array);
+    void 레이싱카_기본_파싱_성공() {
+        Assertions.assertSimpleTest(() -> {
+            //given
+            String input = "hello,hi,ho,,";
+
+            //when
+            List<String> parsingNames = InputParser.parseCarNames(input);
+
+            //then
+            assertThat(parsingNames).isEqualTo(Arrays.asList("hello", "hi", "ho"));
+        });
+    }
+
+    @Test
+    void 레이싱카_빈콤마_무시_파싱_성공() {
+        Assertions.assertSimpleTest(() -> {
+            //given
+            String input = ",,,";
+
+            //when
+            List<String> parsingNames = InputParser.parseCarNames(input);
+
+            //then
+            assertThat(parsingNames).isEqualTo(Arrays.asList());
+        });
     }
 
 }
