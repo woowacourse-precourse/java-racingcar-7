ecd3c6a574105087735f3d6ee06ee95d8be012da
apple-mint <174938449+apple-mint@users.noreply.github.com>
2024-10-28 18:34:21 +0900

    style: 불필요한 내용 삭제 및 공백 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -12,29 +12,20 @@ import java.util.*;
 public class Application {
     public static void main(String[] args) {
 
-        // 1. 문자열 입력받기
         String words = InputView.inputWords();
 
-        // 2. 문자열이 공백 또는 쉼표로 시작하거나 끝나는 경우,
-        // 또는 하나의 자동차만 있어 자동차 경주 게임을 하는 의미가 없는 경우
-        // IllegalArgumentException를 발생시킴
         if (!WordsController.isValid(words) | !WordsController.moreThanOne(words)) {
             throw new IllegalArgumentException();
         }
 
-        // 3. 쉼표를 기준으로 경주할 자동차 이름을 분리
         List<String> carNames = List.of(words.split(","));
 
-        // 4. 자동차 이름이 공백으로 시작하거나 끝나는 경우,
-        // 또는 한글 또는 영어대소문자 또는 숫자로 이루어진 1 이상 5 이하의 글자가 아닐 경우
-        // IllegalArgumentException를 발생시킴
         if (!CarNamesController.isValid(carNames) | !CarNamesController.checkConditions(carNames)) {
             throw new IllegalArgumentException();
         }
 
-        // 5. 자동차 이름을 key, 전진한 거리를 value로 가지는 맵을 만들고
-        // key는 중복을 허용하지 않는다는 특성을 활용해 같은 이름이 2개 이상 존재하는지 확인
         HashMap<String, Integer> carPositions = new HashMap<>();
+
         for (String carName : carNames) {
             if (CarNamesController.checkDuplication(carName, carPositions)) {
                 throw new IllegalArgumentException();
@@ -43,25 +34,21 @@ public class Application {
             }
         }
 
-        // 6. 시도할 횟수 입력받기
         String number = InputView.inputTryNumbers();
 
-        // 7. 시도할 횟수가 자연수로 변환 가능한지 확인 후 변환
         if (!NumberController.isInteger(number) | !NumberController.isNaturalNumber(number)) {
             throw new IllegalArgumentException();
         }
+
         int tryNumber = Integer.parseInt(number);
 
-        // 8. 시도할 횟수만큼 게임을 진행하며 차수별 실행 결과 출력
         for (int round = 0; round < tryNumber; round++) {
             RacingController.racing(carNames, carPositions);
             OutputView.progressResults(round, carNames, carPositions);
         }
 
-        // 9. 가장 멀리 간 거리를 측정해 최종 우승자 선발
         List<String> winners = RacingController.findWinners(carNames, carPositions);
 
-        // 10. 최종 우승자 출력
         OutputView.finalResult(winners);
     }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class OutputView {
     public static void progressResults(int round, List<String> carNames, HashMap<String, Integer> carPositions) {
-
         if (round == 0) {
             System.out.println("");
             System.out.println("실행 결과");
@@ -14,6 +13,7 @@ public class OutputView {
         for (String carName : carNames) {
             System.out.println(carName + " : " + "-".repeat(carPositions.get(carName)));
         }
+
         System.out.println("");
     }
 
