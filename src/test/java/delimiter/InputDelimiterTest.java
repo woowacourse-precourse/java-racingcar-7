972555da00e43d21db7ac53b68f7d4dcdd6135b4
soonhong99 <nanayah99@naver.com>
2024-10-28 15:27:04 +0900

    test(Input Delimiter): 제공된 예외 상황이 올바르게 동작하는지 테스트하라
    - 주어진 예외상황을 모두 알맞게 예외처리를 하는지 테스트
    
    Signed-off-by: soonhong99 <nanayah99@naver.com>

---
+++ src/test/java/delimiter/InputDelimiterTest.java
@@ -0,0 +1,52 @@
+package delimiter;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.Application;
+
+@DisplayName("구분자 관련 테스트")
+public class InputDelimiterTest extends NsTest{
+    @Test
+    @DisplayName("구분자로 분리된 리스트 인자의 문자열 길이가 6 이상일 경우 예외 발생")
+    void 실패_구분자로분리된하나의문자열_길이가6이상() {
+        // Given
+        String exampleOfStringLengthSix = "nanayah,honi,moni";
+
+        // When&Then
+        assertThatThrownBy(() -> runException(exampleOfStringLengthSix))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5자 이하로 적어주세요.");
+    }
+
+    @Test
+    @DisplayName("구분자로 분리된 리스트 인자의 문자열 길이가 빈문자열일 경우 예외 발생")
+    void 실패_구분자로분리된하나의문자열_길이가0() {
+        // Given
+        String exampleOfStringLengthZero = "honi,,boni";
+
+        // When&Then
+        assertThatThrownBy(() -> runException(exampleOfStringLengthZero))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("comma(\",\")는 자동차 이름 구분자입니다. 자동차 이름 사이에만 하나의 comma가 올 수 있도록 해주세요.");
+    }
+
+    @Test
+    @DisplayName("구분자로 분리된 리스트 인자의 문자열들 중 중복된 문자열이 있을 경우 예외 발생")
+    void 실패_구분자로분리된문자열들_중복된문자열() {
+        // Given
+        String exampleOfStringDuplicate = "honi,boni,honi";
+
+        // When&Then
+        assertThatThrownBy(() -> runException(exampleOfStringDuplicate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 중복될 수 없습니다. 중복되지 않는 자동차 이름을 입력해주세요.");
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
