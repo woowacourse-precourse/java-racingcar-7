672959b7a63851a84a7a55a238632cf8e5d98d87
greensnapback0229 <smdmim@gmail.com>
2024-10-28 21:56:19 +0900

    feat(Round): add scoreBoard field

--- src/main/java/racingcar/domain/Round.java
+++ src/main/java/racingcar/domain/Round.java
@@ -6,12 +6,18 @@ import java.util.List;
 public class Round {
 
     private Integer id;
-    private List<Car> carList = new ArrayList<>();
-    private int thisRound;
+    private List<Car> carList= new ArrayList<>();
+    private final int thisRound;
     private int gameId;
+    private List<ScoreEntry> scoreBoard = new ArrayList<>();
 
     private Round(Builder builder) {
         this.thisRound = builder.thisRound;
+        this.gameId = builder.gameId;
+        if(builder.carList != null) {
+            this.carList = builder.carList;
+        }
+
     }
 
     public Integer getId() {
@@ -22,12 +28,20 @@ public class Round {
         return carList;
     }
 
-    public int getThisRound() {
-        return thisRound;
+    public void addScoreEntry(ScoreEntry scoreEntry) {
+        this.scoreBoard.add(scoreEntry);
+    }
+
+    public List<ScoreEntry> getScoreBoard(){
+        return scoreBoard;
     }
 
     public Integer getGameId() {
         return gameId;
+    }
+
+    public Integer getThisRound() {
+        return thisRound;
     }
 
     public void setId(int id) {
@@ -47,8 +61,22 @@ public class Round {
     public static class Builder {
         private int thisRound;
 
+        private Integer gameId;
+
+        private List<Car> carList;
+
         public Builder thisRound(int thisRound) {
             this.thisRound = thisRound;
+            return this;
+        }
+
+        public Builder carList(List<Car> carList){
+            this.carList = carList;
+            return this;
+        }
+
+        public Builder gameId(Integer gameId){
+            this.gameId = gameId;
             return this;
         }
 
