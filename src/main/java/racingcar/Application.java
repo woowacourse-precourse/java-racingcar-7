fbd62fea052799b587b9d4f431aa604c3e3bd2be
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-10-28 21:28:19 +0900

    refactor: main 함수에서 View, Service 이외의 것들은 의존성 주입 하지 않도록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,13 @@
 package racingcar;
 
 import racingcar.controller.RacingCarGame;
-import racingcar.domain.Number;
-import racingcar.domain.Cars;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
+import racingcar.service.GameService;
+import racingcar.view.ConsoleInputView;
+import racingcar.view.ConsoleOutputView;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        Number number = new Number();
-        Cars cars = new Cars();
-        RacingCarGame racingCarGame = new RacingCarGame(inputView, outputView, number, cars);
-
-        racingCarGame.initGame();
-        racingCarGame.startGame();
+        RacingCarGame racingCarGame = new RacingCarGame(new ConsoleInputView(), new ConsoleOutputView(), new GameService());
+        racingCarGame.run();
     }
 }
