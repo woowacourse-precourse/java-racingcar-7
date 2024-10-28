5a8a70645d97a5bae9c86652bcd84366276d289e
조윤성 <yunsung1206@gmail.com>
2024-10-28 21:38:16 +0900

    test: 자동차 이름이 중간에 비어있을 때 실패하는 테스트 추가

--- src/test/java/racingcar/model/CarListTest.java
+++ src/test/java/racingcar/model/CarListTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarListTest {
     @Test
@@ -15,6 +17,13 @@ public class CarListTest {
     @Test
     void 자동차_이름이_중복일_때_실패한다() {
         assertThatThrownBy(() -> new CarList("pobi,woni,pobi"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {",woni,jun", "pobi,,jun", "pobi,woni,"})
+    void 자동차_이름이_중간에_비어있을_때_실패한다(String carNames) {
+        assertThatThrownBy(() -> new CarList(carNames))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
