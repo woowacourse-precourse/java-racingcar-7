3281d347d675bc37b922de52fcc429bfad19e791
HIDE_MAIN <ra___be@naver.com>
2024-10-28 22:16:58 +0900

    feat: 컨트롤러 선언, 자동차 리스트 추가, 이름 유효성검사

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,27 @@
+package racingcar.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.view.RacingView;
+import racingcar.service.RacingService;
+
+public class RacingController {
+    private List<Car> cars = new ArrayList<>();
+
+    public void setCarList(String names) {
+        for (String name : names.split(",")) {
+            if (isValidName(name)) {
+                cars.add(new Car(name));
+            }
+        }
+    }
+
+    public boolean isValidName(String name) {
+        if (name.isEmpty() || name.length() > 5) {
+            throw new IllegalArgumentException("잘못된 자동차 이름 입니다.");
+        }
+        return true;
+    }
+}
