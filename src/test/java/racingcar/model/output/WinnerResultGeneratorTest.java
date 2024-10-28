2e3cb0ba9c99b6ecb62a8101fff5ea3fa7c9ddba
Chan531 <chan657312@gmail.com>
2024-10-28 17:52:27 +0900

    test(WinnerResultGeneratorTest): 다수 우승자 결과 문자열 생성 테스트

--- src/test/java/racingcar/model/output/WinnerResultGeneratorTest.java
+++ src/test/java/racingcar/model/output/WinnerResultGeneratorTest.java
@@ -22,4 +22,21 @@ class WinnerResultGeneratorTest {
 		// then
 		assertEquals(winnerName, winnerResult);
 	}
+
+	@Test
+	@DisplayName("다수 우승자일 때, 결과 문자열을 생성할 수 있다.")
+	void 다수_우승자가_담긴_결과_문자열을_생성한다() {
+		// given
+		String winnerName1 = "winner1";
+		String winnerName2 = "winner2";
+		List<String> winner = List.of(winnerName1, winnerName2);
+		String separator = ", ";
+		String expectedResult = winnerName1 + separator + winnerName2;
+
+		// when
+		String winnerResult = WinnerResultGenerator.generateWinnerResult(winner);
+
+		// then
+		assertEquals(expectedResult, winnerResult);
+	}
 }
