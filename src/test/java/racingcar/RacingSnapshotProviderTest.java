15b3e5548df1f70efe8ecc85f7e93be6f465f162
kim-se-hee <saree98@naver.com>
2024-10-28 17:02:25 +0900

    test(RacingSnapshotProviderTest): 스냅샷 반환 로직에 대한 단위 테스트 추가

--- src/test/java/racingcar/RacingSnapshotProviderTest.java
+++ src/test/java/racingcar/RacingSnapshotProviderTest.java
@@ -1,4 +1,25 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class RacingSnapshotProviderTest {
-  
+    private static RacingSnapshotProvider snapshotProvider = new RacingSnapshotProvider();
+
+    @Test
+    public void 거리가_0인_선수() throws Exception{
+        Player player = new Player("a");
+        Assertions.assertThat(snapshotProvider.getSnapshotOf(player)).isEqualTo(player.getName() +  " : ");
+    }
+
+    @Test
+    public void 거리가_0이_아닌_선수() throws Exception{
+        Player player = new Player("a");
+        player.move();
+        player.move();
+        player.move();
+        Assertions.assertThat(snapshotProvider.getSnapshotOf(player)).isEqualTo(player.getName() +  " : ---");
+    }
 }
