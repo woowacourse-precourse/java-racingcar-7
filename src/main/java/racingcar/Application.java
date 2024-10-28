f4142a70cc03ed98bbf8e9f35452ce85b0a67e4f
Grayson1999 <bbt1250912@gmail.com>
2024-10-28 15:55:00 +0900

    feat: 자동차 이름을 쉼표로 구분하여 입력받는 기능 구현
    사용자가 작성한 이름을 쉼표(,)를 구분하는 기능을 추가했습니다.
    추가로, 이름의 길이가 5자를 초과하는 경우 IllegalArgumentException을 발생시킵니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    //쉼표가 아닌 구분자 예외처리
+    //공백 처리
+    public static List<String> splitCarNames(String userInputNames) throws IllegalAccessError {
+        List<String> carNames = Arrays.asList(userInputNames.split(","));
+        for (String carName : carNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException(carName + "은 5글자가 넘습니다.");
+            }
+        }
+        return carNames;
     }
 }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,6 +3,9 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,19 +18,37 @@ class ApplicationTest extends NsTest {
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
+    }
+
+    @Test
+    void 자동차_입력_테스트() {
+        // Given: 테스트할 입력값 설정
+        String input = "pobi,woni,jun";
+        String inputLongName = "pobia, woni fang, junsubae";
+
+        // When: getCarNames() 메서드 호출
+        List<String> carNames = Application.splitCarNames(input);
+
+        // Then: 결과 검증
+        List<String> expectedNames = Arrays.asList("pobi", "woni", "jun");
+        assertThat(carNames).isEqualTo(expectedNames);
+
+        //예외 검증
+        assertThatThrownBy(() -> Application.splitCarNames(inputLongName))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
