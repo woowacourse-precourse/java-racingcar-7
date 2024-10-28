8a34aa2e18cbffe74982745d95680072e2f7bd82
BusanGukbap <dong2857210@gmail.com>
2024-10-28 22:18:14 +0900

    style($model) : 필요없어진 변수들 삭제

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,13 +1,6 @@
 package racingcar.model;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 public class Car {
-
-    private static final int MOVE_THRESHOLD = 4;
-    private static final int RANDOM_START_POINT = 0;
-    private static final int RANDOM_END_POINT = 9;
-    private static final int MAX_NAME_LENGTH = 5;
 
     private final String name;
     private int position;
