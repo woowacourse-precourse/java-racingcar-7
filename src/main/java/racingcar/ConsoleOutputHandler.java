fb4093f7698d93d6b84e2bd48c0e447ae2684656
kim-se-hee <saree98@naver.com>
2024-10-28 16:52:37 +0900

    feat(ConsoleOutputHandler): 출력 클래스 구현
    ConsoleOutputHandler에 출력 관련 로직 추가
    - 경주 진행을 위한 안내문 출력
    - 각 스테이지 마다 진행 상황 출력
    - 최종 우승자 출력

--- src/main/java/racingcar/ConsoleOutputHandler.java
+++ src/main/java/racingcar/ConsoleOutputHandler.java
@@ -1,15 +1,49 @@
 package racingcar;
 
-public class ConsoleInputHandler {
-    public static void showStartComment(){
+import java.util.List;
+
+public class ConsoleOutputHandler {
+    private static final RacingSnapshotProvider snapshotProvider = new RacingSnapshotProvider();
+
+    public void showStartComment() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public static void showCommentForChoosingNumberOfPlayer(){
+    public void showCommentForChoosingStageSize() {
         System.out.println("시도할 횟수는 몇 회인가요?");
     }
 
-    public static void showRacingSnapshot(){
+    public void showSnapshotOfCurrentStage(List<Player> players) {
+
+        players.forEach(
+                player -> {
+                    System.out.println(snapshotProvider.getSnapshotOf(player));
+                }
+        );
+
+        System.out.println();
+    }
+
+    public void showCommentForResult() {
+        System.out.println("실행 결과");
+    }
+
+    public void showWinners(List<Player> winners) {
+
+        String result = "최종 우승자 : ";
+
+        for (Player winner : winners) {
+            result = result + winner.getName() + ",";
+        }
+
+        System.out.println(trimResult(result));
+
+    }
+
+    private static String trimResult(String result) {
+
+        result = result.trim();
+        return result.substring(0, result.length() - 1);
 
     }
 }
