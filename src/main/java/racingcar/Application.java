d680e6f468c6741efd3ffe56374e015f27fdccdb
sobaman <sobaman522@gmail.com>
2024-10-28 23:50:29 +0900

    feat: 실행 영역인 Application 구현
    - AppConfig를 통해 RacingCarLauncher 인스턴스를 생성하고, 레이싱 시작 메서드를 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.config.AppConfig;
+import racingcar.manager.RacingManager;
+
 public class Application {
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        AppConfig appConfig = new AppConfig();
+        RacingCarLauncher racingCarLauncher = appConfig.racingCarLauncher();
+        racingCarLauncher.start();
     }
 }
