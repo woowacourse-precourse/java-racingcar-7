935a99078522a4796bf9682590767cb70c09e979
sjchae <soojch12@gmail.com>
2024-10-28 22:49:43 +0900

    Feat: 오류 처리 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,15 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String carNames = InputView.getCarNames();
-        int tryCount = InputView.getTryCount();
+        try {
+            List<Car> cars = InputView.getCarNames();
+            int tryCount = InputView.getTryCount();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -2,10 +2,17 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
-    public static String getCarNames() {
+    public static List<Car> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+        String carNames = Console.readLine();
+        return Arrays.stream(carNames.split(","))
+                .map(Car::new)
+                .collect(Collectors.toList());
     }
 
     public static int getTryCount() {
