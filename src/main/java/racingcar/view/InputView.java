4f9522fe15d0fe21df128f3291dbfa2c62856502
SongMinhyuk <dnfldpden32@gmail.com>
2024-10-28 15:04:17 +0900

    refactor: 검증함수 validator클래스로 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,12 @@
 package racingcar.view;
 
 
+import static racingcar.validator.Validator.checkBlank;
+import static racingcar.validator.Validator.checkDuplicate;
+import static racingcar.validator.Validator.checkMaxLength;
+import static racingcar.validator.Validator.checkOnlyRest;
+import static racingcar.validator.Validator.checkPositive;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.util.HashSet;
 import java.util.List;
@@ -9,16 +15,13 @@ import java.util.Set;
 public class InputView {
 
     private static final int MAX_PLAYER_NAME_LENGTH = 5;
-
     public List<String> readPlayerNameList() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String userInput=Console.readLine();
 
         checkBlank(userInput);
         List<String> playerNames= List.of(userInput.split(","));
-        checkOnlyRest(playerNames);
-        checkMaxLength(playerNames);
-        checkDuplicate(playerNames);
+        validate(playerNames);
         return playerNames;
     }
 
@@ -32,45 +35,17 @@ public class InputView {
         return turn;
     }
 
-    private void checkPositive(int turn) {
-        if(turn <=0){
-            throw new IllegalArgumentException("0이하의 값은 허용되지 않습니다.");
-        }
-    }
-
     private int parseInt(String userInput) {
         try {
             return Integer.parseInt(userInput);
         }catch (NumberFormatException e){
-            throw new IllegalArgumentException("숫자이외의 값은 허용되지 않습니다.");
+            throw new IllegalArgumentException("숫자 이외의 값은 허용되지 않습니다.");
         }
     }
 
-
-    private void checkDuplicate(List<String> playerNames) {
-        Set<String> uniquePlayerNames=new HashSet<>(playerNames);
-        if(uniquePlayerNames.size()!=playerNames.size()){
-            throw new IllegalArgumentException("중복된 이름은 허용하지 않습니다.");
-        }
-    }
-
-    private void checkMaxLength(List<String> playerNames) {
-        for (String playerName: playerNames){
-            if(playerName.length()>MAX_PLAYER_NAME_LENGTH){
-                throw new IllegalArgumentException("5글자 이하만 가능합니다");
-            }
-        }
-    }
-
-    private void checkOnlyRest(List<String> playerNames) {
-        if(playerNames.isEmpty()){
-            throw new IllegalArgumentException("플레이어 이름이 입력되지 않았습니다");
-        }
-    }
-
-    private void checkBlank(String userInput) {
-        if(userInput.isBlank()) {
-            throw new IllegalArgumentException("플레이어 이름이 입력되지 않았습니다");
-        }
+    private void validate(List<String> playerNames){
+        checkOnlyRest(playerNames);
+        checkMaxLength(playerNames);
+        checkDuplicate(playerNames);
     }
 }
