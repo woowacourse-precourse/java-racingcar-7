5dbc483ff0c379648b52eb318e6f5542b99f1696
jmlee <p2achtrii@gmail.com>
2024-10-28 22:34:16 +0900

    feat: RaceOrNot function

--- src/main/java/racingcar/RandomRaceOrNot.java
+++ src/main/java/racingcar/RandomRaceOrNot.java
@@ -6,17 +6,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class RandomNumber {
+public class RandomRaceOrNot {
     public static void main(String[] args) {
         InputCarName CarNameInput = new InputCarName();
         String[] CarList = CarNameInput.getCarList();
-
         int carNum = CarList.length;
-        List<Integer> raceAmount = new ArrayList<>();
 
+        List<StringBuilder> progressList = new ArrayList<>();
+        for (int i = 0; i < carNum; i++) {
+            progressList.add(new StringBuilder());
+        }
+        List<Integer> raceAmount = new ArrayList<>();
         for (int i = 0; i < carNum; i++) {
             raceAmount.add(i, Randoms.pickNumberInRange(0, 9));
+
+            if (raceAmount.get(i) >= 4){
+                progressList.get(i).append("-");
+            }
         }
+
 
 
     }
