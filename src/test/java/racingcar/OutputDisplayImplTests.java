d757f27e3ee8786b849e1d778e3594fe77ed9de5
choign3123 <choign3123@naver.com>
2024-10-28 17:01:18 +0900

    Test(OutputDisplayImplTests): 우승자 출력에 대한 테스트 코드 작성

---
+++ src/test/java/racingcar/OutputDisplayImplTests.java
@@ -0,0 +1,49 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.car.CarImpl;
+import racingcar.car.ICar;
+import racingcar.display.OutputDisplayImpl;
+
+public class OutputDisplayImplTests {
+
+    private OutputDisplayImpl outputDisplay;
+    private ByteArrayOutputStream outputStream;
+
+    @BeforeEach
+    public void init() {
+        outputDisplay = new OutputDisplayImpl();
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @Test
+    public void printWinner() {
+        List<ICar> carList = new ArrayList<>();
+        carList.add(new CarImpl("gana"));
+
+        outputDisplay.printWinners(carList);
+
+        assertThat(outputStream.toString())
+                .contains("최종 우승자 : gana");
+    }
+
+    @Test
+    public void printWinnerList() {
+        List<ICar> carList = new ArrayList<>();
+        carList.add(new CarImpl("gana"));
+        carList.add(new CarImpl("dara"));
+
+        outputDisplay.printWinners(carList);
+
+        assertThat(outputStream.toString())
+                .contains("최종 우승자 : gana, dara");
+    }
+}
