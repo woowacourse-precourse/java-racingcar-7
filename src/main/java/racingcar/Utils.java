78afab43ae5bcd67b6622b46cd12254163dc93df
making-a-scene <4936joo@naver.com>
2024-10-28 13:49:15 +0900

    style: 불필요한 import문 제거

--- src/main/java/racingcar/Utils.java
+++ src/main/java/racingcar/Utils.java
@@ -3,7 +3,6 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.domain.Car;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
