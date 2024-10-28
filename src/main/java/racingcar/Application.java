db61981b9d50b2291fd524b6dcfad749eaedb217
baesh <baesh6778@gmail.com>
2024-10-28 20:26:50 +0900

    fix: readme 3번째 항목 출력 사항 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,9 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
         try {
@@ -14,9 +17,15 @@ public class Application {
             System.out.println("시도할 횟수는 몇 회인가요?");
             int numberOfAttempts = Integer.parseInt(Console.readLine());
 
+            // 자동차 초기화
+            Map<String, Integer> carPositions = initializeCarPositions(carNames);
+
             // 자동차 이동
+            System.out.println("\n실행 결과");
             for (int i = 0; i < numberOfAttempts; i++) {
-                moveCars(carNames);
+                moveCars(carPositions);
+                printCarPositions(carPositions);
+                System.out.println();
             }
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -33,14 +42,26 @@ public class Application {
         return carNames;
     }
 
-    static void moveCars(String[] carNames) {
-        for (String car : carNames) {
+    static Map<String, Integer> initializeCarPositions(String[] carNames) {
+        Map<String, Integer> carPositions = new HashMap<>();
+        for (String name : carNames) {
+            carPositions.put(name, 0);
+        }
+        return carPositions;
+    }
+
+    static void moveCars(Map<String, Integer> carPositions) {
+        for (String car : carPositions.keySet()) {
             int randomValue = Randoms.pickNumberInRange(0, 9);
             if (randomValue >= 4) {
-                System.out.println(car + "가 전진합니다.");
-            } else {
-                System.out.println(car + "가 멈춥니다.");
+                carPositions.put(car, carPositions.get(car) + 1);
             }
+        }
+    }
+
+    static void printCarPositions(Map<String, Integer> carPositions) {
+        for (Map.Entry<String, Integer> entry : carPositions.entrySet()) {
+            System.out.println(entry.getKey() + " : " + "-".repeat(entry.getValue()));
         }
     }
 }
