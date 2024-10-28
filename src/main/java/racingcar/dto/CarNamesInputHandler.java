a92a2b89e6df2fb3f91c048d30efc5ca0e7f56dc
smyeongL <sangmyeonglee123@gmail.com>
2024-10-28 15:57:06 +0900

    fix(input): remove unused error handling

--- src/main/java/racingcar/dto/CarNamesInputHandler.java
+++ src/main/java/racingcar/dto/CarNamesInputHandler.java
@@ -6,10 +6,6 @@ import java.util.List;
 
 public class CarNamesInputHandler {
     public static void handle(List<String> carNames) {
-        if (carNames == null) {
-            // At least a list is expected
-            throw new IllegalArgumentException();
-        }
         for (String name : carNames) {
             CarNamesValidator.validate(name);
         }
