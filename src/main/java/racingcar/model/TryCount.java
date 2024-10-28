1255d0be3f1175b5b6dc9b5e9e0a273192b7a3d7
DoubleKK <138849238+kwonkeonhyeong@users.noreply.github.com>
2024-10-28 10:48:18 +0900

    refactor(RacingGame): Trycount 객체 내부 상수 처리
    TryCount 객체 내부 하드 코딩, 매직 넘버 상수 처리

--- src/main/java/racingcar/model/TryCount.java
+++ src/main/java/racingcar/model/TryCount.java
@@ -2,6 +2,13 @@ package racingcar.model;
 
 public class TryCount {
 
+    private static final String INVALID_NUMERIC_MESSAGE = "시도 횟수는 숫자만 입력 가능합니다.";
+    private static final String INVALID_EMPTY_MESSAGE = "시도 횟수에 빈 값을 입력할 수 없습니다.";
+    private static final String INVALID_COUNT_RANGE = "입력 가능한 시도 횟수 범위는 1 ~ 10입니다.";
+
+    private static final int MIN_COUNT = 1;
+    private static final int MAX_COUNT = 10;
+
     private final int count;
 
     private TryCount(int count) {
@@ -23,19 +30,19 @@ public class TryCount {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("시도 횟수는 숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException(INVALID_NUMERIC_MESSAGE);
         }
     }
 
     private static void validateEmpty(String input) {
         if (input == null || input.isEmpty()) {
-            throw new IllegalArgumentException("시도 횟수에 빈 값을 입력할 수 없습니다.");
+            throw new IllegalArgumentException(INVALID_EMPTY_MESSAGE);
         }
     }
 
     private void validateRange(int count) {
-        if (count < 1 || count > 10) {
-            throw new IllegalArgumentException("입력 가능한 시도 횟수 범위는 1 ~ 10입니다.");
+        if (count < MIN_COUNT || count > MAX_COUNT) {
+            throw new IllegalArgumentException(INVALID_COUNT_RANGE);
         }
     }
 }
