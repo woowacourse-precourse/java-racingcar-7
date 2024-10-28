3e36b891349651d196536d136a165277e275a1a7
hootzz <wjqwjql3@gmail.com>
2024-10-28 19:55:23 +0900

    feat(validator): 자동차 이름 검증 클래스 구현

--- src/main/java/racingcar/domain/validator/CarNameValidator.java
+++ src/main/java/racingcar/domain/validator/CarNameValidator.java
@@ -1,4 +1,22 @@
 package racingcar.domain.validator;
 
 public class CarNameValidator {
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+
+    public void validate(String name) {
+        validateNull(name);
+        validateLength(name);
+    }
+
+    private void validateNull(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
+        }
+    }
+
+    private void validateLength(String name) {
+        if (name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
+    }
 }
