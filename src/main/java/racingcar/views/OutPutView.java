6b51ffacf942150cbc366b619c035a4c7acbb075
sangxxjin <sj_0368@naver.com>
2024-10-28 16:14:06 +0900

    feat: 실행 결과 메세지 출력 구현
    본문:
    메세지 상수화 했습니다.

---
+++ src/main/java/racingcar/views/OutPutView.java
@@ -0,0 +1,10 @@
+package racingcar.views;
+
+public class OutPutView {
+    private static final String RESULT_MESSAGE = "실행 결과";
+
+    public static void displayResultMessage() {
+        System.out.println(RESULT_MESSAGE);
+    }
+
+}
