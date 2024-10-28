0bed63bc0efee8fc3b649c4b9ffc8f49547a7ef3
minjcho <117441212+minjcho@users.noreply.github.com>
2024-10-28 10:42:59 +0900

    feat: 자동차 이름 입력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,9 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> carNames = InputView.readCarNames();
     }
 }
