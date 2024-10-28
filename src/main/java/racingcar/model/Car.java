a384c4c90da56622ebd67273af853a5defe9b3c4
JeongHyeon Jo <wjdgusdlekd3@yonsei.ac.kr>
2024-10-28 13:33:23 +0900

    refactor(model): Manage responsibilities in Car class with Singleton pattern
    - 싱글턴 패턴을 이용하여 구현
    - 규칙에 어긋난 변수명 수정

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -5,21 +5,20 @@ import racingcar.utils.Engine;
 import racingcar.utils.Validator;
 
 public class Car {
-    private final String CarName;
+    private final String carName;
     private final Engine engine;
     private int position;
-
-    Validator validator = new CarNameValidator();
+    Validator validator = CarNameValidator.getInstance();
 
     public Car(String carName, Engine randomEngine) {
         validator.validation(carName);
-        this.CarName = carName;
+        this.carName = carName;
         this.engine = randomEngine;
         this.position = 0;
     }
 
     public String getCarName() {
-        return CarName;
+        return carName;
     }
 
     public void move() {
