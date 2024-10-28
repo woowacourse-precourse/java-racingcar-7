63c133047c544177597d47fcee32915f19ad953f
plan11plan <tjtfy7028@gmail.com>
2024-10-28 17:02:36 +0900

    fix: Id 자동 생성기 공유 버그 수정
    static 제거, 초깃값 0 세팅

--- src/main/java/racingcar/util/IdGenerator.java
+++ src/main/java/racingcar/util/IdGenerator.java
@@ -3,7 +3,7 @@ package racingcar.util;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class IdGenerator {
-    private static final AtomicLong sequence = new AtomicLong();
+    private final AtomicLong sequence = new AtomicLong(0L);
 
     public Long generate() {
         return sequence.incrementAndGet();
