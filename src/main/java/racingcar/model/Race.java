a6e8cde14b9216a60731693fa50dbd2f81731a68
username0w <grandcias123@gmail.com>
2024-10-28 22:19:58 +0900

    feat: 상수 추가 및 경기 업데이트 기능, 우승자 구하기 기능 추가

--- src/main/java/racingcar/model/Race.java
+++ src/main/java/racingcar/model/Race.java
@@ -1,22 +1,53 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Race {
-
-    private final int totalRounds;
+    private static final int MOVE_DISTANCE = 1;
+    private static final int CAR_MOVE_THRESHOLD = 4;
     private final List<Car> participatingCars;
+    private final int totalRounds;
 
-    public Race(int totalRounds, List<Car> participatingCars) {
-        this.totalRounds = totalRounds;
+    public Race(List<Car> participatingCars, int totalRounds) {
         this.participatingCars = participatingCars;
+        this.totalRounds = totalRounds;
+    }
+
+    public List<Car> getParticipatingCars() {
+        return participatingCars;
     }
 
     public int getTotalRounds() {
         return totalRounds;
     }
 
-    public List<Car> getParticipatingCars() {
+    public List<Car> startEachRound(List<Car> participatingCars) {
+        for (Car car : participatingCars) {
+            updateCarDistance(car);
+        }
         return participatingCars;
+    }
+
+    private void updateCarDistance(Car car) {
+        if (canCarMove()) {
+            car.setDistance(car.getDistance() + MOVE_DISTANCE);
+        }
+    }
+
+    private boolean canCarMove() {
+        int randNum = Randoms.pickNumberInRange(0, 9);
+        return randNum >= CAR_MOVE_THRESHOLD;
+    }
+
+    public List<String> getWinners(List<Car> participatingCars) {
+        int maxDistance = findMaxLocation(participatingCars);
+        return participatingCars.stream().filter(car -> car.getDistance() == maxDistance).map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private int findMaxLocation(List<Car> participatingCars) {
+        return participatingCars.stream().mapToInt(Car::getDistance).max().orElse(0);
     }
 }
