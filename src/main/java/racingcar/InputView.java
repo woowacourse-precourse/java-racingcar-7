9691c847974226d67935727b891fcc45561cd955
gabean13 <choegabin171@gmail.com>
2024-10-28 10:32:55 +0900

    feat(InputView) : input view 기능 구현
    - carList 입력 및 valid check 구현
    - 시도 횟수 입력 및 valid check 구현

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,60 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.LinkedHashMap;
+
+public class InputView {
+
+    public LinkedHashMap<String, Car> getCarList() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return convertStringToMap(Console.readLine());
+    }
+
+    protected LinkedHashMap<String, Car> convertStringToMap(String input) {
+        LinkedHashMap<String, Car> carMap = new LinkedHashMap<>();
+
+        String[] carNames = input.split(",");
+        for (String str : carNames) {
+            if (!isCarListInputValid(carMap, str)) {
+                throw new IllegalArgumentException();
+            }
+            carMap.put(str, new Car(str));
+        }
+
+        return carMap;
+    }
+
+    protected boolean isCarListInputValid(LinkedHashMap<String, Car> map, String str) {
+        if (map.containsKey(str)) {
+            return false;
+        }
+
+        if (str.length() > 5 || str.length() == 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public Long getRoundNumber() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String input = Console.readLine();
+        if (!isRoundNumberValid(input)) {
+            throw new IllegalArgumentException();
+        }
+        return Long.parseLong(input);
+    }
+
+    protected boolean isRoundNumberValid(String input) {
+        try {
+            Long num = Long.parseLong(input);
+            if (num < 1L) {
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        return true;
+    }
+}
