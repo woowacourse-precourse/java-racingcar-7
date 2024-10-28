8f7700f4ea63879b499151fe2a067556f6ddf710
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-10-28 16:14:03 +0900

    feat : 자동차 이름 초기 입력 값 검증 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,8 +2,15 @@ package racingcar;
 
 public class Application {
     private final static String INPUT_PRINT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final static String INPUT_VALIDATION_EXCEPTION_MESSAGE = "자동차 이름 형식을 맞춰주세요. 자동차 이름은 필수 값입니다.";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+    public static void validateInput(String input){
+        if(input == null || input.trim().isEmpty()){
+            throw new IllegalArgumentException(INPUT_VALIDATION_EXCEPTION_MESSAGE);
+        }
+    }
 
     public static void inputCarNamePrint(){
         System.out.println(INPUT_PRINT);
