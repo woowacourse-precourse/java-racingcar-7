08b33985d7e92fe16872caa287b7b2df17eeb036
BusanGukbap <dong2857210@gmail.com>
2024-10-28 15:59:56 +0900

    feat($Util) : create class - inputValidator

---
+++ src/main/java/racingcar/util/InputValidator.java
@@ -0,0 +1,18 @@
+package racingcar.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputValidator {
+
+    private static final String NAME_DELIMITER = ",";
+    private static final int MAX_NAME_LENGTH = 5;
+
+    public static List<String> splitCarNames() {
+
+    }
+
+    private static void validateCarName(String name) {
+
+    }
+}
