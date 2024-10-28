c52c70e02b07a202f1b89fb679227ac0b048106a
DongchannN <ldc991104@gmail.com>
2024-10-28 16:00:14 +0900

    refactor: 전략 패턴 도입
    Before : Car 인터페이스를 구현한 RandomMovingCar로 움직임 정의로 일반화 과잉 문제 발생
    
    After : 자동차의 움직임인 move()메서드만 전략패턴으로 추상화 향상

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -5,7 +5,8 @@ import java.util.List;
 import racingcar.dto.RacingRequestDto;
 import racingcar.dto.RacingResponseDto;
 import racingcar.model.car.Car;
-import racingcar.model.car.RandomMovingCar;
+import racingcar.model.car.strategy.MoveBehavior;
+import racingcar.model.car.strategy.RandomMove;
 import racingcar.model.racing.Racing;
 import racingcar.model.result.RoundResult;
 import racingcar.util.CarNameParser;
@@ -37,10 +38,10 @@ public class RacingController {
     }
 
     private Racing initRacing(RacingRequestDto racingRequest) {
+        MoveBehavior behavior = new RandomMove();
         List<String> carNames = CarNameParser.parseCarName(racingRequest.rawCarNames());
         List<Car> cars = carNames.stream()
-                .map(RandomMovingCar::new)
-                .map(car -> (Car) car)
+                .map(carName -> new Car(behavior, carName))
                 .toList();
         int roundsToRace = RaceRoundParser.parseRaceRound(racingRequest.rawRoundsToRace());
         return Racing.from(cars, roundsToRace);
--- src/main/java/racingcar/model/car/Car.java
+++ src/main/java/racingcar/model/car/Car.java
@@ -1,9 +1,44 @@
 package racingcar.model.car;
 
-public interface Car {
-    void move();
+import racingcar.exception.ErrorMessage;
+import racingcar.exception.RacingCarException;
+import racingcar.model.car.strategy.MoveBehavior;
 
-    String getName();
+public class Car {
+    private static final int MAX_CAR_NAME_LENGTH = 5;
 
-    int getMovedDistance();
+    private final MoveBehavior moveBehavior;
+    private final String name;
+    private int movedDistance;
+
+    public Car(MoveBehavior moveBehavior, String name) {
+        validateName(name);
+        this.moveBehavior = moveBehavior;
+        this.name = name;
+        movedDistance = 0;
+    }
+
+
+    public void move() {
+        int moveAmount = moveBehavior.getMoveAmount();
+        movedDistance += moveAmount;
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getMovedDistance() {
+        return this.movedDistance;
+    }
+
+    private static void validateName(String carName) {
+        if (carName == null || carName.isBlank()) {
+            throw new RacingCarException(ErrorMessage.EMPTY_CAR_NAME);
+        }
+        if (carName.length() > MAX_CAR_NAME_LENGTH) {
+            throw new RacingCarException(ErrorMessage.CAR_NAME_TOO_LONG);
+        }
+    }
 }
