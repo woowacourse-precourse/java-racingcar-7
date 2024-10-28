1dfa0f0e11c488d356fc8fe149e65d55b096cf3a
JGM <cnxw4570123@gmail.com>
2024-10-28 22:29:02 +0900

    fix : 게임 실행 시 시도 횟수 이상 반복하던 버그 수정
    - IntStream.iterate()에서 증감식이 제대로 작동하지 않았던 문제 수정

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -1,8 +1,15 @@
 package racingcar.domain;
 
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Game {
 	public static final String TRY_COUNT_MUST_BE_POSITIVE_NUMBER = "시도 횟수는 양의 정수여야 합니다.";
 	public static final String TRY_COUNT_MUST_BE_NUMBER = "시도 횟수는 숫자여야 합니다.";
+	private Supplier<Integer> numberSupplier = () -> Randoms.pickNumberInRange(0, 9);
 	private int tryCounts;
 	private Cars racers;
 
@@ -25,9 +32,28 @@ public class Game {
 		this.tryCounts = tryCounts;
 	}
 
+	private Game(Supplier<Integer> numberSupplier, int tryCounts, Cars racers) {
+		this.numberSupplier = numberSupplier;
+		this.tryCounts = tryCounts;
+		this.racers = racers;
+	}
+
 	public static Game fromInputAndCars(String input, Cars cars) {
 		int counts = validateIsNumber(input);
 		validateIsPositive(counts);
 		return new Game(counts, cars);
+	}
+
+	public static Game fromInputAndCarsWithSupplier(String input, Cars cars, Supplier<Integer> numberSupplier) {
+		int counts = validateIsNumber(input);
+		validateIsPositive(counts);
+		return new Game(numberSupplier, counts, cars);
+	}
+
+	public void play(Consumer<String> printer) {
+		IntStream.iterate(tryCounts, i -> i > 0, i -> --i)
+			.forEach(value -> {
+				printer.accept(racers.startRound(numberSupplier));
+			});
 	}
 }
