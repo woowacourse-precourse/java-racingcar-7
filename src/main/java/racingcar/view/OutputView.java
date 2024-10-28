6ee3002aa00914f2bc87eadb7cd94b8f9c72f62a
hmlssghm <kwonse326@gmail.com>
2024-10-28 21:42:44 +0900

    refactor(OutputView): 메시지 get 메서드 추출

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,37 +6,45 @@ import racingcar.view.messages.InteractionMessageEnum;
 import java.util.List;
 
 public class OutputView {
-    // TODO: InteractionMessageEnum.CAR_NAMES.getInteractionMessage() 이거 너무 긴데 매번 이렇게 길게 써야됨??
+
     public static void printCarNames() {
-        System.out.println(InteractionMessageEnum.CAR_NAMES.getInteractionMessage());
+        extractAndBreak(InteractionMessageEnum.CAR_NAMES);
     }
 
     public static void printAttemptCount() {
-        System.out.println(InteractionMessageEnum.ATTEMPT_COUNT.getInteractionMessage());
+        extractAndBreak(InteractionMessageEnum.ATTEMPT_COUNT);
     }
 
     public static void printResultMessage() {
         System.out.print("\n");
-        System.out.println(InteractionMessageEnum.RESULT_MESSAGE.getInteractionMessage());
+        extractAndBreak(InteractionMessageEnum.RESULT_MESSAGE);
     }
 
-    public static void printResult(List<Car> cars) { // TODO: 이거 static 맞나??
+    public static void printResult(List<Car> cars) {
         for (Car car: cars) {
-            System.out.println(car.getName() + InteractionMessageEnum.COLON.getInteractionMessage() +
-                    InteractionMessageEnum.HYPHEN.getInteractionMessage().repeat(car.getMove())); // TODO: 넘 길다.
+            System.out.print(car.getName());
+            extract(InteractionMessageEnum.COLON);
+            System.out.println(InteractionMessageEnum.HYPHEN.getInteractionMessage().repeat(car.getMove()));
         }
-        System.out.print("\n"); // TODO: 줄바꿈 이렇게?
+        System.out.print("\n");
     }
 
     public static void printWinner(List<Car> winners) {
-        // TODO: 이렇게 한줄한줄 해야되나?
-        System.out.print(InteractionMessageEnum.WINNERS_MESSAGE.getInteractionMessage());
-        System.out.print(InteractionMessageEnum.COLON.getInteractionMessage());
+        extract(InteractionMessageEnum.WINNERS_MESSAGE);
+        extract(InteractionMessageEnum.COLON);
 
-        // TODO: 아래 로직 메서드 분리 할지말지?
         for (int i = 0; i < winners.size() - 1; i++) {
-            System.out.print(winners.get(i).getName() + InteractionMessageEnum.COMMA.getInteractionMessage());
+            System.out.print(winners.get(i).getName());
+            extract(InteractionMessageEnum.COMMA);
         }
         System.out.println(winners.get(winners.size() - 1).getName());
+    }
+
+    private static void extractAndBreak(InteractionMessageEnum interactionMessageEnum) {
+        System.out.println(interactionMessageEnum.getInteractionMessage());
+    }
+
+    private static void extract(InteractionMessageEnum interactionMessageEnum) {
+        System.out.print(interactionMessageEnum.getInteractionMessage());
     }
 }
