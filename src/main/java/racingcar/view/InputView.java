3f2b262a1f17d11a787ed3af7f186607129912a5
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-10-28 20:12:05 +0900

    refactor: 입력 메서드 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,47 +9,44 @@ import java.util.regex.Pattern;
 
 public class InputView {
     // 자동차 이름 입력 메소드
-    public static String[] inputCars(){
+    public static String inputCars(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String text = Console.readLine();
+        return text;
+    }
+
+    public static String[] parseCars(String text) {
         text = text.replaceAll("^,+|,+$", ""); // 앞뒤 , 오는경우 제거
         String[] cars = text.split(",");
-        validateCars(cars);
-        return cars;
-    }
-
-    // 시도 횟수 입력 메소드
-    public static int inputAttempts() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String input = Console.readLine();
-        int attempts = validateAttemptsInput(input);
-        return attempts;
-    }
-
-    // 자동차 이름 유효성 검사
-    public static void testValidateCars(String[] cars) {
-        validateCars(cars);
-    }
-    // 시도 횟수 유효성 검사
-    public static int testValidateAttemptsInput(String input) {
-        return validateAttemptsInput(input);
-    }
-
-    private static void validateCars(String[] cars) {
-        Set<String> cheakUnique = new HashSet<>();
-        Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+$");
-
-        for (String name : cars) {
-            if (name.trim().isEmpty()) {
-                throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
-            }
+        if (text.isEmpty()) {
+            throw new IllegalArgumentException("자동차는 최소 1대이상 입력해야합니다.");
         }
         cars = Arrays.stream(cars)
                 .map(String::trim)
                 .filter(name -> !name.isEmpty())
                 .toArray(String[]::new);
+        validateCars(cars);
+        return cars;
+    }
 
-        if (cars.length <2 ) throw new IllegalArgumentException("자동차는 최소 2대 이상 입력해야 합니다.");
+
+    // 시도 횟수 입력 메소드
+    public static String inputAttempts() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String input = Console.readLine();
+        return input;
+    }
+
+    public static int parseAttempts(String input) {
+        int attempts = validateAttemptsInput(input);
+        return attempts;
+    }
+
+
+    private static void validateCars(String[] cars) {
+        Set<String> cheakUnique = new HashSet<>();
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+$");
+
         for (String name : cars) {
             if (name.length() > 5) throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
             if (!pattern.matcher(name).matches()) throw new IllegalArgumentException("자동차 이름은 영문자와 숫자만 포함해야 합니다.");
