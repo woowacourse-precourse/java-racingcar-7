25f2bf05d25cb30fb8078e3ad1a7d5d6041ad6fb
rhxfkg <rhxfkg@gmail.com>
2024-10-28 16:41:46 +0900

    Update
    Car객체 사용하도록 수정

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,30 +1,35 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 public class Racing {
-    public int[] racing(String[] carList, int number) {
-        int[] racingResult = new int[carList.length];
-        for (int i = 0; i < carList.length; i++) {
-            racingResult[i] = countMovements(number);
-            printRacingResult(carList[i], racingResult[i]);
+    private static final String ONE_STEP = "-";
+
+    public static void start(Car[] cars, int attemptCount) {
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < attemptCount; i++) {
+            Racing.racing(cars);
+        }
+        System.out.print("최종 우승자 : ");
+        Winner.printFinalWinner(cars);
+    }
+
+    public static void racing(Car[] cars) {
+        carMove(cars);
+        for (Car car : cars) {
+            printRacingResult(car, car.getMoveCount());
         }
         System.out.println();
-        return racingResult;
     }
 
-    private int countMovements(int number) {
-        int count = 0;
-        for (int i = 0; i < number; i++) {
-            if (Randoms.pickNumberInRange(0, 9) >= 4) {
-                count++;
+    private static void carMove(Car[] cars) {
+        for (Car car : cars) {
+            if (camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 9) >= 4) {
+                car.addMoveCount();
             }
         }
-        return count;
     }
 
-    private void printRacingResult(String carName, int racingResult) {
-        System.out.print(carName + " : ");
-        System.out.println("-".repeat(racingResult));
+    private static void printRacingResult(Car car, int moveCount) {
+        System.out.print(car.getName() + " : ");
+        System.out.println(ONE_STEP.repeat(moveCount));
     }
 }
