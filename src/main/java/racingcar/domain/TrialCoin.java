416e2c424d09b24de84cf85881af6cab2ce3704d
Park - Jeonghwan <990706leo@gmail.com>
2024-10-28 17:01:15 +0900

    refactor(TrialCoin): 메서드 및 변수 네이밍 수정

--- src/main/java/racingcar/domain/TrialCoin.java
+++ src/main/java/racingcar/domain/TrialCoin.java
@@ -3,25 +3,25 @@ package racingcar.domain;
 public class TrialCoin {
 
     private static final int MIN_TRIAL_COUNT = 0;
-    private static final int DECREASE_AMOUT = 1;
+    private static final int DECREASE_AMOUNT = 1;
 
-    private int trialCount;
+    private int trialCoin;
 
     public TrialCoin(int value) {
         validateNotNegative(value);
-        this.trialCount = value;
+        this.trialCoin = value;
     }
 
     public boolean isCoinLeft() {
-        return trialCount > 0;
+        return trialCoin > 0;
     }
 
     public void decrease() {
-        trialCount -= DECREASE_AMOUT;
+        trialCoin -= DECREASE_AMOUNT;
     }
 
-    private void validateNotNegative(int left) {
-        if (left < MIN_TRIAL_COUNT) {
+    private void validateNotNegative(int rest) {
+        if (rest < MIN_TRIAL_COUNT) {
             throw new IllegalArgumentException("시도횟수는 음수이면 안됩니다");
         }
     }
