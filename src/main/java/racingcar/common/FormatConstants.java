f841323db970aa1dcacffb36db7320fbe68c7ad0
koseonje <kosunje1344@naver.com>
2024-10-28 17:59:07 +0900

    refactor : 상수관리 클래스 final선언 및 기본생성자 private으로 변경

--- src/main/java/racingcar/common/FormatConstants.java
+++ src/main/java/racingcar/common/FormatConstants.java
@@ -1,6 +1,10 @@
 package racingcar.common;
 
-public class FormatConstants {
+public final class FormatConstants {
+
+    private FormatConstants() {
+
+    }
 
     public static final String LINE_BREAKER = "\n";
     public static final String DOUBLE_LINE_BREAKER = "\n\n";
--- src/main/java/racingcar/common/RacingCarConstant.java
+++ src/main/java/racingcar/common/RacingCarConstant.java
@@ -1,9 +1,14 @@
 package racingcar.common;
 
-public class RacingCarConstant {
+public final class RacingCarConstant {
+
+    private RacingCarConstant() {
+
+    }
 
     public static final String FORWARD_BAR = "-";
     public static final String DEFAULT_STATE = "";
+    public static final String PROGRESS_SEPARATOR = " : ";
     public static final int MIN_RANDOM_BOUND = 0;
     public static final int MAX_RANDOM_BOUND = 9;
     public static final int MOVE_THRESHOLD = 4;
