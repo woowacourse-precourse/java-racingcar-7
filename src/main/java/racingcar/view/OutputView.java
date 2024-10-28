abd94adc41e1462b81b0458b4cff5c39fea22c6a
ImSeongHun <daltjdgns111@naver.com>
2024-10-28 10:41:59 +0900

    feat(OutputView) : 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,18 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
+import java.util.List;
+
 public class OutputView {
+    public static void printRaceStatus(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + car.getPositionAsString());
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
 }
