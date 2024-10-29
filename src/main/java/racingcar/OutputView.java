eca15160437e4ae19cb994bacba8aee837dee017
Yehun <97829817+yhyh4420@users.noreply.github.com>
2024-10-29 17:45:15 +0900

    Update src/main/java/racingcar/OutputView.java
    Co-authored-by: 커찬 <44027393+leegwichan@users.noreply.github.com>

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -5,6 +5,6 @@ import java.util.List;
 public class OutputView {
 
     public void displayResult(List listWinner) {
-        System.out.print("최종 우승자 : " + String.join(",", listWinner));
+        System.out.print("최종 우승자 : " + String.join(", ", listWinner));
     }
 }
