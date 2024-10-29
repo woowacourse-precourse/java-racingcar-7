17dfe228bd8413ffbe0bbd9cb78c397fca30cc48
Si Yeong Park <psy915@swu.ac.kr>
2024-10-29 23:35:36 +0900

    refactor(OutputView): printError 메서드 제거

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,20 +1,17 @@
 package racingcar.view;
 
-import racingcar.model.domain.GameResult;
 import racingcar.model.domain.RoundResult;
 
 import java.util.List;
 
 public class OutputView {
-    public static void printRecord(List<RoundResult> roundResults) {
+    public static void printResult(List<RoundResult> roundResults) {
+        System.out.println("실행 결과");
         for (RoundResult roundResult : roundResults) {
-            System.out.print(roundResult + "\n");
+            System.out.print(roundResult);
         }
     }
     public static void printWinner(List<String> winners) {
         System.out.printf("최종 우승자 : %s\n", String.join(", ", winners));
-    }
-    public static void printError(String message) {
-        System.out.println(message + "\n입력 오류로 프로그램을 종료합니다.");
     }
 }
