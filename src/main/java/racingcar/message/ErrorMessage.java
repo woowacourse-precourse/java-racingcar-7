d9bc42978023f4347f799eacee3b61db5233d9d1
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-30 17:45:27 +0900

    refactor: 중복되는 에러코드 삭제

--- src/main/java/racingcar/message/ErrorMessage.java
+++ src/main/java/racingcar/message/ErrorMessage.java
@@ -1,7 +1,6 @@
 package racingcar.message;
 
 public enum ErrorMessage {
-    INVALID_NAME_LENGTH("자동차 이름은 5자 이하만 가능합니다."),
     INVALID_COMMA_INPUT("자동차 이름으로 ',' 는 사용할 수 없습니다."),
     INVALID_NUMBER_FORMAT("시도 횟수 입력은 0 이상의 정수 형식이어야 합니다."),
     CAR_NAME_IS_EMPTY("자동차 이름이 비어있습니다."),
