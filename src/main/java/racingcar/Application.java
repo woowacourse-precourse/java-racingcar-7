787edd6f4979bbc64f5ca7e8c77db413a9e65bd7
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-30 20:12:09 +0900

    feat: 실행만을 위한 RunnerConfig 클래스 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,9 @@
 package racingcar;
 
-import racingcar.controller.RacingController;
+import racingcar.config.RunnerConfig;
 
 public class Application {
     public static void main(String[] args) {
-        new RacingController().playGame();
+        RunnerConfig.getInstance().run();
     }
 }
