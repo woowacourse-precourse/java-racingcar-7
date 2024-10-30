7c35fdd375f5df2f433f87fa32c5c091c2c25a4a
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-30 20:41:13 +0900

    refactor: 진입점 방식 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,15 @@
 package racingcar;
 
-import racingcar.config.RunnerConfig;
+import racingcar.controller.RacingController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        RunnerConfig.getInstance().run();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        RacingController racingController = new RacingController(inputView, outputView);
+
+        racingController.playGame();
     }
 }
