e4bc9933fbdeed306271c17025ee3003ab66b988
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-30 19:50:41 +0900

    refactor: 불필요한 Cars 인터페이스 삭제

--- src/main/java/racingcar/model/Cars.java
+++
@@ -1,9 +0,0 @@
-package racingcar.model;
-
-import java.util.Collection;
-
-public interface Cars {
-    void moveCars();
-    Collection<Car> getCars();
-}
-
--- src/main/java/racingcar/model/RaceProcess.java
+++ src/main/java/racingcar/model/RaceProcess.java
@@ -1,53 +1,27 @@
 package racingcar.model;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
 
-import racingcar.message.ErrorMessage;
-import racingcar.exception.CarNameException;
 import racingcar.util.MoveStrategy;
+import racingcar.validation.CarsValidator;
 
-public class RaceProcess implements Cars {
+public class RaceProcess {
     private final MoveStrategy moveStrategy;
     private final Collection<Car> cars;
 
-    public RaceProcess(Collection<Car> cars, MoveStrategy moveStrategy) {
-        validateEmpty(cars);
-        validateDuplicateNames(cars);
+    public RaceProcess(Collection<Car> cars, MoveStrategy moveStrategy, CarsValidator carsValidator) {
+        carsValidator.validate(cars);
         this.cars = cars;
         this.moveStrategy = moveStrategy;
     }
 
-    @Override
     public void moveCars() {
         cars.stream()
                 .filter(car -> moveStrategy.isMovable())
                 .forEach(Car::moveForward);
     }
 
-    @Override
     public Collection<Car> getCars() {
         return cars;
-    }
-
-    private void validateEmpty(Collection<Car> cars) {
-        Optional.ofNullable(cars)
-                .filter(c -> !c.isEmpty())
-                .orElseThrow(() -> new CarNameException(ErrorMessage.CAR_NAME_IS_EMPTY.getMessage()));
-    }
-
-    private void validateDuplicateNames(Collection<Car> cars) {
-        Set<String> carNames = new HashSet<>();
-        cars.stream()
-                .map(Car::getName)
-                .forEach(name -> validateUniqueName(name, carNames));
-    }
-
-    private void validateUniqueName(String name, Set<String> carNames) {
-        Optional.of(name)
-                .filter(carNames::add)
-                .orElseThrow(() -> new CarNameException(ErrorMessage.CAR_NAME_IS_DUPLICATED.getMessage()));
     }
 }
