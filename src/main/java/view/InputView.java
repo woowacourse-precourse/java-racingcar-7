60fa378f9c273317230b015019edf5161d7d2972
김동진 <dongjin403@naver.com>
2025-01-05 01:10:57 +0900

    refactor : 테스트 할 수 있도록 input값을 파라미터로 받도록 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,10 +8,14 @@ import racingcar.Car;
 
 public class InputView {
 
-    public List<Car> makeCars() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    public List<Car> inputCarNames() {
+        OutputView.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return makeCars(Console.readLine().trim());
+    }
+
+    public List<Car> makeCars(String inputCarNames) {
         List<Car> cars = new ArrayList<>();
-        String[] carNames = Console.readLine().trim().split(",");
+        String[] carNames = inputCarNames.split(",");
         for (String carName : carNames) {
             cars.add(new Car(carName));
         }
@@ -19,17 +23,23 @@ public class InputView {
     }
 
     public int inputAttemptCount() {
-        System.out.println("시도할 회수는 몇회인가요? (최대 10억까지 가능)");
+        OutputView.println("시도할 회수는 몇회인가요? (최대 10억까지 가능)");
+        return validCount(Console.readLine().trim());
+    }
+
+    public int validCount(String inputCount) {
         try {
-            int attemptCount = Integer.parseInt(Console.readLine().trim());
+            int attemptCount = Integer.parseInt(inputCount);
             if (attemptCount < 1 || attemptCount > 1_000_000_000) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("시도 횟수는 1 이상 1,000,000,000 이하의 숫자여야 합니다.");
             }
             return attemptCount;
+        } catch (NumberFormatException e) {
+            CustomException.throwException("숫자만 입력해야 합니다.");
         } catch (IllegalArgumentException e) {
-            CustomException.throwException("시도 횟수를 정확히 입력해주세요(숫자만 입력)");
+            CustomException.throwException(e.getMessage());
         }
-        return 0;
+        return 0; // 필요없는 줄
     }
 
 
