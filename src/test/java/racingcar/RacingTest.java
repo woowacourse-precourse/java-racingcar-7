67e98619326ab602332c2d67e5e51e843d3f626d
김동진 <dongjin403@naver.com>
2025-01-05 20:58:46 +0900

    test : 최종 우승자 찾는 함수 테스트 코드 작성

---
+++ src/test/java/racingcar/RacingTest.java
@@ -0,0 +1,40 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RacingTest {
+
+    private Racing racing;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @DisplayName("우승자를 정확히 구하는지 확인한다")
+    @Test
+    void racingTest1() {
+        List<Car> cars = new ArrayList<>();
+        Car car1 = new Car("Car1");
+        Car car2 = new Car("Car2");
+        Car car3 = new Car("Car3");
+        car1.move(Car.MINIMUM_MOVE_VALUE);
+        car2.move(Car.MINIMUM_MOVE_VALUE);
+        car3.move(Car.MINIMUM_MOVE_VALUE - 1);
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        racing = new Racing(cars, 5);
+
+        List<String> winners = racing.findWinners();
+        Assertions.assertThat(winners.size()).isEqualTo(2);
+        Assertions.assertThat(winners.get(0)).isEqualTo("Car1");
+        Assertions.assertThat(winners.get(1)).isEqualTo("Car2");
+    }
+
+}
