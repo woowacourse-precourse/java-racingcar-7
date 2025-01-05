330a127e252146e5313699b6df01f6f76cee55d5
김동진 <dongjin403@naver.com>
2025-01-05 17:29:17 +0900

    refactor : Racing객체가 경주자동차 정보와 진행 횟수를 가지게 변경

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -2,16 +2,52 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Racing {
+
+    List<Car> cars;
+    final int attemptCount;
+
+    public Racing(List<Car> cars, int attemptCount) {
+        this.cars = cars;
+        this.attemptCount = attemptCount;
+    }
 
     public int makeRandomValue() {
         return Randoms.pickNumberInRange(0, 9);
     }
 
-    public void playRacing(List<Car> cars) {
+    public void playRacing() {
         for (Car car : cars) {
             car.move(makeRandomValue());
         }
     }
+
+    public void printRacingResult() {
+        for (Car car : cars) {
+            car.printCarInfo();
+        }
+    }
+
+    public void printAllRacingResult() {
+        System.out.printf("%n실행 결과%n");
+        for (int i = 0; i < attemptCount; i++) {
+            playRacing();
+            printRacingResult();
+            System.out.println();
+        }
+    }
+
+    public List<String> findWinners() {
+        int maxPosition = cars.stream().mapToInt(Car::getPosition).max()
+                .orElseThrow(() -> new RuntimeException("No cars found"));
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,26 +1,21 @@
 package view;
 
 import java.util.List;
-import racingcar.Car;
 import racingcar.Racing;
 
 public class OutputView {
-
-    Racing racing = new Racing();
 
     public static void println(String message) {
         System.out.println(message);
     }
 
-    public void printRacingResult(List<Car> cars, int attemptCount) {
-        System.out.printf("%n실행 결과%n");
-        for (int i = 0; i < attemptCount; i++) {
-            racing.playRacing(cars);
-            for (Car car : cars) {
-                car.printCarInfo();
-            }
-            System.out.println();
-        }
+    public void printRacingResult(Racing racing) {
+        racing.printAllRacingResult();
+    }
+
+    public void printFinalWinners(Racing racing) {
+        List<String> winners = racing.findWinners();
+        System.out.printf("최종 우승자 : %s", String.join(", ", winners));
     }
 
 }
