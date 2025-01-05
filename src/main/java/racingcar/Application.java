ee955ed803e91e87c053d26784f9f5a39e3e007f
김동진 <dongjin403@naver.com>
2025-01-05 20:59:04 +0900

    feat : 프로그램 기능 완성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import java.util.List;
+import view.InputView;
+import view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        List<Car> cars = inputView.inputCarNames();
+        int attemptCount = inputView.inputAttemptCount();
+        Racing racing = new Racing(cars, attemptCount);
+
+        outputView.printRacingResult(racing);
+        outputView.printFinalWinners(racing);
     }
 }
