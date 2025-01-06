00c78ce75f0d4e297371ce9141b1fadf9111a566
김동진 <dongjin403@naver.com>
2025-01-06 23:57:52 +0900

    refactor : 메서드 선언 순서 변경 / getter 가장 밑으로 내림

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,29 +3,22 @@ package racingcar;
 import exception.CustomException;
 
 public class Car {
+    public static final int MINIMUM_MOVE_VALUE = 4;
+    private static final int MAXIMUM_CAR_NAME_LENGTH = 5;
+    private static final String FORWARD_UNIT = "-";
     private final String name;
     private int position = 0;
-    public static final int MINIMUM_MOVE_VALUE = 4;
-    public static final String FORWARD_UNIT = "-";
 
     public Car(String name) {
         verifyCarName(name);
         this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getPosition() {
-        return position;
-    }
-
     public void verifyCarName(String name) {
         if (name.isEmpty()) {
             CustomException.throwException("자동차 이름을 입력해주세요");
         }
-        if (name.length() > 5) {
+        if (name.length() > MAXIMUM_CAR_NAME_LENGTH) {
             CustomException.throwException("자동차 이름은 최대 5자입니다.");
         }
     }
@@ -42,6 +35,14 @@ public class Car {
             forwardUnits.append(FORWARD_UNIT);
         }
         System.out.printf("%s : %s%n", this.name, forwardUnits);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 
 }
