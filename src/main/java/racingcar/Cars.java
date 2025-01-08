6f277126aa51682fe7d738b2cded2614ac3f99c1
김동진 <dongjin403@naver.com>
2025-01-08 20:38:09 +0900

    refactor : cars를 일급 컬렉션으로 변경

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,38 @@
+package racingcar;
+
+import exception.CustomIllegalArgException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void playRacing(int randomValue) {
+        for (Car car : cars) {
+            car.move(randomValue);
+        }
+    }
+
+    public void printRacingResult() {
+        for (Car car : cars) {
+            car.printCarInfo();
+        }
+    }
+
+    public List<String> findWinners() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElseThrow(() -> new CustomIllegalArgException("No cars found"));
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+}
--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -2,51 +2,35 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Racing {
 
-    List<Car> cars;
-    final int attemptCount;
+    private final Cars cars;
+    private final int attemptCount;
+    private final int RANDOM_START = 0;
+    private final int RANDOM_END = 9;
 
     public Racing(List<Car> cars, int attemptCount) {
-        this.cars = cars;
+        this.cars = new Cars(cars);
         this.attemptCount = attemptCount;
     }
 
-    public int makeRandomValue() {
-        return Randoms.pickNumberInRange(0, 9);
-    }
-
-    public void playRacing() {
-        for (Car car : cars) {
-            car.move(makeRandomValue());
-        }
-    }
-
-    public void printRacingResult() {
-        for (Car car : cars) {
-            car.printCarInfo();
-        }
-    }
 
     public void printAllRacingResult() {
         System.out.printf("%n실행 결과%n");
         for (int i = 0; i < attemptCount; i++) {
-            playRacing();
-            printRacingResult();
+            cars.playRacing(makeRandomValue());
+            cars.printRacingResult();
             System.out.println();
         }
     }
 
-    public List<String> findWinners() {
-        int maxPosition = cars.stream().mapToInt(Car::getPosition).max()
-                .orElseThrow(() -> new RuntimeException("No cars found"));
+    public int makeRandomValue() {
+        return Randoms.pickNumberInRange(RANDOM_START, RANDOM_END);
+    }
 
-        return cars.stream()
-                .filter(car -> car.getPosition() == maxPosition)
-                .map(Car::getName)
-                .collect(Collectors.toList());
+    public List<String> findWinners() {
+        return cars.findWinners();
     }
 
 
